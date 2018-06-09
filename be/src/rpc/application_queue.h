7e2a3aa1b384e3f4ef3d219a3b5a2bb351c55ad8
李超勇 <lichaoyong121@qq.com>
2018-06-09 19:12:16 +0800

    modify the license (#203)
    some license is replaced not correctly.

--- be/src/rpc/application_queue.h
+++ be/src/rpc/application_queue.h
@@ -1,17 +1,21 @@
-// Copyright (c) 2017, Baidu.com, Inc. All Rights Reserved
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright (C) 2007-2016 Hypertable, Inc.
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+// This file is part of Hypertable.
+// 
+// Hypertable is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or any later version.
 //
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Hypertable is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
 
 #ifndef BDG_PALO_BE_SRC_RPC_APPLICATION_QUEUE_H
 #define BDG_PALO_BE_SRC_RPC_APPLICATION_QUEUE_H
