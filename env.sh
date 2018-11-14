78f5e485a485ce40a63bc8e81f04faf0dcbbf7b4
lide-reed <lide@apache.org>
2018-11-14 10:43:40 +0800

    Update tip when gcc version not right

--- env.sh
+++ env.sh
@@ -78,6 +78,7 @@ gcc_ver=`${DORIS_GCC_HOME}/bin/gcc -dumpversion`
 required_ver="5.3.1"
 if [[ ! "$(printf '%s\n' "$required_ver" "$gcc_ver" | sort -V | head -n1)" = "$required_ver" ]]; then 
     echo "Error: GCC version (${gcc_ver}) must be greater than or equal to ${required_ver}"
+    echo "DORIS_GCC_HOME is need to be set in env.sh after you installed a new version of GCC."
     exit 1
 fi
 
