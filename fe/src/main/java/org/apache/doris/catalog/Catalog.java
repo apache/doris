dbe56d70f3c36aec395f9412b685610733cfccad
yiguolei <yiguolei@baidu.com>
2018-12-18 10:41:28 +0800

    Parse thrift port from cluster state

--- fe/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -3875,10 +3875,10 @@ public class Catalog {
 
             // properties
             sb.append("\nPROPERTIES (\n");
-            sb.append("\"host\" = \"").append(esTable.getHosts()).append("\",\n");
+            sb.append("\"hosts\" = \"").append(esTable.getHosts()).append("\",\n");
             sb.append("\"user\" = \"").append(esTable.getUserName()).append("\",\n");
             sb.append("\"password\" = \"").append(hidePassword ? "" : esTable.getPasswd()).append("\",\n");
-            sb.append("\"index\" = \"").append(esTable.getIndexName()).append("\"\n");
+            sb.append("\"index\" = \"").append(esTable.getIndexName()).append("\",\n");
             sb.append("\"type\" = \"").append(esTable.getMappingType()).append("\"\n");
             sb.append(");");
         }
