cefafe171fc591dea573e4fda37fcbc37197ee8f
lide-reed <lide@apache.org>
2018-12-27 16:12:46 +0800

    Complete build script

--- env.sh
+++ env.sh
@@ -22,9 +22,11 @@ if [[ -z ${DORIS_HOME} ]]; then
 fi
 
 # check OS type
-if [[ "$OSTYPE" != "linux-gnu" ]]; then
-    echo "Error: Unsupported OS type: $OSTYPE"
-    exit 1
+if [[ ! -z "$OSTYPE" ]]; then
+    if [[ "$OSTYPE" != "linux-gnu" ]]; then
+        echo "Error: Unsupported OS type: $OSTYPE"
+        exit 1
+    fi
 fi
 
 # include custom environment variables
