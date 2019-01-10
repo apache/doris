9eb0f3903e94fada7362955443777b03b3572f8e
kangpinghuang <40422952+kangpinghuang@users.noreply.github.com>
2019-01-10 20:21:31 +0800

    Add rowset reader context builder (#514)

--- be/src/olap/rowset/rowset_reader.h
+++ be/src/olap/rowset/rowset_reader.h
@@ -18,15 +18,7 @@
 #ifndef DORIS_BE_SRC_OLAP_ROWSET_ROWSET_READER_H
 #define DORIS_BE_SRC_OLAP_ROWSET_ROWSET_READER_H
 
-#include "olap/new_status.h"
-#include "olap/schema.h"
-#include "olap/column_predicate.h"
-#include "olap/row_cursor.h"
-#include "olap/row_block.h"
-#include "olap/lru_cache.h"
-#include "olap/olap_cond.h"
-#include "olap/delete_handler.h"
-#include "runtime/runtime_state.h"
+#include "olap/rowset/rowset_reader_context_builder.h"
 
 #include <memory>
 #include <unordered_map>
@@ -35,29 +27,6 @@ namespace doris {
 
 class RowsetReader;
 using RowsetReaderSharedPtr = std::shared_ptr<RowsetReader>;
-
-struct ReaderContext {
-	const RowFields& tablet_schema;
-    // projection columns
-    const std::vector<uint32_t>& return_columns;
-    // columns to load bloom filter index
-    // including columns in "=" or "in" conditions
-    const std::set<uint32_t>& load_bf_columns;
-    // column filter conditions by delete sql
-    const Conditions& conditions;
-    // column name -> column predicate
-    // adding column_name for predicate to make use of column selectivity
-    std::unordered_map<std::string, ColumnPredicate&> predicates;
-    const std::vector<RowCursor*>& lower_bound_keys;
-    std::vector<bool> is_lower_keys_included;
-    const std::vector<RowCursor*>& upper_bound_keys;
-    std::vector<bool> is_upper_keys_included;
-    const DeleteHandler& delete_handler;
-    OlapReaderStatistics* stats;
-    bool is_using_cache;
-    Cache* lru_cache;
-    RuntimeState* runtime_state;
-};
 
 class RowsetReader {
 public:
@@ -77,6 +46,6 @@ public:
     virtual void close() = 0;
 };
 
-}
+} // namespace doris
 
 #endif // DORIS_BE_SRC_OLAP_ROWSET_ROWSET_READER_H
