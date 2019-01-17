6bef41633c5b2c7553adf538aeca2b0eb99763ba
EmmyMiao87 <522274284@qq.com>
2019-01-17 14:19:13 +0800

    Add DORIS_THIRDPARTY env in docker image (#539)
    * Add param of specified thirdparty path
    1. The thirdparth path can be specify on build.sh: ./build.sh --thirdparty /specified/path/to/thirdparty
    2. If there are only thirdparty param of build.sh, it will build both fe and be
    3. Add unit test of routine load stmt
    4. Remove source code in docker image
    
    * Add DORIS_THIRDPARTY env in docker image
    1. Set DORIS_THIRDPARTY env in docker image. The build.sh will use /var/local/thirdparty instead of /source/code/thirdparty
    2. remove --thirdparty param of build.sh
    
    * Change image workdir to /root

--- fe/src/test/java/org/apache/doris/load/routineload/KafkaRoutineLoadJobTest.java
+++ fe/src/test/java/org/apache/doris/load/routineload/KafkaRoutineLoadJobTest.java
@@ -17,32 +17,9 @@
 
 package org.apache.doris.load.routineload;
 
-import org.apache.doris.catalog.Catalog;
-import org.apache.doris.catalog.Database;
-import org.apache.doris.common.AnalysisException;
-import org.apache.doris.common.LabelAlreadyUsedException;
-import org.apache.doris.common.MetaNotFoundException;
-import org.apache.doris.common.SystemIdGenerator;
-import org.apache.doris.load.RoutineLoadDesc;
-import org.apache.doris.qe.ConnectContext;
-import org.apache.doris.system.SystemInfoService;
-import org.apache.doris.thrift.TResourceInfo;
-import org.apache.doris.transaction.BeginTransactionException;
-import org.apache.doris.transaction.GlobalTransactionMgr;
-import org.apache.doris.transaction.TransactionState;
-
+import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.google.common.collect.Maps;
 import mockit.Deencapsulation;
 import mockit.Expectations;
 import mockit.Injectable;
@@ -50,15 +27,70 @@ import mockit.Mock;
 import mockit.MockUp;
 import mockit.Mocked;
 import mockit.Verifications;
+import org.apache.doris.analysis.ColumnSeparator;
+import org.apache.doris.analysis.CreateRoutineLoadStmt;
+import org.apache.doris.analysis.ParseNode;
+import org.apache.doris.analysis.PartitionNames;
+import org.apache.doris.analysis.TableName;
+import org.apache.doris.catalog.OlapTable;
+import org.apache.doris.catalog.Table;
+import org.apache.doris.common.LoadException;
+import org.apache.doris.load.RoutineLoadDesc;
+import org.apache.doris.catalog.Catalog;
+import org.apache.doris.catalog.Database;
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.LabelAlreadyUsedException;
+import org.apache.doris.common.MetaNotFoundException;
+import org.apache.doris.common.SystemIdGenerator;
+import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.system.SystemInfoService;
+import org.apache.doris.thrift.TResourceInfo;
+import org.apache.doris.transaction.BeginTransactionException;
+import org.apache.doris.transaction.GlobalTransactionMgr;
+import org.apache.doris.transaction.TransactionState;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class KafkaRoutineLoadJobTest {
 
+    private static final Logger LOG = LogManager.getLogger(KafkaRoutineLoadJobTest.class);
+
     private static final int DEFAULT_TASK_TIMEOUT_SECONDS = 10;
+
+    private String jobName = "job1";
+    private String dbName = "db1";
+    private String tableNameString = "table1";
+    private String topicName = "topic1";
+    private String serverAddress = "http://127.0.0.1:8080";
+    private String kafkaPartitionString = "1,2,3";
+
+    private PartitionNames partitionNames;
+
+    private ColumnSeparator columnSeparator = new ColumnSeparator(",");
 
     @Mocked
     ConnectContext connectContext;
     @Mocked
     TResourceInfo tResourceInfo;
+
+    @Before
+    public void init() {
+        List<String> partitionNameString = Lists.newArrayList();
+        partitionNameString.add("p1");
+        partitionNames = new PartitionNames(partitionNameString);
+    }
 
     @Test
     public void testBeNumMin(@Mocked KafkaConsumer kafkaConsumer,
@@ -96,7 +128,7 @@ public class KafkaRoutineLoadJobTest {
 
         KafkaRoutineLoadJob kafkaRoutineLoadJob =
                 new KafkaRoutineLoadJob("1", "kafka_routine_load_job", 1L,
-                                        1L, routineLoadDesc ,3, 0,
+                                        1L, routineLoadDesc, 3, 0,
                                         "", "", null);
         Assert.assertEquals(1, kafkaRoutineLoadJob.calculateCurrentConcurrentTaskNum());
     }
@@ -109,7 +141,7 @@ public class KafkaRoutineLoadJobTest {
                                          @Mocked RoutineLoadDesc routineLoadDesc)
             throws BeginTransactionException, LabelAlreadyUsedException, AnalysisException {
 
-        new Expectations(){
+        new Expectations() {
             {
                 connectContext.toResourceCtx();
                 result = tResourceInfo;
@@ -118,7 +150,7 @@ public class KafkaRoutineLoadJobTest {
 
         KafkaRoutineLoadJob kafkaRoutineLoadJob =
                 new KafkaRoutineLoadJob("1", "kafka_routine_load_job", 1L,
-                                        1L, routineLoadDesc , 3, 0,
+                                        1L, routineLoadDesc, 3, 0,
                                         "", "", null);
 
         new Expectations() {
@@ -158,7 +190,7 @@ public class KafkaRoutineLoadJobTest {
             throws AnalysisException, LabelAlreadyUsedException,
             BeginTransactionException {
 
-        new Expectations(){
+        new Expectations() {
             {
                 connectContext.toResourceCtx();
                 result = tResourceInfo;
@@ -167,7 +199,7 @@ public class KafkaRoutineLoadJobTest {
 
         RoutineLoadJob routineLoadJob =
                 new KafkaRoutineLoadJob("1", "kafka_routine_load_job", 1L,
-                                        1L, routineLoadDesc ,3, 0,
+                                        1L, routineLoadDesc, 3, 0,
                                         "", "", null);
         new Expectations() {
             {
@@ -216,5 +248,121 @@ public class KafkaRoutineLoadJobTest {
                         .getPartitions().get(0));
             }
         };
+    }
+
+    @Test
+    public void testFromCreateStmtWithErrorPartition(@Mocked Catalog catalog,
+                                                     @Injectable Database database,
+                                                     @Injectable OlapTable table) throws LoadException {
+        CreateRoutineLoadStmt createRoutineLoadStmt = initCreateRoutineLoadStmt();
+        RoutineLoadDesc routineLoadDesc = new RoutineLoadDesc(columnSeparator, null, null, partitionNames.getPartitionNames());
+        Deencapsulation.setField(createRoutineLoadStmt, "routineLoadDesc", routineLoadDesc);
+
+        new Expectations() {
+            {
+                catalog.getDb(dbName);
+                result = database;
+                database.getTable(tableNameString);
+                result = table;
+                table.getPartition("p1");
+                result = null;
+                table.getType();
+                result = Table.TableType.OLAP;
+            }
+        };
+
+        try {
+            KafkaRoutineLoadJob kafkaRoutineLoadJob = KafkaRoutineLoadJob.fromCreateStmt(createRoutineLoadStmt);
+            Assert.fail();
+        } catch (AnalysisException e) {
+            LOG.info(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testFromCreateStmtWithErrorTable(@Mocked Catalog catalog,
+                                                 @Injectable Database database) throws LoadException {
+        CreateRoutineLoadStmt createRoutineLoadStmt = initCreateRoutineLoadStmt();
+        RoutineLoadDesc routineLoadDesc = new RoutineLoadDesc(columnSeparator, null, null, partitionNames.getPartitionNames());
+        Deencapsulation.setField(createRoutineLoadStmt, "routineLoadDesc", routineLoadDesc);
+
+        new Expectations() {
+            {
+                catalog.getDb(dbName);
+                result = database;
+                database.getTable(tableNameString);
+                result = null;
+            }
+        };
+
+        try {
+            KafkaRoutineLoadJob kafkaRoutineLoadJob = KafkaRoutineLoadJob.fromCreateStmt(createRoutineLoadStmt);
+            Assert.fail();
+        } catch (AnalysisException e) {
+            LOG.info(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testFromCreateStmt(@Mocked Catalog catalog,
+                                   @Injectable Database database,
+                                   @Injectable OlapTable table) throws LoadException, AnalysisException {
+        CreateRoutineLoadStmt createRoutineLoadStmt = initCreateRoutineLoadStmt();
+        RoutineLoadDesc routineLoadDesc = new RoutineLoadDesc(columnSeparator, null, null, partitionNames.getPartitionNames());
+        Deencapsulation.setField(createRoutineLoadStmt, "routineLoadDesc", routineLoadDesc);
+        List<Integer> kafkaIntegerList = Lists.newArrayList();
+        for (String s : kafkaPartitionString.split(",")) {
+            kafkaIntegerList.add(Integer.valueOf(s));
+        }
+        Deencapsulation.setField(createRoutineLoadStmt, "kafkaPartitions", kafkaIntegerList);
+        Deencapsulation.setField(createRoutineLoadStmt, "kafkaEndpoint", serverAddress);
+        Deencapsulation.setField(createRoutineLoadStmt, "kafkaTopic", topicName);
+        long dbId = 1l;
+        long tableId = 2L;
+
+        new Expectations() {
+            {
+                catalog.getDb(dbName);
+                result = database;
+                database.getTable(tableNameString);
+                result = table;
+                database.getId();
+                result = dbId;
+                table.getId();
+                result = tableId;
+                table.getType();
+                result = Table.TableType.OLAP;
+            }
+        };
+
+        KafkaRoutineLoadJob kafkaRoutineLoadJob = KafkaRoutineLoadJob.fromCreateStmt(createRoutineLoadStmt);
+        Assert.assertEquals(jobName, kafkaRoutineLoadJob.getName());
+        Assert.assertEquals(dbId, kafkaRoutineLoadJob.getDbId());
+        Assert.assertEquals(tableId, kafkaRoutineLoadJob.getTableId());
+        Assert.assertEquals(serverAddress, Deencapsulation.getField(kafkaRoutineLoadJob, "serverAddress"));
+        Assert.assertEquals(topicName, Deencapsulation.getField(kafkaRoutineLoadJob, "topic"));
+        List<Integer> kafkaPartitionResult = Deencapsulation.getField(kafkaRoutineLoadJob, "kafkaPartitions");
+        Assert.assertEquals(kafkaPartitionString, Joiner.on(",").join(kafkaPartitionResult));
+        Assert.assertEquals(routineLoadDesc, kafkaRoutineLoadJob.getRoutineLoadDesc());
+    }
+
+    private CreateRoutineLoadStmt initCreateRoutineLoadStmt() {
+        TableName tableName = new TableName(dbName, tableNameString);
+        List<ParseNode> loadPropertyList = new ArrayList<>();
+        loadPropertyList.add(columnSeparator);
+        loadPropertyList.add(partitionNames);
+        Map<String, String> properties = Maps.newHashMap();
+        properties.put(CreateRoutineLoadStmt.DESIRED_CONCURRENT_NUMBER_PROPERTY, "2");
+        String typeName = LoadDataSourceType.KAFKA.name();
+        Map<String, String> customProperties = Maps.newHashMap();
+
+        customProperties.put(CreateRoutineLoadStmt.KAFKA_TOPIC_PROPERTY, topicName);
+        customProperties.put(CreateRoutineLoadStmt.KAFKA_ENDPOINT_PROPERTY, serverAddress);
+        customProperties.put(CreateRoutineLoadStmt.KAFKA_PARTITIONS_PROPERTY, kafkaPartitionString);
+
+        CreateRoutineLoadStmt createRoutineLoadStmt = new CreateRoutineLoadStmt(jobName, tableName,
+                                                                                loadPropertyList, properties,
+                                                                                typeName, customProperties);
+        return createRoutineLoadStmt;
     }
 }
