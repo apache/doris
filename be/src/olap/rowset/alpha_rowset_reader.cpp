cb074295f72ee16c3fcc1bf24bbcd548f67e085a
huangkangping <huangkangping@baidu.com>
2019-02-25 09:56:33 +0800

    fix pr problems

--- be/src/olap/rowset/alpha_rowset_reader.cpp
+++ be/src/olap/rowset/alpha_rowset_reader.cpp
@@ -143,33 +143,20 @@ OLAPStatus AlphaRowsetReader::_get_next_not_filtered_row(size_t pos, RowCursor**
         return OLAP_ERR_DATA_EOF;
     }
     bool found_row = false;
-    while (!found_row && current_row_block != nullptr) {
-        // get next row that is not filtered by delete conditions
-        while (current_row_block->has_remaining()) {
-            size_t pos = current_row_block->pos();
-            current_row_block->get_row(pos, *row);
-            const DeleteHandler* delete_handler = _current_read_context->delete_handler;
-            bool row_filtered = delete_handler->is_filter_data(
-                    _alpha_rowset_meta->version().second, *(*row));
-            if (row_filtered) {
-                _current_read_context->stats->rows_del_filtered++;
-                current_row_block->pos_inc();
-                if (!current_row_block->has_remaining()) {
-                    OLAPStatus status = _get_next_block(pos, &current_row_block);
-                    if (status != OLAP_SUCCESS) {
-                        LOG(WARNING) << "_get_next_block failed, status:" << status;
-                        return status;
-                    }
-                }
-            } else {
-                found_row = true;
-                break;
+    const DeleteHandler* delete_handler = _current_read_context->delete_handler;
+    while (!found_row) {
+        if (!current_row_block->has_remaining()) {
+            OLAPStatus status = _get_next_block(pos, &current_row_block);
+            if (status != OLAP_SUCCESS) {
+                LOG(WARNING) << "_get_next_block failed, status:" << status;
+                *row = nullptr;
+                return status;
             }
         }
-    }
-    if (!found_row) {
-        *row = nullptr;
-        return OLAP_ERR_DATA_EOF;
+
+        size_t pos = current_row_block->pos();
+        current_row_block->get_row(pos, *row);
+        found_row = !delete_handler->is_filter_data(_alpha_rowset_meta->version().second, *(*row));
     }
     
     return OLAP_SUCCESS;
@@ -195,10 +182,9 @@ OLAPStatus AlphaRowsetReader::_get_next_row_for_singleton_rowset(RowCursor** row
         return OLAP_ERR_DATA_EOF;
     }
     *row = min_row;
-    RowBlock* row_block = _row_blocks[min_index];
-    row_block->pos_inc();
-    if (!row_block->has_remaining()) {
-        OLAPStatus status = _get_next_block(min_index, &row_block);
+    _row_blocks[min_index]->pos_inc();
+    if (!_row_blocks[min_index]->has_remaining()) {
+        OLAPStatus status = _get_next_block(min_index, &_row_blocks[min_index]);
         if (status == OLAP_ERR_DATA_EOF) {
             _row_blocks[min_index] = nullptr;
             return OLAP_SUCCESS;
@@ -213,10 +199,9 @@ OLAPStatus AlphaRowsetReader::_get_next_row_for_singleton_rowset(RowCursor** row
 OLAPStatus AlphaRowsetReader::_get_next_row_for_cumulative_rowset(RowCursor** row) {
     size_t pos = 0;
     OLAPStatus status = _get_next_not_filtered_row(pos, row);
-    RowBlock* row_block = _row_blocks[pos];
-    row_block->pos_inc();
-    if (!row_block->has_remaining()) {
-        OLAPStatus status = _get_next_block(pos, &row_block);
+    _row_blocks[pos]->pos_inc();
+    if (!_row_blocks[pos]->has_remaining()) {
+        OLAPStatus status = _get_next_block(pos, &_row_blocks[pos]);
         if (status == OLAP_ERR_DATA_EOF) {
             _row_blocks[pos] = nullptr;
             return OLAP_SUCCESS;
