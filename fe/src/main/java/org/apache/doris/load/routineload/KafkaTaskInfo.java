c6f33544f9f9874a8c9f087c119dd0aff1d57842
EmmyMiao87 <522274284@qq.com>
2019-03-04 11:25:58 +0800

    Submit routine load task immediately (#682)
    1. Use submit_routine_load_task instead of agentTaskQueue
    2. Remove thrift dependency in StreamLoadPlanner and StreamLoadScanNode

--- fe/src/main/java/org/apache/doris/load/routineload/KafkaTaskInfo.java
+++ fe/src/main/java/org/apache/doris/load/routineload/KafkaTaskInfo.java
@@ -17,12 +17,26 @@
 
 package org.apache.doris.load.routineload;
 
+import com.google.common.base.Joiner;
 import org.apache.doris.catalog.Catalog;
+import org.apache.doris.catalog.Database;
+import org.apache.doris.catalog.OlapTable;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.LabelAlreadyUsedException;
 import org.apache.doris.common.LoadException;
+import org.apache.doris.common.UserException;
+import org.apache.doris.planner.StreamLoadPlanner;
 import org.apache.doris.task.KafkaRoutineLoadTask;
 import org.apache.doris.task.RoutineLoadTask;
+import org.apache.doris.task.StreamLoadTask;
+import org.apache.doris.thrift.TExecPlanFragmentParams;
+import org.apache.doris.thrift.TFileFormatType;
+import org.apache.doris.thrift.TFileType;
+import org.apache.doris.thrift.TKafkaLoadInfo;
+import org.apache.doris.thrift.TLoadSourceType;
+import org.apache.doris.thrift.TRoutineLoadTask;
+import org.apache.doris.thrift.TStreamLoadPutRequest;
+import org.apache.doris.thrift.TUniqueId;
 import org.apache.doris.transaction.BeginTransactionException;
 
 import com.google.common.collect.Maps;
@@ -38,7 +52,7 @@ public class KafkaTaskInfo extends RoutineLoadTaskInfo {
 
     private List<Integer> partitions;
 
-    public KafkaTaskInfo(String id, String jobId) throws LabelAlreadyUsedException,
+    public KafkaTaskInfo(UUID id, long jobId) throws LabelAlreadyUsedException,
             BeginTransactionException, AnalysisException {
         super(id, jobId);
         this.partitions = new ArrayList<>();
@@ -46,7 +60,7 @@ public class KafkaTaskInfo extends RoutineLoadTaskInfo {
 
     public KafkaTaskInfo(KafkaTaskInfo kafkaTaskInfo) throws LabelAlreadyUsedException,
             BeginTransactionException, AnalysisException {
-        super(UUID.randomUUID().toString(), kafkaTaskInfo.getJobId());
+        super(UUID.randomUUID(), kafkaTaskInfo.getJobId());
         this.partitions = kafkaTaskInfo.getPartitions();
     }
 
@@ -58,9 +72,10 @@ public class KafkaTaskInfo extends RoutineLoadTaskInfo {
         return partitions;
     }
 
+    // todo: reuse plan fragment of stream load
     @Override
-    public RoutineLoadTask createStreamLoadTask(long beId) throws LoadException {
-        RoutineLoadJob routineLoadJob = routineLoadManager.getJob(jobId);
+    public TRoutineLoadTask createRoutineLoadTask(long beId) throws LoadException, UserException {
+        KafkaRoutineLoadJob routineLoadJob = (KafkaRoutineLoadJob) routineLoadManager.getJob(jobId);
         Map<Integer, Long> partitionIdToOffset = Maps.newHashMap();
         for (Integer partitionId : partitions) {
             KafkaProgress kafkaProgress = (KafkaProgress) routineLoadJob.getProgress();
@@ -69,13 +84,41 @@ public class KafkaTaskInfo extends RoutineLoadTaskInfo {
             }
             partitionIdToOffset.put(partitionId, kafkaProgress.getPartitionIdToOffset().get(partitionId));
         }
-        RoutineLoadTask routineLoadTask = new KafkaRoutineLoadTask(routineLoadJob.getResourceInfo(),
-                                                                   beId, routineLoadJob.getDbId(),
-                                                                   routineLoadJob.getTableId(),
-                                                                   id, txnId, partitionIdToOffset);
-        if (routineLoadJob.getRoutineLoadDesc() != null) {
-            routineLoadTask.setRoutineLoadDesc(routineLoadJob.getRoutineLoadDesc());
-        }
-        return routineLoadTask;
+
+        // init tRoutineLoadTask and create plan fragment
+        TRoutineLoadTask tRoutineLoadTask = new TRoutineLoadTask();
+        TUniqueId queryId = new TUniqueId(id.getMostSignificantBits(), id.getLeastSignificantBits());
+        tRoutineLoadTask.setId(queryId);
+        tRoutineLoadTask.setJob_id(jobId);
+        tRoutineLoadTask.setTxn_id(txnId);
+        Database database = Catalog.getCurrentCatalog().getDb(routineLoadJob.getDbId());
+        tRoutineLoadTask.setDb(database.getFullName());
+        tRoutineLoadTask.setTbl(database.getTable(routineLoadJob.getTableId()).getName());
+        StringBuilder stringBuilder = new StringBuilder();
+        // label = (serviceAddress_topic_partition1:offset_partition2:offset).hashcode()
+        String label = String.valueOf(stringBuilder.append(routineLoadJob.getServerAddress()).append("_")
+                                              .append(routineLoadJob.getTopic()).append("_")
+                                              .append(Joiner.on("_").withKeyValueSeparator(":")
+                                                              .join(partitionIdToOffset)).toString().hashCode());
+        tRoutineLoadTask.setLabel(label);
+        tRoutineLoadTask.setAuth_code(routineLoadJob.getAuthCode());
+        TKafkaLoadInfo tKafkaLoadInfo = new TKafkaLoadInfo();
+        tKafkaLoadInfo.setTopic((routineLoadJob).getTopic());
+        tKafkaLoadInfo.setBrokers((routineLoadJob).getServerAddress());
+        tKafkaLoadInfo.setPartition_begin_offset(partitionIdToOffset);
+        tRoutineLoadTask.setKafka_load_info(tKafkaLoadInfo);
+        tRoutineLoadTask.setType(TLoadSourceType.KAFKA);
+        tRoutineLoadTask.setParams(createTExecPlanFragmentParams(routineLoadJob));
+        return tRoutineLoadTask;
+    }
+
+
+    private TExecPlanFragmentParams createTExecPlanFragmentParams(RoutineLoadJob routineLoadJob) throws UserException {
+        StreamLoadTask streamLoadTask = StreamLoadTask.fromRoutineLoadTaskInfo(this);
+        Database database = Catalog.getCurrentCatalog().getDb(routineLoadJob.getDbId());
+        StreamLoadPlanner planner = new StreamLoadPlanner(database,
+                                                          (OlapTable) database.getTable(routineLoadJob.getTableId()),
+                                                          streamLoadTask);
+        return planner.plan();
     }
 }
