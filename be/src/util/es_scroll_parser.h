e893d36f916a4d4df973a754ee913a21d535f3c3
lide-reed <lide@apache.org>
2019-03-26 22:16:31 +0800

    Add fill_tuple to ScrollParser

--- be/src/util/es_scroll_parser.h
+++ be/src/util/es_scroll_parser.h
@@ -18,7 +18,10 @@
 #pragma once
 
 #include<string>
+
 #include "rapidjson/document.h"
+#include "runtime/descriptors.h"
+#include "runtime/tuple.h"
 
 namespace doris {
 
@@ -27,12 +30,17 @@ class Status;
 class ScrollParser {
 
 public:
-    ScrollParser(const std::string& scroll_id, int total, int size);
+    ScrollParser(const std::string& scroll_id, int total, int size = 0);
     ~ScrollParser();
 
     static ScrollParser* parse_from_string(const std::string& scroll_result);
 
-    Status read_next_line(const char** ptr, size_t* size, bool* line_eof);
+    Status fill_tuple(const TupleDescriptor* _tuple_desc, Tuple* tuple, 
+                MemPool* mem_pool, bool* line_eof);
+
+    void set_inner_hits_node(rapidjson::Value& inner_hits_node) {
+        _inner_hits_node = inner_hits_node;
+    }
 
     const std::string& get_scroll_id();
     int get_total();
@@ -43,7 +51,8 @@ private:
     const std::string& _scroll_id;
     int _total;
     int _size;
+    rapidjson::SizeType _line_index;
 
-    //const rapidjson::Value& _inner_hits_node;
+    rapidjson::Value _inner_hits_node;
 };
 }
