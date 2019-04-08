4caca3c19ba67c5d2d3b522c267ebe4b3a68b4d7
wuyunfeng <wyfsky888@126.com>
2019-04-08 14:42:14 +0800

    Modify some FE error and Add predicate validator for BE

--- fe/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -3919,6 +3919,7 @@ public class Catalog {
             sb.append("\"password\" = \"").append(hidePassword ? "" : esTable.getPasswd()).append("\",\n");
             sb.append("\"index\" = \"").append(esTable.getIndexName()).append("\",\n");
             sb.append("\"type\" = \"").append(esTable.getMappingType()).append("\"\n");
+            sb.append("\"transport\" = \"").append(esTable.getTransport()).append("\"\n");
             sb.append(");");
         }
 
