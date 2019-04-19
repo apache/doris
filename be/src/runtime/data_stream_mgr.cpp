451fb5a917de71c52b922b9f68041313e8d8a707
chenhao <510341142@qq.com>
2019-04-19 17:08:49 +0800

    Fix but that query cause be core when collecting query statistics (#970)

--- be/src/runtime/data_stream_mgr.cpp
+++ be/src/runtime/data_stream_mgr.cpp
@@ -111,14 +111,17 @@ Status DataStreamMgr::transmit_data(const PTransmitDataParams* request, ::google
         return Status::OK;
     }
 
+    // request can only be used before calling recvr's add_batch or when request 
+    // is the last for the sender, because request maybe released after it's batch 
+    // is consumed by ExchangeNode.
+    if (request->has_query_statistics()) {
+        recvr->add_sub_plan_statistics(request->query_statistics(), request->sender_id());
+    }
+
     bool eos = request->eos();
     if (request->has_row_batch()) {
         recvr->add_batch(request->row_batch(), request->sender_id(), 
                 request->be_number(), request->packet_seq(), eos ? nullptr : done);
-    }
-
-    if (request->has_query_statistics()) {
-        recvr->add_sub_plan_statistics(request->query_statistics(), request->sender_id());
     }
 
     if (eos) {
