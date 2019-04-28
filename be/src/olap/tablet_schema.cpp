497736d8a438d46d7e73c6bfd4c1681b82495a83
huangkangping <huangkangping@baidu.com>
2019-04-28 21:59:14 +0800

    Fix default value bug

--- be/src/olap/tablet_schema.cpp
+++ be/src/olap/tablet_schema.cpp
@@ -71,6 +71,9 @@ OLAPStatus TabletColumn::to_schema_pb(ColumnPB* column) {
         column->set_precision(_precision);
         column->set_frac(_frac);
     }
+    if (_has_default_value) {
+        column->set_default_value(_default_value);
+    }
     column->set_length(_length);
     column->set_index_length(_index_length);
     column->set_is_bf_column(_is_bf_column);
