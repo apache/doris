8d0e2221a0bb1dafbbf492d04f44b8e3c6e631dc
chenhao <510341142@qq.com>
2019-05-11 23:10:58 +0800

    FE support assigning constant conjunct and calculating expression (#1126)

--- fe/src/main/java/org/apache/doris/rewrite/FoldConstantsRule.java
+++ fe/src/main/java/org/apache/doris/rewrite/FoldConstantsRule.java
@@ -18,33 +18,10 @@
 package org.apache.doris.rewrite;
 
 
-import org.apache.doris.analysis.Analyzer;
-import org.apache.doris.analysis.ArithmeticExpr;
-import org.apache.doris.analysis.CastExpr;
-import org.apache.doris.analysis.Expr;
-import org.apache.doris.analysis.FunctionCallExpr;
-import org.apache.doris.analysis.LiteralExpr;
-import org.apache.doris.analysis.NullLiteral;
-import org.apache.doris.catalog.Function;
-import org.apache.doris.catalog.ScalarType;
-import org.apache.doris.catalog.Type;
+import org.apache.doris.analysis.*;
 import org.apache.doris.common.AnalysisException;
-
-import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.ImmutableSet;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * This rule replaces a constant Expr with its equivalent LiteralExpr by evaluating the
@@ -66,19 +43,8 @@ public class FoldConstantsRule implements ExprRewriteRule {
 
     public static ExprRewriteRule INSTANCE = new FoldConstantsRule();
 
-    private ImmutableMultimap<String, FEFunctionInvoker> functions;
-    // For most build-in functions, it will return NullLiteral when params contain NullLiteral.
-    // But a few functions need to handle NullLiteral differently, such as "if". It need to add 
-    // an attribute to LiteralExpr to mark null and check the attribute to decide whether to 
-    // replace the result with NullLiteral when function finished. It leaves to be realized.
-    // TODO chenhao16.
-    private ImmutableSet<String> nonNullResultWithNullParamFunctions;
-
     @Override
     public Expr apply(Expr expr, Analyzer analyzer) throws AnalysisException {
-        if (functions == null) {
-            registerFunctions();
-        }
         // Avoid calling Expr.isConstant() because that would lead to repeated traversals
         // of the Expr tree. Assumes the bottom-up application of this rule. Constant
         // children should have been folded at this point.
@@ -100,6 +66,7 @@ public class FoldConstantsRule implements ExprRewriteRule {
                 return expr;
             }
         }
+
         // Analyze constant exprs, if necessary. Note that the 'expr' may become non-constant
         // after analysis (e.g., aggregate functions).
         if (!expr.isAnalyzed()) {
@@ -108,177 +75,7 @@ public class FoldConstantsRule implements ExprRewriteRule {
                 return expr;
             }
         }
-        return simplify(expr);
-    }
-
-    private Expr simplify(Expr constExpr) throws AnalysisException {
-        if (constExpr instanceof ArithmeticExpr
-                || constExpr instanceof FunctionCallExpr
-                || constExpr instanceof CastExpr) {
-            Function fn = constExpr.getFn();
-            // unused cast
-            if (fn == null) {
-               Preconditions.checkState((constExpr instanceof CastExpr)
-                        && constExpr.getChildren().size() == 1);    
-               return constExpr.getChild(0);
-            }
-
-            // null
-            if (!nonNullResultWithNullParamFunctions.contains(fn.getFunctionName().getFunction())) {
-                for (Expr e : constExpr.getChildren()) {
-                    if (e instanceof NullLiteral) {
-                        return new NullLiteral();
-                    }
-                }
-            }
-
-            List<ScalarType> argTypes = new ArrayList<>();
-            for (Type type : fn.getArgs()) {
-                argTypes.add((ScalarType) type);
-            }
-            FEFunctionSignature signature = new FEFunctionSignature(fn.functionName(),
-                    argTypes.toArray(new ScalarType[argTypes.size()]), (ScalarType) fn.getReturnType());
-            FEFunctionInvoker invoker = getFunction(signature);
-            if (invoker != null) {
-                try {
-                    return invoker.invoke(constExpr.getChildrenWithoutCast());
-                } catch (AnalysisException e) {
-                    LOG.debug("failed to invoke", e);
-                    return constExpr;
-                }
-            }
-        }
-        return constExpr;
-    }
-
-    private FEFunctionInvoker getFunction(FEFunctionSignature signature) {
-        Collection<FEFunctionInvoker> functionInvokers = functions.get(signature.getName());
-
-        if (functionInvokers == null) {
-            return null;
-        }
-
-        for (FEFunctionInvoker invoker : functionInvokers) {
-            if (!invoker.getSignature().returnType.equals(signature.getReturnType())) {
-                continue;
-            }
-
-            ScalarType[] argTypes1 = invoker.getSignature().getArgTypes();
-            ScalarType[] argTypes2 = signature.getArgTypes();
-
-            if (!Arrays.equals(argTypes1, argTypes2)) {
-                continue;
-            }
-            return invoker;
-        }
-        return null;
-    }
-
-    private synchronized void registerFunctions() {
-        // double checked locking pattern
-        // functions only need to init once
-        if (functions != null) {
-           return;
-        }
-        ImmutableMultimap.Builder<String, FEFunctionInvoker> mapBuilder =
-                        new ImmutableMultimap.Builder<String, FEFunctionInvoker>();
-        Class clazz = FEFunctions.class;
-        for (Method method : clazz.getDeclaredMethods()) {
-            FEFunction annotation = method.getAnnotation(FEFunction.class);
-            if (annotation != null) {
-                String name = annotation.name();
-                ScalarType returnType = ScalarType.createType(annotation.returnType());
-                List<ScalarType> argTypes = new ArrayList<>();
-                for (String type : annotation.argTypes()) {
-                    argTypes.add(ScalarType.createType(type));
-                }
-                FEFunctionSignature signature = new FEFunctionSignature(name,
-                        argTypes.toArray(new ScalarType[argTypes.size()]), returnType);
-                mapBuilder.put(name, new FEFunctionInvoker(method, signature));
-            }
-        }
-        this.functions = mapBuilder.build();
-
-        // Functions that need to handle null.
-        ImmutableSet.Builder<String> setBuilder =
-                        new ImmutableSet.Builder<String>();
-        setBuilder.add("if");
-        setBuilder.add("hll_hash");
-        setBuilder.add("concat_ws");
-        this.nonNullResultWithNullParamFunctions = setBuilder.build();
-    }
-
-    public static class FEFunctionInvoker {
-        private final Method method;
-        private final FEFunctionSignature signature;
-
-        public FEFunctionInvoker(Method method, FEFunctionSignature signature) {
-            this.method = method;
-            this.signature = signature;
-        }
-
-        public Method getMethod() {
-            return method;
-        }
-
-        public FEFunctionSignature getSignature() {
-            return signature;
-        }
-
-        public LiteralExpr invoke(List<Expr> args) throws AnalysisException {
-            try {
-                return (LiteralExpr) method.invoke(null, args.toArray());
-            } catch (InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
-                throw new AnalysisException(e.getLocalizedMessage(), e);
-            }
-        }
-    }
-
-    public static class FEFunctionSignature {
-        private final String name;
-        private final ScalarType[] argTypes;
-        private final ScalarType returnType;
-
-        public FEFunctionSignature(String name, ScalarType[] argTypes, ScalarType returnType) {
-            this.name = name;
-            this.argTypes = argTypes;
-            this.returnType = returnType;
-        }
-
-        public ScalarType[] getArgTypes() {
-            return argTypes;
-        }
-
-        public ScalarType getReturnType() {
-            return returnType;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("FEFunctionSignature. name: ").append(name).append(", return: ").append(returnType);
-            sb.append(", args: ").append(Joiner.on(",").join(argTypes));
-            return sb.toString();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-            FEFunctionSignature signature = (FEFunctionSignature) o;
-            return Objects.equals(name, signature.name) && Arrays.equals(argTypes, signature.argTypes)
-                    && Objects.equals(returnType, signature.returnType);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, argTypes, returnType);
-        }
+        return expr.getResultValue();
     }
 }
+
