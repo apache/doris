075a44cff5a84b475e30184cb2ac1326c4e78240
zhaochun <zhaochun@baidu.com>
2019-06-11 23:44:11 +0800

    Add OrdinalPageIndex and EncodingInfo

--- run-ut.sh
+++ run-ut.sh
@@ -228,6 +228,8 @@ ${DORIS_TEST_BINARY_DIR}/olap/olap_header_manager_test
 ${DORIS_TEST_BINARY_DIR}/olap/olap_meta_test
 ${DORIS_TEST_BINARY_DIR}/olap/delta_writer_test
 ${DORIS_TEST_BINARY_DIR}/olap/field_info_test
+${DORIS_TEST_BINARY_DIR}/olap/rowset/segment_v2/encoding_info_test
+${DORIS_TEST_BINARY_DIR}/olap/rowset/segment_v2/ordinal_page_index_test
 
 # Running routine load test
 ${DORIS_TEST_BINARY_DIR}/runtime/kafka_consumer_pipe_test
