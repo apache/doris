1ba05105b9f5498eaf353005c93a69239ca37373
ZHAO Chun <buaa.zhaoc@gmail.com>
2019-06-15 20:41:48 +0800

    Remove unused code (#1320)

--- be/src/exec/broker_scan_node.cpp
+++ be/src/exec/broker_scan_node.cpp
@@ -322,7 +322,7 @@ Status BrokerScanNode::scanner_scan(
 
             // The reason we check if partition_expr_ctxs is empty is when loading data to
             // a unpartitioned table who has no partition_expr_ctxs, user can specify
-            // a partition name. And we check here to aovid this check and make our
+            // a partition name. And we check here to avoid crash and make our
             // process run as normal
             if (scan_range.params.__isset.partition_ids && !partition_expr_ctxs.empty()) {
                 int64_t partition_id = get_partition_id(partition_expr_ctxs, row);
