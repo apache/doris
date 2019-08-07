kangkaisen/incubator-doris

Add bitmap agg type and udaf

fe/src/main/java/org/apache/doris/alter/SchemaChangeHandler.java
@@ -76,6 +76,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.apache.doris.catalog.AggregateType.BITMAP_COUNT;
+
 public class SchemaChangeHandler extends AlterHandler {
     private static final Logger LOG = LogManager.getLogger(SchemaChangeHandler.class);
 
@@ -519,6 +521,10 @@ public class SchemaChangeHandler extends AlterHandler {
         // hll must be used in agg_keys
         if (newColumn.getType().isHllType() && KeysType.AGG_KEYS != olapTable.getKeysType()) {
             throw new DdlException("HLL must be used in AGG_KEYS");
+        }
+
+        if (newColumn.getAggregationType() == BITMAP_COUNT  && KeysType.AGG_KEYS != olapTable.getKeysType()) {
+            throw new DdlException("BITMAP_COUNT must be used in AGG_KEYS");
         }
 
         List<Column> baseSchema = olapTable.getBaseSchema();

fe/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
@@ -536,6 +536,18 @@ public class SingleNodePlanner {
                             returnColumnValidate = false;
                             break;
                         }
+                    } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("BITMAP")) {
+                        if ((!col.isKey())) {
+                            turnOffReason = "BITMAP function with non-key column: " + col.getName();
+                            returnColumnValidate = false;
+                            break;
+                        }
+                    } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("BITMAP_COUNT")) {
+                        if (col.getAggregationType() != AggregateType.BITMAP_COUNT) {
+                            turnOffReason = "Aggregate Operator not match: BITMAP_COUNT <--> " + col.getAggregationType();
+                            returnColumnValidate = false;
+                            break;
+                        }
                     } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("multi_distinct_count")) {
                         // count(distinct k1), count(distinct k2) / count(distinct k1,k2) can turn on pre aggregation
                         if ((!col.isKey())) {

apache/doris

fe/src/main/java/org/apache/doris/alter/SchemaChangeHandler.java
@@ -76,6 +76,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static org.apache.doris.catalog.AggregateType.BITMAP_UNION;
+
 public class SchemaChangeHandler extends AlterHandler {
     private static final Logger LOG = LogManager.getLogger(SchemaChangeHandler.class);
 
@@ -519,6 +521,10 @@ public class SchemaChangeHandler extends AlterHandler {
         // hll must be used in agg_keys
         if (newColumn.getType().isHllType() && KeysType.AGG_KEYS != olapTable.getKeysType()) {
             throw new DdlException("HLL must be used in AGG_KEYS");
+        }
+
+        if (newColumn.getAggregationType() == BITMAP_UNION  && KeysType.AGG_KEYS != olapTable.getKeysType()) {
+            throw new DdlException("BITMAP_COUNT must be used in AGG_KEYS");
         }
 
         List<Column> baseSchema = olapTable.getBaseSchema();

fe/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
@@ -536,6 +536,18 @@ public class SingleNodePlanner {
                             returnColumnValidate = false;
                             break;
                         }
+                    } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("BITMAP")) {
+                        if ((!col.isKey())) {
+                            turnOffReason = "BITMAP function with non-key column: " + col.getName();
+                            returnColumnValidate = false;
+                            break;
+                        }
+                    } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("BITMAP_COUNT")) {
+                        if (col.getAggregationType() != AggregateType.BITMAP_UNION) {
+                            turnOffReason = "Aggregate Operator not match: BITMAP_COUNT <--> " + col.getAggregationType();
+                            returnColumnValidate = false;
+                            break;
+                        }
                     } else if (aggExpr.getFnName().getFunction().equalsIgnoreCase("multi_distinct_count")) {
                         // count(distinct k1), count(distinct k2) / count(distinct k1,k2) can turn on pre aggregation
                         if ((!col.isKey())) {