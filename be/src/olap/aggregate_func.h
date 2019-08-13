4d4365825800d5a5a4dc54849260238efac308e1
ZHAO Chun <buaa.zhaoc@gmail.com>
2019-08-13 14:38:21 +0800

    Fix HLLContext cast error (#1632)

--- be/src/olap/aggregate_func.h
+++ be/src/olap/aggregate_func.h
@@ -291,21 +291,20 @@ struct AggregateFuncTraits<OLAP_FIELD_AGGREGATION_HLL_UNION, OLAP_FIELD_TYPE_HLL
         // TODO(zc): refactor HLL implementation
         *reinterpret_cast<bool*>(dst) = false;
         Slice* slice = reinterpret_cast<Slice*>(dst + 1);
-        size_t hll_ptr = *(size_t*)(slice->data - sizeof(HllContext*));
-        HllContext* context = (reinterpret_cast<HllContext*>(hll_ptr));
+        HllContext* context = *reinterpret_cast<HllContext**>(slice->data - sizeof(HllContext*));
         HllSetHelper::init_context(context);
         context->has_value = true;
     }
 
     static void update(RowCursorCell* dst, const RowCursorCell& src, Arena* arena) {
         auto l_slice = reinterpret_cast<Slice*>(dst->mutable_cell_ptr());
-        auto context = reinterpret_cast<HllContext*>(l_slice->data - sizeof(HllContext*));
+        auto context = *reinterpret_cast<HllContext**>(l_slice->data - sizeof(HllContext*));
         HllSetHelper::fill_set((const char*)src.cell_ptr(), context);
     }
 
     static void finalize(char* data, Arena* arena) {
         auto slice = reinterpret_cast<Slice*>(data);
-        auto context = reinterpret_cast<HllContext*>(slice->data - sizeof(HllContext*));
+        auto context = *reinterpret_cast<HllContext**>(slice->data - sizeof(HllContext*));
         std::map<int, uint8_t> index_to_value;
         if (context->has_sparse_or_full ||
                 context->hash64_set->size() > HLL_EXPLICLIT_INT64_NUM) {
