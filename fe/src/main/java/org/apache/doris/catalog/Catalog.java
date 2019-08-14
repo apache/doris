e95d90b8d73d156010989c3d8159af835c3db061
Mingyu Chen <morningman@163.com>
2019-08-14 09:49:44 +0800

    Fix bug that bad replica can not be synchronized when report (#1634)
    When the replica is recovered from bad on BE, the report process
    should change the bad status of replica on FE to false, or the replica
    can not be recovered.

--- fe/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -4227,6 +4227,7 @@ public class Catalog {
         Replica replica = tablet.getReplicaByBackendId(info.getBackendId());
         Preconditions.checkNotNull(replica, info);
         replica.updateVersionInfo(info.getVersion(), info.getVersionHash(), info.getDataSize(), info.getRowCount());
+        replica.setBad(false);
     }
 
     public void replayAddReplica(ReplicaPersistInfo info) {
