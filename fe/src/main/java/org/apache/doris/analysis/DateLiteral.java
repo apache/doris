40c0dfe7c9a021b288251b72dcb4d1ab51aac6de
HangyuanLiu <460660596@qq.com>
2019-08-19 14:26:54 +0800

    add fe time zone

--- fe/src/main/java/org/apache/doris/analysis/DateLiteral.java
+++ fe/src/main/java/org/apache/doris/analysis/DateLiteral.java
@@ -17,9 +17,13 @@
 
 package org.apache.doris.analysis;
 
+import org.apache.doris.catalog.Catalog;
 import org.apache.doris.catalog.PrimitiveType;
+import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.FeMetaVersion;
+import org.apache.doris.common.io.Text;
 import org.apache.doris.common.util.TimeUtils;
 import org.apache.doris.thrift.TDateLiteral;
 import org.apache.doris.thrift.TExprNode;
@@ -29,6 +33,8 @@ import com.google.common.base.Preconditions;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormatterBuilder;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -38,24 +44,39 @@ import java.util.Date;
 
 public class DateLiteral extends LiteralExpr {
     private static final Logger LOG = LogManager.getLogger(DateLiteral.class);
-    private Date date;
+    //private Date date;
+    int hour;
+    int minute;
+    int second;
+    int year;
+    int month;
+    int day;
+    int microsecond;
 
     private DateLiteral() {
         super();
     }
 
-    public DateLiteral(Type type, boolean isMax) throws AnalysisException {
+    public DateLiteral(Type type, boolean isMax) {
         super();
         this.type = type;
         if (type == Type.DATE) {
-            date = isMax ? TimeUtils.MAX_DATE : TimeUtils.MIN_DATE;
+            if (isMax) {
+                init("1900-01-01", Type.DATE);
+            } else {
+                init("9999-12-31", Type.DATE);
+            }
         } else {
-            date = isMax ? TimeUtils.MAX_DATETIME : TimeUtils.MIN_DATETIME;
+            if (isMax) {
+                init("1900-01-01 00:00:00", Type.DATETIME);
+            } else {
+                init("9999-12-31 23:59:59", Type.DATETIME);
+            }
         }
         analysisDone();
     }
 
-    public DateLiteral(String s, Type type) throws AnalysisException {
+    public DateLiteral(String s, Type type) {
         super();
         init(s, type);
         analysisDone();
@@ -63,7 +84,14 @@ public class DateLiteral extends LiteralExpr {
 
     protected DateLiteral(DateLiteral other) {
         super(other);
-        date = other.date;
+        hour = other.hour;
+        minute = other.minute;
+        second = other.second;
+        year = other.year;
+        month = other.month;
+        day = other.day;
+        microsecond = other.microsecond;
+        //date = other.date;
     }
 
     @Override
@@ -72,19 +100,26 @@ public class DateLiteral extends LiteralExpr {
     }
 
     public static DateLiteral createMinValue(Type type) {
-        DateLiteral dateLiteral = new DateLiteral();
-        dateLiteral.type = type;
-        if (type == Type.DATE) {
-            dateLiteral.date = TimeUtils.MIN_DATE;
-        } else {
-            dateLiteral.date = TimeUtils.MIN_DATETIME;
-        }
-
+        DateLiteral dateLiteral = new DateLiteral(type, false);
         return dateLiteral;
     }
 
-    private void init(String s, Type type) throws AnalysisException {
+    //private void init(String s, Type type) throws AnalysisException {
+    private void init(String s, Type type) {
         Preconditions.checkArgument(type.isDateType());
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+        LocalDateTime datetime = builder.toFormatter().parseLocalDateTime(s);
+
+        year = datetime.getYear();
+        month = datetime.getMonthOfYear();
+        day = datetime.getDayOfMonth();
+
+        hour = datetime.getHourOfDay();
+        minute = datetime.getMinuteOfHour();
+        second = datetime.getSecondOfMinute();
+        this.type = type;
+
+        /*
         date = TimeUtils.parseDate(s, type);
         if (type.isScalarType(PrimitiveType.DATE)) {
             if (date.compareTo(TimeUtils.MAX_DATE) > 0 || date.compareTo(TimeUtils.MIN_DATE) < 0) {
@@ -98,15 +133,16 @@ public class DateLiteral extends LiteralExpr {
             }
         }
         this.type = type;
+        */
     }
 
     @Override
     public boolean isMinValue() {
         switch (type.getPrimitiveType()) {
             case DATE:
-                return this.date.compareTo(TimeUtils.MIN_DATE) == 0;
+                return this.getStringValue().compareTo("1900-01-01") == 0;
             case DATETIME:
-                return this.date.compareTo(TimeUtils.MIN_DATETIME) == 0;
+                return this.getStringValue().compareTo("1900-01-01 00:00:00") == 0;
             default:
                 return false;
         }
@@ -114,13 +150,15 @@ public class DateLiteral extends LiteralExpr {
 
     @Override
     public Object getRealValue() {
-        return TimeUtils.dateTransform(date.getTime(), type);
+        return getStringValue();
+        //return TimeUtils.dateTransform(date.getTime(), type);
     }
 
     // Date column and Datetime column's hash value is not same.
     @Override
     public ByteBuffer getHashValue(PrimitiveType type) {
-        String value = TimeUtils.format(date, type);
+        //String value = TimeUtils.format(date, type);
+        String value = getStringValue();
         ByteBuffer buffer;
         try {
             buffer = ByteBuffer.wrap(value.getBytes("UTF-8"));
@@ -150,17 +188,23 @@ public class DateLiteral extends LiteralExpr {
 
     @Override
     public String getStringValue() {
-        return TimeUtils.format(date, type);
+        //return TimeUtils.format(date, type);
+        if (type == Type.DATE) {
+            return year + "-" + month + "-" + day;
+        } else {
+            return year + "-" + month + "-" + day +
+                    " " + hour + ":" + minute + ":" + second;
+        }
     }
 
     @Override
     public long getLongValue() {
-        return date.getTime();
+        return (year * 10000 + month * 100 + day) * 1000000L + hour * 10000 + minute * 100 + second;
     }
 
     @Override
     public double getDoubleValue() {
-        return date.getTime();
+        return getLongValue();
     }
 
     @Override
@@ -168,10 +212,11 @@ public class DateLiteral extends LiteralExpr {
         msg.node_type = TExprNodeType.DATE_LITERAL;
         msg.date_literal = new TDateLiteral(getStringValue());
     }
-
+    /*
     public Date getValue() {
         return date;
     }
+    */
 
     @Override
     protected Expr uncheckedCastTo(Type targetType) throws AnalysisException {
@@ -187,13 +232,38 @@ public class DateLiteral extends LiteralExpr {
     @Override
     public void write(DataOutput out) throws IOException {
         super.write(out);
-        out.writeLong(date.getTime());
+        long ymd = ((year * 13 + month) << 5) | day;
+        long hms = (hour << 12) | (minute << 6) | second;
+        long tmp = ((ymd << 17) | hms) << 24 + microsecond;
+        long packed_datetime =  tmp;
+        out.writeLong(packed_datetime);
     }
 
     @Override
     public void readFields(DataInput in) throws IOException {
         super.readFields(in);
-        date = new Date(in.readLong());
+        //date = new Date(in.readLong());
+        if (Catalog.getCurrentCatalogJournalVersion() >= FeMetaVersion.VERSION_59) {
+            long packed_time = in.readLong();
+            microsecond = (int) (packed_time % (1L << 24));
+            int ymdhms = (int) (packed_time >> 24);
+            int ymd = ymdhms >> 17;
+            int hms = ymdhms % (1 << 17);
+
+            day = ymd % (1 << 5);
+            int ym = ymd >> 5;
+            month = ym % 13;
+            year = ym / 13;
+            year %= 10000;
+            second = hms % (1 << 6);
+            minute = (hms >> 6) % (1 << 6);
+            hour = (hms >> 12);
+            this.type = Type.DATETIME;
+        } else {
+            Date date = new Date(in.readLong());
+            String date_str = TimeUtils.format(date, Type.DATETIME);
+            init(date_str, Type.DATETIME);
+        }
     }
 
     public static DateLiteral read(DataInput in) throws IOException {
