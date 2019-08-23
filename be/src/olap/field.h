23434a206e28598db02104385a05e9eb26e25608
huangkangping <huangkangping>
2019-08-23 15:01:52 +0800

    modify name

--- be/src/olap/field.h
+++ be/src/olap/field.h
@@ -183,7 +183,7 @@ public:
         if (is_null) {
             return;
         }
-        _type_info->deep_copy_by_arena(dst->mutable_cell_ptr(), src.cell_ptr(), arena);
+        _type_info->deep_copy_with_arena(dst->mutable_cell_ptr(), src.cell_ptr(), arena);
     }
 
     // This function will initialize destination with source.
@@ -206,7 +206,7 @@ public:
 
     // copy filed content from src to dest without nullbyte
     inline void deep_copy_content(char* dest, const char* src, Arena* arena) const {
-        _type_info->deep_copy_by_arena(dest, src, arena);
+        _type_info->deep_copy_with_arena(dest, src, arena);
     }
 
     // Copy srouce content to destination in index format.
