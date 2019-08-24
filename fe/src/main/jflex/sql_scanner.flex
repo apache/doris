kangkaisen/incubator-doris

Add bitmap agg type and udaf

fe/src/main/jflex/sql_scanner.flex
@@ -167,7 +167,8 @@ import org.apache.doris.common.util.SqlUtils;
         keywordMap.put("hash", new Integer(SqlParserSymbols.KW_HASH));
         keywordMap.put("having", new Integer(SqlParserSymbols.KW_HAVING));
         keywordMap.put("help", new Integer(SqlParserSymbols.KW_HELP));
-        keywordMap.put("hll_union", new Integer(SqlParserSymbols.KW_HLL_UNION)); 
+        keywordMap.put("hll_union", new Integer(SqlParserSymbols.KW_HLL_UNION));
+        keywordMap.put("bitmap_count", new Integer(SqlParserSymbols.KW_BITMAP_COUNT));
         keywordMap.put("hub", new Integer(SqlParserSymbols.KW_HUB)); 
         keywordMap.put("identified", new Integer(SqlParserSymbols.KW_IDENTIFIED));
         keywordMap.put("if", new Integer(SqlParserSymbols.KW_IF));

apache/doris

fe/src/main/jflex/sql_scanner.flex
@@ -168,7 +168,8 @@ import org.apache.doris.common.util.SqlUtils;
         keywordMap.put("hash", new Integer(SqlParserSymbols.KW_HASH));
         keywordMap.put("having", new Integer(SqlParserSymbols.KW_HAVING));
         keywordMap.put("help", new Integer(SqlParserSymbols.KW_HELP));
-        keywordMap.put("hll_union", new Integer(SqlParserSymbols.KW_HLL_UNION)); 
+        keywordMap.put("hll_union", new Integer(SqlParserSymbols.KW_HLL_UNION));
+        keywordMap.put("bitmap_union", new Integer(SqlParserSymbols.KW_BITMAP_UNION));
         keywordMap.put("hub", new Integer(SqlParserSymbols.KW_HUB)); 
         keywordMap.put("identified", new Integer(SqlParserSymbols.KW_IDENTIFIED));
         keywordMap.put("if", new Integer(SqlParserSymbols.KW_IF));