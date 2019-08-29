4f7c0b87be48a02dab95fbfed9ab781643b0e80f
Yunfeng,Wu <wuyunfeng01@baidu.com>
2019-08-29 18:57:49 +0800

    Avoid consistency problem when has no more data (#1716)

--- be/src/runtime/memory_scratch_sink.cpp
+++ be/src/runtime/memory_scratch_sink.cpp
@@ -222,8 +222,8 @@ Status MemoryScratchSink::close(RuntimeState* state, Status exec_status) {
     if (_closed) {
         return Status::OK();
     }
-    // shutdown queue, then blocking_get return false, put sentinel
-    if (_queue) {
+    // put sentinel
+    if (_queue != nullptr) {
         _queue->blocking_put(nullptr);
     }
     Expr::close(_output_expr_ctxs, state);
