53f60067cd58ca049ed956f0f1c93653e6cc8d1b
kangkaisen <kangkaisen@apache.org>
2019-09-10 20:46:21 +0800

    Add frame_of_reference page

---
+++ be/src/util/frame_of_reference_coding.h
@@ -0,0 +1,152 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef DORIS_FRAME_OF_REFERENCE_CODING_H
+#define DORIS_FRAME_OF_REFERENCE_CODING_H
+
+
+#include <cstdlib>
+#include <iostream>
+
+#include "util/bit_stream_utils.h"
+#include "util/bit_stream_utils.inline.h"
+#include "util/faststring.h"
+
+namespace doris {
+
+struct ForCoding {
+    const static uint8_t FRAME_VALUE_NUM = 128;
+};
+
+// The implementation for frame-of-reference coding
+// The detail of frame-of-reference coding, please refer to
+// https://lemire.me/blog/2012/02/08/effective-compression-using-frame-of-reference-and-delta-coding/
+// and https://www.elastic.co/cn/blog/frame-of-reference-and-roaring-bitmaps
+//
+// The encoded data format is as follows:
+//
+// 1. Body:
+//      BitPackingFrame * FrameCount
+// 2. Footer:
+//      (2 bit OrderFlag + 6 bit BitWidth) * FrameCount
+//      32 bit ValuesNum
+//
+// The not ascending order BitPackingFrame format:
+//      MinValue, (Value - MinVale) * FRAME_VALUE_NUM
+//
+// The ascending order BitPackingFrame format:
+//      MinValue, (Value[i] - Value[i - 1]) * FRAME_VALUE_NUM
+//
+// The OrderFlag is 1 represents ascending order, 0 represents  not ascending order
+// The last frame value num maybe less than 128
+template<typename T>
+class ForEncoder {
+public:
+    explicit ForEncoder(faststring* buffer): _buffer(buffer) {}
+
+    void put(const T value) {
+        return put_batch(&value, 1);
+    }
+
+    void put_batch(const T* value, size_t count);
+
+    // Flushes any pending values to the underlying buffer.
+    // Returns the total number of bytes written
+    uint32_t flush();
+
+    // underlying buffer size + footer meta size.
+    // Note: should call this method before flush.
+    uint32_t len() {
+        return _buffer->size() + _order_flag_and_bit_widths.size() + 4;
+    }
+
+    // Resets all the state in the encoder.
+    void clear() {
+        _values_num = 0;
+        _buffered_values_num = 0;
+        _buffer->clear();
+    }
+
+private:
+    void bit_pack(T *input, uint8_t in_num, int bit_width, uint8_t *output);
+
+    void bit_packing_one_frame_value(const T* input);
+
+    const T* copy_value(const T* val, size_t count);
+
+    uint32_t _values_num = 0;
+    uint8_t _buffered_values_num = 0;
+    T _buffered_values[ForCoding::FRAME_VALUE_NUM];
+
+    faststring* _buffer;
+    std::vector<uint8_t> _order_flag_and_bit_widths;
+};
+
+template<typename T>
+class ForDecoder {
+public:
+    explicit ForDecoder(const uint8_t* in_buffer, size_t buffer_len)
+        :_buffer (in_buffer),
+         _buffer_len (buffer_len){}
+
+    // read footer metadata
+    bool init();
+
+    bool get(T* val) {
+        return get_batch(val, 1);
+    }
+
+    // Gets the next batch value.  Returns false if there are no more.
+    bool get_batch(T* val, size_t count);
+
+    // The skip_num is positive means move forwards
+    // The skip_num is negative means move backwards
+    bool skip(int32_t skip_num);
+
+    uint32_t count() const {
+        return _values_num;
+    }
+
+private:
+
+    void bit_unpack(const uint8_t *input, uint8_t in_num, int bit_width, T *output);
+
+    void decode_current_frame(T* output);
+
+    T* copy_value(T* val, size_t count);
+
+    bool need_decode_frame() {
+        return !(ForCoding::FRAME_VALUE_NUM * _current_decoded_frame < _current_index
+                 && _current_index < ForCoding::FRAME_VALUE_NUM * (_current_decoded_frame + 1));
+    }
+
+    uint8_t _last_frame_num = ForCoding::FRAME_VALUE_NUM;
+    uint32_t _values_num = 0;
+    uint32_t _frame_count = 0;
+    uint32_t _current_index = 0;
+    uint32_t _current_decoded_frame = -1;
+    T _out_buffer[ForCoding::FRAME_VALUE_NUM];
+    std::vector<uint32_t> _frame_offsets;
+    std::vector<uint8_t> _bit_widths;
+    std::vector<uint8_t> _order_flags;
+    const uint8_t* _buffer;
+    size_t _buffer_len = 0;
+};
+}
+
+
+#endif //DORIS_FRAME_OF_REFERENCE_CODING_H
---
+++ be/test/olap/rowset/segment_v2/frame_of_reference_page_test.cpp
@@ -0,0 +1,180 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "olap/rowset/segment_v2/options.h"
+#include "olap/rowset/segment_v2/page_builder.h"
+#include "olap/rowset/segment_v2/page_decoder.h"
+#include "olap/rowset/segment_v2/frame_of_reference_page.h"
+#include "util/arena.h"
+#include "util/logging.h"
+
+using doris::segment_v2::PageBuilderOptions;
+using doris::segment_v2::PageDecoderOptions;
+
+namespace doris {
+class FrameOfReferencePageTest : public testing::Test {
+public:
+    template<FieldType type, class PageDecoderType>
+    void copy_one(PageDecoderType* decoder, typename TypeTraits<type>::CppType* ret) {
+        Arena arena;
+        uint8_t null_bitmap = 0;
+        ColumnBlock block(get_type_info(type), (uint8_t*)ret, &null_bitmap, &arena);
+        ColumnBlockView column_block_view(&block);
+
+        size_t n = 1;
+        decoder->next_batch(&n, &column_block_view);
+        ASSERT_EQ(1, n);
+    }
+
+    template <FieldType Type, class PageBuilderType, class PageDecoderType>
+    void test_encode_decode_page_template(typename TypeTraits<Type>::CppType* src,
+                                          size_t size) {
+        typedef typename TypeTraits<Type>::CppType CppType;
+        PageBuilderOptions builder_options;
+        builder_options.data_page_size = 256 * 1024;
+        PageBuilderType for_page_builder(builder_options);
+        for_page_builder.add(reinterpret_cast<const uint8_t *>(src), &size);
+        Slice s = for_page_builder.finish();
+        ASSERT_EQ(size, for_page_builder.count());
+        LOG(INFO) << "FrameOfReference Encoded size for 10k values: " << s.size
+                  << ", original size:" << size * sizeof(CppType);
+
+        PageDecoderOptions decoder_options;
+        PageDecoderType for_page_decoder(s, decoder_options);
+        Status status = for_page_decoder.init();
+        ASSERT_TRUE(status.ok());
+        ASSERT_EQ(0, for_page_decoder.current_index());
+        ASSERT_EQ(size, for_page_decoder.count());
+
+        Arena arena;
+        CppType* values = reinterpret_cast<CppType*>(arena.Allocate(size * sizeof(CppType)));
+        uint8_t* null_bitmap = reinterpret_cast<uint8_t*>(arena.Allocate(BitmapSize(size)));
+        ColumnBlock block(get_type_info(Type), (uint8_t*)values, null_bitmap, &arena);
+        ColumnBlockView column_block_view(&block);
+        size_t size_to_fetch = size;
+        status = for_page_decoder.next_batch(&size_to_fetch, &column_block_view);
+        ASSERT_TRUE(status.ok());
+        ASSERT_EQ(size, size_to_fetch);
+
+        for (uint i = 0; i < size; i++) {
+            if (src[i] != values[i]) {
+                FAIL() << "Fail at index " << i <<
+                       " inserted=" << src[i] << " got=" << values[i];
+            }
+        }
+
+        // Test Seek within block by ordinal
+        for (int i = 0; i < 100; i++) {
+            int seek_off = random() % size;
+            for_page_decoder.seek_to_position_in_page(seek_off);
+            EXPECT_EQ((int32_t )(seek_off), for_page_decoder.current_index());
+            CppType ret;
+            copy_one<Type, PageDecoderType>(&for_page_decoder, &ret);
+            EXPECT_EQ(values[seek_off], ret);
+        }
+    }
+};
+
+TEST_F(FrameOfReferencePageTest, TestInt32BlockEncoderRandom) {
+    const uint32_t size = 10000;
+
+    std::unique_ptr<int32_t[]> ints(new int32_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = random();
+    }
+
+    test_encode_decode_page_template<OLAP_FIELD_TYPE_INT, segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_INT>,
+            segment_v2::FrameOfReferencePageDecoder<OLAP_FIELD_TYPE_INT> >(ints.get(), size);
+}
+
+TEST_F(FrameOfReferencePageTest, TestInt32BlockEncoderEqual) {
+    const uint32_t size = 10000;
+
+    std::unique_ptr<int32_t[]> ints(new int32_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = 12345;
+    }
+
+    test_encode_decode_page_template<OLAP_FIELD_TYPE_INT, segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_INT>,
+            segment_v2::FrameOfReferencePageDecoder<OLAP_FIELD_TYPE_INT> >(ints.get(), size);
+}
+
+TEST_F(FrameOfReferencePageTest, TestInt32BlockEncoderSequence) {
+    const uint32_t size = 10000;
+
+    std::unique_ptr<int32_t[]> ints(new int32_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = 12345 + i;
+    }
+
+    test_encode_decode_page_template<OLAP_FIELD_TYPE_INT, segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_INT>,
+            segment_v2::FrameOfReferencePageDecoder<OLAP_FIELD_TYPE_INT> >(ints.get(), size);
+}
+
+TEST_F(FrameOfReferencePageTest, TestInt64BlockEncoderSequence) {
+    const uint32_t size = 10000;
+
+    std::unique_ptr<int64_t[]> ints(new int64_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = 21474836478 + i;
+    }
+
+    test_encode_decode_page_template<OLAP_FIELD_TYPE_BIGINT, segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_BIGINT>,
+            segment_v2::FrameOfReferencePageDecoder<OLAP_FIELD_TYPE_BIGINT> >(ints.get(), size);
+}
+
+TEST_F(FrameOfReferencePageTest, TestInt32SequenceBlockEncoderSize) {
+    size_t size = 128;
+    std::unique_ptr<int32_t[]> ints(new int32_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = i;
+    }
+    PageBuilderOptions builder_options;
+    builder_options.data_page_size = 256 * 1024;
+    segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_INT> page_builder(builder_options);
+    page_builder.add(reinterpret_cast<const uint8_t *>(ints.get()), &size);
+    Slice s = page_builder.finish();
+    // body: 4 bytes min value + 128 * 1 /8 packing value = 20
+    // header: 1 + 4 = 5
+    ASSERT_EQ(25, s.size);
+}
+
+TEST_F(FrameOfReferencePageTest, TestInt32NormalBlockEncoderSize) {
+    size_t size = 128;
+    std::unique_ptr<int32_t[]> ints(new int32_t[size]);
+    for (int i = 0; i < size; i++) {
+        ints.get()[i] = 128 - i;
+    }
+    PageBuilderOptions builder_options;
+    builder_options.data_page_size = 256 * 1024;
+    segment_v2::FrameOfReferencePageBuilder<OLAP_FIELD_TYPE_INT> page_builder(builder_options);
+    page_builder.add(reinterpret_cast<const uint8_t *>(ints.get()), &size);
+    Slice s = page_builder.finish();
+    // body: 4 bytes min value + 128 * 7 /8 packing value = 116
+    // header: 1 + 4 = 5
+    ASSERT_EQ(121, s.size);
+}
+
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
