11eafe524fea9fa0fc2aa8c8c761fe099c75ed0a
ZHAO Chun <buaa.zhaoc@gmail.com>
2019-09-13 08:27:24 +0800

    Add ChunkAllocator to accelerate chunk allocation (#1792)
    I add ChunkAllocator in this CL to put unused memory chunk to a chunk
    pool other than return it to system allocator. Now we only change
    MemPool's chunk allocation and free to this.
    
    And two configuration are introduduced too. 'chunk_reserved_bytes_limit'
    is the limit of how many bytes this chunk pool can reserve in total and
    its default value is 2147483648(2GB). 'use_mmap_allocate_chunk': if
    chunk is allocated via mmap and default value is false.
    
    And in my test case with default configuration a simple like
    "select * from table limit 10", this can improve throughput from 280 QPS
    to to 650 QPS. And when I config 'chunk_reserved_bytes_limit' to 0,
    which means this is disabled, the throughput is the same with origin's.

--- run-ut.sh
+++ run-ut.sh
@@ -211,6 +211,8 @@ ${DORIS_TEST_BINARY_DIR}/runtime/snapshot_loader_test
 ${DORIS_TEST_BINARY_DIR}/runtime/user_function_cache_test
 ${DORIS_TEST_BINARY_DIR}/runtime/small_file_mgr_test
 ${DORIS_TEST_BINARY_DIR}/runtime/mem_pool_test
+${DORIS_TEST_BINARY_DIR}/runtime/memory/chunk_allocator_test
+${DORIS_TEST_BINARY_DIR}/runtime/memory/system_allocator_test
 # Running expr Unittest
 
 # Running http
