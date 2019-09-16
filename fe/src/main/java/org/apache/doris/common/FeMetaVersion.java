a34775b0bc5d51ca14f0691f237e28ec4ea82e1a
xionglei0 <55084968+xionglei0@users.noreply.github.com>
2019-09-16 16:41:14 +0800

    Add parallel_exchange_instance_num to set parallel after exchange (#1788)

--- fe/src/main/java/org/apache/doris/common/FeMetaVersion.java
+++ fe/src/main/java/org/apache/doris/common/FeMetaVersion.java
@@ -132,4 +132,6 @@ public final class FeMetaVersion {
     public static final int VERSION_60 = 60;
     // for alter job v2
     public static final int VERSION_61 = 61;
+    // add param: doris_shuffle_partitions
+    public static final int VERSION_62 = 62;
 }
