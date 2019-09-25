05f88670088e789a9302b5b791da5d5c03cbf51a
yangzhg <yangzhgg@gmail.com>
2019-09-25 19:24:35 +0800

    Fix direct compilation failed #1862

--- be/src/olap/key_coder.h
+++ be/src/olap/key_coder.h
@@ -221,4 +221,13 @@ public:
     }
 };
 
+struct EnumClassHash
+{
+    template <typename T>
+    std::size_t operator()(T t) const
+    {
+        return static_cast<std::size_t>(t);
+    }
+};
+
 }
