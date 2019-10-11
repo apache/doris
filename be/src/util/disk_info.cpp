f1677c0b94a3c99f9fd0b60df101fa60e3f344dc
yangzhg <780531911@qq.com>
2019-10-11 17:21:41 +0800

    Fix some warning when compile type is debug using -Werror flag(because of use deprecated funcrtions) (#1953)

--- be/src/util/disk_info.cpp
+++ be/src/util/disk_info.cpp
@@ -24,6 +24,7 @@
 #include <sys/vfs.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/sysmacros.h>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/join.hpp>
