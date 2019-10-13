aa5ea6eafe9b5dd3d794be8a207f6b8828f2d61f
wangbo <506340561@qq.com>
2019-10-13 13:23:43 +0800

    Make Segment v2 use string's real length(#1943) (#1944)

--- be/src/olap/types.cpp
+++ be/src/olap/types.cpp
@@ -19,6 +19,8 @@
 
 namespace doris {
 
+void (*FieldTypeTraits<OLAP_FIELD_TYPE_CHAR>::set_to_max)(void*) = nullptr;
+
 template<typename TypeTraitsClass>
 TypeInfo::TypeInfo(TypeTraitsClass t)
       : _equal(TypeTraitsClass::equal),
@@ -27,7 +29,6 @@ TypeInfo::TypeInfo(TypeTraitsClass t)
         _deep_copy(TypeTraitsClass::deep_copy),
         _deep_copy_with_arena(TypeTraitsClass::deep_copy_with_arena),
         _direct_copy(TypeTraitsClass::direct_copy),
-        _allocate_value_from_arena(TypeTraitsClass::allocate_value_from_arena),
         _from_string(TypeTraitsClass::from_string),
         _to_string(TypeTraitsClass::to_string),
         _set_to_max(TypeTraitsClass::set_to_max),
--- be/src/olap/types.h
+++ be/src/olap/types.h
@@ -64,10 +64,6 @@ public:
         _direct_copy(dest, src);
     }
 
-    inline char* allocate_value_from_arena(Arena* arena) const {
-        return _allocate_value_from_arena(arena);
-    }
-
     OLAPStatus from_string(void* buf, const std::string& scan_key) const {
         return _from_string(buf, scan_key);
     }
@@ -89,7 +85,6 @@ private:
     void (*_deep_copy)(void* dest, const void* src, MemPool* mem_pool);
     void (*_deep_copy_with_arena)(void* dest, const void* src, Arena* arena);
     void (*_direct_copy)(void* dest, const void* src);
-    char* (*_allocate_value_from_arena)(Arena* arena);
 
     OLAPStatus (*_from_string)(void* buf, const std::string& scan_key);
     std::string (*_to_string)(const void* src);
@@ -216,10 +211,6 @@ struct BaseFieldtypeTraits : public CppTypeTraits<field_type> {
 
     static inline uint32_t hash_code(const void* data, uint32_t seed) {
         return HashUtil::hash(data, sizeof(CppType), seed);
-    }
-
-    static inline char* allocate_value_from_arena(Arena* arena) {
-        return arena->Allocate(sizeof(CppType));
     }
 
     static std::string to_string(const void* src) {
@@ -563,12 +554,10 @@ struct FieldTypeTraits<OLAP_FIELD_TYPE_CHAR> : public BaseFieldtypeTraits<OLAP_F
         memory_copy(l_slice->data, r_slice->data, r_slice->size);
         l_slice->size = r_slice->size;
     }
-    static void set_to_max(void* buf) {
-        // this function is used by scan key,
-        // the size may be greater than length in schema.
-        auto slice = reinterpret_cast<Slice*>(buf);
-        memset(slice->data, 0xff, slice->size);
-    }
+
+    // using field.set_to_max to set varchar/char,not here
+    static void (*set_to_max)(void*);
+
     static void set_to_min(void* buf) {
         auto slice = reinterpret_cast<Slice*>(buf);
         memset(slice->data, 0, slice->size);
@@ -576,13 +565,6 @@ struct FieldTypeTraits<OLAP_FIELD_TYPE_CHAR> : public BaseFieldtypeTraits<OLAP_F
     static uint32_t hash_code(const void* data, uint32_t seed) {
         auto slice = reinterpret_cast<const Slice*>(data);
         return HashUtil::hash(slice->data, slice->size, seed);
-    }
-    static char* allocate_value_from_arena(Arena* arena) {
-        char* type_value = arena->Allocate(sizeof(Slice));
-        auto slice = reinterpret_cast<Slice*>(type_value);
-        slice->size = OLAP_CHAR_MAX_LENGTH;
-        slice->data = arena->Allocate(OLAP_CHAR_MAX_LENGTH);
-        return type_value;
     }
 };
 
@@ -601,21 +583,10 @@ struct FieldTypeTraits<OLAP_FIELD_TYPE_VARCHAR> : public FieldTypeTraits<OLAP_FI
         slice->size = value_len;
         return OLAP_SUCCESS;
     }
-    static void set_to_max(void* buf) {
-        auto slice = reinterpret_cast<Slice*>(buf);
-        slice->size = 1;
-        memset(slice->data, 0xFF, 1);
-    }
+
     static void set_to_min(void* buf) {
         auto slice = reinterpret_cast<Slice*>(buf);
         slice->size = 0;
-    }
-    static char* allocate_value_from_arena(Arena* arena) {
-        char* type_value = arena->Allocate(sizeof(Slice));
-        auto slice = reinterpret_cast<Slice*>(type_value);
-        slice->size = OLAP_STRING_MAX_LENGTH;
-        slice->data = arena->Allocate(OLAP_STRING_MAX_LENGTH);
-        return type_value;
     }
 };
 
