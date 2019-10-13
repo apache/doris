3b6c580d4955cb097c89a3d4d300bf507afba7de
Lijia Liu <liutang123@yeah.net>
2019-10-13 22:06:09 +0800

    Support segment-level zone map (#1931)

--- be/src/olap/rowset/segment_v2/segment.cpp
+++ be/src/olap/rowset/segment_v2/segment.cpp
@@ -23,6 +23,7 @@
 #include "olap/rowset/segment_v2/column_reader.h" // ColumnReader
 #include "olap/rowset/segment_v2/segment_writer.h" // k_segment_magic_length
 #include "olap/rowset/segment_v2/segment_iterator.h"
+#include "olap/rowset/segment_v2/empty_segment_iterator.h"
 #include "util/slice.h" // Slice
 #include "olap/tablet_schema.h"
 #include "util/crc32c.h"
@@ -37,7 +38,8 @@ Segment::Segment(
         const TabletSchema* tablet_schema)
         : _fname(std::move(fname)),
         _segment_id(segment_id),
-        _tablet_schema(tablet_schema) {
+        _tablet_schema(tablet_schema),
+        _index_loaded(false) {
 }
 
 Segment::~Segment() {
@@ -50,17 +52,69 @@ Status Segment::open() {
     RETURN_IF_ERROR(Env::Default()->new_random_access_file(_fname, &_input_file));
     // parse footer to get meta
     RETURN_IF_ERROR(_parse_footer());
-    // parse short key index
-    RETURN_IF_ERROR(_parse_index());
-    // initial all column reader
-    RETURN_IF_ERROR(_initial_column_readers());
+
     return Status::OK();
 }
 
-std::unique_ptr<SegmentIterator> Segment::new_iterator(const Schema& schema, const StorageReadOptions& read_options) {
-    auto it = std::unique_ptr<SegmentIterator>(new SegmentIterator(this->shared_from_this(), schema));
-    it->init(read_options);
-    return it;
+Status Segment::new_iterator(
+        const Schema& schema,
+        const StorageReadOptions& read_options,
+        std::unique_ptr<RowwiseIterator>* iter) {
+
+    if (read_options.conditions != nullptr) {
+        for (auto& column_condition : read_options.conditions->columns()) {
+            int32_t column_id = column_condition.first;
+            auto entry = _column_id_to_footer_ordinal.find(column_id);
+            if (entry == _column_id_to_footer_ordinal.end()) {
+                continue;
+            }
+            auto& c_meta = _footer.columns(entry->second);
+            if (!c_meta.has_zone_map()) {
+                continue;
+            }
+            auto& c_zone_map = c_meta.zone_map();
+            if (!c_zone_map.has_not_null() && !c_zone_map.has_null()) {
+                // no data
+                iter->reset(new EmptySegmentIterator(schema));
+                return Status::OK();
+            }
+            // TODO Logic here and the similar logic in ColumnReader::_get_filtered_pages should be unified.
+            TypeInfo* type_info = get_type_info((FieldType)c_meta.type());
+            if (type_info == nullptr) {
+                return Status::NotSupported(Substitute("unsupported typeinfo, type=$0", c_meta.type()));
+            }
+            FieldType type = type_info->type();
+            std::unique_ptr<WrapperField> min_value(WrapperField::create_by_type(type));
+            std::unique_ptr<WrapperField> max_value(WrapperField::create_by_type(type));
+            if (c_zone_map.has_not_null()) {
+                min_value->from_string(c_zone_map.min());
+                max_value->from_string(c_zone_map.max());
+            }
+            if (c_zone_map.has_null()) {
+                min_value->set_null();
+                if (!c_zone_map.has_not_null()) {
+                    max_value->set_null();
+                }
+            }
+            if (!column_condition.second->eval({min_value.get(), max_value.get()})) {
+                // any condition not satisfied, return.
+                iter->reset(new EmptySegmentIterator(schema));
+                return Status::OK();
+            }
+        }
+    }
+
+
+    if(!_index_loaded) {
+        // parse short key index
+        RETURN_IF_ERROR(_parse_index());
+        // initial all column reader
+        RETURN_IF_ERROR(_initial_column_readers());
+        _index_loaded = true;
+    }
+    iter->reset(new SegmentIterator(this->shared_from_this(), schema));
+    iter->get()->init(read_options);
+    return Status::OK();
 }
 
 Status Segment::_parse_footer() {
@@ -103,6 +157,11 @@ Status Segment::_parse_footer() {
     if (!_footer.ParseFromString(footer_buf)) {
         return Status::Corruption(Substitute("Bad segment file $0: failed to parse SegmentFooterPB", _fname));
     }
+
+    for (uint32_t ordinal = 0; ordinal < _footer.columns().size(); ++ordinal) {
+        auto& column_pb = _footer.columns(ordinal);
+        _column_id_to_footer_ordinal.emplace(column_pb.unique_id(), ordinal);
+    }
     return Status::OK();
 }
 
@@ -120,15 +179,6 @@ Status Segment::_parse_index() {
 }
 
 Status Segment::_initial_column_readers() {
-    // Map from column unique id to column ordinal in footer's ColumnMetaPB
-    // If we can't find unique id, it means this segment is created
-    // with an old schema. So we should create a DefaultValueIterator
-    // for this column.
-    std::unordered_map<uint32_t, uint32_t> unique_id_to_ordinal;
-    for (uint32_t ordinal = 0; ordinal < _footer.columns().size(); ++ordinal) {
-        auto& column_pb = _footer.columns(ordinal);
-        unique_id_to_ordinal.emplace(column_pb.unique_id(), ordinal);
-    }
     // TODO(zc): Lazy init()?
     // There may be too many columns, majority of them would not be used
     // in query, so we should not init them here.
@@ -136,8 +186,8 @@ Status Segment::_initial_column_readers() {
 
     for (uint32_t ordinal = 0; ordinal < _tablet_schema->num_columns(); ++ordinal) {
         auto& column = _tablet_schema->columns()[ordinal];
-        auto iter = unique_id_to_ordinal.find(column.unique_id());
-        if (iter == unique_id_to_ordinal.end()) {
+        auto iter = _column_id_to_footer_ordinal.find(column.unique_id());
+        if (iter == _column_id_to_footer_ordinal.end()) {
             continue;
         }
 
