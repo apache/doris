7d2796c71a6e353061707d2b802014bdd9b7ad0e
emmymiao87 <522274284@qq.com>
2019-10-15 21:25:53 +0800

    Add log

--- fe/src/main/java/org/apache/doris/load/loadv2/LoadJob.java
+++ fe/src/main/java/org/apache/doris/load/loadv2/LoadJob.java
@@ -744,6 +744,9 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         writeLock();
         try {
             executeReplayTxnAttachment(txnState);
+            LOG.debug(new LogBuilder(LogKey.LOAD_JOB, id)
+                              .add("msg", "replay on committed")
+                              .build());
         } finally {
             writeUnlock();
         }
@@ -820,6 +823,11 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
             progress = 100;
             finishTimestamp = txnState.getFinishTime();
             state = JobState.FINISHED;
+            LOG.debug(new LogBuilder(LogKey.LOAD_JOB, id)
+                              .add("state", state)
+                              .add("progress", progress)
+                              .add("msg", "replay on visible")
+                              .build());
         } finally {
             writeUnlock();
         }
