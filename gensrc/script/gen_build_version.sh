cd60085b65a882e03c5640e196e60f5c5f50e327
Zhao Chun <zhaoc@apache.org>
2019-10-17 07:00:35 +0000

    Change build_version to 0.11.0

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="trunk"
+build_version="0.11.0"
 
 unset LANG
 unset LC_CTYPE
