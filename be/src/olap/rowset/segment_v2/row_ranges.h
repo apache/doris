a514fee4669b066208cf00fe38d2c41317bb8b4e
huangkangping <huangkangping>
2019-10-25 17:08:32 +0800

    add unit test for RowBlockV2

--- be/src/olap/rowset/segment_v2/row_ranges.h
+++ be/src/olap/rowset/segment_v2/row_ranges.h
@@ -257,6 +257,9 @@ private:
     // trying to union the specified range to the last ranges in the list. The specified range shall be larger(*) than
     // the last one or might be overlapped with some of the last ones.
     void add(const RowRange& range) {
+        if (range.is_empty()) {
+            return;
+        }
         RowRange range_to_add = range;
         for (int i = _ranges.size() - 1; i >= 0; --i) {
             const RowRange last = _ranges[i];
