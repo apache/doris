7011a629ad42c255001edf82246bd936aaee550e
kangpinghuang <kphuang@163.com>
2019-11-07 19:06:24 +0800

    Fix default value column bug (#2134)

--- be/src/olap/rowset/segment_v2/segment.cpp
+++ be/src/olap/rowset/segment_v2/segment.cpp
@@ -200,11 +200,12 @@ Status Segment::_create_column_readers() {
 Status Segment::new_column_iterator(uint32_t cid, ColumnIterator** iter) {
     if (_column_readers[cid] == nullptr) {
         const TabletColumn& tablet_column = _tablet_schema->column(cid);
-        if (!tablet_column.has_default_value()) {
+        if (!tablet_column.has_default_value() && !tablet_column.is_nullable()) {
             return Status::InternalError("invalid nonexistent column without default value.");
         }
         std::unique_ptr<DefaultValueColumnIterator> default_value_iter(
-                new DefaultValueColumnIterator(tablet_column.default_value(),
+                new DefaultValueColumnIterator(tablet_column.has_default_value(),
+                tablet_column.default_value(),
                 tablet_column.is_nullable(), tablet_column.type()));
         ColumnIteratorOptions iter_opts;
         RETURN_IF_ERROR(default_value_iter->init(iter_opts));
