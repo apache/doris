e14ccf9863649d26da556cd4509b2be132c3eb25
huangwei5 <huangwei5@xiaomi.com>
2019-11-20 18:30:52 +0800

    add unit test of parsing storage_root_path

--- run-ut.sh
+++ run-ut.sh
@@ -248,6 +248,7 @@ ${DORIS_TEST_BINARY_DIR}/olap/row_cursor_test
 ${DORIS_TEST_BINARY_DIR}/olap/skiplist_test
 ${DORIS_TEST_BINARY_DIR}/olap/serialize_test
 # ${DORIS_TEST_BINARY_DIR}/olap/memtable_flush_executor_test
+${DORIS_TEST_BINARY_DIR}/olap/options_test
 
 # Running routine load test
 ${DORIS_TEST_BINARY_DIR}/olap/tablet_meta_manager_test
