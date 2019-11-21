f4ef70546d6fa47e0256768fed282a64653a26bf
xy720 <xuyang25@baidu.com>
2019-11-21 16:55:59 +0800

    commit 8: add unit tests

--- fe/src/test/java/org/apache/doris/analysis/SqlModeTest.java
+++ fe/src/test/java/org/apache/doris/analysis/SqlModeTest.java
@@ -9,7 +9,7 @@ import java.io.StringReader;
 public class SqlModeTest {
 
     @Test
-    public void testDefaultMode() {
+    public void testScannerConstructor() {
         String stmt = new String("SELECT * FROM db1.tbl1 WHERE name = 'BILL GATES'");
         SqlParser parser = new SqlParser(new SqlScanner(new StringReader(stmt)));
         SelectStmt selectStmt = null;
@@ -19,6 +19,7 @@ public class SqlModeTest {
             Assert.fail(e.getMessage());
         }
         Assert.assertEquals("SELECT  FROM `db1`.`tbl1` WHERE `name` = 'BILL GATES'", selectStmt.toSql());
+
         parser = new SqlParser(new SqlScanner(new StringReader(stmt), SqlModeHelper.MODE_DEFAULT));
         try {
             selectStmt = (SelectStmt) parser.parse().value;
@@ -30,6 +31,7 @@ public class SqlModeTest {
 
     @Test
     public void testPipesAsConcatMode() {
+        // Mode Active
         String stmt = new String("SELECT 'a' || 'b' || 'c'");
         SqlParser parser = new SqlParser(new SqlScanner(new StringReader(stmt), SqlModeHelper.MODE_PIPES_AS_CONCAT));
         SelectStmt selectStmt = null;
@@ -38,10 +40,23 @@ public class SqlModeTest {
         } catch (Exception e) {
             Assert.fail(e.getMessage());
         }
-        Expr e = selectStmt.getSelectList().getItems().get(0).getExpr();
-        if (!(e instanceof FunctionCallExpr)) {
+        Expr expr = selectStmt.getSelectList().getItems().get(0).getExpr();
+        if (!(expr instanceof FunctionCallExpr)) {
             Assert.fail("Mode not working");
         }
-        Assert.assertEquals("concat('a', 'b', 'c')", e.toSql());
+        Assert.assertEquals("concat('a', 'b', 'c')", expr.toSql());
+
+        // Mode DeActive
+        parser = new SqlParser(new SqlScanner(new StringReader(stmt), SqlModeHelper.MODE_DEFAULT));
+        try {
+            selectStmt = (SelectStmt) parser.parse().value;
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+        expr = selectStmt.getSelectList().getItems().get(0).getExpr();
+        if (!(expr instanceof CompoundPredicate)) {
+            Assert.fail();
+        }
+        Assert.assertEquals("(('a') OR ('b')) OR ('c')", expr.toSql());
     }
 }
