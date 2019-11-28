92681c78a5358f540c93b54ef2c5feba2d0226a4
wangcong18 <wangcong18@xiaomi.com>
2019-11-28 15:14:08 +0800

    encapsulate some function from DynamicPartitionScheduler to TableProperty

--- fe/src/main/java/org/apache/doris/catalog/TableProperty.java
+++ fe/src/main/java/org/apache/doris/catalog/TableProperty.java
@@ -18,25 +18,47 @@
 package org.apache.doris.catalog;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import org.apache.doris.analysis.AddPartitionClause;
+import org.apache.doris.analysis.DistributionDesc;
+import org.apache.doris.analysis.HashDistributionDesc;
+import org.apache.doris.analysis.PartitionKeyDesc;
+import org.apache.doris.analysis.PartitionValue;
+import org.apache.doris.analysis.SingleRangePartitionDesc;
+import org.apache.doris.common.DdlException;
+import org.apache.doris.common.Pair;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
 import org.apache.doris.common.util.PropertyAnalyzer;
+import org.apache.doris.task.DynamicPartitionScheduler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TableProperty implements Writable {
-    private Map<String, String> dynamicProperties = new HashMap<>();
+    private static final Logger LOG = LogManager.getLogger(TableProperty.class);
 
     public static final String LAST_UPDATE_TIME = "LastUpdateTime";
     public static final String MSG = "Msg";
     public static final String STATE = "State";
 
+    private static final String TIMESTAMP_FORMAT = "yyyyMMdd";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     private long dbId;
     private long tableId;
+    private Map<String, String> dynamicProperties = new HashMap<>();
 
     public enum State {
         NORMAL,
@@ -136,5 +158,113 @@ public class TableProperty implements Writable {
         TableProperty info = new TableProperty();
         info.readFields(in);
         return info;
+    }
+
+    public static boolean checkDynamicPartitionTable(Table table) {
+        if (!(table instanceof OlapTable)) {
+            return false;
+        }
+        if (!(((OlapTable) table).getPartitionInfo().getType().equals(PartitionType.RANGE))) {
+            return false;
+        }
+        RangePartitionInfo rangePartitionInfo = (RangePartitionInfo) ((OlapTable) table).getPartitionInfo();
+        String enable = ((OlapTable) table).getTableProperty().getDynamicPartitionEnable();
+        return rangePartitionInfo.getPartitionColumns().size() == 1 &&
+                !Strings.isNullOrEmpty(enable) && enable.equalsIgnoreCase(PropertyAnalyzer.TRUE);
+    }
+
+    public static void dynamicAddPartition() throws DdlException {
+        for (Pair<Long, Long> tableInfo : DynamicPartitionScheduler.getDynamicPartitionTableInfo()) {
+            Long dbId = tableInfo.first;
+            Long tableId = tableInfo.second;
+            Database db = Catalog.getInstance().getDb(dbId);
+            if (db == null || db.getTable((tableId)) == null) {
+                DynamicPartitionScheduler.removeDynamicPartitionTable(dbId, tableId);
+                continue;
+            }
+
+            OlapTable table = (OlapTable) db.getTable(tableId);
+            String rangePartitionFormat;
+            RangePartitionInfo rangePartitionInfo = (RangePartitionInfo) table.getPartitionInfo();
+            Column partitionColumn = rangePartitionInfo.getPartitionColumns().get(0);
+            if (partitionColumn.getDataType().equals(PrimitiveType.DATE)) {
+                rangePartitionFormat = DATE_FORMAT;
+            } else if (partitionColumn.getDataType().equals(PrimitiveType.DATETIME)) {
+                rangePartitionFormat = DATETIME_FORMAT;
+            } else {
+                rangePartitionFormat = TIMESTAMP_FORMAT;
+            }
+
+            Calendar calendar = Calendar.getInstance();
+            TableProperty tableProperty = table.getTableProperty();
+            int end = Integer.parseInt(tableProperty.getDynamicPartitionEnd());
+            for (int i = 1; i <= end; i++) {
+                String dynamicPartitionPrefix = tableProperty.getDynamicPartitionPrefix();
+                String partitionName = dynamicPartitionPrefix + getPartitionRange(
+                        tableProperty.getDynamicPartitionTimeUnit(), i, (Calendar) calendar.clone(), rangePartitionFormat);
+                String validPartitionName = partitionName.replace("-", "");
+                if (table.getPartition(validPartitionName) != null ||
+                        !(table.getDefaultDistributionInfo() instanceof HashDistributionInfo)) {
+                    continue;
+                }
+
+                String nextBorder = getPartitionRange(tableProperty.getDynamicPartitionTimeUnit(),
+                        i + 1,(Calendar) calendar.clone(), rangePartitionFormat);
+                PartitionValue partitionValue = new PartitionValue(nextBorder);
+                PartitionKeyDesc partitionKeyDesc = new PartitionKeyDesc(Collections.singletonList(partitionValue));
+                HashMap<String, String> partitionProperties = new HashMap<>(1);
+                partitionProperties.put("replication_num", String.valueOf(estimateReplicateNum(table)));
+                SingleRangePartitionDesc desc = new SingleRangePartitionDesc(false,
+                        validPartitionName, partitionKeyDesc, partitionProperties);
+
+                HashDistributionInfo hashDistributionInfo = (HashDistributionInfo) table.getDefaultDistributionInfo();
+                List<String> distColumnNames = new ArrayList<>();
+                for (Column distributionColumn : hashDistributionInfo.getDistributionColumns()) {
+                    distColumnNames.add(distributionColumn.getName());
+                }
+                DistributionDesc distributionDesc = new HashDistributionDesc(
+                        Integer.parseInt(tableProperty.getDynamicPartitionBuckets()), distColumnNames);
+
+                AddPartitionClause addPartitionClause = new AddPartitionClause(desc, distributionDesc, null);
+                HashMap<String, String> properties = Maps.newHashMapWithExpectedSize(2);
+                try {
+                    Catalog.getInstance().addPartition(db, table.getName(), addPartitionClause);
+                    properties.put(TableProperty.STATE, TableProperty.State.NORMAL.toString());
+                    properties.put(TableProperty.MSG, " ");
+                } catch (DdlException e) {
+                    properties.put(TableProperty.STATE, TableProperty.State.ERROR.toString());
+                    properties.put(TableProperty.MSG, e.getMessage());
+                } finally {
+                    Catalog.getInstance().modifyTableDynamicPartition(db, table, properties);
+                }
+            }
+        }
+    }
+
+    private static String getPartitionRange(String timeUnit, int offset, Calendar calendar, String format) {
+        if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.DAY)) {
+            calendar.add(Calendar.DAY_OF_MONTH, offset);
+        } else if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.WEEK)){
+            calendar.add(Calendar.WEEK_OF_MONTH, offset);
+        } else if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.MONTH)) {
+            calendar.add(Calendar.MONTH, offset);
+        } else {
+            LOG.error("Unsupported time unit: " + timeUnit);
+            return null;
+        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    private static int estimateReplicateNum(OlapTable table) {
+        int replicateNum = 3;
+        long maxPartitionId = 0;
+        for (Partition partition: table.getPartitions()) {
+            if (partition.getId() > maxPartitionId) {
+                maxPartitionId = partition.getId();
+                replicateNum = table.getPartitionInfo().getReplicationNum(partition.getId());
+            }
+        }
+        return replicateNum;
     }
 }
--- fe/src/main/java/org/apache/doris/task/DynamicPartitionScheduler.java
+++ fe/src/main/java/org/apache/doris/task/DynamicPartitionScheduler.java
@@ -17,47 +17,21 @@
 
 package org.apache.doris.task;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
-import org.apache.doris.analysis.AddPartitionClause;
-import org.apache.doris.analysis.DistributionDesc;
-import org.apache.doris.analysis.HashDistributionDesc;
-import org.apache.doris.analysis.PartitionKeyDesc;
-import org.apache.doris.analysis.PartitionValue;
-import org.apache.doris.analysis.SingleRangePartitionDesc;
 import org.apache.doris.catalog.Catalog;
-import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.Database;
-import org.apache.doris.catalog.HashDistributionInfo;
-import org.apache.doris.catalog.OlapTable;
-import org.apache.doris.catalog.Partition;
-import org.apache.doris.catalog.PartitionType;
-import org.apache.doris.catalog.PrimitiveType;
-import org.apache.doris.catalog.RangePartitionInfo;
 import org.apache.doris.catalog.Table;
 import org.apache.doris.catalog.TableProperty;
 import org.apache.doris.common.DdlException;
 import org.apache.doris.common.Pair;
 import org.apache.doris.common.util.MasterDaemon;
-import org.apache.doris.common.util.PropertyAnalyzer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class DynamicPartitionScheduler extends MasterDaemon {
     private static final Logger LOG = LogManager.getLogger(DynamicPartitionScheduler.class);
-
-    private static final String TIMESTAMP_FORMAT = "yyyyMMdd";
-    private static final String DATE_FORMAT = "yyyy-MM-dd";
-    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     private static Set<Pair<Long, Long>> dynamicPartitionTableInfo = new HashSet<>();
     private boolean initialize;
@@ -65,32 +39,6 @@ public class DynamicPartitionScheduler extends MasterDaemon {
     public DynamicPartitionScheduler() {
         this.initialize = false;
     }
-
-    private void initDynamicPartitionTable() {
-        for (Long dbId : Catalog.getInstance().getDbIds()) {
-            Database db = Catalog.getInstance().getDb(dbId);
-            for (Table table : Catalog.getInstance().getDb(dbId).getTables()) {
-                if (checkDynamicPartitionTable(table)) {
-                    registerDynamicPartitionTable(db.getId(), table.getId());
-                }
-            }
-        }
-        initialize = true;
-    }
-
-    private boolean checkDynamicPartitionTable(Table table) {
-        if (!(table instanceof OlapTable)) {
-            return false;
-        }
-        if (!(((OlapTable) table).getPartitionInfo().getType().equals(PartitionType.RANGE))) {
-            return false;
-        }
-        RangePartitionInfo rangePartitionInfo = (RangePartitionInfo) ((OlapTable) table).getPartitionInfo();
-        String enable = ((OlapTable) table).getTableProperty().getDynamicPartitionEnable();
-        return rangePartitionInfo.getPartitionColumns().size() == 1 &&
-                !Strings.isNullOrEmpty(enable) && enable.equalsIgnoreCase(PropertyAnalyzer.TRUE);
-    }
-
     public static void registerDynamicPartitionTable(Long dbId, Long tableId) {
         dynamicPartitionTableInfo.add(new Pair<>(dbId, tableId));
     }
@@ -99,116 +47,30 @@ public class DynamicPartitionScheduler extends MasterDaemon {
         dynamicPartitionTableInfo.remove(new Pair<>(dbId, tableId));
     }
 
-    private void dynamicAddPartition() throws DdlException {
-        for (Pair<Long, Long> tableInfo : dynamicPartitionTableInfo) {
-            Long dbId = tableInfo.first;
-            Long tableId = tableInfo.second;
+    public static Set<Pair<Long, Long>> getDynamicPartitionTableInfo() {
+        return dynamicPartitionTableInfo;
+    }
+
+    private void initDynamicPartitionTable() {
+        for (Long dbId : Catalog.getInstance().getDbIds()) {
             Database db = Catalog.getInstance().getDb(dbId);
-            if (db == null) {
-                dynamicPartitionTableInfo.remove(new Pair<>(dbId, tableId));
-                continue;
-            }
-            OlapTable table = (OlapTable) db.getTable(tableId);
-            if (table == null) {
-                dynamicPartitionTableInfo.remove(new Pair<>(dbId, tableId));
-                continue;
-            }
-
-            String rangePartitionFormat;
-            RangePartitionInfo rangePartitionInfo = (RangePartitionInfo) table.getPartitionInfo();
-            Column partitionColumn = rangePartitionInfo.getPartitionColumns().get(0);
-            if (partitionColumn.getDataType().equals(PrimitiveType.DATE)) {
-                rangePartitionFormat = DATE_FORMAT;
-            } else if (partitionColumn.getDataType().equals(PrimitiveType.DATETIME)) {
-                rangePartitionFormat = DATETIME_FORMAT;
-            } else {
-                rangePartitionFormat = TIMESTAMP_FORMAT;
-            }
-
-            Calendar calendar = Calendar.getInstance();
-            TableProperty tableProperty = table.getTableProperty();
-            int end = Integer.parseInt(tableProperty.getDynamicPartitionEnd());
-            for (int i = 1; i <= end; i++) {
-                String dynamicPartitionPrefix = tableProperty.getDynamicPartitionPrefix();
-                String partitionName = dynamicPartitionPrefix + getPartitionRange(
-                        tableProperty.getDynamicPartitionTimeUnit(), i, (Calendar) calendar.clone(), rangePartitionFormat);
-                String validPartitionName = partitionName.replace("-", "");
-                if (table.getPartition(validPartitionName) != null) {
-                    continue;
-                }
-
-                if (!(table.getDefaultDistributionInfo() instanceof HashDistributionInfo)) {
-                    LOG.warn("Auto add partition task should only support hash distribution buckets");
-                    continue;
-                }
-
-                String nextBorder = getPartitionRange(tableProperty.getDynamicPartitionTimeUnit(),
-                        i + 1,(Calendar) calendar.clone(), rangePartitionFormat);
-                PartitionValue partitionValue = new PartitionValue(nextBorder);
-                PartitionKeyDesc partitionKeyDesc = new PartitionKeyDesc(Collections.singletonList(partitionValue));
-                HashMap<String, String> partitionProperties = new HashMap<>(1);
-                partitionProperties.put("replication_num", String.valueOf(estimateReplicateNum(table)));
-                SingleRangePartitionDesc desc = new SingleRangePartitionDesc(false,
-                        validPartitionName, partitionKeyDesc, partitionProperties);
-
-                HashDistributionInfo hashDistributionInfo = (HashDistributionInfo) table.getDefaultDistributionInfo();
-                List<String> distColumnNames = new ArrayList<>();
-                for (Column distributionColumn : hashDistributionInfo.getDistributionColumns()) {
-                    distColumnNames.add(distributionColumn.getName());
-                }
-                DistributionDesc distributionDesc = new HashDistributionDesc(
-                        Integer.parseInt(tableProperty.getDynamicPartitionBuckets()), distColumnNames);
-
-                AddPartitionClause addPartitionClause = new AddPartitionClause(desc, distributionDesc, null);
-                HashMap<String, String> properties = Maps.newHashMapWithExpectedSize(2);
-                try {
-                    Catalog.getInstance().addPartition(db, table.getName(), addPartitionClause);
-                    properties.put(TableProperty.STATE, TableProperty.State.NORMAL.toString());
-                    properties.put(TableProperty.MSG, " ");
-                } catch (DdlException e) {
-                    properties.put(TableProperty.STATE, TableProperty.State.ERROR.toString());
-                    properties.put(TableProperty.MSG, e.getMessage());
-                } finally {
-                    Catalog.getInstance().modifyTableDynamicPartition(db, table, properties);
+            for (Table table : Catalog.getInstance().getDb(dbId).getTables()) {
+                if (TableProperty.checkDynamicPartitionTable(table)) {
+                    registerDynamicPartitionTable(db.getId(), table.getId());
                 }
             }
         }
-    }
-
-    private String getPartitionRange(String timeUnit, int offset, Calendar calendar, String format) {
-        if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.DAY)) {
-            calendar.add(Calendar.DAY_OF_MONTH, offset);
-        } else if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.WEEK)){
-            calendar.add(Calendar.WEEK_OF_MONTH, offset);
-        } else if (timeUnit.equalsIgnoreCase(PropertyAnalyzer.MONTH)) {
-            calendar.add(Calendar.MONTH, offset);
-        } else {
-            LOG.error("Unsupported time unit: " + timeUnit);
-            return null;
-        }
-        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
-        return dateFormat.format(calendar.getTime());
-    }
-
-    private int estimateReplicateNum(OlapTable table) {
-        int replicateNum = 3;
-        long maxPartitionId = 0;
-        for (Partition partition: table.getPartitions()) {
-            if (partition.getId() > maxPartitionId) {
-                maxPartitionId = partition.getId();
-                replicateNum = table.getPartitionInfo().getReplicationNum(partition.getId());
-            }
-        }
-        return replicateNum;
+        initialize = true;
     }
 
     @Override
     protected void runAfterCatalogReady() {
         if (!initialize) {
+            // check Dynamic Partition tables only when FE start
             initDynamicPartitionTable();
         }
         try {
-            dynamicAddPartition();
+            TableProperty.dynamicAddPartition();
         } catch (DdlException e) {
             LOG.warn("dynamic add partition failed: " + e.getMessage());
         }
