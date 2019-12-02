48027645799192700cd3ac6a8b166cde1aa91561
morningman <morningman@163.com>
2019-12-02 09:37:52 +0800

    Add classes related to "tag".
        Resource:   is the collective name of the nodes that provide various service capabilities in Doris cluster.
        Tag:        A Tag consists of type and name.
        TagSet:     TagSet represents a set of tags.
        TagManager: maintains 2 indexes:
            one is from tag to resource.
            one is from resource to tags

---
+++ fe/src/main/java/org/apache/doris/resource/Tag.java
@@ -0,0 +1,117 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.resource;
+
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.io.Text;
+import org.apache.doris.common.io.Writable;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Objects;
+
+/*
+ * A Tag consists of type and name.
+ * Tag type and name are both case insensitive, and represented in lower case.
+ * Tag is printed as { "type": "name" }
+ * 
+ * A Tag is immutable once it being created.
+ */
+public class Tag implements Writable {
+    // Classification of tag
+    public enum Type {
+        TYPE, FUNCTION, LOCATION, CUSTOM;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+    
+    public static final ImmutableSet<String> RESERVED_TAG_NAMES = ImmutableSet.of(
+            "frontend", "backend", "broker", "remote_storage", "store", "computation", "default_cluster");
+    private static final String TAG_NAME_REGEX = "^[a-z][a-z0-9_]{0,32}$";
+
+    public Type type;
+    public String tag;
+
+    private Tag(Type type, String tag) {
+        this.type = type;
+        this.tag = tag.toLowerCase();
+    }
+
+    public static Tag create(String typeName, String tagName) throws AnalysisException {
+        try {
+            Type tagType = Type.valueOf(typeName.toUpperCase());
+            tagName = tagName.toLowerCase();
+            if (Strings.isNullOrEmpty(tagName) || !tagName.matches(TAG_NAME_REGEX)) {
+                throw new AnalysisException("Invalid tag name: " + tagName);
+            }
+
+            return create(tagType, tagName);
+        } catch (IllegalArgumentException e) {
+            throw new AnalysisException("Invalid tag type: " + typeName);
+        }
+    }
+
+    public static Tag create(Type type, String tagName) {
+        return new Tag(type, tagName);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, tag);
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (!(other instanceof Tag)) {
+            return false;
+        }
+        Tag otherTag = (Tag) other;
+        return type == otherTag.type && Objects.equals(tag, otherTag.tag);
+    }
+
+    @Override
+    public String toString() {
+        return "{\"" + type.toString() + "\" : \"" + tag + "\"}";
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        Text.writeString(out, type.name());
+        Text.writeString(out, tag);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        type = Tag.Type.valueOf(Text.readString(in));
+        tag = Text.readString(in);
+    }
+
+    public static Tag read(DataInput in) throws IOException {
+        Type type = Tag.Type.valueOf(Text.readString(in));
+        String tag = Text.readString(in);
+        return Tag.create(type, tag);
+    }
+}
---
+++ fe/src/main/java/org/apache/doris/resource/TagSet.java
@@ -0,0 +1,216 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.resource;
+
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.io.Writable;
+import org.apache.doris.resource.Tag.Type;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.gson.Gson;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/*
+ * TagSet represents a set of tags.
+ * TagSet is printed as { "type1" : "tag1,tag2", "type2" : "tag1" }
+ * TagSet is mutable and not thread safe
+ */
+public class TagSet implements Writable {
+    public static final TagSet EMPTY_TAGSET = new TagSet();
+
+    private Set<Tag> tags = Sets.newHashSet();
+
+    private TagSet() {
+    }
+
+    private TagSet(TagSet other) {
+        for (Tag tag : other.getAllTags()) {
+            tags.add(tag);
+        }
+    }
+
+    // create TagSet from tag map: { "type1" : "tag1,tag2", "type2" : "tag1" }
+    public static TagSet create(Map<String, String> tagsMap) throws AnalysisException {
+        TagSet tagSet = new TagSet();
+        for (Map.Entry<String, String> entry : tagsMap.entrySet()) {
+            String typeStr = entry.getKey();
+            String tagsStr = entry.getValue();
+            String[] tagParts = tagsStr.split(",");
+            for (String tagPart : tagParts) {
+                Tag tag = Tag.create(typeStr, tagPart.trim());
+                tagSet.addTag(tag);
+            }
+        }
+        return tagSet;
+    }
+
+    // create from a single tag
+    public static TagSet create(String type, String tagName) throws AnalysisException {
+        TagSet tagSet = new TagSet();
+        Tag tag = Tag.create(type, tagName.trim());
+        tagSet.addTag(tag);
+        return tagSet;
+    }
+
+    // create from multi tags
+    public static TagSet create(Tag... tags) {
+        TagSet tagSet = new TagSet();
+        for (Tag tag : tags) {
+            tagSet.addTag(tag);
+        }
+        return tagSet;
+    }
+
+    public static TagSet copyFrom(TagSet other) {
+        TagSet tagSet = new TagSet();
+        for (Tag tag : other.tags) {
+            tagSet.addTag(tag);
+        }
+        return tagSet;
+    }
+
+    // return true if tag doesn't exist
+    public boolean addTag(Tag tag) {
+        return this.tags.add(tag);
+    }
+
+    // return true if tag exist
+    public boolean deleteTag(Tag tag) {
+        return tags.remove(tag);
+    }
+
+    // get a set of tags by tag type
+    public TagSet getTagsByType(Tag.Type type) {
+        TagSet tagSet = new TagSet();
+        for (Tag tag : tags) {
+            if (tag.type == type) {
+                tagSet.addTag(tag);
+            }
+        }
+        return tagSet;
+    }
+
+    public boolean containsTag(Tag tag) {
+        return tags.contains(tag);
+    }
+
+    // the result is the union of 2 sets.
+    public void union(TagSet other) {
+        for (Tag tag : other.tags) {
+            addTag(tag);
+        }
+    }
+
+    // return all types in this tag set
+    public Set<Tag.Type> getTypes() {
+        Set<Tag.Type> set = Sets.newHashSet();
+        for (Tag tag : tags) {
+            set.add(tag.type);
+        }
+        return set;
+    }
+
+    // delete all tags of specified type
+    private void deleteType(Type type) {
+        tags = tags.stream().filter(t -> t.type != type).collect(Collectors.toSet());
+    }
+
+    // merge 2 tag sets, but all types of tag in target tag set will be substituted by type in 'other' tagset
+    // eg:
+    // tagset A: { "type1" : "tag1,tag2", "type2" : "tag1" }
+    // tagset B: { "type1" : "tag3", "type3" : "tag4" }
+    // result of A.substituteMerge(B): { "type1" : "tag3", "type2" : "tag1", "type3" : "tag4" }
+    public void substituteMerge(TagSet other) {
+        Set<Tag.Type> types = other.getTypes();
+        for (Tag.Type type : types) {
+            deleteType(type);
+            union(other.getTagsByType(type));
+        }
+    }
+
+    public Set<Tag> getAllTags() {
+        return tags;
+    }
+
+    public boolean isEmpty() {
+        return tags.isEmpty();
+    }
+
+    // print as { "type1" : "tag1,tag2", "type2" : "tag1" }
+    @Override
+    public String toString() {
+        Map<String, String> map = Maps.newHashMap();
+        Gson gson = new Gson();
+        for (Tag.Type type : Tag.Type.values()) {
+            TagSet tagSet = getTagsByType(type);
+            if (!tagSet.isEmpty()) {
+                map.put(type.toString(), Joiner.on(",").join(tagSet.getAllTags().stream().map(t -> t.tag).collect(Collectors.toList())));
+            }
+        }
+        return gson.toJson(map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tags);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof TagSet)) {
+            return false;
+        }
+        TagSet tagSet = (TagSet) obj;
+        return tags.equals(tagSet.tags);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(tags.size());
+        for (Tag tag : tags) {
+            tag.write(out);
+        }
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        int size = in.readInt();
+        for (int i = 0; i < size; i++) {
+            Tag tag = Tag.read(in);
+            tags.add(tag);
+        }
+    }
+
+    public static TagSet read(DataInput in) throws IOException {
+        TagSet tagSet = new TagSet();
+        tagSet.readFields(in);
+        return tagSet;
+    }
+}
