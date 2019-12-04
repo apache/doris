c159463d9bf689fc804c1be580a6f0edf2e9b5a6
wangcong18 <wangcong18@xiaomi.com>
2019-12-04 17:10:57 +0800

    fix typo error

--- fe/src/main/java/org/apache/doris/qe/ShowExecutor.java
+++ fe/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -1461,7 +1461,7 @@ public class ShowExecutor {
                                                 dynamicPartitionScheduler.lastUpdateTime,
                                                 dynamicPartitionScheduler.lastSchedulerTime,
                                                 dynamicPartitionScheduler.dynamicPartitionState.toString(),
-                                                dynamicPartitionScheduler.Msg));
+                                                dynamicPartitionScheduler.msg));
                 }
             } finally {
                 db.readUnlock();
