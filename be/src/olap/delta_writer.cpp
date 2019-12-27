1421a9be414312f56c8dbb012fc75861b0327532
Mingyu Chen <morningman.cmy@gmail.com>
2019-12-27 10:08:41 +0800

    [Compaction] Support compact only one rowset (#2558)
    Support compaction operation to compact only one rowset.
    After the modification, the last rowset of the tablet will
    also be compacted.
    
    At the same time, we added a `segments_overlap_pb` field to
    the rowset meta. Used to describe whether the segment data
    in the rowset overlaps. This field is set by `rowset_writer`.
    Initially UNKNOWN for compatibility with existing data.
    
    In addition, the version hash of the rowset generated after
    compaction is directly set to the version hash of last rowset
    participating in compaction, to ensure that the tablet's
    version hash remains unchanged after compaction.

--- be/src/olap/delta_writer.cpp
+++ be/src/olap/delta_writer.cpp
@@ -136,6 +136,7 @@ OLAPStatus DeltaWriter::init() {
     writer_context.rowset_state = PREPARED;
     writer_context.txn_id = _req.txn_id;
     writer_context.load_id = _req.load_id;
+    writer_context.segments_overlap = OVERLAPPING;
     RETURN_NOT_OK(RowsetFactory::create_rowset_writer(writer_context, &_rowset_writer));
 
     _tablet_schema = &(_tablet->tablet_schema());
