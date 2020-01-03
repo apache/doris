fcfe2dc10f2d5b8575819b000a40639b22c1b1f0
liulijia <liutang123@yeah.net>
2020-01-03 18:58:48 +0800

    use doris << in for page test.

--- be/test/olap/rowset/segment_v2/frame_of_reference_page_test.cpp
+++ be/test/olap/rowset/segment_v2/frame_of_reference_page_test.cpp
@@ -28,29 +28,7 @@
 
 using doris::segment_v2::PageBuilderOptions;
 using doris::segment_v2::PageDecoderOptions;
-std::ostream& operator<<( std::ostream& dest, doris::int128_t value) {
-    std::ostream::sentry s( dest );
-    if ( s ) {
-        __uint128_t tmp = value < 0 ? -value : value;
-        char buffer[ 128 ];
-        char* d = std::end( buffer );
-        do
-        {
-            -- d;
-            *d = "0123456789"[ tmp % 10 ];
-            tmp /= 10;
-        } while ( tmp != 0 );
-        if ( value < 0 ) {
-            -- d;
-            *d = '-';
-        }
-        int len = std::end( buffer ) - d;
-        if ( dest.rdbuf()->sputn( d, len ) != len ) {
-            dest.setstate( std::ios_base::badbit );
-        }
-    }
-    return dest;
-}
+using doris::operator<<;
 
 namespace doris {
 class FrameOfReferencePageTest : public testing::Test {
