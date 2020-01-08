f3f2ad1894548b3c9b4970c6222bb134a3af37f6
IanWatsons <neoyoho@gmail.com>
2020-01-08 20:35:14 +0800

    fix code style

--- samples/connect/cpp/doris_client.cpp
+++ samples/connect/cpp/doris_client.cpp
@@ -16,14 +16,16 @@
 // under the License.
 
 /*************************************************************************
+> How to use:
+    1. g++ doris_client.cpp -o doris_client `mysql_config --cflags --libs`
+    2. ./doris_client
 
-This is a cpp demo for doris connection. You should install mysql prior to
-running this demo.
+> What can this demo do:
+    This is a cpp demo for doris client, you can test basic function such as
+    connection, CRUD of your doris. You should install mysql prior to running
+    this demo.
 
-> Useage:
-        1. g++ doris_client.cpp -o doris_client `mysql_config --cflags --libs`
-        2. ./doris_client
-> Comment: Supported mysql version: 5.6, 5.7, 8.0
+> Supported mysql version: 5.6, 5.7, ..., 8.0
 ************************************************************************/
 
 #include "doris_client.h"
