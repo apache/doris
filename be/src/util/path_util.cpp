42dadb44552491b9c3bcf9562c2ce4d108d302dc
LingBin <lingbinlb@gmail.com>
2020-01-13 20:18:10 +0800

    Add path util
    Note that the methods in path_util are only related to path processing,
    and do not involve any file and IO operations
    
    The upcoming patch will use these util methods, used to extract operations
    such as concatenation of directory strings from processing logic.

---
+++ be/src/util/path_util.cpp
@@ -0,0 +1,88 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "util/path_util.h"
+
+// Use the POSIX version of dirname(3). See `man 3 dirname`
+#include <libgen.h>
+
+#include <cstring>
+
+#include <common/logging.h>
+#include "gutil/strings/split.h"
+#include "gutil/strings/stringpiece.h"
+#include "gutil/strings/strip.h"
+#include "env/env.h"
+#include "common/status.h"
+
+using std::string;
+using std::vector;
+using strings::SkipEmpty;
+using strings::Split;
+
+namespace doris {
+namespace path_util {
+
+const string kTmpInfix = ".doristmp";
+
+string join_path_segments(const string& a, const string& b) {
+    CHECK(!a.empty()) << "empty first component: " << a;
+    CHECK(!b.empty() && b[0] != '/')
+            << "second path component must be non-empty and relative: "
+            << b;
+    if (a.back() == '/') {
+        return a + b;
+    } else {
+        return a + "/" + b;
+    }
+}
+
+vector<string> join_path_segments_v(const vector<string>& v, const string& s) {
+    vector<string> out;
+    for (const string& path : v) {
+        out.emplace_back(join_path_segments(path, s));
+    }
+    return out;
+}
+
+vector<string> split_path(const string& path) {
+    if (path.empty()) {
+        return {};
+    }
+    vector<string> segments;
+    if (path[0] == '/') {
+        segments.emplace_back("/");
+    }
+    vector<StringPiece> pieces = Split(path, "/", SkipEmpty());
+    for (const StringPiece& piece : pieces) {
+        segments.emplace_back(piece.data(), piece.size());
+    }
+    return segments;
+}
+
+string dir_name(const string& path) {
+    std::unique_ptr<char[]> path_copy(strdup(path.c_str()));
+    return dirname(path_copy.get());
+}
+
+string base_name(const string& path) {
+    std::unique_ptr<char[]> path_copy(strdup(path.c_str()));
+    return basename(path_copy.get());
+}
+
+} // namespace path_util
+} // namespace doris
--- run-ut.sh
+++ run-ut.sh
@@ -166,6 +166,7 @@ ${DORIS_TEST_BINARY_DIR}/util/frame_of_reference_coding_test
 ${DORIS_TEST_BINARY_DIR}/util/zip_util_test
 ${DORIS_TEST_BINARY_DIR}/util/utf8_check_test
 ${DORIS_TEST_BINARY_DIR}/util/cgroup_util_test
+${DORIS_TEST_BINARY_DIR}/util/path_util_test
 
 # Running common Unittest
 ${DORIS_TEST_BINARY_DIR}/common/resource_tls_test
