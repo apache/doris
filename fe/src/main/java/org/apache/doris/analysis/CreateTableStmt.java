df75ebe304a90a5d31c6033bda99dd1af9152594
wangbo <506340561@qq.com>
2020-01-16 18:54:27 +0800

    fix code style

--- fe/src/main/java/org/apache/doris/analysis/CreateTableStmt.java
+++ fe/src/main/java/org/apache/doris/analysis/CreateTableStmt.java
@@ -51,7 +51,6 @@ import org.apache.logging.log4j.Logger;
 import java.io.DataInput;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
