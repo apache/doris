4d34883ebe73f0f30a4fd0fa053bbf75b487b467
WangCong <1018957763@qq.com>
2020-03-27 12:48:05 +0800

    fix

--- fe/src/test/java/org/apache/doris/utframe/DorisAssert.java
+++ fe/src/test/java/org/apache/doris/utframe/DorisAssert.java
@@ -133,7 +133,7 @@ public class DorisAssert {
         }
 
         public void explainContains(String keywords, int count) throws Exception {
-            Assert.assertTrue(StringUtils.countMatches(explainQuery(), keywords) == count);
+            Assert.assertEquals(StringUtils.countMatches(explainQuery(), keywords), count);
         }
 
         public void explainWithout(String s) throws Exception {
