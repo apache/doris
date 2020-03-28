57571e2561ea6d4335205da8100b934ea60fe4fa
Yingchun Lai <405403881@qq.com>
2020-03-28 16:26:38 +0800

    [web] Dump configs on BE's website '/varz' (#3220)
    Dump configs on BE's website '/varz'
    Change NAVIGATION_BAR_PREFIX from 'Impala' to 'Doris'
    Format the related files by clang-format

--- be/src/common/configbase.h
+++ be/src/common/configbase.h
@@ -19,6 +19,7 @@
 #define DORIS_BE_SRC_COMMON_CONFIGBASE_H
 
 #include <stdint.h>
+
 #include <list>
 #include <map>
 #include <vector>
@@ -33,11 +34,8 @@ public:
         const char* name;
         void* storage;
         const char* defval;
-        Field(const char* ftype, const char* fname, void* fstorage, const char* fdefval) :
-            type(ftype), 
-            name(fname),
-            storage(fstorage),
-            defval(fdefval) {}
+        Field(const char* ftype, const char* fname, void* fstorage, const char* fdefval)
+                : type(ftype), name(fname), storage(fstorage), defval(fdefval) {}
     };
 
 public:
@@ -51,53 +49,53 @@ public:
         Field field(ftype, fname, fstorage, fdefval);
         _s_fieldlist->push_back(field);
     }
-}; 
+};
 
-#define DEFINE_FIELD(FIELD_TYPE, FIELD_NAME, FIELD_DEFAULT)\
-    FIELD_TYPE FIELD_NAME;\
+#define DEFINE_FIELD(FIELD_TYPE, FIELD_NAME, FIELD_DEFAULT) \
+    FIELD_TYPE FIELD_NAME;                                  \
     static Register reg_##FIELD_NAME(#FIELD_TYPE, #FIELD_NAME, &FIELD_NAME, FIELD_DEFAULT);
 
-#define DECLARE_FIELD(FIELD_TYPE, FIELD_NAME)   extern FIELD_TYPE FIELD_NAME;
+#define DECLARE_FIELD(FIELD_TYPE, FIELD_NAME) extern FIELD_TYPE FIELD_NAME;
 
-#ifdef __IN_CONFIGBASE_CPP__ 
-#define CONF_Bool(name, defaultstr)         DEFINE_FIELD(bool, name, defaultstr)
-#define CONF_Int16(name, defaultstr)        DEFINE_FIELD(int16_t, name, defaultstr)
-#define CONF_Int32(name, defaultstr)        DEFINE_FIELD(int32_t, name, defaultstr)
-#define CONF_Int64(name, defaultstr)        DEFINE_FIELD(int64_t, name, defaultstr)
-#define CONF_Double(name, defaultstr)       DEFINE_FIELD(double, name, defaultstr)
-#define CONF_String(name, defaultstr)       DEFINE_FIELD(std::string, name, defaultstr)
-#define CONF_Bools(name, defaultstr)        DEFINE_FIELD(std::vector<bool>, name, defaultstr)
-#define CONF_Int16s(name, defaultstr)       DEFINE_FIELD(std::vector<int16_t>, name, defaultstr)
-#define CONF_Int32s(name, defaultstr)       DEFINE_FIELD(std::vector<int32_t>, name, defaultstr)
-#define CONF_Int64s(name, defaultstr)       DEFINE_FIELD(std::vector<int64_t>, name, defaultstr)
-#define CONF_Doubles(name, defaultstr)      DEFINE_FIELD(std::vector<double>, name, defaultstr)
-#define CONF_Strings(name, defaultstr)      DEFINE_FIELD(std::vector<std::string>, name, defaultstr)
+#ifdef __IN_CONFIGBASE_CPP__
+#define CONF_Bool(name, defaultstr) DEFINE_FIELD(bool, name, defaultstr)
+#define CONF_Int16(name, defaultstr) DEFINE_FIELD(int16_t, name, defaultstr)
+#define CONF_Int32(name, defaultstr) DEFINE_FIELD(int32_t, name, defaultstr)
+#define CONF_Int64(name, defaultstr) DEFINE_FIELD(int64_t, name, defaultstr)
+#define CONF_Double(name, defaultstr) DEFINE_FIELD(double, name, defaultstr)
+#define CONF_String(name, defaultstr) DEFINE_FIELD(std::string, name, defaultstr)
+#define CONF_Bools(name, defaultstr) DEFINE_FIELD(std::vector<bool>, name, defaultstr)
+#define CONF_Int16s(name, defaultstr) DEFINE_FIELD(std::vector<int16_t>, name, defaultstr)
+#define CONF_Int32s(name, defaultstr) DEFINE_FIELD(std::vector<int32_t>, name, defaultstr)
+#define CONF_Int64s(name, defaultstr) DEFINE_FIELD(std::vector<int64_t>, name, defaultstr)
+#define CONF_Doubles(name, defaultstr) DEFINE_FIELD(std::vector<double>, name, defaultstr)
+#define CONF_Strings(name, defaultstr) DEFINE_FIELD(std::vector<std::string>, name, defaultstr)
 #else
-#define CONF_Bool(name, defaultstr)         DECLARE_FIELD(bool, name)
-#define CONF_Int16(name, defaultstr)        DECLARE_FIELD(int16_t, name)
-#define CONF_Int32(name, defaultstr)        DECLARE_FIELD(int32_t, name)
-#define CONF_Int64(name, defaultstr)        DECLARE_FIELD(int64_t, name)
-#define CONF_Double(name, defaultstr)       DECLARE_FIELD(double, name)
-#define CONF_String(name, defaultstr)       DECLARE_FIELD(std::string, name)
-#define CONF_Bools(name, defaultstr)        DECLARE_FIELD(std::vector<bool>, name)
-#define CONF_Int16s(name, defaultstr)       DECLARE_FIELD(std::vector<int16_t>, name)
-#define CONF_Int32s(name, defaultstr)       DECLARE_FIELD(std::vector<int32_t>, name)
-#define CONF_Int64s(name, defaultstr)       DECLARE_FIELD(std::vector<int64_t>, name)
-#define CONF_Doubles(name, defaultstr)      DECLARE_FIELD(std::vector<double>, name)
-#define CONF_Strings(name, defaultstr)      DECLARE_FIELD(std::vector<std::string>, name)
+#define CONF_Bool(name, defaultstr) DECLARE_FIELD(bool, name)
+#define CONF_Int16(name, defaultstr) DECLARE_FIELD(int16_t, name)
+#define CONF_Int32(name, defaultstr) DECLARE_FIELD(int32_t, name)
+#define CONF_Int64(name, defaultstr) DECLARE_FIELD(int64_t, name)
+#define CONF_Double(name, defaultstr) DECLARE_FIELD(double, name)
+#define CONF_String(name, defaultstr) DECLARE_FIELD(std::string, name)
+#define CONF_Bools(name, defaultstr) DECLARE_FIELD(std::vector<bool>, name)
+#define CONF_Int16s(name, defaultstr) DECLARE_FIELD(std::vector<int16_t>, name)
+#define CONF_Int32s(name, defaultstr) DECLARE_FIELD(std::vector<int32_t>, name)
+#define CONF_Int64s(name, defaultstr) DECLARE_FIELD(std::vector<int64_t>, name)
+#define CONF_Doubles(name, defaultstr) DECLARE_FIELD(std::vector<double>, name)
+#define CONF_Strings(name, defaultstr) DECLARE_FIELD(std::vector<std::string>, name)
 #endif
 
 class Properties {
 public:
     bool load(const char* filename);
-    template<typename T> 
+    template <typename T>
     bool get(const char* key, const char* defstr, T& retval) const;
     const std::map<std::string, std::string>& getmap() const;
 
 private:
-    template <typename T> 
+    template <typename T>
     static bool strtox(const std::string& valstr, std::vector<T>& retval);
-    template<typename T> 
+    template <typename T>
     static bool strtointeger(const std::string& valstr, T& retval);
     static bool strtox(const std::string& valstr, bool& retval);
     static bool strtox(const std::string& valstr, int16_t& retval);
