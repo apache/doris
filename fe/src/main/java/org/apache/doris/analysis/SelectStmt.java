77c891a1aa7625a5081134fa04ba03c746db74d7
emmymiao87 <522274284@qq.com>
2020-04-09 17:10:50 +0800

    Forbidden multi subquery in having clause Forbidden subquery in having clause when no aggregation in stmt Closed #3290

--- fe/src/main/java/org/apache/doris/analysis/SelectStmt.java
+++ fe/src/main/java/org/apache/doris/analysis/SelectStmt.java
@@ -965,6 +965,10 @@ public class SelectStmt extends QueryStmt {
                 Expr.isAggregatePredicate()))) {
             // We're not computing aggregates but we still need to register the HAVING
             // clause which could, e.g., contain a constant expression evaluating to false.
+            if (havingClauseAfterAnaylzed.contains(Subquery.class)) {
+                throw new AnalysisException("Only constant expr could be supported in having clause "
+                        + "when no aggregation in stmt");
+            }
             if (havingClauseAfterAnaylzed != null) {
                 analyzer.registerConjuncts(havingClauseAfterAnaylzed, true);
             }
