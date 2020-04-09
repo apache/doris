2f5dffd364272a9796b632b81f3f58ad8af0c470
caiconghui [蔡聪辉] <caiconghui@xiaomi.com>
2020-04-09 22:12:43 +0800

    fix config

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -493,7 +493,7 @@ namespace config {
 
     // max number of txns for every txn_partition_map in txn manager
     // this is a self protection to avoid too many txns saving in manager
-    CONF_Int64(max_runnings_transactions_per_txn_map, "100");
+    CONF_mInt64(max_runnings_transactions_per_txn_map, "100");
 
     // tablet_map_lock shard size, the value is 2^n, n=0,1,2,3,4
     // this is a an enhancement for better performance to manage tablet
