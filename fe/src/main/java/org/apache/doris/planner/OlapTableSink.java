483f971a48e7e4c22d60da33f8b60d43c5f9d398
WangCong <1018957763@qq.com>
2020-04-22 09:47:36 +0800

    fix

--- fe/src/main/java/org/apache/doris/planner/OlapTableSink.java
+++ fe/src/main/java/org/apache/doris/planner/OlapTableSink.java
@@ -118,7 +118,7 @@ public class OlapTableSink extends DataSink {
     }
 
     // must called after tupleDescriptor is computed
-    public void completeq() throws UserException {
+    public void complete() throws UserException {
         TOlapTableSink tSink = tDataSink.getOlap_table_sink();
 
         tSink.setTable_id(dstTable.getId());
