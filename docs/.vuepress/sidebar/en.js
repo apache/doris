0a9a084dbb4e0089f00cdcbf0676f6e365b6d443
fariel <huangfeng@ablecloud.cn>
2020-04-29 15:36:23 +0800

    fix spelling

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -426,7 +426,7 @@ module.exports = [
     children: ["debug-tool", "format-code"],
   },
   {
-    title: "Apache Commnity",
+    title: "Apache Community",
     directoryPath: "community/",
     children: [
       "gitter",
