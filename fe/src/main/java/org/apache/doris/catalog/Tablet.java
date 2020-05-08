6ca9bdffb50ba0917af8450f75707e5df1673cd0
wangcong18 <wangcong18@xiaomi.com>
2020-05-08 12:10:34 +0800

    fix

--- fe/src/main/java/org/apache/doris/catalog/Tablet.java
+++ fe/src/main/java/org/apache/doris/catalog/Tablet.java
@@ -39,7 +39,6 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
