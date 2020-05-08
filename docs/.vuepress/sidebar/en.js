77a9f3b9f647ad6733541941d4ac47b30318df26
Zhao Chun <zhaoc@apache.org>
2020-05-08 12:53:02 +0800

    [community] Add Committer Guide

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -437,6 +437,7 @@ module.exports = [
       "how-to-contribute",
       "members",
       "pull-request",
+      "committer-guide",
       "release-process",
       "subscribe-mail-list",
       "verify-apache-release",
