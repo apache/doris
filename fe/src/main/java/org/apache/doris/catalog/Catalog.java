53b39ceca64d78ad63463db3325ca5e7f3988236
wangcong18 <wangcong18@xiaomi.com>
2020-05-10 10:21:09 +0800

    fix

--- fe/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -6385,10 +6385,7 @@ public class Catalog {
 
     public void setConfig(AdminSetConfigStmt stmt) throws DdlException {
         Map<String, String> configs = stmt.getConfigs();
-        // configs can not be null
-        if (configs.size() != 1) {
-            throw new DdlException("config parameter size is not equal to 1");
-        }
+        Preconditions.checkState(configs.size() == 1);
 
         for (Map.Entry<String, String> entry : configs.entrySet()) {
             ConfigBase.setMutableConfig(entry.getKey(), entry.getValue());
