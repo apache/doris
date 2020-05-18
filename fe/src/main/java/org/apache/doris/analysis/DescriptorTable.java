271f25f0a4e98c3d9130c0772bc386e7786cbae4
Mingyu Chen <morningman.cmy@gmail.com>
2020-05-18 10:34:21 +0800

    [Bug] Fix bug that descriptor table is not reset before planning next routine load task (#3605)
    Before planning for next routine load task, the analyzer and descriptor table
    in it should be reset. Otherwise, a lot of historical objects will
    accumulate inside, causing memory leaks.

--- fe/src/main/java/org/apache/doris/analysis/DescriptorTable.java
+++ fe/src/main/java/org/apache/doris/analysis/DescriptorTable.java
@@ -41,17 +41,15 @@ import java.util.List;
 public class DescriptorTable {
     private final static Logger LOG = LogManager.getLogger(DescriptorTable.class);
 
-    private final HashMap<TupleId, TupleDescriptor> tupleDescs;
+    private final HashMap<TupleId, TupleDescriptor> tupleDescs = new HashMap<TupleId, TupleDescriptor>();
     // List of referenced tables with no associated TupleDescriptor to ship to the BE.
     // For example, the output table of an insert query.
-    private final List<Table>                       referencedTables;
+    private final List<Table> referencedTables = new ArrayList<Table>();;
     private final IdGenerator<TupleId> tupleIdGenerator_ = TupleId.createGenerator();
     private final IdGenerator<SlotId> slotIdGenerator_ = SlotId.createGenerator();
     private final HashMap<SlotId, SlotDescriptor> slotDescs = Maps.newHashMap();
 
     public DescriptorTable() {
-        tupleDescs = new HashMap<TupleId, TupleDescriptor>();
-        referencedTables = new ArrayList<Table>();
     }
 
     public TupleDescriptor createTupleDescriptor() {
