35ec2b3d4346b875189559b7b7f329a6fe8c4678
caiconghui [蔡聪辉] <caiconghui@xiaomi.com>
2020-05-25 11:02:39 +0800

    Add doc for thrift_client_timeout_ms config

--- fe/src/main/java/org/apache/doris/common/Config.java
+++ fe/src/main/java/org/apache/doris/common/Config.java
@@ -283,7 +283,7 @@ public class Config extends ConfigBase {
     @ConfField public static int http_backlog_num = 1024;
 
     /*
-     * The connection timeout and socket timeout setting for thrift server
+     * The connection timeout and socket timeout config for thrift server
      * The value for thrift_client_timeout_ms is set to be larger than zero to prevent
      * some hang up problems in java.net.SocketInputStream.socketRead0
      */
