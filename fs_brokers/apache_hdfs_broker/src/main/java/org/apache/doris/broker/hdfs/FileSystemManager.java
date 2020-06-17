800f6666e64421ddc7689c5959bf129de4609749
xy720 <xuyang25@baidu.com>
2020-06-17 10:48:14 +0800

    fix debug log

--- fs_brokers/apache_hdfs_broker/src/main/java/org/apache/doris/broker/hdfs/FileSystemManager.java
+++ fs_brokers/apache_hdfs_broker/src/main/java/org/apache/doris/broker/hdfs/FileSystemManager.java
@@ -562,7 +562,9 @@ public class FileSystemManager {
                     throw new BrokerException(TBrokerOperationStatusCode.END_OF_FILE,
                             "end of file reached");
                 }
-                logger.debug("read buffer from input stream, buffer size:" + buf.capacity() + ", read length:" + readLength);
+				if (logger.isDebugEnable()) {
+                    logger.debug("read buffer from input stream, buffer size:" + buf.capacity() + ", read length:" + readLength);
+				}
                 return buf;
             } catch (IOException e) {
                 logger.error("errors while read data from stream", e);
