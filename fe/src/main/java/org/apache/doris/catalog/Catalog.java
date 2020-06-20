apache/doris

commit 173dd3953da8ff929bf87f22f2befa291ea8833a

[Code Refactor] Remove Catalog.getInstance() method (#3784)
Use Catalog.getCurrentCatalog() instead, to avoid potential meta operation error.

fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -202,9 +202,8 @@ import org.apache.doris.task.PullLoadJobMgr;
 import org.apache.doris.thrift.TStorageFormat;
 import org.apache.doris.thrift.TStorageMedium;
 import org.apache.doris.thrift.TStorageType;
-import org.apache.doris.thrift.TTaskType;
 import org.apache.doris.thrift.TTabletType;
-
+import org.apache.doris.thrift.TTaskType;
 import org.apache.doris.transaction.GlobalTransactionMgr;
 import org.apache.doris.transaction.PublishVersionDaemon;
 
@@ -540,11 +539,6 @@ public class Catalog {
         if (CHECKPOINT != null) {
             CHECKPOINT = null;
         }
-    }
-
-    // use this to get real Catalog instance
-    public static Catalog getInstance() {
-        return SingletonHolder.INSTANCE;
     }
 
     // use this to get CheckPoint Catalog instance
@@ -3601,7 +3595,7 @@ public class Catalog {
         TableIndexes indexes = new TableIndexes(stmt.getIndexes());
 
         // create table
-        long tableId = Catalog.getInstance().getNextId();
+        long tableId = Catalog.getCurrentCatalog().getNextId();
         OlapTable olapTable = new OlapTable(tableId, tableName, baseSchema, keysType, partitionInfo,
                 distributionInfo, indexes);
         olapTable.setComment(stmt.getComment());
@@ -3855,7 +3849,7 @@ public class Catalog {
 
         List<Column> columns = stmt.getColumns();
 
-        long tableId = Catalog.getInstance().getNextId();
+        long tableId = Catalog.getCurrentCatalog().getNextId();
         MysqlTable mysqlTable = new MysqlTable(tableId, tableName, columns, stmt.getProperties());
         mysqlTable.setComment(stmt.getComment());
         if (!db.createTableWithLock(mysqlTable, false, stmt.isSetIfNotExists())) {
@@ -3885,7 +3879,7 @@ public class Catalog {
             partitionInfo = new SinglePartitionInfo();
         }
 
-        long tableId = Catalog.getInstance().getNextId();
+        long tableId = Catalog.getCurrentCatalog().getNextId();
         EsTable esTable = new EsTable(tableId, tableName, baseSchema, stmt.getProperties(), partitionInfo);
         esTable.setComment(stmt.getComment());
 
@@ -3901,7 +3895,7 @@ public class Catalog {
 
         List<Column> columns = stmt.getColumns();
 
-        long tableId = Catalog.getInstance().getNextId();
+        long tableId = Catalog.getCurrentCatalog().getNextId();
         BrokerTable brokerTable = new BrokerTable(tableId, tableName, columns, stmt.getProperties());
         brokerTable.setComment(stmt.getComment());
         brokerTable.setBrokerProperties(stmt.getExtProperties());
@@ -4206,7 +4200,7 @@ public class Catalog {
             GroupId groupId = null;
             if (colocateIndex.isColocateTable(tabletMeta.getTableId())) {
                 // if this is a colocate table, try to get backend seqs from colocation index.
-                Database db = Catalog.getInstance().getDb(tabletMeta.getDbId());
+                Database db = Catalog.getCurrentCatalog().getDb(tabletMeta.getDbId());
                 groupId = colocateIndex.getGroup(tabletMeta.getTableId());
                 // Use db write lock here to make sure the backendsPerBucketSeq is consistent when the backendsPerBucketSeq is updating.
                 // This lock will release very fast.
@@ -5428,7 +5422,7 @@ public class Catalog {
 
         List<Column> columns = stmt.getColumns();
 
-        long tableId = Catalog.getInstance().getNextId();
+        long tableId = Catalog.getCurrentCatalog().getNextId();
         View newView = new View(tableId, tableName, columns);
         newView.setComment(stmt.getComment());
         newView.setInlineViewDefWithSqlMode(stmt.getInlineViewDef(),
@@ -6017,8 +6011,8 @@ public class Catalog {
                 InfoSchemaDb db;
                 // Use real Catalog instance to avoid InfoSchemaDb id continuously increment
                 // when checkpoint thread load image.
-                if (Catalog.getInstance().getFullNameToDb().containsKey(dbName)) {
-                    db = (InfoSchemaDb)Catalog.getInstance().getFullNameToDb().get(dbName);
+                if (Catalog.getCurrentCatalog().getFullNameToDb().containsKey(dbName)) {
+                    db = (InfoSchemaDb)Catalog.getCurrentCatalog().getFullNameToDb().get(dbName);
                 } else {
                     db = new InfoSchemaDb(cluster.getName());
                     db.setClusterName(cluster.getName());

fe/src/main/java/org/apache/doris/common/util/MasterDaemon.java
@@ -42,7 +42,7 @@ public class MasterDaemon extends Daemon {
 
     @Override
     protected final void runOneCycle() {
-        while (!Catalog.getInstance().isReady()) {
+        while (!Catalog.getCurrentCatalog().isReady()) {
             // here we use getInstance(), not getCurrentCatalog() because we truly want the Catalog instance,
             // not the Checkpoint catalog instance.
             // and if catalog is not ready, do not run

fe/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -83,7 +83,7 @@ public class BDBJEJournal implements Journal {
     private void initBDBEnv(String nodeName) {
         environmentPath = Catalog.getCurrentCatalog().getBdbDir();
         try {
-            Pair<String, Integer> selfNode = Catalog.getInstance().getSelfNode();
+            Pair<String, Integer> selfNode = Catalog.getCurrentCatalog().getSelfNode();
             if (isPortUsing(selfNode.first, selfNode.second)) {
                 LOG.error("edit_log_port {} is already in use. will exit.", selfNode.second);
                 System.exit(-1);
@@ -310,11 +310,11 @@ public class BDBJEJournal implements Journal {
         if (bdbEnvironment == null) {
             File dbEnv = new File(environmentPath);
             bdbEnvironment = new BDBEnvironment();
-            Pair<String, Integer> helperNode = Catalog.getInstance().getHelperNode();
+            Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
             String helperHostPort = helperNode.first + ":" + helperNode.second;
             try {
                 bdbEnvironment.setup(dbEnv, selfNodeName, selfNodeHostPort,
-                                     helperHostPort, Catalog.getInstance().isElectable());
+                                     helperHostPort, Catalog.getCurrentCatalog().isElectable());
             } catch (Exception e) {
                 LOG.error("catch an exception when setup bdb environment. will exit.", e);
                 System.exit(-1);
@@ -322,7 +322,7 @@ public class BDBJEJournal implements Journal {
         }
         
         // Open a new journal database or get last existing one as current journal database
-        Pair<String, Integer> helperNode = Catalog.getInstance().getHelperNode();
+        Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
         List<Long> dbNames = null;
         for (int i = 0; i < RETRY_TIME; i++) {
             try {
@@ -339,7 +339,7 @@ public class BDBJEJournal implements Journal {
                      *  here we should open database with name image max journal id + 1.
                      *  (default Catalog.getInstance().getReplayedJournalId() is 0)
                      */
-                    String dbName = Long.toString(Catalog.getInstance().getReplayedJournalId() + 1);
+                    String dbName = Long.toString(Catalog.getCurrentCatalog().getReplayedJournalId() + 1);
                     LOG.info("the very first time to open bdb, dbname is {}", dbName);
                     currentJournalDB = bdbEnvironment.openDatabase(dbName);
                 } else {
@@ -360,7 +360,7 @@ public class BDBJEJournal implements Journal {
                 restore.execute(insufficientLogEx, config);
                 bdbEnvironment.close();
                 bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort, 
-                                     helperNode.first + ":" + helperNode.second, Catalog.getInstance().isElectable());
+                                     helperNode.first + ":" + helperNode.second, Catalog.getCurrentCatalog().isElectable());
             }
         }
     }

fe/src/main/java/org/apache/doris/master/Checkpoint.java
@@ -124,14 +124,14 @@ public class Checkpoint extends MasterDaemon {
         
         // push image file to all the other non master nodes
         // DO NOT get other nodes from HaProtocol, because node may not in bdbje replication group yet.
-        List<Frontend> allFrontends = Catalog.getInstance().getFrontends(null);
+        List<Frontend> allFrontends = Catalog.getCurrentCatalog().getFrontends(null);
         int successPushed = 0;
         int otherNodesCount = 0;
         if (!allFrontends.isEmpty()) {
             otherNodesCount = allFrontends.size() - 1; // skip master itself
             for (Frontend fe : allFrontends) {
                 String host = fe.getHost();
-                if (host.equals(Catalog.getInstance().getMasterIp())) {
+                if (host.equals(Catalog.getCurrentCatalog().getMasterIp())) {
                     // skip master itself
                     continue;
                 }
@@ -160,7 +160,7 @@ public class Checkpoint extends MasterDaemon {
             if (successPushed > 0) {
                 for (Frontend fe : allFrontends) {
                     String host = fe.getHost();
-                    if (host.equals(Catalog.getInstance().getMasterIp())) {
+                    if (host.equals(Catalog.getCurrentCatalog().getMasterIp())) {
                         // skip master itself
                         continue;
                     }

fe/src/test/java/org/apache/doris/alter/RollupJobV2Test.java
@@ -155,7 +155,7 @@ public class RollupJobV2Test {
         fakeCatalog = new FakeCatalog();
         fakeEditLog = new FakeEditLog();
         FakeCatalog.setCatalog(masterCatalog);
-        MaterializedViewHandler materializedViewHandler = Catalog.getInstance().getRollupHandler();
+        MaterializedViewHandler materializedViewHandler = Catalog.getCurrentCatalog().getRollupHandler();
         ArrayList<AlterClause> alterClauses = new ArrayList<>();
         alterClauses.add(clause);
         alterClauses.add(clause2);
@@ -176,7 +176,7 @@ public class RollupJobV2Test {
         fakeCatalog = new FakeCatalog();
         fakeEditLog = new FakeEditLog();
         FakeCatalog.setCatalog(masterCatalog);
-        MaterializedViewHandler materializedViewHandler = Catalog.getInstance().getRollupHandler();
+        MaterializedViewHandler materializedViewHandler = Catalog.getCurrentCatalog().getRollupHandler();
         ArrayList<AlterClause> alterClauses = new ArrayList<>();
         alterClauses.add(clause);
         Database db = masterCatalog.getDb(CatalogTestUtil.testDbId1);
@@ -193,7 +193,7 @@ public class RollupJobV2Test {
         fakeCatalog = new FakeCatalog();
         fakeEditLog = new FakeEditLog();
         FakeCatalog.setCatalog(masterCatalog);
-        MaterializedViewHandler materializedViewHandler = Catalog.getInstance().getRollupHandler();
+        MaterializedViewHandler materializedViewHandler = Catalog.getCurrentCatalog().getRollupHandler();
 
         // add a rollup job
         ArrayList<AlterClause> alterClauses = new ArrayList<>();
@@ -297,7 +297,7 @@ public class RollupJobV2Test {
         fakeCatalog = new FakeCatalog();
         fakeEditLog = new FakeEditLog();
         FakeCatalog.setCatalog(masterCatalog);
-        MaterializedViewHandler materializedViewHandler = Catalog.getInstance().getRollupHandler();
+        MaterializedViewHandler materializedViewHandler = Catalog.getCurrentCatalog().getRollupHandler();
 
         // add a rollup job
         ArrayList<AlterClause> alterClauses = new ArrayList<>();

fe/src/test/java/org/apache/doris/qe/ShowExecutorTest.java
@@ -191,7 +191,7 @@ public class ShowExecutorTest {
                 minTimes = 0;
                 result = auth;
 
-                Catalog.getInstance();
+                Catalog.getCurrentCatalog();
                 minTimes = 0;
                 result = catalog;
 

commit 0555d7ba906ec84593f8db5ea860697f32b7f538

[Bug] Checkpoint thread is not running (#3913)
This bug is introduced by PR #3784 
In #3784, I remove the `Catalog.getInstance()`, and use `Catalog.getCurrentCatalog()` instead.

But actually, there are some place that should use the serving catalog explicitly.

Mainly changed:

1. Add a new method `getServingCatalog()` to explicitly return the real catalog instance.
2. Fix a compile bug of broker introduced by #3881

fe/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -554,6 +554,12 @@ public class Catalog {
         }
     }
 
+    // NOTICE: in most case, we should use getCurrentCatalog() to get the right catalog.
+    // but in some cases, we should get the serving catalog explicitly.
+    public static Catalog getServingCatalog() {
+        return SingletonHolder.INSTANCE;
+    }
+
     public PullLoadJobMgr getPullLoadJobMgr() {
         return pullLoadJobMgr;
     }

fe/src/main/java/org/apache/doris/common/util/MasterDaemon.java
@@ -42,8 +42,8 @@ public class MasterDaemon extends Daemon {
 
     @Override
     protected final void runOneCycle() {
-        while (!Catalog.getCurrentCatalog().isReady()) {
-            // here we use getInstance(), not getCurrentCatalog() because we truly want the Catalog instance,
+        while (!Catalog.getServingCatalog().isReady()) {
+            // here we use getServingCatalog(), not getCurrentCatalog() because we truly want the Catalog instance,
             // not the Checkpoint catalog instance.
             // and if catalog is not ready, do not run
             try {

fe/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -337,7 +337,7 @@ public class BDBJEJournal implements Journal {
                      *  This is the very first time to open. Usually, we will open a new database named "1".
                      *  But when we start cluster with an image file copied from other cluster, 
                      *  here we should open database with name image max journal id + 1.
-                     *  (default Catalog.getInstance().getReplayedJournalId() is 0)
+                     *  (default Catalog.getCurrentCatalog().getReplayedJournalId() is 0)
                      */
                     String dbName = Long.toString(Catalog.getCurrentCatalog().getReplayedJournalId() + 1);
                     LOG.info("the very first time to open bdb, dbname is {}", dbName);

fe/src/main/java/org/apache/doris/master/Checkpoint.java
@@ -55,7 +55,7 @@ public class Checkpoint extends MasterDaemon {
 
     public Checkpoint(EditLog editLog) {
         super("leaderCheckpointer", FeConstants.checkpoint_interval_second * 1000L);
-        this.imageDir = Catalog.getCurrentCatalog().getImageDir();
+        this.imageDir = Catalog.getServingCatalog().getImageDir();
         this.editLog = editLog;
     }
 
@@ -124,14 +124,14 @@ public class Checkpoint extends MasterDaemon {
         
         // push image file to all the other non master nodes
         // DO NOT get other nodes from HaProtocol, because node may not in bdbje replication group yet.
-        List<Frontend> allFrontends = Catalog.getCurrentCatalog().getFrontends(null);
+        List<Frontend> allFrontends = Catalog.getServingCatalog().getFrontends(null);
         int successPushed = 0;
         int otherNodesCount = 0;
         if (!allFrontends.isEmpty()) {
             otherNodesCount = allFrontends.size() - 1; // skip master itself
             for (Frontend fe : allFrontends) {
                 String host = fe.getHost();
-                if (host.equals(Catalog.getCurrentCatalog().getMasterIp())) {
+                if (host.equals(Catalog.getServingCatalog().getMasterIp())) {
                     // skip master itself
                     continue;
                 }
@@ -160,7 +160,7 @@ public class Checkpoint extends MasterDaemon {
             if (successPushed > 0) {
                 for (Frontend fe : allFrontends) {
                     String host = fe.getHost();
-                    if (host.equals(Catalog.getCurrentCatalog().getMasterIp())) {
+                    if (host.equals(Catalog.getServingCatalog().getMasterIp())) {
                         // skip master itself
                         continue;
                     }

fs_brokers/apache_hdfs_broker/src/main/java/org/apache/doris/broker/hdfs/FileSystemManager.java
@@ -562,7 +562,7 @@ public class FileSystemManager {
                     throw new BrokerException(TBrokerOperationStatusCode.END_OF_FILE,
                             "end of file reached");
                 }
-                if (logger.isDebugEnable()) {
+                if (logger.isDebugEnabled()) {
                     logger.debug("read buffer from input stream, buffer size:" + buf.capacity() + ", read length:" + readLength);
                 }
                 return buf;

fe/src/test/java/org/apache/doris/alter/RollupJobV2Test.java
@@ -134,10 +134,6 @@ public class RollupJobV2Test {
 
         new MockUp<Catalog>() {
             @Mock
-            public Catalog getInstance() {
-                return masterCatalog;
-            }
-            @Mock
             public Catalog getCurrentCatalog() {
                 return masterCatalog;
             }

fe/src/test/java/org/apache/doris/analysis/DescribeStmtTest.java
@@ -17,11 +17,7 @@
 
 package org.apache.doris.analysis;
 
-import mockit.Expectations;
-import mockit.Mock;
-import mockit.MockUp;
 import org.apache.doris.catalog.Catalog;
-import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.qe.ConnectContext;
 
@@ -29,6 +25,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import mockit.Mock;
+import mockit.MockUp;
 
 public class DescribeStmtTest {
     private Analyzer analyzer;
@@ -52,10 +51,6 @@ public class DescribeStmtTest {
         };
 
         new MockUp<Catalog>() {
-            @Mock
-            Catalog getInstance() {
-                return catalog;
-            }
             @Mock
             Catalog getCurrentCatalog() {
                 return catalog;

fe/src/test/java/org/apache/doris/http/DorisHttpTestCase.java
@@ -316,10 +316,6 @@ abstract public class DorisHttpTestCase {
                 return new MaterializedViewHandler();
             }
             @Mock
-            Catalog getInstance() {
-                return catalog;
-            }
-            @Mock
             Catalog getCurrentCatalog() {
                 return catalog;
             }

fe/src/test/java/org/apache/doris/qe/ShowExecutorTest.java
@@ -17,9 +17,6 @@
 
 package org.apache.doris.qe;
 
-import mockit.Expectations;
-import mockit.Mock;
-import mockit.MockUp;
 import org.apache.doris.analysis.AccessTestUtil;
 import org.apache.doris.analysis.Analyzer;
 import org.apache.doris.analysis.DescribeStmt;
@@ -68,6 +65,10 @@ import org.junit.rules.ExpectedException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 
 public class ShowExecutorTest {
     private ConnectContext ctx;
@@ -295,10 +296,6 @@ public class ShowExecutorTest {
         Catalog catalog = AccessTestUtil.fetchAdminCatalog();
 
         new MockUp<Catalog>() {
-            @Mock
-            Catalog getInstance() {
-                return catalog;
-            }
             @Mock
             Catalog getCurrentCatalog() {
                 return catalog;