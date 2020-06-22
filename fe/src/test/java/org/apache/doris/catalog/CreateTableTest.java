32060e69acfb254a81fbb97e6e49b0a68294e907
WangCong <1018957763@qq.com>
2020-06-22 23:26:06 +0800

    fix conflict

--- fe/src/test/java/org/apache/doris/catalog/CreateTableTest.java
+++ fe/src/test/java/org/apache/doris/catalog/CreateTableTest.java
@@ -19,34 +19,28 @@ package org.apache.doris.catalog;
 
 import org.apache.doris.analysis.CreateDbStmt;
 import org.apache.doris.analysis.CreateTableStmt;
+import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ConfigBase;
 import org.apache.doris.common.DdlException;
-import org.apache.doris.common.FeConstants;
+import org.apache.doris.common.ExceptionChecker;
 import org.apache.doris.qe.ConnectContext;
-
 import org.apache.doris.utframe.UtFrameUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
 import java.util.UUID;
 
 public class CreateTableTest {
-
-    private static String runningDir = "fe/mocked/CreateTableTest/" + UUID.randomUUID().toString() + "/";
+    private static String runningDir = "fe/mocked/CreateTableTest2/" + UUID.randomUUID().toString() + "/";
 
     private static ConnectContext connectContext;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
     @BeforeClass
     public static void beforeClass() throws Exception {
-        FeConstants.default_scheduler_interval_millisecond = 1000;
-        FeConstants.runningUnitTest = true;
-
         UtFrameUtils.createMinDorisCluster(runningDir);
 
         // create connect context
@@ -58,8 +52,9 @@ public class CreateTableTest {
     }
 
     @AfterClass
-    public static void TearDown() {
-        UtFrameUtils.cleanDorisFeDir(runningDir);
+    public static void tearDown() {
+        File file = new File(runningDir);
+        file.delete();
     }
 
     private static void createTable(String sql) throws Exception {
@@ -68,65 +63,101 @@ public class CreateTableTest {
     }
 
     @Test
-    public void testNormalOlap() throws Exception {
-        String createOlapTblStmt = "create table test.tb1(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1');";
-        createTable(createOlapTblStmt);
-    }
+    public void testNormal() throws DdlException {
+        ExceptionChecker.expectThrowsNoException(
+                () -> createTable("create table test.tbl1\n" + "(k1 int, k2 int)\n" + "duplicate key(k1)\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
 
-    @Test
-    public void testUnknownDatabase() throws Exception {
-        String createOlapTblStmt = "create table testDb.tb2(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1');";
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Unknown database 'default_cluster:testDb'");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker.expectThrowsNoException(() -> createTable("create table test.tbl2\n" + "(k1 int, k2 int)\n"
+                + "duplicate key(k1)\n" + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
 
-    @Test
-    public void testShortKeyTooLarge() throws Exception {
-        String createOlapTblStmt = "create table test.tb3(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1', 'short_key' = '3');";
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Short key is too large. should less than: 2");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker.expectThrowsNoException(
+                () -> createTable("create table test.tbl3\n" + "(k1 varchar(40), k2 int)\n" + "duplicate key(k1)\n"
+                        + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1');"));
 
-    @Test
-    public void testShortKeyVarcharMiddle() throws Exception {
-        String createOlapTblStmt = "create table test.tb4(key1 varchar(10), key2 int) distributed by hash(key1) buckets 1 properties('replication_num' = '1', 'short_key' = '2');";
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Varchar should not in the middle of short keys.");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker.expectThrowsNoException(
+                () -> createTable("create table test.tbl4\n" + "(k1 varchar(40), k2 int, v1 int sum)\n"
+                        + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k1) buckets 1\n" + "properties('replication_num' = '1');"));
 
-    @Test
-    public void testNotEnoughBackend() throws Exception {
-        String createOlapTblStmt = "create table test.tb5(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '3');";
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Failed to find enough host with storage medium is HDD. need: 3");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker.expectThrowsNoException(() -> createTable(
+                "create table test.tbl5\n" + "(k1 varchar(40), k2 int, v1 int sum)\n" + "aggregate key(k1,k2)\n"
+                        + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k1) buckets 1\n" + "properties('replication_num' = '1');"));
 
-    @Test
-    public void testOlapTableExists() throws Exception {
-        String createOlapTblStmt = "create table test.tb6(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1');";
-        createTable(createOlapTblStmt);
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Table 'tb6' already exists");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker.expectThrowsNoException(() -> createTable(
+                "create table test.tbl6\n" + "(k1 varchar(40), k2 int, k3 int)\n" + "duplicate key(k1, k2, k3)\n"
+                        + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k1) buckets 1\n" + "properties('replication_num' = '1');"));
 
-    @Test
-    public void testTableNotFoundStorageMedium() throws Exception {
-        ConfigBase.setMutableConfig("enable_strict_storage_medium_check", "true");
-        String createOlapTblStmt = "create table test.tb7(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1', 'storage_medium' = 'ssd');";
-        expectedException.expect(DdlException.class);
-        expectedException.expectMessage("errCode = 2, detailMessage = Failed to find enough host with storage medium is SSD in all backends. need: 1");
-        createTable(createOlapTblStmt);
-    }
+        ExceptionChecker
+                .expectThrowsNoException(() -> createTable("create table test.tbl7\n" + "(k1 varchar(40), k2 int)\n"
+                        + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1');"));
 
-    @Test
-    public void testNotSetStorageMediumCheck() throws Exception {
         ConfigBase.setMutableConfig("enable_strict_storage_medium_check", "false");
-        String createOlapTblStmt = "create table test.tb7(key1 int, key2 varchar(10)) distributed by hash(key1) buckets 1 properties('replication_num' = '1', 'storage_medium' = 'ssd');";
-        createTable(createOlapTblStmt);
+        ExceptionChecker
+                .expectThrowsNoException(() -> createTable("create table test.tb7(key1 int, key2 varchar(10)) \n"
+                        + "distributed by hash(key1) buckets 1 properties('replication_num' = '1', 'storage_medium' = 'ssd');"));
+
+        Database db = Catalog.getCurrentCatalog().getDb("default_cluster:test");
+        OlapTable tbl6 = (OlapTable) db.getTable("tbl6");
+        Assert.assertTrue(tbl6.getColumn("k1").isKey());
+        Assert.assertTrue(tbl6.getColumn("k2").isKey());
+        Assert.assertTrue(tbl6.getColumn("k3").isKey());
+
+        OlapTable tbl7 = (OlapTable) db.getTable("tbl7");
+        Assert.assertTrue(tbl7.getColumn("k1").isKey());
+        Assert.assertFalse(tbl7.getColumn("k2").isKey());
+        Assert.assertTrue(tbl7.getColumn("k2").getAggregationType() == AggregateType.NONE);
+    }
+
+    @Test
+    public void testAbormal() throws DdlException {
+        ExceptionChecker.expectThrowsWithMsg(DdlException.class,
+                "Floating point type column can not be distribution column",
+                () -> createTable("create table test.atbl1\n" + "(k1 int, k2 float)\n" + "duplicate key(k1)\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
+
+        ExceptionChecker.expectThrowsWithMsg(AnalysisException.class,
+                "Floating point type column can not be partition column",
+                () -> createTable("create table test.atbl3\n" + "(k1 int, k2 int, k3 float)\n" + "duplicate key(k1)\n"
+                        + "partition by range(k3)\n" + "(partition p1 values less than(\"10\"))\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
+
+        ExceptionChecker.expectThrowsWithMsg(DdlException.class,
+                "Varchar should not in the middle of short keys",
+                () -> createTable("create table test.atbl3\n" + "(k1 varchar(40), k2 int, k3 int)\n"
+                        + "duplicate key(k1, k2, k3)\n" + "distributed by hash(k1) buckets 1\n"
+                        + "properties('replication_num' = '1', 'short_key' = '3');"));
+
+        ExceptionChecker.expectThrowsWithMsg(DdlException.class, "Short key is too large. should less than: 3",
+                () -> createTable("create table test.atbl4\n" + "(k1 int, k2 int, k3 int)\n"
+                        + "duplicate key(k1, k2, k3)\n" + "distributed by hash(k1) buckets 1\n"
+                        + "properties('replication_num' = '1', 'short_key' = '4');"));
+
+        ExceptionChecker
+                .expectThrowsWithMsg(DdlException.class, "Failed to find enough host in all backends. need: 3",
+                        () -> createTable("create table test.atbl5\n" + "(k1 int, k2 int, k3 int)\n"
+                                + "duplicate key(k1, k2, k3)\n" + "distributed by hash(k1) buckets 1\n"
+                                + "properties('replication_num' = '3');"));
+
+        ExceptionChecker.expectThrowsNoException(
+                () -> createTable("create table test.atbl6\n" + "(k1 int, k2 int)\n" + "duplicate key(k1)\n"
+                        + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
+
+        ExceptionChecker
+                .expectThrowsWithMsg(DdlException.class, "Table 'atbl6' already exists",
+                        () -> createTable("create table test.atbl6\n" + "(k1 int, k2 int, k3 int)\n"
+                                + "duplicate key(k1, k2, k3)\n" + "distributed by hash(k1) buckets 1\n"
+                                + "properties('replication_num' = '1');"));
+
+        ConfigBase.setMutableConfig("enable_strict_storage_medium_check", "true");
+        ExceptionChecker
+                .expectThrowsWithMsg(DdlException.class, "Failed to find enough host with storage medium is SSD in all backends. need: 1",
+                        () -> createTable("create table test.tb7(key1 int, key2 varchar(10)) distributed by hash(key1) \n"
+                                + "buckets 1 properties('replication_num' = '1', 'storage_medium' = 'ssd');"));
     }
 }
