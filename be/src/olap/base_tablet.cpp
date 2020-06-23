71b207b2d52524bbc0de2dfc9483eb25b748c85e
lichaoyong <lichaoyong@baidu.com>
2020-06-23 16:17:00 +0800

    Revert "[Memory Engine] MemTablet creation and compatibility handling in BE (#3762)"
    This reverts commit ca96ea30560c9e9837c28cfd2cdd8ed24196f787.

--- be/src/olap/base_tablet.cpp
+++ be/src/olap/base_tablet.cpp
@@ -26,30 +26,11 @@ BaseTablet::BaseTablet(TabletMetaSharedPtr tablet_meta, DataDir* data_dir)
         : _state(tablet_meta->tablet_state()),
           _tablet_meta(tablet_meta),
           _schema(tablet_meta->tablet_schema()),
-          _data_dir(data_dir),
-          _is_bad(false) {
+          _data_dir(data_dir) {
     _gen_tablet_path();
 }
 
 BaseTablet::~BaseTablet() {}
-
-
-OLAPStatus BaseTablet::init() {
-    return _init_once.call([this] { return _init_once_action(); });
-}
-
-// should save tablet meta to remote meta store
-// if it's a primary replica
-void BaseTablet::save_meta() {
-    auto res = _tablet_meta->save_meta(_data_dir);
-    CHECK_EQ(res, OLAP_SUCCESS) << "fail to save tablet_meta. res=" << res
-                                << ", root=" << _data_dir->path();
-    // User could directly update tablet schema by _tablet_meta,
-    // So we need to refetch schema again
-    _schema = _tablet_meta->tablet_schema();
-    // TODO: update _mem_schema too?
-}
-
 
 OLAPStatus BaseTablet::set_tablet_state(TabletState state) {
     if (_tablet_meta->tablet_state() == TABLET_SHUTDOWN && state != TABLET_SHUTDOWN) {
@@ -70,14 +51,5 @@ void BaseTablet::_gen_tablet_path() {
         _tablet_path = path;
     }
 }
-
-OLAPStatus BaseTablet::set_partition_id(int64_t partition_id) {
-    return _tablet_meta->set_partition_id(partition_id);
-}
-
-TabletInfo BaseTablet::get_tablet_info() const {
-    return TabletInfo(tablet_id(), schema_hash(), tablet_uid());
-}
-
 
 } /* namespace doris */
