300c666304e2b85dbcdd1c6332facaf2942d1137
chenmingyu <chenmingyu@baidu.com>
2020-06-24 09:19:35 +0800

    remove unused import

--- fe/src/main/java/org/apache/doris/system/Backend.java
+++ fe/src/main/java/org/apache/doris/system/Backend.java
@@ -21,7 +21,6 @@ import org.apache.doris.alter.DecommissionBackendJob.DecommissionType;
 import org.apache.doris.catalog.Catalog;
 import org.apache.doris.catalog.DiskInfo;
 import org.apache.doris.catalog.DiskInfo.DiskState;
-import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
