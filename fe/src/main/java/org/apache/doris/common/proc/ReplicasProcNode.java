b4ef6252e43e8aa4516dc8215821d1296500b5c1
morningman <morningman@163.com>
2020-06-24 16:25:27 +0800

    [Enhance] Add MetaUrl and CompactionUrl for "show tablet" stmt
    Add MetaUrl and CompactionUrl in result of following stmt:
    
    `show tablet 10010`;

--- fe/src/main/java/org/apache/doris/common/proc/ReplicasProcNode.java
+++ fe/src/main/java/org/apache/doris/common/proc/ReplicasProcNode.java
@@ -17,10 +17,13 @@
 
 package org.apache.doris.common.proc;
 
+import org.apache.doris.catalog.Catalog;
 import org.apache.doris.catalog.Replica;
 import org.apache.doris.common.util.TimeUtils;
+import org.apache.doris.system.Backend;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,21 +38,37 @@ public class ReplicasProcNode implements ProcNodeInterface {
             .add("LstSuccessVersion").add("LstSuccessVersionHash")
             .add("LstFailedVersion").add("LstFailedVersionHash")
             .add("LstFailedTime").add("SchemaHash").add("DataSize").add("RowCount").add("State")
-            .add("IsBad").add("VersionCount").add("PathHash")
+            .add("IsBad").add("VersionCount").add("PathHash").add("MetaUrl").add("CompactionStatus")
             .build();
     
+    private long tabletId;
     private List<Replica> replicas;
 
-    public ReplicasProcNode(List<Replica> replicas) {
+    public ReplicasProcNode(long tabletId, List<Replica> replicas) {
+        this.tabletId = tabletId;
         this.replicas = replicas;
     }
 
     @Override
     public ProcResult fetchResult() {
+        ImmutableMap<Long, Backend> backendMap = Catalog.getCurrentSystemInfo().getIdToBackend();
 
         BaseProcResult result = new BaseProcResult();
         result.setNames(TITLE_NAMES);
         for (Replica replica : replicas) {
+            String metaUrl = String.format("http://%s:%d/api/meta/header/%d/%d",
+                    backendMap.get(replica.getBackendId()).getHost(),
+                    backendMap.get(replica.getBackendId()).getHttpPort(),
+                    tabletId,
+                    replica.getSchemaHash());
+
+            String compactionUrl = String.format(
+                    "http://%s:%d/api/compaction/show?tablet_id=%d&schema_hash=%d",
+                    backendMap.get(replica.getBackendId()).getHost(),
+                    backendMap.get(replica.getBackendId()).getHttpPort(),
+                    tabletId,
+                    replica.getSchemaHash());
+
             result.addRow(Arrays.asList(String.valueOf(replica.getId()),
                                         String.valueOf(replica.getBackendId()),
                                         String.valueOf(replica.getVersion()),
@@ -65,7 +84,9 @@ public class ReplicasProcNode implements ProcNodeInterface {
                                         String.valueOf(replica.getState()),
                                         String.valueOf(replica.isBad()),
                                         String.valueOf(replica.getVersionCount()),
-                                        String.valueOf(replica.getPathHash())));
+                                        String.valueOf(replica.getPathHash()),
+                                        metaUrl,
+                                        compactionUrl));
         }
         return result;
     }
