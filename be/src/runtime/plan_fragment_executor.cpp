apache/doris

commit 51367abce76282fa713673868e7a3d3b1c09c3de

[Bug] Fix bug that BE crash when doing Insert Operation (#3872)
Mainly change:
1. Fix the bug in `update_status(status)` of `PlanFragmentExecutor`.
2. When the FE Coordinator executes `execRemoteFragmentAsync()`, if it finds an RPC error, return a Future with an error code instead of exception.
3. Protect the `_status` in RuntimeState with lock
4. Move the `_runtime_profile` of RuntimeState before the `_obj_pool`, so that the profile will be
deconstructed after the object pool.
5. Remove the unused `ObjectPool` param in RuntimeProfile constructor. If I don't remove it,
RuntimeProfile will depends on the `_obj_pool` in RuntimeProfile.

be/src/runtime/plan_fragment_executor.cpp
@@ -323,7 +323,12 @@ Status PlanFragmentExecutor::open_internal() {
     {
         SCOPED_TIMER(profile()->total_time_counter());
         collect_query_statistics();
-        Status status = _sink->close(runtime_state(), _status);
+        Status status;
+        {
+            boost::lock_guard<boost::mutex> l(_status_lock);
+            status = _status;
+        }
+        status = _sink->close(runtime_state(), status);
         RETURN_IF_ERROR(status);
     }
 
@@ -489,8 +494,7 @@ void PlanFragmentExecutor::update_status(const Status& status) {
 
     {
         boost::lock_guard<boost::mutex> l(_status_lock);
-
-        if (_status.ok()) {
+        if (!_status.ok()) {
             if (status.is_mem_limit_exceeded()) {
                 _runtime_state->set_mem_limit_exceeded(status.get_error_msg());
             }
@@ -547,7 +551,12 @@ void PlanFragmentExecutor::close() {
 
         if (_sink.get() != NULL) {
             if (_prepared) {
-                _sink->close(runtime_state(), _status);
+                Status status;
+                {
+                    boost::lock_guard<boost::mutex> l(_status_lock);
+                    status = _status;
+                }
+                _sink->close(runtime_state(), status);
             } else {
                 _sink->close(runtime_state(), Status::InternalError("prepare failed"));
             }

be/src/exec/exec_node.cpp
@@ -552,7 +552,7 @@ void ExecNode::try_do_aggregate_serde_improve() {
 void ExecNode::init_runtime_profile(const std::string& name) {
     std::stringstream ss;
     ss << name << " (id=" << _id << ")";
-    _runtime_profile.reset(new RuntimeProfile(_pool, ss.str()));
+    _runtime_profile.reset(new RuntimeProfile(ss.str()));
     _runtime_profile->set_metadata(_id);
 }
 

be/src/exec/tablet_sink.cpp
@@ -457,7 +457,7 @@ Status OlapTableSink::prepare(RuntimeState* state) {
     _num_senders = state->num_per_fragment_instances();
 
     // profile must add to state's object pool
-    _profile = state->obj_pool()->add(new RuntimeProfile(_pool, "OlapTableSink"));
+    _profile = state->obj_pool()->add(new RuntimeProfile("OlapTableSink"));
     _mem_tracker = _pool->add(new MemTracker(-1, "OlapTableSink", state->instance_mem_tracker()));
 
     SCOPED_TIMER(_profile->total_time_counter());

be/src/runtime/buffered_block_mgr2.cc
@@ -1259,7 +1259,7 @@ void BufferedBlockMgr2::init(
 
     io_mgr->register_context(&_io_request_context);
 
-    _profile.reset(new RuntimeProfile(&_obj_pool, "BlockMgr"));
+    _profile.reset(new RuntimeProfile("BlockMgr"));
     parent_profile->add_child(_profile.get(), true, NULL);
 
     _block_size_counter = ADD_COUNTER(_profile.get(), "MaxBlockSize", TUnit::BYTES);

be/src/runtime/data_spliter.cpp
@@ -94,7 +94,7 @@ Status DataSpliter::prepare(RuntimeState* state) {
     for (auto& iter : _rollup_map) {
         RETURN_IF_ERROR(iter.second->prepare(state, _row_desc, _expr_mem_tracker.get()));
     }
-    _profile = state->obj_pool()->add(new RuntimeProfile(state->obj_pool(), title.str()));
+    _profile = state->obj_pool()->add(new RuntimeProfile(title.str()));
     for (auto iter : _partition_infos) {
         RETURN_IF_ERROR(iter->prepare(state, _row_desc, _expr_mem_tracker.get()));
     }

be/src/runtime/data_stream_sender.cpp
@@ -381,7 +381,7 @@ Status DataStreamSender::prepare(RuntimeState* state) {
     _state = state;
     std::stringstream title;
     title << "DataStreamSender (dst_id=" << _dest_node_id << ")";
-    _profile = _pool->add(new RuntimeProfile(_pool, title.str()));
+    _profile = _pool->add(new RuntimeProfile(title.str()));
     SCOPED_TIMER(_profile->total_time_counter());
     _mem_tracker.reset(
             new MemTracker(_profile, -1, "DataStreamSender", state->instance_mem_tracker()));

be/src/runtime/dpp_sink.cpp
@@ -569,7 +569,7 @@ Status Translator::create_profile(RuntimeState* state) {
     std::stringstream ss;
     ss << "Dpp translator(" << _tablet_desc.partition_id << "_" << _tablet_desc.bucket_id
         << "_" << _rollup_name << ")";
-    _profile = state->obj_pool()->add(new RuntimeProfile(state->obj_pool(), ss.str()));
+    _profile = state->obj_pool()->add(new RuntimeProfile(ss.str()));
 
     _add_batch_timer = ADD_TIMER(_profile, "add batch time");
     _sort_timer = ADD_TIMER(_profile, "sort time");
@@ -875,7 +875,7 @@ Status Translator::close(RuntimeState* state) {
 }
 
 Status DppSink::init(RuntimeState* state) {
-    _profile = state->obj_pool()->add(new RuntimeProfile(state->obj_pool(), "Dpp sink"));
+    _profile = state->obj_pool()->add(new RuntimeProfile("Dpp sink"));
     return Status::OK();
 }
 

be/src/runtime/export_sink.cpp
@@ -64,7 +64,7 @@ Status ExportSink::prepare(RuntimeState* state) {
     std::stringstream title;
     title << "ExportSink (frag_id=" << state->fragment_instance_id() << ")";
     // create profile
-    _profile = state->obj_pool()->add(new RuntimeProfile(state->obj_pool(), title.str()));
+    _profile = state->obj_pool()->add(new RuntimeProfile(title.str()));
     SCOPED_TIMER(_profile->total_time_counter());
 
     _mem_tracker.reset(new MemTracker(-1, "ExportSink", state->instance_mem_tracker()));

commit 2804b69be0a511816b029c2971c30b9521811caa

[Bug] Fix bug that a query plan is not correctly cancelled (#3933)
This bug is introduced by #3872.
It will cause some expected to be cancelled queries not being cancelled.

be/src/runtime/plan_fragment_executor.cpp
@@ -487,21 +487,22 @@ Status PlanFragmentExecutor::get_next_internal(RowBatch** batch) {
     return Status::OK();
 }
 
-void PlanFragmentExecutor::update_status(const Status& status) {
-    if (status.ok()) {
+void PlanFragmentExecutor::update_status(const Status& new_status) {
+    if (new_status.ok()) {
         return;
     }
 
     {
         boost::lock_guard<boost::mutex> l(_status_lock);
-        if (!_status.ok()) {
-            if (status.is_mem_limit_exceeded()) {
-                _runtime_state->set_mem_limit_exceeded(status.get_error_msg());
+        // if current `_status` is ok, set it to `new_status` to record the error.
+        if (_status.ok()) {
+            if (new_status.is_mem_limit_exceeded()) {
+                _runtime_state->set_mem_limit_exceeded(new_status.get_error_msg());
             }
-            _status = status;
+            _status = new_status;
             if (_runtime_state->query_options().query_type == TQueryType::EXTERNAL) {
                 TUniqueId fragment_instance_id = _runtime_state->fragment_instance_id();
-                _exec_env->result_queue_mgr()->update_queue_status(fragment_instance_id, status);
+                _exec_env->result_queue_mgr()->update_queue_status(fragment_instance_id, new_status);
             }
         }
     }