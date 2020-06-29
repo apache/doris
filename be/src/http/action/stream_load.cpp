212aa7f0ac080cbe6060402ed5cc6febf1b1c988
caiconghui <55968745+caiconghui@users.noreply.github.com>
2020-06-29 19:16:16 +0800

    Support rpc_timeout property in stream load request to cancel request in fe in time when stream load request is timeout (#3948)
    This PR is to enable cancel stream load request in FE in time
    when stream load request is timeout to make stream load more robust.

--- be/src/http/action/stream_load.cpp
+++ be/src/http/action/stream_load.cpp
@@ -386,7 +386,7 @@ Status StreamLoadAction::_process_put(HttpRequest* http_req, StreamLoadContext* 
     if (ctx->timeout_second != -1) {
         request.__set_timeout(ctx->timeout_second);
     }
-
+    request.__set_thrift_rpc_timeout_ms(config::thrift_rpc_timeout_ms);
     // plan this load
     TNetworkAddress master_addr = _exec_env->master_info()->network_address;
 #ifndef BE_TEST
