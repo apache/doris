2b731dc948963ae2650d287fbd512f362ec279ad
Dayue Gao <gaodayue@meituan.com>
2020-06-30 11:09:53 +0800

    [Bug] Insert may leak DeltaWriter when re-analyzed (#3973)

--- fe/src/main/java/org/apache/doris/analysis/InsertStmt.java
+++ fe/src/main/java/org/apache/doris/analysis/InsertStmt.java
@@ -744,6 +744,7 @@ public class InsertStmt extends DdlStmt {
     @Override
     public void reset() {
         super.reset();
+        targetPartitionIds.clear();
         queryStmt.reset();
         resultExprs.clear();
         exprByName.clear();
