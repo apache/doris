49cd3a59f4eec1409e5ab241adc227a5b7d3cbad
caiconghui <caiconghui2013@163.com>
2020-07-05 18:26:30 +0800

    remove unused import in ThriftServer.java

--- fe/src/main/java/org/apache/doris/common/ThriftServer.java
+++ fe/src/main/java/org/apache/doris/common/ThriftServer.java
@@ -21,7 +21,6 @@ import org.apache.doris.thrift.TNetworkAddress;
 
 import com.google.common.collect.Sets;
 
-import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TProcessor;
