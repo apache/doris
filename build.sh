146032788c5bbec5fb94515988ae991fbcb416b3
chenmingyu <chenmingyu@baidu.com>
2020-07-05 11:15:18 +0800

    first

--- build.sh
+++ build.sh
@@ -41,7 +41,8 @@ if [[ ! -f ${DORIS_THIRDPARTY}/installed/lib/libs2.a ]]; then
     ${DORIS_THIRDPARTY}/build-thirdparty.sh
 fi
 
-PARALLEL=$[$(nproc)/4+1]
+#PARALLEL=$[$(nproc)/4+1]
+PARALLEL=12
 
 # Check args
 usage() {
