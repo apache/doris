43bed864358653e2c9b3be35a51639163f318566
Yunfeng,Wu <wuyunfeng01@baidu.com>
2020-07-07 09:04:05 +0800

    [Doris On ES][Refactor] refactor and enchanment ES sync meta logic (#4012)
    After PR #3454 was merged, we should refactor and reorganize some logic for long-term sustainable iteration for Doris On ES.
    To facilitate code review，I would divided into this work to multiple PRs (some other WIP work I also need to think carefully)
    
    This PR include:
    
    1. introduce SearchContext for all state we needed
    2. divide meta-sync logic into three phase
    3. modify some logic processing
    4. introduce version detect logic for future using

--- fe/src/test/java/org/apache/doris/external/elasticsearch/EsUtilTest.java
+++ fe/src/test/java/org/apache/doris/external/elasticsearch/EsUtilTest.java
@@ -27,38 +27,38 @@ import org.junit.Test;
 
 public class EsUtilTest {
 
-    private String jsonStr = "{\"settings\": {\n" 
-            + "               \"index\": {\n" 
-            + "                  \"bpack\": {\n" 
-            + "                     \"partition\": {\n" 
-            + "                        \"upperbound\": \"12\"\n" 
-            + "                     }\n" 
-            + "                  },\n" 
-            + "                  \"number_of_shards\": \"5\",\n" 
-            + "                  \"provided_name\": \"indexa\",\n" 
-            + "                  \"creation_date\": \"1539328532060\",\n" 
-            + "                  \"number_of_replicas\": \"1\",\n" 
-            + "                  \"uuid\": \"plNNtKiiQ9-n6NpNskFzhQ\",\n" 
-            + "                  \"version\": {\n" 
-            + "                     \"created\": \"5050099\"\n" 
-            + "                  }\n" 
-            + "               }\n" 
+    private String jsonStr = "{\"settings\": {\n"
+            + "               \"index\": {\n"
+            + "                  \"bpack\": {\n"
+            + "                     \"partition\": {\n"
+            + "                        \"upperbound\": \"12\"\n"
+            + "                     }\n"
+            + "                  },\n"
+            + "                  \"number_of_shards\": \"5\",\n"
+            + "                  \"provided_name\": \"indexa\",\n"
+            + "                  \"creation_date\": \"1539328532060\",\n"
+            + "                  \"number_of_replicas\": \"1\",\n"
+            + "                  \"uuid\": \"plNNtKiiQ9-n6NpNskFzhQ\",\n"
+            + "                  \"version\": {\n"
+            + "                     \"created\": \"5050099\"\n"
+            + "                  }\n"
+            + "               }\n"
             + "            }}";
-    
+
     @Test
     public void testGetJsonObject() {
         JSONObject json = new JSONObject(jsonStr);
         JSONObject upperBoundSetting = EsUtil.getJsonObject(json, "settings.index.bpack.partition", 0);
         assertTrue(upperBoundSetting.has("upperbound"));
         assertEquals("12", upperBoundSetting.getString("upperbound"));
-        
+
         JSONObject unExistKey = EsUtil.getJsonObject(json, "set", 0);
         assertNull(unExistKey);
-        
+
         JSONObject singleKey = EsUtil.getJsonObject(json, "settings", 0);
         assertTrue(singleKey.has("index"));
     }
-    
+
     @Test(expected = JSONException.class)
     public void testGetJsonObjectWithException() {
         JSONObject json = new JSONObject(jsonStr);
