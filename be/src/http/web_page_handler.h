4bd70cad3cf5c39ba6423b38db25036e20424e5b
Yingchun Lai <405403881@qq.com>
2020-07-07 23:08:29 +0800

    [webserver] Make BE webserver handle static files (#4021)
    Make BE webserver handle static files, e.g. css, js, ico, then we can make
    BE website more pretty.

--- be/src/http/web_page_handler.h
+++ be/src/http/web_page_handler.h
@@ -37,34 +37,52 @@ class EvHttpServer;
 class WebPageHandler : public HttpHandler {
 public:
     typedef std::map<std::string, std::string> ArgumentMap;
-    typedef boost::function<void (const ArgumentMap& args, std::stringstream* output)> 
+    typedef boost::function<void (const ArgumentMap& args, std::stringstream* output)>
             PageHandlerCallback;
-    WebPageHandler(EvHttpServer* http_server);
 
-    virtual ~WebPageHandler() {
-    }
+    WebPageHandler(EvHttpServer* http_server);
+    virtual ~WebPageHandler();
 
     void handle(HttpRequest *req) override;
 
-    // Just use old code
-    void register_page(const std::string& path, const PageHandlerCallback& callback);
+    // Register a route 'path'.
+    // If 'is_on_nav_bar' is true, a link to the page will be placed on the navbar
+    // in the header of styled pages. The link text is given by 'alias'.
+    void register_page(const std::string& path, const std::string& alias,
+                       const PageHandlerCallback& callback, bool is_on_nav_bar);
 
 private:
     void bootstrap_page_header(std::stringstream* output);
     void bootstrap_page_footer(std::stringstream* output);
     void root_handler(const ArgumentMap& args, std::stringstream* output);
 
-    // all
-    class PageHandlers {
+    // Container class for a list of path handler callbacks for a single URL.
+    class PathHandler {
     public:
-        void add_callback(const PageHandlerCallback& callback) {
-            _callbacks.push_back(callback);
-        }
-        const std::vector<PageHandlerCallback>& callbacks() const {
-            return _callbacks;
-        }
+        PathHandler(bool is_styled, bool is_on_nav_bar, std::string alias,
+                    PageHandlerCallback callback)
+                : is_styled_(is_styled),
+                  is_on_nav_bar_(is_on_nav_bar),
+                  alias_(std::move(alias)),
+                  callback_(std::move(callback)) {}
+
+        bool is_styled() const { return is_styled_; }
+        bool is_on_nav_bar() const { return is_on_nav_bar_; }
+        const std::string& alias() const { return alias_; }
+        const PageHandlerCallback& callback() const { return callback_; }
+
     private:
-        std::vector<PageHandlerCallback> _callbacks;
+        // If true, the page appears is rendered styled.
+        bool is_styled_;
+
+        // If true, the page appears in the navigation bar.
+        bool is_on_nav_bar_;
+
+        // Alias used when displaying this link on the nav bar.
+        std::string alias_;
+
+        // Callback to render output for this page.
+        PageHandlerCallback callback_;
     };
 
     EvHttpServer* _http_server;
@@ -73,7 +91,7 @@ private:
     // Map of path to a PathHandler containing a list of handlers for that
     // path. More than one handler may register itself with a path so that many
     // components may contribute to a single page.
-    typedef std::map<std::string, PageHandlers> PageHandlersMap;
+    typedef std::map<std::string, PathHandler*> PageHandlersMap;
     PageHandlersMap _page_map;
 };
 
