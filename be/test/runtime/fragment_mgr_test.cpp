d36b063fd7ea56f4c91c77dff3fde3242bde44d9
huangwei5 <huangwei5@xiaomi.com>
2020-07-09 13:28:15 +0800

    [] ut

--- be/test/runtime/fragment_mgr_test.cpp
+++ be/test/runtime/fragment_mgr_test.cpp
@@ -29,6 +29,7 @@ namespace doris {
 
 static Status s_prepare_status;
 static Status s_open_status;
+static int s_abort_cnt;
 // Mock used for this unittest
 PlanFragmentExecutor::PlanFragmentExecutor(ExecEnv* exec_env,
                                            const report_status_callback& report_status_cb)
@@ -49,6 +50,7 @@ void PlanFragmentExecutor::cancel() {}
 
 void PlanFragmentExecutor::set_abort() {
     LOG(INFO) << "Plan Aborted";
+    s_abort_cnt++;
 }
 
 void PlanFragmentExecutor::close() {}
@@ -122,16 +124,27 @@ TEST_F(FragmentMgrTest, PrepareFailed) {
 }
 
 TEST_F(FragmentMgrTest, OfferPoolFailed) {
-    // config::fragment_pool_thread_num = 1;
-    // config::fragment_pool_queue_size = 0;
-    // FragmentMgr mgr(nullptr);
-    // for (int i = 0; i < 8; ++i) {
-    //     TExecPlanFragmentParams params;
-    //     params.params.fragment_instance_id = TUniqueId();
-    //     params.params.fragment_instance_id.__set_hi(100 + i);
-    //     params.params.fragment_instance_id.__set_lo(200);
-    //     ASSERT_TRUE(mgr.exec_plan_fragment(params).ok());
-    // }
+    config::fragment_pool_thread_num_min = 1;
+    config::fragment_pool_thread_num_max = 1;
+    config::fragment_pool_queue_size = 0;
+    s_abort_cnt = 0;
+    FragmentMgr mgr(nullptr);
+
+    TExecPlanFragmentParams params;
+    params.params.fragment_instance_id = TUniqueId();
+    params.params.fragment_instance_id.__set_hi(100);
+    params.params.fragment_instance_id.__set_lo(200);
+    ASSERT_TRUE(mgr.exec_plan_fragment(params).ok());
+
+    // the first plan open will cost 50ms, so the next 3 plans will be aborted.
+    for (int i = 1; i < 4; ++i) {
+        TExecPlanFragmentParams params;
+        params.params.fragment_instance_id = TUniqueId();
+        params.params.fragment_instance_id.__set_hi(100 + i);
+        params.params.fragment_instance_id.__set_lo(200);
+        ASSERT_FALSE(mgr.exec_plan_fragment(params).ok());
+    }
+    ASSERT_EQ(3, s_abort_cnt);
 }
 
 } // namespace doris
