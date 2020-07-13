e3e406ecdc4943be2cc5f90234ff7c00e2cd558f
hf200012 <zhangjf1@gmail.com>
2020-07-13 12:33:39 +0800

    modify auth check

--- fe/src/main/java/org/apache/doris/http/meta/MetaService.java
+++ fe/src/main/java/org/apache/doris/http/meta/MetaService.java
@@ -42,10 +42,12 @@ import java.io.OutputStream;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@RestController
 public class MetaService extends RestBaseController {
     private static final Logger LOG = LogManager.getLogger(MetaService.class);
 
