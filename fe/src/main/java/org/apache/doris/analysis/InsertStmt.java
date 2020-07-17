de3c4b198e35533996d33ed858b4f6af1a693f02
HangyuanLiu <460660596@qq.com>
2020-07-17 10:43:36 +0800

    Support materialized view extend column in load and insert (#3677)
    This commit mainly supports load bitmap_union, hll_union, and count materialized views.
    
    The main changes are as follows:
    1、insert stmt support load extend column
    2、load stmt support load extend column
    
    Issue : #3344
    
    Co-authored-by: HangyuanLiu <460660956@qq.com>

--- fe/src/main/java/org/apache/doris/analysis/InsertStmt.java
+++ fe/src/main/java/org/apache/doris/analysis/InsertStmt.java
@@ -33,6 +33,7 @@ import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.DdlException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
+import org.apache.doris.common.Pair;
 import org.apache.doris.common.UserException;
 import org.apache.doris.metric.MetricRepo;
 import org.apache.doris.mysql.privilege.PrivPredicate;
@@ -427,25 +428,46 @@ public class InsertStmt extends DdlStmt {
 
         /*
          * When doing schema change, there may be some shadow columns. we should add
-         * them to the end of targetColumns. And use 'origColIdxsForShadowCols' to save
+         * them to the end of targetColumns. And use 'origColIdxsForExtendCols' to save
          * the index of column in 'targetColumns' which the shadow column related to.
          * eg: origin targetColumns: (A,B,C), shadow column: __doris_shadow_B after
          * processing, targetColumns: (A, B, C, __doris_shadow_B), and
-         * origColIdxsForShadowCols has 1 element: "1", which is the index of column B
+         * origColIdxsForExtendCols has 1 element: "1", which is the index of column B
          * in targetColumns.
          * 
          * Rule A: If the column which the shadow column related to is not mentioned,
          * then do not add the shadow column to targetColumns. They will be filled by
          * null or default value when loading.
+         *
+         * When table have materialized view, there may be some materialized view columns.
+         * we should add them to the end of targetColumns.
+         * eg: origin targetColumns: (A,B,C), shadow column: mv_bitmap_union_C
+         * after processing, targetColumns: (A, B, C, mv_bitmap_union_C), and
+         * origColIdx2MVColumn has 1 element: "2, mv_bitmap_union_C"
+         * will be used in as a mapping from queryStmt.getResultExprs() to targetColumns define expr
          */
-        List<Integer> origColIdxsForShadowCols = Lists.newArrayList();
+        List<Pair<Integer, Column>> origColIdxsForExtendCols = Lists.newArrayList();
         for (Column column : targetTable.getFullSchema()) {
             if (column.isNameWithPrefix(SchemaChangeHandler.SHADOW_NAME_PRFIX)) {
                 String origName = Column.removeNamePrefix(column.getName());
                 for (int i = 0; i < targetColumns.size(); i++) {
                     if (targetColumns.get(i).nameEquals(origName, false)) {
                         // Rule A
-                        origColIdxsForShadowCols.add(i);
+                        origColIdxsForExtendCols.add(new Pair<>(i, null));
+                        targetColumns.add(column);
+                        break;
+                    }
+                }
+            }
+            if (column.isNameWithPrefix(CreateMaterializedViewStmt.MATERIALIZED_VIEW_NAME_PRFIX)) {
+                SlotRef refColumn = column.getRefColumn();
+                if (refColumn == null) {
+                    ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_FIELD_ERROR, column.getName(), targetTable.getName());
+                }
+                String origName = refColumn.getColumnName();
+                for (int originColumnIdx = 0; originColumnIdx < targetColumns.size(); originColumnIdx++) {
+                    if (targetColumns.get(originColumnIdx).nameEquals(origName, false)) {
+                        origColIdxsForExtendCols.add(new Pair<>(originColumnIdx, column));
                         targetColumns.add(column);
                         break;
                     }
@@ -472,7 +494,7 @@ public class InsertStmt extends DdlStmt {
                 // INSERT INTO VALUES(...)
                 List<ArrayList<Expr>> rows = selectStmt.getValueList().getRows();
                 for (int rowIdx = 0; rowIdx < rows.size(); ++rowIdx) {
-                    analyzeRow(analyzer, targetColumns, rows, rowIdx, origColIdxsForShadowCols);
+                    analyzeRow(analyzer, targetColumns, rows, rowIdx, origColIdxsForExtendCols);
                 }
 
                 // clear these 2 structures, rebuild them using VALUES exprs
@@ -487,7 +509,7 @@ public class InsertStmt extends DdlStmt {
                 // INSERT INTO SELECT 1,2,3 ...
                 List<ArrayList<Expr>> rows = Lists.newArrayList();
                 rows.add(selectStmt.getResultExprs());
-                analyzeRow(analyzer, targetColumns, rows, 0, origColIdxsForShadowCols);
+                analyzeRow(analyzer, targetColumns, rows, 0, origColIdxsForExtendCols);
                 // rows may be changed in analyzeRow(), so rebuild the result exprs
                 selectStmt.getResultExprs().clear();
                 for (Expr expr : rows.get(0)) {
@@ -497,12 +519,22 @@ public class InsertStmt extends DdlStmt {
             isStreaming = true;
         } else {
             // INSERT INTO SELECT ... FROM tbl
-            if (!origColIdxsForShadowCols.isEmpty()) {
+            if (!origColIdxsForExtendCols.isEmpty()) {
                 // extend the result expr by duplicating the related exprs
-                for (Integer idx : origColIdxsForShadowCols) {
-                    queryStmt.getResultExprs().add(queryStmt.getResultExprs().get(idx));
+                for (Pair<Integer, Column> entry : origColIdxsForExtendCols) {
+                    if (entry.second == null) {
+                        queryStmt.getResultExprs().add(queryStmt.getResultExprs().get(entry.first));
+                    } else {
+                        //substitute define expr slot with select statement result expr
+                        ExprSubstitutionMap smap = new ExprSubstitutionMap();
+                        smap.getLhs().add(entry.second.getRefColumn());
+                        smap.getRhs().add(queryStmt.getResultExprs().get(entry.first));
+                        Expr e = Expr.substituteList(Lists.newArrayList(entry.second.getDefineExpr()), smap, analyzer, false).get(0);
+                        queryStmt.getResultExprs().add(e);
+                    }
                 }
             }
+
             // check compatibility
             for (int i = 0; i < targetColumns.size(); ++i) {
                 Column column = targetColumns.get(i);
@@ -518,17 +550,26 @@ public class InsertStmt extends DdlStmt {
             }
         }
 
-        // expand baseTblResultExprs and colLabels in QueryStmt
-        if (!origColIdxsForShadowCols.isEmpty()) {
+        // expand colLabels in QueryStmt
+        if (!origColIdxsForExtendCols.isEmpty()) {
             if (queryStmt.getResultExprs().size() != queryStmt.getBaseTblResultExprs().size()) {
-                for (Integer idx : origColIdxsForShadowCols) {
-                    queryStmt.getBaseTblResultExprs().add(queryStmt.getBaseTblResultExprs().get(idx));
+                for (Pair<Integer, Column> entry  : origColIdxsForExtendCols) {
+                    if (entry.second == null) {
+                        queryStmt.getBaseTblResultExprs().add(queryStmt.getBaseTblResultExprs().get(entry.first));
+                    } else {
+                        //substitute define expr slot with select statement result expr
+                        ExprSubstitutionMap smap = new ExprSubstitutionMap();
+                        smap.getLhs().add(entry.second.getRefColumn());
+                        smap.getRhs().add(queryStmt.getResultExprs().get(entry.first));
+                        Expr e = Expr.substituteList(Lists.newArrayList(entry.second.getDefineExpr()), smap, analyzer, false).get(0);
+                        queryStmt.getBaseTblResultExprs().add(e);
+                    }
                 }
             }
 
             if (queryStmt.getResultExprs().size() != queryStmt.getColLabels().size()) {
-                for (Integer idx : origColIdxsForShadowCols) {
-                    queryStmt.getColLabels().add(queryStmt.getColLabels().get(idx));
+                for (Pair<Integer, Column> entry : origColIdxsForExtendCols) {
+                    queryStmt.getColLabels().add(queryStmt.getColLabels().get(entry.first));
                 }
             }
         }
@@ -547,16 +588,16 @@ public class InsertStmt extends DdlStmt {
     }
 
     private void analyzeRow(Analyzer analyzer, List<Column> targetColumns, List<ArrayList<Expr>> rows,
-            int rowIdx, List<Integer> origColIdxsForShadowCols) throws AnalysisException {
+            int rowIdx, List<Pair<Integer, Column>> origColIdxsForExtendCols) throws AnalysisException {
         // 1. check number of fields if equal with first row
         // targetColumns contains some shadow columns, which is added by system,
         // so we should minus this
-        if (rows.get(rowIdx).size() != targetColumns.size() - origColIdxsForShadowCols.size()) {
+        if (rows.get(rowIdx).size() != targetColumns.size() - origColIdxsForExtendCols.size()) {
             throw new AnalysisException("Column count doesn't match value count at row " + (rowIdx + 1));
         }
 
         ArrayList<Expr> row = rows.get(rowIdx);
-        if (!origColIdxsForShadowCols.isEmpty()) {
+        if (!origColIdxsForExtendCols.isEmpty()) {
             /**
              * we should extends the row for shadow columns.
              * eg:
@@ -566,14 +607,20 @@ public class InsertStmt extends DdlStmt {
             ArrayList<Expr> extentedRow = Lists.newArrayList();
             extentedRow.addAll(row);
             
-            for (Integer idx : origColIdxsForShadowCols) {
-                extentedRow.add(extentedRow.get(idx));
+            for (Pair<Integer, Column> entry : origColIdxsForExtendCols) {
+                if (entry == null) {
+                    extentedRow.add(extentedRow.get(entry.first));
+                } else {
+                    ExprSubstitutionMap smap = new ExprSubstitutionMap();
+                    smap.getLhs().add(entry.second.getRefColumn());
+                    smap.getRhs().add(extentedRow.get(entry.first));
+                    extentedRow.add(Expr.substituteList(Lists.newArrayList(entry.second.getDefineExpr()), smap, analyzer, false).get(0));
+                }
             }
 
             row = extentedRow;
             rows.set(rowIdx, row);
         }
-
         // check the compatibility of expr in row and column in targetColumns
         for (int i = 0; i < row.size(); ++i) {
             Expr expr = row.get(i);
