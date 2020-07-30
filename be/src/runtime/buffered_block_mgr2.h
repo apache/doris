5483ad2cae9850bea4f028fbb9f080f66407b462
HappenLee <happenlee@hotmail.com>
2020-07-30 10:29:12 +0800

    [Spill To Disk][1/6] The adjustment of the basic BufferedBlockMgr includes the following change (#4151)
    1. Add Exec msg for BufferedBlockMgr for debug tuning
        2. Change the API of Consume Memory? We will use it in HashTable in the future
        3. Fix mistake of count _unfullfilled_reserved_buffers  in BufferedBlockMgr

--- be/src/runtime/buffered_block_mgr2.h
+++ be/src/runtime/buffered_block_mgr2.h
@@ -508,6 +508,9 @@ private:
     bool validate() const;
     std::string debug_internal() const;
 
+    // Add BE hostname and fragmentid for debug tuning
+    Status add_exec_msg(const std::string& msg) const;
+
     // Size of the largest/default block in bytes.
     const int64_t _max_block_size;
 
@@ -637,6 +640,9 @@ private:
     // d'tor will be called at which point the weak ptr will be removed from the map.
     typedef boost::unordered_map<TUniqueId, boost::weak_ptr<BufferedBlockMgr2> > BlockMgrsMap;
     static BlockMgrsMap _s_query_to_block_mgrs;
+
+    // Unowned.
+    RuntimeState* _state;
 
 }; // class BufferedBlockMgr2
 
