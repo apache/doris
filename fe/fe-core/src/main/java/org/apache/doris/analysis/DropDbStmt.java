apache/doris

commit 150f8e0e2b5e6c0224554c75715d030efe64fa8d

Support check committed txns before catalog drop meta, like db, table, partition etc (#4029)
This PR is to ensure that dropped db , table or partition can be with normal state after recovered by user. Commited txns can not be aborted, because the partitions's commited versions have been changed, and some tablets may already have new visible versions. If user just don't want the meta(db, table or partition) anymore, just use drop force instead of drop to skip committed txn check.

fe/fe-core/src/main/cup/sql_parser.cup
@@ -235,7 +235,7 @@ terminal String KW_ADD, KW_ADMIN, KW_AFTER, KW_AGGREGATE, KW_ALL, KW_ALTER, KW_A
     KW_DELETE, KW_DISTINCT, KW_DISTINCTPC, KW_DISTINCTPCSA, KW_DISTRIBUTED, KW_DISTRIBUTION, KW_DYNAMIC, KW_BUCKETS, KW_DIV, KW_DOUBLE, KW_DROP, KW_DROPP, KW_DUPLICATE,
     KW_ELSE, KW_END, KW_ENGINE, KW_ENGINES, KW_ENTER, KW_ERRORS, KW_EVENTS, KW_EXCEPT, KW_EXISTS, KW_EXPORT,
     KW_EXTERNAL, KW_EXTRACT,
-    KW_FALSE, KW_FOLLOWER, KW_FOLLOWING, KW_FREE, KW_FROM, KW_FILE, KW_FIRST, KW_FLOAT, KW_FOR, KW_FORMAT, KW_FRONTEND, KW_FRONTENDS, KW_FULL, KW_FUNCTION, KW_FUNCTIONS,
+    KW_FALSE, KW_FOLLOWER, KW_FOLLOWING, KW_FREE, KW_FROM, KW_FILE, KW_FIRST, KW_FLOAT, KW_FOR, KW_FORCE, KW_FORMAT, KW_FRONTEND, KW_FRONTENDS, KW_FULL, KW_FUNCTION, KW_FUNCTIONS,
     KW_GLOBAL, KW_GRANT, KW_GRANTS, KW_GROUP, KW_GROUPING,
     KW_HASH, KW_HAVING, KW_HELP,KW_HLL, KW_HLL_UNION, KW_HOUR, KW_HUB,
     KW_IDENTIFIED, KW_IF, KW_IN, KW_INDEX, KW_INDEXES, KW_INFILE, KW_INSTALL,
@@ -463,6 +463,7 @@ nonterminal String opt_db, procedure_or_function, opt_comment, opt_engine;
 nonterminal ColumnDef.DefaultValue opt_default_value;
 nonterminal Boolean opt_if_exists, opt_if_not_exists;
 nonterminal Boolean opt_external;
+nonterminal Boolean opt_force;
 nonterminal IndexDef.IndexType opt_index_type;
 
 nonterminal ShowAlterStmt.AlterType opt_alter_type;
@@ -882,9 +883,9 @@ alter_table_clause ::=
     {:
         RESULT = new AddPartitionClause(desc, distribution, properties, isTempPartition);
     :}
-    | KW_DROP opt_tmp:isTempPartition KW_PARTITION opt_if_exists:ifExists ident:partitionName
+    | KW_DROP opt_tmp:isTempPartition KW_PARTITION opt_force:force opt_if_exists:ifExists ident:partitionName
     {:
-        RESULT = new DropPartitionClause(ifExists, partitionName, isTempPartition);
+        RESULT = new DropPartitionClause(ifExists, partitionName, isTempPartition, force ? !force : !isTempPartition);
     :}
     | KW_MODIFY KW_PARTITION ident:partitionName KW_SET LPAREN key_value_map:properties RPAREN
     {:
@@ -1575,13 +1576,13 @@ revoke_stmt ::=
 // Drop statement
 drop_stmt ::=
     /* Database */
-    KW_DROP KW_DATABASE opt_if_exists:ifExists ident:db
+    KW_DROP KW_DATABASE opt_force:force opt_if_exists:ifExists ident:db
     {:
-        RESULT = new DropDbStmt(ifExists, db);
+        RESULT = new DropDbStmt(ifExists, db, !force);
     :}
-    | KW_DROP KW_SCHEMA opt_if_exists:ifExists ident:db
+    | KW_DROP KW_SCHEMA opt_force:force opt_if_exists:ifExists ident:db
     {:
-        RESULT = new DropDbStmt(ifExists, db);
+        RESULT = new DropDbStmt(ifExists, db, !force);
     :}
     /* cluster */
     | KW_DROP KW_CLUSTER opt_if_exists:ifExists ident:cluster
@@ -1594,9 +1595,9 @@ drop_stmt ::=
         RESULT = new DropFunctionStmt(functionName, args);
     :}
     /* Table */
-    | KW_DROP KW_TABLE opt_if_exists:ifExists table_name:name
+    | KW_DROP KW_TABLE opt_force:force opt_if_exists:ifExists table_name:name
     {:
-        RESULT = new DropTableStmt(ifExists, name);
+        RESULT = new DropTableStmt(ifExists, name, !force);
     :}
     /* User */
     | KW_DROP KW_USER user_identity:userId
@@ -1606,7 +1607,7 @@ drop_stmt ::=
     /* View */
     | KW_DROP KW_VIEW opt_if_exists:ifExists table_name:name
     {:
-        RESULT = new DropTableStmt(ifExists, name, true);
+        RESULT = new DropTableStmt(ifExists, name, true, false);
     :}
     | KW_DROP KW_REPOSITORY ident:repoName
     {:
@@ -2020,6 +2021,17 @@ opt_external ::=
         RESULT = false;
     :}
     | KW_EXTERNAL
+    {:
+        RESULT = true;
+    :}
+    ;
+
+opt_force ::=
+     /* empty */
+    {:
+        RESULT = false;
+    :}
+    | KW_FORCE
     {:
         RESULT = true;
     :}

fe/fe-core/src/main/java/org/apache/doris/analysis/DropDbStmt.java
@@ -33,10 +33,12 @@ import com.google.common.base.Strings;
 public class DropDbStmt extends DdlStmt {
     private boolean ifExists;
     private String dbName;
+    private boolean needCheckCommittedTxns;
 
-    public DropDbStmt(boolean ifExists, String dbName) {
+    public DropDbStmt(boolean ifExists, String dbName, boolean needCheckCommittedTxns) {
         this.ifExists = ifExists;
         this.dbName = dbName;
+        this.needCheckCommittedTxns = needCheckCommittedTxns;
     }
 
     public boolean isSetIfExists() {
@@ -45,6 +47,10 @@ public class DropDbStmt extends DdlStmt {
 
     public String getDbName() {
         return this.dbName;
+    }
+
+    public boolean isNeedCheckCommittedTxns() {
+        return this.needCheckCommittedTxns;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/analysis/DropPartitionClause.java
@@ -32,13 +32,15 @@ public class DropPartitionClause extends AlterTableClause {
     private String partitionName;
     // true if this is to drop a temp partition
     private boolean isTempPartition;
+    private boolean needCheckCommittedTxns;
 
-    public DropPartitionClause(boolean ifExists, String partitionName, boolean isTempPartition) {
+    public DropPartitionClause(boolean ifExists, String partitionName, boolean isTempPartition, boolean needCheckCommittedTxns) {
         super(AlterOpType.DROP_PARTITION);
         this.ifExists = ifExists;
         this.partitionName = partitionName;
         this.isTempPartition = isTempPartition;
         this.needTableStable = false;
+        this.needCheckCommittedTxns = needCheckCommittedTxns;
     }
 
     public boolean isSetIfExists() {
@@ -51,6 +53,10 @@ public class DropPartitionClause extends AlterTableClause {
 
     public boolean isTempPartition() {
         return isTempPartition;
+    }
+
+    public boolean isNeedCheckCommittedTxns() {
+        return needCheckCommittedTxns;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/analysis/DropTableStmt.java
@@ -32,17 +32,20 @@ public class DropTableStmt extends DdlStmt {
     private boolean ifExists;
     private final TableName tableName;
     private final boolean isView;
+    private boolean needCheckCommittedTxns;
 
-    public DropTableStmt(boolean ifExists, TableName tableName) {
+    public DropTableStmt(boolean ifExists, TableName tableName, boolean needCheckCommittedTxns) {
         this.ifExists = ifExists;
         this.tableName = tableName;
         this.isView = false;
+        this.needCheckCommittedTxns = needCheckCommittedTxns;
     }
 
-    public DropTableStmt(boolean ifExists, TableName tableName, boolean isView) {
+    public DropTableStmt(boolean ifExists, TableName tableName, boolean isView, boolean needCheckCommittedTxns) {
         this.ifExists = ifExists;
         this.tableName = tableName;
         this.isView = isView;
+        this.needCheckCommittedTxns = needCheckCommittedTxns;
     }
 
     public boolean isSetIfExists() {
@@ -59,6 +62,10 @@ public class DropTableStmt extends DdlStmt {
 
     public boolean isView() {
         return isView;
+    }
+
+    public boolean isNeedCheckCommittedTxns() {
+        return this.needCheckCommittedTxns;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -2678,6 +2678,13 @@ public class Catalog {
             Database db = this.fullNameToDb.get(dbName);
             db.writeLock();
             try {
+                if (stmt.isNeedCheckCommittedTxns()) {
+                    if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), null, null)) {
+                       throw new DdlException("There are still some transactions in the COMMITTED state waiting to be completed. " +
+                               "The database [" + dbName +"] cannot be dropped. If you want to forcibly drop(cannot be recovered)," +
+                               " please use \"DROP database FORCE\".");
+                    }
+                }
                 if (db.getDbState() == DbState.LINK && dbName.equals(db.getAttachDb())) {
                     // We try to drop a hard link.
                     final DropLinkDbAndUpdateDbInfo info = new DropLinkDbAndUpdateDbInfo();
@@ -2713,8 +2720,10 @@ public class Catalog {
 
                 // save table names for recycling
                 Set<String> tableNames = db.getTableNamesWithLock();
-                unprotectDropDb(db);
-                Catalog.getCurrentRecycleBin().recycleDatabase(db, tableNames);
+                unprotectDropDb(db, !stmt.isNeedCheckCommittedTxns());
+                if (stmt.isNeedCheckCommittedTxns()) {
+                    Catalog.getCurrentRecycleBin().recycleDatabase(db, tableNames);
+                }
             } finally {
                 db.writeUnlock();
             }
@@ -2724,17 +2733,17 @@ public class Catalog {
             fullNameToDb.remove(db.getFullName());
             final Cluster cluster = nameToCluster.get(db.getClusterName());
             cluster.removeDb(dbName, db.getId());
-            editLog.logDropDb(dbName);
+            editLog.logDropDb(dbName, !stmt.isNeedCheckCommittedTxns());
         } finally {
             unlock();
         }
 
-        LOG.info("finish drop database[{}]", dbName);
+        LOG.info("finish drop database[{}], is force : {}", dbName, !stmt.isNeedCheckCommittedTxns());
     }
 
-    public void unprotectDropDb(Database db) {
+    public void unprotectDropDb(Database db, boolean isForeDrop) {
         for (Table table : db.getTables()) {
-            unprotectDropTable(db, table.getId());
+            unprotectDropTable(db, table.getId(), isForeDrop);
         }
     }
 
@@ -2754,15 +2763,17 @@ public class Catalog {
         }
     }
 
-    public void replayDropDb(String dbName) throws DdlException {
+    public void replayDropDb(String dbName, boolean isForceDrop) throws DdlException {
         tryLock(true);
         try {
             Database db = fullNameToDb.get(dbName);
             db.writeLock();
             try {
                 Set<String> tableNames = db.getTableNamesWithLock();
-                unprotectDropDb(db);
-                Catalog.getCurrentRecycleBin().recycleDatabase(db, tableNames);
+                unprotectDropDb(db, isForceDrop);
+                if (!isForceDrop) {
+                    Catalog.getCurrentRecycleBin().recycleDatabase(db, tableNames);
+                }
             } finally {
                 db.writeUnlock();
             }
@@ -3322,14 +3333,24 @@ public class Catalog {
         if (isTempPartition) {
             olapTable.dropTempPartition(partitionName, true);
         } else {
-            olapTable.dropPartition(db.getId(), partitionName);
+            if (clause.isNeedCheckCommittedTxns()) {
+                Partition partition = olapTable.getPartition(partitionName);
+                if (partition != null) {
+                    if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), olapTable.getId(), partition.getId())) {
+                        throw new DdlException("There are still some transactions in the COMMITTED state waiting to be completed." +
+                                " The partition [" + partitionName + "] cannot be dropped. If you want to forcibly drop(cannot be recovered)," +
+                                " please use \"DROP partition FORCE\".");
+                    }
+                }
+            }
+            olapTable.dropPartition(db.getId(), partitionName, !clause.isNeedCheckCommittedTxns());
         }
 
         // log
-        DropPartitionInfo info = new DropPartitionInfo(db.getId(), olapTable.getId(), partitionName, isTempPartition);
+        DropPartitionInfo info = new DropPartitionInfo(db.getId(), olapTable.getId(), partitionName, isTempPartition, !clause.isNeedCheckCommittedTxns());
         editLog.logDropPartition(info);
 
-        LOG.info("succeed in droping partition[{}]", partitionName);
+        LOG.info("succeed in droping partition[{}], is temp : {}, is force : {}", partitionName, isTempPartition, !clause.isNeedCheckCommittedTxns());
     }
 
     public void replayDropPartition(DropPartitionInfo info) {
@@ -3340,7 +3361,7 @@ public class Catalog {
             if (info.isTempPartition()) {
                 olapTable.dropTempPartition(info.getPartitionName(), true);
             } else {
-                olapTable.dropPartition(info.getDbId(), info.getPartitionName());
+                olapTable.dropPartition(info.getDbId(), info.getPartitionName(), info.isForceDrop());
             }
         } finally {
             db.writeUnlock();
@@ -4284,18 +4305,24 @@ public class Catalog {
                 }
             }
 
-            unprotectDropTable(db, table.getId());
-
-            DropInfo info = new DropInfo(db.getId(), table.getId(), -1L);
+            if (stmt.isNeedCheckCommittedTxns()) {
+                if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), table.getId(), null)) {
+                    throw new DdlException("There are still some transactions in the COMMITTED state waiting to be completed. " +
+                            "The table [" + tableName +"] cannot be dropped. If you want to forcibly drop(cannot be recovered)," +
+                            " please use \"DROP table FORCE\".");
+                }
+            }
+            unprotectDropTable(db, table.getId(), !stmt.isNeedCheckCommittedTxns());
+            DropInfo info = new DropInfo(db.getId(), table.getId(), -1L, !stmt.isNeedCheckCommittedTxns());
             editLog.logDropTable(info);
         } finally {
             db.writeUnlock();
         }
 
-        LOG.info("finished dropping table: {} from db: {}", tableName, dbName);
+        LOG.info("finished dropping table: {} from db: {}, is force: {}", tableName, dbName, !stmt.isNeedCheckCommittedTxns());
     }
 
-    public boolean unprotectDropTable(Database db, long tableId) {
+    public boolean unprotectDropTable(Database db, long tableId, boolean isForceDrop) {
         Table table = db.getTable(tableId);
         // delete from db meta
         if (table == null) {
@@ -4311,17 +4338,18 @@ public class Catalog {
         }
 
         db.dropTable(table.getName());
-
-        Catalog.getCurrentRecycleBin().recycleTable(db.getId(), table);
+        if (!isForceDrop) {
+            Catalog.getCurrentRecycleBin().recycleTable(db.getId(), table);
+        }
 
         LOG.info("finished dropping table[{}] in db[{}]", table.getName(), db.getFullName());
         return true;
     }
 
-    public void replayDropTable(Database db, long tableId) {
+    public void replayDropTable(Database db, long tableId, boolean isForceDrop) {
         db.writeLock();
         try {
-            unprotectDropTable(db, tableId);
+            unprotectDropTable(db, tableId, isForceDrop);
         } finally {
             db.writeUnlock();
         }

commit 237271c764e19fb68c23b9d571741d65a0c6d61a

[Bug] Fix fe meta version problem, make drop meta check code easy to read and add doc content for drop meta check (#4205)
This PR is mainly do three things:
1. Fix fe meta version bug introduced by #4029 , when fix conflict with #4086 
2. Make drop check code easy to read
3. Add doc content for drop meta check

fe/fe-core/src/main/cup/sql_parser.cup
@@ -885,7 +885,7 @@ alter_table_clause ::=
     :}
     | KW_DROP opt_tmp:isTempPartition KW_PARTITION opt_force:force opt_if_exists:ifExists ident:partitionName
     {:
-        RESULT = new DropPartitionClause(ifExists, partitionName, isTempPartition, force ? !force : !isTempPartition);
+        RESULT = new DropPartitionClause(ifExists, partitionName, isTempPartition, force ? force : isTempPartition);
     :}
     | KW_MODIFY KW_PARTITION ident:partitionName KW_SET LPAREN key_value_map:properties RPAREN
     {:
@@ -1578,7 +1578,7 @@ drop_stmt ::=
     /* Database */
     KW_DROP KW_DATABASE opt_force:force opt_if_exists:ifExists ident:db
     {:
-        RESULT = new DropDbStmt(ifExists, db, !force);
+        RESULT = new DropDbStmt(ifExists, db, force);
     :}
     | KW_DROP KW_SCHEMA opt_force:force opt_if_exists:ifExists ident:db
     {:
@@ -1597,7 +1597,7 @@ drop_stmt ::=
     /* Table */
     | KW_DROP KW_TABLE opt_force:force opt_if_exists:ifExists table_name:name
     {:
-        RESULT = new DropTableStmt(ifExists, name, !force);
+        RESULT = new DropTableStmt(ifExists, name, force);
     :}
     /* User */
     | KW_DROP KW_USER user_identity:userId

fe/fe-core/src/main/java/org/apache/doris/analysis/DropDbStmt.java
@@ -20,7 +20,6 @@ package org.apache.doris.analysis;
 import org.apache.doris.catalog.Catalog;
 import org.apache.doris.catalog.InfoSchemaDb;
 import org.apache.doris.cluster.ClusterNamespace;
-import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
@@ -33,12 +32,12 @@ import com.google.common.base.Strings;
 public class DropDbStmt extends DdlStmt {
     private boolean ifExists;
     private String dbName;
-    private boolean needCheckCommittedTxns;
+    private boolean forceDrop;
 
-    public DropDbStmt(boolean ifExists, String dbName, boolean needCheckCommittedTxns) {
+    public DropDbStmt(boolean ifExists, String dbName, boolean forceDrop) {
         this.ifExists = ifExists;
         this.dbName = dbName;
-        this.needCheckCommittedTxns = needCheckCommittedTxns;
+        this.forceDrop = forceDrop;
     }
 
     public boolean isSetIfExists() {
@@ -49,12 +48,12 @@ public class DropDbStmt extends DdlStmt {
         return this.dbName;
     }
 
-    public boolean isNeedCheckCommittedTxns() {
-        return this.needCheckCommittedTxns;
+    public boolean isForceDrop() {
+        return this.forceDrop;
     }
 
     @Override
-    public void analyze(Analyzer analyzer) throws AnalysisException, UserException {
+    public void analyze(Analyzer analyzer) throws UserException {
         super.analyze(analyzer);
         if (Strings.isNullOrEmpty(dbName)) {
             ErrorReport.reportAnalysisException(ErrorCode.ERR_WRONG_DB_NAME, dbName);

fe/fe-core/src/main/java/org/apache/doris/analysis/DropPartitionClause.java
@@ -32,15 +32,15 @@ public class DropPartitionClause extends AlterTableClause {
     private String partitionName;
     // true if this is to drop a temp partition
     private boolean isTempPartition;
-    private boolean needCheckCommittedTxns;
+    private boolean forceDrop;
 
-    public DropPartitionClause(boolean ifExists, String partitionName, boolean isTempPartition, boolean needCheckCommittedTxns) {
+    public DropPartitionClause(boolean ifExists, String partitionName, boolean isTempPartition, boolean forceDrop) {
         super(AlterOpType.DROP_PARTITION);
         this.ifExists = ifExists;
         this.partitionName = partitionName;
         this.isTempPartition = isTempPartition;
         this.needTableStable = false;
-        this.needCheckCommittedTxns = needCheckCommittedTxns;
+        this.forceDrop = forceDrop;
     }
 
     public boolean isSetIfExists() {
@@ -55,8 +55,8 @@ public class DropPartitionClause extends AlterTableClause {
         return isTempPartition;
     }
 
-    public boolean isNeedCheckCommittedTxns() {
-        return needCheckCommittedTxns;
+    public boolean isForceDrop() {
+        return forceDrop;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/analysis/DropTableStmt.java
@@ -18,7 +18,6 @@
 package org.apache.doris.analysis;
 
 import org.apache.doris.catalog.Catalog;
-import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
@@ -32,20 +31,20 @@ public class DropTableStmt extends DdlStmt {
     private boolean ifExists;
     private final TableName tableName;
     private final boolean isView;
-    private boolean needCheckCommittedTxns;
+    private boolean forceDrop;
 
-    public DropTableStmt(boolean ifExists, TableName tableName, boolean needCheckCommittedTxns) {
+    public DropTableStmt(boolean ifExists, TableName tableName, boolean forceDrop) {
         this.ifExists = ifExists;
         this.tableName = tableName;
         this.isView = false;
-        this.needCheckCommittedTxns = needCheckCommittedTxns;
+        this.forceDrop = forceDrop;
     }
 
-    public DropTableStmt(boolean ifExists, TableName tableName, boolean isView, boolean needCheckCommittedTxns) {
+    public DropTableStmt(boolean ifExists, TableName tableName, boolean isView, boolean forceDrop) {
         this.ifExists = ifExists;
         this.tableName = tableName;
         this.isView = isView;
-        this.needCheckCommittedTxns = needCheckCommittedTxns;
+        this.forceDrop = forceDrop;
     }
 
     public boolean isSetIfExists() {
@@ -64,12 +63,12 @@ public class DropTableStmt extends DdlStmt {
         return isView;
     }
 
-    public boolean isNeedCheckCommittedTxns() {
-        return this.needCheckCommittedTxns;
+    public boolean isForceDrop() {
+        return this.forceDrop;
     }
 
     @Override
-    public void analyze(Analyzer analyzer) throws AnalysisException, UserException {
+    public void analyze(Analyzer analyzer) throws UserException {
         if (Strings.isNullOrEmpty(tableName.getDb())) {
             tableName.setDb(analyzer.getDefaultDb());
         }

fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -166,6 +166,7 @@ import org.apache.doris.persist.BackendTabletsInfo;
 import org.apache.doris.persist.ClusterInfo;
 import org.apache.doris.persist.ColocatePersistInfo;
 import org.apache.doris.persist.DatabaseInfo;
+import org.apache.doris.persist.DropDbInfo;
 import org.apache.doris.persist.DropInfo;
 import org.apache.doris.persist.DropLinkDbAndUpdateDbInfo;
 import org.apache.doris.persist.DropPartitionInfo;
@@ -2678,7 +2679,7 @@ public class Catalog {
             Database db = this.fullNameToDb.get(dbName);
             db.writeLock();
             try {
-                if (stmt.isNeedCheckCommittedTxns()) {
+                if (!stmt.isForceDrop()) {
                     if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), null, null)) {
                        throw new DdlException("There are still some transactions in the COMMITTED state waiting to be completed. " +
                                "The database [" + dbName +"] cannot be dropped. If you want to forcibly drop(cannot be recovered)," +
@@ -2720,8 +2721,8 @@ public class Catalog {
 
                 // save table names for recycling
                 Set<String> tableNames = db.getTableNamesWithLock();
-                unprotectDropDb(db, !stmt.isNeedCheckCommittedTxns());
-                if (stmt.isNeedCheckCommittedTxns()) {
+                unprotectDropDb(db, stmt.isForceDrop());
+                if (!stmt.isForceDrop()) {
                     Catalog.getCurrentRecycleBin().recycleDatabase(db, tableNames);
                 }
             } finally {
@@ -2733,12 +2734,13 @@ public class Catalog {
             fullNameToDb.remove(db.getFullName());
             final Cluster cluster = nameToCluster.get(db.getClusterName());
             cluster.removeDb(dbName, db.getId());
-            editLog.logDropDb(dbName, !stmt.isNeedCheckCommittedTxns());
+            DropDbInfo info = new DropDbInfo(dbName, stmt.isForceDrop());
+            editLog.logDropDb(info);
         } finally {
             unlock();
         }
 
-        LOG.info("finish drop database[{}], is force : {}", dbName, !stmt.isNeedCheckCommittedTxns());
+        LOG.info("finish drop database[{}], is force : {}", dbName, stmt.isForceDrop());
     }
 
     public void unprotectDropDb(Database db, boolean isForeDrop) {
@@ -3333,7 +3335,7 @@ public class Catalog {
         if (isTempPartition) {
             olapTable.dropTempPartition(partitionName, true);
         } else {
-            if (clause.isNeedCheckCommittedTxns()) {
+            if (!clause.isForceDrop()) {
                 Partition partition = olapTable.getPartition(partitionName);
                 if (partition != null) {
                     if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), olapTable.getId(), partition.getId())) {
@@ -3343,14 +3345,14 @@ public class Catalog {
                     }
                 }
             }
-            olapTable.dropPartition(db.getId(), partitionName, !clause.isNeedCheckCommittedTxns());
+            olapTable.dropPartition(db.getId(), partitionName, clause.isForceDrop());
         }
 
         // log
-        DropPartitionInfo info = new DropPartitionInfo(db.getId(), olapTable.getId(), partitionName, isTempPartition, !clause.isNeedCheckCommittedTxns());
+        DropPartitionInfo info = new DropPartitionInfo(db.getId(), olapTable.getId(), partitionName, isTempPartition, clause.isForceDrop());
         editLog.logDropPartition(info);
 
-        LOG.info("succeed in droping partition[{}], is temp : {}, is force : {}", partitionName, isTempPartition, !clause.isNeedCheckCommittedTxns());
+        LOG.info("succeed in droping partition[{}], is temp : {}, is force : {}", partitionName, isTempPartition, clause.isForceDrop());
     }
 
     public void replayDropPartition(DropPartitionInfo info) {
@@ -4305,21 +4307,21 @@ public class Catalog {
                 }
             }
 
-            if (stmt.isNeedCheckCommittedTxns()) {
+            if (!stmt.isForceDrop()) {
                 if (Catalog.getCurrentCatalog().getGlobalTransactionMgr().existCommittedTxns(db.getId(), table.getId(), null)) {
                     throw new DdlException("There are still some transactions in the COMMITTED state waiting to be completed. " +
                             "The table [" + tableName +"] cannot be dropped. If you want to forcibly drop(cannot be recovered)," +
                             " please use \"DROP table FORCE\".");
                 }
             }
-            unprotectDropTable(db, table.getId(), !stmt.isNeedCheckCommittedTxns());
-            DropInfo info = new DropInfo(db.getId(), table.getId(), -1L, !stmt.isNeedCheckCommittedTxns());
+            unprotectDropTable(db, table.getId(), stmt.isForceDrop());
+            DropInfo info = new DropInfo(db.getId(), table.getId(), -1L, stmt.isForceDrop());
             editLog.logDropTable(info);
         } finally {
             db.writeUnlock();
         }
 
-        LOG.info("finished dropping table: {} from db: {}, is force: {}", tableName, dbName, !stmt.isNeedCheckCommittedTxns());
+        LOG.info("finished dropping table: {} from db: {}, is force: {}", tableName, dbName, stmt.isForceDrop());
     }
 
     public boolean unprotectDropTable(Database db, long tableId, boolean isForceDrop) {

fe/fe-core/src/main/java/org/apache/doris/persist/DropDbInfo.java
@@ -56,7 +56,7 @@ public class DropDbInfo implements Writable {
     }
 
     public static DropDbInfo read(DataInput in) throws IOException {
-        if (Catalog.getCurrentCatalogJournalVersion() < FeMetaVersion.VERSION_88) {
+        if (Catalog.getCurrentCatalogJournalVersion() < FeMetaVersion.VERSION_89) {
             DropDbInfo info = new DropDbInfo();
             info.readFields(in);
             return info;

fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -889,8 +889,8 @@ public class EditLog {
         logEdit(OperationType.OP_CREATE_DB, db);
     }
 
-    public void logDropDb(String dbName, boolean isForceDrop) {
-        logEdit(OperationType.OP_DROP_DB, new Text(dbName));
+    public void logDropDb(DropDbInfo dropDbInfo) {
+        logEdit(OperationType.OP_DROP_DB, dropDbInfo);
     }
 
     public void logEraseDb(long dbId) {

fe/fe-core/src/test/java/org/apache/doris/persist/DropDbInfoTest.java
@@ -17,7 +17,6 @@
 
 package org.apache.doris.persist;
 
-import org.apache.doris.catalog.Catalog;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.meta.MetaContext;
 import org.junit.Assert;