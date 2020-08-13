d6028863f3e9d8f401f1dea34a119e48fd21c7fe
ZhangYu0123 <67053339+ZhangYu0123@users.noreply.github.com>
2020-08-13 23:41:46 +0800

    [Compaction] Manually trigger compaction RESTapi interface (#4312)
    Add restapi to be which do compaction task by manual trigger. The detail design in #4311 .

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -256,6 +256,9 @@ namespace config {
     CONF_Bool(disable_storage_page_cache, "false");
 
     // be policy
+    // whether disable automatic compaction task
+    CONF_mBool(disable_auto_compaction, "false");
+
     // CONF_Int64(base_compaction_start_hour, "20");
     // CONF_Int64(base_compaction_end_hour, "7");
     CONF_mInt32(base_compaction_check_interval_seconds, "60");
