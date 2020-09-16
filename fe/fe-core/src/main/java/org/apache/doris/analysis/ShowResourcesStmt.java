3dc6e4323a4aac513041835f335ea844302f5bc2
HappenLee <happenlee@hotmail.com>
2020-09-16 17:57:48 +0800

    [SQL] Support where, limit, order clause in show resourcestmt. (#4502)
    * [SQL] Support where, limit, order clause in show resourcestmt.
    
    Grammar
    
        SHOW RESOURCES
        [
            WHERE
            [NAME [ = "your_resource_name" | LIKE "name_matcher"]]
            [RESOURCETYPE = ["SPARK"]]
        ]
        [ORDER BY ...]
        [LIMIT limit][OFFSET offset];
    
    issue #4501

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ShowResourcesStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ShowResourcesStmt.java
@@ -17,16 +17,244 @@
 
 package org.apache.doris.analysis;
 
+import com.google.common.base.Strings;
 import org.apache.doris.catalog.Column;
+import org.apache.doris.catalog.Resource.ResourceType;
 import org.apache.doris.catalog.ResourceMgr;
 import org.apache.doris.catalog.ScalarType;
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.UserException;
+import org.apache.doris.common.util.OrderByPair;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.ShowResultSetMetaData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// SHOW Resource statement used to show resource of Doris.
+//
+// syntax:
+//      SHOW RESOURCES [LIKE mask]
 public class ShowResourcesStmt extends ShowStmt {
-    public ShowResourcesStmt() {
+    private static final Logger LOG = LogManager.getLogger(ShowResourcesStmt.class);
+
+    private Expr whereClause;
+    private LimitElement limitElement;
+    private List<OrderByElement> orderByElements;
+
+    private String nameValue;
+    private String typeValue;
+    private boolean isAccurateMatch;
+
+    private ArrayList<OrderByPair> orderByPairs;
+
+   public ShowResourcesStmt() {
     }
 
+    public ShowResourcesStmt(Expr labelExpr, List<OrderByElement> orderByElements, LimitElement limitElement) {
+        this.whereClause = labelExpr;
+        this.orderByElements = orderByElements;
+        this.limitElement = limitElement;
+
+        this.nameValue = null;
+        this.typeValue = null;
+        this.isAccurateMatch = false;
+    }
+
+    public ArrayList<OrderByPair> getOrderByPairs() {
+        return this.orderByPairs;
+    }
+
+    public long getLimit() {
+        if (limitElement != null && limitElement.hasLimit()) {
+            return limitElement.getLimit();
+        }
+        return -1L;
+    }
+
+    public long getOffset() {
+        if (limitElement != null && limitElement.hasOffset()) {
+            return limitElement.getOffset();
+        }
+        return -1L;
+    }
+
+    public String getNameValue() {
+        return this.nameValue;
+    }
+
+    public Set<String> getTypeSet() {
+        if (Strings.isNullOrEmpty(typeValue)) {
+            return null;
+        }
+
+        Set<String> resources = new HashSet<>();
+        resources.add(typeValue.toUpperCase());
+
+        return resources;
+    }
+
+    public boolean isAccurateMatch() {
+        return isAccurateMatch;
+    }
+
+    @Override
+    public void analyze(Analyzer analyzer) throws AnalysisException, UserException {
+        super.analyze(analyzer);
+        
+        // analyze where clause if not null
+        if (whereClause != null) {
+            if (whereClause instanceof CompoundPredicate) {
+                CompoundPredicate cp = (CompoundPredicate) whereClause;
+                if (cp.getOp() != org.apache.doris.analysis.CompoundPredicate.Operator.AND) {
+                    throw new AnalysisException("Only allow compound predicate with operator AND");
+                }
+
+                analyzeSubPredicate(cp.getChild(0));
+                analyzeSubPredicate(cp.getChild(1));
+            } else {
+                analyzeSubPredicate(whereClause);
+            }
+        }
+
+        // order by
+        if (orderByElements != null && !orderByElements.isEmpty()) {
+            orderByPairs = new ArrayList<OrderByPair>();
+            for (OrderByElement orderByElement : orderByElements) {
+                if (!(orderByElement.getExpr() instanceof SlotRef)) {
+                    throw new AnalysisException("Should order by column");
+                }
+                SlotRef slotRef = (SlotRef) orderByElement.getExpr();
+                int index = ResourceMgr.analyzeColumn(slotRef.getColumnName());
+                OrderByPair orderByPair = new OrderByPair(index, !orderByElement.getIsAsc());
+                orderByPairs.add(orderByPair);
+            }
+        }
+    }
+
+    private void analyzeSubPredicate(Expr subExpr) throws AnalysisException {
+        if (subExpr == null) {
+            return;
+        }
+
+        boolean valid = true;
+        boolean hasName = false;
+        boolean hasType = false;
+
+        CHECK: {
+            if (subExpr instanceof BinaryPredicate) {
+                BinaryPredicate binaryPredicate = (BinaryPredicate) subExpr;
+                if (binaryPredicate.getOp() != BinaryPredicate.Operator.EQ) {
+                    valid = false;
+                    break CHECK;
+                }
+            } else if (subExpr instanceof LikePredicate) {
+                LikePredicate likePredicate = (LikePredicate) subExpr;
+                if (likePredicate.getOp() != LikePredicate.Operator.LIKE) {
+                    valid = false;
+                    break CHECK;
+                }
+            } else {
+                valid = false;
+                break CHECK;
+            }
+
+            // left child
+            if (!(subExpr.getChild(0) instanceof SlotRef)) {
+                valid = false;
+                break CHECK;
+            }
+            String leftKey = ((SlotRef) subExpr.getChild(0)).getColumnName();
+            if (leftKey.equalsIgnoreCase("Name")) {
+                hasName = true;
+            } else if (leftKey.equalsIgnoreCase("ResourceType")) {
+                hasType = true;
+            } else {
+                valid = false;
+                break CHECK;
+            }
+
+            if (hasType && !(subExpr instanceof BinaryPredicate)) {
+                valid = false;
+                break CHECK;
+            }
+
+            if (hasName && subExpr instanceof BinaryPredicate) {
+                isAccurateMatch = true;
+            }
+
+            // right child
+            if (!(subExpr.getChild(1) instanceof StringLiteral)) {
+                valid = false;
+                break CHECK;
+            }
+
+            String value = ((StringLiteral) subExpr.getChild(1)).getStringValue();
+            if (Strings.isNullOrEmpty(value)) {
+                valid = false;
+                break CHECK;
+            }
+
+            if (hasName) {
+                nameValue = value;
+            } else if (hasType) {
+                typeValue = value.toUpperCase();
+
+                try {
+                    ResourceType.valueOf(typeValue);
+                } catch (Exception e) {
+                    valid = false;
+                    break CHECK;
+                }
+            }
+        }
+
+        if (!valid) {
+            throw new AnalysisException("Where clause should looks like: NAME = \"your_resource_name\","
+                    + " or NAME LIKE \"matcher\", " + " or RESOURCETYPE = \"SPARK\", "
+                    + " or compound predicate with operator AND");
+        }
+    }
+
+    @Override
+    public String toSql() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SHOW RESOURCES");
+
+        if (whereClause != null) {
+            sb.append(" WHERE ").append(whereClause.toSql());
+        }
+
+        // Order By clause
+        if (orderByElements != null) {
+            sb.append(" ORDER BY ");
+            for (int i = 0; i < orderByElements.size(); ++i) {
+                sb.append(orderByElements.get(i).getExpr().toSql());
+                sb.append((orderByElements.get(i).getIsAsc()) ? " ASC" : " DESC");
+                sb.append((i + 1 != orderByElements.size()) ? ", " : "");
+            }
+        }
+
+        if (getLimit() != -1L) {
+            sb.append(" LIMIT ").append(getLimit());
+        }
+
+        if (getOffset() != -1L) {
+            sb.append(" OFFSET ").append(getOffset());
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toSql();
+    }
+    
     @Override
     public ShowResultSetMetaData getMetaData() {
         ShowResultSetMetaData.Builder builder = ShowResultSetMetaData.builder();
