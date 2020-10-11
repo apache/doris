eba595583e923c873295a02599ec47e0bafc68b2
weizuo93 <68884553+weizuo93@users.noreply.github.com>
2020-10-11 11:39:25 +0800

    [Optimize] Optimize the execution model of compaction to limit memory consumption (#4670)
    Currently, there are M threads to do base compaction and N threads to do cumulative compaction for each disk. 
    Too many compaction tasks may run out of memory, so the max concurrency of running compaction tasks
    is limited by semaphore.
    If the running threads cost too much memory, we can't defense it. In addition, reducing concurrency to avoid OOM
    will lead to some compaction tasks can't be executed in time and we may encounter more heavy compaction. 
    Therefore, concurrency limitation is not enough.
    
    The strategy proposed in #3624  may be effective to solve the OOM. 
    
    A CompactionPermitLimiter is used for compaction limitation, and use single-producer/multi-consumer model.
    Producer will try to generate compaction tasks and acquire `permits` for each task. 
    The compaction task which can hold `permits` will be executed in thread pool and each finished task will
    release its `permits`.
    
    `permits` should be applied for before a compaction task can execute. When the sum of `permits`
    held by executing compaction tasks reaches a threshold, subsequent compaction task will be no longer allowed,
    until some `permits` are released. Tablet compaction score is used as `permits` of compaction task here.
    
    To some extent, memory consumption can be limited by setting appropriate `permits` threshold.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -264,12 +264,12 @@ namespace config {
     // be policy
     // whether disable automatic compaction task
     CONF_mBool(disable_auto_compaction, "false");
+    // check the configuration of auto compaction in seconds when auto compaction disabled
+    CONF_mInt32(check_auto_compaction_interval_seconds, "5");
 
     // CONF_Int64(base_compaction_start_hour, "20");
     // CONF_Int64(base_compaction_end_hour, "7");
-    CONF_mInt32(base_compaction_check_interval_seconds, "60");
     CONF_mInt64(base_compaction_num_cumulative_deltas, "5");
-    CONF_Int32(base_compaction_num_threads_per_disk, "1");
     CONF_mDouble(base_cumulative_delta_ratio, "0.3");
     CONF_mInt64(base_compaction_interval_seconds_since_last_operation, "86400");
     CONF_mInt32(base_compaction_write_mbytes_per_sec, "5");
@@ -296,10 +296,8 @@ namespace config {
     CONF_mInt64(cumulative_size_based_compaction_lower_size_mbytes, "64");
 
     // cumulative compaction policy: max delta file's size unit:B
-    CONF_mInt32(cumulative_compaction_check_interval_seconds, "10");
     CONF_mInt64(min_cumulative_compaction_num_singleton_deltas, "5");
     CONF_mInt64(max_cumulative_compaction_num_singleton_deltas, "1000");
-    CONF_Int32(cumulative_compaction_num_threads_per_disk, "1");
     CONF_mInt64(cumulative_compaction_budgeted_bytes, "104857600");
     // CONF_Int32(cumulative_compaction_write_mbytes_per_sec, "100");
     // cumulative compaction skips recently published deltas in order to prevent
@@ -310,13 +308,19 @@ namespace config {
     // if compaction of a tablet failed, this tablet should not be chosen to
     // compaction until this interval passes.
     CONF_mInt64(min_compaction_failure_interval_sec, "600"); // 10 min
-    // Too many compaction tasks may run out of memory.
-    // This config is to limit the max concurrency of running compaction tasks.
-    // -1 means no limit, and the max concurrency will be:
-    //      C = (cumulative_compaction_num_threads_per_disk + base_compaction_num_threads_per_disk) * dir_num
-    // set it to larger than C will be set to equal to C.
-    // This config can be set to 0, which means to forbid any compaction, for some special cases.
-    CONF_Int32(max_compaction_concurrency, "-1");
+
+    // This config can be set to limit thread number in compaction thread pool.
+    CONF_mInt32(min_compaction_threads, "10");
+    CONF_mInt32(max_compaction_threads, "10");
+
+    // The upper limit of "permits" held by all compaction tasks. This config can be set to limit memory consumption for compaction.
+    CONF_mInt64(total_permits_for_compaction_score, "10000");
+
+    // Compaction task number per disk.
+    CONF_mInt32(compaction_task_num_per_disk, "2");
+
+    // How many rounds of cumulative compaction for each round of base compaction when compaction tasks generation.
+    CONF_mInt32(cumulative_compaction_rounds_for_each_base_compaction_round, "9");
 
     // Threshold to logging compaction trace, in seconds.
     CONF_mInt32(base_compaction_trace_threshold, "10");
