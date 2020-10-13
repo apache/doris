ae4c83fd14826271e9a28e7b4c080db0c54c710a
HangyuanLiu <460660596@qq.com>
2020-10-13 19:28:36 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
@@ -334,10 +334,16 @@ public class FunctionCallExpr extends Expr {
                 throw new AnalysisException(fnName.getFunction() + " only used in analytic function");
             } else {
                 if (children.size() > 2) {
-                    if (!getChild(2).isConstant()) {
+                    if (!getChild(1).isConstant() || !getChild(2).isConstant()) {
                         throw new AnalysisException(
                                 "The default parameter (parameter 3) of LAG must be a constant: "
                                         + this.toSql());
+                    }
+                    getChild(1).type = Type.BIGINT;
+                    //To ensure that the correct function is selected,the third parameter
+                    //of the lead function must be consistent with the first parameter
+                    if (!getChild(2).type.matchesType(getChild(0).type) && !getChild(2).type.matchesType(Type.NULL)) {
+                        getChild(2).type = getChild(0).type;
                     }
                 }
                 return;
