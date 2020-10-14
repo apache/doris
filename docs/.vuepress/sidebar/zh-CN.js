1c69eb2050fe1b2b4e30184e7ff150e2a135203e
xueyan.li <astralidea@163.com>
2020-10-14 09:34:58 +0800

    [Docs] update data types doc and fix some typo (#4712)
    * update data types doc and fix some typo
    
    * update data types doc and fix some typo
    
    Co-authored-by: lixueyan07 <lixueyan07@meituan.com>

--- docs/.vuepress/sidebar/zh-CN.js
+++ docs/.vuepress/sidebar/zh-CN.js
@@ -492,6 +492,7 @@ module.exports = [
             directoryPath: "Data Types/",
             children: [
               "BIGINT",
+              "BITMAP",
               "BOOLEAN",
               "CHAR",
               "DATE",
