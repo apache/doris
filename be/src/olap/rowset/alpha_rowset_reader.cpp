apache/doris

commit b9dabc3b5b61845f25f2040f529232767af6bb6b

[Enhance] Push down predicate on value column of unique table  to base rowset (#5022)

be/src/olap/rowset/alpha_rowset_reader.cpp
@@ -354,10 +354,22 @@ OLAPStatus AlphaRowsetReader::_init_merge_ctxs(RowsetReaderContext* read_context
                 continue;
             }
         } else {
+            std::vector<ColumnPredicate*> predicates;
+            if (read_context->predicates != nullptr) {
+                predicates.insert(predicates.end(), read_context->predicates->begin(),
+                                  read_context->predicates->end());
+            }
+            // if unique table with rowset [0-x] or [0-1] [2-y] [...],
+            // value column predicates can be pushdown on rowset [0-x] or [2-y]
+            if (read_context->value_predicates != nullptr && _rowset->keys_type() == UNIQUE_KEYS &&
+                (_rowset->start_version() == 0 || _rowset->start_version() == 2)) {
+                predicates.insert(predicates.end(), read_context->value_predicates->begin(),
+                                  read_context->value_predicates->end());
+            }
             new_column_data->set_read_params(
                     *read_context->return_columns, *read_context->seek_columns,
-                    *read_context->load_bf_columns, *read_context->conditions,
-                    *read_context->predicates, use_index_stream_cache, read_context->runtime_state);
+                    *read_context->load_bf_columns, *read_context->conditions, predicates,
+                    use_index_stream_cache, read_context->runtime_state);
             // filter
             if (new_column_data->rowset_pruning_filter()) {
                 _stats->rows_stats_filtered += new_column_data->num_rows();

be/src/exec/olap_scanner.cpp
@@ -164,8 +164,16 @@ Status OlapScanner::_init_params(const std::vector<OlapScanRange*>& key_ranges,
     // TODO(zc)
     _params.profile = _profile;
     _params.runtime_state = _runtime_state;
-
-    if (_aggregation) {
+    // if the table with rowset [0-x] or [0-1] [2-y], and [0-1] is empty
+    bool single_version =
+            (_params.rs_readers.size() == 1 &&
+             _params.rs_readers[0]->rowset()->start_version() == 0 &&
+             !_params.rs_readers[0]->rowset()->rowset_meta()->is_segments_overlapping()) ||
+            (_params.rs_readers.size() == 2 &&
+             _params.rs_readers[1]->rowset()->rowset_meta()->num_rows() == 0 &&
+             _params.rs_readers[1]->rowset()->start_version() == 2 &&
+             !_params.rs_readers[1]->rowset()->rowset_meta()->is_segments_overlapping());
+    if (_aggregation || single_version) {
         _params.return_columns = _return_columns;
     } else {
         for (size_t i = 0; i < _tablet->num_key_columns(); ++i) {

be/src/olap/iterators.h
@@ -71,7 +71,7 @@ public:
     // used to fiter rows in row block
     // TODO(hkp): refactor the column predicate framework
     // to unify Conditions and ColumnPredicate
-    const std::vector<ColumnPredicate*>* column_predicates = nullptr;
+    std::vector<ColumnPredicate*> column_predicates;
 
     // REQUIRED (null is not allowed)
     OlapReaderStatistics* stats = nullptr;

be/src/olap/reader.cpp
@@ -284,6 +284,9 @@ void Reader::close() {
     for (auto pred : _col_predicates) {
         delete pred;
     }
+    for (auto pred : _value_col_predicates) {
+        delete pred;
+    }
 }
 
 OLAPStatus Reader::_capture_rs_readers(const ReaderParams& read_params) {
@@ -365,6 +368,7 @@ OLAPStatus Reader::_capture_rs_readers(const ReaderParams& read_params) {
     _reader_context.load_bf_columns = &_load_bf_columns;
     _reader_context.conditions = &_conditions;
     _reader_context.predicates = &_col_predicates;
+    _reader_context.value_predicates = &_value_col_predicates;
     _reader_context.lower_bound_keys = &_keys_param.start_keys;
     _reader_context.is_lower_keys_included = &_is_lower_keys_included;
     _reader_context.upper_bound_keys = &_keys_param.end_keys;
@@ -575,7 +579,13 @@ void Reader::_init_conditions_param(const ReaderParams& read_params) {
         DCHECK_EQ(OLAP_SUCCESS, _conditions.append_condition(condition));
         ColumnPredicate* predicate = _parse_to_predicate(condition);
         if (predicate != nullptr) {
-            _col_predicates.push_back(predicate);
+            if (_tablet->tablet_schema()
+                        .column(_tablet->field_index(condition.column_name))
+                        .aggregation() != FieldAggregationMethod::OLAP_FIELD_AGGREGATION_NONE) {
+                _value_col_predicates.push_back(predicate);
+            } else {
+                _col_predicates.push_back(predicate);
+            }
         }
     }
 }
@@ -685,9 +695,6 @@ ColumnPredicate* Reader::_parse_to_predicate(const TCondition& condition) {
         return nullptr;
     }
     const TabletColumn& column = _tablet->tablet_schema().column(index);
-    if (column.aggregation() != FieldAggregationMethod::OLAP_FIELD_AGGREGATION_NONE) {
-        return nullptr;
-    }
     ColumnPredicate* predicate = nullptr;
     if (condition.condition_op == "*=" && condition.condition_values.size() == 1) {
         predicate = _new_eq_pred(column, index, condition.condition_values[0]);
@@ -820,6 +827,7 @@ ColumnPredicate* Reader::_parse_to_predicate(const TCondition& condition) {
     } else if (condition.condition_op == "is") {
         predicate = new NullPredicate(index, condition.condition_values[0] == "null");
     }
+
     return predicate;
 }
 

be/src/olap/reader.h
@@ -171,6 +171,7 @@ private:
     std::vector<bool> _is_upper_keys_included;
     Conditions _conditions;
     std::vector<ColumnPredicate*> _col_predicates;
+    std::vector<ColumnPredicate*> _value_col_predicates;
     DeleteHandler _delete_handler;
 
     OLAPStatus (Reader::*_next_row_func)(RowCursor* row_cursor, MemPool* mem_pool,

be/src/olap/rowset/alpha_rowset.cpp
@@ -310,22 +310,24 @@ OLAPStatus AlphaRowset::init() {
         if (segment_group_meta.zone_maps_size() != 0) {
             size_t zone_maps_size = segment_group_meta.zone_maps_size();
             // after 0.12.10 the value column in duplicate table also has zone map.
-            size_t expect_zone_maps_num = _schema->keys_type() == KeysType::DUP_KEYS
+            // after 0.14 the value column in duplicate table also has zone map.
+            size_t expect_zone_maps_num = _schema->keys_type() != KeysType::AGG_KEYS
                                                   ? _schema->num_columns()
                                                   : _schema->num_key_columns();
-            if ((_schema->keys_type() != KeysType::DUP_KEYS &&
+            if ((_schema->keys_type() == KeysType::AGG_KEYS &&
                  expect_zone_maps_num != zone_maps_size) ||
-                (_schema->keys_type() == KeysType::DUP_KEYS &&
+                (_schema->keys_type() != KeysType::AGG_KEYS &&
                  expect_zone_maps_num < zone_maps_size)) {
-                LOG(ERROR) << "column pruning size is error."
+                LOG(ERROR) << "column pruning size is error. "
                            << "KeysType=" << KeysType_Name(_schema->keys_type()) << ", "
                            << "zone_maps_size=" << zone_maps_size << ", "
                            << "num_key_columns=" << _schema->num_key_columns() << ", "
                            << "num_columns=" << _schema->num_columns();
                 return OLAP_ERR_TABLE_INDEX_VALIDATE_ERROR;
             }
-            // Before 0.12.10, the zone map columns number in duplicate table is the same with the key column numbers,
-            // but after 0.12.10 we build zone map for the value column, so when first start the two number is not the same,
+            // Before 0.12.10, the zone map columns number in duplicate/unique table is the same with the key column numbers,
+            // but after 0.12.10 we build zone map for duplicate table value column, after 0.14 we build zone map for unique
+            // table value column, so when first start the two number is not the same,
             // it causes start failed. When `expect_zone_maps_num > zone_maps_size` it may be the first start after upgrade
             if (expect_zone_maps_num > zone_maps_size) {
                 LOG(WARNING)

be/src/olap/rowset/beta_rowset_reader.cpp
@@ -61,7 +61,19 @@ OLAPStatus BetaRowsetReader::init(RowsetReaderContext* read_context) {
         read_context->delete_handler->get_delete_conditions_after_version(
                 _rowset->end_version(), &read_options.delete_conditions);
     }
-    read_options.column_predicates = read_context->predicates;
+    if (read_context->predicates != nullptr) {
+        read_options.column_predicates.insert(read_options.column_predicates.end(),
+                                              read_context->predicates->begin(),
+                                              read_context->predicates->end());
+    }
+    // if unique table with rowset [0-x] or [0-1] [2-y] [...],
+    // value column predicates can be pushdown on rowset [0-x] or [2-y]
+    if (read_context->value_predicates != nullptr && _rowset->keys_type() == UNIQUE_KEYS &&
+        (_rowset->start_version() == 0 || _rowset->start_version() == 2)) {
+        read_options.column_predicates.insert(read_options.column_predicates.end(),
+                                              read_context->value_predicates->begin(),
+                                              read_context->value_predicates->end());
+    }
     read_options.use_page_cache = read_context->use_page_cache;
 
     // create iterator for each segment

be/src/olap/rowset/rowset.h
@@ -25,6 +25,7 @@
 #include "gen_cpp/olap_file.pb.h"
 #include "gutil/macros.h"
 #include "olap/rowset/rowset_meta.h"
+#include "olap/tablet_schema.h"
 
 namespace doris {
 
@@ -36,7 +37,6 @@ class Rowset;
 using RowsetSharedPtr = std::shared_ptr<Rowset>;
 class RowsetFactory;
 class RowsetReader;
-class TabletSchema;
 
 // the rowset state transfer graph:
 //    ROWSET_UNLOADED    <--|
@@ -159,6 +159,7 @@ public:
     bool delete_flag() const { return rowset_meta()->delete_flag(); }
     int64_t num_segments() const { return rowset_meta()->num_segments(); }
     void to_rowset_pb(RowsetMetaPB* rs_meta) { return rowset_meta()->to_rowset_pb(rs_meta); }
+    inline KeysType keys_type() { return _schema->keys_type(); }
 
     // remove all files in this rowset
     // TODO should we rename the method to remove_files() to be more specific?

commit f726014e0fe5c96d89f1616f79467b155b921a6b

fix a core dump caused by pr #5022

be/CMakeLists.txt
@@ -300,7 +300,7 @@ set(CXX_GCC_FLAGS "${CXX_GCC_FLAGS} -g -Wno-unused-local-typedefs")
 # Debug information is stored as dwarf2 to be as compatible as possible
 #   -Werror: compile warnings should be errors when using the toolchain compiler.
 # Only enable for debug builds because this is what we test in pre-commit tests.
-set(CXX_FLAGS_DEBUG "${CXX_GCC_FLAGS} -Werror -ggdb -O0 -gdwarf-2")
+set(CXX_FLAGS_DEBUG "${CXX_GCC_FLAGS} -Werror -ggdb3 -O0 -gdwarf-2")
 
 # For CMAKE_BUILD_TYPE=Release
 #   -O3: Enable all compiler optimizations

be/src/olap/rowset/alpha_rowset_reader.cpp
@@ -354,16 +354,16 @@ OLAPStatus AlphaRowsetReader::_init_merge_ctxs(RowsetReaderContext* read_context
                 continue;
             }
         } else {
-            std::vector<ColumnPredicate*> predicates;
+            std::shared_ptr<std::vector<ColumnPredicate*>> predicates(new std::vector<ColumnPredicate*>());
             if (read_context->predicates != nullptr) {
-                predicates.insert(predicates.end(), read_context->predicates->begin(),
+                predicates->insert(predicates->end(), read_context->predicates->begin(),
                                   read_context->predicates->end());
             }
             // if unique table with rowset [0-x] or [0-1] [2-y] [...],
             // value column predicates can be pushdown on rowset [0-x] or [2-y]
             if (read_context->value_predicates != nullptr && _rowset->keys_type() == UNIQUE_KEYS &&
                 (_rowset->start_version() == 0 || _rowset->start_version() == 2)) {
-                predicates.insert(predicates.end(), read_context->value_predicates->begin(),
+                predicates->insert(predicates->end(), read_context->value_predicates->begin(),
                                   read_context->value_predicates->end());
             }
             new_column_data->set_read_params(

be/src/olap/rowset/column_data.cpp
@@ -418,11 +418,11 @@ void ColumnData::set_read_params(const std::vector<uint32_t>& return_columns,
                                  const std::vector<uint32_t>& seek_columns,
                                  const std::set<uint32_t>& load_bf_columns,
                                  const Conditions& conditions,
-                                 const std::vector<ColumnPredicate*>& col_predicates,
+                                 std::shared_ptr<std::vector<ColumnPredicate*>> col_predicates,
                                  bool is_using_cache, RuntimeState* runtime_state) {
     _conditions = &conditions;
-    _col_predicates = &col_predicates;
-    _need_eval_predicates = !col_predicates.empty();
+    _col_predicates = col_predicates;
+    _need_eval_predicates = !col_predicates->empty();
     _is_using_cache = is_using_cache;
     _runtime_state = runtime_state;
     _return_columns = return_columns;

be/src/olap/rowset/column_data.h
@@ -62,7 +62,7 @@ public:
     void set_read_params(const std::vector<uint32_t>& return_columns,
                          const std::vector<uint32_t>& seek_columns,
                          const std::set<uint32_t>& load_bf_columns, const Conditions& conditions,
-                         const std::vector<ColumnPredicate*>& col_predicates, bool is_using_cache,
+                         std::shared_ptr<std::vector<ColumnPredicate*>> col_predicates, bool is_using_cache,
                          RuntimeState* runtime_state);
 
     OLAPStatus get_first_row_block(RowBlock** row_block);
@@ -143,7 +143,7 @@ private:
     // 当到达文件末尾或者到达end key时设置此标志
     bool _eof;
     const Conditions* _conditions;
-    const std::vector<ColumnPredicate*>* _col_predicates;
+    std::shared_ptr<std::vector<ColumnPredicate*>> _col_predicates;
     const DeleteHandler* _delete_handler = nullptr;
     DelCondSatisfied _delete_status;
     RuntimeState* _runtime_state;