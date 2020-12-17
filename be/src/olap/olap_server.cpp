apache/doris

commit 3f57c8c3545f7d51f453683b042670b8f227581f

[Refactor] Execute 'pick rowsets' before applying for permits for a compaction task (#4891)
The current compaction mechanism is that there is a producer thread that has been producing compaction tasks,
and the selected tablet must apply for `permits`.
When a tablet could hold `permits`, compaction task for this tablet will be submitted to  thread pool.
We take compaction score as `permits` which is used for limiting memory consumption.
However,  `pick_rowset_to_compaction()` will be executed before the file merge in compaction thread,
and the number of segment files that actually perform the merge operation is smaller than compaction score.
In addition, it is also possible that compaction task exits directly because the tablet doesn't meet
the requirements of compaction. 

This patch optimizes and refactors the code of compaction, so that we can execute 'pick rowsets'
before applying for permits for a compaction task, calculate the number of segment files that actually
participate in the merge operation, and take this number as `permits`.

be/src/olap/olap_server.cpp
@@ -345,7 +345,10 @@ void StorageEngine::_compaction_tasks_producer_callback() {
                 continue;
             }
             for (const auto& tablet : tablets_compaction) {
-                int64_t permits = tablet->calc_compaction_score(compaction_type);
+                int64_t permits = tablet->prepare_compaction_and_calculate_permits(compaction_type, tablet);
+                if (permits == 0) {
+                    continue;
+                }
                 if (_permit_limiter.request(permits)) {
                     {
                         // Push to _tablet_submitted_compaction before submitting task
@@ -353,40 +356,24 @@ void StorageEngine::_compaction_tasks_producer_callback() {
                         _tablet_submitted_compaction[tablet->data_dir()].emplace_back(
                                 tablet->tablet_id());
                     }
-                    if (compaction_type == CompactionType::CUMULATIVE_COMPACTION) {
-                        _compaction_thread_pool->submit_func([=]() {
-                            CgroupsMgr::apply_system_cgroup();
-                            _perform_cumulative_compaction(tablet);
-                            _permit_limiter.release(permits);
-                            std::unique_lock<std::mutex> lock(_tablet_submitted_compaction_mutex);
-                            std::vector<TTabletId>::iterator it_tablet =
-                                    find(_tablet_submitted_compaction[tablet->data_dir()].begin(),
-                                         _tablet_submitted_compaction[tablet->data_dir()].end(),
-                                         tablet->tablet_id());
-                            if (it_tablet !=
-                                _tablet_submitted_compaction[tablet->data_dir()].end()) {
-                                _tablet_submitted_compaction[tablet->data_dir()].erase(it_tablet);
-                                _wakeup_producer_flag = 1;
-                                _compaction_producer_sleep_cv.notify_one();
-                            }
-                        });
-                    } else {
-                        _compaction_thread_pool->submit_func([=]() {
-                            CgroupsMgr::apply_system_cgroup();
-                            _perform_base_compaction(tablet);
-                            _permit_limiter.release(permits);
-                            std::unique_lock<std::mutex> lock(_tablet_submitted_compaction_mutex);
-                            std::vector<TTabletId>::iterator it_tablet =
-                                    find(_tablet_submitted_compaction[tablet->data_dir()].begin(),
-                                         _tablet_submitted_compaction[tablet->data_dir()].end(),
-                                         tablet->tablet_id());
-                            if (it_tablet !=
-                                _tablet_submitted_compaction[tablet->data_dir()].end()) {
-                                _tablet_submitted_compaction[tablet->data_dir()].erase(it_tablet);
-                                _wakeup_producer_flag = 1;
-                                _compaction_producer_sleep_cv.notify_one();
-                            }
-                        });
+                    auto st =_compaction_thread_pool->submit_func([=]() {
+                      CgroupsMgr::apply_system_cgroup();
+                      tablet->execute_compaction(compaction_type);
+                      _permit_limiter.release(permits);
+                      std::unique_lock<std::mutex> lock(_tablet_submitted_compaction_mutex);
+                      std::vector<TTabletId>::iterator it_tablet =
+                              find(_tablet_submitted_compaction[tablet->data_dir()].begin(),
+                                   _tablet_submitted_compaction[tablet->data_dir()].end(),
+                                   tablet->tablet_id());
+                      if (it_tablet !=
+                          _tablet_submitted_compaction[tablet->data_dir()].end()) {
+                          _tablet_submitted_compaction[tablet->data_dir()].erase(it_tablet);
+                          _wakeup_producer_flag = 1;
+                          _compaction_producer_sleep_cv.notify_one();
+                      }
+                    });
+                    if (!st.ok()) {
+                        _permit_limiter.release(permits);
                     }
                 }
             }

be/src/olap/tablet.cpp
@@ -28,6 +28,8 @@
 #include <map>
 #include <set>
 
+#include "olap/base_compaction.h"
+#include "olap/cumulative_compaction.h"
 #include "olap/olap_common.h"
 #include "olap/olap_define.h"
 #include "olap/reader.h"
@@ -38,7 +40,9 @@
 #include "olap/tablet_meta_manager.h"
 #include "util/path_util.h"
 #include "util/pretty_printer.h"
+#include "util/scoped_cleanup.h"
 #include "util/time.h"
+#include "util/trace.h"
 
 namespace doris {
 
@@ -67,7 +71,8 @@ Tablet::Tablet(TabletMetaSharedPtr tablet_meta, DataDir* data_dir,
           _cumulative_point(K_INVALID_CUMULATIVE_POINT),
           _cumulative_compaction_type(cumulative_compaction_type),
           _last_record_scan_count(0),
-          _last_record_scan_count_timestamp(time(nullptr)) {
+          _last_record_scan_count_timestamp(time(nullptr)),
+          _is_clone_occurred(false) {
     // construct _timestamped_versioned_tracker from rs and stale rs meta
     _timestamped_version_tracker.construct_versioned_tracker(_tablet_meta->all_rs_metas(),
                                                              _tablet_meta->all_stale_rs_metas());
@@ -1320,4 +1325,107 @@ double Tablet::calculate_scan_frequency() {
     return scan_frequency;
 }
 
-} // namespace doris
+int64_t Tablet::prepare_compaction_and_calculate_permits(CompactionType compaction_type,
+                                                         TabletSharedPtr tablet) {
+    std::vector<RowsetSharedPtr> compaction_rowsets;
+    if (compaction_type == CompactionType::CUMULATIVE_COMPACTION) {
+        scoped_refptr<Trace> trace(new Trace);
+        MonotonicStopWatch watch;
+        watch.start();
+        SCOPED_CLEANUP({
+            if (watch.elapsed_time() / 1e9 > config::cumulative_compaction_trace_threshold) {
+                LOG(WARNING) << "Trace:" << std::endl << trace->DumpToString(Trace::INCLUDE_ALL);
+            }
+        });
+        ADOPT_TRACE(trace.get());
+
+        TRACE("create cumulative compaction");
+        StorageEngine::instance()->create_cumulative_compaction(tablet, _cumulative_compaction);
+        DorisMetrics::instance()->cumulative_compaction_request_total->increment(1);
+        OLAPStatus res = _cumulative_compaction->prepare_compact();
+        if (res != OLAP_SUCCESS) {
+            return 0;
+        }
+        compaction_rowsets = _cumulative_compaction->get_input_rowsets();
+    } else {
+        DCHECK_EQ(compaction_type, CompactionType::BASE_COMPACTION);
+        scoped_refptr<Trace> trace(new Trace);
+        MonotonicStopWatch watch;
+        watch.start();
+        SCOPED_CLEANUP({
+            if (watch.elapsed_time() / 1e9 > config::base_compaction_trace_threshold) {
+                LOG(WARNING) << "Trace:" << std::endl << trace->DumpToString(Trace::INCLUDE_ALL);
+            }
+        });
+        ADOPT_TRACE(trace.get());
+
+        TRACE("create base compaction");
+        StorageEngine::instance()->create_base_compaction(tablet, _base_compaction);
+        DorisMetrics::instance()->base_compaction_request_total->increment(1);
+        OLAPStatus res = _base_compaction->prepare_compact();
+        if (res != OLAP_SUCCESS) {
+            set_last_base_compaction_failure_time(UnixMillis());
+            DorisMetrics::instance()->base_compaction_request_failed->increment(1);
+            if (res != OLAP_ERR_BE_NO_SUITABLE_VERSION) {
+                LOG(WARNING) << "failed to pick rowsets for base compaction. res=" << res
+                             << ", tablet=" << full_name();
+            }
+            return 0;
+        }
+        compaction_rowsets = _base_compaction->get_input_rowsets();
+    }
+    int64_t permits = 0;
+    for (auto rowset : compaction_rowsets) {
+        permits += rowset->rowset_meta()->get_compaction_score();
+    }
+    return permits;
+}
+
+void Tablet::execute_compaction(CompactionType compaction_type) {
+    if (compaction_type == CompactionType::CUMULATIVE_COMPACTION) {
+        scoped_refptr<Trace> trace(new Trace);
+        MonotonicStopWatch watch;
+        watch.start();
+        SCOPED_CLEANUP({
+            if (watch.elapsed_time() / 1e9 > config::cumulative_compaction_trace_threshold) {
+                LOG(WARNING) << "Trace:" << std::endl << trace->DumpToString(Trace::INCLUDE_ALL);
+            }
+        });
+        ADOPT_TRACE(trace.get());
+
+        TRACE("execute cumulative compaction");
+        OLAPStatus res = _cumulative_compaction->execute_compact();
+        if (res != OLAP_SUCCESS) {
+            set_last_cumu_compaction_failure_time(UnixMillis());
+            DorisMetrics::instance()->cumulative_compaction_request_failed->increment(1);
+            LOG(WARNING) << "failed to do cumulative compaction. res=" << res
+                         << ", tablet=" << full_name();
+            return;
+        }
+        set_last_cumu_compaction_failure_time(0);
+    } else {
+        DCHECK_EQ(compaction_type, CompactionType::BASE_COMPACTION);
+        scoped_refptr<Trace> trace(new Trace);
+        MonotonicStopWatch watch;
+        watch.start();
+        SCOPED_CLEANUP({
+            if (watch.elapsed_time() / 1e9 > config::base_compaction_trace_threshold) {
+                LOG(WARNING) << "Trace:" << std::endl << trace->DumpToString(Trace::INCLUDE_ALL);
+            }
+        });
+        ADOPT_TRACE(trace.get());
+
+        TRACE("create base compaction");
+        OLAPStatus res = _base_compaction->execute_compact();
+        if (res != OLAP_SUCCESS) {
+            set_last_base_compaction_failure_time(UnixMillis());
+            DorisMetrics::instance()->base_compaction_request_failed->increment(1);
+            LOG(WARNING) << "failed to do base compaction. res=" << res
+                         << ", tablet=" << full_name();
+            return;
+        }
+        set_last_base_compaction_failure_time(0);
+    }
+}
+
+}  // namespace doris

be/src/olap/tablet.h
@@ -45,6 +45,8 @@ class DataDir;
 class Tablet;
 class TabletMeta;
 class CumulativeCompactionPolicy;
+class CumulativeCompaction;
+class BaseCompaction;
 
 using TabletSharedPtr = std::shared_ptr<Tablet>;
 
@@ -242,6 +244,12 @@ public:
 
     double calculate_scan_frequency();
 
+    int64_t prepare_compaction_and_calculate_permits(CompactionType compaction_type, TabletSharedPtr tablet);
+    void execute_compaction(CompactionType compaction_type);
+
+    void set_clone_occurred(bool clone_occurred) { _is_clone_occurred = clone_occurred; }
+    bool get_clone_occurred() { return _is_clone_occurred; }
+
 private:
     OLAPStatus _init_once_action();
     void _print_missed_versions(const std::vector<Version>& missed_versions) const;
@@ -320,6 +328,11 @@ private:
     int64_t _last_record_scan_count;
     // the timestamp of the last record.
     time_t _last_record_scan_count_timestamp;
+
+    std::shared_ptr<CumulativeCompaction> _cumulative_compaction;
+    std::shared_ptr<BaseCompaction> _base_compaction;
+    // whether clone task occurred during the tablet is in thread pool queue to wait for compaction
+    std::atomic<bool> _is_clone_occurred;
 
     DISALLOW_COPY_AND_ASSIGN(Tablet);
 

be/src/olap/base_compaction.cpp
@@ -29,6 +29,13 @@ BaseCompaction::BaseCompaction(TabletSharedPtr tablet, const std::string& label,
 BaseCompaction::~BaseCompaction() {}
 
 OLAPStatus BaseCompaction::compact() {
+    RETURN_NOT_OK(prepare_compact());
+    RETURN_NOT_OK(execute_compact());
+
+    return OLAP_SUCCESS;
+}
+
+OLAPStatus BaseCompaction::prepare_compact() {
     if (!_tablet->init_succeeded()) {
         return OLAP_ERR_INPUT_PARAMETER_ERROR;
     }
@@ -44,9 +51,28 @@ OLAPStatus BaseCompaction::compact() {
     RETURN_NOT_OK(pick_rowsets_to_compact());
     TRACE("rowsets picked");
     TRACE_COUNTER_INCREMENT("input_rowsets_count", _input_rowsets.size());
+    _tablet->set_clone_occurred(false);
+
+    return OLAP_SUCCESS;
+}
+
+OLAPStatus BaseCompaction::execute_compact() {
+    MutexLock lock(_tablet->get_base_lock(), TRY_LOCK);
+    if (!lock.own_lock()) {
+        LOG(WARNING) << "another base compaction is running. tablet=" << _tablet->full_name();
+        return OLAP_ERR_BE_TRY_BE_LOCK_ERROR;
+    }
+    TRACE("got base compaction lock");
+
+    // Clone task may happen after compaction task is submitted to thread pool, and rowsets picked
+    // for compaction may change. In this case, current compaction task should not be executed.
+    if (_tablet->get_clone_occurred()) {
+        _tablet->set_clone_occurred(false);
+        return OLAP_ERR_BE_CLONE_OCCURRED;
+    }
 
     // 2. do base compaction, merge rowsets
-    int64_t permits = _tablet->calc_compaction_score(CompactionType::BASE_COMPACTION);
+    int64_t permits = get_compaction_permits();
     RETURN_NOT_OK(do_compaction(permits));
     TRACE("compaction finished");
 

be/src/olap/base_compaction.h
@@ -35,6 +35,11 @@ public:
 
     OLAPStatus compact() override;
 
+    OLAPStatus prepare_compact() override;
+    OLAPStatus execute_compact() override;
+
+    std::vector<RowsetSharedPtr> get_input_rowsets() { return _input_rowsets; }
+
 protected:
     OLAPStatus pick_rowsets_to_compact() override;
     std::string compaction_name() const override { return "base compaction"; }

be/src/olap/compaction.cpp
@@ -66,8 +66,8 @@ OLAPStatus Compaction::do_compaction_impl(int64_t permits) {
     _tablet->compute_version_hash_from_rowsets(_input_rowsets, &_output_version_hash);
 
     LOG(INFO) << "start " << compaction_name() << ". tablet=" << _tablet->full_name()
-              << ", output version is=" << _output_version.first << "-" << _output_version.second
-              << ", score: " << permits;
+              << ", output_version=" << _output_version.first << "-" << _output_version.second
+              << ", permits: " << permits;
 
     RETURN_NOT_OK(construct_output_rowset_writer());
     RETURN_NOT_OK(construct_input_rowset_readers());
@@ -268,4 +268,12 @@ int64_t Compaction::_get_input_num_rows_from_seg_grps() {
     return num_rows;
 }
 
-} // namespace doris
+int64_t Compaction::get_compaction_permits() {
+    int64_t permits = 0;
+    for (auto rowset : _input_rowsets) {
+        permits += rowset->rowset_meta()->get_compaction_score();
+    }
+    return permits;
+}
+
+}  // namespace doris

be/src/olap/compaction.h
@@ -50,6 +50,9 @@ public:
 
     virtual OLAPStatus compact() = 0;
 
+    virtual OLAPStatus prepare_compact() = 0;
+    virtual OLAPStatus execute_compact() = 0;
+
 protected:
     virtual OLAPStatus pick_rowsets_to_compact() = 0;
     virtual std::string compaction_name() const = 0;
@@ -68,6 +71,7 @@ protected:
     OLAPStatus check_correctness(const Merger::Statistics& stats);
     OLAPStatus find_longest_consecutive_version(std::vector<RowsetSharedPtr>* rowsets,
                                                 std::vector<Version>* missing_version);
+    int64_t get_compaction_permits();
 
 private:
     // get num rows from segment group meta of input rowsets.

be/src/olap/cumulative_compaction.cpp
@@ -31,6 +31,13 @@ CumulativeCompaction::CumulativeCompaction(TabletSharedPtr tablet, const std::st
 CumulativeCompaction::~CumulativeCompaction() {}
 
 OLAPStatus CumulativeCompaction::compact() {
+    RETURN_NOT_OK(prepare_compact());
+    RETURN_NOT_OK(execute_compact());
+
+    return OLAP_SUCCESS;
+}
+
+OLAPStatus CumulativeCompaction::prepare_compact() {
     if (!_tablet->init_succeeded()) {
         return OLAP_ERR_CUMULATIVE_INVALID_PARAMETERS;
     }
@@ -52,9 +59,28 @@ OLAPStatus CumulativeCompaction::compact() {
     RETURN_NOT_OK(pick_rowsets_to_compact());
     TRACE("rowsets picked");
     TRACE_COUNTER_INCREMENT("input_rowsets_count", _input_rowsets.size());
+    _tablet->set_clone_occurred(false);
+
+    return OLAP_SUCCESS;
+}
+
+OLAPStatus CumulativeCompaction::execute_compact() {
+    MutexLock lock(_tablet->get_cumulative_lock(), TRY_LOCK);
+    if (!lock.own_lock()) {
+        LOG(INFO) << "The tablet is under cumulative compaction. tablet=" << _tablet->full_name();
+        return OLAP_ERR_CE_TRY_CE_LOCK_ERROR;
+    }
+    TRACE("got cumulative compaction lock");
+
+    // Clone task may happen after compaction task is submitted to thread pool, and rowsets picked
+    // for compaction may change. In this case, current compaction task should not be executed.
+    if (_tablet->get_clone_occurred()) {
+        _tablet->set_clone_occurred(false);
+        return OLAP_ERR_CUMULATIVE_CLONE_OCCURRED;
+    }
 
     // 3. do cumulative compaction, merge rowsets
-    int64_t permits = _tablet->calc_compaction_score(CompactionType::CUMULATIVE_COMPACTION);
+    int64_t permits = get_compaction_permits();
     RETURN_NOT_OK(do_compaction(permits));
     TRACE("compaction finished");
 

commit ed77b31ba0718b3c2f7acf87c01f9ffaa576daec

[Bug] Fix tablet shared ptr circular reference causing the tablet not to be cleared
Regardless of whether the tablet is submitted for compaction or not,
we need to call 'reset_compaction' to clean up the base_compaction or cumulative_compaction objects
in the tablet, because these two objects store the tablet's own shared_ptr.
If it is not cleaned up, the reference count of the tablet will always be greater than 1,
thus cannot be collected by the garbage collector. (TabletManager::start_trash_sweep)

This bug is introduced from #4891

be/src/olap/olap_server.cpp
@@ -344,12 +344,15 @@ void StorageEngine::_compaction_tasks_producer_callback() {
                                                        [=] { return _wakeup_producer_flag == 1; });
                 continue;
             }
+
+            /// Regardless of whether the tablet is submitted for compaction or not,
+            /// we need to call 'reset_compaction' to clean up the base_compaction or cumulative_compaction objects
+            /// in the tablet, because these two objects store the tablet's own shared_ptr.
+            /// If it is not cleaned up, the reference count of the tablet will always be greater than 1,
+            /// thus cannot be collected by the garbage collector. (TabletManager::start_trash_sweep)
             for (const auto& tablet : tablets_compaction) {
                 int64_t permits = tablet->prepare_compaction_and_calculate_permits(compaction_type, tablet);
-                if (permits == 0) {
-                    continue;
-                }
-                if (_permit_limiter.request(permits)) {
+                if (permits > 0 && _permit_limiter.request(permits)) {
                     {
                         // Push to _tablet_submitted_compaction before submitting task
                         std::unique_lock<std::mutex> lock(_tablet_submitted_compaction_mutex);
@@ -371,10 +374,17 @@ void StorageEngine::_compaction_tasks_producer_callback() {
                           _wakeup_producer_flag = 1;
                           _compaction_producer_sleep_cv.notify_one();
                       }
+                      // reset compaction
+                      tablet->reset_compaction(compaction_type); 
                     });
                     if (!st.ok()) {
                         _permit_limiter.release(permits);
+                        // reset compaction
+                        tablet->reset_compaction(compaction_type); 
                     }
+                } else {
+                    // reset compaction
+                    tablet->reset_compaction(compaction_type);
                 }
             }
         } else {

be/src/olap/tablet.cpp
@@ -1426,4 +1426,12 @@ void Tablet::execute_compaction(CompactionType compaction_type) {
     }
 }
 
+void Tablet::reset_compaction(CompactionType compaction_type) {
+    if (compaction_type == CompactionType::CUMULATIVE_COMPACTION) {
+        _cumulative_compaction.reset();
+    } else {
+        _base_compaction.reset();
+    }
+}
+
 }  // namespace doris

be/src/olap/tablet.h
@@ -246,6 +246,7 @@ public:
 
     int64_t prepare_compaction_and_calculate_permits(CompactionType compaction_type, TabletSharedPtr tablet);
     void execute_compaction(CompactionType compaction_type);
+    void reset_compaction(CompactionType compaction_type);
 
     void set_clone_occurred(bool clone_occurred) { _is_clone_occurred = clone_occurred; }
     bool get_clone_occurred() { return _is_clone_occurred; }