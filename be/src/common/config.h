5d6a1a72903ed042823a0a4d9887f15f65f9fff1
HuangWei <huangwei@apache.org>
2021-01-09 23:40:51 +0800

    [Load] support ignoring eovercrowded when tablet sink (#5156)
    If adding the ignore_eovercrowded flag, the `PTabletWriterAddBatchRequest`
    won't failed on `EOVERCROWDED` to avoid load jobs failed in this error.
    It only effects the NodeChannel(the load job), other rpc requests will still check if overcrowded.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -330,8 +330,12 @@ CONF_mInt64(streaming_load_json_max_mb, "100");
 CONF_mInt32(streaming_load_rpc_max_alive_time_sec, "1200");
 // the timeout of a rpc to open the tablet writer in remote BE.
 // short operation time, can set a short timeout
-CONF_mInt32(tablet_writer_open_rpc_timeout_sec, "60");
+CONF_Int32(tablet_writer_open_rpc_timeout_sec, "60");
+// You can ignore brpc error '[E1011]The server is overcrowded' when writing data.
+CONF_mBool(tablet_writer_ignore_eovercrowded, "false");
+
 // OlapTableSink sender's send interval, should be less than the real response time of a tablet writer rpc.
+// You may need to lower the speed when the sink receiver bes are too busy.
 CONF_mInt32(olap_table_sink_send_interval_ms, "1");
 
 // Fragment thread pool
