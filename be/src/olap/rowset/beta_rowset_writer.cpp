4d7dbd732913815e703ae7ef10feabae3f490f00
morningman <chenmingyu@baidu.com>
2021-01-22 09:43:47 +0800

    fix bug

--- be/src/olap/rowset/beta_rowset_writer.cpp
+++ be/src/olap/rowset/beta_rowset_writer.cpp
@@ -142,10 +142,15 @@ OLAPStatus BetaRowsetWriter::flush_single_memtable(MemTable* memtable, int64_t* 
     // Create segment writer for each memtable, so that
     // all memtables can be flushed in parallel.
     std::unique_ptr<segment_v2::SegmentWriter> writer;
-    RETURN_NOT_OK(_create_segment_writer(&writer));
 
     MemTable::Iterator it(memtable);
-    for (it.seek_to_first(); it.valid(); it.next()) {
+    it.seek_to_first();
+    if (it.valid()) {
+        // Only create writer if memtable has data.
+        // Because we do not allow to flush a empty segment writer.
+        RETURN_NOT_OK(_create_segment_writer(&writer));
+    }
+    for ( ; it.valid(); it.next()) {
         ContiguousRow dst_row = it.get_current_row();
         auto s = writer->append_row(dst_row);
         if (PREDICT_FALSE(!s.ok())) {
