aa328e5c76c2700e172fb8299d68d82c2144f018
HappenLee <happenlee@hotmail.com>
2021-02-03 14:05:23 +0800

    [Bug] Fix bug of create ODBC table first column is float or double, issue#5350

--- fe/fe-core/src/test/java/org/apache/doris/planner/QueryPlanTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/planner/QueryPlanTest.java
@@ -362,7 +362,7 @@ public class QueryPlanTest {
 
         Config.enable_odbc_table = true;
         createTable("create external table test.odbc_oracle\n" +
-                "(k1 int, k2 int)\n" +
+                "(k1 float, k2 int)\n" +
                 "ENGINE=ODBC\n" +
                 "PROPERTIES (\n" +
                 "\"host\" = \"127.0.0.1\",\n" +
