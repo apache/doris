9c46432a8285619b0af5a4b920050f1e30f483c1
Tan Hao <tanhaoyc@gmail.com>
2021-02-05 15:13:38 +0800

    Fix null type

--- be/src/exec/es/es_scroll_parser.cpp
+++ be/src/exec/es/es_scroll_parser.cpp
@@ -319,6 +319,11 @@ Status ScrollParser::fill_tuple(const TupleDescriptor* tuple_desc, Tuple* tuple,
 
         void* slot = tuple->get_slot(slot_desc->tuple_offset());
         PrimitiveType type = slot_desc->type().type;
+
+        if (col.IsNull()) {
+            slot = nullptr;
+            continue;
+        }
         switch (type) {
         case TYPE_CHAR:
         case TYPE_VARCHAR: {
