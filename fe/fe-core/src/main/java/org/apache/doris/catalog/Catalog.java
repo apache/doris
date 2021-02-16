493ea97d107050c33be8230f533fb1f58cc1672d
xxiao2018 <78575849+xxiao2018@users.noreply.github.com>
2021-02-16 22:34:48 +0800

    [Bug] Fix NPE when replaying modify table property (#5378)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -5410,7 +5410,8 @@ public class Catalog {
         try {
             TableProperty tableProperty = olapTable.getTableProperty();
             if (tableProperty == null) {
-                olapTable.setTableProperty(new TableProperty(properties).buildProperty(opCode));
+                tableProperty = new TableProperty(properties).buildProperty(opCode);
+                olapTable.setTableProperty(tableProperty);
             } else {
                 tableProperty.modifyTableProperties(properties);
                 tableProperty.buildProperty(opCode);
