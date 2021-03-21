2f500152fe9dd69159e48d425dbc18adef5c311e
yangzhg <yangzhgg@gmail.com>
2021-03-21 17:42:03 +0800

    0.14.0-release

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="trunk"
+build_version="0.14.0-release"
 
 unset LANG
 unset LC_CTYPE
