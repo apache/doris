9597a5f9016d04ef92045ac72cbe26cb9467eeca
Userwhite <925945594@qq.com>
2021-04-13 13:51:18 +0800

    modify the type

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -451,7 +451,7 @@ public class StmtExecutor {
                         originStmt, context.getStmtId(), parser.getErrorMsg(originStmt.originStmt), e);
                 throw new AnalysisException("Unexpected exception: " + e.getMessage());
             }
-            
+	    
 	    analyzeVariablesInStmt();
         }
         redirectStatus = parsedStmt.getRedirectStatus();
