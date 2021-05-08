apache/doris

commit 859f4bf3eeed6d851b7c0b2e5a5a6909278b742e

[Bug-fix] Fix the replay error in stream load manager (#5722)
The previous replay logic does not record the size of the map, which eventually resulted in EOF when reading the log.
This pr replaces the replay logic directly with json.

At the same time, the replay logic of image is supplemented.
The pr ensure that the attributes 'lastStreamLoadTime' of backend can be correctly recorded in the image.

fe/fe-core/src/main/java/org/apache/doris/system/Backend.java
@@ -24,14 +24,15 @@ import org.apache.doris.catalog.DiskInfo.DiskState;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonUtils;
 import org.apache.doris.system.HeartbeatResponse.HbStatus;
 import org.apache.doris.thrift.TDisk;
+import org.apache.doris.thrift.TStorageMedium;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import org.apache.doris.thrift.TStorageMedium;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -42,6 +43,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.google.gson.annotations.SerializedName;
 
 /**
  * This class extends the primary identifier of a Backend with ephemeral state,
@@ -57,28 +60,43 @@ public class Backend implements Writable {
 
     private static final Logger LOG = LogManager.getLogger(Backend.class);
 
+    @SerializedName("id")
     private long id;
+    @SerializedName("host")
     private String host;
     private String version;
 
+    @SerializedName("heartbeatPort")
     private int heartbeatPort; // heartbeat
+    @SerializedName("bePort")
     private volatile int bePort; // be
+    @SerializedName("httpPort")
     private volatile int httpPort; // web service
+    @SerializedName("beRpcPort")
     private volatile int beRpcPort; // be rpc port
+    @SerializedName("brpcPort")
     private volatile int brpcPort = -1;
 
+    @SerializedName("lastUpdateMs")
     private volatile long lastUpdateMs;
+    @SerializedName("lastStartTime")
     private volatile long lastStartTime;
+    @SerializedName("isAlive")
     private AtomicBoolean isAlive;
 
+    @SerializedName("isDecommissioned")
     private AtomicBoolean isDecommissioned;
+    @SerializedName("decommissionType")
     private volatile int decommissionType;
+    @SerializedName("ownerClusterName")
     private volatile String ownerClusterName;
     // to index the state in some cluster
+    @SerializedName("backendState")
     private volatile int backendState;
     // private BackendState backendState;
 
     // rootPath -> DiskInfo
+    @SerializedName("disksRef")
     private volatile ImmutableMap<String, DiskInfo> disksRef;
 
     private String heartbeatErrMsg = "";
@@ -93,9 +111,8 @@ public class Backend implements Writable {
     private volatile long tabletMaxCompactionScore = 0;
 
     // additional backendStatus information for BE, display in JSON format
+    @SerializedName("backendStatus")
     private BackendStatus backendStatus = new BackendStatus();
-
-    private long lastStreamLoadTime = -1;
 
     public Backend() {
         this.host = "";
@@ -114,8 +131,6 @@ public class Backend implements Writable {
         this.backendState = BackendState.free.ordinal();
         
         this.decommissionType = DecommissionType.SystemDecommission.ordinal();
-
-        this.lastStreamLoadTime = -1;
     }
 
     public Backend(long id, String host, int heartbeatPort) {
@@ -136,8 +151,6 @@ public class Backend implements Writable {
         this.ownerClusterName = "";
         this.backendState = BackendState.free.ordinal();
         this.decommissionType = DecommissionType.SystemDecommission.ordinal();
-
-        this.lastStreamLoadTime = -1;
     }
 
     public long getId() {
@@ -176,10 +189,10 @@ public class Backend implements Writable {
         return heartbeatErrMsg;
     }
 
-    public long getLastStreamLoadTime() { return lastStreamLoadTime; }
+    public long getLastStreamLoadTime() { return this.backendStatus.lastStreamLoadTime; }
 
     public void setLastStreamLoadTime(long lastStreamLoadTime) {
-        this.lastStreamLoadTime = lastStreamLoadTime;
+        this.backendStatus.lastStreamLoadTime = lastStreamLoadTime;
     }
 
     // for test only
@@ -487,36 +500,19 @@ public class Backend implements Writable {
     }
 
     public static Backend read(DataInput in) throws IOException {
-        Backend backend = new Backend();
-        backend.readFields(in);
-        return backend;
+        if (Catalog.getCurrentCatalogJournalVersion() < FeMetaVersion.VERSION_99) {
+            Backend backend = new Backend();
+            backend.readFields(in);
+            return backend;
+        }
+        String json = Text.readString(in);
+        return GsonUtils.GSON.fromJson(json, Backend.class);
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
-        out.writeLong(id);
-        Text.writeString(out, host);
-        out.writeInt(heartbeatPort);
-        out.writeInt(bePort);
-        out.writeInt(httpPort);
-        out.writeInt(beRpcPort);
-        out.writeBoolean(isAlive.get());
-        out.writeBoolean(isDecommissioned.get());
-        out.writeLong(lastUpdateMs);
-        out.writeLong(lastStartTime);
-
-        ImmutableMap<String, DiskInfo> disks = disksRef;
-        out.writeInt(disks.size());
-        for (Map.Entry<String, DiskInfo> entry : disks.entrySet()) {
-            Text.writeString(out, entry.getKey());
-            entry.getValue().write(out);
-        }
-
-        Text.writeString(out, ownerClusterName);
-        out.writeInt(backendState);
-        out.writeInt(decommissionType);
-
-        out.writeInt(brpcPort);
+        String json = GsonUtils.GSON.toJson(this);
+        Text.writeString(out, json);
     }
 
     public void readFields(DataInput in) throws IOException {
@@ -696,6 +692,10 @@ public class Backend implements Writable {
     public class BackendStatus {
         // this will be output as json, so not using FeConstants.null_string;
         public String lastSuccessReportTabletsTime = "N/A";
+        @SerializedName("lastStreamLoadTime")
+        // the last time when the stream load status was reported by backend
+        public long lastStreamLoadTime = -1;
+
     }
 }
 

be/src/common/config.h
@@ -355,6 +355,9 @@ CONF_mInt32(streaming_load_rpc_max_alive_time_sec, "1200");
 CONF_Int32(tablet_writer_open_rpc_timeout_sec, "60");
 // You can ignore brpc error '[E1011]The server is overcrowded' when writing data.
 CONF_mBool(tablet_writer_ignore_eovercrowded, "false");
+// Whether to enable stream load record function, the default is false.
+// False: disable stream load record
+CONF_mBool(enable_stream_load_record, "false");
 // batch size of stream load record reported to FE
 CONF_mInt32(stream_load_record_batch_size, "50");
 // expire time of stream load record in rocksdb.

be/src/http/action/stream_load.cpp
@@ -162,8 +162,10 @@ void StreamLoadAction::handle(HttpRequest* req) {
     str = str + '\n';
     HttpChannel::send_reply(req, str);
 #ifndef BE_TEST
-    str = ctx->prepare_stream_load_record(str);
-    _sava_stream_load_record(ctx, str);
+    if (config::enable_stream_load_record) {
+        str = ctx->prepare_stream_load_record(str);
+        _sava_stream_load_record(ctx, str);
+    }
 #endif
     // update statstics
     streaming_load_requests_total->increment(1);

fe/fe-core/src/main/java/org/apache/doris/catalog/DiskInfo.java
@@ -21,6 +21,7 @@ import org.apache.doris.common.Config;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonUtils;
 import org.apache.doris.thrift.TStorageMedium;
 
 import org.apache.logging.log4j.LogManager;
@@ -29,6 +30,8 @@ import org.apache.logging.log4j.Logger;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+
+import com.google.gson.annotations.SerializedName;
 
 public class DiskInfo implements Writable {
     private static final Logger LOG = LogManager.getLogger(DiskInfo.class);
@@ -40,10 +43,15 @@ public class DiskInfo implements Writable {
     
     private static final long DEFAULT_CAPACITY_B = 1024 * 1024 * 1024 * 1024L; // 1T
 
+    @SerializedName("rootPath")
     private String rootPath;
+    @SerializedName("totalCapacityB")
     private long totalCapacityB;
+    @SerializedName("dataUsedCapacityB")
     private long dataUsedCapacityB;
+    @SerializedName("diskAvailableCapacityB")
     private long diskAvailableCapacityB;
+    @SerializedName("state")
     private DiskState state;
 
     // path hash and storage medium are reported from Backend and no need to persist
@@ -155,11 +163,8 @@ public class DiskInfo implements Writable {
 
     @Override
     public void write(DataOutput out) throws IOException {
-        Text.writeString(out, rootPath);
-        out.writeLong(totalCapacityB);
-        out.writeLong(dataUsedCapacityB);
-        out.writeLong(diskAvailableCapacityB);
-        Text.writeString(out, state.name());
+        String json = GsonUtils.GSON.toJson(this);
+        Text.writeString(out, json);
     }
 
     public void readFields(DataInput in) throws IOException {
@@ -177,8 +182,13 @@ public class DiskInfo implements Writable {
     }
 
     public static DiskInfo read(DataInput in) throws IOException {
-        DiskInfo diskInfo = new DiskInfo();
-        diskInfo.readFields(in);
-        return diskInfo;
+        if (Catalog.getCurrentCatalogJournalVersion() < FeMetaVersion.VERSION_99) {
+            DiskInfo diskInfo = new DiskInfo();
+            diskInfo.readFields(in);
+            return diskInfo;
+        } else {
+            String json = Text.readString(in);
+            return GsonUtils.GSON.fromJson(json, DiskInfo.class);
+        }
     }
 }

fe/fe-core/src/main/java/org/apache/doris/common/FeMetaVersion.java
@@ -208,6 +208,8 @@ public final class FeMetaVersion {
     public static final int VERSION_97 = 97;
     // add list partition
     public static final int VERSION_98 = 98;
+    // add audit steam load and change the serialization backend method to json
+    public static final int VERSION_99 = 99;
     // note: when increment meta version, should assign the latest version to VERSION_CURRENT
-    public static final int VERSION_CURRENT = VERSION_98;
+    public static final int VERSION_CURRENT = VERSION_99;
 }

fe/fe-core/src/main/java/org/apache/doris/load/StreamLoadRecordMgr.java
@@ -17,8 +17,6 @@
 
 package org.apache.doris.load;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
 import org.apache.doris.analysis.ShowStreamLoadStmt.StreamLoadState;
 import org.apache.doris.catalog.Catalog;
 import org.apache.doris.catalog.Database;
@@ -26,9 +24,11 @@ import org.apache.doris.cluster.ClusterNamespace;
 import org.apache.doris.common.ClientPool;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.UserException;
+import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
 import org.apache.doris.common.util.MasterDaemon;
 import org.apache.doris.common.util.TimeUtils;
+import org.apache.doris.persist.gson.GsonUtils;
 import org.apache.doris.plugin.AuditEvent;
 import org.apache.doris.plugin.AuditEvent.EventType;
 import org.apache.doris.plugin.StreamLoadAuditEvent;
@@ -38,11 +38,14 @@ import org.apache.doris.thrift.BackendService;
 import org.apache.doris.thrift.TNetworkAddress;
 import org.apache.doris.thrift.TStreamLoadRecord;
 import org.apache.doris.thrift.TStreamLoadRecordResult;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.google.common.collect.Maps;
-import com.google.common.collect.ImmutableMap;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -53,11 +56,13 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
+
+import com.google.gson.annotations.SerializedName;
 
 public class StreamLoadRecordMgr extends MasterDaemon {
     private static final Logger LOG = LogManager.getLogger(StreamLoadRecordMgr.class);
@@ -231,12 +236,14 @@ public class StreamLoadRecordMgr extends MasterDaemon {
                     TStreamLoadRecord streamLoadItem= entry.getValue();
                     String startTime = TimeUtils.longToTimeString(streamLoadItem.getStartTime(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"));
                     String finishTime = TimeUtils.longToTimeString(streamLoadItem.getFinishTime(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS"));
-                    LOG.debug("receive stream load record info from backend: {}. label: {}, db: {}, tbl: {}, user: {}, user_ip: {}," +
-                                    " status: {}, message: {}, error_url: {}, total_rows: {}, loaded_rows: {}, filtered_rows: {}," +
-                                    " unselected_rows: {}, load_bytes: {}, start_time: {}, finish_time: {}.",
-                            backend.getHost(), streamLoadItem.getLabel(), streamLoadItem.getDb(), streamLoadItem.getTbl(), streamLoadItem.getUser(), streamLoadItem.getUserIp(),
-                            streamLoadItem.getStatus(), streamLoadItem.getMessage(), streamLoadItem.getUrl(), streamLoadItem.getTotalRows(), streamLoadItem.getLoadedRows(),
-                            streamLoadItem.getFilteredRows(), streamLoadItem.getUnselectedRows(), streamLoadItem.getLoadBytes(), startTime, finishTime);
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("receive stream load record info from backend: {}. label: {}, db: {}, tbl: {}, user: {}, user_ip: {}," +
+                                        " status: {}, message: {}, error_url: {}, total_rows: {}, loaded_rows: {}, filtered_rows: {}," +
+                                        " unselected_rows: {}, load_bytes: {}, start_time: {}, finish_time: {}.",
+                                backend.getHost(), streamLoadItem.getLabel(), streamLoadItem.getDb(), streamLoadItem.getTbl(), streamLoadItem.getUser(), streamLoadItem.getUserIp(),
+                                streamLoadItem.getStatus(), streamLoadItem.getMessage(), streamLoadItem.getUrl(), streamLoadItem.getTotalRows(), streamLoadItem.getLoadedRows(),
+                                streamLoadItem.getFilteredRows(), streamLoadItem.getUnselectedRows(), streamLoadItem.getLoadBytes(), startTime, finishTime);
+                    }
 
                     AuditEvent auditEvent = new StreamLoadAuditEvent.AuditEventBuilder().setEventType(EventType.STREAM_LOAD_FINISH)
                             .setLabel(streamLoadItem.getLabel()).setDb(streamLoadItem.getDb()).setTable(streamLoadItem.getTbl())
@@ -313,13 +320,14 @@ public class StreamLoadRecordMgr extends MasterDaemon {
         for (Backend backend : backends.values()) {
             if (beIdToLastStreamLoad.containsKey(backend.getId())) {
                 long lastStreamLoadTime = beIdToLastStreamLoad.get(backend.getId());
-                LOG.info("Replay stream load bdbje. backend: {}, last stream load version: {}", backend.getHost(), lastStreamLoadTime);
+                LOG.info("Replay stream load bdbje. backend: {}, last stream load time: {}", backend.getHost(), lastStreamLoadTime);
                 backend.setLastStreamLoadTime(lastStreamLoadTime);
             }
         }
     }
 
     public static class FetchStreamLoadRecord implements Writable {
+        @SerializedName("beIdToLastStreamLoad")
         private Map<Long, Long> beIdToLastStreamLoad;
 
         public FetchStreamLoadRecord(Map<Long, Long> beIdToLastStreamLoad) {
@@ -336,34 +344,13 @@ public class StreamLoadRecordMgr extends MasterDaemon {
 
         @Override
         public void write(DataOutput out) throws IOException {
-            for (Map.Entry<Long, Long> entry : beIdToLastStreamLoad.entrySet()) {
-                out.writeBoolean(true);
-                out.writeLong(entry.getKey());
-                out.writeBoolean(true);
-                out.writeLong(entry.getValue());
-                LOG.debug("Write stream load bdbje. key: {}, value: {} ", entry.getKey(), entry.getValue());
-            }
+            String json = GsonUtils.GSON.toJson(this);
+            Text.writeString(out, json);
         }
 
         public static FetchStreamLoadRecord read(DataInput in) throws IOException {
-            Map<Long, Long> idToLastStreamLoad = Maps.newHashMap();
-            int beNum = Catalog.getCurrentSystemInfo().getIdToBackend().size();
-            for (int i = 0; i < beNum; i++) {
-                long beId = -1;
-                long lastStreamLoad = -1;
-                if (in.readBoolean()) {
-                    beId = in.readLong();
-                }
-                if (in.readBoolean()) {
-                    lastStreamLoad = in.readLong();
-                }
-                if (beId != -1 && lastStreamLoad != -1) {
-                    idToLastStreamLoad.put(beId, lastStreamLoad);
-                }
-                LOG.debug("Read stream load bdbje. key: {}, value: {} ", beId, lastStreamLoad);
-            }
-            FetchStreamLoadRecord fetchStreamLoadRecord = new FetchStreamLoadRecord(idToLastStreamLoad);
-            return fetchStreamLoadRecord;
+            String json = Text.readString(in);
+            return GsonUtils.GSON.fromJson(json, FetchStreamLoadRecord.class);
         }
     }
 }

fe/fe-core/src/main/java/org/apache/doris/persist/gson/GsonUtils.java
@@ -21,8 +21,8 @@ import org.apache.doris.alter.AlterJobV2;
 import org.apache.doris.alter.RollupJobV2;
 import org.apache.doris.alter.SchemaChangeJobV2;
 import org.apache.doris.catalog.DistributionInfo;
-import org.apache.doris.catalog.OdbcCatalogResource;
 import org.apache.doris.catalog.HashDistributionInfo;
+import org.apache.doris.catalog.OdbcCatalogResource;
 import org.apache.doris.catalog.RandomDistributionInfo;
 import org.apache.doris.catalog.Resource;
 import org.apache.doris.catalog.ScalarType;
@@ -34,10 +34,21 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Table;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
@@ -56,14 +67,7 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 /*
  * Some utilities about Gson.
@@ -124,7 +128,9 @@ public class GsonUtils {
             .registerTypeAdapterFactory(distributionInfoTypeAdapterFactory)
             .registerTypeAdapterFactory(resourceTypeAdapterFactory)
             .registerTypeAdapterFactory(alterJobV2TypeAdapterFactory)
-            .registerTypeAdapterFactory(loadJobStateUpdateInfoTypeAdapterFactory);
+            .registerTypeAdapterFactory(loadJobStateUpdateInfoTypeAdapterFactory)
+            .registerTypeAdapter(ImmutableMap.class, new ImmutableMapDeserializer())
+            .registerTypeAdapter(AtomicBoolean.class, new AtomicBooleanAdapter());
 
     // this instance is thread-safe.
     public static final Gson GSON = GSON_BUILDER.create();
@@ -323,6 +329,39 @@ public class GsonUtils {
                 map.putAll(entry.getKey(), entry.getValue());
             }
             return map;
+        }
+    }
+
+    private static class AtomicBooleanAdapter
+            implements JsonSerializer<AtomicBoolean>, JsonDeserializer<AtomicBoolean> {
+
+        @Override
+        public AtomicBoolean deserialize(JsonElement jsonElement, Type type,
+                                         JsonDeserializationContext jsonDeserializationContext)
+                throws JsonParseException {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            boolean value = jsonObject.get("boolean").getAsBoolean();
+            return new AtomicBoolean(value);
+        }
+
+        @Override
+        public JsonElement serialize(AtomicBoolean atomicBoolean, Type type,
+                                     JsonSerializationContext jsonSerializationContext) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("boolean", atomicBoolean.get());
+            return jsonObject;
+        }
+    }
+
+    public final static class ImmutableMapDeserializer implements JsonDeserializer<ImmutableMap<?,?>> {
+        @Override
+        public ImmutableMap<?,?> deserialize(final JsonElement json, final Type type,
+                                             final JsonDeserializationContext context) throws JsonParseException
+        {
+            final Type type2 =
+                    ParameterizedTypeImpl.make(Map.class, ((ParameterizedType) type).getActualTypeArguments(), null);
+            final Map<?,?> map = context.deserialize(json, type2);
+            return ImmutableMap.copyOf(map);
         }
     }
 

commit ed6b702d7781f89d17ae9d5d08afe3eab2032c5c

[Bug] Fix Backend edit log read bug
This bug is introduced from #5722

Also fix a bug of creating dynamic history partitions, introduced from #5703

fe/fe-core/src/main/java/org/apache/doris/common/util/DynamicPartitionUtil.java
@@ -37,11 +37,11 @@ import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.FeNameFormat;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -317,11 +317,13 @@ public class DynamicPartitionUtil {
         }
 
         int end = DynamicPartitionProperty.MAX_END_OFFSET;
+        boolean hasEnd = false;
         if (properties.containsKey(DynamicPartitionProperty.END)) {
             String endValue = properties.get(DynamicPartitionProperty.END);
             end = checkEnd(endValue);
             properties.remove(DynamicPartitionProperty.END);
             analyzedProperties.put(DynamicPartitionProperty.END, endValue);
+            hasEnd = true;
         }
 
         boolean createHistoryPartition = false;
@@ -337,7 +339,7 @@ public class DynamicPartitionUtil {
         if (!createHistoryPartition) {
             start = 0;
         }
-        if (end - start > Config.max_dynamic_partition_num) {
+        if (hasEnd && (end - start > Config.max_dynamic_partition_num)) {
             throw new DdlException("Too many dynamic partitions: " + (end - start) + ". Limit: " + Config.max_dynamic_partition_num);
         }
 

fe/fe-core/src/main/java/org/apache/doris/journal/JournalEntity.java
@@ -40,10 +40,10 @@ import org.apache.doris.load.DeleteInfo;
 import org.apache.doris.load.ExportJob;
 import org.apache.doris.load.LoadErrorHub;
 import org.apache.doris.load.LoadJob;
+import org.apache.doris.load.StreamLoadRecordMgr.FetchStreamLoadRecord;
 import org.apache.doris.load.loadv2.LoadJob.LoadJobStateUpdateInfo;
 import org.apache.doris.load.loadv2.LoadJobFinalOperation;
 import org.apache.doris.load.routineload.RoutineLoadJob;
-import org.apache.doris.load.StreamLoadRecordMgr.FetchStreamLoadRecord;
 import org.apache.doris.master.Checkpoint;
 import org.apache.doris.mysql.privilege.UserPropertyInfo;
 import org.apache.doris.persist.AlterRoutineLoadJobOperationLog;
@@ -312,8 +312,7 @@ public class JournalEntity implements Writable {
             case OperationType.OP_ADD_BACKEND:
             case OperationType.OP_DROP_BACKEND:
             case OperationType.OP_BACKEND_STATE_CHANGE: {
-                data = new Backend();
-                ((Backend) data).readFields(in);
+                data = Backend.read(in);
                 isRead = true;
                 break;
             }

fe/fe-core/src/main/java/org/apache/doris/system/Backend.java
@@ -32,6 +32,7 @@ import org.apache.doris.thrift.TStorageMedium;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.gson.annotations.SerializedName;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -43,8 +44,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import com.google.gson.annotations.SerializedName;
 
 /**
  * This class extends the primary identifier of a Backend with ephemeral state,
@@ -504,9 +503,10 @@ public class Backend implements Writable {
             Backend backend = new Backend();
             backend.readFields(in);
             return backend;
+        } else {
+            String json = Text.readString(in);
+            return GsonUtils.GSON.fromJson(json, Backend.class);
         }
-        String json = Text.readString(in);
-        return GsonUtils.GSON.fromJson(json, Backend.class);
     }
 
     @Override
@@ -515,7 +515,8 @@ public class Backend implements Writable {
         Text.writeString(out, json);
     }
 
-    public void readFields(DataInput in) throws IOException {
+    @Deprecated
+    private void readFields(DataInput in) throws IOException {
         id = in.readLong();
         host = Text.readString(in);
         heartbeatPort = in.readInt();
@@ -695,7 +696,6 @@ public class Backend implements Writable {
         @SerializedName("lastStreamLoadTime")
         // the last time when the stream load status was reported by backend
         public long lastStreamLoadTime = -1;
-
     }
 }
 