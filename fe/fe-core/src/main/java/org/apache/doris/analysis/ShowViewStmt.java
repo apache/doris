e2194ea2f01d1737a9693325bf1595f6097354bd
xy720 <22125576+xy720@users.noreply.github.com>
2021-05-19 09:29:00 +0800

    [Feature] Support show view statement for table (#5813)
    Help to find all views which contain the given table

---
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ShowViewStmt.java
@@ -0,0 +1,148 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.analysis;
+
+import org.apache.doris.catalog.Catalog;
+import org.apache.doris.catalog.Column;
+import org.apache.doris.catalog.Database;
+import org.apache.doris.catalog.OlapTable;
+import org.apache.doris.catalog.ScalarType;
+import org.apache.doris.catalog.Table;
+import org.apache.doris.catalog.View;
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.ErrorCode;
+import org.apache.doris.common.ErrorReport;
+import org.apache.doris.common.UserException;
+import org.apache.doris.mysql.privilege.PrivPredicate;
+import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.qe.ShowResultSetMetaData;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+// Show view statement, used to show view information of one table.
+//
+// Syntax:
+//      SHOW VIEW { FROM | IN } table [ FROM db ]
+public class ShowViewStmt extends ShowStmt {
+    private static final ShowResultSetMetaData META_DATA =
+            ShowResultSetMetaData.builder()
+                    .addColumn(new Column("View", ScalarType.createVarchar(30)))
+                    .addColumn(new Column("Create View", ScalarType.createVarchar(65535)))
+                    .build();
+
+    private String db;
+    private TableName tbl;
+
+    private List<View> matchViews = Lists.newArrayList();;
+
+    public ShowViewStmt(String db, TableName tbl) {
+        this.db = db;
+        this.tbl = tbl;
+    }
+
+    public String getDb() {
+        return tbl.getDb();
+    }
+
+    public String getTbl() {
+        return tbl.getTbl();
+    }
+
+    public List<View> getMatchViews() {
+        return matchViews;
+    }
+
+    @Override
+    public void analyze(Analyzer analyzer) throws AnalysisException, UserException {
+        super.analyze(analyzer);
+        if (tbl == null) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_NO_TABLES_USED);
+        }
+        if (!Strings.isNullOrEmpty(db)) {
+            // if user specify the `from db`, overwrite the db in `tbl` with this db.
+            // for example:
+            //      show view from db1.tbl1 from db2;
+            // will be rewrote to:
+            //      show view from db2.tbl1;
+            // this act same as in MySQL
+            tbl.setDb(db);
+        }
+        tbl.analyze(analyzer);
+
+        String dbName = tbl.getDb();
+        if (!Catalog.getCurrentCatalog().getAuth().checkTblPriv(ConnectContext.get(), dbName, getTbl(), PrivPredicate.SHOW)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "SHOW VIEW",
+                    ConnectContext.get().getQualifiedUser(),
+                    ConnectContext.get().getRemoteIP(),
+                    getTbl());
+        }
+
+        Database database = Catalog.getCurrentCatalog().getDb(dbName);
+        if (database == null) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_DB_ERROR, dbName);
+        }
+
+        Table showTable = database.getTable(tbl.getTbl());
+        if (showTable == null) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_BAD_TABLE_ERROR, getTbl());
+        }
+
+        if (!(showTable instanceof OlapTable)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_NOT_OLAP_TABLE, getTbl());
+        }
+
+        for (Table table : database.getViews()) {
+            View view = (View) table;
+            Map<Long, Table> tableMap = Maps.newHashMap();
+            // get tables from view sql
+            getTables(analyzer, view, tableMap);
+            if (tableMap.containsKey(showTable.getId())) {
+                matchViews.add(view);
+            }
+        }
+    }
+
+    private void getTables(Analyzer analyzer, View view, Map<Long, Table> tableMap) throws UserException {
+        Set<String> parentViewNameSet = Sets.newHashSet();
+        QueryStmt queryStmt = view.getQueryStmt();
+        queryStmt.getTables(analyzer, tableMap, parentViewNameSet);
+    }
+
+    @Override
+    public String toSql() {
+        StringBuilder sb = new StringBuilder("SHOW VIEW FROM ");
+        sb.append(tbl.toSql());
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toSql();
+    }
+
+    @Override
+    public ShowResultSetMetaData getMetaData() {
+        return META_DATA;
+    }
+}
