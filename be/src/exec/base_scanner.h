7483dd60e006316ccfb2edc4cec1a799c985d0a5
stdpain <34912776+stdpain@users.noreply.github.com>
2021-05-25 10:34:10 +0800

    [BUG] Fix Stream Load cost too much memory (#5875)

--- be/src/exec/base_scanner.h
+++ be/src/exec/base_scanner.h
@@ -60,6 +60,7 @@ public:
     void fill_slots_of_columns_from_path(int start,
                                          const std::vector<std::string>& columns_from_path);
 
+    void free_expr_local_allocations();
 protected:
     RuntimeState* _state;
     const TBrokerScanRangeParams& _params;
@@ -88,6 +89,10 @@ protected:
 	const std::vector<ExprContext*>& _pre_filter_ctxs;
 
     bool _strict_mode;
+
+    int32_t _line_counter;
+    // reference to HASH_JOIN_NODE::RELEASE_CONTEXT_COUNTER
+    const static constexpr int32_t RELEASE_CONTEXT_COUNTER = 1 << 5;
     // Profile
     RuntimeProfile* _profile;
     RuntimeProfile::Counter* _rows_read_counter;
