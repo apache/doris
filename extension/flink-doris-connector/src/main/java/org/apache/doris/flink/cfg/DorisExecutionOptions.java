651d44a34bde1ce6114f32a6b4d6107199db172d
wudi <wud3@shuhaisc.com>
2021-05-28 14:56:53 +0800

    [Feature]:Support time interval for flink connecor

--- extension/flink-doris-connector/src/main/java/org/apache/doris/flink/cfg/DorisExecutionOptions.java
+++ extension/flink-doris-connector/src/main/java/org/apache/doris/flink/cfg/DorisExecutionOptions.java
@@ -17,7 +17,10 @@
 package org.apache.doris.flink.cfg;
 
 
+import org.apache.flink.util.Preconditions;
+
 import java.io.Serializable;
+import java.time.Duration;
 
 /**
  * JDBC sink batch options.
@@ -27,10 +30,13 @@ public class DorisExecutionOptions  implements Serializable {
 
     private final Integer batchSize;
     private final Integer maxRetries;
+    private final Long batchIntervalMs;
 
-    public DorisExecutionOptions(Integer batchSize, Integer maxRetries) {
+    public DorisExecutionOptions(Integer batchSize, Integer maxRetries,Long batchIntervalMs) {
+        Preconditions.checkArgument(maxRetries >= 0);
         this.batchSize = batchSize;
         this.maxRetries = maxRetries;
+        this.batchIntervalMs = batchIntervalMs;
     }
 
     public Integer getBatchSize() {
@@ -39,6 +45,10 @@ public class DorisExecutionOptions  implements Serializable {
 
     public Integer getMaxRetries() {
         return maxRetries;
+    }
+
+    public Long getBatchIntervalMs() {
+        return batchIntervalMs;
     }
 
     public static Builder builder() {
@@ -51,6 +61,7 @@ public class DorisExecutionOptions  implements Serializable {
     public static class Builder {
         private Integer batchSize;
         private Integer maxRetries;
+        private Long batchIntervalMs;
 
         public Builder setBatchSize(Integer batchSize) {
             this.batchSize = batchSize;
@@ -62,8 +73,13 @@ public class DorisExecutionOptions  implements Serializable {
             return this;
         }
 
+        public Builder setBatchIntervalMs(Long batchIntervalMs) {
+            this.batchIntervalMs = batchIntervalMs;
+            return this;
+        }
+
         public DorisExecutionOptions build() {
-            return new DorisExecutionOptions(batchSize,maxRetries);
+            return new DorisExecutionOptions(batchSize,maxRetries,batchIntervalMs);
         }
     }
 
