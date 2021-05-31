ce057392a6a6d1a5e4c37d8cad9537ade9195dad
jiafeng.zhang <zhangjf1@gmail.com>
2021-05-31 12:35:42 +0800

    add it to he side bar
    add it to he side bar

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -143,7 +143,7 @@ module.exports = [
         title: "Maintainence Operation",
         directoryPath: "operation/",
         children: [
-		  "doris_error_code"
+          "doris-error-code",
           "metadata-operation",
           "monitor-alert",
           "multi-tenant",
