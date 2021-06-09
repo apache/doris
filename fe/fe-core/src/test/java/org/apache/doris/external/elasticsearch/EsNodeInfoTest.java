6da3e967ea2ef85b2a337a08cf9635707510e62f
jie <chjie93@gmail.com>
2021-06-09 09:20:14 +0800

    [bug] add Apache license for EsNodeInfoTest file.

--- fe/fe-core/src/test/java/org/apache/doris/external/elasticsearch/EsNodeInfoTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/external/elasticsearch/EsNodeInfoTest.java
@@ -1,29 +1,31 @@
-/*********************************************************************************
- * Copyright (c)2020 CEC Health
- * FILE: EsNodeInfoTest
- * 版本      DATE             BY               REMARKS
- * ----  -----------  ---------------  ------------------------------------------
- * 1.0   2021-06-08        chenjie
- ********************************************************************************/
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.apache.doris.external.elasticsearch;
 
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * @Description:
- * @Author: chenjie
- * @Date: 2021/6/8 8:39 下午
- * @Version V1.0
- **/
 public class EsNodeInfoTest  extends EsTestCase{
 
     @Test
