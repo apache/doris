9f7781996235e3abcaf1a70a0634b001cd462bb0
jiafeng.zhang <zhangjf1@gmail.com>
2021-07-04 20:58:43 +0800

    [DOC]Doris BE development and debugging environment construction (#6056)
    Doris BE development and debugging environment construction
    
    Add installation under ubuntu, dependent installation
    
    Compile on ubuntu 20.04 physical machine, the actual test needs to install these dependencies:
    autoconf automake libtool autopoint

--- docs/.vuepress/sidebar/zh-CN.js
+++ docs/.vuepress/sidebar/zh-CN.js
@@ -547,7 +547,8 @@ module.exports = [
     children: [
         "debug-tool",
         "fe-eclipse-dev",
-        "fe-idea-dev",
+        "fe-idea-dev", 
+        "be-vscode-dev",
         "format-code",
     ],
   },
