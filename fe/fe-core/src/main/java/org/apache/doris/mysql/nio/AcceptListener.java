1937995dcbf5a17b938de1c8f71b4e2e0d2da154
yuzhou.zk <yuzhou.zk@alibaba-inc.com>
2021-07-17 09:42:42 +0800

    print as error log

--- fe/fe-core/src/main/java/org/apache/doris/mysql/nio/AcceptListener.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/nio/AcceptListener.java
@@ -80,8 +80,10 @@ public class AcceptListener implements ChannelListener<AcceptingChannel<StreamCo
                     context.cleanup();
                 } catch (Throwable e) {
                     // should be unexpected exception, so print warn log
-                    if (context.getCurrentUserIdentity() != null || (e instanceof Error)) {
+                    if (context.getCurrentUserIdentity() != null) {
                         LOG.warn("connect processor exception because ", e);
+                    } else if (e instanceof Error) {
+                        LOG.error("connect processor exception because ", e);
                     } else {
                         // for unauthrorized access such lvs probe request, may cause exception, just log it in debug level
                         LOG.debug("connect processor exception because ", e);
