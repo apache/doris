xinghuayu007/incubator-doris

lower/upper case tranfer function vertorized

be/src/util/vectorized-tool/lower.h
@@ -1,0 +1,37 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+#pragma once
+#ifndef BE_LOWER_H
+#define BE_LOWER_H
+
+#include "lower_upper_impl.h"
+#include <stdint.h>
+
+namespace doris {
+class Lower {
+public:
+    static void to_lower(uint8_t * src, int64_t len, uint8_t * dst) {
+        if (len <= 0) {
+            return;
+        }
+        LowerUpperImpl<'A', 'Z'> lowerUpper;
+        lowerUpper.array(src, src + len, dst);
+    }
+};
+}
+
+#endif //BE_LOWER_H

be/src/util/vectorized-tool/upper.h
@@ -1,0 +1,36 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+#pragma once
+#ifndef BE_UPPER_H
+#define BE_UPPER_H
+
+#include "lower_upper_impl.h"
+#include <stdint.h>
+
+namespace doris {
+    class Upper {
+    public:
+        static void to_upper(uint8_t * src, int64_t len, uint8_t * dst) {
+            if (len <= 0) {
+                return;
+            }
+            LowerUpperImpl<'a', 'z'> lowerUpper;
+            lowerUpper.array(src, src + len, dst);
+        }
+    };
+}
+#endif //BE_UPPER_H

be/test/exprs/string_functions_test.cpp
@@ -623,6 +623,25 @@ TEST_F(StringFunctionsTest, bit_length) {
     delete context;
 }
 
+TEST_F(StringFunctionsTest, lower) {
+    ASSERT_EQ(StringVal("hello"), StringFunctions::v_lower(ctx, StringVal("hello")));
+    ASSERT_EQ(StringVal("hello"), StringFunctions::v_lower(ctx, StringVal("HELLO")));
+    ASSERT_EQ(StringVal("hello123"), StringFunctions::v_lower(ctx, StringVal("HELLO123")));
+    ASSERT_EQ(StringVal("hello, 123"), StringFunctions::v_lower(ctx, StringVal("HELLO, 123")));
+    ASSERT_EQ(StringVal::null(), StringFunctions::v_lower(ctx, StringVal::null()));
+    ASSERT_EQ(StringVal(""), StringFunctions::v_lower(ctx, StringVal("")));
+}
+
+TEST_F(StringFunctionsTest, upper) {
+    // function test
+    ASSERT_EQ(StringVal("HELLO"), StringFunctions::v_upper(ctx, StringVal("HELLO")));
+    ASSERT_EQ(StringVal("HELLO"), StringFunctions::v_upper(ctx, StringVal("hello")));
+    ASSERT_EQ(StringVal("HELLO123"), StringFunctions::v_upper(ctx, StringVal("hello123")));
+    ASSERT_EQ(StringVal("HELLO, 123"), StringFunctions::v_upper(ctx, StringVal("hello, 123")));
+    ASSERT_EQ(StringVal::null(), StringFunctions::v_upper(ctx, StringVal::null()));
+    ASSERT_EQ(StringVal(""), StringFunctions::v_upper(ctx, StringVal("")));
+}
+
 } // namespace doris
 
 int main(int argc, char** argv) {

apache/doris

be/src/util/vectorized-tool/lower.h
@@ -1,0 +1,37 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+#pragma once
+#ifndef BE_LOWER_H
+#define BE_LOWER_H
+
+#include "lower_upper_impl.h"
+#include <stdint.h>
+
+namespace doris {
+class Lower {
+public:
+    static void to_lower(uint8_t * src, int64_t len, uint8_t * dst) {
+        if (len <= 0) {
+            return;
+        }
+        LowerUpperImpl<'A', 'Z'> lowerUpper;
+        lowerUpper.transfer(src, src + len, dst);
+    }
+};
+}
+
+#endif //BE_LOWER_H

be/src/util/vectorized-tool/upper.h
@@ -1,0 +1,36 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+#pragma once
+#ifndef BE_UPPER_H
+#define BE_UPPER_H
+
+#include "lower_upper_impl.h"
+#include <stdint.h>
+
+namespace doris {
+    class Upper {
+    public:
+        static void to_upper(uint8_t * src, int64_t len, uint8_t * dst) {
+            if (len <= 0) {
+                return;
+            }
+            LowerUpperImpl<'a', 'z'> lowerUpper;
+            lowerUpper.transfer(src, src + len, dst);
+        }
+    };
+}
+#endif //BE_UPPER_H

be/test/exprs/string_functions_test.cpp
@@ -623,6 +623,25 @@ TEST_F(StringFunctionsTest, bit_length) {
     delete context;
 }
 
+TEST_F(StringFunctionsTest, lower) {
+    ASSERT_EQ(StringVal("hello"), StringFunctions::lower(ctx, StringVal("hello")));
+    ASSERT_EQ(StringVal("hello"), StringFunctions::lower(ctx, StringVal("HELLO")));
+    ASSERT_EQ(StringVal("hello123"), StringFunctions::lower(ctx, StringVal("HELLO123")));
+    ASSERT_EQ(StringVal("hello, 123"), StringFunctions::lower(ctx, StringVal("HELLO, 123")));
+    ASSERT_EQ(StringVal::null(), StringFunctions::lower(ctx, StringVal::null()));
+    ASSERT_EQ(StringVal(""), StringFunctions::lower(ctx, StringVal("")));
+}
+
+TEST_F(StringFunctionsTest, upper) {
+    // function test
+    ASSERT_EQ(StringVal("HELLO"), StringFunctions::upper(ctx, StringVal("HELLO")));
+    ASSERT_EQ(StringVal("HELLO"), StringFunctions::upper(ctx, StringVal("hello")));
+    ASSERT_EQ(StringVal("HELLO123"), StringFunctions::upper(ctx, StringVal("hello123")));
+    ASSERT_EQ(StringVal("HELLO, 123"), StringFunctions::upper(ctx, StringVal("hello, 123")));
+    ASSERT_EQ(StringVal::null(), StringFunctions::upper(ctx, StringVal::null()));
+    ASSERT_EQ(StringVal(""), StringFunctions::upper(ctx, StringVal("")));
+}
+
 } // namespace doris
 
 int main(int argc, char** argv) {