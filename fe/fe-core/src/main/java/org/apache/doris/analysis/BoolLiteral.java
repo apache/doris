cd608e21636650f85825fa695c11d196883a3a5d
Xiang Wei <weixiao5220@gmail.com>
2021-07-27 13:38:42 +0800

    [Bug][SparkLoad] Fix bucket_hash_value for bool value (#6284)
    Co-authored-by: weixiang <weixiang06@meituan.com>

--- fe/fe-core/src/main/java/org/apache/doris/analysis/BoolLiteral.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/BoolLiteral.java
@@ -20,7 +20,10 @@ package org.apache.doris.analysis;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
+import org.apache.doris.catalog.PrimitiveType;
 import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.thrift.TBoolLiteral;
@@ -97,6 +100,16 @@ public class BoolLiteral extends LiteralExpr {
     @Override
     public String getStringValue() {
         return value ? "1" : "0";
+    }
+
+    @Override
+    public ByteBuffer getHashValue(PrimitiveType type) {
+        byte v = (byte) (value ? 1 : 0);
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.put(v);
+        buffer.flip();
+        return buffer;
     }
 
     @Override
