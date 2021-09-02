6423557706195aada4f8c0d2b9537899d05e8658
smallhibiscus <844981280@qq.com>
2021-09-02 18:14:44 +0800

    1、Spark consumes Kafka data using stream loading and storage (DorisSink method) example; 2、Spark reads files into the library (hdfs file, the format can be text or other), and uses Stream to load into the library example; 3、Spark connector reads Doris data to build DataFrame for analysis example;

---
+++ samples/doris-demo/spark-demo/src/main/java/org/apache/doris/demo/spark/demo/connector/SparkDorisConnectorDemo.java
@@ -0,0 +1,46 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package java.org.apache.doris.demo.spark.demo.connector;
+
+import org.apache.doris.spark.constant.SparkToDorisConstants;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+
+public class SparkDorisConnectorDemo {
+
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("SparkDorisConnectorDemo")
+                .setMaster("local[1]");
+        SparkSession sparkSession = SparkSession.builder().config(sparkConf).getOrCreate();
+
+        Dataset<Row> dorisSparkDF = sparkSession.read().format("doris")
+                .option("doris.table.identifier", SparkToDorisConstants.DORIS_DBNAME.concat(".").concat(SparkToDorisConstants.DORIS_TABLE_NAME))
+                .option("doris.fenodes", SparkToDorisConstants.DORIS_FE_NODES)
+                .option("user", SparkToDorisConstants.DORIS_USER)
+                .option("password", SparkToDorisConstants.DORIS_PASSWORD)
+                .load();
+
+        dorisSparkDF.groupBy("name").count().show();
+
+        sparkSession.close();
+    }
+}
