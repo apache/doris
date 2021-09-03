47389c1745dce5e5f429c9cedc668957f40e3e99
weizuo <weizuo@xiaomi.com>
2021-09-03 15:59:09 +0800

    create new partition which have the same number of tablet as base partition for truncate operation

--- fe/fe-core/src/test/java/org/apache/doris/catalog/TruncateTableTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/TruncateTableTest.java
@@ -26,17 +26,14 @@ public class TruncateTableTest {
         connectContext = UtFrameUtils.createDefaultCtx();
         // create database
         String createDbStmtStr = "create database test;";
-        String createTablleStr = "create table test.tbl(" +
-                                        "d1 date," +
-                                        "k1 int," +
-                                        "k2 bigint)" +
+        String createTableStr = "create table test.tbl(d1 date, k1 int, k2 bigint)" +
                                         "duplicate key(d1, k1) " +
                                         "PARTITION BY RANGE(d1)" +
                                         "(PARTITION p20210901 VALUES [('2021-09-01'), ('2021-09-02')))" +
                                         "distributed by hash(k1) buckets 2 " +
                                         "properties('replication_num' = '1');";
         createDb(createDbStmtStr);
-        createTable(createTablleStr);
+        createTable(createTableStr);
     }
 
     @AfterClass
@@ -45,18 +42,8 @@ public class TruncateTableTest {
         file.delete();
     }
 
-    private static void createDb(String sql) throws Exception {
-        CreateDbStmt createDbStmt = (CreateDbStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, connectContext);
-        Catalog.getCurrentCatalog().createDb(createDbStmt);
-    }
-
-    private static void createTable(String sql) throws Exception {
-        CreateTableStmt createTableStmt = (CreateTableStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, connectContext);
-        Catalog.getCurrentCatalog().createTable(createTableStmt);
-    }
-
     @Test
-    public void testTruncatePartition() throws Exception {
+    public void testTruncateTable() throws Exception {
         String stmtStr = "ALTER TABLE test.tbl ADD PARTITION p20210902 VALUES [('2021-09-02'), ('2021-09-03')) DISTRIBUTED BY HASH(`k1`) BUCKETS 3;";
         alterTable(stmtStr);
         stmtStr = "ALTER TABLE test.tbl ADD PARTITION p20210903 VALUES [('2021-09-03'), ('2021-09-04')) DISTRIBUTED BY HASH(`k1`) BUCKETS 4;";
@@ -67,22 +54,52 @@ public class TruncateTableTest {
         checkShowTabletResultNum("test.tbl", "p20210902", 3);
         checkShowTabletResultNum("test.tbl", "p20210903", 4);
         checkShowTabletResultNum("test.tbl", "p20210904", 5);
-        String truncateStr = "truncate table test.tbl partition (p20210901);";
+
+        String truncateStr = "truncate table test.tbl;";
         TruncateTableStmt truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
         Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
         checkShowTabletResultNum("test.tbl", "p20210901", 2);
+        checkShowTabletResultNum("test.tbl", "p20210902", 3);
+        checkShowTabletResultNum("test.tbl", "p20210903", 4);
+        checkShowTabletResultNum("test.tbl", "p20210904", 5);
+
+        truncateStr = "truncate table test.tbl partition(p20210901, p20210902, p20210903, p20210904);";
+        truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
+        Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
+        checkShowTabletResultNum("test.tbl", "p20210901", 2);
+        checkShowTabletResultNum("test.tbl", "p20210902", 3);
+        checkShowTabletResultNum("test.tbl", "p20210903", 4);
+        checkShowTabletResultNum("test.tbl", "p20210904", 5);
+
+        truncateStr = "truncate table test.tbl partition (p20210901);";
+        truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
+        Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
+        checkShowTabletResultNum("test.tbl", "p20210901", 2);
+
         truncateStr = "truncate table test.tbl partition (p20210902);";
         truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
         Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
         checkShowTabletResultNum("test.tbl", "p20210902", 3);
+
         truncateStr = "truncate table test.tbl partition (p20210903);";
         truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
         Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
         checkShowTabletResultNum("test.tbl", "p20210903", 4);
+
         truncateStr = "truncate table test.tbl partition (p20210904);";
         truncateTableStmt = (TruncateTableStmt)UtFrameUtils.parseAndAnalyzeStmt(truncateStr, connectContext);
         Catalog.getCurrentCatalog().truncateTable(truncateTableStmt);
         checkShowTabletResultNum("test.tbl", "p20210904", 5);
+    }
+
+    private static void createDb(String sql) throws Exception {
+        CreateDbStmt createDbStmt = (CreateDbStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, connectContext);
+        Catalog.getCurrentCatalog().createDb(createDbStmt);
+    }
+
+    private static void createTable(String sql) throws Exception {
+        CreateTableStmt createTableStmt = (CreateTableStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, connectContext);
+        Catalog.getCurrentCatalog().createTable(createTableStmt);
     }
 
     private List<List<String>> checkShowTabletResultNum(String tbl, String partition, int expected) throws Exception {
@@ -103,5 +120,4 @@ public class TruncateTableTest {
             throw e;
         }
     }
-
 }
