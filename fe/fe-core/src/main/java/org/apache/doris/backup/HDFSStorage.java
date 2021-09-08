167e0958e5aabe748b9a37b1a33e882fc70d1928
EmmyMiao87 <522274284@qq.com>
2021-09-08 16:09:09 +0800

    Support hdfs in select outfile clause

---
+++ fe/fe-core/src/main/java/org/apache/doris/backup/HDFSStorage.java
@@ -0,0 +1,46 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.backup;
+
+import org.apache.doris.common.UserException;
+
+import java.util.Map;
+
+// TODO: extend BlobStorage
+public class HDFSStorage {
+    public static final String HDFS_DEFAULT_FS = "fs.defaultFS";
+    public static final String USER = "hdfs_user";
+    public static final String NAME_SERVICES = "dfs.nameservices";
+    public static final String NAME_NODES = "dfs.ha.namenodes";
+    public static final String RPC_ADDRESS = "dfs.namenode.rpc-address";
+    public static final String FAILOVER_PROXY = "dfs.client.failover.proxy.provider";
+
+
+    public static void checkHDFS(Map<String, String> properties) throws UserException {
+        if (!properties.containsKey(HDFS_DEFAULT_FS)) {
+            throw new UserException(HDFS_DEFAULT_FS + " not found.");
+        }
+        for (String key : properties.keySet()) {
+            if (!(key.startsWith(NAME_SERVICES) || key.startsWith(NAME_NODES)
+                    || key.startsWith(RPC_ADDRESS) || key.startsWith(FAILOVER_PROXY)
+                    || key.equals(USER) || key.equals(HDFS_DEFAULT_FS))) {
+                throw new UserException("Unknown properties " + key);
+            }
+        }
+    }
+}
