2f203341032167d11a130f3e433f06fd005f74f2
jiafeng.zhang <zhangjf1@gmail.com>
2021-09-15 13:52:45 +0800

    Add the description document of the error code returned by the OLAP function on the BE side
    Add the description document of the error code returned by the OLAP function on the BE side

--- docs/.vuepress/sidebar/zh-CN.js
+++ docs/.vuepress/sidebar/zh-CN.js
@@ -153,6 +153,7 @@ module.exports = [
         directoryPath: "operation/",
         children: [
           "doris-error-code",
+          "be-olap-error-code",
           "disk-capacity",
           "metadata-operation",
           "monitor-alert",
