6cfed4834cbba5adc6150c2a92b2abe970e6110e
Zeno Yang <cookie.yz@qq.com>
2021-09-16 10:26:15 +0800

    [Bug] Fix the bug of generating sql_key in SqlCache (#6668)
    The current SqlCache sql_key is generated by taking the md5 value of selectStmt.toSql(), but selectStmt.toSql() is spliced through the operator tree, and sometimes some specific parameters cannot be displayed, resulting in sql hits with different parameters The same cache is used, and the query results are inconsistent with expectations.
    
    For example, our user has a sql with more than 300 rows, which contains a lot of parameters, including partitions. But the result of selectStmt.toSql() is:
    
    SELECT `tb`.`type` AS `type`, `tb`.`name` AS `name`, `tb`.`name1` AS `name1`, `tb`.`name2` AS `name2`, `tb`.`name3` AS `name3`
    FROM (
      SELECT 3 AS `type`, `cc`.`name` AS `name`, `cc`.`name1` AS `name1`
        , coalesce(`bb`.`name`, '请联系您的品牌业务经理进行咨询。') AS `name2`, `bb`.`name1` AS `name3`
      FROM `cc`
        LEFT JOIN `bb` ON `cc`.`id` = `bb`.`id1`
      UNION ALL
      SELECT `dd`.`type` AS `type`, `dd`.`name` AS `name`, `dd`.`name1` AS `name1`, `dd`.`name2` AS `name2`, `dd`.`name3` AS `name3`
      FROM `dd`
      UNION ALL
      SELECT `ee`.`type` AS `type`, `ee`.`name` AS `name`, `ee`.`name1` AS `name1`, `ee`.`name2` AS `name2`, `ee`.`name3` AS `name3`
      FROM `ee`
    ) tb
    LIMIT 10
    In this way, the user specified different partitions for query, and the same cache was queried, which was inconsistent with the expected result. Therefore, it is recommended to use originStmt instead of selectStmt.toSql() to generate sql_key.

--- fe/fe-core/src/main/java/org/apache/doris/qe/cache/SqlCache.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/cache/SqlCache.java
@@ -41,7 +41,7 @@ public class SqlCache extends Cache {
 
     public InternalService.PFetchCacheResult getCacheData(Status status) {
         InternalService.PFetchCacheRequest request = InternalService.PFetchCacheRequest.newBuilder()
-                .setSqlKey(CacheProxy.getMd5(selectStmt.toSql()))
+                .setSqlKey(CacheProxy.getMd5(selectStmt.getOrigStmt().originStmt))
                 .addParams(InternalService.PCacheParam.newBuilder()
                         .setPartitionKey(latestTable.latestPartitionId)
                         .setLastVersion(latestTable.latestVersion)
@@ -73,8 +73,9 @@ public class SqlCache extends Cache {
             return;
         }
 
-        InternalService.PUpdateCacheRequest updateRequest = rowBatchBuilder.buildSqlUpdateRequest(selectStmt.toSql(),
-                latestTable.latestPartitionId, latestTable.latestVersion, latestTable.latestTime);
+        InternalService.PUpdateCacheRequest updateRequest =
+                rowBatchBuilder.buildSqlUpdateRequest(selectStmt.getOrigStmt().originStmt,
+                        latestTable.latestPartitionId, latestTable.latestVersion, latestTable.latestTime);
         if (updateRequest.getValuesCount() > 0) {
             CacheBeProxy proxy = new CacheBeProxy();
             Status status = new Status();
