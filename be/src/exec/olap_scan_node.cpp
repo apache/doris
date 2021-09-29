apache/doris

commit 6062fd896cd6dd1b2d5affa7dcba3543295a2410

[Bug][bdbje] handle bdb rollbackexception (#6582)
when use 3 FE follower, when restart the fe, and regardless of order, we probability can't start fe success,
and bdb throw RollbackException，
In this scenario, the bdb suggests to catch the exception, simply closing all your ReplicatedEnvironment handles,
and then reopening.

so we catch the RollbackException, and reopen the ReplicatedEnvironment

fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -37,6 +37,7 @@ import com.sleepycat.je.OperationStatus;
 import com.sleepycat.je.rep.InsufficientLogException;
 import com.sleepycat.je.rep.NetworkRestore;
 import com.sleepycat.je.rep.NetworkRestoreConfig;
+import com.sleepycat.je.rep.RollbackException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -191,7 +192,7 @@ public class BDBJEJournal implements Journal {
 
     @Override
     public JournalEntity read(long journalId) {
-        List<Long> dbNames = bdbEnvironment.getDatabaseNames();
+        List<Long> dbNames = getDatabaseNames();
         if (dbNames == null) {
             return null;
         }
@@ -252,7 +253,7 @@ public class BDBJEJournal implements Journal {
         if (bdbEnvironment == null) {
             return ret;
         }
-        List<Long> dbNames = bdbEnvironment.getDatabaseNames();
+        List<Long> dbNames = getDatabaseNames();
         if (dbNames == null) {
             return ret;
         }
@@ -275,7 +276,7 @@ public class BDBJEJournal implements Journal {
         if (bdbEnvironment == null) {
             return ret;
         }
-        List<Long> dbNames = bdbEnvironment.getDatabaseNames();
+        List<Long> dbNames = getDatabaseNames();
         if (dbNames == null) {
             return ret;
         }
@@ -323,7 +324,7 @@ public class BDBJEJournal implements Journal {
         List<Long> dbNames = null;
         for (int i = 0; i < RETRY_TIME; i++) {
             try {
-                dbNames = bdbEnvironment.getDatabaseNames();
+                dbNames = getDatabaseNames();
                 
                 if (dbNames == null) {
                     LOG.error("fail to get dbNames while open bdbje journal. will exit");
@@ -364,7 +365,7 @@ public class BDBJEJournal implements Journal {
     
     @Override
     public void deleteJournals(long deleteToJournalId) {
-        List<Long> dbNames = bdbEnvironment.getDatabaseNames();
+        List<Long> dbNames = getDatabaseNames();
         if (dbNames == null) {
             LOG.info("delete database names is null.");
             return;
@@ -393,7 +394,7 @@ public class BDBJEJournal implements Journal {
     
     @Override
     public long getFinalizedJournalId() {
-        List<Long> dbNames = bdbEnvironment.getDatabaseNames();
+        List<Long> dbNames = getDatabaseNames();
         if (dbNames == null) {
             LOG.error("database name is null.");
             return 0;
@@ -417,8 +418,31 @@ public class BDBJEJournal implements Journal {
         if (bdbEnvironment == null) {
             return null;
         }
-        
-        return bdbEnvironment.getDatabaseNames();
+
+        // Open a new journal database or get last existing one as current journal database
+        Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
+        List<Long>  dbNames = null;
+        for (int i = 0; i < RETRY_TIME; i++) {
+            try {
+                dbNames = bdbEnvironment.getDatabaseNames();
+            } catch (InsufficientLogException insufficientLogEx) {
+                // Copy the missing log files from a member of the replication group who owns the files
+                LOG.warn("catch insufficient log exception. will recover and try again.", insufficientLogEx);
+                NetworkRestore restore = new NetworkRestore();
+                NetworkRestoreConfig config = new NetworkRestoreConfig();
+                config.setRetainLogFiles(false);
+                restore.execute(insufficientLogEx, config);
+                bdbEnvironment.close();
+                bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort,
+                        helperNode.first + ":" + helperNode.second, Catalog.getCurrentCatalog().isElectable());
+            } catch (RollbackException rollbackEx) {
+                LOG.warn("catch rollback log exception. will reopen the ReplicatedEnvironment.", rollbackEx);
+                bdbEnvironment.closeReplicatedEnvironment();
+                bdbEnvironment.openReplicatedEnvironment(new File(environmentPath));
+            }
+        }
+
+        return dbNames;
     }
     
     public boolean isPortUsing(String host, int port) throws UnknownHostException {  

fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBEnvironment.java
@@ -33,6 +33,7 @@ import com.sleepycat.je.Durability.SyncPolicy;
 import com.sleepycat.je.EnvironmentConfig;
 import com.sleepycat.je.EnvironmentFailureException;
 import com.sleepycat.je.rep.InsufficientLogException;
+import com.sleepycat.je.rep.RollbackException;
 import com.sleepycat.je.rep.NetworkRestore;
 import com.sleepycat.je.rep.NetworkRestoreConfig;
 import com.sleepycat.je.rep.NoConsistencyRequiredPolicy;
@@ -320,6 +321,8 @@ public class BDBEnvironment {
                 break;
             } catch (InsufficientLogException e) {
                 throw e;
+            } catch (RollbackException e) {
+                throw e;
             } catch (EnvironmentFailureException e) {
                 tried++;
                 if (tried == RETRY_TIME) {
@@ -384,6 +387,40 @@ public class BDBEnvironment {
         }
     }
     
+        // Close environment
+    public void closeReplicatedEnvironment() {
+        if (replicatedEnvironment != null) {
+            try {
+                // Finally, close the store and environment.
+                replicatedEnvironment.close();
+            } catch (DatabaseException exception) {
+                LOG.error("Error closing replicatedEnvironment", exception);
+                System.exit(-1);
+            }
+        }
+    }
+        // open environment
+    public void openReplicatedEnvironment(File envHome) {
+        for (int i = 0; i < RETRY_TIME; i++) {
+            try {
+                // open the environment
+                replicatedEnvironment = new ReplicatedEnvironment(envHome, replicationConfig, environmentConfig);
+                break;
+            } catch (DatabaseException e) {
+                if (i < RETRY_TIME - 1) {
+                    try {
+                        Thread.sleep(5 * 1000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                } else {
+                    LOG.error("error to open replicated environment. will exit.", e);
+                    System.exit(-1);
+                }
+            }
+        }
+    }
+
     private SyncPolicy getSyncPolicy(String policy) {
         if (policy.equalsIgnoreCase("SYNC")) {
             return Durability.SyncPolicy.SYNC;

commit 620041f9b5cb313806d52da445242917688944fb

[Bug] Fix bdbje getDatabaseNames() bug and scan node close bug (#6769)
1. This bug is introduced from #6582
2. Optimize the error log of Address used used error msg.
3. Add some document about compilation.
    1. Add a custom thirdparty download url.
    2. Add a custom com.alibaba maven jar package for DataX.
4. Fix bug that BE crash when closing scan node, introduced from #6622.

be/src/exec/olap_scan_node.cpp
@@ -372,8 +372,11 @@ Status OlapScanNode::close(RuntimeState* state) {
     _row_batch_added_cv.notify_all();
     _scan_batch_added_cv.notify_all();
 
-    // join transfer thread
-    _transfer_thread->join();
+	// _transfer_thread
+	// _transfer_thread may not be initialized. So need to check it
+	if (_transfer_thread != nullptr) {
+		_transfer_thread->join();
+	}
 
     // clear some row batch in queue
     for (auto row_batch : _materialized_row_batches) {

be/src/runtime/routine_load/routine_load_task_executor.cpp
@@ -351,6 +351,7 @@ void RoutineLoadTaskExecutor::err_handler(StreamLoadContext* ctx, const Status& 
 // for test only
 Status RoutineLoadTaskExecutor::_execute_plan_for_test(StreamLoadContext* ctx) {
     auto mock_consumer = [this, ctx]() {
+		ctx->ref();
         std::shared_ptr<StreamLoadPipe> pipe = _exec_env->load_stream_mgr()->get(ctx->id);
         bool eof = false;
         std::stringstream ss;
@@ -377,6 +378,9 @@ Status RoutineLoadTaskExecutor::_execute_plan_for_test(StreamLoadContext* ctx) {
             } else {
                 ss << one;
             }
+        }
+        if (ctx->unref()) {
+            delete ctx;
         }
     };
 

fe/fe-core/src/main/java/org/apache/doris/PaloFe.java
@@ -25,6 +25,7 @@ import org.apache.doris.common.Log4jConfig;
 import org.apache.doris.common.ThreadPoolManager;
 import org.apache.doris.common.Version;
 import org.apache.doris.common.util.JdkUtils;
+import org.apache.doris.common.util.NetUtils;
 import org.apache.doris.http.HttpServer;
 import org.apache.doris.journal.bdbje.BDBDebugger;
 import org.apache.doris.journal.bdbje.BDBTool;
@@ -34,6 +35,9 @@ import org.apache.doris.service.ExecuteEnv;
 import org.apache.doris.service.FeServer;
 import org.apache.doris.service.FrontendOptions;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -41,9 +45,6 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.google.common.base.Charsets;
-import com.google.common.base.Strings;
 
 import java.io.File;
 import java.io.IOException;
@@ -111,6 +112,9 @@ public class PaloFe {
 
             FrontendOptions.init();
 
+            // check all port
+            checkAllPorts();
+
             if (Config.enable_bdbje_debug_mode) {
                 // Start in BDB Debug mode
                 BDBDebugger.get().startDebugMode(dorisHomeDir);
@@ -129,7 +133,7 @@ public class PaloFe {
             FeServer feServer = new FeServer(Config.rpc_port);
 
             feServer.start();
-
+            
             if (!Config.enable_http_server_v2) {
                 HttpServer httpServer = new HttpServer(
                         Config.http_port,
@@ -159,6 +163,25 @@ public class PaloFe {
         } catch (Throwable e) {
             e.printStackTrace();
             return;
+        }
+    }
+
+    private static void checkAllPorts() throws IOException {
+        if (!NetUtils.isPortAvailable(FrontendOptions.getLocalHostAddress(), Config.edit_log_port,
+                "Edit log port", NetUtils.EDIT_LOG_PORT_SUGGESTION)) {
+            throw new IOException("port " + Config.edit_log_port + " already in use");
+        }
+        if (!NetUtils.isPortAvailable(FrontendOptions.getLocalHostAddress(), Config.http_port,
+                "Http port", NetUtils.HTTP_PORT_SUGGESTION)) {
+            throw new IOException("port " + Config.http_port + " already in use");
+        }
+        if (!NetUtils.isPortAvailable(FrontendOptions.getLocalHostAddress(), Config.query_port,
+                "Query port", NetUtils.QUERY_PORT_SUGGESTION)) {
+            throw new IOException("port " + Config.query_port + " already in use");
+        }
+        if (!NetUtils.isPortAvailable(FrontendOptions.getLocalHostAddress(), Config.rpc_port,
+                "Rpc port", NetUtils.RPC_PORT_SUGGESTION)) {
+            throw new IOException("port " + Config.rpc_port + " already in use");
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/catalog/Catalog.java
@@ -287,6 +287,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+
 import javax.annotation.Nullable;
 
 public class Catalog {
@@ -1456,7 +1457,9 @@ public class Catalog {
 
         Frontend fe = checkFeExist(selfNode.first, selfNode.second);
         if (fe == null) {
-            LOG.error("current node is not added to the cluster, will exit");
+            LOG.error("current node {}:{} is not added to the cluster, will exit." +
+                            " Your FE IP maybe changed, please set 'priority_networks' config in fe.conf properly.",
+                    selfNode.first, selfNode.second);
             System.exit(-1);
         } else if (fe.getRole() != role) {
             LOG.error("current node role is {} not match with frontend recorded role {}. will exit", role,

fe/fe-core/src/main/java/org/apache/doris/common/util/NetUtils.java
@@ -20,9 +20,12 @@ package org.apache.doris.common.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
+import java.net.ServerSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
@@ -30,6 +33,14 @@ import java.util.List;
 
 public class NetUtils {
     private static final Logger LOG = LogManager.getLogger(NetUtils.class);
+
+    public static final String EDIT_LOG_PORT_SUGGESTION = "Please change the 'edit_log_port' in fe.conf and try again." +
+            " But if this is not the first time your start this FE, please DO NOT change it. " +
+            " You need to find the service that occupies the port and shut it down, and then return the port to Doris.";
+    public static final String QUERY_PORT_SUGGESTION = "Please change the 'query_port' in fe.conf and try again.";
+    public static final String HTTP_PORT_SUGGESTION = "Please change the 'http_port' in fe.conf and try again. " +
+            "But you need to make sure that ALL FEs http_port are same.";
+    public static final String RPC_PORT_SUGGESTION = "Please change the 'rpc_port' in fe.conf and try again.";
 
     // Target format is "host:port"
     public static InetSocketAddress createSocketAddr(String target) {
@@ -73,5 +84,33 @@ public class NetUtils {
             hostName = "unknown";
         }
         return hostName;
+    }
+
+    // This is the implementation is inspired by Apache camel project:
+    public static boolean isPortAvailable(String host, int port, String portName, String suggestion) {
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+            LOG.warn("{} {} is already in use. {}", portName, port, suggestion, e);
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /* should not be thrown */
+                }
+            }
+        }
+        return false;
     }
 }

fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -46,9 +46,6 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -82,19 +79,10 @@ public class BDBJEJournal implements Journal {
      * node name is ip_port (the port is edit_log_port)
      */
     private void initBDBEnv(String nodeName) {
-        environmentPath = Catalog.getCurrentCatalog().getBdbDir();
-        try {
-            Pair<String, Integer> selfNode = Catalog.getCurrentCatalog().getSelfNode();
-            if (isPortUsing(selfNode.first, selfNode.second)) {
-                LOG.error("edit_log_port {} is already in use. will exit.", selfNode.second);
-                System.exit(-1);
-            }
-            selfNodeName = nodeName;
-            selfNodeHostPort = selfNode.first + ":" + selfNode.second;
-        } catch (IOException e) {
-            LOG.error(e);
-            System.exit(-1);
-        }
+        environmentPath = Catalog.getServingCatalog().getBdbDir();
+        Pair<String, Integer> selfNode = Catalog.getServingCatalog().getSelfNode();
+        selfNodeName = nodeName;
+        selfNodeHostPort = selfNode.first + ":" + selfNode.second;
     }
 
     /*
@@ -308,11 +296,11 @@ public class BDBJEJournal implements Journal {
         if (bdbEnvironment == null) {
             File dbEnv = new File(environmentPath);
             bdbEnvironment = new BDBEnvironment();
-            Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
+            Pair<String, Integer> helperNode = Catalog.getServingCatalog().getHelperNode();
             String helperHostPort = helperNode.first + ":" + helperNode.second;
             try {
                 bdbEnvironment.setup(dbEnv, selfNodeName, selfNodeHostPort,
-                                     helperHostPort, Catalog.getCurrentCatalog().isElectable());
+                        helperHostPort, Catalog.getServingCatalog().isElectable());
             } catch (Exception e) {
                 LOG.error("catch an exception when setup bdb environment. will exit.", e);
                 System.exit(-1);
@@ -320,7 +308,6 @@ public class BDBJEJournal implements Journal {
         }
         
         // Open a new journal database or get last existing one as current journal database
-        Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
         List<Long> dbNames = null;
         for (int i = 0; i < RETRY_TIME; i++) {
             try {
@@ -333,36 +320,43 @@ public class BDBJEJournal implements Journal {
                 if (dbNames.size() == 0) {
                     /*
                      *  This is the very first time to open. Usually, we will open a new database named "1".
-                     *  But when we start cluster with an image file copied from other cluster, 
+                     *  But when we start cluster with an image file copied from other cluster,
                      *  here we should open database with name image max journal id + 1.
-                     *  (default Catalog.getCurrentCatalog().getReplayedJournalId() is 0)
+                     *  (default Catalog.getServingCatalog().getReplayedJournalId() is 0)
                      */
-                    String dbName = Long.toString(Catalog.getCurrentCatalog().getReplayedJournalId() + 1);
+                    String dbName = Long.toString(Catalog.getServingCatalog().getReplayedJournalId() + 1);
                     LOG.info("the very first time to open bdb, dbname is {}", dbName);
                     currentJournalDB = bdbEnvironment.openDatabase(dbName);
                 } else {
                     // get last database as current journal database
                     currentJournalDB = bdbEnvironment.openDatabase(dbNames.get(dbNames.size() - 1).toString());
                 }
-                
+
                 // set next journal id
                 nextJournalId.set(getMaxJournalId() + 1);
-                
+
                 break;
             } catch (InsufficientLogException insufficientLogEx) {
-                // Copy the missing log files from a member of the replication group who owns the files
-                LOG.warn("catch insufficient log exception. will recover and try again.", insufficientLogEx);
-                NetworkRestore restore = new NetworkRestore();
-                NetworkRestoreConfig config = new NetworkRestoreConfig();
-                config.setRetainLogFiles(false);
-                restore.execute(insufficientLogEx, config);
-                bdbEnvironment.close();
-                bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort, 
-                                     helperNode.first + ":" + helperNode.second, Catalog.getCurrentCatalog().isElectable());
+                reSetupBdbEnvironment(insufficientLogEx);
             }
         }
     }
-    
+
+    private void reSetupBdbEnvironment(InsufficientLogException insufficientLogEx) {
+        LOG.warn("catch insufficient log exception. will recover and try again.", insufficientLogEx);
+        // Copy the missing log files from a member of the replication group who owns the files
+        // ATTN: here we use `getServingCatalog()`, because only serving catalog has helper nodes.
+        Pair<String, Integer> helperNode = Catalog.getServingCatalog().getHelperNode();
+        NetworkRestore restore = new NetworkRestore();
+        NetworkRestoreConfig config = new NetworkRestoreConfig();
+        config.setRetainLogFiles(false);
+        restore.execute(insufficientLogEx, config);
+        bdbEnvironment.close();
+        bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort,
+                helperNode.first + ":" + helperNode.second,
+                Catalog.getServingCatalog().isElectable());
+    }
+
     @Override
     public void deleteJournals(long deleteToJournalId) {
         List<Long> dbNames = getDatabaseNames();
@@ -370,7 +364,7 @@ public class BDBJEJournal implements Journal {
             LOG.info("delete database names is null.");
             return;
         }
-        
+
         String msg = "existing database names: ";
         for (long name : dbNames) {
             msg += name + " ";
@@ -420,41 +414,36 @@ public class BDBJEJournal implements Journal {
         }
 
         // Open a new journal database or get last existing one as current journal database
-        Pair<String, Integer> helperNode = Catalog.getCurrentCatalog().getHelperNode();
         List<Long>  dbNames = null;
         for (int i = 0; i < RETRY_TIME; i++) {
             try {
                 dbNames = bdbEnvironment.getDatabaseNames();
+                break;
             } catch (InsufficientLogException insufficientLogEx) {
-                // Copy the missing log files from a member of the replication group who owns the files
-                LOG.warn("catch insufficient log exception. will recover and try again.", insufficientLogEx);
-                NetworkRestore restore = new NetworkRestore();
-                NetworkRestoreConfig config = new NetworkRestoreConfig();
-                config.setRetainLogFiles(false);
-                restore.execute(insufficientLogEx, config);
-                bdbEnvironment.close();
-                bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort,
-                        helperNode.first + ":" + helperNode.second, Catalog.getCurrentCatalog().isElectable());
+                /*
+                 * If this is not a checkpoint thread, which means this maybe the FE startup thread,
+                 * or a replay thread. We will reopen bdbEnvironment for these 2 cases to get valid log
+                 * from helper nodes.
+                 *
+                 * The checkpoint thread will only run on Master FE. And Master FE should not encounter
+                 * these exception. So if it happens, throw exception out.
+                 */
+                if (!Catalog.isCheckpointThread()) {
+                    reSetupBdbEnvironment(insufficientLogEx);
+                } else {
+                    throw insufficientLogEx;
+                }
             } catch (RollbackException rollbackEx) {
-                LOG.warn("catch rollback log exception. will reopen the ReplicatedEnvironment.", rollbackEx);
-                bdbEnvironment.closeReplicatedEnvironment();
-                bdbEnvironment.openReplicatedEnvironment(new File(environmentPath));
+                if (!Catalog.isCheckpointThread()) {
+                    LOG.warn("catch rollback log exception. will reopen the ReplicatedEnvironment.", rollbackEx);
+                    bdbEnvironment.closeReplicatedEnvironment();
+                    bdbEnvironment.openReplicatedEnvironment(new File(environmentPath));
+                } else {
+                    throw rollbackEx;
+                }
             }
         }
 
         return dbNames;
-    }
-    
-    public boolean isPortUsing(String host, int port) throws UnknownHostException {  
-        boolean flag = false;  
-        InetAddress theAddress = InetAddress.getByName(host);
-        try {  
-            Socket socket = new Socket(theAddress, port);
-            flag = true;
-            socket.close();
-        } catch (IOException e) {
-            // do nothing
-        }  
-        return flag;  
     }
 }

fe/fe-core/src/main/java/org/apache/doris/master/Checkpoint.java
@@ -83,7 +83,7 @@ public class Checkpoint extends MasterDaemon {
             if (imageVersion >= checkPointVersion) {
                 return;
             }
-        } catch (IOException e) {
+        } catch (Throwable e) {
             LOG.error("Does not get storage info", e);
             if (MetricRepo.isInit) {
                 MetricRepo.COUNTER_IMAGE_WRITE_FAILED.increase(1L);
@@ -117,7 +117,7 @@ public class Checkpoint extends MasterDaemon {
                 MetricRepo.COUNTER_IMAGE_WRITE_SUCCESS.increase(1L);
             }
             LOG.info("checkpoint finished save image.{}", replayedJournalId);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             e.printStackTrace();
             LOG.error("Exception when generate new image file", e);
             if (MetricRepo.isInit) {
@@ -127,7 +127,7 @@ public class Checkpoint extends MasterDaemon {
         } finally {
             // destroy checkpoint catalog, reclaim memory
             catalog = null;
-            Catalog.destroyCheckpoint(); 
+            Catalog.destroyCheckpoint();
         }
         
         // push image file to all the other non master nodes
@@ -202,7 +202,7 @@ public class Checkpoint extends MasterDaemon {
                             if (minOtherNodesJournalId > id) {
                                 minOtherNodesJournalId = id;
                             }
-                        } catch (IOException e) {
+                        } catch (Throwable e) {
                             throw new CheckpointException(String.format("Exception when getting current replayed journal id. host=%s, port=%d",
                                     host, port), e);
                         } finally {
@@ -235,7 +235,7 @@ public class Checkpoint extends MasterDaemon {
             if (MetricRepo.isInit) {
                 MetricRepo.COUNTER_IMAGE_CLEAN_SUCCESS.increase(1L);
             }
-        } catch (IOException e) {
+        } catch (Throwable e) {
             LOG.error("Master delete old image file fail.", e);
             if (MetricRepo.isInit) {
                 MetricRepo.COUNTER_IMAGE_CLEAN_FAILED.increase(1L);