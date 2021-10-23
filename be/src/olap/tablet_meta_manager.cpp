88760d66d112927cba52ab22c448133c26a355d1
qiye <jianliang5669@gmail.com>
2021-10-23 16:51:58 +0800

    [MetaTool]add error message when loading meta by meta tool (#6893)
    When loading meta by meta_tool goes wrong, we only get an error code from `json2pb`,
    which is inconvenient for us to locate the problem.
    
    This change is adding error message when loading meta goes wrong.
    
    Log change is like below.
    
    ```
    # before
    ./meta_tool --root_path=/home/disk1/qjl/mydoris/be/storage --operation=load_meta --json_meta_path=/home/disk1/qjl/data/meta-json.json
    WARNING: Logging before InitGoogleLogging() is written to STDERR
    I1020 11:41:56.564241 74937 data_dir.cpp:837] path: /home/disk1/qjl/mydoris/be/storage total capacity: 7750843404288, available capacity: 7583325925376
    I1020 11:41:56.564415 74937 data_dir.cpp:275] path: /home/disk1/qjl/mydoris/be/storage, hash: 7528840506668047470
    load meta failed, status:-1410
    
    # after 
    ./meta_tool --root_path=/home/disk1/qjl/mydoris/be/storage --operation=load_meta --json_meta_path=/home/disk1/qjl/data/meta-json.json
    WARNING: Logging before InitGoogleLogging() is written to STDERR
    I1020 14:41:40.084342 50727 data_dir.cpp:837] path: /home/disk1/qjl/mydoris/be/storage total capacity: 7750843404288, available capacity: 7584601022464
    I1020 14:41:40.084496 50727 data_dir.cpp:275] path: /home/disk1/qjl/mydoris/be/storage, hash: 7528840506668047470
    E1020 14:41:40.163007 50727 tablet_meta_manager.cpp:161] JSON to protobuf message failed: Fail to decode base64 string=0
    load meta failed, status:-1410
    ```

--- be/src/olap/tablet_meta_manager.cpp
+++ be/src/olap/tablet_meta_manager.cpp
@@ -155,8 +155,10 @@ OLAPStatus TabletMetaManager::load_json_meta(DataDir* store, const std::string& 
     }
     boost::algorithm::trim(json_meta);
     TabletMetaPB tablet_meta_pb;
-    bool ret = json2pb::JsonToProtoMessage(json_meta, &tablet_meta_pb);
+    std::string error;
+    bool ret = json2pb::JsonToProtoMessage(json_meta, &tablet_meta_pb, &error);
     if (!ret) {
+        LOG(ERROR) << "JSON to protobuf message failed: " << error;
         return OLAP_ERR_HEADER_LOAD_JSON_HEADER;
     }
 
