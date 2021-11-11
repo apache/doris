632f8fcc754d23f5674e32ddef2ddec9664b759e
pengxiangyu <diablowcg@163.com>
2021-11-11 15:21:21 +0800

    [libhdfs] Add errno for hdfs writer. when no dir, hdfs writer open failed, the dir need to be created. (#7050)
    1. Add errno message for hdfs writer failed.
    2. When call openWrite for hdfs, the dir will be created when it doesn't exist,

--- be/src/exec/hdfs_writer.cpp
+++ be/src/exec/hdfs_writer.cpp
@@ -17,7 +17,10 @@
 
 #include "exec/hdfs_writer.h"
 
+#include <filesystem>
+
 #include "common/logging.h"
+#include "service/backend_options.h"
 
 namespace doris {
 const static std::string FS_KEY = "fs.defaultFS";
@@ -47,11 +50,30 @@ Status HDFSWriter::open() {
         // the path already exists
         return Status::AlreadyExist(_path + " already exists.");
     }
+
+    std::filesystem::path hdfs_path(_path);
+    std::string hdfs_dir = hdfs_path.parent_path().string();
+    exists = hdfsExists(_hdfs_fs, hdfs_dir.c_str());
+    if (exists != 0) {
+        LOG(INFO) << "hdfs dir doesn't exist, create it: " << hdfs_dir;
+        int ret = hdfsCreateDirectory(_hdfs_fs, hdfs_dir.c_str());
+        if (ret != 0) {
+            std::stringstream ss;
+            ss << "create dir failed. " << "(BE: " << BackendOptions::get_localhost() << ")"
+                    << " namenode: " << _namenode << " path: " << hdfs_dir
+                    << ", err: " << strerror(errno);
+            LOG(WARNING) << ss.str();
+            return Status::InternalError(ss.str());
+        }
+    }
     // open file
     _hdfs_file = hdfsOpenFile(_hdfs_fs, _path.c_str(), O_WRONLY, 0, 0, 0);
     if (_hdfs_file == nullptr) {
         std::stringstream ss;
-        ss << "open file failed. namenode:" << _namenode << " path:" << _path;
+        ss << "open file failed. " << "(BE: " << BackendOptions::get_localhost() << ")"
+                << " namenode:" << _namenode << " path:" << _path
+                << ", err: " << strerror(errno);
+        LOG(WARNING) << ss.str();
         return Status::InternalError(ss.str());
     }
     LOG(INFO) << "open file. namenode:" << _namenode << " path:" << _path;
@@ -66,7 +88,9 @@ Status HDFSWriter::write(const uint8_t* buf, size_t buf_len, size_t* written_len
     int32_t result = hdfsWrite(_hdfs_fs, _hdfs_file, buf, buf_len);
     if (result < 0) {
         std::stringstream ss;
-        ss << "write file failed. namenode:" << _namenode << " path:" << _path;
+        ss << "write file failed. " << "(BE: " << BackendOptions::get_localhost() << ")"
+                << "namenode:" << _namenode << " path:" << _path
+                << ", err: " << strerror(errno);
         LOG(WARNING) << ss.str();
         return Status::InternalError(ss.str());
     }
@@ -91,7 +115,9 @@ Status HDFSWriter::close() {
     int result = hdfsFlush(_hdfs_fs, _hdfs_file);
     if (result == -1) {
         std::stringstream ss;
-        ss << "failed to flush hdfs file. namenode:" << _namenode << " path:" << _path;
+        ss << "failed to flush hdfs file. " << "(BE: " << BackendOptions::get_localhost() << ")"
+                << "namenode:" << _namenode << " path:" << _path
+                << ", err: " << strerror(errno);
         LOG(WARNING) << ss.str();
         return Status::InternalError(ss.str());
     }
