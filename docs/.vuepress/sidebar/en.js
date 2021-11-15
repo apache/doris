195af3d7a07f02ca1c41979843cafb3c0563393e
morningman <morningman@163.com>
2021-11-15 20:21:52 +0800

    add doc

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -631,7 +631,8 @@ module.exports = [
         "be-vscode-dev",		
         "java-format-code",
         "cpp-format-code",
-        "How-to-Share-blogs"
+        "How-to-Share-blogs",
+		"minidump"
     ],
   },
   {
