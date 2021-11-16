3f45d959b26bfa8220f6b56d30ec0f7305122699
Mingyu Chen <morningman.cmy@gmail.com>
2021-11-16 18:37:54 +0800

    [License] Add License header for missing files (#7130)
    1. Add License header for missing files
    2. Modify the spark pom.xml to correct the location of `thrift`

--- extension/DataX/init-env.sh
+++ extension/DataX/init-env.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-# Copyright (c) 2017, Baidu.com, Inc. All Rights Reserved
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
