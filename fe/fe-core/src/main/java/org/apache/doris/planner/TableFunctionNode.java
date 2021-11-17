eeed090b3915d81b4de3eeeaa5fd66f545982575
EmmyMiao87 <522274284@qq.com>
2021-11-17 11:13:08 +0800

    [Lateral View] Multi lateral views map one TableFunctionNode (#7000)
    1. Forbidden non-string column as params of explode_view.
    The first param of explode_view must be string column(VARCHAR/CHAR/STRING)
    
    2. N-1 n lateral views map one TableFunctionNode
    The TableFunctionNode include all of fnExprs which belongs to one table.
    For example:
    select pageid,mycol1, mycol2 from pageAds
        lateral view explode_string(col1) myTable1 as mycol1
        lateral view explode_string(col2) myTable2 as mycol2;
    TableFunctionNode
    |----
    |- fnExprList: explode_string(col1), explode_string(col2)

--- fe/fe-core/src/main/java/org/apache/doris/planner/TableFunctionNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/TableFunctionNode.java
@@ -18,34 +18,41 @@
 package org.apache.doris.planner;
 
 import org.apache.doris.analysis.Analyzer;
+import org.apache.doris.analysis.Expr;
 import org.apache.doris.analysis.FunctionCallExpr;
 import org.apache.doris.analysis.LateralViewRef;
+import org.apache.doris.analysis.TupleId;
 import org.apache.doris.common.UserException;
 import org.apache.doris.thrift.TExplainLevel;
 import org.apache.doris.thrift.TPlanNode;
 import org.apache.doris.thrift.TPlanNodeType;
 import org.apache.doris.thrift.TTableFunctionNode;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TableFunctionNode extends PlanNode {
 
-    private LateralViewRef lateralViewRef;
+    private List<LateralViewRef> lateralViewRefs;
+    private List<FunctionCallExpr> fnCallExprList;
+    private List<TupleId> lateralViewTupleIds;
 
-    private FunctionCallExpr fnCallExpr;
-
-    protected TableFunctionNode(PlanNodeId id, PlanNode inputNode, LateralViewRef lateralViewRef) {
+    protected TableFunctionNode(PlanNodeId id, PlanNode inputNode, List<LateralViewRef> lateralViewRefs) {
         super(id, "TABLE FUNCTION NODE");
         tupleIds.addAll(inputNode.getTupleIds());
         tblRefIds.addAll(inputNode.getTupleIds());
-        tupleIds.add(lateralViewRef.getDesc().getId());
-        tblRefIds.add(lateralViewRef.getDesc().getId());
+        lateralViewTupleIds = lateralViewRefs.stream().map(e -> e.getDesc().getId())
+                .collect(Collectors.toList());
+        tupleIds.addAll(lateralViewTupleIds);
+        tblRefIds.addAll(lateralViewTupleIds);
         children.add(inputNode);
-        this.lateralViewRef = lateralViewRef;
+        this.lateralViewRefs = lateralViewRefs;
     }
 
     @Override
     public void init(Analyzer analyzer) throws UserException {
         super.init(analyzer);
-        fnCallExpr = lateralViewRef.getFnExpr();
+        fnCallExprList = lateralViewRefs.stream().map(e -> e.getFnExpr()).collect(Collectors.toList());
         computeStats(analyzer);
     }
 
@@ -59,10 +66,27 @@ public class TableFunctionNode extends PlanNode {
     @Override
     public String getNodeExplainString(String prefix, TExplainLevel detailLevel) {
         StringBuilder output = new StringBuilder();
-        output.append(prefix + "table function: ").append(fnCallExpr.toSql() + "\n");
+        output.append(prefix + "table function: ");
+        for (FunctionCallExpr fnExpr : fnCallExprList) {
+            output.append(fnExpr.toSqlImpl() + " ");
+        }
+        output.append("\n");
+
+        output.append(prefix + "lateral view tuple id: ");
+        for (TupleId tupleId : lateralViewTupleIds) {
+            output.append(tupleId.asInt() + " ");
+        }
+        output.append("\n");
+
         if (detailLevel == TExplainLevel.BRIEF) {
             return output.toString();
         }
+
+        output.append(prefix + "tuple id: ");
+        for (TupleId tupleId : tupleIds) {
+            output.append(tupleId.asInt() + " ");
+        }
+        output.append("\n");
 
         if (!conjuncts.isEmpty()) {
             output.append(prefix).append("PREDICATES: ").append(
@@ -76,6 +100,6 @@ public class TableFunctionNode extends PlanNode {
     protected void toThrift(TPlanNode msg) {
         msg.node_type = TPlanNodeType.TABLE_FUNCTION_NODE;
         msg.table_function_node = new TTableFunctionNode();
-        msg.table_function_node.setFnCallExpr(fnCallExpr.treeToThrift());
+        msg.table_function_node.setFnCallExprList(Expr.treesToThrift(fnCallExprList));
     }
 }
