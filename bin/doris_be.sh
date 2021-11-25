ff51c6e54a1d20689377a74550206f2321e8de19
jiafeng.zhang <zhangjf1@gmail.com>
2021-11-25 20:59:49 +0800

    bugfix
    bugfix

--- bin/doris_be.sh
+++ bin/doris_be.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 export DORIS_HOME=`cd "$curdir"; pwd`
-export LOG_DIR=${DORIS_HOME}/log
 export PID_DIR=`cd "$curdir"; pwd`
 function start() {
     echo "Starting Apache Doris Frontend"
@@ -12,7 +27,6 @@ function start() {
       sleep 1s
       pid=`cat $DORIS_HOME/bin/be.pid`
       echo "Doris Be pid : ${pid}"
-      echo `sed -n '$p' $DORIS_HOME/log/be.out`
       echo "Apache Doris Backend start success!"
     fi
 }
@@ -32,6 +46,7 @@ function restart() {
     echo "****************************"
     echo "Ready to stop Doris Backend"
     stop
+    sleep 5s
     echo "****************************"
     echo "Ready to start Doris Backend"
     start
