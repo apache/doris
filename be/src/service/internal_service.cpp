df8fb95b816320788c9c9dad981dc04bb7a842b7
Zhengguo Yang <yangzhgg@gmail.com>
2021-12-01 10:02:34 +0800

    1. Fix an error when fetch string type field may cause malform packet error. (#7262)
    This is beacuse of an const MAX_PHYSICAL_PACKET_LENGTH  in fe should be 2^24 -1,
       but it is set as 2^24 -2 by mistake.
    2. Fix bitmap_to_string may failed when the result is large than 2G

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -195,9 +195,7 @@ void PInternalServiceImpl<T>::fetch_data(google::protobuf::RpcController* cntl_b
                                          const PFetchDataRequest* request, PFetchDataResult* result,
                                          google::protobuf::Closure* done) {
     brpc::Controller* cntl = static_cast<brpc::Controller*>(cntl_base);
-    bool resp_in_attachment =
-            request->has_resp_in_attachment() ? request->resp_in_attachment() : true;
-    GetResultBatchCtx* ctx = new GetResultBatchCtx(cntl, resp_in_attachment, result, done);
+    GetResultBatchCtx* ctx = new GetResultBatchCtx(cntl, result, done);
     _exec_env->result_mgr()->fetch_data(request->finst_id(), ctx);
 }
 
