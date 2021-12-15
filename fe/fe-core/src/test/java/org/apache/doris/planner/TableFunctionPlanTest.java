428eb1cc3416a062c1f1ec764ce894b505e8e2c4
morningman <morningman@163.com>
2021-12-15 11:58:30 +0800

    add ut

--- fe/fe-core/src/test/java/org/apache/doris/planner/TableFunctionPlanTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/planner/TableFunctionPlanTest.java
@@ -55,6 +55,11 @@ public class TableFunctionPlanTest {
                 + "DUPLICATE KEY(k1) distributed by hash(k1) buckets 3 properties('replication_num' = '1');";
         CreateTableStmt createTableStmt = (CreateTableStmt) UtFrameUtils.parseAndAnalyzeStmt(createTblStmtStr, ctx);
         Catalog.getCurrentCatalog().createTable(createTableStmt);
+
+        createTblStmtStr = "create table db1.tbl2(k1 int, k2 varchar, v1 bitmap bitmap_union) "
+                + "distributed by hash(k1) buckets 3 properties('replication_num' = '1');";
+        createTableStmt = (CreateTableStmt) UtFrameUtils.parseAndAnalyzeStmt(createTblStmtStr, ctx);
+        Catalog.getCurrentCatalog().createTable(createTableStmt);
     }
 
     // test planner
@@ -381,29 +386,59 @@ public class TableFunctionPlanTest {
         Assert.assertTrue(explainString.contains("tuple ids: 1 3"));
         String formatString = explainString.replaceAll(" ", "");
         Assert.assertTrue(formatString.contains(
-                          "SlotDescriptor{id=0,col=k1,type=INT}\n"
+                "SlotDescriptor{id=0,col=k1,type=INT}\n"
                         + "parent=0\n"
                         + "materialized=true"
         ));
         Assert.assertTrue(formatString.contains(
-                          "SlotDescriptor{id=1,col=k2,type=VARCHAR(*)}\n"
+                "SlotDescriptor{id=1,col=k2,type=VARCHAR(*)}\n"
                         + "parent=0\n"
                         + "materialized=true"
         ));
         Assert.assertTrue(formatString.contains(
-                          "SlotDescriptor{id=2,col=null,type=INT}\n"
+                "SlotDescriptor{id=2,col=null,type=INT}\n"
                         + "parent=1\n"
                         + "materialized=true"
         ));
         Assert.assertTrue(formatString.contains(
-                          "SlotDescriptor{id=3,col=null,type=VARCHAR(*)}\n"
+                "SlotDescriptor{id=3,col=null,type=VARCHAR(*)}\n"
                         + "parent=1\n"
                         + "materialized=true"
         ));
         Assert.assertTrue(formatString.contains(
-                          "SlotDescriptor{id=6,col=e1,type=VARCHAR(*)}\n"
+                "SlotDescriptor{id=6,col=e1,type=VARCHAR(*)}\n"
                         + "parent=3\n"
                         + "materialized=true"
         ));
+    }
+
+    @Test
+    public void testExplodeBitmap() throws Exception {
+        String sql = "desc select k1, e1 from db1.tbl2 lateral view explode_bitmap(v1) tmp1 as e1 ";
+        String explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, true);
+        System.out.println(explainString);
+        Assert.assertTrue(explainString.contains("table function: explode_bitmap(`default_cluster:db1`.`tbl2`.`v1`)"));
+        Assert.assertTrue(explainString.contains("output slot id: 1 2"));
+    }
+
+    @Test
+    public void testExplodeJsonArray() throws Exception {
+        String sql = "desc select k1, e1 from db1.tbl2 lateral view explode_json_array_int('[1,2,3]') tmp1 as e1 ";
+        String explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, true);
+        System.out.println(explainString);
+        Assert.assertTrue(explainString.contains("table function: explode_json_array_int('[1,2,3]')"));
+        Assert.assertTrue(explainString.contains("output slot id: 0 1"));
+
+        sql = "desc select k1, e1 from db1.tbl2 lateral view explode_json_array_string('[\"a\",\"b\",\"c\"]') tmp1 as e1 ";
+        explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, true);
+        System.out.println(explainString);
+        Assert.assertTrue(explainString.contains("table function: explode_json_array_string('[\"a\",\"b\",\"c\"]')"));
+        Assert.assertTrue(explainString.contains("output slot id: 0 1"));
+
+        sql = "desc select k1, e1 from db1.tbl2 lateral view explode_json_array_double('[1.1, 2.2, 3.3]') tmp1 as e1 ";
+        explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, true);
+        System.out.println(explainString);
+        Assert.assertTrue(explainString.contains("table function: explode_json_array_double('[1.1, 2.2, 3.3]')"));
+        Assert.assertTrue(explainString.contains("output slot id: 0 1"));
     }
 }
