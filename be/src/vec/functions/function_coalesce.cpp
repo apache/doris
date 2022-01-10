0f399d37dbf68937f00c5b0e95758812727b75b2
zhangstar333 <2561612514@qq.com>
2022-01-10 11:25:41 +0800

    [Enhancement][Vectorized] use simd to speed up coalesce function

--- be/src/vec/functions/function_coalesce.cpp
+++ be/src/vec/functions/function_coalesce.cpp
@@ -52,36 +52,51 @@ public:
     Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
                         size_t result, size_t input_rows_count) override {
         DCHECK_GE(arguments.size(), 1);
+        DataTypePtr result_type = block.get_by_position(result).type;
         ColumnNumbers filtered_args;
         filtered_args.reserve(arguments.size());
-        for (const auto& arg : arguments) {
-            const auto& type = block.get_by_position(arg).type;
-            if (type->only_null()) {
+
+        for (size_t i = 0; i < arguments.size(); ++i) {
+            const auto& arg_type = block.get_by_position(arguments[i]).type;
+            if (arg_type->only_null()) {
                 continue;
             }
-            filtered_args.push_back(arg);
-            if (!type->is_nullable()) {
-                break;
+            filtered_args.push_back(arguments[i]);
+            if (!arg_type->is_nullable()) {
+                if (i == 0) { //if the first column not null, return it's directly
+                    block.get_by_position(result).column = block.get_by_position(arguments[0]).column;
+                    return Status::OK();
+                } else {
+                    break;
+                }
             }
         }
 
         size_t remaining_rows = input_rows_count;
         size_t argument_size = filtered_args.size();
-        std::vector<int> record_idx(input_rows_count, -1); //used to save column idx
+        std::vector<int> record_idx(input_rows_count, 0); //used to save column idx, record the result data of each row from which column 
+        std::vector<int> filled_flag(input_rows_count, 0); //used to save filled flag, in order to check current row whether have filled data
         MutableColumnPtr result_column;
-
-        DataTypePtr type = block.get_by_position(result).type;
-        if (!type->is_nullable()) {
-            result_column = type->create_column();
+        
+        if (!result_type->is_nullable()) {
+            result_column = result_type->create_column();
         } else {
-            result_column = remove_nullable(type)->create_column();
+            result_column = remove_nullable(result_type)->create_column();
         }
 
-        result_column->reserve(input_rows_count);
+        //because now the string types does not support random position writing,
+        //so insert into result data have two methods, one is for string types, one is for others type remaining
+        bool is_string_result = result_column->is_column_string();
+        if (is_string_result) {
+            result_column->reserve(input_rows_count);
+        } else {
+            result_column->insert_many_defaults(input_rows_count);
+        }
+
         auto return_type = std::make_shared<DataTypeUInt8>();
-        auto null_map = ColumnUInt8::create(input_rows_count, 1);
-        auto& null_map_data = null_map->get_data();
-        ColumnPtr argument_columns[argument_size];
+        auto null_map = ColumnUInt8::create(input_rows_count, 1);  //if null_map_data==1, the current row should be null
+        auto* __restrict null_map_data = null_map->get_data().data();
+        ColumnPtr argument_columns[argument_size]; //use to save nested_column if is nullable column
 
         for (size_t i = 0; i < argument_size; ++i) {
             block.get_by_position(filtered_args[i]).column =
@@ -102,37 +117,130 @@ public:
 
             auto res_column = std::move(*temporary_block.get_by_position(1).column->convert_to_full_column_if_const()).mutate();
             auto& res_map = assert_cast<ColumnVector<UInt8>*>(res_column.get())->get_data();
-            auto* res = res_map.data();
+            auto* __restrict res = res_map.data();
 
-            //TODO: if need to imporve performance in the feature, here it's could SIMD
-            for (size_t j = 0; j < input_rows_count && remaining_rows; ++j) {
-                if (res[j] && null_map_data[j]) {
-                    null_map_data[j] = 0;
-                    remaining_rows--;
-                    record_idx[j] = i;
+            // Here it's SIMD thought the compiler automatically
+            // true: res[j]==1 && null_map_data[j]==1, false: others
+            // if true: remaining_rows--; record_idx[j]=column_idx; null_map_data[j]=0, so the current row could fill result
+            for (size_t j = 0; j < input_rows_count; ++j) {
+                remaining_rows -= (res[j] & null_map_data[j]);
+                record_idx[j] += (res[j] & null_map_data[j]) * i;
+                null_map_data[j] -= (res[j] & null_map_data[j]);
+            }
+
+            if (remaining_rows == 0) {
+                //check whether all result data from the same column
+                int is_same_column = 1;
+                for (size_t row = 0; row < input_rows_count; ++row) {
+                    is_same_column = is_same_column & (record_idx[row] == record_idx[0]);
+                }
+
+                if (is_same_column) {
+                    if (result_type->is_nullable()) {
+                        block.get_by_position(result).column = make_nullable(argument_columns[i], false);
+                    } else {
+                        block.get_by_position(result).column = argument_columns[i];
+                    }
+                    return Status::OK();
+                }
+            }
+
+            if (!is_string_result) {
+                //if not string type, could check one column firstly,
+                //and then fill the not null value in result column,
+                //this method may result in higher CPU cache
+                filled_result_column(result_type, result_column, argument_columns[i], null_map_data,
+                                     filled_flag, input_rows_count);
+            }
+        }
+
+        if (is_string_result) {
+            //if string type,  should according to the record results, fill in result one by one, 
+            for (size_t row = 0; row < input_rows_count; ++row) {
+                if (null_map_data[row]) { //should be null
+                    result_column->insert_default();
+                } else {
+                    result_column->insert_from(*argument_columns[record_idx[row]].get(), row);
                 }
             }
         }
-        //TODO: According to the record results, fill in result one by one, 
-        //that's fill in result use different methods for different types,
-        //because now the string types does not support random position writing,
-        //But other type could, so could check one column, and fill the not null value in result column,
-        //and then next column, this method may result in higher CPU cache
-        for (int row = 0; row < input_rows_count; ++row) {
-            if (record_idx[row] == -1) {
-                result_column->insert_default();
-            } else {
-                result_column->insert_from(*argument_columns[record_idx[row]].get(), row);
-            }
-        }
 
-        if (type->is_nullable()) {
+        if (result_type->is_nullable()) {
             block.replace_by_position(result, ColumnNullable::create(std::move(result_column), std::move(null_map)));
         } else {
             block.replace_by_position(result, std::move(result_column));
         }
 
         return Status::OK();
+    }
+
+    template <typename ColumnType>
+    Status insert_result_data(MutableColumnPtr& result_column, ColumnPtr& argument_column,
+                              UInt8* null_map_data, std::vector<int>& filled_flag,
+                              size_t& input_rows_count) {
+        auto* __restrict result_raw_data =
+                reinterpret_cast<ColumnType*>(result_column.get())->get_data().data();
+        auto* __restrict column_raw_data =
+                reinterpret_cast<ColumnType*>(argument_column->assume_mutable().get())->get_data().data();
+
+        // Here it's SIMD thought the compiler automatically also
+        // true: null_map_data[row]==0 && filled_idx[row]==0
+        // if true, could filled current row data into result column
+        for (size_t row = 0; row < input_rows_count; ++row) {
+            result_raw_data[row] += (!(null_map_data[row] | filled_flag[row])) * column_raw_data[row];
+            filled_flag[row] += (!(null_map_data[row] | filled_flag[row]));
+        }
+        return Status::OK();
+    }
+
+    //TODO: this function is same as case when, should be replaced by macro
+    Status filled_result_column(const DataTypePtr& data_type, MutableColumnPtr& result_column,
+                                ColumnPtr& argument_column, UInt8* null_map_data,
+                                std::vector<int>& filled_flag, size_t& input_rows_count) {
+        WhichDataType which(data_type->is_nullable()
+                                    ? reinterpret_cast<const DataTypeNullable*>(data_type.get())
+                                              ->get_nested_type()
+                                    : data_type);
+        if (which.is_uint8()) {
+            return insert_result_data<ColumnUInt8>(result_column, argument_column, null_map_data,
+                                                   filled_flag, input_rows_count);
+        } else if (which.is_int16()) {
+            return insert_result_data<ColumnInt16>(result_column, argument_column, null_map_data,
+                                                   filled_flag, input_rows_count);
+        } else if (which.is_uint32()) {
+            return insert_result_data<ColumnUInt32>(result_column, argument_column, null_map_data,
+                                                    filled_flag, input_rows_count);
+        } else if (which.is_uint64()) {
+            return insert_result_data<ColumnUInt64>(result_column, argument_column, null_map_data,
+                                                    filled_flag, input_rows_count);
+        } else if (which.is_int8()) {
+            return insert_result_data<ColumnInt8>(result_column, argument_column, null_map_data,
+                                                  filled_flag, input_rows_count);
+        } else if (which.is_int16()) {
+            return insert_result_data<ColumnInt16>(result_column, argument_column, null_map_data,
+                                                   filled_flag, input_rows_count);
+        } else if (which.is_int32()) {
+            return insert_result_data<ColumnInt32>(result_column, argument_column, null_map_data,
+                                                   filled_flag, input_rows_count);
+        } else if (which.is_int64()) {
+            return insert_result_data<ColumnInt64>(result_column, argument_column, null_map_data,
+                                                   filled_flag, input_rows_count);
+        } else if (which.is_date_or_datetime()) {
+            return insert_result_data<ColumnVector<DateTime>>(
+                    result_column, argument_column, null_map_data, filled_flag, input_rows_count);
+        } else if (which.is_float32()) {
+            return insert_result_data<ColumnFloat32>(result_column, argument_column, null_map_data,
+                                                     filled_flag, input_rows_count);
+        } else if (which.is_float64()) {
+            return insert_result_data<ColumnFloat64>(result_column, argument_column, null_map_data,
+                                                     filled_flag, input_rows_count);
+        } else if (which.is_decimal()) {
+            return insert_result_data<ColumnDecimal<Decimal128>>(
+                    result_column, argument_column, null_map_data, filled_flag, input_rows_count);
+        } else {
+            return Status::NotSupported(fmt::format("Unexpected type {} of argument of function {}",
+                                                    data_type->get_name(), get_name()));
+        }
     }
 };
 
