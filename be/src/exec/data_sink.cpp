e7217d3ad17d3e3ee31939e59a1834319f707f6d
morningman <chenmingyu@baidu.com>
2022-01-18 12:00:50 +0800

    rebase and disable vtablet_sink

--- be/src/exec/data_sink.cpp
+++ be/src/exec/data_sink.cpp
@@ -145,7 +145,8 @@ Status DataSink::create_data_sink(ObjectPool* pool, const TDataSink& thrift_sink
         Status status;
         DCHECK(thrift_sink.__isset.olap_table_sink);
         if (is_vec) {
-            sink->reset(new stream_load::VOlapTableSink(pool, row_desc, output_exprs, &status));
+            // sink->reset(new stream_load::VOlapTableSink(pool, row_desc, output_exprs, &status));
+            return Status::NotSupported("VOlapTableSink is not supported yet");
         } else {
             sink->reset(new stream_load::OlapTableSink(pool, row_desc, output_exprs, &status));
         }
