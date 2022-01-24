97c70e62f87d5604de2ceea03aca52ed9b6aa78b
Amos Bird <amosbird@gmail.com>
2022-01-24 13:16:37 +0800

    [chore](ldb-toolchain) Support ldb_toolchain on ubuntu 20 (#7846)

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -265,9 +265,9 @@ build_thrift() {
     # NOTE(amos): libtool discard -static. --static works.
     ./configure CPPFLAGS="-I${TP_INCLUDE_DIR}" LDFLAGS="-L${TP_LIB_DIR} --static" LIBS="-lcrypto -ldl -lssl" CFLAGS="-fPIC" \
     --prefix=$TP_INSTALL_DIR --docdir=$TP_INSTALL_DIR/doc --enable-static --disable-shared --disable-tests \
-    --disable-tutorial --without-qt4 --without-qt5 --without-csharp --without-erlang --without-nodejs \
-    --without-lua --without-perl --without-php --without-php_extension --without-dart --without-ruby \
-    --without-haskell --without-go --without-haxe --without-d --without-python -without-java -without-rs --with-cpp \
+    --disable-tutorial --without-qt4 --without-qt5 --without-csharp --without-erlang --without-nodejs --without-nodets --without-swift \
+    --without-lua --without-perl --without-php --without-php_extension --without-dart --without-ruby --without-cl \
+    --without-haskell --without-go --without-haxe --without-d --without-python -without-java --without-dotnetcore -without-rs --with-cpp \
     --with-libevent=$TP_INSTALL_DIR --with-boost=$TP_INSTALL_DIR --with-openssl=$TP_INSTALL_DIR
 
     if [ -f compiler/cpp/thrifty.hh ];then
