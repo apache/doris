16f5cbba296f097c0d2723750281450d43c7c26a
luozenglin <37725793+luozenglin@users.noreply.github.com>
2022-01-25 09:59:24 +0800

    [fix](ldap) fix ldap password logic (#7862)
    1. write ldap info to image;
    2. optimizing LdapClient class thread safety.

--- fe/fe-core/src/test/java/org/apache/doris/ldap/LdapClientTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/ldap/LdapClientTest.java
@@ -17,12 +17,13 @@
 
 package org.apache.doris.ldap;
 
-import com.clearspring.analytics.util.Lists;
-import mockit.Delegate;
-import mockit.Expectations;
-import mockit.Mocked;
+import org.apache.doris.catalog.Catalog;
 import org.apache.doris.common.LdapConfig;
-import org.apache.doris.common.util.SymmetricEncryption;
+import org.apache.doris.mysql.privilege.PaloAuth;
+import org.apache.doris.persist.LdapInfo;
+
+import com.clearspring.analytics.util.Lists;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,14 +33,42 @@ import org.springframework.ldap.query.LdapQuery;
 
 import java.util.List;
 
+import mockit.Delegate;
+import mockit.Expectations;
+import mockit.Mocked;
+
 public class LdapClientTest {
     private static final String ADMIN_PASSWORD = "admin";
 
     @Mocked
     private LdapTemplate ldapTemplate;
 
+    @Mocked
+    private Catalog catalog;
+
+    @Mocked
+    private PaloAuth auth;
+
+    private LdapInfo ldapInfo = new LdapInfo(ADMIN_PASSWORD);
+
     @Before
     public void setUp() {
+        new Expectations() {
+            {
+                Catalog.getCurrentCatalog();
+                minTimes = 0;
+                result = catalog;
+
+                catalog.getAuth();
+                minTimes = 0;
+                result = auth;
+
+                auth.getLdapInfo();
+                minTimes = 0;
+                result = ldapInfo;
+            }
+        };
+
         LdapConfig.ldap_authentication_enabled = true;
         LdapConfig.ldap_host = "127.0.0.1";
         LdapConfig.ldap_port = 389;
@@ -47,7 +76,6 @@ public class LdapClientTest {
         LdapConfig.ldap_user_basedn = "dc=baidu,dc=com";
         LdapConfig.ldap_group_basedn = "ou=group,dc=baidu,dc=com";
         LdapConfig.ldap_user_filter = "(&(uid={login}))";
-        LdapClient.init(SymmetricEncryption.encrypt(ADMIN_PASSWORD));
     }
     
     private void mockLdapTemplateSearch(List list) {
