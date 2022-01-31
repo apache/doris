98dcc056e30ea0ba88980d2cd46382b592981d94
jiafeng.zhang <zhangjf1@gmail.com>
2022-01-31 22:12:34 +0800

    [fix](httpv2) make http v2 and v1 interface compatible (#7848)
    http v2 TableSchemaAction adds the return value of aggregation_type,
    and modifies the corresponding code of Flink/Spark Connector

--- extension/flink-doris-connector/src/main/java/org/apache/doris/flink/rest/models/Field.java
+++ extension/flink-doris-connector/src/main/java/org/apache/doris/flink/rest/models/Field.java
@@ -25,16 +25,26 @@ public class Field {
     private String comment;
     private int precision;
     private int scale;
+    private String aggregation_type;
 
     public Field() {
     }
 
-    public Field(String name, String type, String comment, int precision, int scale) {
+    public Field(String name, String type, String comment, int precision, int scale, String aggregation_type) {
         this.name = name;
         this.type = type;
         this.comment = comment;
         this.precision = precision;
         this.scale = scale;
+        this.aggregation_type = aggregation_type;
+    }
+
+    public String getAggregation_type() {
+        return aggregation_type;
+    }
+
+    public void setAggregation_type(String aggregation_type) {
+        this.aggregation_type = aggregation_type;
     }
 
     public String getName() {
