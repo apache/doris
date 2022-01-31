8040c60fdbd740cfc39d0e6a6a829982f0f3fa2e
dataroaring <98214048+dataroaring@users.noreply.github.com>
2022-01-31 22:15:10 +0800

    [typo](doc) rename DDL Statements to SQL Statements in sidebars of docs in english (#7922)

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -492,7 +492,7 @@ module.exports = [
         ],
       },
       {
-        title: "DDL Statements",
+        title: "SQL Statements",
         directoryPath: "sql-statements/",
         children: [
           {
