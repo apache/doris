eeaf6725fd4341caeb6fa37ecac261104c5f342c
EmmyMiao87 <522274284@qq.com>
2022-02-09 13:07:03 +0800

    (fix)[lateral-view] Solve the problem of not recognizing the lateral view on the view (#7968)
    If the tableRef behind represents a CTE or a view,
    the tableRef will be reset during semantic parsing.
    The new tableRef needs to inherit the lateral view property of the origin tableRef
    to ensure that the lateral view is not accidentally lost during parsing.

--- fe/fe-core/src/main/java/org/apache/doris/analysis/InlineViewRef.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/InlineViewRef.java
@@ -108,9 +108,12 @@ public class InlineViewRef extends TableRef {
         // TODO(zc)
         // view_.getTableName().toString().toLowerCase(), view.getName().toLowerCase()
         if (view.isLocalView()) {
-            aliases_ = new String[] { view.getName() };
+            aliases_ = new String[]{view.getName()};
         } else {
-            aliases_ = new String[] { name.toString(), view.getName() };
+            aliases_ = new String[]{name.toString(), view.getName()};
+        }
+        if (origTblRef.getLateralViewRefs() != null) {
+            lateralViewRefs = (ArrayList<LateralViewRef>) origTblRef.getLateralViewRefs().clone();
         }
     }
 
