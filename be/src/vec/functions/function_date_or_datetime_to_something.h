914a6b09813ecd009575d0d8cc31f0ed3cf5223d
HappenLee <happenlee@hotmail.com>
2022-02-17 10:49:25 +0800

    [feature][fix](vec)(function) Fix multi args function call the DATETIME type not effective in DATE type and add the alias function (#8050)
    1. Support some function alias of mod/fmod, adddate/add_data
    2. Support some function of multi args: week, yearweek
    3. Fix bug of multi args function call the DATETIME type not effective in DATE type

--- be/src/vec/functions/function_date_or_datetime_to_something.h
+++ be/src/vec/functions/function_date_or_datetime_to_something.h
@@ -30,12 +30,19 @@ template <typename ToDataType, typename Transform>
 class FunctionDateOrDateTimeToSomething : public IFunction {
 public:
     static constexpr auto name = Transform::name;
+    static constexpr bool has_variadic_argument =
+            !std::is_void_v<decltype(has_variadic_argument_types(std::declval<Transform>()))>;
+
     static FunctionPtr create() { return std::make_shared<FunctionDateOrDateTimeToSomething>(); }
 
     String get_name() const override { return name; }
 
     bool is_variadic() const override { return true; }
     size_t get_number_of_arguments() const override { return 0; }
+    DataTypes get_variadic_argument_types_impl() const override {
+        if constexpr (has_variadic_argument) return Transform::get_variadic_argument_types();
+        return {};
+    }
 
     DataTypePtr get_return_type_impl(const ColumnsWithTypeAndName& arguments) const override {
         if (arguments.size() == 1) {
