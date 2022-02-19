apache/doris

commit 6420859dd6cd180675e9f5dfbbbf94f86f91e033

[improvement](query) Improve fe high concurrent query performance (#7936)

fe/fe-core/src/main/java/org/apache/doris/qe/ConnectScheduler.java
@@ -46,10 +46,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ConnectScheduler {
     private static final Logger LOG = LogManager.getLogger(ConnectScheduler.class);
     private int maxConnections;
-    private int numberConnection;
+    private AtomicInteger numberConnection;
     private AtomicInteger nextConnectionId;
-    private Map<Long, ConnectContext> connectionMap = Maps.newConcurrentMap();
-    private Map<String, AtomicInteger> connByUser = Maps.newHashMap();
+    private Map<Integer, ConnectContext> connectionMap = Maps.newConcurrentMap();
+    private Map<String, AtomicInteger> connByUser = Maps.newConcurrentMap();
     private ExecutorService executor = ThreadPoolManager.newDaemonCacheThreadPool(Config.max_connection_scheduler_threads_num, "connect-scheduler-pool", true);
 
     // Use a thread to check whether connection is timeout. Because
@@ -61,7 +61,7 @@ public class ConnectScheduler {
 
     public ConnectScheduler(int maxConnections) {
         this.maxConnections = maxConnections;
-        numberConnection = 0;
+        numberConnection = new AtomicInteger(0);
         nextConnectionId = new AtomicInteger(0);
         checkTimer.scheduleAtFixedRate(new TimeoutChecker(), 0, 1000L, TimeUnit.MILLISECONDS);
     }
@@ -70,10 +70,8 @@ public class ConnectScheduler {
         @Override
         public void run() {
             long now = System.currentTimeMillis();
-            synchronized (ConnectScheduler.this) {
-                for (ConnectContext connectContext : connectionMap.values()) {
-                    connectContext.checkTimeout(now);
-                }
+            for (ConnectContext connectContext : connectionMap.values()) {
+                connectContext.checkTimeout(now);
             }
         }
     }
@@ -96,50 +94,50 @@ public class ConnectScheduler {
     }
 
     // Register one connection with its connection id.
-    public synchronized boolean registerConnection(ConnectContext ctx) {
-        if (numberConnection >= maxConnections) {
+    public boolean registerConnection(ConnectContext ctx) {
+        if (numberConnection.incrementAndGet() > maxConnections) {
+            numberConnection.decrementAndGet();
             return false;
         }
         // Check user
-        if (connByUser.get(ctx.getQualifiedUser()) == null) {
-            connByUser.put(ctx.getQualifiedUser(), new AtomicInteger(0));
-        }
-        int conns = connByUser.get(ctx.getQualifiedUser()).get();
+        connByUser.putIfAbsent(ctx.getQualifiedUser(), new AtomicInteger(0));
+        AtomicInteger conns = connByUser.get(ctx.getQualifiedUser());
         if (ctx.getIsTempUser()) {
-            if (conns >= LdapAuthenticate.getMaxConn()) {
+            if (conns.incrementAndGet() > LdapAuthenticate.getMaxConn()) {
+                conns.decrementAndGet();
+                numberConnection.decrementAndGet();
                 return false;
             }
-        } else if (conns >= ctx.getCatalog().getAuth().getMaxConn(ctx.getQualifiedUser())) {
+        } else if (conns.incrementAndGet() > ctx.getCatalog().getAuth().getMaxConn(ctx.getQualifiedUser())) {
+            conns.decrementAndGet();
+            numberConnection.decrementAndGet();
             return false;
         }
-        numberConnection++;
-        connByUser.get(ctx.getQualifiedUser()).incrementAndGet();
-        connectionMap.put((long) ctx.getConnectionId(), ctx);
+        connectionMap.put(ctx.getConnectionId(), ctx);
         return true;
     }
 
-    public synchronized void unregisterConnection(ConnectContext ctx) {
+    public void unregisterConnection(ConnectContext ctx) {
         ctx.closeTxn();
-        if (connectionMap.remove((long) ctx.getConnectionId()) != null) {
-            numberConnection--;
+        if (connectionMap.remove(ctx.getConnectionId()) != null) {
             AtomicInteger conns = connByUser.get(ctx.getQualifiedUser());
             if (conns != null) {
                 conns.decrementAndGet();
             }
+            numberConnection.decrementAndGet();
         }
     }
 
-    public synchronized ConnectContext getContext(long connectionId) {
+    public ConnectContext getContext(long connectionId) {
         return connectionMap.get(connectionId);
     }
 
-    public synchronized int getConnectionNum() {
-        return numberConnection;
+    public int getConnectionNum() {
+        return numberConnection.get();
     }
 
-    public synchronized List<ConnectContext.ThreadInfo> listConnection(String user) {
+    public List<ConnectContext.ThreadInfo> listConnection(String user) {
         List<ConnectContext.ThreadInfo> infos = Lists.newArrayList();
-
         for (ConnectContext ctx : connectionMap.values()) {
             // Check auth
             if (!ctx.getQualifiedUser().equals(user) &&

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -526,7 +526,9 @@ public class StmtExecutor implements ProfileWriter {
 
     // Analyze one statement to structure in memory.
     public void analyze(TQueryOptions tQueryOptions) throws UserException {
-        LOG.info("begin to analyze stmt: {}, forwarded stmt id: {}", context.getStmtId(), context.getForwardedStmtId());
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("begin to analyze stmt: {}, forwarded stmt id: {}", context.getStmtId(), context.getForwardedStmtId());
+        }
 
         parse();
 

fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -471,8 +471,9 @@ public class Coordinator {
                     addressToBackendID.get(execBeAddr),
                     toBrpcHost(execBeAddr),
                     queryOptions.query_timeout * 1000);
-
-            LOG.info("dispatch query job: {} to {}", DebugUtil.printId(queryId), topParams.instanceExecParams.get(0).host);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("dispatch query job: {} to {}", DebugUtil.printId(queryId), topParams.instanceExecParams.get(0).host);
+            }
 
             if (topDataSink instanceof ResultFileSink
                     && ((ResultFileSink) topDataSink).getStorageType() == StorageBackend.StorageType.BROKER) {

fe/fe-core/src/main/java/org/apache/doris/qe/QeProcessorImpl.java
@@ -81,7 +81,9 @@ public final class QeProcessorImpl implements QeProcessor {
 
     @Override
     public void registerQuery(TUniqueId queryId, QueryInfo info) throws UserException {
-        LOG.info("register query id = " + DebugUtil.printId(queryId) + ", job: " + info.getCoord().getJobId());
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("register query id = " + DebugUtil.printId(queryId) + ", job: " + info.getCoord().getJobId());
+        }
         final QueryInfo result = coordinatorMap.putIfAbsent(queryId, info);
         if (result != null) {
             throw new UserException("queryId " + queryId + " already exists");
@@ -121,7 +123,9 @@ public final class QeProcessorImpl implements QeProcessor {
     public void unregisterQuery(TUniqueId queryId) {
         QueryInfo queryInfo = coordinatorMap.remove(queryId);
         if (queryInfo != null) {
-            LOG.info("deregister query id {}", DebugUtil.printId(queryId));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("deregister query id {}", DebugUtil.printId(queryId));
+            }
             if (queryInfo.getConnectContext() != null &&
                     !Strings.isNullOrEmpty(queryInfo.getConnectContext().getQualifiedUser())
             ) {

commit 9df5b2dfdca1a0014e7780d0f353262db47dcf49

[fix](variables) Fix bug that execute showVariablesStmt with where expression return empty resultset (#8094)
This Bug is introduced by PR #7936 , which change key type of connectionMap from Long to Integer,
which cause connectionMap could not find connectContext by connectionId

fe/fe-core/src/main/cup/sql_parser.cup
@@ -3506,15 +3506,15 @@ restore_stmt ::=
 kill_stmt ::=
     KW_KILL INTEGER_LITERAL:value
     {:
-        RESULT = new KillStmt(true, value.longValue());
+        RESULT = new KillStmt(true, value.intValue());
     :}
     | KW_KILL KW_CONNECTION INTEGER_LITERAL:value
     {:
-        RESULT = new KillStmt(true, value.longValue());
+        RESULT = new KillStmt(true, value.intValue());
     :}
     | KW_KILL KW_QUERY INTEGER_LITERAL:value
     {:
-        RESULT = new KillStmt(false, value.longValue());
+        RESULT = new KillStmt(false, value.intValue());
     :}
     ;
 

fe/fe-core/src/main/java/org/apache/doris/analysis/KillStmt.java
@@ -24,9 +24,9 @@ package org.apache.doris.analysis;
  */
 public class KillStmt extends StatementBase {
     private final boolean isConnectionKill;
-    private final long connectionId;
+    private final int connectionId;
 
-    public KillStmt(boolean isConnectionKill, long connectionId) {
+    public KillStmt(boolean isConnectionKill, int connectionId) {
         this.isConnectionKill = isConnectionKill;
         this.connectionId = connectionId;
     }
@@ -35,7 +35,7 @@ public class KillStmt extends StatementBase {
         return isConnectionKill;
     }
 
-    public long getConnectionId() {
+    public int getConnectionId() {
         return connectionId;
     }
 

fe/fe-core/src/main/java/org/apache/doris/http/rest/ConnectionAction.java
@@ -47,7 +47,7 @@ public class ConnectionAction extends RestBaseAction {
             sendResult(request, response, HttpResponseStatus.BAD_REQUEST);
             return;
         }
-        long connectionId = Long.valueOf(connStr.trim());
+        int connectionId = Integer.valueOf(connStr.trim());
         ConnectContext context = ExecuteEnv.getInstance().getScheduler().getContext(connectionId);
         if (context == null || context.queryId() == null) {
             response.getContent().append("connection id " + connectionId + " not found.");

fe/fe-core/src/main/java/org/apache/doris/httpv2/rest/ConnectionAction.java
@@ -65,9 +65,9 @@ public class ConnectionAction extends RestBaseController {
             return ResponseEntityBuilder.badRequest("Missing connection_id");
         }
 
-        long connectionId = -1;
+        int connectionId = -1;
         try {
-            connectionId = Long.valueOf(connStr.trim());
+            connectionId = Integer.valueOf(connStr.trim());
         } catch (NumberFormatException e) {
             return ResponseEntityBuilder.badRequest("Invalid connection id: " + e.getMessage());
         }

fe/fe-core/src/main/java/org/apache/doris/qe/ConnectScheduler.java
@@ -128,7 +128,7 @@ public class ConnectScheduler {
         }
     }
 
-    public ConnectContext getContext(long connectionId) {
+    public ConnectContext getContext(int connectionId) {
         return connectionMap.get(connectionId);
     }
 

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -728,7 +728,7 @@ public class StmtExecutor implements ProfileWriter {
     // Handle kill statement.
     private void handleKill() throws DdlException {
         KillStmt killStmt = (KillStmt) parsedStmt;
-        long id = killStmt.getConnectionId();
+        int id = killStmt.getConnectionId();
         ConnectContext killCtx = context.getConnectScheduler().getContext(id);
         if (killCtx == null) {
             ErrorReport.reportDdlException(ErrorCode.ERR_NO_SUCH_THREAD, id);

fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
@@ -426,7 +426,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
         Map<String, String> map = Maps.newHashMap();
         result.setVariables(map);
         // Find connect
-        ConnectContext ctx = exeEnv.getScheduler().getContext(params.getThreadId());
+        ConnectContext ctx = exeEnv.getScheduler().getContext((int) params.getThreadId());
         if (ctx == null) {
             return result;
         }

fe/fe-core/src/test/java/org/apache/doris/qe/StmtExecutorTest.java
@@ -342,7 +342,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = ctx;
             }
         };
@@ -399,7 +399,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = killCtx;
             }
         };
@@ -420,7 +420,7 @@ public class StmtExecutorTest {
 
                 killStmt.getConnectionId();
                 minTimes = 0;
-                result = 1L;
+                result = 1;
 
                 killStmt.isConnectionKill();
                 minTimes = 0;
@@ -455,7 +455,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = killCtx;
             }
         };
@@ -475,7 +475,7 @@ public class StmtExecutorTest {
 
                 killStmt.getConnectionId();
                 minTimes = 0;
-                result = 1L;
+                result = 1;
 
                 killStmt.getRedirectStatus();
                 minTimes = 0;
@@ -490,7 +490,7 @@ public class StmtExecutorTest {
 
         new Expectations(scheduler) {
             {
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = null;
             }
         };