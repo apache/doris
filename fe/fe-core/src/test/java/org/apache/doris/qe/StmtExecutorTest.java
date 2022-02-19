cbdc8a8fe5e35e6224869e51f3663571ba2bc063
caiconghui <55968745+caiconghui@users.noreply.github.com>
2022-02-19 11:58:17 +0800

    [fix](variables) Fix bug that execute showVariablesStmt with where expression return empty resultset (#8094)
    This Bug is introduced by PR #7936 , which change key type of connectionMap from Long to Integer,
    which cause connectionMap could not find connectContext by connectionId

--- fe/fe-core/src/test/java/org/apache/doris/qe/StmtExecutorTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/qe/StmtExecutorTest.java
@@ -342,7 +342,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = ctx;
             }
         };
@@ -399,7 +399,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = killCtx;
             }
         };
@@ -420,7 +420,7 @@ public class StmtExecutorTest {
 
                 killStmt.getConnectionId();
                 minTimes = 0;
-                result = 1L;
+                result = 1;
 
                 killStmt.isConnectionKill();
                 minTimes = 0;
@@ -455,7 +455,7 @@ public class StmtExecutorTest {
         new Expectations(scheduler) {
             {
                 // suicide
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = killCtx;
             }
         };
@@ -475,7 +475,7 @@ public class StmtExecutorTest {
 
                 killStmt.getConnectionId();
                 minTimes = 0;
-                result = 1L;
+                result = 1;
 
                 killStmt.getRedirectStatus();
                 minTimes = 0;
@@ -490,7 +490,7 @@ public class StmtExecutorTest {
 
         new Expectations(scheduler) {
             {
-                scheduler.getContext(1L);
+                scheduler.getContext(1);
                 result = null;
             }
         };
