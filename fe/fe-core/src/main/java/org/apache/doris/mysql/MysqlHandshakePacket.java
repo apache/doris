dc548eedc06e1e36d067258074e8aa1593efeffa
Zhengguo Yang <yangzhgg@gmail.com>
2022-02-25 11:14:57 +0800

    [refactor] change mysql server version to avoid some cve issues (#8223)
    5.1.0 -> 5.1.73

--- fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlHandshakePacket.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlHandshakePacket.java
@@ -22,8 +22,8 @@ public class MysqlHandshakePacket extends MysqlPacket {
     private static final int SCRAMBLE_LENGTH = 20;
     // Version of handshake packet, since MySQL 3.21.0, Handshake of protocol 10 is used
     private static final int PROTOCOL_VERSION = 10;
-    // JDBC use this version to check which protocol the server support
-    private static final String SERVER_VERSION = "5.1.0";
+    // JDBC uses this version to check which protocol the server support
+    private static final String SERVER_VERSION = "5.1.73";
     // 33 stands for UTF-8 character set
     private static final int CHARACTER_SET = 33;
     // use default capability for all
