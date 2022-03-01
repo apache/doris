ee7cf82f41438b1ed107796c18f6bf5ed2f30297
zhangstar333 <2561612514@qq.com>
2022-03-01 15:28:28 +0800

    [fix] delete the repeat same code

--- be/src/exprs/minmax_predicate.h
+++ be/src/exprs/minmax_predicate.h
@@ -25,7 +25,6 @@ namespace doris {
 // only used in Runtime Filter
 class MinMaxFuncBase {
 public:
-    virtual ~MinMaxFuncBase() = default;
     virtual void insert(const void* data) = 0;
     virtual bool find(void* data) = 0;
     virtual bool is_empty() = 0;
