0ef8917312e6e7b9bfd4edafe170d6e7559b8f0b
steadyBoy <35027788+steadyBoy@users.noreply.github.com>
2022-03-09 13:05:13 +0800

    [docs] add document conditional-functions (#8339)
    Add conditional-functions.

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -484,6 +484,17 @@ module.exports = [
             ],
           },
           {
+            title: "conditional function",
+            directoryPath: "conditional-functions/",
+            children: [
+              "case",
+              "coalesce",
+              "if",
+              "ifnull",
+              "nullif"
+            ],
+          },
+          {
             title: "json function",
             directoryPath: "json-functions/",
             initialOpenGroupIndex: -1,
