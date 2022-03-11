fa091f38e73847c1c54f6d1df6c84b57482b627f
caiconghui <55968745+caiconghui@users.noreply.github.com>
2022-03-11 17:36:23 +0800

    [fix](transaction) Fix committed transaction couldn't be finished when table is dropped (#8423)
    Issue Number: close #8426

--- fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
+++ fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
@@ -27,7 +27,6 @@ import org.apache.doris.catalog.Table;
 import org.apache.doris.catalog.Tablet;
 import org.apache.doris.catalog.TabletInvertedIndex;
 import org.apache.doris.common.Config;
-import org.apache.doris.common.UserException;
 import org.apache.doris.common.util.MasterDaemon;
 import org.apache.doris.task.AgentBatchTask;
 import org.apache.doris.task.AgentTaskExecutor;
@@ -73,10 +72,10 @@ public class PublishVersionDaemon extends MasterDaemon {
         return true;
     }
     
-    private void publishVersion() throws UserException {
+    private void publishVersion() {
         GlobalTransactionMgr globalTransactionMgr = Catalog.getCurrentGlobalTransactionMgr();
         List<TransactionState> readyTransactionStates = globalTransactionMgr.getReadyToPublishTransactions();
-        if (readyTransactionStates == null || readyTransactionStates.isEmpty()) {
+        if (readyTransactionStates.isEmpty()) {
             return;
         }
 
@@ -240,7 +239,7 @@ public class PublishVersionDaemon extends MasterDaemon {
                     // one transaction exception should not affect other transaction
                     globalTransactionMgr.finishTransaction(transactionState.getDbId(), transactionState.getTransactionId(), publishErrorReplicaIds);
                 } catch (Exception e) {
-                    LOG.warn("error happends when finish transaction {} ", transactionState.getTransactionId(), e);
+                    LOG.warn("error happens when finish transaction {}", transactionState.getTransactionId(), e);
                 }
                 if (transactionState.getTransactionStatus() != TransactionStatus.VISIBLE) {
                     // if finish transaction state failed, then update publish version time, should check 
