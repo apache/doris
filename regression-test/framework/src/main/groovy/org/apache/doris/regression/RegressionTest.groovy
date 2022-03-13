f9be37a44b0388fb9559b1b1ecd3b1f50d6ad62d
Yi WU <dataroaring@gmail.com>
2022-03-13 22:15:47 +0800

    let framework support sql cases
    We generate groovy files from sql cases and run the generated groovy
    file. This way, we can just put sql cases, then framework handles
    left work.

--- regression-test/framework/src/main/groovy/org/apache/doris/regression/RegressionTest.groovy
+++ regression-test/framework/src/main/groovy/org/apache/doris/regression/RegressionTest.groovy
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 package org.apache.doris.regression
 
 import groovy.transform.CompileStatic
@@ -32,6 +31,7 @@ import java.util.stream.Collectors
 @Slf4j
 @CompileStatic
 class RegressionTest {
+
     static ClassLoader classloader
     static CompilerConfiguration compileConfig
     static GroovyShell shell
@@ -57,25 +57,62 @@ class RegressionTest {
 
     static List<File> findSuiteFiles(String root) {
         if (root == null) {
-            log.warn("Not specify suite path")
+            log.warn('Not specify suite path')
             return new ArrayList<File>()
         }
         List<File> files = new ArrayList<>()
+        // 1. generate groovy for sql
         new File(root).eachFileRecurse { f ->
-            if (f.isFile() && f.name.endsWith(".groovy")) {
+            if (f.isFile() && f.name.endsWith('.sql')) {
+                genetate_groovy_from_sql(f)
+            }
+        }
+
+        // 2. collect groovy files.
+        new File(root).eachFileRecurse { f ->
+            if (f.isFile() && f.name.endsWith('.groovy')) {
                 files.add(f)
             }
         }
         return files
     }
 
+    static void genetate_groovy_from_sql(File f) {
+        File groovy_file = new File(f.getAbsolutePath() + '.generated.groovy')
+        groovy_file.delete()
+        groovy_file.createNewFile()
+        int separatorIndex = f.name.lastIndexOf('.')
+        String action_name = f.name.substring(0, separatorIndex)
+        groovy_file.text = "qt_${action_name} \"\"\"${f.text}\"\"\""
+    }
+
     static String parseGroup(Config config, File suiteFile) {
         String group = new File(config.suitePath).relativePath(suiteFile)
         int separatorIndex = group.lastIndexOf(File.separator)
         if (separatorIndex == -1) {
-            return ""
+            return ''
         } else {
             return group.substring(0, separatorIndex)
+        }
+    }
+
+    static void runSuite(Config config, SuiteFile sf, Recorder recorder) {
+        File file = sf.file
+        String suiteName = sf.suiteName
+        String group = sf.group
+        def suiteConn = config.getConnection()
+        new SuiteContext(file, suiteConn, config, recorder).withCloseable { context ->
+            try {
+                log.info("Run ${suiteName} in $file".toString())
+                Suite suite = shell.parse(file) as Suite
+                suite.init(suiteName, group, context)
+                suite.run()
+                recorder.onSuccess(new SuiteInfo(file, group, suiteName))
+                log.info("Run ${suiteName} in ${file.absolutePath} succeed".toString())
+            } catch (Throwable t) {
+                recorder.onFailure(new SuiteInfo(file, group, suiteName))
+                log.error("Run ${suiteName} in ${file.absolutePath} failed".toString(), t)
+            }
         }
     }
 
@@ -83,44 +120,28 @@ class RegressionTest {
         def files = findSuiteFiles(config.suitePath)
         def recorder = new Recorder()
         List<SuiteFile> runScripts = files.stream().map({ file ->
-            String suiteName = file.name.substring(0, file.name.lastIndexOf("."))
+            String suiteName = file.name.substring(0, file.name.lastIndexOf('.'))
             String group = parseGroup(config, file)
             return new SuiteFile(file, suiteName, group)
         }).filter({ sf ->
             canRun(config, sf.suiteName, sf.group)
         }).collect(Collectors.toList())
 
-        log.info("Start to run suites")
+        log.info('Start to run suites')
         int totalFile = runScripts.size()
         runScripts.eachWithIndex { sf, i ->
-            File file = sf.file
-            String suiteName = sf.suiteName
-            String group = sf.group
-            def suiteConn = config.getConnection()
-            new SuiteContext(file, suiteConn, config, recorder).withCloseable { context ->
-                try {
-                    log.info("[${i + 1}/${totalFile}] Run ${suiteName} in $file".toString())
-                    Suite suite = shell.parse(file) as Suite
-                    suite.init(suiteName, group, context)
-                    suite.run()
-                    recorder.onSuccess(new SuiteInfo(file, group, suiteName))
-                    log.info("Run ${suiteName} in ${file.absolutePath} succeed".toString())
-                } catch (Throwable t) {
-                    recorder.onFailure(new SuiteInfo(file, group, suiteName))
-                    log.error("Run ${suiteName} in ${file.absolutePath} failed".toString(), t)
-                }
-            }
+            log.info("[${i + 1}/${totalFile}] Run ${sf.suiteName} in ${sf.file}".toString())
+            runSuite(config, sf, recorder)
         }
         return recorder
     }
 
     static boolean canRun(Config config, String suiteName, String group) {
-        Set<String> suiteGroups = group.split(",").collect {g -> g.trim()}.toSet()
+        Set<String> suiteGroups = group.split(',').collect { g -> g.trim() }.toSet()
         if (config.suiteWildcard.size() == 0 ||
                 (suiteName != null && (config.suiteWildcard.any {
                     suiteWildcard -> Wildcard.match(suiteName, suiteWildcard)
                 }))) {
-
             if (config.groups == null || config.groups.isEmpty()
                     || !config.groups.intersect(suiteGroups).isEmpty()) {
                 return true
@@ -138,7 +159,7 @@ class RegressionTest {
         {
             String successList = recorder.successList.collect { info ->
                 "${info.file.absolutePath}: group=${info.group}, name=${info.suiteName}"
-            }.join("\n")
+            }.join('\n')
             log.info("successList suites:\n${successList}".toString())
         }
 
@@ -146,36 +167,37 @@ class RegressionTest {
         if (!recorder.failureList.isEmpty()) {
             def failureList = recorder.failureList.collect() { info ->
                 "${info.file.absolutePath}: group=${info.group}, name=${info.suiteName}"
-            }.join("\n")
+            }.join('\n')
             log.info("Failure suites:\n${failureList}".toString())
             printFailed()
-            throw new IllegalStateException("Test failed")
+            throw new IllegalStateException('Test failed')
         } else {
             printPassed()
         }
     }
 
     static void printPassed() {
-        log.info("""All suites success.
-                 | ____   _    ____ ____  _____ ____  
-                 ||  _ \\ / \\  / ___/ ___|| ____|  _ \\ 
+        log.info('''All suites success.
+                 | ____   _    ____ ____  _____ ____
+                 ||  _ \\ / \\  / ___/ ___|| ____|  _ \\
                  || |_) / _ \\ \\___ \\___ \\|  _| | | | |
                  ||  __/ ___ \\ ___) |__) | |___| |_| |
-                 ||_| /_/   \\_\\____/____/|_____|____/                         
-                 |""".stripMargin())
+                 ||_| /_/   \\_\\____/____/|_____|____/
+                 |'''.stripMargin())
     }
 
     static void printFailed() {
-        log.info("""Some suites failed.
-                 | _____ _    ___ _     _____ ____  
-                 ||  ___/ \\  |_ _| |   | ____|  _ \\ 
+        log.info('''Some suites failed.
+                 | _____ _    ___ _     _____ ____
+                 ||  ___/ \\  |_ _| |   | ____|  _ \\
                  || |_ / _ \\  | || |   |  _| | | | |
                  ||  _/ ___ \\ | || |___| |___| |_| |
-                 ||_|/_/   \\_\\___|_____|_____|____/ 
-                 |""".stripMargin())
+                 ||_|/_/   \\_\\___|_____|_____|____/
+                 |'''.stripMargin())
     }
 
     static class SuiteFile {
+
         File file
         String suiteName
         String group
@@ -185,5 +207,7 @@ class RegressionTest {
             this.suiteName = suiteName
             this.group = group
         }
+
     }
+
 }
