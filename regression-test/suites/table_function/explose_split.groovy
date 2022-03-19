52366d0d7943215c0426af8c0048286c8e998255
Lchangliang <915311741@qq.com>
2022-03-19 15:37:26 +0800

    add regression test

---
+++ regression-test/suites/table_function/explose_split.groovy
@@ -0,0 +1,22 @@
+def tableName = "test_lv_str"
+
+sql """ DROP TABLE IF EXISTS ${tableName} """
+sql """
+    CREATE TABLE ${tableName} 
+    (k1 INT, k2 STRING) 
+    UNIQUE KEY(k1) DISTRIBUTED BY HASH(k1) BUCKETS 8  
+    PROPERTIES("replication_num" = "1")
+"""
+
+sql """ INSERT INTO ${tableName} VALUES (1, 'a,b,c') """
+
+sql """ set enable_lateral_view = true """
+sql """ set enable_vectorized_engine = true """
+
+qt_explose_split """ select * from ${tableName} 
+                    lateral view explode_split(k2, ',') tmp1 as e1 """
+
+qt_explose_split """ select * from ${tableName}
+                    lateral view explode_split(k2, ',') tmp1 as e1 
+                    lateral view explode_split(k2, ',') tmp2 as e2 """
+
