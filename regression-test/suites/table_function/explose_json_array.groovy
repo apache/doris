86be06fd2a5e02070abf2b546fd191573b159c15
Lchangliang <915311741@qq.com>
2022-03-20 14:51:36 +0800

    orderby the cases

--- regression-test/suites/table_function/explose_json_array.groovy
+++ regression-test/suites/table_function/explose_json_array.groovy
@@ -23,19 +23,22 @@ sql """ set enable_lateral_view = true """
 // not vectorized
 qt_explose_json_array """ SELECT * FROM ${tableName} 
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[30, 60]') t1 as c_age 
-                    LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age """
+                    LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age 
+                    ORDER BY id, c_age, d_age """
 
 qt_explose_json_array """ SELECT c_age, COUNT(1) FROM ${tableName}
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[30, 60]') t1 as c_age 
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age 
-                    GROUP BY c_age """
+                    GROUP BY c_age ORDER BY c_age """
 
 qt_explose_json_array """ SELECT * FROM ${tableName}
-                        LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[]') t1 AS c_age """
+                        LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[]') t1 AS c_age 
+                        ORDER BY id, c_age """
 
 qt_explose_json_array """ SELECT * FROM ${tableName}
                     LATERAL VIEW EXPLODE_JSON_ARRAY_STRING('[1, "b", 3]') t1 as c 
-                    LATERAL VIEW EXPLODE_JSON_ARRAY_DOUBLE('[1.23, 22.214, 214.1]') t2 as d """
+                    LATERAL VIEW EXPLODE_JSON_ARRAY_DOUBLE('[1.23, 22.214, 214.1]') t2 as d 
+                    ORDER BY id, c, d """
 
 // vectorized
 sql """ set enable_vectorized_engine = true """
@@ -43,16 +46,19 @@ sql """ set enable_vectorized_engine = true """
 qt_explose_json_array """ select @@enable_vectorized_engine """
 qt_explose_json_array """ SELECT * FROM ${tableName} 
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[30, 60]') t1 as c_age 
-                    LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age """
+                    LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age 
+                    ORDER BY id, c_age, d_age """
 
 qt_explose_json_array """ SELECT c_age, COUNT(1) FROM ${tableName}
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[30, 60]') t1 as c_age 
                     LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[40, 80]') t2 as d_age 
-                    GROUP BY c_age """
+                    GROUP BY c_age ORDER BY c_age """
 
 qt_explose_json_array """ SELECT * FROM ${tableName}
-                        LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[]') t1 AS c_age """
+                        LATERAL VIEW EXPLODE_JSON_ARRAY_INT('[]') t1 AS c_age 
+                        ORDER BY id, c_age """
 
 qt_explose_json_array """ SELECT * FROM ${tableName}
                     LATERAL VIEW EXPLODE_JSON_ARRAY_STRING('[1, "b", 3]') t1 as c 
-                    LATERAL VIEW EXPLODE_JSON_ARRAY_DOUBLE('[1.23, 22.214, 214.1]') t2 as d """
+                    LATERAL VIEW EXPLODE_JSON_ARRAY_DOUBLE('[1.23, 22.214, 214.1]') t2 as d 
+                    ORDER BY id, c, d """
