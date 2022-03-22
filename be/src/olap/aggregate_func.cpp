4ea927d5a90f368412cc2a4745c792131058b35f
Adonis Ling <adonis0147@gmail.com>
2022-03-22 15:28:26 +0800

    [feature-wip](array-type) Support nested array insertion. (#8305) (#8586)
    Please refer to #8304 .

--- be/src/olap/aggregate_func.cpp
+++ be/src/olap/aggregate_func.cpp
@@ -119,6 +119,8 @@ AggregateFuncResolver::AggregateFuncResolver() {
                           OLAP_FIELD_TYPE_VARCHAR>();
     add_aggregate_mapping<OLAP_FIELD_AGGREGATION_NONE, OLAP_FIELD_TYPE_ARRAY,
                           OLAP_FIELD_TYPE_CHAR>();
+    add_aggregate_mapping<OLAP_FIELD_AGGREGATION_NONE, OLAP_FIELD_TYPE_ARRAY,
+                          OLAP_FIELD_TYPE_ARRAY>();
 
     // Min Aggregate Function
     add_aggregate_mapping<OLAP_FIELD_AGGREGATION_MIN, OLAP_FIELD_TYPE_TINYINT>();
--- be/src/runtime/raw_value.cpp
+++ be/src/runtime/raw_value.cpp
@@ -319,10 +319,10 @@ void RawValue::write(const void* value, void* dst, const TypeDescriptor& type, M
         CollectionValue* val = reinterpret_cast<CollectionValue*>(dst);
 
         if (pool != nullptr) {
-            auto children_type = type.children.at(0).type;
-            CollectionValue::init_collection(pool, src->size(), children_type, val);
-            ArrayIterator src_iter = src->iterator(children_type);
-            ArrayIterator val_iter = val->iterator(children_type);
+            const auto& item_type = type.children[0];
+            CollectionValue::init_collection(pool, src->size(), item_type.type, val);
+            ArrayIterator src_iter = src->iterator(item_type.type);
+            ArrayIterator val_iter = val->iterator(item_type.type);
 
             val->set_has_null(src->has_null());
             val->copy_null_signs(src);
@@ -330,7 +330,7 @@ void RawValue::write(const void* value, void* dst, const TypeDescriptor& type, M
             while (src_iter.has_next() && val_iter.has_next()) {
                 if (!src_iter.is_null()) {
                     // write children
-                    write(src_iter.value(), val_iter.value(), children_type, pool);
+                    write(src_iter.value(), val_iter.value(), item_type, pool);
                 }
                 src_iter.next();
                 val_iter.next();
