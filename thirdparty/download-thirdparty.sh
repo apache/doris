25e7d2d8dfa57aed08d66476a203587805ae1a15
Zhengguo Yang <yangzhgg@gmail.com>
2022-03-28 09:35:51 +0800

    [chore] optimize aws thirdparty package download. (#8637)

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -305,8 +305,13 @@ echo "Finished patching $LIBRDKAFKA_SOURCE"
 cd $TP_SOURCE_DIR/$AWS_SDK_SOURCE
 if [ ! -f $PATCHED_MARK ]; then
     if [ $AWS_SDK_SOURCE == "aws-sdk-cpp-1.9.211" ]; then
-        wget --no-check-certificate -q https://doris-thirdparty-repo.bj.bcebos.com/thirdparty/aws-crt-cpp-1.9.211.tar.gz
-        tar xzf aws-crt-cpp-1.9.211.tar.gz
+        wget --no-check-certificate -q https://doris-thirdparty-repo.bj.bcebos.com/thirdparty/aws-crt-cpp-1.9.211.tar.gz -O aws-crt-cpp-1.9.211.tar.gz
+        ret="$?"
+        if [ $ret -eq 0 ] ; then
+            tar xzf aws-crt-cpp-1.9.211.tar.gz
+        else
+            bash ./prefetch_crt_dependency.sh
+        fi
     else
         bash ./prefetch_crt_dependency.sh
     fi
