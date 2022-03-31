d24735e95afb3ed7f622302d9e839d09aea84f7b
Pxl <952130278@qq.com>
2022-03-31 13:53:41 +0800

    [refactor] add some clang-tidy checks && some code style fix (#8752)

--- be/src/vec/aggregate_functions/aggregate_function.h
+++ be/src/vec/aggregate_functions/aggregate_function.h
@@ -57,7 +57,7 @@ public:
     /// Get the result type.
     virtual DataTypePtr get_return_type() const = 0;
 
-    virtual ~IAggregateFunction() {}
+    virtual ~IAggregateFunction() = default;
 
     /** Create empty data for aggregation with `placement new` at the specified location.
       * You will have to destroy them using the `destroy` method.
@@ -147,14 +147,16 @@ public:
 
     void add_batch(size_t batch_size, AggregateDataPtr* places, size_t place_offset,
                    const IColumn** columns, Arena* arena) const override {
-        for (size_t i = 0; i < batch_size; ++i)
+        for (size_t i = 0; i < batch_size; ++i) {
             static_cast<const Derived*>(this)->add(places[i] + place_offset, columns, i, arena);
+        }
     }
 
     void add_batch_single_place(size_t batch_size, AggregateDataPtr place, const IColumn** columns,
                                 Arena* arena) const override {
-        for (size_t i = 0; i < batch_size; ++i)
+        for (size_t i = 0; i < batch_size; ++i) {
             static_cast<const Derived*>(this)->add(place, columns, i, arena);
+        }
     }
     //now this is use for sum/count/avg/min/max win function, other win function should override this function in class
     void add_range_single_place(int64_t partition_start, int64_t partition_end, int64_t frame_start,
@@ -169,8 +171,9 @@ public:
 
     void add_batch_range(size_t batch_begin, size_t batch_end, AggregateDataPtr place,
                          const IColumn** columns, Arena* arena, bool has_null) override {
-        for (size_t i = batch_begin; i <= batch_end; ++i)
+        for (size_t i = batch_begin; i <= batch_end; ++i) {
             static_cast<const Derived*>(this)->add(place, columns, i, arena);
+        }
     }
 };
 
--- be/src/vec/aggregate_functions/key_holder_helpers.h
+++ be/src/vec/aggregate_functions/key_holder_helpers.h
@@ -28,21 +28,22 @@ namespace doris::vectorized {
 template <bool is_plain_column = false>
 static auto get_key_holder(const IColumn& column, size_t row_num, Arena& arena) {
     if constexpr (is_plain_column) {
-        return ArenaKeyHolder{column.get_data_at(row_num), arena};
+        return ArenaKeyHolder {column.get_data_at(row_num), arena};
     } else {
         const char* begin = nullptr;
         StringRef serialized = column.serialize_value_into_arena(row_num, arena, begin);
         assert(serialized.data != nullptr);
-        return SerializedKeyHolder{serialized, arena};
+        return SerializedKeyHolder {serialized, arena};
     }
 }
 
 template <bool is_plain_column>
 static void deserialize_and_insert(StringRef str, IColumn& data_to) {
-    if constexpr (is_plain_column)
+    if constexpr (is_plain_column) {
         data_to.insert_data(str.data, str.size);
-    else
+    } else {
         data_to.deserialize_and_insert_from_arena(str.data);
+    }
 }
 
 } // namespace doris::vectorized
