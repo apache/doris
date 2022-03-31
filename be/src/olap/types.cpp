543c46b4a5f99e423cc109dcab691acbb6b59d54
caiconghui1 <caiconghui1@jd.com>
2022-03-31 14:28:55 +0800

    use array to replace swicth to speed up get_type_info function

--- be/src/olap/types.cpp
+++ be/src/olap/types.cpp
@@ -48,63 +48,50 @@ bool is_olap_string_type(FieldType field_type) {
 }
 
 const TypeInfo* get_scalar_type_info(FieldType field_type) {
-    switch (field_type) {
-    case OLAP_FIELD_TYPE_TINYINT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_TINYINT>();
-    case OLAP_FIELD_TYPE_SMALLINT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_SMALLINT>();
-    case OLAP_FIELD_TYPE_INT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_INT>();
-    case OLAP_FIELD_TYPE_UNSIGNED_INT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_INT>();
-    case OLAP_FIELD_TYPE_BIGINT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_BIGINT>();
-    case OLAP_FIELD_TYPE_UNSIGNED_BIGINT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_BIGINT>();
-    case OLAP_FIELD_TYPE_LARGEINT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_LARGEINT>();
-    case OLAP_FIELD_TYPE_FLOAT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_FLOAT>();
-    case OLAP_FIELD_TYPE_DOUBLE:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_DOUBLE>();
-    case OLAP_FIELD_TYPE_DATE:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_DATE>();
-    case OLAP_FIELD_TYPE_DATETIME:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_DATETIME>();
-    case OLAP_FIELD_TYPE_DECIMAL:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_DECIMAL>();
-    case OLAP_FIELD_TYPE_CHAR:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_CHAR>();
-    case OLAP_FIELD_TYPE_VARCHAR:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_VARCHAR>();
-    case OLAP_FIELD_TYPE_STRING:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_STRING>();
-    case OLAP_FIELD_TYPE_BOOL:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_BOOL>();
-    case OLAP_FIELD_TYPE_HLL:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_HLL>();
-    case OLAP_FIELD_TYPE_OBJECT:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_OBJECT>();
-    case OLAP_FIELD_TYPE_QUANTILE_STATE:
-        return get_scalar_type_info<OLAP_FIELD_TYPE_QUANTILE_STATE>();
-    default:
-        DCHECK(false) << "Bad field type: " << field_type;
-        break;
-    }
-    return nullptr;
+    static const TypeInfo* field_type_array[] = {
+        nullptr,
+        get_scalar_type_info<OLAP_FIELD_TYPE_TINYINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_TINYINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_SMALLINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_SMALLINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_INT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_INT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_BIGINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_UNSIGNED_BIGINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_LARGEINT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_FLOAT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_DOUBLE>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_DISCRETE_DOUBLE>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_CHAR>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_DATE>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_DATETIME>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_DECIMAL>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_VARCHAR>(),
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        get_scalar_type_info<OLAP_FIELD_TYPE_HLL>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_BOOL>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_OBJECT>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_STRING>(),
+        get_scalar_type_info<OLAP_FIELD_TYPE_QUANTILE_STATE>(),
+    };
+    return field_type_array[field_type];
 }
 
-#define INIT_ARRAY_TYPE_INFO_LIST(type)                        \
-    {                                                          \
-        *get_init_array_type_info<type>(0),                    \
-        *get_init_array_type_info<type>(1),                    \
-        *get_init_array_type_info<type>(2),                    \
-        *get_init_array_type_info<type>(3),                    \
-        *get_init_array_type_info<type>(4),                    \
-        *get_init_array_type_info<type>(5),                    \
-        *get_init_array_type_info<type>(6),                    \
-        *get_init_array_type_info<type>(7),                    \
-        *get_init_array_type_info<type>(8)                     \
+#define INIT_ARRAY_TYPE_INFO_LIST(type)                       \
+    {                                                         \
+        get_init_array_type_info<type>(0),                    \
+        get_init_array_type_info<type>(1),                    \
+        get_init_array_type_info<type>(2),                    \
+        get_init_array_type_info<type>(3),                    \
+        get_init_array_type_info<type>(4),                    \
+        get_init_array_type_info<type>(5),                    \
+        get_init_array_type_info<type>(6),                    \
+        get_init_array_type_info<type>(7),                    \
+        get_init_array_type_info<type>(8)                     \
     }
 
 template <FieldType field_type>
@@ -118,90 +105,59 @@ inline const ArrayTypeInfo* get_init_array_type_info(int32_t iterations) {
     static ArrayTypeInfo nested_type_info_6(&nested_type_info_5);
     static ArrayTypeInfo nested_type_info_7(&nested_type_info_6);
     static ArrayTypeInfo nested_type_info_8(&nested_type_info_7);
-    switch (iterations) {
-        case 0:
-            return &nested_type_info_0;
-        case 1:
-            return &nested_type_info_1;
-        case 2:
-            return &nested_type_info_2;
-        case 3:
-            return &nested_type_info_3;
-        case 4:
-            return &nested_type_info_4;
-        case 5:
-            return &nested_type_info_5;
-        case 6:
-            return &nested_type_info_6;
-        case 7:
-            return &nested_type_info_7;
-        case 8:
-            return &nested_type_info_8;
-        default:
-            DCHECK(false) << "the depth of nested array type should not be less than 0 and larger than 9";
-    }
-    return nullptr;
+    static ArrayTypeInfo* nested_type_info_array[] = {
+        &nested_type_info_0,
+        &nested_type_info_1,
+        &nested_type_info_2,
+        &nested_type_info_3,
+        &nested_type_info_4,
+        &nested_type_info_5,
+        &nested_type_info_6,
+        &nested_type_info_7,
+        &nested_type_info_8
+    };
+    return nested_type_info_array[iterations];
 }
 
 const TypeInfo* get_array_type_info(FieldType leaf_type, int32_t iterations) {
-    DCHECK(iterations <= 8) << "the depth of nested array type should not be larger than 9";
+    DCHECK(iterations <= 8) << "the depth of nested array type should not be larger than 8";
     static constexpr int32_t depth = 9;
-    static ArrayTypeInfo tinyint_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_TINYINT);
-    static ArrayTypeInfo smallint_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_SMALLINT);
-    static ArrayTypeInfo int_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_INT);
-    static ArrayTypeInfo unsigned_int_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_UNSIGNED_INT);
-    static ArrayTypeInfo bool_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_BOOL);
-    static ArrayTypeInfo bigint_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_BIGINT);;
-    static ArrayTypeInfo largeint_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_LARGEINT);
-    static ArrayTypeInfo float_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_FLOAT);
-    static ArrayTypeInfo double_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DOUBLE);
-    static ArrayTypeInfo decimal_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DECIMAL);
-    static ArrayTypeInfo date_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DATE);
-    static ArrayTypeInfo datetime_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DATETIME);;
-    static ArrayTypeInfo char_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_CHAR);
-    static ArrayTypeInfo varchar_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_VARCHAR);
-    static ArrayTypeInfo string_type_info[depth] = INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_STRING);
-    switch (leaf_type) {
-    case OLAP_FIELD_TYPE_TINYINT:
-        return &tinyint_type_info[iterations];
-    case OLAP_FIELD_TYPE_SMALLINT:
-        return &smallint_type_info[iterations];
-    case OLAP_FIELD_TYPE_INT:
-        return &int_type_info[iterations];
-    case OLAP_FIELD_TYPE_UNSIGNED_INT:
-        return &unsigned_int_type_info[iterations];
-    case OLAP_FIELD_TYPE_BOOL:
-        return &bool_type_info[iterations];
-    case OLAP_FIELD_TYPE_BIGINT:
-        return &bigint_type_info[iterations];
-    case OLAP_FIELD_TYPE_LARGEINT:
-        return &largeint_type_info[iterations];
-    case OLAP_FIELD_TYPE_FLOAT:
-        return &float_type_info[iterations];
-    case OLAP_FIELD_TYPE_DOUBLE:
-        return &double_type_info[iterations];
-    case OLAP_FIELD_TYPE_DECIMAL:
-        return &decimal_type_info[iterations];
-    case OLAP_FIELD_TYPE_DATE:
-        return &date_type_info[iterations];
-    case OLAP_FIELD_TYPE_DATETIME:
-        return &datetime_type_info[iterations];
-    case OLAP_FIELD_TYPE_CHAR:
-        return &char_type_info[iterations];
-    case OLAP_FIELD_TYPE_VARCHAR:
-        return &varchar_type_info[iterations];
-    case OLAP_FIELD_TYPE_STRING:
-        return &string_type_info[iterations];
-    default:
-        DCHECK(false) << "Bad field type: " << leaf_type;
-    }
-    return nullptr;
+    static const ArrayTypeInfo* array_type_Info_arr[][depth] = {
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_TINYINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_UNSIGNED_TINYINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_SMALLINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_UNSIGNED_SMALLINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_INT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_UNSIGNED_INT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_BIGINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_UNSIGNED_BIGINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_LARGEINT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_FLOAT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DOUBLE),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DISCRETE_DOUBLE),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_CHAR),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DATE),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DATETIME),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_DECIMAL),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_VARCHAR),
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr },
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_HLL),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_BOOL),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_OBJECT),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_STRING),
+        INIT_ARRAY_TYPE_INFO_LIST(OLAP_FIELD_TYPE_QUANTILE_STATE),
+    };
+    return array_type_Info_arr[leaf_type][iterations];
 }
 
 const TypeInfo* get_type_info(segment_v2::ColumnMetaPB* column_meta_pb) {
     FieldType type = (FieldType) column_meta_pb->type();
-    switch (type) {
-    case OLAP_FIELD_TYPE_ARRAY: {
+    if (UNLIKELY(type == OLAP_FIELD_TYPE_ARRAY)) {
         int32_t iterations = 0;
         const auto* child_column = &column_meta_pb->children_columns(0);
         while (child_column->type() == OLAP_FIELD_TYPE_ARRAY) {
@@ -209,16 +165,14 @@ const TypeInfo* get_type_info(segment_v2::ColumnMetaPB* column_meta_pb) {
             child_column = &child_column->children_columns(0);
         }
         return get_array_type_info((FieldType) child_column->type(), iterations);
-    }
-    default:
+    } else {
         return get_scalar_type_info(type);
     }
 }
 
 const TypeInfo* get_type_info(const TabletColumn* col) {
     auto type = col->type();
-    switch (type) {
-    case OLAP_FIELD_TYPE_ARRAY: {
+    if (UNLIKELY(type == OLAP_FIELD_TYPE_ARRAY)) {
         int32_t iterations = 0;
         const auto* child_column = &col->get_sub_column(0);
         while (child_column->type() == OLAP_FIELD_TYPE_ARRAY) {
@@ -226,8 +180,7 @@ const TypeInfo* get_type_info(const TabletColumn* col) {
             child_column = &child_column->get_sub_column(0);
         }
         return get_array_type_info(child_column->type(), iterations);
-    }
-    default:
+    } else {
         return get_scalar_type_info(type);
     }
 }
