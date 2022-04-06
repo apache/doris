250ff22b63bd516cb48ad338e5f5b443ef26a250
BiteTheDDDDt <952130278@qq.com>
2022-04-06 18:33:02 +0800

    fix datetime

--- be/src/vec/columns/column_vector.h
+++ be/src/vec/columns/column_vector.h
@@ -21,6 +21,7 @@
 #pragma once
 
 #include <cmath>
+#include <type_traits>
 
 #include "olap/uint24.h"
 #include "vec/columns/column.h"
@@ -188,7 +189,8 @@ public:
         for (int i = 0; i < num; i++) {
             const char* cur_ptr = data_ptr + value_size * i;
             uint64_t value = *reinterpret_cast<const uint64_t*>(cur_ptr);
-            vectorized::VecDateTimeValue date(value);
+            vectorized::VecDateTimeValue date;
+            date.from_olap_datetime(value);
             this->insert_data(reinterpret_cast<char*>(&date), 0);
         }
     }
