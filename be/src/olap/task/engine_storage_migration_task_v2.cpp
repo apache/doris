47853d252fe9be753cf8c35b2d2099117b512a46
pengxianyu <diablowcg@163.com>
2022-04-08 19:27:20 +0800

    Support remote storage, step3, only for be, add migration_task_v2

--- be/src/olap/task/engine_storage_migration_task_v2.cpp
+++ be/src/olap/task/engine_storage_migration_task_v2.cpp
@@ -49,7 +49,7 @@ OLAPStatus EngineStorageMigrationTaskV2::execute() {
         return res;
     }
 
-    LOG(INFO) << "success to create new alter tablet. res=" << res
+    LOG(INFO) << "success to create new storage migration v2. res=" << res
               << " base_tablet_id=" << _storage_migration_req.base_tablet_id << ", base_schema_hash"
               << _storage_migration_req.base_schema_hash
               << ", new_tablet_id=" << _storage_migration_req.new_tablet_id
