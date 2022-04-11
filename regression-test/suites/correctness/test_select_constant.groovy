02b50ab114163242bf841d18ccfc5be9568c03c2
Pxl <952130278@qq.com>
2022-04-11 09:39:32 +0800

    [fix](vectorized) core dump on ST_AsText (#8870)

--- regression-test/suites/correctness/test_select_constant.groovy
+++ regression-test/suites/correctness/test_select_constant.groovy
@@ -16,5 +16,6 @@
 // under the License.
 
 suite("test_select_constant") {
-    qt_select1 'select 100, "test", date("2021-01-02")'
+    qt_select1 'select 100, "test", date("2021-01-02");'
+    qt_select_geo1 'SELECT ST_AsText(ST_Point(123.12345678901234567890,89.1234567890));'
 }
