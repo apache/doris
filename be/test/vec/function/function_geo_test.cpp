b148099932269c65afe5ef15d5d511c5ba4fab84
Gabriel <gabrielleebuaa@gmail.com>
2022-04-11 09:36:53 +0800

    [feature](function)(vectorized) Support all geolocation functions on vectorized engine (#8846)

--- be/test/vec/function/function_geo_test.cpp
+++ be/test/vec/function/function_geo_test.cpp
@@ -142,6 +142,203 @@ TEST(function_geo_test, function_geo_st_distance_sphere) {
     }
 }
 
+TEST(function_geo_test, function_geo_st_contains) {
+    std::string func_name = "st_contains";
+    {
+        InputTypeSet input_types = {TypeIndex::String, TypeIndex::String};
+
+        std::string buf1;
+        std::string buf2;
+        std::string buf3;
+        GeoParseStatus status;
+
+        std::string shape1 = std::string("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))");
+        std::unique_ptr<GeoShape> shape(
+                GeoShape::from_wkt(shape1.data(), shape1.size(), &status));
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        ASSERT_TRUE(shape != nullptr);
+        shape->encode_to(&buf1);
+
+        GeoPoint point1;
+        status = point1.from_coord(5, 5);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        point1.encode_to(&buf2);
+
+        GeoPoint point2;
+        status = point2.from_coord(50, 50);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        point2.encode_to(&buf3);
+
+        DataSet data_set = {
+                {{buf1, buf2}, (uint8_t) 1},
+                {{buf1, buf3}, (uint8_t) 0},
+                {{buf1, Null()}, Null()},
+                {{Null(), buf3}, Null()}};
+
+        check_function<DataTypeUInt8 , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_circle) {
+    std::string func_name = "st_circle";
+    {
+        InputTypeSet input_types = {TypeIndex::Float64, TypeIndex::Float64, TypeIndex::Float64};
+
+        GeoCircle circle;
+        std::string buf;
+        auto value = circle.init(111, 64, 10000);
+        ASSERT_TRUE(value == GEO_PARSE_OK);
+        circle.encode_to(&buf);
+        DataSet data_set = {
+                {{(double) 111, (double) 64, (double) 10000}, buf},
+                {{Null(), (double) 64, (double) 10000}, Null()},
+                {{(double) 111, Null(), (double) 10000}, Null()},
+                {{(double) 111, (double) 64, Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_geometryfromtext) {
+    std::string func_name = "st_geometryfromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "LINESTRING (1 1, 2 2)";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+                {{std::string("LINESTRING (1 1, 2 2)")}, buf},
+                {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_geomfromtext) {
+    std::string func_name = "st_geomfromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "LINESTRING (1 1, 2 2)";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+            {{std::string("LINESTRING (1 1, 2 2)")}, buf},
+            {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_linefromtext) {
+    std::string func_name = "st_linefromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "LINESTRING (1 1, 2 2)";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+            {{std::string("LINESTRING (1 1, 2 2)")}, buf},
+            {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_linestringfromtext) {
+    std::string func_name = "st_linestringfromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "LINESTRING (1 1, 2 2)";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+                {{std::string("LINESTRING (1 1, 2 2)")}, buf},
+                {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_polygon) {
+    std::string func_name = "st_polygon";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+                {{std::string("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))")}, buf},
+                {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_polygonfromtext) {
+    std::string func_name = "st_polygonfromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+                {{std::string("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))")}, buf},
+                {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
+TEST(function_geo_test, function_geo_st_polyfromtext) {
+    std::string func_name = "st_polyfromtext";
+    {
+        InputTypeSet input_types = {TypeIndex::String};
+
+        GeoParseStatus status;
+        std::string buf;
+        std::string input = "POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))";
+        std::unique_ptr<GeoShape> shape(GeoShape::from_wkt(input.data(), input.size(), &status));
+        ASSERT_TRUE(shape != nullptr);
+        ASSERT_TRUE(status == GEO_PARSE_OK);
+        shape->encode_to(&buf);
+        DataSet data_set = {
+                {{std::string("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))")}, buf},
+                {{Null()}, Null()}};
+
+        check_function<DataTypeString , true>(func_name, input_types, data_set);
+    }
+}
+
 } // namespace doris::vectorized
 
 int main(int argc, char** argv) {
