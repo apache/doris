cd75401a2a908cc3bf893dbff396ffe0b279097b
platoneko <platonekosama@gmail.com>
2022-04-13 12:06:00 +0800

    test LocalWriteStream

--- be/test/filesystem/local_file_system_test.cpp
+++ be/test/filesystem/local_file_system_test.cpp
@@ -3,10 +3,13 @@
 #include <fcntl.h>
 #include <gtest/gtest.h>
 
+#include <cstddef>
+#include <cstdio>
 #include <filesystem>
 #include <string_view>
 
 #include "filesystem/local_read_stream.h"
+#include "filesystem/local_write_stream.h"
 
 namespace doris {
 
@@ -94,7 +97,7 @@ TEST_F(LocalFileSystemTest, read_file) {
     ASSERT_EQ(::write(fd, content.c_str(), content.size()), content.size());
     size_t file_size = content.size();
 
-    constexpr size_t buffer_size = 32;
+    constexpr size_t buffer_size = 64;
     LocalReadStream istream(fd, file_size, buffer_size);
 
     char buf1[BUFSIZ];
@@ -105,37 +108,93 @@ TEST_F(LocalFileSystemTest, read_file) {
     ASSERT_TRUE(istream.eof());
     ASSERT_EQ(std::string_view(buf1, read_n1), content);
 
-    s = istream.seek(0);
+    size_t off = 0;
+    s = istream.seek(off);
     ASSERT_TRUE(s.ok());
     istream.read(buf1, 16, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data(), 16));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 16));
+    off += 16;
     istream.read(buf1, 32, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + 16, 32));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 32));
+    off += 32;
     istream.read(buf1, 128, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + 48, 128));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 128));
 
-    s = istream.seek(60);
+    off = 60;
+    s = istream.seek(off);
     ASSERT_TRUE(s.ok());
     istream.read(buf1, 16, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + 60, 16));
-    istream.read(buf1, 32, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + 76, 32));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 16));
+    off += 16;
     istream.read(buf1, 64, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + 108, 64));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 64));
+    off += 64;
+    istream.read(buf1, 128, &read_n1);
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 128));
 
-    s = istream.seek(file_size - 108);
+    off = file_size - 108;
+    s = istream.seek(off);
     ASSERT_TRUE(s.ok());
     istream.read(buf1, 16, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1),
-              std::string_view(content.data() + file_size - 108, 16));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 16));
+    off += 16;
     istream.read(buf1, 32, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1),
-              std::string_view(content.data() + file_size - 92, 32));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 32));
+    off += 32;
     istream.read(buf1, 64, &read_n1);
-    ASSERT_EQ(std::string_view(buf1, read_n1),
-              std::string_view(content.data() + file_size - 60, 60));
+    ASSERT_EQ(std::string_view(buf1, read_n1), std::string_view(content.data() + off, 60));
+
+    ASSERT_TRUE(istream.close().ok());
 
     ::close(fd);
+}
+
+TEST_F(LocalFileSystemTest, write_file) {
+    const std::string content =
+            "O wild West Wind, thou breath of Autumn's being\n"
+            "Thou, from whose unseen presence the leaves dead\n"
+            "Are driven, like ghosts from an enchanter fleeing,\n"
+            "Yellow, and black, and pale, and hectic red,\n"
+            "Pestilence-stricken multitudes:O thou\n"
+            "Who chariotest to their dark wintry bed\n"
+            "The winged seeds, where they lie cold and low,\n"
+            "Each like a corpse within its grave, until\n"
+            "Thine azure sister of the Spring shall blow\n"
+            "Her clarion o'er the dreaming earth, and fill\n"
+            "(Driving sweet buds like flocks to feed in air)\n"
+            "With living hues and odors plain and hill:\n"
+            "Wild Spirit, which art moving everywhere;\n"
+            "Destroyer and preserver; hear, oh, hear!";
+
+    int fd = ::open("ut_dir/write_file", O_RDWR | O_CREAT | O_CLOEXEC, 0666);
+
+    constexpr size_t buffer_size = 64;
+    LocalWriteStream ostream(fd, buffer_size);
+
+    size_t off = 0;
+    ASSERT_TRUE(ostream.write(content.data(), 16).ok());
+    off += 16;
+    ASSERT_TRUE(ostream.write(content.data() + off, 32).ok());
+    off += 32;
+    ASSERT_TRUE(ostream.write(content.data() + off, 64).ok());
+    off += 64;
+    ASSERT_TRUE(ostream.write(content.data() + off, 64).ok());
+    off += 64;
+    ASSERT_TRUE(ostream.write(content.data() + off, 128).ok());
+    off += 128;
+    ASSERT_TRUE(ostream.sync().ok());
+
+    char buf1[BUFSIZ];
+    auto res = ::pread(fd, buf1, BUFSIZ, 0);
+    ASSERT_EQ(std::string_view(buf1, res), std::string_view(content.data(), off));
+
+    ASSERT_TRUE(ostream.write(content.data() + off, content.size() - off).ok());
+    ASSERT_TRUE(ostream.sync().ok());
+
+    res = ::pread(fd, buf1, BUFSIZ, 0);
+    ASSERT_EQ(std::string_view(buf1, res), content);
+
+    ASSERT_TRUE(ostream.close().ok());
 }
 
 } // namespace doris
