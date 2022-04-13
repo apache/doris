ae3df779c87ec3d97bffa1d803defa2b5a24514b
qiye <jianliang5669@gmail.com>
2022-04-13 09:52:03 +0800

    [feature](cold-hot) support s3 resource (#8808)
    Add cold hot support in FE meta, support alter resource DDL in FE

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyPartitionClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyPartitionClause.java
@@ -18,13 +18,11 @@
 package org.apache.doris.analysis;
 
 import org.apache.doris.alter.AlterOpType;
-import org.apache.doris.catalog.DataProperty;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.util.PrintableMap;
 import org.apache.doris.common.util.PropertyAnalyzer;
 
 import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -98,10 +96,10 @@ public class ModifyPartitionClause extends AlterTableClause {
     // 3. in_memory
     // 4. tablet type
     private void checkProperties(Map<String, String> properties) throws AnalysisException {
-        // 1. data property
-        DataProperty newDataProperty = null;
-        newDataProperty = PropertyAnalyzer.analyzeDataProperty(properties, DataProperty.DEFAULT_DATA_PROPERTY);
-        Preconditions.checkNotNull(newDataProperty);
+        // 1. data property, can not modify partition property remote_storage_resource
+        if (properties.containsKey(PropertyAnalyzer.PROPERTIES_REMOTE_STORAGE_RESOURCE)) {
+            throw new AnalysisException("Do not support modify partition data property `remote_storage_resource`.");
+        }
 
         // 2. replica allocation
         PropertyAnalyzer.analyzeReplicaAllocation(properties, "");
