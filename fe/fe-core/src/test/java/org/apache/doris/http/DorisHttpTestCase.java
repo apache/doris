apache/doris

commit 5aab69700d29897e3d1945a75a1c058a6644c56f

[refactor] Change ALL OLAPStatus to Status (#8855)
Currently, there are 2 status code in BE, one is common/Status.h,
and the other is olap/olap_define.h called OLAPStatus.
OLAPStatus is just an enum type, it is very simple and could not save many informations,
I will unify these code to common/Status.

be/src/olap/rowset/CMakeLists.txt
@@ -46,3 +46,5 @@ add_library(Rowset STATIC
     beta_rowset.cpp
     beta_rowset_reader.cpp
     beta_rowset_writer.cpp)
+
+target_compile_options(Rowset PUBLIC "-Wno-error=maybe-uninitialized")

be/src/olap/rowset/run_length_integer_reader.cpp
@@ -26,15 +26,15 @@ namespace doris {
 RunLengthIntegerReader::RunLengthIntegerReader(ReadOnlyFileStream* input, bool is_singed)
         : _input(input), _signed(is_singed), _num_literals(0), _used(0) {}
 
-OLAPStatus RunLengthIntegerReader::_read_values() {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::_read_values() {
+    Status res = Status::OK();
 
     // read the first 2 bits and determine the encoding type
     uint8_t first_byte;
 
     res = _input->read((char*)&first_byte);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to read first byte.[res=%d]", res);
+    if (!res.ok()) {
+        LOG(WARNING) << "fail to read first byte.res = " << res;
         return res;
     } else {
         int enc = (first_byte >> 6) & 0x03;
@@ -53,8 +53,8 @@ OLAPStatus RunLengthIntegerReader::_read_values() {
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
+    Status res = Status::OK();
 
     // extract the number of fixed bits
     uint32_t fb = (first_byte >> 1) & 0x1f;
@@ -65,11 +65,10 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
 
     // extract the blob run length
     int32_t len = (first_byte & 0x01) << 8;
-    uint8_t byte;
+    uint8_t byte = 0;
 
     res = _input->read((char*)&byte);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to read byte from instream.[res=%d]", res);
+    if (!res.ok()) {
         return res;
     }
 
@@ -80,14 +79,14 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
 
     if (_signed) {
         res = ser::read_var_signed(_input, &first_val);
-        if (OLAP_SUCCESS != res) {
-            OLAP_LOG_WARNING("fail to read var signed.[res=%d]", res);
+        if (!res.ok()) {
+            LOG(WARNING) << "fail to read var signed.res = " << res;
             return res;
         }
     } else {
         res = ser::read_var_unsigned(_input, &first_val);
-        if (OLAP_SUCCESS != res) {
-            OLAP_LOG_WARNING("fail to read var unsigned.[res=%d]", res);
+        if (!res.ok()) {
+            LOG(WARNING) << "fail to read var unsigned.res = " << res;
             return res;
         }
     }
@@ -103,8 +102,8 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
         int64_t fd = 0;
 
         res = ser::read_var_signed(_input, &fd);
-        if (OLAP_SUCCESS != res) {
-            OLAP_LOG_WARNING("fail to read var signed.[res=%d]", res);
+        if (!res.ok()) {
+            LOG(WARNING) << "fail to read var signed.res = " << res;
             return res;
         }
 
@@ -118,8 +117,8 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
         int64_t delta_base = 0;
 
         res = ser::read_var_signed(_input, &delta_base);
-        if (OLAP_SUCCESS != res) {
-            OLAP_LOG_WARNING("fail to read var signed.[res=%d]", res);
+        if (!res.ok()) {
+            LOG(WARNING) << "fail to read var signed.res = " << res;
             return res;
         }
 
@@ -132,8 +131,8 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
         // value to result buffer. if the delta base value is negative then it
         // is a decreasing sequence else an increasing sequence
         res = ser::read_ints(_input, &_literals[_num_literals], len, fb);
-        if (OLAP_SUCCESS != res) {
-            OLAP_LOG_WARNING("fail to read ints.[res = %d]", res);
+        if (!res.ok()) {
+            LOG(WARNING) << "fail to read ints.res = " << res;
             return res;
         }
 
@@ -153,8 +152,8 @@ OLAPStatus RunLengthIntegerReader::_read_delta_values(uint8_t first_byte) {
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte) {
+    Status res = Status::OK();
 
     // extract the number of fixed bits
     int32_t fbo = (first_byte >> 1) & 0x1f;
@@ -162,11 +161,10 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
 
     // extract the run length of data blob
     int32_t len = (first_byte & 0x01) << 8;
-    uint8_t byte;
+    uint8_t byte = 0;
 
     res = _input->read((char*)&byte);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to read byte from in_straem.[res=%d]", res);
+    if (!res.ok()) {
         return res;
     }
 
@@ -178,8 +176,8 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     char third_byte = '\0';
 
     res = _input->read(&third_byte);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to read byte from in_stream.[res=%d]", res);
+    if (!res.ok()) {
+        LOG(WARNING) << "fail to read byte from in_stream.res = " << res;
         return res;
     }
 
@@ -195,8 +193,8 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     char four_byte = '\0';
 
     res = _input->read(&four_byte);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to read byte from in_straem.[res=%d]", res);
+    if (!res.ok()) {
+        LOG(WARNING) << "fail to read byte from in_straem.res = " << res;
         return res;
     }
 
@@ -211,8 +209,8 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     int64_t base = 0;
 
     res = ser::bytes_to_long_be(_input, bw, &base);
-    if (OLAP_SUCCESS != res) {
-        OLAP_LOG_WARNING("fail to bytes to long be.[res=%d]", res);
+    if (!res.ok()) {
+        LOG(WARNING) << "fail to bytes to long be.res = " << res;
         return res;
     }
 
@@ -229,7 +227,7 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     int64_t unpacked[len];
 
     res = ser::read_ints(_input, unpacked, len, fb);
-    if (OLAP_SUCCESS != res) {
+    if (!res.ok()) {
         return res;
     }
 
@@ -238,7 +236,7 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     uint32_t bit_width = ser::get_closet_fixed_bits(pw + pgw);
 
     res = ser::read_ints(_input, unpacked_patch, pl, bit_width);
-    if (OLAP_SUCCESS != res) {
+    if (!res.ok()) {
         return res;
     }
 
@@ -304,8 +302,8 @@ OLAPStatus RunLengthIntegerReader::_read_patched_base_values(uint8_t first_byte)
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
+    Status res = Status::OK();
 
     // extract the number of fixed bits
     uint32_t fbo = (first_byte >> 1) & 0x1f;
@@ -316,7 +314,7 @@ OLAPStatus RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
     uint8_t byte;
 
     res = _input->read((char*)&byte);
-    if (OLAP_SUCCESS != res) {
+    if (!res.ok()) {
         return res;
     }
 
@@ -326,7 +324,7 @@ OLAPStatus RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
 
     // write the unpacked values and zigzag decode to result buffer
     res = ser::read_ints(_input, _literals, len, fb);
-    if (OLAP_SUCCESS != res) {
+    if (!res.ok()) {
         return res;
     }
 
@@ -342,8 +340,8 @@ OLAPStatus RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::_read_short_repeat_values(uint8_t first_byte) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::_read_short_repeat_values(uint8_t first_byte) {
+    Status res = Status::OK();
 
     // read the number of bytes occupied by the value
     int32_t size = (first_byte >> 3) & 0x07;
@@ -359,7 +357,7 @@ OLAPStatus RunLengthIntegerReader::_read_short_repeat_values(uint8_t first_byte)
     int64_t val = 0;
 
     res = ser::bytes_to_long_be(_input, size, &val);
-    if (OLAP_SUCCESS != res) {
+    if (!res.ok()) {
         return res;
     }
 
@@ -375,10 +373,10 @@ OLAPStatus RunLengthIntegerReader::_read_short_repeat_values(uint8_t first_byte)
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::seek(PositionProvider* position) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::seek(PositionProvider* position) {
+    Status res = Status::OK();
 
-    if (OLAP_SUCCESS != (res = _input->seek(position))) {
+    if (!(res = _input->seek(position))) {
         return res;
     }
 
@@ -390,7 +388,7 @@ OLAPStatus RunLengthIntegerReader::seek(PositionProvider* position) {
             _num_literals = 0;
 
             res = _read_values();
-            if (OLAP_SUCCESS != res) {
+            if (!res.ok()) {
                 return res;
             }
 
@@ -405,8 +403,8 @@ OLAPStatus RunLengthIntegerReader::seek(PositionProvider* position) {
     return res;
 }
 
-OLAPStatus RunLengthIntegerReader::skip(uint64_t num_values) {
-    OLAPStatus res = OLAP_SUCCESS;
+Status RunLengthIntegerReader::skip(uint64_t num_values) {
+    Status res = Status::OK();
 
     while (num_values > 0) {
         if (_used == _num_literals) {
@@ -414,8 +412,8 @@ OLAPStatus RunLengthIntegerReader::skip(uint64_t num_values) {
             _used = 0;
 
             res = _read_values();
-            if (OLAP_SUCCESS != res) {
-                OLAP_LOG_WARNING("fail to read values.[res=%d]", res);
+            if (!res.ok()) {
+                LOG(WARNING) << "fail to read values.res = " << res;
                 return res;
             }
         }

commit 32f4f777ffb8b0c8af6666f6f4a32da661dd731d

[fix](ut)(compile) Fix BE compile bug and FE unit test (#9027)
1. The compile bug is introduced from #8855
2. FE ut bug is introduced from #8848 and #8770

be/src/olap/rowset/CMakeLists.txt
@@ -47,4 +47,4 @@ add_library(Rowset STATIC
     beta_rowset_reader.cpp
     beta_rowset_writer.cpp)
 
-target_compile_options(Rowset PUBLIC "-Wno-error=maybe-uninitialized")
+target_compile_options(Rowset PUBLIC)

be/src/olap/rowset/column_reader.cpp
@@ -190,7 +190,7 @@ Status StringColumnDirectReader::next_vector(ColumnVector* column_vector, uint32
             string_buffer_size += length;
         }
 
-        uint8_t* allocated_mem;
+        uint8_t* allocated_mem = nullptr;
         res = mem_pool->allocate_safely(string_buffer_size, allocated_mem);
         if (!res.ok()) {
             return res;
@@ -229,7 +229,7 @@ Status StringColumnDirectReader::next_vector(ColumnVector* column_vector, uint32
             }
         }
 
-        uint8_t* allocated_mem;
+        uint8_t* allocated_mem = nullptr;
         res = mem_pool->allocate_safely(string_buffer_size, allocated_mem);
         if (!res.ok()) {
             return res;

be/src/olap/rowset/run_length_integer_reader.cpp
@@ -30,7 +30,7 @@ Status RunLengthIntegerReader::_read_values() {
     Status res = Status::OK();
 
     // read the first 2 bits and determine the encoding type
-    uint8_t first_byte;
+    uint8_t first_byte = 0;
 
     res = _input->read((char*)&first_byte);
     if (!res.ok()) {
@@ -311,7 +311,7 @@ Status RunLengthIntegerReader::_read_direct_values(uint8_t first_byte) {
 
     // extract the run length
     int32_t len = (first_byte & 0x01) << 8;
-    uint8_t byte;
+    uint8_t byte = 0;
 
     res = _input->read((char*)&byte);
     if (!res.ok()) {

be/src/tools/meta_tool.cpp
@@ -114,7 +114,7 @@ void get_meta(DataDir* data_dir) {
     std::string value;
     Status s =
             TabletMetaManager::get_json_meta(data_dir, FLAGS_tablet_id, FLAGS_schema_hash, &value);
-    if (s == doris::Status::OLAPInternalError(OLAP_ERR_META_KEY_NOT_FOUND)) {
+    if (s == doris::Status::OLAPInternalError(doris::OLAP_ERR_META_KEY_NOT_FOUND)) {
         std::cout << "no tablet meta for tablet_id:" << FLAGS_tablet_id
                   << ", schema_hash:" << FLAGS_schema_hash << std::endl;
         return;

fe/fe-core/src/main/java/org/apache/doris/PaloFe.java
@@ -142,7 +142,7 @@ public class PaloFe {
             httpServer.setWorkers(Config.jetty_server_workers);
             httpServer.setMaxThreads(Config.jetty_threadPool_maxThreads);
             httpServer.setMaxThreads(Config.jetty_threadPool_minThreads);
-            httpServer.start(dorisHomeDir);
+            httpServer.start();
             
             qeService.start();
 

fe/fe-core/src/main/java/org/apache/doris/common/Log4jConfig.java
@@ -19,14 +19,14 @@ package org.apache.doris.common;
 
 import org.apache.doris.httpv2.config.SpringLog4j2Config;
 
+import com.google.common.collect.Maps;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 import org.apache.logging.log4j.core.lookup.Interpolator;
 import org.apache.logging.log4j.core.lookup.StrSubstitutor;
-
-import com.google.common.collect.Maps;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;

fe/fe-core/src/main/java/org/apache/doris/httpv2/HttpServer.java
@@ -19,6 +19,7 @@ package org.apache.doris.httpv2;
 
 import org.apache.doris.PaloFe;
 import org.apache.doris.common.Config;
+import org.apache.doris.common.FeConstants;
 import org.apache.doris.httpv2.config.SpringLog4j2Config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -77,7 +78,7 @@ public class HttpServer extends SpringBootServletInitializer {
         return application.sources(HttpServer.class);
     }
 
-    public void start(String dorisHome) {
+    public void start() {
         Map<String, Object> properties = new HashMap<>();
         properties.put("server.port", port);
         properties.put("server.servlet.context-path", "/");
@@ -101,7 +102,12 @@ public class HttpServer extends SpringBootServletInitializer {
             System.setProperty("spring.http.multipart.location", PaloFe.DORIS_HOME_DIR);
         }
         System.setProperty("spring.banner.image.location", "doris-logo.png");
-        properties.put("logging.config", Config.custom_config_dir + "/" + SpringLog4j2Config.SPRING_LOG_XML_FILE);
+        if (FeConstants.runningUnitTest) {
+            // this is currently only used for unit test
+            properties.put("logging.config", getClass().getClassLoader().getResource("log4j2.xml").getPath());
+        } else {
+            properties.put("logging.config", Config.custom_config_dir + "/" + SpringLog4j2Config.SPRING_LOG_XML_FILE);
+        }
         new SpringApplicationBuilder()
                 .sources(HttpServer.class)
                 .properties(properties)

fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBDebugger.java
@@ -89,7 +89,7 @@ public class BDBDebugger {
 
         HttpServer httpServer = new HttpServer();
         httpServer.setPort(Config.http_port);
-        httpServer.start(dorisHomeDir);
+        httpServer.start();
 
         // MySQl server
         QeService qeService = new QeService(Config.query_port, Config.mysql_service_nio_enabled, ExecuteEnv.getInstance().getScheduler());

fe/fe-core/src/test/java/org/apache/doris/http/DorisHttpTestCase.java
@@ -38,9 +38,9 @@ import org.apache.doris.catalog.Tablet;
 import org.apache.doris.catalog.TabletInvertedIndex;
 import org.apache.doris.catalog.TabletMeta;
 import org.apache.doris.common.AnalysisException;
-import org.apache.doris.common.Config;
 import org.apache.doris.common.DdlException;
 import org.apache.doris.common.ExceptionChecker.ThrowingRunnable;
+import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.jmockit.Deencapsulation;
 import org.apache.doris.httpv2.HttpServer;
 import org.apache.doris.httpv2.IllegalArgException;
@@ -53,6 +53,7 @@ import org.apache.doris.system.SystemInfoService;
 import org.apache.doris.thrift.TStorageMedium;
 import org.apache.doris.thrift.TStorageType;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import org.junit.After;
@@ -60,7 +61,10 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -113,7 +117,19 @@ abstract public class DorisHttpTestCase {
 
     protected String rootAuth = Credentials.basic("root", "");
 
-    public static final String DORIS_HOME_DIR = System.getenv("DORIS_HOME");
+    private static final String DORIS_HOME;
+
+    static {
+        String dorisHome = System.getenv("DORIS_HOME");
+        if (Strings.isNullOrEmpty(dorisHome)) {
+            try {
+                dorisHome = Files.createTempDirectory("DORIS_HOME").toAbsolutePath().toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        DORIS_HOME = dorisHome;
+    }
 
     @Mocked
     private static EditLog editLog;
@@ -296,15 +312,21 @@ abstract public class DorisHttpTestCase {
             }
         }
 
+        FeConstants.runningUnitTest = true;
         httpServer = new HttpServer();
-        httpServer.setPort(Config.http_port);
-        httpServer.setMaxHttpPostSize(Config.jetty_server_max_http_post_size);
-        httpServer.setAcceptors(Config.jetty_server_acceptors);
-        httpServer.setSelectors(Config.jetty_server_selectors);
-        httpServer.setWorkers(Config.jetty_server_workers);
-        httpServer.start(DORIS_HOME_DIR);
+        httpServer.setPort(HTTP_PORT);
+        httpServer.setMaxHttpPostSize(100 * 1024 * 1024);
+        httpServer.setAcceptors(2);
+        httpServer.setSelectors(4);
+        httpServer.setWorkers(0);
+        httpServer.start();
     }
 
+    @AfterClass
+    public static void afterClass() {
+        File file = new File(DORIS_HOME);
+        file.delete();
+    }
 
 
     @Before

fe/fe-core/src/test/java/org/apache/doris/http/TableQueryPlanActionTest.java
@@ -58,9 +58,9 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         String respStr = response.body().string();
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
         System.out.println(respStr);
-        Assert.assertEquals(200, (long) jsonObject.get("status"));
+        Assert.assertEquals(200, (long) ((JSONObject) jsonObject.get("data")).get("status"));
 
-        JSONObject partitionsObject = (JSONObject) jsonObject.get("partitions");
+        JSONObject partitionsObject = (JSONObject) ((JSONObject) jsonObject.get("data")).get("partitions");
         Assert.assertNotNull(partitionsObject);
         for (Object tabletKey : partitionsObject.keySet()) {
             JSONObject tabletObject = (JSONObject) partitionsObject.get(tabletKey);
@@ -70,7 +70,7 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
             Assert.assertEquals(testSchemaHash, (long) tabletObject.get("schemaHash"));
 
         }
-        String queryPlan = (String) jsonObject.get("opaqued_query_plan");
+        String queryPlan = (String) ((JSONObject) jsonObject.get("data")).get("opaqued_query_plan");
         Assert.assertNotNull(queryPlan);
         byte[] binaryPlanInfo = Base64.getDecoder().decode(queryPlan);
         TDeserializer deserializer = new TDeserializer();
@@ -93,8 +93,8 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         System.out.println(respStr);
         Assert.assertNotNull(respStr);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(400, (long) jsonObject.get("status"));
-        String exception = (String) jsonObject.get("exception");
+        Assert.assertEquals(403, (long) jsonObject.get("code"));
+        String exception = (String) jsonObject.get("data");
         Assert.assertNotNull(exception);
         Assert.assertEquals("POST body must contains [sql] root object", exception);
     }
@@ -112,8 +112,8 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         System.out.println(respStr);
         Assert.assertNotNull(respStr);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(400, (long) jsonObject.get("status"));
-        String exception = (String) jsonObject.get("exception");
+        Assert.assertEquals(403, (long) jsonObject.get("code"));
+        String exception = (String) jsonObject.get("data");
         Assert.assertNotNull(exception);
         Assert.assertEquals("POST body must contains [sql] root object", exception);
     }
@@ -131,8 +131,8 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         System.out.println(respStr);
         Assert.assertNotNull(respStr);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(400, (long) jsonObject.get("status"));
-        String exception = (String) jsonObject.get("exception");
+        Assert.assertEquals(400, (long) ((JSONObject) jsonObject.get("data")).get("status"));
+        String exception = (String) ((JSONObject) jsonObject.get("data")).get("exception");
         Assert.assertNotNull(exception);
         Assert.assertTrue(exception.startsWith("requested database and table must consistent with sql"));
     }
@@ -149,15 +149,15 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         String respStr = response.body().string();
         Assert.assertNotNull(respStr);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(400, (long) jsonObject.get("status"));
-        String exception = (String) jsonObject.get("exception");
+        Assert.assertEquals(403, (long) jsonObject.get("code"));
+        String exception = (String) jsonObject.get("data");
         Assert.assertNotNull(exception);
         Assert.assertTrue(exception.startsWith("malformed json"));
     }
 
     @Test
     public void testNotOlapTableFailure() throws IOException {
-        RequestBody body = RequestBody.create(JSON, "{ \"sql\" :  \" select k1,k2 from " + DB_NAME + ".es_table" + " \" }");
+        RequestBody body = RequestBody.create("{ \"sql\" :  \" select k1,k2 from " + DB_NAME + ".es_table" + " \" }", JSON);
         Request request = new Request.Builder()
                 .post(body)
                 .addHeader("Authorization", rootAuth)
@@ -167,8 +167,9 @@ public class TableQueryPlanActionTest extends DorisHttpTestCase {
         String respStr = response.body().string();
         Assert.assertNotNull(respStr);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(403, (long) jsonObject.get("status"));
-        String exception = (String) jsonObject.get("exception");
+        System.out.println(respStr);
+        Assert.assertEquals(1, (long) jsonObject.get("code"));
+        String exception = (String) jsonObject.get("data");
         Assert.assertTrue(exception.contains("table type is not OLAP"));
     }
 }

fe/fe-core/src/test/java/org/apache/doris/http/TableRowCountActionTest.java
@@ -40,8 +40,10 @@ public class TableRowCountActionTest extends DorisHttpTestCase {
                 .build();
 
         Response response = networkClient.newCall(request).execute();
-        JSONObject jsonObject = (JSONObject) JSONValue.parse(response.body().string());
-        Assert.assertEquals(200, (long) jsonObject.get("status"));
-        Assert.assertEquals(2000, (long) jsonObject.get("size"));
+        String res = response.body().string();
+        System.out.println(res);
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(res);
+        Assert.assertEquals(200, (long) ((JSONObject) jsonObject.get("data")).get("status"));
+        Assert.assertEquals(2000, (long) ((JSONObject) jsonObject.get("data")).get("size"));
     }
 }

fe/fe-core/src/test/java/org/apache/doris/http/TableSchemaActionTest.java
@@ -45,8 +45,8 @@ public class TableSchemaActionTest extends DorisHttpTestCase {
         String respStr = response.body().string();
         Assert.assertNotNull(respStr);
         JSONObject object = (JSONObject) JSONValue.parse(respStr);
-        Assert.assertEquals(200, (long) object.get("status"));
-        JSONArray propArray = (JSONArray) object.get("properties");
+        Assert.assertEquals(200, (long) ((JSONObject) object.get("data")).get("status"));
+        JSONArray propArray = (JSONArray) ((JSONObject) object.get("data")).get("properties");
         // k1, k2
         Assert.assertEquals(2, propArray.size());
     }

fe/fe-core/src/test/java/org/apache/doris/rewrite/ExtractCommonFactorsRuleFunctionTest.java
@@ -18,7 +18,6 @@
 
 package org.apache.doris.rewrite;
 
-import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.utframe.DorisAssert;
 import org.apache.doris.utframe.UtFrameUtils;
@@ -256,62 +255,55 @@ public class ExtractCommonFactorsRuleFunctionTest {
         // tinyint
         String sql = "select * from tb3 where k1 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainContains("CAST(`k1` AS CHARACTER) LIKE '%4%'");
+        dorisAssert.query(sql).explainContains("`k1` LIKE '%4%'");
 
         // smallint
         sql = "select * from tb3 where k2 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainContains("CAST(`k2` AS CHARACTER) LIKE '%4%'");
+        dorisAssert.query(sql).explainContains("`k2` LIKE '%4%'");
 
         // int
         sql = "select * from tb3 where k3 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainContains("CAST(`k3` AS CHARACTER) LIKE '%4%'");
+        dorisAssert.query(sql).explainContains("`k3` LIKE '%4%'");
 
         // bigint
         sql = "select * from tb3 where k4 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainContains("CAST(`k4` AS CHARACTER) LIKE '%4%'");
+        dorisAssert.query(sql).explainContains("`k4` LIKE '%4%'");
 
         // largeint
         sql = "select * from tb3 where k5 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainContains("CAST(`k5` AS CHARACTER) LIKE '%4%'");
+        dorisAssert.query(sql).explainContains("`k5` LIKE '%4%'");
     }
 
-    @Test(expected = AnalysisException.class)
+    @Test
     public void testRewriteLikePredicateDate() throws Exception {
         // date
         String sql = "select * from tb3 where k6 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainQuery();
-        Assert.fail("No exception throws.");
     }
 
-    @Test(expected = AnalysisException.class)
+    @Test
     public void testRewriteLikePredicateDateTime() throws Exception {
         // datetime
         String sql = "select * from tb3 where k7 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
         dorisAssert.query(sql).explainQuery();
-        Assert.fail("No exception throws.");
     }
 
-    @Test(expected = AnalysisException.class)
+    @Test
     public void testRewriteLikePredicateFloat() throws Exception {
         // date
         String sql = "select * from tb3 where k8 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainQuery();
-        Assert.fail("No exception throws.");
     }
 
-    @Test(expected = AnalysisException.class)
+    @Test
     public void testRewriteLikePredicateDouble() throws Exception {
         // date
         String sql = "select * from tb3 where k9 like '%4%';";
         LOG.info("EXPLAIN:{}", dorisAssert.query(sql).explainQuery());
-        dorisAssert.query(sql).explainQuery();
-        Assert.fail("No exception throws.");
     }
 }