apache/doris

commit 77aac49857956eb15f09c69210646834aa9b98ad

[refactor](schema_hash) remove schema_hash since every tablet id in be is unique (#8574)

be/src/agent/task_worker_pool.cpp
@@ -378,8 +378,7 @@ void TaskWorkerPool::_create_tablet_worker_thread_callback() {
         } else {
             ++_s_report_version;
             // get path hash of the created tablet
-            TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(
-                    create_tablet_req.tablet_id, create_tablet_req.tablet_schema.schema_hash);
+            TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(create_tablet_req.tablet_id);
             DCHECK(tablet != nullptr);
             TTabletInfo tablet_info;
             tablet_info.tablet_id = tablet->table_id();
@@ -433,7 +432,7 @@ void TaskWorkerPool::_drop_tablet_worker_thread_callback() {
         TStatus task_status;
         string err;
         TabletSharedPtr dropped_tablet = StorageEngine::instance()->tablet_manager()->get_tablet(
-                drop_tablet_req.tablet_id, drop_tablet_req.schema_hash, false, &err);
+                drop_tablet_req.tablet_id, false, &err);
         if (dropped_tablet != nullptr) {
             OLAPStatus drop_status = StorageEngine::instance()->tablet_manager()->drop_tablet(
                     drop_tablet_req.tablet_id, drop_tablet_req.schema_hash);
@@ -824,7 +823,7 @@ void TaskWorkerPool::_update_tablet_meta_worker_thread_callback() {
 
         for (auto tablet_meta_info : update_tablet_meta_req.tabletMetaInfos) {
             TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(
-                    tablet_meta_info.tablet_id, tablet_meta_info.schema_hash);
+                    tablet_meta_info.tablet_id);
             if (tablet == nullptr) {
                 LOG(WARNING) << "could not find tablet when update partition id"
                              << " tablet_id=" << tablet_meta_info.tablet_id
@@ -977,11 +976,9 @@ void TaskWorkerPool::_storage_medium_migrate_worker_thread_callback() {
 OLAPStatus TaskWorkerPool::_check_migrate_request(const TStorageMediumMigrateReq& req,
                                                   TabletSharedPtr& tablet, DataDir** dest_store) {
     int64_t tablet_id = req.tablet_id;
-    int32_t schema_hash = req.schema_hash;
-    tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+    tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
-        LOG(WARNING) << "can't find tablet. tablet_id= " << tablet_id
-                     << " schema_hash=" << schema_hash;
+        LOG(WARNING) << "can't find tablet. tablet_id= " << tablet_id;
         return OLAP_ERR_TABLE_NOT_FOUND;
     }
 
@@ -1552,10 +1549,9 @@ void TaskWorkerPool::_move_dir_thread_callback() {
 Status TaskWorkerPool::_move_dir(const TTabletId tablet_id, const TSchemaHash schema_hash,
                                       const std::string& src, int64_t job_id, bool overwrite) {
     TabletSharedPtr tablet =
-            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
-        LOG(INFO) << "failed to get tablet. tablet_id:" << tablet_id
-                  << ", schema hash:" << schema_hash;
+        LOG(INFO) << "failed to get tablet. tablet_id:" << tablet_id;
         return Status::InvalidArgument("Could not find tablet");
     }
 

be/src/exec/olap_scan_node.cpp
@@ -775,8 +775,7 @@ Status OlapScanNode::start_scan_thread(RuntimeState* state) {
         auto tablet_id = scan_range->tablet_id;
         int32_t schema_hash = strtoul(scan_range->schema_hash.c_str(), nullptr, 10);
         std::string err;
-        TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(
-                tablet_id, schema_hash, true, &err);
+        TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, true, &err);
         if (tablet == nullptr) {
             std::stringstream ss;
             ss << "failed to get tablet: " << tablet_id << " with schema hash: " << schema_hash

be/src/exec/olap_scanner.cpp
@@ -74,8 +74,7 @@ Status OlapScanner::prepare(
     _version = strtoul(scan_range.version.c_str(), nullptr, 10);
     {
         std::string err;
-        _tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash,
-                                                                          true, &err);
+        _tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, true, &err);
         if (_tablet.get() == nullptr) {
             std::stringstream ss;
             ss << "failed to get tablet. tablet_id=" << tablet_id

be/src/http/action/compaction_action.cpp
@@ -42,20 +42,17 @@ const static std::string HEADER_JSON = "application/json";
 bool CompactionAction::_is_compaction_running = false;
 std::mutex CompactionAction::_compaction_running_mutex;
 
-Status CompactionAction::_check_param(HttpRequest* req, uint64_t* tablet_id,
-                                      uint32_t* schema_hash) {
+Status CompactionAction::_check_param(HttpRequest* req, uint64_t* tablet_id) {
     std::string req_tablet_id = req->param(TABLET_ID_KEY);
-    std::string req_schema_hash = req->param(TABLET_SCHEMA_HASH_KEY);
-    if (req_tablet_id == "" && req_schema_hash == "") {
+    if (req_tablet_id == "") {
         return Status::OK();
     }
 
     try {
         *tablet_id = std::stoull(req_tablet_id);
-        *schema_hash = std::stoul(req_schema_hash);
     } catch (const std::exception& e) {
         return Status::InternalError(
-            strings::Substitute("convert tablet_id and schema_hash failed, $0", e.what()));
+            strings::Substitute("convert tablet_id failed, $0", e.what()));
     }
 
     return Status::OK();
@@ -64,16 +61,14 @@ Status CompactionAction::_check_param(HttpRequest* req, uint64_t* tablet_id,
 // for viewing the compaction status
 Status CompactionAction::_handle_show_compaction(HttpRequest* req, std::string* json_result) {
     uint64_t tablet_id = 0;
-    uint32_t schema_hash = 0;
-    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id, &schema_hash),
+    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id),
                                    "check param failed");
 
     TabletSharedPtr tablet =
-            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
         return Status::NotFound(
-            strings::Substitute("Tablet not found. tablet_id=$0, schema_hash=$1",
-                                tablet_id, schema_hash));
+            strings::Substitute("Tablet not found. tablet_id=$0", tablet_id));
     }
 
     tablet->get_compaction_status(json_result);
@@ -82,10 +77,9 @@ Status CompactionAction::_handle_show_compaction(HttpRequest* req, std::string* 
 
 Status CompactionAction::_handle_run_compaction(HttpRequest* req, std::string* json_result) {
     // 1. param check
-    // check req_tablet_id and req_schema_hash is not empty
+    // check req_tablet_id is not empty
     uint64_t tablet_id = 0;
-    uint32_t schema_hash = 0;
-    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id, &schema_hash),
+    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id),
                                    "check param failed");
 
     // check compaction_type equals 'base' or 'cumulative'
@@ -96,13 +90,12 @@ Status CompactionAction::_handle_run_compaction(HttpRequest* req, std::string* j
             strings::Substitute("The compaction type '$0' is not supported", compaction_type));
     }
 
-    // 2. fetch the tablet by tablet_id and schema_hash
+    // 2. fetch the tablet by tablet_id
     TabletSharedPtr tablet =
-            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
         return Status::NotFound(
-            strings::Substitute("Tablet not found. tablet_id=$0, schema_hash=$1",
-                                tablet_id, schema_hash));
+            strings::Substitute("Tablet not found. tablet_id=$0", tablet_id));
     }
 
     // 3. execute compaction task
@@ -146,27 +139,21 @@ Status CompactionAction::_handle_run_compaction(HttpRequest* req, std::string* j
 
 Status CompactionAction::_handle_run_status_compaction(HttpRequest* req, std::string* json_result) {
     uint64_t tablet_id = 0;
-    uint32_t schema_hash = 0;
 
-    // check req_tablet_id and req_schema_hash is not empty
-    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id, &schema_hash),
-                                   "check param failed");
-
+    // check req_tablet_id is not empty
+    RETURN_NOT_OK_STATUS_WITH_WARN(_check_param(req, &tablet_id),"check param failed");
 
     if (tablet_id == 0) {
         // overall compaction status
         RETURN_IF_ERROR(StorageEngine::instance()->get_compaction_status_json(json_result));
         return Status::OK();
     } else {
-        // fetch the tablet by tablet_id and schema_hash
-        TabletSharedPtr tablet =
-                StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
-    
+        // fetch the tablet by tablet_id
+        TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
         if (tablet == nullptr) {
-            LOG(WARNING) << "invalid argument.tablet_id:" << tablet_id
-                         << ", schema_hash:" << schema_hash;
+            LOG(WARNING) << "invalid argument.tablet_id:" << tablet_id;
             return Status::InternalError(
-                    strings::Substitute("fail to get $0, $1", tablet_id, schema_hash));
+                    strings::Substitute("fail to get $0", tablet_id));
         }
     
         std::string json_template = R"({
@@ -174,8 +161,7 @@ Status CompactionAction::_handle_run_status_compaction(HttpRequest* req, std::st
             "run_status" : $0,
             "msg" : "$1",
             "tablet_id" : $2,
-            "schema_hash" : $3,
-            "compact_type" : "$4"
+            "compact_type" : "$3"
 })";
     
         std::string msg = "compaction task for this tablet is not running";
@@ -190,7 +176,7 @@ Status CompactionAction::_handle_run_status_compaction(HttpRequest* req, std::st
                 compaction_type = "cumulative";
                 run_status = 1;
                 *json_result = strings::Substitute(json_template, run_status, msg, tablet_id,
-                                                   schema_hash, compaction_type);
+                                                   compaction_type);
                 return Status::OK();
             }
         }
@@ -202,14 +188,12 @@ Status CompactionAction::_handle_run_status_compaction(HttpRequest* req, std::st
                 msg = "compaction task for this tablet is running";
                 compaction_type = "base";
                 run_status = 1;
-                *json_result = strings::Substitute(json_template, run_status, msg, tablet_id,
-                                                   schema_hash, compaction_type);
+                *json_result = strings::Substitute(json_template, run_status, msg, tablet_id, compaction_type);
                 return Status::OK();
             }
         }
         // not running any compaction
-        *json_result = strings::Substitute(json_template, run_status, msg, tablet_id, schema_hash,
-                                           compaction_type);
+        *json_result = strings::Substitute(json_template, run_status, msg, tablet_id, compaction_type);
         return Status::OK();
     }
 }

be/src/http/action/compaction_action.h
@@ -64,8 +64,8 @@ private:
     /// fetch compaction running status
     Status _handle_run_status_compaction(HttpRequest* req, std::string* json_result);
 
-    /// check param and fetch tablet_id and schema_hash from req
-    Status _check_param(HttpRequest* req, uint64_t* tablet_id, uint32_t* schema_hash);
+    /// check param and fetch tablet_id from req
+    Status _check_param(HttpRequest* req, uint64_t* tablet_id);
 
     std::shared_ptr<CumulativeCompactionPolicy> _create_cumulative_compaction_policy();
 

be/src/http/action/meta_action.cpp
@@ -41,28 +41,23 @@ const static std::string HEADER_JSON = "application/json";
 Status MetaAction::_handle_header(HttpRequest* req, std::string* json_meta) {
     req->add_output_header(HttpHeaders::CONTENT_TYPE, HEADER_JSON.c_str());
     std::string req_tablet_id = req->param(TABLET_ID_KEY);
-    std::string req_schema_hash = req->param(TABLET_SCHEMA_HASH_KEY);
     std::string req_enable_base64 = req->param(ENABLE_BYTE_TO_BASE64);
     uint64_t tablet_id = 0;
-    uint32_t schema_hash = 0;
     bool enable_byte_to_base64 = false;
     if (std::strcmp(req_enable_base64.c_str(), "true") == 0) {
         enable_byte_to_base64 = true;
     }
     try {
         tablet_id = std::stoull(req_tablet_id);
-        schema_hash = std::stoul(req_schema_hash);
     } catch (const std::exception& e) {
         LOG(WARNING) << "invalid argument.tablet_id:" << req_tablet_id
-                     << ", schema_hash:" << req_schema_hash
                      << ", enable_byte_to_base64: " << req_enable_base64;
         return Status::InternalError(strings::Substitute("convert failed, $0", e.what()));
     }
 
-    TabletSharedPtr tablet =
-            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+    TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
-        LOG(WARNING) << "no tablet for tablet_id:" << tablet_id << " schema hash:" << schema_hash;
+        LOG(WARNING) << "no tablet for tablet_id:" << tablet_id;
         return Status::InternalError("no tablet exist");
     }
     TabletMetaSharedPtr tablet_meta(new TabletMeta());

be/src/http/action/restore_tablet_action.cpp
@@ -82,8 +82,7 @@ Status RestoreTabletAction::_handle(HttpRequest* req) {
     int32_t schema_hash = std::atoi(schema_hash_str.c_str());
     LOG(INFO) << "get restore tablet action request: " << tablet_id << "-" << schema_hash;
 
-    TabletSharedPtr tablet =
-            StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+    TabletSharedPtr tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet != nullptr) {
         LOG(WARNING) << "find tablet. tablet_id=" << tablet_id << " schema_hash=" << schema_hash;
         return Status::InternalError("tablet already exists, can not restore.");

be/src/http/action/tablet_migration_action.cpp
@@ -187,9 +187,9 @@ Status TabletMigrationAction::_check_param(HttpRequest* req, int64_t& tablet_id,
 Status TabletMigrationAction::_check_migrate_request(int64_t tablet_id, int32_t schema_hash,
                                                      string dest_disk, TabletSharedPtr& tablet,
                                                      DataDir** dest_store) {
-    tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id, schema_hash);
+    tablet = StorageEngine::instance()->tablet_manager()->get_tablet(tablet_id);
     if (tablet == nullptr) {
-        LOG(WARNING) << "no tablet for tablet_id:" << tablet_id << " schema hash:" << schema_hash;
+        LOG(WARNING) << "no tablet for tablet_id:" << tablet_id;
         return Status::NotFound("Tablet not found");
     }
 

commit 5be974f88a94ce27b6979e5044bbd2fc4108a078

[fix] Fix bug of wrong argument of drop_tablet function (#9031)
introduced from #8574

be/src/agent/task_worker_pool.cpp
@@ -421,7 +421,7 @@ void TaskWorkerPool::_drop_tablet_worker_thread_callback() {
                 drop_tablet_req.tablet_id, false, &err);
         if (dropped_tablet != nullptr) {
             Status drop_status = StorageEngine::instance()->tablet_manager()->drop_tablet(
-                    drop_tablet_req.tablet_id, drop_tablet_req.schema_hash);
+                    drop_tablet_req.tablet_id);
             if (!drop_status.ok()) {
                 LOG(WARNING) << "drop table failed! signature: " << agent_task_req.signature;
                 error_msgs.push_back("drop table failed!");