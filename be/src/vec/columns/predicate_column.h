e5cf6ec4bb16c56d7eb084544f4363bc9e47c6e9
BiteTheDDDDt <952130278@qq.com>
2022-04-20 15:30:31 +0800

    remove unused include

--- be/src/vec/columns/predicate_column.h
+++ be/src/vec/columns/predicate_column.h
@@ -23,7 +23,6 @@
 #include "vec/columns/column.h"
 #include "vec/columns/column_decimal.h"
 #include "vec/columns/column_impl.h"
-#include "vec/columns/column_nullable.h"
 #include "vec/columns/column_string.h"
 #include "vec/columns/column_vector.h"
 #include "vec/core/types.h"
