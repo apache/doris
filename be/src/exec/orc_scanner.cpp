92a64909bb30820e948ef045d6815ea9d7dab695
SleepyBear <kaijie3503@live.cn>
2022-04-22 21:48:32 +0800

    Update orc_scanner.cpp

--- be/src/exec/orc_scanner.cpp
+++ be/src/exec/orc_scanner.cpp
@@ -357,8 +357,12 @@ Status ORCScanner::get_next(Tuple* tuple, MemPool* tuple_pool, bool* eof, bool* 
             COUNTER_UPDATE(_rows_read_counter, 1);
             SCOPED_TIMER(_materialize_timer);
             RETURN_IF_ERROR(fill_dest_tuple(tuple, tuple_pool));
-            *fill_tuple = _success;
-            free_expr_local_allocations();
+            if (_success) {
+                free_expr_local_allocations();
+                *fill_tuple = true;
+            } else {
+                *fill_tuple = false;
+            }
             break;
         }
         if (_scanner_eof) {
