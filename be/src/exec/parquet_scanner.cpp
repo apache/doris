c3d0fee01b98ca153cf88d42be297b35ca595764
SleepyBear <kaijie3503@live.cn>
2022-04-25 10:01:42 +0800

    [fix](broker load) sync the workflow of BrokerScanner to other Scanner to avoid oom (#9173)

--- be/src/exec/parquet_scanner.cpp
+++ be/src/exec/parquet_scanner.cpp
@@ -80,8 +80,7 @@ Status ParquetScanner::get_next(Tuple* tuple, MemPool* tuple_pool, bool* eof, bo
 
         COUNTER_UPDATE(_rows_read_counter, 1);
         SCOPED_TIMER(_materialize_timer);
-        RETURN_IF_ERROR(fill_dest_tuple(tuple, tuple_pool));
-        *fill_tuple = _success;
+        RETURN_IF_ERROR(fill_dest_tuple(tuple, tuple_pool, fill_tuple));
         break; // break always
     }
     if (_scanner_eof) {
