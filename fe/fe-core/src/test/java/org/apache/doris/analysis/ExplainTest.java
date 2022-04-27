apache/doris

commit 0f86fed547a1f0c93fee8837d666ff13a14ea0d3

[improvement](insert) Support verbose keyword in insert query stmt (#9047)

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -1243,8 +1243,9 @@ public class StmtExecutor implements ProfileWriter {
         }
 
         if (insertStmt.getQueryStmt().isExplain()) {
-            insertStmt.setIsExplain(new ExplainOptions(true, false));
-            String explainString = planner.getExplainString(planner.getFragments(), new ExplainOptions(true, false));
+            ExplainOptions explainOptions = insertStmt.getQueryStmt().getExplainOptions();
+            insertStmt.setIsExplain(explainOptions);
+            String explainString = planner.getExplainString(planner.getFragments(), explainOptions);
             handleExplainStmt(explainString);
             return;
         }

fe/fe-core/src/main/cup/sql_parser.cup
@@ -3080,9 +3080,9 @@ describe_stmt ::=
         query.setIsExplain(options);
         RESULT = query;
     :}   
-    | describe_command insert_stmt:stmt
+    | describe_command opt_explain_options:options insert_stmt:stmt
     {:
-        stmt.getQueryStmt().setIsExplain(new ExplainOptions(true, false));
+        stmt.getQueryStmt().setIsExplain(options);
         RESULT = stmt;
     :}
     ;

commit 12240ee78eadfc5afaa010175e516fd4bb575634

Fix the ut problem introduced by #9047 and fixed the review problem

fe/fe-core/src/main/java/org/apache/doris/planner/OlapScanNode.java
@@ -402,6 +402,12 @@ public class OlapScanNode extends ScanNode {
         }
         // when node scan has no data, cardinality should be 0 instead of a invalid value after computeStats()
         cardinality = cardinality == -1 ? 0 : cardinality;
+
+        // update statsDeriveResult for real statistics
+        // After statistics collection is complete, remove the logic
+        if (analyzer.safeIsEnableJoinReorderBasedCost()) {
+            statsDeriveResult.setRowCount(cardinality);
+        }
     }
 
     @Override
@@ -557,19 +563,6 @@ public class OlapScanNode extends ScanNode {
             bucketSeq2locations.put(tabletId2BucketSeq.get(tabletId), scanRangeLocations);
 
             result.add(scanRangeLocations);
-        }
-
-        // FIXME(dhc): we use cardinality here to simulate ndv
-        // update statsDeriveResult for real statistics
-        // After statistics collection is complete, remove the logic
-        if (analyzer.safeIsEnableJoinReorderBasedCost()) {
-            statsDeriveResult.setRowCount(cardinality);
-            for (Map.Entry<Long, Long> entry : statsDeriveResult.getColumnToNdv().entrySet()) {
-                if (entry.getValue() > 0) {
-                    cardinality = Math.min(cardinality, entry.getValue());
-                }
-            }
-            statsDeriveResult.setRowCount(cardinality);
         }
 
         if (tablets.size() == 0) {

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -699,14 +699,6 @@ public class StmtExecutor implements ProfileWriter {
                 }
                 if (explainOptions != null) parsedStmt.setIsExplain(explainOptions);
             }
-
-            if (parsedStmt instanceof InsertStmt && parsedStmt.isExplain()) {
-                if (ConnectContext.get() != null &&
-                        ConnectContext.get().getExecutor() != null &&
-                        ConnectContext.get().getExecutor().getParsedStmt() != null) {
-                    ConnectContext.get().getExecutor().getParsedStmt().setIsExplain(new ExplainOptions(true, false));
-                }
-            }
         }
         plannerProfile.setQueryAnalysisFinishTime();
 

fe/fe-core/src/main/java/org/apache/doris/statistics/BaseStatsDerive.java
@@ -20,6 +20,7 @@ package org.apache.doris.statistics;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import org.apache.doris.analysis.Expr;
+import org.apache.doris.analysis.SlotId;
 import org.apache.doris.common.UserException;
 import org.apache.doris.planner.PlanNode;
 import org.apache.logging.log4j.LogManager;
@@ -133,22 +134,25 @@ public class BaseStatsDerive {
 
     // Currently it simply adds the number of rows of children
     protected long deriveRowCount() {
+        for (StatsDeriveResult statsDeriveResult : childrenStatsResult) {
+            rowCount = Math.max(rowCount, statsDeriveResult.getRowCount());
+        }
         applyConjunctsSelectivity();
         capRowCountAtLimit();
         return rowCount;
     }
 
 
-    protected HashMap<Long, Long> deriveColumnToDataSize() {
-        HashMap<Long, Long> columnToDataSize = new HashMap<>();
+    protected HashMap<SlotId, Float> deriveColumnToDataSize() {
+        HashMap<SlotId, Float> columnToDataSize = new HashMap<>();
         for (StatsDeriveResult child : childrenStatsResult) {
             columnToDataSize.putAll(child.getColumnToDataSize());
         }
         return columnToDataSize;
     }
 
-    protected HashMap<Long, Long> deriveColumnToNdv() {
-        HashMap<Long, Long> columnToNdv = new HashMap<>();
+    protected HashMap<SlotId, Long> deriveColumnToNdv() {
+        HashMap<SlotId, Long> columnToNdv = new HashMap<>();
         for (StatsDeriveResult child : childrenStatsResult) {
             columnToNdv.putAll(child.getColumnToNdv());
         }

fe/fe-core/src/main/java/org/apache/doris/statistics/OlapScanStatsDerive.java
@@ -19,7 +19,9 @@ package org.apache.doris.statistics;
 
 import com.google.common.base.Preconditions;
 import org.apache.doris.analysis.SlotDescriptor;
+import org.apache.doris.analysis.SlotId;
 import org.apache.doris.catalog.Catalog;
+import org.apache.doris.common.Pair;
 import org.apache.doris.common.UserException;
 import org.apache.doris.planner.OlapScanNode;
 import org.apache.doris.planner.PlanNode;
@@ -34,14 +36,15 @@ public class OlapScanStatsDerive extends BaseStatsDerive {
 
     // The rowCount here is the number of rows.
     private long inputRowCount = -1;
-    private Map<Long, Long> slotIdToDataSize;
-    private Map<Long, Long> slotIdToNdv;
+    private Map<SlotId, Float> slotIdToDataSize;
+    private Map<SlotId, Long> slotIdToNdv;
+    private Map<SlotId, Pair<Long, String>> slotIdToTableIdAndColumnName;
 
     @Override
     public void init(PlanNode node) throws UserException {
         Preconditions.checkState(node instanceof OlapScanNode);
         super.init(node);
-        buildColumnToStats((OlapScanNode)node);
+        buildStructure((OlapScanNode)node);
     }
 
     @Override
@@ -54,10 +57,17 @@ public class OlapScanStatsDerive extends BaseStatsDerive {
          * - So only an inaccurate cardinality can be calculated here.
          */
         rowCount = inputRowCount;
-        return super.deriveStats();
+        for (Map.Entry<SlotId, Pair<Long, String>> pairEntry : slotIdToTableIdAndColumnName.entrySet()) {
+            Pair<Long, Float> ndvAndDataSize = getNdvAndDataSizeFromStatistics(pairEntry.getValue());
+            long ndv = ndvAndDataSize.first;
+            float dataSize = ndvAndDataSize.second;
+            slotIdToNdv.put(pairEntry.getKey(), ndv);
+            slotIdToDataSize.put(pairEntry.getKey(), dataSize);
+        }
+        return new StatsDeriveResult(deriveRowCount(), slotIdToDataSize, slotIdToNdv);
     }
 
-    public void buildColumnToStats(OlapScanNode node) {
+    public void buildStructure(OlapScanNode node) {
         slotIdToDataSize = new HashMap<>();
         slotIdToNdv = new HashMap<>();
         if (node.getTupleDesc() != null
@@ -73,35 +83,30 @@ public class OlapScanStatsDerive extends BaseStatsDerive {
 
             long tableId = slot.getParent().getTable().getId();
             String columnName = slot.getColumn().getName();
-            /*TODO:Implement the getStatistics interface
-            //now there is nothing in statistics, need to wait for collection finished
-            long ndv = -1;
-            long dataSize = -1;
-            getNdvAndDataSizeFromStatistics(ndv, dataSize);
-
-            slotIdToNdv.put(slot.getId(), ndv);
-            slotIdToDataSize.put(slot.getId(), dataSize);
-             */
+            slotIdToTableIdAndColumnName.put(slot.getId(), new Pair<>(tableId, columnName));
         }
     }
 
-    public void getNdvAndDataSizeFromStatistics(long ndv, long dataSize) {
+    //TODO:Implement the getStatistics interface
+    //now there is nothing in statistics, need to wait for collection finished
+    public Pair<Long, Float> getNdvAndDataSizeFromStatistics(Pair<Long, String> pair) {
+        long ndv = -1;
+        float dataSize = -1;
         /*
-        ndv = -1;
-        dataSize = -1;
         if (Catalog.getCurrentCatalog()
                     .getStatisticsManager()
                     .getStatistics()
-                    .getColumnStats(tableId) != null) {
+                    .getColumnStats(pair.first) != null) {
                 ndv = Catalog.getCurrentCatalog()
                         .getStatisticsManager()
                         .getStatistics()
-                        .getColumnStats(tableId).get(columnName).getNdv();
+                        .getColumnStats(pair.first).get(pair.second).getNdv();
                 dataSize = Catalog.getCurrentCatalog()
                         .getStatisticsManager()
                         .getStatistics()
-                        .getColumnStats(tableId).get(columnName).getDataSize();
+                        .getColumnStats(pair.first).get(pair.second).getDataSize();
          }
          */
+        return new Pair<>(ndv, dataSize);
     }
 }

fe/fe-core/src/main/java/org/apache/doris/statistics/StatsDeriveResult.java
@@ -18,6 +18,7 @@
 package org.apache.doris.statistics;
 
 import com.google.common.collect.Maps;
+import org.apache.doris.analysis.SlotId;
 
 import java.util.Map;
 
@@ -26,14 +27,12 @@ public class StatsDeriveResult {
     private long rowCount = -1;
     // The data size of the corresponding column in the operator
     // The actual key is slotId
-    private final Map<Long, Long> columnToDataSize = Maps.newHashMap();
+    private final Map<SlotId, Float> columnToDataSize = Maps.newHashMap();
     // The ndv of the corresponding column in the operator
     // The actual key is slotId
-    private final Map<Long, Long> columnToNdv = Maps.newHashMap();
+    private final Map<SlotId, Long> columnToNdv = Maps.newHashMap();
 
-    public StatsDeriveResult() {}
-
-    public StatsDeriveResult(long rowCount, Map<Long, Long> columnToDataSize, Map<Long, Long> columnToNdv) {
+    public StatsDeriveResult(long rowCount, Map<SlotId, Float> columnToDataSize, Map<SlotId, Long> columnToNdv) {
         this.rowCount = rowCount;
         this.columnToDataSize.putAll(columnToDataSize);
         this.columnToNdv.putAll(columnToNdv);
@@ -42,16 +41,16 @@ public class StatsDeriveResult {
     public void setRowCount(long rowCount) {
         this.rowCount = rowCount;
     }
-    
+
     public long getRowCount() {
         return rowCount;
     }
 
-    public Map<Long, Long> getColumnToNdv() {
+    public Map<SlotId, Long> getColumnToNdv() {
         return columnToNdv;
     }
 
-    public Map<Long, Long> getColumnToDataSize() {
+    public Map<SlotId, Float> getColumnToDataSize() {
         return columnToDataSize;
     }
 }

fe/fe-core/src/test/java/org/apache/doris/analysis/ExplainTest.java
@@ -72,6 +72,20 @@ public class ExplainTest {
         Assert.assertEquals(dropDbStmt.toSql(), dropSchemaStmt.toSql());
     }
 
+    public void testExplainInsertInto() throws Exception {
+        String sql = "explain insert into test_explain.explain_t1 select * from test_explain.explain_t2";
+        String explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, false);
+        System.out.println(explainString);
+        Assert.assertFalse(explainString.contains("CAST"));
+    }
+
+    public void testExplainVerboseInsertInto() throws Exception {
+        String sql = "explain verbose insert into test_explain.explain_t1 select * from test_explain.explain_t2";
+        String explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, true);
+        System.out.println(explainString);
+        Assert.assertTrue(explainString.contains("CAST"));
+    }
+
     public void testExplainSelect() throws Exception {
         String sql = "explain select * from test_explain.explain_t1 where dt = '1001';";
         String explainString = UtFrameUtils.getSQLPlanOrErrorMsg(ctx, sql, false);

fe/fe-core/src/test/java/org/apache/doris/planner/QueryPlanTest.java
@@ -1991,6 +1991,8 @@ public class QueryPlanTest {
     public void testExplainInsertInto() throws Exception {
         ExplainTest explainTest = new ExplainTest();
         explainTest.before(connectContext);
+        explainTest.testExplainInsertInto();
+        explainTest.testExplainVerboseInsertInto();
         explainTest.testExplainSelect();
         explainTest.testExplainInsertInto();
         explainTest.testExplainVerboseSelect();
@@ -2088,7 +2090,7 @@ public class QueryPlanTest {
                 "\"storage_medium\" = \"HDD\",\n" +
                 "\"storage_format\" = \"V2\"\n" +
                 ");\n");
-        String queryStr = "EXPLAIN INSERT INTO result_exprs\n" +
+        String queryStr = "EXPLAIN VERBOSE INSERT INTO result_exprs\n" +
                 "SELECT a.aid,\n" +
                 "       b.bid\n" +
                 "FROM\n" +