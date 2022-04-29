fd1ac362f175a4ed9d419ef829b0430a0d5699c4
morningman <morningman@163.com>
2022-04-29 17:41:03 +0800

    [doc][community] add more collaborator and modify documents
    1. Add collaborator morrySnow
    2. Add document: How To Be A Committer
    3. Add guidance to install clang-format 11

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -1010,7 +1010,8 @@ module.exports = [
         initialOpenGroupIndex: -1,
         children: [
           "how-to-contribute",
-          "committer-guide",
+          "contributor-guide",
+          "how-to-be-a-committer",
           "commit-format-specification",
           "pull-request",
         ],
--- docs/.vuepress/sidebar/zh-CN.js
+++ docs/.vuepress/sidebar/zh-CN.js
@@ -1011,7 +1011,8 @@ module.exports = [
         initialOpenGroupIndex: -1,
         children: [
           "how-to-contribute",
-          "committer-guide",
+          "contributor-guide",
+          "how-to-be-a-committer",
           "commit-format-specification",
           "pull-request",
         ],
