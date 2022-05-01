99abad4ef4bcaa4041fb18f95c48fc309980410d
stalary <stalary@163.com>
2022-05-01 11:22:41 +0800

    MOD: 修改细节问题

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CreateTableAsSelectStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CreateTableAsSelectStmt.java
@@ -17,8 +17,6 @@
 
 package org.apache.doris.analysis;
 
-import lombok.Getter;
-
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
