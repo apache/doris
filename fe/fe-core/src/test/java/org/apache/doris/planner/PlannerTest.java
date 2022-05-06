28d05db7ed7a83c2ff88ff9aef761c1db1d78739
Shuo Wang <wangshuo128@gmail.com>
2022-05-06 11:11:03 +0800

    remove useless import

--- fe/fe-core/src/test/java/org/apache/doris/planner/PlannerTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/planner/PlannerTest.java
@@ -29,7 +29,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
