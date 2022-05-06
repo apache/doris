80f7bbb68e08b5a0d8db758f66266acd4d0d13dd
Chen Zhang <chzhang1987@gmail.com>
2022-05-06 19:50:25 +0800

    reformat code

--- fe/fe-core/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/Config.java
@@ -335,10 +335,10 @@ public class Config extends ConfigBase {
      * and one thread can handle the read and write of many sockets, so the number of thread pools is small.
      *
      * For most projects, only 1-2 acceptors threads are needed, and 2 to 4 selectors threads are sufficient.
-     * Workers are obstructive business logic, often have more database operations, and require a large number of threads.
-     * The specific number depends on the proportion of QPS and IO events of the application. The higher the QPS,
-     * the more threads are required, the higher the proportion of IO,
-     * the more threads waiting, and the more total threads required.
+     * Workers are obstructive business logic, often have more database operations, and require a large number of
+     * threads. The specific number depends on the proportion of QPS and IO events of the application. The higher the
+     * QPS, the more threads are required, the higher the proportion of IO, the more threads waiting, and the more
+     * total threads required.
      */
     @ConfField public static int jetty_server_acceptors = 2;
     @ConfField public static int jetty_server_selectors = 4;
