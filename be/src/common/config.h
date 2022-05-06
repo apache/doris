f968d52cf0974012dbefb14c83f84facbd69a960
yangzhg <yangzhgg@gmail.com>
2022-05-06 22:29:57 +0800

    [fix](s3) fix s3 Temp file may write failed because of has no space on disk

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -737,7 +737,6 @@ CONF_Validator(string_type_length_soft_limit_bytes,
 // used for olap scanner to save memory, when the size of unused_object_pool
 // is greater than object_pool_buffer_size, release the object in the unused_object_pool.
 CONF_Int32(object_pool_buffer_size, "100");
-
 } // namespace config
 
 } // namespace doris
