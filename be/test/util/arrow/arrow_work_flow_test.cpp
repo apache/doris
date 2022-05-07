28e15f5e271f4461ad1927598849ac533764afe1
Mingyu Chen <morningman.cmy@gmail.com>
2022-05-07 08:43:51 +0800

    [doc] Add window functions sql help doc (#9393)

--- be/test/util/arrow/arrow_work_flow_test.cpp
+++ be/test/util/arrow/arrow_work_flow_test.cpp
@@ -65,7 +65,11 @@ protected:
         EXPECT_EQ(system("rm -rf ./test_run"), 0);
 
         delete _state;
-        doris::ExecEnv::destroy(_exec_env);
+        if (_exec_env) {
+            delete _exec_env->_result_queue_mgr;
+            delete _exec_env->_thread_mgr;
+            delete _exec_env->_buffer_reservation;
+        }
     }
 
     void init();
