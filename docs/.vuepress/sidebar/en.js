243181f9bb2e664976ee2b506b27c4b87106089c
caoliang-web <71004656+caoliang-web@users.noreply.github.com>
2022-05-07 19:20:56 +0800

    [doc]Add create java udf document (#9430)
    * Add create java udf document
    
    * Add create java udf document

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -261,7 +261,8 @@ module.exports = [
         children: [
           "native-user-defined-function",
           "remote-user-defined-function",
-          "contribute-udf"
+          "contribute-udf",
+          "java-user-defined-function"
         ],
       },
     ],
