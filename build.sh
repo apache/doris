fdf73a62535dc62ad9adf8f87750e92c3fd8c98e
Adonis Ling <adonis0147@gmail.com>
2022-05-19 16:36:37 +0800

    [enhancement] Improve debugging experience. (#9677)

--- build.sh
+++ build.sh
@@ -213,6 +213,10 @@ if [[ -z ${STRIP_DEBUG_INFO} ]]; then
     STRIP_DEBUG_INFO=OFF
 fi
 
+if [[ -z ${USE_DWARF} ]]; then
+    USE_DWARF=OFF
+fi
+
 echo "Get params:
     BUILD_FE            -- $BUILD_FE
     BUILD_BE            -- $BUILD_BE
@@ -229,6 +233,7 @@ echo "Get params:
     USE_AVX2            -- $USE_AVX2
     USE_LIBCPP          -- $USE_LIBCPP
     USE_LLD             -- $USE_LLD
+    USE_DWARF           -- $USE_DWARF
     STRIP_DEBUG_INFO    -- $STRIP_DEBUG_INFO
 "
 
@@ -290,6 +295,7 @@ if [ ${BUILD_BE} -eq 1 ] ; then
             -DUSE_LLD=${USE_LLD} \
             -DBUILD_JAVA_UDF=${BUILD_JAVA_UDF} \
             -DSTRIP_DEBUG_INFO=${STRIP_DEBUG_INFO} \
+            -DUSE_DWARF=${USE_DWARF} \
             -DUSE_AVX2=${USE_AVX2} \
             -DGLIBC_COMPATIBILITY=${GLIBC_COMPATIBILITY} ../
     ${BUILD_SYSTEM} -j ${PARALLEL}
