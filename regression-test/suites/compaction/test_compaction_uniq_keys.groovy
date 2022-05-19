1d04ba5a55d8810625b384f59fabff5f2f1ea5ef
Gabriel <gabrielleebuaa@gmail.com>
2022-05-19 14:00:33 +0800

    [refactor][regressiontest] reorder license header and import statement (#9672)

--- regression-test/suites/compaction/test_compaction_uniq_keys.groovy
+++ regression-test/suites/compaction/test_compaction_uniq_keys.groovy
@@ -1,5 +1,3 @@
-import org.codehaus.groovy.runtime.IOGroovyMethods
-
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
@@ -16,6 +14,8 @@ import org.codehaus.groovy.runtime.IOGroovyMethods
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
+import org.codehaus.groovy.runtime.IOGroovyMethods
 
 suite("test_compaction_uniq_keys") {
     def tableName = "compaction_uniq_keys_regression_test"
