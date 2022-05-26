32a210f426f9422a971a1e0157a349c6f88c9dcd
Mingyu Chen <morningman.cmy@gmail.com>
2022-05-26 08:44:00 +0800

    [fix](help) fix bug of help command (#9761)
    This bug is introduced from #9306, that user need to execute
    "help stream-load" to show the help doc.
    But actually, it should be "help stream load".

--- fe/fe-core/src/main/java/org/apache/doris/common/MarkDownParser.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/MarkDownParser.java
@@ -166,7 +166,9 @@ public class MarkDownParser {
         }
         // Note that multiple line breaks at content's end will be merged to be one,
         // and other whitespace characters will be deleted.
-        return Maps.immutableEntry(key.substring(headLevel).trim(),
+        // Also, the header in md file is like "## STREAM-LOAD", we need to convert it to "STREAM LOAD",
+        // so that we can execute "help stream load" to show the help doc.
+        return Maps.immutableEntry(key.substring(headLevel).trim().replaceAll("-", " "),
                 sb.toString().replaceAll("\\s+$", "\n"));
     }
 }
