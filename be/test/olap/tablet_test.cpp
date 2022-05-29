fa6b72617c6415b57602eb7b9c49e235bf4055f1
Mingyu Chen <morningman.cmy@gmail.com>
2022-05-29 12:25:41 +0800

    [fix](ut) fix BE ut (#9831)
    introduced from #8923, the github checks has some problem that failed to check BE ut in #8923

--- be/test/olap/tablet_test.cpp
+++ be/test/olap/tablet_test.cpp
@@ -35,9 +35,9 @@ public:
     virtual ~TestTablet() {}
 
     virtual void SetUp() {
-        _tablet_meta = static_cast<TabletMetaSharedPtr>(
-                new TabletMeta(1, 2, 15673, 4, 5, TTabletSchema(), 6, {{7, 8}}, UniqueId(9, 10),
-                               TTabletType::TABLET_TYPE_DISK, TStorageMedium::HDD));
+        _tablet_meta = static_cast<TabletMetaSharedPtr>(new TabletMeta(
+                1, 2, 15673, 4, 5, TTabletSchema(), 6, {{7, 8}}, UniqueId(9, 10),
+                TTabletType::TABLET_TYPE_DISK, TStorageMedium::HDD, TCompressionType::LZ4F));
         _json_rowset_meta = R"({
             "rowset_id": 540081,
             "tablet_id": 15673,
