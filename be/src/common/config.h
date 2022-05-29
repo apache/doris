5cc789e29977543aacfc838b92fa08acfef281ab
yiguolei <676222867@qq.com>
2022-05-29 17:24:21 +0800

    Update be/src/common/config.h
    Co-authored-by: Mingyu Chen <morningman.cmy@gmail.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -748,7 +748,7 @@ CONF_Int32(parquet_reader_max_buffer_size, "50");
 
 // When the rows number reached this limit, will check the filter rate the of bloomfilter
 // if it is lower than a specific threshold, the predicate will be disabled.
-CONF_Int32(bloom_filter_predicate_check_row_num, "1000");
+CONF_mInt32(bloom_filter_predicate_check_row_num, "1000");
 
 } // namespace config
 
