bda73c9ba5cc089ad9dc9e73e0e24593b6c4d461
smallhibiscus <844981280@qq.com>
2022-05-30 09:44:12 +0800

    [improvement](script) Add jvm parameters and the process will automatically stop when oom occurs in fe. (#9765)

--- bin/start_fe.sh
+++ bin/start_fe.sh
@@ -164,10 +164,10 @@ if [ x"$HELPER" != x"" ]; then
 fi
 
 if [ ${RUN_DAEMON} -eq 1 ]; then
-    nohup $LIMIT $JAVA $final_java_opt org.apache.doris.PaloFe ${HELPER} "$@" >> $LOG_DIR/fe.out 2>&1 < /dev/null &
+    nohup $LIMIT $JAVA $final_java_opt -XX:OnOutOfMemoryError="kill -9 %p" org.apache.doris.PaloFe ${HELPER} "$@" >> $LOG_DIR/fe.out 2>&1 < /dev/null &
 else
     export DORIS_LOG_TO_STDERR=1
-    $LIMIT $JAVA $final_java_opt org.apache.doris.PaloFe ${HELPER} "$@" < /dev/null
+    $LIMIT $JAVA $final_java_opt -XX:OnOutOfMemoryError="kill -9 %p" org.apache.doris.PaloFe ${HELPER} "$@" < /dev/null
 fi
 
 echo $! > $pidfile
