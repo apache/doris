dac41fbd8bb6ab1bf6204ec026550f23b7a485c8
zy-kkk <815574403@qq.com>
2022-05-31 22:36:22 +0800

    Moving the file path and optimize the code

--- regression-test/suites/query/sql_functions/spatial_functions/test_gis_function.groovy
+++ regression-test/suites/query/sql_functions/spatial_functions/test_gis_function.groovy
@@ -17,20 +17,29 @@
 suite("test_gis_function", "query") {
     sql "set enable_vectorized_engine = true;"
     sql "set batch_size = 4096;"
+
     qt_sql "SELECT ST_AsText(ST_Point(24.7, 56.7));"
     qt_sql "SELECT ST_AsWKT(ST_Point(24.7, 56.7));"
+
     qt_sql "SELECT ST_AsText(ST_Circle(111, 64, 10000));"
+
     qt_sql "SELECT ST_Contains(ST_Polygon(\"POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))\"), ST_Point(5, 5));"
     qt_sql "SELECT ST_Contains(ST_Polygon(\"POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))\"), ST_Point(50, 50));"
+
     qt_sql "SELECT ST_DISTANCE_SPHERE(116.35620117, 39.939093, 116.4274406433, 39.9020987219);"
+
     qt_sql "SELECT ST_AsText(ST_GeometryFromText(\"LINESTRING (1 1, 2 2)\"));"
     qt_sql "SELECT ST_AsText(ST_GeomFromText(\"LINESTRING (1 1, 2 2)\"));"
+
     qt_sql "SELECT ST_AsText(ST_LineFromText(\"LINESTRING (1 1, 2 2)\"));"
     qt_sql "SELECT ST_AsText(ST_LineStringFromText(\"LINESTRING (1 1, 2 2)\"));"
+
     qt_sql "SELECT ST_AsText(ST_Point(24.7, 56.7));"
+
     qt_sql "SELECT ST_AsText(ST_Polygon(\"POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))\"));"
     qt_sql "SELECT ST_AsText(ST_PolyFromText(\"POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))\"));"
     qt_sql "SELECT ST_AsText(ST_PolygonFromText(\"POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))\"));"
+
     qt_sql "SELECT ST_X(ST_Point(24.7, 56.7));"
     qt_sql "SELECT ST_Y(ST_Point(24.7, 56.7));"
 }
