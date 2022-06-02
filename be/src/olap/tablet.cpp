ce7b71f7f96781ed1611b60df090922f694d71b0
jacktengg <tengjp@outlook.com>
2022-06-02 09:32:24 +0800

    [feature] support convert alpha rowset: add lock when finding alpha rowsets of a tablet

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -1052,6 +1052,7 @@ void Tablet::pick_candidate_rowsets_to_cumulative_compaction(
 }
 
 void Tablet::find_alpha_rowsets(std::vector<RowsetSharedPtr>* rowsets) const {
+    std::shared_lock rdlock(_meta_lock);
     for (auto& it : _rs_version_map) {
         if (it.second->rowset_meta()->rowset_type() == RowsetTypePB::ALPHA_ROWSET) {
             rowsets->push_back(it.second);
