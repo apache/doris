bc27ab1b5e0413479e6cff63a010c71798061b00
EmmyMiao87 <522274284@qq.com>
2022-06-04 22:40:47 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/analysis/DescriptorTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/DescriptorTable.java
@@ -107,6 +107,7 @@ public class DescriptorTable {
 
     /**
      * Return all tuple desc by idList.
+     * 
      * @param idList the tuple id list.
      */
     public List<TupleDescriptor> getTupleDesc(List<TupleId> idList) throws AnalysisException {
