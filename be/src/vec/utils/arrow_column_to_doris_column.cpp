bf35367a4784604b5f253b9fa9659f1a8af1fb39
Tao Yin <373141588@qq.com>
2022-06-06 21:15:40 +0800

    fix bug, retrigger p0

--- be/src/vec/utils/arrow_column_to_doris_column.cpp
+++ be/src/vec/utils/arrow_column_to_doris_column.cpp
@@ -246,7 +246,7 @@ static Status convert_offset_from_list_column(const arrow::Array* array, size_t 
     auto arrow_offsets = down_cast<arrow::Int32Array*>(arrow_offsets_array.get());
     for (int64_t i = array_idx + 1; i < array_idx + num_elements + 1; ++i) {
         // convert to doris offset, start from 0
-        offsets_data.emplace_back(arrow_offsets->Value(i) - arrow_offsets->Value(array_idx + 1));
+        offsets_data.emplace_back(arrow_offsets->Value(i) - arrow_offsets->Value(array_idx));
     }
     *start_idx_for_data = arrow_offsets->Value(array_idx);
     *num_for_data = offsets_data.back();
