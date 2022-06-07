5d3f07feb0be5d1b136242242b9da3485e799233
morningman <morningman@163.com>
2022-06-07 09:49:50 +0800

    [hotfix] fix bloomfilter disable threshold
    This issue is introduced from #9792, that config may cause some query slow

--- be/src/olap/bloom_filter_predicate.h
+++ be/src/olap/bloom_filter_predicate.h
@@ -71,9 +71,6 @@ public:
 private:
     std::shared_ptr<IBloomFilterFuncBase> _filter;
     SpecificFilter* _specific_filter; // owned by _filter
-    mutable uint64_t _evaluated_rows = 1;
-    mutable uint64_t _passed_rows = 0;
-    mutable bool _enable_pred = true;
 };
 
 // bloom filter column predicate do not support in segment v1
@@ -116,9 +113,6 @@ void BloomFilterColumnPredicate<T>::evaluate(vectorized::IColumn& column, uint16
                                                 uint16_t* size) const {
     uint16_t new_size = 0;
     using FT = typename PredicatePrimitiveTypeTraits<T>::PredicateFieldType;
-    if (!_enable_pred) {
-        return;
-    }
     if (column.is_nullable()) {
         auto* nullable_col = vectorized::check_and_get_column<vectorized::ColumnNullable>(column);
         auto& null_map_data = nullable_col->get_null_map_column().get_data();
@@ -161,16 +155,6 @@ void BloomFilterColumnPredicate<T>::evaluate(vectorized::IColumn& column, uint16
             sel[new_size] = idx;
             const auto* cell_value = reinterpret_cast<const void*>(&(pred_col_data[idx]));
             new_size += _specific_filter->find_olap_engine(cell_value);
-        }
-    }
-    // If the pass rate is very high, for example > 50%, then the bloomfilter is useless.
-    // Some bloomfilter is useless, for example ssb 4.3, it consumes a lot of cpu but it is
-    // useless.
-    _evaluated_rows += *size;
-    _passed_rows += new_size;
-    if (_evaluated_rows > config::bloom_filter_predicate_check_row_num) {
-        if (_passed_rows / (_evaluated_rows * 1.0) > 0.5) {
-            _enable_pred = false;
         }
     }
     *size = new_size;
