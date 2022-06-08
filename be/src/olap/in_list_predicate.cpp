529414d83698256c48f2e8acad99881ca9256adf
minghong.zhou <minghong.zhou@163.com>
2022-06-08 11:18:50 +0800

    use DCHECK instead of assert

--- be/src/olap/in_list_predicate.cpp
+++ be/src/olap/in_list_predicate.cpp
@@ -138,7 +138,7 @@ IN_LIST_PRED_COLUMN_BLOCK_EVALUATE(NotInListPredicate, ==)
                         uint16_t idx = sel[i];                                                   \
                         sel[new_size] = idx;                                                     \
                         const auto& cell_value = data_array[idx];                                \
-                        assert(cell_value < selected.size());                                    \
+                        DCHECK(cell_value < selected.size());                                    \
                         bool ret = !null_bitmap[idx] && (selected[cell_value] OP false);         \
                         new_size += _opposite ? !ret : ret;                                      \
                     }                                                                            \
@@ -168,7 +168,7 @@ IN_LIST_PRED_COLUMN_BLOCK_EVALUATE(NotInListPredicate, ==)
                     uint16_t idx = sel[i];                                                       \
                     sel[new_size] = idx;                                                         \
                     const auto& cell_value = data_array[idx];                                    \
-                    assert(cell_value < selected.size());                                        \
+                    DCHECK(cell_value < selected.size());                                        \
                     auto result = (selected[cell_value] OP false);                               \
                     new_size += _opposite ? !result : result;                                    \
                 }                                                                                \
