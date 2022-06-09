d0422416a514eac46286baf31f8301d82307b961
kikyo <cyborgz1999@gmail.com>
2022-06-09 20:27:14 +0800

    add some comments

--- fe/fe-core/src/main/java/org/apache/doris/nereids/PlanOperatorVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/PlanOperatorVisitor.java
@@ -27,6 +27,11 @@ import org.apache.doris.nereids.operators.plans.physical.PhysicalSort;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalPlan;
 
+/**
+ * Base class for the processing of logical and physical plan
+ * @param <R> Return type of each visit method
+ * @param <C> Context type
+ */
 @SuppressWarnings("rawtypes")
 public abstract class PlanOperatorVisitor<R, C> {
 
