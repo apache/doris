19bc14cf8d96bd7990c0ad52d9356cf7e686a441
yinzhijian <373141588@qq.com>
2022-06-09 12:11:47 +0800

    [feature-wip](array-type) Add array type support for vectorized parquet-orc scanner (#9856)
    Only support one level array now.
    for example:
    - nullable(array(nullable(tinyint))) is **support**.
    - nullable(array(nullable(array(xx))) is **not support**.

--- be/src/vec/exec/varrow_scanner.cpp
+++ be/src/vec/exec/varrow_scanner.cpp
@@ -255,7 +255,7 @@ Status VArrowScanner::_append_batch_to_src_block(Block* block) {
         auto* array = _batch->column(column_pos++).get();
         auto& column_with_type_and_name = block->get_by_name(slot_desc->col_name());
         RETURN_IF_ERROR(arrow_column_to_doris_column(array, _arrow_batch_cur_idx,
-                                                     column_with_type_and_name, num_elements,
+                                                     column_with_type_and_name.column, num_elements,
                                                      _state->timezone()));
     }
 
