80eea78e5e55db11f9f8e6a284cf28c3f6b9eb9e
zy-kkk <815574403@qq.com>
2022-06-10 10:13:08 +0800

    Add drop force to cases associated with ALTER operations

--- regression-test/suites/rollup/test_rollup_agg.groovy
+++ regression-test/suites/rollup/test_rollup_agg.groovy
@@ -74,6 +74,6 @@ suite("test_rollup_agg", "rollup") {
         contains("rollup: rollup_city")
     }
     qt_sql "SELECT citycode,SUM(pv) FROM ${tbName} GROUP BY citycode"
-    sql "ALTER TABLE ${tbName} DROP ROLLUP rollup_city"
-    sql "DROP TABLE ${tbName}"
+    sql "ALTER TABLE ${tbName} DROP ROLLUP rollup_city;"
+    sql "DROP TABLE ${tbName} FORCE;"
 }
--- regression-test/suites/schema_change/test_alter_table_column.groovy
+++ regression-test/suites/schema_change/test_alter_table_column.groovy
@@ -73,9 +73,9 @@ suite("test_alter_table_column", "schema_change") {
     sql "SHOW ALTER TABLE COLUMN;"
     sql "insert into ${tbName1} values(1,1,10,20);"
     sql "insert into ${tbName1} values(1,1,30,40);"
-    qt_sql "desc ${tbName1}"
-    qt_sql "select * from ${tbName1}"
-    sql "DROP TABLE ${tbName1}"
+    qt_sql "desc ${tbName1};"
+    qt_sql "select * from ${tbName1};"
+    sql "DROP TABLE ${tbName1} FORCE;"
 
     def tbName2 = "alter_table_column_agg"
     sql "DROP TABLE IF EXISTS ${tbName2}"
@@ -109,7 +109,7 @@ suite("test_alter_table_column", "schema_change") {
     sql "SHOW ALTER TABLE COLUMN"
     sql "insert into ${tbName2} values(1,1,10,20);"
     sql "insert into ${tbName2} values(1,1,30,40);"
-    qt_sql "desc ${tbName2}"
-    qt_sql "select * from ${tbName2}"
-    sql "DROP TABLE ${tbName2}"
+    qt_sql "desc ${tbName2};"
+    qt_sql "select * from ${tbName2};"
+    sql "DROP TABLE ${tbName2} FORCE;"
 }
