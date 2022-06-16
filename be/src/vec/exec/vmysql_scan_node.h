28e8effc525ec23f7f11623c95f4f3d12d1ca608
Gabriel <gabrielleebuaa@gmail.com>
2022-06-16 11:10:56 +0800

    [Refactor] Refactor vectorized scan node (#9968)

--- be/src/vec/exec/vmysql_scan_node.h
+++ be/src/vec/exec/vmysql_scan_node.h
@@ -19,7 +19,6 @@
 
 #include <memory>
 
-#include "exec/mysql_scan_node.h"
 #include "exec/mysql_scanner.h"
 #include "exec/scan_node.h"
 #include "runtime/descriptors.h"
@@ -32,19 +31,65 @@ class Status;
 
 namespace vectorized {
 
-class VMysqlScanNode : public MysqlScanNode {
+class VMysqlScanNode : public ScanNode {
 public:
     VMysqlScanNode(ObjectPool* pool, const TPlanNode& tnode, const DescriptorTbl& descs);
-    ~VMysqlScanNode();
+    ~VMysqlScanNode() = default;
 
-    using MysqlScanNode::get_next;
-    // Fill the next block by calling next() on the _mysql_scanner,
+    // initialize mysql_scanner, and create text_converter.
+    Status prepare(RuntimeState* state) override;
+
+    // Start MySQL scan using mysql_scanner.
+    Status open(RuntimeState* state) override;
+
+    Status get_next(RuntimeState* state, RowBatch* row_batch, bool* eos) override {
+        return Status::NotSupported("Not Implemented VMysqlScanNode Node::get_next scalar");
+    }
+    // Fill the next block by calling next() on the mysql_scanner,
     // converting text data in MySQL cells to binary data.
-    virtual Status get_next(RuntimeState* state, vectorized::Block* block, bool* eos);
+    Status get_next(RuntimeState* state, vectorized::Block* block, bool* eos) override;
+
+    // Close the mysql_scanner, and report errors.
+    Status close(RuntimeState* state) override;
+
+    // No use
+    Status set_scan_ranges(const std::vector<TScanRangeParams>& scan_ranges) override;
 
 private:
     Status write_text_column(char* value, int value_length, SlotDescriptor* slot,
                              vectorized::MutableColumnPtr* column_ptr, RuntimeState* state);
+    // Write debug string of this into out.
+    void debug_string(int indentation_level, std::stringstream* out) const override;
+    // Writes a slot in tuple from an MySQL value containing text data.
+    // The Mysql value is converted into the appropriate target type.
+    Status write_text_slot(char* value, int value_length, SlotDescriptor* slot,
+                           RuntimeState* state);
+
+    bool _is_init;
+    MysqlScannerParam _my_param;
+    // Name of Mysql table
+    std::string _table_name;
+
+    // Tuple id resolved in prepare() to set _tuple_desc;
+    TupleId _tuple_id;
+
+    // select columns
+    std::vector<std::string> _columns;
+    // where clause
+    std::vector<std::string> _filters;
+
+    // Descriptor of tuples read from MySQL table.
+    const TupleDescriptor* _tuple_desc;
+    // Tuple index in tuple row.
+    int _slot_num;
+    // Pool for allocating tuple data, including all varying-length slots.
+    std::unique_ptr<MemPool> _tuple_pool;
+    // Jni helper for scanning an HBase table.
+    std::unique_ptr<MysqlScanner> _mysql_scanner;
+    // Helper class for converting text to other types;
+    std::unique_ptr<TextConverter> _text_converter;
+    // Current tuple.
+    doris::Tuple* _tuple = nullptr;
 };
 } // namespace vectorized
 } // namespace doris
