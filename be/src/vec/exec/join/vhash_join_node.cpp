e04e1a63aae1dab9613c1ad5673193c9f04c6137
lihaopeng <lihaopeng@baidu.com>
2022-06-17 18:20:17 +0800

    [Bug][Vectorized] Fix coredump in other join conjunt is const expr

--- be/src/vec/exec/join/vhash_join_node.cpp
+++ be/src/vec/exec/join/vhash_join_node.cpp
@@ -469,7 +469,7 @@ struct ProcessHashTableProbe {
         output_block->swap(mutable_block.to_block());
 
         // dispose the other join conjunt exec
-        {
+        if (output_block->rows()) {
             int result_column_id = -1;
             int orig_columns = output_block->columns();
             (*_join_node->_vother_join_conjunct_ptr)->execute(output_block, &result_column_id);
