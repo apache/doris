78b9b631e5b2cb8e00299154d437954a648544ac
englefly <minghong.zhou@163.com>
2022-06-23 03:54:30 +0000

    fmt

--- be/src/olap/delta_writer.cpp
+++ be/src/olap/delta_writer.cpp
@@ -221,29 +221,19 @@ OLAPStatus DeltaWriter::flush_memtable_and_wait(bool need_wait) {
         // and at that time, the writer may not be initialized yet and that is a normal case.
         return OLAP_SUCCESS;
     }
-    
+
     if (_is_cancelled) {
         return OLAP_ERR_ALREADY_CANCELLED;
     }
 
-    //TODO: this change is only for 1.1.0-preview2. there are some bugs in memtracker, flush memtable directly.
+    // TODO: we don't check if the delta writer already submitted a memtable int thread pool,
+    // because there are some bugs in memtracker, flush memtable directly.
+    // This change is only for 1.1.0-preview2.
     VLOG_NOTICE << "flush memtable to reduce mem consumption. memtable size: "
-            << _mem_table->memory_usage() << ", tablet: " << _req.tablet_id
-            << ", load id: " << print_id(_req.load_id);
+                << _mem_table->memory_usage() << ", tablet: " << _req.tablet_id
+                << ", load id: " << print_id(_req.load_id);
     RETURN_NOT_OK(_flush_memtable_async());
     _reset_mem_table();
-
-    // if (mem_consumption() == _mem_table->memory_usage()) {
-    //     // equal means there is no memtable in flush queue, just flush this memtable
-    //     VLOG_NOTICE << "flush memtable to reduce mem consumption. memtable size: "
-    //             << _mem_table->memory_usage() << ", tablet: " << _req.tablet_id
-    //             << ", load id: " << print_id(_req.load_id);
-    //     RETURN_NOT_OK(_flush_memtable_async());
-    //     _reset_mem_table();
-    // } else {
-    //     DCHECK(mem_consumption() > _mem_table->memory_usage());
-    //     // this means there should be at least one memtable in flush queue.
-    // }
 
     if (need_wait) {
         // wait all memtables in flush queue to be flushed.
