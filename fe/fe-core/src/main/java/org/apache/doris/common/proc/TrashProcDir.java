b3f2dd25bb3a35334bdf90d544ffc3b63f986a3c
caiconghui <55968745+caiconghui@users.noreply.github.com>
2022-06-24 08:56:32 +0800

    [fix](proc) Fix bug that TrashProcDir cannot show backend trash detail normally (#10365)

--- fe/fe-core/src/main/java/org/apache/doris/common/proc/TrashProcDir.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/proc/TrashProcDir.java
@@ -26,7 +26,6 @@ import org.apache.doris.system.Backend;
 import org.apache.doris.thrift.BackendService;
 import org.apache.doris.thrift.TNetworkAddress;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
@@ -73,7 +72,6 @@ public class TrashProcDir implements ProcDirInterface {
     }
 
     public static void getTrashInfo(List<Backend> backends, List<List<String>> infos) {
-
         for (Backend backend : backends) {
             BackendService.Client client = null;
             TNetworkAddress address = null;
@@ -114,23 +112,17 @@ public class TrashProcDir implements ProcDirInterface {
     }
 
     @Override
-    public ProcNodeInterface lookup(String backendHostAndPort) throws AnalysisException {
-        if (Strings.isNullOrEmpty(backendHostAndPort)) {
-            throw new AnalysisException("BackendHost:HeartBeatPort is null");
-        }
-        String backendHost;
-        int backendHeartBeatPort;
+    public ProcNodeInterface lookup(String backendIdStr) throws AnalysisException {
+        long backendId = -1;
         try {
-            backendHost = backendHostAndPort.split(":")[0];
-            backendHeartBeatPort = Integer.parseInt(backendHostAndPort.split(":")[1]);
+            backendId = Long.parseLong(backendIdStr);
         } catch (NumberFormatException e) {
-            throw new AnalysisException("Invalid backend format: " + backendHostAndPort);
+            throw new AnalysisException("Invalid backend id format: " + backendIdStr);
         }
-        Backend backend = Catalog.getCurrentSystemInfo().getBackendWithHeartbeatPort(backendHost, backendHeartBeatPort);
+        Backend backend = Catalog.getCurrentSystemInfo().getBackend(backendId);
         if (backend == null) {
-            throw new AnalysisException("Backend[" + backendHostAndPort + "] does not exist.");
+            throw new AnalysisException("Backend[" + backendId + "] does not exist.");
         }
-
         return new TrashProcNode(backend);
     }
 }
