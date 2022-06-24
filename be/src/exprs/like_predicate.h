7246fb9b257ae053e41391601a818ff097318efe
Mingyu Chen <morningman.cmy@gmail.com>
2022-06-24 14:38:52 +0800

    Revert "[improvement](function) optimize substr performance (#10169)" (#10390)
    This reverts commit 2335d233f1f52eb64a380b4c9959becdf182b71b.

--- be/src/exprs/like_predicate.h
+++ be/src/exprs/like_predicate.h
@@ -73,7 +73,7 @@ private:
         void set_search_string(const std::string& search_string_arg) {
             search_string = search_string_arg;
             search_string_sv = StringValue(search_string);
-            substring_pattern.set_pattern(&search_string_sv);
+            substring_pattern = StringSearch(&search_string_sv);
         }
     };
 
