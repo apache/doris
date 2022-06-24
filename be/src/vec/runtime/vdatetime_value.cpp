ba425435cd130c9d14eea2a83a6b52f21f34a60d
liuzhuang2017 <justdodt@outlook.com>
2022-06-24 16:46:30 +0800

    [fix][be] Delete uncivilized comments.

--- be/src/vec/runtime/vdatetime_value.cpp
+++ be/src/vec/runtime/vdatetime_value.cpp
@@ -97,7 +97,7 @@ bool VecDateTimeValue::from_date_str(const char* date_str, int len) {
     int digits = pos - ptr;
     bool is_interval_format = false;
 
-    // Compatible with MySQL. Shit!!!
+    // Compatible with MySQL.
     // For YYYYMMDD/YYYYMMDDHHMMSS is 4 digits years
     if (pos == end || *pos == '.') {
         if (digits == 4 || digits == 8 || digits >= 14) {
