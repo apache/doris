9bb791d12a16f1f034743f6491757f7d5f61183a
starocean999 <12095047@qq.com>
2022-06-26 00:12:59 +0800

    update fe ut

--- fe/fe-core/src/test/java/org/apache/doris/analysis/QueryStmtTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/analysis/QueryStmtTest.java
@@ -21,9 +21,7 @@ import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.UserException;
-import org.apache.doris.common.VecNotImplException;
 import org.apache.doris.qe.ConnectContext;
-import org.apache.doris.qe.SessionVariable;
 import org.apache.doris.rewrite.FoldConstantsRule;
 import org.apache.doris.thrift.TExpr;
 import org.apache.doris.utframe.DorisAssert;
@@ -231,28 +229,13 @@ public class QueryStmtTest {
                 + "                FROM aa\n" + "                GROUP BY mon)\n" + "SELECT bb.mon,\n"
                 + "        round(cc.num / bb.total, 4) rate\n" + "FROM bb\n" + "LEFT JOIN cc ON cc.mon = bb.mon\n"
                 + "ORDER BY mon;";
-
-        // When disable vec engine, this sql can be analyzed successfully.
-        // But when enable vec engine and for now, it will throw VecNotImplException
-        // with msg: "could not be changed to nullable".
-        // So here we make a "if else" check, and once this VecNotImplException is fixed, we should remove this check.
-        SessionVariable sv = new SessionVariable();
-        if (!sv.enableVectorizedEngine) {
-            stmt = (QueryStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, ctx);
-            exprsMap.clear();
-            stmt.collectExprs(exprsMap);
-            Assert.assertEquals(18, exprsMap.size());
-            constMap.clear();
-            constMap = getConstantExprMap(exprsMap, analyzer);
-            Assert.assertEquals(4, constMap.size());
-        } else {
-            try {
-                UtFrameUtils.parseAndAnalyzeStmt(sql, ctx);
-                Assert.fail();
-            } catch (VecNotImplException e) {
-                Assert.assertTrue(e.getMessage().contains("could not be changed to nullable"));
-            }
-        }
+        stmt = (QueryStmt) UtFrameUtils.parseAndAnalyzeStmt(sql, ctx);
+        exprsMap.clear();
+        stmt.collectExprs(exprsMap);
+        Assert.assertEquals(18, exprsMap.size());
+        constMap.clear();
+        constMap = getConstantExprMap(exprsMap, analyzer);
+        Assert.assertEquals(4, constMap.size());
     }
 
     @Test
