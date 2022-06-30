7571263ed767a017eb115a22993138667c5fa5b4
Pxl <952130278@qq.com>
2022-07-01 07:37:44 +0800

    [Bug][Vectorized] fix timestamp(datetime) not regist right (#10525)

--- be/src/vec/functions/date_time_transforms.h
+++ be/src/vec/functions/date_time_transforms.h
@@ -137,7 +137,6 @@ struct TimeStampImpl {
 template <typename DateValueType, typename ArgType>
 struct DayNameImpl {
     using ARG_TYPE = ArgType;
-    using DATE_TYPE = DateValueType;
     static constexpr auto name = "dayname";
     static constexpr auto max_size = MAX_DAY_NAME_LEN;
 
@@ -158,7 +157,7 @@ struct DayNameImpl {
     }
 
     static DataTypes get_variadic_argument_types() {
-        if constexpr (std::is_same_v<DATE_TYPE, VecDateTimeValue>) {
+        if constexpr (std::is_same_v<DateValueType, VecDateTimeValue>) {
             return {std::make_shared<DataTypeDateTime>()};
         } else {
             return {std::make_shared<DataTypeDateV2>()};
@@ -169,12 +168,11 @@ struct DayNameImpl {
 template <typename DateValueType, typename ArgType>
 struct MonthNameImpl {
     using ARG_TYPE = ArgType;
-    using DATE_TYPE = DateValueType;
     static constexpr auto name = "monthname";
     static constexpr auto max_size = MAX_MONTH_NAME_LEN;
 
-    static inline auto execute(const DATE_TYPE& dt, ColumnString::Chars& res_data, size_t& offset,
-                               bool& is_null) {
+    static inline auto execute(const DateValueType& dt, ColumnString::Chars& res_data,
+                               size_t& offset, bool& is_null) {
         const auto* month_name = dt.month_name();
         is_null = !dt.is_valid_date();
         if (month_name == nullptr || is_null) {
@@ -190,7 +188,7 @@ struct MonthNameImpl {
     }
 
     static DataTypes get_variadic_argument_types() {
-        if constexpr (std::is_same_v<DATE_TYPE, VecDateTimeValue>) {
+        if constexpr (std::is_same_v<DateValueType, VecDateTimeValue>) {
             return {std::make_shared<DataTypeDateTime>()};
         } else {
             return {std::make_shared<DataTypeDateV2>()};
