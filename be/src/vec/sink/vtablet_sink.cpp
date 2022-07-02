630aa02c31bd0df0115dd7b8fee3b5c51ea4eea6
englefly <minghong.zhou@163.com>
2022-07-02 10:46:41 +0800

    add config table_sink_pending_bytes_limitation

--- be/src/vec/sink/vtablet_sink.cpp
+++ be/src/vec/sink/vtablet_sink.cpp
@@ -117,16 +117,18 @@ Status VOlapTableSink::send(RuntimeState* state, vectorized::Block* input_block)
         _partition_to_tablet_map.clear();
     }
     
-    //if pending bytes is more than 500M, wait at most 1 min
-    constexpr size_t MAX_PENDING_BYTES = 500 * 1024 * 1024;
+    //if pending bytes is more than table_sink_pending_bytes_limitation, wait at most 1 min
+    size_t MAX_PENDING_BYTES = config::table_sink_pending_bytes_limitation;
     constexpr int max_retry = 120;
     int retry = 0;
     while (get_pending_bytes() > MAX_PENDING_BYTES && retry++ < max_retry) {
         std::this_thread::sleep_for(std::chrono::microseconds(500));
     }
-    if (get_pending_bytes() > MAX_PENDING_BYTES){
+    if (get_pending_bytes() > MAX_PENDING_BYTES) {
         std::stringstream str;
-        str << "Load task " << _load_id << ": Memory exceed limit. ";
+        str << "Load task " << _load_id
+            << ": pending bytes exceed limit (config::table_sink_pending_bytes_limitation):"
+            << MAX_PENDING_BYTES;
         return Status::MemoryLimitExceeded(str.str());
     }
 
