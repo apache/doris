d77af7bc8d3ac7929e9ad2a8e80ef0a1225f3c29
yiguolei <yiguolei@gmail.com>
2022-07-04 14:55:59 +0800

    v21

--- be/test/olap/in_list_predicate_test.cpp
+++ be/test/olap/in_list_predicate_test.cpp
@@ -536,6 +536,14 @@ TEST_F(TestInListPredicate, DATE_COLUMN) {
     values.insert(value3);
     ColumnPredicate* pred = new InListPredicate<uint24_t>(0, std::move(values));
 
+    std::vector<std::string> date_array;
+    date_array.push_back("2017-09-07");
+    date_array.push_back("2017-09-08");
+    date_array.push_back("2017-09-09");
+    date_array.push_back("2017-09-10");
+    date_array.push_back("2017-09-11");
+    date_array.push_back("2017-09-12");
+
     // for ColumnBlock no nulls
     init_row_block(&tablet_schema, size);
     ColumnBlock col_block = _row_block->column_block(0);
