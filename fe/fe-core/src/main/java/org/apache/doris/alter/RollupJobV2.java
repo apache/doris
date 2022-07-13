1d3575bc572d868016fe8534cbde8c104fde4042
BiteTheDDDDt <952130278@qq.com>
2022-07-13 17:15:39 +0800

    fix fe crash because npe on rollupBatchTask

--- fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
@@ -542,7 +542,11 @@ public class RollupJobV2 extends AlterJobV2 implements GsonPostProcessable {
 
     private void cancelInternal() {
         // clear tasks if has
-        AgentTaskQueue.removeBatchTask(rollupBatchTask, TTaskType.ALTER);
+        if (rollupBatchTask != null) {
+            AgentTaskQueue.removeBatchTask(rollupBatchTask, TTaskType.ALTER);
+        } else {
+            LOG.warn("rollupBatchTask is null");
+        }
         // remove all rollup indexes, and set state to NORMAL
         TabletInvertedIndex invertedIndex = Catalog.getCurrentInvertedIndex();
         Database db = Catalog.getCurrentInternalCatalog().getDbNullable(dbId);
