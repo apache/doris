1ab06ea36bbbd3610239dbe61e47e924f54eb306
morningman <morningman@apache.org>
2022-07-14 22:58:08 +0800

    [improvement](arrow) Avoid parse timezone for each datetime value
    Convert arrow batch to doris block is too slow when there are datetime values.
    Because we call `TimezoneUtils::find_cctz_time_zone` for each values.
    
    After modify, the tpch-100 q1 with external table cost from 40s -> 9s

--- be/src/vec/utils/arrow_column_to_doris_column.h
+++ be/src/vec/utils/arrow_column_to_doris_column.h
@@ -35,6 +35,6 @@ const PrimitiveType arrow_type_to_primitive_type(::arrow::Type::type type);
 
 Status arrow_column_to_doris_column(const arrow::Array* arrow_column, size_t arrow_batch_cur_idx,
                                     ColumnPtr& doris_column, const DataTypePtr& type,
-                                    size_t num_elements, const std::string& timezone);
+                                    size_t num_elements, const cctz::time_zone& ctz);
 
 } // namespace doris::vectorized
