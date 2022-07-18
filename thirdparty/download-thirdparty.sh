2303f475f85441a8a5f25e6960437dd1256caf21
Adonis Ling <adonis0147@gmail.com>
2022-07-18 10:50:30 +0800

    [enhancement](thirdparty) Support building thirdparty on macOS (#10677)

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -349,4 +349,12 @@ fi
 cd -
 echo "Finished patching $AWS_SDK_SOURCE"
 
-
+cd "${TP_SOURCE_DIR}/${BRPC_SOURCE}"
+if [[ ! -f $PATCHED_MARK ]]; then
+    if [[ "$(uname -s)" == 'Darwin' ]]; then
+        patch -p1 <"${TP_PATCH_DIR}/brpc-1.1.0.patch"
+        touch ${PATCHED_MARK}
+    fi
+fi
+cd -
+echo "Finished patching ${BRPC_SOURCE}"
