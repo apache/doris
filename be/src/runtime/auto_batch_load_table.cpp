212382079ba84a3197a4d606a68ddbae8a753167
meiyi <myimeiyi@gmail.com>
2022-07-19 19:36:47 +0800

    Be implements recovery wal

--- be/src/runtime/auto_batch_load_table.cpp
+++ be/src/runtime/auto_batch_load_table.cpp
@@ -136,7 +136,78 @@ Status AutoBatchLoadTable::commit(int64_t& wal_id, std::string& wal_path) {
 }
 
 Status AutoBatchLoadTable::recovery_wal(const int64_t& wal_id, const std::string& wal_path) {
-    return Status::OK();
+    Status st;
+    // 0. if wal length is 0, delete wal and return
+    auto wal_reader = WalReader(wal_path);
+    RETURN_IF_ERROR(wal_reader.init());
+    if (wal_reader.file_length() == 0) {
+        LOG(INFO) << "Wal: " << wal_path << " length is 0";
+        FileUtils::remove(wal_path);
+        return Status::OK();
+    }
+    // 1. begin txn
+    std::string label;
+    TUniqueId fragment_instance_id;
+    int64_t txn_id;
+    // check if this label is used and abort the previous txn
+    st = _begin_auto_batch_load(wal_id, label, fragment_instance_id, txn_id);
+    if (!st.ok()) {
+        if (st.code() == TStatusCode::LABEL_ALREADY_EXISTS) {
+            // if the previous txn is commit or visible, then skip recovery this wal
+            st = _wait_txn_success(label, -1);
+            if (st.ok()) {
+                VLOG(1) << "wal related txn is already committed, wal_path=" << wal_path;
+                FileUtils::remove(wal_path);
+                return Status::OK();
+            } else {
+                // abort the previous txn and begin the new txn
+                std::string reason = "recovery wal";
+                RETURN_IF_ERROR(_abort_txn(label, reason));
+                RETURN_IF_ERROR(
+                        _begin_auto_batch_load(wal_id, label, fragment_instance_id, txn_id));
+            }
+        } else if (st.code() == TStatusCode::TABLE_NOT_FOUND) {
+            // If table does not exist, it means table is deleted, remove the wal file directly.
+            FileUtils::remove(wal_path);
+            return Status::OK();
+        } else {
+            LOG(ERROR) << "begin auto batch load error when recovery wal: " << st.to_string();
+            return st;
+        }
+    }
+    auto pipe = _exec_env->fragment_mgr()->get_pipe(fragment_instance_id);
+    if (pipe == nullptr) {
+        return Status::InternalError("pip is null");
+    }
+    // 2. read rows from wal_reader
+    while (true) {
+        doris::PDataRow prow;
+        st = wal_reader.read_row(prow);
+        if (LIKELY(st.ok())) {
+            PDataRow* row = new PDataRow();
+            row->CopyFrom(prow);
+            // 3. write row to stream load pip
+            RETURN_IF_ERROR(pipe->append_and_flush(reinterpret_cast<char*>(&row), sizeof(row),
+                                                   sizeof(row) + row->ByteSizeLong()));
+        } else if (st.is_end_of_file()) {
+            break;
+        } else {
+            LOG(WARNING) << "read wal file: " << wal_path << ", error, " << st.to_string();
+            return st;
+        }
+    }
+    // 4. finish pip and commit txn
+    st = pipe->finish();
+    if (UNLIKELY(!st.ok())) {
+        LOG(WARNING) << "finish pip failed, " << st.to_string();
+    }
+    // 5. wait for tnx is commit or visible
+    st = _wait_txn_success(label, txn_id);
+    // 6. delete wal if success
+    if (st.ok()) {
+        st = FileUtils::remove(wal_path);
+    }
+    return st;
 }
 
 int64_t AutoBatchLoadTable::_next_wal_id() {
