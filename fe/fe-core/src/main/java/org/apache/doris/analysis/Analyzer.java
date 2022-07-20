2d2f341e9b3af5724d0f6f596dbba1d12cd03833
Kikyou1997 <33112463+Kikyou1997@users.noreply.github.com>
2022-07-20 12:28:29 +0800

    [Ehancement](planner) Rewrite implicit cast to the predicates (#10920)
    During the analysis of BinaryPredicate, it will generate a CastExpr if the slot implicitly in the below case:
    SELECT * FROM t1 WHERE t1.col1 = '1';
    col1 is integer column.
    
    This will prevent the binary predicate from pushing down to OlapScan which would impact the performance.

--- fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
@@ -57,6 +57,7 @@ import org.apache.doris.rewrite.RewriteBinaryPredicatesRule;
 import org.apache.doris.rewrite.RewriteDateLiteralRule;
 import org.apache.doris.rewrite.RewriteEncryptKeyRule;
 import org.apache.doris.rewrite.RewriteFromUnixTimeRule;
+import org.apache.doris.rewrite.RewriteImplicitCastRule;
 import org.apache.doris.rewrite.RewriteInPredicateRule;
 import org.apache.doris.rewrite.mvrewrite.CountDistinctToBitmap;
 import org.apache.doris.rewrite.mvrewrite.CountDistinctToBitmapOrHLLRule;
@@ -354,6 +355,7 @@ public class Analyzer {
             rules.add(NormalizeBinaryPredicatesRule.INSTANCE);
             // Put it after NormalizeBinaryPredicatesRule, make sure slotRef is on the left and Literal is on the right.
             rules.add(RewriteBinaryPredicatesRule.INSTANCE);
+            rules.add(RewriteImplicitCastRule.INSTANCE);
             rules.add(FoldConstantsRule.INSTANCE);
             rules.add(RewriteFromUnixTimeRule.INSTANCE);
             rules.add(CompoundPredicateWriteRule.INSTANCE);
