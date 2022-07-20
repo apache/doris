94da89d5598022181f826469ba1c54c07c580f0c
Gabriel <gabrielleebuaa@gmail.com>
2022-07-20 18:01:32 +0800

    update

--- be/src/vec/functions/function_timestamp.cpp
+++ be/src/vec/functions/function_timestamp.cpp
@@ -206,8 +206,11 @@ struct UnixTimeStampImpl {
                                const ColumnNumbers& arguments, size_t result,
                                size_t input_rows_count) {
         auto col_result = ColumnVector<Int32>::create();
+        col_result->resize(input_rows_count);
+        auto& col_result_data = col_result->get_data();
+        auto res_value = context->impl()->state()->timestamp_ms() / 1000;
         for (int i = 0; i < input_rows_count; i++) {
-            col_result->insert(context->impl()->state()->timestamp_ms() / 1000);
+            col_result_data[i] = res_value;
         }
         block.replace_by_position(result, std::move(col_result));
         return Status::OK();
