a4b0c52cf4d17bbd5d7875c7c9b9d1d0db01856c
stephen <hello_stephen@qq.com>
2022-07-20 12:21:37 +0800

    opt

--- tools/clickbench-tools/load-clickbench-data.sh
+++ tools/clickbench-tools/load-clickbench-data.sh
@@ -93,45 +93,43 @@ echo "PASSWORD: $PASSWORD"
 echo "DB: $DB"
 
 function check_doirs_conf() {
-    echo "check if Doris FE's and BE's conf"
-    exit_flag=false
-
     cv=$(mysql -h$FE_HOST -P$FE_QUERY_PORT -u$USER -e 'admin show frontend config' | grep 'stream_load_default_timeout_second' | awk '{print $2}')
     if (($cv < 3600)); then
-        echo "please revise your Doris FE's conf to set 'stream_load_default_timeout_second=3600' or above"
-        exit_flag=true
+        echo "advise: revise your Doris FE's conf to set 'stream_load_default_timeout_second=3600' or above"
     fi
 
     cv=$(curl "${BE_HOST}:${BE_WEBSERVER_PORT}/varz" 2>/dev/null | grep 'streaming_load_max_mb' | awk -F'=' '{print $2}')
     if (($cv < 16000)); then
-        echo -e "please revise your Doris BE's conf to set 'streaming_load_max_mb=16000' or above \noptional: 'flush_thread_num_per_store=5' to speed up load."
-        exit_flag=true
+        echo -e "advise: revise your Doris BE's conf to set 'streaming_load_max_mb=16000' or above and 'flush_thread_num_per_store=5' to speed up load."
     fi
-
-    if ${exit_flag}; then exit 1; fi
 }
 
 function load() {
     echo "(1/2) prepare clickbench data file"
-    if [ ! -f $DATA_DIR/hits.tsv.gz ]; then
-        echo "can not find data file in $DATA_DIR, will download it"
-        if [ ! -d $DATA_DIR ]; then mkdir -p $DATA_DIR; fi
-        cd $DATA_DIR
-        wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
-        # wget --continue 'https://doris-build-1308700295.cos.ap-beijing.myqcloud.com/ClickBench/hits.tsv.gz'
-        cd -
-    fi
+    need_download=false
+    cd $DATA_DIR
+    for i in $(seq 0 9); do
+        if [ ! -f "$DATA_DIR/hits_split${i}" ]; then
+            echo "will download hits_split${i} to $DATA_DIR"
+            wget --continue "https://doris-test-data.oss-cn-hongkong.aliyuncs.com/ClickBench/hits_split${i}" &
+            # wget --continue "https://doris-test-data.oss-cn-hongkong-internal.aliyuncs.com/ClickBench/hits_split${i}" &
+        fi
+    done
 
-    echo "(2/2) load clickbench data file $DATA_DIR/hits.tsv.gz into Doris"
-    curl --location-trusted \
-        -u $USER:$PASSWORD \
-        -T "$DATA_DIR/hits.tsv.gz" \
-        -H "label:hits" \
-        -H "compress_type:GZ" \
-        -H "column_separator:\t" \
-        -H "columns:WatchID,JavaEnable,Title,GoodEvent,EventTime,EventDate,CounterID,ClientIP,RegionID,UserID,CounterClass,OS,UserAgent,URL,Referer,IsRefresh,RefererCategoryID,RefererRegionID,URLCategoryID,URLRegionID,ResolutionWidth,ResolutionHeight,ResolutionDepth,FlashMajor,FlashMinor,FlashMinor2,NetMajor,NetMinor,UserAgentMajor,UserAgentMinor,CookieEnable,JavascriptEnable,IsMobile,MobilePhone,MobilePhoneModel,Params,IPNetworkID,TraficSourceID,SearchEngineID,SearchPhrase,AdvEngineID,IsArtifical,WindowClientWidth,WindowClientHeight,ClientTimeZone,ClientEventTime,SilverlightVersion1,SilverlightVersion2,SilverlightVersion3,SilverlightVersion4,PageCharset,CodeVersion,IsLink,IsDownload,IsNotBounce,FUniqID,OriginalURL,HID,IsOldCounter,IsEvent,IsParameter,DontCountHits,WithHash,HitColor,LocalEventTime,Age,Sex,Income,Interests,Robotness,RemoteIP,WindowName,OpenerName,HistoryLength,BrowserLanguage,BrowserCountry,SocialNetwork,SocialAction,HTTPError,SendTiming,DNSTiming,ConnectTiming,ResponseStartTiming,ResponseEndTiming,FetchTiming,SocialSourceNetworkID,SocialSourcePage,ParamPrice,ParamOrderID,ParamCurrency,ParamCurrencyID,OpenstatServiceName,OpenstatCampaignID,OpenstatAdID,OpenstatSourceID,UTMSource,UTMMedium,UTMCampaign,UTMContent,UTMTerm,FromTag,HasGCLID,RefererHash,URLHash,CLID" \
-        http://$FE_HOST:$FE_HTTP_PORT/api/$DB/hits/_stream_load
+    echo "wait for download task done..."
+    wait
+    cd -
 
+    echo "(2/2) load clickbench data file $DATA_DIR/hits_split[0-9] into Doris"
+    for i in $(seq 0 9); do
+        echo -e "
+        start loading hits_split${i}"
+        curl --location-trusted \
+            -u $USER:$PASSWORD \
+            -T "$DATA_DIR/hits_split${i}" \
+            -H "columns:WatchID,JavaEnable,Title,GoodEvent,EventTime,EventDate,CounterID,ClientIP,RegionID,UserID,CounterClass,OS,UserAgent,URL,Referer,IsRefresh,RefererCategoryID,RefererRegionID,URLCategoryID,URLRegionID,ResolutionWidth,ResolutionHeight,ResolutionDepth,FlashMajor,FlashMinor,FlashMinor2,NetMajor,NetMinor,UserAgentMajor,UserAgentMinor,CookieEnable,JavascriptEnable,IsMobile,MobilePhone,MobilePhoneModel,Params,IPNetworkID,TraficSourceID,SearchEngineID,SearchPhrase,AdvEngineID,IsArtifical,WindowClientWidth,WindowClientHeight,ClientTimeZone,ClientEventTime,SilverlightVersion1,SilverlightVersion2,SilverlightVersion3,SilverlightVersion4,PageCharset,CodeVersion,IsLink,IsDownload,IsNotBounce,FUniqID,OriginalURL,HID,IsOldCounter,IsEvent,IsParameter,DontCountHits,WithHash,HitColor,LocalEventTime,Age,Sex,Income,Interests,Robotness,RemoteIP,WindowName,OpenerName,HistoryLength,BrowserLanguage,BrowserCountry,SocialNetwork,SocialAction,HTTPError,SendTiming,DNSTiming,ConnectTiming,ResponseStartTiming,ResponseEndTiming,FetchTiming,SocialSourceNetworkID,SocialSourcePage,ParamPrice,ParamOrderID,ParamCurrency,ParamCurrencyID,OpenstatServiceName,OpenstatCampaignID,OpenstatAdID,OpenstatSourceID,UTMSource,UTMMedium,UTMCampaign,UTMContent,UTMTerm,FromTag,HasGCLID,RefererHash,URLHash,CLID" \
+            http://$FE_HOST:$FE_HTTP_PORT/api/$DB/hits/_stream_load
+    done
 }
 
 echo "start..."
