bb0514f79a2695de64f3b7601eb5aadae09300c4
englefly <minghong.zhou@163.com>
2022-07-21 10:41:39 +0800

    style fix

--- fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Group.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Group.java
@@ -30,7 +30,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
