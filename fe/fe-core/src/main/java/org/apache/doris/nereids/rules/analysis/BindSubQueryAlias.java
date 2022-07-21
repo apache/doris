eb784e024d47205c05a61d3b3ba1f2ee9b8200cd
sohardforaname <organic_chemistry@foxmail.com>
2022-07-21 18:56:07 +0800

    [FE] fixing analyze bug
    [FE] fixing analyze bug

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSubQueryAlias.java
@@ -0,0 +1,24 @@
+package org.apache.doris.nereids.rules.analysis;
+
+import org.apache.doris.nereids.rules.Rule;
+import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.trees.expressions.Slot;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalSubQueryAlias;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+public class BindSubQueryAlias implements AnalysisRuleFactory {
+    @Override
+    public List<Rule> buildRules() {
+        return ImmutableList.of(
+                RuleType.BINDING_ALIAS_SLOT.build(
+                        logicalSubQueryAlias().then(alias -> {
+                            return new LogicalSubQueryAlias<>(alias.getAlias(), alias.child());
+                        })
+                )
+        );
+    }
+}
