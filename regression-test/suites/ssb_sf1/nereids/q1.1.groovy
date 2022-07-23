7ac1f7dd3a9b31e967ed5323812b9d9b7265862e
924060929 <924060929@qq.com>
2022-07-23 12:41:47 +0800

    (fix)(Nereids) fix ssb and add regression test case (#11095)
    current nereids planner execute ssb will run into dead loop and crash be, this pr fix this problem and add some regression test case prevent execute ssb failed

---
+++ regression-test/suites/ssb_sf1/nereids/q1.1.groovy
@@ -0,0 +1,36 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("ssb_sf1_q1_1_nereids") {
+    String realDb = context.config.getDbByLastGroup(context.group)
+    // get parent directory's group
+    realDb = realDb.substring(0, realDb.lastIndexOf("_"))
+
+    sql "use ${realDb}"
+
+    sql 'set enable_nereids_planner=true'
+    // nereids need vectorized
+    sql 'set enable_vectorized_engine=true'
+
+    sql 'set exec_mem_limit=2147483648*2'
+
+    test {
+        sql(new File(context.file.parentFile, "../sql/q1.1.sql").text)
+
+        resultFile(file = "../sql/q1.1.out", tag = "q1.1")
+    }
+}
