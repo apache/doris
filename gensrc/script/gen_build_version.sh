2dbd70bf92ed8944d60f38cb43ef556feb73b41d
yiguolei <676222867@qq.com>
2022-07-25 11:04:03 +0800

    1.1.1 rc03 (#11161)
    * [fix](fe_plugins) update fe plugins' dependency to fe 1.0-SNAPSHOT (#11141)
    
    * 1.1.1 rc03
    
    Co-authored-by: Mingyu Chen <morningman.cmy@gmail.com>
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="1.1.1-rc02"
+build_version="1.1.1-rc03"
 
 unset LANG
 unset LC_CTYPE
