211ea397f04bee4f864ca339c9860348d293070c
sohardforaname <organic_chemistry@foxmail.com>
2022-07-25 11:38:43 +0800

    [FE] add eliminate alias node rule and job

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/EliminateAliasNode.java
@@ -0,0 +1,12 @@
+package org.apache.doris.nereids.rules.analysis;
+
+import org.apache.doris.nereids.rules.Rule;
+
+import java.util.List;
+
+public class EliminateAliasNode implements AnalysisRuleFactory {
+    @Override
+    public List<Rule> buildRules() {
+        return null;
+    }
+}
