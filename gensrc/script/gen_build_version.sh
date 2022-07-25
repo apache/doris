a89490562b2256a0e36609fab28bfce0b6053c7d
yiguolei <676222867@qq.com>
2022-07-25 08:46:30 +0800

    1.1.1 rc02 (#11158)
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="1.1.1"
+build_version="1.1.1-rc02"
 
 unset LANG
 unset LC_CTYPE
