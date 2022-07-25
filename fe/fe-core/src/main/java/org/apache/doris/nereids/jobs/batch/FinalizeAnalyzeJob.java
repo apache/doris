393e9ff2ea4ed3b256803ebf0880d5c5e9b89219
sohardforaname <organic_chemistry@foxmail.com>
2022-07-25 16:36:52 +0800

    [FE] add eliminate alias node rule and job

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/FinalizeAnalyzeJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/FinalizeAnalyzeJob.java
@@ -5,9 +5,9 @@ import org.apache.doris.nereids.rules.analysis.EliminateAliasNode;
 
 import com.google.common.collect.ImmutableList;
 
-public class finalizeAnalyzeJob extends BatchRulesJob {
+public class FinalizeAnalyzeJob extends BatchRulesJob {
 
-    public finalizeAnalyzeJob(PlannerContext plannerContext) {
+    public FinalizeAnalyzeJob(PlannerContext plannerContext) {
         super(plannerContext);
         rulesJob.addAll(ImmutableList.of(
                 bottomUpBatch(ImmutableList.of(
