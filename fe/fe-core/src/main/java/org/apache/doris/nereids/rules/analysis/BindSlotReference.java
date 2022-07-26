91a1e54504a939f73777c0518b9b03531e965ccf
sohardforaname <organic_chemistry@foxmail.com>
2022-07-26 17:28:07 +0800

    [FE] test plan OK

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSlotReference.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSlotReference.java
@@ -256,7 +256,7 @@ public class BindSlotReference implements AnalysisRuleFactory {
      */
     private class BoundStar extends NamedExpression {
         public BoundStar(List<Slot> children) {
-            super(ExpressionType.BOUND_STAR, children.toArray(new Slot[0]));
+            super(children.toArray(new Slot[0]));
             Preconditions.checkArgument(children.stream().allMatch(slot -> !(slot instanceof UnboundSlot)),
                     "BoundStar can not wrap UnboundSlot"
             );
