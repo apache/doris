1474a10d068953625639e1c12a1debc058999da9
yiguolei <676222867@qq.com>
2022-07-27 08:46:36 +0800

    [fix](update) check LOAD priv for update stmt (#11099) (#11231)
    Co-authored-by: Mingyu Chen <morningman.cmy@gmail.com>

--- fe/fe-core/src/main/java/org/apache/doris/analysis/UpdateStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/UpdateStmt.java
@@ -25,7 +25,12 @@ import org.apache.doris.catalog.OlapTable;
 import org.apache.doris.catalog.Table;
 import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.ErrorCode;
+import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
+import org.apache.doris.mysql.privilege.PrivPredicate;
+import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.rewrite.ExprRewriter;
 
 import com.google.common.base.Preconditions;
 import org.apache.doris.rewrite.ExprRewriter;
@@ -98,6 +103,13 @@ public class UpdateStmt extends DdlStmt {
     private void analyzeTargetTable(Analyzer analyzer) throws AnalysisException {
         // step1: analyze table name
         tableName.analyze(analyzer);
+
+        // check priv
+        if (!Catalog.getCurrentCatalog().getAuth()
+                .checkTblPriv(ConnectContext.get(), tableName.getDb(), tableName.getTbl(), PrivPredicate.LOAD)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_SPECIFIC_ACCESS_DENIED_ERROR, "LOAD");
+        }
+
         // step2: resolve table name with catalog, only unique olap table could be update
         String dbName = tableName.getDb();
         String targetTableName = tableName.getTbl();
