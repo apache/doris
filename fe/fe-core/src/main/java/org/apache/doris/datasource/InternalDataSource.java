b95f79be09d105800feccd5f4c4df45260d22449
Byte Yue <yj976240184@gmail.com>
2022-07-28 14:17:07 +0800

    add property to control whether light schema change or not

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalDataSource.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalDataSource.java
@@ -1716,11 +1716,6 @@ public class InternalDataSource implements DataSourceIf<Database> {
         OlapTable olapTable = new OlapTable(tableId, tableName, baseSchema, keysType, partitionInfo,
                 defaultDistributionInfo, indexes);
 
-        for (Column column : baseSchema) {
-            column.setUniqueId(olapTable.incAndGetMaxColUniqueId());
-            LOG.debug("table: {}, newColumn: {}, uniqueId: {}", olapTable.getName(), column.getName(),
-                    column.getUniqueId());
-        }
         olapTable.setComment(stmt.getComment());
 
         // set base index id
