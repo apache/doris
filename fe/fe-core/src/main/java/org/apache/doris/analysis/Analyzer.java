9f3f60bf11685ce988ac5b47f681c2d6ebb646d2
morrySnow <101034200+morrySnow@users.noreply.github.com>
2022-07-29 21:34:24 +0800

    [fix](planner)do not change non-null to nullable for outer join table slot (#11268)
    * remove nullable for outer join table
    
    * fix ut
    
    * remove useless comment
    
    * fix for original engine

--- fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
@@ -39,6 +39,7 @@ import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.IdGenerator;
 import org.apache.doris.common.Pair;
 import org.apache.doris.common.util.TimeUtils;
+import org.apache.doris.common.util.VectorizedUtil;
 import org.apache.doris.external.hudi.HudiTable;
 import org.apache.doris.external.hudi.HudiUtils;
 import org.apache.doris.planner.PlanNode;
@@ -800,8 +801,13 @@ public class Analyzer {
         }
         result = globalState.descTbl.addSlotDescriptor(d);
         result.setColumn(col);
-        // TODO: need to remove this outer join'
-        result.setIsNullable(col.isAllowNull() || isOuterJoined(d.getId()));
+        boolean isNullable;
+        if (VectorizedUtil.isVectorized()) {
+            isNullable = col.isAllowNull();
+        } else {
+            isNullable = col.isAllowNull() || isOuterJoined(d.getId());
+        }
+        result.setIsNullable(isNullable);
 
         slotRefMap.put(key, result);
         return result;
