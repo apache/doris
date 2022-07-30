cd62d137e0d274b4246b8f5d5b28cffacc6958ed
starocean999 <40539150+starocean999@users.noreply.github.com>
2022-07-30 14:15:13 +0800

    [FIX]DCHECK error of array functions (#11335)

--- be/src/vec/functions/array/function_array_binary.h
+++ be/src/vec/functions/array/function_array_binary.h
@@ -39,7 +39,11 @@ public:
     DataTypePtr get_return_type_impl(const DataTypes& arguments) const override {
         DCHECK(is_array(arguments[0])) << arguments[0]->get_name();
         DCHECK(is_array(arguments[1])) << arguments[1]->get_name();
-        DCHECK(arguments[0]->equals(*arguments[1]))
+        auto left_nested_type = remove_nullable(
+                assert_cast<const DataTypeArray&>(*(arguments[0])).get_nested_type());
+        auto right_nested_type = remove_nullable(
+                assert_cast<const DataTypeArray&>(*(arguments[1])).get_nested_type());
+        DCHECK(left_nested_type->equals(*right_nested_type))
                 << "data type " << arguments[0]->get_name() << " not equal with "
                 << arguments[1]->get_name();
         return Impl::get_return_type(arguments);
