f4f05cf95d00e6a8e80476a93e944b1d63c076be
zy-kkk <zhongykk@qq.com>
2022-08-01 14:31:39 +0800

    [test]Delete some unnecessary information (#11382)

--- regression-test/suites/correctness/test_lateral_view_in_subquery.groovy
+++ regression-test/suites/correctness/test_lateral_view_in_subquery.groovy
@@ -16,15 +16,15 @@
  // under the License.
 
  suite("test_lateral_view_in_subquery") {
-     sql """ DROP TABLE IF EXISTS user_tag_bitmap """
-     sql """ DROP TABLE IF EXISTS tag_group_width """
+     sql """ DROP TABLE IF EXISTS bm """
+     sql """ DROP TABLE IF EXISTS gp """
      sql """
-         CREATE TABLE `user_tag_bitmap` (
-             `tag_id` bigint(20) NULL COMMENT "标签id",
-             `hid` smallint(6) NULL COMMENT "分桶id",
-             `user_hash_bitmap` bitmap BITMAP_UNION NULL COMMENT ""
+         CREATE TABLE `bm` (
+             `id` bigint(20) NULL,
+             `hid` smallint(6) NULL,
+             `bitmap` bitmap BITMAP_UNION NULL
          ) ENGINE=OLAP
-         AGGREGATE KEY(`tag_id`, `hid`)
+         AGGREGATE KEY(`id`, `hid`)
          COMMENT "OLAP"
          DISTRIBUTED BY HASH(`hid`) BUCKETS 3
          PROPERTIES (
@@ -34,15 +34,15 @@
          )
      """
      sql """
-         CREATE TABLE `tag_group_width` (
-             `device_id` varchar(128) NOT NULL COMMENT "设备id",
-             `user_hash` bigint(20) NOT NULL COMMENT "用户hash，通过对hash算法计算device_id生成",
-             `uuid` varchar(128) MAX NULL COMMENT "用户uuid",
-             `group_1` varchar(32) MAX NULL COMMENT "标签值"
+         CREATE TABLE `gp` (
+             `id` varchar(128) NOT NULL,
+             `uh` bigint(20) NOT NULL,
+             `uid` varchar(128) MAX NULL,
+             `group` varchar(32) MAX NULL
          ) ENGINE=OLAP
-         AGGREGATE KEY(`device_id`, `user_hash`)
+         AGGREGATE KEY(`id`, `uh`)
          COMMENT "OLAP"
-         DISTRIBUTED BY HASH(`device_id`) BUCKETS 3
+         DISTRIBUTED BY HASH(`id`) BUCKETS 3
          PROPERTIES (
              "replication_allocation" = "tag.location.default: 1",
              "in_memory" = "false",
@@ -51,26 +51,26 @@
      """
 
      sql """
-         insert into user_tag_bitmap values(20001, 3, to_bitmap(1000)), (20001, 3, to_bitmap(2000));
+         insert into bm values(20001, 3, to_bitmap(1000)), (20001, 3, to_bitmap(2000));
      """
 
      sql """
-         insert into tag_group_width values("1", 1000, "1000", "1000"), ("1", 2000, "1000", "1000"), ("1", 3000, "1000", "1000");
+         insert into gp values("1", 1000, "1000", "1000"), ("1", 2000, "1000", "1000"), ("1", 3000, "1000", "1000");
      """
 
      order_qt_select """
          select * 
-         from tag_group_width 
-         where user_hash in 
+         from gp 
+         where uh in 
          (
-            select user_hash 
+            select uh 
             from 
             (
-                select bitmap_intersect(user_hash_bitmap) as user_bm 
-                from user_tag_bitmap 
-                where tag_id in (20001, 30001) and hid=3
+                select bitmap_intersect(bitmap) as ubm 
+                from bm 
+                where id in (20001, 30001) and hid=3
             ) t 
-            lateral view explode_bitmap(user_bm) tmp as user_hash
+            lateral view explode_bitmap(ubm) tmp as uh
         )
      """
  }
