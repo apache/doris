9c20fe08b7dd3b0b21f0721cdaec0ec377756c42
morrySnow <morrysnow@126.com>
2022-08-02 16:00:31 +0800

    merge ddl to load

--- regression-test/suites/nereids_syntax/load.groovy
+++ regression-test/suites/nereids_syntax/load.groovy
@@ -16,12 +16,136 @@
 // under the License.
 
 suite("load") {
-    def tables = ["customer", "lineorder", "part", "dates", "supplier"]
+    sql """
+        DROP TABLE IF EXISTS `customer`;
+    """
 
-    for (String table in tables) {
-        sql new File("""${context.file.parent}/ddl/${table}_create.sql""").text
-        sql new File("""${context.file.parent}/ddl/${table}_delete.sql""").text
-    }
+    sql """
+        DROP TABLE IF EXISTS `dates`;
+    """
+
+    sql """
+        DROP TABLE IF EXISTS `lineorder`;
+    """
+
+    sql """
+        DROP TABLE IF EXISTS `part`;
+    """
+
+    sql """
+        DROP TABLE IF EXISTS `supplier`;
+    """
+
+    sql """
+        CREATE TABLE IF NOT EXISTS `customer` (
+          `c_custkey` int(11) NOT NULL COMMENT "",
+          `c_name` varchar(26) NOT NULL COMMENT "",
+          `c_address` varchar(41) NOT NULL COMMENT "",
+          `c_city` varchar(11) NOT NULL COMMENT "",
+          `c_nation` varchar(16) NOT NULL COMMENT "",
+          `c_region` varchar(13) NOT NULL COMMENT "",
+          `c_phone` varchar(16) NOT NULL COMMENT "",
+          `c_mktsegment` varchar(11) NOT NULL COMMENT ""
+        )
+        DISTRIBUTED BY HASH(`c_custkey`) BUCKETS 10
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
+
+    sql """
+        CREATE TABLE IF NOT EXISTS `dates` (
+          `d_datekey` int(11) NOT NULL COMMENT "",
+          `d_date` varchar(20) NOT NULL COMMENT "",
+          `d_dayofweek` varchar(10) NOT NULL COMMENT "",
+          `d_month` varchar(11) NOT NULL COMMENT "",
+          `d_year` int(11) NOT NULL COMMENT "",
+          `d_yearmonthnum` int(11) NOT NULL COMMENT "",
+          `d_yearmonth` varchar(9) NOT NULL COMMENT "",
+          `d_daynuminweek` int(11) NOT NULL COMMENT "",
+          `d_daynuminmonth` int(11) NOT NULL COMMENT "",
+          `d_daynuminyear` int(11) NOT NULL COMMENT "",
+          `d_monthnuminyear` int(11) NOT NULL COMMENT "",
+          `d_weeknuminyear` int(11) NOT NULL COMMENT "",
+          `d_sellingseason` varchar(14) NOT NULL COMMENT "",
+          `d_lastdayinweekfl` int(11) NOT NULL COMMENT "",
+          `d_lastdayinmonthfl` int(11) NOT NULL COMMENT "",
+          `d_holidayfl` int(11) NOT NULL COMMENT "",
+          `d_weekdayfl` int(11) NOT NULL COMMENT ""
+        )
+        DISTRIBUTED BY HASH(`d_datekey`) BUCKETS 1
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
+
+    sql """
+        CREATE TABLE IF NOT EXISTS `lineorder` (
+          `lo_orderkey` bigint(20) NOT NULL COMMENT "",
+          `lo_linenumber` bigint(20) NOT NULL COMMENT "",
+          `lo_custkey` int(11) NOT NULL COMMENT "",
+          `lo_partkey` int(11) NOT NULL COMMENT "",
+          `lo_suppkey` int(11) NOT NULL COMMENT "",
+          `lo_orderdate` int(11) NOT NULL COMMENT "",
+          `lo_orderpriority` varchar(16) NOT NULL COMMENT "",
+          `lo_shippriority` int(11) NOT NULL COMMENT "",
+          `lo_quantity` bigint(20) NOT NULL COMMENT "",
+          `lo_extendedprice` bigint(20) NOT NULL COMMENT "",
+          `lo_ordtotalprice` bigint(20) NOT NULL COMMENT "",
+          `lo_discount` bigint(20) NOT NULL COMMENT "",
+          `lo_revenue` bigint(20) NOT NULL COMMENT "",
+          `lo_supplycost` bigint(20) NOT NULL COMMENT "",
+          `lo_tax` bigint(20) NOT NULL COMMENT "",
+          `lo_commitdate` bigint(20) NOT NULL COMMENT "",
+          `lo_shipmode` varchar(11) NOT NULL COMMENT ""
+        )
+        PARTITION BY RANGE(`lo_orderdate`)
+        (PARTITION p1992 VALUES [("-2147483648"), ("19930101")),
+        PARTITION p1993 VALUES [("19930101"), ("19940101")),
+        PARTITION p1994 VALUES [("19940101"), ("19950101")),
+        PARTITION p1995 VALUES [("19950101"), ("19960101")),
+        PARTITION p1996 VALUES [("19960101"), ("19970101")),
+        PARTITION p1997 VALUES [("19970101"), ("19980101")),
+        PARTITION p1998 VALUES [("19980101"), ("19990101")))
+        DISTRIBUTED BY HASH(`lo_orderkey`) BUCKETS 48
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
+
+    sql """
+        CREATE TABLE IF NOT EXISTS `part` (
+          `p_partkey` int(11) NOT NULL COMMENT "",
+          `p_name` varchar(23) NOT NULL COMMENT "",
+          `p_mfgr` varchar(7) NOT NULL COMMENT "",
+          `p_category` varchar(8) NOT NULL COMMENT "",
+          `p_brand` varchar(10) NOT NULL COMMENT "",
+          `p_color` varchar(12) NOT NULL COMMENT "",
+          `p_type` varchar(26) NOT NULL COMMENT "",
+          `p_size` int(11) NOT NULL COMMENT "",
+          `p_container` varchar(11) NOT NULL COMMENT ""
+        )
+        DISTRIBUTED BY HASH(`p_partkey`) BUCKETS 10
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
+
+    sql """
+        CREATE TABLE IF NOT EXISTS `supplier` (
+          `s_suppkey` int(11) NOT NULL COMMENT "",
+          `s_name` varchar(26) NOT NULL COMMENT "",
+          `s_address` varchar(26) NOT NULL COMMENT "",
+          `s_city` varchar(11) NOT NULL COMMENT "",
+          `s_nation` varchar(16) NOT NULL COMMENT "",
+          `s_region` varchar(13) NOT NULL COMMENT "",
+          `s_phone` varchar(16) NOT NULL COMMENT ""
+        )
+        DISTRIBUTED BY HASH(`s_suppkey`) BUCKETS 10
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
 
     sql """
         INSERT INTO customer VALUES
