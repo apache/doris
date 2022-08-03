4dea17d1055c52471c644567f946f139add74e26
morrySnow <101034200+morrySnow@users.noreply.github.com>
2022-08-03 12:14:17 +0800

    [enhancement](Nereids) support all join type in Nereids that could do join by HashJoinNode (#11446)
    add and test join type:
    1. inner join
    2. left outer join
    3. right outer join
    4. left semi join
    5. right semi join
    6. left anti join
    7. right anti join

---
+++ regression-test/suites/nereids_syntax/join.groovy
@@ -0,0 +1,67 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("join") {
+    sql """
+        SET enable_vectorized_engine=true
+    """
+
+    sql """
+        SET enable_nereids_planner=true
+    """
+
+    order_qt_inner_join_1 """
+        SELECT * FROM lineorder JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_inner_join_2 """
+        SELECT * FROM lineorder INNER JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_left_outer_join_1 """
+        SELECT * FROM lineorder LEFT JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_left_outer_join_2 """
+        SELECT * FROM lineorder LEFT OUTER JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_right_outer_join_1 """
+        SELECT * FROM lineorder RIGHT JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_right_outer_join_2 """
+        SELECT * FROM lineorder RIGHT OUTER JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_left_semi_join """
+        SELECT * FROM lineorder LEFT SEMI JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_right_semi_join """
+        SELECT * FROM lineorder RIGHT SEMI JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_left_anti_join """
+        SELECT * FROM lineorder LEFT ANTI JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+
+    order_qt_right_anti_join """
+        SELECT * FROM lineorder RIGHT ANTI JOIN supplier ON lineorder.lo_suppkey = supplier.s_suppkey
+    """
+}
+
