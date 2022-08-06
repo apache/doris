5ad242dcfb33e931129f820f631428894bddc67b
Shuo Wang <wangshuo128@gmail.com>
2022-08-06 11:22:24 +0800

    fix

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/expressions/ExpressionTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/expressions/ExpressionTest.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ExpressionTest {
-
     @Test
     public void testConstantExpression() {
         // literal is constant
