87e52488deabb232fd6a309744319b25e4f04cb4
Chen Zhang <chzhang1987@gmail.com>
2022-08-09 16:16:42 +0800

    update

--- be/src/vec/exec/volap_scanner.cpp
+++ be/src/vec/exec/volap_scanner.cpp
@@ -242,7 +242,9 @@ Status VOlapScanner::_init_tablet_reader_params(
         _tablet_reader_params.use_page_cache = true;
     }
 
-    _tablet_reader_params.delete_bitmap = &_tablet->tablet_meta()->delete_bitmap();
+    if (_tablet->enable_unique_key_merge_on_write()) {
+        _tablet_reader_params.delete_bitmap = &_tablet->tablet_meta()->delete_bitmap();
+    }
 
     if (_parent->_olap_scan_node.__isset.sort_info &&
         _parent->_olap_scan_node.sort_info.is_asc_order.size() > 0) {
