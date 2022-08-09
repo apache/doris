710d539eb00764bc3c1dab87d03681bb1eb7af3d
Mingyu Chen <morningman.cmy@gmail.com>
2022-08-09 14:09:38 +0800

    [improvement](alter) modify table's default replica if table is unpartitioned (#11550)
    Before, if a table is unpartitioned, when executing following alter stmt:
    ```
    alter table tbl1 set ("replication_num" = "1");
    ```
    Only the tbl1 partition's replication_num is changed
    (for unpartitioned table, it also has a single partition with same name as table's)
    But the table's default replication_num is unchanged.
    So when executing `show create table tbl1`, you will find that the replication_num is still the origin value.
    
    This CL mainly changes:
    1. For unpartitioned table, if user change it's replication num, both table's and partition's replication_num will be changed.

--- fe/fe-core/src/test/java/org/apache/doris/persist/BatchModifyPartitionsInfoTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/persist/BatchModifyPartitionsInfoTest.java
@@ -22,6 +22,7 @@ import org.apache.doris.catalog.ReplicaAllocation;
 import org.apache.doris.common.AnalysisException;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -59,8 +60,8 @@ public class BatchModifyPartitionsInfoTest {
 
         List<Long> partitionIds = Lists.newArrayList(PARTITION_ID_1, PARTITION_ID_2, PARTITION_ID_3);
         for (long partitionId : partitionIds) {
-            modifyInfos.add(new ModifyPartitionInfo(DB_ID, TB_ID, partitionId,
-                    DataProperty.DEFAULT_DATA_PROPERTY, ReplicaAllocation.DEFAULT_ALLOCATION, true, ""));
+            modifyInfos.add(new ModifyPartitionInfo(DB_ID, TB_ID, partitionId, DataProperty.DEFAULT_DATA_PROPERTY,
+                    ReplicaAllocation.DEFAULT_ALLOCATION, true, "", Maps.newHashMap()));
         }
 
         BatchModifyPartitionsInfo batchModifyPartitionsInfo = new BatchModifyPartitionsInfo(modifyInfos);
