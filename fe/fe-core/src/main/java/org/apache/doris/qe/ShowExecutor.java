dca4c1df39636dc01c2d1a597cdff97840fbb4bd
nextdreamblue <zxw520blue1@163.com>
2022-08-10 11:52:07 +0800

    [improvement] sort result by tablename when show tables like 'show data'

--- fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -718,6 +718,11 @@ public class ShowExecutor {
                 rows.add(Lists.newArrayList(tbl.getName()));
             }
         }
+        if (rows.size() != 0) {
+            rows.sort((x, y) -> {
+                return x.get(0).compareTo(y.get(0));
+            });
+        }
         resultSet = new ShowResultSet(showTableStmt.getMetaData(), rows);
     }
 
