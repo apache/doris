1017f352df5d9947f201463f25ab2b2e159bf62e
Chen Zhang <chzhang1987@gmail.com>
2022-08-11 18:25:45 +0800

    fix show create table

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -2909,6 +2909,11 @@ public class Env {
                 sb.append(olapTable.getSequenceType().toString()).append("\"");
             }
 
+            if (olapTable.getEnableUniqueKeyMergeOnWrite()) {
+                sb.append(",\n\"").append(PropertyAnalyzer.ENABLE_UNIQUE_KEY_MERGE_ON_WRITE).append("\" = \"");
+                sb.append(olapTable.getEnableUniqueKeyMergeOnWrite()).append("\"");
+            }
+
             sb.append("\n)");
         } else if (table.getType() == TableType.MYSQL) {
             MysqlTable mysqlTable = (MysqlTable) table;
