180cc35815ed1023cbbd22d0c5a4b6016ef11495
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2022-08-11 12:42:19 +0800

    [Feature](nereids) support sub-query and alias in FromClause (#11035)
    Support sub-query and alias for TPC-H，for example:
    select * from (select * from (T1) A join T2 as B on T1.id = T2.id) T;

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindRelation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindRelation.java
@@ -20,9 +20,13 @@ package org.apache.doris.nereids.rules.analysis;
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.Table;
+import org.apache.doris.catalog.TableIf.TableType;
+import org.apache.doris.nereids.analyzer.NereidsAnalyzer;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
 import org.apache.doris.nereids.trees.plans.logical.LogicalOlapScan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalSubQueryAlias;
 import org.apache.doris.qe.ConnectContext;
 
 import com.google.common.collect.ImmutableList;
@@ -41,16 +45,11 @@ public class BindRelation extends OneAnalysisRuleFactory {
             switch (nameParts.size()) {
                 case 1: {
                     // Use current database name from catalog.
-                    String dbName = connectContext.getDatabase();
-                    Table table = getTable(dbName, nameParts.get(0), connectContext.getEnv());
-                    // TODO: should generate different Scan sub class according to table's type
-                    return new LogicalOlapScan(table, ImmutableList.of(dbName));
+                    return bindWithCurrentDb(connectContext, nameParts);
                 }
                 case 2: {
                     // Use database name from table name parts.
-                    String dbName = connectContext.getClusterName() + ":" + nameParts.get(0);
-                    Table table = getTable(dbName, nameParts.get(1), connectContext.getEnv());
-                    return new LogicalOlapScan(table, ImmutableList.of(dbName));
+                    return bindWithDbNameFromNamePart(connectContext, nameParts);
                 }
                 default:
                     throw new IllegalStateException("Table name [" + ctx.root.getTableName() + "] is invalid.");
@@ -68,5 +67,34 @@ public class BindRelation extends OneAnalysisRuleFactory {
         } finally {
             db.readUnlock();
         }
+    }
+
+    private LogicalPlan bindWithCurrentDb(ConnectContext ctx, List<String> nameParts) {
+        String dbName = ctx.getDatabase();
+        Table table = getTable(dbName, nameParts.get(0), ctx.getEnv());
+        // TODO: should generate different Scan sub class according to table's type
+        if (table.getType() == TableType.OLAP) {
+            return new LogicalOlapScan(table, ImmutableList.of(dbName));
+        } else if (table.getType() == TableType.VIEW) {
+            LogicalPlan viewPlan = new NereidsAnalyzer(ctx).analyze(table.getDdlSql());
+            return new LogicalSubQueryAlias<>(table.getName(), viewPlan);
+        }
+        throw new RuntimeException("Unsupported tableType:" + table.getType());
+    }
+
+    private LogicalPlan bindWithDbNameFromNamePart(ConnectContext ctx, List<String> nameParts) {
+        // if the relation is view, nameParts.get(0) is dbName.
+        String dbName = nameParts.get(0);
+        if (!dbName.equals(ctx.getDatabase())) {
+            dbName = ctx.getClusterName() + ":" + nameParts.get(0);
+        }
+        Table table = getTable(dbName, nameParts.get(1), ctx.getEnv());
+        if (table.getType() == TableType.OLAP) {
+            return new LogicalOlapScan(table, ImmutableList.of(dbName));
+        } else if (table.getType() == TableType.VIEW) {
+            LogicalPlan viewPlan = new NereidsAnalyzer(ctx).analyze(table.getDdlSql());
+            return new LogicalSubQueryAlias<>(table.getName(), viewPlan);
+        }
+        throw new RuntimeException("Unsupported tableType:" + table.getType());
     }
 }
