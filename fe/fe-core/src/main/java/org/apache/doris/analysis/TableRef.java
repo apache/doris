6dff66f099e402df9e3c35a7027f9c561ff185df
minghong <minghong.zhou@163.com>
2022-08-11 09:36:43 +0800

    [fix](analyzer) InferFilterRule bug: equations in on clause of outer/anti join  are not inferable. (#11515)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/TableRef.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/TableRef.java
@@ -27,6 +27,7 @@ import org.apache.doris.common.UserException;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
 import org.apache.doris.rewrite.ExprRewriter;
+import org.apache.doris.rewrite.ExprRewriter.ClauseType;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
@@ -573,7 +574,7 @@ public class TableRef implements ParseNode, Writable {
         Preconditions.checkState(isAnalyzed);
         if (onClause != null) {
             Expr expr = onClause.clone();
-            onClause = rewriter.rewrite(onClause, analyzer, ExprRewriter.ClauseType.ON_CLAUSE);
+            onClause = rewriter.rewrite(onClause, analyzer, ClauseType.fromJoinType(joinOp));
             if (joinOp.isOuterJoin() || joinOp.isSemiAntiJoin()) {
                 if (onClause instanceof BoolLiteral && !((BoolLiteral) onClause).getValue()) {
                     onClause = expr;
