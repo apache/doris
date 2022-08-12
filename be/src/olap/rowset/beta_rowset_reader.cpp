2ceb98609c35bbf46e77e07b69c254db3500c24c
Userwhite <925945594@qq.com>
2022-08-12 11:32:40 +0800

    fix type

--- be/src/olap/rowset/beta_rowset_reader.cpp
+++ be/src/olap/rowset/beta_rowset_reader.cpp
@@ -174,7 +174,8 @@ Status BetaRowsetReader::init(RowsetReaderContext* read_context) {
     if (read_context->reuse_block != nullptr) {
         _input_block = read_context->reuse_block;
     } else {
-        _input_block.reset(new RowBlockV2(*_input_schema, std::min(1024, read_context->batch_size)));
+        _input_block.reset(
+                new RowBlockV2(*_input_schema, std::min(1024, read_context->batch_size)));
         read_context->reuse_block = _input_block;
     }
 
