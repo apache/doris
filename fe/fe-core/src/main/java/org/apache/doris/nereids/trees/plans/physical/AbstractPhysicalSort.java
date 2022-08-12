6cbd0cf1c21dc9de43e6401da36d04e743ef43b2
morrySnow <101034200+morrySnow@users.noreply.github.com>
2022-08-12 17:08:23 +0800

    [enhancement](Nereids)refactor sort plan in nereids (#11673)
    1. rename PhysicalHeapSort to PhysicalQuickSort
    2. add LogicalTopN and PhysicalTopN
    3. add implementation rule for LogicalTopN
    4. add a interface Sort for both logical and physical sort
    5. add a interface TopN for both logical and physical top-n
    6. add a AbstractPhysicalSort as super class of PhysicalQuickSort and PhysicalTopN

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/AbstractPhysicalSort.java
@@ -0,0 +1,83 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.trees.plans.physical;
+
+import org.apache.doris.nereids.memo.GroupExpression;
+import org.apache.doris.nereids.properties.LogicalProperties;
+import org.apache.doris.nereids.properties.OrderKey;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.PlanType;
+import org.apache.doris.nereids.trees.plans.algebra.Sort;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Abstract class for all concrete physical sort plan.
+ */
+public abstract class AbstractPhysicalSort<CHILD_TYPE extends Plan> extends PhysicalUnary<CHILD_TYPE> implements Sort {
+
+    protected final List<OrderKey> orderKeys;
+
+    public AbstractPhysicalSort(PlanType type, List<OrderKey> orderKeys,
+            LogicalProperties logicalProperties, CHILD_TYPE child) {
+        this(type, orderKeys, Optional.empty(), logicalProperties, child);
+    }
+
+    /**
+     * Constructor of PhysicalHashJoinNode.
+     */
+    public AbstractPhysicalSort(PlanType type, List<OrderKey> orderKeys,
+            Optional<GroupExpression> groupExpression, LogicalProperties logicalProperties,
+            CHILD_TYPE child) {
+        super(type, groupExpression, logicalProperties, child);
+        this.orderKeys = ImmutableList.copyOf(Objects.requireNonNull(orderKeys, "orderKeys can not be null"));
+    }
+
+    public List<OrderKey> getOrderKeys() {
+        return orderKeys;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractPhysicalSort that = (AbstractPhysicalSort) o;
+        return Objects.equals(orderKeys, that.orderKeys);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderKeys);
+    }
+
+    @Override
+    public List<Expression> getExpressions() {
+        return orderKeys.stream()
+                .map(OrderKey::getExpr)
+                .collect(ImmutableList.toImmutableList());
+    }
+}
