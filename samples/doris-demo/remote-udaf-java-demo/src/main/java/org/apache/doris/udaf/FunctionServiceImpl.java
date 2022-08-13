00216873980e8bab46fef6dff77b607b9b5c97f7
chenlinzhong <490103404@qq.com>
2022-08-13 17:55:23 +0800

    [remote-udaf](java-sample) impl some udaf demo with java

--- samples/doris-demo/remote-udaf-java-demo/src/main/java/org/apache/doris/udaf/FunctionServiceImpl.java
+++ samples/doris-demo/remote-udaf-java-demo/src/main/java/org/apache/doris/udaf/FunctionServiceImpl.java
@@ -17,6 +17,10 @@
 
 package org.apache.doris.udaf;
 
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import org.apache.doris.proto.FunctionService;
 import org.apache.doris.proto.PFunctionServiceGrpc;
 import org.apache.doris.proto.Types;
@@ -26,6 +30,15 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import io.grpc.stub.StreamObserver;
+
+import java.util.Map;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.*;
+
+
+
 
 public class FunctionServiceImpl extends PFunctionServiceGrpc.PFunctionServiceImplBase {
     private static final Logger logger = Logger.getLogger(FunctionServiceImpl.class.getName());
@@ -41,7 +54,6 @@ public class FunctionServiceImpl extends PFunctionServiceGrpc.PFunctionServiceIm
         // symbol is functionName
         String functionName = request.getFunctionName();
         logger.info("fnCall request=" + request);
-
         FunctionService.PFunctionCallResponse.Builder res = FunctionService.PFunctionCallResponse.newBuilder();
         if ("rpc_sum_update".equals(functionName)) {
             Types.PStatus.Builder statusCode = Types.PStatus.newBuilder().setStatusCode(0);
@@ -138,6 +150,105 @@ public class FunctionServiceImpl extends PFunctionServiceGrpc.PFunctionServiceIm
             
             pValues.setHasNull(false);
             pValues.addDoubleValue(total/size);
+            pValues.setType(returnType.build());
+            res.setStatus(statusCode.build());
+            res.addResult(pValues.build());
+        }
+        if ("rpc_count_visit_info_update".equals(functionName)) {
+            Types.PStatus.Builder statusCode = Types.PStatus.newBuilder().setStatusCode(0);
+            Types.PValues.Builder pValues = Types.PValues.newBuilder();
+            Types.PGenericType.Builder returnType = Types.PGenericType.newBuilder().setId(Types.PGenericType.TypeId.STRING);
+
+            long total = 0;
+            int size = request.getArgs(0).getStringValueCount();
+
+            JSONObject GlobalIpMap = new JSONObject();
+            if(request.getContext().getFunctionContext().getArgsDataCount()>0
+              && request.getContext().getFunctionContext().getArgsData(0).getStringValueCount()>0 ){
+                String  s= request.getContext().getFunctionContext().getArgsData(0).getStringValue(0);
+                GlobalIpMap = JSONObject.parseObject(s);
+            }
+            for(int i=0;i<size;i++){
+                String str = request.getArgs(0).getStringValue(i);
+                JSONObject object = JSONObject.parseObject(str);
+                String ip = object.getString("ip");
+                if(GlobalIpMap.containsKey(ip)){
+                    int num = GlobalIpMap.getIntValue(ip);
+                    num ++;
+                    GlobalIpMap.put(ip,num);
+                }else{
+                    GlobalIpMap.put(ip,1);
+                }
+            }
+            String objStr = JSON.toJSONString(GlobalIpMap);
+            pValues.setHasNull(false);
+            pValues.addStringValue(objStr);
+            pValues.setType(returnType.build());
+            res.setStatus(statusCode.build());
+            res.addResult(pValues.build());
+        }
+        if ("rpc_count_visit_info_merge".equals(functionName)) {
+            Types.PStatus.Builder statusCode = Types.PStatus.newBuilder().setStatusCode(0);
+            Types.PValues.Builder pValues = Types.PValues.newBuilder();
+            Types.PGenericType.Builder returnType = Types.PGenericType.newBuilder().setId(Types.PGenericType.TypeId.STRING);
+
+            String  s1= request.getArgs(0).getStringValue(0);
+            JSONObject GlobalIpMap1 = JSONObject.parseObject(s1);
+            String  s2= request.getArgs(1).getStringValue(0);
+            JSONObject GlobalIpMap2 = JSONObject.parseObject(s2);
+            Iterator iter = GlobalIpMap2.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                String ip = (entry.getKey().toString());
+                int num2 = Integer.parseInt(entry.getValue().toString());
+                if(GlobalIpMap1.containsKey(ip)){
+                    int num1 = GlobalIpMap1.getIntValue(ip);
+                    int total = num1+num2;
+                    GlobalIpMap1.put(ip,total);
+                }else{
+                    GlobalIpMap1.put(ip,num2);
+                }
+            }
+            String objStr = JSON.toJSONString(GlobalIpMap1);
+            pValues.setHasNull(false);
+            pValues.addStringValue(objStr);
+            pValues.setType(returnType.build());
+            res.setStatus(statusCode.build());
+            res.addResult(pValues.build());
+        }
+        if ("rpc_count_visit_info_finalize".equals(functionName)) {
+            Types.PStatus.Builder statusCode = Types.PStatus.newBuilder().setStatusCode(0);
+            Types.PValues.Builder pValues = Types.PValues.newBuilder();
+            Types.PGenericType.Builder returnType = Types.PGenericType.newBuilder().setId(Types.PGenericType.TypeId.STRING);
+            String  s= request.getContext().getFunctionContext().getArgsData(0).getStringValue(0);
+            JSONObject finalMap = JSONObject.parseObject(s);
+            Map<String,Integer> mapRepeat = new HashMap<String,Integer>();
+            Iterator iter = finalMap.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                String key = entry.getKey().toString();
+                int value = Integer.parseInt(entry.getValue().toString());
+                mapRepeat.put(key, value);
+            }
+            List<Map.Entry<String, Integer>> list1 = new ArrayList<Map.Entry<String, Integer>>();
+            list1.addAll(mapRepeat.entrySet());
+            Collections.sort(list1, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    return o2.getValue() - o1.getValue();
+                }
+            });
+            int topN=3;
+            String sortedOutputStr="";
+            for (Map.Entry<String, Integer> entry : list1) {
+                topN --;
+                if(topN<0){
+                    break;
+                }
+                sortedOutputStr += entry.getKey()+":"+entry.getValue()+" ";
+            }
+            pValues.setHasNull(false);
+            pValues.addStringValue(sortedOutputStr);
             pValues.setType(returnType.build());
             res.setStatus(statusCode.build());
             res.addResult(pValues.build());
