6c39758f9b5718213f7bab4ccc19fa440cf9d302
Zhengguo Yang <yangzhgg@gmail.com>
2022-08-15 13:24:57 +0800

    [fix](update) Fix where clause is not reanalyzed after rewrite (#11723)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/UpdateStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/UpdateStmt.java
@@ -200,6 +200,8 @@ public class UpdateStmt extends DdlStmt {
         }
         whereExpr.analyze(analyzer);
         whereExpr = analyzer.getExprRewriter().rewrite(whereExpr, analyzer, ExprRewriter.ClauseType.WHERE_CLAUSE);
+        whereExpr.reset();
+        whereExpr.analyze(analyzer);
         if (!whereExpr.getType().equals(Type.BOOLEAN)) {
             throw new AnalysisException("Where clause is not a valid statement return bool");
         }
