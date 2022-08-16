041ffe41d067db54fd2475cf1617c833cabf9918
stalary <stalary@163.com>
2022-08-16 23:04:59 +0800

    FIX: ut

--- be/test/exec/es_scan_reader_test.cpp
+++ be/test/exec/es_scan_reader_test.cpp
@@ -227,7 +227,8 @@ TEST_F(MockESServerTest, workflow) {
     std::vector<EsPredicate*> predicates;
     std::map<std::string, std::string> docvalue_context;
     bool doc_value_mode = false;
-    props[ESScanReader::KEY_QUERY] = ESScrollQueryBuilder::build(props, fields, _predicates, docvalue_context, &doc_value_mode);
+    props[ESScanReader::KEY_QUERY] = ESScrollQueryBuilder::build(props, fields, predicates,
+                                                                 docvalue_context, &doc_value_mode);
     ESScanReader reader(target, props, doc_value_mode);
     auto st = reader.open();
     EXPECT_TRUE(st.ok());
