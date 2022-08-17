87a301383fd578944fbd9926ba1f396783260247
Xinyi Zou <zouxinyi02@gmail.com>
2022-08-17 22:09:06 +0800

    fix mem tracker details

--- be/src/runtime/load_channel.cpp
+++ be/src/runtime/load_channel.cpp
@@ -46,7 +46,8 @@ LoadChannel::~LoadChannel() {
               << ", is high priority=" << _is_high_priority << ", sender_ip=" << _sender_ip
               << ", is_vec=" << _is_vec;
     // Load channel tracker cannot be completely accurate, offsetting the impact on the load channel mgr tracker.
-    _mem_tracker->parent()->consumption_revise(-_mem_tracker->consumption());
+    _mem_tracker->parent()->consume_local(-_mem_tracker->consumption(),
+                                          ExecEnv::GetInstance()->process_mem_tracker().get());
 }
 
 Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
