8b1f8cbeb75a60c31c064dac6fe989c9688aadc6
Stalary <stalary@163.com>
2022-08-18 10:31:17 +0800

    [Enhancement](Doe) Be query es use fe generate dsl.  (#11840)

--- be/src/vec/exec/ves_http_scan_node.cpp
+++ be/src/vec/exec/ves_http_scan_node.cpp
@@ -124,6 +124,12 @@ Status VEsHttpScanNode::open(RuntimeState* state) {
     SCOPED_CONSUME_MEM_TRACKER(mem_tracker());
     RETURN_IF_CANCELLED(state);
 
+    // fe by enable_new_es_dsl to control whether to generate DSL for easy rollback. After the code is stable, can delete the be generation logic
+    if (_properties.find(ESScanReader::KEY_QUERY_DSL) != _properties.end()) {
+        RETURN_IF_ERROR(start_scanners());
+        return Status::OK();
+    }
+
     // if conjunct is constant, compute direct and set eos = true
     for (int conj_idx = 0; conj_idx < _conjunct_ctxs.size(); ++conj_idx) {
         if (_conjunct_ctxs[conj_idx]->root()->is_constant()) {
