e6bbb1b9f57e9b5d835e6eac5b2dcdd5f93f506b
Adonis Ling <adonis0147@gmail.com>
2022-08-19 17:58:40 +0800

    [fix](script) Support starting BE without Java environment (#11910)

--- bin/start_be.sh
+++ bin/start_be.sh
@@ -75,8 +75,8 @@ done
 export DORIS_JNI_CLASSPATH_PARAMETER="-Djava.class.path=${DORIS_JNI_CLASSPATH_PARAMETER}"
 
 jdk_version() {
+    local java_cmd="${1}"
     local result
-    local java_cmd="${JAVA_HOME:-.}/bin/java"
     local IFS=$'\n'
 
     if [[ -z "${java_cmd}" ]]; then
@@ -97,24 +97,34 @@ jdk_version() {
     return 0
 }
 
-jvm_arch="amd64"
-MACHINE_TYPE="$(uname -m)"
-if [[ "${MACHINE_TYPE}" == "aarch64" ]]; then
-    jvm_arch="aarch64"
-fi
-java_version="$(
-    set -e
-    jdk_version
-)"
-if [[ "${java_version}" -gt 8 ]]; then
-    export LD_LIBRARY_PATH="${JAVA_HOME}/lib/server:${JAVA_HOME}/lib:${LD_LIBRARY_PATH}"
-# JAVA_HOME is jdk
-elif [[ -d "${JAVA_HOME}/jre" ]]; then
-    export LD_LIBRARY_PATH="${JAVA_HOME}/jre/lib/${jvm_arch}/server:${JAVA_HOME}/jre/lib/${jvm_arch}:${LD_LIBRARY_PATH}"
-# JAVA_HOME is jre
-else
-    export LD_LIBRARY_PATH="${JAVA_HOME}/lib/${jvm_arch}/server:${JAVA_HOME}/lib/${jvm_arch}:${LD_LIBRARY_PATH}"
-fi
+setup_java_env() {
+    local java_version
+
+    if [[ -z "${JAVA_HOME}" ]]; then
+        return 1
+    fi
+
+    local jvm_arch='amd64'
+    if [[ "$(uname -m)" == 'aarch64' ]]; then
+        jvm_arch='aarch64'
+    fi
+    java_version="$(
+        set -e
+        jdk_version "${JAVA_HOME}/bin/java"
+    )"
+    if [[ "${java_version}" -gt 8 ]]; then
+        export LD_LIBRARY_PATH="${JAVA_HOME}/lib/server:${JAVA_HOME}/lib:${LD_LIBRARY_PATH}"
+        # JAVA_HOME is jdk
+    elif [[ -d "${JAVA_HOME}/jre" ]]; then
+        export LD_LIBRARY_PATH="${JAVA_HOME}/jre/lib/${jvm_arch}/server:${JAVA_HOME}/jre/lib/${jvm_arch}:${LD_LIBRARY_PATH}"
+        # JAVA_HOME is jre
+    else
+        export LD_LIBRARY_PATH="${JAVA_HOME}/lib/${jvm_arch}/server:${JAVA_HOME}/lib/${jvm_arch}:${LD_LIBRARY_PATH}"
+    fi
+}
+
+# prepare jvm if needed
+setup_java_env || true
 
 # export env variables from be.conf
 #
