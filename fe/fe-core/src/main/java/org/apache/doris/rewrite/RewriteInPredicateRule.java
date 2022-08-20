bd96efbd6f8048c28da7a67e367f6d79b5e81434
Zhengguo Yang <yangzhgg@gmail.com>
2022-08-20 18:27:08 +0800

    Update RewriteInPredicateRule.java

--- fe/fe-core/src/main/java/org/apache/doris/rewrite/RewriteInPredicateRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/rewrite/RewriteInPredicateRule.java
@@ -55,6 +55,10 @@ public class RewriteInPredicateRule implements ExprRewriteRule {
         }
         InPredicate inPredicate = (InPredicate) expr;
         SlotRef slotRef;
+        // When the select stmt contains group by, we use oriGroupingExprs to store the original group by statement
+        // and reset it with the rewritten groupingExpr. Therefore, origroupingexprs cannot be analyzed.
+        // However, in #4197, oriGroupingExprs is rewritten to fix the problem of constant fold.
+        // The newly added InPredicteRewriteRule requires that expr must be analyzed before being rewritten
         if (!inPredicate.isAnalyzed() || inPredicate.contains(Subquery.class) || !inPredicate.isLiteralChildren()
                 || inPredicate.isNotIn() || !(inPredicate.getChild(0).unwrapExpr(false) instanceof SlotRef)
                 || (slotRef = inPredicate.getChild(0).getSrcSlotRef()) == null || slotRef.getColumn() == null) {
