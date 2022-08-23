73a25a8149005e8beb2c51bad22127a5f5730b4d
starocean999 <12095047@qq.com>
2022-08-23 12:00:38 +0800

    add comments

--- fe/fe-core/src/main/java/org/apache/doris/planner/HashJoinNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/HashJoinNode.java
@@ -441,6 +441,9 @@ public class HashJoinNode extends PlanNode {
         int rightNullableNumber = 0;
         if (copyLeft) {
             for (TupleDescriptor leftTupleDesc : analyzer.getDescTbl().getTupleDesc(getChild(0).getOutputTblRefIds())) {
+                // if the child is cross join node, the only way to get the correct nullable info of its output slots
+                // is to check if the output tuple ids are outer joined or not.
+                // then pass this nullable info to hash join node will be correct.
                 boolean needSetToNullable =
                         getChild(0) instanceof CrossJoinNode && analyzer.isOuterJoined(leftTupleDesc.getId());
                 for (SlotDescriptor leftSlotDesc : leftTupleDesc.getSlots()) {
