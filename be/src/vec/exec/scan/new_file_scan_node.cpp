7b1ffe97f82ec086d07756a4782d0fd253e808d8
Jibing Li <jibing3@gmail.com>
2022-08-23 14:05:05 +0800

    Add file scan node

---
+++ be/src/vec/exec/scan/new_file_scan_node.cpp
@@ -0,0 +1,115 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "vec/exec/scan/new_file_scan_node.h"
+
+#include "vec/columns/column_const.h"
+#include "vec/exec/scan/new_olap_scanner.h"
+#include "vec/functions/in.h"
+#include "vec/exec/scan/new_file_arrow_scanner.h"
+#include "vec/exec/scan/new_file_text_scanner.h"
+
+namespace doris::vectorized {
+
+NewFileScanNode::NewFileScanNode(ObjectPool* pool, const TPlanNode& tnode,
+                                 const DescriptorTbl& descs)
+        : VScanNode(pool, tnode, descs), _file_scan_node(tnode.file_scan_node) {
+    _output_tuple_id = tnode.file_scan_node.tuple_id;
+}
+
+Status NewFileScanNode::prepare(RuntimeState* state) {
+    RETURN_IF_ERROR(VScanNode::prepare(state));
+    _scanner_mem_tracker = std::make_unique<MemTracker>("NewFileScanners");
+    return Status::OK();
+}
+
+void NewFileScanNode::set_scan_ranges(const std::vector<TScanRangeParams>& scan_ranges) {
+    int max_scanners = config::doris_scanner_thread_pool_thread_num;
+    if (scan_ranges.size() <= max_scanners) {
+        _scan_ranges = scan_ranges;
+    } else {
+        // There is no need for the number of scanners to exceed the number of threads in thread pool.
+        _scan_ranges.clear();
+        auto range_iter = scan_ranges.begin();
+        for (int i = 0; i < max_scanners && range_iter != scan_ranges.end(); ++i, ++range_iter) {
+            _scan_ranges.push_back(*range_iter);
+        }
+        for (int i = 0; range_iter != scan_ranges.end(); ++i, ++range_iter) {
+            if (i == max_scanners) {
+                i = 0;
+            }
+            auto& ranges = _scan_ranges[i].scan_range.ext_scan_range.file_scan_range.ranges;
+            auto& merged_ranges = range_iter->scan_range.ext_scan_range.file_scan_range.ranges;
+            ranges.insert(ranges.end(), merged_ranges.begin(), merged_ranges.end());
+        }
+        _scan_ranges.shrink_to_fit();
+        LOG(INFO) << "Merge " << scan_ranges.size() << " scan ranges to " << _scan_ranges.size();
+    }
+}
+
+Status NewFileScanNode::_init_profile() {
+    return Status::OK();
+}
+
+Status NewFileScanNode::_process_conjuncts() {
+    RETURN_IF_ERROR(VScanNode::_process_conjuncts());
+    if (_eos) {
+        return Status::OK();
+    }
+    // TODO: Push conjuncts down to reader.
+    return Status::OK();
+}
+
+Status NewFileScanNode::_init_scanners(std::list<VScanner*>* scanners) {
+    if (_scan_ranges.empty()) {
+        _eos = true;
+        return Status::OK();
+    }
+
+    for (auto& scan_range : _scan_ranges) {
+        VScanner* scanner = (VScanner*) _create_scanner(scan_range.scan_range.ext_scan_range.file_scan_range);
+        scanners->push_back(scanner);
+    }
+
+    return Status::OK();
+}
+
+VScanner* NewFileScanNode::_create_scanner(const TFileScanRange& scan_range) {
+    NewFileScanner* scanner = nullptr;
+    switch (scan_range.params.format_type) {
+        case TFileFormatType::FORMAT_PARQUET:
+            scanner = new NewFileParquetScanner(_state, this, _limit_per_scanner,
+                                                scan_range, _scanner_mem_tracker.get(), runtime_profile());
+            break;
+        case TFileFormatType::FORMAT_ORC:
+            scanner = new NewFileORCScanner(_state, this, _limit_per_scanner,
+                                            scan_range, _scanner_mem_tracker.get(), runtime_profile());
+            break;
+
+        default:
+            scanner = new NewFileTextScanner(_state, this, _limit_per_scanner,
+                                             scan_range, _scanner_mem_tracker.get(), runtime_profile());
+            break;
+    }
+    _scanner_pool.add(scanner);
+    scanner->prepare(_vconjunct_ctx_ptr.get());
+    // TODO: Can we remove _conjunct_ctxs and use _vconjunct_ctx_ptr instead?
+    scanner->reg_conjunct_ctxs(_conjunct_ctxs);
+    return scanner;
+}
+
+}; // namespace doris::vectorized
---
+++ be/src/vec/exec/scan/new_file_text_scanner.h
@@ -0,0 +1,63 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include "exprs/bloomfilter_predicate.h"
+#include "exprs/function_filter.h"
+#include "vec/exec/scan/vscanner.h"
+#include "vec/exec/scan/new_file_scanner.h"
+#include <exec/arrow/arrow_reader.h>
+#include "exec/line_reader.h"
+
+namespace doris::vectorized {
+    class NewFileTextScanner : public NewFileScanner {
+    public:
+        NewFileTextScanner(RuntimeState *state, NewFileScanNode *parent, int64_t limit,
+                           const TFileScanRange &scan_range, MemTracker *tracker, RuntimeProfile* profile);
+
+        Status open(RuntimeState* state) override;
+
+    protected:
+        void _init_profiles(RuntimeProfile* profile) override {}
+        Status _get_block_impl(RuntimeState* state, Block* block, bool* eos) override;
+
+    private:
+        Status _fill_file_columns(const Slice& line, vectorized::Block* _block);
+        Status _open_next_reader();
+        Status _open_file_reader();
+        Status _open_line_reader();
+        Status _line_split_to_values(const Slice& line);
+        Status _split_line(const Slice& line);
+        // Reader
+        std::shared_ptr<FileReader> _cur_file_reader;
+        LineReader* _cur_line_reader;
+        bool _cur_line_reader_eof;
+
+        // When we fetch range start from 0, header_type="csv_with_names" skip first line
+        // When we fetch range start from 0, header_type="csv_with_names_and_types" skip first two line
+        // When we fetch range doesn't start
+        int _skip_lines;
+        std::vector<Slice> _split_values;
+        std::string _value_separator;
+        std::string _line_delimiter;
+        int _value_separator_length;
+        int _line_delimiter_length;
+
+        bool _success;
+    };
+}
