d9f63c573ac9ca2e9ed214cf6c26115ce724b001
fuyu29 <fuyu29@meituan.com>
2022-08-23 19:07:03 +0800

    support rewrite rule: inPredicate to equalTo

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rewrite/rules/InPredicateToEqualToRule.java
@@ -0,0 +1,45 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+
+package org.apache.doris.nereids.rules.expression.rewrite.rules;
+
+import org.apache.doris.nereids.rules.expression.rewrite.AbstractExpressionRewriteRule;
+import org.apache.doris.nereids.rules.expression.rewrite.ExpressionRewriteContext;
+import org.apache.doris.nereids.trees.expressions.EqualTo;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.expressions.InPredicate;
+
+/**
+ * Rewrites InPredicate into an EqualTo Expression, if there exists only one element in InPredicate.Options.
+ * Examples:
+ * where A in (x) ==> where A = x
+ */
+public class InPredicateToEqualToRule extends AbstractExpressionRewriteRule {
+
+    public static InPredicateToEqualToRule INSTANCE = new InPredicateToEqualToRule();
+
+    @Override
+    public Expression visitInPredicate(InPredicate inPredicate, ExpressionRewriteContext context) {
+        if (inPredicate.getOptions().size() > 1) {
+            return inPredicate;
+        }
+        Expression left = inPredicate.getCompareExpr();
+        Expression right = inPredicate.getOptions().get(0);
+        return new EqualTo(left, right);
+    }
+}
