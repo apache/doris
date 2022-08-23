693e181414fce87d9b7ae6415f62ce47f416df0d
Zenglin Luo <luozenglin@gmail.com>
2022-08-23 22:55:25 +0800

    update

--- be/src/util/telemetry/telemetry.cpp
+++ be/src/util/telemetry/telemetry.cpp
@@ -47,16 +47,7 @@ public:
     void Handle(internal_log::LogLevel level, const char* file, int line, const char* msg,
                 const opentelemetry::sdk::common::AttributeMap& attributes) noexcept override {
         if (level == internal_log::LogLevel::Error || level == internal_log::LogLevel::Warning) {
-            std::stringstream output_s;
-            output_s << "OpenTelemetry ";
-            if (file != nullptr) {
-                output_s << "File: " << file << ":" << line << " ";
-            }
-            if (msg != nullptr) {
-                output_s << msg;
-            }
-            output_s << std::endl;
-            LOG(WARNING) << output_s.str();
+            LOG(WARNING) << fmt::format("OpenTelemetry File: {}:{} {}", file, line, msg);
         }
     }
 };
