2c927f6ba519af84e4aca2b5c3aa935a046d4955
fuyu29 <fuyu29@meituan.com>
2022-08-24 17:55:50 +0800

    add regression-test file for rule:InPredicateToEqualTo

--- regression-test/suites/nereids_syntax_p0/inpredicate.groovy
+++ regression-test/suites/nereids_syntax_p0/inpredicate.groovy
@@ -55,5 +55,13 @@ suite("inpredicate") {
     order_qt_in_predicate_8 """
         SELECT * FROM supplier WHERE s_nation not in ('PERU', 'ETHIOPIA');
     """
+  
+    order_qt_in_predicate_9 """
+        SELECT * FROM supplier WHERE s_suppkey in (1);
+    """
+
+    order_qt_in_predicate_10 """
+        SELECT * FROM supplier WHERE s_suppkey not in (1);
+    """
 }
 
