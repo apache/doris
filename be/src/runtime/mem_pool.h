9a22e0eab62dff1df42437c94ad88b04bb48aca3
Xinyi Zou <zouxinyi02@gmail.com>
2022-08-24 14:24:14 +0800

    [fix](memtracker) Improve performance of tracking real physical memory of PodArray #12021

--- be/src/runtime/mem_pool.h
+++ be/src/runtime/mem_pool.h
@@ -212,7 +212,7 @@ private:
     bool check_integrity(bool check_current_chunk_empty);
 
     void reset_peak() {
-        if (total_allocated_bytes_ - peak_allocated_bytes_ > 1024) {
+        if (total_allocated_bytes_ - peak_allocated_bytes_ > 4096) {
             THREAD_MEM_TRACKER_TRANSFER_FROM(total_allocated_bytes_ - peak_allocated_bytes_,
                                              ExecEnv::GetInstance()->process_mem_tracker().get());
             peak_allocated_bytes_ = total_allocated_bytes_;
