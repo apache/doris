3abc4f357fde4d6c21d1ad1b3644dddf03315e15
HappenLee <happenlee@hotmail.com>
2022-08-24 08:51:53 +0800

    [Bug](bitmap) intersect_count function use in string cause ASAN error (#11936)

--- be/src/util/bitmap_intersect.h
+++ be/src/util/bitmap_intersect.h
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 #pragma once
+#include <parallel_hashmap/phmap.h>
+
 #include "runtime/string_value.h"
 #include "udf/udf.h"
 #include "util/bitmap_value.h"
@@ -103,6 +105,15 @@ inline char* Helper::write_to<StringValue>(const StringValue& v, char* dest) {
     dest += v.len;
     return dest;
 }
+
+template <>
+inline char* Helper::write_to<std::string>(const std::string& v, char* dest) {
+    *(uint32_t*)dest = v.size();
+    dest += 4;
+    memcpy(dest, v.c_str(), v.size());
+    dest += v.size();
+    return dest;
+}
 // write_to end
 
 template <>
@@ -118,6 +129,11 @@ inline int32_t Helper::serialize_size<DecimalV2Value>(const DecimalV2Value& v) {
 template <>
 inline int32_t Helper::serialize_size<StringValue>(const StringValue& v) {
     return v.len + 4;
+}
+
+template <>
+inline int32_t Helper::serialize_size<std::string>(const std::string& v) {
+    return v.size() + 4;
 }
 // serialize_size end
 
@@ -145,6 +161,14 @@ inline void Helper::read_from<StringValue>(const char** src, StringValue* result
     int32_t length = *(int32_t*)(*src);
     *src += 4;
     *result = StringValue((char*)*src, length);
+    *src += length;
+}
+
+template <>
+inline void Helper::read_from<std::string>(const char** src, std::string* result) {
+    int32_t length = *(int32_t*)(*src);
+    *src += 4;
+    *result = std::string((char*)*src, length);
     *src += length;
 }
 // read_from end
@@ -240,6 +264,93 @@ public:
 
 private:
     std::map<T, BitmapValue> _bitmaps;
+};
+
+template <>
+struct BitmapIntersect<std::string_view> {
+public:
+    BitmapIntersect() = default;
+
+    explicit BitmapIntersect(const char* src) { deserialize(src); }
+
+    void add_key(const std::string_view key) {
+        BitmapValue empty_bitmap;
+        _bitmaps[key] = empty_bitmap;
+    }
+
+    void update(const std::string_view& key, const BitmapValue& bitmap) {
+        if (_bitmaps.find(key) != _bitmaps.end()) {
+            _bitmaps[key] |= bitmap;
+        }
+    }
+
+    void merge(const BitmapIntersect& other) {
+        for (auto& kv : other._bitmaps) {
+            if (_bitmaps.find(kv.first) != _bitmaps.end()) {
+                _bitmaps[kv.first] |= kv.second;
+            } else {
+                _bitmaps[kv.first] = kv.second;
+            }
+        }
+    }
+
+    // intersection
+    BitmapValue intersect() const {
+        BitmapValue result;
+        auto it = _bitmaps.begin();
+        result |= it->second;
+        it++;
+        for (; it != _bitmaps.end(); it++) {
+            result &= it->second;
+        }
+        return result;
+    }
+
+    // calculate the intersection for _bitmaps's bitmap values
+    int64_t intersect_count() const {
+        if (_bitmaps.empty()) {
+            return 0;
+        }
+        return intersect().cardinality();
+    }
+
+    // the serialize size
+    size_t size() {
+        size_t size = 4;
+        for (auto& kv : _bitmaps) {
+            size += detail::Helper::serialize_size(kv.first);
+            size += kv.second.getSizeInBytes();
+        }
+        return size;
+    }
+
+    //must call size() first
+    void serialize(char* dest) {
+        char* writer = dest;
+        *(int32_t*)writer = _bitmaps.size();
+        writer += 4;
+        for (auto& kv : _bitmaps) {
+            writer = detail::Helper::write_to(kv.first, writer);
+            kv.second.write(writer);
+            writer += kv.second.getSizeInBytes();
+        }
+    }
+
+    void deserialize(const char* src) {
+        const char* reader = src;
+        int32_t bitmaps_size = *(int32_t*)reader;
+        reader += 4;
+        for (int32_t i = 0; i < bitmaps_size; i++) {
+            std::string key;
+            detail::Helper::read_from(&reader, &key);
+            BitmapValue bitmap(reader);
+            reader += bitmap.getSizeInBytes();
+            _bitmaps[key] = bitmap;
+        }
+    }
+
+private:
+    phmap::flat_hash_map<std::string, BitmapValue> _bitmaps;
 };
 
 } // namespace doris
