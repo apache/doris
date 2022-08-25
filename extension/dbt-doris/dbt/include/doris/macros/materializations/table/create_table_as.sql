abf4344b27affe3fc7575b62518af62098c67b8a
catpineapple <42031973+catpineapple@users.noreply.github.com>
2022-08-25 15:03:28 +0800

    [feature] (dbt) add incremental and init interactive command line (#11870)
    add dbt-doris incremental model and init interactive command line

--- extension/dbt-doris/dbt/include/doris/macros/materializations/table/create_table_as.sql
+++ extension/dbt-doris/dbt/include/doris/macros/materializations/table/create_table_as.sql
@@ -19,9 +19,25 @@
     {% set sql_header = config.get('sql_header', none) %}
     {% set table = relation.include(database=False) %}
     {{ sql_header if sql_header is not none }}
+    {%if temporary %}
+        {{doris__drop_relation(relation)}};
+    {% endif %}
     create table {{ table }}
+    {{ doris__duplicate_key() }}
     {{ doris__partition_by() }}
     {{ doris__distributed_by() }}
     {{ doris__properties() }} as {{ sql }};
-    insert into {{ table }} {{ sql }};
+
+{%- endmacro %}
+
+{% macro doris__create_unique_table_as(temporary, relation, sql) -%}
+    {% set sql_header = config.get('sql_header', none) %}
+    {% set table = relation.include(database=False) %}
+    {{ sql_header if sql_header is not none }}
+    create table {{ table }}
+    {{ doris__unique_key() }}
+    {{ doris__partition_by() }}
+    {{ doris__distributed_by() }}
+    {{ doris__properties() }} as {{ sql }};
+
 {%- endmacro %}
