fe8168aa957aa1e86973955b99f12701e998fc28
camby <104178625@qq.com>
2022-08-29 17:29:23 +0800

    [fix](array-type) array can not be distributed key and aggregation key (#12082)
    Array column should not be distributed key or group by or aggregate key, we should forbid it before create table.
    Co-authored-by: cambyzju <zhuxiaoli01@baidu.com>

--- fe/fe-core/src/test/java/org/apache/doris/catalog/CreateTableTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/CreateTableTest.java
@@ -230,7 +230,7 @@ public class CreateTableTest {
     @Test
     public void testAbnormal() throws DdlException {
         ExceptionChecker.expectThrowsWithMsg(DdlException.class,
-                "Floating point type column can not be distribution column",
+                "Floating point type should not be used in distribution column",
                 () -> createTable("create table test.atbl1\n" + "(k1 int, k2 float)\n" + "duplicate key(k1)\n"
                         + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); "));
 
