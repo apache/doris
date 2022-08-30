2e0c9c286854b31f0189f7c032b842e8ce963ff9
jiafeng.zhang <zhangjf1@gmail.com>
2022-08-30 18:43:47 +0800

    fix

--- fe/spark-dpp/src/main/java/org/apache/doris/load/loadv2/dpp/SparkDpp.java
+++ fe/spark-dpp/src/main/java/org/apache/doris/load/loadv2/dpp/SparkDpp.java
@@ -266,7 +266,7 @@ public final class SparkDpp implements java.io.Serializable {
                             }
                             lastBucketKey = curBucketKey;
                         }
-                        InternalRow internalRow = encoder.createDeserializer().apply(rowWithoutBucketKey);
+                        InternalRow internalRow = encoder.createSerializer().apply(rowWithoutBucketKey);
                         parquetWriter.write(internalRow);
                     }
                     if (parquetWriter != null) {
