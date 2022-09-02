apache/doris

commit 39325bd66618adcfb826707d3d82aec4926cb330

[Bug](tablet) Fix bug that segment files are removed as trash but tablet meta is normal (#10721)

be/src/olap/tablet_manager.cpp
@@ -161,6 +161,16 @@ Status TabletManager::_add_tablet_unlocked(TTabletId tablet_id, const TabletShar
         res = _add_tablet_to_map_unlocked(tablet_id, tablet, update_meta, keep_files,
                                           true /*drop_old*/);
     } else {
+        tablet->set_tablet_state(TABLET_SHUTDOWN);
+        tablet->save_meta();
+        {
+            std::lock_guard<std::shared_mutex> shutdown_tablets_wrlock(_shutdown_tablets_lock);
+            _shutdown_tablets.push_back(tablet);
+        }
+        LOG(INFO) << "set tablet to shutdown state."
+                  << "tablet_id=" << tablet->tablet_id()
+                  << ", tablet_path=" << tablet->tablet_path();
+
         res = Status::OLAPInternalError(OLAP_ERR_ENGINE_INSERT_OLD_TABLET);
     }
     LOG(WARNING) << "add duplicated tablet. force=" << force << ", res=" << res

commit 1365e2b78e17085755fc036497a21580d6846b83

[dev-1.1.2][fix] Fix build failed for dev-1.1.2 (#12291)
introduced from #10721

be/src/olap/tablet_manager.cpp
@@ -163,12 +163,12 @@ OLAPStatus TabletManager::_add_tablet_unlocked(TTabletId tablet_id, SchemaHash s
         tablet->set_tablet_state(TABLET_SHUTDOWN);
         tablet->save_meta();
         {
-            std::lock_guard<std::shared_mutex> shutdown_tablets_wrlock(_shutdown_tablets_lock);
+            WriteLock shutdown_tablets_wrlock(_shutdown_tablets_lock);
             _shutdown_tablets.push_back(tablet);
         }
         LOG(INFO) << "set tablet to shutdown state."
                   << "tablet_id=" << tablet->tablet_id()
-                  << ", tablet_path=" << tablet->tablet_path();
+                  << ", tablet_path=" << tablet->tablet_path_desc().debug_string();
 
         res = OLAP_ERR_ENGINE_INSERT_OLD_TABLET;
     }