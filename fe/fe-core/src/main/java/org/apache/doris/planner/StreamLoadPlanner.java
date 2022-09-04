5b4f0ceaf6537323aff7bb92ec47c97104432fad
Gabriel <gabrielleebuaa@gmail.com>
2022-09-04 15:18:47 +0800

    update

--- fe/fe-core/src/main/java/org/apache/doris/planner/StreamLoadPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/StreamLoadPlanner.java
@@ -143,10 +143,15 @@ public class StreamLoadPlanner {
             scanSlotDesc.setColumn(col);
             scanSlotDesc.setIsNullable(col.isAllowNull());
             for (ImportColumnDesc importColumnDesc : taskInfo.getColumnExprDescs().descs) {
-                if (!importColumnDesc.isColumn() && importColumnDesc.getColumnName() != null
-                        && importColumnDesc.getColumnName().equals(col.getName())) {
-                    scanSlotDesc.setIsNullable(importColumnDesc.getExpr().isNullable());
-                    break;
+                try {
+                    if (!importColumnDesc.isColumn() && importColumnDesc.getColumnName() != null
+                            && importColumnDesc.getColumnName().equals(col.getName())) {
+                        scanSlotDesc.setIsNullable(importColumnDesc.getExpr().isNullable());
+                        break;
+                    }
+                } catch (Exception e) {
+                    // An exception may be thrown here because the `importColumnDesc.getExpr()` is not analyzed now.
+                    // We just skip this case here.
                 }
             }
             if (negative && !col.isKey() && col.getAggregationType() != AggregateType.SUM) {
