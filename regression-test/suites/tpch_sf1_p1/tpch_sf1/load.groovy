a47eb55d7c8cf6f61afb3437248ea3a722daba25
Dongyang Li <hello_stephen@qq.com>
2022-09-05 19:25:01 +0800

    [regression](load)split dataset to cover more situation (#12311)

--- regression-test/suites/tpch_sf1_p1/tpch_sf1/load.groovy
+++ regression-test/suites/tpch_sf1_p1/tpch_sf1/load.groovy
@@ -53,7 +53,41 @@ suite("load") {
 
             // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
             // also, you can stream load a http stream, e.g. http://xxx/some.csv
-            file """${context.sf1DataPath}/tpch/sf1/${tableName}.csv.gz"""
+            file """${context.sf1DataPath}/tpch/sf1/${tableName}.csv.split00.gz"""
+
+            time 10000 // limit inflight 10s
+
+            // stream load action will check result, include Success status, and NumberTotalRows == NumberLoadedRows
+
+            // if declared a check callback, the default check condition will ignore.
+            // So you must check all condition
+            check { result, exception, startTime, endTime ->
+                if (exception != null) {
+                    throw exception
+                }
+                log.info("Stream load result: ${result}".toString())
+                def json = parseJson(result)
+                assertEquals("success", json.Status.toLowerCase())
+                assertEquals(json.NumberTotalRows, json.NumberLoadedRows)
+                assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
+            }
+        }
+        streamLoad {
+            // a default db 'regression_test' is specified in
+            // ${DORIS_HOME}/conf/regression-conf.groovy
+            table tableName
+
+            // default label is UUID:
+            // set 'label' UUID.randomUUID().toString()
+
+            // default column_separator is specify in doris fe config, usually is '\t'.
+            // this line change to ','
+            set 'column_separator', '|'
+            set 'compress_type', 'GZ'
+
+            // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
+            // also, you can stream load a http stream, e.g. http://xxx/some.csv
+            file """${context.sf1DataPath}/tpch/sf1/${tableName}.csv.split01.gz"""
 
             time 10000 // limit inflight 10s
 
