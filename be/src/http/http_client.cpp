c28d73c9c581c4affefd9f6363d9d79471b9255d
stalary <stalary@163.com>
2022-09-07 09:30:09 +0800

    MOD: format

--- be/src/http/http_client.cpp
+++ be/src/http/http_client.cpp
@@ -16,9 +16,9 @@
 // under the License.
 
 #include "http/http_client.h"
-#include "util/stack_util.h"
 
 #include "common/config.h"
+#include "util/stack_util.h"
 
 namespace doris {
 
@@ -163,7 +163,8 @@ Status HttpClient::execute(const std::function<bool(const void* data, size_t len
     _callback = &callback;
     auto code = curl_easy_perform(_curl);
     if (code != CURLE_OK) {
-        LOG(WARNING) << "fail to execute HTTP client, errmsg=" << _to_errmsg(code) << " trace=" << get_stack_trace();
+        LOG(WARNING) << "fail to execute HTTP client, errmsg=" << _to_errmsg(code)
+                     << " trace=" << get_stack_trace();
         return Status::InternalError(_to_errmsg(code));
     }
     return Status::OK();
