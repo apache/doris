b2942d36b0c98f00919a621e6c50469ab44e550b
jackwener <jakevingoo@gmail.com>
2022-09-07 13:52:55 +0800

    ut

--- fe/fe-core/src/test/java/org/apache/doris/nereids/memo/MemoCopyInTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/memo/MemoCopyInTest.java
@@ -17,73 +17,64 @@
 
 package org.apache.doris.nereids.memo;
 
-import org.apache.doris.nereids.analyzer.UnboundRelation;
-import org.apache.doris.nereids.trees.expressions.literal.BooleanLiteral;
 import org.apache.doris.nereids.trees.plans.JoinType;
-import org.apache.doris.nereids.trees.plans.logical.LogicalFilter;
 import org.apache.doris.nereids.trees.plans.logical.LogicalJoin;
+import org.apache.doris.nereids.trees.plans.logical.LogicalOlapScan;
 import org.apache.doris.nereids.util.MemoTestUtils;
 import org.apache.doris.nereids.util.PatternMatchSupported;
 import org.apache.doris.nereids.util.PlanChecker;
+import org.apache.doris.nereids.util.PlanConstructor;
 
-import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemoCopyInTest implements PatternMatchSupported {
+    LogicalJoin<LogicalOlapScan, LogicalOlapScan> logicalJoinAB = new LogicalJoin<>(JoinType.INNER_JOIN,
+            PlanConstructor.newLogicalOlapScan(0, "A", 0), PlanConstructor.newLogicalOlapScan(1, "B", 0));
+    LogicalJoin<LogicalJoin<LogicalOlapScan, LogicalOlapScan>, LogicalOlapScan> logicalJoinABC = new LogicalJoin<>(
+            JoinType.INNER_JOIN, logicalJoinAB, PlanConstructor.newLogicalOlapScan(2, "C", 0));
+
     /**
      * Original:
-     * Group 0: UnboundRelation C
-     * Group 1: UnboundRelation B
-     * Group 2: UnboundRelation A
+     * Group 0: LogicalOlapScan C
+     * Group 1: LogicalOlapScan B
+     * Group 2: LogicalOlapScan A
      * Group 3: Join(Group 1, Group 2)
      * Group 4: Join(Group 0, Group 3)
-     * Group 5: Filter(Group 4)
      * <p>
      * Then:
      * Copy In Join(Group 2, Group 1) into Group 3
      * <p>
      * Expected:
-     * Group 0: UnboundRelation C
-     * Group 1: UnboundRelation B
-     * Group 2: UnboundRelation A
-     * Group 3: Join(Group 1, Group 2), Join(Group 1, Group 2)
+     * Group 0: LogicalOlapScan C
+     * Group 1: LogicalOlapScan B
+     * Group 2: LogicalOlapScan A
+     * Group 3: Join(Group 1, Group 2), Join(Group 2, Group 1)
      * Group 4: Join(Group 0, Group 3)
-     * Group 5: Filter(Group 4)
      */
     @Test
-    public void testMergeGroup() {
-        UnboundRelation unboundRelationA = new UnboundRelation(Lists.newArrayList("A"));
-        UnboundRelation unboundRelationB = new UnboundRelation(Lists.newArrayList("B"));
-        UnboundRelation unboundRelationC = new UnboundRelation(Lists.newArrayList("C"));
-        LogicalJoin<UnboundRelation, UnboundRelation> logicalJoinBA = new LogicalJoin<>(JoinType.INNER_JOIN,
-                unboundRelationB, unboundRelationA);
-        LogicalJoin<UnboundRelation, LogicalJoin<UnboundRelation, UnboundRelation>> logicalJoinCBA = new LogicalJoin<>(
-                JoinType.INNER_JOIN, unboundRelationC, logicalJoinBA);
-        LogicalFilter<LogicalJoin<UnboundRelation, LogicalJoin<UnboundRelation, UnboundRelation>>> logicalFilter
-                = new LogicalFilter<>(new BooleanLiteral(true), logicalJoinCBA);
-
-        PlanChecker.from(MemoTestUtils.createConnectContext(), logicalFilter)
-                .checkGroupNum(6)
+    public void testInsertSameGroup() {
+        PlanChecker.from(MemoTestUtils.createConnectContext(), logicalJoinABC)
                 .transform(
                         // swap join's children
-                        logicalJoin(unboundRelation(), unboundRelation()).then(joinBA ->
+                        logicalJoin(logicalOlapScan(), logicalOlapScan()).then(joinBA ->
                                 new LogicalJoin<>(JoinType.INNER_JOIN, joinBA.right(), joinBA.left())
                         ))
-                .checkGroupNum(6)
-                .checkGroupExpressionNum(7)
+                .checkGroupNum(5)
+                .checkGroupExpressionNum(6)
                 .checkMemo(memo -> {
                     Group root = memo.getRoot();
                     Assertions.assertEquals(1, root.getLogicalExpressions().size());
-                    GroupExpression filter = root.getLogicalExpression();
-                    GroupExpression joinCBA = filter.child(0).getLogicalExpression();
-                    Assertions.assertEquals(1, joinCBA.child(0).getLogicalExpressions().size());
-                    Assertions.assertEquals(2, joinCBA.child(1).getLogicalExpressions().size());
-                    GroupExpression joinBA = joinCBA.child(1).getLogicalExpressions().get(0);
-                    GroupExpression joinAB = joinCBA.child(1).getLogicalExpressions().get(1);
+                    GroupExpression joinABC = root.getLogicalExpression();
+                    Assertions.assertEquals(2, joinABC.child(0).getLogicalExpressions().size());
+                    Assertions.assertEquals(1, joinABC.child(1).getLogicalExpressions().size());
+                    GroupExpression joinAB = joinABC.child(0).getLogicalExpressions().get(0);
+                    GroupExpression joinBA = joinABC.child(0).getLogicalExpressions().get(1);
                     Assertions.assertTrue(joinAB.getPlan() instanceof LogicalJoin);
                     Assertions.assertTrue(joinBA.getPlan() instanceof LogicalJoin);
                 });
 
     }
+
+    // TODO: test mergeGroup().
 }
