6e51d74ab1f1d59698f8e79567ca5c3c45f5bcbb
Yijia Su <suyijia@selectdb.com>
2022-09-08 18:16:00 +0800

    add licensed

--- docker/rapid-deployment/docker-file/register/single_fe-single_be/shell/register.sh
+++ docker/rapid-deployment/docker-file/register/single_fe-single_be/shell/register.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 echo "Welcome to Apache-Doris Docker-Compose to quickly build test images!"
 echo "Start modifying the configuration files of FE and BE and run FE and BE!"
 docker cp /root/init_fe.sh doris-fe:/root/init_fe.sh
