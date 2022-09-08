e7aa1315067b42116af06d198860561a805574a5
yixiutt <102007456+yixiutt@users.noreply.github.com>
2022-09-08 08:37:16 +0800

    [enhancement](tcmalloc) add aggressive_memory_decommit conf and make it disable (#12436)
    Co-authored-by: yixiutt <yixiu@selectdb.com>

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -53,6 +53,8 @@ CONF_String(priority_networks, "");
 CONF_mInt64(tc_use_memory_min, "10737418240");
 // free memory rate.[0-100]
 CONF_mInt64(tc_free_memory_rate, "20");
+// tcmallc aggressive_memory_decommit
+CONF_mBool(tc_enable_aggressive_memory_decommit, "false");
 
 // Bound on the total amount of bytes allocated to thread caches.
 // This bound is not strict, so it is possible for the cache to go over this bound
