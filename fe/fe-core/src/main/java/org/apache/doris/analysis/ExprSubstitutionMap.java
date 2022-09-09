4461fd70c06f3578f230281a717255f5bf6826cd
Mingyu Chen <morningman.cmy@gmail.com>
2022-09-09 09:02:45 +0800

    [improvment](planner) unset common fields to reduce plan thrift size (#12495)
    1. For query with 1656 union, the plan thrift size will be reduced from 400MB+ to 2MB.
    This optimization is introduced from #4904, but lost after #9720
    
    2. Disable ExprSubstitutionMap.verify when debug is disable.
    So that the plan time of query with 1656 union will be reduced from 20s to 2s

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ExprSubstitutionMap.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ExprSubstitutionMap.java
@@ -279,17 +279,21 @@ public final class ExprSubstitutionMap {
      * and that all rhs exprs are analyzed.
      */
     private void verify() {
-        for (int i = 0; i < lhs_.size(); ++i) {
-            for (int j = i + 1; j < lhs_.size(); ++j) {
-                if (lhs_.get(i).equals(lhs_.get(j))) {
-                    if (LOG.isTraceEnabled()) {
-                        LOG.trace("verify: smap=" + this.debugString());
+        // This method is very very time consuming, especially when planning large complex query.
+        // So disable it by default.
+        if (LOG.isDebugEnabled()) {
+            for (int i = 0; i < lhs_.size(); ++i) {
+                for (int j = i + 1; j < lhs_.size(); ++j) {
+                    if (lhs_.get(i).equals(lhs_.get(j))) {
+                        if (LOG.isTraceEnabled()) {
+                            LOG.trace("verify: smap=" + this.debugString());
+                        }
+                        // TODO(zc): partition by k1, order by k1, there is failed.
+                        // Preconditions.checkState(false);
                     }
-                    // TODO(zc): partition by k1, order by k1, there is failed.
-                    // Preconditions.checkState(false);
                 }
+                Preconditions.checkState(!checkAnalyzed_ || rhs_.get(i).isAnalyzed());
             }
-            Preconditions.checkState(!checkAnalyzed_ || rhs_.get(i).isAnalyzed());
         }
     }
 
