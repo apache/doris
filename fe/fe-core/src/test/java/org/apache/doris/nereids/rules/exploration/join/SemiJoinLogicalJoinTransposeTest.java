7d1b1aa1341bd55753deb1327c41810a07419dfb
jackwener <jakevingoo@gmail.com>
2022-09-14 13:11:28 +0800

    enhance UT

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/SemiJoinLogicalJoinTransposeTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/SemiJoinLogicalJoinTransposeTest.java
@@ -33,13 +33,42 @@ import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SemiJoinLogicalJoinTransposeProjectTest {
+public class SemiJoinLogicalJoinTransposeTest {
     public static final LogicalOlapScan scan1 = PlanConstructor.newLogicalOlapScan(0, "t1", 0);
     public static final LogicalOlapScan scan2 = PlanConstructor.newLogicalOlapScan(1, "t2", 0);
     public static final LogicalOlapScan scan3 = PlanConstructor.newLogicalOlapScan(2, "t3", 0);
 
     @Test
     public void testSemiJoinLogicalTransposeCommute() {
+        LogicalPlan topJoin = new LogicalPlanBuilder(scan1)
+                .hashJoinUsing(scan2, JoinType.INNER_JOIN, Pair.of(0, 0))
+                .hashJoinUsing(scan3, JoinType.LEFT_SEMI_JOIN, Pair.of(0, 0))
+                .build();
+
+        PlanChecker.from(MemoTestUtils.createConnectContext(), topJoin)
+                .transform(SemiJoinLogicalJoinTranspose.LEFT_DEEP.build())
+                .checkMemo(memo -> {
+                    Group root = memo.getRoot();
+                    Assertions.assertEquals(2, root.getLogicalExpressions().size());
+                    Plan plan = memo.copyOut(root.getLogicalExpressions().get(1), false);
+
+                    LogicalJoin<?, ?> newTopJoin = (LogicalJoin<?, ?>) plan;
+                    LogicalJoin<?, ?> newBottomJoin = (LogicalJoin<?, ?>) newTopJoin.left();
+                    Assertions.assertEquals(JoinType.INNER_JOIN, newTopJoin.getJoinType());
+                    Assertions.assertEquals(JoinType.LEFT_SEMI_JOIN, newBottomJoin.getJoinType());
+
+                    LogicalOlapScan newBottomJoinLeft = (LogicalOlapScan) newBottomJoin.left();
+                    LogicalOlapScan newBottomJoinRight = (LogicalOlapScan) newBottomJoin.right();
+                    LogicalOlapScan newTopJoinRight = (LogicalOlapScan) newTopJoin.right();
+
+                    Assertions.assertEquals("t1", newBottomJoinLeft.getTable().getName());
+                    Assertions.assertEquals("t3", newBottomJoinRight.getTable().getName());
+                    Assertions.assertEquals("t2", newTopJoinRight.getTable().getName());
+                });
+    }
+
+    @Test
+    public void testSemiJoinLogicalTransposeProjectCommute() {
         LogicalPlan topJoin = new LogicalPlanBuilder(scan1)
                 .hashJoinUsing(scan2, JoinType.INNER_JOIN, Pair.of(0, 0))
                 .project(ImmutableList.of(0))
@@ -53,11 +82,18 @@ public class SemiJoinLogicalJoinTransposeProjectTest {
                     Assertions.assertEquals(2, root.getLogicalExpressions().size());
                     Plan plan = memo.copyOut(root.getLogicalExpressions().get(1), false);
 
-                    Plan join = plan.child(0);
-                    Assertions.assertTrue(join instanceof LogicalJoin);
-                    Assertions.assertEquals(JoinType.INNER_JOIN, ((LogicalJoin<?, ?>) join).getJoinType());
-                    Assertions.assertEquals(JoinType.LEFT_SEMI_JOIN,
-                            ((LogicalJoin<?, ?>) ((LogicalJoin<?, ?>) join).left()).getJoinType());
+                    LogicalJoin<?, ?> newTopJoin = (LogicalJoin<?, ?>) plan.child(0);
+                    LogicalJoin<?, ?> newBottomJoin = (LogicalJoin<?, ?>) newTopJoin.left().child(0);
+                    Assertions.assertEquals(JoinType.INNER_JOIN, newTopJoin.getJoinType());
+                    Assertions.assertEquals(JoinType.LEFT_SEMI_JOIN, newBottomJoin.getJoinType());
+
+                    LogicalOlapScan newBottomJoinLeft = (LogicalOlapScan) newBottomJoin.left();
+                    LogicalOlapScan newBottomJoinRight = (LogicalOlapScan) newBottomJoin.right();
+                    LogicalOlapScan newTopJoinRight = (LogicalOlapScan) newTopJoin.right();
+
+                    Assertions.assertEquals("t1", newBottomJoinLeft.getTable().getName());
+                    Assertions.assertEquals("t3", newBottomJoinRight.getTable().getName());
+                    Assertions.assertEquals("t2", newTopJoinRight.getTable().getName());
                 });
     }
 }
