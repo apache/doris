e1dcc4b78dffb9ca2d07c0a140d84d31a3f529e1
Zhengguo Yang <yangzhgg@gmail.com>
2022-09-16 20:57:22 +0800

    [Bugfix](stack_over_flow) fix be may core dump because of stack-buffer-overflow when TBrokerOpenReaderResponse too large (#12658)

--- be/src/io/broker_reader.cpp
+++ be/src/io/broker_reader.cpp
@@ -25,6 +25,7 @@
 #include "runtime/broker_mgr.h"
 #include "runtime/client_cache.h"
 #include "runtime/exec_env.h"
+#include "util/defer_op.h"
 
 namespace doris {
 
@@ -76,7 +77,8 @@ Status BrokerReader::open() {
     request.__set_clientId(client_id(_env, broker_addr));
     request.__set_properties(_properties);
 
-    TBrokerOpenReaderResponse response;
+    TBrokerOpenReaderResponse* response = new TBrokerOpenReaderResponse();
+    Defer del_reponse {[&] { delete response; }};
     try {
         Status status;
         BrokerServiceConnection client(client_cache(_env), broker_addr,
@@ -88,11 +90,11 @@ Status BrokerReader::open() {
         }
 
         try {
-            client->openReader(response, request);
+            client->openReader(*response, request);
         } catch (apache::thrift::transport::TTransportException& e) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
             RETURN_IF_ERROR(client.reopen());
-            client->openReader(response, request);
+            client->openReader(*response, request);
         }
     } catch (apache::thrift::TException& e) {
         std::stringstream ss;
@@ -101,21 +103,21 @@ Status BrokerReader::open() {
         return Status::RpcError(ss.str());
     }
 
-    if (response.opStatus.statusCode != TBrokerOperationStatusCode::OK) {
+    if (response->opStatus.statusCode != TBrokerOperationStatusCode::OK) {
         std::stringstream ss;
         ss << "Open broker reader failed, broker:" << broker_addr
-           << " failed:" << response.opStatus.message;
+           << " failed:" << response->opStatus.message;
         LOG(WARNING) << ss.str();
         return Status::InternalError(ss.str());
     }
     // TODO(cmy): The file size is no longer got from openReader() method.
     // But leave the code here for compatibility.
     // This will be removed later.
-    if (response.__isset.size) {
-        _file_size = response.size;
+    if (response->__isset.size) {
+        _file_size = response->size;
     }
 
-    _fd = response.fd;
+    _fd = response->fd;
     _is_fd_valid = true;
     return Status::OK();
 }
