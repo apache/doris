39d087c5271fc16ffef347ddd789d24c86788cbe
HappenLee <happenlee@hotmail.com>
2022-09-17 17:02:43 +0800

    [Opt](hash) Speed up insert from dict data map and not datetime (#12670)
    Speed up dict data read and not datetime. same target #12636

--- be/src/vec/columns/column_vector.h
+++ be/src/vec/columns/column_vector.h
@@ -198,14 +198,14 @@ public:
         use by date, datetime, basic type
     */
     void insert_many_fix_len_data(const char* data_ptr, size_t num) override {
-        if constexpr (std::is_same_v<T, vectorized::Int128>) {
+        if constexpr (!std::is_same_v<T, vectorized::Int64>) {
             insert_many_in_copy_way(data_ptr, num);
         } else if (IColumn::is_date) {
             insert_date_column(data_ptr, num);
         } else if (IColumn::is_date_time) {
             insert_datetime_column(data_ptr, num);
         } else {
-            insert_many_default_type(data_ptr, num);
+            insert_many_in_copy_way(data_ptr, num);
         }
     }
 
