0c124a6806ba42ebf14e220b99d5750eb7e9c9b9
Liqf <109049295+LemonLiTree@users.noreply.github.com>
2022-09-19 09:22:53 +0800

    [test](time_add or sub)add time_add and time_sub funcation case #12641

--- regression-test/suites/query_p0/sql_functions/datetime_functions/test_date_function.groovy
+++ regression-test/suites/query_p0/sql_functions/datetime_functions/test_date_function.groovy
@@ -311,4 +311,52 @@ suite("test_date_function") {
                       length(cast(current_timestamp(1) as string)), length(cast(current_timestamp(2) as string)),
                       length(cast(current_timestamp(3) as string)), length(cast(current_timestamp(4) as string)),
                       length(cast(current_timestamp(5) as string)), length(cast(current_timestamp(6) as string)); """
+
+
+   tableName = "test_time_add_sub_function"
+
+    sql """ DROP TABLE IF EXISTS ${tableName} """
+    sql """
+            CREATE TABLE IF NOT EXISTS ${tableName} (
+                test_time datetime NULL COMMENT ""
+            ) ENGINE=OLAP
+            DUPLICATE KEY(test_time)
+            COMMENT "OLAP"
+            DISTRIBUTED BY HASH(test_time) BUCKETS 1
+            PROPERTIES (
+                "replication_allocation" = "tag.location.default: 1",
+                "in_memory" = "false",
+                "storage_format" = "V2"
+            )
+        """
+    sql """ insert into ${tableName} values ("2019-08-01 13:21:03") """
+    //years_add 
+    qt_sql """ select years_add(test_time,1) result from ${tableName}; """
+    //months_add 
+    qt_sql """ select months_add(test_time,1) result from ${tableName}; """
+    //weeks_add 
+    qt_sql """ select weeks_add(test_time,1) result from ${tableName}; """
+    //days_add 
+    qt_sql """ select days_add(test_time,1) result from ${tableName}; """
+    //hours_add 
+    qt_sql """ select hours_add(test_time,1) result from ${tableName}; """
+    //minutes_add 
+    qt_sql """ select minutes_add(test_time,1) result from ${tableName}; """
+    //seconds_add 
+    qt_sql """ select seconds_add(test_time,1) result from ${tableName}; """
+
+    //years_sub 
+    qt_sql """ select years_sub(test_time,1) result from ${tableName}; """
+    //months_sub 
+    qt_sql """ select months_sub(test_time,1) result from ${tableName}; """
+    //weeks_sub 
+    qt_sql """ select weeks_sub(test_time,1) result from ${tableName}; """
+    //days_sub 
+    qt_sql """ select days_sub(test_time,1) result from ${tableName}; """
+    //hours_sub 
+    qt_sql """ select hours_sub(test_time,1) result from ${tableName}; """
+    //minutes_sub 
+    qt_sql """ select minutes_sub(test_time,1) result from ${tableName}; """
+    //seconds_sub 
+    qt_sql """ select seconds_sub(test_time,1) result from ${tableName}; """
 }
