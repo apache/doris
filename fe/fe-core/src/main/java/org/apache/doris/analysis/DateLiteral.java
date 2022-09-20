a9f3e328a98819cba35ea9a7c7ca58818b576689
Gabriel <gabrielleebuaa@gmail.com>
2022-09-20 21:09:26 +0800

    [Bug](date) Fix wrong result produced by date function (#12720)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/DateLiteral.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/DateLiteral.java
@@ -600,7 +600,11 @@ public class DateLiteral extends LiteralExpr {
 
     @Override
     public long getLongValue() {
-        return (year * 10000 + month * 100 + day) * 1000000L + hour * 10000 + minute * 100 + second;
+        if (this.getType().isDate() || this.getType().isDateV2()) {
+            return year * 10000 + month * 100 + day;
+        } else {
+            return (year * 10000 + month * 100 + day) * 1000000L + hour * 10000 + minute * 100 + second;
+        }
     }
 
     @Override
