86fde57b88e174c13add5d76a7d181755b23002d
jackwener <jakevingoo@gmail.com>
2022-09-20 11:08:56 +0800

    check child

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/Validator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/Validator.java
@@ -42,6 +42,7 @@ public class Validator extends PlanPostProcessor {
         // List<NamedExpression> projects = project.getProjects();
         // Set<Slot> childOutputSet = child.getOutputSet();
 
+        child.accept(this, context);
         return project;
     }
 
@@ -58,6 +59,7 @@ public class Validator extends PlanPostProcessor {
             Preconditions.checkState(childOutputSet.contains(slot));
         }
 
+        child.accept(this, context);
         return filter;
     }
 }
