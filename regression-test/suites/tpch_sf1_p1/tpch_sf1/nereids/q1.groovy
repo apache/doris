7b46e2400f6f25a0c658b2f1bd89adfac0b20c78
morrySnow <101034200+morrySnow@users.noreply.github.com>
2022-09-21 12:18:37 +0800

    [enhancement](Nereids) add all necessary PhysicalDistribute on Join's child to ensure get correct cost (#12483)
    In an earlier PR #11976 , we add shuffle join and bucket shuffle support. But if join's right child's distribution spec satisfied join's require, we do not add distribute on right child. Instead of, do it in plan translator.
    It is hard to calculate accurate cost in this way, since we some distribute cost do not calculated.
    In this PR, we introduce a new shuffle type BUCKET, and change the way of add enforce to ensure all necessary distribute will be added in cost and enforcer job.

--- regression-test/suites/tpch_sf1_p1/tpch_sf1/nereids/q1.groovy
+++ regression-test/suites/tpch_sf1_p1/tpch_sf1/nereids/q1.groovy
@@ -25,10 +25,7 @@ suite("tpch_sf1_q1_nereids") {
     sql "use ${realDb}"
 
     sql 'set enable_nereids_planner=true'
-
-    sql 'set disable_colocate_plan=true'
-
-    sql 'set enable_bucket_shuffle_join=false'
+    sql 'set enable_fallback_to_original_planner=false'
 
     qt_select """
     select
