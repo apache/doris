d2257b99c717b9eb444e17add38de374e67ec53e
dinggege <dinggege1024@gmail.com>
2022-09-21 11:19:29 +0800

    fix(fe): fix unuse imports

--- fe/fe-core/src/test/java/org/apache/doris/http/DorisHttpTestCase.java
+++ fe/fe-core/src/test/java/org/apache/doris/http/DorisHttpTestCase.java
@@ -65,13 +65,11 @@ import mockit.Mocked;
 import okhttp3.Credentials;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.nio.file.Files;
