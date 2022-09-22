df54e246330436478133328de8168c6e9952f7c9
starocean999 <40539150+starocean999@users.noreply.github.com>
2022-09-22 09:59:30 +0800

    [enhancement](like)pass data to like function in block not in row (#12825)
    The like predicate process data in block perform better than in row. Currently, only not null column is optimized, nullable column will be handled later.
    
    SELECT COUNT(*) FROM hits WHERE URL LIKE '%google%';
    before: ~680ms
    after: ~570ms

--- be/src/olap/like_column_predicate.h
+++ be/src/olap/like_column_predicate.h
@@ -118,23 +118,32 @@ private:
                 }
             } else {
                 if (column.is_column_dictionary()) {
-                    auto* nested_col_ptr = vectorized::check_and_get_column<
-                            vectorized::ColumnDictionary<vectorized::Int32>>(column);
-                    auto& data_array = nested_col_ptr->get_data();
-                    for (uint16_t i = 0; i < size; i++) {
-                        StringValue cell_value = nested_col_ptr->get_value(data_array[i]);
-                        if constexpr (is_and) {
-                            unsigned char flag = 0;
-                            (_state->function)(
-                                    const_cast<vectorized::LikeSearchState*>(&_like_state),
-                                    cell_value, pattern, &flag);
-                            flags[i] &= _opposite ^ flag;
-                        } else {
-                            unsigned char flag = 0;
-                            (_state->function)(
-                                    const_cast<vectorized::LikeSearchState*>(&_like_state),
-                                    cell_value, pattern, &flag);
-                            flags[i] = _opposite ^ flag;
+                    if (LIKELY(_like_state.search_string_sv.len > 0)) {
+                        auto* nested_col_ptr = vectorized::check_and_get_column<
+                                vectorized::ColumnDictionary<vectorized::Int32>>(column);
+                        auto& data_array = nested_col_ptr->get_data();
+                        StringValue values[size];
+                        unsigned char temp_flags[size];
+                        for (uint16_t i = 0; i != size; i++) {
+                            values[i] = nested_col_ptr->get_value(data_array[i]);
+                        }
+                        (_state->function_vec_dict)(
+                                const_cast<vectorized::LikeSearchState*>(&_like_state), pattern,
+                                values, size, temp_flags);
+                        for (uint16_t i = 0; i < size; i++) {
+                            if constexpr (is_and) {
+                                flags[i] &= _opposite ^ temp_flags[i];
+                            } else {
+                                flags[i] = _opposite ^ temp_flags[i];
+                            }
+                        }
+                    } else {
+                        for (uint16_t i = 0; i < size; i++) {
+                            if constexpr (is_and) {
+                                flags[i] &= _opposite ^ true;
+                            } else {
+                                flags[i] = _opposite ^ true;
+                            }
                         }
                     }
                 } else {
