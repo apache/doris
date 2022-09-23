9b80ad37d9d979b6f44d2c4627f2de140a4f7ec1
morrySnow <101034200+morrySnow@users.noreply.github.com>
2022-09-23 11:00:31 +0800

    [test](Nereids) add TPC-H Q2 as regression test case (#12840)

--- regression-test/suites/tpch_sf1_p1/tpch_sf1/nereids/q5.groovy
+++ regression-test/suites/tpch_sf1_p1/tpch_sf1/nereids/q5.groovy
@@ -56,7 +56,7 @@ suite("tpch_sf1_q5_nereids") {
 
     qt_select """
 
-    select /*+SET_VAR(exec_mem_limit=8589934592, parallel_fragment_exec_instance_num=8, enable_vectorized_engine=true, batch_size=4096, disable_join_reorder=false, enable_cost_based_join_reorder=false, enable_projection=true) */
+    select /*+SET_VAR(exec_mem_limit=8589934592, parallel_fragment_exec_instance_num=2, enable_vectorized_engine=true, batch_size=4096, disable_join_reorder=false, enable_cost_based_join_reorder=false, enable_projection=true) */
         n_name,
         sum(l_extendedprice * (1 - l_discount)) as revenue
     from
