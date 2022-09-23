a5320fd1f783be9cee9d963a95a9994fb015bad9
HappenLee <happenlee@hotmail.com>
2022-09-23 18:29:54 +0800

    [Opt](Vectorized) Support push down no grouping agg (#12803)
    Support push down no grouping agg

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -147,12 +147,14 @@ Status NewOlapScanner::_init_tablet_reader_params(
                       ->rowset()
                       ->rowset_meta()
                       ->is_segments_overlapping());
-
+    auto real_parent = reinterpret_cast<NewOlapScanNode*>(_parent);
     if (_state->skip_storage_engine_merge()) {
         _tablet_reader_params.direct_mode = true;
         _aggregation = true;
     } else {
-        _tablet_reader_params.direct_mode = _aggregation || single_version;
+        _tablet_reader_params.direct_mode =
+                _aggregation || single_version ||
+                real_parent->_olap_scan_node.__isset.push_down_agg_type_opt;
     }
 
     RETURN_IF_ERROR(_init_return_columns(!_tablet_reader_params.direct_mode));
@@ -161,6 +163,9 @@ Status NewOlapScanner::_init_tablet_reader_params(
     _tablet_reader_params.tablet_schema = _tablet_schema;
     _tablet_reader_params.reader_type = READER_QUERY;
     _tablet_reader_params.aggregation = _aggregation;
+    if (real_parent->_olap_scan_node.__isset.push_down_agg_type_opt)
+        _tablet_reader_params.push_down_agg_type_opt =
+                real_parent->_olap_scan_node.push_down_agg_type_opt;
     _tablet_reader_params.version = Version(0, _version);
 
     // Condition
