a038121f9986d83db88b11b8efa69a26a6eaa1cd
Shuo Wang <wangshuo128@gmail.com>
2022-09-23 16:27:18 +0800

    fix import

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/mv/SelectRollupWithAggregate.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/mv/SelectRollupWithAggregate.java
@@ -52,7 +52,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 
 import java.util.Comparator;
 import java.util.List;
