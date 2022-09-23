d7a14508014b4b5398ac7cc5e3716f63a1132ff4
Jerry Hu <mrhhsg@gmail.com>
2022-09-23 18:06:22 +0800

    [chore](config) increase minimum thread num of some thread pool (#12925)

--- be/src/runtime/exec_env_init.cpp
+++ be/src/runtime/exec_env_init.cpp
@@ -120,19 +120,19 @@ Status ExecEnv::_init(const std::vector<StorePath>& store_paths) {
                                    config::doris_remote_scanner_thread_pool_queue_size);
 
     ThreadPoolBuilder("LimitedScanThreadPool")
-            .set_min_threads(1)
+            .set_min_threads(config::doris_scanner_thread_pool_thread_num)
             .set_max_threads(config::doris_scanner_thread_pool_thread_num)
             .set_max_queue_size(config::doris_scanner_thread_pool_queue_size)
             .build(&_limited_scan_thread_pool);
 
     ThreadPoolBuilder("SendBatchThreadPool")
-            .set_min_threads(1)
+            .set_min_threads(config::send_batch_thread_pool_thread_num)
             .set_max_threads(config::send_batch_thread_pool_thread_num)
             .set_max_queue_size(config::send_batch_thread_pool_queue_size)
             .build(&_send_batch_thread_pool);
 
     ThreadPoolBuilder("DownloadCacheThreadPool")
-            .set_min_threads(1)
+            .set_min_threads(config::download_cache_thread_pool_thread_num)
             .set_max_threads(config::download_cache_thread_pool_thread_num)
             .set_max_queue_size(config::download_cache_thread_pool_queue_size)
             .build(&_download_cache_thread_pool);
