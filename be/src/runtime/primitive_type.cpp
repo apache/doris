de26a66ec62be21cb0a842209caf57d8f3b32a6d
Shane <wzy545@vip.qq.com>
2022-09-25 14:06:49 +0800

    [feature](JSON datatype)Support JSON datatype (#10322)
    Add `JSON` datatype, following features are implemented by this PR:
    1. `CREATE` tables with `JSON` type columns
    2. `INSERT` values containing `JSON` type value stored in `String`, which is represented as binary format(AKA `JSONB`) at BE 
    3. `SELECT` JSON columns
    
    Detail design refers [DSIP-016: Support JSON type](https://cwiki.apache.org/confluence/display/DORIS/DSIP-016%3A+Support+JSON+type)
    
    * add JSONB data storage format type
    
    * fix JsonLiteral resolve bug
    
    * add DataTypeJson case in data_type_factory
    
    * add JSON syntax check in FE
    
    * add operators for jsonb_document, currently not support comparison between any JSON type value
    
    * add ColumnJson and DataTypeJson
    
    * add JsonField to store JsonValue
    
    * add JsonValue to convert String JSON to BINARY JSON and JsonLiteral case for vliteral
    
    * add push_json for MysqlResultWriter
    
    * JSON column need no zone_map_index
    
    * Revert "JSON column need no zone_map_index"
    
    This reverts commit f71d1ce1ded9dbae44a5d58abcec338816b70d79.
    
    * add JSON writer and reader, ignore zone-map for JSON column
    
    * add json_to_string for DataTypeJson
    
    * add olap_data_convertor for JSON type
    
    * add some enum
    
    * add OLAP_FIELD_TYPE_JSON type, FieldTypeTraits for it and corresponding cases or functions
    
    * fix column_json offsets overflow bug, format code
    
    * remove useless TODOs, add CmpType cases for JSON type
    
    * add license header
    
    * format license
    
    * format be codes
    
    * resolve rebase master conflicts
    
    * fix bugs for CREATE and meta related code
    
    * refactor JsonValue constructors, add fe JSON cases and fix some bugs, reformat codes
    
    * modification be codes along code review advice
    
    * fix rebase conflicts with master
    
    * add unit test for json_value and column_json
    
    * fix rebase error
    
    * rename json to jsonb
    
    * fix some data convert bugs, set Mysql type to JSON

--- be/src/runtime/primitive_type.cpp
+++ be/src/runtime/primitive_type.cpp
@@ -21,6 +21,7 @@
 
 #include "gen_cpp/Types_types.h"
 #include "runtime/collection_value.h"
+#include "runtime/jsonb_value.h"
 #include "runtime/string_value.h"
 
 namespace doris {
@@ -77,6 +78,8 @@ PrimitiveType convert_type_to_primitive(FunctionContext::Type type) {
         return PrimitiveType::TYPE_DATETIMEV2;
     case FunctionContext::Type::TYPE_TIMEV2:
         return PrimitiveType::TYPE_TIMEV2;
+    case FunctionContext::Type::TYPE_JSONB:
+        return PrimitiveType::TYPE_JSONB;
     default:
         DCHECK(false);
     }
@@ -277,6 +280,9 @@ PrimitiveType thrift_to_type(TPrimitiveType::type ttype) {
     case TPrimitiveType::STRING:
         return TYPE_STRING;
 
+    case TPrimitiveType::JSONB:
+        return TYPE_JSONB;
+
     case TPrimitiveType::BINARY:
         return TYPE_BINARY;
 
@@ -368,6 +374,9 @@ TPrimitiveType::type to_thrift(PrimitiveType ptype) {
     case TYPE_STRING:
         return TPrimitiveType::STRING;
 
+    case TYPE_JSONB:
+        return TPrimitiveType::JSONB;
+
     case TYPE_BINARY:
         return TPrimitiveType::BINARY;
 
@@ -458,6 +467,9 @@ std::string type_to_string(PrimitiveType t) {
 
     case TYPE_STRING:
         return "STRING";
+
+    case TYPE_JSONB:
+        return "JSONB";
 
     case TYPE_BINARY:
         return "BINARY";
@@ -551,6 +563,9 @@ std::string type_to_odbc_string(PrimitiveType t) {
     case TYPE_STRING:
         return "string";
 
+    case TYPE_JSONB:
+        return "jsonb";
+
     case TYPE_BINARY:
         return "binary";
 
@@ -621,6 +636,8 @@ int get_slot_size(PrimitiveType type) {
     case TYPE_HLL:
     case TYPE_QUANTILE_STATE:
         return sizeof(StringValue);
+    case TYPE_JSONB:
+        return sizeof(JsonBinaryValue);
     case TYPE_ARRAY:
         return sizeof(CollectionValue);
 
