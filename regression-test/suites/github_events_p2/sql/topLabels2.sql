60556070bbc932e74cab59d1b7499398b7b0378d
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2022-09-25 23:46:15 +0800

    [enhancement](test) add github events cases to p2 (#12696)

---
+++ regression-test/suites/github_events_p2/sql/topLabels2.sql
@@ -0,0 +1,9 @@
+-- SELECT
+--     label,
+--     count() AS c
+-- FROM github_events
+-- LATERAL VIEW explode_split(labels, ',') t AS label
+-- WHERE (event_type IN ('IssuesEvent', 'PullRequestEvent', 'IssueCommentEvent')) AND (action IN ('created', 'opened', 'labeled')) AND ((lower(label) LIKE '%bug%') OR (lower(label) LIKE '%feature%'))
+-- GROUP BY label
+-- ORDER BY c DESC
+-- LIMIT 50
