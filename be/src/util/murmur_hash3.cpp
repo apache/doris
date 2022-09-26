060d2ec6c11cdd64b79d4d6f02abd1f254bdd1f7
TengJianPing <18241664+jacktengg@users.noreply.github.com>
2022-09-26 14:23:37 +0800

    [function](hash) add support of murmur_hash3_64 (#12923)

--- be/src/util/murmur_hash3.cpp
+++ be/src/util/murmur_hash3.cpp
@@ -31,11 +31,11 @@
 
 #define FORCE_INLINE inline __attribute__((always_inline))
 
-inline uint32_t rotl32(uint32_t x, int8_t r) {
+FORCE_INLINE uint32_t rotl32(uint32_t x, int8_t r) {
     return (x << r) | (x >> (32 - r));
 }
 
-inline uint64_t rotl64(uint64_t x, int8_t r) {
+FORCE_INLINE uint64_t rotl64(uint64_t x, int8_t r) {
     return (x << r) | (x >> (64 - r));
 }
 
