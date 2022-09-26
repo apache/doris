bb85e54c762c691cac96288e9b5e9fe3de7aa02f
shee <13843187+qzsee@users.noreply.github.com>
2022-09-26 17:16:23 +0800

    [feature](Nereids) constant expression folding (#12151)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/LessThanEqual.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/LessThanEqual.java
@@ -18,6 +18,7 @@
 package org.apache.doris.nereids.trees.expressions;
 
 import org.apache.doris.nereids.exceptions.UnboundException;
+import org.apache.doris.nereids.trees.expressions.functions.PropagateNullable;
 import org.apache.doris.nereids.trees.expressions.visitor.ExpressionVisitor;
 
 import com.google.common.base.Preconditions;
@@ -27,7 +28,7 @@ import java.util.List;
 /**
  * Less than and equal expression: a <= b.
  */
-public class LessThanEqual extends ComparisonPredicate {
+public class LessThanEqual extends ComparisonPredicate implements PropagateNullable {
     /**
      * Constructor of Less Than And Equal.
      *
