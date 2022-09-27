20f09af384ccfa1f7a648846cd9a355a6ec067ff
jackwener <jakevingoo@gmail.com>
2022-09-27 20:31:30 +0800

    add filter when cross include otherJoinCondition

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/PushFilterInsideJoin.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/PushFilterInsideJoin.java
@@ -20,20 +20,14 @@ package org.apache.doris.nereids.rules.rewrite.logical;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
 import org.apache.doris.nereids.rules.rewrite.OneRewriteRuleFactory;
-import org.apache.doris.nereids.trees.expressions.ComparisonPredicate;
-import org.apache.doris.nereids.trees.expressions.EqualTo;
 import org.apache.doris.nereids.trees.expressions.Expression;
-import org.apache.doris.nereids.trees.expressions.Slot;
-import org.apache.doris.nereids.trees.expressions.SlotReference;
 import org.apache.doris.nereids.trees.plans.GroupPlan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalJoin;
 import org.apache.doris.nereids.util.ExpressionUtils;
-import org.apache.doris.nereids.util.PlanUtils;
 
 import com.google.common.collect.Lists;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * Push the predicate in the LogicalFilter to the join children.
@@ -45,47 +39,14 @@ public class PushFilterInsideJoin extends OneRewriteRuleFactory {
     public Rule build() {
         return logicalFilter(logicalJoin())
                 // TODO: current just handle cross/inner join.
-                .when(filter -> filter.child().getJoinType().isCrossJoin() || filter.child().getJoinType()
-                        .isInnerJoin())
+                .when(filter -> filter.child().getJoinType().isCrossJoin()
+                        || filter.child().getJoinType().isInnerJoin())
                 .then(filter -> {
+                    List<Expression> otherConditions = Lists.newArrayList(filter.getPredicates());
                     LogicalJoin<GroupPlan, GroupPlan> join = filter.child();
-                    List<Expression> predicates = ExpressionUtils.extractConjunction(filter.getPredicates());
-                    Set<Slot> leftOutputs = join.left().getOutputSet();
-                    Set<Slot> rightOutputs = join.right().getOutputSet();
-
-                    // TODO: current just handle col1 > col2 (ComparisonPredicate but not equal).
-                    List<Expression> otherConditions = Lists.newArrayList();
-                    join.getOtherJoinCondition().ifPresent(otherConditions::add);
-                    List<Expression> remainingPredicates = Lists.newArrayList();
-                    for (Expression predicate : predicates) {
-                        if (predicate instanceof ComparisonPredicate) {
-                            if (predicate instanceof EqualTo) {
-                                remainingPredicates.add(predicate);
-                                continue;
-                            }
-                        }
-                        ComparisonPredicate comparisonPredicate = (ComparisonPredicate) predicate;
-                        Set<Slot> leftSlots = comparisonPredicate.left().collect(SlotReference.class::isInstance);
-                        Set<Slot> rightSlots = comparisonPredicate.right().collect(SlotReference.class::isInstance);
-
-                        if (leftSlots.size() == 0 || rightSlots.size() == 0) {
-                            remainingPredicates.add(predicate);
-                            continue;
-                        }
-
-                        if ((leftOutputs.containsAll(leftSlots) && rightOutputs.containsAll(rightSlots))
-                                || (leftOutputs.containsAll(rightSlots) && rightOutputs.containsAll(leftSlots))) {
-                            otherConditions.add(predicate);
-                        }
-                    }
-
-                    if (predicates.size() == remainingPredicates.size()) {
-                        return null;
-                    }
-
-                    return PlanUtils.filterOrSelf(remainingPredicates,
-                            new LogicalJoin<>(join.getJoinType(), join.getHashJoinConjuncts(),
-                                    ExpressionUtils.optionalAnd(otherConditions), join.left(), join.right()));
+                    join.getOtherJoinCondition().map(otherConditions::add);
+                    return new LogicalJoin<>(join.getJoinType(), join.getHashJoinConjuncts(),
+                            ExpressionUtils.optionalAnd(otherConditions), join.left(), join.right());
                 }).toRule(RuleType.PUSH_FILTER_INSIDE_JOIN);
     }
 }
