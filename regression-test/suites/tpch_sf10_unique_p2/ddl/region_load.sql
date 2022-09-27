eba71cf5dadc73437925ee36d8ee8acbe1ba7154
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2022-09-27 17:12:37 +0800

    [enhancement](test) add tpch_sf10 cases to p2 (#12698)

---
+++ regression-test/suites/tpch_sf10_unique_p2/ddl/region_load.sql
@@ -0,0 +1,6 @@
+LOAD LABEL ${loadLabel} (
+    DATA INFILE("s3://${s3BucketName}/regression/tpch/sf10/region.tbl")
+    INTO TABLE region
+    COLUMNS TERMINATED BY "|"
+    (r_regionkey, r_name, r_comment, temp)
+)
