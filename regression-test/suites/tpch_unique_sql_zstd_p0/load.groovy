6fc85c1653d37a463cb4516b712eac2afb32b681
Jerry Hu <mrhhsg@gmail.com>
2022-09-28 11:47:19 +0800

    [chore](regression-test) add default group(p0) for regression-test (#12977)

--- regression-test/suites/tpch_unique_sql_zstd_p0/load.groovy
+++ regression-test/suites/tpch_unique_sql_zstd_p0/load.groovy
@@ -54,7 +54,7 @@ suite("load") {
 
             // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
             // also, you can stream load a http stream, e.g. http://xxx/some.csv
-            file """${context.sf1DataPath}/tpch/sf0.1/${tableName}.tbl.gz"""
+            file """${context.sf1DataPath}/regression/tpch/sf0.1/${tableName}.tbl.gz"""
 
             time 10000 // limit inflight 10s
 
