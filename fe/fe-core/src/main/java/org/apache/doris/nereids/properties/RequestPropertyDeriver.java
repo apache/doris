f020d587c3a91cd581317e0441da2d1daba669a6
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2022-09-29 16:01:10 +0800

    [enhancement](Nereids) planner performance speed up (#12858)
    optimize planner by:
    1. reduce duplicated calculation on equals, getOutput, computeOutput eq.
    2. getOnClauseUsedSlots: the two side of equalTo is centainly slot, so not need to use List.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/properties/RequestPropertyDeriver.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/properties/RequestPropertyDeriver.java
@@ -69,8 +69,9 @@ public class RequestPropertyDeriver extends PlanVisitor<Void, PlanContext> {
 
     @Override
     public Void visit(Plan plan, PlanContext context) {
-        List<PhysicalProperties> requiredPropertyList = Lists.newArrayList();
-        for (int i = 0; i < context.getGroupExpression().arity(); i++) {
+        List<PhysicalProperties> requiredPropertyList =
+                Lists.newArrayListWithCapacity(context.getGroupExpression().arity());
+        for (int i = context.getGroupExpression().arity(); i > 0; --i) {
             requiredPropertyList.add(PhysicalProperties.ANY);
         }
         requestPropertyToChildren.add(requiredPropertyList);
