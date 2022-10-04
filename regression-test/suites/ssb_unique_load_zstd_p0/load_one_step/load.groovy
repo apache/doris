c4be3881db6b8703baf9d6ffda6bffc20a6e4560
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2022-10-04 20:56:22 +0800

    [fix](test) try to  let cases run in parallel (#13114)

--- regression-test/suites/ssb_unique_load_zstd_p0/load_one_step/load.groovy
+++ regression-test/suites/ssb_unique_load_zstd_p0/load_one_step/load.groovy
@@ -15,45 +15,22 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Most of the cases are copied from https://github.com/trinodb/trino/tree/master
-// /testing/trino-product-tests/src/main/resources/sql-tests/testcases
-// and modified by Doris.
 
-// Note: To filter out tables from sql files, use the following one-liner comamnd
-// sed -nr 's/.*tables: (.*)$/\1/gp' /path/to/*.sql | sed -nr 's/,/\n/gp' | sort | uniq
-suite("load") {
-    def tables = ["customer", "lineorder", "part", "date", "supplier"]
-    def columns = ["""c_custkey,c_name,c_address,c_city,c_nation,c_region,c_phone,c_mktsegment,no_use""",
-                    """lo_orderkey,lo_linenumber,lo_custkey,lo_partkey,lo_suppkey,lo_orderdate,lo_orderpriority, 
+suite("load_one_step") {
+    def tables = ["customer": ["""c_custkey,c_name,c_address,c_city,c_nation,c_region,c_phone,c_mktsegment,no_use""", 3000], "lineorder": ["""lo_orderkey,lo_linenumber,lo_custkey,lo_partkey,lo_suppkey,lo_orderdate,lo_orderpriority, 
                     lo_shippriority,lo_quantity,lo_extendedprice,lo_ordtotalprice,lo_discount, 
-                    lo_revenue,lo_supplycost,lo_tax,lo_commitdate,lo_shipmode,lo_dummy""",
-                    """p_partkey,p_name,p_mfgr,p_category,p_brand,p_color,p_type,p_size,p_container,p_dummy""",
-                    """d_datekey,d_date,d_dayofweek,d_month,d_year,d_yearmonthnum,d_yearmonth,
+                    lo_revenue,lo_supplycost,lo_tax,lo_commitdate,lo_shipmode,lo_dummy""", 600572], "part": ["""p_partkey,p_name,p_mfgr,p_category,p_brand,p_color,p_type,p_size,p_container,p_dummy""", 20000], "date": ["""d_datekey,d_date,d_dayofweek,d_month,d_year,d_yearmonthnum,d_yearmonth,
                     d_daynuminweek,d_daynuminmonth,d_daynuminyear,d_monthnuminyear,d_weeknuminyear,
-                    d_sellingseason,d_lastdayinweekfl,d_lastdayinmonthfl,d_holidayfl,d_weekdayfl,d_dummy""",
-                    """s_suppkey,s_name,s_address,s_city,s_nation,s_region,s_phone,s_dummy"""]
+                    d_sellingseason,d_lastdayinweekfl,d_lastdayinmonthfl,d_holidayfl,d_weekdayfl,d_dummy""", 255], "supplier": ["""s_suppkey,s_name,s_address,s_city,s_nation,s_region,s_phone,s_dummy""", 200]]
 
-    for (String table in tables) {
-        sql new File("""${context.file.parent}/ddl/${table}_create.sql""").text
-        sql new File("""${context.file.parent}/ddl/${table}_delete.sql""").text
-    }
-    i = 0
-    for (String tableName in tables) {   
+    tables.each { tableName, rows ->
+        sql """ DROP TABLE IF EXISTS $tableName """
+        sql new File("""${context.file.parentFile.parent}/ddl/${tableName}_create.sql""").text
         streamLoad {
-            // a default db 'regression_test' is specified in
-            // ${DORIS_HOME}/conf/regression-conf.groovy
             table tableName
-
-            // default label is UUID:
-            // set 'label' UUID.randomUUID().toString()
-
-            // default column_separator is specify in doris fe config, usually is '\t'.
-            // this line change to ','
             set 'column_separator', '|'
             set 'compress_type', 'GZ'
-            set 'columns', columns[i]
-            // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
-            // also, you can stream load a http stream, e.g. http://xxx/some.csv
+            set 'columns', rows[0]
             file """${context.sf1DataPath}/ssb/sf0.1/${tableName}.tbl.gz"""
 
             time 10000 // limit inflight 10s
@@ -73,6 +50,11 @@ suite("load") {
                 assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
             }
         }
-        i++
+        sql 'sync'
+        for (int i = 1; i <= 5; i++) {
+            def loadRowCount = sql "select count(1) from ${tableName}"
+            logger.info("select ${tableName} numbers: ${loadRowCount[0][0]}".toString())
+            assertTrue(loadRowCount[0][0] == rows[1])
+        }
     }
 }
