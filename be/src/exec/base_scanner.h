d286aa7bf7f0585fc1e4e9464e69eb8f378d6d40
Mingyu Chen <morningman.cmy@gmail.com>
2022-10-05 23:00:56 +0800

    [fix](spark-load) no need to filter row group when doing spark load (#13116)
    1. Fix issue #13115 
    2. Modify the method of `get_next_block` or `GenericReader`, to return "read_rows" explicitly.
        Some columns in block may not be filled in reader, if the first column is not filled, use `block->rows()` can not return real row numbers.
    3. Add more checks for broker load test cases.

--- be/src/exec/base_scanner.h
+++ be/src/exec/base_scanner.h
@@ -153,7 +153,7 @@ protected:
     int _num_of_columns_from_file;
 
     // slot_ids for parquet predicate push down are in tuple desc
-    TupleId _tupleId;
+    TupleId _tupleId = -1;
     std::vector<ExprContext*> _conjunct_ctxs;
 
 private:
