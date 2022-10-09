b9b030193f5eec96e6edd7b2eb5865c4d80eedb3
yiguolei <yiguolei@gmail.com>
2022-10-09 18:31:18 +0800

    fix shell format

--- build.sh
+++ build.sh
@@ -99,9 +99,9 @@ clean_fe() {
 
 # Copy the common files like licenses, notice.txt to output folder
 function copy_common_files() {
-    cp -r -p ${DORIS_HOME}/NOTICE.txt "$1/"
-    cp -r -p ${DORIS_HOME}/dist/LICENSE-dist.txt "$1/"
-    cp -r -p ${DORIS_HOME}/dist/licenses "$1/"
+    cp -r -p "${DORIS_HOME}/NOTICE.txt" "$1/"
+    cp -r -p "${DORIS_HOME}/dist/LICENSE-dist.txt" "$1/"
+    cp -r -p "${DORIS_HOME}/dist/licenses" "$1/"
 }
 
 if ! OPTS="$(getopt \
@@ -453,7 +453,7 @@ if [[ "${BUILD_FE}" -eq 1 ]]; then
     cp -r -p "${DORIS_HOME}/webroot/static" "${DORIS_OUTPUT}/fe/webroot"/
 
     cp -r -p "${DORIS_THIRDPARTY}/installed/webroot"/* "${DORIS_OUTPUT}/fe/webroot/static"/
-    copy_common_files ${DORIS_OUTPUT}/fe/
+    copy_common_files "${DORIS_OUTPUT}/fe/"
     mkdir -p "${DORIS_OUTPUT}/fe/log"
     mkdir -p "${DORIS_OUTPUT}/fe/doris-meta"
 fi
@@ -499,7 +499,7 @@ if [[ "${BUILD_BE}" -eq 1 ]]; then
     fi
 
     cp -r -p "${DORIS_THIRDPARTY}/installed/webroot"/* "${DORIS_OUTPUT}/be/www"/
-    copy_common_files ${DORIS_OUTPUT}/be/
+    copy_common_files "${DORIS_OUTPUT}/be/"
     mkdir -p "${DORIS_OUTPUT}/be/log"
     mkdir -p "${DORIS_OUTPUT}/be/storage"
 fi
@@ -511,7 +511,7 @@ if [[ "${BUILD_BROKER}" -eq 1 ]]; then
     ./build.sh
     rm -rf "${DORIS_OUTPUT}/apache_hdfs_broker"/*
     cp -r -p "${DORIS_HOME}/fs_brokers/apache_hdfs_broker/output/apache_hdfs_broker"/* "${DORIS_OUTPUT}/apache_hdfs_broker"/
-    copy_common_files ${DORIS_OUTPUT}/apache_hdfs_broker/
+    copy_common_files "${DORIS_OUTPUT}/apache_hdfs_broker/"
     cd "${DORIS_HOME}"
 fi
 
