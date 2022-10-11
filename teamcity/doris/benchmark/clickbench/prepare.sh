9f94bad8a36eba49514b528c83b729b0dc4c2e0e
stephen <hello_stephen@qq.com>
2022-10-11 14:24:31 +0800

    update

--- teamcity/doris/benchmark/clickbench/prepare.sh
+++ teamcity/doris/benchmark/clickbench/prepare.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -ex
 
-pullrequestID=%teamcity.pullRequest.number%
-test_branch=$pullrequestID
-source_branch=%teamcity.pullRequest.source.branch%
-target_branch=%teamcity.pullRequest.target.branch%
-build_id=%teamcity.build.id%
-pipeline_home=/home/ec2-user/teamcity/
-checkout_home=$(pwd)
+pipeline_home=${HOME}/teamcity/
+
 skip_pipeline=${skip_pipeline:=false}
 
 echo 'check if skip'
@@ -27,13 +22,3 @@ if [[ ! -d "${pipeline_home}/selectdb-qa" ]]; then
 fi
 qa_home="${pipeline_home}/selectdb-qa"
 cd "${qa_home}" && git stash && git checkout main && git pull && cd -
-
-echo 'check if exists older commits of this pull request'
-outdate_builds_of_pr=($(grep ${test_branch}_${source_branch}_${target_branch}_incubator-doris $pipeline_home/OpenSourceDorisBuild.log | awk '{print $1}'))
-for old_build_id in ${outdate_builds_of_pr[@]}; do
-    echo "STRAT checking build $old_build_id"
-    old_build_status=$(bash ../common/teamcity_api.sh --show_build_state $old_build_id)
-    if [[ $old_build_status == "running" ]]; then
-        bash teamcity_api.sh --cancel_running_build $old_build_id
-    fi
-done
