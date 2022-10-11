94b8f358f549173b80130f00d4e2b4f960ba0003
morrySnow <morrysnow@126.com>
2022-10-11 10:40:59 +0800

    [enhancement](Nereids) fallback to legacy planner when nereids throw exception

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -73,6 +73,7 @@ import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.MetaNotFoundException;
+import org.apache.doris.common.NereidsException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.Version;
 import org.apache.doris.common.util.DebugUtil;
@@ -412,7 +413,13 @@ public class StmtExecutor implements ProfileWriter {
                         context.getTracer().spanBuilder("query analysis").setParent(Context.current()).startSpan();
                 try (Scope scope = queryAnalysisSpan.makeCurrent()) {
                     // analyze this query
-                    analyze(context.getSessionVariable().toThrift());
+                    try {
+                        analyze(context.getSessionVariable().toThrift());
+                    } catch (NereidsException ignored) {
+                        // fall back to legacy planner
+                        parsedStmt = null;
+                        analyze(context.getSessionVariable().toThrift());
+                    }
                 } catch (Exception e) {
                     queryAnalysisSpan.recordException(e);
                     throw e;
@@ -589,7 +596,6 @@ public class StmtExecutor implements ProfileWriter {
 
     /**
      * get variables in stmt.
-     * TODO: only support select stmt now. need to support Nereids.
      *
      * @throws DdlException
      */
@@ -697,6 +703,9 @@ public class StmtExecutor implements ProfileWriter {
                     throw e;
                 } catch (Exception e) {
                     LOG.warn("Analyze failed. {}", context.getQueryIdentifier(), e);
+                    if (parsedStmt instanceof LogicalPlanAdapter) {
+                        throw new NereidsException();
+                    }
                     throw new AnalysisException("Unexpected exception: " + e.getMessage());
                 } finally {
                     MetaLockUtils.readUnlockTables(tables);
