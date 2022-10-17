e2274dcbcfcbeeb8dfcc1938959d3563c1b455e7
abmdocrt <Yukang.Lian2022@gmail.com>
2022-10-17 11:00:47 +0800

    [Feature](Retention) support retention function (#13056)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/AggregateFunction.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/AggregateFunction.java
@@ -50,7 +50,8 @@ public class AggregateFunction extends Function {
             "dense_rank", "multi_distinct_count", "multi_distinct_sum", "hll_union_agg", "hll_union", "bitmap_union",
             "bitmap_intersect", "orthogonal_bitmap_intersect", "orthogonal_bitmap_intersect_count", "intersect_count",
             "orthogonal_bitmap_union_count", FunctionSet.COUNT, "approx_count_distinct", "ndv",
-            FunctionSet.BITMAP_UNION_INT, FunctionSet.BITMAP_UNION_COUNT, "ndv_no_finalize", FunctionSet.WINDOW_FUNNEL);
+            FunctionSet.BITMAP_UNION_INT, FunctionSet.BITMAP_UNION_COUNT, "ndv_no_finalize", FunctionSet.WINDOW_FUNNEL,
+            FunctionSet.RETENTION);
 
     public static ImmutableSet<String> ALWAYS_NULLABLE_AGGREGATE_FUNCTION_NAME_SET =
             ImmutableSet.of("stddev_samp", "variance_samp", "var_samp", "percentile_approx");
---
+++ regression-test/suites/query_p0/sql_functions/aggregate_functions/test_aggregate_retention.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS retention_test;
+
+CREATE TABLE retention_test(
+                `uid` int COMMENT 'user id', 
+                `date` datetime COMMENT 'date time' 
+                )
+DUPLICATE KEY(uid) 
+DISTRIBUTED BY HASH(uid) BUCKETS 3 
+PROPERTIES ( 
+    "replication_num" = "1"
+); 
+
+INSERT into retention_test (uid, date) values (0, '2022-10-12'),
+                                        (0, '2022-10-13'),
+                                        (0, '2022-10-14'),
+                                        (1, '2022-10-12'),
+                                        (1, '2022-10-13'),
+                                        (2, '2022-10-12'); 
+
+SELECT * from retention_test ORDER BY uid;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12', date = '2022-10-13')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12', date = '2022-10-13', date = '2022-10-14')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
+
+SET parallel_fragment_exec_instance_num=4;
+
+SELECT * from retention_test ORDER BY uid;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12', date = '2022-10-13')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
+
+SELECT 
+    uid,     
+    retention(date = '2022-10-12', date = '2022-10-13', date = '2022-10-14')
+        AS r 
+            FROM retention_test 
+            GROUP BY uid 
+            ORDER BY uid ASC;
