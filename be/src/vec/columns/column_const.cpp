cd3450bd9d0f04b33af8dff736941915a2c1bc32
Gabriel <gabrielleebuaa@gmail.com>
2022-10-18 12:37:17 +0800

    [Improvement](join) optimize join probing phase (#13357)

--- be/src/vec/columns/column_const.cpp
+++ be/src/vec/columns/column_const.cpp
@@ -67,7 +67,8 @@ ColumnPtr ColumnConst::replicate(const Offsets& offsets) const {
     return ColumnConst::create(data, replicated_size);
 }
 
-void ColumnConst::replicate(const uint32_t* counts, size_t target_size, IColumn& column) const {
+void ColumnConst::replicate(const uint32_t* counts, size_t target_size, IColumn& column,
+                            size_t begin, int count_sz) const {
     if (s == 0) return;
     auto& res = reinterpret_cast<ColumnConst&>(column);
     res.s = s;
