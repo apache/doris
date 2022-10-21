0c47d62edf611420913521b9fd441ff58feb5ab9
Gabriel <gabrielleebuaa@gmail.com>
2022-10-21 16:38:39 +0800

    [Bug](sort) Fix bug in string sorter

--- be/src/vec/core/sort_block.h
+++ be/src/vec/core/sort_block.h
@@ -401,8 +401,9 @@ private:
                 return a.inline_value > b.inline_value ? 1
                                                        : (a.inline_value < b.inline_value ? -1 : 0);
             } else {
-                return memcmp_small_allow_overflow15(a.inline_value.data, a.inline_value.size,
-                                                     b.inline_value.data, b.inline_value.size);
+                return memcmp_small_allow_overflow15(
+                        a.inline_value.data + a.inline_value.size, a.inline_value.size,
+                        b.inline_value.data + b.inline_value.size, b.inline_value.size);
             }
         };
 
