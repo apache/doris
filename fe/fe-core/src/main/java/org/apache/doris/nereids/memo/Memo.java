2b423b2a73bf01052eb697a538357d016157bdbe
sohardforaname <organic_chemistry@foxmail.com>
2022-10-21 19:55:15 +0800

    fix-bugs

--- fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
@@ -34,7 +34,6 @@ import com.google.common.collect.Maps;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
