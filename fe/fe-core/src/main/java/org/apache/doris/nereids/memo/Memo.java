ccbbaac606eec24c20387405bfe548ac28537bb0
sohardforaname <organic_chemistry@foxmail.com>
2022-10-21 15:56:04 +0800

    fix-bugs

--- fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
@@ -377,7 +377,7 @@ public class Memo {
             return source;
         }
         List<GroupExpression> needReplaceChild = source.getParentGroupExpressions().stream().filter(
-                e -> e.getOwnerGroup() != null && !e.getOwnerGroup().equals(destination)
+                e -> !e.getOwnerGroup().equals(destination)
         ).collect(Collectors.toList());
         for (GroupExpression groupExpression : needReplaceChild) {
             groupExpressions.remove(groupExpression);
