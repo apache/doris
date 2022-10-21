c5366b83b7df2cd460d793d1cfffcb1efebb37fa
sohardforaname <organic_chemistry@foxmail.com>
2022-10-21 16:47:17 +0800

    fix-bugs

--- fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Group.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Group.java
@@ -39,7 +39,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Representation for group in cascades optimizer.
