ffdd62f2e54b31643b761c94c6dd2c48383c7740
jakevin <jakevingoo@gmail.com>
2022-10-24 17:11:43 +0800

    [improve](Nereids): ReorderJoin eliminate this recursion (#13505)

--- fe/fe-core/src/test/java/org/apache/doris/nereids/sqltest/MultiJoinTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/sqltest/MultiJoinTest.java
@@ -21,6 +21,7 @@ import org.apache.doris.nereids.rules.rewrite.logical.ReorderJoin;
 import org.apache.doris.nereids.util.PlanChecker;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,8 +30,9 @@ public class MultiJoinTest extends SqlTestBase {
     @Test
     void testMultiJoinEliminateCross() {
         List<String> sqls = ImmutableList.<String>builder()
-                .add("SELECT * FROM T1, T2 LEFT JOIN T3 ON T2.id = T3.id WHERE T1.id = T2.id")
                 .add("SELECT * FROM T2 LEFT JOIN T3 ON T2.id = T3.id, T1 WHERE T1.id = T2.id")
+                .add("SELECT * FROM T2 LEFT JOIN T3 ON T2.id = T3.id, T1 WHERE T1.id = T2.id AND T1.score > 0")
+                .add("SELECT * FROM T2 LEFT JOIN T3 ON T2.id = T3.id, T1 WHERE T1.id = T2.id AND T1.score > 0 AND T1.id + T2.id + T3.id > 0")
                 .build();
 
         for (String sql : sqls) {
@@ -45,6 +47,41 @@ public class MultiJoinTest extends SqlTestBase {
                     )
                     .printlnTree();
         }
+    }
+
+    @Test
+    @Disabled
+    // TODO: MultiJoin And EliminateOuter
+    void testEliminateBelowOuter() {
+        String sql = "SELECT * FROM T1, T2 LEFT JOIN T3 ON T2.id = T3.id WHERE T1.id = T2.id";
+        PlanChecker.from(connectContext)
+                .analyze(sql)
+                .applyBottomUp(new ReorderJoin())
+                .printlnTree();
+    }
+
+    @Test
+    void testPushdownAndEliminateOuter() {
+        String sql = "SELECT * FROM T1 LEFT JOIN T2 ON T1.id = T2.id WHERE T2.score > 0";
+        PlanChecker.from(connectContext)
+                .analyze(sql)
+                .rewrite()
+                .printlnTree()
+                .matches(
+                        logicalJoin().when(join -> join.getJoinType().isInnerJoin())
+                );
+
+        String sql1 = "SELECT * FROM T1, T2 LEFT JOIN T3 ON T2.id = T3.id WHERE T1.id = T2.id AND T3.score > 0";
+        PlanChecker.from(connectContext)
+                .analyze(sql1)
+                .rewrite()
+                .printlnTree()
+                .matches(
+                        logicalJoin(
+                                logicalJoin().when(join -> join.getJoinType().isInnerJoin()),
+                                any()
+                        ).when(join -> join.getJoinType().isInnerJoin())
+                );
     }
 
     @Test
