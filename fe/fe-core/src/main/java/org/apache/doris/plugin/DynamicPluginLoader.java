4cbf4163bf6729d3035b5fb767cea6d96ff6dc2e
wangxiangyu@360shuke.com <wangxiangyu@360shuke.com>
2022-10-25 12:37:45 +0800

    [fix](plugin) bugfix for dirty uninstallation of dynamic plugin (#13540)

--- fe/fe-core/src/main/java/org/apache/doris/plugin/DynamicPluginLoader.java
+++ fe/fe-core/src/main/java/org/apache/doris/plugin/DynamicPluginLoader.java
@@ -49,7 +49,6 @@ public class DynamicPluginLoader extends PluginLoader {
     protected Path installPath;
 
     protected String expectedMd5sum;
-
     // for processing install stmt
     DynamicPluginLoader(String pluginDir, String source, String expectedMd5sum) {
         super(pluginDir, source);
@@ -208,7 +207,7 @@ public class DynamicPluginLoader extends PluginLoader {
 
         Class<? extends Plugin> pluginClass;
         try {
-            pluginClass = loader.loadClass(pluginInfo.getClassName()).asSubclass(Plugin.class);
+             pluginClass = loader.loadClass(pluginInfo.getClassName()).asSubclass(Plugin.class);
         } catch (ClassNotFoundException e) {
             throw new UserException("Could not find plugin class [" + pluginInfo.getClassName() + "]", e);
         }
