235c105554d240a09cef3d21d1f3db3572c11198
lihangyu <15605149486@163.com>
2022-10-25 15:12:11 +0800

    [feature-array](array-type) Add array function array_enumerate (#13612)
    Add array function array_enumerate

--- be/src/vec/functions/array/function_array_register.cpp
+++ be/src/vec/functions/array/function_array_register.cpp
@@ -35,6 +35,7 @@ void register_function_array_except(SimpleFunctionFactory&);
 void register_function_array_intersect(SimpleFunctionFactory&);
 void register_function_array_slice(SimpleFunctionFactory&);
 void register_function_array_difference(SimpleFunctionFactory&);
+void register_function_array_enumerate(SimpleFunctionFactory&);
 
 void register_function_array(SimpleFunctionFactory& factory) {
     register_function_array_element(factory);
@@ -50,6 +51,7 @@ void register_function_array(SimpleFunctionFactory& factory) {
     register_function_array_intersect(factory);
     register_function_array_slice(factory);
     register_function_array_difference(factory);
+    register_function_array_enumerate(factory);
 }
 
 } // namespace doris::vectorized
