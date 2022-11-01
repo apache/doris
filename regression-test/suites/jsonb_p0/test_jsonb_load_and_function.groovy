124bb83d397237cf5988a797516dc1aaa9236e20
Kang <kxiao.tiger@gmail.com>
2022-11-01 12:52:43 +0800

    combine jsonb load and query testcase

--- regression-test/suites/jsonb_p0/test_jsonb_load_and_function.groovy
+++ regression-test/suites/jsonb_p0/test_jsonb_load_and_function.groovy
@@ -15,16 +15,82 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_jsonb_function", "p0") {
+suite("test_jsonb_load_and_function", "p0") {
     // define a sql table
-    def testTable = "regression_test_load_p0_stream_load.tbl_test_load_jsonb"
+    def testTable = "tbl_test_jsonb"
+    def dataFile = "test_jsonb.csv"
 
-    sql "set enable_vectorized_engine = true"
-    
-    // check table data
+    sql """ set enable_vectorized_engine = true """
+
+    sql "DROP TABLE IF EXISTS ${testTable}"
+
+    sql """
+        CREATE TABLE IF NOT EXISTS ${testTable} (
+            id INT,
+            j JSONB
+        )
+        DUPLICATE KEY(id)
+        DISTRIBUTED BY HASH(id) BUCKETS 10
+        PROPERTIES("replication_num" = "1");
+        """
+
+    // load the jsonb data from csv file
+    // fail by default for invalid data rows
+    streamLoad {
+        table testTable
+        
+        file dataFile // import csv file
+        time 10000 // limit inflight 10s
+
+        // if declared a check callback, the default check condition will ignore.
+        // So you must check all condition
+        check { result, exception, startTime, endTime ->
+            if (exception != null) {
+                throw exception
+            }
+            log.info("Stream load result: ${result}".toString())
+            def json = parseJson(result)
+            assertEquals("fail", json.Status.toLowerCase())
+            assertEquals("too many filtered rows", json.Message)
+            assertEquals(25, json.NumberTotalRows)
+            assertEquals(18, json.NumberLoadedRows)
+            assertEquals(7, json.NumberFilteredRows)
+            assertTrue(json.LoadBytes > 0)
+        }
+    }
+
+    // load the jsonb data from csv file
+    // success with header 'max_filter_ratio: 0.3'
+    streamLoad {
+        table testTable
+        
+        // set http request header params
+        set 'max_filter_ratio', '0.3'
+        file dataFile // import csv file
+        time 10000 // limit inflight 10s
+
+        // if declared a check callback, the default check condition will ignore.
+        // So you must check all condition
+        check { result, exception, startTime, endTime ->
+            if (exception != null) {
+                throw exception
+            }
+            log.info("Stream load result: ${result}".toString())
+            def json = parseJson(result)
+            assertEquals("success", json.Status.toLowerCase())
+            assertEquals(25, json.NumberTotalRows)
+            assertEquals(18, json.NumberLoadedRows)
+            assertEquals(7, json.NumberFilteredRows)
+            assertTrue(json.LoadBytes > 0)
+        }
+    }
+
+    // check result
     qt_select "SELECT * FROM ${testTable} ORDER BY id"
 
-    //
+    // insert into 1 row and then check result
+    sql """INSERT INTO ${testTable} VALUES(26, '{"k1":"v1", "k2": 200}')"""
+    qt_select "SELECT * FROM ${testTable} ORDER BY id"
 
     // jsonb_extract
     qt_select "SELECT id, j, jsonb_extract(j, '\$') FROM ${testTable} ORDER BY id"
