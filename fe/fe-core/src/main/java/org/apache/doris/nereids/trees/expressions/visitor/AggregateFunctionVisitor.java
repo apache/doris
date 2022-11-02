7c291f677cd55e8d9b328e35ea3dd0a3cbc24a28
924060929 <924060929@qq.com>
2022-11-02 18:01:08 +0800

    [feature](Nereids) Support lots of scalar function and fix some bug (#13764)
    Proposed changes
    1. function interfaces that can search the matched signature, say ComputeSignature. It's equal to the Function.CompareMode.
       - IdenticalSignature: equal to Function.CompareMode.IS_IDENTICAL
       - NullOrIdenticalSignature: equal to Function.CompareMode.IS_INDISTINGUISHABLE
       - ImplicitlyCastableSignature: equal to Function.CompareMode.IS_SUPERTYPE_OF
       - ExplicitlyCastableSignature: equal to Function.CompareMode.IS_NONSTRICT_SUPERTYPE_OF
    3. generate lots of scalar functions
    4. bug-fix: disassemble avg function compute wrong result because the wrong input type, the AggregateParam.inputTypesBeforeDissemble is use to save the origin input type and pass to backend to find the correct global aggregate function.
    5. bug-fix: subquery with OneRowRelation will crash because wrong nullable property
    
    
    Note:
    1. currently no more unit test/regression test for the scalar functions, I will add the test until migrate aggregate functions for unified processing.
    2. A known problem is can not invoke the variable length function, I will fix it later.

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/AggregateFunctionVisitor.java
@@ -0,0 +1,50 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.trees.expressions.visitor;
+
+import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateFunction;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Avg;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Count;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Max;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Min;
+import org.apache.doris.nereids.trees.expressions.functions.agg.Sum;
+
+/** AggregateFunctionVisitor. */
+public interface AggregateFunctionVisitor<R, C> {
+    R visitAggregateFunction(AggregateFunction aggregateFunction, C context);
+
+    default R visitAvg(Avg avg, C context) {
+        return visitAggregateFunction(avg, context);
+    }
+
+    default R visitCount(Count count, C context) {
+        return visitAggregateFunction(count, context);
+    }
+
+    default R visitMax(Max max, C context) {
+        return visitAggregateFunction(max, context);
+    }
+
+    default R visitMin(Min min, C context) {
+        return visitAggregateFunction(min, context);
+    }
+
+    default R visitSum(Sum sum, C context) {
+        return visitAggregateFunction(sum, context);
+    }
+}
