22ba9d7b052a603aa0bbb552039792490123fff8
nextdreamblue <zxw520blue1@163.com>
2022-11-02 16:56:33 +0800

    [fix](ctas) text column type len = 1 when create table as select
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -1206,7 +1206,8 @@ public class InternalCatalog implements CatalogIf<Database> {
                 Expr resultExpr = resultExprs.get(i);
                 Type resultType = resultExpr.getType();
                 if (resultType.isStringType() && resultType.getLength() < 0) {
-                    typeDef = new TypeDef(Type.STRING);
+                    // alway set text length MAX_STRING_LENGTH
+                    typeDef = new TypeDef(ScalarType.createStringType());
                 } else if (resultType.isDecimalV2() && resultType.equals(ScalarType.DECIMALV2)) {
                     typeDef = new TypeDef(ScalarType.createDecimalType(27, 9));
                 } else if (resultType.isDecimalV3()) {
