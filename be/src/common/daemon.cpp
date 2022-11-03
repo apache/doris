225a1baf522a9976816195d95d89755fabf328a9
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2022-11-03 23:33:20 +0800

    [improvement](memory) simplify memory config related to tcmalloc (#13784)
    There are several configs related to tcmalloc, users do know how to config them. Actually users just want two modes, performance or compact, in performance mode, users want doris run query and load quickly while in compact mode, users want doris run with less memory usage.
    
    If we want to config tcmalloc individually, we can use env variables which are supported by tcmalloc.

--- be/src/common/daemon.cpp
+++ be/src/common/daemon.cpp
@@ -69,6 +69,15 @@ bool k_doris_exit = false;
 
 void Daemon::tcmalloc_gc_thread() {
     // TODO All cache GC wish to be supported
+
+    size_t tc_use_memory_min = MemInfo::mem_limit();
+    if (config::memory_mode == std::string("performance")) {
+        tc_use_memory_min = std::max(tc_use_memory_min / 10 * 9,
+                                     tc_use_memory_min - size_t(10) * 1024 * 1024 * 1024);
+    } else {
+        tc_use_memory_min = tc_use_memory_min >> 1;
+    }
+
     while (!_stop_background_threads_latch.wait_for(MonoDelta::FromSeconds(10))) {
         size_t used_size = 0;
         size_t free_size = 0;
@@ -78,8 +87,8 @@ void Daemon::tcmalloc_gc_thread() {
         MallocExtension::instance()->GetNumericProperty("tcmalloc.pageheap_free_bytes", &free_size);
         size_t alloc_size = used_size + free_size;
 
-        if (alloc_size > config::tc_use_memory_min) {
-            size_t max_free_size = alloc_size * config::tc_free_memory_rate / 100;
+        if (alloc_size > tc_use_memory_min) {
+            size_t max_free_size = alloc_size * 20 / 100;
             if (free_size > max_free_size) {
                 MallocExtension::instance()->ReleaseToSystem(free_size - max_free_size);
             }
