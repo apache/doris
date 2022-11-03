f1ef2f18b1028aad02d23749d38eaacd6956e10f
carlvinhust2012 <huchenghappy@126.com>
2022-11-03 17:12:17 +0800

    [optimization](array-type) update the exception message when create table with array column (#13731)
    This pr is used to update the exception message when create table with array column.
    Co-authored-by: hucheng01 <hucheng01@baidu.com>

--- regression-test/suites/datatype_p0/string/test_string_basic.groovy
+++ regression-test/suites/datatype_p0/string/test_string_basic.groovy
@@ -20,7 +20,7 @@ suite("test_string_basic") {
     // first column could not be string
     test {
         sql """CREATE TABLE IF NOT EXISTS fail_tb1 (k1 STRING NOT NULL, v1 STRING NOT NULL) DISTRIBUTED BY HASH(k1) BUCKETS 5 properties("replication_num" = "1")"""
-        exception "The olap table first column could not be float, double, string use decimal or varchar instead."
+        exception "The olap table first column could not be float, double, string or array, please use decimal or varchar instead."
     }
     // string type should could not be key
     test {
