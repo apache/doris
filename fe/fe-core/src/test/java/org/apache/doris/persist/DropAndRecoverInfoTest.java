apache/doris

commit 8ba252b2d6bc711ccc35fe7027307766387f0563

[meta](recover) change dropInfo and RecoverInfo to GSON (#13830)

fe/fe-core/src/main/java/org/apache/doris/persist/RecoverInfo.java
@@ -21,17 +21,26 @@ import org.apache.doris.catalog.Env;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonUtils;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class RecoverInfo implements Writable {
+    @SerializedName(value = "dbId")
     private long dbId;
+    @SerializedName(value = "newDbName")
     private String newDbName;
+    @SerializedName(value = "tableId")
     private long tableId;
+    @SerializedName(value = "newTableName")
     private String newTableName;
+    @SerializedName(value = "partitionId")
     private long partitionId;
+    @SerializedName(value = "newPartitionName")
     private String newPartitionName;
 
     public RecoverInfo() {
@@ -83,15 +92,20 @@ public class RecoverInfo implements Writable {
         Text.writeString(out, newPartitionName);
     }
 
-    public void readFields(DataInput in) throws IOException {
-        dbId = in.readLong();
-        tableId = in.readLong();
-        partitionId = in.readLong();
+    public static RecoverInfo read(DataInput in) throws IOException {
         if (Env.getCurrentEnvJournalVersion() >= FeMetaVersion.VERSION_114) {
-            newDbName = Text.readString(in);
-            newTableName = Text.readString(in);
-            newPartitionName = Text.readString(in);
+            return GsonUtils.GSON.fromJson(Text.readString(in), RecoverInfo.class);
+        } else {
+            RecoverInfo recoverInfo = new RecoverInfo();
+            recoverInfo.readFields(in);
+            return recoverInfo;
         }
     }
 
+    @Deprecated
+    private void readFields(DataInput in) throws IOException {
+        dbId = in.readLong();
+        tableId = in.readLong();
+        partitionId = in.readLong();
+    }
 }

fe/fe-core/src/main/java/org/apache/doris/journal/JournalEntity.java
@@ -196,8 +196,7 @@ public class JournalEntity implements Writable {
                 break;
             }
             case OperationType.OP_DROP_TABLE: {
-                data = new DropInfo();
-                ((DropInfo) data).readFields(in);
+                data = DropInfo.read(in);
                 isRead = true;
                 break;
             }
@@ -238,14 +237,12 @@ public class JournalEntity implements Writable {
             case OperationType.OP_RECOVER_DB:
             case OperationType.OP_RECOVER_TABLE:
             case OperationType.OP_RECOVER_PARTITION: {
-                data = new RecoverInfo();
-                ((RecoverInfo) data).readFields(in);
+                data = RecoverInfo.read(in);
                 isRead = true;
                 break;
             }
             case OperationType.OP_DROP_ROLLUP: {
-                data = new DropInfo();
-                ((DropInfo) data).readFields(in);
+                data = DropInfo.read(in);
                 isRead = true;
                 break;
             }

fe/fe-core/src/main/java/org/apache/doris/persist/DropInfo.java
@@ -19,18 +19,26 @@ package org.apache.doris.persist;
 
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.FeMetaVersion;
+import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonUtils;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class DropInfo implements Writable {
+    @SerializedName(value = "dbId")
     private long dbId;
+    @SerializedName(value = "tableId")
     private long tableId;
-
+    @SerializedName(value = "indexId")
     private long indexId;
+    @SerializedName(value = "forceDrop")
     private boolean forceDrop = false;
+    @SerializedName(value = "recycleTime")
     private long recycleTime = 0;
 
     public DropInfo() {
@@ -61,24 +69,16 @@ public class DropInfo implements Writable {
     }
 
     public Long getRecycleTime() {
-        return  recycleTime;
+        return recycleTime;
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
-        out.writeLong(dbId);
-        out.writeLong(tableId);
-        out.writeBoolean(forceDrop);
-        if (indexId == -1L) {
-            out.writeBoolean(false);
-        } else {
-            out.writeBoolean(true);
-            out.writeLong(indexId);
-        }
-        out.writeLong(recycleTime);
+        Text.writeString(out, GsonUtils.GSON.toJson(this));
     }
 
-    public void readFields(DataInput in) throws IOException {
+    @Deprecated
+    private void readFields(DataInput in) throws IOException {
         dbId = in.readLong();
         tableId = in.readLong();
         forceDrop = in.readBoolean();
@@ -88,15 +88,16 @@ public class DropInfo implements Writable {
         } else {
             indexId = -1L;
         }
-        if (Env.getCurrentEnvJournalVersion() >= FeMetaVersion.VERSION_114) {
-            recycleTime = in.readLong();
-        }
     }
 
     public static DropInfo read(DataInput in) throws IOException {
-        DropInfo dropInfo = new DropInfo();
-        dropInfo.readFields(in);
-        return dropInfo;
+        if (Env.getCurrentEnvJournalVersion() >= FeMetaVersion.VERSION_114) {
+            return GsonUtils.GSON.fromJson(Text.readString(in), DropInfo.class);
+        } else {
+            DropInfo dropInfo = new DropInfo();
+            dropInfo.readFields(in);
+            return dropInfo;
+        }
     }
 
     public boolean equals(Object obj) {

commit 08bf484cd845ea192f3142e45ace536700b5911f

[fix](meta)(recover) fix recover info persist bug (#13948)
introduced from #13830

fe/fe-core/src/main/java/org/apache/doris/persist/RecoverInfo.java
@@ -43,7 +43,7 @@ public class RecoverInfo implements Writable {
     @SerializedName(value = "newPartitionName")
     private String newPartitionName;
 
-    public RecoverInfo() {
+    private RecoverInfo() {
         // for persist
     }
 
@@ -83,13 +83,7 @@ public class RecoverInfo implements Writable {
 
     @Override
     public void write(DataOutput out) throws IOException {
-        out.writeLong(dbId);
-        out.writeLong(tableId);
-        out.writeLong(partitionId);
-
-        Text.writeString(out, newDbName);
-        Text.writeString(out, newTableName);
-        Text.writeString(out, newPartitionName);
+        Text.writeString(out, GsonUtils.GSON.toJson(this));
     }
 
     public static RecoverInfo read(DataInput in) throws IOException {

fe/fe-core/src/test/java/org/apache/doris/persist/DropAndRecoverInfoTest.java
@@ -29,9 +29,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class DropInfoTest {
+public class DropAndRecoverInfoTest {
     @Test
-    public void testSerialization() throws Exception {
+    public void testDropInfoSerialization() throws Exception {
         MetaContext metaContext = new MetaContext();
         metaContext.setMetaVersion(FeMetaVersion.VERSION_CURRENT);
         metaContext.setThreadLocalInfo();
@@ -69,6 +69,38 @@ public class DropInfoTest {
         Assert.assertFalse(info2.equals(new DropInfo(1, 0, -1L, true, 0)));
         Assert.assertFalse(info2.equals(new DropInfo(1, 2, -1L, false, 0)));
         Assert.assertTrue(info2.equals(new DropInfo(1, 2, -1L, true, 0)));
+
+        // 3. delete files
+        dis.close();
+        file.delete();
+    }
+
+    @Test
+    public void testRecoveryInfoSerialization() throws Exception {
+        MetaContext metaContext = new MetaContext();
+        metaContext.setMetaVersion(FeMetaVersion.VERSION_CURRENT);
+        metaContext.setThreadLocalInfo();
+
+        // 1. Write objects to file
+        File file = new File("./recoverInfo");
+        file.createNewFile();
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(file));
+
+        RecoverInfo info1 = new RecoverInfo(1, 2, 3, "a", "b", "c");
+        info1.write(dos);
+        dos.flush();
+        dos.close();
+
+        // 2. Read objects from file
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        RecoverInfo rInfo1 = RecoverInfo.read(dis);
+
+        Assert.assertEquals(1, rInfo1.getDbId());
+        Assert.assertEquals(2, rInfo1.getTableId());
+        Assert.assertEquals(3, rInfo1.getPartitionId());
+        Assert.assertEquals("a", rInfo1.getNewDbName());
+        Assert.assertEquals("b", rInfo1.getNewTableName());
+        Assert.assertEquals("c", rInfo1.getNewPartitionName());
 
         // 3. delete files
         dis.close();