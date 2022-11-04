76f29f526abf75d42b2abf16656a7aa070b534cc
BePPPower <ftw2139@163.com>
2022-11-04 23:42:48 +0800

    fix description

--- fe/fe-core/src/main/java/org/apache/doris/analysis/SchemaTableType.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/SchemaTableType.java
@@ -65,9 +65,9 @@ public enum SchemaTableType {
     SCH_VARIABLES("VARIABLES", "VARIABLES", TSchemaTableType.SCH_VARIABLES),
     SCH_VIEWS("VIEWS", "VIEWS", TSchemaTableType.SCH_VIEWS),
     SCH_CREATE_TABLE("CREATE_TABLE", "CREATE_TABLE", TSchemaTableType.SCH_CREATE_TABLE),
+    SCH_INVALID("NULL", "NULL", TSchemaTableType.SCH_INVALID),
     SCH_ROWSETS("ROWSETS", "ROWSETS", TSchemaTableType.SCH_ROWSETS),
-    SCH_BACKENDS("BACKENDS", "BACKENDS", TSchemaTableType.SCH_BACKENDS),
-    SCH_INVALID("NULL", "NULL", TSchemaTableType.SCH_INVALID);
+    SCH_BACKENDS("BACKENDS", "BACKENDS", TSchemaTableType.SCH_BACKENDS);
     private static final String dbName = "INFORMATION_SCHEMA";
     private static SelectList fullSelectLists;
 
