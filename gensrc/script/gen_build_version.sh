632beff8d63e85276af91289fde9a6789a64f347
yiguolei <yiguolei@gmail.com>
2022-11-07 14:35:35 +0800

    rename tags

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="1.1.4-rc01"
+build_version="1.1-lts"
 
 unset LANG
 unset LC_CTYPE
