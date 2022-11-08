9f5ceb2a111a4d8eab042c77fced43f52b5afdc4
zhengyu <freeman.zhang1992@gmail.com>
2022-11-08 10:44:50 +0800

    Update binary_dict_page.cpp

--- be/src/olap/rowset/segment_v2/binary_dict_page.cpp
+++ be/src/olap/rowset/segment_v2/binary_dict_page.cpp
@@ -37,7 +37,8 @@ BinaryDictPageBuilder::BinaryDictPageBuilder(const PageBuilderOptions& options)
     // TODO: the data page builder type can be created by Factory according to user config
     _data_page_builder.reset(new BitshufflePageBuilder<OLAP_FIELD_TYPE_INT>(options));
     PageBuilderOptions dict_builder_options;
-    dict_builder_options.data_page_size = _options.data_page_size;
+    dict_builder_options.data_page_size =
+            std::min(_options.data_page_size, _options.dict_page_size);
     dict_builder_options.is_dict_page = true;
     _dict_builder.reset(new BinaryPlainPageBuilder<OLAP_FIELD_TYPE_VARCHAR>(dict_builder_options));
     reset();
