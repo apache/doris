0f98ba956df15a6461cb1fe77404fc889f532932
xueweizhang <zxw520blue1@163.com>
2022-11-10 14:55:41 +0800

    [feature](function) add new function uuid() (#14092)

--- be/src/vec/functions/simple_function_factory.h
+++ be/src/vec/functions/simple_function_factory.h
@@ -69,6 +69,7 @@ void register_function_ifnull(SimpleFunctionFactory& factory);
 void register_function_like(SimpleFunctionFactory& factory);
 void register_function_regexp(SimpleFunctionFactory& factory);
 void register_function_random(SimpleFunctionFactory& factory);
+void register_function_uuid(SimpleFunctionFactory& factory);
 void register_function_coalesce(SimpleFunctionFactory& factory);
 void register_function_grouping(SimpleFunctionFactory& factory);
 void register_function_datetime_floor_ceil(SimpleFunctionFactory& factory);
@@ -208,6 +209,7 @@ public:
             register_function_like(instance);
             register_function_regexp(instance);
             register_function_random(instance);
+            register_function_uuid(instance);
             register_function_coalesce(instance);
             register_function_grouping(instance);
             register_function_datetime_floor_ceil(instance);
