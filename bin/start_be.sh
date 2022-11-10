9dcaf8e493377619bb318646f958fdeb6447708d
Yongqiang YANG <dataroaring@gmail.com>
2022-11-10 01:48:07 +0000

    [fix](chore) read max_map_count from proc and make notice much more unstandable
    Some users can not use sysctl under non-root in linux, so we read max_map_count
    from proc. Notice users that they can change max_map_count under root.

--- bin/start_be.sh
+++ bin/start_be.sh
@@ -63,9 +63,10 @@ DORIS_HOME="$(
 export DORIS_HOME
 
 if [[ "$(uname -s)" != 'Darwin' ]]; then
-    MAX_MAP_COUNT="$(sysctl -n vm.max_map_count)"
+    MAX_MAP_COUNT="$(cat /proc/sys/vm/max_map_count)"
     if [[ "${MAX_MAP_COUNT}" -lt 2000000 ]]; then
-        echo "Please set vm.max_map_count to be 2000000. sysctl -w vm.max_map_count=2000000"
+        echo "Please set vm.max_map_count to be 2000000 under root using 'sysctl -w vm.max_map_count=2000000' OR"\
+             "'echo 2000000 > /proc/sys/vm/max_map_count'."
         exit 1
     fi
 fi
