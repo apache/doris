bf79805a66e92a38d060e11426bdd55e8912b660
Mingyu Chen <morningman@163.com>
2022-11-12 11:13:52 +0800

    [regression-test] sleep longer to void  error (#14186)

--- regression-test/suites/nereids_syntax_p0/rollup.groovy
+++ regression-test/suites/nereids_syntax_p0/rollup.groovy
@@ -59,7 +59,7 @@ suite("rollup") {
             }
         }
     }
-    Thread.sleep(200)
+    Thread.sleep(2000)
 
     sql "insert into rollup_t1 values(1, 2, 3, 4)"
     sql "insert into rollup_t1 values(1, 2, 3, 2)"
