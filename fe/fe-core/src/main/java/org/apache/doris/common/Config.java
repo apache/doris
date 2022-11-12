beaf2fcaf667b6eba3c1d7dee2f62af59808f566
catpineapple <42031973+catpineapple@users.noreply.github.com>
2022-11-12 20:52:37 +0800

    [feature](partition) support new create partition syntax (#13772)
    Create partitions use ：
    ```
    PARTITION BY RANGE(event_day)(
            FROM ("2000-11-14") TO ("2021-11-14") INTERVAL 1 YEAR,
            FROM ("2021-11-14") TO ("2022-11-14") INTERVAL 1 MONTH,
            FROM ("2022-11-14") TO ("2023-01-03") INTERVAL 1 WEEK,
            FROM ("2023-01-03") TO ("2023-01-14") INTERVAL 1 DAY,
            PARTITION p_20230114 VALUES [('2023-01-14'), ('2023-01-15'))
    )
    
    PARTITION BY RANGE(event_time)(
            FROM ("2023-01-03 12") TO ("2023-01-14 22") INTERVAL 1 HOUR
    )
    ```
    can create a year/month/week/day/hour's date partitions in a batch,
    also it is compatible with the single partitioning method.

--- fe/fe-core/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/Config.java
@@ -1475,6 +1475,14 @@ public class Config extends ConfigBase {
     public static int max_dynamic_partition_num = 500;
 
     /**
+     * Used to limit the maximum number of partitions that can be created when creating multi partition,
+     * to avoid creating too many partitions at one time.
+     * The number is determined by "start" and "end" in the multi partition parameters.
+     */
+    @ConfField(mutable = true, masterOnly = true)
+    public static int max_multi_partition_num = 4096;
+
+    /**
      * Control the max num of backup/restore job per db
      */
     @ConfField(mutable = true, masterOnly = true)
