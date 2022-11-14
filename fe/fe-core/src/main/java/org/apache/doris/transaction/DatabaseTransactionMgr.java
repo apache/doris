apache/doris

commit 49fecd2a6dae2a6327490301525911d07b519018

[improvement](log) print info of error replicas (#14220)

fe/fe-core/src/main/java/org/apache/doris/transaction/DatabaseTransactionMgr.java
@@ -417,7 +417,8 @@ public class DatabaseTransactionMgr {
     private void checkCommitStatus(List<Table> tableList, TransactionState transactionState,
                                    List<TabletCommitInfo> tabletCommitInfos, TxnCommitAttachment txnCommitAttachment,
                                    Set<Long> errorReplicaIds, Map<Long, Set<Long>> tableToPartition,
-                                    Set<Long> totalInvolvedBackends) throws UserException {
+                                   Set<Long> totalInvolvedBackends) throws UserException {
+
         Database db = env.getInternalCatalog().getDbOrMetaException(dbId);
 
         // update transaction state extra if exists
@@ -525,6 +526,7 @@ public class DatabaseTransactionMgr {
                         // save the error replica ids for current tablet
                         // this param is used for log
                         Set<Long> errorBackendIdsForTablet = Sets.newHashSet();
+                        String errorReplicaInfo = new String();
                         for (long tabletBackend : tabletBackends) {
                             Replica replica = tabletInvertedIndex.getReplica(tabletId, tabletBackend);
                             if (replica == null) {
@@ -541,21 +543,28 @@ public class DatabaseTransactionMgr {
                                 // for example, a replica is in clone state
                                 if (replica.getLastFailedVersion() < 0) {
                                     ++successReplicaNum;
+                                } else {
+                                    errorReplicaInfo += " replica [" + replica.getId() + "], lastFailedVersion ["
+                                                        + replica.getLastFailedVersion() + "]";
                                 }
                             } else {
                                 errorBackendIdsForTablet.add(tabletBackend);
                                 errorReplicaIds.add(replica.getId());
                                 // not remove rollup task here, because the commit maybe failed
                                 // remove rollup task when commit successfully
+                                errorReplicaInfo += " replica [" + replica.getId() + "] commitBackends null or "
+                                                    + "tabletBackend [" + tabletBackend + "] does not "
+                                                    + "in commitBackends";
                             }
                         }
 
                         if (successReplicaNum < quorumReplicaNum) {
                             LOG.warn("Failed to commit txn [{}]. "
                                             + "Tablet [{}] success replica num is {} < quorum replica num {} "
-                                            + "while error backends {}", transactionState.getTransactionId(),
-                                    tablet.getId(), successReplicaNum, quorumReplicaNum,
-                                    Joiner.on(",").join(errorBackendIdsForTablet));
+                                            + "while error backends {} error replica info {}",
+                                    transactionState.getTransactionId(), tablet.getId(), successReplicaNum,
+                                    quorumReplicaNum, Joiner.on(",").join(errorBackendIdsForTablet),
+                                    errorBackendIdsForTablet);
                             throw new TabletQuorumFailedException(transactionState.getTransactionId(), tablet.getId(),
                                     successReplicaNum, quorumReplicaNum,
                                     errorBackendIdsForTablet);

commit 77e592200648c1a38899fdbdfb03fec81e9ca388

(log) fix wrong print introduced by 49fecd2a6dae

fe/fe-core/src/main/java/org/apache/doris/transaction/DatabaseTransactionMgr.java
@@ -564,7 +564,7 @@ public class DatabaseTransactionMgr {
                                             + "while error backends {} error replica info {}",
                                     transactionState.getTransactionId(), tablet.getId(), successReplicaNum,
                                     quorumReplicaNum, Joiner.on(",").join(errorBackendIdsForTablet),
-                                    errorBackendIdsForTablet);
+                                    errorReplicaInfo);
                             throw new TabletQuorumFailedException(transactionState.getTransactionId(), tablet.getId(),
                                     successReplicaNum, quorumReplicaNum,
                                     errorBackendIdsForTablet);