17fd83ca329148d9120b21b028f1f3476c605477
Yukang Lian <Yukang.Lian2022@gmail.com>
2022-11-14 12:53:36 +0000

    reformat

--- be/src/vec/functions/array/function_array_compact.h
+++ be/src/vec/functions/array/function_array_compact.h
@@ -66,8 +66,8 @@ public:
         auto& dest_offsets = dest_column_ptr->get_offsets();
         DCHECK(dest_nested_column != nullptr);
 
-        auto res_val = _execute_by_type(*src_nested_column, src_offsets, *dest_nested_column,
-                                        dest_offsets, nested_type);
+        auto res_val = _execute(*src_nested_column, src_offsets, *dest_nested_column,
+                                        dest_offsets);
         if (!res_val) {
             return Status::RuntimeError(
                     fmt::format("execute failed or unsupported types for function {}({})",
@@ -79,10 +79,8 @@ public:
     }
 
 private:
-    template <typename ColumnType>
-    bool _execute_number(const IColumn& src_column, const ColumnArray::Offsets64& src_offsets,
+    bool _execute(const IColumn& src_column, const ColumnArray::Offsets64& src_offsets,
                          IColumn& dest_column, ColumnArray::Offsets64& dest_offsets) {
-        using NestType = typename ColumnType::value_type;
 
         ColumnArray::Offset64 src_offsets_size = src_offsets.size();
         ColumnArray::Offset64 src_pos = 0;
@@ -110,71 +108,6 @@ private:
         return true;
     }
 
-    bool _execute_generic(const IColumn& src_column, const ColumnArray::Offsets64& src_offsets,
-                          IColumn& dest_column, ColumnArray::Offsets64& dest_offsets) {
-        dest_column.reserve(src_column.size());
-
-        ColumnArray::Offset64 src_offsets_size = src_offsets.size();
-        ColumnArray::Offset64 src_pos = 0;
-        ColumnArray::Offset64 dest_pos = 0;
-
-        for (size_t i = 0; i < src_offsets_size; ++i) {
-            auto src_offset = src_offsets[i];
-            if (src_pos < src_offset) {
-                // Insert first element
-                dest_column.insert_from(src_column, src_pos);
-                ++src_pos;
-                ++dest_pos;
-
-                // For the rest of elements, insert if the element is different from the previous.
-                for (; src_pos < src_offset; ++src_pos) {
-                    if (0 != (src_column.compare_at(src_pos - 1, src_pos, src_column, 1))) {
-                        dest_column.insert_from(src_column, src_pos);
-                        ++dest_pos;
-                    }
-                }
-            }
-            dest_offsets.push_back(dest_pos);
-        }
-        return true;
-    }
-
-    bool _execute_by_type(const IColumn& src_column, const ColumnArray::Offsets64& src_offsets,
-                          IColumn& dest_column, ColumnArray::Offsets64& dest_offsets,
-                          DataTypePtr& nested_type) {
-        bool res = false;
-        WhichDataType which(remove_nullable(nested_type));
-        if (which.is_uint8()) {
-            res = _execute_number<ColumnUInt8>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_int8()) {
-            res = _execute_number<ColumnInt8>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_int16()) {
-            res = _execute_number<ColumnInt16>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_int32()) {
-            res = _execute_number<ColumnInt32>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_int64()) {
-            res = _execute_number<ColumnInt64>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_int128()) {
-            res = _execute_number<ColumnInt128>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_float32()) {
-            res = _execute_number<ColumnFloat32>(src_column, src_offsets, dest_column,
-                                                 dest_offsets);
-        } else if (which.is_float64()) {
-            res = _execute_number<ColumnFloat64>(src_column, src_offsets, dest_column,
-                                                 dest_offsets);
-        } else if (which.is_date()) {
-            res = _execute_number<ColumnDate>(src_column, src_offsets, dest_column, dest_offsets);
-        } else if (which.is_date_time()) {
-            res = _execute_number<ColumnDateTime>(src_column, src_offsets, dest_column,
-                                                  dest_offsets);
-        } else if (which.is_decimal128()) {
-            res = _execute_number<ColumnDecimal128>(src_column, src_offsets, dest_column,
-                                                    dest_offsets);
-        } else {
-            res = _execute_generic(src_column, src_offsets, dest_column, dest_offsets);
-        }
-        return res;
-    }
 };
 
 } // namespace doris::vectorized
