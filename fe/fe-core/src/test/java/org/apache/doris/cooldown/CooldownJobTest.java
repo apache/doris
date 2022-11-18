8246f08314e6b2b6f719c847618063f9e4ee3cb4
pengxianyu <diablowcg@163.com>
2022-11-18 20:22:30 +0800

    remote single file on fe

--- fe/fe-core/src/test/java/org/apache/doris/cooldown/CooldownJobTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/cooldown/CooldownJobTest.java
@@ -19,12 +19,15 @@ package org.apache.doris.cooldown;
 
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
+import org.apache.doris.catalog.KeysType;
 import org.apache.doris.catalog.MaterializedIndex;
 import org.apache.doris.catalog.OlapTable;
 import org.apache.doris.catalog.Partition;
+import org.apache.doris.catalog.PartitionInfo;
 import org.apache.doris.catalog.Replica;
 import org.apache.doris.catalog.Tablet;
 import org.apache.doris.catalog.TabletMeta;
+import org.apache.doris.cluster.Cluster;
 import org.apache.doris.thrift.TCooldownType;
 import org.apache.doris.thrift.TStorageMedium;
 
@@ -33,10 +36,10 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class CooldownJobTest {
 
-    private static Replica replica = new Replica();
-    private static CooldownJob cooldownJob;
     private static long jobId = 100L;
     private static long dbId = 101L;
     private static long tableId = 102L;
@@ -47,28 +50,37 @@ public class CooldownJobTest {
     private static long backendId = 107L;
     private static TCooldownType cooldownType = TCooldownType.UPLOAD_DATA;
     private static long timeoutMs = 10000L;
+    private static Replica replica = new Replica(replicaId, backendId, 1, Replica.ReplicaState.NORMAL);
 
     public static CooldownJob createCooldownJob() {
         return new CooldownJob(jobId, dbId, tableId, partitionId, indexId, tabletId, replicaId, backendId, cooldownType,
                 timeoutMs);
     }
+
     @BeforeClass
     public static void beforeClass() throws Exception {
-        cooldownJob = createCooldownJob();
 
-        Database db = new Database(dbId, "db1");
-        Env.getCurrentInternalCatalog().replayCreateDb(db, "db1");
-        OlapTable table = new OlapTable();
-        table.setId(tableId);
-        db.createTable(table);
-        MaterializedIndex baseIndex = new MaterializedIndex();
-        baseIndex.setIdForRestore(indexId);
-        Partition partition = new Partition(partitionId, "part1", baseIndex, null);
-        table.addPartition(partition);
-        Tablet tablet = new Tablet(tabletId);
-        TabletMeta tabletMeta = new TabletMeta(dbId, tableId, partitionId, indexId, 1, TStorageMedium.HDD);
-        baseIndex.addTablet(tablet, tabletMeta);
-        tablet.addReplica(replica);
+        try {
+            Cluster testCluster = new Cluster("test_cluster", 0);
+            Database db = new Database(dbId, "db1");
+            db.setClusterName("test_cluster");
+            Env.getCurrentEnv().addCluster(testCluster);
+            Env.getCurrentEnv().unprotectCreateDb(db);
+            OlapTable table = new OlapTable(tableId, "testTable", new ArrayList<>(), KeysType.DUP_KEYS,
+                    new PartitionInfo(), null);
+            table.setId(tableId);
+            db.createTable(table);
+            MaterializedIndex baseIndex = new MaterializedIndex();
+            baseIndex.setIdForRestore(indexId);
+            Partition partition = new Partition(partitionId, "part1", baseIndex, null);
+            table.addPartition(partition);
+            Tablet tablet = new Tablet(tabletId);
+            TabletMeta tabletMeta = new TabletMeta(dbId, tableId, partitionId, indexId, 1, TStorageMedium.HDD);
+            baseIndex.addTablet(tablet, tabletMeta);
+            tablet.addReplica(replica);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterClass
@@ -78,6 +90,7 @@ public class CooldownJobTest {
 
     @Test
     public void testPending() throws Exception {
+        CooldownJob cooldownJob = createCooldownJob();
         cooldownJob.runPendingJob();
         Assert.assertEquals(CooldownJob.JobState.SEND_CONF, cooldownJob.jobState);
         CooldownJob job1 = createCooldownJob();
@@ -87,6 +100,7 @@ public class CooldownJobTest {
 
     @Test
     public void testSendConf() throws Exception {
+        CooldownJob cooldownJob = createCooldownJob();
         cooldownJob.runPendingJob();
         cooldownJob.runSendJob();
         Assert.assertEquals(CooldownJob.JobState.RUNNING, cooldownJob.jobState);
