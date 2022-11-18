4718df8d4e982f58c55445f8343a1a555e764508
chenlinzhong <490103404@qq.com>
2022-11-18 17:45:34 +0800

    init

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskProcessor.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskProcessor.java
@@ -19,6 +19,7 @@ package org.apache.doris.mtmv;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.QueryState;
 import org.apache.doris.qe.StmtExecutor;
@@ -26,8 +27,11 @@ import org.apache.doris.thrift.TUniqueId;
 import org.apache.doris.analysis.StatementBase;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.DdlException;
+
 import java.util.concurrent.atomic.AtomicLong;
+
 import com.google.common.collect.Lists;
+
 import java.io.DataOutput;
 import java.io.IOException;
 import java.io.StringReader;
@@ -36,6 +40,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
+
 import org.apache.doris.analysis.SqlParser;
 import org.apache.doris.analysis.SqlScanner;
 import org.apache.doris.common.util.SqlParserUtils;
@@ -52,6 +57,7 @@ import org.apache.doris.catalog.DatabaseIf;
 import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.MetaNotFoundException;
 import org.apache.doris.datasource.InternalCatalog;
+
 import java.util.UUID;
 
 import org.apache.doris.common.FeConstants;
@@ -59,10 +65,11 @@ import org.apache.doris.mtmv.MTMVUtils;
 import org.apache.doris.mtmv.metadata.MTMVJob;
 import org.apache.doris.catalog.MaterializedView;
 import org.apache.doris.common.UserException;
+
 import java.io.IOException;
+
 import org.apache.doris.mtmv.MTMVUtils.TaskState;
 import org.apache.doris.mtmv.metadata.ChangeMTMVTask;
-
 
 
 public class MTMVTaskProcessor {
@@ -70,124 +77,132 @@ public class MTMVTaskProcessor {
     private ConnectContext context;
     private static final AtomicLong STMT_ID_GENERATOR = new AtomicLong(0);
 
-    void process(MTMVTaskContext context) throws Exception{
+    void process(MTMVTaskContext context) throws Exception {
         String taskId = context.getTask().getTaskId();
         long jobId = context.getJob().getId();
         LOG.info("run mv logic start, task_id:{}, jobid:{}", taskId, jobId);
         String tableName = context.getTask().getMvName();
         String tmpTableName = genTmpTableName(tableName);
         DatabaseIf db = Env.getCurrentEnv().getCatalogMgr().getCatalog(InternalCatalog.INTERNAL_CATALOG_NAME)
-            .getDbOrAnalysisException(context.getTask().getDbName());
+                .getDbOrAnalysisException(context.getTask().getDbName());
         MaterializedView table = (MaterializedView) db.getTableOrAnalysisException(tableName);
-        if(!table.tryMvTaskLock()){
+        if (!table.tryMvTaskLock()) {
             LOG.warn("run mv task  failed, taskid:{}, jobid:{}, msg:{}", taskId, jobId, "get lock fail");
-            return ;
+            return;
         }
-        try{
+        try {
             //step1 create tmp table 
             String tmpCreateTableStmt = genCreateTempMaterializedViewStmt(context, tableName, tmpTableName);
             //check whther tmp table exists, if exists means run mv task failed before, so need to drop it first
-            if(db.isTableExist(tmpTableName)){
+            if (db.isTableExist(tmpTableName)) {
                 String dropStml = genDropStml(context, tmpTableName);
-                ConnectContext dropResult= execSQL(context, dropStml);
-                LOG.info("exec drop table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, dropStml, dropResult.getState(), dropResult.getState().getInfoMessage());
+                ConnectContext dropResult = execSQL(context, dropStml);
+                LOG.info("exec drop table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, dropStml,
+                        dropResult.getState(), dropResult.getState().getInfoMessage());
             }
             ConnectContext createTempTableResult = execSQL(context, tmpCreateTableStmt);
-            LOG.info("exec tmp table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, tmpCreateTableStmt, createTempTableResult.getState(), createTempTableResult.getState().getInfoMessage());
-            if(createTempTableResult.getState().getStateType()!= QueryState.MysqlStateType.OK ){
-                throw new Throwable("create tmp table failed, sql:"+tmpCreateTableStmt);
+            LOG.info("exec tmp table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, tmpCreateTableStmt,
+                    createTempTableResult.getState(), createTempTableResult.getState().getInfoMessage());
+            if (createTempTableResult.getState().getStateType() != QueryState.MysqlStateType.OK) {
+                throw new Throwable("create tmp table failed, sql:" + tmpCreateTableStmt);
             }
 
             //step2  insert data to tmp table 
             String insertStmt = genInsertIntoStmt(context, tmpTableName);
             ConnectContext insertDataResult = execSQL(context, insertStmt);
-            LOG.info("exec insert into stmt, taskid:{}, stmt:{}, ret:{}, msg:{}, effected_row:{}", taskId, insertStmt, insertDataResult.getState(), insertDataResult.getState().getInfoMessage(), insertDataResult.getState().getAffectedRows());
-            if(insertDataResult.getState().getStateType() != QueryState.MysqlStateType.OK ){
-                throw new Throwable("insert data failed, sql:"+insertStmt);
+            LOG.info("exec insert into stmt, taskid:{}, stmt:{}, ret:{}, msg:{}, effected_row:{}", taskId, insertStmt,
+                    insertDataResult.getState(), insertDataResult.getState().getInfoMessage(),
+                    insertDataResult.getState().getAffectedRows());
+            if (insertDataResult.getState().getStateType() != QueryState.MysqlStateType.OK) {
+                throw new Throwable("insert data failed, sql:" + insertStmt);
             }
-            
+
             //step3  swap tmp table with origin table 
             String swapStmt = genSwapStmt(context, tableName, tmpTableName);
             ConnectContext swapResult = execSQL(context, swapStmt);
-            LOG.info("exec swap stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, swapStmt, swapResult.getState(), swapResult.getState().getInfoMessage());
-            if(swapResult.getState().getStateType() != QueryState.MysqlStateType.OK ){
-                throw new Throwable("swap table failed, sql:"+swapStmt);
+            LOG.info("exec swap stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, swapStmt, swapResult.getState(),
+                    swapResult.getState().getInfoMessage());
+            if (swapResult.getState().getStateType() != QueryState.MysqlStateType.OK) {
+                throw new Throwable("swap table failed, sql:" + swapStmt);
             }
             //step4 update task info
             context.getTask().setMessage(insertDataResult.getState().getInfoMessage());
             context.getTask().setState(TaskState.SUCCESS);
             LOG.info("run mv task success, task_id:{},jobid:{}", taskId, jobId);
-        }catch (AnalysisException e){
+        } catch (AnalysisException e) {
             LOG.warn("run mv task failed, taskid:{}, jobid:{}, msg:{}", taskId, jobId, e.getMessage());
             context.getTask().setMessage("run task failed, caused by " + e.getMessage());
             context.getTask().setState(TaskState.FAILED);
-         }catch (Throwable e){
+        } catch (Throwable e) {
             LOG.warn("run mv task failed, taskid:{}, jobid:{}, msg:{}", taskId, jobId, e.getMessage());
             context.getTask().setMessage("run task failed, caused by " + e.getMessage());
             context.getTask().setState(TaskState.FAILED);
-        }finally{
+        } finally {
             context.getTask().setFinishTime(MTMVUtils.getNowTimeStamp());
             table.mvTaskUnLock();
             //double check 
-            if(db.isTableExist(tmpTableName)){
+            if (db.isTableExist(tmpTableName)) {
                 String dropStml = genDropStml(context, tmpTableName);
-                ConnectContext dropResult= execSQL(context, dropStml);
-                LOG.info("exec drop table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, dropStml, dropResult.getState(), dropResult.getState().getInfoMessage());
+                ConnectContext dropResult = execSQL(context, dropStml);
+                LOG.info("exec drop table stmt, taskid:{}, stmt:{}, ret:{}, msg:{}", taskId, dropStml,
+                        dropResult.getState(), dropResult.getState().getInfoMessage());
             }
         }
     }
 
-    private String genDropStml(MTMVTaskContext context, String tableName){
+    private String genDropStml(MTMVTaskContext context, String tableName) {
         String stmt = "DROP MATERIALIZED VIEW  if exists " + tableName;
         LOG.info("gen drop stmt, taskid:{}, stmt:{}", context.getTask().getTaskId(), stmt);
         return stmt;
     }
 
-    private String genTmpTableName(String tableName){
+    private String genTmpTableName(String tableName) {
         String tmpTableName = FeConstants.TEMP_MATERIZLIZE_DVIEW_PREFIX + tableName;
-        return  tmpTableName;
+        return tmpTableName;
     }
 
     // ALTER TABLE t1 REPLACE WITH TABLE t1_mirror PROPERTIES('swap' = 'false');  
-    private String genSwapStmt(MTMVTaskContext context, String tableName, String tmpTableName){
-        String stmt = "ALTER TABLE "  +tableName+" REPLACE WITH TABLE " +tmpTableName+ " PROPERTIES('swap' = 'false');";
+    private String genSwapStmt(MTMVTaskContext context, String tableName, String tmpTableName) {
+        String stmt = "ALTER TABLE " + tableName + " REPLACE WITH TABLE " + tmpTableName
+                + " PROPERTIES('swap' = 'false');";
         LOG.info("gen swap stmt, taskid:{}, stmt:{}", context.getTask().getTaskId(), stmt);
         return stmt;
     }
 
-    private String genInsertIntoStmt(MTMVTaskContext context, String tmpTableName){
+    private String genInsertIntoStmt(MTMVTaskContext context, String tmpTableName) {
         String query = context.getQuery();
-        String stmt =  "insert into "+tmpTableName+" " + query;
-        stmt = stmt.replaceAll(SystemInfoService.DEFAULT_CLUSTER+":","");
+        String stmt = "insert into " + tmpTableName + " " + query;
+        stmt = stmt.replaceAll(SystemInfoService.DEFAULT_CLUSTER + ":", "");
         LOG.info("gen insert into stmt, taskid:{}, stmt:{}", context.getTask().getTaskId(), stmt);
         return stmt;
     }
 
-    private String genCreateTempMaterializedViewStmt(MTMVTaskContext context, String tableName, String tmpTableName){
+    private String genCreateTempMaterializedViewStmt(MTMVTaskContext context, String tableName, String tmpTableName) {
         try {
             String dbName = context.getTask().getDbName();
             String originViewStmt = getCreateViewStmt(dbName, tableName);
             String tmpViewStmt = convertCreateViewStmt(originViewStmt, tmpTableName);
-            LOG.info("gen tmp table stmt, taskid:{}, originstml:{},  stmt:{}", context.getTask().getTaskId(), originViewStmt.replaceAll("\n", " "), tmpViewStmt);
+            LOG.info("gen tmp table stmt, taskid:{}, originstml:{},  stmt:{}", context.getTask().getTaskId(),
+                    originViewStmt.replaceAll("\n", " "), tmpViewStmt);
             return tmpViewStmt;
-        }catch (Throwable e) {
+        } catch (Throwable e) {
             LOG.warn("fail to gen tmp table stmt, taskid:{}, msg:{}", context.getTask().getTaskId(), e.getMessage());
             return "";
         }
     }
 
     //Generate temporary view table statement
-    private String convertCreateViewStmt(String stmt, String tmpTable){
-		stmt = stmt.replace("`","");
-		String regex = "CREATE MATERIALIZED VIEW.*\n";
-		String replacement  = "CREATE MATERIALIZED VIEW " + tmpTable + "\n";
-		stmt = stmt.replaceAll(regex, replacement);
-		// regex = "BUILD.*\n";
-		// stmt = stmt.replaceAll(regex, " BUILD deferred never REFRESH \n");
-		stmt = stmt.replaceAll("\n", " ");
-        stmt = stmt.replaceAll(SystemInfoService.DEFAULT_CLUSTER+":","");
-		return stmt;
-	}
+    private String convertCreateViewStmt(String stmt, String tmpTable) {
+        stmt = stmt.replace("`", "");
+        String regex = "CREATE MATERIALIZED VIEW.*\n";
+        String replacement = "CREATE MATERIALIZED VIEW " + tmpTable + "\n";
+        stmt = stmt.replaceAll(regex, replacement);
+        // regex = "BUILD.*\n";
+        // stmt = stmt.replaceAll(regex, " BUILD deferred never REFRESH \n");
+        stmt = stmt.replaceAll("\n", " ");
+        stmt = stmt.replaceAll(SystemInfoService.DEFAULT_CLUSTER + ":", "");
+        return stmt;
+    }
 
     // get origin table create stmt from env
     private String getCreateViewStmt(String dbName, String tableName) throws AnalysisException {
@@ -202,7 +217,7 @@ public class MTMVTaskProcessor {
             List<String> createTableStmt = Lists.newArrayList();
             Env.getDdlStmt(table, createTableStmt, null, null, false, true /* hide password */, -1L);
             if (createTableStmt.isEmpty()) {
-               return "";
+                return "";
             }
             return createTableStmt.get(0);
         } catch (Throwable e) {
@@ -213,7 +228,7 @@ public class MTMVTaskProcessor {
         return "";
     }
 
-    private ConnectContext execSQL(MTMVTaskContext context, String originStmt)throws AnalysisException, DdlException{
+    private ConnectContext execSQL(MTMVTaskContext context, String originStmt) throws AnalysisException, DdlException {
         ConnectContext ctx = new ConnectContext();
         ctx.setEnv(Env.getCurrentEnv());
         ctx.setCluster(SystemInfoService.DEFAULT_CLUSTER);
@@ -224,21 +239,24 @@ public class MTMVTaskProcessor {
         ctx.setQualifiedUser("root");
         ctx.setCurrentUserIdentity(UserIdentity.createAnalyzedUserIdentWithIp("root", "%"));
         ctx.getState().reset();
-        
+
         List<StatementBase> stmts = null;
-        StatementBase parsedStmt=null;
+        StatementBase parsedStmt = null;
         stmts = parse(ctx, originStmt);
         parsedStmt = stmts.get(0);
-       try {
+        try {
             StmtExecutor executor = new StmtExecutor(ctx, parsedStmt);
             ctx.setExecutor(executor);
-            executor.execute(); 
+            executor.execute();
         } catch (IOException e) {
-            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(),  e.getMessage(), originStmt);
+            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(), e.getMessage(),
+                    originStmt);
         } catch (UserException e) {
-            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(),  e.getMessage(), originStmt);
+            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(), e.getMessage(),
+                    originStmt);
         } catch (Throwable e) {
-            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(),  e.getMessage(), originStmt);
+            LOG.warn("execSQL failed, taskid:{}, msg:{}, stmt:{}", context.getTask().getTaskId(), e.getMessage(),
+                    originStmt);
         } finally {
             LOG.debug("execSQL succ, taskid:{}, stmt:{}", context.getTask().getTaskId(), originStmt);
         }
