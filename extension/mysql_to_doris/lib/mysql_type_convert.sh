a6c0fcc81c2593abc329e33ba62b9978df654aa5
gnehil <adamlee489@gmail.com>
2022-11-18 10:54:06 +0800

    [improvement](extension) process negative number default value and delete unicode collate expr

--- extension/mysql_to_doris/lib/mysql_type_convert.sh
+++ extension/mysql_to_doris/lib/mysql_type_convert.sh
@@ -29,11 +29,13 @@ sed -i 's/CHARACTER SET utf8mb4 COLLATE utf8mb4_bin//g' $path
 sed -i "s/DEFAULT '0000-00-00 00:00:00'/DEFAULT '2000-01-01 00:00:00'/g" $path
 sed -i 's/DEFAULT CURRENT_TIMESTAMP//g' $path
 sed -i 's/DEFAULT b/DEFAULT/g' $path
-sed -i "s/\([0-9]\+\|[0-9]\+\.[0-9]\+\)/DEFAULT '\1'/g" $path
+sed -i "s/DEFAULT \(\(\-\)\?[0-9]\+\(\.[0-9]\+\)\?\)/DEFAULT '\1'/g" $path
 sed -i 's/CHARACTER SET utf8mb4//g' $path
 sed -i 's/CHARACTER SET utf8//g' $path
 sed -i 's/COLLATE utf8mb4_general_ci//g' $path
 sed -i 's/COLLATE utf8_general_ci//g'  $path
+sed -i 's/COLLATE utf8mb4_unicode_ci//g' $path
+sed -i 's/COLLATE utf8_unicode_ci//g'  $path
 sed -i 's/COLLATE utf8_bin//g'  $path
 sed -i 's/\<tinytext\>/varchar(65533)/g' $path
 sed -i 's/\<text\>/varchar(65533)/g' $path
