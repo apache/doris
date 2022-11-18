d2afcae7aa6929e74ff7a564d1498502a5b7b1c2
shizhiqiang03 <shizhiqiang03@meituan.com>
2022-11-18 11:23:42 +0800

    [Bug] fix bug for grouping set query which where condition is false

--- fe/fe-core/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
@@ -78,6 +78,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -960,6 +961,10 @@ public class SingleNodePlanner {
         }
 
         if (analyzer.hasEmptySpjResultSet() && selectStmt.getAggInfo() != null) {
+            GroupByClause groupByClause = selectStmt.getGroupByClause();
+            if (Objects.nonNull(groupByClause) && groupByClause.isGroupByExtension()) {
+                rowTuples.add(selectStmt.getGroupingInfo().getVirtualTuple().getId());
+            }
             final PlanNode emptySetNode = new EmptySetNode(ctx.getNextNodeId(), rowTuples);
             emptySetNode.init(analyzer);
             emptySetNode.setOutputSmap(selectStmt.getBaseTblSmap());
