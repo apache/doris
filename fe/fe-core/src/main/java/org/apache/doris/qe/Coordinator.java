4ab6d7d32ab834a6616233a35a4f08a8a831484d
zhannngchen <48427519+zhannngchen@users.noreply.github.com>
2022-11-20 00:04:48 +0800

    [fix](exec) fix thread token shutdown (#14418)
    Fix Thread pool token was shut down error.
    This is because when there are more than 1 fragment of a query on one BE, the thread token maybe
    reset incorrectly, causing thread token shutdown earlier.
    cherry-pick from master
    Introduced from #13021

--- fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -1695,8 +1695,9 @@ public class Coordinator {
         // and returned_all_results_ is true.
         // (UpdateStatus() initiates cancellation, if it hasn't already been initiated)
         if (!(returnedAllResults && status.isCancelled()) && !status.ok()) {
-            LOG.warn("one instance report fail, query_id={} instance_id={}",
-                    DebugUtil.printId(queryId), DebugUtil.printId(params.getFragmentInstanceId()));
+            LOG.warn("one instance report fail, query_id={} instance_id={}, error message: {}",
+                    DebugUtil.printId(queryId), DebugUtil.printId(params.getFragmentInstanceId()),
+                    status.getErrorMsg());
             updateStatus(status, params.getFragmentInstanceId());
         }
         if (execState.done) {
