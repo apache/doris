496185080c739cd985c6ad9172f1a48a604477fe
morningman <morningman@163.com>
2022-11-23 23:13:09 +0800

    [fix](storage-policy) use Long instead of Date to persiste cooldowntime in storage policy

--- fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
@@ -1151,14 +1151,11 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                     rEntry.setPolicyName(iter.getPolicyName());
                     //java 8 not support ifPresentOrElse
                     final long[] ttlCoolDown = {-1};
-                    Optional.ofNullable(iter.getCooldownTtl())
-                        .ifPresent(ttl -> ttlCoolDown[0] = Integer.parseInt(ttl));
+                    Optional.ofNullable(iter.getCooldownTtl()).ifPresent(ttl -> ttlCoolDown[0] = Integer.parseInt(ttl));
                     rEntry.setCooldownTtl(ttlCoolDown[0]);
 
-                    final long[] secondTimestamp = {-1};
-                    Optional.ofNullable(iter.getCooldownDatetime())
-                        .ifPresent(date -> secondTimestamp[0] = date.getTime() / 1000);
-                    rEntry.setCooldownDatetime(secondTimestamp[0]);
+                    rEntry.setCooldownDatetime(
+                            iter.getCooldownTimestampMs() == -1 ? -1 : iter.getCooldownTimestampMs() / 100);
 
                     Optional.ofNullable(iter.getMd5Checksum()).ifPresent(rEntry::setMd5Checksum);
 
