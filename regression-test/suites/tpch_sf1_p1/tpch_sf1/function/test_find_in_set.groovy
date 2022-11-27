7b49bd63b4241521b4eb0bf72b673279846c6b17
minghong <minghong.zhou@163.com>
2022-11-27 23:57:51 +0800

    [test](regression) adjust nereids related regression cases under datev2 (#14578)
    1. revert 14439, recovery dup&unique test cases
    2. adjust nereids related case

--- regression-test/suites/tpch_sf1_p1/tpch_sf1/function/test_find_in_set.groovy
+++ regression-test/suites/tpch_sf1_p1/tpch_sf1/function/test_find_in_set.groovy
@@ -19,16 +19,12 @@
 // along with an empty one to find_in_set, BE 
 
 suite("test_find_in_set") {
-    String realDb = context.config.getDbNameByFile(context.file)
-    // get parent directory's group
-    realDb = realDb.substring(0, realDb.lastIndexOf("_"))
 
-    sql "use ${realDb}"
     qt_select """
         select find_in_set(
-        cast(orders.`O_COMMENT` as varchar),
+        cast(regression_test_tpch_sf1_p1.orders.`O_COMMENT` as varchar),
         cast(BITMAP_TO_STRING(
-        cast(BITMAP_EMPTY() as bitmap)) as varchar)) from orders limit 1
+        cast(BITMAP_EMPTY() as bitmap)) as varchar)) from regression_test_tpch_sf1_p1.orders limit 1
     """
 
 }
