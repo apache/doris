25698b2b8b461a1214c2bdea6b2a2d982e34255a
BePPPower <ftw2139@163.com>
2022-11-27 14:54:36 +0800

    fix 3

--- fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcClient.java
@@ -42,7 +42,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
