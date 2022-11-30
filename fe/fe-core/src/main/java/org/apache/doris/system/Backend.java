9bbbcf031c84fedbd78ee15bd288e5d654360329
caiconghui <55968745+caiconghui@users.noreply.github.com>
2022-11-30 20:42:15 +0800

    [enhancement](k8s) Support fqdn mode for be  in k8s enviroment (#9172)
    In the k8s environment, the ip of the pod can be changed, but the hostname of pod is stable. When the host machine of the pod fails, the k8s can schedule the failed pod to the new host machine for reconstruction. After that, the newly created pod's hostname remains unchanged, and the ip address has been changed. The change of the be node's ip address can be detected by FQDNManager when enable_fqdn_mode is true
    
    Co-authored-by: caiconghui1 <caiconghui1@jd.com>

--- fe/fe-core/src/main/java/org/apache/doris/system/Backend.java
+++ fe/fe-core/src/main/java/org/apache/doris/system/Backend.java
@@ -47,7 +47,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -70,7 +69,9 @@ public class Backend implements Writable {
     @SerializedName("id")
     private long id;
     @SerializedName("host")
-    private String host;
+    private volatile String ip;
+    @SerializedName("hostName")
+    private String hostName;
     private String version;
 
     @SerializedName("heartbeatPort")
@@ -142,7 +143,7 @@ public class Backend implements Writable {
     private int heartbeatFailureCounter = 0;
 
     public Backend() {
-        this.host = "";
+        this.ip = "";
         this.version = "";
         this.lastUpdateMs = 0;
         this.lastStartTime = 0;
@@ -160,9 +161,14 @@ public class Backend implements Writable {
         this.tagMap.put(locationTag.type, locationTag.value);
     }
 
-    public Backend(long id, String host, int heartbeatPort) {
+    public Backend(long id, String ip, int heartbeatPort) {
+        this(id, ip, null, heartbeatPort);
+    }
+
+    public Backend(long id, String ip, String hostName, int heartbeatPort) {
         this.id = id;
-        this.host = host;
+        this.ip = ip;
+        this.hostName = hostName;
         this.version = "";
         this.heartbeatPort = heartbeatPort;
         this.bePort = -1;
@@ -186,7 +192,11 @@ public class Backend implements Writable {
     }
 
     public String getHost() {
-        return host;
+        return ip;
+    }
+
+    public String getHostName() {
+        return hostName;
     }
 
     public String getVersion() {
@@ -278,6 +288,10 @@ public class Backend implements Writable {
         this.backendState = state.ordinal();
     }
 
+    public void setHost(String ip) {
+        this.ip = ip;
+    }
+
     public void setAlive(boolean isAlive) {
         this.isAlive.set(isAlive);
     }
@@ -351,31 +365,12 @@ public class Backend implements Writable {
     }
 
     /**
-     * backend belong to some cluster
-     *
-     * @return
-     */
-    public boolean isUsedByCluster() {
-        return this.backendState == BackendState.using.ordinal();
-    }
-
-    /**
      * backend is free, and it isn't belong to any cluster
      *
      * @return
      */
     public boolean isFreeFromCluster() {
         return this.backendState == BackendState.free.ordinal();
-    }
-
-    /**
-     * backend execute discommission in cluster , and backendState will be free
-     * finally
-     *
-     * @return
-     */
-    public boolean isOffLineFromCluster() {
-        return this.backendState == BackendState.offline.ordinal();
     }
 
     public ImmutableMap<String, DiskInfo> getDisks() {
@@ -478,15 +473,6 @@ public class Backend implements Writable {
             }
         }
         return exceedLimit;
-    }
-
-    public String getPathByPathHash(long pathHash) {
-        for (DiskInfo diskInfo : disksRef.values()) {
-            if (diskInfo.getPathHash() == pathHash) {
-                return diskInfo.getRootPath();
-            }
-        }
-        return null;
     }
 
     public void updateDisks(Map<String, TDisk> backendDisks) {
@@ -611,7 +597,7 @@ public class Backend implements Writable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, host, heartbeatPort, bePort, isAlive);
+        return Objects.hash(id, ip, heartbeatPort, bePort, isAlive);
     }
 
     @Override
@@ -625,13 +611,13 @@ public class Backend implements Writable {
 
         Backend backend = (Backend) obj;
 
-        return (id == backend.id) && (host.equals(backend.host)) && (heartbeatPort == backend.heartbeatPort)
+        return (id == backend.id) && (ip.equals(backend.ip)) && (heartbeatPort == backend.heartbeatPort)
                 && (bePort == backend.bePort) && (isAlive.get() == backend.isAlive.get());
     }
 
     @Override
     public String toString() {
-        return "Backend [id=" + id + ", host=" + host + ", heartbeatPort=" + heartbeatPort + ", alive=" + isAlive.get()
+        return "Backend [id=" + id + ", host=" + ip + ", heartbeatPort=" + heartbeatPort + ", alive=" + isAlive.get()
                 + ", tags: " + tagMap + "]";
     }
 
@@ -799,18 +785,5 @@ public class Backend implements Writable {
 
     public String getTagMapString() {
         return "{" + new PrintableMap<>(tagMap, ":", true, false).toString() + "}";
-    }
-
-    /**
-     * Get Tag by type, return Optional.empty if no such tag with given type
-     *
-     * @param type
-     * @return
-     */
-    public Optional<Tag> getTagByType(String type) {
-        if (!tagMap.containsKey(type)) {
-            return Optional.empty();
-        }
-        return Optional.of(Tag.createNotCheck(type, tagMap.get(type)));
     }
 }
