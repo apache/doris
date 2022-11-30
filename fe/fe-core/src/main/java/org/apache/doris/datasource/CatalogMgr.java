bdf38ddc8ddbcfe45c585e3ac34736d9b5107e94
Yulei-Yang <yulei.yang0699@gmail.com>
2022-11-30 09:49:24 +0800

    sort show catalogs result by name

--- fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
@@ -302,7 +302,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
                     if (Env.getCurrentEnv().getAuth()
                             .checkCtlPriv(ConnectContext.get(), catalog.getName(), PrivPredicate.SHOW)) {
                         String name = catalog.getName();
-                        // Filter dbname
+                        // Filter catalog name
                         if (matcher != null && !matcher.match(name)) {
                             continue;
                         }
@@ -312,6 +312,11 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
                         row.add(catalog.getType());
                         rows.add(row);
                     }
+
+                    // sort by catalog name
+                    rows.sort((x, y) -> {
+                        return x.get(1).compareTo(y.get(1));
+                    });
                 }
             } else {
                 if (!nameToCatalog.containsKey(showStmt.getCatalogName())) {
