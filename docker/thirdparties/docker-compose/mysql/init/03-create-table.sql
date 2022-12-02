7627defc88cb0b3a3b38aab7d4d0dc4b27ae61e5
Tiewei Fang <43782773+BePPPower@users.noreply.github.com>
2022-12-02 11:58:11 +0800

    [fix](regression-test) Add test data for test_mysql_jdbc_catalog and fix mysql-5.7.yaml about UTF8 (#14749)
    Fix two things:
    1. Fix that the MySQL table displays the garbled code even if the UTF8 is specified for table.
    2. Fix that `test_mysql_jdbc_catalog.out` lack of returned data for table `ex_tb13`.

--- docker/thirdparties/docker-compose/mysql/init/03-create-table.sql
+++ docker/thirdparties/docker-compose/mysql/init/03-create-table.sql
@@ -90,34 +90,34 @@ CREATE TABLE doris_test.ex_tb7 (
   `id` varchar(32) NULL DEFAULT "",
   `user_name` varchar(32) NULL DEFAULT "",
   `member_list` DECIMAL(10,3)
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb8 (
    `date` date NOT NULL COMMENT "",
    `uid` varchar(64) NOT NULL,
    `stat_type` int(11) NOT NULL COMMENT "",
    `price` varchar(255) NULL COMMENT "price"
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb9 (
    c_date date null
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb10 (
     `aa` varchar(200) NULL,
     `bb` int NULL,
     `cc` bigint NULL
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb11 (
  `aa` varchar(200) PRIMARY KEY,
  `bb` int NULL
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb12 (
  `cc` varchar(200) PRIMARY KEY,
  `dd` int NULL
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb13 (
      name varchar(128),
@@ -129,7 +129,7 @@ CREATE TABLE doris_test.ex_tb13 (
      country varchar(128),
      gender varchar(128),
      covid BOOLEAN
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb14 (
      tid varchar(128),
@@ -137,7 +137,7 @@ CREATE TABLE doris_test.ex_tb14 (
      dt  date,
      cmd varchar(128),
      dp_from varchar(128)
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb15 (
     col1 varchar(10) NULL ,
@@ -152,7 +152,7 @@ CREATE TABLE doris_test.ex_tb15 (
     col10 varchar(10) NULL ,
     col11 varchar(10) NULL ,
     col12 datetime NULL
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb16 (
    `id` bigint(20) NOT NULL COMMENT '',
@@ -162,7 +162,7 @@ CREATE TABLE doris_test.ex_tb16 (
    `modify_uid` bigint(20) NULL,
    `ctime` bigint(20) NULL,
    `mtime` bigint(20) NULL
-);
+) engine=innodb charset=utf8;
 
 CREATE TABLE doris_test.ex_tb17 (
   `id` bigint(20) NULL,
@@ -182,9 +182,9 @@ CREATE TABLE doris_test.ex_tb17 (
   `account_number` varchar(765) NULL,
   `currency_type` tinyint(4) NULL,
   `order_source` tinyint(4) NULL
-);
+) engine=innodb charset=utf8;
 
-create table ex_tb18 (
+create table doris_test.ex_tb18 (
     num_tinyint tinyint,
     num_tinyint2 tinyint unsigned,
     num_smallint SMALLINT,
@@ -206,7 +206,7 @@ create table ex_tb18 (
     text_value TEXT(123)
 ) engine=innodb charset=utf8;
 
-create table ex_tb19 (
+create table doris_test.ex_tb19 (
     date_value date,
     time_value time,
     year_value year,
