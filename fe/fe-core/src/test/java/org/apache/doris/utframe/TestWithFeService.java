9cc3789996b2653af638fe432c9ffb086154e56c
morningman <morningman@163.com>
2022-12-06 10:01:45 +0800

    [fix](ut) try to fix ConcurrentModifycationException bug

--- fe/fe-core/src/test/java/org/apache/doris/utframe/TestWithFeService.java
+++ fe/fe-core/src/test/java/org/apache/doris/utframe/TestWithFeService.java
@@ -120,6 +120,7 @@ public abstract class TestWithFeService {
 
     @BeforeAll
     public final void beforeAll() throws Exception {
+        FeConstants.runningUnitTest = true;
         beforeCreatingConnectContext();
         connectContext = createDefaultCtx();
         beforeCluster();
