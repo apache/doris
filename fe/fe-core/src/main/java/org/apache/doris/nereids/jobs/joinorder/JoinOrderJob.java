deefc246d4df8cdcfbc332557e00c253bbd40932
xiejiann <jianxie0@gmail.com>
2022-12-06 14:15:58 +0800

    fix bugs: update plan when prune column in DPHyp
    Signed-off-by: xiejiann <jianxie0@gmail.com>

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/joinorder/JoinOrderJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/joinorder/JoinOrderJob.java
@@ -42,7 +42,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Join Order job with DPHyp
