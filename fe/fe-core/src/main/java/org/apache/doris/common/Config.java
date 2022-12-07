c221b5ff166fe9b2ad3a26fd5c6761ac6752bdb3
BiteTheDDDDt <pxl290@qq.com>
2022-12-07 12:58:15 +0800

    update config

--- fe/fe-core/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/Config.java
@@ -1681,10 +1681,10 @@ public class Config extends ConfigBase {
     public static boolean enable_quantile_state_type = false;
 
     @ConfField
-    public static boolean enable_vectorized_load = false;
+    public static boolean enable_vectorized_load = true;
 
     @ConfField
-    public static boolean enable_pipeline_load = true;
+    public static boolean enable_pipeline_load = false;
 
     @ConfField(mutable = false, masterOnly = true)
     public static int backend_rpc_timeout_ms = 60000; // 1 min
