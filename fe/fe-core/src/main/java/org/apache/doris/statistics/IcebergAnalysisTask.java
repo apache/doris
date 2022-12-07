c4cb70bc4637db1bfbf9d25c8f5f53e1f9a03481
kikyo <cyborgz1999@gmail.com>
2022-12-07 16:48:38 +0800

    fix analysis

--- fe/fe-core/src/main/java/org/apache/doris/statistics/IcebergAnalysisTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/IcebergAnalysisTask.java
@@ -50,7 +50,7 @@ public class IcebergAnalysisTask extends HMSAnalysisTask {
 
     private static final String INSERT_TABLE_SQL_TEMPLATE = "INSERT INTO "
             + "${internalDB}.${columnStatTbl}"
-            + " values ('${id}','${catalogId}', '${dbId}', '${tblId}', '${colId}', NULL, "
+            + " values ('${id}','${catalogId}', '${dbId}', '${tblId}', '-1', '${colId}', NULL, "
             + "${numRows}, 0, ${nulls}, '0', '0', ${dataSize}, '${update_time}')";
 
 
