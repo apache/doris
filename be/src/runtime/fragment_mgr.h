48a9166aa489bcce7e169e5d6dd4ec797452016b
Pxl <pxl290@qq.com>
2022-12-07 15:29:56 +0800

    [Pipeline](sink) support olap table sink operator (#14872)
    * support olap table sink operator
    
    * update config

--- be/src/runtime/fragment_mgr.h
+++ be/src/runtime/fragment_mgr.h
@@ -32,7 +32,6 @@
 #include "http/rest_monitor_iface.h"
 #include "runtime_filter_mgr.h"
 #include "util/countdown_latch.h"
-#include "util/hash_util.hpp"
 #include "util/metrics.h"
 #include "util/thread.h"
 
@@ -65,12 +64,10 @@ public:
     using FinishCallback = std::function<void(PlanFragmentExecutor*)>;
 
     FragmentMgr(ExecEnv* exec_env);
-    virtual ~FragmentMgr();
+    ~FragmentMgr() override;
 
     // execute one plan fragment
     Status exec_plan_fragment(const TExecPlanFragmentParams& params);
-
-    Status exec_pipeline(const TExecPlanFragmentParams& params);
 
     void remove_pipeline_context(
             std::shared_ptr<pipeline::PipelineFragmentContext> pipeline_context);
@@ -92,7 +89,7 @@ public:
 
     void cancel_worker();
 
-    virtual void debug(std::stringstream& ss);
+    void debug(std::stringstream& ss) override;
 
     // input: TScanOpenParams fragment_instance_id
     // output: selected_columns
--- fe/fe-core/src/main/java/org/apache/doris/planner/StreamLoadPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/StreamLoadPlanner.java
@@ -250,6 +250,7 @@ public class StreamLoadPlanner {
         // for stream load, we use exec_mem_limit to limit the memory usage of load channel.
         queryOptions.setLoadMemLimit(taskInfo.getMemLimit());
         queryOptions.setEnableVectorizedEngine(Config.enable_vectorized_load);
+        queryOptions.setEnablePipelineEngine(Config.enable_pipeline_load);
         queryOptions.setBeExecVersion(Config.be_exec_version);
 
         params.setQueryOptions(queryOptions);
