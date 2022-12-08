69c7c52e0e45effe2ea0f97958d48ac7080f8cd0
stalary <stalary@163.com>
2022-12-08 21:07:22 +0800

    FIX: codestyle

--- fe/fe-core/src/main/java/org/apache/doris/httpv2/rest/manager/NodeAction.java
+++ fe/fe-core/src/main/java/org/apache/doris/httpv2/rest/manager/NodeAction.java
@@ -55,7 +55,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
