df8d3371230b31bb5b2a1f9c4f1e62c2c6a87015
Yulei-Yang <yulei.yang0699@gmail.com>
2022-12-12 21:10:00 +0800

    fix code conflicts

--- regression-test/suites/query_p0/sql_functions/string_functions/test_string_function.groovy
+++ regression-test/suites/query_p0/sql_functions/string_functions/test_string_function.groovy
@@ -154,17 +154,6 @@ suite("test_string_function") {
     qt_sql "select sub_replace(\"this is origin str\",\"NEW-STR\",1);"
     qt_sql "select sub_replace(\"doris\",\"***\",1,2);"
 
-    sql 'set enable_nereids_planner=true'
-    sql 'set enable_fallback_to_original_planner=false'
-
-    qt_sql "select elt(0, \"hello\", \"doris\");"
-    qt_sql "select elt(1, \"hello\", \"doris\");"
-    qt_sql "select elt(2, \"hello\", \"doris\");"
-    qt_sql "select elt(3, \"hello\", \"doris\");"
-
-    qt_sql "select sub_replace(\"this is origin str\",\"NEW-STR\",1);"
-    qt_sql "select sub_replace(\"doris\",\"***\",1,2);"
-    
     qt_sql "select substring_index(\"hello world\", \" \", 1);"
     qt_sql "select substring_index(\"hello world\", \" \", 2);"
     qt_sql "select substring_index(\"hello world\", \" \", 3);"
@@ -178,4 +167,15 @@ suite("test_string_function") {
     qt_sql "select substring_index(\"prefix_string\", null, 1);"
     qt_sql "select substring_index(\"prefix_string\", \"_\", null);"
     qt_sql "select substring_index(\"prefix_string\", \"__\", -1);"
+
+    sql 'set enable_nereids_planner=true'
+    sql 'set enable_fallback_to_original_planner=false'
+
+    qt_sql "select elt(0, \"hello\", \"doris\");"
+    qt_sql "select elt(1, \"hello\", \"doris\");"
+    qt_sql "select elt(2, \"hello\", \"doris\");"
+    qt_sql "select elt(3, \"hello\", \"doris\");"
+
+    qt_sql "select sub_replace(\"this is origin str\",\"NEW-STR\",1);"
+    qt_sql "select sub_replace(\"doris\",\"***\",1,2);"
 }
