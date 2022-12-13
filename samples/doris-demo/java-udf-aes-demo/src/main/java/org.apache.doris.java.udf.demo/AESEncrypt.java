592ef5affbc8064d5bd35dc8b6d6612216213650
jiafeng.zhang <zhangjf1@gmail.com>
2022-12-13 12:09:03 +0800

    [samples](java udf demo)Java udf demo aes (#15011)
    * add java udf function demo

---
+++ samples/doris-demo/java-udf-aes-demo/src/main/java/org.apache.doris.java.udf.demo/AESEncrypt.java
@@ -0,0 +1,34 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.udf.demo;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.commons.lang3.StringUtils;
+
+public class AESEncrypt extends UDF {
+
+    public String evaluate(String content, String secret) throws Exception {
+        if (StringUtils.isBlank(content)) {
+            throw new Exception("content not is null");
+        }
+        if (StringUtils.isBlank(secret)) {
+            throw new Exception("Secret not is null");
+        }
+        return AESUtil.encrypt(content, secret);
+    }
+}
