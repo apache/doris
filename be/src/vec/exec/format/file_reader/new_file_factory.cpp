b8f93681eb07488263122f8ca408a0226193cdf2
Tiewei Fang <43782773+BePPPower@users.noreply.github.com>
2022-12-14 12:48:02 +0800

    [feature-wip](file reader) Merge broker reader to the new file reader (#14980)
    Currently, there are two sets of file readers in Doris, this pr rewrites the old broker reader with the new file reader.
    
    TODO:
    1. rewrite stream load pipe and kafka consumer pipe

--- be/src/vec/exec/format/file_reader/new_file_factory.cpp
+++ be/src/vec/exec/format/file_reader/new_file_factory.cpp
@@ -20,6 +20,7 @@
 #include "io/broker_reader.h"
 #include "io/broker_writer.h"
 #include "io/buffered_reader.h"
+#include "io/fs/broker_file_system.h"
 #include "io/fs/file_system.h"
 #include "io/fs/hdfs_file_system.h"
 #include "io/fs/s3_file_system.h"
@@ -126,6 +127,12 @@ Status NewFileFactory::create_file_reader(RuntimeProfile* /*profile*/,
                                            &file_system_ptr, file_reader));
         break;
     }
+    case TFileType::FILE_BROKER: {
+        RETURN_IF_ERROR(create_broker_reader(system_properties.broker_addresses[0],
+                                             system_properties.properties, file_description.path,
+                                             &file_system_ptr, file_reader));
+        break;
+    }
     default:
         return Status::NotSupported("unsupported file reader type: {}", std::to_string(type));
     }
@@ -177,6 +184,17 @@ Status NewFileFactory::create_s3_reader(const std::map<std::string, std::string>
     *s3_file_system = new io::S3FileSystem(s3_conf, "");
     (dynamic_cast<io::S3FileSystem*>(*s3_file_system))->connect();
     (*s3_file_system)->open_file(s3_uri.get_key(), reader);
+    return Status::OK();
+}
+
+Status NewFileFactory::create_broker_reader(const TNetworkAddress& broker_addr,
+                                            const std::map<std::string, std::string>& prop,
+                                            const std::string& path,
+                                            io::FileSystem** broker_file_system,
+                                            io::FileReaderSPtr* reader) {
+    *broker_file_system = new io::BrokerFileSystem(broker_addr, prop);
+    (dynamic_cast<io::BrokerFileSystem*>(*broker_file_system))->connect();
+    (*broker_file_system)->open_file(path, reader);
     return Status::OK();
 }
 } // namespace doris
