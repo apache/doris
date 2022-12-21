045e2be1ebdf8b31b7fc5bfba2d5f194a0795757
BePPPower <ftw2139@163.com>
2022-12-21 11:28:03 +0800

    add test

--- fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcClient.java
@@ -21,6 +21,7 @@ import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.PrimitiveType;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
+import org.apache.doris.common.Config;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.util.Util;
 
@@ -345,6 +346,9 @@ public class JdbcClient {
                     int precision = fieldSchema.getColumnSize() + 1;
                     int scale = fieldSchema.getDecimalDigits();
                     if (precision <= ScalarType.MAX_DECIMAL128_PRECISION) {
+                        if (!Config.enable_decimal_conversion && precision > ScalarType.MAX_DECIMALV2_PRECISION) {
+                            return ScalarType.createStringType();
+                        }
                         return ScalarType.createDecimalType(precision, scale);
                     } else {
                         return ScalarType.createStringType();
@@ -380,6 +384,9 @@ public class JdbcClient {
                 int precision = fieldSchema.getColumnSize();
                 int scale = fieldSchema.getDecimalDigits();
                 if (precision <= ScalarType.MAX_DECIMAL128_PRECISION) {
+                    if (!Config.enable_decimal_conversion && precision > ScalarType.MAX_DECIMALV2_PRECISION) {
+                        return ScalarType.createStringType();
+                    }
                     return ScalarType.createDecimalType(precision, scale);
                 } else {
                     return ScalarType.createStringType();
