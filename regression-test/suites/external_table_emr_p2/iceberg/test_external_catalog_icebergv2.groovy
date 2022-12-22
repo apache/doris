8214caff0916535847603ce3d5aac044bfd503a6
slothever <18522955+wsjz@users.noreply.github.com>
2022-12-22 13:45:07 +0800

    [regresion-test](icebergv2) add icebergv2 test case (#15187)

---
+++ regression-test/suites/external_table_emr_p2/iceberg/test_external_catalog_icebergv2.groovy
@@ -0,0 +1,47 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_external_catalog_icebergv2", "p2") {
+    String enabled = context.config.otherConfigs.get("enableExternalHiveTest")
+    if (enabled != null && enabled.equalsIgnoreCase("true")) {
+        String extHiveHmsHost = context.config.otherConfigs.get("extHiveHmsHost")
+        String extHiveHmsPort = context.config.otherConfigs.get("extHiveHmsPort")
+        String catalog_name = "test_external_catalog_iceberg"
+
+        sql """drop catalog if exists ${catalog_name};"""
+        sql """
+            create catalog if not exists ${catalog_name} properties (
+                'type'='hms',
+                'hive.metastore.uris' = 'thrift://${extHiveHmsHost}:${extHiveHmsPort}'
+            );
+        """
+
+        sql """switch ${catalog_name};"""
+        // test parquet format format
+        def q01 = {
+            qt_q01 """ select count(1) as c from customer_small;"""
+            qt_q02 """ select count(1) from orders """
+            qt_q03 """ select count(1) from customer_small where c_name = 'Customer#0063356' order by c_custkey limit 1; """
+            qt_q04 """ select * from customer_small order by c_custkey limit 3 """
+            qt_q05 """ select o_orderkey from orders where o_orderkey > 652566 limit 3"""
+            qt_q06 """ select o_orderkey from orders where o_custkey < 3357 limit 3"""
+            qt_q07 """ select count(1) as c from customer;"""
+        }
+        sql """ use `tpch_1000_icebergv2`; """
+        q01()
+    }
+}
