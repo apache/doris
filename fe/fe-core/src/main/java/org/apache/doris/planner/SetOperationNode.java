2e24b3c08a5e9b3986b671e32d4d5c31c9de8cfc
starocean999 <40539150+starocean999@users.noreply.github.com>
2022-12-23 10:27:49 +0800

    [fix](union)the union node should not pass through children in some case (#15286)
    the union node will make children pass through in wrong condition. If the children's materialized slots are different from union node, children can't be passed through.

--- fe/fe-core/src/main/java/org/apache/doris/planner/SetOperationNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/SetOperationNode.java
@@ -285,6 +285,9 @@ public abstract class SetOperationNode extends PlanNode {
 
         for (int i = 0; i < setOpResultExprs.size(); ++i) {
             if (!setOpTupleDescriptor.getSlots().get(i).isMaterialized()) {
+                if (VectorizedUtil.isVectorized() && childTupleDescriptor.getSlots().get(i).isMaterialized()) {
+                    return false;
+                }
                 continue;
             }
             SlotRef setOpSlotRef = setOpResultExprs.get(i).unwrapSlotRef(false);
