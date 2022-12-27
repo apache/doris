e9556c3b4ecc07df6cc7e6c1cd6f53d8259c947a
sohardforaname <organic_chemistry@foxmail.com>
2022-12-27 18:44:33 +0800

    fix-bug

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -257,6 +257,7 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
                     .getSlots()
                     .subList(groupSlotList.size(), outputTupleDesc.getSlots().size())
                     .stream()
+                    .peek(slot -> slot.setIsNullable(true))
                     .map(slot -> slot.getId().asInt())
                     .collect(ImmutableList.toImmutableList());
         }
