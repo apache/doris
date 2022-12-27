c4e0f150a9fe96e56cbff1cbd3ac899db74b0101
zhengshengjun <zhengshengjun@apache.org>
2022-12-27 11:14:25 +0800

    [fix](mow-uniquekey) fix dereference to nullptr in Tablet::calc_delete_bitmap (#15375)

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -2013,7 +2013,7 @@ Status Tablet::calc_delete_bitmap(RowsetId rowset_id,
                     }
                 }
 
-                if (!specified_rowset_ids->empty()) {
+                if (specified_rowset_ids != nullptr && !specified_rowset_ids->empty()) {
                     auto st = lookup_row_key(*key, specified_rowset_ids, &loc,
                                              dummy_version.first - 1);
                     CHECK(st.ok() || st.is<NOT_FOUND>() || st.is<ALREADY_EXIST>());
