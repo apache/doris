28bb13a02624540cff4b1e5d2ad26fb119fd91f7
Mingyu Chen <morningman@163.com>
2022-12-28 09:29:26 +0800

    [feature](light-schema-change) enable light schema change by default (#15344)

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -1848,7 +1848,7 @@ public class InternalCatalog implements CatalogIf<Database> {
         Map<String, String> properties = stmt.getProperties();
 
         // get use light schema change
-        Boolean enableLightSchemaChange = false;
+        Boolean enableLightSchemaChange;
         try {
             enableLightSchemaChange = PropertyAnalyzer.analyzeUseLightSchemaChange(properties);
         } catch (AnalysisException e) {
@@ -1889,7 +1889,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                 keysDesc.keysColumnSize(), storageFormat);
         olapTable.setDataSortInfo(dataSortInfo);
 
-        boolean enableUniqueKeyMergeOnWrite = false;
+        boolean enableUniqueKeyMergeOnWrite;
         try {
             enableUniqueKeyMergeOnWrite = PropertyAnalyzer.analyzeUniqueKeyMergeOnWrite(properties);
         } catch (AnalysisException e) {
