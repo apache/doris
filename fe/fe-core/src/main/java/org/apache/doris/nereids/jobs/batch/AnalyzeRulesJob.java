0088c0c1fa6a7c2284f3b5ce4b1b35b144c237c9
sohardforaname <organic_chemistry@foxmail.com>
2022-12-30 17:08:19 +0800

    fix-bug

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/AnalyzeRulesJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/AnalyzeRulesJob.java
@@ -19,6 +19,7 @@ package org.apache.doris.nereids.jobs.batch;
 
 import org.apache.doris.nereids.CascadesContext;
 import org.apache.doris.nereids.rules.analysis.AvgDistinctToSumDivCount;
+import org.apache.doris.nereids.rules.analysis.AdjustAggregateNullableForEmptySet;
 import org.apache.doris.nereids.rules.analysis.BindFunction;
 import org.apache.doris.nereids.rules.analysis.BindRelation;
 import org.apache.doris.nereids.rules.analysis.BindSlotReference;
@@ -71,6 +72,7 @@ public class AnalyzeRulesJob extends BatchRulesJob {
                     // please see rule BindSlotReference or BindFunction for example
                     new ProjectWithDistinctToAggregate(),
                     new AvgDistinctToSumDivCount(),
+                    new AdjustAggregateNullableForEmptySet(),
                     new ResolveOrdinalInOrderByAndGroupBy(),
                     new ReplaceExpressionByChildOutput(),
                     new HideOneRowRelationUnderUnion(),
