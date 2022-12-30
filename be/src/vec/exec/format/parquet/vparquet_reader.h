b4cc7fe0e971a1a34d0d221fd4f5e3a10b5e7033
Ashin Gau <AshinGau@users.noreply.github.com>
2022-12-30 08:57:55 +0800

    [optimize](multi-catalog) use dictionary encode&filter to process delete files (#15441)
    **Optimize**
    PR #14470 has used `Expr` to filter delete rows to match current data file,
    but the rows in the delete file are [sorted by file_path then position](https://iceberg.apache.org/spec/#position-delete-files)
    to optimize filtering rows while scanning, so this PR remove `Expr` and use binary search to filter delete rows.
    
    In addition, delete files are likely to be encoded in dictionary, it's time-consuming to decode `file_path`
    columns into `ColumnString`, so this PR use `ColumnDictionary` to read `file_path` column.
    
    After testing, the performance of iceberg v2's MOR is improved by 30%+.
    
    **Fix Bug**
    Lazy-read-block may not have the filter column, if the whole group is filtered by `Expr`
    and the batch_eof is generated from next batch.

--- be/src/vec/exec/format/parquet/vparquet_reader.h
+++ be/src/vec/exec/format/parquet/vparquet_reader.h
@@ -156,7 +156,9 @@ private:
     io::FileReaderSPtr _file_reader = nullptr;
     std::shared_ptr<FileMetaData> _file_metadata;
     const tparquet::FileMetaData* _t_metadata;
-    std::unique_ptr<RowGroupReader> _current_group_reader;
+    std::unique_ptr<RowGroupReader> _current_group_reader = nullptr;
+    // read to the end of current reader
+    bool _row_group_eof = true;
     int32_t _total_groups;                  // num of groups(stripes) of a parquet(orc) file
     std::map<std::string, int> _map_column; // column-name <---> column-index
     std::unordered_map<std::string, ColumnValueRangeType>* _colname_to_value_range;
