0b7257b5519329d59e13ab1a88e477460c475ab3
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2023-01-03 16:03:43 +0800

    [vectorized](udaf) fix udaf result is null when has multiple aggs (#15554)

--- regression-test/suites/javaudf_p0/test_javaudaf_mysum_int.groovy
+++ regression-test/suites/javaudf_p0/test_javaudaf_mysum_int.groovy
@@ -67,6 +67,10 @@ suite("test_javaudaf_mysum_int") {
         qt_select1 """ SELECT udaf_my_sum_int(user_id) result FROM ${tableName}; """
 
         qt_select2 """ select user_id, udaf_my_sum_int(user_id) from ${tableName} group by user_id order by user_id; """
+
+        qt_select3 """ select user_id, sum(user_id), udaf_my_sum_int(user_id) from ${tableName} group by user_id order by user_id; """
+
+        qt_select4 """ select user_id, udaf_my_sum_int(user_id), sum(user_id) from ${tableName} group by user_id order by user_id; """
         
         sql """ DROP FUNCTION udaf_my_sum_int(int); """
     } finally {
