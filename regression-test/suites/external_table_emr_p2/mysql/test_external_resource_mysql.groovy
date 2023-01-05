6db61676785e965b7907d86d07630149af2d8f84
zhangguoqiang <18372634969@163.com>
2023-01-05 16:57:01 +0800

    add external table p2 regression cases according doris1.2 docs 20230105

--- regression-test/suites/external_table_emr_p2/mysql/test_external_resource_mysql.groovy
+++ regression-test/suites/external_table_emr_p2/mysql/test_external_resource_mysql.groovy
@@ -1,4 +1,3 @@
-package suites.external_table_emr_p2.mysql
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
