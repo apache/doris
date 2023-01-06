d3eeb9c26465f3a1515886f30485ef4a948bebec
HappenLee <happenlee@hotmail.com>
2023-01-06 10:50:50 +0800

    [Refactor](pipeline) refactor the code of channel buffer limit and change the default value

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -200,7 +200,7 @@ CONF_mInt32(max_pushdown_conditions_per_column, "1024");
 // return_row / total_row
 CONF_mInt32(doris_max_pushdown_conjuncts_return_rate, "90");
 // (Advanced) Maximum size of per-query receive-side buffer
-CONF_mInt32(exchg_node_buffer_size_bytes, "10485760");
+CONF_mInt32(exchg_node_buffer_size_bytes, "20485760");
 
 CONF_mInt64(column_dictionary_key_ratio_threshold, "0");
 CONF_mInt64(column_dictionary_key_size_threshold, "0");
