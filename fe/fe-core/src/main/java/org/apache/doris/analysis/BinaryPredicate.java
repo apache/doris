97a2f16c12f96b294134970a6b2300eb67eb758a
Gabriel <gabrielleebuaa@gmail.com>
2023-01-07 14:46:17 +0800

    [Bug](DECIMALV3) Fix binary predicate between decimalv3 and float

--- fe/fe-core/src/main/java/org/apache/doris/analysis/BinaryPredicate.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/BinaryPredicate.java
@@ -390,7 +390,8 @@ public class BinaryPredicate extends Predicate implements Writable {
             }
         }
 
-        if ((t1.isDecimalV3Type() && !t2.isStringType()) || (t2.isDecimalV3Type() && !t1.isStringType())) {
+        if ((t1.isDecimalV3Type() && !t2.isStringType() && !t2.isFloatingPointType())
+                || (t2.isDecimalV3Type() && !t1.isStringType() && !t1.isFloatingPointType())) {
             return Type.getAssignmentCompatibleType(getChild(0).getType(), getChild(1).getType(), false);
         }
 
