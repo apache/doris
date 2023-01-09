c69cacc2a3af597a762ff1cbcfd4d93618489f65
zhoubintao <1229701101@qq.com>
2023-01-09 09:20:03 +0800

    fix conflict

--- be/src/olap/memtable.cpp
+++ be/src/olap/memtable.cpp
@@ -301,7 +301,7 @@ bool MemTable::need_flush() const {
 }
 
 bool MemTable::need_agg() const {
-    if (keys_type() == KeysType::AGG_KEYS) {
+    if (_keys_type == KeysType::AGG_KEYS) {
         return memory_usage() >= config::write_buffer_size_for_agg;
     }
 
