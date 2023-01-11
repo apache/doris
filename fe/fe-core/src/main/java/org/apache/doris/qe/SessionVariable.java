21c7f031fb1d00374932a0f42d1b157e04235029
xy720 <22125576+xy720@users.noreply.github.com>
2023-01-11 11:24:28 +0800

    [chore](Session) remove unused codes for enable_lateral_view
    session variable `enable_lateral_view` has been removed for a long time.
    This cl just remove variable name `enable_lateral_view`.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -175,8 +175,6 @@ public class SessionVariable implements Serializable, Writable {
 
     public static final String ENABLE_PARALLEL_OUTFILE = "enable_parallel_outfile";
 
-    public static final String ENABLE_LATERAL_VIEW = "enable_lateral_view";
-
     public static final String SQL_QUOTE_SHOW_CREATE = "sql_quote_show_create";
 
     public static final String RETURN_OBJECT_DATA_AS_BINARY = "return_object_data_as_binary";
