d1521b0a68997010edc30916912a26a638af1ff0
englefly <englefly@gmail.com>
2023-01-13 10:08:57 +0800

    const fold on in predicate

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/InPredicate.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/InPredicate.java
@@ -18,7 +18,6 @@
 package org.apache.doris.nereids.trees.expressions;
 
 import org.apache.doris.nereids.exceptions.UnboundException;
-import org.apache.doris.nereids.trees.expressions.functions.PropagateNullable;
 import org.apache.doris.nereids.trees.expressions.literal.Literal;
 import org.apache.doris.nereids.trees.expressions.visitor.ExpressionVisitor;
 import org.apache.doris.nereids.types.BooleanType;
@@ -35,7 +34,7 @@ import java.util.stream.Collectors;
 /**
  * In predicate expression.
  */
-public class InPredicate extends Expression implements PropagateNullable {
+public class InPredicate extends Expression {
 
     private final Expression compareExpr;
     private final List<Expression> options;
