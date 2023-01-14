0a57f1257897dd06f67561e332af1e40f3859391
Gabriel <gabrielleebuaa@gmail.com>
2023-01-14 18:36:36 +0800

    [Bug](datev2) Fix bugs for datev2 (#15860)
    These bugs are found when I run regression test with enable_date_conversion on

--- fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
@@ -1329,7 +1329,7 @@ public class FunctionCallExpr extends Expr {
                             && children.get(0).getType().isDecimalV3() && args[ix].isDecimalV3()) {
                         continue;
                     } else if (!argTypes[i].matchesType(args[ix]) && !(
-                            argTypes[i].isDateType() && args[ix].isDateType())
+                            argTypes[i].isDateOrDateTime() && args[ix].isDateOrDateTime())
                             && (!fn.getReturnType().isDecimalV3()
                             || (argTypes[i].isValid() && !argTypes[i].isDecimalV3() && args[ix].isDecimalV3()))) {
                         uncheckedCastChild(args[ix], i);
