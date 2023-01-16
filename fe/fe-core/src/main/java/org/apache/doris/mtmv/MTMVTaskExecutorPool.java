d17245eafe1efb798d8cfbbf4636d202a69c55a6
huangzhaowei <carlmartinmax@gmail.com>
2023-01-16 18:10:03 +0800

    Revert "fix p0 bug"
    This reverts commit 0d1835bbed0aa5b81fc0d8c7be29a069d669a4d9.

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskExecutorPool.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskExecutorPool.java
@@ -51,6 +51,11 @@ public class MTMVTaskExecutorPool {
             do {
                 try {
                     isSuccess = taskExecutor.executeTask();
+                    if (isSuccess) {
+                        task.setState(TaskState.SUCCESS);
+                    } else {
+                        task.setState(TaskState.FAILED);
+                    }
                 } catch (Exception ex) {
                     LOG.warn("failed to execute task.", ex);
                 } finally {
@@ -58,9 +63,7 @@ public class MTMVTaskExecutorPool {
                 }
                 retryTimes--;
             } while (!isSuccess && retryTimes >= 0);
-            if (isSuccess) {
-                task.setState(TaskState.SUCCESS);
-            } else {
+            if (!isSuccess) {
                 task.setState(TaskState.FAILED);
                 task.setErrorCode(-1);
             }
