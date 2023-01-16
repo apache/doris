65a4c8b163a598894c1d1a313e85cfe928208586
zbtzbtzbt <bit_pku_zbt@163.com>
2023-01-16 21:50:21 +0800

    [refactor] refactor segment writer (#15705)
    Co-authored-by: zhoubintao <1229701101@qq.com>

--- be/src/olap/rowset/segment_v2/segment_writer.cpp
+++ be/src/olap/rowset/segment_v2/segment_writer.cpp
@@ -184,8 +184,7 @@ Status SegmentWriter::init(const std::vector<uint32_t>& col_ids, bool has_key) {
 
 Status SegmentWriter::append_block(const vectorized::Block* block, size_t row_pos,
                                    size_t num_rows) {
-    assert(block && num_rows > 0 && row_pos + num_rows <= block->rows() &&
-           block->columns() == _column_writers.size());
+    assert(block->columns() == _column_writers.size());
     _olap_data_convertor->set_source_content(block, row_pos, num_rows);
 
     // find all row pos for short key indexes
@@ -369,7 +368,7 @@ uint64_t SegmentWriter::estimate_segment_size() {
     return size;
 }
 
-Status SegmentWriter::finalize_columns(uint64_t* index_size) {
+Status SegmentWriter::finalize_columns_data() {
     if (_has_key) {
         _row_count = _num_rows_written;
     } else {
@@ -381,33 +380,36 @@ Status SegmentWriter::finalize_columns(uint64_t* index_size) {
         RETURN_IF_ERROR(column_writer->finish());
     }
     RETURN_IF_ERROR(_write_data());
-    uint64_t index_offset = _file_writer->bytes_appended();
+
+    return Status::OK();
+}
+
+Status SegmentWriter::finalize_columns_index(uint64_t* index_size) {
+    uint64_t index_start = _file_writer->bytes_appended();
     RETURN_IF_ERROR(_write_ordinal_index());
     RETURN_IF_ERROR(_write_zone_map());
     RETURN_IF_ERROR(_write_bitmap_index());
     RETURN_IF_ERROR(_write_inverted_index());
     RETURN_IF_ERROR(_write_bloom_filter_index());
 
-    *index_size = _file_writer->bytes_appended() - index_offset;
+    *index_size = _file_writer->bytes_appended() - index_start;
     if (_has_key) {
         if (_tablet_schema->keys_type() == UNIQUE_KEYS && _opts.enable_unique_key_merge_on_write) {
             RETURN_IF_ERROR(_write_primary_key_index());
         } else {
             RETURN_IF_ERROR(_write_short_key_index());
         }
-        *index_size = _file_writer->bytes_appended() - index_offset;
+        *index_size = _file_writer->bytes_appended() - index_start;
     }
+
     // reset all column writers and data_conveter
-    _reset_column_writers();
-    _column_ids.clear();
-    _olap_data_convertor.reset();
+    clear();
+
     return Status::OK();
 }
 
 Status SegmentWriter::finalize_footer(uint64_t* segment_file_size) {
     RETURN_IF_ERROR(_write_footer());
-    RETURN_IF_ERROR(_file_writer->finalize());
-    *segment_file_size = _file_writer->bytes_appended();
     return Status::OK();
 }
 
@@ -416,19 +418,26 @@ Status SegmentWriter::finalize(uint64_t* segment_file_size, uint64_t* index_size
     if (_data_dir != nullptr && _data_dir->reach_capacity_limit((int64_t)estimate_segment_size())) {
         return Status::InternalError("disk {} exceed capacity limit.", _data_dir->path_hash());
     }
-
-    RETURN_IF_ERROR(finalize_columns(index_size));
-
-    // writer footer
+    // write data
+    RETURN_IF_ERROR(finalize_columns_data());
+    // write index
+    RETURN_IF_ERROR(finalize_columns_index(index_size));
+    // write footer
     RETURN_IF_ERROR(finalize_footer(segment_file_size));
+    // finish
+    RETURN_IF_ERROR(_file_writer->finalize());
+    *segment_file_size = _file_writer->bytes_appended();
+
     return Status::OK();
 }
 
-void SegmentWriter::_reset_column_writers() {
+void SegmentWriter::clear() {
     for (auto& column_writer : _column_writers) {
         column_writer.reset();
     }
     _column_writers.clear();
+    _column_ids.clear();
+    _olap_data_convertor.reset();
 }
 
 // write column data to file one by one
