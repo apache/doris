5389fe22843aed62800c89d8dac868cbed3cd807
sohardforaname <organic_chemistry@foxmail.com>
2023-01-17 20:23:47 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/scalar_function/L-Q.groovy
+++ regression-test/suites/nereids_function_p0/scalar_function/L-Q.groovy
@@ -16,6 +16,7 @@
 // under the License.
 
 suite("nereids_scalar_fn_3") {
+    sql "use regression_test_nereids_function_p0"
     sql "set enable_nereids_planner=true"
     sql "set enable_fallback_to_original_planner=false"
 
