c59e2740c27be89aaaa5d2df0bab6dffd73ca143
sohardforaname <organic_chemistry@foxmail.com>
2023-01-18 01:08:23 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/scalar_function/R-W.groovy
+++ regression-test/suites/nereids_function_p0/scalar_function/R-W.groovy
@@ -17,7 +17,7 @@
 
 suite("nereids_scalar_fn_4") {
     sql "use regression_test_nereids_function_p0"
-    sql "set enable_nereids_planner=false"
+    sql "set enable_nereids_planner=true"
     sql "set enable_fallback_to_original_planner=false"
 
     qt_sql "select radians(kdbl) from fn_test order by kdbl"
