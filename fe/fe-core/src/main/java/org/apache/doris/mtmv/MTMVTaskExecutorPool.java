36fe2017fb13da75c75104557c03b6c8406b79e4
Adonis Ling <adonis0147@gmail.com>
2023-01-17 16:31:22 +0800

    Add sleep before retry

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskExecutorPool.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVTaskExecutorPool.java
@@ -28,9 +28,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class MTMVTaskExecutorPool {
     private static final Logger LOG = LogManager.getLogger(MTMVTaskExecutorPool.class);
+    private static final long RETRY_INTERVAL = TimeUnit.SECONDS.toMillis(30);
     private final ExecutorService taskPool = Executors.newCachedThreadPool();
 
     public void executeTask(MTMVTaskExecutor taskExecutor) {
@@ -61,6 +63,15 @@ public class MTMVTaskExecutorPool {
                     LOG.warn("Failed to execute the task, taskId=" + task.getTaskId() + ".", t);
                 }
                 retryTimes--;
+
+                if (retryTimes > 0) {
+                    try {
+                        Thread.sleep(RETRY_INTERVAL);
+                    } catch (InterruptedException e) {
+                        LOG.warn("Failed to sleep.", e);
+                        break;
+                    }
+                }
             } while (!isSuccess && retryTimes > 0);
             if (!isSuccess) {
                 task.setState(TaskState.FAILURE);
