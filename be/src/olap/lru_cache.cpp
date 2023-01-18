ed4dd917f003e633f1b5345687b65ffb58e4e8bb
YueW <45946325+Tanya-W@users.noreply.github.com>
2023-01-18 09:30:55 +0800

    [Feature-WIP](inverted index) support use inverted index searcher cache (#16003)
    use inverted index searcher cache to improve query performance
    
    dependency pr: #14211 #15807 #15823

--- be/src/olap/lru_cache.cpp
+++ be/src/olap/lru_cache.cpp
@@ -240,6 +240,23 @@ void LRUCache::release(Cache::Handle* handle) {
                 } else if (e->priority == CachePriority::DURABLE) {
                     _lru_append(&_lru_durable, e);
                 }
+                // _cache_value_check_timestamp is true,
+                // means evict entry will depends on the timestamp sequence,
+                // the timestamp is updated by higher level caller,
+                // and the timestamp of hit entry is different with the insert entry,
+                // that is why need check timestamp to evict entry,
+                // in order to keep the survival time of hit entries
+                // longer than the entries just inserted,
+                // so use priority_queue to sorted these entries's timestamp and LRUHandle*
+                if (_cache_value_check_timestamp) {
+                    if (e->priority == CachePriority::NORMAL) {
+                        _sorted_normal_entries_with_timestamp.push(
+                                std::make_pair(_cache_value_time_extractor(e->value), e));
+                    } else if (e->priority == CachePriority::DURABLE) {
+                        _sorted_durable_entries_with_timestamp.push(
+                                std::make_pair(_cache_value_time_extractor(e->value), e));
+                    }
+                }
             }
         }
     }
@@ -247,6 +264,46 @@ void LRUCache::release(Cache::Handle* handle) {
     // free handle out of mutex
     if (last_ref) {
         e->free();
+    }
+}
+
+void LRUCache::_evict_from_lru_with_time(size_t total_size, LRUHandle** to_remove_head) {
+    // 1. evict normal cache entries
+    while (_usage + total_size > _capacity && !_sorted_normal_entries_with_timestamp.empty()) {
+        auto entry_pair = _sorted_normal_entries_with_timestamp.top();
+        LRUHandle* remove_handle = entry_pair.second;
+        if (_cache_value_time_extractor(remove_handle->value) != entry_pair.first) {
+            // Time in cache value maybe updated when higher level call LRUCache::release(),
+            // get time by _cache_value_time_extractor is the latest.
+            // Because remove element can only pop from the priority_queue header,
+            // so old <timestamp, LRUHandle*> keep in the priority_queue until pop it here.
+            _sorted_normal_entries_with_timestamp.pop();
+            continue;
+        }
+        DCHECK(remove_handle->priority == CachePriority::NORMAL);
+        _evict_one_entry(remove_handle);
+        remove_handle->next = *to_remove_head;
+        *to_remove_head = remove_handle;
+        _sorted_normal_entries_with_timestamp.pop();
+    }
+
+    // 2. evict durable cache entries if need
+    while (_usage + total_size > _capacity && !_sorted_durable_entries_with_timestamp.empty()) {
+        auto entry_pair = _sorted_durable_entries_with_timestamp.top();
+        LRUHandle* remove_handle = entry_pair.second;
+        if (_cache_value_time_extractor(remove_handle->value) != entry_pair.first) {
+            // Time in cache value maybe updated when higher level call LRUCache::release(),
+            // get time by _cache_value_time_extractor is the latest.
+            // Because remove element can only pop from the priority_queue header,
+            // so old <timestamp, LRUHandle*> keep in the priority_queue until pop it here.
+            _sorted_durable_entries_with_timestamp.pop();
+            continue;
+        }
+        DCHECK(remove_handle->priority == CachePriority::DURABLE);
+        _evict_one_entry(remove_handle);
+        remove_handle->next = *to_remove_head;
+        *to_remove_head = remove_handle;
+        _sorted_durable_entries_with_timestamp.pop();
     }
 }
 
@@ -306,7 +363,11 @@ Cache::Handle* LRUCache::insert(const CacheKey& key, uint32_t hash, void* value,
 
         // Free the space following strict LRU policy until enough space
         // is freed or the lru list is empty
-        _evict_from_lru(e->total_size, &to_remove_head);
+        if (_cache_value_check_timestamp) {
+            _evict_from_lru_with_time(e->total_size, &to_remove_head);
+        } else {
+            _evict_from_lru(e->total_size, &to_remove_head);
+        }
 
         // insert into the cache
         // note that the cache might get larger than its capacity if not enough
@@ -424,6 +485,14 @@ int64_t LRUCache::prune_if(CacheValuePredicate pred) {
     return pruned_count;
 }
 
+void LRUCache::set_cache_value_time_extractor(CacheValueTimeExtractor cache_value_time_extractor) {
+    _cache_value_time_extractor = cache_value_time_extractor;
+}
+
+void LRUCache::set_cache_value_check_timestamp(bool cache_value_check_timestamp) {
+    _cache_value_check_timestamp = cache_value_check_timestamp;
+}
+
 inline uint32_t ShardedLRUCache::_hash_slice(const CacheKey& s) {
     return s.hash(s.data(), s.size(), 0);
 }
@@ -457,6 +526,17 @@ ShardedLRUCache::ShardedLRUCache(const std::string& name, size_t total_capacity,
     INT_ATOMIC_COUNTER_METRIC_REGISTER(_entity, cache_lookup_count);
     INT_ATOMIC_COUNTER_METRIC_REGISTER(_entity, cache_hit_count);
     INT_DOUBLE_METRIC_REGISTER(_entity, cache_hit_ratio);
+}
+
+ShardedLRUCache::ShardedLRUCache(const std::string& name, size_t total_capacity, LRUCacheType type,
+                                 uint32_t num_shards,
+                                 CacheValueTimeExtractor cache_value_time_extractor,
+                                 bool cache_value_check_timestamp)
+        : ShardedLRUCache(name, total_capacity, type, num_shards) {
+    for (int s = 0; s < _num_shards; s++) {
+        _shards[s]->set_cache_value_time_extractor(cache_value_time_extractor);
+        _shards[s]->set_cache_value_check_timestamp(cache_value_check_timestamp);
+    }
 }
 
 ShardedLRUCache::~ShardedLRUCache() {
