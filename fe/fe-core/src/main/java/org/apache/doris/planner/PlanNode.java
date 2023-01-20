e4da52642862c1ef90a695b1d702f5d6b9cfa383
kikyo <cyborgz1999@gmail.com>
2023-01-20 12:35:59 +0800

    enhancement for limit clause

--- fe/fe-core/src/main/java/org/apache/doris/planner/PlanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/PlanNode.java
@@ -290,6 +290,13 @@ public abstract class PlanNode extends TreeNode<PlanNode> implements PlanStats {
         this.offset = offset;
     }
 
+    /**
+     * Used by new optimizer only.
+     */
+    public void setOffSetDirectly(long offset) {
+        this.offset = offset;
+    }
+
     public boolean hasLimit() {
         return limit > -1;
     }
