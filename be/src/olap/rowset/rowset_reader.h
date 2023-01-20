171404228f7b80d161c2aad6a3224234512c3c5a
yixiutt <102007456+yixiutt@users.noreply.github.com>
2023-01-20 16:38:23 +0800

    [improvement](vertical compaction) cache segment in vertical compaction (#16101)
    1.In vertical compaction, segments will be loaded for every column group, so
    we should cache segment ptr to avoid too many repeated io.
    2.fix vertical compaction data size bug

--- be/src/olap/rowset/rowset_reader.h
+++ be/src/olap/rowset/rowset_reader.h
@@ -44,7 +44,8 @@ public:
     virtual Status init(RowsetReaderContext* read_context) = 0;
 
     virtual Status get_segment_iterators(RowsetReaderContext* read_context,
-                                         std::vector<RowwiseIterator*>* out_iters) = 0;
+                                         std::vector<RowwiseIterator*>* out_iters,
+                                         bool use_cache = false) = 0;
     virtual void reset_read_options() = 0;
 
     virtual Status next_block(vectorized::Block* block) = 0;
