fd4d8abdc29233a2b4b3158f6d7531c654b6ccdd
Yukang Lian <Yukang.Lian2022@gmail.com>
2023-01-23 16:49:19 +0800

    fix

--- regression-test/suites/query_p0/sql_functions/datetime_functions/test_cast_to_datetimev2_function.groovy
+++ regression-test/suites/query_p0/sql_functions/datetime_functions/test_cast_to_datetimev2_function.groovy
@@ -20,23 +20,31 @@ suite("test_cast_to_datetimev2_function") {
     def tableName = "test_cast_to_datetimev2_function_table"
         sql "DROP TABLE IF EXISTS ${tableName}"
         sql """
-    	    CREATE TABLE IF NOT EXISTS ${tableName} (
-                c_id INT,
-                c_date_timev2 DATETIMEV2(4),
-                c_string VARCHAR(30))
-            DISTRIBUTED BY HASH(c_id) BUCKETS 3
-            PROPERTIES (
-                "replication_num" = "1"
-            )
-            """
-        sql "INSERT INTO ${tableName} values(1,'2022-12-01 22:23:24.9999','2022-12-01 22:23:24.999999')"
-        sql "INSERT INTO ${tableName} values(2,'2022-12-02 22:23:24.9999','2022-12-02 22:23:24.999999')"
-        
-        qt_select1 "select * from ${tableName} order by c_id asc"
-        qt_select2 "select c_id, c_date_timev2, cast(c_string as datetimev2(4)) from ${tableName} order by c_id asc"
+            CREATE TABLE IF NOT EXISTS `${tableName}` (
+            `uid` int(11) NULL COMMENT "",
+            `c_date_timev2` datetimev2(3) NULL COMMENT "",
+            `c_string` varchar(30) NULL COMMENT ""
+            ) ENGINE=OLAP
+        DUPLICATE KEY(`uid`)
+        COMMENT "OLAP"
+        DISTRIBUTED BY HASH(`uid`) BUCKETS 1
+        PROPERTIES (
+        "replication_allocation" = "tag.location.default: 1",
+        "in_memory" = "false",
+        "storage_format" = "V2"
+        )
+        """
 
-        qt_select3 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(4)),cast ('2022-12-02 22:23:23.999999' as datetimev2(4))"
-        qt_select4 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(4)),cast ('2022-12-02 22:23:23.999999' as datetimev2(4)) from ${tableName}"
+        sql """INSERT INTO ${tableName} values
+        (1,'2022-12-01 22:23:24.999999','2022-12-01 22:23:24.999999'),
+        (2,'2022-12-02 22:23:24.999999','2022-12-02 22:23:24.999999')
+        """
+        
+        qt_select1 "select * from ${tableName}"
+        qt_select2 "select uid, c_date_timev2, cast(c_string as datetimev2(3)) from ${tableName} order by uid asc"
+
+        qt_select3 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(3)),cast ('2022-12-02 22:23:23.999999' as datetimev2(3))"
+        qt_select4 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(3)),cast ('2022-12-02 22:23:23.999999' as datetimev2(3)) from ${tableName}"
         sql "DROP TABLE IF EXISTS ${tableName}"
-        qt_select5 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(4)),cast ('2022-12-02 22:23:23.999999' as datetimev2(4))"
+        qt_select5 "select cast ('2022-12-02 22:23:24.999999' as datetimev2(3)),cast ('2022-12-02 22:23:23.999999' as datetimev2(3))"
 }
