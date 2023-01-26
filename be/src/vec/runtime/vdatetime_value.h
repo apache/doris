36183b9cffbb414d5ccbf9a4b520053495b33e36
yiguolei <yiguolei@gmail.com>
2023-01-26 16:59:06 +0800

    f3

--- be/src/vec/runtime/vdatetime_value.h
+++ be/src/vec/runtime/vdatetime_value.h
@@ -581,7 +581,7 @@ public:
         return value;
     }
 
-    uint32_t hash(int seed) const { return doris::HashUtil::hash(this, sizeof(*this), seed); }
+    uint32_t hash(int seed) const { return ::doris::HashUtil::hash(this, sizeof(*this), seed); }
 
     int day_of_year() const { return daynr() - calc_daynr(_year, 1, 1) + 1; }
 
@@ -1040,7 +1040,7 @@ public:
 
     DateV2Value<T>& operator--() { return *this += -1; }
 
-    uint32_t hash(int seed) const { return HashUtil::hash(this, sizeof(*this), seed); }
+    uint32_t hash(int seed) const { return ::doris::HashUtil::hash(this, sizeof(*this), seed); }
 
     int day_of_year() const { return daynr() - calc_daynr(this->year(), 1, 1) + 1; }
 
