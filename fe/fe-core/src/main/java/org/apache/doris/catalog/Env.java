apache/doris

commit d257059e6b777472b2b6352b97101b84c5fd9afd

[refactor](remove hadoop dpp) remove hadoop dpp code since it is not used (#16009)

fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -145,14 +145,10 @@ import org.apache.doris.journal.JournalCursor;
 import org.apache.doris.journal.JournalEntity;
 import org.apache.doris.journal.bdbje.Timestamp;
 import org.apache.doris.load.DeleteHandler;
-import org.apache.doris.load.EtlJobType;
 import org.apache.doris.load.ExportChecker;
 import org.apache.doris.load.ExportJob;
 import org.apache.doris.load.ExportMgr;
 import org.apache.doris.load.Load;
-import org.apache.doris.load.LoadChecker;
-import org.apache.doris.load.LoadErrorHub;
-import org.apache.doris.load.LoadJob;
 import org.apache.doris.load.StreamLoadRecordMgr;
 import org.apache.doris.load.loadv2.LoadEtlChecker;
 import org.apache.doris.load.loadv2.LoadJobScheduler;
@@ -1354,9 +1350,6 @@ public class Env {
         // heartbeat mgr
         heartbeatMgr.setMaster(clusterId, token, epoch);
         heartbeatMgr.start();
-        // Load checker
-        LoadChecker.init(Config.load_checker_interval_second * 1000L);
-        LoadChecker.startAll();
         // New load scheduler
         pendingLoadTaskScheduler.start();
         loadingLoadTaskScheduler.start();
@@ -1714,55 +1707,6 @@ public class Env {
         return getInternalCatalog().loadDb(dis, checksum);
     }
 
-    public long loadLoadJob(DataInputStream dis, long checksum) throws IOException, DdlException {
-        // load jobs
-        int jobSize = dis.readInt();
-        long newChecksum = checksum ^ jobSize;
-        for (int i = 0; i < jobSize; i++) {
-            long dbId = dis.readLong();
-            newChecksum ^= dbId;
-
-            int loadJobCount = dis.readInt();
-            newChecksum ^= loadJobCount;
-            long currentTimeMs = System.currentTimeMillis();
-            for (int j = 0; j < loadJobCount; j++) {
-                LoadJob job = new LoadJob();
-                job.readFields(dis);
-
-                // Delete the history load jobs that are older than
-                // LABEL_KEEP_MAX_MS
-                // This job must be FINISHED or CANCELLED
-                if (!job.isExpired(currentTimeMs)) {
-                    if (job.getEtlJobType() != EtlJobType.HADOOP) {
-                        LOG.warn("job {} with type is deprecated, skip it", job.getId(), job.getEtlJobType());
-                        continue;
-                    }
-                    load.unprotectAddLoadJob(job, true /* replay */);
-                }
-            }
-        }
-
-        // delete jobs
-        // Delete job has been moved to DeleteHandler. Here the jobSize is always 0, we need do nothing.
-        jobSize = dis.readInt();
-        Preconditions.checkState(jobSize == 0, jobSize);
-        newChecksum ^= jobSize;
-
-        // load error hub info
-        LoadErrorHub.Param param = new LoadErrorHub.Param();
-        param.readFields(dis);
-        load.setLoadErrorHubInfo(param);
-
-        // 4. load delete jobs
-        // Delete job has been moved to DeleteHandler. Here the jobSize is always 0, we need do nothing.
-        int deleteJobSize = dis.readInt();
-        Preconditions.checkState(deleteJobSize == 0, deleteJobSize);
-        newChecksum ^= deleteJobSize;
-
-        LOG.info("finished replay loadJob from image");
-        return newChecksum;
-    }
-
     public long loadExportJob(DataInputStream dis, long checksum) throws IOException, DdlException {
         long curTime = System.currentTimeMillis();
         long newChecksum = checksum;
@@ -2058,45 +2002,6 @@ public class Env {
 
     public long saveDb(CountingDataOutputStream dos, long checksum) throws IOException {
         return getInternalCatalog().saveDb(dos, checksum);
-    }
-
-    public long saveLoadJob(CountingDataOutputStream dos, long checksum) throws IOException {
-        // 1. save load.dbToLoadJob
-        Map<Long, List<LoadJob>> dbToLoadJob = load.getDbToLoadJobs();
-        int jobSize = dbToLoadJob.size();
-        checksum ^= jobSize;
-        dos.writeInt(jobSize);
-        for (Entry<Long, List<LoadJob>> entry : dbToLoadJob.entrySet()) {
-            long dbId = entry.getKey();
-            checksum ^= dbId;
-            dos.writeLong(dbId);
-
-            List<LoadJob> loadJobs = entry.getValue();
-            int loadJobCount = loadJobs.size();
-            checksum ^= loadJobCount;
-            dos.writeInt(loadJobCount);
-            for (LoadJob job : loadJobs) {
-                job.write(dos);
-            }
-        }
-
-        // 2. save delete jobs
-        // delete jobs are moved to DeleteHandler. So here we just set job size as 0.
-        jobSize = 0;
-        checksum ^= jobSize;
-        dos.writeInt(jobSize);
-
-        // 3. load error hub info
-        LoadErrorHub.Param param = load.getLoadErrorHubInfo();
-        param.write(dos);
-
-        // 4. save delete load job info
-        // delete jobs are moved to DeleteHandler. So here we just set job size as 0.
-        int deleteJobSize = 0;
-        checksum ^= deleteJobSize;
-        dos.writeInt(deleteJobSize);
-
-        return checksum;
     }
 
     public long saveExportJob(CountingDataOutputStream dos, long checksum) throws IOException {

fe/fe-core/src/main/java/org/apache/doris/persist/meta/MetaPersistMethod.java
@@ -71,12 +71,6 @@ public class MetaPersistMethod {
                 metaPersistMethod.writeMethod =
                         Env.class.getDeclaredMethod("saveDb", CountingDataOutputStream.class, long.class);
                 break;
-            case "loadJob":
-                metaPersistMethod.readMethod =
-                        Env.class.getDeclaredMethod("loadLoadJob", DataInputStream.class, long.class);
-                metaPersistMethod.writeMethod =
-                        Env.class.getDeclaredMethod("saveLoadJob", CountingDataOutputStream.class, long.class);
-                break;
             case "alterJob":
                 metaPersistMethod.readMethod =
                         Env.class.getDeclaredMethod("loadAlterJob", DataInputStream.class, long.class);

fe/fe-core/src/main/java/org/apache/doris/persist/meta/PersistMetaModules.java
@@ -35,7 +35,7 @@ public class PersistMetaModules {
     public static final List<MetaPersistMethod> MODULES_IN_ORDER;
 
     public static final ImmutableList<String> MODULE_NAMES = ImmutableList.of(
-            "masterInfo", "frontends", "backends", "datasource", "db", "loadJob", "alterJob", "recycleBin",
+            "masterInfo", "frontends", "backends", "datasource", "db", "alterJob", "recycleBin",
             "globalVariable", "cluster", "broker", "resources", "exportJob", "syncJob", "backupHandler",
             "paloAuth", "transactionState", "colocateTableIndex", "routineLoadJobs", "loadJobV2", "smallFiles",
             "plugins", "deleteHandler", "sqlBlockRule", "policy", "mtmvJobManager", "cooldownJob");

be/src/agent/agent_server.cpp
@@ -165,8 +165,7 @@ void AgentServer::submit_tasks(TAgentResult& agent_result,
                         "task(signature={}) has wrong request member = push_req", signature);
                 break;
             }
-            if (task.push_req.push_type == TPushType::LOAD ||
-                task.push_req.push_type == TPushType::LOAD_V2) {
+            if (task.push_req.push_type == TPushType::LOAD_V2) {
                 _push_workers->submit_task(task);
             } else if (task.push_req.push_type == TPushType::DELETE) {
                 _delete_workers->submit_task(task);

be/src/olap/olap_common.h
@@ -184,9 +184,9 @@ enum OLAPCompressionType {
 };
 
 enum PushType {
-    PUSH_NORMAL = 1,          // for broker/hadoop load
+    PUSH_NORMAL = 1,          // for broker/hadoop load, not used any more
     PUSH_FOR_DELETE = 2,      // for delete
-    PUSH_FOR_LOAD_DELETE = 3, // not use
+    PUSH_FOR_LOAD_DELETE = 3, // not used any more
     PUSH_NORMAL_V2 = 4,       // for spark load
 };
 

be/src/olap/push_handler.h
@@ -34,8 +34,6 @@
 
 namespace doris {
 
-class BinaryFile;
-class BinaryReader;
 struct ColumnMapping;
 class RowCursor;
 
@@ -56,11 +54,7 @@ public:
 
 private:
     Status _convert_v2(TabletSharedPtr cur_tablet, RowsetSharedPtr* cur_rowset,
-                       TabletSchemaSPtr tablet_schema);
-    // Convert local data file to internal formatted delta,
-    // return new delta's rowset
-    Status _convert(TabletSharedPtr cur_tablet, RowsetSharedPtr* cur_rowset,
-                    TabletSchemaSPtr tablet_schema);
+                       TabletSchemaSPtr tablet_schema, PushType push_type);
 
     // Only for debug
     std::string _debug_version_list(const Versions& versions) const;
@@ -78,102 +72,6 @@ private:
     int64_t _write_bytes = 0;
     int64_t _write_rows = 0;
     DISALLOW_COPY_AND_ASSIGN(PushHandler);
-};
-
-// package FileHandlerWithBuf to read header of dpp output file
-class BinaryFile : public FileHandlerWithBuf {
-public:
-    BinaryFile() = default;
-    virtual ~BinaryFile() { close(); }
-
-    Status init(const char* path);
-
-    size_t header_size() const { return _header.size(); }
-    size_t file_length() const { return _header.file_length(); }
-    uint32_t checksum() const { return _header.checksum(); }
-    SchemaHash schema_hash() const { return _header.message().schema_hash(); }
-
-private:
-    FileHeader<OLAPRawDeltaHeaderMessage, int32_t, FileHandlerWithBuf> _header;
-
-    DISALLOW_COPY_AND_ASSIGN(BinaryFile);
-};
-
-class IBinaryReader {
-public:
-    static IBinaryReader* create(bool need_decompress);
-    virtual ~IBinaryReader() = default;
-
-    virtual Status init(TabletSchemaSPtr tablet_schema, BinaryFile* file) = 0;
-    virtual Status finalize() = 0;
-
-    virtual Status next(RowCursor* row) = 0;
-
-    virtual bool eof() = 0;
-
-    // call this function after finalize()
-    bool validate_checksum() { return _adler_checksum == _file->checksum(); }
-
-protected:
-    IBinaryReader()
-            : _file(nullptr),
-              _content_len(0),
-              _curr(0),
-              _adler_checksum(ADLER32_INIT),
-              _ready(false) {}
-
-    BinaryFile* _file;
-    TabletSchemaSPtr _tablet_schema;
-    size_t _content_len;
-    size_t _curr;
-    uint32_t _adler_checksum;
-    bool _ready;
-};
-
-// input file reader for Protobuffer format
-class BinaryReader : public IBinaryReader {
-public:
-    explicit BinaryReader();
-    ~BinaryReader() override { finalize(); }
-
-    Status init(TabletSchemaSPtr tablet_schema, BinaryFile* file) override;
-    Status finalize() override;
-
-    Status next(RowCursor* row) override;
-
-    bool eof() override { return _curr >= _content_len; }
-
-private:
-    char* _row_buf;
-    size_t _row_buf_size;
-};
-
-class LzoBinaryReader : public IBinaryReader {
-public:
-    explicit LzoBinaryReader();
-    ~LzoBinaryReader() override { finalize(); }
-
-    Status init(TabletSchemaSPtr tablet_schema, BinaryFile* file) override;
-    Status finalize() override;
-
-    Status next(RowCursor* row) override;
-
-    bool eof() override { return _curr >= _content_len && _row_num == 0; }
-
-private:
-    Status _next_block();
-
-    using RowNumType = uint32_t;
-    using CompressedSizeType = uint64_t;
-
-    char* _row_buf;
-    char* _row_compressed_buf;
-    char* _row_info_buf;
-    size_t _max_row_num;
-    size_t _max_row_buf_size;
-    size_t _max_compressed_buf_size;
-    size_t _row_num;
-    size_t _next_row_start;
 };
 
 class PushBrokerReader {

be/src/olap/task/engine_batch_load_task.cpp
@@ -60,7 +60,7 @@ EngineBatchLoadTask::~EngineBatchLoadTask() {}
 Status EngineBatchLoadTask::execute() {
     SCOPED_ATTACH_TASK(_mem_tracker);
     Status status;
-    if (_push_req.push_type == TPushType::LOAD || _push_req.push_type == TPushType::LOAD_V2) {
+    if (_push_req.push_type == TPushType::LOAD_V2) {
         RETURN_IF_ERROR(_init());
         uint32_t retry_time = 0;
         while (retry_time < PUSH_MAX_RETRY) {
@@ -95,7 +95,7 @@ Status EngineBatchLoadTask::_init() {
     }
 
     // check disk capacity
-    if (_push_req.push_type == TPushType::LOAD || _push_req.push_type == TPushType::LOAD_V2) {
+    if (_push_req.push_type == TPushType::LOAD_V2) {
         if (tablet->data_dir()->reach_capacity_limit(_push_req.__isset.http_file_size)) {
             return Status::IOError("Disk does not have enough capacity");
         }
@@ -267,11 +267,7 @@ Status EngineBatchLoadTask::_push(const TPushReq& request,
         return Status::InternalError("could not find tablet {}", request.tablet_id);
     }
 
-    PushType type = PUSH_NORMAL;
-    if (request.push_type == TPushType::LOAD_V2) {
-        type = PUSH_NORMAL_V2;
-    }
-
+    PushType type = PUSH_NORMAL_V2;
     int64_t duration_ns = 0;
     PushHandler push_handler;
     if (!request.__isset.transaction_id) {

commit 3463df3f3c40fc8c87c99b76aa7139f45ce21c19

[bugfix](metareader) meta reader could not load image (#16148)
This bug is introduced by PR #16009.
Co-authored-by: yiguolei <yiguolei@gmail.com>

fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -149,6 +149,7 @@ import org.apache.doris.load.ExportChecker;
 import org.apache.doris.load.ExportJob;
 import org.apache.doris.load.ExportMgr;
 import org.apache.doris.load.Load;
+import org.apache.doris.load.LoadErrorHub;
 import org.apache.doris.load.StreamLoadRecordMgr;
 import org.apache.doris.load.loadv2.LoadEtlChecker;
 import org.apache.doris.load.loadv2.LoadJobScheduler;
@@ -1709,6 +1710,62 @@ public class Env {
 
     public long loadDb(DataInputStream dis, long checksum) throws IOException, DdlException {
         return getInternalCatalog().loadDb(dis, checksum);
+    }
+
+    public long saveLoadJob(CountingDataOutputStream dos, long checksum) throws IOException {
+        // 1. save load.dbToLoadJob, force set to 0 since there should be none load jobs
+        int jobSize = 0;
+        checksum ^= jobSize;
+        dos.writeInt(jobSize);
+
+        // 2. save delete jobs
+        // delete jobs are moved to DeleteHandler. So here we just set job size as 0.
+        jobSize = 0;
+        checksum ^= jobSize;
+        dos.writeInt(jobSize);
+
+        // 3. load error hub info
+        LoadErrorHub.Param param = load.getLoadErrorHubInfo();
+        param.write(dos);
+
+        // 4. save delete load job info
+        // delete jobs are moved to DeleteHandler. So here we just set job size as 0.
+        int deleteJobSize = 0;
+        checksum ^= deleteJobSize;
+        dos.writeInt(deleteJobSize);
+        return checksum;
+    }
+
+    public long loadLoadJob(DataInputStream dis, long checksum) throws IOException, DdlException {
+        // load jobs
+        int jobSize = dis.readInt();
+        long newChecksum = checksum ^ jobSize;
+
+        if (jobSize > 0) {
+            LOG.warn("there should be no load jobs, please rollback to 1.2.x and check if there are hadoop load jobs");
+            throw new RuntimeException("there should be no load jobs, please rollback to 1.2.x "
+                    + "and check if there are hadoop load jobs");
+        }
+
+        // delete jobs
+        // Delete job has been moved to DeleteHandler. Here the jobSize is always 0, we need do nothing.
+        jobSize = dis.readInt();
+        Preconditions.checkState(jobSize == 0, jobSize);
+        newChecksum ^= jobSize;
+
+        // load error hub info
+        LoadErrorHub.Param param = new LoadErrorHub.Param();
+        param.readFields(dis);
+        load.setLoadErrorHubInfo(param);
+
+        // 4. load delete jobs
+        // Delete job has been moved to DeleteHandler. Here the jobSize is always 0, we need do nothing.
+        int deleteJobSize = dis.readInt();
+        Preconditions.checkState(deleteJobSize == 0, deleteJobSize);
+        newChecksum ^= deleteJobSize;
+
+        LOG.info("finished replay loadJob from image");
+        return newChecksum;
     }
 
     public long loadExportJob(DataInputStream dis, long checksum) throws IOException, DdlException {

fe/fe-core/src/main/java/org/apache/doris/persist/meta/MetaPersistMethod.java
@@ -71,6 +71,12 @@ public class MetaPersistMethod {
                 metaPersistMethod.writeMethod =
                         Env.class.getDeclaredMethod("saveDb", CountingDataOutputStream.class, long.class);
                 break;
+            case "loadJob":
+                metaPersistMethod.readMethod =
+                        Env.class.getDeclaredMethod("loadLoadJob", DataInputStream.class, long.class);
+                metaPersistMethod.writeMethod =
+                        Env.class.getDeclaredMethod("saveLoadJob", CountingDataOutputStream.class, long.class);
+                break;
             case "alterJob":
                 metaPersistMethod.readMethod =
                         Env.class.getDeclaredMethod("loadAlterJob", DataInputStream.class, long.class);

fe/fe-core/src/main/java/org/apache/doris/persist/meta/PersistMetaModules.java
@@ -35,7 +35,7 @@ public class PersistMetaModules {
     public static final List<MetaPersistMethod> MODULES_IN_ORDER;
 
     public static final ImmutableList<String> MODULE_NAMES = ImmutableList.of(
-            "masterInfo", "frontends", "backends", "datasource", "db", "alterJob", "recycleBin",
+            "masterInfo", "frontends", "backends", "datasource", "db", "loadJob", "alterJob", "recycleBin",
             "globalVariable", "cluster", "broker", "resources", "exportJob", "syncJob", "backupHandler",
             "paloAuth", "transactionState", "colocateTableIndex", "routineLoadJobs", "loadJobV2", "smallFiles",
             "plugins", "deleteHandler", "sqlBlockRule", "policy", "mtmvJobManager", "cooldownJob");