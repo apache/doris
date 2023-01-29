eb7da1c0eedecc7db01151bda3f12aa7c88ef00b
abmdocrt <Yukang.Lian2022@gmail.com>
2023-01-29 14:26:08 +0800

    [fix](datatype) fix some bugs about data type array datetimev2 and decimalv3 (#16132)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/DateLiteral.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/DateLiteral.java
@@ -546,6 +546,7 @@ public class DateLiteral extends LiteralExpr {
         if (type.isDate() || type.isDateV2()) {
             return String.format("%04d-%02d-%02d", year, month, day);
         } else if (type.isDatetimeV2()) {
+            int scale = ((ScalarType) type).getScalarScale();
             long ms = Double.valueOf(microsecond / (int) (Math.pow(10, 6 - ((ScalarType) type).getScalarScale()))
                     * (Math.pow(10, 6 - ((ScalarType) type).getScalarScale()))).longValue();
             String tmp = String.format("%04d-%02d-%02d %02d:%02d:%02d",
@@ -553,7 +554,7 @@ public class DateLiteral extends LiteralExpr {
             if (ms == 0) {
                 return tmp;
             }
-            return tmp + String.format(".%06d", ms);
+            return tmp + String.format(".%06d", ms).substring(0, scale + 1);
         } else {
             return String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
         }
