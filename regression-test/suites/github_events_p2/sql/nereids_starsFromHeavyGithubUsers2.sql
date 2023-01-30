f4ca7a7f6f048f22c1c2653634983c9b3cbb4594
AKIRA <33112463+Kikyou1997@users.noreply.github.com>
2023-01-30 18:33:31 +0800

    [test](pipeline) Run nereids cases in p1/p2 (#16130)

---
+++ regression-test/suites/github_events_p2/sql/nereids_starsFromHeavyGithubUsers2.sql
@@ -0,0 +1,17 @@
+SET enable_nereids_planner=TRUE;
+SET enable_fallback_to_original_planner=FALSE;
+SELECT
+    repo_name,
+    count()
+FROM github_events
+WHERE (event_type = 'WatchEvent') AND (actor_login IN
+(
+    SELECT actor_login
+    FROM github_events
+    WHERE (event_type = 'PullRequestEvent') AND (action = 'opened')
+    GROUP BY actor_login
+    HAVING count() >= 10
+))
+GROUP BY repo_name
+ORDER BY count() DESC
+LIMIT 50
