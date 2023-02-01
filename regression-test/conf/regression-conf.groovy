3174cc56389c764aa970bd022169c910002d48f9
gaoxin <AshinGau@outlook.com>
2023-02-01 21:30:36 +0800

    [test](regression) add tvf regression to test the remove of eof check

--- regression-test/conf/regression-conf.groovy
+++ regression-test/conf/regression-conf.groovy
@@ -96,6 +96,7 @@ es_8_port=39200
 enableExternalHiveTest = false
 extHiveHmsHost = "***.**.**.**"
 extHiveHmsPort = 7004
+extHdfsPort = 4007
 extHiveHmsUser = "****"
 extHiveHmsPassword= "***********"
 
