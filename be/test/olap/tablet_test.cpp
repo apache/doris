e774387d70da59081e8230879a3141c5774ae869
ByteYue <yj976240184@gmail.com>
2023-02-03 12:55:20 +0800

    format code

--- be/test/olap/tablet_test.cpp
+++ be/test/olap/tablet_test.cpp
@@ -280,7 +280,7 @@ TEST_F(TestTablet, pad_rowset) {
 TEST_F(TestTablet, cooldown_policy) {
     std::vector<RowsetMetaSharedPtr> rs_metas;
     RowsetMetaSharedPtr ptr1(new RowsetMeta());
-    init_rs_meta(ptr1,0, 2, 200);
+    init_rs_meta(ptr1, 0, 2, 200);
     rs_metas.push_back(ptr1);
     RowsetSharedPtr rowset1 = make_shared<BetaRowset>(nullptr, "", ptr1);
 
