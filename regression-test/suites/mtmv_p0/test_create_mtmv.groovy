8f7521bbd7ea8da53333493cb817493e2724f2ec
huangzhaowei <carlmartinmax@gmail.com>
2023-02-04 20:17:45 +0800

    [feature-wip](MTMV) Support refresh mtmv (#16218)
    Support using this sql to refresh mtmv manually. It can generate a mtmv task right now.
    
    ```
    REFRESH MATERIALIZED VIEW test_mv_view [complete];
    ```
    
    You can use `show mtmv task` to show the latest task.
    
    In this pr, I also try to clear the mtmv tasks when drop the mtmv to make sure test suite to be right

--- regression-test/suites/mtmv_p0/test_create_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_create_mtmv.groovy
@@ -66,10 +66,12 @@ suite("test_create_mtmv") {
     def index = show_task_meta.indexOf(['State', 'CHAR'])
     def query = "SHOW MTMV TASK ON ${mvName}"
     def show_task_result
-    def state
+    def state = "PENDING"
     do {
         show_task_result = sql "${query}"
-        state = show_task_result.last().get(index)
+        if (!show_task_result.isEmpty()) {
+            state = show_task_result.last().get(index)
+        }
         println "The state of ${query} is ${state}"
         Thread.sleep(1000);
     } while (state.equals('PENDING') || state.equals('RUNNING'))
@@ -93,15 +95,35 @@ suite("test_create_mtmv") {
         SELECT ${tableName}.username, ${tableNamePv}.pv FROM ${tableName}, ${tableNamePv} WHERE ${tableName}.id=${tableNamePv}.id;
     """
     // wait task to be finished to avoid task leak in suite.
+    state = "PENDING"
     do {
         show_task_result = sql "${query}"
-        state = show_task_result.last().get(index)
+        if (!show_task_result.isEmpty()) {
+            state = show_task_result.last().get(index)
+        }
         println "The state of ${query} is ${state}"
         Thread.sleep(1000);
     } while (state.equals('PENDING') || state.equals('RUNNING'))
 
     def show_job_result = sql "SHOW MTMV JOB ON ${mvName}"
     assertEquals 1, show_job_result.size()
+
+    // test REFRESH make sure only define one mv and already run a task.
+    sql """
+        REFRESH MATERIALIZED VIEW ${mvName} COMPLETE
+    """
+    state = "PENDING"
+    do {
+        show_task_result = sql "${query}"
+        if (!show_task_result.isEmpty()) {
+            state = show_task_result.last().get(index)
+        }
+        println "The state of ${query} is ${state}"
+        Thread.sleep(1000);
+    } while (state.equals('PENDING') || state.equals('RUNNING'))
+
+    assertEquals 'SUCCESS', state, show_task_result.last().toString()
+    assertEquals 2, show_task_result.size()
 
     sql """
         DROP MATERIALIZED VIEW ${mvName}
