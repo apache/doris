43b60c08b5d5e5341dcfc755075804325ee1af13
wudi <676366545@qq.com>
2023-02-04 23:05:00 +0800

    [Bug](vectorized)Fix reading date and datetime types conversion error (#16252)
    from pr #15612, Type conversion error when reading date and datetime types
    
    
    
    ---------
    
    Co-authored-by: wudi <>

--- be/src/util/arrow/block_convertor.cpp
+++ be/src/util/arrow/block_convertor.cpp
@@ -107,7 +107,8 @@ public:
             case vectorized::TypeIndex::Date:
             case vectorized::TypeIndex::DateTime: {
                 char buf[64];
-                const DateTimeValue* time_val = (const DateTimeValue*)(data_ref.data);
+                const vectorized::VecDateTimeValue* time_val =
+                        (const vectorized::VecDateTimeValue*)(data_ref.data);
                 int len = time_val->to_buffer(buf);
                 ARROW_RETURN_NOT_OK(builder.Append(buf, len));
                 break;
