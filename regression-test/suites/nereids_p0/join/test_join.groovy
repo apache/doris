788ca72272e6be3ce652473eeeefb741ff73854b
sohardforaname <organic_chemistry@foxmail.com>
2023-02-05 23:50:41 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/join/test_join.groovy
+++ regression-test/suites/nereids_p0/join/test_join.groovy
@@ -174,14 +174,14 @@ suite("test_join", "nereids_p0") {
                 order by 1, 2, 3, 4, 5 limit 65535"""
     }
 
-    explain {
+    test {
         sql """
             verbose select a.k1, b.k1, count(a.k1), count(b.k1) from test a inner join baseall b
                 on a.k2 = b.k2 and a.k1 > 0
                 inner join bigtable c on a.k3 = c.k3 and b.k1 = c.k1 + 1 and c.k3 > 0
                 group by 1, 2 order by 1, 2 limit 65535
         """
-        contains " <- expr_(k1 + 1)"
+        result([1,3,1,1],[2,2,1,1])
     }
 
     // test_left_join
