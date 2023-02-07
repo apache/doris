8cafda9611a92c5b3eaaca5023ea0b9d8bc3c25a
Ashin Gau <AshinGau@users.noreply.github.com>
2023-02-07 11:32:57 +0800

    [improvement](multi-catalog) push down all predicates into rowgroup/page filtering for ParquetReader (#16388)
    Tow improvements:
    1. Refactor rowgroup&page filtering in `ParquetReader`, and use the operator overloading of Doris native c++ type to process comparison.
    2. Support decimal/decimal v3/date/datev2/datetime/datetimev2

--- be/src/vec/exec/format/parquet/vparquet_page_index.cpp
+++ be/src/vec/exec/format/parquet/vparquet_page_index.cpp
@@ -40,15 +40,17 @@ Status PageIndex::create_skipped_row_range(tparquet::OffsetIndex& offset_index,
 Status PageIndex::collect_skipped_page_range(tparquet::ColumnIndex* column_index,
                                              ColumnValueRangeType& col_val_range,
                                              const FieldSchema* col_schema,
-                                             std::vector<int>& skipped_ranges) {
+                                             std::vector<int>& skipped_ranges,
+                                             const cctz::time_zone& ctz) {
     const std::vector<std::string>& encoded_min_vals = column_index->min_values;
     const std::vector<std::string>& encoded_max_vals = column_index->max_values;
     DCHECK_EQ(encoded_min_vals.size(), encoded_max_vals.size());
 
     const int num_of_pages = column_index->null_pages.size();
     for (int page_id = 0; page_id < num_of_pages; page_id++) {
-        if (determine_filter_min_max(col_val_range, col_schema, encoded_min_vals[page_id],
-                                     encoded_max_vals[page_id])) {
+        if (ParquetPredicate::filter_by_min_max(col_val_range, col_schema,
+                                                encoded_min_vals[page_id],
+                                                encoded_max_vals[page_id], ctz)) {
             skipped_ranges.emplace_back(page_id);
         }
     }
