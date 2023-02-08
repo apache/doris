cc11695402db871baba9aaed514cb37e490b236d
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2023-02-08 16:51:30 +0800

    [Fix](multi catalog)(planner) Fix external table statistic collection bug (#16486)
    Add index id to column statistic id. Refresh statistic cache after analyze.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -27,6 +27,7 @@ import org.apache.doris.thrift.TResourceLimit;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -38,6 +39,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
