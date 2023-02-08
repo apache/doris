555cea6db7c180430a9a6a5954dfee3748d72664
xy720 <xuyang25@baidu.com>
2023-02-08 16:15:32 +0800

    [fix] fix fe ut

--- fe/fe-core/src/main/java/org/apache/doris/analysis/Expr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/Expr.java
@@ -1354,7 +1354,7 @@ public abstract class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
     }
 
     public Expr checkTypeCompatibility(Type targetType) throws AnalysisException {
-        if (targetType.isComplexType() && targetType.getPrimitiveType() == type.getPrimitiveType()) {
+        if (!targetType.isComplexType() && targetType.getPrimitiveType() == type.getPrimitiveType()) {
             if (targetType.isDecimalV2() && type.isDecimalV2()) {
                 return this;
             } else if (!PrimitiveType.typeWithPrecision.contains(type.getPrimitiveType())) {
