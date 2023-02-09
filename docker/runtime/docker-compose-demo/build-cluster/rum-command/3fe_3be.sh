bdcd38306ff155dd46a0193285634906039ecab6
Yijia Su <suyijia@selectdb.com>
2023-02-09 10:33:21 +0800

    change style

--- docker/runtime/docker-compose-demo/build-cluster/rum-command/3fe_3be.sh
+++ docker/runtime/docker-compose-demo/build-cluster/rum-command/3fe_3be.sh
@@ -1,74 +1,74 @@
 docker network create --driver bridge --subnet=172.20.80.0/24 doris-network
 docker run -itd \
---name=fe-01 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env FE_ID=1 \
--p 8031:8030 \
--p 9031:9030 \
--v /data/fe-01/doris-meta:/opt/apache-doris/fe/doris-meta \
--v /data/fe-01/conf:/opt/apache-doris/fe/conf \
--v /data/fe-01/log:/opt/apache-doris/fe/log \
---network=doris-network \
---ip=172.20.80.2 \
-apache/doris:1.2.1-fe-x86_64
+    --name=fe-01 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env FE_ID=1 \
+    -p 8031:8030 \
+    -p 9031:9030 \
+    -v /data/fe-01/doris-meta:/opt/apache-doris/fe/doris-meta \
+    -v /data/fe-01/conf:/opt/apache-doris/fe/conf \
+    -v /data/fe-01/log:/opt/apache-doris/fe/log \
+    --network=doris-network \
+    --ip=172.20.80.2 \
+    apache/doris:1.2.1-fe-x86_64
 
 docker run -itd \
---name=fe-02 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env FE_ID=2 \
--p 8032:8030 \
--p 9032:9030 \
--v /data/fe-02/doris-meta:/opt/apache-doris/fe/doris-meta \
--v /data/fe-02/conf:/opt/apache-doris/fe/conf \
--v /data/fe-02/log:/opt/apache-doris/fe/log \
---network=doris-network \
---ip=172.20.80.3 \
-apache/doris:1.2.1-fe-x86_64
+    --name=fe-02 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env FE_ID=2 \
+    -p 8032:8030 \
+    -p 9032:9030 \
+    -v /data/fe-02/doris-meta:/opt/apache-doris/fe/doris-meta \
+    -v /data/fe-02/conf:/opt/apache-doris/fe/conf \
+    -v /data/fe-02/log:/opt/apache-doris/fe/log \
+    --network=doris-network \
+    --ip=172.20.80.3 \
+    apache/doris:1.2.1-fe-x86_64
 
 docker run -itd \
---name=fe-03 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env FE_ID=3 \
--p 8033:8030 \
--p 9033:9030 \
--v /data/fe-03/doris-meta:/opt/apache-doris/fe/doris-meta \
--v /data/fe-03/conf:/opt/apache-doris/fe/conf \
--v /data/fe-03/log:/opt/apache-doris/fe/log \
---network=doris-network \
---ip=172.20.80.4 \
-apache/doris:1.2.1-fe-x86_64
+    --name=fe-03 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env FE_ID=3 \
+    -p 8033:8030 \
+    -p 9033:9030 \
+    -v /data/fe-03/doris-meta:/opt/apache-doris/fe/doris-meta \
+    -v /data/fe-03/conf:/opt/apache-doris/fe/conf \
+    -v /data/fe-03/log:/opt/apache-doris/fe/log \
+    --network=doris-network \
+    --ip=172.20.80.4 \
+    apache/doris:1.2.1-fe-x86_64
 
 docker run -itd \
---name=be-01 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env BE_ADDR="172.20.80.5:9050" \
--p 8041:8040 \
--v /data/be-01/storage:/opt/apache-doris/be/storage \
--v /data/be-01/conf:/opt/apache-doris/be/conf \
--v /data/be-01/log:/opt/apache-doris/be/log \
---network=doris-network \
---ip=172.20.80.5 \
-apache/doris:1.2.1-be-x86_64
+    --name=be-01 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env BE_ADDR="172.20.80.5:9050" \
+    -p 8041:8040 \
+    -v /data/be-01/storage:/opt/apache-doris/be/storage \
+    -v /data/be-01/conf:/opt/apache-doris/be/conf \
+    -v /data/be-01/log:/opt/apache-doris/be/log \
+    --network=doris-network \
+    --ip=172.20.80.5 \
+    apache/doris:1.2.1-be-x86_64
 
 docker run -itd \
---name=be-02 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env BE_ADDR="172.20.80.6:9050" \
--p 8042:8040 \
--v /data/be-02/storage:/opt/apache-doris/be/storage \
--v /data/be-02/conf:/opt/apache-doris/be/conf \
--v /data/be-02/log:/opt/apache-doris/be/log \
---network=doris-network \
---ip=172.20.80.6 \
-apache/doris:1.2.1-be-x86_64
+    --name=be-02 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env BE_ADDR="172.20.80.6:9050" \
+    -p 8042:8040 \
+    -v /data/be-02/storage:/opt/apache-doris/be/storage \
+    -v /data/be-02/conf:/opt/apache-doris/be/conf \
+    -v /data/be-02/log:/opt/apache-doris/be/log \
+    --network=doris-network \
+    --ip=172.20.80.6 \
+    apache/doris:1.2.1-be-x86_64
 
 docker run -itd \
---name=be-03 \
---env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
---env BE_ADDR="172.20.80.7:9050" \
--p 8043:8040 \
--v /data/be-03/storage:/opt/apache-doris/be/storage \
--v /data/be-03/conf:/opt/apache-doris/be/conf \
--v /data/be-03/log:/opt/apache-doris/be/log \
---network=doris-network \
---ip=172.20.80.7 \
+    --name=be-03 \
+    --env FE_SERVERS="fe1:172.20.80.2:9010,fe2:172.20.80.3:9010,fe3:172.20.80.4:9010" \
+    --env BE_ADDR="172.20.80.7:9050" \
+    -p 8043:8040 \
+    -v /data/be-03/storage:/opt/apache-doris/be/storage \
+    -v /data/be-03/conf:/opt/apache-doris/be/conf \
+    -v /data/be-03/log:/opt/apache-doris/be/log \
+    --network=doris-network \
+    --ip=172.20.80.7 \
