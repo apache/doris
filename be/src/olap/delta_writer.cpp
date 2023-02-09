9852ca58ffeab5c97eac664d020fd96cf0224c85
morningman <morningman@163.com>
2023-02-09 09:42:39 +0800

    [bugfix](writer) add _is_closed state to DeltaWriter and avoid write/close core after close
    cherry-pick #16453.
    But different with master, when delta write close twice, it will return OK.

--- be/src/olap/delta_writer.cpp
+++ be/src/olap/delta_writer.cpp
@@ -218,6 +218,12 @@ Status DeltaWriter::write(const vectorized::Block* block, const std::vector<int>
         return _cancel_status;
     }
 
+    if (_is_closed) {
+        LOG(WARNING) << "write block after closed tablet_id=" << _req.tablet_id
+                     << " load_id=" << _req.load_id << " txn_id=" << _req.txn_id;
+        return Status::InternalError("already closed");
+    }
+
     _total_received_rows += row_idxs.size();
     _mem_table->insert(block, row_idxs);
 
@@ -331,8 +337,15 @@ Status DeltaWriter::close() {
         return _cancel_status;
     }
 
+    if (_is_closed) {
+        LOG(WARNING) << "close after closed tablet_id=" << _req.tablet_id
+                     << " load_id=" << _req.load_id << " txn_id=" << _req.txn_id;
+        return Status::OK();
+    }
+
     auto s = _flush_memtable_async();
     _mem_table.reset();
+    _is_closed = true;
     if (OLAP_UNLIKELY(!s.ok())) {
         return s;
     } else {
--- be/src/olap/delta_writer.h
+++ be/src/olap/delta_writer.h
@@ -131,6 +131,7 @@ private:
 
     bool _is_init = false;
     bool _is_cancelled = false;
+    bool _is_closed = false;
     Status _cancel_status;
     WriteRequest _req;
     TabletSharedPtr _tablet;
