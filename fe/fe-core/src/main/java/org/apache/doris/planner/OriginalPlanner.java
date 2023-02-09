608e659b1a7ea2269c06c41142db1244d525c8a6
Kang <kxiao.tiger@gmail.com>
2023-02-09 23:05:08 +0800

    add null pointer check

--- fe/fe-core/src/main/java/org/apache/doris/planner/OriginalPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/OriginalPlanner.java
@@ -448,6 +448,7 @@ public class OriginalPlanner extends Planner {
             SortNode sortNode = (SortNode) node;
             PlanNode child = sortNode.getChild(0);
             if (child instanceof OlapScanNode && sortNode.getLimit() > 0
+                    && ConnectContext.get() != null && ConnectContext.get().getSessionVariable() != null
                     && sortNode.getLimit() <= ConnectContext.get().getSessionVariable().topnOptLimitThreshold
                     && sortNode.getSortInfo().getMaterializedOrderingExprs().size() > 0) {
                 Expr firstSortExpr = sortNode.getSortInfo().getMaterializedOrderingExprs().get(0);
