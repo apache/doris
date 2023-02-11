481dbc7714f27861dec72ca1d20f4e385feee141
chenlinzhong <490103404@qq.com>
2023-02-11 23:33:08 +0800

    Update be/src/service/internal_service.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -821,7 +821,7 @@ void PInternalServiceImpl::reset_rpc_channel(google::protobuf::RpcController* co
                                              PResetRPCChannelResponse* response,
                                              google::protobuf::Closure* done) {
     DorisMetrics::instance()->reset_rpc_channel->increment(1);
-    _light_work_pool.offer([this, controller, request, response, done]() {
+    _light_work_pool.offer([ controller, request, response, done]() {
         brpc::ClosureGuard closure_guard(done);
         response->mutable_status()->set_status_code(0);
         if (request->all()) {
