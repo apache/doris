5f9d0df5864b27ef4f2f54980b645959ba3ac0ee
sohardforaname <organic_chemistry@foxmail.com>
2023-02-12 14:39:55 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/scalar_function/V.groovy
+++ regression-test/suites/nereids_function_p0/scalar_function/V.groovy
@@ -1,5 +1,5 @@
-suite('nereids_fn_test_new') {
-    sql 'use regression_test_nereids_function_p0'
-    sql 'set enable_nereids_planner=false'
-    sql 'set enable_fallback_to_original_planner=false'
+suite(nereids_scalar_fn_V) {
+	sql 'use regression_test_nereids_function_p0'
+	sql 'set enable_nereids_planner=false'
+	sql 'set enable_fallback_to_original_planner=false'
 }
