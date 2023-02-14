ce83f04dc0fd496b01685b594ff9330a325c0e5f
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2023-02-14 14:32:27 +0800

    [Fix](multi catalog)Support read hive1.x orc file. (#16677)
    Hive 1.x may write orc file with internal column name (_col0, _col1, _col2...).
    This will cause query result be NULL because column name in orc file doesn't match
    with column name in Doris table schema. This pr is to support query Hive orc files with internal column names. 
    
    For now, we haven't see any problem in Parquet file, will send new pr to fix parquet if any problem show up in the future.

--- fe/fe-core/src/main/java/org/apache/doris/planner/external/ExternalFileScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/external/ExternalFileScanNode.java
@@ -401,6 +401,12 @@ public class ExternalFileScanNode extends ExternalScanNode {
             createScanRangeLocations(context, scanProvider);
             this.inputSplitsNum += scanProvider.getInputSplitNum();
             this.totalFileSize += scanProvider.getInputFileSize();
+            TableIf table = desc.getTable();
+            if (table instanceof HMSExternalTable) {
+                if (((HMSExternalTable) table).getDlaType().equals(HMSExternalTable.DLAType.HIVE)) {
+                    genSlotToSchemaIdMap(context);
+                }
+            }
             if (scanProvider instanceof HiveScanProvider) {
                 this.totalPartitionNum = ((HiveScanProvider) scanProvider).getTotalPartitionNum();
                 this.readPartitionNum = ((HiveScanProvider) scanProvider).getReadPartitionNum();
@@ -420,6 +426,12 @@ public class ExternalFileScanNode extends ExternalScanNode {
             createScanRangeLocations(context, scanProvider);
             this.inputSplitsNum += scanProvider.getInputSplitNum();
             this.totalFileSize += scanProvider.getInputFileSize();
+            TableIf table = desc.getTable();
+            if (table instanceof HMSExternalTable) {
+                if (((HMSExternalTable) table).getDlaType().equals(HMSExternalTable.DLAType.HIVE)) {
+                    genSlotToSchemaIdMap(context);
+                }
+            }
             if (scanProvider instanceof HiveScanProvider) {
                 this.totalPartitionNum = ((HiveScanProvider) scanProvider).getTotalPartitionNum();
                 this.readPartitionNum = ((HiveScanProvider) scanProvider).getReadPartitionNum();
@@ -632,6 +644,22 @@ public class ExternalFileScanNode extends ExternalScanNode {
     private void createScanRangeLocations(ParamCreateContext context, FileScanProviderIf scanProvider)
             throws UserException {
         scanProvider.createScanRangeLocations(context, backendPolicy, scanRangeLocations);
+    }
+
+    private void genSlotToSchemaIdMap(ParamCreateContext context) {
+        List<Column> baseSchema = desc.getTable().getBaseSchema();
+        Map<String, Integer> columnNameToPosition = Maps.newHashMap();
+        for (SlotDescriptor slot : desc.getSlots()) {
+            int idx = 0;
+            for (Column col : baseSchema) {
+                if (col.getName().equals(slot.getColumn().getName())) {
+                    columnNameToPosition.put(col.getName(), idx);
+                    break;
+                }
+                idx += 1;
+            }
+        }
+        context.params.setSlotNameToSchemaPos(columnNameToPosition);
     }
 
     @Override
