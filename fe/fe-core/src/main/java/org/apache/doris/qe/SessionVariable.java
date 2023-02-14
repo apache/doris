56ccd5bc63c37d4e79627d2b3c63044ffeb208fa
lvliang <meredith620@gmail.com>
2023-02-14 13:40:20 +0800

    fix style check error

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -1457,7 +1457,6 @@ public class SessionVariable implements Serializable, Writable {
     public void setEnableUnicodeNameSupport(boolean enableUnicodeNameSupport) {
         this.enableUnicodeNameSupport = enableUnicodeNameSupport;
     }
-    
     public boolean isDropTableIfCtasFailed() {
         return dropTableIfCtasFailed;
     }
