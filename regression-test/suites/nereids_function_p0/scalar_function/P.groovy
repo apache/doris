be6b6882ba9f7a350f084a874f5a4435f9f04b4a
sohardforaname <organic_chemistry@foxmail.com>
2023-02-14 17:41:25 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/scalar_function/P.groovy
+++ regression-test/suites/nereids_function_p0/scalar_function/P.groovy
@@ -21,13 +21,13 @@ suite("nereids_scalar_fn_P") {
 	sql 'set enable_nereids_planner=false'
 	sql 'set enable_fallback_to_original_planner=false'
 	sql "select parse_url(kvchrs1, 'HOST') from fn_test order by kvchrs1, kvchrs1"
-	sql "select parse_url(kvchrs1, 'HOST') from fn_test order by kvchrs1, kvchrs1"
+	sql "select parse_url(kvchrs1, 'HOST') from fn_test_not_nullable order by kvchrs1, kvchrs1"
 	sql "select parse_url(kstr, 'HOST') from fn_test order by kstr, kstr"
-	sql "select parse_url(kstr, 'HOST') from fn_test order by kstr, kstr"
+	sql "select parse_url(kstr, 'HOST') from fn_test_not_nullable order by kstr, kstr"
 	sql "select parse_url(kvchrs1, 'HOST', 'PROTOCOL') from fn_test order by kvchrs1, kvchrs1, kvchrs1"
-	sql "select parse_url(kvchrs1, 'HOST', 'PROTOCOL') from fn_test order by kvchrs1, kvchrs1, kvchrs1"
+	sql "select parse_url(kvchrs1, 'HOST', 'PROTOCOL') from fn_test_not_nullable order by kvchrs1, kvchrs1, kvchrs1"
 	sql "select parse_url(kstr, 'HOST', 'PROTOCOL') from fn_test order by kstr, kstr, kstr"
-	sql "select parse_url(kstr, 'HOST', 'PROTOCOL') from fn_test order by kstr, kstr, kstr"
+	sql "select parse_url(kstr, 'HOST', 'PROTOCOL') from fn_test_not_nullable order by kstr, kstr, kstr"
 	qt_sql_pmod_BigInt_BigInt "select pmod(kbint, kbint) from fn_test order by kbint, kbint"
 	qt_sql_pmod_BigInt_BigInt "select pmod(kbint, kbint) from fn_test_not_nullable order by kbint, kbint"
 	qt_sql_pmod_Double_Double "select pmod(kdbl, kdbl) from fn_test order by kdbl, kdbl"
