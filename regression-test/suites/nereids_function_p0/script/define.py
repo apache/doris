b1b9e18aba059b968108996ab3cd305ffce6343d
sohardforaname <organic_chemistry@foxmail.com>
2023-02-15 22:02:52 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/script/define.py
+++ regression-test/suites/nereids_function_p0/script/define.py
@@ -85,6 +85,8 @@ const_sql = {
     'topn_String_Integer': 'select topn(kstr, 3) from ${t}',
     'topn_Varchar_Integer_Integer': 'select topn(kvchrs1, 3, 100) from ${t}',
     'topn_String_Integer_Integer': 'select topn(kstr, 3, 100) from ${t}',
+    'window_funnel_BigInt_String_DateTime_Boolean': '',
+    'window_funnel_BigInt_String_DateTimeV2_Boolean': '',
 }
 
 not_check_result = {
