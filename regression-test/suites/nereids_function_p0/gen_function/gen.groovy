436a7cb5bc5ee80373a7e90d3415ee4fe84d6e44
sohardforaname <organic_chemistry@foxmail.com>
2023-02-16 15:10:56 +0800

    'fix-bugs'

--- regression-test/suites/nereids_function_p0/gen_function/gen.groovy
+++ regression-test/suites/nereids_function_p0/gen_function/gen.groovy
@@ -19,22 +19,22 @@ suite("nereids_gen_fn") {
 	sql 'use regression_test_nereids_function_p0'
 	sql 'set enable_nereids_planner=true'
 	sql 'set enable_fallback_to_original_planner=false'
-	qt_sql_explode_bitmap_Bitmap "select kint, e from (select kint from fn_test) t lateral view explode_bitmap(to_bitmap(kbint)) lv as e"
-	qt_sql_explode_bitmap_Bitmap_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_bitmap(to_bitmap(kbint)) lv as e"
+	qt_sql_explode_bitmap_Bitmap "select kint, e from (select kint, kbint from fn_test) t lateral view explode_bitmap(to_bitmap(kbint)) lv as e"
+	qt_sql_explode_bitmap_Bitmap_notnull "select kint, e from (select kint, kbint from fn_test_not_nullable) t lateral view explode_bitmap(to_bitmap(kbint)) lv as e"
 
-	qt_sql_explode_bitmap_outer_Bitmap "select kint, e from (select kint from fn_test) t lateral view explode_bitmap_outer(to_bitmap(kbint)) lv as e"
-	qt_sql_explode_bitmap_outer_Bitmap_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_bitmap_outer(to_bitmap(kbint)) lv as e"
+	qt_sql_explode_bitmap_outer_Bitmap "select kint, e from (select kint, kbint from fn_test) t lateral view explode_bitmap_outer(to_bitmap(kbint)) lv as e"
+	qt_sql_explode_bitmap_outer_Bitmap_notnull "select kint, e from (select kint, kbint from fn_test_not_nullable) t lateral view explode_bitmap_outer(to_bitmap(kbint)) lv as e"
 
-	qt_sql_explode_numbers_Integer "select kint, e from (select kint from fn_test) t lateral view explode_numbers(kint) lv as e"
-	qt_sql_explode_numbers_Integer_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_numbers(kint) lv as e"
+	qt_sql_explode_numbers_Integer "select kint, e from (select kint, kint from fn_test) t lateral view explode_numbers(kint) lv as e"
+	qt_sql_explode_numbers_Integer_notnull "select kint, e from (select kint, kint from fn_test_not_nullable) t lateral view explode_numbers(kint) lv as e"
 
-	qt_sql_explode_numbers_outer_Integer "select kint, e from (select kint from fn_test) t lateral view explode_numbers_outer(kint) lv as e"
-	qt_sql_explode_numbers_outer_Integer_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_numbers_outer(kint) lv as e"
+	qt_sql_explode_numbers_outer_Integer "select kint, e from (select kint, kint from fn_test) t lateral view explode_numbers_outer(kint) lv as e"
+	qt_sql_explode_numbers_outer_Integer_notnull "select kint, e from (select kint, kint from fn_test_not_nullable) t lateral view explode_numbers_outer(kint) lv as e"
 
-	qt_sql_explode_split_Varchar_Varchar "select kint, e from (select kint from fn_test) t lateral view explode_split(kvchrs1, kvchrs1) lv as e"
-	qt_sql_explode_split_Varchar_Varchar_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_split(kvchrs1, kvchrs1) lv as e"
+	qt_sql_explode_split_Varchar_Varchar "select kint, e from (select kint, kvchrs1, kvchrs1 from fn_test) t lateral view explode_split(kvchrs1, kvchrs1) lv as e"
+	qt_sql_explode_split_Varchar_Varchar_notnull "select kint, e from (select kint, kvchrs1, kvchrs1 from fn_test_not_nullable) t lateral view explode_split(kvchrs1, kvchrs1) lv as e"
 
-	qt_sql_explode_split_outer_Varchar_Varchar "select kint, e from (select kint from fn_test) t lateral view explode_split_outer(kvchrs1, kvchrs1) lv as e"
-	qt_sql_explode_split_outer_Varchar_Varchar_notnull "select kint, e from (select kint from fn_test_not_nullable) t lateral view explode_split_outer(kvchrs1, kvchrs1) lv as e"
+	qt_sql_explode_split_outer_Varchar_Varchar "select kint, e from (select kint, kvchrs1, kvchrs1 from fn_test) t lateral view explode_split_outer(kvchrs1, kvchrs1) lv as e"
+	qt_sql_explode_split_outer_Varchar_Varchar_notnull "select kint, e from (select kint, kvchrs1, kvchrs1 from fn_test_not_nullable) t lateral view explode_split_outer(kvchrs1, kvchrs1) lv as e"
 
 }
