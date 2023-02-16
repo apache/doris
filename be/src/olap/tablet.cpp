ec0a4d6f5a0278fceec83bcea934f6e1cef29e72
Chen Zhang <chzhang1987@gmail.com>
2023-02-17 03:22:28 +0800

    [feature](merge-on-write) add DCHECK in compaction to detect inconsistency

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -2563,11 +2563,12 @@ Status Tablet::update_delete_bitmap(const RowsetSharedPtr& rowset, DeleteBitmapP
     return Status::OK();
 }
 
-void Tablet::calc_compaction_output_rowset_delete_bitmap(
+uint64_t Tablet::calc_compaction_output_rowset_delete_bitmap(
         const std::vector<RowsetSharedPtr>& input_rowsets, const RowIdConversion& rowid_conversion,
         uint64_t start_version, uint64_t end_version,
         std::map<RowsetSharedPtr, std::list<std::pair<RowLocation, RowLocation>>>* location_map,
         DeleteBitmap* output_rowset_delete_bitmap) {
+    uint64_t  missed_rows = 0;
     RowLocation src;
     RowLocation dst;
     for (auto& rowset : input_rowsets) {
@@ -2589,6 +2590,7 @@ void Tablet::calc_compaction_output_rowset_delete_bitmap(
                                       << " src loaction: |" << src.rowset_id << "|"
                                       << src.segment_id << "|" << src.row_id
                                       << " version: " << cur_version;
+                        missed_rows++;
                         continue;
                     }
                     VLOG_DEBUG << "calc_compaction_output_rowset_delete_bitmap dst location: |"
@@ -2603,6 +2605,7 @@ void Tablet::calc_compaction_output_rowset_delete_bitmap(
             }
         }
     }
+    return missed_rows;
 }
 
 void Tablet::merge_delete_bitmap(const DeleteBitmap& delete_bitmap) {
