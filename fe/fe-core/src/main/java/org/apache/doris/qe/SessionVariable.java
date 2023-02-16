b0918963500fb84c1d0775c326120e96f3d19e8e
Gabriel <gabrielleebuaa@gmail.com>
2023-02-16 15:02:27 +0800

    [regression](fuzzy) Make pipeline engine fuzzy test mode (#16807)

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -741,11 +741,11 @@ public class SessionVariable implements Serializable, Writable {
         }
         // pull_request_id default value is 0
         if (Config.pull_request_id % 2 == 1) {
-            // this.enablePipelineEngine = true;
+            this.enablePipelineEngine = true;
             // this.enableFoldConstantByBe = true;
             // this.enableTwoPhaseReadOpt = false;
         } else {
-            // this.enablePipelineEngine = false;
+            this.enablePipelineEngine = false;
             // this.enableFoldConstantByBe = false;
             // this.enableTwoPhaseReadOpt = true;
         }
