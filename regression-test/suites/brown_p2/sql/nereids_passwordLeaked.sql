7d8a9abc024dc3ead8d5eb980f7fb500c96b2ca3
morrySnow <101034200+morrySnow@users.noreply.github.com>
2023-02-17 18:48:27 +0800

    Revert "[test](pipeline) Run nereids cases in p1/p2 (#16130)" (#16792)
    This reverts commit b480db2e119ac0516e8621ea3d53c40f250c1d24.

--- regression-test/suites/brown_p2/sql/nereids_passwordLeaked.sql
+++
@@ -1,9 +0,0 @@
-SET enable_nereids_planner=TRUE;
-SET enable_fallback_to_original_planner=FALSE;
-SELECT *
-FROM logs2
-WHERE status_code >= 200
-  AND status_code < 300
-  AND request LIKE '%/etc/passwd%'
-  AND log_time >= TIMESTAMP '2012-05-06 00:00:00'
-  AND log_time < TIMESTAMP '2012-05-20 00:00:00';
--- regression-test/suites/yandex_metrica_p2/sql/nereids_where02.sql
+++
@@ -1,3 +0,0 @@
-SET enable_nereids_planner=TRUE;
-SET enable_fallback_to_original_planner=FALSE;
-SELECT CounterID, count(distinct UserID) FROM hits WHERE CAST(0 AS BOOLEAN) AND CounterID = 1704509 GROUP BY CounterID
