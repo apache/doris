c2d291f8ff7be2f5961f6478bb13f2608ab81b49
Kang <kxiao.tiger@gmail.com>
2023-02-18 11:15:57 +0800

    fix INTERNAL_ERROR

--- be/src/vec/olap/vcollect_iterator.cpp
+++ be/src/vec/olap/vcollect_iterator.cpp
@@ -246,7 +246,7 @@ Status VCollectIterator::_topn_next(Block* block) {
     MutableBlock mutable_block = vectorized::MutableBlock::build_mutable_block(&cloneBlock);
 
     if (!_reader->_reader_context.read_orderby_key_columns) {
-        return Status::Error<INTERNAL_ERROR>("read_orderby_key_columns should not be nullptr");
+        return Status::Error<ErrorCode::INTERNAL_ERROR>("read_orderby_key_columns should not be nullptr");
     }
 
     size_t first_sort_column_idx = (*_reader->_reader_context.read_orderby_key_columns)[0];
