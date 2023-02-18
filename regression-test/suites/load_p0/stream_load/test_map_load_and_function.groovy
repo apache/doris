e7a912bd1b13cfff8a8c8246833d0a04097f3de2
Kang <kxiao.tiger@gmail.com>
2023-02-18 10:05:35 +0800

    add testcase for basic map functions

--- regression-test/suites/load_p0/stream_load/test_map_load_and_function.groovy
+++ regression-test/suites/load_p0/stream_load/test_map_load_and_function.groovy
@@ -72,6 +72,76 @@ suite("test_map_load_and_function", "p0") {
     // map element_at
     qt_select_m "SELECT id, m['k2'] FROM ${testTable} ORDER BY id"
 
+    // check result
+    qt_select2 "SELECT * FROM ${testTable} ORDER BY id"
+
+    // map construct
+    qt_select_map1 "SELECT map('k11', 1000, 'k22', 2000)"
+
+    // map element_at
+    qt_select_element1 "SELECT map('k11', 1000, 'k22', 2000)['k11']"
+    qt_select_element2 "SELECT map('k11', 1000, 'k22', 2000)['k22']"
+    qt_select_element3 "SELECT map('k11', 1000, 'k22', 2000)['nokey']"
+    qt_select_element4 "SELECT map('k11', 1000, 'k22', 2000)['']"
+    // qt_select_element5 "SELECT map('k11', 1000, 'k22', 2000)[NULL]"
+    qt_select_element6 "SELECT id, m, m['k2'] FROM ${testTable} ORDER BY id"
+
+    // map size
+    qt_select_map_size1 "SELECT size(map('k11', 1000, 'k22', 2000))"
+    qt_select_map_size2 "SELECT id, m, size(m), map_size(m) FROM ${testTable} ORDER BY id"
+
+    // map_contains_key
+    qt_select_map_contains_key1 "SELECT map_contains_key(map('k11', 1000, 'k22', 2000), 'k11')"
+    qt_select_map_contains_key2 "SELECT map_contains_key(map('k11', 1000, 'k22', 2000), 'k22')"
+    qt_select_map_contains_key3 "SELECT map_contains_key(map('k11', 1000, 'k22', 2000), 'nokey')"
+    qt_select_map_contains_key4 "SELECT map_contains_key(map('k11', 1000, 'k22', 2000), '')"
+    qt_select_map_contains_key5 "SELECT map_contains_key(map('k11', 1000, 'k22', 2000), NULL)"
+    qt_select_map_contains_key101 "SELECT id, m, map_contains_key(m, 'k1') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key102 "SELECT id, m, map_contains_key(m, 'k2') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key103 "SELECT id, m, map_contains_key(m, '  11amory  ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key104 "SELECT id, m, map_contains_key(m, 'beat') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key105 "SELECT id, m, map_contains_key(m, ' clever ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key106 "SELECT id, m, map_contains_key(m, '  33,amory  ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key107 "SELECT id, m, map_contains_key(m, ' bet ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key108 "SELECT id, m, map_contains_key(m, ' cler ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key109 "SELECT id, m, map_contains_key(m, '  1,amy  ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key110 "SELECT id, m, map_contains_key(m, ' k2 ') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key111 "SELECT id, m, map_contains_key(m, 'null') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key112 "SELECT id, m, map_contains_key(m, '') FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key113 "SELECT id, m, map_contains_key(m, NULL) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_key114 "SELECT id, m, map_contains_key(m, 'nokey') FROM ${testTable} ORDER BY id"
+
+    // map_contains_value
+    qt_select_map_contains_value1 "SELECT map_contains_value(map('k11', 1000, 'k22', 2000), 1000)"
+    qt_select_map_contains_value2 "SELECT map_contains_value(map('k11', 1000, 'k22', 2000), 2000)"
+    qt_select_map_contains_value3 "SELECT map_contains_value(map('k11', 1000, 'k22', 2000), 100)"
+    qt_select_map_contains_value4 "SELECT map_contains_value(map('k11', 1000, 'k22', 2000), NULL)"
+    qt_select_map_contains_value101 "SELECT id, m, map_contains_value(m, 23) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value102 "SELECT id, m, map_contains_value(m, 20) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value103 "SELECT id, m, map_contains_value(m, 66) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value104 "SELECT id, m, map_contains_value(m, 31) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value105 "SELECT id, m, map_contains_value(m, 300) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value106 "SELECT id, m, map_contains_value(m, 41) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value107 "SELECT id, m, map_contains_value(m, 400) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value108 "SELECT id, m, map_contains_value(m, 33) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value109 "SELECT id, m, map_contains_value(m, 2) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value110 "SELECT id, m, map_contains_value(m, 26) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value111 "SELECT id, m, map_contains_value(m, 90) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value112 "SELECT id, m, map_contains_value(m, 33) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value113 "SELECT id, m, map_contains_value(m, 1) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value114 "SELECT id, m, map_contains_value(m, 2) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value115 "SELECT id, m, map_contains_value(m, 0) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value116 "SELECT id, m, map_contains_value(m, -1) FROM ${testTable} ORDER BY id"
+    qt_select_map_contains_value117 "SELECT id, m, map_contains_value(m, NULL) FROM ${testTable} ORDER BY id"
+
+    // map_keys
+    qt_select_map_keys1 "SELECT map_keys(map('k11', 1000, 'k22', 2000))"
+    qt_select_map_keys2 "SELECT id, m, map_keys(m) FROM ${testTable} ORDER BY id"
+
+    // map_values
+    qt_select_map_values1 "SELECT map_values(map('k11', 1000, 'k22', 2000))"
+    qt_select_map_values2 "SELECT id, m, map_values(m) FROM ${testTable} ORDER BY id"
+
 
     testTable = "tbl_test_map2"
     sql "DROP TABLE IF EXISTS ${testTable}"
