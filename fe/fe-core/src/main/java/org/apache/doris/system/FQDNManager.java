f7a22114a66220fc616ca6f98a00c8e711b93085
zhangdong <493738387@qq.com>
2023-02-18 13:07:48 +0800

    3

--- fe/fe-core/src/main/java/org/apache/doris/system/FQDNManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/system/FQDNManager.java
@@ -32,8 +32,6 @@ import java.net.UnknownHostException;
 public class FQDNManager extends MasterDaemon {
     private static final Logger LOG = LogManager.getLogger(FQDNManager.class);
 
-    public static final String  UNKNOWN_HOST_IP = "unknown";
-
     private SystemInfoService nodeMgr;
 
     public FQDNManager(SystemInfoService nodeMgr) {
@@ -52,23 +50,13 @@ public class FQDNManager extends MasterDaemon {
                     InetAddress inetAddress = InetAddress.getByName(be.getHostName());
                     if (!be.getIp().equalsIgnoreCase(inetAddress.getHostAddress())) {
                         String ip = be.getIp();
-                        if (!ip.equalsIgnoreCase(UNKNOWN_HOST_IP)) {
-                            ClientPool.backendPool.clearPool(new TNetworkAddress(ip, be.getBePort()));
-                        }
+                        ClientPool.backendPool.clearPool(new TNetworkAddress(ip, be.getBePort()));
                         be.setIp(inetAddress.getHostAddress());
                         Env.getCurrentEnv().getEditLog().logBackendStateChange(be);
                         LOG.warn("ip for {} of be has been changed from {} to {}", be.getHostName(), ip, be.getIp());
                     }
                 } catch (UnknownHostException e) {
                     LOG.warn("unknown host name for be, {}", be.getHostName(), e);
-                    // add be alive check to make ip work when be is still alive and dns has some problem.
-                    if (!be.isAlive() && !be.getIp().equalsIgnoreCase(UNKNOWN_HOST_IP)) {
-                        String ip = be.getIp();
-                        ClientPool.backendPool.clearPool(new TNetworkAddress(ip, be.getBePort()));
-                        be.setIp(UNKNOWN_HOST_IP);
-                        Env.getCurrentEnv().getEditLog().logBackendStateChange(be);
-                        LOG.warn("ip for {} of be has been changed from {} to {}", be.getHostName(), ip, "unknown");
-                    }
                 }
             }
         }
