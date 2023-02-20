8a9dc53421d87cad4f62f1195e7703d63c16e4b3
huangzhaowei <carlmartinmax@gmail.com>
2023-02-20 16:55:36 +0800

    code style

--- fe/fe-core/src/main/java/org/apache/doris/metric/DorisMetricRegistry.java
+++ fe/fe-core/src/main/java/org/apache/doris/metric/DorisMetricRegistry.java
@@ -19,8 +19,6 @@ package org.apache.doris.metric;
 
 import org.apache.doris.catalog.Env;
 
-import com.google.common.collect.Lists;
-
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
