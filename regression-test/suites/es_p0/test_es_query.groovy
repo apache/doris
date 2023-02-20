f7d907f06d74122ed29b2ee371a1b33044cc16c6
zhangguoqiang <18372634969@163.com>
2023-02-20 20:24:00 +0800

    set compile required; add clickbench and arm pipeline trigger to buildall;

--- regression-test/suites/es_p0/test_es_query.groovy
+++ regression-test/suites/es_p0/test_es_query.groovy
@@ -46,6 +46,7 @@ suite("test_es_query", "p0") {
             "enable_keyword_sniff"="true"
         );
         """
+
         sql """create resource if not exists es8_resource properties(
             "type"="es",
             "hosts"="http://127.0.0.1:$es_8_port",
