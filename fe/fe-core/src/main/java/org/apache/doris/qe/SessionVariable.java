b34b0d5c6892068400ce8657877d0c78c057692b
zhangstar333 <2561612514@qq.com>
2023-02-21 12:08:49 +0800

    open enableFoldConstantByBe

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -960,13 +960,13 @@ public class SessionVariable implements Serializable, Writable {
         }
         // pull_request_id default value is 0
         if (Config.pull_request_id % 2 == 1) {
-            // this.enablePipelineEngine = true;
-            // this.enableFoldConstantByBe = true;
+            this.enablePipelineEngine = true;
+            this.enableFoldConstantByBe = true;
             // this.enableTwoPhaseReadOpt = false;
             this.runtimeFilterType |= TRuntimeFilterType.BITMAP.getValue();
         } else {
-            // this.enablePipelineEngine = false;
-            // this.enableFoldConstantByBe = false;
+            this.enablePipelineEngine = false;
+            this.enableFoldConstantByBe = false;
             // this.enableTwoPhaseReadOpt = true;
             this.runtimeFilterType &= ~TRuntimeFilterType.BITMAP.getValue();
         }
