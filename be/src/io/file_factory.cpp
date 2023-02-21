cf7ec58e92d700b3bf5cd28b5677f989f8070d50
yiguolei <yiguolei@gmail.com>
2023-02-21 20:00:48 +0800

    f

--- be/src/io/file_factory.cpp
+++ be/src/io/file_factory.cpp
@@ -195,8 +195,7 @@ Status FileFactory::create_pipe_reader(const TUniqueId& load_id, io::FileReaderS
     if (!stream_load_ctx) {
         return Status::InternalError("unknown stream load id: {}", UniqueId(load_id).to_string());
     }
-    *file_reader = std::reinterpret_pointer_cast<std::shared_ptr<io::FileReader>>(
-            stream_load_ctx->body_sink);
+    *file_reader = std::reinterpret_pointer_cast<io::FileReader>(stream_load_ctx->body_sink);
     return Status::OK();
 }
 
