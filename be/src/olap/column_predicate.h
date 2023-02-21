23d414c27657b1628dbb60adf3a01008fb4398ea
Tanya-W <tanya1218w@163.com>
2023-02-21 19:51:02 +0800

    [enhancement](inverted index) Support fulltext index evaluate equal query and list query

--- be/src/olap/column_predicate.h
+++ be/src/olap/column_predicate.h
@@ -110,6 +110,10 @@ struct PredicateTypeTraits {
         return (type == PredicateType::IN_LIST || type == PredicateType::NOT_IN_LIST);
     }
 
+    static constexpr bool is_eqaul_and_list(PredicateType type) {
+        return (type == PredicateType::EQ || type == PredicateType::IN_LIST);
+    }
+
     static constexpr bool is_comparison(PredicateType type) {
         return (type == PredicateType::EQ || type == PredicateType::NE ||
                 type == PredicateType::LT || type == PredicateType::LE ||
