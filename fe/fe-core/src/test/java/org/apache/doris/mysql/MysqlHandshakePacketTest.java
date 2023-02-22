fa8ad2efae292ac94d799fc0500ade82d573a205
Yukang Lian <Yukang.Lian2022@gmail.com>
2023-02-22 23:06:15 +0800

    fix

--- fe/fe-core/src/test/java/org/apache/doris/mysql/MysqlHandshakePacketTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/mysql/MysqlHandshakePacketTest.java
@@ -77,7 +77,7 @@ public class MysqlHandshakePacketTest {
         // capability flags
         flags |= MysqlProto.readInt2(buffer) << 16;
         Assert.assertEquals(MysqlProto.SERVER_USE_SSL
-            ? MysqlCapability.SSL_CAPABILITY.getFlags() : MysqlCapability.DEFAULT_CAPABILITY.getFlags(), flags);
+                ? MysqlCapability.SSL_CAPABILITY.getFlags() : MysqlCapability.DEFAULT_CAPABILITY.getFlags(), flags);
         // length of plugin data
         Assert.assertEquals(21, MysqlProto.readInt1(buffer));
         // length of plugin data
