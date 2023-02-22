505bee7205decfbbc4dab98fa6884bdf270fdc19
zxealous <xealous0729@gmail.com>
2023-02-22 10:15:39 +0800

    [fix](struct-type) fix be core when load array orc file (#16978)
    * fix be core when load array orc file

--- be/src/runtime/types.cpp
+++ be/src/runtime/types.cpp
@@ -270,6 +270,18 @@ TypeDescriptor::TypeDescriptor(const google::protobuf::RepeatedPtrField<PTypeNod
     }
 }
 
+void TypeDescriptor::add_sub_type(TypeDescriptor&& sub_type, bool&& is_nullable) {
+    children.emplace_back(sub_type);
+    contains_nulls.emplace_back(is_nullable);
+}
+
+void TypeDescriptor::add_sub_type(TypeDescriptor&& sub_type, std::string&& field_name,
+                                  bool&& is_nullable) {
+    children.emplace_back(sub_type);
+    field_names.emplace_back(field_name);
+    contains_nulls.emplace_back(is_nullable);
+}
+
 std::string TypeDescriptor::debug_string() const {
     std::stringstream ss;
     switch (type) {
