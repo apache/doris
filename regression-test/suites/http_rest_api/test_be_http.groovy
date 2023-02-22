08ddf65c46a3c63d9f5032a61cccb6aa9b260a4c
Tang Siyang <tangsiyang2001@foxmail.com>
2023-02-22 23:32:21 +0800

    fix conn reset

--- regression-test/suites/http_rest_api/test_be_http.groovy
+++ regression-test/suites/http_rest_api/test_be_http.groovy
@@ -76,114 +76,114 @@ suite("test_be_http") {
 
     //test_check_rpc_channel
     def uri = "/api/check_rpc_channel/${beIp}/${bePort}/1024000"
-    def conn = doGet(dst, uri)
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn1 = doGet(dst, uri)
+    assertTrue(conn1.responseCode == 200 || conn1.responseCode == 201)
 
     //test_reset_rpc_channel
-    conn = doGet(dst,"/api/reset_rpc_channel/all")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn2 = doGet(dst,"/api/reset_rpc_channel/all")
+    assertTrue(conn2.responseCode == 200 || conn2.responseCode == 201)
 
     //test_check_tablet_segment
-    conn = doPost(dst,"/api/check_tablet_segment_lost?repair=true", null)
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn3 = doPost(dst,"/api/check_tablet_segment_lost?repair=false", null)
+    assertTrue(conn3.responseCode == 200 || conn3.responseCode == 201)
 
     //test_checksum
-    conn = doGet(dst,"/api/checksum?tablet_id=${tabletId}&schema_hash=${schemaHash}&version=${version}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn4 = doGet(dst,"/api/checksum?tablet_id=${tabletId}&schema_hash=${schemaHash}&version=${version}")
+    assertTrue(conn4.responseCode == 200 || conn4.responseCode == 201)
 
 
     //test_compaction
-    conn = doGet(dst,"/api/compaction/run_status")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn5 = doGet(dst,"/api/compaction/run_status")
+    assertTrue(conn5.responseCode == 200 || conn5.responseCode == 201)
 
-    conn = doGet(dst,"/api/compaction/show?tablet_id=${tabletId}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn6 = doGet(dst,"/api/compaction/show?tablet_id=${tabletId}")
+    assertTrue(conn6.responseCode == 200 || conn6.responseCode == 201)
 
-    conn = doPost(dst,"/api/compaction/run?tablet_id=${tabletId}&compact_type=cumulative")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn7 = doPost(dst,"/api/compaction/run?tablet_id=${tabletId}&compact_type=cumulative")
+    assertTrue(conn7.responseCode == 200 || conn7.responseCode == 201)
 
-    conn = doGet(dst,"/api/compaction/run_status?tablet_id=${tabletId}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn8 = doGet(dst,"/api/compaction/run_status?tablet_id=${tabletId}")
+    assertTrue(conn8.responseCode == 200 || conn8.responseCode == 201)
 
 
     //test_config
-    conn = doGet(dst,"/api/show_config")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn9 = doGet(dst,"/api/show_config")
+    assertTrue(conn9.responseCode == 200 || conn9.responseCode == 201)
 
-    conn = doPost(dst,"/api/update_config?&persist=false&slave_replica_writer_rpc_timeout_sec=90")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn10 = doPost(dst,"/api/update_config?&persist=false&slave_replica_writer_rpc_timeout_sec=90")
+    assertTrue(conn10.responseCode == 200 || conn10.responseCode == 201)
 
     //test_health
-    conn = doGet(dst,"/api/health")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn11 = doGet(dst,"/api/health")
+    assertTrue(conn11.responseCode == 200 || conn11.responseCode == 201)
 
     //test_jeprofile
-    conn = doGet(dst,"/jeheap/dump")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn12 = doGet(dst,"/jeheap/dump")
+    assertTrue(conn12.responseCode == 200 || conn12.responseCode == 201)
 
     //test_meta
-    conn = doGet(dst,"/api/meta/header/${tabletId}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn13 = doGet(dst,"/api/meta/header/${tabletId}")
+    assertTrue(conn13.responseCode == 200 || conn13.responseCode == 201)
 
     //test_metrics
-    conn = doGet(dst,"/metrics?type=core")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn14 = doGet(dst,"/metrics?type=core")
+    assertTrue(conn14.responseCode == 200 || conn14.responseCode == 201)
 
-    conn = doGet(dst,"/metrics?type=json?with_tablet=true")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn15 = doGet(dst,"/metrics?type=json?with_tablet=true")
+    assertTrue(conn15.responseCode == 200 || conn15.responseCode == 201)
 
-    conn = doGet(dst,"/metrics?with_tablet=false")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn16 = doGet(dst,"/metrics?with_tablet=false")
+    assertTrue(conn16.responseCode == 200 || conn16.responseCode == 201)
 
 
     //test_pprof
-    conn = doGet(dst,"/api/pprof/heap")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn17 = doGet(dst,"/api/pprof/heap")
+    assertTrue(conn17.responseCode == 200 || conn17.responseCode == 201)
 
-    conn = doGet(dst,"/api/pprof/growth")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn18 = doGet(dst,"/api/pprof/growth")
+    assertTrue(conn18.responseCode == 200 || conn18.responseCode == 201)
 
-    conn = doGet(dst,"/api/pprof/profile")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn19 = doGet(dst,"/api/pprof/profile")
+    assertTrue(conn19.responseCode == 200 || conn19.responseCode == 201)
 
-    conn = doGet(dst,"/api/pprof/cmdline")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn20 = doGet(dst,"/api/pprof/cmdline")
+    assertTrue(conn20.responseCode == 200 || conn20.responseCode == 201)
 
-    conn = doGet(dst,"/api/pprof/symbol")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn21 = doGet(dst,"/api/pprof/symbol")
+    assertTrue(conn21.responseCode == 200 || conn21.responseCode == 201)
 
-    conn = doPost(dst,"/api/pprof/symbol")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn22 = doPost(dst,"/api/pprof/symbol")
+    assertTrue(conn22.responseCode == 200 || conn22.responseCode == 201)
 
 
     //test_snapshot
-    conn = doGet(dst,"/api/snapshot?tablet_id=${tabletId}&schema_hash=${schemaHash}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn23 = doGet(dst,"/api/snapshot?tablet_id=${tabletId}&schema_hash=${schemaHash}")
+    assertTrue(conn23.responseCode == 200 || conn23.responseCode == 201)
 
     //test_reset_rpc_channel
-    conn = doGet(dst,"/api/reset_rpc_channel/all")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn24 = doGet(dst,"/api/reset_rpc_channel/all")
+    assertTrue(conn24.responseCode == 200 || conn24.responseCode == 201)
 
     //test_distribution
-    conn = doGet(dst,"/api/tablets_distribution?group_by=partition&partition_id=1")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn25 = doGet(dst,"/api/tablets_distribution?group_by=partition&partition_id=1")
+    assertTrue(conn25.responseCode == 200 || conn25.responseCode == 201)
 
     //test_tablets_info
-    conn = doGet(dst,"/tablets_json")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn26 = doGet(dst,"/tablets_json")
+    assertTrue(conn26.responseCode == 200 || conn26.responseCode == 201)
 
     //test_be_info
-    conn = doGet(dst,"/api/be_version_info")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn27 = doGet(dst,"/api/be_version_info")
+    assertTrue(conn27.responseCode == 200 || conn27.responseCode == 201)
 
     def partitionInfo = sql """ SHOW PARTITIONS FROM ${tableName} """
     def partitionId = partitionInfo[0][0]
-    conn = doGet(dst,"/api/tablets_distribution?group_by=partition&partition_id=${partitionId}")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn28 = doGet(dst,"/api/tablets_distribution?group_by=partition&partition_id=${partitionId}")
+    assertTrue(conn28.responseCode == 200 || conn28.responseCode == 201)
 
     //test_pad_rowset
-    conn = doPost(dst,"/api/pad_rowset?tablet_id=${tabletId}&start_version=1&end_version=2")
-    assertTrue(conn.responseCode == 200 || conn.responseCode == 201)
+    def conn29 = doPost(dst,"/api/pad_rowset?tablet_id=${tabletId}&start_version=1&end_version=2")
+    assertTrue(conn29.responseCode == 200 || conn29.responseCode == 201)
 
 
     sql """ DROP TABLE IF EXISTS ${tableName} """
