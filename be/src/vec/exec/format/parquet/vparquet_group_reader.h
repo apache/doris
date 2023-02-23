61826e3a77573d82cdec11c5e9c5111b50ab136f
Qi Chen <kaka11.chen@gmail.com>
2023-02-23 14:41:30 +0800

    [Improvement](parquet-reader) Improve performance of parquet reader filter calculation. (#16934)
    Improve performance of parquet reader filter calculation.
    
    - Use `filter_data` instead of `(*filter_ptr)` to merge filter to improve performance. 
    - Use mutable column filter func instead of original new column filter func which introduced by #16850.
    - Avoid column ref-count increasing which caused unnecessary copying by passing column pointer ref.

--- be/src/vec/exec/format/parquet/vparquet_group_reader.h
+++ be/src/vec/exec/format/parquet/vparquet_group_reader.h
@@ -132,11 +132,12 @@ private:
             Block* block, size_t rows,
             const std::unordered_map<std::string, VExprContext*>& missing_columns);
     Status _build_pos_delete_filter(size_t read_rows);
-    Status _filter_block(Block* block, const ColumnPtr filter_column, int column_to_keep,
+    Status _filter_block(Block* block, const ColumnPtr& filter_column, int column_to_keep,
                          std::vector<uint32_t> columns_to_filter);
     Status _filter_block(Block* block, int column_to_keep,
                          const vector<uint32_t>& columns_to_filter);
-    Status _filter_block_internal(Block* block, const vector<uint32_t>& columns_to_filter);
+    Status _filter_block_internal(Block* block, const vector<uint32_t>& columns_to_filter,
+                                  const IColumn::Filter& filter);
 
     io::FileReaderSPtr _file_reader;
     std::unordered_map<std::string, std::unique_ptr<ParquetColumnReader>> _column_readers;
@@ -154,7 +155,7 @@ private:
     // If continuous batches are skipped, we can cache them to skip a whole page
     size_t _cached_filtered_rows = 0;
     std::unique_ptr<TextConverter> _text_converter = nullptr;
-    std::unique_ptr<IColumn::Filter> _filter_ptr = nullptr;
+    std::unique_ptr<IColumn::Filter> _pos_delete_filter_ptr = nullptr;
     int64_t _total_read_rows = 0;
 };
 } // namespace doris::vectorized
