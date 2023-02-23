7a47c50cab5c73fb99dcba65f538d2bc0d99d253
pengxianyu <diablowcg@163.com>
2023-02-23 15:08:25 +0800

    add ut for CooldownConfHandler

--- fe/fe-core/src/test/java/org/apache/doris/cooldown/CooldownConfHandlerTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/cooldown/CooldownConfHandlerTest.java
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.apache.doris.cooldown;
 
 import org.apache.doris.analysis.CreateUserStmt;
@@ -20,14 +37,16 @@ import org.apache.doris.system.SystemInfoService;
 import org.apache.doris.utframe.TestWithFeService;
 
 import com.google.common.collect.Lists;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class CooldownConfHandlerTest extends TestWithFeService {
-
+    private static final Logger LOG = LogManager.getLogger(CooldownConfHandlerTest.class);
     private long dbId = 100L;
     private long tableId = 101L;
     private long partitionId = 102L;
@@ -44,7 +63,7 @@ public class CooldownConfHandlerTest extends TestWithFeService {
         useDatabase("test");
         createTable("create table table1\n"
                 + "(k1 int, k2 int) distributed by hash(k1) buckets 1\n"
-                + "properties(\"replication_num\" = \"3\");");
+                + "properties(\"replication_num\" = \"1\");");
         // create user
         UserIdentity user = new UserIdentity("test_cooldown", "%");
         user.analyze(SystemInfoService.DEFAULT_CLUSTER);
@@ -71,6 +90,8 @@ public class CooldownConfHandlerTest extends TestWithFeService {
         assert tablets.size() > 0;
         tablet = tablets.get(0);
         tabletId = tablet.getId();
+        LOG.info("create table: db: {}, tbl: {}, partition: {}, index: {}, tablet: {}", dbId, tableId, partitionId,
+                indexId, tabletId);
     }
 
     @Test
