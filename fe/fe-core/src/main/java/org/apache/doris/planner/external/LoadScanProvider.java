046cc5c0518a067efa043595e675a2e3088e86b6
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2023-02-23 13:54:36 +0800

    [Fix](load)Pass hidden column to load columns (#17004)
    The LoadScanProvider doesn't get Hidden Columns from stream load parameter.
    This may cause stream load delete operation fail. This pr is to pass the hidden columns to LoadScanProvider.

--- fe/fe-core/src/main/java/org/apache/doris/planner/external/LoadScanProvider.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/external/LoadScanProvider.java
@@ -206,7 +206,8 @@ public class LoadScanProvider implements FileScanProviderIf {
         List<Integer> srcSlotIds = Lists.newArrayList();
         Load.initColumns(fileGroupInfo.getTargetTable(), columnDescs, context.fileGroup.getColumnToHadoopFunction(),
                 context.exprMap, analyzer, context.srcTupleDescriptor, context.srcSlotDescByName, srcSlotIds,
-                formatType(context.fileGroup.getFileFormat(), ""), null, VectorizedUtil.isVectorized());
+                formatType(context.fileGroup.getFileFormat(), ""), fileGroupInfo.getHiddenColumns(),
+                VectorizedUtil.isVectorized());
 
         int columnCountFromPath = 0;
         if (context.fileGroup.getColumnNamesFromPath() != null) {
