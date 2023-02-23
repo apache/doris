b2655ab098b86faf97779633ac694374b46c39a4
amory <wangqiannan@selectdb.com>
2023-02-23 20:47:26 +0800

    [Improve](map-type) Add contains_null for map  (#16948)
    Add contains_null for map type.

--- gensrc/thrift/Types.thrift
+++ gensrc/thrift/Types.thrift
@@ -141,7 +141,7 @@ struct TTypeNode {
     3: optional list<TStructField> struct_fields
 
     // only used for complex types, such as array, map and etc.
-    4: optional bool contains_null
+    4: optional list<bool> contains_nulls
 }
 
 // A flattened representation of a tree of column types obtained by depth-first
