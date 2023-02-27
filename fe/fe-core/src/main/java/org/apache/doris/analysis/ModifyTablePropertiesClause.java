650b82af1dc5650333d5ced376a13fdf8ce3c97f
Yusheng Xu <30896780+1330571@users.noreply.github.com>
2023-02-27 18:42:31 +0800

    [feature](cooldown) Forbid storage policy for MoW tables (#17148)
    * disable setting storage policy on MoW table
    
    * fix error in regression test
    
    * make the name of test table unique
    
    * use Strings.isNullOrEmpty to replace equals
    
    * fix error in if statement

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyTablePropertiesClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyTablePropertiesClause.java
@@ -25,6 +25,8 @@ import org.apache.doris.common.util.DynamicPartitionUtil;
 import org.apache.doris.common.util.PrintableMap;
 import org.apache.doris.common.util.PropertyAnalyzer;
 
+import com.google.common.base.Strings;
+
 import java.util.Map;
 
 // clause which is used to modify table properties
@@ -101,7 +103,14 @@ public class ModifyTablePropertiesClause extends AlterTableClause {
             throw new AnalysisException("Alter tablet type not supported");
         } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_STORAGE_POLICY)) {
             this.needTableStable = false;
-            setStoragePolicy(properties.getOrDefault(PropertyAnalyzer.PROPERTIES_STORAGE_POLICY, ""));
+            String storagePolicy = properties.getOrDefault(PropertyAnalyzer.PROPERTIES_STORAGE_POLICY, "");
+            if (!Strings.isNullOrEmpty(storagePolicy)
+                    && properties.containsKey(PropertyAnalyzer.ENABLE_UNIQUE_KEY_MERGE_ON_WRITE)) {
+                throw new AnalysisException(
+                        "Can not set UNIQUE KEY table that enables Merge-On-write"
+                                + " with storage policy(" + storagePolicy + ")");
+            }
+            setStoragePolicy(storagePolicy);
         } else if (properties.containsKey(PropertyAnalyzer.ENABLE_UNIQUE_KEY_MERGE_ON_WRITE)) {
             throw new AnalysisException("Can not change UNIQUE KEY to Merge-On-Write mode");
         } else {
