68cc8d256e16969d530726b1486df29267443b7c
lihangyu <15605149486@163.com>
2023-02-27 10:39:22 +0800

    [Optimize](simd json reader) Cached search results for previous row (keyed as index in JSON object) - used as a hint. (#17124)
    * [Optimize](simd json reader) Cached search results for previous row (keyed as index in JSON object) - used as a hint.
    
    `_simdjson_set_column_value` could become a hot spot while parsing json in simdjson mode,
    introduce `_prev_positions` to cache results for previous row (keyed as index in JSON object) due to the json name field order,
    should be quite the same between each lines
    
    * fix case

--- be/src/vec/exec/format/json/new_json_reader.h
+++ be/src/vec/exec/format/json/new_json_reader.h
@@ -25,6 +25,7 @@
 #include "io/file_factory.h"
 #include "io/fs/file_reader.h"
 #include "olap/iterators.h"
+#include "vec/common/hash_table/hash_map.h"
 #include "vec/exec/format/generic_reader.h"
 
 namespace doris {
@@ -141,6 +142,8 @@ private:
     Status _append_error_msg(simdjson::ondemand::object* obj, std::string error_msg,
                              std::string col_name, bool* valid);
 
+    size_t _column_index(const StringRef& name, size_t key_index);
+
     Status (NewJsonReader::*_vhandle_json_callback)(
             std::vector<vectorized::MutableColumnPtr>& columns,
             const std::vector<SlotDescriptor*>& slot_descs, bool* is_empty_row, bool* eof);
@@ -199,8 +202,16 @@ private:
     RuntimeProfile::Counter* _file_read_timer;
 
     bool _is_dynamic_schema = false;
+
+    // ======SIMD JSON======
     // name mapping
-    phmap::flat_hash_map<String, size_t> _slot_desc_index;
+    /// Hash table match `field name -> position in the block`. NOTE You can use perfect hash map.
+    using NameMap = HashMap<StringRef, size_t, StringRefHash>;
+    NameMap _slot_desc_index;
+    /// Cached search results for previous row (keyed as index in JSON object) - used as a hint.
+    std::vector<NameMap::LookupResult> _prev_positions;
+    /// Set of columns which already met in row. Exception is thrown if there are more than one column with the same name.
+    std::vector<UInt8> _seen_columns;
     // simdjson
     static constexpr size_t _init_buffer_size = 1024 * 1024 * 8;
     size_t _padded_size = _init_buffer_size + simdjson::SIMDJSON_PADDING;
