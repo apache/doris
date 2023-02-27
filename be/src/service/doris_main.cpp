1911d40dc769703e9b8965ce67598cfc6f875b13
gaoxin <AshinGau@outlook.com>
2023-02-27 19:24:47 +0800

    [fix](FileCache) load file cache before start up daemon threads

--- be/src/service/doris_main.cpp
+++ be/src/service/doris_main.cpp
@@ -385,10 +385,6 @@ int main(int argc, char** argv) {
         }
     }
 
-    doris::Daemon daemon;
-    daemon.init(argc, argv, paths);
-    daemon.start();
-
     if (doris::config::enable_file_cache) {
         std::vector<doris::CachePath> cache_paths;
         olap_res = doris::parse_conf_cache_paths(doris::config::file_cache_path, cache_paths);
@@ -426,6 +422,10 @@ int main(int argc, char** argv) {
             }
         }
     }
+
+    doris::Daemon daemon;
+    daemon.init(argc, argv, paths);
+    daemon.start();
 
     doris::ResourceTls::init();
     if (!doris::BackendOptions::init()) {
