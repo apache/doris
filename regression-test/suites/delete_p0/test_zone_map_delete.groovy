af212ded29c538ab3d4ac944db918583ed926b9e
nextdreamblue <zxw520blue1@163.com>
2023-02-28 10:46:49 +0800

    add regression-test
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- regression-test/suites/delete_p0/test_zone_map_delete.groovy
+++ regression-test/suites/delete_p0/test_zone_map_delete.groovy
@@ -149,6 +149,29 @@ suite("test_zone_map_delete") {
 
     qt_sql """select k2,k3 from ${tableName} where k3 = 11 ORDER BY k3;"""
 
+
+    sql """truncate table ${tableName};"""
+
+    sql """insert into ${tableName} values(0,1,11),(0,1,22),(0,1,33),(0,1,44),(0,1,55),(0,1,66),(0,1,77),(0,1,88),(0,1,99),(0,1,100),(0,1,101),(0,1,102),(0,1,111),(0,1,122),(0,1,133),(0,1,144),(0,1,155),(0,1,166),(0,1,177),(0,1,188),(0,1,199),(0,1,200),(0,1,201),(0,1,202);"""
+
+    sql """delete from ${tableName} where k2 is null;"""
+
+    qt_sql """select k2,k3 from ${tableName} ORDER BY k3;"""
+
+    qt_sql """select k2,k3 from ${tableName} where k2 is not null ORDER BY k3;"""
+
+
+    sql """truncate table ${tableName};"""
+
+    sql """insert into ${tableName} values(0,null,11),(0,null,22),(0,null,33),(0,null,44),(0,null,55),(0,null,66),(0,null,77),(0,null,88),(0,null,99),(0,null,100),(0,null,101),(0,null,102),(0,null,111),(0,null,122),(0,null,133),(0,null,144),(0,null,155),(0,null,166),(0,null,177),(0,null,188),(0,null,199),(0,null,200),(0,null,201),(0,null,202);"""
+
+    sql """delete from ${tableName} where k2 is not null;"""
+
+    qt_sql """select k2,k3 from ${tableName} ORDER BY k3;"""
+
+    qt_sql """select k2,k3 from ${tableName} where k2 is null ORDER BY k3;"""
+
+
     sql """ DROP TABLE IF EXISTS ${tableName} """
 
 }
