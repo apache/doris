e415ad9c82e78081c423e1e6fb6c43567eec88a6
nextdreamblue <zxw520blue1@163.com>
2023-02-28 21:54:36 +0800

    add regression-test
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- regression-test/suites/jsonb_p0/test_jsonb_load_and_function.groovy
+++ regression-test/suites/jsonb_p0/test_jsonb_load_and_function.groovy
@@ -416,4 +416,8 @@ suite("test_jsonb_load_and_function", "p0") {
     qt_select """SELECT CAST('{x' AS JSONB)"""
     qt_select """SELECT CAST('[123, abc]' AS JSONB)"""
 
+    qt_select """SELECT id, JSON_VALID(j) FROM ${testTable} ORDER BY id"""
+    qt_select """SELECT JSON_VALID('{"k1":"v31","k2":300}')"""
+    qt_select """SELECT JSON_VALID('invalid json')"""
+    qt_select """SELECT JSON_VALID(NULL)"""
 }
