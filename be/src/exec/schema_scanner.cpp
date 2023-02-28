72a46a2ed61956beb25207709f9fbadc7693287e
Cai-Yao <729673078@qq.com>
2023-02-28 15:42:50 +0800

    format

--- be/src/exec/schema_scanner.cpp
+++ be/src/exec/schema_scanner.cpp
@@ -94,7 +94,7 @@ Status SchemaScanner::init(SchemaScannerParam* param, ObjectPool* pool) {
     _get_table_timer = ADD_TIMER(_param->profile, "GetTableTime");
     _get_describe_timer = ADD_TIMER(_param->profile, "GetDescribeTime");
     _fill_block_timer = ADD_TIMER(_param->profile, "FillBlockTime");
- 
+
     return Status::OK();
 }
 
