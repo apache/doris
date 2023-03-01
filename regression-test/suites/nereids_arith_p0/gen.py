9f71b3bcde1ce70d4a9ffbcad3ed03cc289f2df9
sohardforaname <organic_chemistry@foxmail.com>
2023-03-01 23:29:19 +0800

    'fix-bugs'

--- regression-test/suites/nereids_arith_p0/gen.py
+++ regression-test/suites/nereids_arith_p0/gen.py
@@ -1,6 +1,31 @@
 
 
-unary_arithmetic = ['~', '!']
-binary_arithmetic = ['+', '-', '*', '/', '%', 'div']
-bit_arithmetic = []
+unary_arithmetic = ['~', '!', 'div']
+binary_arithmetic = ['+', '-', '*', '/', '%']
+bit_arithmetic = ['BITAND', 'BITOR', 'BITXOR', '&', '|', '^', '~']
 
+type_to_column_map = {
+    'TinyInt': ['ktint'],
+    'SmallInt': ['ksint'],
+    'Integer': ['kint'],
+    'BigInt': ['kbint'],
+    'LargeInt': ['klint'],
+    'Float': ['kfloat'],
+    'Double': ['kdbl'],
+    'DecimalV2': ['kdcml'],
+    'Char': ['kchrs1'],
+    'Varchar': ['kvchrs1'],
+    'String': ['kstr'],
+    'Date': ['kdt'],
+    'DateTime': ['kdtm'],
+    'DateV2': ['kdtv2'],
+    'DateTimeV2': ['kdtmv2'],
+    'Boolean': ['kbool'],
+}
+
+type_set = {
+    'int': ['Boolean', 'TinyInt', 'SmallInt', 'Integer', 'BigInt', 'LargeInt'],
+    'float': ['Float', 'Double'],
+    'string': ['Char', 'Varchar', 'String'],
+    'date': ['Date', 'DateTime', 'DateV2', 'DateTimeV2']
+}
