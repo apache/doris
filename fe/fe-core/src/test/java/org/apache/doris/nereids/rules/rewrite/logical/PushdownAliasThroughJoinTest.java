49f9046f38802e5a3651294cf632f7f2289360e9
jakevin <jakevingoo@gmail.com>
2023-03-01 16:34:23 +0800

    [fix](Nereids): fix master problem (#17275)

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/rewrite/logical/PushdownAliasThroughJoinTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/rewrite/logical/PushdownAliasThroughJoinTest.java
@@ -21,15 +21,15 @@ import org.apache.doris.common.Pair;
 import org.apache.doris.nereids.trees.plans.JoinType;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
 import org.apache.doris.nereids.util.LogicalPlanBuilder;
+import org.apache.doris.nereids.util.MemoPatternMatchSupported;
 import org.apache.doris.nereids.util.MemoTestUtils;
-import org.apache.doris.nereids.util.PatternMatchSupported;
 import org.apache.doris.nereids.util.PlanChecker;
 import org.apache.doris.nereids.util.PlanConstructor;
 
 import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Test;
 
-class PushdownAliasThroughJoinTest implements PatternMatchSupported {
+class PushdownAliasThroughJoinTest implements MemoPatternMatchSupported {
 
     @Test
     void testPushdown() {
