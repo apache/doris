88916776e69e8d1c9c629f077525eb9e8c4addfa
yixiutt <yixiu@flywheels.com>
2023-03-01 11:00:12 +0800

    [bugfix](merger) traverse rs_meta in lock
    tablet_schema(version) will traverse rowset_meta and it should
    call in meta_lock.

--- be/src/olap/merger.cpp
+++ be/src/olap/merger.cpp
@@ -43,18 +43,20 @@ Status Merger::vmerge_rowsets(TabletSharedPtr tablet, ReaderType reader_type,
     reader_params.reader_type = reader_type;
     reader_params.rs_readers = src_rowset_readers;
     reader_params.version = dst_rowset_writer->version();
+
+    TabletSchemaSPtr merge_tablet_schema = std::make_shared<TabletSchema>();
+    merge_tablet_schema->copy_from(*cur_tablet_schema);
     {
         std::shared_lock rdlock(tablet->get_header_lock());
         auto delete_preds = tablet->delete_predicates();
         std::copy(delete_preds.cbegin(), delete_preds.cend(),
                   std::inserter(reader_params.delete_predicates,
                                 reader_params.delete_predicates.begin()));
-    }
-    TabletSchemaSPtr merge_tablet_schema = std::make_shared<TabletSchema>();
-    merge_tablet_schema->copy_from(*cur_tablet_schema);
-    // Merge the columns in delete predicate that not in latest schema in to current tablet schema
-    for (auto& del_pred_rs : reader_params.delete_predicates) {
-        merge_tablet_schema->merge_dropped_columns(tablet->tablet_schema(del_pred_rs->version()));
+        // Merge the columns in delete predicate that not in latest schema in to current tablet schema
+        for (auto& del_pred_rs : reader_params.delete_predicates) {
+            merge_tablet_schema->merge_dropped_columns(
+                    tablet->tablet_schema(del_pred_rs->version()));
+        }
     }
     reader_params.tablet_schema = merge_tablet_schema;
 
@@ -172,23 +174,26 @@ Status Merger::vertical_compact_one_group(
     reader_params.reader_type = reader_type;
     reader_params.rs_readers = src_rowset_readers;
     reader_params.version = dst_rowset_writer->version();
+
+    TabletSchemaSPtr merge_tablet_schema = std::make_shared<TabletSchema>();
+    merge_tablet_schema->copy_from(*tablet_schema);
     {
         std::shared_lock rdlock(tablet->get_header_lock());
         auto delete_preds = tablet->delete_predicates();
         std::copy(delete_preds.cbegin(), delete_preds.cend(),
                   std::inserter(reader_params.delete_predicates,
                                 reader_params.delete_predicates.begin()));
+
+        for (auto& del_pred_rs : reader_params.delete_predicates) {
+            merge_tablet_schema->merge_dropped_columns(
+                    tablet->tablet_schema(del_pred_rs->version()));
+        }
     }
+    reader_params.tablet_schema = merge_tablet_schema;
+
     if (is_key && stats_output && stats_output->rowid_conversion) {
         reader_params.record_rowids = true;
     }
-    TabletSchemaSPtr merge_tablet_schema = std::make_shared<TabletSchema>();
-    merge_tablet_schema->copy_from(*tablet_schema);
-    // Merge the columns in delete predicate that not in latest schema in to current tablet schema
-    for (auto& del_pred_rs : reader_params.delete_predicates) {
-        merge_tablet_schema->merge_dropped_columns(tablet->tablet_schema(del_pred_rs->version()));
-    }
-    reader_params.tablet_schema = merge_tablet_schema;
 
     reader_params.return_columns = column_group;
     reader_params.origin_return_columns = &reader_params.return_columns;
