eb0d6016b78d8086a1b6739d4e33f36acf1a2909
Yukang Lian <Yukang.Lian2022@gmail.com>
2023-03-02 20:02:50 +0800

    fix

--- regression-test/suites/mysql_ssl_p0/test_mysql_connection.groovy
+++ regression-test/suites/mysql_ssl_p0/test_mysql_connection.groovy
@@ -17,11 +17,40 @@
 
 suite("test_mysql_connection") {
 
-    String cmdDefault = "mysql -uroot -h172.19.0.2 -P9131 -e \"show variables\"";
-    String cmdDisabledSsl= "mysql --ssl-mode=DISABLE -uroot -h172.19.0.2 -P9131 -e \"show variables\"";
-    String cmdSsl12 = "mysql --ssl-mode=REQUIRED -uroot -h172.19.0.2 -P9131 --tls-version=TLSv1.2 -e \"show variables\"";
+    def executeMySQLCommand = { String command ->
+        try {
+            String line;
+            StringBuilder errMsg = new StringBuilder();
+            StringBuilder msg = new StringBuilder();
+            Process p = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", command});
+
+            BufferedReader errInput = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line = errInput.readLine()) != null) {
+                errMsg.append(line);
+            }
+            assert errMsg.length() == 0: "error occurred!" + errMsg.toString();
+            errInput.close();
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                msg.append(line);
+            }
+            assert msg.toString().contains("version"): "error occurred!" + errMsg.toString();
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String jdbcUrlConfig = context.config.jdbcUrl;
+    String tempString = jdbcUrlConfig.substring(jdbcUrlConfig.indexOf("jdbc:mysql://") + 13);
+    String mysqlHost = tempString.substring(0, tempString.indexOf(":"));
+    String mysqlPort = tempString.substring(tempString.indexOf(":") + 1, tempString.indexOf("/"));
+    String cmdDefault = "mysql -uroot -h" + mysqlHost + " -P" + mysqlPort + " -e \"show variables\"";
+    String cmdDisabledSsl = "mysql --ssl-mode=DISABLE -uroot -h" + mysqlHost + " -P" + mysqlPort + " -e \"show variables\"";
+    String cmdSsl12 = "mysql --ssl-mode=REQUIRED -uroot -h" + mysqlHost + " -P" + mysqlPort + " --tls-version=TLSv1.2 -e \"show variables\"";
     // The current mysql-client version of the test environment is 5.7.32, which does not support TLSv1.3, so comment this part.
-    // String cmdSsl13 = "mysql --ssl-mode=REQUIRED -uroot -h172.19.0.2 -P9131 --tls-version=TLSv1.3 -e \"show variables\"";
+    // String cmdSsl13 = "mysql --ssl-mode=REQUIRED -uroot -h" + mysqlHost + " -P" + mysqlPort +  " --tls-version=TLSv1.3 -e \"show variables\"";
     executeMySQLCommand(cmdDefault);
     executeMySQLCommand(cmdDisabledSsl);
     executeMySQLCommand(cmdSsl12);
