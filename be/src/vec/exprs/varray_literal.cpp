17f4990bd335b3562c955d163c0b60a054aa2206
yiguolei <676222867@qq.com>
2023-03-02 16:23:54 +0800

    [enhancement](functioncontext) function context should use shared ptr and simply function context (#17311)
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- be/src/vec/exprs/varray_literal.cpp
+++ be/src/vec/exprs/varray_literal.cpp
@@ -28,7 +28,7 @@ Status VArrayLiteral::prepare(RuntimeState* state, const RowDescriptor& row_desc
     Field array = is_null ? Field() : Array();
     for (const auto child : _children) {
         Field item;
-        ColumnPtrWrapper* const_col_wrapper = nullptr;
+        std::shared_ptr<ColumnPtrWrapper> const_col_wrapper;
         RETURN_IF_ERROR(child->get_const_col(context, &const_col_wrapper));
         const_col_wrapper->column_ptr->get(0, item);
         array.get<Array>().push_back(item);
