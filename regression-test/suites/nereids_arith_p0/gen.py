2a97bf743a8148786b6899dc16e6bf4dab82b8ab
sohardforaname <organic_chemistry@foxmail.com>
2023-03-02 13:23:11 +0800

    'fix-bugs'

--- regression-test/suites/nereids_arith_p0/gen.py
+++ regression-test/suites/nereids_arith_p0/gen.py
@@ -1,5 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# usage: run this script and paste the output to corresponding suite.
+
 unary_op = ['~', '!', '~']
-binary_op = ['+', '-', '*', '/', '%', 'BITAND', 'BITOR', 'BITXOR', '&', '|', '^']
+binary_op = ['+', '-', '*', '/', 'DIV', '%', '&', '|', '^']
+bit_op = ['BITAND', 'BITOR', 'BITXOR']
 
 type_to_column_map = {
     'TinyInt': ['ktint'],
@@ -27,7 +47,6 @@ type_set = {
     'date': ['Date', 'DateTime', 'DateV2', 'DateTimeV2']
 }
 
-op_map = {
-    ['int', 'int']: [],
+special_ret_map = {
 
 }
