673292348abdd536863a9b900168b45f29db0bf1
BiteTheDDDDt <pxl290@qq.com>
2023-03-02 15:32:04 +0800

    update

--- be/src/vec/functions/array/function_array_aggregation.cpp
+++ be/src/vec/functions/array/function_array_aggregation.cpp
@@ -117,8 +117,8 @@ struct AggregateFunction {
     using Function = typename Derived::template TypeTraits<T>::Function;
 
     static auto create(const DataTypePtr& data_type_ptr) -> AggregateFunctionPtr {
-        return AggregateFunctionPtr(
-                creator_with_type::create<Function>(true, DataTypes {data_type_ptr}));
+        return AggregateFunctionPtr(creator_with_type::create<Function>(
+                true, DataTypes {make_nullable(data_type_ptr)}));
     }
 };
 
@@ -223,8 +223,8 @@ struct NameArrayMin {
 template <>
 struct AggregateFunction<AggregateFunctionImpl<AggregateOperation::MIN>> {
     static auto create(const DataTypePtr& data_type_ptr) -> AggregateFunctionPtr {
-        return AggregateFunctionPtr(
-                create_aggregate_function_min(NameArrayMin::name, {data_type_ptr}, true));
+        return AggregateFunctionPtr(create_aggregate_function_min(
+                NameArrayMin::name, {make_nullable(data_type_ptr)}, true));
     }
 };
 
@@ -235,8 +235,8 @@ struct NameArrayMax {
 template <>
 struct AggregateFunction<AggregateFunctionImpl<AggregateOperation::MAX>> {
     static auto create(const DataTypePtr& data_type_ptr) -> AggregateFunctionPtr {
-        return AggregateFunctionPtr(
-                create_aggregate_function_max(NameArrayMax::name, {data_type_ptr}, true));
+        return AggregateFunctionPtr(create_aggregate_function_max(
+                NameArrayMax::name, {make_nullable(data_type_ptr)}, true));
     }
 };
 
