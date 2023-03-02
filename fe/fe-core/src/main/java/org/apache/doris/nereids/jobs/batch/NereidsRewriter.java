2c01368b8a6359da6f9dc1cbe74a19d2c9e9d7f4
jackwener <jakevingoo@gmail.com>
2023-03-02 19:15:11 +0800

    [refactor](Nereids): refactor PushdownLimit

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/NereidsRewriter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/NereidsRewriter.java
@@ -52,7 +52,6 @@ import org.apache.doris.nereids.rules.rewrite.logical.InferFilterNotNull;
 import org.apache.doris.nereids.rules.rewrite.logical.InferJoinNotNull;
 import org.apache.doris.nereids.rules.rewrite.logical.InferPredicates;
 import org.apache.doris.nereids.rules.rewrite.logical.InnerToCrossJoin;
-import org.apache.doris.nereids.rules.rewrite.logical.LimitPushDown;
 import org.apache.doris.nereids.rules.rewrite.logical.MergeFilters;
 import org.apache.doris.nereids.rules.rewrite.logical.MergeProjects;
 import org.apache.doris.nereids.rules.rewrite.logical.MergeSetOperations;
@@ -60,6 +59,7 @@ import org.apache.doris.nereids.rules.rewrite.logical.NormalizeAggregate;
 import org.apache.doris.nereids.rules.rewrite.logical.PruneOlapScanPartition;
 import org.apache.doris.nereids.rules.rewrite.logical.PruneOlapScanTablet;
 import org.apache.doris.nereids.rules.rewrite.logical.PushFilterInsideJoin;
+import org.apache.doris.nereids.rules.rewrite.logical.PushdownLimit;
 import org.apache.doris.nereids.rules.rewrite.logical.ReorderJoin;
 
 import java.util.List;
@@ -191,7 +191,7 @@ public class NereidsRewriter extends BatchRewriteJob {
                     new PruneOlapScanTablet(),
                     new EliminateAggregate(),
                     new MergeSetOperations(),
-                    new LimitPushDown(),
+                    new PushdownLimit(),
                     new BuildAggForUnion()
             )),
 
