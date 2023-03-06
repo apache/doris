b818bc5600e5254bef3e05b470b66e3df36b842f
yiguolei <yiguolei@gmail.com>
2023-03-06 17:24:34 +0800

    f

--- be/src/vec/aggregate_functions/aggregate_function_approx_count_distinct.h
+++ be/src/vec/aggregate_functions/aggregate_function_approx_count_distinct.h
@@ -85,7 +85,8 @@ public:
                     HashUtil::murmur_hash64A((char*)&value, sizeof(value), HashUtil::MURMUR_SEED));
         } else {
             auto value = static_cast<const ColumnDataType*>(columns[0])->get_data_at(row_num);
-            uint64_t hash_value = HashUtil::murmur_hash64A(sv.data, sv.size, HashUtil::MURMUR_SEED);
+            uint64_t hash_value =
+                    HashUtil::murmur_hash64A(value.data, value.size, HashUtil::MURMUR_SEED);
             this->data(place).add(hash_value);
         }
     }
