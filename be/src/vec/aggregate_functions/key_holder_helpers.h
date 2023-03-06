78c2df9db7792ac071c958ced659fc3a84890440
BiteTheDDDDt <pxl290@qq.com>
2023-03-06 14:56:32 +0800

    remove inline

--- be/src/vec/aggregate_functions/key_holder_helpers.h
+++ be/src/vec/aggregate_functions/key_holder_helpers.h
@@ -26,7 +26,7 @@
 namespace doris::vectorized {
 
 template <bool is_plain_column = false>
-inline auto get_key_holder(const IColumn& column, size_t row_num, Arena& arena) {
+auto get_key_holder(const IColumn& column, size_t row_num, Arena& arena) {
     if constexpr (is_plain_column) {
         return ArenaKeyHolder {column.get_data_at(row_num), arena};
     } else {
@@ -38,7 +38,7 @@ inline auto get_key_holder(const IColumn& column, size_t row_num, Arena& arena) 
 }
 
 template <bool is_plain_column>
-inline void deserialize_and_insert(StringRef str, IColumn& data_to) {
+void deserialize_and_insert(StringRef str, IColumn& data_to) {
     if constexpr (is_plain_column) {
         data_to.insert_data(str.data, str.size);
     } else {
