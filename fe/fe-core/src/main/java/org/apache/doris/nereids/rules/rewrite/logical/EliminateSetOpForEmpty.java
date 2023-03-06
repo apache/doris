377fc478101416956bb54b4078e18bf114056efa
sohardforaname <organic_chemistry@foxmail.com>
2023-03-06 16:50:11 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/EliminateSetOpForEmpty.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/EliminateSetOpForEmpty.java
@@ -17,10 +17,13 @@
 
 package org.apache.doris.nereids.rules.rewrite.logical;
 
+import org.apache.doris.nereids.pattern.PatternDescriptor;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
 import org.apache.doris.nereids.rules.rewrite.RewriteRuleFactory;
 import org.apache.doris.nereids.trees.plans.logical.LogicalEmptyRelation;
+import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalSetOperation;
 
 import com.google.common.collect.ImmutableList;
 
@@ -33,13 +36,25 @@ public class EliminateSetOpForEmpty implements RewriteRuleFactory {
 
     @Override
     public List<Rule> buildRules() {
+        PatternDescriptor<LogicalSetOperation> base = logicalSetOperation().when(this::checkEmpty);
         return ImmutableList.of(
                 RuleType.ELIMINATE_INTERSECT.build(
-                        logicalIntersect()
-                                .when(intersect -> intersect.child(0) instanceof LogicalEmptyRelation
-                                        || intersect.child(1) instanceof LogicalEmptyRelation)
-                                .then(intersect -> )
+                        base.then(intersect -> new LogicalEmptyRelation(intersect.child(0).getOutput()))
+                ),
+                RuleType.ELIMINATE_UNION.build(
+                        base.then(union -> {
+                            int emptyIndex = union.child(1) instanceof LogicalEmptyRelation ? 0 : 1;
+                            return union.child(emptyIndex);
+                        })
+                ),
+                RuleType.ELIMINATE_EXCEPT.build(
+                        base.then(except -> except.child(0))
                 )
-        )
+        );
+    }
+
+    private boolean checkEmpty(LogicalPlan plan) {
+        return plan.child(0) instanceof LogicalEmptyRelation
+                || plan.child(1) instanceof LogicalEmptyRelation;
     }
 }
