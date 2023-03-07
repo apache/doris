02d835e27424c0b305f9a6f5141b9d4976a9943e
yiguolei <yiguolei@gmail.com>
2023-03-07 18:14:26 +0800

    [enhancement](exception) add exception structure

---
+++ be/src/common/exception.cpp
@@ -0,0 +1,48 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include "common/exception.h"
+
+namespace doris {
+
+Exception::Exception(int code, const std::string_view msg) {
+    _code = code;
+    _err_msg = std::make_unique<ErrMsg>();
+    _err_msg->_msg = msg;
+#ifdef ENABLE_STACKTRACE
+    if constexpr (capture_stacktrace<code>()) {
+        _err_msg->_stack = get_stack_trace();
+    }
+#endif
+}
+Exception::Exception(int code, const std::string_view msg, const Exception& nested) {
+    _code = code;
+    _err_msg = std::make_unique<ErrMsg>();
+    _err_msg->_msg = msg;
+#ifdef ENABLE_STACKTRACE
+    if constexpr (capture_stacktrace<code>()) {
+        _err_msg->_stack = get_stack_trace();
+    }
+#endif
+    _nested_excption = std::make_unique<Exception>();
+    _nested_excption->_code = nested._code;
+    _nested_excption->_err_msg = nested._err_msg;
+}
+
+} // namespace doris
