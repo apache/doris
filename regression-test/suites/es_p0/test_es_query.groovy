f0bd002911ec7ed758a02a48820e769fc7b147ca
qiye <jianliang5669@gmail.com>
2023-03-08 21:51:17 +0800

    [fix](DOE) Fix esquery not working (#17566)
    Function esquery does not work because there is a problem parsing the first parameter type.
    The first parameter, which is SlotRef, will be cast to CastExpr. This will cause error while generating ES DSL.
    Add more types to adapt esquery function.

--- regression-test/suites/es_p0/test_es_query.groovy
+++ regression-test/suites/es_p0/test_es_query.groovy
@@ -96,7 +96,8 @@ suite("test_es_query", "p0") {
                 "http_ssl_enabled"="false"
             );
         """
-        order_qt_sql52 """select * from test_v1 where test2='text#1'"""
+        order_qt_sql51 """select * from test_v1 where test2='text#1'"""
+        order_qt_sql52 """select * from test_v1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
 
        sql """
             CREATE TABLE `test_v2` (
@@ -133,7 +134,8 @@ suite("test_es_query", "p0") {
                 "http_ssl_enabled"="false"
             );
         """
-        order_qt_sql51 """select * from test_v2 where test2='text#1'"""
+        order_qt_sql53 """select * from test_v2 where test2='text#1'"""
+        order_qt_sql54 """select * from test_v2 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
 
 
         sql """create catalog if not exists es6 with resource es6_resource;"""
@@ -146,6 +148,7 @@ suite("test_es_query", "p0") {
         order_qt_sql64 """select * from test2_20220808 where substring(test2, 2) = 'ext2'"""
         order_qt_sql65 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql66 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2_20220808"""
+        order_qt_sql67 """select * from test1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
         sql """switch es7"""
         // order_qt_sql71 """show tables"""
         order_qt_sql72 """select * from test1 where test2='text#1'"""
@@ -153,11 +156,13 @@ suite("test_es_query", "p0") {
         order_qt_sql74 """select * from test2_20220808 where substring(test2, 2) = 'ext2'"""
         order_qt_sql75 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql76 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2"""
+        order_qt_sql77 """select * from test1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
         sql """switch es8"""
         order_qt_sql81 """select * from test1 where test2='text#1'"""
         order_qt_sql82 """select * from test2_20220808 where test4='2022-08-08'"""
         order_qt_sql83 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql84 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2"""
+        order_qt_sql85 """select * from test1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
 
 
         sql """drop catalog if exists es6;"""
