5930423cb83f312ac25102ac90c567af424ea81d
Zenglin Luo <luozenglin@gmail.com>
2023-03-08 16:45:53 +0800

    [fix](in-bitmap) fix result may be wrong if the left side of the in bitmap predicate is a constant

--- fe/fe-core/src/main/java/org/apache/doris/planner/NestedLoopJoinNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/NestedLoopJoinNode.java
@@ -49,6 +49,18 @@ import java.util.List;
 public class NestedLoopJoinNode extends JoinNodeBase {
     private static final Logger LOG = LogManager.getLogger(NestedLoopJoinNode.class);
 
+    // If isOutputLeftSideOnly=true, the data from the left table is returned directly without a join operation.
+    // This is used to optimize `in bitmap`, because bitmap will make a lot of copies when doing Nested Loop Join,
+    // which is very resource intensive.
+    // `in bitmap` has two cases:
+    // 1. select * from tbl1 where k1 in (select bitmap_col from tbl2);
+    //   This will generate a bitmap runtime filter to filter the left table, because the bitmap is an exact filter
+    //   and does not need to be filtered again in the NestedLoopJoinNode, so it returns the left table data directly.
+    // 2. select * from tbl1 where 1 in (select bitmap_col from tbl2);
+    //    This sql will be rewritten to
+    //    "select * from tbl1 left semi join tbl2 where bitmap_contains(tbl2.bitmap_col, 1);"
+    //    return all data in the left table to parent node when there is data on the build side, and return empty when
+    //    there is no data on the build side.
     private boolean isOutputLeftSideOnly = false;
 
     private List<Expr> runtimeFilterExpr = Lists.newArrayList();
@@ -246,8 +258,8 @@ public class NestedLoopJoinNode extends JoinNodeBase {
         if (!runtimeFilters.isEmpty()) {
             output.append(detailPrefix).append("runtime filters: ");
             output.append(getRuntimeFilterExplainString(true));
-            output.append(detailPrefix).append("is output left side only: ").append(isOutputLeftSideOnly).append("\n");
         }
+        output.append(detailPrefix).append("is output left side only: ").append(isOutputLeftSideOnly).append("\n");
         output.append(detailPrefix).append(String.format("cardinality=%,d", cardinality)).append("\n");
         // todo unify in plan node
         if (vOutputTupleDesc != null) {
