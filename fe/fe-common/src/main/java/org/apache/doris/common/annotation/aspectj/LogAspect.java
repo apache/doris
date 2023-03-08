83a46b7bcbda8d6c670b2bd5af37fcf5a306b782
NetShrimp <kongxuan.yao@shopee.com>
2023-03-08 13:37:27 +0800

    add java annotation to catch throwable from a method and print log

---
+++ fe/fe-common/src/main/java/org/apache/doris/common/annotation/aspectj/LogAspect.java
@@ -0,0 +1,40 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.common.annotation.aspectj;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LogAspect {
+
+    private static final Logger LOG = LogManager.getLogger(LogAspect.class);
+
+    @Pointcut("@annotation(org.apache.doris.common.annotation.ExceptionLog)")
+    public void logPointCut() {
+    }
+
+    @AfterThrowing(value = "logPointCut()", throwing = "e")
+    public void exceptionLog(JoinPoint joinPoint, Exception e)  {
+        LOG.error(e.getMessage());
+    }
+}
