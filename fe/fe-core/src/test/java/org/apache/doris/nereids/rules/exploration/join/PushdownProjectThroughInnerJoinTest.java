2b6133f4d0742ffa2446a46eee278cb909cd6de4
jakevin <jakevingoo@gmail.com>
2023-03-08 12:00:56 +0800

    [feature](Nereids): pushdown complex project through inner/outer Join. (#17365)

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/PushdownProjectThroughInnerJoinTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/PushdownProjectThroughInnerJoinTest.java
@@ -32,94 +32,120 @@ import org.apache.doris.nereids.util.PlanChecker;
 import org.apache.doris.nereids.util.PlanConstructor;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-class PushdownProjectThroughSemiJoinTest implements MemoPatternMatchSupported {
+class PushdownProjectThroughInnerJoinTest implements MemoPatternMatchSupported {
     private final LogicalOlapScan scan1 = PlanConstructor.newLogicalOlapScan(0, "t1", 0);
     private final LogicalOlapScan scan2 = PlanConstructor.newLogicalOlapScan(1, "t2", 0);
 
     @Test
-    public void pushdownProject() {
-        // project (t1.id + 1) as alias, t1.name
+    public void pushBothSide() {
+        // project (t1.id + 1) as alias, t1.name, (t2.id + 1) as alias, t2.name
         List<NamedExpression> projectExprs = ImmutableList.of(
                 new Alias(new Add(scan1.getOutput().get(0), Literal.of(1)), "alias"),
-                scan1.getOutput().get(1)
+                scan1.getOutput().get(1),
+                new Alias(new Add(scan2.getOutput().get(0), Literal.of(1)), "alias"),
+                scan2.getOutput().get(1)
         );
         // complex projection contain ti.id, which isn't in Join Condition
         LogicalPlan plan = new LogicalPlanBuilder(scan1)
-                .join(scan2, JoinType.LEFT_SEMI_JOIN, Pair.of(1, 1))
+                .join(scan2, JoinType.INNER_JOIN, Pair.of(1, 1))
                 .projectExprs(projectExprs)
                 .build();
 
         PlanChecker.from(MemoTestUtils.createConnectContext(), plan)
-                .applyExploration(PushdownProjectThroughSemiJoin.INSTANCE.build())
+                .applyExploration(PushdownProjectThroughInnerJoin.INSTANCE.build())
                 .printlnOrigin()
                 .printlnExploration()
                 .matchesExploration(
-                        leftSemiLogicalJoin(
-                                logicalProject(
-                                        logicalOlapScan()
-                                ).when(project -> project.getProjects().size() == 2),
-                                logicalOlapScan()
+                        logicalJoin(
+                                logicalProject().when(project -> project.getProjects().size() == 2),
+                                logicalProject().when(project -> project.getProjects().size() == 2)
                         )
                 );
     }
 
     @Test
     public void pushdownProjectInCondition() {
-        // project (t1.id + 1) as alias, t1.name
+        // project (t1.id + 1) as alias, t1.name, (t2.id + 1) as alias, t2.name
         List<NamedExpression> projectExprs = ImmutableList.of(
                 new Alias(new Add(scan1.getOutput().get(0), Literal.of(1)), "alias"),
-                scan1.getOutput().get(1)
+                scan1.getOutput().get(1),
+                new Alias(new Add(scan2.getOutput().get(0), Literal.of(1)), "alias"),
+                scan2.getOutput().get(1)
         );
         // complex projection contain ti.id, which is in Join Condition
         LogicalPlan plan = new LogicalPlanBuilder(scan1)
-                .join(scan2, JoinType.LEFT_SEMI_JOIN, Pair.of(0, 0))
+                .join(scan2, JoinType.INNER_JOIN, Pair.of(0, 0))
                 .projectExprs(projectExprs)
                 .build();
 
         PlanChecker.from(MemoTestUtils.createConnectContext(), plan)
-                .applyExploration(PushdownProjectThroughSemiJoin.INSTANCE.build())
+                .applyExploration(PushdownProjectThroughInnerJoin.INSTANCE.build())
                 .printlnOrigin()
                 .printlnExploration()
                 .matchesExploration(
                         logicalProject(
-                                leftSemiLogicalJoin(
-                                        logicalProject(
-                                                logicalOlapScan()
-                                        ).when(project -> project.getProjects().size() == 3),
-                                        logicalOlapScan()
+                                logicalJoin(
+                                        logicalProject().when(project -> project.getProjects().size() == 3),
+                                        logicalProject().when(project -> project.getProjects().size() == 3)
                                 )
-                        ).when(project -> project.getProjects().size() == 2)
+                        )
                 );
     }
 
     @Test
     void pushComplexProject() {
-        // project (t1.id + t1.name) as complex
+        // project (t1.id + t1.name) as complex1, (t2.id + t2.name) as complex2
         List<NamedExpression> projectExprs = ImmutableList.of(
-                new Alias(new Add(scan1.getOutput().get(0), scan1.getOutput().get(1)), "complex"));
+                new Alias(new Add(scan1.getOutput().get(0), scan1.getOutput().get(1)), "complex1"),
+                new Alias(new Add(scan2.getOutput().get(0), scan2.getOutput().get(1)), "complex2")
+        );
         // complex projection contain ti.id, which is in Join Condition
         LogicalPlan plan = new LogicalPlanBuilder(scan1)
-                .join(scan2, JoinType.LEFT_SEMI_JOIN, Pair.of(0, 0))
+                .join(scan2, JoinType.INNER_JOIN, Pair.of(0, 0))
                 .projectExprs(projectExprs)
                 .build();
 
         PlanChecker.from(MemoTestUtils.createConnectContext(), plan)
-                .applyExploration(PushdownProjectThroughSemiJoin.INSTANCE.build())
+                .applyExploration(PushdownProjectThroughInnerJoin.INSTANCE.build())
                 .printlnOrigin()
                 .printlnExploration()
                 .matchesExploration(
                     logicalProject(
-                        leftSemiLogicalJoin(
+                        logicalJoin(
                             logicalProject()
-                                .when(project -> project.getProjects().get(0).toSql().equals("(id + name) AS `complex`")
-                                    && project.getProjects().get(1).toSql().equals("id")),
-                            logicalOlapScan()
+                                .when(project ->
+                                        project.getProjects().get(0).toSql().equals("(id + name) AS `complex1`")
+                                        && project.getProjects().get(1).toSql().equals("id")),
+                            logicalProject()
+                                .when(project ->
+                                        project.getProjects().get(0).toSql().equals("(id + name) AS `complex2`")
+                                        && project.getProjects().get(1).toSql().equals("id"))
                         )
-                    ).when(project -> project.getProjects().get(0).toSql().equals("complex"))
+                    ).when(project -> project.getProjects().get(0).toSql().equals("complex1")
+                            && project.getProjects().get(1).toSql().equals("complex2")
+                    )
                 );
+    }
+
+    @Test
+    void rejectHyperEdgeProject() {
+        // project (t1.id + t2.id) as alias
+        List<NamedExpression> projectExprs = ImmutableList.of(
+                new Alias(new Add(scan1.getOutput().get(0), scan2.getOutput().get(0)), "alias")
+        );
+        // complex projection contain ti.id, which is in Join Condition
+        LogicalPlan plan = new LogicalPlanBuilder(scan1)
+                .join(scan2, JoinType.INNER_JOIN, Pair.of(0, 0))
+                .projectExprs(projectExprs)
+                .build();
+
+        PlanChecker.from(MemoTestUtils.createConnectContext(), plan)
+                .applyExploration(PushdownProjectThroughInnerJoin.INSTANCE.build())
+                .checkMemo(memo -> Assertions.assertEquals(1, memo.getRoot().getLogicalExpressions().size()));
     }
 }
