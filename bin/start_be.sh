5e4ed5a26cbe3cd0b0a654176ed03adf64644995
Adonis Ling <adonis0147@gmail.com>
2023-03-08 10:21:45 +0800

    [chore](macOS) Disable detect_container_overflow at BE startup (#17514)
    BE failed to start up due to container-overflow errors reported by address sanitizer.

--- bin/start_be.sh
+++ bin/start_be.sh
@@ -187,7 +187,7 @@ if [[ "${RUN_IN_AWS}" -eq 0 ]]; then
 fi
 
 ## set asan and ubsan env to generate core file
-export ASAN_OPTIONS=symbolize=1:abort_on_error=1:disable_coredump=0:unmap_shadow_on_exit=1
+export ASAN_OPTIONS=symbolize=1:abort_on_error=1:disable_coredump=0:unmap_shadow_on_exit=1:detect_container_overflow=0
 export UBSAN_OPTIONS=print_stacktrace=1
 
 ## set TCMALLOC_HEAP_LIMIT_MB to limit memory used by tcmalloc
