b8d56fe1c3e45966df543d111a8e3cebcb8700a6
minghong <englefly@gmail.com>
2023-03-09 09:30:24 +0800

    [feature](nereids)support bitmap runtime filter on nereids (#16927)
    * A in(B) -> bitmap_contains(bitmap_union(B), A)
    support bitmap runtime filter on nereids
    
    * GroupPlan -> Plan
    
    * fmt
    
    * fix target cast problem
    remove test code

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/RuntimeFilterTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/RuntimeFilterTranslator.java
@@ -22,21 +22,27 @@ import org.apache.doris.analysis.Expr;
 import org.apache.doris.analysis.SlotId;
 import org.apache.doris.analysis.SlotRef;
 import org.apache.doris.analysis.TupleId;
+import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.processor.post.RuntimeFilterContext;
+import org.apache.doris.nereids.trees.expressions.ExprId;
 import org.apache.doris.nereids.trees.expressions.Slot;
+import org.apache.doris.nereids.trees.expressions.SlotReference;
 import org.apache.doris.nereids.trees.plans.RelationId;
-import org.apache.doris.nereids.trees.plans.physical.PhysicalHashJoin;
+import org.apache.doris.nereids.trees.plans.physical.AbstractPhysicalJoin;
 import org.apache.doris.nereids.trees.plans.physical.RuntimeFilter;
 import org.apache.doris.planner.HashJoinNode;
 import org.apache.doris.planner.HashJoinNode.DistributionMode;
+import org.apache.doris.planner.JoinNodeBase;
 import org.apache.doris.planner.RuntimeFilter.RuntimeFilterTarget;
 import org.apache.doris.planner.ScanNode;
+import org.apache.doris.thrift.TRuntimeFilterType;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * translate runtime filter
@@ -50,7 +56,7 @@ public class RuntimeFilterTranslator {
         context.generatePhysicalHashJoinToRuntimeFilter();
     }
 
-    public List<RuntimeFilter> getRuntimeFilterOfHashJoinNode(PhysicalHashJoin join) {
+    public List<RuntimeFilter> getRuntimeFilterOfHashJoinNode(AbstractPhysicalJoin join) {
         return context.getRuntimeFilterOnHashJoinNode(join);
     }
 
@@ -68,38 +74,81 @@ public class RuntimeFilterTranslator {
         context.getScanNodeOfLegacyRuntimeFilterTarget().put(slot, node);
     }
 
+    private class RuntimeFilterExpressionTranslator extends ExpressionTranslator {
+        Map<ExprId, SlotRef> nereidsExprIdToSlotRef;
+
+        RuntimeFilterExpressionTranslator(Map<ExprId, SlotRef> nereidsExprIdToSlotRef) {
+            this.nereidsExprIdToSlotRef = nereidsExprIdToSlotRef;
+        }
+
+        @Override
+        public Expr visitSlotReference(SlotReference slotReference, PlanTranslatorContext context) {
+            SlotRef slot = nereidsExprIdToSlotRef.get(slotReference.getExprId());
+            if (slot == null) {
+                throw new AnalysisException("cannot find SlotRef for " + slotReference);
+            }
+            return slot;
+        }
+    }
+
     /**
      * generate legacy runtime filter
      * @param filter nereids runtime filter
      * @param node hash join node
      * @param ctx plan translator context
      */
-    public void createLegacyRuntimeFilter(RuntimeFilter filter, HashJoinNode node, PlanTranslatorContext ctx) {
+    public void createLegacyRuntimeFilter(RuntimeFilter filter, JoinNodeBase node, PlanTranslatorContext ctx) {
         Expr target = context.getExprIdToOlapScanNodeSlotRef().get(filter.getTargetExpr().getExprId());
         if (target == null) {
             context.setTargetNullCount();
             return;
         }
+        Expr targetExpr = null;
+        if (filter.getType() == TRuntimeFilterType.BITMAP) {
+            if (filter.getTargetExpression().equals(filter.getTargetExpr())) {
+                targetExpr = target;
+            } else {
+                RuntimeFilterExpressionTranslator translator = new RuntimeFilterExpressionTranslator(
+                        context.getExprIdToOlapScanNodeSlotRef());
+                try {
+                    targetExpr = filter.getTargetExpression().accept(translator, ctx);
+                    targetExpr.finalizeForNereids();
+                } catch (org.apache.doris.common.AnalysisException e) {
+                    throw new AnalysisException(
+                            "Translate Nereids expression to stale expression failed. " + e.getMessage(), e);
+                }
+
+            }
+        } else {
+            targetExpr = target;
+        }
+
         Expr src = ExpressionTranslator.translate(filter.getSrcExpr(), ctx);
         SlotRef targetSlot = target.getSrcSlotRef();
         TupleId targetTupleId = targetSlot.getDesc().getParent().getId();
         SlotId targetSlotId = targetSlot.getSlotId();
         // adjust data type
-        if (!src.getType().equals(target.getType())) {
-            target = new CastExpr(src.getType(), target);
+        if (!src.getType().equals(target.getType()) && filter.getType() != TRuntimeFilterType.BITMAP) {
+            targetExpr = new CastExpr(src.getType(), targetExpr);
         }
         org.apache.doris.planner.RuntimeFilter origFilter
                 = org.apache.doris.planner.RuntimeFilter.fromNereidsRuntimeFilter(
-                filter.getId(), node, src, filter.getExprOrder(), target,
+                filter.getId(), node, src, filter.getExprOrder(), targetExpr,
                 ImmutableMap.of(targetTupleId, ImmutableList.of(targetSlotId)),
                 filter.getType(), context.getLimits());
-        origFilter.setIsBroadcast(node.getDistributionMode() == DistributionMode.BROADCAST);
+        if (node instanceof HashJoinNode) {
+            origFilter.setIsBroadcast(((HashJoinNode) node).getDistributionMode() == DistributionMode.BROADCAST);
+        } else {
+            //bitmap rf requires isBroadCast=false, it always requires merge filter
+            origFilter.setIsBroadcast(false);
+        }
         ScanNode scanNode = context.getScanNodeOfLegacyRuntimeFilterTarget().get(filter.getTargetExpr());
         origFilter.addTarget(new RuntimeFilterTarget(
                 scanNode,
-                target,
+                targetExpr,
                 true,
                 scanNode.getFragmentId().equals(node.getFragmentId())));
+        origFilter.setBitmapFilterNotIn(filter.isBitmapFilterNotIn());
         context.getLegacyFilters().add(finalize(origFilter));
     }
 
