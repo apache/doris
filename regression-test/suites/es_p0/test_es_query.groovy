b621a24926493d5efb383b7b771fcd3b502dd180
huangzhaowei <carlmartinmax@gmail.com>
2023-03-10 09:59:20 +0800

    [Enhance](DOE)Support parse default es iso datetime string (#17412)
    * support parse default es iso datetime string

--- regression-test/suites/es_p0/test_es_query.groovy
+++ regression-test/suites/es_p0/test_es_query.groovy
@@ -83,6 +83,7 @@ suite("test_es_query", "p0") {
                 `c_person` array<text> NULL,
                 `test6` datetime NULL,
                 `test7` datetime NULL,
+                `test8` datetime NULL,
                 `c_byte` array<tinyint(4)> NULL,
                 `c_bool` array<boolean> NULL,
                 `c_integer` array<int(11)> NULL
@@ -98,6 +99,7 @@ suite("test_es_query", "p0") {
         """
         order_qt_sql51 """select * from test_v1 where test2='text#1'"""
         order_qt_sql52 """select * from test_v1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
+        order_qt_sql53 """select test4,test5,test6,test7,test8 from test_v1 order by test8"""
 
        sql """
             CREATE TABLE `test_v2` (
@@ -121,6 +123,7 @@ suite("test_es_query", "p0") {
                 `c_person` array<text> NULL,
                 `test6` datetimev2 NULL,
                 `test7` datetimev2 NULL,
+                `test8` datetimev2 NULL,
                 `c_byte` array<tinyint(4)> NULL,
                 `c_bool` array<boolean> NULL,
                 `c_integer` array<int(11)> NULL
@@ -136,7 +139,7 @@ suite("test_es_query", "p0") {
         """
         order_qt_sql53 """select * from test_v2 where test2='text#1'"""
         order_qt_sql54 """select * from test_v2 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
-
+        order_qt_sql55 """select test4,test5,test6,test7,test8 from test_v2 order by test8"""
 
         sql """create catalog if not exists es6 with resource es6_resource;"""
         sql """create catalog if not exists es7 with resource es7_resource;"""
@@ -144,7 +147,7 @@ suite("test_es_query", "p0") {
         sql """switch es6"""
         // order_qt_sql61 """show tables"""
         order_qt_sql62 """select * from test1 where test2='text#1'"""
-        order_qt_sql63 """select * from test2_20220808 where test4='2022-08-08'"""
+        order_qt_sql63 """select * from test2_20220808 where test4 >= '2022-08-08 00:00:00' and test4 < '2022-08-08 23:59:59'"""
         order_qt_sql64 """select * from test2_20220808 where substring(test2, 2) = 'ext2'"""
         order_qt_sql65 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql66 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2_20220808"""
@@ -152,14 +155,14 @@ suite("test_es_query", "p0") {
         sql """switch es7"""
         // order_qt_sql71 """show tables"""
         order_qt_sql72 """select * from test1 where test2='text#1'"""
-        order_qt_sql73 """select * from test2_20220808 where test4='2022-08-08'"""
+        order_qt_sql73 """select * from test2_20220808 where test4 >= '2022-08-08 00:00:00' and test4 < '2022-08-08 23:59:59'"""
         order_qt_sql74 """select * from test2_20220808 where substring(test2, 2) = 'ext2'"""
         order_qt_sql75 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql76 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2"""
         order_qt_sql77 """select * from test1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
         sql """switch es8"""
         order_qt_sql81 """select * from test1 where test2='text#1'"""
-        order_qt_sql82 """select * from test2_20220808 where test4='2022-08-08'"""
+        order_qt_sql82 """select * from test2_20220808 where test4 >= '2022-08-08 00:00:00' and test4 < '2022-08-08 23:59:59'"""
         order_qt_sql83 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test1"""
         order_qt_sql84 """select c_bool[1], c_byte[1], c_short[1], c_integer[1], c_long[1], c_unsigned_long[1], c_float[1], c_half_float[1], c_double[1], c_scaled_float[1], c_date[1], c_datetime[1], c_keyword[1], c_text[1], c_ip[1], c_person[1] from test2"""
         order_qt_sql85 """select * from test1 where esquery(test2, '{"match":{"test2":"text#1"}}')"""
