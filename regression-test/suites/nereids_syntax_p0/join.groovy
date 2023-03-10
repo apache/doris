e5fdd20cfd47541fea4066df46b6a74160f33ed5
englefly <englefly@gmail.com>
2023-03-10 10:23:27 +0800

    fix case

--- regression-test/suites/nereids_syntax_p0/join.groovy
+++ regression-test/suites/nereids_syntax_p0/join.groovy
@@ -224,7 +224,7 @@ suite("join") {
     assertTrue(
         //if analyze finished
             explainStr.contains("4:VAGGREGATE (update serialize)") && explainStr.contains("6:VAGGREGATE (merge finalize)")
-                    && explainStr.contains("wtid[#8] = CAST(wtid[#3] AS CHARACTER)") && explainStr.contains("projections: wtid[#5], wfid[#6]")
+                    && explainStr.contains("wtid[#8] = wtid[#3]") && explainStr.contains("projections: wtid[#5], wfid[#6]")
                     ||
         //analyze not finished
                     explainStr.contains("7:VAGGREGATE (update finalize)") && explainStr.contains("5:VAGGREGATE (update finalize)")
