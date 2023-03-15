f4355e9c209420e03b28f417b4119146d0ee6751
ByteYue <yj976240184@gmail.com>
2023-03-15 11:28:35 +0800

    add missed header

--- regression-test/suites/cold_heat_separation_p2/create_table_use_partition_policy.groovy
+++ regression-test/suites/cold_heat_separation_p2/create_table_use_partition_policy.groovy
@@ -14,6 +14,8 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+import groovy.json.JsonSlurper
+import org.codehaus.groovy.runtime.IOGroovyMethods
 
 suite("create_table_use_partition_policy") {
     def fetchBeHttp = { check_func, meta_url ->
