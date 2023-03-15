c1929523601bc3d084e7f472c6ed850398a6d2e2
gitccl <chenchuanle6@gmail.com>
2023-03-15 12:59:15 +0800

    [feature](json) add json_extract function

--- gensrc/script/doris_builtins_functions.py
+++ gensrc/script/doris_builtins_functions.py
@@ -1499,6 +1499,7 @@ visible_functions = [
     [['json_object'], 'VARCHAR', ['VARCHAR', '...'], 'ALWAYS_NOT_NULLABLE'],
     [['json_quote'], 'VARCHAR', ['VARCHAR'], ''],
     [['json_valid'], 'INT', ['VARCHAR'], 'ALWAYS_NULLABLE'],
+    [['json_extract'], 'VARCHAR', ['VARCHAR', 'VARCHAR', '...'], ''],
 
     #hll function
     [['hll_cardinality'], 'BIGINT', ['HLL'], 'ALWAYS_NOT_NULLABLE'],
