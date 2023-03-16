2ef9a2c5334c7ff2df57cd209aba6c06b8bcebf9
morrySnow <morrysnow@126.com>
2023-03-16 11:38:10 +0800

    [fix](regression) fix unstable test cases and remove redundant cases

--- regression-test/suites/nereids_syntax_p0/aggregate_strategies.groovy
+++ regression-test/suites/nereids_syntax_p0/aggregate_strategies.groovy
@@ -184,31 +184,31 @@ suite("aggregate_strategies") {
         sql """select
                 /*+SET_VAR(disable_nereids_rules='TWO_PHASE_AGGREGATE_WITH_DISTINCT')*/
                 count(distinct number)
-                from numbers('number' = '10000000', 'backend_num'='10')"""
-        result([[10000000L]])
+                from numbers('number' = '10000', 'backend_num'='10')"""
+        result([[10000L]])
     }
 
     test {
         sql """select
                 /*+SET_VAR(disable_nereids_rules='THREE_PHASE_AGGREGATE_WITH_DISTINCT')*/
                 count(distinct number)
-                from numbers('number' = '10000000', 'backend_num'='10')"""
-        result([[10000000L]])
+                from numbers('number' = '10000', 'backend_num'='10')"""
+        result([[10000L]])
     }
 
     test {
         sql """select
                 /*+SET_VAR(disable_nereids_rules='TWO_PHASE_AGGREGATE_WITH_DISTINCT')*/
                 count(distinct number)
-                from numbers('number' = '10000000', 'backend_num'='1')"""
-        result([[10000000L]])
+                from numbers('number' = '10000', 'backend_num'='1')"""
+        result([[10000L]])
     }
 
     test {
         sql """select
                 /*+SET_VAR(disable_nereids_rules='THREE_PHASE_AGGREGATE_WITH_DISTINCT')*/
                 count(distinct number)
-                from numbers('number' = '10000000', 'backend_num'='1')"""
-        result([[10000000L]])
+                from numbers('number' = '10000', 'backend_num'='1')"""
+        result([[10000L]])
     }
 }
