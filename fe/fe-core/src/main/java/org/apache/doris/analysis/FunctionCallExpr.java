637f7afb91602e75e80da127c0c9b8ccda3218f8
Kang <kxiao.tiger@gmail.com>
2023-03-17 10:28:17 +0800

    [feature](map) basic functions for map datatype (#16916)
    basic functions for map datatype:
    - MAP<K, V> map(K k1, V v1, ...)
    - BIGINT map_size(MAP<K, V> m)
    - BOOL map_contains_key(MAP<K, V> m, K k1)
    - BOOL map_contains_value(MAP<K, V> m, V v1)
    - ARRAY< K> map_keys(MAP<K, V> m)
    - ARRAY< V> map_values(MAP<K, V> m)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
@@ -28,6 +28,7 @@ import org.apache.doris.catalog.DatabaseIf;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.Function;
 import org.apache.doris.catalog.FunctionSet;
+import org.apache.doris.catalog.MapType;
 import org.apache.doris.catalog.ScalarFunction;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
@@ -1401,6 +1402,12 @@ public class FunctionCallExpr extends Expr {
             for (int i = 0; i < argTypes.length - orderByElements.size(); ++i) {
                 // For varargs, we must compare with the last type in callArgs.argTypes.
                 int ix = Math.min(args.length - 1, i);
+                // map varargs special case map(key_type, value_type, ...)
+                if (i >= args.length && i >= 2 && args.length >= 2
+                        && fnName.getFunction().equalsIgnoreCase("map")) {
+                    ix = i % 2 == 0 ? 0 : 1;
+                }
+
                 if ((fnName.getFunction().equalsIgnoreCase("money_format") || fnName.getFunction()
                         .equalsIgnoreCase("histogram")
                         || fnName.getFunction().equalsIgnoreCase("hist"))
@@ -1520,6 +1527,10 @@ public class FunctionCallExpr extends Expr {
         if (fnName.getFunction().equalsIgnoreCase("array")) {
             if (children.size() > 0) {
                 this.type = new ArrayType(children.get(0).getType());
+            }
+        } else if (fnName.getFunction().equalsIgnoreCase("map")) {
+            if (children.size() > 1) {
+                this.type = new MapType(children.get(0).getType(), children.get(1).getType());
             }
         } else if (fnName.getFunction().equalsIgnoreCase("if")) {
             if (children.get(1).getType().isArrayType() && (
