e3b9ded79d0ed48a78a26092f0bc143c2c6b937a
ZhangYu0123 <neuswczyzy@163.com>
2023-03-19 13:32:46 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/analysis/LambdaFunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/LambdaFunctionCallExpr.java
@@ -29,6 +29,7 @@ import com.google.common.collect.ImmutableSortedSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LambdaFunctionCallExpr extends FunctionCallExpr {
