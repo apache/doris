d1f34a3be45b527a9eb9f43d99a9c790a97e1812
Kang <kxiao.tiger@gmail.com>
2023-03-27 11:29:42 +0800

    [bugfix](inverted index)temporary disable skip read column data if it match inverted index (#18065)
    The optimization for skip reading column data if it match inverted index and only used in WHERE clause may get wrong result for complex SQL.
    
    This PR temporary disable the optimization and later PRs will resolve the problem fundamentality.

--- be/src/olap/rowset/segment_v2/segment_iterator.cpp
+++ be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -852,13 +852,7 @@ Status SegmentIterator::_apply_inverted_index_on_block_column_predicate(
 }
 
 bool SegmentIterator::_need_read_data(ColumnId cid) {
-    int32_t unique_id = _schema.unique_id(cid);
-    if (_need_read_data_indices.count(unique_id) > 0 && !_need_read_data_indices[unique_id] &&
-        _output_columns.count(unique_id) < 1) {
-        VLOG_DEBUG << "SegmentIterator no need read data for column: "
-                   << _opts.tablet_schema->column_by_uid(unique_id).name();
-        return false;
-    }
+    // TODO(xk) impl right logic
     return true;
 }
 
