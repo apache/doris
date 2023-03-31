592794f0763e6a3b41a49b618f0e1cb0f6dd01c8
morningman <morningman@163.com>
2023-03-31 23:17:25 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/analysis/OutFileClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/OutFileClause.java
@@ -27,7 +27,6 @@ import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.FeConstants;
-import org.apache.doris.common.FeNameFormat;
 import org.apache.doris.common.Pair;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.util.ParseUtil;
