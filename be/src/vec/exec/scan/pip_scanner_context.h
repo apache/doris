54cbbf3d32f951dc0200a77200b81a11a5a424b3
wangbo <506340561@qq.com>
2023-04-03 12:17:10 +0800

    as suggest

--- be/src/vec/exec/scan/pip_scanner_context.h
+++ be/src/vec/exec/scan/pip_scanner_context.h
@@ -56,16 +56,16 @@ public:
                 return Status::OK();
             }
         }
+        _current_used_bytes -= (*block)->allocated_bytes();
         {
-            std::unique_lock<std::mutex> l(_transfer_lock);
             if (has_enough_space_in_blocks_queue()) {
+                std::unique_lock<std::mutex> l(_transfer_lock);
                 auto submit_st = _scanner_scheduler->submit(this);
                 if (submit_st.ok()) {
                     _num_scheduling_ctx++;
                 }
             }
         }
-        _current_used_bytes -= (*block)->allocated_bytes();
         return Status::OK();
     }
 
