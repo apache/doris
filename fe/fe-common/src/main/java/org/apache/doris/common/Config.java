7c36bef6bc8943d83dedb97ec8e84124b9a2d103
huangzhaowei <huangzhaowei.416@bytedance.com>
2023-04-04 22:44:48 +0800

    [Feature-Wip](MySQL Load)Show load warning for my sql load (#18224)
    1. Support the show load warnings for mysql load to get the detail error message.
    2. Fix fillByteBufferAsync not mark the load as finished in same data load
    3. Fix drain data only in client mode.

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2098,6 +2098,8 @@ public class Config extends ConfigBase {
     @ConfField(mutable = false, masterOnly = false)
     public static String mysql_load_server_secure_path = "";
 
+    @ConfField(mutable = false, masterOnly = false)
+    public static int mysql_load_in_memory_record = 20;
 
     @ConfField(mutable = false, masterOnly = false)
     public static int mysql_load_thread_pool = 4;
