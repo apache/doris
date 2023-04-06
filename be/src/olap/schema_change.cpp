48caf35965458d55a261575ac4bd4d446e1ab204
YueW <45946325+Tanya-W@users.noreply.github.com>
2023-04-06 15:07:37 +0800

    [fix](alter inverted index) Temporary deal with add or drop inverted index by directly schema change (#18378)
    In the current implementation of the function of dynamically add and drop inverted index, there is a problem that the inverted index information of historical data is out of date after compaction on the base tablet.
    
    In the future, I will submit PRs to solve this problem. Now, temporarily add or drop inverted index by the directly schema change logic

--- be/src/olap/schema_change.cpp
+++ be/src/olap/schema_change.cpp
@@ -1765,7 +1765,9 @@ Status SchemaChangeHandler::_parse_request(const SchemaChangeParams& sc_params,
                 column_new.frac() != column_old.frac() ||
                 column_new.length() != column_old.length() ||
                 column_new.is_bf_column() != column_old.is_bf_column() ||
-                column_new.has_bitmap_index() != column_old.has_bitmap_index()) {
+                column_new.has_bitmap_index() != column_old.has_bitmap_index() ||
+                new_tablet_schema->has_inverted_index(column_new.unique_id()) !=
+                        base_tablet_schema->has_inverted_index(column_old.unique_id())) {
                 *sc_directly = true;
                 return Status::OK();
             }
