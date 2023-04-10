7212bc5b1d3da71896bf54b291499e1654e690fb
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2023-04-10 17:32:50 +0800

    [vectorized][cherry-pick] remove unless null key when no split in convert key range (#18506)

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -24,8 +24,7 @@
 namespace doris::vectorized {
 
 NewOlapScanner::NewOlapScanner(RuntimeState* state, NewOlapScanNode* parent, int64_t limit,
-                               bool aggregation, bool need_agg_finalize,
-                               const TPaloScanRange& scan_range, RuntimeProfile* profile)
+                               bool aggregation, bool need_agg_finalize, RuntimeProfile* profile)
         : VScanner(state, static_cast<VScanNode*>(parent), limit, profile),
           _aggregation(aggregation),
           _need_agg_finalize(need_agg_finalize),
