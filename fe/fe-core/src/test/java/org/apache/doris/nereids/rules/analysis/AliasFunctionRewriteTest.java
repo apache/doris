b09aaa31eac0247967b9d4316ce3dbe5bbd1e29c
sohardforaname <organic_chemistry@foxmail.com>
2023-04-10 11:52:04 +0800

    'fix-bugs'

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/analysis/AliasFunctionRewriteTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/analysis/AliasFunctionRewriteTest.java
@@ -67,7 +67,6 @@ public class AliasFunctionRewriteTest extends TestWithFeService implements MemoP
                 new VarcharLiteral("day")
         );
 
-
         PlanChecker.from(connectContext)
                 .analyze(sql)
                 .matches(
