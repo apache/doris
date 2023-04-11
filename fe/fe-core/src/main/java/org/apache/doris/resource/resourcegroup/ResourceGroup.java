53eeca062f87ace5a6a0013218aee6e9ab60c450
Zenglin Luo <luozenglin@gmail.com>
2023-04-11 23:26:29 +0800

    [improvement](resource-group) add test for resource group

--- fe/fe-core/src/main/java/org/apache/doris/resource/resourcegroup/ResourceGroup.java
+++ fe/fe-core/src/main/java/org/apache/doris/resource/resourcegroup/ResourceGroup.java
@@ -27,6 +27,7 @@ import org.apache.doris.thrift.TPipelineResourceGroup;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.gson.annotations.SerializedName;
 import org.apache.commons.lang.StringUtils;
 
@@ -65,9 +66,22 @@ public class ResourceGroup implements Writable {
         this.version = 0;
     }
 
-    public static ResourceGroup createResourceGroup(String name, Map<String, String> properties) throws DdlException {
+    private ResourceGroup(ResourceGroup other) {
+        this.id = other.id;
+        this.name = other.name;
+        this.properties = Maps.newHashMap();
+        this.properties.putAll(other.properties);
+        this.version = other.version;
+    }
+
+    public static ResourceGroup create(String name, Map<String, String> properties) throws DdlException {
         checkProperties(properties);
         return new ResourceGroup(Env.getCurrentEnv().getNextId(), name, properties);
+    }
+
+    @Override
+    public ResourceGroup clone() {
+        return new ResourceGroup(this);
     }
 
     private static void checkProperties(Map<String, String> properties) throws DdlException {
--- fe/fe-core/src/main/java/org/apache/doris/resource/resourcegroup/ResourceGroupMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/resource/resourcegroup/ResourceGroupMgr.java
@@ -114,7 +114,7 @@ public class ResourceGroupMgr implements Writable, GsonPostProcessable {
             }
             Map<String, String> properties = Maps.newHashMap();
             properties.put(ResourceGroup.CPU_SHARE, "10");
-            defaultResourceGroup = ResourceGroup.createResourceGroup(DEFAULT_GROUP_NAME, properties);
+            defaultResourceGroup = ResourceGroup.create(DEFAULT_GROUP_NAME, properties);
             nameToResourceGroup.put(DEFAULT_GROUP_NAME, defaultResourceGroup);
             idToResourceGroup.put(defaultResourceGroup.getId(), defaultResourceGroup);
             Env.getCurrentEnv().getEditLog().logCreateResourceGroup(defaultResourceGroup);
@@ -127,8 +127,11 @@ public class ResourceGroupMgr implements Writable, GsonPostProcessable {
     }
 
     public void createResourceGroup(CreateResourceGroupStmt stmt) throws DdlException {
-        ResourceGroup resourceGroup = ResourceGroup.createResourceGroup(stmt.getResourceGroupName(),
-                stmt.getProperties());
+        if (!Config.enable_resource_group) {
+            throw new DdlException("Please add `enable_resource_group=true` in fe.conf to enable resource group.");
+        }
+
+        ResourceGroup resourceGroup = ResourceGroup.create(stmt.getResourceGroupName(), stmt.getProperties());
         String resourceGroupNameName = resourceGroup.getName();
         writeLock();
         try {
@@ -158,6 +161,30 @@ public class ResourceGroupMgr implements Writable, GsonPostProcessable {
 
     public List<List<String>> getResourcesInfo() {
         return procNode.fetchResult().getRows();
+    }
+
+    // for ut
+    public Map<String, ResourceGroup> getNameToResourceGroup() {
+        Map<String, ResourceGroup> results = Maps.newHashMap();
+        readLock();
+        try {
+            this.nameToResourceGroup.forEach((key, value) -> results.put(key, value.clone()));
+        } finally {
+            readUnlock();
+        }
+        return results;
+    }
+
+    // for ut
+    public Map<Long, ResourceGroup> getIdToResourceGroup() {
+        Map<Long, ResourceGroup> results = Maps.newHashMap();
+        readLock();
+        try {
+            this.idToResourceGroup.forEach((key, value) -> results.put(key, value.clone()));
+        } finally {
+            readUnlock();
+        }
+        return results;
     }
 
     @Override
