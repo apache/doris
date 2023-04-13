741a7bfafcdbd11f35a3340c003bca6750996c12
ElvinWei <zhengte.wei@outlook.com>
2023-04-14 02:45:33 +0800

    Show histogram statistics, show specified column statistics

--- fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsRepository.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsRepository.java
@@ -56,6 +56,9 @@ public class StatisticsRepository {
     private static final String FULL_QUALIFIED_COLUMN_STATISTICS_NAME = FULL_QUALIFIED_DB_NAME + "."
             + "`" + StatisticConstants.STATISTIC_TBL_NAME + "`";
 
+    private static final String FULL_QUALIFIED_COLUMN_HISTOGRAM_NAME = FULL_QUALIFIED_DB_NAME + "."
+            + "`" + StatisticConstants.HISTOGRAM_TBL_NAME + "`";
+
     private static final String FULL_QUALIFIED_ANALYSIS_JOB_TABLE_NAME = FULL_QUALIFIED_DB_NAME + "."
             + "`" + StatisticConstants.ANALYSIS_JOB_TABLE + "`";
 
@@ -66,6 +69,10 @@ public class StatisticsRepository {
     private static final String FETCH_PARTITIONS_STATISTIC_TEMPLATE = "SELECT * FROM "
             + FULL_QUALIFIED_COLUMN_STATISTICS_NAME
             + " WHERE `id` IN (${idList})";
+
+    private static final String FETCH_COLUMN_HISTOGRAM_TEMPLATE = "SELECT * FROM "
+            + FULL_QUALIFIED_COLUMN_HISTOGRAM_NAME
+            + " WHERE `id` = '${id}'";
 
     private static final String PERSIST_ANALYSIS_TASK_SQL_TEMPLATE = "INSERT INTO "
             + FULL_QUALIFIED_ANALYSIS_JOB_TABLE_NAME + " VALUES(${jobId}, ${taskId}, '${catalogName}', '${dbName}',"
@@ -115,10 +122,19 @@ public class StatisticsRepository {
     }
 
     public static ResultRow queryColumnStatisticById(long tblId, String colName) {
+        return queryColumnStatisticById(tblId, colName, false);
+    }
+
+    public static ResultRow queryColumnHistogramById(long tblId, String colName) {
+        return queryColumnStatisticById(tblId, colName, true);
+    }
+
+    private static ResultRow queryColumnStatisticById(long tblId, String colName, boolean isHistogram) {
         Map<String, String> map = new HashMap<>();
         String id = constructId(tblId, -1, colName);
         map.put("id", id);
-        List<ResultRow> rows = StatisticsUtil.executeQuery(FETCH_COLUMN_STATISTIC_TEMPLATE, map);
+        List<ResultRow> rows = isHistogram ? StatisticsUtil.executeQuery(FETCH_COLUMN_HISTOGRAM_TEMPLATE, map) :
+                StatisticsUtil.executeQuery(FETCH_COLUMN_STATISTIC_TEMPLATE, map);
         int size = rows.size();
         if (size > 1) {
             throw new IllegalStateException(String.format("id: %s should be unique, but return more than one row", id));
@@ -135,6 +151,14 @@ public class StatisticsRepository {
         params.put("idList", sj.toString());
         List<ResultRow> rows = StatisticsUtil.executeQuery(FETCH_PARTITIONS_STATISTIC_TEMPLATE, params);
         return rows == null ? Collections.emptyList() : rows;
+    }
+
+    public static Histogram queryColumnHistogramByName(long tableId, String colName) {
+        ResultRow resultRow = queryColumnHistogramById(tableId, colName);
+        if (resultRow == null) {
+            return Histogram.UNKNOWN;
+        }
+        return Histogram.fromResultRow(resultRow);
     }
 
     private static String constructId(Object... params) {
