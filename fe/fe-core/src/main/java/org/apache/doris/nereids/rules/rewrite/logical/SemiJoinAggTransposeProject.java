8b1a29850016aba3fee191b304c38a0c7552db9e
jackwener <jakevingoo@gmail.com>
2023-04-13 19:17:45 +0800

    [feature](Nereids): pullup semiJoin through aggregate.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/SemiJoinAggTransposeProject.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/logical/SemiJoinAggTransposeProject.java
@@ -20,15 +20,10 @@ package org.apache.doris.nereids.rules.rewrite.logical;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
 import org.apache.doris.nereids.rules.rewrite.OneRewriteRuleFactory;
-import org.apache.doris.nereids.trees.expressions.Expression;
 import org.apache.doris.nereids.trees.expressions.Slot;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalAggregate;
 import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Pushdown semi-join through agg
@@ -38,27 +33,12 @@ public class SemiJoinAggTransposeProject extends OneRewriteRuleFactory {
     public Rule build() {
         return logicalJoin(logicalProject(logicalAggregate()), any())
                 .when(join -> join.getJoinType().isLeftSemiOrAntiJoin())
+                .when(join -> join.left().isAllSlots())
                 .when(join -> join.left().getProjects().stream().allMatch(n -> n instanceof Slot))
                 .then(join -> {
                     LogicalProject<LogicalAggregate<Plan>> project = join.left();
                     LogicalAggregate<Plan> aggregate = project.child();
-                    Set<Slot> canPushDownSlots = new HashSet<>();
-                    if (aggregate.hasRepeat()) {
-                        // When there is a repeat, the push-down condition is consistent with the repeat
-                        canPushDownSlots.addAll(aggregate.getSourceRepeat().get().getCommonGroupingSetExpressions());
-                    } else {
-                        for (Expression groupByExpression : aggregate.getGroupByExpressions()) {
-                            if (groupByExpression instanceof Slot) {
-                                canPushDownSlots.add((Slot) groupByExpression);
-                            }
-                        }
-                    }
-                    Set<Slot> leftOutputSet = join.left().getOutputSet();
-                    Set<Slot> conditionSlot = join.getConditionSlot()
-                            .stream()
-                            .filter(leftOutputSet::contains)
-                            .collect(Collectors.toSet());
-                    if (!canPushDownSlots.containsAll(conditionSlot)) {
+                    if (!SemiJoinAggTranspose.canTranspose(aggregate, join)) {
                         return null;
                     }
                     Plan newPlan = aggregate.withChildren(join.withChildren(aggregate.child(), join.right()));
