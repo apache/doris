b28310079ef2e8754f35114aed8382d7534476cd
sohardforaname <organic_chemistry@foxmail.com>
2023-04-14 15:22:02 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/InsertIntoSelectCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/InsertIntoSelectCommand.java
@@ -17,5 +17,51 @@
 
 package org.apache.doris.nereids.trees.plans.commands;
 
-public class InsertIntoSelectCommand {
+import org.apache.doris.nereids.trees.plans.PlanType;
+import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalPlan;
+import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
+
+import com.google.common.base.Preconditions;
+
+import java.util.List;
+
+/**
+ * insert into select command
+ */
+public class InsertIntoSelectCommand extends Command implements ForwardWithSync {
+    private final String tableName;
+    private final String labelName;
+    private final List<String> colNames;
+    private final LogicalPlan logicalQuery;
+    private final PhysicalPlan physicalQuery;
+
+    /**
+     * constructor
+     */
+    public InsertIntoSelectCommand(String tableName, String labelName, List<String> colNames,
+            LogicalPlan logicalQuery, PhysicalPlan physicalQuery) {
+        super(PlanType.INSERT_INTO_SELECT_COMMAND);
+        Preconditions.checkArgument(tableName != null, "tableName cannot be null in insert-into-select command");
+        Preconditions.checkArgument(logicalQuery != null, "logicalQuery cannot be null in insert-into-select command");
+        this.tableName = tableName;
+        this.labelName = labelName;
+        this.colNames = colNames;
+        this.logicalQuery = logicalQuery;
+        this.physicalQuery = physicalQuery;
+    }
+
+    public InsertIntoSelectCommand(String tableName, String labelName, List<String> colNames,
+            LogicalPlan logicalQuery) {
+        this(tableName, labelName, colNames, logicalQuery, null);
+    }
+
+    public InsertIntoSelectCommand withPhyscialQuery(PhysicalPlan physicalQuery) {
+        return new InsertIntoSelectCommand(tableName, labelName, colNames, logicalQuery, physicalQuery);
+    }
+
+    @Override
+    public <R, C> R accept(PlanVisitor<R, C> visitor, C context) {
+        return visitor.visitInsertIntoCommand(this, context);
+    }
 }
