2783f52f18877fac52e71594e2771dc25a07b389
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2023-04-14 20:20:21 +0800

    [Refactor](multi catalog)Remove redundant param context for FileQueryScanNode (#18636)
    Remove redundant param context for FileQueryScanNode.
    Remove duplicated code for QueryScanProviders.

--- fe/fe-core/src/main/java/org/apache/doris/planner/external/iceberg/IcebergScanProvider.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/external/iceberg/IcebergScanProvider.java
@@ -23,7 +23,6 @@ import org.apache.doris.common.DdlException;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.MetaNotFoundException;
 import org.apache.doris.common.UserException;
-import org.apache.doris.planner.external.FileQueryScanNode;
 import org.apache.doris.planner.external.IcebergSplitter;
 import org.apache.doris.planner.external.QueryScanProvider;
 import org.apache.doris.thrift.TFileAttributes;
@@ -143,11 +142,6 @@ public class IcebergScanProvider extends QueryScanProvider {
     @Override
     public Map<String, String> getLocationProperties() throws MetaNotFoundException, DdlException {
         return icebergSource.getCatalog().getProperties();
-    }
-
-    @Override
-    public FileQueryScanNode.ParamCreateContext createContext(Analyzer analyzer) throws UserException {
-        return icebergSource.createContext();
     }
 
     @Override
