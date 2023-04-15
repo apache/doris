e7cf45a2b6571ddb58c1b618c8705a55a3018c66
Yongqiang YANG <dataroaring@gmail.com>
2023-04-15 17:23:06 +0800

    fix isset

--- be/src/runtime/runtime_state.h
+++ be/src/runtime/runtime_state.h
@@ -90,7 +90,7 @@ public:
     int query_parallel_instance_num() const { return _query_options.parallel_instance; }
     int max_errors() const { return _query_options.max_errors; }
     int execution_timeout() const {
-        return _query_options.__iset.execution_timeout ? _query_options.execution_timeout :
+        return _query_options.__isset.execution_timeout ? _query_options.execution_timeout :
                                                          _query_options.query_timeout;
     }
     int max_io_buffers() const { return _query_options.max_io_buffers; }
