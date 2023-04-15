5ce056fa8b1651c4871241146d962dc9a90a11ff
Gabriel <gabrielleebuaa@gmail.com>
2023-04-15 17:49:09 +0800

    [minor](thrift) modify identifier to compatible with 1.2-lts (#18641)
    * [minor](thrift) modify identifier
    
    * udpate

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -181,23 +181,24 @@ struct TQueryOptions {
 
   54: optional bool enable_share_hash_table_for_broadcast_join
 
-  55: optional bool enable_pipeline_engine = false
-
-  56: optional i32 repeat_max_num = 0
-  57: optional bool check_overflow_for_decimal = false
-
-  58: optional i64 external_sort_bytes_threshold = 0
-
-  59: optional i32 partitioned_hash_agg_rows_threshold = 0
-
-  60: optional bool enable_file_cache = true
-  
-  61: optional i32 insert_timeout = 14400
-
-  62: optional i32 execution_timeout = 3600
+  55: optional bool check_overflow_for_decimal = false
 
   // For debug purpose, skip delete bitmap when reading data
-  63: optional bool skip_delete_bitmap = false
+  56: optional bool skip_delete_bitmap = false
+
+  57: optional bool enable_pipeline_engine = false
+
+  58: optional i32 repeat_max_num = 0
+
+  59: optional i64 external_sort_bytes_threshold = 0
+
+  60: optional i32 partitioned_hash_agg_rows_threshold = 0
+
+  61: optional bool enable_file_cache = true
+  
+  62: optional i32 insert_timeout = 14400
+
+  63: optional i32 execution_timeout = 3600
 
   64: optional bool dry_run_query = false
 
