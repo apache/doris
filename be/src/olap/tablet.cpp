b4cba583a48665c66ffaf63a19e6c033134014b9
morningman <morningman@163.com>
2023-04-17 20:52:27 +0800

    [fix](merge-on-write) the mow keyset check is quite slow when loading large data set, remove it from release version(#18750)
    cherry-pick #18750

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -2236,7 +2236,7 @@ Status Tablet::update_delete_bitmap(const RowsetSharedPtr& rowset, const TabletT
     RETURN_IF_ERROR(calc_delete_bitmap(rowset->rowset_id(), segments, &rowset_ids_to_add,
                                        delete_bitmap, cur_version - 1, true));
 
-    // Check the delete_bitmap correctness.
+    // Check the delete_bitmap correctness, now the check is only enabled in DEBUG env.
     if (load_info->num_keys != 0) {
         DeleteBitmap rs_bm(tablet_id());
         delete_bitmap->subset({rowset->rowset_id(), 0, 0},
