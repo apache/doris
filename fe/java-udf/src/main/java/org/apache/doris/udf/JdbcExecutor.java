c6a80f9e65da6437c7bf86bfc6b4265b91b07f08
zhangstar333 <2561612514@qq.com>
2023-04-18 14:16:21 +0800

    update

--- fe/java-udf/src/main/java/org/apache/doris/udf/JdbcExecutor.java
+++ fe/java-udf/src/main/java/org/apache/doris/udf/JdbcExecutor.java
@@ -54,7 +54,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
