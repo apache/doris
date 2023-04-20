ed2dd67fd9abee625e98b811d861d33a62798b69
starocean999 <40539150+starocean999@users.noreply.github.com>
2023-04-20 17:02:36 +0800

    [fix](nereids)disable SelectMaterializedIndexWithAggregate rule (#18380)
    * [fix](nereids)disable SelectMaterializedIndexWithAggregate rule
    
    * rebase code
    
    * disable related test cases
    
    * remove failed test cases for now

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/NereidsRewriter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/batch/NereidsRewriter.java
@@ -30,7 +30,6 @@ import org.apache.doris.nereids.rules.analysis.LogicalSubQueryAliasToLogicalProj
 import org.apache.doris.nereids.rules.expression.ExpressionNormalization;
 import org.apache.doris.nereids.rules.expression.ExpressionOptimization;
 import org.apache.doris.nereids.rules.expression.ExpressionRewrite;
-import org.apache.doris.nereids.rules.mv.SelectMaterializedIndexWithAggregate;
 import org.apache.doris.nereids.rules.mv.SelectMaterializedIndexWithoutAggregate;
 import org.apache.doris.nereids.rules.rewrite.logical.AdjustNullable;
 import org.apache.doris.nereids.rules.rewrite.logical.AggScalarSubQueryToWindowFunction;
@@ -250,7 +249,8 @@ public class NereidsRewriter extends BatchRewriteJob {
 
             topic("MV optimization",
                 topDown(
-                    new SelectMaterializedIndexWithAggregate(),
+                    // TODO: enable this rule after https://github.com/apache/doris/issues/18263 is fixed
+                    // new SelectMaterializedIndexWithAggregate(),
                     new SelectMaterializedIndexWithoutAggregate(),
                     new PushdownFilterThroughProject(),
                     new MergeProjects(),
