c42113d120b8e1ce1253b4a4f86b58275dea47be
sohardforaname <organic_chemistry@foxmail.com>
2023-04-20 16:21:43 +0800

    'fix-bugs'

---
+++ regression-test/suites/nereids_p0/insert_into_table/insert_into_table.groovy
@@ -0,0 +1,118 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("nereids_insert_into_table") {
+    sql 'drop table if exists src'
+    sql 'drop table if exists target'
+
+    sql '''
+        create table src (
+            k1 int,
+            k2 int,
+            k3 int,
+            k4 varchar(20),
+            k5 varchar(20)
+        ) engine=OLAP
+        DUPLICATE KEY(k1, k2)
+        DISTRIBUTED BY HASH(k5) BUCKETS 4
+        PROPERTIES (
+        "replication_num" = "1"
+        )
+    '''
+
+    sql '''
+        create table target (
+            k1 int,
+            k2 int,
+            k3 int,
+            k4 varchar(20),
+            k5 varchar(20)
+        ) engine=OLAP
+        DUPLICATE KEY(k1, k2)
+        PARTITION BY RANGE(k1)
+        (
+            PARTITION `p2` VALUES LESS THAN ("2"),
+            PARTITION `p5` VALUES LESS THAN ("5"),
+            PARTITION `p10` VALUES LESS THAN ("10"),
+            PARTITION `p20` VALUES LESS THAN ("20")
+        )
+        DISTRIBUTED BY HASH(k5) BUCKETS 4
+        PROPERTIES (
+        "replication_num" = "1"
+        )
+    '''
+
+    sql '''
+        insert into src values
+            (1, 2, 3, '4', '5'),
+            (4, 5, 6, '7', '8'),
+            (12, 13, 14, '15', '16'),
+            (null, null, null, null, null)
+    '''
+    sql 'set enable_nereids_planner=true'
+    sql 'set enable_fallback_to_original_planner=false'
+
+    sql 'insert into target select * from src'
+    test {
+        sql 'select * from target'
+        result([
+                [1, 2, 3, '4', '5'],
+                [4, 5, 6, '7', '8'],
+                [12, 13, 14, '15', '16'],
+                [null, null, null, null, null]
+        ])
+    }
+    sql 'truncate table target'
+
+    test {
+        sql '''insert into target select 'x', 'x', 'x', 1, 2'''
+        exception ''
+    }
+
+    test {
+        sql '''insert into target select 1, 2, 3'''
+        exception ''
+    }
+
+    sql 'insert into target partition (p10, p5) select * from src where k1 < 10 and k1 > 2'
+    test {
+        sql 'select * from target'
+        result([
+                [4, 5, 6, '7', '8']
+        ])
+    }
+
+    sql 'insert into target partition (p10, p5) (k1, k3, k5) select k1, k2, k4 from src where k1 < 10 and k1 > 2'
+    test {
+        sql 'select * from target'
+        result([
+                [4, 5, 6, '7', '8'],
+                [4, null, 5, null, '7']
+        ])
+    }
+
+    sql 'insert into target partition (p10, p5) with label `test` (k1, k3, k5) select k2, k4, k5 from src where k1 < 10 and k1 > 2'
+    test {
+        sql 'select * from target'
+        result([
+                [4, 5, 6, '7', '8'],
+                [4, null, 5, null, '7'],
+                [5, null, 7, null, '8']
+        ])
+    }
+    sql 'truncate table target'
+}
