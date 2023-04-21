782a4edcaa498651ba2c42064d773d463a14acdb
Mryange <2319153948@qq.com>
2023-04-21 12:04:45 +0800

    interval.month

--- be/src/vec/runtime/vdatetime_value.cpp
+++ be/src/vec/runtime/vdatetime_value.cpp
@@ -1583,7 +1583,8 @@ bool VecDateTimeValue::date_add_interval(const TimeInterval& interval) {
         if (_year < MIN_YEAR || _year > MAX_YEAR) {
             return false;
         }
-        _month = ((_month + 11 + sign * interval.month + 12) % 12) + 1;
+        // interval.month may be >= 12
+        _month = ((_month + 11 + sign * (interval.month % 12)  + 12) % 12) + 1;
         if (_day > s_days_in_month[_month]) {
             _day = s_days_in_month[_month];
             if (_month == 2 && doris::is_leap(_year)) {
