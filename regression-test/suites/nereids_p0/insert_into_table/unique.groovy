a5e8419c211a16d0351e6d51bed1cf5d098112f6
sohardforaname <organic_chemistry@foxmail.com>
2023-04-21 20:18:52 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/insert_into_table/unique.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/unique.groovy
@@ -15,5 +15,209 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("nereids_insert_into_table") {
+suite("nereids_insert_duplicate") {
+    sql 'use nereids_insert_into_table_test'
+
+    // DDL
+    sql '''
+        create table dup_t (
+            `id` int null,
+            `kbool` boolean null,
+            `ktint` tinyint(4) null,
+            `ksint` smallint(6) null,
+            `kint` int(11) null,
+            `kbint` bigint(20) null,
+            `klint` largeint(40) null,
+            `kfloat` float null,
+            `kdbl` double null,
+            `kdcml` decimal(9, 3) null,
+            `kchr` char(10) null,
+            `kvchr` varchar(10) null,
+            `kstr` string null,
+            `kdt` date null,
+            `kdtv2` datev2 null,
+            `kdtm` datetime null,
+            `kdtmv2` datetimev2(0) null,
+            `kdcml32v3` decimalv3(7, 3) null,
+            `kdcml64v3` decimalv3(10, 5) null,
+            `kdcml128v3` decimalv3(20, 8) null
+        ) engine=OLAP
+        duplicate key(id)
+        distributed by hash(id) buckets 4
+        partition by range(id) (
+            partition p1 values less than ("3"),
+            partition p2 values less than ("5"),
+            partition p3 values less than ("7"),
+            partition p4 values less than ("9")
+        )
+        properties (
+           "replication_num"="1"
+        )
+    '''
+
+    sql '''
+        create table dup_light_sc_t (
+            `id` int null,
+            `kbool` boolean null,
+            `ktint` tinyint(4) null,
+            `ksint` smallint(6) null,
+            `kint` int(11) null,
+            `kbint` bigint(20) null,
+            `klint` largeint(40) null,
+            `kfloat` float null,
+            `kdbl` double null,
+            `kdcml` decimal(9, 3) null,
+            `kchr` char(10) null,
+            `kvchr` varchar(10) null,
+            `kstr` string null,
+            `kdt` date null,
+            `kdtv2` datev2 null,
+            `kdtm` datetime null,
+            `kdtmv2` datetimev2(0) null,
+            `kdcml32v3` decimalv3(7, 3) null,
+            `kdcml64v3` decimalv3(10, 5) null,
+            `kdcml128v3` decimalv3(20, 8) null
+        ) engine=OLAP
+        duplicate key(id)
+        distributed by hash(id) buckets 4
+        partition by range(id) (
+            partition p1 values less than ("3"),
+            partition p2 values less than ("5"),
+            partition p3 values less than ("7"),
+            partition p4 values less than ("9")
+        )
+        properties (
+           "replication_num"="1"
+           "light_schema_change"="true"
+        )
+    '''
+
+    sql '''
+        create table dup_not_null_t (
+            `id` int not null,
+            `kbool` boolean not null,
+            `ktint` tinyint(4) not null,
+            `ksint` smallint(6) not null,
+            `kint` int(11) not null,
+            `kbint` bigint(20) not null,
+            `klint` largeint(40) not null,
+            `kfloat` float not null,
+            `kdbl` double not null,
+            `kdcml` decimal(9, 3) not null,
+            `kchr` char(10) not null,
+            `kvchr` varchar(10) not null,
+            `kstr` string not null,
+            `kdt` date not null,
+            `kdtv2` datev2 not null,
+            `kdtm` datetime not null,
+            `kdtmv2` datetimev2(0) not null,
+            `kdcml32v3` decimalv3(7, 3) not null,
+            `kdcml64v3` decimalv3(10, 5) not null,
+            `kdcml128v3` decimalv3(20, 8) not null
+        ) engine=OLAP
+        duplicate key(id)
+        distributed by hash(id) buckets 4
+        partition by range(id) (
+            partition p1 values less than ("3"),
+            partition p2 values less than ("5"),
+            partition p3 values less than ("7"),
+            partition p4 values less than ("9")
+        )
+        properties (
+           "replication_num"="1"
+        )
+    '''
+
+    sql '''
+        create table dup_light_sc_not_null_t (
+            `id` int not null,
+            `kbool` boolean not null,
+            `ktint` tinyint(4) not null,
+            `ksint` smallint(6) not null,
+            `kint` int(11) not null,
+            `kbint` bigint(20) not null,
+            `klint` largeint(40) not null,
+            `kfloat` float not null,
+            `kdbl` double not null,
+            `kdcml` decimal(9, 3) not null,
+            `kchr` char(10) not null,
+            `kvchr` varchar(10) not null,
+            `kstr` string not null,
+            `kdt` date not null,
+            `kdtv2` datev2 not null,
+            `kdtm` datetime not null,
+            `kdtmv2` datetimev2(0) not null,
+            `kdcml32v3` decimalv3(7, 3) not null,
+            `kdcml64v3` decimalv3(10, 5) not null,
+            `kdcml128v3` decimalv3(20, 8) not null
+        ) engine=OLAP
+        duplicate key(id)
+        distributed by hash(id) buckets 4
+        partition by range(id) (
+            partition p1 values less than ("3"),
+            partition p2 values less than ("5"),
+            partition p3 values less than ("7"),
+            partition p4 values less than ("9")
+        )
+        properties (
+           "replication_num"="1",
+           "light_schema_change"="true"
+        )
+    '''
+    // DDL end
+
+    sql 'enable_nereids_planner=true'
+    sql 'enable_fallback_to_original_planner=false'
+
+    sql '''insert into dup_t
+            select * except(kaint) from src order by id, kint'''
+    qt_11 'select * from dup_t'
+
+    sql '''insert into dup_t
+            with cte as (select * except(kaint) from src)
+            select * from cte order by id, kint'''
+    qt_12 'select * from dup_t'
+
+    sql '''insert into dup_t partition (p1, p2) with label label_dup
+            select * except(kaint) from src order by id, kint where id < 4'''
+    qt_13 'select * from dup_t'
+
+    sql '''insert into dup_light_sc_t
+            select * except(kaint) from src order by id, kint'''
+    qt_21 'select * from dup_light_sc_t'
+
+    sql '''insert into dup_light_sc_t
+            with cte as (select * except(kaint) from src)
+            select * from cte order by id, kint'''
+    qt_22 'select * from dup_light_sc_t'
+
+    sql '''insert into dup_light_sc_t partition (p1, p2) with label label_dup_light_sc
+            select * except(kaint) from src order by id, kint id < 4'''
+    qt_23 'select * from dup_light_sc_t'
+
+    sql '''insert into dup_not_null_t
+            select * except(kaint) from src order by id, kint where id is not null'''
+    qt_31 'select * from dup_not_null_t'
+
+    sql '''insert into dup_not_null_t
+            with cte as (select * except(kaint) from src)
+            select * from cte order by id, kint where id is not null'''
+    qt_32 'select * from dup_not_null_t'
+
+    sql '''insert into dup_not_null_t partition (p1, p2) with label label_dup_not_null
+            select * except(kaint) from src order by id, kint id < 4 where id is not null'''
+    qt_33 'select * from dup_not_null_t'
+
+    sql '''insert into dup_t_light_sc_not_null_t
+            select * except(kaint) from src order by id, kint where id is not null'''
+    qt_41 'select * from dup_light_sc_not_null_t'
+
+    sql '''insert into dup_t_light_sc_not_null_t
+            with cte as (select * except(kaint) from src)
+            select * from cte order by id, kint where id is not null'''
+    qt_42 'select * from dup_light_sc_not_null_t'
+
+    sql '''insert into dup_t_light_sc_not_null_t partition (p1, p2) with label label_dup_light_sc_not_null
+            select * except(kaint) from src order by id, kint where id < 4 where id is not null'''
+    qt_43 'select * from dup_light_sc_not_null_t'
 }
