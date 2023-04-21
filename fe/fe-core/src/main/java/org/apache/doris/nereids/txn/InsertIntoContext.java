c70d2e6e62c01335edc09f454c40bcdeef827a16
sohardforaname <organic_chemistry@foxmail.com>
2023-04-21 15:45:01 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/txn/InsertIntoContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/txn/InsertIntoContext.java
@@ -26,7 +26,6 @@ import java.util.List;
  */
 public class InsertIntoContext {
     private List<DataType> targetSchema = null;
-    private int keyNums;
 
     public void setTargetSchema(List<DataType> targetSchema) {
         this.targetSchema = targetSchema;
@@ -34,13 +33,5 @@ public class InsertIntoContext {
 
     public List<DataType> getTargetSchema() {
         return targetSchema;
-    }
-
-    public void setKeyNums(int keyNums) {
-        this.keyNums = keyNums;
-    }
-
-    public int getKeyNums() {
-        return keyNums;
     }
 }
