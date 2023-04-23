08181308e1721de7ccaccfaf1d812be753ca0262
sohardforaname <organic_chemistry@foxmail.com>
2023-04-23 14:51:11 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
@@ -21,8 +21,8 @@ suite("nereids_insert_array_type") {
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
 
-    sql 'enable_nereids_planner=false'
-    sql 'enable_fallback_to_original_planner=false'
+    sql 'set enable_nereids_planner=false'
+    sql 'set enable_fallback_to_original_planner=false'
 
     test {
         sql 'insert into arr_t select id, kaint from src'
