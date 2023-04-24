61835d631b94665039870be405f16aa35a0b393e
zzzzzzzs <1443539042@qq.com>
2023-04-24 10:25:01 +0800

    separately check tOperationStatus

--- fe/fe-core/src/main/java/org/apache/doris/common/util/BrokerUtil.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/util/BrokerUtil.java
@@ -564,7 +564,9 @@ public class BrokerUtil {
                         LOG.warn("Broker close writer failed. filePath={}, address={}", brokerFilePath, address, ex);
                     }
                 }
-                if (tOperationStatus.getStatusCode() != TBrokerOperationStatusCode.OK) {
+                if (tOperationStatus == null) {
+                    LOG.warn("Broker close reader failed. fd={}, address={}", fd.toString(), address);
+                } else if (tOperationStatus.getStatusCode() != TBrokerOperationStatusCode.OK) {
                     LOG.warn("Broker close writer failed. filePath={}, address={}, error={}", brokerFilePath,
                              address, tOperationStatus.getMessage());
                 } else {
