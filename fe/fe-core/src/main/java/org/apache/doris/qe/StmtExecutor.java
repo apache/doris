apache/doris

commit 14c98697f3765058f150cfb93c5ad236335e4fa4

[Fix](variables) fix session variable does not take effect immediately when set global variable in follower FE (#18609)

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -783,6 +783,12 @@ public class StmtExecutor implements ProfileWriter {
         masterOpExecutor = new MasterOpExecutor(originStmt, context, redirectStatus, isQuery());
         LOG.debug("need to transfer to Master. stmt: {}", context.getStmtId());
         masterOpExecutor.execute();
+        if (parsedStmt instanceof SetStmt) {
+            SetStmt setStmt = (SetStmt) parsedStmt;
+            setStmt.modifySetVarsForExecute();
+            SetExecutor executor = new SetExecutor(context, setStmt);
+            executor.execute();
+        }
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/qe/VariableMgr.java
@@ -241,7 +241,8 @@ public class VariableMgr {
     // Input:
     //      sessionVariable: the variable of current session
     //      setVar: variable information that needs to be set
-    public static void setVar(SessionVariable sessionVariable, SetVar setVar) throws DdlException {
+    public static void setVar(SessionVariable sessionVariable, SetVar setVar)
+            throws DdlException {
         VarContext ctx = ctxByVarName.get(setVar.getVariable());
         if (ctx == null) {
             ErrorReport.reportDdlException(ErrorCode.ERR_UNKNOWN_SYSTEM_VARIABLE, setVar.getVariable());

fe/fe-core/src/main/java/org/apache/doris/analysis/SetLdapPassVar.java
@@ -30,6 +30,7 @@ public class SetLdapPassVar extends SetVar {
 
     public SetLdapPassVar(PassVar passVar) {
         this.passVar = passVar;
+        this.varType = SetVarType.SET_LDAP_PASS_VAR;
     }
 
     public String getLdapPassword() {

fe/fe-core/src/main/java/org/apache/doris/analysis/SetNamesVar.java
@@ -31,6 +31,7 @@ public class SetNamesVar extends SetVar {
 
     public SetNamesVar(String charsetName) {
         this(charsetName, null);
+        this.varType = SetVarType.SET_NAMES_VAR;
     }
 
     public SetNamesVar(String charsetName, String collate) {

fe/fe-core/src/main/java/org/apache/doris/analysis/SetPassVar.java
@@ -36,6 +36,7 @@ public class SetPassVar extends SetVar {
     public SetPassVar(UserIdentity userIdent, PassVar passVar) {
         this.userIdent = userIdent;
         this.passVar = passVar;
+        this.varType = SetVarType.SET_PASS_VAR;
     }
 
     public UserIdentity getUserIdent() {

fe/fe-core/src/main/java/org/apache/doris/analysis/SetStmt.java
@@ -35,6 +35,17 @@ public class SetStmt extends StatementBase {
         return setVars;
     }
 
+    // remove setvar of non-set-session-var,
+    // change type global to session avoid to write in non-master node.
+    public void modifySetVarsForExecute() {
+        setVars.removeIf(setVar -> setVar.getVarType() != SetVar.SetVarType.SET_SESSION_VAR);
+        for (SetVar var : setVars) {
+            if (var.getType() == SetType.GLOBAL) {
+                var.setType(SetType.SESSION);
+            }
+        }
+    }
+
     @Override
     public boolean needAuditEncryption() {
         for (SetVar var : setVars) {

fe/fe-core/src/main/java/org/apache/doris/analysis/SetTransaction.java
@@ -19,6 +19,7 @@ package org.apache.doris.analysis;
 
 public class SetTransaction extends SetVar {
     public SetTransaction() {
+        this.varType = SetVarType.SET_TRANSACTION;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/analysis/SetUserPropertyVar.java
@@ -39,6 +39,7 @@ public class SetUserPropertyVar extends SetVar {
     public SetUserPropertyVar(String key, String value) {
         this.key = key;
         this.value = value;
+        this.varType = SetVarType.SET_USER_PROPERTY_VAR;
     }
 
     public String getPropertyKey() {

commit 228cc90e4e3ace81ae4c1ead5e9dd441ae432e26

[fix](session-var) ignore exception when setting global only var in non master FE (#18949)
Introduced from #18609.

When setting global variables from Non Master FE, there will be error like:

`Variable 'password_history' is a GLOBAL variable and should be set with SET GLOBAL`

Because when setting global variables from Non Master FE, Doris will do following step:

1. forward this SetStmt to Master FE to execute.
2. Change this SetStmt to "SESSION" level, and execute it again on this Non Master FE.

But for "GLOBAL only" variable, such ash "password_history", it doesn't allow to set on SESSION level.
So when doing step 2, "set password_history=xxx" without "GLOBAL" keywords will throw exception.
So in this case, we should just ignore this exception and return.

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -794,8 +794,9 @@ public class StmtExecutor implements ProfileWriter {
         if (parsedStmt instanceof SetStmt) {
             SetStmt setStmt = (SetStmt) parsedStmt;
             setStmt.modifySetVarsForExecute();
-            SetExecutor executor = new SetExecutor(context, setStmt);
-            executor.execute();
+            for (SetVar var : setStmt.getSetVars()) {
+                VariableMgr.setVarForNonMasterFE(context.getSessionVariable(), var);
+            }
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/qe/VariableMgr.java
@@ -251,6 +251,33 @@ public class VariableMgr {
     //      setVar: variable information that needs to be set
     public static void setVar(SessionVariable sessionVariable, SetVar setVar)
             throws DdlException {
+        VarContext varCtx = setVarPreCheck(setVar);
+        checkUpdate(setVar, varCtx.getFlag());
+        setVarInternal(sessionVariable, setVar, varCtx);
+    }
+
+    // The only difference between setVar and setVarForNonMasterFE
+    // is that setVarForNonMasterFE will just return if "checkUpdate" throw exception.
+    // This is because, when setting global variables from Non Master FE, Doris will do following step:
+    //      1. forward this SetStmt to Master FE to execute.
+    //      2. Change this SetStmt to "SESSION" level, and execute it again on this Non Master FE.
+    // But for "GLOBAL only" variable, such ash "password_history", it doesn't allow to set on SESSION level.
+    // So when doing step 2, "set password_history=xxx" without "GLOBAL" keywords will throw exception.
+    // So in this case, we should just ignore this exception and return.
+    public static void setVarForNonMasterFE(SessionVariable sessionVariable, SetVar setVar)
+            throws DdlException {
+        VarContext varCtx = setVarPreCheck(setVar);
+        try {
+            checkUpdate(setVar, varCtx.getFlag());
+        } catch (DdlException e) {
+            LOG.debug("no need to set var for non master fe: {}", setVar.getVariable(), e);
+            return;
+        }
+        setVarInternal(sessionVariable, setVar, varCtx);
+    }
+
+    @NotNull
+    private static VarContext setVarPreCheck(SetVar setVar) throws DdlException {
         String varName = setVar.getVariable();
         boolean hasExpPrefix = false;
         if (varName.startsWith(ExperimentalUtil.EXPERIMENTAL_PREFIX)) {
@@ -265,9 +292,11 @@ public class VariableMgr {
         if (hasExpPrefix && ctx.getField().getAnnotation(VarAttr.class).expType() == ExperimentalType.NONE) {
             ErrorReport.reportDdlException(ErrorCode.ERR_UNKNOWN_SYSTEM_VARIABLE, setVar.getVariable());
         }
-        // Check variable attribute and setVar
-        checkUpdate(setVar, ctx.getFlag());
+        return ctx;
+    }
 
+    private static void setVarInternal(SessionVariable sessionVariable, SetVar setVar, VarContext ctx)
+            throws DdlException {
         // To modify to default value.
         VarAttr attr = ctx.getField().getAnnotation(VarAttr.class);
         String value;