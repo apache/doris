50317841e93a5a17ffd8b3cf22d1113df26ee714
sohardforaname <organic_chemistry@foxmail.com>
2023-04-25 12:55:39 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -255,10 +255,15 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
             DataPartition dataPartition;
             if (distribute.getDistributionSpec().equals(PhysicalProperties.GATHER.getDistributionSpec())) {
                 dataPartition = DataPartition.UNPARTITIONED;
+            } else if (distribute.getDistributionSpec() instanceof DistributionSpecHash) {
+                DistributionSpecHash specHash = ((DistributionSpecHash) distribute.getDistributionSpec());
+                List<ExprId> ids = specHash.getOrderedShuffledColumns();
+                dataPartition = DataPartition.hashPartitioned(ids.stream()
+                        .map(context::findSlotRef)
+                        .collect(Collectors.toList()));
             } else {
                 throw new AnalysisException("Unsupported PhysicalDistribute in the root plan: " + distribute);
             }
-
             ExchangeNode exchangeNode = (ExchangeNode) rootFragment.getPlanRoot();
             PlanFragment currentFragment = new PlanFragment(context.nextFragmentId(), exchangeNode, dataPartition);
             rootFragment.setOutputPartition(dataPartition);
