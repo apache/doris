767b187892a80ff65da2baa060e43b4dba4cb755
starocean999 <40539150+starocean999@users.noreply.github.com>
2023-04-25 12:43:45 +0800

    [fix](nereids)enable fall back to old planner for single query (#19037)
    * pick pr 17994
    
    * enable fall back to old planner for single query

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/pre/EliminateLogicalSelectHint.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/pre/EliminateLogicalSelectHint.java
@@ -72,5 +72,17 @@ public class EliminateLogicalSelectHint extends PlanPreprocessor {
                 }
             }
         }
+
+        // if sv set enable_nereids_planner=true and hint set enable_nereids_planner=false, we should set
+        // enable_fallback_to_original_planner=true and revert it after executing.
+        // throw exception to fall back to original planner
+        if (!sessionVariable.isEnableNereidsPlanner()) {
+            try {
+                sessionVariable.enableFallbackToOriginalPlannerOnce();
+            } catch (Throwable t) {
+                throw new AnalysisException("failed to set fallback to original planner to true", t);
+            }
+            throw new AnalysisException("The nereids is disabled in this sql, fallback to original planner");
+        }
     }
 }
