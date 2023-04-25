6285547f01c62be7106583fe0112bd05d40470f6
ZYT <zhangyongtianaa@sina.com>
2023-04-25 12:22:03 +0800

    [Fix] Fix the extension mysql_to_doris bug (#19034)

--- extension/mysql_to_doris/bin/e_auto.sh
+++ extension/mysql_to_doris/bin/e_auto.sh
@@ -17,7 +17,7 @@
 #!/bin/bash
 source ../conf/doris.conf
 #Execute the create external table sql
-echo "source ../result/e_mysql_to_doris.sql;" |mysql -h$fe_host -P$fe_master_port -uroot -p$fe_password 2>error.log
+echo "source ../result/mysql_to_doris.sql;" |mysql -h$master_host -P$master_port -u$doris_user -p$doris_password 2>error.log
 
 #Circulation monitoring mysql or conf
 while (( 1 == 1 ))
@@ -27,26 +27,26 @@ do
 sleep 30
 
 #get new create table sql
-sh ./e_mysql_to_doris.sh ../result/new_e_mysql_to_doris.sql 2>error.log
+sh ./e_mysql_to_doris.sh ../result/new_mysql_to_doris.sql 2>error.log
 
 #get a md5 from old create table sql
-old=`md5sum ../result/e_mysql_to_doris.sql |awk -F ' ' '{print $1}'`
+old=`md5sum ../result/mysql_to_doris.sql |awk -F ' ' '{print $1}'`
 
 #get a md5 from new create table sql
-new=`md5sum ../result/new_e_mysql_to_doris.sql |awk -F ' ' '{print $1}'`
+new=`md5sum ../result/new_mysql_to_doris.sql |awk -F ' ' '{print $1}'`
 
         if [[ $old != $new ]];then
 #table charges to drop old table and create new table
                 for table in $(cat ../conf/doris_tables |grep -v '#' | awk -F '\n' '{print $1}')
                 do
-                       echo "drop table if exists ${table};" |mysql -h$fe_host -P$fe_master_port -uroot -p$fe_password
+                       echo "drop table if exists ${table};" |mysql -h$master_host -P$master_port -u$doris_user -p$doris_password
                 done
-                echo "source ../result/new_e_mysql_to_doris.sql;" |mysql -h$fe_host -P$fe_master_port -uroot -p$fe_password 2>error.log
+                echo "source ../result/new_mysql_to_doris.sql;" |mysql -h$master_host -P$master_port -u$doris_user -p$doris_password 2>error.log
 #delete old create table
-                rm -rf ../result/e_mysql_to_doris.sql
-#alter new table sql name 
-                mv ../result/new_e_mysql_to_doris.sql ../result/e_mysql_to_doris.sql
+                rm -rf ../result/mysql_to_doris.sql
+#alter new table sql name
+                mv ../result/new_mysql_to_doris.sql ../result/mysql_to_doris.sql
                 fi
-#if table no charge delete new create table 
-        rm -f ../result/new_e_mysql_to_doris.sql
+#if table no charge delete new create table
+        rm -f ../result/new_mysql_to_doris.sql
 done
