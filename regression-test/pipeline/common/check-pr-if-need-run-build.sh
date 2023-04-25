022e98c7482f614d11e60a73503370aedb2d628a
morningman <morningman@163.com>
2023-04-25 17:59:00 +0800

    2

--- regression-test/pipeline/common/check-pr-if-need-run-build.sh
+++ regression-test/pipeline/common/check-pr-if-need-run-build.sh
@@ -87,7 +87,7 @@ need_run_fe_ut() {
     for af in ${all_files}; do
         if [[ "${af}" == 'fe'* ]] ||
             [[ "${af}" == 'fe_plugins'* ]] ||
-            [[ "${af}" == 'fe_plugins'* ]] ||
+            [[ "${af}" == 'bin/start_fe.sh' ]] ||
             [[ "${af}" == 'docs/zh-CN/docs/sql-manual/'* ]] ||
             [[ "${af}" == 'docs/en/docs/sql-manual/'* ]] ||
             [[ "${af}" == 'bin/stop_fe.sh' ]] ||
