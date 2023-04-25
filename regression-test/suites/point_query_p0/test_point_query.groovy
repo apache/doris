c90b4d1f98ae81527e51845e7a1f764d84d2f957
eldenmoon <15605149486@163.com>
2023-04-25 15:31:40 +0800

    [Bug](mysql proto) fix binary proto with dynamic mode
    Dynamic mode used in array type when serialize it to mysql row buffer using dynamic mode, when combine binary row format with dynamic mode,something goes wrong, and lead to invalid binary row format.

--- regression-test/suites/point_query_p0/test_point_query.groovy
+++ regression-test/suites/point_query_p0/test_point_query.groovy
@@ -75,7 +75,9 @@ suite("test_point_query") {
                 `k7` float NULL COMMENT "",
                 `k8` datev2 NULL COMMENT "",
                 `k9` boolean NULL COMMENT "",
-                `k10` decimalv3(20, 3) NULL COMMENT ""
+                `k10` decimalv3(20, 3) NULL COMMENT "",
+                `k11` array<decimalv3(27, 9)> NULL COMMENT "",
+                `k12` array<text> NULL COMMENT ""
               ) ENGINE=OLAP
               UNIQUE KEY(`k1`, `k2`, `k3`)
               DISTRIBUTED BY HASH(`k1`, k2, k3) BUCKETS 1
@@ -87,16 +89,16 @@ suite("test_point_query") {
               "storage_format" = "V2"
               )
           """
-      sql """ INSERT INTO ${tableName} VALUES(1231, 119291.11, "ddd", "laooq", null, "2020-01-01 12:36:38", null, "1022-01-01 11:30:38", 0, 1.111112) """
-      sql """ INSERT INTO ${tableName} VALUES(1232, 12222.99121135, "xxx", "laooq", "2023-01-02", "2020-01-01 12:36:38", 522.762, "2022-01-01 11:30:38", 1, 212.111) """
-      sql """ INSERT INTO ${tableName} VALUES(1233, 1.392932911, "yyy", "laooq", "2024-01-02", "2020-01-01 12:36:38", 52.862, "3022-01-01 11:30:38", 0, 5973903488739435.668) """
-      sql """ INSERT INTO ${tableName} VALUES(1234, 12919291.129191137, "xxddd", "laooq", "2025-01-02", "2020-01-01 12:36:38", 552.872, "4022-01-01 11:30:38", 1, 5973903488739435.668) """
-      sql """ INSERT INTO ${tableName} VALUES(1235, 991129292901.11138, "dd", null, "2120-01-02", "2020-01-01 12:36:38", 652.692, "5022-01-01 11:30:38", 0, 90696620686827832.374) """
-      sql """ INSERT INTO ${tableName} VALUES(1236, 100320.11139, "laa    ddd", "laooq", "2220-01-02", "2020-01-01 12:36:38", 2.7692, "6022-01-01 11:30:38", 1, 23698.299) """
-      sql """ INSERT INTO ${tableName} VALUES(1237, 120939.11130, "a    ddd", "laooq", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 0, 90696620686827832.374) """
-      sql """ INSERT INTO ${tableName} VALUES(251, 120939.11130, "${generateString(251)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 251, "7022-01-01 11:30:38", 1, 90696620686827832.374) """
-      sql """ INSERT INTO ${tableName} VALUES(252, 120939.11130, "${generateString(252)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 252, "7022-01-01 11:30:38", 0, 90696620686827832.374) """
-      sql """ INSERT INTO ${tableName} VALUES(298, 120939.11130, "${generateString(298)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 298, "7022-01-01 11:30:38", 1, 90696620686827832.374) """
+      sql """ INSERT INTO ${tableName} VALUES(1231, 119291.11, "ddd", "laooq", null, "2020-01-01 12:36:38", null, "1022-01-01 11:30:38", null, 1.111112, [119181.1111, 819019.1191, null], null) """
+      sql """ INSERT INTO ${tableName} VALUES(1232, 12222.99121135, "xxx", "laooq", "2023-01-02", "2020-01-01 12:36:38", 522.762, "2022-01-01 11:30:38", 1, 212.111, null, null) """
+      sql """ INSERT INTO ${tableName} VALUES(1233, 1.392932911, "yyy", "laooq", "2024-01-02", "2020-01-01 12:36:38", 52.862, "3022-01-01 11:30:38", 0, 5973903488739435.668, [119181.1111, null, 819019.1191], ["dijiiixxx"]) """
+      sql """ INSERT INTO ${tableName} VALUES(1234, 12919291.129191137, "xxddd", "laooq", "2025-01-02", "2020-01-01 12:36:38", 552.872, "4022-01-01 11:30:38", 1, 5973903488739435.668, [1888109181.192111, 192129019.1191], ["1", "2", "3"]) """
+      sql """ INSERT INTO ${tableName} VALUES(1235, 991129292901.11138, "dd", null, "2120-01-02", "2020-01-01 12:36:38", 652.692, "5022-01-01 11:30:38", 0, 90696620686827832.374, [119181.1111], ["${generateString(251)}"]) """
+      sql """ INSERT INTO ${tableName} VALUES(1236, 100320.11139, "laa    ddd", "laooq", "2220-01-02", "2020-01-01 12:36:38", 2.7692, "6022-01-01 11:30:38", 1, 23698.299, [], ["${generateString(251)}"]) """
+      sql """ INSERT INTO ${tableName} VALUES(1237, 120939.11130, "a    ddd", "laooq", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 0, 90696620686827832.374, [1.1, 2.2, 3.3, 4.4, 5.5], []) """
+      sql """ INSERT INTO ${tableName} VALUES(251, 120939.11130, "${generateString(251)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 251, "7022-01-01 11:30:38", 1, 90696620686827832.374, [11111], []) """
+      sql """ INSERT INTO ${tableName} VALUES(252, 120939.11130, "${generateString(252)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 252, "7022-01-01 11:30:38", 0, 90696620686827832.374, [0], null) """
+      sql """ INSERT INTO ${tableName} VALUES(298, 120939.11130, "${generateString(298)}", "laooq", "2030-01-02", "2020-01-01 12:36:38", 298, "7022-01-01 11:30:38", 1, 90696620686827832.374, [], []) """
 
     def nprep_sql = {sql_str->
         def url_without_prep ="jdbc:mysql://" + sql_ip + ":" + sql_port + "/" + realDb
@@ -121,14 +123,21 @@ suite("test_point_query") {
       stmt.setString(3, "a    ddd")
       qe_point_select stmt
 
+      stmt.setInt(1, 1232)
+      stmt.setBigDecimal(2, new BigDecimal("12222.99121135"))
+      stmt.setString(3, 'xxx')
+      qe_point_select stmt
+
       stmt.setInt(1, 251)
       stmt.setBigDecimal(2, new BigDecimal("120939.11130"))
       stmt.setString(3, generateString(251))
       qe_point_select stmt
+
       stmt.setInt(1, 252)
       stmt.setBigDecimal(2, new BigDecimal("120939.11130"))
       stmt.setString(3, generateString(252))
       qe_point_select stmt
+
       stmt.setInt(1, 298)
       stmt.setBigDecimal(2, new BigDecimal("120939.11130"))
       stmt.setString(3, generateString(298))
@@ -153,13 +162,13 @@ suite("test_point_query") {
           ALTER table ${tableName} ADD COLUMN new_column0 INT default "0";
           """
       sleep(1);
-      nprep_sql """ INSERT INTO ${tableName} VALUES(1235, 120939.11130, "a    ddd", "laooq", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 123, 1, 1.1111111299) """
+      nprep_sql """ INSERT INTO ${tableName} VALUES(1235, 120939.11130, "a    ddd", "laooq", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 1, 1.1111299, [119291.19291], ["111", "222", "333"], 1) """
       stmt.setBigDecimal(1, new BigDecimal("120939.11130"))
       stmt.setString(2, "a    ddd")
       qe_point_select stmt
       qe_point_select stmt
       // invalidate cache
-      nprep_sql """ INSERT INTO ${tableName} VALUES(1235, 120939.11130, "a    ddd", "xxxxxx", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 123, 0, 1929111.1111) """
+      nprep_sql """ INSERT INTO ${tableName} VALUES(1235, 120939.11130, "a    ddd", "xxxxxx", "2030-01-02", "2020-01-01 12:36:38", 22.822, "7022-01-01 11:30:38", 0, 1929111.1111,[119291.19291], ["111", "222", "333"], 2) """
       qe_point_select stmt
       qe_point_select stmt
       qe_point_select stmt
