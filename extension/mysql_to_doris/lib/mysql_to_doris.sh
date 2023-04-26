eacf782bb862a69a17af5ccf0447c72c4521c8c7
ZYT <zhangyongtianaa@sina.com>
2023-04-26 13:41:08 +0800

    [Fix] Fix the extension mysql_to_doris bug (#19092)

--- extension/mysql_to_doris/lib/mysql_to_doris.sh
+++ extension/mysql_to_doris/lib/mysql_to_doris.sh
@@ -23,11 +23,19 @@ sed -i 's/CHARACTER SET utf8mb4 COLLATE utf8mb4_bin//g' $path
 sed -i 's/CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci//g'  $path
 sed -i 's/CHARACTER SET utf8mb4 COLLATE utf8_general_ci//g' $path
 sed -i 's/CHARACTER SET utf8 COLLATE utf8_general_ci//g' $path
+sed -i "s/DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP([0-9]*)//g" $path
+sed -i "s/DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP//g" $path
+sed -i 's/DEFAULT CURRENT_TIMESTAMP([0-9]*) ON UPDATE CURRENT_TIMESTAMP([0-9]*)//g' $path
 sed -i 's/DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP//g' $path
-sed -i 's/DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP//g' $path
 sed -i 's/CHARACTER SET utf8mb4 COLLATE utf8mb4_bin//g' $path
+sed -i 's/DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP([0-9]*)//g' $path
 sed -i 's/DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP//g' $path
+sed -i 's/DEFAULT CURRENT_TIMESTAMP([0-9]*)//g' $path
 sed -i 's/DEFAULT CURRENT_TIMESTAMP//g' $path
+sed -i "s/DEFAULT '0000-00-00 00:00:00'//g" $path
+sed -i "s/DEFAULT '0000-00-00'//g" $path
+sed -i "s/float/decimal/g" $path
+sed -i "s/COLLATE utf8_unicode_ci//g" $path
 sed -i 's/CHARACTER SET utf8mb4//g' $path
 sed -i 's/CHARACTER SET utf8//g' $path
 sed -i 's/COLLATE utf8mb4_general_ci//g' $path
