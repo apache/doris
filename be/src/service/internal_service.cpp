53ced269ef4228b1800c145ff14e80a0ff79482b
TsukiokaKogane <cby141994@gmail.com>
2023-04-27 15:56:07 +0800

    remove space

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -1211,8 +1211,8 @@ void PInternalServiceImpl::request_slave_tablet_pull_rowset(
             }
 
             std::stringstream ss;
-            ss << "http://" << get_host_port(host, http_port) 
-               << "/api/_tablet/_download?token=" << token << "&file=" << rowset_path << "/" 
+            ss << "http://" << get_host_port(host, http_port)
+               << "/api/_tablet/_download?token=" << token << "&file=" << rowset_path << "/"
                << remote_rowset_id << "_" << segment.first << ".dat";
             std::string remote_file_url = ss.str();
             ss.str("");
