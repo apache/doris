6b5d86fb37923943ebc1646c5a004f8b8fc51897
Gabriel <gabrielleebuaa@gmail.com>
2023-04-28 08:46:42 +0800

    [profile](rf) refactor profile of runtime filters (#19134)
    * [profile](rf) refactor profile of runtime filters
    
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- be/src/vec/exec/scan/vscan_node.cpp
+++ be/src/vec/exec/scan/vscan_node.cpp
@@ -119,9 +119,12 @@ Status VScanNode::prepare(RuntimeState* state) {
     RETURN_IF_ERROR(ExecNode::prepare(state));
 
     // init profile for runtime filter
+    std::stringstream ss;
     for (auto& rf_ctx : _runtime_filter_ctxs) {
         rf_ctx.runtime_filter->init_profile(_runtime_profile.get());
+        ss << rf_ctx.runtime_filter->get_name() << ", ";
     }
+    _runtime_profile->add_info_string("RuntimeFilters: ", ss.str());
 
     if (_is_pipeline_scan) {
         if (_shared_scan_opt) {
