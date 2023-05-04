d0c22b893f25124b9fbd7dee524ca41fa5913144
Tang Siyang <tangsiyang2001@foxmail.com>
2023-05-04 18:23:29 +0800

    Revert "include canceled job when checking label used"
    This reverts commit fc0c32131b3f58f3199a577348434ff926d10f08.

--- fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
@@ -666,7 +666,8 @@ public class LoadManager implements Writable {
             Map<String, List<LoadJob>> labelToLoadJobs = dbIdToLabelToLoadJobs.get(dbId);
             if (labelToLoadJobs.containsKey(label)) {
                 List<LoadJob> labelLoadJobs = labelToLoadJobs.get(label);
-                Optional<LoadJob> loadJobOptional = labelLoadJobs.stream().findFirst();
+                Optional<LoadJob> loadJobOptional = labelLoadJobs.stream()
+                        .filter(entity -> entity.getState() != JobState.CANCELLED).findFirst();
                 if (loadJobOptional.isPresent()) {
                     LOG.warn("Failed to add load job when label {} has been used.", label);
                     throw new LabelAlreadyUsedException(label);
