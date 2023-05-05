e4c9e8e5963f6f9f7fb4d365941d6af5e333235b
yiguolei <yiguolei@gmail.com>
2023-05-05 16:38:50 +0800

    f

--- be/src/vec/exprs/vexpr_context.h
+++ be/src/vec/exprs/vexpr_context.h
@@ -69,7 +69,7 @@ public:
     [[nodiscard]] static Status filter_block(VExprContext* vexpr_ctx, Block* block,
                                              int column_to_keep);
 
-    static [[nodiscard]] Status get_output_block_after_execute_exprs(
+    [[nodiscard]] static Status get_output_block_after_execute_exprs(
             const std::vector<vectorized::VExprContext*>&, const Block&, Block*);
 
     int get_last_result_column_id() const {
