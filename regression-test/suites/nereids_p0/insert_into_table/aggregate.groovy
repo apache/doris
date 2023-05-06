9dbe7855a13f84d32e90e5636d8380a3bca7d94e
sohardforaname <organic_chemistry@foxmail.com>
2023-05-06 20:31:56 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/insert_into_table/aggregate.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/aggregate.groovy
@@ -22,55 +22,55 @@ suite("nereids_insert_aggregate") {
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
 
-    sql '''insert into agg_t
+    sql '''insert into nereids_insert_into_table_test.agg_t
             select * except(kaint) from src'''
     qt_11 'select * from agg_t order by id, kint'
 
-    sql '''insert into agg_t with label label_agg_cte
+    sql '''insert into nereids_insert_into_table_test.agg_t with label label_agg_cte
             with cte as (select * except(kaint) from src)
             select * from cte'''
     qt_12 'select * from agg_t order by id, kint'
 
-    sql '''insert into agg_t partition (p1, p2) with label label_agg
+    sql '''insert into nereids_insert_into_table_test.agg_t partition (p1, p2) with label label_agg
             select * except(kaint) from src where id < 4'''
     qt_13 'select * from agg_t order by id, kint'
 
-    sql '''insert into agg_light_sc_t
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_t
             select * except(kaint) from src'''
     qt_21 'select * from agg_light_sc_t order by id, kint'
 
-    sql '''insert into agg_light_sc_t with label label_agg_light_sc_cte
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_t with label label_agg_light_sc_cte
             with cte as (select * except(kaint) from src)
             select * from cte'''
     qt_22 'select * from agg_light_sc_t order by id, kint'
 
-    sql '''insert into agg_light_sc_t partition (p1, p2) with label label_agg_light_sc
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_t partition (p1, p2) with label label_agg_light_sc
             select * except(kaint) from src where id < 4'''
     qt_23 'select * from agg_light_sc_t order by id, kint'
 
-    sql '''insert into agg_not_null_t
+    sql '''insert into nereids_insert_into_table_test.agg_not_null_t
             select * except(kaint) from src where id is not null'''
     qt_31 'select * from agg_not_null_t order by id, kint'
 
-    sql '''insert into agg_not_null_t with label label_agg_not_null_cte
+    sql '''insert into nereids_insert_into_table_test.agg_not_null_t with label label_agg_not_null_cte
             with cte as (select * except(kaint) from src)
             select * from cte where id is not null'''
     qt_32 'select * from agg_not_null_t order by id, kint'
 
-    sql '''insert into agg_not_null_t partition (p1, p2) with label label_agg_not_null
+    sql '''insert into nereids_insert_into_table_test.agg_not_null_t partition (p1, p2) with label label_agg_not_null
             select * except(kaint) from src where id < 4 and id is not null'''
     qt_33 'select * from agg_not_null_t order by id, kint'
 
-    sql '''insert into agg_light_sc_not_null_t
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_not_null_t
             select * except(kaint) from src where id is not null'''
     qt_41 'select * from agg_light_sc_not_null_t order by id, kint'
 
-    sql '''insert into agg_light_sc_not_null_t with label label_agg_light_sc_not_null_cte
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_not_null_t with label label_agg_light_sc_not_null_cte
             with cte as (select * except(kaint) from src)
             select * from cte where id is not null'''
     qt_42 'select * from agg_light_sc_not_null_t order by id, kint'
 
-    sql '''insert into agg_light_sc_not_null_t partition (p1, p2) with label label_agg_light_sc_not_null
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_not_null_t partition (p1, p2) with label label_agg_light_sc_not_null
             select * except(kaint) from src where id < 4 and id is not null'''
     qt_43 'select * from agg_light_sc_not_null_t order by id, kint'
 
@@ -78,11 +78,11 @@ suite("nereids_insert_aggregate") {
     sql 'alter table agg_light_sc_t rename column ktint ktinyint'
     sql 'alter table agg_light_sc_not_null_t rename column ktint ktinyint'
 
-    sql '''insert into agg_light_sc_t
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_t
             select * except(kaint) from src'''
     qt_lsc1 'select * from agg_light_sc_t order by id, kint'
 
-    sql '''insert into agg_light_sc_not_null_t
+    sql '''insert into nereids_insert_into_table_test.agg_light_sc_not_null_t
             select * except(kaint) from src where id is not null'''
     qt_lsc2 'select * from agg_light_sc_not_null_t order by id, kint'
 }
