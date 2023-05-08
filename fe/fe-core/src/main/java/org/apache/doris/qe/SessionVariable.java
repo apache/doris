49e2d9d8b888e7f839d58eb2c493ce932881db34
morningman <morningman@163.com>
2023-05-08 11:19:10 +0800

    [branch-1.2] set enable_function_pushdown to false by default
    The expr close logic on branch 1.2 has some bugs that is not fixed.
    So disable this feature on 1.2 branch.
    This issue has been fixed on master

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -600,7 +600,7 @@ public class SessionVariable implements Serializable, Writable {
     public boolean enableSingleReplicaInsert = false;
 
     @VariableMgr.VarAttr(name = ENABLE_FUNCTION_PUSHDOWN)
-    public boolean enableFunctionPushdown;
+    public boolean enableFunctionPushdown = false;
 
     @VariableMgr.VarAttr(name = ENABLE_LOCAL_EXCHANGE, fuzzy = true)
     public boolean enableLocalExchange = true;
