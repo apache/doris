15bd065e9f68ee0f260eab5db7a4d2db41eae97d
BePPPower <ftw2139@163.com>
2023-05-09 17:03:04 +0800

    fix 3

--- regression-test/suites/export_p0/test_export_data_types.groovy
+++ regression-test/suites/export_p0/test_export_data_types.groovy
@@ -124,6 +124,8 @@ suite("test_export_data_types", "p0") {
             ${sb.toString()}
         """
    
+    def insert_res = sql "show last insert;"
+    logger.info("insert result: " + insert_res.toString())
     qt_select_export1 """ SELECT * FROM ${table_export_name} t ORDER BY user_id; """
 
     def check_path_exists = { dir_path ->
