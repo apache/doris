db3704e45f437382df737392db4ecfb271228a85
huangqixiang.871 <huangqixiang.871@bytedance.com>
2023-05-09 20:49:08 +0800

    fix fe

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CreateTableStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CreateTableStmt.java
@@ -438,8 +438,8 @@ public class CreateTableStmt extends DdlStmt {
             columnDef.analyze(engineName.equals("olap"));
 
             if (columnDef.getType().isArrayType() && engineName.equals("olap")) {
-                if (keysDesc.getKeysType() == KeysType.UNIQUE_KEYS) {
-                    throw new AnalysisException("Array column can't be used in unique table");
+                if (keysDesc.getKeysType() == KeysType.AGG_KEYS) {
+                    throw new AnalysisException("Array column can't be used in aggregate table");
                 }
                 if (columnDef.getAggregateType() != null && columnDef.getAggregateType() != AggregateType.NONE
                         && columnDef.getAggregateType() != AggregateType.REPLACE) {
