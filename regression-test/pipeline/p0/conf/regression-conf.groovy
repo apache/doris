d2f6c29f8aeff12cd0254c9b002a738465ae244f
yiguolei <676222867@qq.com>
2023-05-10 12:06:02 +0800

    [bugfix](regression-test) add some window function test (#19460)
    Only 2000 union will cause BE use a lot of memory, so that I enable other test in this PR only disable 2000 union case.
    
    
    
    ---------
    
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -48,7 +48,7 @@ testDirectories = ""
 // this groups will not be executed
 excludeGroups = ""
 // this suites will not be executed
-excludeSuites = "test_broker_load,test_window_function"
+excludeSuites = "test_broker_load"
 // this directories will not be executed
 excludeDirectories = ""
 
