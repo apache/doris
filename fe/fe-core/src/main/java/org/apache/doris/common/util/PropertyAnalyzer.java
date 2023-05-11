6e2764cbba7a93e5288958a4a006d4d664319fae
lsy3993 <xm610487344@hotmail.com>
2023-05-11 17:17:41 +0800

    add new check for catalog property

--- fe/fe-core/src/main/java/org/apache/doris/common/util/PropertyAnalyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/util/PropertyAnalyzer.java
@@ -127,6 +127,8 @@ public class PropertyAnalyzer {
 
     private static final Logger LOG = LogManager.getLogger(PropertyAnalyzer.class);
     private static final String COMMA_SEPARATOR = ",";
+    private static final String BLANK = " ";
+    private static final String TABS = "\t";
     private static final double MAX_FPP = 0.05;
     private static final double MIN_FPP = 0.0001;
 
@@ -794,6 +796,14 @@ public class PropertyAnalyzer {
      */
     public static void checkCatalogProperties(Map<String, String> properties, boolean isAlter)
             throws AnalysisException {
+        // firstly, check if the property's key contains empty character
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            String key = entry.getKey();
+            if (key.contains(BLANK) || key.contains(TABS)) {
+                throw new AnalysisException("property key contains ' ' or '\t' character");
+            }
+        }
+
         // validate the properties of es catalog
         if ("es".equalsIgnoreCase(properties.get("type"))) {
             try {
