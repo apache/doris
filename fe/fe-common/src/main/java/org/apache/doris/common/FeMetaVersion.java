ed685217f48679f2cf20c7c82e76a3b559ec4be4
Mingyu Chen <morningman@163.com>
2023-05-12 15:32:50 +0800

    [Fix](multi-catalog) Make BE selection policy works fine when enable prefer_compute_node_for_external_table (#19346)

--- fe/fe-common/src/main/java/org/apache/doris/common/FeMetaVersion.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/FeMetaVersion.java
@@ -58,11 +58,13 @@ public final class FeMetaVersion {
     public static final int VERSION_118 = 118;
     // TablePropertyInfo add db id
     public static final int VERSION_119 = 119;
-
+    // For export job
     public static final int VERSION_120 = 120;
+    // For BackendHbResponse node type
+    public static final int VERSION_121 = 121;
 
     // note: when increment meta version, should assign the latest version to VERSION_CURRENT
-    public static final int VERSION_CURRENT = VERSION_120;
+    public static final int VERSION_CURRENT = VERSION_121;
 
     // all logs meta version should >= the minimum version, so that we could remove many if clause, for example
     // if (FE_METAVERSION < VERSION_94) ...
