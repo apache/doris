38ca42a92bff2ed30ea64627f73b42636666d92c
nextdreamblue <zxw520blue1@163.com>
2023-05-16 14:24:44 +0800

    fix code 600
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- fe/fe-core/src/main/java/org/apache/doris/alter/MaterializedViewHandler.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/MaterializedViewHandler.java
@@ -348,8 +348,8 @@ public class MaterializedViewHandler extends AlterHandler {
         boolean isKeysRequired = !(mvKeysType == KeysType.DUP_KEYS);
         short mvShortKeyColumnCount = Env.calcShortKeyColumnCount(mvColumns, properties, isKeysRequired);
         if (mvShortKeyColumnCount <= 0 && olapTable.isDuplicateWithoutKey()) {
-            throw new DdlException("Not support create duplicate materialized view without order " +
-                            "by based on a duplicate table without keys");
+            throw new DdlException("Not support create duplicate materialized view without order "
+                            + "by based on a duplicate table without keys");
         }
         // get timeout
         long timeoutMs = PropertyAnalyzer.analyzeTimeout(properties, Config.alter_table_timeout_second) * 1000;
