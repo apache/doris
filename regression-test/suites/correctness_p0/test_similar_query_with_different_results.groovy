a44493c5df893b2bca0eed91f8c09fbde87e51b4
ZI-MA <chime316@qq.com>
2023-05-16 11:55:14 +0800

    add the ORDER BY

--- regression-test/suites/correctness_p0/test_similar_query_with_different_results.groovy
+++ regression-test/suites/correctness_p0/test_similar_query_with_different_results.groovy
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 suite("test_similar_query_with_different_results") {
     def tableName = "t0"
 
@@ -13,7 +30,7 @@ suite("test_similar_query_with_different_results") {
     sql """ INSERT INTO ${tableName} (c0) VALUES 
             (DEFAULT),('false'),('true'),(DEFAULT),('true'),(DEFAULT),(DEFAULT),('false')
         """
-    qt_select1 """ SELECT c0 FROM ${tableName} """
+    qt_select1 """ SELECT c0 FROM ${tableName} ORDER BY c0 """
     qt_select2 """ SELECT c0 FROM ${tableName} WHERE c0 """
     qt_select3 """ SELECT c0 FROM ${tableName} WHERE (NOT c0) """
     qt_select4 """ SELECT c0 FROM ${tableName} WHERE ((c0) IS NULL) """
