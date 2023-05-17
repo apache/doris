a51cda35f6dc486f52bafd9d0bef6c9316180eb2
yiguolei <yiguolei@gmail.com>
2023-05-17 18:52:31 +0800

    f

--- regression-test/suites/correctness/test_set_session_default_val.groovy
+++ regression-test/suites/correctness/test_set_session_default_val.groovy
@@ -34,7 +34,6 @@ suite("test_set_session_default_val") {
     sql """set max_execution_time=3000;"""
     def query_timeout2 = sql """show variables where variable_name = 'query_timeout';"""
     def max_execute_timeout2 = sql """show variables where variable_name = 'max_execution_time';"""
-    assertTrue(query_timeout2[0][1] == "3")
-    assertTrue(max_execute_timeout2[0][1] != default_max_execute_timeout[0][1])
-    assertTrue(max_execute_timeout2[0][1] == 3000)
+    assertEquals(query_timeout2[0][1], "3")
+    assertEquals(max_execute_timeout2[0][1], "3000")
 }
