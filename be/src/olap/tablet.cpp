56c0b94f9387b25785e49adde24941ed514fc7ca
liaoxin <liaoxinbit@126.com>
2023-05-17 16:43:35 +0800

    [enhancement](merge-on-write) Avoiding unnecessary primary key index traversal

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -3004,8 +3004,10 @@ Status Tablet::update_delete_bitmap(const RowsetSharedPtr& rowset, const TabletT
         delete_bitmap->remove({to_del, 0, 0}, {to_del, UINT32_MAX, INT64_MAX});
     }
 
-    RETURN_IF_ERROR(calc_delete_bitmap(rowset, segments, &rowset_ids_to_add, delete_bitmap,
-                                       cur_version - 1, false, rowset_writer));
+    if (!rowset_ids_to_add.empty()) {
+        RETURN_IF_ERROR(calc_delete_bitmap(rowset, segments, &rowset_ids_to_add, delete_bitmap,
+                                           cur_version - 1, false, rowset_writer));
+    }
 
     // update version without write lock, compaction and publish_txn
     // will update delete bitmap, handle compaction with _rowset_update_lock
