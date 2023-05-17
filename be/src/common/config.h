8fd1eb0d1ebe85dae0424acdf34f936bfa2a6af0
Gabriel <gabrielleebuaa@gmail.com>
2023-05-17 09:58:26 +0800

    [minor](hash table) parameterize hash table (#19653)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -141,6 +141,11 @@ DECLARE_mInt64(mmap_threshold); // bytes
 // Increase can reduce the number of hash table resize, but may waste more memory.
 DECLARE_mInt32(hash_table_double_grow_degree);
 
+// The max fill rate for hash table
+DECLARE_mInt32(max_fill_rate);
+
+DECLARE_mInt32(double_resize_threshold);
+
 // Expand the hash table before inserting data, the maximum expansion size.
 // There are fewer duplicate keys, reducing the number of resize hash tables
 // There are many duplicate keys, and the hash table filled bucket is far less than the hash table build bucket.
