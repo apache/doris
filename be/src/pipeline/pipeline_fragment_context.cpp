ef0657c072cac36230d2853e2e822ec5c2e2b6b1
HappenLee <happenlee@hotmail.com>
2023-05-18 18:57:25 +0800

    [Bug](pipeline) RegressionTest failed release resouce cause DCHECK failed (#19783)
    RegressionTest failed release resouce cause DCHECK failed

--- be/src/pipeline/pipeline_fragment_context.cpp
+++ be/src/pipeline/pipeline_fragment_context.cpp
@@ -309,8 +309,8 @@ Status PipelineFragmentContext::prepare(const doris::TPipelineFragmentParams& re
     _root_pipeline = fragment_context->add_pipeline();
     RETURN_IF_ERROR(_build_pipelines(_root_plan, _root_pipeline));
     if (_sink) {
-        RETURN_IF_ERROR(
-                _create_sink(request.local_params[idx].sender_id, request.fragment.output_sink));
+        RETURN_IF_ERROR(_create_sink(request.local_params[idx].sender_id,
+                                     request.fragment.output_sink, _runtime_state.get()));
     }
     RETURN_IF_ERROR(_build_pipeline_tasks(request));
     if (_sink) {
@@ -685,7 +685,12 @@ Status PipelineFragmentContext::submit() {
 
 void PipelineFragmentContext::close_if_prepare_failed() {
     if (_tasks.empty()) {
-        _root_plan->close(_runtime_state.get());
+        if (_root_plan) {
+            _root_plan->close(_runtime_state.get());
+        }
+        if (_sink) {
+            _sink->close(_runtime_state.get(), Status::RuntimeError("prepare failed"));
+        }
     }
     for (auto& task : _tasks) {
         DCHECK(!task->is_pending_finish());
@@ -695,7 +700,8 @@ void PipelineFragmentContext::close_if_prepare_failed() {
 }
 
 // construct sink operator
-Status PipelineFragmentContext::_create_sink(int sender_id, const TDataSink& thrift_sink) {
+Status PipelineFragmentContext::_create_sink(int sender_id, const TDataSink& thrift_sink,
+                                             RuntimeState* state) {
     OperatorBuilderPtr sink_;
     switch (thrift_sink.type) {
     case TDataSinkType::DATA_STREAM_SINK: {
@@ -754,6 +760,12 @@ Status PipelineFragmentContext::_create_sink(int sender_id, const TDataSink& thr
             OperatorBuilderPtr sink_op_builder = std::make_shared<ExchangeSinkOperatorBuilder>(
                     next_operator_builder_id(), _multi_cast_stream_sink_senders[i].get(), this, i);
             new_pipeline->set_sink(sink_op_builder);
+
+            // 4. init and prepare the data_stream_sender of diff exchange
+            TDataSink t;
+            t.stream_sink = thrift_sink.multi_cast_stream_sink.sinks[i];
+            RETURN_IF_ERROR(_multi_cast_stream_sink_senders[i]->init(t));
+            RETURN_IF_ERROR(_multi_cast_stream_sink_senders[i]->prepare(state));
         }
 
         return Status::OK();
