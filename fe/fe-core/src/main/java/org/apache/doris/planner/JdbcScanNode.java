a5497c0398239d47b52f6c84e6ee9396a4b67e14
yongkang.zhong <zhongykk@qq.com>
2023-05-18 15:14:59 +0800

    checkstyle

--- fe/fe-core/src/main/java/org/apache/doris/planner/JdbcScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/JdbcScanNode.java
@@ -162,7 +162,8 @@ public class JdbcScanNode extends ScanNode {
         StringBuilder sql = new StringBuilder("SELECT ");
 
         // Oracle use the where clause to do top n
-        if (shouldPushDownLimit() && (jdbcType == TOdbcTableType.ORACLE || jdbcType == TOdbcTableType.OCEANBASE_ORACLE)) {
+        if (shouldPushDownLimit() && (jdbcType == TOdbcTableType.ORACLE
+                || jdbcType == TOdbcTableType.OCEANBASE_ORACLE)) {
             filters.add("ROWNUM <= " + limit);
         }
 
