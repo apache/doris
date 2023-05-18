058299993e26001b3de6dee5eae0f8de17d4452a
WenYao <729673078@qq.com>
2023-05-18 15:33:49 +0800

    Update regression-conf.groovy

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -48,7 +48,7 @@ testDirectories = ""
 // this groups will not be executed
 excludeGroups = ""
 // this suites will not be executed
-excludeSuites = "test_broker_load", "test_spark_load"
+excludeSuites = "test_broker_load,test_spark_load"
 // this directories will not be executed
 excludeDirectories = ""
 
