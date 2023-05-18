32da959cab6c1ef4542cf3d6ae1328826a1294a7
zhangdong <493738387@qq.com>
2023-05-18 17:17:24 +0800

    broker support fqdn

--- fe/fe-core/src/main/java/org/apache/doris/catalog/BrokerMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/BrokerMgr.java
@@ -50,7 +50,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class BrokerMgr {
     public static final ImmutableList<String> BROKER_PROC_NODE_TITLE_NAMES = new ImmutableList.Builder<String>()
-            .add("Name").add("IP").add("HostName").add("Port").add("Alive")
+            .add("Name").add("Host").add("Port").add("Alive")
             .add("LastStartTime").add("LastUpdateTime").add("ErrMsg")
             .build();
 
@@ -228,7 +228,7 @@ public class BrokerMgr {
             }
             Env.getCurrentEnv().getEditLog().logAddBroker(new ModifyBrokerInfo(name, addedBrokerAddress));
             for (FsBroker address : addedBrokerAddress) {
-                brokerAddrsMap.put(address.ip, address);
+                brokerAddrsMap.put(address.host, address);
             }
             brokersMap.put(name, brokerAddrsMap);
             brokerListMap.put(name, Lists.newArrayList(brokerAddrsMap.values()));
@@ -246,7 +246,7 @@ public class BrokerMgr {
                 brokersMap.put(name, brokerAddrsMap);
             }
             for (FsBroker address : addresses) {
-                brokerAddrsMap.put(address.ip, address);
+                brokerAddrsMap.put(address.host, address);
             }
 
             brokerListMap.put(name, Lists.newArrayList(brokerAddrsMap.values()));
@@ -280,7 +280,7 @@ public class BrokerMgr {
             }
             Env.getCurrentEnv().getEditLog().logDropBroker(new ModifyBrokerInfo(name, droppedAddressList));
             for (FsBroker address : droppedAddressList) {
-                brokerAddrsMap.remove(address.ip, address);
+                brokerAddrsMap.remove(address.host, address);
             }
 
             brokerListMap.put(name, Lists.newArrayList(brokerAddrsMap.values()));
@@ -294,7 +294,7 @@ public class BrokerMgr {
         try {
             ArrayListMultimap<String, FsBroker> brokerAddrsMap = brokersMap.get(name);
             for (FsBroker addr : addresses) {
-                brokerAddrsMap.remove(addr.ip, addr);
+                brokerAddrsMap.remove(addr.host, addr);
             }
 
             brokerListMap.put(name, Lists.newArrayList(brokerAddrsMap.values()));
@@ -364,8 +364,7 @@ public class BrokerMgr {
                     for (FsBroker broker : entry.getValue().values()) {
                         List<String> row = Lists.newArrayList();
                         row.add(brokerName);
-                        row.add(broker.ip);
-                        row.add(NetUtils.getHostnameByIp(broker.ip));
+                        row.add(broker.host);
                         row.add(String.valueOf(broker.port));
                         row.add(String.valueOf(broker.isAlive));
                         row.add(TimeUtils.longToTimeString(broker.lastStartTime));
