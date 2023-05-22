0b7b01840157a87e6dfb55823cf5e18f73a91960
sohardforaname <organic_chemistry@foxmail.com>
2023-05-22 19:18:45 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/load.groovy
+++ regression-test/suites/nereids_p0/load.groovy
@@ -16,7 +16,7 @@
 // under the License.
 
 suite("load_test_query_db") {
-    sql "SET enable_nereids_planner=false"
+    sql "SET enable_nereids_planner=true"
     sql "SET enable_fallback_to_original_planner=false"
     // init query case data
     def dbName = "test_query_db"
