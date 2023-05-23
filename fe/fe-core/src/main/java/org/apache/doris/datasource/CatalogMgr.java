1c879ea21ae2e633a5e1b2938545bce8857d51fc
卫诗 <q763562998@163.com>
2023-05-23 15:51:44 +0800

    [fix](multi-catalog) check Exception.getMessage()

--- fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
@@ -554,6 +554,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
                 } catch (DdlException ddlException) {
                     //把数据回退回去
                     ((ExternalCatalog) catalog).rollBackCatalogProps(oldProps);
+
                     throw new DdlException("props error" + ddlException.getMessage());
                 }
 
