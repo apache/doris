c9eeabad6114e4521e20c03fe23dc7977948a40d
starocean999 <12095047@qq.com>
2023-05-23 17:26:45 +0800

    fix boolean to decimalv3 bug

--- fe/fe-common/src/main/java/org/apache/doris/catalog/ScalarType.java
+++ fe/fe-common/src/main/java/org/apache/doris/catalog/ScalarType.java
@@ -1128,6 +1128,8 @@ public class ScalarType extends Type {
         if (t1.isDecimalV3() || t2.isDecimalV3()) {
             if (t1.isFloatingPointType() || t2.isFloatingPointType()) {
                 return t1.isFloatingPointType() ? t1 : t2;
+            } else if (t1.isBoolean() || t2.isBoolean()) {
+                return t1.isDecimalV3() ? t1 : t2;
             }
         }
 
