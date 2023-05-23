e897e61142d5c127bcc224de07f84d5136d8c552
nextdreamblue <zxw520blue1@163.com>
2023-05-23 17:45:43 +0800

    add regression test 6
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- regression-test/suites/data_model_p0/duplicate/test_duplicate_table_without_keys.groovy
+++ regression-test/suites/data_model_p0/duplicate/test_duplicate_table_without_keys.groovy
@@ -22,6 +22,26 @@ suite("test_duplicate_table_without_keys") {
         sql "CREATE DATABASE  ${dbName}"
     }
     sql "use ${dbName}"
+
+    def tbName1 = "test_default_data_model"
+    sql "DROP TABLE IF EXISTS ${tbName1}"
+    sql """
+            CREATE TABLE IF NOT EXISTS ${tbName1} (
+                k1 int,
+                k2 int, 
+                k3 int,
+                int_value int
+            )
+            DISTRIBUTED BY HASH(k1) BUCKETS 5 
+            properties("replication_num" = "1","enable_duplicate_without_keys_by_default" = "false");
+        """
+    sql "insert into ${tbName1} values(0, 1, 2, 4)"
+    sql "insert into ${tbName1} values(0, 1, 2, 5)"
+    sql "insert into ${tbName1} values(0, 1, 2, 3)"
+    order_qt_select_dup_table "select * from ${tbName1}"
+    qt_desc_dup_table "desc ${tbName1}"
+    qt_show_dup_table "show create table ${tbName1}"
+    sql "DROP TABLE ${tbName1}"
     
     def tbName2 = "test_default_data_model_no_keys"
     sql "DROP TABLE IF EXISTS ${tbName2}"
