bf1ccc1374a2dc515f11b1b132d2410e98339869
sohardforaname <organic_chemistry@foxmail.com>
2023-05-23 16:08:11 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
@@ -47,7 +47,6 @@ import org.apache.doris.nereids.trees.expressions.NamedExpression;
 import org.apache.doris.nereids.trees.plans.AbstractPlan;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.commands.ExplainCommand.ExplainLevel;
-import org.apache.doris.nereids.trees.plans.logical.LogicalOlapTableSink;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalPlan;
@@ -240,7 +239,7 @@ public class NereidsPlanner extends Planner {
             deriveStats();
             serializeStatUsed(statementContext.getConnectContext());
 
-            adjustRequiredProperties(cascadesContext.getRewritePlan());
+            cascadesContext.setJobContext(PhysicalProperties.GATHER);
 
             optimize();
             NereidsTracer.logImportantTime("EndOptimizePlan");
@@ -343,17 +342,6 @@ public class NereidsPlanner extends Planner {
             dpHypOptimize();
         }
         new CascadesOptimizer(cascadesContext).execute();
-    }
-
-    private void adjustRequiredProperties(Plan plan) {
-        PhysicalProperties properties = null;
-        if (plan instanceof LogicalOlapTableSink) {
-            properties = ((LogicalOlapTableSink<?>) plan).calculatePhysicalProperties();
-        }
-        if (properties == null) {
-            properties = PhysicalProperties.GATHER;
-        }
-        cascadesContext.setJobContext(properties);
     }
 
     private PhysicalPlan postProcess(PhysicalPlan physicalPlan) {
