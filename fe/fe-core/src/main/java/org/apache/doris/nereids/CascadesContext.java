cfe6acf0f43a5fac01b6a074a0f0a49a1139da69
sohardforaname <organic_chemistry@foxmail.com>
2023-05-24 12:33:10 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/CascadesContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/CascadesContext.java
@@ -99,8 +99,8 @@ public class CascadesContext implements ScheduleContext, PlanSource {
     private Optional<Scope> outerScope = Optional.empty();
 
     public CascadesContext(Plan plan, Memo memo, StatementContext statementContext,
-            PhysicalProperties requiredProperties) {
-        this(plan, memo, statementContext, new CTEContext(), requiredProperties);
+            PhysicalProperties requestProperties) {
+        this(plan, memo, statementContext, new CTEContext(), requestProperties);
     }
 
     /**
@@ -110,14 +110,14 @@ public class CascadesContext implements ScheduleContext, PlanSource {
      * @param statementContext {@link StatementContext} reference
      */
     public CascadesContext(Plan plan, Memo memo, StatementContext statementContext,
-            CTEContext cteContext, PhysicalProperties requiredProperties) {
+            CTEContext cteContext, PhysicalProperties requestProperties) {
         this.plan = plan;
         this.memo = memo;
         this.statementContext = statementContext;
         this.ruleSet = new RuleSet();
         this.jobPool = new JobStack();
         this.jobScheduler = new SimpleJobScheduler();
-        this.currentJobContext = new JobContext(this, requiredProperties, Double.MAX_VALUE);
+        this.currentJobContext = new JobContext(this, requestProperties, Double.MAX_VALUE);
         this.subqueryExprIsAnalyzed = new HashMap<>();
         this.runtimeFilterContext = new RuntimeFilterContext(getConnectContext().getSessionVariable());
         this.cteContext = cteContext;
@@ -129,8 +129,8 @@ public class CascadesContext implements ScheduleContext, PlanSource {
     }
 
     public static CascadesContext newRewriteContext(StatementContext statementContext,
-            Plan initPlan, PhysicalProperties requiredProperties) {
-        return new CascadesContext(initPlan, null, statementContext, requiredProperties);
+            Plan initPlan, PhysicalProperties requestProperties) {
+        return new CascadesContext(initPlan, null, statementContext, requestProperties);
     }
 
     public static CascadesContext newRewriteContext(StatementContext statementContext,
