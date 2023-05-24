77e4515b3190fb2eef02095934f668ef9e2dbfa4
Xinyi Zou <zouxinyi02@gmail.com>
2023-05-24 14:24:03 +0800

    [fix](hashtable) Check query cancel status during build hash table #19970
    should cancel query during hash table build stage if the query is cancelled.

--- be/src/vec/exec/vset_operation_node.h
+++ be/src/vec/exec/vset_operation_node.h
@@ -49,7 +49,7 @@ protected:
     //It's time to abstract out the same methods and provide them directly to others;
     void hash_table_init();
     Status hash_table_build(RuntimeState* state);
-    Status process_build_block(Block& block, uint8_t offset);
+    Status process_build_block(Block& block, uint8_t offset, RuntimeState* state);
     Status extract_build_column(Block& block, ColumnRawPtrs& raw_ptrs);
     Status extract_probe_column(Block& block, ColumnRawPtrs& raw_ptrs, int child_id);
     template <bool keep_matched>
