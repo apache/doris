4e0b0766821632b13389a1b943965284511a0e0b
sohardforaname <organic_chemistry@foxmail.com>
2023-05-25 16:49:33 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/txn/Transaction.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/txn/Transaction.java
@@ -48,7 +48,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.spark.executor.package$;
 
 /**
  * transaction wrapper for Nereids
