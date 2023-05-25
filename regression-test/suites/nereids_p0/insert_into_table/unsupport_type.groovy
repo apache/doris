ad83a75980ff0a695375ca65344361dff3889718
sohardforaname <organic_chemistry@foxmail.com>
2023-05-25 16:30:58 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
@@ -23,9 +23,6 @@ suite("nereids_insert_array_type") {
     sql 'set enable_nereids_dml=true'
     sql 'set parallel_fragment_exec_instance_num=13'
 
-    sql 'set enable_nereids_planner=true'
-    sql 'set enable_fallback_to_original_planner=false'
-
     test {
         sql 'insert into arr_t select id, kaint from src'
         exception 'type ARRAY<INT> is unsupported for Nereids'
