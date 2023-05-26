4cb834a09d0c0e9f6252e3fb63253255e348a4e1
sohardforaname <organic_chemistry@foxmail.com>
2023-05-26 17:39:49 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -346,6 +346,7 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
             rootFragment.setPlanRoot(exchangeNode.getChild(0));
             rootFragment.setDestination(exchangeNode);
             context.addPlanFragment(currentFragment);
+            currentFragment.setDataPartition(rootFragment.getDataPartition());
             rootFragment = currentFragment;
         }
         rootFragment.setSink(sink);
@@ -2552,15 +2553,15 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
     private PlanFragment setPlanFragmentForInsert(PlanFragment inputFragment, List<Expr> execExprList) {
         OlapTable olapTable = context.getInsertTargetTable();
         HashDistributionInfo distributionInfo = ((HashDistributionInfo) olapTable.getDefaultDistributionInfo());
-        List<Integer> colIdx = distributionInfo.getDistributionColumns().stream()
-                .map(column -> olapTable.getFullSchema().indexOf(column))
-                .collect(Collectors.toList());
+        // List<Integer> colIdx = distributionInfo.getDistributionColumns().stream()
+        //         .map(column -> olapTable.getFullSchema().indexOf(column))
+        //         .collect(Collectors.toList());
 
-        List<Expr> partitionExprs = colIdx.stream()
-                .map(idx -> execExprList.get(idx + olapTable.getFullSchema().size()))
-                .collect(Collectors.toList());
+        // List<Expr> partitionExprs = colIdx.stream()
+        //         .map(idx -> execExprList.get(idx + olapTable.getFullSchema().size()))
+        //         .collect(Collectors.toList());
 
-        inputFragment.setDataPartition(DataPartition.hashPartitioned(partitionExprs));
+        // inputFragment.setDataPartition(DataPartition.hashPartitioned(partitionExprs));
         List<Expr> castExprs = Lists.newArrayList();
         for (int i = 0; i < olapTable.getFullSchema().size(); ++i) {
             try {
