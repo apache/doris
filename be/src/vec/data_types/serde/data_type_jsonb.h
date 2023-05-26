ee34b6de2dab7f870448249479e98ca4d3d18793
amory <wangqiannan@selectdb.com>
2023-05-26 14:11:17 +0800

    [Refact] (serde) refact mysql serde with data type (#19543)
    refact mysql output (de)serialize with data type serde , avoid accoriding switch case Primitive type writed in mysqlWriter

--- be/src/vec/data_types/serde/data_type_jsonb.h
+++ be/src/vec/data_types/serde/data_type_jsonb.h
@@ -16,23 +16,12 @@
 // under the License.
 
 #pragma once
-
 #include <gen_cpp/types.pb.h>
 #include <glog/logging.h>
 #include <stddef.h>
 #include <stdint.h>
 
-#include <ostream>
-#include <string>
-
-#include "common/status.h"
 #include "data_type_number_serde.h"
-#include "olap/olap_common.h"
-#include "util/jsonb_document.h"
-#include "util/jsonb_writer.h"
-#include "vec/columns/column.h"
-#include "vec/columns/column_vector.h"
-#include "vec/common/string_ref.h"
 #include "vec/core/types.h"
 
 namespace doris {
@@ -41,32 +30,33 @@ class JsonbOutStream;
 namespace vectorized {
 class Arena;
 
-class DataTypeDateTimeV2SerDe : public DataTypeNumberSerDe<UInt64> {
-public:
-    DataTypeDateTimeV2SerDe(int scale) : scale(scale) {};
-    void write_column_to_arrow(const IColumn& column, const UInt8* null_map,
-                               arrow::ArrayBuilder* array_builder, int start,
-                               int end) const override;
-    void read_column_from_arrow(IColumn& column, const arrow::Array* arrow_array, int start,
-                                int end, const cctz::time_zone& ctz) const override {
-        LOG(FATAL) << "not support read arrow array to uint64 column";
-    }
+class DataTypeTimeSerDe : public DataTypeNumberSerDe<Float64> {
     Status write_column_to_mysql(const IColumn& column, std::vector<MysqlRowBuffer<false>>& result,
                                  int row_idx, int start, int end, bool col_const) const override {
-        return _write_column_to_mysql(column, result, row_idx, start, end, col_const);
+        return _write_date_time_column_to_mysql(column, result, row_idx, start, end, col_const);
     }
-
     Status write_column_to_mysql(const IColumn& column, std::vector<MysqlRowBuffer<true>>& result,
                                  int row_idx, int start, int end, bool col_const) const override {
-        return _write_column_to_mysql(column, result, row_idx, start, end, col_const);
+        return _write_date_time_column_to_mysql(column, result, row_idx, start, end, col_const);
     }
 
 private:
     template <bool is_binary_format>
-    Status _write_column_to_mysql(const IColumn& column,
-                                  std::vector<MysqlRowBuffer<is_binary_format>>& result,
-                                  int row_idx, int start, int end, bool col_const) const;
-    int scale;
+    Status _write_date_time_column_to_mysql(const IColumn& column,
+                                            std::vector<MysqlRowBuffer<is_binary_format>>& result,
+                                            int row_idx, int start, int end, bool col_const) const {
+        int buf_ret = 0;
+        auto& data = assert_cast<const ColumnVector<Float64>&>(column).get_data();
+        for (int i = start; i < end; ++i) {
+            if (0 != buf_ret) {
+                return Status::InternalError("pack mysql buffer failed.");
+            }
+            const auto col_index = index_check_const(i, col_const);
+            buf_ret = result[row_idx].push_time(data[col_index]);
+            ++row_idx;
+        }
+        return Status::OK();
+    }
 };
 } // namespace vectorized
 } // namespace doris
