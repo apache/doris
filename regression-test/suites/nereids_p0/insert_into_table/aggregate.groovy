04aa2fcfeca5c6ba0eef2e5cca1d10fe6b3f632b
sohardforaname <organic_chemistry@foxmail.com>
2023-05-26 16:07:25 +0800

    'fix-bugs'

--- regression-test/suites/nereids_p0/insert_into_table/aggregate.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/aggregate.groovy
@@ -20,7 +20,7 @@ suite("nereids_insert_aggregate") {
     sql 'clean label from nereids_insert_into_table_test'
 
     sql 'set enable_nereids_planner=true'
-    sql 'set enable_fallback_to_original_planner=false'
+    sql 'set enable_fallback_to_original_planner=true'
 
     sql '''insert into nereids_insert_into_table_test.agg_t
             select * except(kaint) from src'''
