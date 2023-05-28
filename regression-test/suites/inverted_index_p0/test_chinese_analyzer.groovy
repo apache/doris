deb00c908fc5f578fbac228047bd7315f18b5071
姜凯 <airborne08@gmail.com>
2023-05-28 20:03:36 +0800

    [test](inverted index) add more test for chinese analyzer

--- regression-test/suites/inverted_index_p0/test_chinese_analyzer.groovy
+++ regression-test/suites/inverted_index_p0/test_chinese_analyzer.groovy
@@ -51,5 +51,32 @@ suite("test_chinese_analyzer"){
     qt_sql "SELECT * FROM $indexTblName WHERE c MATCH '我' ORDER BY id;"
     qt_sql "SELECT * FROM $indexTblName WHERE c MATCH '清华' ORDER BY id;"
     qt_sql "SELECT * FROM $indexTblName WHERE c MATCH '大学' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName WHERE c MATCH '清华大学' ORDER BY id;"
     qt_sql "SELECT * FROM $indexTblName WHERE c MATCH '人民' ORDER BY id;"
+
+    def indexTblName2 = "chinese_analyzer_test2"
+
+    sql "DROP TABLE IF EXISTS ${indexTblName2}"
+    // create 1 replica table
+    sql """
+	CREATE TABLE IF NOT EXISTS ${indexTblName2}(
+		`id`int(11)NULL,
+		`c` text NULL,
+		INDEX c_idx(`c`) USING INVERTED PROPERTIES("parser"="chinese","parser_mode"="coarse_grained") COMMENT ''
+	) ENGINE=OLAP
+	DUPLICATE KEY(`id`)
+	COMMENT 'OLAP'
+	DISTRIBUTED BY HASH(`id`) BUCKETS 1
+	PROPERTIES(
+                "replication_allocation" = "tag.location.default: 1"
+        );
+    """
+
+    sql "INSERT INTO $indexTblName2 VALUES (1, '我来到北京清华大学'), (2, '我爱你中国'), (3, '人民可以得到更多实惠');"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '我爱你' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '我' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '清华' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '大学' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '清华大学' ORDER BY id;"
+    qt_sql "SELECT * FROM $indexTblName2 WHERE c MATCH '人民' ORDER BY id;"
 }
