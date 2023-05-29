apache/doris

commit 2b260e95b9e10153169f2ffa728b1383dd58b1e6

[Enhancement](multi catalog) Support hudi mor only java side ,be side not support (#19909)
Support reading Hudi MOR table by using jni connector.
Note:
the FE part of the current PR is not completed all, and the BE part will be supplemented in next PR.

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HMSExternalTable.java
@@ -69,6 +69,18 @@ public class HMSExternalTable extends ExternalTable {
         SUPPORTED_HIVE_FILE_FORMATS.add("org.apache.hadoop.mapred.TextInputFormat");
     }
 
+    private static final Set<String> SUPPORTED_HUDI_FILE_FORMATS;
+
+    static {
+        SUPPORTED_HUDI_FILE_FORMATS = Sets.newHashSet();
+        SUPPORTED_HUDI_FILE_FORMATS.add("org.apache.hudi.hadoop.HoodieParquetInputFormat");
+        SUPPORTED_HUDI_FILE_FORMATS.add("com.uber.hoodie.hadoop.HoodieInputFormat");
+        SUPPORTED_HUDI_FILE_FORMATS.add("org.apache.hudi.hadoop.HoodieParquetInputFormat");
+        SUPPORTED_HUDI_FILE_FORMATS.add("com.uber.hoodie.hadoop.HoodieInputFormat");
+        SUPPORTED_HUDI_FILE_FORMATS.add("org.apache.hudi.hadoop.realtime.HoodieParquetRealtimeInputFormat");
+        SUPPORTED_HUDI_FILE_FORMATS.add("com.uber.hoodie.hadoop.realtime.HoodieRealtimeInputFormat");
+    }
+
     private volatile org.apache.hadoop.hive.metastore.api.Table remoteTable = null;
     private List<Column> partitionColumns;
 
@@ -136,8 +148,7 @@ public class HMSExternalTable extends ExternalTable {
             return false;
         }
         String inputFormatName = remoteTable.getSd().getInputFormat();
-        return inputFormatName != null
-                && inputFormatName.equalsIgnoreCase("org.apache.hudi.hadoop.HoodieParquetInputFormat");
+        return inputFormatName != null && SUPPORTED_HUDI_FILE_FORMATS.contains(inputFormatName);
     }
 
     /**
@@ -341,6 +352,9 @@ public class HMSExternalTable extends ExternalTable {
         List<FieldSchema> schema = ((HMSExternalCatalog) catalog).getClient().getSchema(dbName, name);
         if (dlaType.equals(DLAType.ICEBERG)) {
             columns = getIcebergSchema(schema);
+        }
+        if (dlaType.equals(DLAType.HUDI)) {
+            columns = getHudiSchema(schema);
         } else {
             List<Column> tmpSchema = Lists.newArrayListWithCapacity(schema.size());
             for (FieldSchema field : schema) {
@@ -352,6 +366,20 @@ public class HMSExternalTable extends ExternalTable {
         }
         initPartitionColumns(columns);
         return columns;
+    }
+
+
+    public List<Column> getHudiSchema(List<FieldSchema> hmsSchema) {
+        org.apache.avro.Schema schema = HiveMetaStoreClientHelper.getHudiTableSchema(this);
+        List<Column> tmpSchema = Lists.newArrayListWithCapacity(hmsSchema.size());
+        for (FieldSchema field : hmsSchema) {
+            tmpSchema.add(new Column(field.getName(),
+                    HiveMetaStoreClientHelper.hiveTypeToDorisType(field.getType(),
+                            IcebergExternalTable.ICEBERG_DATETIME_SCALE_MS), true, null,
+                    true, null, field.getComment(), true, null,
+                    schema.getIndexNamed(field.getName()), null));
+        }
+        return tmpSchema;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/catalog/HiveMetaStoreClientHelper.java
@@ -47,6 +47,7 @@ import com.aliyun.datalake.metastore.hive2.ProxyMetaStoreClient;
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Queues;
+import org.apache.avro.Schema;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -70,6 +71,9 @@ import org.apache.hadoop.hive.ql.plan.ExprNodeGenericFuncDesc;
 import org.apache.hadoop.hive.serde2.typeinfo.PrimitiveTypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
+import org.apache.hudi.avro.HoodieAvroUtils;
+import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.TableSchemaResolver;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -899,6 +903,28 @@ public class HiveMetaStoreClientHelper {
         hiveCatalog.initialize("hive", catalogProperties);
 
         return hiveCatalog.loadTable(TableIdentifier.of(table.getDbName(), table.getName()));
+    }
+
+    public static Schema getHudiTableSchema(HMSExternalTable table) {
+        HoodieTableMetaClient metaClient = getHudiClient(table);
+        TableSchemaResolver schemaUtil = new TableSchemaResolver(metaClient);
+        Schema hudiSchema;
+        try {
+            hudiSchema = HoodieAvroUtils.createHoodieWriteSchema(schemaUtil.getTableAvroSchema());
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot get hudi table schema.");
+        }
+        return hudiSchema;
+    }
+
+    public static HoodieTableMetaClient getHudiClient(HMSExternalTable table) {
+        String hudiBasePath = table.getRemoteTable().getSd().getLocation();
+
+        Configuration conf = getConfiguration(table);
+
+        HoodieTableMetaClient metaClient =
+                HoodieTableMetaClient.builder().setConf(conf).setBasePath(hudiBasePath).build();
+        return metaClient;
     }
 
     public static Configuration getConfiguration(HMSExternalTable table) {

fe/fe-core/src/main/java/org/apache/doris/catalog/TableIf.java
@@ -134,7 +134,7 @@ public interface TableIf {
     enum TableType {
         MYSQL, ODBC, OLAP, SCHEMA, INLINE_VIEW, VIEW, BROKER, ELASTICSEARCH, HIVE, ICEBERG, @Deprecated HUDI, JDBC,
         TABLE_VALUED_FUNCTION, HMS_EXTERNAL_TABLE, ES_EXTERNAL_TABLE, MATERIALIZED_VIEW, JDBC_EXTERNAL_TABLE,
-        ICEBERG_EXTERNAL_TABLE, TEST_EXTERNAL_TABLE, MAX_COMPUTE_EXTERNAL_TABLE;
+        ICEBERG_EXTERNAL_TABLE, TEST_EXTERNAL_TABLE, MAX_COMPUTE_EXTERNAL_TABLE, HUDI_EXTERNAL_TABLE;
 
         public String toEngineName() {
             switch (this) {
@@ -169,6 +169,8 @@ public interface TableIf {
                     return "es";
                 case ICEBERG_EXTERNAL_TABLE:
                     return "iceberg";
+                case HUDI_EXTERNAL_TABLE:
+                    return "hudi";
                 default:
                     return null;
             }

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HudiExternalDatabase.java
@@ -1,0 +1,72 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.catalog.external;
+
+import org.apache.doris.catalog.TableIf;
+import org.apache.doris.datasource.ExternalCatalog;
+import org.apache.doris.datasource.InitDatabaseLog;
+import org.apache.doris.datasource.hudi.HudiHMSExternalCatalog;
+import org.apache.doris.persist.gson.GsonPostProcessable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HudiExternalDatabase extends ExternalDatabase<HudiExternalTable> implements GsonPostProcessable {
+
+    private static final Logger LOG = LogManager.getLogger(HudiExternalDatabase.class);
+
+    public HudiExternalDatabase(ExternalCatalog extCatalog, Long id, String name) {
+        super(extCatalog, id, name, InitDatabaseLog.Type.HUDI);
+    }
+
+    @Override
+    protected HudiExternalTable getExternalTable(String tableName, long tblId, ExternalCatalog catalog) {
+        return new HudiExternalTable(tblId, tableName, name, (HudiHMSExternalCatalog) extCatalog);
+    }
+
+    @Override
+    public List<HudiExternalTable> getTablesOnIdOrder() {
+        // Sort the name instead, because the id may change.
+        return getTables().stream().sorted(Comparator.comparing(TableIf::getName)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void dropTable(String tableName) {
+        LOG.debug("drop table [{}]", tableName);
+        makeSureInitialized();
+        Long tableId = tableNameToId.remove(tableName);
+        if (tableId == null) {
+            LOG.warn("drop table [{}] failed", tableName);
+        }
+        idToTbl.remove(tableId);
+    }
+
+    @Override
+    public void createTable(String tableName, long tableId) {
+        LOG.debug("create table [{}]", tableName);
+        makeSureInitialized();
+        tableNameToId.put(tableName, tableId);
+        HudiExternalTable table = new HudiExternalTable(tableId, tableName, name,
+                (HudiHMSExternalCatalog) extCatalog);
+        idToTbl.put(tableId, table);
+    }
+}

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HudiExternalTable.java
@@ -1,0 +1,125 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+
+package org.apache.doris.catalog.external;
+
+import org.apache.doris.catalog.Column;
+import org.apache.doris.catalog.Type;
+import org.apache.doris.datasource.hudi.HudiHMSExternalCatalog;
+import org.apache.doris.thrift.THudiTable;
+import org.apache.doris.thrift.TTableDescriptor;
+import org.apache.doris.thrift.TTableType;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import org.apache.avro.Schema;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hudi.common.table.HoodieTableConfig;
+import org.apache.hudi.common.util.Option;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HudiExternalTable extends ExternalTable {
+
+    public HudiExternalTable(long id, String name, String dbName, HudiHMSExternalCatalog catalog) {
+        super(id, name, catalog, dbName, TableType.HUDI_EXTERNAL_TABLE);
+    }
+
+    public String getHudiCatalogType() {
+        return ((HudiHMSExternalCatalog) catalog).getHudiCatalogType();
+    }
+
+    protected synchronized void makeSureInitialized() {
+        super.makeSureInitialized();
+        if (!objectCreated) {
+            objectCreated = true;
+        }
+    }
+
+    @Override
+    public TTableDescriptor toThrift() {
+        List<Column> schema = getFullSchema();
+        String hudiCatalogType = getHudiCatalogType();
+        if (hudiCatalogType.equals("hms")) {
+            THudiTable hudiTable = new THudiTable();
+            hudiTable.setDbName(dbName);
+            hudiTable.setTableName(dbName);
+
+            TTableDescriptor tTableDescriptor = new TTableDescriptor(getId(), TTableType.HIVE_TABLE, schema.size(), 0,
+                    getName(), dbName);
+            tTableDescriptor.setHudiTable(hudiTable);
+            return tTableDescriptor;
+        }
+        throw new UnsupportedOperationException("hudi not supported  the catalog type :" + hudiCatalogType);
+    }
+
+    public static String fromAvroHudiTypeToHiveTypeString(Schema avroSchema) {
+        Schema.Type columnType = avroSchema.getType();
+        switch (columnType) {
+            case BOOLEAN:
+                return Type.BOOLEAN.toString();
+            case INT:
+                return Type.INT.toString();
+            case LONG:
+                return Type.BIGINT.toString();
+            case FLOAT:
+                return Type.FLOAT.toString();
+            case DOUBLE:
+                return Type.DOUBLE.toString();
+            case STRING:
+                return Type.STRING.toString();
+            case ARRAY:
+                String elementType = fromAvroHudiTypeToHiveTypeString(avroSchema.getElementType());
+                return String.format("array<%s>", elementType);
+            case RECORD:
+                List<Schema.Field> fields = avroSchema.getFields();
+                Preconditions.checkArgument(fields.size() > 0);
+                String nameToType = fields.stream()
+                        .map(f -> String.format("%s:%s", f.name(),
+                                fromAvroHudiTypeToHiveTypeString(f.schema())))
+                        .collect(Collectors.joining(","));
+                return String.format("struct<%s>", nameToType);
+            case MAP:
+                Schema value = avroSchema.getValueType();
+                String valueType = fromAvroHudiTypeToHiveTypeString(value);
+                return String.format("map<%s,%s>", "string", valueType);
+            default:
+                String errorMsg = String.format("Unsupported hudi %s type of column %s", avroSchema.getType().getName(),
+                        avroSchema.getName());
+                throw new IllegalArgumentException(errorMsg);
+        }
+    }
+
+    public static List<String> getPartitionColumnNames(Table table, HoodieTableConfig tableConfig) {
+        List<String> partitionColumnNames = Lists.newArrayList();
+        Option<String[]> hudiPartitionFields = tableConfig.getPartitionFields();
+
+        if (hudiPartitionFields.isPresent()) {
+            partitionColumnNames.addAll(Arrays.asList(hudiPartitionFields.get()));
+        } else if (!table.getPartitionKeys().isEmpty()) {
+            for (FieldSchema fieldSchema : table.getPartitionKeys()) {
+                String partField = fieldSchema.getName();
+                partitionColumnNames.add(partField);
+            }
+        }
+        return partitionColumnNames;
+    }
+}

fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogFactory.java
@@ -116,6 +116,9 @@ public class CatalogFactory {
             case "max_compute":
                 catalog = new MaxComputeExternalCatalog(catalogId, name, resource, props);
                 break;
+            // case "hudi":
+            //     catalog = new HudiHMSExternalCatalog(catalogId, name, resource, props);
+            //     break;
             case "test":
                 if (!FeConstants.runningUnitTest) {
                     throw new DdlException("test catalog is only for FE unit test");

fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
@@ -443,6 +443,8 @@ public abstract class ExternalCatalog
                 return new IcebergExternalDatabase(this, dbId, dbName);
             case MAX_COMPUTE:
                 return new MaxComputeExternalDatabase(this, dbId, dbName);
+            //case HUDI:
+                //return new HudiExternalDatabase(this, dbId, dbName);
             case TEST:
                 return new TestExternalDatabase(this, dbId, dbName);
             default:

fe/fe-core/src/main/java/org/apache/doris/datasource/InitCatalogLog.java
@@ -38,6 +38,7 @@ public class InitCatalogLog implements Writable {
         JDBC,
         ICEBERG,
         MAX_COMPUTE,
+        HUDI,
         TEST,
         UNKNOWN;
     }

commit 500995c4420b23cfb9ff449a36437d9d9e0d2eb9

[Fix](multi catalog)Fix Iceberg table missing column unique id bug (#20152)
This pr is to fix the bug introduced by PR #19909
The bug failed to set column unique id for iceberg table, which will cause the query result for iceberg table are all NULL.

```
mysql> select * from iceberg_partition_lower_case_parquet limit 1;
+------+------+------+---------+
| k1   | k2   | k3   | city    |
+------+------+------+---------+
| NULL | NULL | NULL | Beijing |
+------+------+------+---------+
1 row in set (0.60 sec)
```
After fix:
```
mysql> select * from iceberg_partition_lower_case_parquet limit 1;
+------+------+------+---------+
| k1   | k2   | k3   | city    |
+------+------+------+---------+
|    1 | k2_1 | k3_1 | Beijing |
+------+------+------+---------+
1 row in set (0.35 sec)
```

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HMSExternalTable.java
@@ -352,8 +352,7 @@ public class HMSExternalTable extends ExternalTable {
         List<FieldSchema> schema = ((HMSExternalCatalog) catalog).getClient().getSchema(dbName, name);
         if (dlaType.equals(DLAType.ICEBERG)) {
             columns = getIcebergSchema(schema);
-        }
-        if (dlaType.equals(DLAType.HUDI)) {
+        } else if (dlaType.equals(DLAType.HUDI)) {
             columns = getHudiSchema(schema);
         } else {
             List<Column> tmpSchema = Lists.newArrayListWithCapacity(schema.size());