badad2d5f81aa98481ae9a3959bfaee172ea7a38
wudi <>
2023-05-29 23:37:21 +0800

    add source example

---
+++ samples/doris-demo/doris-source-demo/src/main/java/org/apache/doris/sdk/DorisReaderExample.java
@@ -0,0 +1,159 @@
+package org.apache.doris.sdk;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.ipc.ArrowStreamReader;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.doris.sdk.thrift.TDorisExternalService;
+import org.apache.doris.sdk.thrift.TScanBatchResult;
+import org.apache.doris.sdk.thrift.TScanCloseParams;
+import org.apache.doris.sdk.thrift.TScanColumnDesc;
+import org.apache.doris.sdk.thrift.TScanNextBatchParams;
+import org.apache.doris.sdk.thrift.TScanOpenParams;
+import org.apache.doris.sdk.thrift.TScanOpenResult;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.thrift.TConfiguration;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DorisReaderExample {
+    static String dorisUrl = "127.0.0.1:8030";
+    static String user = "root";
+    static String password = "";
+    static String database = "db1";
+    static String table = "tbl2";
+    static String sql = "select * from db1.tbl2";
+
+    public static void main(String[] args) throws Exception {
+        JSONObject queryPlan = getQueryPlan();
+        System.out.println(queryPlan);
+        readData(queryPlan);
+
+    }
+
+    private static JSONObject getQueryPlan() throws Exception {
+        try (CloseableHttpClient client = HttpClients.custom().build()) {
+            HttpPost put = new HttpPost(String.format("http://%s/api/%s/%s/_query_plan", dorisUrl, database, table));
+            put.setHeader(HttpHeaders.EXPECT, "100-continue");
+            put.setHeader(HttpHeaders.AUTHORIZATION,  "Basic " + new String(Base64.encodeBase64((user + ":" + password).getBytes(StandardCharsets.UTF_8))));
+
+            Map<String,String> params = new HashMap<>();
+            params.put("sql",sql);
+            StringEntity entity = new StringEntity(JSON.toJSONString(params));
+            put.setEntity(entity);
+
+            try (CloseableHttpResponse response = client.execute(put)) {
+                if (response.getEntity() != null ) {
+                    String loadResult = EntityUtils.toString(response.getEntity());
+                    JSONObject queryPlan = JSONObject.parseObject(loadResult);
+                    return queryPlan;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void readData(JSONObject queryRes) throws Exception {
+        JSONObject data = queryRes.getJSONObject("data");
+        String queryPlan = data.getString("opaqued_query_plan");
+        JSONObject partitions = data.getJSONObject("partitions");
+        for(Map.Entry<String, Object> tablet : partitions.entrySet()){
+            Long tabletId = Long.parseLong(tablet.getKey());
+            JSONObject value = JSONObject.parseObject(JSON.toJSONString(tablet.getValue()));
+            //get first backend
+            String routingsBackend = value.getJSONArray("routings").getString(0);
+            String backendHost = routingsBackend.split(":")[0];
+            String backendPort = routingsBackend.split(":")[1];
+
+            //connect backend
+            TBinaryProtocol.Factory factory = new TBinaryProtocol.Factory();
+            TTransport transport = new TSocket(new TConfiguration(), backendHost, Integer.parseInt(backendPort));
+            TProtocol protocol = factory.getProtocol(transport);
+            TDorisExternalService.Client client = new TDorisExternalService.Client(protocol);
+            if (!transport.isOpen()) {
+                transport.open();
+            }
+
+            //build params
+            TScanOpenParams params = new TScanOpenParams();
+            params.cluster = "default_cluster";
+            params.database = database;
+            params.table = table;
+            params.tablet_ids = Arrays.asList(tabletId);
+            params.opaqued_query_plan = queryPlan;
+            // max row number of one read batch
+            params.setBatchSize(1024);
+            params.setQueryTimeout(3600);
+            params.setMemLimit(2147483648L);
+            params.setUser(user);
+            params.setPasswd(password);
+
+            int offset =0;
+            //open scanner
+            TScanOpenResult tScanOpenResult = client.openScanner(params);
+            List<TScanColumnDesc> selectedColumns = tScanOpenResult.getSelectedColumns();
+
+            TScanNextBatchParams nextBatchParams = new TScanNextBatchParams();
+            nextBatchParams.setContextId(tScanOpenResult.getContextId());
+            boolean eos = false;
+            //connect
+            while(!eos){
+                nextBatchParams.setOffset(offset);
+                TScanBatchResult next = client.getNext(nextBatchParams);
+                eos = next.isEos();
+                if(!eos){
+                    int i = convertArrow(next, selectedColumns);
+                    offset += i;
+                }
+            }
+
+            //close
+            TScanCloseParams closeParams = new TScanCloseParams();
+            closeParams.setContextId(tScanOpenResult.getContextId());
+            client.closeScanner(closeParams);
+            if ((transport != null) && transport.isOpen()) {
+                transport.close();
+            }
+            //System.out.println(String.format("read tablet %s from backend %s finished", tabletId, routingsBackend));
+        }
+    }
+
+    private static int convertArrow(TScanBatchResult nextResult, List<TScanColumnDesc> selectedColumns) throws Exception {
+        int offset = 0;
+        RootAllocator rootAllocator = new RootAllocator(Integer.MAX_VALUE);
+        ArrowStreamReader arrowStreamReader = new ArrowStreamReader(new ByteArrayInputStream(nextResult.getRows()), rootAllocator);
+
+        VectorSchemaRoot root = arrowStreamReader.getVectorSchemaRoot();
+        while (arrowStreamReader.loadNextBatch()) {
+            List<FieldVector>  fieldVectors = root.getFieldVectors();
+            //selectedColumns.forEach(desc -> System.out.print("columnHeader: " + desc.getName()));
+            for (int col = 0; col < fieldVectors.size(); col++) {
+                FieldVector fieldVector = fieldVectors.get(col);
+                //It needs to be converted into data according to the corresponding arrow type
+                System.out.print(fieldVector);
+            }
+            System.out.println();
+            offset += root.getRowCount();
+        }
+        return offset;
+    }
+
+}
