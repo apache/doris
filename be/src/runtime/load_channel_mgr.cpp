03787e3d7f9c6c572f90cf8941746cac13bbfde8
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2023-05-29 09:54:03 +0800

    [enhancement](load) add some profile items for load (#20141)

--- be/src/runtime/load_channel_mgr.cpp
+++ be/src/runtime/load_channel_mgr.cpp
@@ -125,7 +125,7 @@ Status LoadChannelMgr::open(const PTabletWriterOpenRequest& params) {
 #endif
             channel.reset(new LoadChannel(load_id, std::move(channel_mem_tracker),
                                           channel_timeout_s, is_high_priority, params.sender_ip(),
-                                          params.backend_id()));
+                                          params.backend_id(), params.enable_profile()));
             _load_channels.insert({load_id, channel});
         }
     }
@@ -182,11 +182,13 @@ Status LoadChannelMgr::add_batch(const PTabletWriterAddBlockRequest& request,
     if (!status.ok() || is_eof) {
         return status;
     }
+    SCOPED_TIMER(channel->get_mgr_add_batch_timer());
 
     if (!channel->is_high_priority()) {
         // 2. check if mem consumption exceed limit
         // If this is a high priority load task, do not handle this.
         // because this may block for a while, which may lead to rpc timeout.
+        SCOPED_TIMER(channel->get_handle_mem_limit_timer());
         _handle_mem_exceed_limit();
     }
 
