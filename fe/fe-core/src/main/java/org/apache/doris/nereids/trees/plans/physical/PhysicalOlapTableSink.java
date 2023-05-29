937dffe51d40068397d7db754daeafa62fa1a14f
sohardforaname <organic_chemistry@foxmail.com>
2023-05-29 12:34:07 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalOlapTableSink.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalOlapTableSink.java
@@ -204,6 +204,6 @@ public class PhysicalOlapTableSink<CHILD_TYPE extends Plan> extends PhysicalUnar
         }
         return PhysicalProperties.createHash(columnIndexes.stream()
                 .map(colIdx -> child().getOutput().get(colIdx).getExprId())
-                .collect(Collectors.toList()), ShuffleType.ENFORCED);
+                .collect(Collectors.toList()), ShuffleType.NATURAL);
     }
 }
