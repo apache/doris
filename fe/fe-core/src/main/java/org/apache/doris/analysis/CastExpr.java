0262af90c56bd965fb3c0d563b7edd2ffffad277
sohardforaname <organic_chemistry@foxmail.com>
2023-05-31 11:20:57 +0800

    'fix-bugs'

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
@@ -426,7 +426,18 @@ public class CastExpr extends Expr {
         } catch (AnalysisException ae) {
             targetExpr = this;
         } catch (NumberFormatException nfe) {
-            targetExpr = new NullLiteral();
+            // may be date or datetime, represented as string, we try to parse the string to date or datetime
+            try {
+                LiteralExpr dateLiteral = ((LiteralExpr) value.castTo(Type.DATETIME));
+                targetExpr = castTo(dateLiteral);
+                if (targetTypeDef != null) {
+                    targetExpr.setType(targetTypeDef.getType());
+                } else {
+                    targetExpr.setType(type);
+                }
+            } catch (Exception e) {
+                targetExpr = new NullLiteral();
+            }
         }
         return targetExpr;
     }
