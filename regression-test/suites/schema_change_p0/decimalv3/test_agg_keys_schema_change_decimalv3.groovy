f05a51a21c1ebfc6064065462c61d74c607b882e
shuke <37901441+shuke987@users.noreply.github.com>
2023-06-01 23:38:39 +0800

    [fix](regression-test) variable's scope returned by curl (#20347)

--- regression-test/suites/schema_change_p0/decimalv3/test_agg_keys_schema_change_decimalv3.groovy
+++ regression-test/suites/schema_change_p0/decimalv3/test_agg_keys_schema_change_decimalv3.groovy
@@ -43,7 +43,7 @@ suite("test_agg_keys_schema_change_decimalv3") {
             String tablet_id = tablet[0]
             backend_id = tablet[2]
             logger.info("run compaction:" + tablet_id)
-            def (code, out, err) = be_run_cumulative_compaction(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), tablet_id )
+            (code, out, err) = be_run_cumulative_compaction(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), tablet_id )
             logger.info("Run compaction: code=" + code + ", out=" + out + ", err=" + err)
         }
 
@@ -54,7 +54,7 @@ suite("test_agg_keys_schema_change_decimalv3") {
                 Thread.sleep(100)
                 String tablet_id = tablet[0]
                 backend_id = tablet[2]
-                def (code, out, err) = be_get_compaction_status(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), tablet_id)
+                (code, out, err) = be_get_compaction_status(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), tablet_id)
                 logger.info("Get compaction status: code=" + code + ", out=" + out + ", err=" + err)
                 assertEquals(code, 0)
                 def compactionStatus = parseJson(out.trim())
