367c7bf0156645c4fc4e8b662cfc5311cb64e3cb
hugoluo <hugoluo@tencent.com>
2023-06-02 17:15:16 +0800

    [Feature](multi-catalog) paimon catalog scan

--- fe/fe-core/src/main/java/org/apache/doris/datasource/paimon/PaimonHMSExternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/paimon/PaimonHMSExternalCatalog.java
@@ -22,7 +22,6 @@ import org.apache.doris.datasource.property.PropertyConverter;
 import org.apache.doris.datasource.property.constants.HMSProperties;
 import org.apache.doris.datasource.property.constants.PaimonProperties;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.paimon.catalog.Catalog;
 import org.apache.paimon.catalog.CatalogContext;
@@ -34,15 +33,9 @@ import org.apache.paimon.options.CatalogOptions;
 import org.apache.paimon.options.ConfigOption;
 import org.apache.paimon.options.ConfigOptions;
 import org.apache.paimon.options.Options;
-import org.apache.paimon.utils.StringUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Map;
-import javax.annotation.Nullable;
 
 
 
@@ -92,7 +85,7 @@ public class PaimonHMSExternalCatalog extends PaimonExternalCatalog {
         String uri = context.options().get(CatalogOptions.URI);
         String hiveConfDir = context.options().get(HiveCatalogOptions.HIVE_CONF_DIR);
         String hadoopConfDir = context.options().get(HiveCatalogOptions.HADOOP_CONF_DIR);
-        HiveConf hiveConf = createHiveConf(hiveConfDir, hadoopConfDir);
+        HiveConf hiveConf = HiveCatalog.createHiveConf(hiveConfDir, hadoopConfDir);
 
         // always using user-set parameters overwrite hive-site.xml parameters
         context.options().toMap().forEach(hiveConf::set);
@@ -103,66 +96,5 @@ public class PaimonHMSExternalCatalog extends PaimonExternalCatalog {
         String clientClassName = context.options().get(METASTORE_CLIENT_CLASS);
 
         return new HiveCatalog(fileIO, hiveConf, clientClassName, context.options().toMap());
-    }
-
-    public static HiveConf createHiveConf(@Nullable String hiveConfDir, @Nullable String hadoopConfDir) {
-        Configuration hadoopConf = null;
-        if (!StringUtils.isNullOrWhitespaceOnly(hadoopConfDir)) {
-            hadoopConf = HiveCatalog.getHadoopConfiguration(hadoopConfDir);
-            if (hadoopConf == null) {
-                String possiableUsedConfFiles = "core-site.xml | hdfs-site.xml | yarn-site.xml | mapred-site.xml";
-                throw new RuntimeException("Failed to load the hadoop conf from specified path:" + hadoopConfDir,
-                        new FileNotFoundException("Please check the path none of the conf files ("
-                            + possiableUsedConfFiles + ") exist in the folder."));
-            }
-        }
-
-        if (hadoopConf == null) {
-            hadoopConf = new Configuration();
-        }
-
-        if (hiveConfDir == null) {
-            return new HiveConf(hadoopConf, HiveConf.class);
-        } else {
-            HiveConf.setHiveSiteLocation((URL) null);
-            HiveConf.setLoadMetastoreConfig(false);
-            HiveConf.setLoadHiveServer2Config(false);
-            HiveConf hiveConf = new HiveConf(hadoopConf, HiveConf.class);
-            org.apache.hadoop.fs.Path hiveSite = new org.apache.hadoop.fs.Path(hiveConfDir, "hive-site.xml");
-            if (!hiveSite.toUri().isAbsolute()) {
-                hiveSite = new org.apache.hadoop.fs.Path((new File(hiveSite.toString())).toURI());
-            }
-
-            try {
-                InputStream inputStream = hiveSite.getFileSystem(hadoopConf).open(hiveSite);
-                Throwable var6 = null;
-
-                try {
-                    hiveConf.addResource(inputStream, hiveSite.toString());
-                    HiveCatalog.isEmbeddedMetastore(hiveConf);
-                } catch (Throwable var16) {
-                    var6 = var16;
-                    throw var16;
-                } finally {
-                    if (inputStream != null) {
-                        if (var6 != null) {
-                            try {
-                                inputStream.close();
-                            } catch (Throwable var15) {
-                                var6.addSuppressed(var15);
-                            }
-                        } else {
-                            inputStream.close();
-                        }
-                    }
-
-                }
-            } catch (IOException var18) {
-                throw new RuntimeException("Failed to load hive-site.xml from specified path:" + hiveSite, var18);
-            }
-
-            hiveConf.addResource(hiveSite);
-            return hiveConf;
-        }
     }
 }
