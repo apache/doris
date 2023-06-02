e0caff99d0b40c624767310cdb0726a4f977374c
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-06-02 10:33:11 +0800

    [Fix](Nereids) should not gather data when sink (#20330)

--- regression-test/suites/nereids_p0/insert_into_table/type_cast_aggregate.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/type_cast_aggregate.groovy
@@ -21,6 +21,7 @@ suite("nereids_insert_aggregate_type_cast") {
 
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
+    sql 'set enable_nereids_dml=true'
     sql 'set parallel_fragment_exec_instance_num=13'
 
     sql '''insert into agg_t_type_cast
