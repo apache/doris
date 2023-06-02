6a019a396462c57637f9533f12693e3cbd962c4d
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-06-02 10:33:11 +0800

    [Fix](Nereids) should not gather data when sink (#20330)

--- regression-test/suites/nereids_p0/insert_into_table/no_partition.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/no_partition.groovy
@@ -21,6 +21,7 @@ suite('nereids_insert_no_partition') {
 
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
+    sql 'set enable_nereids_dml=true'
     sql 'set parallel_fragment_exec_instance_num=13'
 
     sql '''insert into agg_nop_t
