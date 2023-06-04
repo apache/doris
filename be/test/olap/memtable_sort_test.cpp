39c6ac416d1a94584d1e9fd179b6b12bd028095c
Kaijie Chen <ckj@apache.org>
2023-06-04 15:54:55 +0800

    improve UT

--- be/test/olap/memtable_sort_test.cpp
+++ be/test/olap/memtable_sort_test.cpp
@@ -15,15 +15,18 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "olap/memtable.h"
-
 #include <gtest/gtest.h>
+
+#include "olap/memtable.h"
 
 namespace doris {
 
 class MemTableSortTest : public ::testing::Test {};
 
 TEST_F(MemTableSortTest, Tie) {
+    auto t0 = Tie{0, 0};
+    EXPECT_FALSE(t0.iter().next());
+
     auto tie = Tie {0, 1};
     EXPECT_FALSE(tie.iter().next());
 
@@ -33,48 +36,48 @@ TEST_F(MemTableSortTest, Tie) {
     }
 
     auto it1 = t.iter();
-    EXPECT_EQ(it1.next(), true);
+    EXPECT_TRUE(it1.next());
     EXPECT_EQ(it1.left(), 10);
     EXPECT_EQ(it1.right(), 30);
 
-    EXPECT_EQ(it1.next(), false);
+    EXPECT_FALSE(it1.next());
 
     t[13] = t[14] = t[22] = t[29] = 0;
     auto it2 = t.iter();
 
-    EXPECT_EQ(it2.next(), true);
+    EXPECT_TRUE(it2.next());
     EXPECT_EQ(it2.left(), 10);
     EXPECT_EQ(it2.right(), 13);
 
-    EXPECT_EQ(it2.next(), true);
+    EXPECT_TRUE(it2.next());
     EXPECT_EQ(it2.left(), 14);
     EXPECT_EQ(it2.right(), 22);
 
-    EXPECT_EQ(it2.next(), true);
+    EXPECT_TRUE(it2.next());
     EXPECT_EQ(it2.left(), 22);
     EXPECT_EQ(it2.right(), 29);
 
-    EXPECT_EQ(it2.next(), false);
+    EXPECT_FALSE(it2.next());
+    EXPECT_FALSE(it2.next());
 
     // 100000000...
     for (int i = 11; i < 30; i++) {
         t[i] = 0;
     }
-    auto it3 = t.iter();
-    EXPECT_EQ(it3.next(), false);
+    EXPECT_FALSE(t.iter().next());
 
     // 000000000...
     t[10] = 0;
-    auto it4 = t.iter();
-    EXPECT_EQ(it4.next(), false);
+    EXPECT_FALSE(t.iter().next());
 
     // 000000000...001
     t[29] = 1;
-    auto it5 = t.iter();
-    EXPECT_EQ(it5.next(), true);
-    EXPECT_EQ(it5.left(), 28);
-    EXPECT_EQ(it5.right(), 30);
-    EXPECT_EQ(it5.next(), false);
+    auto it3 = t.iter();
+    EXPECT_TRUE(it3.next());
+    EXPECT_EQ(it3.left(), 28);
+    EXPECT_EQ(it3.right(), 30);
+
+    EXPECT_FALSE(it3.next());
 }
 
 } // namespace doris
