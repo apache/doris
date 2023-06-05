0c7ef3beb7d4035cd92db8d9201ee573e9f19493
nextdreamblue <zxw520blue1@163.com>
2023-06-05 15:20:43 +0800

    add regression
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

---
+++ regression-test/suites/ddl_p0/test_truncate_table.groovy
@@ -0,0 +1,62 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// this suite is for creating table with timestamp datatype in defferent 
+// case. For example: 'year' and 'Year' datatype should also be valid in definition
+
+suite("sql_truncate_table") {
+    def testTable = "test_truncate_table"
+
+    sql "DROP TABLE IF EXISTS ${testTable}"
+
+    sql """
+        CREATE TABLE ${testTable}
+		(
+			k1 DATE,
+			k2 DECIMAL(10, 2) DEFAULT "10.5",
+			k3 CHAR(10) COMMENT "string column",
+			k4 INT NOT NULL DEFAULT "1" COMMENT "int column"
+		)
+		PARTITION BY RANGE(k1)
+		(
+			PARTITION p1 VALUES LESS THAN ("2020-02-01"),
+			PARTITION p2 VALUES LESS THAN ("2020-03-01"),
+			PARTITION p3 VALUES LESS THAN ("2020-04-01")
+		)
+		DISTRIBUTED BY HASH(k2) BUCKETS 32
+		PROPERTIES (
+			"replication_num" = "1"
+		);
+		"""
+	def result1 = sql "show partitions from ${testTable}"
+	assertEquals(result.size(), 3)
+    assertEquals(result.get(0).get(1), "p1")	
+
+	sql """truncate table ${testTable};"""
+	result1 = sql "show partitions from ${testTable}"
+	assertEquals(result.size(), 3)
+    assertEquals(result.get(0).get(1), "p1")
+
+    sql """truncate table ${testTable} partitions (p1, P1);"""
+
+	result1 = sql "show partitions from ${testTable}"
+	assertEquals(result.size(), 3)
+    assertEquals(result.get(0).get(1), "p1")
+	
+    sql "DROP TABLE IF EXISTS ${testTable}"
+}
+
