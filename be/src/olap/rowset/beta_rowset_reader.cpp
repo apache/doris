97b0137bb06cd0bebbe6e3c200dc25f8ae7e50dd
Xin Liao <liaoxinbit@126.com>
2023-06-05 19:18:34 +0800

    [fix](sequence) value predicates shouldn't be push down when has sequence column (#20408)
    * (fix)[sequence] value predicates shouldn't be push down when has sequence column
    
    * add case

--- be/src/olap/rowset/beta_rowset_reader.cpp
+++ be/src/olap/rowset/beta_rowset_reader.cpp
@@ -304,10 +304,12 @@ Status BetaRowsetReader::next_block_view(vectorized::BlockView* block_view) {
 
 bool BetaRowsetReader::_should_push_down_value_predicates() const {
     // if unique table with rowset [0-x] or [0-1] [2-y] [...],
-    // value column predicates can be pushdown on rowset [0-x] or [2-y], [2-y] must be compaction and not overlapping
+    // value column predicates can be pushdown on rowset [0-x] or [2-y], [2-y]
+    // must be compaction, not overlapping and don't have sequence column
     return _rowset->keys_type() == UNIQUE_KEYS &&
            (((_rowset->start_version() == 0 || _rowset->start_version() == 2) &&
-             !_rowset->_rowset_meta->is_segments_overlapping()) ||
+             !_rowset->_rowset_meta->is_segments_overlapping() &&
+             _context->sequence_id_idx == -1) ||
             _context->enable_unique_key_merge_on_write);
 }
 
