5104a2800ec5b75c06cdd71ac82bc86135c72c2c
liaoxin <liaoxinbit@126.com>
2023-06-05 11:08:28 +0800

    add case

--- regression-test/suites/data_model_p0/unique/test_unique_value_predicate.groovy
+++ regression-test/suites/data_model_p0/unique/test_unique_value_predicate.groovy
@@ -18,6 +18,7 @@
 suite("test_unique_value_predicate") {
     // test uniq table
     def tbName = "test_uniq_value_predicate"
+    // mor without seq
     sql "DROP TABLE IF EXISTS ${tbName}"
     sql """
             CREATE TABLE IF NOT EXISTS ${tbName} (
@@ -29,7 +30,7 @@ suite("test_unique_value_predicate") {
             UNIQUE KEY(k)
             DISTRIBUTED BY HASH(k) BUCKETS 1
             properties("replication_num" = "1",
-                       "function_column.sequence_col" = "int_value",
+                       "disable_auto_compaction" = "true",
                        "enable_unique_key_merge_on_write" = "false");
         """
     sql "insert into ${tbName} values(0, 2, 'test int', '2000-02-02')"
@@ -39,6 +40,7 @@ suite("test_unique_value_predicate") {
     qt_select "select * from ${tbName} where k = 0 && int_value = 2"
     qt_select "select * from ${tbName} where k = 0 && int_value = 1"
 
+    // mor with seq
     sql "DROP TABLE IF EXISTS ${tbName}"
     sql """
             CREATE TABLE IF NOT EXISTS ${tbName} (
@@ -51,6 +53,52 @@ suite("test_unique_value_predicate") {
             DISTRIBUTED BY HASH(k) BUCKETS 1
             properties("replication_num" = "1",
                        "function_column.sequence_col" = "int_value",
+                       "disable_auto_compaction" = "true",
+                       "enable_unique_key_merge_on_write" = "false");
+        """
+    sql "insert into ${tbName} values(0, 2, 'test int', '2000-02-02')"
+    sql "insert into ${tbName} values(0, 1, 'test char', '2000-01-01')"
+    qt_select "select * from ${tbName}"
+    qt_select "select * from ${tbName} where k = 0"
+    qt_select "select * from ${tbName} where k = 0 && int_value = 2"
+    qt_select "select * from ${tbName} where k = 0 && int_value = 1"
+
+    // mow without seq
+    sql "DROP TABLE IF EXISTS ${tbName}"
+    sql """
+            CREATE TABLE IF NOT EXISTS ${tbName} (
+                k int,
+                int_value int,
+                char_value char(10),
+                date_value date
+            )
+            UNIQUE KEY(k)
+            DISTRIBUTED BY HASH(k) BUCKETS 1
+            properties("replication_num" = "1",
+                       "disable_auto_compaction" = "true",
+                       "enable_unique_key_merge_on_write" = "true");
+        """
+    sql "insert into ${tbName} values(0, 2, 'test int', '2000-02-02')"
+    sql "insert into ${tbName} values(0, 1, 'test char', '2000-01-01')"
+    qt_select "select * from ${tbName}"
+    qt_select "select * from ${tbName} where k = 0"
+    qt_select "select * from ${tbName} where k = 0 && int_value = 2"
+    qt_select "select * from ${tbName} where k = 0 && int_value = 1"
+
+    // mow with seq
+    sql "DROP TABLE IF EXISTS ${tbName}"
+    sql """
+            CREATE TABLE IF NOT EXISTS ${tbName} (
+                k int,
+                int_value int,
+                char_value char(10),
+                date_value date
+            )
+            UNIQUE KEY(k)
+            DISTRIBUTED BY HASH(k) BUCKETS 1
+            properties("replication_num" = "1",
+                       "function_column.sequence_col" = "int_value",
+                       "disable_auto_compaction" = "true",
                        "enable_unique_key_merge_on_write" = "true");
         """
     sql "insert into ${tbName} values(0, 2, 'test int', '2000-02-02')"
