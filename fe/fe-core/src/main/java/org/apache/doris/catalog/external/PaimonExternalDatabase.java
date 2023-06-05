f0fa32e00f23f1c5b4561e55bc163a50d753d158
hugoluo <hugoluo@tencent.com>
2023-06-05 21:33:58 +0800

    [Feature](multi-catalog) paimon catalog scan

--- fe/fe-core/src/main/java/org/apache/doris/catalog/external/PaimonExternalDatabase.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/external/PaimonExternalDatabase.java
@@ -23,23 +23,16 @@ import org.apache.doris.datasource.InitDatabaseLog;
 import org.apache.doris.datasource.paimon.PaimonExternalCatalog;
 import org.apache.doris.persist.gson.GsonPostProcessable;
 
-import com.google.common.collect.Maps;
-import com.google.gson.annotations.SerializedName;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PaimonExternalDatabase extends ExternalDatabase<PaimonExternalTable> implements GsonPostProcessable {
 
     private static final Logger LOG = LogManager.getLogger(PaimonExternalDatabase.class);
-    // Cache of table name to table id.
-    private Map<String, Long> tableNameToId = Maps.newConcurrentMap();
-    @SerializedName(value = "idToTbl")
-    private Map<Long, PaimonExternalTable> idToTbl = Maps.newConcurrentMap();
 
     public PaimonExternalDatabase(ExternalCatalog extCatalog, Long id, String name) {
         super(extCatalog, id, name, InitDatabaseLog.Type.PAIMON);
@@ -48,31 +41,6 @@ public class PaimonExternalDatabase extends ExternalDatabase<PaimonExternalTable
     @Override
     protected PaimonExternalTable getExternalTable(String tableName, long tblId, ExternalCatalog catalog) {
         return new PaimonExternalTable(tblId, tableName, name, (PaimonExternalCatalog) extCatalog);
-    }
-
-    public void replayInitDb(InitDatabaseLog log, ExternalCatalog catalog) {
-        Map<String, Long> tmpTableNameToId = Maps.newConcurrentMap();
-        Map<Long, PaimonExternalTable> tmpIdToTbl = Maps.newConcurrentMap();
-        for (int i = 0; i < log.getRefreshCount(); i++) {
-            PaimonExternalTable table = getTableForReplay(log.getRefreshTableIds().get(i));
-            tmpTableNameToId.put(table.getName(), table.getId());
-            tmpIdToTbl.put(table.getId(), table);
-        }
-        for (int i = 0; i < log.getCreateCount(); i++) {
-            PaimonExternalTable table = new PaimonExternalTable(log.getCreateTableIds().get(i),
-                    log.getCreateTableNames().get(i), name, (PaimonExternalCatalog) catalog);
-            tmpTableNameToId.put(table.getName(), table.getId());
-            tmpIdToTbl.put(table.getId(), table);
-        }
-        tableNameToId = tmpTableNameToId;
-        idToTbl = tmpIdToTbl;
-        initialized = true;
-    }
-
-    public void setTableExtCatalog(ExternalCatalog extCatalog) {
-        for (PaimonExternalTable table : idToTbl.values()) {
-            table.setCatalog(extCatalog);
-        }
     }
 
     @Override
