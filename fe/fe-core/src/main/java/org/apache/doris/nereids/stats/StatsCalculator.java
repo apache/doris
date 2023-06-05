570554614b50738459ac49428f796c4ee93697aa
minghong <englefly@gmail.com>
2023-06-05 13:01:58 +0800

    [fix](nereids) avg size of column stats always be 0 (#20341)
    it takes lot of effort to compute the avgSizeByte for col stats.
    we use schema information to avoid compute actual average size

--- fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
@@ -551,6 +551,11 @@ public class StatsCalculator extends DefaultPlanVisitor<Statistics, Void> {
                 throw new RuntimeException(String.format("Invalid slot: %s", slotReference.getExprId()));
             }
             ColumnStatistic cache = Config.enable_stats ? getColumnStatistic(table, colName) : ColumnStatistic.UNKNOWN;
+            if (cache.avgSizeByte <= 0) {
+                cache = new ColumnStatisticBuilder(cache)
+                        .setAvgSizeByte(slotReference.getColumn().get().getType().getSlotSize())
+                        .build();
+            }
             if (cache == ColumnStatistic.UNKNOWN && !colName.equals("__DORIS_DELETE_SIGN__")) {
                 if (forbidUnknownColStats) {
                     if (StatisticsUtil.statsTblAvailable()) {
