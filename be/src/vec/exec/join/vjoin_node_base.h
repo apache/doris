7fae2afb4ee4c7d1814774111cb761b1bcbb79b7
yiguolei <676222867@qq.com>
2023-06-06 08:55:19 +0800

    [enhancement](profile) add build get child next time (#20460)
    Currently, build time not include child(1)->get next time, it is very confusing during shared hash table scenario. So that I add a profile.
    
    ---------
    
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- be/src/vec/exec/join/vjoin_node_base.h
+++ be/src/vec/exec/join/vjoin_node_base.h
@@ -124,6 +124,7 @@ protected:
     MutableColumnPtr _tuple_is_null_right_flag_column;
 
     RuntimeProfile::Counter* _build_timer;
+    RuntimeProfile::Counter* _build_get_next_timer;
     RuntimeProfile::Counter* _probe_timer;
     RuntimeProfile::Counter* _build_rows_counter;
     RuntimeProfile::Counter* _probe_rows_counter;
