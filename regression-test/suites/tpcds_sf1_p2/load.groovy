e3c4d08a6911d63de7c7ff714b714969c79e3d1e
zclllyybb <zhaochangle@selectdb.com>
2023-06-06 09:57:38 +0800

    [fix](regression-test) Add lost ddl file for tpcds_sf1_p2 #20288

--- regression-test/suites/tpcds_sf1_p2/load.groovy
+++ regression-test/suites/tpcds_sf1_p2/load.groovy
@@ -19,33 +19,7 @@
 // /testing/trino-product-tests/src/main/resources/sql-tests/testcases/tpcds
 // and modified by Doris.
 suite("load") {
-    def tables=["store", "store_returns", "customer", "date_dim", "web_sales",
-                "catalog_sales", "store_sales", "item", "web_returns", "catalog_returns",
-                "catalog_page", "web_site", "customer_address", "customer_demographics",
-                "ship_mode", "promotion", "inventory", "time_dim", "income_band",
-                "call_center", "reason", "household_demographics", "warehouse", "web_page"]
-    def columnsMap = [
-        "item": """tmp_item_sk, tmp_item_id, tmp_rec_start_date, tmp_rec_end_date, tmp_item_desc,
-                tmp_current_price, tmp_wholesale_cost, tmp_brand_id, tmp_brand, tmp_class_id, tmp_class,
-                tmp_category_id, tmp_category, tmp_manufact_id, tmp_manufact, tmp_size, tmp_formulation,
-                tmp_color, tmp_units, tmp_container, tmp_manager_id, tmp_product_name,
-                i_item_sk=tmp_item_sk, i_item_id=tmp_item_id, i_rec_start_date=tmp_rec_start_date,
-                i_rec_end_date=tmp_rec_end_date, i_item_desc=tmp_item_desc, i_current_price=tmp_current_price,
-                i_wholesale_cost=tmp_wholesale_cost, i_brand_id=tmp_brand_id, i_brand=tmp_brand,
-                i_class_id=tmp_class_id, i_class=tmp_class, i_category_id=tmp_category_id,
-                i_category=nullif(tmp_category, ''), i_manufact_id=tmp_manufact_id, i_manufact=tmp_manufact,
-                i_size=tmp_size, i_formulation=tmp_formulation, i_color=tmp_color, i_units=tmp_units,
-                i_container=tmp_container, i_manager_id=tmp_manager_id, i_product_name=tmp_product_name""",
-
-        "customer_address": """tmp_address_sk, tmp_address_id, tmp_street_number, tmp_street_name, tmp_street_type, tmp_suite_number,
-                            tmp_city, tmp_county, tmp_state, tmp_zip, tmp_country, tmp_gmt_offset, tmp_location_type,
-                            ca_address_sk=tmp_address_sk, ca_address_id=tmp_address_id, ca_street_number=tmp_street_number,
-                            ca_street_name=tmp_street_name, ca_street_type=tmp_street_type, ca_suite_number=tmp_suite_number, ca_city=tmp_city,
-                            ca_county=nullif(tmp_county, ''), ca_state=tmp_state, ca_zip=tmp_zip, ca_country=tmp_country,
-                            ca_gmt_offset=tmp_gmt_offset, ca_location_type=tmp_location_type""",
-    ]
-
-    def specialTables = ["item", "customer_address"]
+    def tables=["catalog_page"]
 
     for (String table in tables) {
         sql """ DROP TABLE IF EXISTS $table """
@@ -71,11 +45,6 @@ suite("load") {
             // this line change to ','
             set 'column_separator', '|'
             set 'compress_type', 'GZ'
-
-            if (specialTables.contains(tableName)) {
-            set "columns", columnsMap[tableName]
-            }
-
 
             // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
             // also, you can stream load a http stream, e.g. http://xxx/some.csv
