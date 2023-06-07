0e0cca21749fc4f7cc924a3bb07d0873c70c215c
姜凯 <airborne08@gmail.com>
2023-06-07 15:55:23 +0800

    [Chore](inverted index) fix PR comment

--- be/src/olap/rowset/segment_v2/inverted_index_reader.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_reader.cpp
@@ -499,7 +499,7 @@ Status StringTypeInvertedIndexReader::query(OlapReaderStatistics* stats,
                                 });
     } catch (const CLuceneError& e) {
         if (_is_range_query(query_type) && e.number() == CL_ERR_TooManyClauses) {
-            LOG(INFO) << "range query term exceeds limits, try to downgrade from inverted index,"
+            LOG(WARNING) << "range query term exceeds limits, try to downgrade from inverted index,"
                       << "column name:" << column_name << " search_str:" << search_str;
             return Status::Error<ErrorCode::INVERTED_INDEX_BYPASS>();
         } else {
