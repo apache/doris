b18be1e51dfc4b06fd9169afeb673d4bcb60a556
Gabriel <gabrielleebuaa@gmail.com>
2023-06-08 13:37:34 +0800

    test 0608

--- be/src/vec/columns/column_string.cpp
+++ be/src/vec/columns/column_string.cpp
@@ -113,6 +113,9 @@ void ColumnString::insert_indices_from(const IColumn& src, const int* indices_be
         if (*x == -1) {
             ColumnString::insert_default();
         } else {
+            if (x + 64 < indices_end) {
+                ColumnString::prefetch(src, *(x + 64));
+            }
             ColumnString::insert_from(src, *x);
         }
     }
