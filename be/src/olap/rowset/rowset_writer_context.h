546b45b7312294022ee631a1c81d453024def5ba
lihangyu <15605149486@163.com>
2023-06-08 10:55:22 +0800

    [Bug](row-store) Fix row store with materialize index (#20356)
    If a query hits a materialized view that has row storage enabled, but the row storage column is not present in the materialized view, it will result in a query crash. Therefore, it is necessary to include the row storage column when creating the materialized view, and serialize the row storage column during the execution of SchemaChange.

--- be/src/olap/rowset/rowset_writer_context.h
+++ be/src/olap/rowset/rowset_writer_context.h
@@ -20,6 +20,7 @@
 #include <gen_cpp/olap_file.pb.h>
 
 #include "io/fs/file_system.h"
+#include "olap/olap_define.h"
 #include "olap/tablet.h"
 #include "olap/tablet_schema.h"
 
@@ -82,9 +83,7 @@ struct RowsetWriterContext {
     int64_t newest_write_timestamp;
     bool enable_unique_key_merge_on_write = false;
     std::set<int32_t> skip_inverted_index;
-    // If it is directly write from load procedure, else
-    // it could be compaction or schema change etc..
-    bool is_direct_write = false;
+    DataWriteType write_type = DataWriteType::TYPE_DEFAULT;
     std::shared_ptr<Tablet> tablet = nullptr;
     // for tracing local schema change record
     std::shared_ptr<vectorized::schema_util::LocalSchemaChangeRecorder> schema_change_recorder =
