56f4281fed63062a68e28378e0beb1ea12cd8518
slothever <18522955+wsjz@users.noreply.github.com>
2023-06-09 09:18:43 +0800

    [fix](multi-catalog)fix hive catalog docs, obs impl, dlf properties (#20342)
    1. fix hive catalog docs 
    2. fix dlf properties
    3. fix obs impl

--- fe/fe-core/src/main/java/org/apache/doris/datasource/property/constants/DLFProperties.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/property/constants/DLFProperties.java
@@ -19,6 +19,10 @@ package org.apache.doris.datasource.property.constants;
 
 import org.apache.doris.datasource.credentials.CloudCredential;
 
+import com.aliyun.datalake.metastore.common.DataLakeConfig;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
 import java.util.Map;
 
 public class DLFProperties extends BaseProperties {
@@ -30,12 +34,27 @@ public class DLFProperties extends BaseProperties {
     public static final String SECRET_KEY = "dlf.secret_key";
     public static final String REGION = "dlf.region";
     public static final String SESSION_TOKEN = "dlf.session_token";
+    public static final List<String> META_KEYS = ImmutableList.of(DataLakeConfig.CATALOG_PROXY_MODE,
+            Site.ACCESS_PUBLIC,
+            DataLakeConfig.CATALOG_USER_ID,
+            DataLakeConfig.CATALOG_CREATE_DEFAULT_DB,
+            DataLakeConfig.CATALOG_ENDPOINT,
+            DataLakeConfig.CATALOG_REGION_ID,
+            DataLakeConfig.CATALOG_SECURITY_TOKEN,
+            DataLakeConfig.CATALOG_ACCESS_KEY_SECRET,
+            DataLakeConfig.CATALOG_ACCESS_KEY_ID);
 
     public static class Site {
         public static final String ACCESS_PUBLIC = "dlf.catalog.accessPublic";
     }
 
     public static CloudCredential getCredential(Map<String, String> props) {
-        return getCloudCredential(props, ACCESS_KEY, SECRET_KEY, SESSION_TOKEN);
+        CloudCredential credential = getCloudCredential(props, ACCESS_KEY, SECRET_KEY, SESSION_TOKEN);
+        if (!credential.isWhole()) {
+            credential = getCloudCredential(props, DataLakeConfig.CATALOG_ACCESS_KEY_ID,
+                    DataLakeConfig.CATALOG_ACCESS_KEY_SECRET,
+                    DataLakeConfig.CATALOG_SECURITY_TOKEN);
+        }
+        return credential;
     }
 }
