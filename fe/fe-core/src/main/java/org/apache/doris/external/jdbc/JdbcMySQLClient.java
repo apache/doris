7cad81fee1964fb47e80808c782f5c33ae5edb98
GoGoWen <82132356+GoGoWen@users.noreply.github.com>
2023-06-12 11:16:19 +0800

    [Feature] (Multi-Catalog) support query hll column in doris jdbc table - part 1 (#19413)
    Issue Number: close #17895

--- fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcMySQLClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/external/jdbc/JdbcMySQLClient.java
@@ -20,6 +20,7 @@ package org.apache.doris.external.jdbc;
 import org.apache.doris.catalog.PrimitiveType;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
+import org.apache.doris.common.util.Util;
 
 import avro.shaded.com.google.common.collect.Lists;
 
@@ -27,7 +28,10 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class JdbcMySQLClient extends JdbcClient {
@@ -74,6 +78,103 @@ public class JdbcMySQLClient extends JdbcClient {
     protected ResultSet getColumns(DatabaseMetaData databaseMetaData, String catalogName, String schemaName,
                                    String tableName) throws SQLException {
         return databaseMetaData.getColumns(schemaName, null, tableName, null);
+    }
+
+    /**
+     * get all columns like DatabaseMetaData.getColumns in mysql-jdbc-connector
+     */
+    private Map<String, String> getJdbcColumnsTypeInfo(String dbName, String tableName) {
+        Connection conn = getConnection();
+        ResultSet resultSet = null;
+        Map<String, String> fieldtoType = new HashMap<String, String>();
+
+        StringBuilder queryBuf = new StringBuilder("SHOW FULL COLUMNS FROM ");
+        queryBuf.append(tableName);
+        queryBuf.append(" FROM ");
+        queryBuf.append(dbName);
+        try (Statement stmt = conn.createStatement()) {
+            resultSet = stmt.executeQuery(queryBuf.toString());
+            while (resultSet.next()) {
+                // get column name
+                String fieldName = resultSet.getString("Field");
+                // get original type name
+                String typeName = resultSet.getString("Type");
+                fieldtoType.put(fieldName, typeName);
+            }
+        } catch (SQLException e) {
+            throw new JdbcClientException("failed to get column list from jdbc for table %s:%s", tableName,
+                Util.getRootCauseMessage(e));
+        } finally {
+            close(resultSet, conn);
+        }
+
+        return fieldtoType;
+    }
+
+    /**
+     * get all columns of one table
+     */
+    @Override
+    public List<JdbcFieldSchema> getJdbcColumnsInfo(String dbName, String tableName) {
+        Connection conn = getConnection();
+        ResultSet rs = null;
+        List<JdbcFieldSchema> tableSchema = com.google.common.collect.Lists.newArrayList();
+        // if isLowerCaseTableNames == true, tableName is lower case
+        // but databaseMetaData.getColumns() is case sensitive
+        if (isLowerCaseTableNames) {
+            dbName = lowerDBToRealDB.get(dbName);
+            tableName = lowerTableToRealTable.get(tableName);
+        }
+        try {
+            DatabaseMetaData databaseMetaData = conn.getMetaData();
+            String catalogName = getCatalogName(conn);
+            tableName = modifyTableNameIfNecessary(tableName);
+            rs = getColumns(databaseMetaData, catalogName, dbName, tableName);
+            boolean needGetDorisColumns = true;
+            Map<String, String> mapFieldtoType = null;
+            while (rs.next()) {
+                if (isTableModified(tableName, rs.getString("TABLE_NAME"))) {
+                    continue;
+                }
+                JdbcFieldSchema field = new JdbcFieldSchema();
+                field.setColumnName(rs.getString("COLUMN_NAME"));
+                field.setDataType(rs.getInt("DATA_TYPE"));
+
+                // in mysql-jdbc-connector-8.0.*, TYPE_NAME of the HLL column in doris will be "UNKNOWN"
+                // in mysql-jdbc-connector-5.1.*, TYPE_NAME of the HLL column in doris will be "HLL"
+                field.setDataTypeName(rs.getString("TYPE_NAME"));
+                if (rs.getString("TYPE_NAME").equalsIgnoreCase("UNKNOWN")) {
+                    if (needGetDorisColumns) {
+                        mapFieldtoType = getJdbcColumnsTypeInfo(dbName, tableName);
+                        needGetDorisColumns = false;
+                    }
+
+                    if (mapFieldtoType != null) {
+                        field.setDataTypeName(mapFieldtoType.get(rs.getString("COLUMN_NAME")));
+                    }
+                }
+
+                field.setColumnSize(rs.getInt("COLUMN_SIZE"));
+                field.setDecimalDigits(rs.getInt("DECIMAL_DIGITS"));
+                field.setNumPrecRadix(rs.getInt("NUM_PREC_RADIX"));
+                /*
+                   Whether it is allowed to be NULL
+                   0 (columnNoNulls)
+                   1 (columnNullable)
+                   2 (columnNullableUnknown)
+                 */
+                field.setAllowNull(rs.getInt("NULLABLE") != 0);
+                field.setRemarks(rs.getString("REMARKS"));
+                field.setCharOctetLength(rs.getInt("CHAR_OCTET_LENGTH"));
+                tableSchema.add(field);
+            }
+        } catch (SQLException e) {
+            throw new JdbcClientException("failed to get table name list from jdbc for table %s:%s", e, tableName,
+                Util.getRootCauseMessage(e));
+        } finally {
+            close(rs, conn);
+        }
+        return tableSchema;
     }
 
     @Override
@@ -170,6 +271,8 @@ public class JdbcMySQLClient extends JdbcClient {
             case "VARBINARY":
             case "ENUM":
                 return ScalarType.createStringType();
+            case "HLL":
+                return ScalarType.createHllType();
             default:
                 return Type.UNSUPPORTED;
         }
