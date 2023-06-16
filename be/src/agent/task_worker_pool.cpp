fe4d710c1600435214fa287e398310dc2a7bbd96
Chen Zhang <chzhang1987@gmail.com>
2023-06-16 19:18:42 +0800

    update totoal publish task time

--- be/src/agent/task_worker_pool.cpp
+++ be/src/agent/task_worker_pool.cpp
@@ -1521,7 +1521,8 @@ void PublishVersionTaskPool::_publish_version_worker_thread_callback() {
             LOG_INFO("successfully publish version")
                     .tag("signature", agent_task_req.signature)
                     .tag("transaction_id", publish_version_req.transaction_id)
-                    .tag("tablets_num", succ_tablet_ids.size());
+                    .tag("tablets_num", succ_tablet_ids.size())
+                    .tag("cost(s)", time(nullptr) - agent_task_req.recv_time);
         }
 
         status.to_thrift(&finish_task_request.task_status);
