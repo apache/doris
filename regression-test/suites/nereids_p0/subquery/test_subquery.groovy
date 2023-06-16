c755a9327b3b0d5a0d199fbead4019878ce86487
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-06-16 16:11:14 +0800

    [Enhance](regression-test) use another db  than test_query_db for nereids_p0 (#19467)
    replace test_query_db to nereids_test_query_db in nereids_p0 directory
    split test_join into five files to run faster.

--- regression-test/suites/nereids_p0/subquery/test_subquery.groovy
+++ regression-test/suites/nereids_p0/subquery/test_subquery.groovy
@@ -22,7 +22,7 @@ suite("test_subquery") {
             select c1, c3, m2 from 
                 (select c1, c3, max(c2) m2 from 
                     (select c1, c2, c3 from 
-                        (select k3 c1, k2 c2, max(k1) c3 from test_query_db.test 
+                        (select k3 c1, k2 c2, max(k1) c3 from nereids_test_query_db.test 
                          group by 1, 2 order by 1 desc, 2 desc limit 5) x 
                     ) x2 group by c1, c3 limit 10
                 ) t 
@@ -30,17 +30,17 @@ suite("test_subquery") {
             """
 
         qt_sql2 """
-        with base as (select k1, k2 from test_query_db.test as t where k1 in (select k1 from test_query_db.baseall
+        with base as (select k1, k2 from nereids_test_query_db.test as t where k1 in (select k1 from nereids_test_query_db.baseall
         where k7 = 'wangjuoo4' group by 1 having count(distinct k7) > 0)) select * from base limit 10;
         """
 
         qt_sql3 """
-        SELECT k1 FROM test_query_db.test GROUP BY k1 HAVING k1 IN (SELECT k1 FROM test_query_db.baseall WHERE
-        k2 >= (SELECT min(k3) FROM test_query_db.bigtable WHERE k2 = baseall.k2)) order by k1;
+        SELECT k1 FROM nereids_test_query_db.test GROUP BY k1 HAVING k1 IN (SELECT k1 FROM nereids_test_query_db.baseall WHERE
+        k2 >= (SELECT min(k3) FROM nereids_test_query_db.bigtable WHERE k2 = baseall.k2)) order by k1;
         """
 
         qt_sql4 """
         select /*+SET_VAR(enable_projection=false) */
-        count() from (select k2, k1 from test_query_db.baseall order by k1 limit 1) a;
+        count() from (select k2, k1 from nereids_test_query_db.baseall order by k1 limit 1) a;
         """
 }
