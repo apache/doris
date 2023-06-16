5bfa1c9ba37d65d549a940958af33ddac1ad82c6
Chen Zhang <chzhang1987@gmail.com>
2023-06-16 14:03:55 +0800

    reformat code

--- be/src/olap/delta_writer.cpp
+++ be/src/olap/delta_writer.cpp
@@ -456,8 +456,8 @@ Status DeltaWriter::close_wait(const PSlaveTabletNodes& slave_tablet_nodes,
         }
         int64_t cur_max_version = _tablet->max_version().second;
         RETURN_IF_ERROR(_tablet->commit_phase_update_delete_bitmap(
-                _cur_rowset, _rowset_ids, _delete_bitmap, cur_max_version, segments,
-                _req.txn_id, _rowset_writer.get()));
+                _cur_rowset, _rowset_ids, _delete_bitmap, cur_max_version, segments, _req.txn_id,
+                _rowset_writer.get()));
         _rowset_ids = _tablet->all_rs_id(cur_max_version);
     }
     Status res = _storage_engine->txn_manager()->commit_txn(_req.partition_id, _tablet, _req.txn_id,
