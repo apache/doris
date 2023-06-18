df8a80d739d43a8441da8e09b323d497657a8bbe
Xin Liao <liaoxinbit@126.com>
2023-06-18 19:26:02 +0800

    [bugfix](merge-on-write) optimize rowset tree and tablet header lock (#20911)

--- be/src/service/point_query_executor.cpp
+++ be/src/service/point_query_executor.cpp
@@ -246,6 +246,11 @@ Status PointQueryExecutor::_lookup_row_key() {
     // 2. lookup row location
     Status st;
     std::unordered_map<RowsetId, SegmentCacheHandle, HashOfRowsetId> segment_caches;
+    std::vector<RowsetSharedPtr> specified_rowsets;
+    {
+        std::shared_lock rlock(_tablet->get_header_lock());
+        specified_rowsets = _tablet->get_rowset_by_ids(nullptr);
+    }
     for (size_t i = 0; i < _row_read_ctxs.size(); ++i) {
         RowLocation location;
         if (!config::disable_storage_row_cache) {
@@ -260,8 +265,9 @@ Status PointQueryExecutor::_lookup_row_key() {
         }
         // Get rowlocation and rowset, ctx._rowset_ptr will acquire wrap this ptr
         auto rowset_ptr = std::make_unique<RowsetSharedPtr>();
-        st = (_tablet->lookup_row_key(_row_read_ctxs[i]._primary_key, true, nullptr, &location,
-                                      INT32_MAX /*rethink?*/, segment_caches, rowset_ptr.get()));
+        st = (_tablet->lookup_row_key(_row_read_ctxs[i]._primary_key, true, specified_rowsets,
+                                      &location, INT32_MAX /*rethink?*/, segment_caches,
+                                      rowset_ptr.get()));
         if (st.is_not_found()) {
             continue;
         }
