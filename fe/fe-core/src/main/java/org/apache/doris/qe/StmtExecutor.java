09a6c66d50285ec0235615f3047547263d510560
morrySnow <morrysnow@126.com>
2023-06-18 21:34:15 +0800

    [opt](Nereids) change log level to debug to avoid log explode

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -422,14 +422,14 @@ public class StmtExecutor {
                         MinidumpUtils.saveMinidumpString(context.getMinidump(), DebugUtil.printId(context.queryId()));
                     }
                     // try to fall back to legacy planner
-                    LOG.warn("nereids cannot process statement\n" + originStmt.originStmt
+                    LOG.debug("nereids cannot process statement\n" + originStmt.originStmt
                             + "\n because of " + e.getMessage(), e);
                     if (e instanceof NereidsException
                             && !context.getSessionVariable().enableFallbackToOriginalPlanner) {
                         LOG.warn("Analyze failed. {}", context.getQueryIdentifier(), e);
                         throw ((NereidsException) e).getException();
                     }
-                    LOG.info("fall back to legacy planner");
+                    LOG.debug("fall back to legacy planner on statement:\n{}", originStmt.originStmt);
                     parsedStmt = null;
                     context.getState().setNereids(false);
                     executeByLegacy(queryId);
@@ -475,7 +475,7 @@ public class StmtExecutor {
     }
 
     private void executeByNereids(TUniqueId queryId) throws Exception {
-        LOG.info("Nereids start to execute query:\n {}", originStmt.originStmt);
+        LOG.debug("Nereids start to execute query:\n {}", originStmt.originStmt);
         context.setQueryId(queryId);
         context.setStartTime();
         profile.getSummaryProfile().setQueryBeginTime();
@@ -493,7 +493,7 @@ public class StmtExecutor {
                 if (isForwardToMaster()) {
                     if (isProxy) {
                         // This is already a stmt forwarded from other FE.
-                        // If goes here, which means we can't find a valid Master FE(some error happens).
+                        // If we goes here, means we can't find a valid Master FE(some error happens).
                         // To avoid endless forward, throw exception here.
                         throw new NereidsException(new UserException("The statement has been forwarded to master FE("
                                 + Env.getCurrentEnv().getSelfNode().getHost() + ") and failed to execute"
@@ -509,17 +509,17 @@ public class StmtExecutor {
             try {
                 ((Command) logicalPlan).run(context, this);
             } catch (QueryStateException e) {
-                LOG.warn("", e);
+                LOG.debug("DDL statement(" + originStmt.originStmt + ") process failed.", e);
                 context.setState(e.getQueryState());
-                throw new NereidsException(e);
+                throw new NereidsException("DDL statement(" + originStmt.originStmt + ") process failed", e);
             } catch (UserException e) {
                 // Return message to info client what happened.
-                LOG.warn("DDL statement({}) process failed.", originStmt.originStmt, e);
+                LOG.debug("DDL statement(" + originStmt.originStmt + ") process failed.", e);
                 context.getState().setError(e.getMysqlErrorCode(), e.getMessage());
                 throw new NereidsException("DDL statement(" + originStmt.originStmt + ") process failed", e);
             } catch (Exception e) {
                 // Maybe our bug
-                LOG.warn("DDL statement(" + originStmt.originStmt + ") process failed.", e);
+                LOG.debug("DDL statement(" + originStmt.originStmt + ") process failed.", e);
                 context.getState().setError(ErrorCode.ERR_UNKNOWN_ERROR, "Unexpected exception: " + e.getMessage());
                 throw new NereidsException("DDL statement(" + originStmt.originStmt + ") process failed.", e);
             }
@@ -533,7 +533,7 @@ public class StmtExecutor {
             try {
                 planner.plan(parsedStmt, context.getSessionVariable().toThrift());
             } catch (Exception e) {
-                LOG.warn("Nereids plan query failed:\n{}", originStmt.originStmt);
+                LOG.debug("Nereids plan query failed:\n{}", originStmt.originStmt);
                 throw new NereidsException(new AnalysisException("Unexpected exception: " + e.getMessage(), e));
             }
             profile.getSummaryProfile().setQueryPlanFinishTime();
