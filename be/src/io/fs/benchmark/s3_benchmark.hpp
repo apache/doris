3cfe0b3c6b96fb6cee2cc0d345666e40f200bd5a
kaka11chen <kaka11.chen@gmail.com>
2023-06-19 11:52:28 +0800

    Init commit.

--- be/src/io/fs/benchmark/s3_benchmark.hpp
+++ be/src/io/fs/benchmark/s3_benchmark.hpp
@@ -27,8 +27,9 @@ namespace doris::io {
 
 class S3ReadBenchmark : public BaseBenchmark {
 public:
-    S3ReadBenchmark(int iterations, const std::map<std::string, std::string>& conf_map)
-            : BaseBenchmark("S3ReadBenchmark", iterations, conf_map), _result(buffer, 128) {}
+    S3ReadBenchmark(int threads, int iterations, const std::map<std::string, std::string>& conf_map)
+            : BaseBenchmark("S3ReadBenchmark", threads, iterations, conf_map),
+              _result(buffer, 128) {}
     virtual ~S3ReadBenchmark() = default;
 
     Status init() override {
@@ -41,7 +42,9 @@ public:
         return Status::OK();
     }
 
-    Status run() override { return _reader->read_at(0, _result, &_bytes_read); }
+    Status run(benchmark::State& state) override {
+        return _reader->read_at(0, _result, &_bytes_read);
+    }
 
 private:
     doris::S3Conf _s3_conf;
