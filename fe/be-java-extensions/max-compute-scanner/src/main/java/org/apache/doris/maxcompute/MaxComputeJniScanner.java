1cb674332b5dd3ada5642015dc11fb1ff3c77ba4
slothever <jinzhe@selectdb.com>
2023-06-19 22:02:37 +0800

    fix parallel

--- fe/be-java-extensions/max-compute-scanner/src/main/java/org/apache/doris/maxcompute/MaxComputeJniScanner.java
+++ fe/be-java-extensions/max-compute-scanner/src/main/java/org/apache/doris/maxcompute/MaxComputeJniScanner.java
@@ -22,9 +22,7 @@ import org.apache.doris.common.jni.vec.ColumnType;
 import org.apache.doris.common.jni.vec.ScanPredicate;
 
 import com.aliyun.odps.Column;
-import com.aliyun.odps.Odps;
 import com.aliyun.odps.OdpsType;
-import com.aliyun.odps.account.AliyunAccount;
 import com.aliyun.odps.data.ArrowRecordReader;
 import com.aliyun.odps.tunnel.TableTunnel;
 import com.aliyun.odps.type.TypeInfo;
@@ -42,17 +40,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * MaxComputeJ JniScanner. BE will read data from the scanner object.
  */
 public class MaxComputeJniScanner extends JniScanner {
-    private Odps odps;
-    private TableTunnel tunnel;
-
     private static final Logger LOG = Logger.getLogger(MaxComputeJniScanner.class);
-    private static final String odpsUrlTemplate = "http://service.{}.maxcompute.aliyun.com/api";
-    private static final String tunnelUrlTemplate = "http://dt.{}.maxcompute.aliyun-inc.com";
     private static final String REGION = "region";
     private static final String PROJECT = "project";
     private static final String TABLE = "table";
@@ -61,40 +55,28 @@ public class MaxComputeJniScanner extends JniScanner {
     private static final String START_OFFSET = "start_offset";
     private static final String SPLIT_SIZE = "split_size";
     private static final String PUBLIC_ACCESS = "public_access";
+    private static final Map<String, MaxComputeTableScan> tableScans = new ConcurrentHashMap<>();
+    private final String region;
     private final String project;
     private final String table;
+    private final MaxComputeTableScan curTableScan;
     private MaxComputeColumnValue columnValue;
     private long remainBatchRows = 0;
     private long totalRows = 0;
-    private TableTunnel.DownloadSession session;
     private RootAllocator arrowAllocator;
     private ArrowRecordReader curReader;
-    private List<Column> columns;
-    private Map<String, Integer> readColumnsId;
+    private List<Column> readColumns;
+    private Map<String, Integer> readColumnsToId;
     private long startOffset = -1L;
     private long splitSize = -1L;
 
     public MaxComputeJniScanner(int batchSize, Map<String, String> params) {
-        String region = Objects.requireNonNull(params.get(REGION), "required property '" + REGION + "'.");
+        region = Objects.requireNonNull(params.get(REGION), "required property '" + REGION + "'.");
         project = Objects.requireNonNull(params.get(PROJECT), "required property '" + PROJECT + "'.");
         table = Objects.requireNonNull(params.get(TABLE), "required property '" + TABLE + "'.");
-        if (!Strings.isNullOrEmpty(params.get(START_OFFSET))
-                && !Strings.isNullOrEmpty(params.get(SPLIT_SIZE))) {
-            startOffset = Long.parseLong(params.get(START_OFFSET));
-            splitSize = Long.parseLong(params.get(SPLIT_SIZE));
-        }
-        String accessKey = Objects.requireNonNull(params.get(ACCESS_KEY), "required property '" + ACCESS_KEY + "'.");
-        String secretKey = Objects.requireNonNull(params.get(SECRET_KEY), "required property '" + SECRET_KEY + "'.");
-        odps = new Odps(new AliyunAccount(accessKey, secretKey));
-        odps.setEndpoint(odpsUrlTemplate.replace("{}", region));
-        odps.setDefaultProject(project);
-        tunnel = new TableTunnel(odps);
-        String tunnelUrl = tunnelUrlTemplate.replace("{}", region);
-        boolean enablePublicAccess = Boolean.parseBoolean(params.getOrDefault(PUBLIC_ACCESS, "false"));
-        if (enablePublicAccess) {
-            tunnelUrl = tunnelUrlTemplate.replace("-inc", "");
-        }
-        tunnel.setEndpoint(tunnelUrl);
+        tableScans.putIfAbsent(tableUniqKey(), newTableScan(params));
+        curTableScan = tableScans.get(tableUniqKey());
+
         String[] requiredFields = params.get("required_fields").split(",");
         String[] types = params.get("columns_types").split("#");
         ColumnType[] columnTypes = new ColumnType[types.length];
@@ -112,45 +94,60 @@ public class MaxComputeJniScanner extends JniScanner {
         initTableInfo(columnTypes, requiredFields, predicates, batchSize);
     }
 
+    private MaxComputeTableScan newTableScan(Map<String, String> params) {
+        if (!Strings.isNullOrEmpty(params.get(START_OFFSET))
+                && !Strings.isNullOrEmpty(params.get(SPLIT_SIZE))) {
+            startOffset = Long.parseLong(params.get(START_OFFSET));
+            splitSize = Long.parseLong(params.get(SPLIT_SIZE));
+        }
+        String accessKey = Objects.requireNonNull(params.get(ACCESS_KEY), "required property '" + ACCESS_KEY + "'.");
+        String secretKey = Objects.requireNonNull(params.get(SECRET_KEY), "required property '" + SECRET_KEY + "'.");
+        boolean enablePublicAccess = Boolean.parseBoolean(params.getOrDefault(PUBLIC_ACCESS, "false"));
+        return new MaxComputeTableScan(region, project, table, accessKey, secretKey, enablePublicAccess);
+    }
+
+    public String tableUniqKey() {
+        return region + "#" + project + "." + table;
+    }
+
     @Override
     protected void initTableInfo(ColumnType[] requiredTypes, String[] requiredFields, ScanPredicate[] predicates,
                                  int batchSize) {
         super.initTableInfo(requiredTypes, requiredFields, predicates, batchSize);
-        columns = new ArrayList<>();
-        readColumnsId = new HashMap<>();
+        readColumns = new ArrayList<>();
+        readColumnsToId = new HashMap<>();
         for (int i = 0; i < fields.length; i++) {
             if (!Strings.isNullOrEmpty(fields[i])) {
-                columns.add(createOdpsColumn(i, types[i]));
-                readColumnsId.put(fields[i], i);
+                readColumns.add(createOdpsColumn(i, types[i]));
+                readColumnsToId.put(fields[i], i);
             }
         }
         // reorder columns
-        List<Column> columnList = odps.tables().get(table).getSchema().getColumns();
+        List<Column> columnList = curTableScan.getSchema().getColumns();
         Map<String, Integer> columnRank = new HashMap<>();
         for (int i = 0; i < columnList.size(); i++) {
             columnRank.put(columnList.get(i).getName(), i);
         }
         // Downloading columns data from Max compute only supports the order of table metadata.
         // We might get an error message if no sort here: Column reorder is not supported in legacy arrow mode.
-        columns.sort((Comparator.comparing(o -> columnRank.get(o.getName()))));
+        readColumns.sort((Comparator.comparing(o -> columnRank.get(o.getName()))));
     }
 
     @Override
     public void open() throws IOException {
-        if (columns.isEmpty()) {
+        if (readColumns.isEmpty()) {
             return;
         }
         try {
-            session = tunnel.createDownloadSession(project, table);
-            if (splitSize > 0) {
-                totalRows = Math.min(splitSize, session.getRecordCount());
-            } else {
-                totalRows = session.getRecordCount();
-            }
+            TableTunnel.DownloadSession session = curTableScan.getSession();
             long start = startOffset == -1L ? 0 : startOffset;
+            long recordCount = session.getRecordCount();
+            totalRows = splitSize > 0 ? Math.min(splitSize, recordCount) : recordCount;
+
             arrowAllocator = new RootAllocator(Long.MAX_VALUE);
-            curReader = session.openArrowRecordReader(start, totalRows, columns, arrowAllocator);
+            curReader = session.openArrowRecordReader(start, totalRows, readColumns, arrowAllocator);
         } catch (Exception e) {
+            close();
             throw new IOException(e);
         }
         remainBatchRows = totalRows;
@@ -209,6 +206,11 @@ public class MaxComputeJniScanner extends JniScanner {
 
     @Override
     public void close() throws IOException {
+        String tableName = tableUniqKey();
+        MaxComputeTableScan scan = tableScans.get(tableName);
+        if (scan != null && scan.endOfScan()) {
+            tableScans.remove(tableName);
+        }
         remainBatchRows = 0;
         totalRows = 0;
         startOffset = -1;
@@ -232,6 +234,7 @@ public class MaxComputeJniScanner extends JniScanner {
             return 0;
         }
         remainBatchRows -= realRows;
+        curTableScan.increaseReadRows(realRows);
         return realRows;
     }
 
@@ -246,7 +249,7 @@ public class MaxComputeJniScanner extends JniScanner {
                     columnValue.reset(column);
                     batchRows = column.getValueCount();
                     for (int j = 0; j < batchRows; j++) {
-                        appendData(readColumnsId.get(column.getName()), columnValue);
+                        appendData(readColumnsToId.get(column.getName()), columnValue);
                     }
                 }
                 curReadRows += batchRows;
