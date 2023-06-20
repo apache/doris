d2507e9a809b9517643e96776de545e7477aac3d
zy-kkk <zhongyk10@gmail.com>
2023-06-20 10:39:34 +0800

    [improvement](jdbc mysql) support auto calculate the precision of timestamp/datetime (#20788)

--- docker/thirdparties/docker-compose/mysql/init/03-create-table.sql
+++ docker/thirdparties/docker-compose/mysql/init/03-create-table.sql
@@ -290,3 +290,13 @@ CREATE TABLE `doris_test`.`auto_default_t` (
     `dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) engine=innodb charset=utf8;
+
+create table doris_test.dt (
+  `timestamp0` timestamp(0),
+  `timestamp1` timestamp(1),
+  `timestamp2` timestamp(2),
+  `timestamp3` timestamp(3),
+  `timestamp4` timestamp(4),
+  `timestamp5` timestamp(5),
+  `timestamp6` timestamp(6)
+) engine=innodb charset=utf8;
