0f4baedd8d93e670d046f69bdf335b6ea8f9f049
AKIRA <33112463+Kikyou1997@users.noreply.github.com>
2023-06-20 16:11:03 +0900

    [enhancement](nereids) log for exception stack of sync analyze (#21013)

--- fe/fe-core/src/main/java/org/apache/doris/statistics/AnalysisManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/AnalysisManager.java
@@ -795,7 +795,7 @@ public class AnalysisManager extends Daemon implements Writable {
                 } catch (Throwable t) {
                     colNames.add(task.info.colName);
                     updateSyncTaskStatus(task, AnalysisState.FAILED);
-                    LOG.info("Failed to analyze, info: {}", task);
+                    LOG.warn("Failed to analyze, info: {}", task, t);
                 }
             }
             if (!colNames.isEmpty()) {
