5d2f5e786cadbbacf53c3fab93fcfd0250a4d34f
Kang <kxiao.tiger@gmail.com>
2023-06-22 15:57:51 +0800

    change version to 2.0-beta

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -28,12 +28,12 @@
 set -eo pipefail
 
 build_version_prefix="doris"
-build_version_major=0
+build_version_major=2
 build_version_minor=0
 build_version_patch=0
-build_version_rc_version="trunk"
+build_version_rc_version="beta"
 
-build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}-${build_version_rc_version}"
+build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}-${build_version_rc_version}"
 
 unset LANG
 unset LC_CTYPE
