2a14fca2e7e35e1ddfc0b2ecc1d099abe6b678b3
Siyang Tang <82279870+TangSiyang2001@users.noreply.github.com>
2023-06-25 17:45:31 +0800

    [feature](load-refactor-with-tvf) S3 load with S3 tvf and native insert (#19937)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
@@ -98,11 +98,11 @@ public class NativeInsertStmt extends InsertStmt {
     private static final String SHUFFLE_HINT = "SHUFFLE";
     private static final String NOSHUFFLE_HINT = "NOSHUFFLE";
 
-    private final TableName tblName;
+    protected final TableName tblName;
     private final PartitionNames targetPartitionNames;
     // parsed from targetPartitionNames.
     private List<Long> targetPartitionIds;
-    private final List<String> targetColumnNames;
+    protected List<String> targetColumnNames;
     private QueryStmt queryStmt;
     private final List<String> planHints;
     private Boolean isRepartition;
@@ -113,7 +113,7 @@ public class NativeInsertStmt extends InsertStmt {
 
     private final Map<String, Expr> exprByName = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
 
-    private Table targetTable;
+    protected Table targetTable;
 
     private DatabaseIf db;
     private long transactionId;
@@ -254,8 +254,7 @@ public class NativeInsertStmt extends InsertStmt {
         return isTransactionBegin;
     }
 
-    @Override
-    public void analyze(Analyzer analyzer) throws UserException {
+    protected void preCheckAnalyze(Analyzer analyzer) throws UserException {
         super.analyze(analyzer);
 
         if (targetTable == null) {
@@ -279,6 +278,20 @@ public class NativeInsertStmt extends InsertStmt {
         if (targetPartitionNames != null) {
             targetPartitionNames.analyze(analyzer);
         }
+    }
+
+    /**
+     * translate load related stmt to`insert into xx select xx from tvf` semantic
+     */
+    protected void convertSemantic(Analyzer analyzer) throws UserException {
+        // do nothing
+    }
+
+    @Override
+    public void analyze(Analyzer analyzer) throws UserException {
+        preCheckAnalyze(analyzer);
+
+        convertSemantic(analyzer);
 
         // set target table and
         analyzeTargetTable(analyzer);
@@ -321,8 +334,7 @@ public class NativeInsertStmt extends InsertStmt {
         }
     }
 
-    private void analyzeTargetTable(Analyzer analyzer) throws AnalysisException {
-        // Get table
+    protected void initTargetTable(Analyzer analyzer) throws AnalysisException {
         if (targetTable == null) {
             DatabaseIf db = analyzer.getEnv().getCatalogMgr()
                     .getCatalog(tblName.getCtl()).getDbOrAnalysisException(tblName.getDb());
@@ -335,6 +347,11 @@ public class NativeInsertStmt extends InsertStmt {
                 throw new AnalysisException("Not support insert target table.");
             }
         }
+    }
+
+    private void analyzeTargetTable(Analyzer analyzer) throws AnalysisException {
+        // Get table
+        initTargetTable(analyzer);
 
         if (targetTable instanceof OlapTable) {
             OlapTable olapTable = (OlapTable) targetTable;
@@ -888,10 +905,5 @@ public class NativeInsertStmt extends InsertStmt {
         } else {
             return RedirectStatus.FORWARD_WITH_SYNC;
         }
-    }
-
-    @Override
-    public String toSql() {
-        return null;
     }
 }
