b714c72e593f8fad7d4cb6fccf4bfe129f163e1f
shuke <37901441+shuke987@users.noreply.github.com>
2023-06-25 18:53:20 +0800

    [fix](test) fix two case bug #21124

--- regression-test/suites/mv_p0/multi_slot_k123p/multi_slot_k123p.groovy
+++ regression-test/suites/mv_p0/multi_slot_k123p/multi_slot_k123p.groovy
@@ -50,6 +50,7 @@ suite ("multi_slot_k123p") {
         file 'multi_slot_k123p.csv'
         time 10000
     }
+    sql """sync"""
     qt_select_star "select * from d_table order by k1,k4;"
 
     explain {
