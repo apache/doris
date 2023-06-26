91781873a38646f1371fb0758f91aa14711f428d
airborne12 <airborne12@gmail.com>
2023-06-26 15:32:38 +0800

    [Enchancement](inverted index) do origin compaction when index compaction fail

--- be/src/olap/compaction.cpp
+++ be/src/olap/compaction.cpp
@@ -396,13 +396,16 @@ Status Compaction::do_compaction_impl(int64_t permits) {
                   << ", destination index size=" << dest_segment_num << ".";
         std::for_each(
                 ctx.skip_inverted_index.cbegin(), ctx.skip_inverted_index.cend(),
-                [&src_segment_num, &dest_segment_num, &index_writer_path, &src_index_files,
+                [&ctx, &src_segment_num, &dest_segment_num, &index_writer_path, &src_index_files,
                  &dest_index_files, &fs, &tablet_path, &trans_vec, &dest_segment_num_rows,
                  this](int32_t column_uniq_id) {
-                    compact_column(
+                    auto st = compact_column(
                             _cur_tablet_schema->get_inverted_index(column_uniq_id)->index_id(),
                             src_segment_num, dest_segment_num, src_index_files, dest_index_files,
                             fs, index_writer_path, tablet_path, trans_vec, dest_segment_num_rows);
+                    if (!st.ok()) {
+                        ctx.skip_inverted_index.erase(column_uniq_id);
+                    }
                 });
 
         LOG(INFO) << "succeed to do index compaction"
