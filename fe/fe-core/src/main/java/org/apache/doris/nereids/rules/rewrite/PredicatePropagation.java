361b5b610bd768829c23954e658266951c03790b
LiBinfeng <1204975323@qq.com>
2023-06-26 14:16:18 +0800

    [Fix](Nereids) Add cast comparison with slot reference when inferring predicate

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PredicatePropagation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PredicatePropagation.java
@@ -17,6 +17,7 @@
 
 package org.apache.doris.nereids.rules.rewrite;
 
+import org.apache.doris.nereids.trees.expressions.Cast;
 import org.apache.doris.nereids.trees.expressions.ComparisonPredicate;
 import org.apache.doris.nereids.trees.expressions.EqualTo;
 import org.apache.doris.nereids.trees.expressions.Expression;
@@ -70,19 +71,36 @@ public class PredicatePropagation {
 
             @Override
             public Expression visitComparisonPredicate(ComparisonPredicate cp, Void context) {
-                if (cp.left().isSlot() && cp.right().isConstant()) {
+                if ((cp.left().isSlot() || (cp.left() instanceof Cast && cp.left().child(0).isSlot()))
+                        && (cp.right().isConstant())) {
                     return replaceSlot(cp);
-                } else if (cp.left().isConstant() && cp.right().isSlot()) {
+                } else if ((cp.right().isSlot() || (cp.right() instanceof Cast && cp.right().child(0).isSlot()))
+                        && cp.left().isConstant()) {
                     return replaceSlot(cp);
                 }
                 return super.visit(cp, context);
             }
 
+            private boolean isTwoExpressionEqualWithCast(Expression left, Expression right) {
+                if (left.getDataType() != right.getDataType()) {
+                    return false;
+                }
+                if (left instanceof Cast && right instanceof Cast) {
+                    return ((Cast) left).child().equals(((Cast) right).child());
+                } else if (left instanceof Cast) {
+                    return ((Cast) left).child().equals(right);
+                } else if (right instanceof Cast) {
+                    return ((Cast) right).child().equals(left);
+                } else {
+                    return left.equals(right);
+                }
+            }
+
             private Expression replaceSlot(Expression expr) {
                 return expr.rewriteUp(e -> {
-                    if (e.equals(leftSlotEqualToRightSlot.child(0))) {
+                    if (isTwoExpressionEqualWithCast(e, leftSlotEqualToRightSlot.child(0))) {
                         return leftSlotEqualToRightSlot.child(1);
-                    } else if (e.equals(leftSlotEqualToRightSlot.child(1))) {
+                    } else if (isTwoExpressionEqualWithCast(e, leftSlotEqualToRightSlot.child(1))) {
                         return leftSlotEqualToRightSlot.child(0);
                     } else {
                         return e;
@@ -98,7 +116,8 @@ public class PredicatePropagation {
      */
     private boolean canEquivalentInfer(Expression predicate) {
         return predicate instanceof EqualTo
-                && predicate.children().stream().allMatch(e -> e instanceof SlotReference)
+                && predicate.children().stream().allMatch(e ->
+                    (e instanceof SlotReference) || (e instanceof Cast && e.child(0).isSlot()))
                 && predicate.child(0).getDataType().equals(predicate.child(1).getDataType());
     }
 
