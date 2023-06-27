e228dce63b8c16319d97769308599004a555d727
王翔宇 <wangxiangyu@360shuke.com>
2023-06-27 17:35:51 +0800

    [Enhancement](heartbeat) make heartbeat ok when config repeated host-ip pairs.

--- be/src/util/network_util.cpp
+++ be/src/util/network_util.cpp
@@ -99,10 +99,9 @@ Status hostname_to_ip_addrs(const std::string& name, std::vector<std::string>* a
         // add address if not exists
         std::string address = std::string(addr_buf);
         if (std::find(addresses->begin(), addresses->end(), address) != addresses->end()) {
-            LOG(WARNING)
-                    << "Repeated ip addresses has been found for host: " << name
-                    << ", ip address:" << address
-                    << ", please check your network configuration";
+            LOG(WARNING) << "Repeated ip addresses has been found for host: " << name
+                         << ", ip address:" << address
+                         << ", please check your network configuration";
         } else {
             addresses->push_back(address);
         }
