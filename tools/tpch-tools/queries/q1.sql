7075bcc52645e8953c982d884f075fe27debe0a9
Gabriel <gabrielleebuaa@gmail.com>
2023-07-01 16:14:36 +0800

    [tools](refactor) remove unused session variables (#21405)

--- tools/tpch-tools/queries/q1.sql
+++ tools/tpch-tools/queries/q1.sql
@@ -15,7 +15,7 @@
 -- specific language governing permissions and limitations
 -- under the License.
 
-select /*+SET_VAR(enable_nereids_planner=true,enable_pipeline_engine=true) */
+select
     l_returnflag,
     l_linestatus,
     sum(l_quantity) as sum_qty,
--- tools/tpch-tools/queries/q21.sql
+++ tools/tpch-tools/queries/q21.sql
@@ -15,7 +15,7 @@
 -- specific language governing permissions and limitations
 -- under the License.
 
-select /*+SET_VAR(enable_nereids_planner=true,enable_pipeline_engine=true) */
+select
     s_name,
     count(*) as numwait
 from
