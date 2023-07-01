bca7edbc5851d3bdcc16b2fb9723a8017ef593f1
htyoung <hty551020@hotmail.com>
2023-07-01 08:39:38 +0800

    [fix](truncate) it will directly return and avoid throwing IllegalStateException caused by bufferSize equals zero when table has no partition (#21378)
    if table currently has no partition, the truncate SQL will be a empty command, it should directly return and avoid throwing IllegalStateException caused by bufferSize equals zero
    
    Issue Number: close #21316
    Co-authored-by: tongyang.han <tongyang.han@jiduauto.com>

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -2565,6 +2565,11 @@ public class InternalCatalog implements CatalogIf<Database> {
                     partitionsDistributionInfo.put(partition.getId(), partition.getDistributionInfo());
                 }
             }
+            // if table currently has no partitions, this sql like empty command and do nothing, should return directly
+            // at the same time, it will avoid throwing IllegalStateException when `bufferSize` equals zero
+            if (origPartitions.isEmpty()) {
+                return;
+            }
             copiedTbl = olapTable.selectiveCopy(origPartitions.keySet(), IndexExtState.VISIBLE, false);
         } finally {
             olapTable.readUnlock();
