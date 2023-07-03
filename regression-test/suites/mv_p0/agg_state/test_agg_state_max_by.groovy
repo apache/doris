3b12e7bf810a48479d18f456c01d187b62788ab6
Pxl <pxl290@qq.com>
2023-07-03 10:19:31 +0800

    [Feature](materialized view) support query match mv with agg_state on nereids planner (#21067)
    * support create mv contain aggstate column
    
    * update
    
    * update
    
    * update
    
    * support query match mv with agg_state on nereids planner
    
    update
    
    * update
    
    * update

--- regression-test/suites/mv_p0/agg_state/test_agg_state_max_by.groovy
+++ regression-test/suites/mv_p0/agg_state/test_agg_state_max_by.groovy
@@ -36,20 +36,45 @@ suite ("test_agg_state_max_by") {
         """
 
     sql "insert into d_table select 1,1,1,'a';"
-    sql "insert into d_table select 2,2,2,'b';"
-    sql "insert into d_table select 3,-3,null,'c';"
+    sql "insert into d_table select 1,2,2,'b';"
+    sql "insert into d_table select 1,-3,null,'c';"
     sql "insert into d_table(k4,k2) values('d',4);"
 
     createMV("create materialized view k1mb as select k1,max_by(k2,k3) from d_table group by k1;")
 
-    sql "insert into d_table select -4,-4,-4,'d';"
+    sql "insert into d_table select 1,-4,-4,'d';"
 
-    qt_select_star "select * from d_table order by k1;"
-/*
+    qt_select_star "select * from d_table order by 1,2;"
     explain {
-        sql("select k1,max_by(k2,k3) from d_table group by k1 order by k1;")
+        sql("select k1,max_by(k2,k3) from d_table group by k1 order by 1,2;")
         contains "(k1mb)"
     }
-    qt_select_mv "select k1,max_by(k2,k3) from d_table group by k1 order by k1;"
-*/
+    qt_select_mv "select k1,max_by(k2,k3) from d_table group by k1 order by 1,2;"
+
+    createMV("create materialized view k1mbcp1 as select k1,max_by(k2+k3,abs(k3)) from d_table group by k1;")
+    createMV("create materialized view k1mbcp2 as select k1,max_by(k2+k3,k3) from d_table group by k1;")
+    createMV("create materialized view k1mbcp3 as select k1,max_by(k2,abs(k3)) from d_table group by k1;")
+
+    sql "insert into d_table(k4,k2) values('d',4);"
+    sql "set enable_nereids_dml = true"
+    sql "insert into d_table(k4,k2) values('d',4);"
+    sql "insert into d_table select 1,-4,-4,'d';"
+
+    explain {
+        sql("select k1,max_by(k2+k3,abs(k3)) from d_table group by k1 order by 1,2;")
+        contains "(k1mbcp1)"
+    }
+    qt_select_mv "select k1,max_by(k2+k3,k3) from d_table group by k1 order by 1,2;"
+
+    explain {
+        sql("select k1,max_by(k2+k3,k3) from d_table group by k1 order by 1,2;")
+        contains "(k1mbcp2)"
+    }
+    qt_select_mv "select k1,max_by(k2+k3,k3) from d_table group by k1 order by 1,2;"
+
+    explain {
+        sql("select k1,max_by(k2,abs(k3)) from d_table group by k1 order by 1,2;")
+        contains "(k1mbcp3)"
+    }
+    qt_select_mv "select k1,max_by(k2,abs(k3)) from d_table group by k1 order by 1,2;"
 }
