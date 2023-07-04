e9a368a8d90a6c160e6b95606afbca341355c28c
Ashin Gau <AshinGau@users.noreply.github.com>
2023-07-04 17:04:49 +0800

    [opt](hudi) use spark bundle to read hudi data (#21260)
    Use spark-bundle to read hudi data instead of using hive-bundle to read hudi data.
    
    **Advantage** for using spark-bundle to read hudi data:
    1. The performance of spark-bundle is more than twice that of hive-bundle
    2. spark-bundle using `UnsafeRow` can reduce data copying and GC time of the jvm
    3. spark-bundle support `Time Travel`, `Incremental Read`, and `Schema Change`, these functions can be quickly ported to Doris
    
    **Disadvantage** for using spark-bundle to read hudi data:
    1. More dependencies make hudi-dependency.jar very cumbersome(from 138M -> 300M)
    2. spark-bundle only provides `RDD` interface and cannot be used directly

--- fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
+++ fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
@@ -21,7 +21,6 @@ import org.apache.doris.common.jni.JniScanner;
 import org.apache.doris.common.jni.utils.OffHeap;
 import org.apache.doris.common.jni.vec.ColumnType;
 import org.apache.doris.common.jni.vec.ScanPredicate;
-import org.apache.doris.common.jni.vec.TableSchema;
 
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.log4j.Logger;
@@ -139,12 +138,6 @@ public class PaimonJniScanner extends JniScanner {
             throw new RuntimeException(e);
         }
         return rows;
-    }
-
-    @Override
-    protected TableSchema parseTableSchema() throws UnsupportedOperationException {
-        // do nothing
-        return null;
     }
 
     private Catalog create(CatalogContext context) throws IOException {
