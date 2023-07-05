a5f092472886b74aa87de2e6697274e71438c72d
Yukang Lian <Yukang.Lian2022@gmail.com>
2023-07-05 23:02:44 +0800

    update

--- be/src/olap/full_compaction.cpp
+++ be/src/olap/full_compaction.cpp
@@ -48,7 +48,6 @@ Status FullCompaction::prepare_compact() {
 
     // 1. pick rowsets to compact
     RETURN_IF_ERROR(pick_rowsets_to_compact());
-    TRACE_COUNTER_INCREMENT("input_rowsets_count", _input_rowsets.size());
     _tablet->set_clone_occurred(false);
 
     return Status::OK();
