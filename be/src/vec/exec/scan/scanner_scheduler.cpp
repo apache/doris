9eb1f80219439fec62349a293f09662532cdac76
airborne12 <airborne12@gmail.com>
2023-07-06 09:27:43 +0800

    [Fix](ScannerScheduler) fix dead lock when shutdown group_local_scan_thread_pool

--- be/src/vec/exec/scan/scanner_scheduler.cpp
+++ be/src/vec/exec/scan/scanner_scheduler.cpp
@@ -66,22 +66,23 @@ ScannerScheduler::~ScannerScheduler() {
 
     _is_closed = true;
 
+    _task_group_local_scan_queue->close();
     _scheduler_pool->shutdown();
     _local_scan_thread_pool->shutdown();
     _remote_scan_thread_pool->shutdown();
     _limited_scan_thread_pool->shutdown();
+    _group_local_scan_thread_pool->shutdown();
 
     _scheduler_pool->wait();
     _local_scan_thread_pool->join();
+    _remote_scan_thread_pool->wait();
+    _limited_scan_thread_pool->wait();
+    _group_local_scan_thread_pool->wait();
 
     for (int i = 0; i < QUEUE_NUM; i++) {
         delete _pending_queues[i];
     }
     delete[] _pending_queues;
-
-    _task_group_local_scan_queue->close();
-    _group_local_scan_thread_pool->shutdown();
-    _group_local_scan_thread_pool->wait();
 }
 
 Status ScannerScheduler::init(ExecEnv* env) {
