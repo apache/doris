b1be59c799769d690414262ec0b5bf0bb6687a9e
Siyang Tang <82279870+TangSiyang2001@users.noreply.github.com>
2023-07-06 10:25:38 +0800

    [enhancement](query) enable strong consistency by syncing max journal id from master (#21205)
    Add a session var & config enable_strong_consistency_read to solve the problem that loading result may be shortly invisible to follwers, to meet users requirements in strong consistency read scenario.
    
    Will sync max journal id from master and wait for replaying.

--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -468,6 +468,7 @@ struct TMasterOpRequest {
     21: optional map<string, string> trace_carrier
     22: optional string clientNodeHost
     23: optional i32 clientNodePort
+    24: optional bool syncJournalOnly // if set to true, this request means to do nothing but just sync max journal id of master
 }
 
 struct TColumnDefinition {
