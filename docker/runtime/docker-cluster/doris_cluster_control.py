36c875d43630cc0723bc943db4d542fb004fe92a
yujun <yu.jun.reach@gmail.com>
2023-07-06 15:47:10 +0800

    add start stop command

--- docker/runtime/docker-cluster/doris_cluster_control.py
+++ docker/runtime/docker-cluster/doris_cluster_control.py
@@ -93,8 +93,12 @@ class Meta(object):
     def add_node(self, node_type, id):
         ids = self.idsets.get(node_type)
         if not ids:
-            raise Exception("Unknown node type ".format(node_type))
+            raise Exception("Unknown node type {}".format(node_type))
         return ids.add(id)
+
+    def contain_node(self, node_type, id):
+        idset = self.idsets.get(node_type, None)
+        return idset and id in idset.ids
 
     @staticmethod
     def load_cluster_meta(cluster_name):
@@ -138,7 +142,7 @@ class Node(object):
     TYPE_ALL = [TYPE_FE, TYPE_BE]
 
     @staticmethod
-    def new(node_type, meta, id):
+    def new(meta, node_type, id):
         if node_type == Node.TYPE_FE:
             return FE(meta, id)
         elif node_type == Node.TYPE_BE:
@@ -307,6 +311,25 @@ class Cluster(object):
         return Cluster(meta)
 
     @staticmethod
+    def load_cluster(cluster_name):
+        if not cluster_name:
+            raise Exception("cluster is empty")
+        cluster_path = get_cluster_path(cluster_name)
+        if not os.path.exists(cluster_path):
+            raise Exception(
+                "cluster directory {} not exists.".format(cluster_path))
+        meta_path = os.path.join(cluster_path, "meta")
+        if not os.path.exists(meta_path):
+            raise Exception(
+                "cluster meta file {} not exists.".format(meta_path))
+        meta = Meta.load_cluster_meta(cluster_name)
+        if not meta:
+            raise Exception(
+                "load cluster meta failed, please check file {}".format(
+                    meta_path))
+        return Cluster(meta)
+
+    @staticmethod
     def _gen_subnet_prefix():
         used_subnet_prefix = {}
         for cluster_name in os.listdir(DORIS_LOCAL_ROOT):
@@ -332,7 +355,7 @@ class Cluster(object):
 
     def _add_node(self, node_type, id):
         id = self.meta.add_node(node_type, id)
-        Node.new(node_type, self.meta, id).init_dir()
+        Node.new(self.meta, node_type, id).init_dir()
 
     def save_meta(self):
         self.meta.save()
@@ -341,7 +364,7 @@ class Cluster(object):
         services = {}
         for node_type, idset in self.meta.idsets.items():
             for id in idset.ids:
-                node = Node.new(node_type, self.meta, id)
+                node = Node.new(self.meta, node_type, id)
                 services[node.service_name()] = node.compose()
 
         compose = {
@@ -371,11 +394,28 @@ class Cluster(object):
         self.save_compose()
 
     def start(self):
-        cmd = "docker-compose -f {} up -d".format(self.get_compose_path())
-        exec_shell_command(cmd)
+        self._run_docker_compose("up")
+
+    def start_node(self, node_type, id):
+        node = self._get_node(node_type, id)
+        self._run_docker_compose("start {}".format(node.service_name()))
+
+    def stop_node(self, node_type, id):
+        node = self._get_node(node_type, id)
+        self._run_docker_compose("stop {}".format(node.service_name()))
+
+    def _run_docker_compose(self, cmd):
+        return exec_shell_command("docker-compose -f {} {}".format(
+            self.get_compose_path(), cmd))
+
+    def _get_node(self, node_type, id):
+        if not self.meta.contain_node(node_type, id):
+            raise Exception("No found {} node with id {}".format(
+                node_type, id))
+        return Node.new(self.meta, node_type, id)
 
 
-def run(args):
+def new(args):
     cluster = Cluster.new_cluster(args.name, args.IMAGE)
     LOG.info("Add cluster {}".format(cluster.get_cluster_name()))
     for i in range(args.fe):
@@ -387,15 +427,41 @@ def run(args):
     LOG.info("Run cluster {} succ".format(cluster.get_cluster_name()))
 
 
+def start(args):
+    cluster = Cluster.load_cluster(args.NAME)
+    cluster.start_node(args.NODE_TYPE, args.ID)
+    LOG.info("Start node {} with id {} succ".format(args.NODE_TYPE, args.ID))
+
+
+def stop(args):
+    cluster = Cluster.load_cluster(args.NAME)
+    cluster.stop_node(args.NODE_TYPE, args.ID)
+    LOG.info("Stop node {} with id {} succ".format(args.NODE_TYPE, args.ID))
+
+
 def parse_args():
     ap = argparse.ArgumentParser(description="")
     sub_aps = ap.add_subparsers(dest="command")
 
-    ap_run = sub_aps.add_parser("new", help="new a doris cluster")
-    ap_run.add_argument("IMAGE", help="specify docker image")
-    ap_run.add_argument("--fe", type=int, default=3, help="specify fe count")
-    ap_run.add_argument("--be", type=int, default=3, help="specify be count")
-    ap_run.add_argument("--name", default="", help="specific cluster name")
+    ap_new = sub_aps.add_parser("new", help="new a doris cluster")
+    ap_new.add_argument("IMAGE", help="specify docker image")
+    ap_new.add_argument("--fe", type=int, default=3, help="specify fe count")
+    ap_new.add_argument("--be", type=int, default=3, help="specify be count")
+    ap_new.add_argument("--name", default="", help="specific cluster name")
+
+    ap_start = sub_aps.add_parser("start", help="start a fe or be node")
+    ap_start.add_argument("NAME", help="specify cluster name")
+    ap_start.add_argument("NODE_TYPE",
+                          choices=Node.TYPE_ALL,
+                          help="specify node type")
+    ap_start.add_argument("ID", type=int, help="specify node id")
+
+    ap_stop = sub_aps.add_parser("stop", help="stop a fe or be node")
+    ap_stop.add_argument("NAME", help="specify cluster name")
+    ap_stop.add_argument("NODE_TYPE",
+                         choices=Node.TYPE_ALL,
+                         help="specify node type")
+    ap_stop.add_argument("ID", type=int, help="specify node id")
 
     return ap.format_usage(), ap.format_help(), ap.parse_args()
 
@@ -403,7 +469,11 @@ def parse_args():
 def main():
     usage, _, args = parse_args()
     if args.command == "new":
-        return run(args)
+        return new(args)
+    elif args.command == "start":
+        return start(args)
+    elif args.command == "stop":
+        return stop(args)
     else:
         print(usage)
         return -1
