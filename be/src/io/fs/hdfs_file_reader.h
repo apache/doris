21ffa44d7e62d609ecaecf7fd81c745f73a82351
Qi Chen <kaka11.chen@gmail.com>
2023-07-07 14:52:14 +0800

    [Enhancement](multi-catalog) Add hdfs read statistics profile. (#21442)
    Add hdfs read statistics profile.
    ```
      -  HdfsIO:  0ns
        -  TotalBytesRead:  133.47  MB
        -  TotalLocalBytesRead:  133.47  MB
        -  TotalShortCircuitBytesRead:  133.47  MB
        -  TotalZeroCopyBytesRead:  0.00  
    ```

--- be/src/io/fs/hdfs_file_reader.h
+++ be/src/io/fs/hdfs_file_reader.h
@@ -38,7 +38,8 @@ class IOContext;
 
 class HdfsFileReader : public FileReader {
 public:
-    HdfsFileReader(Path path, const std::string& name_node, FileHandleCache::Accessor accessor);
+    HdfsFileReader(Path path, const std::string& name_node, FileHandleCache::Accessor accessor,
+                   RuntimeProfile* profile);
 
     ~HdfsFileReader() override;
 
@@ -57,11 +58,24 @@ protected:
                         const IOContext* io_ctx) override;
 
 private:
+#ifdef USE_HADOOP_HDFS
+    struct HDFSProfile {
+        RuntimeProfile::Counter* total_bytes_read;
+        RuntimeProfile::Counter* total_local_bytes_read;
+        RuntimeProfile::Counter* total_short_circuit_bytes_read;
+        RuntimeProfile::Counter* total_total_zero_copy_bytes_read;
+    };
+#endif
+
     Path _path;
     const std::string& _name_node;
     FileHandleCache::Accessor _accessor;
     CachedHdfsFileHandle* _handle = nullptr; // owned by _cached_file_handle
     std::atomic<bool> _closed = false;
+    RuntimeProfile* _profile;
+#ifdef USE_HADOOP_HDFS
+    HDFSProfile _hdfs_profile;
+#endif
 };
 } // namespace io
 } // namespace doris
