60c3410f43b517d4344c184d4372482327bef609
starocean999 <40539150+starocean999@users.noreply.github.com>
2023-07-10 09:50:43 +0800

    [fix](planner)need recalculate nullable info of output slots for join node (#21651)
    pick from master #21650

--- fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
@@ -433,6 +433,15 @@ public abstract class JoinNodeBase extends PlanNode {
         }
         // 5. replace tuple is null expr
         TupleIsNullPredicate.substitueListForTupleIsNull(vSrcToOutputSMap.getLhs(), originTidsToIntermediateTidMap);
+
+        Preconditions.checkState(vSrcToOutputSMap.getLhs().size() == vOutputTupleDesc.getSlots().size());
+        List<Expr> exprs = vSrcToOutputSMap.getLhs();
+        ArrayList<SlotDescriptor> slots = vOutputTupleDesc.getSlots();
+        for (int i = 0; i < slots.size(); i++) {
+            slots.get(i).setIsNullable(exprs.get(i).isNullable() || slots.get(i).getIsNullable());
+        }
+        vSrcToOutputSMap.reCalculateNullableInfoForSlotInRhs();
+        vOutputTupleDesc.computeMemLayout();
     }
 
     protected abstract List<SlotId> computeSlotIdsForJoinConjuncts(Analyzer analyzer);
