d01a4ec47bee6cc6817ff66de2d08fcd29971427
century <919745273@qq.com>
2023-07-10 20:38:04 +0800

    fix fe ut

--- fe/fe-core/src/test/java/org/apache/doris/catalog/ModifyStorageMediumTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/ModifyStorageMediumTest.java
@@ -1,6 +1,13 @@
 package org.apache.doris.catalog;
 
+import org.apache.doris.alter.AlterJobV2;
+import org.apache.doris.alter.AlterJobV2.JobState;
+import org.apache.doris.analysis.AlterTableStmt;
 import org.apache.doris.common.DdlException;
+import org.apache.doris.planner.OlapScanNode;
+import org.apache.doris.planner.PlanFragment;
+import org.apache.doris.planner.Planner;
+import org.apache.doris.qe.StmtExecutor;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.SystemInfoService;
 import org.apache.doris.thrift.TStorageMedium;
@@ -10,25 +17,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 public class ModifyStorageMediumTest extends TestWithFeService {
 
     @Override
     protected void runBeforeAll() throws Exception {
-        createDatabase("test");
-        connectContext.setDatabase("default_cluster:test");
-        connectContext.getState().reset();
-
         SystemInfoService clusterInfo = Env.getCurrentEnv().getClusterInfo();
         List<Backend> allBackends = clusterInfo.getAllBackends();
         // set all backends' storage medium to SSD
         for (Backend backend : allBackends) {
             if (backend.hasPathHash()) {
                 backend.getDisks().values().stream()
-                        .map(diskInfo ->  {
-                            diskInfo.setStorageMedium(TStorageMedium.SSD);
-                            return diskInfo;
-                        });
+                        .peek(diskInfo -> diskInfo.setStorageMedium(TStorageMedium.SSD));
             }
         }
     }
@@ -38,16 +39,19 @@ public class ModifyStorageMediumTest extends TestWithFeService {
         Env.getCurrentEnv().clear();
     }
 
-    @Test
-    public void testCreatingAfterModifyStorageMedium() throws Exception {
-        String sql1 = "CREATE TABLE IF NOT EXISTS test.t1 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
+    public void envTest() throws Exception {
+        createDatabase("db1");
+        System.out.println(Env.getCurrentInternalCatalog().getDbNames());
+
+        String sql1 = "CREATE TABLE IF NOT EXISTS db1.t1 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
                 + "DISTRIBUTED BY HASH(pk) BUCKETS 1 PROPERTIES ('replication_num' = '1');";
         Assertions.assertDoesNotThrow(() -> createTables(sql1));
-        String sql2 = "CREATE TABLE IF NOT EXISTS test.t1 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
+        String sql2 = "CREATE TABLE IF NOT EXISTS db1.t2 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
                 + "DISTRIBUTED BY HASH(pk) BUCKETS 1 PROPERTIES ('replication_num' = '1', 'storage_medium' = 'ssd');";
         Assertions.assertDoesNotThrow(() -> createTables(sql2));
-        String sql3 = "CREATE TABLE IF NOT EXISTS test.t1 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
+        String sql3 = "CREATE TABLE IF NOT EXISTS db1.t3 (pk INT, v1 INT sum) AGGREGATE KEY (pk) "
                 + "DISTRIBUTED BY HASH(pk) BUCKETS 1 PROPERTIES ('replication_num' = '1', 'storage_medium' = 'hdd');";
         Assertions.assertThrows(DdlException.class, () -> createTables(sql3));
     }
+
 }
