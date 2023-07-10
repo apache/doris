842fe001571f85dafc1bdcafe0f9557089eacc8c
Siyang Tang <82279870+TangSiyang2001@users.noreply.github.com>
2023-07-10 17:14:33 +0800

    [enhancement](flush) make writer write fail status visible (#21530)

--- be/src/olap/rowset/beta_rowset_writer.cpp
+++ be/src/olap/rowset/beta_rowset_writer.cpp
@@ -871,7 +871,7 @@ Status BetaRowsetWriter::_flush_segment_writer(std::unique_ptr<segment_v2::Segme
     Status s = (*writer)->finalize(&segment_size, &index_size);
     if (!s.ok()) {
         LOG(WARNING) << "failed to finalize segment: " << s.to_string();
-        return Status::Error<WRITER_DATA_WRITE_ERROR>();
+        return Status::Error(s.code());
     }
     VLOG_DEBUG << "tablet_id:" << _context.tablet_id
                << " flushing filename: " << (*writer)->get_data_dir()->path()
