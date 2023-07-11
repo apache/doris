0444941fa5148c2fb167b52b7f73085f55d28ffb
century <919745273@qq.com>
2023-07-11 09:30:00 +0800

    fix checkstyle

--- fe/fe-core/src/test/java/org/apache/doris/catalog/ModifyStorageMediumTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/ModifyStorageMediumTest.java
@@ -1,23 +1,31 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.apache.doris.catalog;
 
-import org.apache.doris.alter.AlterJobV2;
-import org.apache.doris.alter.AlterJobV2.JobState;
-import org.apache.doris.analysis.AlterTableStmt;
 import org.apache.doris.common.DdlException;
-import org.apache.doris.planner.OlapScanNode;
-import org.apache.doris.planner.PlanFragment;
-import org.apache.doris.planner.Planner;
-import org.apache.doris.qe.StmtExecutor;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.SystemInfoService;
 import org.apache.doris.thrift.TStorageMedium;
 import org.apache.doris.utframe.TestWithFeService;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Map;
 
 public class ModifyStorageMediumTest extends TestWithFeService {
 
