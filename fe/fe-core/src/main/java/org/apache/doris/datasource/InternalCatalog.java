46e7c2c7e37dc702faeb5aa7ca95d4cc335a60c4
Jack Drogon <jack.xsuperman@gmail.com>
2023-07-13 11:47:49 +0800

    [fix](ccr) Add tableName in DropInfo && BatchDropInfo (#21736)
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -893,7 +893,7 @@ public class InternalCatalog implements CatalogIf<Database> {
             } finally {
                 table.writeUnlock();
             }
-            DropInfo info = new DropInfo(db.getId(), table.getId(), -1L, stmt.isForceDrop(), recycleTime);
+            DropInfo info = new DropInfo(db.getId(), table.getId(), tableName, -1L, stmt.isForceDrop(), recycleTime);
             Env.getCurrentEnv().getEditLog().logDropTable(info);
             Env.getCurrentEnv().getQueryStats().clear(Env.getCurrentEnv().getCurrentCatalog().getId(),
                     db.getId(), table.getId());
