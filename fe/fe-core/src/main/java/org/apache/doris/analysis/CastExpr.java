31bb279bec83836b640c6999bb02a2c52096e4a1
BiteTheDDDDt <pxl290@qq.com>
2023-07-14 17:15:45 +0800

    support fold constant on delete stmt

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
@@ -204,11 +204,11 @@ public class CastExpr extends Expr {
 
     @Override
     public String toSqlImpl() {
-        boolean isVerbose = ConnectContext.get() != null
-                && ConnectContext.get().getExecutor() != null
+        boolean isVerbose = ConnectContext.get() != null && (ConnectContext.get().getExecutor() != null
                 && ConnectContext.get().getExecutor().getParsedStmt() != null
                 && ConnectContext.get().getExecutor().getParsedStmt().getExplainOptions() != null
-                && ConnectContext.get().getExecutor().getParsedStmt().getExplainOptions().isVerbose();
+                && ConnectContext.get().getExecutor().getParsedStmt().getExplainOptions().isVerbose()
+                || !ConnectContext.get().getState().isQuery());
         if (isImplicit && !isVerbose) {
             return getChild(0).toSql();
         }
