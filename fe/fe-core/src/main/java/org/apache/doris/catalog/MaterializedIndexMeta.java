4d44cea7846c16175cb79980b9800fb8be16f0e9
Pxl <pxl290@qq.com>
2023-07-14 15:39:38 +0800

    [Bug](materialized-view) check group expr at create mv (#21798)
    check group expr at create mv

--- fe/fe-core/src/main/java/org/apache/doris/catalog/MaterializedIndexMeta.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/MaterializedIndexMeta.java
@@ -111,6 +111,9 @@ public class MaterializedIndexMeta implements Writable, GsonPostProcessable {
 
     public void setWhereClause(Expr whereClause) {
         this.whereClause = whereClause;
+        if (this.whereClause != null) {
+            this.whereClause.setDisableTableName(true);
+        }
     }
 
     public Expr getWhereClause() {
