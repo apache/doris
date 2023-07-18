d59503a0ff112bb89d40d2f2a348e982516bb2ef
Kang <kxiao.tiger@gmail.com>
2023-07-19 00:59:53 +0800

    fix for pick commit 54795289dcd88e7cf08a86dc344ba4c4e94bf0f1 [Bug](pipeline) adjust scanner scheduler.submit and _num_scheduling_ctx maintain adjust scanner scheduler.submit and _num_scheduling_ctx maintain

--- be/src/pipeline/pipeline_task.h
+++ be/src/pipeline/pipeline_task.h
@@ -112,20 +112,7 @@ class PipelineTask {
 public:
     PipelineTask(PipelinePtr& pipeline, uint32_t index, RuntimeState* state, Operators& operators,
                  OperatorPtr& sink, PipelineFragmentContext* fragment_context,
-                 RuntimeProfile* parent_profile)
-            : _index(index),
-              _pipeline(pipeline),
-              _operators(operators),
-              _source(_operators.front()),
-              _root(_operators.back()),
-              _sink(sink),
-              _prepared(false),
-              _opened(false),
-              _state(state),
-              _cur_state(PipelineTaskState::NOT_READY),
-              _data_state(SourceState::DEPEND_ON_SOURCE),
-              _fragment_context(fragment_context),
-              _parent_profile(parent_profile) {}
+                 RuntimeProfile* parent_profile);
 
     Status prepare(RuntimeState* state);
 
