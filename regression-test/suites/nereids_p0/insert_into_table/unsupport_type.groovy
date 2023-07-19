e48a713b88e17ec46a8806814ff2b2ef96c0db1d
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-07-19 13:52:15 +0800

    [Enhancement](Nereids)enable nereids DML by default. (#21539)
    TODO: fix cast agg_state type when do insert

--- regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/unsupport_type.groovy
@@ -20,8 +20,6 @@ suite("nereids_insert_array_type") {
 
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
-    sql 'set enable_nereids_dml=true'
-    sql 'set parallel_fragment_exec_instance_num=13'
 
     test {
         sql 'insert into arr_t select id, kaint from src'
