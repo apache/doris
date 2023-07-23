04d9ddbe264507da2a77f7560e0498595317431b
zhangdong <493738387@qq.com>
2023-07-23 11:24:20 +0800

    [fix](catalog) do not call makeSureInitialized when create/drop table/db from hms meta event (#21941)
    Supplement to #21104

--- fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/CatalogMgr.java
@@ -738,7 +738,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
         }
         db.writeLock();
         try {
-            db.dropTable(table.getName());
+            db.dropTableForReplay(table.getName());
             db.setLastUpdateTime(log.getLastUpdateTime());
         } finally {
             db.writeUnlock();
@@ -809,7 +809,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
         }
         db.writeLock();
         try {
-            db.replayCreateTableFromEvent(log.getTableName(), log.getTableId());
+            db.createTableForReplay(log.getTableName(), log.getTableId());
             db.setLastUpdateTime(log.getLastUpdateTime());
         } finally {
             db.writeUnlock();
@@ -855,7 +855,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
                 LOG.warn("No db found with id:[{}], it may have been dropped.", log.getDbId());
                 return;
             }
-            catalog.dropDatabase(db.getFullName());
+            catalog.dropDatabaseForReplay(db.getFullName());
             Env.getCurrentEnv().getExtMetaCacheMgr().invalidateDbCache(catalog.getId(), db.getFullName());
         } finally {
             writeUnlock();
@@ -896,7 +896,7 @@ public class CatalogMgr implements Writable, GsonPostProcessable {
                 LOG.warn("No catalog found with id:[{}], it may have been dropped.", log.getCatalogId());
                 return;
             }
-            catalog.createDatabase(log.getDbId(), log.getDbName());
+            catalog.createDatabaseForReplay(log.getDbId(), log.getDbName());
         } finally {
             writeUnlock();
         }
