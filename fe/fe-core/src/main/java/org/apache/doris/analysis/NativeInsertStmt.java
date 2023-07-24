d7521ed7f4f4ab6884a2bf32a4c4069ec0769e76
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-07-24 12:08:43 +0800

    [Fix](Nereids)fix insert into default value exception (#21924)
    default value in the first cell of values when rise a cast exception, we filter it when check the types of values in insert, when the literal is string and value is the specific default value string, we skip type check.

--- fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
@@ -524,7 +524,7 @@ public class NativeInsertStmt extends InsertStmt {
                 final List<Expr> resultExprs = queryStmt.getResultExprs();
                 Preconditions.checkState(resultExprs.isEmpty(), "result exprs should be empty.");
                 for (int i = 0; i < rowSize; i++) {
-                    resultExprs.add(new IntLiteral(1));
+                    resultExprs.add(new StringLiteral(SelectStmt.DEFAULT_VALUE));
                     final DefaultValueExpr defaultValueExpr = new DefaultValueExpr();
                     valueList.getFirstRow().add(defaultValueExpr);
                     colLabels.add(defaultValueExpr.toColumnLabel());
@@ -540,11 +540,15 @@ public class NativeInsertStmt extends InsertStmt {
         // Check if all columns mentioned is enough
         checkColumnCoverage(mentionedColumns, targetTable.getBaseSchema());
 
-        realTargetColumnNames = targetColumns.stream().map(column -> column.getName()).collect(Collectors.toList());
+        realTargetColumnNames = targetColumns.stream().map(Column::getName).collect(Collectors.toList());
         Map<String, Expr> slotToIndex = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
         for (int i = 0; i < queryStmt.getResultExprs().size(); i++) {
-            slotToIndex.put(realTargetColumnNames.get(i), queryStmt.getResultExprs().get(i)
-                    .checkTypeCompatibility(targetTable.getColumn(realTargetColumnNames.get(i)).getType()));
+            Expr expr = queryStmt.getResultExprs().get(i);
+            if (!(expr instanceof StringLiteral && ((StringLiteral) expr).getValue()
+                    .equals(SelectStmt.DEFAULT_VALUE))) {
+                slotToIndex.put(realTargetColumnNames.get(i), queryStmt.getResultExprs().get(i)
+                        .checkTypeCompatibility(targetTable.getColumn(realTargetColumnNames.get(i)).getType()));
+            }
         }
 
         for (Column column : targetTable.getBaseSchema()) {
