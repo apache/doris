b41fcbb7834bf89f9744d351b1cfb9ac2485008b
Jerry Hu <mrhhsg@gmail.com>
2023-07-25 11:21:03 +0800

    [feature](agg) add the aggregation function 'mag_agg' (#22043)
    New aggregation function: map_agg.
    
    This function requires two arguments: a key and a value, which are used to build a map.
    
    select map_agg(column1, column2) from t group by column3;

--- be/src/vec/aggregate_functions/aggregate_function_simple_factory.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_simple_factory.cpp
@@ -59,6 +59,7 @@ void register_aggregate_function_avg_weighted(AggregateFunctionSimpleFactory& fa
 void register_aggregate_function_histogram(AggregateFunctionSimpleFactory& factory);
 void register_aggregate_function_count_old(AggregateFunctionSimpleFactory& factory);
 void register_aggregate_function_sum_old(AggregateFunctionSimpleFactory& factory);
+void register_aggregate_function_map_agg(AggregateFunctionSimpleFactory& factory);
 
 AggregateFunctionSimpleFactory& AggregateFunctionSimpleFactory::instance() {
     static std::once_flag oc;
@@ -93,6 +94,7 @@ AggregateFunctionSimpleFactory& AggregateFunctionSimpleFactory::instance() {
         register_aggregate_function_sequence_match(instance);
         register_aggregate_function_avg_weighted(instance);
         register_aggregate_function_histogram(instance);
+        register_aggregate_function_map_agg(instance);
 
         register_aggregate_function_stddev_variance_samp(instance);
         register_aggregate_function_replace_reader_load(instance);
