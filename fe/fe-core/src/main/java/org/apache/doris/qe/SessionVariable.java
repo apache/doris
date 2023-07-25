f84af95ac411bb5c9251db2055300b14d8be297e
LiBinfeng <46676950+LiBinfeng-01@users.noreply.github.com>
2023-07-25 15:26:19 +0800

    [feature](Nereids) Add minidump replay and refactor user feature of minidump (#20716)
    ### Two main changes:
    - 1、add minidump replay
    - 2、change minidump serialization of statistic messages and some interface between main logic of nereids optimizer and minidump
    
    ### Use of nereids ut:
    - 1、save minidump files:  
            Execute command by mysql-client:
    ```
    set enable_nereids_planner=true;
    set enable_minidump=true;
    ```
            Execute sql in mysql-client
    - 2、use nereids-ut script to execute directory:
    ```
    cp -r ${DORIS_HOME}/minidump ${DORIS_HOME}/output/fe && cd ${DORIS_HOME}/output/fe
    ./nereids_ut --d ${directory_of_minidump_files}
    ```
    
    ### Refactor of minidump
    - move statistics used serialization to serialization of input and serialize with catalogs
    - generating minidump file only when enable_minidump flag is set, minidump module interactive with main optimizer only by :
    serializeInputsToDumpFile(catalog, statistics, query) && serializeOutputsToDumpFile(outputplan).

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -159,7 +159,7 @@ public class SessionVariable implements Serializable, Writable {
     // if the right table is greater than this value in the hash join,  we will ignore IN filter
     public static final String RUNTIME_FILTER_MAX_IN_NUM = "runtime_filter_max_in_num";
 
-    public static final String BE_NUMBER_FOR_TEST = "be_number_for_test";
+    public static final String BE_NUMBER = "be_number_for_test";
 
     // max ms to wait transaction publish finish when exec insert stmt.
     public static final String INSERT_VISIBLE_TIMEOUT_MS = "insert_visible_timeout_ms";
@@ -319,6 +319,8 @@ public class SessionVariable implements Serializable, Writable {
     public static final String SHOW_USER_DEFAULT_ROLE = "show_user_default_role";
 
     public static final String ENABLE_MINIDUMP = "enable_minidump";
+
+    public static final String MINIDUMP_PATH = "minidump_path";
 
     public static final String TRACE_NEREIDS = "trace_nereids";
 
@@ -679,12 +681,16 @@ public class SessionVariable implements Serializable, Writable {
     @VariableMgr.VarAttr(name = USE_RF_DEFAULT)
     public boolean useRuntimeFilterDefaultSize = false;
 
-    public int getBeNumberForTest() {
-        return beNumberForTest;
+    public int getBeNumber() {
+        return beNumber;
     }
 
-    @VariableMgr.VarAttr(name = BE_NUMBER_FOR_TEST)
-    private int beNumberForTest = -1;
+    public void setBeNumber(int beNumber) {
+        this.beNumber = beNumber;
+    }
+
+    @VariableMgr.VarAttr(name = BE_NUMBER)
+    private int beNumber = -1;
 
     public double getCboCpuWeight() {
         return cboCpuWeight;
@@ -977,6 +983,9 @@ public class SessionVariable implements Serializable, Writable {
 
     @VariableMgr.VarAttr(name = ENABLE_FOLD_NONDETERMINISTIC_FN)
     public boolean enableFoldNondeterministicFn = true;
+
+    @VariableMgr.VarAttr(name = MINIDUMP_PATH)
+    public String minidumpPath = "";
 
     @VariableMgr.VarAttr(name = TRACE_NEREIDS)
     public boolean traceNereids = false;
@@ -2296,6 +2305,14 @@ public class SessionVariable implements Serializable, Writable {
 
     public void setEnableMinidump(boolean enableMinidump) {
         this.enableMinidump = enableMinidump;
+    }
+
+    public String getMinidumpPath() {
+        return minidumpPath;
+    }
+
+    public void setMinidumpPath(String minidumpPath) {
+        this.minidumpPath = minidumpPath;
     }
 
     public boolean isTraceNereids() {
