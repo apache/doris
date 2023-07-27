bf956519814760b941e2e3622d1d804bfb5938b8
Gabriel <gabrielleebuaa@gmail.com>
2023-07-27 10:01:34 +0800

    [round](decimalv2) round precise decimalv2 value (#22257)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
@@ -28,7 +28,6 @@ import org.apache.doris.catalog.DatabaseIf;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.Function;
 import org.apache.doris.catalog.FunctionSet;
-import org.apache.doris.catalog.MapType;
 import org.apache.doris.catalog.ScalarFunction;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Type;
@@ -1474,10 +1473,17 @@ public class FunctionCallExpr extends Expr {
                                             && ((ArrayType) args[ix]).getItemType().isDecimalV2()))) {
                         continue;
                     } else if (!argTypes[i].matchesType(args[ix])
-                            && !(argTypes[i].isDateOrDateTime() && args[ix].isDateOrDateTime())
-                            && (!fn.getReturnType().isDecimalV3()
-                                    || (argTypes[i].isValid() && !argTypes[i].isDecimalV3()
-                                            && args[ix].isDecimalV3()))) {
+                            && ROUND_FUNCTION_SET.contains(fnName.getFunction())
+                            && ConnectContext.get() != null
+                            && ConnectContext.get().getSessionVariable().roundPreciseDecimalV2Value
+                            && argTypes[i].isDecimalV2()
+                            && args[ix].isDecimalV3()) {
+                        uncheckedCastChild(ScalarType.createDecimalV3Type(ScalarType.MAX_DECIMALV2_PRECISION,
+                                ((ScalarType) argTypes[i]).getScalarScale()), i);
+                    } else if (!argTypes[i].matchesType(args[ix])
+                            && !(argTypes[i].isDecimalV3OrContainsDecimalV3()
+                            && args[ix].isDecimalV3OrContainsDecimalV3())) {
+                        // Do not do this cast if types are both decimalv3 with different precision/scale.
                         uncheckedCastChild(args[ix], i);
                     }
                 }
