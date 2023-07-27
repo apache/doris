e1a462d75af7e5ebedbe97e3cd0a3c77ea82f542
Jack Drogon <jack.xsuperman@gmail.com>
2023-07-27 20:16:21 +0800

    [Enhancement](binlog) Add binlog enable diable check in BinlogManager (#22173)
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
+++ fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -750,6 +750,7 @@ public class EditLog {
                 case OperationType.OP_MODIFY_REPLICATION_NUM: {
                     ModifyTablePropertyOperationLog log = (ModifyTablePropertyOperationLog) journal.getData();
                     env.replayModifyTableProperty(opCode, log);
+                    env.getBinlogManager().addModifyTableProperty(log, logId);
                     break;
                 }
                 case OperationType.OP_MODIFY_DISTRIBUTION_BUCKET_NUM: {
@@ -1039,6 +1040,7 @@ public class EditLog {
                     LOG.info("replay alter database property: {}", alterDatabasePropertyInfo);
                     env.replayAlterDatabaseProperty(alterDatabasePropertyInfo.getDbName(),
                             alterDatabasePropertyInfo.getProperties());
+                    env.getBinlogManager().addAlterDatabaseProperty(alterDatabasePropertyInfo, logId);
                     break;
                 }
                 case OperationType.OP_GC_BINLOG: {
@@ -1619,24 +1621,30 @@ public class EditLog {
         logEdit(OperationType.OP_MODIFY_DISTRIBUTION_TYPE, tableInfo);
     }
 
-    public void logDynamicPartition(ModifyTablePropertyOperationLog info) {
-        logEdit(OperationType.OP_DYNAMIC_PARTITION, info);
+    private long logModifyTableProperty(short op, ModifyTablePropertyOperationLog info) {
+        long logId = logEdit(op, info);
+        Env.getCurrentEnv().getBinlogManager().addModifyTableProperty(info, logId);
+        return logId;
     }
 
-    public void logModifyReplicationNum(ModifyTablePropertyOperationLog info) {
-        logEdit(OperationType.OP_MODIFY_REPLICATION_NUM, info);
+    public void logDynamicPartition(ModifyTablePropertyOperationLog info) {
+        logModifyTableProperty(OperationType.OP_DYNAMIC_PARTITION, info);
+    }
+
+    public long logModifyReplicationNum(ModifyTablePropertyOperationLog info) {
+        return logModifyTableProperty(OperationType.OP_MODIFY_REPLICATION_NUM, info);
     }
 
     public void logModifyDefaultDistributionBucketNum(ModifyTableDefaultDistributionBucketNumOperationLog info) {
         logEdit(OperationType.OP_MODIFY_DISTRIBUTION_BUCKET_NUM, info);
     }
 
-    public void logModifyInMemory(ModifyTablePropertyOperationLog info) {
-        logEdit(OperationType.OP_MODIFY_IN_MEMORY, info);
+    public long logModifyInMemory(ModifyTablePropertyOperationLog info) {
+        return logModifyTableProperty(OperationType.OP_MODIFY_IN_MEMORY, info);
     }
 
-    public void logUpdateBinlogConfig(ModifyTablePropertyOperationLog info) {
-        logEdit(OperationType.OP_UPDATE_BINLOG_CONFIG, info);
+    public long logUpdateBinlogConfig(ModifyTablePropertyOperationLog info) {
+        return logModifyTableProperty(OperationType.OP_UPDATE_BINLOG_CONFIG, info);
     }
 
     public void logAlterLightSchemaChange(AlterLightSchemaChangeInfo info) {
@@ -1825,7 +1833,9 @@ public class EditLog {
     }
 
     public long logAlterDatabaseProperty(AlterDatabasePropertyInfo log) {
-        return logEdit(OperationType.OP_ALTER_DATABASE_PROPERTY, log);
+        long logId = logEdit(OperationType.OP_ALTER_DATABASE_PROPERTY, log);
+        Env.getCurrentEnv().getBinlogManager().addAlterDatabaseProperty(log, logId);
+        return logId;
     }
 
     public long logGcBinlog(BinlogGcInfo log) {
