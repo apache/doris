f1d84526a729a8cb01d602e4d84d579863b5cfd8
daidai <2017501503@qq.com>
2023-07-29 00:31:01 +0800

    [opt](hive)opt select count(*) stmt push down agg on parquet in hive . (#22115)
    Optimization "select count(*) from table" stmtement , push down "count" type to BE.
    support file type : parquet ，orc in hive .
    
    1. 4kfiles , 60kwline num 
        before:  1 min 37.70 sec 
        after:   50.18 sec
    
    2. 50files , 60kwline num
        before: 1.12 sec
        after: 0.82 sec

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -636,7 +636,7 @@ struct TOlapScanNode {
   // It's limit for scanner instead of scanNode so we add a new limit.
   10: optional i64 sort_limit
   11: optional bool enable_unique_key_merge_on_write
-  12: optional TPushAggOp push_down_agg_type_opt
+  12: optional TPushAggOp push_down_agg_type_opt //Deprecated
   13: optional bool use_topn_opt
   14: optional list<Descriptors.TOlapTableIndex> indexes_desc
   15: optional set<i32> output_column_unique_ids
@@ -1142,6 +1142,8 @@ struct TPlanNode {
   46: optional TNestedLoopJoinNode nested_loop_join_node
   47: optional TTestExternalScanNode test_external_scan_node
 
+  48: optional TPushAggOp push_down_agg_type_opt
+  
   101: optional list<Exprs.TExpr> projections
   102: optional Types.TTupleId output_tuple_id
   103: optional TPartitionSortNode partition_sort_node
