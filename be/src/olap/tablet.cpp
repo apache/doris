e58851586df691c5f3690955dc581d4dd4d484e0
bobhan1 <bh2444151092@outlook.com>
2023-07-28 10:15:15 +0800

    update

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -3669,7 +3669,9 @@ Status Tablet::calc_delete_bitmap_between_segments(
 void Tablet::add_sentinel_mark_to_delete_bitmap(DeleteBitmapPtr delete_bitmap,
                                                 const RowsetIdUnorderedSet& rowsetids) {
     for (const auto& rowsetid : rowsetids) {
-        delete_bitmap->add({rowsetid, 0, 0}, DeleteBitmap::ROWSET_SENTINEL_MARK);
+        delete_bitmap->add(
+                {rowsetid, DeleteBitmap::INVALID_SEGMENT_ID, DeleteBitmap::INVALID_VERSION},
+                DeleteBitmap::ROWSET_SENTINEL_MARK);
     }
 }
 
