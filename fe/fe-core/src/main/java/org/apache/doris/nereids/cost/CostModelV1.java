260b11153477312d11330ed426cde7144728f68c
zhongjian.xzj <zhongjian.xzj@zhongjianxzjdeMacBook-Pro.local>
2023-07-28 18:25:57 +0800

    [enhance](nereids) enhance broadcast join cost calculation

--- fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
@@ -68,7 +68,10 @@ class CostModelV1 extends PlanVisitor<Cost, PlanContext> {
     private int beNumber = 1;
 
     public CostModelV1() {
-        if (ConnectContext.get().getSessionVariable().getBeNumber() != -1) {
+        if (ConnectContext.get().getSessionVariable().isPlayNereidsDump()) {
+            // TODO: @bingfeng refine minidump setting, and pass testMinidumpUt
+            beNumber = 1;
+        } else if (ConnectContext.get().getSessionVariable().getBeNumber() != -1) {
             beNumber = ConnectContext.get().getSessionVariable().getBeNumber();
         } else {
             beNumber = Math.max(1, ConnectContext.get().getEnv().getClusterInfo().getBackendsNumber(true));
