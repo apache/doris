f5b00eb84964d4f6ee6b5e3d3845508dc11046b3
bobhan1 <bh2444151092@outlook.com>
2023-08-01 12:08:55 +0800

    [fix](window_funnel_function) fix upgrade compatibility due to the added field in `WindowFunnelState` (#22416)

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1043,6 +1043,9 @@ DEFINE_mInt64(lookup_connection_cache_bytes_limit, "4294967296");
 // level of compression when using LZ4_HC, whose defalut value is LZ4HC_CLEVEL_DEFAULT
 DEFINE_mInt64(LZ4_HC_compression_level, "9");
 
+// enable window_funnel_function with different modes
+DEFINE_mBool(enable_window_funnel_function_v2, "false");
+
 #ifdef BE_TEST
 // test s3
 DEFINE_String(test_s3_resource, "resource");
