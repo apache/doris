apache/doris

commit 5f846056f7b8c8bb2fa4c39f8b17b2122eb65652

[fix](forward) fix MissingFormatArgumentException when failed to forward stmt to Master (#22142)

fe/fe-core/src/main/java/org/apache/doris/qe/MasterOpExecutor.java
@@ -117,9 +117,9 @@ public class MasterOpExecutor {
             // may throw NullPointerException. add err msg
             throw new Exception("Failed to get master client.", e);
         }
-        final StringBuilder forwardMsg = new StringBuilder(String.format("forward to Master %s", thriftAddress));
+        final StringBuilder forwardMsg = new StringBuilder("forward to master FE %s" + thriftAddress.toString());
         if (!params.isSyncJournalOnly()) {
-            forwardMsg.append(", statement: %s").append(ctx.getStmtId());
+            forwardMsg.append(", statement id: ").append(ctx.getStmtId());
         }
         LOG.info(forwardMsg.toString());
 
@@ -131,7 +131,7 @@ public class MasterOpExecutor {
         } catch (TTransportException e) {
             // wrap the raw exception.
             forwardMsg.append(" : failed");
-            Exception exception = new ForwardToMasterException(String.format(forwardMsg.toString()), e);
+            Exception exception = new ForwardToMasterException(forwardMsg.toString(), e);
 
             boolean ok = ClientPool.frontendPool.reopen(client, thriftTimeoutMs);
             if (!ok) {

commit 3d26f35c97ae797c0cef7b5ecba7816cffede602

[branch-2.0] remove %s to avoid MissingFormatArgumentException
fix bug introduced from #22142

fe/fe-core/src/main/java/org/apache/doris/qe/MasterOpExecutor.java
@@ -117,7 +117,7 @@ public class MasterOpExecutor {
             // may throw NullPointerException. add err msg
             throw new Exception("Failed to get master client.", e);
         }
-        final StringBuilder forwardMsg = new StringBuilder("forward to master FE %s" + thriftAddress.toString());
+        final StringBuilder forwardMsg = new StringBuilder("forward to master FE " + thriftAddress.toString());
         if (!params.isSyncJournalOnly()) {
             forwardMsg.append(", statement id: ").append(ctx.getStmtId());
         }