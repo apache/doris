3df7bb16e4b8a08c236a21855825a3c7083acbb5
tsy <tangsiyang2001@foxmail.com>
2023-08-02 10:47:52 +0800

    mod ut

--- be/test/olap/delete_handler_test.cpp
+++ be/test/olap/delete_handler_test.cpp
@@ -366,26 +366,26 @@ TEST_F(TestDeleteConditionHandler, StoreCondSucceed) {
 
     // 验证存储在header中的过滤条件正确
     EXPECT_EQ(size_t(6), del_pred.sub_predicates_v2_size());
-    EXPECT_STREQ("k1", del_pred.sub_predicates_v2(0).column_name);
-    EXPECT_STREQ("k2", del_pred.sub_predicates_v2(1).column_name);
-    EXPECT_STREQ("k3", del_pred.sub_predicates_v2(2).column_name);
-    EXPECT_STREQ("k4", del_pred.sub_predicates_v2(3).column_name);
-    EXPECT_STREQ("k5", del_pred.sub_predicates_v2(4).column_name);
-    EXPECT_STREQ("k12", sub_predicates_v2(5).column_name);
+    EXPECT_STREQ("k1", del_pred.sub_predicates_v2(0).column_name().c_str());
+    EXPECT_STREQ("k2", del_pred.sub_predicates_v2(1).column_name().c_str());
+    EXPECT_STREQ("k3", del_pred.sub_predicates_v2(2).column_name().c_str());
+    EXPECT_STREQ("k4", del_pred.sub_predicates_v2(3).column_name().c_str());
+    EXPECT_STREQ("k5", del_pred.sub_predicates_v2(4).column_name().c_str());
+    EXPECT_STREQ("k12", sub_predicates_v2(5).column_name().c_str());
 
-    EXPECT_STREQ("=", del_pred.sub_predicates_v2(0).op);
-    EXPECT_STREQ(">>", del_pred.sub_predicates_v2(1).op);
-    EXPECT_STREQ("<=", del_pred.sub_predicates_v2(2).op);
-    EXPECT_STREQ("IS", del_pred.sub_predicates_v2(3).op);
-    EXPECT_STREQ("=", del_pred.sub_predicates_v2(4).op);
-    EXPECT_STREQ("!=", del_pred.sub_predicates_v2(5).op);
+    EXPECT_STREQ("=", del_pred.sub_predicates_v2(0).op().c_str());
+    EXPECT_STREQ(">>", del_pred.sub_predicates_v2(1).op().c_str());
+    EXPECT_STREQ("<=", del_pred.sub_predicates_v2(2).op().c_str());
+    EXPECT_STREQ("IS", del_pred.sub_predicates_v2(3).op().c_str());
+    EXPECT_STREQ("=", del_pred.sub_predicates_v2(4).op().c_str());
+    EXPECT_STREQ("!=", del_pred.sub_predicates_v2(5).op().c_str());
 
-    EXPECT_STREQ("1", del_pred.sub_predicates_v2(0).cond_value);
-    EXPECT_STREQ("3", del_pred.sub_predicates_v2(1).cond_value);
-    EXPECT_STREQ("5", del_pred.sub_predicates_v2(2).cond_value);
-    EXPECT_STREQ("NULL", del_pred.sub_predicates_v2(4).cond_value);
-    EXPECT_STREQ("7", del_pred.sub_predicates_v2(4).cond_value);
-    EXPECT_STREQ("9", del_pred.sub_predicates_v2(5).cond_value);
+    EXPECT_STREQ("1", del_pred.sub_predicates_v2(0).cond_value().c_str());
+    EXPECT_STREQ("3", del_pred.sub_predicates_v2(1).cond_value().c_str());
+    EXPECT_STREQ("5", del_pred.sub_predicates_v2(2).cond_value().c_str());
+    EXPECT_STREQ("NULL", del_pred.sub_predicates_v2(4).cond_value().c_str());
+    EXPECT_STREQ("7", del_pred.sub_predicates_v2(4).cond_value().c_str());
+    EXPECT_STREQ("9", del_pred.sub_predicates_v2(5).cond_value().c_str());
 
     EXPECT_EQ(size_t(1), del_pred.in_predicates_size());
     EXPECT_FALSE(del_pred.in_predicates(0).is_not_in());
