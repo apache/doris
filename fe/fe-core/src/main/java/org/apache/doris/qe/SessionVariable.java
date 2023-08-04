7d1e08eafabcd04aca80c8d6c5827b86ef9795e0
mch_ucchi <41606806+sohardforaname@users.noreply.github.com>
2023-08-04 15:36:03 +0800

    [Fix](Nereids) rand() and uuid() should not fold constant (#22492)
    rand() and uuid() should not fold constant and we change the default value of fold constant for non-deterministic function to false.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -1000,7 +1000,7 @@ public class SessionVariable implements Serializable, Writable {
     public boolean enableMinidump = false;
 
     @VariableMgr.VarAttr(name = ENABLE_FOLD_NONDETERMINISTIC_FN)
-    public boolean enableFoldNondeterministicFn = true;
+    public boolean enableFoldNondeterministicFn = false;
 
     @VariableMgr.VarAttr(name = MINIDUMP_PATH)
     public String minidumpPath = "";
