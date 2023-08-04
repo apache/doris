dcf7b75eb4caaca3a99208ff7294156f08d6c424
Zenglin Luo <luozenglin@gmail.com>
2023-08-04 10:46:02 +0800

    [fix](compatibility) Version 1.2 upgraded to 2.0 compatible with miniload metadata

--- fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
@@ -51,7 +51,6 @@ import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.OriginStatement;
 import org.apache.doris.thrift.TUniqueId;
 import org.apache.doris.transaction.DatabaseTransactionMgr;
-import org.apache.doris.transaction.TransactionState;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
@@ -819,6 +818,10 @@ public class LoadManager implements Writable {
 
         out.writeInt(loadJobs.size());
         for (LoadJob loadJob : loadJobs) {
+            if (loadJob instanceof MiniLoadJob) {
+                // Deprecated, no longer write image
+                continue;
+            }
             loadJob.write(out);
         }
     }
@@ -836,25 +839,9 @@ public class LoadManager implements Writable {
             }
 
             if (loadJob.getJobType() == EtlJobType.MINI) {
-                // This is a bug fix. the mini load job should not with state LOADING.
-                if (loadJob.getState() == JobState.LOADING) {
-                    LOG.warn("skip mini load job {} in db {} with LOADING state", loadJob.getId(), loadJob.getDbId());
-                    continue;
-                }
-
-                if (loadJob.getState() == JobState.PENDING) {
-                    // bad case. When a mini load job is created and then FE restart.
-                    // the job will be in PENDING state forever.
-                    // This is a temp solution to remove these jobs.
-                    // And the mini load job should be deprecated in Doris v1.1
-                    TransactionState state = Env.getCurrentEnv().getGlobalTransactionMgr()
-                            .getTransactionState(loadJob.getDbId(), loadJob.getTransactionId());
-                    if (state == null) {
-                        LOG.warn("skip mini load job {} in db {} with PENDING state and with txn: {}", loadJob.getId(),
-                                loadJob.getDbId(), loadJob.getTransactionId());
-                        continue;
-                    }
-                }
+                LOG.warn("skip mini load job {} in db {} as it is no longer supported", loadJob.getId(),
+                        loadJob.getDbId());
+                continue;
             }
             idToLoadJob.put(loadJob.getId(), loadJob);
             Map<String, List<LoadJob>> map = dbIdToLabelToLoadJobs.get(loadJob.getDbId());
