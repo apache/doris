7be03c4b72b7aa3c34cd163fd41590f4c7db5cbe
Kang <kxiao.tiger@gmail.com>
2023-08-05 21:48:38 +0800

    fix merge conflict

--- fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/memo/Memo.java
@@ -49,6 +49,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
