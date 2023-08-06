96f42ca20a178f45ade11b17c429034bb4e52a49
Xinyi Zou <zouxinyi02@gmail.com>
2023-08-06 10:56:31 +0800

    [fix](memory) Independent count exec node memory profile (#22598)
    Independent count exec node memory profile, after #22582

--- be/src/vec/runtime/vdata_stream_recvr.h
+++ be/src/vec/runtime/vdata_stream_recvr.h
@@ -154,6 +154,7 @@ private:
     RuntimeProfile::Counter* _decompress_bytes;
     RuntimeProfile::Counter* _memory_usage_counter;
     RuntimeProfile::HighWaterMarkCounter* _blocks_memory_usage;
+    RuntimeProfile::Counter* _peak_memory_usage_counter;
 
     std::shared_ptr<QueryStatisticsRecvr> _sub_plan_query_statistics_recvr;
 
