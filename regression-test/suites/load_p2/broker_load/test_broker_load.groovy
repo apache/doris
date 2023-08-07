6dac9daf57812d1ceb37082153d3e05d417482c2
morningman <morningman@163.com>
2023-08-07 16:06:46 +0800

    add test

--- regression-test/suites/load_p2/broker_load/test_broker_load.groovy
+++ regression-test/suites/load_p2/broker_load/test_broker_load.groovy
@@ -361,6 +361,65 @@ suite("test_broker_load_p2", "p2") {
             order_qt_parquet_s3_case8 """ select count(*) from parquet_s3_case8 where p_partkey=1"""
             order_qt_parquet_s3_case9 """ select * from parquet_s3_case9"""
 
+            // pr 22666
+            def tbl_22666 = "part_22666"
+            sql """
+                CREATE TABLE ${tbl_22666} (
+                    p_partkey          int NULL,
+                    p_name        VARCHAR(55) NULL,
+                    p_mfgr        VARCHAR(25) NULL,
+                    p_brand       VARCHAR(10) NULL,
+                    p_type        VARCHAR(25) NULL,
+                    p_size        int NULL,
+                    p_container   VARCHAR(10) NULL,
+                    p_retailprice decimal(15, 2) NULL,
+                    p_comment     VARCHAR(23) NULL
+                )ENGINE=OLAP
+                DUPLICATE KEY(`p_partkey`)
+                DISTRIBUTED BY HASH(`p_partkey`) BUCKETS 3
+                PROPERTIES (
+                    "replication_num" = "1"
+                );
+            """
+
+            def label_22666 = "part_" + UUID.randomUUID().toString().replace("-", "0")
+            sql """
+                LOAD LABEL ${label_22666} (
+                    DATA INFILE("s3://doris-build-1308700295/regression/load/data/part0.parquet")
+                    INTO TABLE ${tbl_22666}
+                    FORMAT AS "PARQUET"
+                    (p_partkey, p_name, p_mfgr),
+                    DATA INFILE("s3://doris-build-1308700295/regression/load/data/part1.parquet")
+                    INTO TABLE ${tbl_22666}
+                    FORMAT AS "PARQUET"
+                    (p_partkey, p_brand, p_type)
+                )
+                WITH S3 (
+                    "AWS_ACCESS_KEY" = "$ak",
+                    "AWS_SECRET_KEY" = "$sk",
+                    "AWS_ENDPOINT" = "cos.ap-beijing.myqcloud.com",
+                    "AWS_REGION" = "ap-beijing"
+                );
+            """
+
+            def max_try_milli_secs = 600000
+            while (max_try_milli_secs > 0) {
+                String[][] result = sql """ show load where label="$label_22666" order by createtime desc limit 1; """
+                if (result[0][2].equals("FINISHED")) {
+                    logger.info("Load FINISHED " + label_22666)
+                    break;
+                }
+                if (result[0][2].equals("CANCELLED")) {
+                    assertTrue(false, "load failed: $result")
+                    break;
+                }
+                Thread.sleep(1000)
+                max_try_milli_secs -= 1000
+                if(max_try_milli_secs <= 0) {
+                    assertTrue(1 == 2, "load Timeout: $label_22666")
+                }
+            }
+
         } finally {
             for (String table in tables) {
                 sql new File("""${context.file.parent}/ddl/${table}_drop.sql""").text
