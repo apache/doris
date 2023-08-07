0ad237d14b006057fffa79a97e17d9b6acb70593
AKIRA <33112463+Kikyou1997@users.noreply.github.com>
2023-08-07 11:41:38 +0800

    fix full auto analyze (#22650)

--- fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsAutoAnalyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsAutoAnalyzer.java
@@ -46,6 +46,8 @@ public class StatisticsAutoAnalyzer extends MasterDaemon {
 
     public StatisticsAutoAnalyzer() {
         super("Automatic Analyzer", TimeUnit.MINUTES.toMillis(Config.auto_check_statistics_in_minutes));
+        analysisTaskExecutor = new AnalysisTaskExecutor(Config.full_auto_analyze_simultaneously_running_task_num);
+        analysisTaskExecutor.start();
     }
 
     @Override
