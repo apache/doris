428537720f5e4b365f672bf9134aa22ad301f60a
csun5285 <csun5285@gmail.com>
2023-08-08 15:45:44 +0800

    adjust nullptr

--- be/src/olap/single_replica_compaction.cpp
+++ be/src/olap/single_replica_compaction.cpp
@@ -200,6 +200,12 @@ Status SingleReplicaCompaction::_get_rowset_verisons_from_peer(
     std::shared_ptr<PBackendService_Stub> stub =
             ExecEnv::GetInstance()->brpc_internal_client_cache()->get_client(addr.host,
                                                                              addr.brpc_port);
+    if (stub == nullptr) {
+        LOG(WARNING) << "get rowset versions from peer: get rpc stub failed, host = " << addr.host
+                     << " port = " << addr.brpc_port;
+        return Status::Cancelled("get rpc stub failed");
+    }
+
     brpc::Controller cntl;
     stub->get_tablet_rowset_versions(&cntl, &request, &response, nullptr);
     if (cntl.Failed()) {
