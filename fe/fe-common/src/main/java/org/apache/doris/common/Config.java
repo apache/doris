1fa5e44790fbb2f19e5f198ad821eae1ed649be8
zy-kkk <zhongyk10@gmail.com>
2023-08-10 18:53:31 +0800

    [enhancement](data type) add use_mysql_bigint_for_largeint config Tell Doris to use bigint when returning largeint type to mysql jdbc (#22835)

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2063,4 +2063,9 @@ public class Config extends ConfigBase {
             "Export任务允许的最大并行数",
             "The maximum parallelism allowed by Export job"})
     public static int maximum_parallelism_of_export_job = 50;
+
+    @ConfField(mutable = true, description = {
+            "是否用 mysql 的 bigint 类型来返回 Doris 的 largeint 类型",
+            "Whether to use mysql's bigint type to return Doris's largeint type"})
+    public static boolean use_mysql_bigint_for_largeint = false;
 }
