519e4f6f6188d58c4fc4b1aa0d609fee8201fa5f
Chuanle Chen <60637740+gitccl@users.noreply.github.com>
2023-08-10 20:07:42 +0800

    [Enhancement](tvf) Table value function support reading local file (#17404)
    I tested the local tvf with tpch queries. First, generate `lineitem` datasets with 6001215 rows, and load it into `lineitem` table by:
    ```
    insert into lineitem select c11, c1, c4, c2, c3, c5, c6, c7, c8, c9, c10, c12, c13, c14, c15, c16 
    from local(
            "file_path" = "tools/tpch-tools/bin/tpch-data/lineitem.tbl.1", 
            "backend_id" = "10003", 
            "format" = "csv", 
            "column_separator" = "|"
    );
    ```
    Then, run `q1` and `q16` tpch queries, the query result is correct.
    
    It can also analyze the BE's log directly like:
    
    ```
    mysql> select * from local(
            "file_path" = "log/be.out",
            "backend_id" = "10006",
            "format" = "csv")
           where c1 like "%start_time%" limit 10;
    +--------------------------------------------------------+
    | c1                                                     |
    +--------------------------------------------------------+
    | start time: 2023年 08月 07日 星期一 23:20:32 CST       |
    | start time: 2023年 08月 07日 星期一 23:32:10 CST       |
    | start time: 2023年 08月 08日 星期二 00:20:50 CST       |
    | start time: 2023年 08月 08日 星期二 00:29:15 CST       |
    +--------------------------------------------------------+
    ```

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1111,6 +1111,9 @@ DECLARE_Int32(hdfs_hedged_read_thread_num);
 // Maybe overwritten by the value specified when creating catalog
 DECLARE_Int32(hdfs_hedged_read_threshold_time);
 
+// The secure path with user files, used in the `local` table function.
+DECLARE_mString(user_files_secure_path);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
