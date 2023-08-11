209f36f1bff591ef474774052a7997f050dab62a
slothever <18522955+wsjz@users.noreply.github.com>
2023-08-11 14:36:19 +0800

    [fix](multi-catalog)fix jdbc loader (#22814)

--- be/src/util/jni-util.cpp
+++ be/src/util/jni-util.cpp
@@ -374,12 +374,11 @@ Status JniUtil::init_jni_scanner_loader(JNIEnv* env) {
     return Status::OK();
 }
 
-Status JniUtil::get_jni_scanner_class(JNIEnv* env, std::string classname,
+Status JniUtil::get_jni_scanner_class(JNIEnv* env, const char* classname,
                                       jclass* jni_scanner_class) {
     // Get JNI scanner class by class name;
-    jobject loaded_class_obj =
-            env->CallObjectMethod(jni_scanner_loader_obj_, jni_scanner_loader_method_,
-                                  env->NewStringUTF(classname.c_str()));
+    jobject loaded_class_obj = env->CallObjectMethod(
+            jni_scanner_loader_obj_, jni_scanner_loader_method_, env->NewStringUTF(classname));
     RETURN_ERROR_IF_EXC(env);
     *jni_scanner_class = reinterpret_cast<jclass>(env->NewGlobalRef(loaded_class_obj));
     RETURN_ERROR_IF_EXC(env);
--- be/src/vec/exec/jni_connector.cpp
+++ be/src/vec/exec/jni_connector.cpp
@@ -201,7 +201,8 @@ Status JniConnector::close() {
 }
 
 Status JniConnector::_init_jni_scanner(JNIEnv* env, int batch_size) {
-    RETURN_IF_ERROR(JniUtil::get_jni_scanner_class(env, _connector_class, &_jni_scanner_cls));
+    RETURN_IF_ERROR(
+            JniUtil::get_jni_scanner_class(env, _connector_class.c_str(), &_jni_scanner_cls));
     if (_jni_scanner_cls == NULL) {
         if (env->ExceptionOccurred()) env->ExceptionDescribe();
         return Status::InternalError("Fail to get JniScanner class.");
