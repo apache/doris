ae6509c2a5d9fdcf452c7b6608729bf92baf2238
bobhan1 <bh2444151092@outlook.com>
2023-08-11 21:12:35 +0800

    [Enhancement](merge-on-write) add correctness check for the calculation of delete bitmap (#22282)
    Currently, for merge-on-write unique table, the delete bitmap of a rowset will be calculated during flush phase, commit phase and publish phase. In this PR, we add a special mark in every rowset considered when we calculate delete bitmap in these three phases. Before we finally merge the delete bitmap to the table meta's delete bitmap, we will check if all the rowsets contain the special mark to check if we have considered all the rowsets during the above three phases.
    Because the executor can not fail in publish phase if the coordinator have received successful commits info from all the executors, we just print logs if this correctness check failed rather than report a failure.

--- be/src/olap/tablet.h
+++ be/src/olap/tablet.h
@@ -549,6 +549,8 @@ public:
     int64_t binlog_max_bytes() const { return _tablet_meta->binlog_config().max_bytes(); }
 
     void set_binlog_config(BinlogConfig binlog_config);
+    void add_sentinel_mark_to_delete_bitmap(DeleteBitmapPtr delete_bitmap,
+                                            const RowsetIdUnorderedSet& rowsetids);
 
 private:
     Status _init_once_action();
@@ -596,6 +598,10 @@ private:
     ////////////////////////////////////////////////////////////////////////////
     // end cooldown functions
     ////////////////////////////////////////////////////////////////////////////
+
+    void _remove_sentinel_mark_from_delete_bitmap(DeleteBitmapPtr delete_bitmap);
+    Status _check_delete_bitmap_correctness(DeleteBitmapPtr delete_bitmap, int64_t max_version,
+                                            const RowsetIdUnorderedSet& rowset_ids) const;
 
 public:
     static const int64_t K_INVALID_CUMULATIVE_POINT = -1;
