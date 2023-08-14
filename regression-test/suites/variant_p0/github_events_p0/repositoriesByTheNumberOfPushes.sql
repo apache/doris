7e2d798899735a86472d76e0d907d125cbea7ec5
eldenmoon <15605149486@163.com>
2023-08-14 17:55:57 +0800

    add github events test

---
+++ regression-test/suites/variant_p0/github_events_p0/repositoriesByTheNumberOfPushes.sql
@@ -0,0 +1,18 @@
+--ERROR: crash column.h:496] not support
+SELECT
+    cast(v:repo.name as string),
+    count() AS pushes,
+    count(distinct cast(v:actor.login as string)) AS authors
+FROM github_events
+WHERE (cast(v:type as string) = 'PushEvent') AND (cast(v:repo.name as string) IN
+(
+    SELECT cast(v:repo.name as string)
+    FROM github_events
+    WHERE cast(v:type as string) = 'WatchEvent'
+    GROUP BY cast(v:repo.name as string)
+    ORDER BY count() DESC
+    LIMIT 10000
+))
+GROUP BY cast(v:repo.name as string)
+ORDER BY count() DESC
+LIMIT 50
---
+++ regression-test/suites/variant_p0/github_events_p0/whoAreAllThosePeopleGivingStars3.sql
@@ -0,0 +1,14 @@
+-- ERROR: unmatched column
+SELECT
+    cast(v:repo.name as string),
+    count() AS stars
+FROM github_events
+WHERE (cast(v:type as string) = 'WatchEvent') AND (cast(v:repo.name as string) IN
+(
+    SELECT cast(v:repo.name as string)
+    FROM github_events
+    WHERE (cast(v:type as string) = 'WatchEvent') AND (cast(v:actor.login as string) = 'cliffordfajardo')
+))
+GROUP BY cast(v:repo.name as string)
+ORDER BY stars DESC
+LIMIT 50
