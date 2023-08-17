420096732c9343c8918b2a5e7bc35d648583a3f6
TengJianPing <18241664+jacktengg@users.noreply.github.com>
2023-08-17 09:17:09 +0800

    [improvement](hash join) Return result early if probe side has no data (#23044)

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -229,6 +229,8 @@ struct TQueryOptions {
   76: optional bool enable_inverted_index_query = true;
 
   77: optional bool truncate_char_or_varchar_columns = false
+
+  78: optional bool enable_hash_join_early_start_probe = false
 }
 
 
