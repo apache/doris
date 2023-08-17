7daf2afeb9cd4da8f19cf19df344ea13665ed5cd
tsy <tangsiyang2001@foxmail.com>
2023-08-17 13:12:31 +0800

    add dft val case

--- fe/fe-core/src/main/java/org/apache/doris/planner/FileLoadScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/FileLoadScanNode.java
@@ -255,7 +255,6 @@ public class FileLoadScanNode extends FileScanNode {
                     if (column.getDefaultValue() != null) {
                         if (column.getDefaultValueExprDef() != null) {
                             expr = column.getDefaultValueExpr();
-                            expr.analyze(analyzer);
                         } else {
                             expr = new StringLiteral(destSlotDesc.getColumn().getDefaultValue());
                         }
