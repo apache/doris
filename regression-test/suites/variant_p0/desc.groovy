a3cf9acfa4e14bb98d25d2b194c6e92421854422
Chenyang Sun <csun5285@gmail.com>
2023-08-17 17:32:59 +0800

    [improve](variant)Desc variant (#23111)
    support desc variant with remote fetch
    
    ---------
    
    Co-authored-by: eldenmoon <15605149486@163.com>

---
+++ regression-test/suites/variant_p0/desc.groovy
@@ -0,0 +1,174 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("regression_test_variant_desc", "variant_type_desc"){
+
+    def load_json_data = {table_name, file_name ->
+        // load the json data
+        streamLoad {
+            table "${table_name}"
+
+            // set http request header params
+            set 'read_json_by_line', 'true' 
+            set 'format', 'json' 
+            set 'max_filter_ratio', '0.1'
+            file file_name // import json file
+            time 10000 // limit inflight 10s
+
+            // if declared a check callback, the default check condition will ignore.
+            // So you must check all condition
+
+            check { result, exception, startTime, endTime ->
+                if (exception != null) {
+                        throw exception
+                }
+                logger.info("Stream load ${file_name} result: ${result}".toString())
+                def json = parseJson(result)
+                assertEquals("success", json.Status.toLowerCase())
+                // assertEquals(json.NumberTotalRows, json.NumberLoadedRows + json.NumberUnselectedRows)
+                assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
+            }
+        }
+    }
+
+    def create_table = { table_name, buckets="auto" ->
+        sql "DROP TABLE IF EXISTS ${table_name}"
+        sql """
+            CREATE TABLE IF NOT EXISTS ${table_name} (
+                k bigint,
+                v variant
+            )
+            DUPLICATE KEY(`k`)
+            DISTRIBUTED BY HASH(k) BUCKETS ${buckets}
+            properties("replication_num" = "1", "disable_auto_compaction" = "false");
+        """
+    }
+
+    def create_table_partition = { table_name, buckets="auto" ->
+        sql "DROP TABLE IF EXISTS ${table_name}"
+        sql """
+            CREATE TABLE IF NOT EXISTS ${table_name} (
+                k bigint,
+                v variant
+            )
+            DUPLICATE KEY(`k`)
+            PARTITION BY RANGE(k)
+            (
+                PARTITION p1 VALUES LESS THAN (3000),
+                PARTITION p2 VALUES LESS THAN (50000),
+                PARTITION p3 VALUES LESS THAN (100000)
+            )
+            DISTRIBUTED BY HASH(k) BUCKETS ${buckets}
+            properties("replication_num" = "1", "disable_auto_compaction" = "false");
+        """
+    }
+
+    def set_be_config = { key, value ->
+        String backend_id;
+        def backendId_to_backendIP = [:]
+        def backendId_to_backendHttpPort = [:]
+        getBackendIpHttpPort(backendId_to_backendIP, backendId_to_backendHttpPort);
+
+        backend_id = backendId_to_backendIP.keySet()[0]
+        def (code, out, err) = update_be_config(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), key, value)
+        logger.info("update config: code=" + code + ", out=" + out + ", err=" + err)
+    }
+
+    try {
+        // sparse columns
+        def table_name = "sparse_columns"
+        create_table table_name
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "0.95")
+        sql """insert into  sparse_columns select 0, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}'  as json_str
+            union  all select 0, '{"a": 1123}' as json_str union all select 0, '{"a" : 1234, "xxxx" : "kaana"}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        qt_sql_1 """desc ${table_name}"""
+        sql "truncate table sparse_columns"
+        sql """insert into  sparse_columns select 0, '{"a": 1123, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}, "zzz" : null, "oooo" : {"akakaka" : null, "xxxx" : {"xxx" : 123}}}'  as json_str
+            union  all select 0, '{"a" : 1234, "xxxx" : "kaana", "ddd" : {"aaa" : 123, "mxmxm" : [456, "789"]}}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        qt_sql_2 """desc ${table_name}"""
+        sql "truncate table sparse_columns"
+
+        // no sparse columns
+        table_name = "no_sparse_columns"
+        create_table.call(table_name, "4")
+        sql "set enable_two_phase_read_opt = false;"
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "1")
+        sql """insert into  ${table_name} select 0, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}'  as json_str
+            union  all select 0, '{"a": 1123}' as json_str union all select 0, '{"a" : 1234, "xxxx" : "kaana"}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        qt_sql_3 """desc ${table_name}"""
+        sql "truncate table ${table_name}"
+
+        // always sparse column
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "0")
+        sql """insert into  ${table_name} select 0, '{"a": 1123, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}, "zzz" : null, "oooo" : {"akakaka" : null, "xxxx" : {"xxx" : 123}}}'  as json_str
+            union  all select 0, '{"a" : 1234, "xxxx" : "kaana", "ddd" : {"aaa" : 123, "mxmxm" : [456, "789"]}}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        qt_sql_5 """desc ${table_name}"""
+        sql "truncate table ${table_name}"
+
+        // partititon
+        table_name = "partition_data"
+        create_table_partition.call(table_name, "4")
+        sql "set enable_two_phase_read_opt = false;"
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "0.95")
+        sql """insert into  ${table_name} select 2500, '{"a": 1123, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}, "zzz" : null, "oooo" : {"akakaka" : null, "xxxx" : {"xxx" : 123}}}'  as json_str
+            union  all select 2500, '{"a" : 1234, "xxxx" : "kaana", "ddd" : {"aaa" : 123, "mxmxm" : [456, "789"]}}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        sql """insert into  ${table_name} select 45000, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}'  as json_str
+            union  all select 45000, '{"a": 1123}' as json_str union all select 45000, '{"a" : 1234, "xxxx" : "kaana"}' as json_str from numbers("number" = "4096") limit 4096 ;"""
+        sql """insert into  ${table_name} values(95000, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}')"""
+        qt_sql_6_1 """desc ${table_name} partition p1"""
+        qt_sql_6_2 """desc ${table_name} partition p2"""
+        qt_sql_6_3 """desc ${table_name} partition p3"""
+        qt_sql_6 """desc ${table_name}"""
+        sql "truncate table ${table_name}"
+
+        // drop partition
+        table_name = "drop_partition"
+        create_table_partition.call(table_name, "4")
+        // insert into partition p1
+        sql """insert into  ${table_name} values(2500, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}')"""
+        // insert into partition p2
+        sql """insert into  ${table_name} values(45000, '{"a": 11245, "xxxx" : "kaana"}')"""
+        // insert into partition p3
+         sql """insert into  ${table_name} values(95000, '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}')"""
+        // drop p1
+        sql """alter table ${table_name} drop partition p1"""
+        qt_sql_7 """desc ${table_name}"""
+        qt_sql_7_1 """desc ${table_name} partition p2"""
+        qt_sql_7_2 """desc ${table_name} partition p3"""
+        sql "truncate table ${table_name}"
+
+        // more variant
+        table_name = "more_variant_table"
+        sql """
+            CREATE TABLE IF NOT EXISTS ${table_name} (
+                k bigint,
+                v1 variant,
+                v2 variant,
+                v3 variant
+            )
+            DUPLICATE KEY(`k`)
+            DISTRIBUTED BY HASH(k) BUCKETS 5
+            properties("replication_num" = "1", "disable_auto_compaction" = "false");
+        """
+        sql """ insert into ${table_name} values (0, '{"a": 1123, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}, "zzz" : null, "oooo" : {"akakaka" : null, "xxxx" : {"xxx" : 123}}}', '{"a": 11245, "xxxx" : "kaana"}', '{"a": 11245, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}')"""
+        qt_sql_8 """desc ${table_name}"""
+        sql "truncate table ${table_name}"
+    } finally {
+        // reset flags
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "0.95")
+    }
+}
