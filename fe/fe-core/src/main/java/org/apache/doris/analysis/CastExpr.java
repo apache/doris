3cf4da14aa3978889bedeafa54aacb2209a36794
LiBinfeng <46676950+LiBinfeng-01@users.noreply.github.com>
2023-08-17 16:37:07 +0800

    [Fix](Planner) fix case function with null cast to array null (#22947)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
@@ -314,7 +314,7 @@ public class CastExpr extends Expr {
         // it is necessary to check if it is castable before creating fn.
         // char type will fail in canCastTo, so for compatibility, only the cast of array type is checked here.
         if (type.isArrayType() || childType.isArrayType()) {
-            if (childType.isNull() || !Type.canCastTo(childType, type)) {
+            if (!Type.canCastTo(childType, type)) {
                 throw new AnalysisException("Invalid type cast of " + getChild(0).toSql()
                         + " from " + childType + " to " + type);
             }
