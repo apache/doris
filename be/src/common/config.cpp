c8241eeb6f26eec472e5f201109935884aec9d3f
Kaijie Chen <ckj@apache.org>
2023-08-17 17:45:30 +0800

    [feature](move-memtable)[5/7] add olap table sink v2 and writers
    Co-authored-by: laihui <1353307710@qq.com>

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -723,6 +723,11 @@ DEFINE_mInt32(mem_tracker_consume_min_size_bytes, "1048576");
 // In most cases, it does not need to be modified.
 DEFINE_mDouble(tablet_version_graph_orphan_vertex_ratio, "0.1");
 
+// number of brpc stream per OlapTableSinkV2
+DEFINE_Int32(num_streams_per_sink, "5");
+// timeout for open stream sink rpc in ms
+DEFINE_Int64(open_stream_sink_timeout_ms, "500");
+
 // max send batch parallelism for OlapTableSink
 // The value set by the user for send_batch_parallelism is not allowed to exceed max_send_batch_parallelism_per_job,
 // if exceed, the value of send_batch_parallelism would be max_send_batch_parallelism_per_job
