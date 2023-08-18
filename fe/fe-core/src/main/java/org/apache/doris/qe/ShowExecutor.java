9a0561e67b8929318016a0fb495892642906fe73
Mingyu Chen <morningman@163.com>
2023-08-18 18:30:09 +0800

    [fix](show-table-status) fix priv error on show table status stmt (#22918)

--- fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -861,7 +861,8 @@ public class ShowExecutor {
 
                 // check tbl privs
                 if (!Env.getCurrentEnv().getAccessManager()
-                        .checkTblPriv(ConnectContext.get(), db.getFullName(), table.getName(), PrivPredicate.SHOW)) {
+                        .checkTblPriv(ConnectContext.get(), showStmt.getCatalog(),
+                                db.getFullName(), table.getName(), PrivPredicate.SHOW)) {
                     continue;
                 }
                 List<String> row = Lists.newArrayList();
