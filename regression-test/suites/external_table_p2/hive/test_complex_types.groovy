4bf055c818f995cbb16d2f5330ac7c5ff572545d
Ashin Gau <AshinGau@users.noreply.github.com>
2023-08-20 22:59:18 +0800

    [fix](parquet) the key colum of map type in parquet may be nullable (#23180)
    Fix errors when reading map type with nullable key column in parquet file. `ParquetReader` support to read nullable key column, but add a check to prevent reading nullable key column. Unfortunately, this check error was not thrown correctly, causing the BE to crash, and thrown meaningless error logs in be.out:
    ```
    ...
    11# doris::vectorized::ParquetReader::get_columns(std::unordered_map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, doris::TypeDescriptor, std::hash<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::equal_to<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::allocator<std::pair<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const, doris::TypeDescriptor> > >*, std::unordered_set<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::hash<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::equal_to<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::allocator<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > >*) at /root/doris/be/src/vec/exec/format/parquet/vparquet_reader.cpp:508
    12# doris::vectorized::VFileScanner::_get_next_reader() in /root/yun_you_external/output/be/lib/doris_be
    13# doris::vectorized::VFileScanner::_get_block_impl(doris::RuntimeState*, doris::vectorized::Block*, bool*) at /root/doris/be/src/vec/exec/scan/vfile_scanner.cpp:241
    ...
    ```

--- regression-test/suites/external_table_p2/hive/test_complex_types.groovy
+++ regression-test/suites/external_table_p2/hive/test_complex_types.groovy
@@ -52,5 +52,9 @@ suite("test_complex_types", "p2,external,hive,external_remote,external_remote_hi
         qt_array_last """select max(array_last(i -> i > 0, capacity)) from byd where array_last(i -> i > 0, capacity) < 0.99"""
 
         qt_offsets_check """select * from complex_offsets_check order by id"""
+
+        qt_map_with_nullable_key """select * from parquet_all_types limit 1"""
+
+        sql """drop catalog ${catalog_name};"""
     }
 }
