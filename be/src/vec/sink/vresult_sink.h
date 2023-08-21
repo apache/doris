9ec5c6102bc20934afcf05966d4b58bb4f108a8c
Gabriel <gabrielleebuaa@gmail.com>
2023-08-21 16:03:32 +0800

    [pipelineX](projection) Support projection and blocking agg

--- be/src/vec/sink/vresult_sink.h
+++ be/src/vec/sink/vresult_sink.h
@@ -118,6 +118,9 @@ struct ResultFileOptions {
     }
 };
 
+
+constexpr int RESULT_SINK_BUFFER_SIZE = 4096;
+
 class VResultSink : public DataSink {
 public:
     friend class pipeline::ResultSinkOperator;
