c749bc93af014f6c414b8e23390ffbf573124534
starocean999 <40539150+starocean999@users.noreply.github.com>
2023-08-24 18:14:43 +0800

    [fix](nereids) remove useless cast in in-predicate (#23171)
    consider sql "select * from test_simplify_in_predicate_t where a in ('1992-01-31', '1992-02-01', '1992-02-02', '1992-02-03', '1992-02-04');"
    before:
    
    ```
    |   0:VOlapScanNode                                                                                                                                                                                      |
    |      TABLE: default_cluster:bugfix.test_simplify_in_predicate_t(test_simplify_in_predicate_t), PREAGGREGATION: OFF. Reason: No aggregate on scan.                                                      |
    |      PREDICATES: CAST(a[#0] AS DATETIMEV2(0)) IN ('1992-01-31 00:00:00', '1992-02-01 00:00:00', '1992-02-02 00:00:00', '1992-02-03 00:00:00', '1992-02-04 00:00:00') AND __DORIS_DELETE_SIGN__[#1] = 0 |
    |      partitions=0/1, tablets=0/0, tabletList=                                                                                                                                                          |
    |      cardinality=1, avgRowSize=0.0, numNodes=1                                                                                                                                                         |
    |      pushAggOp=NONE                                                                                                                                                                                    |
    |      projections: a[#0]                                                                                                                                                                                |
    |      project output tuple id: 1                                                                                                                                                                        |
    |      tuple ids: 0  
    ```
    after:
    
    ```
    |   0:VOlapScanNode                                                                                                                                 |
    |      TABLE: default_cluster:bugfix.test_simplify_in_predicate_t(test_simplify_in_predicate_t), PREAGGREGATION: OFF. Reason: No aggregate on scan. |
    |      PREDICATES: a[#0] IN ('1992-01-31', '1992-02-01', '1992-02-02', '1992-02-03', '1992-02-04') AND __DORIS_DELETE_SIGN__[#1] = 0                |
    |      partitions=0/1, tablets=0/0, tabletList=                                                                                                     |
    |      cardinality=1, avgRowSize=0.0, numNodes=1                                                                                                    |
    |      pushAggOp=NONE                                                                                                                               |
    |      projections: a[#0]                                                                                                                           |
    |      project output tuple id: 1                                                                                                                   |
    |      tuple ids: 0  
    
    ```

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionOptimization.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionOptimization.java
@@ -22,6 +22,7 @@ import org.apache.doris.nereids.rules.expression.rules.ExtractCommonFactorRule;
 import org.apache.doris.nereids.rules.expression.rules.OrToIn;
 import org.apache.doris.nereids.rules.expression.rules.SimplifyComparisonPredicate;
 import org.apache.doris.nereids.rules.expression.rules.SimplifyDecimalV3Comparison;
+import org.apache.doris.nereids.rules.expression.rules.SimplifyInPredicate;
 import org.apache.doris.nereids.rules.expression.rules.SimplifyRange;
 
 import com.google.common.collect.ImmutableList;
@@ -36,6 +37,7 @@ public class ExpressionOptimization extends ExpressionRewrite {
             ExtractCommonFactorRule.INSTANCE,
             DistinctPredicatesRule.INSTANCE,
             SimplifyComparisonPredicate.INSTANCE,
+            SimplifyInPredicate.INSTANCE,
             SimplifyDecimalV3Comparison.INSTANCE,
             SimplifyRange.INSTANCE,
             OrToIn.INSTANCE
