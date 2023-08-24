apache/doris

commit 35d0c9e71e2a117809658b1e5ff228153c56e1f4

[refactor](nereids) Refactor stats collection framework (#22963)
* remove auto analyze grammer
* refactor ResultRow

fe/fe-core/src/main/java/org/apache/doris/analysis/ShowTableStatsStmt.java
@@ -32,12 +32,13 @@ import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.ShowResultSet;
 import org.apache.doris.qe.ShowResultSetMetaData;
-import org.apache.doris.statistics.TableStatistic;
-import org.apache.doris.statistics.util.StatisticsUtil;
+import org.apache.doris.statistics.TableStats;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowTableStatsStmt extends ShowStmt {
@@ -45,9 +46,14 @@ public class ShowTableStatsStmt extends ShowStmt {
     // TODO add more columns
     private static final ImmutableList<String> TITLE_NAMES =
             new ImmutableList.Builder<String>()
-                    .add("row_count")
-                    .add("update_time")
-                    .add("last_analyze_time")
+                    .add("updated_rows")
+                    .add("query_times")
+                    .add("row_count(for external_table only)")
+                    .add("method")
+                    .add("type")
+                    .add("updated_time")
+                    .add("columns")
+                    .add("trigger")
                     .build();
 
     private final TableName tableName;
@@ -126,12 +132,20 @@ public class ShowTableStatsStmt extends ShowStmt {
         return table.getPartition(partitionName).getId();
     }
 
-    public ShowResultSet constructResultSet(TableStatistic tableStatistic) {
+    public ShowResultSet constructResultSet(TableStats tableStatistic) {
+        if (tableStatistic == null) {
+            return new ShowResultSet(getMetaData(), new ArrayList<>());
+        }
         List<List<String>> result = Lists.newArrayList();
         List<String> row = Lists.newArrayList();
+        row.add(String.valueOf(tableStatistic.updatedRows));
+        row.add(String.valueOf(tableStatistic.queriedTimes.get()));
         row.add(String.valueOf(tableStatistic.rowCount));
-        row.add(String.valueOf(tableStatistic.updateTime));
-        row.add(StatisticsUtil.getReadableTime(tableStatistic.lastAnalyzeTimeInMs));
+        row.add(tableStatistic.analysisMethod.toString());
+        row.add(tableStatistic.analysisType.toString());
+        row.add(new Date(tableStatistic.updatedTime).toString());
+        row.add(tableStatistic.columns);
+        row.add(tableStatistic.jobType.toString());
         result.add(row);
         return new ShowResultSet(getMetaData(), result);
     }

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HMSExternalTable.java
@@ -32,7 +32,7 @@ import org.apache.doris.statistics.BaseAnalysisTask;
 import org.apache.doris.statistics.ColumnStatistic;
 import org.apache.doris.statistics.ColumnStatisticBuilder;
 import org.apache.doris.statistics.HMSAnalysisTask;
-import org.apache.doris.statistics.TableStatistic;
+import org.apache.doris.statistics.TableStats;
 import org.apache.doris.statistics.util.StatisticsUtil;
 import org.apache.doris.thrift.THiveTable;
 import org.apache.doris.thrift.TTableDescriptor;
@@ -106,6 +106,9 @@ public class HMSExternalTable extends ExternalTable {
     private List<Column> partitionColumns;
 
     private DLAType dlaType = DLAType.UNKNOWN;
+
+    // No as precise as row count in TableStats, but better than none.
+    private long estimatedRowCount = -1;
 
     public enum DLAType {
         UNKNOWN, HIVE, HUDI, ICEBERG
@@ -422,13 +425,20 @@ public class HMSExternalTable extends ExternalTable {
     @Override
     public long estimatedRowCount() {
         try {
-            Optional<TableStatistic> tableStatistics = Env.getCurrentEnv().getStatisticsCache().getTableStatistics(
-                    catalog.getId(), catalog.getDbOrAnalysisException(dbName).getId(), id);
-            if (tableStatistics.isPresent()) {
-                long rowCount = tableStatistics.get().rowCount;
+            TableStats tableStats = Env.getCurrentEnv().getAnalysisManager().findTableStatsStatus(id);
+            if (tableStats != null) {
+                long rowCount = tableStats.rowCount;
                 LOG.debug("Estimated row count for db {} table {} is {}.", dbName, name, rowCount);
                 return rowCount;
             }
+
+            if (estimatedRowCount != -1) {
+                return estimatedRowCount;
+            }
+            // Cache the estimated row count in this structure
+            // though the table never get analyzed, since the row estimation might be expensive caused by RPC.
+            estimatedRowCount = getRowCount();
+            return estimatedRowCount;
         } catch (Exception e) {
             LOG.warn("Fail to get row count for table {}", name, e);
         }

fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -138,7 +138,6 @@ import org.apache.doris.catalog.Tablet;
 import org.apache.doris.catalog.TabletInvertedIndex;
 import org.apache.doris.catalog.TabletMeta;
 import org.apache.doris.catalog.View;
-import org.apache.doris.catalog.external.ExternalTable;
 import org.apache.doris.catalog.external.HMSExternalTable;
 import org.apache.doris.clone.DynamicPartitionScheduler;
 import org.apache.doris.cluster.ClusterNamespace;
@@ -199,7 +198,7 @@ import org.apache.doris.statistics.AnalysisInfo;
 import org.apache.doris.statistics.ColumnStatistic;
 import org.apache.doris.statistics.Histogram;
 import org.apache.doris.statistics.StatisticsRepository;
-import org.apache.doris.statistics.TableStatistic;
+import org.apache.doris.statistics.TableStats;
 import org.apache.doris.statistics.query.QueryStatsUtil;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.Diagnoser;
@@ -242,7 +241,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
@@ -2452,30 +2450,8 @@ public class ShowExecutor {
     private void handleShowTableStats() {
         ShowTableStatsStmt showTableStatsStmt = (ShowTableStatsStmt) stmt;
         TableIf tableIf = showTableStatsStmt.getTable();
-        long partitionId = showTableStatsStmt.getPartitionId();
-        boolean showCache = showTableStatsStmt.isCached();
-        try {
-            if (tableIf instanceof ExternalTable && showCache) {
-                Optional<TableStatistic> tableStatistics = Env.getCurrentEnv().getStatisticsCache().getTableStatistics(
-                        tableIf.getDatabase().getCatalog().getId(),
-                        tableIf.getDatabase().getId(),
-                        tableIf.getId());
-                if (tableStatistics.isPresent()) {
-                    resultSet = showTableStatsStmt.constructResultSet(tableStatistics.get());
-                } else {
-                    resultSet = showTableStatsStmt.constructResultSet(TableStatistic.UNKNOWN);
-                }
-            } else if (partitionId > 0) {
-                TableStatistic partStats = StatisticsRepository.fetchTableLevelOfPartStats(partitionId);
-                resultSet = showTableStatsStmt.constructResultSet(partStats);
-            } else {
-                TableStatistic tableStats = StatisticsRepository.fetchTableLevelStats(tableIf.getId());
-                resultSet = showTableStatsStmt.constructResultSet(tableStats);
-            }
-        } catch (DdlException e) {
-            LOG.warn("Table statistics do not exist: {}", tableIf.getName());
-            resultSet = showTableStatsStmt.constructResultSet(TableStatistic.UNKNOWN);
-        }
+        TableStats tableStats = Env.getCurrentEnv().getAnalysisManager().findTableStatsStatus(tableIf.getId());
+        resultSet = showTableStatsStmt.constructResultSet(tableStats);
     }
 
     private void handleShowColumnStats() throws AnalysisException {

fe/fe-core/src/main/java/org/apache/doris/statistics/HMSAnalysisTask.java
@@ -24,7 +24,6 @@ import org.apache.doris.common.util.TimeUtils;
 import org.apache.doris.qe.AutoCloseConnectContext;
 import org.apache.doris.qe.QueryState;
 import org.apache.doris.qe.StmtExecutor;
-import org.apache.doris.statistics.util.InternalQueryResult;
 import org.apache.doris.statistics.util.StatisticsUtil;
 
 import org.apache.commons.lang3.StringUtils;
@@ -117,39 +116,14 @@ public class HMSAnalysisTask extends BaseAnalysisTask {
      * Get table row count and insert the result to __internal_schema.table_statistics
      */
     private void getTableStats() throws Exception {
-        // Get table level information. An example sql for table stats:
-        // INSERT INTO __internal_schema.table_statistics VALUES
-        //   ('13055', 13002, 13038, 13055, -1, 'NULL', 5, 1686111064658, NOW())
-        Map<String, String> parameters = table.getRemoteTable().getParameters();
-        if (isPartitionOnly) {
-            for (String partId : partitionNames) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(ANALYZE_SQL_PARTITION_TEMPLATE);
-                sb.append(" where ");
-                String[] splits = partId.split("/");
-                for (int i = 0; i < splits.length; i++) {
-                    String value = splits[i].split("=")[1];
-                    splits[i] = splits[i].replace(value, "\'" + value + "\'");
-                }
-                sb.append(StringUtils.join(splits, " and "));
-                Map<String, String> params = buildTableStatsParams(partId);
-                setParameterData(parameters, params);
-                List<InternalQueryResult.ResultRow> columnResult =
-                        StatisticsUtil.execStatisticQuery(new StringSubstitutor(params)
-                        .replace(sb.toString()));
-                String rowCount = columnResult.get(0).getColumnValue("rowCount");
-                params.put("rowCount", rowCount);
-                StatisticsRepository.persistTableStats(params);
-            }
-        } else {
-            Map<String, String> params = buildTableStatsParams(null);
-            List<InternalQueryResult.ResultRow> columnResult =
-                    StatisticsUtil.execStatisticQuery(new StringSubstitutor(params)
-                    .replace(ANALYZE_TABLE_COUNT_TEMPLATE));
-            String rowCount = columnResult.get(0).getColumnValue("rowCount");
-            params.put("rowCount", rowCount);
-            StatisticsRepository.persistTableStats(params);
-        }
+        Map<String, String> params = buildTableStatsParams(null);
+        List<ResultRow> columnResult =
+                StatisticsUtil.execStatisticQuery(new StringSubstitutor(params)
+                        .replace(ANALYZE_TABLE_COUNT_TEMPLATE));
+        String rowCount = columnResult.get(0).get(0);
+        Env.getCurrentEnv().getAnalysisManager()
+                .updateTableStatsStatus(
+                        new TableStats(table.getId(), Long.parseLong(rowCount), null));
     }
 
     /**
@@ -295,10 +269,6 @@ public class HMSAnalysisTask extends BaseAnalysisTask {
 
     @Override
     protected void afterExecution() {
-        if (isTableLevelTask) {
-            Env.getCurrentEnv().getStatisticsCache().refreshTableStatsSync(catalog.getId(), db.getId(), tbl.getId());
-        } else {
-            Env.getCurrentEnv().getStatisticsCache().syncLoadColStats(tbl.getId(), -1, col.getName());
-        }
+        Env.getCurrentEnv().getStatisticsCache().syncLoadColStats(tbl.getId(), -1, col.getName());
     }
 }

fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2087,7 +2087,7 @@ public class Config extends ConfigBase {
     public static int force_olap_table_replication_num = 0;
 
     @ConfField
-    public static int full_auto_analyze_simultaneously_running_task_num = 5;
+    public static int full_auto_analyze_simultaneously_running_task_num = 1;
 
     @ConfField
     public static int cpu_resource_limit_per_analyze_task = 1;
@@ -2114,4 +2114,7 @@ public class Config extends ConfigBase {
 
     @ConfField
     public static boolean forbid_running_alter_job = false;
+
+    @ConfField
+    public static int table_stats_health_threshold = 80;
 }

fe/fe-core/src/main/cup/sql_parser.cup
@@ -5998,12 +5998,6 @@ with_analysis_properties ::=
             put("incremental", "true");
         }};
     :}
-    | KW_AUTO
-    {:
-        RESULT = new HashMap<String, String>() {{
-            put("automatic", "true");
-        }};
-    :}
     | KW_SAMPLE KW_PERCENT INTEGER_LITERAL:samplePercent
     {:
         RESULT = new HashMap<String, String>() {{

fe/fe-core/src/main/java/org/apache/doris/analysis/AnalyzeProperties.java
@@ -22,6 +22,7 @@ import org.apache.doris.common.util.PrintableMap;
 import org.apache.doris.statistics.AnalysisInfo.AnalysisType;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.gson.annotations.SerializedName;
 import org.apache.commons.lang3.StringUtils;
 import org.quartz.CronExpression;
 
@@ -31,8 +32,6 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class AnalyzeProperties {
-
-    private final Map<String, String> properties;
 
     public static final String PROPERTY_SYNC = "sync";
     public static final String PROPERTY_INCREMENTAL = "incremental";
@@ -54,6 +53,9 @@ public class AnalyzeProperties {
     public static final String PROPERTY_PERIOD_CRON = "period.cron";
 
     private CronExpression cronExpression;
+
+    @SerializedName("analyzeProperties")
+    private final Map<String, String> properties;
 
     private static final ImmutableSet<String> PROPERTIES_SET = new ImmutableSet.Builder<String>()
             .add(PROPERTY_SYNC)

commit 966561a6ed83433bfadd6b0eadb856506cab25d8

[improvement and fix](statistics)Load the cache for external table row count while init table (#23170)
1. Load the cache for external table row count while init table, this could avoid no row number stats for the very first time to run an sql.
2. Show cardinality for an external scan node when explain the sql.
3. fix bugs introduced by https://github.com/apache/doris/pull/22963

fe/fe-core/src/main/java/org/apache/doris/analysis/ShowTableStatsStmt.java
@@ -150,6 +150,21 @@ public class ShowTableStatsStmt extends ShowStmt {
         return new ShowResultSet(getMetaData(), result);
     }
 
+    public ShowResultSet constructResultSet(long rowCount) {
+        List<List<String>> result = Lists.newArrayList();
+        List<String> row = Lists.newArrayList();
+        row.add("");
+        row.add("");
+        row.add(String.valueOf(rowCount));
+        row.add("");
+        row.add("");
+        row.add("");
+        row.add("");
+        row.add("");
+        result.add(row);
+        return new ShowResultSet(getMetaData(), result);
+    }
+
     public boolean isCached() {
         return cached;
     }

fe/fe-core/src/main/java/org/apache/doris/catalog/external/ExternalTable.java
@@ -72,6 +72,7 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
     @SerializedName(value = "lastUpdateTime")
     protected long lastUpdateTime;
 
+    protected long dbId;
     protected boolean objectCreated;
     protected ExternalCatalog catalog;
     protected ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock(true);
@@ -113,6 +114,7 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
         try {
             // getDbOrAnalysisException will call makeSureInitialized in ExternalCatalog.
             ExternalDatabase db = catalog.getDbOrAnalysisException(dbName);
+            dbId = db.getId();
             db.makeSureInitialized();
         } catch (AnalysisException e) {
             Util.logAndThrowRuntimeException(LOG, String.format("Exception to get db %s", dbName), e);

fe/fe-core/src/main/java/org/apache/doris/catalog/external/HMSExternalTable.java
@@ -149,6 +149,7 @@ public class HMSExternalTable extends ExternalTable {
                 }
             }
             objectCreated = true;
+            estimatedRowCount = getRowCountFromExternalSource();
         }
     }
 
@@ -272,6 +273,15 @@ public class HMSExternalTable extends ExternalTable {
     @Override
     public long getRowCount() {
         makeSureInitialized();
+        long rowCount = getRowCountFromExternalSource();
+        if (rowCount == -1) {
+            LOG.debug("Will estimate row count from file list.");
+            rowCount = StatisticsUtil.getRowCountFromFileList(this);
+        }
+        return rowCount;
+    }
+
+    private long getRowCountFromExternalSource() {
         long rowCount;
         switch (dlaType) {
             case HIVE:
@@ -283,10 +293,6 @@ public class HMSExternalTable extends ExternalTable {
             default:
                 LOG.warn("getRowCount for dlaType {} is not supported.", dlaType);
                 rowCount = -1;
-        }
-        if (rowCount == -1) {
-            LOG.debug("Will estimate row count from file list.");
-            rowCount = StatisticsUtil.getRowCountFromFileList(this);
         }
         return rowCount;
     }

fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -437,7 +437,9 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
         TableRef ref = new TableRef(tableName, null, null);
         BaseTableRef tableRef = new BaseTableRef(ref, table, tableName);
         tupleDescriptor.setRef(tableRef);
-
+        if (fileScan.getStats() != null) {
+            scanNode.setCardinality((long) fileScan.getStats().getRowCount());
+        }
         Utils.execWithUncheckedException(scanNode::init);
         context.addScanNode(scanNode);
         ScanNode finalScanNode = scanNode;

fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -2451,7 +2451,16 @@ public class ShowExecutor {
         ShowTableStatsStmt showTableStatsStmt = (ShowTableStatsStmt) stmt;
         TableIf tableIf = showTableStatsStmt.getTable();
         TableStats tableStats = Env.getCurrentEnv().getAnalysisManager().findTableStatsStatus(tableIf.getId());
-        resultSet = showTableStatsStmt.constructResultSet(tableStats);
+        /*
+           HMSExternalTable table will fetch row count from HMS
+           or estimate with file size and schema if it's not analyzed.
+           tableStats == null means it's not analyzed, in this case show the estimated row count.
+         */
+        if (tableStats == null && tableIf instanceof HMSExternalTable) {
+            resultSet = showTableStatsStmt.constructResultSet(tableIf.estimatedRowCount());
+        } else {
+            resultSet = showTableStatsStmt.constructResultSet(tableStats);
+        }
     }
 
     private void handleShowColumnStats() throws AnalysisException {

fe/fe-core/src/main/java/org/apache/doris/statistics/AnalysisManager.java
@@ -32,6 +32,7 @@ import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.TableIf;
 import org.apache.doris.catalog.View;
+import org.apache.doris.catalog.external.ExternalTable;
 import org.apache.doris.catalog.external.HMSExternalTable;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.Config;
@@ -285,9 +286,7 @@ public class AnalysisManager extends Daemon implements Writable {
                 // columnNames null means to add all visitable columns.
                 // Will get all the visible columns in analyzeTblStmt.check()
                 AnalyzeTblStmt analyzeTblStmt = new AnalyzeTblStmt(analyzeProperties, tableName,
-                        table.getBaseSchema().stream().filter(c -> !StatisticsUtil.isUnsupportedType(c.getType())).map(
-                                Column::getName).collect(
-                                Collectors.toList()), db.getId(), table);
+                        null, db.getId(), table);
                 try {
                     analyzeTblStmt.check();
                 } catch (AnalysisException analysisException) {
@@ -334,11 +333,10 @@ public class AnalysisManager extends Daemon implements Writable {
 
         boolean isSync = stmt.isSync();
         Map<Long, BaseAnalysisTask> analysisTaskInfos = new HashMap<>();
+        createTaskForEachColumns(jobInfo, analysisTaskInfos, isSync);
         if (stmt.isAllColumns()
                 && StatisticsUtil.isExternalTable(jobInfo.catalogName, jobInfo.dbName, jobInfo.tblName)) {
-            createTaskForExternalTable(jobInfo, analysisTaskInfos, isSync);
-        } else {
-            createTaskForEachColumns(jobInfo, analysisTaskInfos, isSync);
+            createTableLevelTaskForExternalTable(jobInfo, analysisTaskInfos, isSync);
         }
         if (isSync) {
             syncExecute(analysisTaskInfos.values());
@@ -583,7 +581,7 @@ public class AnalysisManager extends Daemon implements Writable {
     }
 
     @VisibleForTesting
-    public void createTaskForExternalTable(AnalysisInfo jobInfo,
+    public void createTableLevelTaskForExternalTable(AnalysisInfo jobInfo,
             Map<Long, BaseAnalysisTask> analysisTasks,
             boolean isSync) throws DdlException {
 
@@ -616,6 +614,10 @@ public class AnalysisManager extends Daemon implements Writable {
     public void updateTableStats(AnalysisInfo jobInfo) {
         TableIf tbl = StatisticsUtil.findTable(jobInfo.catalogName,
                 jobInfo.dbName, jobInfo.tblName);
+        // External Table update table stats after table level task finished.
+        if (tbl instanceof ExternalTable) {
+            return;
+        }
         // TODO: set updatedRows to 0, when loadedRows of transaction info is ready.
         updateTableStatsStatus(new TableStats(tbl.getId(), tbl.estimatedRowCount(), jobInfo));
     }

fe/fe-core/src/main/java/org/apache/doris/statistics/HMSAnalysisTask.java
@@ -123,7 +123,7 @@ public class HMSAnalysisTask extends BaseAnalysisTask {
         String rowCount = columnResult.get(0).get(0);
         Env.getCurrentEnv().getAnalysisManager()
                 .updateTableStatsStatus(
-                        new TableStats(table.getId(), Long.parseLong(rowCount), null));
+                        new TableStats(table.getId(), Long.parseLong(rowCount), info));
     }
 
     /**
@@ -269,6 +269,10 @@ public class HMSAnalysisTask extends BaseAnalysisTask {
 
     @Override
     protected void afterExecution() {
+        // Table level task doesn't need to sync any value to sync stats, it stores the value in metadata.
+        if (isTableLevelTask) {
+            return;
+        }
         Env.getCurrentEnv().getStatisticsCache().syncLoadColStats(tbl.getId(), -1, col.getName());
     }
 }

regression-test/suites/external_table_p2/hive/test_hive_statistic.groovy
@@ -234,11 +234,11 @@ suite("test_hive_statistic", "p2,external,hive,external_remote,external_remote_h
         sql """analyze database `statistics` with sync"""
         result = sql """show table stats statistics"""
         assertTrue(result.size() == 1)
-        assertTrue(result[0][0] == "100")
+        assertTrue(result[0][2] == "100")
 
         result = sql """show table cached stats statistics"""
         assertTrue(result.size() == 1)
-        assertTrue(result[0][0] == "100")
+        assertTrue(result[0][2] == "100")
 
         sql """drop stats statistics"""
         result = sql """show column cached stats statistics"""

regression-test/suites/external_table_p2/hive/test_hive_statistic_cache.groovy
@@ -29,6 +29,40 @@ suite("test_hive_statistic_cache", "p2,external,hive,external_remote,external_re
                 'hive.metastore.uris' = 'thrift://${extHiveHmsHost}:${extHiveHmsPort}'
             );
         """
+        sql """use ${catalog_name}.tpch_1000_parquet"""
+        sql """desc customer""";
+        sql """desc lineitem""";
+        sql """desc region""";
+        sql """desc nation""";
+        sql """desc orders""";
+        sql """desc part""";
+        sql """desc partsupp""";
+        sql """desc supplier""";
+        Thread.sleep(1000);
+        def result = sql """show table cached stats customer"""
+        assertTrue(result[0][2] == "150000000")
+
+        result = sql """show table cached stats lineitem"""
+        assertTrue(result[0][2] == "5999989709")
+
+        result = sql """show table cached stats region"""
+        assertTrue(result[0][2] == "5")
+
+        result = sql """show table cached stats nation"""
+        assertTrue(result[0][2] == "25")
+
+        result = sql """show table cached stats orders"""
+        assertTrue(result[0][2] == "1500000000")
+
+        result = sql """show table cached stats part"""
+        assertTrue(result[0][2] == "200000000")
+
+        result = sql """show table cached stats partsupp"""
+        assertTrue(result[0][2] == "800000000")
+
+        result = sql """show table cached stats supplier"""
+        assertTrue(result[0][2] == "10000000")
+
         logger.info("catalog " + catalog_name + " created")
         sql """switch ${catalog_name};"""
         logger.info("switched to catalog " + catalog_name)
@@ -37,7 +71,7 @@ suite("test_hive_statistic_cache", "p2,external,hive,external_remote,external_re
         sql """analyze table `stats` with sync;"""
         sql """select count(*) from stats"""
         Thread.sleep(5000);
-        def result = sql """show column cached stats `stats` (lo_orderkey)"""
+        result = sql """show column cached stats `stats` (lo_orderkey)"""
         assertTrue(result[0][0] == "lo_orderkey")
         assertTrue(result[0][1] == "100.0")
         assertTrue(result[0][2] == "26.0")