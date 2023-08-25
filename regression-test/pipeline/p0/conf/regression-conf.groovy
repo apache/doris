d9b61a8eccdab6bebca5d0e6984da6a957922adb
zhangguoqiang <18372634969@163.com>
2023-08-25 10:28:52 +0800

    Update regression-conf.groovy
    open two hive case about core

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -54,7 +54,7 @@ testDirectories = ""
 // this groups will not be executed
 excludeGroups = ""
 // this suites will not be executed
-excludeSuites = "test_pk_uk_index_change,test_pk_uk_case,test_default_limit,test_profile,test_broker_load,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_export_parquet,test_doris_jdbc_catalog,test_transactional_hive,nereids_delete_mow_partial_update,test_catalog_hive_orc,test_hdfs_tvf,test_hive_orc"
+excludeSuites = "test_pk_uk_index_change,test_pk_uk_case,test_default_limit,test_profile,test_broker_load,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_export_parquet,test_doris_jdbc_catalog,test_transactional_hive,nereids_delete_mow_partial_update,test_hdfs_tvf"
 
 // this directories will not be executed
 excludeDirectories = "workload_manager_p1"
