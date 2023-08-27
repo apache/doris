5f6b009e3dafe7251def62a78296204c32162b67
eldenmoon <15605149486@163.com>
2023-08-27 22:34:57 +0800

    [to-pick] fix segment iterator return emtpy row may cause inpit block's types missmatched, since _current_return_columns may converted to segment column type which is inconsistent with type from input block

--- be/src/olap/rowset/segment_v2/segment_iterator.cpp
+++ be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -2068,6 +2068,8 @@ Status SegmentIterator::_next_batch_internal(vectorized::Block* block) {
     _opts.stats->raw_rows_read += _current_batch_rows_read;
 
     if (_current_batch_rows_read == 0) {
+        // Convert all columns in _current_return_columns to schema column
+        RETURN_IF_ERROR(_convert_to_expected_type(_schema->column_ids()));
         for (int i = 0; i < block->columns(); i++) {
             auto cid = _schema->column_id(i);
             // todo(wb) abstract make column where
