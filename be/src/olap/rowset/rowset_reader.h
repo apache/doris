b05f871a1ac7f0eacdc739b19f3953ab571e0ac3
Yongqiang YANG <dataroaring@gmail.com>
2023-08-29 10:23:14 +0800

    fix

--- be/src/olap/rowset/rowset_reader.h
+++ be/src/olap/rowset/rowset_reader.h
@@ -58,7 +58,6 @@ public:
 
     virtual Status get_segment_iterators(RowsetReaderContext* read_context,
                                          std::vector<RowwiseIteratorUPtr>* out_iters,
-                                         const RowSetSplits& rs_splits = {},
                                          bool use_cache = false) = 0;
     virtual void reset_read_options() = 0;
 
