96e1d3dbaf31f671a2c3be7cd2bd4708c3797797
amory <wangqiannan@selectdb.com>
2023-08-29 16:18:11 +0800

    [FIX](array)update array olapconvertor and support array nested other complex type (#23489)
    * update array olapconvertor and support array nested other complex type
    
    * update for inverted index

--- regression-test/suites/external_table_p0/tvf/test_local_tvf_with_complex_type_insertinto_doris.groovy
+++ regression-test/suites/external_table_p0/tvf/test_local_tvf_with_complex_type_insertinto_doris.groovy
@@ -19,25 +19,34 @@ import org.junit.Assert
 
 // This suit test the `backends` tvf
 suite("test_local_tvf_with_complex_type_insertinto_doris", "p0") {
+    sql """set enable_nereids_planner=false"""
     List<List<Object>> backends =  sql """ select * from backends(); """
     assertTrue(backends.size() > 0)
     def be_id = backends[0][0]
     def dataFilePath = context.config.dataPath + "/external_table_p0/tvf/"
     def table_name = "comp"
 
+    def outFilePath= dataFilePath
+    if (backends.size() > 1) {
         // cluster mode need to make sure all be has this data
-        def outFilePath="/"
-        def transFile01="${dataFilePath}/comp.orc"
-        def transFile02="${dataFilePath}/comp.parquet"
+        outFilePath="/"
+        def transFile01 = "${dataFilePath}/comp.orc"
+        def transFile02 = "${dataFilePath}/comp.parquet"
+        def transFile1="${dataFilePath}/comp_arr.orc"
+        def transFile2="${dataFilePath}/comp_arr.parquet"
         for (List<Object> backend : backends) {
             def be_host = backend[1]
-            scpFiles ("root", be_host, transFile01, outFilePath, false);
-            scpFiles ("root", be_host, transFile02, outFilePath, false);
+            scpFiles("root", be_host, transFile01, outFilePath, false)
+            scpFiles("root", be_host, transFile02, outFilePath, false)
+            scpFiles ("root", be_host, transFile1, outFilePath, false)
+            scpFiles ("root", be_host, transFile2, outFilePath, false);
         }
+    }
 
     qt_sql """ADMIN SET FRONTEND CONFIG ('disable_nested_complex_type' = 'false')"""
 
     // create doris table
+    sql """ DROP TABLE IF EXISTS ${table_name} """
     qt_sql """
              CREATE TABLE IF NOT EXISTS ${table_name} (
               `id` int(11) NULL,
@@ -87,5 +96,62 @@ suite("test_local_tvf_with_complex_type_insertinto_doris", "p0") {
     qt_sql """ select * from ${table_name} order by id"""
 
     qt_sql """ drop table ${table_name} """
+
+    // array-map
+    def table_arr = "comp_arr"
+    sql """ DROP TABLE IF EXISTS ${table_arr} """
+    qt_sql """
+             CREATE TABLE IF NOT EXISTS ${table_arr} (
+              `id` int(11) NULL,
+              `aa` ARRAY<ARRAY<double>> NULL,
+              `am` ARRAY<MAP<text,int>> NULL
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`id`)
+            COMMENT 'OLAP'
+            DISTRIBUTED BY HASH(`id`) BUCKETS 1
+            PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "is_being_synced" = "false",
+            "storage_format" = "V2",
+            "light_schema_change" = "true",
+            "disable_auto_compaction" = "false",
+            "enable_single_replica_compaction" = "false"
+            ); """
+
+
+    qt_sql_orc_tvf """
+        select * from local(
+            "file_path" = "${outFilePath}/comp_arr.orc",
+            "backend_id" = "${be_id}",
+            "format" = "orc");"""
+
+    qt_sql """
+        insert into ${table_arr} select * from local (
+            "file_path" = "${outFilePath}/comp_arr.orc",
+            "backend_id" = "${be_id}",
+             "format" = "orc");"""
+
+    qt_sql_orc_inner """ select * from ${table_arr} order by id; """
+
+    qt_sql """ truncate table ${table_arr} """
+
+    qt_sql_parquet_tvf """
+        select * from local(
+            "file_path" = "${outFilePath}/comp_arr.parquet",
+            "backend_id" = "${be_id}",
+            "format" = "parquet"); """
+
+    qt_sql """
+        insert into ${table_arr} select * from local(
+            "file_path" = "${outFilePath}/comp_arr.parquet",
+            "backend_id" = "${be_id}",
+            "format" = "parquet"); """
+
+    qt_sql_count """ select count(*) from ${table_arr} """
+
+    qt_sql_parquet_inner """ select * from ${table_arr} order by id"""
+
+    qt_sql """ drop table ${table_arr} """
+
 
 }
