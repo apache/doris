9fc72e0080765e9bff1d54863e08bb154659a702
morrySnow <101034200+morrySnow@users.noreply.github.com>
2023-08-29 18:30:16 +0800

    [fix](view) The parameter positions of timestamp diff function to sql are reversed (#23601)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/FunctionCallExpr.java
@@ -557,8 +557,8 @@ public class FunctionCallExpr extends Expr {
                 || fnName.getFunction().equalsIgnoreCase("hours_diff")
                 || fnName.getFunction().equalsIgnoreCase("minutes_diff")
                 || fnName.getFunction().equalsIgnoreCase("seconds_diff")) {
-            sb.append(children.get(1).toSql()).append(", ");
-            sb.append(children.get(0).toSql()).append(")");
+            sb.append(children.get(0).toSql()).append(", ");
+            sb.append(children.get(1).toSql()).append(")");
             return sb.toString();
         }
         // used by nereids END
