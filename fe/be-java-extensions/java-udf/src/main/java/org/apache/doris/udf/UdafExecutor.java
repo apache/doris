96c4471b4a6a7531a882b206347534efc9596117
Mryange <59914473+Mryange@users.noreply.github.com>
2023-08-31 07:44:18 +0800

    [feature](udf) udf array/map support decimal and  update doc  (#23560)
    * update
    
    * decimal
    
    * update table name
    
    * remove log
    
    * add log

--- fe/be-java-extensions/java-udf/src/main/java/org/apache/doris/udf/UdafExecutor.java
+++ fe/be-java-extensions/java-udf/src/main/java/org/apache/doris/udf/UdafExecutor.java
@@ -48,7 +48,6 @@ public class UdafExecutor extends BaseExecutor {
 
     private static final Logger LOG = Logger.getLogger(UdafExecutor.class);
 
-    private long inputPlacesPtr;
     private HashMap<String, Method> allMethods;
     private HashMap<Long, Object> stateObjMap;
     private Class retClass;
@@ -88,11 +87,11 @@ public class UdafExecutor extends BaseExecutor {
         PrimitiveType valueType = argTypes[argIdx].getValueType().getPrimitiveType();
         Object[] keyCol = convertMapArg(keyType, argIdx, isNullable, rowStart, rowEnd, nullMapAddr, offsetsAddr,
                 keyNestedNullMapAddr, keyDataAddr,
-                keyStrOffsetAddr);
+                keyStrOffsetAddr, argTypes[argIdx].getKeyScale());
         Object[] valueCol = convertMapArg(valueType, argIdx, isNullable, rowStart, rowEnd, nullMapAddr, offsetsAddr,
                 valueNestedNullMapAddr,
                 valueDataAddr,
-                valueStrOffsetAddr);
+                valueStrOffsetAddr, argTypes[argIdx].getValueScale());
         return buildHashMap(keyType, valueType, keyCol, valueCol);
     }
 
@@ -304,12 +303,13 @@ public class UdafExecutor extends BaseExecutor {
             UdfUtils.UNSAFE.putByte(nullMapAddr + row, (byte) 0);
         }
         copyTupleArrayResultImpl(hasPutElementNum, isNullable, row, result, nullMapAddr, offsetsAddr, nestedNullMapAddr,
-                dataAddr, strOffsetAddr, retType.getItemType().getPrimitiveType());
+                dataAddr, strOffsetAddr, retType.getItemType().getPrimitiveType(), retType.getScale());
     }
 
     public void copyTupleMapResult(long hasPutElementNum, boolean isNullable, int row, Object result, long nullMapAddr,
             long offsetsAddr,
-            long keyNsestedNullMapAddr, long keyDataAddr, long keyStrOffsetAddr,
+            long keyNsestedNullMapAddr, long keyDataAddr,
+            long keyStrOffsetAddr,
             long valueNsestedNullMapAddr, long valueDataAddr, long valueStrOffsetAddr) throws UdfRuntimeException {
         if (nullMapAddr > 0) {
             UdfUtils.UNSAFE.putByte(nullMapAddr + row, (byte) 0);
@@ -323,16 +323,15 @@ public class UdafExecutor extends BaseExecutor {
         buildArrayListFromHashMap(resultArr, keyType, valueType, keyCol, valueCol);
         copyTupleArrayResultImpl(hasPutElementNum, isNullable, row,
                 valueCol[0], nullMapAddr, offsetsAddr,
-                valueNsestedNullMapAddr, valueDataAddr, valueStrOffsetAddr, valueType);
+                valueNsestedNullMapAddr, valueDataAddr, valueStrOffsetAddr, valueType, retType.getKeyScale());
         copyTupleArrayResultImpl(hasPutElementNum, isNullable, row, keyCol[0], nullMapAddr, offsetsAddr,
-                keyNsestedNullMapAddr, keyDataAddr, keyStrOffsetAddr, keyType);
+                keyNsestedNullMapAddr, keyDataAddr, keyStrOffsetAddr, keyType, retType.getValueScale());
     }
 
     @Override
     protected void init(TJavaUdfExecutorCtorParams request, String jarPath, Type funcRetType,
             Type... parameterTypes) throws UdfRuntimeException {
         String className = request.fn.aggregate_fn.symbol;
-        inputPlacesPtr = request.input_places_ptr;
         allMethods = new HashMap<>();
         stateObjMap = new HashMap<>();
 
