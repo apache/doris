855d9e435ed67bdc78fb7d830bb734b35889e221
jacktengg <18241664+jacktengg@users.noreply.github.com>
2023-08-30 14:53:01 +0800

    [improvement](Block) Replace Block(const PBlock&) with deserialize because it has heavy operations in ctor

--- be/src/runtime/tablets_channel.cpp
+++ be/src/runtime/tablets_channel.cpp
@@ -436,9 +436,10 @@ Status TabletsChannel::add_batch(const PTabletWriterAddBlockRequest& request,
         }
     }
 
-    auto get_send_data = [&]() { return vectorized::Block(request.block()); };
+    vectorized::Block send_data;
+    auto get_send_data = [&]() { return send_data.deserialize(request.block()); };
 
-    auto send_data = get_send_data();
+    RETURN_IF_ERROR(get_send_data());
     CHECK(send_data.rows() == request.tablet_ids_size())
             << "block rows: " << send_data.rows()
             << ", tablet_ids_size: " << request.tablet_ids_size();
