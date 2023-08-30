4d2bd29dc5218955f8a6737be0f3dd1f791da14c
jiafeng.zhang <zhangjf1@gmail.com>
2023-08-30 13:03:53 +0800

    [fix](build)fix env.sh

--- env.sh
+++ env.sh
@@ -146,7 +146,15 @@ elif [[ "${DORIS_TOOLCHAIN}" == "clang" ]]; then
     fi
 
     covs=()
-    while IFS='' read -r line; do covs+=("${line}"); done < <(find "${DORIS_CLANG_HOME}" -name "llvm-cov*")
+    tmp_file=$(mktemp)
+    find "${DORIS_CLANG_HOME}" -name "llvm-cov*" > "$tmp_file"
+
+    # Read lines from the temporary file and populate the array
+    while IFS= read -r line; do
+       covs+=("$line")
+    done < "$tmp_file"
+
+
     if [[ ${#covs[@]} -ge 1 ]]; then
         LLVM_COV="${covs[0]}"
     else
@@ -155,7 +163,10 @@ elif [[ "${DORIS_TOOLCHAIN}" == "clang" ]]; then
     export LLVM_COV
 
     profdatas=()
-    while IFS='' read -r line; do profdatas+=("${line}"); done < <(find "${DORIS_CLANG_HOME}" -name "llvm-profdata*")
+
+    find "${DORIS_CLANG_HOME}" -name "llvm-profdata*" | while IFS= read -r line; do
+       profdatas+=("$line")
+    done
     if [[ ${#profdatas[@]} -ge 1 ]]; then
         LLVM_PROFDATA="${profdatas[0]}"
     else
