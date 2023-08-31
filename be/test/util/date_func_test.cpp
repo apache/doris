6574c92e09691b3e80f91e363b6cd8e880df9f49
yiguolei <yiguolei@gmail.com>
2023-08-31 17:01:18 +0800

    f

--- be/test/util/date_func_test.cpp
+++ be/test/util/date_func_test.cpp
@@ -61,10 +61,12 @@ TEST_F(DateFuncTest, convert_string_to_int) {
     EXPECT_EQ(std::string("1400-01-01"), abnormal_result2.to_string());
 }
 
-TEST_F(DateFuncTest, convert_string_to_datev1) {
+TEST_F(DateFuncTest, convert_string_to_datev2) {
     uint32_t result1 = DateFuncTest::test_timestamp_from_date_v2(std::string("2021-06-08"));
     uint32_t result11 = timestamp_from_date_v2(std::string("2021-06-08")).to_date_int_val();
-
+    EXPECT_EQ(result1, result11);
+    result1 = DateFuncTest::test_timestamp_from_date_v2(std::string("2021-16-08"));
+    result11 = timestamp_from_date_v2(std::string("2021-16-08")).to_date_int_val();
     std::cout << result1 << "   " << result11 << std::endl;
 }
 
