6a5b9f5a8503c07d86533b47a7aa942a59f7bcdb
Gabriel <gabrielleebuaa@gmail.com>
2023-08-31 22:21:58 +0800

    update

--- be/src/runtime/runtime_state.cpp
+++ be/src/runtime/runtime_state.cpp
@@ -431,7 +431,6 @@ std::shared_ptr<doris::pipeline::PipelineXLocalStateBase> RuntimeState::get_loca
 void RuntimeState::emplace_sink_local_state(
         int id, std::shared_ptr<doris::pipeline::PipelineXSinkLocalStateBase> state) {
     std::unique_lock<std::mutex> l(_local_sink_state_lock);
-    LOG(WARNING) << "=======1 " << id << " " << typeid(state.get()).name();
     _op_id_to_sink_local_state.emplace(id, state);
 }
 
@@ -439,7 +438,6 @@ std::shared_ptr<doris::pipeline::PipelineXSinkLocalStateBase> RuntimeState::get_
         int id) {
     std::unique_lock<std::mutex> l(_local_sink_state_lock);
     DCHECK(_op_id_to_sink_local_state.find(id) != _op_id_to_sink_local_state.end());
-    LOG(WARNING) << "=======2 " << id << " " << typeid(_op_id_to_sink_local_state[id].get()).name();
     return _op_id_to_sink_local_state[id];
 }
 
