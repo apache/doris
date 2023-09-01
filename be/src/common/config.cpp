18d470ecf7f2be17a1a672fdbe74d65be7871b37
Kang <kxiao.tiger@gmail.com>
2023-09-02 01:14:14 +0800

    [improvement](config) add a specific be config for segment_cache_capacity (#23701)
    * add segment_cache_capacity config istead of fd limit * 2/5
    * default -1 for backward compatibility

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1031,6 +1031,9 @@ DEFINE_Bool(enable_shrink_memory, "false");
 DEFINE_mInt32(schema_cache_capacity, "1024");
 DEFINE_mInt32(schema_cache_sweep_time_sec, "100");
 
+// max number of segment cache, default -1 for backward compatibility fd_number*2/5
+DEFINE_mInt32(segment_cache_capacity, "-1");
+
 // enable feature binlog, default false
 DEFINE_Bool(enable_feature_binlog, "false");
 
