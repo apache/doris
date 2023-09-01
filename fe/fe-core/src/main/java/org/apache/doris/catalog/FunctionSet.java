9c2457ddbefd9975038dfe8a015796307f0014c9
Kang <kxiao.tiger@gmail.com>
2023-09-01 14:54:25 +0800

    addd isDebugEnabled

--- fe/fe-core/src/main/java/org/apache/doris/catalog/FunctionSet.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/FunctionSet.java
@@ -345,10 +345,12 @@ public class FunctionSet<T> {
     public Function specializeTemplateFunction(Function templateFunction, Function requestFunction, boolean isVariadic) {
         try {
             boolean hasTemplateType = false;
-            LOG.debug("templateFunction signature: {}, return type: {}",
-                        templateFunction.signatureString(), templateFunction.getReturnType());
-            LOG.debug("requestFunction signature: {}, return type: {}",
-                        requestFunction.signatureString(), requestFunction.getReturnType());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("templateFunction signature: {}, return type: {}",
+                            templateFunction.signatureString(), templateFunction.getReturnType());
+                LOG.debug("requestFunction signature: {}, return type: {}",
+                            requestFunction.signatureString(), requestFunction.getReturnType());
+            }
             List<Type> newArgTypes = Lists.newArrayList();
             List<Type> newRetType = Lists.newArrayList();
             if (isVariadic) {
@@ -397,11 +399,13 @@ public class FunctionSet<T> {
                         specializedFunction.getReturnType().specializeTemplateType(
                         requestFunction.getReturnType(), specializedTypeMap, true));
             }
-            LOG.debug("specializedFunction signature: {}, return type: {}",
-                        specializedFunction.signatureString(), specializedFunction.getReturnType());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("specializedFunction signature: {}, return type: {}",
+                            specializedFunction.signatureString(), specializedFunction.getReturnType());
+            }
             return hasTemplateType ? specializedFunction : templateFunction;
         } catch (TypeException e) {
-            if (inited) {
+            if (inited && LOG.isDebugEnabled()) {
                 LOG.debug("specializeTemplateFunction exception", e);
             }
             return null;
