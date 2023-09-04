3d6c51d6c1115a5d18e4e05ebfd083ad252a8e80
Mingyu Chen <morningman@163.com>
2023-09-04 15:19:37 +0800

    [fix](row-policy) fix creating row policy with forward issue (#23801)
    The `CreateRowPolicyCommand` is implemented with overriding `run()` method.
    
    So when executing `create row policy` in non-master FE, and forward it to Master FE,
    it will call `execute(TUniqueId queryId)` method and go through `executeByNereids()`.
    And because without `run()` method, it will do nothing and return OK.
    So after `show row policy`, user will get empty result.
    
    This PR fix it by implmenting the `run()` method but throw an Exception, so that it will
    fallback to old planner, to do the creating row policy command normally.
    
    The full implement of `run()` method should be implemented later.
    This is just a tmp fix.

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
@@ -56,7 +56,6 @@ import org.apache.doris.nereids.glue.LogicalPlanAdapter;
 import org.apache.doris.nereids.minidump.MinidumpUtils;
 import org.apache.doris.nereids.parser.NereidsParser;
 import org.apache.doris.nereids.stats.StatsErrorEstimator;
-import org.apache.doris.nereids.trees.plans.commands.CreatePolicyCommand;
 import org.apache.doris.plugin.AuditEvent.EventType;
 import org.apache.doris.proto.Data;
 import org.apache.doris.qe.QueryState.MysqlStateType;
@@ -386,14 +385,6 @@ public class ConnectProcessor {
         if (mysqlCommand == MysqlCommand.COM_QUERY && ctx.getSessionVariable().isEnableNereidsPlanner()) {
             try {
                 stmts = new NereidsParser().parseSQL(originStmt);
-                for (StatementBase stmt : stmts) {
-                    LogicalPlanAdapter logicalPlanAdapter = (LogicalPlanAdapter) stmt;
-                    // TODO: remove this after we could process CreatePolicyCommand
-                    if (logicalPlanAdapter.getLogicalPlan() instanceof CreatePolicyCommand) {
-                        stmts = null;
-                        break;
-                    }
-                }
             } catch (Exception e) {
                 // TODO: We should catch all exception here until we support all query syntax.
                 LOG.debug("Nereids parse sql failed. Reason: {}. Statement: \"{}\".",
@@ -846,4 +837,5 @@ public class ConnectProcessor {
         }
     }
 }
+
 
