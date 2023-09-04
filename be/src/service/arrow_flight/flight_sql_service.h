9d29d3155c59826c3a2bc3a87555beda72334b84
Xinyi Zou <zouxinyi02@gmail.com>
2023-09-04 19:19:55 +0800

    [feature-wip] (arrow-flight) (step1) BE support Arrow Flight server, read data only (#23765)

--- be/src/service/arrow_flight/flight_sql_service.h
+++ be/src/service/arrow_flight/flight_sql_service.h
@@ -17,44 +17,33 @@
 
 #pragma once
 
-#include <stdint.h>
-
-#include <string>
-
+#include "arrow/flight/sql/server.h"
+#include "arrow/result.h"
 #include "common/status.h"
+#include "service/arrow_flight/arrow_flight_batch_reader.h"
 
 namespace doris {
+namespace flight {
 
-namespace vectorized {
-class Block;
-}
-class RuntimeState;
-
-// abstract class of the result writer
-class ResultWriter {
+class FlightSqlServer : public arrow::flight::sql::FlightSqlServerBase {
 public:
-    ResultWriter() = default;
-    virtual ~ResultWriter() = default;
+    ~FlightSqlServer() override;
 
-    virtual Status init(RuntimeState* state) = 0;
+    static arrow::Result<std::shared_ptr<FlightSqlServer>> create();
 
-    virtual Status close() = 0;
+    arrow::Result<std::unique_ptr<arrow::flight::FlightDataStream>> DoGetStatement(
+            const arrow::flight::ServerCallContext& context,
+            const arrow::flight::sql::StatementQueryTicket& command) override;
 
-    [[nodiscard]] virtual int64_t get_written_rows() const { return _written_rows; }
+    Status init(int port);
+    Status join();
 
-    [[nodiscard]] bool output_object_data() const { return _output_object_data; }
+private:
+    class Impl;
+    std::shared_ptr<Impl> impl_;
 
-    virtual Status append_block(vectorized::Block& block) = 0;
-
-    virtual bool can_sink() { return true; }
-
-    void set_output_object_data(bool output_object_data) {
-        _output_object_data = output_object_data;
-    }
-
-protected:
-    int64_t _written_rows = 0; // number of rows written
-    bool _output_object_data = false;
+    explicit FlightSqlServer(std::shared_ptr<Impl> impl);
 };
 
+} // namespace flight
 } // namespace doris
