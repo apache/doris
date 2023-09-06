320f1157f66a3aa437c97c03bcb37f2de87dbbe3
Xinyi Zou <zouxinyi02@gmail.com>
2023-09-06 20:26:32 +0800

    1

--- be/src/service/doris_main.cpp
+++ be/src/service/doris_main.cpp
@@ -612,6 +612,9 @@ int main(int argc, char** argv) {
         sleep(3);
     }
 
+    // Stop before static variables are destructed
+    daemon.stop();
+
     // For graceful shutdown, need to wait for all running queries to stop
     exec_env->wait_for_all_tasks_done();
 
