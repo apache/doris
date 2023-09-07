6b45436390000736e070b4a79de7e69a2ffb8d63
Mryange <59914473+Mryange@users.noreply.github.com>
2023-09-07 19:03:49 +0800

    [feature](Datetime) add from_microsecond / from_millisecond function (#23902)

--- be/src/vec/functions/function_date_or_datetime_computation.h
+++ be/src/vec/functions/function_date_or_datetime_computation.h
@@ -1104,6 +1104,55 @@ struct SecToTimeImpl {
         return Status::OK();
     }
 };
+struct MicroSec {
+    static constexpr auto name = "from_microsecond";
+    static constexpr Int64 ratio = 1000000;
+};
+struct MilliSec {
+    static constexpr auto name = "from_millisecond";
+    static constexpr Int64 ratio = 1000;
+};
+struct Sec {
+    static constexpr auto name = "from_second";
+    static constexpr Int64 ratio = 1;
+};
+template <typename Impl>
+struct TimestampToDateTime : IFunction {
+    using ReturnType = DataTypeDateTimeV2;
+    static constexpr auto name = Impl::name;
+    static constexpr Int64 ratio_to_micro = (1000 * 1000) / Impl::ratio;
+    String get_name() const override { return name; }
+
+    size_t get_number_of_arguments() const override { return 1; }
+
+    DataTypePtr get_return_type_impl(const ColumnsWithTypeAndName& arguments) const override {
+        return make_nullable(std::make_shared<ReturnType>());
+    }
+
+    static FunctionPtr create() { return std::make_shared<TimestampToDateTime<Impl>>(); }
+
+    Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
+                        size_t result, size_t input_rows_count) override {
+        const auto& arg_col = block.get_by_position(arguments[0]).column;
+        const auto& column_data = assert_cast<const ColumnInt64&>(*arg_col);
+        auto res_col = ColumnUInt64::create();
+        auto null_vector = ColumnVector<UInt8>::create();
+        res_col->get_data().resize_fill(input_rows_count, 0);
+        null_vector->get_data().resize_fill(input_rows_count, false);
+        NullMap& null_map = null_vector->get_data();
+        auto& res_data = res_col->get_data();
+        const cctz::time_zone& time_zone = context->state()->timezone_obj();
+        for (int i = 0; i < input_rows_count; ++i) {
+            Int64 value = column_data.get_element(i);
+            auto& dt = reinterpret_cast<DateV2Value<DateTimeV2ValueType>&>(res_data[i]);
+            null_map[i] = !dt.from_unixtime(value / Impl::ratio, time_zone);
+            dt.set_microsecond((value % Impl::ratio) * ratio_to_micro);
+        }
+        block.get_by_position(result).column =
+                ColumnNullable::create(std::move(res_col), std::move(null_vector));
+        return Status::OK();
+    }
+};
 
 struct UtcTimestampImpl {
     using ReturnType = DataTypeDateTime;
---
+++ regression-test/suites/correctness/test_from_millisecond_microsecond.groovy
@@ -0,0 +1,131 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_from_millisecond_microsecond") {
+     sql """ DROP TABLE IF EXISTS millimicro """
+     sql """
+        CREATE TABLE IF NOT EXISTS millimicro (
+              `id` INT(11) NULL COMMENT ""   ,
+              `t` BigINT NULL COMMENT ""
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`id`)
+            DISTRIBUTED BY HASH(`id`) BUCKETS 1
+            PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "storage_format" = "V2"
+    );
+    """
+
+    sql """
+        insert into millimicro values(1,1919810114514);
+    """
+    sql """
+        insert into millimicro values(2,89417891234789);
+    """
+    sql """
+        insert into millimicro values(3,1235817896941);
+    """
+
+    sql """
+        insert into millimicro values(4,NULL);
+    """
+    sql """
+        set enable_nereids_planner=false
+    """
+
+    qt_select1 """
+        select 
+        from_millisecond(t) as t1 , 
+        microseconds_add(cast(from_unixtime(t/1000) as datetime(3)), cast((t % 1000) * 1000 as int)) as t2 
+        from millimicro order by id;
+    """
+
+    qt_select2 """
+        select 
+        from_microsecond(t) as t1 , 
+        microseconds_add(cast(from_unixtime(t/1000000) as datetime(6)), cast((t % 1000000) as int)) as t2 
+        from millimicro order by id;
+    """ 
+
+    qt_select3 """
+        select 
+        FROM_UNIXTIME(2147483647),from_second(2147483647),
+        FROM_UNIXTIME(2147483647 + 1),from_second(2147483647 + 1),
+        FROM_UNIXTIME(21474836470),from_second(21474836470);
+    """ 
+
+    qt_select4 """
+        select 
+        t,
+        from_second(t), 
+        second_timestamp(from_second(t))
+        from millimicro order by id;
+    """ 
+    qt_select5 """
+        select 
+        t,
+        from_millisecond(t), 
+        millisecond_timestamp(from_millisecond(t))
+        from millimicro order by id;
+    """ 
+    qt_select6 """
+        select 
+        t,
+        from_microsecond(t), 
+        microsecond_timestamp(from_microsecond(t))
+        from millimicro order by id;
+    """ 
+    sql """
+        set enable_nereids_planner=true,enable_fold_constant_by_be = false,forbid_unknown_col_stats = false
+    """
+   
+    qt_select7 """
+        select from_millisecond(t) as t1 from millimicro order by id;
+    """
+    qt_select8 """
+        select from_microsecond(t) as t1 from millimicro order by id;
+    """
+
+    qt_select9 """
+        select 
+        FROM_UNIXTIME(2147483647),from_second(2147483647),
+        FROM_UNIXTIME(2147483647 + 1),from_second(2147483647 + 1),
+        FROM_UNIXTIME(21474836470),from_second(21474836470);
+    """ 
+
+    qt_select10 """
+        select 
+        t,
+        from_second(t), 
+        second_timestamp(from_second(t))
+        from millimicro order by id;
+    """ 
+    qt_select11 """
+        select 
+        t,
+        from_millisecond(t), 
+        millisecond_timestamp(from_millisecond(t))
+        from millimicro order by id;
+    """ 
+    qt_select12 """
+        select 
+        t,
+        from_microsecond(t), 
+        microsecond_timestamp(from_microsecond(t))
+        from millimicro order by id;
+    """ 
+}
