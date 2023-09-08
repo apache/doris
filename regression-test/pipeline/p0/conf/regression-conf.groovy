c8af224bec5776607ca00f05eca620b1da32f595
Dongyang Li <hello_stephen@qq.com>
2023-09-08 20:53:45 +0800

    Update regression-conf.groovy

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -55,7 +55,7 @@ testDirectories = ""
 excludeGroups = ""
 // this suites will not be executed
 
-excludeSuites = "test_ddl,test_analyze,test_leading,test_stream_load_move_memtable,ds_rf95ds_rf16,ds_rf94,test_profile,test_broker_load,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_export_parquet,test_doris_jdbc_catalog,nereids_delete_mow_partial_update"
+excludeSuites = "test_ddl,test_analyze,test_leading,test_stream_load_move_memtable,ds_rf95,ds_rf16,ds_rf94,test_profile,test_broker_load,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_export_parquet,test_doris_jdbc_catalog,nereids_delete_mow_partial_update"
 
 // this directories will not be executed
 excludeDirectories = "workload_manager_p1"
