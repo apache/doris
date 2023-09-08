575a6e314f930f2067bdb2e778b338cce358d896
Gabriel <gabrielleebuaa@gmail.com>
2023-09-08 21:21:02 +0800

    [regression](pipelineX) disable runtime filter for pipelineX test cases

--- regression-test/suites/tpch_unique_sql_zstd_p0/sql/pipelinex_q07.sql
+++ regression-test/suites/tpch_unique_sql_zstd_p0/sql/pipelinex_q07.sql
@@ -1,5 +1,5 @@
 -- tables: supplier,lineitem,orders,customer,nation
-SELECT /*+SET_VAR(experimental_enable_pipeline_x_engine=true) */
+SELECT /*+SET_VAR(experimental_enable_pipeline_x_engine=true, runtime_filter_mode=OFF) */
   supp_nation,
   cust_nation,
   l_year,
