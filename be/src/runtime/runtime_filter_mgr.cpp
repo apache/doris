cdb1b341c796aa8c07df73e0a04c33594d087c6d
Gabriel <gabrielleebuaa@gmail.com>
2023-09-08 10:17:22 +0800

    [pipelineX](runtime filter) Support runtime filter (#24054)

--- be/src/runtime/runtime_filter_mgr.cpp
+++ be/src/runtime/runtime_filter_mgr.cpp
@@ -65,6 +65,7 @@ Status RuntimeFilterMgr::init() {
 Status RuntimeFilterMgr::get_producer_filter(const int filter_id, IRuntimeFilter** target) {
     int32_t key = filter_id;
 
+    std::lock_guard<std::mutex> l(_lock);
     auto iter = _producer_map.find(key);
     if (iter == _producer_map.end()) {
         LOG(WARNING) << "unknown runtime filter: " << key << ", role: PRODUCER";
@@ -77,6 +78,7 @@ Status RuntimeFilterMgr::get_producer_filter(const int filter_id, IRuntimeFilter
 
 Status RuntimeFilterMgr::get_consume_filter(const int filter_id, const int node_id,
                                             IRuntimeFilter** consumer_filter) {
+    std::lock_guard<std::mutex> l(_lock);
     auto iter = _consumer_map.find(filter_id);
     if (iter == _consumer_map.cend()) {
         LOG(WARNING) << "unknown runtime filter: " << filter_id << ", role: consumer";
@@ -97,6 +99,7 @@ Status RuntimeFilterMgr::get_consume_filter(const int filter_id, const int node_
 Status RuntimeFilterMgr::get_consume_filters(const int filter_id,
                                              std::vector<IRuntimeFilter*>& consumer_filters) {
     int32_t key = filter_id;
+    std::lock_guard<std::mutex> l(_lock);
     auto iter = _consumer_map.find(key);
     if (iter == _consumer_map.end()) {
         LOG(WARNING) << "unknown runtime filter: " << key << ", role: consumer";
@@ -114,29 +117,26 @@ Status RuntimeFilterMgr::register_consumer_filter(const TRuntimeFilterDesc& desc
     SCOPED_CONSUME_MEM_TRACKER(_tracker.get());
     int32_t key = desc.filter_id;
 
+    std::lock_guard<std::mutex> l(_lock);
     auto iter = _consumer_map.find(key);
     if (desc.__isset.opt_remote_rf && desc.opt_remote_rf && desc.has_remote_targets &&
         desc.type == TRuntimeFilterType::BLOOM) {
         // if this runtime filter has remote target (e.g. need merge), we reuse the runtime filter between all instances
         DCHECK(_query_ctx != nullptr);
 
-        {
-            std::lock_guard<std::mutex> l(_lock);
-
-            iter = _consumer_map.find(key);
-            if (iter != _consumer_map.end()) {
-                for (auto holder : iter->second) {
-                    if (holder.node_id == node_id) {
-                        return Status::OK();
-                    }
+        iter = _consumer_map.find(key);
+        if (iter != _consumer_map.end()) {
+            for (auto holder : iter->second) {
+                if (holder.node_id == node_id) {
+                    return Status::OK();
                 }
             }
-            IRuntimeFilter* filter;
-            RETURN_IF_ERROR(IRuntimeFilter::create(_query_ctx, &_query_ctx->obj_pool, &desc,
-                                                   &options, RuntimeFilterRole::CONSUMER, node_id,
-                                                   &filter, build_bf_exactly));
-            _consumer_map[key].emplace_back(node_id, filter);
         }
+        IRuntimeFilter* filter;
+        RETURN_IF_ERROR(IRuntimeFilter::create(_query_ctx, &_query_ctx->obj_pool, &desc, &options,
+                                               RuntimeFilterRole::CONSUMER, node_id, &filter,
+                                               build_bf_exactly));
+        _consumer_map[key].emplace_back(node_id, filter);
     } else {
         DCHECK(_state != nullptr);
 
@@ -162,6 +162,7 @@ Status RuntimeFilterMgr::register_producer_filter(const TRuntimeFilterDesc& desc
                                                   bool build_bf_exactly) {
     SCOPED_CONSUME_MEM_TRACKER(_tracker.get());
     int32_t key = desc.filter_id;
+    std::lock_guard<std::mutex> l(_lock);
     auto iter = _producer_map.find(key);
 
     DCHECK(_state != nullptr);
