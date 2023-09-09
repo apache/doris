d5c6c7a0347859016c64c327f98e813717a5469c
minghong <englefly@gmail.com>
2023-09-09 20:53:31 +0800

    [opt](nereids)push down filter through window #23935
    select rank() over (partition by A, B) as r, sum(x) over(A, C) as s from T;
    A is a common partition key for all windowExpressions, that is A is intersection of {A,B} and {A, C}
    we could push filter A=1 through this window, since A is a common Partition key:
    select * from (select a, row_number() over (partition by a) from win) T where a=1;
    origin plan:
    
    ----filter((T.a = 1))
    ----------PhysicalWindow
    ------------PhysicalQuickSort
    --------------PhysicalProject
    ------------------PhysicalOlapScan[win]
    transformed to
    
    ----PhysicalWindow
    ------PhysicalQuickSort
    --------PhysicalProject
    ----------filter((T.a = 1))
    ------------PhysicalOlapScan[win]
    But C=1 can not be pushed through window.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PushdownFilterThroughWindow.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PushdownFilterThroughWindow.java
@@ -19,45 +19,22 @@ package org.apache.doris.nereids.rules.rewrite;
 
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
-import org.apache.doris.nereids.trees.expressions.BinaryOperator;
-import org.apache.doris.nereids.trees.expressions.EqualTo;
-import org.apache.doris.nereids.trees.expressions.ExprId;
 import org.apache.doris.nereids.trees.expressions.Expression;
-import org.apache.doris.nereids.trees.expressions.LessThan;
-import org.apache.doris.nereids.trees.expressions.LessThanEqual;
-import org.apache.doris.nereids.trees.expressions.NamedExpression;
 import org.apache.doris.nereids.trees.expressions.SlotReference;
-import org.apache.doris.nereids.trees.expressions.WindowExpression;
-import org.apache.doris.nereids.trees.expressions.literal.IntegerLikeLiteral;
 import org.apache.doris.nereids.trees.plans.Plan;
-import org.apache.doris.nereids.trees.plans.logical.LogicalEmptyRelation;
 import org.apache.doris.nereids.trees.plans.logical.LogicalFilter;
-import org.apache.doris.nereids.trees.plans.logical.LogicalPartitionTopN;
 import org.apache.doris.nereids.trees.plans.logical.LogicalWindow;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
 
 /**
- * Push down the 'filter' into the 'window'.
- * It will convert the filter condition to the 'limit value' and push down below the 'window'.
- * But there are some restrictions, the details are explained below.
- * For example:
- * 'SELECT * FROM (
- *     SELECT *, ROW_NUMBER() OVER (ORDER BY b) AS row_number
- *     FROM t
- * ) AS tt WHERE row_number <= 100;'
- * The filter 'row_number <= 100' can be pushed down into the window operator.
- * The following will demonstrate how the plan changes:
+ * Push down the 'filter' into the 'window' if filter key is window partition key.
  * Logical plan tree:
  *                 any_node
  *                   |
- *                filter (row_number <= 100)
+ *                filter (a <= 100)
  *                   |
  *                window (PARTITION BY a ORDER BY b)
  *                   |
@@ -65,11 +42,9 @@ import java.util.function.Predicate;
  * transformed to:
  *                 any_node
  *                   |
- *                filter (row_number <= 100)
- *                   |
  *                window (PARTITION BY a ORDER BY b)
  *                   |
- *                partition_topn(PARTITION BY: a, ORDER BY b, Partition Limit: 100)
+ *                filter (a <= 100)
  *                   |
  *                 any_node
  */
@@ -81,88 +56,43 @@ public class PushdownFilterThroughWindow extends OneRewriteRuleFactory {
         return logicalFilter(logicalWindow()).thenApply(ctx -> {
             LogicalFilter<LogicalWindow<Plan>> filter = ctx.root;
             LogicalWindow<Plan> window = filter.child();
-
-            // We have already done such optimization rule, so just ignore it.
-            if (window.child(0) instanceof LogicalPartitionTopN) {
-                return filter;
-            }
-
-            List<NamedExpression> windowExprs = window.getWindowExpressions();
-            if (windowExprs.size() != 1) {
-                return filter;
-            }
-            NamedExpression windowExpr = windowExprs.get(0);
-            if (windowExpr.children().size() != 1 || !(windowExpr.child(0) instanceof WindowExpression)) {
-                return filter;
-            }
-
-            // Check the filter conditions. Now, we currently only support simple conditions of the form
-            // 'column </ <=/ = constant'. We will extract some related conjuncts and do some check.
-            Set<Expression> conjuncts = filter.getConjuncts();
-            Set<Expression> relatedConjuncts = extractRelatedConjuncts(conjuncts, windowExpr.getExprId());
-
-            boolean hasPartitionLimit = false;
-            long partitionLimit = Long.MAX_VALUE;
-
-            for (Expression conjunct : relatedConjuncts) {
-                Preconditions.checkArgument(conjunct instanceof BinaryOperator);
-                BinaryOperator op = (BinaryOperator) conjunct;
-                Expression leftChild = op.children().get(0);
-                Expression rightChild = op.children().get(1);
-
-                Preconditions.checkArgument(leftChild instanceof SlotReference
-                        && rightChild instanceof IntegerLikeLiteral);
-
-                long limitVal = ((IntegerLikeLiteral) rightChild).getLongValue();
-                // Adjust the value for 'limitVal' based on the comparison operators.
-                if (conjunct instanceof LessThan) {
-                    limitVal--;
+            // now we only handle single slot used as partition key
+            // for example:
+            // select * from (select T.*, rank() over(partition by c2+c3 order by c4) rn from T) abc where c2=1;
+            // c2=1 cannot be pushed down.
+            Set<SlotReference> commonPartitionKeys = window.getCommonPartitionKeyFromWindowExpressions();
+            Set<Expression> bottomConjuncts = Sets.newHashSet();
+            Set<Expression> upperConjuncts = Sets.newHashSet();
+            for (Expression expr : filter.getConjuncts()) {
+                boolean pushed = false;
+                for (Expression partitionKey : commonPartitionKeys) {
+                    // partitionKey is a single slot reference,
+                    // we want to push expressions which have only one input slot, and the input slot is used as
+                    // partition key in all windowExpressions.
+                    if (partitionKey.getInputSlots().containsAll(expr.getInputSlots())) {
+                        bottomConjuncts.add(expr);
+                        pushed = true;
+                        break;
+                    }
                 }
-                if (limitVal < 0) {
-                    return new LogicalEmptyRelation(ctx.statementContext.getNextRelationId(), filter.getOutput());
-                }
-                if (hasPartitionLimit) {
-                    partitionLimit = Math.min(partitionLimit, limitVal);
-                } else {
-                    partitionLimit = limitVal;
-                    hasPartitionLimit = true;
+                if (!pushed) {
+                    upperConjuncts.add(expr);
                 }
             }
-
-            if (!hasPartitionLimit) {
-                return filter;
+            if (bottomConjuncts.isEmpty()) {
+                return null;
             }
 
-            Optional<Plan> newWindow = window.pushPartitionLimitThroughWindow(partitionLimit, false);
-            if (!newWindow.isPresent()) {
-                return filter;
+            LogicalFilter<Plan> bottomFilter = new LogicalFilter<>(bottomConjuncts, window.child());
+            window = (LogicalWindow<Plan>) window.withChildren(bottomFilter);
+            if (upperConjuncts.isEmpty()) {
+                return window;
+            } else {
+                LogicalFilter<Plan> upperFilter = (LogicalFilter<Plan>) filter
+                        .withConjuncts(upperConjuncts).withChildren(window);
+                return upperFilter;
             }
-            return filter.withChildren(newWindow.get());
         }).toRule(RuleType.PUSHDOWN_FILTER_THROUGH_WINDOW);
     }
 
-    private Set<Expression> extractRelatedConjuncts(Set<Expression> conjuncts, ExprId slotRefID) {
-        Predicate<Expression> condition = conjunct -> {
-            if (!(conjunct instanceof BinaryOperator)) {
-                return false;
-            }
-            BinaryOperator op = (BinaryOperator) conjunct;
-            Expression leftChild = op.children().get(0);
-            Expression rightChild = op.children().get(1);
-
-            if (!(conjunct instanceof LessThan || conjunct instanceof LessThanEqual || conjunct instanceof EqualTo)) {
-                return false;
-            }
-
-            // TODO: Now, we only support the column on the left side.
-            if (!(leftChild instanceof SlotReference) || !(rightChild instanceof IntegerLikeLiteral)) {
-                return false;
-            }
-            return ((SlotReference) leftChild).getExprId() == slotRefID;
-        };
-
-        return conjuncts.stream()
-                .filter(condition)
-                .collect(ImmutableSet.toImmutableSet());
-    }
 }
