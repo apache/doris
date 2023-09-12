6913d68ba0c40dfe2a2ee43600804b69c8d0bc12
bobhan1 <bh2444151092@outlook.com>
2023-09-12 08:56:46 +0800

    [Enhancement](merge-on-write) use delete bitmap to mark delete for rows with delete sign when sequence column doesn't exist (#24011)

--- regression-test/suites/unique_with_mow_p0/test_delete_sign_delete_bitmap.groovy
+++ regression-test/suites/unique_with_mow_p0/test_delete_sign_delete_bitmap.groovy
@@ -15,12 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite('test_partial_update_delete_sign') {
-    sql 'set enable_nereids_planner=false'
-    sql "set experimental_enable_nereids_planner=false;"
-    sql 'set enable_nereids_dml=false'
+suite('test_delete_sign_delete_bitmap') {
 
-    def tableName1 = "test_partial_update_delete_sign1"
+    def tableName1 = "test_delete_sign_delete_bitmap1"
     sql "DROP TABLE IF EXISTS ${tableName1};"
     sql """ CREATE TABLE IF NOT EXISTS ${tableName1} (
             `k1` int NOT NULL,
@@ -38,17 +35,8 @@ suite('test_partial_update_delete_sign') {
 
     sql "insert into ${tableName1} values(1,1,1,1,1),(2,2,2,2,2),(3,3,3,3,3),(4,4,4,4,4),(5,5,5,5,5);"
     qt_sql "select * from ${tableName1} order by k1,c1,c2,c3,c4;"
-    streamLoad {
-        table "${tableName1}"
-
-        set 'column_separator', ','
-        set 'format', 'csv'
-        set 'partial_columns', 'true'
-        set 'columns', 'k1,__DORIS_DELETE_SIGN__'
-
-        file 'delete_sign.csv'
-        time 10000 // limit inflight 10s
-    }
+    // sql "insert into ${tableName1}(k1,c1,c2,c3,c4,__DORIS_DELETE_SIGN__) select k1,c1,c2,c3,c4,1 from ${tableName1} where k1 in (1,3,5);"
+    sql """insert into ${tableName1}(k1,c1,c2,c3,c4,__DORIS_DELETE_SIGN__) values(1,1,1,1,1,1),(3,3,3,3,3,1),(5,5,5,5,5,1);"""
     sql "sync"
     qt_after_delete "select * from ${tableName1} order by k1,c1,c2,c3,c4;"
     sql "set skip_delete_sign=true;"
@@ -70,7 +58,7 @@ suite('test_partial_update_delete_sign') {
     sql "set skip_storage_engine_merge=false;"
     sql "set skip_delete_bitmap=false;"
     sql "sync"
-    def tableName2 = "test_partial_update_delete_sign2"
+    def tableName2 = "test_delete_sign_delete_bitmap2"
     sql "DROP TABLE IF EXISTS ${tableName2};"
     sql """ CREATE TABLE IF NOT EXISTS ${tableName2} (
             `k1` int NOT NULL,
@@ -89,20 +77,9 @@ suite('test_partial_update_delete_sign') {
 
     sql "insert into ${tableName2} values(1,1,1,1,1),(2,2,2,2,2),(3,3,3,3,3),(4,4,4,4,4),(5,5,5,5,5);"
     qt_sql "select * from ${tableName2} order by k1,c1,c2,c3,c4;"
-    streamLoad {
-        table "${tableName2}"
-
-        set 'column_separator', ','
-        set 'format', 'csv'
-        set 'partial_columns', 'true'
-        set 'columns', 'k1,__DORIS_DELETE_SIGN__'
-
-        file 'delete_sign.csv'
-        time 10000 // limit inflight 10s
-    }
+    sql """insert into ${tableName2}(k1,c1,c2,c3,c4,__DORIS_DELETE_SIGN__) values(1,1,1,1,1,1),(3,3,3,3,3,1),(5,5,5,5,5,1);"""
     sql "sync"
     qt_after_delete "select * from ${tableName2} order by k1,c1,c2,c3,c4;"
-
     sql "set skip_delete_sign=true;"
     sql "set skip_storage_engine_merge=true;"
     sql "set skip_delete_bitmap=true;"
