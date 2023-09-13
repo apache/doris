95da8adc97a3a3d2b09bd2b26899884225cafa1d
eldenmoon <15605149486@163.com>
2023-09-13 10:19:51 +0800

    add config to modify tablet schema recycle interval

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1144,6 +1144,8 @@ DECLARE_mInt32(tablet_schema_cache_recycle_interval);
 // Use `LOG(FATAL)` to replace `throw` when true
 DECLARE_mBool(exit_on_exception);
 
+DECLARE_mInt32(tablet_schema_cache_recycle_interval);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
