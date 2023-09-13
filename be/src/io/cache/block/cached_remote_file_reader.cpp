apache/doris

commit 09bcedb116639e64e905dcd45cf536b1064ff4bf

[feature](merge-cloud) Remove deprecated old cache (#23881)
* Remove deprecated old cache

be/src/io/cache/block/cached_remote_file_reader.cpp
@@ -42,27 +42,27 @@ namespace doris {
 namespace io {
 
 CachedRemoteFileReader::CachedRemoteFileReader(FileReaderSPtr remote_file_reader,
-                                               const std::string& cache_path,
-                                               const long modification_time)
+                                               const FileReaderOptions* opts)
         : _remote_file_reader(std::move(remote_file_reader)) {
-    // Use path and modification time to build cache key
-    std::string unique_path = fmt::format("{}:{}", cache_path, modification_time);
-    _cache_key = IFileCache::hash(unique_path);
-    _cache = FileCacheFactory::instance().get_by_path(_cache_key);
-}
-
-CachedRemoteFileReader::CachedRemoteFileReader(FileReaderSPtr remote_file_reader,
-                                               const std::string& cache_base_path,
-                                               const std::string& cache_path,
-                                               const long modification_time)
-        : _remote_file_reader(std::move(remote_file_reader)) {
-    std::string unique_path = fmt::format("{}:{}", cache_path, modification_time);
-    _cache_key = IFileCache::hash(unique_path);
-    _cache = FileCacheFactory::instance().get_by_path(cache_base_path);
-    if (_cache == nullptr) {
-        LOG(WARNING) << "Can't get cache from base path: " << cache_base_path
-                     << ", using random instead.";
+    DCHECK(opts) << remote_file_reader->path().native();
+    _is_doris_table = opts->is_doris_table;
+    if (_is_doris_table) {
+        _cache_key = IFileCache::hash(path().filename().native());
         _cache = FileCacheFactory::instance().get_by_path(_cache_key);
+    } else {
+        // Use path and modification time to build cache key
+        std::string unique_path = fmt::format("{}:{}", path().native(), opts->modification_time);
+        _cache_key = IFileCache::hash(unique_path);
+        if (!opts->cache_base_path.empty()) {
+            // from query session variable: file_cache_base_path
+            _cache = FileCacheFactory::instance().get_by_path(opts->cache_base_path);
+            if (_cache == nullptr) {
+                LOG(WARNING) << "Can't get cache from base path: " << opts->cache_base_path
+                             << ", using random instead.";
+                _cache = FileCacheFactory::instance().get_by_path(_cache_key);
+            }
+        }
+        _cache = FileCacheFactory::instance().get_by_path(path().native());
     }
 }
 

be/src/common/config.cpp
@@ -882,20 +882,14 @@ DEFINE_mInt32(remove_unused_remote_files_interval_sec, "21600"); // 6h
 DEFINE_mInt32(confirm_unused_remote_files_interval_sec, "60");
 DEFINE_Int32(cold_data_compaction_thread_num, "2");
 DEFINE_mInt32(cold_data_compaction_interval_sec, "1800");
-DEFINE_mInt64(generate_cache_cleaner_task_interval_sec, "43200"); // 12 h
 DEFINE_Int32(concurrency_per_dir, "2");
-DEFINE_mInt64(cooldown_lag_time_sec, "10800");       // 3h
-DEFINE_mInt64(max_sub_cache_file_size, "104857600"); // 100MB
-DEFINE_mInt64(file_cache_alive_time_sec, "604800");  // 1 week
 // file_cache_type is used to set the type of file cache for remote files.
 // "": no cache, "sub_file_cache": split sub files from remote file.
 // "whole_file_cache": the whole file.
 DEFINE_mString(file_cache_type, "file_block_cache");
-DEFINE_Validator(file_cache_type, [](const std::string config) -> bool {
-    return config == "sub_file_cache" || config == "whole_file_cache" || config == "" ||
-           config == "file_block_cache";
+DEFINE_Validator(file_cache_type, [](std::string_view config) -> bool {
+    return config == "" || config == "file_block_cache";
 });
-DEFINE_mInt64(file_cache_max_size_per_disk, "0"); // zero for no limit
 
 DEFINE_Int32(s3_transfer_executor_pool_size, "2");
 

be/src/common/config.h
@@ -933,16 +933,11 @@ DECLARE_mInt32(remove_unused_remote_files_interval_sec); // 6h
 DECLARE_mInt32(confirm_unused_remote_files_interval_sec);
 DECLARE_Int32(cold_data_compaction_thread_num);
 DECLARE_mInt32(cold_data_compaction_interval_sec);
-DECLARE_mInt64(generate_cache_cleaner_task_interval_sec); // 12 h
 DECLARE_Int32(concurrency_per_dir);
-DECLARE_mInt64(cooldown_lag_time_sec);     // 3h
-DECLARE_mInt64(max_sub_cache_file_size);   // 100MB
-DECLARE_mInt64(file_cache_alive_time_sec); // 1 week
 // file_cache_type is used to set the type of file cache for remote files.
 // "": no cache, "sub_file_cache": split sub files from remote file.
 // "whole_file_cache": the whole file.
 DECLARE_mString(file_cache_type);
-DECLARE_mInt64(file_cache_max_size_per_disk); // zero for no limit
 
 DECLARE_Int32(s3_transfer_executor_pool_size);
 

be/src/io/cache/block/cached_remote_file_reader.h
@@ -39,11 +39,7 @@ struct FileCacheStatistics;
 
 class CachedRemoteFileReader final : public FileReader {
 public:
-    CachedRemoteFileReader(FileReaderSPtr remote_file_reader, const std::string& cache_path,
-                           const long modification_time);
-
-    CachedRemoteFileReader(FileReaderSPtr remote_file_reader, const std::string& cache_base_path,
-                           const std::string& cache_path, const long modification_time);
+    CachedRemoteFileReader(FileReaderSPtr remote_file_reader, const FileReaderOptions* opts);
 
     ~CachedRemoteFileReader() override;
 
@@ -69,6 +65,7 @@ private:
     FileReaderSPtr _remote_file_reader;
     IFileCache::Key _cache_key;
     CloudFileCachePtr _cache;
+    bool _is_doris_table;
 
     struct ReadStatistics {
         bool hit_cache = true;

be/src/io/cache/dummy_file_cache.cpp
@@ -1,104 +1,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-#include "io/cache/dummy_file_cache.h"
-
-#include <time.h>
-
-#include <memory>
-#include <string>
-
-#include "io/fs/local_file_system.h"
-
-namespace doris {
-namespace io {
-
-DummyFileCache::DummyFileCache(const Path& cache_dir, int64_t alive_time_sec)
-        : _cache_dir(cache_dir), _alive_time_sec(alive_time_sec) {}
-
-DummyFileCache::~DummyFileCache() = default;
-
-void DummyFileCache::_add_file_cache(const Path& data_file) {
-    Path cache_file = _cache_dir / data_file;
-    int64_t file_size = -1;
-    time_t m_time = 0;
-    if (io::global_local_filesystem()->file_size(cache_file, &file_size).ok() &&
-        io::global_local_filesystem()->mtime(cache_file, &m_time).ok()) {
-        _gc_lru_queue.push({cache_file, m_time});
-        _cache_file_size += file_size;
-    } else {
-        _unfinished_files.push_back(cache_file);
-    }
-}
-
-void DummyFileCache::_load() {
-    std::vector<Path> cache_names;
-    if (!_get_dir_files_and_remove_unfinished(_cache_dir, cache_names).ok()) {
-        return;
-    }
-
-    for (const auto& file : cache_names) {
-        _add_file_cache(file);
-    }
-}
-
-Status DummyFileCache::load_and_clean() {
-    _load();
-    RETURN_IF_ERROR(_clean_unfinished_files(_unfinished_files));
-    return _check_and_delete_empty_dir(_cache_dir);
-}
-
-Status DummyFileCache::clean_timeout_cache() {
-    while (!_gc_lru_queue.empty() &&
-           time(nullptr) - _gc_lru_queue.top().last_match_time > _alive_time_sec) {
-        size_t cleaned_size = 0;
-        RETURN_IF_ERROR(_clean_cache_internal(_gc_lru_queue.top().file, &cleaned_size));
-        _cache_file_size -= cleaned_size;
-        _gc_lru_queue.pop();
-    }
-    return Status::OK();
-}
-
-Status DummyFileCache::clean_all_cache() {
-    while (!_gc_lru_queue.empty()) {
-        RETURN_IF_ERROR(_clean_cache_internal(_gc_lru_queue.top().file, nullptr));
-        _gc_lru_queue.pop();
-    }
-    _cache_file_size = 0;
-    return _check_and_delete_empty_dir(_cache_dir);
-}
-
-Status DummyFileCache::clean_one_cache(size_t* cleaned_size) {
-    if (!_gc_lru_queue.empty()) {
-        const auto& cache = _gc_lru_queue.top();
-        RETURN_IF_ERROR(_clean_cache_internal(cache.file, cleaned_size));
-        _cache_file_size -= *cleaned_size;
-        _gc_lru_queue.pop();
-    }
-    if (_gc_lru_queue.empty()) {
-        RETURN_IF_ERROR(_check_and_delete_empty_dir(_cache_dir));
-    }
-    return Status::OK();
-}
-
-Status DummyFileCache::_clean_cache_internal(const Path& cache_file_path, size_t* cleaned_size) {
-    Path done_file_path = cache_file_path.native() + CACHE_DONE_FILE_SUFFIX;
-    return _remove_cache_and_done(cache_file_path, done_file_path, cleaned_size);
-}
-
-} // namespace io
-} // namespace doris

be/src/io/cache/dummy_file_cache.h
@@ -1,102 +1,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-#pragma once
-
-#include <stddef.h>
-#include <stdint.h>
-
-#include <filesystem>
-#include <queue>
-#include <vector>
-
-#include "common/status.h"
-#include "io/cache/file_cache.h"
-#include "io/fs/file_reader_writer_fwd.h"
-#include "io/fs/file_system.h"
-#include "io/fs/path.h"
-#include "util/slice.h"
-
-namespace doris {
-namespace io {
-class IOContext;
-
-// Only used for GC
-class DummyFileCache final : public FileCache {
-public:
-    DummyFileCache(const Path& cache_dir, int64_t alive_time_sec);
-
-    ~DummyFileCache() override;
-
-    Status close() override { return Status::OK(); }
-
-    const Path& path() const override { return _cache_dir; }
-
-    size_t size() const override { return 0; }
-
-    bool closed() const override { return true; }
-
-    const Path& cache_dir() const override { return _cache_dir; }
-
-    io::FileReaderSPtr remote_file_reader() const override { return nullptr; }
-
-    Status clean_timeout_cache() override;
-
-    Status clean_all_cache() override;
-
-    Status clean_one_cache(size_t* cleaned_size) override;
-
-    Status load_and_clean();
-
-    bool is_dummy_file_cache() override { return true; }
-
-    int64_t get_oldest_match_time() const override {
-        return _gc_lru_queue.empty() ? 0 : _gc_lru_queue.top().last_match_time;
-    }
-
-    bool is_gc_finish() const override { return _gc_lru_queue.empty(); }
-
-    FileSystemSPtr fs() const override { return nullptr; }
-
-protected:
-    Status read_at_impl(size_t offset, Slice result, size_t* bytes_read,
-                        const IOContext* io_ctx) override {
-        return Status::NotSupported("dummy file cache only used for GC");
-    }
-
-private:
-    void _add_file_cache(const Path& data_file);
-    void _load();
-    Status _clean_cache_internal(const Path&, size_t*);
-
-private:
-    struct DummyFileInfo {
-        Path file;
-        int64_t last_match_time;
-    };
-    using DummyGcQueue = std::priority_queue<DummyFileInfo, std::vector<DummyFileInfo>,
-                                             SubFileLRUComparator<DummyFileInfo>>;
-    DummyGcQueue _gc_lru_queue;
-
-    Path _cache_dir;
-    int64_t _alive_time_sec;
-
-    std::vector<Path> _unfinished_files;
-};
-
-} // namespace io
-} // namespace doris

be/src/io/cache/file_cache.cpp
@@ -1,210 +1,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-#include "io/cache/file_cache.h"
-
-#include <fmt/format.h>
-#include <glog/logging.h>
-#include <string.h>
-
-#include <algorithm>
-#include <filesystem>
-#include <list>
-#include <ostream>
-#include <set>
-#include <utility>
-
-#include "common/config.h"
-#include "common/status.h"
-#include "gutil/strings/util.h"
-#include "io/fs/file_system.h"
-#include "io/fs/file_writer.h"
-#include "io/fs/local_file_system.h"
-#include "runtime/exec_env.h"
-#include "util/string_util.h"
-
-namespace doris {
-using namespace ErrorCode;
-namespace io {
-
-Status FileCache::download_cache_to_local(const Path& cache_file, const Path& cache_done_file,
-                                          io::FileReaderSPtr remote_file_reader, size_t req_size,
-                                          size_t offset) {
-    LOG(INFO) << "Download cache file from remote file: " << remote_file_reader->path().native()
-              << " -> " << cache_file.native() << ". offset: " << offset
-              << ", request size: " << req_size;
-    io::FileWriterPtr file_writer;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->create_file(cache_file, &file_writer),
-            fmt::format("Create local cache file failed: {}", cache_file.native()));
-    auto func = [cache_file, cache_done_file, remote_file_reader, req_size,
-                 offset](io::FileWriter* file_writer) {
-        char* file_buf = ExecEnv::GetInstance()->get_download_cache_buf(
-                ExecEnv::GetInstance()->get_serial_download_cache_thread_token());
-        size_t count_bytes_read = 0;
-        size_t need_req_size = config::download_cache_buffer_size;
-        while (count_bytes_read < req_size) {
-            memset(file_buf, 0, need_req_size);
-            if (req_size - count_bytes_read < config::download_cache_buffer_size) {
-                need_req_size = req_size - count_bytes_read;
-            }
-            Slice file_slice(file_buf, need_req_size);
-            size_t bytes_read = 0;
-            RETURN_NOT_OK_STATUS_WITH_WARN(
-                    remote_file_reader->read_at(offset + count_bytes_read, file_slice, &bytes_read),
-                    fmt::format("read remote file failed. {}. offset: {}, request size: {}",
-                                remote_file_reader->path().native(), offset + count_bytes_read,
-                                need_req_size));
-            if (bytes_read != need_req_size) {
-                return Status::Error<OS_ERROR>(
-                        "read remote file failed: {}, bytes read: {} vs need read size: {}",
-                        remote_file_reader->path().native(), bytes_read, need_req_size);
-            }
-            count_bytes_read += bytes_read;
-            RETURN_NOT_OK_STATUS_WITH_WARN(
-                    file_writer->append(file_slice),
-                    fmt::format("Write local cache file failed: {}", cache_file.native()));
-        }
-        return Status::OK();
-    };
-    auto st = func(file_writer.get());
-    if (!st.ok()) {
-        WARN_IF_ERROR(file_writer->close(),
-                      fmt::format("Close local cache file failed: {}", cache_file.native()));
-        return st;
-    }
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            file_writer->close(),
-            fmt::format("Close local cache file failed: {}", cache_file.native()));
-    io::FileWriterPtr done_file_writer;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->create_file(cache_done_file, &done_file_writer),
-            fmt::format("Create local done file failed: {}", cache_done_file.native()));
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            done_file_writer->close(),
-            fmt::format("Close local done file failed: {}", cache_done_file.native()));
-    return Status::OK();
-}
-
-Status FileCache::_remove_file(const Path& file, size_t* cleaned_size) {
-    bool cache_file_exist = false;
-    RETURN_NOT_OK_STATUS_WITH_WARN(io::global_local_filesystem()->exists(file, &cache_file_exist),
-                                   "Check local cache file exist failed.");
-    int64_t file_size = -1;
-    if (cache_file_exist) {
-        RETURN_NOT_OK_STATUS_WITH_WARN(
-                io::global_local_filesystem()->file_size(file, &file_size),
-                fmt::format("get local cache file size failed: {}", file.native()));
-
-        RETURN_NOT_OK_STATUS_WITH_WARN(
-                io::global_local_filesystem()->delete_file(file),
-                fmt::format("Delete local cache file failed: {}", file.native()));
-        LOG(INFO) << "Delete local cache file successfully: " << file.native()
-                  << ", file size: " << file_size;
-    }
-    if (cleaned_size) {
-        *cleaned_size = file_size;
-    }
-    return Status::OK();
-}
-
-Status FileCache::_remove_cache_and_done(const Path& cache_file, const Path& cache_done_file,
-                                         size_t* cleaned_size) {
-    RETURN_IF_ERROR(_remove_file(cache_done_file, nullptr));
-    RETURN_IF_ERROR(_remove_file(cache_file, cleaned_size));
-    return Status::OK();
-}
-
-Status FileCache::_get_dir_files_and_remove_unfinished(const Path& cache_dir,
-                                                       std::vector<Path>& cache_names) {
-    bool cache_dir_exist = true;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->exists(cache_dir, &cache_dir_exist),
-            fmt::format("Check local cache dir exist failed. {}", cache_dir.native()));
-    if (!cache_dir_exist) {
-        return Status::OK();
-    }
-
-    // list all files
-    std::vector<FileInfo> cache_files;
-    bool exists = true;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->list(cache_dir, true, &cache_files, &exists),
-            fmt::format("List dir failed: {}", cache_dir.native()))
-
-    // separate DATA file and DONE file
-    std::set<Path> cache_names_temp;
-    std::list<Path> done_names_temp;
-    for (auto& cache_file : cache_files) {
-        if (ends_with(cache_file.file_name, CACHE_DONE_FILE_SUFFIX)) {
-            done_names_temp.push_back(cache_file.file_name);
-        } else {
-            cache_names_temp.insert(cache_file.file_name);
-        }
-    }
-
-    // match DONE file with DATA file
-    for (auto done_file : done_names_temp) {
-        Path cache_filename = StringReplace(done_file.native(), CACHE_DONE_FILE_SUFFIX, "", true);
-        if (auto cache_iter = cache_names_temp.find(cache_filename);
-            cache_iter != cache_names_temp.end()) {
-            cache_names_temp.erase(cache_iter);
-            cache_names.push_back(std::move(cache_filename));
-        } else {
-            // not data file, but with DONE file
-            RETURN_IF_ERROR(_remove_file(done_file, nullptr));
-        }
-    }
-    // data file without DONE file
-    for (auto& file : cache_names_temp) {
-        RETURN_IF_ERROR(_remove_file(file, nullptr));
-    }
-    return Status::OK();
-}
-
-Status FileCache::_clean_unfinished_files(const std::vector<Path>& unfinished_files) {
-    // remove cache file without done file
-    for (auto file : unfinished_files) {
-        RETURN_IF_ERROR(_remove_file(file, nullptr));
-    }
-    return Status::OK();
-}
-
-Status FileCache::_check_and_delete_empty_dir(const Path& cache_dir) {
-    bool cache_dir_exist = true;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->exists(cache_dir, &cache_dir_exist),
-            fmt::format("Check local cache dir exist failed. {}", cache_dir.native()));
-    if (!cache_dir_exist) {
-        return Status::OK();
-    }
-
-    std::vector<FileInfo> cache_files;
-    bool exists = true;
-    RETURN_NOT_OK_STATUS_WITH_WARN(
-            io::global_local_filesystem()->list(cache_dir, true, &cache_files, &exists),
-            fmt::format("List dir failed: {}", cache_dir.native()));
-    if (cache_files.empty()) {
-        RETURN_NOT_OK_STATUS_WITH_WARN(io::global_local_filesystem()->delete_directory(cache_dir),
-                                       fmt::format("Delete dir failed: {}", cache_dir.native()));
-        LOG(INFO) << "Delete empty dir: " << cache_dir.native();
-    }
-    return Status::OK();
-}
-
-} // namespace io
-} // namespace doris

be/src/io/cache/file_cache.h
@@ -1,106 +1,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-#pragma once
-
-#include <butil/macros.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#include <memory>
-#include <string>
-#include <vector>
-
-#include "common/status.h"
-#include "io/fs/file_reader.h"
-#include "io/fs/file_reader_writer_fwd.h"
-#include "io/fs/path.h"
-#include "util/slice.h"
-
-namespace doris {
-namespace io {
-class IOContext;
-
-const std::string CACHE_DONE_FILE_SUFFIX = "_DONE";
-
-class FileCache : public FileReader {
-public:
-    FileCache() : _cache_file_size(0) {}
-    ~FileCache() override = default;
-
-    DISALLOW_COPY_AND_ASSIGN(FileCache);
-
-    virtual const Path& cache_dir() const = 0;
-
-    size_t cache_file_size() const { return _cache_file_size; }
-
-    virtual io::FileReaderSPtr remote_file_reader() const = 0;
-
-    virtual Status clean_timeout_cache() = 0;
-
-    virtual Status clean_all_cache() = 0;
-
-    virtual Status clean_one_cache(size_t* cleaned_size) = 0;
-
-    virtual bool is_gc_finish() const = 0;
-
-    virtual bool is_dummy_file_cache() { return false; }
-
-    Status download_cache_to_local(const Path& cache_file, const Path& cache_done_file,
-                                   io::FileReaderSPtr remote_file_reader, size_t req_size,
-                                   size_t offset = 0);
-
-    virtual int64_t get_oldest_match_time() const = 0;
-
-protected:
-    Status read_at_impl(size_t offset, Slice result, size_t* bytes_read,
-                        const IOContext* io_ctx) override {
-        return Status::NotSupported("dummy file cache only used for GC");
-    }
-
-    Status _remove_file(const Path& file, size_t* cleaned_size);
-
-    Status _remove_cache_and_done(const Path& cache_file, const Path& cache_done_file,
-                                  size_t* cleaned_size);
-
-    Status _get_dir_files_and_remove_unfinished(const Path& cache_dir,
-                                                std::vector<Path>& cache_names);
-
-    Status _clean_unfinished_files(const std::vector<Path>& unfinished_files);
-
-    Status _check_and_delete_empty_dir(const Path& cache_dir);
-
-    template <typename T>
-    struct SubFileLRUComparator {
-        bool operator()(const T& lhs, const T& rhs) const {
-            return lhs.last_match_time > rhs.last_match_time;
-        }
-    };
-
-    size_t _cache_file_size;
-};
-
-using FileCachePtr = std::shared_ptr<FileCache>;
-
-struct FileCacheLRUComparator {
-    bool operator()(const FileCachePtr& lhs, const FileCachePtr& rhs) const {
-        return lhs->get_oldest_match_time() > rhs->get_oldest_match_time();
-    }
-};
-
-} // namespace io
-} // namespace doris

commit 50c8719e98d679684d40cc86cbe1d6e9c61d5090

[fix](FileCache) the logic of selecting the cache path is reversed (#24277)
Bug was introduced by https://github.com/apache/doris/pull/23881/files
The logic of selecting the cache path is reversed, and BE will be crashed when enable file cache.

be/src/io/cache/block/cached_remote_file_reader.cpp
@@ -52,7 +52,10 @@ CachedRemoteFileReader::CachedRemoteFileReader(FileReaderSPtr remote_file_reader
         // Use path and modification time to build cache key
         std::string unique_path = fmt::format("{}:{}", path().native(), opts.mtime);
         _cache_key = IFileCache::hash(unique_path);
-        if (!opts.cache_base_path.empty()) {
+        if (opts.cache_base_path.empty()) {
+            // if cache path is not specified by session variable, chose randomly.
+            _cache = FileCacheFactory::instance()->get_by_path(_cache_key);
+        } else {
             // from query session variable: file_cache_base_path
             _cache = FileCacheFactory::instance()->get_by_path(opts.cache_base_path);
             if (_cache == nullptr) {
@@ -61,7 +64,6 @@ CachedRemoteFileReader::CachedRemoteFileReader(FileReaderSPtr remote_file_reader
                 _cache = FileCacheFactory::instance()->get_by_path(_cache_key);
             }
         }
-        _cache = FileCacheFactory::instance()->get_by_path(path().native());
     }
 }
 
@@ -91,6 +93,7 @@ std::pair<size_t, size_t> CachedRemoteFileReader::_align_size(size_t offset,
 Status CachedRemoteFileReader::_read_from_cache(size_t offset, Slice result, size_t* bytes_read,
                                                 const IOContext* io_ctx) {
     size_t bytes_req = result.size;
+    bytes_req = std::min(bytes_req, size() - offset);
     ReadStatistics stats;
     auto [align_left, align_size] = _align_size(offset, bytes_req);
     CacheContext cache_context(io_ctx);