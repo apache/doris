acb04b1781881d7f5c58ad8caf36ea73060ee9d5
zhiqiang-hhhh <seuhezhiqiang@163.com>
2023-09-14 19:02:12 +0800

    uuid-numeric

--- gensrc/script/doris_builtins_functions.py
+++ gensrc/script/doris_builtins_functions.py
@@ -1976,7 +1976,8 @@ visible_functions = {
     ],
 
     "UUID": [
-        [['uuid'], 'VARCHAR', [], 'ALWAYS_NOT_NULLABLE']
+        [['uuid'], 'VARCHAR', [], 'ALWAYS_NOT_NULLABLE'],
+        [['uuid_numeric'], 'LARGEINT', [], 'ALWAYS_NOT_NULLABLE']
     ],
 
     #ip functions
