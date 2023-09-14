4b96c806c0f162b6cb4e9ee64361a17f2f39a73a
Kang <kxiao.tiger@gmail.com>
2023-09-14 21:23:41 +0800

    fix merge be compile error

--- be/src/vec/exec/join/vnested_loop_join_node.h
+++ be/src/vec/exec/join/vnested_loop_join_node.h
@@ -260,7 +260,6 @@ private:
     std::stack<uint16_t> _probe_offset_stack;
     VExprContextSPtrs _join_conjuncts;
     RuntimeProfile::Counter* _loop_join_timer;
-    template <typename Parent>
     friend struct RuntimeFilterBuild;
 };
 
