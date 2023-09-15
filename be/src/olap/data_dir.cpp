6ad97b61098de634270f28249ffec1606f93a84e
yujun <yu.jun.reach@gmail.com>
2023-09-15 11:39:43 +0800

    [improvement](create tablet) backend create tablet round robin among disks (#23218)
    Backend choose disk by disk available bytes and tablet num. If both are equal, round robin among them.

--- be/src/olap/data_dir.cpp
+++ be/src/olap/data_dir.cpp
@@ -834,7 +834,15 @@ void DataDir::update_remote_data_size(int64_t size) {
     disks_remote_used_capacity->set_value(size);
 }
 
-size_t DataDir::tablet_size() const {
+size_t DataDir::disk_capacity() const {
+    return _disk_capacity_bytes;
+}
+
+size_t DataDir::disk_available() const {
+    return _available_bytes;
+}
+
+size_t DataDir::tablet_num() const {
     std::lock_guard<std::mutex> l(_mutex);
     return _tablet_set.size();
 }
