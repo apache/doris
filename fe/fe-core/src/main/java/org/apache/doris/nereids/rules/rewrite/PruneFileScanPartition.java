3fe79bc60eb3b1fa307f60dff11a69eb50f11361
Mingyu Chen <morningman@163.com>
2023-09-16 12:17:44 +0800

    [fix](hive) fix partition prune issue and some external table test cases (#24482)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PruneFileScanPartition.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PruneFileScanPartition.java
@@ -34,6 +34,7 @@ import org.apache.doris.nereids.trees.plans.logical.LogicalFileScan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalFileScan.SelectedPartitions;
 import org.apache.doris.nereids.trees.plans.logical.LogicalFilter;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import org.apache.commons.collections.CollectionUtils;
 
@@ -55,20 +56,20 @@ public class PruneFileScanPartition extends OneRewriteRuleFactory {
 
     @Override
     public Rule build() {
-        return logicalFilter(logicalFileScan()).when(p -> !p.child().getSelectedPartitions().isPartitionPruned())
+        return logicalFilter(logicalFileScan()).whenNot(p -> p.child().getSelectedPartitions().isPruned)
                 .thenApply(ctx -> {
                     LogicalFilter<LogicalFileScan> filter = ctx.root;
                     LogicalFileScan scan = filter.child();
                     ExternalTable tbl = scan.getTable();
-                    SelectedPartitions selectedPartitions = new SelectedPartitions(0, Maps.newHashMap(), false);
 
+                    SelectedPartitions selectedPartitions;
                     // TODO(cmy): support other external table
                     if (tbl instanceof HMSExternalTable && ((HMSExternalTable) tbl).getDlaType() == DLAType.HIVE) {
                         HMSExternalTable hiveTbl = (HMSExternalTable) tbl;
                         selectedPartitions = pruneHivePartitions(hiveTbl, filter, scan, ctx.cascadesContext);
                     } else {
-                        // set isPartitionPruned so that it won't go pass the partition prune again
-                        selectedPartitions.isPartitionPruned = true;
+                        // set isPruned so that it won't go pass the partition prune again
+                        selectedPartitions = new SelectedPartitions(0, ImmutableMap.of(), true);
                     }
 
                     LogicalFileScan rewrittenScan = scan.withConjuncts(filter.getConjuncts())
@@ -81,9 +82,9 @@ public class PruneFileScanPartition extends OneRewriteRuleFactory {
             LogicalFilter<LogicalFileScan> filter, LogicalFileScan scan, CascadesContext ctx) {
         Map<Long, PartitionItem> selectedPartitionItems = Maps.newHashMap();
         if (CollectionUtils.isEmpty(hiveTbl.getPartitionColumns())) {
-            // non partitioned table, return null.
-            // and it will be handled in HiveScanNode
-            return new SelectedPartitions(1, Maps.newHashMap(), false);
+            // non partitioned table, return NOT_PRUNED.
+            // non partition table will be handled in HiveScanNode.
+            return SelectedPartitions.NOT_PRUNED;
         }
         Map<String, Slot> scanOutput = scan.getOutput()
                 .stream()
