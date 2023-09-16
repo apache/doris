be6eb00e12e698d7fce720beac44c00c7850ceb5
Lei Zhang <27994433+SWJTU-ZhangLei@users.noreply.github.com>
2023-09-16 14:52:27 +0800

    [fix](bdbje) Remove improper check for journalId (#24464)
    * Introduced by  https://github.com/apache/doris/pull/24259

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -2653,14 +2653,6 @@ public class Env {
             LOG.warn("replay journal cost too much time: {} replayedJournalId: {}", cost, replayedJournalId);
         }
 
-        if (replayedJournalId.get() != newToJournalId) {
-            String msg = "replayedJournalId:" + replayedJournalId.get() + " not equal with newToJournalId:"
-                    + newToJournalId + " , will exit";
-            LOG.error(msg);
-            Util.stdoutWithTime(msg);
-            System.exit(-1);
-        }
-
         return hasLog;
     }
 
