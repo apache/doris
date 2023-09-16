dc6110d29da595c0cfb64b80f3f57fd8fac5b4d8
bobhan1 <bh2444151092@outlook.com>
2023-09-17 07:20:00 +0800

    fix

--- be/src/vec/sink/vtablet_sink.h
+++ be/src/vec/sink/vtablet_sink.h
@@ -424,7 +424,7 @@ public:
     void set_tablets_filtered_rows(
             const std::vector<std::pair<int64_t, int64_t>>& tablets_filtered_rows, int64_t node_id);
     int64_t num_rows_filtered() {
-        CHECK(!_tablets_filtered_rows.empty());
+        DCHECK(!_tablets_filtered_rows.empty());
         // the Unique table has no roll up or materilized view
         // we just add up filtered rows from all partitions
         return std::accumulate(_tablets_filtered_rows.cbegin(), _tablets_filtered_rows.cend(), 0,
