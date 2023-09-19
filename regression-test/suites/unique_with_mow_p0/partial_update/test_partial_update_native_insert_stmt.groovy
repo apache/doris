69412017e1ec5757f4fb113c6f4146ab437cf362
bobhan1 <bh2444151092@outlook.com>
2023-09-19 10:24:22 +0800

    fix

--- regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_native_insert_stmt.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_native_insert_stmt.groovy
@@ -100,6 +100,7 @@ suite("test_partial_update_native_insert_stmt", "p0") {
     sql """insert into ${tableName3} values(2, "doris2", 2000, 223, 1),(1, "doris", 1000, 123, 1);"""
     qt_3 """ select * from ${tableName3} order by id; """
     sql "set enable_unique_key_partial_update=true;"
+    sql "set enable_insert_strict = false;"
     sql "sync;"
     // in partial update, the unmentioned columns should have default values or be nullable
     // but field `name` is not nullable and doesn't have default value
