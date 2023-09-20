1405b7ca82e7beaffa461be384915afee312acc4
lihangyu <15605149486@163.com>
2023-09-20 17:00:24 +0800

    [improve](scan) support lower the thread priority of scan thread (#24526)
    The configuration item is used to lower the priority of the scanner thread,
    typically employed to ensure CPU scheduling for write operations.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1159,6 +1159,12 @@ DECLARE_Int16(bitmap_serialize_version);
 // This config can be set to limit thread number in group commit insert thread pool.
 DECLARE_mInt32(group_commit_insert_threads);
 
+// The configuration item is used to lower the priority of the scanner thread,
+// typically employed to ensure CPU scheduling for write operations.
+// Default is 0, which is default value of thread nice value, increase this value
+// to lower the priority of scan threads
+DECLARE_Int32(scan_thread_nice_value);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
