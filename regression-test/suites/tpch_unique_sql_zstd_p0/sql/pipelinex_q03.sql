a34757422c0b621964db0ca6de24ec056953badb
Gabriel <gabrielleebuaa@gmail.com>
2023-09-20 16:32:03 +0800

    [regression](pipelineX) enable pipelineX test cases (#24670)

--- regression-test/suites/tpch_unique_sql_zstd_p0/sql/pipelinex_q03.sql
+++ regression-test/suites/tpch_unique_sql_zstd_p0/sql/pipelinex_q03.sql
@@ -1,5 +1,5 @@
 -- tables: customer,orders,lineitem
-SELECT /*+SET_VAR(experimental_enable_pipeline_x_engine=false) */
+SELECT /*+SET_VAR(experimental_enable_pipeline_x_engine=true) */
   l_orderkey,
   sum(l_extendedprice * (1 - l_discount)) AS revenue,
   o_orderdate,
