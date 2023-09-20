d1c85a8935b1f63dffcc0b4e3f4a85f90b5ac7fb
yiguolei <yiguolei@gmail.com>
2023-09-20 08:42:24 +0800

    f

--- be/src/vec/data_types/data_type.cpp
+++ be/src/vec/data_types/data_type.cpp
@@ -174,7 +174,7 @@ PGenericType_TypeId IDataType::get_pdata_type(const IDataType* data_type) {
     case TypeIndex::TimeV2:
         return PGenericType::TIMEV2;
     default:
-        LOG(FATAL) << "could not mapping type " << data_type->get_type_id() << " to pb type";
+        LOG(FATAL) << fmt::format("could not mapping type {} to pb type", data_type->get_type_id());
         return PGenericType::UNKNOWN;
     }
 }
