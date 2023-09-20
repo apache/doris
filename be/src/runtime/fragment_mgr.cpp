1ffff0736b7477010b5775359ba2cdc0e057eddf
BiteTheDDDDt <pxl290@qq.com>
2023-09-20 14:00:32 +0800

    update

--- be/src/runtime/fragment_mgr.cpp
+++ be/src/runtime/fragment_mgr.cpp
@@ -185,9 +185,9 @@ void FragmentMgr::coordinator_callback(const ReportStatusRequest& req) {
     if (!coord_status.ok()) {
         std::stringstream ss;
         UniqueId uid(req.query_id.hi, req.query_id.lo);
-        req.update_fn(
-                Status::InternalError("query_id: {}, couldn't get a client for {}, reason is {}",
-                                      uid, req.coord_addr, coord_status));
+        req.update_fn(Status::InternalError(
+                "query_id: {}, couldn't get a client for {}, reason is {}", uid.to_string(),
+                PrintThriftNetworkAddress(req.coord_addr), coord_status.to_string()));
         return;
     }
 
@@ -324,8 +324,8 @@ void FragmentMgr::coordinator_callback(const ReportStatusRequest& req) {
 
         rpc_status = Status::create<false>(res.status);
     } catch (TException& e) {
-        rpc_status = Status::InternalError("ReportExecStatus() to {} failed: {}", req.coord_addr,
-                                           e.what());
+        rpc_status = Status::InternalError("ReportExecStatus() to {} failed: {}",
+                                           PrintThriftNetworkAddress(req.coord_addr), e.what());
     }
 
     if (!rpc_status.ok()) {
