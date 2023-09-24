814c5507c34a17548f20794c686bb59b3c472990
xiejiann <jianxie0@gmail.com>
2023-09-24 22:17:14 +0800

    optimize cost and stats

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/cascades/ApplyRuleJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/cascades/ApplyRuleJob.java
@@ -30,6 +30,7 @@ import org.apache.doris.nereids.metrics.event.TransformEvent;
 import org.apache.doris.nereids.minidump.NereidsTracer;
 import org.apache.doris.nereids.pattern.GroupExpressionMatching;
 import org.apache.doris.nereids.rules.Rule;
+import org.apache.doris.nereids.rules.RuleType;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
 
@@ -82,11 +83,20 @@ public class ApplyRuleJob extends Job {
                 newGroupExpression.setFromRule(rule);
                 if (newPlan instanceof LogicalPlan) {
                     pushJob(new OptimizeGroupExpressionJob(newGroupExpression, context));
+                    if (!rule.getRuleType().equals(RuleType.LOGICAL_JOIN_COMMUTE)) {
+                        pushJob(new DeriveStatsJob(newGroupExpression, context));
+                    } else {
+                        groupExpression.setStatDerived(true);
+                    }
                 } else {
                     pushJob(new CostAndEnforcerJob(newGroupExpression, context));
+                    if (newGroupExpression.children().stream().anyMatch(g -> g.getLogicalExpressions().isEmpty())) {
+                        pushJob(new DeriveStatsJob(newGroupExpression, context));
+                    } else {
+                        groupExpression.setStatDerived(true);
+                    }
                 }
-                // we should derive stats for new logical/physical plan if the plan missing the stats
-                pushJob(new DeriveStatsJob(newGroupExpression, context));
+
                 NereidsTracer.logApplyRuleEvent(rule.toString(), plan, newGroupExpression.getPlan());
                 APPLY_RULE_TRACER.log(TransformEvent.of(groupExpression, plan, newPlans, rule.getRuleType()),
                         rule::isRewrite);
