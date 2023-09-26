65b8b98ab8c21a76a6a9e6e127d74b34a7a393a3
xiejiann <jianxie0@gmail.com>
2023-09-26 23:44:39 +0800

    add warn for cost and enforce and remove stats derived in costAndEnforce job

--- fe/fe-core/src/main/java/org/apache/doris/statistics/Statistics.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/Statistics.java
@@ -123,6 +123,24 @@ public class Statistics {
         }
     }
 
+    public void enforceValid() {
+        for (Entry<Expression, ColumnStatistic> entry : expressionToColumnStats.entrySet()) {
+            ColumnStatistic columnStatistic = entry.getValue();
+            if (!checkColumnStatsValid(columnStatistic)) {
+                ColumnStatisticBuilder columnStatisticBuilder = new ColumnStatisticBuilder(columnStatistic);
+                columnStatisticBuilder.setNdv(Math.min(columnStatistic.ndv, rowCount));
+                columnStatisticBuilder.setNumNulls(Math.min(columnStatistic.numNulls, rowCount));
+                columnStatisticBuilder.setCount(rowCount);
+                columnStatistic = columnStatisticBuilder.build();
+            }
+            expressionToColumnStats.put(entry.getKey(), columnStatistic);
+        }
+    }
+
+    public boolean checkColumnStatsValid(ColumnStatistic columnStatistic) {
+        return columnStatistic.ndv <= rowCount && columnStatistic.numNulls <= rowCount;
+    }
+
     public Statistics withSel(double sel) {
         sel = StatsMathUtil.minNonNaN(sel, 1);
         return withRowCount(rowCount * sel);
