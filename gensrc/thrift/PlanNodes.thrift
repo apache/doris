3e2cca7d8a39251c3b1d2b3c84e10aa72d494d14
zhangdong <493738387@qq.com>
2023-09-28 15:48:56 +0800

    1

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -306,6 +306,11 @@ struct TPaimonFileDesc {
     4: optional string table_name
     5: optional string paimon_predicate
     6: optional map<string, string> paimon_options
+    7: optional i64 ctl_id
+    8: optional i64 db_id
+    9: optional i64 tbl_id
+    10: optional i64 last_update_time
+
 }
 
 
