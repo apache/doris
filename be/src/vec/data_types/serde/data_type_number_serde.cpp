d5ac9232969a84ab7ff6a48217fae7f1aa9ca31c
BePPPower <ftw2139@163.com>
2023-10-07 23:01:04 +0800

    fix beut

--- be/src/vec/data_types/serde/data_type_number_serde.cpp
+++ be/src/vec/data_types/serde/data_type_number_serde.cpp
@@ -21,8 +21,11 @@
 
 #include <type_traits>
 
+#include "common/exception.h"
+#include "common/status.h"
 #include "gutil/strings/numbers.h"
 #include "util/mysql_global.h"
+#include "vec/core/types.h"
 #include "vec/io/io_helper.h"
 
 namespace doris {
@@ -197,6 +200,30 @@ void DataTypeNumberSerDe<T>::read_column_from_arrow(IColumn& column,
         }
         return;
     }
+
+    // only for largeint(int128) type
+    if (arrow_array->type_id() == arrow::Type::STRING) {
+        auto concrete_array = dynamic_cast<const arrow::StringArray*>(arrow_array);
+        std::shared_ptr<arrow::Buffer> buffer = concrete_array->value_data();
+
+        for (size_t offset_i = start; offset_i < end; ++offset_i) {
+            if (!concrete_array->IsNull(offset_i)) {
+                const auto* raw_data = buffer->data() + concrete_array->value_offset(offset_i);
+                const auto raw_data_len = concrete_array->value_length(offset_i);
+
+                Int128 val = 0;
+                ReadBuffer rb(raw_data, raw_data_len);
+                if (!read_int_text_impl(val, rb)) {
+                    throw doris::Exception(ErrorCode::INVALID_ARGUMENT,
+                                           "parse number fail, string: '{}'",
+                                           std::string(rb.position(), rb.count()).c_str());
+                }
+                col_data.emplace_back(val);
+            }
+        }
+        return;
+    }
+
     /// buffers[0] is a null bitmap and buffers[1] are actual values
     std::shared_ptr<arrow::Buffer> buffer = arrow_array->data()->buffers[1];
     const auto* raw_data = reinterpret_cast<const T*>(buffer->data()) + start;
