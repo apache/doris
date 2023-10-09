d70ed56d7a6aa8ca72891e92fd125c520708b19a
minghong <englefly@gmail.com>
2023-10-09 10:11:36 +0800

    pick 25068 :align node id in explain with nereids node id (#25068)
    it is painful to align node in `explain` and node in `explain physical plan`, since they use two different sets of node IDs.
    This pr makes 'explain' command use node IDs of their correspond node in 'explain physical plan'
    
    (cherry picked from commit ffaa145728d39c8b97ef9b6d95a1cad7cbde4815)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalOlapScan.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalOlapScan.java
@@ -120,7 +120,7 @@ public class PhysicalOlapScan extends PhysicalCatalogRelation implements OlapSca
 
     @Override
     public String toString() {
-        return Utils.toSqlString("PhysicalOlapScan[" + relationId.asInt() + "]" + getGroupIdAsString(),
+        return Utils.toSqlString("PhysicalOlapScan[" + id.asInt() + "]" + getGroupIdAsString(),
                 "qualified", Utils.qualifiedName(qualifier, table.getName()),
                 "stats", statistics, "fr", getMutableState(AbstractPlan.FRAGMENT_ID)
         );
