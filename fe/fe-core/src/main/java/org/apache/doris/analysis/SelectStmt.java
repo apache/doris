95ec9c9caa67eb66028b5493ced31a5bb0ac793a
JingDas <winds0109@163.com>
2023-10-12 00:33:26 +0800

    [fix](meta) support to infer column name when create table as

--- fe/fe-core/src/main/java/org/apache/doris/analysis/SelectStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/SelectStmt.java
@@ -546,10 +546,12 @@ public class SelectStmt extends QueryStmt {
                     String columnLabel = null;
                     Class<? extends StatementBase> statementClazz = analyzer.getStatementClazz();
                     if (statementClazz != null && (CreateViewStmt.class.isAssignableFrom(statementClazz) ||
-                            CreateMaterializedViewStmt.class.isAssignableFrom(statementClazz))) {
+                            CreateMaterializedViewStmt.class.isAssignableFrom(statementClazz) ||
+                            CreateTableAsSelectStmt.class.isAssignableFrom(statementClazz))) {
                         columnLabel = item.toColumnLabel(i);
                     }
                     if (columnLabel == null) {
+                        // column label without position is applicative for query
                         columnLabel = item.toColumnLabel();
                     }
                     SlotRef aliasRef = new SlotRef(null, columnLabel);
