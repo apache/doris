d612d432eed7eb1ad7982087eb43c0ae4b9a177a
BePPPower <ftw2139@163.com>
2023-10-12 13:14:49 +0800

    modify docs and add tests

--- regression-test/suites/load_p0/tvf/test_tvf_empty_file.groovy
+++ regression-test/suites/load_p0/tvf/test_tvf_empty_file.groovy
@@ -16,29 +16,54 @@
 // under the License.
 
 suite("test_tvf_empty_file", "p0") {
-    String ak = getS3AK()
-    String sk = getS3SK()
-    String s3_endpoint = getS3Endpoint()
-    String region = getS3Region()
-    String bucket = context.config.otherConfigs.get("s3BucketName");
+       String ak = getS3AK()
+       String sk = getS3SK()
+       String s3_endpoint = getS3Endpoint()
+       String region = getS3Region()
+       String bucket = context.config.otherConfigs.get("s3BucketName");
 
-    String path = "regression/datalake"
+       String path = "regression/datalake"
 
-    order_qt_select """ SELECT * FROM S3 (
-                            "uri" = "http://${s3_endpoint}/${bucket}/${path}/empty_file_test.csv",
-                            "ACCESS_KEY"= "${ak}",
-                            "SECRET_KEY" = "${sk}",
-                            "format" = "csv",
-                            "region" = "${region}"
-                            );
-                     """
+       // ${path}/empty_file_test.csv is an empty file
+       // so it should return empty sets.
+       order_qt_select """ SELECT * FROM S3 (
+                                   "uri" = "http://${bucket}.${s3_endpoint}/${path}/empty_file_test.csv",
+                                   "ACCESS_KEY"= "${ak}",
+                                   "SECRET_KEY" = "${sk}",
+                                   "format" = "csv",
+                                   "region" = "${region}"
+                                   );
+                            """
 
-    order_qt_desc """ desc function S3 (
-                            "uri" = "http://${s3_endpoint}/${bucket}/${path}/empty_file_test.csv",
-                            "ACCESS_KEY"= "${ak}",
-                            "SECRET_KEY" = "${sk}",
-                            "format" = "csv",
-                            "region" = "${region}"
-                            );
-                     """
+       order_qt_desc """ desc function S3 (
+                                   "uri" = "http://${bucket}.${s3_endpoint}/${path}/empty_file_test.csv",
+                                   "ACCESS_KEY"= "${ak}",
+                                   "SECRET_KEY" = "${sk}",
+                                   "format" = "csv",
+                                   "region" = "${region}"
+                                   );
+                            """
+
+       // ${path}/empty_file_test*.csv matches 3 files:
+       // empty_file_test.csv, empty_file_test_1.csv, empty_file_test_2.csv
+       // empty_file_test.csv is an empty file, but
+       // empty_file_test_1.csv and empty_file_test_2.csv have data
+       // so it should return data of empty_file_test_1.csv and empty_file_test_2.cs
+       order_qt_select2 """ SELECT * FROM S3 (
+                                   "uri" = "http://${bucket}.${s3_endpoint}/${path}/empty_file_test*.csv",
+                                   "ACCESS_KEY"= "${ak}",
+                                   "SECRET_KEY" = "${sk}",
+                                   "format" = "csv",
+                                   "region" = "${region}"
+                                   ) order by c1;
+                            """
+
+       order_qt_des2 """ desc function S3 (
+                                   "uri" = "http://${bucket}.${s3_endpoint}/${path}/empty_file_test*.csv",
+                                   "ACCESS_KEY"= "${ak}",
+                                   "SECRET_KEY" = "${sk}",
+                                   "format" = "csv",
+                                   "region" = "${region}"
+                                   );
+                            """
 }
