9faa47b77f8db26cec9f297446e3bc34f2d3f5f0
lsy3993 <xm610487344@hotmail.com>
2023-10-13 11:09:16 +0800

    support underscore prefix when select outfile

--- fe/fe-core/src/main/java/org/apache/doris/common/FeNameFormat.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/FeNameFormat.java
@@ -30,12 +30,14 @@ import com.google.common.base.Strings;
 public class FeNameFormat {
     private static final String LABEL_REGEX = "^[-_A-Za-z0-9:]{1,128}$";
     private static final String COMMON_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9-_]{0,63}$";
+    private static final String UNDERSCORE_COMMON_NAME_REGEX = "^[_a-zA-Z][a-zA-Z0-9-_]{0,63}$";
     private static final String TABLE_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9-_]*$";
     private static final String USER_NAME_REGEX = "^[a-zA-Z][a-zA-Z0-9.-_]*$";
     private static final String COLUMN_NAME_REGEX = "^[_a-zA-Z@0-9\\s<>/][.a-zA-Z0-9_+-/><?@#$%^&*\"\\s,:]{0,255}$";
 
     private static final String UNICODE_LABEL_REGEX = "^[-_A-Za-z0-9:\\p{L}]{1,128}$";
     private static final String UNICODE_COMMON_NAME_REGEX = "^[a-zA-Z\\p{L}][a-zA-Z0-9-_\\p{L}]{0,63}$";
+    private static final String UNICODE_UNDERSCORE_COMMON_NAME_REGEX = "^[_a-zA-Z\\p{L}][a-zA-Z0-9-_\\p{L}]{0,63}$";
     private static final String UNICODE_TABLE_NAME_REGEX = "^[a-zA-Z\\p{L}][a-zA-Z0-9-_\\p{L}]*$";
     private static final String UNICODE_USER_NAME_REGEX = "^[a-zA-Z\\p{L}][a-zA-Z0-9.-_\\p{L}]*$";
     private static final String UNICODE_COLUMN_NAME_REGEX
@@ -137,6 +139,11 @@ public class FeNameFormat {
             ErrorReport.reportAnalysisException(ErrorCode.ERR_WRONG_NAME_FORMAT, type, name);
         }
     }
+    public static void checkSuccessFileName(String type, String name) throws AnalysisException {
+        if (Strings.isNullOrEmpty(name) || !name.matches(getSuccessFileNameRegex())) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_WRONG_NAME_FORMAT, type, name);
+        }
+    }
 
     private static boolean isEnableUnicodeNameSupport() {
         boolean unicodeSupport;
@@ -186,5 +193,9 @@ public class FeNameFormat {
         } else {
             return COMMON_NAME_REGEX;
         }
+    }
+
+    public static String getSuccessFileNameRegex() {
+        return FeNameFormat.isEnableUnicodeNameSupport() ? UNICODE_UNDERSCORE_COMMON_NAME_REGEX : UNDERSCORE_COMMON_NAME_REGEX;
     }
 }
