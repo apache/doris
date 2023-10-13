441c80ea9210c7e422d429f8bfffefc5cd2efedd
Jerry Hu <mrhhsg@gmail.com>
2023-10-13 11:44:18 -0500

    [improvement](scanner) Remove the predicate that is always true for the segment (#25366) (#25427)
    By utilizing the zonemap index of the segment, we can ascertain if a predicate is always true. For example, if the segment’s maximum value is 100 and the predicate is col < 101, then this predicate is always true for this segment.

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1085,6 +1085,8 @@ DEFINE_mInt32(tablet_schema_cache_recycle_interval, "86400");
 
 DEFINE_Bool(exit_on_exception, "false")
 
+DEFINE_Bool(ignore_always_true_predicate_for_segment, "true");
+
 // clang-format off
 #ifdef BE_TEST
 // test s3
