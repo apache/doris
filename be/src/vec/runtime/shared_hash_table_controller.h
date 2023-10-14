032ea1e6c9ede7508363e382b5c8bb00bf45bdf6
Adonis Ling <adonis0147@gmail.com>
2023-10-14 04:25:15 +0000

    [chore](build) Fix compilation errors reported by GCC-13

--- be/src/vec/runtime/shared_hash_table_controller.h
+++ be/src/vec/runtime/shared_hash_table_controller.h
@@ -26,6 +26,7 @@
 #include <vector>
 
 #include "common/status.h"
+#include "vec/core/block.h"
 
 namespace doris {
 
@@ -42,7 +43,6 @@ class SharedHashTableDependency;
 namespace vectorized {
 
 class Arena;
-class Block;
 
 struct SharedRuntimeFilterContext {
     std::shared_ptr<MinMaxFuncBase> minmax_func;
