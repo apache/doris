112a9ae391300db8671821123232e34ba5da0c49
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2023-10-16 11:05:48 +0800

    [fix](functions) add quantile_state_empty function signature (#25306)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/BuiltinScalarFunctions.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/BuiltinScalarFunctions.java
@@ -286,6 +286,7 @@ import org.apache.doris.nereids.trees.expressions.functions.scalar.Pow;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Power;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Protocol;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.QuantilePercent;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.QuantileStateEmpty;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Quarter;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Radians;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Random;
@@ -693,6 +694,7 @@ public class BuiltinScalarFunctions implements FunctionHelper {
             scalar(Power.class, "power"),
             scalar(Protocol.class, "protocol"),
             scalar(QuantilePercent.class, "quantile_percent"),
+            scalar(QuantileStateEmpty.class, "quantile_state_empty"),
             scalar(Quarter.class, "quarter"),
             scalar(Radians.class, "radians"),
             scalar(Random.class, "rand", "random"),
