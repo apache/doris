26430c4f0536158159ef7e6a33096517ceca401d
Yukang-Lian <Yukang.Lian2022@gmail.com>
2023-10-18 12:35:27 +0800

    5

--- be/test/olap/compaction_delete_bitmap_calculator_test.cpp
+++ be/test/olap/compaction_delete_bitmap_calculator_test.cpp
@@ -244,8 +244,7 @@ TEST_F(CompactionDeleteBitmapCalculatorTest, test) {
     RowsetIdUnorderedSet set;
     set.insert(_tablet_meta->all_rs_metas()[0]->rowset_id());
     _txn_mgr->set_txn_related_delete_bitmap(partition_id, 1, _tablet->tablet_id(),
-                                                           _tablet->tablet_uid(), true,
-                                                           delete_bitmap, set);
+                                            _tablet->tablet_uid(), true, delete_bitmap, set);
 
     // commit rowset 2
     load_id.set_hi(2);
@@ -256,9 +255,8 @@ TEST_F(CompactionDeleteBitmapCalculatorTest, test) {
                                   false);
     EXPECT_TRUE(status == Status::OK());
     set.insert(_tablet_meta->all_rs_metas()[1]->rowset_id());
-    _txn_mgr->set_txn_related_delete_bitmap(partition_id, 2, tablet_id,
-                                                           _tablet_uid, true,
-                                                           delete_bitmap, set);
+    _txn_mgr->set_txn_related_delete_bitmap(partition_id, 2, tablet_id, _tablet_uid, true,
+                                            delete_bitmap, set);
 
     // prepare rowset 3
     load_id.set_hi(3);
@@ -277,5 +275,3 @@ TEST_F(CompactionDeleteBitmapCalculatorTest, test) {
     EXPECT_EQ(commit_tablet_txn_info_vec.size(), 1);
 }
 } // namespace doris
-
-// @brief Test Stub
