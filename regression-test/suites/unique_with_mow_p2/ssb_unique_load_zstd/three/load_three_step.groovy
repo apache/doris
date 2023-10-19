62f904f00eed30defa84b00acb4a35f763253fbc
Chen Zhang <chzhang1987@gmail.com>
2023-10-19 17:13:44 +0800

    [fix](regression-test) use correct dataset for unique_with_mow_p2

--- regression-test/suites/unique_with_mow_p2/ssb_unique_load_zstd/three/load_three_step.groovy
+++ regression-test/suites/unique_with_mow_p2/ssb_unique_load_zstd/three/load_three_step.groovy
@@ -40,7 +40,7 @@ suite("load_three_step") {
 
                 // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
                 // also, you can stream load a http stream, e.g. http://xxx/some.csv
-                file """${getS3Url()}/regression/ssb/sf0.1/${tableName}.tbl.gz"""
+                file """${getS3Url()}/regression/ssb/sf100/${tableName}.tbl.gz"""
 
                 time 10000 // limit inflight 10s
 
