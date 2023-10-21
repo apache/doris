f2f8a6afcae582fc0931efe9e5f61f271043b896
谢健 <jianxie0@gmail.com>
2023-10-22 00:45:52 +0800

    [fix](Nereids) fix row count unconsistent when join ordering (#24589) (#25666)
    In the context of reorder join, when a new plan is generated, it may include a project operation. In this case, the newly generated join root and the original join root will no longer be in the same group. To avoid inconsistencies in the statistics between these two groups, we keep the child group's row count unchanged when the parent group expression is a project operation.

--- regression-test/suites/nereids_tpcds_shape_sf100_p0/rf/ds_rf59.groovy
+++ regression-test/suites/nereids_tpcds_shape_sf100_p0/rf/ds_rf59.groovy
@@ -93,6 +93,5 @@ limit 100;
     // def outFile = "regression-test/suites/nereids_tpcds_shape_sf100_p0/ddl/rf/rf.59"
     // File file = new File(outFile)
     // file.write(getRuntimeFilters(plan))
-    
      assertEquals("RF0[d_date_sk->[ss_sold_date_sk],RF4[s_store_sk->[ss_store_sk],RF3[d_week_seq->[d_week_seq],RF2[s_store_sk->[ss_store_sk],RF1[d_week_seq->[d_week_seq]", getRuntimeFilters(plan))
 }
