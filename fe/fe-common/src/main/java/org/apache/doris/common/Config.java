ee3ba7114a92dd9b1f74e5a0a5486ab9de1772d3
guanzhaoyun <guanzhaoyun@bigo.sg>
2023-10-23 19:06:57 +0800

    [fix](meta) change config name  (#25764)
    Add timeout config for sync

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2261,11 +2261,11 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = true, masterOnly = true, description = {
         "FE扩容时，从主节点同步元数据的timeout时间，根据${meta_dir}/image文件夹下面的image文件大小和节点间的网络环境调整，" +
-            "单位为毫秒，默认值30000",
+            "单位为秒，默认值30",
         "The timeout for new FE Follower/Observer synchronizing metadata from the FE Master, " +
             "adjust by the size of image file in the ${meta_dir}/image and the network environment between nodes. " +
-            "The default values is 3000 ms."
+            "The default values is 30s."
     })
-    public static int sync_new_image_timeout = 30000;
+    public static int sync_new_image_timeout_s = 30000;
 
 }
