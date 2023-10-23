75000a5f6e572d92fb0301ffe93d6d053d607bef
Siyang Tang <82279870+TangSiyang2001@users.noreply.github.com>
2023-10-23 14:57:02 +0800

    [enhancement](stmt-exec) make CTAS use insert timeout fix forward timeout (#25731)

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
@@ -748,7 +748,7 @@ public class ConnectContext {
         } else {
             String timeoutTag = "query";
             // insert stmt particularly
-            if (executor != null && executor.isInsertStmt()) {
+            if (executor != null && executor.isSyncLoadKindStmt()) {
                 timeoutTag = "insert";
             }
             // to ms
@@ -802,7 +802,7 @@ public class ConnectContext {
      * @return exact execution timeout
      */
     public int getExecTimeout() {
-        if (executor != null && executor.isInsertStmt()) {
+        if (executor != null && executor.isSyncLoadKindStmt()) {
             // particular for insert stmt, we can expand other type of timeout in the same way
             return Math.max(sessionVariable.getInsertTimeoutS(), sessionVariable.getQueryTimeoutS());
         } else if (executor != null && executor.isAnalyzeStmt()) {
