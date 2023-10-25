c8a2d2af6c1ea5106d09eabaa913e80d02dccb0d
Mryange <59914473+Mryange@users.noreply.github.com>
2023-10-25 10:00:35 +0800

    [feature](AuditLog) add scanRows scanBytes in auditlog (#25435)

--- be/src/vec/exec/vexchange_node.cpp
+++ be/src/vec/exec/vexchange_node.cpp
@@ -150,7 +150,11 @@ Status VExchangeNode::collect_query_statistics(QueryStatistics* statistics) {
     statistics->merge(_sub_plan_query_statistics_recvr.get());
     return Status::OK();
 }
-
+Status VExchangeNode::collect_query_statistics(QueryStatistics* statistics, int sender_id) {
+    RETURN_IF_ERROR(ExecNode::collect_query_statistics(statistics));
+    statistics->merge(_sub_plan_query_statistics_recvr.get(), sender_id);
+    return Status::OK();
+}
 Status VExchangeNode::close(RuntimeState* state) {
     if (is_closed()) {
         return Status::OK();
