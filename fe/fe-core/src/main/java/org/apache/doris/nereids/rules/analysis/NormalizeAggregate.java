0f71ca4037923d846512a6fb9ec1f400b42dcbf6
morrySnow <101034200+morrySnow@users.noreply.github.com>
2023-10-27 16:42:00 +0800

    [fix](Nereids) could not run multi group_concat distinct (#25851)
    could not run multi group_concat distinct with more than one parameters.
    This bug is not just for group_concat, but we usually use literal as
    parameters in group_concat. So group_concat brought the problem to light.
    
    In the original logic, we think only distinct aggregate function with
    zero or one parameter could run in multi distinct mode. But it is wrong.
    We could process all distinct aggregate function with not more than one
    input slots.
    
    Think about sql:
    ```sql
    SELECT
      group_concat(distinct c1, ','), group_concat(distinct c2, ',')
    FROM t
    GROUP BY c3
    ```

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/NormalizeAggregate.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/NormalizeAggregate.java
@@ -30,6 +30,7 @@ import org.apache.doris.nereids.trees.expressions.SlotReference;
 import org.apache.doris.nereids.trees.expressions.SubqueryExpr;
 import org.apache.doris.nereids.trees.expressions.WindowExpression;
 import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateFunction;
+import org.apache.doris.nereids.trees.expressions.literal.Literal;
 import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionVisitor;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalAggregate;
@@ -150,7 +151,7 @@ public class NormalizeAggregate extends OneRewriteRuleFactory implements Normali
                 for (AggregateFunction distinctAggFunc : distinctAggFuncs) {
                     List<Expression> newChildren = Lists.newArrayList();
                     for (Expression child : distinctAggFunc.children()) {
-                        if (child instanceof SlotReference) {
+                        if (child instanceof SlotReference || child instanceof Literal) {
                             newChildren.add(child);
                         } else {
                             NamedExpression alias;
