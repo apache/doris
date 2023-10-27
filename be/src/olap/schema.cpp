605cea820f953137101cc1c6c9f6925178856ad0
amorynan <wangqiannan@selectdb.com>
2023-10-27 11:39:11 +0800

    fix upgrade for predict column delete collection type will make core

--- be/src/olap/schema.cpp
+++ be/src/olap/schema.cpp
@@ -203,7 +203,9 @@ vectorized::IColumn::MutablePtr Schema::get_predicate_column_ptr(const Field& fi
         ptr = doris::vectorized::PredicateColumnType<TYPE_DECIMAL256>::create();
         break;
     default:
-        LOG(FATAL) << "Unexpected type when choosing predicate column, type=" << int(field.type());
+        throw Exception(ErrorCode::SCHEMA_SCHEMA_FIELD_INVALID,
+                        fmt::format("Unexpected type when choosing predicate column, type={}",
+                                    int(field.type())));
     }
 
     if (field.is_nullable()) {
