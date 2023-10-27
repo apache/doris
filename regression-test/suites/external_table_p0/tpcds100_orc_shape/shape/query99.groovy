3beba1764c01b6712b108556433c96429c59cc45
Qi Chen <kaka11.chen@gmail.com>
2023-10-27 18:39:29 +0800

    [Test](multi-catalog) Add tpcds sf100 hive shape. (#25639)
    Add tpcds sf100 hive shapes.
    
    Disable query64 temporarily because it is not same with emr cluster after collecting metadata by analyze table xxx.
    And the root cause need to analyze, will enable in future PR.

---
+++ regression-test/suites/external_table_p0/tpcds100_orc_shape/shape/query99.groovy
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+suite("query99", "p0,external,hive,external_docker,external_docker_hive") {
+    String enabled = context.config.otherConfigs.get("enableHiveTest")
+    if (enabled != null && enabled.equalsIgnoreCase("true")) {
+        String hms_port = context.config.otherConfigs.get("hms_port")
+        String catalog_name = "test_catalog_tpcds100_orc"
+        String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
+
+        sql """create catalog if not exists ${catalog_name} properties (
+            "type"="hms",
+            'hive.metastore.uris' = 'thrift://${externalEnvIp}:${hms_port}'
+        );"""
+        sql """refresh catalog ${catalog_name}"""
+        sql """switch ${catalog_name}"""
+        sql """use `tpcds100_orc`"""
+        sql 'set enable_nereids_planner=true'
+        sql 'set enable_fallback_to_original_planner=false'
+        sql 'set exec_mem_limit=21G'
+        sql 'set be_number_for_test=3'
+        sql 'set parallel_fragment_exec_instance_num=8; '
+        sql 'set parallel_pipeline_task_num=8; '
+        sql 'set forbid_unknown_col_stats=true'
+        sql 'set broadcast_row_count_limit = 30000000'
+        sql 'set enable_nereids_timeout = false'
+
+        qt_ds_shape_99 '''
+    explain shape plan
+    select  
+   substr(w_warehouse_name,1,20)
+  ,sm_type
+  ,cc_name
+  ,sum(case when (cs_ship_date_sk - cs_sold_date_sk <= 30 ) then 1 else 0 end)  as "30 days" 
+  ,sum(case when (cs_ship_date_sk - cs_sold_date_sk > 30) and 
+                 (cs_ship_date_sk - cs_sold_date_sk <= 60) then 1 else 0 end )  as "31-60 days" 
+  ,sum(case when (cs_ship_date_sk - cs_sold_date_sk > 60) and 
+                 (cs_ship_date_sk - cs_sold_date_sk <= 90) then 1 else 0 end)  as "61-90 days" 
+  ,sum(case when (cs_ship_date_sk - cs_sold_date_sk > 90) and
+                 (cs_ship_date_sk - cs_sold_date_sk <= 120) then 1 else 0 end)  as "91-120 days" 
+  ,sum(case when (cs_ship_date_sk - cs_sold_date_sk  > 120) then 1 else 0 end)  as ">120 days" 
+from
+   catalog_sales
+  ,warehouse
+  ,ship_mode
+  ,call_center
+  ,date_dim
+where
+    d_month_seq between 1224 and 1224 + 11
+and cs_ship_date_sk   = d_date_sk
+and cs_warehouse_sk   = w_warehouse_sk
+and cs_ship_mode_sk   = sm_ship_mode_sk
+and cs_call_center_sk = cc_call_center_sk
+group by
+   substr(w_warehouse_name,1,20)
+  ,sm_type
+  ,cc_name
+order by substr(w_warehouse_name,1,20)
+        ,sm_type
+        ,cc_name
+limit 100;
+
+    '''
+    }
+}
