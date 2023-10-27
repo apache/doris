fe79b09f60ae9453f27a6069f1c6086554c1f419
zzzxl1993 <474696115@qq.com>
2023-10-27 12:03:42 +0800

    [fix](seek) add SeekCount and SeekTime profile

--- be/src/olap/rowset/segment_v2/segment_iterator.cpp
+++ be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -1648,8 +1648,11 @@ Status SegmentIterator::_read_columns_by_index(uint32_t nrows_read_limit, uint32
         }
         for (auto& range : _split_row_ranges) {
             size_t nrows = range.second - range.first;
-
-            RETURN_IF_ERROR(_column_iterators[cid]->seek_to_ordinal(range.first));
+            {
+                _opts.stats->block_first_read_seek_num += 1;
+                SCOPED_RAW_TIMER(&_opts.stats->block_first_read_seek_ns);
+                RETURN_IF_ERROR(_column_iterators[cid]->seek_to_ordinal(range.first));
+            }
             size_t rows_read = nrows;
             RETURN_IF_ERROR(_column_iterators[cid]->next_batch(&rows_read, column));
             if (rows_read != nrows) {
