6ba1aa1a49dba53024ee87da70a5c7b735961372
morningman <morningman@163.com>
2023-10-30 19:58:39 +0800

    [branch2021](meta) add OP_ALTER_CATALOG_COMMENT for compatibility

--- fe/fe-core/src/main/java/org/apache/doris/journal/JournalEntity.java
+++ fe/fe-core/src/main/java/org/apache/doris/journal/JournalEntity.java
@@ -844,9 +844,11 @@ public class JournalEntity implements Writable {
                 isRead = true;
                 break;
             }
+            case OperationType.OP_ALTER_CATALOG_COMMENT:
             case OperationType.OP_UPDATE_TABLE_STATS:
             case OperationType.OP_PERSIST_AUTO_JOB:
             case OperationType.OP_DELETE_TABLE_STATS: {
+                // For backward compatible with 2.0.3
                 isRead = true;
                 break;
             }
