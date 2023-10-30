07c541a75e040d6e937d6f9a318ebdb55cfdaf9f
Gabriel <gabrielleebuaa@gmail.com>
2023-10-30 18:39:22 +0800

    [date](parser) Support DateV1 keyword (#25414)

--- fe/fe-common/src/main/java/org/apache/doris/catalog/ScalarType.java
+++ fe/fe-common/src/main/java/org/apache/doris/catalog/ScalarType.java
@@ -440,6 +440,18 @@ public class ScalarType extends Type {
     }
 
     @SuppressWarnings("checkstyle:MissingJavadocMethod")
+    public static ScalarType createDatetimeV1Type() {
+        Preconditions.checkState(!Config.disable_datev1, "Datev1 is disable in fe.conf!");
+        return new ScalarType(PrimitiveType.DATETIME);
+    }
+
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
+    public static ScalarType createDateV1Type() {
+        Preconditions.checkState(!Config.disable_datev1, "Datev1 is disable in fe.conf!");
+        return new ScalarType(PrimitiveType.DATE);
+    }
+
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     public static ScalarType createTimeType() {
         if (!Config.enable_date_conversion) {
             return new ScalarType(PrimitiveType.TIME);
