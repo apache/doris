c437af8843d135f9972b2c19819c751903aa0841
HappenLee <happenlee@hotmail.com>
2023-10-31 11:03:35 +0800

    Join rewrite (#26140)
    Co-authored-by: BiteTheDDDDt <pxl290@qq.com>
    
    update rf
    
    update
    
    fix bug in shared hash table (#26158)
    
    support tpch q21 (#26248)
    
    fix rf and outer join performance (#26320)
    
    opt for rf
    
    fix tpch q13
    
    fix profile and change timer profile (#26468)
    
    opt for some join (#26513)
    
    opt for right anti (#26697)
    
    fix runtime filter performance (#26727)
    
    some fix (#26761)

--- be/src/vec/core/block.cpp
+++ be/src/vec/core/block.cpp
@@ -338,8 +338,9 @@ void Block::check_number_of_rows(bool allow_null_columns) const {
         }
 
         if (!elem.column) {
-            LOG(FATAL) << fmt::format(
-                    "Column {} in block is nullptr, in method check_number_of_rows.", elem.name);
+            throw Exception(ErrorCode::INTERNAL_ERROR,
+                            "Column {} in block is nullptr, in method check_number_of_rows.",
+                            elem.name);
         }
 
         ssize_t size = elem.column->size();
@@ -347,8 +348,8 @@ void Block::check_number_of_rows(bool allow_null_columns) const {
         if (rows == -1) {
             rows = size;
         } else if (rows != size) {
-            LOG(FATAL) << fmt::format("Sizes of columns doesn't match: {}:{},{}:{}, col size: {}",
-                                      data.front().name, rows, elem.name, size, each_col_size());
+            throw Exception(ErrorCode::INTERNAL_ERROR, "Sizes of columns doesn't match, block={}",
+                            dump_structure());
         }
     }
 }
@@ -1088,7 +1089,7 @@ std::unique_ptr<Block> Block::create_same_struct_block(size_t size, bool is_rese
         if (is_reserve) {
             column->reserve(size);
         } else {
-            column->resize(size);
+            column->insert_many_defaults(size);
         }
         temp_block->insert({std::move(column), d.type, d.name});
     }
