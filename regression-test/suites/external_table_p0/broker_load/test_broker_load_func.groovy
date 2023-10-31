af2c14a902496fca1ee12578849e30ef541345d3
zhangguoqiang <18372634969@163.com>
2023-10-31 10:38:40 +0800

    enhance case test_broker_load_func

--- regression-test/suites/external_table_p0/broker_load/test_broker_load_func.groovy
+++ regression-test/suites/external_table_p0/broker_load/test_broker_load_func.groovy
@@ -25,7 +25,8 @@ suite("test_broker_load_func", "p0,external,hive,external_docker,external_docker
 
             String database_name = "test_broker_load_func"
             String broker_name = "hdfs"
-            String test_load_label="label_test_broker_load_func"
+            def uuid = UUID.randomUUID().toString()
+            def test_load_label="label_test_broker_load_func_${uuid}"
             String table_name="simple"
 
             sql """drop database if exists ${database_name}; """
