f460efd97168e86d2db2f2c53b109868be0ad858
TengJianPing <18241664+jacktengg@users.noreply.github.com>
2023-10-31 16:35:26 +0800

    [fix](multicast) fix DCHECK failure of block mem reuse for multicast (#26127)

--- be/src/vec/exprs/vexpr_context.h
+++ be/src/vec/exprs/vexpr_context.h
@@ -90,7 +90,8 @@ public:
                                                      int column_to_keep, IColumn::Filter& filter);
 
     [[nodiscard]] static Status get_output_block_after_execute_exprs(const VExprContextSPtrs&,
-                                                                     const Block&, Block*);
+                                                                     const Block&, Block*,
+                                                                     bool do_projection = false);
 
     int get_last_result_column_id() const {
         DCHECK(_last_result_column_id != -1);
