ef0cc032366b78822f41307eea9306aac6a48afe
bobhan1 <bh2444151092@outlook.com>
2023-10-31 10:29:17 +0800

    tmp

--- regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_concurrency_schema_change.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_concurrency_schema_change.groovy
@@ -16,12 +16,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import java.util.concurrent.atomic.AtomicInteger
+
 suite("test_partial_update_concurrency_schema_change", "p0") {
 
     def tableName = "test_partial_update_concurrency_schema_change"
     sql """ DROP TABLE IF EXISTS ${tableName} """
     sql """ CREATE TABLE ${tableName} (
-        k1 int,
+        k1 varchar(20) not null,
         v1 varchar(20),
         v2 varchar(20),
         v3 varchar(20),
@@ -47,21 +49,45 @@ suite("test_partial_update_concurrency_schema_change", "p0") {
     }
     qt_sql """ select * from ${tableName} order by k1;"""
 
+    AtomicInteger v2_length = new AtomicInteger(20);
+
+    def wait_for_schema_change = {
+        def try_times=100
+        while(true){
+            def res = sql " SHOW ALTER TABLE COLUMN WHERE TableName = '${tableName}' ORDER BY CreateTime DESC LIMIT 1 "
+            Thread.sleep(10)
+            if(res[0][9].toString() == "FINISHED"){
+                break;
+            }
+            assert(try_times>0)
+            try_times--
+        }
+    }
+
     def threads = []
     def threads_num = 5
     def cnt = -1
     for (int i = 1; i <= threads_num; i++) {
-        def columns = "k1,v${i}"
-        logger.info("in ${i}: columns: ${columns}")
+        def columns = "k1,tmp,v${i}=substr(tmp,1,20)"
+        def idx = i
         threads.add(Thread.startDaemon {    
-            def num = "${cnt}"
+            def num = cnt
+            
             while (num < 0 || num-- > 0) {
+                logger.info("num: ${num}")
+                if (idx == 2) {
+                    def cur_length = v2_length.get()
+                    logger.info("idx=2, cur_length=${cur_length}")
+                    columns = "k1,tmp,v${idx}=substr(tmp,1,${cur_length})"
+                }
+                logger.info("in ${idx}: columns: ${columns}")
                 streamLoad {
                     table "${tableName}"
                     set 'column_separator', ','
                     set 'format', 'csv'
                     set 'partial_columns', 'true'
                     set 'columns', columns
+                    set 'strict_mode', "false"
                     file 'concurrency_update.csv'
                     time 10000 // limit inflight 10s
                 }
@@ -69,13 +95,22 @@ suite("test_partial_update_concurrency_schema_change", "p0") {
         })
     }
 
-    // def t1 = Thread.startDaemon {
-    //     for (int i = 1; i <= threads_num; i++) {
-    //         Thread.sleep(1000)
-    //         sql """ alter table ${tableName} modify column v${i} varchar(40);"""
-    //     }  
-    // }
-    // t1.join()
+    def t1 = Thread.startDaemon {
+        def num = cnt
+        def cur_length = v2_length.get()
+        while (cur_length < 100 && (num < 0 || num-- > 0)) {
+            // for (int i = 1; i <= threads_num; i++) {
+                sql """ alter table ${tableName} modify column v2 varchar(${cur_length+1});"""
+                wait_for_schema_change()
+                logger.info("column v2 change to varchar(${cur_length+1})");
+                v2_length.incrementAndGet()
+                cur_length = v2_length.get()
+                sleep(1000)
+            // }  
+            // cur_length++
+        }
+    }
+    t1.join()
 
     for (int i = 0; i < threads_num; i++) {
         threads[i].join()
