13b889753855890c165c67741888df78365efdc5
Gabriel <gabrielleebuaa@gmail.com>
2023-10-31 22:23:32 +0800

    [bug](scanner) Fix scanner core dump (#26156)

--- be/src/vec/exec/scan/pip_scanner_context.h
+++ be/src/vec/exec/scan/pip_scanner_context.h
@@ -76,15 +76,18 @@ public:
             if (_blocks_queues[id].empty()) {
                 *eos = _is_finished || _should_stop;
                 return Status::OK();
-            } else {
-                *block = std::move(_blocks_queues[id].front());
-                _blocks_queues[id].pop_front();
+            }
+            if (_process_status.is<ErrorCode::CANCELLED>()) {
+                *eos = true;
+                return Status::OK();
+            }
+            *block = std::move(_blocks_queues[id].front());
+            _blocks_queues[id].pop_front();
 
-                RETURN_IF_ERROR(validate_block_schema((*block).get()));
+            RETURN_IF_ERROR(validate_block_schema((*block).get()));
 
-                if (_blocks_queues[id].empty() && _data_dependency) {
-                    _data_dependency->block_reading();
-                }
+            if (_blocks_queues[id].empty() && _data_dependency) {
+                _data_dependency->block_reading();
             }
         }
         _current_used_bytes -= (*block)->allocated_bytes();
