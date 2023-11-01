a73b23d561f4a5e5d1497f04bb4f1d9065098afb
BiteTheDDDDt <pxl290@qq.com>
2023-11-01 14:22:15 +0800

    remove unused code about HashTableTraits

--- be/src/vec/common/hash_table/partitioned_hash_map.h
+++ be/src/vec/common/hash_table/partitioned_hash_map.h
@@ -56,17 +56,5 @@ using PartitionedHashMap =
         PartitionedHashMapTable<HashMap<Key, Mapped, Hash, PartitionedHashTableGrower<>>>;
 
 template <typename Key, typename Mapped, typename Hash = DefaultHash<Key>>
-using PHPartitionedHashMap = PartitionedHashMapTable<PHHashMap<Key, Mapped, Hash, true>>;
-
-template <typename Key, typename Mapped, typename Hash = DefaultHash<Key>>
 using PHNormalHashMap = PHHashMap<Key, Mapped, Hash, false>;
 
-template <typename Key, typename Mapped, typename Hash>
-struct HashTableTraits<PHPartitionedHashMap<Key, Mapped, Hash>> {
-    static constexpr bool is_phmap = true;
-};
-
-template <template <typename> class Derived, typename Key, typename Mapped, typename Hash>
-struct HashTableTraits<Derived<PHPartitionedHashMap<Key, Mapped, Hash>>> {
-    static constexpr bool is_phmap = true;
-};
