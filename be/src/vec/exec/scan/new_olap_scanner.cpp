64d33b8a15f4d62a590cc80cd86f28d910f9f1c3
lamb <lambxu@tencent.com>
2023-11-01 20:37:04 +0800

    fix conflict

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -181,7 +181,8 @@ Status NewOlapScanner::init() {
             ReadSource read_source;
             {
                 std::shared_lock rdlock(_tablet->get_header_lock());
-                auto st = _tablet->capture_rs_readers(rd_version, &read_source.rs_splits,
+                auto st = _tablet->capture_rs_readers(_tablet_reader_params.version,
+                                                      &read_source.rs_splits,
                                                       _state->skip_missing_version());
                 if (!st.ok()) {
                     LOG(WARNING) << "fail to init reader.res=" << st;
