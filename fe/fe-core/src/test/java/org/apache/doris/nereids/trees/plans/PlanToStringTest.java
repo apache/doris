2ddfb1f4dad6f502e1181dc6c74b88fb4bcd0989
Pxl <pxl290@qq.com>
2023-11-02 14:12:22 +0800

    [Bug](materialized-view) SelectMaterializedIndexWithAggregate do not … (#26192)
    SelectMaterializedIndexWithAggregate do not change plan when match base index

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/PlanToStringTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/PlanToStringTest.java
@@ -80,10 +80,8 @@ public class PlanToStringTest {
     @Test
     public void testLogicalOlapScan() {
         LogicalOlapScan plan = PlanConstructor.newLogicalOlapScan(0, "table", 0);
-        Assertions.assertTrue(
-                plan.toString().matches("LogicalOlapScan \\( qualified=db\\.table, "
-                        + "indexName=<index_not_selected>, "
-                        + "selectedIndexId=-1, preAgg=ON \\)"));
+        Assertions.assertTrue(plan.toString().matches("LogicalOlapScan \\( qualified=db\\.table, " + "indexName=table, "
+                + "selectedIndexId=-1, preAgg=ON \\)"), plan.toString());
     }
 
     @Test
