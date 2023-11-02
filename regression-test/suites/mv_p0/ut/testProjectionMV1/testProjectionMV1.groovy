bfdc00815479c42e6e3702b937b0b2f910593a1c
starocean999 <12095047@qq.com>
2023-11-02 18:53:31 +0800

    disable some case for nereids

--- regression-test/suites/mv_p0/ut/testProjectionMV1/testProjectionMV1.groovy
+++ regression-test/suites/mv_p0/ut/testProjectionMV1/testProjectionMV1.groovy
@@ -34,6 +34,7 @@ suite ("testProjectionMV1") {
     sql """insert into emps values("2020-01-01",1,"a",1,1,1);"""
     sql """insert into emps values("2020-01-02",2,"b",2,2,2);"""
 
+    sql """set enable_nereids_planner=false"""
     test {
         sql "create materialized view emps_mv as select deptno, empid from emps t order by deptno;"
         exception "errCode = 2,"
