fbf2611e2743627539d9ddf06eb19d7f79db1bf8
Yongqiang YANG <dataroaring@gmail.com>
2023-11-03 16:11:54 +0800

    fix

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -52,7 +52,7 @@ testSuites = ""
 testDirectories = ""
 
 // this groups will not be executed
-excludeGroups = "http_stream"
+excludeGroups = "p0/http_stream"
 // this suites will not be executed
 
 excludeSuites = "test_sql_block_rule,test_profile,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_jdbc_query_mysql"
