6ae87c1c21ec2bf3894b9050851b8ec0d53fa3f3
morrySnow <101034200+morrySnow@users.noreply.github.com>
2023-11-03 14:24:01 +0800

    [fix](Nereids) could not run query with repeat node in cte (#26330)
    ExpressionDeepCopier not process VirtualReference, so we generate inline
    plan with mistake.

--- regression-test/suites/nereids_syntax_p0/cte.groovy
+++ regression-test/suites/nereids_syntax_p0/cte.groovy
@@ -253,7 +253,7 @@ suite("cte") {
                 ORDER BY dd.s_suppkey;
     """
 
-    sql "set experimental_enable_pipeline_engine=true"
+    sql "set enable_pipeline_engine=true"
 
     qt_cte14 """
             SELECT abs(dd.s_suppkey)
@@ -305,6 +305,13 @@ suite("cte") {
         sql("WITH cte_0 AS ( SELECT 1 AS a ) SELECT * from cte_0 t1 join cte_0 t2 on true WHERE false;")
         notContains "MultiCastDataSinks"
     }
+
+    sql "WITH cte_0 AS ( SELECT 1 AS a ) SELECT * from cte_0 t1 LIMIT 10 UNION SELECT * from cte_0 t1 LIMIT 10"
+
+    qt_cte_with_repeat """
+        with cte_0 as (select lo_orderkey, lo_linenumber, grouping_id(lo_orderkey) as id from lineorder group by cube(lo_orderkey, lo_linenumber))
+        select * from cte_0 order by lo_orderkey, lo_linenumber, id
+    """
 
     qt_test """
         SELECT * FROM (
