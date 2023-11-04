434b2898498d87e52d0b3b97863db8f07f7f6452
stephen <hello-stephen@qq.com>
2023-11-05 00:33:05 +0800

    2

--- regression-test/pipeline/common/github-utils.sh
+++ regression-test/pipeline/common/github-utils.sh
@@ -8,12 +8,22 @@ function create_an_issue_comment() {
 
     local OWNER='apache'
     local REPO='doris'
-    if curl -s \
+    if ret=$(curl -s \
         -X POST \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_TOKEN:-}" \
         https://api.github.com/repos/"${OWNER}"/"${REPO}"/issues/"${ISSUE_NUMBER}"/comments \
-        -d "{\"body\": \"${COMMENT_BODY}\"}"; then
+        -d "{\"body\": \"${COMMENT_BODY}\"}"); then
+        if echo "${ret}" | grep "Problems parsing JSON"; then
+            is_succ=false
+        else
+            is_succ=true
+        fi
+    else
+        is_succ=false
+    fi
+
+    if ${is_succ}; then
         echo -e "\033[32m Create issue(${ISSUE_NUMBER}) comment SUCCESS... \033[0m" && return 0
     else
         echo -e "\033[31m Create issue(${ISSUE_NUMBER}) comment FAIL... \033[0m" && return 1
