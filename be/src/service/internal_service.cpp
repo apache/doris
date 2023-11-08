58bf79f79e6bccea1942d0234855f667fce8546e
Kaijie Chen <ckj@apache.org>
2023-11-08 16:16:33 +0800

    [fix](move-memtable) pass load stream num to backends (#26198)

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -401,7 +401,6 @@ void PInternalServiceImpl::open_load_stream(google::protobuf::RpcController* con
             return;
         }
 
-        load_stream->add_rpc_stream();
         VLOG_DEBUG << "get streamid =" << streamid;
         st.to_protobuf(response->mutable_status());
     });
