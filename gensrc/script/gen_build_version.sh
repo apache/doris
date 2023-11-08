a08ae813490a467553d2af8feb7907e9bc755acf
Kang <kxiao.tiger@gmail.com>
2023-11-08 12:59:35 +0800

    change version to 2.0.3-rc02

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -31,7 +31,7 @@ build_version_prefix="doris"
 build_version_major=2
 build_version_minor=0
 build_version_patch=3
-build_version_rc_version="rc01"
+build_version_rc_version="rc02"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}-${build_version_rc_version}"
 
