9968781f0d6b09754b37d8fd31ce1a99784a695b
kaka11chen <kaka11.chen@gmail.com>
2023-11-09 14:13:29 +0800

    [Opt](scanner-scheduler) Optimize `BlockingQueue`, `BlockingPriorityQueue` and change remote scan thread pool.

--- be/src/vec/exec/scan/scanner_scheduler.cpp
+++ be/src/vec/exec/scan/scanner_scheduler.cpp
@@ -97,7 +97,7 @@ void ScannerScheduler::stop() {
 
     _scheduler_pool->wait();
     _local_scan_thread_pool->join();
-    _remote_scan_thread_pool->wait();
+    _remote_scan_thread_pool->join();
     _limited_scan_thread_pool->wait();
     _group_local_scan_thread_pool->wait();
 
@@ -123,15 +123,9 @@ Status ScannerScheduler::init(ExecEnv* env) {
             config::doris_scanner_thread_pool_queue_size, "local_scan");
 
     // 3. remote scan thread pool
-    _remote_thread_pool_max_size = config::doris_max_remote_scanner_thread_pool_thread_num != -1
-                                           ? config::doris_max_remote_scanner_thread_pool_thread_num
-                                           : std::max(512, CpuInfo::num_cores() * 10);
-    static_cast<void>(
-            ThreadPoolBuilder("RemoteScanThreadPool")
-                    .set_min_threads(config::doris_scanner_thread_pool_thread_num) // 48 default
-                    .set_max_threads(_remote_thread_pool_max_size)
-                    .set_max_queue_size(config::doris_scanner_thread_pool_queue_size)
-                    .build(&_remote_scan_thread_pool));
+    _remote_scan_thread_pool = std::make_unique<PriorityThreadPool>(
+            config::doris_remote_scanner_thread_pool_thread_num,
+            config::doris_remote_scanner_thread_pool_queue_size, "RemoteScanThreadPool");
 
     // 4. limited scan thread pool
     static_cast<void>(ThreadPoolBuilder("LimitedScanThreadPool")
@@ -269,9 +263,12 @@ void ScannerScheduler::_schedule_scanners(ScannerContext* ctx) {
                         ret = _local_scan_thread_pool->offer(task);
                     }
                 } else {
-                    ret = _remote_scan_thread_pool->submit_func([this, scanner = *iter, ctx] {
+                    PriorityThreadPool::Task task;
+                    task.work_function = [this, scanner = *iter, ctx] {
                         this->_scanner_scan(this, ctx, scanner);
-                    });
+                    };
+                    task.priority = nice;
+                    ret = _remote_scan_thread_pool->offer(task);
                 }
                 if (ret) {
                     this_run.erase(iter++);
@@ -482,7 +479,7 @@ void ScannerScheduler::_register_metrics() {
     REGISTER_HOOK_METRIC(remote_scan_thread_pool_queue_size,
                          [this]() { return _remote_scan_thread_pool->get_queue_size(); });
     REGISTER_HOOK_METRIC(remote_scan_thread_pool_thread_num,
-                         [this]() { return _remote_scan_thread_pool->num_threads(); });
+                         [this]() { return _remote_scan_thread_pool->get_active_threads(); });
     REGISTER_HOOK_METRIC(limited_scan_thread_pool_queue_size,
                          [this]() { return _limited_scan_thread_pool->get_queue_size(); });
     REGISTER_HOOK_METRIC(limited_scan_thread_pool_thread_num,
@@ -503,5 +500,4 @@ void ScannerScheduler::_deregister_metrics() {
     DEREGISTER_HOOK_METRIC(group_local_scan_thread_pool_queue_size);
     DEREGISTER_HOOK_METRIC(group_local_scan_thread_pool_thread_num);
 }
-
 } // namespace doris::vectorized
