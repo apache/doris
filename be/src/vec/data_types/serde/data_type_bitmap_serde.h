4b2a9001e57e98ebe7dce3577ab31289c5b0c86e
GoGoWen2021 <GoGoWen2021@gmail.com>
2023-11-09 18:24:03 +0800

    support read bitmap column with connector

--- be/src/vec/data_types/serde/data_type_bitmap_serde.h
+++ be/src/vec/data_types/serde/data_type_bitmap_serde.h
@@ -63,10 +63,7 @@ public:
 
     void write_column_to_arrow(const IColumn& column, const NullMap* null_map,
                                arrow::ArrayBuilder* array_builder, int start,
-                               int end) const override {
-        throw doris::Exception(ErrorCode::NOT_IMPLEMENTED_ERROR,
-                               "write_column_to_arrow with type " + column.get_name());
-    }
+                               int end) const override;
 
     void read_column_from_arrow(IColumn& column, const arrow::Array* arrow_array, int start,
                                 int end, const cctz::time_zone& ctz) const override {
