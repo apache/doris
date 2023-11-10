8321190a896b0c554974c3245b3438c4b0e3565b
李旭 <xulovemin1413@163.com>
2023-11-10 10:12:45 +0800

    Catalog suport gaussdb

--- fe/fe-core/src/main/java/org/apache/doris/catalog/JdbcResource.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/JdbcResource.java
@@ -65,6 +65,7 @@ public class JdbcResource extends Resource {
     public static final String JDBC_MYSQL = "jdbc:mysql";
     public static final String JDBC_MARIADB = "jdbc:mariadb";
     public static final String JDBC_POSTGRESQL = "jdbc:postgresql";
+    public static final String JDBC_GAUSSDB = "jdbc:gaussdb";
     public static final String JDBC_ORACLE = "jdbc:oracle";
     public static final String JDBC_SQLSERVER = "jdbc:sqlserver";
     public static final String JDBC_CLICKHOUSE = "jdbc:clickhouse";
@@ -268,7 +269,7 @@ public class JdbcResource extends Resource {
     public static String parseDbType(String url) throws DdlException {
         if (url.startsWith(JDBC_MYSQL) || url.startsWith(JDBC_MARIADB)) {
             return MYSQL;
-        } else if (url.startsWith(JDBC_POSTGRESQL)) {
+        } else if (url.startsWith(JDBC_POSTGRESQL) || url.startsWith(JDBC_GAUSSDB)) {
             return POSTGRESQL;
         } else if (url.startsWith(JDBC_ORACLE)) {
             return ORACLE;
