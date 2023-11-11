6c34f5f920d2f9e68bd3db1dfa714ca5ee63add0
ryanzryu <ryanzryu@tencent.com>
2023-11-11 13:20:29 +0800

    delete unrelated config item group_commit_max_queue_size

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1148,9 +1148,6 @@ DECLARE_Bool(ignore_always_true_predicate_for_segment);
 // Dir of default timezone files
 DECLARE_String(default_tzfiles_path);
 
-// Max size(bytes) of group commit queues, used for mem back pressure.
-DECLARE_Int32(group_commit_max_queue_size);
-
 // the max package bytes be thrift server can receive
 // avoid accepting error or too large package causing OOM,default 20000000(20M)
 DECLARE_Int32(be_thrift_max_pkg_bytes);
