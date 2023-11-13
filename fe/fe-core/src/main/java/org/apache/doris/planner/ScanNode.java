44480b7835968a716a6c05313faa69af164186b2
starocean999 <12095047@qq.com>
2023-11-13 17:14:08 +0800

    [fix](planner)scan node should project all required expr from parent node

--- fe/fe-core/src/main/java/org/apache/doris/planner/ScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/ScanNode.java
@@ -54,6 +54,7 @@ import org.apache.doris.thrift.TScanRangeLocation;
 import org.apache.doris.thrift.TScanRangeLocations;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Range;
@@ -64,6 +65,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -611,6 +613,47 @@ public abstract class ScanNode extends PlanNode {
                 }
             }
             outputSmap.updateRhsExprs(newRhs);
+        }
+    }
+
+    @Override
+    public void initOutputSlotIds(Set<SlotId> requiredSlotIdSet, Analyzer analyzer) {
+        if (outputTupleDesc != null) {
+            Preconditions.checkNotNull(outputSmap);
+            ArrayList<SlotId> materializedSlotIds = outputTupleDesc.getMaterializedSlotIds();
+            Preconditions.checkState(projectList != null && projectList.size() == materializedSlotIds.size(),
+                    "projectList's size should be same as materializedSlotIds's size");
+
+            // create a mapping between materialized slot's id and project expr
+            Map<SlotId, Expr> slotToExpr = new HashMap<>();
+            for (int i = 0; i < materializedSlotIds.size(); i++) {
+                slotToExpr.put(materializedSlotIds.get(i), projectList.get(i));
+            }
+
+            // find new project expr from outputSmap based on requiredSlotIdSet
+            ArrayList<SlotId> allSlots = outputTupleDesc.getAllSlotIds();
+            boolean hasNewSlot = false;
+            for (SlotId slotId : requiredSlotIdSet) {
+                if (!materializedSlotIds.contains(slotId) && allSlots.contains(slotId)) {
+                    SlotDescriptor slot = outputTupleDesc.getSlot(slotId.asInt());
+                    for (Expr expr : outputSmap.getRhs()) {
+                        if (expr instanceof SlotRef && ((SlotRef) expr).getSlotId() == slotId) {
+                            slot.setIsMaterialized(true);
+                            slotToExpr.put(slotId, expr.getSrcSlotRef());
+                            hasNewSlot = true;
+                        }
+                    }
+                }
+            }
+
+            if (hasNewSlot) {
+                // recreate the project list
+                projectList.clear();
+                materializedSlotIds = outputTupleDesc.getMaterializedSlotIds();
+                for (SlotId slotId : materializedSlotIds) {
+                    projectList.add(slotToExpr.get(slotId));
+                }
+            }
         }
     }
 
