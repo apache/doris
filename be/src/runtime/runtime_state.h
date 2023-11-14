c579341cca0f665403c414e80557ba965333976d
zhiqiang <seuhezhiqiang@163.com>
2023-11-14 07:05:59 -0600

    [feature](runtime filter) New session variable runtime_filter_wait_infinitely (#26888)
    New session variable: runtime_filter_wait_infinitely. If set runtime_filter_wait_infinitely = true, consumer of rf will wait on receiving until query is timeout.

--- be/src/runtime/runtime_state.h
+++ be/src/runtime/runtime_state.h
@@ -333,6 +333,11 @@ public:
         return _query_options.runtime_filter_wait_time_ms;
     }
 
+    bool runtime_filter_wait_infinitely() const {
+        return _query_options.__isset.runtime_filter_wait_infinitely &&
+               _query_options.runtime_filter_wait_infinitely;
+    }
+
     int32_t runtime_filter_max_in_num() const { return _query_options.runtime_filter_max_in_num; }
 
     int be_exec_version() const {
