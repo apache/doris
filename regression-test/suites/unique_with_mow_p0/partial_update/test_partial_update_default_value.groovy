05cfa1286554b1baad09dcd3d386248309a7152b
bobhan1 <bh2444151092@outlook.com>
2023-11-14 11:10:46 +0800

    update

--- regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_default_value.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_default_value.groovy
@@ -17,48 +17,70 @@
 // under the License.
 
 suite("test_primary_key_partial_update_default_value", "p0") {
-    def tableName = "test_primary_key_partial_update_default_value"
 
-    // create table
-    sql """ DROP TABLE IF EXISTS ${tableName} """
-    sql """
-            CREATE TABLE ${tableName} (
-                `id` int(11) NOT NULL COMMENT "用户 ID",
-                `name` varchar(65533) NOT NULL DEFAULT "yixiu" COMMENT "用户姓名",
-                `score` int(11) NOT NULL COMMENT "用户得分",
-                `test` int(11) NULL DEFAULT "4321" COMMENT  "test",
-                `dft` int(11) DEFAULT "4321")
-                UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
-                PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true")
-    """
-    // insert 2 lines
-    sql """
-        insert into ${tableName} values(2, "doris2", 2000, 223, 1)
-    """
-
-    sql """
-        insert into ${tableName} values(1, "doris", 1000, 123, 1)
-    """
-
-    // stream load with key not exit before
-    streamLoad {
-        table "${tableName}"
-
-        set 'column_separator', ','
-        set 'format', 'csv'
-        set 'partial_columns', 'true'
-        set 'columns', 'id,score'
-
-        file 'default.csv'
-        time 10000 // limit inflight 10s
+    def db = "regression_test_unique_with_mow_p0_partial_update"
+    def genCreateTableStmt = { str, flag -> 
+        String ret = str
+        if (flag) {
+            ret += """ ,"store_row_column" = "true"); """
+        } else {
+            ret += ");"
+        }
+        return ret
     }
 
-    sql "sync"
+    sql "select 1;" // to create database
 
-    qt_select_default """
-        select * from ${tableName} order by id;
-    """
+    for (def use_row_store : [false, true]) {
+        logger.info("current params: use_row_store: ${use_row_store}")
 
-    // drop drop
-    sql """ DROP TABLE IF EXISTS ${tableName} """
+        connect(user = context.config.jdbcUser, password = context.config.jdbcPassword, url = context.config.jdbcUrl) {
+            sql "use ${db};"
+
+            def tableName = "test_primary_key_partial_update_default_value"
+
+            // create table
+            sql """ DROP TABLE IF EXISTS ${tableName} """
+            def createTableStmt = """
+                    CREATE TABLE ${tableName} (
+                        `id` int(11) NOT NULL COMMENT "用户 ID",
+                        `name` varchar(65533) NOT NULL DEFAULT "yixiu" COMMENT "用户姓名",
+                        `score` int(11) NOT NULL COMMENT "用户得分",
+                        `test` int(11) NULL DEFAULT "4321" COMMENT  "test",
+                        `dft` int(11) DEFAULT "4321")
+                        UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+                        PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true" """
+            sql genCreateTableStmt(createTableStmt, use_row_store)
+            // insert 2 lines
+            sql """
+                insert into ${tableName} values(2, "doris2", 2000, 223, 1)
+            """
+
+            sql """
+                insert into ${tableName} values(1, "doris", 1000, 123, 1)
+            """
+
+            // stream load with key not exit before
+            streamLoad {
+                table "${tableName}"
+
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'partial_columns', 'true'
+                set 'columns', 'id,score'
+
+                file 'default.csv'
+                time 10000 // limit inflight 10s
+            }
+
+            sql "sync"
+
+            qt_select_default """
+                select * from ${tableName} order by id;
+            """
+
+            // drop drop
+            sql """ DROP TABLE IF EXISTS ${tableName} """
+        }
+    }
 }
