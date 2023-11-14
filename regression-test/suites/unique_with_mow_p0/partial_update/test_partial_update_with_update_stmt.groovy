fa01b90b617828f6cca123a7dafdb549233fdcc6
bobhan1 <bh2444151092@outlook.com>
2023-11-14 10:50:25 +0800

    tmp

--- regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_with_update_stmt.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_with_update_stmt.groovy
@@ -17,120 +17,140 @@
 // under the License.
 
 suite("test_primary_key_partial_update_with_update_stmt", "p0") {
-    def tableName = "test_primary_key_partial_update_with_update_stmt"
-    def tableNameJoinA = "test_primary_key_partial_update_with_update_stmt_join_a"
-    def tableNameJoinB = "test_primary_key_partial_update_with_update_stmt_join_b"
 
-    // create table
-    sql """ DROP TABLE IF EXISTS ${tableName} """
-    sql """ DROP TABLE IF EXISTS ${tableNameJoinA} """
-    sql """ DROP TABLE IF EXISTS ${tableNameJoinB} """
-    sql """
-            CREATE TABLE ${tableName} (
-                `id` int(11) NOT NULL COMMENT "用户 ID",
-                `name` varchar(65533) NOT NULL COMMENT "用户姓名",
-                `score` int(11) NOT NULL COMMENT "用户得分",
-                `test` int(11) NULL COMMENT "null test",
-                `dft` int(11) DEFAULT "4321")
-                UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
-                PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true")
-    """
-    // insert 2 lines
-    sql """
-        insert into ${tableName} values(2, "doris2", 2000, 223, 1)
-    """
+    def db = "regression_test_unique_with_mow_p0_partial_update"
+    def genCreateTableStmt = { str, flag -> 
+        String ret = str
+        if (flag) {
+            ret += """ ,"store_row_column" = "true"); """
+        } else {
+            ret += ");"
+        }
+        return ret
+    }
 
-    sql """
-        insert into ${tableName} values(1, "doris", 1000, 123, 1)
-    """
+    sql "select 1;" // to create database
 
-    // case 1: partially update normally
-    sql """
-        update ${tableName} set score = 4000 where id = 1
-    """
+    for (def use_row_store : [false, true]) {
+        logger.info("current params: use_row_store: ${use_row_store}")
 
-    sql "sync"
+        connect(user = context.config.jdbcUser, password = context.config.jdbcPassword, url = context.config.jdbcUrl) {
+            sql "use ${db};"
 
-    qt_select_default """
-        select * from ${tableName} order by id;
-    """
+            def tableName = "test_primary_key_partial_update_with_update_stmt"
+            def tableNameJoinA = "test_primary_key_partial_update_with_update_stmt_join_a"
+            def tableNameJoinB = "test_primary_key_partial_update_with_update_stmt_join_b"
 
-    // case 2: partially update non-exist key
-    def result1 = sql """
-        update ${tableName} set score = 2000 where id = 3
-    """
-    assertTrue(result1.size() == 1)
-    assertTrue(result1[0].size() == 1)
-    assertTrue(result1[0][0] == 0, "Query OK, 0 rows affected")
+            // create table
+            sql """ DROP TABLE IF EXISTS ${tableName} """
+            sql """ DROP TABLE IF EXISTS ${tableNameJoinA} """
+            sql """ DROP TABLE IF EXISTS ${tableNameJoinB} """
+            def createTableStmt = """
+                    CREATE TABLE ${tableName} (
+                        `id` int(11) NOT NULL COMMENT "用户 ID",
+                        `name` varchar(65533) NOT NULL COMMENT "用户姓名",
+                        `score` int(11) NOT NULL COMMENT "用户得分",
+                        `test` int(11) NULL COMMENT "null test",
+                        `dft` int(11) DEFAULT "4321")
+                        UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+                        PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true" """
+            sql genCreateTableStmt(createTableStmt, use_row_store)
+            // insert 2 lines
+            sql """
+                insert into ${tableName} values(2, "doris2", 2000, 223, 1)
+            """
 
-    sql "sync"
+            sql """
+                insert into ${tableName} values(1, "doris", 1000, 123, 1)
+            """
 
-    // create two table for join
-    sql """
-            CREATE TABLE ${tableNameJoinA} (
-                `id` int(11) NOT NULL COMMENT "用户 ID",
-                `name` varchar(65533) NOT NULL COMMENT "用户姓名")
-                UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
-                PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true")
-    """
+            // case 1: partially update normally
+            sql """
+                update ${tableName} set score = 4000 where id = 1
+            """
 
-    sql """
-            CREATE TABLE ${tableNameJoinB} (
-                `id` int(11) NOT NULL COMMENT "用户 ID",
-                `score` int(11) NOT NULL COMMENT "用户得分")
-                UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
-                PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true")
-    """
+            sql "sync"
 
-    // case 3: non-exsit key with join
-    sql """
-        insert into ${tableNameJoinA} values(4, "doris6")
-    """
-    sql """
-        insert into ${tableNameJoinB} values(4, 4000)
-    """
-    def result2 = sql """
-        update ${tableName} set ${tableName}.score = ${tableNameJoinB}.score, ${tableName}.name = ${tableNameJoinA}.name
-        from ${tableNameJoinA} inner join ${tableNameJoinB} on ${tableNameJoinA}.id = ${tableNameJoinB}.id
-        where ${tableName}.id = ${tableNameJoinA}.id
-    """
-    assertTrue(result2.size() == 1)
-    assertTrue(result2[0].size() == 1)
-    assertTrue(result2[0][0] == 0, "Query OK, 0 rows affected")
+            qt_select_default """
+                select * from ${tableName} order by id;
+            """
 
-    sql "sync"
+            // case 2: partially update non-exist key
+            def result1 = sql """
+                update ${tableName} set score = 2000 where id = 3
+            """
+            assertTrue(result1.size() == 1)
+            assertTrue(result1[0].size() == 1)
+            assertTrue(result1[0][0] == 0, "Query OK, 0 rows affected")
 
-    // case 4: partially update normally with join
-    sql """
-        insert into ${tableNameJoinA} values(2, "doris4")
-    """
+            sql "sync"
 
-    sql """
-        insert into ${tableNameJoinA} values(1, "doris3")
-    """
+            // create two table for join
+            createTableStmt = """CREATE TABLE ${tableNameJoinA} (
+                        `id` int(11) NOT NULL COMMENT "用户 ID",
+                        `name` varchar(65533) NOT NULL COMMENT "用户姓名")
+                        UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+                        PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true" """
+            sql genCreateTableStmt(createTableStmt, use_row_store)
+ 
+            createTableStmt = """CREATE TABLE ${tableNameJoinB} (
+                        `id` int(11) NOT NULL COMMENT "用户 ID",
+                        `score` int(11) NOT NULL COMMENT "用户得分")
+                        UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+                        PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true" """
+            sql genCreateTableStmt(createTableStmt, use_row_store)
 
-    sql """
-        insert into ${tableNameJoinB} values(2, 8000)
-    """
+            // case 3: non-exsit key with join
+            sql """
+                insert into ${tableNameJoinA} values(4, "doris6")
+            """
+            sql """
+                insert into ${tableNameJoinB} values(4, 4000)
+            """
+            def result2 = sql """
+                update ${tableName} set ${tableName}.score = ${tableNameJoinB}.score, ${tableName}.name = ${tableNameJoinA}.name
+                from ${tableNameJoinA} inner join ${tableNameJoinB} on ${tableNameJoinA}.id = ${tableNameJoinB}.id
+                where ${tableName}.id = ${tableNameJoinA}.id
+            """
+            assertTrue(result2.size() == 1)
+            assertTrue(result2[0].size() == 1)
+            assertTrue(result2[0][0] == 0, "Query OK, 0 rows affected")
 
-    sql """
-        insert into ${tableNameJoinB} values(3, 7000)
-    """
+            sql "sync"
 
-    sql """
-        update ${tableName} set ${tableName}.score = ${tableNameJoinB}.score, ${tableName}.name = ${tableNameJoinA}.name
-        from ${tableNameJoinA} inner join ${tableNameJoinB} on ${tableNameJoinA}.id = ${tableNameJoinB}.id
-        where ${tableName}.id = ${tableNameJoinA}.id
-    """
+            // case 4: partially update normally with join
+            sql """
+                insert into ${tableNameJoinA} values(2, "doris4")
+            """
 
-    sql "sync"
+            sql """
+                insert into ${tableNameJoinA} values(1, "doris3")
+            """
 
-    qt_select_join """
-        select * from ${tableName} order by id;
-    """
+            sql """
+                insert into ${tableNameJoinB} values(2, 8000)
+            """
+
+            sql """
+                insert into ${tableNameJoinB} values(3, 7000)
+            """
+
+            sql """
+                update ${tableName} set ${tableName}.score = ${tableNameJoinB}.score, ${tableName}.name = ${tableNameJoinA}.name
+                from ${tableNameJoinA} inner join ${tableNameJoinB} on ${tableNameJoinA}.id = ${tableNameJoinB}.id
+                where ${tableName}.id = ${tableNameJoinA}.id
+            """
+
+            sql "sync"
+
+            qt_select_join """
+                select * from ${tableName} order by id;
+            """
 
 
-    sql """ DROP TABLE IF EXISTS ${tableName} """
-    sql """ DROP TABLE IF EXISTS ${tableNameJoinA} """
-    sql """ DROP TABLE IF EXISTS ${tableNameJoinB} """
+            sql """ DROP TABLE IF EXISTS ${tableName} """
+            sql """ DROP TABLE IF EXISTS ${tableNameJoinA} """
+            sql """ DROP TABLE IF EXISTS ${tableNameJoinB} """
+        }
+    }
 }
