a4457578d4e76202317ab4bbc1c26db4de784d27
liugddx <liugddx@gmail.com>
2023-11-14 16:41:36 +0800

    1

--- regression-test/suites/load_p0/stream_load/test_stream_load.groovy
+++ regression-test/suites/load_p0/stream_load/test_stream_load.groovy
@@ -1316,7 +1316,7 @@ suite("test_stream_load", "p0") {
             }
             log.info("Stream load result: ${result}".toString())
             def json = parseJson(result)
-            assertEquals("[INVALID_ARGUMENT]send_batch_parallelism must be an integer, stoi", json.Message)
+            assertEquals("success", json.Status.toLowerCase())
         }
     }
 
