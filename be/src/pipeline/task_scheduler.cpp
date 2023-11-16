72b74090d34cb2bad0023a74836795c5b46aca52
HappenLee <happenlee@hotmail.com>
2023-11-16 19:47:58 +0800

    [Bug](pipeline) try fix the exchange sink buffer result error (#27087)

--- be/src/pipeline/task_scheduler.cpp
+++ be/src/pipeline/task_scheduler.cpp
@@ -325,24 +325,28 @@ void TaskScheduler::_try_close_task(PipelineTask* task, PipelineTaskState state)
         return;
     }
     auto status = task->try_close();
-    if (!status.ok() && state != PipelineTaskState::CANCELED) {
-        // Call `close` if `try_close` failed to make sure allocated resources are released
-        task->close();
+    auto cancel = [&]() {
         task->fragment_context()->cancel(PPlanFragmentCancelReason::INTERNAL_ERROR,
                                          status.to_string());
         state = PipelineTaskState::CANCELED;
-    } else if (task->is_pending_finish()) {
-        task->set_state(PipelineTaskState::PENDING_FINISH);
-        _blocked_task_scheduler->add_blocked_task(task);
-        return;
-    } else {
+    };
+
+    auto try_close_failed = !status.ok() && state != PipelineTaskState::CANCELED;
+    if (try_close_failed) {
+        cancel();
+        // Call `close` if `try_close` failed to make sure allocated resources are released
+        static_cast<void>(task->close());
+    } else if (!task->is_pending_finish()) {
         status = task->close();
         if (!status.ok() && state != PipelineTaskState::CANCELED) {
-            task->fragment_context()->cancel(PPlanFragmentCancelReason::INTERNAL_ERROR,
-                                             status.to_string());
-            state = PipelineTaskState::CANCELED;
+            cancel();
         }
-        DCHECK(!task->is_pending_finish()) << task->debug_string();
+    }
+
+    if (task->is_pending_finish()) {
+        task->set_state(PipelineTaskState::PENDING_FINISH);
+        static_cast<void>(_blocked_task_scheduler->add_blocked_task(task));
+        return;
     }
     task->set_state(state);
     task->set_close_pipeline_time();
