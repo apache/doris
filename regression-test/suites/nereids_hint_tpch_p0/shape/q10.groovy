c500eaadf1ac46050667583455383d96b21d6fc2
englefly <minghong.zhou@163.com>
2023-11-17 19:26:18 +0800

    fix

--- regression-test/suites/nereids_hint_tpch_p0/shape/q10.groovy
+++ regression-test/suites/nereids_hint_tpch_p0/shape/q10.groovy
@@ -31,7 +31,7 @@ suite("q10") {
 
     
 sql 'set be_number_for_test=3'|
-sql "set enable_runtime_filter_prune=false'
+sql "set enable_runtime_filter_prune=false"
 
     qt_select """
     explain shape plan
