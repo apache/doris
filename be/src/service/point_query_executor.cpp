dabb4ae0a16cec445922f8963bcbcaea4f0aab9b
lihangyu <15605149486@163.com>
2023-11-17 10:18:21 +0800

    [Optimize](point query) clear names to reduce mem consumption and cpu cost related to block column name (#26931)

--- be/src/service/point_query_executor.cpp
+++ be/src/service/point_query_executor.cpp
@@ -48,7 +48,7 @@
 namespace doris {
 
 Reusable::~Reusable() {}
-constexpr static int s_preallocted_blocks_num = 64;
+constexpr static int s_preallocted_blocks_num = 32;
 Status Reusable::init(const TDescriptorTable& t_desc_tbl, const std::vector<TExpr>& output_exprs,
                       size_t block_size) {
     SCOPED_MEM_COUNT_BY_HOOK(&_mem_size);
@@ -334,6 +334,7 @@ Status PointQueryExecutor::_lookup_row_data() {
 template <typename MysqlWriter>
 Status _serialize_block(MysqlWriter& mysql_writer, vectorized::Block& block,
                         PTabletKeyLookupResponse* response) {
+    block.clear_names();
     RETURN_IF_ERROR(mysql_writer.append_block(block));
     assert(mysql_writer.results().size() == 1);
     uint8_t* buf = nullptr;
