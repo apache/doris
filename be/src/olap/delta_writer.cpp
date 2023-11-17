e73df36c30acc343b487a1aa0b21f342dc6efd57
Kaijie Chen <ckj@apache.org>
2023-11-17 11:27:31 +0800

    fixup

--- be/src/olap/delta_writer.cpp
+++ be/src/olap/delta_writer.cpp
@@ -113,14 +113,14 @@ Status DeltaWriter::write(const vectorized::Block* block, const std::vector<int>
     if (UNLIKELY(row_idxs.empty() && !is_append)) {
         return Status::OK();
     }
-    while (_memtable_writer->get_flush_token_stats().flush_running_count > 1) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
     _lock_watch.start();
     std::lock_guard<std::mutex> l(_lock);
     _lock_watch.stop();
     if (!_is_init && !_is_cancelled) {
         RETURN_IF_ERROR(init());
+    }
+    while (_memtable_writer->get_flush_token_stats().flush_running_count > 1) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     return _memtable_writer->write(block, row_idxs, is_append);
 }
--- be/src/olap/delta_writer_v2.cpp
+++ be/src/olap/delta_writer_v2.cpp
@@ -146,14 +146,14 @@ Status DeltaWriterV2::write(const vectorized::Block* block, const std::vector<in
     if (UNLIKELY(row_idxs.empty() && !is_append)) {
         return Status::OK();
     }
-    while (_memtable_writer->get_flush_token_stats().flush_running_count > 1) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
     _lock_watch.start();
     std::lock_guard<std::mutex> l(_lock);
     _lock_watch.stop();
     if (!_is_init && !_is_cancelled) {
         RETURN_IF_ERROR(init());
+    }
+    while (_memtable_writer->get_flush_token_stats().flush_running_count > 1) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     SCOPED_RAW_TIMER(&_write_memtable_time);
     return _memtable_writer->write(block, row_idxs, is_append);
