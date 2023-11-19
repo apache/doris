ef9438fa719a88d7bf046639d878c43d32bc7e3e
wuwenchi <wuwenchihdu@hotmail.com>
2023-11-19 10:49:49 +0800

    add arrow data type for stream load

---
+++ be/src/vec/exec/format/arrow/arrow_pip_input_stream.h
@@ -0,0 +1,77 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <gen_cpp/PlanNodes_types.h>
+#include <gen_cpp/internal_service.pb.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "arrow/buffer.h"
+#include "arrow/io/interfaces.h"
+#include "arrow/status.h"
+#include "common/status.h"
+#include "io/file_factory.h"
+#include "io/fs/file_reader_writer_fwd.h"
+#include "util/slice.h"
+#include "vec/data_types/data_type.h"
+#include "vec/exec/format/file_reader/new_plain_text_line_reader.h"
+#include "vec/exec/format/generic_reader.h"
+
+namespace doris {
+
+namespace io {
+class FileSystem;
+struct IOContext;
+} // namespace io
+
+namespace vectorized {
+
+class ARROW_EXPORT ArrowPipInputStream : public arrow::io::InputStream {
+    ENABLE_FACTORY_CREATOR(ArrowPipInputStream);
+
+public:
+    ArrowPipInputStream(io::FileReaderSPtr file_reader);
+    ~ArrowPipInputStream() override {}
+
+    arrow::Status Close() override;
+    bool closed() const override;
+
+    arrow::Result<int64_t> Tell() const override;
+
+    arrow::Result<int64_t> Read(int64_t nbytes, void* out) override;
+
+    arrow::Result<std::shared_ptr<arrow::Buffer>> Read(int64_t nbytes) override;
+
+    Status HasNext(bool* get);
+
+private:
+    io::FileReaderSPtr _file_reader;
+    int64_t _pos;
+    bool _begin;
+    uint8_t* _read_buf;
+};
+
+} // namespace vectorized
+} // namespace doris
