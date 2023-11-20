5ae4f6efb30931bc1f2cbc5e9ffde5e96c47e8a1
Lei Zhang <27994433+SWJTU-ZhangLei@users.noreply.github.com>
2023-11-20 20:57:23 +0800

    [refact](bdbje) Refact `BDBEnvironment` and `BDBJEJournal`
    * Add more ut about "org.apache.doris.journal.bdbje"
    * Make tiny refactor about "org.apache.doris.journal.bdbje"

--- fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
+++ fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -18,6 +18,7 @@
 package org.apache.doris.journal.bdbje;
 
 import org.apache.doris.catalog.Env;
+import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.io.DataOutputBuffer;
 import org.apache.doris.common.io.Writable;
 import org.apache.doris.common.util.NetUtils;
@@ -73,14 +74,6 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
     private AtomicLong nextJournalId = new AtomicLong(1);
 
     public BDBJEJournal(String nodeName) {
-        initBDBEnv(nodeName);
-    }
-
-    /*
-     * Initialize bdb environment.
-     * node name is ip_port (the port is edit_log_port)
-     */
-    private void initBDBEnv(String nodeName) {
         environmentPath = Env.getServingEnv().getBdbDir();
         HostInfo selfNode = Env.getServingEnv().getSelfNode();
         selfNodeName = nodeName;
@@ -325,13 +318,14 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
     public synchronized void open() {
         if (bdbEnvironment == null) {
             File dbEnv = new File(environmentPath);
-            bdbEnvironment = new BDBEnvironment();
+
+            boolean metadataFailureRecovery = null != System.getProperty(FeConstants.METADATA_FAILURE_RECOVERY_KEY);
+            bdbEnvironment = new BDBEnvironment(Env.getServingEnv().isElectable(), metadataFailureRecovery);
 
             HostInfo helperNode = Env.getServingEnv().getHelperNode();
             String helperHostPort = NetUtils.getHostPortInAccessibleFormat(helperNode.getHost(), helperNode.getPort());
             try {
-                bdbEnvironment.setup(dbEnv, selfNodeName, selfNodeHostPort, helperHostPort,
-                        Env.getServingEnv().isElectable());
+                bdbEnvironment.setup(dbEnv, selfNodeName, selfNodeHostPort, helperHostPort);
             } catch (Exception e) {
                 if (e instanceof DatabaseNotFoundException) {
                     LOG.error("It is not allowed to set metadata_failure_recovery"
@@ -378,7 +372,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
                 reSetupBdbEnvironment(insufficientLogEx);
             } catch (RollbackException rollbackEx) {
                 LOG.warn("catch rollback log exception. will reopen the ReplicatedEnvironment.", rollbackEx);
-                bdbEnvironment.closeReplicatedEnvironment();
+                bdbEnvironment.close();
                 bdbEnvironment.openReplicatedEnvironment(new File(environmentPath));
             }
         }
@@ -412,8 +406,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
 
         bdbEnvironment.close();
         bdbEnvironment.setup(new File(environmentPath), selfNodeName, selfNodeHostPort,
-                NetUtils.getHostPortInAccessibleFormat(helperNode.getHost(), helperNode.getPort()),
-                Env.getServingEnv().isElectable());
+                NetUtils.getHostPortInAccessibleFormat(helperNode.getHost(), helperNode.getPort()));
     }
 
     @Override
@@ -504,7 +497,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
             } catch (RollbackException rollbackEx) {
                 if (!Env.isCheckpointThread()) {
                     LOG.warn("catch rollback log exception. will reopen the ReplicatedEnvironment.", rollbackEx);
-                    bdbEnvironment.closeReplicatedEnvironment();
+                    bdbEnvironment.close();
                     bdbEnvironment.openReplicatedEnvironment(new File(environmentPath));
                 } else {
                     throw rollbackEx;
