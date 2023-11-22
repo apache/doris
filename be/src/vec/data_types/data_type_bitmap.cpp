94872b3ff0fc3a97488b00fe395e79760541ddd2
lamb <lambxu@tencent.com>
2023-11-22 16:56:07 +0800

    format

--- be/src/vec/data_types/data_type_bitmap.cpp
+++ be/src/vec/data_types/data_type_bitmap.cpp
@@ -114,7 +114,7 @@ void DataTypeBitMap::to_string(const IColumn& column, size_t row_num, BufferWrit
     ColumnPtr ptr = result.first;
     row_num = result.second;
 
-    auto& data = 
+    auto& data =
             const_cast<BitmapValue&>(assert_cast<const ColumnBitmap&>(*ptr).get_element(row_num));
     std::string buffer(data.getSizeInBytes(), '0');
     data.write_to(const_cast<char*>(buffer.data()));
