a524bd340f9293888201f2a7a0f3e97f00753774
Gabriel <gabrielleebuaa@gmail.com>
2023-11-23 00:24:32 +0800

    [pipelineX](api) Fix core dump for pipelineX API (#27437)

--- be/src/pipeline/exec/partition_sort_source_operator.h
+++ be/src/pipeline/exec/partition_sort_source_operator.h
@@ -83,15 +83,13 @@ public:
     using Base = PipelineXLocalState<PartitionSortSourceDependency>;
     PartitionSortSourceLocalState(RuntimeState* state, OperatorXBase* parent)
             : PipelineXLocalState<PartitionSortSourceDependency>(state, parent),
-              _get_sorted_timer(nullptr),
-              _get_next_timer(nullptr) {}
+              _get_sorted_timer(nullptr) {}
 
     Status init(RuntimeState* state, LocalStateInfo& info) override;
 
 private:
     friend class PartitionSortSourceOperatorX;
     RuntimeProfile::Counter* _get_sorted_timer;
-    RuntimeProfile::Counter* _get_next_timer;
     std::atomic<int> _sort_idx = 0;
 };
 
