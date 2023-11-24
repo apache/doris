6aff83dee5209660b6d82d352d574305349bd617
Guangdong Liu <liugddx@gmail.com>
2023-11-24 10:15:47 +0800

    [regression test](routine test) add case for `num_as_string` (#27436)

--- regression-test/suites/load_p0/routine_load/test_routine_load.groovy
+++ regression-test/suites/load_p0/routine_load/test_routine_load.groovy
@@ -534,6 +534,87 @@ suite("test_routine_load","p0") {
         }
     }
 
+    // num_as_string
+    i = 0
+    if (enabled != null && enabled.equalsIgnoreCase("true")) {
+        try {
+            for (String tableName in tables) {
+                sql new File("""${context.file.parent}/ddl/${tableName}_drop.sql""").text
+                sql new File("""${context.file.parent}/ddl/${tableName}_create.sql""").text
+
+                def name = "routine_load_" + tableName
+                sql """
+                    CREATE ROUTINE LOAD ${jobs[i]} ON ${name}
+                    COLUMNS(${columns[i]}),
+                    COLUMNS TERMINATED BY "|"
+                    PROPERTIES
+                    (
+                        "format" = "json",
+                        "num_as_string" = "true",
+                        "max_batch_interval" = "5",
+                        "max_batch_rows" = "300000",
+                        "max_batch_size" = "209715200"
+                    )
+                    FROM KAFKA
+                    (
+                        "kafka_broker_list" = "${externalEnvIp}:${kafka_port}",
+                        "kafka_topic" = "${jsonTopic[i]}",
+                        "property.kafka_default_offsets" = "OFFSET_BEGINNING"
+                    );
+                """
+                sql "sync"
+                i++
+            }
+
+            i = 0
+            for (String tableName in tables) {
+                while (true) {
+                    sleep(1000)
+                    def res = sql "show routine load for ${jobs[i]}"
+                    def state = res[0][8].toString()
+                    if (state == "NEED_SCHEDULE") {
+                        continue;
+                    }
+                    log.info("reason of state changed: ${res[0][17].toString()}".toString())
+                    assertEquals(res[0][8].toString(), "RUNNING")
+                    break;
+                }
+
+                def count = 0
+                def tableName1 =  "routine_load_" + tableName
+                while (true) {
+                    def res = sql "select count(*) from ${tableName1}"
+                    def state = sql "show routine load for ${jobs[i]}"
+                    log.info("routine load state: ${state[0][8].toString()}".toString())
+                    log.info("routine load statistic: ${state[0][14].toString()}".toString())
+                    log.info("reason of state changed: ${state[0][17].toString()}".toString())
+                    if (res[0][0] > 0) {
+                        break
+                    }
+                    if (count >= 120) {
+                        log.error("routine load can not visible for long time")
+                        assertEquals(20, res[0][0])
+                        break
+                    }
+                    sleep(5000)
+                    count++
+                }
+
+                if (i <= 3) {
+                    qt_sql_num_as_string "select * from ${tableName1} order by k00,k01"
+                } else {
+                    qt_sql_num_as_string "select * from ${tableName1} order by k00"
+                }
+
+                sql "stop routine load for ${jobs[i]}"
+                i++
+            }
+        } finally {
+            for (String tableName in tables) {
+                sql new File("""${context.file.parent}/ddl/${tableName}_drop.sql""").text
+            }
+        }
+    }
 
     // exec_mem_limit
     i = 0
@@ -1698,7 +1779,7 @@ suite("test_routine_load","p0") {
                     sleep(5000)
                     count++
                 }
-                
+
                 if (i <= 3) {
                     qt_show_command "select * from ${tableName1} order by k00,k01"
                 } else {
@@ -1785,7 +1866,7 @@ suite("test_routine_load","p0") {
                     sleep(5000)
                     count++
                 }
-                
+
                 if (i <= 3) {
                     qt_pause_and_resume_command "select * from ${tableName1} order by k00,k01"
                 } else {
@@ -1900,7 +1981,7 @@ suite("test_routine_load","p0") {
                     sleep(5000)
                     count++
                 }
-                
+
                 if (i <= 3) {
                     qt_update_command "select * from ${tableName1} order by k00,k01"
                 } else {
