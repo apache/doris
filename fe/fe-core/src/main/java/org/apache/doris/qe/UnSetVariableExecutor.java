a504ddf9add4c3e7f7041411b889e601a23d2495
Yulei-Yang <yulei.yang0699@gmail.com>
2023-11-26 20:28:57 +0800

    update

--- fe/fe-core/src/main/java/org/apache/doris/qe/UnSetVariableExecutor.java
+++
@@ -1,7 +0,0 @@
-package org.apache.doris.qe;/**
- * Copyright (C) 2015 THL A29 Limited, a Tencent company. All rights reserved.
- *
- * @author yuleiyang on 2023/11/26 19:08
- */
-public class UnSetVariableExecutor {
-}
