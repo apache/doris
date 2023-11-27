ec8348b0dee96ce34241d3da5ecded2859b5d2ad
yiguolei <yiguolei@gmail.com>
2023-11-27 17:36:30 +0800

    f

--- fe/fe-core/src/main/java/org/apache/doris/common/util/ProfileManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/util/ProfileManager.java
@@ -71,8 +71,6 @@ public class ProfileManager {
         }
 
         private final RuntimeProfile profile;
-        // cache the result of getProfileContent method
-        private volatile String profileContent;
         public Map<String, String> infoStrings = Maps.newHashMap();
         public MultiProfileTreeBuilder builder = null;
         public String errMsg = "";
@@ -82,8 +80,7 @@ public class ProfileManager {
         // lazy load profileContent because sometimes profileContent is very large
         public String getProfileContent() {
             // no need to lock because the possibility of concurrent read is very low
-            profileContent = profile.toString();
-            return profileContent;
+            return profile.toString();
         }
 
         public String getProfileBrief() {
