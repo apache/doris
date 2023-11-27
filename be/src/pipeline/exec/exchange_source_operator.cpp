f9d73cfc0ad7f6a45b55e663e661b7cb3afc354d
Gabriel <gabrielleebuaa@gmail.com>
2023-11-27 20:18:17 +0800

    update

--- be/src/pipeline/exec/exchange_source_operator.cpp
+++ be/src/pipeline/exec/exchange_source_operator.cpp
@@ -51,7 +51,7 @@ std::string ExchangeLocalState::debug_string(int indentation_level) const {
     const auto& queues = stream_recvr->sender_queues();
     for (size_t i = 0; i < queues.size(); i++) {
         fmt::format_to(debug_string_buffer,
-                       "No. {} queue: (_num_remaining_senders = {}, block_queue size = {})",
+                       "No. {} queue: (_num_remaining_senders = {}, block_queue size = {})", i,
                        queues[i]->_num_remaining_senders, queues[i]->_block_queue.size());
     }
     fmt::format_to(debug_string_buffer, ")");
