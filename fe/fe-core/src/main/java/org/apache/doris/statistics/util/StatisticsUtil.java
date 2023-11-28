3e82116cf6698b532e18274b14b55407bcc0975d
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2023-11-28 21:24:20 +0800

    [fix](statistics)Fix sample min max npe bug (#27702)
    Min and max value may be NULL, need to handle this case in sample analyze.

--- fe/fe-core/src/main/java/org/apache/doris/statistics/util/StatisticsUtil.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/util/StatisticsUtil.java
@@ -90,12 +90,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
@@ -935,6 +937,21 @@ public class StatisticsUtil {
             LOG.warn("Failed to get value of table_stats_health_threshold, return default", e);
         }
         return StatisticConstants.ANALYZE_TIMEOUT_IN_SEC;
+    }
+
+    public static String encodeValue(ResultRow row, int index) {
+        if (row == null || row.getValues().size() <= index) {
+            return "NULL";
+        }
+        return encodeString(row.get(index));
+    }
+
+    public static String encodeString(String value) {
+        if (value == null) {
+            return "NULL";
+        } else {
+            return Base64.getEncoder().encodeToString(value.getBytes(StandardCharsets.UTF_8));
+        }
     }
 
 }
