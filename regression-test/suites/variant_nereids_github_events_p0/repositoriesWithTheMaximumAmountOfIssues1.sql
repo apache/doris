abc958781a7a4ccfeb56d29fed8dd120bac4be5d
eldenmoon <15605149486@163.com>
2023-11-29 00:35:12 +0800

    add case

---
+++ regression-test/suites/variant_nereids_github_events_p0/repositoriesWithTheMaximumAmountOfIssues1.sql
@@ -0,0 +1,1 @@
+SELECT cast(v["repo"]["name"] as string), count() AS c, count(distinct cast(v["actor"]["login"] as string)) AS u FROM github_events WHERE cast(v["type"] as string) = 'IssuesEvent' AND cast(v["payload"]["action"] as string) = 'opened' GROUP BY cast(v["repo"]["name"] as string) ORDER BY c DESC, cast(v["repo"]["name"] as string) LIMIT 50
