4898c72ee57a3238cefc7536d0e2fbdb2d5f6c0f
eldenmoon <15605149486@163.com>
2023-11-30 13:07:43 +0800

    fix ut

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/SlotReference.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/SlotReference.java
@@ -126,7 +126,8 @@ public class SlotReference extends Slot {
         DataType dataType = DataType.fromCatalogType(column.getType());
         SlotReference slot = new SlotReference(StatementScopeIdGenerator.newExprId(), column.getName(), dataType,
                 column.isAllowNull(), qualifier, column, Optional.empty(), null);
-        if (relation != null && ConnectContext.get() != null) {
+        if (relation != null && ConnectContext.get() != null
+                && ConnectContext.get().getStatementContext() != null) {
             ConnectContext.get().getStatementContext().addSlotToRelation(slot, relation);
         }
         return slot;
