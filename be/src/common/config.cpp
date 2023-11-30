db8e56b9f2f9bcf51159ac24ef8f9f8820825e31
Kaijie Chen <ckj@apache.org>
2023-11-30 15:27:29 +0800

    [improve](move-memtable) increase open load stream timeout (#26909)

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -747,7 +747,7 @@ DEFINE_mDouble(tablet_version_graph_orphan_vertex_ratio, "0.1");
 // share delta writers when memtable_on_sink_node = true
 DEFINE_Bool(share_delta_writers, "true");
 // timeout for open load stream rpc in ms
-DEFINE_Int64(open_load_stream_timeout_ms, "500");
+DEFINE_Int64(open_load_stream_timeout_ms, "60000"); // 60s
 // timeout for load stream close wait in ms
 DEFINE_Int64(close_load_stream_timeout_ms, "600000"); // 10 min
 
