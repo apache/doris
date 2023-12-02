2e1ce758f181ebc0e5f921beaf30306f4e49519c
Chester <42577861+superdiaodiao@users.noreply.github.com>
2023-12-02 11:48:19 +0800

    [feature](function) support ip function ipv6numtostring(alias inet6_ntoa) (#27342)

--- be/src/vec/functions/function_ip.cpp
+++ be/src/vec/functions/function_ip.cpp
@@ -30,5 +30,7 @@ void register_function_ip(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionIPv4StringToNum<IPStringToNumExceptionMode::Null>>();
     factory.register_alias(FunctionIPv4StringToNum<IPStringToNumExceptionMode::Throw>::name,
                            "inet_aton");
+    factory.register_function<FunctionIPv6NumToString>();
+    factory.register_alias(FunctionIPv6NumToString::name, "inet6_ntoa");
 }
 } // namespace doris::vectorized
