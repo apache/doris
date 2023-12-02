72b2a336e973df45ed3a180d1970fd7aa4072424
jakevin <jakevingoo@gmail.com>
2023-12-02 23:19:49 +0800

    [chore](Nereids): use debug to show phase of nereids (#27903)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
@@ -267,28 +267,28 @@ public class NereidsPlanner extends Planner {
     }
 
     private void analyze() {
-        LOG.info("Start analyze plan");
+        LOG.debug("Start analyze plan");
         cascadesContext.newAnalyzer().analyze();
         NereidsTracer.logImportantTime("EndAnalyzePlan");
-        LOG.info("End analyze plan");
+        LOG.debug("End analyze plan");
     }
 
     /**
      * Logical plan rewrite based on a series of heuristic rules.
      */
     private void rewrite() {
-        LOG.info("Start rewrite plan");
+        LOG.debug("Start rewrite plan");
         Rewriter.getWholeTreeRewriter(cascadesContext).execute();
         NereidsTracer.logImportantTime("EndRewritePlan");
-        LOG.info("End rewrite plan");
+        LOG.debug("End rewrite plan");
     }
 
     // DependsRules: EnsureProjectOnTopJoin.class
     private void optimize() {
-        LOG.info("Start optimize plan");
+        LOG.debug("Start optimize plan");
         new Optimizer(cascadesContext).execute();
         NereidsTracer.logImportantTime("EndOptimizePlan");
-        LOG.info("End optimize plan");
+        LOG.debug("End optimize plan");
     }
 
     private PhysicalPlan postProcess(PhysicalPlan physicalPlan) {
