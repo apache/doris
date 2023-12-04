b98c629b0f8f7df117a3a3cfab5bead9cad03059
Yukang-Lian <Yukang.Lian2022@gmail.com>
2023-12-04 20:41:59 +0800

    3

--- regression-test/suites/load_p0/stream_load/test_group_commit_wal_limit.groovy
+++ regression-test/suites/load_p0/stream_load/test_group_commit_wal_limit.groovy
@@ -93,30 +93,31 @@ suite("test_group_commit_wal_limit") {
 
     // httpload 
     // normal case
-    StringBuilder strBuilder = new StringBuilder()
-    strBuilder.append("curl --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
-    strBuilder.append(" -H \"group_commit:true\" -H \"column_separator:,\" " )
-    strBuilder.append(" -H \"compress_type:gz\" -H \"format:csv\" " )
+    strBuilder = new StringBuilder()
+    strBuilder.append("curl -v --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
+    String sql = " -H \"sql:insert into " + db + "." + tableName + " (k,v) select c1, c2 from http_stream(\\\"format\\\" = \\\"csv\\\", \\\"column_separator\\\" = \\\",\\\", \\\"compress_type\\\" = \\\"gz\\\" ) \" "
+    strBuilder.append(sql)
+    strBuilder.append(" -H \"group_commit:true\"") 
     strBuilder.append(" -T " + context.config.dataPath + "/load_p0/stream_load/test_group_commit_wal_limit.csv.gz")
-    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/${db}/${tableName}/_stream_load")
+    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/_http_stream")
 
-    String command = strBuilder.toString()
+    command = strBuilder.toString()
     logger.info("command is " + command)
-    def process = ['bash','-c',command].execute() 
-    def code = process.waitFor()
+    process = ['bash','-c',command].execute() 
+    code = process.waitFor()
     assertEquals(code, 0)
-    def out = process.text
+    out = process.text
     logger.info("out is " + out )
     assertTrue(out.contains('group_commit'))
 
     // chunked data case
     strBuilder = new StringBuilder()
-    strBuilder.append("curl --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
-    strBuilder.append(" -H \"group_commit:true\" -H \"column_separator:,\" " )
-    strBuilder.append(" -H \"compress_type:gz\" -H \"format:csv\" " )
-    strBuilder.append(" -H \"Content-Length:0\" " )
+    strBuilder.append("curl -v --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
+    sql = " -H \"sql:insert into " + db + "." + tableName + " (k,v) select c1, c2 from http_stream(\\\"format\\\" = \\\"csv\\\", \\\"column_separator\\\" = \\\",\\\", \\\"compress_type\\\" = \\\"gz\\\" ) \" "
+    strBuilder.append(sql)
+    strBuilder.append(" -H \"group_commit:true\" -H \"Content-Length:0\"") 
     strBuilder.append(" -T " + context.config.dataPath + "/load_p0/stream_load/test_group_commit_wal_limit.csv.gz")
-    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/${db}/${tableName}/_stream_load")
+    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/_http_stream")
 
     command = strBuilder.toString()
     logger.info("command is " + command)
@@ -125,16 +126,16 @@ suite("test_group_commit_wal_limit") {
     assertEquals(code, 0)
     out = process.text
     logger.info("out is " + out )
-    assertTrue(out.contains('[INTERNAL_ERROR]Stream load size too large'))
+    assertTrue(out.contains('[INTERNAL_ERROR]Http load size too large'))
 
     // too lagre data case 1TB
     strBuilder = new StringBuilder()
-    strBuilder.append("curl --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
-    strBuilder.append(" -H \"group_commit:true\" -H \"column_separator:,\" " )
-    strBuilder.append(" -H \"compress_type:gz\" -H \"format:csv\" " )
-    strBuilder.append(" -H \"Content-Length:1099511627776\" " )
+    strBuilder.append("curl -v --location-trusted -u " + context.config.jdbcUser + ":" + context.config.jdbcPassword)
+    sql = " -H \"sql:insert into " + db + "." + tableName + " (k,v) select c1, c2 from http_stream(\\\"format\\\" = \\\"csv\\\", \\\"column_separator\\\" = \\\",\\\", \\\"compress_type\\\" = \\\"gz\\\" ) \" "
+    strBuilder.append(sql)
+    strBuilder.append(" -H \"group_commit:true\" -H \"Content-Length:1099511627776\"") 
     strBuilder.append(" -T " + context.config.dataPath + "/load_p0/stream_load/test_group_commit_wal_limit.csv.gz")
-    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/${db}/${tableName}/_stream_load")
+    strBuilder.append(" http://" + context.config.feHttpAddress + "/api/_http_stream")
 
     command = strBuilder.toString()
     logger.info("command is " + command)
@@ -143,5 +144,5 @@ suite("test_group_commit_wal_limit") {
     assertEquals(code, 0)
     out = process.text
     logger.info("out is " + out )
-    assertTrue(out.contains('[INTERNAL_ERROR]Stream load size too large'))
+    assertTrue(out.contains('[INTERNAL_ERROR]Http load size too large'))
 }
