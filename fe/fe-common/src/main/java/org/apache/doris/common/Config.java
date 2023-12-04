3309a908496058c4233faa4895756e7cb3298fe7
xueweizhang <zxw520blue1@163.com>
2023-12-04 11:19:45 +0800

    [enhancement](backup-restore) add config for upload/download task num per be (#27772)
    set upload/download task num per be, and improve the overall speed of upload/download, enhance the performance of backup and recovery.
    
    ---------
    
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2298,6 +2298,18 @@ public class Config extends ConfigBase {
             "The path of the nereids trace file."})
     public static String nereids_trace_log_dir = System.getenv("DORIS_HOME") + "/log/nereids_trace";
 
+    @ConfField(mutable = true, masterOnly = true, description = {
+            "备份过程中，分配给每个be的upload任务最大个数，默认值为3个。",
+            "The max number of upload tasks assigned to each be during the backup process, the default value is 3."
+    })
+    public static int backup_upload_task_num_per_be = 3;
+
+    @ConfField(mutable = true, masterOnly = true, description = {
+            "恢复过程中，分配给每个be的download任务最大个数，默认值为3个。",
+            "The max number of download tasks assigned to each be during the restore process, the default value is 3."
+    })
+    public static int restore_download_task_num_per_be = 3;
+
     @ConfField(description = {"是否开启通过http接口获取log文件的功能",
             "Whether to enable the function of getting log files through http interface"})
     public static boolean enable_get_log_file_api = false;
