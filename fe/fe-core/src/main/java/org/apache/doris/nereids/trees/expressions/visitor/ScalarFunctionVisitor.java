c98b80ae6ae5fb9cdad084fcfb60a3d8bfec5d91
zclllyybb <zhaochangle@selectdb.com>
2023-12-05 14:09:32 +0800

    [Feature](functions) support ignore and nullable functions (#27848)
    support ignore and nullable functions

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/ScalarFunctionVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/ScalarFunctionVisitor.java
@@ -182,6 +182,7 @@ import org.apache.doris.nereids.trees.expressions.functions.scalar.HoursAdd;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.HoursDiff;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.HoursSub;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.If;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.Ignore;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Initcap;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.InnerProduct;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Instr;
@@ -1081,6 +1082,10 @@ public interface ScalarFunctionVisitor<R, C> {
     }
 
     default R visitIf(If function, C context) {
+        return visitScalarFunction(function, context);
+    }
+
+    default R visitIgnore(Ignore function, C context) {
         return visitScalarFunction(function, context);
     }
 
