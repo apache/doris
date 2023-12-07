e04526e58f4eaf050db13c4fbe01af3591f4f7ef
ziyang zhang <zhangziyang@stu.cdut.edu.cn>
2023-12-07 09:35:44 +0000

    reset the paramters after test

--- regression-test/suites/load_p0/insert/test_insert_with_aggregation_memtable.groovy
+++ regression-test/suites/load_p0/insert/test_insert_with_aggregation_memtable.groovy
@@ -18,6 +18,7 @@
 suite("test_insert_with_aggregation_memtable") {
     def backendId_to_backendIP = [:]
     def backendId_to_backendHttpPort = [:]
+    def backendId_to_params = [string:[:]]
     getBackendIpHttpPort(backendId_to_backendIP, backendId_to_backendHttpPort);
 
     def set_be_param = { paramName, paramValue ->
@@ -28,6 +29,36 @@ suite("test_insert_with_aggregation_memtable") {
 		    def (code, out, err) = curl("POST", String.format("http://%s:%s/api/update_config?%s=%s", beIp, bePort, paramName, paramValue))
 		    assertTrue(out.contains("OK"))
 	    }
+    }
+
+    def reset_be_param = { paramName ->
+        // for eache be node, reset paramName to default
+        for (String id in backendId_to_backendIP.keySet()) {
+            def beIp = backendId_to_backendIP.get(id)
+            def bePort = backendId_to_backendHttpPort.get(id)
+            def original_value = backendId_to_params.get(id).get(paramName)
+            def (code, out, err) = curl("POST", String.format("http://%s:%s/api/update_config?%s=%s", beIp, bePort, paramName, original_value))
+            assertTrue(out.contains("OK"))
+        }
+    }
+
+    def get_be_param = { paramName ->
+        // for eache be node, get param value by default
+        def paramValue = ""
+        for (String id in backendId_to_backendIP.keySet()) {
+            def beIp = backendId_to_backendIP.get(id)
+            def bePort = backendId_to_backendHttpPort.get(id)
+            // get the config value from be
+            def (code, out, err) = curl("GET", String.format("http://%s:%s/api/show_config?conf_item=%s", beIp, bePort, paramName))
+            assertTrue(code == 0)
+            assertTrue(out.contains(paramName))
+            // parsing
+            def resultList = parseJson(out)[0]
+            assertTrue(resultList.size() == 4)
+            // get original value
+            paramValue = resultList[2]
+            backendId_to_params.get(id, [:]).put(paramName, paramValue)
+        }
     }
     
     def testTable = "test_memtable_enable_with_aggregate"
@@ -61,12 +92,17 @@ suite("test_insert_with_aggregation_memtable") {
         (10004,"2017-10-01","深圳",35,0,"2017-10-01 10:00:15",100,3,3),
         (10004,"2017-10-03","深圳",35,0,"2017-10-03 10:20:22",11,6,6);
     """
+  
     sql testTableDDL
     sql "sync"
     sql insert_sql
     sql "sync"
     qt_sql "select * from ${testTable} order by id asc"
     
+    // store the original value
+    get_be_param("enable_shrink_memory")
+    get_be_param("write_buffer_size_for_agg")
+
     // the original value is false
     set_be_param("enable_shrink_memory", "true")
     // the original value is 400MB
@@ -78,6 +114,7 @@ suite("test_insert_with_aggregation_memtable") {
     sql "sync"
     qt_sql "select * from ${testTable} order by id asc"
     sql """ set enable_memtable_on_sink_node=false"""
-    set_be_param("enable_shrink_memory", "false")
-    set_be_param("write_buffer_size_for_agg", "419430400") // change it back to 400MB
+
+    reset_be_param("enable_shrink_memory")
+    reset_be_param("write_buffer_size_for_agg")
 }
