8eed760704cf8f707cda839dfc570eef875a7a6a
Mingyu Chen <morningman@163.com>
2023-12-08 23:15:45 +0800

    [fix](planner) separate table's isPartitioned() method (#28163)
    This PR #27515 change the logic if Table's `isPartitioned()` method.
    But this method has 2 usages:
    
    1. To check whether a table is range or list partitioned, for some DML operation such as Alter, Export.
    
        For this case, it should return true if the table is range or list partitioned. even if it has only
        one partition and one buckets.
    
    2. To check whether the data is distributed (either by partitions or by buckets), for query planner.
    
        For this case, it should return true if table has more than one bucket. Even if this table is not
        range or list partitioned, if it has more than one bucket, it should return true.
    
    So we should separate this method into 2, for different usages.
    Otherwise, it may cause some unreasonable plan shape

--- fe/fe-core/src/test/java/org/apache/doris/planner/PlannerTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/planner/PlannerTest.java
@@ -21,6 +21,7 @@ import org.apache.doris.analysis.ExplainOptions;
 import org.apache.doris.analysis.Expr;
 import org.apache.doris.analysis.UserIdentity;
 import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.FeConstants;
 import org.apache.doris.qe.QueryState;
 import org.apache.doris.qe.QueryState.MysqlStateType;
 import org.apache.doris.qe.StmtExecutor;
@@ -681,5 +682,37 @@ public class PlannerTest extends TestWithFeService {
             String plan1 = planner1.getExplainString(new ExplainOptions(false, false, false));
             Assertions.assertFalse(plan1.contains("order by:"));
             }
+    }
+
+    @Test
+    public void testInsertPlan() throws Exception {
+        FeConstants.runningUnitTest = true;
+        // 1. should not contains exchange node in old planner
+        boolean v = connectContext.getSessionVariable().isEnableNereidsPlanner();
+        try {
+            connectContext.getSessionVariable().setEnableNereidsPlanner(false);
+            String sql1 = "explain insert into db1.tbl1 select * from db1.tbl1";
+            StmtExecutor stmtExecutor1 = new StmtExecutor(connectContext, sql1);
+            stmtExecutor1.execute();
+            Planner planner1 = stmtExecutor1.planner();
+            String plan1 = planner1.getExplainString(new ExplainOptions(false, false, false));
+            Assertions.assertFalse(plan1.contains("VEXCHANGE"));
+        } finally {
+            connectContext.getSessionVariable().setEnableNereidsPlanner(v);
+        }
+
+        // 1. should not contains exchange node in new planner
+        v = connectContext.getSessionVariable().isEnableNereidsPlanner();
+        try {
+            connectContext.getSessionVariable().setEnableNereidsPlanner(true);
+            String sql1 = "explain insert into db1.tbl1 select * from db1.tbl1";
+            StmtExecutor stmtExecutor1 = new StmtExecutor(connectContext, sql1);
+            stmtExecutor1.execute();
+            Planner planner1 = stmtExecutor1.planner();
+            String plan1 = planner1.getExplainString(new ExplainOptions(false, false, false));
+            Assertions.assertFalse(plan1.contains("VEXCHANGE"));
+        } finally {
+            connectContext.getSessionVariable().setEnableNereidsPlanner(v);
+        }
     }
 }
