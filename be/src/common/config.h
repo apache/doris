884c7eec4f3529c9fbef29aa711b1f698f754448
Gabriel <gabrielleebuaa@gmail.com>
2023-12-11 21:25:31 +0800

    [pipelineX](improvement) block local shuffle sink by mem usage (#28224)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1222,6 +1222,8 @@ DECLARE_Bool(enable_snapshot_action);
 // The max columns size for a tablet schema
 DECLARE_mInt32(variant_max_merged_tablet_schema_size);
 
+DECLARE_mInt64(local_exchange_buffer_mem_limit);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
