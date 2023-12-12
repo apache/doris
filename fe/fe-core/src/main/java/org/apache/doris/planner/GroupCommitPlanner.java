45b2dbab6afa093f3402ac919929f97ff28f5c9a
meiyi <myimeiyi@gmail.com>
2023-12-12 16:33:36 +0800

    [improve](group commit) Group commit support max filter ratio when rows is less than value in config (#28139)

--- fe/fe-core/src/main/java/org/apache/doris/planner/GroupCommitPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/GroupCommitPlanner.java
@@ -98,11 +98,12 @@ public class GroupCommitPlanner {
         }
         streamLoadPutRequest
                 .setDb(db.getFullName())
-                .setMaxFilterRatio(1)
+                .setMaxFilterRatio(ConnectContext.get().getSessionVariable().enableInsertStrict ? 0 : 1)
                 .setTbl(table.getName())
                 .setFileType(TFileType.FILE_STREAM).setFormatType(TFileFormatType.FORMAT_CSV_PLAIN)
                 .setMergeType(TMergeType.APPEND).setThriftRpcTimeoutMs(5000).setLoadId(queryId)
-                .setTrimDoubleQuotes(true).setGroupCommitMode(groupCommit);
+                .setTrimDoubleQuotes(true).setGroupCommitMode(groupCommit)
+                .setStrictMode(ConnectContext.get().getSessionVariable().enableInsertStrict);
         StreamLoadTask streamLoadTask = StreamLoadTask.fromTStreamLoadPutRequest(streamLoadPutRequest);
         StreamLoadPlanner planner = new StreamLoadPlanner(db, table, streamLoadTask);
         // Will using load id as query id in fragment
