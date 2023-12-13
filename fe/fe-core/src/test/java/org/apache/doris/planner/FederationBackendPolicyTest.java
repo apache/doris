07f2a98aa8069eb8c86519bdac83303550c8d974
morningman <morningman@163.com>
2023-12-13 22:56:44 +0800

    [opt](task-assignment) use consistent hash as default task assigner and cache the consistent hash ring

--- fe/fe-core/src/test/java/org/apache/doris/planner/FederationBackendPolicyTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/planner/FederationBackendPolicyTest.java
@@ -20,8 +20,15 @@ package org.apache.doris.planner;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.UserException;
 import org.apache.doris.planner.external.FederationBackendPolicy;
+import org.apache.doris.planner.external.FileSplit;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.SystemInfoService;
+import org.apache.doris.thrift.TExternalScanRange;
+import org.apache.doris.thrift.TFileRangeDesc;
+import org.apache.doris.thrift.TFileScanRange;
+import org.apache.doris.thrift.TFileType;
+import org.apache.doris.thrift.TScanRange;
+import org.apache.doris.thrift.TScanRangeLocations;
 
 import com.google.common.base.Stopwatch;
 import mockit.Mock;
@@ -31,6 +38,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -93,12 +101,50 @@ public class FederationBackendPolicyTest {
         int invokeTimes = 1000000;
         Assertions.assertEquals(policy.numBackends(), backendNum);
         List<String> localHosts = Arrays.asList("192.168.1.0", "192.168.1.1", "192.168.1.2");
+        TScanRangeLocations scanRangeLocations = getScanRangeLocations("path1", 0, 100);
         Stopwatch sw = Stopwatch.createStarted();
         for (int i = 0; i < invokeTimes; i++) {
-            Assertions.assertTrue(localHosts.contains(policy.getNextLocalBe(localHosts).getHost()));
+            Assertions.assertTrue(localHosts.contains(policy.getNextLocalBe(localHosts, scanRangeLocations).getHost()));
         }
         sw.stop();
         System.out.println("Invoke getNextLocalBe() " + invokeTimes
                     + " times cost [" + sw.elapsed(TimeUnit.MILLISECONDS) + "] ms");
+    }
+
+    @Test
+    public void testConsistentHash() throws UserException {
+        FederationBackendPolicy policy = new FederationBackendPolicy();
+        policy.init();
+        int backendNum = 200;
+        Assertions.assertEquals(policy.numBackends(), backendNum);
+
+        TScanRangeLocations scanRangeLocations = getScanRangeLocations("path1", 0, 100);
+        Assertions.assertEquals(39, policy.getNextConsistentBe(scanRangeLocations).getId());
+
+        scanRangeLocations = getScanRangeLocations("path2", 0, 100);
+        Assertions.assertEquals(78, policy.getNextConsistentBe(scanRangeLocations).getId());
+    }
+
+    private TScanRangeLocations getScanRangeLocations(String path, long startOffset, long size) {
+        // Generate on file scan range
+        TFileScanRange fileScanRange = new TFileScanRange();
+        // Scan range
+        TExternalScanRange externalScanRange = new TExternalScanRange();
+        externalScanRange.setFileScanRange(fileScanRange);
+        TScanRange scanRange = new TScanRange();
+        scanRange.setExtScanRange(externalScanRange);
+        scanRange.getExtScanRange().getFileScanRange().addToRanges(createRangeDesc(path, startOffset, size));
+        // Locations
+        TScanRangeLocations locations = new TScanRangeLocations();
+        locations.setScanRange(scanRange);
+        return locations;
+    }
+
+    private TFileRangeDesc createRangeDesc(String path, long startOffset, long size) {
+        TFileRangeDesc rangeDesc = new TFileRangeDesc();
+        rangeDesc.setPath(path);
+        rangeDesc.setStartOffset(startOffset);
+        rangeDesc.setSize(size);
+        return rangeDesc;
     }
 }
