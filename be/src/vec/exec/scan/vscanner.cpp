ec91dd11295a58ff7e5caffa8e6949232a1b28b8
Ashin Gau <AshinGau@users.noreply.github.com>
2023-12-13 19:31:08 +0800

    [opt](vfilescanner) interrupt running parquet/orc readers when scannode is finished (#28223)
    VScanNode::get_next will check whether the ScanNode has reached limit condition, and send eos to TaskScheduler, and TaskScheduler will try to close ScanNode.
    However, ScanNode must wait all running scanners finished, so even if ScanNode has reached limit condition, it can't be closed immediately.
    This PR try to interrupt the running readers, and make ScanNode to end as soon as possible.

--- be/src/vec/exec/scan/vscanner.cpp
+++ be/src/vec/exec/scan/vscanner.cpp
@@ -106,7 +106,7 @@ Status VScanner::get_block(RuntimeState* state, Block* block, bool* eof) {
             }
             // record rows return (after filter) for _limit check
             _num_rows_return += block->rows();
-        } while (!state->is_cancelled() && block->rows() == 0 && !(*eof) &&
+        } while (!_should_stop && !state->is_cancelled() && block->rows() == 0 && !(*eof) &&
                  _num_rows_read < rows_read_threshold);
     }
 
