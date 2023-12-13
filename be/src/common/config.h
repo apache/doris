e6e86321671f4534cc5aecc774e1ffb39eb06a53
Xin Liao <liaoxinbit@126.com>
2023-12-13 16:59:25 +0800

    [improvement](merge-on-write) Optimize publish when there are missing versions (#28012)
    1. Do not retry publishing on be When there are too many missing versions, just
    add to async publish task.
    2. To reduce memory consumption, clean up the tasks when there are too many
    async publish tasks.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1149,6 +1149,9 @@ DECLARE_mInt64(variant_threshold_rows_to_estimate_sparse_column);
 DECLARE_mBool(enable_merge_on_write_correctness_check);
 // rowid conversion correctness check when compaction for mow table
 DECLARE_mBool(enable_rowid_conversion_correctness_check);
+// When the number of missing versions is more than this value, do not directly
+// retry the publish and handle it through async publish.
+DECLARE_mInt32(mow_publish_max_discontinuous_version_num);
 
 // The secure path with user files, used in the `local` table function.
 DECLARE_mString(user_files_secure_path);
