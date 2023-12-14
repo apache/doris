f0beee0fc292f3f9408789af32e26245e5719a8c
ci <ci@selectdb.com>
2023-12-14 20:52:06 +0800

    [ci](perf) add perf check of tpcds

---
+++ regression-test/pipeline/common/get-or-set-tmp-env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+tmp_env_file_path="${PWD}/.my_tmp_env"
+
+usage() {
+    echo -e "
+Usage:
+    $0 'get'
+    $0 'set' \"export skip_pipeline='true'\"
+    note: 'get' will return env file path; 'set' will add your new item into env file"
+    exit 1
+}
+
+if [[ $1 == 'get' ]]; then
+    echo "${tmp_env_file_path}"
+elif [[ $1 == 'set' ]]; then
+    if [[ -z $2 ]]; then usage; fi
+    echo "$2" >>"${tmp_env_file_path}"
+else
+    usage
+fi
---
+++ regression-test/pipeline/performance/conf/README.md
@@ -0,0 +1,5 @@
+## 简介
+这里的 conf 用于社区 tpcds 流水线
+基于 master 分支的默认 conf 文件，  
+参考：https://github.com/apache/doris/tree/master/conf  
+添加 fe_custom.conf，be_custom.conf，session_variables 来设置跑流水线的自定义设置。
