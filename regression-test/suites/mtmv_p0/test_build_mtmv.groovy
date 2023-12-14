c7f5f457a616e75508402b920634f2e350a2ef4d
zhangdong <493738387@qq.com>
2023-12-14 12:59:05 +0800

    1

--- regression-test/suites/mtmv_p0/test_build_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_build_mtmv.groovy
@@ -15,6 +15,11 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.Instant;
+import java.time.ZoneId;
+
 suite("test_build_mtmv") {
     def tableName = "t_test_create_mtmv_user"
     def tableNamePv = "t_test_create_mtmv_user_pv"
