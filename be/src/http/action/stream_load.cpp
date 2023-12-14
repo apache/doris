b55e610e8c283878b421adf2607d7724f4d1a60b
meiyi <myimeiyi@gmail.com>
2023-12-14 14:38:56 +0800

    [fix](group commit) Fix some group commit problems (#28319)

--- be/src/http/action/stream_load.cpp
+++ be/src/http/action/stream_load.cpp
@@ -199,28 +199,36 @@ int StreamLoadAction::on_header(HttpRequest* req) {
             group_commit_mode = "";
         }
     }
-    if (!group_commit_mode.empty() || config::wait_internal_group_commit_finish) {
+    auto partial_columns = !req->header(HTTP_PARTIAL_COLUMNS).empty() &&
+                           iequal(req->header(HTTP_PARTIAL_COLUMNS), "true");
+    ctx->two_phase_commit = req->header(HTTP_TWO_PHASE_COMMIT) == "true";
+    auto temp_partitions = !req->header(HTTP_TEMP_PARTITIONS).empty();
+    auto partitions = !req->header(HTTP_PARTITIONS).empty();
+    if (!partial_columns && !partitions && !temp_partitions && !ctx->two_phase_commit &&
+        (!group_commit_mode.empty() || config::wait_internal_group_commit_finish)) {
         if (!group_commit_mode.empty() && !ctx->label.empty()) {
             st = Status::InternalError("label and group_commit can't be set at the same time");
         }
-        ctx->group_commit = load_size_smaller_than_wal_limit(req);
-        if (!ctx->group_commit) {
-            LOG(WARNING) << "The data size for this stream load("
-                         << std::stol(req->header(HttpHeaders::CONTENT_LENGTH))
-                         << " Bytes) exceeds the WAL (Write-Ahead Log) limit ("
-                         << config::wal_max_disk_size * 0.8
-                         << " Bytes). Please set this load to \"group commit\"=false.";
-            st = Status::InternalError("Stream load size too large.");
+        if (config::wait_internal_group_commit_finish) {
+            ctx->group_commit = true;
+        } else {
+            ctx->group_commit = load_size_smaller_than_wal_limit(req);
+            if (!ctx->group_commit) {
+                LOG(WARNING) << "The data size for this stream load("
+                             << req->header(HttpHeaders::CONTENT_LENGTH)
+                             << " Bytes) exceeds the WAL (Write-Ahead Log) limit ("
+                             << config::wal_max_disk_size * 0.8
+                             << " Bytes). Please set this load to \"group commit\"=false.";
+                st = Status::InternalError("Stream load size too large.");
+            }
         }
     }
     if (!ctx->group_commit && ctx->label.empty()) {
         ctx->label = generate_uuid_string();
     }
 
-    ctx->two_phase_commit = req->header(HTTP_TWO_PHASE_COMMIT) == "true";
-
     LOG(INFO) << "new income streaming load request." << ctx->brief() << ", db=" << ctx->db
-              << ", tbl=" << ctx->table;
+              << ", tbl=" << ctx->table << ", group_commit=" << ctx->group_commit;
 
     if (st.ok()) {
         st = _on_header(req, ctx);
