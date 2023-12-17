apache/doris

commit 8c05f7a784ee6c9c6ad6c42b6410abc2190fb931

[refactor](cluster)(step-4) remove cluster related to Database (#27861)
Issue Number: #19897

Remove `default_cluster` prefix related to database.
When upgrading, all prefix will be removed.

fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -60,7 +60,6 @@ import org.apache.doris.catalog.ColocateTableIndex.GroupId;
 import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.DataProperty;
 import org.apache.doris.catalog.Database;
-import org.apache.doris.catalog.Database.DbState;
 import org.apache.doris.catalog.DatabaseIf;
 import org.apache.doris.catalog.DatabaseProperty;
 import org.apache.doris.catalog.DistributionInfo;
@@ -214,8 +213,8 @@ public class InternalCatalog implements CatalogIf<Database> {
     public InternalCatalog() {
         // create internal databases
         List<MysqlCompatibleDatabase> mysqlCompatibleDatabases = new ArrayList<>();
-        mysqlCompatibleDatabases.add(new InfoSchemaDb(SystemInfoService.DEFAULT_CLUSTER));
-        mysqlCompatibleDatabases.add(new MysqlDb(SystemInfoService.DEFAULT_CLUSTER));
+        mysqlCompatibleDatabases.add(new InfoSchemaDb());
+        mysqlCompatibleDatabases.add(new MysqlDb());
         MysqlCompatibleDatabase.COUNT = 2;
 
         for (MysqlCompatibleDatabase idb : mysqlCompatibleDatabases) {
@@ -264,13 +263,7 @@ public class InternalCatalog implements CatalogIf<Database> {
             // and finally get information_schema db from the name map.
             String fullName = ClusterNamespace.getNameFromFullName(dbName);
             if (fullName.equalsIgnoreCase(InfoSchemaDb.DATABASE_NAME)) {
-                String clusterName = ClusterNamespace.getClusterNameFromFullName(dbName);
-                fullName = ClusterNamespace.getFullName(clusterName, fullName.toLowerCase());
-                // If the fullname is not valid, ClusterNamespace.getFullName may return null
-                // and fullNameToDb.get(fullName) may throw null pointer exception
-                if (fullName == null) {
-                    return null;
-                }
+                fullName = fullName.toLowerCase();
                 return fullNameToDb.get(fullName);
             }
         }
@@ -414,7 +407,6 @@ public class InternalCatalog implements CatalogIf<Database> {
 
         long id = Env.getCurrentEnv().getNextId();
         Database db = new Database(id, fullDbName);
-        db.setClusterName(SystemInfoService.DEFAULT_CLUSTER);
         // check and analyze database properties before create database
         db.setDbProperties(new DatabaseProperty(properties));
 
@@ -798,10 +790,6 @@ public class InternalCatalog implements CatalogIf<Database> {
             db = fullNameToDb.get(fullDbName);
             if (db == null) {
                 ErrorReport.reportDdlException(ErrorCode.ERR_BAD_DB_ERROR, fullDbName);
-            }
-
-            if (db.getDbState() == DbState.LINK || db.getDbState() == DbState.MOVE) {
-                ErrorReport.reportDdlException(ErrorCode.ERR_CLUSTER_RENAME_DB_ERR, fullDbName);
             }
             // check if name is already used
             if (fullNameToDb.get(newFullDbName) != null) {
@@ -1534,7 +1522,7 @@ public class InternalCatalog implements CatalogIf<Database> {
         }
         try {
             long partitionId = idGeneratorBuffer.getNextId();
-            Partition partition = createPartitionWithIndices(db.getClusterName(), db.getId(), olapTable.getId(),
+            Partition partition = createPartitionWithIndices(db.getId(), olapTable.getId(),
                     olapTable.getName(), olapTable.getBaseIndexId(), partitionId, partitionName, indexIdToMeta,
                     distributionInfo, dataProperty.getStorageMedium(), singlePartitionDesc.getReplicaAlloc(),
                     singlePartitionDesc.getVersionInfo(), bfColumns, olapTable.getBfFpp(), tabletIdSet,
@@ -1791,7 +1779,7 @@ public class InternalCatalog implements CatalogIf<Database> {
         }
     }
 
-    private Partition createPartitionWithIndices(String clusterName, long dbId, long tableId, String tableName,
+    private Partition createPartitionWithIndices(long dbId, long tableId, String tableName,
             long baseIndexId, long partitionId, String partitionName, Map<Long, MaterializedIndexMeta> indexIdToMeta,
             DistributionInfo distributionInfo, TStorageMedium storageMedium, ReplicaAllocation replicaAlloc,
             Long versionInfo, Set<String> bfColumns, double bfFpp, Set<Long> tabletIdSet, List<Index> indexes,
@@ -1840,7 +1828,7 @@ public class InternalCatalog implements CatalogIf<Database> {
             // create tablets
             int schemaHash = indexMeta.getSchemaHash();
             TabletMeta tabletMeta = new TabletMeta(dbId, tableId, partitionId, indexId, schemaHash, storageMedium);
-            createTablets(clusterName, index, ReplicaState.NORMAL, distributionInfo, version, replicaAlloc, tabletMeta,
+            createTablets(index, ReplicaState.NORMAL, distributionInfo, version, replicaAlloc, tabletMeta,
                     tabletIdSet, idGeneratorBuffer, isStorageMediumSpecified);
 
             boolean ok = false;
@@ -2457,7 +2445,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                             "Database " + db.getFullName() + " create unpartitioned table " + tableName + " increasing "
                                     + totalReplicaNum + " of replica exceeds quota[" + db.getReplicaQuota() + "]");
                 }
-                Partition partition = createPartitionWithIndices(db.getClusterName(), db.getId(), olapTable.getId(),
+                Partition partition = createPartitionWithIndices(db.getId(), olapTable.getId(),
                         olapTable.getName(), olapTable.getBaseIndexId(), partitionId, partitionName,
                         olapTable.getIndexIdToMeta(), partitionDistributionInfo,
                         partitionInfo.getDataProperty(partitionId).getStorageMedium(),
@@ -2533,7 +2521,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                     }
                     Env.getCurrentEnv().getPolicyMgr().checkStoragePolicyExist(partionStoragePolicy);
 
-                    Partition partition = createPartitionWithIndices(db.getClusterName(), db.getId(),
+                    Partition partition = createPartitionWithIndices(db.getId(),
                             olapTable.getId(), olapTable.getName(), olapTable.getBaseIndexId(), entry.getValue(),
                             entry.getKey(), olapTable.getIndexIdToMeta(), partitionDistributionInfo,
                             dataProperty.getStorageMedium(), partitionInfo.getReplicaAllocation(entry.getValue()),
@@ -2728,7 +2716,7 @@ public class InternalCatalog implements CatalogIf<Database> {
     }
 
     @VisibleForTesting
-    public void createTablets(String clusterName, MaterializedIndex index, ReplicaState replicaState,
+    public void createTablets(MaterializedIndex index, ReplicaState replicaState,
             DistributionInfo distributionInfo, long version, ReplicaAllocation replicaAlloc, TabletMeta tabletMeta,
             Set<Long> tabletIdSet, IdGeneratorBuffer idGeneratorBuffer, boolean isStorageMediumSpecified)
             throws DdlException {
@@ -2981,7 +2969,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                 // which is the right behavior.
                 long oldPartitionId = entry.getValue();
                 long newPartitionId = idGeneratorBuffer.getNextId();
-                Partition newPartition = createPartitionWithIndices(db.getClusterName(), db.getId(), copiedTbl.getId(),
+                Partition newPartition = createPartitionWithIndices(db.getId(), copiedTbl.getId(),
                         copiedTbl.getName(), copiedTbl.getBaseIndexId(), newPartitionId, entry.getKey(),
                         copiedTbl.getIndexIdToMeta(), partitionsDistributionInfo.get(oldPartitionId),
                         copiedTbl.getPartitionInfo().getDataProperty(oldPartitionId).getStorageMedium(),

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateTableInfo.java
@@ -39,7 +39,6 @@ import org.apache.doris.catalog.Index;
 import org.apache.doris.catalog.KeysType;
 import org.apache.doris.catalog.PartitionType;
 import org.apache.doris.catalog.Type;
-import org.apache.doris.cluster.ClusterNamespace;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
@@ -233,8 +232,6 @@ public class CreateTableInfo {
             throw new AnalysisException(e.getMessage(), e);
         }
 
-        clusterName = ctx.getClusterName();
-
         // analyze catalog name
         if (Strings.isNullOrEmpty(ctlName)) {
             if (ctx.getCurrentCatalog() != null) {
@@ -253,9 +250,7 @@ public class CreateTableInfo {
 
         // analyze table name
         if (Strings.isNullOrEmpty(dbName)) {
-            dbName = ClusterNamespace.getFullName(clusterName, ctx.getDatabase());
-        } else {
-            dbName = ClusterNamespace.getFullName(clusterName, dbName);
+            dbName = ctx.getDatabase();
         }
 
         if (!Env.getCurrentEnv().getAccessManager().checkTblPriv(ConnectContext.get(), dbName,
@@ -858,7 +853,7 @@ public class CreateTableInfo {
                 catalogColumns, catalogIndexes, engineName,
                 new KeysDesc(keysType, keys, clusterKeysColumnNames, clusterKeysColumnIds),
                 partitionDesc, distributionDesc, Maps.newHashMap(properties), extProperties,
-                comment, addRollups, clusterName, null);
+                comment, addRollups, null);
     }
 
     private static ArrayList<Expr> convertToLegacyAutoPartitionExprs(List<Expression> expressions) {

commit a4bdfbadbc8f1422766c1c2c739a899894bd6a38

[fix](meta) fix meta replay issue when upgrading from v2.0 to master (#28532)
Introduced from #27861

The `dbName` saved in `CreateTableInfo` has `default_cluster` prefix, it should be removed.

Also modify the entry of `getDb` in internal catalog. This is a cover-up plan in case there may still 
db name exist with `default_cluster` prefix.

fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -254,6 +254,8 @@ public class InternalCatalog implements CatalogIf<Database> {
         if (StringUtils.isEmpty(dbName)) {
             return null;
         }
+        // ATTN: this should be removed in v3.0
+        dbName = ClusterNamespace.getNameFromFullName(dbName);
         if (fullNameToDb.containsKey(dbName)) {
             return fullNameToDb.get(dbName);
         } else {
@@ -1261,7 +1263,6 @@ public class InternalCatalog implements CatalogIf<Database> {
     }
 
     public void replayCreateTable(String dbName, Table table) throws MetaNotFoundException {
-
         Database db = this.fullNameToDb.get(dbName);
         try {
             db.createTableWithLock(table, true, false);

fe/fe-core/src/main/java/org/apache/doris/journal/JournalEntity.java
@@ -217,8 +217,7 @@ public class JournalEntity implements Writable {
                 break;
             }
             case OperationType.OP_CREATE_TABLE: {
-                data = new CreateTableInfo();
-                ((CreateTableInfo) data).readFields(in);
+                data = CreateTableInfo.read(in);
                 isRead = true;
                 break;
             }

fe/fe-core/src/main/java/org/apache/doris/persist/AlterDatabasePropertyInfo.java
@@ -17,8 +17,10 @@
 
 package org.apache.doris.persist;
 
+import org.apache.doris.cluster.ClusterNamespace;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonPostProcessable;
 import org.apache.doris.persist.gson.GsonUtils;
 
 import com.google.gson.annotations.SerializedName;
@@ -28,7 +30,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.Map;
 
-public class AlterDatabasePropertyInfo implements Writable {
+public class AlterDatabasePropertyInfo implements Writable, GsonPostProcessable {
     @SerializedName(value = "dbId")
     private long dbId;
 
@@ -37,12 +39,6 @@ public class AlterDatabasePropertyInfo implements Writable {
 
     @SerializedName(value = "properties")
     private Map<String, String> properties;
-
-    public AlterDatabasePropertyInfo() {
-        // for persist
-        this.dbName = "";
-        this.properties = null;
-    }
 
     public AlterDatabasePropertyInfo(long dbId, String dbName, Map<String, String> properties) {
         this.dbId = dbId;
@@ -73,5 +69,10 @@ public class AlterDatabasePropertyInfo implements Writable {
 
     public String toJson() {
         return GsonUtils.GSON.toJson(this);
+    }
+
+    @Override
+    public void gsonPostProcess() throws IOException {
+        dbName = ClusterNamespace.getNameFromFullName(dbName);
     }
 }

fe/fe-core/src/main/java/org/apache/doris/persist/CreateTableInfo.java
@@ -18,8 +18,10 @@
 package org.apache.doris.persist;
 
 import org.apache.doris.catalog.Table;
+import org.apache.doris.cluster.ClusterNamespace;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.io.Writable;
+import org.apache.doris.persist.gson.GsonPostProcessable;
 import org.apache.doris.persist.gson.GsonUtils;
 
 import com.google.gson.annotations.SerializedName;
@@ -31,7 +33,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.Objects;
 
-public class CreateTableInfo implements Writable {
+public class CreateTableInfo implements Writable, GsonPostProcessable {
     public static final Logger LOG = LoggerFactory.getLogger(CreateTableInfo.class);
 
     @SerializedName(value = "dbName")
@@ -61,15 +63,15 @@ public class CreateTableInfo implements Writable {
         table.write(out);
     }
 
-    public void readFields(DataInput in) throws IOException {
-        dbName = Text.readString(in);
-        table = Table.read(in);
-    }
-
     public static CreateTableInfo read(DataInput in) throws IOException {
         CreateTableInfo createTableInfo = new CreateTableInfo();
         createTableInfo.readFields(in);
         return createTableInfo;
+    }
+
+    private void readFields(DataInput in) throws IOException {
+        dbName = ClusterNamespace.getNameFromFullName(Text.readString(in));
+        table = Table.read(in);
     }
 
     @Override
@@ -98,5 +100,10 @@ public class CreateTableInfo implements Writable {
     @Override
     public String toString() {
         return toJson();
+    }
+
+    @Override
+    public void gsonPostProcess() throws IOException {
+        dbName = ClusterNamespace.getNameFromFullName(dbName);
     }
 }

fe/fe-core/src/main/java/org/apache/doris/persist/meta/PersistMetaModules.java
@@ -43,7 +43,7 @@ public class PersistMetaModules {
 
     // Modules in this list is deprecated and will not be saved in meta file. (also should not be in MODULE_NAMES)
     public static final ImmutableList<String> DEPRECATED_MODULE_NAMES = ImmutableList.of(
-            "loadJob", "cooldownJob", "AnalysisMgr", "mtmvJobManager");
+            "loadJob", "cooldownJob", "AnalysisMgr", "mtmvJobManager", "JobTaskManager");
 
     static {
         MODULES_MAP = Maps.newHashMap();