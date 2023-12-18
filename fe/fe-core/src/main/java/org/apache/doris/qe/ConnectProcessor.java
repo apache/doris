f6ec05a19c50d3b26dcc689d9bf41057a9ce5b9c
zhangdong <493738387@qq.com>
2023-12-18 19:26:24 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectProcessor.java
@@ -179,7 +179,7 @@ public abstract class ConnectProcessor {
                 .setSqlHash(ctx.getSqlHash());
 
         List<StatementBase> stmts = null;
-
+        Exception nereidsParseException = null;
         // Nereids do not support prepare and execute now, so forbid prepare command, only process query command
         if (mysqlCommand == MysqlCommand.COM_QUERY && ctx.getSessionVariable().isEnableNereidsPlanner()) {
             try {
@@ -192,6 +192,7 @@ public abstract class ConnectProcessor {
                 // TODO: We should catch all exception here until we support all query syntax.
                 LOG.debug("Nereids parse sql failed. Reason: {}. Statement: \"{}\".",
                         e.getMessage(), originStmt);
+                nereidsParseException = e;
             }
         }
 
@@ -200,6 +201,12 @@ public abstract class ConnectProcessor {
             try {
                 stmts = parse(originStmt);
             } catch (Throwable throwable) {
+                // if NereidsParser and oldParser both failed,
+                // prove is a new feature implemented only on the nereids,
+                // so an error message for the new nereids is thrown
+                if (nereidsParseException != null) {
+                    throwable = nereidsParseException;
+                }
                 // Parse sql failed, audit it and return
                 handleQueryException(throwable, originStmt, null, null);
                 return;
