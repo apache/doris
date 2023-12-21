db821f26f56aec3a811c04035fc1ab48863531a5
ci <ci@selectdb.com>
2023-12-21 20:10:50 +0800

    1221

--- regression-test/pipeline/performance/run-clickbench.sh
+++ regression-test/pipeline/performance/run-clickbench.sh
@@ -105,7 +105,6 @@ set global parallel_fragment_exec_instance_num=16;
 set global parallel_pipeline_task_num=16;
 set global enable_single_distinct_column_opt=true;
 set global enable_function_pushdown=true;
-set global enable_local_exchange=true;
 set global forbid_unknown_col_stats=false;
 set global runtime_filter_mode=global;
 "
