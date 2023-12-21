42290f553c99c5f91e708ec11dc41973c44bbb81
yiguolei <yiguolei@gmail.com>
2023-12-21 17:35:59 +0800

    f

--- be/src/vec/exec/scan/scanner_context.h
+++ be/src/vec/exec/scan/scanner_context.h
@@ -176,8 +176,7 @@ protected:
                    const std::list<std::shared_ptr<ScannerDelegate>>& scanners_, int64_t limit_,
                    int64_t max_bytes_in_blocks_queue_, const int num_parallel_instances,
                    pipeline::ScanLocalStateBase* local_state,
-                   std::shared_ptr<pipeline::ScanDependency> dependency,
-                   std::shared_ptr<pipeline::Dependency> finish_dependency);
+                   std::shared_ptr<pipeline::ScanDependency> dependency);
     virtual void _dispose_coloate_blocks_not_in_queue() {}
 
     void _set_scanner_done();
