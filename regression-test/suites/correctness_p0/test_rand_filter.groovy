apache/doris

commit 0f5325c50062e94be800c1158f8e9bef28b2574e

[fix](planner)should bind expr using no slot to correct tuple (#28656)

fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
@@ -1335,6 +1335,14 @@ public class Analyzer {
     public void registerConjuncts(Expr e, boolean fromHavingClause, List<TupleId> ids) throws AnalysisException {
         for (Expr conjunct : e.getConjuncts()) {
             registerConjunct(conjunct);
+            if (!e.isConstant()) {
+                ArrayList<TupleId> tupleIds = Lists.newArrayList();
+                ArrayList<SlotId> slotIds = Lists.newArrayList();
+                e.getIds(tupleIds, slotIds);
+                if (tupleIds.isEmpty() && slotIds.isEmpty()) {
+                    e.setBoundTupleIds(ids);
+                }
+            }
             if (ids != null) {
                 for (TupleId id : ids) {
                     registerConstantConjunct(id, conjunct);

regression-test/suites/correctness_p0/test_rand_filter.groovy
@@ -1,0 +1,36 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_rand_filter") {
+    sql"""set enable_nereids_planner=false;"""
+    sql """ DROP TABLE IF EXISTS test_rand_filter_t """
+    sql """
+            CREATE TABLE  test_rand_filter_t (
+                a int
+            )
+            DUPLICATE KEY(a)
+            DISTRIBUTED BY HASH(a) BUCKETS 1
+            PROPERTIES (
+              "replication_num" = "1"
+            )
+        """
+    explain {
+        sql("""select * from test_rand_filter_t where rand() < 0.5 union all select * from test_rand_filter_t where rand() > 0.3;""")
+        notContains("AND")
+    }
+    sql """ DROP TABLE IF EXISTS test_rand_filter_t """
+}

fe/fe-core/src/main/java/org/apache/doris/analysis/Expr.java
@@ -298,6 +298,8 @@ public abstract class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
     protected boolean printSqlInParens = false;
     protected Optional<String> exprName = Optional.empty();
 
+    protected List<TupleId> boundTupleIds = null;
+
     protected Expr() {
         super();
         type = Type.INVALID;
@@ -1293,12 +1295,19 @@ public abstract class Expr extends TreeNode<Expr> implements ParseNode, Cloneabl
      * Returns true if expr is fully bound by tids, otherwise false.
      */
     public boolean isBoundByTupleIds(List<TupleId> tids) {
+        if (boundTupleIds != null && !boundTupleIds.isEmpty()) {
+            return boundTupleIds.stream().anyMatch(id -> tids.contains(id));
+        }
         for (Expr child : children) {
             if (!child.isBoundByTupleIds(tids)) {
                 return false;
             }
         }
         return true;
+    }
+
+    public void setBoundTupleIds(List<TupleId> tids) {
+        boundTupleIds = tids;
     }
 
 

commit 2aaa577371f0a2c9f6f0f3249a17f1aa381f5cc2

[fix](planner)fix bug of bound conjunct to wrong tuple (#28811)
this fix bug introduced by #28656

fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
@@ -1335,12 +1335,12 @@ public class Analyzer {
     public void registerConjuncts(Expr e, boolean fromHavingClause, List<TupleId> ids) throws AnalysisException {
         for (Expr conjunct : e.getConjuncts()) {
             registerConjunct(conjunct);
-            if (!e.isConstant()) {
+            if (!conjunct.isConstant()) {
                 ArrayList<TupleId> tupleIds = Lists.newArrayList();
                 ArrayList<SlotId> slotIds = Lists.newArrayList();
-                e.getIds(tupleIds, slotIds);
+                conjunct.getIds(tupleIds, slotIds);
                 if (tupleIds.isEmpty() && slotIds.isEmpty()) {
-                    e.setBoundTupleIds(ids);
+                    conjunct.setBoundTupleIds(ids);
                 }
             }
             if (ids != null) {

regression-test/suites/correctness_p0/test_rand_filter.groovy
@@ -32,5 +32,11 @@ suite("test_rand_filter") {
         sql("""select * from test_rand_filter_t where rand() < 0.5 union all select * from test_rand_filter_t where rand() > 0.3;""")
         notContains("AND")
     }
+    explain {
+        sql("""select * from test_rand_filter_t 
+                union all (select * from test_rand_filter_t where rand() < 0.3) 
+                union all (select * from test_rand_filter_t where a > 5 and rand() < 0.4);""")
+        notContains("rand() < 0.3 AND rand() < 0.4")
+    }
     sql """ DROP TABLE IF EXISTS test_rand_filter_t """
 }