b2b5ba11abdc2876ba8d5b8d5ff53bcc7e11374d
deardeng <565620795@qq.com>
2023-12-21 10:23:14 +0800

    [fix](partition) Add more log for single replica load when partition id eq 0 (#28639)

--- be/src/olap/txn_manager.cpp
+++ be/src/olap/txn_manager.cpp
@@ -335,8 +335,15 @@ Status TxnManager::commit_txn(OlapMeta* meta, TPartitionId partition_id,
     // save meta need access disk, it maybe very slow, so that it is not in global txn lock
     // it is under a single txn lock
     if (!is_recovery) {
-        Status save_status = RowsetMetaManager::save(meta, tablet_uid, rowset_ptr->rowset_id(),
-                                                     rowset_ptr->rowset_meta()->get_rowset_pb());
+        auto rs_pb = rowset_ptr->rowset_meta()->get_rowset_pb();
+        // TODO(dx): remove log after fix partition id eq 0 bug
+        if (!rs_pb.has_partition_id() || rs_pb.partition_id() == 0) {
+            rs_pb.set_partition_id(partition_id);
+            LOG(WARNING) << "cant get partition id from rs pb, get from func arg partition_id="
+                         << partition_id;
+        }
+        Status save_status =
+                RowsetMetaManager::save(meta, tablet_uid, rowset_ptr->rowset_id(), rs_pb);
         DBUG_EXECUTE_IF("TxnManager.RowsetMetaManager.save_wait", {
             if (auto wait = dp->param<int>("duration", 0); wait > 0) {
                 LOG_WARNING("TxnManager.RowsetMetaManager.save_wait").tag("wait ms", wait);
