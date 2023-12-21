25e90bdf2f0572c34b99f6429b165d44dbcb9715
yujun <yu.jun.reach@gmail.com>
2023-12-21 11:10:08 +0800

    fix publish version fail but return ok

--- be/src/olap/storage_engine.cpp
+++ be/src/olap/storage_engine.cpp
@@ -1233,14 +1233,15 @@ void StorageEngine::notify_listeners() {
 }
 
 bool StorageEngine::notify_listener(TaskWorkerPool::TaskWorkerType task_worker_type) {
+    bool found = false;
     std::lock_guard<std::mutex> l(_report_mtx);
     for (auto& listener : _report_listeners) {
         if (listener->task_worker_type() == task_worker_type) {
             listener->notify_thread();
-            return true;
+            found = true;
         }
     }
-    return false;
+    return found;
 }
 
 Status StorageEngine::execute_task(EngineTask* task) {
