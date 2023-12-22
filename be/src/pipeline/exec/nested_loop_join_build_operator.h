9ba1c303f7efe11ace8e34e7dbbdf42fcc222560
Gabriel <gabrielleebuaa@gmail.com>
2023-12-22 11:10:50 +0800

    [pipelineX](refactor) rename functions

--- be/src/pipeline/exec/nested_loop_join_build_operator.h
+++ be/src/pipeline/exec/nested_loop_join_build_operator.h
@@ -102,7 +102,7 @@ public:
     Status sink(RuntimeState* state, vectorized::Block* in_block,
                 SourceState source_state) override;
 
-    DataDistribution get_local_exchange_type() const override {
+    DataDistribution required_data_distribution() const override {
         if (_join_op == TJoinOp::NULL_AWARE_LEFT_ANTI_JOIN) {
             return {ExchangeType::NOOP};
         }
