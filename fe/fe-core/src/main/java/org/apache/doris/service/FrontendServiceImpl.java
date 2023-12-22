9b2ad8be2b1e8e0fd2394cad87c005c6ead5335e
huanghaibin <284824253@qq.com>
2023-12-23 02:31:50 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
@@ -241,7 +241,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
