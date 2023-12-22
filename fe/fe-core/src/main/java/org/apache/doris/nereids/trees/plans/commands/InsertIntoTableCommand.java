57980712b9457314a432ffaf97dbe42a31e48025
morrySnow <101034200+morrySnow@users.noreply.github.com>
2023-12-22 11:06:07 +0800

    [fix](Nereids) explain should fallback too if Nereids is not enable (#28475) (#28574)
    pick from master
    PR #28475
    commit b50bc0d2c902856f6727c5301d256ca6a561f8a3

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/InsertIntoTableCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/InsertIntoTableCommand.java
@@ -160,6 +160,14 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
 
     @Override
     public Plan getExplainPlan(ConnectContext ctx) {
+        if (!ctx.getSessionVariable().isEnableNereidsDML()) {
+            try {
+                ctx.getSessionVariable().enableFallbackToOriginalPlannerOnce();
+            } catch (Exception e) {
+                throw new AnalysisException("failed to set fallback to original planner to true", e);
+            }
+            throw new AnalysisException("Nereids DML is disabled, will try to fall back to the original planner");
+        }
         return this.logicalQuery;
     }
 
