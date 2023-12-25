e6ab73b97de0291ea231bab431f5e3a15ec7e522
HappenLee <happenlee@hotmail.com>
2023-12-25 20:15:08 +0800

    [Opt](join) opt the join copy column from build side

--- be/src/vec/columns/column_decimal.h
+++ be/src/vec/columns/column_decimal.h
@@ -124,11 +124,16 @@ public:
         auto origin_size = size();
         auto new_size = indices_end - indices_begin;
         data.resize(origin_size + new_size);
-        const T* __restrict src_data = reinterpret_cast<const T*>(src.get_raw_data().data);
 
-        for (uint32_t i = 0; i < new_size; ++i) {
-            data[origin_size + i] = src_data[indices_begin[i]];
-        }
+        auto copy = [](const T* __restrict src, T* __restrict dest,
+                       const uint32_t* __restrict begin, const uint32_t* __restrict end) {
+            for (auto it = begin; it != end; ++it) {
+                *dest = src[*it];
+                ++dest;
+            }
+        };
+        copy(reinterpret_cast<const T*>(src.get_raw_data().data), data.data() + origin_size,
+             indices_begin, indices_end);
     }
 
     void insert_many_fix_len_data(const char* data_ptr, size_t num) override;
