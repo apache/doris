fc1675735099926889c01b38181da9b5e54cdb14
Guangming Lu <lgm0862@qq.com>
2023-12-26 11:21:12 +0800

    [Enhancement] support user custom defined privite settings,xml for build fe #29014

--- build.sh
+++ build.sh
@@ -584,7 +584,7 @@ fi
 
 # Allowed user customer set env param USER_SETTINGS_MVN_REPO means settings.xml file path
 declare MVN_PRIVITE_PENDING=""
-if [[ -f ${USER_SETTINGS_MVN_REPO} ]]; then
+if [[ -n ${USER_SETTINGS_MVN_REPO} && -f ${USER_SETTINGS_MVN_REPO} ]]; then
     MVN_PRIVITE_PENDING="-gs ${USER_SETTINGS_MVN_REPO}"
 fi
 
@@ -596,9 +596,9 @@ if [[ "${FE_MODULES}" != '' ]]; then
         clean_fe
     fi
     if [[ "${DISABLE_JAVA_CHECK_STYLE}" = "ON" ]]; then
-        "${MVN_CMD}" package -pl ${FE_MODULES:+${FE_MODULES}} -Dskip.doc=true -DskipTests -Dcheckstyle.skip=true ${MVN_OPT:+${MVN_OPT}} ${MVN_PRIVITE_PENDING}
+        "${MVN_CMD}" package -pl ${FE_MODULES:+${FE_MODULES}} -Dskip.doc=true -DskipTests -Dcheckstyle.skip=true ${MVN_OPT:+${MVN_OPT}} "${MVN_PRIVITE_PENDING}"
     else
-        "${MVN_CMD}" package -pl ${FE_MODULES:+${FE_MODULES}} -Dskip.doc=true -DskipTests ${MVN_OPT:+${MVN_OPT}} ${MVN_PRIVITE_PENDING}
+        "${MVN_CMD}" package -pl ${FE_MODULES:+${FE_MODULES}} -Dskip.doc=true -DskipTests ${MVN_OPT:+${MVN_OPT}} "${MVN_PRIVITE_PENDING}"
     fi
     cd "${DORIS_HOME}"
 fi
