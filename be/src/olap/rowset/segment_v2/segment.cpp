246a0e7d0a07bd7c767be6333744faecdb1953dd
Luwei <814383175@qq.com>
2023-12-27 20:05:23 +0800

    fix out of range

--- be/src/olap/rowset/segment_v2/segment.cpp
+++ be/src/olap/rowset/segment_v2/segment.cpp
@@ -510,16 +510,6 @@ Status Segment::new_column_iterator_with_path(const TabletColumn& tablet_column,
 Status Segment::new_column_iterator(const TabletColumn& tablet_column,
                                     std::unique_ptr<ColumnIterator>* iter,
                                     const StorageReadOptions* opt) {
-    if (config::enable_column_type_check &&
-        tablet_column.type() != _column_readers.at(tablet_column.unique_id())->get_meta_type()) {
-        LOG(WARNING) << "different type between schema and column reader,"
-                     << " column schema name: " << tablet_column.name()
-                     << " column schema type: " << int(tablet_column.type())
-                     << " column reader meta type"
-                     << int(_column_readers.at(tablet_column.unique_id())->get_meta_type());
-        return Status::InternalError("different type between schema and column reader");
-    }
-
     // init column iterator by path info
     if (!tablet_column.path_info().empty() || tablet_column.is_variant_type()) {
         return new_column_iterator_with_path(tablet_column, iter, opt);
@@ -533,6 +523,16 @@ Status Segment::new_column_iterator(const TabletColumn& tablet_column,
     ColumnIterator* it;
     RETURN_IF_ERROR(_column_readers.at(tablet_column.unique_id())->new_iterator(&it));
     iter->reset(it);
+
+    if (config::enable_column_type_check &&
+        tablet_column.type() != _column_readers.at(tablet_column.unique_id())->get_meta_type()) {
+        LOG(WARNING) << "different type between schema and column reader,"
+                     << " column schema name: " << tablet_column.name()
+                     << " column schema type: " << int(tablet_column.type())
+                     << " column reader meta type"
+                     << int(_column_readers.at(tablet_column.unique_id())->get_meta_type());
+        return Status::InternalError("different type between schema and column reader");
+    }
     return Status::OK();
 }
 
