8fbbb90b257823f218a4d0c5525ae3152f2d271e
Calvin Kirs <acm_master@163.com>
2023-12-27 17:03:25 +0800

    [Improve](Job)Create task adds concurrency control

--- fe/fe-core/src/main/java/org/apache/doris/job/extensions/insert/InsertJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/extensions/insert/InsertJob.java
@@ -192,7 +192,7 @@ public class InsertJob extends AbstractJob<InsertTask, Map<Object, Object>> {
                      Long createTimeMs,
                      String executeSql) {
         super(getNextJobId(), jobName, jobStatus, dbName, comment, createUser,
-                jobConfig, createTimeMs, executeSql, null);
+                jobConfig, createTimeMs, executeSql);
         this.dbId = ConnectContext.get().getCurrentDbId();
     }
 
