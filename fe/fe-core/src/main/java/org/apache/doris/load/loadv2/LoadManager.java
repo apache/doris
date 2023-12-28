66fd9b11ca34af270415502ad1d982b867eba87e
caiconghui <55968745+caiconghui@users.noreply.github.com>
2023-12-28 14:17:35 +0800

    [fix](DatabaseTransactionMgr) Fix clean label bug which may cause inconsitent editlog operation (#29198)

--- fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadManager.java
@@ -786,22 +786,16 @@ public class LoadManager implements Writable {
         } finally {
             writeUnlock();
         }
-        LOG.info("clean {} labels on db {} with label '{}' in load mgr.", counter, dbId, label);
-
         // 2. Remove from DatabaseTransactionMgr
         try {
-            Env.getCurrentGlobalTransactionMgr().cleanLabel(dbId, label);
+            Env.getCurrentGlobalTransactionMgr().cleanLabel(dbId, label, isReplay);
         } catch (AnalysisException e) {
             // just ignore, because we don't want to throw any exception here.
             LOG.warn("Exception:", e);
         }
 
-        // 3. Log
-        if (!isReplay) {
-            CleanLabelOperationLog log = new CleanLabelOperationLog(dbId, label);
-            Env.getCurrentEnv().getEditLog().logCleanLabel(log);
-        }
-        LOG.info("finished to clean label on db {} with label {}. is replay: {}", dbId, label, isReplay);
+        LOG.info("finished to clean {} labels on db {} with label '{}' in load mgr. is replay: {}",
+                counter, dbId, label, isReplay);
     }
 
     private void readLock() {
