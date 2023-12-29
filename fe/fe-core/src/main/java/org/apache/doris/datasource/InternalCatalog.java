1a2ec2fbd92cf1e211a626cda7cac61b0b1cc8dc
Vallish Pai <vallishpai@gmail.com>
2023-12-29 13:58:04 +0530

    [fix](meta)Fix  error code for unknown table #27751 (#27976)
    This fix will improve the error code for unknown table
    exception cases.

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -912,7 +912,7 @@ public class InternalCatalog implements CatalogIf<Database> {
             Env.getCurrentEnv().getAnalysisManager().removeTableStats(table.getId());
             Env.getCurrentEnv().getMtmvService().dropTable(table);
         } catch (UserException e) {
-            throw new DdlException(e.getMessage(), e);
+            throw new DdlException(e.getMessage(), e.getMysqlErrorCode());
         } finally {
             db.writeUnlock();
         }
