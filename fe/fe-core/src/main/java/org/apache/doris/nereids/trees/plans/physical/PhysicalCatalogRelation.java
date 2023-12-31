dfa739e08c51ce9881b739f4a7663cdcc3a3429f
englefly <englefly@gmail.com>
2023-12-31 13:31:58 +0800

    expand_runtime_filter_by_inner_join will create some redundant rfs. Tpch q5 and q9

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalCatalogRelation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalCatalogRelation.java
@@ -138,6 +138,7 @@ public abstract class PhysicalCatalogRelation extends PhysicalRelation implement
             getAppliedRuntimeFilters()
                     .stream().forEach(rf -> shapeBuilder.append(" RF").append(rf.getId().asInt()));
         }
+        //shapeBuilder.append("jump: ").append(getMutableState(MutableState.KEY_RF_JUMP));
         return shapeBuilder.toString();
     }
 }
