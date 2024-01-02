556004a30b3676d6d03bd0b06b4ba9a7cf540b83
stephen <hello-stephen@qq.com>
2024-01-02 22:11:12 +0800

    2

--- regression-test/pipeline/performance/compile.sh
+++ regression-test/pipeline/performance/compile.sh
@@ -136,6 +136,7 @@ sudo docker run -i --rm \
                     && export CUSTOM_NPM_REGISTRY=https://registry.npmjs.org \
                     && bash build.sh --fe --be --clean 2>&1 | tee build.log"
 set +x
+set -x
 succ_symble="Successfully build Doris"
 if [[ -d outout ]] && grep "${succ_symble}" "${teamcity_build_checkoutDir}"/build.log; then
     echo "INFO: ${succ_symble}"
