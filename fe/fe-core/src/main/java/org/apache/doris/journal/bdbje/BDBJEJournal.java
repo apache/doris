8740ecdd35b16f0af952f47f51b4a3493b8ef402
Kang <kxiao.tiger@gmail.com>
2024-01-02 08:31:24 +0800

    Revert "[fix](journal) Fix infinite block due to initial BDB journal failed (#29205) (#29310)"
    This reverts commit d23a1171d7d53d67a90d9182b3b03a1f725ba6b8.

--- fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
+++ fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -276,15 +276,6 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
 
     @Override
     public long getMaxJournalId() {
-        return getMaxJournalIdInternal(true);
-    }
-
-    // get max journal id but do not check whether the txn is matched.
-    private long getMaxJournalIdWithoutCheck() {
-        return getMaxJournalIdInternal(false);
-    }
-
-    private long getMaxJournalIdInternal(boolean checkTxnMatched) {
         long ret = -1;
         if (bdbEnvironment == null) {
             return ret;
@@ -301,7 +292,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
         String dbName = dbNames.get(index).toString();
         long dbNumberName = dbNames.get(index);
         Database database = bdbEnvironment.openDatabase(dbName);
-        if (checkTxnMatched && !isReplicaTxnAreMatched(database, dbNumberName)) {
+        if (!isReplicaTxnAreMatched(database, dbNumberName)) {
             LOG.warn("The current replica hasn't synced up with the master, current db name: {}", dbNumberName);
             if (index != 0) {
                 // Because roll journal occurs after write, the previous write must have
@@ -432,7 +423,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
                 }
 
                 // set next journal id
-                nextJournalId.set(getMaxJournalIdWithoutCheck() + 1);
+                nextJournalId.set(getMaxJournalId() + 1);
 
                 break;
             } catch (InsufficientLogException insufficientLogEx) {
