829a970a27abd5791cfe8d42ef0039ef7c994507
HHoflittlefish777 <77738092+HHoflittlefish777@users.noreply.github.com>
2024-01-04 23:03:35 +0800

    [fix](move-memtable) disable move memtable when light schema change is false (#29362)

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -79,6 +79,7 @@ import org.apache.doris.analysis.UseStmt;
 import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
+import org.apache.doris.catalog.OlapTable;
 import org.apache.doris.catalog.PrimitiveType;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.Table;
@@ -1996,6 +1997,13 @@ public class StmtExecutor {
 
                 QeProcessorImpl.INSTANCE.registerQuery(context.queryId(), coord);
 
+                Table table = insertStmt.getTargetTable();
+                if (table instanceof OlapTable) {
+                    boolean isEnableMemtableOnSinkNode =
+                            ((OlapTable) table).getTableProperty().getUseSchemaLightChange()
+                            ? coord.getQueryOptions().isEnableMemtableOnSinkNode() : false;
+                    coord.getQueryOptions().setEnableMemtableOnSinkNode(isEnableMemtableOnSinkNode);
+                }
                 coord.exec();
                 int execTimeout = context.getExecTimeout();
                 LOG.debug("Insert {} execution timeout:{}", DebugUtil.printId(context.queryId()), execTimeout);
