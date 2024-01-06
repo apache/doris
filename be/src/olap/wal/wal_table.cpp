1088637432af29214e765cc32fe027df2c501b12
huanghaibin <284824253@qq.com>
2024-01-06 21:04:47 +0800

    edit4

--- be/src/olap/wal/wal_table.cpp
+++ be/src/olap/wal/wal_table.cpp
@@ -351,8 +351,7 @@ Status WalTable::_get_column_info(int64_t db_id, int64_t tb_id,
 Status WalTable::_read_wal_header(const std::string& wal_path, std::string& columns) {
     std::shared_ptr<doris::WalReader> wal_reader = std::make_shared<WalReader>(wal_path);
     RETURN_IF_ERROR(wal_reader->init());
-    uint32_t version = 0;
-    RETURN_IF_ERROR(wal_reader->read_header(version, columns));
+    RETURN_IF_ERROR(wal_reader->read_header(columns));
     RETURN_IF_ERROR(wal_reader->finalize());
     return Status::OK();
 }
