74bf7911189111fdeaa299ea881c1d0b8f8781eb
HappenLee <happenlee@hotmail.com>
2024-01-09 19:19:44 +0800

    [Performance](exec) opt the exchange performance (#29579)

--- be/src/vec/data_types/data_type_decimal.cpp
+++ be/src/vec/data_types/data_type_decimal.cpp
@@ -22,6 +22,7 @@
 
 #include <fmt/format.h>
 #include <gen_cpp/data.pb.h>
+#include <streamvbyte.h>
 #include <string.h>
 
 #include <utility>
@@ -105,35 +106,86 @@ Status DataTypeDecimal<T>::from_string(ReadBuffer& rb, IColumn* column) const {
 template <typename T>
 int64_t DataTypeDecimal<T>::get_uncompressed_serialized_bytes(const IColumn& column,
                                                               int be_exec_version) const {
-    return sizeof(uint32_t) + column.size() * sizeof(FieldType);
+    if (be_exec_version >= 4) {
+        auto size = sizeof(T) * column.size();
+        if (size <= SERIALIZED_MEM_SIZE_LIMIT) {
+            return sizeof(uint32_t) + size;
+        } else {
+            return sizeof(uint32_t) + sizeof(size_t) +
+                   std::max(size, streamvbyte_max_compressedbytes(upper_int32(size)));
+        }
+    } else {
+        return sizeof(uint32_t) + column.size() * sizeof(FieldType);
+    }
 }
 
 template <typename T>
 char* DataTypeDecimal<T>::serialize(const IColumn& column, char* buf, int be_exec_version) const {
-    // row num
-    const auto row_num = column.size();
-    *reinterpret_cast<uint32_t*>(buf) = row_num;
-    buf += sizeof(uint32_t);
-    // column values
-    auto ptr = column.convert_to_full_column_if_const();
-    const auto* origin_data = assert_cast<const ColumnType&>(*ptr.get()).get_data().data();
-    memcpy(buf, origin_data, row_num * sizeof(FieldType));
-    buf += row_num * sizeof(FieldType);
-    return buf;
+    if (be_exec_version >= 4) {
+        // row num
+        const auto mem_size = column.size() * sizeof(T);
+        *reinterpret_cast<uint32_t*>(buf) = mem_size;
+        buf += sizeof(uint32_t);
+        // column data
+        auto ptr = column.convert_to_full_column_if_const();
+        const auto* origin_data =
+                assert_cast<const ColumnDecimal<T>&>(*ptr.get()).get_data().data();
+        if (mem_size <= SERIALIZED_MEM_SIZE_LIMIT) {
+            memcpy(buf, origin_data, mem_size);
+            return buf + mem_size;
+        }
+
+        auto encode_size =
+                streamvbyte_encode(reinterpret_cast<const uint32_t*>(origin_data),
+                                   upper_int32(mem_size), (uint8_t*)(buf + sizeof(size_t)));
+        *reinterpret_cast<size_t*>(buf) = encode_size;
+        buf += sizeof(size_t);
+        return buf + encode_size;
+    } else {
+        // row num
+        const auto row_num = column.size();
+        *reinterpret_cast<uint32_t*>(buf) = row_num;
+        buf += sizeof(uint32_t);
+        // column values
+        auto ptr = column.convert_to_full_column_if_const();
+        const auto* origin_data = assert_cast<const ColumnType&>(*ptr.get()).get_data().data();
+        memcpy(buf, origin_data, row_num * sizeof(FieldType));
+        buf += row_num * sizeof(FieldType);
+        return buf;
+    }
 }
 
 template <typename T>
 const char* DataTypeDecimal<T>::deserialize(const char* buf, IColumn* column,
                                             int be_exec_version) const {
-    // row num
-    uint32_t row_num = *reinterpret_cast<const uint32_t*>(buf);
-    buf += sizeof(uint32_t);
-    // column values
-    auto& container = assert_cast<ColumnType*>(column)->get_data();
-    container.resize(row_num);
-    memcpy(container.data(), buf, row_num * sizeof(FieldType));
-    buf += row_num * sizeof(FieldType);
-    return buf;
+    if (be_exec_version >= 4) {
+        // row num
+        uint32_t mem_size = *reinterpret_cast<const uint32_t*>(buf);
+        buf += sizeof(uint32_t);
+        // column data
+        auto& container = assert_cast<ColumnDecimal<T>*>(column)->get_data();
+        container.resize(mem_size / sizeof(T));
+        if (mem_size <= SERIALIZED_MEM_SIZE_LIMIT) {
+            memcpy(container.data(), buf, mem_size);
+            return buf + mem_size;
+        }
+
+        size_t encode_size = *reinterpret_cast<const size_t*>(buf);
+        buf += sizeof(size_t);
+        streamvbyte_decode((const uint8_t*)buf, (uint32_t*)(container.data()),
+                           upper_int32(mem_size));
+        return buf + encode_size;
+    } else {
+        // row num
+        uint32_t row_num = *reinterpret_cast<const uint32_t*>(buf);
+        buf += sizeof(uint32_t);
+        // column values
+        auto& container = assert_cast<ColumnType*>(column)->get_data();
+        container.resize(row_num);
+        memcpy(container.data(), buf, row_num * sizeof(FieldType));
+        buf += row_num * sizeof(FieldType);
+        return buf;
+    }
 }
 
 template <typename T>
