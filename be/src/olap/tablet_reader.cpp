24fd323c128c8c49b59c37c4b4df6baf45bcffd0
liaoxin <liaoxinbit@126.com>
2024-01-09 21:41:06 +0800

    fix comment

--- be/src/olap/tablet_reader.cpp
+++ be/src/olap/tablet_reader.cpp
@@ -214,8 +214,14 @@ Status TabletReader::_capture_rs_readers(const ReaderParams& read_params) {
             // unique keys with merge on write, no need to merge sort keys in rowset
             need_ordered_result = false;
         }
-        if (_aggregation || _direct_mode) {
+        if (_aggregation) {
             // compute engine will aggregate rows with the same key,
+            // it's ok for rowset to return unordered result
+            need_ordered_result = false;
+        }
+
+        if (_direct_mode) {
+            // direct mode indicates that the storage layer does not need to merge,
             // it's ok for rowset to return unordered result
             need_ordered_result = false;
         }
