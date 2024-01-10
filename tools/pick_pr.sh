8bc0ee3f6a4180af480a1a9dc76c67f0613e3754
Kang <kxiao.tiger@gmail.com>
2024-01-10 12:16:25 +0800

    format

--- tools/pick_pr.sh
+++ tools/pick_pr.sh
@@ -15,89 +15,78 @@ fi
 doris_repo='apache/doris'
 
 branch_from=$1
-remote_from=$(echo $branch_from | awk -F/ '{print $1}')
+remote_from=$(echo "${branch_from}" | awk -F/ '{print $1}')
 
 branch_to=$2
-branch_to_name=$(echo $branch_to | awk -F/ '{print $2}')
-remote_to=$(echo $branch_to | awk -F/ '{print $1}')
+branch_to_name=$(echo "${branch_to}" | awk -F/ '{print $2}')
+remote_to=$(echo "${branch_to}" | awk -F/ '{print $1}')
 
 push_remote=$3
-push_url=$(git remote get-url --push $push_remote)
+push_url=$(git remote get-url --push "${push_remote}")
 # https://github.com/your_name/doris.git
-push_url=$(echo $push_url | sed 's|^https://github.com/||')
+push_url=$(echo "${push_url}" | sed 's|^https://github.com/||')
 # git@github.com:your_name/doris.git
-push_url=$(echo $push_url | sed 's|^git@github.com:||')
+push_url=$(echo "${push_url}" | sed 's|^git@github.com:||')
 # get your_name
-push_id=$(echo $push_url | awk -F/ '{print $1}')
+push_id=$(echo "${push_url}" | awk -F/ '{print $1}')
 
 pr=$4
 
-echo "step1: git fetch to update local git repos $remote_from and $remote_to"
-git fetch $remote_from
-git fetch $remote_to
+echo "step1: git fetch to update local git repos ${remote_from} and ${remote_to}"
+git fetch "${remote_from}"
+git fetch "${remote_to}"
 
 echo
-echo "step2: get pr $pr commit id using gh cli"
-commitid=$(gh pr view $pr --repo $doris_repo --json mergeCommit -t '{{.mergeCommit.oid}}')
-title=$(gh pr view $pr --repo $doris_repo --json title -t '{{.title}}')
-echo "commit id for $pr is '$commitid'"
-# len=`echo -n $commitid | wc -c`
-# if [ "$len" != "40" ]
-# then
-# 	echo "commit id '$commitid' length != 40 is invalid"
-# 	echo
-# 	exit 2
-# fi
-git show --stat $commitid
-if [ $? -ne 0 ]; then
-    echo "git show --stat $commitid failed, $commitid is invalid"
+echo "step2: get pr ${pr} commit id using gh cli"
+commitid=$(gh pr view "${pr}" --repo "${doris_repo}" --json mergeCommit -t '{{.mergeCommit.oid}}')
+if ! git show --stat "${commitid}" ; then
+    echo "git show --stat ${commitid} failed, ${commitid} is invalid"
     echo
     exit 2
 fi
 
 echo
-echo "step3: create local branch $branch_pick based on remote branch $branch_to"
-branch_pick=$(echo pick_${pr}_to_${branch_to} | sed 's|/|_|g')
-git checkout -b $branch_pick ${branch_to}
-if [ $? -ne 0 ]; then
-    echo "git checkout -b $branch_pick ${branch_to} failed"
+echo "step3: create local branch ${branch_pick} based on remote branch ${branch_to}"
+branch_pick=$(echo "pick_${pr}_to_${branch_to}" | sed 's|/|_|g')
+if ! git checkout -b "${branch_pick} ${branch_to}" ; then
+    echo "git checkout -b ${branch_pick} ${branch_to} failed"
     echo
     exit 3
 fi
 
 echo
-echo -n "step4: will run git cherry-pick $commitid , please confirm y/n: "
+echo -n "step4: will run git cherry-pick ${commitid} , please confirm y/n: "
 
-read ans
+read -r ans
 echo
-if [ "$ans" == "y" ]; then
-    git cherry-pick $commitid
-    if [ $? -ne 0 ]; then
+if [ "${ans}" = "y" ]; then
+    if ! git cherry-pick "${commitid}" ; then
         echo -n "git cherry-pick return none zero $?, wait for manual processing, please confirm continue or exit c/e: "
-        read ans
-        if [ "$ans" != "c" ]; then
-            echo "manual processing confirm $ans is not c, git cherry-pick --abort and exit now"
+        read -r ans
+        if [ "${ans}" != "c" ]; then
+            echo "manual processing confirm ${ans} is not c, git cherry-pick --abort and exit now"
             git cherry-pick --abort
             exit 4
         fi
     fi
 
     echo
-    echo "step5: push to your remote repo $push_remote $(git remote get-url --push $push_remote)"
-    git push $push_remote
+    echo "step5: push to your remote repo ${push_remote} $(git remote get-url --push "${push_remote}")"
+    git push "${push_remote}"
 
     echo
     echo "step6: create pr using gh cli"
-    newpr_url=$(gh pr create --repo $doris_repo --base $branch_to_name --head ${push_id}:${branch_pick} --title "$title #${pr}" --body "cherry pick from #${pr}")
-    echo "new pr url: $newpr_url"
+    title=$(gh pr view "${pr}" --repo "${doris_repo}" --json title -t '{{.title}}')
+    newpr_url=$(gh pr create --repo "${doris_repo}" --base "${branch_to_name}" --head "${push_id}:${branch_pick}" --title "${title} #${pr}" --body "cherry pick from #${pr}")
+    echo "new pr url: ${newpr_url}"
 
     echo
     echo "step7: comment 'run buildall' to trigger tests using gh cli"
-    newpr=$(echo $newpr_url | awk -F/ '{print $NF}')
-    if [ "$newpr" != "" ]; then
-        gh pr comment --repo $doris_repo $newpr --body 'run buildall'
+    newpr=$(echo "${newpr_url}" | awk -F/ '{print $NF}')
+    if [ "${newpr}" != "" ]; then
+        gh pr comment --repo "${doris_repo}" "${newpr}" --body 'run buildall'
     fi
 else
-    echo "do nothing for $pr"
+    echo "do nothing for ${pr}"
     exit 5
 fi
