059fe688c759578bd86c588d170431d89fec6f26
HHoflittlefish777 <77738092+HHoflittlefish777@users.noreply.github.com>
2024-01-10 21:35:59 +0800

    (fix)[insert] fix insert into lose data #29802 (#29813)

--- fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -2226,7 +2226,14 @@ public class Coordinator implements CoordInterface {
                         status.getErrorMsg());
                 updateStatus(status, params.getFragmentInstanceId());
             }
-            if (ctx.fragmentInstancesMap.get(params.fragment_instance_id).getIsDone()) {
+
+            // params.isDone() should be promised.
+            // There are some periodic reports during the load process,
+            // and the reports from the intermediate process may be concurrent with the last report.
+            // The last report causes the counter to decrease to zero,
+            // but it is possible that the report without commit-info triggered the commit operation,
+            // resulting in the data not being published.
+            if (ctx.fragmentInstancesMap.get(params.fragment_instance_id).getIsDone() && params.isDone()) {
                 if (params.isSetDeltaUrls()) {
                     updateDeltas(params.getDeltaUrls());
                 }
@@ -2278,7 +2285,14 @@ public class Coordinator implements CoordInterface {
                         status.getErrorMsg());
                 updateStatus(status, params.getFragmentInstanceId());
             }
-            if (execState.done) {
+
+            // params.isDone() should be promised.
+            // There are some periodic reports during the load process,
+            // and the reports from the intermediate process may be concurrent with the last report.
+            // The last report causes the counter to decrease to zero,
+            // but it is possible that the report without commit-info triggered the commit operation,
+            // resulting in the data not being published.
+            if (execState.done && params.isDone()) {
                 if (params.isSetDeltaUrls()) {
                     updateDeltas(params.getDeltaUrls());
                 }
