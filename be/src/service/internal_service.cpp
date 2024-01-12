453c06cb24750743e3ef5d9957edaf801015e00d
Kaijie Chen <ckj@apache.org>
2024-01-12 21:34:36 +0800

    [fix](move-memtable) set idle timeout equal to load timeout (#29839)

--- be/src/service/internal_service.cpp
+++ be/src/service/internal_service.cpp
@@ -391,7 +391,9 @@ void PInternalServiceImpl::open_load_stream(google::protobuf::RpcController* con
         }
 
         stream_options.handler = load_stream.get();
-        stream_options.idle_timeout_ms = config::load_stream_idle_timeout_ms;
+        stream_options.idle_timeout_ms = request->idle_timeout_ms();
+        DBUG_EXECUTE_IF("PInternalServiceImpl.open_load_stream.set_idle_timeout",
+                        { stream_options.idle_timeout_ms = 1; });
 
         StreamId streamid;
         if (brpc::StreamAccept(&streamid, *cntl, &stream_options) != 0) {
