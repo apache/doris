0fd4451b62d3680d9c1b10cc813c04f6f126cdd8
amorynan <wangqiannan@selectdb.com>
2024-01-13 18:35:54 +0800

    support array_enumerate_uniq and array_suffle for nereids

--- regression-test/suites/datatype_p0/nested_types/query/array_functions/test_array_zip_array_enumerate_uniq.groovy
+++ regression-test/suites/datatype_p0/nested_types/query/array_functions/test_array_zip_array_enumerate_uniq.groovy
@@ -17,10 +17,6 @@
 
 suite("test_array_zip_array_enumerate_uniq", "p0") {
 
-    sql "drop table if exists numbers;"
-    sql "create table if not exists numbers (number int) ENGINE=OLAP DISTRIBUTED BY HASH(number) BUCKETS 1 PROPERTIES('replication_num' = '1');"
-    sql "insert into numbers values (NULL), (0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10);"
-
     sql "set enable_nereids_planner=false;"
 //     ========== array-zip ==========
 //     wrong case
@@ -79,20 +75,26 @@ suite("test_array_zip_array_enumerate_uniq", "p0") {
 
     // nereid not support array_enumerate_uniq
     // ============= array_enumerate_uniq =========
-//    qt_sql "SELECT 'array_enumerate_uniq';"
-//    order_qt_nereid_sql """ SELECT array_enumerate_uniq(array_enumerate_uniq(array(cast(10 as LargeInt), cast(100 as LargeInt), cast(2 as LargeInt))), array(cast(123 as LargeInt), cast(1023 as LargeInt), cast(123 as LargeInt))); """
-//
-//    order_qt_nereid_sql """SELECT array_enumerate_uniq(
-//            [111111, 222222, 333333],
-//            [444444, 555555, 666666],
-//            [111111, 222222, 333333],
-//            [444444, 555555, 666666],
-//            [111111, 222222, 333333],
-//            [444444, 555555, 666666],
-//            [111111, 222222, 333333],
-//            [444444, 555555, 666666]);"""
-//    order_qt_nereid_sql """SELECT array_enumerate_uniq(array(STDDEV_SAMP(910947.571364)), array(NULL)) from numbers;"""
+    qt_sql "SELECT 'array_enumerate_uniq';"
+    order_qt_nereid_sql """ SELECT array_enumerate_uniq(array_enumerate_uniq(array(cast(10 as LargeInt), cast(100 as LargeInt), cast(2 as LargeInt))), array(cast(123 as LargeInt), cast(1023 as LargeInt), cast(123 as LargeInt))); """
+
+    order_qt_nereid_sql """SELECT array_enumerate_uniq(
+            [111111, 222222, 333333],
+            [444444, 555555, 666666],
+            [111111, 222222, 333333],
+            [444444, 555555, 666666],
+            [111111, 222222, 333333],
+            [444444, 555555, 666666],
+            [111111, 222222, 333333],
+            [444444, 555555, 666666]);"""
+    order_qt_nereid_sql """SELECT array_enumerate_uniq(array(STDDEV_SAMP(910947.571364)), array(NULL)) from numbers;"""
 //    //order_qt_sql """ SELECT max(array_join(arr)) FROM (SELECT array_enumerate_uniq(group_array(DIV(number, 54321)) AS nums, group_array(cast(DIV(number, 98765) as string))) AS arr FROM (SELECT number FROM numbers LIMIT 1000000) GROUP BY bitmap_hash(number) % 100000);"""
 
+    // array_shuffle
+    // do not check result, since shuffle result is random
+    sql "SELECT array_sum(array_shuffle([1, 2, 3, 3, null, null, 4, 4])), array_shuffle([1, 2, 3, 3, null, null, 4, 4], 0);"
+    sql "SELECT array_sum(array_shuffle([1.111, 2.222, 3.333])), array_shuffle([1.111, 2.222, 3.333], 0)"
+    sql "SELECT array_size(array_shuffle(['aaa', null, 'bbb', 'fff'])), array_shuffle(['aaa', null, 'bbb', 'fff'], 0)"
+    sql """select array_size(array("2020-01-02", "2022-01-03", "2021-01-01", "1996-04-17")), array_shuffle(array("2020-01-02", "2022-01-03", "2021-01-01", "1996-04-17"), 0)"""
 
 }
