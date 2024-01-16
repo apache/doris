759a6943ec555aceafab742f461880e2d75773d2
zclllyybb <zhaochangle@selectdb.com>
2024-01-16 17:30:12 +0800

    [branch-2.0](test-framework) Support operator performance test for branch2.0 (#29661)
    * [Opt](functions) Opt tvf number for performance regression framework (#27582)
    
    Opt tvf number for performance regression framework
    
    * [Feature](functions) support ignore and nullable functions (#27848)
    
    support ignore and nullable functions
    
    * [Feature](function) support generating const values from tvf numbers (#28051)
    
    If specified, got a column of constant. otherwise an incremental series like it always be.
    
    mysql> select * from numbers("number" = "5", "const_value" = "-123");
    +--------+
    | number |
    +--------+
    |   -123 |
    |   -123 |
    |   -123 |
    |   -123 |
    |   -123 |
    +--------+
    5 rows in set (0.11 sec)
    
    * fix compile
    
    * [feature](nereids) support non_nullable function (#23762)
    
    * fix cases
    
    ---------
    
    Co-authored-by: minghong <englefly@gmail.com>

--- be/src/vec/functions/function_nullables.cpp
+++ be/src/vec/functions/function_nullables.cpp
@@ -14,18 +14,11 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-// This file is copied from
-// https://github.com/ClickHouse/ClickHouse/blob/master/src/Functions/Ifnull.h
-// and modified by Doris
-
-#include <stddef.h>
-
-#include <memory>
 
 #include "common/status.h"
-#include "vec/aggregate_functions/aggregate_function.h"
 #include "vec/columns/column.h"
 #include "vec/columns/column_nullable.h"
+#include "vec/columns/columns_number.h"
 #include "vec/core/block.h"
 #include "vec/core/column_numbers.h"
 #include "vec/core/column_with_type_and_name.h"
@@ -40,6 +33,38 @@ class FunctionContext;
 } // namespace doris
 
 namespace doris::vectorized {
+
+class FunctionNullable : public IFunction {
+public:
+    static constexpr auto name = "nullable";
+
+    static FunctionPtr create() { return std::make_shared<FunctionNullable>(); }
+
+    String get_name() const override { return name; }
+
+    size_t get_number_of_arguments() const override { return 1; }
+
+    DataTypePtr get_return_type_impl(const DataTypes& arguments) const override {
+        return make_nullable(arguments[0]);
+    }
+
+    bool use_default_implementation_for_nulls() const override { return false; }
+
+    // trans nullable column to non-nullable column. If argument is already non-nullable, raise error.
+    Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
+                        size_t result, size_t input_rows_count) override {
+        ColumnPtr& col = block.get_by_position(arguments[0]).column;
+        if (const auto* col_null = check_and_get_column<ColumnNullable>(col); col_null == nullptr) {
+            // not null
+            block.replace_by_position(
+                    result, ColumnNullable::create(col, ColumnBool::create(input_rows_count, 0)));
+        } else { // column is ColumnNullable
+            block.replace_by_position(result, col->clone_resized(input_rows_count));
+        }
+        return Status::OK();
+    }
+};
+
 class FunctionNonNullable : public IFunction {
 public:
     static constexpr auto name = "non_nullable";
@@ -60,23 +85,28 @@ public:
     Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
                         size_t result, size_t input_rows_count) override {
         auto& data = block.get_by_position(arguments[0]);
-        const ColumnNullable* column = check_and_get_column<ColumnNullable>(data.column);
-
-        if (column == nullptr) // raise error if input is not nullable.
+        if (const auto* col_null = check_and_get_column<ColumnNullable>(data.column);
+            col_null == nullptr) // raise error if input is not nullable.
         {
-            return Status::RuntimeError(
-                    "Try to use originally non-nullable column {} in nullable's non-nullable \
-                    convertion.",
-                    data.column->get_name(), get_name());
+            return Status::InvalidArgument(
+                    "Try to use originally non-nullable column {} in nullable's non-nullable "
+                    "convertion.",
+                    data.column->get_name());
         } else { // column is ColumnNullable
-            const ColumnPtr& type_ptr = column->get_nested_column_ptr();
-            block.replace_by_position(result, type_ptr->clone_resized(type_ptr->size()));
+            if (col_null->has_null()) [[unlikely]] {
+                return Status::InvalidArgument(
+                        "There's NULL value in column {} which is illegal for non_nullable",
+                        data.column->get_name());
+            }
+            const ColumnPtr& nest_col = col_null->get_nested_column_ptr();
+            block.replace_by_position(result, nest_col->clone_resized(nest_col->size()));
         }
         return Status::OK();
     }
 };
 
-void register_function_non_nullable(SimpleFunctionFactory& factory) {
+void register_function_nullables(SimpleFunctionFactory& factory) {
+    factory.register_function<FunctionNullable>();
     factory.register_function<FunctionNonNullable>();
 }
 
