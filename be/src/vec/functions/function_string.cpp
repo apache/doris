c6de052432a75b315855739a6d56a50418d15d76
koarz <3577087577@qq.com>
2024-01-16 22:48:50 +0800

    version

--- be/src/vec/functions/function_string.cpp
+++ be/src/vec/functions/function_string.cpp
@@ -1017,6 +1017,12 @@ void register_function_string(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionSubReplace<SubReplaceThreeImpl>>();
     factory.register_function<FunctionSubReplace<SubReplaceFourImpl>>();
 
+    /// @TEMPORARY: for be_exec_version=3
+    factory.register_function<FunctionSubstringOld<Substr3ImplOld>>();
+    factory.register_function<FunctionSubstringOld<Substr2ImplOld>>();
+    factory.register_function<FunctionLeftOld>();
+    factory.register_function<FunctionRightOld>();
+
     factory.register_alias(FunctionLeft::name, "strleft");
     factory.register_alias(FunctionRight::name, "strright");
     factory.register_alias(SubstringUtil::name, "substr");
