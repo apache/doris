b671aa2ac3a6838c8b157de4e91f229c80ff98af
zy-kkk <zhongyk10@gmail.com>
2024-01-16 15:54:52 +0800

    [improvement](catalog) fix jdbc mysql catalog to_date fun pushdown (#29900)

--- fe/fe-core/src/main/java/org/apache/doris/planner/external/jdbc/JdbcFunctionPushDownRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/external/jdbc/JdbcFunctionPushDownRule.java
@@ -72,6 +72,7 @@ public class JdbcFunctionPushDownRule {
 
     static {
         REPLACE_MYSQL_FUNCTIONS.put("nvl", "ifnull");
+        REPLACE_MYSQL_FUNCTIONS.put("to_date", "date");
     }
 
     private static boolean isReplaceMysqlFunctions(String functionName) {
