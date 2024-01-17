21515711aaed3d2cd2c9b27f42b260635c6866a7
GoGoWen2021 <GoGoWen2021@gmail.com>
2024-01-17 21:27:38 +0800

    fix code format

--- be/src/vec/exec/format/parquet/parquet_pred_cmp.h
+++ be/src/vec/exec/format/parquet/parquet_pred_cmp.h
@@ -241,7 +241,7 @@ private:
                     min_value.from_unixtime(min_date_value * 24 * 60 * 60, ctz);
                     max_value.from_unixtime(max_date_value * 24 * 60 * 60, ctz);
 
-                    // as VecDateTimeValue can not compare date and datetime, so need 
+                    // as VecDateTimeValue can not compare date and datetime, so need
                     // cast to date here
                     if constexpr (std::is_same_v<CppType, VecDateTimeValue>) {
                         min_value.cast_to_date();
