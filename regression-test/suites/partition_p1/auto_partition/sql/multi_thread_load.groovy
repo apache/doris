649a1f74f8430e4bf121cce70591e125cb1fdb82
zhaochangle <zhaochangle@selectdb.com>
2024-01-19 16:34:07 +0800

    stress case to reprocedure

--- regression-test/suites/partition_p1/auto_partition/sql/multi_thread_load.groovy
+++ regression-test/suites/partition_p1/auto_partition/sql/multi_thread_load.groovy
@@ -20,7 +20,6 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.net.URL
 import java.io.File
-import java.util.concurrent.locks.ReentrantLock
 
 suite("multi_thread_load") {
     // get doris-db from s3
