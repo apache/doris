f4c2324392078cfcf444f125f1171c8d711968d2
Yongqiang YANG <dataroaring@gmail.com>
2024-01-20 21:24:44 +0800

    [fix](brpc) upgrade brpc to 1.7.0
    There are some fixes and improvement since 1.3.0.

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -388,7 +388,7 @@ if [[ "${SIMDJSON_SOURCE}" = "simdjson-3.0.1" ]]; then
 fi
 echo "Finished patching ${SIMDJSON_SOURCE}"
 
-if [[ "${BRPC_SOURCE}" == 'brpc-1.4.0' ]]; then
+if [[ "${BRPC_SOURCE}" == 'brpc-1.7.0' ]]; then
     cd "${TP_SOURCE_DIR}/${BRPC_SOURCE}"
     if [[ ! -f "${PATCHED_MARK}" ]]; then
         for patch_file in "${TP_PATCH_DIR}"/brpc-*; do
