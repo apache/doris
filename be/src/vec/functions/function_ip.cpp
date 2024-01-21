d6ed4149f5b2b15e97a5a8861387141749c0339e
Chester <42577861+superdiaodiao@users.noreply.github.com>
2024-01-21 10:51:58 +0800

    [feature](function) support ip function is_ipv4_compat, is_ipv4_mapped (#29954)

--- be/src/vec/functions/function_ip.cpp
+++ be/src/vec/functions/function_ip.cpp
@@ -27,6 +27,7 @@ void register_function_ip(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionIPv4StringToNum<IPStringToNumExceptionMode::Null>>();
     factory.register_alias(FunctionIPv4StringToNum<IPStringToNumExceptionMode::Throw>::name,
                            "inet_aton");
+
     factory.register_function<FunctionIPv6NumToString>();
     factory.register_alias(FunctionIPv6NumToString::name, "inet6_ntoa");
     factory.register_function<FunctionIPv6StringToNum<IPStringToNumExceptionMode::Throw>>();
@@ -34,6 +35,9 @@ void register_function_ip(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionIPv6StringToNum<IPStringToNumExceptionMode::Null>>();
     factory.register_alias(FunctionIPv6StringToNum<IPStringToNumExceptionMode::Throw>::name,
                            "inet6_aton");
+
+    factory.register_function<FunctionIsIPv4Compat>();
+    factory.register_function<FunctionIsIPv4Mapped>();
     factory.register_function<FunctionIsIPString<IPv4>>();
     factory.register_function<FunctionIsIPString<IPv6>>();
     factory.register_function<FunctionIsIPAddressInRange>();
