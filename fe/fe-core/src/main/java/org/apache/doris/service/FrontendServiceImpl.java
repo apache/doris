d112743894b95d781a6a398658dcdbffab9b2738
meiyi <myimeiyi@gmail.com>
2024-01-22 15:36:19 +0800

    fix fe

--- fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
@@ -1660,7 +1660,7 @@ public class FrontendServiceImpl implements FrontendService.Iface {
             throw new MetaNotFoundException("db " + request.getDb() + " does not exist");
         }
         long dbId = db.getId();
-        if (request.isSetTxnId()) {
+        if (request.getTxnId() != 0) { // txnId is required in thrift
             TransactionState transactionState = Env.getCurrentGlobalTransactionMgr()
                     .getTransactionState(dbId, request.getTxnId());
             if (transactionState == null) {
