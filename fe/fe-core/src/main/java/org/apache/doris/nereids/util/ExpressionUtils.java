54bd881579c1f62cff9c2f75296ea0e1cfd7aaf8
englefly <minghong.zhou@163.com>
2024-01-23 11:56:00 +0800

    comments

--- fe/fe-core/src/main/java/org/apache/doris/nereids/util/ExpressionUtils.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/util/ExpressionUtils.java
@@ -51,6 +51,7 @@ import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionRewri
 import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionVisitor;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.visitor.ExpressionLineageReplacer;
+import org.apache.doris.nereids.types.coercion.NumericType;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
@@ -59,7 +60,6 @@ import com.google.common.collect.ImmutableList.Builder;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.apache.doris.nereids.types.coercion.NumericType;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -662,7 +662,9 @@ public class ExpressionUtils {
         return expression;
     }
 
-    // the expressions can be used as runtime filter targets
+    /**
+     * the expressions can be used as runtime filter targets
+     */
     public static Expression getSingleNumericSlotOrExpressionCoveredByCast(Expression expression) {
         if (expression.getInputSlots().size() == 1) {
             Slot slot = expression.getInputSlots().iterator().next();
@@ -670,6 +672,9 @@ public class ExpressionUtils {
                 return expression.getInputSlots().iterator().next();
             }
         }
+        // for other datatype, only support cast.
+        // example: T1 join T2 on subStr(T1.a, 1,4) = subStr(T2.a, 1,4)
+        // the cost of subStr is too high, and hence we do not generate RF subStr(T2.a, 1,4)->subStr(T1.a, 1,4)
         while (expression instanceof Cast) {
             expression = ((Cast) expression).child();
         }
