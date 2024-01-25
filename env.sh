ef0d24d05dbd32ef9d83f6e324969fb2d59c386c
谢健 <jianxie0@gmail.com>
2024-01-25 14:01:27 +0800

    [fix](Nereids): when predicate contains right output, don't convert outer to anti join (#30276)

--- env.sh
+++ env.sh
@@ -235,7 +235,7 @@ if test -z "${BUILD_THIRDPARTY_WIP:-}"; then
     fi
 
     # check maven
-    MVN_CMD='mvnd'
+    MVN_CMD='mvn'
     if [[ -n "${CUSTOM_MVN}" ]]; then
         MVN_CMD="${CUSTOM_MVN}"
     fi
