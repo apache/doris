75ba0aa90a6d43977e02ecba812f8b726d693075
huanghaibin <284824253@qq.com>
2024-01-25 21:20:10 +0800

    [fix](group_commit) Fix commit txn problem when replaying wal (#30361)

--- be/src/olap/wal/wal_table.cpp
+++ be/src/olap/wal/wal_table.cpp
@@ -264,6 +264,7 @@ Status WalTable::_handle_stream_load(int64_t wal_id, const std::string& wal,
         // wait stream load finish
         RETURN_IF_ERROR(ctx->future.get());
         if (ctx->status.ok()) {
+            ctx->auth.auth_code = wal_id;
             auto commit_st = _exec_env->stream_load_executor()->commit_txn(ctx.get());
             st = commit_st;
         } else if (!ctx->status.ok()) {
