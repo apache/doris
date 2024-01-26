3e24c585edd1b66c26e2d3c98c55f609a9668cab
meiyi <myimeiyi@gmail.com>
2024-01-26 09:49:14 +0800

    tmp

--- be/src/runtime/group_commit_mgr.cpp
+++ be/src/runtime/group_commit_mgr.cpp
@@ -157,8 +157,8 @@ void LoadBlockQueue::remove_load_id(const UniqueId& load_id) {
 Status LoadBlockQueue::add_load_id(const UniqueId& load_id) {
     std::unique_lock l(mutex);
     if (_need_commit) {
-        return Status::InternalError("block queue is set need commit, id=" +
-                                     load_instance_id.to_string());
+        return Status::InternalError<false>("block queue is set need commit, id=" +
+                                            load_instance_id.to_string());
     }
     _load_ids.emplace(load_id);
     return Status::OK();
@@ -204,7 +204,7 @@ Status GroupCommitTable::get_first_block_load_queue(
                 }
             }
             if (!is_schema_version_match) {
-                return Status::DataQualityError("schema version not match");
+                return Status::DataQualityError<false>("schema version not match");
             }
             if (!_need_plan_fragment) {
                 _need_plan_fragment = true;
@@ -220,14 +220,14 @@ Status GroupCommitTable::get_first_block_load_queue(
                         return Status::OK();
                     }
                 } else if (base_schema_version < load_block_queue->schema_version) {
-                    return Status::DataQualityError("schema version not match");
+                    return Status::DataQualityError<false>("schema version not match");
                 }
                 load_block_queue.reset();
             }
         }
     }
-    return Status::InternalError("can not get a block queue for table_id: " +
-                                 std::to_string(_table_id));
+    return Status::InternalError<false>("can not get a block queue for table_id: " +
+                                        std::to_string(_table_id));
 }
 
 Status GroupCommitTable::_create_group_commit_load(
@@ -272,7 +272,7 @@ Status GroupCommitTable::_create_group_commit_load(
             },
             10000L);
     RETURN_IF_ERROR(st);
-    st = Status::create(result.status);
+    st = Status::create<false>(result.status);
     if (!st.ok()) {
         LOG(WARNING) << "create group commit load error, st=" << st.to_string();
     }
@@ -366,7 +366,7 @@ Status GroupCommitTable::_finish_group_commit_load(int64_t db_id, int64_t table_
                     client->loadTxnRollback(result, request);
                 },
                 10000L);
-        result_status = Status::create(result.status);
+        result_status = Status::create<false>(result.status);
     }
     std::shared_ptr<LoadBlockQueue> load_block_queue;
     {
