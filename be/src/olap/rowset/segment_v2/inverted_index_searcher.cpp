3c24263355eb957bf20b7b04e471195c5a13d0fd
姜凯 <airborne08@gmail.com>
2024-01-31 16:51:45 +0800

    fix header

--- be/src/olap/rowset/segment_v2/inverted_index_searcher.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_searcher.cpp
@@ -20,6 +20,7 @@
 #include <CLucene/search/IndexSearcher.h>
 #include <CLucene/util/bkd/bkd_reader.h>
 
+#include "common/config.h"
 #include "olap/rowset/segment_v2/inverted_index_compound_directory.h"
 #include "olap/rowset/segment_v2/inverted_index_compound_reader.h"
 #include "olap/rowset/segment_v2/inverted_index_desc.h"
