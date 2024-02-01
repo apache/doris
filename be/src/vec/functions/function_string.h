8cb6f3415d5ac7dd4b1f3d69c8e663dc91de0546
Guangdong Liu <804167098@qq.com>
2024-02-01 09:35:06 +0800

    Update be/src/vec/functions/function_string.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- be/src/vec/functions/function_string.h
+++ be/src/vec/functions/function_string.h
@@ -2616,8 +2616,8 @@ public:
         }
     bool use_default_implementation_for_nulls() const override { return true; }
 
-    Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
-                            size_t result, size_t input_rows_count) const override {
+    static Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
+                            size_t result, size_t input_rows_count) override {
             auto null_map = ColumnUInt8::create(input_rows_count, 0);
             auto& null_map_data = null_map->get_data();
 
