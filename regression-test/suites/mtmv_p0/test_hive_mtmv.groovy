135e0554092720af401f2e8cc41c0481ac71652d
zhangdong <493738387@qq.com>
2024-02-02 12:36:05 +0800

    1

--- regression-test/suites/mtmv_p0/test_hive_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_hive_mtmv.groovy
@@ -57,11 +57,19 @@ suite("test_hive_mtmv", "p0,external,hive,external_docker,external_docker_hive")
             order_qt_refresh_one_partition "SELECT * FROM ${mvName} order by id"
 
             //refresh other partitions
+            // current, for hive, auto refresh will not change data
             sql """
                     REFRESH MATERIALIZED VIEW ${mvName}
                 """
             waitingMTMVTaskFinished(jobName)
             order_qt_refresh_other_partition "SELECT * FROM ${mvName} order by id"
+
+            //refresh complete
+            sql """
+                    REFRESH MATERIALIZED VIEW ${mvName} complete
+                """
+            waitingMTMVTaskFinished(jobName)
+            order_qt_refresh_complete "SELECT * FROM ${mvName} order by id"
 
             sql """drop materialized view if exists ${mvName};"""
 
