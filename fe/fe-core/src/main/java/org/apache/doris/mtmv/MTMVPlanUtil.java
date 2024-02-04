0b784d72ae55cc1536c3c2f9c08f79b58f449ea9
zhangdong <493738387@qq.com>
2024-02-04 11:11:58 +0800

    [feature](mtmv) mtmv support workload group (#29595)
    MTMV supports controlling the resource usage of refresh tasks by setting the name of workload group
    about workload group : https://doris.apache.org/zh-CN/docs/dev/admin-manual/workload-group

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVPlanUtil.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVPlanUtil.java
@@ -43,6 +43,7 @@ import org.apache.doris.qe.SessionVariable;
 import com.google.common.collect.Sets;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public class MTMVPlanUtil {
@@ -59,6 +60,10 @@ public class MTMVPlanUtil {
         ctx.changeDefaultCatalog(catalog.getName());
         ctx.setDatabase(catalog.getDbOrAnalysisException(mtmv.getEnvInfo().getDbId()).getFullName());
         ctx.getSessionVariable().enableFallbackToOriginalPlanner = false;
+        Optional<String> workloadGroup = mtmv.getWorkloadGroup();
+        if (workloadGroup.isPresent()) {
+            ctx.getSessionVariable().setWorkloadGroup(workloadGroup.get());
+        }
         ctx.getSessionVariable().enableNereidsDML = true;
         return ctx;
     }
