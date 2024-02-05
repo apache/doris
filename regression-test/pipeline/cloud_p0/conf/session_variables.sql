da18f6c5c08f063d47429cc954ff0e8128fc34e1
Dongyang Li <hello_stephen@qq.com>
2024-02-05 11:52:17 +0800

    [ci](cloud) add cloud p0 pipeline (#30772)
    * [ci](cloud) add cloud p0 pipeline
    Co-authored-by: stephen <hello-stephen@qq.com>

---
+++ regression-test/pipeline/cloud_p0/conf/session_variables.sql
@@ -0,0 +1,4 @@
+-- set those session variables before run cloud p0 regression
+set global insert_visible_timeout_ms=60000;
+set global enable_auto_analyze=false;
+set global enable_audit_plugin=true;
