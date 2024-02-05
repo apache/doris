1959f47ed5a782dc345646203eb6362f5c2db960
ByteYue <yj976240184@gmail.com>
2024-02-05 22:55:08 +0800

    add one http action to add policy back

--- fe/fe-core/src/main/java/org/apache/doris/policy/PolicyMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/policy/PolicyMgr.java
@@ -144,6 +144,31 @@ public class PolicyMgr implements Writable {
     }
 
     /**
+     * Create policy through http api.
+     **/
+    public void addPolicy(Policy policy) throws UserException {
+        writeLock();
+        try {
+            boolean storagePolicyExists = false;
+            if (PolicyTypeEnum.STORAGE == policy.getType()) {
+                // The name of the storage policy remains globally unique until it is renamed by user.
+                // So we could just compare the policy name to check if there are redundant ones.
+                // Otherwise two storage policy share one same name but with different resource name
+                // will not be filtered. See github #25025 for more details.
+                storagePolicyExists = getPoliciesByType(PolicyTypeEnum.STORAGE)
+                        .stream().anyMatch(p -> p.getPolicyName().equals(policy.getPolicyName()));
+            }
+            if (storagePolicyExists || existPolicy(policy)) {
+                throw new DdlException("the policy " + policy.getPolicyName() + " already create");
+            }
+            unprotectedAdd(policy);
+            Env.getCurrentEnv().getEditLog().logCreatePolicy(policy);
+        } finally {
+            writeUnlock();
+        }
+    }
+
+    /**
      * Drop policy through stmt.
      **/
     public void dropPolicy(DropPolicyStmt stmt) throws DdlException, AnalysisException {
