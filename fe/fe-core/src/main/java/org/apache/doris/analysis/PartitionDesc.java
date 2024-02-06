bbc78b8dee1952baa84385a3626acb0b006d8258
zhangdong <493738387@qq.com>
2024-02-06 17:14:37 +0800

    change fe config to session variable

--- fe/fe-core/src/main/java/org/apache/doris/analysis/PartitionDesc.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/PartitionDesc.java
@@ -161,7 +161,7 @@ public class PartitionDesc {
 
         int createTablePartitionMaxNum = ConnectContext.get().getSessionVariable().getCreateTablePartitionMaxNum();
         if (singlePartitionDescs.size() > createTablePartitionMaxNum) {
-            throw new org.apache.doris.nereids.exceptions.AnalysisException(String.format(
+            throw new AnalysisException(String.format(
                     "The number of partitions to be created is [%s], exceeding the maximum value of [%s]. "
                             + "Creating too many partitions can be time-consuming. If necessary, "
                             + "You can set the session variable 'create_table_partition_max_num' to a larger value.",
