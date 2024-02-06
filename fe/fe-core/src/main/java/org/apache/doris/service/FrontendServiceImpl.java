46a4d697ce4ca28226c59d5a271aa5fc7ee5c3bc
laihui <1353307710@qq.com>
2024-02-06 16:22:23 +0800

    reduce routine load job transaction info storage in memory

--- fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
+++ fe/fe-core/src/main/java/org/apache/doris/service/FrontendServiceImpl.java
@@ -1906,7 +1906,6 @@ public class FrontendServiceImpl implements FrontendService.Iface {
         }
         long timeoutMs = request.isSetThriftRpcTimeoutMs() ? request.getThriftRpcTimeoutMs() : 5000;
         List planFragmentParamsList = new ArrayList<>(tableNames.size());
-        List<Long> tableIds = olapTables.stream().map(OlapTable::getId).collect(Collectors.toList());
         // todo: if is multi table, we need consider the lock time and the timeout
         boolean enablePipelineLoad = Config.enable_pipeline_load;
         try {
@@ -1924,8 +1923,6 @@ public class FrontendServiceImpl implements FrontendService.Iface {
                 }
                 multiTableFragmentInstanceIdIndexMap.put(request.getTxnId(), ++index);
             }
-            Env.getCurrentGlobalTransactionMgr()
-                    .putTransactionTableNames(db.getId(), request.getTxnId(), tableIds);
             LOG.debug("receive stream load multi table put request result: {}", result);
         } catch (Throwable e) {
             LOG.warn("catch unknown result.", e);
