23a58cc11c5d04465a1221ae54591d253a8648d8
wangbo <506340561@qq.com>
2024-02-07 17:10:44 +0800

    +1

--- regression-test/suites/load_p0/broker_load/test_s3_load.groovy
+++ regression-test/suites/load_p0/broker_load/test_s3_load.groovy
@@ -17,10 +17,7 @@
 
 suite("test_s3_load", "load_p0") {
 
-    sql "create workload group if not exists broker_load_test "
-           + "properties ( "
-           + " 'cpu_share'='1024' "
-           + ");"
+    sql "create workload group if not exists broker_load_test properties ( 'cpu_share'='1024'); "
 
     sql "set workload_group=broker_load_test;"
 
