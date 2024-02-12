50351d2ad01a67eaffafe6243e7a70b94eef800d
liugddx <liugddx@gmail.com>
2024-02-12 18:23:19 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ExportCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ExportCommand.java
@@ -246,7 +246,7 @@ public class ExportCommand extends Command implements ForwardWithSync {
         exportJob.setTableName(tblName);
         exportJob.setExportTable(table);
         exportJob.setTableId(table.getId());
-
+        exportJob.setOrigStmt(ctx.getExecutor().getOriginStmt());
         // set partitions
         exportJob.setPartitionNames(this.partitionsNames);
         // set where expression
