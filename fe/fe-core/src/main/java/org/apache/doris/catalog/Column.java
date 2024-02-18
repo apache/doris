5417bb4db013a5aec6111442459504818fe8d792
abmdocrt <Yukang.Lian2022@gmail.com>
2024-02-18 10:03:03 +0800

    [Enhancement](auto-inc) Show create table support auto inc init value (#31021)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Column.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Column.java
@@ -803,7 +803,7 @@ public class Column implements Writable, GsonPostProcessable {
             sb.append(" NOT NULL");
         }
         if (isAutoInc) {
-            sb.append(" AUTO_INCREMENT");
+            sb.append(" AUTO_INCREMENT(").append(autoIncInitValue).append(")");
         }
         if (defaultValue != null && getDataType() != PrimitiveType.HLL && getDataType() != PrimitiveType.BITMAP) {
             if (defaultValueExprDef != null) {
