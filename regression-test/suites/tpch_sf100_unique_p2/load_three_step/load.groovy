1d498d254b74afc1ca5e74df7cf5f6a29e6c8106
Gavin Chou <gavineaglechou@gmail.com>
2024-02-20 23:30:42 +0800

    [regression-test] Analyze each table after loading all tables
    Analyze may rely on row count reported by BEs, if we analyze immediately
    after loading to a table, the row count of the table may be still 0
    which may lead to improper behavior of analyze manager.
    
    This change will introduce extra 70 seconds wait time for the cases affected.
    "70s" is slightly lager than default value (60s) of
    tablet_stat_update_interval_second fe.conf.
    To reduce this duration, change tablet_stat_update_interval_second
    to a smaller value, say 10 seconds and change 70s to 15s.

--- regression-test/suites/tpch_sf100_unique_p2/load_three_step/load.groovy
+++ regression-test/suites/tpch_sf100_unique_p2/load_three_step/load.groovy
@@ -92,6 +92,10 @@ suite("load_three_step") {
             }
             sleep(5000)
         }
+    }
+
+    Thread.sleep(70000) // wait for row count report of the tables just loaded
+    tables.each { table, rows ->
         sql """SET query_timeout = 1800"""
         sql """ ANALYZE TABLE $table WITH SYNC """
     }
