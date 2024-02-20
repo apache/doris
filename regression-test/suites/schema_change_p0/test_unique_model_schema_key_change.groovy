906318bf237ba28e927e8f4155c0d42e3c8f38fd
cjj2010 <2449402815@qq.com>
2024-02-20 16:50:00 +0800

    Optimize testUniqueModelSchemaKeyChange

--- regression-test/suites/schema_change_p0/test_unique_model_schema_key_change.groovy
+++ regression-test/suites/schema_change_p0/test_unique_model_schema_key_change.groovy
@@ -62,7 +62,6 @@ suite("test_unique_model_schema_key_change","p0") {
      sql initTableData
      sql """ alter  table ${tbName} add  column special_area BOOLEAN KEY DEFAULT "0" AFTER username """
      insertSql = "insert into ${tbName} values(123456689, 'Alice', 0, 'Yaan', 25, 0, 13812345678, 'No. 123 Street, Beijing', '2022-01-01 10:00:00'); "
-
      waitForSchemaChangeDone({
           sql getTableStatusSql
           time 60
