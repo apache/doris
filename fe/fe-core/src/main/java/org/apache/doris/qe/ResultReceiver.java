8ffd635633f2839b13f30fadf6c67dfc0867431a
zhiqiang-hhhh <seuhezhiqiang@163.com>
2024-02-22 12:26:11 +0800

    Fix style

--- fe/fe-core/src/main/java/org/apache/doris/qe/ResultReceiver.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ResultReceiver.java
@@ -95,10 +95,10 @@ public class ResultReceiver {
                         }
                     } catch (TimeoutException e) {
                         LOG.warn("Query {} get result timeout, get result duration {} ms",
-                                DebugUtil.printId(this.queryId), (timeoutTs - currentTs)/1000);
+                                DebugUtil.printId(this.queryId), (timeoutTs - currentTs) / 1000);
                         isCancel = true;
                         status.setStatus(Status.TIMEOUT);
-                        updateCancelReason("Fetch date timeout");
+                        updateCancelReason("Fetch data timeout");
                         return null;
                     } catch (InterruptedException e) {
                         // continue to get result
