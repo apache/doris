1082df4a37bf0290c2ae56898a588c5f38761b60
Jibing Li <jibing3@gmail.com>
2024-02-27 10:05:06 +0800

    High priority queue and map.

--- fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsCollector.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsCollector.java
@@ -61,10 +61,6 @@ public abstract class StatisticsCollector extends MasterDaemon {
     @VisibleForTesting
     protected void createSystemAnalysisJob(AnalysisInfo jobInfo)
             throws DdlException {
-        if (jobInfo.colToPartitions.isEmpty()) {
-            // No statistics need to be collected or updated
-            return;
-        }
         Map<Long, BaseAnalysisTask> analysisTasks = new HashMap<>();
         AnalysisManager analysisManager = Env.getCurrentEnv().getAnalysisManager();
         analysisManager.createTaskForEachColumns(jobInfo, analysisTasks, false);
---
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticsJobAppender.java
@@ -0,0 +1,135 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.statistics;
+
+import org.apache.doris.catalog.Database;
+import org.apache.doris.catalog.Env;
+import org.apache.doris.catalog.Table;
+import org.apache.doris.catalog.TableIf;
+import org.apache.doris.common.util.MasterDaemon;
+import org.apache.doris.datasource.InternalCatalog;
+import org.apache.doris.statistics.util.StatisticsUtil;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StatisticsJobAppender extends MasterDaemon {
+
+    private static final Logger LOG = LogManager.getLogger(StatisticsJobAppender.class);
+
+    public static final long INTERVAL = 1000;
+    public static final int JOB_MAP_SIZE = 1000;
+
+    private long currentDbId;
+    private long currentTableId;
+
+    public StatisticsJobAppender() {
+        super("Statistics Job Appender", INTERVAL);
+    }
+
+    @Override
+    protected void runAfterCatalogReady() {
+        if (!StatisticsUtil.enableAutoAnalyze()) {
+            return;
+        }
+        if (!Env.getCurrentEnv().isMaster()) {
+            return;
+        }
+        if (!StatisticsUtil.statsTblAvailable()) {
+            LOG.info("Stats table not available, skip");
+            return;
+        }
+        if (Env.isCheckpointThread()) {
+            return;
+        }
+        appendJobs();
+    }
+
+    protected void appendJobs() {
+        AnalysisManager manager = Env.getCurrentEnv().getAnalysisManager();
+        appendColumnsToJobs(manager.highPriorityColumns, manager.highPriorityJobs);
+        appendColumnsToJobs(manager.midPriorityColumns, manager.midPriorityJobs);
+        appendToLowQueue(manager.lowPriorityJobs);
+    }
+
+    protected void appendColumnsToJobs(Queue<HighPriorityColumn> columnQueue, Map<TableIf, Set<String>> jobsMap) {
+        int size = columnQueue.size();
+        for (int i = 0; i < size; i++) {
+            HighPriorityColumn column = columnQueue.poll();
+            TableIf table = StatisticsUtil.findTable(column.catalogId, column.dbId, column.tblId);
+            synchronized (jobsMap) {
+                // If job map reach the upper limit, stop putting new jobs.
+                if (!jobsMap.containsKey(table) && jobsMap.size() >= JOB_MAP_SIZE) {
+                    break;
+                }
+                if (jobsMap.containsKey(table)) {
+                    jobsMap.get(table).add(column.colName);
+                } else {
+                    jobsMap.put(table, Collections.singleton(column.colName));
+                }
+            }
+        }
+    }
+
+    protected void appendToLowQueue(Map<TableIf, Set<String>>  jobsMap) {
+
+        InternalCatalog catalog = Env.getCurrentInternalCatalog();
+        List<Long> sortedDbs = catalog.getDbIds().stream().sorted().collect(Collectors.toList());
+        for (long dbId : sortedDbs) {
+            if (dbId < currentDbId
+                    || StatisticConstants.SYSTEM_DBS.contains(catalog.getDbNullable(dbId).getFullName())) {
+                continue;
+            }
+            currentDbId = dbId;
+            Optional<Database> db = catalog.getDb(dbId);
+            List<Table> tables = db.get().getTables().stream()
+                    .sorted((t1, t2) -> (int) (t1.getId() - t2.getId())).collect(Collectors.toList());
+            for (Table t : tables) {
+                if (t.getId() <= currentTableId) {
+                    continue;
+                }
+                synchronized (jobsMap) {
+                    // If job map reach the upper limit, stop putting new jobs.
+                    if (!jobsMap.containsKey(t) && jobsMap.size() >= JOB_MAP_SIZE) {
+                        return;
+                    }
+                    Set<String> columns
+                            = t.getColumns().stream().filter(c -> !StatisticsUtil.isUnsupportedType(c.getType()))
+                            .map(c -> c.getName()).collect(Collectors.toSet());
+                    if (jobsMap.containsKey(t)) {
+                        jobsMap.get(t).addAll(columns);
+                    } else {
+                        jobsMap.put(t, columns);
+                    }
+                }
+                currentTableId = t.getId();
+            }
+        }
+        // All tables have been processed once, reset for the next loop.
+        currentDbId = 0;
+        currentTableId = 0;
+    }
+}
