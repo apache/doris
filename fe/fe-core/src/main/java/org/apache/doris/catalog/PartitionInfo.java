a5a6ea0d361c632c752911295bd6df50361ee010
zhangdong <493738387@qq.com>
2024-02-27 21:52:01 +0800

    fix getIdToItem cause ConcurrentModificationException

--- fe/fe-core/src/main/java/org/apache/doris/catalog/PartitionInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/PartitionInfo.java
@@ -61,9 +61,9 @@ public class PartitionInfo implements Writable {
     // partition columns for list and range partitions
     protected List<Column> partitionColumns = Lists.newArrayList();
     // formal partition id -> partition item
-    protected Map<Long, PartitionItem> idToItem = Maps.newHashMap();
+    protected Map<Long, PartitionItem> idToItem = Maps.newConcurrentMap();
     // temp partition id -> partition item
-    protected Map<Long, PartitionItem> idToTempItem = Maps.newHashMap();
+    protected Map<Long, PartitionItem> idToTempItem = Maps.newConcurrentMap();
     // partition id -> data property
     @SerializedName("IdToDataProperty")
     protected Map<Long, DataProperty> idToDataProperty;
@@ -133,7 +133,7 @@ public class PartitionInfo implements Writable {
      * @return both normal partition and temp partition
      */
     public Map<Long, PartitionItem> getAllPartitions() {
-        HashMap all = new HashMap<>();
+        Map all = Maps.newConcurrentMap();
         all.putAll(idToTempItem);
         all.putAll(idToItem);
         return all;
