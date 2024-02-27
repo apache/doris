90107dab9759a73c8d97d74f3a100c6a9c136abc
wuwenchi <wuwenchihdu@hotmail.com>
2024-02-27 21:48:30 +0800

    rebase

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalTable.java
@@ -305,8 +305,6 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
             LOG.warn("Failed to initialize table {}.{}.{}", catalog.name, dbName, name, e);
             return 0;
         }
-        // All external table should get external row count from cache.
-        makeSureInitialized();
         return Env.getCurrentEnv().getExtMetaCacheMgr().getRowCountCache().getCachedRowCount(catalog.getId(), dbId, id);
     }
 
