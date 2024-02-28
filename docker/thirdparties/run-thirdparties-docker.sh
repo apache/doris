2d015a7dd82684ec579c50b5cd13515479549692
胥剑旭 <xujianxu@xujianxudeMacBook-Pro.local>
2024-02-28 20:24:39 +0800

    update routine load docker

--- docker/thirdparties/run-thirdparties-docker.sh
+++ docker/thirdparties/run-thirdparties-docker.sh
@@ -251,39 +251,24 @@ if [[ "${RUN_KAFKA}" -eq 1 ]]; then
     sed -i "s/doris--/${CONTAINER_UID}/g" "${ROOT}"/docker-compose/kafka/kafka.yaml
     sed -i "s/localhost/${IP_HOST}/g" "${ROOT}"/docker-compose/kafka/kafka.yaml
     sudo docker compose -f "${ROOT}"/docker-compose/kafka/kafka.yaml --env-file "${ROOT}"/docker-compose/kafka/kafka.env down
-    #start_kafka_producers() {
-    #    local container_id="$1"
-    #    local ip_host="$2"
-    #    local backup_dir=/home/work/pipline/backup_center
+    create_kafka_topics() {
+       local container_id="$1"
+       local ip_host="$2"
+       local backup_dir=/home/work/pipline/backup_center
 
-        # declare -a topics=("basic_data" "basic_array_data" "basic_data_with_errors" "basic_array_data_with_errors" "basic_data_timezone" "basic_array_data_timezone")
+        declare -a topics=("basic_data" "basic_array_data" "basic_data_with_errors" "basic_array_data_with_errors" "basic_data_timezone" "basic_array_data_timezone")
 
-        # for topic in "${topics[@]}"; do
-        #     while IFS= read -r line; do
-        #         touch ${backup_dir}/kafka_info.log
-        #         echo $(date) >> ${backup_dir}/kafka_info.log
-        #         echo "docker exec "${container_id}" bash -c echo '$line' | /opt/kafka/bin/kafka-console-producer.sh --broker-list '${ip_host}:19193' --topic '${topic}'" >> ${backup_dir}/kafka_info.log
-        #         docker exec "${container_id}" bash -c "echo '$line' | /opt/kafka/bin/kafka-console-producer.sh --broker-list '${ip_host}:19193' --topic '${topic}'"
-        #     done < "${ROOT}/docker-compose/kafka/scripts/${topic}.csv"
-        # done
+        for topic in "${topics[@]}"; do
+            echo "docker exec "${container_id}" bash -c echo '/opt/kafka/bin/kafka-topics.sh --create --broker-list '${ip_host}:19193' --partitions 10' --topic '${topic}'" 
+            docker exec "${container_id}" bash -c "/opt/kafka/bin/kafka-topics.sh --create --broker-list '${ip_host}:19193' --partitions 10' --topic '${topic}'"
+        done
 
-        # declare -a json_topics=("basic_data_json" "basic_array_data_json" "basic_array_data_json_by_line" "basic_data_json_by_line")
-        
-        # for json_topic in "${json_topics[@]}"; do
-        #     echo ${json_topics}
-        #     while IFS= read -r json_line; do
-        #         docker exec "${container_id}" bash -c "echo '$json_line' | /opt/kafka/bin/kafka-console-producer.sh --broker-list '${ip_host}:19193' --topic '${json_topic}'"
-        #         echo "echo '$json_line' | /opt/kafka/bin/kafka-console-producer.sh --broker-list '${ip_host}:19193' --topic '${json_topic}'"
-        #     done < "${ROOT}/docker-compose/kafka/scripts/${json_topic}.json"
-        # done
-        # copy kafka log to backup path
-        #docker cp "${container_id}":/opt/kafka/logs ${backup_dir}/kafka_logs
-    #}
+    }
 
     if [[ "${STOP}" -ne 1 ]]; then
         sudo docker compose -f "${ROOT}"/docker-compose/kafka/kafka.yaml --env-file "${ROOT}"/docker-compose/kafka/kafka.env up --build --remove-orphans -d
-        sleep 30s
-        #start_kafka_producers "${KAFKA_CONTAINER_ID}" "${IP_HOST}"
+        sleep 10s
+        create_kafka_topics "${KAFKA_CONTAINER_ID}" "${IP_HOST}"
     fi
 fi
 
