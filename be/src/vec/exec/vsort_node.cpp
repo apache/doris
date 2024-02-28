2a87783b459cee21db3ac1ed8c794ea5758154d2
Pxl <pxl290@qq.com>
2024-02-28 21:40:12 +0800

    [Opt](Exec) Support runtime update topn filter (#31250)

--- be/src/vec/exec/vsort_node.cpp
+++ be/src/vec/exec/vsort_node.cpp
@@ -82,18 +82,19 @@ Status VSortNode::init(const TPlanNode& tnode, RuntimeState* state) {
     // init runtime predicate
     _use_topn_opt = tnode.sort_node.use_topn_opt;
     if (_use_topn_opt) {
-        auto query_ctx = state->get_query_ctx();
+        auto* query_ctx = state->get_query_ctx();
         auto first_sort_expr_node = tnode.sort_node.sort_info.ordering_exprs[0].nodes[0];
         if (first_sort_expr_node.node_type == TExprNodeType::SLOT_REF) {
             auto first_sort_slot = first_sort_expr_node.slot_ref;
-            for (auto tuple_desc : this->intermediate_row_desc().tuple_descriptors()) {
+            for (auto* tuple_desc : this->intermediate_row_desc().tuple_descriptors()) {
                 if (tuple_desc->id() != first_sort_slot.tuple_id) {
                     continue;
                 }
-                for (auto slot : tuple_desc->slots()) {
+                for (auto* slot : tuple_desc->slots()) {
                     if (slot->id() == first_sort_slot.slot_id) {
-                        RETURN_IF_ERROR(query_ctx->get_runtime_predicate().init(slot->type().type,
-                                                                                _nulls_first[0]));
+                        RETURN_IF_ERROR(query_ctx->get_runtime_predicate().init(
+                                slot->type().type, _nulls_first[0], _is_asc_order[0],
+                                slot->col_name()));
                         break;
                     }
                 }
@@ -144,15 +145,9 @@ Status VSortNode::sink(RuntimeState* state, vectorized::Block* input_block, bool
         if (_use_topn_opt) {
             Field new_top = _sorter->get_top_value();
             if (!new_top.is_null() && new_top != old_top) {
-                auto& sort_description = _sorter->get_sort_description();
-                auto col = input_block->get_by_position(sort_description[0].column_number);
-                if (!col.name.empty()) {
-                    bool is_reverse = sort_description[0].direction < 0;
-                    auto* query_ctx = state->get_query_ctx();
-                    RETURN_IF_ERROR(query_ctx->get_runtime_predicate().update(new_top, col.name,
-                                                                              is_reverse));
-                    old_top = std::move(new_top);
-                }
+                auto* query_ctx = state->get_query_ctx();
+                RETURN_IF_ERROR(query_ctx->get_runtime_predicate().update(new_top));
+                old_top = std::move(new_top);
             }
         }
         if (!_reuse_mem) {
