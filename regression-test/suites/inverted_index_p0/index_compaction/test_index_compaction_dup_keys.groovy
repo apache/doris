350fbdf8dcc11b08fd489c579ee26f7d92475a68
qiye <jianliang5669@gmail.com>
2024-02-29 11:08:42 +0800

    [fix](regression test)Make index compaction caset nonCouncurrent (#31520)
    Co-authored-by: Luennng <luennng@gmail.com>

--- regression-test/suites/inverted_index_p0/index_compaction/test_index_compaction_dup_keys.groovy
+++ regression-test/suites/inverted_index_p0/index_compaction/test_index_compaction_dup_keys.groovy
@@ -17,7 +17,7 @@
 
 import org.codehaus.groovy.runtime.IOGroovyMethods
 
-suite("test_index_compaction_dup_keys", "p0") {
+suite("test_index_compaction_dup_keys", "nonConcurrent") {
     def tableName = "test_index_compaction_dup_keys"
     def backendId_to_backendIP = [:]
     def backendId_to_backendHttpPort = [:]
