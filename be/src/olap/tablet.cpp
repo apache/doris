31f1edd53e2ea16f0c6386f7a83196ed9c55980e
HappenLee <happenlee@hotmail.com>
2024-03-01 16:59:24 +0800

    [Bug](coredump) fix regresstion test coredump in multi thread access map

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -2537,8 +2537,7 @@ Status Tablet::check_rowid_conversion(
     }
     std::vector<segment_v2::SegmentSharedPtr> dst_segments;
     RETURN_IF_ERROR(_load_rowset_segments(dst_rowset, &dst_segments));
-    std::unordered_map<RowsetId, std::vector<segment_v2::SegmentSharedPtr>, HashOfRowsetId>
-            input_rowsets_segment;
+    std::unordered_map<RowsetId, std::vector<segment_v2::SegmentSharedPtr>> input_rowsets_segment;
 
     VLOG_DEBUG << "check_rowid_conversion, dst_segments size: " << dst_segments.size();
     for (auto [src_rowset, locations] : location_map) {
