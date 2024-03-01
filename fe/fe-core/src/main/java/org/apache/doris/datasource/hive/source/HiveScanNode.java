7fdb0a6f5e4875fff034ddfbcc1e04ad61bb584b
morningman <morningman@163.com>
2024-03-01 16:27:26 +0800

    [opt] refine some method name

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hive/source/HiveScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hive/source/HiveScanNode.java
@@ -507,7 +507,7 @@ public class HiveScanNode extends FileQueryScanNode {
 
     @Override
     public DataPartition constructInputPartitionByDistributionInfo() {
-        if (hmsTable.isBucketedTable()) {
+        if (hmsTable.isSparkBucketedTable()) {
             DistributionInfo distributionInfo = hmsTable.getDefaultDistributionInfo();
             if (!(distributionInfo instanceof HashDistributionInfo)) {
                 return DataPartition.RANDOM;
@@ -530,7 +530,7 @@ public class HiveScanNode extends FileQueryScanNode {
 
     @Override
     public THashType getHashType() {
-        if (hmsTable.isBucketedTable()
+        if (hmsTable.isSparkBucketedTable()
                 && hmsTable.getDefaultDistributionInfo() instanceof HashDistributionInfo) {
             return THashType.SPARK_MURMUR32;
         }
