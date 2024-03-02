58d816355085f7a08d1f6132de61a1db6ad6b9be
Yongqiang YANG <dataroaring@gmail.com>
2024-03-02 08:50:18 +0800

    [fix](cloud) enable all p0 cases in cloud p0

--- regression-test/pipeline/cloud_p1/conf/session_variables.sql
+++ regression-test/pipeline/cloud_p1/conf/session_variables.sql
@@ -4,3 +4,5 @@ set global enable_auto_analyze=false;
 set global enable_audit_plugin=true;
 set global enable_memtable_on_sink_node=false;
 set global enable_pipeline_x_engine=false;
+set global enable_pipeline_engine=false;
+set global enable_parallel_scan=false;
