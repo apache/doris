b78be401c245bfeec852d52879b45c7aac94bc7e
zhaochangle <zhaochangle@selectdb.com>
2024-03-03 22:55:24 +0800

    1

--- regression-test/suites/partition_p2/auto_partition/high_concur_load/stress_test_high_concurrency_load.groovy
+++ regression-test/suites/partition_p2/auto_partition/high_concur_load/stress_test_high_concurrency_load.groovy
@@ -21,7 +21,7 @@ import java.nio.file.Paths
 import java.net.URL
 import java.io.File
 
-suite("stress_test_high_concurrency_load") {
+suite("stress_test_high_concurrency_load", "p2,nonConcurrent") {
 
     sql """ADMIN SET FRONTEND CONFIG ('max_auto_partition_num' = '10000000')"""
 
