d125bf3eb331956b6daff84a4ae460c80d2af057
Yongqiang YANG <dataroaring@gmail.com>
2024-03-04 16:11:28 +0800

    fix

--- regression-test/pipeline/cloud_p0/conf/session_variables.sql
+++ regression-test/pipeline/cloud_p0/conf/session_variables.sql
@@ -3,6 +3,3 @@ set global insert_visible_timeout_ms=60000;
 set global enable_auto_analyze=false;
 set global enable_audit_plugin=true;
 set global enable_memtable_on_sink_node=false;
-set global enable_pipeline_x_engine=false;
-set global enable_pipeline_engine=false;
-set global enable_parallel_scan=false;
