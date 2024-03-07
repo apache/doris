a593cdcf4cc0cca3f20c2c595ce869ebeae9b764
Hao Tan <45457672+e0c9@users.noreply.github.com>
2024-03-07 10:52:01 +0800

    [fix](es-catalog) Add support for IPv6 address resolution in publish_address
    This merge request primarily focuses on enhancing the parsing of IPv6 addresses within the publish_address function. The updated code now supports IPv6 addresses in the format [xxx::xxxx::::xxx]:1234

--- fe/fe-core/src/main/java/org/apache/doris/datasource/es/EsNodeInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/es/EsNodeInfo.java
@@ -74,8 +74,10 @@ public class EsNodeInfo {
                 // if network.publish_host is set to hostname like localhost,
                 // the publish_address contains hostname like "localhost/127.0.0.1:9200"
                 address = address.substring(address.lastIndexOf('/') + 1);
-                String[] scratch = address.split(":");
-                this.publishAddress = new TNetworkAddress((httpSslEnabled ? "https://" : "") + scratch[0], Integer.parseInt(scratch[1]));
+                int pos = addrees.lastIndexOf(":");
+                String host = address.substring(0, pos);
+                String port = address.substring(pos + 1);
+                this.publishAddress = new TNetworkAddress((httpSslEnabled ? "https://" : "") + host, Integer.parseInt(port));
                 this.hasHttp = true;
             } else {
                 this.publishAddress = null;
