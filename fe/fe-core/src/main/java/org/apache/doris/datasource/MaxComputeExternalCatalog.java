1525ac803fb0df24cacdf3625017a678273cbb5b
slothever <jinzhe@selectdb.com>
2024-03-07 20:28:09 +0800

    2

--- fe/fe-core/src/main/java/org/apache/doris/datasource/MaxComputeExternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/MaxComputeExternalCatalog.java
@@ -19,10 +19,6 @@ package org.apache.doris.datasource;
 
 
 import org.apache.doris.common.DdlException;
-import org.apache.doris.datasource.CatalogProperty;
-import org.apache.doris.datasource.ExternalCatalog;
-import org.apache.doris.datasource.InitCatalogLog;
-import org.apache.doris.datasource.SessionContext;
 import org.apache.doris.datasource.credentials.CloudCredential;
 import org.apache.doris.datasource.property.constants.MCProperties;
 
