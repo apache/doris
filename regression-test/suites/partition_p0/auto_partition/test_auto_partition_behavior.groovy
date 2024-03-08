cdcc0a1234e3f1b424327cba88262697f9515a23
abmdocrt <Yukang.Lian2022@gmail.com>
2024-03-08 13:47:56 +0800

    [Fix](partial-update) Fix wrong column number passing to BE when partial and enable nereids (#31461)
    * Problem:
    Inconsistent behavior occurs when executing partial column update `UPDATE` statements and `INSERT` statements on merge-on-write tables with the Nereids optimizer enabled. The number of columns passed to BE differs; `UPDATE` operations incorrectly pass all columns, while `INSERT` operations correctly pass only the updated columns.
    
    Reason:
    The Nereids optimizer does not handle partial column update `UPDATE` statements properly. The processing logic for `UPDATE` statements rewrites them as equivalent `INSERT` statements, which are then processed according to the logic of `INSERT` statements. For example, assuming a MoW table structure with columns k1, k2, v1, v2, the correct rewrite should be:
    * `UPDATE` table t1 set v1 = v1 + 1 where k1 = 1 and k2 = 2
     * =>
     * `INSERT` into table (v1) select v1 + 1 from table t1 where k1 = 1 and k2 = 2
    
    However, the actual rewriting process does not consider the logic for partial column updates, leading to all columns being included in the `INSERT` statement, i.e., the result is:
    * `INSERT` into table (k1, k2, v1, v2) select k1, k2, v1 + 1, v2 from table t1 where k1 = 1 and k2 = 2
    
    This results in `UPDATE` operations incorrectly passing all columns to BE.
    
    Solution:
    Having analyzed the cause, the solution is straightforward: when rewriting partial column update `UPDATE` statements to `INSERT` statements, only retain the updated columns and all key columns (as partial column updates must include all key columns). Additionally, this PR includes error injection cases to verify the number of columns passed to BE is correct.
    
    * 2
    
    * 3
    
    * 4
    
    * 5

--- regression-test/suites/partition_p0/auto_partition/test_auto_partition_behavior.groovy
+++ regression-test/suites/partition_p0/auto_partition/test_auto_partition_behavior.groovy
@@ -60,13 +60,6 @@ suite("test_auto_partition_behavior") {
     result = sql "show partitions from unique_table"
     assertEquals(result.size(), 9)
     qt_sql3 """ select str,length(str) from unique_table order by `str` """
-    // modify value 
-    sql """ update unique_table set str = "modified" where str in (" ", "  ") """ // only "  "
-    qt_sql4 """ select str,length(str) from unique_table where str = '  ' order by `str` """ // modified
-    qt_sql5 """ select count() from unique_table where str = 'modified' """
-    // crop
-    qt_sql6 """ select str from unique_table where ((str > ' ! ' || str = 'modified') && str != 'Xxx') order by str """
-
 
     /// duplicate key table
     sql "drop table if exists dup_table"
