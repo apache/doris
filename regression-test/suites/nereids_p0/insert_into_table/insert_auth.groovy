65becbe54ef8b0e47782d85ef4bb8cb81fdbbc25
deardeng <565620795@qq.com>
2024-03-08 18:52:21 +0800

    [fix](cloud) Fix cloud create table need cluster auth and no cluster … (#31772)
    * [fix](cloud) Fix cloud create table need cluster auth and no cluster auth select throw npe
    
    ```
    2024-03-04 15:24:22,555 DEBUG (mysql-nio-pool-9|574) [Role.checkCloudPriv():411] failed to get wanted privs: priv predicate: OR, Admin_priv Usage_priv Cluster_Usage_priv , granted:
    2024-03-04 15:24:22,555 WARN (mysql-nio-pool-9|574) [ConnectContext.getCloudCluster():1094] cant get a valid cluster for user 'nereids_user'@'%' to use
    2024-03-04 15:24:22,555 DEBUG (mysql-nio-pool-9|574) [CloudCoordinator.prepare():67] get cluster by context null
    2024-03-04 15:24:22,555 WARN (mysql-nio-pool-9|574) [CloudCoordinator.prepare():75] invalid clusterName: null
    2024-03-04 15:24:22,555 WARN (mysql-nio-pool-9|574) [StmtExecutor.sendResult():1700] cancel fragment query_id:f4e0832dc2a94f0c-bba7afaee9bdb38b cause null
    2024-03-04 15:24:22,555 WARN (mysql-nio-pool-9|574) [Coordinator.cancel():1473] Cancel execution of query f4e0832dc2a94f0c-bba7afaee9bdb38b, this is a outside invoke
    2024-03-04 15:24:22,555 INFO (mysql-nio-pool-9|574) [QueryQueue.returnToken():154] wgId= 1, version=0,maxConcurrency=2147483647, maxQueueSize=0, queueTimeout=0, currentRunningQueryNum=5, currentWaitingQueryNum=0
    2024-03-04 15:24:22,555 DEBUG (mysql-nio-pool-9|574) [QeProcessorImpl.unregisterQuery():145] Deregister query id f4e0832dc2a94f0c-bba7afaee9bdb38b
    2024-03-04 15:24:22,555 WARN (mysql-nio-pool-9|574) [ConnectProcessor.handleQueryException():361] Process one query failed because unknown reason:
    java.lang.NullPointerException: null
            at org.apache.doris.qe.Coordinator.computeScanRangeAssignment(Coordinator.java:2280) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.Coordinator.processFragmentAssignmentAndParams(Coordinator.java:578) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.cloud.qe.CloudCoordinator.processFragmentAssignmentAndParams(CloudCoordinator.java:84) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.Coordinator.execInternal(Coordinator.java:651) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.Coordinator.exec(Coordinator.java:630) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.sendResult(StmtExecutor.java:1589) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.handleQueryStmt(StmtExecutor.java:1562) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.handleQueryWithRetry(StmtExecutor.java:711) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.executeByNereids(StmtExecutor.java:663) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.execute(StmtExecutor.java:496) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.StmtExecutor.execute(StmtExecutor.java:475) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.ConnectProcessor.executeQuery(ConnectProcessor.java:279) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.ConnectProcessor.handleQuery(ConnectProcessor.java:197) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.MysqlConnectProcessor.handleQuery(MysqlConnectProcessor.java:176) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.MysqlConnectProcessor.dispatch(MysqlConnectProcessor.java:205) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.qe.MysqlConnectProcessor.processOnce(MysqlConnectProcessor.java:258) ~[doris-fe.jar:1.2-SNAPSHOT]
            at org.apache.doris.mysql.ReadListener.lambda$handleEvent$0(ReadListener.java:52) ~[doris-fe.jar:1.2-SNAPSHOT]
            at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1142) ~[?:1.8.0_131]
            at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:617) ~[?:1.8.0_131]
            at java.lang.Thread.run(Thread.java:748) ~[?:1.8.0_131]
    ```
    
    * fix more regression case run in cloud
    
    * fix review
    
    * fix

--- regression-test/suites/nereids_p0/insert_into_table/insert_auth.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/insert_auth.groovy
@@ -48,6 +48,14 @@ suite('nereids_insert_auth') {
     try_sql("DROP USER ${user}")
     sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
 
+    //cloud-mode
+    if (isCloudMode()) {
+        def clusters = sql " SHOW CLUSTERS; "
+        assertTrue(!clusters.isEmpty())
+        def validCluster = clusters[0][0]
+        sql """GRANT USAGE_PRIV ON CLUSTER ${validCluster} TO ${user}""";
+    }
+
     connect(user=user, password="${pwd}", url=url) {
         try {
             sql """ insert into ${db}.${t1} values (1, 1) """
