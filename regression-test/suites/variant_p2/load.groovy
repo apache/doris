ef1fdb31e516cd37aae5f35fbc5c2a17f810031f
eldenmoon <15605149486@163.com>
2024-03-08 17:13:34 +0800

    [regression-test](Variant) add clickbench test for variant type

--- regression-test/suites/variant_p2/load.groovy
+++ regression-test/suites/variant_p2/load.groovy
@@ -17,8 +17,7 @@
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-suite("github_event_advance_p2", "variant_type,p2"){
-
+suite("variant_p2_load", "variant_type,p2"){
     def load_json_data = {table_name, file_name ->
         // load the json data
         streamLoad {
@@ -47,92 +46,119 @@ suite("github_event_advance_p2", "variant_type,p2"){
         }
     }
 
-    def create_table = {table_name, buckets="auto" ->
-        sql "DROP TABLE IF EXISTS ${table_name}"
-        sql """
-            CREATE TABLE IF NOT EXISTS ${table_name} (
-            id BIGINT NOT NULL,
-            type VARCHAR(30) NULL,
-            actor VARIANT NULL,
-            repo VARIANT NULL,
-            payload VARIANT NULL,
-            public BOOLEAN NULL,
-            created_at DATETIME NULL,
-            org JSON NULL
-            -- INDEX idx_payload(payload) USING INVERTED PROPERTIES("parser" = "english") COMMENT '',
-            -- INDEX idx_repo(repo) USING INVERTED PROPERTIES("parser" = "english") COMMENT '',
-            -- INDEX idx_actor(actor) USING INVERTED PROPERTIES("parser" = "english") COMMENT ''
-        )
-        DUPLICATE KEY(`id`)
-        DISTRIBUTED BY HASH(id) BUCKETS ${buckets}
-        properties("replication_num" = "1", "disable_auto_compaction" = "false");
-        """
-    }
-
+    def backendId_to_backendIP = [:]
+    def backendId_to_backendHttpPort = [:]
+    getBackendIpHttpPort(backendId_to_backendIP, backendId_to_backendHttpPort);
     def set_be_config = { key, value ->
-        String backend_id;
-        def backendId_to_backendIP = [:]
-        def backendId_to_backendHttpPort = [:]
-        getBackendIpHttpPort(backendId_to_backendIP, backendId_to_backendHttpPort);
-
-        backend_id = backendId_to_backendIP.keySet()[0]
-        def (code, out, err) = update_be_config(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), key, value)
-        logger.info("update config: code=" + code + ", out=" + out + ", err=" + err)
+        for (String backend_id: backendId_to_backendIP.keySet()) {
+            def (code, out, err) = update_be_config(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id), key, value)
+            logger.info("update config: code=" + code + ", out=" + out + ", err=" + err)
+        }
     }
 
-    // Configuration for the number of threads
-    def numberOfThreads = 10 // Set this to your desired number of threads
-
-    // Executor service for managing threads
-    def executorService = Executors.newFixedThreadPool(numberOfThreads)
-
-    try {
-        def table_name = "github_events"
-        set_be_config.call("variant_ratio_of_defaults_as_sparse_column", "1.0")
-        def s3load_paral_wait = {tbl, fmt, path, paral ->
-            String ak = getS3AK()
-            String sk = getS3SK()
-            String s3BucketName = getS3BucketName()
-            String s3Endpoint = getS3Endpoint()
-            String s3Region = getS3Region()
-            def load_label = "part_" + UUID.randomUUID().toString().replace("-", "0")
-            sql """
-                LOAD LABEL ${load_label} (
-                    DATA INFILE("s3://${s3BucketName}/${path}")
-                    INTO TABLE ${tbl}
-                    COLUMNS TERMINATED BY ","
-                    FORMAT AS "${fmt}"
-                )
-                WITH S3 (
-                    "AWS_ACCESS_KEY" = "$ak",
-                    "AWS_SECRET_KEY" = "$sk",
-                    "AWS_ENDPOINT" = "${s3Endpoint}",
-                    "AWS_REGION" = "${s3Region}"
-                )
-                PROPERTIES(
-                    "load_parallelism" = "${paral}"
-                );
-            """
-            // Waiting for job finished or cancelled
-            def max_try_milli_secs = 600000
-            while (max_try_milli_secs > 0) {
-                String[][] result = sql """ show load where label="$load_label" order by createtime desc limit 1; """
-                if (result[0][2].equals("FINISHED")) {
-                    logger.info("Load FINISHED " + load_label)
-                    break;
-                }
-                if (result[0][2].equals("CANCELLED")) {
-                    log.warn("load failed: $result")
-                    assertTrue(false, "load failed: $result")
-                    break;
-                }
-                Thread.sleep(6000)
-                max_try_milli_secs -= 6000
-                if(max_try_milli_secs <= 0) {
-                    assertTrue(1 == 2, "load Timeout: $load_label")
-                }
+    def s3load_paral_wait = {tbl, fmt, path, paral ->
+        String ak = getS3AK()
+        String sk = getS3SK()
+        String s3BucketName = getS3BucketName()
+        String s3Endpoint = getS3Endpoint()
+        String s3Region = getS3Region()
+        def load_label = "part_" + UUID.randomUUID().toString().replace("-", "0")
+        sql """
+            LOAD LABEL ${load_label} (
+                DATA INFILE("s3://${s3BucketName}/${path}")
+                INTO TABLE ${tbl}
+                COLUMNS TERMINATED BY ","
+                FORMAT AS "${fmt}"
+            )
+            WITH S3 (
+                "AWS_ACCESS_KEY" = "$ak",
+                "AWS_SECRET_KEY" = "$sk",
+                "AWS_ENDPOINT" = "${s3Endpoint}",
+                "AWS_REGION" = "${s3Region}"
+            )
+            PROPERTIES(
+                "load_parallelism" = "${paral}"
+            );
+        """
+        // Waiting for job finished or cancelled
+        def max_try_milli_secs = 600000
+        while (max_try_milli_secs > 0) {
+            String[][] result = sql """ show load where label="$load_label" order by createtime desc limit 1; """
+            if (result[0][2].equals("FINISHED")) {
+                logger.info("Load FINISHED " + load_label)
+                break;
+            }
+            if (result[0][2].equals("CANCELLED")) {
+                log.warn("load failed: $result")
+                assertTrue(false, "load failed: $result")
+                break;
+            }
+            Thread.sleep(6000)
+            max_try_milli_secs -= 6000
+            if(max_try_milli_secs <= 0) {
+                assertTrue(1 == 2, "load Timeout: $load_label")
             }
         }
+    }
+
+    def load_ckb = { ->
+        def create_table = { table_name, buckets="auto" ->
+            sql "DROP TABLE IF EXISTS ${table_name}"
+            sql """CREATE TABLE ${table_name} (
+                    EventDate DateV2,
+                    EventTime DateTimeV2 ,
+                    ClientEventTime DateTimeV2 ,
+                    LocalEventTime DateTimeV2 ,
+                    UserID BIGINT ,
+                    WatchID BIGINT ,
+                    FUniqID BIGINT ,
+                    ParamPrice BIGINT ,
+                    RefererHash BIGINT ,
+                    URLHash BIGINT ,
+                    var VARIANT,
+                    INDEX idx_var (`var`) USING INVERTED COMMENT ''
+                )
+                DUPLICATE KEY (EventDate, EventTime)
+                DISTRIBUTED BY HASH(EventDate, EventTime) BUCKETS 16
+                PROPERTIES ( "replication_num"="1");
+            """
+        }
+        def table_name = "ckb_hits"
+        create_table.call(table_name)
+        s3load_paral_wait.call(table_name, "JSON", "regression/variant/ckb/*", 3)
+        qt_sql("select count() from ${table_name}")
+    }
+
+    def load_gh = { ->
+        def create_table = {table_name, buckets="auto" ->
+            sql "DROP TABLE IF EXISTS ${table_name}"
+            sql """
+                CREATE TABLE IF NOT EXISTS ${table_name} (
+                id BIGINT NOT NULL,
+                type VARCHAR(30) NULL,
+                actor VARIANT NULL,
+                repo VARIANT NULL,
+                payload VARIANT NULL,
+                public BOOLEAN NULL,
+                created_at DATETIME NULL,
+                org JSON NULL
+                -- INDEX idx_payload(payload) USING INVERTED PROPERTIES("parser" = "english") COMMENT '',
+                -- INDEX idx_repo(repo) USING INVERTED PROPERTIES("parser" = "english") COMMENT '',
+                -- INDEX idx_actor(actor) USING INVERTED PROPERTIES("parser" = "english") COMMENT ''
+            )
+            DUPLICATE KEY(`id`)
+            DISTRIBUTED BY HASH(id) BUCKETS ${buckets}
+            properties("replication_num" = "1", "disable_auto_compaction" = "false");
+            """
+        }
+
+        // Configuration for the number of threads
+        def numberOfThreads = 5 // Set this to your desired number of threads
+
+        // Executor service for managing threads
+        def executorService = Executors.newFixedThreadPool(numberOfThreads)
+
+        def table_name = "github_events"
         create_table.call(table_name)
         List<Long> daysEveryMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         // 2015
@@ -164,6 +190,11 @@ suite("github_event_advance_p2", "variant_type,p2"){
         executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)
 
         qt_sql("select count() from github_events")
+    }
+    try {
+        set_be_config.call("variant_ratio_of_defaults_as_sparse_column", "1.0")
+        load_ckb.call()
+        load_gh.call()
     } finally {
         // reset flags
         set_be_config.call("variant_ratio_of_defaults_as_sparse_column", "0.95")
