1c59726d58750b682ae419b9ddcdf722806ccfcc
Tiewei Fang <43782773+BePPPower@users.noreply.github.com>
2024-03-11 17:49:13 +0800

    [Feature](TrinoConnector-Catalog) Be supports trino-connector catalog (#31879)

--- fe/fe-core/src/main/java/org/apache/doris/datasource/property/constants/TrinoConnectorProperties.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/property/constants/TrinoConnectorProperties.java
@@ -19,26 +19,4 @@ package org.apache.doris.datasource.property.constants;
 
 public class TrinoConnectorProperties {
     public static final String TRINO_CONNECTOR_NAME = "connector.name";
-
-    // ================= hive connector =================
-    public static final String TRINO_CONNECTOR_HIVE_METASTORE_URI = "hive.metastore.uri";
-    public static final String TRINO_CONNECTOR_HIVE_CONFIG_RESOURCES = "hive.config.resources";
-
-    // ================= mysql connector =================
-    public static final String TRINO_MYSQL_CONNECTION_URL =  "connection-url";
-    public static final String TRINO_MYSQL_CONNECTION_USER = "connection-user";
-    public static final String TRINO_MYSQL_CONNECTION_PASSWORD = "connection-password";
-
-    public static final String TRINO_CONNECTOR_PREFIX = "";
-    public static final String TRINO_CONNECTOR_CATALOG_TYPE = "metastore";
-    public static final String HIVE_METASTORE_URIS = "uri";
-    public static final String TRINO_CONNECTOR_S3_ENDPOINT = "s3.endpoint";
-    public static final String TRINO_CONNECTOR_S3_ACCESS_KEY = "s3.access-key";
-    public static final String TRINO_CONNECTOR_S3_SECRET_KEY = "s3.secret-key";
-    public static final String TRINO_CONNECTOR_OSS_ENDPOINT = org.apache.hadoop.fs.aliyun.oss.Constants.ENDPOINT_KEY;
-    public static final String TRINO_CONNECTOR_OSS_ACCESS_KEY = org.apache.hadoop.fs.aliyun.oss.Constants.ACCESS_KEY_ID;
-    public static final String TRINO_CONNECTOR_OSS_SECRET_KEY =
-            org.apache.hadoop.fs.aliyun.oss.Constants.ACCESS_KEY_SECRET;
-    public static final String TRINO_CONNECTOR_HMS_CATALOG = "hive";
-    public static final String TRINO_CONNECTOR_FILESYSTEM_CATALOG = "filesystem";
 }
