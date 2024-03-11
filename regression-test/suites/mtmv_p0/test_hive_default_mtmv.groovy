fdcde97e3647a75d847d9d7d57bf7d09a44dc18b
zhangdong <493738387@qq.com>
2024-03-11 18:07:43 +0800

    1

--- regression-test/suites/mtmv_p0/test_hive_default_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_hive_default_mtmv.groovy
@@ -36,12 +36,12 @@ suite("test_hive_default_mtmv", "p0,external,hive,external_docker,external_docke
                                 STORED AS ORC;
                             """
 
-    // def set_dynamic_partition_str = """ set hive.exec.dynamic.partition=true;"""
-    // def set_dynamic_partition_mode_str = """ set hive.exec.dynamic.partition.mode=nonstrict;"""
-    def insert_str = """
-        insert into ${hive_database}.${hive_table} PARTITION(year=2020,month) values(1,1,1);
-        """
-    def show_partition_str = """show partitions ${hive_database}.${hive_table}"""
+    def insert_str1 = """
+                        insert into ${hive_database}.${hive_table} PARTITION(year=2020,month) values(1,1,1);
+                        """
+    def insert_str1 = """
+                        insert into ${hive_database}.${hive_table} PARTITION(year=2020,month) values(2,2,null);
+                        """
 
     logger.info("hive sql: " + drop_table_str)
     hive_docker """ ${drop_table_str} """
@@ -51,15 +51,62 @@ suite("test_hive_default_mtmv", "p0,external,hive,external_docker,external_docke
     hive_docker """ ${create_database_str}"""
     logger.info("hive sql: " + create_table_str)
     hive_docker """ ${create_table_str} """
-    //logger.info("hive sql: " + set_dynamic_partition_str)
-    //hive_docker """ ${set_dynamic_partition_str} """
-    //logger.info("hive sql: " + set_dynamic_partition_mode_str)
-    //hive_docker """ ${set_dynamic_partition_mode_str} """
     logger.info("hive sql: " + insert_str)
     hive_docker """ ${insert_str} """
-    logger.info("hive sql: " + show_partition_str)
-    def a = hive_docker """ ${show_partition_str} """
-    logger.info("result: " + a)
+    logger.info("hive sql: " + insert_str1)
+    hive_docker """ ${insert_str1} """
+
+
+    // prepare catalog
+    String hms_port = context.config.otherConfigs.get("hms_port")
+    String catalog_name = "hive_default_mtmv_test_catalog"
+    String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
+
+    sql """drop catalog if exists ${catalog_name}"""
+    sql """create catalog if not exists ${catalog_name} properties (
+        "type"="hms",
+        'hive.metastore.uris' = 'thrift://${externalEnvIp}:${hms_port}'
+    );"""
+
+    order_qt_test "SELECT * FROM ${catalog_name}.${hive_database}.${hive_table} order by num"
+
+
+    // prepare mtmv
+    def mvName = "test_hive_default_mtmv"
+    def dbName = "regression_test_mtmv_p0"
+    sql """drop materialized view if exists ${mvName};"""
+
+    sql """
+        CREATE MATERIALIZED VIEW ${mvName}
+            BUILD DEFERRED REFRESH AUTO ON MANUAL
+            partition by(`month`)
+            DISTRIBUTED BY RANDOM BUCKETS 2
+            PROPERTIES ('replication_num' = '1')
+            AS
+            SELECT user_id,num,year,month FROM ${catalog_name}.${hive_database}.${hive_table};
+        """
+    def showPartitionsResult = sql """show partitions from ${mvName}"""
+    logger.info("showPartitionsResult: " + showPartitionsResult.toString())
+    assertTrue(showPartitionsResult.toString().contains("p_NULL"))
+    assertTrue(showPartitionsResult.toString().contains("p_1"))
+
+    sql """
+            REFRESH MATERIALIZED VIEW ${mvName} partitions(p_NULL);
+        """
+    def jobName = getJobName(dbName, mvName);
+    waitingMTMVTaskFinished(jobName)
+    order_qt_null "SELECT * FROM ${mvName} order by num"
+
+    sql """
+            REFRESH MATERIALIZED VIEW ${mvName} partitions(p_1);
+        """
+    def jobName = getJobName(dbName, mvName);
+    waitingMTMVTaskFinished(jobName)
+    order_qt_1 "SELECT * FROM ${mvName} order by num"
+
+    sql """drop materialized view if exists ${mvName};"""
+
+    sql """drop catalog if exists ${catalog_name}"""
 
 }
 
