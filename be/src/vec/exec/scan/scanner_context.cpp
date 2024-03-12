61928f7df53f5ffbaf2794cee8b2fc56100b90f7
Gabriel <gabrielleebuaa@gmail.com>
2024-03-12 12:11:06 +0800

    [pipelineX](scanner) Use the actual instances num when ignore data distribution (#32081)

--- be/src/vec/exec/scan/scanner_context.cpp
+++ be/src/vec/exec/scan/scanner_context.cpp
@@ -76,7 +76,8 @@ ScannerContext::ScannerContext(RuntimeState* state, const TupleDescriptor* outpu
     _max_thread_num = _state->num_scanner_threads() > 0
                               ? _state->num_scanner_threads()
                               : config::doris_scanner_thread_pool_thread_num /
-                                        state->query_parallel_instance_num();
+                                        (_local_state ? num_parallel_instances
+                                                      : state->query_parallel_instance_num());
     _max_thread_num *= num_parallel_instances;
     _max_thread_num = _max_thread_num == 0 ? 1 : _max_thread_num;
     _max_thread_num = std::min(_max_thread_num, (int32_t)scanners.size());
