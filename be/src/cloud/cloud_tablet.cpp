41eaa0c67398ea3f69deb846528a26767b45ad54
Luwei <814383175@qq.com>
2024-03-12 16:14:23 +0800

    [Fix](merge-cloud) calculate delete bitmap during cloud full compaction

--- be/src/cloud/cloud_tablet.cpp
+++ be/src/cloud/cloud_tablet.cpp
@@ -509,7 +509,18 @@ std::vector<RowsetSharedPtr> CloudTablet::pick_candidate_rowsets_to_single_repli
 }
 
 std::vector<RowsetSharedPtr> CloudTablet::pick_candidate_rowsets_to_full_compaction() {
-    return pick_candidate_rowsets_to_single_replica_compaction();
+    std::vector<RowsetSharedPtr> candidate_rowsets;
+    {
+        std::shared_lock rlock(_meta_lock);
+        for (auto& [v, rs] : _rs_version_map) {
+            // MUST NOT compact rowset [0-1] for some historical reasons (see cloud_schema_change)
+            if (v.first != 0) {
+                candidate_rowsets.push_back(rs);
+            }
+        }
+    }
+    std::sort(candidate_rowsets.begin(), candidate_rowsets.end(), Rowset::comparator);
+    return candidate_rowsets;
 }
 
 CalcDeleteBitmapExecutor* CloudTablet::calc_delete_bitmap_executor() {
