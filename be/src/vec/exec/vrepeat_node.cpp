1f74c7329abcb0d89e8e9e04ab69c1fec28d99e1
Mryange <59914473+Mryange@users.noreply.github.com>
2024-03-14 13:09:49 +0800

    [fix](Operator) RepeatNode does not handle empty expressions. (#32112)
    In the past, RepeatNode did not handle empty expressions.
    It used DCHECK to check if the expression was non-empty.
    In non-debug mode, this caused _child_block to remain unprocessed, resulting in a deadlock.
    Now, if the expression is empty, the output block directly outputs _child_block

--- be/src/vec/exec/vrepeat_node.cpp
+++ be/src/vec/exec/vrepeat_node.cpp
@@ -191,6 +191,10 @@ Status VRepeatNode::pull(doris::RuntimeState* state, vectorized::Block* output_b
             release_block_memory(_child_block);
             _repeat_id_idx = 0;
         }
+    } else if (_expr_ctxs.empty()) {
+        DCHECK(!_intermediate_block || (_intermediate_block && _intermediate_block->rows() == 0));
+        output_block->swap(*_child_block);
+        release_block_memory(*_child_block);
     }
     RETURN_IF_ERROR(VExprContext::filter_block(_conjuncts, output_block, output_block->columns()));
     *eos = _child_eos && _child_block.rows() == 0;
@@ -202,7 +206,6 @@ Status VRepeatNode::pull(doris::RuntimeState* state, vectorized::Block* output_b
 Status VRepeatNode::push(RuntimeState* state, vectorized::Block* input_block, bool eos) {
     _child_eos = eos;
     DCHECK(!_intermediate_block || _intermediate_block->rows() == 0);
-    DCHECK(!_expr_ctxs.empty());
 
     if (input_block->rows() > 0) {
         _intermediate_block = Block::create_unique();
