apache/doris

commit 836cda65d8296af8d278b6f9614b885de0c62050

[refactor](profilev2) split merged profile to a single runtime profile to make the logic more clear (#27184)

fe/fe-core/src/main/java/org/apache/doris/common/profile/AggregatedProfile.java
@@ -1,0 +1,41 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.common.profile;
+
+import org.apache.doris.common.util.RuntimeProfile;
+
+import java.util.Map;
+
+/**
+* AggregatedProfile is part of a query profile.
+* It contains the aggregated information of a query.
+*/
+public class AggregatedProfile {
+
+    public static final String PROFILE_NAME = "MergedProfile";
+    private ExecutionProfile executionProfile;
+
+    public AggregatedProfile(RuntimeProfile rootProfile, ExecutionProfile executionProfile) {
+        this.executionProfile = executionProfile;
+    }
+
+    public RuntimeProfile getAggregatedFragmentsProfile(Map<Integer, String> planNodeMap) {
+        return executionProfile.getAggregatedFragmentsProfile(planNodeMap);
+    }
+
+}

fe/fe-core/src/main/java/org/apache/doris/common/profile/Profile.java
@@ -21,34 +21,33 @@ import org.apache.doris.common.util.ProfileManager;
 import org.apache.doris.common.util.RuntimeProfile;
 import org.apache.doris.planner.Planner;
 
-import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.List;
 import java.util.Map;
 
 /**
- * Profile is a class to record the execution time of a query.
- * It has the following structure:
- * root profile:
- *     // summary of this profile, such as start time, end time, query id, etc.
- *     [SummaryProfile]
- *     // each execution profile is a complete execution of a query, a job may contain multiple queries.
- *     [List<ExecutionProfile>]
+ * Profile is a class to record the execution time of a query. It has the
+ * following structure: root profile: // summary of this profile, such as start
+ * time, end time, query id, etc. [SummaryProfile] // each execution profile is
+ * a complete execution of a query, a job may contain multiple queries.
+ * [List<ExecutionProfile>]
  *
- * SummaryProfile:
- *     Summary:
- *         Execution Summary:
+ * SummaryProfile: Summary: Execution Summary:
  *
- * ExecutionProfile:
- *     Fragment 0:
- *     Fragment 1:
- *     ...
+ *
+ * ExecutionProfile: Fragment 0: Fragment 1: ...
  */
 public class Profile {
+    private static final Logger LOG = LogManager.getLogger(Profile.class);
     private RuntimeProfile rootProfile;
     private SummaryProfile summaryProfile;
-    private List<ExecutionProfile> executionProfiles = Lists.newArrayList();
+    private AggregatedProfile aggregatedProfile;
+    private ExecutionProfile executionProfile;
     private boolean isFinished;
+    private Map<Integer, String> planNodeMap;
 
     public Profile(String name, boolean isEnable) {
         this.rootProfile = new RuntimeProfile(name);
@@ -57,29 +56,70 @@ public class Profile {
         this.isFinished = !isEnable;
     }
 
-    public void addExecutionProfile(ExecutionProfile executionProfile) {
-        this.executionProfiles.add(executionProfile);
-        executionProfile.addToProfileAsChild(rootProfile);
+    public void setExecutionProfile(ExecutionProfile executionProfile) {
+        if (executionProfile == null) {
+            LOG.warn("try to set a null excecution profile, it is abnormal", new Exception());
+            return;
+        }
+        this.executionProfile = executionProfile;
+        this.executionProfile.addToProfileAsChild(rootProfile);
+        this.aggregatedProfile = new AggregatedProfile(rootProfile, executionProfile);
     }
 
     public synchronized void update(long startTime, Map<String, String> summaryInfo, boolean isFinished,
             int profileLevel, Planner planner, boolean isPipelineX) {
-        if (this.isFinished) {
-            return;
-        }
-        summaryProfile.update(summaryInfo);
-        for (ExecutionProfile executionProfile : executionProfiles) {
+        try {
+            if (this.isFinished) {
+                return;
+            }
+            if (executionProfile == null) {
+                // Sometimes execution profile is not set
+                return;
+            }
+            summaryProfile.update(summaryInfo);
             executionProfile.update(startTime, isFinished);
+            rootProfile.computeTimeInProfile();
+            // Nerids native insert not set planner, so it is null
+            if (planner != null) {
+                this.planNodeMap = planner.getExplainStringMap();
+            }
+            rootProfile.setIsPipelineX(isPipelineX);
+            ProfileManager.getInstance().pushProfile(this);
+            this.isFinished = isFinished;
+        } catch (Throwable t) {
+            LOG.warn("update profile failed", t);
+            throw t;
         }
-        rootProfile.computeTimeInProfile();
-        rootProfile.setFragmentPlanInfo(planner);
-        rootProfile.setProfileLevel(profileLevel);
-        rootProfile.setIsPipelineX(isPipelineX);
-        ProfileManager.getInstance().pushProfile(rootProfile);
-        this.isFinished = isFinished;
+    }
+
+    public RuntimeProfile getRootProfile() {
+        return this.rootProfile;
     }
 
     public SummaryProfile getSummaryProfile() {
         return summaryProfile;
+    }
+
+    public String getProfileByLevel() {
+        StringBuilder builder = new StringBuilder();
+        // add summary to builder
+        summaryProfile.prettyPrint(builder);
+        LOG.info(builder.toString());
+        builder.append("\n MergedProfile \n");
+        aggregatedProfile.getAggregatedFragmentsProfile(planNodeMap).prettyPrint(builder, "     ");
+        try {
+            builder.append("\n");
+            executionProfile.getExecutionProfile().prettyPrint(builder, "");
+            LOG.info(builder.toString());
+        } catch (Throwable aggProfileException) {
+            LOG.warn("build merged simple profile failed", aggProfileException);
+            builder.append("build merged simple profile failed");
+        }
+        return builder.toString();
+    }
+
+    public String getProfileBrief() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(rootProfile.toBrief());
     }
 }

fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadLoadingTask.java
@@ -137,7 +137,7 @@ public class LoadLoadingTask extends LoadTask {
         Coordinator curCoordinator = new Coordinator(callback.getCallbackId(), loadId, planner.getDescTable(),
                 planner.getFragments(), planner.getScanNodes(), planner.getTimezone(), loadZeroTolerance);
         if (this.jobProfile != null) {
-            this.jobProfile.addExecutionProfile(curCoordinator.getExecutionProfile());
+            this.jobProfile.setExecutionProfile(curCoordinator.getExecutionProfile());
         }
         curCoordinator.setQueryType(TQueryType.LOAD);
         curCoordinator.setExecMemoryLimit(execMemLimit);

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -893,10 +893,16 @@ public class StmtExecutor {
         if (!context.getSessionVariable().enableProfile()) {
             return;
         }
-
-        profile.update(context.startTime, getSummaryInfo(isFinished), isFinished,
-                context.getSessionVariable().profileLevel, this.planner,
-                context.getSessionVariable().getEnablePipelineXEngine());
+        // If any error happends in update profile, we should ignore this error
+        // and ensure the sql is finished normally. For example, if update profile
+        // failed, the insert stmt should be success
+        try {
+            profile.update(context.startTime, getSummaryInfo(isFinished), isFinished,
+                    context.getSessionVariable().profileLevel, this.planner,
+                    context.getSessionVariable().getEnablePipelineXEngine());
+        } catch (Throwable t) {
+            LOG.warn("failed to update profile, ingore this error", t);
+        }
     }
 
     // Analyze one statement to structure in memory.
@@ -1447,7 +1453,7 @@ public class StmtExecutor {
             }
             QeProcessorImpl.INSTANCE.registerQuery(context.queryId(),
                     new QeProcessorImpl.QueryInfo(context, originStmt.originStmt, coord));
-            profile.addExecutionProfile(coord.getExecutionProfile());
+            profile.setExecutionProfile(coord.getExecutionProfile());
             coordBase = coord;
         }
 
@@ -1949,7 +1955,7 @@ public class StmtExecutor {
                 coord = new Coordinator(context, analyzer, planner, context.getStatsErrorEstimator());
                 coord.setLoadZeroTolerance(context.getSessionVariable().getEnableInsertStrict());
                 coord.setQueryType(TQueryType.LOAD);
-                profile.addExecutionProfile(coord.getExecutionProfile());
+                profile.setExecutionProfile(coord.getExecutionProfile());
 
                 QeProcessorImpl.INSTANCE.registerQuery(context.queryId(), coord);
 
@@ -2710,7 +2716,7 @@ public class StmtExecutor {
             }
             RowBatch batch;
             coord = new Coordinator(context, analyzer, planner, context.getStatsErrorEstimator());
-            profile.addExecutionProfile(coord.getExecutionProfile());
+            profile.setExecutionProfile(coord.getExecutionProfile());
             try {
                 QeProcessorImpl.INSTANCE.registerQuery(context.queryId(),
                         new QeProcessorImpl.QueryInfo(context, originStmt.originStmt, coord));

be/src/exec/data_sink.h
@@ -126,6 +126,16 @@ protected:
 
     // Maybe this will be transferred to BufferControlBlock.
     std::shared_ptr<QueryStatistics> _query_statistics;
+
+    RuntimeProfile::Counter* _exec_timer = nullptr;
+    RuntimeProfile::Counter* _blocks_sent_counter = nullptr;
+    RuntimeProfile::Counter* _output_rows_counter = nullptr;
+
+    void init_sink_common_profile() {
+        _exec_timer = ADD_TIMER_WITH_LEVEL(_profile, "ExecTime", 1);
+        _output_rows_counter = ADD_COUNTER_WITH_LEVEL(_profile, "OutputRows", TUnit::UNIT, 1);
+        _blocks_sent_counter = ADD_COUNTER_WITH_LEVEL(_profile, "OutputBlockCount", TUnit::UNIT, 1);
+    }
 };
 
 } // namespace doris

be/src/exec/exec_node.cpp
@@ -127,11 +127,11 @@ Status ExecNode::init(const TPlanNode& tnode, RuntimeState* state) {
 Status ExecNode::prepare(RuntimeState* state) {
     DCHECK(_runtime_profile.get() != nullptr);
     _exec_timer = ADD_TIMER_WITH_LEVEL(runtime_profile(), "ExecTime", 1);
-    _rows_returned_counter =
-            ADD_COUNTER_WITH_LEVEL(_runtime_profile, "RowsReturned", TUnit::UNIT, 1);
+    _rows_returned_counter = ADD_COUNTER_WITH_LEVEL(_runtime_profile, "OutputRows", TUnit::UNIT, 1);
     _output_bytes_counter =
             ADD_COUNTER_WITH_LEVEL(_runtime_profile, "OutputBytes", TUnit::BYTES, 1);
-    _block_count_counter = ADD_COUNTER_WITH_LEVEL(_runtime_profile, "BlockCount", TUnit::UNIT, 1);
+    _block_count_counter =
+            ADD_COUNTER_WITH_LEVEL(_runtime_profile, "OutputBlockCount", TUnit::UNIT, 1);
     _projection_timer = ADD_TIMER(_runtime_profile, "ProjectionTime");
     _rows_returned_rate = runtime_profile()->add_derived_counter(
             ROW_THROUGHPUT_COUNTER, TUnit::UNIT_PER_SECOND,

be/src/pipeline/exec/exchange_sink_operator.h
@@ -197,27 +197,27 @@ private:
     friend class vectorized::BlockSerializer<ExchangeSinkLocalState>;
 
     std::unique_ptr<ExchangeSinkBuffer<ExchangeSinkLocalState>> _sink_buffer;
-    RuntimeProfile::Counter* _serialize_batch_timer;
-    RuntimeProfile::Counter* _compress_timer;
-    RuntimeProfile::Counter* _brpc_send_timer;
-    RuntimeProfile::Counter* _brpc_wait_timer;
-    RuntimeProfile::Counter* _bytes_sent_counter;
-    RuntimeProfile::Counter* _uncompressed_bytes_counter;
-    RuntimeProfile::Counter* _local_sent_rows;
-    RuntimeProfile::Counter* _local_send_timer;
-    RuntimeProfile::Counter* _split_block_hash_compute_timer;
-    RuntimeProfile::Counter* _split_block_distribute_by_channel_timer;
-    RuntimeProfile::Counter* _blocks_sent_counter;
+    RuntimeProfile::Counter* _serialize_batch_timer = nullptr;
+    RuntimeProfile::Counter* _compress_timer = nullptr;
+    RuntimeProfile::Counter* _brpc_send_timer = nullptr;
+    RuntimeProfile::Counter* _brpc_wait_timer = nullptr;
+    RuntimeProfile::Counter* _bytes_sent_counter = nullptr;
+    RuntimeProfile::Counter* _uncompressed_bytes_counter = nullptr;
+    RuntimeProfile::Counter* _local_sent_rows = nullptr;
+    RuntimeProfile::Counter* _local_send_timer = nullptr;
+    RuntimeProfile::Counter* _split_block_hash_compute_timer = nullptr;
+    RuntimeProfile::Counter* _split_block_distribute_by_channel_timer = nullptr;
+    RuntimeProfile::Counter* _blocks_sent_counter = nullptr;
     // Throughput per total time spent in sender
-    RuntimeProfile::Counter* _overall_throughput;
+    RuntimeProfile::Counter* _overall_throughput = nullptr;
     // Used to counter send bytes under local data exchange
-    RuntimeProfile::Counter* _local_bytes_send_counter;
-    RuntimeProfile::Counter* _merge_block_timer;
-    RuntimeProfile::Counter* _memory_usage_counter;
-    RuntimeProfile::Counter* _peak_memory_usage_counter;
+    RuntimeProfile::Counter* _local_bytes_send_counter = nullptr;
+    RuntimeProfile::Counter* _merge_block_timer = nullptr;
+    RuntimeProfile::Counter* _memory_usage_counter = nullptr;
+    RuntimeProfile::Counter* _peak_memory_usage_counter = nullptr;
 
-    RuntimeProfile::Counter* _wait_queue_timer;
-    RuntimeProfile::Counter* _wait_broadcast_buffer_timer;
+    RuntimeProfile::Counter* _wait_queue_timer = nullptr;
+    RuntimeProfile::Counter* _wait_broadcast_buffer_timer = nullptr;
     std::vector<RuntimeProfile::Counter*> _wait_channel_timer;
 
     // Sender instance id, unique within a fragment.

be/src/util/runtime_profile.cpp
@@ -49,8 +49,10 @@ RuntimeProfile::RuntimeProfile(const std::string& name, bool is_averaged_profile
           _metadata(-1),
           _timestamp(-1),
           _is_averaged_profile(is_averaged_profile),
-          _counter_total_time(TUnit::TIME_NS, 0, 1),
+          _counter_total_time(TUnit::TIME_NS, 0, 3),
           _local_time_percent(0) {
+    // TotalTime counter has level3 to disable it from plan profile, because
+    // it contains its child running time, we use exec time instead.
     _counter_map["TotalTime"] = &_counter_total_time;
 }
 

commit 8d8a93a2dd5e9726b6b80e35e6d9d849f2c2d6ba

[bugfix](profile) support multi execution profile for brokerload (#32280)
The bug is introduced by #27184
Profile Format is :
Summary
MergedProfile
ExecutionProfile1
ExecutionProfile2
...

There maybe multiple execution profiles for broker load.

fe/fe-core/src/main/java/org/apache/doris/common/profile/AggregatedProfile.java
@@ -1,41 +1,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-package org.apache.doris.common.profile;
-
-import org.apache.doris.common.util.RuntimeProfile;
-
-import java.util.Map;
-
-/**
-* AggregatedProfile is part of a query profile.
-* It contains the aggregated information of a query.
-*/
-public class AggregatedProfile {
-
-    public static final String PROFILE_NAME = "MergedProfile";
-    private ExecutionProfile executionProfile;
-
-    public AggregatedProfile(RuntimeProfile rootProfile, ExecutionProfile executionProfile) {
-        this.executionProfile = executionProfile;
-    }
-
-    public RuntimeProfile getAggregatedFragmentsProfile(Map<Integer, String> planNodeMap) {
-        return executionProfile.getAggregatedFragmentsProfile(planNodeMap);
-    }
-
-}

fe/fe-core/src/main/java/org/apache/doris/common/profile/Profile.java
@@ -21,11 +21,13 @@ import org.apache.doris.common.util.ProfileManager;
 import org.apache.doris.common.util.RuntimeProfile;
 import org.apache.doris.planner.Planner;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,20 +35,23 @@ import java.util.Map;
  * following structure: root profile: // summary of this profile, such as start
  * time, end time, query id, etc. [SummaryProfile] // each execution profile is
  * a complete execution of a query, a job may contain multiple queries.
- * [List<ExecutionProfile>]
+ * [List<ExecutionProfile>].
+ * There maybe multi execution profiles for one job, for example broker load job.
+ * It will create one execution profile for every single load task.
  *
  * SummaryProfile: Summary: Execution Summary:
  *
  *
- * ExecutionProfile: Fragment 0: Fragment 1: ...
+ * ExecutionProfile1: Fragment 0: Fragment 1: ...
+ * ExecutionProfile2: Fragment 0: Fragment 1: ...
+ *
  */
 public class Profile {
     private static final Logger LOG = LogManager.getLogger(Profile.class);
     private static final int MergedProfileLevel = 1;
     private RuntimeProfile rootProfile;
     private SummaryProfile summaryProfile;
-    private AggregatedProfile aggregatedProfile;
-    private ExecutionProfile executionProfile;
+    private List<ExecutionProfile> executionProfiles = Lists.newArrayList();
     private boolean isFinished;
     private Map<Integer, String> planNodeMap;
 
@@ -59,14 +64,13 @@ public class Profile {
         this.isFinished = !isEnable;
     }
 
-    public void setExecutionProfile(ExecutionProfile executionProfile) {
+    public void addExecutionProfile(ExecutionProfile executionProfile) {
         if (executionProfile == null) {
             LOG.warn("try to set a null excecution profile, it is abnormal", new Exception());
             return;
         }
-        this.executionProfile = executionProfile;
-        this.executionProfile.addToProfileAsChild(rootProfile);
-        this.aggregatedProfile = new AggregatedProfile(rootProfile, executionProfile);
+        this.executionProfiles.add(executionProfile);
+        executionProfile.addToProfileAsChild(rootProfile);
     }
 
     public synchronized void update(long startTime, Map<String, String> summaryInfo, boolean isFinished,
@@ -75,12 +79,10 @@ public class Profile {
             if (this.isFinished) {
                 return;
             }
-            if (executionProfile == null) {
-                // Sometimes execution profile is not set
-                return;
-            }
             summaryProfile.update(summaryInfo);
-            executionProfile.update(startTime, isFinished);
+            for (ExecutionProfile executionProfile : executionProfiles) {
+                executionProfile.update(startTime, isFinished);
+            }
             rootProfile.computeTimeInProfile();
             // Nerids native insert not set planner, so it is null
             if (planner != null) {
@@ -109,18 +111,22 @@ public class Profile {
         // add summary to builder
         summaryProfile.prettyPrint(builder);
         LOG.info(builder.toString());
-        if (this.profileLevel == MergedProfileLevel) {
+        // Only generate merged profile for select, insert into select.
+        // Not support broker load now.
+        if (this.profileLevel == MergedProfileLevel && this.executionProfiles.size() == 1) {
             try {
                 builder.append("\n MergedProfile \n");
-                aggregatedProfile.getAggregatedFragmentsProfile(planNodeMap).prettyPrint(builder, "     ");
+                this.executionProfiles.get(0).getAggregatedFragmentsProfile(planNodeMap).prettyPrint(builder, "     ");
             } catch (Throwable aggProfileException) {
                 LOG.warn("build merged simple profile failed", aggProfileException);
                 builder.append("build merged simple profile failed");
             }
         }
         try {
-            builder.append("\n");
-            executionProfile.getExecutionProfile().prettyPrint(builder, "");
+            for (ExecutionProfile executionProfile : executionProfiles) {
+                builder.append("\n");
+                executionProfile.getExecutionProfile().prettyPrint(builder, "");
+            }
         } catch (Throwable aggProfileException) {
             LOG.warn("build profile failed", aggProfileException);
             builder.append("build  profile failed");

fe/fe-core/src/main/java/org/apache/doris/load/loadv2/LoadLoadingTask.java
@@ -145,7 +145,7 @@ public class LoadLoadingTask extends LoadTask {
                 loadId, planner.getDescTable(),
                 planner.getFragments(), planner.getScanNodes(), planner.getTimezone(), loadZeroTolerance);
         if (this.jobProfile != null) {
-            this.jobProfile.setExecutionProfile(curCoordinator.getExecutionProfile());
+            this.jobProfile.addExecutionProfile(curCoordinator.getExecutionProfile());
         }
         curCoordinator.setQueryType(TQueryType.LOAD);
         curCoordinator.setExecMemoryLimit(execMemLimit);

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
@@ -123,7 +123,7 @@ public abstract class AbstractInsertExecutor {
         String queryId = DebugUtil.printId(ctx.queryId());
         coordinator.setLoadZeroTolerance(ctx.getSessionVariable().getEnableInsertStrict());
         coordinator.setQueryType(TQueryType.LOAD);
-        executor.getProfile().setExecutionProfile(coordinator.getExecutionProfile());
+        executor.getProfile().addExecutionProfile(coordinator.getExecutionProfile());
         QueryInfo queryInfo = new QueryInfo(ConnectContext.get(), executor.getOriginStmtInString(), coordinator);
         QeProcessorImpl.INSTANCE.registerQuery(ctx.queryId(), queryInfo);
         coordinator.exec();

fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -1596,7 +1596,7 @@ public class StmtExecutor {
                 planner, context.getStatsErrorEstimator());
             QeProcessorImpl.INSTANCE.registerQuery(context.queryId(),
                     new QeProcessorImpl.QueryInfo(context, originStmt.originStmt, coord));
-            profile.setExecutionProfile(coord.getExecutionProfile());
+            profile.addExecutionProfile(coord.getExecutionProfile());
             coordBase = coord;
         }
 
@@ -2030,7 +2030,7 @@ public class StmtExecutor {
                         planner, context.getStatsErrorEstimator());
                 coord.setLoadZeroTolerance(context.getSessionVariable().getEnableInsertStrict());
                 coord.setQueryType(TQueryType.LOAD);
-                profile.setExecutionProfile(coord.getExecutionProfile());
+                profile.addExecutionProfile(coord.getExecutionProfile());
                 QueryInfo queryInfo = new QueryInfo(ConnectContext.get(), this.getOriginStmtInString(), coord);
                 QeProcessorImpl.INSTANCE.registerQuery(context.queryId(), queryInfo);
 
@@ -2869,7 +2869,7 @@ public class StmtExecutor {
             RowBatch batch;
             coord =  EnvFactory.getInstance().createCoordinator(context, analyzer,
                     planner, context.getStatsErrorEstimator());
-            profile.setExecutionProfile(coord.getExecutionProfile());
+            profile.addExecutionProfile(coord.getExecutionProfile());
             try {
                 QeProcessorImpl.INSTANCE.registerQuery(context.queryId(),
                         new QeProcessorImpl.QueryInfo(context, originStmt.originStmt, coord));