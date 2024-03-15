f32fdd4b66ea6a4dfcc8c63d8a68be04df2aff09
ryanzryu <ryanzryu@tencent.com>
2024-03-15 14:44:02 +0800

    [fix](s3 client)add default ca cert list for s3 client to avoid problem:'curlCode:77'

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1199,6 +1199,12 @@ DEFINE_mInt32(max_s3_client_retry, "10");
 
 DEFINE_String(trino_connector_plugin_dir, "${DORIS_HOME}/connectors");
 
+// ca_cert_file is in this path by default, Normally no modification is required
+// ca cert default path is different from different OS
+DEFINE_mString(ca_cert_file_paths,
+               "/etc/pki/tls/certs/ca-bundle.crt;/etc/ssl/certs/ca-certificates.crt;"
+               "/etc/ssl/ca-bundle.pem");
+
 // clang-format off
 #ifdef BE_TEST
 // test s3
