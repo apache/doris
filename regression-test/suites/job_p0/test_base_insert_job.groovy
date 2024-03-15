3d0ab498338027909fe3ad706f506e1bce524559
zhangstar333 <2561612514@qq.com>
2024-03-15 13:15:15 +0800

    update 2

--- regression-test/suites/job_p0/test_base_insert_job.groovy
+++ regression-test/suites/job_p0/test_base_insert_job.groovy
@@ -25,6 +25,7 @@ suite("test_base_insert_job") {
     def jobName = "insert_recovery_test_base_insert_job"
     def jobMixedName = "Insert_recovery_Test_base_insert_job"
     sql """drop table if exists `${tableName}` force"""
+    sql """set enable_fold_constant_by_be = false;"""
     sql """
         DROP JOB IF EXISTS where jobname =  '${jobName}'
     """
