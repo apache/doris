546f6896bf9f49d73b553f44b32a594630f3549b
Mingyu Chen <morningman@163.com>
2024-03-18 10:31:51 +0800

    [feature](proxy-protocol) Support proxy protocol v1 (#32338)
    Enable proxy protocol to support IP transparency.
    See: `IP Transparency` in https://github.com/apache/doris/blob/f57387b502035fa138d946936e63d1c55e7d1ff4/docs/en/docs/admin-manual/cluster-management/load-balancing.md
    for details

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -2532,6 +2532,12 @@ public class Config extends ConfigBase {
     })
     public static String inverted_index_storage_format = "V1";
 
+    @ConfField(description = {
+            "是否开启 Proxy Protocol 支持",
+            "Whether to enable proxy protocol"
+    })
+    public static boolean enable_proxy_protocol = false;
+
     //==========================================================================
     //                    begin of cloud config
     //==========================================================================
