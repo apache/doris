6cb3cfb416a26663187a209553325e09b4c801e3
zhangdong <493738387@qq.com>
2024-03-18 17:58:44 +0800

    add mtmv db2 case

--- regression-test/suites/mtmv_p0/test_mysql_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_mysql_mtmv.groovy
@@ -15,59 +15,33 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_db2_mtmv", "p0,external,hive,external_docker,external_docker_hive") {
+suite("test_mysql_mtmv", "p0,external,hive,external_docker,external_docker_hive") {
     String enabled = context.config.otherConfigs.get("enableJdbcTest")
     logger.info("enabled: " + enabled)
     String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
     logger.info("externalEnvIp: " + externalEnvIp)
+    String mysql_port = context.config.otherConfigs.get("mysql_57_port");
+    logger.info("mysql_port: " + mysql_port)
     String s3_endpoint = getS3Endpoint()
     logger.info("s3_endpoint: " + s3_endpoint)
     String bucket = getS3BucketName()
     logger.info("bucket: " + bucket)
-    String driver_url = "https://${bucket}.${s3_endpoint}/regression/jdbc_driver/jcc-11.5.8.0.jar"
+    String driver_url = "https://${bucket}.${s3_endpoint}/regression/jdbc_driver/mysql-connector-java-8.0.25.jar"
     logger.info("driver_url: " + driver_url)
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
-        String catalog_name = "db2_mtmv_catalog";
-        String mvName = "db2_mtmv";
-        String dbName = "regression_test_mtmv_p0";
-        String db2_database = "DORIS_MTMV";
-        String db2_port = context.config.otherConfigs.get("db2_11_port");
-        String db2_table = "USER";
-        logger.info("DROP TABLE")
-        db2_docker "DROP TABLE IF EXISTS ${db2_database}.${db2_table};"
-        logger.info("DROP SCHEMA")
-        try {
-            db2_docker "DROP SCHEMA ${db2_database} restrict;"
-        } catch (Exception e) {
-          logger.info("drop schema failed.")
-        }
-
-        logger.info("CREATE SCHEMA")
-        db2_docker "CREATE SCHEMA ${db2_database};"
-        logger.info("CREATE TABLE")
-        db2_docker """CREATE TABLE ${db2_database}.${db2_table} (
-            k1 INT GENERATED ALWAYS AS IDENTITY,
-            k2 INT
-        );"""
-        logger.info("INSERT INTO")
-        db2_docker """INSERT INTO ${db2_database}.${db2_table} (
-            k2
-        ) VALUES (
-            1
-        );"""
-
+        String catalog_name = "mysql_mtmv_catalog";
         sql """drop catalog if exists ${catalog_name} """
 
         sql """create catalog if not exists ${catalog_name} properties(
             "type"="jdbc",
-            "user"="db2inst1",
+            "user"="root",
             "password"="123456",
-            "jdbc_url" = "jdbc:db2://${externalEnvIp}:${db2_port}/doris",
+            "jdbc_url" = "jdbc:mysql://${externalEnvIp}:${mysql_port}/doris_test?useSSL=false&zeroDateTimeBehavior=convertToNull",
             "driver_url" = "${driver_url}",
-            "driver_class" = "com.ibm.db2.jcc.DB2Driver"
+            "driver_class" = "com.mysql.cj.jdbc.Driver"
         );"""
 
-        qt_catalog """select * from ${catalog_name}.${db2_database}.${db2_table} order by k1"""
+        qt_catalog """select * from ${catalog_name}.doris_test.ex_tb2 order by id"""
 
         sql """drop materialized view if exists ${mvName};"""
 
@@ -77,7 +51,7 @@ suite("test_db2_mtmv", "p0,external,hive,external_docker,external_docker_hive") 
                 DISTRIBUTED BY RANDOM BUCKETS 2
                 PROPERTIES ('replication_num' = '1')
                 AS
-                SELECT * FROM ${catalog_name}.${db2_database}.${db2_table};
+                SELECT * FROM ${catalog_name}.doris_test.ex_tb2;
             """
 
         sql """
@@ -85,26 +59,10 @@ suite("test_db2_mtmv", "p0,external,hive,external_docker,external_docker_hive") 
             """
         def jobName = getJobName(dbName, mvName);
         waitingMTMVTaskFinished(jobName)
-        order_qt_mtmv_1 "SELECT * FROM ${mvName} order by k1"
-
-        logger.info("INSERT INTO")
-                db2_docker """INSERT INTO ${db2_database}.${db2_table} (
-                    k2
-                ) VALUES (
-                    2
-                );"""
-        sql """
-                REFRESH MATERIALIZED VIEW ${mvName} complete
-            """
-        waitingMTMVTaskFinished(jobName)
-        order_qt_mtmv_2 "SELECT * FROM ${mvName} order by k1"
+        order_qt_mtmv "SELECT * FROM ${mvName} order by k1"
 
         sql """drop materialized view if exists ${mvName};"""
         sql """ drop catalog if exists ${catalog_name} """
-        db2_docker "DROP TABLE IF EXISTS ${db2_database}.${db2_table};"
-        db2_docker "DROP SCHEMA ${db2_database} restrict;"
-
-
     }
 }
 
