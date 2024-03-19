ee75305b0675803ce046744d9c93e9155d900856
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2024-03-19 10:19:23 +0800

    Support auto analyze mv (#32433)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -762,6 +762,17 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         return columns;
     }
 
+    public List<Column> getMvColumns(boolean full) {
+        List<Column> columns = Lists.newArrayList();
+        for (Long indexId : indexIdToMeta.keySet()) {
+            if (indexId == baseIndexId) {
+                continue;
+            }
+            columns.addAll(getSchemaByIndexId(indexId, full));
+        }
+        return columns;
+    }
+
     public List<Column> getBaseSchemaKeyColumns() {
         return getKeyColumnsByIndexId(baseIndexId);
     }
