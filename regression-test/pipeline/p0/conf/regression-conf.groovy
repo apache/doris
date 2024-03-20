cdc87db7f9eae25c862e092856809f27ffd146a4
shuke <37901441+shuke987@users.noreply.github.com>
2024-03-20 16:57:26 +0800

    [regression-test](case) forbid test_stream_stub_fault_injection (#32540)

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -61,7 +61,7 @@ excludeGroups = ""
 // this suites will not be executed
 // load_stream_fault_injection may cause bad disk
 
-excludeSuites = "test_index_failure_injection,test_dump_image,test_profile,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_information_schema_external"
+excludeSuites = "test_stream_stub_fault_injection,test_index_failure_injection,test_dump_image,test_profile,test_spark_load,test_refresh_mtmv,test_bitmap_filter,test_information_schema_external"
 
 // this directories will not be executed
 excludeDirectories = "workload_manager_p1,nereids_rules_p0/subquery"
