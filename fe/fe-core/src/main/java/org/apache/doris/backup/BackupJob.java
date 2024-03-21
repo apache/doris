09b0e59e0a116849b389ac23ff3f768f0da3061f
nextdreamblue <zxw520blue1@163.com>
2024-03-21 21:45:28 +0800

    [fix](backup) clear snapshotInfos and backupMeta when cancel
    Signed-off-by: nextdreamblue <zxw520blue1@163.com>

--- fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
@@ -858,6 +858,9 @@ public class BackupJob extends AbstractJob {
             }
         }
 
+        // meta info and job info not need save in log when cancel, we need to clean them here
+        backupMeta = null;
+        jobInfo = null;
         releaseSnapshots();
         snapshotInfos.clear();
 
