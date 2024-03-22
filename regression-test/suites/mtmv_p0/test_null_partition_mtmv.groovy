5f03eae6869bfadb2d0ec07596ae36671a447b95
zhangdong <493738387@qq.com>
2024-03-22 17:31:54 +0800

    1

--- regression-test/suites/mtmv_p0/test_null_partition_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_null_partition_mtmv.groovy
@@ -58,7 +58,7 @@ suite("test_null_partition_mtmv") {
     assertTrue(showPartitionsResult.toString().contains("p_1"))
     assertTrue(showPartitionsResult.toString().contains("p_NULL"))
 
-    order_qt_list_null "SELECT * FROM ${mvName} partitions(p_null) order by user_id,num"
+    order_qt_list_null "SELECT * FROM ${mvName} partitions(p_NULL) order by user_id,num"
     order_qt_list_1 "SELECT * FROM ${mvName} partitions(p_1) order by user_id,num"
 
     sql """drop table if exists `${tableName}`"""
