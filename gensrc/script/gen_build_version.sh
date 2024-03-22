dc065ead16c570bc054f77c4a430e34094d0cb53
Kang <kxiao.tiger@gmail.com>
2024-03-22 21:19:10 +0800

    [chore](version) change branch-2.0 version to 2.0.7 (#32640)

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -31,7 +31,7 @@ build_version_prefix="doris"
 build_version_major=2
 build_version_minor=0
 build_version_patch=7
-build_version_rc_version="dev"
+build_version_rc_version=""
 
 if [[ -z "${build_version_rc_version}" ]]; then
     build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}"
