deb116d9e43e7a97fb84463918ee0f9284443056
englefly <minghong.zhou@163.com>
2024-03-22 09:33:28 +0800

    fix-nlj-rf

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterGenerator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterGenerator.java
@@ -315,6 +315,11 @@ public class RuntimeFilterGenerator extends PlanPostProcessor {
         if (join.getJoinType() != JoinType.LEFT_SEMI_JOIN && join.getJoinType() != JoinType.CROSS_JOIN) {
             return;
         }
+        if (ctx.getSessionVariable().isIgnoreStorageDataDistribution()) {
+            // BITMAP filter is not supported to merge. So we disable this kind of runtime filter
+            // if IgnoreStorageDataDistribution is enabled.
+            return;
+        }
         List<Slot> leftSlots = join.left().getOutput();
         List<Slot> rightSlots = join.right().getOutput();
         List<Expression> bitmapRuntimeFilterConditions = JoinUtils.extractBitmapRuntimeFilterConditions(leftSlots,
@@ -446,11 +451,6 @@ public class RuntimeFilterGenerator extends PlanPostProcessor {
             return join;
         }
         RuntimeFilterContext ctx = context.getRuntimeFilterContext();
-        if (ctx.getSessionVariable().isIgnoreStorageDataDistribution()) {
-            // BITMAP filter is not supported to merge. So we disable this kind of runtime filter
-            // if IgnoreStorageDataDistribution is enabled.
-            return join;
-        }
 
         if ((ctx.getSessionVariable().getRuntimeFilterType() & TRuntimeFilterType.BITMAP.getValue()) != 0) {
             generateBitMapRuntimeFilterForNLJ(join, ctx);
