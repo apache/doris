a9563c6da8130f86fbad086da157d97d2567a9cb
zclllyybb <zhaochangle@selectdb.com>
2024-03-24 14:19:28 +0800

    [Feature](partition) Support OLAP table null partition (#31827)
        for auto partition, support nullable partition column.
        for auto list partition, support create real null partition for null values.
        for auto range partition, null value will raise a error now. but maybe we can improve this in the future

--- fe/fe-core/src/main/java/org/apache/doris/catalog/PartitionKey.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/PartitionKey.java
@@ -95,7 +95,8 @@ public class PartitionKey implements Comparable<PartitionKey>, Writable {
         for (i = 0; i < keys.size(); ++i) {
             Type keyType = columns.get(i).getType();
             // If column type is datatime and key type is date, we should convert date to datetime.
-            if (keyType.isDatetime() || keyType.isDatetimeV2()) {
+            // if it's max value, no need to parse.
+            if (!keys.get(i).isMax() && (keyType.isDatetime() || keyType.isDatetimeV2())) {
                 Literal dateTimeLiteral = getDateTimeLiteral(keys.get(i).getStringValue(), keyType);
                 partitionKey.keys.add(dateTimeLiteral.toLegacyLiteral());
             } else {
