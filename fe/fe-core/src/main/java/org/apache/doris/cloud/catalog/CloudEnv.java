e84f8795179ba7ca6b584719a6c05cbdeba826a2
deardeng <565620795@qq.com>
2024-03-25 14:24:27 +0800

    [feature](merge-cloud) Support auto suspend and auto start cluster in cloud

--- fe/fe-core/src/main/java/org/apache/doris/cloud/catalog/CloudEnv.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/catalog/CloudEnv.java
@@ -382,7 +382,6 @@ public class CloudEnv extends Env {
 
     public void changeCloudCluster(String clusterName, ConnectContext ctx) throws DdlException {
         checkCloudClusterPriv(clusterName);
-        // TODO(merge-cloud): pick cloud auto start
         CloudSystemInfoService.waitForAutoStart(clusterName);
         try {
             ((CloudSystemInfoService) Env.getCurrentSystemInfo()).addCloudCluster(clusterName, "");
