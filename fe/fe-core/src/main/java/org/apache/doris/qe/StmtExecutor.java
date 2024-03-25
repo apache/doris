2627ca8b5c9bfe7af007d2255a092179eb8689a6
zzzzzzzs <1443539042@qq.com>
2024-03-25 17:39:15 +0800

    update

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -558,7 +558,7 @@ public class StmtExecutor {
                     boolean isInsertCommand = parsedStmt != null
                             && parsedStmt instanceof LogicalPlanAdapter
                             && ((LogicalPlanAdapter) parsedStmt).getLogicalPlan() instanceof InsertIntoTableCommand;
-                     /*boolean isGroupCommit = (Config.wait_internal_group_commit_finish
+                    /*boolean isGroupCommit = (Config.wait_internal_group_commit_finish
                             || context.sessionVariable.isEnableInsertGroupCommit()) && isInsertCommand;*/
                     boolean forceFallback = isInsertCommand && !context.isTxnModel();
                     if (e instanceof NereidsException && !context.getSessionVariable().enableFallbackToOriginalPlanner
