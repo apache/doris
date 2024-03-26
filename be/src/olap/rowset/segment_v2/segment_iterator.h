b78551455a349ccc3c99d8ae0d9b926fd5e6c8a6
BiteTheDDDDt <pxl290@qq.com>
2024-03-26 14:46:15 +0800

    format

--- be/src/olap/rowset/segment_v2/segment_iterator.h
+++ be/src/olap/rowset/segment_v2/segment_iterator.h
@@ -218,7 +218,7 @@ private:
                                                 bool set_block_rowid);
     void _replace_version_col(size_t num_rows);
     Status _init_current_block(vectorized::Block* block,
-                             std::vector<vectorized::MutableColumnPtr>& non_pred_vector);
+                               std::vector<vectorized::MutableColumnPtr>& non_pred_vector);
     uint16_t _evaluate_vectorization_predicate(uint16_t* sel_rowid_idx, uint16_t selected_size);
     uint16_t _evaluate_short_circuit_predicate(uint16_t* sel_rowid_idx, uint16_t selected_size);
     void _output_non_pred_columns(vectorized::Block* block);
