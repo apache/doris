151abde6cf81b08a9fab4db3c5d1ac788889e648
Xinyi Zou <zouxinyi02@gmail.com>
2024-03-26 20:55:49 +0800

    [fix](memory) Fix task repeat attach task DCHECK failed #32784

--- be/src/olap/task/engine_clone_task.cpp
+++ be/src/olap/task/engine_clone_task.cpp
@@ -157,7 +157,6 @@ EngineCloneTask::EngineCloneTask(StorageEngine& engine, const TCloneReq& clone_r
 
 Status EngineCloneTask::execute() {
     // register the tablet to avoid it is deleted by gc thread during clone process
-    SCOPED_ATTACH_TASK(_mem_tracker);
     if (!_engine.tablet_manager()->register_clone_tablet(_clone_req.tablet_id)) {
         return Status::InternalError("tablet {} is under clone", _clone_req.tablet_id);
     }
