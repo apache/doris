17fa9f44f2420ef51b15b631a3e929c7ec5e93c4
morningman <morningman@163.com>
2024-03-29 00:28:39 +0800

    [opt](rowcount) refresh external table's rowcount async

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalRowCountCache.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalRowCountCache.java
@@ -24,6 +24,7 @@ import org.apache.doris.statistics.util.StatisticsUtil;
 
 import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -37,14 +38,19 @@ public class ExternalRowCountCache {
     private static final Logger LOG = LogManager.getLogger(ExternalRowCountCache.class);
     private final AsyncLoadingCache<RowCountKey, Optional<Long>> rowCountCache;
 
-    public ExternalRowCountCache(ExecutorService executor) {
+    public ExternalRowCountCache(ExecutorService executor, long refreshAfterWriteSeconds,
+            BasicAsyncCacheLoader<RowCountKey, Optional<Long>> loader) {
+        // 1. set expireAfterWrite to 1 day, avoid too many entries
+        // 2. set refreshAfterWrite to 10min(default), so that the cache will be refreshed after 10min
         rowCountCache = Caffeine.newBuilder()
                 .maximumSize(Config.max_external_table_row_count_cache_num)
-                .expireAfterWrite(Duration.ofMinutes(Config.external_cache_expire_time_minutes_after_access))
+                .expireAfterAccess(Duration.ofDays(1))
+                .refreshAfterWrite(Duration.ofSeconds(refreshAfterWriteSeconds))
                 .executor(executor)
-                .buildAsync(new RowCountCacheLoader());
+                .buildAsync(loader == null ? new RowCountCacheLoader() : loader);
     }
 
+    @Getter
     public static class RowCountKey {
         private final long catalogId;
         private final long dbId;
@@ -74,7 +80,6 @@ public class ExternalRowCountCache {
     }
 
     public static class RowCountCacheLoader extends BasicAsyncCacheLoader<RowCountKey, Optional<Long>> {
-
         @Override
         protected Optional<Long> doLoad(RowCountKey rowCountKey) {
             try {
