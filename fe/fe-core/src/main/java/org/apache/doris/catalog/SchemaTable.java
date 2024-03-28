267b8363c2a7a6f4790ef7c52e36e0b90d41f4da
wangbo <wangbo@apache.org>
2024-03-28 17:31:47 +0800

    [improvement](executor)Add tag property for workload group #32874

--- fe/fe-core/src/main/java/org/apache/doris/catalog/SchemaTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/SchemaTable.java
@@ -486,6 +486,7 @@ public class SchemaTable extends Table {
                             .column("MIN_REMOTE_SCAN_THREAD_NUM", ScalarType.createType(PrimitiveType.BIGINT))
                             .column("SPILL_THRESHOLD_LOW_WATERMARK", ScalarType.createVarchar(256))
                             .column("SPILL_THRESHOLD_HIGH_WATERMARK", ScalarType.createVarchar(256))
+                            .column("TAG", ScalarType.createVarchar(256))
                             .build()))
             .build();
 
