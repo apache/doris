f2b70a5247bbccb71d9a68f6ac8e657ad5b9c27b
meiyi <myimeiyi@gmail.com>
2024-03-28 20:40:57 +0800

    fix com

--- fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
+++ fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
@@ -45,6 +45,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
