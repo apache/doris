e73362d145e549b92f815aa7531a385a31a0581d
Gabriel <gabrielleebuaa@gmail.com>
2024-03-29 15:18:49 +0800

    [pipelineX](local exchange) Fix potential timeout problem

--- be/src/pipeline/pipeline_x/local_exchange/local_exchange_sink_operator.cpp
+++ be/src/pipeline/pipeline_x/local_exchange/local_exchange_sink_operator.cpp
@@ -40,13 +40,25 @@ Status LocalExchangeSinkLocalState::init(RuntimeState* state, LocalSinkStateInfo
     return Status::OK();
 }
 
+Status LocalExchangeSinkLocalState::close(RuntimeState* state, Status exec_status) {
+    if (_closed) {
+        return Status::OK();
+    }
+    RETURN_IF_ERROR(Base::close(state, exec_status));
+    if (!_release_count) {
+        _shared_state->sub_running_sink_operators();
+    }
+    return Status::OK();
+}
+
 std::string LocalExchangeSinkLocalState::debug_string(int indentation_level) const {
     fmt::memory_buffer debug_string_buffer;
     fmt::format_to(debug_string_buffer,
-                   "{}, _channel_id: {}, _num_partitions: {}, _num_senders: {}, _num_sources: {}",
+                   "{}, _channel_id: {}, _num_partitions: {}, _num_senders: {}, _num_sources: {}, "
+                   "_running_sink_operators: {}, _release_count: {}",
                    Base::debug_string(indentation_level), _channel_id, _exchanger->_num_partitions,
                    _exchanger->_num_senders, _exchanger->_num_sources,
-                   _exchanger->_running_sink_operators);
+                   _exchanger->_running_sink_operators, _release_count);
     return fmt::to_string(debug_string_buffer);
 }
 
@@ -59,6 +71,7 @@ Status LocalExchangeSinkOperatorX::sink(RuntimeState* state, vectorized::Block* 
 
     if (eos) {
         local_state._shared_state->sub_running_sink_operators();
+        local_state._release_count = true;
     }
 
     return Status::OK();
