3e25370093ed65e513550b730a5c549d8131dbdc
Mingyu Chen <morningman@163.com>
2024-04-01 11:01:29 +0800

    [fix](avro) avoid BE crash if avro scanner's dependency jars is mssing (#33031)
    1. Check the return value of avro reader's init_fetch_table_schema_reader()
    2. Also fix a bug but the parse exception of Nereids may suppress the real exception from old planner
        It will result unable to see the real error msg.

--- regression-test/suites/demo_p0/test_action.groovy
+++ regression-test/suites/demo_p0/test_action.groovy
@@ -19,7 +19,7 @@ suite("test_action") {
     test {
         sql "abcdefg"
         // check exception message contains
-        exception "ParseException"
+        exception "Syntax error in line 1"
     }
 
     test {
