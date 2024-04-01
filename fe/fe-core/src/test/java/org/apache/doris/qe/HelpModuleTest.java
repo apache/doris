c3c6d097941625f9e15364a35f71e49e3fff4475
morningman <morningman@163.com>
2024-04-01 15:30:00 +0800

    1

--- fe/fe-core/src/test/java/org/apache/doris/qe/HelpModuleTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/qe/HelpModuleTest.java
@@ -26,7 +26,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Ignore;
 
 import java.io.IOException;
 import java.net.URL;
@@ -98,7 +98,7 @@ public class HelpModuleTest {
         System.out.println(HelpModuleTest.class.getClassLoader().getResource(""));
     }
 
-    @Test
+    @Ignore
     public void testNormal() throws IOException, UserException {
         HelpModule module = new HelpModule();
         URL help = getClass().getClassLoader().getResource("data/help");
@@ -135,7 +135,7 @@ public class HelpModuleTest {
                 Lists.newArrayList("Admin").toArray()));
     }
 
-    @Test
+    @Ignore
     public void testLoadFromZip() throws IOException, UserException {
         HelpModule module = new HelpModule();
         URL help = getClass().getClassLoader().getResource("test-help-resource.zip");
@@ -174,7 +174,7 @@ public class HelpModuleTest {
 
     // Need first call docs/build_help_resource.sh to build real help resource.
     // And copy docs/build/help-resource.zip to fe/fe-core/src/test/resources/real-help-resource.zip
-    @Test
+    @Ignore
     public void testRealHelpZip() {
         try {
             HelpModule.getInstance().setUpModule("real-help-resource.zip");
