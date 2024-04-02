1f3524bed03bacf3dd384a0324f8e8ca11817d0f
zhengyu <freeman.zhang1992@gmail.com>
2024-04-02 12:19:14 +0800

    [fix](analyze) correct duplicated counting of updated rows for mv in cloud mode (#33136)
    Signed-off-by: freemandealer <freeman.zhang1992@gmail.com>

--- regression-test/suites/statistics/test_analyze_mv.groovy
+++ regression-test/suites/statistics/test_analyze_mv.groovy
@@ -111,6 +111,7 @@ suite("test_analyze_mv") {
     sql """drop database if exists test_analyze_mv"""
     sql """create database test_analyze_mv"""
     sql """use test_analyze_mv"""
+    sql """set global force_sample_analyze=false"""
 
     sql """CREATE TABLE mvTestDup (
             key1 bigint NOT NULL,
@@ -118,12 +119,12 @@ suite("test_analyze_mv") {
             value1 int NOT NULL,
             value2 int NOT NULL,
             value3 int NOT NULL
-        )ENGINE=OLAP 
+        )ENGINE=OLAP
         DUPLICATE KEY(`key1`, `key2`)
         COMMENT "OLAP"
         DISTRIBUTED BY HASH(`key1`) BUCKETS 2
         PROPERTIES (
-            "replication_num" = "1" 
+            "replication_num" = "1"
         )
     """
     sql """create materialized view mv1 as select key1 from mvTestDup;"""
@@ -226,13 +227,13 @@ suite("test_analyze_mv") {
             key2 bigint NOT NULL,
             value1 int SUM NOT NULL,
             value2 int MAX NOT NULL,
-            value3 int MIN NOT NULL 
+            value3 int MIN NOT NULL
         )ENGINE=OLAP
         AGGREGATE KEY(`key1`, `key2`)
         COMMENT "OLAP"
         DISTRIBUTED BY HASH(`key1`) BUCKETS 2
         PROPERTIES (
-            "replication_num" = "1" 
+            "replication_num" = "1"
         );
     """
 
@@ -362,7 +363,7 @@ suite("test_analyze_mv") {
 
 
     sql """
-        CREATE TABLE mvTestUni ( 
+        CREATE TABLE mvTestUni (
             key1 bigint NOT NULL,
             key2 bigint NOT NULL,
             value1 int NOT NULL,
