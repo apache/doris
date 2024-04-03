cc4f44677a4d45b296694d4a03982155d9883f80
zhangdong <493738387@qq.com>
2024-04-03 11:49:24 +0800

    [enhance](mtmv)support create mtmv with other mtmv (#32984)

--- regression-test/suites/mtmv_p0/test_build_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_build_mtmv.groovy
@@ -122,21 +122,6 @@ suite("test_build_mtmv") {
         log.info(e.getMessage())
     }
 
-    // not allow create mv use other mv
-    try {
-        sql """
-            CREATE MATERIALIZED VIEW ${mvNameRenamed}
-            BUILD DEFERRED REFRESH COMPLETE ON MANUAL
-            DISTRIBUTED BY RANDOM BUCKETS 2
-            PROPERTIES ('replication_num' = '1')
-            AS
-            SELECT * from ${mvName};
-        """
-        Assert.fail();
-    } catch (Exception e) {
-        log.info(e.getMessage())
-    }
-
     // not allow create mv use view
     try {
         sql """
