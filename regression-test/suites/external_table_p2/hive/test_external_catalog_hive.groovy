1f013b1cbd249c609a54b52eee829265292a6d67
gaoxin <AshinGau@outlook.com>
2024-04-03 15:57:37 +0800

    [fix](parquet) fix time zone error in parquet reader

--- regression-test/suites/external_table_p2/hive/test_external_catalog_hive.groovy
+++ regression-test/suites/external_table_p2/hive/test_external_catalog_hive.groovy
@@ -48,7 +48,7 @@ suite("test_external_catalog_hive", "p2,external,hive,external_remote,external_r
         // test small table(text format)
         def q01 = {
             qt_q01 """ select name, count(1) as c from student group by name order by c desc;"""
-            qt_q02 """ select lo_orderkey, count(1) as c from lineorder group by lo_orderkey order by c desc;"""
+            qt_q02 """ select lo_orderkey, count(1) as c from lineorder group by lo_orderkey order by lo_orderkey asc;"""
             qt_q03 """ select * from test1 order by col_1;"""
             qt_q04 """ select * from string_table order by p_partkey desc;"""
             qt_q05 """ select * from account_fund order by batchno;"""
@@ -108,6 +108,9 @@ suite("test_external_catalog_hive", "p2,external,hive,external_remote,external_r
         qt_par_fields_in_file_parquet4 """ select * from multi_catalog.par_fields_in_file_parquet where month = 8 and year >= 2022 order by id; """
         qt_par_fields_in_file_orc5 """ select * from multi_catalog.par_fields_in_file_orc where month = 8 and year = 2022 order by id; """
         qt_par_fields_in_file_parquet5 """ select * from multi_catalog.par_fields_in_file_parquet where month = 8 and year = 2022 order by id; """
+
+        // timestamp with isAdjustedToUTC=true
+        qt_parquet_adjusted_utc """select * from multi_catalog.timestamp_with_time_zone order by date_col;"""
 
         // test unsupported input format query
         try {
