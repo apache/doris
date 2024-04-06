0151d466197d373bd14be857a7bdc7eb490d3025
morningman <morningman@163.com>
2024-04-07 00:32:13 +0800

    2

--- fe/fe-core/src/test/java/org/apache/doris/external/iceberg/util/IcebergPredicateTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/external/iceberg/util/IcebergPredicateTest.java
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package org.apache.doris.datasource.iceberg;
+package org.apache.doris.external.iceberg.util;
 
 import org.apache.doris.analysis.BinaryPredicate;
 import org.apache.doris.analysis.BoolLiteral;
