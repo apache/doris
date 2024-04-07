daeb9e1fbbf3a2a5adb954613530cea25065de7a
stephen <hello-stephen@qq.com>
2024-04-07 11:39:42 +0800

    [chore](ci) cloud_p* collect log when regression fail

--- regression-test/pipeline/cloud_p0/run.sh
+++ regression-test/pipeline/cloud_p0/run.sh
@@ -43,6 +43,7 @@ echo "#### Run cloud_p0 test on Doris ####"
 DORIS_HOME="${teamcity_build_checkoutDir}/output"
 export DORIS_HOME
 exit_flag=0
+need_collect_log=false
 
 # shellcheck disable=SC2317
 run() {
@@ -66,6 +67,7 @@ run() {
         -actionParallel 2; then
         echo
     else
+        need_collect_log=true
         # regression 测试跑完后输出的汇总信息，Test 1961 suites, failed 1 suites, fatal 0 scripts, skipped 0 scripts
         # 如果 test_suites>0 && failed_suites<=3  && fatal_scripts=0，就把返回状态码改为正常的0，让teamcity根据跑case的情况去判断成功还是失败
         # 这样预期能够快速 mute 不稳定的 case
@@ -91,7 +93,7 @@ timeout "${timeout_minutes}" bash -cx run
 exit_flag="$?"
 
 echo "#### 5. check if need backup doris logs"
-if [[ ${exit_flag} != "0" ]]; then
+if [[ ${exit_flag} != "0" ]] || ${need_collect_log}; then
     check_if_need_gcore "${exit_flag}"
     if core_file_name=$(archive_doris_coredump "${pr_num_from_trigger}_${commit_id_from_trigger}_$(date +%Y%m%d%H%M%S)_doris_coredump.tar.gz"); then
         reporting_build_problem "coredump"
