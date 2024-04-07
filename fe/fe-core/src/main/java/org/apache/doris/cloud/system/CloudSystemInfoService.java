eec54b4f02d2b05a5bf259d8fc493fe10a964ff0
ByteYue <yj976240184@gmail.com>
2024-04-07 16:11:06 +0800

    remove config

--- fe/fe-core/src/main/java/org/apache/doris/cloud/system/CloudSystemInfoService.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/system/CloudSystemInfoService.java
@@ -92,8 +92,7 @@ public class CloudSystemInfoService extends SystemInfoService {
     public Cloud.GetClusterResponse getCloudCluster(String clusterName, String clusterId, String userName) {
         Cloud.GetClusterRequest.Builder builder = Cloud.GetClusterRequest.newBuilder();
         builder.setCloudUniqueId(Config.cloud_unique_id)
-            .setClusterName(clusterName).setClusterId(clusterId).setMysqlUserName(userName)
-                .setRequiredBuiltInVault(!Config.enable_start_without_built_in_vault);
+            .setClusterName(clusterName).setClusterId(clusterId).setMysqlUserName(userName);
         final Cloud.GetClusterRequest pRequest = builder.build();
         Cloud.GetClusterResponse response;
         try {
