826d2281a3dac33596e3ed9aab7cf70d0501ca3f
LiBinfeng <46676950+LiBinfeng-01@users.noreply.github.com>
2024-04-07 14:24:47 +0800

    [chore](test) nereids support window function but some cases does not open yet (#33098)

--- regression-test/suites/nereids_p0/join/test_join_15.groovy
+++ regression-test/suites/nereids_p0/join/test_join_15.groovy
@@ -200,13 +200,11 @@ suite("test_join_15", "nereids_p0") {
             order by a.k1, b.k1"""
     }
     //  windows
-    // Nereids does't support window function
-    // def res97 = sql"""select * from (select k1, k2, sum(k2) over (partition by k1) as ss from ${null_table_2})a
-    //    left join ${null_table_1} b on  a.k2=b.k2 and a.k1 >b.k1 order by a.k1, b.k1"""
+     def res97 = sql"""select * from (select k1, k2, sum(k2) over (partition by k1) as ss from ${null_table_2})a
+        left join ${null_table_1} b on  a.k2=b.k2 and a.k1 >b.k1 order by a.k1, b.k1"""
     def res98 = sql"""select * from (select k1, k2, k5 from ${null_table_2}) a left join ${null_table_1} b
       on  a.k2=b.k2 and a.k1 >b.k1 order by a.k1, b.k1"""
-    // Nereids does't support window function
-    // check2_doris(res97, res98)
+    check2_doris(res97, res98)
     sql"drop table ${null_table_1}"
     sql"drop table ${null_table_2}"
 }
