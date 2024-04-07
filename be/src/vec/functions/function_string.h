3181054e254f409cc6b94d0d464b9e496816303a
xiedeyantu <czjourney@163.com>
2024-04-07 22:02:34 +0800

    fix

--- be/src/vec/functions/function_string.h
+++ be/src/vec/functions/function_string.h
@@ -351,10 +351,8 @@ private:
         }
     }
 
-    static void vector_vector(
-            const ColumnString& vec0,
-            const ColumnString& vec1,
-            ColumnInt16& res) {
+    static void vector_vector(const ColumnString& vec0, const ColumnString& vec1,
+                              ColumnInt16& res) {
         size_t size = vec0.size();
         for (size_t i = 0; i < size; ++i) {
             res.get_data()[i] = vec0.get_data_at(i).compare(vec1.get_data_at(i));
