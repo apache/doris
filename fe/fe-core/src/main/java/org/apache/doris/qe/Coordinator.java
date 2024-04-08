f6c8fa255734c098f8967c51b259ebba81e541bd
BePPPower <ftw2139@163.com>
2024-04-08 11:04:47 +0800

    fix conflict

--- fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -4045,22 +4045,6 @@ public class Coordinator implements CoordInterface {
         return fragments;
     }
 
-    private void attachInstanceProfileToFragmentProfile() {
-        if (enablePipelineEngine) {
-            for (PipelineExecContext ctx : pipelineExecContexts.values()) {
-                if (!enablePipelineXEngine) {
-                    ctx.profileStream()
-                            .forEach(p -> executionProfile.addInstanceProfile(ctx.profileFragmentId, p));
-                }
-            }
-        } else {
-            for (BackendExecState backendExecState : backendExecStates) {
-                executionProfile.addInstanceProfile(backendExecState.profileFragmentId,
-                        backendExecState.instanceProfile);
-            }
-        }
-    }
-
     // Runtime filter target fragment instance param
     static class FRuntimeFilterTargetParam {
         public TUniqueId targetFragmentInstanceId;
