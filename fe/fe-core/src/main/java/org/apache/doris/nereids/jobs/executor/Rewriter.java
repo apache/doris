167eac426c3c8e08df2a5b904312cfc44913fa9b
LiBinfeng <1204975323@qq.com>
2024-04-09 17:06:27 +0800

    fix fe ut and p0 problems

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Rewriter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Rewriter.java
@@ -210,8 +210,7 @@ public class Rewriter extends AbstractBatchJobExecutor {
                             new EliminateJoinCondition(),
                             new EliminateAssertNumRows(),
                             new EliminateSemiJoin()
-                    ),
-                    bottomUp(RuleSet.PUSH_DOWN_FILTERS)
+                    )
             ),
             // please note: this rule must run before NormalizeAggregate
             topDown(new AdjustAggregateNullableForEmptySet()),
