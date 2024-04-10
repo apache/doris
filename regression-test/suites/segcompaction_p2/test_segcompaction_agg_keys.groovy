4fab50c4f04a15ec7712296a97ee102ee15ea2b0
huanghaibin <284824253@qq.com>
2024-04-10 18:12:46 +0800

    edit

--- regression-test/suites/segcompaction_p2/test_segcompaction_agg_keys.groovy
+++ regression-test/suites/segcompaction_p2/test_segcompaction_agg_keys.groovy
@@ -19,6 +19,7 @@ import org.codehaus.groovy.runtime.IOGroovyMethods
 
 suite("test_segcompaction_agg_keys") {
     def tableName = "segcompaction_agg_keys_regression_test"
+    def tableName2 = "segcompaction_agg_keys_regression_test_big_table"
     String ak = getS3AK()
     String sk = getS3SK()
     String endpoint = getS3Endpoint()
@@ -117,30 +118,29 @@ suite("test_segcompaction_agg_keys") {
         logger.info("got rows: ${count_src[0][0]}")
 
         // test partial update
-        def tableName2 = "segcompaction_agg_keys_regression_test_big_table"
         sql """ DROP TABLE IF EXISTS ${tableName2} """
         sql """
             CREATE TABLE IF NOT EXISTS ${tableName2} (
-                `col_0` BIGINT NOT NULL,`col_1` VARCHAR(20) REPLACE,`col_2` VARCHAR(20) REPLACE,`col_3` VARCHAR(20) REPLACE,`col_4` VARCHAR(20) REPLACE,
-                `col_5` VARCHAR(20) REPLACE,`col_6` VARCHAR(20) REPLACE,`col_7` VARCHAR(20) REPLACE,`col_8` VARCHAR(20) REPLACE,`col_9` VARCHAR(20) REPLACE,
-                `col_10` VARCHAR(20) REPLACE,`col_11` VARCHAR(20) REPLACE,`col_12` VARCHAR(20) REPLACE,`col_13` VARCHAR(20) REPLACE,`col_14` VARCHAR(20) REPLACE,
-                `col_15` VARCHAR(20) REPLACE,`col_16` VARCHAR(20) REPLACE,`col_17` VARCHAR(20) REPLACE,`col_18` VARCHAR(20) REPLACE,`col_19` VARCHAR(20) REPLACE,
-                `col_20` VARCHAR(20) REPLACE,`col_21` VARCHAR(20) REPLACE,`col_22` VARCHAR(20) REPLACE,`col_23` VARCHAR(20) REPLACE,`col_24` VARCHAR(20) REPLACE,
-                `col_25` VARCHAR(20) REPLACE,`col_26` VARCHAR(20) REPLACE,`col_27` VARCHAR(20) REPLACE,`col_28` VARCHAR(20) REPLACE,`col_29` VARCHAR(20) REPLACE,
-                `col_30` VARCHAR(20) REPLACE,`col_31` VARCHAR(20) REPLACE,`col_32` VARCHAR(20) REPLACE,`col_33` VARCHAR(20) REPLACE,`col_34` VARCHAR(20) REPLACE,
-                `col_35` VARCHAR(20) REPLACE,`col_36` VARCHAR(20) REPLACE,`col_37` VARCHAR(20) REPLACE,`col_38` VARCHAR(20) REPLACE,`col_39` VARCHAR(20) REPLACE,
-                `col_40` VARCHAR(20) REPLACE,`col_41` VARCHAR(20) REPLACE,`col_42` VARCHAR(20) REPLACE,`col_43` VARCHAR(20) REPLACE,`col_44` VARCHAR(20) REPLACE,
-                `col_45` VARCHAR(20) REPLACE,`col_46` VARCHAR(20) REPLACE,`col_47` VARCHAR(20) REPLACE,`col_48` VARCHAR(20) REPLACE,`col_49` VARCHAR(20) REPLACE,
-                `col_50` VARCHAR(20) REPLACE,`col_51` VARCHAR(20) REPLACE,`col_52` VARCHAR(20) REPLACE,`col_53` VARCHAR(20) REPLACE,`col_54` VARCHAR(20) REPLACE,
-                `col_55` VARCHAR(20) REPLACE,`col_56` VARCHAR(20) REPLACE,`col_57` VARCHAR(20) REPLACE,`col_58` VARCHAR(20) REPLACE,`col_59` VARCHAR(20) REPLACE,
-                `col_60` VARCHAR(20) REPLACE,`col_61` VARCHAR(20) REPLACE,`col_62` VARCHAR(20) REPLACE,`col_63` VARCHAR(20) REPLACE,`col_64` VARCHAR(20) REPLACE,
-                `col_65` VARCHAR(20) REPLACE,`col_66` VARCHAR(20) REPLACE,`col_67` VARCHAR(20) REPLACE,`col_68` VARCHAR(20) REPLACE,`col_69` VARCHAR(20) REPLACE,
-                `col_70` VARCHAR(20) REPLACE,`col_71` VARCHAR(20) REPLACE,`col_72` VARCHAR(20) REPLACE,`col_73` VARCHAR(20) REPLACE,`col_74` VARCHAR(20) REPLACE,
-                `col_75` VARCHAR(20) REPLACE,`col_76` VARCHAR(20) REPLACE,`col_77` VARCHAR(20) REPLACE,`col_78` VARCHAR(20) REPLACE,`col_79` VARCHAR(20) REPLACE,
-                `col_80` VARCHAR(20) REPLACE,`col_81` VARCHAR(20) REPLACE,`col_82` VARCHAR(20) REPLACE,`col_83` VARCHAR(20) REPLACE,`col_84` VARCHAR(20) REPLACE,
-                `col_85` VARCHAR(20) REPLACE,`col_86` VARCHAR(20) REPLACE,`col_87` VARCHAR(20) REPLACE,`col_88` VARCHAR(20) REPLACE,`col_89` VARCHAR(20) REPLACE,
-                `col_90` VARCHAR(20) REPLACE,`col_91` VARCHAR(20) REPLACE,`col_92` VARCHAR(20) REPLACE,`col_93` VARCHAR(20) REPLACE,`col_94` VARCHAR(20) REPLACE,
-                `col_95` VARCHAR(20) REPLACE,`col_96` VARCHAR(20) REPLACE,`col_97` VARCHAR(20) REPLACE,`col_98` VARCHAR(20) REPLACE,`col_99` VARCHAR(20) REPLACE
+                `col_0` BIGINT NOT NULL,`col_1` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_2` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_3` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_4` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_5` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_6` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_7` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_8` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_9` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_10` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_11` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_12` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_13` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_14` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_15` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_16` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_17` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_18` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_19` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_20` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_21` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_22` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_23` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_24` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_25` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_26` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_27` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_28` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_29` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_30` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_31` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_32` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_33` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_34` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_35` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_36` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_37` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_38` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_39` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_40` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_41` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_42` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_43` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_44` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_45` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_46` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_47` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_48` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_49` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_50` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_51` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_52` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_53` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_54` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_55` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_56` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_57` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_58` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_59` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_60` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_61` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_62` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_63` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_64` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_65` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_66` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_67` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_68` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_69` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_70` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_71` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_72` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_73` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_74` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_75` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_76` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_77` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_78` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_79` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_80` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_81` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_82` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_83` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_84` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_85` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_86` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_87` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_88` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_89` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_90` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_91` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_92` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_93` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_94` VARCHAR(20) REPLACE_IF_NOT_NULL,
+                `col_95` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_96` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_97` VARCHAR(20) REPLACE_IF_NOT_NULL,`col_98` VARCHAR(20) REPLACE_IF_NOT_NULL
                 )
             AGGREGATE KEY(`col_0`) DISTRIBUTED BY HASH(`col_0`) BUCKETS 1
             PROPERTIES ( "replication_num" = "1" );
@@ -148,16 +148,24 @@ suite("test_segcompaction_agg_keys") {
         sql "set enable_agg_key_partial_update=true;"
         sql "set enable_insert_strict = false;"
         sql "sync;"
+        def columns2 = "col_0, col_50, col_51, col_52, col_53, col_54, col_55, col_56, col_57, col_58, col_59, col_60, col_61, col_62, col_63, col_64, col_65, col_66, col_67, col_68, col_69, col_70, col_71, col_72, col_73, col_74, col_75, col_76, col_77, col_78, col_79, col_80, col_81, col_82, col_83, col_84, col_85, col_86, col_87, col_88, col_89, col_90, col_91, col_92, col_93, col_94, col_95, col_96, col_97, col_98"
         sql "insert into ${tableName2}(${columns}) select ${columns} from ${tableName};"
+        sql "insert into ${tableName2}(${columns2}) select ${columns} from ${tableName};"
         def count_dest = sql " select count() from ${tableName2}; "
         logger.info("got rows: ${count_dest[0][0]}")
         assertEquals(count_src[0][0], count_dest[0][0])
-        qt_select_default """ SELECT * FROM ${tableName2} WHERE col_0=47; """
+        qt_select_default2 """ SELECT * FROM ${tableName2} WHERE col_0=47; """
         sql "set enable_agg_key_partial_update=false;"
         sql "set enable_insert_strict = true;"
+        qt_checksum1 """SELECT MD5(Group_Concat(concat_ws('|', col_0, col_1, col_2, col_3, col_4, col_5, col_6, col_7, col_8, col_9, col_10) ORDER BY col_0 ASC)) AS checksum FROM ${tableName2};"""
+        qt_checksum2 """SELECT MD5(Group_Concat(concat_ws('|', col_0, col_11, col_12, col_13, col_14, col_15, col_16, col_17, col_18, col_19, col_20) ORDER BY col_0 ASC)) AS checksum FROM ${tableName2};"""
+        qt_checksum3 """SELECT MD5(Group_Concat(concat_ws('|', col_0, col_81, col_82, col_83, col_84, col_85, col_86, col_87, col_88, col_89, col_90) ORDER BY col_0 ASC)) AS checksum FROM ${tableName2};"""
+        qt_checksum4 """SELECT MD5(Group_Concat(concat_ws('|', col_0, col_91, col_92, col_93, col_94, col_95, col_96, col_97, col_98) ORDER BY col_0 ASC)) AS checksum FROM ${tableName2};"""
+
         sql "sync;"
 
     } finally {
         try_sql("DROP TABLE IF EXISTS ${tableName}")
+        try_sql("DROP TABLE IF EXISTS ${tableName2}")
     }
 }
