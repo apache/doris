241981ec31dc72520aff0184c014aa5d38e6e77f
zzzxl <33418555+zzzxl1993@users.noreply.github.com>
2024-04-10 17:56:13 +0800

    [fix](inverted index) cloud mod supports time series (#33414)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -2185,7 +2185,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.compactionPolicy();
         }
-        return "";
+        return PropertyAnalyzer.SIZE_BASED_COMPACTION_POLICY;
     }
 
     public void setTimeSeriesCompactionGoalSizeMbytes(long timeSeriesCompactionGoalSizeMbytes) {
@@ -2199,7 +2199,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.timeSeriesCompactionGoalSizeMbytes();
         }
-        return null;
+        return PropertyAnalyzer.TIME_SERIES_COMPACTION_GOAL_SIZE_MBYTES_DEFAULT_VALUE;
     }
 
     public void setTimeSeriesCompactionFileCountThreshold(long timeSeriesCompactionFileCountThreshold) {
@@ -2213,7 +2213,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.timeSeriesCompactionFileCountThreshold();
         }
-        return null;
+        return PropertyAnalyzer.TIME_SERIES_COMPACTION_FILE_COUNT_THRESHOLD_DEFAULT_VALUE;
     }
 
     public void setTimeSeriesCompactionTimeThresholdSeconds(long timeSeriesCompactionTimeThresholdSeconds) {
@@ -2228,7 +2228,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.timeSeriesCompactionTimeThresholdSeconds();
         }
-        return null;
+        return PropertyAnalyzer.TIME_SERIES_COMPACTION_TIME_THRESHOLD_SECONDS_DEFAULT_VALUE;
     }
 
     public void setTimeSeriesCompactionEmptyRowsetsThreshold(long timeSeriesCompactionEmptyRowsetsThreshold) {
@@ -2242,7 +2242,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.timeSeriesCompactionEmptyRowsetsThreshold();
         }
-        return null;
+        return PropertyAnalyzer.TIME_SERIES_COMPACTION_EMPTY_ROWSETS_THRESHOLD_DEFAULT_VALUE;
     }
 
     public void setTimeSeriesCompactionLevelThreshold(long timeSeriesCompactionLevelThreshold) {
@@ -2256,7 +2256,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf {
         if (tableProperty != null) {
             return tableProperty.timeSeriesCompactionLevelThreshold();
         }
-        return null;
+        return PropertyAnalyzer.TIME_SERIES_COMPACTION_LEVEL_THRESHOLD_DEFAULT_VALUE;
     }
 
     public int getBaseSchemaVersion() {
