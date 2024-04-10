018073c096510783c45a417186d8a9a4b2b3edbf
zhangdong <493738387@qq.com>
2024-04-10 15:36:07 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ShowDataStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ShowDataStmt.java
@@ -123,11 +123,6 @@ public class ShowDataStmt extends ShowStmt {
             Util.prohibitExternalCatalog(analyzer.getDefaultCatalog(), this.getClass().getSimpleName());
         }
 
-        // check auth
-        if (!Env.getCurrentEnv().getAccessManager().checkGlobalPriv(ConnectContext.get(), PrivPredicate.ADMIN)) {
-            ErrorReport.reportAnalysisException(ErrorCode.ERR_SPECIFIC_ACCESS_DENIED_ERROR, "ADMIN");
-        }
-
         Database db = Env.getCurrentInternalCatalog().getDbOrAnalysisException(dbName);
 
         // order by
@@ -427,7 +422,11 @@ public class ShowDataStmt extends ShowStmt {
         return toSql();
     }
 
-    private void getAllDbStats() {
+    private void getAllDbStats() throws AnalysisException {
+        // check auth
+        if (!Env.getCurrentEnv().getAccessManager().checkGlobalPriv(ConnectContext.get(), PrivPredicate.ADMIN)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_SPECIFIC_ACCESS_DENIED_ERROR, "ADMIN");
+        }
         List<String> dbNames = Env.getCurrentInternalCatalog().getDbNames();
         if (dbNames == null || dbNames.isEmpty()) {
             return;
