a9005c8ea753a042a459d7a795920bee7e541e32
Yukang-Lian <Yukang.Lian2022@gmail.com>
2024-04-12 01:57:34 +0800

    2

--- be/src/olap/rowset/segment_v2/segment_writer.cpp
+++ be/src/olap/rowset/segment_v2/segment_writer.cpp
@@ -666,7 +666,7 @@ Status SegmentWriter::fill_missing_columns(vectorized::MutableColumns& mutable_f
                                    to_lower(_tablet_schema->column(cids_missing[i]).default_value())
                                                    .find(to_lower("CURRENT_DATE")) !=
                                            std::string::npos)) {
-                    DateV2Value<DateV2ValueType> dv;
+                    vectorized::DateV2Value<vectorized::DateV2ValueType> dv;
                     dv.from_unixtime(_opts.rowset_ctx->partial_update_info->timestamp_ms / 1000,
                                      _opts.rowset_ctx->partial_update_info->timezone);
                     default_value = dv.debug_string();
