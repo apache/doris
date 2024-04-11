8a59d291641d65444bc3c5c31c43eca21c55fd50
zhangstar333 <87313068+zhangstar333@users.noreply.github.com>
2024-04-11 22:45:29 +0800

    [bug](not in) if not in (null) could eos early (#33482)
    * [bug](not in) if not in (null) could eos early

--- be/src/vec/exec/scan/vscan_node.cpp
+++ be/src/vec/exec/scan/vscan_node.cpp
@@ -864,7 +864,8 @@ Status VScanNode::_normalize_not_in_and_not_eq_predicate(VExpr* expr, VExprConte
 
         HybridSetBase::IteratorBase* iter = state->hybrid_set->begin();
         auto fn_name = std::string("");
-        if (!is_fixed_range && state->hybrid_set->contain_null()) {
+        // https://github.com/apache/doris/pull/15466
+        if (state->hybrid_set->contain_null()) {
             _eos = true;
         }
         while (iter->has_next()) {
