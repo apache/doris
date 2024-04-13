0efa129a052c284c3642a8ad45d89235d9fe838d
minghong <englefly@gmail.com>
2024-04-13 14:04:52 +0800

    cse session var

--- regression-test/suites/tpch_sf0.1_p1/sql/cse.groovy
+++ regression-test/suites/tpch_sf0.1_p1/sql/cse.groovy
@@ -20,6 +20,7 @@
 // and modified by Doris.
 
 suite('cse') {
+    sql "set enable_common_sub_expression=true"
     def q1 = """select s_suppkey,n_regionkey,(s_suppkey + n_regionkey) + 1 as x, (s_suppkey + n_regionkey) + 2 as y 
             from supplier join nation on s_nationkey=n_nationkey order by s_suppkey , n_regionkey limit 10 ;
             """
