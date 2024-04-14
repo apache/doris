7eb186686ba05b09796151c9d10ce16ca33ece6a
AlexYue <yj976240184@gmail.com>
2024-04-14 09:40:38 +0800

    [feature](Cloud) Add S3 storage vault implementation (#33377)

--- fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -267,7 +267,6 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 // Execute one show statement.
 public class ShowExecutor {
@@ -3071,11 +3070,8 @@ public class ShowExecutor {
         try {
             Cloud.GetObjStoreInfoResponse resp = MetaServiceProxy.getInstance()
                     .getObjStoreInfo(Cloud.GetObjStoreInfoRequest.newBuilder().build());
-            rows = Stream.concat(
-                    resp.getObjInfoList().stream()
-                            .map(StorageVault::convertToShowStorageVaultProperties),
-                    resp.getStorageVaultList().stream()
-                            .map(StorageVault::convertToShowStorageVaultProperties))
+            rows = resp.getStorageVaultList().stream()
+                            .map(StorageVault::convertToShowStorageVaultProperties)
                     .collect(Collectors.toList());
         } catch (RpcException e) {
             throw new AnalysisException(e.getMessage());
