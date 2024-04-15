b778648a28c5857a2dc565c4d1061e7fc118608c
AlexYue <yj976240184@gmail.com>
2024-04-15 09:45:33 +0800

    [feature](Cloud) Forbid alter table's storage vault related properties (#33620)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyTablePropertiesClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyTablePropertiesClause.java
@@ -302,6 +302,10 @@ public class ModifyTablePropertiesClause extends AlterTableClause {
         } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_FILE_CACHE_TTL_SECONDS)) {
             this.needTableStable = false;
             this.opType = AlterOpType.MODIFY_TABLE_PROPERTY_SYNC;
+        } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_STORAGE_VAULT_NAME)) {
+            throw new AnalysisException("You can not modify storage vault name");
+        } else if (properties.containsKey(PropertyAnalyzer.PROPERTIES_STORAGE_VAULT_ID)) {
+            throw new AnalysisException("You can not modify storage vault id");
         } else {
             throw new AnalysisException("Unknown table property: " + properties.keySet());
         }
