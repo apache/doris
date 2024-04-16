42ec417e60914499d055b6b7ce5b169bfe3fbe92
Xinyi Zou <zouxinyi02@gmail.com>
2024-04-16 15:02:45 +0800

    1

--- be/src/vec/common/pod_array.h
+++ be/src/vec/common/pod_array.h
@@ -139,10 +139,12 @@ protected:
 
     template <typename... TAllocatorParams>
     void alloc(size_t bytes, TAllocatorParams&&... allocator_params) {
-        c_start = c_end = reinterpret_cast<char*>(TAllocator::alloc(
-                                  bytes, std::forward<TAllocatorParams>(allocator_params)...)) +
-                          pad_left;
-        c_end_of_storage = c_start + bytes - pad_right - pad_left;
+        char* allocated = reinterpret_cast<char*>(
+                TAllocator::alloc(bytes, std::forward<TAllocatorParams>(allocator_params)...));
+
+        c_start = allocated + pad_left;
+        c_end = c_start;
+        c_end_of_storage = allocated + bytes - pad_right;
 
         if (pad_left) memset(c_start - ELEMENT_SIZE, 0, ELEMENT_SIZE);
     }
@@ -166,13 +168,13 @@ protected:
 
         ptrdiff_t end_diff = c_end - c_start;
 
-        c_start = reinterpret_cast<char*>(TAllocator::realloc(
-                          c_start - pad_left, allocated_bytes(), bytes,
-                          std::forward<TAllocatorParams>(allocator_params)...)) +
-                  pad_left;
+        char* allocated = reinterpret_cast<char*>(
+                TAllocator::realloc(c_start - pad_left, allocated_bytes(), bytes,
+                                    std::forward<TAllocatorParams>(allocator_params)...));
 
+        c_start = allocated + pad_left;
         c_end = c_start + end_diff;
-        c_end_of_storage = c_start + bytes - pad_right - pad_left;
+        c_end_of_storage = allocated + bytes - pad_right;
     }
 
     bool is_initialized() const {
@@ -280,11 +282,9 @@ protected:
 
     T* t_start() { return reinterpret_cast<T*>(this->c_start); }
     T* t_end() { return reinterpret_cast<T*>(this->c_end); }
-    T* t_end_of_storage() { return reinterpret_cast<T*>(this->c_end_of_storage); }
 
     const T* t_start() const { return reinterpret_cast<const T*>(this->c_start); }
     const T* t_end() const { return reinterpret_cast<const T*>(this->c_end); }
-    const T* t_end_of_storage() const { return reinterpret_cast<const T*>(this->c_end_of_storage); }
 
 public:
     using value_type = T;
@@ -300,7 +300,7 @@ public:
         const_iterator(const T* ptr_) : const_iterator::iterator_adaptor_(ptr_) {}
     };
 
-    PODArray() {}
+    PODArray() = default;
 
     PODArray(size_t n) {
         this->alloc_for_num_elements(n);
@@ -379,8 +379,9 @@ public:
 
     template <typename U, typename... TAllocatorParams>
     void push_back(U&& x, TAllocatorParams&&... allocator_params) {
-        if (UNLIKELY(this->c_end == this->c_end_of_storage))
+        if (UNLIKELY(this->c_end + sizeof(T) > this->c_end_of_storage)) {
             this->reserve_for_next_size(std::forward<TAllocatorParams>(allocator_params)...);
+        }
 
         new (t_end()) T(std::forward<U>(x));
         this->c_end += this->byte_size(1);
@@ -420,7 +421,9 @@ public:
       */
     template <typename... Args>
     void emplace_back(Args&&... args) {
-        if (UNLIKELY(this->c_end == this->c_end_of_storage)) this->reserve_for_next_size();
+        if (UNLIKELY(this->c_end + sizeof(T) > this->c_end_of_storage)) {
+            this->reserve_for_next_size();
+        }
 
         new (t_end()) T(std::forward<Args>(args)...);
         this->c_end += this->byte_size(1);
@@ -598,23 +601,27 @@ public:
 
     void assign(const PODArray& from) { assign(from.begin(), from.end()); }
 
-    bool operator==(const PODArray& other) const {
-        if (this->size() != other.size()) return false;
+    bool operator==(const PODArray& rhs) const {
+        if (this->size() != rhs.size()) {
+            return false;
+        }
 
-        const_iterator this_it = begin();
-        const_iterator that_it = other.begin();
+        const_iterator lhs_it = begin();
+        const_iterator rhs_it = rhs.begin();
 
-        while (this_it != end()) {
-            if (*this_it != *that_it) return false;
+        while (lhs_it != end()) {
+            if (*lhs_it != *rhs_it) {
+                return false;
+            }
 
-            ++this_it;
-            ++that_it;
+            ++lhs_it;
+            ++rhs_it;
         }
 
         return true;
     }
 
-    bool operator!=(const PODArray& other) const { return !operator==(other); }
+    bool operator!=(const PODArray& rhs) const { return !operator==(rhs); }
 };
 
 template <typename T, size_t initial_bytes, typename TAllocator, size_t pad_right_>
