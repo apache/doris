14a1b54cfa5dd0f9d1161f055a574791d4b359cf
HappenLee <happenlee@hotmail.com>
2024-04-17 14:04:43 +0800

    change comment

--- be/src/vec/columns/column_string.h
+++ be/src/vec/columns/column_string.h
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 // This file is copied from
-// https://github.com/ClickHouse/ClickHouse/blob/master/src/Columns/ColumnStr<T>.h
+// https://github.com/ClickHouse/ClickHouse/blob/master/src/Columns/ColumnString.h
 // and modified by Doris
 
 #pragma once
