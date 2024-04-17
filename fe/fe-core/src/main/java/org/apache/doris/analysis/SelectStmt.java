09022a8f126df921655cff42fec6727aac63495d
starocean999 <12095047@qq.com>
2024-04-17 17:51:36 +0800

    [fix](planner)create view statement should forbid mv rewrite

--- fe/fe-core/src/main/java/org/apache/doris/analysis/SelectStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/SelectStmt.java
@@ -501,8 +501,6 @@ public class SelectStmt extends QueryStmt {
                 }
             }
         }
-        fromClause.setNeedToSql(needToSql);
-        fromClause.analyze(analyzer);
 
         if (!isForbiddenMVRewrite()) {
             Boolean haveMv = false;
@@ -520,6 +518,9 @@ public class SelectStmt extends QueryStmt {
                 forbiddenMVRewrite();
             }
         }
+
+        fromClause.setNeedToSql(needToSql);
+        fromClause.analyze(analyzer);
 
         // Generate !empty() predicates to filter out empty collections.
         // Skip this step when analyzing a WITH-clause because CollectionTableRefs
