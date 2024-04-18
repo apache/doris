4954b1cea8cbbea66f2647e3cd2350f71f910208
starocean999 <40539150+starocean999@users.noreply.github.com>
2024-04-18 12:24:55 +0800

    [fix](planner)create view statement should forbid mv rewrite (#33785)

--- regression-test/suites/mv_p0/test_base/test_base.groovy
+++ regression-test/suites/mv_p0/test_base/test_base.groovy
@@ -54,4 +54,36 @@ suite ("test_base") {
         contains "(dwd)"
     }
     qt_select_mv "SELECT id,created_at FROM dwd order by 1, 2;"
+
+    sql """set enable_nereids_planner=false;"""
+    sql """drop table if exists test_mv_view_t;"""
+    sql """drop view if exists test_mv_view_t_view;"""
+    sql """CREATE TABLE `test_mv_view_t` (
+            `day` date NOT NULL,
+            `game_code` varchar(100) NOT NULL ,
+            `plat_code` varchar(100) NOT NULL
+            ) ENGINE=OLAP
+            duplicate KEY(`day`)
+            DISTRIBUTED BY HASH(`day`) BUCKETS 4
+            PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1"
+            );"""
+    sql """INSERT INTO test_mv_view_t VALUES('2024-04-01',  'x', 'y');"""
+    createMV ("""create  materialized view  test_mv_view_t_mv as
+                select `day`, count(game_code)
+                from test_mv_view_t group by day;""")
+    sql """create view test_mv_view_t_view 
+            as
+            select `day`
+                from test_mv_view_t
+                where day<'2024-04-15'
+
+            union all
+                select `day`
+                from test_mv_view_t
+                where day>='2024-04-15';"""
+    explain {
+        sql("""SELECT  * from test_mv_view_t_view where day='2024-04-15';""")
+        notContains("mv_day")
+    }
 }
