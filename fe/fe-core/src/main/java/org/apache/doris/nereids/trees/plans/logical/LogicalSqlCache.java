010fbb0638858719ca616af36095aa8d7d37fdef
924060929 <924060929@qq.com>
2024-04-18 10:43:22 +0800

    [fix](Nereids) fix bug of dry run query with sql cache (#33799)
    1. dry run query should not use sql cache
    2. fix test sql cache in cloud mode
    3. enable cache OneRowRelation and EmptyRelation in frontend to skip parse sql

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalSqlCache.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalSqlCache.java
@@ -32,6 +32,7 @@ import org.apache.doris.nereids.trees.plans.algebra.SqlCache;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
 import org.apache.doris.nereids.util.Utils;
 import org.apache.doris.proto.InternalService;
+import org.apache.doris.qe.ResultSet;
 import org.apache.doris.thrift.TUniqueId;
 
 import com.google.common.collect.ImmutableList;
@@ -46,6 +47,7 @@ public class LogicalSqlCache extends LogicalLeaf implements SqlCache, TreeString
     private final TUniqueId queryId;
     private final List<String> columnLabels;
     private final List<Expr> resultExprs;
+    private final Optional<ResultSet> resultSetInFe;
     private final List<InternalService.PCacheValue> cacheValues;
     private final String backendAddress;
     private final String planBody;
@@ -53,11 +55,13 @@ public class LogicalSqlCache extends LogicalLeaf implements SqlCache, TreeString
     /** LogicalSqlCache */
     public LogicalSqlCache(TUniqueId queryId,
             List<String> columnLabels, List<Expr> resultExprs,
-            List<InternalService.PCacheValue> cacheValues, String backendAddress, String planBody) {
+            Optional<ResultSet> resultSetInFe, List<InternalService.PCacheValue> cacheValues,
+            String backendAddress, String planBody) {
         super(PlanType.LOGICAL_SQL_CACHE, Optional.empty(), Optional.empty());
         this.queryId = Objects.requireNonNull(queryId, "queryId can not be null");
         this.columnLabels = Objects.requireNonNull(columnLabels, "columnLabels can not be null");
         this.resultExprs = Objects.requireNonNull(resultExprs, "resultExprs can not be null");
+        this.resultSetInFe = Objects.requireNonNull(resultSetInFe, "resultSetInFe can not be null");
         this.cacheValues = Objects.requireNonNull(cacheValues, "cacheValues can not be null");
         this.backendAddress = Objects.requireNonNull(backendAddress, "backendAddress can not be null");
         this.planBody = Objects.requireNonNull(planBody, "planBody can not be null");
@@ -65,6 +69,10 @@ public class LogicalSqlCache extends LogicalLeaf implements SqlCache, TreeString
 
     public TUniqueId getQueryId() {
         return queryId;
+    }
+
+    public Optional<ResultSet> getResultSetInFe() {
+        return resultSetInFe;
     }
 
     public List<InternalService.PCacheValue> getCacheValues() {
