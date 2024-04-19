439027119e215a562ba157a1c4dd5d4659999841
Luwei <814383175@qq.com>
2024-04-19 22:57:25 +0800

    [fix](schema change) fix schema change check does not calculate reader merged rows (#33825) (#33908)

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1281,6 +1281,8 @@ DECLARE_mInt64(hive_sink_max_file_size);
 // Retry the Open num_retries time waiting 100 milliseconds between retries.
 DECLARE_mInt32(thrift_client_open_num_tries);
 
+DECLARE_mBool(ignore_schema_change_check);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
