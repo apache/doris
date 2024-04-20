e6a6b8220123719f1fad2337c1a3e0e74e9000e0
seawinde <149132972+seawinde@users.noreply.github.com>
2024-04-21 00:09:22 +0800

    [nereids](mtmv) Support rewrite by mv nested materialized view (#33362)
    Support query rewritting by nested materialized view.
    Such as `inner_mv` def is as following
    
    >             select
    >             l_linenumber,
    >             o_custkey,
    >             o_orderkey,
    >             o_orderstatus,
    >             l_partkey,
    >             l_suppkey,
    >             l_orderkey
    >             from lineitem
    >             inner join orders on lineitem.l_orderkey = orders.o_orderkey;
    
    the mv1_0 def is as following:
    
    >             select
    >             l_linenumber,
    >             o_custkey,
    >             o_orderkey,
    >             o_orderstatus,
    >             l_partkey,
    >             l_suppkey,
    >             l_orderkey,
    >             ps_availqty
    >             from inner_mv
    >             inner join partsupp on l_partkey = ps_partkey AND l_suppkey = ps_suppkey;
    
    
    for the following query, both inner_mv and mv1_0 can be successful when query rewritting by materialized view，and cbo will chose `mv1_0` finally.
    
    >            select lineitem.l_linenumber
    >             from lineitem
    >             inner join orders on l_orderkey = o_orderkey
    >             inner join partsupp on  l_partkey = ps_partkey AND l_suppkey = ps_suppkey
    >             where o_orderstatus = 'o' AND l_linenumber in (1, 2, 3, 4, 5)

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/mv/HyperGraphAggTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/mv/HyperGraphAggTest.java
@@ -31,6 +31,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.BitSet;
 import java.util.Objects;
 
 class HyperGraphAggTest extends SqlTestBase {
@@ -49,7 +50,7 @@ class HyperGraphAggTest extends SqlTestBase {
                 .rewrite()
                 .applyExploration(RuleSet.BUSHY_TREE_JOIN_REORDER)
                 .getAllPlan().get(0).child(0);
-        HyperGraph h1 = HyperGraph.builderForMv(p2).buildAll().get(0);
+        HyperGraph h1 = HyperGraph.builderForMv(p2).build();
         Assertions.assertEquals("id", Objects.requireNonNull(((StructInfoNode) h1.getNode(1)).getExpressions()).get(0).toSql());
     }
 
@@ -79,8 +80,8 @@ class HyperGraphAggTest extends SqlTestBase {
                 .rewrite()
                 .applyExploration(RuleSet.BUSHY_TREE_JOIN_REORDER)
                 .getAllPlan().get(0).child(0);
-        HyperGraph h1 = HyperGraph.builderForMv(p1).buildAll().get(0);
-        HyperGraph h2 = HyperGraph.builderForMv(p2).buildAll().get(0);
+        HyperGraph h1 = HyperGraph.builderForMv(p1).build();
+        HyperGraph h2 = HyperGraph.builderForMv(p2).build();
         ComparisonResult res = HyperGraphComparator.isLogicCompatible(h1, h2, constructContext(p1, p2));
         Assertions.assertTrue(!res.isInvalid());
         Assertions.assertEquals(2, res.getViewNoNullableSlot().size());
@@ -88,9 +89,9 @@ class HyperGraphAggTest extends SqlTestBase {
 
     LogicalCompatibilityContext constructContext(Plan p1, Plan p2) {
         StructInfo st1 = MaterializedViewUtils.extractStructInfo(p1,
-                null).get(0);
+                null, new BitSet()).get(0);
         StructInfo st2 = MaterializedViewUtils.extractStructInfo(p2,
-                null).get(0);
+                null, new BitSet()).get(0);
         RelationMapping rm = RelationMapping.generate(st1.getRelations(), st2.getRelations()).get(0);
         SlotMapping sm = SlotMapping.generate(rm);
         return LogicalCompatibilityContext.from(rm, sm, st1, st2);
