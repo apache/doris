bd8d2f47832c08874ab79ef75aa04773bac6a613
Luwei <814383175@qq.com>
2024-04-23 20:52:58 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/alter/SchemaChangeJobV2.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/SchemaChangeJobV2.java
@@ -75,6 +75,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
