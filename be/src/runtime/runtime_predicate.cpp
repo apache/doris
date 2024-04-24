ffac92c7e57e3bea8ac560855af555869c1973b7
Pxl <pxl290@qq.com>
2024-04-24 10:54:02 +0800

    [Feature](top-n) support topn filter on vscan node (#33496)

--- be/src/runtime/runtime_predicate.cpp
+++ be/src/runtime/runtime_predicate.cpp
@@ -133,7 +133,7 @@ Status RuntimePredicate::init(PrimitiveType type, bool nulls_first, bool is_asc,
 Status RuntimePredicate::update(const Field& value) {
     std::unique_lock<std::shared_mutex> wlock(_rwlock);
     // skip null value
-    if (value.is_null() || !_inited || !_tablet_schema) {
+    if (value.is_null() || !_inited) {
         return Status::OK();
     }
 
@@ -149,7 +149,9 @@ Status RuntimePredicate::update(const Field& value) {
         }
     }
 
-    if (!updated) {
+    _has_value = true;
+
+    if (!updated || !_tablet_schema) {
         return Status::OK();
     }
 
--- be/src/vec/exprs/vruntimefilter_wrapper.cpp
+++ be/src/vec/exprs/vruntimefilter_wrapper.cpp
@@ -35,6 +35,7 @@
 #include "vec/core/column_with_type_and_name.h"
 #include "vec/core/types.h"
 #include "vec/data_types/data_type.h"
+#include "vec/utils/util.hpp"
 
 namespace doris {
 class RowDescriptor;
@@ -81,16 +82,9 @@ void VRuntimeFilterWrapper::close(VExprContext* context,
 Status VRuntimeFilterWrapper::execute(VExprContext* context, Block* block, int* result_column_id) {
     DCHECK(_open_finished || _getting_const_col);
     if (_always_true) {
-        auto res_data_column = ColumnVector<UInt8>::create(block->rows(), 1);
-        size_t num_columns_without_result = block->columns();
-        if (_data_type->is_nullable()) {
-            auto null_map = ColumnVector<UInt8>::create(block->rows(), 0);
-            block->insert({ColumnNullable::create(std::move(res_data_column), std::move(null_map)),
-                           _data_type, expr_name()});
-        } else {
-            block->insert({std::move(res_data_column), _data_type, expr_name()});
-        }
-        *result_column_id = num_columns_without_result;
+        block->insert({create_always_true_column(block->rows(), _data_type->is_nullable()),
+                       _data_type, expr_name()});
+        *result_column_id = block->columns() - 1;
         return Status::OK();
     } else {
         int64_t input_rows = 0, filter_rows = 0;
@@ -118,76 +112,12 @@ Status VRuntimeFilterWrapper::execute(VExprContext* context, Block* block, int* 
 
         const auto rows = block->rows();
         ColumnWithTypeAndName& result_column = block->get_by_position(*result_column_id);
-        if (is_column_const(*result_column.column)) {
-            auto* constant_val = const_cast<char*>(result_column.column->get_data_at(0).data);
-            auto filter =
-                    (constant_val == nullptr) || (!reinterpret_cast<const uint8_t*>(constant_val));
-            // if _null_aware is true, we should check the first args column is nullable. if value in
-            // column is null. we should set it to true
-            if (_null_aware) {
-                DCHECK(!args.empty());
-                // if args is only null, result may be const null column
-                DCHECK(is_column_const(*block->get_by_position(args[0]).column) ||
-                       constant_val == nullptr);
-                if (filter &&
-                    block->get_by_position(args[0]).column->get_data_at(0).data == nullptr) {
-                    auto res_col = ColumnVector<uint8_t>::create(1, 1);
-                    if (result_column.type->is_nullable()) {
-                        result_column.column = make_nullable(std::move(res_col), false);
-                    } else {
-                        result_column.column = std::move(res_col);
-                    }
-                    filter = false;
-                }
-            }
-            if (filter) {
-                filter_rows += rows;
-            }
-        } else if (auto* nullable = check_and_get_column<ColumnNullable>(*result_column.column)) {
-            auto* __restrict data = ((ColumnVector<UInt8>*)nullable->get_nested_column_ptr().get())
-                                            ->get_data()
-                                            .data();
-            auto* __restrict null_map = const_cast<uint8_t*>(nullable->get_null_map_data().data());
 
-            if (_null_aware && block->get_by_position(args[0]).column->is_nullable()) {
-                auto* __restrict null_map_args =
-                        ((ColumnNullable*)block->get_by_position(args[0]).column.get())
-                                ->get_null_map_data()
-                                .data();
-                // TODO: try to simd the code
-                for (int i = 0; i < rows; ++i) {
-                    if (null_map_args[i]) {
-                        null_map[i] = 0;
-                        data[i] = 1;
-                    }
-                    filter_rows += (!null_map[i]) && (data[i] == 1);
-                }
-            } else {
-                filter_rows += doris::simd::count_zero_num(
-                        reinterpret_cast<const int8_t* __restrict>(data), null_map, rows);
-            }
-        } else if (const auto* res_col =
-                           check_and_get_column<ColumnVector<UInt8>>(*result_column.column)) {
-            auto* __restrict data = const_cast<uint8_t*>(res_col->get_data().data());
-            if (_null_aware && block->get_by_position(args[0]).column->is_nullable()) {
-                auto* __restrict null_map_args =
-                        ((ColumnNullable*)block->get_by_position(args[0]).column.get())
-                                ->get_null_map_data()
-                                .data();
-                for (int i = 0; i < rows; ++i) {
-                    data[i] |= null_map_args[i];
-                    filter_rows += data[i];
-                }
-            } else {
-                filter_rows +=
-                        doris::simd::count_zero_num(reinterpret_cast<const int8_t*>(data), rows);
-            }
-        } else {
-            return Status::InternalError(
-                    "Invalid type for runtime filters!, and _expr_name is: {}. _data_type is: {}. "
-                    "result_column_id is: {}. block structure: {}.",
-                    _expr_name, _data_type->get_name(), *result_column_id, block->dump_structure());
+        if (_null_aware) {
+            change_null_to_true(result_column.column, block->get_by_position(args[0]).column);
         }
+
+        filter_rows = rows - calculate_false_number(result_column.column);
         _filtered_rows += filter_rows;
         _scan_rows += input_rows;
         calculate_filter(VRuntimeFilterWrapper::EXPECTED_FILTER_RATE, _filtered_rows, _scan_rows,
