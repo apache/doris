9d5b0a728e76d5d776465e3b8aa8ee19654b99a1
wangbo <506340561@qq.com>
2024-04-24 19:56:48 +0800

    +1

--- be/src/agent/workload_group_listener.cpp
+++ be/src/agent/workload_group_listener.cpp
@@ -73,12 +73,11 @@ void WorkloadGroupListener::handle_topic_info(const std::vector<TopicInfo>& topi
     // NOTE(wb) when is_set_workload_group_info=false, it means FE send a empty workload group list
     // this case happens, but it should not always happen,because FE should has at least one normal group.
     // when this happens, we should not delete any workload group.
-    if (is_set_workload_group_info) {
-        _exec_env->workload_group_mgr()->delete_workload_group_by_ids(current_wg_ids);
-    } else {
+    if (!is_set_workload_group_info) {
         LOG(INFO) << "[topic_publish_wg]unexpected error happens, no workload group info is "
                      "set, list size="
                   << list_size;
     }
+    _exec_env->workload_group_mgr()->delete_workload_group_by_ids(current_wg_ids);
 }
 } // namespace doris
