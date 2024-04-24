2989082770197c11c37e897c6d238fdbd605777a
Mingyu Chen <morningman@163.com>
2024-04-24 21:44:07 +0800

    [fix](parse) set origin stmt for select stmt generated from show stmt (#34015)
    * [fix](parse) set origin stmt for select stmt generated from show stmt
    
    * 2

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -1110,6 +1110,9 @@ public class StmtExecutor {
         if (parsedStmt instanceof ShowStmt) {
             SelectStmt selectStmt = ((ShowStmt) parsedStmt).toSelectStmt(analyzer);
             if (selectStmt != null) {
+                // Need to set origin stmt for new "parsedStmt"(which is selectStmt here)
+                // Otherwise, the log printing may result in NPE
+                selectStmt.setOrigStmt(parsedStmt.getOrigStmt());
                 setParsedStmt(selectStmt);
             }
         }
