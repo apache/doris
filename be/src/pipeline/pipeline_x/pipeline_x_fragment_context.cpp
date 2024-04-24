4e6c22ef81c6ccb461f87abe298ef710bf19ec5e
Gabriel <gabrielleebuaa@gmail.com>
2024-04-24 21:49:11 +0800

    [pipeline](fix) Prevent re-cancel pipeline tasks (#34073)

--- be/src/pipeline/pipeline_x/pipeline_x_fragment_context.cpp
+++ be/src/pipeline/pipeline_x/pipeline_x_fragment_context.cpp
@@ -130,6 +130,13 @@ PipelineXFragmentContext::~PipelineXFragmentContext() {
 
 void PipelineXFragmentContext::cancel(const PPlanFragmentCancelReason& reason,
                                       const std::string& msg) {
+    {
+        std::lock_guard<std::mutex> l(_task_mutex);
+        if (_closed_tasks == _total_tasks) {
+            // All tasks in this PipelineXFragmentContext already closed.
+            return;
+        }
+    }
     LOG_INFO("PipelineXFragmentContext::cancel")
             .tag("query_id", print_id(_query_id))
             .tag("fragment_id", _fragment_id)
@@ -158,6 +165,9 @@ void PipelineXFragmentContext::cancel(const PPlanFragmentCancelReason& reason,
     // _exec_env->result_queue_mgr()->update_queue_status(id, Status::Aborted(msg));
     for (auto& tasks : _tasks) {
         for (auto& task : tasks) {
+            if (task->is_finished()) {
+                continue;
+            }
             task->clear_blocking_state();
         }
     }
