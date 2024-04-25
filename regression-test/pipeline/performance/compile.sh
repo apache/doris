600e427af8204e697e476d8bd1dcae4ee0e56d80
Xinyi Zou <zouxinyi02@gmail.com>
2024-04-26 07:21:00 +0800

    Revert "[fix](memory) Fix Jemalloc hook failed to start BE with JDK 17 #33946" (#34107)
    This reverts commit 2c13d49076b1487739a961ab645d1e8f480a3813.

--- regression-test/pipeline/performance/compile.sh
+++ regression-test/pipeline/performance/compile.sh
@@ -135,7 +135,6 @@ sudo docker run -i --rm \
                     && export CCACHE_REMOTE_STORAGE=file:///root/ccache \
                     && export EXTRA_CXX_FLAGS=-O3 \
                     && export USE_JEMALLOC='ON' \
-                    && export USE_JEMALLOC_HOOK='OFF' \
 					&& export ENABLE_PCH=OFF ${jdk17_str}\
                     && export CUSTOM_NPM_REGISTRY=https://registry.npmjs.org \
                     && bash build.sh --fe --be --clean 2>&1 | tee build.log"
