03a7b511deda33fc1383660f1c8c40b13b4d7a34
StarryVerse <113903752+StarryVerse@users.noreply.github.com>
2024-04-25 20:04:55 +0800

    Update doris_main.cpp
    Log(FATAL) introduces a core dump, which is confusing for users. We should print error msg and exit without a core dump.

--- be/src/service/doris_main.cpp
+++ be/src/service/doris_main.cpp
@@ -169,7 +169,7 @@ auto instruction_fail_to_string(InstructionFail fail) {
     case InstructionFail::ARM_NEON:
         ret("ARM_NEON");
     }
-    LOG(FATAL) << "__builtin_unreachable";
+    LOG(ERROR) << "__builtin_unreachable";
     __builtin_unreachable();
 }
 
