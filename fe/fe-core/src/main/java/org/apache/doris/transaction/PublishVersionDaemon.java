a9d19bee151b836e985aceda8a055ed425e4a597
deardeng <565620795@qq.com>
2024-04-26 14:12:32 +0800

    rebase

--- fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
+++ fe/fe-core/src/main/java/org/apache/doris/transaction/PublishVersionDaemon.java
@@ -48,9 +48,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class PublishVersionDaemon extends MasterDaemon {
 
