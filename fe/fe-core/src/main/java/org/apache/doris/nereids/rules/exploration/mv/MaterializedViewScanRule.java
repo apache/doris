36e9199df4ac202983c333e865cc6934191b3282
seawinde <149132972+seawinde@users.noreply.github.com>
2024-04-27 13:08:23 +0800

    [feature](mtmv)Support single table mv rewrite (#34185)
    Support Single table  query rewrite with out group by
    this is useful for complex filter or expresission
    
    the mv def and query is as following
    which can be query rewritten
    
    mv def:
    ```
              select *
                from lineitem where l_comment like '%xx%'
    ```
    
    query:
    ```
                select l_linenumber, l_receiptdate
                from lineitem where l_comment like '%xx%'
    ```
    
    
    Co-authored-by: zfr9527 <qhu15zhang3294197@163.com>

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializedViewScanRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializedViewScanRule.java
@@ -17,17 +17,67 @@
 
 package org.apache.doris.nereids.rules.exploration.mv;
 
-import org.apache.doris.nereids.rules.Rule;
+import org.apache.doris.nereids.rules.exploration.mv.StructInfo.PlanCheckContext;
+import org.apache.doris.nereids.rules.exploration.mv.mapping.SlotMapping;
+import org.apache.doris.nereids.trees.expressions.Alias;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.expressions.NamedExpression;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
+
+import com.google.common.collect.ImmutableSet;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This is responsible for single table rewriting according to different pattern
  * */
-public class MaterializedViewScanRule extends AbstractMaterializedViewRule {
+public abstract class MaterializedViewScanRule extends AbstractMaterializedViewRule {
 
     @Override
-    public List<Rule> buildRules() {
-        return null;
+    protected Plan rewriteQueryByView(MatchMode matchMode,
+            StructInfo queryStructInfo,
+            StructInfo viewStructInfo,
+            SlotMapping targetToSourceMapping,
+            Plan tempRewritedPlan,
+            MaterializationContext materializationContext) {
+        // Rewrite top projects, represent the query projects by view
+        List<Expression> expressionsRewritten = rewriteExpression(
+                queryStructInfo.getExpressions(),
+                queryStructInfo.getTopPlan(),
+                materializationContext.getMvExprToMvScanExprMapping(),
+                targetToSourceMapping,
+                true,
+                queryStructInfo.getTableBitSet()
+        );
+        // Can not rewrite, bail out
+        if (expressionsRewritten.isEmpty()) {
+            materializationContext.recordFailReason(queryStructInfo,
+                    "Rewrite expressions by view in scan fail",
+                    () -> String.format("expressionToRewritten is %s,\n mvExprToMvScanExprMapping is %s,\n"
+                                    + "targetToSourceMapping = %s", queryStructInfo.getExpressions(),
+                            materializationContext.getMvExprToMvScanExprMapping(),
+                            targetToSourceMapping));
+            return null;
+        }
+        return new LogicalProject<>(
+                expressionsRewritten.stream()
+                        .map(expression -> expression instanceof NamedExpression ? expression : new Alias(expression))
+                        .map(NamedExpression.class::cast)
+                        .collect(Collectors.toList()),
+                tempRewritedPlan);
+    }
+
+    /**
+     * Check scan is whether valid or not. Support join's input only support project, filter, join,
+     * logical relation, simple aggregate node. Con not have aggregate above on join.
+     * Join condition should be slot reference equals currently.
+     */
+    @Override
+    protected boolean checkPattern(StructInfo structInfo) {
+        PlanCheckContext checkContext = PlanCheckContext.of(ImmutableSet.of());
+        return structInfo.getTopPlan().accept(StructInfo.SCAN_PLAN_PATTERN_CHECKER, checkContext)
+                && !checkContext.isContainsTopAggregate();
     }
 }
