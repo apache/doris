b6b72407c44717623a7bdb6a22a14da223de8a42
zy-kkk <zhongyk10@gmail.com>
2024-04-27 17:52:15 +0800

    add more test

--- regression-test/suites/query_p0/dual/dual.groovy
+++ regression-test/suites/query_p0/dual/dual.groovy
@@ -24,8 +24,26 @@ suite('dual') {
     qt_sql 'select 1 from dual where 1 = 0'
     qt_sql 'select 1+1 from dual'
 
-    sql 'drop table if exists `dual`'
+    // Testing constant expressions in more complex contexts
+    qt_sql 'select * from (select 1 as a from dual) sub'
+    qt_sql 'select 1 from dual group by 1'
+    qt_sql 'select 1 from dual having 1'
+    qt_sql 'select 1 from dual group by 1 having 1'
+    qt_sql 'select 1 from dual order by 1'
+    qt_sql 'select 1 from dual order by 1 desc'
+    qt_sql 'select 1 from dual order by 1 limit 1'
+    qt_sql 'select 1 from dual order by 1 limit 1 offset 1'
+    qt_sql 'select 1 from dual where 1 in (1)'
+    qt_sql 'select 1 from dual where 1 group by 1'
+    qt_sql 'select 1 from dual where 1 having 1'
+    qt_sql 'select 1 from dual where 1 group by 1 having 1'
+    qt_sql 'select 1 from dual where 1 order by 1'
+    qt_sql 'with cte as (select 1 as a from dual) select a from cte'
+    qt_sql 'select a from (select 1 as a from dual union all select 2 as a from dual) u'
+    qt_sql 'select row_number() over (order by 1) from dual;'
 
+    // Dropping and creating a table named 'dual' to test behavior when dual is a real table
+    sql 'drop table if exists `dual`'
     sql '''
         create table `dual` (
             k0 int
@@ -35,36 +53,41 @@ suite('dual') {
             'replication_num'='1'
         )
     '''
-
     sql 'insert into `dual` values (1)'
     sql 'insert into `dual` values (2)'
     sql 'insert into `dual` values (3)'
 
     qt_sql 'select 1 from `dual`'
     qt_sql 'select 1 from dual'
+
+    // Tests for dropping 'dual' and ensuring correct error handling
     test {
         sql 'drop table if exists dual'
         exception """DUAL is keyword, maybe `DUAL`"""
     }
     sql 'drop table if exists `dual`'
 
+    // Test error handling when table does not exist
     test {
         sql "select 1 from `dual`"
         exception "Table [dual] does not exist in database [regression_test_query_p0_dual]"
     }
 
+    // Disable and enable Nereids planner to check behavior differences
     sql "set enable_nereids_planner = false"
     test {
         sql "select 1 from `dual`"
         exception "Unknown table 'dual'"
     }
     sql "set enable_nereids_planner = true"
+
+    // Tests for unknown column errors
     test {
-            sql "select a from dual"
-            exception "Unknown column 'a' in 'table list'"
+        sql "select a from dual"
+        exception "Unknown column 'a' in 'table list'"
     }
     test {
-            sql "select 1, a from dual"
-            exception "Unknown column 'a' in 'table list'"
+        sql "select 1, a from dual"
+        exception "Unknown column 'a' in 'table list'"
     }
 }
