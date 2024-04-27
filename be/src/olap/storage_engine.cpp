dc713f993eddc117c7ca8eff0c7cc5da973a3314
abmdocrt <Yukang.Lian2022@gmail.com>
2024-04-27 13:51:28 +0800

    [Enhancement](full compaction) Add run status support for full compaction (#34043)
    * The usage is `curl http://{ip}:{host}/api/compaction/run_status?tablet_id={tablet_id}`
    e.g. `curl http://127.0.0.1:8040/api/compaction/run_status?tablet_id=10084`
    
    If full compaction is running, the output will be
    ```
    {
    "status" : "Success",
    "run_status" : true,
    "msg" : "compaction task for this tablet is running",
    "tablet_id" : 10084,
    "compact_type" : "full"
    }
    ```
    else the ouput will be
    ```
    {
    "status" : "Success",
    "run_status" : false,
    "msg" : "compaction task for this tablet is not running",
    "tablet_id" : 10084,
    "compact_type" : "full"
    }
    ```
    
    * 2

--- be/src/olap/storage_engine.cpp
+++ be/src/olap/storage_engine.cpp
@@ -1396,6 +1396,30 @@ Status StorageEngine::get_compaction_status_json(std::string* result) {
     }
     root.AddMember(base_key, path_obj2, root.GetAllocator());
 
+    // full
+    const std::string& full = "FullCompaction";
+    rapidjson::Value full_key;
+    full_key.SetString(full.c_str(), full.length(), root.GetAllocator());
+    rapidjson::Document path_obj3;
+    path_obj3.SetObject();
+    for (auto& it : _tablet_submitted_full_compaction) {
+        const std::string& dir = it.first->path();
+        rapidjson::Value path_key;
+        path_key.SetString(dir.c_str(), dir.length(), path_obj3.GetAllocator());
+
+        rapidjson::Document arr;
+        arr.SetArray();
+
+        for (auto& tablet_id : it.second) {
+            rapidjson::Value key;
+            const std::string& key_str = std::to_string(tablet_id);
+            key.SetString(key_str.c_str(), key_str.length(), path_obj3.GetAllocator());
+            arr.PushBack(key, root.GetAllocator());
+        }
+        path_obj3.AddMember(path_key, arr, path_obj3.GetAllocator());
+    }
+    root.AddMember(full_key, path_obj3, root.GetAllocator());
+
     rapidjson::StringBuffer strbuf;
     rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(strbuf);
     root.Accept(writer);
