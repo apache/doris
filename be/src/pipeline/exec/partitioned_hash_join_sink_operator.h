30a68c1240b16322f1f7145e84debe22affb7b5a
Jerry Hu <mrhhsg@gmail.com>
2024-04-27 10:43:02 +0800

    [fix](spill) use different algorithm to avoid partition data skew (#34162)

--- be/src/pipeline/exec/partitioned_hash_join_sink_operator.h
+++ be/src/pipeline/exec/partitioned_hash_join_sink_operator.h
@@ -26,7 +26,6 @@
 #include "pipeline/exec/join_build_sink_operator.h"
 #include "pipeline/pipeline_x/operator.h"
 #include "vec/runtime/partitioner.h"
-#include "vec/sink/vdata_stream_sender.h" // ShuffleChannelIds
 
 namespace doris {
 class ExecNode;
@@ -34,7 +33,7 @@ class RuntimeState;
 
 namespace pipeline {
 
-using PartitionerType = vectorized::Crc32HashPartitioner<vectorized::ShuffleChannelIds>;
+using PartitionerType = vectorized::Crc32HashPartitioner<vectorized::SpillPartitionChannelIds>;
 
 class PartitionedHashJoinSinkOperatorX;
 
