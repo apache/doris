8f40882701294b35d382b018f4496fc10c88eeea
minghong <englefly@gmail.com>
2024-04-28 15:33:22 +0800

    [fix](nereids) disable PROJECT_OTHER_JOIN_CONDITION rule if bitmap filter is enabled. #34189
    this pr is a quick solution, but not complete.
    runtime filter on NestLoopJoin suffers this bug even without PROJECT_OTHER_JOIN_CONDITION rule.
    
    for example, when enable Min_Max Runtime filter, the target Expression is n_regionkey, but it should be "n_regionkey - 28"
    explain
    select n_nationkey, nrkey
    from (select n_regionkey -28 nrkey, n_nationkey from nation) T
    join region on nrkey > r_regionkey;
    
    we will refactor RuntimeFilterGenerator to completely solve this issue in following pr.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -2561,6 +2561,10 @@ public class SessionVariable implements Serializable, Writable {
         return runtimeFilterType;
     }
 
+    public boolean allowedRuntimeFilterType(TRuntimeFilterType type) {
+        return (runtimeFilterType & type.getValue()) != 0;
+    }
+
     public boolean isRuntimeFilterTypeEnabled(TRuntimeFilterType type) {
         return (runtimeFilterType & type.getValue()) == type.getValue();
     }
