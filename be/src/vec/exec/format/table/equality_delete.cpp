AshinGau/incubator-doris

equality delete

be/src/vec/exec/format/table/equality_delete.cpp
@@ -1,0 +1,143 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "vec/exec/format/table/equality_delete.h"
+
+namespace doris::vectorized {
+
+std::unique_ptr<EqualityDeleteBase> EqualityDeleteBase::get_delete_impl(Block* delete_block) {
+    if (delete_block->columns() == 1) {
+        return std::make_unique<SimpleEqualityDelete>(delete_block);
+    } else {
+        return std::make_unique<MultiEqualityDelete>(delete_block);
+    }
+}
+
+Status SimpleEqualityDelete::_build_set() {
+    COUNTER_UPDATE(num_delete_rows, _delete_block->rows());
+    if (_delete_block->columns() != 1) {
+        return Status::InternalError("Simple equality delete can be only applied with one column");
+    }
+    auto& column_and_type = _delete_block->get_by_position(0);
+    _delete_column_name = column_and_type.name;
+    PrimitiveType type = remove_nullable(column_and_type.type)->get_type_as_type_descriptor().type;
+    _hybrid_set.reset(create_set(type, _delete_block->rows()));
+    _hybrid_set->insert_fixed_len(column_and_type.column, 0);
+    return Status::OK();
+}
+
+Status SimpleEqualityDelete::filter_data_block(Block* data_block) {
+    SCOPED_TIMER(equality_delete_time);
+    auto* column_and_type = data_block->try_get_by_name(_delete_column_name);
+    if (column_and_type == nullptr) {
+        return Status::InternalError("Can't find the delete column '{}' in data file",
+                                     _delete_column_name);
+    }
+    size_t rows = data_block->rows();
+    if (_filter == nullptr) {
+        _filter = std::make_unique<IColumn::Filter>(rows, 1);
+    } else {
+        _filter->resize_fill(rows, 1);
+    }
+
+    if (column_and_type->column->is_nullable()) {
+        const NullMap& null_map =
+                reinterpret_cast<const ColumnNullable*>(column_and_type->column.get())
+                        ->get_null_map_data();
+        _hybrid_set->find_batch_nullable(
+                remove_nullable(column_and_type->column)->assume_mutable_ref(), rows, null_map,
+                *_filter.get());
+        if (_hybrid_set->contain_null()) {
+            auto* filter_data = _filter->data();
+            for (size_t i = 0; i < rows; ++i) {
+                filter_data[i] = filter_data[i] || null_map[i];
+            }
+        }
+    } else {
+        _hybrid_set->find_batch(column_and_type->column->assume_mutable_ref(), rows,
+                                *_filter.get());
+    }
+
+    Block::filter_block_internal(data_block, *_filter.get(), data_block->columns());
+    return Status::OK();
+}
+
+Status MultiEqualityDelete::_build_set() {
+    COUNTER_UPDATE(num_delete_rows, _delete_block->rows());
+    size_t rows = _delete_block->rows();
+    _delete_hashes.clear();
+    _delete_hashes.resize(rows, 0);
+    for (ColumnPtr column : _delete_block->get_columns()) {
+        column->update_hashes_with_value(_delete_hashes.data(), nullptr);
+    }
+    for (size_t i = 0; i < rows; ++i) {
+        _delete_hash_map[_delete_hashes[i]] = i;
+    }
+    _data_column_index.resize(_delete_block->columns());
+    return Status::OK();
+}
+
+Status MultiEqualityDelete::filter_data_block(Block* data_block) {
+    SCOPED_TIMER(equality_delete_time);
+    size_t column_index = 0;
+    for (string column_name : _delete_block->get_names()) {
+        auto* column_and_type = data_block->try_get_by_name(column_name);
+        if (column_and_type == nullptr) {
+            return Status::InternalError("Can't find the delete column '{}' in data file",
+                                         column_name);
+        }
+        _data_column_index[column_index++] = data_block->get_position_by_name(column_name);
+    }
+    size_t rows = data_block->rows();
+    _data_hashes.clear();
+    _data_hashes.resize(rows, 0);
+    for (size_t index : _data_column_index) {
+        data_block->get_by_position(index).column->update_hashes_with_value(_data_hashes.data(),
+                                                                            nullptr);
+    }
+
+    if (_filter == nullptr) {
+        _filter = std::make_unique<IColumn::Filter>(rows, 1);
+    } else {
+        _filter->resize_fill(rows, 1);
+    }
+    auto* filter_data = _filter->data();
+    for (size_t i = 0; i < rows; ++i) {
+        auto iter = _delete_hash_map.find(_data_hashes[i]);
+        if (iter != _delete_hash_map.end()) {
+            filter_data[i] = _equal(data_block, i, iter->second) ? 0 : 1;
+        }
+    }
+
+    Block::filter_block_internal(data_block, *_filter.get(), data_block->columns());
+    return Status::OK();
+}
+
+bool MultiEqualityDelete::_equal(Block* data_block, size_t data_row_index,
+                                 size_t delete_row_index) {
+    for (size_t i = 0; i < _delete_block->columns(); ++i) {
+        ColumnPtr data_col = data_block->get_by_position(_data_column_index[i]).column;
+        ColumnPtr delete_col = _delete_block->get_by_position(i).column;
+        if (data_col->compare_at(data_row_index, delete_row_index, delete_col->assume_mutable_ref(),
+                                 -1) != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+} // namespace doris::vectorized

be/src/vec/exec/format/table/iceberg_reader.cpp
@@ -182,6 +182,11 @@ Status IcebergTableReader::get_next_block(Block* block, size_t* read_rows, bool*
         }
         block->initialize_index_by_name();
     }
+
+    if (_equality_delete_impl != nullptr) {
+        RETURN_IF_ERROR(_equality_delete_impl->filter_data_block(block));
+        *read_rows = block->rows();
+    }
     return res;
 }
 
@@ -213,22 +218,86 @@ Status IcebergTableReader::init_row_filters(const TFileRangeDesc& range) {
     if (version < MIN_SUPPORT_DELETE_FILES_VERSION) {
         return Status::OK();
     }
-    auto& delete_file_type = table_desc.content;
-    const std::vector<TIcebergDeleteFileDesc>& files = table_desc.delete_files;
-    if (files.empty()) {
-        return Status::OK();
+
+    std::vector<TIcebergDeleteFileDesc> position_delete_files;
+    std::vector<TIcebergDeleteFileDesc> equality_delete_files;
+    for (const TIcebergDeleteFileDesc& desc : table_desc.delete_files) {
+        if (desc.__isset.field_ids) {
+            equality_delete_files.emplace_back(desc);
+        } else {
+            position_delete_files.emplace_back(desc);
+        }
     }
 
-    if (delete_file_type == POSITION_DELETE) {
-        RETURN_IF_ERROR(_position_delete(files));
+    if (position_delete_files.size() > 0) {
+        RETURN_IF_ERROR(_position_delete(position_delete_files));
+    }
+    if (equality_delete_files.size() > 0) {
+        RETURN_IF_ERROR(_equality_delete(equality_delete_files));
     }
 
-    // todo: equality delete
-    //       If it is a count operation and it has equality delete file kind,
-    //       the push down operation of the count for this split needs to be canceled.
-
-    COUNTER_UPDATE(_iceberg_profile.num_delete_files, files.size());
+    COUNTER_UPDATE(_iceberg_profile.num_delete_files, table_desc.delete_files.size());
     return Status::OK();
+}
+
+Status IcebergTableReader::_equality_delete(
+        const std::vector<TIcebergDeleteFileDesc>& delete_files) {
+    bool init_schema = false;
+    std::vector<std::string> delete_file_col_names;
+    std::vector<TypeDescriptor> delete_file_col_types;
+    std::unordered_map<std::string, std::tuple<std::string, const SlotDescriptor*>>
+            partition_columns;
+    std::unordered_map<std::string, VExprContextSPtr> missing_columns;
+
+    for (auto& delete_file : delete_files) {
+        TFileRangeDesc delete_range;
+        // must use __set() method to make sure __isset is true
+        delete_range.__set_fs_name(_range.fs_name);
+        delete_range.path = delete_file.path;
+        delete_range.start_offset = 0;
+        delete_range.size = -1;
+        delete_range.file_size = -1;
+        ParquetReader delete_reader(_profile, _params, delete_range, 102400,
+                                    const_cast<cctz::time_zone*>(&_state->timezone_obj()), _io_ctx,
+                                    _state);
+        if (!init_schema) {
+            static_cast<void>(delete_reader.get_parsed_schema(&delete_file_col_names,
+                                                              &delete_file_col_types));
+            _generate_equality_delete_block(&_equality_delete_block, delete_file_col_names,
+                                            delete_file_col_types);
+            init_schema = true;
+        }
+        RETURN_IF_ERROR(delete_reader.open());
+        RETURN_IF_ERROR(delete_reader.init_reader(delete_file_col_names, _not_in_file_col_names,
+                                                  nullptr, {}, nullptr, nullptr, nullptr, nullptr,
+                                                  nullptr, false));
+        RETURN_IF_ERROR(delete_reader.set_fill_columns(partition_columns, missing_columns));
+        bool eof = false;
+        while (!eof) {
+            Block block;
+            _generate_equality_delete_block(&block, delete_file_col_names, delete_file_col_types);
+            size_t read_rows = 0;
+            RETURN_IF_ERROR(delete_reader.get_next_block(&block, &read_rows, &eof));
+            if (read_rows > 0) {
+                MutableBlock mutable_block(&_equality_delete_block);
+                RETURN_IF_ERROR(mutable_block.merge(block));
+            }
+        }
+    }
+    _equality_delete_impl = EqualityDeleteBase::get_delete_impl(&_equality_delete_block);
+    return Status::OK();
+}
+
+void IcebergTableReader::_generate_equality_delete_block(
+        Block* block, const std::vector<std::string>& delete_file_col_names,
+        const std::vector<TypeDescriptor>& delete_file_col_types) {
+    for (int i = 0; i < delete_file_col_names.size(); ++i) {
+        DataTypePtr data_type =
+                DataTypeFactory::instance().create_data_type(delete_file_col_types[i], true);
+        MutableColumnPtr data_column = data_type->create_column();
+        block->insert(
+                ColumnWithTypeAndName(std::move(data_column), data_type, delete_file_col_names[i]));
+    }
 }
 
 Status IcebergTableReader::_position_delete(

apache/doris

be/src/vec/exec/format/table/equality_delete.cpp
@@ -1,0 +1,156 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "vec/exec/format/table/equality_delete.h"
+
+namespace doris::vectorized {
+
+std::unique_ptr<EqualityDeleteBase> EqualityDeleteBase::get_delete_impl(Block* delete_block) {
+    if (delete_block->columns() == 1) {
+        return std::make_unique<SimpleEqualityDelete>(delete_block);
+    } else {
+        return std::make_unique<MultiEqualityDelete>(delete_block);
+    }
+}
+
+Status SimpleEqualityDelete::_build_set() {
+    COUNTER_UPDATE(num_delete_rows, _delete_block->rows());
+    if (_delete_block->columns() != 1) {
+        return Status::InternalError("Simple equality delete can be only applied with one column");
+    }
+    auto& column_and_type = _delete_block->get_by_position(0);
+    _delete_column_name = column_and_type.name;
+    _delete_column_type = remove_nullable(column_and_type.type)->get_type_as_type_descriptor().type;
+    _hybrid_set.reset(create_set(_delete_column_type, _delete_block->rows()));
+    _hybrid_set->insert_fixed_len(column_and_type.column, 0);
+    return Status::OK();
+}
+
+Status SimpleEqualityDelete::filter_data_block(Block* data_block) {
+    SCOPED_TIMER(equality_delete_time);
+    auto* column_and_type = data_block->try_get_by_name(_delete_column_name);
+    if (column_and_type == nullptr) {
+        return Status::InternalError("Can't find the delete column '{}' in data file",
+                                     _delete_column_name);
+    }
+    if (remove_nullable(column_and_type->type)->get_type_as_type_descriptor().type !=
+        _delete_column_type) {
+        return Status::InternalError("Not support type change in column '{}'", _delete_column_name);
+    }
+    size_t rows = data_block->rows();
+    // _filter: 1 => in _hybrid_set; 0 => not in _hybrid_set
+    if (_filter == nullptr) {
+        _filter = std::make_unique<IColumn::Filter>(rows, 0);
+    } else {
+        _filter->resize_fill(rows, 0);
+    }
+
+    if (column_and_type->column->is_nullable()) {
+        const NullMap& null_map =
+                reinterpret_cast<const ColumnNullable*>(column_and_type->column.get())
+                        ->get_null_map_data();
+        _hybrid_set->find_batch_nullable(
+                remove_nullable(column_and_type->column)->assume_mutable_ref(), rows, null_map,
+                *_filter.get());
+        if (_hybrid_set->contain_null()) {
+            auto* filter_data = _filter->data();
+            for (size_t i = 0; i < rows; ++i) {
+                filter_data[i] = filter_data[i] || null_map[i];
+            }
+        }
+    } else {
+        _hybrid_set->find_batch(column_and_type->column->assume_mutable_ref(), rows,
+                                *_filter.get());
+    }
+    // should reverse _filter
+    auto* filter_data = _filter->data();
+    for (size_t i = 0; i < rows; ++i) {
+        filter_data[i] = !filter_data[i];
+    }
+
+    Block::filter_block_internal(data_block, *_filter.get(), data_block->columns());
+    return Status::OK();
+}
+
+Status MultiEqualityDelete::_build_set() {
+    COUNTER_UPDATE(num_delete_rows, _delete_block->rows());
+    size_t rows = _delete_block->rows();
+    _delete_hashes.clear();
+    _delete_hashes.resize(rows, 0);
+    for (ColumnPtr column : _delete_block->get_columns()) {
+        column->update_hashes_with_value(_delete_hashes.data(), nullptr);
+    }
+    for (size_t i = 0; i < rows; ++i) {
+        _delete_hash_map[_delete_hashes[i]] = i;
+    }
+    _data_column_index.resize(_delete_block->columns());
+    return Status::OK();
+}
+
+Status MultiEqualityDelete::filter_data_block(Block* data_block) {
+    SCOPED_TIMER(equality_delete_time);
+    size_t column_index = 0;
+    for (string column_name : _delete_block->get_names()) {
+        auto* column_and_type = data_block->try_get_by_name(column_name);
+        if (column_and_type == nullptr) {
+            return Status::InternalError("Can't find the delete column '{}' in data file",
+                                         column_name);
+        }
+        if (!_delete_block->get_by_name(column_name).type->equals(*column_and_type->type)) {
+            return Status::InternalError("Not support type change in column '{}'", column_name);
+        }
+        _data_column_index[column_index++] = data_block->get_position_by_name(column_name);
+    }
+    size_t rows = data_block->rows();
+    _data_hashes.clear();
+    _data_hashes.resize(rows, 0);
+    for (size_t index : _data_column_index) {
+        data_block->get_by_position(index).column->update_hashes_with_value(_data_hashes.data(),
+                                                                            nullptr);
+    }
+
+    if (_filter == nullptr) {
+        _filter = std::make_unique<IColumn::Filter>(rows, 1);
+    } else {
+        _filter->resize_fill(rows, 1);
+    }
+    auto* filter_data = _filter->data();
+    for (size_t i = 0; i < rows; ++i) {
+        auto iter = _delete_hash_map.find(_data_hashes[i]);
+        if (iter != _delete_hash_map.end()) {
+            filter_data[i] = _equal(data_block, i, iter->second) ? 0 : 1;
+        }
+    }
+
+    Block::filter_block_internal(data_block, *_filter.get(), data_block->columns());
+    return Status::OK();
+}
+
+bool MultiEqualityDelete::_equal(Block* data_block, size_t data_row_index,
+                                 size_t delete_row_index) {
+    for (size_t i = 0; i < _delete_block->columns(); ++i) {
+        ColumnPtr data_col = data_block->get_by_position(_data_column_index[i]).column;
+        ColumnPtr delete_col = _delete_block->get_by_position(i).column;
+        if (data_col->compare_at(data_row_index, delete_row_index, delete_col->assume_mutable_ref(),
+                                 -1) != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+} // namespace doris::vectorized

be/src/vec/exec/format/table/iceberg_reader.cpp
@@ -142,6 +142,11 @@ Status IcebergTableReader::get_next_block(Block* block, size_t* read_rows, bool*
         }
         block->initialize_index_by_name();
     }
+
+    if (_equality_delete_impl != nullptr) {
+        RETURN_IF_ERROR(_equality_delete_impl->filter_data_block(block));
+        *read_rows = block->rows();
+    }
     return res;
 }
 
@@ -173,23 +178,97 @@ Status IcebergTableReader::init_row_filters(const TFileRangeDesc& range) {
     if (version < MIN_SUPPORT_DELETE_FILES_VERSION) {
         return Status::OK();
     }
-    auto& delete_file_type = table_desc.content;
-    const std::vector<TIcebergDeleteFileDesc>& files = table_desc.delete_files;
-    if (files.empty()) {
-        return Status::OK();
+
+    std::vector<TIcebergDeleteFileDesc> position_delete_files;
+    std::vector<TIcebergDeleteFileDesc> equality_delete_files;
+    for (const TIcebergDeleteFileDesc& desc : table_desc.delete_files) {
+        if (desc.content == POSITION_DELETE) {
+            position_delete_files.emplace_back(desc);
+        } else if (desc.content == EQUALITY_DELETE) {
+            equality_delete_files.emplace_back(desc);
+        }
     }
 
-    if (delete_file_type == POSITION_DELETE) {
-        RETURN_IF_ERROR(_position_delete_base(files));
-    } else if (delete_file_type == EQUALITY_DELETE) {
-        // todo: equality delete
-        //       If it is a count operation and it has equality delete file kind,
-        //       the push down operation of the count for this split needs to be canceled.
-        return Status::NotSupported("NOT SUPPORT EQUALITY_DELETE!");
+    if (position_delete_files.size() > 0) {
+        RETURN_IF_ERROR(_position_delete_base(position_delete_files));
     }
-    COUNTER_UPDATE(_iceberg_profile.num_delete_files, files.size());
+    if (equality_delete_files.size() > 0) {
+        RETURN_IF_ERROR(_equality_delete_base(equality_delete_files));
+    }
+
+    COUNTER_UPDATE(_iceberg_profile.num_delete_files, table_desc.delete_files.size());
     return Status::OK();
 }
+
+Status IcebergTableReader::_equality_delete_base(
+        const std::vector<TIcebergDeleteFileDesc>& delete_files) {
+    bool init_schema = false;
+    std::vector<std::string> equality_delete_col_names;
+    std::vector<TypeDescriptor> equality_delete_col_types;
+    std::unordered_map<std::string, std::tuple<std::string, const SlotDescriptor*>>
+            partition_columns;
+    std::unordered_map<std::string, VExprContextSPtr> missing_columns;
+    std::vector<std::string> not_in_file_col_names;
+
+    for (auto& delete_file : delete_files) {
+        TFileRangeDesc delete_desc;
+        // must use __set() method to make sure __isset is true
+        delete_desc.__set_fs_name(_range.fs_name);
+        delete_desc.path = delete_file.path;
+        delete_desc.start_offset = 0;
+        delete_desc.size = -1;
+        delete_desc.file_size = -1;
+        std::unique_ptr<GenericReader> delete_reader = _create_equality_reader(delete_desc);
+        if (!init_schema) {
+            RETURN_IF_ERROR(delete_reader->get_parsed_schema(&equality_delete_col_names,
+                                                             &equality_delete_col_types));
+            _generate_equality_delete_block(&_equality_delete_block, equality_delete_col_names,
+                                            equality_delete_col_types);
+            init_schema = true;
+        }
+        if (auto* parquet_reader = typeid_cast<ParquetReader*>(delete_reader.get())) {
+            RETURN_IF_ERROR(parquet_reader->open());
+            RETURN_IF_ERROR(parquet_reader->init_reader(equality_delete_col_names,
+                                                        not_in_file_col_names, nullptr, {}, nullptr,
+                                                        nullptr, nullptr, nullptr, nullptr, false));
+        } else if (auto* orc_reader = typeid_cast<OrcReader*>(delete_reader.get())) {
+            RETURN_IF_ERROR(orc_reader->init_reader(&equality_delete_col_names, nullptr, {}, false,
+                                                    {}, {}, nullptr, nullptr));
+        } else {
+            return Status::InternalError("Unsupported format of delete file");
+        }
+
+        RETURN_IF_ERROR(delete_reader->set_fill_columns(partition_columns, missing_columns));
+
+        bool eof = false;
+        while (!eof) {
+            Block block;
+            _generate_equality_delete_block(&block, equality_delete_col_names,
+                                            equality_delete_col_types);
+            size_t read_rows = 0;
+            RETURN_IF_ERROR(delete_reader->get_next_block(&block, &read_rows, &eof));
+            if (read_rows > 0) {
+                MutableBlock mutable_block(&_equality_delete_block);
+                RETURN_IF_ERROR(mutable_block.merge(block));
+            }
+        }
+    }
+    _equality_delete_impl = EqualityDeleteBase::get_delete_impl(&_equality_delete_block);
+    return _equality_delete_impl->init(_profile);
+}
+
+void IcebergTableReader::_generate_equality_delete_block(
+        Block* block, const std::vector<std::string>& equality_delete_col_names,
+        const std::vector<TypeDescriptor>& equality_delete_col_types) {
+    for (int i = 0; i < equality_delete_col_names.size(); ++i) {
+        DataTypePtr data_type =
+                DataTypeFactory::instance().create_data_type(equality_delete_col_types[i], true);
+        MutableColumnPtr data_column = data_type->create_column();
+        block->insert(ColumnWithTypeAndName(std::move(data_column), data_type,
+                                            equality_delete_col_names[i]));
+    }
+}
+
 Status IcebergTableReader::_position_delete_base(
         const std::vector<TIcebergDeleteFileDesc>& delete_files) {
     std::string data_file_path = _range.path;