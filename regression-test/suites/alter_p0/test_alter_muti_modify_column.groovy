cb16b2594279c34858324e9e682e7b2e9bb2dc93
kobe6th <519500479@qq.com>
2024-04-29 02:50:55 +0000

    udpate regression test

--- regression-test/suites/alter_p0/test_alter_muti_modify_column.groovy
+++ regression-test/suites/alter_p0/test_alter_muti_modify_column.groovy
@@ -33,15 +33,40 @@ suite('test_alter_muti_modify_column') {
     """
 
     sql """
+        INSERT INTO ${tbl} VALUES
+        (1, 10, 'abc', 'def'),
+        (2, 20, 'xyz', 'uvw'),
+        (3, 30, 'pqr', 'lmn');
+    """
+
+    // Check data before ALTER TABLE
+    def resultBefore = sql """ SELECT * FROM ${tbl} ORDER BY id """
+    assertEquals(3, resultBefore.size())
+    assertEquals([1, 10, 'abc', 'def'], resultBefore[0])
+    assertEquals([2, 20, 'xyz', 'uvw'], resultBefore[1])
+    assertEquals([3, 30, 'pqr', 'lmn'], resultBefore[2])
+
+    sql """
         ALTER TABLE ${tbl} 
         MODIFY COLUMN `v2` VARCHAR(512) NULL AFTER `v1`,
         MODIFY COLUMN `v3` VARCHAR(512) NULL AFTER `v2`;
     """
 
-    def result = sql """ show create table ${tbl} """
-    def createTbl = result[0][1].toString()
+    // Wait for ALTER TABLE to take effect
+    Thread.sleep(5000)
+
+    // Check table structure after ALTER TABLE
+    def resultCreate = sql """ SHOW CREATE TABLE ${tbl} """
+    def createTbl = resultCreate[0][1].toString()
     assertTrue(createTbl.indexOf("`v2` VARCHAR(512) NULL") > 0)
     assertTrue(createTbl.indexOf("`v3` VARCHAR(512) NULL") > 0)
+
+    // Check data after ALTER TABLE
+    def resultAfter = sql """ SELECT * FROM ${tbl} ORDER BY id """
+    assertEquals(3, resultAfter.size())
+    assertEquals([1, 10, 'abc', 'def'], resultAfter[0])
+    assertEquals([2, 20, 'xyz', 'uvw'], resultAfter[1])
+    assertEquals([3, 30, 'pqr', 'lmn'], resultAfter[2])
 
     sql "DROP TABLE IF EXISTS ${tbl} FORCE"
 }
