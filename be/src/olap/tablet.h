8b899acfb897c0726589fc16730f0be761a57274
Sun Chenyang <csun5285@gmail.com>
2024-04-29 15:00:50 +0800

    [fix](compaction test) show single replica compaction status and fix test (#33076)

--- be/src/olap/tablet.h
+++ be/src/olap/tablet.h
@@ -347,6 +347,8 @@ public:
 
     bool should_fetch_from_peer();
 
+    bool should_fetch_from_peer();
+
     inline bool all_beta() const {
         std::shared_lock rdlock(_meta_lock);
         return _tablet_meta->all_beta();
