5fc1f11cf113d6d091c45e3a593466d4abc657ca
Mingyu Chen <morningman@163.com>
2024-04-30 22:00:05 +0800

    [improvement](hive)add the `queryid` to the temporary file path (#34278) (#34368)
    bp #34278
    
    Co-authored-by: wuwenchi <wuwenchihdu@hotmail.com>

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hive/HMSTransaction.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hive/HMSTransaction.java
@@ -1187,7 +1187,8 @@ public class HMSTransaction implements Transaction {
             String writePath = tableAndMore.getCurrentLocation();
             if (!targetPath.equals(writePath)) {
                 Path path = new Path(targetPath);
-                String oldTablePath = new Path(path.getParent(), "_temp_" + path.getName()).toString();
+                String oldTablePath = new Path(
+                        path.getParent(), "_temp_" + queryId + "_" + path.getName()).toString();
                 Status status = wrapperRenameDirWithProfileSummary(
                         targetPath,
                         oldTablePath,
@@ -1342,7 +1343,8 @@ public class HMSTransaction implements Transaction {
 
             if (!targetPath.equals(writePath)) {
                 Path path = new Path(targetPath);
-                String oldPartitionPath = new Path(path.getParent(), "_temp_" + path.getName()).toString();
+                String oldPartitionPath = new Path(
+                        path.getParent(), "_temp_" + queryId + "_" + path.getName()).toString();
                 Status status = wrapperRenameDirWithProfileSummary(
                         targetPath,
                         oldPartitionPath,
