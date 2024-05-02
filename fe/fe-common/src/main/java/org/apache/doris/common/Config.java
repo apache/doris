8e0e497f8c7132650f8a7bf4347b1099c69b8758
HappenLee <happenlee@hotmail.com>
2024-05-02 11:53:30 +0800

    [Opt](func) opt the percentile func performance (#34373)

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -1765,7 +1765,7 @@ public class Config extends ConfigBase {
      * Max data version of backends serialize block.
      */
     @ConfField(mutable = false)
-    public static int max_be_exec_version = 4;
+    public static int max_be_exec_version = 5;
 
     /**
      * Min data version of backends serialize block.
