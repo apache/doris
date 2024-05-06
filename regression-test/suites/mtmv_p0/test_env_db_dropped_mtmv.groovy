2a9e0983d2e101ba1fc8ff99138731b74c15e094
zhangdong <493738387@qq.com>
2024-05-06 17:14:16 +0800

    1

--- regression-test/suites/mtmv_p0/test_env_db_dropped_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_env_db_dropped_mtmv.groovy
@@ -87,7 +87,7 @@ suite("test_env_db_dropped_mtmv") {
             REFRESH MATERIALIZED VIEW ${dbName2}.${mvName} AUTO;
         """
     waitingMTMVTaskFinishedNotNeedSuccess(jobName)
-    def  msg = sql """select ErrorMsg from tasks('type'='mv') where JobName = '${jobName}' order by CreateTime DESC limit 1"""
-    assertTrue(msg.toString().contains("has been deleted")
+    def msg = sql """select ErrorMsg from tasks('type'='mv') where JobName = '${jobName}' order by CreateTime DESC limit 1"""
     logger.info(msg.toString())
+    assertTrue(msg.toString().contains("has been deleted")
 }
