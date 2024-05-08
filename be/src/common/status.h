a1dd0e1be50a286ba14b1b9ab03c311b21036a75
yiguolei <676222867@qq.com>
2024-05-08 14:07:30 +0800

    [bugfix](compile error) unused variables compile error (#34509)
    * [bugfix](compile error) unused variables compile error
    
    * f
    
    ---------
    
    Co-authored-by: yiguolei <yiguolei@gmail.com>

--- be/src/common/status.h
+++ be/src/common/status.h
@@ -334,7 +334,9 @@ public:
     void check_init() {
         //the signal value is 0, it means the global error states not inited, it's logical error
         // DO NOT use dcheck here, because dcheck depend on glog, and glog maybe not inited at this time.
-        assert(signal_value != 0);
+        if (signal_value == 0) {
+            exit(-1);
+        }
     }
 
 private:
