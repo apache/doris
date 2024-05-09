a9f0212c494a4962fde08a65044488ead032e878
zy-kkk <zhongyk10@gmail.com>
2024-05-09 23:20:49 +0800

    [fix](jdbc catalog) Fix ClassLoader Scope in JdbcExecutor Initialization

--- fe/be-java-extensions/jdbc-scanner/src/main/java/org/apache/doris/jdbc/JdbcExecutor.java
+++ fe/be-java-extensions/jdbc-scanner/src/main/java/org/apache/doris/jdbc/JdbcExecutor.java
@@ -481,13 +481,13 @@ public class JdbcExecutor {
             } else {
                 ClassLoader parent = getClass().getClassLoader();
                 ClassLoader classLoader = UdfUtils.getClassLoader(config.getJdbcDriverUrl(), parent);
+                Thread.currentThread().setContextClassLoader(classLoader);
                 hikariDataSource = JdbcDataSource.getDataSource().getSource(hikariDataSourceKey);
                 if (hikariDataSource == null) {
                     synchronized (hikariDataSourceLock) {
                         hikariDataSource = JdbcDataSource.getDataSource().getSource(hikariDataSourceKey);
                         if (hikariDataSource == null) {
                             long start = System.currentTimeMillis();
-                            Thread.currentThread().setContextClassLoader(classLoader);
                             HikariDataSource ds = new HikariDataSource();
                             ds.setDriverClassName(config.getJdbcDriverClass());
                             ds.setJdbcUrl(config.getJdbcUrl());
