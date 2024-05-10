ff2695840ec027de32b1e84acb9c8cddeaadaac2
shuke <37901441+shuke987@users.noreply.github.com>
2024-05-10 11:39:49 +0800

    [regression-test](pipelinex parameter) modify fuzz batch_size to make regression more faster #34512

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -1958,7 +1958,7 @@ public class SessionVariable implements Serializable, Writable {
                     this.batchSize = 4064;
                     this.enableFoldConstantByBe = true;
                 } else {
-                    this.batchSize = 50;
+                    this.batchSize = 1024;
                     this.enableFoldConstantByBe = false;
                 }
             }
