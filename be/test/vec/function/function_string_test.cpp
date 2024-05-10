0aad42ccca9381b78c25b8eb382b91940a5f3822
koarz <66543806+koarz@users.noreply.github.com>
2024-05-10 17:59:00 +0800

    Update function_string_test.cpp

--- be/test/vec/function/function_string_test.cpp
+++ be/test/vec/function/function_string_test.cpp
@@ -2818,29 +2818,6 @@ TEST(function_string_test, function_overlay_test) {
     }
 }
 
-TEST(function_string_test, function_strcmp_test) {
-    std::string func_name = "strcmp";
-    {
-        BaseInputTypeSet input_types = {TypeIndex::String, TypeIndex::String};
-
-        DataSet data_set = {{{Null(), Null()}, Null()},
-                            {{std::string(""), std::string("")}, (int8_t)0},
-                            {{std::string("test"), std::string("test")}, (int8_t)0},
-                            {{std::string("test1"), std::string("test")}, (int8_t)1},
-                            {{std::string("test"), std::string("test1")}, (int8_t)-1},
-                            {{Null(), std::string("test")}, Null()},
-                            {{std::string("test"), Null()}, Null()},
-                            {{VARCHAR(""), VARCHAR("")}, (int8_t)0},
-                            {{VARCHAR("test"), VARCHAR("test")}, (int8_t)0},
-                            {{VARCHAR("test1"), VARCHAR("test")}, (int8_t)1},
-                            {{VARCHAR("test"), VARCHAR("test1")}, (int8_t)-1},
-                            {{Null(), VARCHAR("test")}, Null()},
-                            {{VARCHAR("test"), Null()}, Null()}};
-        static_cast<void>(
-                check_function_all_arg_comb<DataTypeInt8, true>(func_name, input_types, data_set));
-    }
-}
-
 //bug TEST(function_string_test, function_strcmp_test) {
 //     std::string func_name = "strcmp";
 //     {
