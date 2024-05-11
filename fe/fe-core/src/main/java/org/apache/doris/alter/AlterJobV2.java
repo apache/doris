f6344ba6b6df5e7ad3cb7422000e2aa95a08e37d
yujun777 <yu.jun.reach@gmail.com>
2024-05-11 11:30:23 +0800

    fix schema change cluster name npe

--- fe/fe-core/src/main/java/org/apache/doris/alter/AlterJobV2.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/AlterJobV2.java
@@ -31,6 +31,7 @@ import org.apache.doris.persist.gson.GsonUtils;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.task.AgentTask;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.google.gson.annotations.SerializedName;
 import org.apache.logging.log4j.LogManager;
@@ -223,7 +224,7 @@ public abstract class AlterJobV2 implements Writable {
             return;
         }
 
-        if (!cloudClusterName.isEmpty()) {
+        if (!Strings.isNullOrEmpty(cloudClusterName)) {
             ConnectContext ctx = new ConnectContext();
             ctx.setThreadLocalInfo();
             ctx.setCloudCluster(cloudClusterName);
