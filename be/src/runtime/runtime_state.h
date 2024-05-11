0abdcc761e3b1516cb3bd27fb861586b3b60c81f
BiteTheDDDDt <pxl290@qq.com>
2024-05-11 14:17:45 +0800

    update

--- be/src/runtime/runtime_state.h
+++ be/src/runtime/runtime_state.h
@@ -35,6 +35,7 @@
 #include <utility>
 #include <vector>
 
+#include "agent/be_exec_version_manager.h"
 #include "cctz/time_zone.h"
 #include "common/compiler_util.h" // IWYU pragma: keep
 #include "common/factory_creator.h"
@@ -350,9 +351,8 @@ public:
     int32_t runtime_filter_max_in_num() const { return _query_options.runtime_filter_max_in_num; }
 
     int be_exec_version() const {
-        if (!_query_options.__isset.be_exec_version) {
-            return 0;
-        }
+        CHECK(_query_options.__isset.be_exec_version &&
+              BeExecVersionManager::check_be_exec_version(_query_options.be_exec_version));
         return _query_options.be_exec_version;
     }
     bool enable_pipeline_x_exec() const {
