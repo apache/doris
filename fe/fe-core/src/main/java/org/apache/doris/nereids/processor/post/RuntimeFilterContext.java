dd1b54cf628645214dcd7b96c27d5fa48cfda84b
minghong <englefly@gmail.com>
2024-05-11 09:44:24 +0800

    [pick](nereids)Runtime filter pushdown refactor for branch-2.1 (#34682)
    * [refactor](Nereids)refactor runtime filter generator (#34275)
    
    1. unify the process of generating rf for hash join and for nested loop join
    2. fix some bugs in generating rf
    3. remove some duplicated check
    
    (cherry picked from commit 07267faac0d9c6ef3bb1fd4ee101b4c761c8a2f2)
    
    * [refactor](nereids) do not deny a runtime filter by removing an entry in aliasMap (#34559)
    
    in current version, there are 2 approaches to verify whether a join condition can be used to generate a runtime filter, they are
    1. remove the output slot from aliasMap
    2. pushDownVisitor.visit(...) return false
    the 1st approach has some drawbacks, we prefer to the 2ed approach.
    In this pr, all the cases are handled by the 2ed approach, and remove the related code for the 1st approach.
    
    (cherry picked from commit a29082bf31e66efa2df193b38347e610f2bf7464)
    
    * rebase

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterContext.java
@@ -45,7 +45,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -131,6 +130,7 @@ public class RuntimeFilterContext {
 
     private int targetNullCount = 0;
 
+    private final Map<Plan, Set<PhysicalRelation>> relationsUsedByPlan = Maps.newHashMap();
     private final List<ExpandRF> expandedRF = Lists.newArrayList();
 
     /**
@@ -158,6 +158,23 @@ public class RuntimeFilterContext {
     public RuntimeFilterContext(SessionVariable sessionVariable) {
         this.sessionVariable = sessionVariable;
         this.limits = new FilterSizeLimits(sessionVariable);
+    }
+
+    public void setRelationsUsedByPlan(Plan plan, Set<PhysicalRelation> relations) {
+        relationsUsedByPlan.put(plan, relations);
+    }
+
+    /**
+     * return true, if the relation is in the subtree
+     */
+    public boolean isRelationUseByPlan(Plan plan, PhysicalRelation relation) {
+        Set<PhysicalRelation> relations = relationsUsedByPlan.get(plan);
+        if (relations == null) {
+            relations = Sets.newHashSet();
+            RuntimeFilterGenerator.getAllScanInfo(plan, relations);
+            relationsUsedByPlan.put(plan, relations);
+        }
+        return relations.contains(relation);
     }
 
     public SessionVariable getSessionVariable() {
@@ -273,10 +290,6 @@ public class RuntimeFilterContext {
         return aliasTransferMap;
     }
 
-    public Pair<PhysicalRelation, Slot> aliasTransferMapRemove(NamedExpression slot) {
-        return aliasTransferMap.remove(slot);
-    }
-
     public Pair<PhysicalRelation, Slot> getAliasTransferPair(NamedExpression slot) {
         return aliasTransferMap.get(slot);
     }
@@ -354,7 +367,7 @@ public class RuntimeFilterContext {
     }
 
     public List<ExprId> getTargetExprIdByFilterJoin(AbstractPhysicalJoin join) {
-        return joinToTargetExprId.getOrDefault(join, new ArrayList<>());
+        return joinToTargetExprId.getOrDefault(join, Lists.newArrayList());
     }
 
     public SlotReference getCorrespondingOlapSlotReference(SlotReference slot) {
