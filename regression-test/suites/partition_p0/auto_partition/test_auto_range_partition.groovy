77b766d82ce1d1b262afe736151b17d07d41d071
zhaochangle <zhaochangle@selectdb.com>
2024-05-13 02:27:39 +0800

    1

--- regression-test/suites/partition_p0/auto_partition/test_auto_range_partition.groovy
+++ regression-test/suites/partition_p0/auto_partition/test_auto_range_partition.groovy
@@ -140,8 +140,8 @@ suite("test_auto_range_partition") {
     logger.info("${result2}")
     assertEquals(result2.size(), 2)
 
-     // partition expr extraction
-
+    // partition expr extraction
+    sql " set enable_memtable_on_sink_node = false; " //FIXME: remove when fixed.
     sql " drop table if exists isit "
     sql " drop table if exists isit_src "
     sql """
