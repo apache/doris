1545d96617c6bf5cac7605cbc9884f777e5ab650
morrySnow <101034200+morrySnow@users.noreply.github.com>
2024-05-13 15:12:12 +0800

    [WIP](test) remove enable_nereids_planner in regression cases (part 4) (#34642)
    before PR are
    #34417
    #34490
    #34558

--- regression-test/suites/datatype_p0/nested_types/query/array_functions/test_array_functions_array_intersect_sort.groovy
+++ regression-test/suites/datatype_p0/nested_types/query/array_functions/test_array_functions_array_intersect_sort.groovy
@@ -16,68 +16,6 @@
 // under the License.
 
 suite("test_array_functions_array_intersect_sort", "p0") {
-
-    sql "set enable_nereids_planner=false;"
-    // ========= array_intersect ===========
-    // with sort
-    qt_oLd_sql "SELECT 'array_intersect-array-sort';"
-    sql "drop table if exists tbl_array_intersect;"
-    sql "create table tbl_array_intersect (date Date, arr Array<Int>)  ENGINE=OLAP DISTRIBUTED BY HASH(date) BUCKETS 1 PROPERTIES('replication_num' = '1');"
-
-    sql "insert into tbl_array_intersect values ('2019-01-01', [1,2,3]);"
-    sql "insert into tbl_array_intersect values ('2019-01-02', [1,2]);"
-    sql "insert into tbl_array_intersect values ('2019-01-03', [1]);"
-    sql "insert into tbl_array_intersect values ('2019-01-04', []);"
-
-    order_qt_old_sql "SELECT array_sort(array_intersect(arr, [1,2])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect(arr, [])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([], arr)) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1,2], arr)) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1,2], [1,2,3,4])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([], [])) from tbl_array_intersect order by date;"
-
-
-    order_qt_old_sql "SELECT array_sort(array_intersect(arr, [1,2])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect(arr, [])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([], arr)) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1,2], arr)) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1,2], [1,2,3,4])) from tbl_array_intersect order by date;"
-    order_qt_old_sql "SELECT array_sort(array_intersect([], [])) from tbl_array_intersect order by date;"
-
-
-    order_qt_old_sql "SELECT array_sort(array_intersect([-100], [156]));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1], [257]));"
-
-    order_qt_old_sql "SELECT array_sort(array_intersect(['a', 'b', 'c'], ['a', 'a']));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 1], [2, 2]));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 1], [1, 2]));"
-    //  nereids not support array_intersect with three argument
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 1, 1], [3], [2, 2, 2]));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 2], [1, 2], [2]));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [1]));"
-    order_qt_old_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [2, 2, 2]));"
-
-    // nereids not support array_intersect with one argument
-    try {
-        sql """
-                SELECT array_sort(array_intersect([]));
-                """
-    } catch (Exception ex) {
-        assertTrue(ex.getMessage().contains("No matching function with signature: array_intersect(ARRAY<NULL_TYPE>)"))
-    }
-    try {
-        sql """
-                SELECT array_sort(array_intersect([1, 2, 3]));
-                """
-    } catch (Exception ex) {
-        assertTrue(ex.getMessage().contains("No matching function with signature: array_intersect(ARRAY<TINYINT>)"))
-    }
-
-    // nereids
-    sql "set enable_nereids_planner=true;"
-    sql "set enable_fallback_to_original_planner=false;"
-
-
     // ========= array_intersect ===========
     // with sort
     qt_nereid_sql "SELECT 'array_intersect-array-sort';"
@@ -111,26 +49,17 @@ suite("test_array_functions_array_intersect_sort", "p0") {
     order_qt_nereid_sql "SELECT array_sort(array_intersect(['a', 'b', 'c'], ['a', 'a']));"
     order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [2, 2]));"
     order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [1, 2]));"
-    //  nereids not support array_intersect with three argument
-//    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1, 1], [3], [2, 2, 2]));"
-//    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 2], [1, 2], [2]));"
-//    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [1]));"
-//    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [2, 2, 2]));"
-//
-//    // nereids not support array_intersect with one argument
-//    try {
-//        sql """
-//                SELECT array_sort(array_intersect([]));
-//                """
-//    } catch (Exception ex) {
-//        assertTrue(ex.getMessage().contains("No matching function with signature: array_intersect(ARRAY<NULL_TYPE>)"))
-//    }
-//    try {
-//        sql """
-//                SELECT array_sort(array_intersect([1, 2, 3]));
-//                """
-//    } catch (Exception ex) {
-//        assertTrue(ex.getMessage().contains("No matching function with signature: array_intersect(ARRAY<TINYINT>)"))
-//    }
+    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1, 1], [3], [2, 2, 2]));"
+    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 2], [1, 2], [2]));"
+    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [1]));"
+    order_qt_nereid_sql "SELECT array_sort(array_intersect([1, 1], [2, 1], [2, 2], [2, 2, 2]));"
 
+    test {
+        sql "SELECT array_sort(array_intersect([]))"
+        exception "Can not found function 'array_intersect' which has 1 arity"
+    }
+    test {
+        sql "SELECT array_sort(array_intersect([1, 2, 3]))"
+        exception "Can not found function 'array_intersect' which has 1 arity"
+    }
 }
