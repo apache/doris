d2a72e342a470e5b4025ac03cd49f187414824e3
morningman <morningman@163.com>
2024-05-14 11:58:11 +0800

    [opt](fe) exit FE when transfert to (non)master failed

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -1526,12 +1526,14 @@ public class Env {
             } else {
                 if (journalVersion <= FeMetaVersion.VERSION_114) {
                     // if journal version is less than 114, which means it is upgraded from version before 2.0.
-                    // When upgrading from 1.2 to 2.0, we need to make sure that the parallelism of query remain unchanged
+                    // When upgrading from 1.2 to 2.0,
+                    // we need to make sure that the parallelism of query remain unchanged
                     // when switch to pipeline engine, otherwise it may impact the load of entire cluster
                     // because the default parallelism of pipeline engine is higher than previous version.
                     // so set parallel_pipeline_task_num to parallel_fragment_exec_instance_num
                     int newVal = VariableMgr.newSessionVariable().parallelExecInstanceNum;
-                    VariableMgr.refreshDefaultSessionVariables("1.x to 2.x", SessionVariable.PARALLEL_PIPELINE_TASK_NUM,
+                    VariableMgr.refreshDefaultSessionVariables("1.x to 2.x",
+                            SessionVariable.PARALLEL_PIPELINE_TASK_NUM,
                             String.valueOf(newVal));
 
                     // similar reason as above, need to upgrade broadcast scale factor during 1.2 to 2.x
