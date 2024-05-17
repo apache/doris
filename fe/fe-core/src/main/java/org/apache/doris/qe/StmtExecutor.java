90e5c5985e439d94e7df808db27f7670b95cc980
zhiqiang <seuhezhiqiang@163.com>
2024-05-17 17:53:08 +0800

    [fix](show proc) Fix insert into command cannot be seen in show proc #34953

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -1955,8 +1955,9 @@ public class StmtExecutor {
                 coord.setLoadZeroTolerance(context.getSessionVariable().getEnableInsertStrict());
                 coord.setQueryType(TQueryType.LOAD);
                 profile.addExecutionProfile(coord.getExecutionProfile());
-
-                QeProcessorImpl.INSTANCE.registerQuery(context.queryId(), coord);
+                QeProcessorImpl.QueryInfo queryInfo =
+                            new QeProcessorImpl.QueryInfo(context, this.getOriginStmtInString(), coord);
+                QeProcessorImpl.INSTANCE.registerQuery(context.queryId(), queryInfo);
 
                 coord.exec();
                 int execTimeout = context.getExecTimeout();
