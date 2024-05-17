apache/doris

commit 2d19f2fbfe628077eab7fc8ce78f2c53abf3b7b2

[fix](planner)need call materializeSrcExpr for materialized slots in join node (#25204)

fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
@@ -168,9 +168,6 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(0) instanceof JoinNodeBase && analyzer.isOuterJoined(leftTupleDesc.getId());
                 for (SlotDescriptor leftSlotDesc : leftTupleDesc.getSlots()) {
-                    if (!isMaterializedByChild(leftSlotDesc, getChild(0).getOutputSmap())) {
-                        continue;
-                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(vOutputTupleDesc, leftSlotDesc);
                     if (leftNullable) {
@@ -191,9 +188,6 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(1) instanceof JoinNodeBase && analyzer.isOuterJoined(rightTupleDesc.getId());
                 for (SlotDescriptor rightSlotDesc : rightTupleDesc.getSlots()) {
-                    if (!isMaterializedByChild(rightSlotDesc, getChild(1).getOutputSmap())) {
-                        continue;
-                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(vOutputTupleDesc, rightSlotDesc);
                     if (rightNullable) {
@@ -226,6 +220,7 @@ public abstract class JoinNodeBase extends PlanNode {
                 rSlotRef.getDesc().setIsMaterialized(lSlotRef.getDesc().isMaterialized());
             } else {
                 rSlotRef.getDesc().setIsMaterialized(true);
+                rSlotRef.materializeSrcExpr();
             }
         }
 

regression-test/data/correctness_p0/test_inlineview_with_project.out
@@ -7,3 +7,6 @@
 
 -- !select3 --
 
+-- !select4 --
+0.0
+

regression-test/suites/correctness_p0/test_inlineview_with_project.groovy
@@ -382,4 +382,77 @@ suite("test_inlineview_with_project") {
     sql """
         drop table if exists cir2824_table;
     """
+
+    sql """
+        drop table if exists dws_mf_wms_t1;
+    """
+
+    sql """
+        drop table if exists dws_mf_wms_t2;
+    """
+
+    sql """
+        drop table if exists dws_mf_wms_t3;
+    """
+
+    sql """
+        CREATE TABLE `dws_mf_wms_t1` (
+        `id` varchar(20) NOT NULL COMMENT '',
+        `final_weight` double NULL COMMENT ''
+        ) ENGINE=OLAP
+        UNIQUE KEY(`id`)
+        COMMENT ''
+        DISTRIBUTED BY HASH(`id`) BUCKETS 1
+        PROPERTIES (
+        "replication_allocation" = "tag.location.default: 1"
+        );
+    """
+
+    sql """
+        CREATE TABLE `dws_mf_wms_t2` (
+        `plate_id` varchar(32) NULL COMMENT '',
+        `entry_time` datetime NULL COMMENT ''
+        ) ENGINE=OLAP
+        UNIQUE KEY(`plate_id`)
+        COMMENT ''
+        DISTRIBUTED BY HASH(`plate_id`) BUCKETS 1
+        PROPERTIES (
+        "replication_allocation" = "tag.location.default: 1"
+        );
+    """
+
+    sql """
+        CREATE TABLE `dws_mf_wms_t3` (
+        `material_id` varchar(50) NULL,
+        `out_time` datetime NULL COMMENT ''
+        ) ENGINE=OLAP
+        UNIQUE KEY(`material_id`)
+        COMMENT ' '
+        DISTRIBUTED BY HASH(`material_id`) BUCKETS 1
+        PROPERTIES (
+        "replication_allocation" = "tag.location.default: 1"
+        );
+    """
+
+    sql """insert into dws_mf_wms_t1 values( '1', 1.0);"""
+    sql """insert into dws_mf_wms_t2 values( '1', '2020-02-02 22:22:22');"""
+    sql """insert into dws_mf_wms_t3 values( '1', '2020-02-02 22:22:22');"""
+
+    qt_select4 """select cur_final_weight from (
+                    SELECT
+                        round(`t1`.`final_weight` / 1000 , 2) AS `cur_final_weight`,
+                        coalesce(`t5`.`avg_inv_hours`, 0) AS `avg_inv_hours`,
+                        coalesce(`t5`.`max_inv_hours`, 0) AS `max_inv_hours`
+                    FROM
+                        `dws_mf_wms_t1` t1
+                    LEFT OUTER JOIN (
+                        SELECT
+                            round(avg(timestampdiff(SECOND, `t1`.`entry_time`, `t2`.`out_time`)) / 3600.0, 1) AS `avg_inv_hours`,
+                            round(max(timestampdiff(SECOND, `t1`.`entry_time`, `t2`.`out_time`)) / 3600.0, 1) AS `max_inv_hours`
+                        FROM
+                            `dws_mf_wms_t2` t1
+                        LEFT OUTER JOIN `dws_mf_wms_t3` t2 ON
+                            `t1`.`plate_id` = `t2`.`material_id`) t5 ON
+                        1 = 1
+                        )res;"""
 }

fe/fe-core/src/main/java/org/apache/doris/analysis/SlotDescriptor.java
@@ -92,6 +92,7 @@ public class SlotDescriptor {
         this.isAgg = false;
         this.stats = src.stats;
         this.type = src.type;
+        this.sourceExprs.add(new SlotRef(src));
     }
 
     public boolean isMultiRef() {

fe/fe-core/src/test/java/org/apache/doris/planner/QueryPlanTest.java
@@ -731,7 +731,7 @@ public class QueryPlanTest extends TestWithFeService {
                 + "left join join2 on join1.id = join2.id\n"
                 + "and join1.id > 1;";
         String explainString = getSQLPlanOrErrorMsg("explain " + sql);
-        Assert.assertTrue(explainString.contains("other join predicates: <slot 12> > 1"));
+        Assert.assertTrue(explainString.contains("other join predicates: <slot 12> <slot 0> > 1"));
         Assert.assertFalse(explainString.contains("PREDICATES: `join1`.`id` > 1"));
 
         /*
@@ -818,7 +818,7 @@ public class QueryPlanTest extends TestWithFeService {
                 + "left anti join join2 on join1.id = join2.id\n"
                 + "and join1.id > 1;";
         explainString = getSQLPlanOrErrorMsg("explain " + sql);
-        Assert.assertTrue(explainString.contains("other join predicates: <slot 7> > 1"));
+        Assert.assertTrue(explainString.contains("other join predicates: <slot 7> <slot 0> > 1"));
         Assert.assertFalse(explainString.contains("PREDICATES: `join1`.`id` > 1"));
 
         // test semi join, left table join predicate, only push to left table
@@ -1541,7 +1541,6 @@ public class QueryPlanTest extends TestWithFeService {
     public void testEmptyNode() throws Exception {
         connectContext.setDatabase("default_cluster:test");
         String emptyNode = "EMPTYSET";
-        String denseRank = "dense_rank";
 
         List<String> sqls = Lists.newArrayList();
         sqls.add("explain select * from baseall limit 0");
@@ -1560,7 +1559,6 @@ public class QueryPlanTest extends TestWithFeService {
         for (String sql : sqls) {
             String explainString = getSQLPlanOrErrorMsg(sql);
             Assert.assertTrue(explainString.contains(emptyNode));
-            Assert.assertFalse(explainString.contains(denseRank));
         }
     }
 
@@ -1651,25 +1649,25 @@ public class QueryPlanTest extends TestWithFeService {
         connectContext.setDatabase("default_cluster:test");
         //valid date
         String sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.aid, b.bid FROM (SELECT 3 AS aid) a right outer JOIN (SELECT 4 AS bid) b ON (a.aid=b.bid)";
-        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2>\n" + "    <slot 3>");
+        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2> <slot 0> 3\n" + "    <slot 3>  4");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.aid, b.bid FROM (SELECT 3 AS aid) a left outer JOIN (SELECT 4 AS bid) b ON (a.aid=b.bid)";
-        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2>\n" + "    <slot 3>");
+        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2> <slot 0> 3\n" + "    <slot 3>  4");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.aid, b.bid FROM (SELECT 3 AS aid) a full outer JOIN (SELECT 4 AS bid) b ON (a.aid=b.bid)";
-        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2>\n" + "    <slot 3>");
+        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2> <slot 0> 3\n" + "    <slot 3>  4");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.aid, b.bid FROM (SELECT 3 AS aid) a JOIN (SELECT 4 AS bid) b ON (a.aid=b.bid)";
-        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2>\n" + "    <slot 3>");
+        assertSQLPlanOrErrorMsgContains(sql, "OUTPUT EXPRS:\n" + "    <slot 2> <slot 0> 3\n" + "    <slot 3>  4");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.k1, b.k2 FROM (SELECT k1 from baseall) a LEFT OUTER JOIN (select k1, 999 as k2 from baseall) b ON (a.k1=b.k1)";
-        assertSQLPlanOrErrorMsgContains(sql, "<slot 7>\n" + "    <slot 9>");
+        assertSQLPlanOrErrorMsgContains(sql, "<slot 7>  `k1`\n" + "    <slot 9> <slot 4> 999");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.k1, b.k2 FROM (SELECT 1 as k1 from baseall) a RIGHT OUTER JOIN (select k1, 999 as k2 from baseall) b ON (a.k1=b.k1)";
-        assertSQLPlanOrErrorMsgContains(sql, "<slot 8>\n" + "    <slot 10>");
+        assertSQLPlanOrErrorMsgContains(sql, "<slot 8> <slot 0> 1\n" + "    <slot 10> <slot 3> 999");
 
         sql = "SELECT /*+ SET_VAR(enable_nereids_planner=false) */ a.k1, b.k2 FROM (SELECT 1 as k1 from baseall) a FULL JOIN (select k1, 999 as k2 from baseall) b ON (a.k1=b.k1)";
-        assertSQLPlanOrErrorMsgContains(sql, "<slot 8>\n" + "    <slot 10>");
+        assertSQLPlanOrErrorMsgContains(sql, "<slot 8> <slot 0> 1\n" + "    <slot 10> <slot 3> 999");
     }
 
     @Test
@@ -2064,7 +2062,7 @@ public class QueryPlanTest extends TestWithFeService {
         Assert.assertFalse(explainString.contains("OUTPUT EXPRS:\n    3\n    4"));
         System.out.println(explainString);
         Assert.assertTrue(explainString.contains(
-                "OUTPUT EXPRS:\n" + "    CAST(<slot 4> AS INT)\n" + "    CAST(<slot 5> AS INT)"));
+                "OUTPUT EXPRS:\n" + "    CAST(<slot 4> <slot 2> 3 AS INT)\n" + "    CAST(<slot 5> <slot 3> 4 AS INT)"));
     }
 
     @Test

commit b9f176732a5c001006f047db2ad01e9bd63b0f26

[fix](planner) fix unrequired slot bug when join node introduced by #25204 (#34923)
before fix, join node will retain some slots, which are not materialized and unrequired.
join node need remove these slots and not make them be output slots.

Signed-off-by: nextdreamblue <zxw520blue1@163.com>

fe/fe-core/src/main/java/org/apache/doris/planner/JoinNodeBase.java
@@ -171,6 +171,9 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(0) instanceof JoinNodeBase && analyzer.isOuterJoined(leftTupleDesc.getId());
                 for (SlotDescriptor leftSlotDesc : leftTupleDesc.getSlots()) {
+                    if (!isMaterializedByChild(leftSlotDesc, getChild(0).getOutputSmap())) {
+                        continue;
+                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(outputTupleDesc, leftSlotDesc);
                     if (leftNullable) {
@@ -191,6 +194,9 @@ public abstract class JoinNodeBase extends PlanNode {
                 boolean needSetToNullable =
                         getChild(1) instanceof JoinNodeBase && analyzer.isOuterJoined(rightTupleDesc.getId());
                 for (SlotDescriptor rightSlotDesc : rightTupleDesc.getSlots()) {
+                    if (!isMaterializedByChild(rightSlotDesc, getChild(1).getOutputSmap())) {
+                        continue;
+                    }
                     SlotDescriptor outputSlotDesc =
                             analyzer.getDescTbl().copySlotDescriptor(outputTupleDesc, rightSlotDesc);
                     if (rightNullable) {

regression-test/data/correctness_p0/test_inlineview_with_project.out
@@ -14,9 +14,9 @@
 3
 
 -- !select5 --
-1
-2
-3
+1.0
+2.0
+3.0
 
 -- !select5 --
 1
@@ -28,4 +28,7 @@
 
 -- !select5 --
 3
+
+-- !select6 --
+2020-01-01
 

regression-test/suites/correctness_p0/test_inlineview_with_project.groovy
@@ -665,4 +665,88 @@ suite("test_inlineview_with_project") {
 
     sql """DROP TABLE IF EXISTS `dr_user_test_t1`;"""
     sql """DROP TABLE IF EXISTS `dr_user_test_t2`;"""
+
+    sql """
+        drop table if exists dws_mf_wms_join_t1;
+    """
+
+    sql """
+        drop table if exists dws_mf_wms_join_t2;
+    """
+    
+
+    sql """CREATE TABLE `dws_mf_wms_join_t1` (
+          `ddate` DATE NULL COMMENT '日期字段',
+          `game_id` VARCHAR(65533) NULL,
+          `main_currency_stock` BIGINT NULL
+          ) ENGINE=OLAP
+          DUPLICATE KEY(`ddate`)
+          DISTRIBUTED BY HASH(`ddate`) BUCKETS 10
+          PROPERTIES (
+          "replication_allocation" = "tag.location.default: 1"
+         );"""
+
+    sql """CREATE TABLE `dws_mf_wms_join_t2` (
+          `game_id` VARCHAR(65533) NULL,
+          ) ENGINE=OLAP
+          DUPLICATE KEY(`game_id`)
+          DISTRIBUTED BY HASH(`game_id`) BUCKETS 10
+          PROPERTIES (
+          "replication_allocation" = "tag.location.default: 1"
+         );"""
+
+    sql """insert into dws_mf_wms_join_t1 values('2020-01-01','12345',100);"""
+    sql """insert into dws_mf_wms_join_t2 values('12345');"""
+
+    qt_select6 """SELECT
+                a1.ddate
+            FROM
+                (
+                    SELECT
+                        aaa.ddate
+                    FROM
+                        (
+                            SELECT
+                                aa.ddate,
+                                CONCAT('main', aa.main_currency_stock) AS arr,
+                                ROW_NUMBER() OVER (
+                                    PARTITION BY aa.ddate
+                                ) AS rn
+                            FROM
+                                (
+                                    SELECT
+                                        ddate,
+                                        main_currency_stock,
+                                        game_id
+                                    FROM
+                                        dws_mf_wms_join_t1 a
+                                ) aa
+                                LEFT JOIN (
+                                    SELECT
+                                        game_id
+                                    FROM
+                                        dws_mf_wms_join_t2
+                                ) b ON aa.game_id = b.game_id
+                        ) aaa
+                        CROSS JOIN (
+                            select
+                                1 as newarr
+                        ) b
+                    WHERE
+                        rn = 1
+                ) a1
+            GROUP BY
+                GROUPING SETS (
+                    (
+                        a1.ddate
+                    )
+                );"""
+
+    sql """
+        drop table if exists dws_mf_wms_join_join_t1;
+    """
+
+    sql """
+        drop table if exists dws_mf_wms_join_join_t2;
+    """
 }