afea97778b8353175c89bb03d1951c198986ee7a
TengJianPing <18241664+jacktengg@users.noreply.github.com>
2024-05-20 10:10:11 +0800

    [fix](agg) fix DCHECK failure of agg when failed to alloc memory (#35011)
    * [fix](agg) fix DCHECK failure of agg when failed to alloc memory
    
    * add comment

--- be/src/vec/common/string_buffer.hpp
+++ be/src/vec/common/string_buffer.hpp
@@ -42,9 +42,11 @@ public:
     }
 
     void commit() {
-        ColumnString::check_chars_length(_offsets.back() + _now_offset, 0);
-        _offsets.push_back(_offsets.back() + _now_offset);
+        auto now_offset = _now_offset;
         _now_offset = 0;
+        // the following code may throw exception, and DCHECK in destructor will fail if _now_offset is not reset to 0
+        ColumnString::check_chars_length(_offsets.back() + now_offset, 0);
+        _offsets.push_back(_offsets.back() + now_offset);
     }
 
     ~BufferWritable() { DCHECK(_now_offset == 0); }
