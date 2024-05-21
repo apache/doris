77b395bfad0b4759ce7d6f822e54ca1f0d41a1b4
gaoxin <AshinGau@outlook.com>
2024-05-21 16:31:25 +0800

    remove log

--- be/src/common/status.h
+++ be/src/common/status.h
@@ -412,7 +412,6 @@ public:
         if (stacktrace && ErrorCode::error_states[abs(code)].stacktrace) {
             // Delete the first one frame pointers, which are inside the status.h
             status._err_msg->_stack = get_stack_trace(1);
-            LOG(WARNING) << "meet error status: " << status; // may print too many stacks.
         }
 #endif
         return status;
@@ -431,7 +430,6 @@ public:
 #ifdef ENABLE_STACKTRACE
         if (stacktrace && ErrorCode::error_states[abs(code)].stacktrace) {
             status._err_msg->_stack = get_stack_trace(1);
-            LOG(WARNING) << "meet error status: " << status; // may print too many stacks.
         }
 #endif
         return status;
