a981ea43cd221c6f1c829b92c935aff7363180cc
wangbo <506340561@qq.com>
2024-05-21 18:53:16 +0800

    Fix workload policy test

--- regression-test/suites/workload_manager_p0/test_workload_sched_policy.groovy
+++ regression-test/suites/workload_manager_p0/test_workload_sched_policy.groovy
@@ -115,7 +115,6 @@ suite("test_workload_sched_policy") {
     qt_select_policy_tvf_after_drop "select name,condition,action,priority,enabled,version from information_schema.workload_policy where name in('be_policy','fe_policy','set_action_policy','test_cancel_policy') order by name;"
 
     // test workload policy
-    sql "ADMIN SET FRONTEND CONFIG ('workload_sched_policy_interval_ms' = '500');"
     sql """drop user if exists test_workload_sched_user"""
     sql """create user test_workload_sched_user identified by '12345'"""
     sql """grant ADMIN_PRIV on *.*.* to test_workload_sched_user"""
@@ -123,36 +122,42 @@ suite("test_workload_sched_policy") {
     // 1 create test_set_var_policy
     sql """drop workload policy if exists test_set_var_policy;"""
     sql """drop workload policy if exists test_set_var_policy2;"""
+    sql "drop workload group if exists test_set_session_wg;"
+    sql "drop workload group if exists test_set_session_wg2;"
+    sql "create workload group test_set_session_wg properties('cpu_share'='1024');"
+    sql "create workload group test_set_session_wg2 properties('cpu_share'='1024');"
+
     sql "create workload policy test_set_var_policy conditions(username='test_workload_sched_user')" +
-            "actions(set_session_variable 'parallel_pipeline_task_num=33');"
+            "actions(set_session_variable 'workload_group=test_set_session_wg');"
+
     def result1 = connect(user = 'test_workload_sched_user', password = '12345', url = context.config.jdbcUrl) {
         logger.info("begin sleep 15s to wait")
         Thread.sleep(15000)
-        sql "show variables like '%parallel_pipeline_task_num%';"
+        sql "show variables like 'workload_group';"
     }
-    assertEquals("parallel_pipeline_task_num", result1[0][0])
-    assertEquals("33", result1[0][1])
+    assertEquals("workload_group", result1[0][0])
+    assertEquals("test_set_session_wg", result1[0][1])
 
     // 2 create test_set_var_policy2 with higher priority
     sql "create workload policy test_set_var_policy2 conditions(username='test_workload_sched_user') " +
-            "actions(set_session_variable 'parallel_pipeline_task_num=22') properties('priority'='10');"
+            "actions(set_session_variable 'workload_group=test_set_session_wg2') properties('priority'='10');"
     def result2 = connect(user = 'test_workload_sched_user', password = '12345', url = context.config.jdbcUrl) {
         Thread.sleep(3000)
-        sql "show variables like '%parallel_pipeline_task_num%';"
+        sql "show variables like 'workload_group';"
     }
-    assertEquals("parallel_pipeline_task_num", result2[0][0])
-    assertEquals("22", result2[0][1])
+    assertEquals("workload_group", result2[0][0])
+    assertEquals("test_set_session_wg2", result2[0][1])
 
     // 3 disable test_set_var_policy2
     sql "alter workload policy test_set_var_policy2 properties('enabled'='false');"
     def result3 = connect(user = 'test_workload_sched_user', password = '12345', url = context.config.jdbcUrl) {
         Thread.sleep(3000)
-        sql "show variables like '%parallel_pipeline_task_num%';"
+        sql "show variables like 'workload_group';"
     }
-    assertEquals("parallel_pipeline_task_num", result3[0][0])
-    assertEquals("33", result3[0][1])
-    
-    sql "ADMIN SET FRONTEND CONFIG ('workload_sched_policy_interval_ms' = '10000');"
+    assertEquals("workload_group", result3[0][0])
+    assertEquals("test_set_session_wg", result3[0][1])
+    sql "drop workload group if exists test_set_session_wg;"
+    sql "drop workload group if exists test_set_session_wg2;"
 
     sql "drop workload policy if exists test_set_var_policy;"
     sql "drop workload policy if exists test_set_var_policy2;"
