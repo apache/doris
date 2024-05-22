e3a0f147befa0cb249dc83ca220283f85d112b40
cambyzhu <cambyzhu@tencent.com>
2024-05-22 16:48:19 +0800

    bugfix for add and drop index for history data

--- be/src/olap/schema_change.cpp
+++ be/src/olap/schema_change.cpp
@@ -731,6 +731,9 @@ SchemaChangeJob::SchemaChangeJob(StorageEngine& local_storage_engine,
     if (_base_tablet && _new_tablet) {
         _base_tablet_schema = std::make_shared<TabletSchema>();
         _base_tablet_schema->update_tablet_columns(*_base_tablet->tablet_schema(), request.columns);
+        // The request only include column info, do not include bitmap or bloomfilter index info,
+        // So we also need to copy index info from the real base tablet
+        _base_tablet_schema->update_index_info_from(*_base_tablet->tablet_schema());
         // During a schema change, the extracted columns of a variant should not be included in the tablet schema.
         // This is because the schema change for a variant needs to ignore the extracted columns.
         // Otherwise, the schema types in different rowsets might be inconsistent. When performing a schema change,
