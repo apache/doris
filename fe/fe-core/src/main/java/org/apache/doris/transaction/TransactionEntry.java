b699cf3ba0aaa934f752945517e6199a92e68cf6
meiyi <myimeiyi@gmail.com>
2024-05-22 09:38:10 +0800

    [fix](txn load) Fix txn insert connect to follower fe (#35075)

--- fe/fe-core/src/main/java/org/apache/doris/transaction/TransactionEntry.java
+++ fe/fe-core/src/main/java/org/apache/doris/transaction/TransactionEntry.java
@@ -17,21 +17,28 @@
 
 package org.apache.doris.transaction;
 
+import org.apache.doris.analysis.RedirectStatus;
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.DatabaseIf;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.Table;
 import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.DdlException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.proto.InternalService;
 import org.apache.doris.proto.Types;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.InsertStreamTxnExecutor;
+import org.apache.doris.qe.MasterOpExecutor;
 import org.apache.doris.qe.MasterTxnExecutor;
+import org.apache.doris.qe.OriginStatement;
 import org.apache.doris.service.FrontendOptions;
 import org.apache.doris.system.Backend;
+import org.apache.doris.thrift.TLoadTxnBeginRequest;
+import org.apache.doris.thrift.TLoadTxnBeginResult;
 import org.apache.doris.thrift.TTabletCommitInfo;
+import org.apache.doris.thrift.TTxnLoadInfo;
 import org.apache.doris.thrift.TTxnParams;
 import org.apache.doris.thrift.TWaitingTxnStatusRequest;
 import org.apache.doris.thrift.TWaitingTxnStatusResult;
@@ -44,12 +51,9 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.thrift.TException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 public class TransactionEntry {
@@ -58,6 +62,7 @@ public class TransactionEntry {
 
     private String label = "";
     private DatabaseIf database;
+    private long dbId = -1;
 
     // for insert into values for one table
     private Table table;
@@ -71,7 +76,6 @@ public class TransactionEntry {
     private boolean isTransactionBegan = false;
     private long transactionId = -1;
     private TransactionState transactionState;
-    private List<SubTransactionState> subTransactionStates = new ArrayList<>();
     private long timeoutTimestamp = -1;
 
     public TransactionEntry() {
@@ -80,6 +84,7 @@ public class TransactionEntry {
     public TransactionEntry(TTxnParams txnConf, Database db, Table table) {
         this.txnConf = txnConf;
         this.database = db;
+        this.dbId = this.database.getId();
         this.table = table;
     }
 
@@ -97,6 +102,7 @@ public class TransactionEntry {
 
     public void setDb(Database db) {
         this.database = db;
+        this.dbId = this.database.getId();
     }
 
     public Table getTable() {
@@ -164,7 +170,7 @@ public class TransactionEntry {
     }
 
     // Used for insert into select, return the sub_txn_id for this insert
-    public long beginTransaction(TableIf table) throws UserException {
+    public long beginTransaction(TableIf table) throws Exception {
         if (isInsertValuesTxnBegan()) {
             // FIXME: support mix usage of `insert into values` and `insert into select`
             throw new AnalysisException(
@@ -174,14 +180,26 @@ public class TransactionEntry {
         if (!isTransactionBegan) {
             long timeoutSecond = ConnectContext.get().getExecTimeout();
             this.timeoutTimestamp = System.currentTimeMillis() + timeoutSecond * 1000;
-            this.transactionId = Env.getCurrentGlobalTransactionMgr().beginTransaction(
-                    database.getId(), Lists.newArrayList(table.getId()), label,
-                    new TxnCoordinator(TxnSourceType.FE, FrontendOptions.getLocalHostAddress()),
-                    LoadJobSourceType.INSERT_STREAMING, timeoutSecond);
+            if (Env.getCurrentEnv().isMaster()) {
+                this.transactionId = Env.getCurrentGlobalTransactionMgr().beginTransaction(
+                        database.getId(), Lists.newArrayList(table.getId()), label,
+                        new TxnCoordinator(TxnSourceType.FE, FrontendOptions.getLocalHostAddress()),
+                        LoadJobSourceType.INSERT_STREAMING, timeoutSecond);
+            } else {
+                String token = Env.getCurrentEnv().getLoadManager().getTokenManager().acquireToken();
+                MasterTxnExecutor masterTxnExecutor = new MasterTxnExecutor(ConnectContext.get());
+                TLoadTxnBeginRequest request = new TLoadTxnBeginRequest();
+                request.setDb(database.getFullName()).setTbl(table.getName()).setToken(token)
+                        .setLabel(label).setUser("").setUserIp("").setPasswd("").setTimeout(timeoutSecond);
+                TLoadTxnBeginResult result = masterTxnExecutor.beginTxn(request);
+                this.transactionId = result.getTxnId();
+            }
             this.isTransactionBegan = true;
             this.database = database;
+            this.dbId = this.database.getId();
             this.transactionState = Env.getCurrentGlobalTransactionMgr()
                     .getTransactionState(database.getId(), transactionId);
+            this.transactionState.resetSubTransactionStates();
             return this.transactionId;
         } else {
             if (this.database.getId() != database.getId()) {
@@ -198,19 +216,27 @@ public class TransactionEntry {
     public TransactionStatus commitTransaction() throws Exception {
         if (isTransactionBegan) {
             try {
-                beforeFinishTransaction();
-                if (Env.getCurrentGlobalTransactionMgr()
-                        .commitAndPublishTransaction(database, transactionId, subTransactionStates,
-                                ConnectContext.get().getSessionVariable().getInsertVisibleTimeoutMs())) {
-                    return TransactionStatus.VISIBLE;
+                if (Env.getCurrentEnv().isMaster()) {
+                    beforeFinishTransaction();
+                    if (Env.getCurrentGlobalTransactionMgr().commitAndPublishTransaction(database, transactionId,
+                            transactionState.getSubTransactionStates(),
+                            ConnectContext.get().getSessionVariable().getInsertVisibleTimeoutMs())) {
+                        return TransactionStatus.VISIBLE;
+                    } else {
+                        return TransactionStatus.COMMITTED;
+                    }
                 } else {
-                    return TransactionStatus.COMMITTED;
+                    OriginStatement originStmt = new OriginStatement("commit", 0);
+                    MasterOpExecutor masterOpExecutor = new MasterOpExecutor(originStmt, ConnectContext.get(),
+                            RedirectStatus.NO_FORWARD, false);
+                    LOG.info("forward commit to Master for txn_id={}", this.transactionId);
+                    masterOpExecutor.execute();
+                    return waitingTxnVisible(this.dbId, this.transactionId);
                 }
             } catch (UserException e) {
                 LOG.error("Failed to commit transaction", e);
                 try {
-                    Env.getCurrentGlobalTransactionMgr()
-                            .abortTransaction(database.getId(), transactionId, e.getMessage());
+                    abortTransaction(e.getMessage());
                 } catch (Exception e1) {
                     LOG.error("Failed to abort transaction", e1);
                 }
@@ -224,26 +250,8 @@ public class TransactionEntry {
             }
             // commit txn
             executor.commitTransaction();
-
             // wait txn visible
-            TWaitingTxnStatusRequest request = new TWaitingTxnStatusRequest();
-            request.setDbId(txnConf.getDbId()).setTxnId(txnConf.getTxnId());
-            request.setLabelIsSet(false);
-            request.setTxnIdIsSet(true);
-
-            TWaitingTxnStatusResult statusResult = getWaitingTxnStatus(request);
-            TransactionStatus txnStatus = TransactionStatus.valueOf(statusResult.getTxnStatusId());
-            if (txnStatus == TransactionStatus.COMMITTED) {
-                throw new AnalysisException("transaction commit successfully, BUT data will be visible later.");
-            } else if (txnStatus != TransactionStatus.VISIBLE) {
-                String errMsg = "commit failed, rollback.";
-                if (statusResult.getStatus().isSetErrorMsgs()
-                        && statusResult.getStatus().getErrorMsgs().size() > 0) {
-                    errMsg = String.join(". ", statusResult.getStatus().getErrorMsgs());
-                }
-                throw new AnalysisException(errMsg);
-            }
-            return txnStatus;
+            return waitingTxnVisible(txnConf.getDbId(), txnConf.getTxnId());
         } else {
             LOG.info("No transaction to commit");
             return null;
@@ -253,8 +261,7 @@ public class TransactionEntry {
     private TWaitingTxnStatusResult getWaitingTxnStatus(TWaitingTxnStatusRequest request) throws Exception {
         TWaitingTxnStatusResult statusResult = null;
         if (Env.getCurrentEnv().isMaster()) {
-            statusResult = Env.getCurrentGlobalTransactionMgr()
-                    .getWaitingTxnStatus(request);
+            statusResult = Env.getCurrentGlobalTransactionMgr().getWaitingTxnStatus(request);
         } else {
             MasterTxnExecutor masterTxnExecutor = new MasterTxnExecutor(ConnectContext.get());
             statusResult = masterTxnExecutor.getWaitingTxnStatus(request);
@@ -262,12 +269,24 @@ public class TransactionEntry {
         return statusResult;
     }
 
-    public long abortTransaction()
-            throws UserException, TException, ExecutionException, InterruptedException, TimeoutException {
+    public long abortTransaction() throws Exception {
+        return abortTransaction("user rollback");
+    }
+
+    private long abortTransaction(String reason) throws Exception {
         if (isTransactionBegan) {
-            beforeFinishTransaction();
-            Env.getCurrentGlobalTransactionMgr().abortTransaction(database.getId(), transactionId, "user rollback");
-            return transactionId;
+            if (Env.getCurrentEnv().isMaster()) {
+                beforeFinishTransaction();
+                Env.getCurrentGlobalTransactionMgr().abortTransaction(database.getId(), transactionId, reason);
+                return transactionId;
+            } else {
+                OriginStatement originStmt = new OriginStatement("rollback", 0);
+                MasterOpExecutor masterOpExecutor = new MasterOpExecutor(originStmt, ConnectContext.get(),
+                        RedirectStatus.NO_FORWARD, false);
+                LOG.info("forward rollback to Master for txn_id={}", transactionId);
+                masterOpExecutor.execute();
+                return transactionId;
+            }
         } else if (isInsertValuesTxnBegan()) {
             InsertStreamTxnExecutor executor = new InsertStreamTxnExecutor(this);
             executor.abortTransaction();
@@ -282,7 +301,7 @@ public class TransactionEntry {
         if (isTransactionBegan) {
             List<Long> tableIds = transactionState.getTableIdList().stream().distinct().collect(Collectors.toList());
             transactionState.setTableIdList(tableIds);
-            subTransactionStates.sort((s1, s2) -> {
+            transactionState.getSubTransactionStates().sort((s1, s2) -> {
                 if (s1.getSubTransactionType() == SubTransactionType.INSERT
                         && s2.getSubTransactionType() == SubTransactionType.DELETE) {
                     return 1;
@@ -293,8 +312,8 @@ public class TransactionEntry {
                     return Long.compare(s1.getSubTransactionId(), s2.getSubTransactionId());
                 }
             });
-            LOG.info("subTransactionStates={}", subTransactionStates);
-            transactionState.setSubTransactionStates(subTransactionStates);
+            LOG.info("subTransactionStates={}", transactionState.getSubTransactionStates());
+            transactionState.resetSubTxnIds();
         }
     }
 
@@ -321,12 +340,13 @@ public class TransactionEntry {
             LOG.debug("label={}, txn_id={}, sub_txn_id={}, table={}, commit_infos={}",
                     label, transactionId, subTxnId, table, commitInfos);
         }
-        this.subTransactionStates.add(new SubTransactionState(subTxnId, table, commitInfos, subTransactionType));
-        Preconditions.checkState(transactionState.getTableIdList().size() == subTransactionStates.size(),
-                "txn_id={}, expect table_list={}, but is={}",
-                transactionId,
-                subTransactionStates.stream().map(s -> s.getTable().getId()).collect(Collectors.toList()),
-                transactionState.getTableIdList());
+        transactionState.getSubTransactionStates()
+                .add(new SubTransactionState(subTxnId, table, commitInfos, subTransactionType));
+        Preconditions.checkState(
+                transactionState.getTableIdList().size() == transactionState.getSubTransactionStates().size(),
+                "txn_id=" + transactionId + ", expect table_list=" + transactionState.getSubTransactionStates().stream()
+                        .map(s -> s.getTable().getId()).collect(Collectors.toList()) + ", real table_list="
+                        + transactionState.getTableIdList());
     }
 
     public boolean isTransactionBegan() {
@@ -335,5 +355,91 @@ public class TransactionEntry {
 
     public long getTimeout() {
         return (timeoutTimestamp - System.currentTimeMillis()) / 1000;
+    }
+
+    private TransactionStatus waitingTxnVisible(long dbId, long transactionId) throws Exception {
+        // wait txn visible
+        TWaitingTxnStatusRequest request = new TWaitingTxnStatusRequest();
+        request.setDbId(dbId).setTxnId(transactionId);
+        request.setLabelIsSet(false);
+        request.setTxnIdIsSet(true);
+
+        TWaitingTxnStatusResult statusResult = getWaitingTxnStatus(request);
+        TransactionStatus txnStatus = TransactionStatus.valueOf(statusResult.getTxnStatusId());
+        if (txnStatus == TransactionStatus.COMMITTED) {
+            throw new AnalysisException("transaction commit successfully, BUT data will be visible later.");
+        } else if (txnStatus != TransactionStatus.VISIBLE) {
+            String errMsg = "commit failed, rollback.";
+            if (statusResult.getStatus().isSetErrorMsgs()
+                    && statusResult.getStatus().getErrorMsgs().size() > 0) {
+                errMsg = String.join(". ", statusResult.getStatus().getErrorMsgs());
+            }
+            throw new AnalysisException(errMsg);
+        }
+        return txnStatus;
+    }
+
+    /**
+     * handle 2 forward cases:
+     * 1. the first dml stmt in a txn, now the txn is not began: only know the [label]
+     *    beginTxn -> txnId
+     *    calculate timeoutTimestamp
+     *    set dbId
+     * 2. the others dml stmts in a txn, now the txn is already began: know the [label, txnId, dbId, timeoutTimestamp]
+     */
+    public void setTxnInfoInMaster(TTxnLoadInfo txnLoadInfo) throws DdlException {
+        this.setTxnConf(new TTxnParams().setNeedTxn(true).setTxnId(-1));
+        this.label = txnLoadInfo.getLabel();
+        if (txnLoadInfo.isSetTxnId()) {
+            this.dbId = txnLoadInfo.getDbId();
+            this.database = Env.getCurrentInternalCatalog().getDbOrDdlException(dbId);
+            this.transactionId = txnLoadInfo.getTxnId();
+            this.transactionState = Env.getCurrentGlobalTransactionMgr().getTransactionState(dbId, transactionId);
+            Preconditions.checkNotNull(this.transactionState,
+                    "db_id" + dbId + " txn_id=" + transactionId + " not found");
+            Preconditions.checkState(this.label.equals(this.transactionState.getLabel()), "expected label="
+                    + this.label + ", real label=" + this.transactionState.getLabel());
+            this.isTransactionBegan = true;
+            this.timeoutTimestamp = txnLoadInfo.getTimeoutTimestamp();
+        }
+        LOG.info("set txn info in master, label={}, txnId={}, dbId={}, timeoutTimestamp={}",
+                label, transactionId, dbId, timeoutTimestamp);
+    }
+
+    public TTxnLoadInfo getTxnInfoInMaster() {
+        TTxnLoadInfo txnLoadInfo = new TTxnLoadInfo();
+        txnLoadInfo.setLabel(label);
+        txnLoadInfo.setTxnId(transactionId);
+        txnLoadInfo.setDbId(dbId);
+        txnLoadInfo.setTimeoutTimestamp(timeoutTimestamp);
+        LOG.info("master return txn info: {}", txnLoadInfo);
+        return txnLoadInfo;
+    }
+
+    public TTxnLoadInfo getTxnLoadInfoInObserver() throws AnalysisException {
+        if (isInsertValuesTxnBegan()) {
+            throw new AnalysisException(
+                    "Transaction insert can not insert into values and insert into select at the same time");
+        }
+        TTxnLoadInfo txnLoadInfo = new TTxnLoadInfo();
+        txnLoadInfo.setLabel(label);
+        if (this.isTransactionBegan) {
+            txnLoadInfo.setTxnId(transactionId);
+            txnLoadInfo.setDbId(dbId);
+            txnLoadInfo.setTimeoutTimestamp(timeoutTimestamp);
+        }
+        LOG.info("get txn load info in observer: {}", txnLoadInfo);
+        return txnLoadInfo;
+    }
+
+    public void setTxnLoadInfoInObserver(TTxnLoadInfo txnLoadInfo) {
+        Preconditions.checkState(txnLoadInfo.getLabel().equals(this.label),
+                "expected label=" + this.label + ", real label=" + txnLoadInfo.getLabel());
+        this.isTransactionBegan = true;
+        this.transactionId = txnLoadInfo.txnId;
+        this.timeoutTimestamp = txnLoadInfo.timeoutTimestamp;
+        this.dbId = txnLoadInfo.dbId;
+        LOG.info("set txn load info in observer, label={}, txnId={}, dbId={}, timeoutTimestamp={}",
+                label, transactionId, dbId, timeoutTimestamp);
     }
 }
