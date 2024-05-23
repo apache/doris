87ad2cf463f209c973779661f170ded599df48ce
suxiaogang223 <suxiaogang223@icloud.com>
2024-05-23 20:28:47 +0800

    check fileds' size and paimon fields' size in jni reader

--- fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
+++ fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
@@ -107,6 +107,10 @@ public class PaimonJniScanner extends JniScanner {
 
     private void initReader() throws IOException {
         ReadBuilder readBuilder = table.newReadBuilder();
+        if (this.fields.length != this.paimonAllFieldNames.size()) {
+            throw new IOException(String.format("The jni reader fields' size {%s} is not matched with paimon fields' size {%s}",
+                    fields.length, paimonAllFieldNames.size()));
+        }
         readBuilder.withProjection(getProjected());
         readBuilder.withFilter(getPredicates());
         reader = readBuilder.newRead().createReader(getSplit());
