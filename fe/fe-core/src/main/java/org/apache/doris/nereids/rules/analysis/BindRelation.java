c3bb415703c63e49ab1e8f3522aa359df393667f
Butao Zhang <zhangbutao@cmss.chinamobile.com>
2024-05-23 12:28:01 +0800

    [feat](nereids) support Iceberg time travel syntax (#34681)
    https://github.com/apache/doris/pull/15418
    added Iceberg time travel in legacy parser but not added this syntax Neredis.
    If we enable nereids and disable fallback to original palnner, time travel won't be available.
    
    This PR added time travel syntas in Neredis.
    
    BTW, we already have nereids time travel regression-test in
    
    https://github.com/apache/doris/blob/master/regression-test/suites/external_table_p2/iceberg/test_external_catalog_icebergv2_nereids.groovy
    
    this regression-test will always fail without this PR.
    
    https://github.com/apache/doris/blob/846694c6d697b4ccce8cf041fefbc66a80ab198e/regression-test/suites/external_table_p2/iceberg/test_external_catalog_icebergv2_nereids.groovy#L70-L75

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindRelation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindRelation.java
@@ -274,13 +274,15 @@ public class BindRelation extends OneAnalysisRuleFactory {
                     }
                     hmsTable.setScanParams(unboundRelation.getScanParams());
                     return new LogicalFileScan(unboundRelation.getRelationId(), (HMSExternalTable) table,
-                            qualifierWithoutTableName, unboundRelation.getTableSample());
+                            qualifierWithoutTableName, unboundRelation.getTableSample(),
+                            unboundRelation.getTableSnapshot());
                 case ICEBERG_EXTERNAL_TABLE:
                 case PAIMON_EXTERNAL_TABLE:
                 case MAX_COMPUTE_EXTERNAL_TABLE:
                 case TRINO_CONNECTOR_EXTERNAL_TABLE:
                     return new LogicalFileScan(unboundRelation.getRelationId(), (ExternalTable) table,
-                            qualifierWithoutTableName, unboundRelation.getTableSample());
+                            qualifierWithoutTableName, unboundRelation.getTableSample(),
+                            unboundRelation.getTableSnapshot());
                 case SCHEMA:
                     return new LogicalSchemaScan(unboundRelation.getRelationId(), table, qualifierWithoutTableName);
                 case JDBC_EXTERNAL_TABLE:
