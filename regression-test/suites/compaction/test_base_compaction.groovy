856647c5ccf6e996108dba8a768d85bde160544d
Yulei-Yang <yulei.yang0699@gmail.com>
2024-05-23 20:21:19 +0800

    update

--- regression-test/suites/compaction/test_base_compaction.groovy
+++ regression-test/suites/compaction/test_base_compaction.groovy
@@ -275,6 +275,5 @@ suite("test_base_compaction") {
     (code, out, err) = be_get_overall_compaction_status(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id))
     logger.info("Get overall compaction status: code=" + code + ", out=" + out + ", err=" + err)
     assertEquals(code, 0)
-    def compactJson = parseJson(out.trim())
-    assertTrue(compactJson.msg.toLowerCase().contains("basecompaction"))
+    assertTrue(out.toLowerCase().contains("basecompaction"))
 }
--- regression-test/suites/fault_injection_p0/test_full_compaciton_run_status.groovy
+++ regression-test/suites/fault_injection_p0/test_full_compaciton_run_status.groovy
@@ -94,8 +94,7 @@ suite("test_full_compaction_run_status","nonConcurrent") {
         (code, out, err) = be_get_overall_compaction_status(backendId_to_backendIP.get(backend_id), backendId_to_backendHttpPort.get(backend_id))
         logger.info("Get overall compaction status: code=" + code + ", out=" + out + ", err=" + err)
         assertEquals(code, 0)
-        def compactJson = parseJson(out.trim())
-        assertTrue(compactJson.msg.toLowerCase().contains("cumulativecompaction"))
+        assertTrue(out.toLowerCase().contains("cumulativecompaction"))
     } catch (Exception e) {
         logger.info(e.getMessage())
         exception = true;
