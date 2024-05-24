90371a44dbd3baf7d1e430e72b9559ba201528bf
苏小刚 <suxiaogang223@icloud.com>
2024-05-24 14:43:33 +0800

    Update fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
    Co-authored-by: Mingyu Chen <morningman.cmy@gmail.com>

--- fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
+++ fe/be-java-extensions/paimon-scanner/src/main/java/org/apache/doris/paimon/PaimonJniScanner.java
@@ -109,7 +109,7 @@ public class PaimonJniScanner extends JniScanner {
         ReadBuilder readBuilder = table.newReadBuilder();
         if (this.fields.length != this.paimonAllFieldNames.size()) {
             throw new IOException(
-                    String.format("The jni reader fields' size {%s} is not matched with paimon fields' size {%s}",
+                    String.format("The jni reader fields' size {%s} is not matched with paimon fields' size {%s}. Please refresh table and try again",
                             fields.length, paimonAllFieldNames.size()));
         }
         readBuilder.withProjection(getProjected());
