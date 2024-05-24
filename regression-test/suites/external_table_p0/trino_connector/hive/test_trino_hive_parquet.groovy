9f27de1439ff0968cf03467da8db0452840b004a
BePPPower <ftw2139@163.com>
2024-05-24 19:34:34 +0800

    fix

--- regression-test/suites/external_table_p0/trino_connector/hive/test_trino_hive_parquet.groovy
+++ regression-test/suites/external_table_p0/trino_connector/hive/test_trino_hive_parquet.groovy
@@ -191,8 +191,8 @@ suite("test_trino_hive_parquet", "p0,external,hive,external_docker,external_dock
             sql """
                 create catalog if not exists ${catalog_name} properties (
                     "type"="trino-connector",
-                    "connector.name"="hive",
-                    'hive.metastore.uri' = 'thrift://${externalEnvIp}:${hms_port}'
+                    "trino.connector.name"="hive",
+                    'trino.hive.metastore.uri' = 'thrift://${externalEnvIp}:${hms_port}'
                 );
             """
 
