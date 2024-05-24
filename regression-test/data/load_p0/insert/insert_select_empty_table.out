apache/doris

commit 64e3670be84e2cb97f778c4b04c08b77156ea39f

[Feat](nereids) when dealing insert into stmt with empty table source, fe returns directly (#34418)
When a LogicalOlapScan has no partitions, transform it to a LogicalEmptyRelation.
When dealing insert into stmt with empty table source, fe returns directly.

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertIntoTableCommand.java
@@ -30,12 +30,14 @@ import org.apache.doris.nereids.NereidsPlanner;
 import org.apache.doris.nereids.analyzer.UnboundTableSink;
 import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.glue.LogicalPlanAdapter;
+import org.apache.doris.nereids.trees.TreeNode;
 import org.apache.doris.nereids.trees.plans.Explainable;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.PlanType;
 import org.apache.doris.nereids.trees.plans.commands.Command;
 import org.apache.doris.nereids.trees.plans.commands.ForwardWithSync;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalEmptyRelation;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalHiveTableSink;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalOlapTableSink;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalSink;
@@ -162,10 +164,13 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
                     // return;
                     throw new AnalysisException("group commit is not supported in Nereids now");
                 }
+                boolean emptyInsert = leafIsEmptyRelation(physicalSink);
                 OlapTable olapTable = (OlapTable) targetTableIf;
                 // the insertCtx contains some variables to adjust SinkNode
-                insertExecutor = ctx.isTxnModel() ? new OlapTxnInsertExecutor(ctx, olapTable, label, planner, insertCtx)
-                        : new OlapInsertExecutor(ctx, olapTable, label, planner, insertCtx);
+                insertExecutor = ctx.isTxnModel()
+                        ? new OlapTxnInsertExecutor(ctx, olapTable, label, planner, insertCtx, emptyInsert)
+                        : new OlapInsertExecutor(ctx, olapTable, label, planner, insertCtx, emptyInsert);
+
                 boolean isEnableMemtableOnSinkNode =
                         olapTable.getTableProperty().getUseSchemaLightChange()
                                 ? insertExecutor.getCoordinator().getQueryOptions().isEnableMemtableOnSinkNode()
@@ -173,9 +178,10 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
                 insertExecutor.getCoordinator().getQueryOptions()
                         .setEnableMemtableOnSinkNode(isEnableMemtableOnSinkNode);
             } else if (physicalSink instanceof PhysicalHiveTableSink) {
+                boolean emptyInsert = leafIsEmptyRelation(physicalSink);
                 HMSExternalTable hiveExternalTable = (HMSExternalTable) targetTableIf;
                 insertExecutor = new HiveInsertExecutor(ctx, hiveExternalTable, label, planner,
-                        Optional.of(insertCtx.orElse((new HiveInsertCommandContext()))));
+                        Optional.of(insertCtx.orElse((new HiveInsertCommandContext()))), emptyInsert);
                 // set hive query options
             } else {
                 // TODO: support other table types
@@ -203,6 +209,10 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
 
     private void runInternal(ConnectContext ctx, StmtExecutor executor) throws Exception {
         AbstractInsertExecutor insertExecutor = initPlan(ctx, executor);
+        // if the insert stmt data source is empty, directly return, no need to be executed.
+        if (insertExecutor.isEmptyInsert()) {
+            return;
+        }
         insertExecutor.executeSingleInsert(executor, jobId);
     }
 
@@ -218,5 +228,17 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
     @Override
     public <R, C> R accept(PlanVisitor<R, C> visitor, C context) {
         return visitor.visitInsertIntoTableCommand(this, context);
+    }
+
+    private boolean leafIsEmptyRelation(TreeNode<Plan> node) {
+        if (node.children() == null || node.children().isEmpty()) {
+            return node instanceof PhysicalEmptyRelation;
+        }
+        for (TreeNode<Plan> child : node.children()) {
+            if (!leafIsEmptyRelation(child)) {
+                return false;
+            }
+        }
+        return true;
     }
 }

regression-test/data/load_p0/insert/insert_select_empty_table.out
@@ -1,0 +1,20 @@
+-- This file is automatically generated. You should know what you did if you want to edit this
+-- !test_shape --
+PhysicalOlapTableSink
+--PhysicalEmptyRelation
+
+-- !test_shape_union --
+PhysicalOlapTableSink
+--PhysicalEmptyRelation
+
+-- !test_shape_join --
+PhysicalOlapTableSink
+--PhysicalProject
+----PhysicalEmptyRelation
+
+-- !test_shape_agg_filter_limit --
+PhysicalOlapTableSink
+--PhysicalLimit[GLOBAL]
+----PhysicalLimit[LOCAL]
+------PhysicalEmptyRelation
+

regression-test/suites/load_p0/insert/insert_select_empty_table.groovy
@@ -1,0 +1,56 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("insert_select_empty_table") {
+    sql "SET enable_nereids_planner=true"
+    sql "SET enable_fallback_to_original_planner=false"
+    sql """
+         DROP TABLE IF EXISTS insert_select_empty_table1
+        """
+
+    sql """
+        create table insert_select_empty_table1(pk int, a int, b int) distributed by hash(pk) buckets 10
+        properties('replication_num' = '1'); 
+        """
+    sql """
+         DROP TABLE IF EXISTS insert_select_empty_table2
+        """
+
+    sql """
+        create table insert_select_empty_table2(pk int, a int, b int) distributed by hash(pk) buckets 10
+        properties('replication_num' = '1'); 
+        """
+    sql "insert into insert_select_empty_table1 select * from insert_select_empty_table2;"
+    qt_test_shape "explain shape plan insert into insert_select_empty_table1 select * from insert_select_empty_table2;"
+
+    sql """insert into insert_select_empty_table1 select * from insert_select_empty_table2 
+        union all select * from insert_select_empty_table2"""
+    qt_test_shape_union """explain shape plan insert into insert_select_empty_table1 select * from insert_select_empty_table2 
+        union all select * from insert_select_empty_table2 """
+
+    sql """insert into insert_select_empty_table1 select t1.* from insert_select_empty_table2 t1
+        inner join insert_select_empty_table2 t2 on t1.a=t2.a"""
+    qt_test_shape_join """explain shape plan insert into insert_select_empty_table1 select t1.* from insert_select_empty_table2 t1
+        inner join insert_select_empty_table2 t2 on t1.a=t2.a"""
+
+    sql """
+    insert into insert_select_empty_table1 select pk,a,b from insert_select_empty_table2 where a > 10 group by pk,a,b  limit 10;
+    """
+    qt_test_shape_agg_filter_limit """ explain shape plan
+    insert into insert_select_empty_table1 select pk,a,b from insert_select_empty_table2 where a > 10 group by pk,a,b  limit 10;
+    """
+}

commit 5ffa069c039e151a4ef25adc1e6d3cefda53c4c3

[Fix](nereids) fix when insert into select empty table (#35291)
This bug introduced by #34418.
Some operators can produce data even if the input is an empty table, such as AGGREGATE. Therefore, it is necessary to check whether sink's child is in an empty relation. Only when the child is in an empty relation can it return directly without performing actual data insertion.

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertIntoTableCommand.java
@@ -31,7 +31,6 @@ import org.apache.doris.nereids.NereidsPlanner;
 import org.apache.doris.nereids.analyzer.UnboundTableSink;
 import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.glue.LogicalPlanAdapter;
-import org.apache.doris.nereids.trees.TreeNode;
 import org.apache.doris.nereids.trees.plans.Explainable;
 import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.PlanType;
@@ -167,7 +166,7 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
                     // return;
                     throw new AnalysisException("group commit is not supported in Nereids now");
                 }
-                boolean emptyInsert = leafIsEmptyRelation(physicalSink);
+                boolean emptyInsert = childIsEmptyRelation(physicalSink);
                 OlapTable olapTable = (OlapTable) targetTableIf;
                 // the insertCtx contains some variables to adjust SinkNode
                 insertExecutor = ctx.isTxnModel()
@@ -181,13 +180,13 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
                 insertExecutor.getCoordinator().getQueryOptions()
                         .setEnableMemtableOnSinkNode(isEnableMemtableOnSinkNode);
             } else if (physicalSink instanceof PhysicalHiveTableSink) {
-                boolean emptyInsert = leafIsEmptyRelation(physicalSink);
+                boolean emptyInsert = childIsEmptyRelation(physicalSink);
                 HMSExternalTable hiveExternalTable = (HMSExternalTable) targetTableIf;
                 insertExecutor = new HiveInsertExecutor(ctx, hiveExternalTable, label, planner,
                         Optional.of(insertCtx.orElse((new HiveInsertCommandContext()))), emptyInsert);
                 // set hive query options
             } else if (physicalSink instanceof PhysicalIcebergTableSink) {
-                boolean emptyInsert = leafIsEmptyRelation(physicalSink);
+                boolean emptyInsert = childIsEmptyRelation(physicalSink);
                 IcebergExternalTable icebergExternalTable = (IcebergExternalTable) targetTableIf;
                 insertExecutor = new IcebergInsertExecutor(ctx, icebergExternalTable, label, planner,
                         Optional.of(insertCtx.orElse((new BaseExternalTableInsertCommandContext()))), emptyInsert);
@@ -238,15 +237,11 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
         return visitor.visitInsertIntoTableCommand(this, context);
     }
 
-    private boolean leafIsEmptyRelation(TreeNode<Plan> node) {
-        if (node.children() == null || node.children().isEmpty()) {
-            return node instanceof PhysicalEmptyRelation;
+    private boolean childIsEmptyRelation(PhysicalSink sink) {
+        if (sink.children() != null && sink.children().size() == 1
+                && sink.child(0) instanceof PhysicalEmptyRelation) {
+            return true;
         }
-        for (TreeNode<Plan> child : node.children()) {
-            if (!leafIsEmptyRelation(child)) {
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 }

regression-test/data/load_p0/insert/insert_select_empty_table.out
@@ -15,3 +15,9 @@ PhysicalOlapTableSink
 PhysicalOlapTableSink
 --PhysicalEmptyRelation
 
+-- !test_insert_empty_agg --
+1
+
+-- !test_insert_empty_agg_select --
+\N	0	\N
+

regression-test/suites/load_p0/insert/insert_select_empty_table.groovy
@@ -50,4 +50,8 @@ suite("insert_select_empty_table") {
     qt_test_shape_agg_filter_limit """ explain shape plan
     insert into insert_select_empty_table1 select pk,a,b from insert_select_empty_table2 where a > 10 group by pk,a,b  limit 10;
     """
+
+    qt_test_insert_empty_agg "insert into insert_select_empty_table1(a) select count(*) from insert_select_empty_table1"
+    qt_test_insert_empty_agg_select "select * from insert_select_empty_table1"
+
 }