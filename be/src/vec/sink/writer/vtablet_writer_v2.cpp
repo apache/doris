25141535dee5dc10779b92c67a519a2d87d321a7
Kaijie Chen <ckj@apache.org>
2024-05-25 19:34:28 +0800

    use constexpr

--- be/src/vec/sink/writer/vtablet_writer_v2.cpp
+++ be/src/vec/sink/writer/vtablet_writer_v2.cpp
@@ -311,7 +311,8 @@ Status VTabletWriterV2::_build_tablet_node_mapping() {
                     tablet.set_index_id(index.index_id);
                     tablet.set_tablet_id(tablet_id);
                     _tablets_for_node[node].emplace(tablet_id, tablet);
-                    if (tablet_id == 0) [[unlikely]] {
+                    constexpr int64_t DUMMY_TABLET_ID = 0;
+                    if (tablet_id == DUMMY_TABLET_ID) [[unlikely]] {
                         // ignore fake tablet for auto partition
                         continue;
                     }
