3725af3a1a94e69835ae12b39ad747b939fd97df
Vallish <vallishpai@gmail.com>
2024-05-27 14:23:19 +0000

    rebase compile issue fix

--- be/src/exec/schema_scanner/schema_table_options_scanner.cpp
+++ be/src/exec/schema_scanner/schema_table_options_scanner.cpp
@@ -124,7 +124,7 @@ Status SchemaTableOptionsScanner::get_next_block(vectorized::Block* block, bool*
 
     int current_batch_rows = std::min(_block_rows_limit, _total_rows - _row_idx);
     vectorized::MutableBlock mblock = vectorized::MutableBlock::build_mutable_block(block);
-    mblock.add_rows(_tableoptions_block.get(), _row_idx, current_batch_rows);
+    RETURN_IF_ERROR(mblock.add_rows(_tableoptions_block.get(), _row_idx, current_batch_rows));
     _row_idx += current_batch_rows;
 
     *eos = _row_idx == _total_rows;
