apache/doris

commit 19acfd23bacceb25a95ffa062c038e3b126aa646

[feat](nereids) add merge aggregate rule (#31811)

fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/MergeAggregate.java
@@ -1,0 +1,211 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.rules.rewrite;
+
+import org.apache.doris.nereids.rules.Rule;
+import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.trees.expressions.Alias;
+import org.apache.doris.nereids.trees.expressions.ExprId;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.expressions.NamedExpression;
+import org.apache.doris.nereids.trees.expressions.SlotReference;
+import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateFunction;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalAggregate;
+import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
+import org.apache.doris.nereids.util.ExpressionUtils;
+import org.apache.doris.nereids.util.PlanUtils;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**MergeAggregate*/
+public class MergeAggregate implements RewriteRuleFactory {
+    private static final ImmutableSet<String> ALLOW_MERGE_AGGREGATE_FUNCTIONS =
+            ImmutableSet.of("min", "max", "sum", "any_value");
+    private Map<ExprId, AggregateFunction> innerAggExprIdToAggFunc = new HashMap<>();
+
+    @Override
+    public List<Rule> buildRules() {
+        return ImmutableList.of(
+                logicalAggregate(logicalAggregate()).when(this::canMergeAggregateWithoutProject)
+                        .then(this::mergeTwoAggregate)
+                        .toRule(RuleType.MERGE_AGGREGATE),
+                logicalAggregate(logicalProject(logicalAggregate()))
+                        .when(this::canMergeAggregateWithProject)
+                        .then(this::mergeAggProjectAgg)
+                        .toRule(RuleType.MERGE_AGGREGATE));
+    }
+
+    /**
+     * before:
+     * LogicalAggregate
+     *   +--LogicalAggregate
+     * after:
+     * LogicalAggregate
+     */
+    private Plan mergeTwoAggregate(LogicalAggregate<LogicalAggregate<Plan>> outerAgg) {
+        LogicalAggregate<Plan> innerAgg = outerAgg.child();
+
+        List<NamedExpression> newOutputExpressions = outerAgg.getOutputExpressions().stream()
+                .map(e -> rewriteAggregateFunction(e, innerAggExprIdToAggFunc))
+                .collect(Collectors.toList());
+        return outerAgg.withAggOutput(newOutputExpressions).withChildren(innerAgg.children());
+    }
+
+    /**
+     * before:
+     * LogicalAggregate (outputExpressions = [col2, sum(col1)], groupByKeys = [col2])
+     *   +--LogicalProject (projects = [a as col2, col1])
+     *     +--LogicalAggregate (outputExpressions = [a, b, sum(c) as col1], groupByKeys = [a,b])
+     * after:
+     * LogicalProject (projects = [a as col2, sum(col1) as sum(col1)]
+     *   +--LogicalAggregate (outputExpression = [a, sum(c) as sum(col1)], groupByKeys = [a])
+     */
+    private Plan mergeAggProjectAgg(LogicalAggregate<LogicalProject<LogicalAggregate<Plan>>> outerAgg) {
+        LogicalProject<LogicalAggregate<Plan>> project = outerAgg.child();
+        LogicalAggregate<Plan> innerAgg = project.child();
+
+        // rewrite agg function. e.g. max(max)
+        List<NamedExpression> aggFunc = outerAgg.getOutputExpressions().stream()
+                .filter(expr -> (expr instanceof Alias) && (expr.child(0) instanceof AggregateFunction))
+                .map(e -> rewriteAggregateFunction(e, innerAggExprIdToAggFunc))
+                .collect(Collectors.toList());
+        // rewrite agg function directly refer to the slot below the project
+        List<Expression> replacedAggFunc = PlanUtils.replaceExpressionByProjections(project.getProjects(),
+                (List) aggFunc);
+        // replace groupByKeys directly refer to the slot below the project
+        List<Expression> replacedGroupBy = PlanUtils.replaceExpressionByProjections(project.getProjects(),
+                outerAgg.getGroupByExpressions());
+        List<NamedExpression> newOutputExpressions = ImmutableList.<NamedExpression>builder()
+                .addAll(replacedGroupBy.stream().map(slot -> (NamedExpression) slot).iterator())
+                .addAll(replacedAggFunc.stream().map(alias -> (NamedExpression) alias).iterator()).build();
+        // construct agg
+        LogicalAggregate<Plan> resAgg = outerAgg.withGroupByAndOutput(replacedGroupBy, newOutputExpressions)
+                .withChildren(innerAgg.children());
+
+        // construct upper project
+        Map<SlotReference, Alias> childToAlias = project.getProjects().stream()
+                .filter(expr -> (expr instanceof Alias) && (expr.child(0) instanceof SlotReference))
+                .collect(Collectors.toMap(alias -> (SlotReference) alias.child(0), alias -> (Alias) alias));
+        List<Expression> projectGroupBy = ExpressionUtils.replace(replacedGroupBy, childToAlias);
+        List<NamedExpression> upperProjects = ImmutableList.<NamedExpression>builder()
+                .addAll(projectGroupBy.stream().map(namedExpr -> (NamedExpression) namedExpr).iterator())
+                .addAll(replacedAggFunc.stream().map(expr -> ((NamedExpression) expr).toSlot()).iterator())
+                .build();
+        return new LogicalProject<Plan>(upperProjects, resAgg);
+    }
+
+    private NamedExpression rewriteAggregateFunction(NamedExpression e,
+            Map<ExprId, AggregateFunction> innerAggExprIdToAggFunc) {
+        return (NamedExpression) e.rewriteDownShortCircuit(expr -> {
+            if (expr instanceof Alias && ((Alias) expr).child() instanceof AggregateFunction) {
+                Alias alias = (Alias) expr;
+                AggregateFunction aggFunc = (AggregateFunction) alias.child();
+                ExprId childExprId = ((SlotReference) aggFunc.child(0)).getExprId();
+                if (innerAggExprIdToAggFunc.containsKey(childExprId)) {
+                    return new Alias(alias.getExprId(), innerAggExprIdToAggFunc.get(childExprId),
+                            alias.getName());
+                } else {
+                    return expr;
+                }
+            } else {
+                return expr;
+            }
+        });
+    }
+
+    boolean commonCheck(LogicalAggregate<? extends Plan> outerAgg, LogicalAggregate<Plan> innerAgg,
+            boolean sameGroupBy) {
+        innerAggExprIdToAggFunc = innerAgg.getOutputExpressions().stream()
+                .filter(expr -> (expr instanceof Alias) && (expr.child(0) instanceof AggregateFunction))
+                .collect(Collectors.toMap(NamedExpression::getExprId, value -> (AggregateFunction) value.child(0),
+                        (existValue, newValue) -> existValue));
+        Set<AggregateFunction> aggregateFunctions = outerAgg.getAggregateFunctions();
+        for (AggregateFunction outerFunc : aggregateFunctions) {
+            if (!(ALLOW_MERGE_AGGREGATE_FUNCTIONS.contains(outerFunc.getName()))) {
+                return false;
+            }
+            if (outerFunc.isDistinct() && !sameGroupBy) {
+                return false;
+            }
+            // not support outerAggFunc: sum(a+1),sum(a+b)
+            if (!(outerFunc.child(0) instanceof SlotReference)) {
+                return false;
+            }
+            ExprId childExprId = ((SlotReference) outerFunc.child(0)).getExprId();
+            if (innerAggExprIdToAggFunc.containsKey(childExprId)) {
+                AggregateFunction innerFunc = innerAggExprIdToAggFunc.get(childExprId);
+                if (innerFunc.isDistinct() && !sameGroupBy) {
+                    return false;
+                }
+                // support sum(sum),min(min),max(max),any_value(any_value),sum(count)
+                // sum(count) -> count() need outerAgg having group by keys (reason: nullable)
+                if (!(outerFunc.getName().equals("sum") && innerFunc.getName().equals("count")
+                        && !outerAgg.getGroupByExpressions().isEmpty())
+                        && !innerFunc.getName().equals(outerFunc.getName())) {
+                    return false;
+                }
+            } else {
+                // select a, max(b), min(b), any_value(b) from (select a,b from t1 group by a, b) group by a;
+                // equals select a, max(b), min(b), any_value(b) from t1 group by a;
+                if (!outerFunc.getName().equals("max")
+                        && !outerFunc.getName().equals("min")
+                        && !outerFunc.getName().equals("any_value")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean canMergeAggregateWithoutProject(LogicalAggregate<LogicalAggregate<Plan>> outerAgg) {
+        LogicalAggregate<Plan> innerAgg = outerAgg.child();
+        if (!new HashSet<>(innerAgg.getGroupByExpressions()).containsAll(outerAgg.getGroupByExpressions())) {
+            return false;
+        }
+        boolean sameGroupBy = (innerAgg.getGroupByExpressions().size() == outerAgg.getGroupByExpressions().size());
+
+        return commonCheck(outerAgg, innerAgg, sameGroupBy);
+    }
+
+    private boolean canMergeAggregateWithProject(LogicalAggregate<LogicalProject<LogicalAggregate<Plan>>> outerAgg) {
+        LogicalProject<LogicalAggregate<Plan>> project = outerAgg.child();
+        LogicalAggregate<Plan> innerAgg = project.child();
+
+        List<Expression> outerAggGroupByKeys = PlanUtils.replaceExpressionByProjections(project.getProjects(),
+                outerAgg.getGroupByExpressions());
+        if (!new HashSet<>(innerAgg.getGroupByExpressions()).containsAll(outerAggGroupByKeys)) {
+            return false;
+        }
+        // project cannot have expressions like a+1
+        if (ExpressionUtils.anyMatch(project.getProjects(),
+                expr -> !(expr instanceof SlotReference) && !(expr instanceof Alias))) {
+            return false;
+        }
+        boolean sameGroupBy = (innerAgg.getGroupByExpressions().size() == outerAgg.getGroupByExpressions().size());
+        return commonCheck(outerAgg, innerAgg, sameGroupBy);
+    }
+}

regression-test/data/nereids_rules_p0/merge_aggregate/merge_aggregate.out
@@ -1,0 +1,248 @@
+-- This file is automatically generated. You should know what you did if you want to edit this
+-- !sumCount_empty_table --
+\N
+
+-- !maxMax_minMin_sumSum_sumCount --
+1	1	1	1
+2	2	2	1
+6	6	6	1
+7	2	20	5
+8	6	26	4
+8	8	8	1
+9	5	20	3
+
+-- !maxGroupKey_minGroupKey --
+\N	\N	6	6
+1	1	2	1
+2	2	3	3
+3	3	2	1
+4	4	2	2
+5	5	4	3
+7	7	6	6
+
+-- !agg_project_agg --
+\N	\N	\N	6	1
+1	1	1	20	5
+2	2	2	8	1
+3	3	3	20	3
+4	4	4	2	1
+5	5	5	26	4
+7	7	7	1	1
+
+-- !upper_plan_can_use_name --
+2
+3
+7
+8
+9
+9
+10
+
+-- !outer_agg_has_distinct_same_keys --
+1	1	1	1
+2	2	2	1
+4	2	6	2
+6	6	6	1
+6	6	6	1
+6	6	6	1
+7	3	14	3
+8	6	20	3
+8	8	8	1
+9	5	14	2
+
+-- !inner_agg_has_distinct_same_keys --
+1	1	1	1
+2	2	2	1
+4	2	6	2
+6	6	6	1
+6	6	6	1
+6	6	6	1
+7	3	14	3
+8	6	14	3
+8	8	8	1
+9	5	14	2
+
+-- !sumCount_empty_table_shape --
+PhysicalResultSink
+--hashAgg[GLOBAL]
+----PhysicalDistribute[DistributionSpecGather]
+------hashAgg[LOCAL]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------PhysicalOlapScan[mal_test2]
+
+-- !agg_project_agg_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------PhysicalOlapScan[mal_test1]
+
+-- !maxMax_minMin_sumSum_sumCount_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------PhysicalOlapScan[mal_test1]
+
+-- !maxGroupKey_minGroupKey_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------PhysicalOlapScan[mal_test1]
+
+-- !outer_agg_has_distinct_same_keys_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[LOCAL]
+------------PhysicalOlapScan[mal_test1]
+
+-- !inner_agg_has_distinct_same_keys_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[DISTINCT_LOCAL]
+------------hashAgg[GLOBAL]
+--------------hashAgg[LOCAL]
+----------------PhysicalOlapScan[mal_test1]
+
+-- !middle_project_has_expression_cannot_merge_shape1 --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[LOCAL]
+--------------------PhysicalProject
+----------------------PhysicalOlapScan[mal_test1]
+
+-- !middle_project_has_expression_cannot_merge_shape2 --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[LOCAL]
+--------------------PhysicalOlapScan[mal_test1]
+
+-- !maxGroupKey_minGroupKey_sumGroupKey_cannot_merge_shape --
+PhysicalResultSink
+--PhysicalDistribute[DistributionSpecGather]
+----PhysicalProject
+------hashAgg[GLOBAL]
+--------PhysicalDistribute[DistributionSpecHash]
+----------hashAgg[LOCAL]
+------------hashAgg[LOCAL]
+--------------PhysicalProject
+----------------PhysicalOlapScan[mal_test1]
+
+-- !maxMin_cannot_merge_shape --
+PhysicalResultSink
+--PhysicalDistribute[DistributionSpecGather]
+----PhysicalProject
+------hashAgg[GLOBAL]
+--------PhysicalDistribute[DistributionSpecHash]
+----------hashAgg[LOCAL]
+------------PhysicalProject
+--------------hashAgg[LOCAL]
+----------------PhysicalOlapScan[mal_test1]
+
+-- !group_key_not_contain_cannot_merge_shape --
+PhysicalResultSink
+--PhysicalDistribute[DistributionSpecGather]
+----PhysicalProject
+------hashAgg[GLOBAL]
+--------PhysicalDistribute[DistributionSpecHash]
+----------hashAgg[LOCAL]
+------------PhysicalProject
+--------------hashAgg[LOCAL]
+----------------PhysicalOlapScan[mal_test1]
+
+-- !outer_agg_has_distinct_cannot_merge_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[LOCAL]
+--------------------PhysicalOlapScan[mal_test1]
+
+-- !inner_agg_has_distinct_cannot_merge_shape --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[DISTINCT_LOCAL]
+--------------------hashAgg[GLOBAL]
+----------------------hashAgg[LOCAL]
+------------------------PhysicalOlapScan[mal_test1]
+
+-- !agg_with_expr_cannot_merge_shape1 --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[LOCAL]
+--------------------PhysicalProject
+----------------------PhysicalOlapScan[mal_test1]
+
+-- !agg_with_expr_cannot_merge_shape2 --
+PhysicalResultSink
+--PhysicalQuickSort[MERGE_SORT]
+----PhysicalDistribute[DistributionSpecGather]
+------PhysicalQuickSort[LOCAL_SORT]
+--------PhysicalProject
+----------hashAgg[GLOBAL]
+------------PhysicalDistribute[DistributionSpecHash]
+--------------hashAgg[LOCAL]
+----------------PhysicalProject
+------------------hashAgg[LOCAL]
+--------------------PhysicalProject
+----------------------PhysicalOlapScan[mal_test1]
+

commit c74a2c9879e4be67f74de8907b38c9896f235881

[Fix](nereids) fix merge aggregate setting top projection bug (#35348)
introduced by #31811

sql like this:

    select col1, col2 from  (select a as col1, a as col2 from mal_test1 group by a) t group by col1, col2 ;

Transformation Description:
In the process of optimizing the query, an agg-project-agg pattern is transformed into a project-agg pattern:
Before Transformation:

LogicalAggregate
+-- LogicalPrject
    +-- LogicalAggregate

After Transformation:

LogicalProject
+-- LogicalAggregate

Before the transformation, the projection in the LogicalProject was a AS col1, a AS col2, and the outer aggregate group by keys were col1, col2. After the transformation, the aggregate group by keys became a, a, and the projection remained a AS col1, a AS col2.

Problem:
When building the project projections, the group by key a, a needed to be transformed to a AS col1, a AS col2. The old code had a bug where it used the slot as the map key and the alias in the projections as the map value. This approach did not account for the situation where aliases might have the same slot.

Solution:
The new code fixes this issue by using the original outer aggregate group by expression's exprId. It searches within the original project projections to find the NamedExpression that has the same exprId. These expressions are then placed into the new projections. This method ensures that the correct aliases are maintained, resolving the bug.

fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/MergeAggregate.java
@@ -17,8 +17,10 @@
 
 package org.apache.doris.nereids.rules.rewrite;
 
+import org.apache.doris.nereids.annotation.DependsRules;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.rules.analysis.NormalizeAggregate;
 import org.apache.doris.nereids.trees.expressions.Alias;
 import org.apache.doris.nereids.trees.expressions.ExprId;
 import org.apache.doris.nereids.trees.expressions.Expression;
@@ -44,6 +46,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**MergeAggregate*/
+@DependsRules({
+        NormalizeAggregate.class
+})
 public class MergeAggregate implements RewriteRuleFactory {
     private static final ImmutableSet<String> ALLOW_MERGE_AGGREGATE_FUNCTIONS =
             ImmutableSet.of("min", "max", "sum", "any_value");
@@ -108,10 +113,17 @@ public class MergeAggregate implements RewriteRuleFactory {
                 .withChildren(innerAgg.children());
 
         // construct upper project
-        Map<SlotReference, Alias> childToAlias = project.getProjects().stream()
-                .filter(expr -> (expr instanceof Alias) && (expr.child(0) instanceof SlotReference))
-                .collect(Collectors.toMap(alias -> (SlotReference) alias.child(0), alias -> (Alias) alias));
-        List<Expression> projectGroupBy = ExpressionUtils.replace(replacedGroupBy, childToAlias);
+        Map<ExprId, NamedExpression> exprIdToNameExpressionMap = new HashMap<>();
+        for (NamedExpression pro : project.getProjects()) {
+            exprIdToNameExpressionMap.put(pro.getExprId(), pro);
+        }
+        List<Expression> originOuterAggGroupBy = outerAgg.getGroupByExpressions();
+        List<Expression> projectGroupBy = new ArrayList<>();
+        for (Expression expression : originOuterAggGroupBy) {
+            ExprId exprId = ((NamedExpression) expression).getExprId();
+            NamedExpression namedExpression = exprIdToNameExpressionMap.get(exprId);
+            projectGroupBy.add(namedExpression);
+        }
         List<NamedExpression> upperProjects = ImmutableList.<NamedExpression>builder()
                 .addAll(projectGroupBy.stream().map(namedExpr -> (NamedExpression) namedExpr).iterator())
                 .addAll(replacedAggFunc.stream().map(expr -> ((NamedExpression) expr).toSlot()).iterator())

regression-test/data/nereids_rules_p0/merge_aggregate/merge_aggregate.out
@@ -297,3 +297,12 @@ PhysicalResultSink
 ------hashAgg[LOCAL]
 --------PhysicalOlapScan[mal_test2]
 
+-- !agg_project_agg_the_project_has_duplicate_slot_output --
+1	7	7
+2	4	4
+6	\N	\N
+7	1	1
+8	2	2
+8	5	5
+9	3	3
+

regression-test/suites/nereids_rules_p0/merge_aggregate/merge_aggregate.groovy
@@ -256,4 +256,10 @@ suite("merge_aggregate") {
         explain shape plan
         select sum(col1),min(col2),max(col3) from (select sum(a) col1, min(b) col2, max(pk) col3 from mal_test2 group by a) t;
     """
+
+    qt_agg_project_agg_the_project_has_duplicate_slot_output """
+    select max(col1), col10, col11 from 
+        (select a,max(b) as col1, count(b) as col4, a as col10, a as col11 
+        from mal_test1 group by a) t group by col10, col11 order by 1,2,3;
+    """
 }