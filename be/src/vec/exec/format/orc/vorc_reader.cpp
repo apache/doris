5468df60d009a003a9e80e1f4082d8c9689fbd4e
daidai <2017501503@qq.com>
2024-05-31 22:56:22 +0800

    [fix](orc)fix orc reader missing column. (#35737)
    bp #35583

--- be/src/vec/exec/format/orc/vorc_reader.cpp
+++ be/src/vec/exec/format/orc/vorc_reader.cpp
@@ -778,6 +778,15 @@ Status OrcReader::set_fill_columns(
         if (iter == predicate_columns.end()) {
             _lazy_read_ctx.missing_columns.emplace(kv.first, kv.second);
         } else {
+            //For check missing column :   missing column == xx, missing column is null,missing column is not null.
+            if (_slot_id_to_filter_conjuncts->find(iter->second.second) !=
+                _slot_id_to_filter_conjuncts->end()) {
+                for (auto& ctx : _slot_id_to_filter_conjuncts->find(iter->second.second)->second) {
+                    _filter_conjuncts.emplace_back(ctx);
+                }
+            }
+
+            // predicate_missing_columns is VLiteral.To fill in default values for missing columns.
             _lazy_read_ctx.predicate_missing_columns.emplace(kv.first, kv.second);
             _lazy_read_ctx.all_predicate_col_ids.emplace_back(iter->second.first);
         }
@@ -1615,6 +1624,7 @@ Status OrcReader::get_next_block(Block* block, size_t* read_rows, bool* eof) {
             }
             RETURN_IF_CATCH_EXCEPTION(
                     Block::filter_block_internal(block, columns_to_filter, result_filter));
+            //_not_single_slot_filter_conjuncts check : missing column1 == missing column2 , missing column == exists column  ...
             if (!_not_single_slot_filter_conjuncts.empty()) {
                 static_cast<void>(_convert_dict_cols_to_string_cols(block, &batch_vec));
                 RETURN_IF_CATCH_EXCEPTION(
