f87a109c032b45e7b266b8579af3234e3ef36329
BePPPower <ftw2139@163.com>
2024-05-31 21:16:21 +0800

    fix 8

--- regression-test/suites/external_table_p0/tvf/test_ctas_with_hdfs.groovy
+++ regression-test/suites/external_table_p0/tvf/test_ctas_with_hdfs.groovy
@@ -29,6 +29,7 @@ suite("test_ctas_with_hdfs","external,hive,tvf,external_docker") {
         String tableName = "ctas_tvf_1";
         def uri = "${defaultFS}" + "/user/doris/preinstalled_data/orc/orc_all_types/p1_col=desktops/p2_col=bigint_col/*"
 
+        // test varchar type as partition column
         sql """drop table if exists ${tableName}; """
         sql """ create table ${tableName}
             PARTITION BY LIST(p1_col,p2_col) (
@@ -47,6 +48,7 @@ suite("test_ctas_with_hdfs","external,hive,tvf,external_docker") {
         order_qt_desc """desc ${tableName};"""
         order_qt_select """ select * from ${tableName} order by tinyint_col"""
 
+        // test char type as partition column
         tableName = "ctas_tvf_2";
         sql """drop table if exists ${tableName}; """
         sql """
@@ -65,7 +67,7 @@ suite("test_ctas_with_hdfs","external,hive,tvf,external_docker") {
         order_qt_desc_2 """desc ${tableName};"""
         order_qt_select_2 """ select * from ${tableName} order by tinyint_col"""
 
-
+        // test tinyint_col type as partition column
         tableName = "ctas_tvf_3";
         sql """drop table if exists ${tableName}; """
         sql """
@@ -85,5 +87,43 @@ suite("test_ctas_with_hdfs","external,hive,tvf,external_docker") {
             """
         order_qt_desc_3 """desc ${tableName};"""
         order_qt_select_3 """ select * from ${tableName} order by tinyint_col"""
+
+
+        // test String as partition column
+        tableName = "ctas_tvf_4";
+        sql """drop table if exists ${tableName}; """
+        sql """
+            create table ${tableName}
+            PARTITION BY LIST(string_col) (
+                PARTITION p1 VALUES IN (("Width activation annoying. Speaker senator cultivate convention silence price second1. Ok personal formulate princess. Screening debt salary reluctant have circulate exclusion. Shy immigrant trousers fifteen cat opponent.")),
+                PARTITION p2 VALUES IN (("Merge fulfil authentic. Paint honest keyboard wave live2. Involvement fighting suppose freeze investigate hers glass. Marriage celebrity shut philosophical."))
+            )
+            PROPERTIES("replication_num" = "1")
+            as
+            select * from
+            HDFS(
+                "uri" = "${uri}",
+                "hadoop.username" = "${hdfsUserName}",
+                "format" = "${format}") where smallint_col <11;
+            """
+        order_qt_desc_4 """desc ${tableName};"""
+        order_qt_select_4 """ select * from ${tableName} order by tinyint_col"""
+        
+        // test varchar type as distributed column
+        tableName = "ctas_tvf_5";
+        sql """drop table if exists ${tableName}; """
+        sql """ create table ${tableName}
+            DISTRIBUTED BY HASH(p1_col) BUCKETS 10
+            PROPERTIES("replication_num" = "1") 
+            as
+            select * from
+            HDFS(
+                "uri" = "${uri}",
+                "hadoop.username" = "${hdfsUserName}",
+                "path_partition_keys"="p1_col,p2_col",
+                "format" = "${format}") where tinyint_col < 4;
+        """
+        order_qt_desc5 """desc ${tableName};"""
+        order_qt_select5 """ select * from ${tableName} order by tinyint_col"""
     }
 }
