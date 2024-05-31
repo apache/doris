056d72fd690ba1c8bff569265eb5837f0cb3b002
gaoxin <AshinGau@outlook.com>
2024-05-31 09:01:29 +0800

    move s3 provider

--- fe/fe-common/src/main/java/org/apache/doris/common/credentials/CloudCredentialWithEndpoint.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/credentials/CloudCredentialWithEndpoint.java
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package org.apache.doris.datasource.credentials;
+package org.apache.doris.common.credentials;
 
 public class CloudCredentialWithEndpoint extends CloudCredential {
 
