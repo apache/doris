ce2527e755be5513ce5905f96c4cf257c134e35e
Kang <kxiao.tiger@gmail.com>
2024-06-02 00:38:14 +0800

    [fix](partial update) duplicate key occur when BE restart after conflict concurrent partial update #35739

--- be/src/olap/tablet.h
+++ be/src/olap/tablet.h
@@ -505,7 +505,7 @@ public:
             const std::vector<segment_v2::SegmentSharedPtr>& segments, int64_t txn_id,
             CalcDeleteBitmapToken* token, RowsetWriter* rowset_writer = nullptr);
 
-    Status update_delete_bitmap(const TabletTxnInfo* txn_info, int64_t txn_id);
+    Status update_delete_bitmap(TabletTxnInfo* txn_info, int64_t txn_id);
     void calc_compaction_output_rowset_delete_bitmap(
             const std::vector<RowsetSharedPtr>& input_rowsets,
             const RowIdConversion& rowid_conversion, uint64_t start_version, uint64_t end_version,
