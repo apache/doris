1b9cb161a59afb120ba99a71b871c54bb7d7fc41
xiejiann <jianxie0@gmail.com>
2024-06-03 10:30:56 +0800

    Optimize Join Penalty Calculation Based on Build Side Data Volume

--- fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
@@ -364,6 +364,13 @@ class CostModelV1 extends PlanVisitor<Cost, PlanContext> {
             } else if (probeStats.getWidthInJoinCluster() < buildStats.getWidthInJoinCluster()) {
                 leftRowCount += 1;
             }
+            if (probeStats.getWidthInJoinCluster() == buildStats.getWidthInJoinCluster()
+                    && probeStats.computeSize() < buildStats.computeSize()) {
+                // When the number of rows and the width on both sides of the join are the same,
+                // we need to consider the cost of materializing the output.
+                // When there is more data on the build side, a greater penalty will be given.
+                rightRowCount += 1;
+            }
         }
 
         /*
