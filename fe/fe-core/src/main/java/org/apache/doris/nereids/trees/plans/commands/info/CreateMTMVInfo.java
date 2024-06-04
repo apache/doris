6500ae048ea9496c063cf3d76cef8fd209a54474
zhangdong <493738387@qq.com>
2024-06-04 14:21:07 +0800

    [fix](mtmv) mtmv disable dynamic partition (#35686)
    1. MTMV do not allow setting dynamic partition related properties
    2. add case for show data

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateMTMVInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateMTMVInfo.java
@@ -33,6 +33,7 @@ import org.apache.doris.catalog.TableIf;
 import org.apache.doris.catalog.View;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.FeNameFormat;
+import org.apache.doris.common.util.DynamicPartitionUtil;
 import org.apache.doris.mtmv.EnvInfo;
 import org.apache.doris.mtmv.MTMVPartitionInfo;
 import org.apache.doris.mtmv.MTMVPartitionInfo.MTMVPartitionType;
@@ -178,6 +179,9 @@ public class CreateMTMVInfo {
     }
 
     private void analyzeProperties() {
+        if (DynamicPartitionUtil.checkDynamicPartitionPropertiesExist(properties)) {
+            throw new AnalysisException("Not support dynamic partition properties on async materialized view");
+        }
         for (String key : MTMVPropertyUtil.mvPropertyKeys) {
             if (properties.containsKey(key)) {
                 MTMVPropertyUtil.analyzeProperty(key, properties.get(key));
