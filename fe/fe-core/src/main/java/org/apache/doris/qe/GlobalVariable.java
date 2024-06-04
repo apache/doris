23016dd24fc7d7c54a8dc5dedb2a08613b872e43
Madrid-Ista <wangjiahao@selectdb.com>
2024-06-04 18:47:53 +0800

    (selectdb-cloud) Modify default value of enableAuditLoader to true (#3406)
    modify default value of enableAuditLoader to true
    
    Co-authored-by: wangjiahao <user.email=wangjiahao@selectdb.com>

--- fe/fe-core/src/main/java/org/apache/doris/qe/GlobalVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/GlobalVariable.java
@@ -120,7 +120,7 @@ public final class GlobalVariable {
     public static String sqlConverterServiceUrl = "";
 
     @VariableMgr.VarAttr(name = ENABLE_AUDIT_PLUGIN, flag = VariableMgr.GLOBAL)
-    public static boolean enableAuditLoader = false;
+    public static boolean enableAuditLoader = true;
 
     @VariableMgr.VarAttr(name = AUDIT_PLUGIN_MAX_BATCH_BYTES, flag = VariableMgr.GLOBAL)
     public static long auditPluginMaxBatchBytes = 50 * 1024 * 1024;
