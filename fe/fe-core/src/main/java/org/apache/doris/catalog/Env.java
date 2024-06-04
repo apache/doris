d1a434bf87cec5e259e44cf9271ee7ed709791d1
Mingyu Chen <morningman@163.com>
2024-06-04 10:27:27 +0800

    [opt](log) refine the FE logger (#35679)
    ## Backgroup
    
    Previously, FE logs were written to files. The main FE logs include
    fe.log, fe.warn.log, fe.audit.log, fe.out, and fe.gc.log.
    In a K8s deployment environment, logs usually need to be output to
    standard output, and then other components process the log stream.
    
    ## Solution
    
    This PR made the following changes:
    
    1. Modified the log4j configuration template
    
    - When started with `--daemon`, logs are still written to various files,
    and the format remains unchanged.
    - When started with `--console`, all logs are output to standard output
    and marked with different prefixes:
    	
    		- `StdoutLogger`: logs for standard output
    		- `StderrLogger`: logs for standard error output
    		- `RuntimeLogger`: logs for fe.log or fe.warn.log
    		- `AuditLogger:` logs for fe.audit.log
    		- No prefix: logs for fe.gc.log
    		
    		Examples are as follows:
    		
    		```
    RuntimeLogger 2024-06-03 14:54:51,229 INFO (binlog-gcer|62)
    [BinlogManager.gc():359] begin gc binlog
    		```
    
    2. Added a new FE config: `enable_file_logger`
    
    Defaults to true. Indicates that logs will be recorded to files
    regardless of the startup method. For example, if it is started with
    `--console`, the log will be output to both the file and the standard
    output. If it is `false`, the log will not be recorded in the file
    regardless of the startup method.
    
    3. Optimized the log format of standard output
    
    The byte streams of stdout and stderr are captured. The logs previously
    outputted using `System.out` will be captured in fe.log for unified
    management.

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -103,6 +103,7 @@ import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.FeMetaVersion;
+import org.apache.doris.common.LogUtils;
 import org.apache.doris.common.MetaNotFoundException;
 import org.apache.doris.common.NereidsSqlCacheManager;
 import org.apache.doris.common.Pair;
@@ -1508,7 +1509,7 @@ public class Env {
             if (Config.enable_check_compatibility_mode) {
                 String msg = "check metadata compatibility successfully";
                 LOG.info(msg);
-                System.out.println(msg);
+                LogUtils.stdout(msg);
                 System.exit(0);
             }
 
@@ -1612,7 +1613,7 @@ public class Env {
             checkLowerCaseTableNames();
 
             String msg = "master finished to replay journal, can write now.";
-            Util.stdoutWithTime(msg);
+            LogUtils.stdout(msg);
             LOG.info(msg);
             // for master, there are some new thread pools need to register metric
             ThreadPoolManager.registerAllThreadPoolMetric();
@@ -2721,7 +2722,7 @@ public class Env {
         try {
             String msg = "notify new FE type transfer: " + newType;
             LOG.warn(msg);
-            Util.stdoutWithTime(msg);
+            LogUtils.stdout(msg);
             this.typeTransferQueue.put(newType);
         } catch (InterruptedException e) {
             LOG.error("failed to put new FE type: {}", newType, e);
@@ -2739,7 +2740,7 @@ public class Env {
                         newType = typeTransferQueue.take();
                     } catch (InterruptedException e) {
                         LOG.error("got exception when take FE type from queue", e);
-                        Util.stdoutWithTime("got exception when take FE type from queue. " + e.getMessage());
+                        LogUtils.stdout("got exception when take FE type from queue. " + e.getMessage());
                         System.exit(-1);
                     }
                     Preconditions.checkNotNull(newType);
@@ -2821,7 +2822,7 @@ public class Env {
                             // exit if master changed to any other type
                             String msg = "transfer FE type from MASTER to " + newType.name() + ". exit";
                             LOG.error(msg);
-                            Util.stdoutWithTime(msg);
+                            LogUtils.stdout(msg);
                             System.exit(-1);
                             break;
                         }
@@ -6320,13 +6321,13 @@ public class Env {
     private void replayJournalsAndExit() {
         replayJournal(-1);
         LOG.info("check metadata compatibility successfully");
-        System.out.println("check metadata compatibility successfully");
+        LogUtils.stdout("check metadata compatibility successfully");
 
         if (Config.checkpoint_after_check_compatibility) {
             String imagePath = dumpImage();
             String msg = "the new image file path is: " + imagePath;
             LOG.info(msg);
-            System.out.println(msg);
+            LogUtils.stdout(msg);
         }
 
         System.exit(0);
--- fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBEnvironment.java
+++ fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBEnvironment.java
@@ -105,8 +105,7 @@ public class BDBEnvironment {
             DbResetRepGroup resetUtility = new DbResetRepGroup(
                     envHome, PALO_JOURNAL_GROUP, selfNodeName, selfNodeHostPort);
             resetUtility.reset();
-            LOG.warn("WARNING: metadata recovery mode, group has been reset.");
-            System.out.println("WARNING: metadata recovery mode, group has been reset.");
+            LOG.warn("metadata recovery mode, group has been reset.");
         }
 
         // set replication config
