0a5a18ba39b89a658257aa94cc8020dce1a0eb01
meiyi <myimeiyi@gmail.com>
2024-06-05 15:49:24 +0800

    [improve](txn insert) Txn load support cloud mode (#34721)
    ## Proposed changes
    
    ### Purpose
    
    The user doc:
    https://doris.apache.org/zh-CN/docs/dev/data-operate/import/transaction-load-manual
    
    We have supported insert into
    select(https://github.com/apache/doris/pull/31666),
    update(https://github.com/apache/doris/pull/33034) and
    delete(https://github.com/apache/doris/pull/33100) in transaction load.
    
    https://github.com/apache/doris/pull/32980 implements one txn write to
    one partition more than one rowsets.
    
    This pr implements to cloud mode of
    https://github.com/apache/doris/pull/32980
    
    ### Implementation
    
    #### sub_txn_id
    
    see https://github.com/apache/doris/pull/32980
    
    #### Meta service supports commit txn
    
    This process is generally the same as commit_txn, the difference is that
    he partitions version will plus 1 in multi sub txns.
    
    One example:
    Suppose the table, partition, tablet and version info is:
    ```
    --------------------------------------------
    | table | partition | tablet    | version |
    --------------------------------------------
    | t1    | t1_p1     | t1_p1.1   | 1       |
    | t1    | t1_p1     | t1_p1.2   | 1       |
    | t1    | t1_p2     | t1_p2.1   | 2       |
    | t2    | t2_p3     | t2_p3.1   | 3       |
    | t2    | t2_p4     | t2_p4.1   | 4       |
    --------------------------------------------
    ```
    
    Now we commit a txn with 3 sub txns and the tablets are:
     *  sub_txn1: t1_p1.1, t1_p1.2, t1_p2.1
     *  sub_txn2: t2_p3.1
     *  sub_txn3: t1_p1.1, t1_p1.2
    
    When commit, the partitions version will be:
     *  sub_txn1: t1_p1(1 -> 2), t1_p2(2 -> 3)
     *  sub_txn2: t2_p3(3 -> 4)
     *  sub_txn3: t1_p1(2 -> 3)
    
    After commit, the partitions version will be:
     *  t1: t1_p1(3), t1_p2(3)
     *  t2: t2_p3(4), t2_p4(4)
    
    #### Meta service support generate sub_txn_id by `begin_sub_txn`

--- fe/fe-core/src/main/java/org/apache/doris/cloud/rpc/MetaServiceClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/rpc/MetaServiceClient.java
@@ -200,6 +200,24 @@ public class MetaServiceClient {
         return blockingStub.getCurrentMaxTxnId(request);
     }
 
+    public Cloud.BeginSubTxnResponse beginSubTxn(Cloud.BeginSubTxnRequest request) {
+        if (!request.hasCloudUniqueId()) {
+            Cloud.BeginSubTxnRequest.Builder builder = Cloud.BeginSubTxnRequest.newBuilder();
+            builder.mergeFrom(request);
+            return blockingStub.beginSubTxn(builder.setCloudUniqueId(Config.cloud_unique_id).build());
+        }
+        return blockingStub.beginSubTxn(request);
+    }
+
+    public Cloud.AbortSubTxnResponse abortSubTxn(Cloud.AbortSubTxnRequest request) {
+        if (!request.hasCloudUniqueId()) {
+            Cloud.AbortSubTxnRequest.Builder builder = Cloud.AbortSubTxnRequest.newBuilder();
+            builder.mergeFrom(request);
+            return blockingStub.abortSubTxn(builder.setCloudUniqueId(Config.cloud_unique_id).build());
+        }
+        return blockingStub.abortSubTxn(request);
+    }
+
     public Cloud.CheckTxnConflictResponse checkTxnConflict(Cloud.CheckTxnConflictRequest request) {
         return blockingStub.checkTxnConflict(request);
     }
