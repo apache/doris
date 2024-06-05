6bc49ead5cac26c133049bc764ed976133edc26a
Toms1999 <mj_tangsi@163.com>
2024-06-05 23:30:55 +0800

    [enhancement](regression-test) Change to a more special line separator

--- regression-test/suites/load_p0/mysql_load/test_mysql_load.groovy
+++ regression-test/suites/load_p0/mysql_load/test_mysql_load.groovy
@@ -103,13 +103,17 @@ suite("test_mysql_load", "p0") {
     """
 
     def test_line_delimiter_file = getLoalFilePath "test_line_delimiter.csv"
+    def fileContent = new File(test_line_delimiter_file).text
+    def newFileContent = fileContent.replace("\\r\\n", "\r\n")
+    new File(test_line_delimiter_file).withWriter { writer -> writer << newFileContent }
+
     sql """
         LOAD DATA 
         LOCAL
         INFILE '${test_line_delimiter_file}'
         INTO TABLE ${tableName}
         COLUMNS TERMINATED BY '|'
-        LINES TERMINATED BY 'weizuo'
+        LINES TERMINATED BY '\r\n'
         (id, value);
     """
 
