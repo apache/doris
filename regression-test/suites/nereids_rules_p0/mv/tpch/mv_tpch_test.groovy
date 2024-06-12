5bfcc7fa7978712442506a8cc69c05bb128bd474
seawinde <149132972+seawinde@users.noreply.github.com>
2024-06-12 12:03:05 +0800

    [opt](mtmv) Set query rewrite by materialized view default enable (#35897)
    Set query rewrite by materialized view default enable
    As before, if want to use query reweirte by async materialized view,
    must run: SET enable_materialized_view_rewrite=true
    After this, not need

--- regression-test/suites/nereids_rules_p0/mv/tpch/mv_tpch_test.groovy
+++ regression-test/suites/nereids_rules_p0/mv/tpch/mv_tpch_test.groovy
@@ -84,10 +84,7 @@ suite("mv_tpch_test") {
 
     String db = context.config.getDbNameByFile(context.file)
     sql "use ${db}"
-    sql "SET enable_nereids_planner=true"
     sql "set runtime_filter_mode=OFF"
-    sql "SET enable_fallback_to_original_planner=false"
-    sql "SET enable_materialized_view_rewrite=true"
     // mv8 Nereids cost too much time ( > 5s )
     sql "SET enable_nereids_timeout=false"
 
