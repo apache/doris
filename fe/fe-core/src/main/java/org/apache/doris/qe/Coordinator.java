wangbo/incubator-doris

Refacor query queue

fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -649,11 +649,7 @@ public class Coordinator implements CoordInterface {
                         throw new UserException("could not find query queue");
                     }
                     queueToken = queryQueue.getToken();
-                    if (!queueToken.waitSignal(this.queryOptions.getExecutionTimeout() * 1000)) {
-                        LOG.error("query (id=" + DebugUtil.printId(queryId) + ") " + queueToken.getOfferResultDetail());
-                        queryQueue.returnToken(queueToken);
-                        throw new UserException(queueToken.getOfferResultDetail());
-                    }
+                    queueToken.get(this.queryOptions.getExecutionTimeout() * 1000);
                 }
             } else {
                 context.setWorkloadGroupName("");
@@ -666,7 +662,7 @@ public class Coordinator implements CoordInterface {
     public void close() {
         if (queryQueue != null && queueToken != null) {
             try {
-                queryQueue.returnToken(queueToken);
+                queryQueue.notifyQueuedQuery(queueToken);
             } catch (Throwable t) {
                 LOG.error("error happens when coordinator close ", t);
             }
@@ -1475,7 +1471,7 @@ public class Coordinator implements CoordInterface {
     public void cancel() {
         cancel(new Status(TStatusCode.CANCELLED, "query is cancelled by user"));
         if (queueToken != null) {
-            queueToken.signalForCancel();
+            queueToken.cancel();
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/resource/workloadgroup/QueryQueue.java
@@ -106,16 +106,16 @@ public class QueryQueue {
                 LOG.info(this.debugString());
             }
             if (currentRunningQueryNum < maxConcurrency) {
+                QueueToken retToken = new QueueToken(TokenState.READY_TO_RUN, queueTimeout, this);
+                retToken.complete();
                 currentRunningQueryNum++;
-                QueueToken retToken = new QueueToken(TokenState.READY_TO_RUN, queueTimeout, "offer success");
-                retToken.setQueueTimeWhenOfferSuccess();
                 return retToken;
             }
             if (priorityTokenQueue.size() >= maxQueueSize) {
                 throw new UserException("query waiting queue is full, queue length=" + maxQueueSize);
             }
             QueueToken newQueryToken = new QueueToken(TokenState.ENQUEUE_SUCCESS, queueTimeout,
-                    "query wait timeout " + queueTimeout + " ms");
+                    this);
             newQueryToken.setQueueTimeWhenQueueSuccess();
             this.priorityTokenQueue.offer(newQueryToken);
             return newQueryToken;
@@ -127,35 +127,26 @@ public class QueryQueue {
         }
     }
 
-    // If the token is acquired and do work success, then call this method to release it.
-    public void returnToken(QueueToken token) {
+    public void notifyQueuedQuery(QueueToken curQueryToken) {
         queueLock.lock();
         try {
-            // If current token is not in ready to run state, then it is still in the queue
-            // it is not running, just remove it.
-            if (!token.isReadyToRun()) {
-                this.priorityTokenQueue.remove(token);
-                return;
+            if (curQueryToken.isReadyToRun()) {
+                currentRunningQueryNum--;
             }
-            currentRunningQueryNum--;
             Preconditions.checkArgument(currentRunningQueryNum >= 0);
-            // If return token and find user changed concurrency num,  then maybe need signal
-            // more tokens.
             while (currentRunningQueryNum < maxConcurrency) {
-                QueueToken nextToken = this.priorityTokenQueue.poll();
-                if (nextToken != null) {
-                    if (nextToken.signal()) {
-                        ++currentRunningQueryNum;
-                    }
-                } else {
+                QueueToken queueToken = this.priorityTokenQueue.poll();
+                if (queueToken == null) {
                     break;
                 }
+                queueToken.complete();
+                currentRunningQueryNum++;
             }
         } finally {
+            queueLock.unlock();
             if (LOG.isDebugEnabled()) {
                 LOG.info(this.debugString());
             }
-            queueLock.unlock();
         }
     }
 
@@ -170,6 +161,15 @@ public class QueryQueue {
             if (LOG.isDebugEnabled()) {
                 LOG.debug(this.debugString());
             }
+            queueLock.unlock();
+        }
+    }
+
+    public void removeToken(QueueToken queueToken) {
+        queueLock.lock();
+        try {
+            priorityTokenQueue.remove(queueToken);
+        } finally {
             queueLock.unlock();
         }
     }

apache/doris

fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -656,11 +656,7 @@ public class Coordinator implements CoordInterface {
                         throw new UserException("could not find query queue");
                     }
                     queueToken = queryQueue.getToken();
-                    if (!queueToken.waitSignal(this.queryOptions.getExecutionTimeout() * 1000)) {
-                        LOG.error("query (id=" + DebugUtil.printId(queryId) + ") " + queueToken.getOfferResultDetail());
-                        queryQueue.returnToken(queueToken);
-                        throw new UserException(queueToken.getOfferResultDetail());
-                    }
+                    queueToken.get(this.queryOptions.getExecutionTimeout() * 1000);
                 }
             } else {
                 context.setWorkloadGroupName("");
@@ -673,7 +669,7 @@ public class Coordinator implements CoordInterface {
     public void close() {
         if (queryQueue != null && queueToken != null) {
             try {
-                queryQueue.returnToken(queueToken);
+                queryQueue.releaseAndNotify(queueToken);
             } catch (Throwable t) {
                 LOG.error("error happens when coordinator close ", t);
             }
@@ -1482,7 +1478,7 @@ public class Coordinator implements CoordInterface {
     public void cancel() {
         cancel(new Status(TStatusCode.CANCELLED, "query is cancelled by user"));
         if (queueToken != null) {
-            queueToken.signalForCancel();
+            queueToken.cancel();
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/resource/workloadgroup/QueryQueue.java
@@ -106,17 +106,16 @@ public class QueryQueue {
                 LOG.info(this.debugString());
             }
             if (currentRunningQueryNum < maxConcurrency) {
+                QueueToken retToken = new QueueToken(TokenState.READY_TO_RUN, queueTimeout, this);
+                retToken.complete();
                 currentRunningQueryNum++;
-                QueueToken retToken = new QueueToken(TokenState.READY_TO_RUN, queueTimeout, "offer success");
-                retToken.setQueueTimeWhenOfferSuccess();
                 return retToken;
             }
             if (priorityTokenQueue.size() >= maxQueueSize) {
                 throw new UserException("query waiting queue is full, queue length=" + maxQueueSize);
             }
             QueueToken newQueryToken = new QueueToken(TokenState.ENQUEUE_SUCCESS, queueTimeout,
-                    "query wait timeout " + queueTimeout + " ms");
-            newQueryToken.setQueueTimeWhenQueueSuccess();
+                    this);
             this.priorityTokenQueue.offer(newQueryToken);
             return newQueryToken;
         } finally {
@@ -127,35 +126,27 @@ public class QueryQueue {
         }
     }
 
-    // If the token is acquired and do work success, then call this method to release it.
-    public void returnToken(QueueToken token) {
+    public void releaseAndNotify(QueueToken releaseToken) {
         queueLock.lock();
         try {
-            // If current token is not in ready to run state, then it is still in the queue
-            // it is not running, just remove it.
-            if (!token.isReadyToRun()) {
-                this.priorityTokenQueue.remove(token);
-                return;
+            //NOTE:token's tokenState need to be locked by queueLock
+            if (releaseToken.isReadyToRun()) {
+                currentRunningQueryNum--;
             }
-            currentRunningQueryNum--;
             Preconditions.checkArgument(currentRunningQueryNum >= 0);
-            // If return token and find user changed concurrency num,  then maybe need signal
-            // more tokens.
             while (currentRunningQueryNum < maxConcurrency) {
-                QueueToken nextToken = this.priorityTokenQueue.poll();
-                if (nextToken != null) {
-                    if (nextToken.signal()) {
-                        ++currentRunningQueryNum;
-                    }
-                } else {
+                QueueToken queueToken = this.priorityTokenQueue.poll();
+                if (queueToken == null) {
                     break;
                 }
+                queueToken.complete();
+                currentRunningQueryNum++;
             }
         } finally {
+            queueLock.unlock();
             if (LOG.isDebugEnabled()) {
                 LOG.info(this.debugString());
             }
-            queueLock.unlock();
         }
     }
 
@@ -170,6 +161,15 @@ public class QueryQueue {
             if (LOG.isDebugEnabled()) {
                 LOG.debug(this.debugString());
             }
+            queueLock.unlock();
+        }
+    }
+
+    public void removeToken(QueueToken queueToken) {
+        queueLock.lock();
+        try {
+            priorityTokenQueue.remove(queueToken);
+        } finally {
             queueLock.unlock();
         }
     }