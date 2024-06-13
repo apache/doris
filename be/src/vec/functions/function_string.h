fe736c3a235079027a94a754cd86d3dc448f265a
liujiwen-up <uptual@163.com>
2024-06-13 11:58:47 +0800

    Changing split_part fucntion behavior

--- be/src/vec/functions/function_string.h
+++ be/src/vec/functions/function_string.h
@@ -1542,6 +1542,10 @@ public:
                                         reinterpret_cast<const char*>(str.data + pre_offset + 1),
                                         (size_t)offset - pre_offset - 1},
                                 i, res_chars, res_offsets);
+                    } else if (num == 0) {
+                        StringOP::push_value_string(
+                            std::string_view {reinterpret_cast<const char*>(str.data), str.size},
+                            i, res_chars, res_offsets);
                     } else {
                         StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                     }
@@ -1572,6 +1576,10 @@ public:
                                                           str.data + pre_offset + delimiter.size),
                                                   (size_t)offset - pre_offset - delimiter.size},
                                 i, res_chars, res_offsets);
+                    } else if (num == 0) {
+                        StringOP::push_value_string(
+                            std::string_view {reinterpret_cast<const char*>(str.data), str.size},
+                            i, res_chars, res_offsets);
                     } else {
                         StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                     }
@@ -1611,6 +1619,10 @@ public:
                                         (size_t)pre_offset - offset - delimiter.size)},
                                 i, res_chars, res_offsets);
                     }
+                } else if (num == 0) {
+                        StringOP::push_value_string(
+                            std::string_view {reinterpret_cast<const char*>(str.data), str.size},
+                            i, res_chars, res_offsets);
                 } else {
                     StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                 }
