38245d7edf6bb1bebaa483a76bb19f405f1fd6fd
Mingyu Chen <morningman@163.com>
2024-06-13 11:50:40 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/RuleSet.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/RuleSet.java
@@ -62,6 +62,7 @@ import org.apache.doris.nereids.rules.implementation.LogicalFileSinkToPhysicalFi
 import org.apache.doris.nereids.rules.implementation.LogicalFilterToPhysicalFilter;
 import org.apache.doris.nereids.rules.implementation.LogicalGenerateToPhysicalGenerate;
 import org.apache.doris.nereids.rules.implementation.LogicalHiveTableSinkToPhysicalHiveTableSink;
+import org.apache.doris.nereids.rules.implementation.LogicalHudiScanToPhysicalHudiScan;
 import org.apache.doris.nereids.rules.implementation.LogicalIcebergTableSinkToPhysicalIcebergTableSink;
 import org.apache.doris.nereids.rules.implementation.LogicalIntersectToPhysicalIntersect;
 import org.apache.doris.nereids.rules.implementation.LogicalJdbcScanToPhysicalJdbcScan;
@@ -163,6 +164,7 @@ public class RuleSet {
             .add(new LogicalOlapScanToPhysicalOlapScan())
             .add(new LogicalDeferMaterializeOlapScanToPhysicalDeferMaterializeOlapScan())
             .add(new LogicalSchemaScanToPhysicalSchemaScan())
+            .add(new LogicalHudiScanToPhysicalHudiScan())
             .add(new LogicalFileScanToPhysicalFileScan())
             .add(new LogicalJdbcScanToPhysicalJdbcScan())
             .add(new LogicalOdbcScanToPhysicalOdbcScan())
