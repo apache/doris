2017c245c2a9ef43392c952c29fa1045d47af3c9
Pxl <pxl290@qq.com>
2024-06-13 16:34:33 +0800

    [Feature](sink) support parallel result sink (#36053)
    ## Proposed changes
    support parallel result sink

--- be/src/runtime/runtime_state.h
+++ be/src/runtime/runtime_state.h
@@ -367,10 +367,6 @@ public:
         return _query_options.return_object_data_as_binary;
     }
 
-    bool enable_exchange_node_parallel_merge() const {
-        return _query_options.enable_enable_exchange_node_parallel_merge;
-    }
-
     segment_v2::CompressionTypePB fragement_transmission_compression_type() const {
         if (_query_options.__isset.fragment_transmission_compression_codec) {
             if (_query_options.fragment_transmission_compression_codec == "lz4") {
