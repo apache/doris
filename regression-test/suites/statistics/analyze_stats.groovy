b6aa17ae32282322cdbafb1baf8f3c58fa88e410
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2024-06-17 13:04:51 +0800

    [fix](statistics)Fix stats analyze p0 case. (#36251) (#36364)
    Fix stats p0 case. Def a variable before using it.
    backport https://github.com/apache/doris/pull/36251

--- regression-test/suites/statistics/analyze_stats.groovy
+++ regression-test/suites/statistics/analyze_stats.groovy
@@ -1167,7 +1167,7 @@ PARTITION `p599` VALUES IN (599)
     );"""
     sql """insert into test_analyze_specific_column values('%.', 2, 1);"""
     sql """ANALYZE TABLE test_analyze_specific_column(col2) WITH SYNC"""
-    result = sql """SHOW COLUMN STATS test_analyze_specific_column"""
+    def result = sql """SHOW COLUMN STATS test_analyze_specific_column"""
     assert result.size() == 1
 
     // test escape sql
@@ -2754,7 +2754,7 @@ PARTITION `p599` VALUES IN (599)
 
     // Test analyze default full.
     sql """analyze table trigger_test with sync"""
-    def result = sql """show column stats trigger_test"""
+    result = sql """show column stats trigger_test"""
     logger.info("show column trigger_test stats: " + result)
     assertEquals(2, result.size())
     assertEquals("4.0", result[0][2])
