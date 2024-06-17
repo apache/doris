a42835c73b2f2940994037c456b27bc08491c6f2
deardeng <565620795@qq.com>
2024-06-17 15:16:18 +0800

    [fix](cloud) add cluster info for `admin show replica distribution from`

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ShowReplicaDistributionStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ShowReplicaDistributionStmt.java
@@ -37,6 +37,7 @@ public class ShowReplicaDistributionStmt extends ShowStmt {
             .add("BackendId").add("ReplicaNum").add("ReplicaSize")
             .add("NumGraph").add("NumPercent")
             .add("SizeGraph").add("SizePercent")
+            .add("Cloud").add("CloudClusterName").add("CloudClusterId")
             .build();
 
     private TableRef tblRef;
