e9aa27cda78a34ccfd8e6db41d0cb63ab6eb0203
airborne12 <airborne08@gmail.com>
2024-06-17 15:19:16 +0800

    [Fix](inverted index) fix inverted index compound reader memory leak (#36146)

--- be/src/olap/rowset/segment_v2/inverted_index_compound_reader.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_compound_reader.cpp
@@ -129,7 +129,6 @@ DorisCompoundReader::DorisCompoundReader(lucene::store::Directory* d, const char
           dir(d),
           ram_dir(new lucene::store::RAMDirectory()),
           file_name(name),
-          stream(nullptr),
           entries(_CLNEW EntriesType(true, true)) {
     bool success = false;
     try {
@@ -209,7 +208,13 @@ void DorisCompoundReader::copyFile(const char* file, int64_t file_length, uint8_
 }
 
 DorisCompoundReader::~DorisCompoundReader() {
-    _CLDELETE(entries)
+    if (!_closed) {
+        try {
+            close();
+        } catch (CLuceneError& err) {
+            LOG(ERROR) << "DorisCompoundReader finalize error:" << err.what();
+        }
+    }
 }
 
 const char* DorisCompoundReader::getClassName() {
@@ -292,14 +297,22 @@ bool DorisCompoundReader::openInput(const char* name, lucene::store::IndexInput*
 void DorisCompoundReader::close() {
     std::lock_guard<std::mutex> wlock(_this_lock);
     if (stream != nullptr) {
-        entries->clear();
         stream->close();
         _CLDELETE(stream)
     }
-    ram_dir->close();
-    dir->close();
-    _CLDECDELETE(dir)
-    _CLDELETE(ram_dir)
+    if (entries != nullptr) {
+        entries->clear();
+        _CLDELETE(entries)
+    }
+    if (ram_dir) {
+        ram_dir->close();
+        _CLDELETE(ram_dir)
+    }
+    if (dir) {
+        dir->close();
+        _CLDECDELETE(dir)
+    }
+    _closed = true;
 }
 
 bool DorisCompoundReader::doDeleteFile(const char* /*name*/) {
