b53edb3e0670308c3d39778e8015ed8bf5a67e65
cjj2010 <2449402815@qq.com>
2024-06-17 17:43:43 +0800

    [enhancement](regression-test) unique tests

--- regression-test/suites/schema_change_p0/test_unique_model_schema_value_change2.groovy
+++ regression-test/suites/schema_change_p0/test_unique_model_schema_value_change2.groovy
@@ -16,8 +16,8 @@
 // under the License.
 
 suite("test_unique_model_schema_value_change2", "p0") {
-    def tbName = "test_unique_model_schema_value_change2"
-    def tbName2 = "test_unique_model_schema_value_change_2"
+    def tbName = "test_unique_model_value_change2"
+    def tbName2 = "test_unique_model_value_change_2"
     def on_write = getRandomBoolean()
     println String.format("current enable_unique_key_merge_on_write is : %s ",on_write)
     //Test the unique model by adding a value column
