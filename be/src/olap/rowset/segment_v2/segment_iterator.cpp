542a9f89310af3344fcdd72d30291af05cb42d12
姜凯 <airborne08@gmail.com>
2024-06-18 16:29:52 +0800

    update

--- be/src/olap/rowset/segment_v2/segment_iterator.cpp
+++ be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -1687,9 +1687,6 @@ void SegmentIterator::_vec_init_char_column_id(vectorized::Block* block) {
                     _char_type_idx_no_0.emplace_back(i);
                 }
             }
-            if (column_desc->type() == FieldType::OLAP_FIELD_TYPE_CHAR) {
-                _is_char_type[cid] = true;
-            }
         }
     }
 }
