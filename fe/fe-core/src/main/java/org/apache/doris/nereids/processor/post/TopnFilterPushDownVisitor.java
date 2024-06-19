apache/doris

commit 2d3e8f4cab06f2a71bc5867b0da63a660f2140b3

[Feature](topn) support general expr with topn filter and some refactor (#35405)
support general expr with topn filter and some refactor
Co-authored-by: minghong <englefly@gmail.com>

fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/TopnFilterPushDownVisitor.java
@@ -1,0 +1,233 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.processor.post;
+
+import org.apache.doris.nereids.processor.post.TopnFilterPushDownVisitor.PushDownContext;
+import org.apache.doris.nereids.trees.expressions.Alias;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.expressions.NamedExpression;
+import org.apache.doris.nereids.trees.expressions.visitor.ExpressionVisitors;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.algebra.TopN;
+import org.apache.doris.nereids.trees.plans.algebra.Union;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalCTEAnchor;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalCTEProducer;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalDeferMaterializeOlapScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalEsScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalFileScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalHashJoin;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalJdbcScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalNestedLoopJoin;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalOdbcScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalOlapScan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalProject;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalRelation;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalSetOperation;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalTopN;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalWindow;
+import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
+
+import com.google.common.collect.Maps;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * push down topn filter
+ */
+public class TopnFilterPushDownVisitor extends PlanVisitor<Boolean, PushDownContext> {
+    private TopnFilterContext topnFilterContext;
+
+    public TopnFilterPushDownVisitor(TopnFilterContext topnFilterContext) {
+        this.topnFilterContext = topnFilterContext;
+    }
+
+    /**
+     * topn filter push-down context
+     */
+    public static class PushDownContext {
+        final Expression probeExpr;
+        final TopN topn;
+        final boolean nullsFirst;
+
+        public PushDownContext(TopN topn, Expression probeExpr, boolean nullsFirst) {
+            this.topn = topn;
+            this.probeExpr = probeExpr;
+            this.nullsFirst = nullsFirst;
+        }
+
+        public PushDownContext withNewProbeExpression(Expression newProbe) {
+            return new PushDownContext(topn, newProbe, nullsFirst);
+        }
+
+    }
+
+    @Override
+    public Boolean visit(Plan plan, PushDownContext ctx) {
+        boolean pushed = false;
+        for (Plan child : plan.children()) {
+            pushed |= child.accept(this, ctx);
+        }
+        return pushed;
+    }
+
+    @Override
+    public Boolean visitPhysicalProject(
+            PhysicalProject<? extends Plan> project, PushDownContext ctx) {
+        // project ( A+1 as x)
+        // probeExpr: abs(x) => abs(A+1)
+        PushDownContext ctxProjectProbeExpr = ctx;
+        Map<Expression, Expression> replaceMap = Maps.newHashMap();
+        for (NamedExpression ne : project.getProjects()) {
+            if (ne instanceof Alias && ctx.probeExpr.getInputSlots().contains(ne.toSlot())) {
+                replaceMap.put(ctx.probeExpr.getInputSlots().iterator().next(), ((Alias) ne).child());
+            }
+        }
+        if (! replaceMap.isEmpty()) {
+            Expression newProbeExpr = ctx.probeExpr.accept(ExpressionVisitors.EXPRESSION_MAP_REPLACER, replaceMap);
+            ctxProjectProbeExpr = ctx.withNewProbeExpression(newProbeExpr);
+        }
+        return project.child().accept(this, ctxProjectProbeExpr);
+    }
+
+    @Override
+    public Boolean visitPhysicalSetOperation(
+            PhysicalSetOperation setOperation, PushDownContext ctx) {
+        boolean pushedDown = pushDownFilterToSetOperatorChild(setOperation, ctx, 0);
+
+        if (setOperation instanceof Union) {
+            for (int i = 1; i < setOperation.children().size(); i++) {
+                // push down to the other children
+                pushedDown |= pushDownFilterToSetOperatorChild(setOperation, ctx, i);
+            }
+        }
+        return pushedDown;
+    }
+
+    private Boolean pushDownFilterToSetOperatorChild(PhysicalSetOperation setOperation,
+            PushDownContext ctx, int childIdx) {
+        Plan child = setOperation.child(childIdx);
+        Map<Expression, Expression> replaceMap = Maps.newHashMap();
+
+        List<NamedExpression> setOutputs = setOperation.getOutputs();
+        for (int i = 0; i < setOutputs.size(); i++) {
+            replaceMap.put(setOutputs.get(i).toSlot(),
+                    setOperation.getRegularChildrenOutputs().get(childIdx).get(i));
+        }
+        if (!replaceMap.isEmpty()) {
+            Expression newProbeExpr = ctx.probeExpr.accept(ExpressionVisitors.EXPRESSION_MAP_REPLACER,
+                    replaceMap);
+            PushDownContext childPushDownContext = ctx.withNewProbeExpression(newProbeExpr);
+            return child.accept(this, childPushDownContext);
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalCTEAnchor(PhysicalCTEAnchor<? extends Plan, ? extends Plan> anchor,
+            PushDownContext ctx) {
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalCTEProducer(PhysicalCTEProducer<? extends Plan> anchor,
+            PushDownContext ctx) {
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalTopN(PhysicalTopN<? extends Plan> topn, PushDownContext ctx) {
+        if (topn.equals(ctx.topn)) {
+            return topn.child().accept(this, ctx);
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalWindow(PhysicalWindow<? extends Plan> window, PushDownContext ctx) {
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalHashJoin(PhysicalHashJoin<? extends Plan, ? extends Plan> join,
+            PushDownContext ctx) {
+        if (ctx.nullsFirst && join.getJoinType().isOuterJoin()) {
+            // topn-filter can be pushed down to the left child of leftOuterJoin
+            // and to the right child of rightOuterJoin,
+            // but PushDownTopNThroughJoin rule already pushes topn to the left and right side.
+            // the topn-filter will be generated according to the inferred topn node.
+            return false;
+        }
+        if (join.left().getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
+            return join.left().accept(this, ctx);
+        }
+        if (join.right().getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
+            // expand expr to the other side of hash join condition:
+            // T1 join T2 on T1.x = T2.y order by T2.y limit 10
+            // we rewrite probeExpr from T2.y to T1.x and try to push T1.x to left side
+            for (Expression conj : join.getHashJoinConjuncts()) {
+                if (ctx.probeExpr.equals(conj.child(1))) {
+                    // push to left child. right child is blocking operator, do not need topn-filter
+                    PushDownContext newCtx = ctx.withNewProbeExpression(conj.child(0));
+                    return join.left().accept(this, newCtx);
+                }
+            }
+        }
+        // topn key is combination of left and right
+        // select * from (select T1.A+T2.B as x from T1 join T2) T3 order by x limit 1;
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalNestedLoopJoin(PhysicalNestedLoopJoin<? extends Plan, ? extends Plan> join,
+            PushDownContext ctx) {
+        if (ctx.nullsFirst && join.getJoinType().isOuterJoin()) {
+            // topn-filter can be pushed down to the left child of leftOuterJoin
+            // and to the right child of rightOuterJoin,
+            // but PushDownTopNThroughJoin rule already pushes topn to the left and right side.
+            // the topn-filter will be generated according to the inferred topn node.
+            return false;
+        }
+        // push to left child. right child is blocking operator, do not need topn-filter
+        if (join.left().getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
+            return join.left().accept(this, ctx);
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean visitPhysicalRelation(PhysicalRelation relation, PushDownContext ctx) {
+        if (supportPhysicalRelations(relation)
+                && relation.getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
+            if (relation.getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
+                topnFilterContext.addTopnFilter(ctx.topn, relation, ctx.probeExpr);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean supportPhysicalRelations(PhysicalRelation relation) {
+        return relation instanceof PhysicalOlapScan
+                || relation instanceof PhysicalOdbcScan
+                || relation instanceof PhysicalEsScan
+                || relation instanceof PhysicalFileScan
+                || relation instanceof PhysicalJdbcScan
+                || relation instanceof PhysicalDeferMaterializeOlapScan;
+    }
+}

regression-test/suites/nereids_tpch_p0/tpch/topn-filter.groovy
@@ -79,7 +79,7 @@ suite("topn-filter") {
 
     qt_check_result2 "${multi_topn_desc}"
 
-    // push down topn-filter to both join children
+    // push down topn-filter to join left child
     explain {
         sql """
                 select o_orderkey, c_custkey
@@ -87,7 +87,7 @@ suite("topn-filter") {
                 join customer on o_custkey = c_custkey 
                 order by c_custkey limit 2; 
             """
-        contains "TOPN OPT:"
+        contains "TOPN OPT:4"
     }
 
     // push topn filter down through AGG
@@ -107,7 +107,7 @@ suite("topn-filter") {
             join nation on s_nationkey = n_nationkey 
             order by s_nationkey limit 1;
         """
-        contains "TOPN OPT:"
+        contains "TOPN OPT:7"
     }
 
     explain {
@@ -141,7 +141,7 @@ suite("topn-filter") {
     // this topn-filter is generated by topn2, not topn1
     explain {
         sql "select * from nation left outer join region on r_regionkey = n_regionkey order by n_regionkey nulls first limit 1; "
-        contains "TOPN OPT:"
+        multiContains ("TOPN OPT:", 1)
     }
 
     // TODO: support latter, push topn to right outer join

be/src/olap/iterators.h
@@ -99,9 +99,8 @@ public:
     TabletSchemaSPtr tablet_schema = nullptr;
     bool enable_unique_key_merge_on_write = false;
     bool record_rowids = false;
-    // flag for enable topn opt
-    bool use_topn_opt = false;
     std::vector<int> topn_filter_source_node_ids;
+    int topn_filter_target_node_id = -1;
     // used for special optimization for query : ORDER BY key DESC LIMIT n
     bool read_orderby_key_reverse = false;
     // columns for orderby keys

be/src/olap/rowset/beta_rowset_reader.cpp
@@ -220,8 +220,8 @@ Status BetaRowsetReader::get_segment_iterators(RowsetReaderContext* read_context
     _read_options.enable_unique_key_merge_on_write =
             _read_context->enable_unique_key_merge_on_write;
     _read_options.record_rowids = _read_context->record_rowids;
-    _read_options.use_topn_opt = _read_context->use_topn_opt;
     _read_options.topn_filter_source_node_ids = _read_context->topn_filter_source_node_ids;
+    _read_options.topn_filter_target_node_id = _read_context->topn_filter_target_node_id;
     _read_options.read_orderby_key_reverse = _read_context->read_orderby_key_reverse;
     _read_options.read_orderby_key_columns = _read_context->read_orderby_key_columns;
     _read_options.io_ctx.reader_type = _read_context->reader_type;

be/src/olap/rowset/rowset_reader_context.h
@@ -36,9 +36,8 @@ struct RowsetReaderContext {
     ReaderType reader_type = ReaderType::READER_QUERY;
     Version version {-1, -1};
     TabletSchemaSPtr tablet_schema = nullptr;
-    // flag for enable topn opt
-    bool use_topn_opt = false;
     std::vector<int> topn_filter_source_node_ids;
+    int topn_filter_target_node_id = -1;
     // whether rowset should return ordered rows.
     bool need_ordered_result = true;
     // used for special optimization for query : ORDER BY key DESC LIMIT n

be/src/olap/rowset/segment_v2/segment.cpp
@@ -164,13 +164,12 @@ Status Segment::new_iterator(SchemaSPtr schema, const StorageReadOptions& read_o
             return Status::OK();
         }
     }
-    if (read_options.use_topn_opt) {
+
+    if (!read_options.topn_filter_source_node_ids.empty()) {
         auto* query_ctx = read_options.runtime_state->get_query_ctx();
         for (int id : read_options.topn_filter_source_node_ids) {
-            if (!query_ctx->get_runtime_predicate(id).need_update()) {
-                continue;
-            }
-            auto runtime_predicate = query_ctx->get_runtime_predicate(id).get_predicate();
+            auto runtime_predicate = query_ctx->get_runtime_predicate(id).get_predicate(
+                    read_options.topn_filter_target_node_id);
 
             int32_t uid =
                     read_options.tablet_schema->column(runtime_predicate->column_id()).unique_id();

be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -523,7 +523,7 @@ Status SegmentIterator::_get_row_ranges_by_column_conditions() {
     RETURN_IF_ERROR(_apply_inverted_index());
 
     if (!_row_bitmap.isEmpty() &&
-        (_opts.use_topn_opt || !_opts.col_id_to_predicates.empty() ||
+        (!_opts.topn_filter_source_node_ids.empty() || !_opts.col_id_to_predicates.empty() ||
          _opts.delete_condition_predicates->num_of_column_predicate() > 0)) {
         RowRanges condition_row_ranges = RowRanges::create_single(_segment->num_rows());
         RETURN_IF_ERROR(_get_row_ranges_from_conditions(&condition_row_ranges));
@@ -572,7 +572,7 @@ Status SegmentIterator::_get_row_ranges_from_conditions(RowRanges* condition_row
         SCOPED_RAW_TIMER(&_opts.stats->block_conditions_filtered_zonemap_ns);
         RowRanges zone_map_row_ranges = RowRanges::create_single(num_rows());
         // second filter data by zone map
-        for (auto& cid : cids) {
+        for (const auto& cid : cids) {
             DCHECK(_opts.col_id_to_predicates.count(cid) > 0);
             if (!_segment->can_apply_predicate_safely(cid, _opts.col_id_to_predicates.at(cid).get(),
                                                       *_schema, _opts.io_ctx.reader_type)) {
@@ -600,16 +600,12 @@ Status SegmentIterator::_get_row_ranges_from_conditions(RowRanges* condition_row
         RowRanges::ranges_intersection(*condition_row_ranges, zone_map_row_ranges,
                                        condition_row_ranges);
 
-        if (_opts.use_topn_opt) {
-            SCOPED_RAW_TIMER(&_opts.stats->block_conditions_filtered_zonemap_ns);
+        if (!_opts.topn_filter_source_node_ids.empty()) {
             auto* query_ctx = _opts.runtime_state->get_query_ctx();
             for (int id : _opts.topn_filter_source_node_ids) {
-                if (!query_ctx->get_runtime_predicate(id).need_update()) {
-                    continue;
-                }
-
                 std::shared_ptr<doris::ColumnPredicate> runtime_predicate =
-                        query_ctx->get_runtime_predicate(id).get_predicate();
+                        query_ctx->get_runtime_predicate(id).get_predicate(
+                                _opts.topn_filter_target_node_id);
                 if (_segment->can_apply_predicate_safely(runtime_predicate->column_id(),
                                                          runtime_predicate.get(), *_schema,
                                                          _opts.io_ctx.reader_type)) {
@@ -1579,7 +1575,7 @@ Status SegmentIterator::_vec_init_lazy_materialization() {
 
     std::set<const ColumnPredicate*> delete_predicate_set {};
     _opts.delete_condition_predicates->get_all_column_predicate(delete_predicate_set);
-    for (const auto predicate : delete_predicate_set) {
+    for (const auto* const predicate : delete_predicate_set) {
         if (PredicateTypeTraits::is_range(predicate->type())) {
             _delete_range_column_ids.push_back(predicate->column_id());
         } else if (PredicateTypeTraits::is_bloom_filter(predicate->type())) {
@@ -1593,16 +1589,13 @@ Status SegmentIterator::_vec_init_lazy_materialization() {
     //  but runtime predicate will filter some rows and read more than N rows.
     // should add add for order by none-key column, since none-key column is not sorted and
     //  all rows should be read, so runtime predicate will reduce rows for topn node
-    if (_opts.use_topn_opt &&
+    if (!_opts.topn_filter_source_node_ids.empty() &&
         (_opts.read_orderby_key_columns == nullptr || _opts.read_orderby_key_columns->empty())) {
         for (int id : _opts.topn_filter_source_node_ids) {
-            if (!_opts.runtime_state->get_query_ctx()->get_runtime_predicate(id).need_update()) {
-                continue;
-            }
-
             auto& runtime_predicate =
                     _opts.runtime_state->get_query_ctx()->get_runtime_predicate(id);
-            _col_predicates.push_back(runtime_predicate.get_predicate().get());
+            _col_predicates.push_back(
+                    runtime_predicate.get_predicate(_opts.topn_filter_target_node_id).get());
         }
     }
 

be/src/olap/tablet_reader.cpp
@@ -233,14 +233,14 @@ Status TabletReader::_capture_rs_readers(const ReaderParams& read_params) {
     _reader_context.version = read_params.version;
     _reader_context.tablet_schema = _tablet_schema;
     _reader_context.need_ordered_result = need_ordered_result;
-    _reader_context.use_topn_opt = read_params.use_topn_opt;
     _reader_context.topn_filter_source_node_ids = read_params.topn_filter_source_node_ids;
+    _reader_context.topn_filter_target_node_id = read_params.topn_filter_target_node_id;
     _reader_context.read_orderby_key_reverse = read_params.read_orderby_key_reverse;
     _reader_context.read_orderby_key_limit = read_params.read_orderby_key_limit;
     _reader_context.filter_block_conjuncts = read_params.filter_block_conjuncts;
     _reader_context.return_columns = &_return_columns;
     _reader_context.read_orderby_key_columns =
-            _orderby_key_columns.size() > 0 ? &_orderby_key_columns : nullptr;
+            !_orderby_key_columns.empty() ? &_orderby_key_columns : nullptr;
     _reader_context.predicates = &_col_predicates;
     _reader_context.predicates_except_leafnode_of_andnode = &_col_preds_except_leafnode_of_andnode;
     _reader_context.value_predicates = &_value_col_predicates;
@@ -575,12 +575,11 @@ Status TabletReader::_init_conditions_param_except_leafnode_of_andnode(
         }
     }
 
-    if (read_params.use_topn_opt) {
-        for (int id : read_params.topn_filter_source_node_ids) {
-            auto& runtime_predicate =
-                    read_params.runtime_state->get_query_ctx()->get_runtime_predicate(id);
-            RETURN_IF_ERROR(runtime_predicate.set_tablet_schema(_tablet_schema));
-        }
+    for (int id : read_params.topn_filter_source_node_ids) {
+        auto& runtime_predicate =
+                read_params.runtime_state->get_query_ctx()->get_runtime_predicate(id);
+        RETURN_IF_ERROR(runtime_predicate.set_tablet_schema(read_params.topn_filter_target_node_id,
+                                                            _tablet_schema));
     }
     return Status::OK();
 }

commit 8799b04be6d651a72ff1199b813ea9ba6baf9a71

[fix](topn-filter) throw out of bound exception  when union has no child (#36469)
introduced by #35405

fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/TopnFilterPushDownVisitor.java
@@ -52,7 +52,8 @@ import java.util.Map;
  * push down topn filter
  */
 public class TopnFilterPushDownVisitor extends PlanVisitor<Boolean, PushDownContext> {
-    private TopnFilterContext topnFilterContext;
+
+    private final TopnFilterContext topnFilterContext;
 
     public TopnFilterPushDownVisitor(TopnFilterContext topnFilterContext) {
         this.topnFilterContext = topnFilterContext;
@@ -109,10 +110,13 @@ public class TopnFilterPushDownVisitor extends PlanVisitor<Boolean, PushDownCont
     @Override
     public Boolean visitPhysicalSetOperation(
             PhysicalSetOperation setOperation, PushDownContext ctx) {
-        boolean pushedDown = pushDownFilterToSetOperatorChild(setOperation, ctx, 0);
+        boolean pushedDown = false;
+        if (setOperation.arity() > 0) {
+            pushedDown = pushDownFilterToSetOperatorChild(setOperation, ctx, 0);
+        }
 
         if (setOperation instanceof Union) {
-            for (int i = 1; i < setOperation.children().size(); i++) {
+            for (int i = 1; i < setOperation.arity(); i++) {
                 // push down to the other children
                 pushedDown |= pushDownFilterToSetOperatorChild(setOperation, ctx, i);
             }

regression-test/suites/nereids_tpch_p0/tpch/topn-filter.groovy
@@ -156,4 +156,6 @@ suite("topn-filter") {
     }
 
     qt_groupingsets2 "select n_regionkey, sum(n_nationkey) from nation group by grouping sets((n_regionkey)) order by n_regionkey limit 2;"
+
+    sql """select * from (select 1 as id union all select 2 as id) tmp order by id limit 100"""
 }