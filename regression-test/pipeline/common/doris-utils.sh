ec462c699897b179379e24c3f049d4f76cc78349
stephen <hello-stephen@qq.com>
2024-06-20 16:53:22 +0800

    [ci](perf) print running pipeline tasks to debug

--- regression-test/pipeline/common/doris-utils.sh
+++ regression-test/pipeline/common/doris-utils.sh
@@ -733,3 +733,12 @@ function check_if_need_gcore() {
         echo "ERROR: unknown exit_flag ${exit_flag}" && return 1
     fi
 }
+
+function print_running_pipeline_tasks() {
+    webserver_port=$(get_doris_conf_value "${DORIS_HOME}"/be/conf/be.conf webserver_port)
+    echo "--------------------------------------------------"
+    echo "curl -m 10 http://127.0.0.1:${webserver_port}/api/running_pipeline_tasks/30"
+    echo ""
+    curl -m 10 "http://127.0.0.1:${webserver_port}/api/running_pipeline_tasks/30"
+    echo "--------------------------------------------------"
+}
