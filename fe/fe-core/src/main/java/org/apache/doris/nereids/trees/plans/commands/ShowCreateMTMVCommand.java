8f1936231dda4d251519f571836cc13e006f5a75
zhangdong <493738387@qq.com>
2024-06-21 11:56:58 +0800

    [enhance](mtmv)show create materialized view (#36188)
    - support show create materialized view
    - Prohibit show create table from specifying MTMV
    
     mysql> show create MATERIALIZED VIEW mv1\G
    *************************** 1. row ***************************
    Materialized View: mv1
    Create Materialized View: CREATE MATERIALIZED VIEW mv1
    (k1,k2)
    BUILD DEFERRED REFRESH AUTO ON MANUAL
    DUPLICATE KEY(`k1`, `k2`)
    DISTRIBUTED BY RANDOM BUCKETS 2
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1",
    "min_load_replica_num" = "-1",
    "is_being_synced" = "false",
    "storage_medium" = "hdd",
    "storage_format" = "V2",
    "inverted_index_storage_format" = "V2",
    "light_schema_change" = "true",
    "disable_auto_compaction" = "false",
    "enable_single_replica_compaction" = "false",
    "group_commit_interval_ms" = "10000",
    "group_commit_data_bytes" = "134217728"
    )
    AS SELECT * from user1
    1 row in set (0.01 sec)
    
    mysql> show create table rollup_mv;
    ERROR 1105 (HY000): errCode = 2, detailMessage = not support async
    materialized view, please use `show create materialized view`

---
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ShowCreateMTMVCommand.java
@@ -0,0 +1,49 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.trees.plans.commands;
+
+import org.apache.doris.nereids.trees.plans.PlanType;
+import org.apache.doris.nereids.trees.plans.commands.info.ShowCreateMTMVInfo;
+import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
+import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.qe.StmtExecutor;
+
+import java.util.Objects;
+
+/**
+ * resume mtmv
+ */
+public class ShowCreateMTMVCommand extends Command implements ForwardWithSync, NotAllowFallback {
+    private final ShowCreateMTMVInfo showCreateMTMVInfo;
+
+    public ShowCreateMTMVCommand(ShowCreateMTMVInfo showCreateMTMVInfo) {
+        super(PlanType.SHOW_CREATE_MTMV_COMMAND);
+        this.showCreateMTMVInfo = Objects.requireNonNull(showCreateMTMVInfo, "require showCreateMTMVInfo object");
+    }
+
+    @Override
+    public void run(ConnectContext ctx, StmtExecutor executor) throws Exception {
+        showCreateMTMVInfo.analyze(ctx);
+        showCreateMTMVInfo.run(executor);
+    }
+
+    @Override
+    public <R, C> R accept(PlanVisitor<R, C> visitor, C context) {
+        return visitor.visitShowCreateMTMVCommand(this, context);
+    }
+}
