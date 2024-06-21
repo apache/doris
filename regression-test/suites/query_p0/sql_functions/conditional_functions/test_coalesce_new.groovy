e51e690996adb293b551d2b1b35568d06e21012b
Yulei-Yang <yulei.yang0699@gmail.com>
2024-06-21 20:16:19 +0800

    [fix](nereids) fix wrong resut of function ifnull/coalesce caused by mixed use multiple columns with type of datetime/datetimev2/date/datev2 of function coalesce when enable nereids

--- regression-test/suites/query_p0/sql_functions/conditional_functions/test_coalesce_new.groovy
+++ regression-test/suites/query_p0/sql_functions/conditional_functions/test_coalesce_new.groovy
@@ -60,6 +60,40 @@ suite("test_coalesce_new") {
     """
     assertEquals(result12.size(), 2);
 
+    //test enable_date_conversion=true and enable_nereids
+    sql """
+        admin set frontend config ("enable_date_conversion"="true")
+        """
+    sql """
+        SET enable_nereids_planner=true
+        """
+    sql """
+        SET enable_fallback_to_original_planner=false
+        """
+    def result13 = try_sql """
+        select dt from test_cls where coalesce (dt, str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-01'
+    """
+    assertEquals(result13.size(), 2);
+    def result14 = try_sql """
+        select dt from test_cls where coalesce (dt, dt, str_to_date(concat('202306', '01'), '%Y%m%d')) < '2023-06-01'
+    """
+    assertEquals(result14.size(), 1);
+    def result15 = try_sql """
+        select dt from test_cls where coalesce (dt, str_to_date(concat('202306', '01'), '%Y%m%d'), str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-02'
+    """
+    assertEquals(result15.size(), 1);
+    def result16 = try_sql """
+        select dt from test_cls where ifnull(dt, str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-01'
+    """
+    assertEquals(result16.size(), 2);
+    def result17 = try_sql """
+        select dt from test_cls where coalesce(str_to_date(concat('202306', '01'), '%Y%m%d'),dt) < '2023-06-02'
+    """
+    assertEquals(result17.size(), 3);
+    def result18 = try_sql """
+        select dt from test_cls where ifnull(str_to_date(concat('202306', '01'), '%Y%m%d'),dt) < '2023-06-03'
+    """
+    assertEquals(result18.size(), 3);
 
     // test parameter:datetimev2, datev2
     sql """
@@ -105,6 +139,37 @@ suite("test_coalesce_new") {
     """
     assertEquals(result22.size(), 2);
 
+    //test enable_date_conversion=true and enable_nereids
+    sql """
+        SET enable_nereids_planner=true
+        """
+    sql """
+        SET enable_fallback_to_original_planner=false
+        """
+    def result23 = try_sql """
+        select dt from test_cls_dtv2 where coalesce (dt, str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-01'
+    """
+    assertEquals(result23.size(), 2);
+    def result24 = try_sql """
+        select dt from test_cls_dtv2 where coalesce (dt, dt, str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-02'
+    """
+    assertEquals(result24.size(), 1);
+    def result25 = try_sql """
+        select dt from test_cls_dtv2 where coalesce (dt, str_to_date(concat('202306', '01'), '%Y%m%d'), str_to_date(concat('202306', '01'), '%Y%m%d')) >= '2023-06-02'
+    """
+    assertEquals(result25.size(), 1);
+    def result26 = try_sql """
+        select dt from test_cls_dtv2 where ifnull(dt, str_to_date(concat('202306', '01'), '%Y%m%d')) < '2023-06-01'
+    """
+    assertEquals(result26.size(), 1);
+    def result27 = try_sql """
+        select dt from test_cls_dtv2 where coalesce(str_to_date(concat('202306', '01'), '%Y%m%d'),dt) < '2023-06-01'
+    """
+    assertEquals(result27.size(), 0);
+    def result28 = try_sql """
+        select dt from test_cls_dtv2 where ifnull(str_to_date(concat('202306', '01'), '%Y%m%d'),dt) < '2023-06-01'
+    """
+    assertEquals(result28.size(), 0);
 
     sql """
         drop table test_cls
