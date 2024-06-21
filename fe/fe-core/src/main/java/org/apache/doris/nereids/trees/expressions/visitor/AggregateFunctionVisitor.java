0a9d938300b38d87c39f92ceb289d4fab222da68
HappenLee <happenlee@hotmail.com>
2024-06-21 11:00:48 +0800

    [Fix](func) CoreDump and Result Error in percentile function

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/AggregateFunctionVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/AggregateFunctionVisitor.java
@@ -204,7 +204,7 @@ public interface AggregateFunctionVisitor<R, C> {
     }
 
     default R visitPercentile(Percentile percentile, C context) {
-        return visitAggregateFunction(percentile, context);
+        return visitNullableAggregateFunction(percentile, context);
     }
 
     default R visitPercentileApprox(PercentileApprox percentileApprox, C context) {
