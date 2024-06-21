02e5c6f59b861ce1e90ab52256bfe32b68c12a1b
zhangdong <493738387@qq.com>
2024-06-21 11:56:58 +0800

    [enhance](mtmv)show create materialized view (#36188)
    - support show create materialized view
    - Prohibit show create table from specifying MTMV
      
     mysql> show create MATERIALIZED VIEW mv1\G
    *************************** 1. row ***************************
    Materialized View: mv1
    Create Materialized View: CREATE MATERIALIZED VIEW mv1
    (k1,k2)
    BUILD DEFERRED REFRESH AUTO ON MANUAL
    DUPLICATE KEY(`k1`, `k2`)
    DISTRIBUTED BY RANDOM BUCKETS 2
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1",
    "min_load_replica_num" = "-1",
    "is_being_synced" = "false",
    "storage_medium" = "hdd",
    "storage_format" = "V2",
    "inverted_index_storage_format" = "V2",
    "light_schema_change" = "true",
    "disable_auto_compaction" = "false",
    "enable_single_replica_compaction" = "false",
    "group_commit_interval_ms" = "10000",
    "group_commit_data_bytes" = "134217728"
    )
    AS SELECT * from user1
    1 row in set (0.01 sec)
    
    mysql> show create table rollup_mv;
    ERROR 1105 (HY000): errCode = 2, detailMessage = not support async
    materialized view, please use `show create materialized view`

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ShowCreateMTMVStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ShowCreateMTMVStmt.java
@@ -22,7 +22,6 @@ import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.MTMV;
 import org.apache.doris.catalog.ScalarType;
 import org.apache.doris.catalog.TableIf;
-import org.apache.doris.catalog.View;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
@@ -30,44 +29,18 @@ import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.ShowResultSetMetaData;
 
-// SHOW CREATE TABLE statement.
-public class ShowCreateTableStmt extends ShowStmt {
+// SHOW CREATE Materialized View statement.
+public class ShowCreateMTMVStmt extends ShowStmt {
     private static final ShowResultSetMetaData META_DATA =
-            ShowResultSetMetaData.builder()
-                    .addColumn(new Column("Table", ScalarType.createVarchar(20)))
-                    .addColumn(new Column("Create Table", ScalarType.createVarchar(30)))
-                    .build();
-
-    private static final ShowResultSetMetaData VIEW_META_DATA =
-            ShowResultSetMetaData.builder()
-                    .addColumn(new Column("View", ScalarType.createVarchar(20)))
-                    .addColumn(new Column("Create View", ScalarType.createVarchar(30)))
-                    .addColumn(new Column("character_set_client", ScalarType.createVarchar(30)))
-                    .addColumn(new Column("collation_connection", ScalarType.createVarchar(30)))
-                    .build();
-
-    private static final ShowResultSetMetaData MATERIALIZED_VIEW_META_DATA =
             ShowResultSetMetaData.builder()
                     .addColumn(new Column("Materialized View", ScalarType.createVarchar(20)))
                     .addColumn(new Column("Create Materialized View", ScalarType.createVarchar(30)))
                     .build();
 
     private TableName tbl;
-    private boolean isView;
-    private boolean needBriefDdl;
 
-    public ShowCreateTableStmt(TableName tbl) {
-        this(tbl, false, false);
-    }
-
-    public ShowCreateTableStmt(TableName tbl, boolean needBriefDdl) {
-        this(tbl, false, needBriefDdl);
-    }
-
-    public ShowCreateTableStmt(TableName tbl, boolean isView, boolean needBriefDdl) {
+    public ShowCreateMTMVStmt(TableName tbl) {
         this.tbl = tbl;
-        this.isView = isView;
-        this.needBriefDdl = needBriefDdl;
     }
 
 
@@ -83,22 +56,6 @@ public class ShowCreateTableStmt extends ShowStmt {
         return tbl.getTbl();
     }
 
-    public boolean isView() {
-        return isView;
-    }
-
-    public boolean isNeedBriefDdl() {
-        return needBriefDdl;
-    }
-
-    public static ShowResultSetMetaData getViewMetaData() {
-        return VIEW_META_DATA;
-    }
-
-    public static ShowResultSetMetaData getMaterializedViewMetaData() {
-        return MATERIALIZED_VIEW_META_DATA;
-    }
-
     @Override
     public void analyze(Analyzer analyzer) throws AnalysisException {
         if (tbl == null) {
@@ -110,30 +67,20 @@ public class ShowCreateTableStmt extends ShowStmt {
                 .getCatalogOrAnalysisException(tbl.getCtl())
                 .getDbOrAnalysisException(tbl.getDb()).getTableOrAnalysisException(tbl.getTbl());
 
-        if (tableIf instanceof MTMV) {
-            ErrorReport.reportAnalysisException("not support async materialized view, "
-                    + "please use `show create materialized view`");
-        }
-
-        PrivPredicate wanted;
-        if (tableIf instanceof View) {
-            wanted = PrivPredicate.SHOW_VIEW;
-        } else {
-            wanted = PrivPredicate.SHOW;
+        if (!(tableIf instanceof MTMV)) {
+            ErrorReport.reportAnalysisException("only support async materialized view");
         }
 
         if (!Env.getCurrentEnv().getAccessManager().checkTblPriv(ConnectContext.get(), tbl.getCtl(), tbl.getDb(),
-                tbl.getTbl(), wanted)) {
-            ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "SHOW CREATE TABLE",
-                                                ConnectContext.get().getQualifiedUser(),
-                                                ConnectContext.get().getRemoteIP(),
-                                                tbl.getDb() + ": " + tbl.getTbl());
+                tbl.getTbl(), PrivPredicate.SHOW)) {
+            ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLE_ACCESS_DENIED_ERROR,
+                    PrivPredicate.SHOW.getPrivs().toString(), getTable());
         }
     }
 
     @Override
     public String toSql() {
-        return "SHOW CREATE TABLE " + tbl;
+        return "SHOW CREATE MATERIALIZED VIEW " + tbl;
     }
 
     @Override
