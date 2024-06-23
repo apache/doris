2971efd194f69e076d7e1dc091d070e13eef23cc
Kang <kxiao.tiger@gmail.com>
2024-06-23 19:07:13 +0800

    [release](version) change branch-2.0 version to 2.0.12 (#36626)

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -30,7 +30,7 @@ set -eo pipefail
 build_version_prefix="doris"
 build_version_major=2
 build_version_minor=0
-build_version_patch=11
+build_version_patch=12
 build_version_rc_version=""
 
 if [[ -z "${build_version_rc_version}" ]]; then
