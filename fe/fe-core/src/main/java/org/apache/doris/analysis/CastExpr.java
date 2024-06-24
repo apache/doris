d2b0cd5c18f472bba7bb5d1e46c4ddee0bc4ecb8
amory <wangqiannan@selectdb.com>
2024-06-24 20:00:45 +0800

    [feature](cast) Support cast complex type to json (#36548)
    support cast complex type to json which we can use array_agg/map_agg
    result into json_object

--- fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/CastExpr.java
@@ -153,6 +153,10 @@ public class CastExpr extends Expr {
             Type from = getActualArgTypes(collectChildReturnTypes())[0];
             Type to = getActualType(type);
             NullableMode nullableMode = TYPE_NULLABLE_MODE.get(Pair.of(from, to));
+            // for complex type cast to jsonb we make ret is always nullable
+            if (from.isComplexType() && type.isJsonbType()) {
+                nullableMode = Function.NullableMode.ALWAYS_NULLABLE;
+            }
             Preconditions.checkState(nullableMode != null,
                     "cannot find nullable node for cast from " + from + " to " + to);
             fn = new Function(new FunctionName(getFnName(type)), Lists.newArrayList(e.type), type,
