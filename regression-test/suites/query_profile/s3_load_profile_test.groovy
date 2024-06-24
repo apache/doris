dbf195345497eadc288337f16e43af70e25e887b
zhiqiang <seuhezhiqiang@163.com>
2024-06-24 21:40:31 +0800

    [fix](profile) Fix regresstion s3_load_profile_test case. (#36709)

--- regression-test/suites/query_profile/s3_load_profile_test.groovy
+++ regression-test/suites/query_profile/s3_load_profile_test.groovy
@@ -175,16 +175,9 @@ PROPERTIES (
     logger.info("profileDataString:" + profileDataString)
     def taskStateIdx = profileDataString.indexOf("Task&nbsp;&nbsp;State:&nbsp;&nbsp;FINISHED")
     assertFalse(taskStateIdx == -1)
-    def fragmentIdx = profileDataString.indexOf("RowsReturned")
-    assertFalse(fragmentIdx == -1)
     def executionProfileIdx = profileDataString.indexOf("Execution&nbsp;&nbsp;Profile")
     assertFalse(executionProfileIdx == -1)
-    if (!profileDataString.contains("NumScanners") || !profileDataString.contains("BlocksProduced")
-        || !profileDataString.contains("RowsProduced") || !profileDataString.contains("RowsRead")) {
-            logger.info("profileDataString:" + profileDataString)
-    }
     assertTrue(profileDataString.contains("NumScanners"))
-    assertTrue(profileDataString.contains("BlocksProduced"))
     assertTrue(profileDataString.contains("RowsProduced"))
     assertTrue(profileDataString.contains("RowsRead"))
 }
