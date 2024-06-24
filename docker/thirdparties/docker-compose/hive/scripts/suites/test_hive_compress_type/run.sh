5be9efaf1e0d61e36f26de3dd9fe7cecbd9ebca3
morningman <morningman@163.com>
2024-06-24 18:31:12 +0800

    2

---
+++ docker/thirdparties/docker-compose/hive/scripts/suites/test_hive_compress_type/run.sh
@@ -0,0 +1,19 @@
+set -x
+
+if [[ ! -d "/mnt/scripts/suites/test_hive_compress_type/data" ]]; then
+    echo "/mnt/scripts/suites/test_hive_compress_type/data does not exist"
+    cd /mnt/scripts/suites/test_hive_compress_type/
+    curl -O https://s3BucketName.s3Endpoint/regression/datalake/pipeline_data/test_hive_compress_type/data.tar.gz
+    cd -
+else
+    echo "/mnt/scripts/suites/test_hive_compress_type/data exist, continue !"
+fi
+
+## mkdir and put data to hdfs
+cd /mnt/scripts/suites/test_hive_compress_type/ && tar xzf data.tar.gz
+hadoop fs -mkdir -p /user/doris/suites/test_hive_compress_type/
+hadoop fs -put /mnt/scripts/suites/test_hive_compress_type/data/* /user/doris/suites/test_hive_compress_type/
+
+# create table
+hive -f /mnt/scripts/suites/test_hive_compress_type/create_table.hql
+
