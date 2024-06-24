a8c3af161f3825653fdec9cffc5bf29d4175cdd3
zhiqiang-hhhh <seuhezhiqiang@163.com>
2024-06-24 11:07:37 +0800

    for regresstion test

--- regression-test/suites/query_p0/sql_functions/string_functions/test_compress_varchar.groovy
+++ regression-test/suites/query_p0/sql_functions/string_functions/test_compress_varchar.groovy
@@ -27,54 +27,67 @@ suite("test_compress_varchar") {
     sql """
         insert into compress_varchar values(1,'a','ab','abc','abcd','abcde','abcdef','abcdefg','abcdefgh','abcdefghi','abcdefghij','abcdefghijk','abcdefghijkl','abcdefghijklm','abcdefghijklmn','abcdefghijklmno');
     """
+    sql """
+        insert into compress_varchar values(2,'','','','','','','','','','','','','','','');
+    """
+    sql """
+        insert into compress_varchar values(3,'b','b','b','b','b','b','b','b','b','b','b','b','b','b','b');
+    """
     qt_select_1 """
-        select vc1, compress_as_smallint(vc1), decompress_varchar(compress_as_smallint(vc1)) = vc1 from compress_varchar;
+        select vc1, compress_as_smallint(vc1), decompress_varchar(compress_as_smallint(vc1)) = vc1 from compress_varchar order by rowid;
     """
     qt_select_2 """
-        select vc2, compress_as_int(vc2), decompress_varchar(compress_as_int(vc2)) = vc2 from compress_varchar;
+        select vc2, compress_as_int(vc2), decompress_varchar(compress_as_int(vc2)) = vc2 from compress_varchar order by rowid;
     """
     qt_select_3 """
-        select vc3, compress_as_int(vc3), decompress_varchar(compress_as_int(vc3)) = vc3 from compress_varchar;
+        select vc3, compress_as_int(vc3), decompress_varchar(compress_as_int(vc3)) = vc3 from compress_varchar order by rowid;
     """
     qt_select_4 """
-        select vc4, compress_as_bigint(vc4), decompress_varchar(compress_as_bigint(vc4)) =vc4 from compress_varchar;
+        select vc4, compress_as_bigint(vc4), decompress_varchar(compress_as_bigint(vc4)) =vc4 from compress_varchar order by rowid;
     """
     qt_select_5 """
-        select vc5, compress_as_bigint(vc5), decompress_varchar(compress_as_bigint(vc5)) = vc5 from compress_varchar;
+        select vc5, compress_as_bigint(vc5), decompress_varchar(compress_as_bigint(vc5)) = vc5 from compress_varchar order by rowid;
     """
     qt_select_6 """
-        select vc6, compress_as_bigint(vc6), decompress_varchar(compress_as_bigint(vc6)) = vc6 from compress_varchar;
+        select vc6, compress_as_bigint(vc6), decompress_varchar(compress_as_bigint(vc6)) = vc6 from compress_varchar order by rowid;
     """
     qt_select_7 """
-        select vc7, compress_as_bigint(vc7), decompress_varchar(compress_as_bigint(vc7)) = vc7 from compress_varchar;
+        select vc7, compress_as_bigint(vc7), decompress_varchar(compress_as_bigint(vc7)) = vc7 from compress_varchar order by rowid;
     """
     qt_select_8 """
-        select vc8, compress_as_largeint(vc8), decompress_varchar(compress_as_largeint(vc8)) = vc8 from compress_varchar;
+        select vc8, compress_as_largeint(vc8), decompress_varchar(compress_as_largeint(vc8)) = vc8 from compress_varchar order by rowid;
     """
     qt_select_9 """
-        select vc9, compress_as_largeint(vc9), decompress_varchar(compress_as_largeint(vc9)) = vc9 from compress_varchar;
+        select vc9, compress_as_largeint(vc9), decompress_varchar(compress_as_largeint(vc9)) = vc9 from compress_varchar order by rowid;
     """
     qt_select_10 """
-        select vc10, compress_as_largeint(vc10), decompress_varchar(compress_as_largeint(vc10)) = vc10 from compress_varchar;
+        select vc10, compress_as_largeint(vc10), decompress_varchar(compress_as_largeint(vc10)) = vc10 from compress_varchar order by rowid;
     """
     qt_select_11 """
-        select vc11, compress_as_largeint(vc11), decompress_varchar(compress_as_largeint(vc11)) = vc11 from compress_varchar;
+        select vc11, compress_as_largeint(vc11), decompress_varchar(compress_as_largeint(vc11)) = vc11 from compress_varchar order by rowid;
     """
     qt_select_12 """
-        select vc12, compress_as_largeint(vc12), decompress_varchar(compress_as_largeint(vc12)) = vc12 from compress_varchar;
+        select vc12, compress_as_largeint(vc12), decompress_varchar(compress_as_largeint(vc12)) = vc12 from compress_varchar order by rowid;
     """
     qt_select_13 """
-        select vc13, compress_as_largeint(vc13), decompress_varchar(compress_as_largeint(vc13)) = vc13 from compress_varchar;
+        select vc13, compress_as_largeint(vc13), decompress_varchar(compress_as_largeint(vc13)) = vc13 from compress_varchar order by rowid;
     """
     qt_select_14 """
-        select vc14, compress_as_largeint(vc14), decompress_varchar(compress_as_largeint(vc14)) = vc14 from compress_varchar;
+        select vc14, compress_as_largeint(vc14), decompress_varchar(compress_as_largeint(vc14)) = vc14 from compress_varchar order by rowid;
     """
     qt_select_15 """
-        select vc15, compress_as_largeint(vc15), decompress_varchar(compress_as_largeint(vc15)) = vc15 from compress_varchar;
+        select vc15, compress_as_largeint(vc15), decompress_varchar(compress_as_largeint(vc15)) = vc15 from compress_varchar order by rowid;
     """
     qt_select_16 """
         select decompress_varchar(cast('8321' as SMALLINT))
     """
+    qt_select_17 """
+        select decompress_varchar(compress_as_smallint('')) == '';
+    """
+    qt_select_18 """
+        select decompress_varchar(compress_as_smallint('\0')) == '\0';
+    """
+
     test{
         sql """
             select compress_as_largeint("abcdefghijklmnop");
