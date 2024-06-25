810065470efd35400292a8e88eaec12c5468150d
amorynan <wangqiannan@selectdb.com>
2024-06-25 17:46:30 +0800

    use sMaxKeyId instead of 0 to present empty key

--- be/src/util/jsonb_utils.h
+++ be/src/util/jsonb_utils.h
@@ -191,7 +191,8 @@ private:
             if (iter->klen()) {
                 string_to_json(iter->getKeyStr(), iter->klen());
             } else {
-                if (iter->getKeyId() == 0) {
+                // NOTE: we use sMaxKeyId to represent an empty key. see jsonb_writer.h
+                if (iter->getKeyId() == JsonbKeyValue::sMaxKeyId) {
                     string_to_json(nullptr, 0);
                 } else {
                     os_.write(iter->getKeyId());
