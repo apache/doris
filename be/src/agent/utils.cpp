apache/doris

commit 3757071afa8f75e6717ca8bb0232a9526d4d2d04

[chore](thrift exception) do not log message in thrift exception (#34994)

be/src/agent/utils.cpp
@@ -90,6 +90,9 @@ Status MasterServerClient::finish_task(const TFinishTaskRequest& request, TMaste
         try {
             client->finishTask(*result, request);
         } catch (TTransportException& e) {
+#ifdef ADDRESS_SANITIZER
+            return Status::RpcError<false>("Master client finish task failed due to {}", e.what());
+#else
             LOG(WARNING) << "master client, retry finishTask: " << e.what();
             client_status = client.reopen(config::thrift_rpc_timeout_ms);
             if (!client_status.ok()) {
@@ -97,9 +100,10 @@ Status MasterServerClient::finish_task(const TFinishTaskRequest& request, TMaste
                              << "host=" << _master_info.network_address.hostname
                              << ", port=" << _master_info.network_address.port
                              << ", code=" << client_status.code();
-                return Status::InternalError("Master client finish task failed");
+                return Status::RpcError("Master client finish task failed");
             }
             client->finishTask(*result, request);
+#endif
         }
     } catch (std::exception& e) {
         RETURN_IF_ERROR(client.reopen(config::thrift_rpc_timeout_ms));
@@ -129,6 +133,9 @@ Status MasterServerClient::report(const TReportRequest& request, TMasterResult* 
         try {
             client->report(*result, request);
         } catch (TTransportException& e) {
+#ifdef ADDRESS_SANITIZER
+            return Status::RpcError<false>("Master client report failed due to {}", e.what());
+#else
             TTransportException::TTransportExceptionType type = e.getType();
             if (type != TTransportException::TTransportExceptionType::TIMED_OUT) {
                 // if not TIMED_OUT, retry
@@ -150,6 +157,7 @@ Status MasterServerClient::report(const TReportRequest& request, TMasterResult* 
                 LOG(WARNING) << "fail to report to master: " << e.what();
                 return Status::InternalError("Fail to report to master");
             }
+#endif
         }
     } catch (std::exception& e) {
         RETURN_IF_ERROR(client.reopen(config::thrift_rpc_timeout_ms));

be/src/util/thrift_rpc_helper.cpp
@@ -75,8 +75,9 @@ Status ThriftRpcHelper::rpc(const std::string& ip, const int32_t port,
         } catch (apache::thrift::transport::TTransportException& e) {
             std::cerr << "thrift error, reason=" << e.what();
 #ifdef ADDRESS_SANITIZER
-            return Status::RpcError("failed to call frontend service, FE address={}:{}, reason: {}",
-                                    ip, port, e.what());
+            return Status::RpcError<false>(
+                    "failed to call frontend service, FE address={}:{}, reason: {}", ip, port,
+                    e.what());
 #else
             LOG(WARNING) << "retrying call frontend service after "
                          << config::thrift_client_retry_interval_ms << " ms, address=" << address

commit 066300dbd9ae2102f035af9f381ea38e0c4e27d3

[chore](client) Reopen client if exception is catched (#36808)
It was introduced by #34994.

be/src/agent/utils.cpp
@@ -91,19 +91,19 @@ Status MasterServerClient::finish_task(const TFinishTaskRequest& request, TMaste
             client->finishTask(*result, request);
         } catch (TTransportException& e) {
 #ifdef ADDRESS_SANITIZER
-            return Status::RpcError<false>("Master client finish task failed due to {}", e.what());
-#else
             LOG(WARNING) << "master client, retry finishTask: " << e.what();
+#endif
             client_status = client.reopen(config::thrift_rpc_timeout_ms);
             if (!client_status.ok()) {
+#ifdef ADDRESS_SANITIZER
                 LOG(WARNING) << "fail to get master client from cache. "
                              << "host=" << _master_info.network_address.hostname
                              << ", port=" << _master_info.network_address.port
                              << ", code=" << client_status.code();
+#endif
                 return Status::RpcError("Master client finish task failed");
             }
             client->finishTask(*result, request);
-#endif
         }
     } catch (std::exception& e) {
         RETURN_IF_ERROR(client.reopen(config::thrift_rpc_timeout_ms));
@@ -133,20 +133,21 @@ Status MasterServerClient::report(const TReportRequest& request, TMasterResult* 
         try {
             client->report(*result, request);
         } catch (TTransportException& e) {
-#ifdef ADDRESS_SANITIZER
-            return Status::RpcError<false>("Master client report failed due to {}", e.what());
-#else
             TTransportException::TTransportExceptionType type = e.getType();
             if (type != TTransportException::TTransportExceptionType::TIMED_OUT) {
+#ifdef ADDRESS_SANITIZER
                 // if not TIMED_OUT, retry
                 LOG(WARNING) << "master client, retry finishTask: " << e.what();
+#endif
 
                 client_status = client.reopen(config::thrift_rpc_timeout_ms);
                 if (!client_status.ok()) {
+#ifdef ADDRESS_SANITIZER
                     LOG(WARNING) << "fail to get master client from cache. "
                                  << "host=" << _master_info.network_address.hostname
                                  << ", port=" << _master_info.network_address.port
                                  << ", code=" << client_status.code();
+#endif
                     return Status::InternalError("Fail to get master client from cache");
                 }
 
@@ -154,10 +155,11 @@ Status MasterServerClient::report(const TReportRequest& request, TMasterResult* 
             } else {
                 // TIMED_OUT exception. do not retry
                 // actually we don't care what FE returns.
+#ifdef ADDRESS_SANITIZER
                 LOG(WARNING) << "fail to report to master: " << e.what();
+#endif
                 return Status::InternalError("Fail to report to master");
             }
-#endif
         }
     } catch (std::exception& e) {
         RETURN_IF_ERROR(client.reopen(config::thrift_rpc_timeout_ms));