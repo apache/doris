0c5ce6e054666033037609e02023395783255c9e
AlexYue <yj976240184@gmail.com>
2024-06-26 17:07:02 +0800

    [enhance](Azure) Check delete operation's response on Azure Blob Storage (#36800)
    We should check if the delete operation is successful or not.

--- be/src/io/fs/azure_obj_storage_client.cpp
+++ be/src/io/fs/azure_obj_storage_client.cpp
@@ -25,6 +25,7 @@
 #include <azure/core/http/http_status_code.hpp>
 #include <azure/core/io/body_stream.hpp>
 #include <azure/storage/blobs.hpp>
+#include <azure/storage/blobs/blob_batch.hpp>
 #include <azure/storage/blobs/blob_client.hpp>
 #include <azure/storage/blobs/blob_container_client.hpp>
 #include <azure/storage/blobs/rest_client.hpp>
@@ -32,10 +33,14 @@
 #include <azure/storage/common/storage_exception.hpp>
 #include <exception>
 #include <iterator>
+#include <ranges>
 
+#include "common/exception.h"
 #include "common/logging.h"
 #include "common/status.h"
 #include "io/fs/obj_storage_client.h"
+
+using namespace Azure::Storage::Blobs;
 
 namespace {
 std::string wrap_object_storage_path_msg(const doris::io::ObjectStoragePathOptions& opts) {
@@ -77,6 +82,46 @@ ObjectStorageResponse do_azure_client_call(Func f, const ObjectStoragePathOption
     }
     return {};
 }
+
+struct AzureBatchDeleter {
+    AzureBatchDeleter(BlobContainerClient* client, const ObjectStoragePathOptions& opts)
+            : _client(client), _batch(client->CreateBatch()), _opts(opts) {}
+    // Submit one blob to be deleted in `AzureBatchDeleter::execute`
+    void delete_blob(const std::string& blob_name) {
+        deferred_resps.emplace_back(_batch.DeleteBlob(blob_name));
+    }
+    ObjectStorageResponse execute() {
+        if (deferred_resps.empty()) {
+            return {};
+        }
+        auto resp = do_azure_client_call([&]() { _client->SubmitBatch(_batch); }, _opts);
+        if (resp.status.code != ErrorCode::OK) {
+            return resp;
+        }
+
+        auto get_defer_response = [](const auto& defer) {
+            // DeferredResponse<Models::DeleteBlobResult> might throw exception
+            if (!defer.GetResponse().Value.Deleted) {
+                throw Exception(Status::IOError<false>("Batch delete failed"));
+            }
+        };
+        for (auto&& defer_response : deferred_resps) {
+            auto response =
+                    do_azure_client_call([&]() { get_defer_response(defer_response); }, _opts);
+            if (response.status.code != ErrorCode::OK) {
+                return response;
+            }
+        }
+
+        return {};
+    }
+
+private:
+    BlobContainerClient* _client;
+    BlobContainerBatch _batch;
+    const ObjectStoragePathOptions& _opts;
+    std::vector<Azure::Storage::DeferredResponse<Models::DeleteBlobResult>> deferred_resps;
+};
 
 // Azure would do nothing
 ObjectStorageUploadResponse AzureObjStorageClient::create_multipart_upload(
@@ -136,7 +181,7 @@ ObjectStorageResponse AzureObjStorageClient::complete_multipart_upload(
 
 ObjectStorageHeadResponse AzureObjStorageClient::head_object(const ObjectStoragePathOptions& opts) {
     try {
-        Azure::Storage::Blobs::Models::BlobProperties properties =
+        Models::BlobProperties properties =
                 _client->GetBlockBlobClient(opts.key).GetProperties().Value;
         return {.file_size = properties.BlobSize};
     } catch (Azure::Core::RequestFailedException& e) {
@@ -166,7 +211,7 @@ ObjectStorageResponse AzureObjStorageClient::get_object(const ObjectStoragePathO
     auto client = _client->GetBlockBlobClient(opts.key);
     return do_azure_client_call(
             [&]() {
-                Azure::Storage::Blobs::DownloadBlobToOptions download_opts;
+                DownloadBlobToOptions download_opts;
                 Azure::Core::Http::HttpRange range {static_cast<int64_t>(offset), bytes_read};
                 download_opts.Range = range;
                 auto resp = client.DownloadTo(reinterpret_cast<uint8_t*>(buffer), bytes_read,
@@ -178,7 +223,7 @@ ObjectStorageResponse AzureObjStorageClient::get_object(const ObjectStoragePathO
 
 ObjectStorageResponse AzureObjStorageClient::list_objects(const ObjectStoragePathOptions& opts,
                                                           std::vector<FileInfo>* files) {
-    auto get_file_file = [&](Azure::Storage::Blobs::ListBlobsPagedResponse& resp) {
+    auto get_file_file = [&](ListBlobsPagedResponse& resp) {
         std::ranges::transform(resp.Blobs, std::back_inserter(*files), [](auto&& blob_item) {
             return FileInfo {
                     .file_name = blob_item.Name, .file_size = blob_item.BlobSize, .is_file = true};
@@ -186,7 +231,7 @@ ObjectStorageResponse AzureObjStorageClient::list_objects(const ObjectStoragePat
     };
     return do_azure_client_call(
             [&]() {
-                Azure::Storage::Blobs::ListBlobsOptions list_opts;
+                ListBlobsOptions list_opts;
                 list_opts.Prefix = opts.prefix;
                 auto resp = _client->ListBlobs(list_opts);
                 get_file_file(resp);
@@ -210,16 +255,15 @@ ObjectStorageResponse AzureObjStorageClient::delete_objects(const ObjectStorageP
     auto end = std::end(objs);
 
     while (begin != end) {
-        auto batch = _client->CreateBatch();
-        auto chunkEnd = begin;
-        std::advance(chunkEnd, std::min(BlobBatchMaxOperations,
-                                        static_cast<size_t>(std::distance(begin, end))));
-        for (auto it = begin; it != chunkEnd; ++it) {
-            batch.DeleteBlob(*it);
-        }
-        begin = chunkEnd;
-        auto resp = do_azure_client_call([&]() { _client->SubmitBatch(batch); }, opts);
-        if (resp.status.code != ErrorCode::OK) {
+        auto deleter = AzureBatchDeleter(_client.get(), opts);
+        auto chunk_end = begin;
+        std::advance(chunk_end, std::min(BlobBatchMaxOperations,
+                                         static_cast<size_t>(std::distance(begin, end))));
+
+        std::ranges::for_each(std::ranges::subrange(begin, chunk_end),
+                              [&](const std::string& obj) { deleter.delete_blob(obj); });
+        begin = chunk_end;
+        if (auto resp = deleter.execute(); resp.status.code != ErrorCode::OK) {
             return resp;
         }
     }
@@ -227,32 +271,42 @@ ObjectStorageResponse AzureObjStorageClient::delete_objects(const ObjectStorageP
 }
 
 ObjectStorageResponse AzureObjStorageClient::delete_object(const ObjectStoragePathOptions& opts) {
-    return do_azure_client_call([&]() { _client->DeleteBlob(opts.key); }, opts);
+    return do_azure_client_call(
+            [&]() {
+                auto resp = _client->DeleteBlob(opts.key);
+                if (!resp.Value.Deleted) {
+                    throw Exception(Status::IOError<false>("Delete azure blob failed"));
+                }
+            },
+            opts);
 }
 
 ObjectStorageResponse AzureObjStorageClient::delete_objects_recursively(
         const ObjectStoragePathOptions& opts) {
-    Azure::Storage::Blobs::ListBlobsOptions list_opts;
+    ListBlobsOptions list_opts;
     list_opts.Prefix = opts.prefix;
     list_opts.PageSizeHint = BlobBatchMaxOperations;
+    auto delete_func = [&](const std::vector<Models::BlobItem>& blobs) -> ObjectStorageResponse {
+        auto deleter = AzureBatchDeleter(_client.get(), opts);
+        auto batch = _client->CreateBatch();
+        for (auto&& blob_item : blobs) {
+            deleter.delete_blob(blob_item.Name);
+        }
+        if (auto response = deleter.execute(); response.status.code != ErrorCode::OK) {
+            return response;
+        }
+        return {};
+    };
     auto resp = _client->ListBlobs(list_opts);
-    auto batch = _client->CreateBatch();
-    for (auto&& blob_item : resp.Blobs) {
-        batch.DeleteBlob(blob_item.Name);
-    }
-    auto response = do_azure_client_call([&]() { _client->SubmitBatch(batch); }, opts);
-    if (response.status.code != ErrorCode::OK) {
+    if (auto response = delete_func(resp.Blobs); response.status.code != ErrorCode::OK) {
         return response;
     }
+
     while (!resp.NextPageToken->empty()) {
-        batch = _client->CreateBatch();
         list_opts.ContinuationToken = resp.NextPageToken;
         resp = _client->ListBlobs(list_opts);
-        for (auto&& blob_item : resp.Blobs) {
-            batch.DeleteBlob(blob_item.Name);
-        }
-        auto response = do_azure_client_call([&]() { _client->SubmitBatch(batch); }, opts);
-        if (response.status.code != ErrorCode::OK) {
+
+        if (auto response = delete_func(resp.Blobs); response.status.code != ErrorCode::OK) {
             return response;
         }
     }
