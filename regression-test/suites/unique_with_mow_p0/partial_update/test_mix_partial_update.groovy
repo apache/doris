2d9fcff5db31859b5c02611bd5ae2a0048fa01ab
Yukang-Lian <Yukang.Lian2022@gmail.com>
2024-06-27 18:16:25 +0800

    2

--- regression-test/suites/unique_with_mow_p0/partial_update/test_mix_partial_update.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_mix_partial_update.groovy
@@ -73,17 +73,11 @@ suite('test_mix_partial_update') {
             sql "set enable_insert_strict=false;"
 
             sql "insert into ${tableInsertName1} (k1,seq) values(2,1)"
-            // seq的更新没生效
-            // error
-            // 1,10,null,30,1
             // 2,2,2,2,1
             // 3,3,3,3,3
             qt_select6 "select * from ${tableInsertName1} order by k1"
 
             sql "insert into ${tableInsertName1} (k1,seq,__DORIS_DELETE_SIGN__) values(3,1,1)"
-            // 部分列更新能更新delete sign？
-            // error
-            // 1,10,null,30,1
             // 2,2,2,2,1
             // 3,3,3,3,3
             qt_select7 "select * from ${tableInsertName1} order by k1"
@@ -91,19 +85,15 @@ suite('test_mix_partial_update') {
             sql "set enable_unique_key_partial_update=false;"
             sql "set enable_insert_strict=true;"
             sql "insert into ${tableInsertName1} values(4,4,4,4,4)"
-            // 1,10,null,30,1
             // 2,2,2,2,1
             // 3,3,3,3,3
             // 4,4,4,4,4
             qt_select8 "select * from ${tableInsertName1} order by k1"
 
             sql "update ${tableInsertName1} set seq = 1 where k1 = 4"
-            // update没有更新
-            // error
-            // 1,10,null,30,1
             // 2,2,2,2,1
             // 3,3,3,3,3
-            // 4,4,4,4,1
+            // 4,4,4,4,4
             qt_select9 "select * from ${tableInsertName1} order by k1"
 
             def tableStreamName1 = "test_mix_partial_update"
@@ -121,29 +111,121 @@ suite('test_mix_partial_update') {
                     "replication_num" = "1",
                     "function_column.sequence_col" = "seq",
                     "store_row_column" = "${use_row_store}"); """
-            //sql "insert into ${tableStreamName1} values(1,1,1,1,1),(1,1,1,1,10),(2,2,2,2,2),(3,3,3,3,3)"
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                file "test_mix_partial_update_load1.csv"
+                time 10000 // limit inflight 10s
+            }
+            // sql "insert into ${tableInsertName1} values(1,1,1,1,1),(1,1,1,1,10),(2,2,2,2,2),(3,3,3,3,3)"
             // 1,1,1,1,10
             // 2,2,2,2,2
             // 3,3,3,3,3
             qt_select1 "select * from ${tableStreamName1} order by k1"
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'columns', 'k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__'
+                //set 'partial_columns', 'true'
+                file "test_mix_partial_update_load2.csv"
+                time 10000 // limit inflight 10s
+            }
             //sql "insert into ${tableStreamName1} (k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__) values(1,10,null,10,10,1)"
             // 2,2,2,2,2
             // 3,3,3,3,3
             qt_select2 "select * from ${tableStreamName1} order by k1"
+
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'columns', 'k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__'
+                //set 'partial_columns', 'true'
+                file "test_mix_partial_update_load3.csv"
+                time 10000 // limit inflight 10s
+            }
             //sql "insert into ${tableStreamName1} (k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__) values(1,10,null,10,20,1)"
             // 2,2,2,2,2
             // 3,3,3,3,3
             qt_select3 "select * from ${tableStreamName1} order by k1"
+
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'columns', 'k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__'
+                //set 'partial_columns', 'true'
+                file "test_mix_partial_update_load4.csv"
+                time 10000 // limit inflight 10s
+            }
             //sql "insert into ${tableStreamName1} (k1,c1,c2,c3,seq,__DORIS_DELETE_SIGN__) values(1,10,null,10,1,0)"
             // 1,10,null,10,1
             // 2,2,2,2,2
             // 3,3,3,3,3
             qt_select4 "select * from ${tableStreamName1} order by k1"
-            //sql "update ${tableStreamName1} set seq = 30"
+            sql "update ${tableInsertName1} set seq = 30 where k1 = 1"
             // 1,10,null,30,1
             // 2,2,2,2,2
             // 3,3,3,3,3
             qt_select5 "select * from ${tableStreamName1} order by k1"
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'columns', 'k1,seq'
+                set 'partial_columns', 'true'
+                file "test_mix_partial_update_load5.csv"
+                time 10000 // limit inflight 10s
+            }
+            //sql "set enable_unique_key_partial_update=true;"
+            //sql "set enable_insert_strict=false;"
+            //sql "insert into ${tableStreamName1} (k1,seq) values(2,1)"
+            // 2,2,2,2,1
+            // 3,3,3,3,3
+            qt_select6 "select * from ${tableStreamName1} order by k1"
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                set 'columns', 'k1,seq,__DORIS_DELETE_SIGN__'
+                set 'partial_columns', 'true'
+                file "test_mix_partial_update_load6.csv"
+                time 10000 // limit inflight 10s
+            }
+            //sql "set enable_unique_key_partial_update=true;"
+            //sql "set enable_insert_strict=false;"
+            // sql "insert into ${tableStreamName1} (k1,seq,__DORIS_DELETE_SIGN__) values(3,1,1)"
+            // 2,2,2,2,1
+            // 3,3,3,3,3
+            qt_select7 "select * from ${tableStreamName1} order by k1"
+
+            streamLoad {
+                table "${tableStreamName1}"
+                set 'column_separator', ','
+                set 'format', 'csv'
+                file "test_mix_partial_update_load7.csv"
+                time 10000 // limit inflight 10s
+            }
+            // sql "insert into ${tableStreamName1} values(4,4,4,4,4)"
+            // 2,2,2,2,1
+            // 3,3,3,3,3
+            // 4,4,4,4,4
+            qt_select8 "select * from ${tableStreamName1} order by k1"
+
+            sql "update ${tableStreamName1} set seq = 1 where k1 = 4"
+            // 2,2,2,2,1
+            // 3,3,3,3,3
+            // 4,4,4,4,4
+            qt_select9 "select * from ${tableStreamName1} order by k1"
+
         }
     }
 }
