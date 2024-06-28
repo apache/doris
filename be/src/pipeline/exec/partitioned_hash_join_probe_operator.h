92a6702197cc120b84f4486815e6a9ed81dfb0f3
Jerry Hu <mrhhsg@gmail.com>
2024-06-28 17:25:54 +0800

    [refactor](spill) unify the entry point of spill tasks

--- be/src/pipeline/exec/partitioned_hash_join_probe_operator.h
+++ be/src/pipeline/exec/partitioned_hash_join_probe_operator.h
@@ -24,14 +24,12 @@
 #include "pipeline/exec/hashjoin_build_sink.h"
 #include "pipeline/exec/hashjoin_probe_operator.h"
 #include "pipeline/exec/join_build_sink_operator.h"
-#include "vec/runtime/partitioner.h"
+#include "pipeline/exec/spill_utils.h"
 
 namespace doris {
 class RuntimeState;
 
 namespace pipeline {
-
-using PartitionerType = vectorized::Crc32HashPartitioner<vectorized::SpillPartitionChannelIds>;
 
 class PartitionedHashJoinProbeOperatorX;
 
