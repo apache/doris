f4a6434da7a8db84692d922e4773d903077e0113
slothever <18522955+wsjz@users.noreply.github.com>
2024-06-28 17:27:44 +0800

    [feature](insert)support external hive truncate table DDL (#36801)
    ## Proposed changes
    
    Issue Number: #31442
    
    1. support hive3 'truncate table'
    2. forbiden hive2 'truncate table' because of it is not supported by HMS
    Client API on hive2

--- fe/fe-core/src/main/java/org/apache/doris/datasource/operations/ExternalMetadataOps.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/operations/ExternalMetadataOps.java
@@ -62,6 +62,14 @@ public interface ExternalMetadataOps {
 
     /**
      *
+     * @param dbName
+     * @param tblName
+     * @param partitions
+     */
+    void truncateTable(String dbName, String tblName, List<String> partitions) throws DdlException;
+
+    /**
+     *
      * @return
      */
     List<String> listDatabaseNames();
