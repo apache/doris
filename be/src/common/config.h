798d9d6fc60a7cca8bb3f975041fe5064d62b739
camby <cambyzhu@tencent.com>
2024-07-01 15:33:18 +0800

    [pick21][opt](mow) reduce memory usage for mow table compaction (#36865) (#36968)
    cherry-pick https://github.com/apache/doris/pull/36865 to branch-2.1

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1159,6 +1159,8 @@ DECLARE_mInt64(variant_threshold_rows_to_estimate_sparse_column);
 DECLARE_mBool(enable_merge_on_write_correctness_check);
 // rowid conversion correctness check when compaction for mow table
 DECLARE_mBool(enable_rowid_conversion_correctness_check);
+// missing rows correctness check when compaction for mow table
+DECLARE_mBool(enable_missing_rows_correctness_check);
 // When the number of missing versions is more than this value, do not directly
 // retry the publish and handle it through async publish.
 DECLARE_mInt32(mow_publish_max_discontinuous_version_num);
