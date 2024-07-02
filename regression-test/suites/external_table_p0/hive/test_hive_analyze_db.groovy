cb8b99488e58298a046f9719326ac433b2a8edca
zhangdong <493738387@qq.com>
2024-07-02 23:25:35 +0800

    [test](migrate) move 2 cases from p2 to p0 (#36935)
    1. Extend the exploration time of hive docker
    2. move case
    - test_hive_remove_partition
    - test_hive_statistics_from_hms
    - test_hive_partition_column_analyze
    - test_hive_parquet_alter_column
    - test_hive_analyze_db

--- regression-test/suites/external_table_p0/hive/test_hive_analyze_db.groovy
+++ regression-test/suites/external_table_p0/hive/test_hive_analyze_db.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
- suite("test_hive_analyze_db", "p2,external,hive,external_remote,external_remote_hive") {
+ suite("test_hive_analyze_db", "p0,external,hive,external_docker,external_docker_hive") {
 
      def verify_column_stats_result = { column, result, count, ndv, nulls, size, avg_size, min, max ->
          def found = false;
@@ -34,11 +34,16 @@
          assertTrue(found)
      }
 
-     String enabled = context.config.otherConfigs.get("enableExternalHiveTest")
-     if (enabled != null && enabled.equalsIgnoreCase("true")) {
-         String extHiveHmsHost = context.config.otherConfigs.get("extHiveHmsHost")
-         String extHiveHmsPort = context.config.otherConfigs.get("extHiveHmsPort")
-         String catalog_name = "test_hive_analyze_db"
+     String enabled = context.config.otherConfigs.get("enableHiveTest")
+     if (enabled == null || !enabled.equalsIgnoreCase("true")) {
+         logger.info("diable Hive test.")
+         return;
+     }
+
+     for (String hivePrefix : ["hive2", "hive3"]) {
+         String extHiveHmsHost = context.config.otherConfigs.get("externalEnvIp")
+         String extHiveHmsPort = context.config.otherConfigs.get(hivePrefix + "HmsPort")
+         String catalog_name = "${hivePrefix}_test_hive_partition_column_analyze"
          sql """drop catalog if exists ${catalog_name};"""
          sql """
             create catalog if not exists ${catalog_name} properties (
