0e9bbb9acb19da9ce6d764766609a7e49e164e24
Mingyu Chen <morningman@163.com>
2024-07-03 21:37:33 +0800

    [feature](serde) support presto compatible output format (#37039)
    the output format of some data types are different between Presto/Trino
    and Doris,
    especially for complex type such as array, map and struct.
    When user migrate from Presto to Doris, they expect the same format so
    that they
    don't need to modify their business code.
    
    This PR mainly changes:
    
    1. Add a new session variable `serde_dialect`
    Default is `doris`, options are `presto` or `trino`. If set to presto or
    trino,
    the output format returned to MySQL client of some datatypes will be
    changed:
    
        - Array
            Doris: `["abc", "def", "", null]`
            Presto: `[abc, def, , NULL]`
    
        - Map
            Doris: `{"k1":null, "k2":"v3"}`
            Presto: `{k1=NULL, k2=v3}`
    
        - Struct
            Doris: `{"s_id":100, "s_name":"abc , "", "s_address":null}`
            Presto: `{s_id=100, s_name=abc , ", s_address=NULL}`
     
    2. Change the output format of struct type
    
        Remove the space after `:`
    
        - Before: `{"s_id": 100, "s_name": "abc , "", "s_address": null}`
        - After: ``{"s_id":100, "s_name":"abc , "", "s_address":null}``

--- be/src/vec/data_types/serde/data_type_jsonb_serde.h
+++ be/src/vec/data_types/serde/data_type_jsonb_serde.h
@@ -38,9 +38,11 @@ public:
     DataTypeJsonbSerDe(int nesting_level = 1) : DataTypeStringSerDe(nesting_level) {};
 
     Status write_column_to_mysql(const IColumn& column, MysqlRowBuffer<true>& row_buffer,
-                                 int row_idx, bool col_const) const override;
+                                 int row_idx, bool col_const,
+                                 const FormatOptions& options) const override;
     Status write_column_to_mysql(const IColumn& column, MysqlRowBuffer<false>& row_buffer,
-                                 int row_idx, bool col_const) const override;
+                                 int row_idx, bool col_const,
+                                 const FormatOptions& options) const override;
     void write_column_to_arrow(const IColumn& column, const NullMap* null_map,
                                arrow::ArrayBuilder* array_builder, int start, int end,
                                const cctz::time_zone& ctz) const override;
@@ -72,7 +74,7 @@ public:
 private:
     template <bool is_binary_format>
     Status _write_column_to_mysql(const IColumn& column, MysqlRowBuffer<is_binary_format>& result,
-                                  int row_idx, bool col_const) const;
+                                  int row_idx, bool col_const, const FormatOptions& options) const;
 };
 } // namespace vectorized
 } // namespace doris
