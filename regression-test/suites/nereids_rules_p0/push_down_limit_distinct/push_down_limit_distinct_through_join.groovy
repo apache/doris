07b7748eb16c3df7a8e6cb5c09b27ab145a0dfdc
minghong <englefly@gmail.com>
2024-07-03 15:05:15 +0800

    rt

--- regression-test/suites/nereids_rules_p0/push_down_limit_distinct/push_down_limit_distinct_through_join.groovy
+++ regression-test/suites/nereids_rules_p0/push_down_limit_distinct/push_down_limit_distinct_through_join.groovy
@@ -16,11 +16,16 @@
 // under the License.
 
 suite("push_down_limit_distinct_through_join") {
-    sql "SET enable_nereids_planner=true"
-    sql "set runtime_filter_mode=OFF"
-    sql "SET enable_fallback_to_original_planner=false"
-    sql "SET ignore_shape_nodes='PhysicalDistribute,PhysicalProject'"
-    sql "SET disable_join_reorder=true"
+      multi_sql """
+      SET enable_nereids_planner=true;
+      SET enable_fallback_to_original_planner=false;
+      set runtime_filter_mode=OFF;
+      SET ignore_shape_nodes='PhysicalDistribute,PhysicalProject';
+      SET ENABLE_NEREIDS_RULES=push_down_limit_distinct_through_join;
+      set push_topn_to_agg = true;
+      SET disable_join_reorder=true;
+      set topn_opt_limit_threshold=1024;
+      """
 
     sql """
         DROP TABLE IF EXISTS t;
@@ -48,11 +53,6 @@ suite("push_down_limit_distinct_through_join") {
     sql "insert into t values (8, null, 'c')"
     sql "insert into t values (9, 3, null)"
     sql "insert into t values (10, null, null)"
-
-    multi_sql """
-      SET ENABLE_NEREIDS_RULES=push_down_limit_distinct_through_join;
-      set push_topn_to_agg = true;
-      """
 
     qt_basic """
         explain shape plan (select id from t t1 union select id from t t2) limit 0, 200;
