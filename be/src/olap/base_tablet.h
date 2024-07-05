4434b09cc87bf07b943dac18f686e3e9e1ab249d
Luwei <814383175@qq.com>
2024-07-06 00:03:15 +0800

    [fix](compaction) fixing the inaccurate statistics of concurrent compaction tasks (#37318)
    Specify the specific stages of compaction tasks and accurately count
    concurrent tasks only while they are actively running

--- be/src/olap/base_tablet.h
+++ be/src/olap/base_tablet.h
@@ -23,6 +23,7 @@
 
 #include "common/status.h"
 #include "olap/iterators.h"
+#include "olap/olap_common.h"
 #include "olap/partial_update_info.h"
 #include "olap/rowset/segment_v2/segment.h"
 #include "olap/tablet_fwd.h"
@@ -42,6 +43,8 @@ struct TabletWithVersion {
     BaseTabletSPtr tablet;
     int64_t version;
 };
+
+enum class CompactionStage { NOT_SCHEDULED, PENDING, EXECUTING };
 
 // Base class for all tablet classes
 class BaseTablet {
@@ -301,6 +304,7 @@ public:
     std::atomic<int64_t> write_count = 0;
     std::atomic<int64_t> compaction_count = 0;
 
+    CompactionStage compaction_stage = CompactionStage::NOT_SCHEDULED;
     std::mutex sample_info_lock;
     std::vector<CompactionSampleInfo> sample_infos;
     Status last_compaction_status = Status::OK();
