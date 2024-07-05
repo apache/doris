083604df5168f586dd6a5f37c7df475d9c03fba7
Mryange <2319153948@qq.com>
2024-07-05 18:01:13 +0800

    rename

--- regression-test/suites/function_p0/test_agg_foreach_notnull.groovy
+++ regression-test/suites/function_p0/test_agg_foreach_notnull.groovy
@@ -36,11 +36,11 @@ suite("test_agg_foreach_not_null") {
     sql """ set enable_fallback_to_original_planner=false;"""
    
     sql """
-        drop table if exists foreach_table;
+        drop table if exists foreach_table_not_null;
     """
 
     sql """
-       CREATE TABLE IF NOT EXISTS foreach_table (
+       CREATE TABLE IF NOT EXISTS foreach_table_not_null (
               `id` INT(11) not null COMMENT "",
               `a` array<INT> not null   COMMENT "",
               `b` array<array<INT>>  not null  COMMENT "",
@@ -54,7 +54,7 @@ suite("test_agg_foreach_not_null") {
     );
     """
     sql """
-    insert into foreach_table values
+    insert into foreach_table_not_null values
     (1,[1,2,3],[[1],[1,2,3],[2]],["ab","123","114514"]),
     (2,[20],[[2]],["cd"]),
     (3,[100],[[1]],["efg"]) , 
@@ -63,67 +63,67 @@ suite("test_agg_foreach_not_null") {
 
    // this case also test combinator should be case-insensitive
    qt_sql """
-       select min_ForEach(a), min_by_foreach(a,a),max_foreach(a),max_by_foreach(a,a) , avg_foreach(a),avg_weighted_foreach(a,a) from foreach_table ;
+       select min_ForEach(a), min_by_foreach(a,a),max_foreach(a),max_by_foreach(a,a) , avg_foreach(a),avg_weighted_foreach(a,a) from foreach_table_not_null ;
    """
 
    qt_sql """
-   select  sum_foreach(a)  , stddev_foreach(a) ,stddev_samp_foreach(a)  , variance_foreach(a) , var_samp_foreach(a) from foreach_table ;
+   select  sum_foreach(a)  , stddev_foreach(a) ,stddev_samp_foreach(a)  , variance_foreach(a) , var_samp_foreach(a) from foreach_table_not_null ;
    """
 
    qt_sql """
-   select covar_foreach(a,a)  , covar_samp_foreach(a,a) , corr_foreach(a,a) from foreach_table ; 
+   select covar_foreach(a,a)  , covar_samp_foreach(a,a) , corr_foreach(a,a) from foreach_table_not_null ; 
    """
     qt_sql """
-   select topn_foreach(a,a) ,topn_foreach(a,a,a)  , topn_array_foreach(a,a) ,topn_array_foreach(a,a,a)from foreach_table ;
+   select topn_foreach(a,a) ,topn_foreach(a,a,a)  , topn_array_foreach(a,a) ,topn_array_foreach(a,a,a)from foreach_table_not_null ;
    """
 
 
    qt_sql """
-   select count_foreach(a)  , count_by_enum_foreach(a)  , approx_count_distinct_foreach(a) from foreach_table;
+   select count_foreach(a)  , count_by_enum_foreach(a)  , approx_count_distinct_foreach(a) from foreach_table_not_null;
    """
 
    qt_sql """
-   select histogram_foreach(a) from foreach_table;
+   select histogram_foreach(a) from foreach_table_not_null;
    """
    
    qt_sql """
-      select PERCENTILE_foreach(a,a)  from foreach_table;
+      select PERCENTILE_foreach(a,a)  from foreach_table_not_null;
    """
   
    qt_sql """
-      select PERCENTILE_ARRAY_foreach(a,b) from foreach_table where id = 1;
+      select PERCENTILE_ARRAY_foreach(a,b) from foreach_table_not_null where id = 1;
    """
 
    qt_sql """
 
-   select PERCENTILE_APPROX_foreach(a,a) from foreach_table;
+   select PERCENTILE_APPROX_foreach(a,a) from foreach_table_not_null;
    """
 
    qt_sql """
-   select GROUP_BIT_AND_foreach(a), GROUP_BIT_OR_foreach(a), GROUP_BIT_XOR_foreach(a)  from foreach_table;
+   select GROUP_BIT_AND_foreach(a), GROUP_BIT_OR_foreach(a), GROUP_BIT_XOR_foreach(a)  from foreach_table_not_null;
    """
 
    qt_sql """
-   select GROUP_CONCAT_foreach(s), GROUP_CONCAT_foreach(s,s) from foreach_table;
+   select GROUP_CONCAT_foreach(s), GROUP_CONCAT_foreach(s,s) from foreach_table_not_null;
    """
    
    qt_sql """
-   select retention_foreach(a), retention_foreach(a,a ),retention_foreach(a,a,a) , retention_foreach(a,a,a ,a) from foreach_table;
+   select retention_foreach(a), retention_foreach(a,a ),retention_foreach(a,a,a) , retention_foreach(a,a,a ,a) from foreach_table_not_null;
    """
    
    qt_sql """
-   select any_value_foreach(s), any_value_foreach(a) from foreach_table;
+   select any_value_foreach(s), any_value_foreach(a) from foreach_table_not_null;
    """
 
    qt_sql """
-   select collect_set_foreach(a), collect_set_foreach(s) , collect_set_foreach(a,a) from foreach_table;
+   select collect_set_foreach(a), collect_set_foreach(s) , collect_set_foreach(a,a) from foreach_table_not_null;
    """
 
    qt_sql """
-   select collect_list_foreach(a), collect_list_foreach(s) , collect_list_foreach(a,a) from foreach_table;
+   select collect_list_foreach(a), collect_list_foreach(s) , collect_list_foreach(a,a) from foreach_table_not_null;
    """
 
    qt_sql """
-   select map_agg_foreach(a,a), map_agg_foreach(a,s) , map_agg_foreach(s,s) from foreach_table;
+   select map_agg_foreach(a,a), map_agg_foreach(a,s) , map_agg_foreach(s,s) from foreach_table_not_null;
    """
 }
