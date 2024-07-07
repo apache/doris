7c988c03e8498bb876033cec4ba03ad72be9d66d
meiyi <myimeiyi@gmail.com>
2024-07-07 21:43:33 +0800

    f

--- be/src/vec/sink/group_commit_block_sink.cpp
+++ be/src/vec/sink/group_commit_block_sink.cpp
@@ -49,7 +49,7 @@ GroupCommitBlockSink::GroupCommitBlockSink(ObjectPool* pool, const RowDescriptor
 GroupCommitBlockSink::~GroupCommitBlockSink() {
     if (_load_block_queue) {
         _remove_estimated_wal_bytes();
-        _load_block_queue->group_commit_load_count.fetch_add(1);
+        _load_block_queue->remove_load_id(_load_id);
     }
 }
 
