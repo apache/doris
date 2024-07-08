b0e71a37b166c10b08e2c128e14fb10b2545749c
Mingyu Chen <morningman@163.com>
2024-07-08 15:52:23 +0800

    [fix](case) fix struct format out files (#37350)
    follow up #37349
    1. Some of constants are calculated in FE, it should output same format
    as in BE.
        Add `FormatOptions` to control the output format of complex types.
    
    3. Modify some of result files of test cases.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -4120,7 +4120,7 @@ public class SessionVariable implements Serializable, Writable {
         return this.maxMsgSizeOfResultReceiver;
     }
 
-    private TSerdeDialect getSerdeDialect() {
+    public TSerdeDialect getSerdeDialect() {
         switch (serdeDialect) {
             case "doris":
                 return TSerdeDialect.DORIS;
