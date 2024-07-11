057d1f4d5118136d696cb64d24f75a53aa041b7a
wangbo <wangbo@apache.org>
2024-07-11 14:30:46 +0800

    [bugfix](wg)refactor query queue for robustness (#37642)
    ## Proposed changes
    To simplify QueryQueue's code for robustness, redefine QueryQueue's
    usage in two steps:
    1 use QueryQueue.getToken to get token, then token state maybe running
    or queued；
    2 release QueryToken when coordinator.close，decrement runningQueryNum or
    remove it from waiting queue；
    We just need to keep this two step is atomic，then whether QueueToken.get
    is succ or exception is not important.
    So this PR remove ```removeToken``` method and just release QueueToken
    in ```coord.close```.
    
    
    imported:  #35929

--- fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/Coordinator.java
@@ -660,15 +660,21 @@ public class Coordinator implements CoordInterface {
 
     @Override
     public void close() {
-        for (ScanNode scanNode : scanNodes) {
-            scanNode.stop();
-        }
+        // NOTE: all close method should be no exception
         if (queryQueue != null && queueToken != null) {
             try {
                 queryQueue.releaseAndNotify(queueToken);
             } catch (Throwable t) {
                 LOG.error("error happens when coordinator close ", t);
             }
+        }
+
+        try {
+            for (ScanNode scanNode : scanNodes) {
+                scanNode.stop();
+            }
+        } catch (Throwable t) {
+            LOG.error("error happens when scannode stop ", t);
         }
     }
 
