83fa28d3444a6116e862b44fa634db69887c72b9
stephen <hello-stephen@qq.com>
2024-07-14 10:35:27 +0800

    2

--- regression-test/suites/load_p0/broker_load/test_seq_load.groovy
+++ regression-test/suites/load_p0/broker_load/test_seq_load.groovy
@@ -101,7 +101,7 @@ suite("test_seq_load", "load_p0") {
                 "AWS_ACCESS_KEY" = "$ak",
                 "AWS_SECRET_KEY" = "$sk",
                 "AWS_ENDPOINT" = "${getS3Endpoint()}",
-                "AWS_REGION" = "${getS3Region()}",
+                "AWS_REGION" = "${getS3Region()}"
             )
             properties(
                 "use_new_load_scan_node" = "true"
