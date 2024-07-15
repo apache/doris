d2720190fb1b9ef69ebf25f83da68c0932136d5f
Xinyi Zou <zouxinyi02@gmail.com>
2024-07-15 15:55:16 +0800

    6

--- be/test/testutil/run_all_tests.cpp
+++ be/test/testutil/run_all_tests.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
             doris::TabletSchemaCache::create_global_schema_cache(
                     doris::config::tablet_schema_cache_capacity));
     LOG(INFO) << "init config " << st;
-    s = config::set_config("enable_stacktrace", "false").ok();
+    Status s = doris::config::set_config("enable_stacktrace", "false").ok();
     if (!s.ok()) {
         LOG(WARNING) << "set enable_stacktrace=false failed";
     }
