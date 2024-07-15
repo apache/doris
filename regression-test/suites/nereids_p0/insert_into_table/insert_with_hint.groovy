fae4a5b2e7d31ea29f6f8f5b452dcb2ef7e87f60
bobhan1 <bh2444151092@outlook.com>
2024-07-15 22:14:24 +0800

    add case

--- regression-test/suites/nereids_p0/insert_into_table/insert_with_hint.groovy
+++ regression-test/suites/nereids_p0/insert_into_table/insert_with_hint.groovy
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import groovy.json.JsonSlurper
+
 suite('nereids_insert_with_hint') {
     sql 'set enable_nereids_planner=true'
     sql 'set enable_fallback_to_original_planner=false'
@@ -125,4 +127,63 @@ suite('nereids_insert_with_hint') {
     sql "insert /*+ SET_VAR(enable_unique_key_partial_update=true) */into ${tableName4}(k,v) select v2,v3 from ${tableName4};"
     qt_4 "select * from ${tableName4} order by k;"
     sql """ DROP TABLE IF EXISTS ${tableName4}; """
+
+    def tableName5 = "nereids_insert_with_hint7"
+    sql """ DROP TABLE IF EXISTS ${tableName5} """
+    sql """create table ${tableName5} (
+        k int null,
+        v int null,
+        v2 int null,
+        v3 int null
+    ) unique key (k) distributed by hash(k) buckets 1
+    properties("replication_num" = "1",
+    "enable_unique_key_merge_on_write"="true",
+    "disable_auto_compaction"="true"); """
+    sql "insert into ${tableName5} values(1,1,3,4),(2,2,4,5),(3,3,2,3),(4,4,1,2);"
+    qt_5 "select * from ${tableName5} order by k;"
+
+    sql "set enable_profile=true;"
+    sql "sync;"
+
+    def httpGet = { url ->
+        def dst = 'http://' + context.config.feHttpAddress
+        def conn = new URL(dst + url).openConnection()
+        conn.setRequestMethod("GET")
+        def encoding = Base64.getEncoder().encodeToString((context.config.feHttpUser + ":" + 
+                (context.config.feHttpPassword == null ? "" : context.config.feHttpPassword)).getBytes("UTF-8"))
+        conn.setRequestProperty("Authorization", "Basic ${encoding}")
+        return conn.getInputStream().getText()
+    }
+
+    def getProfile = { stmt -> 
+        def profileUrl = '/rest/v1/query_profile/'
+        def profiles = httpGet(profileUrl)
+        // log.info("profiles:{}", profiles);
+        profiles = new JsonSlurper().parseText(profiles)
+        assertEquals(0, profiles.code)
+
+        def profileId = null;
+        for (def profile in profiles["data"]["rows"]) {
+            if (profile["Sql Statement"].contains("${stmt}")) {
+                profileId = profile["Profile ID"]
+                break;
+            }
+        }
+        log.info("profileId=${profileId}")
+        def profileDetail = httpGet("/rest/v1/query_profile/" + profileId)
+        return profileDetail
+    }
+
+    def sql_stmt = "insert /*+ SET_VAR(enable_memtable_on_sink_node=true) */into ${tableName5}(k,v) select v2,v3 from ${tableName5};"
+    sql sql_stmt
+    def profileDetail = getProfile(sql_stmt)
+    assertTrue(profileDetail.contains("DeltaWriterV2"))
+
+    sql_stmt = "insert /*+ SET_VAR(enable_memtable_on_sink_node=false) */into ${tableName5}(k,v) select v2,v3 from ${tableName5};"
+    sql sql_stmt
+    def profileDetail2 = getProfile(sql_stmt)
+    assertTrue(!profileDetail2.contains("DeltaWriterV2"))
+    sql """ DROP TABLE IF EXISTS ${tableName5}; """
+    sql "set enable_profile=false;"
+    sql "sync"
 }
