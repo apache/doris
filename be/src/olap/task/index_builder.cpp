5f5ec1c35bb5c26ce6597824e755f035cb9b21a0
qiye <jianliang5669@gmail.com>
2024-07-16 20:32:02 +0800

    [test](build index)Remove index_meta in tablet schema when the index is dropped. (#37646) (#37902)

--- be/src/olap/task/index_builder.cpp
+++ be/src/olap/task/index_builder.cpp
@@ -18,6 +18,7 @@
 #include "olap/task/index_builder.h"
 
 #include "common/status.h"
+#include "gutil/integral_types.h"
 #include "olap/rowset/beta_rowset.h"
 #include "olap/rowset/rowset_writer.h"
 #include "olap/rowset/rowset_writer_context.h"
@@ -68,9 +69,28 @@ Status IndexBuilder::update_inverted_index_info() {
             // base on input rowset's tablet_schema to build
             // output rowset's tablet_schema which only remove
             // the indexes specified in this drop index request
+            // ATTN: DO NOT REMOVE INDEX AFTER OUTPUT_ROWSET_WRITER CREATED.
             for (auto t_inverted_index : _alter_inverted_indexes) {
                 output_rs_tablet_schema->remove_index(t_inverted_index.index_id);
             }
+            DBUG_EXECUTE_IF("index_builder.update_inverted_index_info.drop_index", {
+                auto indexes_count = DebugPoints::instance()->get_debug_param_or_default<int32_t>(
+                        "index_builder.update_inverted_index_info.drop_index", "indexes_count", 0);
+                if (indexes_count < 0) {
+                    return Status::Error<ErrorCode::INTERNAL_ERROR>(
+                            "indexes count cannot be negative");
+                }
+                int32_t indexes_size = 0;
+                for (auto index : output_rs_tablet_schema->indexes()) {
+                    if (index.index_type() == IndexType::INVERTED) {
+                        indexes_size++;
+                    }
+                }
+                if (indexes_count != indexes_size) {
+                    return Status::Error<ErrorCode::INTERNAL_ERROR>(
+                            "indexes count not equal to expected");
+                }
+            })
         } else {
             // base on input rowset's tablet_schema to build
             // output rowset's tablet_schema which only add
