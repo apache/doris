05f8bb3a1142040a89ff80b2cbb20c337cbaac6e
zzzxl <33418555+zzzxl1993@users.noreply.github.com>
2024-07-17 14:18:19 +0800

    [fix](inverted index) Incorrect handling of NULL literal (#37921)
    1. Index does not handle NULL literal for now

--- be/src/olap/rowset/segment_v2/segment_iterator.cpp
+++ be/src/olap/rowset/segment_v2/segment_iterator.cpp
@@ -2849,6 +2849,11 @@ void SegmentIterator::_calculate_pred_in_remaining_conjunct_root(
     } else if (_is_literal_node(node_type)) {
         auto v_literal_expr = static_cast<const doris::vectorized::VLiteral*>(expr.get());
         _column_predicate_info->query_values.insert(v_literal_expr->value());
+    } else if (node_type == TExprNodeType::NULL_LITERAL) {
+        if (!_column_predicate_info->column_name.empty()) {
+            auto v_literal_expr = static_cast<const doris::vectorized::VLiteral*>(expr.get());
+            _column_predicate_info->query_values.insert(v_literal_expr->value());
+        }
     } else {
         if (node_type == TExprNodeType::MATCH_PRED) {
             _column_predicate_info->query_op = "match";
