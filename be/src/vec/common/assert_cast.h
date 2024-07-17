8ca7d3a36118cc55da14d3074d4471d0fd08e3ea
yiguolei <yiguolei@gmail.com>
2024-07-17 13:45:04 +0800

    f

--- be/src/vec/common/assert_cast.h
+++ be/src/vec/common/assert_cast.h
@@ -23,7 +23,7 @@
 #include <type_traits>
 #include <typeinfo>
 
-#include "commont/config.h"
+#include "common/config.h"
 #include "common/logging.h"
 #include "vec/common/demangle.h"
 
@@ -44,8 +44,8 @@ PURE To assert_cast(From&& from) {
                         return ptr;
                     }
                     LOG(FATAL) << fmt::format("Bad cast from type:{}* to {}",
-                                            demangle(typeid(*from).name()),
-                                            demangle(typeid(To).name()));
+                                              demangle(typeid(*from).name()),
+                                              demangle(typeid(To).name()));
                 }
             } else {
                 if (typeid(from) == typeid(To)) {
@@ -57,7 +57,7 @@ PURE To assert_cast(From&& from) {
         }
 
         LOG(FATAL) << fmt::format("Bad cast from type:{} to {}", demangle(typeid(from).name()),
-                                demangle(typeid(To).name()));
+                                  demangle(typeid(To).name()));
         __builtin_unreachable();
     } else {
         return static_cast<To>(from);
