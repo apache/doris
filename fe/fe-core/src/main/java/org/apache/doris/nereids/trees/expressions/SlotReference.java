1cf941e66d9db71ce3d60cffe2dd37c8269ffc19
moailing <moailing@selectdb.com>
2024-07-18 19:25:50 +0800

    fix slotreference nullable

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/SlotReference.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/SlotReference.java
@@ -195,7 +195,7 @@ public class SlotReference extends Slot {
     public String toString() {
         if (subPath.isEmpty()) {
             // Just return name and exprId, add another method to show fully qualified name when it's necessary.
-            return name.get() + "#" + exprId + " nullable:" + nullable;
+            return name.get() + "#" + exprId;
         }
         return name.get() + "['" + String.join("']['", subPath) + "']" + "#" + exprId;
     }
