8d428b6c0e9b3307dbcd49a629c1244300167307
924060929 <924060929@qq.com>
2024-07-19 12:03:59 +0800

    [enhancement](nereids) enhancement distribute planner for data distribution (#37219)
    1. add benchmark shape test for distribute planner
    2. support read tables of information_schema
    3. fix can not execute query when all tablets pruned
    4. propagate data distribution when the common column of grouping sets
    is the distribution column

---
+++ regression-test/suites/new_shapes_p0/tpcds_sf100/rf_prune/query92.groovy
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+suite("query92") {
+    if (isCloudMode()) {
+        return
+    }
+    String db = context.config.getDbNameByFile(new File(context.file.parent))
+    sql "use ${db}"
+    sql 'set enable_nereids_planner=true'
+    sql 'set enable_nereids_distribute_planner=true'
+    sql 'set enable_fallback_to_original_planner=false'
+    sql 'set exec_mem_limit=21G'
+    sql 'set be_number_for_test=3'
+    sql 'set parallel_fragment_exec_instance_num=8; '
+    sql 'set parallel_pipeline_task_num=8; '
+    sql 'set forbid_unknown_col_stats=true'
+    sql 'set enable_nereids_timeout = false'
+    sql 'set runtime_filter_type=8'
+    sql 'set enable_runtime_filter_prune=true'
+    sql "set disable_nereids_rules=PRUNE_EMPTY_PARTITION"
+
+    def ds = """select  
+   sum(ws_ext_discount_amt)  as "Excess Discount Amount" 
+from 
+    web_sales 
+   ,item 
+   ,date_dim
+where
+i_manufact_id = 320
+and i_item_sk = ws_item_sk 
+and d_date between '2002-02-26' and 
+        (cast('2002-02-26' as date) + interval 90 day)
+and d_date_sk = ws_sold_date_sk 
+and ws_ext_discount_amt  
+     > ( 
+         SELECT 
+            1.3 * avg(ws_ext_discount_amt) 
+         FROM 
+            web_sales 
+           ,date_dim
+         WHERE 
+              ws_item_sk = i_item_sk 
+          and d_date between '2002-02-26' and
+                             (cast('2002-02-26' as date) + interval 90 day)
+          and d_date_sk = ws_sold_date_sk 
+      ) 
+order by sum(ws_ext_discount_amt)
+limit 100"""
+    qt_ds_shape_92 """
+    explain shape plan
+    ${ds}
+    """
+}
