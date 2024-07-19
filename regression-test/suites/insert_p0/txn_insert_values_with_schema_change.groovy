94a22335b87438e0b3e7ec1ce07a942498bf8077
meiyi <myimeiyi@gmail.com>
2024-07-19 12:12:27 +0800

    fix case

--- regression-test/suites/insert_p0/txn_insert_values_with_schema_change.groovy
+++ regression-test/suites/insert_p0/txn_insert_values_with_schema_change.groovy
@@ -45,26 +45,40 @@ suite("txn_insert_values_with_schema_change") {
     sql """ insert into ${table} values(0, 'a', 10) """
 
     def getAlterTableState = { job_state ->
-        waitForSchemaChangeDone {
-            sql """ SHOW ALTER TABLE COLUMN WHERE tablename='${table}' ORDER BY createtime DESC LIMIT 1 """
-            time 600
+        def retry = 0
+        sql "use ${dbName};"
+        while (true) {
+            sleep(2000)
+            def state = sql " show alter table column where tablename = '${table}' order by CreateTime desc limit 1"
+            logger.info("alter table state: ${state}")
+            if (state.size() > 0 && state[0][9] == job_state) {
+                return
+            }
+            retry++
+            if (retry >= 10) {
+                break
+            }
         }
+        assertTrue(false, "alter table job state is ${last_state}, not ${job_state} after retry ${retry} times")
     }
 
     def txnInsert = {
         try (Connection conn = DriverManager.getConnection(url, context.config.jdbcUser, context.config.jdbcPassword);
              Statement statement = conn.createStatement()) {
-            statement.execute("begin")
-            statement.execute("insert into ${table} values(1, 'b', 20), (2, 'c', 30);")
+            try {
+                statement.execute("begin")
+                statement.execute("insert into ${table} values(1, 'b', 20), (2, 'c', 30);")
 
-            schemaChangeLatch.countDown()
-            insertLatch.await(2, TimeUnit.MINUTES)
+                schemaChangeLatch.countDown()
+                insertLatch.await(2, TimeUnit.MINUTES)
 
-            statement.execute("insert into ${table} values(3, 'd', 40);")
-            statement.execute("commit")
-        } catch (Throwable e) {
-            logger.error("txn insert failed", e)
-            errors.add("txn insert failed " + e.getMessage())
+                statement.execute("insert into ${table} values(3, 'd', 40);")
+                statement.execute("commit")
+            } catch (Throwable e) {
+                logger.error("txn insert failed", e)
+                errors.add("txn insert failed " + e.getMessage())
+                statement.execute("rollback")
+            }
         }
     }
 
