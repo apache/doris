de35db402ec0b603f10c771ed7c7b1e02abc9a8b
Toms1999 <mj_tangsi@163.com>
2024-07-22 19:27:32 +0800

    fix alias conflict about test

--- regression-test/suites/nereids_rules_p0/limit_push_down/limit_push_down.groovy
+++ regression-test/suites/nereids_rules_p0/limit_push_down/limit_push_down.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("") {
+suite("limit_push_down") {
     sql "SET enable_nereids_planner=true"
     sql "set runtime_filter_mode=OFF"
     sql "SET enable_fallback_to_original_planner=false"
