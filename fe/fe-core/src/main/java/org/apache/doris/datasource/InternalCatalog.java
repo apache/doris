9e6f590718978085865a1ee50a5d4c97139c0682
lxr599 <anooblxr@gmail.com>
2024-07-23 15:01:13 +0800

    pick branch 'page_size'

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -1710,7 +1710,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                     singlePartitionDesc.isInMemory(),
                     singlePartitionDesc.getTabletType(),
                     storagePolicy, idGeneratorBuffer,
-                    binlogConfig, dataProperty.isStorageMediumSpecified(), null, olapTable.rowStorePageSize());
+                    binlogConfig, dataProperty.isStorageMediumSpecified(), null);
             // TODO cluster key ids
 
             // check again
@@ -2006,7 +2006,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                                                    IdGeneratorBuffer idGeneratorBuffer,
                                                    BinlogConfig binlogConfig,
                                                    boolean isStorageMediumSpecified,
-                                                   List<Integer> clusterKeyIndexes, long rowStorePageSize)
+                                                   List<Integer> clusterKeyIndexes)
             throws DdlException {
         // create base index first.
         Preconditions.checkArgument(tbl.getBaseIndexId() != -1);
@@ -2090,7 +2090,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                             tbl.getTimeSeriesCompactionLevelThreshold(),
                             tbl.storeRowColumn(), binlogConfig,
                             tbl.getRowStoreColumnsUniqueIds(rowStoreColumns),
-                            objectPool, rowStorePageSize);
+                            objectPool, tbl.rowStorePageSize());
 
                     task.setStorageFormat(tbl.getStorageFormat());
                     task.setInvertedIndexFileStorageFormat(tbl.getInvertedIndexFileStorageFormat());
@@ -2880,7 +2880,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                         idGeneratorBuffer,
                         binlogConfigForTask,
                         partitionInfo.getDataProperty(partitionId).isStorageMediumSpecified(),
-                        keysDesc.getClusterKeysColumnIds(), olapTable.rowStorePageSize());
+                        keysDesc.getClusterKeysColumnIds());
                 afterCreatePartitions(db.getId(), olapTable.getId(), null,
                         olapTable.getIndexIdList(), true);
                 olapTable.addPartition(partition);
@@ -2963,7 +2963,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                             partionStoragePolicy, idGeneratorBuffer,
                             binlogConfigForTask,
                             dataProperty.isStorageMediumSpecified(),
-                            keysDesc.getClusterKeysColumnIds(), olapTable.rowStorePageSize());
+                            keysDesc.getClusterKeysColumnIds());
                     olapTable.addPartition(partition);
                     olapTable.getPartitionInfo().getDataProperty(partition.getId())
                             .setStoragePolicy(partionStoragePolicy);
@@ -3430,7 +3430,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                         olapTable.getPartitionInfo().getDataProperty(oldPartitionId).getStoragePolicy(),
                         idGeneratorBuffer, binlogConfig,
                         copiedTbl.getPartitionInfo().getDataProperty(oldPartitionId).isStorageMediumSpecified(),
-                        clusterKeyIdxes, olapTable.rowStorePageSize());
+                        clusterKeyIdxes);
                 newPartitions.add(newPartition);
             }
 
