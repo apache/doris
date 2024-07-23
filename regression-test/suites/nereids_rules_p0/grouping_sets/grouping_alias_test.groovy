1487abfdbe77d4f7fe5ab308c15420acd4657db2
feiniaofeiafei <moailing@selectdb.com>
2024-07-23 21:41:26 +0800

    add some test

--- regression-test/suites/nereids_rules_p0/grouping_sets/grouping_alias_test.groovy
+++ regression-test/suites/nereids_rules_p0/grouping_sets/grouping_alias_test.groovy
@@ -77,4 +77,35 @@ suite("grouping_alias_test"){
     qt_expr_with_or_without_alias_3_expr "select id+1, id+1, id+1 as c1  from grouping_alias_test_t  group by grouping sets((id+1,value2),(id+1)) order by 1,2,3;"
     qt_expr_with_or_without_alias_3_expr_grouping_scalar "select id+1, id+1, id+1 as c3,grouping(id+1)  from grouping_alias_test_t   group by grouping sets((id+1,value2),(id+1)) order by 1,2,3,4;"
     qt_expr_with_or_without_alias_3_expr_agg_func "select id+1 as c1, id+1, id+1,sum(id+1) from grouping_alias_test_t  group by grouping sets((id+1,value2),(id+1)) order by 1,2,3,4;"
+
+    qt_alias_in_grouping "select id as c1 from grouping_alias_test_t group by grouping sets((c1,value2),(id)) order by 1;"
+    qt_alias_grouping_scalar_in_grouping "select id as c1, grouping(id) from grouping_alias_test_t  group by grouping sets((c1,value2),(id)) order by 1,2;"
+    qt_alias_agg_func_in_grouping "select id as c1, sum(id) from grouping_alias_test_t group by grouping sets((id,value2),(c1)) order by 1,2;"
+
+    // TODO: The query result of the following example is different from pg. It needs to be modified later.
+    qt_same_column_different_alias_in_grouping "select id as c1, id as c2 from grouping_alias_test_t group by grouping sets((c1,value2),(c2)) order by 1,2;"
+    qt_same_column_different_alias_grouping_scalar_in_grouping "select id as c1, id as c2, grouping(id) from grouping_alias_test_t  group by grouping sets((c1,value2),(c2)) order by 1,2,3;"
+    qt_same_column_different_alias_agg_func_in_grouping "select id as c1, id as c2 , sum(id) from grouping_alias_test_t group by grouping sets((c1,value2),(c2)) order by 1,2,3;"
+
+    qt_same_column_different_alias_in_grouping_3_column "select id as c1, id as c2, id as c3  from grouping_alias_test_t  group by grouping sets((c1,value2),(c2,c3)) order by 1,2,3;"
+    qt_same_column_different_alias_in_grouping_3_column_grouping_scalar "select id as c1, id as c2, id as c3,grouping(id)  from grouping_alias_test_t   group by grouping sets((c1,value2),(c2,c3)) order by 1,2,3,4;"
+    qt_same_column_different_alias_in_grouping_3_column_agg_func "select id as c1, id as c2, id as c3 ,sum(id) from grouping_alias_test_t  group by grouping sets((c1,value2),(c2,c3)) order by 1,2,3,4;"
+
+    qt_same_column_one_has_alias_in_grouping_the_other_do_not "select id , id as c2 from grouping_alias_test_t  group by grouping sets((id,value2),(c2)) order by 1,2;"
+    qt_same_column_one_has_alias_in_grouping_the_other_do_not_grouping_scalar "select id , id as c2 ,grouping(id) from grouping_alias_test_t  group by grouping sets((id,value2),(c2)) order by 1,2,3;"
+    qt_same_column_one_has_alias_in_grouping_the_other_do_not_agg_func "select id , id as c2 ,sum(id) from grouping_alias_test_t  group by grouping sets((id,value2),(c2)) order by 1,2,3;"
+
+    qt_alias_in_grouping_expr "select id+1 as c1 from grouping_alias_test_t group by grouping sets((id+1,value2),(c1)) order by 1;"
+    qt_alias_in_grouping_grouping_scalar_expr "select id+1 as c1, grouping(id+1) from grouping_alias_test_t  group by grouping sets((id+1,value2),(c1)) order by 1,2;"
+    qt_alias_in_grouping_agg_func_expr "select id+1 as c1, sum(id+1) from grouping_alias_test_t group by grouping sets((id+1,value2),(c1)) order by 1,2;"
+
+    qt_same_expr_different_alias_in_grouping "select id+1 as c1, id+1 as c2 from grouping_alias_test_t group by grouping sets((c1,value2),(id+1,c2)) order by 1,2;"
+    qt_same_expr_different_alias_in_grouping_grouping_scalar "select id+1 as c1, id+1 as c2, grouping(id+1) from grouping_alias_test_t  group by grouping sets((c1,value2),(id+1,c2)) order by 1,2,3;"
+    qt_same_expr_different_alias_in_grouping_agg_func "select id+1 as c1, id+1 as c2 , sum(id+1) from grouping_alias_test_t group by grouping sets((c1,value2),(c2)) order by 1,2,3;"
+
+    qt_same_expr_different_alias_in_grouping_3_expr "select id+1 as c1, id+1 as c2, id+1 as c3,grouping(id+1),sum(id+1) as c3 from grouping_alias_test_t  group by grouping sets((c1,value2),(c2,c3,id+1)) order by 1,2,3;"
+
+    qt_same_expr_one_has_alias_in_grouping_the_other_do_not "select id+1 , id+1 as c2,grouping(id+1),sum(id+1) from grouping_alias_test_t  group by grouping sets((c2,value2),(id+1)) order by 1,2;"
+
+
 }
