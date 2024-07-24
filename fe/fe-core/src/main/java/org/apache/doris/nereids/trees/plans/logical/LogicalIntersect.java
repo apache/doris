95270eb1ee5f1aa8d80655e6dd6e38afcd2297bf
谢健 <jianxie0@gmail.com>
2024-07-24 13:56:40 +0800

    [opt](Nereids) fix comment and remove useless function `computeFdItem ` (#38282)
    ## Proposed changes
    
    1. fix comment in eliminate func deps
    2. remove computeFdItem codes

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalIntersect.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalIntersect.java
@@ -19,9 +19,6 @@ package org.apache.doris.nereids.trees.plans.logical;
 
 import org.apache.doris.nereids.memo.GroupExpression;
 import org.apache.doris.nereids.properties.DataTrait.Builder;
-import org.apache.doris.nereids.properties.ExprFdItem;
-import org.apache.doris.nereids.properties.FdFactory;
-import org.apache.doris.nereids.properties.FdItem;
 import org.apache.doris.nereids.properties.LogicalProperties;
 import org.apache.doris.nereids.trees.expressions.NamedExpression;
 import org.apache.doris.nereids.trees.expressions.Slot;
@@ -38,7 +35,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 /**
  * Logical Intersect.
@@ -158,31 +154,5 @@ public class LogicalIntersect extends LogicalSetOperation {
                     child.getLogicalProperties().getTrait());
         }
         builder.replaceFuncDepsBy(constructReplaceMap());
-    }
-
-    @Override
-    public ImmutableSet<FdItem> computeFdItems() {
-        Set<NamedExpression> output = ImmutableSet.copyOf(getOutput());
-        ImmutableSet.Builder<FdItem> builder = ImmutableSet.builder();
-
-        ImmutableSet<SlotReference> exprs = output.stream()
-                .filter(SlotReference.class::isInstance)
-                .map(SlotReference.class::cast)
-                .collect(ImmutableSet.toImmutableSet());
-
-        if (qualifier == Qualifier.DISTINCT) {
-            ExprFdItem fdItem = FdFactory.INSTANCE.createExprFdItem(exprs, true, exprs);
-            builder.add(fdItem);
-            // inherit from both sides
-            ImmutableSet<FdItem> leftFdItems = child(0).getLogicalProperties()
-                    .getTrait().getFdItems();
-            ImmutableSet<FdItem> rightFdItems = child(1).getLogicalProperties()
-                    .getTrait().getFdItems();
-
-            builder.addAll(leftFdItems);
-            builder.addAll(rightFdItems);
-        }
-
-        return builder.build();
     }
 }
