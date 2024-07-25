bc7fc4106d01ae39091c6a0903e9027398566243
zclllhhjj <zhaochangle@selectdb.com>
2024-07-25 11:38:27 +0800

    [branch-2.1](function) fix FE impl of some time functions (#37746) (#38316)
    pick https://github.com/apache/doris/pull/37746
    
    before:
    ```sql
    mysql> select date_ceil("2020-12-12 12:12:12.123", interval 2 second);
    +-----------------------+
    | '2020-12-12 12:12:12' |
    +-----------------------+
    | 2020-12-12 12:12:12   |
    +-----------------------+
    1 row in set (0.10 sec)
    
    mysql> select CONVERT_TZ('9999-12-31 23:59:59.999999', 'Pacific/Galapagos', 'Pacific/Galapagos');
    +------+
    | NULL |
    +------+
    | NULL |
    +------+
    1 row in set (0.09 sec)
    
    mysql [(none)]>select CONVERT_TZ('9999-12-31 23:59:59.999999', 'Pacific/Galapagos', 'Pacific/GalapaGoS');
    +-----------------------------------------------------------------------------------------------------------+
    | convert_tz(cast('9999-12-31 23:59:59.999999' as DATETIMEV2(6)), 'Pacific/Galapagos', 'Pacific/GalapaGoS') |
    +-----------------------------------------------------------------------------------------------------------+
    | 9999-12-31 23:59:59.999999                                                                                |
    +-----------------------------------------------------------------------------------------------------------+
    1 row in set (0.08 sec) --- gone to BE
    ```
    after:
    ```sql
    mysql> select date_ceil("2020-12-12 12:12:12.123", interval 2 second);
    +------------------------------+
    | '2020-12-12 12:12:14.000000' |
    +------------------------------+
    | 2020-12-12 12:12:14          |
    +------------------------------+
    1 row in set (0.11 sec)
    
    mysql> select CONVERT_TZ('9999-12-31 23:59:59.999999', 'Pacific/Galapagos', 'Pacific/Galapagos');
    +-----------------------------------------------------------------------------------------------------------+
    | convert_tz(cast('9999-12-31 23:59:59.999999' as DATETIMEV2(6)), 'Pacific/Galapagos', 'Pacific/Galapagos') |
    +-----------------------------------------------------------------------------------------------------------+
    | 9999-12-31 23:59:59.999999                                                                                |
    +-----------------------------------------------------------------------------------------------------------+
    1 row in set (0.23 sec)
    
    mysql> select CONVERT_TZ('9999-12-31 23:59:59.999999', 'Pacific/Galapagos', 'Pacific/GalapaGoS');
    +------------------------------+
    | '9999-12-31 23:59:59.999999' |
    +------------------------------+
    | 9999-12-31 23:59:59.999999   |
    +------------------------------+
    1 row in set (0.11 sec) --- finished in FE
    ```

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/literal/DateLiteral.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/literal/DateLiteral.java
@@ -46,7 +46,7 @@ public class DateLiteral extends Literal {
 
     // for cast datetime type to date type.
     private static final LocalDateTime START_OF_A_DAY = LocalDateTime.of(0, 1, 1, 0, 0, 0);
-    private static final LocalDateTime END_OF_A_DAY = LocalDateTime.of(9999, 12, 31, 23, 59, 59);
+    private static final LocalDateTime END_OF_A_DAY = LocalDateTime.of(9999, 12, 31, 23, 59, 59, 999999000);
     private static final DateLiteral MIN_DATE = new DateLiteral(0, 1, 1);
     private static final DateLiteral MAX_DATE = new DateLiteral(9999, 12, 31);
     private static final int[] DAYS_IN_MONTH = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
