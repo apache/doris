f2f53563a6b51acb718b130731c5c22f72892e9a
wangbo <wangbo@apache.org>
2024-07-26 10:13:24 +0800

    [Imrpoment]Add io throttle (#38238)
    ## Proposed changes
    Add IOThrottle to limit workload group's scan IO.

--- fe/fe-core/src/main/java/org/apache/doris/tablefunction/MetadataGenerator.java
+++ fe/fe-core/src/main/java/org/apache/doris/tablefunction/MetadataGenerator.java
@@ -496,8 +496,11 @@ public class MetadataGenerator {
             trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(12))); // spill low watermark
             trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(13))); // spill high watermark
             trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(14))); // tag
-            trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(15))); // running query num
-            trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(16))); // waiting query num
+            trow.addToColumnValue(new TCell().setLongVal(Long.valueOf(rGroupsInfo.get(15)))); // read bytes per second
+            trow.addToColumnValue(
+                    new TCell().setLongVal(Long.valueOf(rGroupsInfo.get(16)))); // remote read bytes per second
+            trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(17))); // running query num
+            trow.addToColumnValue(new TCell().setStringVal(rGroupsInfo.get(18))); // waiting query num
             dataBatch.add(trow);
         }
 
--- gensrc/thrift/BackendService.thrift
+++ gensrc/thrift/BackendService.thrift
@@ -266,6 +266,8 @@ struct TWorkloadGroupInfo {
   11: optional i32 min_remote_scan_thread_num
   12: optional i32 spill_threshold_low_watermark
   13: optional i32 spill_threshold_high_watermark
+  14: optional i64 read_bytes_per_second
+  15: optional i64 remote_read_bytes_per_second
 }
 
 enum TWorkloadMetricType {
