09126b9aea8df40538dece82b1ea7fa1ce65e1ea
Dongyang Li <hello_stephen@qq.com>
2024-07-29 11:00:35 +0800

    [regression](s3) add default conf for s3 releated cases (#37952)
    ## Proposed changes
    
    replace COS with OSS in the TeamCity pipeline to improve stability
    
    <!--Describe your changes.-->
    
    ---------
    
    Co-authored-by: stephen <hello-stephen@qq.com>

--- regression-test/pipeline/cloud_p0/conf/regression-conf-custom.groovy
+++ regression-test/pipeline/cloud_p0/conf/regression-conf-custom.groovy
@@ -74,4 +74,4 @@ enableKafkaTest=true
 // trino-connector catalog test config
 enableTrinoConnectorTest = false
 
-s3Provider = "COS"
+s3Source = "aliyun"
--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -137,10 +137,7 @@ es_8_port=39200
 
 cacheDataPath = "/data/regression/"
 
-s3Endpoint = "cos.ap-hongkong.myqcloud.com"
-s3BucketName = "doris-build-hk-1308700295"
-s3Region = "ap-hongkong"
-s3Provider = "COS"
+s3Source="aliyun"
 
 //arrow flight sql test config
 extArrowFlightSqlHost = "127.0.0.1"
