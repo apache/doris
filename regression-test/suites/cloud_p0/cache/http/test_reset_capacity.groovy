c83be9e2afe66a6bef217ed87c5a01dab5b340f8
Lightman <31928846+Lchangliang@users.noreply.github.com>
2024-07-29 12:03:34 +0800

    (cloud-merge)  Supports online capacity expansion and contraction (#37484)
    1. Support to reset capacity online via BE HTTP requet
    2. Support to analyze cache conf when only get path_name
    `file_cache_path=[{"path":"xxxx"}]`, we can only specify the path for a
    file cache instance from now on, if we don't care about the detailed
    capacity limit
    
    ```
    # clear file cache
    curl ip:http_port/api/file_cache?op=clear
    # reset capacity of a certain instance of file cache
    curl ip:http_port/api/file_cache?op=reset&capacity=${capacity}&path=${cache_path} 
    
    curl 127.0.0.1:8060/api/file_cache?op=clear
    curl 127.0.0.1:8060/api/file_cache?op=reset&capacity=102400000&path=/user/file_cache
    ```

--- regression-test/suites/cloud_p0/cache/http/test_reset_capacity.groovy
+++ regression-test/suites/cloud_p0/cache/http/test_reset_capacity.groovy
@@ -17,19 +17,38 @@
 
 import org.codehaus.groovy.runtime.IOGroovyMethods
 
-suite("test_ttl_preempt") {
+// 1. clear file cache
+// 2. load 19.5G ttl table data into cache (cache capacity is 20G)
+// 3. check ttl size and total size
+// 4. load 1.3G normal table data into cache (just little datas will be cached)
+// 5. select some data from normal table, and it will read from s3
+// 6. select some data from ttl table, and it will not read from s3
+// 7. wait for ttl data timeout
+// 8. drop the normal table and load again. All normal table datas will be cached this time.
+// 9. select some data from normal table to check whether all datas are cached
+suite("test_reset_capacity") {
     sql """ use @regression_cluster_name1 """
-    def ttlProperties = """ PROPERTIES("file_cache_ttl_seconds"="120") """
-    String[][] backends = sql """ show backends """
-    String backendId;
+    sql """ set global enable_auto_analyze = false; """
+    def ttlProperties = """ PROPERTIES("file_cache_ttl_seconds"="4200") """
+    //doris show backends: BackendId  Host  HeartbeatPort  BePort  HttpPort  BrpcPort  ArrowFlightSqlPort  LastStartTime  LastHeartbeat  Alive  SystemDecommissioned  TabletNum  DataUsedCapacity  TrashUsedCapcacity  AvailCapacity  TotalCapacity  UsedPct  MaxDiskUsedPct  RemoteUsedCapacity  Tag  ErrMsg  Version  Status  HeartbeatFailureCounter  NodeRole
+    def backends = sql_return_maparray "show backends;"
+    assertTrue(backends.size() > 0)
+    String backend_id;
     def backendIdToBackendIP = [:]
     def backendIdToBackendHttpPort = [:]
     def backendIdToBackendBrpcPort = [:]
-    for (String[] backend in backends) {
-        if (backend[9].equals("true") && backend[19].contains("regression_cluster_name1")) {
-            backendIdToBackendIP.put(backend[0], backend[1])
-            backendIdToBackendHttpPort.put(backend[0], backend[4])
-            backendIdToBackendBrpcPort.put(backend[0], backend[5])
+    String host = ''
+    for (def backend in backends) {
+        if (backend.keySet().contains('Host')) {
+            host = backend.Host
+        } else {
+            host = backend.IP
+        }
+        def cloud_tag = parseJson(backend.Tag)
+        if (backend.Alive.equals("true") && cloud_tag.cloud_cluster_name.contains("regression_cluster_name1")) {
+            backendIdToBackendIP.put(backend.BackendId, host)
+            backendIdToBackendHttpPort.put(backend.BackendId, backend.HttpPort)
+            backendIdToBackendBrpcPort.put(backend.BackendId, backend.BrpcPort)
         }
     }
     assertEquals(backendIdToBackendIP.size(), 1)
@@ -41,6 +60,19 @@ suite("test_ttl_preempt") {
         httpTest {
             endpoint ""
             uri url
+            op "get"
+            body ""
+            check check_func
+        }
+    }
+
+    def resetUrl = backendIdToBackendIP.get(backendId) + ":" + backendIdToBackendHttpPort.get(backendId) + """/api/file_cache?op=reset&capacity="""
+    def capacity = "0"
+    logger.info(resetUrl)
+    def resetFileCache = { check_func ->
+        httpTest {
+            endpoint ""
+            uri resetUrl + capacity
             op "get"
             body ""
             check check_func
@@ -61,13 +93,12 @@ suite("test_ttl_preempt") {
         |"AWS_ACCESS_KEY" = "${getS3AK()}",
         |"AWS_SECRET_KEY" = "${getS3SK()}",
         |"AWS_ENDPOINT" = "${getS3Endpoint()}",
-        |"AWS_REGION" = "${getS3Region()}",
-        |"provider" = "${getS3Provider()}")
+        |"AWS_REGION" = "${getS3Region()}")
         |PROPERTIES(
         |"exec_mem_limit" = "8589934592",
         |"load_parallelism" = "3")""".stripMargin()
-    
-    
+
+
     sql new File("""${context.file.parent}/../ddl/customer_ttl_delete.sql""").text
     sql new File("""${context.file.parent}/../ddl/customer_delete.sql""").text
     def load_customer_ttl_once =  { String table ->
@@ -124,43 +155,24 @@ suite("test_ttl_preempt") {
 
     // one customer table would take about 1.3GB, the total cache size is 20GB
     // the following would take 19.5G all
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-    load_customer_once("customer")
-
-    getMetricsMethod.call() {
-        respCode, body ->
-            assertEquals("${respCode}".toString(), "200")
-            String out = "${body}".toString()
-            def strs = out.split('\n')
-            Boolean flag1 = false;
-            for (String line in strs) {
-                if (flag1) break;
-                if (line.contains("ttl_cache_size")) {
-                    if (line.startsWith("#")) {
-                        continue
-                    }
-                    def i = line.indexOf(' ')
-                    assertEquals(line.substring(i).toLong(), 0)
-                    flag1 = true
-                }
-            }
-            assertTrue(flag1)
-    }
-    // will cache all datas
     load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+
+    // The max ttl cache size is 90% cache capacity
+    long ttl_cache_size = 0
     sleep(30000)
     getMetricsMethod.call() {
         respCode, body ->
@@ -175,10 +187,111 @@ suite("test_ttl_preempt") {
                         continue
                     }
                     def i = line.indexOf(' ')
-                    assertTrue(line.substring(i).toLong() > 1073741824)
+                    ttl_cache_size = line.substring(i).toLong()
+                    logger.info("current ttl_cache_size " + ttl_cache_size);
+                    assertTrue(ttl_cache_size <= 19327352832)
                     flag1 = true
                 }
             }
             assertTrue(flag1)
     }
+    capacity = "-1"
+    resetFileCache.call() {
+        respCode, body -> {
+            assertFalse("${respCode}".toString().equals("200"))
+        }
+    }
+
+    capacity = "-faf13r1r"
+    resetFileCache.call() {
+        respCode, body -> {
+            assertFalse("${respCode}".toString().equals("200"))
+        }
+    }
+
+    capacity = "0"
+    resetFileCache.call() {
+        respCode, body -> {
+            assertFalse("${respCode}".toString().equals("200"))
+        }
+    }
+
+    capacity = "1073741824&path=/xxxxxx" // 1GB
+    resetFileCache.call() {
+        respCode, body -> {
+            assertEquals("${respCode}".toString(), "200")
+        }
+    }
+
+    capacity = "1073741824" // 1GB
+    resetFileCache.call() {
+        respCode, body -> {
+            assertEquals("${respCode}".toString(), "200")
+        }
+    }
+
+    sleep(60000)
+    getMetricsMethod.call() {
+        respCode, body ->
+            assertEquals("${respCode}".toString(), "200")
+            String out = "${body}".toString()
+            def strs = out.split('\n')
+            Boolean flag1 = false;
+            for (String line in strs) {
+                if (flag1) break;
+                if (line.contains("ttl_cache_size")) {
+                    if (line.startsWith("#")) {
+                        continue
+                    }
+                    def i = line.indexOf(' ')
+                    ttl_cache_size = line.substring(i).toLong()
+                    logger.info("current ttl_cache_size " + ttl_cache_size);
+                    assertTrue(ttl_cache_size <= 1073741824)
+                    flag1 = true
+                }
+            }
+            assertTrue(flag1)
+    }
+
+    capacity = "1099511627776" // 1TB
+    resetFileCache.call() {
+        respCode, body -> {
+            assertEquals("${respCode}".toString(), "200")
+        }
+    }
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+    load_customer_ttl_once("customer_ttl")
+
+    sleep(30000)
+    getMetricsMethod.call() {
+        respCode, body ->
+            assertEquals("${respCode}".toString(), "200")
+            String out = "${body}".toString()
+            def strs = out.split('\n')
+            Boolean flag1 = false;
+            for (String line in strs) {
+                if (flag1) break;
+                if (line.contains("ttl_cache_size")) {
+                    if (line.startsWith("#")) {
+                        continue
+                    }
+                    def i = line.indexOf(' ')
+                    ttl_cache_size = line.substring(i).toLong()
+                    logger.info("current ttl_cache_size " + ttl_cache_size);
+                    assertTrue(ttl_cache_size > 1073741824)
+                    flag1 = true
+                }
+            }
+            assertTrue(flag1)
+    }
+
+    capacity = "21474836480" // 20GB
+    resetFileCache.call() {
+        respCode, body -> {
+            assertEquals("${respCode}".toString(), "200")
+        }
+    }
+
+    sql new File("""${context.file.parent}/../ddl/customer_delete.sql""").text
 }
