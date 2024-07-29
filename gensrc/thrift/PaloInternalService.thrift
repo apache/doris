5d442ba2b4d659be4aa1af00dc7f043c58d813e3
qiye <jianliang5669@gmail.com>
2024-07-29 11:15:03 +0800

    [test](inverted index) add an Inverted Index Testing Switch (#38077) (#38448)
    backport #38077
    
    Co-authored-by: zzzxl <33418555+zzzxl1993@users.noreply.github.com>

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -316,6 +316,11 @@ struct TQueryOptions {
   117: optional bool read_csv_empty_line_as_null = false;
 
   118: optional TSerdeDialect serde_dialect = TSerdeDialect.DORIS;
+
+  119: optional bool enable_match_without_inverted_index = true;
+
+  120: optional bool enable_fallback_on_missing_inverted_index = true;
+
   // For cloud, to control if the content would be written into file cache
   1000: optional bool disable_file_cache = false
 }
