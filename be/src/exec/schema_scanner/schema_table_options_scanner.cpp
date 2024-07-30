eb7c068de00aad7da33774f034b80f571a2e5b69
Gabriel <gabrielleebuaa@gmail.com>
2024-07-30 15:31:05 +0800

    update

--- be/src/exec/schema_scanner/schema_table_options_scanner.cpp
+++ be/src/exec/schema_scanner/schema_table_options_scanner.cpp
@@ -103,7 +103,7 @@ Status SchemaTableOptionsScanner::get_block_from_fe() {
     return Status::OK();
 }
 
-Status SchemaTableOptionsScanner::get_next_block(vectorized::Block* block, bool* eos) {
+Status SchemaTableOptionsScanner::get_next_block_internal(vectorized::Block* block, bool* eos) {
     if (!_is_init) {
         return Status::InternalError("Used before initialized.");
     }
