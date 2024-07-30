4dc7e2fe2482898edd95b9b40ee9baa0135101a5
feiniaofeiafei <moailing@selectdb.com>
2024-07-30 16:54:11 +0800

    fix regression

--- regression-test/suites/nereids_rules_p0/push_down_filter/extract_from_disjunction_in_join.groovy
+++ regression-test/suites/nereids_rules_p0/push_down_filter/extract_from_disjunction_in_join.groovy
@@ -20,6 +20,7 @@ suite("extract_from_disjunction_in_join") {
     sql "SET enable_fallback_to_original_planner=false"
     sql "set ignore_shape_nodes='PhysicalDistribute,PhysicalProject'"
     sql "set disable_nereids_rules=PRUNE_EMPTY_PARTITION"
+    sql "set runtime_filter_mode=OFF"
 
 
     sql "drop table if exists extract_from_disjunction_in_join_t1"
