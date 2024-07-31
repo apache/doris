e02a05acb9976d13cb60ae823b76d093214ae392
GoGoWen2021 <GoGoWen2021@gmail.com>
2024-07-31 20:07:35 +0800

    change regression

--- regression-test/suites/rollup_p0/test_rollup_prune_tablet.groovy
+++ regression-test/suites/rollup_p0/test_rollup_prune_tablet.groovy
@@ -15,28 +15,36 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_prune_tablet_mv_original_planner") {
+suite("test_rollup_prune_tablet") {
     sql "SET enable_nereids_planner=false"
 
     sql "DROP TABLE IF EXISTS test_prune_tablet_t2"
     sql """
-	    create table test_prune_tablet_t2(id int, c1 boolean) distributed by hash(id) BUCKETS 16 properties('replication_num'='1');
+	    CREATE TABLE `test_prune_tablet_t2` (
+                     `id` INT NULL,
+                     `c1` BOOLEAN NULL,
+                     `id2` BIGINT NULL,
+                     `id3` BIGINT NULL,
+                     `id4` BIGINT NULL
+                    ) distributed by hash(id) BUCKETS 16 properties('replication_num'='1');
     """
 
     createMV( """
-	    CREATE
-        MATERIALIZED VIEW mv_t2 AS
-        SELECT c1,
-            id
-        FROM test_prune_tablet_t2
-        ORDER BY c1,
-        id; 
+	    alter table test_prune_tablet_t2 add rollup example_rollup_index(c1,id); 
     """)
-    sql "insert into test_prune_tablet_t2 values(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0);"
+
+    sql "insert into test_prune_tablet_t2 values
+            (1,0,1,1,1),
+            (2,0,2,2,2),
+            (3,0,3,3,3),
+            (4,0,4,4,4),
+            (5,0,5,5,5),
+            (6,0,6,6,6),
+            (7,0,7,7,7);"
 
     explain {
-        sql("select * from test_prune_tablet_t2 where c1 = 0 and id = 3;")
-        contains "mv_t2"
+        sql("explain select id,sum(c1) from test_prune_tablet_t2 where c1=0 and id = 3 group by id;")
+        contains "example_rollup_index"
         contains "tablets=1/16"
     }
 
