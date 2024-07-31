6e1f5dcd37fcc0f6ee47a1b0b03d63040be4fbc7
Gabriel <gabrielleebuaa@gmail.com>
2024-07-31 15:29:14 +0800

    [Improvement](schema scan) Use async scanner for schema scanners (#38403)

--- be/src/exec/schema_scanner/schema_schemata_scanner.cpp
+++ be/src/exec/schema_scanner/schema_schemata_scanner.cpp
@@ -81,7 +81,7 @@ Status SchemaSchemataScanner::start(RuntimeState* state) {
     return Status::OK();
 }
 
-Status SchemaSchemataScanner::get_next_block(vectorized::Block* block, bool* eos) {
+Status SchemaSchemataScanner::get_next_block_internal(vectorized::Block* block, bool* eos) {
     if (!_is_init) {
         return Status::InternalError("Used before Initialized.");
     }
