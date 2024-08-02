2077c9dfb776e656c24e412cc88d4098fc743c8d
Luwei <814383175@qq.com>
2024-08-02 16:17:42 +0800

    [enhancement](compaction) reduce the base compaction freeze interval

--- be/src/cloud/config.cpp
+++ be/src/cloud/config.cpp
@@ -35,7 +35,7 @@ DEFINE_Int64(tablet_cache_shards, "16");
 DEFINE_mInt32(tablet_sync_interval_s, "1800");
 
 DEFINE_mInt64(min_compaction_failure_interval_ms, "5000");
-DEFINE_mInt64(base_compaction_freeze_interval_s, "86400");
+DEFINE_mInt64(base_compaction_freeze_interval_s, "7200");
 DEFINE_mInt64(cu_compaction_freeze_interval_s, "1200");
 DEFINE_mInt64(cumu_compaction_interval_s, "1800");
 
