9971c93605eec2972116fa12a2cd9dcbf01d35c4
Dongyang Li <hello_stephen@qq.com>
2024-08-04 10:55:28 +0800

    [ci](branch-3.0) support run cloud_p* and performance (#38696)
    ## Proposed changes
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->
    
    ---------
    
    Co-authored-by: stephen <hello-stephen@qq.com>

--- regression-test/pipeline/cloud_p0/prepare.sh
+++ regression-test/pipeline/cloud_p0/prepare.sh
@@ -47,7 +47,7 @@ if [[ -z "${pr_num_from_trigger}" ]]; then echo "ERROR: env pr_num_from_trigger 
 if [[ -z "${commit_id_from_trigger}" ]]; then echo "ERROR: env commit_id_from_trigger not set" && exit 1; fi
 if [[ -z "${commit_id_from_checkout}" ]]; then echo "ERROR: env commit_id_from_checkout not set" && exit 1; fi
 if [[ -z "${target_branch}" ]]; then echo "ERROR: env target_branch not set" && exit 1; fi
-if [[ -z "${cos_ak}" || -z "${cos_sk}" ]]; then echo "ERROR: env cos_ak or cos_sk not set" && exit 1; fi
+if [[ -z "${s3SourceAk}" || -z "${s3SourceSk}" ]]; then echo "ERROR: env s3SourceAk or s3SourceSk not set" && exit 1; fi
 if [[ -z "${oss_ak}" || -z "${oss_sk}" ]]; then echo "ERROR: env oss_ak or oss_sk not set." && exit 1; fi
 
 echo "#### 1. check if need run"
@@ -67,11 +67,11 @@ fi
 # shellcheck source=/dev/null
 source "$(bash "${teamcity_build_checkoutDir}"/regression-test/pipeline/common/get-or-set-tmp-env.sh 'get')"
 if ${skip_pipeline:=false}; then echo "INFO: skip build pipline" && exit 0; else echo "INFO: no skip"; fi
-if [[ "${target_branch}" == "master" ]]; then
+if [[ "${target_branch}" == "master" || "${target_branch}" == "branch-3.0" ]]; then
     echo "INFO: PR target branch ${target_branch}"
     install_java
 else
-    echo "WARNING: PR target branch ${target_branch} is NOT in (master), skip pipeline."
+    echo "WARNING: PR target branch ${target_branch} is NOT in (master, branch-3.0), skip pipeline."
     bash "${teamcity_build_checkoutDir}"/regression-test/pipeline/common/get-or-set-tmp-env.sh 'set' "export skip_pipeline=true"
     exit 0
 fi
@@ -106,11 +106,11 @@ install_fdb
 clean_fdb "cloud_instance_0"
 
 echo "#### 5. check if binary package ready"
-merge_pr_to_master_commit() {
+merge_pr_to_target_branch_compiled_commit() {
     local pr_num_from_trigger="$1"
     local target_branch="$2"
-    local master_commit="$3"
-    echo "INFO: merge pull request into ${target_branch} ${master_commit}"
+    local target_branch_compiled_commit="$3"
+    echo "INFO: merge pull request into ${target_branch} ${target_branch_compiled_commit}"
     if [[ -z "${teamcity_build_checkoutDir}" ]]; then
         echo "ERROR: env teamcity_build_checkoutDir not set" && return 1
     fi
@@ -119,10 +119,10 @@ merge_pr_to_master_commit() {
     git fetch origin "${target_branch}"
     git checkout "${target_branch}"
     git reset --hard origin/"${target_branch}"
-    git checkout "${master_commit}"
+    git checkout "${target_branch_compiled_commit}"
     returnValue=$?
     if [[ ${returnValue} -ne 0 ]]; then
-        echo "ERROR: checkout ${target_branch} ${master_commit} failed. please rebase to the newest version."
+        echo "ERROR: checkout ${target_branch} ${target_branch_compiled_commit} failed. please rebase to the newest version."
         return 1
     fi
     git rev-parse HEAD
@@ -131,7 +131,7 @@ merge_pr_to_master_commit() {
     echo "git fetch origin refs/pull/${pr_num_from_trigger}/head"
     git fetch origin "refs/pull/${pr_num_from_trigger}/head"
     git merge --no-edit --allow-unrelated-histories FETCH_HEAD
-    echo "INFO: merge refs/pull/${pr_num_from_trigger}/head into ${target_branch} ${master_commit}"
+    echo "INFO: merge refs/pull/${pr_num_from_trigger}/head into ${target_branch} ${target_branch_compiled_commit}"
     # CONFLICTS=$(git ls-files -u | wc -l)
     if [[ $(git ls-files -u | wc -l) -gt 0 ]]; then
         echo "ERROR: merge refs/pull/${pr_num_from_trigger}/head into  failed. Aborting"
@@ -144,11 +144,11 @@ if ! check_oss_file_exist "${pr_num_from_trigger}_${commit_id_from_trigger}.tar.
 if download_oss_file "${pr_num_from_trigger}_${commit_id_from_trigger}.tar.gz"; then
     rm -rf "${teamcity_build_checkoutDir}"/output
     tar -I pigz -xf "${pr_num_from_trigger}_${commit_id_from_trigger}.tar.gz"
-    master_commit_file="master.commit"
-    if [[ -e output/${master_commit_file} ]]; then
+    target_branch_compiled_commit_file="master.commit"
+    if [[ -e output/${target_branch_compiled_commit_file} ]]; then
         # checkout to master commit and merge this pr, to ensure binary and case are same version
-        master_commit=$(cat output/"${master_commit_file}")
-        if merge_pr_to_master_commit "${pr_num_from_trigger}" "${target_branch}" "${master_commit}"; then
+        target_branch_compiled_commit=$(cat output/"${target_branch_compiled_commit_file}")
+        if merge_pr_to_target_branch_compiled_commit "${pr_num_from_trigger}" "${target_branch}" "${target_branch_compiled_commit}"; then
             echo "INFO: merged done"
             if [[ "${teamcity_buildType_id:-}" == "Doris_DorisCloudRegression_CloudP1" ]]; then
                 echo "INFO: 用cloud_p1/conf覆盖cloud_p0/conf"
--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -80,8 +80,7 @@ excludeDirectories = "000_the_start_sentinel_do_not_touch," + // keep this line 
     "cloud_p0," +
     "nereids_rules_p0/subquery," +
     "workload_manager_p1," +
-    "zzz_the_end_sentinel_do_not_touch," +
-    "dialect_compatible"// keep this line as the last line
+    "zzz_the_end_sentinel_do_not_touch"// keep this line as the last line
 
 customConf1 = "test_custom_conf_value"
 
@@ -137,10 +136,7 @@ es_8_port=39200
 
 cacheDataPath = "/data/regression/"
 
-s3Endpoint = "cos.ap-hongkong.myqcloud.com"
-s3BucketName = "doris-build-hk-1308700295"
-s3Region = "ap-hongkong"
-s3Provider = "COS"
+s3Source="aliyun"
 
 //arrow flight sql test config
 extArrowFlightSqlHost = "127.0.0.1"
