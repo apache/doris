864a58935c54fb9763a87fb5c8c97be1ca4a08d8
starocean999 <12095047@qq.com>
2024-08-05 16:09:03 +0800

    [fix](nereids)support group_concat with distinct and order by

--- regression-test/suites/query_p0/group_concat/test_group_concat.groovy
+++ regression-test/suites/query_p0/group_concat/test_group_concat.groovy
@@ -31,21 +31,10 @@ suite("test_group_concat", "query,p0,arrow_flight_sql") {
     qt_select """
                 SELECT abs(k3), group_concat(cast(abs(k2) as varchar), ":" order by abs(k2), k1) FROM test_query_db.baseall group by abs(k3) order by abs(k3)
               """
-    test {
-        sql"""SELECT abs(k3), group_concat(distinct cast(abs(k2) as char) order by abs(k1), k2) FROM test_query_db.baseall group by abs(k3) order by abs(k3);"""
-        check{result, exception, startTime, endTime ->
-            assertTrue(exception != null)
-            logger.info(exception.message)
-        }
-    }
 
-    test {
-        sql"""SELECT abs(k3), group_concat(distinct cast(abs(k2) as char), ":" order by abs(k1), k2) FROM test_query_db.baseall group by abs(k3) order by abs(k3);"""
-        check{result, exception, startTime, endTime ->
-            assertTrue(exception != null)
-            logger.info(exception.message)
-        }
-    }
+    sql"""SELECT abs(k3), group_concat(distinct cast(abs(k2) as char) order by abs(k1), k2) FROM test_query_db.baseall group by abs(k3) order by abs(k3);"""
+
+    sql"""SELECT abs(k3), group_concat(distinct cast(abs(k2) as char), ":" order by abs(k1), k2) FROM test_query_db.baseall group by abs(k3) order by abs(k3);"""
 
     qt_select """
                 SELECT count(distinct k7), group_concat(k6 order by k6) FROM test_query_db.baseall;
