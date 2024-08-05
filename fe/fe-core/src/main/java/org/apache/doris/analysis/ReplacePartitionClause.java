3b1bb2e44535e531d83e9f6b452ff2f2a598339a
walter <w41ter.l@gmail.com>
2024-08-05 20:59:40 +0800

    [fix](fe) Fix the default value of ReplacePartitionClause.isStrictRange (#38688) (#38881)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ReplacePartitionClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ReplacePartitionClause.java
@@ -61,6 +61,15 @@ public class ReplacePartitionClause extends AlterTableClause {
         this.tempPartitionNames = tempPartitionNames;
         this.needTableStable = false;
         this.properties = properties;
+
+        // ATTN: During ReplacePartitionClause.analyze(), the default value of isStrictRange is true.
+        // However, ReplacePartitionClause instances constructed by internal code do not call analyze(),
+        // so their isStrictRange value is incorrect (e.g., INSERT INTO ... OVERWRITE).
+        //
+        // Considering this, we should handle the relevant properties when constructing.
+        this.isStrictRange = getBoolProperty(properties, PropertyAnalyzer.PROPERTIES_STRICT_RANGE, true);
+        this.useTempPartitionName = getBoolProperty(
+                properties, PropertyAnalyzer.PROPERTIES_USE_TEMP_PARTITION_NAME, false);
     }
 
     public List<String> getPartitionNames() {
@@ -120,5 +129,13 @@ public class ReplacePartitionClause extends AlterTableClause {
     @Override
     public String toString() {
         return toSql();
+    }
+
+    public static boolean getBoolProperty(Map<String, String> properties, String propKey, boolean defaultVal) {
+        if (properties != null && properties.containsKey(propKey)) {
+            String val = properties.get(propKey);
+            return Boolean.parseBoolean(val);
+        }
+        return defaultVal;
     }
 }
