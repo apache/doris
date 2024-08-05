da22c35e7e8b5b26f266971be37d819a2cfc4660
bobhan1 <bh2444151092@outlook.com>
2024-08-05 17:43:59 +0800

    fix cluster key

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/DeleteFromCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/DeleteFromCommand.java
@@ -399,6 +399,9 @@ public class DeleteFromCommand extends Command implements ForwardWithSync, Expla
         List<String> cols = Lists.newArrayList();
         boolean isMow = targetTable.getEnableUniqueKeyMergeOnWrite();
         String tableName = tableAlias != null ? tableAlias : targetTable.getName();
+        boolean hasClusterKey = targetTable.getBaseSchema().stream().anyMatch(Column::isClusterKey);
+        // currently cluster key doesn't support partial update, so we can't convert
+        // a delete stmt to partial update load if the table has cluster key
         for (Column column : targetTable.getFullSchema()) {
             NamedExpression expr = null;
             if (column.getName().equalsIgnoreCase(Column.DELETE_SIGN)) {
@@ -410,6 +413,8 @@ public class DeleteFromCommand extends Command implements ForwardWithSync, Expla
                 expr = new UnboundSlot(tableName, column.getName());
             } else if (!isMow && (!column.isVisible() || (!column.isAllowNull() && !column.hasDefaultValue()))) {
                 expr = new UnboundSlot(tableName, column.getName());
+            } else if (hasClusterKey) {
+                expr = new UnboundSlot(tableName, column.getName());
             } else {
                 continue;
             }
@@ -419,7 +424,7 @@ public class DeleteFromCommand extends Command implements ForwardWithSync, Expla
 
         logicalQuery = new LogicalProject<>(selectLists, logicalQuery);
 
-        boolean isPartialUpdate = isMow && cols.size() < targetTable.getColumns().size();
+        boolean isPartialUpdate = isMow && !hasClusterKey && cols.size() < targetTable.getColumns().size();
         logicalQuery = handleCte(logicalQuery);
         // make UnboundTableSink
         return UnboundTableSinkCreator.createUnboundTableSink(nameParts, cols, ImmutableList.of(),
