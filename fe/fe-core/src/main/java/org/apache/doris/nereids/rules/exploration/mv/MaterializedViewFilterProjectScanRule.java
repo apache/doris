425a34586854e37fec351adb4c2104fdfb43be8c
seawinde <daydayup005@yeah.net>
2024-08-07 16:24:11 +0800

    [fix](mtmv) Fix rewrite fail when query direct external table without group by

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializedViewFilterProjectScanRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializedViewFilterProjectScanRule.java
@@ -19,8 +19,9 @@ package org.apache.doris.nereids.rules.exploration.mv;
 
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.logical.LogicalCatalogRelation;
 import org.apache.doris.nereids.trees.plans.logical.LogicalFilter;
-import org.apache.doris.nereids.trees.plans.logical.LogicalOlapScan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
 
 import com.google.common.collect.ImmutableList;
@@ -37,9 +38,11 @@ public class MaterializedViewFilterProjectScanRule extends AbstractMaterializedV
     @Override
     public List<Rule> buildRules() {
         return ImmutableList.of(
-                logicalFilter(logicalProject(logicalOlapScan())).thenApplyMultiNoThrow(ctx -> {
-                    LogicalFilter<LogicalProject<LogicalOlapScan>> root = ctx.root;
-                    return rewrite(root, ctx.cascadesContext);
-                }).toRule(RuleType.MATERIALIZED_VIEW_FILTER_PROJECT_SCAN));
+                logicalFilter(logicalProject(any().when(LogicalCatalogRelation.class::isInstance)))
+                        .thenApplyMultiNoThrow(
+                                ctx -> {
+                                    LogicalFilter<LogicalProject<Plan>> root = ctx.root;
+                                    return rewrite(root, ctx.cascadesContext);
+                                }).toRule(RuleType.MATERIALIZED_VIEW_FILTER_PROJECT_SCAN));
     }
 }
