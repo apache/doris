146c6819ea13c2814549792239e155eed2e368f1
englefly <englefly@gmail.com>
2024-08-07 22:20:08 +0800

    fmt

--- fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
@@ -696,7 +696,8 @@ public class StatsCalculator extends DefaultPlanVisitor<Statistics, Void> {
     public Statistics visitPhysicalStorageLayerAggregate(
             PhysicalStorageLayerAggregate storageLayerAggregate, Void context) {
         PhysicalRelation relation = storageLayerAggregate.getRelation();
-        Preconditions.checkArgument(relation instanceof PhysicalOlapScan, "StorageLayerAggregate only support OlapTable");
+        Preconditions.checkArgument(relation instanceof PhysicalOlapScan,
+            "StorageLayerAggregate only support OlapTable");
         return relation.accept(this, context);
 
     }
