bbe2847061ff771c5b5e16477e923bc92b015f85
amorynan <wangqiannan@selectdb.com>
2024-08-12 15:46:18 +0800

    fixed

--- regression-test/suites/datatype_p0/nested_types/ddl/create_nestedtypes_with_schemachange.groovy
+++ regression-test/suites/datatype_p0/nested_types/ddl/create_nestedtypes_with_schemachange.groovy
@@ -17,11 +17,11 @@
 
 suite("create_nestedtypes_with_schemachange", "p0") {
 
-    def create_nested_table_and_schema_change = {testTablex, nested_type, column_name ->
+    def create_nested_table_and_schema_change = {testTablex, nested_type, column_name, error ->
         // create basic type
         sql "DROP TABLE IF EXISTS $testTablex"
         sql """ CREATE TABLE $testTablex (
-                     col0 BIGINT NOT NULL,  col2 int NOT NULL
+                     col0 BIGINT NOT NULL,  col2 int NOT NULL, col3 array<int> NULL, col4 map<int, int> NULL, col5 struct<f1: int> NULL
                 )
                 /* mow */
                 UNIQUE KEY(col0) DISTRIBUTED BY HASH(col0) BUCKETS 4 PROPERTIES (
@@ -29,18 +29,37 @@ suite("create_nestedtypes_with_schemachange", "p0") {
                   "replication_num" = "1"
                 ); """
         // alter table add nested type
-        sql "ALTER TABLE $testTablex ADD COLUMN $column_name $nested_type"
-        // alter table modify column order
-        sql "ALTER TABLE $testTablex MODIFY COLUMN $column_name $nested_type AFTER col0"
+        if (error != '') {
+            // check nested type do not support other type
+            test {
+               sql "ALTER TABLE $testTablex MODIFY COLUMN $column_name $nested_type AFTER col0"
+               exception (error)
+            }
+        } else {
+            // check nested type can only support change order
+            sql "ALTER TABLE $testTablex ADD COLUMN $column_name $nested_type"
+            sql "ALTER TABLE $testTablex MODIFY COLUMN $column_name $nested_type AFTER col0"
+            waitForSchemaChangeDone {
+                sql """ SHOW ALTER TABLE COLUMN WHERE IndexName='$testTablex' ORDER BY createtime DESC LIMIT 1 """
+                time 600
+            }
+        }
         // desc table
         qt_sql "DESC $testTablex"
     }
 
     // array
-    create_nested_table_and_schema_change.call("test_array_schemachange", "ARRAY<STRING>", "col_array")
+    create_nested_table_and_schema_change.call("test_array_schemachange", "ARRAY<STRING>", "col_array", '')
     // map
-    create_nested_table_and_schema_change.call("test_map_schemachange", "MAP<char(32), string>", "col_map")
+    create_nested_table_and_schema_change.call("test_map_schemachange", "MAP<char(32), string>", "col_map", '')
     // struct
-    create_nested_table_and_schema_change.call("test_struct_schemachange", "STRUCT<f1: varchar(1)>", "col_struct")
+    create_nested_table_and_schema_change.call("test_struct_schemachange", "STRUCT<f1: varchar(1)>", "col_struct", '')
+
+    // array with other type
+    create_nested_table_and_schema_change.call("test_array_schemachange_1", "ARRAY<STRING>", "col3", "errCode = 2");
+    // map with other type
+    create_nested_table_and_schema_change.call("test_map_schemachange_1", "MAP<char(32), string>", "col4", "errCode = 2");
+    // struct with other type
+    create_nested_table_and_schema_change.call("test_struct_schemachange_1", "STRUCT<f1: varchar(1)>", "col5", "errCode = 2");
 
 }
