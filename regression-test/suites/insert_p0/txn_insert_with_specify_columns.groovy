902f312e42b0b85ee2a982eec6ce6853e9034a5c
Yukang-Lian <Yukang.Lian2022@gmail.com>
2024-08-13 21:25:10 +0800

    4

--- regression-test/suites/insert_p0/txn_insert_with_specify_columns.groovy
+++ regression-test/suites/insert_p0/txn_insert_with_specify_columns.groovy
@@ -32,13 +32,39 @@ suite("txn_insert_with_specify_columns", "p0") {
         );
     """
     sql """begin"""
-    sql """insert into ${table} (c1, c3, c2) values(10, 30, 20);"""
+    sql """insert into ${table} (c1, c3, c2) values(1, 3, 2),(111,333,222),(1111,3333,2222);"""
+    sql """insert into ${table} (c3, c1) values(31, 11);"""
+    sql """insert into ${table} (c2, c1) values(22, 12);"""
+    sql """insert into ${table} (c2, c3, c1) values(23, 33, 13);"""
+    sql """insert into ${table} (c1) values(14);"""
+    sql """insert into ${table} (c3, c2, c1) values(35, 25, 15);"""
+    sql """insert into ${table} (__DORIS_DELETE_SIGN__, c3, c2, c1) values(1, 35, 25, 15);"""
+    sql """commit"""
+    sql """set show_hidden_columns=true"""
+    qt_select_unique """select c1,c2,c3,__DORIS_DELETE_SIGN__ from ${table} order by c1,c2,c3"""
+
+    sql """ DROP TABLE IF EXISTS ${table}"""
+    sql """
+        CREATE TABLE ${table}(
+            c1 INT NULL,
+            c2 INT NULL,
+            c3 INT NULL default 1
+        ) ENGINE=OLAP
+        DUPLICATE KEY(c1)
+        DISTRIBUTED BY HASH(c1) BUCKETS 3
+        PROPERTIES (
+            "replication_num" = "1"
+        );
+    """
+    sql """begin"""
+    sql """insert into ${table} (c1, c3, c2) values(1, 3, 2),(111,333,222),(1111,3333,2222);"""
     sql """insert into ${table} (c3, c1) values(31, 11);"""
     sql """insert into ${table} (c2, c1) values(22, 12);"""
     sql """insert into ${table} (c2, c3, c1) values(23, 33, 13);"""
     sql """insert into ${table} (c1) values(14);"""
     sql """insert into ${table} (c3, c2, c1) values(35, 25, 15);"""
     sql """commit"""
+    qt_select_unique """select c1,c2,c3 from ${table} order by c1,c2,c3"""
 
     try {
         sql """set enable_nereids_planner=false"""
@@ -50,6 +76,33 @@ suite("txn_insert_with_specify_columns", "p0") {
                 c3 INT NULL default 1
             ) ENGINE=OLAP
             UNIQUE KEY(c1)
+            DISTRIBUTED BY HASH(c1) BUCKETS 3
+            PROPERTIES (
+                "replication_num" = "1"
+            );
+        """
+        sql """begin"""
+        sql """insert into ${table} (c1, c3, c2) values(10, 30, 20);"""
+        logger.info(failed)
+        assertFalse(true);
+    } catch (Exception e) {
+        logger.info(e.getMessage())
+        assertTrue(e.getMessage().contains("The legacy planner does not support specifying column names"))
+    } finally {
+        sql "commit"
+        sql """ DROP TABLE IF EXISTS ${table}"""
+    }
+
+    try {
+        sql """set enable_nereids_planner=false"""
+        sql """ DROP TABLE IF EXISTS ${table}"""
+        sql """
+            CREATE TABLE ${table}(
+                c1 INT NULL,
+                c2 INT NULL,
+                c3 INT NULL default 1
+            ) ENGINE=OLAP
+            DUPLICATE KEY(c1)
             DISTRIBUTED BY HASH(c1) BUCKETS 3
             PROPERTIES (
                 "replication_num" = "1"
