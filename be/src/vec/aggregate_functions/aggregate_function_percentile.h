4c054c299ac7017409d2c324c22d28e9af074a85
zhangstar333 <2561612514@qq.com>
2024-08-13 21:06:01 +0800

    [Bug](compatibility) fix percentile_approx function coredump when upgrade

--- be/src/vec/aggregate_functions/aggregate_function_percentile.h
+++ be/src/vec/aggregate_functions/aggregate_function_percentile.h
@@ -583,7 +583,7 @@ public:
 template <typename T>
 struct PercentileState {
     mutable std::vector<Counts<T>> vec_counts;
-    std::vector<double> vec_quantile;
+    std::vector<double> vec_quantile {-1};
     bool inited_flag = false;
 
     void write(BufferWritable& buf) const {
