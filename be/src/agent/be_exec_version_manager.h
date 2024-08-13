480e1c27bf33fe223f5fb14d851e8c26aa896d33
jacktengg <18241664+jacktengg@users.noreply.github.com>
2024-08-13 10:20:36 +0800

    [fix](window_funnel) fix wrong result of window_funnel #38954

--- be/src/agent/be_exec_version_manager.h
+++ be/src/agent/be_exec_version_manager.h
@@ -71,7 +71,7 @@ private:
  *    g. do local merge of remote runtime filter
  *    h. "now": ALWAYS_NOT_NULLABLE -> DEPEND_ON_ARGUMENTS
 */
-constexpr inline int BeExecVersionManager::max_be_exec_version = 4;
+constexpr inline int BeExecVersionManager::max_be_exec_version = 5;
 constexpr inline int BeExecVersionManager::min_be_exec_version = 0;
 
 /// functional
