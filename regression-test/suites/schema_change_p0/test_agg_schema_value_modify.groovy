92b538782f4d21d868c604c23dd28c9eaa794e7c
cjj2010 <2449402815@qq.com>
2024-08-14 18:03:02 +0800

    [enhancement](regression-test) agg schema value modify case

--- regression-test/suites/schema_change_p0/test_agg_schema_value_modify.groovy
+++ regression-test/suites/schema_change_p0/test_agg_schema_value_modify.groovy
@@ -1,5 +1,5 @@
 // Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license aggreements.  See the NOTICE file
+// or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
 // regarding copyright ownership.  The ASF licenses this file
 // to you under the Apache License, Version 2.0 (the
@@ -8,11 +8,11 @@
 //
 //   http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or aggreed to in writing,
+// Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
-// specific languagge governing permissions and limitations
+// specific language governing permissions and limitations
 // under the License.
 
 suite("test_agg_schema_value_modify","p0") {
