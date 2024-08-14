apache/doris

commit 2e651bbc9a88de1ca99060a15281711660137d37

[fix](nereids) fix some planner bugs (#21533)
1. allow cast boolean as date like type in nereids, the result is null
2. PruneOlapScanTablet rule can prune tablet even if a mv index is selected.
3. constant conjunct should not be pushed through agg node in old planner

fe/fe-core/src/main/java/org/apache/doris/planner/HashDistributionPruner.java
@@ -17,6 +17,7 @@
 
 package org.apache.doris.planner;
 
+import org.apache.doris.analysis.CreateMaterializedViewStmt;
 import org.apache.doris.analysis.InPredicate;
 import org.apache.doris.analysis.LiteralExpr;
 import org.apache.doris.analysis.SlotRef;
@@ -58,12 +59,15 @@ public class HashDistributionPruner implements DistributionPruner {
     private Map<String, PartitionColumnFilter> distributionColumnFilters;
     private int                                hashMod;
 
+    private boolean isBaseIndexSelected;
+
     public HashDistributionPruner(List<Long> bucketsList, List<Column> columns,
-                           Map<String, PartitionColumnFilter> filters, int hashMod) {
+                           Map<String, PartitionColumnFilter> filters, int hashMod, boolean isBaseIndexSelected) {
         this.bucketsList = bucketsList;
         this.distributionColumns = columns;
         this.distributionColumnFilters = filters;
         this.hashMod = hashMod;
+        this.isBaseIndexSelected = isBaseIndexSelected;
     }
 
     // columnId: which column to compute
@@ -75,7 +79,11 @@ public class HashDistributionPruner implements DistributionPruner {
             return Lists.newArrayList(bucketsList.get((int) ((hashValue & 0xffffffff) % hashMod)));
         }
         Column keyColumn = distributionColumns.get(columnId);
-        PartitionColumnFilter filter = distributionColumnFilters.get(keyColumn.getName());
+        String columnName = isBaseIndexSelected ? keyColumn.getName()
+                : org.apache.doris.nereids.rules.rewrite.mv.AbstractSelectMaterializedIndexRule
+                        .normalizeName(
+                                CreateMaterializedViewStmt.mvColumnBuilder(keyColumn.getName()));
+        PartitionColumnFilter filter = distributionColumnFilters.get(columnName);
         if (null == filter) {
             // no filter in this column, no partition Key
             // return all subPartition

fe/fe-core/src/main/java/org/apache/doris/analysis/Analyzer.java
@@ -40,6 +40,7 @@ import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.IdGenerator;
 import org.apache.doris.common.Pair;
 import org.apache.doris.common.util.TimeUtils;
+import org.apache.doris.planner.AggregationNode;
 import org.apache.doris.planner.PlanNode;
 import org.apache.doris.planner.RuntimeFilter;
 import org.apache.doris.qe.ConnectContext;
@@ -2416,10 +2417,11 @@ public class Analyzer {
      * Wrapper around getUnassignedConjuncts(List<TupleId> tupleIds).
      */
     public List<Expr> getUnassignedConjuncts(PlanNode node) {
-        // constant conjuncts should be push down to all leaf node.
+        // constant conjuncts should be push down to all leaf node except agg node.
+        // (see getPredicatesBoundedByGroupbysSourceExpr method)
         // so we need remove constant conjuncts when expr is not a leaf node.
         List<Expr> unassigned = getUnassignedConjuncts(node.getTblRefIds());
-        if (!node.getChildren().isEmpty()) {
+        if (!node.getChildren().isEmpty() && !(node instanceof AggregationNode)) {
             unassigned = unassigned.stream()
                     .filter(e -> !e.isConstant()).collect(Collectors.toList());
         }

fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/check/CheckCast.java
@@ -59,20 +59,16 @@ public class CheckCast extends AbstractExpressionRewriteRule {
 
     /**
      * forbid this original and target type
-     *   1. boolean to date, datev2, datetime, datetimev2
-     *   2. original type is object type
-     *   3. target type is object type
-     *   4. original type is same with target type
-     *   5. target type is null type
+     *   1. original type is object type
+     *   2. target type is object type
+     *   3. original type is same with target type
+     *   4. target type is null type
      */
     private boolean checkPrimitiveType(DataType originalType, DataType targetType) {
         if (!originalType.isPrimitive() || !targetType.isPrimitive()) {
             return false;
         }
         if (originalType.equals(targetType)) {
-            return false;
-        }
-        if (originalType.isBooleanType() && targetType.isDateLikeType()) {
             return false;
         }
         if (originalType.isNullType()) {

fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/PruneOlapScanTablet.java
@@ -58,8 +58,11 @@ public class PruneOlapScanTablet extends OneRewriteRuleFactory {
                     for (Long id : olapScan.getSelectedPartitionIds()) {
                         Partition partition = table.getPartition(id);
                         MaterializedIndex index = partition.getIndex(olapScan.getSelectedIndexId());
-                        selectedTabletIdsBuilder.addAll(getSelectedTabletIds(filter.getConjuncts(),
-                                index, partition.getDistributionInfo()));
+                        selectedTabletIdsBuilder
+                                .addAll(getSelectedTabletIds(filter.getConjuncts(), index,
+                                        olapScan.getSelectedIndexId() == olapScan.getTable()
+                                                .getBaseIndexId(),
+                                        partition.getDistributionInfo()));
                     }
                     List<Long> selectedTabletIds = selectedTabletIdsBuilder.build();
                     if (new HashSet(selectedTabletIds).equals(new HashSet(olapScan.getSelectedTabletIds()))) {
@@ -70,7 +73,7 @@ public class PruneOlapScanTablet extends OneRewriteRuleFactory {
     }
 
     private Collection<Long> getSelectedTabletIds(Set<Expression> expressions,
-            MaterializedIndex index, DistributionInfo info) {
+            MaterializedIndex index, boolean isBaseIndexSelected, DistributionInfo info) {
         if (info.getType() != DistributionInfoType.HASH) {
             return index.getTabletIdsInOrder();
         }
@@ -81,7 +84,8 @@ public class PruneOlapScanTablet extends OneRewriteRuleFactory {
         return new HashDistributionPruner(index.getTabletIdsInOrder(),
                 hashInfo.getDistributionColumns(),
                 filterMap,
-                hashInfo.getBucketNum()
+                hashInfo.getBucketNum(),
+                isBaseIndexSelected
         ).prune();
     }
 }

fe/fe-core/src/main/java/org/apache/doris/planner/OlapScanNode.java
@@ -692,7 +692,8 @@ public class OlapScanNode extends ScanNode {
                 distributionPruner = new HashDistributionPruner(table.getTabletIdsInOrder(),
                         info.getDistributionColumns(),
                         columnFilters,
-                        info.getBucketNum());
+                        info.getBucketNum(),
+                        getSelectedIndexId() == olapTable.getBaseIndexId());
                 return distributionPruner.prune();
             }
             case RANDOM: {

fe/fe-core/src/test/java/org/apache/doris/planner/HashDistributionPrunerTest.java
@@ -91,7 +91,7 @@ public class HashDistributionPrunerTest {
         filters.put("channel", channelFilter);
         filters.put("shop_type", shopTypeFilter);
 
-        HashDistributionPruner pruner = new HashDistributionPruner(tabletIds, columns, filters, tabletIds.size());
+        HashDistributionPruner pruner = new HashDistributionPruner(tabletIds, columns, filters, tabletIds.size(), true);
 
         Collection<Long> results = pruner.prune();
         // 20 = 1 * 5 * 2 * 2 * 1 (element num of each filter)

fe/fe-core/src/test/java/org/apache/doris/planner/OlapScanNodeTest.java
@@ -69,7 +69,8 @@ public class OlapScanNodeTest {
                 partitions,
                 columns,
                 filterMap,
-                3);
+                3,
+                true);
 
         Collection<Long> ids = partitionPruner.prune();
         Assert.assertEquals(ids.size(), 1);
@@ -112,7 +113,8 @@ public class OlapScanNodeTest {
                 partitions,
                 columns,
                 filterMap,
-                3);
+                3,
+                true);
 
         Collection<Long> ids = partitionPruner.prune();
         Assert.assertEquals(ids.size(), 3);

regression-test/data/correctness_p0/test_constant_having.out
@@ -1,0 +1,5 @@
+-- This file is automatically generated. You should know what you did if you want to edit this
+-- !sql1 --
+
+-- !sql2 --
+

commit f370e35d7b26dc4719847c05dd837b837447dd2a

[Fix](MV) query not hit partition in original planner (#38565)
this issue seems introduced by #21533

Fix issue that query not hit partition in original planner, which will
cause serve performance degradation.

fe/fe-core/src/main/java/org/apache/doris/planner/HashDistributionPruner.java
@@ -24,6 +24,7 @@ import org.apache.doris.analysis.SlotRef;
 import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.PartitionKey;
 import org.apache.doris.common.Config;
+import org.apache.doris.qe.ConnectContext;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -79,10 +80,13 @@ public class HashDistributionPruner implements DistributionPruner {
             return Lists.newArrayList(bucketsList.get((int) ((hashValue & 0xffffffff) % hashMod)));
         }
         Column keyColumn = distributionColumns.get(columnId);
-        String columnName = isBaseIndexSelected ? keyColumn.getName()
-                : org.apache.doris.nereids.rules.rewrite.mv.AbstractSelectMaterializedIndexRule
+        String columnName = keyColumn.getName();
+        if (!isBaseIndexSelected && ConnectContext.get().getSessionVariable().isEnableNereidsPlanner()) {
+            columnName = org.apache.doris.nereids.rules.rewrite.mv.AbstractSelectMaterializedIndexRule
                         .normalizeName(
                                 CreateMaterializedViewStmt.mvColumnBuilder(keyColumn.getName()));
+        }
+
         PartitionColumnFilter filter = distributionColumnFilters.get(columnName);
         if (null == filter) {
             // no filter in this column, no partition Key

regression-test/suites/rollup_p0/test_rollup_prune_tablet.groovy
@@ -1,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_rollup_prune_tablet") {
+    sql "SET enable_nereids_planner=false"
+
+    sql "DROP TABLE IF EXISTS test_prune_tablet_t2"
+    sql """
+	    CREATE TABLE `test_prune_tablet_t2` (
+                     `id` INT NULL,
+                     `c1` BOOLEAN NULL,
+                     `id2` BIGINT NULL,
+                     `id3` BIGINT NULL,
+                     `id4` BIGINT NULL
+                    ) distributed by hash(id) BUCKETS 16 properties('replication_num'='1');
+    """
+
+    createMV( """
+	    alter table test_prune_tablet_t2 add rollup example_rollup_index(c1,id); 
+    """)
+
+    sql """insert into test_prune_tablet_t2 values
+            (1,0,1,1,1),
+            (2,0,2,2,2),
+            (3,0,3,3,3),
+            (4,0,4,4,4),
+            (5,0,5,5,5),
+            (6,0,6,6,6),
+            (7,0,7,7,7);"""
+
+    explain {
+        sql("select id,sum(c1) from test_prune_tablet_t2 where c1=0 and id = 3 group by id;")
+        contains "example_rollup_index"
+        contains "tablets=1/16"
+    }
+
+    sql "DROP TABLE IF EXISTS test_prune_tablet_t2"
+}