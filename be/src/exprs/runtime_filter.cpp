7e7ee67ce6e11a6f1fcb44252be4aae2fe77adc8
Pxl <pxl290@qq.com>
2024-08-14 14:08:10 +0800

    [Improvement](runtime-filter) adjust always true judge rule of runtime filter apply (#38300)
    ## Proposed changes
    adjust always true judge of runtime filter apply
    tpch sf1000:
    <img width="298" alt="图片"
    src="https://github.com/user-attachments/assets/17f06b50-8d78-4213-8f03-860a93bb1bea">
    
    tpcds sf1000:
    <img width="298" alt="图片"
    src="https://github.com/user-attachments/assets/7c51b60c-d768-4291-8541-740878e8c5b7">

--- be/src/exprs/runtime_filter.cpp
+++ be/src/exprs/runtime_filter.cpp
@@ -1827,7 +1827,9 @@ Status RuntimePredicateWrapper::get_push_exprs(
         node.__set_is_nullable(false);
         auto in_pred = vectorized::VDirectInPredicate::create_shared(node, _context->hybrid_set);
         in_pred->add_child(probe_ctx->root());
-        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(node, in_pred, null_aware);
+        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(
+                node, in_pred, get_in_list_ignore_thredhold(_context->hybrid_set->size()),
+                null_aware);
         container.push_back(wrapper);
         break;
     }
@@ -1842,8 +1844,8 @@ Status RuntimePredicateWrapper::get_push_exprs(
                                        min_literal));
         min_pred->add_child(probe_ctx->root());
         min_pred->add_child(min_literal);
-        container.push_back(
-                vectorized::VRuntimeFilterWrapper::create_shared(min_pred_node, min_pred));
+        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(
+                min_pred_node, min_pred, get_comparison_ignore_thredhold()));
         break;
     }
     case RuntimeFilterType::MAX_FILTER: {
@@ -1857,8 +1859,8 @@ Status RuntimePredicateWrapper::get_push_exprs(
                                        max_literal));
         max_pred->add_child(probe_ctx->root());
         max_pred->add_child(max_literal);
-        container.push_back(
-                vectorized::VRuntimeFilterWrapper::create_shared(max_pred_node, max_pred));
+        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(
+                max_pred_node, max_pred, get_comparison_ignore_thredhold()));
         break;
     }
     case RuntimeFilterType::MINMAX_FILTER: {
@@ -1872,8 +1874,8 @@ Status RuntimePredicateWrapper::get_push_exprs(
                                        max_literal));
         max_pred->add_child(probe_ctx->root());
         max_pred->add_child(max_literal);
-        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(max_pred_node,
-                                                                             max_pred, null_aware));
+        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(
+                max_pred_node, max_pred, get_comparison_ignore_thredhold(), null_aware));
 
         vectorized::VExprContextSPtr new_probe_ctx;
         RETURN_IF_ERROR(vectorized::VExpr::create_expr_tree(probe_expr, new_probe_ctx));
@@ -1889,8 +1891,8 @@ Status RuntimePredicateWrapper::get_push_exprs(
                                        _context->minmax_func->get_min(), min_literal));
         min_pred->add_child(new_probe_ctx->root());
         min_pred->add_child(min_literal);
-        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(min_pred_node,
-                                                                             min_pred, null_aware));
+        container.push_back(vectorized::VRuntimeFilterWrapper::create_shared(
+                min_pred_node, min_pred, get_comparison_ignore_thredhold(), null_aware));
         break;
     }
     case RuntimeFilterType::BLOOM_FILTER: {
@@ -1905,7 +1907,8 @@ Status RuntimePredicateWrapper::get_push_exprs(
         auto bloom_pred = vectorized::VBloomPredicate::create_shared(node);
         bloom_pred->set_filter(_context->bloom_filter_func);
         bloom_pred->add_child(probe_ctx->root());
-        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(node, bloom_pred);
+        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(
+                node, bloom_pred, get_bloom_filter_ignore_thredhold());
         container.push_back(wrapper);
         break;
     }
@@ -1921,7 +1924,7 @@ Status RuntimePredicateWrapper::get_push_exprs(
         auto bitmap_pred = vectorized::VBitmapPredicate::create_shared(node);
         bitmap_pred->set_filter(_context->bitmap_filter_func);
         bitmap_pred->add_child(probe_ctx->root());
-        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(node, bitmap_pred);
+        auto wrapper = vectorized::VRuntimeFilterWrapper::create_shared(node, bitmap_pred, 0);
         container.push_back(wrapper);
         break;
     }
