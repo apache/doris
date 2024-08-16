4380f3cb51c37b6785424f7e118be593bbc30349
morrySnow <101034200+morrySnow@users.noreply.github.com>
2024-08-16 09:51:02 +0800

    [fix](variable) support all type functions (#39144) (#39438)
    pick from master #39144

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
@@ -555,7 +555,7 @@ public class ConnectContext {
             } else if (literalExpr instanceof NullLiteral) {
                 return Literal.of(null);
             } else {
-                return Literal.of("");
+                return Literal.of(literalExpr.getStringValue());
             }
         } else {
             // If there are no such user defined var, just return the NULL value.
@@ -585,7 +585,7 @@ public class ConnectContext {
                 desc.setIsNull();
             } else {
                 desc.setType(Type.VARCHAR);
-                desc.setStringValue("");
+                desc.setStringValue(literalExpr.getStringValue());
             }
         } else {
             // If there are no such user defined var, just fill the NULL value.
