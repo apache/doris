7fd2f962b0816597de7adec2e8f94c3e49ee1a36
morrySnow <101034200+morrySnow@users.noreply.github.com>
2024-08-16 18:07:11 +0800

    [opt](Nereids) polish aggregate function signature matching (#39352) (#39466)
    pick from master #39352
    
    use double to match string
    - stddev
    - stddev_samp
    
    use largeint to match string
    - group_bit_and
    - group_bit_or
    - group_git_xor
    
    optimize error message
    - multi_distinct_sum

--- regression-test/suites/nereids_function_p0/type_coercion.groovy
+++ regression-test/suites/nereids_function_p0/type_coercion.groovy
@@ -16,9 +16,33 @@
 // under the License.
 suite("function_type_coercion") {
     sql """set enable_fold_constant_by_be=false""" // remove this if array<double> BE return result be fixed.
+
+    // scalar function
     qt_greatest """select greatest(1, 2222, '333')"""
     qt_least """select least(5,2000000,'3.0023')"""
     qt_if """select if (1, 2222, 33)"""
     qt_pmod """select pmod(2, '1.0')"""
     qt_nullif """SELECT nullif(13, -4851)"""
+
+    // agg function
+    sql """drop table if exists test_agg_signature"""
+
+    sql """
+        create table test_agg_signature (
+            id int,
+            c1 text,
+            c2 text
+        ) distributed by hash(id)
+        properties (
+            "replication_num" = "1"
+        )
+    """
+
+    sql """insert into test_agg_signature values (1, "10", "65537"), (2, "129", "134"), (3, "65548", "3")"""
+
+    qt_group_bit_and """select group_bit_and(c1) from test_agg_signature"""
+    qt_group_bit_or """select group_bit_or(c1) from test_agg_signature"""
+    qt_group_bit_xor """select group_bit_xor(c1) from test_agg_signature"""
+    qt_stddev """select stddev(c1) from test_agg_signature"""
+    qt_stddev_samp """select stddev_samp(c1) from test_agg_signature"""
 }
