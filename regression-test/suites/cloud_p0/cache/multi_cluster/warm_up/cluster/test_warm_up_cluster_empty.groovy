ab1e8489febec326421c3a4b59236b7e0a255d6f
Socrates <suxiaogang223@icloud.com>
2024-08-20 00:24:26 +0800

    add ut and regress test

---
+++ regression-test/suites/cloud_p0/cache/multi_cluster/warm_up/cluster/test_warm_up_cluster_empty.groovy
@@ -0,0 +1,166 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import org.codehaus.groovy.runtime.IOGroovyMethods
+
+suite("test_warm_up_cluster_empty") {
+    def ttlProperties = """ PROPERTIES("file_cache_ttl_seconds"="12000") """
+    def getJobState = { jobId ->
+         def jobStateResult = sql """  SHOW WARM UP JOB WHERE ID = ${jobId} """
+         return jobStateResult[0][2]
+    }
+    def table = "customer"
+
+    def token = context.config.metaServiceToken;
+    def instance_id = context.config.multiClusterInstance
+
+    List<String> ipList = new ArrayList<>();
+    List<String> hbPortList = new ArrayList<>()
+    List<String> httpPortList = new ArrayList<>()
+    List<String> brpcPortList = new ArrayList<>()
+    List<String> beUniqueIdList = new ArrayList<>()
+
+    String[] bes = context.config.multiClusterBes.split(',');
+    println("the value is " + context.config.multiClusterBes);
+    int num = 0
+    for(String values : bes) {
+        if (num++ == 2) break;
+        println("the value is " + values);
+        String[] beInfo = values.split(':');
+        ipList.add(beInfo[0]);
+        hbPortList.add(beInfo[1]);
+        httpPortList.add(beInfo[2]);
+        beUniqueIdList.add(beInfo[3]);
+        brpcPortList.add(beInfo[4]);
+    }
+
+    println("the ip is " + ipList);
+    println("the heartbeat port is " + hbPortList);
+    println("the http port is " + httpPortList);
+    println("the be unique id is " + beUniqueIdList);
+    println("the brpc port is " + brpcPortList);
+
+    for (unique_id : beUniqueIdList) {
+        resp = get_cluster.call(unique_id);
+        for (cluster : resp) {
+            if (cluster.type == "COMPUTE") {
+                drop_cluster.call(cluster.cluster_name, cluster.cluster_id);
+            }
+        }
+    }
+    sleep(20000)
+
+    List<List<Object>> result  = sql "show clusters"
+    assertEquals(result.size(), 0);
+
+    add_cluster.call(beUniqueIdList[0], ipList[0], hbPortList[0],
+                     "regression_cluster_name0", "regression_cluster_id0");
+    add_cluster.call(beUniqueIdList[1], ipList[1], hbPortList[1],
+                     "regression_cluster_name1", "regression_cluster_id1");
+    sleep(20000)
+
+    result  = sql "show clusters"
+    sql """ SET PROPERTY 'default_cloud_cluster' = "regression_cluster_name0"; """
+    assertEquals(result.size(), 2);
+
+    sql new File("""${context.file.parent}/../ddl/${table}_delete.sql""").text
+    sql new File("""${context.file.parent}/../ddl/supplier_delete.sql""").text
+    // create table if not exists
+    sql (new File("""${context.file.parent}/../ddl/${table}.sql""").text + ttlProperties)
+    sql (new File("""${context.file.parent}/../ddl/supplier.sql""").text + ttlProperties)
+
+    sql """ TRUNCATE TABLE __internal_schema.cloud_cache_hotspot; """
+    sleep(30000)
+
+    def s3BucketName = getS3BucketName()
+    def s3WithProperties = """WITH S3 (
+        |"AWS_ACCESS_KEY" = "${getS3AK()}",
+        |"AWS_SECRET_KEY" = "${getS3SK()}",
+        |"AWS_ENDPOINT" = "${getS3Endpoint()}",
+        |"AWS_REGION" = "${getS3Region()}",
+        |"provider" = "${getS3Provider()}")
+        |PROPERTIES(
+        |"exec_mem_limit" = "8589934592",
+        |"load_parallelism" = "3")""".stripMargin()
+    
+    
+
+    def clearFileCache = { ip, port ->
+        httpTest {
+            endpoint ""
+            uri ip + ":" + port + """/api/file_cache?op=clear&sync=true"""
+            op "get"
+            body ""
+        }
+    }
+
+    def getMetricsMethod = { ip, port, check_func ->
+        httpTest {
+            endpoint ip + ":" + port
+            uri "/brpc_metrics"
+            op "get"
+            check check_func
+        }
+    }
+
+    clearFileCache.call(ipList[1], httpPortList[1]);
+
+    sleep(40000)
+    def jobId_ = sql "WARM UP CLUSTER regression_cluster_name1 WITH CLUSTER regression_cluster_name0"
+    def waitJobDone = { jobId ->
+        int retryTime = 120
+        int i = 0
+        for (; i < retryTime; i++) {
+            sleep(1000)
+            def status = getJobState(jobId[0][0])
+            logger.info(status)
+            if (status.equals("CANCELLED")) {
+                assertTrue(false);
+            }
+            if (status.equals("FINISHED")) {
+                break;
+            }
+        }
+        if (i == retryTime) {
+            sql "cancel warm up job where id = ${jobId[0][0]}"
+            assertTrue(false);
+        }
+    }
+    waitJobDone(jobId_)
+    sleep(40000)
+    getMetricsMethod.call(ipList[1], brpcPortList[1]) {
+        respCode, body ->
+            assertEquals("${respCode}".toString(), "200")
+            String out = "${body}".toString()
+            def strs = out.split('\n')
+            Boolean flag = false;
+            for (String line in strs) {
+                if (line.contains("ttl_cache_size")) {
+                    if (line.startsWith("#")) {
+                        continue
+                    }
+                    def j = line.indexOf(' ')
+                    assertEquals(0, line.substring(j).toLong())
+                    flag = true
+                    break
+                }
+            }
+            assertTrue(flag)
+    }
+    sql new File("""${context.file.parent}/../ddl/${table}_delete.sql""").text
+    sql new File("""${context.file.parent}/../ddl/supplier_delete.sql""").text
+}
