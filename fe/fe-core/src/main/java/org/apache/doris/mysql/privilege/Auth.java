5ea4d0d4fc7338aad136adcb8437cb4692d55f22
zhangdong <493738387@qq.com>
2024-08-20 11:39:45 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Auth.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Auth.java
@@ -90,7 +90,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
