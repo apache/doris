dd7e662e204882024d5e40cbbd179a6fcca4de16
Toms1999 <mj_tangsi@163.com>
2024-08-21 21:31:28 +0800

    [chore](nereids) Added compatibility with mysql alias filter

--- fe/fe-core/src/main/java/org/apache/doris/common/AnalysisException.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/AnalysisException.java
@@ -24,6 +24,7 @@ package org.apache.doris.common;
  * Thrown for errors encountered during analysis of a SQL statement.
  */
 public class AnalysisException extends UserException {
+
     public AnalysisException(String msg, Throwable cause) {
         super(msg, cause);
     }
