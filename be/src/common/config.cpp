1c902bf3b38aff9c89f2332d99902b25830b489b
zhiqiang-hhhh <seuhezhiqiang@163.com>
2024-08-21 14:14:41 +0800

    FIX

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1270,7 +1270,8 @@ DEFINE_mBool(enable_parquet_page_index, "false");
 
 DEFINE_mBool(ignore_not_found_file_in_external_table, "true");
 
-DEFINE_mInt64(pipeline_task_leakage_detect_period_secs, "60");
+// TODO: Revert to 60s after bug fixed.
+DEFINE_mInt64(pipeline_task_leakage_detect_period_secs, "1");
 
 // clang-format off
 #ifdef BE_TEST
