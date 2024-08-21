3d210d0d85ae6509d726b678c4fb28dcbe636a2c
shee <13843187+qzsee@users.noreply.github.com>
2024-08-21 09:30:18 +0800

    [BUG] fix partition storage policy info lost (#38700)
    ## Proposed changes
    1. fix partition storage policy info lost
    When adding a storage policy to a table through an alter statement, the
    partition policy is lost when the FE is restarted because the storage
    policy is not set for the partition synchronously.
    
    2. when setting policies, check the uniq table in advance to prevent
    metadata inconsistencies
        
    
    3. show storage policy using for stmt support any string policy name
         
    If the policy name begins with a number, the statement cannot be parsed.
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->
    
    ---------
    
    Co-authored-by: garenshi <garenshi@tencent.com>

--- fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
@@ -80,6 +80,7 @@ import org.apache.doris.thrift.TSortType;
 import org.apache.doris.thrift.TTabletType;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -191,7 +192,19 @@ public class Alter {
             }
             // check currentStoragePolicy resource exist.
             Env.getCurrentEnv().getPolicyMgr().checkStoragePolicyExist(currentStoragePolicy);
-
+            boolean enableUniqueKeyMergeOnWrite;
+            olapTable.readLock();
+            try {
+                enableUniqueKeyMergeOnWrite = olapTable.getEnableUniqueKeyMergeOnWrite();
+            } finally {
+                olapTable.readUnlock();
+            }
+            // must check here whether you can set the policy, otherwise there will be inconsistent metadata
+            if (enableUniqueKeyMergeOnWrite && !Strings.isNullOrEmpty(currentStoragePolicy)) {
+                throw new UserException(
+                    "Can not set UNIQUE KEY table that enables Merge-On-write"
+                        + " with storage policy(" + currentStoragePolicy + ")");
+            }
             olapTable.setStoragePolicy(currentStoragePolicy);
             needProcessOutsideTableLock = true;
         } else if (currentAlterOps.checkIsBeingSynced(alterClauses)) {
