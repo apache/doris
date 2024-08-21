bab53f1fecaec7f1ca43213ac8799d4ab6893ba6
Toms1999 <mj_tangsi@163.com>
2024-08-21 23:01:12 +0800

    [chore](nereids) Added compatibility with mysql alias filter

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalSubQueryAlias.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/logical/LogicalSubQueryAlias.java
@@ -35,7 +35,6 @@ import org.apache.commons.lang3.StringUtils;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
