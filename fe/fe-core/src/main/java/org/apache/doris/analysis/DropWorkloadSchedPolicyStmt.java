f3f17109b073e8e419ebeebea4e5f557b5393c24
morrySnow <101034200+morrySnow@users.noreply.github.com>
2024-08-21 10:39:46 +0800

    [opt](Nereids) support drop statement syntax in Nereids (#39506)

--- fe/fe-core/src/main/java/org/apache/doris/analysis/DropWorkloadSchedPolicyStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/DropWorkloadSchedPolicyStmt.java
@@ -25,7 +25,7 @@ import org.apache.doris.common.UserException;
 import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 
-public class DropWorkloadSchedPolicyStmt extends DdlStmt {
+public class DropWorkloadSchedPolicyStmt extends DdlStmt implements NotFallbackInParser {
 
     private boolean ifExists;
     private String policyName;
