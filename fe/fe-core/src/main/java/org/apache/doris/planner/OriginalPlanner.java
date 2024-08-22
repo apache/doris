a13eb6dced7e1a0cd74cb172f7e5bd77151beea5
Pxl <pxl290@qq.com>
2024-08-22 14:49:13 +0800

    [Chore](top-n) split topn_filter_limit_threshold out of topn_opt_limit_threshold (#39735)
    ## Proposed changes
    split topn_filter_limit_threshold out of topn_opt_limit_threshold

--- fe/fe-core/src/main/java/org/apache/doris/planner/OriginalPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/OriginalPlanner.java
@@ -525,7 +525,7 @@ public class OriginalPlanner extends Planner {
             PlanNode child = sortNode.getChild(0);
             if (child instanceof OlapScanNode && sortNode.getLimit() > 0
                     && ConnectContext.get() != null && ConnectContext.get().getSessionVariable() != null
-                    && sortNode.getLimit() <= ConnectContext.get().getSessionVariable().topnOptLimitThreshold
+                    && sortNode.getLimit() <= ConnectContext.get().getSessionVariable().topnFilterLimitThreshold
                     && sortNode.getSortInfo().getOrigOrderingExprs().size() > 0) {
                 Expr firstSortExpr = sortNode.getSortInfo().getOrigOrderingExprs().get(0);
                 if (firstSortExpr instanceof SlotRef && !firstSortExpr.getType().isFloatingPointType()) {
--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -399,6 +399,7 @@ public class SessionVariable implements Serializable, Writable {
 
     public static final String ENABLE_TWO_PHASE_READ_OPT = "enable_two_phase_read_opt";
     public static final String TOPN_OPT_LIMIT_THRESHOLD = "topn_opt_limit_threshold";
+    public static final String TOPN_FILTER_LIMIT_THRESHOLD = "topn_filter_limit_threshold";
     public static final String ENABLE_SNAPSHOT_POINT_QUERY = "enable_snapshot_point_query";
 
     public static final String ENABLE_FILE_CACHE = "enable_file_cache";
@@ -1465,7 +1466,9 @@ public class SessionVariable implements Serializable, Writable {
     @VariableMgr.VarAttr(name = ENABLE_TWO_PHASE_READ_OPT, fuzzy = true)
     public boolean enableTwoPhaseReadOpt = true;
     @VariableMgr.VarAttr(name = TOPN_OPT_LIMIT_THRESHOLD)
-    public long topnOptLimitThreshold = 10240000;
+    public long topnOptLimitThreshold = 1024;
+    @VariableMgr.VarAttr(name = TOPN_FILTER_LIMIT_THRESHOLD)
+    public long topnFilterLimitThreshold = 10240000;
     @VariableMgr.VarAttr(name = ENABLE_SNAPSHOT_POINT_QUERY)
     public boolean enableSnapshotPointQuery = true;
 
