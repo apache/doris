d89c59a07ce49367ba41dded0200412e8f630660
zclllhhjj <zhaochangle@selectdb.com>
2024-08-23 11:38:27 +0800

    [chore](errmsg) Add some partition expr check on creating table (#39754)
    Add some partition expr check on creating table

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -2182,7 +2182,11 @@ public class InternalCatalog implements CatalogIf<Database> {
     private void checkLegalityofPartitionExprs(CreateTableStmt stmt, PartitionDesc partitionDesc)
             throws AnalysisException {
         for (Expr expr : partitionDesc.getPartitionExprs()) {
-            if (expr != null && expr instanceof FunctionCallExpr) { // test them
+            if (expr instanceof FunctionCallExpr) { // test them
+                if (!partitionDesc.isAutoCreatePartitions() || partitionDesc.getType() != PartitionType.RANGE) {
+                    throw new AnalysisException("only Auto Range Partition support FunctionCallExpr");
+                }
+
                 FunctionCallExpr func = (FunctionCallExpr) expr;
                 ArrayList<Expr> children = func.getChildren();
                 Type[] childTypes = new Type[children.size()];
@@ -2206,6 +2210,12 @@ public class InternalCatalog implements CatalogIf<Database> {
                 if (fn == null) {
                     throw new AnalysisException("partition expr " + func.getExprName() + " is illegal!");
                 }
+            } else if (expr instanceof SlotRef) {
+                if (partitionDesc.isAutoCreatePartitions() && partitionDesc.getType() == PartitionType.RANGE) {
+                    throw new AnalysisException("Auto Range Partition need FunctionCallExpr");
+                }
+            } else {
+                throw new AnalysisException("partition expr " + expr.getExprName() + " is illegal!");
             }
         }
     }
