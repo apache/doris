ab7254566eb4f499bb6bc02c043ed6c5baebbd87
Mingyu Chen <morningman@163.com>
2024-08-23 11:35:47 +0800

    [fix](ctas) fix NPE when ctas with old planner and varchar issue (#39744)
    Fix 2 bugs:
    1. 
    introduced from #35489
    When the target table is unpartitioned with text type column from
    external table,
    NPE will be thrown.
    Only happen when using old planner.
    The new planner works well.
    
    2.
    If first column is varchar type, the new planner will change it to
    string type
    and then failed because first column can not be string type.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/CreateTableCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/CreateTableCommand.java
@@ -121,6 +121,8 @@ public class CreateTableCommand extends Command implements ForwardWithSync {
             Slot s = slots.get(i);
             DataType dataType = s.getDataType().conversion();
             if (i == 0 && dataType.isStringType()) {
+                // first column of olap table can not be string type.
+                // So change it to varchar type.
                 dataType = VarcharType.createVarcharType(ScalarType.MAX_VARCHAR_LENGTH);
             } else {
                 dataType = TypeCoercionUtils.replaceSpecifiedType(dataType,
@@ -133,13 +135,21 @@ public class CreateTableCommand extends Command implements ForwardWithSync {
                         if (createTableInfo.getPartitionTableInfo().inIdentifierPartitions(s.getName())
                                 || (createTableInfo.getDistribution() != null
                                 && createTableInfo.getDistribution().inDistributionColumns(s.getName()))) {
-                            // String type can not be used in partition/distributed column
+                            // String type can not be used in partition/distributed column,
                             // so we replace it to varchar
                             dataType = TypeCoercionUtils.replaceSpecifiedType(dataType,
                                     CharacterType.class, VarcharType.MAX_VARCHAR_TYPE);
                         } else {
-                            dataType = TypeCoercionUtils.replaceSpecifiedType(dataType,
-                                    CharacterType.class, StringType.INSTANCE);
+                            if (i == 0) {
+                                // first column of olap table can not be string type.
+                                // So change it to varchar type.
+                                dataType = TypeCoercionUtils.replaceSpecifiedType(dataType,
+                                        CharacterType.class, VarcharType.MAX_VARCHAR_TYPE);
+                            } else {
+                                // change varchar/char column from external table to string type
+                                dataType = TypeCoercionUtils.replaceSpecifiedType(dataType,
+                                        CharacterType.class, StringType.INSTANCE);
+                            }
                         }
                     }
                 } else {
@@ -215,3 +225,4 @@ public class CreateTableCommand extends Command implements ForwardWithSync {
         return StmtType.CREATE;
     }
 }
+
