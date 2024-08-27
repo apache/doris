ed7b3141753cac624272ff8091a701c8d92de2ad
feiniaofeiafei <moailing@selectdb.com>
2024-08-27 17:08:12 +0800

    fix regresssion

--- regression-test/suites/nereids_rules_p0/infer_predicate/infer_intersect_except.groovy
+++ regression-test/suites/nereids_rules_p0/infer_predicate/infer_intersect_except.groovy
@@ -137,51 +137,47 @@ suite("infer_intersect_except") {
     select abs(a) from infer_intersect_except1 t1 where abs(a)<3 except select abs(a) from infer_intersect_except2 t2    """
 
     qt_except_res """
-    select a,b from infer_intersect_except1 where a>0 except select a,b from infer_intersect_except2 where b>'ab' except select a,b from infer_intersect_except2 where a<10 order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) except (select a,b from infer_intersect_except2 where b>'ab') except (select a,b from infer_intersect_except2 where a<10) order by 1,2;
     """
 
     qt_except_to_empty_res """
-    select a,b from infer_intersect_except1 where a>0 except select a,b from infer_intersect_except2 where b>'ab' except select a,b from infer_intersect_except3 where a<0  order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) except (select a,b from infer_intersect_except2 where b>'ab') except (select a,b from infer_intersect_except3 where a<0)  order by 1,2;
     """
 
     qt_except_not_infer_1_greater_than_0_res """
-    select a,b from infer_intersect_except1 where a>0 except select 1,'abc' from infer_intersect_except2 where b>'ab' except select a,b from infer_intersect_except2 where a<0 order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) except (select 1,'abc' from infer_intersect_except2 where b>'ab') except (select a,b from infer_intersect_except2 where a<0) order by 1,2;
     """
 
     qt_except_number_and_string_res """
-    select a,2 from infer_intersect_except1 where a>0 except select 1,'abc' from infer_intersect_except2 where b>'ab' except select a,b from infer_intersect_except3 where a<0 order by 1,2;
+    (select a,2 from infer_intersect_except1 where a>0) except (select 1,'abc' from infer_intersect_except2 where b>'ab') except (select a,b from infer_intersect_except3 where a<0) order by 1,2;
     """
 
     qt_intersect_res """
-    select a,b from infer_intersect_except1 where a>0 intersect select a,b from infer_intersect_except2 where b>'ab' order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) intersect (select a,b from infer_intersect_except2 where b>'ab') order by 1,2;
     """
     qt_intersect_empty_res """
-    select a,b from infer_intersect_except1 where a>0 intersect select a,b from infer_intersect_except2 where a<0 order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) intersect (select a,b from infer_intersect_except2 where a<0) order by 1,2;
     """
 
     qt_intersect_expr_res """
-    select a+1,b from infer_intersect_except1 where a>0 intersect select a+1,b from infer_intersect_except2 where a+1<0 order by 1,2;
+    (select a+1,b from infer_intersect_except1 where a>0) intersect (select a+1,b from infer_intersect_except2 where a+1<0 ) order by 1,2;
     """
 
     qt_except_and_intersect_res """
-    select a,b from infer_intersect_except1 where a>0 except select 1,'abc' from infer_intersect_except2 where b>'ab' intersect select a,b from infer_intersect_except3 where a<10 order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) except (select 1,'abc' from infer_intersect_except2 where b>'ab') intersect (select a,b from infer_intersect_except3 where a<10) order by 1,2;
     """
 
     qt_except_and_intersect_except_predicate_to_right_res """
-    select a,b from infer_intersect_except1 where a>0 except select a,'abc' from infer_intersect_except2 where b>'ab' intersect select a,b from infer_intersect_except3 where a<10  order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) except (select a,'abc' from infer_intersect_except2 where b>'ab') intersect (select a,b from infer_intersect_except3 where a<10)  order by 1,2;
     """
     qt_intersect_and_except_res """
-    select a,b from infer_intersect_except1 where a>0 intersect select 1,'abc' from infer_intersect_except2 where b>'ab' except select a,b from infer_intersect_except3 where a<10  order by 1,2;
+    (select a,b from infer_intersect_except1 where a>0) intersect (select 1,'abc' from infer_intersect_except2 where b>'ab') except (select a,b from infer_intersect_except3 where a<10)  order by 1,2;
     """
-    def res_test = sql """select abs(a) from infer_intersect_except1 t1 where abs(a)<3 intersect select abs(a) from infer_intersect_except2 t2  order by 1"""
-    logger.info("function_intersect_res:" + res_test)
-    def var_result = sql "show variables"
-    logger.info("show variales result: " + var_result )
 
     qt_function_intersect_res """
-    select abs(a) from infer_intersect_except1 t1 where abs(a)<3 intersect select abs(a) from infer_intersect_except2 t2  order by 1  """
+    (select abs(a) from infer_intersect_except1 t1 where abs(a)<3) intersect (select abs(a) from infer_intersect_except2 t2)  order by 1  """
     qt_function_except_res """
-    select abs(a) from infer_intersect_except1 t1 where abs(a)<3 except select abs(a) from infer_intersect_except2 t2  order by 1  """
+    (select abs(a) from infer_intersect_except1 t1 where abs(a)<3) except (select abs(a) from infer_intersect_except2 t2)  order by 1  """
 
     sql "drop table if exists infer_intersect_except4"
     sql "create table infer_intersect_except4(d_int int, d_char100 char(100), d_smallint smallint, d_tinyint tinyint, d_char10 char(10),d_datetimev2 datetimev2, d_datev2 datev2) properties('replication_num'='1');"
@@ -204,9 +200,9 @@ suite("infer_intersect_except") {
     select d_int from infer_intersect_except4 where d_int>2  intersect select d_char100 from infer_intersect_except4 where d_char100<'abc';
     """
     qt_different_type_date_string_res """
-    select d_datetimev2 from infer_intersect_except4 where d_datetimev2>'2020-01-01'  intersect select d_char100 from infer_intersect_except4 where d_char100<'abc' order by 1;
+    (select d_datetimev2 from infer_intersect_except4 where d_datetimev2>'2020-01-01')  intersect (select d_char100 from infer_intersect_except4 where d_char100<'abc') order by 1;
     """
     qt_different_type_int_string_res """
-    select d_int from infer_intersect_except4 where d_int>2  intersect select d_char100 from infer_intersect_except4 where d_char100<'abc' order by 1;
+    (select d_int from infer_intersect_except4 where d_int>2 ) intersect (select d_char100 from infer_intersect_except4 where d_char100<'abc') order by 1;
     """
 }
