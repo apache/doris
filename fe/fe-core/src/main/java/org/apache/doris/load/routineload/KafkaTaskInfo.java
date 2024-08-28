37a109f72322704fbf36db3e7041b4e0487193e9
hui lai <1353307710@qq.com>
2024-08-28 17:05:16 +0800

    [improve](routine load) delay schedule EOF tasks to avoid too many small transactions (#39975)
    We encountered a scenario where a large number of small transactions
    were generated, resulting in an impact on query performance:
    Kafka's data comes in batches of very small data every very short time,
    which leads to tasks being frequently scheduled and ending very quickly,
    resulting in a large number of small transactions.
    
    To solve this problem, we delay the scheduling of tasks that perceive
    EOF, which would not delay data consumption, for perceiving EOF
    indicates that the consumption speed is greater than the production
    speed.

--- fe/fe-core/src/main/java/org/apache/doris/load/routineload/KafkaTaskInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/routineload/KafkaTaskInfo.java
@@ -60,6 +60,7 @@ public class KafkaTaskInfo extends RoutineLoadTaskInfo {
                 kafkaTaskInfo.getTimeoutMs(), kafkaTaskInfo.getTimeoutBackOffCount(),
                 kafkaTaskInfo.getBeId(), isMultiTable);
         this.partitionIdToOffset = partitionIdToOffset;
+        this.isEof = kafkaTaskInfo.getIsEof();
     }
 
     public List<Integer> getPartitions() {
