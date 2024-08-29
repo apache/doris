aeb7a2ab192fc019b436dc2286881c1852e2a0c1
zhangdong <493738387@qq.com>
2024-08-29 15:49:50 +0800

    [fix](auth) ordinary users can see the processes of other users (#39747)
    Fix when set show_all_fe_connection="true";
    At this time, ordinary users can see the processes of other users

--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -1465,6 +1465,7 @@ struct TGetColumnInfoResult {
 
 struct TShowProcessListRequest {
     1: optional bool show_full_sql
+    2: optional Types.TUserIdentity current_user_ident
 }
 
 struct TShowProcessListResult {
