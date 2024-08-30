325cde83ca39a2916a54bc969ff18ea2b546ee03
morningman <morningman@163.com>
2024-08-30 15:24:11 +0800

    2

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -567,6 +567,7 @@ struct TMetaScanRange {
   7: optional TJobsMetadataParams jobs_params
   8: optional TTasksMetadataParams tasks_params
   9: optional TPartitionsMetadataParams partitions_params
+  10: optional TMetaCacheStatsParams meta_cache_stats_params
 }
 
 // Specification of an individual data range which is held in its entirety
