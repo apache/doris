4dceb000240e94c8d37cf1c478512be4f8ad54b2
zhiqiang <seuhezhiqiang@163.com>
2024-08-30 11:45:14 +0800

    [chore](cancel) Make enable_pipeline_task_leakage_detect false by default (#40145)
    add be.conf enable_pipeline_task_leakage_detect, default to false.

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1320,6 +1320,8 @@ DEFINE_mInt64(pipeline_task_leakage_detect_period_secs, "60");
 DEFINE_mInt32(snappy_compression_block_size, "262144");
 DEFINE_mInt32(lz4_compression_block_size, "262144");
 
+DEFINE_mBool(enable_pipeline_task_leakage_detect, "false");
+
 // clang-format off
 #ifdef BE_TEST
 // test s3
