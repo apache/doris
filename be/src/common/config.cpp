55696d30cb482e76fa994a5aae29e5d61b460ed8
lihangyu <15605149486@163.com>
2024-08-30 17:57:10 +0800

    [regression-test](cases) optimize some cases (#40174)
    1. modify some variant cases to concurrent and p1
    2. rowsstore cases to concurrent

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -988,7 +988,7 @@ DEFINE_Bool(enable_index_apply_preds_except_leafnode_of_andnode, "true");
 
 DEFINE_mBool(variant_enable_flatten_nested, "false");
 DEFINE_mDouble(variant_ratio_of_defaults_as_sparse_column, "1");
-DEFINE_mInt64(variant_threshold_rows_to_estimate_sparse_column, "1000");
+DEFINE_mInt64(variant_threshold_rows_to_estimate_sparse_column, "2048");
 DEFINE_mBool(variant_throw_exeception_on_invalid_json, "false");
 
 // block file cache
