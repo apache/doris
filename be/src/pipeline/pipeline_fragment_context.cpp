9e6a71e519f9f101dba64909968ffabf3ee01060
yiguolei <yiguolei@gmail.com>
2024-08-31 23:18:12 +0800

    f

--- be/src/pipeline/pipeline_fragment_context.cpp
+++ be/src/pipeline/pipeline_fragment_context.cpp
@@ -1184,6 +1184,12 @@ Status PipelineFragmentContext::_create_operator(ObjectPool* pool, const TPlanNo
     }
     case TPlanNodeType::GROUP_COMMIT_SCAN_NODE: {
         op.reset(new GroupCommitOperatorX(pool, tnode, next_operator_id(), descs, _num_instances));
+#ifndef NDEBUG
+        DCHECK(_query_ctx != nullptr);
+        _query_ctx->query_mem_tracker->is_group_commit_load = true;
+        LOG(INFO) << "is group commit load sink stage "
+                  << _query_ctx->query_mem_tracker->label(); // DEBUG
+#endif
         RETURN_IF_ERROR(cur_pipe->add_operator(op));
         if (request.__isset.parallel_instances) {
             cur_pipe->set_num_tasks(request.parallel_instances);
