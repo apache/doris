9561e150a5d936d3de2e1a483dbabad5937b161b
morningman <morningman@163.com>
2024-08-31 10:06:45 +0800

    fix test

--- regression-test/suites/external_table_p0/hive/test_external_catalog_hive.groovy
+++ regression-test/suites/external_table_p0/hive/test_external_catalog_hive.groovy
@@ -180,7 +180,7 @@ suite("test_external_catalog_hive", "p0,external,hive,external_docker,external_d
         }
 
         // test catalog_meta_cache_statistics
-        sql """select * from information_schema.catalog_meta_cache_statistics;"""
-        sql """select * from information_schema.catalog_meta_cache_statistics where catalog_name="${catalog_name}";"""
+        sql """select * from internal.information_schema.catalog_meta_cache_statistics;"""
+        sql """select * from ${catalog_name}.information_schema.catalog_meta_cache_statistics where catalog_name="${catalog_name}";"""
     }
 }
