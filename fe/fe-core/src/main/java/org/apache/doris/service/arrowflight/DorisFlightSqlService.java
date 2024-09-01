apache/doris

commit 5e8477e7ba0a62854113b771501b4eab7f2004f4

[fix](arrow-flight-sql) Fix Arrow Flight bind wrong Host in Fqdn #34850

fe/fe-core/src/main/java/org/apache/doris/service/arrowflight/DorisFlightSqlService.java
@@ -18,7 +18,6 @@
 package org.apache.doris.service.arrowflight;
 
 import org.apache.doris.common.Config;
-import org.apache.doris.service.FrontendOptions;
 import org.apache.doris.service.arrowflight.auth2.FlightBearerTokenAuthenticator;
 import org.apache.doris.service.arrowflight.sessions.FlightSessionsManager;
 import org.apache.doris.service.arrowflight.sessions.FlightSessionsWithTokenManager;
@@ -46,7 +45,7 @@ public class DorisFlightSqlService {
 
     public DorisFlightSqlService(int port) {
         BufferAllocator allocator = new RootAllocator();
-        Location location = Location.forGrpcInsecure(FrontendOptions.getLocalHostAddress(), port);
+        Location location = Location.forGrpcInsecure("0.0.0.0", port);
         // arrow_flight_token_cache_size less than qe_max_connection to avoid `Reach limit of connections`.
         // arrow flight sql is a stateless protocol, connection is usually not actively disconnected.
         // bearer token is evict from the cache will unregister ConnectContext.

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/BatchInsertIntoTableCommand.java
@@ -41,6 +41,7 @@ import org.apache.doris.nereids.trees.plans.physical.PhysicalOneRowRelation;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalUnion;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
 import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.qe.ConnectContext.ConnectType;
 import org.apache.doris.qe.StmtExecutor;
 
 import com.google.common.base.Preconditions;
@@ -105,7 +106,7 @@ public class BatchInsertIntoTableCommand extends Command implements ForwardWithS
             NereidsPlanner planner = new NereidsPlanner(ctx.getStatementContext());
             planner.plan(logicalPlanAdapter, ctx.getSessionVariable().toThrift());
             executor.checkBlockRules();
-            if (ctx.getMysqlChannel() != null) {
+            if (ctx.getConnectType() == ConnectType.MYSQL && ctx.getMysqlChannel() != null) {
                 ctx.getMysqlChannel().reset();
             }
 

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertIntoTableCommand.java
@@ -44,6 +44,7 @@ import org.apache.doris.nereids.trees.plans.physical.PhysicalSink;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
 import org.apache.doris.planner.DataSink;
 import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.qe.ConnectContext.ConnectType;
 import org.apache.doris.qe.StmtExecutor;
 
 import com.google.common.base.Preconditions;
@@ -146,7 +147,7 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
             planner.plan(logicalPlanAdapter, ctx.getSessionVariable().toThrift());
             executor.setPlanner(planner);
             executor.checkBlockRules();
-            if (ctx.getMysqlChannel() != null) {
+            if (ctx.getConnectType() == ConnectType.MYSQL && ctx.getMysqlChannel() != null) {
                 ctx.getMysqlChannel().reset();
             }
             Optional<PhysicalSink<?>> plan = (planner.getPhysicalPlan()

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertOverwriteTableCommand.java
@@ -45,6 +45,7 @@ import org.apache.doris.nereids.trees.plans.physical.PhysicalOlapTableSink;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalTableSink;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
 import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.qe.ConnectContext.ConnectType;
 import org.apache.doris.qe.QueryState.MysqlStateType;
 import org.apache.doris.qe.StmtExecutor;
 
@@ -116,7 +117,7 @@ public class InsertOverwriteTableCommand extends Command implements ForwardWithS
         NereidsPlanner planner = new NereidsPlanner(ctx.getStatementContext());
         planner.plan(logicalPlanAdapter, ctx.getSessionVariable().toThrift());
         executor.checkBlockRules();
-        if (ctx.getMysqlChannel() != null) {
+        if (ctx.getConnectType() == ConnectType.MYSQL && ctx.getMysqlChannel() != null) {
             ctx.getMysqlChannel().reset();
         }
 

commit 96d614fc2ed6cf8cda6bd6804951413366813085

[fix](arrow-flight-sql) Fix return result from FE Arrow Flight server error `0.0.0.0:xxx, connection refused` (#40002)
Fix: #36340
Introduced in: #34850

```
adbc_driver_manager.OperationalError: IO: [FlightSQL] connection error: desc = "transport: Error while dialing: dial tcp 0.0.0.0:8029: connect: connection refused" (Unavailable; DoGet: endpoint 0: [uri:"grpc+tcp://0.0.0.0:8029"])
```

When query results are returned from FE Arrow Flight server, and Doris
FE and ADBC ​​Client are not on the same machine, such as `show
tables;`, `show databases;`, the IP in FlightInfo returned by FE to the
client is wrong, and `0.0.0.0` is returned instead of the real IP of FE.
The client fails to pull the results from `0.0.0.0`.

No problem when query results are returned from BE Arrow Flight server,
such as the normal query statement `select * from xxx`.

fe/fe-core/src/main/java/org/apache/doris/service/arrowflight/DorisFlightSqlService.java
@@ -18,6 +18,7 @@
 package org.apache.doris.service.arrowflight;
 
 import org.apache.doris.common.Config;
+import org.apache.doris.service.FrontendOptions;
 import org.apache.doris.service.arrowflight.auth2.FlightBearerTokenAuthenticator;
 import org.apache.doris.service.arrowflight.sessions.FlightSessionsManager;
 import org.apache.doris.service.arrowflight.sessions.FlightSessionsWithTokenManager;
@@ -45,7 +46,6 @@ public class DorisFlightSqlService {
 
     public DorisFlightSqlService(int port) {
         BufferAllocator allocator = new RootAllocator();
-        Location location = Location.forGrpcInsecure("0.0.0.0", port);
         // arrow_flight_token_cache_size less than qe_max_connection to avoid `Reach limit of connections`.
         // arrow flight sql is a stateless protocol, connection is usually not actively disconnected.
         // bearer token is evict from the cache will unregister ConnectContext.
@@ -54,8 +54,9 @@ public class DorisFlightSqlService {
                 Config.arrow_flight_token_alive_time);
         this.flightSessionsManager = new FlightSessionsWithTokenManager(flightTokenManager);
 
-        DorisFlightSqlProducer producer = new DorisFlightSqlProducer(location, flightSessionsManager);
-        flightServer = FlightServer.builder(allocator, location, producer)
+        DorisFlightSqlProducer producer = new DorisFlightSqlProducer(
+                Location.forGrpcInsecure(FrontendOptions.getLocalHostAddress(), port), flightSessionsManager);
+        flightServer = FlightServer.builder(allocator, Location.forGrpcInsecure("0.0.0.0", port), producer)
                 .headerAuthenticator(new FlightBearerTokenAuthenticator(flightTokenManager)).build();
         LOG.info("Arrow Flight SQL service is created, port: {}, token_cache_size: {}"
                         + ", qe_max_connection: {}, token_alive_time: {}",