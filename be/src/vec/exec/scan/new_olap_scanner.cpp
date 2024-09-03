1cdea945198183f96c2ca04d3f9457f1924f9a11
airborne12 <airborne08@gmail.com>
2024-09-03 10:49:24 +0800

    [Chore](inverted index) remove useless code of compound filters for inverted index (#40258)
    Remove useless code from scan operator to scanner for inverted index
    compound filter

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -239,10 +239,6 @@ Status NewOlapScanner::open(RuntimeState* state) {
     return Status::OK();
 }
 
-void NewOlapScanner::set_compound_filters(const std::vector<TCondition>& compound_filters) {
-    _compound_filters = compound_filters;
-}
-
 // it will be called under tablet read lock because capture rs readers need
 Status NewOlapScanner::_init_tablet_reader_params(
         const std::vector<OlapScanRange*>& key_ranges, const std::vector<TCondition>& filters,
@@ -291,10 +287,6 @@ Status NewOlapScanner::_init_tablet_reader_params(
     for (auto& filter : filters) {
         _tablet_reader_params.conditions.push_back(filter);
     }
-
-    std::copy(_compound_filters.cbegin(), _compound_filters.cend(),
-              std::inserter(_tablet_reader_params.conditions_except_leafnode_of_andnode,
-                            _tablet_reader_params.conditions_except_leafnode_of_andnode.begin()));
 
     std::copy(filter_predicates.bloom_filters.cbegin(), filter_predicates.bloom_filters.cend(),
               std::inserter(_tablet_reader_params.bloom_filters,
