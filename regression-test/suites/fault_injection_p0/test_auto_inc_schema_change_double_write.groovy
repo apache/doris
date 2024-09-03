f7c7d572ba8b5027684c09af5454105afd6b23ef
bobhan1 <bh2444151092@outlook.com>
2024-09-03 10:59:04 +0800

    add cloud

--- regression-test/suites/fault_injection_p0/test_auto_inc_schema_change_double_write.groovy
+++ regression-test/suites/fault_injection_p0/test_auto_inc_schema_change_double_write.groovy
@@ -20,27 +20,22 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
 
 suite("test_auto_inc_schema_change_double_write", "nonConcurrent") {
+    def backends = sql_return_maparray('show backends')
+    def replicaNum = 0
+    for (def be : backends) {
+        def alive = be.Alive.toBoolean()
+        def decommissioned = be.SystemDecommissioned.toBoolean()
+        if (alive && !decommissioned) {
+            replicaNum++
+        }
+    }
+    assertTrue(replicaNum > 0)
+    if (isCloudMode()) {
+        replicaNum = 1
+    }
 
-    def table1 = "test_auto_inc_schema_change_double_write"
-    sql "DROP TABLE IF EXISTS ${table1} FORCE;"
-    sql """ CREATE TABLE IF NOT EXISTS ${table1} (
-            `k1` BIGINT NOT NULL AUTO_INCREMENT,
-            `c1` int,
-            `c2` int,
-            `c3` int,
-            `c4` int
-            )UNIQUE KEY(k1)
-        DISTRIBUTED BY HASH(k1) BUCKETS 1
-        PROPERTIES (
-            "disable_auto_compaction" = "true",
-            "enable_unique_key_merge_on_write" = "true",
-            "replication_num" = "1"); """
-
-    sql """insert into ${table1}(c1,c2,c3,c4) select number,number,number,number from numbers("number"="5000"); """
-    sql "sync;"
-    qt_sql "select count(*) from ${table1} group by k1 having count(*) > 1;"
-
-    def run_test = {thread_num, rows, iters -> 
+    def tableName = "test_auto_inc_schema_change_double_write"
+    def run_test = {table1, thread_num, rows, iters -> 
         def threads = []
         (1..thread_num).each { id1 -> 
             threads.add(Thread.start {
@@ -51,44 +46,85 @@ suite("test_auto_inc_schema_change_double_write", "nonConcurrent") {
         }
 
         threads.each { thread -> thread.join() }
+    }    
+
+    def block_convert_historical_rowsets = {
+        if (isCloudMode()) {
+            GetDebugPoint().enableDebugPointForAllBEs("CloudSchemaChangeJob::_convert_historical_rowsets.block")
+        } else {
+            GetDebugPoint().enableDebugPointForAllBEs("SchemaChangeJob::_convert_historical_rowsets.block")
+        }
     }
 
-    try {
-        GetDebugPoint().clearDebugPointsForAllFEs()
-        GetDebugPoint().clearDebugPointsForAllBEs()
-        GetDebugPoint().enableDebugPointForAllBEs("SchemaChangeJob::_convert_historical_rowsets.block")
-        
-        AtomicBoolean stopped = new AtomicBoolean(false)
-        def t1 = Thread.start {
-            while (!stopped.get()) {
-                run_test(2, 500, 3)
-                Thread.sleep(200)
+    def unblock = {
+        if (isCloudMode()) {
+            GetDebugPoint().disableDebugPointForAllBEs("CloudSchemaChangeJob::_convert_historical_rowsets.block")
+        } else {
+            GetDebugPoint().disableDebugPointForAllBEs("SchemaChangeJob::_convert_historical_rowsets.block")
+        }
+    }
+
+    for (def model : ["UNIQUE", "DUPLICATE"]) {
+        try {
+            GetDebugPoint().clearDebugPointsForAllFEs()
+            GetDebugPoint().clearDebugPointsForAllBEs()
+
+            def table1 = "${tableName}_${model}"
+            sql "DROP TABLE IF EXISTS ${table1} FORCE;"
+            sql """ CREATE TABLE IF NOT EXISTS ${table1} (
+                    `k1` BIGINT NOT NULL AUTO_INCREMENT,
+                    `c1` int,
+                    `c2` int,
+                    `c3` int,
+                    `c4` int
+                    )${model} KEY(k1)
+                DISTRIBUTED BY HASH(k1) BUCKETS 1
+                PROPERTIES (
+                    "disable_auto_compaction" = "true",
+                    "replication_num" = "${replicaNum}"); """
+
+            sql """insert into ${table1}(c1,c2,c3,c4) select number,number,number,number from numbers("number"="5000"); """
+            sql "sync;"
+            qt_sql "select count(*) from ${table1} group by k1 having count(*) > 1;"
+
+            block_convert_historical_rowsets()
+            
+            AtomicBoolean stopped = new AtomicBoolean(false)
+            def t1 = Thread.start {
+                while (!stopped.get()) {
+                    run_test(table1, 2, 500, 3)
+                    Thread.sleep(200)
+                }
             }
+
+            Thread.sleep(3000)
+
+            sql "alter table ${table1} modify column c3 varchar(100) null;"
+
+            Thread.sleep(3000);
+
+            unblock()
+
+            def t2 = Thread.start {
+                waitForSchemaChangeDone {
+                    sql """SHOW ALTER TABLE COLUMN WHERE TableName='${table1}' ORDER BY createtime DESC LIMIT 1"""
+                    time 20000
+                }
+            }
+            
+            Thread.sleep(5000);
+            stopped.set(true)
+            t1.join()
+            t2.join()
+
+            qt_sql "select count(*) from ${table1} group by k1 having count(*) > 1;"
+
+        } catch(Exception e) {
+            logger.info(e.getMessage())
+            throw e
+        } finally {
+            GetDebugPoint().clearDebugPointsForAllFEs()
+            GetDebugPoint().clearDebugPointsForAllBEs()
         }
-
-        Thread.sleep(3000)
-
-        sql "alter table ${table1} modify column c3 varchar(100) null;"
-
-        Thread.sleep(3000);
-
-        GetDebugPoint().disableDebugPointForAllBEs("SchemaChangeJob::_convert_historical_rowsets.block")
-
-        waitForSchemaChangeDone {
-            sql """SHOW ALTER TABLE COLUMN WHERE TableName='${table1}' ORDER BY createtime DESC LIMIT 1"""
-            time 20000
-        }
-
-        stopped.set(true)
-        t1.join()
-
-        qt_sql "select count(*) from ${table1} group by k1 having count(*) > 1;"
-
-    } catch(Exception e) {
-        logger.info(e.getMessage())
-        throw e
-    } finally {
-        GetDebugPoint().clearDebugPointsForAllFEs()
-        GetDebugPoint().clearDebugPointsForAllBEs()
     }
 }
