ff24a4facb0c58090f836974d44da3319b618956
abmdocrt <Yukang.Lian2022@gmail.com>
2024-09-03 11:36:17 +0800

    [Fix](regression-test) Fix `test_group_commit_stream_load_with_nonexist_db_and_table` regression test case (#40109)

--- regression-test/suites/load_p0/stream_load/test_group_commit_stream_load_with_nonexist_db_and_table.groovy
+++ regression-test/suites/load_p0/stream_load/test_group_commit_stream_load_with_nonexist_db_and_table.groovy
@@ -31,7 +31,7 @@ suite("test_group_commit_stream_load_with_nonexist_db_and_table") {
         code = process.waitFor()
         out = process.text
         log.info("stream lad result: ${out}".toString())
-        assertTrue(out.toString().contains("table not found"))
+        assertTrue(out.toString().contains("OlapTable not found"))
     } catch (Exception e) {
         logger.info("failed: " + e.getMessage())
         assertTrue(false)
