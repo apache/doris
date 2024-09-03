e3b1d0ca54ce72e9aa8b3886297dc9576dc58743
lihangyu <15605149486@163.com>
2024-09-03 17:56:00 +0800

    [regression-test](cases) optimize some cases (#40174) (#40326)
    #40174

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -990,7 +990,7 @@ DEFINE_mInt64(workload_group_scan_task_wait_timeout_ms, "10000");
 DEFINE_Bool(enable_index_apply_preds_except_leafnode_of_andnode, "true");
 
 DEFINE_mDouble(variant_ratio_of_defaults_as_sparse_column, "1");
-DEFINE_mInt64(variant_threshold_rows_to_estimate_sparse_column, "1000");
+DEFINE_mInt64(variant_threshold_rows_to_estimate_sparse_column, "2048");
 DEFINE_mBool(variant_throw_exeception_on_invalid_json, "false");
 
 // block file cache
