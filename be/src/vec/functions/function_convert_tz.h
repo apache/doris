37de30079e6e4b1a09b1d097534b97a39c2bec63
ayuanzhang <ayuanzhang@tencent.com>
2024-09-04 10:05:44 +0800

    fix2

--- be/src/vec/functions/function_convert_tz.h
+++ be/src/vec/functions/function_convert_tz.h
@@ -398,12 +398,6 @@ public:
         return {std::make_shared<ArgDateType>(), std::make_shared<DataTypeString>()};
     }
 
-    bool use_default_implementation_for_nulls() const override { return false; }
-
-    Status close(FunctionContext* context, FunctionContext::FunctionStateScope scope) override {
-        return Status::OK();
-    }
-
     Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
                         size_t result, size_t input_rows_count) const override {
         auto result_null_map_column = ColumnUInt8::create(input_rows_count, 0);
@@ -422,10 +416,6 @@ public:
         const auto is_nullable = block.get_by_position(result).type->is_nullable();
 
         default_preprocess_parameter_columns(argument_columns, col_const, {1}, block, arguments);
-
-        for (int i = 0; i < 2; i++) {
-            check_set_nullable(argument_columns[i], result_null_map_column, col_const[i]);
-        }
 
         auto result_column = ColumnType::create();
 
