fc2a7f7e221d25452b3c85cb878e45e8d92a67c7
zhaochangle <zhaochangle@selectdb.com>
2024-09-06 10:09:39 +0800

    1

--- be/test/vec/function/function_math_test.cpp
+++ be/test/vec/function/function_math_test.cpp
@@ -15,21 +15,13 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include <limits.h>
-#include <stdint.h>
-
-#include <cmath>
-#include <iomanip>
+#include <climits>
+#include <cstdint>
 #include <limits>
 #include <string>
-#include <vector>
 
-#include "common/status.h"
 #include "function_test_util.h"
-#include "gtest/gtest_pred_impl.h"
-#include "testutil/any_type.h"
 #include "vec/core/types.h"
-#include "vec/data_types/data_type_nullable.h"
 #include "vec/data_types/data_type_number.h"
 #include "vec/data_types/data_type_string.h"
 
