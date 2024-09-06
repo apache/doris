33f103c1f6600d163ecdbb522e23f948b2974874
morningman <morningman@163.com>
2024-09-06 20:14:24 +0800

    fix

--- regression-test/suites/external_table_p0/jdbc/test_jdbc_catalog_ddl.groovy
+++ regression-test/suites/external_table_p0/jdbc/test_jdbc_catalog_ddl.groovy
@@ -27,40 +27,47 @@ suite("test_jdbc_catalog_ddl", "p0,external,mysql,external_docker,external_docke
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
         String catalog_name = "mysql_jdbc5_catalog";
 
-        sql """drop catalog if exists ${catalog_name} """
-        sql """create catalog if not exists ${catalog_name} properties(
-            "type"="jdbc",
-            "user"="root",
-            "password"="123456",
-            "jdbc_url" = "jdbc:mysql://${externalEnvIp}:${mysql_port}/doris_test?useSSL=false&zeroDateTimeBehavior=convertToNull",
-            "driver_url" = "${driver_url}",
-            "driver_class" = "com.mysql.jdbc.Driver"
-        );"""
-        order_qt_show_db """ show databases from ${catalog_name}; """
+        for (String useMetaCache : ["true", "false"]) {
+            sql """drop catalog if exists ${catalog_name} """
+            sql """create catalog if not exists ${catalog_name} properties(
+                "type"="jdbc",
+                "user"="root",
+                "password"="123456",
+                "jdbc_url" = "jdbc:mysql://${externalEnvIp}:${mysql_port}/doris_test?useSSL=false&zeroDateTimeBehavior=convertToNull",
+                "driver_url" = "${driver_url}",
+                "driver_class" = "com.mysql.jdbc.Driver",
+                "use_meta_cache" = "${useMetaCache}"
+            );"""
+            order_qt_show_db """ show databases from ${catalog_name}; """
 
-        // test wrong catalog and db
-        test {
-            sql """switch unknown_catalog"""
-            exception "Unknown catalog 'unknown_catalog'"
-        }
-        test {
-            sql """use unknown_catalog.db1"""
-            exception """Unknown catalog 'unknown_catalog'"""
-        }
-        test {
-            sql """use ${catalog_name}.unknown_db"""
-            exception """Unknown database 'unknown_db'"""
-        }
+            // test wrong catalog and db
+            test {
+                sql """switch unknown_catalog"""
+                exception "Unknown catalog 'unknown_catalog'"
+            }
+            test {
+                sql """use unknown_catalog.db1"""
+                exception """Unknown catalog 'unknown_catalog'"""
+            }
+            test {
+                sql """use ${catalog_name}.unknown_db"""
+                exception """Unknown database 'unknown_db'"""
+            }
 
-        // create a database in mysql
-        sql """CALL EXECUTE_STMT("${catalog_name}",  "drop database if exists temp_database")"""
-        sql """CALL EXECUTE_STMT("${catalog_name}",  "create database temp_database")"""
-        sql """CALL EXECUTE_STMT("${catalog_name}",  "drop table if exists temp_database.temp_table")"""
-        sql """CALL EXECUTE_STMT("${catalog_name}",  "create table temp_database.temp_table (k1 int)")"""
-        sql """CALL EXECUTE_STMT("${catalog_name}",  "insert into temp_database.temp_table values(12345)")"""
+            // create a database in mysql
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "drop database if exists temp_database")"""
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "create database temp_database")"""
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "drop table if exists temp_database.temp_table")"""
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "create table temp_database.temp_table (k1 int)")"""
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "insert into temp_database.temp_table values(12345)")"""
 
-        sql "use ${catalog_name}.temp_database"
-        qt_sql01 """select * from temp_table"""
+            if (useMetaCache.equals("false")) {
+                sql """refresh catalog ${catalog_name}"""
+            }
+            sql "use ${catalog_name}.temp_database"
+            qt_sql01 """select * from temp_table"""
+            sql """CALL EXECUTE_STMT("${catalog_name}",  "drop database if exists temp_database")"""
+        }
     }
 }
 
