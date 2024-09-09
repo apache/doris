349544fd681bbd4cffdbe0f87ddb412393cd7d7a
daidai <2017501503@qq.com>
2024-09-09 10:10:23 +0800

    fix p2

--- regression-test/suites/external_table_p2/maxcompute/test_external_catalog_maxcompute.groovy
+++ regression-test/suites/external_table_p2/maxcompute/test_external_catalog_maxcompute.groovy
@@ -280,7 +280,7 @@
     );
   
  */
-suite("test_external_catalog_maxcompute", "p0,external,doris,external_docker,external_docker_doris") {
+suite("test_external_catalog_maxcompute", "p2,external,doris,external_docker,external_docker_doris") {
     String enabled = context.config.otherConfigs.get("enableMaxComputeTest")
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
         String ak = context.config.otherConfigs.get("aliYunAk")
--- regression-test/suites/external_table_p2/maxcompute/test_max_compute_complex_type.groovy
+++ regression-test/suites/external_table_p2/maxcompute/test_max_compute_complex_type.groovy
@@ -151,7 +151,7 @@
         )
     );
  */
-suite("test_max_compute_complex_type", "p0,external,doris,external_docker,external_docker_doris") {
+suite("test_max_compute_complex_type", "p2,external,doris,external_docker,external_docker_doris") {
     String enabled = context.config.otherConfigs.get("enableMaxComputeTest")
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
         String ak = context.config.otherConfigs.get("aliYunAk")
