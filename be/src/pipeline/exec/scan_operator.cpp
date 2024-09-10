aa5f88250f276aa60acfb974bc9540d7a2baa365
zhiqiang-hhhh <seuhezhiqiang@163.com>
2024-09-11 00:46:19 +0800

    X

--- be/src/pipeline/exec/scan_operator.cpp
+++ be/src/pipeline/exec/scan_operator.cpp
@@ -996,7 +996,7 @@ Status ScanLocalState<Derived>::_start_scanners(
     auto& p = _parent->cast<typename Derived::Parent>();
     _scanner_ctx = vectorized::ScannerContext::create_shared(
             state(), this, p._output_tuple_desc, p.output_row_descriptor(), scanners, p.limit(),
-            _scan_dependency, p.ignore_data_distribution);
+            _scan_dependency, p.ignore_data_distribution());
     return Status::OK();
 }
 
