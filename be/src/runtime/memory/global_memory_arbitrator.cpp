15d876504d0978894b78f8c11ba288e0a039f36a
Xinyi Zou <zouxinyi02@gmail.com>
2024-09-10 11:24:04 +0800

    [fix](memory) Not check process memory limit when thread reserve memory is sufficient (#40548)

--- be/src/runtime/memory/global_memory_arbitrator.cpp
+++ be/src/runtime/memory/global_memory_arbitrator.cpp
@@ -79,4 +79,12 @@ void GlobalMemoryArbitrator::release_process_reserved_memory(int64_t bytes) {
     }
 }
 
+int64_t GlobalMemoryArbitrator::sub_thread_reserve_memory(int64_t bytes) {
+    doris::ThreadContext* thread_context = doris::thread_context(true);
+    if (thread_context) {
+        return bytes - doris::thread_context()->thread_mem_tracker_mgr->reserved_mem();
+    }
+    return bytes;
+}
+
 } // namespace doris
