5d42d84b46225c9670888f92908a7dc8a54189a3
LiBinfeng <1204975323@qq.com>
2024-09-10 15:03:23 +0800

    test for exception added

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnFE.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnFE.java
@@ -91,7 +91,6 @@ import com.google.common.collect.ImmutableList.Builder;
 import com.google.common.collect.Lists;
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.time.DateTimeException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -436,8 +435,6 @@ public class FoldConstantRuleOnFE extends AbstractExpressionRewriteRule
                     // If cast is from type coercion, we don't use NULL literal and will throw exception.
                     throw t;
                 }
-            } catch (DateTimeException e) {
-                return new NullLiteral(dataType);
             }
         }
         try {
