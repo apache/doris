c4adea1c3a24db8b93ca14ff1f02038714eda4ec
yiguolei <676222867@qq.com>
2024-09-10 17:08:20 +0800

    Revert "[improvement](statistics)Return -1 to neredis if report olap table ro…"
    This reverts commit e43e6e2bba1e50fd7c59d3fcd206d31136ab18fb.

--- fe/fe-core/src/main/java/org/apache/doris/catalog/MaterializedIndex.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/MaterializedIndex.java
@@ -73,8 +73,6 @@ public class MaterializedIndex extends MetaObject implements Writable, GsonPostP
     @SerializedName(value = "rollupFinishedVersion")
     private long rollupFinishedVersion;
 
-    private boolean rowCountReported = false;
-
     public MaterializedIndex() {
         this.state = IndexState.NORMAL;
         this.idToTablets = new HashMap<>();
@@ -206,14 +204,6 @@ public class MaterializedIndex extends MetaObject implements Writable, GsonPostP
             idx++;
         }
         return -1;
-    }
-
-    public void setRowCountReported(boolean reported) {
-        this.rowCountReported = reported;
-    }
-
-    public boolean getRowCountReported() {
-        return this.rowCountReported;
     }
 
     @Override
