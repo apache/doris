042bfcef7b739b3c15b0931f21329c53f38c581f
minghong <englefly@gmail.com>
2024-09-11 16:13:24 +0800

    [opt](nereids) when table.rows > topn.limit+topn.offset, do not generate topn-filter (#40480)
    ## Proposed changes
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/TopnFilterPushDownVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/TopnFilterPushDownVisitor.java
@@ -218,8 +218,12 @@ public class TopnFilterPushDownVisitor extends PlanVisitor<Boolean, PushDownCont
     public Boolean visitPhysicalRelation(PhysicalRelation relation, PushDownContext ctx) {
         if (supportPhysicalRelations(relation)
                 && relation.getOutputSet().containsAll(ctx.probeExpr.getInputSlots())) {
-            topnFilterContext.addTopnFilter(ctx.topn, relation, ctx.probeExpr);
-            return true;
+            // in ut, relation.getStats() may return null
+            if (relation.getStats() == null
+                    || relation.getStats().getRowCount() > ctx.topn.getLimit() + ctx.topn.getOffset()) {
+                topnFilterContext.addTopnFilter(ctx.topn, relation, ctx.probeExpr);
+                return true;
+            }
         }
         return false;
     }
