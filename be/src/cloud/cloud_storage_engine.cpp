d6132bb73c27a3ea43fab7f6e3f5d8551cbbcae7
Yongqiang YANG <98214048+dataroaring@users.noreply.github.com>
2024-09-12 17:22:57 +0800

    [improvement](cloud) manage node via sql like non cloud mode (#40264)
    ## Proposed changes
    
    1. add a config deploy_mode to enable disaggregated mode. In this mode
    users have to config cloud_instance_id and meta_service_endpoint.
    
    2. When a fe starts from empty, it would try to create instance.
    
    3. If the instance does not exists, it creates and starts from master
    role. If the instance exists, then it stats with its role got from ms.
    
    4. Frontends are added via sql alter system add frontend. Backends are
    added via sql alter system add backend.
    
    5. Users do not need config cloud_instance_id and meta_service_endpoint
    in be.conf, because fe sends them to be via heartbeat.
    
    6. Builtin vault is not needed any more, internal tabels are stored in
    default vault.
    
    TODO:
    
    1. decomission fe and be via sql
    2. change cloud_instance_id to cluster id.
    
    doc pr https://github.com/apache/doris-website/pull/1072

--- be/src/cloud/cloud_storage_engine.cpp
+++ be/src/cloud/cloud_storage_engine.cpp
@@ -160,29 +160,7 @@ struct RefreshFSVaultVisitor {
 };
 
 Status CloudStorageEngine::open() {
-    cloud::StorageVaultInfos vault_infos;
-    bool enable_storage_vault = false;
-    do {
-        auto st = _meta_mgr->get_storage_vault_info(&vault_infos, &enable_storage_vault);
-        if (st.ok()) {
-            break;
-        }
-
-        LOG(WARNING) << "failed to get vault info, retry after 5s, err=" << st;
-        std::this_thread::sleep_for(5s);
-    } while (vault_infos.empty());
-
-    for (auto& [id, vault_info, path_format] : vault_infos) {
-        if (auto st = std::visit(VaultCreateFSVisitor {id, path_format}, vault_info); !st.ok())
-                [[unlikely]] {
-            return vault_process_error(id, vault_info, std::move(st));
-        }
-    }
-
-    // vault mode should not support latest_fs to get rid of unexpected storage backends choosen
-    if (!enable_storage_vault) {
-        set_latest_fs(get_filesystem(std::get<0>(vault_infos.back())));
-    }
+    sync_storage_vault();
 
     // TODO(plat1ko): DeleteBitmapTxnManager
 
