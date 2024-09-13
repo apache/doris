b67a43a9abf91b8ab44e0344d2b9835ab4698a1c
wuwenchi <wuwenchihdu@hotmail.com>
2024-09-13 13:48:07 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/qe/MysqlConnectProcessor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/MysqlConnectProcessor.java
@@ -283,6 +283,8 @@ public class MysqlConnectProcessor extends ConnectProcessor {
         finalizeCommand();
 
         ctx.setCommand(MysqlCommand.COM_SLEEP);
+        ctx.clear();
+        executor = null;
     }
 
     public void loop() {
