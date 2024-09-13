431e2e1af955390fb5fffa999430a49da64494aa
Qi Chen <kaka11.chen@gmail.com>
2024-09-13 10:02:12 +0800

    [Enhancement](be-logger) Support custom date time format functionality in be log. (#40727)
    ## Proposed changes
    
    backport #40347

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -220,6 +220,12 @@ DEFINE_Int32(sys_log_verbose_level, "10");
 DEFINE_Int32(sys_log_verbose_flags_v, "-1");
 // log buffer level
 DEFINE_String(log_buffer_level, "");
+// log enable custom date time format
+DEFINE_Bool(sys_log_enable_custom_date_time_format, "false");
+// log custom date time format (https://en.cppreference.com/w/cpp/io/manip/put_time)
+DEFINE_String(sys_log_custom_date_time_format, "%Y-%m-%d %H:%M:%S");
+// log custom date time milliseconds format (fmt::format)
+DEFINE_String(sys_log_custom_date_time_ms_format, ",{:03d}");
 
 // number of threads available to serve backend execution requests
 DEFINE_Int32(be_service_threads, "64");
