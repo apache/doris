6d0b67a76e45ebcc5c31e738a76416ec49ae5701
zclllhhjj <zhaochangle@selectdb.com>
2024-09-14 10:22:04 +0800

    [Fix](Column) refactor ColumnNullable to provide flags safety (#40769)
    ## Proposed changes
    
    Issue Number: close #xxx
    
    In this pr we move null_map and relative flags to an individual base
    class to constrain its visibility.
    please reed the comment carefully
    
    test in master:
    ```
    xxx/column_nullable_test.cpp:187: Failure
    Value of: null_dst->has_null()
      Actual: false
    Expected: true
    
    Error occurred in case0
    [  FAILED  ] ColumnNullableTest.PredicateTest (0 ms)
    [ RUN      ] ColumnNullableTest.HashTest
    [       OK ] ColumnNullableTest.HashTest (0 ms)
    [----------] 3 tests from ColumnNullableTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 3 tests from 1 test suite ran. (1 ms total)
    [  PASSED  ] 2 tests.
    [  FAILED  ] 1 test, listed below:
    [  FAILED  ] ColumnNullableTest.PredicateTest
    ```
    
    and success after this pr
    
    ---------
    
    Co-authored-by: Jerry Hu <mrhhsg@gmail.com>

--- be/test/vec/function/function_test_util.h
+++ be/test/vec/function/function_test_util.h
@@ -272,6 +272,14 @@ Status check_function(const std::string& func_name, const InputTypeSet& input_ty
         fn_ctx_return.type = doris::PrimitiveType::TYPE_DATEV2;
     } else if (std::is_same_v<ReturnType, DateTimeV2>) {
         fn_ctx_return.type = doris::PrimitiveType::TYPE_DATETIMEV2;
+    } else if (std::is_same_v<ReturnType, Decimal128V2>) {
+        fn_ctx_return.type = doris::PrimitiveType::TYPE_DECIMALV2;
+    } else if (std::is_same_v<ReturnType, Decimal32>) {
+        fn_ctx_return.type = doris::PrimitiveType::TYPE_DECIMAL32;
+    } else if (std::is_same_v<ReturnType, Decimal64>) {
+        fn_ctx_return.type = doris::PrimitiveType::TYPE_DECIMAL64;
+    } else if (std::is_same_v<ReturnType, Decimal128V3>) {
+        fn_ctx_return.type = doris::PrimitiveType::TYPE_DECIMAL128I;
     } else {
         fn_ctx_return.type = doris::PrimitiveType::INVALID_TYPE;
     }
