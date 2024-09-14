eaef2ebfac1cafcc8e446e460f6951b0a63243a5
w41ter <w41ter.l@gmail.com>
2024-09-14 09:51:58 +0000

    [fix](restore) Fix view name in atomic restore
    Only olap table need to be replaced in atomic.

--- fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
@@ -908,7 +908,7 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
                 if (Env.isStoredTableNamesLowerCase()) {
                     tableName = tableName.toLowerCase();
                 }
-                if (isAtomicRestore) {
+                if (restoreTbl.getType() == TableType.OLAP && isAtomicRestore) {
                     tableName = tableAliasWithAtomicRestore(tableName);
                 }
                 restoreTbl.setName(tableName);
