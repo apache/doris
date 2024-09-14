4e9d0fb77f187eb549a28982ef8ec6c367aa4d9a
camby <cambyzhu@tencent.com>
2024-09-14 10:18:55 +0800

    [fix](encrypt) wrong mode arg of encrypt and decrypt function make BE crash (#40726)
    Not supported mode arg in sm4_encrypt and sm4_decrypt make BE crash.
    
    Intro by https://github.com/apache/doris/pull/37194

--- be/src/vec/functions/function_encryption.cpp
+++ be/src/vec/functions/function_encryption.cpp
@@ -236,8 +236,9 @@ struct EncryptionAndDecryptTwoImpl {
         if (mode_arg.size != 0) {
             if (!aes_mode_map.contains(mode_str)) {
                 all_insert_null = true;
+            } else {
+                encryption_mode = aes_mode_map.at(mode_str);
             }
-            encryption_mode = aes_mode_map.at(mode_str);
         }
         const ColumnString::Offsets* offsets_column = &column->get_offsets();
         const ColumnString::Chars* chars_column = &column->get_chars();
@@ -346,13 +347,15 @@ struct EncryptionAndDecryptFourImpl {
             if constexpr (is_sm_mode) {
                 if (sm4_mode_map.count(mode_str) == 0) {
                     all_insert_null = true;
+                } else {
+                    encryption_mode = sm4_mode_map.at(mode_str);
                 }
-                encryption_mode = sm4_mode_map.at(mode_str);
             } else {
                 if (aes_mode_map.count(mode_str) == 0) {
                     all_insert_null = true;
+                } else {
+                    encryption_mode = aes_mode_map.at(mode_str);
                 }
-                encryption_mode = aes_mode_map.at(mode_str);
             }
         }
 
