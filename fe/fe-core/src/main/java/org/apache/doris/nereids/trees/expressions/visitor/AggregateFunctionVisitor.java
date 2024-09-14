51e2dd3a8e009f17b05c47226740ad8657239f8c
starocean999 <12095047@qq.com>
2024-09-14 16:05:48 +0800

    [fix](nereids)adjust some agg function's nullability

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/AggregateFunctionVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/AggregateFunctionVisitor.java
@@ -151,7 +151,7 @@ public interface AggregateFunctionVisitor<R, C> {
     }
 
     default R visitCovarSamp(CovarSamp covarSamp, C context) {
-        return visitNullableAggregateFunction(covarSamp, context);
+        return visitAggregateFunction(covarSamp, context);
     }
 
     default R visitMultiDistinctCount(MultiDistinctCount multiDistinctCount, C context) {
@@ -283,7 +283,7 @@ public interface AggregateFunctionVisitor<R, C> {
     }
 
     default R visitStddevSamp(StddevSamp stddevSamp, C context) {
-        return visitNullableAggregateFunction(stddevSamp, context);
+        return visitAggregateFunction(stddevSamp, context);
     }
 
     default R visitSum(Sum sum, C context) {
@@ -311,7 +311,7 @@ public interface AggregateFunctionVisitor<R, C> {
     }
 
     default R visitVarianceSamp(VarianceSamp varianceSamp, C context) {
-        return visitNullableAggregateFunction(varianceSamp, context);
+        return visitAggregateFunction(varianceSamp, context);
     }
 
     default R visitWindowFunnel(WindowFunnel windowFunnel, C context) {
