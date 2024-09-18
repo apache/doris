apache/doris

commit 9c87436f9526e0f160e3254999ff81e58b1d58bd

[fix](memory) Refactor MemCounter (#40542)
1. Add a new class MemCounter.
2. MemTracker and MemTrackerLimiter no longer have inheritance and
dependencies.
3. MemTrackerLimiter is used to count process memory, the BE web page
`/mem_tracker` also only displays MemTrackerLimiter.
4. MemTracker is used to count the memory of operators and some data
structures. It is not counted in the process memory and is used for
logic control and profile.

In addition, it seems that the crash is caused by memory abnormality,
not sure whether this PR can fix the problem, but this will help locate
the problem and more elegant.

```
==6641==ERROR: AddressSanitizer: heap-use-after-free on address 0x603000980e70 at pc 0x559be21880be bp 0x7fd7cfc75070 sp 0x7fd7cfc75068
READ of size 8 at 0x603000980e70 thread T1453 (memory_maintena)
    #0 0x559be21880bd in std::__atomic_base<long>::load(std::memory_order) const /var/local/ldb-toolchain/bin/../lib/gcc/x86_64-linux-gnu/11/../../../../include/c++/11/bits/atomic_base.h:481:9
    #1 0x559be21880bd in doris::MemTracker::MemCounter::current_value() const /home/zcp/repo_center/doris_master/doris/be/src/runtime/memory/mem_tracker.h:139:63
    #2 0x559be21880bd in doris::MemTracker::consumption() const /home/zcp/repo_center/doris_master/doris/be/src/runtime/memory/mem_tracker.h:165:56
    #3 0x559be3985133 in doris::MemTrackerLimiter::refresh_global_counter() /home/zcp/repo_center/doris_master/doris/be/src/runtime/memory/mem_tracker_limiter.cpp:245:59
    #4 0x559bdfcd52d5 in doris::Daemon::memory_maintenance_thread() /home/zcp/repo_center/doris_master/doris/be/src/common/daemon.cpp:239:13
    #5 0x559be40e6c17 in doris::Thread::supervise_thread(void*) /home/zcp/repo_center/doris_master/doris/be/src/util/thread.cpp:498:5
    #6 0x7fdf563e3ac2 in start_thread nptl/pthread_create.c:442:8
    #7 0x7fdf5647584f  misc/../sysdeps/unix/sysv/linux/x86_64/clone3.S:81

be/src/runtime/memory/mem_tracker.h
@@ -15,216 +15,59 @@
 // specific language governing permissions and limitations
 // under the License.
 // This file is copied from
-// https://github.com/apache/impala/blob/branch-2.9.0/be/src/runtime/mem-tracker.h
-// and modified by Doris
 #pragma once
 
-#include <gen_cpp/Metrics_types.h>
-#include <stdint.h>
-
-#include <atomic>
-// IWYU pragma: no_include <bits/std_abs.h>
-#include <cmath> // IWYU pragma: keep
-#include <list>
-#include <memory>
-#include <ostream>
+#include <cstdint>
 #include <string>
-#include <vector>
 
-#include "common/compiler_util.h" // IWYU pragma: keep
-#include "runtime/query_statistics.h"
-#include "util/pretty_printer.h"
+#include "runtime/memory/mem_counter.h"
 
 namespace doris {
 
-class MemTrackerLimiter;
-
-// Used to track memory usage.
-//
-// MemTracker can be consumed manually by consume()/release(), or put into SCOPED_CONSUME_MEM_TRACKER,
-// which will automatically track all memory usage of the code segment where it is located.
-//
-// This class is thread-safe.
-class MemTracker {
+/*
+ * can be consumed manually by consume()/release(), or put into SCOPED_CONSUME_MEM_TRACKER,
+ * which will automatically track all memory usage of the code segment where it is located.
+ *
+ * This class is thread-safe.
+*/
+class MemTracker final {
 public:
-    struct Snapshot {
-        std::string type;
-        std::string label;
-        std::string parent_label;
-        int64_t limit = 0;
-        int64_t cur_consumption = 0;
-        int64_t peak_consumption = 0;
+    MemTracker() = default;
+    MemTracker(const std::string& label);
+    ~MemTracker();
 
-        bool operator<(const Snapshot& rhs) const { return cur_consumption < rhs.cur_consumption; }
-    };
+    void consume(int64_t bytes) { _mem_counter.add(bytes); }
+    void consume_no_update_peak(int64_t bytes) { _mem_counter.add_no_update_peak(bytes); }
+    void release(int64_t bytes) { _mem_counter.sub(bytes); }
+    void set_consumption(int64_t bytes) { _mem_counter.set(bytes); }
+    int64_t consumption() const { return _mem_counter.current_value(); }
+    int64_t peak_consumption() const { return _mem_counter.peak_value(); }
 
-    struct TrackerGroup {
+    const std::string& label() const { return _label; }
+    std::string log_usage() const {
+        return fmt::format("MemTracker Lame={}, Used={}({} B), Peak={}({} B)",
+                           MemCounter::print_bytes(consumption()), consumption(),
+                           MemCounter::print_bytes(peak_consumption()), peak_consumption());
+    }
+
+private:
+    MemCounter _mem_counter;
+    std::string _label {"None"};
+
+    /*
+    * Save all MemTrackers, used by dump memory info.
+    */
+    struct TrackersGroup {
         std::list<MemTracker*> trackers;
         std::mutex group_lock;
     };
-
-    enum class Type {
-        GLOBAL = 0,        // Life cycle is the same as the process, e.g. Cache and default Orphan
-        QUERY = 1,         // Count the memory consumption of all Query tasks.
-        LOAD = 2,          // Count the memory consumption of all Load tasks.
-        COMPACTION = 3,    // Count the memory consumption of all Base and Cumulative tasks.
-        SCHEMA_CHANGE = 4, // Count the memory consumption of all SchemaChange tasks.
-        OTHER = 5
-    };
-
-    static std::string type_string(Type type) {
-        switch (type) {
-        case Type::GLOBAL:
-            return "global";
-        case Type::QUERY:
-            return "query";
-        case Type::LOAD:
-            return "load";
-        case Type::COMPACTION:
-            return "compaction";
-        case Type::SCHEMA_CHANGE:
-            return "schema_change";
-        case Type::OTHER:
-            return "other";
-        default:
-            LOG(FATAL) << "not match type of mem tracker limiter :" << static_cast<int>(type);
-        }
-        LOG(FATAL) << "__builtin_unreachable";
-        __builtin_unreachable();
-    }
-
-    // A counter that keeps track of the current and peak value seen.
-    // Relaxed ordering, not accurate in real time.
-    class MemCounter {
-    public:
-        MemCounter() : _current_value(0), _peak_value(0) {}
-
-        void add(int64_t delta) {
-            int64_t value = _current_value.fetch_add(delta, std::memory_order_relaxed) + delta;
-            update_peak(value);
-        }
-
-        void add_no_update_peak(int64_t delta) {
-            _current_value.fetch_add(delta, std::memory_order_relaxed);
-        }
-
-        bool try_add(int64_t delta, int64_t max) {
-            int64_t cur_val = _current_value.load(std::memory_order_relaxed);
-            int64_t new_val = 0;
-            do {
-                new_val = cur_val + delta;
-                if (UNLIKELY(new_val > max)) {
-                    return false;
-                }
-            } while (UNLIKELY(!_current_value.compare_exchange_weak(cur_val, new_val,
-                                                                    std::memory_order_relaxed)));
-            update_peak(new_val);
-            return true;
-        }
-
-        void sub(int64_t delta) { _current_value.fetch_sub(delta, std::memory_order_relaxed); }
-
-        void set(int64_t v) {
-            _current_value.store(v, std::memory_order_relaxed);
-            update_peak(v);
-        }
-
-        void update_peak(int64_t value) {
-            int64_t pre_value = _peak_value.load(std::memory_order_relaxed);
-            while (value > pre_value && !_peak_value.compare_exchange_weak(
-                                                pre_value, value, std::memory_order_relaxed)) {
-            }
-        }
-
-        int64_t current_value() const { return _current_value.load(std::memory_order_relaxed); }
-        int64_t peak_value() const { return _peak_value.load(std::memory_order_relaxed); }
-
-    private:
-        std::atomic<int64_t> _current_value;
-        std::atomic<int64_t> _peak_value;
-    };
-
-    // Creates and adds the tracker to the mem_tracker_pool.
-    MemTracker(const std::string& label, MemTrackerLimiter* parent = nullptr);
-
-    virtual ~MemTracker();
-
-    static std::string print_bytes(int64_t bytes) {
-        return bytes >= 0 ? PrettyPrinter::print(bytes, TUnit::BYTES)
-                          : "-" + PrettyPrinter::print(std::abs(bytes), TUnit::BYTES);
-    }
-
-public:
-    Type type() const { return _type; }
-    const std::string& label() const { return _label; }
-    const std::string& parent_label() const { return _parent_label; }
-    const std::string& set_parent_label() const { return _parent_label; }
-    // Returns the memory consumed in bytes.
-    int64_t consumption() const { return _consumption->current_value(); }
-    int64_t peak_consumption() const { return _consumption->peak_value(); }
-
-    void consume(int64_t bytes) {
-        if (UNLIKELY(bytes == 0)) {
-            return;
-        }
-        _consumption->add(bytes);
-        if (_query_statistics) {
-            _query_statistics->set_max_peak_memory_bytes(_consumption->peak_value());
-            _query_statistics->set_current_used_memory_bytes(_consumption->current_value());
-        }
-    }
-
-    void consume_no_update_peak(int64_t bytes) { // need extreme fast
-        _consumption->add_no_update_peak(bytes);
-    }
-
-    void release(int64_t bytes) { _consumption->sub(bytes); }
-
-    void set_consumption(int64_t bytes) { _consumption->set(bytes); }
-
-    std::shared_ptr<QueryStatistics> get_query_statistics() { return _query_statistics; }
-
-public:
-    virtual Snapshot make_snapshot() const;
-    // Specify group_num from mem_tracker_pool to generate snapshot.
-    static void make_group_snapshot(std::vector<Snapshot>* snapshots, int64_t group_num,
-                                    std::string parent_label);
-    static void make_all_trackers_snapshots(std::vector<Snapshot>* snapshots);
-    static std::string log_usage(MemTracker::Snapshot snapshot);
-
-    virtual std::string debug_string() {
-        std::stringstream msg;
-        msg << "label: " << _label << "; "
-            << "consumption: " << consumption() << "; "
-            << "peak_consumption: " << peak_consumption() << "; ";
-        return msg.str();
-    }
-
-protected:
-    // Only used by MemTrackerLimiter
-    MemTracker() { _parent_group_num = -1; }
-
-    void bind_parent(MemTrackerLimiter* parent);
-
-    Type _type;
-
-    // label used in the make snapshot, not guaranteed unique.
-    std::string _label;
-
-    std::shared_ptr<MemCounter> _consumption = nullptr;
-
-    // Tracker is located in group num in mem_tracker_pool
-    int64_t _parent_group_num = 0;
-
-    // Use _parent_label to correlate with parent limiter tracker.
-    std::string _parent_label = "-";
-
-    static std::vector<TrackerGroup> mem_tracker_pool;
-
+    // Each group corresponds to several MemCountes and has a lock.
+    // Multiple groups are used to reduce the impact of locks.
+    static std::vector<TrackersGroup> mem_tracker_pool;
+    // Group number in mem_tracker_pool, generated by the timestamp.
+    int64_t _group_num {-1};
     // Iterator into mem_tracker_pool for this object. Stored to have O(1) remove.
-    std::list<MemTracker*>::iterator _tracker_group_it;
-
-    std::shared_ptr<QueryStatistics> _query_statistics = nullptr;
+    std::list<MemTracker*>::iterator _trackers_group_it;
 };
 
 } // namespace doris

be/src/cloud/cloud_tablet_mgr.cpp
@@ -136,7 +136,7 @@ private:
 CloudTabletMgr::CloudTabletMgr(CloudStorageEngine& engine)
         : _engine(engine),
           _tablet_map(std::make_unique<TabletMap>()),
-          _cache(std::make_unique<LRUCachePolicyTrackingManual>(
+          _cache(std::make_unique<LRUCachePolicy>(
                   CachePolicy::CacheType::CLOUD_TABLET_CACHE, config::tablet_cache_capacity,
                   LRUCacheType::NUMBER, 0, config::tablet_cache_shards)) {}
 

be/src/cloud/cloud_txn_delete_bitmap_cache.cpp
@@ -33,8 +33,8 @@
 namespace doris {
 
 CloudTxnDeleteBitmapCache::CloudTxnDeleteBitmapCache(size_t size_in_bytes)
-        : LRUCachePolicyTrackingManual(CachePolicy::CacheType::CLOUD_TXN_DELETE_BITMAP_CACHE,
-                                       size_in_bytes, LRUCacheType::SIZE, 86400, 4),
+        : LRUCachePolicy(CachePolicy::CacheType::CLOUD_TXN_DELETE_BITMAP_CACHE, size_in_bytes,
+                         LRUCacheType::SIZE, 86400, 4),
           _stop_latch(1) {}
 
 CloudTxnDeleteBitmapCache::~CloudTxnDeleteBitmapCache() {

be/src/cloud/cloud_txn_delete_bitmap_cache.h
@@ -30,7 +30,7 @@
 namespace doris {
 
 // Record transaction related delete bitmaps using a lru cache.
-class CloudTxnDeleteBitmapCache : public LRUCachePolicyTrackingManual {
+class CloudTxnDeleteBitmapCache : public LRUCachePolicy {
 public:
     CloudTxnDeleteBitmapCache(size_t size_in_bytes);
 

be/src/http/default_path_handlers.cpp
@@ -142,7 +142,7 @@ void display_tablets_callback(const WebPageHandler::ArgumentMap& args, EasyJson*
 // Registered to handle "/mem_tracker", and prints out memory tracker information.
 void mem_tracker_handler(const WebPageHandler::ArgumentMap& args, std::stringstream* output) {
     (*output) << "<h1>Memory usage by subsystem</h1>\n";
-    std::vector<MemTracker::Snapshot> snapshots;
+    std::vector<MemTrackerLimiter::Snapshot> snapshots;
     auto iter = args.find("type");
     if (iter != args.end()) {
         if (iter->second == "global") {
@@ -159,7 +159,7 @@ void mem_tracker_handler(const WebPageHandler::ArgumentMap& args, std::stringstr
         } else if (iter->second == "other") {
             MemTrackerLimiter::make_type_snapshots(&snapshots, MemTrackerLimiter::Type::OTHER);
         } else if (iter->second == "reserved_memory") {
-            GlobalMemoryArbitrator::make_reserved_memory_snapshots(&snapshots);
+            MemTrackerLimiter::make_all_reserved_trackers_snapshots(&snapshots);
         } else if (iter->second == "all") {
             MemTrackerLimiter::make_all_memory_state_snapshots(&snapshots);
         }
@@ -191,7 +191,6 @@ void mem_tracker_handler(const WebPageHandler::ArgumentMap& args, std::stringstr
     (*output) << "<thead><tr>"
                  "<th data-sortable='true'>Type</th>"
                  "<th data-sortable='true'>Label</th>"
-                 "<th data-sortable='true'>Parent Label</th>"
                  "<th>Limit</th>"
                  "<th data-sortable='true' "
                  ">Current Consumption(Bytes)</th>"
@@ -207,8 +206,8 @@ void mem_tracker_handler(const WebPageHandler::ArgumentMap& args, std::stringstr
         string peak_consumption_normalize = AccurateItoaKMGT(item.peak_consumption);
         (*output) << strings::Substitute(
                 "<tr><td>$0</td><td>$1</td><td>$2</td><td>$3</td><td>$4</td><td>$5</td><td>$6</"
-                "td><td>$7</td></tr>\n",
-                item.type, item.label, item.parent_label, limit_str, item.cur_consumption,
+                "td></tr>\n",
+                item.type, item.label, limit_str, item.cur_consumption,
                 current_consumption_normalize, item.peak_consumption, peak_consumption_normalize);
     }
     (*output) << "</tbody></table>\n";

be/src/olap/memtable.cpp
@@ -161,9 +161,8 @@ MemTable::~MemTable() {
     std::for_each(_row_in_blocks.begin(), _row_in_blocks.end(), std::default_delete<RowInBlock>());
     _insert_mem_tracker->release(_mem_usage);
     _flush_mem_tracker->set_consumption(0);
-    DCHECK_EQ(_insert_mem_tracker->consumption(), 0)
-            << std::endl
-            << MemTracker::log_usage(_insert_mem_tracker->make_snapshot());
+    DCHECK_EQ(_insert_mem_tracker->consumption(), 0) << std::endl
+                                                     << _insert_mem_tracker->log_usage();
     DCHECK_EQ(_flush_mem_tracker->consumption(), 0);
     _arena.reset();
     _agg_buffer_pool.clear();

be/src/olap/memtable_memory_limiter.cpp
@@ -62,10 +62,7 @@ Status MemTableMemoryLimiter::init(int64_t process_mem_limit) {
             _load_hard_mem_limit * config::load_process_safe_mem_permit_percent / 100;
     g_load_hard_mem_limit.set_value(_load_hard_mem_limit);
     g_load_soft_mem_limit.set_value(_load_soft_mem_limit);
-    _memtable_tracker_set =
-            MemTrackerLimiter::create_shared(MemTrackerLimiter::Type::LOAD, "MemTableTrackerSet");
-    _mem_tracker = std::make_unique<MemTracker>("AllMemTableMemory",
-                                                ExecEnv::GetInstance()->details_mem_tracker_set());
+    _mem_tracker = std::make_unique<MemTracker>("AllMemTableMemory");
     REGISTER_HOOK_METRIC(memtable_memory_limiter_mem_consumption,
                          [this]() { return _mem_tracker->consumption(); });
     _log_timer.start();

be/src/olap/memtable_memory_limiter.h
@@ -20,7 +20,7 @@
 #include <stdint.h>
 
 #include "common/status.h"
-#include "runtime/memory/mem_tracker_limiter.h"
+#include "runtime/memory/mem_tracker.h"
 #include "util/countdown_latch.h"
 #include "util/stopwatch.hpp"
 
@@ -45,7 +45,6 @@ public:
 
     void refresh_mem_tracker();
 
-    MemTrackerLimiter* memtable_tracker_set() { return _memtable_tracker_set.get(); }
     MemTracker* mem_tracker() { return _mem_tracker.get(); }
 
     int64_t mem_usage() const { return _mem_usage; }
@@ -68,8 +67,6 @@ private:
     int64_t _write_mem_usage = 0;
     int64_t _active_mem_usage = 0;
 
-    // mem tracker collection of all mem tables.
-    std::shared_ptr<MemTrackerLimiter> _memtable_tracker_set;
     // sum of all mem table memory.
     std::unique_ptr<MemTracker> _mem_tracker;
     int64_t _load_hard_mem_limit = -1;

commit 241cb7adf6247e555d941ca36cb8369298aeeba5

[bugfix](core) fmt lost one parameter (#40914)
## Proposed changes

bug is introduced by https://github.com/apache/doris/pull/40542

<!--Describe your changes.-->

---------

Co-authored-by: yiguolei <yiguolei@gmail.com>

be/src/runtime/memory/mem_tracker.h
@@ -45,7 +45,7 @@ public:
 
     const std::string& label() const { return _label; }
     std::string log_usage() const {
-        return fmt::format("MemTracker Lame={}, Used={}({} B), Peak={}({} B)",
+        return fmt::format("MemTracker name={}, Used={}({} B), Peak={}({} B)", _label,
                            MemCounter::print_bytes(consumption()), consumption(),
                            MemCounter::print_bytes(peak_consumption()), peak_consumption());
     }