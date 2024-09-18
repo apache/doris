9757e26a88c1a7baa40f2d817564c32e2aefc59b
daidai <2017501503@qq.com>
2024-09-18 18:10:47 +0800

    [fix](regression)fix maxcompute p0 case

--- regression-test/suites/external_table_p0/mc/test_max_compute_complex_type.groovy
+++ regression-test/suites/external_table_p0/mc/test_max_compute_complex_type.groovy
@@ -152,7 +152,7 @@ suite("test_max_compute_complex_type", "p0,external,doris,external_docker,extern
         sql """
         CREATE CATALOG IF NOT EXISTS ${mc_catalog_name} PROPERTIES (
                 "type" = "max_compute",
-                "mc.default.project" = "mc_datalake",
+                "mc.default.project" = "jz_datalake",
                 "mc.region" = "cn-beijing",
                 "mc.access_key" = "${ak}",
                 "mc.secret_key" = "${sk}",
@@ -163,7 +163,7 @@ suite("test_max_compute_complex_type", "p0,external,doris,external_docker,extern
         logger.info("catalog " + mc_catalog_name + " created")
         sql """switch ${mc_catalog_name};"""
         logger.info("switched to catalog " + mc_catalog_name)
-        sql """ use mc_datalake """
+        sql """ use jz_datalake """
 
         qt_mc_q1 """ select id,arr3,arr1,arr5,arr2 from array_table order by id desc """
         qt_mc_q2 """ select arr2,arr1 from map_table order by id limit 2 """
