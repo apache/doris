513784188adf88879dc4c23602527fbbd5fb8e96
Gabriel <gabrielleebuaa@gmail.com>
2024-09-18 10:02:58 +0800

    [PipelineX](improvement) Prepare tasks in parallel (#40844) (#40874)
    pick #40844

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -332,6 +332,7 @@ struct TQueryOptions {
 
   125: optional bool enable_segment_cache = true;
 
+  132: optional i32 parallel_prepare_threshold = 0;
   // For cloud, to control if the content would be written into file cache
   // In write path, to control if the content would be written into file cache.
   // In read path, read from file cache or remote storage when execute query.
