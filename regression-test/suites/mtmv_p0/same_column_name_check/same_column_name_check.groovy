5559bd311ea41a26c880b7ee739439f938668d10
seawinde <daydayup005@yeah.net>
2024-09-19 15:48:12 +0800

    fix comment

--- regression-test/suites/mtmv_p0/same_column_name_check/same_column_name_check.groovy
+++ regression-test/suites/mtmv_p0/same_column_name_check/same_column_name_check.groovy
@@ -1,4 +1,3 @@
-package same_column_name_check
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
