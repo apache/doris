956178e05db2df0c4213a72f27b1789abfe2100e
Toms1999 <mj_tangsi@163.com>
2024-09-19 13:07:51 +0800

    [nereids](enhance) Added order by and limit expr for show routine load

--- regression-test/suites/load_p0/routine_load/test_show_routine_load_expr.groovy
+++ regression-test/suites/load_p0/routine_load/test_show_routine_load_expr.groovy
@@ -16,80 +16,87 @@
 // under the License.
 
 suite("test_show_routine_load_expr", "p0") {
-    def tableName = "test_show_routine_load"
 
-    sql """ DROP TABLE IF EXISTS ${tableName} """
-    sql """
-        CREATE TABLE IF NOT EXISTS ${tableName} (
-            `k1` int(20) NULL,
-            `k2` string NULL,
-            `v1` date  NULL,
-            `v2` string  NULL,
-            `v3` datetime  NULL,
-            `v4` string  NULL
-        ) ENGINE=OLAP
-        DUPLICATE KEY(`k1`)
-        COMMENT 'OLAP'
-        DISTRIBUTED BY HASH(`k1`) BUCKETS 3
-        PROPERTIES ("replication_allocation" = "tag.location.default: 1");
-    """
+    String enabled = context.config.otherConfigs.get("enableKafkaTest")
+    if (enabled != null && enabled.equalsIgnoreCase("true")) {
+        def tableName = "test_show_routine_load"
 
-    sql """
-        CREATE ROUTINE LOAD testshow001 ON ${tableName}
-        COLUMNS TERMINATED BY ","
-        PROPERTIES
-        (
-            "max_batch_interval" = "5",
-            "max_batch_rows" = "300000",
-            "max_batch_size" = "209715200"
-        )
-        FROM KAFKA
-        (
-            "kafka_broker_list" = "test",
-            "kafka_topic" = "test",
-            "property.kafka_default_offsets" = "OFFSET_BEGINNING"
-        );
-    """
+        sql """ DROP TABLE IF EXISTS ${tableName} """
+        sql """
+            CREATE TABLE IF NOT EXISTS ${tableName} (
+                `k1` int(20) NULL,
+                `k2` string NULL,
+                `v1` date  NULL,
+                `v2` string  NULL,
+                `v3` datetime  NULL,
+                `v4` string  NULL
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`k1`)
+            COMMENT 'OLAP'
+            DISTRIBUTED BY HASH(`k1`) BUCKETS 3
+            PROPERTIES ("replication_allocation" = "tag.location.default: 1");
+        """
 
-    sql """
-        CREATE ROUTINE LOAD testshow002 ON ${tableName}
-        COLUMNS TERMINATED BY ","
-        PROPERTIES
-        (
-            "max_batch_interval" = "5",
-            "max_batch_rows" = "300000",
-            "max_batch_size" = "209715200"
-        )
-        FROM KAFKA
-        (
-            "kafka_broker_list" = "test",
-            "kafka_topic" = "test",
-            "property.kafka_default_offsets" = "OFFSET_BEGINNING"
-        );
-    """
+        try {
 
-    // Order by name descending
-    def res = sql """SHOW ROUTINE LOAD ORDER BY Name DESC"""
-    log.info("SHOW ROUTINE LOAD result: ${res[0][1]}")
-    // Expect the first job to be testshow002
-    assertTrue(res[0][1] == "testshow002")
+            sql """
+            CREATE ROUTINE LOAD testshow001 ON ${tableName}
+            COLUMNS TERMINATED BY ","
+            PROPERTIES
+            (
+                "max_batch_interval" = "5",
+                "max_batch_rows" = "300000",
+                "max_batch_size" = "209715200"
+            )
+            FROM KAFKA
+            (
+                "kafka_broker_list" = "test",
+                "kafka_topic" = "test",
+                "property.kafka_default_offsets" = "OFFSET_BEGINNING"
+            );
+            """
 
-    // Order by name descending
-    res = sql """SHOW ROUTINE LOAD ORDER BY Name"""
-    log.info("SHOW ROUTINE LOAD result: ${res[0][1]}")
-    // Expect the first job to be testshow001
-    assertTrue(res[0][1] == "testshow001")
+            sql """
+                CREATE ROUTINE LOAD testshow002 ON ${tableName}
+                COLUMNS TERMINATED BY ","
+                PROPERTIES
+                (
+                    "max_batch_interval" = "5",
+                    "max_batch_rows" = "300000",
+                    "max_batch_size" = "209715200"
+                )
+                FROM KAFKA
+                (
+                    "kafka_broker_list" = "test",
+                    "kafka_topic" = "test",
+                    "property.kafka_default_offsets" = "OFFSET_BEGINNING"
+                );
+            """
 
-    // Limit test to get only the first routine load
-    res = sql """SHOW ROUTINE LOAD LIMIT 1"""
-    log.info("SHOW ROUTINE LOAD LIMIT 1 result: ${res}")
-    assertTrue(res.size() == 1)  // Ensure only one result is returned
+            // Order by name descending
+            def res = sql """SHOW ROUTINE LOAD ORDER BY Name DESC"""
+            log.info("SHOW ROUTINE LOAD result: ${res[0][1]}")
+            // Expect the first job to be testshow002
+            assertTrue(res[0][1] == "testshow002")
 
-    res = sql """SHOW ROUTINE LOAD LIMIT 1 , 1"""
-    log.info("SHOW ROUTINE LOAD LIMIT 1 result: ${res}")
-    assertTrue(res.size() == 1)  // Ensure only one result is returned
+            // Order by name descending
+            res = sql """SHOW ROUTINE LOAD ORDER BY Name"""
+            log.info("SHOW ROUTINE LOAD result: ${res[0][1]}")
+            // Expect the first job to be testshow001
+            assertTrue(res[0][1] == "testshow001")
 
-    sql """ DROP TABLE IF EXISTS ${tableName} """
+            // Limit test to get only the first routine load
+            res = sql """SHOW ROUTINE LOAD LIMIT 1"""
+            log.info("SHOW ROUTINE LOAD LIMIT 1 result: ${res}")
+            assertTrue(res.size() == 1)  // Ensure only one result is returned
 
-
+            res = sql """SHOW ROUTINE LOAD LIMIT 1 , 1"""
+            log.info("SHOW ROUTINE LOAD LIMIT 1 result: ${res}")
+            assertTrue(res.size() == 1)  // Ensure only one result is returned
+            sql """ DROP TABLE IF EXISTS ${tableName} """
+        } finally {
+            sql "stop routine load for testshow001"
+            sql "stop routine load for testshow002"
+        }
+    }
 }
