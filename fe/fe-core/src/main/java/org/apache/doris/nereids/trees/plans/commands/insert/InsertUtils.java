80226fe3ad9f5d86c05d839f6ad68095e57d3ef0
bobhan1 <bh2444151092@outlook.com>
2024-09-19 10:39:34 +0800

    [opt](partial update) Forbid partial update on merge-on-write table with sync mv (#40190)
    doc: https://github.com/apache/doris-website/pull/1103

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertUtils.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertUtils.java
@@ -289,6 +289,12 @@ public class InsertUtils {
                         if (unboundLogicalSink.getColNames().isEmpty()) {
                             ((UnboundTableSink<? extends Plan>) unboundLogicalSink).setPartialUpdate(false);
                         } else {
+                            boolean hasSyncMaterializedView = olapTable.getFullSchema().stream()
+                                    .anyMatch(col -> col.isMaterializedViewColumn());
+                            if (hasSyncMaterializedView) {
+                                throw new AnalysisException("Can't do partial update on merge-on-write Unique table"
+                                        + " with sync materialized view.");
+                            }
                             boolean hasMissingColExceptAutoIncKey = false;
                             for (Column col : olapTable.getFullSchema()) {
                                 Optional<String> insertCol = unboundLogicalSink.getColNames().stream()
