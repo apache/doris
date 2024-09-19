251d876261eabf3d19918a4986d671615e6707b7
huanghaibin <284824253@qq.com>
2024-09-19 21:07:15 +0800

    [fix](cloud-mow) Add retry when calculating delete bitmap timeout when loading data (#40562)
    Add retry when calculating delete bitmap timeout on broker load , like
    stream load doing.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
@@ -24,7 +24,6 @@ import org.apache.doris.common.Config;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.FeConstants;
-import org.apache.doris.common.InternalErrorCode;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.util.DebugUtil;
 import org.apache.doris.nereids.NereidsPlanner;
@@ -193,25 +192,7 @@ public abstract class AbstractInsertExecutor {
             executor.updateProfile(false);
             execImpl(executor, jobId);
             checkStrictModeAndFilterRatio();
-            int retryTimes = 0;
-            while (true) {
-                try {
-                    onComplete();
-                    break;
-                } catch (UserException e) {
-                    LOG.warn("failed to commit txn, txnId={}, jobId={}, retryTimes={}",
-                            getTxnId(), jobId, retryTimes, e);
-                    if (e.getErrorCode() == InternalErrorCode.DELETE_BITMAP_LOCK_ERR) {
-                        retryTimes++;
-                        if (retryTimes >= Config.mow_insert_into_commit_retry_times) {
-                            // should throw exception after running out of retry times
-                            throw e;
-                        }
-                    } else {
-                        throw e;
-                    }
-                }
-            }
+            onComplete();
         } catch (Throwable t) {
             onFail(t);
             // retry insert into from select when meet E-230 in cloud
