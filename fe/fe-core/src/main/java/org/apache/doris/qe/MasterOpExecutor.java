dcb2067efb477d04629f5bde19638579012ba182
deardeng <565620795@qq.com>
2024-09-25 10:49:45 +0800

    [fix](case) Fix some non cloud regression case `failed to get cloud compute group`

--- fe/fe-core/src/main/java/org/apache/doris/qe/MasterOpExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/MasterOpExecutor.java
@@ -22,6 +22,7 @@ import org.apache.doris.analysis.RedirectStatus;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ClientPool;
+import org.apache.doris.common.Config;
 import org.apache.doris.common.DdlException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.thrift.FrontendService;
@@ -215,8 +216,11 @@ public class MasterOpExecutor {
         try {
             cluster = ctx.getCloudCluster(false);
         } catch (Exception e) {
-            LOG.warn("failed to get cloud compute group", e);
-            throw new AnalysisException("failed to get cloud compute group", e);
+            if (Config.isCloudMode()) {
+                LOG.warn("failed to get cloud compute group", e);
+                throw new AnalysisException("failed to get cloud compute group", e);
+            }
+            // non cloud mode, do nothing
         }
         if (!Strings.isNullOrEmpty(cluster)) {
             params.setCloudCluster(cluster);
