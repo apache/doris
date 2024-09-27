2d4ba01e8f1bc50eab0fb3a53dca06c7c4302cf3
cjj2010 <2449402815@qq.com>
2024-09-27 11:26:55 +0800

    [enhance]change SpinLock to mutex for _tablets_channels_lock

--- be/src/runtime/load_channel.cpp
+++ be/src/runtime/load_channel.cpp
@@ -115,7 +115,7 @@ Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
     int64_t index_id = params.index_id();
     std::shared_ptr<BaseTabletsChannel> channel;
     {
-        (std::lock_guard<std::mutex>(_lock));
+        std::lock_guard<std::mutex> l(_lock);
         auto it = _tablets_channels.find(index_id);
         if (it != _tablets_channels.end()) {
             channel = it->second;
