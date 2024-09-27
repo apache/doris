1135f7c09cf3da7ec0ad7c3c34f6f6b5931c2bc4
Pxl <pxl290@qq.com>
2024-09-27 11:26:07 +0800

    [Improvement](sort) remove radix sort (#41287)
    ## Proposed changes
    remove radix sort
    
    ```sql
    radix sort
    mysql [test]>SELECT COUNT() FROM (   SELECT UserID   FROM hits_1m   ORDER BY FlashMajor   LIMIT 50000000 ) t;
    +----------+
    | count(*) |
    +----------+
    |  1000000 |
    +----------+
    1 row in set (0.30 sec)
    
    pdq sort
    mysql [test]>SELECT COUNT() FROM (   SELECT UserID   FROM hits_1m   ORDER BY FlashMajor   LIMIT 50000000 ) t;
    +----------+
    | count(*) |
    +----------+
    |  1000000 |
    +----------+
    1 row in set (0.24 sec)
    
    ```

--- be/src/vec/columns/column_vector.cpp
+++ be/src/vec/columns/column_vector.cpp
@@ -35,7 +35,6 @@
 #include "vec/common/assert_cast.h"
 #include "vec/common/memcpy_small.h"
 #include "vec/common/nan_utils.h"
-#include "vec/common/radix_sort.h"
 #include "vec/common/sip_hash.h"
 #include "vec/common/unaligned.h"
 #include "vec/core/sort_block.h"
@@ -234,20 +233,6 @@ struct ColumnVector<T>::greater {
     }
 };
 
-namespace {
-template <typename T>
-struct ValueWithIndex {
-    T value;
-    UInt32 index;
-};
-
-template <typename T>
-struct RadixSortTraits : RadixSortNumTraits<T> {
-    using Element = ValueWithIndex<T>;
-    static T& extract_key(Element& elem) { return elem.value; }
-};
-} // namespace
-
 template <typename T>
 void ColumnVector<T>::get_permutation(bool reverse, size_t limit, int nan_direction_hint,
                                       IColumn::Permutation& res) const {
@@ -269,51 +254,6 @@ void ColumnVector<T>::get_permutation(bool reverse, size_t limit, int nan_direct
             std::partial_sort(res.begin(), res.begin() + limit, res.end(),
                               less(*this, nan_direction_hint));
     } else {
-        /// A case for radix sort
-        if constexpr (std::is_arithmetic_v<T> && !std::is_same_v<T, UInt128>) {
-            /// Thresholds on size. Lower threshold is arbitrary. Upper threshold is chosen by the type for histogram counters.
-            if (s >= 256 && s <= std::numeric_limits<UInt32>::max()) {
-                PaddedPODArray<ValueWithIndex<T>> pairs(s);
-                for (UInt32 i = 0; i < s; ++i) pairs[i] = {data[i], i};
-
-                RadixSort<RadixSortTraits<T>>::execute_lsd(pairs.data(), s);
-
-                /// Radix sort treats all NaNs to be greater than all numbers.
-                /// If the user needs the opposite, we must move them accordingly.
-                size_t nans_to_move = 0;
-                if (std::is_floating_point_v<T> && nan_direction_hint < 0) {
-                    for (ssize_t i = s - 1; i >= 0; --i) {
-                        if (is_nan(pairs[i].value))
-                            ++nans_to_move;
-                        else
-                            break;
-                    }
-                }
-
-                if (reverse) {
-                    if (nans_to_move) {
-                        for (size_t i = 0; i < s - nans_to_move; ++i)
-                            res[i] = pairs[s - nans_to_move - 1 - i].index;
-                        for (size_t i = s - nans_to_move; i < s; ++i)
-                            res[i] = pairs[s - 1 - (i - (s - nans_to_move))].index;
-                    } else {
-                        for (size_t i = 0; i < s; ++i) res[s - 1 - i] = pairs[i].index;
-                    }
-                } else {
-                    if (nans_to_move) {
-                        for (size_t i = 0; i < nans_to_move; ++i)
-                            res[i] = pairs[i + s - nans_to_move].index;
-                        for (size_t i = nans_to_move; i < s; ++i)
-                            res[i] = pairs[i - nans_to_move].index;
-                    } else {
-                        for (size_t i = 0; i < s; ++i) res[i] = pairs[i].index;
-                    }
-                }
-
-                return;
-            }
-        }
-
         /// Default sorting algorithm.
         for (size_t i = 0; i < s; ++i) res[i] = i;
 
