1dafd26a2d0822d8943d96f1b9fe1c6650bb7452
cjj2010 <2449402815@qq.com>
2024-09-27 11:16:59 +0800

    [enhance]change SpinLock to mutex for _tablets_channels_lock

--- be/src/runtime/load_channel.cpp
+++ be/src/runtime/load_channel.cpp
@@ -115,7 +115,7 @@ Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
     int64_t index_id = params.index_id();
     std::shared_ptr<BaseTabletsChannel> channel;
     {
-        std::lock_guard<std::mutex> l(_lock);
+        (std::lock_guard<std::mutex>(_lock));
         auto it = _tablets_channels.find(index_id);
         if (it != _tablets_channels.end()) {
             channel = it->second;
@@ -135,7 +135,7 @@ Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
                                                            _is_high_priority, _self_profile);
             }
             {
-                std::lock_guard<SpinLock> l(_tablets_channels_lock);
+                std::lock_guard<std::mutex> l(_tablets_channels_lock);
                 _tablets_channels.insert({index_id, channel});
             }
         }
@@ -237,7 +237,7 @@ Status LoadChannel::_handle_eos(BaseTabletsChannel* channel,
     if (finished) {
         std::lock_guard<std::mutex> l(_lock);
         {
-            std::lock_guard<SpinLock> l(_tablets_channels_lock);
+            std::lock_guard<std::mutex> l(_tablets_channels_lock);
             _tablets_channels_rows.insert(std::make_pair(
                     index_id,
                     std::make_pair(channel->total_received_rows(), channel->num_rows_filtered())));
@@ -263,7 +263,7 @@ void LoadChannel::_report_profile(PTabletWriterAddBlockResult* response) {
     _self_profile->set_timestamp(_last_updated_time);
 
     {
-        std::lock_guard<SpinLock> l(_tablets_channels_lock);
+        std::lock_guard<std::mutex> l(_tablets_channels_lock);
         for (auto& it : _tablets_channels) {
             it.second->refresh_profile();
         }
