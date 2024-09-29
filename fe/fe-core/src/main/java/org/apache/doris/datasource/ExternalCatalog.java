b665020851e93f5a8dd41bd762aa0c4c39acdd06
zy-kkk <zhongyk10@gmail.com>
2024-09-29 16:23:30 +0800

    [improvement](jdbc catalog) Optimize JdbcCatalog case mapping stability (#40891)
    This PR makes the following changes to the uppercase and lowercase
    mapping of JdbcCatalog
    1. The identifierMapping is managed by JdbcExternalCatalog instead of
    JdbcClient to better control its lifecycle
    2. The identifierMapping no longer loads remoteName alone, but Catalog
    controls the loading uniformly
    3. The identifierMapping will be loaded when each FE performs
    makeSureInitialized() to ensure that each FE has a mapping
    4. The initialization of mapping will only be performed once in
    makeSureInitialized(), which means that even if you use metaCache, if
    your source data is updated when identifierMapping is enabled, you must
    refresh the catalog to query normally.
    5. The identifierMapping is only responsible for the properties of the
    Catalog and is no longer affected by the fe config, simplifying the
    processing logic
    6. If lower_case_mete_names is false and meta_names_mapping is empty in
    the catalog properties, the identifierMapping will no longer take
    effect, further enhancing the stability of the default settings
    7. The JdbcClient is no longer closed during onRefreshCache, reducing
    the repeated creation of resources, improving reuse, and reducing the
    leakage of some global shared threads

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
@@ -46,6 +46,7 @@ import org.apache.doris.datasource.infoschema.ExternalInfoSchemaDatabase;
 import org.apache.doris.datasource.infoschema.ExternalMysqlDatabase;
 import org.apache.doris.datasource.jdbc.JdbcExternalDatabase;
 import org.apache.doris.datasource.lakesoul.LakeSoulExternalDatabase;
+import org.apache.doris.datasource.mapping.IdentifierMapping;
 import org.apache.doris.datasource.maxcompute.MaxComputeExternalDatabase;
 import org.apache.doris.datasource.metacache.MetaCache;
 import org.apache.doris.datasource.operations.ExternalMetadataOps;
@@ -144,6 +145,9 @@ public abstract class ExternalCatalog
     protected Optional<Boolean> useMetaCache = Optional.empty();
     protected MetaCache<ExternalDatabase<? extends ExternalTable>> metaCache;
 
+    protected IdentifierMapping identifierMapping;
+    private boolean mappingsInitialized = false;
+
     public ExternalCatalog() {
     }
 
@@ -176,6 +180,10 @@ public abstract class ExternalCatalog
         }
     }
 
+    // only for forward to master
+    protected void buildDatabaseMapping() {
+    }
+
     // Will be called when creating catalog(so when as replaying)
     // to add some default properties if missing.
     public void setDefaultPropsIfMissing(boolean isReplay) {
@@ -203,6 +211,10 @@ public abstract class ExternalCatalog
      * @return names of tables in specified database
      */
     public abstract List<String> listTableNames(SessionContext ctx, String dbName);
+
+    // only for forward to master
+    protected void buildTableMapping(SessionContext ctx, String dbName) {
+    }
 
     /**
      * check if the specified table exist.
@@ -267,6 +279,10 @@ public abstract class ExternalCatalog
                 init();
             }
             initialized = true;
+        }
+        if (!mappingsInitialized) {
+            buildDatabaseMapping();
+            mappingsInitialized = true;
         }
     }
 
@@ -393,6 +409,7 @@ public abstract class ExternalCatalog
     public void onRefresh(boolean invalidCache) {
         this.objectCreated = false;
         this.initialized = false;
+        this.mappingsInitialized = false;
         synchronized (this.propLock) {
             this.convertedProperties = null;
         }
@@ -722,6 +739,7 @@ public abstract class ExternalCatalog
         }
         this.propLock = new byte[0];
         this.initialized = false;
+        this.mappingsInitialized = false;
         setDefaultPropsIfMissing(true);
     }
 
