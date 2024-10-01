49a3014f33f3a2f2666c452d43bfc95577726a5f
minghong <englefly@gmail.com>
2024-10-01 23:49:55 +0800

    [regressioncase](nereids)tpcds 10t plan shape for external table (#41340)
    ## Proposed changes
    tracking tpcds 10t shape for exteranl table
    simulating exteranl table by olaptable, 
    1. inject row count
    2. tables are distributed by random
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->

---
+++ regression-test/suites/nereids_tpcds_shape_sf10t_orc/shape/query61.groovy
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+suite("query61") {
+    String db = context.config.getDbNameByFile(new File(context.file.parent))
+    if (isCloudMode()) {
+        return
+    }
+    sql """
+         use ${db};
+         set enable_nereids_planner=true;
+         set enable_nereids_distribute_planner=false;
+         set enable_fallback_to_original_planner=false;
+         set exec_mem_limit=21G;
+         set be_number_for_test=3;
+         set parallel_fragment_exec_instance_num=8; ;
+         set parallel_pipeline_task_num=8;
+         set forbid_unknown_col_stats=true;
+         set enable_nereids_timeout = false;
+         set enable_runtime_filter_prune=false;
+         set runtime_filter_type=8;
+         set dump_nereids_memo=false;
+         set disable_nereids_rules='PRUNE_EMPTY_PARTITION';
+         set enable_fold_constant_by_be = false;
+         set push_topn_to_agg = true;
+         set TOPN_OPT_LIMIT_THRESHOLD = 1024;
+         set enable_parallel_result_sink=true;
+         """
+    qt_ds_shape_61 '''
+    explain shape plan
+    select  promotions,total,cast(promotions as decimal(15,4))/cast(total as decimal(15,4))*100
+from
+  (select sum(ss_ext_sales_price) promotions
+   from  store_sales
+        ,store
+        ,promotion
+        ,date_dim
+        ,customer
+        ,customer_address 
+        ,item
+   where ss_sold_date_sk = d_date_sk
+   and   ss_store_sk = s_store_sk
+   and   ss_promo_sk = p_promo_sk
+   and   ss_customer_sk= c_customer_sk
+   and   ca_address_sk = c_current_addr_sk
+   and   ss_item_sk = i_item_sk 
+   and   ca_gmt_offset = -6
+   and   i_category = 'Sports'
+   and   (p_channel_dmail = 'Y' or p_channel_email = 'Y' or p_channel_tv = 'Y')
+   and   s_gmt_offset = -6
+   and   d_year = 2001
+   and   d_moy  = 12) promotional_sales,
+  (select sum(ss_ext_sales_price) total
+   from  store_sales
+        ,store
+        ,date_dim
+        ,customer
+        ,customer_address
+        ,item
+   where ss_sold_date_sk = d_date_sk
+   and   ss_store_sk = s_store_sk
+   and   ss_customer_sk= c_customer_sk
+   and   ca_address_sk = c_current_addr_sk
+   and   ss_item_sk = i_item_sk
+   and   ca_gmt_offset = -6
+   and   i_category = 'Sports'
+   and   s_gmt_offset = -6
+   and   d_year = 2001
+   and   d_moy  = 12) all_sales
+order by promotions, total
+limit 100
+    '''
+}
