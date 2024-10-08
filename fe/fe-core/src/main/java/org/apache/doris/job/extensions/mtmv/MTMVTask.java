3a4b0b27805740373ad0e49018c2d30e4fbde83e
zhangdong <493738387@qq.com>
2024-10-08 10:26:16 +0800

    mvName

--- fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
@@ -250,9 +250,9 @@ public class MTMVTask extends AbstractTask {
 
     private String getDummyStmt(Set<String> refreshPartitionNames) {
         return String.format(
-                "Asynchronous materialized view refresh task, "
+                "Asynchronous materialized view refresh task, mvName: %s,"
                         + "taskId: %s, partitions refreshed by this insert overwrite: %s",
-                super.getTaskId(), refreshPartitionNames);
+                mtmv.getName(), super.getTaskId(), refreshPartitionNames);
     }
 
     @Override
