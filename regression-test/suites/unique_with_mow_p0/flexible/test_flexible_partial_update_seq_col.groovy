91348da1b13372bdbfd251c81251961a6fbd1044
bobhan1 <bh2444151092@outlook.com>
2024-10-08 13:47:46 +0800

    add case

--- regression-test/suites/unique_with_mow_p0/flexible/test_flexible_partial_update_seq_col.groovy
+++ regression-test/suites/unique_with_mow_p0/flexible/test_flexible_partial_update_seq_col.groovy
@@ -20,7 +20,7 @@ suite('test_flexible_partial_update_seq_col') {
     for (def use_row_store : [false, true]) {
         logger.info("current params: use_row_store: ${use_row_store}")
 
-        // 1. sequence map col(without default value)
+        // 1.1. sequence map col(without default value)
         def tableName = "test_flexible_partial_update_seq_map_col1_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -59,7 +59,7 @@ suite('test_flexible_partial_update_seq_col') {
         order_qt_seq_map_no_default_val2 "select k,v1,v2,v3,v4,v5,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
 
 
-        // 2. sequence map col(with default value)
+        // 1.2. sequence map col(with default value)
         tableName = "test_flexible_partial_update_seq_map_col2_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -96,7 +96,7 @@ suite('test_flexible_partial_update_seq_col') {
         order_qt_seq_map_has_default_val2 "select k,v1,v2,v3,v4,v5,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
 
 
-        // 3. sequence type col
+        // 1.3. sequence type col
         tableName = "test_flexible_partial_update_seq_type_col1_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -139,7 +139,7 @@ suite('test_flexible_partial_update_seq_col') {
         // the below cases will have many rows with same keys in one load. Among rows with the same keys, some of them specify sequence col(sequence map col),
         // some of them don't. Those
 
-        // 4. sequence type col
+        // 2.1. sequence type col
         tableName = "test_flexible_partial_update_seq_type_col2_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -182,7 +182,7 @@ suite('test_flexible_partial_update_seq_col') {
         }
         order_qt_seq_type_col_multi_rows_3 "select k,v1,v2,v3,v4,v5,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
 
-        // 5. sequence map col(without default value)
+        // 2.2. sequence map col(without default value)
         tableName = "test_flexible_partial_update_seq_map_col3_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -203,7 +203,7 @@ suite('test_flexible_partial_update_seq_col') {
         "store_row_column" = "${use_row_store}"); """
         sql """insert into ${tableName}(k,v1,v2,v3,v4,v5,v6) select number, number, number, number, number, number, number * 10 from numbers("number" = "6"); """
         order_qt_seq_map_col_no_default_val_multi_rows_1 "select k,v1,v2,v3,v4,v5,v6,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
-        // 5.1 rows with same keys are neighbers
+        // 2.2.1 rows with same keys are neighbers
         streamLoad {
             table "${tableName}"
             set 'format', 'json'
@@ -214,7 +214,7 @@ suite('test_flexible_partial_update_seq_col') {
             time 20000 // limit inflight 10s
         }
         order_qt_seq_map_col_no_default_val_multi_rows_2 "select k,v1,v2,v3,v4,v5,v6,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
-        // 5.2 rows with same keys are interleaved
+        // 2.2.2 rows with same keys are interleaved
         streamLoad {
             table "${tableName}"
             set 'format', 'json'
@@ -226,7 +226,7 @@ suite('test_flexible_partial_update_seq_col') {
         }
         order_qt_seq_map_col_no_default_val_multi_rows_3 "select k,v1,v2,v3,v4,v5,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
 
-        // 6. sequence map col(with default value)
+        // 2.3. sequence map col(with default value)
         tableName = "test_flexible_partial_update_seq_map_col4_${use_row_store}"
         sql """ DROP TABLE IF EXISTS ${tableName} force;"""
         sql """ CREATE TABLE ${tableName} (
@@ -248,7 +248,7 @@ suite('test_flexible_partial_update_seq_col') {
         sql """insert into ${tableName}(k,v1,v2,v3,v4,v5,v6) select number, number, number, number, number, number, number * 10 from numbers("number" = "6"); """
         order_qt_seq_map_col_has_default_val_multi_rows_1 "select k,v1,v2,v3,v4,v5,v6,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
 
-        // 6.1 rows with same keys are neighbers
+        // 2.3.1. rows with same keys are neighbers
         // after merge in memtable, newly inserted rows(key=6) will has two rows, one with sequence map col value=30
         // one without sequence map value. Because the default value of sequence map col(`v6`) is 60, larger than 30,
         // so the row with sequence map col will be deleted by the row without sequence map col
@@ -262,7 +262,7 @@ suite('test_flexible_partial_update_seq_col') {
             time 20000 // limit inflight 10s
         }
         order_qt_seq_map_col_has_default_val_multi_rows_2 "select k,v1,v2,v3,v4,v5,v6,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
-        // 6.2 rows with same keys are interleaved
+        // 2.3.2. rows with same keys are interleaved
         // after merge in memtable, newly inserted rows(key=7) will has two rows, one with sequence map col value=70
         // one without sequence map value. Because the default value of sequence map col(`v6`) is 60, smaller than 70,
         // so the row without sequence map col will be deleted by the row with sequence map col
@@ -276,5 +276,76 @@ suite('test_flexible_partial_update_seq_col') {
             time 20000 // limit inflight 10s
         }
         order_qt_seq_map_col_has_default_val_multi_rows_3 "select k,v1,v2,v3,v4,v5,__DORIS_SEQUENCE_COL__,BITMAP_TO_STRING(__DORIS_SKIP_BITMAP_COL__) from ${tableName};"
+
+        // ==============================================================================================================================
+        // other cases
+
+        // 3.1 sequence map with no default value
+        tableName = "test_flexible_partial_update_seq_map_col5_${use_row_store}"
+        sql """ DROP TABLE IF EXISTS ${tableName} force;"""
+        sql """ CREATE TABLE ${tableName} (
+            `k` BIGINT NOT NULL,
+            `c1` int,
+            `c2` int,
+            `c3` int
+            ) UNIQUE KEY(`k`)
+            DISTRIBUTED BY HASH(`k`) BUCKETS 1
+            PROPERTIES (
+            "replication_num" = "1",
+            "enable_unique_key_merge_on_write" = "true",
+            "enable_unique_key_skip_bitmap_column" = "true",
+            "function_column.sequence_col" = "c1");
+            """
+        sql "insert into ${tableName} values(1,256,1,1),(2,255,2,2),(3,3,3,3),(4,4,4,4);"
+        qt_seq1 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k;"
+        streamLoad {
+            table "${tableName}"
+            set 'format', 'json'
+            set 'read_json_by_line', 'true'
+            set 'strict_mode', 'false'
+            set 'unique_key_update_mode', 'UPDATE_FLEXIBLE_COLUMNS'
+            file "seq1.json"
+            time 20000 // limit inflight 10s
+        }
+        qt_seq1 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k,c1,c2,c3,__DORIS_SEQUENCE_COL__;"
+        sql "set skip_delete_bitmap=true;"
+        sql "sync;"
+        qt_seq1 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k,c1,c2,c3,__DORIS_SEQUENCE_COL__;"
+        sql "set skip_delete_bitmap=false;"
+        sql "sync;"
+
+        // 3.2 sequence map with default value
+        tableName = "test_flexible_partial_update_seq_map_col6_${use_row_store}"
+        sql """ DROP TABLE IF EXISTS ${tableName} force;"""
+        sql """ CREATE TABLE ${tableName} (
+            `k` BIGINT NOT NULL,
+            `c1` int default "256",
+            `c2` int,
+            `c3` int
+            ) UNIQUE KEY(`k`)
+            DISTRIBUTED BY HASH(`k`) BUCKETS 1
+            PROPERTIES (
+            "replication_num" = "1",
+            "enable_unique_key_merge_on_write" = "true",
+            "enable_unique_key_skip_bitmap_column" = "true",
+            "function_column.sequence_col" = "c1");
+            """
+        sql "insert into ${tableName} values(1,256,1,1),(2,255,2,2),(3,3,3,3),(4,4,4,4);"
+        qt_seq2 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k;"
+        streamLoad {
+            table "${tableName}"
+            set 'format', 'json'
+            set 'read_json_by_line', 'true'
+            set 'strict_mode', 'false'
+            set 'unique_key_update_mode', 'UPDATE_FLEXIBLE_COLUMNS'
+            file "seq1.json"
+            time 20000 // limit inflight 10s
+        }
+        qt_seq2 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k,c1,c2,c3,__DORIS_SEQUENCE_COL__;"
+        sql "set skip_delete_bitmap=true;"
+        sql "sync;"
+        qt_seq2 "select k,c1,c2,c3,__DORIS_SEQUENCE_COL__ from ${tableName} order by k,c1,c2,c3,__DORIS_SEQUENCE_COL__;"
+        sql "set skip_delete_bitmap=false;"
+        sql "sync;"
     }
 }
