9a72ceb4ea872a61877b169f929ae9adf4d149f2
Pxl <pxl290@qq.com>
2024-10-10 14:52:19 +0800

    [Feature](compatibility) make some agg function restrict (#41459)
    ## Proposed changes
    make some agg function restrict

--- be/src/vec/aggregate_functions/aggregate_function_stddev.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_stddev.cpp
@@ -51,29 +51,11 @@ AggregateFunctionPtr create_function_single_value(const String& name,
     return nullptr;
 }
 
-template <bool is_stddev, bool is_nullable>
-AggregateFunctionPtr create_aggregate_function_variance_samp_older(const std::string& name,
-                                                                   const DataTypes& argument_types,
-                                                                   const bool result_is_nullable) {
-    return create_function_single_value<AggregateFunctionSamp_OLDER, VarianceSampName,
-                                        SampData_OLDER, is_stddev, is_nullable>(
-            name, argument_types, result_is_nullable, true);
-}
-
 AggregateFunctionPtr create_aggregate_function_variance_samp(const std::string& name,
                                                              const DataTypes& argument_types,
                                                              const bool result_is_nullable) {
     return create_function_single_value<AggregateFunctionSamp, VarianceSampName, SampData, false>(
             name, argument_types, result_is_nullable, false);
-}
-
-template <bool is_stddev, bool is_nullable>
-AggregateFunctionPtr create_aggregate_function_stddev_samp_older(const std::string& name,
-                                                                 const DataTypes& argument_types,
-                                                                 const bool result_is_nullable) {
-    return create_function_single_value<AggregateFunctionSamp_OLDER, StddevSampName, SampData_OLDER,
-                                        is_stddev, is_nullable>(name, argument_types,
-                                                                result_is_nullable, true);
 }
 
 template <bool is_stddev>
@@ -108,18 +90,8 @@ void register_aggregate_function_stddev_variance_pop(AggregateFunctionSimpleFact
 }
 
 void register_aggregate_function_stddev_variance_samp_old(AggregateFunctionSimpleFactory& factory) {
-    factory.register_alternative_function(
-            "variance_samp", create_aggregate_function_variance_samp_older<false, false>, false,
-            AGG_FUNCTION_NULLABLE);
-    factory.register_alternative_function(
-            "variance_samp", create_aggregate_function_variance_samp_older<false, true>, true,
-            AGG_FUNCTION_NULLABLE);
-    factory.register_alternative_function("stddev_samp",
-                                          create_aggregate_function_stddev_samp_older<true, false>,
-                                          false, AGG_FUNCTION_NULLABLE);
-    factory.register_alternative_function("stddev_samp",
-                                          create_aggregate_function_stddev_samp_older<true, true>,
-                                          true, AGG_FUNCTION_NULLABLE);
+    BeExecVersionManager::registe_restrict_function_compatibility("variance_samp");
+    BeExecVersionManager::registe_restrict_function_compatibility("stddev_samp");
 }
 
 void register_aggregate_function_stddev_variance_samp(AggregateFunctionSimpleFactory& factory) {
--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -1892,7 +1892,7 @@ public class Config extends ConfigBase {
      * Max data version of backends serialize block.
      */
     @ConfField(mutable = false)
-    public static int max_be_exec_version = 7;
+    public static int max_be_exec_version = 8;
 
     /**
      * Min data version of backends serialize block.
