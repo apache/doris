apache/doris

commit 280adb544a6fac8e845a5e0170563f0c55597e82

[Chore](runtime-filter) change bloom filter DCHECK to error status (#41648)
## Proposed changes
change bloom filter DCHECK to error status

be/src/exprs/bloom_filter_func.h
@@ -151,14 +151,20 @@ public:
     }
 
     Status merge(BloomFilterFuncBase* bloomfilter_func) {
-        DCHECK(bloomfilter_func != nullptr);
-        DCHECK(bloomfilter_func->_bloom_filter != nullptr);
+        if (bloomfilter_func == nullptr) {
+            return Status::InternalError("bloomfilter_func is nullptr");
+        }
+        if (bloomfilter_func->_bloom_filter == nullptr) {
+            return Status::InternalError("bloomfilter_func->_bloom_filter is nullptr");
+        }
         // If `_inited` is false, there is no memory allocated in bloom filter and this is the first
         // call for `merge` function. So we just reuse this bloom filter, and we don't need to
         // allocate memory again.
         if (!_inited) {
             auto* other_func = static_cast<BloomFilterFuncBase*>(bloomfilter_func);
-            DCHECK(_bloom_filter == nullptr);
+            if (_bloom_filter == nullptr) {
+                return Status::InternalError("_bloom_filter is nullptr");
+            }
             _bloom_filter = bloomfilter_func->_bloom_filter;
             _bloom_filter_alloced = other_func->_bloom_filter_alloced;
             _inited = true;

commit 6c3af583648939b5c358f690fbeb51cce7210d3e

[Bug](runtime-filter) fix wrong check on BloomFilterFuncBase::merge (#41670)
## Proposed changes
fix wrong check on BloomFilterFuncBase::merge
introduced by https://github.com/apache/doris/pull/41648

be/src/exprs/bloom_filter_func.h
@@ -162,8 +162,8 @@ public:
         // allocate memory again.
         if (!_inited) {
             auto* other_func = static_cast<BloomFilterFuncBase*>(bloomfilter_func);
-            if (_bloom_filter == nullptr) {
-                return Status::InternalError("_bloom_filter is nullptr");
+            if (_bloom_filter != nullptr) {
+                return Status::InternalError("_bloom_filter must is nullptr");
             }
             _bloom_filter = bloomfilter_func->_bloom_filter;
             _bloom_filter_alloced = other_func->_bloom_filter_alloced;