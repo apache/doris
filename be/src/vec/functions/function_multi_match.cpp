a8bca8f9e6a808c14d90886623ad3aa203bd060b
zzzxl1993 <474696115@qq.com>
2024-10-10 17:34:56 +0800

    [fix](inverted index) multi_match supports any, all, phrase.

--- be/src/vec/functions/function_multi_match.cpp
+++ be/src/vec/functions/function_multi_match.cpp
@@ -40,10 +40,17 @@ Status FunctionMultiMatch::execute_impl(FunctionContext* /*context*/, Block& blo
 }
 
 InvertedIndexQueryType get_query_type(const std::string& query_type) {
-    if (query_type == "phrase_prefix") {
+    if (query_type == "any") {
+        return InvertedIndexQueryType::MATCH_ANY_QUERY;
+    } else if (query_type == "all") {
+        return InvertedIndexQueryType::MATCH_ALL_QUERY;
+    } else if (query_type == "phrase") {
+        return InvertedIndexQueryType::MATCH_PHRASE_QUERY;
+    } else if (query_type == "phrase_prefix") {
         return InvertedIndexQueryType::MATCH_PHRASE_PREFIX_QUERY;
+    } else {
+        return InvertedIndexQueryType::UNKNOWN_QUERY;
     }
-    return InvertedIndexQueryType::UNKNOWN_QUERY;
 }
 
 Status FunctionMultiMatch::evaluate_inverted_index(
@@ -62,7 +69,6 @@ Status FunctionMultiMatch::evaluate_inverted_index(
                 "parameter query type incorrect for function multi_match: query_type = {}",
                 query_type);
     }
-
     // query
     auto query_str = arguments[1].column->get_data_at(0);
     auto param_type = arguments[1].type->get_type_as_type_descriptor().type;
