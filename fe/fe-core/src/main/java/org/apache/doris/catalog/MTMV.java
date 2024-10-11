793ed8c74d539bf474bdce3c28c66c4061dd9b34
seawinde <149132972+seawinde@users.noreply.github.com>
2024-10-11 20:52:05 +0800

    [fix](mtmv) Fix written by mv successfully but not get mv lock. If use mv, data maybe wrong (#40173)
    When refresh partition mv, meanwhile, the query that can be written
    successfully by the same mv, if we run the query, the result maybe
    wrong.
    this pr fix this.

--- fe/fe-core/src/main/java/org/apache/doris/catalog/MTMV.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/MTMV.java
@@ -111,6 +111,11 @@ public class MTMV extends OlapTable {
         mvRwLock = new ReentrantReadWriteLock(true);
     }
 
+    @Override
+    public boolean needReadLockWhenPlan() {
+        return true;
+    }
+
     public MTMVRefreshInfo getRefreshInfo() {
         readMvLock();
         try {
