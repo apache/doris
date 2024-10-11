43bb8b13917e0aa88fd0f9f4fc0dfb67ac66dfee
Gabriel <gabrielleebuaa@gmail.com>
2024-10-11 14:05:33 +0800

    [Improvement](shuffle) Reduce memory consumption in data stream sender (#41676)

--- be/src/vec/sink/vdata_stream_sender.h
+++ be/src/vec/sink/vdata_stream_sender.h
@@ -307,7 +307,7 @@ public:
 
     Status send_local_block(Status exec_status, bool eos = false);
 
-    Status send_local_block(Block* block);
+    Status send_local_block(Block* block, bool can_be_moved);
     // Flush buffered rows and close channel. This function don't wait the response
     // of close operation, client should call close_wait() to finish channel's close.
     // We split one close operation into two phases in order to make multiple channels
