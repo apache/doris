7fa931e57a1d0adf80bdfdb47128917a206a3d4e
Yulei-Yang <yulei.yang0699@gmail.com>
2024-10-11 16:22:49 +0800

    fix SELECT * from INFORMATION_SCHEMA.TABLES

--- fe/fe-core/src/main/java/org/apache/doris/catalog/TableIf.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/TableIf.java
@@ -405,6 +405,7 @@ public interface TableIf {
                 case ODBC:
                     return "Odbc";
                 case OLAP:
+                case TEMP:
                     return "Doris";
                 case SCHEMA:
                     return "SYSTEM VIEW";
@@ -457,6 +458,7 @@ public interface TableIf {
                 case VIEW:
                     return "VIEW";
                 case OLAP:
+                case TEMP:
                 case MYSQL:
                 case ODBC:
                 case BROKER:
