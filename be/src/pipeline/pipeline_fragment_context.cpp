121537d5fe85a2f1a4c73a004bbbef569869d7f9
Gabriel <gabrielleebuaa@gmail.com>
2024-10-11 19:13:47 +0800

    [fix](followup) Stop tasks if waken by downstream tasks (#41628)
    Follow-up : #41292

--- be/src/pipeline/pipeline_fragment_context.cpp
+++ be/src/pipeline/pipeline_fragment_context.cpp
@@ -1154,8 +1154,7 @@ Status PipelineFragmentContext::_create_data_sink(ObjectPool* pool, const TDataS
         }
 
         _sink.reset(new MultiCastDataStreamSinkOperatorX(
-                sink_id, sources, thrift_sink.multi_cast_stream_sink.sinks.size(), pool,
-                thrift_sink.multi_cast_stream_sink, row_desc));
+                sink_id, sources, pool, thrift_sink.multi_cast_stream_sink, row_desc));
         for (int i = 0; i < sender_size; ++i) {
             auto new_pipeline = add_pipeline();
             RowDescriptor* _row_desc = nullptr;
