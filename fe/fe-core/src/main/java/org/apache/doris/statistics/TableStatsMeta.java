b84e128974059ca963833f4aceee35eaaf0050e3
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2024-10-14 21:14:05 +0800

    [fix](statistics)Fix clear stale mv row count bug. (#41653) (#41807)
    backport: https://github.com/apache/doris/pull/41653

--- fe/fe-core/src/main/java/org/apache/doris/statistics/TableStatsMeta.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/TableStatsMeta.java
@@ -150,10 +150,6 @@ public class TableStatsMeta implements Writable, GsonPostProcessable {
         colToColStatsMeta.remove(Pair.of(indexName, colName));
     }
 
-    public void removeAllColumn() {
-        colToColStatsMeta.clear();
-    }
-
     public Set<Pair<String, String>> analyzeColumns() {
         return colToColStatsMeta.keySet();
     }
@@ -218,18 +214,19 @@ public class TableStatsMeta implements Writable, GsonPostProcessable {
         return indexesRowCount.getOrDefault(indexId, -1L);
     }
 
-    public void clearIndexesRowCount() {
-        indexesRowCount.clear();
-    }
-
-    private void clearStaleIndexRowCount(OlapTable table) {
+    protected void clearStaleIndexRowCount(OlapTable table) {
         Iterator<Long> iterator = indexesRowCount.keySet().iterator();
-        List<Long> indexIds = table.getIndexIds();
+        List<Long> indexIds = table.getIndexIdList();
         while (iterator.hasNext()) {
             long key = iterator.next();
-            if (indexIds.contains(key)) {
+            if (!indexIds.contains(key)) {
                 iterator.remove();
             }
         }
+    }
+
+    // For unit test only.
+    protected void addIndexRowForTest(long indexId, long rowCount) {
+        indexesRowCount.put(indexId, rowCount);
     }
 }
