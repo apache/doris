19be696120ab1e16df1120717251fadb6a64f030
Jibing-Li <64681310+Jibing-Li@users.noreply.github.com>
2024-10-14 22:09:57 +0800

    [Refactor](statistics)Refactor of statistics buildConnectContext. (#41553) (#41814)
    backport: https://github.com/apache/doris/pull/41553

--- fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
@@ -86,18 +86,24 @@ public class AuditLogHelper {
                 .setFuzzyVariables(!printFuzzyVariables ? "" : ctx.getSessionVariable().printFuzzyVariables());
 
         if (ctx.getState().isQuery()) {
-            MetricRepo.COUNTER_QUERY_ALL.increase(1L);
-            MetricRepo.USER_COUNTER_QUERY_ALL.getOrAdd(ctx.getQualifiedUser()).increase(1L);
+            if (!ctx.getSessionVariable().internalSession) {
+                MetricRepo.COUNTER_QUERY_ALL.increase(1L);
+                MetricRepo.USER_COUNTER_QUERY_ALL.getOrAdd(ctx.getQualifiedUser()).increase(1L);
+            }
             if (ctx.getState().getStateType() == MysqlStateType.ERR
                     && ctx.getState().getErrType() != QueryState.ErrType.ANALYSIS_ERR) {
                 // err query
-                MetricRepo.COUNTER_QUERY_ERR.increase(1L);
-                MetricRepo.USER_COUNTER_QUERY_ERR.getOrAdd(ctx.getQualifiedUser()).increase(1L);
+                if (!ctx.getSessionVariable().internalSession) {
+                    MetricRepo.COUNTER_QUERY_ERR.increase(1L);
+                    MetricRepo.USER_COUNTER_QUERY_ERR.getOrAdd(ctx.getQualifiedUser()).increase(1L);
+                }
             } else if (ctx.getState().getStateType() == MysqlStateType.OK
                     || ctx.getState().getStateType() == MysqlStateType.EOF) {
                 // ok query
-                MetricRepo.HISTO_QUERY_LATENCY.update(elapseMs);
-                MetricRepo.USER_HISTO_QUERY_LATENCY.getOrAdd(ctx.getQualifiedUser()).update(elapseMs);
+                if (!ctx.getSessionVariable().internalSession) {
+                    MetricRepo.HISTO_QUERY_LATENCY.update(elapseMs);
+                    MetricRepo.USER_HISTO_QUERY_LATENCY.getOrAdd(ctx.getQualifiedUser()).update(elapseMs);
+                }
 
                 if (elapseMs > Config.qe_slow_log_ms) {
                     String sqlDigest = DigestUtils.md5Hex(((Queriable) parsedStmt).toDigest());
