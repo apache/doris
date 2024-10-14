1007ac2ce3163984fe87a67a39b4ef319e73625e
zhangdong <493738387@qq.com>
2024-10-14 16:34:10 +0800

    1

--- regression-test/suites/auth_p0/test_use_encryptkey_auth.groovy
+++ regression-test/suites/auth_p0/test_use_encryptkey_auth.groovy
@@ -27,6 +27,13 @@ suite("test_use_encryptkey_auth","p0,auth") {
     try_sql("DROP ENCRYPTKEY ${key}")
     sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
     sql """grant select_priv on regression_test to ${user}"""
+    //cloud-mode
+    if (isCloudMode()) {
+        def clusters = sql " SHOW CLUSTERS; "
+        assertTrue(!clusters.isEmpty())
+        def validCluster = clusters[0][0]
+        sql """GRANT USAGE_PRIV ON CLUSTER ${validCluster} TO ${user}""";
+    }
     sql """CREATE ENCRYPTKEY ${key} AS 'ABCD123456789'"""
 
     connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
