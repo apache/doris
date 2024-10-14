6d6c4410a5929bf3d4d0c652a7c29ae7ae4169f3
Mingxi <71588583+joker-star-l@users.noreply.github.com>
2024-10-14 08:30:38 +0800

    [feature](agg-func) agg function linear_histogram (#39546)
    add aggregate function: `linear_histogram(expr, DOUBLE interval[, DOUBLE offset)`
    
    The linear_histogram function is used to describe the distribution of
    the data, It uses an "equal width" bucking strategy, and divides the
    data into buckets according to the value of the data.

--- regression-test/suites/nereids_syntax_p0/mv/aggregate/agg_sync_mv.groovy
+++ regression-test/suites/nereids_syntax_p0/mv/aggregate/agg_sync_mv.groovy
@@ -239,6 +239,15 @@ suite("agg_sync_mv") {
     }
     qt_select_group_concat_mv """select id, group_concat(cast(abs(kint) as varchar)) from agg_mv_test group by id order by id;"""
 
+    qt_select_linear_histogram """select id, linear_histogram(kint, 10) from agg_mv_test group by id order by id;"""
+    sql """drop materialized view if exists mv_sync on agg_mv_test;"""
+    createMV("""create materialized view mv_sync as select id, linear_histogram(kint, 10) from agg_mv_test group by id order by id;""")
+    explain {
+        sql("select id, linear_histogram(kint, 10) from agg_mv_test group by id order by id;")
+        contains "(mv_sync)"
+    }
+    qt_select_linear_histogram_mv """select id, linear_histogram(kint, 10) from agg_mv_test group by id order by id;"""
+
     qt_select_multi_distinct_group_concat """select id, multi_distinct_group_concat(cast(abs(kint) as varchar)) from agg_mv_test group by id order by id;"""
     sql """drop materialized view if exists mv_sync25 on agg_mv_test;"""
     createMV("""create materialized view mv_sync25 as select id, multi_distinct_group_concat(cast(abs(kint) as varchar)) from agg_mv_test group by id order by id;""")
