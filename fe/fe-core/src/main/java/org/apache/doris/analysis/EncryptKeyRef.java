9644ada312fb1317648a3b447f821d53dfae7b3c
zhangdong <493738387@qq.com>
2024-10-14 11:59:34 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/analysis/EncryptKeyRef.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/EncryptKeyRef.java
@@ -19,14 +19,10 @@ package org.apache.doris.analysis;
 
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.EncryptKey;
-import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.Type;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
-import org.apache.doris.datasource.InternalCatalog;
-import org.apache.doris.mysql.privilege.PrivPredicate;
-import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.thrift.TExprNode;
 
 import com.google.common.base.Strings;
@@ -66,12 +62,6 @@ public class EncryptKeyRef extends Expr {
         if ("".equals(dbName)) {
             ErrorReport.reportAnalysisException(ErrorCode.ERR_NO_DB_ERROR);
         } else {
-            if (!Env.getCurrentEnv().getAccessManager()
-                    .checkDbPriv(ConnectContext.get(), InternalCatalog.INTERNAL_CATALOG_NAME,
-                            dbName, PrivPredicate.SHOW)) {
-                ErrorReport.reportAnalysisException(ErrorCode.ERR_DB_ACCESS_DENIED_ERROR,
-                        PrivPredicate.SHOW.getPrivs().toString(), dbName);
-            }
             Database database = analyzer.getEnv().getInternalCatalog().getDbOrAnalysisException(dbName);
 
             EncryptKey encryptKey = database.getEncryptKey(encryptKeyName.getKeyName());
