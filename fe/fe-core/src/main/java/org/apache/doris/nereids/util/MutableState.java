1b78f8b50658b770bb90f9c16d64e5cd4d46bd25
minghong <englefly@gmail.com>
2024-10-17 11:36:52 +0800

    Revert "[opt](nereids) reimplement or-to-in rule (#41222)"
    This reverts commit cfe1506550c000d9cd1b0c1e6fc0d2a22a44458a.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/util/MutableState.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/util/MutableState.java
@@ -28,9 +28,6 @@ public interface MutableState {
     String KEY_PARENT = "parent";
     String KEY_RF_JUMP = "rf-jump";
     String KEY_PUSH_TOPN_TO_AGG = "pushTopnToAgg";
-
-    String KEY_OR_TO_IN = "or_to_in";
-
     <T> Optional<T> get(String key);
 
     MutableState set(String key, Object value);
