ff0eea3257115d64524b701fe8b91e9cf8d86dba
zhangstar333 <2561612514@qq.com>
2024-10-18 10:53:26 +0800

    update

--- regression-test/suites/query_p0/sql_functions/string_functions/test_count_substrings.groovy
+++ regression-test/suites/query_p0/sql_functions/string_functions/test_count_substrings.groovy
@@ -16,7 +16,7 @@
 // under the License.
 
 suite("test_count_substrings") {
-    // const
+    // const / NULL
     qt_select1 "select count_substrings(NULL,NULL);"
     qt_select2 "select count_substrings('a12bc23de345f',NULL);"
     qt_select3 "select count_substrings(NULL, 'a12bc23de345f');"
@@ -44,7 +44,7 @@ suite("test_count_substrings") {
     qt_select6_empty "select count_substrings(s1,p2) from test_count_substrings;"
     qt_select7_empty "select count_substrings(s2,p1) from test_count_substrings;"
 
-
+    // some normal/special/null value
     sql """ INSERT INTO test_count_substrings VALUES(1, 'abcde', 'abcde', '', '') """
     sql """ INSERT INTO test_count_substrings VALUES(2, '', '', '', '') """
     sql """ INSERT INTO test_count_substrings VALUES(3, '', '','a','a') """
@@ -58,9 +58,16 @@ suite("test_count_substrings") {
     sql """ INSERT INTO test_count_substrings VALUES(11, 'a,b,c,12345', 'a,b,c,12345','5','5') """
     sql """ INSERT INTO test_count_substrings VALUES(12, 'a,b,c,12345', 'a,b,c,12345','a','a') """
 
+    // null and not_null combine
     qt_select5_null_null "select s1,p1,count_substrings(s1, p1) from test_count_substrings order by k1;"
     qt_select6_null_not "select s1, p2,count_substrings(s1, p2) from test_count_substrings order by k1;"
     qt_select7_not_null "select s2, p1,count_substrings(s2, p1) from test_count_substrings order by k1;"
     qt_select8_not_not "select s2, p2,count_substrings(s2, p2) from test_count_substrings order by k1;"
+
+    // null const combine
+    qt_select9_null_const "select s1, 'a',count_substrings(s1, 'a') from test_count_substrings order by k1;"
+    qt_select10_not_null_const "select s2, 'a',count_substrings(s2, 'a') from test_count_substrings order by k1;"
+    qt_select11_const_null "select 'a',p1,count_substrings('a', p1) from test_count_substrings order by k1;"
+    qt_select12_const_not_null "select 'a',p2,count_substrings('a', p2) from test_count_substrings order by k1;"
 }
 
