09d02c0528283dfab8dc8199f284b85ca11eef43
vlt <59109857+Yoruet@users.noreply.github.com>
2024-10-21 15:56:00 +0800

    [refactor]remove the redundant nullable value calculation in function call (#41739)
    ## Proposed changes
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->
    
    ---------
    
    Co-authored-by: zhiqiang-hhhh <seuhezhiqiang@163.com>

--- be/src/vec/functions/array/function_array_contains_all.cpp
+++ be/src/vec/functions/array/function_array_contains_all.cpp
@@ -37,8 +37,6 @@ public:
 
     String get_name() const override { return name; }
 
-    bool use_default_implementation_for_nulls() const override { return true; }
-
     bool is_variadic() const override { return false; }
 
     size_t get_number_of_arguments() const override { return 2; }
