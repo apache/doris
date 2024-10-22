b28b5b2edfcdb8abeece06576452f417ac079877
Yukang-Lian <Yukang.Lian2022@gmail.com>
2024-10-22 17:28:24 +0800

    10

--- cloud/src/meta-service/meta_service.h
+++ cloud/src/meta-service/meta_service.h
@@ -40,6 +40,10 @@ class Transaction;
 
 constexpr std::string_view BUILT_IN_STORAGE_VAULT_NAME = "built_in_storage_vault";
 
+void internal_get_rowset(Transaction* txn, int64_t start, int64_t end,
+                         const std::string& instance_id, int64_t tablet_id, MetaServiceCode& code,
+                         std::string& msg, GetRowsetResponse* response, bool snapshot = false);
+
 class MetaServiceImpl : public cloud::MetaService {
 public:
     MetaServiceImpl(std::shared_ptr<TxnKv> txn_kv, std::shared_ptr<ResourceManager> resource_mgr,
