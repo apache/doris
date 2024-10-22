d91a2dd412f916a4e50569ec2710ddf9eed8b0fe
minghong <englefly@gmail.com>
2024-10-22 09:30:46 +0800

    fixed join order

--- regression-test/suites/nereids_rules_p0/eager_aggregate/push_down_count_through_join_one_side.groovy
+++ regression-test/suites/nereids_rules_p0/eager_aggregate/push_down_count_through_join_one_side.groovy
@@ -25,6 +25,7 @@ suite("push_down_count_through_join_one_side") {
     sql """
         DROP TABLE IF EXISTS count_t_one_side;
     """
+    sql "set disable_join_reorder=true"
 
     sql """
     CREATE TABLE IF NOT EXISTS count_t_one_side(
