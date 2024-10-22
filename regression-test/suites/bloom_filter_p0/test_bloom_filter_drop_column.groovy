6e1c0eae15090ab49d0b45804e7db133f6aa89da
姜凯 <airborne08@gmail.com>
2024-10-22 14:43:15 +0800

    [Test](bloom filter) enhance bloom filter test case with retry logic

--- regression-test/suites/bloom_filter_p0/test_bloom_filter_drop_column.groovy
+++ regression-test/suites/bloom_filter_p0/test_bloom_filter_drop_column.groovy
@@ -31,19 +31,42 @@ suite("test_bloom_filter_drop_column") {
     "in_memory" = "false",
     "storage_format" = "V2"
     )"""
+    def timeout = 60000
+    def delta_time = 1000
+    def alter_res = "null"
+    def useTime = 0
 
+    def wait_for_latest_op_on_table_finish = { tableName, OpTimeout ->
+        for(int t = delta_time; t <= OpTimeout; t += delta_time){
+            alter_res = sql """SHOW ALTER TABLE COLUMN WHERE TableName = "${tableName}" ORDER BY CreateTime DESC LIMIT 1;"""
+            alter_res = alter_res.toString()
+            if(alter_res.contains("FINISHED")) {
+                sleep(3000) // wait change table state to normal
+                logger.info(table_name + " latest alter job finished, detail: " + alter_res)
+                break
+            }
+            useTime = t
+            sleep(delta_time)
+        }
+        assertTrue(useTime <= OpTimeout, "wait_for_latest_op_on_table_finish timeout")
+    }
     sql """INSERT INTO ${table_name} values ('1', '1')"""
 
     qt_select """select * from ${table_name} order by a"""
 
     // drop column c1
     sql """ALTER TABLE ${table_name} DROP COLUMN c1"""
+    wait_for_latest_op_on_table_finish(table_name, timeout)
+
     // show create table
     def res = sql """SHOW CREATE TABLE ${table_name}"""
+    log.info("show table:{}", res);
     assert res[0][1].contains("\"bloom_filter_columns\" = \"\"")
 
     // add new column c1
     sql """ALTER TABLE ${table_name} ADD COLUMN c1 ARRAY<STRING>"""
+    wait_for_latest_op_on_table_finish(table_name, timeout)
+
     // insert data
     sql """INSERT INTO ${table_name} values ('2', null)"""
     // select data
