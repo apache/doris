f81d7bd1cf1db105110f49f3f1d8d4b89c6ad1a4
Rayner Chen <morningman@163.com>
2024-10-22 16:10:33 +0800

    [fix](hive-transaction) skip_checking_acid_version_file need to be forwarded (#42225)
    followup #42111
    the skip_checking_acid_version_file need to be forwarded

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -2111,6 +2111,13 @@ public class SessionVariable implements Serializable, Writable {
     })
     public boolean requireSequenceInInsert = true;
 
+
+    @VariableMgr.VarAttr(name = SKIP_CHECKING_ACID_VERSION_FILE, needForward = true, description = {
+            "跳过检查 transactional hive 版本文件 '_orc_acid_version.'",
+            "Skip checking transactional hive version file '_orc_acid_version.'"
+    })
+    public boolean skipCheckingAcidVersionFile = false;
+
     public void setEnableEsParallelScroll(boolean enableESParallelScroll) {
         this.enableESParallelScroll = enableESParallelScroll;
     }
