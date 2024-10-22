f5d43e7ea303cc11bc63738a44a366ef34ab4ce6
Yongqiang YANG <dataroaring@gmail.com>
2024-10-22 09:17:10 +0800

    fix

--- regression-test/suites/backup_restore/test_backup_restore_backup_temp_partition.groovy
+++ regression-test/suites/backup_restore/test_backup_restore_backup_temp_partition.groovy
@@ -68,7 +68,7 @@ suite("test_backup_restore_backup_temp_partition", "backup_restore") {
             TO `${repoName}`
             ON (${tableName})
         """
-        exception "Do not support backup table with temp partitions"
+        exception "Do not support backup table ${tableName} with temp partitions"
     }
 
     sql "DROP TABLE ${dbName}.${tableName} FORCE"
