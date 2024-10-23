e902002df30343777176d513f01e74df69cc6a4a
zhangdong <493738387@qq.com>
2024-10-23 18:37:31 +0800

    [fix](mtmv)fix in the scenario of recreating a table, the materialized view may assume that the data has not changed (#41762)
    snapshot of table,only compare tableName and version,when recerate
    table,and if the version remains unchanged 。
    
    MV will assume that the data has not changed

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -3140,7 +3140,7 @@ public class OlapTable extends Table implements MTMVRelatedTableIf, GsonPostProc
     public MTMVSnapshotIf getTableSnapshot(MTMVRefreshContext context) {
         Map<Long, Long> tableVersions = context.getBaseVersions().getTableVersions();
         long visibleVersion = tableVersions.containsKey(id) ? tableVersions.get(id) : getVisibleVersion();
-        return new MTMVVersionSnapshot(visibleVersion);
+        return new MTMVVersionSnapshot(visibleVersion, id);
     }
 
     @Override
