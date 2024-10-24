084bd998ead5b9a33fc5acef522dc5460d02a0cb
Luwei <814383175@qq.com>
2024-10-24 20:17:43 +0800

    fix

--- be/src/olap/tablet_reader.cpp
+++ be/src/olap/tablet_reader.cpp
@@ -649,9 +649,6 @@ Status TabletReader::_init_delete_condition(const ReaderParams& read_params) {
                       ((read_params.reader_type == ReaderType::READER_CUMULATIVE_COMPACTION &&
                         config::enable_delete_when_cumu_compaction)) ||
                       read_params.reader_type == ReaderType::READER_CHECKSUM);
-    auto* runtime_state = read_params.runtime_state;
-    bool enable_sub_pred_v2 =
-            runtime_state == nullptr ? true : runtime_state->enable_delete_sub_pred_v2();
     return _delete_handler.init(_tablet_schema, read_params.delete_predicates,
                                 read_params.version.second);
 }
