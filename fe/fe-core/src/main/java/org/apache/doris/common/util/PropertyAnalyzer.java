c79ce0757dcdf4f290c6723399080fbec2d7c2de
amorynan <wangqiannan@selectdb.com>
2024-10-25 10:58:53 +0800

    add types

--- fe/fe-core/src/main/java/org/apache/doris/common/util/PropertyAnalyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/util/PropertyAnalyzer.java
@@ -644,14 +644,15 @@ public class PropertyAnalyzer {
 
                         // now we only support BloomFilter on (same behavior with BE):
                         // smallint/int/bigint/largeint
-                        // string/varchar/char
+                        // string/varchar/char/variant
                         // date/datetime/datev2/datetimev2
                         // decimal/decimal32/decimal64/decimal128I/decimal256
                         // ipv4/ipv6
                         //
                         // key columns and none/replace aggregate non-key columns support
                         if (!((type ==  PrimitiveType.SMALLINT || type == PrimitiveType.INT
-                                || type == PrimitiveType.BIGINT) || type.isCharFamily() || type.isDateType()
+                                || type == PrimitiveType.BIGINT || type == PrimitiveType.LARGEINT)
+                                || type.isCharFamily() || type.isDateType() || type.isVariantType()
                                 || type.isDecimalV2Type() || type.isDecimalV3Type() || type.isIPType())) {
                             throw new AnalysisException(type + " is not supported in bloom filter index. "
                                     + "invalid column: " + bfColumn);
