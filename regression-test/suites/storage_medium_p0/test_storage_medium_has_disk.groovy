07f7fcb474709118f56c633cf19a6f136f44e9d2
deardeng <565620795@qq.com>
2024-10-28 10:16:49 +0800

    [fix](docker case) Fix `test_storage_medium_has_disk` due to invalid … #42010 (#42390)
    cherry pick from #42010

--- regression-test/suites/storage_medium_p0/test_storage_medium_has_disk.groovy
+++ regression-test/suites/storage_medium_p0/test_storage_medium_has_disk.groovy
@@ -122,7 +122,6 @@ suite('test_storage_medium_has_disk', 'docker') {
             "dynamic_partition.buckets" = "3",
             "dynamic_partition.replication_num" = "1",
             "dynamic_partition.create_history_partition"= "true",
-            "dynamic_partition.hot"= "true",
             "dynamic_partition.start" = "-4"
         )
         """
@@ -147,7 +146,6 @@ suite('test_storage_medium_has_disk', 'docker') {
                 "dynamic_partition.buckets" = "3",
                 "dynamic_partition.replication_num" = "1",
                 "dynamic_partition.create_history_partition"= "true",
-                "dynamic_partition.hot"= "true",
                 "dynamic_partition.start" = "-4"
             )
             """
