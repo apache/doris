904596af8309d1329c5ba3dc76bfd8512d6f0f87
Xinyi Zou <zouxinyi02@gmail.com>
2024-10-29 11:01:39 +0800

    [opt](memory) Support Jemalloc heap profile start at runtime and automatically generate dot (#42059)

--- be/src/runtime/exec_env.h
+++ be/src/runtime/exec_env.h
@@ -111,6 +111,7 @@ class RowCache;
 class DummyLRUCache;
 class CacheManager;
 class ProcessProfile;
+class HeapProfiler;
 class WalManager;
 class DNSCache;
 
@@ -314,6 +315,7 @@ public:
     RowCache* get_row_cache() { return _row_cache; }
     CacheManager* get_cache_manager() { return _cache_manager; }
     ProcessProfile* get_process_profile() { return _process_profile; }
+    HeapProfiler* get_heap_profiler() { return _heap_profiler; }
     segment_v2::InvertedIndexSearcherCache* get_inverted_index_searcher_cache() {
         return _inverted_index_searcher_cache;
     }
@@ -457,6 +459,7 @@ private:
     RowCache* _row_cache = nullptr;
     CacheManager* _cache_manager = nullptr;
     ProcessProfile* _process_profile = nullptr;
+    HeapProfiler* _heap_profiler = nullptr;
     segment_v2::InvertedIndexSearcherCache* _inverted_index_searcher_cache = nullptr;
     segment_v2::InvertedIndexQueryCache* _inverted_index_query_cache = nullptr;
     QueryCache* _query_cache = nullptr;
