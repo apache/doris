0b925c43ad4bffe197628f8459d3829c1e07962c
/bin/eash <zhangzhiyongde@126.com>
2024-10-30 19:20:32 +0800

    [Enhancement]（backup） add progress for restore and backup job

--- fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
@@ -1068,7 +1068,11 @@ public class BackupJob extends AbstractJob implements GsonPostProcessable {
         info.add(String.valueOf(jobId));
         info.add(label);
         info.add(dbName);
-        info.add(state.name());
+        String processState = "";
+        if (state == BackupJobState.UPLOADING) {
+            processState = (1 - unfinishedTaskIds.entrySet().size() * 1.00  / snapshotInfos.size()) * 100 + "%";
+        }
+        info.add(state.name() + " " + processState);
         info.add(getBackupObjs());
         info.add(TimeUtils.longToTimeString(createTime));
         info.add(TimeUtils.longToTimeString(snapshotFinishedTime));
