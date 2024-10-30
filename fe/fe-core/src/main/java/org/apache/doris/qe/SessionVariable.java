76cbfa776b3b36779d95951d08fe7dc96453804f
jacktengg <18241664+jacktengg@users.noreply.github.com>
2024-10-30 11:29:24 +0800

    [fix](spill) disable fuzzy spill for now

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -2376,6 +2376,8 @@ public class SessionVariable implements Serializable, Writable {
         this.topnOptLimitThreshold = (int) Math.pow(10, random.nextInt(5));
 
         // for spill to disk
+        /*
+        // disable fuzzy spill for now since it's in progress of refactoring
         if (Config.pull_request_id > 10000) {
             if (Config.pull_request_id % 2 == 0) {
                 this.enableJoinSpill = true;
@@ -2403,6 +2405,7 @@ public class SessionVariable implements Serializable, Writable {
                 this.enableAggSpill = false;
             }
         }
+        */
     }
 
     public String printFuzzyVariables() {
