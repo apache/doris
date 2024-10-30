40707602401b240e00440e74453fce13a2a2fffd
daidai <2017501503@qq.com>
2024-10-30 23:16:33 +0800

    [enhancement](jni)append exception check for jni code. (#42507)
    ## Proposed changes
    Because in jni, we need to manually check for exceptions thrown, and if
    we do not handle the exception, the program will continue to execute,
    which is not like the try catch logic we often use. Therefore, in order
    to reduce the potential risks caused by not handling exceptions, I
    checked most of the jni code and added exception detection for it.

--- be/src/vec/functions/function_java_udf.cpp
+++ be/src/vec/functions/function_java_udf.cpp
@@ -42,9 +42,6 @@ JavaFunctionCall::JavaFunctionCall(const TFunction& fn, const DataTypes& argumen
 Status JavaFunctionCall::open(FunctionContext* context, FunctionContext::FunctionStateScope scope) {
     JNIEnv* env = nullptr;
     RETURN_IF_ERROR(JniUtil::GetJNIEnv(&env));
-    if (env == nullptr) {
-        return Status::InternalError("Failed to get/create JVM");
-    }
 
     if (scope == FunctionContext::FunctionStateScope::THREAD_LOCAL) {
         std::shared_ptr<JniContext> jni_ctx = std::make_shared<JniContext>();
