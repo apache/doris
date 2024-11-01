ffa5ebddb7a9b6ed033d551dc7271cf90f1a7f3c
Gabriel <gabrielleebuaa@gmail.com>
2024-11-01 13:35:57 +0800

    update

--- be/src/pipeline/exec/exchange_sink_buffer.h
+++ be/src/pipeline/exec/exchange_sink_buffer.h
@@ -205,6 +205,7 @@ public:
 
 private:
     friend class ExchangeSinkLocalState;
+    void _set_ready_to_finish(bool all_done);
 
     phmap::flat_hash_map<InstanceLoId, std::unique_ptr<std::mutex>>
             _instance_to_package_queue_mutex;
