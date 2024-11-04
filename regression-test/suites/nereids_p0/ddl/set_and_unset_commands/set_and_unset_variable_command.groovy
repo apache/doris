ef709858d1c9552c980b426f04aff6ff159686e9
LiBinfeng <1204975323@qq.com>
2024-11-04 16:27:06 +0800

    rename test files

--- regression-test/suites/nereids_p0/ddl/set_and_unset_commands/set_and_unset_variable_command.groovy
+++ regression-test/suites/nereids_p0/ddl/set_and_unset_commands/set_and_unset_variable_command.groovy
@@ -15,27 +15,27 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("unset_command") {
+suite("set_and_unset_variable_command") {
     sql "set enable_nereids_planner=true"
     sql "set enable_fallback_to_original_planner=false"
 
     checkNereidsExecute("UNSET VARIABLE ALL")
     checkNereidsExecute("UNSET global VARIABLE ALL")
 
-    qt_cmd """set wait_timeout = 1000"""
+    checkNereidsExecute("set wait_timeout = 1000")
     qt_cmd """show variables like 'wait_timeout'"""
 
     checkNereidsExecute("UNSET VARIABLE wait_timeout")
     qt_cmd """show variables like 'wait_timeout'"""
 
-    qt_cmd """set runtime_filter_type='BLOOM_FILTER'"""
+    checkNereidsExecute("set runtime_filter_type='BLOOM_FILTER'")
     qt_cmd """show session variables like 'runtime_filter_type'"""
     qt_cmd """show global variables like 'runtime_filter_type'"""
     checkNereidsExecute("UNSET VARIABLE runtime_filter_type")
     qt_cmd """show session variables like 'runtime_filter_type'"""
     qt_cmd """show global variables like 'runtime_filter_type'"""
 
-    qt_cmd """set global runtime_filter_type='BLOOM_FILTER'"""
+    checkNereidsExecute("set global runtime_filter_type='BLOOM_FILTER'")
     qt_cmd """show session variables like 'runtime_filter_type'"""
     qt_cmd """show global variables like 'runtime_filter_type'"""
     checkNereidsExecute("UNSET global VARIABLE runtime_filter_type")
@@ -43,7 +43,7 @@ suite("unset_command") {
     qt_cmd """show global variables like 'runtime_filter_type'"""
 
     // test variables with experimental_ prefix in session scope
-    qt_cmd """set experimental_enable_agg_state='true'"""
+    checkNereidsExecute("set experimental_enable_agg_state='true'")
     qt_cmd """show session variables like 'experimental_enable_agg_state'"""
     qt_cmd """show global variables like 'experimental_enable_agg_state'"""
     checkNereidsExecute("UNSET VARIABLE experimental_enable_agg_state")
@@ -51,7 +51,7 @@ suite("unset_command") {
     qt_cmd """show global variables like 'experimental_enable_agg_state'"""
 
     // test variables with experimental_ prefix in global scope
-    qt_cmd """set global experimental_enable_agg_state='true'"""
+    checkNereidsExecute("set global experimental_enable_agg_state='true'")
     qt_cmd """show session variables like 'experimental_enable_agg_state'"""
     qt_cmd """show global variables like 'experimental_enable_agg_state'"""
     checkNereidsExecute("UNSET global VARIABLE experimental_enable_agg_state")
@@ -59,7 +59,7 @@ suite("unset_command") {
     qt_cmd """show global variables like 'experimental_enable_agg_state'"""
 
     // test variables with deprecated_ prefix
-    qt_cmd """set deprecated_enable_local_exchange = false"""
+    checkNereidsExecute("set deprecated_enable_local_exchange = false")
     qt_cmd """show session variables like 'deprecated_enable_local_exchange'"""
     qt_cmd """show global variables like 'deprecated_enable_local_exchange'"""
     checkNereidsExecute("UNSET global VARIABLE deprecated_enable_local_exchange")
@@ -67,10 +67,10 @@ suite("unset_command") {
     qt_cmd """show global variables like 'deprecated_enable_local_exchange'"""
 
     // test UNSET VARIABLE ALL
-    qt_cmd """set runtime_filter_type='BLOOM_FILTER'"""
-    qt_cmd """set experimental_enable_agg_state='true'"""
-    qt_cmd """set deprecated_enable_local_exchange = false"""
-    qt_cmd """set show_hidden_columns=true"""
+    checkNereidsExecute("set runtime_filter_type='BLOOM_FILTER'")
+    checkNereidsExecute("set experimental_enable_agg_state='true'")
+    checkNereidsExecute("set deprecated_enable_local_exchange = false")
+    checkNereidsExecute("set show_hidden_columns=true")
     checkNereidsExecute("UNSET VARIABLE ALL")
     qt_cmd """show session variables like 'runtime_filter_type'"""
     qt_cmd """show session variables like 'experimental_enable_agg_state'"""
@@ -80,10 +80,10 @@ suite("unset_command") {
     qt_cmd """select * from information_schema.session_variables where variable_name = 'show_hidden_columns'"""
 
     // test UNSET GLOBAL VARIABLE ALL
-    qt_cmd """set global runtime_filter_type='BLOOM_FILTER'"""
-    qt_cmd """set global experimental_enable_agg_state='true'"""
-    qt_cmd """set global deprecated_enable_local_exchange = false"""
-    qt_cmd """set show_hidden_columns=true"""
+    checkNereidsExecute("set global runtime_filter_type='BLOOM_FILTER'")
+    checkNereidsExecute("set global experimental_enable_agg_state='true'")
+    checkNereidsExecute("set global deprecated_enable_local_exchange = false")
+    checkNereidsExecute("set show_hidden_columns=true")
     checkNereidsExecute("UNSET global VARIABLE ALL")
     qt_cmd """show global variables like 'runtime_filter_type'"""
     qt_cmd """show global variables like 'experimental_enable_agg_state'"""
