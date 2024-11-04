a392e257c69eeec9d7b81476e0c1dcddef64bd96
walter <w41ter.l@gmail.com>
2024-11-04 16:20:06 +0800

    [fix](backup) Fix compressed backup job (#43197)
    This was introduced in #42462

--- fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/BackupJob.java
@@ -1077,7 +1077,7 @@ public class BackupJob extends AbstractJob {
             ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
             try (GZIPOutputStream gzipStream = new GZIPOutputStream(byteStream)) {
                 try (DataOutputStream stream = new DataOutputStream(gzipStream)) {
-                    writeOthers(out);
+                    writeOthers(stream);
                     written = stream.size();
                 }
             }
