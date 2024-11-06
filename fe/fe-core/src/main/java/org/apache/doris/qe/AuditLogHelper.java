1e93d711d63ab13c56a484c3964236137f46bcb2
Mingyu Chen (Rayner) <morningman@163.com>
2024-11-06 15:18:53 +0800

    [fix](auditlog) add missing audit log fields and duplicate audit log error #42262 (#43303)
    cherry pick from #42262

--- fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
@@ -186,6 +186,8 @@ public class AuditLogHelper {
         String cluster = Config.isCloudMode() ? cloudCluster : "";
 
         AuditEventBuilder auditEventBuilder = ctx.getAuditEventBuilder();
+        // ATTN: MUST reset, otherwise, the same AuditEventBuilder instance will be used in the next query.
+        auditEventBuilder.reset();
         auditEventBuilder
                 .setTimestamp(ctx.getStartTime())
                 .setClientIp(ctx.getClientIP())
