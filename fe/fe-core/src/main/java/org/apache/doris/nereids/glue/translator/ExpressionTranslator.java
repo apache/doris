519c6aec260316d24b21a417875c4072a4eec19c
LiBinfeng <46676950+LiBinfeng-01@users.noreply.github.com>
2024-11-06 11:48:58 +0800

    [Impl](Nereids) add propagateNullLiteral trait for special functions (#42256)
    Add a trait PropagateNullLiteral, which ensures that if any of its input
    parameters is a null literal, the output will also be a null literal.
    Note that PropagateNullable also has this property. Due to limitations
    in the GeneratedExpressionRelations map, most functions that implement
    alwaysNullable are included in PropagateNullLiteral. Therefore, defining
    a function that is PropagateNullable but not PropagateNullLiteral is not
    currently allowed.
    function lists：
    Acos AppendTrailingCharIfAbsent Asin BitmapFromArray BitmapFromBase64
    BitmapFromString BitmapMax BitmapMin BitmapSubsetInRange
    BitmapSubsetLimit ConvertTz CryptoFunction DayFloor Date DateFormat
    Divide DigitalMasking Dlog10 Dsqrt Fmod FromBase64 FromDays
    GetJsonDouble GetJsonString HourCeil HourFloor IntegralDivide Lpad Ln
    Log10 Log2 Mod MonthCeil MonthFloor Pmod ParseUrl Rpad Repeat
    RegexpExtract RegexpExtractAll RegexpReplace RegexpReplaceOne SecondCeil
    SplitPart Sqrt StAngle StAngleSphere StAreaSquareKm StAreaSquareMeters
    StDistanceSphere StGeomFromWKB StGeomfromtext StGeometryfromtext
    StLinefromtext StPoint StPolygon StPolyfromtext StX StY StrToDate
    SubReplace ToDate ToDateV2

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/ExpressionTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/ExpressionTranslator.java
@@ -78,6 +78,7 @@ import org.apache.doris.nereids.trees.expressions.VirtualSlotReference;
 import org.apache.doris.nereids.trees.expressions.WhenClause;
 import org.apache.doris.nereids.trees.expressions.functions.AlwaysNotNullable;
 import org.apache.doris.nereids.trees.expressions.functions.AlwaysNullable;
+import org.apache.doris.nereids.trees.expressions.functions.PropagateNullLiteral;
 import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateFunction;
 import org.apache.doris.nereids.trees.expressions.functions.agg.AggregateParam;
 import org.apache.doris.nereids.trees.expressions.functions.agg.Count;
@@ -557,7 +558,7 @@ public class ExpressionTranslator extends DefaultExpressionVisitor<Expr, PlanTra
     @Override
     public Expr visitBinaryArithmetic(BinaryArithmetic binaryArithmetic, PlanTranslatorContext context) {
         NullableMode nullableMode = NullableMode.DEPEND_ON_ARGUMENT;
-        if (binaryArithmetic instanceof AlwaysNullable) {
+        if (binaryArithmetic instanceof AlwaysNullable || binaryArithmetic instanceof PropagateNullLiteral) {
             nullableMode = NullableMode.ALWAYS_NULLABLE;
         } else if (binaryArithmetic instanceof AlwaysNotNullable) {
             nullableMode = NullableMode.ALWAYS_NOT_NULLABLE;
