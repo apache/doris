b13d1f33aef8baab43d066e3c9f72bc8228c4684
qidaye <luen@selectdb.com>
2024-11-07 22:14:30 +0800

    format

--- be/src/common/status.h
+++ be/src/common/status.h
@@ -76,7 +76,7 @@ namespace ErrorCode {
     TStatusError(HTTP_ERROR, true);                       \
     TStatusError(TABLET_MISSING, true);                   \
     TStatusError(NOT_MASTER, true);                       \
-    TStatusError(OBTAIN_LOCK_FAILED, false);                 \
+    TStatusError(OBTAIN_LOCK_FAILED, false);              \
     TStatusError(DELETE_BITMAP_LOCK_ERROR, false);
 // E error_name, error_code, print_stacktrace
 #define APPLY_FOR_OLAP_ERROR_CODES(E)                        \
