9901ac8430a166a6de74c28c6841167ab9993f1d
minghong <zhouminghong@selectdb.com>
2024-11-08 15:01:23 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/nereids/stats/FilterEstimation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/stats/FilterEstimation.java
@@ -76,6 +76,7 @@ public class FilterEstimation extends ExpressionVisitor<Statistics, EstimationCo
 
     public static final double DEFAULT_LIKE_COMPARISON_SELECTIVITY = 0.2;
     public static final double DEFAULT_ISNULL_SELECTIVITY = 0.005;
+    private static final int LARGE_COMPOUND_PREDICATE = 4;
     private Set<Slot> aggSlots;
 
     private boolean isOnBaseTable = false;
