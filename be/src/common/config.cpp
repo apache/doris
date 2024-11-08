f0ee17bc9f3966024e0c67f2a1a5b8799d06b6af
zhengyu <freeman.zhang1992@gmail.com>
2024-11-08 15:39:54 +0800

    [fix](cloud) fix read cache block file return stat NOT_FOUND (#43220)
    Previously, cache blocks could be downloaded as different types than the
    target type we intended to read, leading to false cache misses. E.g., a
    block might be downloaded as an 'idx' type when the current context
    expected a 'ttl' type.
    
    The root cause of this problem is the original design encoding meta info
    such as type and expiration time into cache block file path and readers
    of this cache block file have inconsistent view of the type so they use
    different name to locate file and run into error in the end.
    
    This commit tries other type if the initial type failed to locate the
    file (return NOT_FOUND). Be ware that this is a nasty quick fix. We will
    elimite the metadate encoded in the file path in the near future to get
    rid of all the path related problems.

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1016,8 +1016,6 @@ DEFINE_mInt64(file_cache_ttl_valid_check_interval_second, "0"); // zero for not 
 // If true, evict the ttl cache using LRU when full.
 // Otherwise, only expiration can evict ttl and new data won't add to cache when full.
 DEFINE_Bool(enable_ttl_cache_evict_using_lru, "true");
-// rename ttl filename to new format during read, with some performance cost
-DEFINE_mBool(translate_to_new_ttl_format_during_read, "false");
 DEFINE_mBool(enbale_dump_error_file, "true");
 // limit the max size of error log on disk
 DEFINE_mInt64(file_cache_error_log_limit_bytes, "209715200"); // 200MB
