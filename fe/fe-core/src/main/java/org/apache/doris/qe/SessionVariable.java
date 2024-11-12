901eb1c749eff8f15c77a373d0ea6a5cccef7851
Sun Chenyang <sunchenyang@selectdb.com>
2024-11-12 21:00:39 +0800

    [fix] (inverted index) Disallow variant columns from using inverted index format v1 (#43599)
    ### What problem does this PR solve?
    
    Problem Summary:
    1. When the inverted index of a variant column uses storage format v1,
    schema changes can cause some segments to lack corresponding index
    files.
    
    2. By using storage format v2 for inverted indexes, all indexes
    correspond to a single file, and the corresponding files will always
    exist regardless of whether the variant includes subcolumn indexes.
    
    ### Release note
    
    When creating an inverted index for a variant column, file format v1 is
    not supported

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -665,6 +665,8 @@ public class SessionVariable implements Serializable, Writable {
 
     public static final String MAX_FETCH_REMOTE_TABLET_COUNT = "max_fetch_remote_schema_tablet_count";
 
+    public static final String DISABLE_INVERTED_INDEX_V1_FOR_VARIANT = "disable_inverted_index_v1_for_variant";
+
     // CLOUD_VARIABLES_BEGIN
     public static final String CLOUD_CLUSTER = "cloud_cluster";
     public static final String DISABLE_EMPTY_PARTITION_PRUNE = "disable_empty_partition_prune";
@@ -1223,6 +1225,9 @@ public class SessionVariable implements Serializable, Writable {
 
     @VariableMgr.VarAttr(name = WAIT_FULL_BLOCK_SCHEDULE_TIMES)
     public int waitFullBlockScheduleTimes = 2;
+
+    @VariableMgr.VarAttr(name = DISABLE_INVERTED_INDEX_V1_FOR_VARIANT)
+    private boolean disableInvertedIndexV1ForVaraint = true;
 
     public int getBeNumberForTest() {
         return beNumberForTest;
@@ -4560,5 +4565,13 @@ public class SessionVariable implements Serializable, Writable {
 
     public boolean isUseSerialExchange() {
         return useSerialExchange && getEnableLocalExchange();
+    }
+
+    public void setDisableInvertedIndexV1ForVaraint(boolean disableInvertedIndexV1ForVaraint) {
+        this.disableInvertedIndexV1ForVaraint = disableInvertedIndexV1ForVaraint;
+    }
+
+    public boolean getDisableInvertedIndexV1ForVaraint() {
+        return disableInvertedIndexV1ForVaraint;
     }
 }
