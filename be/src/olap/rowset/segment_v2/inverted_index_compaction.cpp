0e60370741a26b389ae0ec77764a92679bc3b0e8
qidaye <luen@selectdb.com>
2024-11-12 14:40:25 +0800

    fix

--- be/src/olap/rowset/segment_v2/inverted_index_compaction.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_compaction.cpp
@@ -58,6 +58,7 @@ Status compact_column(int64_t index_id,
     std::vector<lucene::store::Directory*> tmp_src_index_dirs(src_index_dirs.size());
     for (size_t i = 0; i < tmp_src_index_dirs.size(); ++i) {
         tmp_src_index_dirs[i] = src_index_dirs[i].get();
+        
     }
     index_writer->indexCompaction(tmp_src_index_dirs, dest_index_dirs, trans_vec,
                                   dest_segment_num_rows);
