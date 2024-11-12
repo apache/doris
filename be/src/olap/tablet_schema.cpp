b3648363fa3d510f3b0c645f7b5a7d46f0fb18c3
zzzxl <yangsiyu@selectdb.com>
2024-11-12 14:48:03 +0800

    test email (#43713)

--- be/src/olap/tablet_schema.cpp
+++ be/src/olap/tablet_schema.cpp
@@ -14,7 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
+#
 #include "olap/tablet_schema.h"
 
 #include <gen_cpp/Descriptors_types.h>
