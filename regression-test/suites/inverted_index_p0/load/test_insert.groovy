515643054565c8b2a5d899106fbc731e5aacafd2
Sun Chenyang <sunchenyang@selectdb.com>
2024-11-12 21:00:39 +0800

    [fix] (inverted index) Disallow variant columns from using inverted index format v1 (#43599)
    Problem Summary:
    1. When the inverted index of a variant column uses storage format v1,
    schema changes can cause some segments to lack corresponding index
    files.
    
    2. By using storage format v2 for inverted indexes, all indexes
    correspond to a single file, and the corresponding files will always
    exist regardless of whether the variant includes subcolumn indexes.
    
    When creating an inverted index for a variant column, file format v1 is
    not supported

--- regression-test/suites/inverted_index_p0/load/test_insert.groovy
+++ regression-test/suites/inverted_index_p0/load/test_insert.groovy
@@ -72,10 +72,14 @@ suite("test_insert_with_index", "p0") {
     }
 
     set_be_config("inverted_index_ram_dir_enable", "true")
+    sql """ set disable_inverted_index_v1_for_variant = false """
     test.call("V1")
+    sql """ set disable_inverted_index_v1_for_variant = true """
     test.call("V2")
     set_be_config("inverted_index_ram_dir_enable", "false")
+    sql """ set disable_inverted_index_v1_for_variant = false """
     test.call("V1")
+    sql """ set disable_inverted_index_v1_for_variant = true """
     test.call("V2")
     set_be_config("inverted_index_ram_dir_enable", "true")
 }
