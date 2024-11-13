apache/doris

commit 2468619d40da60e40487fe74fc0683cd740cd6a0

[opt](binlog) Support modify comment binlog (#39783)
Co-authored-by: walter <w41ter.l@gmail.com>

fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -1955,7 +1955,9 @@ public class EditLog {
     }
 
     public void logModifyComment(ModifyCommentOperationLog op) {
-        logEdit(OperationType.OP_MODIFY_COMMENT, op);
+        long logId = logEdit(OperationType.OP_MODIFY_COMMENT, op);
+        LOG.info("log modify comment, logId : {}, infos: {}", logId, op);
+        Env.getCurrentEnv().getBinlogManager().addModifyComment(op, logId);
     }
 
     public void logCreateSqlBlockRule(SqlBlockRule rule) {

fe/fe-core/src/main/java/org/apache/doris/binlog/BinlogManager.java
@@ -29,6 +29,7 @@ import org.apache.doris.persist.BarrierLog;
 import org.apache.doris.persist.BatchModifyPartitionsInfo;
 import org.apache.doris.persist.BinlogGcInfo;
 import org.apache.doris.persist.DropPartitionInfo;
+import org.apache.doris.persist.ModifyCommentOperationLog;
 import org.apache.doris.persist.ModifyTablePropertyOperationLog;
 import org.apache.doris.persist.ReplacePartitionOperationLog;
 import org.apache.doris.persist.TableAddOrDropColumnsInfo;
@@ -328,6 +329,17 @@ public class BinlogManager {
         long timestamp = -1;
         TBinlogType type = TBinlogType.RENAME_TABLE;
         String data = info.toJson();
+        addBinlog(dbId, tableIds, commitSeq, timestamp, type, data, false, info);
+    }
+
+    public void addModifyComment(ModifyCommentOperationLog info, long commitSeq) {
+        long dbId = info.getDbId();
+        List<Long> tableIds = Lists.newArrayList();
+        tableIds.add(info.getTblId());
+        long timestamp = -1;
+        TBinlogType type = TBinlogType.MODIFY_COMMENT;
+        String data = info.toJson();
+
         addBinlog(dbId, tableIds, commitSeq, timestamp, type, data, false, info);
     }
 

fe/fe-core/src/main/java/org/apache/doris/persist/ModifyCommentOperationLog.java
@@ -94,4 +94,8 @@ public class ModifyCommentOperationLog implements Writable {
         String json = Text.readString(in);
         return GsonUtils.GSON.fromJson(json, ModifyCommentOperationLog.class);
     }
+
+    public String toJson() {
+        return GsonUtils.GSON.toJson(this);
+    }
 }

gensrc/thrift/FrontendService.thrift
@@ -1188,6 +1188,7 @@ enum TBinlogType {
   TRUNCATE_TABLE = 13,
   RENAME_TABLE = 14,
   RENAME_COLUMN = 15,
+  MODIFY_COMMENT = 16,
 }
 
 struct TBinlog {

commit 3f63c8aa60b34b2662347dc5b5e33e6d12e3704f

[fix](binlog) add modify comment binlog when replaying journal
This issue is introduced in #39783

fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -876,6 +876,7 @@ public class EditLog {
                 case OperationType.OP_MODIFY_COMMENT: {
                     ModifyCommentOperationLog operation = (ModifyCommentOperationLog) journal.getData();
                     env.getAlterInstance().replayModifyComment(operation);
+                    env.getBinlogManager().addModifyComment(operation, logId);
                     break;
                 }
                 case OperationType.OP_SET_PARTITION_VERSION: {