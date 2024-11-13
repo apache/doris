1c65114e8d076942f702d29425b96e4e064fe466
Mryange <yanxuecheng@selectdb.com>
2024-11-13 19:15:12 +0800

    test

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -1483,7 +1483,7 @@ public class SessionVariable implements Serializable, Writable {
     public boolean enableCommonExprPushdown = true;
 
     @VariableMgr.VarAttr(name = ENABLE_LOCAL_EXCHANGE, fuzzy = true, varType = VariableAnnotation.DEPRECATED)
-    public boolean enableLocalExchange = true;
+    public boolean enableLocalExchange = false;
 
     /**
      * For debug purpose, don't merge unique key and agg key when reading data.
@@ -2354,7 +2354,7 @@ public class SessionVariable implements Serializable, Writable {
         this.parallelPipelineTaskNum = random.nextInt(8);
         this.parallelPrepareThreshold = random.nextInt(32) + 1;
         this.enableCommonExprPushdown = random.nextBoolean();
-        this.enableLocalExchange = random.nextBoolean();
+        this.enableLocalExchange = false;
         this.useSerialExchange = random.nextBoolean();
         // This will cause be dead loop, disable it first
         // this.disableJoinReorder = random.nextBoolean();
