d62a765190f296f5b69ade870f2874b81b9a1304
walter <maochuan@selectdb.com>
2024-11-13 12:33:51 +0800

    [opt](binlog) Support modify comment binlog #39783 (#43825)
    cherry pick from #39783
    
    Co-authored-by: smallx <e9999e@163.com>

--- fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
+++ fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -1739,7 +1739,9 @@ public class EditLog {
     }
 
     public void logModifyComment(ModifyCommentOperationLog op) {
-        logEdit(OperationType.OP_MODIFY_COMMENT, op);
+        long logId = logEdit(OperationType.OP_MODIFY_COMMENT, op);
+        LOG.info("log modify comment, logId : {}, infos: {}", logId, op);
+        Env.getCurrentEnv().getBinlogManager().addModifyComment(op, logId);
     }
 
     public void logCreateSqlBlockRule(SqlBlockRule rule) {
