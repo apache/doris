8f5af0afa30ce9a059117c91d110e0e767225a09
zhangstar333 <zhangsida@selectdb.com>
2024-11-14 15:13:36 +0800

    test

--- be/src/vec/aggregate_functions/aggregate_function_percentile.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_percentile.cpp
@@ -14,6 +14,8 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+//
+//
 
 #include "vec/aggregate_functions/aggregate_function_percentile.h"
 
