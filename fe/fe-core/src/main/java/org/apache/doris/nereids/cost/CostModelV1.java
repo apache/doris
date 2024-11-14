805eb2299d21f5515ebf6cc898d2fd2437ac21d4
seawinde <wusi@selectdb.com>
2024-11-14 22:05:42 +0800

    add test

--- fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/cost/CostModelV1.java
@@ -113,52 +113,50 @@ class CostModelV1 extends PlanVisitor<Cost, PlanContext> {
         }
         double defaultFactor = 1D;
         if (context.getSessionVariable() != null) {
-            /**
-             * Such query as following:
-             *             SELECT
-             *               s_acctbal,
-             *               s_name,
-             *               n_name,
-             *               p_partkey,
-             *               p_mfgr,
-             *               s_address,
-             *               s_phone,
-             *               s_comment
-             *             FROM
-             *               part,
-             *               supplier,
-             *               partsupp,
-             *               nation,
-             *               region
-             *             WHERE
-             *               p_partkey = ps_partkey
-             *               AND s_suppkey = ps_suppkey
-             *               AND s_nationkey = n_nationkey
-             *               AND n_regionkey = r_regionkey
-             *               AND r_name = 'EUROPE';
-             *  If query can be rewritten by materialized view as following:
-             *             SELECT
-             *               s_acctbal,
-             *               s_name,
-             *               n_name,
-             *               p_partkey,
-             *               p_mfgr,
-             *               s_address,
-             *               s_phone,
-             *               s_comment
-             *             FROM
-             *               mv,
-             *               region
-             *             WHERE
-             *               p_partkey = ps_partkey
-             *               AND s_suppkey = ps_suppkey
-             *               AND p_size = 15
-             *               AND p_type LIKE '%BRASS'
-             *               AND s_nationkey = n_nationkey
-             *               AND n_regionkey = r_regionkey
-             *               AND r_name = 'EUROPE';
-             *  The cost of mv scan factor should be lower, becuase the rewrite has obvious benefit.
-             * */
+            //Such query as following:
+            //            SELECT
+            //              s_acctbal,
+            //              s_name,
+            //              n_name,
+            //              p_partkey,
+            //              p_mfgr,
+            //              s_address,
+            //              s_phone,
+            //              s_comment
+            //            FROM
+            //              part,
+            //              supplier,
+            //              partsupp,
+            //              nation,
+            //              region
+            //            WHERE
+            //              p_partkey = ps_partkey
+            //              AND s_suppkey = ps_suppkey
+            //              AND s_nationkey = n_nationkey
+            //              AND n_regionkey = r_regionkey
+            //              AND r_name = 'EUROPE';
+            // If query can be rewritten by materialized view as following:
+            //            SELECT
+            //              s_acctbal,
+            //              s_name,
+            //              n_name,
+            //              p_partkey,
+            //              p_mfgr,
+            //              s_address,
+            //              s_phone,
+            //              s_comment
+            //            FROM
+            //              mv,
+            //              region
+            //            WHERE
+            //              p_partkey = ps_partkey
+            //              AND s_suppkey = ps_suppkey
+            //              AND p_size = 15
+            //              AND p_type LIKE '%BRASS'
+            //              AND s_nationkey = n_nationkey
+            //              AND n_regionkey = r_regionkey
+            //              AND r_name = 'EUROPE';
+            // The cost of mv scan factor should be lower, because the rewrite has obvious benefit.
             defaultFactor = context.getSessionVariable().scanCostFactor;
         }
         return CostV1.ofCpu(context.getSessionVariable(), (rows - aggMvBonus) * defaultFactor);
