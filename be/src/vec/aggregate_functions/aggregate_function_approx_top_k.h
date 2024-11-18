apache/doris

commit 0e985beed86948d85a520b9dfd6f03349de8bff8

[feature](function) add approx_top_k aggregation function (#40813)
## Proposed changes

1. select approx_top_k(clientip, status, size, 10, 300) from tbl;

This code implements an approximate Top-N query function based on the
SpaceSaving algorithm. SpaceSaving is an efficient streaming algorithm
commonly used to handle frequent element query problems in large
datasets. Below is a description of the main functionalities:
      
      (1) Data Structures and Memory Management:
The SpaceSavingArena class provides a memory pool to manage memory
allocation and deallocation. For keys of type StringRef, it handles the
memory by copying the string into the memory pool.
The Counter struct stores the key, count, and error for each element,
and provides serialization and deserialization functions.

      (2) Insertion and Updates:
The insert method is used to insert new elements or update the count of
existing elements. If the current capacity is not full, it inserts the
new element; if it is full, it replaces the element with the smallest
count based on the element's count and error.

      (3) Merge Operation:
The merge method allows merging two SpaceSaving objects. During the
merge, it adjusts the counts and errors of the existing elements,
ensuring that the result maintains the correct order.

      (4) Top-K Query:
The top_k method returns the current Top-K most frequent elements,
sorted by their count and error.

      (5) Capacity Expansion and Shrinking:
The resize method allows adjusting the storage capacity, and it
recalculates the size of the alpha_map accordingly.

      (6) Serialization and Deserialization:
The write and read methods are provided for serializing the SpaceSaving
structure to disk or reading data from disk.

      (7) Optimization and Performance:
The code uses a hash table-based approach for lookup and storage, and
dynamically adjusts the alpha_map size to optimize performance and
reduce memory waste.

be/src/vec/aggregate_functions/aggregate_function_approx_top_k.h
@@ -1,0 +1,290 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <rapidjson/encodings.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+#include <cstdint>
+#include <string>
+
+#include "vec/aggregate_functions/aggregate_function.h"
+#include "vec/aggregate_functions/aggregate_function_approx_top.h"
+#include "vec/columns/column.h"
+#include "vec/columns/column_array.h"
+#include "vec/columns/column_string.h"
+#include "vec/columns/column_struct.h"
+#include "vec/columns/column_vector.h"
+#include "vec/columns/columns_number.h"
+#include "vec/common/assert_cast.h"
+#include "vec/common/space_saving.h"
+#include "vec/common/string_ref.h"
+#include "vec/core/types.h"
+#include "vec/data_types/data_type_array.h"
+#include "vec/data_types/data_type_ipv4.h"
+#include "vec/data_types/data_type_nullable.h"
+#include "vec/data_types/data_type_struct.h"
+#include "vec/io/io_helper.h"
+
+namespace doris::vectorized {
+
+inline constexpr UInt64 TOP_K_MAX_SIZE = 0xFFFFFF;
+
+struct AggregateFunctionTopKGenericData {
+    using Set = SpaceSaving<StringRef, StringRefHash>;
+
+    Set value;
+};
+
+template <int32_t ArgsSize>
+class AggregateFunctionApproxTopK final
+        : public IAggregateFunctionDataHelper<AggregateFunctionTopKGenericData,
+                                              AggregateFunctionApproxTopK<ArgsSize>>,
+          AggregateFunctionApproxTop {
+private:
+    using State = AggregateFunctionTopKGenericData;
+
+public:
+    AggregateFunctionApproxTopK(std::vector<std::string> column_names,
+                                const DataTypes& argument_types_)
+            : IAggregateFunctionDataHelper<AggregateFunctionTopKGenericData,
+                                           AggregateFunctionApproxTopK<ArgsSize>>(argument_types_),
+              _column_names(std::move(column_names)) {}
+
+    String get_name() const override { return "approx_top_k"; }
+
+    DataTypePtr get_return_type() const override { return std::make_shared<DataTypeString>(); }
+
+    // Serializes the aggregate function's state (including the SpaceSaving structure and threshold) into a buffer.
+    void serialize(ConstAggregateDataPtr __restrict place, BufferWritable& buf) const override {
+        this->data(place).value.write(buf);
+
+        write_var_uint(_column_names.size(), buf);
+        for (const auto& column_name : _column_names) {
+            write_string_binary(column_name, buf);
+        }
+        write_var_uint(_threshold, buf);
+        write_var_uint(_reserved, buf);
+    }
+
+    // Deserializes the aggregate function's state from a buffer (including the SpaceSaving structure and threshold).
+    void deserialize(AggregateDataPtr __restrict place, BufferReadable& buf,
+                     Arena* arena) const override {
+        auto readStringBinaryInto = [](Arena& arena, BufferReadable& buf) {
+            size_t size = 0;
+            read_var_uint(size, buf);
+
+            if (UNLIKELY(size > DEFAULT_MAX_STRING_SIZE)) {
+                throw Exception(ErrorCode::INTERNAL_ERROR, "Too large string size.");
+            }
+
+            char* data = arena.alloc(size);
+            buf.read(data, size);
+
+            return StringRef(data, size);
+        };
+
+        auto& set = this->data(place).value;
+        set.clear();
+
+        size_t size = 0;
+        read_var_uint(size, buf);
+        if (UNLIKELY(size > TOP_K_MAX_SIZE)) {
+            throw Exception(ErrorCode::INTERNAL_ERROR,
+                            "Too large size ({}) for aggregate function '{}' state (maximum is {})",
+                            size, get_name(), TOP_K_MAX_SIZE);
+        }
+
+        set.resize(size);
+        for (size_t i = 0; i < size; ++i) {
+            auto ref = readStringBinaryInto(*arena, buf);
+            uint64_t count = 0;
+            uint64_t error = 0;
+            read_var_uint(count, buf);
+            read_var_uint(error, buf);
+            set.insert(ref, count, error);
+            arena->rollback(ref.size);
+        }
+
+        set.read_alpha_map(buf);
+
+        uint64_t column_size = 0;
+        read_var_uint(column_size, buf);
+        _column_names.clear();
+        for (uint64_t i = 0; i < column_size; i++) {
+            std::string column_name;
+            read_string_binary(column_name, buf);
+            _column_names.emplace_back(std::move(column_name));
+        }
+        read_var_uint(_threshold, buf);
+        read_var_uint(_reserved, buf);
+    }
+
+    // Adds a new row of data to the aggregate function (inserts a new value into the SpaceSaving structure).
+    void add(AggregateDataPtr __restrict place, const IColumn** columns, ssize_t row_num,
+             Arena* arena) const override {
+        if (!_init_flag) {
+            lazy_init(columns, row_num);
+        }
+
+        auto& set = this->data(place).value;
+        if (set.capacity() != _reserved) {
+            set.resize(_reserved);
+        }
+
+        auto all_serialize_value_into_arena =
+                [](size_t i, size_t keys_size, const IColumn** columns, Arena* arena) -> StringRef {
+            const char* begin = nullptr;
+
+            size_t sum_size = 0;
+            for (size_t j = 0; j < keys_size; ++j) {
+                sum_size += columns[j]->serialize_value_into_arena(i, *arena, begin).size;
+            }
+
+            return {begin, sum_size};
+        };
+
+        StringRef str_serialized =
+                all_serialize_value_into_arena(row_num, _column_names.size(), columns, arena);
+        set.insert(str_serialized);
+        arena->rollback(str_serialized.size);
+    }
+
+    void add_many(AggregateDataPtr __restrict place, const IColumn** columns,
+                  std::vector<int>& rows, Arena* arena) const override {
+        for (auto row : rows) {
+            add(place, columns, row, arena);
+        }
+    }
+
+    void reset(AggregateDataPtr __restrict place) const override {
+        this->data(place).value.clear();
+    }
+
+    // Merges the state of another aggregate function into the current one (merges two SpaceSaving sets).
+    void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs,
+               Arena*) const override {
+        auto& rhs_set = this->data(rhs).value;
+        if (!rhs_set.size()) {
+            return;
+        }
+
+        auto& set = this->data(place).value;
+        if (set.capacity() != _reserved) {
+            set.resize(_reserved);
+        }
+        set.merge(rhs_set);
+    }
+
+    void insert_result_into(ConstAggregateDataPtr __restrict place, IColumn& to) const override {
+        auto& data_to = assert_cast<ColumnString&, TypeCheckOnRelease::DISABLE>(to);
+
+        const typename State::Set& set = this->data(place).value;
+        auto result_vec = set.top_k(_threshold);
+
+        rapidjson::StringBuffer buffer;
+        rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+        writer.StartArray();
+        for (auto& result : result_vec) {
+            auto argument_types = this->get_argument_types();
+            MutableColumns argument_columns(_column_names.size());
+            for (size_t i = 0; i < _column_names.size(); ++i) {
+                argument_columns[i] = argument_types[i]->create_column();
+            }
+            rapidjson::StringBuffer sub_buffer;
+            rapidjson::Writer<rapidjson::StringBuffer> sub_writer(sub_buffer);
+            sub_writer.StartObject();
+            const char* begin = result.key.data;
+            for (size_t i = 0; i < _column_names.size(); i++) {
+                begin = argument_columns[i]->deserialize_and_insert_from_arena(begin);
+                std::string row_str = argument_types[i]->to_string(*argument_columns[i], 0);
+                sub_writer.Key(_column_names[i].data(), _column_names[i].size());
+                sub_writer.String(row_str.data(), row_str.size());
+            }
+            sub_writer.Key("count");
+            sub_writer.String(std::to_string(result.count).c_str());
+            sub_writer.EndObject();
+            writer.RawValue(sub_buffer.GetString(), sub_buffer.GetSize(), rapidjson::kObjectType);
+        }
+        writer.EndArray();
+        std::string res = buffer.GetString();
+        data_to.insert_data(res.data(), res.size());
+    }
+
+private:
+    void lazy_init(const IColumn** columns, ssize_t row_num) const {
+        auto get_param = [](size_t idx, const DataTypes& data_types,
+                            const IColumn** columns) -> uint64_t {
+            const auto& data_type = data_types.at(idx);
+            const IColumn* column = columns[idx];
+
+            const auto* type = data_type.get();
+            if (type->is_nullable()) {
+                type = assert_cast<const DataTypeNullable*, TypeCheckOnRelease::DISABLE>(type)
+                               ->get_nested_type()
+                               .get();
+            }
+            int64_t value = 0;
+            WhichDataType which(type);
+            if (which.idx == TypeIndex::Int8) {
+                value = assert_cast<const ColumnInt8*, TypeCheckOnRelease::DISABLE>(column)
+                                ->get_element(0);
+            } else if (which.idx == TypeIndex::Int16) {
+                value = assert_cast<const ColumnInt16*, TypeCheckOnRelease::DISABLE>(column)
+                                ->get_element(0);
+            } else if (which.idx == TypeIndex::Int32) {
+                value = assert_cast<const ColumnInt32*, TypeCheckOnRelease::DISABLE>(column)
+                                ->get_element(0);
+            }
+            if (value <= 0) {
+                throw Exception(ErrorCode::INVALID_ARGUMENT,
+                                "The parameter cannot be less than or equal to 0.");
+            }
+            return value;
+        };
+
+        const auto& data_types = this->get_argument_types();
+        if (ArgsSize == 1) {
+            _threshold =
+                    std::min(get_param(_column_names.size(), data_types, columns), (uint64_t)1000);
+        } else if (ArgsSize == 2) {
+            _threshold =
+                    std::min(get_param(_column_names.size(), data_types, columns), (uint64_t)1000);
+            _reserved = std::min(
+                    std::max(get_param(_column_names.size() + 1, data_types, columns), _threshold),
+                    (uint64_t)1000);
+        }
+
+        if (_threshold == 0 || _reserved == 0 || _threshold > 1000 || _reserved > 1000) {
+            throw Exception(ErrorCode::INTERNAL_ERROR,
+                            "approx_top_k param error, _threshold: {}, _reserved: {}", _threshold,
+                            _reserved);
+        }
+
+        _init_flag = true;
+    }
+
+    mutable std::vector<std::string> _column_names;
+    mutable bool _init_flag = false;
+    mutable uint64_t _threshold = 10;
+    mutable uint64_t _reserved = 300;
+};
+
+} // namespace doris::vectorized

be/src/vec/common/space_saving.h
@@ -1,0 +1,342 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// This file is copied from
+// https://github.com/ClickHouse/ClickHouse/blob/master/src/Common/SpaceSaving.h.h
+// and modified by Doris
+
+#pragma once
+
+#include <boost/range/adaptor/reversed.hpp>
+
+#include "vec/common/arena_with_free_lists.h"
+#include "vec/common/hash_table/hash_map.h"
+#include "vec/common/string_buffer.hpp"
+#include "vec/io/io_helper.h"
+
+namespace doris::vectorized {
+
+template <typename TKey>
+struct SpaceSavingArena {
+    SpaceSavingArena() = default;
+    TKey emplace(const TKey& key) { return key; }
+    void free(const TKey& /*key*/) {}
+};
+
+template <>
+struct SpaceSavingArena<StringRef> {
+    StringRef emplace(StringRef key) {
+        if (!key.data) {
+            return key;
+        }
+
+        return copy_string_in_arena(arena, key);
+    }
+
+    void free(StringRef key) {
+        if (key.data) {
+            arena.free(const_cast<char*>(key.data), key.size);
+        }
+    }
+
+    template <typename Arena>
+    inline StringRef copy_string_in_arena(Arena& arena, StringRef value) {
+        size_t value_size = value.size;
+        char* place_for_key = arena.alloc(value_size);
+        memcpy(reinterpret_cast<void*>(place_for_key), reinterpret_cast<const void*>(value.data),
+               value_size);
+        StringRef result {place_for_key, value_size};
+
+        return result;
+    }
+
+private:
+    ArenaWithFreeLists arena;
+};
+
+template <typename TKey, typename Hash = DefaultHash<TKey>>
+class SpaceSaving {
+private:
+    // This function calculates the next size for the alpha map based on the given value `x`.
+    // It uses `alpha_map_elements_per_counter` to estimate the required number of elements.
+    constexpr uint64_t next_alpha_size(uint64_t x) {
+        constexpr uint64_t alpha_map_elements_per_counter = 6;
+        return 1ULL << (sizeof(uint64_t) * 8 -
+                        std::countl_zero(x * alpha_map_elements_per_counter));
+    }
+
+public:
+    using Self = SpaceSaving;
+
+    struct Counter {
+        Counter() = default;
+
+        explicit Counter(const TKey& k, uint64_t c = 0, uint64_t e = 0, size_t h = 0)
+                : key(k), hash(h), count(c), error(e) {}
+
+        void write(BufferWritable& wb) const {
+            write_binary(key, wb);
+            write_var_uint(count, wb);
+            write_var_uint(error, wb);
+        }
+
+        void read(BufferReadable& rb) {
+            read_binary(key, rb);
+            read_var_uint(count, rb);
+            read_var_uint(error, rb);
+        }
+
+        bool operator>(const Counter& b) const {
+            return (count > b.count) || (count == b.count && error < b.error);
+        }
+
+        TKey key;
+        size_t slot = 0;
+        size_t hash = 0;
+        uint64_t count = 0;
+        uint64_t error = 0;
+    };
+
+    explicit SpaceSaving(size_t c = 10) : alpha_map(next_alpha_size(c)), m_capacity(c) {}
+
+    ~SpaceSaving() { destroy_elements(); }
+
+    size_t size() const { return counter_list.size(); }
+
+    size_t capacity() const { return m_capacity; }
+
+    void clear() { return destroy_elements(); }
+
+    void resize(size_t new_capacity) {
+        counter_list.reserve(new_capacity);
+        alpha_map.resize(next_alpha_size(new_capacity));
+        m_capacity = new_capacity;
+    }
+
+    // Inserts a new element or updates the count of an existing element.
+    // If the element exists, the count and error are increased.
+    // If the element doesn't exist and the capacity is not full, it inserts the new element.
+    // If the capacity is full, it replaces the element with the smallest count and inserts the new one.
+    void insert(const TKey& key, uint64_t increment = 1, uint64_t error = 0) {
+        auto hash = counter_map.hash(key);
+
+        if (auto* counter = find_counter(key, hash); counter) {
+            counter->count += increment;
+            counter->error += error;
+            percolate(counter);
+            return;
+        }
+
+        if (UNLIKELY(size() < capacity())) {
+            push(std::make_unique<Counter>(arena.emplace(key), increment, error, hash));
+            return;
+        }
+
+        auto& min = counter_list.back();
+        if (increment > min->count) {
+            destroy_last_element();
+            push(std::make_unique<Counter>(arena.emplace(key), increment, error, hash));
+            return;
+        }
+
+        const size_t alpha_mask = alpha_map.size() - 1;
+        auto& alpha = alpha_map[hash & alpha_mask];
+        if (alpha + increment < min->count) {
+            alpha += increment;
+            return;
+        }
+
+        alpha_map[min->hash & alpha_mask] = min->count;
+        destroy_last_element();
+
+        push(std::make_unique<Counter>(arena.emplace(key), alpha + increment, alpha + error, hash));
+    }
+
+    // Merges another `SpaceSaving` object into the current one. Updates counts and errors of elements.
+    // If the other object is full, it adds its elements to the current list and maintains sorting.
+    void merge(const Self& rhs) {
+        if (!rhs.size()) {
+            return;
+        }
+
+        uint64_t m1 = 0;
+        uint64_t m2 = 0;
+
+        if (size() == capacity()) {
+            m1 = counter_list.back()->count;
+        }
+
+        if (rhs.size() == rhs.capacity()) {
+            m2 = rhs.counter_list.back()->count;
+        }
+
+        if (m2 > 0) {
+            for (auto& counter : counter_list) {
+                counter->count += m2;
+                counter->error += m2;
+            }
+        }
+
+        for (auto& counter : boost::adaptors::reverse(rhs.counter_list)) {
+            size_t hash = counter_map.hash(counter->key);
+            if (auto* current = find_counter(counter->key, hash)) {
+                current->count += (counter->count - m2);
+                current->error += (counter->error - m2);
+            } else {
+                counter_list.push_back(std::make_unique<Counter>(arena.emplace(counter->key),
+                                                                 counter->count + m1,
+                                                                 counter->error + m1, hash));
+            }
+        }
+
+        std::sort(counter_list.begin(), counter_list.end(),
+                  [](const auto& l, const auto& r) { return *l > *r; });
+
+        if (counter_list.size() > m_capacity) {
+            for (size_t i = m_capacity; i < counter_list.size(); ++i) {
+                arena.free(counter_list[i]->key);
+            }
+            counter_list.resize(m_capacity);
+        }
+
+        for (size_t i = 0; i < counter_list.size(); ++i) {
+            counter_list[i]->slot = i;
+        }
+        rebuild_counter_map();
+    }
+
+    // Retrieves the top-k counters, sorted by their count and error values.
+    std::vector<Counter> top_k(size_t k) const {
+        std::vector<Counter> res;
+        for (auto& counter : counter_list) {
+            res.push_back(*counter);
+            if (res.size() == k) {
+                break;
+            }
+        }
+        return res;
+    }
+
+    void write(BufferWritable& wb) const {
+        write_var_uint(size(), wb);
+        for (auto& counter : counter_list) {
+            counter->write(wb);
+        }
+
+        write_var_uint(alpha_map.size(), wb);
+        for (auto alpha : alpha_map) {
+            write_var_uint(alpha, wb);
+        }
+    }
+
+    void read(BufferReadable& rb) {
+        destroy_elements();
+        size_t count = 0;
+        read_var_uint(count, rb);
+
+        for (size_t i = 0; i < count; ++i) {
+            std::unique_ptr counter = std::make_unique<Counter>();
+            counter->read(rb);
+            counter->hash = counter_map.hash(counter->key);
+            push(std::move(counter));
+        }
+
+        read_alpha_map(rb);
+    }
+
+    // Reads the alpha map data from the provided readable buffer.
+    void read_alpha_map(BufferReadable& rb) {
+        size_t alpha_size = 0;
+        read_var_uint(alpha_size, rb);
+        for (size_t i = 0; i < alpha_size; ++i) {
+            uint64_t alpha = 0;
+            read_var_uint(alpha, rb);
+            alpha_map.push_back(alpha);
+        }
+    }
+
+protected:
+    void push(std::unique_ptr<Counter> counter) {
+        counter->slot = counter_list.size();
+        auto* ptr = counter.get();
+        counter_list.push_back(std::move(counter));
+        counter_map[ptr->key] = ptr;
+        percolate(ptr);
+    }
+
+    void percolate(Counter* counter) {
+        while (counter->slot > 0) {
+            auto& next = counter_list[counter->slot - 1];
+            if (*counter > *next) {
+                std::swap(next->slot, counter->slot);
+                std::swap(counter_list[next->slot], counter_list[counter->slot]);
+            } else {
+                break;
+            }
+        }
+    }
+
+private:
+    void destroy_elements() {
+        for (auto& counter : counter_list) {
+            arena.free(counter->key);
+        }
+
+        counter_map.clear();
+        counter_list.clear();
+        alpha_map.clear();
+    }
+
+    void destroy_last_element() {
+        auto& last_element = counter_list.back();
+        counter_map.erase(last_element->key);
+        arena.free(last_element->key);
+        counter_list.pop_back();
+
+        ++removed_keys;
+        if (removed_keys * 2 > counter_map.size()) {
+            rebuild_counter_map();
+        }
+    }
+
+    Counter* find_counter(const TKey& key, size_t hash) {
+        auto it = counter_map.find(key, hash);
+        if (it == counter_map.end()) {
+            return nullptr;
+        }
+
+        return it->second;
+    }
+
+    void rebuild_counter_map() {
+        removed_keys = 0;
+        counter_map.clear();
+        for (auto& counter : counter_list) {
+            counter_map[counter->key] = counter.get();
+        }
+    }
+
+    using CounterMap = flat_hash_map<TKey, Counter*, Hash>;
+
+    CounterMap counter_map;
+    std::vector<std::unique_ptr<Counter>> counter_list;
+    std::vector<uint64_t> alpha_map;
+    SpaceSavingArena<TKey> arena;
+    size_t m_capacity = 0;
+    size_t removed_keys = 0;
+};
+
+} // namespace doris::vectorized

be/src/vec/aggregate_functions/aggregate_function.h
@@ -43,6 +43,7 @@ class IDataType;
 
 struct AggregateFunctionAttr {
     bool enable_decimal256 {false};
+    std::vector<std::pair<std::string, bool>> column_infos;
 };
 
 template <bool nullable, typename ColVecType>

commit 5e92112850e891243ea95f73d77a66f2b9032f45

[fix](compile) fix macos compile failed (#44061)
fix macos compile failed, introduced by #40813, #42930, #43218, #43289

be/src/vec/aggregate_functions/aggregate_function_approx_top_k.h
@@ -85,7 +85,7 @@ public:
     void deserialize(AggregateDataPtr __restrict place, BufferReadable& buf,
                      Arena* arena) const override {
         auto readStringBinaryInto = [](Arena& arena, BufferReadable& buf) {
-            size_t size = 0;
+            uint64_t size = 0;
             read_var_uint(size, buf);
 
             if (UNLIKELY(size > DEFAULT_MAX_STRING_SIZE)) {
@@ -101,7 +101,7 @@ public:
         auto& set = this->data(place).value;
         set.clear();
 
-        size_t size = 0;
+        uint64_t size = 0;
         read_var_uint(size, buf);
         if (UNLIKELY(size > TOP_K_MAX_SIZE)) {
             throw Exception(ErrorCode::INTERNAL_ERROR,

be/src/vec/aggregate_functions/aggregate_function_collect.h
@@ -98,7 +98,7 @@ struct AggregateFunctionCollectSetData {
     }
 
     void read(BufferReadable& buf) {
-        size_t new_size = 0;
+        uint64_t new_size = 0;
         read_var_uint(new_size, buf);
         ElementNativeType x;
         for (size_t i = 0; i < new_size; ++i) {

be/src/vec/aggregate_functions/aggregate_function_distinct.h
@@ -94,7 +94,7 @@ struct AggregateFunctionDistinctSingleNumericData {
     void deserialize(BufferReadable& buf, Arena*) {
         DCHECK(!stable);
         if constexpr (!stable) {
-            size_t new_size = 0;
+            uint64_t new_size = 0;
             read_var_uint(new_size, buf);
             T x;
             for (size_t i = 0; i < new_size; ++i) {

be/src/vec/common/space_saving.h
@@ -244,10 +244,10 @@ public:
 
     void read(BufferReadable& rb) {
         destroy_elements();
-        size_t count = 0;
+        uint64_t count = 0;
         read_var_uint(count, rb);
 
-        for (size_t i = 0; i < count; ++i) {
+        for (UInt64 i = 0; i < count; ++i) {
             std::unique_ptr counter = std::make_unique<Counter>();
             counter->read(rb);
             counter->hash = counter_map.hash(counter->key);
@@ -259,7 +259,7 @@ public:
 
     // Reads the alpha map data from the provided readable buffer.
     void read_alpha_map(BufferReadable& rb) {
-        size_t alpha_size = 0;
+        uint64_t alpha_size = 0;
         read_var_uint(alpha_size, rb);
         for (size_t i = 0; i < alpha_size; ++i) {
             uint64_t alpha = 0;

be/src/vec/exprs/table_function/vexplode_numbers.cpp
@@ -70,7 +70,7 @@ Status VExplodeNumbersTableFunction::process_init(Block* block, RuntimeState* st
 
         ((ColumnInt32*)_elements_column.get())->clear();
         //_cur_size may be a negative number
-        _cur_size = std::max(0L, _cur_size);
+        _cur_size = std::max(static_cast<int64_t>(0L), _cur_size);
         if (_cur_size &&
             _cur_size <= state->batch_size()) { // avoid elements_column too big or empty
             _is_const = true;                   // use const optimize

be/src/vec/exprs/vruntimefilter_wrapper.h
@@ -77,7 +77,8 @@ public:
     template <typename T>
     static void judge_selectivity(double ignore_threshold, int64_t filter_rows, int64_t input_rows,
                                   T& always_true) {
-        always_true = filter_rows / (input_rows * 1.0L) < ignore_threshold;
+        always_true = static_cast<double>(filter_rows) / static_cast<double>(input_rows) <
+                      ignore_threshold;
     }
 
     bool is_rf_wrapper() const override { return true; }