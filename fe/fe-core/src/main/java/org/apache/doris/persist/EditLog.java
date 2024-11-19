c914d8044a0128203c4a58c0c8e36a4cf3521c8d
walter <maochuan@selectdb.com>
2024-11-19 10:19:43 +0800

    [opt] (binlog) Support Modify ViewDef binlog #41167 (#44115)
    cherry pick from #41167
    
    Co-authored-by: yanmingfu <133083714+xiaoming12306@users.noreply.github.com>
    Co-authored-by: yanmingfu <yanmingfu@baidu.com>

--- fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
+++ fe/fe-core/src/main/java/org/apache/doris/persist/EditLog.java
@@ -312,6 +312,7 @@ public class EditLog {
                 case OperationType.OP_MODIFY_VIEW_DEF: {
                     AlterViewInfo info = (AlterViewInfo) journal.getData();
                     env.getAlterInstance().replayModifyViewDef(info);
+                    env.getBinlogManager().addModifyViewDef(info, logId);
                     break;
                 }
                 case OperationType.OP_RENAME_PARTITION: {
@@ -1577,7 +1578,11 @@ public class EditLog {
     }
 
     public void logModifyViewDef(AlterViewInfo alterViewInfo) {
-        logEdit(OperationType.OP_MODIFY_VIEW_DEF, alterViewInfo);
+        long logId = logEdit(OperationType.OP_MODIFY_VIEW_DEF, alterViewInfo);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("log modify view, logId : {}, infos: {}", logId, alterViewInfo);
+        }
+        Env.getCurrentEnv().getBinlogManager().addModifyViewDef(alterViewInfo, logId);
     }
 
     public void logRollupRename(TableInfo tableInfo) {
