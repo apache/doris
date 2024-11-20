399b893fa1c66c0dc7d34eb3c83f162235b67439
zhangdong <zhangdong@selectdb.com>
2024-11-20 17:41:49 +0800

    1

---
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/paimon/PaimonSnapshotCache.java
@@ -0,0 +1,106 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.datasource.paimon;
+
+import org.apache.doris.common.DdlException;
+import org.apache.doris.mtmv.BaseTableInfo;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PaimonSnapshotCache {
+    // snapshotId ==> SchemaCacheValue
+    private static Map<SnapshotCacheKey, PaimonSchemaCacheValue> snapshotCache = Maps.newHashMap();
+    // snapshotId ==> refNum
+    private static Map<SnapshotCacheKey, AtomicInteger> snapshotIdRefs = Maps.newHashMap();
+
+
+    public static PaimonSchemaCacheValue getSchemaCacheBySnapshotId(PaimonExternalTable paimonExternalTable,
+            long snapshotId) throws DdlException {
+        BaseTableInfo baseTableInfo = new BaseTableInfo(paimonExternalTable);
+        SnapshotCacheKey key = new SnapshotCacheKey(baseTableInfo, snapshotId);
+        if (snapshotCache.containsKey(key)) {
+            return snapshotCache.get(key);
+        }
+        Optional<PaimonSchemaCacheValue> latestSchemaCache = paimonExternalTable.getLatestSchemaCache();
+        if (latestSchemaCache.isPresent() && latestSchemaCache.get().getSnapshootId() == snapshotId) {
+            snapshotCache.put(key, latestSchemaCache.get());
+            return latestSchemaCache.get();
+        }
+        throw new DdlException("schema can not find by: " + key);
+    }
+
+    public static void ref(PaimonExternalTable paimonExternalTable, long snapshotId) {
+        BaseTableInfo baseTableInfo = new BaseTableInfo(paimonExternalTable);
+        SnapshotCacheKey key = new SnapshotCacheKey(baseTableInfo, snapshotId);
+        if (snapshotIdRefs.containsKey(key)) {
+            snapshotIdRefs.get(key).getAndIncrement();
+        } else {
+            snapshotIdRefs.put(key, new AtomicInteger(1));
+        }
+    }
+
+    public static void unref(PaimonExternalTable paimonExternalTable, long snapshotId) {
+        BaseTableInfo baseTableInfo = new BaseTableInfo(paimonExternalTable);
+        SnapshotCacheKey key = new SnapshotCacheKey(baseTableInfo, snapshotId);
+        int i = snapshotIdRefs.get(key).decrementAndGet();
+        if (i == 0) {
+            snapshotIdRefs.remove(key);
+            snapshotCache.remove(key);
+        }
+    }
+
+    private static class SnapshotCacheKey {
+        public BaseTableInfo baseTableInfo;
+        public long snapshotId;
+
+        public SnapshotCacheKey(BaseTableInfo baseTableInfo, long snapshotId) {
+            this.baseTableInfo = baseTableInfo;
+            this.snapshotId = snapshotId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            SnapshotCacheKey that = (SnapshotCacheKey) o;
+            return snapshotId == that.snapshotId && Objects.equal(baseTableInfo, that.baseTableInfo);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(baseTableInfo, snapshotId);
+        }
+
+        @Override
+        public String toString() {
+            return "SnapshotCacheKey{"
+                    + "baseTableInfo=" + baseTableInfo
+                    + ", snapshotId=" + snapshotId
+                    + '}';
+        }
+    }
+}
