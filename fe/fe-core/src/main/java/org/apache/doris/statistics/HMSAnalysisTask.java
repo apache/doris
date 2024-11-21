1d6644109e1f9e93de8664d8b75e90693610cb41
morningman <yunyou@selectdb.com>
2024-11-21 09:56:10 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/statistics/HMSAnalysisTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/HMSAnalysisTask.java
@@ -30,6 +30,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
