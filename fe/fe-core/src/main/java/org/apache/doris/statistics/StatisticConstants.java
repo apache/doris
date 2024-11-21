dd4583f1ef14043bd718766dd37234c20e2ee3f6
James <lijibing@selectdb.com>
2024-11-21 10:51:05 +0800

    [improvement](statistics)Change auto analyze max width to 300 and health threshold to 90. (#42104)
    ### What problem does this PR solve?
    
    Change auto analyze max width to 300 and health threshold to 90. This
    could auto analyze wider tables by default and collect table with data
    changes more frequently.
    
    Issue Number: close #xxx
    
    Related PR: #xxx
    
    Problem Summary:
    
    ### Release note
    
    None

--- fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticConstants.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/StatisticConstants.java
@@ -93,13 +93,13 @@ public class StatisticConstants {
 
     public static final long EXTERNAL_TABLE_AUTO_ANALYZE_INTERVAL_IN_MILLIS = TimeUnit.HOURS.toMillis(24);
 
-    public static final int TABLE_STATS_HEALTH_THRESHOLD = 60;
+    public static final int TABLE_STATS_HEALTH_THRESHOLD = 90;
 
     public static final int ANALYZE_TIMEOUT_IN_SEC = 43200;
 
     public static final int TASK_QUEUE_CAP = 1;
 
-    public static final int AUTO_ANALYZE_TABLE_WIDTH_THRESHOLD = 100;
+    public static final int AUTO_ANALYZE_TABLE_WIDTH_THRESHOLD = 300;
 
     public static final int MSG_LEN_UPPER_BOUND = 1024;
 
