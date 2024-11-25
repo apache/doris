2cf280f6d9d49ab404a7f90ac7fa2e17be5409d4
zzzxl <yangsiyu@selectdb.com>
2024-11-25 17:32:42 +0800

    [fix](inverted index) Content Check for Tokenize Function Parser (#44465)
    Problem Summary:
    1. Prevent users from mistakenly assuming other tokenizers exist.

--- be/src/vec/functions/function_tokenize.cpp
+++ be/src/vec/functions/function_tokenize.cpp
@@ -142,6 +142,12 @@ Status FunctionTokenize::execute_impl(FunctionContext* /*context*/, Block& block
             }
             inverted_index_ctx.parser_type = get_inverted_index_parser_type_from_string(
                     get_parser_string_from_properties(properties));
+            if (inverted_index_ctx.parser_type == InvertedIndexParserType::PARSER_UNKNOWN) {
+                return Status::Error<doris::ErrorCode::INVERTED_INDEX_INVALID_PARAMETERS>(
+                        "unsupported parser type. currently, only 'english', 'chinese', and "
+                        "'unicode' analyzers are supported.");
+            }
+
             inverted_index_ctx.parser_mode = get_parser_mode_string_from_properties(properties);
             inverted_index_ctx.char_filter_map =
                     get_parser_char_filter_map_from_properties(properties);
