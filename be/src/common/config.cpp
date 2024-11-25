29eccc670bcf0ab4cabafcd779f7eac3b4ed52ac
Jerry Hu <hushenggang@selectdb.com>
2024-11-25 11:12:54 +0800

    default 10s

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -554,7 +554,7 @@ DEFINE_Bool(enable_brpc_builtin_services, "true");
 // Enable brpc connection check
 DEFINE_Bool(enable_brpc_connection_check, "false");
 
-DEFINE_mInt64(brpc_connection_check_timeout_ms, "1000");
+DEFINE_mInt64(brpc_connection_check_timeout_ms, "10000");
 
 // The maximum amount of data that can be processed by a stream load
 DEFINE_mInt64(streaming_load_max_mb, "102400");
