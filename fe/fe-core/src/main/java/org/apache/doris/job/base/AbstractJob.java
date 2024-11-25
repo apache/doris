feeb660e7b7bf042749dffc1871ee4f5526095b9
Calvin Kirs <guoqiang@selectdb.com>
2024-11-25 16:13:43 +0800

    [Fix](Job)Fix some issues in the Insert job.
    The job does not account for tasks in the Canceled state.
    When a job is canceled, its status is marked as FAILED, and a NullPointerException (NPE) occurs because resources have already been released.
    The RESUME job does not immediately schedule the job.

--- fe/fe-core/src/main/java/org/apache/doris/job/base/AbstractJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/base/AbstractJob.java
@@ -155,6 +155,7 @@ public abstract class AbstractJob<T extends AbstractTask, C> implements Job<T, C
         }
         for (T task : runningTasks) {
             task.cancel();
+            canceledTaskCount.incrementAndGet();
         }
         runningTasks = new CopyOnWriteArrayList<>();
         logUpdateOperation();
@@ -185,6 +186,7 @@ public abstract class AbstractJob<T extends AbstractTask, C> implements Job<T, C
         runningTasks.stream().filter(task -> task.getTaskId().equals(taskId)).findFirst()
                 .orElseThrow(() -> new JobException("Not found task id: " + taskId)).cancel();
         runningTasks.removeIf(task -> task.getTaskId().equals(taskId));
+        canceledTaskCount.incrementAndGet();
         if (jobConfig.getExecuteType().equals(JobExecuteType.ONE_TIME)) {
             updateJobStatus(JobStatus.FINISHED);
         }
@@ -418,13 +420,13 @@ public abstract class AbstractJob<T extends AbstractTask, C> implements Job<T, C
     /**
      * Generates a common error message when the execution queue is full.
      *
-     * @param taskId                The ID of the task.
-     * @param queueConfigName       The name of the queue configuration.
+     * @param taskId                  The ID of the task.
+     * @param queueConfigName         The name of the queue configuration.
      * @param executeThreadConfigName The name of the execution thread configuration.
      * @return A formatted error message.
      */
     protected String commonFormatMsgWhenExecuteQueueFull(Long taskId, String queueConfigName,
-                                                            String executeThreadConfigName) {
+                                                         String executeThreadConfigName) {
         return String.format("Dispatch task failed, jobId: %d, jobName: %s, taskId: %d, the queue size is full, "
                         + "you can increase the queue size by setting the property "
                         + "%s in the fe.conf file or increase the value of "
