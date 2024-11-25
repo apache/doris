a3c9b80bdd1623114c1a1539d47866f5a6feeaa8
bobhan1 <baohan@selectdb.com>
2024-11-25 11:38:00 +0800

    fix and update case

--- regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_auto_inc.groovy
+++ regression-test/suites/unique_with_mow_p0/partial_update/test_partial_update_auto_inc.groovy
@@ -110,7 +110,8 @@ suite("test_partial_update_auto_inc") {
             (
                 `id`                      bigint         not null AUTO_INCREMENT,
                 `project_code`            varchar(20)    not null,
-                `period_num`              int   
+                `period_num`              int,
+                `c2`     int
             ) unique KEY(`id`)
             DISTRIBUTED BY HASH(`id`) BUCKETS auto
             PROPERTIES (
@@ -118,10 +119,30 @@ suite("test_partial_update_auto_inc") {
             "enable_unique_key_merge_on_write" = "true"
             );   """
         sql "set enable_unique_key_partial_update=true;"
+        sql "set enable_insert_strict=false;"
         sql "sync;"
+        
+        sql "insert into test_primary_key_partial_update_auto_inc3(project_code,period_num) values ('test1',15),('test2',29),('test3',49);"
+        qt_sql "select project_code,period_num from test_primary_key_partial_update_auto_inc3 order by project_code,period_num;"
+        qt_sql "select count(distinct id) from test_primary_key_partial_update_auto_inc3;"
+
+
+        sql """ DROP TABLE IF EXISTS test_primary_key_partial_update_auto_inc4 """
+        sql """ CREATE TABLE test_primary_key_partial_update_auto_inc4 (
+                        `k1` BIGINT NOT NULL AUTO_INCREMENT,
+                        `k2` int,
+                        `c1` int,
+                        `c2` int,
+                        `c3` int)
+                        UNIQUE KEY(`k1`,`k2`) DISTRIBUTED BY HASH(`k1`,`k2`) BUCKETS 1
+                        PROPERTIES("replication_num" = "1", "enable_unique_key_merge_on_write" = "true"); """
+        sql "set enable_unique_key_partial_update=true;"
+        sql "set enable_insert_strict=false;"
+        sql "sync;"
+
         test {
-            sql "insert into test_primary_key_partial_update_auto_inc3(project_code) values ('test1');"
-            exception "distributed column not found"
+            sql "insert into test_primary_key_partial_update_auto_inc4(c1,c2) values(1,1),(2,2),(3,3)"
+            exception "Partial update should include all key columns, missing: k2"
         }
     }
 }
