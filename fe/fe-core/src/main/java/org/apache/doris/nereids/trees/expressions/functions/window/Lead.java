953e5e9e1ffb66dc70738354dc23e8cd3cf0b106
morrySnow <zhangwenxin@selectdb.com>
2024-11-25 19:24:00 +0800

    [opt](Nereids) let lead and lag type coersion behavior same with MySQL
    For input and default are numeric types, use below priority
    - DecimalV3
    - DecimalV2
    - Double
    - Float
    - LargeInt
    - BigInt
    - Int
    - SmallInt
    - TinyInt
    
    For input and default are Date or DateTime types, user below priority
    - DateTimeV2
    - DateTime
    - DateV2
    - Date
    
    other wise, use String

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/functions/window/Lead.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/functions/window/Lead.java
@@ -17,37 +17,17 @@
 
 package org.apache.doris.nereids.trees.expressions.functions.window;
 
-import org.apache.doris.catalog.FunctionSignature;
-import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.trees.expressions.Expression;
-import org.apache.doris.nereids.trees.expressions.functions.ExplicitlyCastableSignature;
-import org.apache.doris.nereids.trees.expressions.literal.Literal;
-import org.apache.doris.nereids.trees.expressions.shape.TernaryExpression;
 import org.apache.doris.nereids.trees.expressions.visitor.ExpressionVisitor;
-import org.apache.doris.nereids.types.BigIntType;
-import org.apache.doris.nereids.types.DataType;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 
 import java.util.List;
 
 /**
  * Window function: Lead()
  */
-public class Lead extends WindowFunction implements TernaryExpression, ExplicitlyCastableSignature,
-        RequireTrivialTypes {
-
-    static {
-        List<FunctionSignature> signatures = Lists.newArrayList();
-        trivialTypes.forEach(t ->
-                signatures.add(FunctionSignature.ret(t).args(t, BigIntType.INSTANCE, t))
-        );
-        SIGNATURES = ImmutableList.copyOf(signatures);
-    }
-
-    private static final List<FunctionSignature> SIGNATURES;
+public class Lead extends LeadOrLag {
 
     public Lead(Expression child, Expression offset, Expression defaultValue) {
         super("lead", child, offset, defaultValue);
@@ -57,68 +37,14 @@ public class Lead extends WindowFunction implements TernaryExpression, Explicitl
         super("lead", children);
     }
 
-    public Expression getOffset() {
-        if (children().size() <= 1) {
-            throw new AnalysisException("Not set offset of Lead(): " + this.toSql());
-        }
-        return child(1);
-    }
-
-    public Expression getDefaultValue() {
-        if (children.size() <= 2) {
-            throw new AnalysisException("Not set default value of Lead(): " + this.toSql());
-        }
-        return child(2);
-    }
-
-    @Override
-    public boolean nullable() {
-        if (children.size() == 3 && child(2).nullable()) {
-            return true;
-        }
-        return child(0).nullable();
-    }
-
     @Override
     public <R, C> R accept(ExpressionVisitor<R, C> visitor, C context) {
         return visitor.visitLead(this, context);
     }
 
     @Override
-    public void checkLegalityBeforeTypeCoercion() {
-        if (children().size() == 1) {
-            return;
-        }
-        if (children().size() >= 2) {
-            checkValidParams(getOffset(), true);
-            if (getOffset() instanceof Literal) {
-                if (((Literal) getOffset()).getDouble() < 0) {
-                    throw new AnalysisException(
-                            "The offset parameter of LEAD must be a constant positive integer: " + this.toSql());
-                }
-            } else {
-                throw new AnalysisException(
-                    "The offset parameter of LAG must be a constant positive integer: " + this.toSql());
-            }
-            if (children().size() >= 3) {
-                checkValidParams(getDefaultValue(), false);
-            }
-        }
-    }
-
-    @Override
-    public List<FunctionSignature> getSignatures() {
-        return SIGNATURES;
-    }
-
-    @Override
     public Lead withChildren(List<Expression> children) {
         Preconditions.checkArgument(children.size() >= 1 && children.size() <= 3);
         return new Lead(children);
-    }
-
-    @Override
-    public DataType getDataType() {
-        return child(0).getDataType();
     }
 }
