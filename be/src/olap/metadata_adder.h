2b0e5f3defe37d5169f7baf2211cdba2f5b63a3c
Xinyi Zou <zouxinyi@selectdb.com>
2024-11-27 21:19:17 +0800

    [opt](memory) Process profile add metadata profile (#44647)
    ### What problem does this PR solve?
    
    Problem Summary:
    
    http://127.0.0.1:8040/profile
    ```
    ObjectHeapDump:
       - AllRowsetsMemBytes: 16.33 MB
       - AllRowsetsNum: 76.45K
       - AllSegmentsMemBytes: 4.16 KB
       - AllSegmentsNum: 1
       - BitmapIndexReaderMemBytes: 0
       - BitmapIndexReaderNum: 0
       - BloomFilterIndexReaderMemBytes: 0
       - BloomFilterIndexReaderNum: 0
       - ColumnReaderMemBytes: 0
       - ColumnReaderNum: 0
       - IndexPageReaderMemBytes: 0
       - IndexPageReaderNum: 0
       - IndexedColumnReaderMemBytes: 0
       - IndexedColumnReaderNum: 0
       - InvertedIndexReaderMemBytes: 0
       - InvertedIndexReaderNum: 0
       - OrdinalIndexReaderMemBytes: 0
       - OrdinalIndexReaderNum: 0
       - RowsetMetaMemBytes: 45.47 MB
       - RowsetMetaNum: 76.45K
       - TabletColumnMemBytes: 253.28 MB
       - TabletColumnNum: 922.18K
       - TabletIndexMemBytes: 16.78 MB
       - TabletIndexNum: 104.74K
       - TabletMetaMemBytes: 17.50 MB
       - TabletMetaNum: 45.89K
       - TabletSchemaMemBytes: 60.15 MB
       - TabletSchemaNum: 49.19K
       - ZoneMapIndexReaderMemBytes: 0
       - ZoneMapIndexReaderNum: 0
     ```

--- be/src/olap/metadata_adder.h
+++ be/src/olap/metadata_adder.h
@@ -20,10 +20,15 @@
 #include <bvar/bvar.h>
 #include <stdint.h>
 
+#include "util/runtime_profile.h"
+
 namespace doris {
 
 inline bvar::Adder<int64_t> g_rowset_meta_mem_bytes("doris_rowset_meta_mem_bytes");
 inline bvar::Adder<int64_t> g_rowset_meta_num("doris_rowset_meta_num");
+
+inline bvar::Adder<int64_t> g_all_rowsets_mem_bytes("doris_all_rowsets_mem_bytes");
+inline bvar::Adder<int64_t> g_all_rowsets_num("doris_all_rowsets_num");
 
 inline bvar::Adder<int64_t> g_tablet_meta_mem_bytes("doris_tablet_meta_mem_bytes");
 inline bvar::Adder<int64_t> g_tablet_meta_num("doris_tablet_meta_num");
@@ -37,8 +42,8 @@ inline bvar::Adder<int64_t> g_tablet_index_num("doris_tablet_index_num");
 inline bvar::Adder<int64_t> g_tablet_schema_mem_bytes("doris_tablet_schema_mem_bytes");
 inline bvar::Adder<int64_t> g_tablet_schema_num("doris_tablet_schema_num");
 
-inline bvar::Adder<int64_t> g_segment_mem_bytes("doris_segment_mem_bytes");
-inline bvar::Adder<int64_t> g_segment_num("doris_segment_num");
+inline bvar::Adder<int64_t> g_all_segments_mem_bytes("doris_all_segments_mem_bytes");
+inline bvar::Adder<int64_t> g_all_segments_num("doris_all_segments_num");
 
 inline bvar::Adder<int64_t> g_column_reader_mem_bytes("doris_column_reader_mem_bytes");
 inline bvar::Adder<int64_t> g_column_reader_num("doris_column_reader_num");
@@ -104,6 +109,8 @@ class MetadataAdder {
 public:
     MetadataAdder();
 
+    static void dump_metadata_object(RuntimeProfile* object_heap_dump_snapshot);
+
 protected:
     MetadataAdder(const MetadataAdder& other);
 
@@ -159,6 +166,8 @@ void MetadataAdder<T>::add_mem_size(int64_t val) {
     }
     if constexpr (std::is_same_v<T, RowsetMeta>) {
         g_rowset_meta_mem_bytes << val;
+    } else if constexpr (std::is_same_v<T, Rowset>) {
+        g_all_rowsets_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, TabletMeta>) {
         g_tablet_meta_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, TabletColumn>) {
@@ -168,7 +177,7 @@ void MetadataAdder<T>::add_mem_size(int64_t val) {
     } else if constexpr (std::is_same_v<T, TabletSchema>) {
         g_tablet_schema_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, segment_v2::Segment>) {
-        g_segment_mem_bytes << val;
+        g_all_segments_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, segment_v2::ColumnReader>) {
         g_column_reader_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, segment_v2::BitmapIndexReader>) {
@@ -185,6 +194,9 @@ void MetadataAdder<T>::add_mem_size(int64_t val) {
         g_ordinal_index_reader_mem_bytes << val;
     } else if constexpr (std::is_same_v<T, segment_v2::ZoneMapIndexReader>) {
         g_zone_map_index_reader_mem_bytes << val;
+    } else {
+        LOG(FATAL) << "add_mem_size not match class type: " << typeid(T).name() << ", " << val;
+        __builtin_unreachable();
     }
 }
 
@@ -195,6 +207,8 @@ void MetadataAdder<T>::add_num(int64_t val) {
     }
     if constexpr (std::is_same_v<T, RowsetMeta>) {
         g_rowset_meta_num << val;
+    } else if constexpr (std::is_same_v<T, Rowset>) {
+        g_all_rowsets_num << val;
     } else if constexpr (std::is_same_v<T, TabletMeta>) {
         g_tablet_meta_num << val;
     } else if constexpr (std::is_same_v<T, TabletColumn>) {
@@ -204,7 +218,7 @@ void MetadataAdder<T>::add_num(int64_t val) {
     } else if constexpr (std::is_same_v<T, TabletSchema>) {
         g_tablet_schema_num << val;
     } else if constexpr (std::is_same_v<T, segment_v2::Segment>) {
-        g_segment_num << val;
+        g_all_segments_num << val;
     } else if constexpr (std::is_same_v<T, segment_v2::ColumnReader>) {
         g_column_reader_num << val;
     } else if constexpr (std::is_same_v<T, segment_v2::BitmapIndexReader>) {
@@ -221,7 +235,123 @@ void MetadataAdder<T>::add_num(int64_t val) {
         g_ordinal_index_reader_num << val;
     } else if constexpr (std::is_same_v<T, segment_v2::ZoneMapIndexReader>) {
         g_zone_map_index_reader_num << val;
+    } else {
+        LOG(FATAL) << "add_num not match class type: " << typeid(T).name() << ", " << val;
+        __builtin_unreachable();
     }
+}
+
+template <typename T>
+void MetadataAdder<T>::dump_metadata_object(RuntimeProfile* object_heap_dump_snapshot) {
+    RuntimeProfile::Counter* rowset_meta_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "RowsetMetaMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* rowset_meta_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "RowsetMetaNum", TUnit::UNIT);
+    COUNTER_SET(rowset_meta_mem_bytes_counter, g_rowset_meta_mem_bytes.get_value());
+    COUNTER_SET(rowset_meta_num_counter, g_rowset_meta_num.get_value());
+
+    RuntimeProfile::Counter* all_rowsets_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "AllRowsetsMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* all_rowsets_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "AllRowsetsNum", TUnit::UNIT);
+    COUNTER_SET(all_rowsets_mem_bytes_counter, g_all_rowsets_mem_bytes.get_value());
+    COUNTER_SET(all_rowsets_num_counter, g_all_rowsets_num.get_value());
+
+    RuntimeProfile::Counter* tablet_meta_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletMetaMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* tablet_meta_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletMetaNum", TUnit::UNIT);
+    COUNTER_SET(tablet_meta_mem_bytes_counter, g_tablet_meta_mem_bytes.get_value());
+    COUNTER_SET(tablet_meta_num_counter, g_tablet_meta_num.get_value());
+
+    RuntimeProfile::Counter* tablet_column_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletColumnMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* tablet_column_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletColumnNum", TUnit::UNIT);
+    COUNTER_SET(tablet_column_mem_bytes_counter, g_tablet_column_mem_bytes.get_value());
+    COUNTER_SET(tablet_column_num_counter, g_tablet_column_num.get_value());
+
+    RuntimeProfile::Counter* tablet_index_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletIndexMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* tablet_index_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletIndexNum", TUnit::UNIT);
+    COUNTER_SET(tablet_index_mem_bytes_counter, g_tablet_index_mem_bytes.get_value());
+    COUNTER_SET(tablet_index_num_counter, g_tablet_index_num.get_value());
+
+    RuntimeProfile::Counter* tablet_schema_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletSchemaMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* tablet_schema_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "TabletSchemaNum", TUnit::UNIT);
+    COUNTER_SET(tablet_schema_mem_bytes_counter, g_tablet_schema_mem_bytes.get_value());
+    COUNTER_SET(tablet_schema_num_counter, g_tablet_schema_num.get_value());
+
+    RuntimeProfile::Counter* all_segments_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "AllSegmentsMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* all_segments_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "AllSegmentsNum", TUnit::UNIT);
+    COUNTER_SET(all_segments_mem_bytes_counter, g_all_segments_mem_bytes.get_value());
+    COUNTER_SET(all_segments_num_counter, g_all_segments_num.get_value());
+
+    RuntimeProfile::Counter* column_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "ColumnReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* column_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "ColumnReaderNum", TUnit::UNIT);
+    COUNTER_SET(column_reader_mem_bytes_counter, g_column_reader_mem_bytes.get_value());
+    COUNTER_SET(column_reader_num_counter, g_column_reader_num.get_value());
+
+    RuntimeProfile::Counter* bitmap_index_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "BitmapIndexReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* bitmap_index_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "BitmapIndexReaderNum", TUnit::UNIT);
+    COUNTER_SET(bitmap_index_reader_mem_bytes_counter, g_bitmap_index_reader_mem_bytes.get_value());
+    COUNTER_SET(bitmap_index_reader_num_counter, g_bitmap_index_reader_num.get_value());
+
+    RuntimeProfile::Counter* bloom_filter_index_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "BloomFilterIndexReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* filter_index_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "BloomFilterIndexReaderNum", TUnit::UNIT);
+    COUNTER_SET(bloom_filter_index_reader_mem_bytes_counter,
+                g_bloom_filter_index_reader_mem_bytes.get_value());
+    COUNTER_SET(filter_index_reader_num_counter, g_bloom_filter_index_reader_num.get_value());
+
+    RuntimeProfile::Counter* index_page_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "IndexPageReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* index_page_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "IndexPageReaderNum", TUnit::UNIT);
+    COUNTER_SET(index_page_reader_mem_bytes_counter, g_index_page_reader_mem_bytes.get_value());
+    COUNTER_SET(index_page_reader_num_counter, g_index_page_reader_num.get_value());
+
+    RuntimeProfile::Counter* indexed_column_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "IndexedColumnReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* indexed_column_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "IndexedColumnReaderNum", TUnit::UNIT);
+    COUNTER_SET(indexed_column_reader_mem_bytes_counter,
+                g_indexed_column_reader_mem_bytes.get_value());
+    COUNTER_SET(indexed_column_reader_num_counter, g_indexed_column_reader_num.get_value());
+
+    RuntimeProfile::Counter* inverted_index_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "InvertedIndexReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* inverted_index_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "InvertedIndexReaderNum", TUnit::UNIT);
+    COUNTER_SET(inverted_index_reader_mem_bytes_counter,
+                g_inverted_index_reader_mem_bytes.get_value());
+    COUNTER_SET(inverted_index_reader_num_counter, g_inverted_index_reader_num.get_value());
+
+    RuntimeProfile::Counter* ordinal_index_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "OrdinalIndexReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* ordinal_index_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "OrdinalIndexReaderNum", TUnit::UNIT);
+    COUNTER_SET(ordinal_index_reader_mem_bytes_counter,
+                g_ordinal_index_reader_mem_bytes.get_value());
+    COUNTER_SET(ordinal_index_reader_num_counter, g_ordinal_index_reader_num.get_value());
+
+    RuntimeProfile::Counter* zone_map_index_reader_mem_bytes_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "ZoneMapIndexReaderMemBytes", TUnit::BYTES);
+    RuntimeProfile::Counter* zone_map_index_reader_num_counter =
+            ADD_COUNTER(object_heap_dump_snapshot, "ZoneMapIndexReaderNum", TUnit::UNIT);
+    COUNTER_SET(zone_map_index_reader_mem_bytes_counter,
+                g_zone_map_index_reader_mem_bytes.get_value());
+    COUNTER_SET(zone_map_index_reader_num_counter, g_zone_map_index_reader_num.get_value());
 }
 
 }; // namespace doris
