apache/doris

commit 1dbd1469b4a531bdc862a42dd4a95833f4eba5fe

[Improvement](join) support single column nullable join hash table context (#42398)
## Proposed changes
support single column nullable join hash table context

![QQ_1729828619996](https://github.com/user-attachments/assets/477353c6-b27b-4cb0-a9af-0773c70f4819)

be/src/pipeline/exec/hashjoin_build_sink.h
@@ -56,7 +56,8 @@ public:
 
 protected:
     Status _hash_table_init(RuntimeState* state);
-    void _set_build_ignore_flag(vectorized::Block& block, const std::vector<int>& res_col_ids);
+    void _set_build_side_has_external_nullmap(vectorized::Block& block,
+                                              const std::vector<int>& res_col_ids);
     Status _do_evaluate(vectorized::Block& block, vectorized::VExprContextSPtrs& exprs,
                         RuntimeProfile::Counter& expr_call_timer, std::vector<int>& res_col_ids);
     std::vector<uint16_t> _convert_block_to_null(vectorized::Block& block);
@@ -79,7 +80,6 @@ protected:
 
     vectorized::MutableBlock _build_side_mutable_block;
     std::shared_ptr<VRuntimeFilterSlots> _runtime_filter_slots;
-    bool _has_set_need_null_map_for_build = false;
 
     /*
      * The comparison result of a null value with any other value is null,
@@ -87,7 +87,7 @@ protected:
      * the result of an equality condition involving null should be false,
      * so null does not need to be added to the hash table.
      */
-    bool _build_side_ignore_null = false;
+    bool _build_side_has_external_nullmap = false;
     std::vector<int> _build_col_ids;
     std::shared_ptr<CountedFinishDependency> _finish_dependency;
 
@@ -154,12 +154,10 @@ private:
     // build expr
     vectorized::VExprContextSPtrs _build_expr_ctxs;
     // mark the build hash table whether it needs to store null value
-    std::vector<bool> _store_null_in_hash_table;
+    std::vector<bool> _serialize_null_into_key;
 
     // mark the join column whether support null eq
     std::vector<bool> _is_null_safe_eq_join;
-
-    std::vector<bool> _should_convert_to_nullable;
 
     bool _is_broadcast_join = false;
     std::shared_ptr<vectorized::SharedHashTableController> _shared_hashtable_controller;
@@ -184,11 +182,10 @@ struct ProcessHashTableBuild {
               _batch_size(batch_size),
               _state(state) {}
 
-    template <int JoinOpType, bool ignore_null, bool short_circuit_for_null,
-              bool with_other_conjuncts>
+    template <int JoinOpType, bool short_circuit_for_null, bool with_other_conjuncts>
     Status run(HashTableContext& hash_table_ctx, vectorized::ConstNullMapPtr null_map,
                bool* has_null_key) {
-        if (short_circuit_for_null || ignore_null) {
+        if (null_map) {
             // first row is mocked and is null
             // TODO: Need to test the for loop. break may better
             for (uint32_t i = 1; i < _rows; i++) {
@@ -208,8 +205,21 @@ struct ProcessHashTableBuild {
         hash_table_ctx.init_serialized_keys(_build_raw_ptrs, _rows,
                                             null_map ? null_map->data() : nullptr, true, true,
                                             hash_table_ctx.hash_table->get_bucket_size());
-        hash_table_ctx.hash_table->template build<JoinOpType, with_other_conjuncts>(
-                hash_table_ctx.keys, hash_table_ctx.bucket_nums.data(), _rows);
+        // only 2 cases need to access the null value in hash table
+        bool keep_null_key = false;
+        if ((JoinOpType == TJoinOp::NULL_AWARE_LEFT_ANTI_JOIN ||
+             JoinOpType == TJoinOp::NULL_AWARE_LEFT_SEMI_JOIN) &&
+            with_other_conjuncts) {
+            //null aware join with other conjuncts
+            keep_null_key = true;
+        } else if (_parent->_shared_state->is_null_safe_eq_join.size() == 1 &&
+                   _parent->_shared_state->is_null_safe_eq_join[0]) {
+            // single null safe eq
+            keep_null_key = true;
+        }
+
+        hash_table_ctx.hash_table->build(hash_table_ctx.keys, hash_table_ctx.bucket_nums.data(),
+                                         _rows, keep_null_key);
         hash_table_ctx.bucket_nums.resize(_batch_size);
         hash_table_ctx.bucket_nums.shrink_to_fit();
 

be/src/pipeline/exec/join/process_hash_table_probe_impl.h
@@ -187,8 +187,8 @@ typename HashTableType::State ProcessHashTableProbe<JoinOpType>::_init_probe_sid
 }
 
 template <int JoinOpType>
-template <bool need_null_map_for_probe, bool ignore_null, typename HashTableType,
-          bool with_other_conjuncts, bool is_mark_join>
+template <bool need_judge_null, typename HashTableType, bool with_other_conjuncts,
+          bool is_mark_join>
 Status ProcessHashTableProbe<JoinOpType>::do_process(HashTableType& hash_table_ctx,
                                                      vectorized::ConstNullMapPtr null_map,
                                                      vectorized::MutableBlock& mutable_block,
@@ -206,8 +206,8 @@ Status ProcessHashTableProbe<JoinOpType>::do_process(HashTableType& hash_table_c
         SCOPED_TIMER(_init_probe_side_timer);
         _init_probe_side<HashTableType>(
                 hash_table_ctx, probe_rows, with_other_conjuncts,
-                need_null_map_for_probe ? null_map->data() : nullptr,
-                need_null_map_for_probe && ignore_null &&
+                null_map ? null_map->data() : nullptr,
+                need_judge_null &&
                         (JoinOpType == doris::TJoinOp::LEFT_ANTI_JOIN ||
                          JoinOpType == doris::TJoinOp::LEFT_SEMI_JOIN ||
                          JoinOpType == doris::TJoinOp::NULL_AWARE_LEFT_ANTI_JOIN ||
@@ -255,14 +255,12 @@ Status ProcessHashTableProbe<JoinOpType>::do_process(HashTableType& hash_table_c
         }
     } else {
         SCOPED_TIMER(_search_hashtable_timer);
-        auto [new_probe_idx, new_build_idx,
-              new_current_offset] = hash_table_ctx.hash_table->template find_batch < JoinOpType,
-              with_other_conjuncts, is_mark_join,
-              need_null_map_for_probe &&
-                      ignore_null > (hash_table_ctx.keys, hash_table_ctx.bucket_nums.data(),
-                                     probe_index, build_index, cast_set<int32_t>(probe_rows),
-                                     _probe_indexs.data(), _probe_visited, _build_indexs.data(),
-                                     has_mark_join_conjunct);
+        auto [new_probe_idx, new_build_idx, new_current_offset] =
+                hash_table_ctx.hash_table->template find_batch<JoinOpType, with_other_conjuncts,
+                                                               is_mark_join, need_judge_null>(
+                        hash_table_ctx.keys, hash_table_ctx.bucket_nums.data(), probe_index,
+                        build_index, cast_set<int32_t>(probe_rows), _probe_indexs.data(),
+                        _probe_visited, _build_indexs.data(), has_mark_join_conjunct);
         probe_index = new_probe_idx;
         build_index = new_build_idx;
         current_offset = new_current_offset;
@@ -675,7 +673,7 @@ Status ProcessHashTableProbe<JoinOpType>::process_data_in_hashtable(
 }
 
 template <int JoinOpType>
-template <bool need_null_map_for_probe, bool ignore_null, typename HashTableType>
+template <bool need_judge_null, typename HashTableType>
 Status ProcessHashTableProbe<JoinOpType>::process(HashTableType& hash_table_ctx,
                                                   vectorized::ConstNullMapPtr null_map,
                                                   vectorized::MutableBlock& mutable_block,
@@ -685,9 +683,9 @@ Status ProcessHashTableProbe<JoinOpType>::process(HashTableType& hash_table_ctx,
     Status res;
     std::visit(
             [&](auto is_mark_join, auto have_other_join_conjunct) {
-                res = do_process<need_null_map_for_probe, ignore_null, HashTableType,
-                                 have_other_join_conjunct, is_mark_join>(
-                        hash_table_ctx, null_map, mutable_block, output_block, probe_rows);
+                res = do_process<need_judge_null, HashTableType, have_other_join_conjunct,
+                                 is_mark_join>(hash_table_ctx, null_map, mutable_block,
+                                               output_block, probe_rows);
             },
             vectorized::make_bool_variant(is_mark_join),
             vectorized::make_bool_variant(have_other_join_conjunct));
@@ -703,27 +701,14 @@ struct ExtractType<T(U)> {
 };
 
 #define INSTANTIATION(JoinOpType, T)                                                               \
-    template Status                                                                                \
-    ProcessHashTableProbe<JoinOpType>::process<false, false, ExtractType<void(T)>::Type>(          \
+    template Status ProcessHashTableProbe<JoinOpType>::process<false, ExtractType<void(T)>::Type>( \
             ExtractType<void(T)>::Type & hash_table_ctx, vectorized::ConstNullMapPtr null_map,     \
             vectorized::MutableBlock & mutable_block, vectorized::Block * output_block,            \
             uint32_t probe_rows, bool is_mark_join, bool have_other_join_conjunct);                \
-    template Status                                                                                \
-    ProcessHashTableProbe<JoinOpType>::process<false, true, ExtractType<void(T)>::Type>(           \
+    template Status ProcessHashTableProbe<JoinOpType>::process<true, ExtractType<void(T)>::Type>(  \
             ExtractType<void(T)>::Type & hash_table_ctx, vectorized::ConstNullMapPtr null_map,     \
             vectorized::MutableBlock & mutable_block, vectorized::Block * output_block,            \
             uint32_t probe_rows, bool is_mark_join, bool have_other_join_conjunct);                \
-    template Status                                                                                \
-    ProcessHashTableProbe<JoinOpType>::process<true, false, ExtractType<void(T)>::Type>(           \
-            ExtractType<void(T)>::Type & hash_table_ctx, vectorized::ConstNullMapPtr null_map,     \
-            vectorized::MutableBlock & mutable_block, vectorized::Block * output_block,            \
-            uint32_t probe_rows, bool is_mark_join, bool have_other_join_conjunct);                \
-    template Status                                                                                \
-    ProcessHashTableProbe<JoinOpType>::process<true, true, ExtractType<void(T)>::Type>(            \
-            ExtractType<void(T)>::Type & hash_table_ctx, vectorized::ConstNullMapPtr null_map,     \
-            vectorized::MutableBlock & mutable_block, vectorized::Block * output_block,            \
-            uint32_t probe_rows, bool is_mark_join, bool have_other_join_conjunct);                \
-                                                                                                   \
     template Status                                                                                \
     ProcessHashTableProbe<JoinOpType>::process_data_in_hashtable<ExtractType<void(T)>::Type>(      \
             ExtractType<void(T)>::Type & hash_table_ctx, vectorized::MutableBlock & mutable_block, \

be/src/pipeline/common/join_utils.h
@@ -80,51 +80,22 @@ struct JoinDataVariants {
             method_variant.emplace<SerializedHashTableContext>();
             break;
         case HashKeyType::int8_key:
-            if (nullable) {
-                method_variant.emplace<I64FixedKeyHashTableContext<nullable>>(
-                        get_key_sizes(data_types));
-            } else {
-                method_variant.emplace<I8HashTableContext>();
-            }
+            method_variant.emplace<I8HashTableContext>();
             break;
         case HashKeyType::int16_key:
-            if (nullable) {
-                method_variant.emplace<I64FixedKeyHashTableContext<nullable>>(
-                        get_key_sizes(data_types));
-            } else {
-                method_variant.emplace<I16HashTableContext>();
-            }
+            method_variant.emplace<I16HashTableContext>();
             break;
         case HashKeyType::int32_key:
-            if (nullable) {
-                method_variant.emplace<I64FixedKeyHashTableContext<nullable>>(
-                        get_key_sizes(data_types));
-            } else {
-                method_variant.emplace<I32HashTableContext>();
-            }
+            method_variant.emplace<I32HashTableContext>();
             break;
         case HashKeyType::int64_key:
-            if (nullable) {
-                method_variant.emplace<I128FixedKeyHashTableContext<nullable>>(
-                        get_key_sizes(data_types));
-            } else {
-                method_variant.emplace<I64HashTableContext>();
-            }
+            method_variant.emplace<I64HashTableContext>();
             break;
         case HashKeyType::int128_key:
-            if (nullable) {
-                method_variant.emplace<I136FixedKeyHashTableContext<nullable>>(
-                        get_key_sizes(data_types));
-            } else {
-                method_variant.emplace<I128HashTableContext>();
-            }
+            method_variant.emplace<I128HashTableContext>();
             break;
         case HashKeyType::int256_key:
-            if (nullable) {
-                method_variant.emplace<SerializedHashTableContext>();
-            } else {
-                method_variant.emplace<I256HashTableContext>();
-            }
+            method_variant.emplace<I256HashTableContext>();
             break;
         case HashKeyType::string_key:
             method_variant.emplace<MethodOneString>();

be/src/pipeline/dependency.h
@@ -606,8 +606,9 @@ struct HashJoinSharedState : public JoinSharedState {
     ENABLE_FACTORY_CREATOR(HashJoinSharedState)
     // mark the join column whether support null eq
     std::vector<bool> is_null_safe_eq_join;
+
     // mark the build hash table whether it needs to store null value
-    std::vector<bool> store_null_in_hash_table;
+    std::vector<bool> serialize_null_into_key;
     std::shared_ptr<vectorized::Arena> arena = std::make_shared<vectorized::Arena>();
 
     // maybe share hash table with other fragment instances

be/src/pipeline/exec/hashjoin_build_sink.cpp
@@ -43,7 +43,7 @@ Status HashJoinBuildSinkLocalState::init(RuntimeState* state, LocalSinkStateInfo
     _shared_state->join_op_variants = p._join_op_variants;
 
     _shared_state->is_null_safe_eq_join = p._is_null_safe_eq_join;
-    _shared_state->store_null_in_hash_table = p._store_null_in_hash_table;
+    _shared_state->serialize_null_into_key = p._serialize_null_into_key;
     _build_expr_ctxs.resize(p._build_expr_ctxs.size());
     for (size_t i = 0; i < _build_expr_ctxs.size(); i++) {
         RETURN_IF_ERROR(p._build_expr_ctxs[i]->clone(state, _build_expr_ctxs[i]));
@@ -227,33 +227,22 @@ Status HashJoinBuildSinkLocalState::_extract_join_column(
         vectorized::Block& block, vectorized::ColumnUInt8::MutablePtr& null_map,
         vectorized::ColumnRawPtrs& raw_ptrs, const std::vector<int>& res_col_ids) {
     auto& shared_state = *_shared_state;
-    auto& p = _parent->cast<HashJoinBuildSinkOperatorX>();
     for (size_t i = 0; i < shared_state.build_exprs_size; ++i) {
-        if (p._should_convert_to_nullable[i]) {
+        const auto* column = block.get_by_position(res_col_ids[i]).column.get();
+        if (!column->is_nullable() && shared_state.serialize_null_into_key[i]) {
             _key_columns_holder.emplace_back(
                     vectorized::make_nullable(block.get_by_position(res_col_ids[i]).column));
             raw_ptrs[i] = _key_columns_holder.back().get();
-            continue;
-        }
-
-        if (shared_state.is_null_safe_eq_join[i]) {
-            raw_ptrs[i] = block.get_by_position(res_col_ids[i]).column.get();
+        } else if (const auto* nullable = check_and_get_column<vectorized::ColumnNullable>(*column);
+                   !shared_state.serialize_null_into_key[i] && nullable) {
+            // update nulllmap and split nested out of ColumnNullable when serialize_null_into_key is false and column is nullable
+            const auto& col_nested = nullable->get_nested_column();
+            const auto& col_nullmap = nullable->get_null_map_data();
+            DCHECK(null_map != nullptr);
+            vectorized::VectorizedUtils::update_null_map(null_map->get_data(), col_nullmap);
+            raw_ptrs[i] = &col_nested;
         } else {
-            const auto* column = block.get_by_position(res_col_ids[i]).column.get();
-            if (const auto* nullable = check_and_get_column<vectorized::ColumnNullable>(*column)) {
-                const auto& col_nested = nullable->get_nested_column();
-                const auto& col_nullmap = nullable->get_null_map_data();
-
-                if (shared_state.store_null_in_hash_table[i]) {
-                    raw_ptrs[i] = nullable;
-                } else {
-                    DCHECK(null_map != nullptr);
-                    vectorized::VectorizedUtils::update_null_map(null_map->get_data(), col_nullmap);
-                    raw_ptrs[i] = &col_nested;
-                }
-            } else {
-                raw_ptrs[i] = column;
-            }
+            raw_ptrs[i] = column;
         }
     }
     return Status::OK();
@@ -284,13 +273,9 @@ Status HashJoinBuildSinkLocalState::process_build_block(RuntimeState* state,
                     .data()[0] = 1;
         }
     }
-    // TODO: Now we are not sure whether a column is nullable only by ExecNode's `row_desc`
-    //  so we have to initialize this flag by the first build block.
-    if (!_has_set_need_null_map_for_build) {
-        _has_set_need_null_map_for_build = true;
-        _set_build_ignore_flag(block, _build_col_ids);
-    }
-    if (p._short_circuit_for_null_in_build_side || _build_side_ignore_null) {
+
+    _set_build_side_has_external_nullmap(block, _build_col_ids);
+    if (_build_side_has_external_nullmap) {
         null_map_val = vectorized::ColumnUInt8::create();
         null_map_val->get_data().assign(rows, (uint8_t)0);
     }
@@ -300,27 +285,23 @@ Status HashJoinBuildSinkLocalState::process_build_block(RuntimeState* state,
 
     st = std::visit(
             vectorized::Overload {
-                    [&](std::monostate& arg, auto join_op, auto has_null_value,
+                    [&](std::monostate& arg, auto join_op,
                         auto short_circuit_for_null_in_build_side,
                         auto with_other_conjuncts) -> Status {
                         LOG(FATAL) << "FATAL: uninited hash table";
                         __builtin_unreachable();
                         return Status::OK();
                     },
-                    [&](auto&& arg, auto&& join_op, auto has_null_value,
-                        auto short_circuit_for_null_in_build_side,
+                    [&](auto&& arg, auto&& join_op, auto short_circuit_for_null_in_build_side,
                         auto with_other_conjuncts) -> Status {
                         using HashTableCtxType = std::decay_t<decltype(arg)>;
                         using JoinOpType = std::decay_t<decltype(join_op)>;
                         ProcessHashTableBuild<HashTableCtxType> hash_table_build_process(
                                 rows, raw_ptrs, this, state->batch_size(), state);
                         auto st = hash_table_build_process.template run<
-                                JoinOpType::value, has_null_value,
-                                short_circuit_for_null_in_build_side, with_other_conjuncts>(
-                                arg,
-                                has_null_value || short_circuit_for_null_in_build_side
-                                        ? &null_map_val->get_data()
-                                        : nullptr,
+                                JoinOpType::value, short_circuit_for_null_in_build_side,
+                                with_other_conjuncts>(
+                                arg, null_map_val ? &null_map_val->get_data() : nullptr,
                                 &_shared_state->_has_null_in_build_side);
                         COUNTER_SET(_memory_used_counter,
                                     _build_blocks_memory_usage->value() +
@@ -330,22 +311,24 @@ Status HashJoinBuildSinkLocalState::process_build_block(RuntimeState* state,
                         return st;
                     }},
             _shared_state->hash_table_variants->method_variant, _shared_state->join_op_variants,
-            vectorized::make_bool_variant(_build_side_ignore_null),
             vectorized::make_bool_variant(p._short_circuit_for_null_in_build_side),
             vectorized::make_bool_variant((p._have_other_join_conjunct)));
 
     return st;
 }
 
-void HashJoinBuildSinkLocalState::_set_build_ignore_flag(vectorized::Block& block,
-                                                         const std::vector<int>& res_col_ids) {
+void HashJoinBuildSinkLocalState::_set_build_side_has_external_nullmap(
+        vectorized::Block& block, const std::vector<int>& res_col_ids) {
     auto& p = _parent->cast<HashJoinBuildSinkOperatorX>();
+    if (p._short_circuit_for_null_in_build_side) {
+        _build_side_has_external_nullmap = true;
+        return;
+    }
     for (size_t i = 0; i < _build_expr_ctxs.size(); ++i) {
-        if (!_shared_state->is_null_safe_eq_join[i] && !p._short_circuit_for_null_in_build_side) {
-            const auto* column = block.get_by_position(res_col_ids[i]).column.get();
-            if (check_and_get_column<vectorized::ColumnNullable>(*column)) {
-                _build_side_ignore_null |= !_shared_state->store_null_in_hash_table[i];
-            }
+        const auto* column = block.get_by_position(res_col_ids[i]).column.get();
+        if (column->is_nullable() && !_shared_state->serialize_null_into_key[i]) {
+            _build_side_has_external_nullmap = true;
+            return;
         }
     }
 }
@@ -359,7 +342,7 @@ Status HashJoinBuildSinkLocalState::_hash_table_init(RuntimeState* state) {
 
         /// For 'null safe equal' join,
         /// the build key column maybe be converted to nullable from non-nullable.
-        if (p._should_convert_to_nullable[i]) {
+        if (p._serialize_null_into_key[i]) {
             data_type = vectorized::make_nullable(data_type);
         }
         data_types.emplace_back(std::move(data_type));
@@ -392,10 +375,6 @@ Status HashJoinBuildSinkOperatorX::init(const TPlanNode& tnode, RuntimeState* st
     if (tnode.hash_join_node.__isset.hash_output_slot_ids) {
         _hash_output_slot_ids = tnode.hash_join_node.hash_output_slot_ids;
     }
-
-    const bool build_stores_null = _join_op == TJoinOp::RIGHT_OUTER_JOIN ||
-                                   _join_op == TJoinOp::FULL_OUTER_JOIN ||
-                                   _join_op == TJoinOp::RIGHT_ANTI_JOIN;
 
     const std::vector<TEqJoinCondition>& eq_join_conjuncts = tnode.hash_join_node.eq_join_conjuncts;
     for (const auto& eq_join_conjunct : eq_join_conjuncts) {
@@ -430,16 +409,18 @@ Status HashJoinBuildSinkOperatorX::init(const TPlanNode& tnode, RuntimeState* st
                 (eq_join_conjunct.right.nodes[0].is_nullable ||
                  eq_join_conjunct.left.nodes[0].is_nullable);
 
-        const bool should_convert_to_nullable = is_null_safe_equal &&
-                                                !eq_join_conjunct.right.nodes[0].is_nullable &&
-                                                eq_join_conjunct.left.nodes[0].is_nullable;
         _is_null_safe_eq_join.push_back(is_null_safe_equal);
-        _should_convert_to_nullable.emplace_back(should_convert_to_nullable);
 
-        // if is null aware, build join column and probe join column both need dispose null value
-        _store_null_in_hash_table.emplace_back(
-                is_null_safe_equal ||
-                (_build_expr_ctxs.back()->root()->is_nullable() && build_stores_null));
+        if (eq_join_conjuncts.size() == 1) {
+            // single column key serialize method must use nullmap for represent null to instead serialize null into key
+            _serialize_null_into_key.emplace_back(false);
+        } else if (is_null_safe_equal) {
+            // use serialize null into key to represent multi column null value
+            _serialize_null_into_key.emplace_back(true);
+        } else {
+            // on normal conditions, because null!=null, it can be expressed directly with nullmap.
+            _serialize_null_into_key.emplace_back(false);
+        }
     }
 
     return Status::OK();

commit 805520bfe4d90965bbd844945bd302eff38dc8f3

[Bug](join) fix single null eq get wrong result (#44613)
introduced by https://github.com/apache/doris/pull/42398
In order to make the null keys equal when using single null eq, all null
keys need to be set to default value.

be/src/pipeline/exec/hashjoin_build_sink.h
@@ -197,7 +197,10 @@ struct ProcessHashTableBuild {
         SCOPED_TIMER(_parent->_build_table_insert_timer);
         hash_table_ctx.hash_table->template prepare_build<JoinOpType>(_rows, _batch_size,
                                                                       *has_null_key);
-
+        // In order to make the null keys equal when using single null eq, all null keys need to be set to default value.
+        if (_build_raw_ptrs.size() == 1 && null_map) {
+            _build_raw_ptrs[0]->assume_mutable()->replace_column_null_data(null_map->data());
+        }
         hash_table_ctx.init_serialized_keys(_build_raw_ptrs, _rows,
                                             null_map ? null_map->data() : nullptr, true, true,
                                             hash_table_ctx.hash_table->get_bucket_size());

be/src/pipeline/exec/join/process_hash_table_probe_impl.h
@@ -173,6 +173,10 @@ typename HashTableType::State ProcessHashTableProbe<JoinOpType>::_init_probe_sid
     if (!_parent->_ready_probe) {
         _parent->_ready_probe = true;
         hash_table_ctx.reset();
+        // In order to make the null keys equal when using single null eq, all null keys need to be set to default value.
+        if (_parent->_probe_columns.size() == 1 && null_map) {
+            _parent->_probe_columns[0]->assume_mutable()->replace_column_null_data(null_map);
+        }
         hash_table_ctx.init_serialized_keys(_parent->_probe_columns, probe_rows, null_map, true,
                                             false, hash_table_ctx.hash_table->get_bucket_size());
         hash_table_ctx.hash_table->pre_build_idxs(hash_table_ctx.bucket_nums,

regression-test/data/nereids_p0/join/test_mark_join.out
@@ -53,3 +53,10 @@
 -- !mark_join_null_conjunct --
 \N
 
+-- !mark_join8 --
+1	1	1	false
+2	2	2	false
+3	\N	\N	\N
+3	\N	3	\N
+4	\N	4	true
+

regression-test/data/query_p0/join/test_join.out
@@ -3243,3 +3243,9 @@ false	true	true	false	false
 -- !sql --
 4
 
+-- !sql --
+2	\N
+
+-- !sql --
+2	\N
+

regression-test/suites/nereids_p0/join/test_mark_join.groovy
@@ -142,4 +142,25 @@ suite("test_mark_join", "nereids_p0") {
     """
 
     qt_mark_join_null_conjunct """select null in ( select k1 from test_mark_join_t1);"""
+
+    qt_mark_join8 """
+            select
+                k1,
+                k2,
+                k3,
+                k1 not in (
+                    select
+                        test_mark_join_t2.k2
+                    from
+                        test_mark_join_t2
+                    where
+                        test_mark_join_t2.k3 <=> test_mark_join_t1.k3
+                ) vv
+            from
+                test_mark_join_t1
+            order by
+                1,
+                2,
+                3;
+    """
 }

regression-test/suites/query_p0/join/test_join.groovy
@@ -720,7 +720,7 @@ suite("test_join", "query,p0") {
     }
 
     qt_left_anti_join_with_other_pred "select b.k1 from baseall b left anti join test t on b.k1 = t.k1 and 1 = 2 order by b.k1"
-
+    // null not in (1,2,3,null) = true
     qt_left_anti_join_null_1 "select b.k1 from baseall b left anti join test t on b.k1 = t.k1 order by b.k1"
 
     qt_left_anti_join_null_2 "select b.k1 from baseall b left anti join test_join_empty_view t on b.k1 = t.k1 order by b.k1"
@@ -931,6 +931,7 @@ suite("test_join", "query,p0") {
     // https://github.com/apache/doris/issues/4210
     qt_join_bug3"""select * from baseall t1 where k1 = (select min(k1) from test t2 where t2.k1 = t1.k1 and t2.k2=t1.k2)
            order by k1"""
+    // null not in (1,2,3) = false
     qt_join_bug4"""select b.k1 from baseall b where b.k1 not in( select k1 from baseall where k1 is not null )"""
 
 
@@ -1330,4 +1331,14 @@ suite("test_join", "query,p0") {
     qt_sql """ select /*+SET_VAR(batch_size=1, disable_join_reorder=true)*/ count(DISTINCT dcqewrt.engineer)  as active_person_count from tbl1 dcqewrt left join [broadcast] tbl2 dd on dd.data_dt = dcqewrt.data_dt; """
     sql """ DROP TABLE IF EXISTS tbl2; """
     sql """ DROP TABLE IF EXISTS tbl1; """
+
+
+    sql "drop table if exists t01;"
+    sql "drop table if exists t02;"
+    sql"""create table t01 (id int, a varchar(10)) properties ("replication_num" = "1");"""
+    sql"""create table t02 (id int, b varchar(10)) properties ("replication_num" = "1");"""
+    sql"insert into t01 values (1, 'a'), (2, null), (3, 'c');"
+    sql"insert into t02 values (1, 'b');"
+    qt_sql"select * from t01 where (not like (a, 'a%')) <=> 'b';"
+    qt_sql"select * from t01 where (not like (a, 'a%')) <=> (select max(b) from t02); "
 }