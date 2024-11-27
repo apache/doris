3d04ba52370d6379f95a40cefb17cd3cca594d2b
Chen Zhang <zhangchen@selectdb.com>
2024-11-27 19:55:00 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -2362,6 +2362,9 @@ public class OlapTable extends Table {
         if (tableProperty == null) {
             return false;
         }
+        if (getKeysType() != KeysType.UNIQUE_KEYS) {
+            return false;
+        }
         return tableProperty.getEnableUniqueKeyMergeOnWrite();
     }
 
