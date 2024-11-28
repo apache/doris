a616855deec8b3368efd85921370846230a1a5b3
zhangdong <zhangdong@selectdb.com>
2024-11-28 10:22:20 +0800

    [feat](mtmv)Unified external table interface supporting partition refresh and partition pruning (#44673)
    - Add `MvccTable` to represent a table that supports querying specified
    version data
    - Add the `MvccSnapshot` interface to store snapshot information of mvcc
    at a certain moment in time
    - Add the `MvccSnapshot` parameter to the method of the
    `MTMVRelatedTableIf `interface to retrieve data of a specified version
    - Partition pruning related methods combined with the `MvccSnapshot`
    parameter are used to obtain partition information for a specified
    version
    - Load the snapshot information of mvccTable at the beginning of the
    query plan and store it in StatementContext
    
    Unified external table interface supporting partition refresh and
    partition pruning

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVRelatedTableIf.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVRelatedTableIf.java
@@ -23,9 +23,11 @@ import org.apache.doris.catalog.PartitionType;
 import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.DdlException;
+import org.apache.doris.datasource.mvcc.MvccSnapshot;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -38,31 +40,35 @@ public interface MTMVRelatedTableIf extends TableIf {
      * Note: This method is called every time there is a refresh and transparent rewrite,
      * so if this method is slow, it will significantly reduce query performance
      *
+     * @param snapshot
      * @return partitionName->PartitionItem
      */
-    Map<String, PartitionItem> getAndCopyPartitionItems() throws AnalysisException;
+    Map<String, PartitionItem> getAndCopyPartitionItems(Optional<MvccSnapshot> snapshot) throws AnalysisException;
 
     /**
      * getPartitionType LIST/RANGE/UNPARTITIONED
      *
+     * @param snapshot
      * @return
      */
-    PartitionType getPartitionType();
+    PartitionType getPartitionType(Optional<MvccSnapshot> snapshot);
 
     /**
      * getPartitionColumnNames
      *
+     * @param snapshot
      * @return
      * @throws DdlException
      */
-    Set<String> getPartitionColumnNames() throws DdlException;
+    Set<String> getPartitionColumnNames(Optional<MvccSnapshot> snapshot) throws DdlException;
 
     /**
      * getPartitionColumns
      *
+     * @param snapshot
      * @return
      */
-    List<Column> getPartitionColumns();
+    List<Column> getPartitionColumns(Optional<MvccSnapshot> snapshot);
 
     /**
      * getPartitionSnapshot
@@ -70,12 +76,14 @@ public interface MTMVRelatedTableIf extends TableIf {
      * If snapshots have already been obtained in bulk in the context,
      * the results should be obtained directly from the context
      *
+     * @param snapshot
      * @param partitionName
      * @param context
      * @return partition snapshot at current time
      * @throws AnalysisException
      */
-    MTMVSnapshotIf getPartitionSnapshot(String partitionName, MTMVRefreshContext context) throws AnalysisException;
+    MTMVSnapshotIf getPartitionSnapshot(String partitionName, MTMVRefreshContext context,
+            Optional<MvccSnapshot> snapshot) throws AnalysisException;
 
     /**
      * getTableSnapshot
@@ -83,11 +91,13 @@ public interface MTMVRelatedTableIf extends TableIf {
      * If snapshots have already been obtained in bulk in the context,
      * the results should be obtained directly from the context
      *
+     * @param snapshot
      * @param context
      * @return table snapshot at current time
      * @throws AnalysisException
      */
-    MTMVSnapshotIf getTableSnapshot(MTMVRefreshContext context) throws AnalysisException;
+    MTMVSnapshotIf getTableSnapshot(MTMVRefreshContext context, Optional<MvccSnapshot> snapshot)
+            throws AnalysisException;
 
     /**
      * Does the current type of table allow timed triggering
