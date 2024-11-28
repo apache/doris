57c10393d7bfe86ab0a35e3fa6c8385993b81b93
924060929 <lanhuajian@selectdb.com>
2024-11-28 15:07:57 +0800

    fix

--- regression-test/suites/nereids_p0/sql_functions/datetime_functions/test_date_function.groovy
+++ regression-test/suites/nereids_p0/sql_functions/datetime_functions/test_date_function.groovy
@@ -720,4 +720,17 @@ suite("test_date_function") {
 
 
     qt_sql_time_value """ select  cast(4562632 as time),  hour(cast(4562632 as time)) ,  minute(cast(4562632 as time)) , second(cast(4562632 as time)); """
+
+    def test_simplify = {
+        sql "drop table if exists test_int_date"
+        sql "create table test_int_date(dt int) distributed by hash(dt) properties('replication_num'='1');"
+        test {
+            sql "select months_add(dt, 1) = date '2024-02-29' from (select date '2024-01-31' as dt)a"
+            result([[true]])
+        }
+        test {
+            sql "select years_add(dt, 1) = date '2025-02-28' from (select date '2024-02-29' as dt)a"
+            result([[true]])
+        }
+    }()
 }
