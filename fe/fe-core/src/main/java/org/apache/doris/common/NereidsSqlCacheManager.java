81b2f897537e835c0684b5c3395ccc18b2427cef
tongyang.han <tongyang.han@jiduauto.com>
2024-11-29 17:10:03 +0800

    [fix](cache) fix same sql return wrong result when switch database with `use db` and enable sql cache;

--- fe/fe-core/src/main/java/org/apache/doris/common/NereidsSqlCacheManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/NereidsSqlCacheManager.java
@@ -74,9 +74,11 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
-/** NereidsSqlCacheManager */
+/**
+ * NereidsSqlCacheManager
+ */
 public class NereidsSqlCacheManager {
-    // key: <user>:<sql>
+    // key: <ctl.db>:<user>:<sql>
     // value: SqlCacheContext
     private volatile Cache<String, SqlCacheContext> sqlCaches;
 
@@ -110,7 +112,7 @@ public class NereidsSqlCacheManager {
                 // auto evict cache when jvm memory too low
                 .softValues();
         if (sqlCacheNum > 0) {
-            cacheBuilder = cacheBuilder.maximumSize(sqlCacheNum);
+            cacheBuilder.maximumSize(sqlCacheNum);
         }
         if (expireAfterAccessSeconds > 0) {
             cacheBuilder = cacheBuilder.expireAfterAccess(Duration.ofSeconds(expireAfterAccessSeconds));
@@ -119,7 +121,9 @@ public class NereidsSqlCacheManager {
         return cacheBuilder.build();
     }
 
-    /** tryAddFeCache */
+    /**
+     * tryAddFeCache
+     */
     public void tryAddFeSqlCache(ConnectContext connectContext, String sql) {
         Optional<SqlCacheContext> sqlCacheContextOpt = connectContext.getStatementContext().getSqlCacheContext();
         if (!sqlCacheContextOpt.isPresent()) {
@@ -127,17 +131,18 @@ public class NereidsSqlCacheManager {
         }
 
         SqlCacheContext sqlCacheContext = sqlCacheContextOpt.get();
-        UserIdentity currentUserIdentity = connectContext.getCurrentUserIdentity();
         String key = sqlCacheContext.getCacheKeyType() == CacheKeyType.SQL
-                ? currentUserIdentity.toString() + ":" + normalizeSql(sql.trim())
-                : currentUserIdentity.toString() + ":" + DebugUtil.printId(sqlCacheContext.getOrComputeCacheKeyMd5());
+                ? generateCacheKey(connectContext, normalizeSql(sql))
+                : generateCacheKey(connectContext, DebugUtil.printId(sqlCacheContext.getOrComputeCacheKeyMd5()));
         if (sqlCaches.getIfPresent(key) == null && sqlCacheContext.getOrComputeCacheKeyMd5() != null
                 && sqlCacheContext.getResultSetInFe().isPresent()) {
             sqlCaches.put(key, sqlCacheContext);
         }
     }
 
-    /** tryAddBeCache */
+    /**
+     * tryAddBeCache
+     */
     public void tryAddBeCache(ConnectContext connectContext, String sql, CacheAnalyzer analyzer) {
         Optional<SqlCacheContext> sqlCacheContextOpt = connectContext.getStatementContext().getSqlCacheContext();
         if (!sqlCacheContextOpt.isPresent()) {
@@ -147,10 +152,9 @@ public class NereidsSqlCacheManager {
             return;
         }
         SqlCacheContext sqlCacheContext = sqlCacheContextOpt.get();
-        UserIdentity currentUserIdentity = connectContext.getCurrentUserIdentity();
         String key = sqlCacheContext.getCacheKeyType() == CacheKeyType.SQL
-                ? currentUserIdentity.toString() + ":" + normalizeSql(sql.trim())
-                : currentUserIdentity.toString() + ":" + DebugUtil.printId(sqlCacheContext.getOrComputeCacheKeyMd5());
+                ? generateCacheKey(connectContext, normalizeSql(sql))
+                : generateCacheKey(connectContext, DebugUtil.printId(sqlCacheContext.getOrComputeCacheKeyMd5()));
         if (sqlCaches.getIfPresent(key) == null && sqlCacheContext.getOrComputeCacheKeyMd5() != null) {
             SqlCache cache = (SqlCache) analyzer.getCache();
             sqlCacheContext.setSumOfPartitionNum(cache.getSumOfPartitionNum());
@@ -167,23 +171,23 @@ public class NereidsSqlCacheManager {
         }
     }
 
-    /** tryParseSql */
+    /**
+     * tryParseSql
+     */
     public Optional<LogicalSqlCache> tryParseSql(ConnectContext connectContext, String sql) {
-        UserIdentity currentUserIdentity = connectContext.getCurrentUserIdentity();
-        String key = currentUserIdentity + ":" + normalizeSql(sql.trim());
+        String key = generateCacheKey(connectContext, normalizeSql(sql.trim()));
         SqlCacheContext sqlCacheContext = sqlCaches.getIfPresent(key);
         if (sqlCacheContext == null) {
             return Optional.empty();
         }
 
         // LOG.info("Total size: " + GraphLayout.parseInstance(sqlCacheContext).totalSize());
-
+        UserIdentity currentUserIdentity = connectContext.getCurrentUserIdentity();
         List<Variable> currentVariables = resolveUserVariables(sqlCacheContext);
         if (usedVariablesChanged(currentVariables, sqlCacheContext)) {
             String md5 = DebugUtil.printId(
                     sqlCacheContext.doComputeCacheKeyMd5(Utils.fastToImmutableSet(currentVariables)));
-
-            String md5CacheKey = currentUserIdentity + ":" + md5;
+            String md5CacheKey = generateCacheKey(connectContext, md5);
             SqlCacheContext sqlCacheContextWithVariable = sqlCaches.getIfPresent(md5CacheKey);
 
             // already exist cache in the fe, but the variable is different to this query,
@@ -201,6 +205,15 @@ public class NereidsSqlCacheManager {
         } else {
             return tryParseSqlWithoutCheckVariable(connectContext, key, sqlCacheContext, currentUserIdentity);
         }
+    }
+
+    private String generateCacheKey(ConnectContext connectContext, String sqlOrMd5) {
+        CatalogIf<?> currentCatalog = connectContext.getCurrentCatalog();
+        String currentCatalogName = currentCatalog != null ? currentCatalog.getName() : "";
+        String currentDatabase = connectContext.getDatabase();
+        String currentDatabaseName = currentDatabase != null ? currentDatabase : "";
+        return currentCatalogName + "." + currentDatabaseName + ":" + connectContext.getCurrentUserIdentity().toString()
+                + ":" + sqlOrMd5;
     }
 
     private String normalizeSql(String sql) {
@@ -402,7 +415,7 @@ public class NereidsSqlCacheManager {
             Variable cachedVariable = cachedUsedVariables.get(i);
             if (!Objects.equals(currentVariable, cachedVariable)
                     || cachedVariable.getRealExpression().anyMatch(
-                            expr -> !((ExpressionTrait) expr).isDeterministic())) {
+                        expr -> !((ExpressionTrait) expr).isDeterministic())) {
                 return true;
             }
         }
