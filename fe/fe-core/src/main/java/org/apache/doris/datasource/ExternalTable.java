0ed601b01bd0fb33e0827818d3c71e32328f3807
daidai <changyuwei@selectdb.com>
2024-12-01 20:17:20 +0800

    [fix](maxcompute)add mc catalog  read partition table partition prune (#44508)
    ### What problem does this PR solve?
    
    Based on the `requiredPartitions` API interface of the maxcompute API,
    we implemented partition pruning when reading partitioned tables to
    avoid queries that scan the entire table.

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalTable.java
@@ -377,7 +377,7 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
      * @return
      */
     public SelectedPartitions initSelectedPartitions(Optional<MvccSnapshot> snapshot) {
-        if (!supportPartitionPruned()) {
+        if (!supportInternalPartitionPruned()) {
             return SelectedPartitions.NOT_PRUNED;
         }
         if (CollectionUtils.isEmpty(this.getPartitionColumns(snapshot))) {
@@ -394,7 +394,7 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
      * @param snapshot if not support mvcc, ignore this
      * @return partitionName ==> PartitionItem
      */
-    public Map<String, PartitionItem> getNameToPartitionItems(Optional<MvccSnapshot> snapshot) {
+    protected Map<String, PartitionItem> getNameToPartitionItems(Optional<MvccSnapshot> snapshot) {
         return Collections.emptyMap();
     }
 
@@ -410,11 +410,12 @@ public class ExternalTable implements TableIf, Writable, GsonPostProcessable {
     }
 
     /**
-     * Does it support partition cpruned， If so, this method needs to be overridden in subclasses
+     * Does it support Internal partition pruned, If so, this method needs to be overridden in subclasses
+     * Internal partition pruned : Implement partition pruning logic without relying on external APIs.
      *
      * @return
      */
-    public boolean supportPartitionPruned() {
+    public boolean supportInternalPartitionPruned() {
         return false;
     }
 }
