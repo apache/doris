0bcf46c3cceed1150821fe8e9c3c8b7c83efe502
feiniaofeiafei <moailing@selectdb.com>
2024-12-02 12:00:06 +0800

    [feat](nereids) add rewrite rule :EliminateGroupByKeyByUniform (#43391)
    This PR introduces two main changes:
    1. Adds an optional constant value to the uniform attribute in
    DataTrait. A slot with a constant value that is not null will be
    considered uniform and not null.
    2. Introduces a new transform rule: EliminateGroupByKeyByUniform, which
    utilizes the newly added part of the uniform attribute. Following is
    example transformation:
    
    from
    
     +--aggregate(group by a,b output a,b,max(c))
    (a is uniform and not null: e.g. a is projection 2 as a in
    logicalProject)
    
    to
    
    +--aggregate(group by b output b,any_value(a) as a,max(c))

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateMTMVInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateMTMVInfo.java
@@ -103,7 +103,8 @@ import java.util.stream.Collectors;
  */
 public class CreateMTMVInfo {
     public static final Logger LOG = LogManager.getLogger(CreateMTMVInfo.class);
-    public static final String MTMV_PLANER_DISABLE_RULES = "OLAP_SCAN_PARTITION_PRUNE,PRUNE_EMPTY_PARTITION";
+    public static final String MTMV_PLANER_DISABLE_RULES = "OLAP_SCAN_PARTITION_PRUNE,PRUNE_EMPTY_PARTITION,"
+            + "ELIMINATE_GROUP_BY_KEY_BY_UNIFORM";
     private final boolean ifNotExists;
     private final TableNameInfo mvName;
     private List<String> keys;
