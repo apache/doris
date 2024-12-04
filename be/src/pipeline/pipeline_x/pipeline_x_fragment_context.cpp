1aa97d00127e8de50676bd484adfca1b3b0102cd
Mryange <yanxuecheng@selectdb.com>
2024-12-04 14:04:54 +0800

    pick

--- be/src/pipeline/pipeline_x/pipeline_x_fragment_context.cpp
+++ be/src/pipeline/pipeline_x/pipeline_x_fragment_context.cpp
@@ -930,7 +930,7 @@ Status PipelineXFragmentContext::_add_local_exchange_impl(
         break;
     case ExchangeType::ADAPTIVE_PASSTHROUGH:
         shared_state->exchanger = AdaptivePassthroughExchanger::create_unique(
-                cur_pipe->num_tasks(), _num_instances,
+                std::max(cur_pipe->num_tasks(), _num_instances), _num_instances,
                 _runtime_state->query_options().__isset.local_exchange_free_blocks_limit
                         ? _runtime_state->query_options().local_exchange_free_blocks_limit
                         : 0);
@@ -1032,9 +1032,13 @@ Status PipelineXFragmentContext::_add_local_exchange(
             << " cur_pipe->operator_xs().size(): " << cur_pipe->operator_xs().size()
             << " new_pip->operator_xs().size(): " << new_pip->operator_xs().size();
 
-    // Add passthrough local exchanger if necessary
+    // There are some local shuffles with relatively heavy operations on the sink.
+    // If the local sink concurrency is 1 and the local source concurrency is n, the sink becomes a bottleneck.
+    // Therefore, local passthrough is used to increase the concurrency of the sink.
+    // op -> local sink(1) -> local source (n)
+    // op -> local passthrough(1) -> local passthrough(n) ->  local sink(n) -> local source (n)
     if (cur_pipe->num_tasks() > 1 && new_pip->num_tasks() == 1 &&
-        Pipeline::is_hash_exchange(data_distribution.distribution_type)) {
+        Pipeline::heavy_operations_on_the_sink(data_distribution.distribution_type)) {
         RETURN_IF_ERROR(_add_local_exchange_impl(
                 new_pip->operator_xs().size(), pool, new_pip, add_pipeline(new_pip, pip_idx + 2),
                 DataDistribution(ExchangeType::PASSTHROUGH), do_local_exchange, num_buckets,
