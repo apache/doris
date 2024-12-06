590f015f26eb46ea7dfa031cd3c63b5df580442e
BiteTheDDDDt <xl@selectdb.com>
2024-12-06 15:04:56 +0800

    update

--- be/src/pipeline/exec/hashjoin_build_sink.cpp
+++ be/src/pipeline/exec/hashjoin_build_sink.cpp
@@ -171,8 +171,8 @@ Status HashJoinBuildSinkLocalState::close(RuntimeState* state, Status exec_statu
                     !p._shared_hash_table_context->complete_build_stage)) {
             throw Exception(ErrorCode::INTERNAL_ERROR, "build_sink::close meet error state");
         } else {
-            RETURN_IF_ERROR(local_state._runtime_filter_slots->copy_from_shared_context(
-                    _shared_hash_table_context));
+            RETURN_IF_ERROR(
+                    _runtime_filter_slots->copy_from_shared_context(p._shared_hash_table_context));
         }
 
         SCOPED_TIMER(_publish_runtime_filter_timer);
