b0f33ecb43684dab10a457fdd98783978c8699f2
shee <13843187+qzsee@users.noreply.github.com>
2024-12-09 14:54:21 +0800

    [Audit](fix) fix audit log lost for npe (#44145)
    ### What problem does this PR solve?
    
    ```
    private static void logAuditLogImpl(ConnectContext ctx, String origStmt, StatementBase parsedStmt,
                org.apache.doris.proto.Data.PQueryStatistics statistics, boolean printFuzzyVariables) {
           
            //........
           // When we execute a statement, we execute the executeQuery method. In this method, we parse the sql using the new optimizer first, and if the parsing fails, we execute handleQueryException to record the audit log. At this time, there is no executor set, resulting in the npe
            if (!Env.getCurrentEnv().isMaster()) {
                if (ctx.executor != null && ctx.executor.isForwardToMaster()) {
                    auditEventBuilder.setState(ctx.executor.getProxyStatus());
                    int proxyStatusCode = ctx.executor.getProxyStatusCode();
                    if (proxyStatusCode != 0) {
                        auditEventBuilder.setErrorCode(proxyStatusCode);
                        auditEventBuilder.setErrorMessage(ctx.executor.getProxyErrMsg());
                    }
                }
            }
            if (ctx.getCommand() == MysqlCommand.COM_STMT_PREPARE && ctx.getState().getErrorCode() == null) {
                auditEventBuilder.setState(String.valueOf(MysqlStateType.OK));
            }
            Env.getCurrentEnv().getWorkloadRuntimeStatusMgr().submitFinishQueryToAudit(auditEventBuilder.build());
        }
    ```
    
    Co-authored-by: garenshi <garenshi@tencent.com>

--- fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
@@ -241,7 +241,7 @@ public class AuditLogHelper {
             auditEventBuilder.setStmt(origStmt);
         }
         if (!Env.getCurrentEnv().isMaster()) {
-            if (ctx.executor.isForwardToMaster()) {
+            if (ctx.executor != null && ctx.executor.isForwardToMaster()) {
                 auditEventBuilder.setState(ctx.executor.getProxyStatus());
                 int proxyStatusCode = ctx.executor.getProxyStatusCode();
                 if (proxyStatusCode != 0) {
