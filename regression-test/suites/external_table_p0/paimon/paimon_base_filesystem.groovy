e749db11cc100065737237b489249daccdd44e8f
Socrates <suyiteng@selectdb.com>
2024-12-11 00:06:55 +0800

    fix

--- regression-test/suites/external_table_p0/paimon/paimon_base_filesystem.groovy
+++ regression-test/suites/external_table_p0/paimon/paimon_base_filesystem.groovy
@@ -23,31 +23,32 @@ suite("paimon_base_filesystem", "p0,external,doris,external_docker,external_dock
     }
 
     try {
+        String catalog_obs = "paimon_base_filesystem_paimon_obs"
+        String catalog_oss = "paimon_base_filesystem_paimon_oss"
         String catalog_cos = "paimon_base_filesystem_paimon_cos"
         String catalog_cosn = "paimon_base_filesystem_paimon_cosn"
-        String catalog_oss = "paimon_base_filesystem_paimon_oss"
-        String catalog_obs = "paimon_base_filesystem_paimon_obs"
         String aliYunAk = context.config.otherConfigs.get("aliYunAk")
         String aliYunSk = context.config.otherConfigs.get("aliYunSk")
-        String txYunAk = context.config.otherConfigs.get("txYunAk")
-        String txYunSk = context.config.otherConfigs.get("txYunSk")
         String hwYunAk = context.config.otherConfigs.get("hwYunAk")
         String hwYunSk = context.config.otherConfigs.get("hwYunSk")
+        String txYunAk = context.config.otherConfigs.get("txYunAk")
+        String txYunSk = context.config.otherConfigs.get("txYunSk")
 
-        def cos = """select c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c18 from ${catalog_cosn}.zd.all_table order by c18"""
-        def cosn = """select c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c18 from ${catalog_cosn}.zd.all_table order by c18"""
-        def oss = """select * from ${catalog_oss}.paimonossdb1.test_tableoss order by a"""
-        def obs = """select * from ${catalog_obs}.piamon_obs_db.paimon_table_obs order by k"""
+        def obs = """select * from ${catalog_obs}.paimon_obs_db.paimon_obs_table order by k"""
+        def oss = """select * from ${catalog_oss}.paimon_oss_db.paimon_oss_table order by k"""
+        def cos = """select * from ${catalog_cos}.paimon_cos_db.paimon_cos_table order by k"""
+        def cosn = """select * from ${catalog_cosn}.paimon_cosn_db.paimon_cosn_table order by k"""
 
+
+        sql """drop catalog if exists ${catalog_obs};"""
+        sql """drop catalog if exists ${catalog_oss};"""
         sql """drop catalog if exists ${catalog_cos};"""
         sql """drop catalog if exists ${catalog_cosn};"""
-        sql """drop catalog if exists ${catalog_oss};"""
-        sql """drop catalog if exists ${catalog_obs};"""
 
         sql """
             create catalog if not exists ${catalog_cos} properties (
                 "type" = "paimon",
-                "warehouse" = "cos://doris-build-1308700295/regression/paimoncos",
+                "warehouse" = "cos://doris-build-1308700295/regression/paimon",
                 "cos.access_key" = "${txYunAk}",
                 "cos.secret_key" = "${txYunSk}",
                 "cos.endpoint" = "cos.ap-beijing.myqcloud.com"
@@ -56,16 +57,16 @@ suite("paimon_base_filesystem", "p0,external,doris,external_docker,external_dock
         sql """
             create catalog if not exists ${catalog_cosn} properties (
                 "type" = "paimon",
-                "warehouse" = "cosn://doris-build-1308700295/regression/paimoncos",
-                "cos.access_key" = "${txYunAk}",
-                "cos.secret_key" = "${txYunSk}",
-                "cos.endpoint" = "cos.ap-beijing.myqcloud.com"
+                "warehouse" = "cosn://doris-build-1308700295/regression/paimon",
+                "cosn.access_key" = "${txYunAk}",
+                "cosn.secret_key" = "${txYunSk}",
+                "cosn.endpoint" = "cos.ap-beijing.myqcloud.com"
             );
         """
         sql """
             create catalog if not exists ${catalog_oss} properties (
                 "type" = "paimon",
-                "warehouse" = "oss://doris-regression-bj/regression/paimonoss",
+                "warehouse" = "oss://doris-regression-bj/regression/paimon",
                 "oss.access_key"="${aliYunAk}",
                 "oss.secret_key"="${aliYunSk}",
                 "oss.endpoint"="oss-cn-beijing.aliyuncs.com"
@@ -80,22 +81,22 @@ suite("paimon_base_filesystem", "p0,external,doris,external_docker,external_dock
                 "obs.endpoint"="obs.cn-north-4.myhuaweicloud.com"
             );
         """
+        logger.info("catalog " + catalog_obs + " created")
+        logger.info("catalog " + catalog_oss + " created")
         logger.info("catalog " + catalog_cos + " created")
         logger.info("catalog " + catalog_cosn + " created")
-        logger.info("catalog " + catalog_oss + " created")
-        logger.info("catalog " + catalog_obs + " created")
 
         sql """set force_jni_scanner=false"""
-        qt_c1 cos
-        qt_c2 cosn
-        qt_c3 oss
-        qt_c4 obs
+        qt_oss oss
+        qt_obs obs
+        qt_cos cos
+        qt_cosn cosn
 
         sql """set force_jni_scanner=true"""
-        qt_c5 cos
-        qt_c6 cosn
-        qt_c7 oss
-        qt_c8 obs
+        qt_oss oss
+        qt_obs obs
+        qt_cos cos
+        qt_cosn cosn
 
     } finally {
         sql """set force_jni_scanner=false"""
