c2f1f07c6fc20c11cd39adb54ef67e0d89998210
wangbo <wangbo@selectdb.com>
2024-12-10 15:23:08 +0800

    add be avail cpu num metric

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1211,7 +1211,7 @@ DEFINE_Bool(exit_on_exception, "false");
 DEFINE_Bool(enable_flush_file_cache_async, "true");
 
 // cgroup
-DEFINE_mString(doris_cgroup_cpu_path, "");
+DEFINE_String(doris_cgroup_cpu_path, "");
 
 DEFINE_mBool(enable_be_proc_monitor, "false");
 DEFINE_mInt32(be_proc_monitor_interval_ms, "10000");
