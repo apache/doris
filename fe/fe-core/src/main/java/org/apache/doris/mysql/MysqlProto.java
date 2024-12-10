71f96e6e06fc8ac0fc30545514c1d4d95ef205af
wangbo <wangbo@selectdb.com>
2024-12-10 20:13:27 +0800

    [Improvement]downgrade resource tag when there is not queryable replica (#44255)

--- fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlProto.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlProto.java
@@ -266,7 +266,8 @@ public class MysqlProto {
         }
 
         // set resource tag if has
-        context.setResourceTags(Env.getCurrentEnv().getAuth().getResourceTags(qualifiedUser));
+        context.setResourceTags(Env.getCurrentEnv().getAuth().getResourceTags(qualifiedUser),
+                Env.getCurrentEnv().getAuth().isAllowResourceTagDowngrade(qualifiedUser));
         return true;
     }
 
