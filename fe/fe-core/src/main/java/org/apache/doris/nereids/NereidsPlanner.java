5a5708b073908354f2d489c9fc1d43610d1d6557
Mingyu Chen (Rayner) <morningman@163.com>
2024-12-09 16:55:19 -0800

    [fix](planner) query should be cancelled if limit reached (#44338)
    ### What problem does this PR solve?
    
    Problem Summary:
    When there is a `limit` cluse in SQL, if FE has obtained data with more
    than the `limit` number of rows,
    it should send a cancel command to BE to cancel the query to prevent BE
    from reading more data.
    However, this function has problems in the current code and does not
    work.
    Especially in external table query, this may result in lots of
    unnecessary network io read.
    
    1. `isBlockQuery`
    
    In the old optimizer, if a query statement contains a `sort` or `agg`
    node,
        `isBlockQuery` will be marked as true, otherwise it will be false.
        In the new optimizer, this value is always true.
      
        Regardless of the old or new optimizer, this logic is wrong.
    But only when `isBlockQuery = false` will the reach limit logic be
    triggered.
    
    2. Calling problem of reach limit logic
    
    The reach limit logic judgment will only be performed when `eos = true`
    in the rowBatch returned by BE.
        This is wrong.
    Because for `limit N` queries, each BE's own `limit` is N. But for FE,
    as long as the total number of rows
    returned by all BEs exceeds N, the reach limit logic can be triggered.
        So it should not be processed only when `eos = true`.
    
    The PR mainly changes:
    
    1. Remove `isBlockQuery`
    
    `isBlockQuery` is only used in the reach limit logic. And it is not
    needed. Remove it completely.
    
    2. Modify the judgment position of reach limit.
    
        When the number of rows obtained by FE is greater than the limit,
        it will check the reach limit logic.
    
    3. fix wrong `limitRows` in `QueryProcessor`
    
        the limitRows should be got from the first fragment, not last.
    
    4. In scanner scheduler on BE side, if scanner has limit, ignore the
    scan bytes threshold per round.
    
    ### Release note
    
    [fix](planner) query should be cancelled if limit reached

--- fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/NereidsPlanner.java
@@ -723,11 +723,6 @@ public class NereidsPlanner extends Planner {
     }
 
     @Override
-    public boolean isBlockQuery() {
-        return true;
-    }
-
-    @Override
     public DescriptorTable getDescTable() {
         return descTable;
     }
