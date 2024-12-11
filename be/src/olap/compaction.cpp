8877c9623911b76d869241f11275370dc2ad1111
bobhan1 <baohan@selectdb.com>
2024-12-11 15:41:32 +0800

    add comment

--- be/src/olap/compaction.cpp
+++ be/src/olap/compaction.cpp
@@ -987,6 +987,18 @@ Status CompactionMixin::modify_rowsets() {
                 merged_missed_rows_size += _stats.filtered_rows;
             }
 
+            // Suppose a heavy schema change process on BE converting tablet A to tablet B.
+            // 1. during schema change double write, new loads write [X-Y] on tablet B.
+            // 2. rowsets with version [a],[a+1],...,[b-1],[b] on tablet B are picked for cumu compaction(X<=a<b<=Y).(cumu compaction
+            //    on new tablet during schema change double write is allowed after https://github.com/apache/doris/pull/16470)
+            // 3. schema change remove all rowsets on tablet B before version Z(b<=Z<=Y) before it begins to convert historical rowsets.
+            // 4. schema change finishes.
+            // 5. cumu compation begins on new tablet with version [a],...,[b]. If there are duplicate keys between these rowsets,
+            //    the compaction check will fail because these rowsets have skipped to calculate delete bitmap in commit phase and
+            //    publish phase because tablet B is in NOT_READY state when writing.
+
+            // Considering that the cumu compaction will fail finally in this situation because `Tablet::modify_rowsets` will check if rowsets in
+            // `to_delete`(_input_rowsets) still exist in tablet's `_rs_version_map`, we can just skip to check missed rows here.
             bool need_to_check_missed_rows = true;
             {
                 std::shared_lock rlock(_tablet->get_header_lock());
