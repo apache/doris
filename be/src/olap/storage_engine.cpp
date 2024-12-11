01abd7fcab4c6b43f4154a37ee4bd3f165c604e5
freeoneplus <suyijia@selectdb.com>
2024-12-11 10:03:33 +0800

    add skip_check_ulimit config

--- be/src/olap/storage_engine.cpp
+++ be/src/olap/storage_engine.cpp
@@ -463,7 +463,7 @@ Status StorageEngine::_check_file_descriptor_number() {
                      << ", use default configuration instead.";
         return Status::OK();
     }
-    if (getenv("SKIP_CHECK_ULIMIT") == true) {
+    if (getenv("SKIP_CHECK_ULIMIT")) {
         LOG(INFO) << "the ‘ulimit’ value check is skipped";
         return Status::OK();
     }
