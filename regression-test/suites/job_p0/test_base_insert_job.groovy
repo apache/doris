8532b9131ff91ba54c4f455c06166bf2eff6d6dd
Calvin Kirs <guoqiang@selectdb.com>
2024-12-12 14:56:40 +0800

    [Fix](Insert-Job)Resolving Task Status Discrepancies in Query Execution Records
    When querying the execution records of tasks, the current system has two sources that may describe task status:
    
    Task status in the scheduling module: Represents the processing status of tasks within the scheduling system.
    Task status in LoadManager: Specifically refers to the status of Insert-type tasks, containing more detailed execution information.
    
    When the data in the LoadManager is deleted or expires, the completion status will revert to the task status from the scheduling system, which could cause confusion. Therefore, consistency needs to be maintained.
    
    Since these two sources may have inconsistencies in their status information, they can easily cause confusion for users and developers.

--- regression-test/suites/job_p0/test_base_insert_job.groovy
+++ regression-test/suites/job_p0/test_base_insert_job.groovy
@@ -116,7 +116,7 @@ suite("test_base_insert_job") {
 
     def taskStatus = sql """select status from tasks("type"="insert") where JobName ='${jobName}'"""
     for (int i = 0; i < taskStatus.size(); i++) {
-        assert taskStatus.get(i).get(0) =="CANCELLED" || taskStatus.get(i).get(0) =="FINISHED"
+        assert taskStatus.get(i).get(0) =="CANCELLED" || taskStatus.get(i).get(0) =="SUCCESS"
     }
     sql """
        CREATE JOB ${jobMixedName}  ON SCHEDULE every 1 second  DO insert into ${tableName} (timestamp, type, user_id) values ('2023-03-18','1','12213');
@@ -168,7 +168,7 @@ suite("test_base_insert_job") {
     // table should have one record after job finished
     assert datas.size() == 1
     // one time job only has one task. when job finished, task status should be FINISHED
-    assert datas.get(0).get(0) == "FINISHED"
+    assert datas.get(0).get(0) == "SUCCESS"
     // check table data
     def dataCount1 = sql """select count(1) from ${tableName} where user_id=1001"""
     assert dataCount1.get(0).get(0) == 1
