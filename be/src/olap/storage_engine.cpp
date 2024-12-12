1a628c4fa31b6f7fd99b75aba1ced3474cbe4f9f
freeoneplus <suyijia@selectdb.com>
2024-12-12 08:38:39 +0800

    add skip_check_ulimit config

--- be/src/olap/storage_engine.cpp
+++ be/src/olap/storage_engine.cpp
@@ -464,14 +464,14 @@ Status StorageEngine::_check_file_descriptor_number() {
         return Status::OK();
     }
     if (getenv("SKIP_CHECK_ULIMIT") == nullptr) {
-        LOG(INFO) << "will check ‘ulimit’ value."
+        LOG(INFO) << "will check ‘ulimit’ value.";
     } else if (std::string(getenv("SKIP_CHECK_ULIMIT")) == "true") {
         LOG(INFO) << "the ‘ulimit’ value check is skipped"
                   << ", the SKIP_CHECK_ULIMIT env value is " << getenv("SKIP_CHECK_ULIMIT");
         return Status::OK();
     } else {
-        LOG(INFO) << "the SKIP_CHECK_ULIMIT env value is " << getenv("SKIP_CHECK_ULIMIT");
-                  << ", will check ulimit value."
+        LOG(INFO) << "the SKIP_CHECK_ULIMIT env value is " << getenv("SKIP_CHECK_ULIMIT")
+                  << ", will check ulimit value.";
     }
     if (l.rlim_cur < config::min_file_descriptor_number) {
         LOG(ERROR) << "File descriptor number is less than " << config::min_file_descriptor_number
