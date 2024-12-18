apache/doris

commit 129bb017bc628076951c7b809eb204578068f8d9

[fix](binlog) Fix linking binlog failure due to existing files (#45445)
If the binlog files already exist, the link file will be failed:

```
W20241212 15:56:51.084318 601741 status.h:424] meet error status: [E-242]fail to create hard link. from=/mnt/disk2/VEC_ASAN/doris.HDD/data/60/367148/684012786/020000000001570ce04a2e65a3a57b99d083b08558dfcf81_0.dat, to=/mnt/disk2/VEC_ASAN/doris.HDD/data/60/367148/684012786/_binlog/020000000001570ce04a2e65a3a57b99d083b08558dfcf81_0.dat, errno=17

        0#  doris::BetaRowset::add_to_binlog() at /home/zcp/repo_center/doris_master/doris/be/src/common/status.h:389
        1#  doris::TxnManager::publish_txn(doris::OlapMeta*, long, long, long, doris::UniqueId, doris::Version const&, doris::TabletPublishStatistics*) at /home/zcp/repo_center/doris_master/doris/be/src/olap/txn_manager.cpp:0
        2#  doris::TxnManager::publish_txn(long, std::shared_ptr<doris::Tablet> const&, long, doris::Version const&, doris::TabletPublishStatistics*) at /home/zcp/repo_center/doris_master/doris/be/src/olap/txn_manager.cpp:197
        3#  doris::TabletPublishTxnTask::handle() at /home/zcp/repo_center/doris_master/doris/be/src/olap/task/engine_publish_version_task.cpp:411
        4#  doris::ThreadPool::dispatch_thread() at /home/zcp/repo_center/doris_master/doris/be/src/util/threadpool.cpp:0
        5#  doris::Thread::supervise_thread(void*) at /var/local/ldb-toolchain/bin/../usr/include/pthread.h:562
        6#  ?
        7#  ?
```

be/src/olap/rowset/beta_rowset.cpp
@@ -557,10 +557,6 @@ Status BetaRowset::add_to_binlog() {
     }
 
     const auto& fs = io::global_local_filesystem();
-
-    // all segments are in the same directory, so cache binlog_dir without multi times check
-    std::string binlog_dir;
-
     auto segments_num = num_segments();
     VLOG_DEBUG << fmt::format("add rowset to binlog. rowset_id={}, segments_num={}",
                               rowset_id().to_string(), segments_num);
@@ -569,28 +565,34 @@ Status BetaRowset::add_to_binlog() {
     std::vector<string> linked_success_files;
     Defer remove_linked_files {[&]() { // clear linked files if errors happen
         if (!status.ok()) {
-            LOG(WARNING) << "will delete linked success files due to error " << status;
+            LOG(WARNING) << "will delete linked success files due to error "
+                         << status.to_string_no_stack();
             std::vector<io::Path> paths;
             for (auto& file : linked_success_files) {
                 paths.emplace_back(file);
                 LOG(WARNING) << "will delete linked success file " << file << " due to error";
             }
             static_cast<void>(fs->batch_delete(paths));
-            LOG(WARNING) << "done delete linked success files due to error " << status;
+            LOG(WARNING) << "done delete linked success files due to error "
+                         << status.to_string_no_stack();
         }
     }};
 
+    // all segments are in the same directory, so cache binlog_dir without multi times check
+    std::string binlog_dir;
     for (int i = 0; i < segments_num; ++i) {
         auto seg_file = local_segment_path(_tablet_path, rowset_id().to_string(), i);
 
         if (binlog_dir.empty()) {
             binlog_dir = std::filesystem::path(seg_file).parent_path().append("_binlog").string();
 
+            // Delete all existing files in binlog dir, to keep binlog dir clean.
             bool exists = true;
             RETURN_IF_ERROR(fs->exists(binlog_dir, &exists));
-            if (!exists) {
-                RETURN_IF_ERROR(fs->create_directory(binlog_dir));
+            if (exists) {
+                RETURN_IF_ERROR(fs->delete_directory(binlog_dir));
             }
+            RETURN_IF_ERROR(fs->create_directory(binlog_dir));
         }
 
         auto binlog_file =
@@ -614,7 +616,12 @@ Status BetaRowset::add_to_binlog() {
                                           std::filesystem::path(index_file).filename())
                                                  .string();
                 VLOG_DEBUG << "link " << index_file << " to " << binlog_index_file;
-                RETURN_IF_ERROR(fs->link_file(index_file, binlog_index_file));
+                if (!fs->link_file(index_file, binlog_index_file).ok()) {
+                    status = Status::Error<OS_ERROR>(
+                            "fail to create hard link. from={}, to={}, errno={}", index_file,
+                            binlog_index_file, Errno::no());
+                    return status;
+                }
                 linked_success_files.push_back(binlog_index_file);
             }
         } else {
@@ -625,7 +632,12 @@ Status BetaRowset::add_to_binlog() {
                                           std::filesystem::path(index_file).filename())
                                                  .string();
                 VLOG_DEBUG << "link " << index_file << " to " << binlog_index_file;
-                RETURN_IF_ERROR(fs->link_file(index_file, binlog_index_file));
+                if (!fs->link_file(index_file, binlog_index_file).ok()) {
+                    status = Status::Error<OS_ERROR>(
+                            "fail to create hard link. from={}, to={}, errno={}", index_file,
+                            binlog_index_file, Errno::no());
+                    return status;
+                }
                 linked_success_files.push_back(binlog_index_file);
             }
         }

be/src/common/status.h
@@ -570,7 +570,7 @@ private:
 // and another thread is call to_string method, it may core, because the _err_msg is an unique ptr and
 // it is deconstructed during copy method.
 // And also we could not use lock, because we need get status frequently to check if it is cancelled.
-// The defaule value is ok.
+// The default value is ok.
 class AtomicStatus {
 public:
     AtomicStatus() : error_st_(Status::OK()) {}

be/src/olap/txn_manager.cpp
@@ -548,8 +548,9 @@ Status TxnManager::publish_txn(OlapMeta* meta, TPartitionId partition_id,
         if (!status.ok()) {
             return Status::Error<ROWSET_ADD_TO_BINLOG_FAILED>(
                     "add rowset to binlog failed. when publish txn rowset_id: {}, tablet id: {}, "
-                    "txn id: {}",
-                    rowset->rowset_id().to_string(), tablet_id, transaction_id);
+                    "txn id: {}, status: {}",
+                    rowset->rowset_id().to_string(), tablet_id, transaction_id,
+                    status.to_string_no_stack());
         }
     }
 

be/test/olap/rowset/beta_rowset_test.cpp
@@ -292,4 +292,14 @@ TEST_F(BetaRowsetTest, ReadTest) {
     }
 }
 
+TEST_F(BetaRowsetTest, AddToBinlogTest) {
+    RowsetMetaSharedPtr rowset_meta = std::make_shared<RowsetMeta>();
+    BetaRowset rowset(nullptr, rowset_meta, "");
+    std::string resource_id = "10000";
+    Status s = rowset.add_to_binlog();
+    ASSERT_TRUE(s.ok()) << "first add_to_binlog(): " << s;
+    s = rowset.add_to_binlog();
+    ASSERT_TRUE(s.ok()) << "second add_to_binlog(): " << s;
+}
+
 } // namespace doris

commit cc14bf161c1090090d052ec408b6b5ca3ad87041

[fix](binlog) Avoid clear binlog dir
Fix the issue introduced by #45445

be/src/olap/rowset/beta_rowset.cpp
@@ -18,6 +18,7 @@
 #include "olap/rowset/beta_rowset.h"
 
 #include <ctype.h>
+#include <errno.h>
 #include <fmt/format.h>
 
 #include <algorithm>
@@ -578,6 +579,10 @@ Status BetaRowset::add_to_binlog() {
         }
     }};
 
+    // The publish_txn might fail even if the add_to_binlog success, so we need to check
+    // whether a file already exists before linking.
+    auto errno_is_file_exists = []() { return Errno::no() == EEXIST; };
+
     // all segments are in the same directory, so cache binlog_dir without multi times check
     std::string binlog_dir;
     for (int i = 0; i < segments_num; ++i) {
@@ -586,20 +591,18 @@ Status BetaRowset::add_to_binlog() {
         if (binlog_dir.empty()) {
             binlog_dir = std::filesystem::path(seg_file).parent_path().append("_binlog").string();
 
-            // Delete all existing files in binlog dir, to keep binlog dir clean.
             bool exists = true;
             RETURN_IF_ERROR(fs->exists(binlog_dir, &exists));
-            if (exists) {
-                RETURN_IF_ERROR(fs->delete_directory(binlog_dir));
+            if (!exists) {
+                RETURN_IF_ERROR(fs->create_directory(binlog_dir));
             }
-            RETURN_IF_ERROR(fs->create_directory(binlog_dir));
         }
 
         auto binlog_file =
                 (std::filesystem::path(binlog_dir) / std::filesystem::path(seg_file).filename())
                         .string();
         VLOG_DEBUG << "link " << seg_file << " to " << binlog_file;
-        if (!fs->link_file(seg_file, binlog_file).ok()) {
+        if (!fs->link_file(seg_file, binlog_file).ok() && !errno_is_file_exists()) {
             status = Status::Error<OS_ERROR>("fail to create hard link. from={}, to={}, errno={}",
                                              seg_file, binlog_file, Errno::no());
             return status;
@@ -616,7 +619,7 @@ Status BetaRowset::add_to_binlog() {
                                           std::filesystem::path(index_file).filename())
                                                  .string();
                 VLOG_DEBUG << "link " << index_file << " to " << binlog_index_file;
-                if (!fs->link_file(index_file, binlog_index_file).ok()) {
+                if (!fs->link_file(index_file, binlog_index_file).ok() && !errno_is_file_exists()) {
                     status = Status::Error<OS_ERROR>(
                             "fail to create hard link. from={}, to={}, errno={}", index_file,
                             binlog_index_file, Errno::no());
@@ -632,7 +635,7 @@ Status BetaRowset::add_to_binlog() {
                                           std::filesystem::path(index_file).filename())
                                                  .string();
                 VLOG_DEBUG << "link " << index_file << " to " << binlog_index_file;
-                if (!fs->link_file(index_file, binlog_index_file).ok()) {
+                if (!fs->link_file(index_file, binlog_index_file).ok() && !errno_is_file_exists()) {
                     status = Status::Error<OS_ERROR>(
                             "fail to create hard link. from={}, to={}, errno={}", index_file,
                             binlog_index_file, Errno::no());