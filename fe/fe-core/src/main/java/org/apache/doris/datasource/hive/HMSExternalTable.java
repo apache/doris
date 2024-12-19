178d29d46cbddac6ce12a7067997013ac4b495e6
zhangdong <zhangdong@selectdb.com>
2024-12-19 19:12:11 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hive/HMSExternalTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hive/HMSExternalTable.java
@@ -331,7 +331,7 @@ public class HMSExternalTable extends ExternalTable implements MTMVRelatedTableI
     }
 
     @Override
-    protected Map<String, PartitionItem> getNameToPartitionItems(Optional<MvccSnapshot> snapshot) {
+    public Map<String, PartitionItem> getNameToPartitionItems(Optional<MvccSnapshot> snapshot) {
         return getNameToPartitionItems();
     }
 
