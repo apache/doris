b065db023ddb6073e6337a6311130a847414a018
Mingyu Chen (Rayner) <morningman@163.com>
2024-12-20 13:33:25 +0800

    [opt](catalog) cache the Configuration object (#45433)
    ### What problem does this PR solve?
    
    Problem Summary:
    Creating Configuration object is very costly, so we cache it for better
    performance

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
@@ -152,6 +152,9 @@ public abstract class ExternalCatalog
     protected MetaCache<ExternalDatabase<? extends ExternalTable>> metaCache;
     protected PreExecutionAuthenticator preExecutionAuthenticator;
 
+    private volatile Configuration cachedConf = null;
+    private final byte[] confLock = new byte[0];
+
     public ExternalCatalog() {
     }
 
@@ -163,6 +166,20 @@ public abstract class ExternalCatalog
     }
 
     public Configuration getConfiguration() {
+        // build configuration is costly, so we cache it.
+        if (cachedConf != null) {
+            return cachedConf;
+        }
+        synchronized (confLock) {
+            if (cachedConf != null) {
+                return cachedConf;
+            }
+            cachedConf = buildConf();
+            return cachedConf;
+        }
+    }
+
+    private Configuration buildConf() {
         Configuration conf = DFSFileSystem.getHdfsConf(ifNotSetFallbackToSimpleAuth());
         Map<String, String> catalogProperties = catalogProperty.getHadoopProperties();
         for (Map.Entry<String, String> entry : catalogProperties.entrySet()) {
@@ -406,6 +423,10 @@ public abstract class ExternalCatalog
         this.initialized = false;
         synchronized (this.propLock) {
             this.convertedProperties = null;
+        }
+
+        synchronized (this.confLock) {
+            this.cachedConf = null;
         }
 
         refreshOnlyCatalogCache(invalidCache);
