d128ed2927d4dd426bae01b20365bb226d235747
morningman <yunyou@selectdb.com>
2024-12-21 15:15:02 +0800

    [fix](catalog) fix npe after replaying the external catalog

--- fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
@@ -153,7 +153,7 @@ public abstract class ExternalCatalog
     protected PreExecutionAuthenticator preExecutionAuthenticator;
 
     private volatile Configuration cachedConf = null;
-    private final byte[] confLock = new byte[0];
+    private byte[] confLock = new byte[0];
 
     public ExternalCatalog() {
     }
@@ -783,6 +783,7 @@ public abstract class ExternalCatalog
             }
         }
         this.propLock = new byte[0];
+        this.confLock = new byte[0];
         this.initialized = false;
         setDefaultPropsIfMissing(true);
         if (tableAutoAnalyzePolicy == null) {
