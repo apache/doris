8f32ef7f5c05144b55cebf90d5f98a8c6aecac00
pengyu <pengyu@selectdb.com>
2024-12-22 16:25:22 +0800

    amend inverted_index* cases

--- regression-test/suites/inverted_index_p0/index_change/test_index_change_with_full_compaction.groovy
+++ regression-test/suites/inverted_index_p0/index_change/test_index_change_with_full_compaction.groovy
@@ -142,36 +142,6 @@ suite("test_index_change_with_full_compaction") {
         sql """ CREATE INDEX idx_city ON ${tableName}(`city`) USING INVERTED """
         wait_for_latest_op_on_table_finish(tableName, timeout)
 
-        // trigger compactions for all tablets in ${tableName}
-        for (def tablet in tablets) {
-            String tablet_id = tablet.TabletId
-            backend_id = tablet.BackendId
-            StringBuilder sb = new StringBuilder();
-            sb.append("curl -X POST http://")
-            sb.append(backendId_to_backendIP.get(backend_id))
-            sb.append(":")
-            sb.append(backendId_to_backendHttpPort.get(backend_id))
-            sb.append("/api/compaction/run?tablet_id=")
-            sb.append(tablet_id)
-            sb.append("&compact_type=full")
-
-            String command = sb.toString()
-            process = command.execute()
-            code = process.waitFor()
-            err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
-            out = process.getText()
-            logger.info("Run compaction: code=" + code + ", out=" + out + ", err=" + err)
-            assertEquals(code, 0)
-            def compactJson = parseJson(out.trim())
-            if (compactJson.status.toLowerCase() == "fail") {
-                assertEquals(disableAutoCompaction, false)
-                logger.info("Compaction was done automatically!")
-            }
-            if (disableAutoCompaction) {
-                assertEquals("success", compactJson.status.toLowerCase())
-            }
-        }
-
         // build index
         if (!isCloudMode()) {
             sql "build index idx_user_id on ${tableName}"
@@ -179,34 +149,8 @@ suite("test_index_change_with_full_compaction") {
             sql "build index idx_city on ${tableName}"
         }
 
-        // wait for all compactions done
-        for (def tablet in tablets) {
-            boolean running = true
-            do {
-                Thread.sleep(1000)
-                String tablet_id = tablet.TabletId
-                backend_id = tablet.BackendId
-                StringBuilder sb = new StringBuilder();
-                sb.append("curl -X GET http://")
-                sb.append(backendId_to_backendIP.get(backend_id))
-                sb.append(":")
-                sb.append(backendId_to_backendHttpPort.get(backend_id))
-                sb.append("/api/compaction/run_status?tablet_id=")
-                sb.append(tablet_id)
-
-                String command = sb.toString()
-                logger.info(command)
-                process = command.execute()
-                code = process.waitFor()
-                err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
-                out = process.getText()
-                logger.info("Get compaction status: code=" + code + ", out=" + out + ", err=" + err)
-                assertEquals(code, 0)
-                def compactionStatus = parseJson(out.trim())
-                assertEquals("success", compactionStatus.status.toLowerCase())
-                running = compactionStatus.run_status
-            } while (running)
-        }
+        // trigger compactions for all tablets in ${tableName}
+        trigger_and_wait_compaction(tableName, "full")
 
         int rowCount = 0
         for (def tablet in tablets) {
