bc8b47405f0be5cf37193fc1be6e43a88fb16986
airborne12 <jiangkai@selectdb.com>
2024-12-23 11:21:48 +0800

    [fix](bf index) add ngram bf index validation in nereids index definition check

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/IndexDefinitionTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/IndexDefinitionTest.java
@@ -17,16 +17,22 @@
 
 package org.apache.doris.nereids.trees.plans.commands;
 
+import com.google.common.collect.Lists;
 import org.apache.doris.catalog.AggregateType;
 import org.apache.doris.catalog.KeysType;
 import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.trees.plans.commands.info.ColumnDefinition;
 import org.apache.doris.nereids.trees.plans.commands.info.IndexDefinition;
+import org.apache.doris.nereids.types.IntegerType;
+import org.apache.doris.nereids.types.StringType;
 import org.apache.doris.nereids.types.VariantType;
 
 import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IndexDefinitionTest {
     @Test
@@ -42,5 +48,52 @@ public class IndexDefinitionTest {
             Assertions.assertTrue(e instanceof AnalysisException);
             Assertions.assertTrue(e.getMessage().contains("not supported in inverted index format V1"));
         }
+    }
+
+    @Test
+    void testNgramBFIndex() throws AnalysisException {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("gram_size", "3");
+        properties.put("bf_size", "10000");
+
+        IndexDefinition def = new IndexDefinition("ngram_bf_index", Lists.newArrayList("col1"), "NGRAM_BF", properties, "comment");
+        def.checkColumn(new ColumnDefinition("col1", StringType.INSTANCE, false, AggregateType.NONE, true, null, "comment"),
+            KeysType.DUP_KEYS, false, false);
+    }
+
+    @Test
+    void testInvalidNgramBFIndexColumnType() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("gram_size", "3");
+        properties.put("bf_size", "10000");
+
+        IndexDefinition def = new IndexDefinition("ngram_bf_index", Lists.newArrayList("col1"), "NGRAM_BF", properties, "comment");
+        Assertions.assertThrows(AnalysisException.class, () ->
+            def.checkColumn(new ColumnDefinition("col1", IntegerType.INSTANCE, false, AggregateType.NONE, true, null, "comment"),
+                KeysType.DUP_KEYS, false, false));
+    }
+
+    @Test
+    void testNgramBFIndexInvalidSize() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("gram_size", "256");
+        properties.put("bf_size", "10000");
+
+        IndexDefinition def = new IndexDefinition("ngram_bf_index", Lists.newArrayList("col1"), "NGRAM_BF", properties, "comment");
+        Assertions.assertThrows(AnalysisException.class, () ->
+            def.checkColumn(new ColumnDefinition("col1", StringType.INSTANCE, false, AggregateType.NONE, true, null, "comment"),
+                KeysType.DUP_KEYS, false, false));
+    }
+
+    @Test
+    void testNgramBFIndexInvalidSize2() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("gram_size", "3");
+        properties.put("bf_size", "65536");
+
+        IndexDefinition def = new IndexDefinition("ngram_bf_index", Lists.newArrayList("col1"), "NGRAM_BF", properties, "comment");
+        Assertions.assertThrows(AnalysisException.class, () ->
+            def.checkColumn(new ColumnDefinition("col1", StringType.INSTANCE, false, AggregateType.NONE, true, null, "comment"),
+                KeysType.DUP_KEYS, false, false));
     }
 }
