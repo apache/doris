fa20625d083ecf017450807c5a8d18c32b4ed202
924060929 <lanhuajian@selectdb.com>
2024-12-23 11:17:45 +0800

    [opt](nereids) optimize rewrite of synchronize materialize view (#45748)
    optimize rewrite of synchronize materialize view
    1. cache toSql
    2. fast parse UnboundSlot in NereidsParser.parseExpression

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/Placeholder.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/Placeholder.java
@@ -66,7 +66,7 @@ public class Placeholder extends Expression implements LeafExpression {
     }
 
     @Override
-    public String toSql() {
+    public String computeToSql() {
         return "?";
     }
 
--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/literal/DateTimeLiteral.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/literal/DateTimeLiteral.java
@@ -266,7 +266,7 @@ public class DateTimeLiteral extends DateLiteral {
     }
 
     @Override
-    public String toSql() {
+    public String computeToSql() {
         return "'" + getStringValue() + "'";
     }
 
