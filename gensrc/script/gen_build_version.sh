15d06272ca64c618a4f202632534295cab753235
yagagagaga <zhangminke@selectdb.com>
2024-12-24 12:02:39 +0800

    fix shell badsmell

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -35,7 +35,7 @@ build_version_hotfix=0
 build_version_rc_version="rc03"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}"
-if [[ ${build_version_hotfix} > 0 ]]; then
+if [[ ${build_version_hotfix} -gt 0 ]]; then
     build_version+=".${build_version_hotfix}"
 fi
 build_version+="-${build_version_rc_version}"
@@ -225,7 +225,7 @@ fi
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}"
 
-if [[ ${build_version_hotfix} > 0 ]]; then
+if [[ ${build_version_hotfix} -gt 0 ]]; then
     build_version+=".${build_version_hotfix}"
 fi
 
