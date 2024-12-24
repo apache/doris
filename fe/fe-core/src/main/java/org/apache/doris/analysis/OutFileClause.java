36d8c7195367b18175be264fb7476e6b9d2d1b52
TieweiFang <fangtiewei@selectdb.com>
2024-12-24 16:09:54 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/analysis/OutFileClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/OutFileClause.java
@@ -266,7 +266,7 @@ public class OutFileClause {
         if (brokerDesc != null && isLocalOutput) {
             throw new AnalysisException("No need to specify BROKER properties in OUTFILE clause for local file output");
         } else if (brokerDesc == null && !isLocalOutput) {
-            throw new AnalysisException("Must specify BROKER properties or current local file path in OUTFILE clause");
+            throw new AnalysisException("Please specify BROKER properties or check out your local file path.");
         }
         isAnalyzed = true;
 
