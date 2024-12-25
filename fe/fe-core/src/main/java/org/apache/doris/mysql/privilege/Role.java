547fbe3e79045fba3102e9b6acf63b99520ae8a7
zhangdong <zhangdong@selectdb.com>
2024-12-25 20:58:29 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Role.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Role.java
@@ -1128,6 +1128,9 @@ public class Role implements Writable, GsonPostProcessable {
     }
 
     public static void compatibilityAuthIndexChange(PrivBitSet privBitSet) {
+        if (privBitSet == null) {
+            return;
+        }
         int currentVersion = Env.getCurrentEnvJournalVersion();
         // not cloud mode,
         // For versions greater than VERSION_123,
