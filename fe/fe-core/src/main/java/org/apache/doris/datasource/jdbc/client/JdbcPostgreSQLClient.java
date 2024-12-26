57a7b75c7494613b31bda3d7dee0ad68078b3c8d
zy-kkk <zhongyk10@gmail.com>
2024-12-26 09:58:31 +0800

    [test](jdbc catalog) add some jdbc query tvf test (#45938)

--- fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcPostgreSQLClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcPostgreSQLClient.java
@@ -25,6 +25,8 @@ import org.apache.doris.common.util.Util;
 import org.apache.doris.datasource.jdbc.util.JdbcFieldSchema;
 
 import com.google.common.collect.Lists;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -37,6 +39,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class JdbcPostgreSQLClient extends JdbcClient {
+    private static final Logger LOG = LogManager.getLogger(JdbcPostgreSQLClient.class);
 
     private static final String[] supportedInnerType = new String[] {
             "int2", "int4", "int8", "smallserial", "serial",
@@ -168,6 +171,7 @@ public class JdbcPostgreSQLClient extends JdbcClient {
     private Type convertArrayType(JdbcFieldSchema fieldSchema) {
         int arrayDimensions = fieldSchema.getArrayDimensions().orElse(0);
         if (arrayDimensions == 0) {
+            LOG.warn("postgres array type without dimensions");
             return Type.UNSUPPORTED;
         }
 
