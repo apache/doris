539ab213f14eb024b07051b7246b6fbf91fac620
shuke <shuke@selectdb.com>
2024-12-26 15:34:54 +0800

    [regression-test](fix) remove some un-suitable test cases (#45984)
    ### What problem does this PR solve?
    
    Issue Number: close #xxx
    
    Related PR: #xxx
    
    Problem Summary:
    plsql is not under developed, skip all cases.
    "auto_partition_in_partition_prune" and "one_col_range_partition" insert
    data in too many tablets, txn to large. not suitable for cloud.

--- regression-test/pipeline/p0/conf/regression-conf.groovy
+++ regression-test/pipeline/p0/conf/regression-conf.groovy
@@ -87,6 +87,7 @@ excludeDirectories = "000_the_start_sentinel_do_not_touch," + // keep this line 
     "nereids_rules_p0/subquery," +
     "unique_with_mow_c_p0," +
     "workload_manager_p1," +
+    "plsql_p0," + // plsql is not developped any more, add by sk
     "zzz_the_end_sentinel_do_not_touch"// keep this line as the last line
 
 customConf1 = "test_custom_conf_value"
