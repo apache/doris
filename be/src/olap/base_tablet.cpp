923a16602429f3471112c0ce566ffadff1e9b2ab
bobhan1 <baohan@selectdb.com>
2024-12-26 12:19:13 +0800

    adjust

--- be/src/olap/base_tablet.cpp
+++ be/src/olap/base_tablet.cpp
@@ -44,6 +44,7 @@
 #include "util/crc32c.h"
 #include "util/debug_points.h"
 #include "util/doris_metrics.h"
+#include "util/key_util.h"
 #include "vec/common/assert_cast.h"
 #include "vec/common/schema_util.h"
 #include "vec/data_types/data_type_factory.hpp"
@@ -443,16 +444,6 @@ Status BaseTablet::lookup_row_data(const Slice& encoded_key, const RowLocation& 
         RowCache::instance()->insert({tablet_id(), encoded_key}, Slice {value.data, value.size});
     }
     return Status::OK();
-}
-
-bool BaseTablet::key_is_not_in_segment(Slice key, const KeyBoundsPB& segment_key_bounds,
-                                       bool is_segments_key_bounds_truncated) {
-    Slice maybe_truncated_min_key {segment_key_bounds.min_key()};
-    Slice maybe_truncated_max_key {segment_key_bounds.max_key()};
-    return Slice::lhs_is_strictly_less_than_rhs(key, false, maybe_truncated_min_key,
-                                                is_segments_key_bounds_truncated) ||
-           Slice::lhs_is_strictly_less_than_rhs(maybe_truncated_max_key,
-                                                is_segments_key_bounds_truncated, key, false);
 }
 
 Status BaseTablet::lookup_row_key(const Slice& encoded_key, TabletSchema* latest_schema,
