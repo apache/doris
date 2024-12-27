fd2da1e899bcda72802a08a8625ab6885f2b7362
englefly <zhouminghong@selectdb.com>
2024-12-27 10:26:12 +0800

    test InferSetOperatorDistinct

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Rewriter.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Rewriter.java
@@ -315,7 +315,7 @@ public class Rewriter extends AbstractBatchJobExecutor {
                         bottomUp(new MergeSetOperations(), new MergeSetOperationsExcept()),
                         bottomUp(new PushProjectIntoOneRowRelation()),
                         topDown(new MergeOneRowRelationIntoUnion()),
-                        costBased(topDown(new InferSetOperatorDistinct())),
+                        topDown(new InferSetOperatorDistinct()),
                         topDown(new BuildAggForUnion()),
                         bottomUp(new EliminateEmptyRelation()),
                         // when union has empty relation child and constantExprsList is not empty,
@@ -355,12 +355,12 @@ public class Rewriter extends AbstractBatchJobExecutor {
                 ),
 
                 topic("Eager aggregation",
-                        topDown(
+                        costBased(topDown(
                                 new PushDownAggWithDistinctThroughJoinOneSide(),
                                 new PushDownAggThroughJoinOneSide(),
-                                new PushDownAggThroughJoin()
+                                new PushDownAggThroughJoin())
                         ),
-                        custom(RuleType.PUSH_DOWN_DISTINCT_THROUGH_JOIN, PushDownDistinctThroughJoin::new),
+                        costBased(custom(RuleType.PUSH_DOWN_DISTINCT_THROUGH_JOIN, PushDownDistinctThroughJoin::new)),
                         topDown(new PushCountIntoUnionAll())
                 ),
 
