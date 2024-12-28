a380f5d2228517492b863e6998c05093842b599f
daidai <changyuwei@selectdb.com>
2024-12-28 10:05:03 +0800

    [enchement](utf8)import enable_text_validate_utf8 session var (#45537) (#46070)
    bp #45537

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hive/source/HiveScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hive/source/HiveScanNode.java
@@ -452,6 +452,8 @@ public class HiveScanNode extends FileQueryScanNode {
         TFileAttributes fileAttributes = new TFileAttributes();
         fileAttributes.setTextParams(textParams);
         fileAttributes.setHeaderType("");
+        fileAttributes.setEnableTextValidateUtf8(
+                ConnectContext.get().getSessionVariable().enableTextValidateUtf8);
         if (textParams.isSet(TFileTextScanRangeParams._Fields.ENCLOSE)) {
             fileAttributes.setTrimDoubleQuotes(true);
         }
