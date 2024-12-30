60a76798a59d716d26e6c5557f6c9b599d1f818f
Gabriel <liwenqiang@selectdb.com>
2024-12-30 10:02:11 +0800

    update

--- regression-test/suites/correctness_p0/test_hash_join_local_shuffle.groovy
+++ regression-test/suites/correctness_p0/test_hash_join_local_shuffle.groovy
@@ -21,12 +21,12 @@
 
 suite("test_hash_join_local_shuffle") {
 
-    sql """drop table if exists test1;"""
-    sql """drop table if exists test2;"""
-    sql """drop table if exists test3;"""
-    sql """drop table if exists test4;"""
+    sql """drop table if exists test_hash_join_local_shuffle1;"""
+    sql """drop table if exists test_hash_join_local_shuffle2;"""
+    sql """drop table if exists test_hash_join_local_shuffle3;"""
+    sql """drop table if exists test_hash_join_local_shuffle4;"""
     sql """
-    CREATE TABLE `test1` (
+    CREATE TABLE `test_hash_join_local_shuffle1` (
               `id1` bigint,
               `id2` bigint,
               `id3` bigint,
@@ -39,7 +39,7 @@ suite("test_hash_join_local_shuffle") {
               "replication_allocation" = "tag.location.default: 1"
             ); """
     sql """
-    CREATE TABLE `test2` (
+    CREATE TABLE `test_hash_join_local_shuffle2` (
               `id1` bigint,
               `id2` bigint,
               `id3` bigint,
@@ -52,7 +52,7 @@ suite("test_hash_join_local_shuffle") {
               "replication_allocation" = "tag.location.default: 1"
             ); """
     sql """
-    CREATE TABLE `test3` (
+    CREATE TABLE `test_hash_join_local_shuffle3` (
               `id1` bigint,
               `id2` bigint,
               `id3` bigint,
@@ -65,7 +65,7 @@ suite("test_hash_join_local_shuffle") {
               "replication_allocation" = "tag.location.default: 1"
             );  """
     sql """
-    CREATE TABLE `test4` (
+    CREATE TABLE `test_hash_join_local_shuffle4` (
               `id1` bigint,
               `id2` bigint,
               `id3` bigint,
@@ -77,13 +77,13 @@ suite("test_hash_join_local_shuffle") {
             PROPERTIES (
               "replication_allocation" = "tag.location.default: 1"
             ); """
-    sql """ insert into test1 values(1,2,3,4); """
-    sql """ insert into test2 values(1,2,3,4); """
-    sql """ insert into test3 values(1,2,3,4); """
-    sql """ insert into test4 values(1,2,3,4); """
+    sql """ insert into test_hash_join_local_shuffle1 values(1,2,3,4); """
+    sql """ insert into test_hash_join_local_shuffle2 values(1,2,3,4); """
+    sql """ insert into test_hash_join_local_shuffle3 values(1,2,3,4); """
+    sql """ insert into test_hash_join_local_shuffle4 values(1,2,3,4); """
 
     qt_select """
-    select /*+ SET_VAR(disable_join_reorder=true)*/ * from   (select tmp2.id1,tmp2.id3,tmp2.id4,count(distinct tmp2.id2) from (select tmp1.id1, tmp1.id2, tmp1.id3, tmp1.id4 from   (select test3.id1,test3.id2,test3.id3,test3.id4 from test2 join[shuffle] test3 on test2.id3 = test3.id3) tmp1 join [broadcast] test4 on test4.id2 = tmp1.id2) tmp2 group by tmp2.id1, tmp2.id3, tmp2.id4) tmp join [shuffle]  test1 on test1.id3 = tmp.id3;
+    select /*+ SET_VAR(disable_join_reorder=true)*/ * from   (select tmp2.id1,tmp2.id3,tmp2.id4,count(distinct tmp2.id2) from (select tmp1.id1, tmp1.id2, tmp1.id3, tmp1.id4 from   (select test_hash_join_local_shuffle3.id1,test_hash_join_local_shuffle3.id2,test_hash_join_local_shuffle3.id3,test_hash_join_local_shuffle3.id4 from test_hash_join_local_shuffle2 join[shuffle] test_hash_join_local_shuffle3 on test_hash_join_local_shuffle2.id3 = test_hash_join_local_shuffle3.id3) tmp1 join [broadcast] test_hash_join_local_shuffle4 on test_hash_join_local_shuffle4.id2 = tmp1.id2) tmp2 group by tmp2.id1, tmp2.id3, tmp2.id4) tmp join [shuffle]  test_hash_join_local_shuffle1 on test_hash_join_local_shuffle1.id3 = tmp.id3;
     """
      
 }
