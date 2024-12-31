cfed08da64f6272e0a30ba3820be8fb7548699bc
feiniaofeiafei <moailing@selectdb.com>
2024-12-31 20:29:28 +0800

    add eliminate order by key by uniform

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/EliminateOrderByConstant.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/EliminateOrderByConstant.java
@@ -17,9 +17,11 @@
 
 package org.apache.doris.nereids.rules.rewrite;
 
+import org.apache.doris.nereids.properties.DataTrait;
 import org.apache.doris.nereids.properties.OrderKey;
 import org.apache.doris.nereids.rules.Rule;
 import org.apache.doris.nereids.rules.RuleType;
+import org.apache.doris.nereids.trees.expressions.Slot;
 
 import com.google.common.collect.ImmutableList;
 
@@ -36,8 +38,10 @@ public class EliminateOrderByConstant extends OneRewriteRuleFactory {
             List<OrderKey> orderKeys = sort.getOrderKeys();
             ImmutableList.Builder<OrderKey> orderKeysWithoutConstBuilder
                     = ImmutableList.builderWithExpectedSize(orderKeys.size());
+            DataTrait dataTrait = sort.child().getLogicalProperties().getTrait();
             for (OrderKey orderKey : orderKeys) {
-                if (!orderKey.getExpr().isConstant()) {
+                if (!orderKey.getExpr().isConstant() && !(orderKey.getExpr() instanceof Slot
+                        && dataTrait.isUniformAndNotNull((Slot) orderKey.getExpr()))) {
                     orderKeysWithoutConstBuilder.add(orderKey);
                 }
             }
