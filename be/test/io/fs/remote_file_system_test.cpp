2b278524cd9b7b8e76964a363f16859aa096fe38
zhiqiang-hhhh <hezhiqiang@selectdb.com>
2024-12-31 22:01:44 +0800

    FIX UT

--- be/test/io/fs/remote_file_system_test.cpp
+++ be/test/io/fs/remote_file_system_test.cpp
@@ -410,10 +410,10 @@ TEST_F(RemoteFileSystemTest, TestHdfsFileSystem) {
 
 TEST_F(RemoteFileSystemTest, TestS3FileSystem) {
     std::unique_ptr<ThreadPool> _pool;
-    ThreadPoolBuilder("S3FileUploadThreadPool")
-            .set_min_threads(5)
-            .set_max_threads(10)
-            .build(&_pool);
+    std::ignore = ThreadPoolBuilder("S3FileUploadThreadPool")
+                          .set_min_threads(5)
+                          .set_max_threads(10)
+                          .build(&_pool);
     ExecEnv::GetInstance()->_s3_file_upload_thread_pool = std::move(_pool);
     S3Conf s3_conf;
     S3URI s3_uri(s3_location);
