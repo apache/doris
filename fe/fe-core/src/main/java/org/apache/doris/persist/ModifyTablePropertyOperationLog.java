aa8c93cfe28a12729dce9e60d3791c9ccfca6df4
Uniqueyou <wangyixuan@selectdb.com>
2024-12-31 09:52:43 +0800

    branch-2.1: [fix](sql) Wrong result for alter set property #46028 (#46141)
    pick https://github.com/apache/doris/pull/46028

--- fe/fe-core/src/main/java/org/apache/doris/persist/ModifyTablePropertyOperationLog.java
+++ fe/fe-core/src/main/java/org/apache/doris/persist/ModifyTablePropertyOperationLog.java
@@ -51,7 +51,8 @@ public class ModifyTablePropertyOperationLog implements Writable {
         StringBuilder sb = new StringBuilder();
         sb.append("SET (");
         for (Map.Entry<String, String> entry : properties.entrySet()) {
-            sb.append(entry.getKey()).append("=").append(entry.getValue()).append(",");
+            sb.append("\"").append(entry.getKey()).append("\"").append(" = ").append("\"").append(entry.getValue())
+                    .append("\" ").append(",");
         }
         sb.deleteCharAt(sb.length() - 1); // remove last ','
         sb.append(")");
@@ -81,5 +82,9 @@ public class ModifyTablePropertyOperationLog implements Writable {
 
     public String toJson()  {
         return GsonUtils.GSON.toJson(this);
+    }
+
+    public String toSql() {
+        return sql;
     }
 }
