619b4b9d974063b84c1b3309701cbeda4610eced
deardeng <dengxin@selectdb.com>
2025-01-01 23:44:12 +0800

    [fix](report) report queue size based on the number of Bes and config (#46190)
    A large cluster has many BE nodes, and the report occasionally
    encounters a "queue full" error. The report queue size will be modified
    based on the number of BE instances.

--- fe/fe-core/src/main/java/org/apache/doris/master/ReportHandler.java
+++ fe/fe-core/src/main/java/org/apache/doris/master/ReportHandler.java
@@ -221,11 +221,13 @@ public class ReportHandler extends Daemon {
 
     private void putToQueue(ReportTask reportTask) throws Exception {
         int currentSize = reportQueue.size();
-        if (currentSize > Config.report_queue_size) {
-            LOG.warn("the report queue size exceeds the limit: {}. current: {}", Config.report_queue_size, currentSize);
+        int maxReportQueueSize = Math.max(Config.report_queue_size,
+                10 * Env.getCurrentSystemInfo().getAllBackendIds().size());
+        if (currentSize > maxReportQueueSize) {
+            LOG.warn("the report queue size exceeds the limit: {}. current: {}", maxReportQueueSize, currentSize);
             throw new Exception(
                     "the report queue size exceeds the limit: "
-                            + Config.report_queue_size + ". current: " + currentSize);
+                            + maxReportQueueSize + ". current: " + currentSize);
         }
 
         BackendReportType backendReportType = new BackendReportType(reportTask.beId, reportTask.reportType);
