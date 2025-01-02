866c8d7ceed5bd905e5bfbfc69b8068407fff2e5
liujiwen <liujiwen@selectdb.com>
2025-01-02 20:20:51 +0800

    [feature](sql-dialect)support convert hive view and presto view use sql convertor service

--- regression-test/suites/dialect_compatible/sql/viewrewrite.sql
+++ regression-test/suites/dialect_compatible/sql/viewrewrite.sql
@@ -3,7 +3,7 @@ set sql_dialect = hive;
 
 --create catalog 
 drop catalog if exists hive;
-create catalog hive properties('type' = 'hms', 'hive.metastore.uris' = 'thrift://10.16.10.6:9083');
+create catalog hive properties('type' = 'hms', 'hive.metastore.uris' = 'thrift://192.168.0.1:9083');
 --switch catalog
 switch hive;
 --test view rewrite
