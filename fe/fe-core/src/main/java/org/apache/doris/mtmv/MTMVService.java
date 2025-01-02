qzsee/incubator-doris

[Improve](mtmv) skip the generation of invalid task for refresh mtmv

fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVService.java
@@ -198,12 +198,21 @@ public class MTMVService implements EventListener {
             try {
                 // check if mtmv should trigger by event
                 MTMV mtmv = (MTMV) MTMVUtil.getTable(baseTableInfo);
-                if (mtmv.getRefreshInfo().getRefreshTriggerInfo().getRefreshTrigger().equals(RefreshTrigger.COMMIT)) {
+                if (canRefresh(mtmv, baseTableInfo)) {
                     jobManager.onCommit(mtmv);
                 }
             } catch (Exception e) {
                 throw new EventException(e);
             }
         }
+    }
+
+    private boolean canRefresh(MTMV mtmv, BaseTableInfo baseTableInfo)  {
+        if (mtmv.getExcludedTriggerTables().contains(baseTableInfo.getTableName())) {
+            LOG.info("skip refresh mtmv: {}, because exclude trigger table: {}",
+                    mtmv.getName(), baseTableInfo.getTableName());
+            return false;
+        }
+        return mtmv.getRefreshInfo().getRefreshTriggerInfo().getRefreshTrigger().equals(RefreshTrigger.COMMIT);
     }
 }

apache/doris

fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVService.java
@@ -198,12 +198,21 @@ public class MTMVService implements EventListener {
             try {
                 // check if mtmv should trigger by event
                 MTMV mtmv = (MTMV) MTMVUtil.getTable(baseTableInfo);
-                if (mtmv.getRefreshInfo().getRefreshTriggerInfo().getRefreshTrigger().equals(RefreshTrigger.COMMIT)) {
+                if (canRefresh(mtmv, table)) {
                     jobManager.onCommit(mtmv);
                 }
             } catch (Exception e) {
                 throw new EventException(e);
             }
         }
+    }
+
+    private boolean canRefresh(MTMV mtmv, TableIf table)   {
+        if (mtmv.getExcludedTriggerTables().contains(table.getName())) {
+            LOG.info("skip refresh mtmv: {}, because exclude trigger table: {}",
+                    mtmv.getName(), table.getName());
+            return false;
+        }
+        return mtmv.getRefreshInfo().getRefreshTriggerInfo().getRefreshTrigger().equals(RefreshTrigger.COMMIT);
     }
 }