7347b95ec7ca44b0cdcc898533c61feca9e1fbf3
yujun <yujun@selectdb.com>
2025-01-02 17:59:06 +0800

    range inference union operate

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/SimplifyRange.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/SimplifyRange.java
@@ -21,32 +21,13 @@ import org.apache.doris.nereids.rules.expression.ExpressionPatternMatcher;
 import org.apache.doris.nereids.rules.expression.ExpressionPatternRuleFactory;
 import org.apache.doris.nereids.rules.expression.ExpressionRewriteContext;
 import org.apache.doris.nereids.rules.expression.ExpressionRuleType;
-import org.apache.doris.nereids.rules.expression.rules.RangeInference.DiscreteValue;
-import org.apache.doris.nereids.rules.expression.rules.RangeInference.EmptyValue;
-import org.apache.doris.nereids.rules.expression.rules.RangeInference.RangeValue;
-import org.apache.doris.nereids.rules.expression.rules.RangeInference.UnknownValue;
 import org.apache.doris.nereids.rules.expression.rules.RangeInference.ValueDesc;
 import org.apache.doris.nereids.trees.expressions.CompoundPredicate;
-import org.apache.doris.nereids.trees.expressions.EqualTo;
 import org.apache.doris.nereids.trees.expressions.Expression;
-import org.apache.doris.nereids.trees.expressions.GreaterThan;
-import org.apache.doris.nereids.trees.expressions.GreaterThanEqual;
-import org.apache.doris.nereids.trees.expressions.InPredicate;
-import org.apache.doris.nereids.trees.expressions.LessThan;
-import org.apache.doris.nereids.trees.expressions.LessThanEqual;
-import org.apache.doris.nereids.trees.expressions.Or;
-import org.apache.doris.nereids.trees.expressions.literal.Literal;
-import org.apache.doris.nereids.util.ExpressionUtils;
 
-import com.google.common.collect.BoundType;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Range;
-import org.apache.commons.lang3.NotImplementedException;
 
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * This class implements the function to simplify expression range.
@@ -84,92 +65,6 @@ public class SimplifyRange implements ExpressionPatternRuleFactory {
     /** rewrite */
     public static Expression rewrite(CompoundPredicate expr, ExpressionRewriteContext context) {
         ValueDesc valueDesc = (new RangeInference()).getValue(expr, context);
-        return INSTANCE.getExpression(valueDesc);
-    }
-
-    private Expression getExpression(ValueDesc value) {
-        if (value instanceof EmptyValue) {
-            return getExpression((EmptyValue) value);
-        } else if (value instanceof DiscreteValue) {
-            return getExpression((DiscreteValue) value);
-        } else if (value instanceof RangeValue) {
-            return getExpression((RangeValue) value);
-        } else if (value instanceof UnknownValue) {
-            return getExpression((UnknownValue) value);
-        } else {
-            throw new NotImplementedException("not implements");
-        }
-    }
-
-    private Expression getExpression(EmptyValue value) {
-        Expression reference = value.getReference();
-        return ExpressionUtils.falseOrNull(reference);
-    }
-
-    private Expression getExpression(RangeValue value) {
-        Expression reference = value.getReference();
-        Range<Literal> range = value.getRange();
-        List<Expression> result = Lists.newArrayList();
-        if (range.hasLowerBound()) {
-            if (range.lowerBoundType() == BoundType.CLOSED) {
-                result.add(new GreaterThanEqual(reference, range.lowerEndpoint()));
-            } else {
-                result.add(new GreaterThan(reference, range.lowerEndpoint()));
-            }
-        }
-        if (range.hasUpperBound()) {
-            if (range.upperBoundType() == BoundType.CLOSED) {
-                result.add(new LessThanEqual(reference, range.upperEndpoint()));
-            } else {
-                result.add(new LessThan(reference, range.upperEndpoint()));
-            }
-        }
-        if (!result.isEmpty()) {
-            return ExpressionUtils.and(result);
-        } else {
-            return ExpressionUtils.trueOrNull(reference);
-        }
-    }
-
-    private Expression getExpression(DiscreteValue value) {
-        Expression reference = value.getReference();
-        Set<Literal> values = value.getValues();
-        // NOTICE: it's related with `InPredicateToEqualToRule`
-        // They are same processes, so must change synchronously.
-        if (values.size() == 1) {
-            return new EqualTo(reference, values.iterator().next());
-
-            // this condition should as same as OrToIn, or else meet dead loop
-        } else if (values.size() < OrToIn.REWRITE_OR_TO_IN_PREDICATE_THRESHOLD) {
-            Iterator<Literal> iterator = values.iterator();
-            return new Or(new EqualTo(reference, iterator.next()), new EqualTo(reference, iterator.next()));
-        } else {
-            return new InPredicate(reference, Lists.newArrayList(values));
-        }
-    }
-
-    private Expression getExpression(UnknownValue value) {
-        List<ValueDesc> sourceValues = value.getSourceValues();
-        Expression originExpr = value.getOriginExpr();
-        if (sourceValues.isEmpty()) {
-            return originExpr;
-        }
-        List<Expression> sourceExprs = Lists.newArrayListWithExpectedSize(sourceValues.size());
-        for (ValueDesc sourceValue : sourceValues) {
-            Expression expr = getExpression(sourceValue);
-            if (value.isAnd()) {
-                sourceExprs.addAll(ExpressionUtils.extractConjunction(expr));
-            } else {
-                sourceExprs.addAll(ExpressionUtils.extractDisjunction(expr));
-            }
-        }
-        Expression result = value.isAnd() ? ExpressionUtils.and(sourceExprs) : ExpressionUtils.or(sourceExprs);
-        result = FoldConstantRuleOnFE.evaluate(result, value.getExpressionRewriteContext());
-        // ATTN: we must return original expr, because OrToIn is implemented with MutableState,
-        //   newExpr will lose these states leading to dead loop by OrToIn -> SimplifyRange -> FoldConstantByFE
-        if (result.equals(originExpr)) {
-            return originExpr;
-        }
-        return result;
+        return valueDesc.toExpression();
     }
 }
