ad9340d3b119dee86a14d052bafb354dc2d7feed
yujun <yujun@selectdb.com>
2025-01-03 18:25:38 +0800

    fix wait master fe ip 10s

--- docker/runtime/doris-compose/cluster.py
+++ docker/runtime/doris-compose/cluster.py
@@ -140,12 +140,9 @@ def gen_subnet_prefix16():
 
 def get_master_fe_endpoint(cluster_name):
     master_fe_ip_file = get_cluster_path(cluster_name) + "/status/master_fe_ip"
-    max_retries = 10
-    for attempt in range(max_retries):
-        if os.path.exists(master_fe_ip_file):
-            with open(master_fe_ip_file, "r") as f:
-                return "{}:{}".format(f.read().strip(), FE_QUERY_PORT)
-        time.sleep(1)
+    if os.path.exists(master_fe_ip_file):
+        with open(master_fe_ip_file, "r") as f:
+            return "{}:{}".format(f.read().strip(), FE_QUERY_PORT)
     try:
         cluster = Cluster.load(cluster_name)
         LOG.info("master file not exist, master ip get from node 1")
--- docker/runtime/doris-compose/command.py
+++ docker/runtime/doris-compose/command.py
@@ -625,8 +625,13 @@ class UpCommand(Command):
             LOG.info("after Waiting for FE master to be elected...")
             if cluster.is_cloud and args.sql_mode_node_mgr:
                 db_mgr = database.get_db_mgr(args.NAME, False)
-                master_fe_endpoint = CLUSTER.get_master_fe_endpoint(
-                    cluster.name)
+                master_fe_endpoint = ""
+                for i in range(10):
+                    master_fe_endpoint = CLUSTER.get_master_fe_endpoint(
+                        cluster.name)
+                    if master_fe_endpoint:
+                        break
+                    time.sleep(1)
                 # Add FEs except master_fe
                 for fe in cluster.get_all_nodes(CLUSTER.Node.TYPE_FE):
                     fe_endpoint = f"{fe.get_ip()}:{CLUSTER.FE_EDITLOG_PORT}"
@@ -1071,7 +1076,8 @@ class ListCommand(Command):
                 if services is None:
                     return COMPOSE_BAD, {}
                 return COMPOSE_GOOD, {
-                    service: ComposeService(
+                    service:
+                    ComposeService(
                         service,
                         list(service_conf["networks"].values())[0]
                         ["ipv4_address"], service_conf["image"])
