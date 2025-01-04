ba7ba36113241b16fbb2be382e6ec74349b62b35
924060929 <lanhuajian@selectdb.com>
2025-01-04 22:56:48 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -1518,7 +1518,7 @@ public class SessionVariable implements Serializable, Writable {
                     "Skip most optimization rules and quickly analyze insert into values statements"
             }
     )
-    private boolean enableFastAnalyzeInsertIntoValues = true;
+    private boolean enableFastAnalyzeInsertIntoValues = false;
 
     @VariableMgr.VarAttr(name = ENABLE_FUNCTION_PUSHDOWN, fuzzy = true)
     public boolean enableFunctionPushdown = false;
