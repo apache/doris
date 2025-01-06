e617e2254e91be33d236d916387c5bc076be2876
BiteTheDDDDt <pxl290@qq.com>
2025-01-06 17:57:04 +0800

    fix

--- be/src/vec/columns/column_string.cpp
+++ be/src/vec/columns/column_string.cpp
@@ -389,7 +389,7 @@ size_t ColumnStr<T>::get_max_row_byte_size() const {
     size_t max_size = 0;
     size_t num_rows = offsets.size();
     for (size_t i = 0; i < num_rows; ++i) {
-        max_size = std::max(max_size, size_at(i));
+        max_size = std::max(max_size, size_t(size_at(i)));
     }
 
     return max_size + sizeof(uint32_t);
