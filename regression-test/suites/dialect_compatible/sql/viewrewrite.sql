6c8a8f97fd50d5ddfda4ef8ef7ae8b61f0cdab06
liujiwen <liujiwen@selectdb.com>
2025-01-06 19:52:46 +0800

    fix test case

--- regression-test/suites/dialect_compatible/sql/viewrewrite.sql
+++ regression-test/suites/dialect_compatible/sql/viewrewrite.sql
@@ -4,11 +4,9 @@ set sql_dialect = hive;
 --create catalog 
 drop catalog if exists hive;
 create catalog hive properties('type' = 'hms', 'hive.metastore.uris' = 'thrift://192.168.0.1:9083');
---switch catalog
-switch hive;
 --test view rewrite
-select * from default.department_view;
-select * from default.department_nesting_view;
+select * from hive.default.department_view;
+select * from hive.default.department_nesting_view;
 
 
 
