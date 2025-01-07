ae36af95f50dd2b0dd500c97bcb8193e14af4af4
yiguolei <guolei@selectdb.com>
2025-01-07 10:15:35 +0800

    [bugfix](be) the config could not be modified on line (#46470)
    This value is saved in some type description, could not be modified
    online. User should restart BE after modify this value.
    For example,
        static TypeDescriptor create_string_type() {
            TypeDescriptor ret;
            ret.type = TYPE_STRING;
            ret.len = config::string_type_length_soft_limit_bytes;
            return ret;
        }

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -915,7 +915,7 @@ DEFINE_String(rpc_load_balancer, "rr");
 
 // a soft limit of string type length, the hard limit is 2GB - 4, but if too long will cause very low performance,
 // so we set a soft limit, default is 1MB
-DEFINE_mInt32(string_type_length_soft_limit_bytes, "1048576");
+DEFINE_Int32(string_type_length_soft_limit_bytes, "1048576");
 
 DEFINE_Validator(string_type_length_soft_limit_bytes,
                  [](const int config) -> bool { return config > 0 && config <= 2147483643; });
