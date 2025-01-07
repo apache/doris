5e682f9e2fc225f5d1d9caaa23621cc04b870e91
Socrates <suyiteng@selectdb.com>
2025-01-07 11:23:50 +0800

    fix hive-metastore

--- docker/thirdparties/docker-compose/hive/scripts/hive-metastore.sh
+++ docker/thirdparties/docker-compose/hive/scripts/hive-metastore.sh
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-set -e
+set -e -x
 
 nohup /opt/hive/bin/hive --service metastore &
 
@@ -30,11 +30,11 @@ hadoop fs -mkdir -p /user/doris/suites/
 lockfile1="mnt/scripts/run-data.lock"
 
 # wait lockfile
-while [ -f "$lockfile1" ]; do
+while [[ -f "${lockfile1}" ]]; do
     sleep 10
 done
 
-touch "$lockfile1"
+touch "${lockfile1}"
 
 DATA_DIR="/mnt/scripts/data/"
 find "${DATA_DIR}" -type f -name "run.sh" -print0 | xargs -0 -n 1 -P 10 -I {} sh -c '
@@ -45,16 +45,16 @@ find "${DATA_DIR}" -type f -name "run.sh" -print0 | xargs -0 -n 1 -P 10 -I {} sh
     echo "Script: {} executed in $EXECUTION_TIME seconds"
 '
 
-rm -f "$lockfile1"
+rm -f "${lockfile1}"
 
 lockfile2="mnt/scripts/download-data.lock"
 
 # wait lockfile
-while [ -f "$lockfile2" ]; do
+while [[ -f "${lockfile2}" ]]; do
     sleep 10
 done
 
-touch "$lockfile2"
+touch "${lockfile2}"
 
 # if you test in your local，better use # to annotation section about tpch1.db
 if [[ ! -d "/mnt/scripts/tpch1.db" ]]; then
@@ -89,39 +89,39 @@ else
     echo "/mnt/scripts/tvf_data exist, continue !"
 fi
 
-rm -f "$lockfile2"
+rm -f "${lockfile2}"
 
 # put data file
 ## put tpch1
-if [ -z "$(ls /mnt/scripts/tpch1.db)" ]; then
+if [[ -z "$(ls /mnt/scripts/tpch1.db)" ]]; then
     echo "tpch1.db does not exist"
     exit 1
 fi
 hadoop fs -mkdir -p /user/doris/
 hadoop fs -put /mnt/scripts/tpch1.db /user/doris/
-if [ -z "$(hadoop fs -ls /user/doris/tpch1.db)" ]; then
+if [[ -z "$(hadoop fs -ls /user/doris/tpch1.db)" ]]; then
     echo "tpch1.db put failed"
     exit 1
 fi
 
 ## put paimon1
-if [ -z "$(ls /mnt/scripts/paimon1)" ]; then
+if [[ -z "$(ls /mnt/scripts/paimon1)" ]]; then
     echo "paimon1 does not exist"
     exit 1
 fi
 hadoop fs -put /mnt/scripts/paimon1 /user/doris/
-if [ -z "$(hadoop fs -ls /user/doris/paimon1)" ]; then
+if [[ -z "$(hadoop fs -ls /user/doris/paimon1)" ]]; then
     echo "paimon1 put failed"
     exit 1
 fi
 
 ## put tvf_data
-if [ -z "$(ls /mnt/scripts/tvf_data)" ]; then
+if [[ -z "$(ls /mnt/scripts/tvf_data)" ]]; then
     echo "tvf_data does not exist"
     exit 1
 fi
 hadoop fs -put /mnt/scripts/tvf_data /user/doris/
-if [ -z "$(hadoop fs -ls /user/doris/tvf_data)" ]; then
+if [[ -z "$(hadoop fs -ls /user/doris/tvf_data)" ]]; then
     echo "tvf_data put failed"
     exit 1
 fi
@@ -143,7 +143,7 @@ START_TIME=$(date +%s)
 hive -f /mnt/scripts/create_view_scripts/create_view.hql
 END_TIME=$(date +%s)
 EXECUTION_TIME=$((END_TIME - START_TIME))
-echo "Script: create_view.hql executed in $EXECUTION_TIME seconds"
+echo "Script: create_view.hql executed in ${EXECUTION_TIME} seconds"
 
 touch /mnt/SUCCESS
 
