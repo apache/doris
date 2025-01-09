fff7068e0eec01347b27cf3230df8ac1bf93662e
seawinde <wusi@selectdb.com>
2025-01-09 19:04:50 +0800

    add more test

--- regression-test/suites/nereids_rules_p0/mv/join/null_aware_anti/null_aware_left_anti_join.groovy
+++ regression-test/suites/nereids_rules_p0/mv/join/null_aware_anti/null_aware_left_anti_join.groovy
@@ -45,6 +45,18 @@ suite("null_aware_anti") {
     """
 
     sql """
+    insert into orders values
+    (1, 1, 'o', 9.5, '2023-12-08', 'a', 'b', 1, 'yy'),
+    (1, 1, 'o', 10.5, '2023-12-08', 'a', 'b', 1, 'yy'),
+    (2, 1, 'o', 11.5, '2023-12-09', 'a', 'b', 1, 'yy'),
+    (3, 1, 'o', 12.5, '2023-12-10', 'a', 'b', 1, 'yy'),
+    (3, 1, 'o', 33.5, '2023-12-10', 'a', 'b', 1, 'yy'),
+    (4, 2, 'o', 43.2, '2023-12-11', 'c','d',2, 'mm'),
+    (5, 2, 'o', 56.2, '2023-12-12', 'c','d',2, 'mi'),
+    (5, 2, 'o', 1.2, '2023-12-12', 'c','d',2, 'mi');  
+    """
+
+    sql """
     drop table if exists lineitem
     """
 
@@ -74,6 +86,14 @@ suite("null_aware_anti") {
     )
     """
 
+    sql """ insert into lineitem values
+    (1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-08', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
+    (2, 4, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-09', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
+    (3, 2, 4, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-10', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
+    (4, 3, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-11', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
+    (5, 2, 3, 6, 7.5, 8.5, 9.5, 10.5, 'k', 'o', '2023-12-12', '2023-12-12', '2023-12-13', 'c', 'd', 'xxxxxxxxx');
+    """
+
     sql """
     drop table if exists partsupp
     """
@@ -93,6 +113,37 @@ suite("null_aware_anti") {
     )
     """
 
+    sql """
+    insert into partsupp values
+    (2, 3, 9, 10.01, 'supply1'),
+    (2, 3, 10, 11.01, 'supply2');
+    """
+
+    sql """
+    drop table if exists partsupp_nullable
+    """
+
+    sql """
+    CREATE TABLE IF NOT EXISTS partsupp_nullable (
+      ps_partkey     INTEGER NULL,
+      ps_suppkey     INTEGER NULL,
+      ps_availqty    INTEGER NULL,
+      ps_supplycost  DECIMALV3(15,2) NULL,
+      ps_comment     VARCHAR(199) NULL 
+    )
+    DUPLICATE KEY(ps_partkey, ps_suppkey)
+    DISTRIBUTED BY HASH(ps_partkey) BUCKETS 3
+    PROPERTIES (
+      "replication_num" = "1"
+    )
+    """
+
+    sql """
+    insert into partsupp_nullable values
+    (2, 3, 9, 10.01, 'supply1'),
+    (2, 4, 9, 10.01, 'supply1'),
+    (2, 3, 10, 11.01, 'supply2');
+    """
 
     sql """
     drop table if exists orders_nullable
@@ -117,26 +168,6 @@ suite("null_aware_anti") {
     )
     """
 
-    sql """ insert into lineitem values
-    (1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-08', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
-    (2, 4, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-09', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
-    (3, 2, 4, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-10', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
-    (4, 3, 3, 4, 5.5, 6.5, 7.5, 8.5, 'o', 'k', '2023-12-11', '2023-12-09', '2023-12-10', 'a', 'b', 'yyyyyyyyy'),
-    (5, 2, 3, 6, 7.5, 8.5, 9.5, 10.5, 'k', 'o', '2023-12-12', '2023-12-12', '2023-12-13', 'c', 'd', 'xxxxxxxxx');
-    """
-
-    sql """
-    insert into orders values
-    (1, 1, 'o', 9.5, '2023-12-08', 'a', 'b', 1, 'yy'),
-    (1, 1, 'o', 10.5, '2023-12-08', 'a', 'b', 1, 'yy'),
-    (2, 1, 'o', 11.5, '2023-12-09', 'a', 'b', 1, 'yy'),
-    (3, 1, 'o', 12.5, '2023-12-10', 'a', 'b', 1, 'yy'),
-    (3, 1, 'o', 33.5, '2023-12-10', 'a', 'b', 1, 'yy'),
-    (4, 2, 'o', 43.2, '2023-12-11', 'c','d',2, 'mm'),
-    (5, 2, 'o', 56.2, '2023-12-12', 'c','d',2, 'mi'),
-    (5, 2, 'o', 1.2, '2023-12-12', 'c','d',2, 'mi');  
-    """
-
     sql """
     insert into orders_nullable values
     (1, 1, 'o', 9.5, '2023-12-08', 'a', 'b', 1, 'yy'),
@@ -149,11 +180,6 @@ suite("null_aware_anti") {
     (5, 2, 'o', 1.2, '2023-12-12', 'c','d',2, 'mi');  
     """
 
-    sql """
-    insert into partsupp values
-    (2, 3, 9, 10.01, 'supply1'),
-    (2, 3, 10, 11.01, 'supply2');
-    """
 
     sql """analyze table lineitem with sync;"""
     sql """analyze table orders with sync;"""
@@ -164,6 +190,7 @@ suite("null_aware_anti") {
     sql """alter table orders modify column o_comment set stats ('row_count'='8');"""
     sql """alter table orders_nullable modify column o_comment set stats ('row_count'='8');"""
     sql """alter table partsupp modify column ps_comment set stats ('row_count'='2');"""
+    sql """alter table partsupp_nullable modify column ps_comment set stats ('row_count'='3');"""
 
 
     def mv1_0 =
@@ -229,4 +256,296 @@ suite("null_aware_anti") {
     async_mv_rewrite_success(db, mv2_1, query2_1, "mv2_1")
     order_qt_query2_1_after "${query2_1}"
     sql """ DROP MATERIALIZED VIEW IF EXISTS mv2_1"""
+
+    // left and right is complex expression
+    def mv2_2 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+            select bin(o_custkey) from orders_nullable
+            );
+            """
+    def query2_2 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+            select bin(o_custkey) from orders_nullable
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query2_2_before "${query2_2}"
+    // test NULL_AWARE_LEFT_ANTI_JOIN, should success
+    async_mv_rewrite_success(db, mv2_2, query2_2, "mv2_2")
+    order_qt_query2_2_after "${query2_2}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv2_2"""
+
+
+    def mv2_3 =
+            """
+            select lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+            select o_custkey from orders_nullable
+            );
+            """
+    def query2_3 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+            select bin(o_custkey) from orders_nullable
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query2_3_before "${query2_3}"
+    // left and right is not consistant
+    async_mv_rewrite_fail(db, mv2_3, query2_3, "mv2_3")
+    order_qt_query2_3_after "${query2_3}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv2_3"""
+
+
+
+    // multi not in with 'or'
+    def mv3_0 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select bin(o_custkey) from orders_nullable
+            )
+            or
+            l_partkey not in (
+                select ps_suppkey from partsupp_nullable
+            );
+            """
+    def query3_0 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select bin(o_custkey) from orders_nullable
+            )
+            or
+            l_partkey not in (
+                select ps_suppkey from partsupp_nullable
+            );
+            """
+    order_qt_query3_0_before "${query3_0}"
+    // markJoinConjuncts should fail
+    async_mv_rewrite_fail(db, mv3_0, query3_0, "mv3_0")
+    order_qt_query3_0_after "${query3_0}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv3_0"""
+
+
+    // multi not in with 'and'
+    def mv3_1 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select o_custkey from orders_nullable
+            )
+            or
+            l_partkey not in (
+                select ps_suppkey from partsupp_nullable
+            );
+            """
+    def query3_1 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select o_custkey from orders_nullable
+            )
+            or
+            l_partkey not in (
+                select ps_suppkey from partsupp_nullable
+            );
+            """
+    order_qt_query3_1_before "${query3_1}"
+    // markJoinConjuncts should fail
+    async_mv_rewrite_fail(db, mv3_1, query3_1, "mv3_1")
+    order_qt_query3_1_after "${query3_1}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv3_1"""
+
+
+    // not in [null literal]
+    def mv4_0 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                null, 1, 2
+            );
+            """
+    def query4_0 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                null, 1, 2
+            );
+            """
+    order_qt_query4_0_before "${query4_0}"
+    async_mv_rewrite_success_without_check_chosen(db, mv4_0, query4_0, "mv4_0")
+    order_qt_query4_0_after "${query4_0}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv4_0"""
+
+
+    // not in is complex plan, such as join + group by + limit + order by + filter + union all + with
+    def mv5_0 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+                group by
+                bin(o_custkey)
+            )
+            """
+    def query5_0 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+                group by
+                bin(o_custkey)
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query5_0_before "${query5_0}"
+    // because agg under join
+    async_mv_rewrite_fail(db, mv5_0, query5_0, "mv5_0")
+    order_qt_query5_0_after "${query5_0}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv5_0"""
+
+
+    def mv5_1 =
+            """
+            select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+                group by
+                bin(o_custkey)
+            """
+    def query5_1 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+                group by
+                bin(o_custkey)
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query5_1_before "${query5_1}"
+    async_mv_rewrite_success(db, mv5_1, query5_1, "mv5_1")
+    order_qt_query5_1_after "${query5_1}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv5_1"""
+
+
+    def mv5_2 =
+            """
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+            """
+    def query5_2 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query5_2_before "${query5_2}"
+    // because agg under join
+    async_mv_rewrite_success(db, mv5_2, query5_2, "mv5_2")
+    order_qt_query5_2_after "${query5_2}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv5_2"""
+
+
+    def mv5_3 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+            );
+            """
+    def query5_3 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join
+                partsupp_nullable on o_orderkey = ps_partkey
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query5_3_before "${query5_3}"
+    async_mv_rewrite_success(db, mv5_3, query5_3, "mv5_3")
+    order_qt_query5_3_after "${query5_3}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv5_3"""
+
+
+    def mv5_4 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join 
+                partsupp_nullable on o_orderkey = ps_partkey
+                where o_custkey > 4
+                order by o_custkey
+                limit 2
+            );
+            """
+    def query5_4 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where bin(L_ORDERKEY) not in (
+                select bin(o_custkey) from orders_nullable
+                left join 
+                partsupp_nullable on o_orderkey = ps_partkey
+                where o_custkey > 4
+                order by o_custkey
+                limit 2
+            ) and L_LINENUMBER = 4;
+            """
+    order_qt_query5_4_before "${query5_4}"
+    async_mv_rewrite_fail(db, mv5_4, query5_4, "mv5_4")
+    order_qt_query5_4_after "${query5_4}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv5_4"""
+
+
+    // nest not in
+    def mv6_0 =
+            """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select o_custkey from orders_nullable
+                where o_custkey not in (
+                    select ps_suppkey from partsupp_nullable
+                )
+            );
+            """
+    def query6_0 = """
+            select  lineitem.L_LINENUMBER
+            from lineitem
+            where L_ORDERKEY not in (
+                select o_custkey from orders_nullable
+                where o_custkey not in (
+                    select ps_suppkey from partsupp_nullable
+                )
+            );
+            """
+    order_qt_query6_0_before "${query6_0}"
+    async_mv_rewrite_success(db, mv6_0, query6_0, "mv6_0")
+    order_qt_query6_0_after "${query6_0}"
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv6_0"""
+
 }
