1b8587462a27a089dc5d45a5e2eb0f77ece571b0
924060929 <lanhuajian@selectdb.com>
2025-01-13 15:43:41 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/nereids/properties/ChildrenPropertiesRegulator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/properties/ChildrenPropertiesRegulator.java
@@ -218,8 +218,10 @@ public class ChildrenPropertiesRegulator extends PlanVisitor<List<List<PhysicalP
         boolean isEnableBucketShuffleJoin = ConnectContext.get().getSessionVariable().isEnableBucketShuffleJoin();
         if (!isEnableBucketShuffleJoin) {
             return true;
-        } else if (otherSideSpec.getShuffleType() != ShuffleType.EXECUTION_BUCKETED
-                || !(oneSidePlan instanceof GroupPlan)) {
+        } else if (
+                (!SessionVariable.canUseNereidsDistributePlanner()
+                    && otherSideSpec.getShuffleType() != ShuffleType.EXECUTION_BUCKETED
+                ) || !(oneSidePlan instanceof GroupPlan)) {
             return false;
         } else {
             PhysicalOlapScan candidate = findDownGradeBucketShuffleCandidate((GroupPlan) oneSidePlan);
