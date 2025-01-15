f4f226ac8a17880fe8d61941fe0f042421e80998
chunping <zhangchunping@selectdb.com>
2025-01-15 22:12:48 +0800

    [improvement](test)aovid cluster contain keyword that will make grant… (#47010)
    ![image](https://github.com/user-attachments/assets/248d112c-10d4-4790-9d98-a0ec2d58b694)

--- regression-test/suites/auth_p0/test_frontends_disks_auth.groovy
+++ regression-test/suites/auth_p0/test_frontends_disks_auth.groovy
@@ -29,7 +29,7 @@ suite("test_frontends_disks_auth","p0,auth") {
         def clusters = sql " SHOW CLUSTERS; "
         assertTrue(!clusters.isEmpty())
         def validCluster = clusters[0][0]
-        sql """GRANT USAGE_PRIV ON CLUSTER ${validCluster} TO ${user}""";
+        sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${user}""";
     }
 
     sql """grant select_priv on regression_test to ${user}"""
--- regression-test/suites/external_table_p0/lower_case/test_meta_cache_select_without_refresh.groovy
+++ regression-test/suites/external_table_p0/lower_case/test_meta_cache_select_without_refresh.groovy
@@ -32,7 +32,7 @@ suite("test_meta_cache_select_without_refresh", "p0,external,doris,external_dock
         def clusters = sql " SHOW CLUSTERS; "
         assertTrue(!clusters.isEmpty())
         def validCluster = clusters[0][0]
-        sql """GRANT USAGE_PRIV ON CLUSTER ${validCluster} TO ${jdbcUser}""";
+        sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${jdbcUser}""";
     }
 
     sql """grant all on *.*.* to ${jdbcUser}"""
