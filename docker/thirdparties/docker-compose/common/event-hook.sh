882ec31361852a08ccee59ec7ffb2b26e8f34eec
zgxme <zhenggaoxiong@selectdb.com>
2025-01-16 17:59:37 +0800

    [Opt](external-docker) Modify kerberos network mode to host (#47043)
    ### What problem does this PR solve?
    In order to be able to perform kerberos testing on a multi-node doris
    cluster.

--- docker/thirdparties/docker-compose/common/event-hook.sh
+++ docker/thirdparties/docker-compose/common/event-hook.sh
@@ -16,18 +16,10 @@
 # specific language governing permissions and limitations
 # under the License.
 
-set -exuo pipefail
-
-TICKET_LIFETIME='30m'
-
-kinit -l "$TICKET_LIFETIME" -f -c /etc/trino/conf/presto-server-krbcc \
-      -kt /etc/trino/conf/presto-server.keytab presto-server/$(hostname -f)@LABS.TERADATA.COM
-
-kinit -l "$TICKET_LIFETIME" -f -c /etc/trino/conf/hive-presto-master-krbcc \
-      -kt /etc/trino/conf/hive-presto-master.keytab hive/$(hostname -f)@LABS.TERADATA.COM
-
-kinit -l "$TICKET_LIFETIME" -f -c /etc/trino/conf/hdfs-krbcc \
-      -kt /etc/hadoop/conf/hdfs.keytab hdfs/hadoop-master@LABS.TERADATA.COM
-
-kinit -l "$TICKET_LIFETIME" -f -c /etc/trino/conf/hive-krbcc \
-      -kt /etc/hive/conf/hive.keytab hive/hadoop-master@LABS.TERADATA.COM
+function exec_success_hook() {
+    echo "Executing success hook"
+    echo "Creating /tmp/success and /tmp/SUCCESS"
+    touch /tmp/success /tmp/SUCCESS
+    echo "Do not exit, just tailing /dev/null"
+    tail -f /dev/null
+}
