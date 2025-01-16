065d3fa49cf296a97b6d18792cacce2749eed8e9
shee <13843187+qzsee@users.noreply.github.com>
2025-01-16 11:40:28 +0800

    [Bug](mtmv) update mapping relation when mtmv occur alter (#46983)
    ### What problem does this PR solve?
    
    CREATE MATERIALIZED VIEW mv_a
    REFRESH COMPLETE ON commit
    DISTRIBUTED BY HASH(`sid`) BUCKETS 1 
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1"
    )
    AS  select * from stu limit 1;
    
    CREATE MATERIALIZED VIEW mv_aa
    REFRESH COMPLETE ON commit
    DISTRIBUTED BY HASH(`sid`) BUCKETS 1 
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1"
    )
    AS  select * from mv_a limit 1;
    
    alter MATERIALIZED VIEW mv_a RENAME mv_b
    
    CREATE TABLE `mv_a` (
      `sid` int(32) NULL,
      `sname` varchar(32) NULL,
    ) ENGINE=OLAP
    DUPLICATE KEY(`sid`)
    DISTRIBUTED BY HASH(`sid`) BUCKETS 1
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1"
    ); 
    
    CREATE MATERIALIZED VIEW mv_a
    REFRESH COMPLETE ON commit
    DISTRIBUTED BY HASH(`sid`) BUCKETS 1 
    PROPERTIES (
    "replication_allocation" = "tag.location.default: 1"
    )
    AS  select * from stu limit 1;
    
    select * from stu limit 1;
    
    java.lang.ClassCastException: class org.apache.doris.catalog.OlapTable cannot be cast to class org.apache.doris.catalog.MTMV (org.apache.doris.catalog.OlapTable and org.apache.doris.catalog.MTMV are in unnamed module of loader 'app')
    
    ---------
    
    Co-authored-by: garenshi <garenshi@tencent.com>

--- fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
@@ -550,6 +550,10 @@ public class Alter {
             throws UserException {
         ReplaceTableClause clause = (ReplaceTableClause) alterClauses.get(0);
         String newTblName = clause.getTblName();
+        Table newTable = db.getTableOrMetaException(newTblName);
+        if (newTable.getType() == TableType.MATERIALIZED_VIEW) {
+            throw new DdlException("replace table[" + newTblName + "] cannot be a materialized view");
+        }
         boolean swapTable = clause.isSwapTable();
         processReplaceTable(db, origTable, newTblName, swapTable);
     }
