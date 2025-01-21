145f9fdcf92438bb86ba73a4226149ddecf5f23d
Uniqueyou <wangyixuan@selectdb.com>
2025-01-21 11:58:10 +0800

    [fix](binlog) Fix table not gc binlog meta/records (#46981)
    db, table enable binlog
    ```
    *************************** 6. row ***************************
                                Name: db
                                Type: db
                                  Id: 1737094589956
                             Dropped: false
                        BinlogLength: 5
                          BinlogSize: 2092
            FirstBinlogCommittedTime: 1737094625988
    ReadableFirstBinlogCommittedTime: 2025-01-17 14:17:05
             LastBinlogCommittedTime: 1737094630182
     ReadableLastBinlogCommittedTime: 2025-01-17 14:17:10
                    BinlogTtlSeconds: 86400
                      BinlogMaxBytes: 9223372036854775807
                BinlogMaxHistoryNums: 9223372036854775807
    ```
    db disable binlog, table enable binlog
    db binlog record will be dropped
    ```
    *************************** 6. row ***************************
                                Name: db.t
                                Type: table
                                  Id: 1737094589958
                             Dropped: false
                        BinlogLength: 4
                          BinlogSize: 955
            FirstBinlogCommittedTime: 1737094625988
    ReadableFirstBinlogCommittedTime: 2025-01-17 14:17:05
             LastBinlogCommittedTime: 1737094630182
     ReadableLastBinlogCommittedTime: 2025-01-17 14:17:10
                    BinlogTtlSeconds: 86400
                      BinlogMaxBytes: 9223372036854775807
                BinlogMaxHistoryNums: 9223372036854775807
    ```
    db, table disable binlog
    ```
    *************************** 6. row ***************************
                                Name: db.t
                                Type: table
                                  Id: 1737094589958
                             Dropped: false
                        BinlogLength: 1
                          BinlogSize: 0
            FirstBinlogCommittedTime: NULL
    ReadableFirstBinlogCommittedTime: NULL
             LastBinlogCommittedTime: NULL
     ReadableLastBinlogCommittedTime: NULL
                    BinlogTtlSeconds: 86400
                      BinlogMaxBytes: 9223372036854775807
                BinlogMaxHistoryNums: 9223372036854775807
    ```

--- fe/fe-core/src/main/java/org/apache/doris/binlog/BinlogManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/binlog/BinlogManager.java
@@ -592,8 +592,10 @@ public class BinlogManager {
                 tombstones.add(dbTombstones);
             }
         }
+
         return tombstones;
     }
+
 
     public void replayGc(BinlogGcInfo binlogGcInfo) {
         lock.writeLock().lock();
