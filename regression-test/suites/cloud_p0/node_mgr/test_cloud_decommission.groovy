733532e56fb1ce180f90d3526cd7cfbea6f5838c
deardeng <dengxin@selectdb.com>
2025-01-21 17:48:13 +0800

    fix license

--- regression-test/suites/cloud_p0/node_mgr/test_cloud_decommission.groovy
+++ regression-test/suites/cloud_p0/node_mgr/test_cloud_decommission.groovy
@@ -3,7 +3,7 @@
 // distributed with this work for additional information
 // regarding copyright ownership.  The ASF licenses this file
 // to you under the Apache License, Version 2.0 (the
-// "License") you may not use this file except in compliance
+// "License"); you may not use this file except in compliance
 // with the License.  You may obtain a copy of the License at
 //
 //   http://www.apache.org/licenses/LICENSE-2.0
@@ -14,11 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 import org.apache.doris.regression.suite.ClusterOptions
-
 
 suite("cloud_decommission", 'p0, docker') {
     if (!isCloudMode()) {
