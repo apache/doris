e2f972f6bd2943d4b1c986151be4a84110655862
zzzxl1993 <yangsiyu@selectdb.com>
2025-01-21 20:18:38 +0800

    [feature](inverted index) add icu analyzer for minority language tokenization

--- be/src/olap/inverted_index_parser.cpp
+++ be/src/olap/inverted_index_parser.cpp
@@ -34,6 +34,8 @@ std::string inverted_index_parser_type_to_string(InvertedIndexParserType parser_
         return INVERTED_INDEX_PARSER_ENGLISH;
     case InvertedIndexParserType::PARSER_CHINESE:
         return INVERTED_INDEX_PARSER_CHINESE;
+    case InvertedIndexParserType::PARSER_ICU:
+        return INVERTED_INDEX_PARSER_ICU;
     default:
         return INVERTED_INDEX_PARSER_UNKNOWN;
     }
@@ -51,6 +53,8 @@ InvertedIndexParserType get_inverted_index_parser_type_from_string(const std::st
         return InvertedIndexParserType::PARSER_ENGLISH;
     } else if (parser_str_lower == INVERTED_INDEX_PARSER_CHINESE) {
         return InvertedIndexParserType::PARSER_CHINESE;
+    } else if (parser_str_lower == INVERTED_INDEX_PARSER_ICU) {
+        return InvertedIndexParserType::PARSER_ICU;
     }
 
     return InvertedIndexParserType::PARSER_UNKNOWN;
