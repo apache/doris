825637a14ebb64c8c36c56517860f49133402f36
feiniaofeiafei <moailing@selectdb.com>
2025-01-21 20:37:21 +0800

    add feut

--- regression-test/suites/nereids_rules_p0/eliminate_order_by_key/eliminate_order_by_key.groovy
+++ regression-test/suites/nereids_rules_p0/eliminate_order_by_key/eliminate_order_by_key.groovy
@@ -15,31 +15,39 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("eliminate_order_by_constant") {
+suite("eliminate_order_by_key") {
     sql "SET ignore_shape_nodes='PhysicalDistribute,PhysicalProject'"
     sql "drop table if exists eliminate_order_by_constant_t"
-    sql """create table eliminate_order_by_constant_t(a int null, b int not null, c varchar(10) null, d date, dt datetime)
+    sql """create table eliminate_order_by_constant_t(a int null, b int not null, c varchar(10) null, d date, dt datetime, id int)
     distributed by hash(a) properties("replication_num"="1");
     """
     sql """
-    INSERT INTO eliminate_order_by_constant_t (a, b, c, d, dt) VALUES
-    (1, 100, 'apple', '2023-01-01', '2023-01-01 10:00:00'),
-    (1, 100, 'apple', '2023-01-01', '2023-01-01 10:00:00'),
-    (2, 101, 'banana', '2023-01-02', '2023-01-02 11:00:00'),
-    (3, 102, 'cherry', '2023-01-03', '2023-01-03 12:00:00'),
-    (3, 102, 'cherry', '2023-01-03', '2023-01-03 12:00:00'), 
-    (NULL, 103, 'date', '2023-01-04', '2023-01-04 13:00:00'),
-    (4, 104, 'elderberry', '2023-01-05', '2023-01-05 14:00:00'),
-    (5, 105, NULL, '2023-01-06', '2023-01-06 15:00:00'),
-    (5, 105, NULL, '2023-01-06', '2023-01-06 15:00:00'),
-    (6, 106, 'fig', '2023-01-07', '2023-01-07 16:00:00'),
-    (NULL, 107, 'grape', '2023-01-08', '2023-01-08 17:00:00');
+    INSERT INTO eliminate_order_by_constant_t (a, b, c, d, dt,id) VALUES
+    (1, 100, 'apple', '2023-01-01', '2023-01-01 10:00:00',1),
+    (1, 100, 'apple', '2023-01-01', '2023-01-01 10:00:00',2),
+    (2, 101, 'banana', '2023-01-02', '2023-01-02 11:00:00',3),
+    (3, 102, 'cherry', '2023-01-03', '2023-01-03 12:00:00',4),
+    (3, 102, 'cherry', '2023-01-03', '2023-01-03 12:00:00',5), 
+    (NULL, 103, 'date', '2023-01-04', '2023-01-04 13:00:00',6),
+    (4, 104, 'elderberry', '2023-01-05', '2023-01-05 14:00:00',7),
+    (5, 105, NULL, '2023-01-06', '2023-01-06 15:00:00',8),
+    (5, 105, NULL, '2023-01-06', '2023-01-06 15:00:00',9),
+    (6, 106, 'fig', '2023-01-07', '2023-01-07 16:00:00',10),
+    (NULL, 107, 'grape', '2023-01-08', '2023-01-08 17:00:00',11);
     """
     qt_predicate "select 1 as c1,a from eliminate_order_by_constant_t where a=1 order by a"
     qt_predicate_order_by_two "select 1 as c1,a from eliminate_order_by_constant_t where a=1 order by a,c1"
     qt_with_group_by """select 1 as c1,a from eliminate_order_by_constant_t where a=1 group by c1,a order by a"""
     qt_predicate_multi_other """select 1 as c1,a,b,c from eliminate_order_by_constant_t where a=1 order by a,'abc',b,c"""
-    qt_predicate_shape "explain shape plan select 1 as c1,a from eliminate_order_by_constant_t where a=1 order by a"
     qt_with_group_by_shape """explain shape plan select 1 as c1,a from eliminate_order_by_constant_t where a=1 group by c1,a order by a"""
-    qt_predicate_multi_other_shape """explain shape plan select 1 as c1,a,b,c from eliminate_order_by_constant_t where a=1 order by a,'abc',b,c"""
+
+    // fd
+    qt_fd "select a,b,c,d,dt from eliminate_order_by_constant_t order by a,abs(a),a+1,id"
+    qt_fd_duplicate "select a,b,c,d,dt from eliminate_order_by_constant_t order by a,abs(a),a,abs(a),a+1,id"
+    qt_fd_topn "select a,b,c,d,dt from eliminate_order_by_constant_t order by a,abs(a),a+1,id limit 5"
+    qt_fd_duplicate_topn "select a,b,c,d,dt from eliminate_order_by_constant_t order by a,abs(a),a,abs(a),a+1,id limit 5"
+
+    // duplicate
+    qt_dup_shape "select a,b,c,d,dt from eliminate_order_by_constant_t order by a,a,id"
+    qt_dup_expr_shape "select a,b,c,d,dt from eliminate_order_by_constant_t order by a+1,a+1,id"
 }
