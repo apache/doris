1a465f9a85bd0f666941be013a693de35e93ab10
zy-kkk <zhongyk10@gmail.com>
2025-01-22 22:43:35 +0800

    [opt](jdbc scan) Add more jdbc scan profile items (#46460)
    ### What problem does this PR solve?
    
    Problem Summary:
    
    ```
    -  GetDataTime:  197.0us (Total time to read data)
        -  CastTime:  2.0us (Convert data from JDBC memory format to Doris BE C++ memory format)
        -  FillBlockTime:  6.0us (Put data into BE Block)
        -  ReadAndFillVectorTableTime:  129.0us (Time to read data and convert to vectorTable)
        -  HasNextTime:  35.0us (The time to obtain each batch of data)
        -  JniSetupTime:  0ns (Start JNI time)
        -  PrepareParamsTime:  24.0us (Preparation parameter time) 
    ```

--- be/src/vec/exec/scan/new_jdbc_scanner.h
+++ be/src/vec/exec/scan/new_jdbc_scanner.h
@@ -59,7 +59,11 @@ protected:
     RuntimeProfile::Counter* _load_jar_timer = nullptr;
     RuntimeProfile::Counter* _init_connector_timer = nullptr;
     RuntimeProfile::Counter* _get_data_timer = nullptr;
-    RuntimeProfile::Counter* _get_block_address_timer = nullptr;
+    RuntimeProfile::Counter* _jni_setup_timer = nullptr;
+    RuntimeProfile::Counter* _has_next_timer = nullptr;
+    RuntimeProfile::Counter* _prepare_params_timer = nullptr;
+    RuntimeProfile::Counter* _cast_timer = nullptr;
+    RuntimeProfile::Counter* _read_and_fill_vector_table_timer = nullptr;
     RuntimeProfile::Counter* _fill_block_timer = nullptr;
     RuntimeProfile::Counter* _check_type_timer = nullptr;
     RuntimeProfile::Counter* _execte_read_timer = nullptr;
