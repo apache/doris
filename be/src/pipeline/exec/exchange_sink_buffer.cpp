c4183905300367bccd0113145e1c5ced73ed45b2
Mryange <yanxuecheng@selectdb.com>
2025-01-23 19:39:53 +0800

    upd

--- be/src/pipeline/exec/exchange_sink_buffer.cpp
+++ be/src/pipeline/exec/exchange_sink_buffer.cpp
@@ -431,12 +431,15 @@ void ExchangeSinkBuffer::_set_receiver_eof(InstanceLoId id) {
 
     std::queue<TransmitInfo, std::list<TransmitInfo>>& q = _instance_to_package_queue[id];
     for (; !q.empty(); q.pop()) {
+        // Must update _total_queue_size here, otherwise if _total_queue_size > _queue_capacity at EOF,
+        // ExchangeSinkQueueDependency will be blocked and pipeline will be deadlocked
         _total_queue_size--;
         if (q.front().block) {
             COUNTER_UPDATE(_parent->memory_used_counter(), -q.front().block->ByteSizeLong());
         }
     }
 
+    // Try to wake up pipeline after clearing the queue
     if (_queue_dependency && _total_queue_size <= _queue_capacity) {
         _queue_dependency->set_ready();
     }
