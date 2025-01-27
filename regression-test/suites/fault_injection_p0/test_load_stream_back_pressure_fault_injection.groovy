f0bec966b9262b22337bd0779084bc706b3c7b65
Kaijie Chen <chenkaijie@selectdb.com>
2025-01-27 10:22:56 +0800

    [test](p0) don't run move-memtable injection cases in cloud mode (#47311)
    The fault injections are for memtable_on_sink_node, which is not
    supported in cloud mode yet.

--- regression-test/suites/fault_injection_p0/test_load_stream_back_pressure_fault_injection.groovy
+++ regression-test/suites/fault_injection_p0/test_load_stream_back_pressure_fault_injection.groovy
@@ -19,84 +19,86 @@ import org.codehaus.groovy.runtime.IOGroovyMethods
 import org.apache.doris.regression.util.Http
 
 suite("test_load_stream_back_pressure_fault_injection", "nonConcurrent") {
-    sql """ set enable_memtable_on_sink_node=true """
-    sql """ DROP TABLE IF EXISTS `baseall` """
-    sql """ DROP TABLE IF EXISTS `test` """
-    sql """
-        CREATE TABLE IF NOT EXISTS `baseall` (
-            `k0` boolean null comment "",
-            `k1` tinyint(4) null comment "",
-            `k2` smallint(6) null comment "",
-            `k3` int(11) null comment "",
-            `k4` bigint(20) null comment "",
-            `k5` decimal(9, 3) null comment "",
-            `k6` char(5) null comment "",
-            `k10` date null comment "",
-            `k11` datetime null comment "",
-            `k7` varchar(20) null comment "",
-            `k8` double max null comment "",
-            `k9` float sum null comment "",
-            `k12` string replace null comment "",
-            `k13` largeint(40) replace null comment ""
-        ) engine=olap
-        DISTRIBUTED BY HASH(`k1`) BUCKETS 5 properties("replication_num" = "1")
-        """
-    sql """
-        CREATE TABLE IF NOT EXISTS `test` (
-            `k0` boolean null comment "",
-            `k1` tinyint(4) null comment "",
-            `k2` smallint(6) null comment "",
-            `k3` int(11) null comment "",
-            `k4` bigint(20) null comment "",
-            `k5` decimal(9, 3) null comment "",
-            `k6` char(5) null comment "",
-            `k10` date null comment "",
-            `k11` datetime null comment "",
-            `k7` varchar(20) null comment "",
-            `k8` double max null comment "",
-            `k9` float sum null comment "",
-            `k12` string replace_if_not_null null comment "",
-            `k13` largeint(40) replace null comment ""
-        ) engine=olap
-        DISTRIBUTED BY HASH(`k1`) BUCKETS 5 properties("replication_num" = "1")
-        """
+    if (!isCloudMode()) {
+        sql """ set enable_memtable_on_sink_node=true """
+        sql """ DROP TABLE IF EXISTS `baseall` """
+        sql """ DROP TABLE IF EXISTS `test` """
+        sql """
+            CREATE TABLE IF NOT EXISTS `baseall` (
+                `k0` boolean null comment "",
+                `k1` tinyint(4) null comment "",
+                `k2` smallint(6) null comment "",
+                `k3` int(11) null comment "",
+                `k4` bigint(20) null comment "",
+                `k5` decimal(9, 3) null comment "",
+                `k6` char(5) null comment "",
+                `k10` date null comment "",
+                `k11` datetime null comment "",
+                `k7` varchar(20) null comment "",
+                `k8` double max null comment "",
+                `k9` float sum null comment "",
+                `k12` string replace null comment "",
+                `k13` largeint(40) replace null comment ""
+            ) engine=olap
+            DISTRIBUTED BY HASH(`k1`) BUCKETS 5 properties("replication_num" = "1")
+            """
+        sql """
+            CREATE TABLE IF NOT EXISTS `test` (
+                `k0` boolean null comment "",
+                `k1` tinyint(4) null comment "",
+                `k2` smallint(6) null comment "",
+                `k3` int(11) null comment "",
+                `k4` bigint(20) null comment "",
+                `k5` decimal(9, 3) null comment "",
+                `k6` char(5) null comment "",
+                `k10` date null comment "",
+                `k11` datetime null comment "",
+                `k7` varchar(20) null comment "",
+                `k8` double max null comment "",
+                `k9` float sum null comment "",
+                `k12` string replace_if_not_null null comment "",
+                `k13` largeint(40) replace null comment ""
+            ) engine=olap
+            DISTRIBUTED BY HASH(`k1`) BUCKETS 5 properties("replication_num" = "1")
+            """
 
-    GetDebugPoint().clearDebugPointsForAllBEs()
-    streamLoad {
-        table "baseall"
-        db "regression_test_fault_injection_p0"
-        set 'column_separator', ','
-        file "baseall.txt"
-    }
+        GetDebugPoint().clearDebugPointsForAllBEs()
+        streamLoad {
+            table "baseall"
+            db "regression_test_fault_injection_p0"
+            set 'column_separator', ','
+            file "baseall.txt"
+        }
 
-    try {
-        GetDebugPoint().enableDebugPointForAllBEs("TabletStream.append_data.long_wait")
-        // the kill thread only means to end the test faster when the code does not behave as expected
-        def kill_thread = new Thread({
-            sleep(5000)
-            def processList = sql "show processlist"
-            logger.info(processList.toString())
-            processList.each { item ->
-                logger.info(item[1].toString())
-                logger.info(item[11].toString())
-                if (item[11].toString() == "insert into test select * from baseall where k1 <= 3".toString()){
-                    def res = sql "kill ${item[1]}"
-                    logger.info(res.toString())
+        try {
+            GetDebugPoint().enableDebugPointForAllBEs("TabletStream.append_data.long_wait")
+            // the kill thread only means to end the test faster when the code does not behave as expected
+            def kill_thread = new Thread({
+                sleep(5000)
+                def processList = sql "show processlist"
+                logger.info(processList.toString())
+                processList.each { item ->
+                    logger.info(item[1].toString())
+                    logger.info(item[11].toString())
+                    if (item[11].toString() == "insert into test select * from baseall where k1 <= 3".toString()){
+                        def res = sql "kill ${item[1]}"
+                        logger.info(res.toString())
+                    }
                 }
-            }
-        })
-        kill_thread.start()
-        def res = sql "insert into test select * from baseall where k1 <= 3"
-        logger.info(res.toString())
-        assertTrue(false, "Expected exception to be thrown")
-    } catch(Exception e) {
-        logger.info(e.getMessage())
-        assertTrue(e.getMessage().contains("wait flush token back pressure time is more than load_stream_max_wait_flush_token_time"))
-    } finally {
-        GetDebugPoint().disableDebugPointForAllBEs("TabletStream.append_data.long_wait")
-    }
+            })
+            kill_thread.start()
+            def res = sql "insert into test select * from baseall where k1 <= 3"
+            logger.info(res.toString())
+            assertTrue(false, "Expected exception to be thrown")
+        } catch(Exception e) {
+            logger.info(e.getMessage())
+            assertTrue(e.getMessage().contains("wait flush token back pressure time is more than load_stream_max_wait_flush_token_time"))
+        } finally {
+            GetDebugPoint().disableDebugPointForAllBEs("TabletStream.append_data.long_wait")
+        }
 
-    sql """ DROP TABLE IF EXISTS `baseall` """
-    sql """ DROP TABLE IF EXISTS `test` """
-    sql """ set enable_memtable_on_sink_node=false """
+        sql """ DROP TABLE IF EXISTS `baseall` """
+        sql """ DROP TABLE IF EXISTS `test` """
+        sql """ set enable_memtable_on_sink_node=false """
+    }
 }
