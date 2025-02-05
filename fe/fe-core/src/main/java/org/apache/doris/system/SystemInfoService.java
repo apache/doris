779eec94aa317380c5fc26fa4a52dda9c0992682
Kaijie Chen <chenkaijie@selectdb.com>
2025-02-05 16:49:05 +0800

    fixup

--- fe/fe-core/src/main/java/org/apache/doris/system/SystemInfoService.java
+++ fe/fe-core/src/main/java/org/apache/doris/system/SystemInfoService.java
@@ -69,10 +69,10 @@ public class SystemInfoService {
     public static final String DEFAULT_CLUSTER = "default_cluster";
 
     public static final String NO_BACKEND_LOAD_AVAILABLE_MSG =
-            "No backend available for load. Please check the status of your backends.";
+            "No backend available for load, please check the status of your backends.";
 
     public static final String NO_SCAN_NODE_BACKEND_AVAILABLE_MSG =
-            "No backend available as scanNode. Please check the status of your backends.";
+            "No backend available as scan node, please check the status of your backends.";
 
     public static final String NOT_USING_VALID_CLUSTER_MSG =
             "Not using valid cloud clusters, please use a cluster before issuing any queries";
