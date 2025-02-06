3a680b245b4192a3a7af623b0857d2d0e3c98a1e
csun5285 <sunchenyang@selectdb.com>
2025-02-06 12:27:52 +0800

    update test case

---
+++ regression-test/suites/variant_p0/update/query.groovy
@@ -0,0 +1,66 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("update_test_query", "p0") {
+
+    def load_json_data = {table_name, file_name ->
+        // load the json data
+        streamLoad {
+            table "${table_name}"
+
+            // set http request header params
+            set 'read_json_by_line', 'true' 
+            set 'format', 'json' 
+            set 'max_filter_ratio', '0.1'
+            set 'memtable_on_sink_node', 'true'
+            file file_name // import json file
+            time 10000 // limit inflight 10s
+
+            // if declared a check callback, the default check condition will ignore.
+            // So you must check all condition
+
+            check { result, exception, startTime, endTime ->
+                if (exception != null) {
+                        throw exception
+                }
+                logger.info("Stream load ${file_name} result: ${result}".toString())
+                def json = parseJson(result)
+                assertEquals("success", json.Status.toLowerCase())
+                // assertEquals(json.NumberTotalRows, json.NumberLoadedRows + json.NumberUnselectedRows)
+                assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
+            }
+        }
+    }
+
+    def table_name = "test_update"
+
+    for (int i = 0; i < 10; i++) {
+        load_json_data.call(table_name, """${getS3Url() + '/regression/load/ghdata_sample.json'}""")
+    }
+
+    qt_sql """ select count() from ${table_name} """
+    qt_sql """ select v['actor'] from ${table_name} order by k limit 1"""
+    qt_sql """ select count(v['id']) from ${table_name}"""
+    qt_sql """ select max(cast (v['public'] as tinyint)) from ${table_name}"""
+
+    trigger_and_wait_compaction(table_name, "full")
+
+    qt_sql """ select count() from ${table_name} """
+    qt_sql """ select v['actor'] from ${table_name} order by k limit 1"""
+    qt_sql """ select count(v['id']) from ${table_name}"""
+    qt_sql """ select max(cast (v['public'] as tinyint)) from ${table_name}"""
+}
