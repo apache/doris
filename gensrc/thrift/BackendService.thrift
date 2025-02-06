675d89cc2ca8a78c9410247ee479770300dc87ac
zhaochangle <zhaochangle@selectdb.com>
2025-02-06 21:03:06 +0800

    add new interface of get specific dict status

--- gensrc/thrift/BackendService.thrift
+++ gensrc/thrift/BackendService.thrift
@@ -420,5 +420,6 @@ service BackendService {
 
     TGetRealtimeExecStatusResponse get_realtime_exec_status(1:TGetRealtimeExecStatusRequest request);
 
+    TDictionaryStatus get_dictionary_status(1:i64 dictionary_id);
     TAllDictionaryStatus get_all_dictionary_status();
 }
