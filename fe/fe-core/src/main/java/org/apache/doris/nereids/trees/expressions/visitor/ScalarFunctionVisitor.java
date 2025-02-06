16381b12962d11cf95f5e6f589956510774307ba
Pxl <xl@selectdb.com>
2025-02-06 15:39:59 +0800

    [Feature](function) support function array_flatten (#47404)
    ### What problem does this PR solve?
    
    ### Check List (For Author)
    
    - Test <!-- At least one of them must be included. -->
        - [X] Regression test
        - [ ] Unit Test
        - [ ] Manual test (add detailed scripts or steps below)
        - [ ] No need to test or manual test. Explain why:
    - [ ] This is a refactor/code format and no logic has been changed.
            - [ ] Previous test can cover this change.
            - [ ] No code files have been changed.
            - [ ] Other reason <!-- Add your reason?  -->
    
    - Behavior changed:
        - [X] No.
        - [ ] Yes. <!-- Explain the behavior change -->
    
    - Does this need documentation?
        - [ ] No.
        - [X] Yes. <https://github.com/apache/doris-website/pull/1951>
    
    ### Check List (For Reviewer who merge this PR)
    
    - [X] Confirm the release note
    - [X] Confirm test cases
    - [X] Confirm document
    - [X] Add branch pick label <!-- Add branch pick label that this PR
    should merge into -->

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/ScalarFunctionVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/ScalarFunctionVisitor.java
@@ -43,6 +43,7 @@ import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayExcept;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayExists;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayFilter;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayFirstIndex;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayFlatten;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayIntersect;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayJoin;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.ArrayLastIndex;
@@ -677,6 +678,10 @@ public interface ScalarFunctionVisitor<R, C> {
 
     default R visitArrayShuffle(ArrayShuffle arrayShuffle, C context) {
         return visitScalarFunction(arrayShuffle, context);
+    }
+
+    default R visitArrayFlatten(ArrayFlatten arrayFlatten, C context) {
+        return visitScalarFunction(arrayFlatten, context);
     }
 
     default R visitArrayMap(ArrayMap arraySort, C context) {
