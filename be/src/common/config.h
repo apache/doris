eb91b0e9caa04d06905f97815033d941a241dc7a
huanghaibin <huanghaibin@selectdb.com>
2025-02-06 10:25:54 +0800

    [improve](cloud-mow)Add delete bitmap metrics (#47028)
    Count the max delete bitmap of tablet and base rowset, adn put them to
    be metrics.

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1507,6 +1507,10 @@ DECLARE_mBool(enable_sleep_between_delete_cumu_compaction);
 
 DECLARE_mInt32(compaction_num_per_round);
 
+DECLARE_mInt32(check_tablet_delete_bitmap_interval_seconds);
+DECLARE_mInt32(check_tablet_delete_bitmap_score_top_n);
+DECLARE_mBool(enable_check_tablet_delete_bitmap_score);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
