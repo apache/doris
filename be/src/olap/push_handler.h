1879b62c4f237db6207a58658de681ab026582bf
gnehil <liheng@selectdb.com>
2025-02-08 11:44:40 +0800

    format code

--- be/src/olap/push_handler.h
+++ be/src/olap/push_handler.h
@@ -107,9 +107,9 @@ protected:
     Status _convert_to_output_block(vectorized::Block* block);
     Status _init_expr_ctxes();
     Status _convert_bitmap(vectorized::Block* block, vectorized::ColumnWithTypeAndName& arg,
-                                       uint32_t idx, vectorized::DataTypePtr return_type);
+                           uint32_t idx, vectorized::DataTypePtr return_type);
     Status _convert_hll(vectorized::Block* block, vectorized::ColumnWithTypeAndName& arg,
-                                       uint32_t idx, vectorized::DataTypePtr return_type);
+                        uint32_t idx, vectorized::DataTypePtr return_type);
 
 private:
     bool _ready;
