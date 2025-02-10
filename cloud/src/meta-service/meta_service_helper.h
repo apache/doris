66d7771b669055fe63a277810a9d12b6a25c3114
Yukang-Lian <lianyukang@selectdb.com>
2025-02-10 21:10:03 +0800

    2

--- cloud/src/meta-service/meta_service_helper.h
+++ cloud/src/meta-service/meta_service_helper.h
@@ -157,29 +157,7 @@ void finish_rpc(std::string_view func_name, brpc::Controller* ctrl, Response* re
                   << " status=" << res->status().ShortDebugString();
     } else if constexpr (std::is_same_v<Response, GetObjStoreInfoResponse> ||
                          std::is_same_v<Response, GetStageResponse>) {
-        std::string debug_string = res->DebugString();
-        // Start position for searching "sk" fields
-        size_t pos = 0;
-        // Iterate through the string and find all occurrences of "sk: "
-        while ((pos = debug_string.find("sk: ", pos)) != std::string::npos) {
-            // Find the start and end of the "sk" value (assumed to be within quotes)
-            // Start after the quote
-            size_t sk_value_start = debug_string.find('\"', pos) + 1;
-            // End at the next quote
-            size_t sk_value_end = debug_string.find('\"', sk_value_start);
-
-            // Extract the "sk" value
-            std::string sk_value =
-                    debug_string.substr(sk_value_start, sk_value_end - sk_value_start);
-            // Encrypt the "sk" value with MD5
-            std::string encrypted_sk = "md5: " + md5(sk_value);
-
-            // Replace the original "sk" value with the encrypted MD5 value
-            debug_string.replace(sk_value_start, sk_value_end - sk_value_start, encrypted_sk);
-
-            // Move the position to the end of the current "sk" field and continue searching
-            pos = sk_value_end;
-        }
+        std::string debug_string = encryt_sk(res->DebugString());
         TEST_SYNC_POINT_CALLBACK("sk_finish_rpc", &debug_string);
         LOG(INFO) << "finish " << func_name << " from " << ctrl->remote_side()
                   << " response=" << debug_string;
