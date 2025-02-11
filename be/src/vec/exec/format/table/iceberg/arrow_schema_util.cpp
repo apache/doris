5b3e59a65317673574b40a6362f06c558b00dd36
Socrates <suyiteng@selectdb.com>
2025-02-11 12:05:27 +0800

    [env](compile) add compiler check and fix some clang-tidy suggestions for vec/exec/format (#45881)
    ### What problem does this PR solve?
    1. Enable compiler check in vec/exec/format files
    2. Fix clang-tidy by `clang-tidy --fix--errors`

--- be/src/vec/exec/format/table/iceberg/arrow_schema_util.cpp
+++ be/src/vec/exec/format/table/iceberg/arrow_schema_util.cpp
@@ -20,8 +20,8 @@
 #include <arrow/type.h>
 #include <arrow/util/key_value_metadata.h>
 
-namespace doris {
-namespace iceberg {
+namespace doris::iceberg {
+#include "common/compile_check_begin.h"
 
 const char* ArrowSchemaUtil::PARQUET_FIELD_ID = "PARQUET:field_id";
 const char* ArrowSchemaUtil::ORIGINAL_TYPE = "originalType";
@@ -82,7 +82,7 @@ Status ArrowSchemaUtil::convert_to(const iceberg::NestedField& field,
         break;
 
     case iceberg::TypeID::DECIMAL: {
-        auto dt = dynamic_cast<DecimalType*>(field.field_type());
+        auto* dt = dynamic_cast<DecimalType*>(field.field_type());
         arrow_type = arrow::decimal(dt->get_precision(), dt->get_scale());
         break;
     }
@@ -130,5 +130,5 @@ Status ArrowSchemaUtil::convert_to(const iceberg::NestedField& field,
     return Status::OK();
 }
 
-} // namespace iceberg
-} // namespace doris
+#include "common/compile_check_end.h"
+} // namespace doris::iceberg
