ee9c491b8201fcbb7465d1ca26691c01f446070b
meiyi <meiyi@selectdb.com>
2025-02-13 14:46:08 +0800

    [fix](case) fix insert_group_commit_into

--- regression-test/suites/insert_p0/insert_group_commit_into.groovy
+++ regression-test/suites/insert_p0/insert_group_commit_into.groovy
@@ -25,7 +25,7 @@ suite("insert_group_commit_into") {
     def table = dbName + "." + tableName
 
     def getRowCount = { expectedRowCount ->
-        Awaitility.await().atMost(30, SECONDS).pollInterval(1, SECONDS).until(
+        Awaitility.await().atMost(90, SECONDS).pollInterval(1, SECONDS).until(
             {
                 def result = sql "select count(*) from ${table}"
                 logger.info("table: ${table}, rowCount: ${result}")
