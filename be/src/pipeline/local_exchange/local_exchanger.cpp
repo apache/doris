7567eb5f2df5ae5083668b328dae9e978846b937
Gabriel <liwenqiang@selectdb.com>
2025-02-13 12:51:28 +0800

    test

--- be/src/pipeline/local_exchange/local_exchanger.cpp
+++ be/src/pipeline/local_exchange/local_exchanger.cpp
@@ -65,6 +65,12 @@ bool Exchanger<BlockType>::_dequeue_data(LocalExchangeSourceLocalState* local_st
             local_state->_shared_state->sub_mem_usage(channel_id, block->_allocated_bytes);
             data_block->swap(block->_data_block);
         }
+        if (_data_queue[channel_id].data_queue.size_approx() == 0) {
+            std::unique_lock l(*_m[channel_id]);
+            if (_data_queue[channel_id].data_queue.size_approx() == 0) {
+                local_state->get_dependency(channel_id)->block();
+            }
+        }
         return true;
     } else if (all_finished) {
         *eos = true;
@@ -377,6 +383,7 @@ Status LocalMergeSortExchanger::sink(RuntimeState* state, vectorized::Block* in_
                         *sink_info.channel_id));
     }
     if (eos && sink_info.local_state) {
+        _eos[*sink_info.channel_id] = true;
         sink_info.local_state->_shared_state->source_deps[*sink_info.channel_id]
                 ->set_always_ready();
     }
@@ -422,7 +429,7 @@ Status LocalMergeSortExchanger::build_merger(RuntimeState* state,
                                                            vectorized::Block* block, bool* eos) {
             BlockWrapperSPtr next_block;
             _dequeue_data(local_state, next_block, eos, block, id);
-            if (!*eos && block->empty()) {
+            if (!*eos && block->empty() && !_eos[id]) {
                 return Status::InternalError(
                         "LocalMergeSortExchanger{} meet error! Block should not be empty when eos "
                         "is false",
