03f1ac1ba4563f9f7900847b85de93f0c205b653
Gabriel <liwenqiang@selectdb.com>
2025-02-17 18:17:14 +0800

    [fix](auto partition) Fix duplicate rows in auto-partitioned table (#47911)
    The same block may be sent twice by TabletSinkHashPartitioner. To get
    the correct result, we should not send any rows the last time.

--- be/src/vec/sink/tablet_sink_hash_partitioner.cpp
+++ be/src/vec/sink/tablet_sink_hash_partitioner.cpp
@@ -82,7 +82,8 @@ Status TabletSinkHashPartitioner::open(RuntimeState* state) {
     return Status::OK();
 }
 
-Status TabletSinkHashPartitioner::do_partitioning(RuntimeState* state, Block* block) const {
+Status TabletSinkHashPartitioner::do_partitioning(RuntimeState* state, Block* block, bool eos,
+                                                  bool* already_sent) const {
     _hash_vals.resize(block->rows());
     if (block->empty()) {
         return Status::OK();
@@ -97,7 +98,8 @@ Status TabletSinkHashPartitioner::do_partitioning(RuntimeState* state, Block* bl
             number_input_rows));
     if (_row_distribution.batching_rows() > 0) {
         SCOPED_TIMER(_local_state->send_new_partition_timer());
-        RETURN_IF_ERROR(_send_new_partition_batch(state, block));
+        RETURN_IF_ERROR(_send_new_partition_batch(state, block, eos));
+        *already_sent = true;
     } else {
         const auto& row_ids = _row_part_tablet_ids[0].row_ids;
         const auto& tablet_ids = _row_part_tablet_ids[0].tablet_ids;
@@ -138,14 +140,15 @@ Status TabletSinkHashPartitioner::close(RuntimeState* state) {
 }
 
 Status TabletSinkHashPartitioner::_send_new_partition_batch(RuntimeState* state,
-                                                            vectorized::Block* input_block) const {
+                                                            vectorized::Block* input_block,
+                                                            bool eos) const {
     RETURN_IF_ERROR(_row_distribution.automatic_create_partition());
     auto& p = _local_state->parent()->cast<pipeline::ExchangeSinkOperatorX>();
     // Recovery back
     _row_distribution.clear_batching_stats();
     _row_distribution._batching_block->clear_column_data();
     _row_distribution._deal_batched = false;
-    RETURN_IF_ERROR(p.sink(state, input_block, false));
+    RETURN_IF_ERROR(p.sink(state, input_block, eos));
     return Status::OK();
 }
 
