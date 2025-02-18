dcea5c7289cd922170babb3c6d1abc8e0e347039
Mryange <yanxuecheng@selectdb.com>
2025-02-18 11:58:04 +0800

    refine code

--- be/src/vec/functions/if.h
+++ be/src/vec/functions/if.h
@@ -40,70 +40,44 @@ private:
     using ColVecResult = ColumnVector<Type>;
     using ColVecT = ColumnVector<Type>;
 
-    static ColumnPtr vector_vector(const ArrayCond& cond, const Array& a, const Array& b) {
+    // res[i] = cond[i] ? a[i] : b[i];
+    template <bool is_const_a, bool is_const_b>
+    static ColumnPtr native_execute(const ArrayCond& cond, const Array& a, const Array& b) {
         size_t size = cond.size();
         auto col_res = ColVecResult::create(size);
         typename ColVecResult::Container& res = col_res->get_data();
-
         for (size_t i = 0; i < size; ++i) {
-            res[i] = cond[i] ? a[i] : b[i];
-        }
-        return col_res;
-    }
-
-    static ColumnPtr vector_constant(const ArrayCond& cond, const Array& a, Type b) {
-        size_t size = cond.size();
-        auto col_res = ColVecResult::create(size);
-        typename ColVecResult::Container& res = col_res->get_data();
-
-        for (size_t i = 0; i < size; ++i) {
-            res[i] = cond[i] ? a[i] : b;
-        }
-        return col_res;
-    }
-
-    static ColumnPtr constant_vector(const ArrayCond& cond, Type a, const Array& b) {
-        size_t size = cond.size();
-        auto col_res = ColVecResult::create(size);
-        typename ColVecResult::Container& res = col_res->get_data();
-
-        for (size_t i = 0; i < size; ++i) {
-            res[i] = cond[i] ? a : b[i];
-        }
-        return col_res;
-    }
-
-    static ColumnPtr constant_constant(const ArrayCond& cond, Type a, Type b) {
-        size_t size = cond.size();
-        auto col_res = ColVecResult::create(size);
-        typename ColVecResult::Container& res = col_res->get_data();
-
-        for (size_t i = 0; i < size; ++i) {
-            res[i] = cond[i] ? a : b;
+            res[i] = cond[i] ? a[index_check_const<is_const_a>(i)]
+                             : b[index_check_const<is_const_b>(i)];
         }
         return col_res;
     }
 
 public:
+    static const Array& get_data_from_column_const(const ColumnConst* column) {
+        return assert_cast<const ColVecT&>(column->get_data_column()).get_data();
+    }
+
     static ColumnPtr execute_if(const ArrayCond& cond, const ColumnPtr& then_col,
                                 const ColumnPtr& else_col) {
         if (const auto* col_then = check_and_get_column<ColVecT>(then_col.get())) {
             if (const auto* col_else = check_and_get_column<ColVecT>(else_col.get())) {
-                return vector_vector(cond, col_then->get_data(), col_else->get_data());
+                return native_execute<false, false>(cond, col_then->get_data(),
+                                                    col_else->get_data());
             } else if (const auto* col_const_else =
                                check_and_get_column_const<ColVecT>(else_col.get())) {
-                return vector_constant(cond, col_then->get_data(),
-                                       col_const_else->template get_value<Type>());
+                return native_execute<false, true>(cond, col_then->get_data(),
+                                                   get_data_from_column_const(col_const_else));
             }
         } else if (const auto* col_const_then =
                            check_and_get_column_const<ColVecT>(then_col.get())) {
             if (const auto* col_else = check_and_get_column<ColVecT>(else_col.get())) {
-                return constant_vector(cond, col_const_then->template get_value<Type>(),
-                                       col_else->get_data());
+                return native_execute<true, false>(cond, get_data_from_column_const(col_const_then),
+                                                   col_else->get_data());
             } else if (const auto* col_const_else =
                                check_and_get_column_const<ColVecT>(else_col.get())) {
-                return constant_constant(cond, col_const_then->template get_value<Type>(),
-                                         col_const_else->template get_value<Type>());
+                return native_execute<true, true>(cond, get_data_from_column_const(col_const_then),
+                                                  get_data_from_column_const(col_const_else));
             }
         }
         return nullptr;
--- be/test/vec/function/function_num_if_test.cpp
+++ be/test/vec/function/function_num_if_test.cpp
@@ -37,24 +37,71 @@ TEST(NumIfImplTest, smallTest) {
 }
 
 TEST(NumIfImplTest, largeTest) {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
     std::vector<uint8_t> cond_data(1024, 0);
     std::vector<int32_t> a_data(1024, 0);
     std::vector<int32_t> b_data(1024, 0);
     for (size_t i = 0; i < 1024; ++i) {
-        cond_data[i] = i % 2;
-        a_data[i] = i;
-        b_data[i] = i + 1;
+        cond_data[i] = std::rand() % 2;
+        a_data[i] = std::rand();
+        b_data[i] = std::rand();
     }
-
     auto cond = ColumnHelper::create_column<DataTypeUInt8>(cond_data);
     auto a = ColumnHelper::create_column<DataTypeInt32>(a_data);
     auto b = ColumnHelper::create_column<DataTypeInt32>(b_data);
     auto res = NumIfImpl<DataTypeInt32::FieldType>::execute_if(get_cond_data(cond), a, b);
     std::vector<int32_t> expected_data(1024, 0);
     for (size_t i = 0; i < 1024; ++i) {
-        expected_data[i] = i % 2 == 0 ? i : i + 1;
+        expected_data[i] = cond_data[i] ? a_data[i] : b_data[i];
     }
     ColumnHelper::column_equal(res, ColumnHelper::create_column<DataTypeInt32>(expected_data));
 }
 
+template <typename DataType, bool is_a_const, bool is_b_const>
+void test_for_all_const_no_const() {
+    using FieldType = DataType::FieldType;
+    const int size = 1052;
+    std::vector<uint8_t> cond_data(size, 0);
+    std::vector<FieldType> a_data(is_a_const ? 1 : size, 0);
+    std::vector<FieldType> b_data(is_b_const ? 1 : size, 0);
+    for (size_t i = 0; i < size; ++i) {
+        cond_data[i] = std::rand() % 2;
+        a_data[is_a_const ? 0 : i] = std::rand();
+        b_data[is_b_const ? 0 : i] = std::rand();
+    }
+    auto cond = ColumnHelper::create_column<DataTypeUInt8>(cond_data);
+    auto a = ColumnHelper::create_column<DataType>(a_data);
+    auto b = ColumnHelper::create_column<DataType>(b_data);
+
+    if (is_a_const) {
+        a = ColumnConst::create(a, 1);
+    }
+    if (is_b_const) {
+        b = ColumnConst::create(b, 1);
+    }
+    auto res = NumIfImpl<FieldType>::execute_if(get_cond_data(cond), a, b);
+    std::vector<FieldType> expected_data(size, 0);
+    for (size_t i = 0; i < size; ++i) {
+        expected_data[i] = cond_data[i] ? a_data[is_a_const ? 0 : i] : b_data[is_b_const ? 0 : i];
+    }
+    ColumnHelper::column_equal(res, ColumnHelper::create_column<DataType>(expected_data));
+}
+
+template <typename DataType>
+void test_for_all_types() {
+    test_for_all_const_no_const<DataType, true, true>();
+    test_for_all_const_no_const<DataType, true, false>();
+    test_for_all_const_no_const<DataType, false, true>();
+    test_for_all_const_no_const<DataType, false, false>();
+}
+
+TEST(NumIfImplTest, allTest) {
+    test_for_all_types<DataTypeInt8>();
+    test_for_all_types<DataTypeInt16>();
+    test_for_all_types<DataTypeInt32>();
+    test_for_all_types<DataTypeInt64>();
+    test_for_all_types<DataTypeInt128>();
+    test_for_all_types<DataTypeFloat32>();
+    test_for_all_types<DataTypeFloat64>();
+}
 } // namespace doris::vectorized
