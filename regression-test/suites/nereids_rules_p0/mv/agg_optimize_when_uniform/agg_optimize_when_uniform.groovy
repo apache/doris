c4a21f170cfda056d74667c6d1625610fa762c40
seawinde <wusi@selectdb.com>
2025-02-18 09:36:01 +0800

    [test](mv) Fix regression test case  to make stable (#47880)

--- regression-test/suites/nereids_rules_p0/mv/agg_optimize_when_uniform/agg_optimize_when_uniform.groovy
+++ regression-test/suites/nereids_rules_p0/mv/agg_optimize_when_uniform/agg_optimize_when_uniform.groovy
@@ -369,6 +369,10 @@ suite("agg_optimize_when_uniform") {
             """
     order_qt_query6_0_before "${query6_0}"
     async_mv_rewrite_success(db, mv6_0, query6_0, "mv6_0")
+
+    def plan_6 = """explain verbose ${query6_0}"""
+    logger.info("plan_6 is " + plan_6)
+
     qt_shape6_0_after """explain shape plan ${query6_0}"""
     order_qt_query6_0_after "${query6_0}"
     sql """ DROP MATERIALIZED VIEW IF EXISTS mv6_0"""
@@ -442,7 +446,6 @@ suite("agg_optimize_when_uniform") {
     order_qt_query7_1_before "${query7_1}"
     // query where has a column not in agg output
     async_mv_rewrite_fail(db, mv7_1, query7_1, "mv7_1")
-    qt_shape7_1_after """explain shape plan ${query7_1}"""
     order_qt_query7_1_after "${query7_1}"
     sql """ DROP MATERIALIZED VIEW IF EXISTS mv7_1"""
 
@@ -478,7 +481,11 @@ suite("agg_optimize_when_uniform") {
     order_qt_query8_0_before "${query8_0}"
     // query success but add agg
     async_mv_rewrite_success(db, mv8_0, query8_0, "mv8_0")
+
+    def plan_8 = """explain verbose ${query6_0}"""
+    logger.info("plan_8 is " + plan_8)
+
     qt_shape8_0_after """explain shape plan ${query8_0}"""
     order_qt_query8_0_after "${query8_0}"
-    sql """ DROP MATERIALIZED VIEW IF EXISTS mv8_0"""
+    sql """ DROP MATERIALIZED VIEW IF EXISTS mv8_0;"""
 }
