5286cafc3c2081464b287ae634e7056b7b148584
yujun <yujun@selectdb.com>
2025-02-19 16:13:38 +0800

    fix push down nonfoldable filter through project

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/Validator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/Validator.java
@@ -59,13 +59,6 @@ public class Validator extends PlanPostProcessor {
         Preconditions.checkArgument(!filter.getConjuncts().isEmpty()
                 && filter.getPredicate() != BooleanLiteral.TRUE, "Filter predicate can't be empty or true");
 
-        Plan child = filter.child();
-        // Forbidden filter-project, we must make filter-project -> project-filter.
-        if (child instanceof PhysicalProject) {
-            throw new AnalysisException(
-                    "Nereids generate a filter-project plan, but backend not support:\n" + filter.treeString());
-        }
-
         return visit(filter, context);
     }
 
