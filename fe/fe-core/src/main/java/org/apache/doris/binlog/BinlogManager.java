360933b885d69d33723d56f44b73e846919a7a96
walter <maochuan@selectdb.com>
2025-02-20 17:08:26 +0800

    branch-2.1: [feat](binlog) Speed binlog gc by locked binlogs #47547 (#48128)
    cherry pick from #47547

--- fe/fe-core/src/main/java/org/apache/doris/binlog/BinlogManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/binlog/BinlogManager.java
@@ -112,6 +112,10 @@ public class BinlogManager {
             return;
         }
 
+        LOG.debug("add binlog, db {}, table {}, commitSeq {}, timestamp {}, type {}, data {}",
+                binlog.getDbId(), binlog.getTableIds(), binlog.getCommitSeq(), binlog.getTimestamp(), binlog.getType(),
+                binlog.getData());
+
         DBBinlog dbBinlog;
         lock.writeLock().lock();
         try {
@@ -588,7 +592,6 @@ public class BinlogManager {
 
         return tombstones;
     }
-
 
     public void replayGc(BinlogGcInfo binlogGcInfo) {
         lock.writeLock().lock();
