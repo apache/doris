def914c9aa22b7e299252093d022bb5629a3ac94
Mryange <yanxuecheng@selectdb.com>
2025-02-24 19:59:47 +0800

    ut

--- be/src/vec/runtime/vdata_stream_recvr.cpp
+++ be/src/vec/runtime/vdata_stream_recvr.cpp
@@ -80,7 +80,7 @@ Status VDataStreamRecvr::SenderQueue::get_batch(Block* block, bool* eos) {
 #endif
     BlockItem block_item;
     {
-        std::lock_guard<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
         //check and get block_item from data_queue
         if (_is_cancelled) {
             RETURN_IF_ERROR(_cancel_status);
@@ -104,7 +104,7 @@ Status VDataStreamRecvr::SenderQueue::get_batch(Block* block, bool* eos) {
     COUNTER_UPDATE(_recvr->_decompress_timer, block->get_decompress_time());
     COUNTER_UPDATE(_recvr->_decompress_bytes, block->get_decompressed_bytes());
     _recvr->_memory_used_counter->update(-(int64_t)block_byte_size);
-    std::lock_guard<std::mutex> l(_lock);
+    INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
     sub_blocks_memory_usage(block_byte_size);
     _record_debug_info();
     if (_block_queue.empty() && _source_dependency) {
@@ -149,7 +149,7 @@ Status VDataStreamRecvr::SenderQueue::add_block(std::unique_ptr<PBlock> pblock, 
                                                 const int64_t wait_for_worker,
                                                 const uint64_t time_to_find_recvr) {
     {
-        std::lock_guard<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
         if (_is_cancelled) {
             return Status::OK();
         }
@@ -173,7 +173,7 @@ Status VDataStreamRecvr::SenderQueue::add_block(std::unique_ptr<PBlock> pblock, 
         }
     }
 
-    std::lock_guard<std::mutex> l(_lock);
+    INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
     if (_is_cancelled) {
         return Status::OK();
     }
@@ -211,7 +211,7 @@ void VDataStreamRecvr::SenderQueue::add_block(Block* block, bool use_move) {
         return;
     }
     {
-        std::unique_lock<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::unique_lock<std::mutex> l(_lock));
         if (_is_cancelled) {
             return;
         }
@@ -236,7 +236,7 @@ void VDataStreamRecvr::SenderQueue::add_block(Block* block, bool use_move) {
 
     auto block_mem_size = nblock->allocated_bytes();
     {
-        std::lock_guard<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
         if (_is_cancelled) {
             return;
         }
@@ -250,7 +250,7 @@ void VDataStreamRecvr::SenderQueue::add_block(Block* block, bool use_move) {
 }
 
 void VDataStreamRecvr::SenderQueue::decrement_senders(int be_number) {
-    std::lock_guard<std::mutex> l(_lock);
+    INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
     if (_sender_eos_set.end() != _sender_eos_set.find(be_number)) {
         return;
     }
@@ -268,7 +268,7 @@ void VDataStreamRecvr::SenderQueue::decrement_senders(int be_number) {
 
 void VDataStreamRecvr::SenderQueue::cancel(Status cancel_status) {
     {
-        std::lock_guard<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
         if (_is_cancelled) {
             return;
         }
@@ -280,7 +280,7 @@ void VDataStreamRecvr::SenderQueue::cancel(Status cancel_status) {
                    << " node_id=" << _recvr->dest_node_id();
     }
     {
-        std::lock_guard<std::mutex> l(_lock);
+        INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
         for (auto closure_pair : _pending_closures) {
             closure_pair.first->Run();
             int64_t elapse_time = closure_pair.second.elapsed_time();
@@ -296,7 +296,7 @@ void VDataStreamRecvr::SenderQueue::close() {
     // If _is_cancelled is not set to true, there may be concurrent send
     // which add batch to _block_queue. The batch added after _block_queue
     // is clear will be memory leak
-    std::lock_guard<std::mutex> l(_lock);
+    INJECT_MOCK_SLEEP(std::lock_guard<std::mutex> l(_lock));
     _is_cancelled = true;
     set_source_ready(l);
 
