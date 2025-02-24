c1ed1da8eadad267029d8467f4c946bdde7663d7
bobhan1 <baohan@selectdb.com>
2025-02-24 10:24:28 +0800

    update

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/LoadCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/LoadCommand.java
@@ -241,8 +241,7 @@ public class LoadCommand extends Command implements NeedAuditEncryption, Forward
         tvfLogicalPlan = new LogicalProject<>(selectLists, tvfLogicalPlan);
         checkAndAddSequenceCol(olapTable, dataDesc, sinkCols, selectLists);
         boolean isPartialUpdate = olapTable.getEnableUniqueKeyMergeOnWrite()
-                && properties.getOrDefault("partial_columns", "false").equalsIgnoreCase("true");
-        LOG.info("[xxx completeQueryPlan] properties={}, isPartialUpdate={}", properties, isPartialUpdate);
+                && sinkCols.size() < olapTable.getColumns().size();
         return UnboundTableSinkCreator.createUnboundTableSink(dataDesc.getNameParts(), sinkCols, ImmutableList.of(),
                 false, dataDesc.getPartitionNames(), isPartialUpdate, DMLCommandType.LOAD, tvfLogicalPlan);
     }
