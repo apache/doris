f2b2de3039ca718e3a6aa309e47846c93eb884da
amory <wangqiannan@selectdb.com>
2025-02-24 20:51:13 +0800

    [fix](serde) Fix the behavior of serializing ip/date types nested in complex types… (#47889)
    Fix the behavior of serializing ip/date types nested in complex types
    
    before with to_json we will have this result if we meet ip/date type in
    complex type
    ```
    {\"a\": 1, \"b\": \"b1\", \"c\": 192.168.1.1, \"d\": 1.000000000}
    ```
    after we fix the behavior
    ```
    {"a":1,"b":"b1","c":"192.168.1.1","d":1.0}
    ```

--- be/src/vec/data_types/serde/data_type_ipv6_serde.cpp
+++ be/src/vec/data_types/serde/data_type_ipv6_serde.cpp
@@ -92,6 +92,9 @@ void DataTypeIPv6SerDe::write_one_cell_to_jsonb(const IColumn& column,
 Status DataTypeIPv6SerDe::serialize_one_cell_to_json(const IColumn& column, int64_t row_num,
                                                      BufferWritable& bw,
                                                      FormatOptions& options) const {
+    if (_nesting_level > 1) {
+        bw.write('"');
+    }
     auto result = check_column_const_set_readability(column, row_num);
     ColumnPtr ptr = result.first;
     row_num = result.second;
@@ -99,11 +102,17 @@ Status DataTypeIPv6SerDe::serialize_one_cell_to_json(const IColumn& column, int6
     IPv6Value ipv6_value(data);
     std::string ipv6_str = ipv6_value.to_string();
     bw.write(ipv6_str.c_str(), ipv6_str.length());
+    if (_nesting_level > 1) {
+        bw.write('"');
+    }
     return Status::OK();
 }
 
 Status DataTypeIPv6SerDe::deserialize_one_cell_from_json(IColumn& column, Slice& slice,
                                                          const FormatOptions& options) const {
+    if (_nesting_level > 1) {
+        slice.trim_quote();
+    }
     auto& column_data = reinterpret_cast<ColumnIPv6&>(column);
     ReadBuffer rb(slice.data, slice.size);
     IPv6 val = 0;
@@ -176,6 +185,8 @@ void DataTypeIPv6SerDe::read_column_from_arrow(IColumn& column, const arrow::Arr
                                        std::string(raw_data, raw_data_len).c_str());
             }
             col_data.emplace_back(ipv6_val);
+        } else {
+            col_data.emplace_back(0);
         }
     }
 }
