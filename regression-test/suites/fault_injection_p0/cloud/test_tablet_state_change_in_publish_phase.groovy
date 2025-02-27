19c68ea68f7cd54ca37711a54181666a64e194ff
bobhan1 <baohan@selectdb.com>
2025-02-27 12:14:45 +0800

    update

--- regression-test/suites/fault_injection_p0/cloud/test_tablet_state_change_in_publish_phase.groovy
+++ regression-test/suites/fault_injection_p0/cloud/test_tablet_state_change_in_publish_phase.groovy
@@ -57,47 +57,73 @@ suite("test_tablet_state_change_in_publish_phase", "docker") {
         def tabletBackendId = tabletStat.BackendId
         def tabletId = tabletStat.TabletId
         def be1
-        def be2
         for (def be : beNodes) {
             if (be.BackendId == tabletBackendId) {
                 be1 = be
-            } else {
-                be2 = be
             }
         }
         logger.info("tablet ${tabletId} on backend ${be1.Host} with backendId=${be1.BackendId}");
+        logger.info("backends: ${cluster.getBackends()}")
+        int beIndex = 1
+        for (def backend : cluster.getBackends()) {
+            if (backend.host == be1.Host) {
+                beIndex = backend.index
+                break
+            }
+        }
+        assert cluster.getBeByIndex(beIndex).backendId as String == tabletBackendId
 
         try {
             GetDebugPoint().enableDebugPointForAllBEs("CloudSchemaChangeJob::_convert_historical_rowsets.block")
             sql "alter table ${table1} modify column c1 varchar(100);"
-            Thread.sleep(1500)
+            Thread.sleep(1000)
 
-            cluster.stopBackends(1)
+            cluster.stopBackends(beIndex)
 
-            Thread.sleep(2000)
+            Thread.sleep(1000)
 
-            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.enable_spin_wait")
-            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.block")
-
-            // these load will be executed on be2 and skip to calculate delete bitmaps in publish phase
-            def t1 = Thread.start{ sql "insert into ${table1} values(1,99,99);" }
-            Thread.sleep(300)
-            def t2 = Thread.start{ sql "insert into ${table1} values(1,88,88);" }
-
-            cluster.startBackends(1)
-
-            waitForSchemaChangeDone {
-                sql """ SHOW ALTER TABLE COLUMN WHERE TableName='${table1}' ORDER BY createtime DESC LIMIT 1 """
-                time 1000
+            def newThreadInDocker = { Closure actionSupplier ->
+                def connInfo = context.threadLocalConn.get()
+                return Thread.start {
+                    connect(connInfo.username, connInfo.password, connInfo.conn.getMetaData().getURL(), actionSupplier)
+                }
+            }
+            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.enable_spin_wait", [token: "token1"])
+            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.block", [pass_token: "-1"])
+            def t1 = newThreadInDocker {
+                // load 1 will not see any historical data when flush
+                sql "insert into ${table1} values(2,88,88);"
+            }
+            Thread.sleep(800)
+            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.enable_spin_wait", [token: "token2"])
+            def t2 = newThreadInDocker {
+                // load 2 will not see any historical data when flush
+                // load 2 will see version 2,3,4,5 when publish
+                sql "insert into ${table1} values(1,99,99);"
             }
 
+            cluster.startBackends(beIndex)
+            GetDebugPoint().enableDebugPointForAllBEs("CloudSchemaChangeJob.process_alter_tablet.sleep")
+            // GetDebugPoint().disableDebugPointForAllBEs("CloudSchemaChangeJob::_convert_historical_rowsets.block")
+
+            dockerAwaitUntil(30) {
+                def res = sql_return_maparray """ SHOW ALTER TABLE COLUMN WHERE TableName='${table1}' ORDER BY createtime DESC LIMIT 1 """
+                logger.info("alter status: ${res}")
+                res[0].State as String == "FINISHED"
+            }
             // tablet state has changed to NORMAL in MS
+
+            Thread.sleep(1200)
+            // let load 1 publish
+            GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.block", [pass_token: "token1"])
+            t1.join()
+
+            // let load 2 publish
             GetDebugPoint().disableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.enable_spin_wait")
             GetDebugPoint().disableDebugPointForAllFEs("CloudGlobalTransactionMgr.getDeleteBitmapUpdateLock.block")
-
-            t1.join()
             t2.join()
 
+            qt_dup_key_count "select k1,count() as cnt from ${table1} group by k1 having cnt>1;"
             qt_sql "select * from ${table1} order by k1;"
 
         } catch(Exception e) {
