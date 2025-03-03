ac11b4e11024db42214ab354c5aff3f279c25629
Gabriel <liwenqiang@selectdb.com>
2025-03-03 21:24:58 +0800

    [rf](ut) test minmax/bitmap filter (#48579)

--- be/src/runtime_filter/runtime_filter_wrapper.cpp
+++ be/src/runtime_filter/runtime_filter_wrapper.cpp
@@ -239,11 +239,11 @@ Status RuntimeFilterWrapper::_assign(const PInFilter& in_filter, bool contain_nu
     }
 
     auto batch_assign = [this](const PInFilter& filter,
-                               void (*assign_func)(std::shared_ptr<HybridSetBase>& _hybrid_set,
+                               void (*assign_func)(std::shared_ptr<HybridSetBase>& hybrid_set,
                                                    PColumnValue&)) {
         for (int i = 0; i < filter.values_size(); ++i) {
             PColumnValue column = filter.values(i);
-            assign_func(_hybrid_set, column);
+            assign_func(hybrid_set, column);
         }
     };
 
@@ -661,7 +661,9 @@ Status RuntimeFilterWrapper::assign(const T& request, butil::IOBufAsZeroCopyInpu
         return _assign(request.minmax_filter(), request.contain_null());
     }
     default:
-        return Status::InternalError("unknown filter type {}", int(filter_type));
+        return Status::InternalError(
+                "`RuntimeFilterWrapper::assign` is not supported by Filter type {}",
+                int(filter_type));
     }
 }
 
