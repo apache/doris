7a53b8ba40d50a4273db28ed4b29624ed550b969
hezhiqiang <hezhiqiang@selectdb.com>
2025-03-04 15:03:54 +0800

    FX

--- be/src/vec/spill/spill_stream_manager.cpp
+++ be/src/vec/spill/spill_stream_manager.cpp
@@ -154,7 +154,7 @@ Status SpillStreamManager::register_spill_stream(RuntimeState* state, SpillStrea
                                                  const std::string& query_id,
                                                  const std::string& operator_name, int32_t node_id,
                                                  int32_t batch_rows, size_t batch_bytes,
-                                                 RuntimeProfile* profile) {
+                                                 RuntimeProfile* operator_profile) {
     auto data_dirs = _get_stores_for_spill(TStorageMedium::type::SSD);
     if (data_dirs.empty()) {
         data_dirs = _get_stores_for_spill(TStorageMedium::type::HDD);
@@ -184,7 +184,7 @@ Status SpillStreamManager::register_spill_stream(RuntimeState* state, SpillStrea
                 "there is no available disk that can be used to spill.");
     }
     spill_stream = std::make_shared<SpillStream>(state, id, data_dir, spill_dir, batch_rows,
-                                                 batch_bytes, profile);
+                                                 batch_bytes, operator_profile);
     RETURN_IF_ERROR(spill_stream->prepare());
     return Status::OK();
 }
