adddfad910e86de51d0be09723a43dee3ab92bdf
zhangdong <zhangdong@selectdb.com>
2025-03-05 10:13:35 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/analysis/DropDbStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/DropDbStmt.java
@@ -24,7 +24,6 @@ import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.util.InternalDatabaseUtil;
-import org.apache.doris.datasource.InternalCatalog;
 import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 
