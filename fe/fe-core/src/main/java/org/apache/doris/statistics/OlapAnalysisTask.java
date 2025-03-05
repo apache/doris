863ef099827f8bebf89e538992c1074d180c1a1f
James <lijibing@selectdb.com>
2025-03-05 15:34:24 +0800

    [improvement](statistics)Add session variable for partition sample count. (#48218)
    ### What problem does this PR solve?
    The previous pr (https://github.com/apache/doris/pull/46534) control the
    memory use when sample analyzing a large partition table.
    This PR make the maximum rows and partition count to sample
    configurable. User could set the value larger if the NDV is not accurate
    enough.

--- fe/fe-core/src/main/java/org/apache/doris/statistics/OlapAnalysisTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/OlapAnalysisTask.java
@@ -63,7 +63,6 @@ public class OlapAnalysisTask extends BaseAnalysisTask {
     private boolean partitionColumnSampleTooManyRows = false;
     private boolean scanFullTable = false;
     private static final long MAXIMUM_SAMPLE_ROWS = 1_000_000_000;
-    private static final int PARTITION_COUNT_TO_SAMPLE = 5;
 
     @VisibleForTesting
     public OlapAnalysisTask() {
@@ -386,7 +385,8 @@ public class OlapAnalysisTask extends BaseAnalysisTask {
                 MaterializedIndex materializedIndex = p.getIndex(indexId);
                 pickedTabletIds.addAll(materializedIndex.getTabletIdsInOrder());
             }
-            if (pickedRows >= MAXIMUM_SAMPLE_ROWS || pickedPartitionCount > PARTITION_COUNT_TO_SAMPLE) {
+            if (pickedRows >= StatisticsUtil.getPartitionSampleRowCount()
+                    || pickedPartitionCount >= StatisticsUtil.getPartitionSampleCount()) {
                 break;
             }
         }
