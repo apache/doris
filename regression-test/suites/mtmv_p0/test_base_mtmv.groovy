9ec20604558afa2c3e176e90806ba70ba18cbd55
yujun <yujun@selectdb.com>
2025-03-10 12:24:34 +0800

    branch-3.0: [fix](nereids) fix create/alter view column name force cast to lower case #48569 (#48600)
    cherry pick from #48569

--- regression-test/suites/mtmv_p0/test_base_mtmv.groovy
+++ regression-test/suites/mtmv_p0/test_base_mtmv.groovy
@@ -168,6 +168,19 @@ suite("test_base_mtmv","mtmv") {
     order_qt_success "select Name,State,RefreshState  from mv_infos('database'='${dbName}') where Name='${mvName}'"
     mv_rewrite_success_without_check_chosen("""${rewriteSql}""", "${mvName}")
 
+    qt_desc_mv_1 "desc ${mvName}"
+
+    sql """ DROP MATERIALIZED VIEW ${mvName}"""
+    sql """
+        CREATE MATERIALIZED VIEW ${mvName}(event_Day,Id,UserName)
+        BUILD DEFERRED REFRESH COMPLETE ON MANUAL
+        DISTRIBUTED BY RANDOM BUCKETS 2
+        PROPERTIES ('replication_num' = '1') 
+        AS 
+        SELECT event_day,id,username FROM ${tableName};
+    """
+    qt_desc_mv_2 "desc ${mvName}"
+
     sql """drop table if exists `${tableName}`"""
     sql """drop table if exists `${newTableName}`"""
     sql """ DROP MATERIALIZED VIEW ${mvName}"""
