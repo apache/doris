6b5ba783adba84072d0c927f4e34719f44b7aca5
Gabriel <liwenqiang@selectdb.com>
2025-03-10 10:00:51 +0800

    [refactor](pipeline) Refine pipeline logics (#48795)

--- be/src/runtime/fragment_mgr.h
+++ be/src/runtime/fragment_mgr.h
@@ -125,8 +125,7 @@ public:
 
     Status exec_plan_fragment(const TPipelineFragmentParams& params, const QuerySource query_type);
 
-    void remove_pipeline_context(
-            std::shared_ptr<pipeline::PipelineFragmentContext> pipeline_context);
+    void remove_pipeline_context(std::pair<TUniqueId, int> key);
 
     // TODO(zc): report this is over
     Status exec_plan_fragment(const TExecPlanFragmentParams& params, const QuerySource query_type,
