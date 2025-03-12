b642246b5586a0cc8e4118752899965f6de86567
TieweiFang <fangtiewei@selectdb.com>
2025-03-12 14:46:41 +0800

    fix 1

--- be/src/runtime/exec_env_init.cpp
+++ be/src/runtime/exec_env_init.cpp
@@ -614,6 +614,8 @@ void ExecEnv::init_mem_tracker() {
             MemTrackerLimiter::create_shared(MemTrackerLimiter::Type::GLOBAL, "S3FileBuffer");
     _stream_load_pipe_tracker =
             MemTrackerLimiter::create_shared(MemTrackerLimiter::Type::LOAD, "StreamLoadPipe");
+    _parquet_meta_tracker =
+            MemTrackerLimiter::create_shared(MemTrackerLimiter::Type::METADATA, "ParquetMeta");
 }
 
 Status ExecEnv::_check_deploy_mode() {
