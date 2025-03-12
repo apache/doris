8ca340ae95995339bf0f8477d9fa3b2b338fa56c
yujun <yujun@selectdb.com>
2025-03-12 19:02:47 +0800

    fix cast date literal exception

--- fe/fe-core/src/main/java/org/apache/doris/nereids/stats/ExpressionEstimation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/stats/ExpressionEstimation.java
@@ -100,7 +100,6 @@ import org.apache.doris.statistics.Statistics;
 import com.google.common.base.Preconditions;
 import org.apache.commons.collections.CollectionUtils;
 
-import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -223,7 +222,7 @@ public class ExpressionEstimation extends ExpressionVisitor<ColumnStatistic, Sta
                     long max = dateMaxLiteral.getValue();
                     builder.setMaxValue(max);
                     builder.setMaxExpr(dateMaxLiteral.toLegacyLiteral());
-                } catch (DateTimeException | AnalysisException e) {
+                } catch (AnalysisException e) {
                     convertSuccess = false;
                 }
             }
