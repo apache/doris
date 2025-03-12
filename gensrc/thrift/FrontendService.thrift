e0e8f58ce208c537c67f800842f97aa34a274b01
Tiewei Fang <fangtiewei@selectdb.com>
2025-03-12 14:01:21 +0800

    [fix](information_schema) The time type in tables under the information_schema should change according to the time_zone. (#48462)
    Problem Summary:
    Fix the issue where the time type in tables under the information_schema
    does not change with the `time_zone` session variable.

--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -846,6 +846,7 @@ struct TSchemaTableRequestParams {
     3: optional bool replay_to_other_fe
     4: optional string catalog  // use for table specific queries
     5: optional i64 dbId         // used for table specific queries
+    6: optional string time_zone // used for DATETIME field
 }
 
 struct TFetchSchemaTableDataRequest {
@@ -1502,6 +1503,7 @@ struct TGetColumnInfoResult {
 struct TShowProcessListRequest {
     1: optional bool show_full_sql
     2: optional Types.TUserIdentity current_user_ident
+    3: optional string time_zone
 }
 
 struct TShowProcessListResult {
