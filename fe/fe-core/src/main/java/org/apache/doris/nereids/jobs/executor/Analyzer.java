3f217933cfa382dcfc6ac8e260deea54564fbdcc
924060929 <lanhuajian@selectdb.com>
2025-03-14 15:14:44 +0800

    Revert "branch-2.1: [fix](nereids) fix distinct window compute wrong result (#48987) (#49010)"
    This reverts commit 7123bbfecb5c95836c564593ace544a9d5c22ed5.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Analyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Analyzer.java
@@ -30,6 +30,7 @@ import org.apache.doris.nereids.rules.analysis.CheckAnalysis;
 import org.apache.doris.nereids.rules.analysis.CheckPolicy;
 import org.apache.doris.nereids.rules.analysis.CollectJoinConstraint;
 import org.apache.doris.nereids.rules.analysis.CollectSubQueryAlias;
+import org.apache.doris.nereids.rules.analysis.EliminateDistinctConstant;
 import org.apache.doris.nereids.rules.analysis.EliminateGroupByConstant;
 import org.apache.doris.nereids.rules.analysis.EliminateLogicalSelectHint;
 import org.apache.doris.nereids.rules.analysis.FillUpMissingSlots;
@@ -39,6 +40,7 @@ import org.apache.doris.nereids.rules.analysis.NormalizeAggregate;
 import org.apache.doris.nereids.rules.analysis.NormalizeRepeat;
 import org.apache.doris.nereids.rules.analysis.OneRowRelationExtractAggregate;
 import org.apache.doris.nereids.rules.analysis.ProjectToGlobalAggregate;
+import org.apache.doris.nereids.rules.analysis.ProjectWithDistinctToAggregate;
 import org.apache.doris.nereids.rules.analysis.ReplaceExpressionByChildOutput;
 import org.apache.doris.nereids.rules.analysis.SubqueryToApply;
 import org.apache.doris.nereids.rules.analysis.VariableToLiteral;
@@ -103,6 +105,13 @@ public class Analyzer extends AbstractBatchJobExecutor {
             bottomUp(new AddInitMaterializationHook()),
             bottomUp(
                     new ProjectToGlobalAggregate(),
+                    // this rule check's the logicalProject node's isDistinct property
+                    // and replace the logicalProject node with a LogicalAggregate node
+                    // so any rule before this, if create a new logicalProject node
+                    // should make sure isDistinct property is correctly passed around.
+                    // please see rule BindSlotReference or BindFunction for example
+                    new EliminateDistinctConstant(),
+                    new ProjectWithDistinctToAggregate(),
                     new ReplaceExpressionByChildOutput(),
                     new OneRowRelationExtractAggregate()
             ),
