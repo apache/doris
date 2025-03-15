ace6182a46f078196683de22a09b8ae432af253f
meiyi <meiyi@selectdb.com>
2025-03-16 04:19:16 +0800

    [improve](mow) improve mow get_agg by cache (#48968)

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1195,6 +1195,8 @@ DEFINE_mInt64(mow_primary_key_index_max_size_in_memory, "52428800");
 // current txn's publishing version and the max version of the tablet exceeds this value,
 // don't print warning log
 DEFINE_mInt32(publish_version_gap_logging_threshold, "200");
+// get agg by cache for mow table
+DEFINE_mBool(enable_mow_get_agg_by_cache, "true");
 
 // The secure path with user files, used in the `local` table function.
 DEFINE_mString(user_files_secure_path, "${DORIS_HOME}");
