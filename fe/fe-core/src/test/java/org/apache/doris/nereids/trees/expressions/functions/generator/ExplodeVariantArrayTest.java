d07506b0fae5dc33738d6b53651b70f4dfb23796
Benjaminwei <taomingwei1996@gmail.com>
2025-03-16 07:14:40 +0000

    [Improve](explode) fe unit test

---
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/expressions/functions/generator/ExplodeVariantArrayTest.java
@@ -0,0 +1,82 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.trees.expressions.functions.generator;
+
+import org.apache.doris.catalog.FunctionSignature;
+import org.apache.doris.nereids.exceptions.AnalysisException;
+import org.apache.doris.nereids.trees.expressions.Expression;
+import org.apache.doris.nereids.trees.expressions.SlotReference;
+import org.apache.doris.nereids.types.ArrayType;
+import org.apache.doris.nereids.types.IntegerType;
+import org.apache.doris.nereids.types.StructType;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ExplodeVariantArrayTest {
+
+    /////////////////////////////////////////
+    // GetSignatures
+    /////////////////////////////////////////
+
+    @Test
+    public void testGetSignatures() {
+        // build explode(array<int>, array<int>) expression
+        Expression[] args = { SlotReference.of("int", ArrayType.of(IntegerType.INSTANCE)), SlotReference.of("int", ArrayType.of(IntegerType.INSTANCE))};
+        ExplodeVariantArray explode = new ExplodeVariantArray(args);
+
+        // check signature
+        List<FunctionSignature> signatures = explode.getSignatures();
+        Assertions.assertEquals(1, signatures.size());
+        FunctionSignature signature = signatures.get(0);
+        Assertions.assertEquals(2, signature.argumentsTypes.size());
+        Assertions.assertTrue(signature.argumentsTypes.get(0).isArrayType());
+        Assertions.assertTrue(signature.returnType.isStructType());
+        StructType returnType = (StructType) signature.returnType;
+        Assertions.assertEquals(2, returnType.getFields().size());
+        Assertions.assertEquals(IntegerType.INSTANCE, returnType.getFields().get(0).getDataType());
+        Assertions.assertEquals(IntegerType.INSTANCE, returnType.getFields().get(1).getDataType());
+    }
+
+    /////////////////////////////////////////
+    // checkLegalityBeforeTypeCoercion
+    /////////////////////////////////////////
+
+    @Test
+    public void testCheckLegalityBeforeTypeCoercionWithInvalidArgument() {
+        // build explode(int)
+        Expression[] args = { SlotReference.of("int", IntegerType.INSTANCE) };
+        ExplodeVariantArray explode = new ExplodeVariantArray(args);
+
+        // type check
+        Assertions.assertThrows(AnalysisException.class, () -> explode.checkLegalityBeforeTypeCoercion());
+    }
+
+    @Test
+    public void testCheckLegalityBeforeTypeCoercionWithValidArgument() {
+        // build explode(array<int>, array<int>) expression
+        Expression[] args = { SlotReference.of("int", ArrayType.of(IntegerType.INSTANCE)),
+            SlotReference.of("int", ArrayType.of(IntegerType.INSTANCE))};
+        ExplodeVariantArray explode = new ExplodeVariantArray(args);
+
+        // type check
+        explode.checkLegalityBeforeTypeCoercion();
+    }
+}
