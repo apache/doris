c18ce60b6bc880bbbbffd0c4c07e30d640777854
xiedeyantu <czjourney@163.com>
2025-03-17 21:01:28 +0800

    support show databases command

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ShowDatabasesCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ShowDatabasesCommand.java
@@ -18,28 +18,34 @@
 package org.apache.doris.nereids.trees.plans.commands;
 
 import org.apache.doris.catalog.Column;
-import org.apache.doris.catalog.InfoSchemaDb;
+import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.ScalarType;
+import org.apache.doris.cluster.ClusterNamespace;
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.CaseSensibility;
+import org.apache.doris.common.PatternMatcher;
+import org.apache.doris.common.PatternMatcherWrapper;
+import org.apache.doris.datasource.CatalogIf;
 import org.apache.doris.datasource.InternalCatalog;
+import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.nereids.analyzer.UnboundSlot;
+import org.apache.doris.nereids.trees.expressions.EqualTo;
 import org.apache.doris.nereids.trees.expressions.Expression;
-import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionRewriter;
+import org.apache.doris.nereids.trees.expressions.literal.Literal;
 import org.apache.doris.nereids.trees.plans.PlanType;
-import org.apache.doris.nereids.trees.plans.commands.info.AliasInfo;
-import org.apache.doris.nereids.trees.plans.commands.info.TableNameInfo;
-import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
-import org.apache.doris.nereids.util.Utils;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.ShowResultSet;
 import org.apache.doris.qe.ShowResultSetMetaData;
 import org.apache.doris.qe.StmtExecutor;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
+import java.util.Set;
 
 /**
  * ShowDatabasesCommand
@@ -54,6 +60,7 @@ public class ShowDatabasesCommand extends ShowCommand {
     private String catalog;
     private final String likePattern;
     private final Expression whereClause;
+    private String dbName;
 
     /**
      * ShowDatabasesCommand
@@ -68,57 +75,70 @@ public class ShowDatabasesCommand extends ShowCommand {
     /**
      * validate
      */
-    private void validate(ConnectContext ctx) {
+    private void validate(ConnectContext ctx) throws AnalysisException {
         if (Strings.isNullOrEmpty(catalog)) {
             catalog = ctx.getDefaultCatalog();
             if (Strings.isNullOrEmpty(catalog)) {
                 catalog = InternalCatalog.INTERNAL_CATALOG_NAME;
             }
         }
-    }
 
-    /**
-     * replaceColumnNameVisitor
-     * replace column name to real column name
-     */
-    private static class ReplaceColumnNameVisitor extends DefaultExpressionRewriter<Void> {
-        @Override
-        public Expression visitUnboundSlot(UnboundSlot slot, Void context) {
-            if (slot.getName().toLowerCase(Locale.ROOT).equals(DB_COL.toLowerCase(Locale.ROOT))) {
-                return UnboundSlot.quoted(ORI_DB_COL);
+        // extract dbName from where clause
+        if (whereClause != null) {
+            if (whereClause instanceof EqualTo) {
+                EqualTo equalTo = (EqualTo) whereClause;
+                if (equalTo.left() instanceof UnboundSlot && equalTo.right() instanceof Literal) {
+                    dbName = ((Literal) equalTo.right()).getValue().toString();
+                } else {
+                    throw new AnalysisException("Only support where clause with schema_name");
+                }
+            } else {
+                throw new AnalysisException("Only support where clause with schema_name");
             }
-            return slot;
         }
-    }
-
-    /**
-     * execute sql and return result
-     */
-    private ShowResultSet execute(ConnectContext ctx, StmtExecutor executor, String whereClause) {
-        List<AliasInfo> selectList = new ArrayList<>();
-        selectList.add(AliasInfo.of(ORI_DB_COL, DB_COL));
-
-        TableNameInfo fullTblName = new TableNameInfo(catalog, InfoSchemaDb.DATABASE_NAME, "schemata");
-
-        // We need to use TABLE_SCHEMA as a condition to query When querying external catalogs.
-        // This also applies to the internal catalog.
-        LogicalPlan plan = Utils.buildLogicalPlan(selectList, fullTblName, whereClause);
-        List<List<String>> rows = Utils.executePlan(ctx, executor, plan);
-        return new ShowResultSet(META_DATA, rows);
     }
 
     @Override
     public ShowResultSet doRun(ConnectContext ctx, StmtExecutor executor) throws Exception {
         validate(ctx);
-        if (whereClause != null) {
-            Expression rewrited = whereClause.accept(new ReplaceColumnNameVisitor(), null);
-            String whereCondition = " WHERE `CATALOG_NAME` = '" + catalog + "'" + rewrited.toSql();
-            return execute(ctx, executor, whereCondition);
-        } else if (likePattern != null) {
-            return execute(ctx, executor, " WHERE `CATALOG_NAME` = '" + catalog
-                    + "'" + ORI_DB_COL + " LIKE '" + likePattern + "'");
+        List<List<String>> rows = Lists.newArrayList();
+        CatalogIf catalogIf = ctx.getCatalog(catalog);
+        if (catalogIf == null) {
+            throw new AnalysisException("No catalog found with name " + catalog);
         }
-        return execute(ctx, executor,  " WHERE `CATALOG_NAME` = '" + catalog + "'");
+        List<String> dbNames = catalogIf.getDbNames();
+        if (!Strings.isNullOrEmpty(dbName) && dbNames.contains(dbName)) {
+            rows.add(Lists.newArrayList(dbName));
+            return new ShowResultSet(META_DATA, rows);
+        }
+
+        PatternMatcher matcher = null;
+        if (likePattern != null) {
+            matcher = PatternMatcherWrapper.createMysqlPattern(likePattern,
+                    CaseSensibility.DATABASE.getCaseSensibility());
+        }
+        Set<String> dbNameSet = Sets.newTreeSet();
+        for (String fullName : dbNames) {
+            final String db = ClusterNamespace.getNameFromFullName(fullName);
+            // Filter dbname
+            if (matcher != null && !matcher.match(db)) {
+                continue;
+            }
+
+            if (!Env.getCurrentEnv().getAccessManager().checkDbPriv(ConnectContext.get(), catalog,
+                    fullName, PrivPredicate.SHOW)) {
+                continue;
+            }
+
+            dbNameSet.add(db);
+        }
+
+        for (String dbName : dbNameSet) {
+            rows.add(Lists.newArrayList(dbName));
+        }
+
+        rows.sort(Comparator.comparing(x -> x.get(0)));
+        return new ShowResultSet(META_DATA, rows);
     }
 
     @Override
