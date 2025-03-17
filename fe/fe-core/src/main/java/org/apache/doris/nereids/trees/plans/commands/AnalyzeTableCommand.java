4933ab85534e40cd8d3c943ce48f67c54a4777f5
xiedeyantu <czjourney@163.com>
2025-03-17 22:40:21 +0800

    add test for AnalyzeTableCommand

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/AnalyzeTableCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/AnalyzeTableCommand.java
@@ -159,7 +159,7 @@ public class AnalyzeTableCommand extends AnalyzeCommand {
         }
     }
 
-    private void checkAnalyzePrivilege(TableNameInfo tableNameInfo) throws AnalysisException {
+    public void checkAnalyzePrivilege(TableNameInfo tableNameInfo) throws AnalysisException {
         ConnectContext ctx = ConnectContext.get();
         // means it a system analyze
         if (ctx == null) {
