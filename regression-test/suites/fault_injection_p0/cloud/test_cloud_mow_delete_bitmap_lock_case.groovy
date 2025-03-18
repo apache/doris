91604d52813790dd38354349851468c89d7dab85
huanghaibin <huanghaibin@selectdb.com>
2025-03-18 16:08:51 +0800

    [improve](cloud-mow)Add more delete bitmap lock case

--- regression-test/suites/fault_injection_p0/cloud/test_cloud_mow_delete_bitmap_lock_case.groovy
+++ regression-test/suites/fault_injection_p0/cloud/test_cloud_mow_delete_bitmap_lock_case.groovy
@@ -15,7 +15,10 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
+import java.util.concurrent.TimeUnit
+import org.awaitility.Awaitility
+
+suite("test_cloud_mow_delete_bitmap_lock_case", "nonConcurrent") {
     if (!isCloudMode()) {
         return
     }
@@ -68,6 +71,10 @@ suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
 
     def customFeConfig1 = [calculate_delete_bitmap_task_timeout_seconds: 2, meta_service_rpc_retry_times: 5]
     def customFeConfig2 = [delete_bitmap_lock_expiration_seconds: 2, meta_service_rpc_retry_times: 5]
+    def customFeConfig3 = [mow_calculate_delete_bitmap_retry_times: 1]
+    def customFeConfig4 = [calculate_delete_bitmap_task_timeout_seconds: 2, mow_calculate_delete_bitmap_retry_times: 1]
+    def customFeConfig5 = [meta_service_rpc_retry_times: 5]
+    def tableName = "tbl_basic"
     String[][] backends = sql """ show backends """
     assertTrue(backends.size() > 0)
     String backendId;
@@ -112,6 +119,104 @@ suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
         }
     }
 
+    def triggerCompaction = { be_host, be_http_port, compact_type, tablet_id ->
+        if (compact_type == "cumulative") {
+            def (code_1, out_1, err_1) = be_run_cumulative_compaction(be_host, be_http_port, tablet_id)
+            logger.info("Run compaction: code=" + code_1 + ", out=" + out_1 + ", err=" + err_1)
+            assertEquals(code_1, 0)
+            return out_1
+        } else if (compact_type == "full") {
+            def (code_2, out_2, err_2) = be_run_full_compaction(be_host, be_http_port, tablet_id)
+            logger.info("Run compaction: code=" + code_2 + ", out=" + out_2 + ", err=" + err_2)
+            assertEquals(code_2, 0)
+            return out_2
+        } else {
+            assertFalse(True)
+        }
+    }
+
+    def getTabletStatus = { be_host, be_http_port, tablet_id ->
+        boolean running = true
+        Thread.sleep(1000)
+        StringBuilder sb = new StringBuilder();
+        sb.append("curl -X GET http://${be_host}:${be_http_port}")
+        sb.append("/api/compaction/show?tablet_id=")
+        sb.append(tablet_id)
+
+        String command = sb.toString()
+        logger.info(command)
+        process = command.execute()
+        code = process.waitFor()
+        out = process.getText()
+        logger.info("Get tablet status:  =" + code + ", out=" + out)
+        assertEquals(code, 0)
+        def tabletStatus = parseJson(out.trim())
+        return tabletStatus
+    }
+
+    def waitForCompaction = { be_host, be_http_port, tablet_id ->
+        boolean running = true
+        do {
+            Thread.sleep(1000)
+            StringBuilder sb = new StringBuilder();
+            sb.append("curl -X GET http://${be_host}:${be_http_port}")
+            sb.append("/api/compaction/run_status?tablet_id=")
+            sb.append(tablet_id)
+
+            String command = sb.toString()
+            logger.info(command)
+            process = command.execute()
+            code = process.waitFor()
+            out = process.getText()
+            logger.info("Get compaction status: code=" + code + ", out=" + out)
+            assertEquals(code, 0)
+            def compactionStatus = parseJson(out.trim())
+            assertEquals("success", compactionStatus.status.toLowerCase())
+            running = compactionStatus.run_status
+        } while (running)
+    }
+
+    def do_stream_load = {
+        streamLoad {
+            table "${tableName}"
+
+            set 'column_separator', ','
+            set 'columns', 'id, name, score'
+            file "test_stream_load.csv"
+
+            time 10000 // limit inflight 10s
+
+            check { result, exception, startTime, endTime ->
+                log.info("Stream load result: ${result}")
+                def json = parseJson(result)
+                assertEquals("success", json.Status.toLowerCase())
+            }
+        }
+    }
+
+    def do_insert_into = {
+        sql """ INSERT INTO ${tableName} (id, name, score) VALUES (1, "Emily", 25),(2, "Benjamin", 35);"""
+    }
+
+    def getAlterTableState = { table_name ->
+        waitForSchemaChangeDone {
+            sql """ SHOW ALTER TABLE COLUMN WHERE tablename='${table_name}' ORDER BY createtime DESC LIMIT 1 """
+            time 600
+        }
+        return true
+    }
+
+    def waitForSC = {
+        Awaitility.await().atMost(60, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS).pollInterval(1000, TimeUnit.MILLISECONDS).until(() -> {
+            def res = sql_return_maparray "SHOW ALTER TABLE COLUMN WHERE TableName='${tableName}' ORDER BY createtime DESC LIMIT 1"
+            assert res.size() == 1
+            if (res[0].State == "FINISHED" || res[0].State == "CANCELLED") {
+                return true;
+            }
+            return false;
+        });
+    }
+
     try {
         GetDebugPoint().enableDebugPointForAllFEs('FE.mow.check.lock.release', null)
         getTotalRetry.call()
@@ -119,19 +224,19 @@ suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
         // store the original value
         get_be_param("mow_stream_load_commit_retry_times")
         set_be_param("mow_stream_load_commit_retry_times", "2")
-        def tableName = "tbl_basic"
         // create table
         sql """ drop table if exists ${tableName}; """
 
         sql """
         CREATE TABLE `${tableName}` (
             `id` int(11) NOT NULL,
-            `name` varchar(1100) NULL,
-            `score` int(11) NULL default "-1"
+            `name` varchar(10) NULL,
+            `score` int(11) NULL
         ) ENGINE=OLAP
         UNIQUE KEY(`id`)
         DISTRIBUTED BY HASH(`id`) BUCKETS 1
         PROPERTIES (
+            "disable_auto_compaction" = "true",
             "enable_unique_key_merge_on_write" = "true",
             "replication_num" = "1"
         );
@@ -323,6 +428,7 @@ suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
             getTotalRetry.call()
             assertEquals(last_total_retry + 4, total_retry)
             qt_sql8 """ select * from ${tableName} order by id"""
+            reset_be_param("txn_commit_rpc_timeout_ms")
         }
         //5. test wait delete bitmap lock timeout, lock is released normally, will retry
         GetDebugPoint().enableDebugPointForAllFEs("FE.mow.get_delete_bitmap_lock.fail")
@@ -420,8 +526,184 @@ suite("test_cloud_mow_stream_load_with_commit_fail", "nonConcurrent") {
             assertEquals(last_total_retry + 7, total_retry)
             qt_sql12 """ select * from ${tableName} order by id"""
         }
+
+        //7. test parallel load
+        GetDebugPoint().disableDebugPointForAllFEs('FE.mow.check.lock.release')
+        setFeConfigTemporary(customFeConfig2) {
+            GetDebugPoint().enableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.execute.enable_wait")
+            def threads = []
+            def now = System.currentTimeMillis()
+            for (int k = 0; k <= 1; k++) {
+                logger.info("start load thread:" + k)
+                threads.add(Thread.startDaemon {
+                    do_stream_load()
+                })
+            }
+            for (Thread th in threads) {
+                th.join()
+            }
+            def time_cost = System.currentTimeMillis() - now
+            log.info("time_cost(ms): ${time_cost}")
+            assertTrue(time_cost > 6000, "wait time should bigger than 6s")
+
+            threads = []
+            now = System.currentTimeMillis()
+            for (int k = 0; k <= 1; k++) {
+                logger.info("start insert into thread:" + k)
+                threads.add(Thread.startDaemon {
+                    do_insert_into()
+                })
+            }
+            for (Thread th in threads) {
+                th.join()
+            }
+            time_cost = System.currentTimeMillis() - now
+            log.info("time_cost(ms): ${time_cost}")
+            assertTrue(time_cost > 6000, "wait time should bigger than 6s")
+            GetDebugPoint().disableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.execute.enable_wait")
+
+        }
+        //8. test insert into timeout config
+        setFeConfigTemporary(customFeConfig3) {
+            try {
+                GetDebugPoint().enableDebugPointForAllFEs("CloudGlobalTransactionMgr.tryCommitLock.timeout", [sleep_time: 15])
+                sql """ set global insert_visible_timeout_ms=15000; """
+                sql """ INSERT INTO ${tableName} (id, name, score) VALUES (1, "Emily", 25),(2, "Benjamin", 35);"""
+            } catch (Exception e) {
+                logger.info("failed: " + e.getMessage())
+                assertTrue(e.getMessage().contains("test get table cloud commit lock timeout"))
+            } finally {
+                GetDebugPoint().disableDebugPointForAllFEs("CloudGlobalTransactionMgr.tryCommitLock.timeout")
+                sql """ set global insert_visible_timeout_ms=60000; """
+            }
+        }
+        setFeConfigTemporary(customFeConfig4) {
+            try {
+                GetDebugPoint().enableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.execute.enable_wait")
+                sql """ INSERT INTO ${tableName} (id, name, score) VALUES (1, "Emily", 25),(2, "Benjamin", 35);"""
+            } catch (Exception e) {
+                logger.info("failed: " + e.getMessage())
+                assertTrue(e.getMessage().contains("Failed to calculate delete bitmap. Timeout"))
+            } finally {
+                GetDebugPoint().disableDebugPointForAllFEs("CloudEngineCalcDeleteBitmapTask.execute.enable_wait")
+            }
+        }
+
+        //9. when load hold delete bitmap lock, compaction and schema change will fail and retry
+        setFeConfigTemporary(customFeConfig5) {
+//            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (1, "AAA", 15);"""
+//            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (2, "BBB", 25);"""
+//            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (3, "CCC", 35);"""
+//            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (4, "DDD", 45);"""
+//            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (5, "EEE", 55);"""
+            GetDebugPoint().enableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.handle.inject_sleep", [percent: "1.0", sleep: "10"])
+            Thread.startDaemon {
+                do_insert_into()
+            }
+            def tablets = sql_return_maparray """ show tablets from ${tableName}; """
+            logger.info("tablets: " + tablets)
+            for (def tablet in tablets) {
+                String tablet_id = tablet.TabletId
+                def tablet_info = sql_return_maparray """ show tablet ${tablet_id}; """
+                logger.info("tablet: " + tablet_info)
+                String trigger_backend_id = tablet.BackendId
+                def now = System.currentTimeMillis()
+                assertTrue(triggerCompaction(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id],
+                        "cumulative", tablet_id).contains("Success"));
+                waitForCompaction(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id], tablet_id)
+                getTabletStatus(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id], tablet_id);
+                def time_cost = System.currentTimeMillis() - now
+                log.info("time_cost(ms): ${time_cost}")
+                assertTrue(time_cost > 10000, "wait time should bigger than 10s")
+            }
+            Thread.startDaemon {
+                do_insert_into()
+            }
+            def now = System.currentTimeMillis()
+            sql """ alter table ${tableName} order by (id,score,name); """
+            assertTrue(getAlterTableState(tableName), "schema change should success")
+            def time_cost = System.currentTimeMillis() - now
+            log.info("time_cost(ms): ${time_cost}")
+            assertTrue(time_cost > 10000, "wait time should bigger than 10s")
+            GetDebugPoint().disableDebugPointForAllFEs("CloudEngineCalcDeleteBitmapTask.handle.inject_sleep")
+        }
+        //10.test stream load will fail when not found delete bitmap cache
+        setFeConfigTemporary(customFeConfig5) {
+            GetDebugPoint().enableDebugPointForAllBEs("CloudTxnDeleteBitmapCache.get_tablet_txn_info.not_found")
+            try {
+                do_insert_into()
+            } catch (Exception e) {
+                logger.info("failed: " + e.getMessage())
+                assertTrue(e.getMessage().contains("NOT_FOUND"))
+            } finally {
+                GetDebugPoint().disableDebugPointForAllBEs("CloudTxnDeleteBitmapCache.get_tablet_txn_info.not_found")
+            }
+        }
+        //11. test rpc timeout
+        setFeConfigTemporary(customFeConfig5) {
+            get_be_param("txn_commit_rpc_timeout_ms")
+            set_be_param("txn_commit_rpc_timeout_ms", "5000")
+            GetDebugPoint().enableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.handle.inject_sleep", [percent: "1.0", sleep: "15"])
+            def threads = []
+            for (int k = 0; k < 5; k++) {
+                logger.info("start load thread:" + k)
+                threads.add(Thread.startDaemon {
+                    do_stream_load()
+                })
+            }
+            for (Thread th in threads) {
+                th.join()
+            }
+            GetDebugPoint().disableDebugPointForAllBEs("CloudEngineCalcDeleteBitmapTask.handle.inject_sleep")
+            reset_be_param("txn_commit_rpc_timeout_ms")
+        }
+        //12. test compaction or schema change fail will release lock
+        setFeConfigTemporary(customFeConfig5) {
+            get_be_param("delete_bitmap_lock_expiration_seconds")
+            set_be_param("delete_bitmap_lock_expiration_seconds", "60")
+            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (1, "AAA", 15);"""
+            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (2, "BBB", 25);"""
+            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (3, "CCC", 35);"""
+            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (4, "DDD", 45);"""
+            sql """ INSERT INTO ${tableName} (id, name, score) VALUES (5, "EEE", 55);"""
+            def tablets = sql_return_maparray """ show tablets from ${tableName}; """
+            logger.info("tablets: " + tablets)
+            GetDebugPoint().enableDebugPointForAllBEs("CumulativeCompaction.modify_rowsets.trigger_abort_job_failed")
+            for (def tablet in tablets) {
+                String tablet_id = tablet.TabletId
+                def tablet_info = sql_return_maparray """ show tablet ${tablet_id}; """
+                logger.info("tablet: " + tablet_info)
+                String trigger_backend_id = tablet.BackendId
+                assertTrue(triggerCompaction(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id],
+                        "cumulative", tablet_id).contains("Success"));
+                waitForCompaction(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id], tablet_id)
+                getTabletStatus(backendId_to_backendIP[trigger_backend_id], backendId_to_backendHttpPort[trigger_backend_id], tablet_id);
+            }
+            def now = System.currentTimeMillis()
+            do_insert_into()
+            def time_cost = System.currentTimeMillis() - now
+            log.info("time_cost(ms): ${time_cost}")
+            assertTrue(time_cost < 10000, "wait time should less than 10s")
+            GetDebugPoint().disableDebugPointForAllBEs("CumulativeCompaction.modify_rowsets.trigger_abort_job_failed")
+
+//            GetDebugPoint().enableDebugPointForAllBEs("CloudMetaMgr::test_update_delete_bitmap_fail")
+//            sql "alter table ${tableName} modify column score varchar(100);"
+//            waitForSC()
+//            def res = sql_return_maparray "SHOW ALTER TABLE COLUMN WHERE TableName='${tableName}' ORDER BY createtime DESC LIMIT 1"
+//            assert res[0].State == "CANCELLED"
+//            assert res[0].Msg.contains("[DELETE_BITMAP_LOCK_ERROR]test update delete bitmap failed")
+//            now = System.currentTimeMillis()
+//            do_insert_into()
+//            time_cost = System.currentTimeMillis() - now
+//            log.info("time_cost(ms): ${time_cost}")
+//            assertTrue(time_cost < 10000, "wait time should less than 10s")
+            reset_be_param("delete_bitmap_lock_expiration_seconds")
+        }
+
     } finally {
         reset_be_param("mow_stream_load_commit_retry_times")
+        reset_be_param("txn_commit_rpc_timeout_ms")
+        reset_be_param("delete_bitmap_lock_expiration_seconds")
         GetDebugPoint().clearDebugPointsForAllBEs()
         GetDebugPoint().clearDebugPointsForAllFEs()
     }
