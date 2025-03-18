apache/doris

commit b807a9b31ac1e2a4c072078b7766345205eabcd2

[fix][cloud] Prevent fe CloudReplica.getBackendIdImpl consuming too much CPU when high-frequency stream loads (#48564)

fe/fe-core/src/main/java/org/apache/doris/clone/TabletSchedCtx.java
@@ -511,27 +511,28 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
         }
         String host = backend.getHost();
         for (Replica replica : tablet.getReplicas()) {
-            Backend be = infoService.getBackend(replica.getBackendIdWithoutException());
+            long replicaBeId = replica.getBackendIdWithoutException();
+            Backend be = infoService.getBackend(beId);
             if (be == null) {
                 // BE has been dropped, skip it
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} does not exist, skip. tablet: {}",
-                            replica.getBackendIdWithoutException(), tabletId);
+                            replicaBeId, tabletId);
                 }
                 continue;
             }
             if (!Config.allow_replica_on_same_host && !FeConstants.runningUnitTest && host.equals(be.getHost())) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} is on same host {}, skip. tablet: {}",
-                            replica.getBackendIdWithoutException(), host, tabletId);
+                            replicaBeId, host, tabletId);
                 }
                 return true;
             }
 
-            if (replica.getBackendIdWithoutException() == beId) {
+            if (replicaBeId == beId) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} is same as dest backend {}, skip. tablet: {}",
-                            replica.getBackendIdWithoutException(), beId, tabletId);
+                            replicaBeId, beId, tabletId);
                 }
                 return true;
             }
@@ -587,10 +588,11 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
          */
         List<Replica> candidates = Lists.newArrayList();
         for (Replica replica : tablet.getReplicas()) {
-            if (exceptBeId != -1 && replica.getBackendIdWithoutException() == exceptBeId) {
+            long replicaBeId = replica.getBackendIdWithoutException();
+            if (exceptBeId != -1 && replicaBeId == exceptBeId) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} is same as except backend {}, skip. tablet: {}",
-                            replica.getBackendIdWithoutException(), exceptBeId, tabletId);
+                            replicaBeId, exceptBeId, tabletId);
                 }
                 continue;
             }
@@ -603,12 +605,12 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
                 continue;
             }
 
-            Backend be = infoService.getBackend(replica.getBackendIdWithoutException());
+            Backend be = infoService.getBackend(replicaBeId);
             if (be == null || !be.isAlive()) {
                 // backend which is in decommission can still be the source backend
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} does not exist or is not alive, skip. tablet: {}",
-                            replica.getBackendIdWithoutException(), tabletId);
+                            replicaBeId, tabletId);
                 }
                 continue;
             }
@@ -640,11 +642,12 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
         // sort replica by version count asc, so that we prefer to choose replicas with fewer versions
         Collections.sort(candidates, VERSION_COUNTER_COMPARATOR);
         for (Replica srcReplica : candidates) {
-            PathSlot slot = backendsWorkingSlots.get(srcReplica.getBackendIdWithoutException());
+            long replicaBeId = srcReplica.getBackendIdWithoutException();
+            PathSlot slot = backendsWorkingSlots.get(replicaBeId);
             if (slot == null) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} does not have working slot, skip. tablet: {}",
-                            srcReplica.getBackendIdWithoutException(), tabletId);
+                            replicaBeId, tabletId);
                 }
                 continue;
             }
@@ -653,7 +656,7 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
             if (srcPathHash == -1) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("replica's backend {} does not have available slot, skip. tablet: {}",
-                            srcReplica.getBackendIdWithoutException(), tabletId);
+                            replicaBeId, tabletId);
                 }
                 continue;
             }
@@ -701,10 +704,11 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
             }
 
             if (!replica.isScheduleAvailable()) {
-                if (Env.getCurrentSystemInfo().checkBackendScheduleAvailable(replica.getBackendIdWithoutException())) {
+                long replicaBeId = replica.getBackendIdWithoutException();
+                if (Env.getCurrentSystemInfo().checkBackendScheduleAvailable(replicaBeId)) {
                     if (LOG.isDebugEnabled()) {
                         LOG.debug("replica's backend {} does not exist or is not scheduler available, skip. tablet: {}",
-                                replica.getBackendIdWithoutException(), tabletId);
+                                replicaBeId, tabletId);
                     }
                 } else {
                     if (LOG.isDebugEnabled()) {
@@ -816,6 +820,7 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
             throw new SchedException(Status.SCHEDULE_FAILED, SubCode.WAITING_SLOT,
                     "unable to take slot of dest path");
         }
+        long chosenReplicaBeId = chosenReplica.getBackendIdWithoutException();
 
         if (chosenReplica.getState() == ReplicaState.DECOMMISSION) {
             // Since this replica is selected as the repair object of VERSION_INCOMPLETE,
@@ -838,9 +843,9 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
             setDecommissionTime(-1);
             LOG.info("choose replica {} on backend {} of tablet {} as dest replica for version incomplete,"
                     + " and change state from DECOMMISSION to NORMAL",
-                    chosenReplica.getId(), chosenReplica.getBackendIdWithoutException(), tabletId);
+                    chosenReplica.getId(), chosenReplicaBeId, tabletId);
         }
-        setDest(chosenReplica.getBackendIdWithoutException(), chosenReplica.getPathHash());
+        setDest(chosenReplicaBeId, chosenReplica.getPathHash());
     }
 
     private boolean checkFurtherRepairFinish(Replica replica, long version) {
@@ -974,10 +979,11 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
 
     // database lock should be held.
     public CloneTask createCloneReplicaAndTask() throws SchedException {
-        Backend srcBe = infoService.getBackend(srcReplica.getBackendIdWithoutException());
+        long beId = srcReplica.getBackendIdWithoutException();
+        Backend srcBe = infoService.getBackend(beId);
         if (srcBe == null) {
             throw new SchedException(Status.SCHEDULE_FAILED,
-                "src backend " + srcReplica.getBackendIdWithoutException() + " does not exist");
+                "src backend " + beId + " does not exist");
         }
 
         Backend destBe = infoService.getBackend(destBackendId);

fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
@@ -1254,7 +1254,8 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
                     boolean isRestoreTask = true;
                     // We don't care the visible version in restore job, the end version is used.
                     long visibleVersion = -1L;
-                    SnapshotTask task = new SnapshotTask(null, replica.getBackendIdWithoutException(),
+                    long beId = replica.getBackendIdWithoutException();
+                    SnapshotTask task = new SnapshotTask(null, beId,
                             signature, jobId, db.getId(),
                             tbl.getId(), part.getId(), index.getId(), tablet.getId(), visibleVersion,
                             tbl.getSchemaHashByIndexId(index.getId()), timeoutMs, isRestoreTask);
@@ -1263,7 +1264,7 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
                     }
                     batchTask.addTask(task);
                     unfinishedSignatureToId.put(signature, tablet.getId());
-                    bePathsMap.put(replica.getBackendIdWithoutException(), replica.getPathHash());
+                    bePathsMap.put(beId, replica.getPathHash());
                 } finally {
                     tbl.readUnlock();
                 }

fe/fe-core/src/main/java/org/apache/doris/catalog/ColocateTableIndex.java
@@ -176,7 +176,7 @@ public class ColocateTableIndex implements Writable {
     private Multimap<GroupId, Long> group2Tables = ArrayListMultimap.create();
     // table_id -> group_id
     @SerializedName(value = "table2Group")
-    private Map<Long, GroupId> table2Group = Maps.newHashMap();
+    private Map<Long, GroupId> table2Group = Maps.newConcurrentMap();
     // group id -> group schema
     @SerializedName(value = "group2Schema")
     private Map<GroupId, ColocateGroupSchema> group2Schema = Maps.newHashMap();
@@ -385,6 +385,13 @@ public class ColocateTableIndex implements Writable {
         }
     }
 
+    // ATTN: in cloud, CloudReplica.getBackendIdImpl has some logic,
+    // If the FE concurrency is high, the CPU may be fully loaded, so try not to lock it here
+    // table2Group is ConcurrentHashMap
+    public boolean isColocateTableNoLock(long tableId) {
+        return table2Group.containsKey(tableId);
+    }
+
     public boolean isColocateTable(long tableId) {
         readLock();
         try {
@@ -422,6 +429,14 @@ public class ColocateTableIndex implements Writable {
         } finally {
             readUnlock();
         }
+    }
+
+    // ATTN: in cloud, CloudReplica.getBackendIdImpl has some logic,
+    // If the FE concurrency is high, the CPU may be fully loaded, so try not to lock it here
+    // table2Group is ConcurrentHashMap
+    public GroupId getGroupNoLock(long tableId) {
+        Preconditions.checkState(table2Group.containsKey(tableId));
+        return table2Group.get(tableId);
     }
 
     public GroupId getGroup(long tableId) {

fe/fe-core/src/main/java/org/apache/doris/catalog/MetadataViewer.java
@@ -90,7 +90,8 @@ public class MetadataViewer {
                             List<String> row = Lists.newArrayList();
 
                             ReplicaStatus status = ReplicaStatus.OK;
-                            Backend be = infoService.getBackend(replica.getBackendIdWithoutException());
+                            long beId = replica.getBackendIdWithoutException();
+                            Backend be = infoService.getBackend(beId);
                             if (be == null || !be.isAlive() || replica.isBad()) {
                                 status = ReplicaStatus.DEAD;
                             } else if (replica.getVersion() < visibleVersion
@@ -109,7 +110,7 @@ public class MetadataViewer {
 
                             row.add(String.valueOf(tabletId));
                             row.add(String.valueOf(replica.getId()));
-                            row.add(String.valueOf(replica.getBackendIdWithoutException()));
+                            row.add(String.valueOf(beId));
                             row.add(String.valueOf(replica.getVersion()));
                             row.add(String.valueOf(replica.getLastFailedVersion()));
                             row.add(String.valueOf(replica.getLastSuccessVersion()));
@@ -197,7 +198,8 @@ public class MetadataViewer {
                             List<String> row = Lists.newArrayList();
 
                             ReplicaStatus status = ReplicaStatus.OK;
-                            Backend be = infoService.getBackend(replica.getBackendIdWithoutException());
+                            long beId = replica.getBackendIdWithoutException();
+                            Backend be = infoService.getBackend(beId);
                             if (be == null || !be.isAlive() || replica.isBad()) {
                                 status = ReplicaStatus.DEAD;
                             } else if (replica.getVersion() < visibleVersion
@@ -216,7 +218,7 @@ public class MetadataViewer {
 
                             row.add(String.valueOf(tabletId));
                             row.add(String.valueOf(replica.getId()));
-                            row.add(String.valueOf(replica.getBackendIdWithoutException()));
+                            row.add(String.valueOf(beId));
                             row.add(String.valueOf(replica.getVersion()));
                             row.add(String.valueOf(replica.getLastFailedVersion()));
                             row.add(String.valueOf(replica.getLastSuccessVersion()));
@@ -338,13 +340,14 @@ public class MetadataViewer {
                 for (MaterializedIndex index : partition.getMaterializedIndices(IndexExtState.VISIBLE)) {
                     for (Tablet tablet : index.getTablets()) {
                         for (Replica replica : tablet.getReplicas()) {
-                            if (!countMap.containsKey(replica.getBackendIdWithoutException())) {
+                            long beId = replica.getBackendIdWithoutException();
+                            if (!countMap.containsKey(beId)) {
                                 continue;
                             }
-                            countMap.put(replica.getBackendIdWithoutException(),
-                                    countMap.get(replica.getBackendIdWithoutException()) + 1);
-                            sizeMap.put(replica.getBackendIdWithoutException(),
-                                    sizeMap.get(replica.getBackendIdWithoutException()) + replica.getDataSize());
+                            countMap.put(beId,
+                                    countMap.get(beId) + 1);
+                            sizeMap.put(beId,
+                                    sizeMap.get(beId) + replica.getDataSize());
                             totalReplicaNum++;
                             totalReplicaSize += replica.getDataSize();
                         }

fe/fe-core/src/main/java/org/apache/doris/clone/DiskRebalancer.java
@@ -313,12 +313,12 @@ public class DiskRebalancer extends Rebalancer {
         if (replica.getDataSize() == 0) {
             throw new SchedException(Status.UNRECOVERABLE, SubCode.DIAGNOSE_IGNORE, "size of src replica is zero");
         }
-
+        long beId = replica.getBackendIdWithoutException();
         // check src slot
-        PathSlot slot = backendsWorkingSlots.get(replica.getBackendIdWithoutException());
+        PathSlot slot = backendsWorkingSlots.get(beId);
         if (slot == null) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug("BE does not have slot: {}", replica.getBackendIdWithoutException());
+                LOG.debug("BE does not have slot: {}", beId);
             }
             throw new SchedException(Status.UNRECOVERABLE, "unable to take src slot");
         }
@@ -329,7 +329,7 @@ public class DiskRebalancer extends Rebalancer {
         // after take src slot, we can set src replica now
         tabletCtx.setSrc(replica);
 
-        BackendLoadStatistic beStat = clusterStat.getBackendLoadStatistic(replica.getBackendIdWithoutException());
+        BackendLoadStatistic beStat = clusterStat.getBackendLoadStatistic(beId);
         if (!beStat.isAvailable()) {
             throw new SchedException(Status.UNRECOVERABLE, "the backend is not available");
         }

fe/fe-core/src/main/java/org/apache/doris/clone/PartitionRebalancer.java
@@ -284,9 +284,10 @@ public class PartitionRebalancer extends Rebalancer {
             // Check src replica's validation
             Replica srcReplica = tabletCtx.getTablet().getReplicaByBackendId(move.fromBe);
             Preconditions.checkNotNull(srcReplica);
-            TabletScheduler.PathSlot slot = backendsWorkingSlots.get(srcReplica.getBackendIdWithoutException());
+            long beId = srcReplica.getBackendIdWithoutException();
+            TabletScheduler.PathSlot slot = backendsWorkingSlots.get(beId);
             Preconditions.checkNotNull(slot, "unable to get fromBe "
-                    + srcReplica.getBackendIdWithoutException() + " slot");
+                    + beId + " slot");
             if (slot.takeBalanceSlot(srcReplica.getPathHash()) != -1) {
                 tabletCtx.setSrc(srcReplica);
             } else {

fe/fe-core/src/main/java/org/apache/doris/clone/TabletScheduler.java
@@ -1107,8 +1107,9 @@ public class TabletScheduler extends MasterDaemon {
         double maxScore = 0;
         long debugHighBeId = DebugPointUtil.getDebugParamOrDefault("FE.HIGH_LOAD_BE_ID", -1L);
         for (Replica replica : replicas) {
+            long beId = replica.getBackendIdWithoutException();
             BackendLoadStatistic beStatistic = statistic
-                    .getBackendLoadStatistic(replica.getBackendIdWithoutException());
+                    .getBackendLoadStatistic(beId);
             if (beStatistic == null) {
                 continue;
             }
@@ -1132,7 +1133,7 @@ public class TabletScheduler extends MasterDaemon {
                 chosenReplica = replica;
             }
 
-            if (debugHighBeId > 0 && replica.getBackendIdWithoutException() == debugHighBeId) {
+            if (debugHighBeId > 0 && beId == debugHighBeId) {
                 chosenReplica = replica;
                 break;
             }
@@ -1219,6 +1220,7 @@ public class TabletScheduler extends MasterDaemon {
          *      If all are finished, which means this replica is
          *      safe to be deleted.
          */
+        long beId = replica.getBackendIdWithoutException();
         if (!force && !Config.enable_force_drop_redundant_replica
                 && !FeConstants.runningUnitTest
                 && (replica.getState().canLoad() || replica.getState() == ReplicaState.DECOMMISSION)) {
@@ -1228,7 +1230,7 @@ public class TabletScheduler extends MasterDaemon {
                 // Remain it as VERY_HIGH may block other task.
                 tabletCtx.setPriority(Priority.NORMAL);
                 LOG.info("set replica {} on backend {} of tablet {} state to DECOMMISSION due to reason {}",
-                        replica.getId(), replica.getBackendIdWithoutException(), tabletCtx.getTabletId(), reason);
+                        replica.getId(), beId, tabletCtx.getTabletId(), reason);
             }
             try {
                 long preWatermarkTxnId = replica.getPreWatermarkTxnId();
@@ -1237,7 +1239,7 @@ public class TabletScheduler extends MasterDaemon {
                             .getTransactionIDGenerator().getNextTransactionId();
                     replica.setPreWatermarkTxnId(preWatermarkTxnId);
                     LOG.info("set decommission replica {} on backend {} of tablet {} pre watermark txn id {}",
-                            replica.getId(), replica.getBackendId(), tabletCtx.getTabletId(), preWatermarkTxnId);
+                            replica.getId(), beId, tabletCtx.getTabletId(), preWatermarkTxnId);
                 }
 
                 long postWatermarkTxnId = replica.getPostWatermarkTxnId();
@@ -1251,7 +1253,7 @@ public class TabletScheduler extends MasterDaemon {
 
                     replica.setPostWatermarkTxnId(postWatermarkTxnId);
                     LOG.info("set decommission replica {} on backend {} of tablet {} post watermark txn id {}",
-                            replica.getId(), replica.getBackendId(), tabletCtx.getTabletId(), postWatermarkTxnId);
+                            replica.getId(), beId, tabletCtx.getTabletId(), postWatermarkTxnId);
                 }
 
                 if (!Env.getCurrentGlobalTransactionMgr().isPreviousTransactionsFinished(postWatermarkTxnId,
@@ -1276,7 +1278,7 @@ public class TabletScheduler extends MasterDaemon {
             // NOTICE: only delete the replica from meta may not work. sometimes we can depend on tablet report
             // deleting these replicas, but in FORCE_REDUNDANT case, replica may be added to meta again in report
             // process.
-            sendDeleteReplicaTask(replica.getBackendIdWithoutException(), tabletCtx.getTabletId(), replica.getId(),
+            sendDeleteReplicaTask(beId, tabletCtx.getTabletId(), replica.getId(),
                     tabletCtx.getSchemaHash());
         }
 
@@ -1286,12 +1288,12 @@ public class TabletScheduler extends MasterDaemon {
                 tabletCtx.getPartitionId(),
                 tabletCtx.getIndexId(),
                 tabletCtx.getTabletId(),
-                replica.getBackendIdWithoutException());
+                beId);
 
         Env.getCurrentEnv().getEditLog().logDeleteReplica(info);
 
         LOG.info("delete replica. tablet id: {}, backend id: {}. reason: {}, force: {}",
-                tabletCtx.getTabletId(), replica.getBackendIdWithoutException(), reason, force);
+                tabletCtx.getTabletId(), beId, reason, force);
     }
 
     private void sendDeleteReplicaTask(long backendId, long tabletId, long replicaId, int schemaHash) {

fe/fe-core/src/main/java/org/apache/doris/cloud/catalog/CloudReplica.java
@@ -94,7 +94,7 @@ public class CloudReplica extends Replica {
     }
 
     private boolean isColocated() {
-        return Env.getCurrentColocateIndex().isColocateTable(tableId);
+        return Env.getCurrentColocateIndex().isColocateTableNoLock(tableId);
     }
 
     public long getColocatedBeId(String clusterId) throws ComputeGroupException {
@@ -130,7 +130,7 @@ public class CloudReplica extends Replica {
                 ComputeGroupException.FailedTypeEnum.COMPUTE_GROUPS_NO_ALIVE_BE);
         }
 
-        GroupId groupId = Env.getCurrentColocateIndex().getGroup(tableId);
+        GroupId groupId = Env.getCurrentColocateIndex().getGroupNoLock(tableId);
         HashCode hashCode = Hashing.murmur3_128().hashLong(groupId.grpId);
         if (availableBes.size() != bes.size()) {
             // some be is dead recently, still hash tablets on all backends.

commit 43cc4570901d14fc4c2df644fea02d7cf84fc338

[fix](TabletSched) Fix filterDestBE use incorrect beid (#49158)
Introduced by PR https://github.com/apache/doris/pull/48564
Problem Summary:

fe/fe-core/src/main/java/org/apache/doris/clone/TabletSchedCtx.java
@@ -512,7 +512,7 @@ public class TabletSchedCtx implements Comparable<TabletSchedCtx> {
         String host = backend.getHost();
         for (Replica replica : tablet.getReplicas()) {
             long replicaBeId = replica.getBackendIdWithoutException();
-            Backend be = infoService.getBackend(beId);
+            Backend be = infoService.getBackend(replicaBeId);
             if (be == null) {
                 // BE has been dropped, skip it
                 if (LOG.isDebugEnabled()) {

fe/fe-core/src/test/java/org/apache/doris/clone/TabletSchedCtxTest.java
@@ -17,22 +17,69 @@
 
 package org.apache.doris.clone;
 
+import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
+import org.apache.doris.catalog.MaterializedIndex;
+import org.apache.doris.catalog.OlapTable;
+import org.apache.doris.catalog.Partition;
 import org.apache.doris.catalog.Replica;
 import org.apache.doris.catalog.ReplicaAllocation;
+import org.apache.doris.catalog.Table;
+import org.apache.doris.catalog.Tablet;
 import org.apache.doris.clone.TabletSchedCtx.Priority;
 import org.apache.doris.clone.TabletSchedCtx.Type;
 import org.apache.doris.common.Config;
+import org.apache.doris.common.util.DebugPointUtil;
+import org.apache.doris.resource.Tag;
+import org.apache.doris.system.Backend;
+import org.apache.doris.utframe.TestWithFeService;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.MinMaxPriorityQueue;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class TabletSchedCtxTest {
+public class TabletSchedCtxTest extends TestWithFeService {
+    private Database db;
+
+    @Override
+    protected void beforeCreatingConnectContext() throws Exception {
+        Config.enable_debug_points = true;
+        Config.allow_replica_on_same_host = false;
+    }
+
+    @Override
+    protected int backendNum() {
+        return 3;
+    }
+
+    @Override
+    protected void runBeforeAll() throws Exception {
+        Thread.sleep(1000);
+        createDatabase("test");
+        useDatabase("test");
+        db = Env.getCurrentInternalCatalog().getDbOrMetaException("test");
+    }
+
+    @Override
+    protected void runBeforeEach() throws Exception {
+        // set back to default value
+        Config.max_scheduling_tablets = 2000;
+        for (Table table : db.getTables()) {
+            dropTable(table.getName(), true);
+        }
+        for (Backend be : Env.getCurrentSystemInfo().getBackendsByTag(Tag.DEFAULT_BACKEND_TAG)) {
+            be.setDecommissioned(false);
+        }
+        Env.getCurrentEnv().getTabletScheduler().clear();
+        DebugPointUtil.clearDebugPoints();
+        Assertions.assertTrue(checkBEHeartbeat(Env.getCurrentSystemInfo().getBackendsByTag(Tag.DEFAULT_BACKEND_TAG)));
+    }
 
     @Test
     public void testAddTablet() {
@@ -143,4 +190,37 @@ public class TabletSchedCtxTest {
         Assert.assertEquals(-1, replicaList.get(3).getVisibleVersionCount());
     }
 
+    @Test
+    public void testFilterDestBE() throws Exception {
+        createTable("CREATE TABLE tbl1 (k INT) DISTRIBUTED BY HASH(k) BUCKETS 1"
+                + " PROPERTIES ('replication_num' = '2')");
+
+        OlapTable table = (OlapTable) db.getTableOrMetaException("tbl1");
+        Partition partition = table.getPartitions().iterator().next();
+        Tablet tablet = partition.getMaterializedIndices(MaterializedIndex.IndexExtState.ALL).iterator().next()
+                .getTablets().iterator().next();
+
+        TabletSchedCtx tabletSchedCtx = new TabletSchedCtx(Type.REPAIR, db.getId(), table.getId(),
+                partition.getId(), table.getBaseIndexId(),
+                tablet.getId(), ReplicaAllocation.DEFAULT_ALLOCATION, System.currentTimeMillis());
+        tabletSchedCtx.setTablet(table.getPartition(tabletSchedCtx.getPartitionId())
+                .getIndex(tabletSchedCtx.getIndexId()).getTablet(tabletSchedCtx.getTabletId()));
+        // 1L not exist return true
+        Assertions.assertTrue(tabletSchedCtx.filterDestBE(1L));
+        List<Long> backendIds = Env.getCurrentSystemInfo().getAllBackendsByAllCluster().values()
+                .stream()
+                .map(Backend::getId)
+                .collect(Collectors.toList());
+        List<Long> replicasBeIds = tablet.getReplicas().stream()
+                .map(Replica::getBackendIdWithoutException).collect(Collectors.toList());
+
+        Assertions.assertEquals(2, replicasBeIds.size());
+        Assertions.assertTrue(tabletSchedCtx.filterDestBE(replicasBeIds.get(0)));
+        Assertions.assertTrue(tabletSchedCtx.filterDestBE(replicasBeIds.get(1)));
+
+        List<Long> notInReplicaBeIds = backendIds.stream()
+                .filter(beId -> !replicasBeIds.contains(beId)).collect(Collectors.toList());
+        Assertions.assertEquals(1, notInReplicaBeIds.size());
+        Assertions.assertFalse(tabletSchedCtx.filterDestBE(notInReplicaBeIds.get(0)));
+    }
 }