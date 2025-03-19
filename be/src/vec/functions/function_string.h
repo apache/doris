3b61f840f4ae47a83b97772db9ba2d29bc031363
Jerry Hu <hushenggang@selectdb.com>
2025-03-19 17:32:47 +0800

    [fix](function) Undefined behavior in parse_url (#49149) (#49226)

--- be/src/vec/functions/function_string.h
+++ be/src/vec/functions/function_string.h
@@ -122,6 +122,7 @@ struct StringOP {
 
     static void push_value_string(const std::string_view& string_value, int index,
                                   ColumnString::Chars& chars, ColumnString::Offsets& offsets) {
+        DCHECK(string_value.data() != nullptr);
         ColumnString::check_chars_length(chars.size() + string_value.size(), offsets.size());
 
         chars.insert(string_value.data(), string_value.data() + string_value.size());
@@ -3170,6 +3171,9 @@ public:
                     StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                     continue;
                 }
+            } else if (parse_res.empty()) {
+                StringOP::push_empty_string(i, res_chars, res_offsets);
+                continue;
             }
 
             StringOP::push_value_string(std::string_view(parse_res.data, parse_res.size), i,
