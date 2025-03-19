1859c799a5ccc3b9c6872b040305437a29494c47
lambxu <lambxu@tencent.com>
2025-03-20 00:02:25 +0800

    add regression test

--- regression-test/framework/src/main/groovy/org/apache/doris/regression/Config.groovy
+++ regression-test/framework/src/main/groovy/org/apache/doris/regression/Config.groovy
@@ -124,6 +124,7 @@ class Config {
     public boolean withOutLoadData
     public boolean runNonConcurrent
     public String caseNamePrefix
+    public String validateBackupPrefix
     public boolean isSmokeTest
     public String multiClusterBes
     public String metaServiceToken
@@ -154,6 +155,7 @@ class Config {
     Config(
             String s3Source,
             String caseNamePrefix,
+            String validateBackupPrefix,
             String defaultDb, 
             String jdbcUrl, 
             String jdbcUser,
@@ -207,6 +209,7 @@ class Config {
             String cloudVersion) {
         this.s3Source = s3Source
         this.caseNamePrefix = caseNamePrefix
+        this.validateBackupPrefix = validateBackupPrefix
         this.defaultDb = defaultDb
         this.jdbcUrl = jdbcUrl
         this.jdbcUser = jdbcUser
@@ -492,6 +495,7 @@ class Config {
         config.withOutLoadData = cmd.hasOption(withOutLoadDataOpt)
         config.runNonConcurrent = Boolean.parseBoolean(cmd.getOptionValue(runNonConcurrentOpt, "True"))
         config.caseNamePrefix = cmd.getOptionValue(caseNamePrefixOpt, config.caseNamePrefix)
+        config.validateBackupPrefix = cmd.getOptionValue(validateBackupPrefixOpt, config.validateBackupPrefix)
         config.dryRun = cmd.hasOption(dryRunOpt)
         config.isSmokeTest = cmd.hasOption(isSmokeTestOpt)
 
@@ -500,6 +504,7 @@ class Config {
         log.info("withOutLoadData is ${config.withOutLoadData}".toString())
         log.info("runNonConcurrent is ${config.runNonConcurrent}".toString())
         log.info("caseNamePrefix is ${config.caseNamePrefix}".toString())
+        log.info("validateBackupPrefix is ${config.validateBackupPrefix}".toString())
         log.info("dryRun is ${config.dryRun}".toString())
         def s3SourceList = ["aliyun", "aliyun-internal", "tencent", "huawei", "azure", "gcp"]
         if (s3SourceList.contains(config.s3Source)) {
@@ -532,6 +537,7 @@ class Config {
         def config = new Config(
             configToString(obj.s3Source),
             configToString(obj.caseNamePrefix),
+            configToString(obj.validateBackupPrefix),
             configToString(obj.defaultDb),
             configToString(obj.jdbcUrl),
             configToString(obj.jdbcUser),
@@ -722,6 +728,11 @@ class Config {
         if (config.caseNamePrefix == null) {
             config.caseNamePrefix = ""
             log.info("set caseNamePrefix to '' because not specify.".toString())
+        }
+
+        if (config.validateBackupPrefix == null) {
+            config.validateBackupPrefix = "doris_validate_backup"
+            log.info("set validateBackupPrefix to 'doris_validate_backup' because not specify.".toString())
         }
 
         if (config.defaultDb == null) {
