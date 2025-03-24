5fa92b22233dd43954ca7c5451c71441820d87f3
Sridhar R Manikarnike <sridhar.nike@gmail.com>
2025-03-24 15:29:03 +0000

    [Enhancement] (nereids)implement showColumnStatsCommand in nereids

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/visitor/CommandVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/visitor/CommandVisitor.java
@@ -134,6 +134,7 @@ import org.apache.doris.nereids.trees.plans.commands.ShowCharsetCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowClustersCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowCollationCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowColumnHistogramStatsCommand;
+import org.apache.doris.nereids.trees.plans.commands.ShowColumnStatsCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowConfigCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowConstraintsCommand;
 import org.apache.doris.nereids.trees.plans.commands.ShowConvertLSCCommand;
@@ -514,6 +515,10 @@ public interface CommandVisitor<R, C> {
 
     default R visitSetOptionsCommand(SetOptionsCommand setOptionsCommand, C context) {
         return visitCommand(setOptionsCommand, context);
+    }
+
+    default R visitShowColumnStatsCommand(ShowColumnStatsCommand showColumnStatsCommand, C context) {
+        return visitCommand(showColumnStatsCommand, context);
     }
 
     default R visitSetTransactionCommand(SetTransactionCommand setTransactionCommand, C context) {
