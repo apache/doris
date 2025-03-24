3d92508f212e60c1ecb9cd1e1e944dff7dc8a90e
Mryange <yanxuecheng@selectdb.com>
2025-03-24 12:21:38 +0800

    test

--- be/src/vec/aggregate_functions/aggregate_function_corr.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_corr.cpp
@@ -25,66 +25,79 @@ namespace doris::vectorized {
 
 template <typename T>
 struct CorrMoment {
-    T m0 {};
-    T x1 {};
-    T y1 {};
-    T xy {};
-    T x2 {};
-    T y2 {};
+    T meanX {};
+    T meanY {};
+    T c2 {};
+    T m2X {};
+    T m2Y {};
+    int64_t count {0};
 
     void add(T x, T y) {
-        ++m0;
-        x1 += x;
-        y1 += y;
-        xy += x * y;
-        x2 += x * x;
-        y2 += y * y;
+        ++count;
+        T deltaX = x - meanX;
+        T deltaY = y - meanY;
+        meanX += deltaX / count;
+        meanY += deltaY / count;
+        c2 += deltaX * (y - meanY);
+        m2X += deltaX * (x - meanX);
+        m2Y += deltaY * (y - meanY);
     }
 
     void merge(const CorrMoment& rhs) {
-        m0 += rhs.m0;
-        x1 += rhs.x1;
-        y1 += rhs.y1;
-        xy += rhs.xy;
-        x2 += rhs.x2;
-        y2 += rhs.y2;
+        if (rhs.count == 0) return;
+        if (count == 0) {
+            *this = rhs;
+            return;
+        }
+
+        T deltaX = rhs.meanX - meanX;
+        T deltaY = rhs.meanY - meanY;
+        int64_t total_count = count + rhs.count;
+
+        meanX = (meanX * count + rhs.meanX * rhs.count) / total_count;
+        meanY = (meanY * count + rhs.meanY * rhs.count) / total_count;
+
+        c2 += rhs.c2 + deltaX * deltaY * count * rhs.count / total_count;
+        m2X += rhs.m2X + deltaX * deltaX * count * rhs.count / total_count;
+        m2Y += rhs.m2Y + deltaY * deltaY * count * rhs.count / total_count;
+
+        count = total_count;
     }
 
     void write(BufferWritable& buf) const {
-        write_binary(m0, buf);
-        write_binary(x1, buf);
-        write_binary(y1, buf);
-        write_binary(xy, buf);
-        write_binary(x2, buf);
-        write_binary(y2, buf);
+        write_binary(meanX, buf);
+        write_binary(meanY, buf);
+        write_binary(c2, buf);
+        write_binary(m2X, buf);
+        write_binary(m2Y, buf);
+        write_binary(count, buf);
     }
 
     void read(BufferReadable& buf) {
-        read_binary(m0, buf);
-        read_binary(x1, buf);
-        read_binary(y1, buf);
-        read_binary(xy, buf);
-        read_binary(x2, buf);
-        read_binary(y2, buf);
+        read_binary(meanX, buf);
+        read_binary(meanY, buf);
+        read_binary(c2, buf);
+        read_binary(m2X, buf);
+        read_binary(m2Y, buf);
+        read_binary(count, buf);
     }
 
     T get() const {
-        // avoid float error(silent nan) when x or y is constant
-        if (m0 * x2 <= x1 * x1 || m0 * y2 <= y1 * y1) [[unlikely]] {
+        if (m2X == 0 || m2Y == 0) {
             return 0;
         }
-        return (m0 * xy - x1 * y1) / sqrt((m0 * x2 - x1 * x1) * (m0 * y2 - y1 * y1));
+        return c2 / sqrt(m2X * m2Y);
     }
 
     static String name() { return "corr"; }
 
     void reset() {
-        m0 = {};
-        x1 = {};
-        y1 = {};
-        xy = {};
-        x2 = {};
-        y2 = {};
+        meanX = {};
+        meanY = {};
+        c2 = {};
+        m2X = {};
+        m2Y = {};
+        count = 0;
     }
 };
 
