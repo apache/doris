030a6d39f8972ee8e3c06bee9a1789de3561432b
abmdocrt <lianyukang@selectdb.com>
2025-03-25 14:06:37 +0800

    [Fix](Compaction) atomic should not be implicitly converted to int64_t (#49427)
    atomic<int64_t> cannot be implicitly converted to int64_t, which may
    lead to data errors in high concurrency situations.

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -1973,7 +1973,8 @@ Status Tablet::prepare_compaction_and_calculate_permits(CompactionType compactio
             }
             if (!res.is<CUMULATIVE_NO_SUITABLE_VERSION>()) {
                 DorisMetrics::instance()->cumulative_compaction_request_failed->increment(1);
-                return Status::InternalError("prepare cumulative compaction with err: {}", res);
+                return Status::InternalError("prepare cumulative compaction with err: {}",
+                                             res.to_string());
             }
             // return OK if OLAP_ERR_CUMULATIVE_NO_SUITABLE_VERSION, so that we don't need to
             // print too much useless logs.
@@ -2008,7 +2009,8 @@ Status Tablet::prepare_compaction_and_calculate_permits(CompactionType compactio
             permits = 0;
             if (!res.is<BE_NO_SUITABLE_VERSION>()) {
                 DorisMetrics::instance()->base_compaction_request_failed->increment(1);
-                return Status::InternalError("prepare base compaction with err: {}", res);
+                return Status::InternalError("prepare base compaction with err: {}",
+                                             res.to_string());
             }
             // return OK if OLAP_ERR_BE_NO_SUITABLE_VERSION, so that we don't need to
             // print too much useless logs.
@@ -2024,7 +2026,8 @@ Status Tablet::prepare_compaction_and_calculate_permits(CompactionType compactio
             tablet->set_last_full_compaction_failure_time(UnixMillis());
             permits = 0;
             if (!res.is<FULL_NO_SUITABLE_VERSION>()) {
-                return Status::InternalError("prepare full compaction with err: {}", res);
+                return Status::InternalError("prepare full compaction with err: {}",
+                                             res.to_string());
             }
             // return OK if OLAP_ERR_BE_NO_SUITABLE_VERSION, so that we don't need to
             // print too much useless logs.
