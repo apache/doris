2e0cc20558193403a49699e244138a70a1823fc2
zhangstar333 <zhangsida@selectdb.com>
2025-03-25 18:13:46 +0800

    update

--- be/src/vec/aggregate_functions/aggregate_function_min_max.h
+++ be/src/vec/aggregate_functions/aggregate_function_min_max.h
@@ -476,6 +476,9 @@ struct AggregateFunctionMaxData : public Data {
     using Data::IsFixedLength;
     constexpr static bool IS_ANY = false;
 
+    AggregateFunctionMaxData() { reset(); }
+    AggregateFunctionMaxData(const DataTypes& argument_types) { reset(); }
+
     void change_if_better(const IColumn& column, size_t row_num, Arena*) {
         if constexpr (Data::IsFixedLength) {
             this->change_if(column, row_num, false);
@@ -501,6 +504,9 @@ struct AggregateFunctionMinData : Data {
     using Self = AggregateFunctionMinData;
     using Data::IsFixedLength;
     constexpr static bool IS_ANY = false;
+
+    AggregateFunctionMinData() { reset(); }
+    AggregateFunctionMinData(const DataTypes& argument_types) { reset(); }
 
     void change_if_better(const IColumn& column, size_t row_num, Arena*) {
         if constexpr (Data::IsFixedLength) {
