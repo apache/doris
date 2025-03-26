49eadafb373517cadf625b7875cf1a97fdd66908
starocean999 <lichi@selectdb.com>
2025-03-26 09:59:38 +0800

    [fix](nereids)canInferNotNullForMarkSlot method get wrong result if fold constant rule is disabled (#49428)
    canInferNotNullForMarkSlot will get wrong result if the expression can't
    be fold to boolean literal or null. After this pr, the mark slot can be
    set as not null only if meets one of the following conditions:
    1. conjunct is fold to True literal in all conditions.
    2. conjunct is fold to Null or False literal in all conditions.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/util/ExpressionUtils.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/util/ExpressionUtils.java
@@ -665,7 +665,7 @@ public class ExpressionUtils {
              * markSlotSize = 3 -> loopCount = 8  ---- 000, 001, 010, 011, 100, 101, 110, 111
              * markSlotSize = 4 -> loopCount = 16 ---- 0000, 0001, ... 1111
              */
-            int loopCount = 2 << markSlotSize;
+            int loopCount = 1 << markSlotSize;
             for (int i = 0; i < loopCount; ++i) {
                 replaceMap.clear();
                 /*
@@ -693,10 +693,13 @@ public class ExpressionUtils {
                     } else {
                         meetNullOrFalse = true;
                     }
+                } else {
+                    return false;
                 }
             }
+            return true;
         }
-        return true;
+        return false;
     }
 
     private static boolean isNullOrFalse(Expression expression) {
