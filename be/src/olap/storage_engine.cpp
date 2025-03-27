1d1e961d895c152938bf27f8d5af502f6e101840
meiyi <meiyi@selectdb.com>
2025-03-27 16:23:10 +0800

    [fix](mow) remove agg cache version for unused rowsets (#49511)
    remove agg cache version for unused rowsets

--- be/src/olap/storage_engine.cpp
+++ be/src/olap/storage_engine.cpp
@@ -949,6 +949,11 @@ void StorageEngine::_clean_unused_rowset_metas() {
         for (auto& rowset_meta : invalid_rowset_metas) {
             static_cast<void>(RowsetMetaManager::remove(
                     data_dir->get_meta(), rowset_meta->tablet_uid(), rowset_meta->rowset_id()));
+            TabletSharedPtr tablet = _tablet_manager->get_tablet(rowset_meta->tablet_id());
+            if (tablet && tablet->tablet_meta()->enable_unique_key_merge_on_write()) {
+                tablet->tablet_meta()->delete_bitmap().remove_rowset_cache_version(
+                        rowset_meta->rowset_id());
+            }
         }
         LOG(INFO) << "remove " << invalid_rowset_metas.size()
                   << " invalid rowset meta from dir: " << data_dir->path();
@@ -1223,6 +1228,7 @@ void StorageEngine::start_delete_unused_rowset() {
             tablet && tablet->enable_unique_key_merge_on_write()) {
             tablet->tablet_meta()->delete_bitmap().remove({rs->rowset_id(), 0, 0},
                                                           {rs->rowset_id(), UINT32_MAX, 0});
+            tablet->tablet_meta()->delete_bitmap().remove_rowset_cache_version(rs->rowset_id());
         }
         Status status = rs->remove();
         unused_rowsets_counter << -1;
