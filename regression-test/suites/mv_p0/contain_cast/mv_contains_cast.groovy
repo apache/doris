bbeea06b26d1d8e9bdced823932769fb9474eea7
seawinde <wusi@selectdb.com>
2025-03-27 20:26:13 +0800

    [opt](mtmv) Add pre materialized view rewrite in RBO to avoid rewrite fail when plan is too complex

--- regression-test/suites/mv_p0/contain_cast/mv_contains_cast.groovy
+++ regression-test/suites/mv_p0/contain_cast/mv_contains_cast.groovy
@@ -21,6 +21,9 @@ suite("mv_contains_cast") {
     sql "set runtime_filter_mode=OFF";
     sql "SET ignore_shape_nodes='PhysicalDistribute,PhysicalProject'"
 
+
+    // this mv rewrite would not be rewritten in RBO phase, so set TRY_IN_RBO explicitly to make case stable
+    sql "set pre_materialized_view_rewrite_strategy = TRY_IN_RBO"
     sql """
     drop table if exists test;
     """
--- regression-test/suites/nereids_syntax_p0/mv/newMv/unique_mv.groovy
+++ regression-test/suites/nereids_syntax_p0/mv/newMv/unique_mv.groovy
@@ -16,6 +16,8 @@
 // under the License.
 
 suite ("unique_mv") {
+    // this mv rewrite would not be rewritten in RBO phase, so set TRY_IN_RBO explicitly to make case stable
+    sql "set pre_materialized_view_rewrite_strategy = TRY_IN_RBO"
     sql """set enable_nereids_planner=true;"""
     sql """set enable_fallback_to_original_planner=false"""
     sql """ DROP TABLE IF EXISTS c5816_t; """
