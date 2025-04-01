276f1970887dce8710c3e3b9f61e41f03f7d24aa
daidai <changyuwei@selectdb.com>
2025-04-01 09:59:08 +0800

    [feat](test)add some be ut for orc/parquet reader  (#49418)
    ### What problem does this PR solve?
    Problem Summary:
    This PR adds unit tests for the existing parquet_reader and orc_reader,
    and removes some useless code.

--- be/src/vec/exec/format/orc/vorc_reader.cpp
+++ be/src/vec/exec/format/orc/vorc_reader.cpp
@@ -486,6 +486,8 @@ bool OrcReader::_check_acid_schema(const orc::Type& type) {
                 return false;
             }
         }
+    } else {
+        return false;
     }
     return true;
 }
@@ -1679,15 +1681,9 @@ Status OrcReader::_fill_doris_data_column(const std::string& col_name,
     case TypeIndex::Decimal128V3:
         return _decode_decimal_column<Decimal128V3, is_filter>(col_name, data_column, data_type,
                                                                cvb, num_values);
-    case TypeIndex::Date:
-        return _decode_time_column<VecDateTimeValue, Int64, orc::LongVectorBatch, is_filter>(
-                col_name, data_column, cvb, num_values);
     case TypeIndex::DateV2:
         return _decode_time_column<DateV2Value<DateV2ValueType>, UInt32, orc::LongVectorBatch,
                                    is_filter>(col_name, data_column, cvb, num_values);
-    case TypeIndex::DateTime:
-        return _decode_time_column<VecDateTimeValue, Int64, orc::TimestampVectorBatch, is_filter>(
-                col_name, data_column, cvb, num_values);
     case TypeIndex::DateTimeV2:
         return _decode_time_column<DateV2Value<DateTimeV2ValueType>, UInt64,
                                    orc::TimestampVectorBatch, is_filter>(col_name, data_column, cvb,
