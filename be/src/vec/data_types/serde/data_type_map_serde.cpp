eb5ed02e6b934e341a9859a187082560d28c444a
TieweiFang <fangtiewei@selectdb.com>
2025-04-01 14:36:26 +0800

    fix 3

--- be/src/vec/data_types/serde/data_type_map_serde.cpp
+++ be/src/vec/data_types/serde/data_type_map_serde.cpp
@@ -429,6 +429,7 @@ Status DataTypeMapSerDe::_write_column_to_mysql(const IColumn& column,
                 return Status::InternalError("pack mysql buffer failed.");
             }
         } else {
+            ++options.level;
             if (is_key_string && options.wrapper_len > 0) {
                 if (0 != result.push_string(options.nested_string_wrapper, options.wrapper_len)) {
                     return Status::InternalError("pack mysql buffer failed.");
@@ -442,6 +443,7 @@ Status DataTypeMapSerDe::_write_column_to_mysql(const IColumn& column,
                 RETURN_IF_ERROR(key_serde->write_column_to_mysql(nested_keys_column, result, j,
                                                                  false, options));
             }
+            --options.level;
         }
         if (0 != result.push_string(&options.map_key_delim, 1)) {
             return Status::InternalError("pack mysql buffer failed.");
@@ -451,6 +453,7 @@ Status DataTypeMapSerDe::_write_column_to_mysql(const IColumn& column,
                 return Status::InternalError("pack mysql buffer failed.");
             }
         } else {
+            ++options.level;
             if (is_val_string && options.wrapper_len > 0) {
                 if (0 != result.push_string(options.nested_string_wrapper, options.wrapper_len)) {
                     return Status::InternalError("pack mysql buffer failed.");
@@ -464,6 +467,7 @@ Status DataTypeMapSerDe::_write_column_to_mysql(const IColumn& column,
                 RETURN_IF_ERROR(value_serde->write_column_to_mysql(nested_values_column, result, j,
                                                                    false, options));
             }
+            --options.level;
         }
     }
     if (0 != result.push_string("}", 1)) {
