280aba1fde6702cecac531370384bc96083eace2
abmdocrt <lianyukang@selectdb.com>
2025-04-01 10:07:58 +0800

    [Fix](case) Fix cold data compaction fault injection case (#49702)

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -1043,17 +1043,17 @@ uint32_t Tablet::_calc_cumulative_compaction_score(
     if (cumulative_compaction_policy == nullptr) [[unlikely]] {
         return 0;
     }
-    DBUG_EXECUTE_IF("Tablet._calc_cumulative_compaction_score.return", {
-        LOG_WARNING("Tablet._calc_cumulative_compaction_score.return")
-                .tag("tablet id", tablet_id());
-        return 0;
-    });
 #ifndef BE_TEST
     if (_cumulative_compaction_policy == nullptr ||
         _cumulative_compaction_policy->name() != cumulative_compaction_policy->name()) {
         _cumulative_compaction_policy = cumulative_compaction_policy;
     }
 #endif
+    DBUG_EXECUTE_IF("Tablet._calc_cumulative_compaction_score.return", {
+        LOG_WARNING("Tablet._calc_cumulative_compaction_score.return")
+                .tag("tablet id", tablet_id());
+        return 0;
+    });
     return _cumulative_compaction_policy->calc_cumulative_compaction_score(this);
 }
 
