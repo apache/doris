6c898b317578809b9d025246f80135ccff094cff
Socrates <suyiteng@selectdb.com>
2025-04-01 16:57:20 +0800

    optimize const

--- be/src/vec/functions/function_map.cpp
+++ be/src/vec/functions/function_map.cpp
@@ -307,34 +307,75 @@ public:
                         uint32_t result, size_t input_rows_count) const override {
         DCHECK(arguments.size() == 3);
 
+        bool cols_const[2];
+        ColumnPtr cols[2];
+        for (size_t i = 0; i < 2; ++i) {
+            cols_const[i] = is_column_const(*block.get_by_position(arguments[i]).column);
+        }
+        // convert to full column if necessary
+        default_preprocess_parameter_columns(cols, cols_const, {0, 1}, block, arguments);
+        const auto& [col3, col3_const] =
+                unpack_if_const(block.get_by_position(arguments[2]).column);
+
+        const auto& str_column = assert_cast<const ColumnString*>(cols[0].get());
+        const auto& pair_delim_column = assert_cast<const ColumnString*>(cols[1].get());
+        const auto& kv_delim_column = assert_cast<const ColumnString*>(col3.get());
+
+        ColumnPtr result_col;
+        if (cols_const[0] && cols_const[1]) {
+            result_col = execute_vector<true, false>(input_rows_count, *str_column,
+                                                     *pair_delim_column, *kv_delim_column);
+        } else if (col3_const) {
+            result_col = execute_vector<false, true>(input_rows_count, *str_column,
+                                                     *pair_delim_column, *kv_delim_column);
+        } else {
+            result_col = execute_vector<false, false>(input_rows_count, *str_column,
+                                                      *pair_delim_column, *kv_delim_column);
+        }
+
+        block.replace_by_position(result, std::move(result_col));
+
+        return Status::OK();
+    }
+
+private:
+    template <bool is_str_and_pair_delim_const, bool is_kv_delim_const>
+    static ColumnPtr execute_vector(const size_t input_rows_count, const ColumnString& str_col,
+                                    const ColumnString& pair_delim_col,
+                                    const ColumnString& kv_delim_col) {
         // map keys column
         auto result_col_map_keys_data =
                 ColumnNullable::create(ColumnString::create(), ColumnUInt8::create());
+        result_col_map_keys_data->reserve(input_rows_count);
         // map values column
         auto result_col_map_vals_data =
                 ColumnNullable::create(ColumnString::create(), ColumnUInt8::create());
+        result_col_map_vals_data->reserve(input_rows_count);
         // map offsets column
         auto result_col_map_offsets = ColumnUInt64::create();
+        result_col_map_offsets->reserve(input_rows_count);
 
-        bool col_const[3];
-        ColumnPtr argument_columns[3];
-        for (size_t i = 0; i < 3; ++i) {
-            col_const[i] = is_column_const(*block.get_by_position(arguments[i]).column);
+        std::vector<std::string_view> kvs;
+        std::string_view kv_delim;
+        if constexpr (is_str_and_pair_delim_const) {
+            auto str = str_col.get_data_at(0).to_string_view();
+            auto pair_delim = pair_delim_col.get_data_at(0).to_string_view();
+            kvs = split_pair_by_delim(str, pair_delim);
         }
-        default_preprocess_parameter_columns(argument_columns, col_const, {0, 1, 2}, block,
-                                             arguments);
+        if constexpr (is_kv_delim_const) {
+            kv_delim = kv_delim_col.get_data_at(0).to_string_view();
+        }
 
-        const auto* str_column = assert_cast<const ColumnString*>(argument_columns[0].get());
-        const auto* pair_delim_column = assert_cast<const ColumnString*>(argument_columns[1].get());
-        const auto* kv_delim_column = assert_cast<const ColumnString*>(argument_columns[2].get());
+        for (size_t i = 0; i < input_rows_count; ++i) {
+            if constexpr (!is_str_and_pair_delim_const) {
+                auto str = str_col.get_data_at(i).to_string_view();
+                auto pair_delim = pair_delim_col.get_data_at(i).to_string_view();
+                kvs = split_pair_by_delim(str, pair_delim);
+            }
+            if constexpr (!is_kv_delim_const) {
+                kv_delim = kv_delim_col.get_data_at(i).to_string_view();
+            }
 
-        // only consider all the parameters are constant
-        if (col_const[0] && col_const[1] && col_const[2]) {
-            auto str = str_column->get_data_at(0).to_string_view();
-            auto pair_delim = pair_delim_column->get_data_at(0).to_string_view();
-            auto kv_delim = kv_delim_column->get_data_at(0).to_string_view();
-
-            auto kvs = split_pair_by_delim(str, pair_delim);
             for (const auto& kv : kvs) {
                 auto kv_parts = split_kv_by_delim(kv, kv_delim);
                 if (kv_parts.size() == 2) {
@@ -345,41 +386,14 @@ public:
                     result_col_map_vals_data->insert_default();
                 }
             }
-            result_col_map_offsets->insert_many_vals(result_col_map_keys_data->size(),
-                                                     input_rows_count);
-        } else {
-            for (size_t i = 0; i < input_rows_count; ++i) {
-                const auto str = str_column->get_data_at(i).to_string_view();
-                const auto pair_delim = pair_delim_column->get_data_at(i).to_string_view();
-                const auto kv_delim = kv_delim_column->get_data_at(i).to_string_view();
-
-                auto kvs = split_pair_by_delim(str, pair_delim);
-                for (const auto& kv : kvs) {
-                    auto kv_parts = split_kv_by_delim(kv, kv_delim);
-                    if (kv_parts.size() == 2) {
-                        result_col_map_keys_data->insert_data(kv_parts[0].data(),
-                                                              kv_parts[0].size());
-                        result_col_map_vals_data->insert_data(kv_parts[1].data(),
-                                                              kv_parts[1].size());
-                    } else {
-                        result_col_map_keys_data->insert_data(kv.data(), kv.size());
-                        result_col_map_vals_data->insert_default();
-                    }
-                }
-                result_col_map_offsets->insert_value(result_col_map_keys_data->size());
-            }
+            result_col_map_offsets->insert_value(result_col_map_keys_data->size());
         }
 
-        auto result_col = ColumnMap::create(std::move(result_col_map_keys_data),
-                                            std::move(result_col_map_vals_data),
-                                            std::move(result_col_map_offsets));
-
-        block.replace_by_position(result, std::move(result_col));
-
-        return Status::OK();
+        return ColumnMap::create(std::move(result_col_map_keys_data),
+                                 std::move(result_col_map_vals_data),
+                                 std::move(result_col_map_offsets));
     }
 
-private:
     static std::vector<std::string_view> split_pair_by_delim(const std::string_view& str,
                                                              const std::string_view& delim) {
         if (str.empty()) {
