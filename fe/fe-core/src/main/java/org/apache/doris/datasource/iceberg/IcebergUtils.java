6943a5c5508b1525a4983c2b02c6c2bc651341ae
wuwenchi <wuwenchi@selectdb.com>
2025-04-02 16:05:11 +0800

    [opt](iceberg)Optimize the batch mode conditions for Iceberg. (#49489)
    Problem Summary:
    
    1. Add control variables for batch mode.
    2. Determine whether to use batch mode by matching the number of
    manifest files.
    
    Related #46398

--- fe/fe-core/src/main/java/org/apache/doris/datasource/iceberg/IcebergUtils.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/iceberg/IcebergUtils.java
@@ -51,10 +51,13 @@ import org.apache.doris.datasource.property.constants.HMSProperties;
 import org.apache.doris.nereids.exceptions.NotSupportedException;
 import org.apache.doris.thrift.TExprOpcode;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.iceberg.CatalogProperties;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.ManifestFile;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Snapshot;
@@ -63,10 +66,13 @@ import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.expressions.And;
 import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.expressions.Expressions;
+import org.apache.iceberg.expressions.ManifestEvaluator;
 import org.apache.iceberg.expressions.Not;
 import org.apache.iceberg.expressions.Or;
+import org.apache.iceberg.expressions.Projections;
 import org.apache.iceberg.expressions.Unbound;
 import org.apache.iceberg.hive.HiveCatalog;
+import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.types.Type.TypeID;
 import org.apache.iceberg.types.Types;
 import org.apache.iceberg.util.LocationUtil;
@@ -688,5 +694,34 @@ public class IcebergUtils {
 
         hiveCatalog.initialize(name, catalogProperties);
         return hiveCatalog;
+    }
+
+    // Retrieve the manifest files that match the query based on partitions in filter
+    public static CloseableIterable<ManifestFile> getMatchingManifest(
+                List<ManifestFile> dataManifests,
+                Map<Integer, PartitionSpec> specsById,
+                Expression dataFilter) {
+        LoadingCache<Integer, ManifestEvaluator> evalCache = Caffeine.newBuilder()
+                .build(
+                        specId -> {
+                            PartitionSpec spec = specsById.get(specId);
+                            return ManifestEvaluator.forPartitionFilter(
+                                    Expressions.and(
+                                            Expressions.alwaysTrue(),
+                                            Projections.inclusive(spec, true).project(dataFilter)),
+                                    spec,
+                                    true);
+                        });
+
+        CloseableIterable<ManifestFile> matchingManifests = CloseableIterable.filter(
+                CloseableIterable.withNoopClose(dataManifests),
+                manifest -> evalCache.get(manifest.partitionSpecId()).eval(manifest));
+
+        matchingManifests =
+                CloseableIterable.filter(
+                        matchingManifests,
+                        manifest -> manifest.hasAddedFiles() || manifest.hasExistingFiles());
+
+        return matchingManifests;
     }
 }
