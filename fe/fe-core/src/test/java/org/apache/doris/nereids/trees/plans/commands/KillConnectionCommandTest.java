4b8867c4595e239c34ac2ec5aee7be4c21011672
yx-keith <yx136264032@163.com>
2025-04-02 16:41:39 +0800

    optimize code style

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/KillConnectionCommandTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/KillConnectionCommandTest.java
@@ -1,14 +1,31 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.apache.doris.nereids.trees.plans.commands;
 
-import mockit.Expectations;
-import mockit.Mocked;
 import org.apache.doris.catalog.Env;
-import org.apache.doris.common.util.DebugUtil;
 import org.apache.doris.mysql.privilege.AccessControllerManager;
 import org.apache.doris.mysql.privilege.PrivPredicate;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.QueryState;
 import org.apache.doris.qe.StmtExecutor;
+
+import mockit.Expectations;
+import mockit.Mocked;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -52,10 +69,8 @@ public class KillConnectionCommandTest {
     public void testKillConnection() throws Exception {
         runBefore();
         stmtExecutor.execute();
-        String queryId = DebugUtil.printId(stmtExecutor.getContext().queryId());
         KillConnectionCommand command = new KillConnectionCommand(stmtExecutor.getContext().getConnectionId());
         Assertions.assertDoesNotThrow(() -> command.doRun(connectContext, stmtExecutor));
         Assertions.assertEquals(connectContext.getState().getStateType(), QueryState.MysqlStateType.OK);
     }
-
 }
