4ac40137c8e2839781ed5f4e75cbddc170b5cf37
huanghaibin <huanghaibin@selectdb.com>
2025-04-03 10:24:49 +0800

    edit

--- cloud/src/meta-service/meta_service.cpp
+++ cloud/src/meta-service/meta_service.cpp
@@ -2685,7 +2685,7 @@ void MetaServiceImpl::remove_delete_bitmap_update_lock(
     DeleteBitmapUpdateLockPB lock_info;
     if (!check_delete_bitmap_lock(code, msg, ss, txn, instance_id, request->table_id(),
                                   request->lock_id(), request->initiator(), lock_key, lock_info,
-                                  true, ", remove lock")) {
+                                  ", remove lock")) {
         LOG(WARNING) << "failed to check delete bitmap tablet lock"
                      << " table_id=" << request->table_id() << " tablet_id=" << request->tablet_id()
                      << " request lock_id=" << request->lock_id()
