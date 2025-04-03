d2c9a16f79be41e58abaa7c6535b8c833c8810d6
xzj7019 <xiongzhongjian@selectdb.com>
2025-04-03 16:50:14 +0800

    [feat](nereids) experimental hbo refine ut

--- regression-test/suites/nereids_p0/hbo/hbo_join_side_opt_test.groovy
+++ regression-test/suites/nereids_p0/hbo/hbo_join_side_opt_test.groovy
@@ -37,20 +37,19 @@ suite("hbo_join_side_opt_test") {
      | Explain String(Nereids Planner)                                                                                                                                                                                                                              |
      +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
      | cost = 1.0030532524420181E8                                                                                                                                                                                                                                  |
-     | PhysicalResultSink[387] ( outputExprs=[__count_0#4] )                                                                                                                                                                                                        |
-     | +--PhysicalHashAggregate[382]@7 ( stats=1, aggPhase=GLOBAL, aggMode=BUFFER_TO_RESULT, maybeUseStreaming=false, groupByExpr=[], outputExpr=[count(partial_count(*)#9) AS `count(*)`#4], partitionExpr=Optional[[]], topnFilter=false, topnPushDown=false )    |
-     |    +--PhysicalDistribute[377]@9 ( stats=1, distributionSpec=DistributionSpecGather )                                                                                                                                                                         |
-     |       +--PhysicalHashAggregate[372]@9 ( stats=1, aggPhase=LOCAL, aggMode=INPUT_TO_BUFFER, maybeUseStreaming=false, groupByExpr=[], outputExpr=[partial_count(*) AS `partial_count(*)`#9], partitionExpr=Optional[[]], topnFilter=false, topnPushDown=false ) |
-     |          +--PhysicalProject[367]@6 ( stats=1,004.59, projects=[1 AS `1`#8] )                                                                                                                                                                                 |
-     |             +--PhysicalHashJoin[362]@5 ( stats=1,004.59, type=INNER_JOIN, hashCondition=[(b#1 = b#3)], otherCondition=[], markCondition=[], runtimeFilters=[RF0[b#1->[b#3](ndv/size = 1002/1024) , RF1[b#1->[b#3](ndv/size = 1002/1024) ] )                  |
-     |                |--PhysicalProject[341]@4 ( stats=100,000, projects=[b#3] )                                                                                                                                                                                   |
+     | PhysicalResultSink[391] ( outputExprs=[__count_0#4] )                                                                                                                                                                                                        |
+     | +--PhysicalHashAggregate[386]@7 ( stats=1, aggPhase=GLOBAL, aggMode=BUFFER_TO_RESULT, maybeUseStreaming=false, groupByExpr=[], outputExpr=[count(partial_count(*)#9) AS `count(*)`#4], partitionExpr=Optional[[]], topnFilter=false, topnPushDown=false )    |
+     |    +--PhysicalDistribute[381]@9 ( stats=1, distributionSpec=DistributionSpecGather )                                                                                                                                                                         |
+     |       +--PhysicalHashAggregate[376]@9 ( stats=1, aggPhase=LOCAL, aggMode=INPUT_TO_BUFFER, maybeUseStreaming=false, groupByExpr=[], outputExpr=[partial_count(*) AS `partial_count(*)`#9], partitionExpr=Optional[[]], topnFilter=false, topnPushDown=false ) |
+     |          +--PhysicalProject[371]@6 ( stats=1,004.59, projects=[1 AS `1`#8] )                                                                                                                                                                                 |
+     |             +--PhysicalHashJoin[366]@5 ( stats=1,004.59, type=INNER_JOIN, hashCondition=[(b#1 = b#3)], otherCondition=[], markCondition=[], runtimeFilters=[RF0[b#1->[b#3](ndv/size = 1002/1024) , RF1[b#1->[b#3](ndv/size = 1002/1024) ] )                  |
+     |                |--PhysicalProject[345]@4 ( stats=100,000, projects=[b#3] )                                                                                                                                                                                   |
      |                |  +--PhysicalOlapScan[hbo_join_side_opt_test2]@3 ( stats=100,000, RFs= RF0 RF1 )                                                                                                                                                             |
-     |                +--PhysicalDistribute[357]@2 ( stats=1,002.8, distributionSpec=DistributionSpecReplicated )                                                                                                                                                   |
-     |                   +--PhysicalProject[352]@2 ( stats=1,002.8, projects=[b#1] )                                                                                                                                                                                |
-     |                      +--PhysicalFilter[347]@1 ( stats=1,002.8, predicates=(a#0 = 1) )                                                                                                                                                                        |
+     |                +--PhysicalDistribute[361]@2 ( stats=1,002.8, distributionSpec=DistributionSpecReplicated )                                                                                                                                                   |
+     |                   +--PhysicalProject[356]@2 ( stats=1,002.8, projects=[b#1] )                                                                                                                                                                                |
+     |                      +--PhysicalFilter[351]@1 ( stats=1,002.8, predicates=(a#0 = 1) )                                                                                                                                                                        |
      |                         +--PhysicalOlapScan[hbo_join_side_opt_test1]@0 ( stats=100,100,000 )                                                                                                                                                                 |
-     +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
-     */
+     +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+*/
     explain {
         sql "physical plan select count(1) from hbo_join_side_opt_test1 s1, hbo_join_side_opt_test2 s2 where s1.b = s2.b and s1.a = 1;"
         contains("stats=1, aggPhase=GLOBAL")
@@ -69,19 +68,21 @@ suite("hbo_join_side_opt_test") {
      |    +--PhysicalDistribute[378]@9 ( stats=(hbo)1, distributionSpec=DistributionSpecGather )                                                                                                                                                                         |
      |       +--PhysicalHashAggregate[373]@9 ( stats=(hbo)1, aggPhase=LOCAL, aggMode=INPUT_TO_BUFFER, maybeUseStreaming=false, groupByExpr=[], outputExpr=[partial_count(*) AS `partial_count(*)`#9], partitionExpr=Optional[[]], topnFilter=false, topnPushDown=false ) |
      |          +--PhysicalProject[368]@6 ( stats=100,000,001, projects=[1 AS `1`#8] )                                                                                                                                                                                   |
-     |             +--PhysicalHashJoin[363]@5 ( stats=100,000,001, type=INNER_JOIN, hashCondition=[(b#1 = b#3)], otherCondition=[], markCondition=[] )                                                                                                                   |
+     |             +--PhysicalHashJoin[363]@5 ( stats=(hbo)100,000,001, type=INNER_JOIN, hashCondition=[(b#1 = b#3)], otherCondition=[], markCondition=[] )                                                                                                              |
      |                |--PhysicalProject[347]@2 ( stats=100,000,001, projects=[b#1] )                                                                                                                                                                                    |
-     |                |  +--PhysicalFilter[342]@1 ( stats=100,000,001, predicates=(a#0 = 1) )                                                                                                                                                                            |
+     |                |  +--PhysicalFilter[342]@1 ( stats=(hbo)100,000,001, predicates=(a#0 = 1) )                                                                                                                                                                       |
      |                |     +--PhysicalOlapScan[hbo_join_side_opt_test1]@0 ( stats=100,100,000 )                                                                                                                                                                         |
      |                +--PhysicalDistribute[358]@4 ( stats=100,000, distributionSpec=DistributionSpecReplicated )                                                                                                                                                        |
      |                   +--PhysicalProject[353]@4 ( stats=100,000, projects=[b#3] )                                                                                                                                                                                     |
-     |                      +--PhysicalOlapScan[hbo_join_side_opt_test2]@3 ( stats=100,000 )                                                                                                                                                                             |
+     |                      +--PhysicalOlapScan[hbo_join_side_opt_test2]@3 ( stats=100,000 )
      +-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
      */
     explain {
         sql "physical plan select count(1) from hbo_join_side_opt_test1 s1, hbo_join_side_opt_test2 s2 where s1.b = s2.b and s1.a = 1;"
-        contains("stats=100,000,001, predicates=(a#0 = 1)")
+        contains("stats=(hbo)100,000,001, predicates=(a#0 = 1)")
         contains("stats=100,000, distributionSpec=DistributionSpecReplicated")
+        contains("stats=(hbo)100,000,001, type=INNER_JOIN")
+        contains("stats=(hbo)1, aggPhase=LOCAL")
         contains("stats=(hbo)1, aggPhase=GLOBAL")
     }
 
