3a282bd3072ccb363f096502e36f89110f8f354c
zhangdong <zhangdong@selectdb.com>
2025-04-04 20:46:43 +0800

    branch-2.1:[fix](auth)Delete from should not check select_priv (#49794)
    pick: https://github.com/apache/doris/pull/49239

--- regression-test/suites/auth_call/test_dml_delete_table_auth.groovy
+++ regression-test/suites/auth_call/test_dml_delete_table_auth.groovy
@@ -64,17 +64,7 @@ suite("test_dml_delete_table_auth","p0,auth_call") {
         assertTrue(del_res.size() == 0)
     }
     sql """grant load_priv on ${dbName}.${tableName} to ${user}"""
-    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
-        sql """set enable_fallback_to_original_planner=false;"""
-        test {
-            sql """DELETE FROM ${dbName}.${tableName} WHERE id = 3;"""
-            exception "denied"
-        }
-        def del_res = sql """show DELETE from ${dbName}"""
-        assertTrue(del_res.size() == 0)
-    }
-    sql """grant select_priv on ${dbName}.${tableName} to ${user}"""
-    connect(user=user, password="${pwd}", url=context.config.jdbcUrl) {
+    connect(user, "${pwd}", context.config.jdbcUrl) {
         sql """DELETE FROM ${dbName}.${tableName} WHERE id = 3;"""
         def del_res = sql """show DELETE from ${dbName}"""
         logger.info("del_res: " + del_res)
