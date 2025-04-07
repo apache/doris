47fb9aa1aee8851bcd67672e0ce9c81df8b6c54b
yx-keith <yx136264032@163.com>
2025-04-07 15:57:44 +0800

    optimize parition for UT test

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/AlterColumnStatsCommandTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/AlterColumnStatsCommandTest.java
@@ -67,15 +67,18 @@ public class AlterColumnStatsCommandTest extends TestWithFeService {
                 result = true;
             }
         };
+
         //test normal
         connectContext.getSessionVariable().enableStats = true;
         createDatabase("test_db");
-        createTable("create table test_db.test_tbl(k1 int) distributed by hash(k1) buckets 3 properties('replication_num' = '1');");
+        createTable("create table test_db.test.tbl\n" + "(k1 int, k2 int)\n"
+                + "duplicate key(k1)\n" + "partition by range(k2)\n" + "(partition p1 values less than(\"10\"))\n"
+                + "distributed by hash(k2) buckets 1\n" + "properties('replication_num' = '1'); ");
 
         TableNameInfo tableNameInfo =
                 new TableNameInfo("test_db", "test_tbl");
         PartitionNamesInfo partitionNamesInfo = new PartitionNamesInfo(false,
-                ImmutableList.of(CatalogMocker.TEST_PARTITION1_NAME));
+                ImmutableList.of("k2"));
         String indexName = null;
         String columnName = "k1";
         Map<String, String> properties = new HashMap<>();
@@ -91,31 +94,46 @@ public class AlterColumnStatsCommandTest extends TestWithFeService {
         Assertions.assertThrows(AnalysisException.class, () -> command2.validate(connectContext),
                 "Only OlapTable support alter index stats. Table mysql-tbl is not OlapTable.");*/
 
-        //test indexId in OlapTable
-        String indexName3 = "invalid_index";
-        AlterColumnStatsCommand command3 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName3, columnName, properties);
+        //test not a partitioned table
+        createTable("create table test_db.test_tbl2(k1 int) distributed by hash(k1) buckets 3 properties('replication_num' = '1');");
+        TableNameInfo tableNameInfo2 =
+                new TableNameInfo("test_db", "test_tbl2");
+        AlterColumnStatsCommand command2 = new AlterColumnStatsCommand(tableNameInfo2, partitionNamesInfo, indexName, columnName, properties);
+        Assertions.assertThrows(AnalysisException.class, () -> command2.validate(connectContext),
+                "Not a partitioned table: test_tbl2");
+
+        //test partition does not exist
+        PartitionNamesInfo partitionNamesInfo2 = new PartitionNamesInfo(false,
+                ImmutableList.of("k3"));
+        AlterColumnStatsCommand command3 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo2, indexName, columnName, properties);
         Assertions.assertThrows(AnalysisException.class, () -> command3.validate(connectContext),
-                "Index invalid_index not exist in table test_tbl2");
+                "Partition does not exist: k3");
+
+        //test indexId not exist in OlapTable
+        String indexName3 = "invalid_index";
+        AlterColumnStatsCommand command4 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName3, columnName, properties);
+        Assertions.assertThrows(AnalysisException.class, () -> command4.validate(connectContext),
+                "Index invalid_index not exist in table test_tbl");
 
         //test invalid statistics
         Map<String, String> properties2 = new HashMap<>();
         properties2.put("histogram", "invalide_value");
-        AlterColumnStatsCommand command7 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties2);
-        Assertions.assertThrows(AnalysisException.class, () -> command7.validate(connectContext),
+        AlterColumnStatsCommand command5 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties2);
+        Assertions.assertThrows(AnalysisException.class, () -> command5.validate(connectContext),
                 "histogram is invalid statistics");
 
         //row_count is not exist
         Map<String, String> properties3 = new HashMap<>();
         properties2.put("avg_size", "100000");
         properties2.put("max_size", "100000000");
-        AlterColumnStatsCommand command8 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties3);
-        Assertions.assertThrows(AnalysisException.class, () -> command8.validate(connectContext),
+        AlterColumnStatsCommand command6 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties3);
+        Assertions.assertThrows(AnalysisException.class, () -> command6.validate(connectContext),
                  "Set column stats must set row_count. e.g. 'row_count'='5'");
 
         //test enable stats
         connectContext.getSessionVariable().enableStats = false;
-        AlterColumnStatsCommand command9 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties);
-        Assertions.assertThrows(UserException.class, () -> command9.validate(connectContext),
+        AlterColumnStatsCommand command7 = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties);
+        Assertions.assertThrows(UserException.class, () -> command7.validate(connectContext),
                 "Analyze function is forbidden, you should add `enable_stats=true` in your FE conf file");
     }
 
@@ -139,16 +157,16 @@ public class AlterColumnStatsCommandTest extends TestWithFeService {
             }
         };
 
-        TableNameInfo tableNameInfo2 =
+        TableNameInfo tableNameInfo =
                     new TableNameInfo(CatalogMocker.TEST_DB_NAME, CatalogMocker.TEST_TBL2_NAME);
-        PartitionNamesInfo partitionNamesInfo2 = new PartitionNamesInfo(false,
+        PartitionNamesInfo partitionNamesInfo = new PartitionNamesInfo(false,
                     ImmutableList.of(CatalogMocker.TEST_PARTITION1_NAME));
 
         String indexName = "index1";
         String columnName = "k1";
         Map<String, String> properties = new HashMap<>();
 
-        AlterColumnStatsCommand command = new AlterColumnStatsCommand(tableNameInfo2, partitionNamesInfo2, indexName, columnName, properties);
+        AlterColumnStatsCommand command = new AlterColumnStatsCommand(tableNameInfo, partitionNamesInfo, indexName, columnName, properties);
         connectContext.getSessionVariable().enableStats = true;
         Assertions.assertThrows(AnalysisException.class, () -> command.validate(connectContext),
                     "ALTER TABLE STATS command denied to user 'null'@'null' for table 'test_db: test_tbl2'");
