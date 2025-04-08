ce7494a50526ae7d9079a459fbbb6dce561ad4dc
yujun <yujun@selectdb.com>
2025-04-08 11:58:49 +0800

    fix test

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/rewrite/RewriteSqlTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/rewrite/RewriteSqlTest.java
@@ -25,12 +25,25 @@ import org.junit.jupiter.api.Test;
 class RewriteSqlTest extends SqlTestBase {
 
     @Test
-    void testExtractSingleTableExpressionFromDisjunction() {
-        String sql = "select * from T1, T2 where T1.id = 1 or T1.score = 2 and (T1.a = 3 or T1.b = 4 and T2.id = 5)";
+    void testExtractSingleTableExpressionFromDisjunction() throws Exception {
+        createTables(
+                "CREATE TABLE IF NOT EXISTS test_extract_single_1 (\n"
+                        + "    a int,\n"
+                        + "    b int,\n"
+                        + "    c int,\n"
+                        + "    d int\n"
+                        + ")\n"
+                        + "DUPLICATE KEY(a)\n"
+                        + "DISTRIBUTED BY HASH(a) BUCKETS 10\n"
+                        + "PROPERTIES (\n"
+                        + "  \"replication_num\" = \"1\"\n"
+                        + ")\n"
+        );
+        String sql = "select * from test_extract_single_1 t1, T2 where t1.a = 1 or t1.b = 2 and (t1.c = 3 or t1.d = 4 and T2.id = 5)";
         PlanChecker.from(connectContext)
                 .analyze(sql)
                 .applyTopDown(new ExtractSingleTableExpressionFromDisjunction())
-                .matches(logicalFilter().when(f -> f.getConjuncts().stream().anyMatch(e -> e.toSql().equals("OR[(id = 1),AND[(score = 2),OR[(a = 3),(b = 4)]]]"))));
+                .matches(logicalFilter().when(f -> f.getConjuncts().stream().anyMatch(e -> e.toSql().equals("OR[(a = 1),AND[(b = 2),OR[(c = 3),(d = 4)]]]"))));
     }
 
 }
