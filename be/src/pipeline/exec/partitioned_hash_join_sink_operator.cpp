d92cdce855f908de16407e6bfd7c4e2297050d6f
Gabriel <liwenqiang@selectdb.com>
2025-04-08 12:58:48 +0800

    update

--- be/src/pipeline/exec/partitioned_hash_join_sink_operator.cpp
+++ be/src/pipeline/exec/partitioned_hash_join_sink_operator.cpp
@@ -324,10 +324,12 @@ Status PartitionedHashJoinSinkLocalState::terminate(RuntimeState* state) {
     if (_terminated) {
         return Status::OK();
     }
+    HashJoinBuildSinkLocalState* inner_sink_state {nullptr};
+    if (auto* tmp_sink_state = _shared_state->inner_runtime_state->get_sink_local_state()) {
+        inner_sink_state = assert_cast<HashJoinBuildSinkLocalState*>(tmp_sink_state);
+    }
     if (_parent->cast<PartitionedHashJoinSinkOperatorX>()._inner_sink_operator) {
-        RETURN_IF_ERROR(
-                _parent->cast<PartitionedHashJoinSinkOperatorX>()._inner_sink_operator->terminate(
-                        state));
+        RETURN_IF_ERROR(inner_sink_state->_runtime_filter_producer_helper->terminate(state));
     }
     return PipelineXSpillSinkLocalState<PartitionedHashJoinSharedState>::terminate(state);
 }
