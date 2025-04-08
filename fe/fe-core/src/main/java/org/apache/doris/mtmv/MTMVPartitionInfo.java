41d953765adeb75a8f64259a881a89bf812e5ee2
zhangdong <zhangdong@selectdb.com>
2025-04-08 17:23:03 +0800

    add import

--- fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVPartitionInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/mtmv/MTMVPartitionInfo.java
@@ -26,6 +26,7 @@ import org.apache.doris.datasource.mvcc.MvccUtil;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * MTMVPartitionInfo
