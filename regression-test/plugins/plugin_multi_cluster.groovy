2f70adb107b616fcc2c6c18776b20e254ac24dbd
shuke987 <shuke@selectdb.com>
2025-04-08 15:53:47 +0800

    4

--- regression-test/plugins/plugin_multi_cluster.groovy
+++ regression-test/plugins/plugin_multi_cluster.groovy
@@ -19,19 +19,34 @@ import org.apache.doris.regression.suite.Suite
 
 /*
  * After running all regression-tests, check all db and tables
- * have same data.
+ * have same data in multi cluster deploy on cloud.
  */
 Suite.metaClass.check_multi_cluster_result = { clusterNames ->
-    def isOrderBy = {String type ->
-        // typeInfo likes
-/*
-+-----------+--------+------+-------+---------+--------------+
-| Field     | Type   | Null | Key   | Default | Extra        |
-+-----------+--------+------+-------+---------+--------------+
-| datekey   | int    | Yes  | true  | NULL    |              |
-*/
+    def ignoreDBs = ["mysql", "__internal_schema", "information_schema"]
+    // TODO: add skip reason.
+    def specialTables = [
+        "regression_test_function_p0" : ["mock_view": CompareSQLPattern.Skip,],
+        "regression_test_nereids_syntax_p0" : ["test_show_keys_v" : CompareSQLPattern.Skip,],
+        "regression_test_datatype_p0_nested_types_base_cases" : ["test_map_one_level": CompareSQLPattern.Skip,],
+        "regression_test_inverted_index_p0_array_contains": ["httplogs_dup_arr": CompareSQLPattern.OrderByGroovy,],
+        "nested_array_test_2_vectorized": ["nested_array_test_2_vectorized": CompareSQLPattern.OrderByGroovy],
+        "regression_test_nereids_p0_sql_functions_bitmap_functions":["v1":CompareSQLPattern.OrderByGroovy],
+        "regression_test_partition_p0_auto_partition": ["table_date_list" : CompareSQLPattern.OrderByGroovy],
+        "regression_test_query_p0_aggregate": ["test_array_agg_complex": CompareSQLPattern.Skip],
+        "regression_test_query_p0_set_operations": ["nested_array_test_2_vectorized" : CompareSQLPattern.Skip],
+        "regression_test_variant_p0": ["multi_variants" : CompareSQLPattern.Skip],
+        "regression_test_variant_p0_rqg": ["table_100_undef_partitions2_keys3_properties4_distributed_by53", CompareSQLPattern.Skip],
+        "regression_test_variant_p0_with_index": ["test_variant_index_parser_empty" : CompareSQLPattern.Skip],
+    ]
+    enum CompareSQLPattern { Skip, OrderByGroovy, OrderBySQL }
+    
+    /*
+     * return true if column type is a complex type.
+     * type maybe like Map<int, String>, etc.
+     */
+    def isComplexColumn = {String type ->
         type = type.toLowerCase()
-        def noOrderByColumns = ["map", "struct", "hll", "json", "array", "bitmap", "variant", "agg_state", "quantile_state"]
+        def ComplexColumns = ["map", "struct", "hll", "json", "array", "bitmap", "variant", "agg_state", "quantile_state"]
         for (int i = 0; i < noOrderByColumns.size(); i++) {
             if (type.contains(noOrderByColumns[i])) {
                 return false
@@ -39,65 +54,57 @@ Suite.metaClass.check_multi_cluster_result = { clusterNames ->
         }
         return true
     }
-
-    def getSelectSql = { tableName ->
-	def selectPart = "select "
-        def orderbyPart = " order by "
-
-        def syntaxMap = [
-            "bitmap" : "bitmap_to_string(%s)",
-            "map" : "noOrderBy"
-        ]
-        def columnTypes = sql_return_maparray """ show columns from ${tableName} """
-        def hasComplexType = false        
-        for (int i = 0; i < columnTypes.size(); i++) {
-            if (isOrderBy(columnTypes[i]["Type"]) == false) {
-                hasComplexType = true
-                break
-            }
+    
+    /*
+     * There are 2 compare methods:
+     *  1. OrderByGroovy: retrieve all result from table and sort in groovy. like: sql 'select * from table', true
+     *  2. OrderBySQL: retrieve sorted limited result from db, like: sql 'select * from table order by 1,2,3,4,5 limit 100'
+     */
+    def getOrderByType = { db, table ->
+        def p = specialTables[db]?[t]
+        if (p == null) {
+            return CompareSQLPattern.OrderBySQL
+        } else {
+            return p
         }
+    }
 
-        if (hasComplexType == false) {
-            selectPart = "select * "
-            for (int i = 0; i < columnTypes.size(); i++) {
-                if (i == 0) {
-                   orderbyPart += " 1"
+    def getSQL = { db, tableName ->
+        def sqlStmt = "select * from ${tableName} order by "
+        def columnTypes = sql_return_maparray """ show columns from ${tableName} """
+        def firstOrderby = true
+        for (int i = 0; i < columnTypes.size(); i++) {
+            def name = columnTypes[i]["Field"]
+            def type = columnTypes[i]["Type"]
+            if (isComplexColumn(type)) {
+                continue // can not add order by.
+            } else {
+                if (firstOrderby) {
+                    firstOrderby = false
+                    sqlStmt += "${i+1}"
                 } else {
-                   orderbyPart += ",${i+1}"
+                    sqlStmt += ",${i+1}"
                 }
             }
-            return selectPart + " from ${tableName} " + orderbyPart + " limit 100"
         }
 
-        def firstSelect = true
-        def firstOrderby = true
-        def selectIndex = 0
-        def skipKeyWords = ["key", "as", "by", "desc", "@timestamp"]
-        for (int i = 0; i < columnTypes.size(); i++) {
-           def name = columnTypes[i]["Field"]
-           if (skipKeyWords.contains(name)) {
-               continue
-           }            
-           selectIndex++
-           def orderby = isOrderBy(columnTypes[i]["Type"])
-           if (firstSelect) {
-               selectPart += String.format("%s", name)
-               firstSelect = false
-           } else {
-               selectPart += String.format(",%s", name)
-           }
+        if (firstOrderby) {
+            throw new IllegalArgumentException("""db: ${db}, table: ${tableName} is not suitable for orderBySQL """)
+        }
+        return sqlStmt + " limit 100"
+    }
 
-           if (orderby) {
-               if (firstOrderby) {
-                  orderbyPart += "${selectIndex}"
-                  firstOrderby = false
-               } else {
-                  orderbyPart += ",${selectIndex}"
-               }
-           }
+    def assertSameResult = { sqlStmt, order ->
+        def result = []
+
+        clusters.forEach { cluster ->
+            sql """ use @${cluster} """
+            result.add(sql """ ${q} """, order )
         }
 
-        return selectPart + " from ${tableName} " + orderbyPart + " limit 100"
+        if (result.toSet().size() != 1) {
+            throw new IllegalArgumentException(""" different result, db: ${db}, table: ${t}. \n${result}  """)
+        }
     }
 
     // main logic
@@ -106,28 +113,36 @@ Suite.metaClass.check_multi_cluster_result = { clusterNames ->
         return
     }
 
-    def ignore_dbs = ["mysql", "__internal_schema", "information_schema", "regression_test_datatype_p0_nested_types_base_cases", "regression_test_inverted_index_p0_array_contains", "regression_test_load_insert","regression_test_mtmv_p0", "regression_test_nereids_p0_sql_functions_array_functions","regression_test_nereids_p0_sql_functions_bitmap_functions", "regression_test_nereids_p0_sql_functions_datetime_functions", "regression_test_partition_p0_auto_partition","regression_test_query_p0_set_operations","regression_test_variant_p0","regression_test_variant_p0_rqg","test_env_db_dropped_mtmv_db2"]
+    int nErrors = 0
     def dbs = sql """ show databases """
     dbs.forEach { db ->
         db = db[0]
-        if (ignore_dbs.contains(db)) {
+        if (ignoreDBs.contains(db)) {
             return
         }
         sql """ use ${db} """
         def tables = sql """ show tables """
         tables.forEach { t -> 
-            t = t[0]
-            logger.info("process db: ${db}, table: ${t}")
-            def q = getSelectSql(t)
-            def result = []
-            clusters.forEach { cluster ->
-                sql """ use @${cluster} """
-                result.add(sql """ ${q} """ )
+            try {
+                t = t[0]
+                logger.info("process db: ${db}, table: ${t}")
+                
+                def p = getOrderByType(db, t)
+                if (p == CompareSQLPattern.OrderBySQL) {
+                    assertSameResult(getSQL(db, t), false)
+                } else if (p == CompareSQLPattern.Skip) {
+                    return
+                } else if (p == CompareSQLPattern.OrderByGroovy) {
+                    assertSameResult("select * from ${t}", true)
+                }
+            } catch (Exception e) {
+                nErrors++
+                logger.info(""" asssertSameResult failed: """, e)
             }
-
-            if (result.toSet().size() != 1) {
-                throw new IllegalArgumentException(""" different result, db: ${db}, table: ${t}. \n${result}  """)
-            } 
         }
+    }
+
+    if (nErrors > 0) {
+        throw new Exception(""" There are ${nErrors} met in assertSameResult """)
     }
 }
