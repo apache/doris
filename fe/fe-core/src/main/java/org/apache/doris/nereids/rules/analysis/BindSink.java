eb21caf786d766b25fd797e7050134f5398d200d
zy-kkk <zhongyongkang@selectdb.com>
2025-04-09 16:54:46 +0800

    trino connector sink

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSink.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSink.java
@@ -35,6 +35,8 @@ import org.apache.doris.datasource.iceberg.IcebergExternalDatabase;
 import org.apache.doris.datasource.iceberg.IcebergExternalTable;
 import org.apache.doris.datasource.jdbc.JdbcExternalDatabase;
 import org.apache.doris.datasource.jdbc.JdbcExternalTable;
+import org.apache.doris.datasource.trinoconnector.TrinoConnectorExternalDatabase;
+import org.apache.doris.datasource.trinoconnector.TrinoConnectorExternalTable;
 import org.apache.doris.dictionary.Dictionary;
 import org.apache.doris.nereids.CascadesContext;
 import org.apache.doris.nereids.StatementContext;
@@ -45,6 +47,7 @@ import org.apache.doris.nereids.analyzer.UnboundIcebergTableSink;
 import org.apache.doris.nereids.analyzer.UnboundJdbcTableSink;
 import org.apache.doris.nereids.analyzer.UnboundSlot;
 import org.apache.doris.nereids.analyzer.UnboundTableSink;
+import org.apache.doris.nereids.analyzer.UnboundTrinoConnectorTableSink;
 import org.apache.doris.nereids.exceptions.AnalysisException;
 import org.apache.doris.nereids.parser.NereidsParser;
 import org.apache.doris.nereids.pattern.MatchingContext;
@@ -75,6 +78,7 @@ import org.apache.doris.nereids.trees.plans.logical.LogicalOneRowRelation;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalProject;
 import org.apache.doris.nereids.trees.plans.logical.LogicalTableSink;
+import org.apache.doris.nereids.trees.plans.logical.LogicalTrinoConnectorTableSink;
 import org.apache.doris.nereids.trees.plans.logical.UnboundLogicalSink;
 import org.apache.doris.nereids.trees.plans.visitor.InferPlanOutputAlias;
 import org.apache.doris.nereids.types.DataType;
@@ -125,10 +129,12 @@ public class BindSink implements AnalysisRuleFactory {
                 // TODO: bind hive target table
                 RuleType.BINDING_INSERT_HIVE_TABLE.build(unboundHiveTableSink().thenApply(this::bindHiveTableSink)),
                 RuleType.BINDING_INSERT_ICEBERG_TABLE.build(
-                    unboundIcebergTableSink().thenApply(this::bindIcebergTableSink)),
+                        unboundIcebergTableSink().thenApply(this::bindIcebergTableSink)),
                 RuleType.BINDING_INSERT_JDBC_TABLE.build(unboundJdbcTableSink().thenApply(this::bindJdbcTableSink)),
                 RuleType.BINDING_INSERT_DICTIONARY_TABLE
-                        .build(unboundDictionarySink().thenApply(this::bindDictionarySink))
+                        .build(unboundDictionarySink().thenApply(this::bindDictionarySink)),
+                RuleType.BINDING_INSERT_TRINO_CONNECTOR_TABLE.build(
+                        unboundTrinoConnectorTableSink().thenApply(this::bindTrinoConnectorTableSink))
         );
     }
 
@@ -613,13 +619,13 @@ public class BindSink implements AnalysisRuleFactory {
         if (boundSink.getCols().size() != child.getOutput().size()) {
             throw new AnalysisException("insert into cols should be corresponding to the query output");
         }
-        Map<String, NamedExpression> columnToOutput = getJdbcColumnToOutput(bindColumns, child);
+        Map<String, NamedExpression> columnToOutput = getSpecifiedColumnsToOutput(bindColumns, child);
         // We don't need to insert unmentioned columns, only user specified columns
         LogicalProject<?> outputProject = getOutputProjectByCoercion(bindColumns, child, columnToOutput);
         return boundSink.withChildAndUpdateOutput(outputProject);
     }
 
-    private static Map<String, NamedExpression> getJdbcColumnToOutput(
+    private static Map<String, NamedExpression> getSpecifiedColumnsToOutput(
             List<Column> bindColumns, LogicalPlan child) {
         Map<String, NamedExpression> columnToOutput = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
 
@@ -702,6 +708,47 @@ public class BindSink implements AnalysisRuleFactory {
         return columnToOutput;
     }
 
+    private Plan bindTrinoConnectorTableSink(MatchingContext<UnboundTrinoConnectorTableSink<Plan>> ctx) {
+        UnboundTrinoConnectorTableSink<?> sink = ctx.root;
+        Pair<TrinoConnectorExternalDatabase, TrinoConnectorExternalTable> pair = bind(ctx.cascadesContext, sink);
+        TrinoConnectorExternalDatabase database = pair.first;
+        TrinoConnectorExternalTable table = pair.second;
+        LogicalPlan child = ((LogicalPlan) sink.child());
+
+        List<Column> bindColumns;
+        if (sink.getColNames().isEmpty()) {
+            bindColumns = table.getFullSchema().stream().collect(ImmutableList.toImmutableList());
+        } else {
+            bindColumns = sink.getColNames().stream().map(cn -> {
+                Column column = table.getColumn(cn);
+                if (column == null) {
+                    throw new AnalysisException(String.format("column %s is not found in table %s",
+                            cn, table.getName()));
+                }
+                return column;
+            }).collect(ImmutableList.toImmutableList());
+        }
+        LogicalTrinoConnectorTableSink<?> boundSink = new LogicalTrinoConnectorTableSink<>(
+                database,
+                table,
+                bindColumns,
+                child.getOutput().stream()
+                    .map(NamedExpression.class::cast)
+                    .collect(ImmutableList.toImmutableList()),
+                sink.getDMLCommandType(),
+                Optional.empty(),
+                Optional.empty(),
+                child);
+        // we need to insert all the columns of the target table
+        if (boundSink.getCols().size() != child.getOutput().size()) {
+            throw new AnalysisException("insert into cols should be corresponding to the query output");
+        }
+        Map<String, NamedExpression> columnToOutput = getSpecifiedColumnsToOutput(bindColumns, child);
+        // We don't need to insert unmentioned columns, only user specified columns
+        LogicalProject<?> outputProject = getOutputProjectByCoercion(bindColumns, child, columnToOutput);
+        return boundSink.withChildAndUpdateOutput(outputProject);
+    }
+
     private Pair<Database, OlapTable> bind(CascadesContext cascadesContext, UnboundTableSink<? extends Plan> sink) {
         List<String> tableQualifier = RelationUtil.getQualifierName(cascadesContext.getConnectContext(),
                 sink.getNameParts());
@@ -763,6 +810,18 @@ public class BindSink implements AnalysisRuleFactory {
             throw new AnalysisException(e.getMessage());
         }
         return Pair.of(db, dictionary);
+    }
+
+    private Pair<TrinoConnectorExternalDatabase, TrinoConnectorExternalTable> bind(CascadesContext cascadesContext,
+            UnboundTrinoConnectorTableSink<? extends Plan> sink) {
+        List<String> tableQualifier = RelationUtil.getQualifierName(cascadesContext.getConnectContext(),
+                sink.getNameParts());
+        Pair<DatabaseIf<?>, TableIf> pair = RelationUtil.getDbAndTable(tableQualifier,
+                cascadesContext.getConnectContext().getEnv());
+        if (pair.second instanceof TrinoConnectorExternalTable) {
+            return Pair.of(((TrinoConnectorExternalDatabase) pair.first), (TrinoConnectorExternalTable) pair.second);
+        }
+        throw new AnalysisException("the target table of insert into is not a trino connector table");
     }
 
     private List<Long> bindPartitionIds(OlapTable table, List<String> partitions, boolean temp) {
