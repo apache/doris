003f8e6973dccd8e9435b92eae131d699a1f3f77
morrySnow <zhangwenxin@selectdb.com>
2025-04-10 17:10:31 +0800

    [feature](index) update phycical plan def for vector index

--- gensrc/thrift/PlanNodes.thrift
+++ gensrc/thrift/PlanNodes.thrift
@@ -790,6 +790,8 @@ struct TOlapScanNode {
   16: optional list<i32> distribute_column_ids
   17: optional i32 schema_version
   18: optional list<i32> topn_filter_source_node_ids //deprecated, move to TPlanNode.106
+  19: optional TSortInfo ann_sort_info
+  20: optional i64 ann_sort_limit
 }
 
 struct TEqJoinCondition {
