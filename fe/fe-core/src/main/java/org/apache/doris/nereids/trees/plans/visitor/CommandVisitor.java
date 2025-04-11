0dfc4b45256bea68d5b30a14f27a483e1907043f
csding <65964830+codeDing18@users.noreply.github.com>
2025-04-11 11:08:45 +0800

    [Enhancement] (nereids)implement CreateUserCommand in nereids (#47554)
    Issue Number: close
    [#42595](https://github.com/apache/doris/issues/42595)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/visitor/CommandVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/visitor/CommandVisitor.java
@@ -60,6 +60,7 @@ import org.apache.doris.nereids.trees.plans.commands.CreateRoleCommand;
 import org.apache.doris.nereids.trees.plans.commands.CreateSqlBlockRuleCommand;
 import org.apache.doris.nereids.trees.plans.commands.CreateTableCommand;
 import org.apache.doris.nereids.trees.plans.commands.CreateTableLikeCommand;
+import org.apache.doris.nereids.trees.plans.commands.CreateUserCommand;
 import org.apache.doris.nereids.trees.plans.commands.CreateViewCommand;
 import org.apache.doris.nereids.trees.plans.commands.CreateWorkloadGroupCommand;
 import org.apache.doris.nereids.trees.plans.commands.DeleteFromCommand;
@@ -958,5 +959,9 @@ public interface CommandVisitor<R, C> {
 
     default R visitDropAnalyzeJobCommand(DropAnalyzeJobCommand dropAnalyzeJobCommand, C context) {
         return visitCommand(dropAnalyzeJobCommand, context);
+    }
+
+    default R visitCreateUserCommand(CreateUserCommand createUserCommand, C context) {
+        return visitCommand(createUserCommand, context);
     }
 }
