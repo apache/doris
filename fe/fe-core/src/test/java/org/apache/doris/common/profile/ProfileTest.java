apache/doris

commit 92f1e54026c5d8a18ab9b7b28327c068c104d08b

[opt](profile) Refine process of auto_profile for less memory usage (#48653)
### What problem does this PR solve?

In previous, auto_profile_threshold is calculated by background thread
of ProfileManager.
When FE has a high qps, this implementation makes profile in memory
stacking until background thread wakes up.

So, we move the calculation of auto_profile_threshold to
Profile::update_summary.

fe/fe-core/src/main/java/org/apache/doris/common/profile/Profile.java
@@ -105,7 +105,7 @@ public class Profile {
     private long queryFinishTimestamp = Long.MAX_VALUE;
     private Map<Integer, String> planNodeMap = Maps.newHashMap();
     private int profileLevel = MergedProfileLevel;
-    private long autoProfileDurationMs = -1;
+    protected long autoProfileDurationMs = -1;
     // Profile size is the size of profile file
     private long profileSize = 0;
 
@@ -264,7 +264,7 @@ public class Profile {
                 return;
             }
 
-            if (planner instanceof NereidsPlanner) {
+            if (planner != null && planner instanceof NereidsPlanner) {
                 NereidsPlanner nereidsPlanner = ((NereidsPlanner) planner);
                 physicalPlan = nereidsPlanner.getPhysicalPlan();
                 physicalRelations.addAll(nereidsPlanner.getPhysicalRelations());
@@ -282,8 +282,21 @@ public class Profile {
             summaryProfile.update(summaryInfo);
 
             if (isFinished) {
-                this.markQueryFinished(System.currentTimeMillis());
+                this.markQueryFinished();
+                long durationMs = this.queryFinishTimestamp - summaryProfile.getQueryBeginTime();
+                // Duration ls less than autoProfileDuration, remove it from memory.
+                long durationThreshold = executionProfiles.isEmpty()
+                                    ? autoProfileDurationMs : executionProfiles.size() * autoProfileDurationMs;
+                if (this.queryFinishTimestamp != Long.MAX_VALUE && durationMs < durationThreshold) {
+                    ProfileManager.getInstance().removeProfile(this.getId());
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Removed profile {} because it's costs {} is less than {}", this.getId(),
+                                durationMs, autoProfileDurationMs * this.executionProfiles.size());
+                    }
+                    return;
+                }
             }
+
             // Nereids native insert not set planner, so it is null
             if (planner != null) {
                 this.planNodeMap = planner.getExplainStringMap();
@@ -432,40 +445,6 @@ public class Profile {
             return false;
         }
 
-        // below is the case where query has finished
-        boolean hasReportingProfile = false;
-
-        if (this.executionProfiles.isEmpty()) {
-            // Query finished, but no execution profile.
-            // 1. Query is executed on FE.
-            // 2. Not a SELECT query, just a DDL.
-            return false;
-        }
-
-        for (ExecutionProfile executionProfile : executionProfiles) {
-            if (!executionProfile.isCompleted()) {
-                hasReportingProfile = true;
-                break;
-            }
-        }
-
-        if (!hasReportingProfile) {
-            // query finished and no flying profile
-            // I do want to use TotalTime in summary profile, but it is an encoded string,
-            // it is hard to write a parse function.
-            long durationMs = this.queryFinishTimestamp - summaryProfile.getQueryBeginTime();
-            // time cost of this query is large enough.
-            if (this.queryFinishTimestamp != Long.MAX_VALUE && durationMs
-                    > (this.executionProfiles.size() * autoProfileDurationMs)) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Query/LoadJob {} costs {} ms, begin {} finish {}, need store its profile",
-                            getId(), durationMs, summaryProfile.getQueryBeginTime(), this.queryFinishTimestamp);
-                }
-                return true;
-            }
-            return false;
-        }
-
         if (this.queryFinishTimestamp == Long.MAX_VALUE) {
             LOG.warn("Logical error, query {} has finished, but queryFinishTimestamp is not set,", getId());
             return false;
@@ -473,8 +452,8 @@ public class Profile {
 
         long currentTimeMillis = System.currentTimeMillis();
         if (this.queryFinishTimestamp != Long.MAX_VALUE
-                    && (currentTimeMillis - this.queryFinishTimestamp)
-                        > Config.profile_waiting_time_for_spill_seconds * 1000) {
+                && (currentTimeMillis - this.queryFinishTimestamp)
+                > Config.profile_waiting_time_for_spill_seconds * 1000) {
             LOG.warn("Profile {} should be stored to storage without waiting for incoming profile,"
                     + " since it has been waiting for {} ms, current time {} query finished time: {}",
                     getId(), currentTimeMillis - this.queryFinishTimestamp, currentTimeMillis,
@@ -500,7 +479,7 @@ public class Profile {
     }
 
     // Profile IO threads races with Coordinator threads.
-    public void markQueryFinished(long queryFinishTime) {
+    public void markQueryFinished() {
         try {
             if (this.profileHasBeenStored()) {
                 LOG.error("Logical error, profile {} has already been stored to storage", getId());

fe/fe-core/src/main/java/org/apache/doris/common/profile/ProfileManager.java
@@ -204,18 +204,22 @@ public class ProfileManager extends MasterDaemon {
             return;
         }
 
-        ProfileElement element = createElement(profile);
-        // 'insert into' does have job_id, put all profiles key with query_id
-        String key = profile.getSummaryProfile().getProfileId();
-        // check when push in, which can ensure every element in the list has QUERY_ID column,
-        // so there is no need to check when remove element from list.
-        if (Strings.isNullOrEmpty(key)) {
-            LOG.warn("the key or value of Map is null, "
-                    + "may be forget to insert 'QUERY_ID' or 'JOB_ID' column into infoStrings");
-        }
-
         writeLock.lock();
         try {
+            if (!queryIdToProfileMap.containsKey(profile.getId())) {
+                deleteOutdatedProfilesFromMemory(1);
+            }
+
+            ProfileElement element = createElement(profile);
+            // 'insert into' does have job_id, put all profiles key with query_id
+            String key = profile.getSummaryProfile().getProfileId();
+            // check when push in, which can ensure every element in the list has QUERY_ID column,
+            // so there is no need to check when remove element from list.
+            if (Strings.isNullOrEmpty(key)) {
+                LOG.warn("the key or value of Map is null, "
+                        + "may be forget to insert 'QUERY_ID' or 'JOB_ID' column into infoStrings");
+            }
+
             // a profile may be updated multiple times in queryIdToProfileMap,
             // and only needs to be inserted into the queryIdDeque for the first time.
             queryIdToProfileMap.put(key, element);
@@ -475,7 +479,6 @@ public class ProfileManager extends MasterDaemon {
         loadProfilesFromStorageIfFirstTime(false);
         writeProfileToStorage();
         deleteBrokenProfiles();
-        deleteOutdatedProfilesFromMemory();
         deleteOutdatedProfilesFromStorage();
         preventExecutionProfileLeakage();
     }
@@ -954,36 +957,12 @@ public class ProfileManager extends MasterDaemon {
         }
     }
 
-    protected void deleteOutdatedProfilesFromMemory() {
+    protected void deleteOutdatedProfilesFromMemory(int numOfNewProfiles) {
         StringBuilder stringBuilder = new StringBuilder();
-        StringBuilder stringBuilderTTL = new StringBuilder();
         writeLock.lock();
 
         try {
-            // Remove profiles that costs less than auto_profile_threshold_ms
-            List<String> profilesToRemove = Lists.newArrayList();
-
-            for (ProfileElement profileElement : this.queryIdToProfileMap.values()) {
-                if (profileElement.profile.shouldBeRemoveFromMemory()) {
-                    String profileId = profileElement.profile.getSummaryProfile().getProfileId();
-                    profilesToRemove.add(profileId);
-                    stringBuilder.append(profileId).append(",");
-                    if (LOG.isDebugEnabled()) {
-                        LOG.debug("Profile {} should be filtered from memory, information {}", profileId,
-                                profileElement.profile.debugInfo());
-                    }
-                }
-            }
-
-            for (String profileId : profilesToRemove) {
-                ProfileElement profileElement = queryIdToProfileMap.get(profileId);
-                queryIdToProfileMap.remove(profileId);
-                for (ExecutionProfile executionProfile : profileElement.profile.getExecutionProfiles()) {
-                    queryIdToExecutionProfiles.remove(executionProfile.getQueryId());
-                }
-            }
-
-            if (this.queryIdToProfileMap.size() <= Config.max_query_profile_num) {
+            if (this.queryIdToProfileMap.size() + numOfNewProfiles <= Config.max_query_profile_num) {
                 return;
             }
 
@@ -992,10 +971,10 @@ public class ProfileManager extends MasterDaemon {
             // query finished time of unfinished query is INT_MAX, so they will be on the bottom of the heap.
             PriorityQueue<ProfileElement> queueIdDeque = getProfileOrderByQueryFinishTime();
 
-            while (queueIdDeque.size() > Config.max_query_profile_num && !queueIdDeque.isEmpty()) {
+            while (queueIdDeque.size() + numOfNewProfiles > Config.max_query_profile_num && !queueIdDeque.isEmpty()) {
                 ProfileElement profileElement = queueIdDeque.poll();
                 String profileId = profileElement.profile.getSummaryProfile().getProfileId();
-                stringBuilderTTL.append(profileId).append(",");
+                stringBuilder.append(profileId).append(",");
                 queryIdToProfileMap.remove(profileId);
                 for (ExecutionProfile executionProfile : profileElement.profile.getExecutionProfiles()) {
                     queryIdToExecutionProfiles.remove(executionProfile.getQueryId());
@@ -1010,10 +989,9 @@ public class ProfileManager extends MasterDaemon {
             int profileNum = queryIdToProfileMap.size();
             writeLock.unlock();
 
-            if (stringBuilder.length() != 0 || stringBuilderTTL.length() != 0) {
-                LOG.info("Filtered profiles {}, outdated profiles {}, they are removed from memory,"
-                                + " current profile map size {}",
-                        stringBuilder.toString(), stringBuilderTTL.toString(), profileNum);
+            if (stringBuilder.length() != 0) {
+                LOG.info("Outdated profiles {}, they are removed from memory, current profile map size {}",
+                        stringBuilder.toString(), profileNum);
             }
         }
     }
@@ -1062,6 +1040,20 @@ public class ProfileManager extends MasterDaemon {
             return isProfileLoaded;
         } finally {
             isProfileLoadedLock.readLock().unlock();
+        }
+    }
+
+    public void removeProfile(String profileId) {
+        writeLock.lock();
+        try {
+            ProfileElement profileToRemove = this.queryIdToProfileMap.remove(profileId);
+            if (profileToRemove != null) {
+                for (ExecutionProfile executionProfile : profileToRemove.profile.getExecutionProfiles()) {
+                    queryIdToExecutionProfiles.remove(executionProfile.getQueryId());
+                }
+            }
+        } finally {
+            writeLock.unlock();
         }
     }
 }

fe/fe-core/src/main/java/org/apache/doris/common/profile/RuntimeProfile.java
@@ -172,10 +172,6 @@ public class RuntimeProfile {
         return this.nodeid;
     }
 
-    public Boolean sinkOperator() {
-        return this.isSinkOperator;
-    }
-
     public Map<String, Counter> getCounterMap() {
         return counterMap;
     }
@@ -196,9 +192,7 @@ public class RuntimeProfile {
         return childCounterMap;
     }
 
-    public double getLocalTimePercent() {
-        return localTimePercent;
-    }
+
 
     public Counter addCounter(String name, TUnit type, String parentCounterName) {
         counterLock.writeLock().lock();

fe/fe-core/src/main/java/org/apache/doris/qe/QeProcessorImpl.java
@@ -75,8 +75,12 @@ public final class QeProcessorImpl implements QeProcessor {
     private Status processQueryProfile(TQueryProfile profile, TNetworkAddress address, boolean isDone) {
         ExecutionProfile executionProfile = ProfileManager.getInstance().getExecutionProfile(profile.query_id);
         if (executionProfile == null) {
-            LOG.warn("Could not find execution profile, query {} be {}",
-                                DebugUtil.printId(profile.query_id), address.toString());
+            // When auto_profile_threshold_ms is not -1, this branch will be very common.
+            // So this log is set to debug level.
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Could not find execution profile, query {} be {}",
+                        DebugUtil.printId(profile.query_id), address.toString());
+            }
             return new Status(TStatusCode.NOT_FOUND, "Could not find execution profile with query id "
                     + DebugUtil.printId(profile.query_id));
         }

commit 328f98b593f57b117254010a4eb3793888628a77

[fix](profile) Fix incorrect check of Profile.shouldStoreToStorage (#49925)
### What problem does this PR solve?

If resource is not busy, we can still see error log like
```
Profile 86e0aa6fa3e445ed-86cdc765118dbf3e should be stored to storage without waiting for incoming profile, since it has been waiting for 10140 ms, current time 1741661595669 query finished time: 1741661585529
```
This pr fix the problem by:

If profile collection is finished, we should write it to storage
immediately.

Add unit test for class Profile.

Bug is introduced by https://github.com/apache/doris/pull/48653

Issue Number: close #xxx

Related PR: https://github.com/apache/doris/pull/48653

Problem Summary:

### Release note

None

### Check List (For Author)

- Test <!-- At least one of them must be included. -->
    - [ ] Regression test
    - [ ] Unit Test
    - [ ] Manual test (add detailed scripts or steps below)
    - [ ] No need to test or manual test. Explain why:
- [ ] This is a refactor/code format and no logic has been changed.
        - [ ] Previous test can cover this change.
        - [ ] No code files have been changed.
        - [ ] Other reason <!-- Add your reason?  -->

- Behavior changed:
    - [ ] No.
    - [ ] Yes. <!-- Explain the behavior change -->

- Does this need documentation?
    - [ ] No.
- [ ] Yes. <!-- Add document PR link here. eg:
https://github.com/apache/doris-website/pull/1214 -->

### Check List (For Reviewer who merge this PR)

- [x] Confirm the release note
- [x] Confirm test cases
- [x] Confirm document
- [x] Add branch pick label <!-- Add branch pick label that this PR
should merge into -->

fe/fe-core/src/main/java/org/apache/doris/common/profile/Profile.java
@@ -450,22 +450,35 @@ public class Profile {
             return false;
         }
 
-        long currentTimeMillis = System.currentTimeMillis();
-        if (this.queryFinishTimestamp != Long.MAX_VALUE
-                && (currentTimeMillis - this.queryFinishTimestamp)
-                > Config.profile_waiting_time_for_spill_seconds * 1000) {
-            LOG.warn("Profile {} should be stored to storage without waiting for incoming profile,"
-                    + " since it has been waiting for {} ms, current time {} query finished time: {}",
-                    getId(), currentTimeMillis - this.queryFinishTimestamp, currentTimeMillis,
-                    this.queryFinishTimestamp);
+        // below is the case where query has finished
+        boolean hasReportingProfile = false;
 
-            this.summaryProfile.setSystemMessage(
-                            "This profile is not complete, since its collection does not finish in time."
-                            + " Maybe increase profile_waiting_time_for_spill_secs in fe.conf current val: "
-                            + String.valueOf(Config.profile_waiting_time_for_spill_seconds));
-            return true;
+        for (ExecutionProfile executionProfile : executionProfiles) {
+            if (!executionProfile.isCompleted()) {
+                hasReportingProfile = true;
+                break;
+            }
         }
 
+        if (!hasReportingProfile) {
+            return true;
+        } else {
+            long currentTimeMillis = System.currentTimeMillis();
+            if (this.queryFinishTimestamp != Long.MAX_VALUE
+                    && (currentTimeMillis - this.queryFinishTimestamp)
+                    > Config.profile_waiting_time_for_spill_seconds * 1000) {
+                LOG.warn("Profile {} should be stored to storage without waiting for incoming profile,"
+                        + " since it has been waiting for {} ms, current time {} query finished time: {}",
+                        getId(), currentTimeMillis - this.queryFinishTimestamp, currentTimeMillis,
+                        this.queryFinishTimestamp);
+
+                this.summaryProfile.setSystemMessage(
+                                "This profile is not complete, since its collection does not finish in time."
+                                + " Maybe increase profile_waiting_time_for_spill_secs in fe.conf current val: "
+                                + String.valueOf(Config.profile_waiting_time_for_spill_seconds));
+                return true;
+            }
+        }
         // query finished, wait a while for reporting profile
         return false;
     }

fe/fe-core/src/test/java/org/apache/doris/common/profile/ProfileTest.java
@@ -1,0 +1,263 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.common.profile;
+
+import org.apache.doris.common.Config;
+import org.apache.doris.common.util.DebugUtil;
+import org.apache.doris.thrift.TUniqueId;
+
+import mockit.Expectations;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ProfileTest {
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    private Profile profile;
+    private File tempDir;
+    private ExecutionProfile executionProfile;
+    private String testProfileStoragePath;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        profile = ProfilePersistentTest.constructRandomProfile(1);
+        // Setup a temporary directory for profile storage
+        tempDir = Files.createTempDirectory("profile_test_").toFile();
+        testProfileStoragePath = tempDir.getAbsolutePath();
+        executionProfile = profile.getExecutionProfiles().get(0);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        ProfileManager.getInstance().removeProfile(profile.getId());
+    }
+
+    @Test
+    public void testBasicProfileCreation() {
+        Assertions.assertNotNull(profile);
+        Assertions.assertFalse(profile.isQueryFinished);
+        Assertions.assertEquals(1, profile.getExecutionProfiles().size());
+    }
+
+    @Test
+    public void testUpdateSummary() {
+        Map<String, String> summaryInfo = new HashMap<>();
+        summaryInfo.put("TestKey", "TestValue");
+
+        profile.updateSummary(summaryInfo, false, null);
+
+        Assertions.assertFalse(profile.isQueryFinished);
+
+        profile.updateSummary(summaryInfo, true, null);
+        Assertions.assertTrue(profile.isQueryFinished);
+        Assertions.assertTrue(Long.MAX_VALUE != profile.getQueryFinishTimestamp());
+    }
+
+    @Test
+    public void testShouldStoreToStorage() {
+        // Initially not finished, should not store
+        Assertions.assertFalse(profile.shouldStoreToStorage());
+
+        // Mark as finished
+        profile.markQueryFinished();
+
+        // Execution profile is not completed yet
+        Assertions.assertFalse(executionProfile.isCompleted());
+
+        // Should still not store because execution profile isn't complete
+        // and time hasn't passed the threshold
+        Assertions.assertFalse(profile.shouldStoreToStorage());
+
+
+        new Expectations(executionProfile) {
+            {
+                executionProfile.isCompleted();
+                result = true;
+            }
+        };
+        // Now it should be ready to store
+        Assertions.assertTrue(profile.shouldStoreToStorage());
+    }
+
+    @Test
+    public void testWriteToStorage() {
+        // Prepare for storage
+        profile.markQueryFinished();
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+        new Expectations(executionProfile) {
+            {
+                executionProfile.isCompleted();
+                result = true;
+            }
+        };
+
+        // Should be true before we write
+        Assertions.assertTrue(profile.shouldStoreToStorage());
+        Assertions.assertFalse(profile.profileHasBeenStored());
+
+        // Write to storage
+        profile.writeToStorage(testProfileStoragePath);
+
+        // Verify it's stored
+        Assertions.assertTrue(profile.profileHasBeenStored());
+        Assertions.assertNotNull(profile.getProfileStoragePath());
+        Assertions.assertTrue(new File(profile.getProfileStoragePath()).exists());
+        Assertions.assertTrue(profile.getProfileSize() > 0);
+    }
+
+    @Test
+    public void testWriteToStorageWithIncompletedExecution() {
+        // Prepare for storage
+        profile.markQueryFinished();
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+
+        // Mock that execution profile is not completed
+        new Expectations(executionProfile) {
+            {
+                executionProfile.isCompleted();
+                result = false;
+            }
+        };
+
+        // Should be false before we write because execution profile isn't complete
+        Assertions.assertFalse(profile.shouldStoreToStorage());
+        Assertions.assertFalse(profile.profileHasBeenStored());
+
+        // Sleep to simulate time passing
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        int orig = Config.profile_waiting_time_for_spill_seconds;
+        Config.profile_waiting_time_for_spill_seconds = 1;
+        Assertions.assertTrue(profile.shouldStoreToStorage());
+        Config.profile_waiting_time_for_spill_seconds = orig;
+    }
+
+    @Test
+    public void testReadFromStorage() throws IOException {
+        profile.markQueryFinished();
+        // First write to storage
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+        profile.writeToStorage(testProfileStoragePath);
+
+        // Now read it back
+        Profile readProfile = Profile.read(profile.getProfileStoragePath());
+
+        // Verify read profile
+        Assertions.assertNotNull(readProfile);
+        Assertions.assertEquals(profile.getId(), readProfile.getId());
+        Assertions.assertTrue(readProfile.isQueryFinished);
+        Assertions.assertTrue(readProfile.profileHasBeenStored());
+    }
+
+    @Test
+    public void testDeleteFromStorage() throws IOException {
+        // First write to storage
+        profile.markQueryFinished();
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+        profile.writeToStorage(testProfileStoragePath);
+
+        String storagePath = profile.getProfileStoragePath();
+        Assertions.assertTrue(new File(storagePath).exists());
+
+        // Now delete it
+        profile.deleteFromStorage();
+
+        // Verify it's gone
+        Assertions.assertFalse(new File(storagePath).exists());
+    }
+
+    @Test
+    public void testCreateProfileFileInputStream() throws IOException {
+        // First write to storage
+        profile.markQueryFinished();
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+        profile.writeToStorage(testProfileStoragePath);
+
+        // Test with valid path
+        FileInputStream fis = Profile.createPorfileFileInputStream(profile.getProfileStoragePath());
+        Assertions.assertNotNull(fis);
+        fis.close();
+
+        // Test with invalid path
+        FileInputStream invalidFis = Profile.createPorfileFileInputStream("/invalid/path/to/profile.zip");
+        Assertions.assertNull(invalidFis);
+    }
+
+    @Test
+    public void testParseProfileFileName() {
+        // Valid profile name
+        long timestamp = System.currentTimeMillis();
+        UUID taskId = UUID.randomUUID();
+        TUniqueId queryId = new TUniqueId(taskId.getMostSignificantBits(), taskId.getLeastSignificantBits());
+        String id = DebugUtil.printId(queryId);
+        String validName = timestamp + "_" + id + ".zip";
+
+        String[] parts = Profile.parseProfileFileName(validName);
+        Assertions.assertNotNull(parts);
+        Assertions.assertEquals(2, parts.length);
+        Assertions.assertEquals(String.valueOf(timestamp), parts[0]);
+        Assertions.assertEquals(id, parts[1]);
+
+        // Invalid profile name
+        String invalidName = "not_a_valid_profile_name";
+        Assertions.assertNull(Profile.parseProfileFileName(invalidName));
+
+        // Wrong extension
+        String wrongExtension = timestamp + "_" + id + ".txt";
+        Assertions.assertNull(Profile.parseProfileFileName(wrongExtension));
+    }
+
+    @Test
+    public void testGetOnStorageProfile() throws IOException {
+        // First write to storage
+        profile.markQueryFinished();
+        profile.setQueryFinishTimestamp(System.currentTimeMillis());
+        profile.writeToStorage(testProfileStoragePath);
+        profile.releaseMemory();
+        StringBuilder builder = new StringBuilder();
+        profile.getOnStorageProfile(builder);
+
+        // Verify we got content
+        Assertions.assertTrue(builder.length() > 0);
+    }
+
+    @Test
+    public void testReleaseMemory() {
+        Assertions.assertEquals(1, profile.getExecutionProfiles().size());
+        profile.setChangedSessionVar("test=1");
+
+        profile.releaseMemory();
+
+        Assertions.assertEquals(0, profile.getExecutionProfiles().size());
+    }
+}