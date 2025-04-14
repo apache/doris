0ada53c7c6802778c70806270517e4382c2b7a8b
csun5285 <sunchenyang@selectdb.com>
2025-04-14 18:41:52 +0800

    1

---
+++ regression-test/suites/variant_p0/predefine/test_predefine_type_index.groovy
@@ -0,0 +1,46 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_variant_predefine_index_type", "p0"){ 
+    sql """ set describe_extend_variant_column = true """
+    sql """ set enable_match_without_inverted_index = false """
+    sql """ set enable_common_expr_pushdown = true """
+
+    def tableName = "test_variant_predefine_index_type"
+    sql "DROP TABLE IF EXISTS ${tableName}"
+    sql """CREATE TABLE ${tableName} (
+        `id` bigint NULL,
+        `var` variant <
+            MATCH_NAME 'path.int' : int,
+            MATCH_NAME 'path.decimal' : decimal(5, 5)
+        > NOT NULL,
+        INDEX idx_a_b (var) USING INVERTED PROPERTIES("field_pattern"="path.int", "parser"="unicode", "support_phrase" = "true") COMMENT '',
+        INDEX idx_a_c (var) USING INVERTED PROPERTIES("field_pattern"="path.decimal") COMMENT ''
+    ) ENGINE=OLAP DUPLICATE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1 PROPERTIES ( "replication_allocation" = "tag.location.default: 1", "disable_auto_compaction" = "true", "variant_max_subcolumns_count" = "0")"""
+
+    sql """insert into ${tableName} values(1, '{"path" : {"int" : 123, "decimal" : 123.12345}}')"""
+    sql """insert into ${tableName} values(2, '{"path" : {"int" : 456, "decimal" : 456.45678}}')"""
+    sql """insert into ${tableName} values(3, '{"path" : {"int" : 789, "decimal" : 789.78912}}')"""
+    sql """insert into ${tableName} values(4, '{"path" : {"int" : 100, "decimal" : 100.10012}}')"""
+    sql """insert into ${tableName} values(5, '{"path" : {"int" : 111, "decimal" : 111.11111}}')"""
+    
+    qt_sql """ desc ${tableName} """
+    qt_sql """select * from ${tableName} order by id"""
+    
+    qt_sql """ select count() from ${tableName} where cast(var['path']['int'] as int) = 789 """
+    qt_sql """ select count() from ${tableName} where cast(var['path']['decimal'] as decimal) = 789.78912 """
+}
