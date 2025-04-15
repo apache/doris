5401d7cffcab8fd157ac4c6d6e0c61c84730faeb
宋光璠 <songguangfan@sf.com>
2025-04-15 16:28:22 +0800

    add regress test http skip line

---
+++ regression-test/suites/load_p0/stream_load/test_stream_load_illegal_skip_lines.groovy
@@ -0,0 +1,48 @@
+suite("test_stream_load_illegal_skip_lines", "p0") {
+    def tableName = "test_stream_load_illegal_skip_lines"
+
+    def be_num = sql "show backends;"
+    if (be_num.size() > 1) {
+        // not suitable for multiple be cluster.
+        return
+    }
+
+    sql "DROP TABLE IF EXISTS ${tableName}"
+    sql """
+        CREATE TABLE IF NOT EXISTS ${tableName} (
+            `k1` bigint(20) NULL,
+            `k2` bigint(20) NULL,
+            `v1` tinyint(4) SUM NULL,
+            `v2` tinyint(4) REPLACE NULL,
+            `v10` char(10) REPLACE_IF_NOT_NULL NULL,
+            `v11` varchar(6) REPLACE_IF_NOT_NULL NULL
+        ) ENGINE=OLAP
+        DISTRIBUTED BY HASH(`k1`, `k2`) BUCKETS 3
+        PROPERTIES ("replication_allocation" = "tag.location.default: 1");
+    """
+
+    streamLoad {
+        table "${tableName}"
+        set 'column_separator', '\t'
+        set 'label', 'test_stream_load_illegal_skip_lines'
+        set 'columns', 'k1, k2, v2, v10, v11'
+        set 'strict_mode','true'
+
+        file 'large_test_file.csv'
+        set 'HTTP_SKIP_LINES', 'abc'  // 非法参数
+
+        check { result, exception, startTime, endTime ->
+            if (exception != null) {
+                throw exception
+            }
+
+            log.info("Stream load result: ${result}".toString())
+            def json = parseJson(result)
+
+            assertEquals("fail", json.Status.toLowerCase())
+            assertTrue(json.Message.toLowerCase().contains("skip_lines") ||
+                       json.Message.toLowerCase().contains("invalid") ||
+                       json.Message.toLowerCase().contains("out of range"))
+        }
+    }
+}
