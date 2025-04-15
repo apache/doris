7882926d02957495cc4502459aceebfaf7769dec
lichi <lichi@selectdb.com>
2025-04-15 10:30:23 +0800

    remove unused code

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateTableInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/CreateTableInfo.java
@@ -59,7 +59,6 @@ import org.apache.doris.nereids.glue.translator.PlanTranslatorContext;
 import org.apache.doris.nereids.properties.PhysicalProperties;
 import org.apache.doris.nereids.rules.analysis.ExpressionAnalyzer;
 import org.apache.doris.nereids.rules.expression.ExpressionRewriteContext;
-import org.apache.doris.nereids.trees.expressions.Cast;
 import org.apache.doris.nereids.trees.expressions.Expression;
 import org.apache.doris.nereids.trees.expressions.Slot;
 import org.apache.doris.nereids.trees.expressions.SlotReference;
@@ -73,7 +72,6 @@ import org.apache.doris.nereids.trees.expressions.functions.scalar.ScalarFunctio
 import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionRewriter;
 import org.apache.doris.nereids.trees.plans.logical.LogicalEmptyRelation;
 import org.apache.doris.nereids.types.DataType;
-import org.apache.doris.nereids.types.coercion.CharacterType;
 import org.apache.doris.nereids.util.TypeCoercionUtils;
 import org.apache.doris.nereids.util.Utils;
 import org.apache.doris.qe.ConnectContext;
@@ -1018,28 +1016,19 @@ public class CreateTableInfo {
             Expression boundSlotExpression = SlotReplacer.INSTANCE.replace(parsedExpression, columnToSlotReference);
             Scope scope = new Scope(slots);
             ExpressionAnalyzer analyzer = new ExpressionAnalyzer(null, scope, cascadesContext, false, false);
-            Expression expression;
+            Expression expr;
             try {
-                expression = analyzer.analyze(boundSlotExpression, new ExpressionRewriteContext(cascadesContext));
+                expr = analyzer.analyze(boundSlotExpression, new ExpressionRewriteContext(cascadesContext));
             } catch (AnalysisException e) {
                 throw new AnalysisException("In generated column '" + column.getName() + "', "
                         + Utils.convertFirstChar(e.getMessage()));
             }
-            checkExpressionInGeneratedColumn(expression, column, nameToColumnDefinition);
-            TypeCoercionUtils.checkCanCastTo(expression.getDataType(), column.getType());
+            checkExpressionInGeneratedColumn(expr, column, nameToColumnDefinition);
+            TypeCoercionUtils.checkCanCastTo(expr.getDataType(), column.getType());
             ExpressionToExpr translator = new ExpressionToExpr(i, translateMap);
-            Expr expr = expression.accept(translator, planTranslatorContext);
-            info.get().setExpr(expr);
-            // Casting slot to its own type is because when loading data(stream load and other load),
-            // the slots reading from files are string type. So we need to cast it to its own type to avoid error.
-            Expression expressionForLoad = expression.rewriteDownShortCircuit(e -> {
-                if (e instanceof SlotReference && !(e.getDataType() instanceof CharacterType)) {
-                    return new Cast(e, e.getDataType());
-                }
-                return e;
-            });
-            Expr exprForLoad = expressionForLoad.accept(translator, planTranslatorContext);
-            exprAndnames.add(new GeneratedColumnUtil.ExprAndname(exprForLoad.clone(), column.getName()));
+            Expr e = expr.accept(translator, planTranslatorContext);
+            info.get().setExpr(e);
+            exprAndnames.add(new GeneratedColumnUtil.ExprAndname(e.clone(), column.getName()));
         }
 
         // for alter drop column
