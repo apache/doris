wangbo/incubator-doris

bind workload group to compute group

fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
@@ -1361,6 +1361,10 @@ public class ConnectContext {
         }
     }
 
+    public Set<String> getComputeGroupNames() {
+        return getComputeGroupSafely().getNames();
+    }
+
     public ComputeGroup getComputeGroup() throws UserException {
         ComputeGroupMgr cgMgr = Env.getCurrentEnv().getComputeGroupMgr();
         if (Config.isCloudMode()) {

fe/fe-core/src/main/java/org/apache/doris/resource/computegroup/ComputeGroup.java
@@ -20,6 +20,7 @@ package org.apache.doris.resource.computegroup;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.SystemInfoService;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -66,6 +67,10 @@ public class ComputeGroup {
 
     public String getName() {
         return name;
+    }
+
+    public Set<String> getNames() {
+        return Sets.newHashSet(name);
     }
 
     public boolean containsBackend(String backendTag) {

apache/doris

fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
@@ -1361,6 +1361,10 @@ public class ConnectContext {
         }
     }
 
+    public Set<String> getComputeGroupNames() {
+        return getComputeGroupSafely().getIdentifiers();
+    }
+
     public ComputeGroup getComputeGroup() throws UserException {
         ComputeGroupMgr cgMgr = Env.getCurrentEnv().getComputeGroupMgr();
         if (Config.isCloudMode()) {

fe/fe-core/src/main/java/org/apache/doris/resource/computegroup/ComputeGroup.java
@@ -20,6 +20,7 @@ package org.apache.doris.resource.computegroup;
 import org.apache.doris.system.Backend;
 import org.apache.doris.system.SystemInfoService;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -66,6 +67,10 @@ public class ComputeGroup {
 
     public String getName() {
         return name;
+    }
+
+    public Set<String> getIdentifiers() {
+        return Sets.newHashSet(name);
     }
 
     public boolean containsBackend(String backendTag) {