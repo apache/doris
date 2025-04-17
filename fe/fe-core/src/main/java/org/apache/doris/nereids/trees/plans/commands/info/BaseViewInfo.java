6e174d4adff88491e0c22b17893fd211649fb1b9
feiniaofeiafei <moailing@selectdb.com>
2025-04-17 17:16:38 +0800

    remove nullable change

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/BaseViewInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/info/BaseViewInfo.java
@@ -167,8 +167,7 @@ public class BaseViewInfo {
             for (Slot output : outputs) {
                 DataType dataType = TypeCoercionUtils.replaceSpecifiedType(output.getDataType(), NullType.class,
                         TinyIntType.INSTANCE);
-                Column column = new Column(output.getName(), dataType.toCatalogDataType(),
-                        !output.isColumnFromTable() || output.nullable());
+                Column column = new Column(output.getName(), dataType.toCatalogDataType(), output.nullable());
                 finalCols.add(column);
             }
         } else {
@@ -180,7 +179,7 @@ public class BaseViewInfo {
                 DataType dataType = TypeCoercionUtils.replaceSpecifiedType(output.getDataType(), NullType.class,
                         TinyIntType.INSTANCE);
                 Column column = new Column(simpleColumnDefinitions.get(i).getName(),
-                        dataType.toCatalogDataType(), !output.isColumnFromTable() || output.nullable());
+                        dataType.toCatalogDataType(), output.nullable());
                 column.setComment(simpleColumnDefinitions.get(i).getComment());
                 finalCols.add(column);
             }
