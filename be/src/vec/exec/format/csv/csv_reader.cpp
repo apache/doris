187599b17e0d233f4e100a23b2cef5577d45f7a6
daidai <changyuwei@selectdb.com>
2025-04-17 11:29:43 +0800

    fix modify const

--- be/src/vec/exec/format/csv/csv_reader.cpp
+++ be/src/vec/exec/format/csv/csv_reader.cpp
@@ -64,7 +64,6 @@ enum class FileCachePolicy : uint8_t;
 
 namespace doris::vectorized {
 #include "common/compile_check_begin.h"
-const static Slice _s_null_slice = Slice("\\N");
 
 void EncloseCsvTextFieldSplitter::do_split(const Slice& line, std::vector<Slice>* splitted_values) {
     const char* data = line.data;
@@ -651,7 +650,7 @@ Status CsvReader::_fill_dest_columns(const Slice& line, Block* block,
         int col_idx = _col_idxs[i];
         // col idx is out of range, fill with null.
         const Slice& value =
-                col_idx < _split_values.size() ? _split_values[col_idx] : _s_null_slice;
+                col_idx < _split_values.size() ? _split_values[col_idx] : Slice{_options.null_format, _options.null_len};
         Slice slice {value.data, value.size};
 
         IColumn* col_ptr = columns[i].get();
