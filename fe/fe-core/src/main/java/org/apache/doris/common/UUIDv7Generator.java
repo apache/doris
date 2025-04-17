e6d49d3873247ad38f2dcf4af2c6b19dd5195e61
felixwluo <felixwluo@gmail.com>
2025-04-17 13:56:37 +0800

    [feat] Use UUIDv7 for queryId

---
+++ fe/fe-core/src/main/java/org/apache/doris/common/UUIDv7Generator.java
@@ -0,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.common;
+
+// Format
+// 0                   1                   2                   3
+// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                           unix_ts_ms                          |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |          unix_ts_ms           |  ver  |       rand_a          |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |var|                        rand_b                             |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                            rand_b                             |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UUIDv7Generator {
+    private static final UUIDv7Generator INSTANCE = new UUIDv7Generator();
+    private static final SecureRandom RANDOM = new SecureRandom();
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);
+    private static final int VERSION = 7;
+    private static final int VARIANT = 2;
+
+    private UUIDv7Generator() {}
+
+    public static UUIDv7Generator getInstance() {
+        return INSTANCE;
+    }
+
+    public UUID nextUUID() {
+        long timestamp = Instant.now().toEpochMilli();
+
+        int counter = COUNTER.getAndIncrement() & 0xFF;
+
+        long random = RANDOM.nextLong();
+
+        long msb = (timestamp << 16) | (VERSION << 12) | counter;
+
+        long lsb = ((long) VARIANT << 62) | (random & 0x3FFFFFFFFFFFFFFFL);
+
+        return new UUID(msb, lsb);
+    }
+}
