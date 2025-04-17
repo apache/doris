d192a2b269748ab2994b2054ccfba1a7df800e93
zzzxl1993 <yangsiyu@selectdb.com>
2025-04-17 18:14:01 +0800

    [feture](inverted index) add custom analyzer

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -157,6 +157,7 @@ import org.apache.doris.ha.MasterInfo;
 import org.apache.doris.httpv2.entity.ResponseBody;
 import org.apache.doris.httpv2.meta.MetaBaseAction;
 import org.apache.doris.httpv2.rest.RestApiStatusCode;
+import org.apache.doris.indexpolicy.IndexPolicyMgr;
 import org.apache.doris.insertoverwrite.InsertOverwriteManager;
 import org.apache.doris.job.base.AbstractJob;
 import org.apache.doris.job.extensions.mtmv.MTMVTask;
@@ -541,6 +542,8 @@ public class Env {
 
     private PolicyMgr policyMgr;
 
+    private IndexPolicyMgr indexPolicyMgr;
+
     private AnalysisManager analysisManager;
 
     private HboPlanStatisticsManager hboPlanStatisticsManager;
@@ -834,6 +837,7 @@ public class Env {
         this.auditEventProcessor = new AuditEventProcessor(this.pluginMgr);
         this.refreshManager = new RefreshManager();
         this.policyMgr = new PolicyMgr();
+        this.indexPolicyMgr = new IndexPolicyMgr();
         this.extMetaCacheMgr = new ExternalMetaCacheMgr(isCheckpointCatalog);
         this.analysisManager = new AnalysisManager();
         this.hboPlanStatisticsManager = new HboPlanStatisticsManager();
@@ -2508,6 +2512,12 @@ public class Env {
         return checksum;
     }
 
+    public long loadIndexPolicy(DataInputStream in, long checksum) throws IOException {
+        indexPolicyMgr = IndexPolicyMgr.read(in);
+        LOG.info("finished replay index policy from image");
+        return checksum;
+    }
+
     /**
      * Load catalogs through file.
      **/
@@ -2807,6 +2817,11 @@ public class Env {
 
     public long savePolicy(CountingDataOutputStream out, long checksum) throws IOException {
         Env.getCurrentEnv().getPolicyMgr().write(out);
+        return checksum;
+    }
+
+    public long saveIndexPolicy(CountingDataOutputStream out, long checksum) throws IOException {
+        Env.getCurrentEnv().getIndexPolicyMgr().write(out);
         return checksum;
     }
 
@@ -4826,6 +4841,10 @@ public class Env {
 
     public PolicyMgr getPolicyMgr() {
         return this.policyMgr;
+    }
+
+    public IndexPolicyMgr getIndexPolicyMgr() {
+        return this.indexPolicyMgr;
     }
 
     public void setMaster(MasterInfo info) {
