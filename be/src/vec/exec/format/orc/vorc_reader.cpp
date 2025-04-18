2d9f03e6b308070bbec2c4c24ecbcc4d1262d4b9
Socrates <suyiteng@selectdb.com>
2025-04-19 06:06:44 +0800

    [fix](orc) Should not pass selection vector when decode child column of List or Map (#50136) Related PR: #18615
    Problem Summary:
    The problem is like https://github.com/apache/doris-thirdparty/pull/256
    When performing late materialization for LIST or MAP types, filters
    should not be applied directly to their child fields. These complex
    types rely on offsets to correctly map parent-child relationships within
    the columnar storage layout (e.g., in ORC or Parquet files).
    
    If filters are applied to the children of a LIST or MAP field, it may
    cause inconsistencies in the offset alignment, leading to incorrect data
    being read—such as mismatched elements, missing values, or even runtime
    errors. This breaks the structural integrity of the nested data and can
    produce incorrect query results.
    
    ```text
    mysql> select * from complex_data_orc;
    +------+--------------------------+-----------------+
    | id   | m                        | l               |
    +------+--------------------------+-----------------+
    |    1 | {"a":1, "b":2}           | ["a", "b"]      |
    |    2 | {"b":3, "c":4}           | ["b"]           |
    |    3 | {"c":5, "a":6, "b":7}    | ["c", "a"]      |
    |    4 | {"a":8, "c":9}           | ["b", "c"]      |
    |    5 | {"b":10, "a":11}         | ["a"]           |
    |    6 | {"c":12, "b":13}         | ["c"]           |
    |    7 | {"a":15}                 | ["a", "a"]      |
    |    8 | {"b":17}                 | ["b", "b"]      |
    |    9 | {"c":19}                 | ["c", "c"]      |
    |   10 | {"a":20, "b":21, "c":22} | ["a", "b", "c"] |
    +------+--------------------------+-----------------+
    10 rows in set (0.02 sec)
    
    !!!WRONG RESULT:
    mysql> select * from complex_data_orc where id > 2;
    +------+--------------------------+----------------+
    | id   | m                        | l              |
    +------+--------------------------+----------------+
    |    3 | {"c":5, "a":6, "b":7}    | ["c", "a"]     |
    |    4 | {"a":8, "c":9}           | ["b", "c"]     |
    |    5 | {"b":10, "":11}          | ["a"]          |
    |    6 | {"":12, "":13}           | ["c"]          |
    |    7 | {"":15}                  | ["a", ""]      |
    |    8 | {"":17}                  | ["", ""]       |
    |    9 | {"":19}                  | ["", ""]       |
    |   10 | {"a":20, "b":21, "c":22} | ["", "b", "c"] |
    +------+--------------------------+----------------+
    8 rows in set (0.02 sec)
    ```
    
    To ensure correctness, filters should only be applied at the top level
    of the LIST or MAP, and their children should be read in full when late
    materialization occurs.
    
    After this pr:
    ```text
    mysql> select * from complex_data_orc where id > 2;
    +------+--------------------------+-----------------+
    | id   | m                        | l               |
    +------+--------------------------+-----------------+
    |    3 | {"c":5, "a":6, "b":7}    | ["c", "a"]      |
    |    4 | {"a":8, "c":9}           | ["b", "c"]      |
    |    5 | {"b":10, "a":11}         | ["a"]           |
    |    6 | {"c":12, "b":13}         | ["c"]           |
    |    7 | {"a":15}                 | ["a", "a"]      |
    |    8 | {"b":17}                 | ["b", "b"]      |
    |    9 | {"c":19}                 | ["c", "c"]      |
    |   10 | {"a":20, "b":21, "c":22} | ["a", "b", "c"] |
    +------+--------------------------+-----------------+
    8 rows in set (1.41 sec)
    ```

--- be/src/vec/exec/format/orc/vorc_reader.cpp
+++ be/src/vec/exec/format/orc/vorc_reader.cpp
@@ -1459,7 +1459,7 @@ Status OrcReader::_fill_doris_data_column(const std::string& col_name,
                         ->get_nested_type());
         const orc::Type* nested_orc_type = orc_column_type->getSubtype(0);
         std::string element_name = col_name + ".element";
-        return _orc_column_to_doris_column<is_filter>(
+        return _orc_column_to_doris_column<false>(
                 element_name, static_cast<ColumnArray&>(*data_column).get_data_ptr(), nested_type,
                 nested_orc_type, orc_list->elements.get(), element_size);
     }
@@ -1485,12 +1485,12 @@ Status OrcReader::_fill_doris_data_column(const std::string& col_name,
         ColumnPtr& doris_value_column = doris_map.get_values_ptr();
         std::string key_col_name = col_name + ".key";
         std::string value_col_name = col_name + ".value";
-        RETURN_IF_ERROR(_orc_column_to_doris_column<is_filter>(key_col_name, doris_key_column,
-                                                               doris_key_type, orc_key_type,
-                                                               orc_map->keys.get(), element_size));
-        return _orc_column_to_doris_column<is_filter>(value_col_name, doris_value_column,
-                                                      doris_value_type, orc_value_type,
-                                                      orc_map->elements.get(), element_size);
+        RETURN_IF_ERROR(_orc_column_to_doris_column<false>(key_col_name, doris_key_column,
+                                                           doris_key_type, orc_key_type,
+                                                           orc_map->keys.get(), element_size));
+        return _orc_column_to_doris_column<false>(value_col_name, doris_value_column,
+                                                  doris_value_type, orc_value_type,
+                                                  orc_map->elements.get(), element_size);
     }
     case TypeIndex::Struct: {
         if (orc_column_type->getKind() != orc::TypeKind::STRUCT) {
