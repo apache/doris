17acd9cc8634ba7e53a70848d8e479b072447eef
BiteTheDDDDt <xl@selectdb.com>
2025-04-18 19:28:32 +0800

    remove some useless wrong code of array function

--- be/src/vec/aggregate_functions/aggregate_function_collect.h
+++ be/src/vec/aggregate_functions/aggregate_function_collect.h
@@ -27,6 +27,7 @@
 #include <new>
 #include <string>
 
+#include "common/status.h"
 #include "vec/aggregate_functions/aggregate_function.h"
 #include "vec/columns/column.h"
 #include "vec/columns/column_array.h"
@@ -90,25 +91,6 @@ struct AggregateFunctionCollectSetData {
         }
     }
 
-    void write(BufferWritable& buf) const {
-        write_var_uint(data_set.size(), buf);
-        for (const auto& value : data_set) {
-            write_binary(value, buf);
-        }
-        write_var_int(max_size, buf);
-    }
-
-    void read(BufferReadable& buf) {
-        uint64_t new_size = 0;
-        read_var_uint(new_size, buf);
-        ElementNativeType x;
-        for (size_t i = 0; i < new_size; ++i) {
-            read_binary(x, buf);
-            data_set.insert(x);
-        }
-        read_var_int(max_size, buf);
-    }
-
     void insert_result_into(IColumn& to) const {
         auto& vec = assert_cast<ColVecType&>(to).get_data();
         vec.reserve(size());
@@ -154,25 +136,6 @@ struct AggregateFunctionCollectSetData<StringRef, HasLimit> {
             key.data = arena->insert(key.data, key.size);
             data_set.insert(key);
         }
-    }
-
-    void write(BufferWritable& buf) const {
-        write_var_uint(size(), buf);
-        for (const auto& elem : data_set) {
-            write_string_binary(elem, buf);
-        }
-        write_var_int(max_size, buf);
-    }
-
-    void read(BufferReadable& buf) {
-        UInt64 size;
-        read_var_uint(size, buf);
-        StringRef ref;
-        for (size_t i = 0; i < size; ++i) {
-            read_string_binary(ref, buf);
-            data_set.insert(ref);
-        }
-        read_var_int(max_size, buf);
     }
 
     void insert_result_into(IColumn& to) const {
@@ -222,20 +185,6 @@ struct AggregateFunctionCollectListData {
         }
     }
 
-    void write(BufferWritable& buf) const {
-        write_var_uint(size(), buf);
-        buf.write(data.raw_data(), size() * sizeof(ElementType));
-        write_var_int(max_size, buf);
-    }
-
-    void read(BufferReadable& buf) {
-        UInt64 rows = 0;
-        read_var_uint(rows, buf);
-        data.resize(rows);
-        buf.read(reinterpret_cast<char*>(data.data()), rows * sizeof(ElementType));
-        read_var_int(max_size, buf);
-    }
-
     void reset() { data.clear(); }
 
     void insert_result_into(IColumn& to) const {
@@ -273,32 +222,6 @@ struct AggregateFunctionCollectListData<StringRef, HasLimit> {
         } else {
             data->insert_range_from(*rhs.data, 0, rhs.size());
         }
-    }
-
-    void write(BufferWritable& buf) const {
-        auto& col = assert_cast<ColVecType&>(*data);
-
-        write_var_uint(col.size(), buf);
-        buf.write(col.get_offsets().raw_data(), col.size() * sizeof(IColumn::Offset));
-
-        write_var_uint(col.get_chars().size(), buf);
-        buf.write(col.get_chars().raw_data(), col.get_chars().size());
-        write_var_int(max_size, buf);
-    }
-
-    void read(BufferReadable& buf) {
-        auto& col = assert_cast<ColVecType&>(*data);
-        UInt64 offs_size = 0;
-        read_var_uint(offs_size, buf);
-        col.get_offsets().resize(offs_size);
-        buf.read(reinterpret_cast<char*>(col.get_offsets().data()),
-                 offs_size * sizeof(IColumn::Offset));
-
-        UInt64 chars_size = 0;
-        read_var_uint(chars_size, buf);
-        col.get_chars().resize(chars_size);
-        buf.read(reinterpret_cast<char*>(col.get_chars().data()), chars_size);
-        read_var_int(max_size, buf);
     }
 
     void reset() { data->clear(); }
@@ -343,48 +266,6 @@ struct AggregateFunctionCollectListData<void, HasLimit> {
         } else {
             column_data->insert_range_from(*rhs.column_data, 0, rhs.size());
         }
-    }
-
-    void write(BufferWritable& buf) const {
-        const size_t size = column_data->size();
-        write_binary(size, buf);
-
-        DataTypeSerDe::FormatOptions opt;
-        auto tmp_str = ColumnString::create();
-        VectorBufferWriter tmp_buf(*tmp_str.get());
-
-        for (size_t i = 0; i < size; i++) {
-            tmp_str->clear();
-            if (Status st = serde->serialize_one_cell_to_json(*column_data, i, tmp_buf, opt); !st) {
-                throw doris::Exception(ErrorCode::INTERNAL_ERROR,
-                                       "Failed to serialize data for " + column_data->get_name() +
-                                               " error: " + st.to_string());
-            }
-            tmp_buf.commit();
-            write_string_binary(tmp_str->get_data_at(0), buf);
-        }
-
-        write_var_int(max_size, buf);
-    }
-
-    void read(BufferReadable& buf) {
-        size_t size = 0;
-        read_binary(size, buf);
-        column_data->clear();
-        column_data->reserve(size);
-
-        StringRef s;
-        DataTypeSerDe::FormatOptions opt;
-        for (size_t i = 0; i < size; i++) {
-            read_string_binary(s, buf);
-            Slice slice(s.data, s.size);
-            if (Status st = serde->deserialize_one_cell_from_json(*column_data, slice, opt); !st) {
-                throw doris::Exception(ErrorCode::INTERNAL_ERROR,
-                                       "Failed to deserialize data for " + column_data->get_name() +
-                                               " error: " + st.to_string());
-            }
-        }
-        read_var_int(max_size, buf);
     }
 
     void reset() { column_data->clear(); }
@@ -462,37 +343,6 @@ struct AggregateFunctionArrayAggData {
         to_arr.get_offsets().push_back(to_nested_col.size());
     }
 
-    void write(BufferWritable& buf) const {
-        const size_t size = null_map->size();
-        write_binary(size, buf);
-
-        for (size_t i = 0; i < size; i++) {
-            write_binary(null_map->data()[i], buf);
-        }
-
-        for (size_t i = 0; i < size; i++) {
-            write_binary(nested_column->get_data()[i], buf);
-        }
-    }
-
-    void read(BufferReadable& buf) {
-        DCHECK(null_map);
-        DCHECK(null_map->empty());
-        size_t size = 0;
-        read_binary(size, buf);
-        null_map->resize(size);
-        nested_column->reserve(size);
-        for (size_t i = 0; i < size; i++) {
-            read_binary(null_map->data()[i], buf);
-        }
-
-        ElementType data_value;
-        for (size_t i = 0; i < size; i++) {
-            read_binary(data_value, buf);
-            nested_column->get_data().push_back(data_value);
-        }
-    }
-
     void merge(const Self& rhs) {
         const auto size = rhs.null_map->size();
         null_map->resize(size);
@@ -562,34 +412,6 @@ struct AggregateFunctionArrayAggData<StringRef> {
         to_arr.get_offsets().push_back(to_nested_col.size());
     }
 
-    void write(BufferWritable& buf) const {
-        const size_t size = null_map->size();
-        write_binary(size, buf);
-        for (size_t i = 0; i < size; i++) {
-            write_binary(null_map->data()[i], buf);
-        }
-        for (size_t i = 0; i < size; i++) {
-            write_string_binary(nested_column->get_data_at(i), buf);
-        }
-    }
-    void read(BufferReadable& buf) {
-        DCHECK(null_map);
-        DCHECK(null_map->empty());
-        size_t size = 0;
-        read_binary(size, buf);
-        null_map->resize(size);
-        nested_column->reserve(size);
-        for (size_t i = 0; i < size; i++) {
-            read_binary(null_map->data()[i], buf);
-        }
-
-        StringRef s;
-        for (size_t i = 0; i < size; i++) {
-            read_string_binary(s, buf);
-            nested_column->insert_data(s.data, s.size);
-        }
-    }
-
     void merge(const Self& rhs) {
         const auto size = rhs.null_map->size();
         null_map->resize(size);
@@ -638,25 +460,6 @@ struct AggregateFunctionArrayAggData<void> {
             to_nested_col.insert_from(*column_data, i);
         }
         to_arr.get_offsets().push_back(to_nested_col.size());
-    }
-
-    void write(BufferWritable& buf) const {
-        const size_t size = column_data->size();
-        write_binary(size, buf);
-        for (size_t i = 0; i < size; i++) {
-            write_string_binary(column_data->get_data_at(i), buf);
-        }
-    }
-
-    void read(BufferReadable& buf) {
-        size_t size = 0;
-        read_binary(size, buf);
-        column_data->reserve(size);
-        StringRef s;
-        for (size_t i = 0; i < size; i++) {
-            read_string_binary(s, buf);
-            column_data->insert_data(s.data, s.size);
-        }
     }
 
     void merge(const Self& rhs) {
@@ -753,12 +556,12 @@ public:
     }
 
     void serialize(ConstAggregateDataPtr __restrict place, BufferWritable& buf) const override {
-        this->data(place).write(buf);
+        throw Exception(ErrorCode::NOT_IMPLEMENTED_ERROR, "collect not support serialize");
     }
 
     void deserialize(AggregateDataPtr __restrict place, BufferReadable& buf,
                      Arena*) const override {
-        this->data(place).read(buf);
+        throw Exception(ErrorCode::NOT_IMPLEMENTED_ERROR, "collect not support deserialize");
     }
 
     void insert_result_into(ConstAggregateDataPtr __restrict place, IColumn& to) const override {
