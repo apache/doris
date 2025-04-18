598ec8eec76229793c299b4c55f7220be020fcb3
daidai <changyuwei@selectdb.com>
2025-04-18 12:11:59 +0800

    fix be ur

--- be/test/vec/exec/format/parquet/parquet_reader_test.cpp
+++ be/test/vec/exec/format/parquet/parquet_reader_test.cpp
@@ -124,10 +124,8 @@ TEST_F(ParquetReaderTest, normal) {
     auto slot_descs = desc_tbl->get_tuple_descriptor(0)->slots();
     auto local_fs = io::global_local_filesystem();
     io::FileReaderSPtr reader;
-    static_cast<void>(
-            local_fs->open_file("/mnt/disk1/changyuwei/doris/be/test/exec/test_data/"
-                                "parquet_scanner/type-decoder.parquet",
-                                &reader));
+    static_cast<void>(local_fs->open_file(
+            "./be/test/exec/test_data/parquet_scanner/type-decoder.parquet", &reader));
 
     cctz::time_zone ctz;
     TimezoneUtils::find_cctz_time_zone(TimezoneUtils::default_time_zone, ctz);
@@ -189,10 +187,8 @@ static ParquetReader* create_parquet_reader(TFileScanRangeParams& scan_params,
     auto slot_descs = desc_tbl->get_tuple_descriptor(0)->slots();
     auto local_fs = io::global_local_filesystem();
     io::FileReaderSPtr reader;
-    static_cast<void>(
-            local_fs->open_file("/mnt/disk1/changyuwei/doris/be/test/exec/test_data/"
-                                "parquet_scanner/type-decoder.parquet",
-                                &reader));
+    static_cast<void>(local_fs->open_file(
+            "./be/test/exec/test_data/parquet_scanner/type-decoder.parquet", &reader));
 
     cctz::time_zone ctz;
     TimezoneUtils::find_cctz_time_zone(TimezoneUtils::default_time_zone, ctz);
