77f82fea515f42760f6dd41cd9fe08d329cd1cc6
Sun Chenyang <sunchenyang@selectdb.com>
2025-04-21 09:58:34 +0800

    [feature](predefine) support index and schema (#50027)

--- regression-test/suites/variant_p0/with_index/var_index.groovy
+++ regression-test/suites/variant_p0/with_index/var_index.groovy
@@ -21,7 +21,7 @@ suite("regression_test_variant_var_index", "p0, nonConcurrent"){
     sql """
         CREATE TABLE IF NOT EXISTS var_index (
             k bigint,
-            v variant,
+            v variant<'timestamp' : double, 'a' : int, 'b' : string, 'c' : int>,
             INDEX idx_var(v) USING INVERTED  PROPERTIES("parser" = "english") COMMENT ''
         )
         DUPLICATE KEY(`k`)
@@ -33,6 +33,7 @@ suite("regression_test_variant_var_index", "p0, nonConcurrent"){
     sql """insert into var_index values(2, '{"a" : 18811, "b" : "hello world", "c" : 1181111}')"""
     sql """insert into var_index values(3, '{"a" : 18811, "b" : "hello wworld", "c" : 11111}')"""
     sql """insert into var_index values(4, '{"a" : 1234, "b" : "hello xxx world", "c" : 8181111}')"""
+    trigger_and_wait_compaction(table_name, "full")
     sql """ set enable_common_expr_pushdown = true """
     sql """set enable_match_without_inverted_index = false""" 
     qt_sql """select * from var_index where cast(v["a"] as smallint) > 123 and cast(v["b"] as string) match 'hello' and cast(v["c"] as int) > 1024 order by k"""
@@ -42,9 +43,10 @@ suite("regression_test_variant_var_index", "p0, nonConcurrent"){
     // insert double/float/array/json
     sql """insert into var_index values(6, '{"timestamp": 1713283200.060359}')"""
     sql """insert into var_index values(7, '{"timestamp": 17.0}')"""
-    sql """insert into var_index values(8, '{"timestamp": [123]}')"""
+    sql """insert into var_index values(8, '{"arr": [123]}')"""
     sql """insert into var_index values(9, '{"timestamp": 17.0}'),(10, '{"timestamp": "17.0"}')"""
     sql """insert into var_index values(11, '{"nested": [{"a" : 1}]}'),(11, '{"nested": [{"b" : "1024"}]}')"""
+    trigger_and_wait_compaction(table_name, "full")
     qt_sql "select * from var_index order by k limit 15"
 
     sql """ set disable_inverted_index_v1_for_variant = true """
@@ -112,4 +114,39 @@ suite("regression_test_variant_var_index", "p0, nonConcurrent"){
     """
     sql """ALTER TABLE var_index ADD INDEX idx_var(v) USING INVERTED"""
     sql """ set disable_inverted_index_v1_for_variant = true """
+    sql "DROP TABLE IF EXISTS objects"
+    sql """
+         CREATE TABLE `objects` (
+          `id` int NOT NULL,
+          `overflow_properties` variant NULL,
+          INDEX idx1 (`overflow_properties`) USING INVERTED PROPERTIES("support_phrase" = "true", "parser" = "english", "lower_case" = "true")
+        ) ENGINE=OLAP
+        DUPLICATE KEY(`id`)
+        DISTRIBUTED BY RANDOM BUCKETS 1
+        PROPERTIES (
+        "replication_allocation" = "tag.location.default: 1",
+        "min_load_replica_num" = "-1",
+        "is_being_synced" = "false",
+        "storage_medium" = "hdd",
+        "storage_format" = "V2",
+        "inverted_index_storage_format" = "V2",
+        "light_schema_change" = "true",
+        "disable_auto_compaction" = "false",
+        "enable_single_replica_compaction" = "false",
+        "group_commit_interval_ms" = "10000",
+        "group_commit_data_bytes" = "134217728",
+         "variant_max_subcolumns_count" = "0"
+        );
+    """ 
+    sql """
+        INSERT INTO objects (id, overflow_properties)
+        VALUES 
+        (6, '{"color":"Bright Red","description":"A bright red circular object with a metallic shine","shape":"Large Circle","tags":["metallic","reflective"]}'),
+        (7, '{"color":"Deep Blue","description":"Opaque square made of plastic in deep blue","shape":"Small Square","tags":["opaque","plastic"]}'),
+        (8, '{"color":"Green","description":"Tall green triangle carved from wood","shape":"Tall Triangle","tags":["matte","wood"]}'),
+        (9, '{"color":"Reddish Orange","description":"Glossy ceramic hexagon with reddish orange tint","shape":"Flat Hexagon","tags":["glossy","ceramic"]}'),
+        (10, '{"color":"Yellow","description":"Shiny yellow circular badge","shape":"Wide Circle","tags":["shiny","plastic"]}');    
+    """
+    sql "set enable_match_without_inverted_index = false"
+    qt_sql "select count() from objects where (overflow_properties['color'] MATCH_PHRASE 'Blue')"    
 }
