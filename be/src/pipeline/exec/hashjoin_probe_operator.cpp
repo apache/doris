3cbb1a734a93281f8429fb7c31ca61de29f9a1f0
BiteTheDDDDt <xl@selectdb.com>
2025-04-22 17:32:57 +0800

    update fix

--- be/src/pipeline/exec/hashjoin_probe_operator.cpp
+++ be/src/pipeline/exec/hashjoin_probe_operator.cpp
@@ -317,8 +317,8 @@ Status HashJoinProbeOperatorX::pull(doris::RuntimeState* state, vectorized::Bloc
                         if constexpr (!std::is_same_v<HashTableProbeType, std::monostate>) {
                             using HashTableCtxType = std::decay_t<decltype(arg)>;
                             if constexpr (!std::is_same_v<HashTableCtxType, std::monostate>) {
-                                st = process_hashtable_ctx.process<need_null_map_for_probe,
-                                                                            ignore_null>(
+                                st = process_hashtable_ctx.template process<
+                                        need_null_map_for_probe, ignore_null, HashTableCtxType>(
                                         arg,
                                         need_null_map_for_probe
                                                 ? &local_state._null_map_column->get_data()
