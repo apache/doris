4fe12a966b06a5b3b487dea4b89824648df2b745
Benjaminwei <taomingwei1996@gmail.com>
2025-04-22 13:53:19 +0000

    [Improve](explode) update max_be_exec_version=7

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -1935,7 +1935,7 @@ public class Config extends ConfigBase {
      * Max data version of backends serialize block.
      */
     @ConfField(mutable = false)
-    public static int max_be_exec_version = 6;
+    public static int max_be_exec_version = 7;
 
     /**
      * Min data version of backends serialize block.
