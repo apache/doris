ce529f114adc4310418f8b5338708008e005cd2a
amory <wangqiannan@selectdb.com>
2025-04-24 10:15:29 +0800

    [fix](json-functions)fix json-replace/insert/set/array behavior with complex type (#50308)
    here are some functions behavior is not same with mysql as JSON_REPLACE
    | JSON_INSERT | JSON_SET | JSON_ARRAY
    which json and complex types are not supported before this PR
    Follow mysql :
    https://dev.mysql.com/doc/refman/8.0/en/json-modification-functions.html#function_json-set
    before  we will meet result:
    ```
    mysql> insert into t01 values (1, '{"a":1, "b":2}');
    Query OK, 1 row affected (0.25 sec)
    {'label':'label_1056456f8d8c46e5_9be38bb344bdd72c', 'status':'VISIBLE', 'txnId':'8019'}
    
    mysql> select b, json_set(b, "$.a", b) from t01;
    +---------------+-----------------------+
    | b             | json_set(b, "$.a", b) |
    +---------------+-----------------------+
    | {"a":1,"b":2} | {"a":1,"b":2}         |
    +---------------+-----------------------+
    ```
    now
    ```
    mysql>  select b, json_set(b, "$.a", b) from t01;
    +---------------+---------------------------+
    | b             | json_set(b, "$.a", b)     |
    +---------------+---------------------------+
    | {"a":1,"b":2} | {"a":{"a":1,"b":2},"b":2} |
    +---------------+---------------------------+
    1 row in set (0.12 sec)
    ```

--- regression-test/suites/query_p0/sql_functions/json_function/test_query_json_insert.groovy
+++ regression-test/suites/query_p0/sql_functions/json_function/test_query_json_insert.groovy
@@ -41,4 +41,60 @@ suite("test_query_json_insert", "query,arrow_flight_sql") {
     sql "insert into ${tableName} values(3,null,9);"
     sql "insert into ${tableName} values(4,null,null);"
     qt_sql1 "select json_insert('{\"id\": 0, \"time\": \"1970-01-01 00:00:00\", \"a1\": [1, 2], \"a2\": [1, 2]}', '\$.id', id, '\$.time', time, '\$.a1[1]', k, '\$.a2[3]', k) from ${tableName} order by id;"
+    sql "DROP TABLE ${tableName};"
+
+    // test json_insert with complex type
+    // array
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array('"aaa"','"bbb"')); """
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array('aaa','bbb')); """
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array(1,2)); """
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array(1.1,2.2)); """
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array(1.1,2)); """
+    qt_sql_array """ SELECT json_insert('{"data": {}}', '\$.data.arr', array(cast(1 as decimal), cast(1.2 as decimal))); """
+    // map
+    qt_sql_map """ SELECT json_insert('{"data": {}}', '\$.data.map', map('a', 'b', 'c', 'd')); """
+    qt_sql_map """ SELECT json_insert('{"data": {}}', '\$.data.map', map('a', 1, 'c', 2)); """
+    qt_sql_map """ SELECT json_insert('{"data": {}}', '\$.data.map', map('a', 1.1, 'c', 2.2)); """
+    qt_sql_map """ SELECT json_insert('{"data": {}}', '\$.data.map', map('a', 1.1, 'c', 2)); """
+    qt_sql_map """ SELECT json_insert('{"data": {}}', '\$.data.map', map('a', cast(1 as decimal), 'c', cast(1.2 as decimal))); """
+    // struct
+    qt_sql_struct """ SELECT json_insert('{"data": {}}', '\$.data.struct', named_struct('name', 'a', 'age', 1)); """
+    qt_sql_struct """ SELECT json_insert('{"data": {}}', '\$.data.struct', named_struct('name', 'a', 'age', 1.1)); """
+    qt_sql_struct """ SELECT json_insert('{"data": {}}', '\$.data.struct', named_struct('name', 'a', 'age', cast(1 as decimal))); """
+    // json
+    qt_sql_json """ SELECT json_insert('{"data": {}}', '\$.data.json', cast('{\"a\":\"b\"}' as JSON)); """
+    qt_sql_json """ SELECT json_insert('{"data": {}}', '\$.data.json', cast('{\"a\":1}' as JSON)); """
+    qt_sql_json """ SELECT json_insert('{"data": {}}', '\$.data.json', cast('{\"a\":1.1}' as JSON)); """
+
+    // test with table
+    tableName = "test_query_json_insert_complex"
+    sql "DROP TABLE IF EXISTS ${tableName}"
+    sql """
+            CREATE TABLE ${tableName} (
+              `k0` int(11) not null,
+              `k1` array<string> NULL,
+              `k2` map<string, string> NULL,
+              `k3` struct<name:string, age:int> NULL,
+              `k4` json NULL
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`k0`)
+            COMMENT "OLAP"
+            DISTRIBUTED BY HASH(`k0`) BUCKETS 1
+            PROPERTIES (
+            "replication_allocation" = "tag.location.default: 1",
+            "in_memory" = "false",
+            "storage_format" = "V2"
+            );
+        """
+    sql "insert into ${tableName} values(1,null,null,null,null);"
+    sql "insert into ${tableName} values(2, array('a','b'), map('a','b'), named_struct('name','a','age',1), '{\"a\":\"b\"}');"
+    sql """insert into ${tableName} values(3, array('"a"', '"b"'), map('"a"', '"b"', '"c"', '"d"'), named_struct('name','"a"','age', 1), '{\"c\":\"d\"}');"""
+    sql """insert into ${tableName} values(4, array(1,2), map(1,2), named_struct('name', 2, 'age',1), '{\"a\":\"b\"}');"""
+    sql """insert into ${tableName} values(5, array(1,2,3,3), map(1,2,3,4), named_struct('name',\"a\",'age',1), '{\"a\":\"b\"}');"""
+    qt_sql2 """select json_insert('{"data": {}}', 
+                              '\$.data.array', k1, 
+                              '\$.data.map', k2, 
+                              '\$.data.struct', k3, 
+                              '\$.data.json', k4) from ${tableName} order by k0;"""
+    sql "DROP TABLE ${tableName};"
 }
