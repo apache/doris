613d908944ee87510d271e9f543fe4f54f66785f
Tiewei Fang <fangtiewei@selectdb.com>
2025-04-29 14:07:51 +0800

    [feat](refactor-param) refactor outfile's code about fileformat (#50471)
    Issue Number: #50238
    
    Problem Summary:
    
    Previously, we refactored the code of the fileFormat attribute (#50225).
    However, we only added the relevant code without modifying the business
    code. This pull request modifies the code of the `SELECT INTO OUTFILE`
    feature that is related to the fileformat.

--- fe/fe-core/src/test/java/org/apache/doris/datasource/property/fileformat/OrcFileFormatPropertiesTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/datasource/property/fileformat/OrcFileFormatPropertiesTest.java
@@ -17,7 +17,9 @@
 
 package org.apache.doris.datasource.property.fileformat;
 
+import org.apache.doris.thrift.TFileAttributes;
 import org.apache.doris.thrift.TFileCompressType;
+import org.apache.doris.thrift.TResultFileSinkOptions;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -41,5 +43,55 @@ public class OrcFileFormatPropertiesTest {
         // Add properties if needed
         orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
         Assert.assertEquals(TFileCompressType.ZLIB, orcFileFormatProperties.getOrcCompressionType());
+    }
+
+    @Test
+    public void testSupportedCompressionTypes() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("compress_type", "plain");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+        Assert.assertEquals(TFileCompressType.PLAIN, orcFileFormatProperties.getOrcCompressionType());
+
+        properties.put("compress_type", "snappy");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+        Assert.assertEquals(TFileCompressType.SNAPPYBLOCK, orcFileFormatProperties.getOrcCompressionType());
+
+        properties.put("compress_type", "zlib");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+        Assert.assertEquals(TFileCompressType.ZLIB, orcFileFormatProperties.getOrcCompressionType());
+
+        properties.put("compress_type", "zstd");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+        Assert.assertEquals(TFileCompressType.ZSTD, orcFileFormatProperties.getOrcCompressionType());
+    }
+
+    @Test
+    public void testCompressionTypeCaseInsensitive() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("compress_type", "SNAPPY");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+        Assert.assertEquals(TFileCompressType.SNAPPYBLOCK, orcFileFormatProperties.getOrcCompressionType());
+    }
+
+    @Test(expected = org.apache.doris.nereids.exceptions.AnalysisException.class)
+    public void testInvalidCompressionType() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("compress_type", "invalid_type");
+        orcFileFormatProperties.analyzeFileFormatProperties(properties, true);
+    }
+
+    @Test
+    public void testFullTResultFileSinkOptions() {
+        TResultFileSinkOptions sinkOptions = new TResultFileSinkOptions();
+        orcFileFormatProperties.fullTResultFileSinkOptions(sinkOptions);
+        Assert.assertEquals(orcFileFormatProperties.getOrcCompressionType(), sinkOptions.getOrcCompressionType());
+        Assert.assertEquals(1, sinkOptions.getOrcWriterVersion());
+    }
+
+    @Test
+    public void testToTFileAttributes() {
+        TFileAttributes attrs = orcFileFormatProperties.toTFileAttributes();
+        Assert.assertNotNull(attrs);
+        Assert.assertNotNull(attrs.getTextParams());
     }
 }
