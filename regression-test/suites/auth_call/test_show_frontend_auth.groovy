6dde5c21abb3d7fb9c8e35478e0b1cc0f744e715
zhaorongsheng <zhaorongsheng@corp.netease.com>
2025-04-29 09:47:04 +0800

    unify the privilege checker about getting frontends/backends conf info

--- regression-test/suites/auth_call/test_show_frontend_auth.groovy
+++ regression-test/suites/auth_call/test_show_frontend_auth.groovy
@@ -34,7 +34,7 @@ suite("test_show_frontend_auth","p0,auth_call") {
     sql """grant select_priv on regression_test to ${user}"""
     def show_grants_result = sql """show grants for ${user}"""
     logger.info("show grants result: " + show_grants_result)
-    sql """revoke select_priv on internal.information_schema.* from ${user}"""
+    // sql """revoke select_priv on internal.information_schema.* from ${user}"""
 
     connect(user, "${pwd}", context.config.jdbcUrl) {
         try {
