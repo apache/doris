d6503f6a04228eede03af56d9c81e903d12fe592
Tiewei Fang <fangtiewei@selectdb.com>
2025-04-29 14:07:51 +0800

    [feat](refactor-param) refactor outfile's code about fileformat (#50471)
    Issue Number: #50238
    
    Problem Summary:
    
    Previously, we refactored the code of the fileFormat attribute (#50225).
    However, we only added the relevant code without modifying the business
    code. This pull request modifies the code of the `SELECT INTO OUTFILE`
    feature that is related to the fileformat.

--- fe/fe-core/src/main/java/org/apache/doris/datasource/property/fileformat/ParquetFileFormatProperties.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/property/fileformat/ParquetFileFormatProperties.java
@@ -25,15 +25,41 @@ import org.apache.doris.thrift.TParquetCompressionType;
 import org.apache.doris.thrift.TParquetVersion;
 import org.apache.doris.thrift.TResultFileSinkOptions;
 
+import com.google.common.collect.Maps;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class ParquetFileFormatProperties extends FileFormatProperties {
     public static final String PARQUET_DISABLE_DICTIONARY = "disable_dictionary";
-    public static final TParquetVersion parquetVersion = TParquetVersion.PARQUET_1_0;
     public static final String PARQUET_VERSION = "version";
+    public static final String PARQUET_PROP_PREFIX = "parquet.";
+
+    public static final Logger LOG = LogManager.getLogger(ParquetFileFormatProperties.class);
+    public static final Map<String, TParquetCompressionType> PARQUET_COMPRESSION_TYPE_MAP = Maps.newHashMap();
+    public static final Map<String, TParquetVersion> PARQUET_VERSION_MAP = Maps.newHashMap();
+
+    static {
+        PARQUET_COMPRESSION_TYPE_MAP.put("snappy", TParquetCompressionType.SNAPPY);
+        PARQUET_COMPRESSION_TYPE_MAP.put("gzip", TParquetCompressionType.GZIP);
+        PARQUET_COMPRESSION_TYPE_MAP.put("brotli", TParquetCompressionType.BROTLI);
+        PARQUET_COMPRESSION_TYPE_MAP.put("zstd", TParquetCompressionType.ZSTD);
+        PARQUET_COMPRESSION_TYPE_MAP.put("lz4", TParquetCompressionType.LZ4);
+        // arrow do not support lzo and bz2 compression type.
+        // PARQUET_COMPRESSION_TYPE_MAP.put("lzo", TParquetCompressionType.LZO);
+        // PARQUET_COMPRESSION_TYPE_MAP.put("bz2", TParquetCompressionType.BZ2);
+        PARQUET_COMPRESSION_TYPE_MAP.put("plain", TParquetCompressionType.UNCOMPRESSED);
+
+        PARQUET_VERSION_MAP.put("v1", TParquetVersion.PARQUET_1_0);
+        PARQUET_VERSION_MAP.put("latest", TParquetVersion.PARQUET_2_LATEST);
+    }
 
     private TParquetCompressionType parquetCompressionType = TParquetCompressionType.SNAPPY;
     private boolean parquetDisableDictionary = false;
+    private TParquetVersion parquetVersion = TParquetVersion.PARQUET_1_0;
 
     public ParquetFileFormatProperties() {
         super(TFileFormatType.FORMAT_PARQUET);
@@ -42,11 +68,47 @@ public class ParquetFileFormatProperties extends FileFormatProperties {
     @Override
     public void analyzeFileFormatProperties(Map<String, String> formatProperties, boolean isRemoveOriginProperty)
             throws AnalysisException {
+        // save compress type
+        if (formatProperties.containsKey(PROP_COMPRESS_TYPE)) {
+            if (PARQUET_COMPRESSION_TYPE_MAP.containsKey(formatProperties.get(PROP_COMPRESS_TYPE)
+                    .toLowerCase())) {
+                this.parquetCompressionType = PARQUET_COMPRESSION_TYPE_MAP.get(
+                        formatProperties.get(PROP_COMPRESS_TYPE).toLowerCase());
+                formatProperties.remove(PROP_COMPRESS_TYPE);
+            } else {
+                throw new AnalysisException("parquet compression type ["
+                        + formatProperties.get(PROP_COMPRESS_TYPE)
+                        + "] is invalid, please choose one among SNAPPY, GZIP, BROTLI, ZSTD, LZ4, LZO, BZ2 or PLAIN");
+            }
+        }
+
+        // save all parquet prefix property
+        Iterator<Entry<String, String>> iter = formatProperties.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, String> entry = iter.next();
+            if (entry.getKey().startsWith(PARQUET_PROP_PREFIX)) {
+                iter.remove();
+                if (entry.getKey().substring(PARQUET_PROP_PREFIX.length())
+                        .equals(PARQUET_DISABLE_DICTIONARY)) {
+                    this.parquetDisableDictionary = Boolean.valueOf(entry.getValue());
+                } else if (entry.getKey().substring(PARQUET_PROP_PREFIX.length())
+                        .equals(PARQUET_VERSION)) {
+                    if (PARQUET_VERSION_MAP.containsKey(entry.getValue())) {
+                        this.parquetVersion = PARQUET_VERSION_MAP.get(entry.getValue());
+                    } else {
+                        LOG.debug("not set parquet version type or is invalid, set default to PARQUET_1.0 version.");
+                    }
+                }
+            }
+        }
     }
 
+
     @Override
-    public TResultFileSinkOptions toTResultFileSinkOptions() {
-        return null;
+    public void fullTResultFileSinkOptions(TResultFileSinkOptions sinkOptions) {
+        sinkOptions.setParquetCompressionType(parquetCompressionType);
+        sinkOptions.setParquetDisableDictionary(parquetDisableDictionary);
+        sinkOptions.setParquetVersion(parquetVersion);
     }
 
     @Override
