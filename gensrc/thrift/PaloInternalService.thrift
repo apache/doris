99c7b43f7d014c745c836c99bf3002ca0772f873
HappenLee <happenlee@selectdb.com>
2025-04-29 10:35:17 +0800

    [Refactor](Exec) Support one rpc send muti blocks (#50113)
    1. Rmove unless channel ptr in trans struct to reduce mem consume
    2. Send multi block one time in one rpc

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -393,6 +393,7 @@ struct TQueryOptions {
   162: optional bool dump_heap_profile_when_mem_limit_exceeded = false
   163: optional bool inverted_index_compatible_read = false
   164: optional bool check_orc_init_sargs_success = false
+  165: optional i32 exchange_multi_blocks_byte_size = 262144 
 
   // For cloud, to control if the content would be written into file cache
   // In write path, to control if the content would be written into file cache.
