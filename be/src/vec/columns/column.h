a02d00d43046ccecc25e443d788640ff83d6a401
HappenLee <happenlee@selectdb.com>
2025-05-03 22:15:41 +0800

    fix compile error

--- be/src/vec/columns/column.h
+++ be/src/vec/columns/column.h
@@ -697,6 +697,7 @@ using ColumnPtr = IColumn::Ptr;
 using MutableColumnPtr = IColumn::MutablePtr;
 using Columns = std::vector<ColumnPtr>;
 using MutableColumns = std::vector<MutableColumnPtr>;
+using ColumnPtrs = std::vector<ColumnPtr>;
 using ColumnRawPtrs = std::vector<const IColumn*>;
 
 template <typename... Args>
