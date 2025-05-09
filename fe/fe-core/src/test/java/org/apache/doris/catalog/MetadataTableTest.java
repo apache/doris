apache/doris

commit 169996d8e4a70840741749376114c95b411fceda

[feature](information_schema) add `rowsets` table into information_s… (#11266)
* [feature](information_schema) add 'segments' table into information_schema

fe/fe-core/src/main/java/org/apache/doris/planner/BackendPartitionedSchemaScanNode.java
@@ -1,0 +1,169 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.planner;
+
+import org.apache.doris.analysis.Analyzer;
+import org.apache.doris.analysis.LiteralExpr;
+import org.apache.doris.analysis.SlotDescriptor;
+import org.apache.doris.analysis.TupleDescriptor;
+import org.apache.doris.catalog.Column;
+import org.apache.doris.catalog.Env;
+import org.apache.doris.catalog.ListPartitionItem;
+import org.apache.doris.catalog.PartitionInfo;
+import org.apache.doris.catalog.PartitionItem;
+import org.apache.doris.catalog.PartitionKey;
+import org.apache.doris.catalog.PartitionType;
+import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.UserException;
+import org.apache.doris.system.Backend;
+import org.apache.doris.thrift.TNetworkAddress;
+import org.apache.doris.thrift.TScanRange;
+import org.apache.doris.thrift.TScanRangeLocation;
+import org.apache.doris.thrift.TScanRangeLocations;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The BackendSchemaScanNode used for those SchemaTable which data are need to acquire from backends.
+ * BackendSchemaScanNode create dynamic `List` type Partition for BACKEND_ID field,
+ * it will create partitionItems for each alive be node.
+ * So, we can use partitionInfo to select the necessary `be` to send query.
+ */
+public class BackendPartitionedSchemaScanNode extends SchemaScanNode {
+    public static final String ROWSETS = "rowsets";
+
+    public static boolean isBackendPartitionedSchemaTable(String tableName) {
+        if (tableName.equalsIgnoreCase(ROWSETS)) {
+            return true;
+        }
+        return false;
+    }
+
+    private List<TScanRangeLocations> shardScanRanges;
+    // backendPartitionInfo is set in generatePartitionInfo().
+    // `backendPartitionInfo` is `List Partition` of Backend_ID, one PartitionItem only have one partitionKey
+    // for example: if the alive be are: 10001, 10002, 10003, `backendPartitionInfo` like
+    // partition_0: ["10001"]
+    // partition_1: ["10002"]
+    // partition_2: ["10003"]
+    private PartitionInfo backendPartitionInfo;
+    // partitionID -> backendID
+    private Map<Long, Long> partitionIDToBackendID;
+    private Collection<Long> selectedPartitionIds = Lists.newArrayList();
+
+    public BackendPartitionedSchemaScanNode(PlanNodeId id, TupleDescriptor desc) {
+        super(id, desc);
+    }
+
+    @Override
+    public void init(Analyzer analyzer) throws UserException {
+        super.init(analyzer);
+        computeColumnFilter();
+        computePartitionInfo();
+    }
+
+    @Override
+    public void finalize(Analyzer analyzer) throws UserException {
+        super.finalize(analyzer);
+        shardScanRanges = getScanRangeLocations();
+    }
+
+    @Override
+    public List<TScanRangeLocations> getScanRangeLocations(long maxScanRangeLength) {
+        return shardScanRanges;
+    }
+
+    @Override
+    public int getNumInstances() {
+        return shardScanRanges.size();
+    }
+
+    private List<TScanRangeLocations> getScanRangeLocations() throws AnalysisException {
+        List<TScanRangeLocations> result = new ArrayList<>();
+        for (Long partitionID : selectedPartitionIds) {
+            Long backendId = partitionIDToBackendID.get(partitionID);
+            Backend be  = Env.getCurrentSystemInfo().getIdToBackend().get(backendId);
+            if (!be.isAlive()) {
+                throw new AnalysisException("backend " + be.getId() + " is not alive.");
+            }
+            TScanRangeLocations locations = new TScanRangeLocations();
+            TScanRangeLocation location = new TScanRangeLocation();
+            location.setBackendId(be.getId());
+            location.setServer(new TNetworkAddress(be.getHost(), be.getBePort()));
+            locations.addToLocations(location);
+            locations.setScanRange(new TScanRange());
+            result.add(locations);
+        }
+        return result;
+    }
+
+    private void computePartitionInfo() throws AnalysisException {
+        List<Column> partitionColumns = new ArrayList<>();
+        for (SlotDescriptor slotDesc : desc.getSlots()) {
+            if ("BACKEND_ID".equalsIgnoreCase(slotDesc.getColumn().getName())) {
+                partitionColumns.add(slotDesc.getColumn());
+                break;
+            }
+        }
+        createPartitionInfo(partitionColumns);
+        PartitionPruner partitionPruner = null;
+        Map<Long, PartitionItem> keyItemMap = backendPartitionInfo.getIdToItem(false);
+        if (analyzer.partitionPruneV2Enabled()) {
+            partitionPruner = new ListPartitionPrunerV2(keyItemMap, backendPartitionInfo.getPartitionColumns(),
+                    columnNameToRange);
+        } else {
+            partitionPruner = new ListPartitionPruner(keyItemMap,
+                    backendPartitionInfo.getPartitionColumns(), columnFilters);
+        }
+        selectedPartitionIds = partitionPruner.prune();
+    }
+
+    /**
+     * create PartitionInfo for partitionColumn
+     * @param partitionColumns The Columns we want to create partitionInfo
+     * @throws AnalysisException
+     */
+    private void createPartitionInfo(List<Column> partitionColumns) throws AnalysisException {
+        backendPartitionInfo = new PartitionInfo(PartitionType.LIST, partitionColumns);
+        partitionIDToBackendID = new HashMap<>();
+        long partitionID = 0;
+        for (Backend be : Env.getCurrentSystemInfo().getIdToBackend().values()) {
+            if (be.isAlive()) {
+                // create partition key
+                PartitionKey partitionKey = new PartitionKey();
+                for (Column partitionColumn : partitionColumns) {
+                    LiteralExpr expr = LiteralExpr.create(String.valueOf(be.getId()), partitionColumn.getType());
+                    partitionKey.pushColumn(expr, partitionColumn.getDataType());
+                }
+                // create partition Item
+                List<PartitionKey> partitionKeys = new ArrayList<>();
+                partitionKeys.add(partitionKey);
+                PartitionItem partitionItem = new ListPartitionItem(partitionKeys);
+                backendPartitionInfo.setItem(partitionID, false, partitionItem);
+                partitionIDToBackendID.put(partitionID, be.getId());
+                ++partitionID;
+            }
+        }
+    }
+}

fe/fe-core/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
@@ -1731,7 +1731,12 @@ public class SingleNodePlanner {
                 scanNode = new MysqlScanNode(ctx.getNextNodeId(), tblRef.getDesc(), (MysqlTable) tblRef.getTable());
                 break;
             case SCHEMA:
-                scanNode = new SchemaScanNode(ctx.getNextNodeId(), tblRef.getDesc());
+                if (BackendPartitionedSchemaScanNode.isBackendPartitionedSchemaTable(
+                        tblRef.getDesc().getTable().getName())) {
+                    scanNode = new BackendPartitionedSchemaScanNode(ctx.getNextNodeId(), tblRef.getDesc());
+                } else {
+                    scanNode = new SchemaScanNode(ctx.getNextNodeId(), tblRef.getDesc());
+                }
                 break;
             case BROKER:
                 scanNode = new BrokerScanNode(ctx.getNextNodeId(), tblRef.getDesc(), "BrokerScanNode",

be/src/exec/CMakeLists.txt
@@ -87,6 +87,7 @@ set(EXEC_FILES
     schema_scanner/schema_user_privileges_scanner.cpp
     schema_scanner/schema_files_scanner.cpp
     schema_scanner/schema_partitions_scanner.cpp
+    schema_scanner/schema_rowsets_scanner.cpp
 
     partitioned_hash_table.cc
     partitioned_aggregation_node.cc

be/src/exec/schema_scanner.cpp
@@ -23,6 +23,7 @@
 #include "exec/schema_scanner/schema_dummy_scanner.h"
 #include "exec/schema_scanner/schema_files_scanner.h"
 #include "exec/schema_scanner/schema_partitions_scanner.h"
+#include "exec/schema_scanner/schema_rowsets_scanner.h"
 #include "exec/schema_scanner/schema_schema_privileges_scanner.h"
 #include "exec/schema_scanner/schema_schemata_scanner.h"
 #include "exec/schema_scanner/schema_table_privileges_scanner.h"
@@ -110,6 +111,8 @@ SchemaScanner* SchemaScanner::create(TSchemaTableType::type type) {
         return new (std::nothrow) SchemaFilesScanner();
     case TSchemaTableType::SCH_PARTITIONS:
         return new (std::nothrow) SchemaPartitionsScanner();
+    case TSchemaTableType::SCH_ROWSETS:
+        return new (std::nothrow) SchemaRowsetsScanner();
     default:
         return new (std::nothrow) SchemaDummyScanner();
         break;

be/src/exec/schema_scanner/schema_rowsets_scanner.cpp
@@ -1,0 +1,176 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "exec/schema_scanner/schema_rowsets_scanner.h"
+
+#include <cstddef>
+
+#include "common/status.h"
+#include "gutil/integral_types.h"
+#include "olap/rowset/beta_rowset.h"
+#include "olap/rowset/rowset.h"
+#include "olap/rowset/segment_v2/segment.h"
+#include "olap/segment_loader.h"
+#include "olap/storage_engine.h"
+#include "olap/tablet.h"
+#include "runtime/descriptors.h"
+#include "runtime/primitive_type.h"
+#include "runtime/string_value.h"
+namespace doris {
+SchemaScanner::ColumnDesc SchemaRowsetsScanner::_s_tbls_columns[] = {
+        //   name,       type,          size,     is_null
+        {"BACKEND_ID", TYPE_BIGINT, sizeof(int64_t), true},
+        {"ROWSET_ID", TYPE_VARCHAR, sizeof(StringValue), true},
+        {"TABLET_ID", TYPE_BIGINT, sizeof(int64_t), true},
+        {"ROWSET_NUM_ROWS", TYPE_BIGINT, sizeof(int64_t), true},
+        {"TXN_ID", TYPE_BIGINT, sizeof(int64_t), true},
+        {"NUM_SEGMENTS", TYPE_BIGINT, sizeof(int64_t), true},
+        {"START_VERSION", TYPE_BIGINT, sizeof(int64_t), true},
+        {"END_VERSION", TYPE_BIGINT, sizeof(int64_t), true},
+        {"INDEX_DISK_SIZE", TYPE_BIGINT, sizeof(size_t), true},
+        {"DATA_DISK_SIZE", TYPE_BIGINT, sizeof(size_t), true},
+        {"CREATION_TIME", TYPE_BIGINT, sizeof(int64_t), true},
+        {"OLDEST_WRITE_TIMESTAMP", TYPE_BIGINT, sizeof(int64_t), true},
+        {"NEWEST_WRITE_TIMESTAMP", TYPE_BIGINT, sizeof(int64_t), true},
+
+};
+
+SchemaRowsetsScanner::SchemaRowsetsScanner()
+        : SchemaScanner(_s_tbls_columns,
+                        sizeof(_s_tbls_columns) / sizeof(SchemaScanner::ColumnDesc)),
+          backend_id_(0),
+          rowsets_idx_(0) {};
+
+Status SchemaRowsetsScanner::start(RuntimeState* state) {
+    if (!_is_init) {
+        return Status::InternalError("used before initialized.");
+    }
+    backend_id_ = state->backend_id();
+    RETURN_IF_ERROR(get_all_rowsets());
+    return Status::OK();
+}
+
+Status SchemaRowsetsScanner::get_next_row(Tuple* tuple, MemPool* pool, bool* eos) {
+    if (!_is_init) {
+        return Status::InternalError("Used before initialized.");
+    }
+    if (nullptr == tuple || nullptr == pool || nullptr == eos) {
+        return Status::InternalError("input pointer is nullptr.");
+    }
+    if (rowsets_idx_ >= rowsets_.size()) {
+        *eos = true;
+        return Status::OK();
+    }
+    *eos = false;
+    return fill_one_row(tuple, pool);
+}
+
+Status SchemaRowsetsScanner::get_all_rowsets() {
+    std::vector<TabletSharedPtr> tablets =
+            StorageEngine::instance()->tablet_manager()->get_all_tablet();
+    for (const auto& tablet : tablets) {
+        // all rowset
+        std::vector<std::pair<Version, RowsetSharedPtr>> all_rowsets;
+        {
+            std::shared_lock rowset_ldlock(tablet->get_header_lock());
+            tablet->acquire_version_and_rowsets(&all_rowsets);
+        }
+        for (const auto& version_and_rowset : all_rowsets) {
+            RowsetSharedPtr rowset = version_and_rowset.second;
+            rowsets_.emplace_back(rowset);
+        }
+    }
+    return Status::OK();
+}
+
+Status SchemaRowsetsScanner::fill_one_row(Tuple* tuple, MemPool* pool) {
+    // set all bit to not null
+    memset((void*)tuple, 0, _tuple_desc->num_null_bytes());
+    RowsetSharedPtr rowset = rowsets_[rowsets_idx_];
+    // BACKEND_ID
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[0]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = backend_id_;
+    }
+    // ROWSET_ID
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[1]->tuple_offset());
+        StringValue* str_slot = reinterpret_cast<StringValue*>(slot);
+        std::string rowset_id = rowset->rowset_id().to_string();
+        str_slot->ptr = (char*)pool->allocate(rowset_id.size());
+        str_slot->len = rowset_id.size();
+        memcpy(str_slot->ptr, rowset_id.c_str(), str_slot->len);
+    }
+    // TABLET_ID
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[2]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->rowset_meta()->tablet_id();
+    }
+    // ROWSET_NUM_ROWS
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[3]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->num_rows();
+    }
+    // TXN_ID
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[4]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->txn_id();
+    }
+    // NUM_SEGMENTS
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[5]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->num_segments();
+    }
+    // START_VERSION
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[6]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->start_version();
+    }
+    // END_VERSION
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[7]->tuple_offset());
+        *(reinterpret_cast<int64_t*>(slot)) = rowset->end_version();
+    }
+    // INDEX_DISK_SIZE
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[8]->tuple_offset());
+        *(reinterpret_cast<size_t*>(slot)) = rowset->index_disk_size();
+    }
+    // DATA_DISK_SIZE
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[9]->tuple_offset());
+        *(reinterpret_cast<size_t*>(slot)) = rowset->data_disk_size();
+    }
+    // CREATION_TIME
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[10]->tuple_offset());
+        *(reinterpret_cast<size_t*>(slot)) = rowset->creation_time();
+    }
+    // OLDEST_WRITE_TIMESTAMP
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[11]->tuple_offset());
+        *(reinterpret_cast<size_t*>(slot)) = rowset->oldest_write_timestamp();
+    }
+    // NEWEST_WRITE_TIMESTAMP
+    {
+        void* slot = tuple->get_slot(_tuple_desc->slots()[12]->tuple_offset());
+        *(reinterpret_cast<size_t*>(slot)) = rowset->newest_write_timestamp();
+    }
+    ++rowsets_idx_;
+    return Status::OK();
+}
+} // namespace doris

be/src/exec/schema_scanner/schema_rowsets_scanner.h
@@ -1,0 +1,50 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "common/status.h"
+#include "exec/schema_scanner.h"
+#include "olap/rowset/rowset.h"
+#include "olap/rowset/segment_v2/segment.h"
+#include "runtime/mem_pool.h"
+#include "runtime/runtime_state.h"
+namespace doris {
+class SchemaRowsetsScanner : public SchemaScanner {
+public:
+    SchemaRowsetsScanner();
+    ~SchemaRowsetsScanner() override = default;
+
+    Status start(RuntimeState* state) override;
+    Status get_next_row(Tuple* tuple, MemPool* pool, bool* eos) override;
+
+private:
+    Status get_all_rowsets();
+    // Status get_new_segments();
+    Status fill_one_row(Tuple* tuple, MemPool* pool);
+
+private:
+    static SchemaScanner::ColumnDesc _s_tbls_columns[];
+    int64_t backend_id_ = 0;
+    std::vector<RowsetSharedPtr> rowsets_;
+    // used for traversing rowsets_
+    int rowsets_idx_ = 0;
+};
+} // namespace doris

be/src/olap/tablet_manager.cpp
@@ -574,6 +574,23 @@ TabletSharedPtr TabletManager::get_tablet(TTabletId tablet_id, TabletUid tablet_
     return nullptr;
 }
 
+std::vector<TabletSharedPtr> TabletManager::get_all_tablet() {
+    std::vector<TabletSharedPtr> res;
+    for (const auto& tablets_shard : _tablets_shards) {
+        std::shared_lock rdlock(tablets_shard.lock);
+        for (const auto& tablet_map : tablets_shard.tablet_map) {
+            // these are tablets which is not deleted
+            TabletSharedPtr tablet = tablet_map.second;
+            if (!tablet->is_used()) {
+                LOG(WARNING) << "tablet cannot be used. tablet=" << tablet->tablet_id();
+                continue;
+            }
+            res.emplace_back(tablet);
+        }
+    }
+    return res;
+}
+
 bool TabletManager::get_tablet_id_and_schema_hash_from_path(const string& path,
                                                             TTabletId* tablet_id,
                                                             TSchemaHash* schema_hash) {

be/src/olap/tablet_manager.h
@@ -78,6 +78,8 @@ public:
     TabletSharedPtr get_tablet(TTabletId tablet_id, TabletUid tablet_uid,
                                bool include_deleted = false, std::string* err = nullptr);
 
+    std::vector<TabletSharedPtr> get_all_tablet();
+
     // Extract tablet_id and schema_hash from given path.
     //
     // The normal path pattern is like "/data/{shard_id}/{tablet_id}/{schema_hash}/xxx.data".

commit 6f817a74a3d7c5329a0781f3ce6af5b1812d45ae

[fix](information_schema) fix backend_active_tasks table only return one backend's data (#50721)
fix backend_active_tasks table only return one backend's data, for
example:
```sql
select sum(SCAN_ROWS), sum(SCAN_BYTES)
from backend_active_tasks 
where QUERY_ID = 'd299cb2156ef4870-aea578938f703503'
```

the root cause is: the partition column `be_id` has be pruned, and can
not get scan ranges from multiple backends, this bug is introduced by #11266

fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -948,7 +948,7 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
         SchemaScanNode scanNode = null;
         if (BackendPartitionedSchemaScanNode.isBackendPartitionedSchemaTable(
                 table.getName())) {
-            scanNode = new BackendPartitionedSchemaScanNode(context.nextPlanNodeId(), tupleDescriptor,
+            scanNode = new BackendPartitionedSchemaScanNode(context.nextPlanNodeId(), table, tupleDescriptor,
                 schemaScan.getSchemaCatalog().orElse(null), schemaScan.getSchemaDatabase().orElse(null),
                 schemaScan.getSchemaTable().orElse(null));
         } else {

fe/fe-core/src/main/java/org/apache/doris/planner/BackendPartitionedSchemaScanNode.java
@@ -19,7 +19,6 @@ package org.apache.doris.planner;
 
 import org.apache.doris.analysis.Analyzer;
 import org.apache.doris.analysis.LiteralExpr;
-import org.apache.doris.analysis.SlotDescriptor;
 import org.apache.doris.analysis.TupleDescriptor;
 import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.Env;
@@ -28,6 +27,7 @@ import org.apache.doris.catalog.PartitionInfo;
 import org.apache.doris.catalog.PartitionItem;
 import org.apache.doris.catalog.PartitionKey;
 import org.apache.doris.catalog.PartitionType;
+import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.system.Backend;
@@ -59,6 +59,7 @@ public class BackendPartitionedSchemaScanNode extends SchemaScanNode {
     // you need to the table's backend column id name to BACKEND_ID_COLUMN_SET
     // it's used to backend pruner, see computePartitionInfo;
     public static final Set<String> BEACKEND_ID_COLUMN_SET = new HashSet<>();
+    private final TableIf tableIf;
 
     static {
         BACKEND_TABLE.add("rowsets");
@@ -90,9 +91,10 @@ public class BackendPartitionedSchemaScanNode extends SchemaScanNode {
     private Map<Long, Long> partitionIDToBackendID;
     private Collection<Long> selectedPartitionIds = Lists.newArrayList();
 
-    public BackendPartitionedSchemaScanNode(PlanNodeId id, TupleDescriptor desc,
+    public BackendPartitionedSchemaScanNode(PlanNodeId id, TableIf table, TupleDescriptor desc,
                                             String schemaCatalog, String schemaDatabase, String schemaTable) {
         super(id, desc, schemaCatalog, schemaDatabase, schemaTable);
+        this.tableIf = table;
     }
 
     @Override
@@ -141,9 +143,9 @@ public class BackendPartitionedSchemaScanNode extends SchemaScanNode {
 
     private void computePartitionInfo() throws UserException {
         List<Column> partitionColumns = new ArrayList<>();
-        for (SlotDescriptor slotDesc : desc.getSlots()) {
-            if (BEACKEND_ID_COLUMN_SET.contains(slotDesc.getColumn().getName().toLowerCase())) {
-                partitionColumns.add(slotDesc.getColumn());
+        for (Column column : tableIf.getColumns()) {
+            if (BEACKEND_ID_COLUMN_SET.contains(column.getName().toLowerCase())) {
+                partitionColumns.add(column);
                 break;
             }
         }

fe/fe-core/src/main/java/org/apache/doris/planner/SingleNodePlanner.java
@@ -1943,8 +1943,8 @@ public class SingleNodePlanner {
             case SCHEMA:
                 if (BackendPartitionedSchemaScanNode.isBackendPartitionedSchemaTable(
                         tblRef.getDesc().getTable().getName())) {
-                    scanNode = new BackendPartitionedSchemaScanNode(ctx.getNextNodeId(), tblRef.getDesc(),
-                        null, null, null);
+                    scanNode = new BackendPartitionedSchemaScanNode(ctx.getNextNodeId(), tblRef.getTable(),
+                            tblRef.getDesc(), null, null, null);
                 } else {
                     scanNode = new SchemaScanNode(ctx.getNextNodeId(), tblRef.getDesc(), null, null, null);
                 }

fe/fe-core/src/test/java/org/apache/doris/catalog/MetadataTableTest.java
@@ -1,0 +1,61 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.catalog;
+
+import org.apache.doris.nereids.NereidsPlanner;
+import org.apache.doris.nereids.StatementContext;
+import org.apache.doris.nereids.glue.LogicalPlanAdapter;
+import org.apache.doris.nereids.parser.NereidsParser;
+import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
+import org.apache.doris.planner.BackendPartitionedSchemaScanNode;
+import org.apache.doris.planner.PlanFragment;
+import org.apache.doris.planner.PlanNode;
+import org.apache.doris.thrift.TScanRangeLocations;
+import org.apache.doris.utframe.TestWithFeService;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetadataTableTest extends TestWithFeService {
+
+    @Override
+    protected int backendNum() {
+        return 2;
+    }
+
+    @Test
+    public void testScanBackendActiveTasks() throws Exception {
+        useDatabase("information_schema");
+
+        LogicalPlan parsed = new NereidsParser().parseSingle(
+                "select  sum(SCAN_ROWS),  sum(SCAN_BYTES)  from  backend_active_tasks  where  QUERY_ID  =  'd299cb2156ef4870-aea578938f703503'");
+        StatementContext statementContext = new StatementContext();
+        NereidsPlanner nereidsPlanner = new NereidsPlanner(statementContext);
+        nereidsPlanner.plan(new LogicalPlanAdapter(parsed, statementContext));
+        List<PlanFragment> fragments = nereidsPlanner.getFragments();
+        PlanNode planRoot = fragments.get(fragments.size() - 1).getPlanRoot();
+        List<BackendPartitionedSchemaScanNode> scanNodes = new ArrayList<>();
+        planRoot.collect(BackendPartitionedSchemaScanNode.class, scanNodes);
+        BackendPartitionedSchemaScanNode scanNode = scanNodes.get(0);
+        List<TScanRangeLocations> scanRangeLocations = scanNode.getScanRangeLocations(0);
+        Assertions.assertEquals(backendNum(), scanRangeLocations.size());
+    }
+}