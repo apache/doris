4c1361805efd90e0c2595e8f9692ed94906bcd1e
yujun <yujun@selectdb.com>
2025-05-09 10:29:04 +0800

    update

--- docker/runtime/doris-compose/database.py
+++ docker/runtime/doris-compose/database.py
@@ -282,7 +282,8 @@ class DBManager(object):
                     ]
             except Exception as e:
                 LOG.warn(
-                    f"Error occurred: fe {self.fe_ip}:{self.fe_port}, err {e}")
+                    f"Error occurred: fe {self.fe_ip}:{self.fe_port}, sql `{sql}`, err {e}"
+                )
                 if "timed out" in str(e).lower() and attempt < retries - 1:
                     LOG.warn(
                         f"Query timed out, fe {self.fe_ip}:{self.fe_port}. Retrying {attempt + 1}/{retries}..."
