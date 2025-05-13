cf5f643a268a4ce74ee1f37e3c39f3658219a760
shuke987 <shuke@selectdb.com>
2025-05-13 11:57:58 +0800

    add profile

--- regression-test/suites/nereids_rules_p0/mv/tpch/mv_tpch_test.groovy
+++ regression-test/suites/nereids_rules_p0/mv/tpch/mv_tpch_test.groovy
@@ -592,7 +592,6 @@ suite("mv_tpch_test") {
     order_qt_query8_after "${query8}"
     sql """ DROP MATERIALIZED VIEW IF EXISTS mv8"""
 
-
     def mv9 = """
             SELECT
               nation,
@@ -659,6 +658,19 @@ suite("mv_tpch_test") {
               nation,
               o_year DESC             
     """
+
+    logger.info("start profile")
+    int randomInt = Math.random() * 2000000000
+    profile("test_profile1_time_${randomInt}") {
+        run {
+            sql "/* test_profile1_time_${randomInt} */ ${query9}"
+        }
+
+        check { profileString, exception ->
+            log.info(profileString)
+        }
+    }
+
     order_qt_query9_before "${query9}"
     async_mv_rewrite_success(db, mv9, query9, "mv9")
     order_qt_query9_after "${query9}"
