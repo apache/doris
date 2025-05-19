45606f6fc558908a3b8cff2ffb4139e7583e612c
Sridhar R Manikarnike <sridhar.nike@gmail.com>
2025-05-19 17:37:43 +0000

    fix compile

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ShowColumnsCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/ShowColumnsCommand.java
@@ -47,7 +47,6 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
