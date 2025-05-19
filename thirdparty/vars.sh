b622d6af73bd232d3d4e8816e05f983143491969
Xinyi Zou <zouxinyi@selectdb.com>
2025-05-19 12:03:37 +0800

    [fix](arrow-flight-sql) Separate arrow-flight-sql connection and mysql connection (#50939)
    Add FE conf `arrow_flight_max_connections` as the upper limit of
    arrow-flight-sql connection, arrow-flight-sql connections and mysql
    connections will be managed separately.

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -253,10 +253,10 @@ GRPC_SOURCE=grpc-1.54.3
 GRPC_MD5SUM="af00a2edeae0f02bb25917cc3473b7de"
 
 # arrow
-ARROW_DOWNLOAD="https://github.com/apache/arrow/archive/refs/tags/apache-arrow-17.0.0.tar.gz"
-ARROW_NAME="apache-arrow-17.0.0.tar.gz"
-ARROW_SOURCE="arrow-apache-arrow-17.0.0"
-ARROW_MD5SUM="ba18bf83e2164abd34b9ac4cb164f0f0"
+ARROW_DOWNLOAD="https://github.com/apache/arrow/archive/refs/tags/apache-arrow-19.0.1.tar.gz"
+ARROW_NAME="apache-arrow-19.0.1.tar.gz"
+ARROW_SOURCE="arrow-apache-arrow-19.0.1"
+ARROW_MD5SUM="8c5091da0f8fb41a47d7f4dad7b712df"
 
 # Abseil
 ABSEIL_DOWNLOAD="https://github.com/abseil/abseil-cpp/archive/refs/tags/20230125.3.tar.gz"
