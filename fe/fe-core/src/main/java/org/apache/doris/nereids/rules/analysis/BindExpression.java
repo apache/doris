61cf269bb7bb5279b461f9900bddd59d96977c46
James <lijibing@selectdb.com>
2025-05-20 12:52:57 +0800

    [improve](nereids)Remove use of session variable deprecated_group_by_and_having_use_alias_first (#51015)
    ### What problem does this PR solve?
    Remove use of session variable
    deprecated_group_by_and_having_use_alias_first, because it is useless.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindExpression.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindExpression.java
@@ -100,7 +100,6 @@ import org.apache.doris.nereids.util.PlanUtils;
 import org.apache.doris.nereids.util.PlanUtils.CollectNonWindowedAggFuncs;
 import org.apache.doris.nereids.util.TypeCoercionUtils;
 import org.apache.doris.nereids.util.Utils;
-import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.SqlModeHelper;
 
 import com.google.common.base.Preconditions;
@@ -1352,12 +1351,6 @@ public class BindExpression implements AnalysisRuleFactory {
             if (isGroupByContainAlias) {
                 break;
             }
-        }
-
-        if (isGroupByContainAlias
-                && ConnectContext.get() != null
-                && ConnectContext.get().getSessionVariable().isGroupByAndHavingUseAliasFirst()) {
-            throw new AnalysisException("group_by_and_having_use_alias=true is unsupported for Nereids");
         }
     }
 
