cac7dd4db2d9e2406b16521bc2d0f43680d83d63
hezhiqiang <hezhiqiang@selectdb.com>
2025-05-20 17:44:47 +0800

    D2

--- be/src/vec/data_types/data_type_factory.hpp
+++ be/src/vec/data_types/data_type_factory.hpp
@@ -56,8 +56,6 @@ public:
         return instance;
     }
 
-    DataTypePtr create_data_type(const TypeIndex& type_index, bool is_nullable = false);
-    DataTypePtr create_data_type(const doris::Field& col_desc);
     DataTypePtr create_data_type(const TabletColumn& col_desc, bool is_nullable = false);
 
     DataTypePtr create_data_type(const PColumnMeta& pcolumn);
