74feb91dc7fb9ad6007e16f8ab60151d7bed0b5e
Socrates <suxiaogang223@icloud.com>
2025-05-21 15:17:06 +0800

    impl IcebergSnapshotsJniScanner

--- fe/be-java-extensions/iceberg-metadata-scanner/src/main/java/org/apache/doris/iceberg/IcebergMetadataJniScanner.java
+++ fe/be-java-extensions/iceberg-metadata-scanner/src/main/java/org/apache/doris/iceberg/IcebergMetadataJniScanner.java
@@ -18,35 +18,35 @@
 package org.apache.doris.iceberg;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
 import org.apache.doris.common.jni.JniScanner;
 import org.apache.doris.common.jni.vec.ColumnType;
 import org.apache.iceberg.Table;
+import org.apache.iceberg.util.SerializationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Abstract class for Iceberg metadata scanner.
  */
-public class IcebergMetadataJniScanner extends JniScanner {
+public abstract class IcebergMetadataJniScanner extends JniScanner {
     private static final Logger LOG = LoggerFactory.getLogger(IcebergMetadataJniScanner.class);
     protected final String serializedTable;
     protected final String[] requiredFields;
     private final String[] metadataColumnNames;
     private final String[] metadataColumnTypes;
     private ColumnType[] requiredTypes;
-    private final String[] nestedFields;
-    private final int fetchSize;
+    private final int batchSize;
     protected final ClassLoader classLoader;
     protected Table table;
     protected String timezone;
 
-    public IcebergMetadataJniScanner(int fetchSize, Map<String, String> params) {
-        this.fetchSize = fetchSize;
+    public IcebergMetadataJniScanner(int batchSize, Map<String, String> params) {
+        this.batchSize = batchSize;
         this.requiredFields = params.get("required_fields").split(",");
-        // this.nestedFields = ScannerHelper.splitAndOmitEmptyStrings(params.getOrDefault("nested_fields", ""), ",");
         this.metadataColumnNames = params.get("metadata_column_names").split(",");
         this.metadataColumnTypes = params.get("metadata_column_types").split("#");
         this.serializedTable = params.get("serialized_table");
@@ -56,17 +56,68 @@ public class IcebergMetadataJniScanner extends JniScanner {
 
     @Override
     public void open() throws IOException {
-
+        try {
+            Thread.currentThread().setContextClassLoader(classLoader);
+            this.table = SerializationUtil.deserializeFromBase64(serializedTable);
+            parseRequiredTypes();
+            initTableInfo(requiredTypes, requiredFields, batchSize);
+            openInner();
+            initReader();
+        } catch (Exception e) {
+            this.close();
+            String msg = String.format("Failed to open IcebergMetadataJniScanner");
+            LOG.error(msg, e);
+            throw new IOException(msg, e);
+        }
     }
 
     @Override
     public int getNext() throws IOException {
-        return 0;
+        try {
+            Thread.currentThread().setContextClassLoader(classLoader);
+            return getNextInner();
+        } catch (Exception e) {
+            this.close();
+            String msg = String.format(
+                    "Failed to get next IcebergMetadataJniScanner");
+            LOG.error(msg, e);
+            throw new IOException(msg, e);
+        }
     }
 
     @Override
     public void close() throws IOException {
-
+        try {
+            Thread.currentThread().setContextClassLoader(classLoader);
+            closeInner();
+        } catch (Exception e) {
+            String msg = String.format("Failed to close IcebergMetadataJniScanner");
+            LOG.error(msg, e);
+            throw new IOException(msg, e);
+        }
     }
 
+    protected abstract void initReader() throws IOException;
+
+    protected abstract void openInner();
+
+    protected abstract int getNextInner();
+
+    protected abstract void closeInner() throws IOException;
+
+    private void parseRequiredTypes() {
+        HashMap<String, String> metadataColumnMap = new HashMap<>();
+        for (int i = 0; i < metadataColumnNames.length; i++) {
+            metadataColumnMap.put(metadataColumnNames[i], metadataColumnTypes[i]);
+        }
+        requiredTypes = new ColumnType[requiredFields.length];
+        for (int i = 0; i < requiredFields.length; i++) {
+            String field = requiredFields[i];
+            String type = metadataColumnMap.get(field);
+            if (type == null) {
+                throw new IllegalArgumentException("Field " + field + " not found in metadata column map");
+            }
+            requiredTypes[i] = ColumnType.parseType(field, type);
+        }
+    }
 }
---
+++ fe/be-java-extensions/iceberg-metadata-scanner/src/main/java/org/apache/doris/iceberg/IcebergSnapshotsJniScanner.java
@@ -0,0 +1,111 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.iceberg;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.doris.common.jni.vec.ColumnValue;
+import org.apache.iceberg.MetadataTableType;
+import org.apache.iceberg.MetadataTableUtils;
+import org.apache.iceberg.StructLike;
+import org.apache.iceberg.TableScan;
+import org.apache.iceberg.io.CloseableIterator;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Streams;
+
+class IcebergSnapshotsJniScanner extends IcebergMetadataJniScanner {
+
+    private CloseableIterator<StructLike> reader;
+    private Map<String, Integer> columnNameToPosition = new HashMap<>();
+
+    public IcebergSnapshotsJniScanner(int batchSize, Map<String, String> params) {
+        super(batchSize, params);
+    }
+
+    @Override
+    protected void initReader() throws IOException {
+        TableScan tableScan = MetadataTableUtils.createMetadataTableInstance(table, MetadataTableType.SNAPSHOTS)
+                .newScan();
+        this.columnNameToPosition = Streams.mapWithIndex(tableScan.schema().columns().stream(),
+                (column, position) -> Maps.immutableEntry(column.name(), Long.valueOf(position).intValue()))
+                .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+        for (String requiredField : requiredFields) {
+            if (!columnNameToPosition.containsKey(requiredField)) {
+                throw new IOException("Invalid required field: " + requiredField);
+            }
+        }
+        this.reader = tableScan.planFiles().iterator().next().asDataTask().rows().iterator();
+    }
+
+    @Override
+    protected void openInner() {
+    }
+
+    @Override
+    protected int getNextInner() {
+        if (reader == null) {
+            return 0;
+        }
+        int rows = 0;
+        while (reader.hasNext() && rows < getBatchSize()) {
+            StructLike dataRow = reader.next();
+            for (int i = 0; i < requiredFields.length; i++) {
+                String columnName = requiredFields[i];
+                Object value = getValue(columnName, dataRow);
+                if (value == null) {
+                    appendData(i, null);
+                } else {
+                    ColumnValue columnValue = new IcebergMetadataColumnValue(value, timezone);
+                    appendData(i, columnValue);
+                }
+            }
+            rows++;
+        }
+        return rows;
+    }
+
+    @Override
+    protected void closeInner() throws IOException {
+        if (reader != null) {
+            reader.close();
+        }
+    }
+
+    private Object getValue(String columnName, StructLike dataRow) {
+        switch (columnName) {
+            case "committed_at":
+                return dataRow.get(columnNameToPosition.get(columnName), Long.class) / 1000;
+            case "snapshot_id":
+                return dataRow.get(columnNameToPosition.get(columnName), Long.class);
+            case "parent_id":
+                return dataRow.get(columnNameToPosition.get(columnName), Long.class);
+            case "operation":
+                return dataRow.get(columnNameToPosition.get(columnName), String.class);
+            case "manifest_list":
+                return dataRow.get(columnNameToPosition.get(columnName), String.class);
+            case "summary":
+                return dataRow.get(columnNameToPosition.get(columnName), Map.class);
+            default:
+                throw new IllegalArgumentException("Unrecognized column name " + columnName);
+        }
+    }
+}
