3053362ce551ad474a5905d09f801e08b5cb64f3
bobhan1 <baohan@selectdb.com>
2025-05-22 11:18:28 +0800

    update

--- fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -1074,6 +1074,8 @@ public class InternalCatalog implements CatalogIf<Database> {
         if (Config.isCloudMode()) {
             ((CloudGlobalTransactionMgr) Env.getCurrentGlobalTransactionMgr())
                     .clearTableLastTxnId(db.getId(), table.getId());
+            ((CloudGlobalTransactionMgr) Env.getCurrentGlobalTransactionMgr())
+                    .clearTableCachedDeleteBitmapLock(db.getId(), table.getId());
         }
     }
 
