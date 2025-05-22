4ea19ab6c19bcac87c01ef28a9f9a6eccc2eea86
zhangdong <zhangdong@selectdb.com>
2025-05-22 11:03:47 +0800

    [fix](case)fix mark_join and right_semi_mark_join has same table name (#51124)
    Related PR: #50720

--- regression-test/suites/query_p0/join/mark_join/mark_join.groovy
+++ regression-test/suites/query_p0/join/mark_join/mark_join.groovy
@@ -16,10 +16,17 @@
 // under the License.
 
 suite("mark_join") {
-    sql "drop table if exists t1;"
-    sql "drop table if exists t2;"
+    String suiteName = "mark_join"
+    String table_t1 = "${suiteName}_table_t1"
+    String table_t2 = "${suiteName}_table_t2"
+    String table_tbl1 = "${suiteName}_table_tbl1"
+    String table_tbl2 = "${suiteName}_table_tbl2"
+    String table_tbl3 = "${suiteName}_table_tbl3"
+    
+    sql "drop table if exists ${table_t1};"
+    sql "drop table if exists ${table_t2};"
     sql """
-        create table t1 (
+        create table ${table_t1} (
             k1 int null,
             k2 int null,
             k3 bigint null,
@@ -31,7 +38,7 @@ suite("mark_join") {
     """
 
     sql """
-        create table t2 (
+        create table ${table_t2} (
             k1 int null,
             k2 int null,
             k3 bigint null,
@@ -42,23 +49,23 @@ suite("mark_join") {
         properties("replication_num" = "1");
     """
 
-    sql "insert into t1 select 1,1,1,'a';"
-    sql "insert into t1 select 2,2,2,'b';"
-    sql "insert into t1 select 3,-3,null,'c';"
-    sql "insert into t1 select 3,3,null,'c';"
+    sql "insert into ${table_t1} select 1,1,1,'a';"
+    sql "insert into ${table_t1} select 2,2,2,'b';"
+    sql "insert into ${table_t1} select 3,-3,null,'c';"
+    sql "insert into ${table_t1} select 3,3,null,'c';"
 
-    sql "insert into t2 select 1,1,1,'a';"
-    sql "insert into t2 select 2,2,2,'b';"
-    sql "insert into t2 select 3,-3,null,'c';"
-    sql "insert into t2 select 3,3,null,'c';"
+    sql "insert into ${table_t2} select 1,1,1,'a';"
+    sql "insert into ${table_t2} select 2,2,2,'b';"
+    sql "insert into ${table_t2} select 3,-3,null,'c';"
+    sql "insert into ${table_t2} select 3,3,null,'c';"
 
     qt_test """
-    select * from t1 where exists (select t2.k3 from t2 where t1.k2 = t2.k2) or k1 < 10 order by k1, k2;
+    select * from ${table_t1} where exists (select ${table_t2}.k3 from ${table_t2} where ${table_t1}.k2 = ${table_t2}.k2) or k1 < 10 order by k1, k2;
     """
     qt_test """
-    select * from t1 where not exists (select t2.k3 from t2 where t1.k2 = t2.k2) or k1 < 10 order by k1, k2;
+    select * from ${table_t1} where not exists (select ${table_t2}.k3 from ${table_t2} where ${table_t1}.k2 = ${table_t2}.k2) or k1 < 10 order by k1, k2;
     """
     qt_test """
-    select * from t1 where t1.k1 not in (select t2.k3 from t2 where t2.k2 = t1.k2) or k1 < 10 order by k1, k2;
+    select * from ${table_t1} where ${table_t1}.k1 not in (select ${table_t2}.k3 from ${table_t2} where ${table_t2}.k2 = ${table_t1}.k2) or k1 < 10 order by k1, k2;
     """
 }
