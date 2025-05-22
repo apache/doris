de9b3adffd1bd826fc801370e37fbfaba30ad4e8
liaoxin <liaoxin@selectdb.com>
2025-05-22 14:44:18 +0000

    [chore](load) reduce log for data load

--- be/src/vec/sink/writer/vtablet_writer.cpp
+++ be/src/vec/sink/writer/vtablet_writer.cpp
@@ -127,8 +127,8 @@ Status IndexChannel::init(RuntimeState* state, const std::vector<TTabletWithPart
                 if (incremental) {
                     _has_inc_node = true;
                 }
-                LOG(INFO) << "init new node for instance " << _parent->_sender_id
-                          << ", node id:" << replica_node_id << ", incremantal:" << incremental;
+                VLOG_CRITICAL << "init new node for instance " << _parent->_sender_id
+                              << ", node id:" << replica_node_id << ", incremantal:" << incremental;
             } else {
                 channel = it->second;
             }
