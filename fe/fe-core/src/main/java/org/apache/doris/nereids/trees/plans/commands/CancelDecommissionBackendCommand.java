cef67d213fcd82fac2a550b67d2eb0049b3fa772
yx-keith <yx136264032@163.com>
2025-05-23 09:54:21 +0800

    add checkSupportedInCloudMode

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/CancelDecommissionBackendCommand.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/CancelDecommissionBackendCommand.java
@@ -20,6 +20,7 @@ package org.apache.doris.nereids.trees.plans.commands;
 import org.apache.doris.analysis.StmtType;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.AnalysisException;
+import org.apache.doris.common.DdlException;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.mysql.privilege.PrivPredicate;
@@ -31,6 +32,8 @@ import org.apache.doris.system.SystemInfoService;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Objects;
@@ -39,6 +42,7 @@ import java.util.Objects;
  * CancelDecommissionBackendCommand
  */
 public class CancelDecommissionBackendCommand extends CancelCommand {
+    private static final Logger LOG = LogManager.getLogger(CancelDecommissionBackendCommand.class);
     private final List<String> params;
     private final List<SystemInfoService.HostInfo> hostInfos;
     private final List<String> ids;
@@ -113,6 +117,12 @@ public class CancelDecommissionBackendCommand extends CancelCommand {
             }
         }
         return sb.toString();
+    }
+
+    @Override
+    protected void checkSupportedInCloudMode(ConnectContext ctx) throws DdlException {
+        LOG.info("AdminRepairTableCommand not supported in cloud mode");
+        throw new DdlException("Unsupported operation");
     }
 
     @Override
