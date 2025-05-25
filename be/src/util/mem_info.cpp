647925ef20980ab01a031e0bed8a72c01434ea22
Zou Xinyi <zouxinyi@selectdb.com>
2025-05-25 21:11:19 +0800

    1

--- be/src/util/mem_info.cpp
+++ be/src/util/mem_info.cpp
@@ -20,6 +20,8 @@
 
 #include "mem_info.h"
 
+#include "runtime/memory/cache_manager.h"
+
 #ifdef __APPLE__
 #include <sys/sysctl.h>
 #endif
@@ -159,6 +161,9 @@ void MemInfo::refresh_proc_meminfo() {
 
     // 2. if physical_mem changed, refresh mem limit and gc size.
     if (physical_mem > 0 && _s_physical_mem.load(std::memory_order_relaxed) != physical_mem) {
+        CacheManager::instance()->for_each_cache_reset_initial_capacity(physical_mem /
+                                                                        (_s_physical_mem * 1.0));
+
         _s_physical_mem.store(physical_mem);
 
         bool is_percent = true;
