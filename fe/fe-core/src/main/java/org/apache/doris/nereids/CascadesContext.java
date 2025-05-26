66624f860ae4bd0eecffdadaee58853bac18e268
zhangdong <zhangdong@selectdb.com>
2025-05-26 17:12:46 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/nereids/CascadesContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/CascadesContext.java
@@ -143,7 +143,8 @@ public class CascadesContext implements ScheduleContext {
         this.jobScheduler = new SimpleJobScheduler();
         this.currentJobContext = new JobContext(this, requireProperties, Double.MAX_VALUE);
         this.subqueryExprIsAnalyzed = new HashMap<>();
-        this.runtimeFilterContext = new RuntimeFilterContext(getConnectContext().getSessionVariable());
+        this.runtimeFilterContext = new RuntimeFilterContext(
+                getConnectContext() == null ? new SessionVariable() : getConnectContext().getSessionVariable());
         this.materializationContexts = new HashSet<>();
         if (statementContext.getConnectContext() != null) {
             ConnectContext connectContext = statementContext.getConnectContext();
