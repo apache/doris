454a0bdba7652bb853e28ae91309fa777eea3c5c
feiniaofeiafei <moailing@selectdb.com>
2025-05-27 17:23:33 +0800

    add some functions

--- fe/fe-core/src/test/java/org/apache/doris/nereids/properties/UniqueTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/properties/UniqueTest.java
@@ -69,7 +69,7 @@ class UniqueTest extends TestWithFeService {
         Assertions.assertTrue(plan.getLogicalProperties().getTrait()
                 .isUnique(plan.getOutput().get(0)));
         plan = PlanChecker.from(connectContext)
-                .analyze("select id, sum(id), avg(id), max(id), min(id), sum0(id) from agg group by id")
+                .analyze("select id, sum(id), avg(id), max(id), min(id), topn(id,2) from agg group by id")
                 .rewrite()
                 .getPlan();
         Assertions.assertTrue(plan.getLogicalProperties().getTrait()
