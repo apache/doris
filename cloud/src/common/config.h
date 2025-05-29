56365c0086d45a38e07289b4683c0626490565bf
meiyi <meiyi@selectdb.com>
2025-05-29 09:46:38 +0800

    [improve](cloud-mow) batch get tablet stats when get_delete_bitmap_update_lock (#47281)

--- cloud/src/common/config.h
+++ cloud/src/common/config.h
@@ -296,6 +296,9 @@ CONF_mBool(use_delete_bitmap_lock_random_version, "false");
 // Comma seprated list: recycler_storage_vault_white_list="aaa,bbb,ccc"
 CONF_Strings(recycler_storage_vault_white_list, "");
 
+// for get_delete_bitmap_update_lock
+CONF_mBool(enable_batch_get_mow_tablet_stats_and_meta, "true");
+
 // aws sdk log level
 //    Off = 0,
 //    Fatal = 1,
