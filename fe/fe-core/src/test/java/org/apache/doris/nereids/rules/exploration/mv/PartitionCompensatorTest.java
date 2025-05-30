278aef2814269ef4861f3f5b6f147ad54bd140f2
seawinde <wusi@selectdb.com>
2025-05-30 09:58:56 +0800

    fix ut 2

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/mv/PartitionCompensatorTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/mv/PartitionCompensatorTest.java
@@ -95,7 +95,7 @@ public class PartitionCompensatorTest extends TestWithFeService {
 
         // Should not make scan to empty relation when the table used by materialized view has no data
         connectContext.getSessionVariable().setDisableNereidsRules(
-                "OLAP_SCAN_PARTITION_PRUNE,PRUNE_EMPTY_PARTITION,ELIMINATE_GROUP_BY_KEY_BY_UNIFORM");
+                "OLAP_SCAN_PARTITION_PRUNE,PRUNE_EMPTY_PARTITION");
     }
 
     // Test when join both side are all partition table and partition column name is same
