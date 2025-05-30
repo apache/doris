4a660c8d32c72b3fa4e41f16955bc9cec9c4b437
Siyang Tang <tangsiyang@selectdb.com>
2025-05-30 17:16:46 +0800

    [fix](compaction) Add read lock for compaction score calculation

--- be/src/olap/base_tablet.cpp
+++ be/src/olap/base_tablet.cpp
@@ -23,6 +23,7 @@
 #include <cstdint>
 #include <iterator>
 #include <random>
+#include <shared_mutex>
 
 #include "common/cast_set.h"
 #include "common/logging.h"
@@ -196,6 +197,7 @@ Status BaseTablet::update_by_least_common_schema(const TabletSchemaSPtr& update_
 }
 
 uint32_t BaseTablet::get_real_compaction_score() const {
+    std::shared_lock l(_meta_lock);
     const auto& rs_metas = _tablet_meta->all_rs_metas();
     return std::accumulate(rs_metas.begin(), rs_metas.end(), 0,
                            [](uint32_t score, const RowsetMetaSharedPtr& rs_meta) {
