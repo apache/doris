a65e0bab8cf25de41012dcb35c19b3a47e662280
Jerry Hu <hushenggang@selectdb.com>
2025-05-30 16:11:40 +0800

    [fix](agg_state) Add new version of function map_agg (#51343)
    ### What problem does this PR solve?
    
    The old version of `map_agg` had issues handling null values and did not
    support null as a key.
    
    The new version function `mag_agg_v2` support null as key and fixed the
    null value issues.
    
    ```
    select k2, map_agg(v1, v2) m from `test_map_agg_2` group  by k2;
    +------+----------------------------------------+
    | k2   | m                                      |
    +------+----------------------------------------+
    |    6 | {null:null}                            |
    |    3 | {null:"3", "c":"c"}                    |
    |    1 | {"e":"f", "k":"j", "a":"b", "ee":"nn"} |
    |    2 | {"a":"a2", null:"2", "b":"bddd"}       |
    +------+----------------------------------------+
    ```
    > `map_agg` is the alias of `map_agg_v2`. Use `map_agg_v1` to access the
    old version of this function.
    
    ```
    select k2, map_agg_v1(v1, v2) m from `test_map_agg_2` group  by k2;
    +------+----------------------------------------+
    | k2   | m                                      |
    +------+----------------------------------------+
    |    6 | {}                                     |
    |    3 | {"c":"c"}                              |
    |    1 | {"k":"j", "e":"f", "a":"b", "ee":"nn"} |
    |    2 | {"a":"a3", "b":"bddd"}                 |
    +------+----------------------------------------+
    ```

--- be/src/vec/core/field.h
+++ be/src/vec/core/field.h
@@ -472,6 +472,83 @@ public:
         }
     }
 
+    std::string to_string() const {
+        std::string res;
+        switch (type) {
+        case PrimitiveType::TYPE_DATETIMEV2: {
+            auto v = get<UInt64>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DATETIME:
+        case PrimitiveType::TYPE_DATE:
+        case PrimitiveType::TYPE_BIGINT: {
+            auto v = get<Int64>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_LARGEINT: {
+            auto v = get<Int128>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_IPV6: {
+            auto v = get<IPv6>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DOUBLE: {
+            auto v = get<Float64>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_STRING:
+        case PrimitiveType::TYPE_CHAR:
+        case PrimitiveType::TYPE_VARCHAR: {
+            res = get<String>();
+            break;
+        }
+        case PrimitiveType::TYPE_DECIMAL32: {
+            auto v = get<DecimalField<Decimal32>>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DECIMAL64: {
+            auto v = get<DecimalField<Decimal64>>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DECIMALV2: {
+            auto v = get<DecimalField<Decimal128V2>>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DECIMAL128I: {
+            auto v = get<DecimalField<Decimal128V3>>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        case PrimitiveType::TYPE_DECIMAL256: {
+            auto v = get<DecimalField<Decimal256>>();
+            res.resize(sizeof(v));
+            memcpy(res.data(), &v, sizeof(v));
+            break;
+        }
+        default:
+            throw Exception(Status::FatalError("type not supported, type={}", get_type_name()));
+        }
+        return res;
+    }
+
 private:
     std::aligned_union_t<DBMS_MIN_FIELD_SIZE - sizeof(PrimitiveType), Null, UInt64, UInt128, Int64,
                          Int128, IPv6, Float64, String, JsonbField, Array, Tuple, Map, VariantMap,
@@ -651,3 +728,14 @@ decltype(auto) cast_to_nearest_field_type(T&& x) {
 }
 
 } // namespace doris::vectorized
+
+template <>
+struct std::hash<doris::vectorized::Field> {
+    size_t operator()(const doris::vectorized::Field& field) const {
+        if (field.is_null()) {
+            return 0;
+        }
+        std::hash<std::string> hasher;
+        return hasher(field.to_string());
+    }
+};
