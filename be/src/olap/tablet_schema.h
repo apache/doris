f664764cefe0918f5319f93e3bdd7081cda9f061
yiguolei <guolei@selectdb.com>
2025-05-31 21:50:15 +0800

    [refactor](be) rename field id to field unique id (#51383)
    ### What problem does this PR solve?
    
    field id is very ambiguous because column id is different from column
    unique id, so I rename it to unique id to make the code more clear.

--- be/src/olap/tablet_schema.h
+++ be/src/olap/tablet_schema.h
@@ -566,7 +566,7 @@ private:
     std::vector<TabletIndexPtr> _indexes;
     std::vector<Cache::Handle*> _index_cache_handlers;
     std::unordered_map<StringRef, int32_t, StringRefHash> _field_name_to_index;
-    std::unordered_map<int32_t, int32_t> _field_id_to_index;
+    std::unordered_map<int32_t, int32_t> _field_uniqueid_to_index;
     std::unordered_map<vectorized::PathInDataRef, int32_t, vectorized::PathInDataRef::Hash>
             _field_path_to_index;
 
