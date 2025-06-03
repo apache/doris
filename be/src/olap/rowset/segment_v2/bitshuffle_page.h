82ac8a981153fcc7ae8d60fc558d9ba2e7132a18
yiguolei <guolei@selectdb.com>
2025-06-03 15:52:23 +0800

    f

--- be/src/olap/rowset/segment_v2/bitshuffle_page.h
+++ be/src/olap/rowset/segment_v2/bitshuffle_page.h
@@ -329,7 +329,7 @@ public:
         DCHECK(_parsed) << "Must call init()";
         if (PREDICT_FALSE(_num_elements == 0)) {
             DCHECK_EQ(0, pos);
-            return Status::Error<ErrorCode::INVALID_ARGUMENT, true>(
+            return Status::Error<ErrorCode::INTERNAL_ERROR, true>(
                     "seek pos {} is larger than total elements {}", pos, _num_elements);
         }
 
