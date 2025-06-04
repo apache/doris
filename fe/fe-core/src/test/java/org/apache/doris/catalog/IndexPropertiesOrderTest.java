015e3fc1b1bbc03fd320ec9fadfd789f5c8abd48
airborne12 <jiangkai@selectdb.com>
2025-06-04 12:07:08 +0800

    [enhancement](inverted index) ensure consistent ordering of properties for inverted index show

--- fe/fe-core/src/test/java/org/apache/doris/catalog/IndexPropertiesOrderTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/IndexPropertiesOrderTest.java
@@ -24,8 +24,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class IndexPropertiesOrderTest {
