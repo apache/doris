d4a40e38fe2808c58252aa6f1e874c17487bd39a
meiyi <meiyi@selectdb.com>
2025-06-06 15:31:24 +0800

    [fix](mow) remove enable_delete_bitmap_merge_on_compaction (#51479)
    Problem Summary:
    
    remove code of https://github.com/apache/doris/pull/40204/ and
    https://github.com/apache/doris/pull/41636/

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1472,7 +1472,6 @@ DEFINE_mInt32(check_score_rounds_num, "1000");
 
 DEFINE_Int32(query_cache_size, "512");
 
-DEFINE_mBool(enable_delete_bitmap_merge_on_compaction, "false");
 // Enable validation to check the correctness of table size.
 DEFINE_Bool(enable_table_size_correctness_check, "false");
 DEFINE_Bool(force_regenerate_rowsetid_on_start_error, "false");
