aeaa02737461b98d273291f63c03dcfbda1efa63
stephen <lidongyang@selectdb.com>
2025-06-11 19:42:47 +0800

    2

--- regression-test/pipeline/nonConcurrent/conf/regression-conf.groovy
+++ regression-test/pipeline/nonConcurrent/conf/regression-conf.groovy
@@ -58,11 +58,14 @@ testSuites = ""
 testDirectories = ""
 
 // this groups will not be executed
-excludeGroups = "p2"
+excludeGroups = "p1,p2"
 // this suites will not be executed
-// load_stream_fault_injection may cause bad disk
 
-excludeSuites = ""
+// this suites will not be executed
+excludeSuites = "000_the_start_sentinel_do_not_touch," + // keep this line as the first line
+    "test_write_inverted_index_exception_fault_injection," + // cause core dump
+    "zzz_the_end_sentinel_do_not_touch"// keep this line as the last line
+
 // this directories will not be executed
 excludeDirectories = "000_the_start_sentinel_do_not_touch," + // keep this line as the first line
     "variant_github_events_nonConcurrent_p2," +
