45787b25ad9ccfa0f45c9cb576d4a6277242d78a
BiteTheDDDDt <xl@selectdb.com>
2025-06-12 20:48:34 +0800

    update

--- be/src/olap/rowset/segment_v2/inverted_index_writer.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_writer.cpp
@@ -529,9 +529,9 @@ public:
                                         "debug point: add_array_values_create_field_error_2");
                             })
                     if (st != Status::OK()) {
-                        LOG(ERROR)
-                                << "create field " << std::string(_field_name.begin(), _field_name.end())
-                                << " error:" << st;
+                        LOG(ERROR) << "create field "
+                                   << std::string(_field_name.begin(), _field_name.end())
+                                   << " error:" << st;
                         return st;
                     }
                     _doc->add(*new_field);
