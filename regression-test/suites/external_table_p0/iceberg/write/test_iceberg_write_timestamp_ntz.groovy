428da1e3953505f58e8eac2e2c09188469a03423
morningman <yunyou@selectdb.com>
2025-06-13 22:51:47 +0800

    fix ut

--- regression-test/suites/external_table_p0/iceberg/write/test_iceberg_write_timestamp_ntz.groovy
+++ regression-test/suites/external_table_p0/iceberg/write/test_iceberg_write_timestamp_ntz.groovy
@@ -24,42 +24,45 @@ suite("test_iceberg_write_timestamp_ntz", "p0,external,iceberg,external_docker,e
     }
 
   
-        try {
+    try {
 
-            String rest_port = context.config.otherConfigs.get("iceberg_rest_uri_port")
-            String minio_port = context.config.otherConfigs.get("iceberg_minio_port")
-            String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
-            String catalog_name = "iceberg_timestamp_ntz_test"
+        String rest_port = context.config.otherConfigs.get("iceberg_rest_uri_port")
+        String minio_port = context.config.otherConfigs.get("iceberg_minio_port")
+        String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
+        String catalog_name = "iceberg_timestamp_ntz_test"
 
-            sql """drop catalog if exists ${catalog_name}"""
-            sql """
-            CREATE CATALOG ${catalog_name} PROPERTIES (
-                   'type'='iceberg',
-                   'iceberg.catalog.type'='rest',
-                   'uri' = 'http://${externalEnvIp}:${rest_port}',
-                   "s3.access_key" = "admin",
-                   "s3.secret_key" = "password",
-                   "s3.endpoint" = "http://${externalEnvIp}:${minio_port}",
-                   "s3.region" = "us-east-1"
-             );"""
+        sql """drop catalog if exists ${catalog_name}"""
+        sql """
+        CREATE CATALOG ${catalog_name} PROPERTIES (
+               'type'='iceberg',
+               'iceberg.catalog.type'='rest',
+               'uri' = 'http://${externalEnvIp}:${rest_port}',
+               "s3.access_key" = "admin",
+               "s3.secret_key" = "password",
+               "s3.endpoint" = "http://${externalEnvIp}:${minio_port}",
+               "s3.region" = "us-east-1"
+         );"""
 
-            logger.info("catalog " + catalog_name + " created")
-            sql """switch ${catalog_name};"""
-            logger.info("switched to catalog " + catalog_name)
-            sql """ use test_db;""" 
+        logger.info("catalog " + catalog_name + " created")
+        sql """switch ${catalog_name};"""
+        logger.info("switched to catalog " + catalog_name)
+        sql """ use test_db;""" 
 
-            sql """INSERT INTO t_ntz_doris VALUES ('2025-02-07 20:12:00');"""
-            sql """INSERT INTO t_tz_doris VALUES ('2025-02-07 20:12:00');"""
+        sql """INSERT INTO t_ntz_doris VALUES ('2025-02-07 20:12:00');"""
+        sql """INSERT INTO t_tz_doris VALUES ('2025-02-07 20:12:01');"""
 
-         
-            qt_timestamp_ntz """select * from t_ntz_doris;"""
-            qt_timestamp_tz  """select * from t_tz_doris;"""
-  
-          
-            sql """drop catalog if exists ${catalog_name}"""
-  
+     
+        sql "set time_zone = 'Asia/Shanghai'"
+        qt_timestamp_ntz """select * from t_ntz_doris;"""
+        qt_timestamp_tz  """select * from t_tz_doris;"""
 
-        } finally {
+        sql "set time_zone = 'Europe/Tirane'"
+        qt_timestamp_ntz2 """select * from t_ntz_doris;"""
+        qt_timestamp_tz2  """select * from t_tz_doris;"""
+      
+        // sql """drop catalog if exists ${catalog_name}"""
 
-        }
+    } finally {
+
+    }
 }
