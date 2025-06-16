9b30eb948337eac03c115a047ba9909f213521dd
daidai <changyuwei@selectdb.com>
2025-06-16 14:14:01 +0800

    [fix](hudi)fix hudi get corrent schema.

--- fe/fe-core/src/main/java/org/apache/doris/datasource/hudi/source/HudiScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/hudi/source/HudiScanNode.java
@@ -56,11 +56,13 @@ import org.apache.hudi.common.model.FileSlice;
 import org.apache.hudi.common.model.HoodieBaseFile;
 import org.apache.hudi.common.model.HoodieLogFile;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.TableSchemaResolver;
 import org.apache.hudi.common.table.timeline.HoodieInstant;
 import org.apache.hudi.common.table.timeline.HoodieTimeline;
 import org.apache.hudi.common.table.view.HoodieTableFileSystemView;
 import org.apache.hudi.common.util.Option;
 import org.apache.hudi.internal.schema.InternalSchema;
+import org.apache.hudi.internal.schema.convert.AvroInternalSchemaConverter;
 import org.apache.hudi.storage.StoragePath;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -275,10 +277,20 @@ public class HudiScanNode extends HiveScanNode {
                         new File(hudiSplit.getPath().get()).getName()));
                 InternalSchema internalSchema = hudiSchemaCacheValue
                         .getCommitInstantInternalSchema(hudiClient, commitInstantTime);
-                params.history_schema_info.computeIfAbsent(
-                        internalSchema.schemaId(),
-                        k -> HudiUtils.getSchemaInfo(internalSchema));
-                fileDesc.setSchemaId(internalSchema.schemaId()); //for schema change. (native reader)
+                putHistorySchemaInfo(internalSchema); //for schema change. (native reader)
+                fileDesc.setSchemaId(internalSchema.schemaId());
+            } else {
+                try {
+                    TableSchemaResolver schemaUtil = new TableSchemaResolver(hudiClient);
+                    InternalSchema internalSchema =
+                            AvroInternalSchemaConverter.convert(schemaUtil.getTableAvroSchema(true));
+                    params.history_schema_info.computeIfAbsent(
+                            internalSchema.schemaId(),
+                            k -> HudiUtils.getSchemaInfo(internalSchema)); // Handle column name case for BE.
+                    fileDesc.setSchemaId(internalSchema.schemaId());
+                } catch (Exception e) {
+                    throw new RuntimeException("Cannot get hudi table schema.", e);
+                }
             }
         }
         tableFormatFileDesc.setHudiParams(fileDesc);
