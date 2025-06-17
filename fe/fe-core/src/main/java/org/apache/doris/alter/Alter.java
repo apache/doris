8654b9130a1351224a1209a7af215aba13645550
camby <104178625@qq.com>
2025-06-17 11:18:47 +0800

    [fix](alter) alter partition without storage_policy property will also cancel storage_policy (#51662)

--- fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/Alter.java
@@ -860,7 +860,9 @@ public class Alter {
                 // check currentStoragePolicy resource exist.
                 Env.getCurrentEnv().getPolicyMgr().checkStoragePolicyExist(currentStoragePolicy);
                 partitionInfo.setStoragePolicy(partition.getId(), currentStoragePolicy);
-            } else {
+            } else if (PropertyAnalyzer.hasStoragePolicy(properties)) {
+                // only set "storage_policy" = "", means cancel storage policy
+                // if current partition is already in remote storage
                 if (partition.getRemoteDataSize() > 0) {
                     throw new AnalysisException(
                         "Cannot cancel storage policy for partition which is already on code storage.");
