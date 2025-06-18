8e1ad51595bbf1012deb2451cd222d6c5d1f8483
csding <csding18@qq.com>
2025-06-18 11:26:47 +0800

    [Chore](nereids) Remove GrantStmt and RevokeStmt

--- fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Auth.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/privilege/Auth.java
@@ -24,12 +24,10 @@ import org.apache.doris.analysis.CreateRoleStmt;
 import org.apache.doris.analysis.CreateUserStmt;
 import org.apache.doris.analysis.DropRoleStmt;
 import org.apache.doris.analysis.DropUserStmt;
-import org.apache.doris.analysis.GrantStmt;
 import org.apache.doris.analysis.PasswordOptions;
 import org.apache.doris.analysis.RefreshLdapStmt;
 import org.apache.doris.analysis.ResourcePattern;
 import org.apache.doris.analysis.ResourceTypeEnum;
-import org.apache.doris.analysis.RevokeStmt;
 import org.apache.doris.analysis.SetLdapPassVar;
 import org.apache.doris.analysis.SetPassVar;
 import org.apache.doris.analysis.SetUserPropertyStmt;
@@ -656,25 +654,6 @@ public class Auth implements Writable {
         }
     }
 
-    // grant
-    public void grant(GrantStmt stmt) throws DdlException {
-        if (stmt.getTblPattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            grantInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getTblPattern(), privs,
-                    stmt.getColPrivileges(), true /* err on non exist */, false /* not replay */);
-        } else if (stmt.getResourcePattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            grantInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getResourcePattern(), privs,
-                    true /* err on non exist */, false /* not replay */);
-        } else if (stmt.getWorkloadGroupPattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            grantInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getWorkloadGroupPattern(), privs,
-                    true /* err on non exist */, false /* not replay */);
-        } else {
-            grantInternal(stmt.getUserIdent(), stmt.getRoles(), false);
-        }
-    }
-
     public void grantRoleCommand(GrantRoleCommand command) throws DdlException {
         grantInternal(command.getUserIdentity(), command.getRoles(), false);
     }
@@ -874,25 +853,6 @@ public class Auth implements Writable {
             return null;
         } finally {
             readUnlock();
-        }
-    }
-
-    // revoke
-    public void revoke(RevokeStmt stmt) throws DdlException {
-        if (stmt.getTblPattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            revokeInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getTblPattern(), privs,
-                    stmt.getColPrivileges(), true /* err on non exist */, false /* is replay */);
-        } else if (stmt.getResourcePattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            revokeInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getResourcePattern(), privs,
-                    true /* err on non exist */, false /* is replay */);
-        } else if (stmt.getWorkloadGroupPattern() != null) {
-            PrivBitSet privs = PrivBitSet.of(stmt.getPrivileges());
-            revokeInternal(stmt.getUserIdent(), stmt.getQualifiedRole(), stmt.getWorkloadGroupPattern(), privs,
-                    true /* err on non exist */, false /* is replay */);
-        } else {
-            revokeInternal(stmt.getUserIdent(), stmt.getRoles(), false);
         }
     }
 
--- fe/fe-core/src/main/java/org/apache/doris/qe/DdlExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/DdlExecutor.java
@@ -100,7 +100,6 @@ import org.apache.doris.analysis.DropTableStmt;
 import org.apache.doris.analysis.DropUserStmt;
 import org.apache.doris.analysis.DropWorkloadGroupStmt;
 import org.apache.doris.analysis.DropWorkloadSchedPolicyStmt;
-import org.apache.doris.analysis.GrantStmt;
 import org.apache.doris.analysis.InstallPluginStmt;
 import org.apache.doris.analysis.KillAnalysisJobStmt;
 import org.apache.doris.analysis.PauseRoutineLoadStmt;
@@ -113,7 +112,6 @@ import org.apache.doris.analysis.RefreshLdapStmt;
 import org.apache.doris.analysis.RefreshTableStmt;
 import org.apache.doris.analysis.RestoreStmt;
 import org.apache.doris.analysis.ResumeRoutineLoadStmt;
-import org.apache.doris.analysis.RevokeStmt;
 import org.apache.doris.analysis.SetDefaultStorageVaultStmt;
 import org.apache.doris.analysis.SetUserPropertyStmt;
 import org.apache.doris.analysis.StopRoutineLoadStmt;
@@ -233,12 +231,6 @@ public class DdlExecutor {
         } else if (ddlStmt instanceof DropUserStmt) {
             DropUserStmt stmt = (DropUserStmt) ddlStmt;
             env.getAuth().dropUser(stmt);
-        } else if (ddlStmt instanceof GrantStmt) {
-            GrantStmt stmt = (GrantStmt) ddlStmt;
-            env.getAuth().grant(stmt);
-        } else if (ddlStmt instanceof RevokeStmt) {
-            RevokeStmt stmt = (RevokeStmt) ddlStmt;
-            env.getAuth().revoke(stmt);
         } else if (ddlStmt instanceof CreateRoleStmt) {
             env.getAuth().createRole((CreateRoleStmt) ddlStmt);
         } else if (ddlStmt instanceof AlterRoleStmt) {
