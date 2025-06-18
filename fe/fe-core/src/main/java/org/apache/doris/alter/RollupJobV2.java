cb8da7887d95e543b05fbc33f25c44a2e38ef876
zhangdong <zhangdong@selectdb.com>
2025-06-18 15:30:55 +0800

    roll up

--- fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
@@ -18,15 +18,13 @@
 package org.apache.doris.alter;
 
 import org.apache.doris.analysis.Analyzer;
-import org.apache.doris.analysis.CreateMaterializedViewStmt;
 import org.apache.doris.analysis.DescriptorTable;
 import org.apache.doris.analysis.Expr;
 import org.apache.doris.analysis.MVColumnItem;
 import org.apache.doris.analysis.SlotDescriptor;
 import org.apache.doris.analysis.SlotRef;
-import org.apache.doris.analysis.SqlParser;
-import org.apache.doris.analysis.SqlScanner;
 import org.apache.doris.analysis.TupleDescriptor;
+import org.apache.doris.analysis.UserIdentity;
 import org.apache.doris.catalog.BinlogConfig;
 import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.Database;
@@ -51,13 +49,15 @@ import org.apache.doris.common.MetaNotFoundException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.io.Text;
 import org.apache.doris.common.util.DbUtil;
-import org.apache.doris.common.util.SqlParserUtils;
 import org.apache.doris.common.util.TimeUtils;
+import org.apache.doris.mysql.privilege.Auth;
+import org.apache.doris.nereids.StatementContext;
+import org.apache.doris.nereids.parser.NereidsParser;
+import org.apache.doris.nereids.trees.plans.commands.CreateMaterializedViewCommand;
 import org.apache.doris.persist.gson.GsonPostProcessable;
 import org.apache.doris.persist.gson.GsonUtils;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.OriginStatement;
-import org.apache.doris.qe.SqlModeHelper;
 import org.apache.doris.task.AgentBatchTask;
 import org.apache.doris.task.AgentTask;
 import org.apache.doris.task.AgentTaskExecutor;
@@ -83,7 +83,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.DataOutput;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -904,7 +903,9 @@ public class RollupJobV2 extends AlterJobV2 implements GsonPostProcessable {
         for (MVColumnItem mvColumnItem : mvColumnItemList) {
             for (Column column : rollupSchema) {
                 if (column.getName().equals(mvColumnItem.getName())) {
-                    column.setDefineExpr(mvColumnItem.getDefineExpr());
+                    Expr defineExpr = mvColumnItem.getDefineExpr();
+                    defineExpr.setDisableTableName(true);
+                    column.setDefineExpr(defineExpr);
                     break;
                 }
             }
@@ -928,25 +929,44 @@ public class RollupJobV2 extends AlterJobV2 implements GsonPostProcessable {
             return;
         }
         // parse the define stmt to schema
-        SqlParser parser = new SqlParser(new SqlScanner(
-                new StringReader(origStmt.originStmt), SqlModeHelper.MODE_DEFAULT));
-        CreateMaterializedViewStmt stmt = null;
+        CreateMaterializedViewCommand command = null;
         try {
-            initAnalyzer();
-            stmt = (CreateMaterializedViewStmt) SqlParserUtils.getStmt(parser, origStmt.idx);
-            stmt.setIsReplay(true);
-            stmt.analyze(analyzer);
+            Database db;
+            try {
+                db = Env.getCurrentInternalCatalog().getDbOrMetaException(dbId);
+            } catch (MetaNotFoundException e) {
+                throw new AnalysisException("error happens when parsing create materialized view stmt: " + origStmt, e);
+            }
+
+            NereidsParser nereidsParser = new NereidsParser();
+            command = (CreateMaterializedViewCommand) nereidsParser.parseSingle(
+                    origStmt.originStmt);
+            ConnectContext ctx = new ConnectContext();
+            ctx.setDatabase(db.getFullName());
+            StatementContext statementContext = new StatementContext();
+            statementContext.setConnectContext(ctx);
+            ctx.setStatementContext(statementContext);
+            ctx.setEnv(Env.getCurrentEnv());
+            ctx.setQualifiedUser(Auth.ADMIN_USER);
+            ctx.setCurrentUserIdentity(UserIdentity.ADMIN);
+            ctx.getState().reset();
+            try {
+                ctx.setThreadLocalInfo();
+                command.validate(ctx);
+            } finally {
+                ctx.cleanup();
+            }
         } catch (Exception e) {
             // Under normal circumstances, the stmt will not fail to analyze.
             // In some cases (such as drop table force), analyze may fail because cancel is
             // not included in the checkpoint.
             jobState = JobState.CANCELLED;
-            LOG.warn("error happens when parsing create materialized view stmt: " + stmt, e);
+            LOG.warn("error happens when parsing create materialized view command: " + command, e);
             return;
         }
-        setColumnsDefineExpr(stmt.getMVColumnItemList());
+        setColumnsDefineExpr(command.getMVColumnItemList());
         if (whereColumn != null) {
-            whereColumn.setDefineExpr(stmt.getWhereClause());
+            whereColumn.setDefineExpr(command.getWhereClauseItem().getDefineExpr());
         }
     }
 
