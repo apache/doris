61fc766751be760ad6e4c7dfead8525e10176b74
englefly <englefly@gmail.com>
2025-06-18 15:32:58 +0800

    pick

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/CompoundPredicate.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/CompoundPredicate.java
@@ -18,25 +18,32 @@
 package org.apache.doris.nereids.trees.expressions;
 
 import org.apache.doris.nereids.exceptions.UnboundException;
+import org.apache.doris.nereids.trees.expressions.functions.ExpressionTrait;
+import org.apache.doris.nereids.trees.expressions.typecoercion.ExpectsInputTypes;
 import org.apache.doris.nereids.trees.expressions.visitor.ExpressionVisitor;
 import org.apache.doris.nereids.types.BooleanType;
 import org.apache.doris.nereids.types.DataType;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Compound predicate expression.
- * Such as &&,||,AND,OR.
+ * Such as AND,OR.
  */
-public abstract class CompoundPredicate extends BinaryOperator {
+public abstract class CompoundPredicate extends Expression implements ExpectsInputTypes {
+    protected final List<Expression> flattenChildren = new ArrayList<>();
+    private String symbol;
 
     public CompoundPredicate(List<Expression> children, String symbol) {
-        super(children, symbol);
+        super(children);
+        this.symbol = symbol;
     }
 
     @Override
     public boolean nullable() throws UnboundException {
-        return left().nullable() || right().nullable();
+        return children.stream().anyMatch(ExpressionTrait::nullable);
     }
 
     @Override
@@ -50,8 +57,8 @@ public abstract class CompoundPredicate extends BinaryOperator {
     }
 
     @Override
-    public DataType inputType() {
-        return BooleanType.INSTANCE;
+    public List<DataType> expectedInputTypes() {
+        return children.stream().map(c -> BooleanType.INSTANCE).collect(Collectors.toList());
     }
 
     /**
@@ -62,8 +69,69 @@ public abstract class CompoundPredicate extends BinaryOperator {
     /**
      * Flip logical `and` and `or` operator with new children.
      */
-    public abstract CompoundPredicate flip(Expression left, Expression right);
+    public abstract CompoundPredicate flip(List<Expression> children);
 
     public abstract Class<? extends CompoundPredicate> flipType();
 
+    protected abstract List<Expression> extract();
+
+    @Override
+    public boolean equals(Object o) {
+        if (compareWidthAndDepth) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            List<Expression> thisChildren = this.children();
+            List<Expression> thatChildren = ((CompoundPredicate) o).children();
+            if (thisChildren.size() != thatChildren.size()) {
+                return false;
+            }
+            for (int i = 0; i < thisChildren.size(); i++) {
+                if (!thisChildren.get(i).equals(thatChildren.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return super.equals(o);
+        }
+    }
+
+    @Override
+    public String toSql() {
+        StringBuilder sb = new StringBuilder();
+        children().forEach(c -> sb.append(c.toSql()).append(","));
+        sb.deleteCharAt(sb.length() - 1);
+        return symbol + "[" + sb + "]";
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        children().forEach(c -> sb.append(c.toString()).append(","));
+        sb.deleteCharAt(sb.length() - 1);
+        return symbol + "[" + sb + "]";
+    }
+
+    @Override
+    public String shapeInfo() {
+        StringBuilder sb = new StringBuilder();
+        children().forEach(c -> sb.append(c.shapeInfo()).append(","));
+        sb.deleteCharAt(sb.length() - 1);
+        return symbol + "[" + sb + "]";
+    }
+
+    @Override
+    public int arity() {
+        // get flattern children
+        return children().size();
+    }
+
+    @Override
+    public Expression child(int index) {
+        return children().get(index);
+    }
 }
