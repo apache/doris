314bed8c9ed3d70d35477fa74317470e1f637f05
BiteTheDDDDt <xl@selectdb.com>
2025-06-18 11:39:36 +0800

    format

--- be/test/exprs/hybrid_set_test.cpp
+++ be/test/exprs/hybrid_set_test.cpp
@@ -442,77 +442,63 @@ TEST_F(HybridSetTest, string) {
     EXPECT_FALSE(set->find(&b));
 }
 
-#define TEST_FIXED_CONTAINER(N)                                                         \
-    std::unique_ptr<HybridSetBase> set(create_set<N>(PrimitiveType::TYPE_INT, false));  \
-                                                                                        \
-    auto column = vectorized::ColumnHelper::create_column<vectorized::DataTypeInt32>(   \
-            {1, 2, 3, 4, 5, 6, 7, 8});                                                  \
-    auto result_column = vectorized::ColumnUInt8::create(N, 0);                         \
-    try {                                                                               \
-        set->find_batch(*column, N, result_column->get_data());                         \
-        ASSERT_TRUE(false) << "should not be here";                                     \
-    } catch (...) {                                                                     \
-    }                                                                                   \
-                                                                                        \
-    for (size_t i = 0; i != N; ++i) {                                                   \
-        set->insert(&i);                                                                \
-    }                                                                                   \
-                                                                                        \
-    for (size_t i = 0; i != N; ++i) {                                                   \
-        ASSERT_TRUE(set->find(&i));                                                     \
-    }                                                                                   \
-                                                                                        \
-    for (size_t i = N; i != 1024; ++i) {                                                \
-        ASSERT_FALSE(set->find(&i));                                                    \
-    }                                                                                   \
-                                                                                        \
-    std::unique_ptr<HybridSetBase> set2(create_set<N>(PrimitiveType::TYPE_INT, false)); \
-    set2->insert(set.get());                                                            \
-                                                                                        \
-    for (size_t i = 0; i != N; ++i) {                                                   \
-        ASSERT_TRUE(set2->find(&i));                                                    \
-    }                                                                                   \
-                                                                                        \
-    for (size_t i = N; i != 1024; ++i) {                                                \
-        ASSERT_FALSE(set2->find(&i));                                                   \
-    }                                                                                   \
-                                                                                        \
-    auto it = set->begin();                                                             \
-    while (it->has_next()) {                                                            \
-        auto value = *(int*)it->get_value();                                            \
-        ASSERT_TRUE(set2->find(&value)) << "cannot find: " << value;                    \
-        it->next();                                                                     \
-    }                                                                                   \
-    PInFilter in_filter;                                                                \
-    set->to_pb(&in_filter);                                                             \
-    set->clear();                                                                       \
-    ASSERT_EQ(set->size(), 0);
+#define TEST_FIXED_CONTAINER(N)                                                             \
+    {                                                                                       \
+        std::unique_ptr<HybridSetBase> set(create_set<N>(PrimitiveType::TYPE_INT, false));  \
+                                                                                            \
+        auto column = vectorized::ColumnHelper::create_column<vectorized::DataTypeInt32>(   \
+                {1, 2, 3, 4, 5, 6, 7, 8});                                                  \
+        auto result_column = vectorized::ColumnUInt8::create(N, 0);                         \
+        try {                                                                               \
+            set->find_batch(*column, N, result_column->get_data());                         \
+            ASSERT_TRUE(false) << "should not be here";                                     \
+        } catch (...) {                                                                     \
+        }                                                                                   \
+                                                                                            \
+        for (size_t i = 0; i != N; ++i) {                                                   \
+            set->insert(&i);                                                                \
+        }                                                                                   \
+                                                                                            \
+        for (size_t i = 0; i != N; ++i) {                                                   \
+            ASSERT_TRUE(set->find(&i));                                                     \
+        }                                                                                   \
+                                                                                            \
+        for (size_t i = N; i != 1024; ++i) {                                                \
+            ASSERT_FALSE(set->find(&i));                                                    \
+        }                                                                                   \
+                                                                                            \
+        std::unique_ptr<HybridSetBase> set2(create_set<N>(PrimitiveType::TYPE_INT, false)); \
+        set2->insert(set.get());                                                            \
+                                                                                            \
+        for (size_t i = 0; i != N; ++i) {                                                   \
+            ASSERT_TRUE(set2->find(&i));                                                    \
+        }                                                                                   \
+                                                                                            \
+        for (size_t i = N; i != 1024; ++i) {                                                \
+            ASSERT_FALSE(set2->find(&i));                                                   \
+        }                                                                                   \
+                                                                                            \
+        auto it = set->begin();                                                             \
+        while (it->has_next()) {                                                            \
+            auto value = *(int*)it->get_value();                                            \
+            ASSERT_TRUE(set2->find(&value)) << "cannot find: " << value;                    \
+            it->next();                                                                     \
+        }                                                                                   \
+        PInFilter in_filter;                                                                \
+        set->to_pb(&in_filter);                                                             \
+        set->clear();                                                                       \
+        ASSERT_EQ(set->size(), 0);                                                          \
+    }
 
 TEST_F(HybridSetTest, FixedContainer) {
-    {
-        TEST_FIXED_CONTAINER(1);
-    }
-    {
-        TEST_FIXED_CONTAINER(2);
-    }
-    {
-        TEST_FIXED_CONTAINER(3);
-    }
-    {
-        TEST_FIXED_CONTAINER(4);
-    }
-    {
-        TEST_FIXED_CONTAINER(5);
-    }
-    {
-        TEST_FIXED_CONTAINER(6);
-    }
-    {
-        TEST_FIXED_CONTAINER(7);
-    }
-    {
-        TEST_FIXED_CONTAINER(8);
-    }
+    TEST_FIXED_CONTAINER(1);
+    TEST_FIXED_CONTAINER(2);
+    TEST_FIXED_CONTAINER(3);
+    TEST_FIXED_CONTAINER(4);
+    TEST_FIXED_CONTAINER(5);
+    TEST_FIXED_CONTAINER(6);
+    TEST_FIXED_CONTAINER(7);
+    TEST_FIXED_CONTAINER(8);
 
     std::unique_ptr<HybridSetBase> set(create_set<8>(PrimitiveType::TYPE_INT, false));
     auto column = vectorized::ColumnHelper::create_column<vectorized::DataTypeInt32>(
