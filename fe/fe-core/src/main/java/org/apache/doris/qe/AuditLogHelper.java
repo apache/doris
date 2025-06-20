ea20e357db8c2525a0ea2c8884718d0623d1d0cd
Mingyu Chen (Rayner) <morningman@163.com>
2025-06-21 00:39:53 +0800

    [opt](audit) use one line in audit log and origin statement in audit table (#52032)
    ### What problem does this PR solve?
    
    Previously, when auditing, we use replace all `\n`, `\t` in origin sql
    string with `\\n`, `\\t`,
    so that the sql string can be written in one line. But this lead to some
    problem:
    
    1. User can not direct use the sql in audit log to execute.
    2. Some replacement is wrong, eg, replace the `\n` in a quota string.
    
    This PR changes the logic:
    
    1. For audit log, only replace `\n` with `\\n` to keep SQL in one line.
    2. For audit table, keep the origin string.
    3. Use special column and line separator for audit log load data, to
    avoid conflict with char in SQL

--- fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/AuditLogHelper.java
@@ -115,9 +115,7 @@ public class AuditLogHelper {
         int maxLen = GlobalVariable.auditPluginMaxSqlLength;
         origStmt = truncateByBytes(origStmt, maxLen, " ... /* truncated. audit_plugin_max_sql_length=" + maxLen
                 + " */");
-        return origStmt.replace("\n", "\\n")
-                .replace("\t", "\\t")
-                .replace("\r", "\\r");
+        return origStmt;
     }
 
     private static Optional<String> handleInsertStmt(String origStmt, StatementBase parsedStmt) {
@@ -148,9 +146,6 @@ public class AuditLogHelper {
                     Math.min(GlobalVariable.auditPluginMaxInsertStmtLength, GlobalVariable.auditPluginMaxSqlLength));
             origStmt = truncateByBytes(origStmt, maxLen, " ... /* total " + rowCnt
                     + " rows, truncated. audit_plugin_max_insert_stmt_length=" + maxLen + " */");
-            origStmt = origStmt.replace("\n", "\\n")
-                    .replace("\t", "\\t")
-                    .replace("\r", "\\r");
             return Optional.of(origStmt);
         } else {
             return Optional.empty();
