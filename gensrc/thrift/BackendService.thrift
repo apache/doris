af68d8b5077782d65ec9cadba479ed58b8789e90
Mingyu Chen (Rayner) <morningman@163.com>
2025-06-21 00:39:30 +0800

    [feat](profile) support getting query progress (#51400)
    ### What problem does this PR solve?
    
    Followup #50791
    
    Add a new FE HTTP API: `/rest/v2/manager/query/statistics/trace_id`.
    This API will return the query runtime statistic corresponding to a
    given trace id.
    The query statistics includes info such as real-time scan rows/bytes.
    
    Internally, Doris will get query id by trace id from all Frontends, and
    then fetch query statistics from BE.
    
    Use pattern:
    1. User set custom trace id by: `set
    session_context="trace_id:my_trace_id"`
    2. User executes a query in same session
    3. Start a http client to get query statistics in real-time during the
    query process.
    
    
    ![progress](https://github.com/user-attachments/assets/0a697c7d-d87a-4e9c-8965-c5a2d7d7836e)
    
    Also fix a bug in `CoordinatorContext.java`, to get real host.
    introduced from #41730
    
    This PR also change the column name of `information_schema.processlist`
    table, to be same as column
    name in `show processlist`.

--- gensrc/thrift/BackendService.thrift
+++ gensrc/thrift/BackendService.thrift
@@ -344,11 +344,14 @@ struct TPublishTopicResult {
 struct TGetRealtimeExecStatusRequest {
     // maybe query id or other unique id
     1: optional Types.TUniqueId id
+    2: optional string req_type // "stats" or "profile"
 }
 
 struct TGetRealtimeExecStatusResponse {
     1: optional Status.TStatus status
     2: optional FrontendService.TReportExecStatusParams report_exec_status_params
+    // query_stats is for getting real-time query statistics of a certain query
+    3: optional FrontendService.TQueryStatistics query_stats
 }
 
 struct TDictionaryStatus {
