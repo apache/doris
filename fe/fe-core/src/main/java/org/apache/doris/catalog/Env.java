a42fe9f6e8d0623ab5e2a79f4b9b2b7f1303bbd2
morrySnow <zhangwenxin@selectdb.com>
2025-06-20 19:49:03 +0800

    branch-2.1: [fix](metadata) replace view will break view's metadata #51058 (#52043)
    cherry-pick from #51058

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -5463,6 +5463,7 @@ public class Env {
             AlterViewStmt alterViewStmt = new AlterViewStmt(stmt.getTableName(), stmt.getColWithComments(),
                     stmt.getViewDefStmt());
             alterViewStmt.setInlineViewDef(stmt.getInlineViewDef());
+            alterViewStmt.setFinalColumns(stmt.getColumns());
             try {
                 alterView(alterViewStmt);
             } catch (UserException e) {
