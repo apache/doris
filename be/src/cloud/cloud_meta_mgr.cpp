c8bb6736c89be0cbea9092a42833a90b6a08d678
HonestManXin <HonestManXin@gmail.com>
2025-06-23 21:47:42 +0800

    [improve](cloud) make meta_service_endpoint configurable (#51748)
    ### Proposed changes
    When scale out the meta service, we need to change the
    `meta_service_endpoint` conf item in both fe and be, and restart all the
    service. This is not the best practice in a cloud environment. Using
    admin set all frontends config to change the config and without
    restarting it is good practice.
    
    In order to prevent setting a wrong meta_service_endpoint config, it has
    a constraint: new config and old config should have endpoint intersect
    to indicate they belong to the same cluster

--- be/src/cloud/cloud_meta_mgr.cpp
+++ be/src/cloud/cloud_meta_mgr.cpp
@@ -187,15 +187,12 @@ public:
 
         long deadline = now;
         // connection age only works without list endpoint.
-        if (!is_meta_service_endpoint_list() &&
-            config::meta_service_connection_age_base_seconds > 0) {
+        if (config::meta_service_connection_age_base_seconds > 0) {
             std::default_random_engine rng(static_cast<uint32_t>(now));
             std::uniform_int_distribution<> uni(
                     config::meta_service_connection_age_base_seconds,
                     config::meta_service_connection_age_base_seconds * 2);
             deadline = now + duration_cast<milliseconds>(seconds(uni(rng))).count();
-        } else {
-            deadline = LONG_MAX;
         }
 
         // Last one WIN
