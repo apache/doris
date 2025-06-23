d79840dcd3f75e9bad038554fbf679760cc449a6
zhangstar333 <zhangsida@selectdb.com>
2025-06-23 10:35:30 +0800

    update

--- be/src/vec/functions/function_regexp.cpp
+++ be/src/vec/functions/function_regexp.cpp
@@ -121,7 +121,7 @@ public:
     }
 
     Status execute_impl(FunctionContext* context, Block& block, const ColumnNumbers& arguments,
-                        uint32_t result, size_t input_rows_count) const override {
+                        size_t result, size_t input_rows_count) const override {
         size_t argument_size = arguments.size();
 
         auto result_null_map = ColumnUInt8::create(input_rows_count, 0);
