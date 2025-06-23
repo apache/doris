fd55881103645b4a8066d6dada1fd62c12de7fae
yujun <yujun@selectdb.com>
2025-06-23 11:38:41 +0800

    fix test

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/expression/rules/ExpressionRewriteSqlTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/expression/rules/ExpressionRewriteSqlTest.java
@@ -24,27 +24,6 @@ import org.junit.jupiter.api.Test;
 
 class ExpressionRewriteSqlTest extends SqlTestBase {
     @Test
-    public void testExtractNonConstant() {
-        String sql = "select * from T1 where id in (score, score, score + 100)";
-        PlanChecker.from(connectContext)
-                .analyze(sql)
-                .rewrite()
-                .matches(
-                        logicalFilter().when(f -> f.getPredicate().toSql().equals(
-                                "OR[(id = score),(id = (score + 100))]"
-                        )));
-
-        sql = "select * from T1 where id in (score,  score + 10, score + score, score, 10, 20, 30, 100 + 200)";
-        PlanChecker.from(connectContext)
-                .analyze(sql)
-                .rewrite()
-                .matches(
-                        logicalFilter().when(f -> f.getPredicate().toSql().equals(
-                                "OR[id IN (10, 20, 30, 300),(id = score),(id = (score + 10)),(id = (score + score))]"
-                )));
-    }
-
-    @Test
     public void testSimplifyRangeAndExtractCommonFactor() {
         String sql = "select * from T1 where id > 1 and score > 1 or id > 1 and score > 10";
         PlanChecker.from(connectContext)
@@ -61,7 +40,7 @@ class ExpressionRewriteSqlTest extends SqlTestBase {
                 .rewrite()
                 .matches(
                         logicalFilter().when(f -> f.getPredicate().toSql().equals(
-                                "AND[(score > 1),(id > 1)]"
+                                "AND[(id > 1),(score > 1)]"
                         )));
 
         sql = "select * from T1 where id > 1 and id < 0 or score > 1 and score < 0";
