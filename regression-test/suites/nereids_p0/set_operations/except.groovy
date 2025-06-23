43270cb9bf63169dd0fe69d71b1e37833ca7e01d
minghong <zhouminghong@selectdb.com>
2025-06-23 16:34:43 +0800

    update shape

--- regression-test/suites/nereids_p0/set_operations/except.groovy
+++ regression-test/suites/nereids_p0/set_operations/except.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_except", "query") {
+suite("except", "query") {
     sql "SET enable_nereids_planner=true"
     sql "SET enable_fallback_to_original_planner=false"
     sql "drop table if exists t1;"
