c460f6563a7a59fdcb8f13dc674738d81d2d233e
englefly <zhouminghong@selectdb.com>
2025-06-24 23:53:09 +0800

    shape0624

--- regression-test/suites/nereids_p0/set_operations/except.groovy
+++ regression-test/suites/nereids_p0/set_operations/except.groovy
@@ -16,8 +16,13 @@
 // under the License.
 
 suite("except", "query") {
-    sql "SET enable_nereids_planner=true"
-    sql "SET enable_fallback_to_original_planner=false"
+    sql """
+        SET enable_nereids_planner=true;
+        SET enable_fallback_to_original_planner=false;
+        set runtime_filter_type=2;
+        set enable_runtime_filter_prune=false;
+        """
+
     sql "drop table if exists t1;"
     sql "drop table if exists t2;"
     sql "drop table if exists t3;"
