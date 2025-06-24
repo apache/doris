4a41ec4ab35bc96826c60c5dac4ca570c0919fa9
zy-kkk <zhongyk10@gmail.com>
2025-06-24 14:11:24 +0800

    [Enhancement](s3) Support reading directory bucket (#52120)
    ### What problem does this PR solve?
    
    ```sql
    Doris > SELECT *
        -> FROM s3(
        ->     "uri"              = "s3://xxx--usw2-az1--x-s3/test1.csv",
        ->     "s3.access_key"    = "xxx",
        ->     "s3.secret_key"    = "xxx",
        ->     "s3.region"        = "us-west-2",
        ->     "s3.endpoint"      = "https://s3express-usw2-az1.us-west-2.amazonaws.com",
        ->     "format"           = "csv"
        -> );
    +---------+
    | c1      |
    +---------+
    | test1.1 |
    | test1.2 |
    +---------+
    2 rows in set (1.75 sec)
    
    Doris > SELECT *
        -> FROM s3(
        ->     "uri"              = "s3://xxx--usw2-az1--x-s3/test2.csv",
        ->     "s3.access_key"    = "xxx",
        ->     "s3.secret_key"    = "xxx",
        ->     "s3.region"        = "us-west-2",
        ->     "s3.endpoint"      = "https://s3express-usw2-az1.us-west-2.amazonaws.com",
        ->     "format"           = "csv"
        -> );
    +---------+
    | c1      |
    +---------+
    | test2.1 |
    | test2.2 |
    +---------+
    2 rows in set (2.43 sec)
    
    Doris > SELECT *
        -> FROM s3(
        ->     "uri"              = "s3://xxx--usw2-az1--x-s3/*",
        ->     "s3.access_key"    = "xxx",
        ->     "s3.secret_key"    = "xxx",
        ->     "s3.region"        = "us-west-2",
        ->     "s3.endpoint"      = "https://s3express-usw2-az1.us-west-2.amazonaws.com",
        ->     "format"           = "csv"
        -> );
    +---------+
    | c1      |
    +---------+
    | test1.1 |
    | test1.2 |
    | test2.1 |
    | test2.2 |
    +---------+
    4 rows in set (2.20 sec)
    ```
    
    #### nessie iceberg rest catalog on directory bucket
    
    **Need to set
    `nessie.catalog.service.s3.default-options.request-signing-enabled=false`
    in nessie**
    
    ```sql
    Doris > CREATE CATALOG `nessie_catalog` PROPERTIES (
        ->   'type'                   = 'iceberg',
        ->   'iceberg.catalog.type'   = 'rest',
        ->   'uri'                    = 'http://localhost:19120/iceberg',
        ->   'warehouse'              = 'warehouse',
        ->   's3.access_key'          = 'xxx',
        ->   's3.secret_key'          = 'xxx',
        ->   's3.endpoint'            = 'https://s3express-usw2-az1.us-west-2.amazonaws.com',
        ->   's3.region'              = 'us-west-2'
        -> );
    Query OK, 0 rows affected (0.02 sec)
    
    Doris > 
    Doris > use nessie_catalog.test;
    
    Database changed
    Doris > select * from test;
    +------+------+
    | id   | name |
    +------+------+
    |    1 | aa   |
    +------+------+
    
    ```

--- fe/fe-core/src/main/java/org/apache/doris/fs/obj/S3ObjStorage.java
+++ fe/fe-core/src/main/java/org/apache/doris/fs/obj/S3ObjStorage.java
@@ -542,11 +542,24 @@ public class S3ObjStorage implements ObjStorage<S3Client> {
 
             String listPrefix = S3Util.getLongestPrefix(globPath); // similar to Azure
             if (LOG.isDebugEnabled()) {
-                LOG.debug("globList listPrefix: {}", listPrefix);
+                LOG.debug("globList listPrefix: '{}' (from globPath: '{}')", listPrefix, globPath);
             }
+
+            // For Directory Buckets, ensure proper prefix handling using standardized approach
+            String finalPrefix = listPrefix;
+
+            if (uri.useS3DirectoryBucket()) {
+                String adjustedPrefix = S3URI.getDirectoryPrefixForGlob(listPrefix);
+                if (LOG.isDebugEnabled() && !adjustedPrefix.equals(listPrefix)) {
+                    LOG.debug("Directory bucket detected, adjusting prefix from '{}' to '{}'",
+                            listPrefix, adjustedPrefix);
+                }
+                finalPrefix = adjustedPrefix;
+            }
+
             ListObjectsV2Request request = ListObjectsV2Request.builder()
                     .bucket(bucket)
-                    .prefix(listPrefix)
+                    .prefix(finalPrefix)
                     .build();
 
             boolean isTruncated = false;
