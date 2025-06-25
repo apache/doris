5ce31052501cb47c6f58081c9bfdd6f68ceba431
yx-keith <yx136264032@163.com>
2025-06-25 14:45:08 +0800

    optimize ut

--- fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/AlterRoutineLoadCommandTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/trees/plans/commands/AlterRoutineLoadCommandTest.java
@@ -17,60 +17,71 @@
 
 package org.apache.doris.nereids.trees.plans.commands;
 
-import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
-import org.apache.doris.common.MetaNotFoundException;
-import org.apache.doris.load.routineload.KafkaRoutineLoadJob;
-import org.apache.doris.load.routineload.kafka.KafkaConfiguration;
-import org.apache.doris.load.routineload.kafka.KafkaDataSourceProperties;
 import org.apache.doris.nereids.trees.plans.commands.info.CreateRoutineLoadInfo;
 import org.apache.doris.nereids.trees.plans.commands.info.LabelNameInfo;
 import org.apache.doris.qe.ConnectContext;
 
 import com.google.common.collect.Maps;
 import mockit.Expectations;
+import mockit.Mocked;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.Map;
 
 public class AlterRoutineLoadCommandTest {
+    @Mocked
+    private Env env;
 
-    @Test
-    public void testValidateNormal() throws MetaNotFoundException, IOException {
-        Database dataBase = new Database(0, "db1");
-        Env.getCurrentInternalCatalog().unprotectCreateDb(dataBase);
+    @Mocked
+    private ConnectContext connectContext;
+
+    private void runBefore() {
         new Expectations() {
             {
-                Env.getCurrentEnv().getRoutineLoadManager().getJob(anyString, anyString);
+                Env.getCurrentEnv();
                 minTimes = 0;
-                result = new KafkaRoutineLoadJob();
+                result = env;
+
+                ConnectContext.get();
+                minTimes = 0;
+                result = connectContext;
+
+                connectContext.isSkipAuth();
+                minTimes = 0;
+                result = true;
             }
         };
-        ConnectContext connectContext = new ConnectContext();
+    }
+
+    @Test
+    public void testValidate() {
+        runBefore();
         Map<String, String> jobProperties = Maps.newHashMap();
+        jobProperties.put(CreateRoutineLoadInfo.DESIRED_CONCURRENT_NUMBER_PROPERTY, "2");
         jobProperties.put(CreateRoutineLoadInfo.MAX_ERROR_NUMBER_PROPERTY, "100");
-        jobProperties.put(CreateRoutineLoadInfo.MAX_BATCH_ROWS_PROPERTY, "200000");
+        jobProperties.put(CreateRoutineLoadInfo.MAX_FILTER_RATIO_PROPERTY, "0.01");
+        jobProperties.put(CreateRoutineLoadInfo.MAX_BATCH_INTERVAL_SEC_PROPERTY, "10");
+        jobProperties.put(CreateRoutineLoadInfo.MAX_BATCH_ROWS_PROPERTY, "300000");
+        jobProperties.put(CreateRoutineLoadInfo.MAX_BATCH_SIZE_PROPERTY, "1048576000");
+        jobProperties.put(CreateRoutineLoadInfo.STRICT_MODE, "false");
+        jobProperties.put(CreateRoutineLoadInfo.TIMEZONE, "Asia/Shanghai");
+
         Map<String, String> dataSourceProperties = Maps.newHashMap();
-        dataSourceProperties.put("property.client.id", "101");
-        dataSourceProperties.put("property.group.id", "mygroup");
-        dataSourceProperties.put(KafkaConfiguration.KAFKA_PARTITIONS.getName(), "1,2,3");
-        dataSourceProperties.put(KafkaConfiguration.KAFKA_OFFSETS.getName(), "10000, 20000, 30000");
-        LabelNameInfo labelNameInfo = new LabelNameInfo("db1", "label1");
+        LabelNameInfo labelNameInfo = new LabelNameInfo("testDb", "label1");
 
         AlterRoutineLoadCommand command = new AlterRoutineLoadCommand(labelNameInfo, jobProperties, dataSourceProperties);
         Assertions.assertDoesNotThrow(() -> command.validate(connectContext));
 
-        Assertions.assertEquals(2, command.getAnalyzedJobProperties().size());
+        Assertions.assertEquals(8, command.getAnalyzedJobProperties().size());
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.DESIRED_CONCURRENT_NUMBER_PROPERTY));
         Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.MAX_ERROR_NUMBER_PROPERTY));
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.MAX_FILTER_RATIO_PROPERTY));
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.MAX_BATCH_INTERVAL_SEC_PROPERTY));
         Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.MAX_BATCH_ROWS_PROPERTY));
-        Assertions.assertTrue(command.hasDataSourceProperty());
-
-        KafkaDataSourceProperties kafkaDataSourceProperties = (KafkaDataSourceProperties) command.getDataSourceProperties();
-        Assertions.assertEquals(2, kafkaDataSourceProperties.getCustomKafkaProperties().size());
-        Assertions.assertTrue(kafkaDataSourceProperties.getCustomKafkaProperties().containsKey("group.id"));
-        Assertions.assertTrue(kafkaDataSourceProperties.getCustomKafkaProperties().containsKey("client.id"));
-        Assertions.assertEquals(3, kafkaDataSourceProperties.getKafkaPartitionOffsets().size());
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.MAX_BATCH_SIZE_PROPERTY));
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.STRICT_MODE));
+        Assertions.assertTrue(command.getAnalyzedJobProperties().containsKey(CreateRoutineLoadInfo.TIMEZONE));
     }
 }
