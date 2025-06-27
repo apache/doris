abbfaebde403210c23b856d1ab1c830459954190
zhangdong <zhangdong@selectdb.com>
2025-06-27 10:36:06 +0800

    fix check

--- fe/fe-core/src/main/java/org/apache/doris/catalog/BuiltinAggregateFunctions.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/BuiltinAggregateFunctions.java
@@ -88,10 +88,12 @@ import org.apache.doris.nereids.trees.expressions.functions.agg.VarianceSamp;
 import org.apache.doris.nereids.trees.expressions.functions.agg.WindowFunnel;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /**
  * Builtin aggregate functions.
@@ -172,9 +174,13 @@ public class BuiltinAggregateFunctions implements FunctionHelper {
             agg(WindowFunnel.class, "window_funnel")
     );
 
-    public final Set<String> aggFuncNames = aggregateFunctions.stream()
-            .flatMap(fun -> fun.names.stream())
-            .collect(ImmutableSet.toImmutableSet());
+    public final Set<String> aggFuncNames = Collections.unmodifiableSet(
+            aggregateFunctions.stream()
+                    .flatMap(fun -> fun.names.stream())
+                    .collect(Collectors.toCollection(
+                            () -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)
+                    ))
+    );
 
     public static final BuiltinAggregateFunctions INSTANCE = new BuiltinAggregateFunctions();
 
