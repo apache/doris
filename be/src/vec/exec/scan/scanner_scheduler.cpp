654d5b947ddccab0c629d4273fddb467e7819204
yiguolei <guolei@selectdb.com>
2025-06-28 16:24:53 +0800

    [bugfix&enhancement](be_metrics) update scan bytes metric correctly

--- be/src/vec/exec/scan/scanner_scheduler.cpp
+++ be/src/vec/exec/scan/scanner_scheduler.cpp
@@ -364,6 +364,10 @@ void ScannerScheduler::_scanner_scan(std::shared_ptr<ScannerContext> ctx,
         scan_task->set_status(status);
         eos = true;
     }
+    // WorkloadGroup Policy will check cputime realtime, so that should update the counter
+    // as soon as possible, could not update it on close.
+    scanner->update_scan_cpu_timer();
+    scanner->update_realtime_counters();
 
     if (eos) {
         scanner->mark_to_need_to_close();
