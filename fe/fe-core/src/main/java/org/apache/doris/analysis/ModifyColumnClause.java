38b3960d73654cb2d53b82969b1114edda4f37b2
Socrates <suxiaogang223@icloud.com>
2025-06-30 17:07:08 +0800

    intro TableMetadataOps
    impl alter column
    
    fix
    
    add more TableMetadataOps
    
    adapt more alter ops
    
    impl alter ops
    
    impl rename table
    
    impl updateColumn for iceberg
    
    impl reorderColumn
    
    fix build
    
    add regression
    
    fix
    
    refresh table after alter
    
    refresh
    
    fix
    
    fix
    
    flush out
    
    impl rename table
    
    impl addColumns
    
    fix
    
    fix
    
    refactor

--- fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyColumnClause.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/ModifyColumnClause.java
@@ -22,7 +22,7 @@ import org.apache.doris.catalog.Column;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.catalog.KeysType;
 import org.apache.doris.catalog.OlapTable;
-import org.apache.doris.catalog.Table;
+import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.AnalysisException;
 import org.apache.doris.common.DdlException;
 
@@ -84,7 +84,9 @@ public class ModifyColumnClause extends AlterTableClause {
             throw new AnalysisException("No column definition in modify column clause.");
         }
         if (tableName != null) {
-            Table table = Env.getCurrentInternalCatalog().getDbOrDdlException(tableName.getDb())
+            TableIf table = Env.getCurrentEnv().getCatalogMgr()
+                    .getCatalogOrDdlException(tableName.getCtl())
+                    .getDbOrDdlException(tableName.getDb())
                     .getTableOrDdlException(tableName.getTbl());
             if (table instanceof OlapTable && ((OlapTable) table).getKeysType() == KeysType.AGG_KEYS
                     && columnDef.getAggregateType() == null) {
