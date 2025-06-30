apache/doris

commit d99fcdbe12aa449f6f10af3db98560a750a14f9b

[feat](nereids) support runtime filter for except/intersect (FE part) (#49947)
### What problem does this PR solve?
support runtime filter for except/intersect (FE part

fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/runtimefilterv2/RuntimeFilterV2Generator.java
@@ -1,0 +1,104 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.doris.nereids.processor.post.runtimefilterv2;
+
+import org.apache.doris.nereids.CascadesContext;
+import org.apache.doris.nereids.processor.post.PlanPostProcessor;
+import org.apache.doris.nereids.trees.expressions.Slot;
+import org.apache.doris.nereids.trees.plans.AbstractPlan;
+import org.apache.doris.nereids.trees.plans.Plan;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalExcept;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalIntersect;
+import org.apache.doris.nereids.trees.plans.physical.PhysicalSetOperation;
+import org.apache.doris.qe.ConnectContext;
+import org.apache.doris.statistics.ColumnStatistic;
+import org.apache.doris.statistics.Statistics;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+/**
+ * RuntimeFilterV2Generator
+ */
+public class RuntimeFilterV2Generator extends PlanPostProcessor {
+
+    public RuntimeFilterV2Generator() {
+    }
+
+    @Override
+    public Plan visitPhysicalIntersect(PhysicalIntersect intersect, CascadesContext context) {
+        computeRuntimeFilterForIntersectAndExcept(intersect, context);
+        return visitPhysicalSetOperation(intersect, context);
+    }
+
+    @Override
+    public Plan visitPhysicalExcept(PhysicalExcept except, CascadesContext context) {
+        computeRuntimeFilterForIntersectAndExcept(except, context);
+        return visitPhysicalSetOperation(except, context);
+    }
+
+    private void computeRuntimeFilterForIntersectAndExcept(PhysicalSetOperation setOp, CascadesContext context) {
+        AbstractPlan child0 = (AbstractPlan) setOp.child(0);
+        if (child0.getStats() != null
+                && ConnectContext.get() != null
+                && ConnectContext.get().getSessionVariable() != null
+                && child0.getStats().getRowCount()
+                < ConnectContext.get().getSessionVariable().runtimeFilterMaxBuildRowCount) {
+            for (int slotIdx : chooseSourceSlots(setOp)) {
+                for (int childId = 1; childId < setOp.children().size(); childId++) {
+                    Plan child = setOp.children().get(childId);
+                    Statistics stats = child0.getStats();
+                    long buildNdvOrRowCount = -1;
+                    if (stats != null) {
+                        buildNdvOrRowCount = (long) stats.getRowCount();
+                        ColumnStatistic colStats = stats.findColumnStatistics(
+                                setOp.child(0).getOutput().get(slotIdx));
+                        if (colStats != null && !colStats.isUnKnown) {
+                            buildNdvOrRowCount = Math.max(1, (long) colStats.ndv);
+                        }
+                    }
+                    PushDownContext pushDownContext = new PushDownContext(
+                            context.getRuntimeFilterV2Context(),
+                            setOp,
+                            setOp.child(0).getOutput().get(slotIdx),
+                            buildNdvOrRowCount,
+                            slotIdx,
+                            setOp.child(childId).getOutput().get(slotIdx));
+                    child.accept(PushDownVisitor.INSTANCE, pushDownContext);
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * do not use metric data type column (such as array, map, struct, json, bitmap, ...) as runtime filter source
+     *
+     */
+    private List<Integer> chooseSourceSlots(PhysicalSetOperation setOp) {
+        List<Slot> output = setOp.getOutput();
+        for (int i = 0; i < output.size(); i++) {
+            if (!output.get(i).getDataType().isOnlyMetricType()
+                    && !setOp.getLogicalProperties().getTrait().getUniformValue(output.get(i)).isPresent()) {
+                return ImmutableList.of(i);
+            }
+        }
+        return ImmutableList.of();
+    }
+}

regression-test/data/shape_check/tpcds_sf100/noStatsRfPrune/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect
+--------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -32,7 +32,7 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim]
+----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
 --------------------PhysicalOlapScan[customer]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RFV2: RF7
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer]
+--------------------PhysicalOlapScan[customer] RFV2: RF8
 

regression-test/data/shape_check/tpcds_sf100/no_stats_shape/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect
+--------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF1 c_customer_sk->[ws_bill_customer_sk]
 --------------------hashAgg[GLOBAL]
@@ -32,7 +32,7 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2 RF3
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim]
+----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
 --------------------PhysicalOlapScan[customer]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF5 c_customer_sk->[ss_customer_sk]
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5 RFV2: RF7
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer]
+--------------------PhysicalOlapScan[customer] RFV2: RF8
 

regression-test/data/shape_check/tpcds_sf100/rf_prune/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect
+--------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -32,7 +32,7 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim]
+----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
 --------------------PhysicalOlapScan[customer]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RFV2: RF7
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer]
+--------------------PhysicalOlapScan[customer] RFV2: RF8
 

regression-test/data/shape_check/tpcds_sf100/rf_prune/query87.out
@@ -5,7 +5,7 @@ PhysicalResultSink
 ----PhysicalDistribute[DistributionSpecGather]
 ------hashAgg[LOCAL]
 --------PhysicalProject
-----------PhysicalExcept
+----------PhysicalExcept RFV2: RF6[ss_customer_sk->d_date] RF7[ss_customer_sk->d_date]
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
 ----------------hashAgg[GLOBAL]
@@ -30,7 +30,7 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim]
+------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
 ----------------PhysicalOlapScan[customer]
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
@@ -43,6 +43,6 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[web_sales] apply RFs: RF4
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim]
+------------------------------PhysicalOlapScan[date_dim] RFV2: RF7
 ----------------PhysicalOlapScan[customer]
 

commit ccc336cbd23b83613a3ed169fdfde27fc0d69640

[fix](nereids) intersect/except runtime filter project wrong child slot (#52435)
### What problem does this PR solve?
a bug introduced by #49947 
tpcds 38/87 are involved

fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/runtimefilterv2/RuntimeFilterV2Generator.java
@@ -19,6 +19,7 @@ package org.apache.doris.nereids.processor.post.runtimefilterv2;
 
 import org.apache.doris.nereids.CascadesContext;
 import org.apache.doris.nereids.processor.post.PlanPostProcessor;
+import org.apache.doris.nereids.trees.expressions.Expression;
 import org.apache.doris.nereids.trees.expressions.Slot;
 import org.apache.doris.nereids.trees.plans.AbstractPlan;
 import org.apache.doris.nereids.trees.plans.Plan;
@@ -61,14 +62,16 @@ public class RuntimeFilterV2Generator extends PlanPostProcessor {
                 && child0.getStats().getRowCount()
                 < ConnectContext.get().getSessionVariable().runtimeFilterMaxBuildRowCount) {
             for (int slotIdx : chooseSourceSlots(setOp)) {
+                Expression sourceExpression = setOp.getRegularChildrenOutputs().get(0).get(slotIdx);
                 for (int childId = 1; childId < setOp.children().size(); childId++) {
                     Plan child = setOp.children().get(childId);
+                    Expression targetExpression = setOp.getRegularChildrenOutputs().get(childId).get(slotIdx);
                     Statistics stats = child0.getStats();
                     long buildNdvOrRowCount = -1;
                     if (stats != null) {
                         buildNdvOrRowCount = (long) stats.getRowCount();
                         ColumnStatistic colStats = stats.findColumnStatistics(
-                                setOp.child(0).getOutput().get(slotIdx));
+                                sourceExpression);
                         if (colStats != null && !colStats.isUnKnown) {
                             buildNdvOrRowCount = Math.max(1, (long) colStats.ndv);
                         }
@@ -76,10 +79,10 @@ public class RuntimeFilterV2Generator extends PlanPostProcessor {
                     PushDownContext pushDownContext = new PushDownContext(
                             context.getRuntimeFilterV2Context(),
                             setOp,
-                            setOp.child(0).getOutput().get(slotIdx),
+                            sourceExpression,
                             buildNdvOrRowCount,
                             slotIdx,
-                            setOp.child(childId).getOutput().get(slotIdx));
+                            targetExpression);
                     child.accept(PushDownVisitor.INSTANCE, pushDownContext);
                 }
             }

regression-test/data/shape_check/tpcds_sf100/noStatsRfPrune/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
+--------------PhysicalIntersect RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -32,8 +32,8 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
---------------------PhysicalOlapScan[customer]
+----------------------------------PhysicalOlapScan[date_dim]
+--------------------PhysicalOlapScan[customer] RFV2: RF6
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RFV2: RF7
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer] RFV2: RF8
+--------------------PhysicalOlapScan[customer] RFV2: RF7
 

regression-test/data/shape_check/tpcds_sf100/no_stats_shape/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
+--------------PhysicalIntersect RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF1 c_customer_sk->[ws_bill_customer_sk]
 --------------------hashAgg[GLOBAL]
@@ -32,8 +32,8 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2 RF3
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
---------------------PhysicalOlapScan[customer]
+----------------------------------PhysicalOlapScan[date_dim]
+--------------------PhysicalOlapScan[customer] RFV2: RF6
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF5 c_customer_sk->[ss_customer_sk]
 --------------------hashAgg[GLOBAL]
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5 RFV2: RF7
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer] RFV2: RF8
+--------------------PhysicalOlapScan[customer] RFV2: RF7
 

regression-test/data/shape_check/tpcds_sf100/rf_prune/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
+--------------PhysicalIntersect RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -32,8 +32,8 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
---------------------PhysicalOlapScan[customer]
+----------------------------------PhysicalOlapScan[date_dim]
+--------------------PhysicalOlapScan[customer] RFV2: RF6
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
 --------------------hashAgg[GLOBAL]
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RFV2: RF7
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer] RFV2: RF8
+--------------------PhysicalOlapScan[customer] RFV2: RF7
 

regression-test/data/shape_check/tpcds_sf100/rf_prune/query87.out
@@ -5,7 +5,7 @@ PhysicalResultSink
 ----PhysicalDistribute[DistributionSpecGather]
 ------hashAgg[LOCAL]
 --------PhysicalProject
-----------PhysicalExcept RFV2: RF6[ss_customer_sk->d_date] RF7[ss_customer_sk->d_date]
+----------PhysicalExcept RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=()
 ----------------hashAgg[GLOBAL]
@@ -30,8 +30,8 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
-----------------PhysicalOlapScan[customer]
+------------------------------PhysicalOlapScan[date_dim]
+----------------PhysicalOlapScan[customer] RFV2: RF6
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=()
 ----------------hashAgg[GLOBAL]
@@ -43,6 +43,6 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[web_sales] apply RFs: RF4
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim] RFV2: RF7
-----------------PhysicalOlapScan[customer]
+------------------------------PhysicalOlapScan[date_dim]
+----------------PhysicalOlapScan[customer] RFV2: RF7
 

regression-test/data/shape_check/tpcds_sf100/shape/query38.out
@@ -7,7 +7,7 @@ PhysicalResultSink
 --------PhysicalDistribute[DistributionSpecGather]
 ----------hashAgg[LOCAL]
 ------------PhysicalProject
---------------PhysicalIntersect RFV2: RF6[d_date->d_date] RF7[d_date->ss_customer_sk] RF8[d_date->c_customer_sk]
+--------------PhysicalIntersect RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF1 c_customer_sk->[ws_bill_customer_sk]
 --------------------hashAgg[GLOBAL]
@@ -32,8 +32,8 @@ PhysicalResultSink
 --------------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2 RF3
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
-----------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
---------------------PhysicalOlapScan[customer]
+----------------------------------PhysicalOlapScan[date_dim]
+--------------------PhysicalOlapScan[customer] RFV2: RF6
 ----------------PhysicalDistribute[DistributionSpecHash]
 ------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF5 c_customer_sk->[ss_customer_sk]
 --------------------hashAgg[GLOBAL]
@@ -42,9 +42,9 @@ PhysicalResultSink
 --------------------------PhysicalProject
 ----------------------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_sold_date_sk = date_dim.d_date_sk)) otherCondition=() build RFs:RF4 d_date_sk->[ss_sold_date_sk]
 ------------------------------PhysicalProject
---------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5 RFV2: RF7
+--------------------------------PhysicalOlapScan[store_sales] apply RFs: RF4 RF5
 ------------------------------PhysicalProject
 --------------------------------filter((date_dim.d_month_seq <= 1194) and (date_dim.d_month_seq >= 1183))
 ----------------------------------PhysicalOlapScan[date_dim]
---------------------PhysicalOlapScan[customer] RFV2: RF8
+--------------------PhysicalOlapScan[customer] RFV2: RF7
 

regression-test/data/shape_check/tpcds_sf100/shape/query87.out
@@ -5,7 +5,7 @@ PhysicalResultSink
 ----PhysicalDistribute[DistributionSpecGather]
 ------hashAgg[LOCAL]
 --------PhysicalProject
-----------PhysicalExcept RFV2: RF6[ss_customer_sk->d_date] RF7[ss_customer_sk->d_date]
+----------PhysicalExcept RFV2: RF6[c_last_name->c_last_name] RF7[c_last_name->c_last_name]
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN broadcast] hashCondition=((store_sales.ss_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF1 c_customer_sk->[ss_customer_sk]
 ----------------hashAgg[GLOBAL]
@@ -30,8 +30,8 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[catalog_sales] apply RFs: RF2 RF3
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim] RFV2: RF6
-----------------PhysicalOlapScan[customer]
+------------------------------PhysicalOlapScan[date_dim]
+----------------PhysicalOlapScan[customer] RFV2: RF6
 ------------PhysicalDistribute[DistributionSpecHash]
 --------------hashJoin[INNER_JOIN shuffle] hashCondition=((web_sales.ws_bill_customer_sk = customer.c_customer_sk)) otherCondition=() build RFs:RF5 c_customer_sk->[ws_bill_customer_sk]
 ----------------hashAgg[GLOBAL]
@@ -43,6 +43,6 @@ PhysicalResultSink
 ----------------------------PhysicalOlapScan[web_sales] apply RFs: RF4 RF5
 --------------------------PhysicalProject
 ----------------------------filter((date_dim.d_month_seq <= 1195) and (date_dim.d_month_seq >= 1184))
-------------------------------PhysicalOlapScan[date_dim] RFV2: RF7
-----------------PhysicalOlapScan[customer]
+------------------------------PhysicalOlapScan[date_dim]
+----------------PhysicalOlapScan[customer] RFV2: RF7
 