7ed195dcbd9c26e83907fe9d66a773c47c3d09c8
Kaijie Chen <chenkaijie@selectdb.com>
2025-06-30 17:10:58 +0800

    fixup

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -3211,6 +3211,9 @@ public class Config extends ConfigBase {
     public static int cloud_warm_up_job_scheduler_interval_millisecond = 1000; // 1 seconds
 
     @ConfField(mutable = true, masterOnly = true)
+    public static long cloud_warm_up_job_max_bytes_per_batch = 21474836480L; // 20GB
+
+    @ConfField(mutable = true, masterOnly = true)
     public static boolean cloud_warm_up_force_all_partitions = false;
 
     @ConfField(mutable = true, masterOnly = true)
