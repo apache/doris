744cf44e0a5ddeb013207586d3997d496ad1637d
vinlee19 <1401597760@qq.com>
2025-07-01 19:33:49 +0800

    delete unused code

--- fe/fe-core/src/main/java/org/apache/doris/datasource/paimon/PaimonMetadataCache.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/paimon/PaimonMetadataCache.java
@@ -157,10 +157,4 @@ public class PaimonMetadataCache {
                 snapshotCache.estimatedSize()));
         return res;
     }
-
-    public org.apache.paimon.table.Table getPaimonTable(CatalogIf catalog, String dbName, String tbName) {
-        PaimonSnapshotCacheKey paimonSnapshotCacheKey = new PaimonSnapshotCacheKey(catalog, dbName, tbName);
-        return ((PaimonExternalCatalog) paimonSnapshotCacheKey.getCatalog()).getPaimonTable(
-                paimonSnapshotCacheKey.getDbName(), paimonSnapshotCacheKey.getTableName());
-    }
 }
