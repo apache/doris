b0783034b1a11804967df790492b30d4e3d4f686
yx-keith <yx136264032@163.com>
2025-07-01 14:30:24 +0800

    optimize regression test

--- regression-test/suites/cloud_p0/cache/multi_cluster/warm_up/cluster/test_warm_up_cluster.groovy
+++ regression-test/suites/cloud_p0/cache/multi_cluster/warm_up/cluster/test_warm_up_cluster.groovy
@@ -236,19 +236,10 @@ suite("test_warm_up_cluster") {
         assertTrue(true)
     }
 
-    try {
-        sql "WARM UP CLUSTER regression_cluster_name3 WITH Table customer"
-        assertTrue(false)
-    } catch (Exception e) {
-        assertTrue(true)
-    }
+    sql "WARM UP CLUSTER regression_cluster_name3 WITH Table customer"
+    assertTrue(false)
 
-    try {
-        sql "WARM UP CLUSTER regression_cluster_name4 WITH Table supplier"
-        assertTrue(false)
-    } catch (Exception e) {
-        assertTrue(true)
-    }
+    sql "WARM UP CLUSTER regression_cluster_name4 WITH Table supplier"
 
     sql new File("""${context.file.parent}/../ddl/${table}_delete.sql""").text
     sql new File("""${context.file.parent}/../ddl/supplier_delete.sql""").text
