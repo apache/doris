3d32b30574c8ca90b9db8bd29342cfed6566c916
HappenLee <happenlee@selectdb.com>
2025-07-02 17:08:28 +0800

    [compile](be) Add USE_NATIVE to speed up be in clould mode

--- build.sh
+++ build.sh
@@ -66,6 +66,7 @@ Usage: $0 <options>
 
   Environment variables:
     USE_AVX2                    If the CPU does not support AVX2 instruction set, please set USE_AVX2=0. Default is ON.
+    USE_NATIVE                  Support -march=native compile to speed up the different CPU in cloud, if USE_NATIVE is ON, the USE_AVX2 auto turn off. Default is OFF.
     STRIP_DEBUG_INFO            If set STRIP_DEBUG_INFO=ON, the debug information in the compiled binaries will be stored separately in the 'be/lib/debug_info' directory. Default is OFF.
     DISABLE_BE_JAVA_EXTENSIONS  If set DISABLE_BE_JAVA_EXTENSIONS=ON, we will do not build binary with java-udf,hadoop-hudi-scanner,jdbc-scanner and so on Default is OFF.
     DISABLE_JAVA_CHECK_STYLE    If set DISABLE_JAVA_CHECK_STYLE=ON, it will skip style check of java code in FE.
@@ -90,6 +91,7 @@ Usage: $0 <options>
     $0 --be-extension-ignore avro-scanner   build be-java-extensions, choose which modules to ignore. Multiple modules separated by commas, like --be-extension-ignore avro-scanner,hadoop-hudi-scanner
 
     USE_AVX2=0 $0 --be                      build Backend and not using AVX2 instruction.
+    USE_NATIVE=1 $0 --be                    build Backend use -march=native compile to speed up the different CPU in cloud.
     USE_AVX2=0 STRIP_DEBUG_INFO=ON $0       build all and not using AVX2 instruction, and strip the debug info for Backend
   "
     exit 1
@@ -372,6 +374,15 @@ fi
 if [[ -z "${USE_AVX2}" ]]; then
     USE_AVX2='ON'
 fi
+
+if [[ -z "${USE_NATIVE}" ]]; then
+    USE_NATIVE='OFF'
+fi
+
+if [[ "${USE_NATIVE}" == "ON" ]]; then
+    USE_AVX2='OFF'
+fi
+
 if [[ -z "${USE_LIBCPP}" ]]; then
     if [[ "${TARGET_SYSTEM}" != 'Darwin' ]]; then
         USE_LIBCPP='OFF'
@@ -519,6 +530,7 @@ echo "Get params:
     CLEAN                               -- ${CLEAN}
     GLIBC_COMPATIBILITY                 -- ${GLIBC_COMPATIBILITY}
     USE_AVX2                            -- ${USE_AVX2}
+    USE_NATIVE                          -- ${USE_NATIVE}
     USE_LIBCPP                          -- ${USE_LIBCPP}
     USE_DWARF                           -- ${USE_DWARF}
     USE_UNWIND                          -- ${USE_UNWIND}
@@ -626,6 +638,7 @@ if [[ "${BUILD_BE}" -eq 1 ]]; then
         -DENABLE_PCH="${ENABLE_PCH}" \
         -DUSE_JEMALLOC="${USE_JEMALLOC}" \
         -DUSE_AVX2="${USE_AVX2}" \
+        -DUSE_NATIVE="${USE_NATIVE}" \
         -DGLIBC_COMPATIBILITY="${GLIBC_COMPATIBILITY}" \
         -DEXTRA_CXX_FLAGS="${EXTRA_CXX_FLAGS}" \
         -DENABLE_CLANG_COVERAGE="${DENABLE_CLANG_COVERAGE}" \
