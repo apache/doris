6a33c72a4a5e2623af751ac3cb7d9c6aa09d25ae
csun5285 <sunchenyang@selectdb.com>
2025-07-02 18:21:37 +0800

    fix pick

--- be/test/vec/function/function_is_null_test.cpp
+++ be/test/vec/function/function_is_null_test.cpp
@@ -218,17 +218,17 @@ TEST_F(FunctionIsNullTest, gc_binlogs_test) {
         auto index_file_reader = std::make_shared<InvertedIndexFileReader>(
                 io::global_local_filesystem(), index_prefix, InvertedIndexStorageFormatPB::V2);
         EXPECT_TRUE(index_file_reader->init().ok());
-        auto index_meta = _tablet_schema->inverted_index(0);
-        EXPECT_TRUE(index_meta);
-        auto bkd_reader = BkdIndexReader::create_shared(index_meta, index_file_reader);
+        auto index_meta = _tablet_schema->inverted_indexs(0);
+        EXPECT_TRUE(index_meta[0]);
+        auto bkd_reader = BkdIndexReader::create_shared(index_meta[0], index_file_reader);
         EXPECT_TRUE(bkd_reader);
         check_result(bkd_reader.get(), true, 1);
         check_result(bkd_reader.get(), false, 2);
 
-        auto index_meta2 = _tablet_schema->inverted_index(1);
-        EXPECT_TRUE(index_meta2);
+        auto index_meta2 = _tablet_schema->inverted_indexs(1);
+        EXPECT_TRUE(index_meta2[0]);
         auto string_reader =
-                StringTypeInvertedIndexReader::create_shared(index_meta2, index_file_reader);
+                StringTypeInvertedIndexReader::create_shared(index_meta2[0], index_file_reader);
         EXPECT_TRUE(string_reader);
         check_result(string_reader.get(), true, 2);
         check_result(string_reader.get(), false, 1);
