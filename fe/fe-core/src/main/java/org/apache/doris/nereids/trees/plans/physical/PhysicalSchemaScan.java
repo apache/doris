d20e617b024c6d35da820a8c9e68f806b875cd52
koarz <lihao@selectdb.com>
2025-07-03 11:56:23 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalSchemaScan.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/physical/PhysicalSchemaScan.java
@@ -163,7 +163,7 @@ public class PhysicalSchemaScan extends PhysicalCatalogRelation {
     }
 
     private boolean checkUserAccessPermission(String tableName) {
-        if (BACKEND_TABLE_ONLY_ROOT_READABLE.contains(tableName.toLowerCase()) && Env.getCurrentEnv().getAccessManager()
+        if (BACKEND_TABLE_ONLY_ROOT_READABLE.contains(tableName.toLowerCase()) && !Env.getCurrentEnv().getAccessManager()
                 .checkGlobalPriv(ConnectContext.get().getCurrentUserIdentity(), PrivPredicate.ADMIN)) {
             return true;
         }
