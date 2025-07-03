0092230f7e3e31cfea5f4c1930f7acd00e7836f1
stephen <lidongyang@selectdb.com>
2025-07-03 17:20:35 +0800

    3

--- regression-test/pipeline/cloud_p0/clean.sh
+++ regression-test/pipeline/cloud_p0/clean.sh
@@ -26,6 +26,6 @@ if ${skip_pipeline:=false}; then echo "INFO: skip build pipline" && exit 0; else
 echo "#### Run tpcds test on Doris ####"
 DORIS_HOME="${teamcity_build_checkoutDir}/output"
 export DORIS_HOME
-exit_flag=$(stop_doris_grace)
+if stop_doris_grace; then exit_flag=0; else exit_flag=1; fi
 clean_fdb "cloud_instance_0"
 exit "${exit_flag}"
