apache/doris

commit 5a99a17ce7e752c5571c2e4b526e36945ee260cd

[feat](jdbc) support custom function rules in catalog properties (#51471)
### What problem does this PR solve?

Add new jdbc catalog property: `function_rules`.
This property will enable user to customize which function can be pushed
down to jdbc datasource,
and which function should be rewrote to function in jdbc datasource:

eg:
```
create catalog jdbc properties (
    ...
    'function_rules' = '{"pushdown" : {"supported": ["to_date"], "unsupported" : ["abs"]}, "rewrite" : {"to_date" : "date2"}}'
)
```
The value is a json format config:
- `pushdown`: the value of `pushdown` is a map, the `supported` array
indicate the function that can be pushed down to datasource. the
`unsupported` array indicate the function that can not.
- `rewrite`: the value of `rewrite` is map, if the function can be
pushed down to datasource, it will be renamed to the value in rewrite
map.

eg:
```
select * from jdbc_table where abs(k1) > 0 and to_date("k2") = "2022-10-10"
```
will generate sql:
```
select * from jdbc_table where date2("k2") = "2022-10-10"
```

You can see the `abs()` is not pushed down, and `to_date()` is rewrote
to `date2()`.

-------
This change is compatible with previous version.
Previously, there are some hard coded function rules, and also a FE
config `jdbc_mysql_unsupported_pushdown_functions`.

Doris will merge the user specified `function_rules` and the above
rules.

fe/fe-core/src/main/java/org/apache/doris/catalog/JdbcTable.java
@@ -22,6 +22,7 @@ import org.apache.doris.common.DdlException;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.io.DeepCopy;
 import org.apache.doris.common.io.Text;
+import org.apache.doris.datasource.ExternalFunctionRules;
 import org.apache.doris.thrift.TJdbcTable;
 import org.apache.doris.thrift.TOdbcTableType;
 import org.apache.doris.thrift.TTableDescriptor;
@@ -103,6 +104,8 @@ public class JdbcTable extends Table {
     private int connectionPoolMaxLifeTime;
     private boolean connectionPoolKeepAlive;
 
+    private ExternalFunctionRules functionRules;
+
     static {
         Map<String, TOdbcTableType> tempMap = new CaseInsensitiveMap();
         tempMap.put("mysql", TOdbcTableType.MYSQL);
@@ -128,6 +131,8 @@ public class JdbcTable extends Table {
             throws DdlException {
         super(id, name, TableType.JDBC, schema);
         validate(properties);
+        // check and set external function rules
+        checkAndSetExternalFunctionRules(properties);
     }
 
     public JdbcTable(long id, String name, List<Column> schema, TableType type) {
@@ -412,6 +417,12 @@ public class JdbcTable extends Table {
         }
     }
 
+    private void checkAndSetExternalFunctionRules(Map<String, String> properties) throws DdlException {
+        ExternalFunctionRules.check(properties.getOrDefault(JdbcResource.FUNCTION_RULES, ""));
+        this.functionRules = ExternalFunctionRules.create(jdbcTypeName,
+                properties.getOrDefault(JdbcResource.FUNCTION_RULES, ""));
+    }
+
     /**
      * Formats the provided name (for example, a database, table, or schema name) according to the specified parameters.
      *
@@ -508,5 +519,14 @@ public class JdbcTable extends Table {
 
     public static String formatNameWithRemoteName(String remoteName, String wrapStart, String wrapEnd) {
         return wrapStart + remoteName + wrapEnd;
+    }
+
+    // This is used when converting JdbcExternalTable to JdbcTable.
+    public void setExternalFunctionRules(ExternalFunctionRules functionRules) {
+        this.functionRules = functionRules;
+    }
+
+    public ExternalFunctionRules getExternalFunctionRules() {
+        return functionRules;
     }
 }

fe/fe-core/src/main/java/org/apache/doris/catalog/JdbcResource.java
@@ -111,6 +111,7 @@ public class JdbcResource extends Resource {
     public static final String CHECK_SUM = "checksum";
     public static final String CREATE_TIME = "create_time";
     public static final String TEST_CONNECTION = "test_connection";
+    public static final String FUNCTION_RULES = "function_rules";
 
     private static final ImmutableList<String> ALL_PROPERTIES = new ImmutableList.Builder<String>().add(
             JDBC_URL,

fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalCatalog.java
@@ -183,6 +183,8 @@ public abstract class ExternalCatalog
     private volatile Configuration cachedConf = null;
     private byte[] confLock = new byte[0];
 
+    private volatile boolean isInitializing = false;
+
     public ExternalCatalog() {
     }
 
@@ -305,38 +307,46 @@ public abstract class ExternalCatalog
      * So you have to make sure the client of third system is initialized before any method was called.
      */
     public final synchronized void makeSureInitialized() {
-        initLocalObjects();
-        if (!initialized) {
-            if (useMetaCache.get()) {
-                if (metaCache == null) {
-                    metaCache = Env.getCurrentEnv().getExtMetaCacheMgr().buildMetaCache(
-                            name,
-                            OptionalLong.of(86400L),
-                            OptionalLong.of(Config.external_cache_expire_time_minutes_after_access * 60L),
-                            Config.max_meta_object_cache_num,
-                            ignored -> getFilteredDatabaseNames(),
-                            localDbName -> Optional.ofNullable(
-                                    buildDbForInit(null, localDbName, Util.genIdByName(name, localDbName), logType,
-                                            true)),
-                            (key, value, cause) -> value.ifPresent(v -> v.setUnInitialized(invalidCacheInInit)));
-                }
-                setLastUpdateTime(System.currentTimeMillis());
-            } else {
-                if (!Env.getCurrentEnv().isMaster()) {
-                    // Forward to master and wait the journal to replay.
-                    int waitTimeOut = ConnectContext.get() == null ? 300 : ConnectContext.get().getExecTimeoutS();
-                    MasterCatalogExecutor remoteExecutor = new MasterCatalogExecutor(waitTimeOut * 1000);
-                    try {
-                        remoteExecutor.forward(id, -1);
-                    } catch (Exception e) {
-                        Util.logAndThrowRuntimeException(LOG,
-                                String.format("failed to forward init catalog %s operation to master.", name), e);
+        if (isInitializing) {
+            return;
+        }
+        isInitializing = true;
+        try {
+            initLocalObjects();
+            if (!initialized) {
+                if (useMetaCache.get()) {
+                    if (metaCache == null) {
+                        metaCache = Env.getCurrentEnv().getExtMetaCacheMgr().buildMetaCache(
+                                name,
+                                OptionalLong.of(86400L),
+                                OptionalLong.of(Config.external_cache_expire_time_minutes_after_access * 60L),
+                                Config.max_meta_object_cache_num,
+                                ignored -> getFilteredDatabaseNames(),
+                                localDbName -> Optional.ofNullable(
+                                        buildDbForInit(null, localDbName, Util.genIdByName(name, localDbName), logType,
+                                                true)),
+                                (key, value, cause) -> value.ifPresent(v -> v.setUnInitialized(invalidCacheInInit)));
                     }
-                    return;
+                    setLastUpdateTime(System.currentTimeMillis());
+                } else {
+                    if (!Env.getCurrentEnv().isMaster()) {
+                        // Forward to master and wait the journal to replay.
+                        int waitTimeOut = ConnectContext.get() == null ? 300 : ConnectContext.get().getExecTimeoutS();
+                        MasterCatalogExecutor remoteExecutor = new MasterCatalogExecutor(waitTimeOut * 1000);
+                        try {
+                            remoteExecutor.forward(id, -1);
+                        } catch (Exception e) {
+                            Util.logAndThrowRuntimeException(LOG,
+                                    String.format("failed to forward init catalog %s operation to master.", name), e);
+                        }
+                        return;
+                    }
+                    init();
                 }
-                init();
+                initialized = true;
             }
-            initialized = true;
+        } finally {
+            isInitializing = false;
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/datasource/ExternalDatabase.java
@@ -101,6 +101,8 @@ public abstract class ExternalDatabase<T extends ExternalTable>
 
     private MetaCache<T> metaCache;
 
+    private volatile boolean isInitializing = false;
+
     /**
      * Create external database.
      *
@@ -154,39 +156,49 @@ public abstract class ExternalDatabase<T extends ExternalTable>
     }
 
     public final synchronized void makeSureInitialized() {
-        extCatalog.makeSureInitialized();
-        if (!initialized) {
-            if (extCatalog.getUseMetaCache().get()) {
-                if (metaCache == null) {
-                    metaCache = Env.getCurrentEnv().getExtMetaCacheMgr().buildMetaCache(
-                            name,
-                            OptionalLong.of(86400L),
-                            OptionalLong.of(Config.external_cache_expire_time_minutes_after_access * 60L),
-                            Config.max_meta_object_cache_num,
-                            ignored -> listTableNames(),
-                            localTableName -> Optional.ofNullable(
-                                    buildTableForInit(null, localTableName,
-                                            Util.genIdByName(extCatalog.getName(), name, localTableName), extCatalog,
-                                            this, true)),
-                            (key, value, cause) -> value.ifPresent(ExternalTable::unsetObjectCreated));
-                }
-                setLastUpdateTime(System.currentTimeMillis());
-            } else {
-                if (!Env.getCurrentEnv().isMaster()) {
-                    // Forward to master and wait the journal to replay.
-                    int waitTimeOut = ConnectContext.get() == null ? 300 : ConnectContext.get().getExecTimeoutS();
-                    MasterCatalogExecutor remoteExecutor = new MasterCatalogExecutor(waitTimeOut * 1000);
-                    try {
-                        remoteExecutor.forward(extCatalog.getId(), id);
-                    } catch (Exception e) {
-                        Util.logAndThrowRuntimeException(LOG,
-                                String.format("failed to forward init external db %s operation to master", name), e);
+        if (isInitializing) {
+            return;
+        }
+        isInitializing = true;
+        try {
+            extCatalog.makeSureInitialized();
+            if (!initialized) {
+                if (extCatalog.getUseMetaCache().get()) {
+                    if (metaCache == null) {
+                        metaCache = Env.getCurrentEnv().getExtMetaCacheMgr().buildMetaCache(
+                                name,
+                                OptionalLong.of(86400L),
+                                OptionalLong.of(Config.external_cache_expire_time_minutes_after_access * 60L),
+                                Config.max_meta_object_cache_num,
+                                ignored -> listTableNames(),
+                                localTableName -> Optional.ofNullable(
+                                        buildTableForInit(null, localTableName,
+                                                Util.genIdByName(extCatalog.getName(), name, localTableName),
+                                                extCatalog,
+                                                this, true)),
+                                (key, value, cause) -> value.ifPresent(ExternalTable::unsetObjectCreated));
                     }
-                    return;
+                    setLastUpdateTime(System.currentTimeMillis());
+                } else {
+                    if (!Env.getCurrentEnv().isMaster()) {
+                        // Forward to master and wait the journal to replay.
+                        int waitTimeOut = ConnectContext.get() == null ? 300 : ConnectContext.get().getExecTimeoutS();
+                        MasterCatalogExecutor remoteExecutor = new MasterCatalogExecutor(waitTimeOut * 1000);
+                        try {
+                            remoteExecutor.forward(extCatalog.getId(), id);
+                        } catch (Exception e) {
+                            Util.logAndThrowRuntimeException(LOG,
+                                    String.format("failed to forward init external db %s operation to master", name),
+                                    e);
+                        }
+                        return;
+                    }
+                    init();
                 }
-                init();
+                initialized = true;
             }
-            initialized = true;
+        } finally {
+            isInitializing = false;
         }
     }
 

commit 127a1c333e7b9e45f8382014da1b0e2c057c1b6a

[fix](test)(jdbc) fix some external regression test cases and jdbc bug (#52817)
Followup #52561
Also fix a bug introduced by #51471, which lost the function rules in
jdbc external table.

fe/fe-core/src/main/java/org/apache/doris/catalog/JdbcTable.java
@@ -36,6 +36,7 @@ import org.apache.commons.collections.map.CaseInsensitiveMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -100,6 +101,9 @@ public class JdbcTable extends Table {
     private boolean connectionPoolKeepAlive;
 
     private ExternalFunctionRules functionRules;
+    // This is used for edit log
+    @SerializedName("frs")
+    private String functionRulesString;
 
     static {
         Map<String, TOdbcTableType> tempMap = new CaseInsensitiveMap();
@@ -381,8 +385,9 @@ public class JdbcTable extends Table {
 
     private void checkAndSetExternalFunctionRules(Map<String, String> properties) throws DdlException {
         ExternalFunctionRules.check(properties.getOrDefault(JdbcResource.FUNCTION_RULES, ""));
-        this.functionRules = ExternalFunctionRules.create(jdbcTypeName,
-                properties.getOrDefault(JdbcResource.FUNCTION_RULES, ""));
+        String functionRulesString = properties.getOrDefault(JdbcResource.FUNCTION_RULES, "");
+        this.functionRules = ExternalFunctionRules.create(jdbcTypeName, functionRulesString);
+        this.functionRulesString = functionRulesString;
     }
 
     /**
@@ -491,4 +496,11 @@ public class JdbcTable extends Table {
     public ExternalFunctionRules getExternalFunctionRules() {
         return functionRules;
     }
+
+    @Override
+    public void gsonPostProcess() throws IOException {
+        super.gsonPostProcess();
+        functionRules = ExternalFunctionRules.create(jdbcTypeName, Strings.nullToEmpty(functionRulesString));
+    }
 }
+

fe/fe-core/src/main/java/org/apache/doris/datasource/iceberg/IcebergMetadataOps.java
@@ -339,6 +339,8 @@ public class IcebergMetadataOps implements ExternalMetadataOps {
         if (db.isPresent()) {
             db.get().setUnInitialized(true);
         }
+        LOG.info("after drop table {}.{}.{}. is db present: {}",
+                dorisCatalog.getName(), dbName, tblName, db.isPresent());
     }
 
     private void performDropTable(String remoteDbName, String remoteTblName, boolean ifExists) throws DdlException {

fe/fe-core/src/main/java/org/apache/doris/journal/bdbje/BDBJEJournal.java
@@ -248,7 +248,7 @@ public class BDBJEJournal implements Journal { // CHECKSTYLE IGNORE THIS LINE: B
             MetricRepo.COUNTER_CURRENT_EDIT_LOG_SIZE_BYTES.increase((long) theData.getSize());
         }
         if (LOG.isDebugEnabled() || theData.getSize() > (1 << 20)) {
-            LOG.info("opCode = {}, journal size = {}", op, theData.getSize());
+            LOG.info("opCode = {}, journal size = {}, log id: {}", op, theData.getSize(), id);
             if (MetricRepo.isInit) {
                 MetricRepo.COUNTER_LARGE_EDIT_LOG.increase(1L);
             }

regression-test/data/external_table_p0/iceberg/test_iceberg_hadoop_case_sensibility.out
@@ -64,6 +64,9 @@ test_iceberg_case_sensibility_hadoop	ICEBERG_HADOOP_CASE_DB2	case_tbl21	k1	1	\N	
 -- !sql18 --
 4
 
+-- !sql_show --
+case_tbl22
+
 -- !sql21 --
 5
 
@@ -159,6 +162,9 @@ test_iceberg_case_sensibility_hadoop	ICEBERG_HADOOP_CASE_DB2	case_tbl21	k1	1	\N	
 -- !sql18 --
 5
 
+-- !sql_show --
+case_tbl22
+
 -- !sql21 --
 5
 
@@ -253,6 +259,9 @@ test_iceberg_case_sensibility_hadoop	ICEBERG_HADOOP_CASE_DB2	case_tbl21	k1	1	\N	
 
 -- !sql18 --
 5
+
+-- !sql_show --
+case_tbl22
 
 -- !sql21 --
 5

regression-test/data/external_table_p0/iceberg/test_iceberg_rest_case_sensibility.out
@@ -64,6 +64,9 @@ test_iceberg_case_sensibility_rest	ICEBERG_REST_CASE_DB2	case_tbl21	k1	1	\N	YES	
 -- !sql18 --
 4
 
+-- !sql_show --
+case_tbl22
+
 -- !sql21 --
 5
 
@@ -159,6 +162,9 @@ test_iceberg_case_sensibility_rest	ICEBERG_REST_CASE_DB2	case_tbl21	k1	1	\N	YES	
 -- !sql18 --
 5
 
+-- !sql_show --
+case_tbl22
+
 -- !sql21 --
 5
 
@@ -253,6 +259,9 @@ test_iceberg_case_sensibility_rest	ICEBERG_REST_CASE_DB2	case_tbl21	k1	1	\N	YES	
 
 -- !sql18 --
 5
+
+-- !sql_show --
+case_tbl22
 
 -- !sql21 --
 5

regression-test/suites/external_table_p0/iceberg/test_iceberg_hadoop_case_sensibility.groovy
@@ -278,7 +278,7 @@ suite("test_iceberg_hadoop_case_sensibility", "p0,external,doris,external_docker
                 exception "Table [case_tbl22] does not exist in database [iceberg_hadoop_case_db2]"
             }
             sql """create table iceberg_hadoop_case_db2.case_tbl22 (k1 int);""" // recreate
-            sql """show tables from iceberg_hadoop_case_db2 like "%case_tbl22%""""
+            qt_sql_show """show tables from iceberg_hadoop_case_db2 like "%case_tbl22%""""
             
             sql """insert into iceberg_hadoop_case_db2.case_tbl22 values(5);"""
             order_qt_sql21 """select * from iceberg_hadoop_case_db2.case_tbl22;"""

regression-test/suites/external_table_p0/iceberg/test_iceberg_rest_case_sensibility.groovy
@@ -279,7 +279,7 @@ suite("test_iceberg_rest_case_sensibility", "p0,external,doris,external_docker,e
                 exception "Table [case_tbl22] does not exist in database [iceberg_rest_case_db2]"
             }
             sql """create table iceberg_rest_case_db2.case_tbl22 (k1 int);""" // recreate
-            sql """show tables from iceberg_rest_case_db2 like "%case_tbl22%""""
+            qt_sql_show """show tables from iceberg_rest_case_db2 like "%case_tbl22%""""
             
             sql """insert into iceberg_rest_case_db2.case_tbl22 values(5);"""
             order_qt_sql21 """select * from iceberg_rest_case_db2.case_tbl22;"""

regression-test/suites/external_table_p0/iceberg/write/test_iceberg_create_table.groovy
@@ -52,9 +52,7 @@ suite("test_iceberg_create_table", "p0,external,doris,external_docker,external_d
         String tb1 = db1 + "_tb1"
         String tb2 = db1 + "_tb2"
 
-        sql """ drop table if exists ${db1}.${tb1} """
-        sql """ drop table if exists ${db1}.${tb2} """
-        sql """ drop database if exists ${db1} """
+        sql """ drop database if exists ${db1} force"""
 
         sql """ create database ${db1} """
 

regression-test/suites/external_table_p0/tvf/test_file_tvf_hdfs.groovy
@@ -333,9 +333,9 @@ suite("test_file_tvf_hdfs","external,hive,tvf,external_docker") {
             // test create view from tvf and alter view from tvf
             uri = "${defaultFS}" + "/user/doris/preinstalled_data/csv_format_test/all_types.csv"
             format = "csv"
-            sql """ DROP VIEW IF EXISTS test_hdfs_tvf_create_view;"""
+            sql """ DROP VIEW IF EXISTS test_file_tvf_hdfs_create_view;"""
             sql """
-                create view test_hdfs_tvf_create_view as
+                create view test_file_tvf_hdfs_create_view as
                 select * from FILE(
                         "uri" = "${uri}",
                         "hadoop.username" = "${hdfsUserName}",
@@ -343,10 +343,10 @@ suite("test_file_tvf_hdfs","external,hive,tvf,external_docker") {
                         "format" = "${format}") order by c1;
                 """
 
-            order_qt_create_view """ select * from test_hdfs_tvf_create_view order by c1 limit 20; """
+            order_qt_create_view """ select * from test_file_tvf_hdfs_create_view order by c1 limit 20; """
 
             sql """
-                alter view test_hdfs_tvf_create_view as
+                alter view test_file_tvf_hdfs_create_view as
                 select c1 from FILE(
                         "uri" = "${uri}",
                         "hadoop.username" = "${hdfsUserName}",
@@ -354,7 +354,7 @@ suite("test_file_tvf_hdfs","external,hive,tvf,external_docker") {
                         "format" = "${format}") order by c1;
                 """
 
-            order_qt_alter_view """ select * from test_hdfs_tvf_create_view order by c1 limit 20; """
+            order_qt_alter_view """ select * from test_file_tvf_hdfs_create_view order by c1 limit 20; """
         } finally {
         }
     }

regression-test/suites/external_table_p0/tvf/upgrade/load.groovy
@@ -30,26 +30,26 @@ suite("test_tvf_upgrade_load", "p0,external,hive,external_docker,external_docker
             // test create view from tvf and alter view from tvf
             uri = "${defaultFS}" + "/user/doris/preinstalled_data/csv_format_test/all_types.csv"
             format = "csv"
-            sql """ DROP VIEW IF EXISTS test_hdfs_tvf_create_view;"""
+            sql """ DROP VIEW IF EXISTS test_tvf_upgrade_load_create_view;"""
             sql """
-                create view test_hdfs_tvf_create_view as
+                create view test_tvf_upgrade_load_create_view as
                 select * from HDFS(
                         "uri" = "${uri}",
                         "hadoop.username" = "${hdfsUserName}",
                         "column_separator" = ",",
                         "format" = "${format}") order by c1;
                 """
-            logger.info("View test_hdfs_tvf_create_view created")
+            logger.info("View test_tvf_upgrade_load_create_view created")
 
 
             sql """
-                alter view test_hdfs_tvf_create_view as
+                alter view test_tvf_upgrade_load_create_view as
                 select c1 from HDFS(
                         "uri" = "${uri}",
                         "hadoop.username" = "${hdfsUserName}",
                         "column_separator" = ",",
                         "format" = "${format}") order by c1;
                 """
-            logger.info("View test_hdfs_tvf_create_view altered")
+            logger.info("View test_tvf_upgrade_load_create_view altered")
     }
 }

regression-test/suites/external_table_p0/tvf/upgrade/test.groovy
@@ -27,8 +27,8 @@ suite("test_tvf_upgrade_test", "p0,external,hive,external_docker,external_docker
 
     String enabled = context.config.otherConfigs.get("enableHiveTest")
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
-        order_qt_create_view """ select * from test_hdfs_tvf_create_view order by c1 limit 20; """
+        order_qt_create_view """ select * from test_tvf_upgrade_load_create_view order by c1 limit 20; """
 
-        order_qt_alter_view """ select * from test_hdfs_tvf_create_view order by c1 limit 20; """
+        order_qt_alter_view """ select * from test_tvf_upgrade_load_create_view order by c1 limit 20; """
     }
 }

regression-test/suites/nereids_rules_p0/mv/external_table/part_partition_invalid.groovy
@@ -38,8 +38,7 @@ suite("part_partition_invalid", "p0,external,external_docker") {
     );"""
 
     sql """switch ${hive_catalog_name};"""
-    sql """drop table if exists ${hive_catalog_name}.${hive_database}.${hive_table}"""
-    sql """ drop database if exists ${hive_database}"""
+    sql """ drop database if exists ${hive_database} force"""
     sql """ create database ${hive_database}"""
     sql """use ${hive_database}"""
     sql """
@@ -203,9 +202,7 @@ suite("part_partition_invalid", "p0,external,external_docker") {
     mv_rewrite_success(query_sql, mv_name)
     order_qt_after_add_data_and_refresh_catalog_and_mv """ ${query_sql}"""
 
-    sql """drop table if exists ${hive_catalog_name}.${hive_database}.${hive_table}"""
-    sql """drop table if exists ${internal_catalog}.${olap_db}.${olap_table}"""
-    sql """drop database if exists ${hive_catalog_name}.${hive_database}"""
+    sql """drop database if exists ${hive_catalog_name}.${hive_database} force"""
     sql """drop materialized view if exists ${internal_catalog}.${olap_db}.${mv_name};"""
     sql """drop catalog if exists ${hive_catalog_name}"""
 }

regression-test/suites/nereids_rules_p0/mv/external_table/single_external_table.groovy
@@ -38,8 +38,7 @@ suite("single_external_table", "p0,external,hive") {
     );"""
 
     sql """switch ${hive_catalog_name};"""
-    sql """drop table if exists ${hive_catalog_name}.${hive_database}.${hive_table}"""
-    sql """ drop database if exists ${hive_database}"""
+    sql """ drop database if exists ${hive_database} force"""
     sql """ create database ${hive_database}"""
     sql """use ${hive_database}"""
     sql """
@@ -192,7 +191,6 @@ suite("single_external_table", "p0,external,hive") {
     sql """ DROP MATERIALIZED VIEW IF EXISTS mv1_5"""
 
 
-    sql """drop table if exists ${hive_catalog_name}.${hive_database}.${hive_table}"""
-    sql """drop database if exists ${hive_catalog_name}.${hive_database}"""
+    sql """drop database if exists ${hive_catalog_name}.${hive_database} force"""
     sql """drop catalog if exists ${hive_catalog_name}"""
 }