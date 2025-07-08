95ae6fc5ea3f4b5a332fafd068ef99b588115766
BiteTheDDDDt <xl@selectdb.com>
2025-07-08 15:35:35 +0800

    pass arena to agg function's add method

--- be/src/olap/schema_change.cpp
+++ be/src/olap/schema_change.cpp
@@ -163,7 +163,7 @@ public:
                     agg_functions[j - key_number]->add(
                             agg_places[j - key_number],
                             const_cast<const vectorized::IColumn**>(&column_ptr), row_ref.position,
-                            nullptr);
+                            &_arena);
                 }
 
                 if (i == rows - 1 || _cmp.compare(row_refs[i], row_refs[i + 1])) {
@@ -249,6 +249,7 @@ private:
 
     TabletSharedPtr _tablet;
     RowRefComparator _cmp;
+    vectorized::Arena _arena;
 };
 
 BlockChanger::BlockChanger(TabletSchemaSPtr tablet_schema, DescriptorTbl desc_tbl)
