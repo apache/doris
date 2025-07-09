692111064f43bdfeeb5f09f3f1dddfdfc8f211c9
daidai <changyuwei@selectdb.com>
2025-07-09 22:31:44 +0800

    [fix](hudi)add hudiOrcReader for read hudi table & orc format. (#52964)
    Related PR: #51341
    
    Problem Summary:
    In pr #51341, hudiOrcReader was deleted, and this pr reintroduced it to
    read hudi orc table.
    Although I encountered this error when testing spark-hudi to read orc,
    the orc file was indeed generated by spark-hudi.
    
    ```
    java.lang.UnsupportedOperationException: Base file format is not currently supported (ORC)
            at org.apache.hudi.HoodieBaseRelation.createBaseFileReader(HoodieBaseRelation.scala:574) ~[hudi-spark3.4-bundle_2.12-0.14.0-1.jar:0.14.0-1]
            at org.apache.hudi.BaseFileOnlyRelation.composeRDD(BaseFileOnlyRelation.scala:96) ~[hudi-spark3.4-bundle_2.12-0.14.0-1.jar:0.14.0-1]
            at org.apache.hudi.HoodieBaseRelation.buildScan(HoodieBaseRelation.scala:381) ~[hudi-spark3.4-bundle_2.12-0.14.0-1.jar:0.14.0-1]
            at org.apache.spark.sql.execution.datasources.DataSourceStrategy$.$anonfun$apply$4(DataSourceStrategy.scala:329) ~[spark-sql_2.12-3.4.2.jar:0.14.0-1]
    ```

--- be/src/vec/exec/scan/vfile_scanner.cpp
+++ be/src/vec/exec/scan/vfile_scanner.cpp
@@ -1115,6 +1115,16 @@ Status VFileScanner::_get_next_reader() {
                 RETURN_IF_ERROR(paimon_reader->init_row_filters());
                 _cur_reader = std::move(paimon_reader);
             } else if (range.__isset.table_format_params &&
+                       range.table_format_params.table_format_type == "hudi") {
+                std::unique_ptr<HudiOrcReader> hudi_reader = HudiOrcReader::create_unique(
+                        std::move(orc_reader), _profile, _state, *_params, range, _io_ctx.get());
+
+                init_status = hudi_reader->init_reader(
+                        _file_col_names, _colname_to_value_range, _push_down_conjuncts,
+                        _real_tuple_desc, _default_val_row_desc.get(),
+                        &_not_single_slot_filter_conjuncts, &_slot_id_to_filter_conjuncts);
+                _cur_reader = std::move(hudi_reader);
+            } else if (range.__isset.table_format_params &&
                        range.table_format_params.table_format_type == "hive") {
                 std::unique_ptr<HiveOrcReader> hive_reader = HiveOrcReader::create_unique(
                         std::move(orc_reader), _profile, _state, *_params, range, _io_ctx.get());
@@ -1220,12 +1230,16 @@ Status VFileScanner::_get_next_reader() {
             break;
         }
         default:
-            return Status::InternalError("Not supported file format: {}", _params->format_type);
+            return Status::NotSupported("Not supported create reader for file format: {}.",
+                                        to_string(_params->format_type));
         }
 
         if (_cur_reader == nullptr) {
-            return Status::InternalError("Failed to create reader for  file format: {}",
-                                         _params->format_type);
+            return Status::NotSupported(
+                    "Not supported create reader for table format: {} / file format: {}.",
+                    range.__isset.table_format_params ? range.table_format_params.table_format_type
+                                                      : "NotSet",
+                    to_string(_params->format_type));
         }
         COUNTER_UPDATE(_file_counter, 1);
         // The VFileScanner for external table may try to open not exist files,
