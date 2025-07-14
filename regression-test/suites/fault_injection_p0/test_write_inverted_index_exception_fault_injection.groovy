07fa7180c3fd2a590bb9a3efbbaff85def4e5fb3
airborne12 <jiangkai@selectdb.com>
2025-07-14 19:10:16 +0800

    [fix](inverted index) fix index memeory leak and refact some code (#53164)

--- regression-test/suites/fault_injection_p0/test_write_inverted_index_exception_fault_injection.groovy
+++ regression-test/suites/fault_injection_p0/test_write_inverted_index_exception_fault_injection.groovy
@@ -219,7 +219,6 @@ suite("test_write_inverted_index_exception_fault_injection", "nonConcurrent") {
         "InvertedIndexColumnWriterImpl::new_char_token_stream__char_string_reader_init_error",
         "InvertedIndexColumnWriterImpl::add_values_field_is_nullptr",
         "InvertedIndexColumnWriterImpl::add_values_index_writer_is_nullptr",
-        "InvertedIndexColumnWriterImpl::add_array_values_count_is_zero",
         "InvertedIndexColumnWriterImpl::add_array_values_index_writer_is_nullptr",
         "InvertedIndexColumnWriterImpl::add_array_values_create_field_error",
         "InvertedIndexColumnWriterImpl::add_array_values_create_field_error_2",
@@ -262,13 +261,7 @@ suite("test_write_inverted_index_exception_fault_injection", "nonConcurrent") {
                     GetDebugPoint().enableDebugPointForAllBEs(debug_point)
                     run_insert("${tableName}")
                     check_count("${tableName}", 6)
-                    // if debug_point equals InvertedIndexColumnWriterImpl::add_array_values_count_is_zero, run_select(false(abnormal))
-                    // else run_select(true(normal))
-                    if (debug_point == "InvertedIndexColumnWriterImpl::add_array_values_count_is_zero") {
-                        run_select("${tableName}", false)
-                    } else {
-                        run_select("${tableName}", true)
-                    }
+                    run_select("${tableName}", true)
                     sql "TRUNCATE TABLE ${tableName}"
                 } catch (Exception e) {
                     log.error("Caught exception: ${e}")
