35279a670dcc6538b39e7eae75df5159d2c22bd8
zhangstar333 <zhangsida@selectdb.com>
2025-07-16 17:51:36 +0800

    branch-3.1: [feature](Nereids): Convert log(x) to ln(x) #52862 (#53276)
    cherry-picked from #52862

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionOptimization.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionOptimization.java
@@ -24,6 +24,7 @@ import org.apache.doris.nereids.rules.expression.rules.DateFunctionRewrite;
 import org.apache.doris.nereids.rules.expression.rules.DistinctPredicatesRule;
 import org.apache.doris.nereids.rules.expression.rules.ExtractCommonFactorRule;
 import org.apache.doris.nereids.rules.expression.rules.LikeToEqualRewrite;
+import org.apache.doris.nereids.rules.expression.rules.LogToLn;
 import org.apache.doris.nereids.rules.expression.rules.NullSafeEqualToEqual;
 import org.apache.doris.nereids.rules.expression.rules.SimplifyComparisonPredicate;
 import org.apache.doris.nereids.rules.expression.rules.SimplifyConflictCompound;
@@ -61,7 +62,8 @@ public class ExpressionOptimization extends ExpressionRewrite {
                 TopnToMax.INSTANCE,
                 NullSafeEqualToEqual.INSTANCE,
                 LikeToEqualRewrite.INSTANCE,
-                BetweenToEqual.INSTANCE
+                BetweenToEqual.INSTANCE,
+                LogToLn.INSTANCE
             )
     );
     private static final ExpressionRuleExecutor EXECUTOR = new ExpressionRuleExecutor(OPTIMIZE_REWRITE_RULES);
