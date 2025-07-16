881f8fdf86a99098cf1e34772a22b7653f087d0d
Socrates <suyiteng@selectdb.com>
2025-07-16 19:46:26 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/datasource/iceberg/source/IcebergScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/iceberg/source/IcebergScanNode.java
@@ -202,7 +202,7 @@ public class IcebergScanNode extends FileQueryScanNode {
             }
         }
         tableFormatFileDesc.setIcebergParams(fileDesc);
-        rangeDesc.setDataLakePartitionValues(icebergSplit.getPartitionValues());
+        rangeDesc.setDataLakePartitionValues(icebergSplit.getIcebergPartitionValues());
         rangeDesc.setTableFormatParams(tableFormatFileDesc);
     }
 
@@ -342,6 +342,7 @@ public class IcebergScanNode extends FileQueryScanNode {
                 String partitionString = IcebergUtils.toPartitionString(field.type(), value);
                 partitionValues.put(field.name(), partitionString);
             }
+            split.setIcebergPartitionValues(partitionValues);
             // Counts the number of partitions read
             partitionPathSet.add(partitionData.toString());
         }
