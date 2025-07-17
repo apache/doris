0e63e36734a9d67fd5eb9b7f1f0737def39b4d97
Gabriel <liwenqiang@selectdb.com>
2025-07-17 11:36:18 +0800

    update

--- be/src/pipeline/pipeline_task.cpp
+++ be/src/pipeline/pipeline_task.cpp
@@ -198,10 +198,7 @@ Status PipelineTask::_open() {
     _dry_run = _sink->should_dry_run(_state);
     for (auto& o : _operators) {
         auto* local_state = _state->get_local_state(o->operator_id());
-        auto st = local_state->open(_state);
-        DCHECK(st.is<ErrorCode::PIP_WAIT_FOR_RF>() ? !_filter_dependencies.empty() : true)
-                << debug_string();
-        RETURN_IF_ERROR(st);
+        RETURN_IF_ERROR(local_state->open(_state));
     }
     RETURN_IF_ERROR(_state->get_sink_local_state()->open(_state));
     RETURN_IF_ERROR(_extract_dependencies());
