c451863cf887fbe745a58f6fd3f3eb58ceecfd47
amory <wangqiannan@selectdb.com>
2025-07-18 13:58:51 +0800

    branch-3.0: [fix](variant)fix variant type conflicts in nested types (#52696) (#53123)
    cherry-pick: https://github.com/apache/doris/pull/52696 and #53418

--- regression-test/suites/variant_p0/load.groovy
+++ regression-test/suites/variant_p0/load.groovy
@@ -14,6 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+import org.awaitility.Awaitility
 
 suite("regression_test_variant", "p0"){
 
