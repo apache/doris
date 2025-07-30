dc15249c313e70067d5da21ad6d3b553b25b6254
/bin/eash <zhangzhiyongde@126.com>
2025-07-30 16:32:20 +0800

    fix Hive catalog query fails due to partition ID conflicts

--- fe/fe-core/src/test/java/org/apache/doris/common/util/UtilTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/common/util/UtilTest.java
@@ -83,4 +83,11 @@ public class UtilTest {
                 "java.lang.Exception: Root cause message With suppressed[0]:Suppressed message",
                 Util.getRootCauseWithSuppressedMessage(chainedException));
     }
+
+    @Test
+    public void sha256longEcoding() {
+        String str = "东南卫视";
+        String str1 = "东方卫视";
+        Assertions.assertNotEquals(Util.sha256long(str), Util.sha256long(str1));
+    }
 }
