3b2356f272232a949768aa10a441a497cecf60f3
0AyanamiRei <3244156674@qq.com>
2025-08-01 03:52:50 +0800

    [fix](function) compile fix

--- be/src/vec/aggregate_functions/aggregate_function_geomean.cpp
+++ be/src/vec/aggregate_functions/aggregate_function_geomean.cpp
@@ -1,17 +1,17 @@
 // Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements. See the NOTICE file
+// or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
-// regarding copyright ownership. The ASF licenses this file
+// regarding copyright ownership.  The ASF licenses this file
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at
+// with the License.  You may obtain a copy of the License at
 //
 //   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. See the License for the
+// KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
 
@@ -25,9 +25,11 @@ namespace doris::vectorized {
 
 template <PrimitiveType T>
 AggregateFunctionPtr create_agg_function_geomean(const DataTypes& argument_types,
-                                                 const bool result_is_nullable) {
+                                                 const bool result_is_nullable,
+                                                 const AggregateFunctionAttr& attr) {
     return creator_without_type::create<AggregateFunctionGeomean<T>>(argument_types,
-                                                                     result_is_nullable);
+                                                                    result_is_nullable,
+                                                                    attr);
 }
 
 AggregateFunctionPtr create_aggregate_function_geomean(const std::string& name,
@@ -40,7 +42,7 @@ AggregateFunctionPtr create_aggregate_function_geomean(const std::string& name,
         return nullptr;
     }
 
-    return create_agg_function_geomean<TYPE_DOUBLE>(argument_types, result_is_nullable);
+    return create_agg_function_geomean<TYPE_DOUBLE>(argument_types, result_is_nullable, attr);
 }
 
 void register_aggregate_function_geomean(AggregateFunctionSimpleFactory& factory) {
