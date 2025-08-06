8521528b718c3bdd32bafd21832b5ce6da5bcb48
Yongqiang YANG <yangyongqiang@selectdb.com>
2025-08-06 16:45:40 +0800

    [fix](case) remove global variable declarations in compaction test [f… (#54360)
    …iles
    
    - Add 'def' declaration for tabletId variables in for loops
    - Add 'def result = null' declaration before using result variables
    - Fix global variable issues in compaction1-11 test files
    - Ensure proper local variable scope in Groovy test scripts
    
    Co-authored-by: Yongqiang YANG <yangyogqiang@selectdb.com>

--- regression-test/suites/cloud_p0/schema_change/compaction1/test_schema_change_with_compaction1.groovy
+++ regression-test/suites/cloud_p0/schema_change/compaction1/test_schema_change_with_compaction1.groovy
@@ -123,6 +123,7 @@ suite('test_schema_change_with_compaction1', 'nonConcurrent') {
             DebugPoint.disableDebugPoint(injectBe.Host, injectBe.HttpPort.toInteger(), NodeType.BE, injectName)
         }
         int max_try_time = 3000
+        def result = null
         while (max_try_time--){
             result = getJobState("date")
             if (result == "FINISHED" || result == "CANCELLED") {
--- regression-test/suites/cloud_p0/schema_change/compaction5/test_schema_change_with_compaction5.groovy
+++ regression-test/suites/cloud_p0/schema_change/compaction5/test_schema_change_with_compaction5.groovy
@@ -143,7 +143,7 @@ suite('test_schema_change_with_compaction5', 'docker') {
 
             // cu compaction
             for (int i = 0; i < array.size(); i++) {
-                tabletId = array[i].TabletId
+                def tabletId = array[i].TabletId
                 logger.info("run compaction:" + tabletId)
                 (code, out, err) = be_run_cumulative_compaction(injectBe.Host, injectBe.HttpPort, tabletId)
                 logger.info("Run compaction: code=" + code + ", out=" + out + ", err=" + err)
@@ -153,7 +153,7 @@ suite('test_schema_change_with_compaction5', 'docker') {
                 running = true
                 do {
                     Thread.sleep(100)
-                    tabletId = array[i].TabletId
+                    def tabletId = array[i].TabletId
                     (code, out, err) = be_get_compaction_status(injectBe.Host, injectBe.HttpPort, tabletId)
                     logger.info("Get compaction status: code=" + code + ", out=" + out + ", err=" + err)
                     assertEquals(code, 0)
@@ -172,6 +172,7 @@ suite('test_schema_change_with_compaction5', 'docker') {
                 GetDebugPoint().disableDebugPointForAllBEs(injectName)
             }
             int max_try_time = 3000
+            def result = null
             while (max_try_time--){
                 result = getJobState("date")
                 if (result == "FINISHED" || result == "CANCELLED") {
