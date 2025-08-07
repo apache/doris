d840ebdfeddd7bdd815c4c9a7a07802ae957fcf5
Lei Zhang <zhanglei@selectdb.com>
2025-08-07 16:13:55 +0800

    [fix](txn lazy commit) fix multi table load error with txn lazy commit (#54322)
    * we maintain a map for cache tablet_id -> (table_id,
    index_id,partition_id) and we use
    `tablet_ids.begin()->second.table_id()` to get table_id, there will be
    problem when multi table load which table1‘s partition key encode may
    use table2's table_id
    * unordered_map.begin is not stable to reproduce the problem， so we use
    map to reproduce the problem surely

--- cloud/src/meta-service/meta_service_txn.cpp
+++ cloud/src/meta-service/meta_service_txn.cpp
@@ -1934,6 +1934,7 @@ void commit_txn_eventually(
         std::shared_ptr<TxnLazyCommitTask> task = txn_lazy_committer->submit(instance_id, txn_id);
         TEST_SYNC_POINT_CALLBACK("commit_txn_eventually::txn_lazy_committer_wait", &txn_id);
         std::pair<MetaServiceCode, std::string> ret = task->wait();
+        TEST_SYNC_POINT_CALLBACK("commit_txn_eventually::task->wait", &ret);
         if (ret.first != MetaServiceCode::OK) {
             LOG(WARNING) << "txn lazy commit failed txn_id=" << txn_id << " code=" << ret.first
                          << " msg=" << ret.second;
