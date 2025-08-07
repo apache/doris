3098c62e7856120d9dfa71cf378ccccdecbffca4
amory <wangqiannan@selectdb.com>
2025-08-07 15:22:11 +0800

    [fix](variant) change session var for disable_variant_flatten_nested to enable_variant_flatten_nested (#54413)

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -693,9 +693,9 @@ public class SessionVariable implements Serializable, Writable {
 
     public static final String DISABLE_INVERTED_INDEX_V1_FOR_VARIANT = "disable_inverted_index_v1_for_variant";
 
-    // disable variant flatten nested as session variable, default is true,
-    // which means disable variant flatten nested when create table
-    public static final String DISABLE_VARIANT_FLATTEN_NESTED = "disable_variant_flatten_nested";
+    // enable variant flatten nested as session variable, default is false,
+    // which means do not flatten nested when create table
+    public static final String ENABLE_VARIANT_FLATTEN_NESTED = "enable_variant_flatten_nested";
 
     // CLOUD_VARIABLES_BEGIN
     public static final String CLOUD_CLUSTER = "cloud_cluster";
@@ -1311,8 +1311,8 @@ public class SessionVariable implements Serializable, Writable {
     @VariableMgr.VarAttr(name = DISABLE_INVERTED_INDEX_V1_FOR_VARIANT, needForward = true)
     private boolean disableInvertedIndexV1ForVaraint = true;
 
-    @VariableMgr.VarAttr(name = DISABLE_VARIANT_FLATTEN_NESTED, needForward = true)
-    private boolean disableVariantFlattenNested = true;
+    @VariableMgr.VarAttr(name = ENABLE_VARIANT_FLATTEN_NESTED, needForward = true)
+    private boolean enableVariantFlattenNested = false;
 
     public int getBeNumberForTest() {
         return beNumberForTest;
@@ -4933,12 +4933,12 @@ public class SessionVariable implements Serializable, Writable {
         return disableInvertedIndexV1ForVaraint;
     }
 
-    public void setDisableVariantFlattenNested(boolean disableVariantFlattenNested) {
-        this.disableVariantFlattenNested = disableVariantFlattenNested;
+    public void setEnableVariantFlattenNested(boolean enableVariantFlattenNested) {
+        this.enableVariantFlattenNested = enableVariantFlattenNested;
     }
 
-    public boolean getDisableVariantFlattenNested() {
-        return disableVariantFlattenNested;
+    public boolean getEnableVariantFlattenNested() {
+        return enableVariantFlattenNested;
     }
 
     public void checkSqlConvertorFeatures(String features) {
