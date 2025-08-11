985aa4c3e7381d6d6dc21d52cdef5172330e722a
yaoxiao <yaoxiao@fosun.com>
2025-08-11 23:32:07 +0800

    remove CancelLoadStmt

--- fe/fe-core/src/main/java/org/apache/doris/qe/DdlExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/DdlExecutor.java
@@ -22,7 +22,6 @@ import org.apache.doris.analysis.AlterRoleStmt;
 import org.apache.doris.analysis.AlterSqlBlockRuleStmt;
 import org.apache.doris.analysis.AlterTableStmt;
 import org.apache.doris.analysis.CancelExportStmt;
-import org.apache.doris.analysis.CancelLoadStmt;
 import org.apache.doris.analysis.CreateEncryptKeyStmt;
 import org.apache.doris.analysis.CreateIndexPolicyStmt;
 import org.apache.doris.analysis.CreateJobStmt;
@@ -50,7 +49,6 @@ import org.apache.doris.catalog.EncryptKeyHelper;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.DdlException;
-import org.apache.doris.job.exception.JobException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -78,14 +76,6 @@ public class DdlExecutor {
             env.alterTable((AlterTableStmt) ddlStmt);
         } else if (ddlStmt instanceof CancelExportStmt) {
             env.getExportMgr().cancelExportJob((CancelExportStmt) ddlStmt);
-        } else if (ddlStmt instanceof CancelLoadStmt) {
-            CancelLoadStmt cs = (CancelLoadStmt) ddlStmt;
-            // cancel all
-            try {
-                env.getJobManager().cancelLoadJob(cs);
-            } catch (JobException e) {
-                env.getLoadManager().cancelLoadJob(cs);
-            }
         } else if (ddlStmt instanceof CreateRoutineLoadStmt) {
             env.getRoutineLoadManager().createRoutineLoadJob((CreateRoutineLoadStmt) ddlStmt);
         } else if (ddlStmt instanceof CreateJobStmt) {
