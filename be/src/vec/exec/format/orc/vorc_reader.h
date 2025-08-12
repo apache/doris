a9ebcb80f0555713fc69b5afdd6811edb95f3655
daidai <changyuwei@selectdb.com>
2025-08-12 11:09:02 +0800

    branch-3.0:[fix](orc)fix core that iceberg-orc reader read miss iceberg.id orc file.(#49051) (#54167)
    ### What problem does this PR solve?
    pick #49051
    but only fix:
    ```
    terminate called after throwing an instance of 'std::range_error'
      what():  Key not found: iceberg.id
    *** Query id: 6a93d7cdc9f44370-a40b07934a14c81b ***
    *** is nereids: 1 ***
    *** tablet id: 0 ***
    *** Aborted at 1753842428 (unix time) try "date -d @1753842428" if you are using GNU date ***
    *** Current BE git commitID: 910c4249c5 ***
    *** SIGABRT unknown detail explain (@0x5a46f) received by PID 369775 (TID 371694 OR 0x7fad067ef640) from PID 369775; stack trace: ***
    terminate called recursively
    terminate called recursively
    terminate called recursively
    terminate called recursively
     0# doris::signal::(anonymous namespace)::FailureSignalHandler(int, siginfo_t*, void*) at /home/zcp/repo_center/doris_release/doris/be/src/common/signal_handler.h:421
     1# 0x00007FB12263EBF0 in /lib64/libc.so.6
     2# __pthread_kill_implementation in /lib64/libc.so.6
     3# gsignal in /lib64/libc.so.6
     4# abort in /lib64/libc.so.6
     5# __gnu_cxx::__verbose_terminate_handler() [clone .cold] at ../../../../libstdc++-v3/libsupc++/vterminate.cc:75
     6# __cxxabiv1::__terminate(void (*)()) at ../../../../libstdc++-v3/libsupc++/eh_terminate.cc:48
     7# 0x000055C047B28EC1 in /opt/apache-doris-3.0.6.2-bin-x64/be/lib/doris_be
     8# 0x000055C047B29014 in /opt/apache-doris-3.0.6.2-bin-x64/be/lib/doris_be
     9# orc::TypeImpl::getAttributeValue(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&) const in /opt/apache-doris-3.0.6.2-bin-x64/be/lib/doris_be
    10# doris::vectorized::OrcReader::get_schema_col_name_attribute(std::vector<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::allocator<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > >*, std::vector<unsigned long, std::allocator<unsigned long> >*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >) at /home/zcp/repo_center/doris_release/doris/be/src/vec/exec/format/orc/vorc_reader.cpp:332
    11# doris::vectorized::IcebergOrcReader::_gen_col_name_maps(doris::vectorized::OrcReader*) at 
    ```

--- be/src/vec/exec/format/orc/vorc_reader.h
+++ be/src/vec/exec/format/orc/vorc_reader.h
@@ -144,6 +144,7 @@ public:
     //If you want to read the file by index instead of column name, set hive_use_column_names to false.
     Status init_reader(
             const std::vector<std::string>* column_names,
+            const std::vector<std::string>& missing_column_names,
             const std::unordered_map<std::string, ColumnValueRangeType>* colname_to_value_range,
             const VExprContextSPtrs& conjuncts, bool is_acid,
             const TupleDescriptor* tuple_descriptor, const RowDescriptor* row_descriptor,
@@ -184,8 +185,8 @@ public:
                              std::vector<TypeDescriptor>* col_types) override;
 
     Status get_schema_col_name_attribute(std::vector<std::string>* col_names,
-                                         std::vector<uint64_t>* col_attributes,
-                                         std::string attribute);
+                                         std::vector<int32_t>* col_attributes,
+                                         const std::string& attribute, bool* exist_attribute);
     void set_table_col_to_file_col(
             std::unordered_map<std::string, std::string> table_col_to_file_col) {
         _table_col_to_file_col = table_col_to_file_col;
@@ -566,6 +567,10 @@ private:
     int64_t _range_size;
     const std::string& _ctz;
     const std::vector<std::string>* _column_names;
+    // _missing_column_names_set: used in iceberg/hudi/paimon, the columns are dropped
+    // but added back(drop column a then add column a). Shouldn't read this column data in this case.
+    std::set<std::string> _missing_column_names_set;
+
     int32_t _offset_days = 0;
     cctz::time_zone _time_zone;
 
@@ -590,7 +595,7 @@ private:
     OrcProfile _orc_profile;
 
     std::unique_ptr<orc::ColumnVectorBatch> _batch;
-    std::unique_ptr<orc::Reader> _reader;
+    std::unique_ptr<orc::Reader> _reader = nullptr;
     std::unique_ptr<orc::RowReader> _row_reader;
     std::unique_ptr<ORCFilterImpl> _orc_filter;
     orc::RowReaderOptions _row_reader_options;
