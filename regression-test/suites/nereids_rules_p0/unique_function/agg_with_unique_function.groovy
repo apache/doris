0a6416f6e671b65097d329d8fa933d78e8de7380
yujun <yujun@selectdb.com>
2025-08-13 14:39:52 +0800

    update test

--- regression-test/suites/nereids_rules_p0/unique_function/agg_with_unique_function.groovy
+++ regression-test/suites/nereids_rules_p0/unique_function/agg_with_unique_function.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite('test_unique_function') {
+suite('agg_with_unique_function') {
     sql 'SET enable_nereids_planner=true'
     sql 'SET runtime_filter_mode=OFF'
     sql 'SET enable_fallback_to_original_planner=false'
@@ -67,71 +67,7 @@ suite('test_unique_function') {
         }
     }
 
-    qt_filter_through_project_1 '''
-        explain shape plan select * from (select id + 100 as a, id + 200 as b, id + 300 as c from t1) t where a > 999 and b > 999
-        '''
-
-    qt_filter_through_project_2 '''
-        explain shape plan select * from (select id + random(1, 10) + 100 as a, id + 200 as b, id + 300 as c from t1) t where a > 999 and b > 999
-        '''
-
-    qt_filter_through_project_3 '''
-        explain shape plan select * from (select id + random(1, 10) + 100 as a, id + random(1, 10) + 200 as b, id + random(1, 10) + 300 as c from t1) t where a > 999 and b > 999
-        '''
-
-    qt_filter_through_project_4 '''
-        explain shape plan select * from (select id + 100 as a, id + 200 as b, id + 300 as c from t1) t where a + random(1, 10) > 999 and b + random(1, 10) > 999
-        '''
-
-    qt_filter_through_project_5 '''
-        explain shape plan select * from (select id + 100 as a, id + 200 as b, id + 300 as c from t1) t where a > 999 and b > 999 limit 10
-        '''
-
-    qt_filter_through_project_6 '''
-        explain shape plan select * from (select id + random(1, 10) + 100 as a, id + 200 as b, id + 300 as c from t1) t where a > 999 and b > 999 limit 10
-        '''
-
-    qt_filter_through_project_7 '''
-        explain shape plan select * from (select id + random(1, 10) + 100 as a, id + random(1, 10) + 200 as b, id + random(1, 10) + 300 as c from t1) t where a > 999 and b > 999 limit 10
-        '''
-
-    qt_filter_through_project_8 '''
-        explain shape plan select * from (select id + 100 as a, id + 200 as b, id + 300 as c from t1) t where a + random(1, 10) > 999 and b + random(1, 10) > 999 limit 10
-        '''
-
-    qt_merge_project_1 '''
-        explain shape plan select a as b, a as c from (select id + 100 as a from t1) t
-        '''
-
-    qt_merge_project_2 '''
-        explain shape plan select a as b, a as c from (select id + random(1, 10) as a from t1) t
-        '''
-
-    qt_merge_project_3 '''
-        explain shape plan select a as b from (select id + random(1, 10) as a from t1) t
-        '''
-
-    qt_merge_project_4 '''
-        explain shape plan select a + 10 + a as b from (select id + random(1, 10) as a from t1) t
-        '''
-
-    qt_merge_project_5 '''
-        explain shape plan select a as b, a + 10 as c from (select id + random(1, 10) as a from t1) t
-        '''
-
-    qt_push_down_filter_through_agg '''
-        explain shape plan
-        select id, sum(id)
-        from t1
-        where id + random(1, 10) > 5
-        group by id
-        having id + random(1, 10) > 6
-        '''
-
-    def tbl = "unique_scalar_function_tbl1"
-    sql "drop table if exists ${tbl} force"
-    sql "create table ${tbl} (k int, a double) properties('replication_num' = '1')"
-    sql "insert into ${tbl} values (0, 0)"
+    def tbl = "tbl_unique_function_with_one_row"
 
     // test no aggregate
     explainAndCheckResult [[0], [1]], 'check_equal_no_agg_1', """
