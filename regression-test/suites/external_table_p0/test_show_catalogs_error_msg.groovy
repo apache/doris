b0ade61f4073153499bffcf7801cd4d2b2f769b6
morningman <yunyou@selectdb.com>
2025-08-15 04:59:45 +0800

    3

--- regression-test/suites/external_table_p0/test_show_catalogs_error_msg.groovy
+++ regression-test/suites/external_table_p0/test_show_catalogs_error_msg.groovy
@@ -24,18 +24,18 @@ suite("test_show_catalogs_error_msg", "p0,external,iceberg,external_docker,exter
     }
 
     try {
-
-        String rest_port = 181812; // use a wrong port
+        String rest_port = context.config.otherConfigs.get("iceberg_rest_uri_port");
         String minio_port = context.config.otherConfigs.get("iceberg_minio_port")
         String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
         String catalog_name = "test_show_catalogs_error_msg"
 
         sql """drop catalog if exists ${catalog_name}"""
+        // use wrong port 181812
         sql """
         CREATE CATALOG ${catalog_name} PROPERTIES (
                'type'='iceberg',
                'iceberg.catalog.type'='rest',
-               'uri' = 'http://${externalEnvIp}:${rest_port}',
+               'uri' = 'http://${externalEnvIp}:181812',
                "s3.access_key" = "admin",
                "s3.secret_key" = "password",
                "s3.endpoint" = "http://${externalEnvIp}:${minio_port}",
@@ -58,8 +58,22 @@ suite("test_show_catalogs_error_msg", "p0,external,iceberg,external_docker,exter
                 }
             }
         }
-
         assertTrue(found, "failed to find invalid catalog") 
+
+        // change to right port, the error msg will be removed
+        sql """alter catalog test_show_catalogs_error_msg set properties('uri' = 'http://127.0.0.1:${rest_port}');"""
+        sql """show databases from test_show_catalogs_error_msg"""
+        res = sql """show catalogs"""
+        for (List<Object> line : res) {
+            logger.info("get show catalogs line: " + line + ", name: " + line[1] + ", msg: " + line[7]);
+            if (line[1].equals("test_show_catalogs_error_msg")) {
+                if (line[7].isEmpty()) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        assertTrue(found, "failed to find valid catalog") 
 
         // sql """drop catalog if exists ${catalog_name}"""
 
