7c378e44d14f51cb55b333a2cd88f73f06121ba9
w41ter <maochuan@selectdb.com>
2025-08-14 12:19:35 +0000

    make gain happy

--- cloud/src/meta-service/meta_service_partition.cpp
+++ cloud/src/meta-service/meta_service_partition.cpp
@@ -730,7 +730,7 @@ void MetaServiceImpl::drop_partition(::google::protobuf::RpcController* controll
     drop_partition_log.set_db_id(request->db_id());
     drop_partition_log.set_table_id(request->table_id());
     drop_partition_log.mutable_index_ids()->CopyFrom(request->index_ids());
-    drop_partition_log.set_expiration_at_s(request->expiration());
+    drop_partition_log.set_expired_at_s(request->expiration());
 
     for (auto part_id : request->partition_ids()) {
         auto key = recycle_partition_key({instance_id, part_id});
