4102457c98c55e6a6ab0c71e040dfc03d21b5465
seawinde <wusi@selectdb.com>
2025-08-18 10:19:35 +0800

    add ut init

--- fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/JoinCommuteTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/nereids/rules/exploration/join/JoinCommuteTest.java
@@ -20,6 +20,7 @@ package org.apache.doris.nereids.rules.exploration.join;
 import org.apache.doris.common.Pair;
 import org.apache.doris.nereids.trees.expressions.GreaterThan;
 import org.apache.doris.nereids.trees.plans.JoinType;
+import org.apache.doris.nereids.trees.plans.Plan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalJoin;
 import org.apache.doris.nereids.trees.plans.logical.LogicalOlapScan;
 import org.apache.doris.nereids.trees.plans.logical.LogicalPlan;
@@ -32,6 +33,8 @@ import org.apache.doris.nereids.util.PlanConstructor;
 import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class JoinCommuteTest implements MemoPatternMatchSupported {
     @Test
@@ -72,5 +75,26 @@ public class JoinCommuteTest implements MemoPatternMatchSupported {
         Assertions.assertEquals(1, PlanChecker.from(MemoTestUtils.createConnectContext(), join)
                 .applyExploration(JoinCommute.BUSHY.build())
                 .getAllPlan().size());
+    }
+
+
+    @Test
+    void testJoinConjunctNullableWhenCommute() {
+        LogicalOlapScan scan1 = PlanConstructor.newLogicalOlapScan(0, "t1", 0);
+        LogicalOlapScan scan2 = PlanConstructor.newLogicalOlapScan(1, "t2", 0);
+
+        LogicalJoin<?, ?> join = (LogicalJoin<?, ?>) new LogicalPlanBuilder(scan1)
+                .join(scan2, JoinType.LEFT_OUTER_JOIN, Pair.of(0, 0))
+                .build();
+        join = join.withJoinConjuncts(
+                ImmutableList.of(),
+                ImmutableList.of(new GreaterThan(scan1.getOutput().get(0).withNullable(true),
+                        scan2.getOutput().get(0))),
+                join.getJoinReorderContext());
+
+        List<Plan> allPlan = PlanChecker.from(MemoTestUtils.createConnectContext(), join)
+                .applyExploration(JoinCommute.BUSHY.build())
+                .getAllPlan();
+        Assertions.assertEquals(1, allPlan.size());
     }
 }
