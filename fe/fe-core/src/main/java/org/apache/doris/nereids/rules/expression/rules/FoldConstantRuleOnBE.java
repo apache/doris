2feaa3d16f22fa88424f6cec8e10c3a9a437ef51
James <lijibing@selectdb.com>
2025-08-18 11:50:24 +0800

    Forbid constant fold for json type.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnBE.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnBE.java
@@ -227,7 +227,7 @@ public class FoldConstantRuleOnBE implements ExpressionPatternRuleFactory {
         // Frontend can not represent those types
         if (expr.getDataType().isAggStateType() || expr.getDataType().isObjectType()
                 || expr.getDataType().isVariantType() || expr.getDataType().isTimeType()
-                || expr.getDataType().isIPv6Type()) {
+                || expr.getDataType().isIPv6Type() || expr.getDataType().isJsonType()) {
             return true;
         }
 
