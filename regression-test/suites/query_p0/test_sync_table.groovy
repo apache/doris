c8c973de38e767c63c1b8596c935c40856c27ebd
Dino123-maker <1275505164@qq.com>
2025-08-20 16:30:02 +0800

    modify test file

--- regression-test/suites/query_p0/test_sync_table.groovy
+++ regression-test/suites/query_p0/test_sync_table.groovy
@@ -1,3 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 suite("test_sync_table") {
     def tableName = "test_sync_table_tbl"
 
@@ -17,14 +33,14 @@ suite("test_sync_table") {
 
     sql """set group_commit = async_mode;"""
 
-    // 插入数据后，不等待直接查，结果为空
     sql """INSERT INTO ${tableName} VALUES (1, "tom"), (2, "jerry");"""
 
+    // cannot view data begin
     qt_select1 """SELECT COUNT(*) FROM ${tableName};"""
 
-    // 执行sync table
+    // run sync table
     sql """SYNC TABLE ${tableName};"""
 
-    // 数据已经写入
+    // can view data
     qt_select2 """SELECT COUNT(*) FROM ${tableName};"""
 }
