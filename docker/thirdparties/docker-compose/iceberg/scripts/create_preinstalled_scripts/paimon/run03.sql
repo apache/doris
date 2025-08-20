cdfa2b93698c48732d02cb996337ed11099c348b
vinlee19 <xiaowenli@selectdb.com>
2025-08-20 11:44:11 +0800

    modify case

--- docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/paimon/run03.sql
+++ docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/paimon/run03.sql
@@ -8,12 +8,11 @@ drop table if exists test_varchar_char_type;
 create table test_varchar_char_type (
     c1 int,
     c2 char(1),
-    c3 char(255),
-    c4 char(256),
-    c5 char(2147483647),
-    c6 varchar(1),
-    c7 varchar(65533),
-    c8 varchar(2147483646),
-    c9 varchar(2147483647),
-    c10 string
+    c3 char(254),
+    c4 char(2147483647),
+    c5 varchar(1),
+    c6 varchar(65533),
+    c7 varchar(2147483646),
+    c8 varchar(2147483647),
+    c9 string
 );
