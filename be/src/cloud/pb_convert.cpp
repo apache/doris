2efcd9e8c93e9f7dbb443567e8a3f4c1c0d89438
Luwei <luwei@selectdb.com>
2025-08-20 18:06:53 +0800

    fix comment

--- be/src/cloud/pb_convert.cpp
+++ be/src/cloud/pb_convert.cpp
@@ -596,7 +596,9 @@ void doris_tablet_meta_to_cloud(TabletMetaCloudPB* out, TabletMetaPB&& in) {
     if (in.has_schema_version()) {
         out->set_schema_version(in.schema_version());
     }
-    out->set_encryption_algorithm(in.encryption_algorithm());
+    if (in.has_encryption_algorithm()) {
+        out->set_encryption_algorithm(in.encryption_algorithm());
+    }
 }
 
 TabletMetaPB cloud_tablet_meta_to_doris(const TabletMetaCloudPB& in) {
@@ -672,7 +674,9 @@ void cloud_tablet_meta_to_doris(TabletMetaPB* out, const TabletMetaCloudPB& in) 
     if (in.has_schema_version()) {
         out->set_schema_version(in.schema_version());
     }
-    out->set_encryption_algorithm(in.encryption_algorithm());
+    if (in.has_encryption_algorithm()) {
+        out->set_encryption_algorithm(in.encryption_algorithm());
+    }
 }
 
 void cloud_tablet_meta_to_doris(TabletMetaPB* out, TabletMetaCloudPB&& in) {
@@ -739,7 +743,9 @@ void cloud_tablet_meta_to_doris(TabletMetaPB* out, TabletMetaCloudPB&& in) {
     if (in.has_schema_version()) {
         out->set_schema_version(in.schema_version());
     }
-    out->set_encryption_algorithm(in.encryption_algorithm());
+    if (in.has_encryption_algorithm()) {
+        out->set_encryption_algorithm(in.encryption_algorithm());
+    }
 }
 
 } // namespace doris::cloud
