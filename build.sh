apache/doris

commit ce921bf92993c6fef8e34cd886f6a2e841f32f13

[Enhancement](build) disable build azure on arm and suppress libunwind build warning (#54521)
fix: move target system and architecture detection to env.sh and set DISABLE_BUILD_AZURE based on architecture
fix: add warning suppression for incompatible pointer types in libunwind build

build.sh
@@ -36,10 +36,6 @@ fi
 export TP_INCLUDE_DIR="${DORIS_THIRDPARTY}/installed/include"
 export TP_LIB_DIR="${DORIS_THIRDPARTY}/installed/lib"
 
-TARGET_SYSTEM="$(uname -s)"
-TARGET_ARCH="$(uname -m)"
-echo "Target system: ${TARGET_SYSTEM}; Target arch: ${TARGET_ARCH}"
-
 . "${DORIS_HOME}/env.sh"
 
 # Check args
@@ -462,7 +458,7 @@ if [[ -z "${DISABLE_JAVA_CHECK_STYLE}" ]]; then
     DISABLE_JAVA_CHECK_STYLE='OFF'
 fi
 
-if [[ -n "${DISABLE_BUILD_AZURE}" || "${TARGET_SYSTEM}" == 'Darwin' ]]; then
+if [[ -n "${DISABLE_BUILD_AZURE}" ]]; then
     BUILD_AZURE='OFF'
 fi
 

thirdparty/build-thirdparty.sh
@@ -1572,7 +1572,7 @@ build_libunwind() {
         # LIBUNWIND_NO_HEAP: https://reviews.llvm.org/D11897
         # LIBUNWIND_IS_NATIVE_ONLY: https://lists.llvm.org/pipermail/cfe-commits/Week-of-Mon-20160523/159802.html
         # -nostdinc++ only required for gcc compilation
-        cflags="-I${TP_INCLUDE_DIR} -std=c99 -D_LIBUNWIND_NO_HEAP=1 -D_DEBUG -D_LIBUNWIND_IS_NATIVE_ONLY -O3 -fno-exceptions -funwind-tables -fno-sanitize=all -nostdinc++ -fno-rtti"
+        cflags="-I${TP_INCLUDE_DIR} -std=c99 -D_LIBUNWIND_NO_HEAP=1 -D_DEBUG -D_LIBUNWIND_IS_NATIVE_ONLY -O3 -fno-exceptions -funwind-tables -fno-sanitize=all -nostdinc++ -fno-rtti -Wno-error=incompatible-pointer-types"
         CFLAGS="${cflags}" LDFLAGS="-L${TP_LIB_DIR} -llzma" ../configure --prefix="${TP_INSTALL_DIR}" --disable-shared --enable-static
 
         make -j "${PARALLEL}"

env.sh
@@ -25,6 +25,10 @@ if [[ -z "${DORIS_HOME}" ]]; then
     exit 1
 fi
 
+TARGET_SYSTEM="$(uname -s)"
+TARGET_ARCH="$(uname -m)"
+echo "Target system: ${TARGET_SYSTEM}; Target arch: ${TARGET_ARCH}"
+
 # check OS type
 if [[ -n "${OSTYPE}" ]]; then
     if [[ "${OSTYPE}" != "linux-gnu" ]] && [[ "${OSTYPE:0:6}" != "darwin" ]]; then
@@ -95,6 +99,15 @@ fi
 # set DORIS_THIRDPARTY
 if [[ -z "${DORIS_THIRDPARTY}" ]]; then
     export DORIS_THIRDPARTY="${DORIS_HOME}/thirdparty"
+fi
+
+# set DISABLE_BUILD_AZURE
+if [[ -z "${DISABLE_BUILD_AZURE}" ]]; then
+    if [[ "${TARGET_ARCH}" == *arm* || "${TARGET_ARCH}" == "aarch64" || "${TARGET_SYSTEM}" == 'Darwin' ]]; then
+        export DISABLE_BUILD_AZURE='ON'
+    else
+        export DISABLE_BUILD_AZURE='OFF'
+    fi
 fi
 
 # check python

commit d961ea692a3836f91716b042672b48fbbb676eb0

[Fix](deps) let thirdparty build libazure by default in x86 (#55111)
Problem Summary:

bug introduced in https://github.com/apache/doris/pull/54521 make we
didn't compile libazure for thirdparty.

btw, `env.sh` may affect thirdparty's build. so if it changed, trigger
on building thirdparty pipeline.

.github/workflows/build-thirdparty.yml
@@ -53,6 +53,7 @@ jobs:
           filters: |
             thirdparty_changes:
               - 'thirdparty/**'
+              - 'env.sh'
 
   build_linux:
     name: Build Third Party Libraries (Linux)

build.sh
@@ -441,7 +441,7 @@ if [[ -z "${DISABLE_JAVA_CHECK_STYLE}" ]]; then
     DISABLE_JAVA_CHECK_STYLE='OFF'
 fi
 
-if [[ "${DISABLE_BUILD_AZURE^^}" == "ON" ]]; then
+if [[ "$(echo "${DISABLE_BUILD_AZURE}" | tr '[:lower:]' '[:upper:]')" == "ON" ]]; then
     BUILD_AZURE='OFF'
 fi
 

thirdparty/build-thirdparty.sh
@@ -121,7 +121,7 @@ if [[ "${HELP}" -eq 1 ]]; then
     usage
 fi
 
-if [[ -n "${DISABLE_BUILD_AZURE}" ]]; then
+if [[ "$(echo "${DISABLE_BUILD_AZURE}" | tr '[:lower:]' '[:upper:]')" == "ON" ]]; then
     BUILD_AZURE='OFF'
 fi
 