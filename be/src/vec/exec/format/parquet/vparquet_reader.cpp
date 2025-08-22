2a445265efb29a246960520e97748b3b7f3e0cbb
daidai <changyuwei@selectdb.com>
2025-08-22 17:44:54 +0800

    [fix](iceberg)fix iceberg read page index core when equal delete.

--- be/src/vec/exec/format/parquet/vparquet_reader.cpp
+++ be/src/vec/exec/format/parquet/vparquet_reader.cpp
@@ -1030,7 +1030,8 @@ Status ParquetReader::_process_page_index(const tparquet::RowGroup& row_group,
         return Status::OK();
     }
     PageIndex page_index;
-    if (!config::enable_parquet_page_index || !_has_page_index(row_group.columns, page_index)) {
+    if (!config::enable_parquet_page_index || !_has_page_index(row_group.columns, page_index) ||
+        _colname_to_slot_id == nullptr) {
         read_whole_row_group();
         return Status::OK();
     }
@@ -1059,8 +1060,10 @@ Status ParquetReader::_process_page_index(const tparquet::RowGroup& row_group,
     for (size_t idx = 0; idx < _read_table_columns.size(); idx++) {
         const auto& read_table_col = _read_table_columns[idx];
         const auto& read_file_col = _read_file_columns[idx];
-
-        DCHECK(_colname_to_slot_id != nullptr && _colname_to_slot_id->contains(read_table_col));
+        if (!_colname_to_slot_id->contains(read_table_col)) {
+            // equal delete may add column to read_table_col, but this column no slot_id.
+            continue;
+        }
         auto slot_id = _colname_to_slot_id->at(read_table_col);
         if (!_push_down_simple_expr.contains(slot_id)) {
             continue;
