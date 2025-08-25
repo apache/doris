5a254779dec0afb1a5f03340c3df505fb174d08d
Jack <jiangkai@selectdb.com>
2025-08-25 18:13:48 +0800

    [feature](inverted index) introduce config enable_new_partition_inverted_index_v2_format to set new partiton inverted index format v2 (#55069)
    Problem Summary:
    This PR introduces a new configuration
    enable_new_partition_inverted_index_v2_format to enable V2 storage
    format for inverted indexes in newly created partitions. When enabled,
    new partitions will use V2 format regardless of the table's original
    format, allowing for mixed-format partition scenarios.

--- fe/fe-core/src/main/java/org/apache/doris/cloud/datasource/CloudInternalCatalog.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/datasource/CloudInternalCatalog.java
@@ -166,7 +166,13 @@ public class CloudInternalCatalog extends InternalCatalog {
             Cloud.CreateTabletsRequest.Builder requestBuilder = Cloud.CreateTabletsRequest.newBuilder();
             List<String> rowStoreColumns =
                     tbl.getTableProperty().getCopiedRowStoreColumns();
+            TInvertedIndexFileStorageFormat effectiveIndexStorageFormat =
+                        (Config.enable_new_partition_inverted_index_v2_format
+                                && tbl.getInvertedIndexFileStorageFormat() == TInvertedIndexFileStorageFormat.V1)
+                                ? TInvertedIndexFileStorageFormat.V2
+                                : tbl.getInvertedIndexFileStorageFormat();
             for (Tablet tablet : index.getTablets()) {
+                // Use resolved format that considers global override for new partitions
                 OlapFile.TabletMetaCloudPB.Builder builder = createTabletMetaBuilder(tbl.getId(), indexId,
                         partitionId, tablet, tabletType, schemaHash, keysType, shortKeyColumnCount,
                         bfColumns, tbl.getBfFpp(), indexes, columns, tbl.getDataSortInfo(),
@@ -179,7 +185,7 @@ public class CloudInternalCatalog extends InternalCatalog {
                         tbl.getTimeSeriesCompactionLevelThreshold(),
                         tbl.disableAutoCompaction(),
                         tbl.getRowStoreColumnsUniqueIds(rowStoreColumns),
-                        tbl.getInvertedIndexFileStorageFormat(),
+                        effectiveIndexStorageFormat,
                         tbl.rowStorePageSize(),
                         tbl.variantEnableFlattenNested(), clusterKeyUids,
                         tbl.storagePageSize(),
