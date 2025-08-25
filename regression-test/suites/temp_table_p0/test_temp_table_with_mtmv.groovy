0898e26cd1b62254b32f17e8307bdf1ccc170acc
pengyu <pengyu@selectdb.com>
2025-08-25 20:48:06 +0800

    remove case picked in another PR

--- regression-test/suites/temp_table_p0/test_temp_table_with_mtmv.groovy
+++ regression-test/suites/temp_table_p0/test_temp_table_with_mtmv.groovy
@@ -24,7 +24,6 @@ suite("test_temp_table_with_mtmv", "p0") {
     sql "create database ${dbName}"
     sql "use ${dbName}"
 
-    // FIXME: this case only work if replication num is not 1
     sql """create table ${tableName}(a int, b int, c int, d int) properties("replication_num" = "1") """
     sql """insert into ${tableName} values(1,2,3,4)"""
     sql """create materialized view ${mtmvName} BUILD IMMEDIATE REFRESH COMPLETE as select * from ${tableName} """
