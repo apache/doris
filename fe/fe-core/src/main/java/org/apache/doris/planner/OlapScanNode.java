bfad27a50b707cdf61e7edd89e24b7b06a4cd73e
924060929 <lanhuajian@selectdb.com>
2025-08-26 22:21:47 +0800

    optimize point query performance

--- fe/fe-core/src/main/java/org/apache/doris/planner/OlapScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/OlapScanNode.java
@@ -1174,12 +1174,17 @@ public class OlapScanNode extends ScanNode {
          * The tablet info could be computed only once.
          * So the scanBackendIds should be empty in the beginning.
          */
-        Preconditions.checkState(scanBackendIds.size() == 0);
-        Preconditions.checkState(scanTabletIds.size() == 0);
+        Preconditions.checkState(scanBackendIds.isEmpty());
+        Preconditions.checkState(scanTabletIds.isEmpty());
         Map<Long, Set<Long>> backendAlivePathHashs = Maps.newHashMap();
         for (Backend backend : Env.getCurrentSystemInfo().getAllClusterBackendsNoException().values()) {
-            backendAlivePathHashs.put(backend.getId(), backend.getDisks().values().stream()
-                    .filter(DiskInfo::isAlive).map(DiskInfo::getPathHash).collect(Collectors.toSet()));
+            Set<Long> hashSet = Sets.newLinkedHashSet();
+            for (DiskInfo diskInfo : backend.getDisks().values()) {
+                if (diskInfo.isAlive()) {
+                    hashSet.add(diskInfo.getPathHash());
+                }
+            }
+            backendAlivePathHashs.put(backend.getId(), hashSet);
         }
 
         for (Long partitionId : selectedPartitionIds) {
