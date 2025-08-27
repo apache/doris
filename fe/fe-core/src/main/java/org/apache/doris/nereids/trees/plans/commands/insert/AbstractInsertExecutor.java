b0025dca160e8e52e48c1f84e2ad8927b616308b
kakachen <chenqi@selectdb.com>
2025-08-27 13:31:16 +0800

    test

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
@@ -43,7 +43,9 @@ import org.apache.doris.thrift.TStatusCode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Abstract insert executor.
@@ -68,6 +70,13 @@ public abstract class AbstractInsertExecutor {
     protected final boolean emptyInsert;
     protected long txnId = INVALID_TXN_ID;
 
+    public interface InsertExecutorListener {
+        void beforeComplete(AbstractInsertExecutor insertExecutor, StmtExecutor executor, long jobId) throws Exception;
+        void afterComplete(AbstractInsertExecutor insertExecutor, StmtExecutor executor, long jobId) throws Exception;
+    }
+
+    private List<InsertExecutorListener> listeners = new CopyOnWriteArrayList<>();
+
     /**
      * Constructor
      */
@@ -80,6 +89,14 @@ public abstract class AbstractInsertExecutor {
         this.database = table.getDatabase();
         this.insertCtx = insertCtx;
         this.emptyInsert = emptyInsert;
+    }
+
+    public void registerListener(InsertExecutorListener listener) {
+        listeners.add(listener);
+    }
+
+    public void unregisterListener(InsertExecutorListener listener) {
+        listeners.remove(listener);
     }
 
     public Coordinator getCoordinator() {
@@ -200,7 +217,13 @@ public abstract class AbstractInsertExecutor {
             executor.updateProfile(false);
             execImpl(executor, jobId);
             checkStrictModeAndFilterRatio();
+            for (InsertExecutorListener listener : listeners) {
+                listener.beforeComplete(this, executor, jobId);
+            }
             onComplete();
+            for (InsertExecutorListener listener : listeners) {
+                listener.afterComplete(this, executor, jobId);
+            }
         } catch (Throwable t) {
             onFail(t);
             // retry insert into from select when meet E-230 in cloud
@@ -211,7 +234,7 @@ public abstract class AbstractInsertExecutor {
         } finally {
             coordinator.close();
             executor.updateProfile(true);
-            QeProcessorImpl.INSTANCE.unregisterQuery(ctx.queryId());
+            // QeProcessorImpl.INSTANCE.unregisterQuery(ctx.queryId());
         }
         afterExec(executor);
     }
