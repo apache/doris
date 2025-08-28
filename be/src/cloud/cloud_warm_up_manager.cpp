apache/doris

commit 13ed5b7f5ce37d0f67d7ad94eed3307f13c54669

[metrics](warmup) add some metrics for warmup jobs #52991 (#52739) (#53189)
pick #52991 

Add the following metrics:

* `file_cache_once_or_periodic_warm_up_submitted_tablet_num`
* `file_cache_once_or_periodic_warm_up_finished_tablet_num`

Fix the following metrics:

* `file_cache_once_or_periodic_warm_up_finished_segment_num`
* `file_cache_once_or_periodic_warm_up_finished_segment_size`
* `file_cache_once_or_periodic_warm_up_finished_index_num`
* `file_cache_once_or_periodic_warm_up_finished_index_size`

be/src/cloud/cloud_warm_up_manager.cpp
@@ -48,6 +48,10 @@ bvar::Adder<uint64_t> g_file_cache_event_driven_warm_up_requested_index_size(
         "file_cache_event_driven_warm_up_requested_index_size");
 bvar::Adder<uint64_t> g_file_cache_event_driven_warm_up_requested_index_num(
         "file_cache_event_driven_warm_up_requested_index_num");
+bvar::Adder<uint64_t> g_file_cache_once_or_periodic_warm_up_submitted_tablet_num(
+        "file_cache_once_or_periodic_warm_up_submitted_tablet_num");
+bvar::Adder<uint64_t> g_file_cache_once_or_periodic_warm_up_finished_tablet_num(
+        "file_cache_once_or_periodic_warm_up_finished_tablet_num");
 bvar::Adder<uint64_t> g_file_cache_once_or_periodic_warm_up_submitted_segment_size(
         "file_cache_once_or_periodic_warm_up_submitted_segment_size");
 bvar::Adder<uint64_t> g_file_cache_once_or_periodic_warm_up_submitted_segment_num(
@@ -110,6 +114,13 @@ void CloudWarmUpManager::submit_download_tasks(io::Path path, int64_t file_size,
             return;
         }
     }
+    if (is_index) {
+        g_file_cache_once_or_periodic_warm_up_submitted_index_num << 1;
+        g_file_cache_once_or_periodic_warm_up_submitted_index_size << file_size;
+    } else {
+        g_file_cache_once_or_periodic_warm_up_submitted_segment_num << 1;
+        g_file_cache_once_or_periodic_warm_up_submitted_segment_size << file_size;
+    }
 
     if (is_index) {
         g_file_cache_once_or_periodic_warm_up_submitted_index_num << 1;
@@ -139,9 +150,19 @@ void CloudWarmUpManager::submit_download_tasks(io::Path path, int64_t file_size,
                                 .is_dryrun = config::enable_reader_dryrun_when_download_file_cache,
                         },
                 .download_done =
-                        [wait](Status st) {
+                        [=](Status st) {
                             if (!st) {
                                 LOG_WARNING("Warm up error ").error(st);
+                            } else if (is_index) {
+                                g_file_cache_once_or_periodic_warm_up_finished_index_num
+                                        << (offset == 0 ? 1 : 0);
+                                g_file_cache_once_or_periodic_warm_up_finished_index_size
+                                        << current_chunk_size;
+                            } else {
+                                g_file_cache_once_or_periodic_warm_up_finished_segment_num
+                                        << (offset == 0 ? 1 : 0);
+                                g_file_cache_once_or_periodic_warm_up_finished_segment_size
+                                        << current_chunk_size;
                             }
                             wait->signal();
                         },
@@ -260,6 +281,7 @@ void CloudWarmUpManager::handle_jobs() {
                     }
                 }
             }
+            g_file_cache_once_or_periodic_warm_up_finished_tablet_num << 1;
         }
 
         timespec time;
@@ -327,6 +349,7 @@ void CloudWarmUpManager::add_job(const std::vector<TJobMeta>& job_metas) {
         std::lock_guard lock(_mtx);
         std::for_each(job_metas.begin(), job_metas.end(), [this](const TJobMeta& meta) {
             _pending_job_metas.emplace_back(std::make_shared<JobMeta>(meta));
+            g_file_cache_once_or_periodic_warm_up_submitted_tablet_num << meta.tablet_ids.size();
         });
     }
     _cond.notify_all();

commit c82e418724d05113f64f2b7579a7fefdd6607976

branch-3.0: [fix](warmup) remove duplicate adding of periodic job metrics (#55283)
### What problem does this PR solve?

Problem Summary:
Some metrics were added twice, remove duplicated adding.
This is caused by #53189 PR merge issue.

### Release note

None

### Check List (For Author)

- Test <!-- At least one of them must be included. -->
    - [ ] Regression test
    - [ ] Unit Test
    - [ ] Manual test (add detailed scripts or steps below)
    - [ ] No need to test or manual test. Explain why:
- [ ] This is a refactor/code format and no logic has been changed.
        - [ ] Previous test can cover this change.
        - [ ] No code files have been changed.
        - [ ] Other reason <!-- Add your reason?  -->

- Behavior changed:
    - [ ] No.
    - [ ] Yes. <!-- Explain the behavior change -->

- Does this need documentation?
    - [ ] No.
- [ ] Yes. <!-- Add document PR link here. eg:
https://github.com/apache/doris-website/pull/1214 -->

### Check List (For Reviewer who merge this PR)

- [ ] Confirm the release note
- [ ] Confirm test cases
- [ ] Confirm document
- [ ] Add branch pick label <!-- Add branch pick label that this PR
should merge into -->

be/src/cloud/cloud_warm_up_manager.cpp
@@ -122,14 +122,6 @@ void CloudWarmUpManager::submit_download_tasks(io::Path path, int64_t file_size,
         g_file_cache_once_or_periodic_warm_up_submitted_segment_size << file_size;
     }
 
-    if (is_index) {
-        g_file_cache_once_or_periodic_warm_up_submitted_index_num << 1;
-        g_file_cache_once_or_periodic_warm_up_submitted_index_size << file_size;
-    } else {
-        g_file_cache_once_or_periodic_warm_up_submitted_segment_num << 1;
-        g_file_cache_once_or_periodic_warm_up_submitted_segment_size << file_size;
-    }
-
     const int64_t chunk_size = 10 * 1024 * 1024; // 10MB
     int64_t offset = 0;
     int64_t remaining_size = file_size;