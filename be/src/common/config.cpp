5a2c6f262dd4b3ef7cb7045e4150f2b43b8fae3a
bobhan1 <baohan@selectdb.com>
2025-09-02 14:05:34 +0800

    turn on by default

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1586,8 +1586,8 @@ DEFINE_mInt32(omp_threads_limit, "8");
 // The capacity of segment partial column cache, used to cache column readers for each segment.
 DEFINE_mInt32(max_segment_partial_column_cache_size, "100");
 
-DEFINE_mBool(enable_prefill_output_dbm_agg_cache_after_compaction, "false");
-DEFINE_mBool(enable_prefill_all_dbm_agg_cache_after_compaction, "false");
+DEFINE_mBool(enable_prefill_output_dbm_agg_cache_after_compaction, "true");
+DEFINE_mBool(enable_prefill_all_dbm_agg_cache_after_compaction, "true");
 
 // clang-format off
 #ifdef BE_TEST
