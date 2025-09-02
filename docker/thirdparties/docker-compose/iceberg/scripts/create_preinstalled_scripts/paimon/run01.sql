2ca3ff6907e833dd5e28f8283a82f5df24f2861b
Socrates <suxiaogang223@icloud.com>
2025-09-02 10:43:59 +0800

    fix

--- docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/paimon/run01.sql
+++ docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/paimon/run01.sql
@@ -68,3 +68,15 @@ CREATE TABLE date_partition (
 );
 
 insert into date_partition values(1,date '2020-01-01');
+
+SET TIME ZONE '+08:00';
+
+CREATE TABLE IF NOT EXISTS t_ts_ntz (
+  id INT,
+  ts TIMESTAMP,
+  ts_ntz TIMESTAMP_NTZ
+) USING paimon;
+
+INSERT INTO t_ts_ntz VALUES
+  (1, CAST('2025-08-12 06:00:00+00:00' AS TIMESTAMP), CAST('2025-08-12 06:00:00' AS TIMESTAMP_NTZ)),
+  (2, CAST('2025-08-12 14:00:00+08:00' AS TIMESTAMP), CAST('2025-08-12 14:00:00' AS TIMESTAMP_NTZ));
