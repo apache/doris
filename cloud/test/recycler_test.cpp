0af28afd42dea9b91643086b1db9efa1e6b29d2b
Uniqueyou <wangyixuan@selectdb.com>
2025-09-04 15:35:52 +0800

    [opt](recycler) Add concurrency recycle for tmp rowset (#52677)
    after pr
    
    | Concurrency | Trial 1 | Trial 2 | Trial 3 |
    |-------------|---------|---------|---------|
    | 1           | 38973   | 36734   | 34172   |
    | 5           | 20714   | 20136   | 22548   |
    | 10          | 14780   | 15022   | 15851   |

--- cloud/test/recycler_test.cpp
+++ cloud/test/recycler_test.cpp
@@ -1449,9 +1449,11 @@ TEST(RecyclerTest, bench_recycle_rowsets) {
 
 TEST(RecyclerTest, recycle_tmp_rowsets) {
     config::retention_seconds = 0;
+    // cloud::config::fdb_cluster_file_path = "fdb.cluster";
+    // auto txn_kv = std::dynamic_pointer_cast<cloud::TxnKv>(std::make_shared<cloud::FdbTxnKv>());
     auto txn_kv = std::make_shared<MemTxnKv>();
     ASSERT_EQ(txn_kv->init(), 0);
-
+    config::instance_recycler_worker_pool_size = 10;
     InstanceInfoPB instance;
     instance.set_instance_id(instance_id);
     instance.set_multi_version_status(MULTI_VERSION_WRITE_ONLY);
@@ -1494,11 +1496,11 @@ TEST(RecyclerTest, recycle_tmp_rowsets) {
     int64_t txn_id_base = 114115;
     int64_t tablet_id_base = 10015;
     int64_t index_id_base = 1000;
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 50; ++i) {
         int64_t txn_id = txn_id_base + i;
-        for (int j = 0; j < 20; ++j) {
+        for (int j = 0; j < 1000; ++j) {
             auto rowset = create_rowset("recycle_tmp_rowsets", tablet_id_base + j,
-                                        index_id_base + j % 4, 5, schemas[i % 5], txn_id);
+                                        index_id_base + j, 5, schemas[i % 5], txn_id);
             create_tmp_rowset(txn_kv.get(), accessor.get(), rowset, i & 1, false, i < 50);
         }
     }
@@ -1506,7 +1508,12 @@ TEST(RecyclerTest, recycle_tmp_rowsets) {
         check_delete_bitmap_keys_size(txn_kv.get(), tablet_id_base + j, 50);
     }
 
+    auto start = std::chrono::steady_clock::now();
     ASSERT_EQ(recycler.recycle_tmp_rowsets(), 0);
+    auto finish = std::chrono::steady_clock::now();
+    std::cout << "recycle tmp rowsets cost="
+              << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()
+              << std::endl;
 
     // check rowset does not exist on obj store
     std::unique_ptr<ListIterator> list_iter;
@@ -1521,8 +1528,9 @@ TEST(RecyclerTest, recycle_tmp_rowsets) {
     ASSERT_EQ(txn->get(begin_key, end_key, &it), TxnErrorCode::TXN_OK);
     ASSERT_EQ(it->size(), 0);
     // Check InvertedIndexIdCache
-    EXPECT_EQ(insert_inverted_index, 16);
-    EXPECT_EQ(insert_no_inverted_index, 4);
+    // EXPECT_GE for InvertedIndexIdCache::get
+    EXPECT_GE(insert_inverted_index, 4000);
+    EXPECT_GE(insert_no_inverted_index, 1000);
     // check rowset ref count key has been deleted
     begin_key = versioned::data_rowset_ref_count_key({instance_id, 0, ""});
     end_key = versioned::data_rowset_ref_count_key({instance_id, INT64_MAX, ""});
