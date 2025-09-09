8c02b27cc90e50aa2397b8262c69bfc14e51eb3e
happenlee <happenlee@selectdb.com>
2025-09-09 12:06:27 +0800

    unblock materalization

---
+++ be/src/pipeline/exec/materialization_opertor.h
@@ -0,0 +1,10 @@
+//
+// Created by lihaopeng on 9/9/25.
+//
+
+#ifndef DORIS_MATERIALIZATION_OPERTOR_H
+#define DORIS_MATERIALIZATION_OPERTOR_H
+
+class materialization_opertor {};
+
+#endif //DORIS_MATERIALIZATION_OPERTOR_H
