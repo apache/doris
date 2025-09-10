3a8939cd18c19e20388166eaac61ec79fec5e3a3
0AyanamiRei <3244156674@qq.com>
2025-09-10 15:38:04 +0800

    code format

--- be/src/olap/rowset_builder.cpp
+++ be/src/olap/rowset_builder.cpp
@@ -169,10 +169,10 @@ Status RowsetBuilder::check_tablet_version_count() {
         if (injection || !GlobalMemoryArbitrator::is_exceed_soft_mem_limit(GB_EXCHANGE_BYTE)) {
             //trigger compaction
             auto st = _engine.submit_compaction_task(tablet_sptr(),
-                                                    CompactionType::CUMULATIVE_COMPACTION, true);
+                                                     CompactionType::CUMULATIVE_COMPACTION, true);
             if (!st.ok()) [[unlikely]] {
                 LOG(WARNING) << "failed to trigger compaction, tablet_id=" << _tablet->tablet_id()
-                            << " : " << st;
+                             << " : " << st;
             }
         }
     }
