9566ecabafd9a6ec7ed6833a1eabef4e339576dd
Luwei <luwei@selectdb.com>
2025-09-12 15:06:03 +0800

    fix code style

--- be/src/olap/rowset/beta_rowset_writer.cpp
+++ be/src/olap/rowset/beta_rowset_writer.cpp
@@ -377,8 +377,7 @@ Status BetaRowsetWriter::_load_noncompacted_segment(segment_v2::SegmentSharedPtr
             .is_doris_table = true,
             .cache_base_path {},
     };
-    auto s = segment_v2::Segment::open(fs, path,
-                                       _rowset_meta->tablet_id(), segment_id, rowset_id(),
+    auto s = segment_v2::Segment::open(fs, path, _rowset_meta->tablet_id(), segment_id, rowset_id(),
                                        _context.tablet_schema, reader_options, &segment);
     if (!s.ok()) {
         LOG(WARNING) << "failed to open segment. " << path << ":" << s;
