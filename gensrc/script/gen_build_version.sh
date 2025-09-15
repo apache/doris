f0da8c60f6b5db06bf1568a50f68b1accfbfe58e
yiguolei <guolei@selectdb.com>
2025-09-15 11:11:10 +0800

    4.0.0-rc01

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -33,11 +33,11 @@ if [[ $1 != "" ]]; then
 fi
 
 build_version_prefix="${vendor}"
-build_version_major=0
+build_version_major=4
 build_version_minor=0
 build_version_patch=0
 build_version_hotfix=0
-build_version_rc_version=""
+build_version_rc_version="rc01"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}"
 if [[ ${build_version_hotfix} -gt 0 ]]; then
