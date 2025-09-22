0cd38838b8f2dd9d84c03d529c69971ce29384a7
yaoxiao <yaoxiao@fosun.com>
2025-09-22 21:39:44 +0800

    optimize ut fe

--- fe/fe-core/src/test/java/org/apache/doris/datasource/systable/SysTableTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/datasource/systable/SysTableTest.java
@@ -19,6 +19,7 @@ package org.apache.doris.datasource.systable;
 
 import org.apache.doris.common.Pair;
 import org.apache.doris.nereids.trees.plans.commands.info.TableValuedFunctionRefInfo;
+
 import org.junit.Assert;
 import org.junit.Test;
 
