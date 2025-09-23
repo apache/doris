8ee91c30e578e7d1c7855538b00225f22b4e37c3
shuke987 <shuke@selectdb.com>
2025-09-23 21:25:29 +0800

    [fix](case) bug

--- regression-test/suites/load_p0/spark_load/test_spark_load.groovy
+++ regression-test/suites/load_p0/spark_load/test_spark_load.groovy
@@ -137,6 +137,7 @@ suite("test_spark_load", "p0,external") {
             sql "DROP TABLE IF EXISTS ${testTable2}"
             create_test_table.call(testTable)
             create_test_table.call(testTable2)
+            create_spark_resource.call("spark", "yarn", "default")
             def test_load_label = UUID.randomUUID().toString().replaceAll("-", "")
             load_from_hdfs.call(testTable, testTable2, test_load_label, hdfs_txt_file_path1, hdfs_txt_file_path2)
             check_load_result.call(test_load_label, testTable, testTable2)
