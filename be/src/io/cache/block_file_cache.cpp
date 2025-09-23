7192fc9d9845651147ab2bfb9b9b7addb7f89aa0
zhengyu <zhangzhengyu@selectdb.com>
2025-09-23 14:24:29 +0800

    Update block_file_cache.cpp

--- be/src/io/cache/block_file_cache.cpp
+++ be/src/io/cache/block_file_cache.cpp
@@ -2216,7 +2216,8 @@ bool BlockFileCache::try_reserve_during_async_load(size_t size,
 void BlockFileCache::clear_need_update_lru_blocks() {
     std::vector<FileBlockSPtr> buffer;
     buffer.reserve(1024);
-    while (_need_update_lru_blocks.try_dequeue_bulk(buffer.data(), buffer.capacity())) {}
+    while (_need_update_lru_blocks.try_dequeue_bulk(buffer.data(), buffer.capacity())) {
+    }
 }
 
 std::string BlockFileCache::clear_file_cache_directly() {
