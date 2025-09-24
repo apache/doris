73c006aab59c16e3a8e06f9f912b9969f164a46c
Kaijie Chen <chenkaijie@selectdb.com>
2025-09-24 11:40:22 +0800

    [metrics](warmup) add bvar for skipped warmup rowsets (#56301)
    ### What problem does this PR solve?
    
    Problem Summary:
    
    Adds the bvar `file_cache_event_driven_warm_up_skipped_rowset_num` to
    the source cluster Backend to improve observability of the cache warmup
    process.
    
    During event-driven warmup, rowsets can be skipped if the Backend fails
    to find the tablet's replica location information from a
    `TGetTabletReplicaInfosRequest`.
    
    This new metric makes it possible to monitor and alert on these events,
    which helps in diagnosing incomplete cache warmups.

--- be/src/cloud/cloud_warm_up_manager.cpp
+++ be/src/cloud/cloud_warm_up_manager.cpp
@@ -42,6 +42,8 @@
 namespace doris {
 #include "common/compile_check_begin.h"
 
+bvar::Adder<uint64_t> g_file_cache_event_driven_warm_up_skipped_rowset_num(
+        "file_cache_event_driven_warm_up_skipped_rowset_num");
 bvar::Adder<uint64_t> g_file_cache_event_driven_warm_up_requested_segment_size(
         "file_cache_event_driven_warm_up_requested_segment_size");
 bvar::Adder<uint64_t> g_file_cache_event_driven_warm_up_requested_segment_num(
@@ -554,6 +556,7 @@ void CloudWarmUpManager::warm_up_rowset(RowsetMeta& rs_meta, int64_t sync_wait_t
     if (replicas.empty()) {
         VLOG_DEBUG << "There is no need to warmup tablet=" << rs_meta.tablet_id()
                    << ", skipping rowset=" << rs_meta.rowset_id().to_string();
+        g_file_cache_event_driven_warm_up_skipped_rowset_num << 1;
         return;
     }
     Status st = _do_warm_up_rowset(rs_meta, replicas, sync_wait_timeout_ms, !cache_hit);
