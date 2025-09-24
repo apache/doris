54a81965e0370374cc2af5ea67ec6e42b80cb992
Mryange <yanxuecheng@selectdb.com>
2025-09-24 15:04:45 +0800

    [chore](type) remove to_string from DataType part I (#56197)
    remove to_string from DataType part I

--- be/src/vec/data_types/data_type_date.h
+++ be/src/vec/data_types/data_type_date.h
@@ -49,15 +49,8 @@ public:
     std::string do_get_name() const override { return "Date"; }
 
     bool equals(const IDataType& rhs) const override;
-    std::string to_string(const IColumn& column, size_t row_num) const override;
-    void to_string(const IColumn& column, size_t row_num, BufferWritable& ostr) const override;
-    void to_string_batch(const IColumn& column, ColumnString& column_to) const final {
-        DataTypeNumberBase<PrimitiveType::TYPE_DATE>::template to_string_batch_impl<DataTypeDate>(
-                column, column_to);
-    }
-
-    size_t number_length() const;
-    void push_number(ColumnString::Chars& chars, const Int64& num) const;
+    /// TODO: remove this in the future
+    using IDataType::to_string;
     std::string to_string(Int64 int_val) const {
         doris::VecDateTimeValue value = binary_cast<Int64, doris::VecDateTimeValue>(int_val);
         char buf[64];
