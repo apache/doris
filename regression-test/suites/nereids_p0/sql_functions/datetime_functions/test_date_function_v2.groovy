ff0920ae6dcce8e20d01031f0e52b96a5a67c509
jianhao <1367919489@qq.com>
2025-09-27 21:57:27 +0800

    last

--- regression-test/suites/nereids_p0/sql_functions/datetime_functions/test_date_function_v2.groovy
+++ regression-test/suites/nereids_p0/sql_functions/datetime_functions/test_date_function_v2.groovy
@@ -27,7 +27,9 @@ suite("test_date_function_v2") {
             CREATE TABLE IF NOT EXISTS ${tableName} (
                 `id` INT,
                 `name` varchar(32),
-                `dt` varchar(32)
+                `dt` varchar(32),
+                `time_str` varchar(32),
+                `datetime_val` datetime(6)
             ) ENGINE=OLAP
             UNIQUE KEY(`id`)
             DISTRIBUTED BY HASH(`id`) BUCKETS 1
@@ -35,7 +37,11 @@ suite("test_date_function_v2") {
             "replication_allocation" = "tag.location.default: 1"
             )
         """
-    sql """ insert into ${tableName} values (3, 'Carl','2024-12-29 10:11:12') """
+    sql """ insert into ${tableName} values 
+            (1, 'Carl', '2024-12-29 10:11:12', '12:34:56', '2024-12-29 10:11:12.123456'),
+            (2, 'Bob', '2023-01-15 08:30:45', '23:59:59', '2023-01-15 08:30:45.000000'),
+            (3, 'Alice', '2025-06-30 15:45:30', '-12:34:56', '2025-06-30 15:45:30.999999')
+        """
     def result1 = try_sql """
         select cast(str_to_date(dt, '%Y-%m-%d %H:%i:%s') as string) from ${tableName};
     """
@@ -161,6 +167,11 @@ suite("test_date_function_v2") {
     qt_sql_addtime3 "select add_time(dt, '122:35:22.123456') from ${tableName};"
     qt_sql_addtime4 "select add_time(cast('822:35:22.123456' as time), cast('421:01:01' as time));"
     qt_sql_addtime5 "select add_time(cast('-82:35:22.123456' as time), cast('-421:01:01' as time));"
+    
+    // 测试表中的time字符串和datetime类型
+    qt_sql_addtime6 "select add_time(datetime_val, time_str) from ${tableName} order by id;"
+    qt_sql_addtime7 "select add_time(cast(time_str as time), cast('02:00:00.123' as time)) from ${tableName} order by id;"
+    qt_sql_addtime8 "select add_time(datetime_val, cast('01:30:00' as time)) from ${tableName} order by id;"
 
     test{
         sql("select add_time('9999-12-29 00:00:00', '122:35:22.123456');")
@@ -174,6 +185,12 @@ suite("test_date_function_v2") {
     qt_sql_subtime3("select sub_time(dt, '22:35:22.123456') from ${tableName};")
     qt_sql_subtime4("select sub_time(cast('-421:01:01' as time), cast('822:35:22' as time));")
     qt_sql_subtime5("select sub_time(cast('421:01:01' as time), cast('-82:35:22.123456' as time));")
+    
+    // 测试表中的time字符串和datetime类型
+    qt_sql_subtime6("select sub_time(datetime_val, time_str) from ${tableName} order by id;")
+    qt_sql_subtime7("select sub_time(cast(time_str as time), cast('02:00:00.123' as time)) from ${tableName} order by id;")
+    qt_sql_subtime8("select sub_time(datetime_val, cast('01:30:00' as time)) from ${tableName} order by id;")
+
     test{
         sql("select sub_time('0000-01-01 00:00:00', '122:35:22.123456');")
         exception "datetime value is out of range in function sub_time"
