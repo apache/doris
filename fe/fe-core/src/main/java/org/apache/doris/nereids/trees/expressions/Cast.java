3787e8780f13c8ecc7997b19a0685f76b1b6dc12
James <lijibing@selectdb.com>
2025-09-28 14:33:21 +0800

    [feature](cast)Support try-cast (#56171)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/Cast.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/Cast.java
@@ -45,9 +45,9 @@ import java.util.Objects;
 public class Cast extends Expression implements UnaryExpression, Monotonic {
 
     // CAST can be from SQL Query or Type Coercion. true for explicitly cast from SQL query.
-    private final boolean isExplicitType; //FIXME: now not useful
+    protected final boolean isExplicitType; //FIXME: now not useful
 
-    private final DataType targetType;
+    protected final DataType targetType;
 
     public Cast(Expression child, DataType targetType) {
         this(child, targetType, false);
@@ -57,7 +57,7 @@ public class Cast extends Expression implements UnaryExpression, Monotonic {
         this(ImmutableList.of(child), targetType, isExplicitType);
     }
 
-    private Cast(List<Expression> child, DataType targetType, boolean isExplicitType) {
+    protected Cast(List<Expression> child, DataType targetType, boolean isExplicitType) {
         super(child);
         this.targetType = Objects.requireNonNull(targetType, "targetType can not be null");
         this.isExplicitType = isExplicitType;
