9e9788fd1ed3574abb764267b331e4e9eef36dc1
Yixuan Wang <wangyixuan@selectdb.com>
2025-09-28 11:30:07 +0800

    branch-4.0: [fix](case) Fix recycler case for global variables (#56538)

--- regression-test/suites/cloud_p0/recycler/test_recycler.groovy
+++ regression-test/suites/cloud_p0/recycler/test_recycler.groovy
@@ -114,7 +114,7 @@ suite("test_recycler") {
         checkJobInfoApi.call() {
             respCode, body ->
                 logger.info("http cli result: ${body} ${respCode}")
-                checkJobInfoResult = body
+                def checkJobInfoResult = body
                 logger.info("checkJobInfoResult:${checkJobInfoResult}")
                 assertEquals(respCode, 200)
                 def info = parseJson(checkJobInfoResult.trim())
