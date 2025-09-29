003118627b7e844413b1b21a9c098949a4dfee17
bobhan1 <baohan@selectdb.com>
2025-09-29 14:07:54 +0800

    add config

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1652,7 +1652,13 @@ DECLARE_mBool(print_stack_when_cache_miss);
 
 DECLARE_mBool(read_cluster_cache_opt_verbose_log);
 
-DECLARE_Bool(use_plain_encoding);
+DECLARE_mDouble(compression_min_space_saving);
+
+DECLARE_Bool(all_use_plain_encoding);
+
+DECLARE_Bool(use_plain_binary_v2);
+
+DECLARE_Bool(numeric_not_use_bitshuffle);
 
 #ifdef BE_TEST
 // test s3
