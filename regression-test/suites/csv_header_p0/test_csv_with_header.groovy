c42344c03aaa0249fb0feb6bdb652240eb7a7fda
0AyanamiRei <3244156674@qq.com>
2025-10-05 15:34:30 +0800

    fix case

--- regression-test/suites/csv_header_p0/test_csv_with_header.groovy
+++ regression-test/suites/csv_header_p0/test_csv_with_header.groovy
@@ -132,19 +132,19 @@ suite("test_csv_with_header", "p0,external") {
         def hdfsFs = getHdfsFs()
         //[broker load] test normal
         label = UUID.randomUUID().toString().replaceAll("-", "")
-        def remote_csv_file = uploadToHdfs("csv_header_p0/"+format_csv_file)
+        def remote_csv_file = uploadToHdfs "csv_header_p0/csv.txt"
         def export_result = import_from_hdfs.call(testTable, label, remote_csv_file, format_csv, brokerName, hdfsUser, hdfsPasswd)
         check_import_result.call(label, testTable, expect_rows * 4)
 
         //[broker load] csv_with_names
         label = UUID.randomUUID().toString().replaceAll("-", "")
-        remote_csv_file = uploadToHdfs("csv_header_p0/"+format_csv_with_names_file)
+        remote_csv_file = uploadToHdfs "csv_header_p0/csv_with_names.txt"
         export_result = import_from_hdfs.call(testTable, label, remote_csv_file, format_csv_with_names, brokerName, hdfsUser, hdfsPasswd)
         check_import_result.call(label, testTable, expect_rows * 5)
 
          //[broker load] csv_with_names_and_types
         label = UUID.randomUUID().toString().replaceAll("-", "")
-        remote_csv_file = uploadToHdfs("csv_header_p0/"+format_csv_with_names_and_types_file)
+        remote_csv_file = uploadToHdfs "csv_header_p0/csv_with_names_and_types.txt"
         export_result = import_from_hdfs.call(testTable, label, remote_csv_file, format_csv_with_names_and_types, brokerName, hdfsUser, hdfsPasswd)
         check_import_result.call(label, testTable, expect_rows * 6)
 
