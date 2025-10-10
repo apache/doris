1b946235657ccbbe0dcd9dbffd07c2a6ccc1c824
wumeibanfa <904360463@qq.com>
2025-10-10 15:12:04 +0800

    fix

--- be/src/vec/columns/subcolumn_tree.h
+++ be/src/vec/columns/subcolumn_tree.h
@@ -204,9 +204,7 @@ public:
     }
 
     /// Find node that matches the path exactly.
-    const Node* find_exact(const PathInData& path) const { return find_impl(path, true); }
-
-    Node* find_exact(const PathInData& path) { return find_impl(path, true); }
+    Node* find_exact(const PathInData& path) const { return find_impl(path, true); }
 
     static const Node* find_leaf(const Node* node, const NodePredicate& predicate) {
         if (!node) {
@@ -227,16 +225,7 @@ public:
     }
 
     /// Find leaf by path.
-    const Node* find_leaf(const PathInData& path) const {
-        const auto* candidate = find_exact(path);
-        if (!candidate || !candidate->is_scalar()) {
-            return nullptr;
-        }
-        return candidate;
-    }
-
-    /// Find leaf by path.
-    Node* find_leaf(const PathInData& path) {
+    Node* find_leaf(const PathInData& path) const {
         auto* candidate = find_exact(path);
         if (!candidate || !candidate->is_scalar()) {
             return nullptr;
