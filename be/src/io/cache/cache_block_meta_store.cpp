94ff7117e1460714031b33034ac25ca663f278d1
zhengyu <zhangzhengyu@selectdb.com>
2025-10-10 22:58:37 +0800

    meta store
    Signed-off-by: zhengyu <zhangzhengyu@selectdb.com>

---
+++ be/src/io/cache/cache_block_meta_store.cpp
@@ -0,0 +1,240 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "io/cache/cache_block_meta_store.h"
+
+#include <butil/logging.h>
+#include <fmt/format.h>
+#include <rocksdb/db.h>
+#include <rocksdb/filter_policy.h>
+#include <rocksdb/table.h>
+
+#include <algorithm>
+#include <cstring>
+#include <sstream>
+
+#include "common/status.h"
+#include "util/threadpool.h"
+#include "vec/common/hex.h"
+
+namespace doris::io {
+
+CacheBlockMetaStore::CacheBlockMetaStore(const std::string& db_path, size_t queue_size)
+        : _db_path(db_path), _write_queue(queue_size) {}
+
+CacheBlockMetaStore::~CacheBlockMetaStore() {
+    _stop_worker.store(true, std::memory_order_release);
+    if (_write_thread.joinable()) {
+        _write_thread.join();
+    }
+
+    if (_db) {
+        _db->Close();
+    }
+}
+
+Status CacheBlockMetaStore::init() {
+    std::filesystem::create_directories(_db_path);
+
+    _options.create_if_missing = true;
+    _options.error_if_exists = false;
+    _options.compression = rocksdb::kNoCompression;
+    _options.max_open_files = 1000;
+    _options.write_buffer_size = 64 * 1024 * 1024; // 64MB
+    _options.target_file_size_base = 64 * 1024 * 1024;
+
+    rocksdb::BlockBasedTableOptions table_options;
+    table_options.filter_policy.reset(rocksdb::NewBloomFilterPolicy(10, false));
+    table_options.block_size = 16 * 1024;
+    _options.table_factory.reset(rocksdb::NewBlockBasedTableFactory(table_options));
+
+    rocksdb::DB* db_ptr = nullptr;
+    rocksdb::Status status = rocksdb::DB::Open(_options, _db_path, &db_ptr);
+    if (!status.ok()) {
+        return Status::InternalError("Failed to open rocksdb: {}", status.ToString());
+    }
+    _db.reset(db_ptr);
+
+    _write_thread = std::thread(&CacheBlockMetaStore::async_write_worker, this);
+
+    return Status::OK();
+}
+
+void CacheBlockMetaStore::put(const BlockMetaKey& key, const BlockMeta& meta) {
+    std::string key_str = serialize_key(key);
+    std::string value_str = serialize_value(meta);
+
+    // Put write task into queue for asynchronous processing
+    WriteOperation op;
+    op.type = OperationType::PUT;
+    op.key = key_str;
+    op.value = value_str;
+    _write_queue.enqueue(op);
+}
+
+BlockMeta CacheBlockMetaStore::get(const BlockMetaKey& key) {
+    std::string key_str = serialize_key(key);
+    std::string value_str;
+
+    rocksdb::Status status = _db->Get(rocksdb::ReadOptions(), key_str, &value_str);
+
+    if (status.ok()) {
+        return deserialize_value(value_str);
+    } else if (status.IsNotFound()) {
+        return BlockMeta();
+    } else {
+        LOG(WARNING) << "Failed to get key from rocksdb: " << status.ToString();
+        return BlockMeta();
+    }
+}
+
+std::unique_ptr<BlockMetaIterator> CacheBlockMetaStore::range_get(int64_t tablet_id) {
+    class RocksDBIterator : public BlockMetaIterator {
+    public:
+        RocksDBIterator(rocksdb::Iterator* iter, const std::string& prefix)
+                : _iter(iter), _prefix(prefix) {
+            _iter->Seek(_prefix);
+        }
+
+        ~RocksDBIterator() override { delete _iter; }
+
+        bool valid() const override { return _iter->Valid() && _iter->key().starts_with(_prefix); }
+
+        void next() override { _iter->Next(); }
+
+        BlockMetaKey key() const override {
+            std::string key_str = _iter->key().ToString();
+            // Key format: "tabletid_hashstring_offset"
+            size_t pos1 = key_str.find('_');
+            size_t pos2 = key_str.find('_', pos1 + 1);
+
+            int64_t tablet_id = std::stoll(key_str.substr(0, pos1));
+            std::string hash_str = key_str.substr(pos1 + 1, pos2 - pos1 - 1);
+            size_t offset = std::stoull(key_str.substr(pos2 + 1));
+
+            // Convert hash string back to UInt128Wrapper
+            // Using unhex_uint to parse hex string to uint128_t
+            uint128_t hash_value = vectorized::unhex_uint<uint128_t>(hash_str.c_str());
+
+            return BlockMetaKey(tablet_id, UInt128Wrapper(hash_value), offset);
+        }
+
+        BlockMeta value() const override {
+            std::string value_str = _iter->value().ToString();
+            // Assuming value format is "type:size"
+            size_t pos = value_str.find(':');
+            int type = std::stoi(value_str.substr(0, pos));
+            size_t size = std::stoull(value_str.substr(pos + 1));
+            return BlockMeta(type, size);
+        }
+
+    private:
+        rocksdb::Iterator* _iter;
+        std::string _prefix;
+    };
+
+    std::string prefix = std::to_string(tablet_id) + "_";
+    rocksdb::Iterator* iter = _db->NewIterator(rocksdb::ReadOptions());
+    return std::make_unique<RocksDBIterator>(iter, prefix);
+}
+
+void CacheBlockMetaStore::delete_key(const BlockMetaKey& key) {
+    std::string key_str = serialize_key(key);
+
+    // Put delete task into queue for asynchronous processing
+    WriteOperation op;
+    op.type = OperationType::DELETE;
+    op.key = key_str;
+    _write_queue.enqueue(op);
+}
+
+void CacheBlockMetaStore::async_write_worker() {
+    while (!_stop_worker.load(std::memory_order_acquire)) {
+        WriteOperation op;
+
+        if (_write_queue.try_dequeue(op)) {
+            std::lock_guard<std::mutex> lock(_db_mutex);
+            rocksdb::Status status;
+
+            if (op.type == OperationType::PUT) {
+                status = _db->Put(rocksdb::WriteOptions(), op.key, op.value);
+            } else if (op.type == OperationType::DELETE) {
+                status = _db->Delete(rocksdb::WriteOptions(), op.key);
+            }
+
+            if (!status.ok()) {
+                LOG(WARNING) << "Failed to " << (op.type == OperationType::PUT ? "write" : "delete")
+                             << " to rocksdb: " << status.ToString();
+            }
+        } else {
+            // Queue is empty, sleep briefly
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
+
+    // Process remaining tasks in the queue
+    WriteOperation op;
+    while (_write_queue.try_dequeue(op)) {
+        std::lock_guard<std::mutex> lock(_db_mutex);
+        rocksdb::Status status;
+
+        if (op.type == OperationType::PUT) {
+            status = _db->Put(rocksdb::WriteOptions(), op.key, op.value);
+        } else if (op.type == OperationType::DELETE) {
+            status = _db->Delete(rocksdb::WriteOptions(), op.key);
+        }
+
+        if (!status.ok()) {
+            LOG(WARNING) << "Failed to " << (op.type == OperationType::PUT ? "write" : "delete")
+                         << " to rocksdb: " << status.ToString();
+        }
+    }
+}
+
+std::string CacheBlockMetaStore::serialize_key(const BlockMetaKey& key) const {
+    return fmt::format("{}_{}_{}", key.tablet_id, key.hash.to_string(), key.offset);
+}
+
+std::string CacheBlockMetaStore::serialize_value(const BlockMeta& meta) const {
+    return fmt::format("{}:{}", meta.type, meta.size);
+}
+
+BlockMetaKey CacheBlockMetaStore::deserialize_key(const std::string& key_str) const {
+    // Key format: "tabletid_hashstring_offset"
+    size_t pos1 = key_str.find('_');
+    size_t pos2 = key_str.find('_', pos1 + 1);
+
+    int64_t tablet_id = std::stoll(key_str.substr(0, pos1));
+    std::string hash_str = key_str.substr(pos1 + 1, pos2 - pos1 - 1);
+    size_t offset = std::stoull(key_str.substr(pos2 + 1));
+
+    // Convert hash string back to UInt128Wrapper
+    // Using unhex_uint to parse hex string to uint128_t
+    uint128_t hash_value = vectorized::unhex_uint<uint128_t>(hash_str.c_str());
+
+    return BlockMetaKey(tablet_id, UInt128Wrapper(hash_value), offset);
+}
+
+BlockMeta CacheBlockMetaStore::deserialize_value(const std::string& value_str) const {
+    // Value format: "type:size"
+    size_t pos = value_str.find(':');
+    int type = std::stoi(value_str.substr(0, pos));
+    size_t size = std::stoull(value_str.substr(pos + 1));
+    return BlockMeta(type, size);
+}
+
+} // namespace doris::io
---
+++ be/test/io/cache/cache_block_meta_store_test.cpp
@@ -0,0 +1,334 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "io/cache/cache_block_meta_store.h"
+
+#include <gtest/gtest.h>
+
+#include <atomic>
+#include <filesystem>
+#include <random>
+#include <thread>
+#include <vector>
+
+#include "common/status.h"
+
+namespace doris::io {
+
+class CacheBlockMetaStoreTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Create a temporary directory for test database
+        test_db_path_ = std::filesystem::temp_directory_path() / "cache_block_meta_store_test";
+        std::filesystem::remove_all(test_db_path_);
+        std::filesystem::create_directories(test_db_path_);
+
+        meta_store_ = std::make_unique<CacheBlockMetaStore>(test_db_path_.string());
+        auto status = meta_store_->init();
+        ASSERT_TRUE(status.ok()) << "Failed to initialize CacheBlockMetaStore: "
+                                 << status.to_string();
+    }
+
+    void TearDown() override {
+        meta_store_.reset();
+        std::filesystem::remove_all(test_db_path_);
+    }
+
+    std::filesystem::path test_db_path_;
+    std::unique_ptr<CacheBlockMetaStore> meta_store_;
+};
+
+TEST_F(CacheBlockMetaStoreTest, BasicPutAndGet) {
+    uint128_t hash1 = (static_cast<uint128_t>(123) << 64) | 456;
+    BlockMetaKey key1(1, UInt128Wrapper(hash1), 0);
+    BlockMeta meta1(1, 1024);
+
+    // Test put operation
+    meta_store_->put(key1, meta1);
+
+    // Wait a bit for async operation to complete
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Test get operation
+    BlockMeta result = meta_store_->get(key1);
+    EXPECT_EQ(result.type, meta1.type);
+    EXPECT_EQ(result.size, meta1.size);
+
+    // Test non-existent key
+    uint128_t hash2 = (static_cast<uint128_t>(999) << 64) | 999;
+    BlockMetaKey non_existent_key(999, UInt128Wrapper(hash2), 999);
+    BlockMeta non_existent_result = meta_store_->get(non_existent_key);
+    EXPECT_EQ(non_existent_result.type, 0);
+    EXPECT_EQ(non_existent_result.size, 0);
+}
+
+TEST_F(CacheBlockMetaStoreTest, MultiplePutsAndGets) {
+    const int num_keys = 10;
+    std::vector<BlockMetaKey> keys;
+    std::vector<BlockMeta> metas;
+
+    // Create multiple keys and metas
+    for (int i = 0; i < num_keys; ++i) {
+        uint128_t hash = (static_cast<uint128_t>(i) << 64) | (i * 100);
+        keys.emplace_back(1, UInt128Wrapper(hash), i * 1024);
+        metas.emplace_back(i % 3, 1024 * (i + 1));
+        meta_store_->put(keys[i], metas[i]);
+    }
+
+    // Wait for async operations
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    // Verify all keys
+    for (int i = 0; i < num_keys; ++i) {
+        BlockMeta result = meta_store_->get(keys[i]);
+        EXPECT_EQ(result.type, metas[i].type);
+        EXPECT_EQ(result.size, metas[i].size);
+    }
+}
+
+TEST_F(CacheBlockMetaStoreTest, RangeQuery) {
+    const int num_tablets = 3;
+    const int blocks_per_tablet = 5;
+
+    // Create data for multiple tablets
+    for (int tablet_id = 1; tablet_id <= num_tablets; ++tablet_id) {
+        for (int i = 0; i < blocks_per_tablet; ++i) {
+            uint128_t hash =
+                    (static_cast<uint128_t>(tablet_id * 100 + i) << 64) | (tablet_id * 200 + i);
+            BlockMetaKey key(tablet_id, UInt128Wrapper(hash), i * 1024);
+            BlockMeta meta(i % 2, 2048 * (i + 1));
+            meta_store_->put(key, meta);
+        }
+    }
+
+    // Wait for async operations
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    // Test range query for each tablet
+    for (int tablet_id = 1; tablet_id <= num_tablets; ++tablet_id) {
+        auto iterator = meta_store_->range_get(tablet_id);
+
+        int count = 0;
+        while (iterator->valid()) {
+            BlockMetaKey key = iterator->key();
+            BlockMeta value = iterator->value();
+
+            EXPECT_EQ(key.tablet_id, tablet_id);
+            EXPECT_TRUE(value.type == 0 || value.type == 1);
+            EXPECT_GT(value.size, 0);
+
+            iterator->next();
+            count++;
+        }
+
+        EXPECT_EQ(count, blocks_per_tablet);
+    }
+
+    // Test range query for non-existent tablet
+    auto iterator = meta_store_->range_get(999);
+    EXPECT_FALSE(iterator->valid());
+}
+
+TEST_F(CacheBlockMetaStoreTest, DeleteOperation) {
+    uint128_t hash1 = (static_cast<uint128_t>(123) << 64) | 456;
+    BlockMetaKey key1(1, UInt128Wrapper(hash1), 0);
+    BlockMeta meta1(1, 1024);
+
+    // Put then delete
+    meta_store_->put(key1, meta1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Verify put worked
+    BlockMeta result = meta_store_->get(key1);
+    EXPECT_EQ(result.type, meta1.type);
+
+    // Delete the key
+    meta_store_->delete_key(key1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Verify deletion
+    BlockMeta deleted_result = meta_store_->get(key1);
+    EXPECT_EQ(deleted_result.type, 0);
+    EXPECT_EQ(deleted_result.size, 0);
+}
+
+TEST_F(CacheBlockMetaStoreTest, SerializationDeserialization) {
+    uint128_t hash3 = (static_cast<uint128_t>(456789) << 64) | 987654;
+    BlockMetaKey original_key(123, UInt128Wrapper(hash3), 1024);
+    BlockMeta original_meta(2, 4096);
+
+    // Test round-trip through put and get operations
+    meta_store_->put(original_key, original_meta);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    BlockMeta retrieved = meta_store_->get(original_key);
+    EXPECT_EQ(retrieved.type, original_meta.type);
+    EXPECT_EQ(retrieved.size, original_meta.size);
+
+    // Test non-existent key
+    uint128_t hash4 = (static_cast<uint128_t>(999999) << 64) | 888888;
+    BlockMetaKey non_existent_key(999, UInt128Wrapper(hash4), 2048);
+    BlockMeta non_existent_result = meta_store_->get(non_existent_key);
+    EXPECT_EQ(non_existent_result.type, 0);
+    EXPECT_EQ(non_existent_result.size, 0);
+}
+
+TEST_F(CacheBlockMetaStoreTest, ConcurrencyTest) {
+    const int num_threads = 4;
+    const int operations_per_thread = 100;
+    std::atomic<int> successful_puts(0);
+
+    // Store keys for later verification
+    std::vector<BlockMetaKey> all_keys;
+    std::mutex keys_mutex;
+
+    auto worker = [&](int thread_id) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(1, 1000);
+
+        for (int i = 0; i < operations_per_thread; ++i) {
+            int64_t tablet_id = thread_id + 1;
+            uint128_t hash_value = (static_cast<uint128_t>(dist(gen)) << 64) | dist(gen);
+            size_t offset = i * 1024;
+
+            BlockMetaKey key(tablet_id, UInt128Wrapper(hash_value), offset);
+            BlockMeta meta(thread_id % 3, 2048);
+
+            // Put operation
+            meta_store_->put(key, meta);
+            successful_puts++;
+
+            // Store key for later verification
+            {
+                std::lock_guard<std::mutex> lock(keys_mutex);
+                all_keys.push_back(key);
+            }
+        }
+    };
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < num_threads; ++i) {
+        threads.emplace_back(worker, i);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    // Wait for all async operations to complete
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    EXPECT_EQ(successful_puts, num_threads * operations_per_thread);
+
+    // Verify we can retrieve the data after all writes are complete
+    int successful_gets = 0;
+    for (const auto& key : all_keys) {
+        BlockMeta result = meta_store_->get(key);
+        if (result.size > 0) {
+            successful_gets++;
+        }
+    }
+
+    EXPECT_GT(successful_gets, 0);
+
+    // Verify we can retrieve some of the data
+    for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
+        for (int i = 0; i < 5; ++i) { // Check a few samples
+            uint128_t hash = (static_cast<uint128_t>(100 + i) << 64) | (200 + i);
+            BlockMetaKey key(thread_id + 1, UInt128Wrapper(hash), i * 1024);
+            BlockMeta result = meta_store_->get(key);
+            EXPECT_GE(result.size, 0);
+        }
+    }
+}
+
+TEST_F(CacheBlockMetaStoreTest, IteratorValidity) {
+    // Put some data
+    for (int i = 0; i < 5; ++i) {
+        uint128_t hash = (static_cast<uint128_t>(100 + i) << 64) | (200 + i);
+        BlockMetaKey key(1, UInt128Wrapper(hash), i * 1024);
+        BlockMeta meta(i % 2, 2048 * (i + 1));
+        meta_store_->put(key, meta);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Test iterator
+    auto iterator = meta_store_->range_get(1);
+    int count = 0;
+
+    while (iterator->valid()) {
+        BlockMetaKey key = iterator->key();
+        BlockMeta value = iterator->value();
+
+        EXPECT_EQ(key.tablet_id, 1);
+        EXPECT_GE(key.offset, 0);
+        EXPECT_GT(value.size, 0);
+
+        iterator->next();
+        count++;
+    }
+
+    EXPECT_EQ(count, 5);
+
+    // Test that iterator becomes invalid after reaching end
+    EXPECT_FALSE(iterator->valid());
+}
+
+TEST_F(CacheBlockMetaStoreTest, KeyToString) {
+    uint128_t hash4 = (static_cast<uint128_t>(456789) << 64) | 987654;
+    BlockMetaKey key(123, UInt128Wrapper(hash4), 1024);
+    std::string key_str = key.to_string();
+
+    // UInt128Wrapper::to_string() returns hex string, so check for hex representations
+    EXPECT_NE(key_str.find("123"), std::string::npos);  // tablet_id in decimal
+    EXPECT_NE(key_str.find("1024"), std::string::npos); // offset in decimal
+
+    // Check for hex representations of the hash parts
+    // 456789 in hex is 6f855, 987654 in hex is f1206
+    EXPECT_NE(key_str.find("6f855"), std::string::npos);
+    EXPECT_NE(key_str.find("f1206"), std::string::npos);
+}
+
+TEST_F(CacheBlockMetaStoreTest, BlockMetaEquality) {
+    BlockMeta meta1(1, 1024);
+    BlockMeta meta2(1, 1024);
+    BlockMeta meta3(2, 1024);
+    BlockMeta meta4(1, 2048);
+
+    EXPECT_TRUE(meta1 == meta2);
+    EXPECT_FALSE(meta1 == meta3);
+    EXPECT_FALSE(meta1 == meta4);
+}
+
+TEST_F(CacheBlockMetaStoreTest, BlockMetaKeyEquality) {
+    uint128_t hash1 = (static_cast<uint128_t>(123) << 64) | 456;
+    uint128_t hash2 = (static_cast<uint128_t>(789) << 64) | 456;
+    BlockMetaKey key1(1, UInt128Wrapper(hash1), 0);
+    BlockMetaKey key2(1, UInt128Wrapper(hash1), 0);
+    BlockMetaKey key3(2, UInt128Wrapper(hash1), 0);
+    BlockMetaKey key4(1, UInt128Wrapper(hash2), 0);
+    BlockMetaKey key5(1, UInt128Wrapper(hash1), 1024);
+
+    EXPECT_TRUE(key1 == key2);
+    EXPECT_FALSE(key1 == key3);
+    EXPECT_FALSE(key1 == key4);
+    EXPECT_FALSE(key1 == key5);
+}
+
+} // namespace doris::io
