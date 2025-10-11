2763ad5a835c8742154a6b9423d542f5b1a65de4
Mingyu Chen (Rayner) <yunyou@selectdb.com>
2025-10-11 17:16:35 +0800

    branch-3.1: [fix](ubsan) fix ub error of uninitialized variable

--- be/src/pipeline/exec/result_sink_operator.h
+++ be/src/pipeline/exec/result_sink_operator.h
@@ -46,8 +46,8 @@ struct ResultFileOptions {
     std::vector<TParquetSchema> parquet_schemas;
     TParquetCompressionType::type parquet_commpression_type;
     TParquetVersion::type parquet_version;
-    bool parquert_disable_dictionary;
-    bool enable_int96_timestamps;
+    bool parquert_disable_dictionary = false;
+    bool enable_int96_timestamps = false;
     //note: use outfile with parquet format, have deprecated 9:schema and 10:file_properties
     //But in order to consider the compatibility when upgrading, so add a bool to check
     //Now the code version is 1.1.2, so when the version is after 1.2, could remove this code.
