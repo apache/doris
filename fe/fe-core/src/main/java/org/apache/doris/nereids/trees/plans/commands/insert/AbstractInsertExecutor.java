d30170a74376031e8ef0f38fa1ebca8a0498df16
Qi Chen <chenqi@selectdb.com>
2025-10-11 11:50:27 +0800

    [feature](file-cache) Implement cache warm up select functionality. (#54822)
    ### What problem does this PR solve?
    
    This PR implements cache warm up select functionality by adding a new
    "WARM UP SELECT" SQL statement that allows preloading file block caches
    into tables. The feature is designed to work with external tables and
    uses a blackhole sink pattern to discard data while collecting cache
    metrics.
    
    Key changes:
    - Adds new "WARM UP SELECT" SQL syntax that supports basic SELECT
    operations with column references and WHERE clauses
    - Implements blackhole sink infrastructure that discards data
    - Adds comprehensive test coverage for both positive and negative cases
    
    Result:
    ```
    mysql> set enable_file_cache=true;
    Query OK, 0 rows affected (0.00 sec)
    
    mysql> warm up select * from hive_krb.tpch100_parquet.lineitem;
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    | BackendId     | ScanRows  | ScanBytes   | ScanBytesFromLocalStorage | ScanBytesFromRemoteStorage | BytesWriteIntoCache |
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    | 1755134092928 | 294744184 | 11821864798 | 538154009                 | 11283717130                | 11899799492         |
    | 1755134092929 | 305293718 | 12244439301 | 560970435                 | 11683475207                | 12332861380         |
    | TOTAL         | 600037902 | 24066304099 | 1099124444                | 22967192337                | 24232660872         |
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    3 rows in set (14.75 sec)
    
    mysql> warm up select * from hive_krb.tpch100_parquet.lineitem;
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    | BackendId     | ScanRows  | ScanBytes   | ScanBytesFromLocalStorage | ScanBytesFromRemoteStorage | BytesWriteIntoCache |
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    | 1755134092928 | 294744184 | 11819050010 | 11819056351               | 0                          | 0                   |
    | 1755134092929 | 305293718 | 12241772102 | 12241778443               | 0                          | 0                   |
    | TOTAL         | 600037902 | 24060822112 | 24060834794               | 0                          | 0                   |
    +---------------+-----------+-------------+---------------------------+----------------------------+---------------------+
    3 rows in set (7.89 sec)
    ```

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/AbstractInsertExecutor.java
@@ -44,7 +44,9 @@ import org.apache.doris.thrift.TStatusCode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Abstract insert executor.
@@ -69,6 +71,25 @@ public abstract class AbstractInsertExecutor {
     protected Optional<InsertCommandContext> insertCtx;
     protected final boolean emptyInsert;
     protected long txnId = INVALID_TXN_ID;
+
+    /**
+     * Insert executor listener
+     */
+    public interface InsertExecutorListener {
+        /**
+         * Called before insert execution begins
+         */
+
+        default void beforeComplete(AbstractInsertExecutor insertExecutor, StmtExecutor executor, long jobId)
+                throws Exception {
+        }
+
+        default void afterComplete(AbstractInsertExecutor insertExecutor, StmtExecutor executor, long jobId)
+                throws Exception {
+        }
+    }
+
+    private List<InsertExecutorListener> listeners = new CopyOnWriteArrayList<>();
 
     /**
      * Randomly generate job ID.
@@ -103,6 +124,14 @@ public abstract class AbstractInsertExecutor {
         this.insertCtx = insertCtx;
         this.emptyInsert = emptyInsert;
         this.jobId = jobId;
+    }
+
+    public void registerListener(InsertExecutorListener listener) {
+        listeners.add(listener);
+    }
+
+    public void unregisterListener(InsertExecutorListener listener) {
+        listeners.remove(listener);
     }
 
     public Coordinator getCoordinator() {
@@ -223,7 +252,13 @@ public abstract class AbstractInsertExecutor {
             executor.updateProfile(false);
             execImpl(executor, jobId);
             checkStrictModeAndFilterRatio();
+            for (InsertExecutorListener listener : listeners) {
+                listener.beforeComplete(this, executor, jobId);
+            }
             onComplete();
+            for (InsertExecutorListener listener : listeners) {
+                listener.afterComplete(this, executor, jobId);
+            }
         } catch (Throwable t) {
             onFail(t);
             // retry insert into from select when meet E-230 in cloud
