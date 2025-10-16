6862d0c8fe3fa3263dce0d2ac19a7f20c132f375
yujun <yujun@selectdb.com>
2025-10-16 12:22:39 +0800

    [feat](case when) rewrite case when to compound predicate (#56756)
    for a boolean data type case when expression, if all its when clauses'
    result are true / false literal, then can rewrite this case when to AND
    / OR expression.
    
    for example:
    
    case when a = 1 then true when b = 1 then false else c = 1 end
    rewrite to:  (a = 1) <=> true  or (not((b = 1) <=> true) and c = 1)
    
    if (a = 1, true,  b = 1)
    rewrite to:  (a=1) <=> true or b = 1

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/ExpressionAnalyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/ExpressionAnalyzer.java
@@ -662,7 +662,7 @@ public class ExpressionAnalyzer extends SubExprAnalyzer<ExpressionRewriteContext
                 newChild = child;
             }
             if (newChild.getDataType().isNullType()) {
-                newChild = new NullLiteral(BooleanType.INSTANCE);
+                newChild = NullLiteral.BOOLEAN_INSTANCE;
             } else {
                 newChild = TypeCoercionUtils.castIfNotSameType(newChild, BooleanType.INSTANCE);
             }
@@ -690,7 +690,7 @@ public class ExpressionAnalyzer extends SubExprAnalyzer<ExpressionRewriteContext
                 newChild = child;
             }
             if (newChild.getDataType().isNullType()) {
-                newChild = new NullLiteral(BooleanType.INSTANCE);
+                newChild = NullLiteral.BOOLEAN_INSTANCE;
             } else {
                 newChild = TypeCoercionUtils.castIfNotSameType(newChild, BooleanType.INSTANCE);
             }
