d352e8899c51304fdff5488eb3530db0b3838394
bobhan1 <baohan@selectdb.com>
2025-10-16 10:40:26 +0800

    [Opt](sys table) Add a system table column_data_sizes (#56815)
    Add a system table `column_data_sizes` for querying columns' data page
    sizes:
    ```sql
    MySQL root@127.1:information_schema> show create table information_schema.column_data_sizes;
    +-------------------+------------------------------------------+
    | Table             | Create Table                             |
    +-------------------+------------------------------------------+
    | column_data_sizes | CREATE TABLE `column_data_sizes` (       |
    |                   |   `BACKEND_ID` bigint NULL,              |
    |                   |   `TABLE_ID` bigint NULL,                |
    |                   |   `INDEX_ID` bigint NULL,                |
    |                   |   `PARTITION_ID` bigint NULL,            |
    |                   |   `TABLET_ID` bigint NULL,               |
    |                   |   `ROWSET_ID` varchar(64) NULL,          |
    |                   |   `COLUMN_UNIQUE_ID` int NULL,           |
    |                   |   `COLUMN_NAME` varchar(64) NULL,        |
    |                   |   `COLUMN_TYPE` varchar(64) NULL,        |
    |                   |   `COMPRESSED_DATA_BYTES` bigint NULL,   |
    |                   |   `UNCOMPRESSED_DATA_BYTES` bigint NULL, |
    |                   |   `RAW_DATA_BYTES` bigint NULL           |
    |                   | ) ENGINE=SCHEMA;                         |
    +-------------------+------------------------------------------+
    ```
    - **COMPRESSED_DATA_BYTES**: total size of compressed data pages in
    bytes, for complex type(array/map/struct), it's the sum of all leaf
    scalar column writers' stats
    - **UNCOMPRESSED_DATA_BYTES**: total size of uncompressed data pages in
    bytes, for complex type(array/map/struct), it's the sum of all leaf
    scalar column writers' stats
    - **RAW_DATA_BYTES**: total size of input raw data in bytes(calculated
    via `XXXPageBuilder`'s `Status add(const uint8_t* vals, size_t* count)`
    ), for complex type(array/map/struct), it's the sum of all leaf scalar
    column writers' stats
    
    To query columns' data page sizes in a table:
    ```sql
    SELECT
        COLUMN_NAME,
        sum(COMPRESSED_DATA_BYTES) AS compressed_data_bytes,
        sum(UNCOMPRESSED_DATA_BYTES) AS uncompressed_data_bytes,
        sum(RAW_DATA_BYTES) as raw_data_bytes
    FROM information_schema.column_data_sizes
    WHERE table_id=${TABLE_ID}
    GROUP BY COLUMN_NAME, COLUMN_TYPE
    ORDER BY data_size desc;
    ```
    
    Note:
    - variant column is not supported.
    - In cloud mode, only BE local's rowsets will be queried.
    
    None
    
    - Test <!-- At least one of them must be included. -->
        - [ ] Regression test
        - [ ] Unit Test
        - [ ] Manual test (add detailed scripts or steps below)
        - [ ] No need to test or manual test. Explain why:
    - [ ] This is a refactor/code format and no logic has been changed.
            - [ ] Previous test can cover this change.
            - [ ] No code files have been changed.
            - [ ] Other reason <!-- Add your reason?  -->
    
    - Behavior changed:
        - [ ] No.
        - [ ] Yes. <!-- Explain the behavior change -->
    
    - Does this need documentation?
        - [ ] No.
    - [ ] Yes. <!-- Add document PR link here. eg:
    https://github.com/apache/doris-website/pull/1214 -->
    
    - [ ] Confirm the release note
    - [ ] Confirm test cases
    - [ ] Confirm document
    - [ ] Add branch pick label <!-- Add branch pick label that this PR
    should merge into -->

--- be/src/olap/rowset/segment_v2/column_writer.cpp
+++ be/src/olap/rowset/segment_v2/column_writer.cpp
@@ -407,7 +407,7 @@ Status ColumnWriter::append(const uint8_t* nullmap, const void* data, size_t num
 
 ScalarColumnWriter::ScalarColumnWriter(const ColumnWriterOptions& opts,
                                        std::unique_ptr<Field> field, io::FileWriter* file_writer)
-        : ColumnWriter(std::move(field), opts.meta->is_nullable()),
+        : ColumnWriter(std::move(field), opts.meta->is_nullable(), opts.meta),
           _opts(opts),
           _file_writer(file_writer),
           _data_size(0) {
@@ -615,7 +615,13 @@ Status ScalarColumnWriter::finish() {
 }
 
 Status ScalarColumnWriter::write_data() {
+    auto offset = _file_writer->bytes_appended();
+    auto collect_uncompressed_bytes = [](const PageFooterPB& footer) {
+        return footer.uncompressed_size() + footer.ByteSizeLong() +
+               sizeof(uint32_t) /* footer size */ + sizeof(uint32_t) /* checksum */;
+    };
     for (auto& page : _pages) {
+        _total_uncompressed_data_pages_size += collect_uncompressed_bytes(page->footer);
         RETURN_IF_ERROR(_write_data_page(page.get()));
     }
     _pages.clear();
@@ -628,6 +634,7 @@ Status ScalarColumnWriter::write_data() {
         footer.set_type(DICTIONARY_PAGE);
         footer.set_uncompressed_size(cast_set<uint32_t>(dict_body.slice().get_size()));
         footer.mutable_dict_page_footer()->set_encoding(PLAIN_ENCODING);
+        _total_uncompressed_data_pages_size += collect_uncompressed_bytes(footer);
 
         PagePointer dict_pp;
         RETURN_IF_ERROR(PageIO::compress_and_write_page(
@@ -635,6 +642,7 @@ Status ScalarColumnWriter::write_data() {
                 {dict_body.slice()}, footer, &dict_pp));
         dict_pp.to_proto(_opts.meta->mutable_dict_page());
     }
+    _total_compressed_data_pages_size += _file_writer->bytes_appended() - offset;
     _page_builder.reset();
     return Status::OK();
 }
@@ -699,6 +707,8 @@ Status ScalarColumnWriter::finish_current_page() {
     if (_opts.need_bloom_filter) {
         RETURN_IF_ERROR(_bloom_filter_index_builder->flush());
     }
+
+    _raw_data_bytes += _page_builder->get_raw_data_size();
 
     // build data page body : encoded values + [nullmap]
     std::vector<Slice> body;
@@ -792,7 +802,7 @@ StructColumnWriter::StructColumnWriter(
         const ColumnWriterOptions& opts, std::unique_ptr<Field> field,
         ScalarColumnWriter* null_writer,
         std::vector<std::unique_ptr<ColumnWriter>>& sub_column_writers)
-        : ColumnWriter(std::move(field), opts.meta->is_nullable()), _opts(opts) {
+        : ColumnWriter(std::move(field), opts.meta->is_nullable(), opts.meta), _opts(opts) {
     for (auto& sub_column_writer : sub_column_writers) {
         _sub_column_writers.push_back(std::move(sub_column_writer));
     }
@@ -901,7 +911,7 @@ ArrayColumnWriter::ArrayColumnWriter(const ColumnWriterOptions& opts, std::uniqu
                                      OffsetColumnWriter* offset_writer,
                                      ScalarColumnWriter* null_writer,
                                      std::unique_ptr<ColumnWriter> item_writer)
-        : ColumnWriter(std::move(field), opts.meta->is_nullable()),
+        : ColumnWriter(std::move(field), opts.meta->is_nullable(), opts.meta),
           _item_writer(std::move(item_writer)),
           _opts(opts) {
     _offset_writer.reset(offset_writer);
@@ -985,7 +995,8 @@ Status ArrayColumnWriter::append_data(const uint8_t** ptr, size_t num_rows) {
                     reinterpret_cast<const uint8_t*>(nested_null_map), offsets_ptr, num_rows));
         } else {
             return Status::NotSupported(
-                    "Ann index can only be build on array with scalar type. but got {} as nested",
+                    "Ann index can only be build on array with scalar type. but got {} as "
+                    "nested",
                     _item_writer->get_field()->type());
         }
     }
@@ -1073,7 +1084,7 @@ Status ArrayColumnWriter::finish_current_page() {
 MapColumnWriter::MapColumnWriter(const ColumnWriterOptions& opts, std::unique_ptr<Field> field,
                                  ScalarColumnWriter* null_writer, OffsetColumnWriter* offset_writer,
                                  std::vector<std::unique_ptr<ColumnWriter>>& kv_writers)
-        : ColumnWriter(std::move(field), opts.meta->is_nullable()), _opts(opts) {
+        : ColumnWriter(std::move(field), opts.meta->is_nullable(), opts.meta), _opts(opts) {
     CHECK_EQ(kv_writers.size(), 2);
     _offsets_writer.reset(offset_writer);
     if (is_nullable()) {
@@ -1210,7 +1221,7 @@ Status MapColumnWriter::write_inverted_index() {
 
 VariantColumnWriter::VariantColumnWriter(const ColumnWriterOptions& opts,
                                          const TabletColumn* column, std::unique_ptr<Field> field)
-        : ColumnWriter(std::move(field), opts.meta->is_nullable()) {
+        : ColumnWriter(std::move(field), opts.meta->is_nullable(), opts.meta) {
     _impl = std::make_unique<VariantColumnWriterImpl>(opts, column);
 }
 
@@ -1250,6 +1261,7 @@ Status VariantColumnWriter::write_inverted_index() {
 Status VariantColumnWriter::write_bloom_filter_index() {
     return _impl->write_bloom_filter_index();
 }
+
 Status VariantColumnWriter::append_nullable(const uint8_t* null_map, const uint8_t** ptr,
                                             size_t num_rows) {
     return _impl->append_nullable(null_map, ptr, num_rows);
