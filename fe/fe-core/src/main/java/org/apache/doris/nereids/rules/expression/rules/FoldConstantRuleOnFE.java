0a2f68339e55b8ab691d1417c5ca03fa40e8ba2c
yujun <yujun@selectdb.com>
2025-10-17 11:35:18 +0800

    fold nullif

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnFE.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/rules/FoldConstantRuleOnFE.java
@@ -74,6 +74,7 @@ import org.apache.doris.nereids.trees.expressions.functions.scalar.Date;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.EncryptKeyRef;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.If;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.LastQueryId;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.NullIf;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Nvl;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.Password;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.SessionUser;
@@ -186,6 +187,7 @@ public class FoldConstantRuleOnFE extends AbstractExpressionRewriteRule
                 matches(SessionUser.class, this::visitSessionUser),
                 matches(LastQueryId.class, this::visitLastQueryId),
                 matches(Nvl.class, this::visitNvl),
+                matches(NullIf.class, this::visitNullIf),
                 matches(Match.class, this::visitMatch)
         );
     }
@@ -551,9 +553,6 @@ public class FoldConstantRuleOnFE extends AbstractExpressionRewriteRule
 
     @Override
     public Expression visitBoundFunction(BoundFunction boundFunction, ExpressionRewriteContext context) {
-        if (!boundFunction.foldable()) {
-            return boundFunction;
-        }
         boundFunction = rewriteChildren(boundFunction, context);
         Optional<Expression> checkedExpr = preProcess(boundFunction);
         if (checkedExpr.isPresent()) {
@@ -735,18 +734,39 @@ public class FoldConstantRuleOnFE extends AbstractExpressionRewriteRule
     public Expression visitNvl(Nvl nvl, ExpressionRewriteContext context) {
         Nvl originNvl = nvl;
         nvl = rewriteChildren(nvl, context);
+        Expression first = nvl.left();
+        Expression second = nvl.right();
+        Expression result = nvl;
+        if (first.equals(second) || second.isNullLiteral() || (first.isLiteral() && !first.isNullLiteral())) {
+            result = first;
+        } else if (first.isNullLiteral()) {
+            result = second;
+        }
+        return TypeCoercionUtils.ensureSameResultType(originNvl, result, context);
+    }
 
-        for (Expression expr : nvl.children()) {
-            if (expr.isLiteral()) {
-                if (!expr.isNullLiteral()) {
-                    return TypeCoercionUtils.ensureSameResultType(originNvl, expr, context);
-                }
-            } else {
-                return TypeCoercionUtils.ensureSameResultType(originNvl, nvl, context);
+    @Override
+    public Expression visitNullIf(NullIf nullIf, ExpressionRewriteContext context) {
+        NullIf originNullIf = nullIf;
+        nullIf = rewriteChildren(nullIf, context);
+        Expression first = nullIf.left();
+        Expression second = nullIf.right();
+        Expression result = nullIf;
+        // if first is null, then first = second will be null
+        if (first.isNullLiteral() || second.isNullLiteral()) {
+            result = first;
+        } else if (first.equals(second)) {
+            // even if first is null, then first = second will be null, then result is first, so the result is also null
+            result = new NullLiteral(originNullIf.getDataType());
+        } else if (first.isLiteral() && second.isLiteral()) {
+            Expression isEqual = visitEqualTo(new EqualTo(first, second), context);
+            if (isEqual.equals(BooleanLiteral.TRUE)) {
+                result = new NullLiteral(originNullIf.getDataType());
+            } else if (isEqual.equals(BooleanLiteral.FALSE) || isEqual.isNullLiteral()) {
+                result = first;
             }
         }
-        // all nulls
-        return TypeCoercionUtils.ensureSameResultType(originNvl, nvl.child(0), context);
+        return TypeCoercionUtils.ensureSameResultType(originNullIf, result, context);
     }
 
     private <E extends Expression> E rewriteChildren(E expr, ExpressionRewriteContext context) {
@@ -787,7 +807,7 @@ public class FoldConstantRuleOnFE extends AbstractExpressionRewriteRule
     }
 
     private Optional<Expression> preProcess(Expression expression) {
-        if (expression instanceof AggregateFunction || expression instanceof TableGeneratingFunction) {
+        if (!expression.foldable()) {
             return Optional.of(expression);
         }
         if (ExpressionUtils.hasNullLiteral(expression.getArguments())
