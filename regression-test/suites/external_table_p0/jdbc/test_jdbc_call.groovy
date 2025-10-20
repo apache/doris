52e6d70f9af157fcb9fbb6ad09bc8cb9ddfe4e47
zy-kkk <zhongyongkang@selectdb.com>
2025-10-20 14:53:33 +0800

    [test](jdbc) Upgraded the mysql jdbc driver jar under test to 8.4.0

--- regression-test/suites/external_table_p0/jdbc/test_jdbc_call.groovy
+++ regression-test/suites/external_table_p0/jdbc/test_jdbc_call.groovy
@@ -21,7 +21,7 @@ suite("test_jdbc_call", "p0,external,doris,external_docker,external_docker_doris
     String jdbcPassword = context.config.jdbcPassword
     String s3_endpoint = getS3Endpoint()
     String bucket = getS3BucketName()
-    String driver_url = "https://${bucket}.${s3_endpoint}/regression/jdbc_driver/mysql-connector-java-8.0.25.jar"
+    String driver_url = "https://${bucket}.${s3_endpoint}/regression/jdbc_driver/mysql-connector-j-8.4.0.jar"
 
     String catalog_name = "jdbc_call";
     String non_jdbc_catalog_name = "non_jdbc_catalog";
