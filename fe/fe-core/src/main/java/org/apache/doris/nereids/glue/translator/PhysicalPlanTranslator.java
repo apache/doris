0cfa249238dc72f0b35a6b7ee462a9176d63d457
Mingyu Chen (Rayner) <morningman@163.com>
2025-10-22 19:43:54 +0800

    [fix](outfile) fix analysis error when specifying parquet schema (#57232)
    Related PR: #33016
    
    Introduced from #33016, when specify the "schema" property in outfile
    clause with parquet format,
    it will return error:
    ```
    Parquet schema number does not equal to select item number
    ```
    
    This is because we wrongly analyze `OutfileClause` twice.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -551,7 +551,7 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
 
         // TODO: should not call legacy planner analyze in Nereids
         try {
-            outFile.analyze(null, outputExprs, labels);
+            outFile.analyze(null, outputExprs, labels, true);
         } catch (Exception e) {
             throw new AnalysisException(e.getMessage(), e.getCause());
         }
