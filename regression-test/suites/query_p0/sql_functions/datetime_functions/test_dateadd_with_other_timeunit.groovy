6ddc1169242723dc1c4eeba4891f92f4961533d6
csding <csding18@qq.com>
2025-10-23 16:13:08 +0800

    add test

---
+++ regression-test/suites/query_p0/sql_functions/datetime_functions/test_dateadd_with_other_timeunit.groovy
@@ -0,0 +1,57 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import java.text.SimpleDateFormat
+
+suite("test_date_add_with_other_timeunit") {
+    def tableName = "test_date_add_with_other_timeunit"
+
+    sql """ DROP TABLE IF EXISTS ${tableName} """
+    sql """
+            CREATE TABLE IF NOT EXISTS ${tableName} (
+                test_datetime datetime NULL COMMENT "",
+                test_date date NULL COMMENT ""
+            ) ENGINE=OLAP
+            DUPLICATE KEY(test_datetime)
+            COMMENT "OLAP"
+            DISTRIBUTED BY HASH(test_datetime) BUCKETS 1
+            PROPERTIES (
+                "replication_allocation" = "tag.location.default: 1",
+                "in_memory" = "false",
+                "storage_format" = "V2"
+            )
+        """
+    sql """ insert into ${tableName} values ("2019-08-01 13:21:03", "2025-09-08") """
+    
+    qt_sql """ select date_add("2019-08-01 13:21:03", INTERVAL "1 1:1:1" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01 13:21:03", INTERVAL "-1 -1:1:1" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01 13:21:03", INTERVAL "-1 -1:1:1 43" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01 13:21:03", INTERVAL "-1 -1:1:1xxx" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01", INTERVAL "1 1:1:1" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01", INTERVAL "-1 -1:1:1" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01", INTERVAL "-1 -1:1:1 34" DAY_SECOND); """
+    qt_sql """ select date_add("2019-08-01", INTERVAL "-1 -1:1:1xx" DAY_SECOND); """
+    qt_sql """ select date_add(test_datetime, INTERVAL "1 1:1:1" DAY_SECOND) result from ${tableName}; """
+    qt_sql """ select date_add(test_date, INTERVAL "1 1:1:1" DAY_SECOND) result from ${tableName}; """
+
+    test {
+        sql """ select date_add(test_datetime, INTERVAL '1 xx:00:01' DAY_SECOND) result from ${tableName}; """
+        // check exception message contains
+        exception "Invalid hours format"
+    }
+
+}
