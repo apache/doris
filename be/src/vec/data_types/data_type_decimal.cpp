7d05ff7cecd2c5cb446c8d1d20c1eaf53cfcf5d2
BiteTheDDDDt <xl@selectdb.com>
2025-10-24 14:18:11 +0800

    make DecimalScaleInfo<TYPE_DECIMALV2> have static precision and scale

--- be/src/vec/data_types/data_type_decimal.cpp
+++ be/src/vec/data_types/data_type_decimal.cpp
@@ -118,6 +118,13 @@ std::string DataTypeDecimal<T>::do_get_name() const {
 template <PrimitiveType T>
 bool DataTypeDecimal<T>::equals(const IDataType& rhs) const {
     if (auto* ptype = typeid_cast<const DataTypeDecimal<T>*>(&rhs)) {
+        if constexpr (T == TYPE_DECIMALV2) {
+            return precision == ptype->precision && scale == ptype->scale &&
+                   DataTypeDecimal<TYPE_DECIMALV2>::get_original_precision() ==
+                           ptype->DataTypeDecimal<TYPE_DECIMALV2>::get_original_precision() &&
+                   DataTypeDecimal<TYPE_DECIMALV2>::get_original_scale() ==
+                           ptype->DataTypeDecimal<TYPE_DECIMALV2>::get_original_scale();
+        }
         return precision == ptype->get_precision() && scale == ptype->get_scale();
     }
     return false;
