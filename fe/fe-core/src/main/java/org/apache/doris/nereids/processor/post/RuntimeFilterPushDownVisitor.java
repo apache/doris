0ecdaca10d2d4b23c987be36efdf352119dc6b40
minghong <zhouminghong@selectdb.com>
2025-10-27 11:46:58 +0800

    tmp

--- fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterPushDownVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/processor/post/RuntimeFilterPushDownVisitor.java
@@ -269,7 +269,11 @@ public class RuntimeFilterPushDownVisitor extends PlanVisitor<Boolean, PushDownC
         probExprList.add(ctx.probeExpr);
         Pair<PhysicalRelation, Slot> srcPair = ctx.rfContext.getAliasTransferMap().get(ctx.srcExpr);
         PhysicalRelation srcNode = (srcPair == null) ? null : srcPair.first;
-        Pair<PhysicalRelation, Slot> targetPair = ctx.rfContext.getAliasTransferMap().get(ctx.probeExpr);
+        Expression probe = ctx.probeExpr;
+        if (ctx.probeExpr instanceof Cast) {
+            probe = ctx.probeExpr.child(0);
+        }
+        Pair<PhysicalRelation, Slot> targetPair = ctx.rfContext.getAliasTransferMap().get(probe);
         if (targetPair == null) {
             /* cases for "targetPair is null"
              when probeExpr is output slot of setOperator, targetPair is null
