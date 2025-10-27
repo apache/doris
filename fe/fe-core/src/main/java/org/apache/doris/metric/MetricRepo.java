4750923ce62a48b4b056e5691ff58b8395c9b967
deardeng <dengxin@selectdb.com>
2025-10-27 13:43:52 +0800

    [opt](cloud) Exposes cloud balance metrics  (#57200)
    Exposes cloud balance related metrics to show whether the compute group
    is performing balance scheduling. When `*_balance_num` metrics are all
    0, the current compute group is considered to be in a balanced state.
    Note: These metrics are valid only when requesting the fe master
    (balance scheduling is performed on the fe master)
    
    
    ```
     curl "http://175.42.1.1:8030/metrics" |rg '_balance_num'
    # HELP doris_fe_cloud_table_balance_num current cluster cloud table balance sync edit log number
    # TYPE doris_fe_cloud_table_balance_num counter
    doris_fe_cloud_table_balance_num{cluster_id="compute_cluster_id", cluster_name="compute_cluster"} 5
    doris_fe_cloud_table_balance_num{cluster_id="other_cluster_id", cluster_name="other_cluster"} 0
    # HELP doris_fe_cloud_partition_balance_num current cluster cloud partition balance sync edit log number
    # TYPE doris_fe_cloud_partition_balance_num counter
    doris_fe_cloud_partition_balance_num{cluster_id="compute_cluster_id", cluster_name="compute_cluster"} 0
    doris_fe_cloud_partition_balance_num{cluster_id="other_cluster_id", cluster_name="other_cluster"} 0
    # HELP doris_fe_cloud_smooth_upgrade_balance_num current cluster cloud smooth upgrade sync edit log number
    # TYPE doris_fe_cloud_smooth_upgrade_balance_num counter
    doris_fe_cloud_smooth_upgrade_balance_num{cluster_id="compute_cluster_id", cluster_name="compute_cluster"} 0
    doris_fe_cloud_smooth_upgrade_balance_num{cluster_id="other_cluster_id", cluster_name="other_cluster"} 0
    # HELP doris_fe_cloud_global_balance_num current cluster cloud be balance sync edit log number
    # TYPE doris_fe_cloud_global_balance_num counter
    doris_fe_cloud_global_balance_num{cluster_id="compute_cluster_id", cluster_name="compute_cluster"} 0
    doris_fe_cloud_global_balance_num{cluster_id="other_cluster_id", cluster_name="other_cluster"} 0
    # HELP doris_fe_cloud_warm_up_balance_num current cluster cloud warm up cache sync edit log number
    # TYPE doris_fe_cloud_warm_up_balance_num counter
    doris_fe_cloud_warm_up_balance_num{cluster_id="compute_cluster_id", cluster_name="compute_cluster"} 0
    doris_fe_cloud_warm_up_balance_num{cluster_id="other_cluster_id", cluster_name="other_cluster"} 0
    ```

--- fe/fe-core/src/main/java/org/apache/doris/metric/MetricRepo.java
+++ fe/fe-core/src/main/java/org/apache/doris/metric/MetricRepo.java
@@ -21,6 +21,7 @@ import org.apache.doris.alter.Alter;
 import org.apache.doris.alter.AlterJobV2.JobType;
 import org.apache.doris.catalog.Database;
 import org.apache.doris.catalog.Env;
+import org.apache.doris.cloud.catalog.CloudTabletRebalancer;
 import org.apache.doris.cloud.system.CloudSystemInfoService;
 import org.apache.doris.common.Config;
 import org.apache.doris.common.InternalErrorCode;
@@ -1036,6 +1037,32 @@ public final class MetricRepo {
 
         String key = clusterId + CloudMetrics.CLOUD_CLUSTER_DELIMITER + clusterName;
         CloudMetrics.CLUSTER_QUERY_LATENCY_HISTO.getOrAdd(key);
+
+        LongCounterMetric clusterCloudPartitionBalanceNum =
+                CloudMetrics.CLUSTER_CLOUD_PARTITION_BALANCE_NUM.getOrAdd(clusterId);
+        clusterCloudPartitionBalanceNum.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(clusterCloudPartitionBalanceNum);
+
+        LongCounterMetric clusterCloudTableBalanceNum =
+                CloudMetrics.CLUSTER_CLOUD_TABLE_BALANCE_NUM.getOrAdd(clusterId);
+        clusterCloudTableBalanceNum.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(clusterCloudTableBalanceNum);
+
+        LongCounterMetric clusterCloudGlobalBalanceNum =
+                CloudMetrics.CLUSTER_CLOUD_GLOBAL_BALANCE_NUM.getOrAdd(clusterId);
+        clusterCloudGlobalBalanceNum.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(clusterCloudGlobalBalanceNum);
+
+        LongCounterMetric clusterCloudSmoothUpgradeBalanceNum =
+                CloudMetrics.CLUSTER_CLOUD_SMOOTH_UPGRADE_BALANCE_NUM.getOrAdd(clusterId);
+        clusterCloudSmoothUpgradeBalanceNum.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(clusterCloudSmoothUpgradeBalanceNum);
+
+        LongCounterMetric clusterCloudWarmUpBalanceNum =
+                CloudMetrics.CLUSTER_CLOUD_WARM_UP_CACHE_BALANCE_NUM.getOrAdd(clusterId);
+        clusterCloudWarmUpBalanceNum.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(clusterCloudWarmUpBalanceNum);
+
     }
 
     public static void increaseClusterRequestAll(String clusterName) {
@@ -1248,5 +1275,39 @@ public final class MetricRepo {
         }
         String key = clusterId + CloudMetrics.CLOUD_CLUSTER_DELIMITER + clusterName;
         CloudMetrics.CLUSTER_QUERY_LATENCY_HISTO.getOrAdd(key).update(elapseMs);
+    }
+
+    public static void updateClusterCloudBalanceNum(String clusterName, String clusterId,
+                                                    CloudTabletRebalancer.StatType type, long num) {
+        if (!MetricRepo.isInit || Config.isNotCloudMode() || Strings.isNullOrEmpty(clusterName)
+                || Strings.isNullOrEmpty(clusterId)) {
+            return;
+        }
+        LongCounterMetric counter = null;
+        switch (type) {
+            case PARTITION:
+                counter = CloudMetrics.CLUSTER_CLOUD_PARTITION_BALANCE_NUM.getOrAdd(clusterId);
+                break;
+            case TABLE:
+                counter = CloudMetrics.CLUSTER_CLOUD_TABLE_BALANCE_NUM.getOrAdd(clusterId);
+                break;
+            case GLOBAL:
+                counter = CloudMetrics.CLUSTER_CLOUD_GLOBAL_BALANCE_NUM.getOrAdd(clusterId);
+                break;
+            case SMOOTH_UPGRADE:
+                counter = CloudMetrics.CLUSTER_CLOUD_SMOOTH_UPGRADE_BALANCE_NUM.getOrAdd(clusterId);
+                break;
+            case WARM_UP_CACHE:
+                counter = CloudMetrics.CLUSTER_CLOUD_WARM_UP_CACHE_BALANCE_NUM.getOrAdd(clusterId);
+                break;
+            default:
+                return;
+        }
+        List<MetricLabel> labels = new ArrayList<>();
+        counter.update(num);
+        labels.add(new MetricLabel("cluster_id", clusterId));
+        labels.add(new MetricLabel("cluster_name", clusterName));
+        counter.setLabels(labels);
+        MetricRepo.DORIS_METRIC_REGISTER.addMetrics(counter);
     }
 }
