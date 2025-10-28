a8a750402fe9588ee45c7855a7dfa481260c3dcd
wudi <wudi@selectdb.com>
2025-10-28 10:29:27 +0800

    fix case

--- fe/fe-core/src/main/java/org/apache/doris/job/extensions/insert/streaming/StreamingInsertTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/extensions/insert/streaming/StreamingInsertTask.java
@@ -27,7 +27,6 @@ import org.apache.doris.job.exception.JobException;
 import org.apache.doris.job.extensions.insert.InsertTask;
 import org.apache.doris.job.offset.Offset;
 import org.apache.doris.job.offset.SourceOffsetProvider;
-import org.apache.doris.job.offset.s3.S3Offset;
 import org.apache.doris.nereids.StatementContext;
 import org.apache.doris.nereids.glue.LogicalPlanAdapter;
 import org.apache.doris.nereids.parser.NereidsParser;
@@ -154,17 +153,12 @@ public class StreamingInsertTask {
     }
 
     private void run() throws JobException {
-        StreamingInsertJob job =
-                (StreamingInsertJob) Env.getCurrentEnv().getJobManager().getJob(getJobId());
-        StreamingInsertTask runningStreamTask = job.getRunningStreamTask();
-        log.info("current running stream task id is {} for job id {}",
-                runningStreamTask == null ? -1 : runningStreamTask.getTaskId(), getJobId());
         if (isCanceled.get()) {
             log.info("task has been canceled, task id is {}", getTaskId());
             return;
         }
-        log.info("start to run streaming insert task, label {}, offset is {}, filepath {}",
-                labelName, runningOffset.toString(), ((S3Offset) runningOffset).getFileLists());
+        log.info("start to run streaming insert task, label {}, offset is {}, fileNum is {}",
+                labelName, runningOffset.toString(), runningOffset.getFileNumber());
         String errMsg = null;
         try {
             taskCommand.run(ctx, stmtExecutor);
