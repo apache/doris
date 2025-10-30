633f777cc13e70ab9d92f6cd6fddb5f55940c01f
jacktengg <tengjianping@selectdb.com>
2025-10-31 00:06:51 +0800

    add tests

--- regression-test/suites/datatype_p0/timestamptz/test_timestamptz_storage_uniq_key.groovy
+++ regression-test/suites/datatype_p0/timestamptz/test_timestamptz_storage_uniq_key.groovy
@@ -68,34 +68,34 @@ suite("test_timestamptz_storage_uniq_key") {
     qt_select_timestamptz_storage_uniq_key """
         SELECT * FROM timestamptz_storage_uniq_key ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_eq """
+    qt_eq """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz = '2023-01-01 12:00:00 +03:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_neq """
+    qt_neq """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz != '2023-01-01 12:00:00 +03:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_gt """
+    qt_gt """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz > '2023-03-03 12:00:00 -05:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_ge """
+    qt_ge """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz >= '2023-03-03 12:00:00 -05:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_lt """
+    qt_lt """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz < '2023-05-05 00:00:00 +08:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_le """
+    qt_le """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz <= '2023-05-05 00:00:00 +08:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_in """
+    qt_in """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz in('2023-01-01 12:00:00 +03:00', '2023-02-02 12:00:00 +03:00', '2023-10-10 10:10:10 -03:00') ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_not_in """
+    qt_not_in """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz not in('2023-01-01 12:00:00 +03:00', '2023-02-02 12:00:00 +03:00', '2023-10-10 10:10:10 -03:00') ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_is_null """
+    qt_is_null """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz IS NULL ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_is_not_null """
+    qt_is_not_null """
         SELECT * FROM timestamptz_storage_uniq_key where ts_tz IS NOT NULL ORDER BY 1, 2;
     """
 
@@ -106,6 +106,55 @@ suite("test_timestamptz_storage_uniq_key") {
         """
         exception "no partition for this tuple"
     }
+
+    // multi key columns
+    sql """
+        DROP TABLE IF EXISTS `timestamptz_storage_uniq_key_multi_key_cols`;
+    """
+    sql """
+        CREATE TABLE `timestamptz_storage_uniq_key_multi_key_cols` (
+          `ts_tz` TIMESTAMPTZ,
+          `name` VARCHAR(50),
+          `ts_tz_replace` TIMESTAMPTZ
+        ) UNIQUE KEY(`ts_tz`, `name`)
+        partition by RANGE(`ts_tz`) (
+            PARTITION p2023_01 VALUES LESS THAN ('2023-02-01 00:00:00 +00:00'),
+            PARTITION p2023_02 VALUES LESS THAN ('2023-03-01 00:00:00 +00:00'),
+            PARTITION p2023_03 VALUES LESS THAN ('2023-04-01 00:00:00 +00:00'),
+            PARTITION p2023_04 VALUES LESS THAN ('2023-05-01 00:00:00 +00:00'),
+            PARTITION p2023_05 VALUES LESS THAN ('2023-06-01 00:00:00 +00:00'),
+            PARTITION p2023_06 VALUES LESS THAN ('2023-07-01 00:00:00 +00:00'),
+            PARTITION p2023_07 VALUES LESS THAN ('2023-08-01 00:00:00 +00:00'),
+            PARTITION p2023_08 VALUES LESS THAN ('2023-09-01 00:00:00 +00:00'),
+            PARTITION p2023_09 VALUES LESS THAN ('2023-10-01 00:00:00 +00:00'),
+            PARTITION p2023_10 VALUES LESS THAN ('2023-11-01 00:00:00 +00:00'),
+            PARTITION p2023_11 VALUES LESS THAN ('2023-12-01 00:00:00 +00:00'),
+            PARTITION p2023_12 VALUES LESS THAN ('2024-01-01 00:00:00 +00:00'),
+            PARTITION pmax VALUES LESS THAN MAXVALUE
+        )
+        DISTRIBUTED BY HASH(`ts_tz`) BUCKETS 16
+        PROPERTIES (
+        "replication_num" = "1"
+        );
+    """
+
+    sql """INSERT INTO timestamptz_storage_uniq_key_multi_key_cols VALUES
+    (null, null, '0000-01-01 00:00:00 +00:00'),
+    (null, 'jack', '0000-01-01 00:00:00 +00:00'),
+    (null, 'rose', '0000-01-01 00:00:00 +00:00'),
+    ('0000-01-01 03:00:00 +03:00', null,   '0000-01-01 00:00:00 +00:00'),
+    ('0000-01-01 03:00:00 +03:00', 'jack', '0000-01-01 00:00:00 +00:00'),
+    ('0000-01-01 03:00:00 +03:00', 'rose', '0000-01-01 00:00:00 +00:00'),
+    ('2023-01-01 15:00:00 -05:00', null,   '0000-01-01 00:00:00 +00:00'),
+    ('2023-01-01 15:00:00 -05:00', 'jack', '0000-01-01 00:00:00 +00:00'),
+    ('2023-01-01 15:00:00 -05:00', 'rose', '0000-01-01 00:00:00 +00:00'),
+    ('9999-12-31 23:59:59 +08:00', null,   '0000-01-01 00:00:00 +00:00'),
+    ('9999-12-31 23:59:59 +08:00', 'jack', '0000-01-01 00:00:00 +00:00'),
+    ('9999-12-31 23:59:59 +08:00', 'rose', '0000-01-01 00:00:00 +00:00')
+    """
+    qt_multi_key_cols_all0 """
+        SELECT * FROM timestamptz_storage_uniq_key_multi_key_cols ORDER BY 1, 2;
+    """
 
     sql """
         DROP TABLE IF EXISTS `timestamptz_storage_uniq_key_scale`;
@@ -154,39 +203,39 @@ suite("test_timestamptz_storage_uniq_key") {
     ('2023-12-12 12:12:12.999999 +09:00', '2023-12-12 12:12:12.999999 +09:00', 13);
     """
 
-    qt_select_timestamptz_storage_uniq_key_scale """
+    qt_scale """
         SELECT * FROM timestamptz_storage_uniq_key_scale ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_eq """
+    qt_scale_eq """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz = '0000-01-01 00:00:00.000001 +00:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_neq """
+    qt_scale_neq """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz != '0000-01-01 00:00:00.000001 +00:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_gt """
+    qt_scale_gt """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz > '0000-01-01 00:00:00.000001 +00:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_ge """
+    qt_scale_ge """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz >= '0000-01-01 00:00:00.000001 +00:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_lt """
+    qt_scale_lt """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz < '9999-12-30 23:59:59.999999 +00:00' ORDER BY 1, 2;
     """
     //    SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz < '9999-12-31 23:59:59.999999 +00:00' ORDER BY 1, 2;
-    qt_select_timestamptz_storage_uniq_key_scale_le """
+    qt_scale_le """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz <= '2023-12-12 12:12:12.123461 +09:00' ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_in """
+    qt_scale_in """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz in('0000-01-01 00:00:00.000001 +00:00', '9999-12-31 23:59:59.999999 +00:00', '2023-12-12 12:12:12.123461 +09:00') ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_not_in """
+    qt_scale_not_in """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz not in('0000-01-01 00:00:00.000001 +00:00', '9999-12-30 23:59:59.999999 +00:00', '2023-12-12 12:12:12.123461 +09:00') ORDER BY 1, 2;
     """
     //    SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz not in('0000-01-01 00:00:00.000001 +00:00', '9999-12-31 23:59:59.999999 +00:00', '2023-12-12 12:12:12.123461 +09:00') ORDER BY 1, 2;
-    qt_select_timestamptz_storage_uniq_key_scale_is_null """
+    qt_scale_is_null """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz IS NULL ORDER BY 1, 2;
     """
-    qt_select_timestamptz_storage_uniq_key_scale_is_not_null """
+    qt_scale_is_not_null """
         SELECT * FROM timestamptz_storage_uniq_key_scale where ts_tz IS NOT NULL ORDER BY 1, 2;
     """
     // no partition
