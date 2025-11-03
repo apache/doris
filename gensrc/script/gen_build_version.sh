54787ed5c655095ffdd0fa0609f24c8b40234cfc
yiguolei <guolei@selectdb.com>
2025-11-03 12:25:39 +0800

    add 4.0.1-rc01

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -35,9 +35,9 @@ fi
 build_version_prefix="${vendor}"
 build_version_major=4
 build_version_minor=0
-build_version_patch=0
+build_version_patch=1
 build_version_hotfix=0
-build_version_rc_version="rc02"
+build_version_rc_version="rc01"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}"
 if [[ ${build_version_hotfix} -gt 0 ]]; then
