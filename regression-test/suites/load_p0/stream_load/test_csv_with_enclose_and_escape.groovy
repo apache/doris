apache/doris

commit 084c8023ada3fe98cf19f347d36b1dbcb3a8fbb8

[fix](csv) escape quote with double quote for csv format table (#50101)
### What problem does this PR solve?

Problem Summary:
According to the CSV standard format definition, quote characters inside
a string should be escaped using a pair of quote characters. However,
the current implementation does not handle this case correctly, which
may lead to incorrect parsing results when the input string contains
quote characters.

be/src/vec/exec/format/file_reader/new_plain_text_line_reader.cpp
@@ -149,11 +149,22 @@ void EncloseCsvLineReaderContext::_on_pre_match_enclose(const uint8_t* start, si
                 _should_escape = !_should_escape;
             } else if (_should_escape) [[unlikely]] {
                 _should_escape = false;
-            } else if (start[_idx] == _enclose) [[unlikely]] {
-                _state.forward_to(ReaderState::MATCH_ENCLOSE);
-                ++_idx;
-                return;
+            } else if (_quote_escape) {
+                if (start[_idx] == _enclose) {
+                    // double quote, escaped by quote
+                    _quote_escape = false;
+                } else {
+                    // match enclose
+                    _quote_escape = false;
+                    _state.forward_to(ReaderState::MATCH_ENCLOSE);
+                    return;
+                }
+            } else if (start[_idx] == _enclose) {
+                _quote_escape = true;
+            } else {
+                _quote_escape = false;
             }
+
             ++_idx;
         } while (_idx != len);
 

regression-test/data/load_p0/stream_load/test_csv_with_enclose_and_escape.out
@@ -3,6 +3,8 @@
 1	abc,d\nef,	2023-07-15	,,\nghi,j	2023-07-20T05:48:31	kl,mn,\n
 2	abc	2023-07-15	def	2023-07-20T05:48:31	ghi
 3	abc"de,fg"h	2023-07-15	i\nj,k\n"	2023-07-20T05:48:31	ghi
+4	abc"de,fg"h	2023-07-16	"abc"def"	2023-07-21T06:00	test "escape" in enclose
+5	abc"de,fg"h	2023-07-17	"abc"def"	2023-07-22T07:00	test "escape" in enclose
 6	ab"c	2023-07-20	d"ef"	2023-07-20T05:48:31	"g"hi
 7	aaa	2023-07-20		2023-07-20T05:48:31	
 8	aaa"bbb"ccc	2023-07-20	"aa"bb	2023-07-20T05:48:31	aa"bb"

regression-test/suites/load_p0/stream_load/test_csv_with_enclose_and_escape.groovy
@@ -1,4 +1,3 @@
-
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
@@ -38,6 +37,8 @@ suite("test_csv_with_enclose_and_escape", "p0") {
     def normalCases = [
             'enclose_normal.csv',
             'enclose_with_escape.csv',
+            'enclose_with_escape_quote.csv',
+            'enclose_with_escape_doublequote.csv',
             'enclose_wrong_position.csv',
             'enclose_empty_values.csv'
     ]

be/src/vec/data_types/serde/data_type_serde.h
@@ -151,6 +151,8 @@ public:
          */
         bool converted_from_string = false;
 
+        char quote_char = '"';
+
         char escape_char = 0;
         /**
          * flags for each byte to indicate if escape is needed.
@@ -267,6 +269,14 @@ public:
 
     virtual Status deserialize_one_cell_from_json(IColumn& column, Slice& slice,
                                                   const FormatOptions& options) const = 0;
+
+    // In some cases, CSV and JSON deserialization behaviors may differ
+    // so we provide a default implementation that uses JSON deserialization
+    virtual Status deserialize_one_cell_from_csv(IColumn& column, Slice& slice,
+                                                 const FormatOptions& options) const {
+        return deserialize_one_cell_from_json(column, slice, options);
+    }
+
     // deserialize text vector is to avoid virtual function call in complex type nested loop
     virtual Status deserialize_column_from_json_vector(IColumn& column, std::vector<Slice>& slices,
                                                        uint64_t* num_deserialized,

be/src/vec/data_types/serde/data_type_string_serde.h
@@ -64,6 +64,31 @@ inline void escape_string(const char* src, size_t& len, char escape_char) {
     len = dest_ptr - start;
 }
 
+// specially escape quote with double quote
+inline void escape_string_for_csv(const char* src, size_t& len, char escape_char, char quote_char) {
+    const char* start = src;
+    char* dest_ptr = const_cast<char*>(src);
+    const char* end = src + len;
+    bool escape_next_char = false;
+
+    while (src < end) {
+        if ((src < end - 1 && *src == quote_char && *(src + 1) == quote_char) ||
+            *src == escape_char) {
+            escape_next_char = !escape_next_char;
+        } else {
+            escape_next_char = false;
+        }
+
+        if (escape_next_char) {
+            ++src;
+        } else {
+            *dest_ptr++ = *src++;
+        }
+    }
+
+    len = dest_ptr - start;
+}
+
 template <typename ColumnType>
 class DataTypeStringSerDeBase : public DataTypeSerDe {
     using ColumnStrType = ColumnType;
@@ -184,6 +209,15 @@ public:
         }
         if (options.escape_char != 0) {
             escape_string(slice.data, slice.size, options.escape_char);
+        }
+        assert_cast<ColumnType&>(column).insert_data(slice.data, slice.size);
+        return Status::OK();
+    }
+
+    Status deserialize_one_cell_from_csv(IColumn& column, Slice& slice,
+                                         const FormatOptions& options) const override {
+        if (options.escape_char != 0) {
+            escape_string_for_csv(slice.data, slice.size, options.escape_char, options.quote_char);
         }
         assert_cast<ColumnType&>(column).insert_data(slice.data, slice.size);
         return Status::OK();

be/src/vec/exec/format/csv/csv_reader.cpp
@@ -337,6 +337,7 @@ Status CsvReader::init_reader(bool is_load) {
             (_state != nullptr && _state->trim_tailing_spaces_for_external_table_query());
 
     _options.escape_char = _escape;
+    _options.quote_char = _enclose;
     if (_params.file_attributes.text_params.collection_delimiter.empty()) {
         switch (_text_serde_type) {
         case TTextSerdeType::JSON_TEXT_SERDE:
@@ -623,8 +624,8 @@ Status CsvReader::deserialize_nullable_string(IColumn& column, Slice& slice) {
         }
     }
     static DataTypeStringSerDe stringSerDe;
-    auto st = stringSerDe.deserialize_one_cell_from_json(null_column.get_nested_column(), slice,
-                                                         _options);
+    auto st = stringSerDe.deserialize_one_cell_from_csv(null_column.get_nested_column(), slice,
+                                                        _options);
     if (!st.ok()) {
         // fill null if fail
         null_column.insert_data(nullptr, 0); // 0 is meaningless here
@@ -677,7 +678,7 @@ Status CsvReader::_fill_dest_columns(const Slice& line, Block* block,
             switch (_text_serde_type) {
             case TTextSerdeType::JSON_TEXT_SERDE:
                 RETURN_IF_ERROR(
-                        _serdes[i]->deserialize_one_cell_from_json(*col_ptr, slice, _options));
+                        _serdes[i]->deserialize_one_cell_from_csv(*col_ptr, slice, _options));
                 break;
             case TTextSerdeType::HIVE_TEXT_SERDE:
                 RETURN_IF_ERROR(

be/src/vec/exec/format/file_reader/new_plain_text_line_reader.h
@@ -220,6 +220,7 @@ public:
     inline void refresh_impl() {
         _idx = 0;
         _should_escape = false;
+        _quote_escape = false;
         _result = nullptr;
         _column_sep_positions.clear();
         _state.reset();
@@ -254,6 +255,8 @@ private:
 
     size_t _idx = 0;
     bool _should_escape = false;
+    // quote is specially escaped by quote in csv format
+    bool _quote_escape = false;
 
     const std::string _column_sep;
     std::vector<size_t> _column_sep_positions;

be/test/vec/data_types/data_type_string_test.cpp
@@ -40,6 +40,7 @@
 #include "vec/data_types/data_type.h"
 #include "vec/data_types/data_type_factory.hpp"
 #include "vec/data_types/data_type_nullable.h"
+#include "vec/data_types/serde/data_type_string_serde.h"
 #include "vec/io/reader_buffer.h"
 
 namespace doris::vectorized {
@@ -336,5 +337,64 @@ TEST_F(DataTypeStringTest, get_field) {
     expr_node.__isset.string_literal = true;
     expr_node.string_literal.value = "a";
     EXPECT_EQ(dt_str.get_field(expr_node), Field::create_field<TYPE_STRING>("a"));
+}
+TEST_F(DataTypeStringTest, escape_string) {
+    {
+        char test_str[] = "hello\\world";
+        size_t len = strlen(test_str);
+        escape_string(test_str, len, '\\');
+        EXPECT_EQ(std::string(test_str, len), "helloworld");
+    }
+    {
+        char test_str[] = "helloworld";
+        size_t len = strlen(test_str);
+        escape_string(test_str, len, '\\');
+        EXPECT_EQ(std::string(test_str, len), "helloworld");
+    }
+    {
+        char test_str[] = R"(hello\\world)";
+        size_t len = strlen(test_str);
+        escape_string(test_str, len, '\\');
+        EXPECT_EQ(std::string(test_str, len), R"(hello\world)");
+    }
+    {
+        char test_str[] = R"(\\hello\\)";
+        size_t len = strlen(test_str);
+        escape_string(test_str, len, '\\');
+        EXPECT_EQ(std::string(test_str, len), R"(\hello\)");
+    }
+}
+
+TEST_F(DataTypeStringTest, escape_string_for_csv) {
+    {
+        char test_str[] = R"(hello""world)";
+        size_t len = strlen(test_str);
+        escape_string_for_csv(test_str, len, '\\', '"');
+        EXPECT_EQ(std::string(test_str, len), R"(hello"world)");
+    }
+    {
+        char test_str[] = "helloworld";
+        size_t len = strlen(test_str);
+        escape_string_for_csv(test_str, len, '\\', '"');
+        EXPECT_EQ(std::string(test_str, len), "helloworld");
+    }
+    {
+        char test_str[] = R"("hello""world")";
+        size_t len = strlen(test_str);
+        escape_string_for_csv(test_str, len, '\\', '"');
+        EXPECT_EQ(std::string(test_str, len), R"("hello"world")");
+    }
+    {
+        char test_str[] = R"(\\"hello\\""world\\)";
+        size_t len = strlen(test_str);
+        escape_string_for_csv(test_str, len, '\\', '"');
+        EXPECT_EQ(std::string(test_str, len), R"(\"hello\"world\)");
+    }
+    {
+        char test_str[] = "";
+        size_t len = strlen(test_str);
+        escape_string_for_csv(test_str, len, '\\', '"');
+        EXPECT_EQ(std::string(test_str, len), "");
+    }
 }
 } // namespace doris::vectorized

commit 2facd9edc87a2ef86438588b3885e98bd148e785

[fix](csv reader) fix wrong result when escape same as enclose (#57632)
### What problem does this PR solve?

Fix wrong result when escape same as enclose, introduced by
https://github.com/apache/doris/pull/50101

data:
```
50,"{""a"": 1}"
60,"{""a"": 2}"
```

query
```
select * from local( "backend_id" = "1760087225568", "file_path" = "test.csv", "format" = "csv", "column_separator" = ",", "enclose" = "\"", "escape" = "\""
```

expectation：
```
+------+------------+
| k1   | k2         |
+------+------------+
|   50 | {"a": 1} |
|   60 | {"a": 2} |
+------+------------+
```

real:
```
+------+------------------------+
| k1   | k2                     |
+------+------------------------+
|   50 | {"a": 1}
60,{"a": 2}

 |
+------+------------------------+
```

be/src/vec/exec/format/file_reader/new_plain_text_line_reader.cpp
@@ -150,7 +150,9 @@ void EncloseCsvLineReaderCtx::_on_normal(const uint8_t* start, size_t& len) {
 void EncloseCsvLineReaderCtx::_on_pre_match_enclose(const uint8_t* start, size_t& len) {
     do {
         do {
-            if (start[_idx] == _escape) [[unlikely]] {
+            // When escape and enclose are the same, only use quote-escape logic (double quote escaping)
+            // to avoid conflicts between escape and enclose handling
+            if (_escape != _enclose && start[_idx] == _escape) [[unlikely]] {
                 _should_escape = !_should_escape;
             } else if (_should_escape) [[unlikely]] {
                 _should_escape = false;

regression-test/data/load_p0/stream_load/test_csv_with_enclose_and_escape.out
@@ -13,4 +13,6 @@
 10	ab@@cd@@efg	2023-07-20	ab@@cd$$$efg	2023-07-20T05:48:31	@@ab$$$cd$$$
 10	abc	2023-07-15	def	2023-07-20T05:48:31	ghi
 11	abc,def	2023-07-15	ghi	2023-07-20T05:48:31	jkl\nmne
+12	{"a": 1}	2023-07-15	def	2023-07-20T05:48:31	{"a": 1}
+13	{"a": 2}	2023-07-15	def	2023-07-20T05:48:31	{"a": 2}
 

regression-test/suites/load_p0/stream_load/test_csv_with_enclose_and_escape.groovy
@@ -106,6 +106,15 @@ suite("test_csv_with_enclose_and_escape", "p0") {
         file "enclose_not_trim_quotes.csv"
     }
 
+    streamLoad {
+        table "${tableName}"
+        set 'column_separator', ','
+        set 'enclose', "\""
+        set 'escape', '\"'
+
+        file "enclose_with_same_escape.csv"
+    }
+
     sql "sync"
     qt_select """
         SELECT * FROM ${tableName} ORDER BY k1, k2 