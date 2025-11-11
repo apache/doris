421c65d23a5995cb26e9e21e587b4665867bdcbd
yaoxiao <yx136264032@163.com>
2025-11-11 11:22:11 +0800

    remove IndexDef

--- fe/fe-core/src/main/java/org/apache/doris/alter/SchemaChangeHandler.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/SchemaChangeHandler.java
@@ -27,8 +27,6 @@ import org.apache.doris.analysis.CreateIndexClause;
 import org.apache.doris.analysis.DropColumnClause;
 import org.apache.doris.analysis.DropIndexClause;
 import org.apache.doris.analysis.Expr;
-import org.apache.doris.analysis.IndexDef;
-import org.apache.doris.analysis.IndexDef.IndexType;
 import org.apache.doris.analysis.InvertedIndexUtil;
 import org.apache.doris.analysis.ModifyColumnClause;
 import org.apache.doris.analysis.ModifyTablePropertiesClause;
@@ -90,6 +88,8 @@ import org.apache.doris.nereids.trees.plans.commands.AlterCommand;
 import org.apache.doris.nereids.trees.plans.commands.CancelAlterTableCommand;
 import org.apache.doris.nereids.trees.plans.commands.CancelBuildIndexCommand;
 import org.apache.doris.nereids.trees.plans.commands.info.ColumnDefinition;
+import org.apache.doris.nereids.trees.plans.commands.info.IndexDefinition;
+import org.apache.doris.nereids.trees.plans.commands.info.IndexDefinition.IndexType;
 import org.apache.doris.persist.AlterLightSchemaChangeInfo;
 import org.apache.doris.persist.RemoveAlterJobV2OperationLog;
 import org.apache.doris.persist.TableAddOrDropColumnsInfo;
@@ -2119,7 +2119,7 @@ public class SchemaChangeHandler extends AlterHandler {
                     }
                 } else if (alterClause instanceof BuildIndexClause) {
                     BuildIndexClause buildIndexClause = (BuildIndexClause) alterClause;
-                    IndexDef indexDef = buildIndexClause.getIndexDef();
+                    IndexDefinition indexDef = buildIndexClause.getIndexDef();
                     Index index = buildIndexClause.getIndex();
 
                     for (Column column : olapTable.getBaseSchema()) {
@@ -2136,11 +2136,11 @@ public class SchemaChangeHandler extends AlterHandler {
                         }
                     }
 
-                    if (indexDef.getPartitionNamesInfo().isEmpty()) {
+                    if (indexDef.getPartitionNames().isEmpty()) {
                         indexOnPartitions.put(index.getIndexId(), olapTable.getPartitionNames());
                     } else {
                         indexOnPartitions.put(
-                                index.getIndexId(), new HashSet<>(indexDef.getPartitionNamesInfo()));
+                                index.getIndexId(), new HashSet<>(indexDef.getPartitionNames()));
                     }
 
                     alterIndexes.add(index);
@@ -2168,11 +2168,11 @@ public class SchemaChangeHandler extends AlterHandler {
                         if (enableAddIndexForNewData) {
                             supportLightIndexChange = (
                                     found.getIndexType() == IndexType.NGRAM_BF
-                                            || found.getIndexType() == IndexDef.IndexType.INVERTED);
+                                            || found.getIndexType() == IndexType.INVERTED);
                         }
                     } else {
-                        supportLightIndexChange = found.getIndexType() == IndexDef.IndexType.INVERTED
-                                || found.getIndexType() == IndexDef.IndexType.ANN;
+                        supportLightIndexChange = found.getIndexType() == IndexType.INVERTED
+                                || found.getIndexType() == IndexType.ANN;
                     }
                     if (found != null && supportLightIndexChange) {
                         alterIndexes.add(found);
@@ -2645,7 +2645,7 @@ public class SchemaChangeHandler extends AlterHandler {
             List<AlterJobV2> schemaChangeJobV2List = getUnfinishedAlterJobV2ByTableId(olapTable.getId());
             // current schemaChangeJob job doesn't support batch operation,so just need to get one job
             schemaChangeJobV2 = schemaChangeJobV2List.size() == 0 ? null
-                : Iterables.getOnlyElement(schemaChangeJobV2List);
+                    : Iterables.getOnlyElement(schemaChangeJobV2List);
             if (schemaChangeJobV2 == null) {
                 throw new DdlException(
                     "Table[" + tableName + "] is under schema change state" + " but could not find related job");
@@ -2733,9 +2733,9 @@ public class SchemaChangeHandler extends AlterHandler {
         }
 
         List<Index> existedIndexes = olapTable.getIndexes();
-        IndexDef indexDef = alterClause.getIndexDef();
+        IndexDefinition indexDef = alterClause.getIndexDef();
         Set<String> newColset = Sets.newTreeSet(String.CASE_INSENSITIVE_ORDER);
-        newColset.addAll(indexDef.getColumns());
+        newColset.addAll(indexDef.getColumnNames());
         Set<Long> existedIndexIdSet = Sets.newHashSet();
         if (checkDuplicateIndexes(existedIndexes, indexDef, newColset, existedIndexIdSet, olapTable)) {
             return true;
@@ -2758,7 +2758,7 @@ public class SchemaChangeHandler extends AlterHandler {
             AnnIndexPropertiesChecker.checkProperties(indexDef.getProperties());
         }
 
-        for (String col : indexDef.getColumns()) {
+        for (String col : indexDef.getColumnNames()) {
             Column column = olapTable.getColumn(col);
             if (column != null) {
                 indexDef.checkColumn(column, olapTable.getKeysType(),
@@ -2776,12 +2776,12 @@ public class SchemaChangeHandler extends AlterHandler {
         // when send index description to BE, there maybe cannot find column by name,
         // so here update column name in CreateIndexClause after checkColumn for indexDef,
         // there will use the column name in olapTable instead of the column name in CreateIndexClause.
-        alterIndex.setColumns(indexDef.getColumns());
+        alterIndex.setColumns(indexDef.getColumnNames());
         newIndexes.add(alterIndex);
         return false;
     }
 
-    private boolean checkDuplicateIndexes(List<Index> indexes, IndexDef indexDef, Set<String> newColset,
+    private boolean checkDuplicateIndexes(List<Index> indexes, IndexDefinition indexDef, Set<String> newColset,
                                           Set<Long> existedIndexIdSet, OlapTable olapTable) throws DdlException {
         for (Index index : indexes) {
             if (index.getIndexName().equalsIgnoreCase(indexDef.getIndexName())) {
@@ -2798,7 +2798,7 @@ public class SchemaChangeHandler extends AlterHandler {
                 if (newColset.size() == 1
                         && olapTable.getInvertedIndexFileStorageFormat()
                             .compareTo(TInvertedIndexFileStorageFormat.V2) >= 0) {
-                    String columnName = indexDef.getColumns().get(0);
+                    String columnName = indexDef.getColumnNames().get(0);
                     Column column = olapTable.getColumn(columnName);
                     if (column != null && (column.getType().isStringType() || column.getType().isVariantType())) {
                         boolean isExistingIndexAnalyzer = index.isAnalyzedInvertedIndex();
@@ -2814,7 +2814,7 @@ public class SchemaChangeHandler extends AlterHandler {
                     }
                 } else {
                     throw new DdlException(
-                        indexDef.getIndexType() + " index for columns (" + String.join(",", indexDef.getColumns())
+                        indexDef.getIndexType() + " index for columns (" + String.join(",", indexDef.getColumnNames())
                             + ") already exist.");
                 }
             }
