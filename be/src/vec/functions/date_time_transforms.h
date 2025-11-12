6554d35d7deb278c66a82afee685d67c69a83418
robll-v1 <3922295484@qq.com>
2025-11-12 21:22:29 +0800

    use format tools and format: date_time_transforms.h

--- be/src/vec/functions/date_time_transforms.h
+++ be/src/vec/functions/date_time_transforms.h
@@ -83,7 +83,6 @@ struct ToCenturyImpl {
     static inline auto execute(const NativeType& t) {
         const auto& date_time_value = (typename PrimitiveTypeTraits<PType>::CppType&)(t);
         int year = date_time_value.year();
-        // calculate logical century
         return (year - 1) / 100 + 1;
     }
 
