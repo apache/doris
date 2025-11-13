apache/doris

commit 6241fa215de0797182d1310fb29528bd914959e6

[optimize](coordinator) broadcast finished rpc to be when query/load finished (#57739)
broadcast finished rpc to BE when query/load finished to stop pipeline
task earlier for save resources

fe/fe-core/src/main/java/org/apache/doris/qe/AbstractJobProcessor.java
@@ -35,11 +35,13 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** AbstractJobProcessor */
 public abstract class AbstractJobProcessor implements JobProcessor {
     private final Logger logger = LogManager.getLogger(getClass());
 
+    protected final AtomicBoolean finished = new AtomicBoolean(false);
     protected final CoordinatorContext coordinatorContext;
     protected volatile Optional<PipelineExecutionTask> executionTask;
     protected volatile Optional<Map<BackendFragmentId, SingleFragmentPipelineTask>> backendFragmentTasks;
@@ -66,6 +68,17 @@ public abstract class AbstractJobProcessor implements JobProcessor {
     }
 
     protected void afterSetPipelineExecutionTask(PipelineExecutionTask pipelineExecutionTask) {}
+
+    @Override
+    public void tryFinishSchedule() {
+        if (finished.compareAndSet(false, true)) {
+            this.executionTask.ifPresent(sqlPipelineTask -> {
+                for (MultiFragmentsPipelineTask fragmentsTask : sqlPipelineTask.getChildrenTasks().values()) {
+                    fragmentsTask.cancelExecute(Status.FINISHED);
+                }
+            });
+        }
+    }
 
     @Override
     public final void updateFragmentExecStatus(TReportExecStatusParams params) {

be/src/common/status.h
@@ -80,7 +80,8 @@ namespace ErrorCode {
     TStatusError(NOT_MASTER, true);                       \
     TStatusError(OBTAIN_LOCK_FAILED, false);              \
     TStatusError(SNAPSHOT_EXPIRED, false);                \
-    TStatusError(DELETE_BITMAP_LOCK_ERROR, false);
+    TStatusError(DELETE_BITMAP_LOCK_ERROR, false);        \
+    TStatusError(FINISHED, false);
 // E error_name, error_code, print_stacktrace
 #define APPLY_FOR_OLAP_ERROR_CODES(E)                        \
     E(OK, 0, false);                                         \

fe/fe-core/src/main/java/org/apache/doris/common/Status.java
@@ -28,6 +28,7 @@ public class Status {
     public static final Status OK = new Status();
     public static final Status CANCELLED = new Status(TStatusCode.CANCELLED, "Cancelled");
     public static final Status TIMEOUT = new Status(TStatusCode.TIMEOUT, "Timeout");
+    public static final Status FINISHED = new Status(TStatusCode.FINISHED, "Finished");
 
     private TStatusCode  errorCode; // anything other than OK
     private String errorMsg;
@@ -89,6 +90,10 @@ public class Status {
 
     public boolean isCancelled() {
         return this.errorCode == TStatusCode.CANCELLED;
+    }
+
+    public boolean isFinished() {
+        return this.errorCode == TStatusCode.FINISHED;
     }
 
     public boolean isRpcError() {

fe/fe-core/src/main/java/org/apache/doris/qe/JobProcessor.java
@@ -27,4 +27,6 @@ public interface JobProcessor {
     void cancel(Status cancelReason);
 
     void updateFragmentExecStatus(TReportExecStatusParams params);
+
+    void tryFinishSchedule();
 }

fe/fe-core/src/main/java/org/apache/doris/qe/ResultReceiver.java
@@ -210,6 +210,9 @@ public class ResultReceiver {
     }
 
     public synchronized void cancel(Status reason) {
+        if (reason.isFinished()) {
+            return;
+        }
         if (!runStatus.ok()) {
             LOG.info("ResultReceiver of query {} cancel failed, because its status not ok, "
                     + "maybe cancelled already. current run status is {}, new status is {}.",

fe/fe-core/src/main/java/org/apache/doris/qe/runtime/LoadProcessor.java
@@ -51,6 +51,7 @@ public class LoadProcessor extends AbstractJobProcessor {
     // MarkedCountDownLatch:
     //  key: fragmentId, value: backendId
     private volatile Optional<MarkedCountDownLatch<Integer, Long>> latch;
+    private volatile Optional<MarkedCountDownLatch<Integer, Long>> topFragmentLatch;
     private volatile List<SingleFragmentPipelineTask> topFragmentTasks;
 
     public LoadProcessor(CoordinatorContext coordinatorContext, long jobId) {
@@ -76,7 +77,9 @@ public class LoadProcessor extends AbstractJobProcessor {
 
         topFragmentTasks = Lists.newArrayList();
 
-        LOG.info("dispatch load job: {} to {}", DebugUtil.printId(queryId), coordinatorContext.backends.get().keySet());
+        LOG.info("dispatch load job: {} to {}",
+                DebugUtil.printId(queryId), coordinatorContext.backends.get().keySet()
+        );
     }
 
     @Override
@@ -102,6 +105,13 @@ public class LoadProcessor extends AbstractJobProcessor {
             }
         }
         this.topFragmentTasks = topFragmentTasks;
+
+        // only wait top fragments
+        MarkedCountDownLatch<Integer, Long> topFragmentLatch = new MarkedCountDownLatch<>(topFragmentTasks.size());
+        for (SingleFragmentPipelineTask topFragmentTask : topFragmentTasks) {
+            topFragmentLatch.addMark(topFragmentTask.getFragmentId(), topFragmentTask.getBackend().getId());
+        }
+        this.topFragmentLatch = Optional.of(topFragmentLatch);
     }
 
     @Override
@@ -225,7 +235,18 @@ public class LoadProcessor extends AbstractJobProcessor {
                 LOG.debug("Query {} fragment {} is marked done",
                         DebugUtil.printId(coordinatorContext.queryId), params.getFragmentId());
             }
-            latch.get().markedCountDown(params.getFragmentId(), params.getBackendId());
+            MarkedCountDownLatch<Integer, Long> latch = this.latch.get();
+            latch.markedCountDown(params.getFragmentId(), params.getBackendId());
+
+            int topFragmentId = coordinatorContext.topDistributedPlan
+                    .getFragmentJob().getFragment().getFragmentId().asInt();
+            if (topFragmentId == params.getFragmentId()) {
+                MarkedCountDownLatch<Integer, Long> topFragmentLatch = this.topFragmentLatch.get();
+                topFragmentLatch.markedCountDown(params.getFragmentId(), params.getBackendId());
+                if (topFragmentLatch.getCount() == 0) {
+                    tryFinishSchedule();
+                }
+            }
         }
     }
 

fe/fe-core/src/main/java/org/apache/doris/qe/runtime/MultiFragmentsPipelineTask.java
@@ -109,7 +109,7 @@ public class MultiFragmentsPipelineTask extends AbstractRuntimeTask<Integer, Sin
         }
 
         if (this.hasCancelled.get() || this.cancelInProcess.get()) {
-            LOG.info("Frangment has already been cancelled. Query {} backend: {}",
+            LOG.info("Fragment has already been cancelled. Query {} backend: {}",
                     DebugUtil.printId(queryId), backend);
             return;
         }
@@ -121,18 +121,21 @@ public class MultiFragmentsPipelineTask extends AbstractRuntimeTask<Integer, Sin
                                 brpcAddress, queryId, cancelReason);
                 Futures.addCallback(cancelResult, new FutureCallback<PCancelPlanFragmentResult>() {
                     public void onSuccess(InternalService.PCancelPlanFragmentResult result) {
-                        cancelInProcess.set(false);
-                        if (result.hasStatus()) {
-                            Status status = new Status(result.getStatus());
-                            if (status.getErrorCode() == TStatusCode.OK) {
-                                hasCancelled.set(true);
+                        try {
+                            if (result.hasStatus()) {
+                                Status status = new Status(result.getStatus());
+                                if (status.getErrorCode() == TStatusCode.OK) {
+                                    hasCancelled.set(true);
+                                } else {
+                                    LOG.warn("Failed to cancel query {} backend: {}, reason: {}",
+                                            DebugUtil.printId(queryId), backend, status.toString());
+                                }
                             } else {
-                                LOG.warn("Failed to cancel query {} backend: {}, reason: {}",
-                                        DebugUtil.printId(queryId), backend, status.toString());
+                                LOG.warn("Failed to cancel query {} backend: {} reason: {}",
+                                        DebugUtil.printId(queryId), backend, "without status");
                             }
-                        } else {
-                            LOG.warn("Failed to cancel query {} backend: {} reason: {}",
-                                    DebugUtil.printId(queryId), backend, "without status");
+                        } finally {
+                            cancelInProcess.set(false);
                         }
                     }
 

fe/fe-core/src/main/java/org/apache/doris/qe/runtime/QueryProcessor.java
@@ -118,6 +118,28 @@ public class QueryProcessor extends AbstractJobProcessor {
     }
 
     public RowBatch getNext() throws UserException, InterruptedException, TException, RpcException, ExecutionException {
+        try {
+            RowBatch resultBatch = doGetNext();
+
+            // if reached limit rows, cancel this query immediately
+            // to avoid BE from reading more data.
+            // ATTN: if change here, also need to change the same logic in Coordinator.getNext();
+            boolean reachedLimit = LimitUtils.cancelIfReachLimit(
+                    resultBatch, limitRows, numReceivedRows, coordinatorContext::cancelSchedule);
+            if (reachedLimit) {
+                resultBatch.setEos(true);
+            } else if (resultBatch.isEos()) {
+                tryFinishSchedule();
+            }
+            return resultBatch;
+        } catch (Throwable t) {
+            tryFinishSchedule();
+            throw t;
+        }
+    }
+
+    private RowBatch doGetNext()
+            throws UserException, InterruptedException, TException, RpcException, ExecutionException {
         Status status = new Status();
         RowBatch resultBatch = receiverConsumer.getNext(status);
         if (!status.ok()) {
@@ -132,8 +154,10 @@ public class QueryProcessor extends AbstractJobProcessor {
                 copyStatus.rewriteErrorMsg();
             }
             if (copyStatus.isRpcError()) {
+                tryFinishSchedule();
                 throw new RpcException(null, copyStatus.getErrorMsg());
             } else {
+                tryFinishSchedule();
                 String errMsg = copyStatus.getErrorMsg();
                 LOG.warn("query failed: {}", errMsg);
                 throw new UserException(errMsg);
@@ -149,16 +173,6 @@ public class QueryProcessor extends AbstractJobProcessor {
             numReceivedRows += resultBatch.getQueryStatistics().getReturnedRows();
         } else if (resultBatch.getBatch() != null) {
             numReceivedRows += resultBatch.getBatch().getRowsSize();
-        }
-
-        // if reached limit rows, cancel this query immediately
-        // to avoid BE from reading more data.
-        // ATTN: if change here, also need to change the same logic in Coordinator.getNext();
-        boolean reachedLimit = LimitUtils.cancelIfReachLimit(
-                resultBatch, limitRows, numReceivedRows, coordinatorContext::cancelSchedule);
-
-        if (reachedLimit) {
-            resultBatch.setEos(true);
         }
         return resultBatch;
     }

commit d990c5ee95f857299c1eed0c18ac49c43dffea49

[fix](coordinator) fix "PStatus: Finished" (#57985)
fix unstable insert exception, introduced by #57739:
```
Exception:
java.sql.SQLException: errCode = 2, detailMessage = (172.16.0.149)[FINISHED]PStatus: Finished
```

fe/fe-core/src/main/java/org/apache/doris/qe/AbstractJobProcessor.java
@@ -24,6 +24,8 @@ import org.apache.doris.qe.runtime.MultiFragmentsPipelineTask;
 import org.apache.doris.qe.runtime.PipelineExecutionTask;
 import org.apache.doris.qe.runtime.SingleFragmentPipelineTask;
 import org.apache.doris.thrift.TReportExecStatusParams;
+import org.apache.doris.thrift.TStatus;
+import org.apache.doris.thrift.TStatusCode;
 import org.apache.doris.thrift.TUniqueId;
 
 import com.google.common.base.Preconditions;
@@ -82,6 +84,9 @@ public abstract class AbstractJobProcessor implements JobProcessor {
 
     @Override
     public final void updateFragmentExecStatus(TReportExecStatusParams params) {
+        if (params.status.status_code == TStatusCode.FINISHED) {
+            params.status = new TStatus(TStatusCode.OK);
+        }
         SingleFragmentPipelineTask fragmentTask = backendFragmentTasks.get().get(
                 new BackendFragmentId(params.getBackendId(), params.getFragmentId()));
         if (fragmentTask == null) {