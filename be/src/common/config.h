886b93f66fd15f17299519f954864de0edcab7e7
bobhan1 <baohan@selectdb.com>
2025-11-13 16:27:47 +0800

    add file_cache_tail_read_extra_factor

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1185,6 +1185,7 @@ DECLARE_mInt64(file_cache_background_lru_dump_tail_record_num);
 DECLARE_mInt64(file_cache_background_lru_log_replay_interval_ms);
 DECLARE_mInt32(file_cache_num_parallel_prefetch);
 DECLARE_mInt64(file_cache_tail_read_extra_bytes_threshold);
+DECLARE_mInt64(file_cache_tail_read_extra_factor);
 DECLARE_mBool(enable_evaluate_shadow_queue_diff);
 
 // inverted index searcher cache
