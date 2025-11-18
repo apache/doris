f05a582ab24177ac462e53f86d293a36c3f120cc
deardeng <565620795@qq.com>
2025-11-18 16:02:56 +0800

    branch-4.0: [feature](cloud) Read peer be cache when balance in same cluster #56384 (#58089)
    cherry pick from #56384

--- regression-test/suites/cloud_p0/balance/test_balance_warm_up_with_compaction_use_peer_cache.groovy
+++ regression-test/suites/cloud_p0/balance/test_balance_warm_up_with_compaction_use_peer_cache.groovy
@@ -18,7 +18,7 @@
 import org.apache.doris.regression.suite.ClusterOptions
 
 
-suite('test_balance_warm_up', 'docker') {
+suite('test_balance_warm_up_with_compaction_use_peer_cache', 'docker') {
     if (!isCloudMode()) {
         return;
     }
@@ -29,13 +29,17 @@ suite('test_balance_warm_up', 'docker') {
         'sys_log_verbose_modules=org',
         'heartbeat_interval_second=1',
         'rehash_tablet_after_be_dead_seconds=3600',
-        'cloud_warm_up_for_rebalance_type=async_warmup'
+        'cloud_warm_up_for_rebalance_type=async_warmup',
+        'cloud_pre_heating_time_limit_sec=30',
+        // disable Auto Analysis Job Executor
+        'auto_check_statistics_in_minutes=60',
     ]
     options.beConfigs += [
         'report_tablet_interval_seconds=1',
         'schedule_sync_tablets_interval_s=18000',
         'disable_auto_compaction=true',
         'sys_log_verbose_modules=*',
+        'cumulative_compaction_min_deltas=5',
         'cache_read_from_peer_expired_seconds=100',
         'enable_cache_read_from_peer=true'
     ]
@@ -44,6 +48,12 @@ suite('test_balance_warm_up', 'docker') {
     options.cloudMode = true
     options.enableDebugPoints()
     
+    def mergeDirs = { base, add ->
+        base + add.collectEntries { host, hashFiles ->
+            [(host): base[host] ? (base[host] + hashFiles) : hashFiles]
+        }
+    }
+
     def getBrpcMetrics = {ip, port, name ->
         def url = "http://${ip}:${port}/brpc_metrics"
         def metrics = new URL(url).text
@@ -58,6 +68,7 @@ suite('test_balance_warm_up', 'docker') {
     def testCase = { table -> 
         def ms = cluster.getAllMetaservices().get(0)
         def msHttpPort = ms.host + ":" + ms.httpPort
+        sql """set enable_file_cache=true"""
         sql """CREATE TABLE $table (
                 `id` BIGINT,
                 `deleted` TINYINT,
@@ -86,111 +97,82 @@ suite('test_balance_warm_up', 'docker') {
         sql """
             insert into $table values  (44, 1, 'comment', 'spez', '2006-10-11 23:00:48', 'Welcome back, Randall',  0, 43, 0, [454465], '', 0, '', [], 0), (46, 0, 'story', 'goldfish', '2006-10-11 23:39:28', '',  0, 0, 0, [454470], 'http://www.rentometer.com/', 0, ' VCs Prefer to Fund Nearby Firms - New York Times', [], 0);
         """
-
-        // more tablets accessed. for test metrics `balance_tablet_be_mapping_size`
-        sql """CREATE TABLE more_tablets_warm_up_test_tbl (
-            `k1` int(11) NULL,
-            `v1` VARCHAR(2048)
-            )
-            DUPLICATE KEY(`k1`)
-            COMMENT 'OLAP'
-            DISTRIBUTED BY HASH(`k1`) BUCKETS 10
-            PROPERTIES (
-            "replication_num"="1"
-            );
+        sql """
+            insert into $table values  (344089, 1, 'comment', 'spez', '2008-10-26 13:49:29', 'Stay tuned...',  0, 343906, 0, [31, 454446], '', 0, '', [], 0), (33, 0, 'comment', 'spez', '2006-10-10 23:50:40', 'winnar winnar chicken dinnar!',  0, 31, 0, [34, 454450], '', 0, '', [], 0);
         """
         sql """
-            insert into more_tablets_warm_up_test_tbl values (1, 'value1'), (2, 'value2'), (3, 'value3'), (4, 'value4'), (5, 'value5'), (6, 'value6'), (7, 'value7'), (8, 'value8'), (9, 'value9'), (10, 'value10'), (11, 'value11'), (12, 'value12'), (13, 'value13'), (14, 'value14'), (15, 'value15'), (16, 'value16'), (17, 'value17'), (18, 'value18'), (19, 'value19'), (20, 'value20');
+            insert into $table values  (449, 1, 'comment', 'spez', '2006-10-11 23:00:48', 'Welcome back, Randall',  0, 43, 0, [454465], '', 0, '', [], 0), (469, 0, 'story', 'goldfish', '2006-10-11 23:39:28', '',  0, 0, 0, [454470], 'http://www.rentometer.com/', 0, ' VCs Prefer to Fund Nearby Firms - New York Times', [], 0);
         """
+        sql """
+            insert into $table values  (344084, 1, 'comment', 'spez', '2008-10-26 13:49:29', 'Stay tuned...',  0, 343906, 0, [31, 454446], '', 0, '', [], 0), (33, 0, 'comment', 'spez', '2006-10-10 23:50:40', 'winnar winnar chicken dinnar!',  0, 31, 0, [34, 454450], '', 0, '', [], 0);
+        """
+        sql """
+            insert into $table values  (849, 1, 'comment', 'spez', '2006-10-11 23:00:48', 'Welcome back, Randall',  0, 43, 0, [454465], '', 0, '', [], 0), (869, 0, 'story', 'goldfish', '2006-10-11 23:39:28', '',  0, 0, 0, [454470], 'http://www.rentometer.com/', 0, ' VCs Prefer to Fund Nearby Firms - New York Times', [], 0);
+        """
+        // trigger compaction to generate some cache files
+        trigger_and_wait_compaction(table, "cumulative")
+        sleep(5 * 1000)
+
+        def beforeCacheDirVersion7 = getTabletFileCacheDirFromBe(msHttpPort, table, 7)
 
         // before add be
         def beforeGetFromFe = getTabletAndBeHostFromFe(table)
         def beforeGetFromBe = getTabletAndBeHostFromBe(cluster.getAllBackends())
-        // version 2
-        def beforeDataCacheDirVersion2 = getTabletFileCacheDirFromBe(msHttpPort, table, 2, "dat")
-        def beforeIdxCacheDirVersion2 = getTabletFileCacheDirFromBe(msHttpPort, table, 2, "idx")
-        logger.info("cache dir version 2 data={}, idx={}", beforeDataCacheDirVersion2, beforeIdxCacheDirVersion2)
-        // version 3
-        def beforeDataCacheDirVersion3 = getTabletFileCacheDirFromBe(msHttpPort, table, 3, "dat")
-        def beforeIdxCacheDirVersion3 = getTabletFileCacheDirFromBe(msHttpPort, table, 3, "idx")
-        logger.info("cache dir version 3 data={}, idx={}", beforeDataCacheDirVersion3, beforeIdxCacheDirVersion3)
-
-        // 通用合并函数：按 host 合并多个 map, 并去重
-        def mergeCacheDirs = { Map[] maps ->
-            def result = [:].withDefault { [] } // 每个不存在的 key 都会生成新的 List
-            maps.each { m ->
-                if (!m) return
-                m.each { host, files ->
-                    if (!files) return
-                    def target = result[host]
-                    if (files instanceof Collection) {
-                        target.addAll(files)
-                    } else {
-                        target << files
-                    }
-                }
-            }
-            // 确保每个 host 的文件列表去重后返回
-            result.collectEntries { host, files -> [ (host): files.unique() ] }
-        }
-
-        def beforeMergedCacheDir = mergeCacheDirs(
-            beforeDataCacheDirVersion2,
-            beforeIdxCacheDirVersion2,
-            beforeDataCacheDirVersion3,
-            beforeIdxCacheDirVersion3
-        )
-        logger.info("before fe tablets {}, be tablets {}, cache dir {}", beforeGetFromFe, beforeGetFromBe, beforeMergedCacheDir)
 
         def beforeWarmUpResult = sql_return_maparray """ADMIN SHOW REPLICA DISTRIBUTION FROM $table"""
         logger.info("before warm up result {}", beforeWarmUpResult)
 
+        // disable cloud balance
+        setFeConfig('enable_cloud_multi_replica', true)
         cluster.addBackend(1, "compute_cluster")
+        GetDebugPoint().enableDebugPointForAllBEs("FileCacheBlockDownloader.download_blocks.balance_task")
+        setFeConfig('enable_cloud_multi_replica', false)
+
+      
+        def afterMergedVersion7CacheDir = [beforeCacheDirVersion7]
+            .inject([:]) { acc, m -> mergeDirs(acc, m) }
+        logger.info("after version 7 fe tablets {}, be tablets {}, cache dir {}", beforeGetFromFe, beforeGetFromBe, afterMergedVersion7CacheDir)
+
+        // after cloud_pre_heating_time_limit_sec = 30s 
+        sleep(40 * 1000)
+        // after 30s task timeout, check tablet in new be
+
         def oldBe = sql_return_maparray('show backends').get(0)
         def newAddBe = sql_return_maparray('show backends').get(1)
         // balance tablet
         awaitUntil(500) {
-            def afterWarmUpResult = sql_return_maparray """ADMIN SHOW REPLICA DISTRIBUTION FROM $table"""
-            logger.info("after warm up result {}", afterWarmUpResult)
-            afterWarmUpResult.any { row -> 
+            def afterWarmUpTaskTimeoutResult = sql_return_maparray """ADMIN SHOW REPLICA DISTRIBUTION FROM $table"""
+            logger.info("after warm up result {}", afterWarmUpTaskTimeoutResult)
+            afterWarmUpTaskTimeoutResult.any { row -> 
                 Integer.valueOf((String) row.ReplicaNum) == 1
             }
         }
-
-        sql """select count(*) from more_tablets_warm_up_test_tbl"""
 
         // from be1 -> be2, warm up this tablet
         // after add be
         def afterGetFromFe = getTabletAndBeHostFromFe(table)
         def afterGetFromBe = getTabletAndBeHostFromBe(cluster.getAllBackends())
-        // version 2
-        def afterDataCacheDirVersion2 = getTabletFileCacheDirFromBe(msHttpPort, table, 2, "dat")
-        def afterIdxCacheDirVersion2 = getTabletFileCacheDirFromBe(msHttpPort, table, 2, "idx")
-        logger.info("after cache dir version 2 data={}, idx={}", afterDataCacheDirVersion2, afterIdxCacheDirVersion2)
-        // version 3
-        def afterDataCacheDirVersion3 = getTabletFileCacheDirFromBe(msHttpPort, table, 3, "dat")
-        def afterIdxCacheDirVersion3 = getTabletFileCacheDirFromBe(msHttpPort, table, 3, "idx")
-        logger.info("after cache dir version 3 data={}, idx={}", afterDataCacheDirVersion3, afterIdxCacheDirVersion3)
+        // version 7
+        def afterCacheDirVersion7 = getTabletFileCacheDirFromBe(msHttpPort, table, 7)
+        logger.info("after cache dir version 7 {}", afterCacheDirVersion7)
+    
+        def afterMergedCacheDir = [afterCacheDirVersion7]
+            .inject([:]) { acc, m -> mergeDirs(acc, m) }
 
-        def afterMergedCacheDir = mergeCacheDirs(
-            afterDataCacheDirVersion2,
-            afterIdxCacheDirVersion2,
-            afterDataCacheDirVersion3,
-            afterIdxCacheDirVersion3 
-        )
         logger.info("after fe tablets {}, be tablets {}, cache dir {}", afterGetFromFe, afterGetFromBe, afterMergedCacheDir)
+        // calc file cache hash on new added BE, but these cache files should not exist on new BE yet
         def newAddBeCacheDir = afterMergedCacheDir.get(newAddBe.Host)
         logger.info("new add be cache dir {}", newAddBeCacheDir)
         assert newAddBeCacheDir.size() != 0
-        assert beforeMergedCacheDir[oldBe.Host].containsAll(afterMergedCacheDir[newAddBe.Host])
+        assert afterMergedVersion7CacheDir[oldBe.Host].containsAll(afterMergedCacheDir[newAddBe.Host])
 
         def be = cluster.getBeByBackendId(newAddBe.BackendId.toLong())
         def dataPath = new File("${be.path}/storage/file_cache")
         logger.info("Checking file_cache directory: {}", dataPath.absolutePath)
         logger.info("Directory exists: {}", dataPath.exists())
-        
+
         def subDirs = []
-        
+
         def collectDirs
         collectDirs = { File dir ->
             if (dir.exists()) {
@@ -201,23 +183,52 @@ suite('test_balance_warm_up', 'docker') {
                 }
             }
         }
-        
+
         collectDirs(dataPath)
         logger.info("BE {} file_cache subdirs: {}", newAddBe.Host, subDirs)
 
+        // check new be not have version 7 cache file
         newAddBeCacheDir.each { hashFile ->
-            assertTrue(subDirs.any { subDir -> subDir.startsWith(hashFile) }, 
-            "Expected cache file pattern ${hashFile} not found in BE ${newAddBe.Host}'s file_cache directory. " + 
+            assertFalse(subDirs.any { subDir -> subDir.startsWith(hashFile) }, 
+            "Expected cache file pattern ${hashFile} should not found in BE ${newAddBe.Host}'s file_cache directory. " + 
             "Available subdirs: ${subDirs}")
         }
 
-        sleep(105 * 1000)
-        // test expired be tablet cache info be removed
-        // after cache_read_from_peer_expired_seconds = 100s
-        assert(0 == getBrpcMetrics(newAddBe.Host, newAddBe.BrpcPort, "balance_tablet_be_mapping_size"))
+        // The query triggers reading the file cache from the peer
+        profile("test_balance_warm_up_with_compaction_use_peer_cache_profile") {
+            sql """ set enable_profile = true;"""
+            sql """ set profile_level = 2;"""
+            run {
+                sql """/* test_balance_warm_up_with_compaction_use_peer_cache_profile */ SELECT count() FROM $table WHERE comment MATCH_ALL 'Welcome'"""
+                sleep(1000)
+            }
+
+            check { profileString, exception ->
+                log.info(profileString)
+                // Use a regular expression to match the numeric value inside parentheses after "NumPeerIOTotal:"
+                def matcher = (profileString =~ /-  InvertedIndexNumPeerIOTotal:\s+(\d+)/)
+                def total = 0
+                while (matcher.find()) {
+                    total += matcher.group(1).toInteger()
+                    logger.info("InvertedIndexNumPeerIOTotal: {}", matcher.group(1))
+                }
+                assertTrue(total > 0)
+            } 
+        }
+        subDirs.clear()
+        collectDirs(dataPath)
+        logger.info("after query, BE {} file_cache subdirs: {}", newAddBe.Host, subDirs) 
+        // peer read cache, so it should have version 7 cache file
+        newAddBeCacheDir.each { hashFile ->
+            assertTrue(subDirs.any { subDir -> subDir.startsWith(hashFile) }, 
+            "Expected cache file pattern ${hashFile} should found in BE ${newAddBe.Host}'s file_cache directory. " + 
+            "Available subdirs: ${subDirs}")
+        }
+        assert(0 != getBrpcMetrics(newAddBe.Host, newAddBe.BrpcPort, "cached_remote_reader_peer_read"))
+        assert(0 == getBrpcMetrics(newAddBe.Host, newAddBe.BrpcPort, "cached_remote_reader_s3_read"))
     }
 
     docker(options) {
-        testCase("test_balance_warm_up_tbl")
+        testCase("test_balance_warm_up_with_compaction_use_peer_cache_tbl")
     }
 }
