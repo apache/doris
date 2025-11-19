1a33b4b9276ee8eaa89f57605c8c433b5ad94996
zhangstar333 <zhangsida@selectdb.com>
2025-11-19 12:00:22 +0800

    update case

--- docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/iceberg/run22.sql
+++ docker/thirdparties/docker-compose/iceberg/scripts/create_preinstalled_scripts/iceberg/run22.sql
@@ -1,5 +1,6 @@
 
-USE iceberg.transform_partition_db;
+create database if not exists demo.test_varbinary;
+USE demo.test_varbinary;
 
 DROP TABLE IF EXISTS test_ice_uuid_orc;
 DROP TABLE IF EXISTS test_ice_uuid_parquet;
@@ -38,3 +39,53 @@ INSERT INTO test_ice_uuid_parquet VALUES
 
 SELECT * FROM test_ice_uuid_orc;
 SELECT * FROM test_ice_uuid_parquet;
+
+DROP TABLE IF EXISTS test_ice_uuid_orc_write_no_mapping;
+DROP TABLE IF EXISTS test_ice_uuid_parquet_write_no_mapping;
+
+CREATE TABLE test_ice_uuid_orc_write_no_mapping (
+    id INT,
+    col1 BINARY COMMENT 'UUID stored as 16-byte binary',
+    col2 BINARY COMMENT 'Binary data'
+)
+USING iceberg
+TBLPROPERTIES(
+    'write.format.default' = 'orc',
+    'format-version' = '1'
+);
+
+CREATE TABLE test_ice_uuid_parquet_write_no_mapping (
+    id INT,
+    col1 BINARY COMMENT 'UUID stored as 16-byte binary',
+    col2 BINARY COMMENT 'Binary data'
+)
+USING iceberg
+TBLPROPERTIES(
+    'write.format.default' = 'parquet',
+    'format-version' = '1'
+);
+
+DROP TABLE IF EXISTS test_ice_uuid_orc_write_with_mapping;
+DROP TABLE IF EXISTS test_ice_uuid_parquet_write_with_mapping;
+
+CREATE TABLE test_ice_uuid_orc_write_with_mapping (
+    id INT,
+    col1 BINARY COMMENT 'UUID stored as 16-byte binary',
+    col2 BINARY COMMENT 'Binary data'
+)
+USING iceberg
+TBLPROPERTIES(
+    'write.format.default' = 'orc',
+    'format-version' = '1'
+);
+
+CREATE TABLE test_ice_uuid_parquet_write_with_mapping (
+    id INT,
+    col1 BINARY COMMENT 'UUID stored as 16-byte binary',
+    col2 BINARY COMMENT 'Binary data'
+)
+USING iceberg
+TBLPROPERTIES(
+    'write.format.default' = 'parquet',
+    'format-version' = '1'
+);
