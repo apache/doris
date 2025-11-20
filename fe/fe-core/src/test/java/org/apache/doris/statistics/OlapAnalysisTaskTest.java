9b950f6017681d9a3e9deb18f0a9b860ed655ae3
yujun <yujun@selectdb.com>
2025-11-20 11:40:25 +0800

    collect min max, also collect count

--- fe/fe-core/src/test/java/org/apache/doris/statistics/OlapAnalysisTaskTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/statistics/OlapAnalysisTaskTest.java
@@ -111,8 +111,8 @@ public class OlapAnalysisTaskTest {
 
         new Expectations() {
             {
-                tableIf.getRowCount();
-                result = 20000000;
+                // tableIf.getRowCount();
+                // result = 20000000;
                 tableIf.getId();
                 result = 30001;
                 catalogIf.getId();
@@ -126,10 +126,11 @@ public class OlapAnalysisTaskTest {
 
         new MockUp<OlapAnalysisTask>() {
             @Mock
-            public ResultRow collectMinMax() {
+            public ResultRow collectMinMaxCount() {
                 List<String> values = Lists.newArrayList();
                 values.add("1");
                 values.add("2");
+                values.add("20000000");
                 return new ResultRow(values);
             }
 
