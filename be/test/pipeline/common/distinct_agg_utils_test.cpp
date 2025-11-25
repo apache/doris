1c9c88e6383111864158c5ad1c7977578f6d4f31
BiteTheDDDDt <xl@selectdb.com>
2025-11-25 13:18:55 +0800

    fix ut

--- be/test/pipeline/common/distinct_agg_utils_test.cpp
+++ be/test/pipeline/common/distinct_agg_utils_test.cpp
@@ -159,6 +159,11 @@ TEST_F(DistinctAggUtilsTest, TestDistinctDataVariantsInitFixedKeys) {
                         vectorized::MethodKeysFixed<DistinctData<vectorized::UInt64>>>(
                     variants.method_variant));
             break;
+        case HashKeyType::fixed72:
+            ASSERT_TRUE(std::holds_alternative<
+                        vectorized::MethodKeysFixed<DistinctData<vectorized::UInt72>>>(
+                    variants.method_variant));
+            break;
         case HashKeyType::fixed128:
             ASSERT_TRUE(std::holds_alternative<
                         vectorized::MethodKeysFixed<DistinctData<vectorized::UInt128>>>(
@@ -190,7 +195,7 @@ TEST_F(DistinctAggUtilsTest, TestDistinctDataVariantsInitFixedKeys) {
 
     test_block({std::make_shared<vectorized::DataTypeInt64>(),
                 std::make_shared<vectorized::DataTypeUInt8>()},
-               HashKeyType::fixed128);
+               HashKeyType::fixed72);
 
     test_block({std::make_shared<vectorized::DataTypeInt64>(),
                 std::make_shared<vectorized::DataTypeInt64>()},
