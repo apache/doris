06790e0ca49d380fe26523d38185fd8b06b6e3f2
0AyanamiRei <3244156674@qq.com>
2025-11-26 10:59:42 +0800

    f

--- regression-test/suites/load_p0/stream_load/test_stream_load_with_set.groovy
+++ regression-test/suites/load_p0/stream_load/test_stream_load_with_set.groovy
@@ -16,12 +16,14 @@
 // under the License.
 
 suite("test_stream_load_with_set", "load_p0") {
-    def tableName = "stream_load_with_set"
+    def tableName1 = "stream_load_with_set_constant_value"
+    def tableName2 = "stream_load_with_set_constant_func"
 
-    sql """ DROP TABLE IF EXISTS ${tableName} """
+    sql """ DROP TABLE IF EXISTS ${tableName1} """
+    sql """ DROP TABLE IF EXISTS ${tableName2} """
 
     sql """
-        CREATE TABLE ${tableName}
+        CREATE TABLE ${tableName1}
         (
             k00 INT             NOT NULL,
             k01 DATE            NOT NULL,
@@ -51,8 +53,39 @@ suite("test_stream_load_with_set", "load_p0") {
         );
     """
 
+    sql """
+        CREATE TABLE ${tableName2}
+        (
+            k00 INT             NOT NULL,
+            k01 DATE            NOT NULL,
+            k02 BOOLEAN         NULL,
+            k03 TINYINT         NULL,
+            k04 SMALLINT        NULL,
+            k05 INT             NULL,
+            k06 BIGINT          NULL,
+            k07 LARGEINT        NULL,
+            k08 FLOAT           NULL,
+            k09 DOUBLE          NULL,
+            k10 DECIMAL(9,1)    NULL,
+            k11 DECIMALV3(9,1)  NULL,
+            k12 DATETIME        NULL,
+            k13 DATEV2          NULL,
+            k14 DATETIMEV2      NULL,
+            k15 CHAR            NULL,
+            k16 VARCHAR         NULL,
+            kd01 INT            NULL,
+            k17 STRING          NULL,
+            k18 JSON            NULL
+        )
+        DUPLICATE KEY(k00)
+        DISTRIBUTED BY HASH(k00) BUCKETS 1
+        PROPERTIES (
+            "replication_num" = "1"
+        );
+    """
+
     streamLoad {
-        table "${tableName}"
+        table "${tableName1}"
         set 'column_separator', '|'
         set 'columns', "k00,k01,k02,k03,k04,k05,k06,k07,k08,k09,k10,k11,k12,k13,k14,k15,k16,k17,k18,kd01=20240123"
         file "basic_data.csv"
@@ -73,7 +106,7 @@ suite("test_stream_load_with_set", "load_p0") {
     }
 
     streamLoad {
-        table "${tableName}"
+        table "${tableName1}"
         set 'column_separator', '|'
         set 'columns', "kd01=20240123"
         file "basic_data.csv"
@@ -92,5 +125,28 @@ suite("test_stream_load_with_set", "load_p0") {
             assertEquals(0, json.NumberUnselectedRows)
         }
     }
-    qt_select """ select count(*) from ${tableName} """
+
+    streamLoad {
+        table "${tableName2}"
+        set 'column_separator', '|'
+        set 'columns', "kd01=abs(-2) + 3"
+        file "basic_data.csv"
+        time 10000 // limit inflight 10s
+
+        check { result, exception, startTime, endTime ->
+            if (exception != null) {
+                throw exception
+            }
+            log.info("Stream load result: ${result}".toString())
+            def json = parseJson(result)
+            assertEquals("success", json.Status.toLowerCase())
+            assertEquals(20, json.NumberTotalRows)
+            assertEquals(20, json.NumberLoadedRows)
+            assertEquals(0, json.NumberFilteredRows)
+            assertEquals(0, json.NumberUnselectedRows)
+        }
+    }
+
+    qt_select1 """ select count(*) from ${tableName1} """
+    qt_select2 """ select count(*) from ${tableName2} """
 }
