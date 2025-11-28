7e62e73e5b50b6b48059c1aacafa0f4631d49f85
zhiqiang <seuhezhiqiang@163.com>
2025-11-28 13:14:56 +0800

    [fix](ann index) fix omp threads limit can not be assigned manually (#58427)
    Make sure user specified omp_threads_limit is working.
    
    Related PR: https://github.com/apache/doris/pull/56796

--- be/test/common/config_validator_test.cpp
+++ be/test/common/config_validator_test.cpp
@@ -17,8 +17,11 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
 #include "common/config.h"
 #include "common/status.h"
+#include "util/cpu_info.h"
 
 namespace doris {
 using namespace config;
@@ -45,5 +48,48 @@ TEST(ConfigValidatorTest, Validator) {
     s = config::set_config("cfg_validator_2", "8");
     EXPECT_TRUE(s.ok());
     EXPECT_EQ(cfg_validator_2, 8);
+}
+
+// When a positive value is specified, validator should use it directly.
+TEST(ConfigValidatorTest, OmpThreadsLimitExplicitValue) {
+    int32_t original_limit = config::omp_threads_limit;
+
+    Status st = config::set_config("omp_threads_limit", "7", false, true);
+    ASSERT_TRUE(st.ok());
+    EXPECT_EQ(7, config::omp_threads_limit);
+
+    config::omp_threads_limit = original_limit;
+}
+
+// When value is -1 and config::num_cores is set,
+// validator should pick 80% of that core count.
+TEST(ConfigValidatorTest, OmpThreadsLimitAutoUsesConfiguredNumCores) {
+    int32_t original_limit = config::omp_threads_limit;
+    int32_t original_num_cores = config::num_cores;
+
+    config::num_cores = 20;
+    Status st = config::set_config("omp_threads_limit", "-1", false, true);
+    ASSERT_TRUE(st.ok());
+    EXPECT_EQ(16, config::omp_threads_limit);
+
+    config::num_cores = original_num_cores;
+    config::omp_threads_limit = original_limit;
+}
+
+// When value is -1 and config::num_cores is 0,
+// validator should fall back to CpuInfo::num_cores().
+TEST(ConfigValidatorTest, OmpThreadsLimitAutoFallsBackToCpuInfo) {
+    int32_t original_limit = config::omp_threads_limit;
+    int32_t original_num_cores = config::num_cores;
+
+    config::num_cores = 0;
+    Status st = config::set_config("omp_threads_limit", "-1", false, true);
+    ASSERT_TRUE(st.ok());
+    CpuInfo::init();
+    int expected = std::max(1, CpuInfo::num_cores() * 4 / 5);
+    EXPECT_EQ(expected, config::omp_threads_limit);
+
+    config::num_cores = original_num_cores;
+    config::omp_threads_limit = original_limit;
 }
 } // namespace doris
