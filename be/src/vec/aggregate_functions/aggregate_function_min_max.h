7be940227200c29af8b94703e69cde3c87e8f9b2
wumeibanfa <904360463@qq.com>
2025-11-28 20:27:06 +0800

    fix

--- be/src/vec/aggregate_functions/aggregate_function_min_max.h
+++ be/src/vec/aggregate_functions/aggregate_function_min_max.h
@@ -523,19 +523,18 @@ public:
     }
 };
 
-struct SingleValueMaxMinDataComplexType {
+struct SingleValueDataComplexType {
 private:
-    using Self = SingleValueMaxMinDataComplexType;
+    using Self = SingleValueDataComplexType;
 
     DataTypePtr column_type;
-    bool has_value =
-            false; /// We need to remember if at least one value has been passed. This is necessary for AggregateFunctionIf.
+    bool has_value = false;
     MutableColumnPtr column_data; // a column ptr only save a single value
     int be_exec_version = -1;
 
 public:
-    SingleValueMaxMinDataComplexType() = default;
-    SingleValueMaxMinDataComplexType(const DataTypes& argument_types, int be_version) {
+    SingleValueDataComplexType() = default;
+    SingleValueDataComplexType(const DataTypes& argument_types, int be_version) {
         column_type = argument_types[0];
         column_data = column_type->create_column();
         be_exec_version = be_version;
@@ -636,7 +635,13 @@ public:
         if (!has()) {
             return false;
         }
-        return !column_data->compare_at(0, row_num, column, -1);
+        auto type = column_type->get_primitive_type();
+        if (type == TYPE_BITMAP || type == TYPE_HLL || type == TYPE_QUANTILE_STATE ||
+            type == TYPE_AGG_STATE) {
+            return false;
+        } else {
+            return !column_data->compare_at(0, row_num, column, -1);
+        }
     }
 
     void change_first_time(const IColumn& column, size_t row_num, Arena& arena) {
@@ -724,99 +729,17 @@ struct AggregateFunctionAnyData : Data {
     using Data::IsFixedLength;
     static const char* name() { return "any"; }
     constexpr static bool IS_ANY = true;
-    void change_if_better(const IColumn& column, size_t row_num, Arena& arena) {
-        this->change_first_time(column, row_num, arena);
-    }
 
-    void change_if_better(const Self& to, Arena& arena) { this->change_first_time(to, arena); }
-};
+    AggregateFunctionAnyData(const DataTypes& argument_types, int be_version)
+            : Data(argument_types, be_version) {};
 
-// this is used for complex type about any_value function
-struct SingleValueDataComplexType {
-    static const char* name() { return "any"; }
-    constexpr static bool IS_ANY = true;
-    constexpr static bool IsFixedLength = false;
-    using Self = SingleValueDataComplexType;
-
-    SingleValueDataComplexType() = default;
-
-    SingleValueDataComplexType(const DataTypes& argument_types, int be_version) {
-        column_type = argument_types[0];
-        column_data = column_type->create_column();
-        be_exec_version = be_version;
-    }
-
-    bool has() const { return has_value; }
-
-    void change_first_time(const IColumn& column, size_t row_num, Arena&) {
-        if (UNLIKELY(!has())) {
-            change_impl(column, row_num);
-        }
-    }
-
-    void change_first_time(const Self& to, Arena&) {
-        if (UNLIKELY(!has() && to.has())) {
-            change_impl(*to.column_data, 0);
-        }
-    }
-
-    void change_impl(const IColumn& column, size_t row_num) {
-        DCHECK_EQ(column_data->size(), 0);
-        column_data->insert_from(column, row_num);
-        has_value = true;
-    }
-
-    void insert_result_into(IColumn& to) const {
-        if (has()) {
-            to.insert_from(*column_data, 0);
-        } else {
-            to.insert_default();
-        }
-    }
-
-    void reset() {
-        column_data->clear();
-        has_value = false;
-    }
-
-    void write(BufferWritable& buf) const {
-        buf.write_binary(has());
-        if (!has()) {
-            return;
-        }
-        auto size_bytes =
-                column_type->get_uncompressed_serialized_bytes(*column_data, be_exec_version);
-        std::string memory_buffer(size_bytes, '0');
-        auto* p = column_type->serialize(*column_data, memory_buffer.data(), be_exec_version);
-        buf.write_binary(memory_buffer);
-        DCHECK_EQ(p, memory_buffer.data() + size_bytes);
-    }
-
-    void read(BufferReadable& buf, Arena& arena) {
-        buf.read_binary(has_value);
-        if (!has()) {
-            return;
-        }
-        std::string memory_buffer;
-        buf.read_binary(memory_buffer);
-        const auto* p =
-                column_type->deserialize(memory_buffer.data(), &column_data, be_exec_version);
-        DCHECK_EQ(p, memory_buffer.data() + memory_buffer.size());
-    }
+    AggregateFunctionAnyData() {};
 
     void change_if_better(const IColumn& column, size_t row_num, Arena& arena) {
         this->change_first_time(column, row_num, arena);
     }
 
     void change_if_better(const Self& to, Arena& arena) { this->change_first_time(to, arena); }
-
-    bool check_if_equal(const IColumn& column, size_t row_num) const { return false; }
-
-private:
-    bool has_value = false;
-    MutableColumnPtr column_data;
-    DataTypePtr column_type;
-    int be_exec_version = -1;
 };
 
 template <typename Data>
@@ -833,11 +756,9 @@ public:
               type(this->argument_types[0]) {}
 
     void create(AggregateDataPtr __restrict place) const override {
-        if constexpr (std::is_same_v<Data, SingleValueDataComplexType> ||
-                      std::is_same_v<Data,
-                                     AggregateFunctionMaxData<SingleValueMaxMinDataComplexType>> ||
-                      std::is_same_v<Data,
-                                     AggregateFunctionMinData<SingleValueMaxMinDataComplexType>>) {
+        if constexpr (std::is_same_v<Data, AggregateFunctionMaxData<SingleValueDataComplexType>> ||
+                      std::is_same_v<Data, AggregateFunctionMinData<SingleValueDataComplexType>> ||
+                      std::is_same_v<Data, AggregateFunctionAnyData<SingleValueDataComplexType>>) {
             new (place) Data(argument_types, IAggregateFunction::version);
         } else {
             new (place) Data;
