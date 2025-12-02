924e6714b70af49a4e8bb4bc89ed0a92d87ed1e6
Calvin Kirs <guoqiang@selectdb.com>
2025-12-02 15:38:57 +0800

    [chore](test)Add OSS-HDFS test

--- regression-test/suites/external_table_p2/refactor_catalog_param/oss_hdfs_catalog_test.groovy
+++ regression-test/suites/external_table_p2/refactor_catalog_param/oss_hdfs_catalog_test.groovy
@@ -88,6 +88,35 @@ suite("oss_hdfs_catalog_test", "p2,external,new_catalog_property") {
         """
         assert dropResult.size() == 0
     }
+    
+    def testQuery = { String catalog_name,String catalogProperties,String queryTable,String expectedCount, boolean force_jni_scanner = false ->
+        
+        sql """
+            DROP CATALOG IF EXISTS ${catalog_name};
+        """
+        if (force_jni_scanner) {
+            sql """ set force_jni_scanner=true; """
+        }else {
+            sql """ set force_jni_scanner=false; """
+        }
+        sql """
+            CREATE CATALOG IF NOT EXISTS ${catalog_name} PROPERTIES (
+                ${catalogProperties}
+            );
+        """
+        sql """
+            switch ${catalog_name};
+        """
+
+        def queryResult = sql """
+            SELECT count(1) FROM ${queryTable};
+        """
+        assert queryResult[0][0] == expectedCount.toInteger()
+
+        sql """
+            DROP CATALOG IF EXISTS ${catalog_name};
+        """
+    }
 
     /****************OSS_HDFS*******************/
     String oss_hdfs_ak = context.config.otherConfigs.get("ossHdfsAk")
@@ -97,6 +126,7 @@ suite("oss_hdfs_catalog_test", "p2,external,new_catalog_property") {
     String oss_hdfs_parent_path = "${oss_hdfs_bucket}/refactor-test"
     String oss_hdfs_region = context.config.otherConfigs.get("ossHdfsRegion")
     
+
     String old_oss_hdfs_storage_properties = """
               'oss.access_key' = '${oss_hdfs_ak}',
               'oss.secret_key' = '${oss_hdfs_sk}',
@@ -114,13 +144,53 @@ suite("oss_hdfs_catalog_test", "p2,external,new_catalog_property") {
               'oss.hdfs.endpoint' = '${oss_hdfs_endpoint}',
               'oss.hdfs.region'='${oss_hdfs_region}'
     """
+
+
+    //**************** Paimon FILESYSTEM ON OSS_HDFS *******************/
+    String paimon_fs_warehouse = context.config.otherConfigs.get("paimonFsWarehouseOnOssHdfs")
+    String query_table_paimon_fs = context.config.otherConfigs.get("paimonFsWarehouseOnOssHdfsQueryTable")
+    String query_count_paimon_fs = context.config.otherConfigs.get("paimonFsWarehouseOnOssHdfsQueryCount")
+    String paimon_file_system_catalog_properties = """
+     'type'='paimon',
+     'paimon.catalog.type'='filesystem',
+     'warehouse'='${paimon_fs_warehouse}',
+    """
+    testQuery("paimon_fs_oss_hdfs_catalog",paimon_file_system_catalog_properties + old_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,true)
+    testQuery("paimon_fs_oss_hdfs_catalog",paimon_file_system_catalog_properties + old_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,false)
+    testQuery("paimon_fs_oss_hdfs_region_catalog",paimon_file_system_catalog_properties + usingOSSHDFSProps + old_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,true)
+    testQuery("paimon_fs_oss_hdfs_region_catalog",paimon_file_system_catalog_properties + usingOSSHDFSProps + old_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,false)
+    testQuery("paimon_fs_oss_hdfs_new_catalog",paimon_file_system_catalog_properties + new_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,true)
+    testQuery("paimon_fs_oss_hdfs_new_catalog",paimon_file_system_catalog_properties + new_oss_hdfs_storage_properties,query_table_paimon_fs,query_count_paimon_fs,false)
+    //**************** ICEBERG FILESYSTEM ON OSS_HDFS *******************/
+    String iceberg_file_system_catalog_properties = """
+     'type'='iceberg',
+     'iceberg.catalog.type'='hadoop',
+    """
+
+    String warehouse = """
+     'warehouse' = 'oss://${oss_hdfs_parent_path}/iceberg-fs-oss-hdfs-warehouse',
+    """
+    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + old_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs",null)
+
+    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + usingOSSHDFSProps +old_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs_region",null)
+
+    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + new_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs",null)
+
+    String enable_oss_hdfs_hms_catalog_test = context.config.otherConfigs.get("enableOssHdfsHmsCatalogTest")
+    if (enable_oss_hdfs_hms_catalog_test == null || !enable_oss_hdfs_hms_catalog_test.equalsIgnoreCase("true")) {
+        return
+    }
+
+    /*HMS props*/
+    String hms_props = context.config.otherConfigs.get("emrHmsProps")
     String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
     String hmsuri ="thrift://${externalEnvIp}:9083"
     String hms_properties = """
         "type"="hms",
-        "hive.metastore.uris" = "${hmsuri}",
+        ${hms_props},
     """
-    
+    //**************** HIVE HMS ON OSS_HDFS *******************/
+
     String db_location
     //OSS-HDFS
     db_location = "oss://${oss_hdfs_parent_path}/hive/hms/" + System.currentTimeMillis()
@@ -139,28 +209,12 @@ suite("oss_hdfs_catalog_test", "p2,external,new_catalog_property") {
     """
 
     // Basic HMS with OSS storage
-    String warehouse = """
+     warehouse = """
                   'warehouse' = 'oss://${oss_hdfs_parent_path}/iceberg-hms-oss-hdfs-warehouse/',
     """
     testQueryAndInsert(iceberg_hms_type_prop  + warehouse + old_oss_hdfs_storage_properties, "iceberg_hms_on_oss_hdfs_old",null)
     testQueryAndInsert(iceberg_hms_type_prop  + oss_hdfs_endpoint + warehouse + old_oss_hdfs_storage_properties, "iceberg_hms_on_oss_hdfs_old",null)
 
     testQueryAndInsert(iceberg_hms_type_prop  + warehouse + new_oss_hdfs_storage_properties , "iceberg_hms_on_oss_hdfs_new",null)
-
-    //**************** ICEBERG FILESYSTEM ON OSS_HDFS *******************/
-    String iceberg_file_system_catalog_properties = """
-     'type'='iceberg',
-     'iceberg.catalog.type'='hadoop',
-    """
-
-    warehouse = """
-     'warehouse' = 'oss://${oss_hdfs_parent_path}/iceberg-fs-oss-hdfs-warehouse',
-    """
-    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + old_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs",null)
     
-    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + usingOSSHDFSProps +old_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs_region",null)
-    
-    testQueryAndInsert(iceberg_file_system_catalog_properties + warehouse + new_oss_hdfs_storage_properties, "iceberg_fs_on_oss_hdfs",null)
-
-
 }
