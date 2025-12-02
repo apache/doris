6ce023ac048b434dc5eb8613a94b198ed31efe3b
Qi Chen <chenqi@selectdb.com>
2025-12-02 14:30:19 +0800

    branch-3.1: [fix](parquet-reader) Fix read parquet footer is missing in FileReadBytes profile. #58383 (#58593)
    picked from #58383

--- be/src/vec/exec/format/parquet/vparquet_reader.cpp
+++ be/src/vec/exec/format/parquet/vparquet_reader.cpp
@@ -249,8 +249,8 @@ Status ParquetReader::_open_file() {
             const auto& file_meta_cache_key =
                     FileMetaCache::get_key(_tracing_file_reader, _file_description);
             if (!_meta_cache->lookup(file_meta_cache_key, &_meta_cache_handle)) {
-                RETURN_IF_ERROR(parse_thrift_footer(_file_reader, &_file_metadata_ptr, &meta_size,
-                                                    _io_ctx));
+                RETURN_IF_ERROR(parse_thrift_footer(_tracing_file_reader, &_file_metadata_ptr,
+                                                    &meta_size, _io_ctx));
                 // _file_metadata_ptr.release() : move control of _file_metadata to _meta_cache_handle
                 _meta_cache->insert(file_meta_cache_key, _file_metadata_ptr.release(),
                                     &_meta_cache_handle);
