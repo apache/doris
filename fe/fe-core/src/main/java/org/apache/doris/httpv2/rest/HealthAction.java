ad76d9deefff3e094e7255ca59e27d7e2a975b82
Mingyu Chen (Rayner) <morningman@163.com>
2025-12-02 09:55:17 +0800

    branch-4.0: [opt](scheduler) Improve Graceful Shutdown Behavior for BE and FE, and Optimize Query Retry During BE Shutdown #56601 #58019 (#58526)
    bp #56601 #58019

--- fe/fe-core/src/main/java/org/apache/doris/httpv2/rest/HealthAction.java
+++ fe/fe-core/src/main/java/org/apache/doris/httpv2/rest/HealthAction.java
@@ -17,8 +17,10 @@
 
 package org.apache.doris.httpv2.rest;
 
+import org.apache.doris.DorisFE;
 import org.apache.doris.catalog.Env;
 import org.apache.doris.common.Config;
+import org.apache.doris.common.FeConstants;
 import org.apache.doris.httpv2.entity.ResponseEntityBuilder;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,6 +41,10 @@ public class HealthAction extends RestBaseController {
     public Object execute(HttpServletRequest request, HttpServletResponse response) {
         if (Config.enable_all_http_auth) {
             executeCheckPassword(request, response);
+        }
+
+        if (!FeConstants.runningUnitTest && !DorisFE.isServerReady()) {
+            return ResponseEntityBuilder.serviceUnavailable("Server is not ready");
         }
 
         Map<String, Object> result = new HashMap<>();
