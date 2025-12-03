df0e4495f69cc9f2c181c4ef227da6636dc08700
zclllyybb <zhaochangle@selectdb.com>
2025-12-03 21:29:51 +0800

    [chore](tools) split TPC-DS' multi sql in one file (#58691)
    split them to multi files. and modify script to cover all of them.

--- tools/tpcds-tools/queries/sf100/query24_1.sql
+++ tools/tpcds-tools/queries/sf100/query24_1.sql
@@ -24,7 +24,7 @@ where ss_ticket_number = sr_ticket_number
   and c_current_addr_sk = ca_address_sk
   and c_birth_country <> upper(ca_country)
   and s_zip = ca_zip
-and s_market_id=8
+  and s_market_id = 8
 group by c_last_name
         ,c_first_name
         ,s_store_name
@@ -40,12 +40,12 @@ select c_last_name
       ,s_store_name
       ,sum(netpaid) paid
 from ssales
-where i_color = 'beige'
+where i_color = 'blue'
 group by c_last_name
         ,c_first_name
         ,s_store_name
 having sum(netpaid) > (select 0.05*avg(netpaid)
-                                 from ssales)
+                           from ssales)
 order by c_last_name
         ,c_first_name
         ,s_store_name
