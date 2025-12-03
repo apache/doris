6b0103b409360e056470ee632287468ee58f4f19
yiguolei <guolei@selectdb.com>
2025-12-03 15:33:35 +0800

    f

--- be/src/runtime/workload_group/workload_group.cpp
+++ be/src/runtime/workload_group/workload_group.cpp
@@ -389,13 +389,14 @@ WorkloadGroupInfo WorkloadGroupInfo::parse_topic_info(
     }
 
     // 9 scan thread num
-    int scan_thread_num = ScannerScheduler::default_local_scan_thread_num();
+    int scan_thread_num = vectorized::ScannerScheduler::default_local_scan_thread_num();
     if (tworkload_group_info.__isset.scan_thread_num && tworkload_group_info.scan_thread_num > 0) {
         scan_thread_num = tworkload_group_info.scan_thread_num;
     }
 
     // 10 max remote scan thread num
-    int max_remote_scan_thread_num = vectorized::ScannerScheduler::default_remote_scan_thread_num();
+    int max_remote_scan_thread_num =
+            vectorized::vectorized::ScannerScheduler::default_remote_scan_thread_num();
     if (tworkload_group_info.__isset.max_remote_scan_thread_num &&
         tworkload_group_info.max_remote_scan_thread_num > 0) {
         max_remote_scan_thread_num = tworkload_group_info.max_remote_scan_thread_num;
@@ -559,9 +560,9 @@ Status WorkloadGroup::upsert_thread_pool_no_lock(WorkloadGroupInfo* wg_info,
                     "ls_" + wg_name, cg_cpu_ctl_ptr, wg_name);
         }
 
-        Status ret = scan_scheduler->start(scan_thread_num, scan_thread_num,
-                                           config::doris_scanner_thread_pool_queue_size,
-                                           ScannerScheduler::default_min_active_scan_threads());
+        Status ret = scan_scheduler->start(
+                scan_thread_num, scan_thread_num, config::doris_scanner_thread_pool_queue_size,
+                vectorized::ScannerScheduler::default_min_active_scan_threads());
         if (ret.ok()) {
             _scan_task_sched = std::move(scan_scheduler);
         } else {
@@ -572,7 +573,7 @@ Status WorkloadGroup::upsert_thread_pool_no_lock(WorkloadGroupInfo* wg_info,
 
     if (_remote_scan_task_sched == nullptr) {
         int remote_scan_thread_queue_size =
-                vectorized::ScannerScheduler::get_remote_scan_thread_queue_size();
+                vectorized::vectorized::ScannerScheduler::get_remote_scan_thread_queue_size();
         std::unique_ptr<vectorized::ScannerScheduler> remote_scan_scheduler;
         if (config::enable_task_executor_in_external_table) {
             remote_scan_scheduler =
@@ -585,7 +586,7 @@ Status WorkloadGroup::upsert_thread_pool_no_lock(WorkloadGroupInfo* wg_info,
         Status ret = remote_scan_scheduler->start(
                 max_remote_scan_thread_num, min_remote_scan_thread_num,
                 remote_scan_thread_queue_size,
-                ScannerScheduler::default_min_active_file_scan_threads());
+                vectorized::ScannerScheduler::default_min_active_file_scan_threads());
         if (ret.ok()) {
             _remote_scan_task_sched = std::move(remote_scan_scheduler);
         } else {
@@ -616,14 +617,15 @@ Status WorkloadGroup::upsert_thread_pool_no_lock(WorkloadGroupInfo* wg_info,
 
     // 2 update thread pool
     if (scan_thread_num > 0 && _scan_task_sched) {
-        _scan_task_sched->reset_thread_num(scan_thread_num, scan_thread_num,
-                                           ScannerScheduler::default_min_active_scan_threads());
+        _scan_task_sched->reset_thread_num(
+                scan_thread_num, scan_thread_num,
+                vectorized::ScannerScheduler::default_min_active_scan_threads());
     }
 
     if (max_remote_scan_thread_num >= min_remote_scan_thread_num && _remote_scan_task_sched) {
         _remote_scan_task_sched->reset_thread_num(
                 max_remote_scan_thread_num, min_remote_scan_thread_num,
-                ScannerScheduler::default_min_active_file_scan_threads());
+                vectorized::ScannerScheduler::default_min_active_file_scan_threads());
     }
 
     return upsert_ret;
