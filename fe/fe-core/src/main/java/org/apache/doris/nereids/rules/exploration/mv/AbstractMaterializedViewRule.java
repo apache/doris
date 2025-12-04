a128c14d396d10ac0e85624cb53f7c8cb28cbc85
seawinde <wusi@selectdb.com>
2025-12-04 14:20:09 +0800

    [fix](mtmv) Fix hudi materialized view union all rewritten plan execute fail because of invalid slot (#58643)
    ### What problem does this PR solve?
    
    Related PR: #57558 #58413
    
    Problem Summary:
    
    This fix addresses the following three issues:
    1. When invoking the method
    org.apache.doris.nereids.trees.plans.logical.LogicalHudiScan#withRelationId,
    the output needs to be recalculated to meet expectations.
    2. After compensating with a union all due to partial partition
    invalidation of a materialized view, during the next round of
    transparent rewriting, the rewriting for the child of the union
    allshould use the query partitioncorresponding to the specific relation
    id to prevent infinite loops.
    3. Currently, in the `test_hudi_rewrite_mtmv` test, if the plan
    rewritten by the materialized view transparent rewriting is not selected
    by the CBO, it is difficult to troubleshoot because explain memo planis
    not used. Therefore, the corresponding test method is modified.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/AbstractMaterializedViewRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/AbstractMaterializedViewRule.java
@@ -316,7 +316,7 @@ public abstract class AbstractMaterializedViewRule implements ExplorationRuleFac
                 continue;
             }
             Pair<Map<BaseTableInfo, Set<String>>, Map<BaseColInfo, Set<String>>> invalidPartitions;
-            if (PartitionCompensator.needUnionRewrite(materializationContext)
+            if (PartitionCompensator.needUnionRewrite(materializationContext, cascadesContext.getStatementContext())
                     && sessionVariable.isEnableMaterializedViewUnionRewrite()) {
                 MTMV mtmv = ((AsyncMaterializationContext) materializationContext).getMtmv();
                 Map<List<String>, Set<String>> queryUsedPartitions = PartitionCompensator.getQueryUsedPartitions(
