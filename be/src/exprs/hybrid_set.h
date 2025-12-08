ef9238464b07a72e5b1779650ad1d0024005d267
zy-kkk <zhongyongkang@selectdb.com>
2025-12-08 11:26:06 +0800

    [fix](build) fix macOS build

--- be/src/exprs/hybrid_set.h
+++ be/src/exprs/hybrid_set.h
@@ -417,7 +417,7 @@ public:
         std::vector<ElementType> elems(_set.begin(), _set.end());
         std::sort(elems.begin(), elems.end());
         if constexpr (std::is_same<ElementType, bool>::value) {
-            for (const auto& v : elems) {
+            for (bool v : elems) {
                 seed = HashUtil::crc_hash64(&v, sizeof(v), seed);
             }
         } else {
