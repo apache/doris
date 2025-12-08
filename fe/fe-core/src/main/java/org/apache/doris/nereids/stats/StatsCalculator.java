9543f0fd96730ea21f0753bb9744c7aa0d34a699
seawinde <wusi@selectdb.com>
2025-12-08 11:23:22 +0800

    [fix](mv) Fix stats unknown when calc sync mv plan statistics (#58426)
    ### What problem does this PR solve?
    
    Related PR: 
    #36760
    #57850
    
    Problem Summary:
    
    Fix stats unknown when calc sync mv plan statistics
    
    For SQLs that are related to statistics, we should not collect or
    compute statistics. Previously this was determined by the `isInternal`
    flag, but `isInternal` is too broad: it covers not only
    statistics-related SQL but also SQL used to generate materialized view
    plans. Materialized view plan generation requires statistics, so we
    introduce a new flag `isPlanWithUnKnownColumnStats` to indicate
    connections that are used for statistics-only operations (treat column
    statistics as unknown).

--- fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/stats/StatsCalculator.java
@@ -1154,7 +1154,7 @@ public class StatsCalculator extends DefaultPlanVisitor<Statistics, Void> {
     }
 
     private ColumnStatistic getColumnStatistic(TableIf table, String colName, long idxId) {
-        if (connectContext != null && connectContext.getState().isInternal()) {
+        if (connectContext != null && connectContext.getState().isPlanWithUnKnownColumnStats()) {
             return ColumnStatistic.UNKNOWN;
         }
         long catalogId;
@@ -1186,7 +1186,7 @@ public class StatsCalculator extends DefaultPlanVisitor<Statistics, Void> {
 
     private ColumnStatistic getColumnStatistic(
             OlapTableStatistics olapTableStatistics, String colName, List<String> partitionNames) {
-        if (connectContext != null && connectContext.getState().isInternal()) {
+        if (connectContext != null && connectContext.getState().isPlanWithUnKnownColumnStats()) {
             return ColumnStatistic.UNKNOWN;
         }
         OlapTable table = olapTableStatistics.olapTable;
