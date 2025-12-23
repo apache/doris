ec5e96ad20cb1fc11cdeccbc8658f88dcda8cb22
hubiao <hubiao01@corp.netease.com>
2025-12-23 17:01:36 +0800

    1

--- regression-test/suites/external_table_p0/iceberg/test_iceberg_mixed_format.groovy
+++ regression-test/suites/external_table_p0/iceberg/test_iceberg_mixed_format.groovy
@@ -17,7 +17,7 @@
 
 suite("test_iceberg_mixed_format", "p0,external,iceberg,external_docker,external_docker_iceberg") {
     String enabled = context.config.otherConfigs.get("enableIcebergTest")
-    if (enabled != null && "false".equalsIgnoreCase(enabled)) {
+    if (enabled == null || !enabled.equalsIgnoreCase("true")) {
         logger.info("disable Iceberg test")
         return
     }
@@ -26,16 +26,23 @@ suite("test_iceberg_mixed_format", "p0,external,iceberg,external_docker,external
     String catalog_name = "test_iceberg_mixed_format"
     String externalEnvIp = context.config.otherConfigs.get("externalEnvIp")
     String hmsPort = context.config.otherConfigs.get(hivePrefix + "HmsPort")
+    String hdfsPort = context.config.otherConfigs.get(hivePrefix + "HdfsPort")
+    String default_fs = "hdfs://${externalEnvIp}:${hdfsPort}"
+    String warehouse = "${default_fs}/warehouse"
 
     sql """drop catalog if exists ${catalog_name}"""
     sql """create catalog if not exists ${catalog_name} properties (
-        "type"="iceberg",
-        "iceberg.catalog.type"="hive",
-        "hive.metastore.uris"="thrift://${externalEnvIp}:${hmsPort}"
+        'type'='iceberg',
+        'iceberg.catalog.type'='hms',
+        'hive.metastore.uris'='thrift://${externalEnvIp}:${hmsPort}',
+        'fs.defaultFS' = '${default_fs}',
+        'warehouse' = '${warehouse}',
+        'use_meta_cache' = 'true'
     );"""
 
     sql """switch ${catalog_name}"""
-    sql """use iceberg_catalog"""
+    sql """create database if not exists test_mixed_format_db"""
+    sql """use test_mixed_format_db"""
 
     sql """drop table if exists mixed_format_table"""
     sql """
