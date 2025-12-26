811332424b58a6433b2ba16d16cc41a97584153e
seawinde <wusi@selectdb.com>
2025-12-26 20:01:39 +0800

    opt performance by prune query struct info space

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializationContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/MaterializationContext.java
@@ -126,7 +126,7 @@ public abstract class MaterializationContext {
                 && ExplainLevel.MEMO_PLAN == parsedStatement.getExplainOptions().getExplainLevel();
         // Construct materialization struct info, catch exception which may cause planner roll back
         this.structInfo = structInfo == null
-                ? constructStructInfo(plan, originalPlan, cascadesContext, new BitSet()).orElseGet(() -> null)
+                ? constructStructInfo(plan, originalPlan, cascadesContext).orElseGet(() -> null)
                 : structInfo;
         this.available = this.structInfo != null;
         if (available) {
@@ -140,22 +140,14 @@ public abstract class MaterializationContext {
      * @param originalPlan original plan, the output is right
      */
     public static Optional<StructInfo> constructStructInfo(Plan plan, Plan originalPlan,
-            CascadesContext cascadesContext, BitSet expectedTableBitSet) {
-        List<StructInfo> viewStructInfos;
+                                                           CascadesContext cascadesContext) {
         try {
-            viewStructInfos = MaterializedViewUtils.extractStructInfo(plan, originalPlan,
-                    cascadesContext, expectedTableBitSet);
-            if (viewStructInfos.size() > 1) {
-                // view struct info should only have one, log error and use the first struct info
-                LOG.warn(String.format("view strut info is more than one, materialization plan is %s",
-                        plan.treeString()));
-            }
+            return Optional.of(StructInfo.of(plan, originalPlan, cascadesContext));
         } catch (Exception exception) {
             LOG.warn(String.format("construct materialization struct info fail, materialization plan is %s",
                     plan.treeString()), exception);
             return Optional.empty();
         }
-        return Optional.of(viewStructInfos.get(0));
     }
 
     public boolean alreadyRewrite(GroupId groupId) {
