0eb94960409374a5111a8b593568a019a96dd0ce
Socrates <suyiteng@selectdb.com>
2026-01-05 15:08:03 +0800

    Add UTC time zone setting for consistent timestamp handling in Hudi SQL script

--- docker/thirdparties/docker-compose/hudi/scripts/create_preinstalled_scripts/hudi/01_config_and_database.sql
+++ docker/thirdparties/docker-compose/hudi/scripts/create_preinstalled_scripts/hudi/01_config_and_database.sql
@@ -3,6 +3,9 @@ SET hoodie.datasource.write.hive_style_partitioning = true;
 SET hoodie.upsert.shuffle.parallelism = 2;
 SET hoodie.insert.shuffle.parallelism = 2;
 
+-- set time zone to UTC to ensure consistent timestamp handling across different machines
+SET TIME ZONE 'UTC';
+
 CREATE DATABASE IF NOT EXISTS regression_hudi;
 USE regression_hudi;
 
