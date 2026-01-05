88d157a59900b1c6f87713842486026ae4453921
zhiqiang-hhhh <seuhezhiqiang@163.com>
2026-01-05 19:46:49 +0800

    fix 1

--- be/src/olap/rowset/rowset_meta.h
+++ be/src/olap/rowset/rowset_meta.h
@@ -434,6 +434,8 @@ public:
         index_pb.set_size(size);
     }
 
+    int32_t schema_version() const { return _rowset_meta_pb.schema_version(); }
+
 private:
     bool _deserialize_from_pb(std::string_view value);
 
