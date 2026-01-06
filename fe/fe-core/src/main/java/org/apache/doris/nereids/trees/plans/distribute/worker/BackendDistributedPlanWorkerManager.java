07020cec7242d1aeb5e29e277afcb3dadac8d14b
Yongqiang YANG <dataroaring@gmail.com>
2026-01-05 17:20:10 -0800

    (log) Suppress stack traces for expected errors
    Suppress stack traces for known user-facing errors to reduce log noise:
    - No backend available as scan node errors in DistributePlanner
    - SQLSyntaxErrorException in StmtExecutionAction
    
    Add suppressStackTrace flag to NereidsException to control stack trace logging.

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/distribute/worker/BackendDistributedPlanWorkerManager.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/distribute/worker/BackendDistributedPlanWorkerManager.java
@@ -26,6 +26,7 @@ import org.apache.doris.common.Reference;
 import org.apache.doris.common.UserException;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.SimpleScheduler;
+import org.apache.doris.system.SystemInfoService;
 import org.apache.doris.resource.computegroup.ComputeGroup;
 import org.apache.doris.resource.computegroup.ComputeGroupMgr;
 import org.apache.doris.system.Backend;
@@ -155,7 +156,10 @@ public class BackendDistributedPlanWorkerManager implements DistributedPlanWorke
             Backend selctedBackend = backends.get(selectedBackendId.getRef());
             return new BackendWorker(catalogId, selctedBackend);
         } catch (Exception t) {
-            throw new NereidsException("Can not get backends: " + t, t);
+            String message = t.getMessage();
+            boolean suppressStack = message != null
+                    && message.contains(SystemInfoService.NO_SCAN_NODE_BACKEND_AVAILABLE_MSG);
+            throw new NereidsException(t, suppressStack);
         }
     }
 
