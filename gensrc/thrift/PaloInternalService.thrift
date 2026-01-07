d900dc4c42fdc035ca6754a6f77f1351379933c2
BiteTheDDDDt <xl@selectdb.com>
2026-01-07 11:51:14 +0800

    fix thrift conflict

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -419,11 +419,11 @@ struct TQueryOptions {
   180: optional i32 max_file_scanners_concurrency = 0;
   181: optional i32 min_file_scanners_concurrency = 0;
   182: optional i32 ivf_nprobe = 1;
-  183: optional i32 cte_max_recursion_depth;
-
   // Enable hybrid sorting: dynamically selects between PdqSort and TimSort based on 
   // runtime profiling to choose the most efficient algorithm for the data pattern
   183: optional bool enable_use_hybrid_sort = false;
+  184: optional i32 cte_max_recursion_depth;
+
 
   // For cloud, to control if the content would be written into file cache
   // In write path, to control if the content would be written into file cache.
