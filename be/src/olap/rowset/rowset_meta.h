903841062fa50c964acc0af12be388b64bcd5dda
zhiqiang <hezhiqiang@selectdb.com>
2026-01-08 10:10:42 +0800

    Revert "[fix](compaction) Check schema version before ordered data compaction…"
    This reverts commit 52a178a652eb03efa6461ab8addc7142f883d330.

--- be/src/olap/rowset/rowset_meta.h
+++ be/src/olap/rowset/rowset_meta.h
@@ -435,8 +435,6 @@ public:
         index_pb.set_packed_file_size(packed_file_size);
     }
 
-    int32_t schema_version() const { return _rowset_meta_pb.schema_version(); }
-
 private:
     bool _deserialize_from_pb(std::string_view value);
 
