b5223b4d52c6897c647466e487b8aae5b3326eee
englefly <zhouminghong@selectdb.com>
2026-01-14 12:51:20 +0800

    fix rf target slot NPE

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/RuntimeFilterTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/RuntimeFilterTranslator.java
@@ -129,9 +129,8 @@ public class RuntimeFilterTranslator {
             if (!src.getType().equals(targetExpr.getType()) && filter.getType() != TRuntimeFilterType.BITMAP) {
                 targetExpr = new CastExpr(src.getType(), targetExpr);
             }
-            SlotRef targetSlot = targetSlotRef.getSrcSlotRef();
-            TupleId targetTupleId = targetSlot.getDesc().getParent().getId();
-            SlotId targetSlotId = targetSlot.getSlotId();
+            TupleId targetTupleId = targetSlotRef.getDesc().getParent().getId();
+            SlotId targetSlotId = targetSlotRef.getSlotId();
             scanNodeList.add(scanNode);
             targetExprList.add(targetExpr);
             targetTupleIdMapList.add(ImmutableMap.of(targetTupleId, ImmutableList.of(targetSlotId)));
