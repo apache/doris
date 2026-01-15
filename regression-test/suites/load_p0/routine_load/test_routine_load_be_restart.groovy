9918ecad2705e7240f3429fd817d1036d47f7fe6
hui lai <laihui@selectdb.com>
2026-01-15 14:08:53 +0800

    [fix](transaction) ensure executing before/after abort when abort transaction in cloud mode (#59425)
    Ensure executing before/after abort when abort transaction in cloud
    mode, so that task can be rescheduled timely when VCG(Virtual Compute
    Group) failover.

---
+++ regression-test/suites/load_p0/routine_load/test_routine_load_be_restart.groovy
@@ -0,0 +1,98 @@
+ // Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for
+// the specific language governing permissions and limitations
+// under the License.
+
+import org.apache.doris.regression.util.RoutineLoadTestUtils
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.ProducerConfig
+
+suite("test_routine_load_be_restart","nonConcurrent") {
+    def kafkaCsvTopics = [
+                  "test_routine_load_be_restart",
+                ]
+
+    if (RoutineLoadTestUtils.isKafkaTestEnabled(context)) {
+        def runSql = { String q -> sql q }
+        def kafka_broker = RoutineLoadTestUtils.getKafkaBroker(context)
+        def producer = RoutineLoadTestUtils.createKafkaProducer(kafka_broker)
+
+        def tableName = "test_routine_load_be_restart"
+        def job = "test_routine_load_be_restart"
+        sql """ DROP TABLE IF EXISTS ${tableName} """
+        sql """
+            CREATE TABLE IF NOT EXISTS ${tableName} (
+                `k1` int(20) NULL,
+                `k2` string NULL,
+                `v1` date  NULL,
+                `v2` string  NULL,
+                `v3` datetime  NULL,
+                `v4` string  NULL
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`k1`)
+            COMMENT 'OLAP'
+            DISTRIBUTED BY HASH(`k1`) BUCKETS 3
+            PROPERTIES ("replication_allocation" = "tag.location.default: 1");
+        """
+
+        try {
+            sql """
+                CREATE ROUTINE LOAD ${job} ON ${tableName}
+                COLUMNS TERMINATED BY ","
+                PROPERTIES
+                (
+                    "max_batch_interval" = "10"
+                )
+                FROM KAFKA
+                (
+                    "kafka_broker_list" = "${kafka_broker}",
+                    "kafka_topic" = "${kafkaCsvTopics[0]}",
+                    "property.kafka_default_offsets" = "OFFSET_BEGINNING"
+                );
+            """
+
+            def injection_load_hang = "load.commit_timeout"
+            def injection_abort_txn = "FE.abortTxnWhenCoordinateBeRestart"
+            // test coordinator be restart
+            try {
+                GetDebugPoint().enableDebugPointForAllFEs(injection_load_hang)
+                RoutineLoadTestUtils.sendTestDataToKafka(producer, kafkaCsvTopics)
+                GetDebugPoint().enableDebugPointForAllFEs(injection_abort_txn)
+                RoutineLoadTestUtils.waitForTaskAbort(runSql, job, 60)
+            } finally {
+                GetDebugPoint().disableDebugPointForAllFEs(injection_abort_txn)
+                GetDebugPoint().disableDebugPointForAllFEs(injection_load_hang)
+            }
+
+            // test coordinator be down
+            def injection_abort_txn_be_down = "HeartbeatMgr.abortTxnWhenCoordinateBeDown"
+            try {
+                GetDebugPoint().enableDebugPointForAllFEs(injection_load_hang)
+                RoutineLoadTestUtils.sendTestDataToKafka(producer, kafkaCsvTopics)
+                GetDebugPoint().enableDebugPointForAllFEs(injection_abort_txn_be_down)
+                RoutineLoadTestUtils.waitForTaskAbort(runSql, job, 60, 2)
+            } finally {
+                GetDebugPoint().disableDebugPointForAllFEs(injection_abort_txn_be_down)
+                GetDebugPoint().disableDebugPointForAllFEs(injection_load_hang)
+            }
+            def count = RoutineLoadTestUtils.waitForTaskFinish(runSql, job, tableName, 0)
+            logger.info("wait count: " + count)
+        } finally {
+            sql "stop routine load for ${job}"
+        }
+    }
+}
