apache/doris

commit 0031179b1e67eca92a916ff4ecb576817902543e

[fix](parquet)fix parquet topn lazy mat complex data error result (#58785)
### What problem does this PR solve?
Related PR: #51329
Problem Summary:
This PR primarily enables the Parquet reader to use page indexes when
reading complex columns, and also fixes a data reading error in PR
#51329 when materializing data based on row number in the second stage
of topn.

be/src/vec/exec/format/parquet/vparquet_column_chunk_reader.h
@@ -34,9 +34,6 @@
 #include "vec/exec/format/parquet/parquet_common.h"
 #include "vparquet_page_reader.h"
 
-namespace cctz {
-class time_zone;
-} // namespace cctz
 namespace doris {
 class BlockCompressionCodec;
 
@@ -54,9 +51,29 @@ template <typename T>
 class ColumnStr;
 using ColumnString = ColumnStr<UInt32>;
 
+struct ColumnChunkReaderStatistics {
+    int64_t decompress_time = 0;
+    int64_t decompress_cnt = 0;
+    int64_t decode_header_time = 0;
+    int64_t decode_value_time = 0;
+    int64_t decode_dict_time = 0;
+    int64_t decode_level_time = 0;
+    int64_t skip_page_header_num = 0;
+    int64_t parse_page_header_num = 0;
+};
+
 /**
  * Read and decode parquet column data into doris block column.
- * <p>Usage:</p>
+ * <p>Usage:</p>    struct ColumnChunkReaderStatistics {
+        int64_t decompress_time = 0;
+        int64_t decompress_cnt = 0;
+        int64_t decode_header_time = 0;
+        int64_t decode_value_time = 0;
+        int64_t decode_dict_time = 0;
+        int64_t decode_level_time = 0;
+        int64_t skip_page_header_num = 0;
+        int64_t parse_page_header_num = 0;
+    };
  * // Create chunk reader
  * ColumnChunkReader chunk_reader(BufferedStreamReader* reader,
  *                                tparquet::ColumnChunk* column_chunk,
@@ -74,58 +91,27 @@ using ColumnString = ColumnStr<UInt32>;
  *   chunk_reader.decode_values(slice, num_values);
  * }
  */
+template <bool IN_COLLECTION, bool OFFSET_INDEX>
 class ColumnChunkReader {
 public:
-    struct Statistics {
-        int64_t decompress_time = 0;
-        int64_t decompress_cnt = 0;
-        int64_t decode_header_time = 0;
-        int64_t decode_value_time = 0;
-        int64_t decode_dict_time = 0;
-        int64_t decode_level_time = 0;
-        int64_t skip_page_header_num = 0;
-        int64_t parse_page_header_num = 0;
-    };
-
     ColumnChunkReader(io::BufferedStreamReader* reader, tparquet::ColumnChunk* column_chunk,
                       FieldSchema* field_schema, const tparquet::OffsetIndex* offset_index,
-                      const cctz::time_zone* ctz, io::IOContext* io_ctx);
+                      size_t total_row, io::IOContext* io_ctx);
     ~ColumnChunkReader() = default;
 
     // Initialize chunk reader, will generate the decoder and codec.
     Status init();
 
     // Whether the chunk reader has a more page to read.
-    bool has_next_page() const { return _chunk_parsed_values < _metadata.num_values; }
-
-    // Deprecated
-    // Seek to the specific page, page_header_offset must be the start offset of the page header.
-    // _end_offset may exceed the actual data area, so we can only use the number of parsed values
-    // to determine whether there are remaining pages to read. That's to say we can't use the
-    // PageLocation in parquet metadata to seek to the specified page. We should call next_page()
-    // and skip_page() to skip pages one by one.
-    // todo: change this interface to seek_to_page(int64_t page_header_offset, size_t num_parsed_values)
-    // and set _chunk_parsed_values = num_parsed_values
-    // [[deprecated]]
-    void seek_to_page(int64_t page_header_offset) {
-        _remaining_num_values = 0;
-        _page_reader->seek_to_page(page_header_offset);
-        _state = INITIALIZED;
-    }
-
-    // Seek to next page. Only read and parse the page header.
-    Status next_page();
-
-    // Skip current page(will not read and parse) if the page is filtered by predicates.
-    Status skip_page() {
-        Status res = Status::OK();
-        _remaining_num_values = 0;
-        if (_state == HEADER_PARSED) {
-            res = _page_reader->skip_page();
+    bool has_next_page() const {
+        if constexpr (OFFSET_INDEX) {
+            return _page_reader->has_next_page();
+        } else {
+            // no offset need parse all page header.
+            return _chunk_parsed_values < _metadata.num_values;
         }
-        _state = PAGE_SKIPPED;
-        return res;
     }
+
     // Skip some values(will not read and parse) in current page if the values are filtered by predicates.
     // when skip_data = false, the underlying decoder will not skip data,
     // only used when maintaining the consistency of _remaining_num_values.
@@ -142,16 +128,6 @@ public:
     }
     // The remaining number of values in current page(including null values). Decreased when reading or skipping.
     uint32_t remaining_num_values() const { return _remaining_num_values; }
-    // null values are generated from definition levels
-    // the caller should maintain the consistency after analyzing null values from definition levels.
-    void insert_null_values(MutableColumnPtr& doris_column, size_t num_values);
-    // Get the raw data of current page.
-    Slice& get_page_data() { return _page_data; }
-
-    // Get the repetition levels
-    size_t get_rep_levels(level_t* levels, size_t n);
-    // Get the definition levels
-    size_t get_def_levels(level_t* levels, size_t n);
 
     // Decode values in current page into doris column.
     Status decode_values(MutableColumnPtr& doris_column, DataTypePtr& data_type,
@@ -170,7 +146,7 @@ public:
     // Get page decoder
     Decoder* get_page_decoder() { return _page_decoder; }
 
-    Statistics& statistics() {
+    ColumnChunkReaderStatistics& statistics() {
         _statistics.decode_header_time = _page_reader->statistics().decode_header_time;
         _statistics.skip_page_header_num = _page_reader->statistics().skip_page_header_num;
         _statistics.parse_page_header_num = _page_reader->statistics().parse_page_header_num;
@@ -187,33 +163,70 @@ public:
                 ->convert_dict_column_to_string_column(dict_column);
     }
 
+    size_t page_start_row() const { return _page_reader->start_row(); }
+
+    size_t page_end_row() const { return _page_reader->end_row(); }
+
+    Status parse_page_header();
+    Status next_page();
+
+    Status seek_to_nested_row(size_t left_row);
+    Status skip_nested_values(const std::vector<level_t>& def_levels);
+    Status fill_def(std::vector<level_t>& def_values) {
+        auto before_sz = def_values.size();
+        auto append_sz = _remaining_def_nums - _remaining_rep_nums;
+        def_values.resize(before_sz + append_sz, 0);
+        if (max_def_level() != 0) {
+            auto ptr = def_values.data() + before_sz;
+            _def_level_decoder.get_levels(ptr, append_sz);
+        }
+        _remaining_def_nums -= append_sz;
+        return Status::OK();
+    }
+
+    Status load_page_nested_rows(std::vector<level_t>& rep_levels, size_t max_rows,
+                                 size_t* result_rows, bool* cross_page);
+    Status load_cross_page_nested_row(std::vector<level_t>& rep_levels, bool* cross_page);
+
 private:
     enum ColumnChunkReaderState { NOT_INIT, INITIALIZED, HEADER_PARSED, DATA_LOADED, PAGE_SKIPPED };
 
+    // for check dict page.
+    Status _parse_first_page_header();
     Status _decode_dict_page();
+
     void _reserve_decompress_buf(size_t size);
     int32_t _get_type_length();
+
     void _get_uncompressed_levels(const tparquet::DataPageHeaderV2& page_v2, Slice& page_data);
+    Status _skip_nested_rows_in_page(size_t num_rows);
 
     ColumnChunkReaderState _state = NOT_INIT;
     FieldSchema* _field_schema = nullptr;
-    level_t _max_rep_level;
-    level_t _max_def_level;
-    tparquet::LogicalType _parquet_logical_type;
+    const level_t _max_rep_level;
+    const level_t _max_def_level;
 
     io::BufferedStreamReader* _stream_reader = nullptr;
     tparquet::ColumnMetaData _metadata;
-    const tparquet::OffsetIndex* _offset_index;
-    //    cctz::time_zone* _ctz;
+    const tparquet::OffsetIndex* _offset_index = nullptr;
+    size_t _current_row = 0;
+    size_t _total_rows = 0;
     io::IOContext* _io_ctx = nullptr;
 
-    std::unique_ptr<PageReader> _page_reader;
+    std::unique_ptr<PageReader<IN_COLLECTION, OFFSET_INDEX>> _page_reader;
     BlockCompressionCodec* _block_compress_codec = nullptr;
 
     LevelDecoder _rep_level_decoder;
     LevelDecoder _def_level_decoder;
     size_t _chunk_parsed_values = 0;
+    // this page remaining rep/def nums
+    // if max_rep_level = 0 / max_def_level = 0, this value retail hava value.
+    uint32_t _remaining_rep_nums = 0;
+    uint32_t _remaining_def_nums = 0;
+    // this page remaining values to be processed (for read/skip).
+    // need parse this page header.
     uint32_t _remaining_num_values = 0;
+
     Slice _page_data;
     DorisUniqueBufferPtr<uint8_t> _decompress_buf;
     size_t _decompress_buf_size = 0;
@@ -225,7 +238,7 @@ private:
     // Map: encoding -> Decoder
     // Plain or Dictionary encoding. If the dictionary grows too big, the encoding will fall back to the plain encoding
     std::unordered_map<int, std::unique_ptr<Decoder>> _decoders;
-    Statistics _statistics;
+    ColumnChunkReaderStatistics _statistics;
 };
 #include "common/compile_check_end.h"
 

commit 52c75e7919725e567b099dffb90fcd1487ca6259

[fix](parqeut)Fixed a performance fallback caused by filling RL DL when reading Parquet scalar columns. (#59833)
### What problem does this PR solve?
Related PR: #58785

Problem Summary:
The performance rollback was introduced in #58785. The reason is that,
prior to #58785, reading ordinary columns did not require filling and
saving the RL DL. #58785 combined the logic for reading ordinary columns
from a struct with the logic for reading ordinary columns, and filled
and saved the RL DL to populate the null map information of the struct.
This PR re-separates the reading logic.

be/src/vec/exec/format/parquet/vparquet_column_chunk_reader.cpp
@@ -361,7 +361,7 @@ Status ColumnChunkReader<IN_COLLECTION, OFFSET_INDEX>::seek_to_nested_row(size_t
     } else {
         while (true) {
             RETURN_IF_ERROR(parse_page_header());
-            if (_page_reader->is_header_v2()) {
+            if (_page_reader->is_header_v2() || !IN_COLLECTION) {
                 if (_page_reader->start_row() <= left_row && left_row < _page_reader->end_row()) {
                     RETURN_IF_ERROR(load_page_data());
                     // this page contain this row.
@@ -447,11 +447,11 @@ Status ColumnChunkReader<IN_COLLECTION, OFFSET_INDEX>::load_page_nested_rows(
     *result_rows = 0;
     rep_levels.reserve(rep_levels.size() + _remaining_rep_nums);
     while (_remaining_rep_nums) {
-        level_t rep_level = _rep_level_decoder.get_next();
+        level_t rep_level = _rep_level_get_next();
         if (rep_level == 0) {               // rep_level 0 indicates start of new row
             if (*result_rows == max_rows) { // this page contain max_rows, page no end.
                 _current_row += max_rows;
-                _rep_level_decoder.rewind_one();
+                _rep_level_rewind_one();
                 return Status::OK();
             }
             (*result_rows)++;
@@ -462,8 +462,8 @@ Status ColumnChunkReader<IN_COLLECTION, OFFSET_INDEX>::load_page_nested_rows(
     _current_row += *result_rows;
 
     auto need_check_cross_page = [&]() -> bool {
-        return !OFFSET_INDEX && _remaining_rep_nums == 0 && !_page_reader->is_header_v2() &&
-               has_next_page();
+        return !OFFSET_INDEX && IN_COLLECTION && _remaining_rep_nums == 0 &&
+               !_page_reader->is_header_v2() && has_next_page();
     };
     *cross_page = need_check_cross_page();
     return Status::OK();
@@ -478,10 +478,10 @@ Status ColumnChunkReader<IN_COLLECTION, OFFSET_INDEX>::load_cross_page_nested_ro
 
     *cross_page = has_next_page();
     while (_remaining_rep_nums) {
-        level_t rep_level = _rep_level_decoder.get_next();
+        level_t rep_level = _rep_level_get_next();
         if (rep_level == 0) { // rep_level 0 indicates start of new row
             *cross_page = false;
-            _rep_level_decoder.rewind_one();
+            _rep_level_rewind_one();
             break;
         }
         _remaining_rep_nums--;

be/src/vec/exec/format/parquet/vparquet_column_chunk_reader.h
@@ -206,6 +206,19 @@ private:
     void _get_uncompressed_levels(const tparquet::DataPageHeaderV2& page_v2, Slice& page_data);
     Status _skip_nested_rows_in_page(size_t num_rows);
 
+    level_t _rep_level_get_next() {
+        if constexpr (IN_COLLECTION) {
+            return _rep_level_decoder.get_next();
+        }
+        return 0;
+    }
+
+    void _rep_level_rewind_one() {
+        if constexpr (IN_COLLECTION) {
+            _rep_level_decoder.rewind_one();
+        }
+    }
+
     ColumnChunkReaderState _state = NOT_INIT;
     FieldSchema* _field_schema = nullptr;
     const level_t _max_rep_level;

be/src/vec/exec/format/parquet/vparquet_column_reader.cpp
@@ -118,8 +118,8 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
         RETURN_IF_ERROR(create(file, &field->children[0], row_group, row_ranges, ctz, io_ctx,
                                element_reader, max_buf_size, col_offsets, true, column_ids,
                                filter_column_ids));
-        //        element_reader->set_nested_column();
         auto array_reader = ArrayColumnReader::create_unique(row_ranges, total_rows, ctz, io_ctx);
+        element_reader->set_column_in_nested();
         RETURN_IF_ERROR(array_reader->init(std::move(element_reader), field));
         array_reader->_filter_column_ids = filter_column_ids;
         reader.reset(array_reader.release());
@@ -133,7 +133,6 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
             RETURN_IF_ERROR(create(file, &field->children[0], row_group, row_ranges, ctz, io_ctx,
                                    key_reader, max_buf_size, col_offsets, true, column_ids,
                                    filter_column_ids));
-            //            key_reader->set_nested_column();
         } else {
             auto skip_reader = std::make_unique<SkipReadingReader>(row_ranges, total_rows, ctz,
                                                                    io_ctx, &field->children[0]);
@@ -146,7 +145,6 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
             RETURN_IF_ERROR(create(file, &field->children[1], row_group, row_ranges, ctz, io_ctx,
                                    value_reader, max_buf_size, col_offsets, true, column_ids,
                                    filter_column_ids));
-            //            value_reader->set_nested_column();
         } else {
             auto skip_reader = std::make_unique<SkipReadingReader>(row_ranges, total_rows, ctz,
                                                                    io_ctx, &field->children[0]);
@@ -154,6 +152,8 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
         }
 
         auto map_reader = MapColumnReader::create_unique(row_ranges, total_rows, ctz, io_ctx);
+        key_reader->set_column_in_nested();
+        value_reader->set_column_in_nested();
         RETURN_IF_ERROR(map_reader->init(std::move(key_reader), std::move(value_reader), field));
         map_reader->_filter_column_ids = filter_column_ids;
         reader.reset(map_reader.release());
@@ -168,7 +168,6 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
                 RETURN_IF_ERROR(create(file, &child, row_group, row_ranges, ctz, io_ctx,
                                        child_reader, max_buf_size, col_offsets, in_collection,
                                        column_ids, filter_column_ids));
-                //                child_reader->set_nested_column();
                 child_readers[child.name] = std::move(child_reader);
                 // Record the first non-SkippingReader
                 if (non_skip_reader_idx == -1) {
@@ -180,6 +179,7 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
                 skip_reader->_filter_column_ids = filter_column_ids;
                 child_readers[child.name] = std::move(skip_reader);
             }
+            child_readers[child.name]->set_column_in_nested();
         }
         // If all children are SkipReadingReader, force the first child to call create
         if (non_skip_reader_idx == -1) {
@@ -187,7 +187,7 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
             RETURN_IF_ERROR(create(file, &field->children[0], row_group, row_ranges, ctz, io_ctx,
                                    child_reader, max_buf_size, col_offsets, in_collection,
                                    column_ids, filter_column_ids));
-            //            child_reader->set_nested_column();
+            child_reader->set_column_in_nested();
             child_readers[field->children[0].name] = std::move(child_reader);
         }
         auto struct_reader = StructColumnReader::create_unique(row_ranges, total_rows, ctz, io_ctx);
@@ -201,8 +201,6 @@ Status ParquetColumnReader::create(io::FileReaderSPtr file, FieldSchema* field,
                                                                       : nullptr;
 
         const tparquet::ColumnChunk& chunk = row_group.columns[physical_index];
-
-        //                ScalarColumnReader::create_unique(row_ranges, total_rows, chunk, offset_index, ctz, io_ctx);
         if (in_collection) {
             if (offset_index == nullptr) {
                 auto scalar_reader = ScalarColumnReader<true, false>::create_unique(
@@ -354,9 +352,6 @@ Status ScalarColumnReader<IN_COLLECTION, OFFSET_INDEX>::_read_values(size_t num_
                     return Status::InternalError("Failed to decode definition level.");
                 }
 
-                for (int i = 0; i < loop_read; i++) {
-                    _def_levels.emplace_back(def_level);
-                }
                 bool is_null = def_level < _field_schema->definition_level;
                 if (!(prev_is_null ^ is_null)) {
                     null_map.emplace_back(0);
@@ -371,14 +366,11 @@ Status ScalarColumnReader<IN_COLLECTION, OFFSET_INDEX>::_read_values(size_t num_
                 prev_is_null = is_null;
                 has_read += loop_read;
             }
-        } else {
-            _def_levels.resize(_def_levels.size() + num_values, 0);
         }
     } else {
         if (_chunk_reader->max_def_level() > 0) {
             return Status::Corruption("Not nullable column has null values in parquet file");
         }
-        _def_levels.resize(_def_levels.size() + num_values, 0);
         data_column = doris_column->assume_mutable();
     }
     if (null_map.size() == 0) {
@@ -560,7 +552,7 @@ Status ScalarColumnReader<IN_COLLECTION, OFFSET_INDEX>::read_column_data(
     _rep_levels.clear();
     *read_rows = 0;
 
-    if constexpr (IN_COLLECTION) {
+    if (_in_nested) {
         RETURN_IF_ERROR(_read_nested_column(resolved_column, resolved_type, filter_map, batch_size,
                                             read_rows, eof, is_dict_filter));
         return _converter->convert(resolved_column, _field_schema->data_type, type, doris_column,
@@ -574,7 +566,6 @@ Status ScalarColumnReader<IN_COLLECTION, OFFSET_INDEX>::read_column_data(
     } else {
         right_row = _chunk_reader->page_end_row();
     }
-    auto before_filter_map_index = _filter_map_index;
 
     do {
         // generate the row ranges that should be read
@@ -640,18 +631,6 @@ Status ScalarColumnReader<IN_COLLECTION, OFFSET_INDEX>::read_column_data(
             RETURN_IF_ERROR(_chunk_reader->next_page());
         }
     }
-
-    if (filter_map.has_filter()) {
-        size_t new_rep_sz = 0;
-        for (size_t idx = before_filter_map_index; idx < _filter_map_index; idx++) {
-            if (filter_map.filter_map_data()[idx]) {
-                _def_levels[new_rep_sz] = _def_levels[idx - before_filter_map_index];
-                new_rep_sz++;
-            }
-        }
-        _def_levels.resize(new_rep_sz);
-    }
-    _rep_levels.resize(_def_levels.size(), 0);
 
     return _converter->convert(resolved_column, _field_schema->data_type, type, doris_column,
                                is_dict_filter);

be/src/vec/exec/format/parquet/vparquet_column_reader.h
@@ -141,6 +141,7 @@ public:
     virtual void reset_filter_map_index() = 0;
 
     FieldSchema* get_field_schema() const { return _field_schema; }
+    void set_column_in_nested() { _in_nested = true; }
 
 protected:
     void _generate_read_ranges(RowRange page_row_range, RowRanges* result_ranges) const;
@@ -155,6 +156,10 @@ protected:
 
     size_t _filter_map_index = 0;
     std::set<uint64_t> _filter_column_ids;
+
+    // _in_nested: column in struct/map/array
+    // IN_COLLECTION : column in map/array
+    bool _in_nested = false;
 };
 
 template <bool IN_COLLECTION, bool OFFSET_INDEX>