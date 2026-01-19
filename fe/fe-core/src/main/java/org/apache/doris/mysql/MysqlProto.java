0a7f5a5999be889926e8b21496d2fcf23b1e6c07
Gavin Chou <gavin@selectdb.com>
2026-01-19 20:58:21 +0800

    [fix](tls) revert #5548 to keep consistenty with mysql server when --ssl-mode=DISABLED

--- fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlProto.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlProto.java
@@ -165,13 +165,6 @@ public class MysqlProto {
                 handshakeResponse = clientRequestPacket;
             }
         } else {
-            // Client doesn't request SSL
-            if (Config.enable_tls && CertificateManager.isProtocolIncluded(CertificateManager.Protocol.mysql)) {
-                // Server requires TLS but client doesn't use it, reject the connection
-                ErrorReport.report(ErrorCode.ERR_NOT_SUPPORTED_AUTH_MODE, "Server requires TLS connection");
-                sendResponsePacket(context);
-                return false;
-            }
             handshakeResponse = clientRequestPacket;
         }
 
