3cef52e0a7059e0ebe34631ab574a6786b2574a4
catpineapple <catpineapple1122@gmail.com>
2026-01-20 17:07:15 +0800

    [fix](nereids) show columns type hide Column Version (#59684)
    ### What problem does this PR solve?
    
    This update fixes an issue where the `SHOW FULL COLUMNS` statement still
    displayed `version` for `date`, `datetime`, and `decimal` data types.
    This change aims for better compatibility with MySQL behavior and BI
    applications.
    
    ```txt
    MySQL [(none)]> SHOW FULL COLUMNS FROM `doris_all_types_demo` FROM `tpch`;
    +---------------------------+---------------+-----------+------+------+---------+-------+------------+-----------------------------------------------------+
    | Field                     | Type          | Collation | Null | Key  | Default | Extra | Privileges | Comment                                             |
    +---------------------------+---------------+-----------+------+------+---------+-------+------------+-----------------------------------------------------+
    | col_09_decimal            | decimal(10,2) |           | YES  | NO   | NULL    | NONE  |            | DECIMAL: exact decimal (precision, scale)           |
    | col_14_date               | date          |           | YES  | NO   | NULL    | NONE  |            | DATE: date format YYYY-MM-DD                        |
    | col_15_datetime           | datetime      |           | YES  | NO   | NULL    | NONE  |            | DATETIME: datetime format YYYY-MM-DD HH:MM:SS       |
    | col_16_datetime_precision | datetime(3)   |           | YES  | NO   | NULL    | NONE  |            | DATETIME(3): datetime with millisecond precision    |
    +---------------------------+---------------+-----------+------+------+---------+-------+------------+-----------------------------------------------------+
    ```
    
    Related PR: #xxx
    
    Problem Summary:
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test <!-- At least one of them must be included. -->
        - [x] Regression test
        - [ ] Unit Test
        - [ ] Manual test (add detailed scripts or steps below)
        - [ ] No need to test or manual test. Explain why:
    - [ ] This is a refactor/code format and no logic has been changed.
            - [ ] Previous test can cover this change.
            - [ ] No code files have been changed.
            - [ ] Other reason <!-- Add your reason?  -->
    
    - Behavior changed:
        - [ ] No.
        - [x] Yes. <!-- Explain the behavior change -->
    
    - Does this need documentation?
        - [x] No.
    - [ ] Yes. <!-- Add document PR link here. eg:
    https://github.com/apache/doris-website/pull/1214 -->
    
    ### Check List (For Reviewer who merge this PR)
    
    - [ ] Confirm the release note
    - [ ] Confirm test cases
    - [ ] Confirm document
    - [ ] Add branch pick label <!-- Add branch pick label that this PR
    should merge into -->

--- fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcMySQLClient.java
+++ fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcMySQLClient.java
@@ -422,7 +422,7 @@ public class JdbcMySQLClient extends JdbcClient {
                 return ScalarType.createDateV2Type();
             case "DATETIME":
             case "DATETIMEV2": {
-                int scale = (openParen == -1) ? 6
+                int scale = (openParen == -1) ? 0
                         : Integer.parseInt(upperType.substring(openParen + 1, upperType.length() - 1));
                 if (scale > 6) {
                     scale = 6;
