212d9947d094de653994b3fab3d12269535d2ab3
koarz <66543806+koarz@users.noreply.github.com>
2026-01-24 15:30:08 +0800

    Remove affectQueryResult from cloudPartitionsTableUseCachedVisibleVersion
    Remove affectQueryResult attribute from cloudPartitionsTableUseCachedVisibleVersion variable.

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -3262,7 +3262,6 @@ public class SessionVariable implements Serializable, Writable {
     public int defaultVariantSparseHashShardCount = 0;
 
     @VariableMgr.VarAttr(name = CLOUD_PARTITIONS_TABLE_USE_CACHED_VISIBLE_VERSION, needForward = false,
-            affectQueryResult = true,
             description = {"partitions系统表的visible_version列在cloud模式是否使用cached",
                     "Whether cache is used for the visible_version column"
                              + "in the partitions system table on cloud mode"})
