bf46530d29676404ef8c1f91b438c778f1c09af3
tommyhugh2333-rgb <tommyhugh2333@gmail.com>
2026-01-26 03:12:33 +0000

    Update: sync regression test data with BE UT

--- regression-test/suites/query_p0/sql_functions/math_functions/test_gamma.groovy
+++ regression-test/suites/query_p0/sql_functions/math_functions/test_gamma.groovy
@@ -16,18 +16,21 @@
 // under the License.
 
 suite("test_gamma") {
-    
-
+    // Test with constant folding enabled
     sql "set debug_skip_fold_constant=false"
-    qt_select_const "select gamma(1), gamma(2), gamma(3)"
+    qt_select_const "select gamma(5), gamma(0.5), gamma(1)"
     qt_select_null "select gamma(null)"
+    qt_select_zero "select gamma(0)"
+    qt_select_neg "select gamma(-1)"
 
-    
+    // Test with constant folding disabled
     sql "set debug_skip_fold_constant=true"
-    qt_select_const_no_fold "select gamma(1), gamma(2), gamma(3)"
+    qt_select_const_no_fold "select gamma(5), gamma(0.5), gamma(1)"
     qt_select_null_no_fold "select gamma(null)"
+    qt_select_zero_no_fold "select gamma(0)"
+    qt_select_neg_no_fold "select gamma(-1)"
 
-    
+    // Test with table data
     sql "drop table if exists test_gamma_tbl"
     sql """
         create table test_gamma_tbl (
@@ -36,7 +39,6 @@ suite("test_gamma") {
         ) distributed by hash(k1) properties("replication_num" = "1");
     """
 
-    
     sql """
         insert into test_gamma_tbl values 
         (1, 1.0),
