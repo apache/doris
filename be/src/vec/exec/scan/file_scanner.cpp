343d66b88a413558cbe33a782c819ddc1fca6774
daidai <changyuwei@selectdb.com>
2026-01-26 11:27:46 +0800

    [fix](parquet)fix parquet reader cannot push down conjuncts for min-max filter (#60197)
    ### What problem does this PR solve?
    Problem Summary:
    1. Fixed an issue where the Parquet reader's pushdown predicate row
    group min-max filter was ineffective due to a refactoring of the column
    predicate #59187.
    
    2. Removed tablet_schema from RuntimePredicate when performing topn
    runtime filters, making RuntimePredicate not limited to olap scan node.
    
    3. Removed the feature in #59053 where the Parquet reader could obtain
    runtime filters in real time, for the purpose of unifying the
    implementation of olap and file scan node logic in the future.
    
    4. Temporarily disable the topn runtime filter for VARBINARY type, as
    the implementation of this type in column predicate is incomplete,
    affecting pr #58721.

--- be/src/vec/exec/scan/file_scanner.cpp
+++ be/src/vec/exec/scan/file_scanner.cpp
@@ -1039,16 +1039,6 @@ Status FileScanner::_get_next_reader() {
             // see IcebergTableReader::init_row_filters for example.
             parquet_reader->set_push_down_agg_type(_get_push_down_agg_type());
 
-            std::function<Status(bool*, VExprContextSPtrs&)> update_late_rf =
-                    [&](bool* changed, VExprContextSPtrs& new_push_down_conjuncts) -> Status {
-                if (!_is_load) {
-                    RETURN_IF_ERROR(try_append_late_arrival_runtime_filter());
-                    RETURN_IF_ERROR(
-                            _process_late_arrival_conjuncts(changed, new_push_down_conjuncts));
-                }
-                return Status::OK();
-            };
-            parquet_reader->set_update_late_rf_func(std::move(update_late_rf));
             RETURN_IF_ERROR(_init_parquet_reader(std::move(parquet_reader), file_meta_cache_ptr));
 
             need_to_get_parsed_schema = true;
