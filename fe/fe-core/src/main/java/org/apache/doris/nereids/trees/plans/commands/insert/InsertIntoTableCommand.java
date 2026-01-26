apache/doris

commit 08cf571b4376043d150deeab2c4b859be900c495

[opt](Nereids) lock table in ascending order of table IDs (#45045)
### What problem does this PR solve?

Problem Summary:

Doris's table locks are fair read-write locks. If two threads acquire
read locks on tables in different orders and simultaneously a third
thread attempts to acquire a write lock on one of these tables, a
deadlock can form between the two threads trying to acquire read locks.
This PR changes the lock acquisition order for queries to follow the
order of table IDs, ensuring that the lock acquisition order for tables
is consistent among different threads.


### Release note

Execute table locking operations in ascending order of table IDs

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertIntoTableCommand.java
@@ -25,6 +25,7 @@ import org.apache.doris.catalog.TableIf;
 import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.profile.ProfileManager.ProfileType;
+import org.apache.doris.common.util.DebugUtil;
 import org.apache.doris.datasource.hive.HMSExternalTable;
 import org.apache.doris.datasource.iceberg.IcebergExternalTable;
 import org.apache.doris.datasource.jdbc.JdbcExternalTable;
@@ -51,6 +52,7 @@ import org.apache.doris.nereids.trees.plans.physical.PhysicalOneRowRelation;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalSink;
 import org.apache.doris.nereids.trees.plans.physical.PhysicalUnion;
 import org.apache.doris.nereids.trees.plans.visitor.PlanVisitor;
+import org.apache.doris.nereids.util.RelationUtil;
 import org.apache.doris.planner.DataSink;
 import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.qe.ConnectContext.ConnectType;
@@ -83,13 +85,14 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
 
     public static final Logger LOG = LogManager.getLogger(InsertIntoTableCommand.class);
 
+    private LogicalPlan originalLogicalQuery;
     private LogicalPlan logicalQuery;
     private Optional<String> labelName;
     /**
      * When source it's from job scheduler,it will be set.
      */
     private long jobId;
-    private Optional<InsertCommandContext> insertCtx;
+    private final Optional<InsertCommandContext> insertCtx;
     private final Optional<LogicalPlan> cte;
 
     /**
@@ -98,7 +101,8 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
     public InsertIntoTableCommand(LogicalPlan logicalQuery, Optional<String> labelName,
                                   Optional<InsertCommandContext> insertCtx, Optional<LogicalPlan> cte) {
         super(PlanType.INSERT_INTO_TABLE_COMMAND);
-        this.logicalQuery = Objects.requireNonNull(logicalQuery, "logicalQuery should not be null");
+        this.originalLogicalQuery = Objects.requireNonNull(logicalQuery, "logicalQuery should not be null");
+        this.logicalQuery = originalLogicalQuery;
         this.labelName = Objects.requireNonNull(labelName, "labelName should not be null");
         this.insertCtx = insertCtx;
         this.cte = cte;
@@ -145,62 +149,95 @@ public class InsertIntoTableCommand extends Command implements ForwardWithSync, 
      */
     public AbstractInsertExecutor initPlan(ConnectContext ctx, StmtExecutor stmtExecutor,
                                            boolean needBeginTransaction) throws Exception {
-        TableIf targetTableIf = InsertUtils.getTargetTable(logicalQuery, ctx);
-        // check auth
-        if (!Env.getCurrentEnv().getAccessManager()
-                .checkTblPriv(ConnectContext.get(), targetTableIf.getDatabase().getCatalog().getName(),
-                        targetTableIf.getDatabase().getFullName(), targetTableIf.getName(),
-                        PrivPredicate.LOAD)) {
-            ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "LOAD",
-                    ConnectContext.get().getQualifiedUser(), ConnectContext.get().getRemoteIP(),
-                    targetTableIf.getDatabase().getFullName() + "." + targetTableIf.getName());
-        }
+        List<String> qualifiedTargetTableName = InsertUtils.getTargetTableQualified(logicalQuery, ctx);
 
-        AbstractInsertExecutor insertExecutor = null;
-        // should lock target table until we begin transaction.
+        AbstractInsertExecutor insertExecutor;
+        int retryTimes = 0;
+        while (++retryTimes < Math.max(ctx.getSessionVariable().dmlPlanRetryTimes, 3)) {
+            TableIf targetTableIf = RelationUtil.getTable(qualifiedTargetTableName, ctx.getEnv());
+            // check auth
+            if (!Env.getCurrentEnv().getAccessManager()
+                    .checkTblPriv(ConnectContext.get(), targetTableIf.getDatabase().getCatalog().getName(),
+                            targetTableIf.getDatabase().getFullName(), targetTableIf.getName(),
+                            PrivPredicate.LOAD)) {
+                ErrorReport.reportAnalysisException(ErrorCode.ERR_TABLEACCESS_DENIED_ERROR, "LOAD",
+                        ConnectContext.get().getQualifiedUser(), ConnectContext.get().getRemoteIP(),
+                        targetTableIf.getDatabase().getFullName() + "." + targetTableIf.getName());
+            }
+            BuildInsertExecutorResult buildResult;
+            try {
+                buildResult = initPlanOnce(ctx, stmtExecutor, targetTableIf);
+            } catch (Throwable e) {
+                Throwables.throwIfInstanceOf(e, RuntimeException.class);
+                throw new IllegalStateException(e.getMessage(), e);
+            }
+            insertExecutor = buildResult.executor;
+            if (!needBeginTransaction) {
+                return insertExecutor;
+            }
+
+            // lock after plan and check does table's schema changed to ensure we lock table order by id.
+            TableIf newestTargetTableIf = RelationUtil.getTable(qualifiedTargetTableName, ctx.getEnv());
+            newestTargetTableIf.readLock();
+            try {
+                if (targetTableIf.getId() != newestTargetTableIf.getId()) {
+                    LOG.warn("insert plan failed {} times. query id is {}. table id changed from {} to {}",
+                            retryTimes, DebugUtil.printId(ctx.queryId()),
+                            targetTableIf.getId(), newestTargetTableIf.getId());
+                    continue;
+                }
+                if (!targetTableIf.getFullSchema().equals(newestTargetTableIf.getFullSchema())) {
+                    LOG.warn("insert plan failed {} times. query id is {}. table schema changed from {} to {}",
+                            retryTimes, DebugUtil.printId(ctx.queryId()),
+                            targetTableIf.getFullSchema(), newestTargetTableIf.getFullSchema());
+                    continue;
+                }
+                if (!insertExecutor.isEmptyInsert()) {
+                    insertExecutor.beginTransaction();
+                    insertExecutor.finalizeSink(
+                            buildResult.planner.getFragments().get(0), buildResult.dataSink,
+                            buildResult.physicalSink
+                    );
+                }
+                newestTargetTableIf.readUnlock();
+            } catch (Throwable e) {
+                newestTargetTableIf.readUnlock();
+                // the abortTxn in onFail need to acquire table write lock
+                if (insertExecutor != null) {
+                    insertExecutor.onFail(e);
+                }
+                Throwables.throwIfInstanceOf(e, RuntimeException.class);
+                throw new IllegalStateException(e.getMessage(), e);
+            }
+            stmtExecutor.setProfileType(ProfileType.LOAD);
+            // We exposed @StmtExecutor#cancel as a unified entry point for statement interruption,
+            // so we need to set this here
+            insertExecutor.getCoordinator().setTxnId(insertExecutor.getTxnId());
+            stmtExecutor.setCoord(insertExecutor.getCoordinator());
+            // for prepare and execute, avoiding normalization for every execute command
+            this.originalLogicalQuery = this.logicalQuery;
+            return insertExecutor;
+        }
+        LOG.warn("insert plan failed {} times. query id is {}.", retryTimes, DebugUtil.printId(ctx.queryId()));
+        throw new AnalysisException("Insert plan failed. Could not get target table lock.");
+    }
+
+    private BuildInsertExecutorResult initPlanOnce(ConnectContext ctx,
+            StmtExecutor stmtExecutor, TableIf targetTableIf) throws Throwable {
         targetTableIf.readLock();
         try {
-            // 1. process inline table (default values, empty values)
-            this.logicalQuery = (LogicalPlan) InsertUtils.normalizePlan(logicalQuery, targetTableIf, insertCtx);
+            // process inline table (default values, empty values)
+            this.logicalQuery = (LogicalPlan) InsertUtils.normalizePlan(originalLogicalQuery, targetTableIf, insertCtx);
             if (cte.isPresent()) {
                 this.logicalQuery = ((LogicalPlan) cte.get().withChildren(logicalQuery));
             }
             OlapGroupCommitInsertExecutor.analyzeGroupCommit(ctx, targetTableIf, this.logicalQuery, this.insertCtx);
-            LogicalPlanAdapter logicalPlanAdapter = new LogicalPlanAdapter(logicalQuery, ctx.getStatementContext());
-
-            BuildInsertExecutorResult buildResult = planInsertExecutor(
-                    ctx, stmtExecutor, logicalPlanAdapter, targetTableIf
-            );
-
-            insertExecutor = buildResult.executor;
-
-            if (!needBeginTransaction) {
-                targetTableIf.readUnlock();
-                return insertExecutor;
-            }
-            if (!insertExecutor.isEmptyInsert()) {
-                insertExecutor.beginTransaction();
-                insertExecutor.finalizeSink(
-                        buildResult.planner.getFragments().get(0), buildResult.dataSink, buildResult.physicalSink
-                );
-            }
+        } finally {
             targetTableIf.readUnlock();
-        } catch (Throwable e) {
-            targetTableIf.readUnlock();
-            // the abortTxn in onFail need to acquire table write lock
-            if (insertExecutor != null) {
-                insertExecutor.onFail(e);
-            }
-            Throwables.propagateIfInstanceOf(e, RuntimeException.class);
-            throw new IllegalStateException(e.getMessage(), e);
         }
 
-        stmtExecutor.setProfileType(ProfileType.LOAD);
-        // We exposed @StmtExecutor#cancel as a unified entry point for statement interruption,
-        // so we need to set this here
-        insertExecutor.getCoordinator().setTxnId(insertExecutor.getTxnId());
-        stmtExecutor.setCoord(insertExecutor.getCoordinator());
-        return insertExecutor;
+        LogicalPlanAdapter logicalPlanAdapter = new LogicalPlanAdapter(logicalQuery, ctx.getStatementContext());
+        return planInsertExecutor(ctx, stmtExecutor, logicalPlanAdapter, targetTableIf);
     }
 
     // we should select the factory type first, but we can not initial InsertExecutor at this time,

fe/fe-core/src/main/java/org/apache/doris/catalog/Column.java
@@ -1054,10 +1054,7 @@ public class Column implements GsonPostProcessable {
                 && isKey == other.isKey
                 && isAllowNull == other.isAllowNull
                 && isAutoInc == other.isAutoInc
-                && getDataType().equals(other.getDataType())
-                && getStrLen() == other.getStrLen()
-                && getPrecision() == other.getPrecision()
-                && getScale() == other.getScale()
+                && Objects.equals(type, other.type)
                 && Objects.equals(comment, other.comment)
                 && visible == other.visible
                 && Objects.equals(children, other.children)

fe/fe-core/src/main/java/org/apache/doris/catalog/MTMV.java
@@ -201,7 +201,7 @@ public class MTMV extends OlapTable {
                 // to connection issues such as S3, so it is directly set to null
                 if (!isReplay) {
                     // shouldn't do this while holding mvWriteLock
-                    mtmvCache = MTMVCache.from(this, MTMVPlanUtil.createMTMVContext(this), true);
+                    mtmvCache = MTMVCache.from(this, MTMVPlanUtil.createMTMVContext(this), true, true);
                 }
             } catch (Throwable e) {
                 mtmvCache = null;
@@ -323,7 +323,7 @@ public class MTMV extends OlapTable {
         MTMVCache mtmvCache;
         try {
             // Should new context with ADMIN user
-            mtmvCache = MTMVCache.from(this, MTMVPlanUtil.createMTMVContext(this), true);
+            mtmvCache = MTMVCache.from(this, MTMVPlanUtil.createMTMVContext(this), true, false);
         } finally {
             connectionContext.setThreadLocalInfo();
         }
@@ -362,7 +362,7 @@ public class MTMV extends OlapTable {
      *
      * @return mvPartitionName ==> mvPartitionKeyDesc
      */
-    public Map<String, PartitionKeyDesc> generateMvPartitionDescs() throws AnalysisException {
+    public Map<String, PartitionKeyDesc> generateMvPartitionDescs() {
         Map<String, PartitionItem> mtmvItems = getAndCopyPartitionItems();
         Map<String, PartitionKeyDesc> result = Maps.newHashMap();
         for (Entry<String, PartitionItem> entry : mtmvItems.entrySet()) {
@@ -392,7 +392,7 @@ public class MTMV extends OlapTable {
         Map<String, String> baseToMv = Maps.newHashMap();
         Map<PartitionKeyDesc, Set<String>> relatedPartitionDescs = MTMVPartitionUtil
                 .generateRelatedPartitionDescs(mvPartitionInfo, mvProperties);
-        Map<String, PartitionItem> mvPartitionItems = getAndCopyPartitionItemsWithoutLock();
+        Map<String, PartitionItem> mvPartitionItems = getAndCopyPartitionItems();
         for (Entry<String, PartitionItem> entry : mvPartitionItems.entrySet()) {
             Set<String> basePartitionNames = relatedPartitionDescs.getOrDefault(entry.getValue().toPartitionKeyDesc(),
                     Sets.newHashSet());

fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -114,7 +114,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -3325,31 +3324,24 @@ public class OlapTable extends Table implements MTMVRelatedTableIf, GsonPostProc
     }
 
     @Override
-    public Map<String, PartitionItem> getAndCopyPartitionItems(Optional<MvccSnapshot> snapshot)
-            throws AnalysisException {
+    public Map<String, PartitionItem> getAndCopyPartitionItems(Optional<MvccSnapshot> snapshot) {
         return getAndCopyPartitionItems();
     }
 
-    public Map<String, PartitionItem> getAndCopyPartitionItems() throws AnalysisException {
-        if (!tryReadLock(1, TimeUnit.MINUTES)) {
-            throw new AnalysisException("get table read lock timeout, database=" + getDBName() + ",table=" + getName());
-        }
+    public Map<String, PartitionItem> getAndCopyPartitionItems() {
+        readLock();
         try {
-            return getAndCopyPartitionItemsWithoutLock();
+            Map<String, PartitionItem> res = Maps.newHashMap();
+            for (Entry<Long, PartitionItem> entry : getPartitionInfo().getIdToItem(false).entrySet()) {
+                Partition partition = idToPartition.get(entry.getKey());
+                if (partition != null) {
+                    res.put(partition.getName(), entry.getValue());
+                }
+            }
+            return res;
         } finally {
             readUnlock();
         }
-    }
-
-    public Map<String, PartitionItem> getAndCopyPartitionItemsWithoutLock() throws AnalysisException {
-        Map<String, PartitionItem> res = Maps.newHashMap();
-        for (Entry<Long, PartitionItem> entry : getPartitionInfo().getIdToItem(false).entrySet()) {
-            Partition partition = idToPartition.get(entry.getKey());
-            if (partition != null) {
-                res.put(partition.getName(), entry.getValue());
-            }
-        }
-        return res;
     }
 
     @Override

fe/fe-core/src/main/java/org/apache/doris/catalog/TableIf.java
@@ -213,7 +213,6 @@ public interface TableIf {
     }
 
     default Set<ForeignKeyConstraint> getForeignKeyConstraints() {
-        readLock();
         try {
             return getConstraintsMapUnsafe().values().stream()
                     .filter(ForeignKeyConstraint.class::isInstance)
@@ -221,24 +220,18 @@ public interface TableIf {
                     .collect(ImmutableSet.toImmutableSet());
         } catch (Exception ignored) {
             return ImmutableSet.of();
-        } finally {
-            readUnlock();
         }
     }
 
     default Map<String, Constraint> getConstraintsMap() {
-        readLock();
         try {
             return ImmutableMap.copyOf(getConstraintsMapUnsafe());
         } catch (Exception ignored) {
             return ImmutableMap.of();
-        } finally {
-            readUnlock();
         }
     }
 
     default Set<PrimaryKeyConstraint> getPrimaryKeyConstraints() {
-        readLock();
         try {
             return getConstraintsMapUnsafe().values().stream()
                     .filter(PrimaryKeyConstraint.class::isInstance)
@@ -246,13 +239,10 @@ public interface TableIf {
                     .collect(ImmutableSet.toImmutableSet());
         } catch (Exception ignored) {
             return ImmutableSet.of();
-        } finally {
-            readUnlock();
         }
     }
 
     default Set<UniqueConstraint> getUniqueConstraints() {
-        readLock();
         try {
             return getConstraintsMapUnsafe().values().stream()
                     .filter(UniqueConstraint.class::isInstance)
@@ -260,8 +250,6 @@ public interface TableIf {
                     .collect(ImmutableSet.toImmutableSet());
         } catch (Exception ignored) {
             return ImmutableSet.of();
-        } finally {
-            readUnlock();
         }
     }
 
@@ -280,34 +268,24 @@ public interface TableIf {
     }
 
     default void addUniqueConstraint(String name, ImmutableList<String> columns, boolean replay) {
-        writeLock();
-        try {
-            Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
-            UniqueConstraint uniqueConstraint =  new UniqueConstraint(name, ImmutableSet.copyOf(columns));
-            checkConstraintNotExistenceUnsafe(name, uniqueConstraint, constraintMap);
-            constraintMap.put(name, uniqueConstraint);
-            if (!replay) {
-                Env.getCurrentEnv().getEditLog().logAddConstraint(
-                        new AlterConstraintLog(uniqueConstraint, this));
-            }
-        } finally {
-            writeUnlock();
+        Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
+        UniqueConstraint uniqueConstraint = new UniqueConstraint(name, ImmutableSet.copyOf(columns));
+        checkConstraintNotExistenceUnsafe(name, uniqueConstraint, constraintMap);
+        constraintMap.put(name, uniqueConstraint);
+        if (!replay) {
+            Env.getCurrentEnv().getEditLog().logAddConstraint(
+                    new AlterConstraintLog(uniqueConstraint, this));
         }
     }
 
     default void addPrimaryKeyConstraint(String name, ImmutableList<String> columns, boolean replay) {
-        writeLock();
-        try {
-            Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
-            PrimaryKeyConstraint primaryKeyConstraint = new PrimaryKeyConstraint(name, ImmutableSet.copyOf(columns));
-            checkConstraintNotExistenceUnsafe(name, primaryKeyConstraint, constraintMap);
-            constraintMap.put(name, primaryKeyConstraint);
-            if (!replay) {
-                Env.getCurrentEnv().getEditLog().logAddConstraint(
-                        new AlterConstraintLog(primaryKeyConstraint, this));
-            }
-        } finally {
-            writeUnlock();
+        Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
+        PrimaryKeyConstraint primaryKeyConstraint = new PrimaryKeyConstraint(name, ImmutableSet.copyOf(columns));
+        checkConstraintNotExistenceUnsafe(name, primaryKeyConstraint, constraintMap);
+        constraintMap.put(name, primaryKeyConstraint);
+        if (!replay) {
+            Env.getCurrentEnv().getEditLog().logAddConstraint(
+                    new AlterConstraintLog(primaryKeyConstraint, this));
         }
     }
 
@@ -326,26 +304,19 @@ public interface TableIf {
 
     default void addForeignConstraint(String name, ImmutableList<String> columns,
             TableIf referencedTable, ImmutableList<String> referencedColumns, boolean replay) {
-        writeLock();
-        referencedTable.writeLock();
-        try {
-            Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
-            ForeignKeyConstraint foreignKeyConstraint =
-                    new ForeignKeyConstraint(name, columns, referencedTable, referencedColumns);
-            checkConstraintNotExistenceUnsafe(name, foreignKeyConstraint, constraintMap);
-            PrimaryKeyConstraint requirePrimaryKeyName = new PrimaryKeyConstraint(name,
-                    foreignKeyConstraint.getReferencedColumnNames());
-            PrimaryKeyConstraint primaryKeyConstraint =
-                    tryGetPrimaryKeyForForeignKeyUnsafe(requirePrimaryKeyName, referencedTable);
-            primaryKeyConstraint.addForeignTable(this);
-            constraintMap.put(name, foreignKeyConstraint);
-            if (!replay) {
-                Env.getCurrentEnv().getEditLog().logAddConstraint(
-                        new AlterConstraintLog(foreignKeyConstraint, this));
-            }
-        } finally {
-            referencedTable.writeUnlock();
-            writeUnlock();
+        Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
+        ForeignKeyConstraint foreignKeyConstraint =
+                new ForeignKeyConstraint(name, columns, referencedTable, referencedColumns);
+        checkConstraintNotExistenceUnsafe(name, foreignKeyConstraint, constraintMap);
+        PrimaryKeyConstraint requirePrimaryKeyName = new PrimaryKeyConstraint(name,
+                foreignKeyConstraint.getReferencedColumnNames());
+        PrimaryKeyConstraint primaryKeyConstraint =
+                tryGetPrimaryKeyForForeignKeyUnsafe(requirePrimaryKeyName, referencedTable);
+        primaryKeyConstraint.addForeignTable(this);
+        constraintMap.put(name, foreignKeyConstraint);
+        if (!replay) {
+            Env.getCurrentEnv().getEditLog().logAddConstraint(
+                    new AlterConstraintLog(foreignKeyConstraint, this));
         }
     }
 
@@ -381,40 +352,31 @@ public interface TableIf {
     }
 
     default void dropConstraint(String name, boolean replay) {
-        writeLock();
-        try {
-            Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
-            if (!constraintMap.containsKey(name)) {
-                throw new AnalysisException(
-                        String.format("Unknown constraint %s on table %s.", name, this.getName()));
-            }
-            Constraint constraint = constraintMap.get(name);
-            constraintMap.remove(name);
-            if (constraint instanceof PrimaryKeyConstraint) {
-                ((PrimaryKeyConstraint) constraint).getForeignTables()
-                        .forEach(t -> t.dropFKReferringPK(this, (PrimaryKeyConstraint) constraint));
-            }
-            if (!replay) {
-                Env.getCurrentEnv().getEditLog().logDropConstraint(new AlterConstraintLog(constraint, this));
-            }
-        } finally {
-            writeUnlock();
+        Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
+        if (!constraintMap.containsKey(name)) {
+            throw new AnalysisException(
+                    String.format("Unknown constraint %s on table %s.", name, this.getName()));
+        }
+        Constraint constraint = constraintMap.get(name);
+        constraintMap.remove(name);
+        if (constraint instanceof PrimaryKeyConstraint) {
+            ((PrimaryKeyConstraint) constraint).getForeignTables()
+                    .forEach(t -> t.dropFKReferringPK(this, (PrimaryKeyConstraint) constraint));
+        }
+        if (!replay) {
+            Env.getCurrentEnv().getEditLog().logDropConstraint(new AlterConstraintLog(constraint, this));
         }
     }
 
     default void dropFKReferringPK(TableIf table, PrimaryKeyConstraint constraint) {
-        writeLock();
-        try {
-            Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
-            Set<String> fkName = constraintMap.entrySet().stream()
-                    .filter(e -> e.getValue() instanceof ForeignKeyConstraint
-                    && ((ForeignKeyConstraint) e.getValue()).isReferringPK(table, constraint))
-                    .map(Entry::getKey)
-                    .collect(Collectors.toSet());
-            fkName.forEach(constraintMap::remove);
-        } finally {
-            writeUnlock();
-        }
+        Map<String, Constraint> constraintMap = getConstraintsMapUnsafe();
+        Set<String> fkName = constraintMap.entrySet().stream()
+                .filter(e -> e.getValue() instanceof ForeignKeyConstraint
+                        && ((ForeignKeyConstraint) e.getValue()).isReferringPK(table, constraint))
+                .map(Entry::getKey)
+                .collect(Collectors.toSet());
+        fkName.forEach(constraintMap::remove);
+
     }
 
     /**

commit 5b6454370992096185c6523b522a61c405014f7a

[fix](insert) fix insert failed when concurrent schema change (#60182)
fix insert failed when concurrent schema change because the lock scope
is small, introduced by #45045.

```
2025-12-22 18:31:27,916 WARN (mysql-nio-pool-20|395) [StmtExecutor.execute():546] Analyze failed. stmt[223, 6daf515a262f4708-959e9e3ef4590797]
org.apache.doris.common.NereidsException: errCode = 2, detailMessage = insert into cols should be corresponding to the query output
	at org.apache.doris.qe.StmtExecutor.executeByNereids(StmtExecutor.java:706)
	at org.apache.doris.qe.StmtExecutor.execute(StmtExecutor.java:541)
	at org.apache.doris.qe.StmtExecutor.queryRetry(StmtExecutor.java:500)
	at org.apache.doris.qe.StmtExecutor.execute(StmtExecutor.java:485)
	at org.apache.doris.qe.ConnectProcessor.executeQuery(ConnectProcessor.java:311)
	at org.apache.doris.qe.ConnectProcessor.handleQuery(ConnectProcessor.java:198)
	at org.apache.doris.qe.MysqlConnectProcessor.handleQuery(MysqlConnectProcessor.java:231)
	at org.apache.doris.qe.MysqlConnectProcessor.dispatch(MysqlConnectProcessor.java:259)
	at org.apache.doris.qe.MysqlConnectProcessor.processOnce(MysqlConnectProcessor.java:403)
	at org.apache.doris.mysql.ReadListener.lambda$handleEvent$0(ReadListener.java:52)
	at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1136)
	at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:635)
	at java.base/java.lang.Thread.run(Thread.java:833)
Caused by: org.apache.doris.common.AnalysisException: errCode = 2, detailMessage = insert into cols should be corresponding to the query output
	... 13 more
Caused by: org.apache.doris.nereids.exceptions.AnalysisException: insert into cols should be corresponding to the query output
	at org.apache.doris.nereids.rules.analysis.BindSink.bindOlapTableSink(BindSink.java:194)
	at org.apache.doris.nereids.pattern.PatternMatcher$1.transform(PatternMatcher.java:96)

fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/commands/insert/InsertIntoTableCommand.java
@@ -345,12 +345,13 @@ public class InsertIntoTableCommand extends Command implements NeedAuditEncrypti
             }
             OlapGroupCommitInsertExecutor.analyzeGroupCommit(
                     ctx, targetTableIf, this.logicalQuery.get(), this.insertCtx);
+
+            LogicalPlanAdapter logicalPlanAdapter
+                    = new LogicalPlanAdapter(logicalQuery.get(), ctx.getStatementContext());
+            return planInsertExecutor(ctx, stmtExecutor, logicalPlanAdapter, targetTableIf);
         } finally {
             targetTableIf.readUnlock();
         }
-
-        LogicalPlanAdapter logicalPlanAdapter = new LogicalPlanAdapter(logicalQuery.get(), ctx.getStatementContext());
-        return planInsertExecutor(ctx, stmtExecutor, logicalPlanAdapter, targetTableIf);
     }
 
     // we should select the factory type first, but we can not initial InsertExecutor at this time,