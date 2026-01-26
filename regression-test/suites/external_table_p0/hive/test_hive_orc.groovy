bdc35811d78d9b714e0270d4e5bd20fd3ba328f0
daidai <changyuwei@selectdb.com>
2026-01-26 15:53:40 +0800

    [fix](orc)Modifying the orc reader makes error reporting more accurate.

--- regression-test/suites/external_table_p0/hive/test_hive_orc.groovy
+++ regression-test/suites/external_table_p0/hive/test_hive_orc.groovy
@@ -167,6 +167,27 @@ suite("test_hive_orc", "all_types,p0,external,hive,external_docker,external_dock
         return;
     }
 
+    def test_orc_nextbatch_error =  {
+        def tb_name  =  "hive_orc_next_batch_test";
+
+        hive_docker """ drop table if exists ${tb_name} """ 
+        hive_docker """  CREATE TABLE ${tb_name} (id  INT, data STRING) STORED AS ORC; """
+        hive_docker """ 
+        INSERT INTO ${tb_name} VALUES
+            (1, '{"age":25,"city":"beijing","score":88}'),
+            (2,   '{"age":30,"city":"shanghai","score":92}') """
+
+        try {
+            sql """select * from ${tb_name} where json_extract_double(data, '.age') = 25; """
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Orc row reader nextBatch failed"))
+        }
+
+        def result = sql """ select * from ${tb_name} where json_extract_double(data, "\$.age")=25 """
+        assertTrue(result.size() == 1);
+    }
+
+
     for (String hivePrefix : ["hive2", "hive3"]) {
         try {
             String hms_port = context.config.otherConfigs.get(hivePrefix + "HmsPort")
@@ -191,7 +212,8 @@ suite("test_hive_orc", "all_types,p0,external,hive,external_docker,external_dock
             predicate_pushdown()    
             test_topn()
             test_topn_abs()
-            
+            test_orc_nextbatch_error()
+
             sql """drop catalog if exists ${catalog_name}"""
 
             // test old create-catalog syntax for compatibility
