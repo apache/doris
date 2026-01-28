c8d5e12acf474568f2c026bf2feff6f169ed9a21
lambxu <lambxu@tencent.com>
2026-01-28 21:10:39 +0800

    save

--- fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
@@ -302,6 +302,14 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
         this.showState = state;
     }
 
+    public String getClusterName() {
+        return "";
+    }
+
+    public String getStorageVaultName() {
+        return "";
+    }
+
     public int getMetaVersion() {
         return metaVersion;
     }
@@ -2383,6 +2391,8 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
         info.add(replicaAlloc.toCreateStmt());
         info.add(String.valueOf(reserveReplica));
         info.add(String.valueOf(reserveDynamicPartitionEnable));
+        info.add(getClusterName());
+        info.add(getStorageVaultName());
         if (!isBrief) {
             info.add(getRestoreObjs());
         }
