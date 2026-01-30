211e68c2e95c421efb84ba79376a8730d411bcd2
linzhenqi <linzhenqi@selectdb.com>
2026-01-31 00:33:42 +0800

    1

--- be/src/vec/aggregate_functions/aggregate_function_null.h
+++ be/src/vec/aggregate_functions/aggregate_function_null.h
@@ -392,7 +392,9 @@ public:
                 this->add(place, columns, i, arena);
             }
         } else {
-            this->set_flag(place);
+            if (batch_size > 0) {
+                this->set_flag(place);
+            }
             const IColumn* nested_column = &column->get_nested_column();
             this->nested_function->add_batch_single_place(batch_size, this->nested_place(place),
                                                           &nested_column, arena);
@@ -408,7 +410,9 @@ public:
                 this->add(place, columns, i, arena);
             }
         } else {
-            this->set_flag(place);
+            if (batch_begin <= batch_end) {
+                this->set_flag(place);
+            }
             const IColumn* nested_column = &column->get_nested_column();
             this->nested_function->add_batch_range(batch_begin, batch_end,
                                                    this->nested_place(place), &nested_column, arena,
