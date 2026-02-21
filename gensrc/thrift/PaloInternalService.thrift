9230a116dbf5ecf5c7b715b73b57783baf945eb1
Chenjunwei <138805230+xylaaaaa@users.noreply.github.com>
2026-02-21 10:17:06 +0800

    [feat](paimon) integrate paimon-cpp reader (#60676)
    Issue Number: #56005
    
    Co-authored-by: morningman <yunyou@selectdb.com>

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -431,6 +431,9 @@ struct TQueryOptions {
 
   195: optional bool enable_left_semi_direct_return_opt;
 
+  200: optional bool enable_adjust_conjunct_order_by_cost;
+  // Use paimon-cpp to read Paimon splits on BE
+  201: optional bool enable_paimon_cpp_reader = false;
   // Whether all fragments of this query are assigned to a single backend.
   // When true, the streaming aggregation operator can use more aggressive
   // hash table expansion thresholds since all data is local.
