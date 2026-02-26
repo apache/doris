b6c48039da9c7cad3e754804999c20360d61af21
morrySnow <zhangwenxin@selectdb.com>
2026-02-26 12:28:03 +0800

    [chore](planner) clear code under analysis package

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/AccessPathInfo.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/AccessPathInfo.java
@@ -14,11 +14,8 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-// This file is copied from
-// https://github.com/apache/impala/blob/branch-2.9.0/fe/src/main/java/org/apache/impala/TupleDescriptor.java
-// and modified by Doris
 
-package org.apache.doris.analysis;
+package org.apache.doris.nereids.rules.rewrite;
 
 import org.apache.doris.nereids.types.DataType;
 import org.apache.doris.thrift.TColumnAccessPath;
