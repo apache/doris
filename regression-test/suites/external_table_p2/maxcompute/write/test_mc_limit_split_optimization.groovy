dfb60731327e1a8cdbf45d7ee8d744f3318bc2ba
morningman <yunyou@selectdb.com>
2026-02-27 23:24:31 +0800

    3

--- regression-test/suites/external_table_p2/maxcompute/write/test_mc_limit_split_optimization.groovy
+++ regression-test/suites/external_table_p2/maxcompute/write/test_mc_limit_split_optimization.groovy
@@ -25,7 +25,7 @@ suite("test_mc_limit_split_optimization", "p2,external,maxcompute,external_remot
     String ak = context.config.otherConfigs.get("ak")
     String sk = context.config.otherConfigs.get("sk")
     String mc_catalog_name = "test_mc_limit_split_opt"
-    String defaultProject = "mc_doris_test_write""
+    String defaultProject = "mc_doris_test_write"
 
     sql """drop catalog if exists ${mc_catalog_name}"""
     sql """
