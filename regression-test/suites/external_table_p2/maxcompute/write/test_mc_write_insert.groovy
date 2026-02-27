d0e15bec6f9eb824424c3f57b0106a9b57a1a9c1
daidai <changyuwei@selectdb.com>
2026-02-27 23:24:50 +0800

    [fix](case)fix some external regression test. (#60744)

--- regression-test/suites/external_table_p2/maxcompute/write/test_mc_write_insert.groovy
+++ regression-test/suites/external_table_p2/maxcompute/write/test_mc_write_insert.groovy
@@ -25,7 +25,7 @@ suite("test_mc_write_insert", "p2,external,maxcompute,external_remote,external_r
     String ak = context.config.otherConfigs.get("ak")
     String sk = context.config.otherConfigs.get("sk")
     String mc_catalog_name = "test_mc_write_insert"
-    String defaultProject = "doris_test_schema"
+    String defaultProject = "mc_doris_test_write"
 
     sql """drop catalog if exists ${mc_catalog_name}"""
     sql """
--- regression-test/suites/external_table_p2/maxcompute/write/test_mc_write_large_data.groovy
+++ regression-test/suites/external_table_p2/maxcompute/write/test_mc_write_large_data.groovy
@@ -25,7 +25,7 @@ suite("test_mc_write_large_data", "p2,external,maxcompute,external_remote,extern
     String ak = context.config.otherConfigs.get("ak")
     String sk = context.config.otherConfigs.get("sk")
     String mc_catalog_name = "test_mc_write_large_data"
-    String defaultProject = "doris_test_schema"
+    String defaultProject = "mc_doris_test_write"
 
     sql """drop catalog if exists ${mc_catalog_name}"""
     sql """
