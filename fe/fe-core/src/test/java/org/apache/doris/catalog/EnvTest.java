6dcdf42957f0698903842b5e2e91db22a3d97d53
shuke987 <shuke@selectdb.com>
2026-03-01 16:35:40 +0800

    Adjust FE JVM opts and revert extra changes

--- fe/fe-core/src/test/java/org/apache/doris/catalog/EnvTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/catalog/EnvTest.java
@@ -17,21 +17,12 @@
 
 package org.apache.doris.catalog;
 
-import org.apache.doris.alter.AlterJobV2;
-import org.apache.doris.alter.MaterializedViewHandler;
-import org.apache.doris.alter.SchemaChangeHandler;
-import org.apache.doris.alter.SchemaChangeJobV2;
 import org.apache.doris.common.FeConstants;
 import org.apache.doris.common.io.CountingDataOutputStream;
-import org.apache.doris.datasource.InternalCatalog;
 import org.apache.doris.meta.MetaContext;
 import org.apache.doris.persist.meta.MetaHeader;
 
 import mockit.Expectations;
-import mockit.Mock;
-import mockit.MockUp;
-import mockit.Mocked;
-import mockit.Verifications;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,10 +34,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class EnvTest {
 
@@ -151,91 +139,5 @@ public class EnvTest {
         dis.close();
 
         deleteDir(dir);
-    }
-
-    @Test
-    public void testOnEraseOlapTableCleansAlterJobs(@Mocked InternalCatalog internalCatalog,
-                                                    @Mocked TabletInvertedIndex invertedIndex,
-                                                    @Mocked ColocateTableIndex colocateTableIndex,
-                                                    @Mocked OlapTable olapTable) {
-        Env env = new Env(true);
-        MaterializedViewHandler materializedViewHandler = new MaterializedViewHandler();
-        SchemaChangeHandler schemaChangeHandler = new SchemaChangeHandler();
-
-        long erasedTableId = 10001L;
-        AlterJobV2 mvTargetJob = new SchemaChangeJobV2("sql", 1L, 1L, erasedTableId, "t1", 1000L);
-        AlterJobV2 mvOtherJob = new SchemaChangeJobV2("sql", 2L, 1L, 20002L, "t2", 1000L);
-        AlterJobV2 scTargetJob = new SchemaChangeJobV2("sql", 3L, 1L, erasedTableId, "t1", 1000L);
-        AlterJobV2 scOtherJob = new SchemaChangeJobV2("sql", 4L, 1L, 30003L, "t3", 1000L);
-
-        Map<Long, AlterJobV2> mvJobs = materializedViewHandler.getAlterJobsV2();
-        Map<Long, AlterJobV2> scJobs = schemaChangeHandler.getAlterJobsV2();
-        mvJobs.put(mvTargetJob.getJobId(), mvTargetJob);
-        mvJobs.put(mvOtherJob.getJobId(), mvOtherJob);
-        scJobs.put(scTargetJob.getJobId(), scTargetJob);
-        scJobs.put(scOtherJob.getJobId(), scOtherJob);
-
-        new MockUp<Env>() {
-            @Mock
-            public static TabletInvertedIndex getCurrentInvertedIndex() {
-                return invertedIndex;
-            }
-
-            @Mock
-            public static ColocateTableIndex getCurrentColocateIndex() {
-                return colocateTableIndex;
-            }
-        };
-
-        new Expectations(env, olapTable) {
-            {
-                invertedIndex.getTabletMetaMap();
-                result = new ConcurrentHashMap<>();
-                minTimes = 0;
-
-                env.getInternalCatalog();
-                result = internalCatalog;
-                minTimes = 0;
-
-                env.getMaterializedViewHandler();
-                result = materializedViewHandler;
-                minTimes = 0;
-
-                env.getSchemaChangeHandler();
-                result = schemaChangeHandler;
-                minTimes = 0;
-
-                olapTable.getId();
-                result = erasedTableId;
-                minTimes = 0;
-
-                olapTable.getName();
-                result = "test_tbl";
-                minTimes = 0;
-
-                olapTable.getAllPartitions();
-                result = Collections.emptyList();
-                minTimes = 0;
-            }
-        };
-
-        env.onEraseOlapTable(1L, olapTable, false);
-
-        Assert.assertEquals(1, mvJobs.size());
-        Assert.assertTrue(mvJobs.containsValue(mvOtherJob));
-        Assert.assertFalse(mvJobs.containsValue(mvTargetJob));
-
-        Assert.assertEquals(1, scJobs.size());
-        Assert.assertTrue(scJobs.containsValue(scOtherJob));
-        Assert.assertFalse(scJobs.containsValue(scTargetJob));
-
-        new Verifications() {
-            {
-                internalCatalog.eraseTableDropBackendReplicas(1L, olapTable, false);
-                times = 1;
-                colocateTableIndex.removeTable(erasedTableId);
-                times = 1;
-            }
-        };
     }
 }
