5dc93b541dc1fd305b5fa6a89aeacc72bddc28b6
Calvin Kirs <guoqiang@selectdb.com>
2026-03-03 21:52:23 +0800

    use scancontext

--- fe/fe-core/src/main/java/org/apache/doris/planner/ScanNode.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/ScanNode.java
@@ -71,6 +71,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -112,7 +113,7 @@ public abstract class ScanNode extends PlanNode implements SplitGenerator {
     public ScanNode(PlanNodeId id, TupleDescriptor desc, String planNodeName, ScanContext scanContext) {
         super(id, desc.getId().asList(), planNodeName);
         this.desc = desc;
-        this.scanContext = scanContext == null ? ScanContext.EMPTY : scanContext;
+        this.scanContext = Objects.requireNonNull(scanContext, "scanContext can not be null");
     }
 
     protected List<Column> getColumns() {
