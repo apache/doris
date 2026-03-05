3ec0c30ac75fbbadb87cad0d35351e6222378668
guoqiang <guoqiang@selectdb.com>
2026-03-05 10:53:30 +0800

    fix

--- regression-test/suites/auth_p0/test_authentication_integration_auth.groovy
+++ regression-test/suites/auth_p0/test_authentication_integration_auth.groovy
@@ -1,16 +1,16 @@
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
+// distributed  this work for additional information
 // regarding copyright ownership.  The ASF licenses this file
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
+//  the License.  You may obtain a copy of the License at
 //
 //   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// "AS IS" BASIS, OUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
@@ -25,14 +25,14 @@ suite("test_authentication_integration_auth", "p0,auth") {
         test {
             sql """
                 CREATE AUTHENTICATION INTEGRATION ${integrationName}
-                WITH PROPERTIES ('ldap.server'='ldap://127.0.0.1:389')
+                 PROPERTIES ('ldap.server'='ldap://127.0.0.1:389')
             """
             exception "Property 'type' is required"
         }
 
         sql """
             CREATE AUTHENTICATION INTEGRATION ${integrationName}
-            WITH PROPERTIES (
+             PROPERTIES (
                 'type'='ldap',
                 'ldap.server'='ldap://127.0.0.1:389',
                 'ldap.admin_password'='123456'
@@ -43,7 +43,7 @@ suite("test_authentication_integration_auth", "p0,auth") {
         test {
             sql """
                 CREATE AUTHENTICATION INTEGRATION ${integrationName}
-                WITH PROPERTIES ('type'='ldap', 'ldap.server'='ldap://127.0.0.1:1389')
+                 PROPERTIES ('type'='ldap', 'ldap.server'='ldap://127.0.0.1:1389')
             """
             exception "already exists"
         }
