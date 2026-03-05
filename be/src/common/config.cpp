b1069d80cad44870c52c4d2baa11743928049528
lihangyu <lihangyu@selectdb.com>
2026-03-05 15:12:37 +0800

    [minor](variant) refactor nested group interface

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -1156,7 +1156,7 @@ DEFINE_mBool(enable_variant_doc_sparse_write_subcolumns, "true");
 // Reserved for future use when NestedGroup expansion moves to storage layer
 // Deeper arrays will be stored as JSONB
 DEFINE_mInt32(variant_nested_group_max_depth, "3");
-DEFINE_mBool(variant_nested_group_discard_scalar_on_conflict, "true");
+DEFINE_mBool(variant_nested_group_discard_scalar_on_conflict, "false");
 
 DEFINE_Validator(variant_max_json_key_length,
                  [](const int config) -> bool { return config > 0 && config <= 65535; });
