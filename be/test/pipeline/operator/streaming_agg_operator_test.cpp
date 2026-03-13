7b8c01a62d0a4279fd6bf4647eebb85930b9911d
HappenLee <happenlee@selectdb.com>
2026-03-13 19:44:36 +0800

    [Opt](exec) cherry pick the opt code #60137 #59492 #59446 #58728 (#61282)
    ### What problem does this PR solve?
    
    cherry pick the opt code #60137 #59492 #59446 #58728
    Problem Summary:
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test <!-- At least one of them must be included. -->
        - [ ] Regression test
        - [ ] Unit Test
        - [ ] Manual test (add detailed scripts or steps below)
        - [ ] No need to test or manual test. Explain why:
    - [ ] This is a refactor/code format and no logic has been changed.
            - [ ] Previous test can cover this change.
            - [ ] No code files have been changed.
            - [ ] Other reason <!-- Add your reason?  -->
    
    - Behavior changed:
        - [ ] No.
        - [ ] Yes. <!-- Explain the behavior change -->
    
    - Does this need documentation?
        - [ ] No.
    - [ ] Yes. <!-- Add document PR link here. eg:
    https://github.com/apache/doris-website/pull/1214 -->
    
    ### Check List (For Reviewer who merge this PR)
    
    - [ ] Confirm the release note
    - [ ] Confirm test cases
    - [ ] Confirm document
    - [ ] Add branch pick label <!-- Add branch pick label that this PR
    should merge into -->

--- be/test/pipeline/operator/streaming_agg_operator_test.cpp
+++ be/test/pipeline/operator/streaming_agg_operator_test.cpp
@@ -109,7 +109,6 @@ TEST_F(StreamingAggOperatorTest, test1) {
             false));
     op->_pool = &pool;
     op->_needs_finalize = false;
-    op->_is_merge = false;
 
     EXPECT_TRUE(op->set_child(child_op));
 
@@ -157,7 +156,9 @@ TEST_F(StreamingAggOperatorTest, test1) {
         EXPECT_TRUE(op->need_more_input_data(state.get()));
     }
 
-    { EXPECT_TRUE(local_state->close(state.get()).ok()); }
+    {
+        EXPECT_TRUE(local_state->close(state.get()).ok());
+    }
 }
 
 TEST_F(StreamingAggOperatorTest, test2) {
@@ -166,7 +167,6 @@ TEST_F(StreamingAggOperatorTest, test2) {
             false));
     op->_pool = &pool;
     op->_needs_finalize = false;
-    op->_is_merge = false;
 
     EXPECT_TRUE(op->set_child(child_op));
 
@@ -234,7 +234,9 @@ TEST_F(StreamingAggOperatorTest, test2) {
         EXPECT_EQ(block.rows(), 3);
     }
 
-    { EXPECT_TRUE(local_state->close(state.get()).ok()); }
+    {
+        EXPECT_TRUE(local_state->close(state.get()).ok());
+    }
 }
 
 TEST_F(StreamingAggOperatorTest, test3) {
@@ -243,7 +245,6 @@ TEST_F(StreamingAggOperatorTest, test3) {
             false));
     op->_pool = &pool;
     op->_needs_finalize = false;
-    op->_is_merge = false;
 
     EXPECT_TRUE(op->set_child(child_op));
 
@@ -314,7 +315,9 @@ TEST_F(StreamingAggOperatorTest, test3) {
         EXPECT_EQ(block.rows(), 3);
     }
 
-    { EXPECT_TRUE(local_state->close(state.get()).ok()); }
+    {
+        EXPECT_TRUE(local_state->close(state.get()).ok());
+    }
 }
 
 TEST_F(StreamingAggOperatorTest, test4) {
@@ -323,7 +326,6 @@ TEST_F(StreamingAggOperatorTest, test4) {
                                       std::make_shared<DataTypeBitMap>(), false));
     op->_pool = &pool;
     op->_needs_finalize = false;
-    op->_is_merge = false;
 
     EXPECT_TRUE(op->set_child(child_op));
 
@@ -406,7 +408,9 @@ TEST_F(StreamingAggOperatorTest, test4) {
         //         << "Expected: " << res_block.dump_data() << ", but got: " << block.dump_data();
     }
 
-    { EXPECT_TRUE(local_state->close(state.get()).ok()); }
+    {
+        EXPECT_TRUE(local_state->close(state.get()).ok());
+    }
 }
 
 } // namespace doris::pipeline
