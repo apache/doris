b5846995fa9b2d0c78f01b564242d77056946a8f
zfr95 <qhu15zhang3294197@163.com>
2026-03-16 17:41:49 +0800

    [test](auth)Resolve the conflict in the backup auth case (#61384)
    [test](auth)Resolve the conflict in the backup auth case
    
    During backup, failing to acquire the lock within 10 seconds makes the
    test case unstable, adding nonConcurrent can fix it.

--- regression-test/suites/auth_call/test_ddl_backup_auth.groovy
+++ regression-test/suites/auth_call/test_ddl_backup_auth.groovy
@@ -18,7 +18,7 @@
 import org.junit.Assert;
 import java.util.UUID
 
-suite("test_ddl_backup_auth","p0,auth_call") {
+suite("test_ddl_backup_auth","p0,auth_call,nonConcurrent") {
     UUID uuid = UUID.randomUUID()
     String randomValue = uuid.toString()
     int hashCode = randomValue.hashCode()
