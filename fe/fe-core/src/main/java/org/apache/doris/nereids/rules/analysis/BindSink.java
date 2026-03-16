6695ac98558a69eab282191372b0ae5ec0fbb642
morningman <yunyou@selectdb.com>
2026-03-16 10:03:24 -0700

    fix compile

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSink.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/analysis/BindSink.java
@@ -104,6 +104,10 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.apache.iceberg.PartitionField;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Table;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
