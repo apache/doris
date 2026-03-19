2d6194da4dd0a13e8ae67ed62c2f718bb05622e3
hageshiame <2664504212@qq.om>
2026-03-19 11:24:50 +0800

    refactor: test files isloated

--- cloud/src/recycler/snapshot_chain_compactor.cpp
+++ cloud/src/recycler/snapshot_chain_compactor.cpp
@@ -25,12 +25,10 @@
 #include "meta-store/keys.h"
 #include "recycler/s3_accessor.h"
 #include "recycler/storage_vault_accessor.h"
-#ifdef UNIT_TEST
-#include "../test/mock_accessor.h"
-#endif
 #ifdef ENABLE_HDFS_STORAGE_VAULT
 #include "recycler/hdfs_accessor.h"
 #endif
+#include "cpp/sync_point.h"
 #include "recycler/util.h"
 #include "snapshot/doris_snapshot_manager.h"
 
@@ -292,9 +290,13 @@ int InstanceChainCompactor::init() {
 
 int InstanceChainCompactor::init_obj_store_accessors() {
     for (const auto& obj_info : instance_info_.obj_info()) {
-#ifdef UNIT_TEST
-        auto accessor = std::make_shared<MockAccessor>();
-#else
+        // Test injection point: allow tests to provide mock accessor
+        TEST_SYNC_POINT_CALLBACK("InstanceChainCompactor::init_obj_store.mock",
+                                 &accessor_map_, &obj_info);
+        if (accessor_map_.count(obj_info.id())) {
+            continue;
+        }
+
         auto s3_conf = S3Conf::from_obj_store_info(obj_info);
         if (!s3_conf) {
             LOG(WARNING) << "failed to init object accessor, instance_id=" << instance_id_;
@@ -308,7 +310,6 @@ int InstanceChainCompactor::init_obj_store_accessors() {
                          << " resource_id=" << obj_info.id();
             return ret;
         }
-#endif
 
         accessor_map_.emplace(obj_info.id(), std::move(accessor));
     }
@@ -333,8 +334,12 @@ int InstanceChainCompactor::init_storage_vault_accessors() {
             LOG(WARNING) << "malformed storage vault, unable to deserialize key=" << hex(k);
             return -1;
         }
-        TEST_SYNC_POINT_CALLBACK("InstanceRecycler::init_storage_vault_accessors.mock_vault",
+        // Test injection point: allow tests to provide mock accessor
+        TEST_SYNC_POINT_CALLBACK("InstanceChainCompactor::init_storage_vault.mock",
                                  &accessor_map_, &vault);
+        if (accessor_map_.count(vault.id())) {
+            continue;
+        }
         if (vault.has_hdfs_info()) {
 #ifdef ENABLE_HDFS_STORAGE_VAULT
             auto accessor = std::make_shared<HdfsAccessor>(vault.hdfs_info());
@@ -351,9 +356,6 @@ int InstanceChainCompactor::init_storage_vault_accessors() {
                        << "but HDFS storage vaults were detected";
 #endif
         } else if (vault.has_obj_info()) {
-#ifdef UNIT_TEST
-            auto accessor = std::make_shared<MockAccessor>();
-#else
             auto s3_conf = S3Conf::from_obj_store_info(vault.obj_info());
             if (!s3_conf) {
                 LOG(WARNING) << "failed to init object accessor, instance_id=" << instance_id_;
@@ -367,7 +369,6 @@ int InstanceChainCompactor::init_storage_vault_accessors() {
                              << " resource_id=" << vault.id() << " name=" << vault.name();
                 return ret;
             }
-#endif
 
             accessor_map_.emplace(vault.id(), std::move(accessor));
         }
