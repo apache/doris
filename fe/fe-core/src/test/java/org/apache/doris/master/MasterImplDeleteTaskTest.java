ab7b1957854641d28c26c55d0147beca53c11afb
Siyang Tang <tangsiyang@selectdb.com>
2026-03-24 12:40:19 +0800

    [fix](test) fix Env mock in delete task test

--- fe/fe-core/src/test/java/org/apache/doris/master/MasterImplDeleteTaskTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/master/MasterImplDeleteTaskTest.java
@@ -66,7 +66,7 @@ public class MasterImplDeleteTaskTest {
 
         new MockUp<Env>() {
             @Mock
-            public static SystemInfoService getCurrentSystemInfo() {
+            public SystemInfoService getCurrentSystemInfo() {
                 return systemInfoService;
             }
         };
