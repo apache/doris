039778d7c906b2566de32a0df7b9562df63abb70
deardeng <dengxin@selectdb.com>
2026-03-26 16:04:55 +0800

    [fix](cloud) Fix cloud not print ms log

--- cloud/script/start.sh
+++ cloud/script/start.sh
@@ -172,8 +172,8 @@ if [[ "${RUN_DAEMON}" -eq 1 ]]; then
     tail -n12 "${out_file}"
     exit 0
 elif [[ "${RUN_CONSOLE}" -eq 1 ]]; then
-    # stdout and stderr both output to doris_cloud.out
-    "${bin}" "$@" >>"${out_file}" 2>&1
+    # stdout for K8S log capture (StdoutLogSink), stderr (crash stack) to .out file
+    "${bin}" "$@" 2>>"${out_file}"
 else
     "${bin}" "$@"
 fi
