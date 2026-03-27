3e1488ff86a9d1380ce2d3803ac95deeff94fe44
linrrarity <linzhenqi@selectdb.com>
2026-03-27 11:05:21 +0800

    [Feature](func) Support table function json_each, json_each_text (#60910)
    doc: https://github.com/apache/doris-website/pull/3422
    
    ```text
    Doris> SELECT k, v
        -> FROM (SELECT 1) dummy
        -> LATERAL VIEW json_each('{"a":"foo","b":"bar"}') t AS k, v;
    +------+-------+
    | k    | v     |
    +------+-------+
    | a    | "foo" |
    | b    | "bar" |
    +------+-------+
    2 rows in set (0.01 sec)
    
    Doris> SELECT k, v
        -> FROM (SELECT 1) dummy
        -> LATERAL VIEW json_each_text('{"a":"foo","b":"bar"}') t AS k, v;
    +------+------+
    | k    | v    |
    +------+------+
    | a    | foo  |
    | b    | bar  |
    +------+------+
    2 rows in set (0.01 sec)
    ```

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/TableGeneratingFunctionVisitor.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/visitor/TableGeneratingFunctionVisitor.java
@@ -38,6 +38,10 @@ import org.apache.doris.nereids.trees.expressions.functions.generator.ExplodeOut
 import org.apache.doris.nereids.trees.expressions.functions.generator.ExplodeSplit;
 import org.apache.doris.nereids.trees.expressions.functions.generator.ExplodeSplitOuter;
 import org.apache.doris.nereids.trees.expressions.functions.generator.ExplodeVariantArray;
+import org.apache.doris.nereids.trees.expressions.functions.generator.JsonEach;
+import org.apache.doris.nereids.trees.expressions.functions.generator.JsonEachOuter;
+import org.apache.doris.nereids.trees.expressions.functions.generator.JsonEachText;
+import org.apache.doris.nereids.trees.expressions.functions.generator.JsonEachTextOuter;
 import org.apache.doris.nereids.trees.expressions.functions.generator.PosExplode;
 import org.apache.doris.nereids.trees.expressions.functions.generator.PosExplodeOuter;
 import org.apache.doris.nereids.trees.expressions.functions.generator.TableGeneratingFunction;
@@ -76,6 +80,22 @@ public interface TableGeneratingFunctionVisitor<R, C> {
 
     default R visitExplodeJsonObjectOuter(ExplodeJsonObjectOuter explodeOuter, C context) {
         return visitTableGeneratingFunction(explodeOuter, context);
+    }
+
+    default R visitJsonEach(JsonEach jsonEach, C context) {
+        return visitTableGeneratingFunction(jsonEach, context);
+    }
+
+    default R visitJsonEachOuter(JsonEachOuter jsonEachOuter, C context) {
+        return visitTableGeneratingFunction(jsonEachOuter, context);
+    }
+
+    default R visitJsonEachText(JsonEachText jsonEachText, C context) {
+        return visitTableGeneratingFunction(jsonEachText, context);
+    }
+
+    default R visitJsonEachTextOuter(JsonEachTextOuter jsonEachTextOuter, C context) {
+        return visitTableGeneratingFunction(jsonEachTextOuter, context);
     }
 
     default R visitExplodeNumbers(ExplodeNumbers explodeNumbers, C context) {
