1ee3e2653907524dca659a4eb5acde750565bc6a
Guangming Lu <lgm0862@qq.com>
2026-03-27 20:49:06 +0800

    [fix](restore) Fix after the table with MVs backup and restore, load data fails

--- fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/backup/RestoreJob.java
@@ -2707,6 +2707,7 @@ public class RestoreJob extends AbstractJob implements GsonPostProcessable {
                         || olapTbl.getState() == OlapTableState.RESTORE_WITH_LOAD) {
                     LOG.info("table {} set state from {} to normal", tableName, olapTbl.getState());
                     olapTbl.setState(OlapTableState.NORMAL);
+                    olapTbl.analyze(db.getFullName());
                 }
                 if (olapTbl.isInAtomicRestore()) {
                     olapTbl.clearInAtomicRestore();
