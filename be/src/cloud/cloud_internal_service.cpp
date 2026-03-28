6e9c49803de15bf307dc1eb0454166bef49a6be3
deardeng <dengxin@selectdb.com>
2026-03-28 18:22:54 +0800

    [fix](filecache) pass tablet_id through FileReaderOptions instead of parsing from path (#61683)
    CachedRemoteFileReader::_execute_remote_read previously parsed tablet_id
    from file paths at runtime via extract_tablet_id(). This breaks when
    enable_packed_file (small file merging) is enabled because packed file
    paths don't follow the expected data/{tablet_id}/... format.
    
    Fix: store tablet_id from FileReaderOptions at construction time and use
    it directly, eliminating runtime path parsing. Propagate tablet_id
    through all code paths: Segment, InvertedIndexFileReader,
    FSIndexInput::open, DownloadFileMeta (warmup/preheating), and
    beta_rowset consistency checks.
    
    Issue Number: close #xxx
    
    Related PR: #xxx
    
    Problem Summary:
    
    None
    
    - Test <!-- At least one of them must be included. -->
        - [ ] Regression test
        - [ ] Unit Test
        - [ ] Manual test (add detailed scripts or steps below)
        - [x] No need to test or manual test. Explain why:
    - [ ] This is a refactor/code format and no logic has been changed.
            - [x] Previous test can cover this change.
            - [ ] No code files have been changed.
            - [ ] Other reason <!-- Add your reason?  -->
    
    - Behavior changed:
        - [x] No.
        - [ ] Yes. <!-- Explain the behavior change -->
    
    - Does this need documentation?
        - [x] No.
    - [ ] Yes. <!-- Add document PR link here. eg:
    https://github.com/apache/doris-website/pull/1214 -->
    
    - [ ] Confirm the release note
    - [ ] Confirm test cases
    - [ ] Confirm document
    - [ ] Add branch pick label <!-- Add branch pick label that this PR
    should merge into -->
    
    (cherry picked from commit 58938de7bcf4246650c7b751b00f54af7abc5d9a)

--- be/src/cloud/cloud_internal_service.cpp
+++ be/src/cloud/cloud_internal_service.cpp
@@ -728,7 +728,8 @@ void CloudInternalServiceImpl::warm_up_rowset(google::protobuf::RpcController* c
                                                          tablet, wait, version, segment_size,
                                                          request_ts, handle_ts, job_id_str,
                                                          upstream_trigger_ts_ms);
-                        }};
+                        },
+                        .tablet_id = tablet_id};
 
                 g_file_cache_event_driven_warm_up_submitted_segment_num << 1;
                 g_file_cache_event_driven_warm_up_submitted_segment_size << segment_size;
@@ -757,7 +758,8 @@ void CloudInternalServiceImpl::warm_up_rowset(google::protobuf::RpcController* c
                                     st, tablet_id, rowset_id, segment_id, index_path, tablet, wait,
                                     version, idx_size, request_ts, handle_ts, job_id_str,
                                     upstream_trigger_ts_ms);
-                        }};
+                        },
+                        .tablet_id = tablet_id};
                 g_file_cache_event_driven_warm_up_submitted_index_num << 1;
                 g_file_cache_event_driven_warm_up_submitted_index_size << idx_size;
                 tablet->update_rowset_warmup_state_inverted_idx_num(
