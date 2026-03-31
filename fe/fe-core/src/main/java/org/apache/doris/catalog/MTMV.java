9b959298b3250e12fa073fbe7ad6e5ba49d3474c
yujun <yujun@selectdb.com>
2026-03-31 16:45:11 +0800

    [refactor](fe) Rename IVM-prefixed classes to Ivm in mtmv/ivm package
    Renamed classes under fe/mtmv/ivm/ from all-caps IVM prefix to camel-case Ivm
    to conform to Java naming conventions. Also removed unused IvmCapabilityResult.
    
    Renamed:
    - IVMCapabilityResult (deleted, was unused)
    - IVMDeltaExecutor    -> IvmDeltaExecutor
    - IVMInfo             -> IvmInfo
    - IVMRefreshContext   -> IvmRefreshContext
    - IVMRefreshManager   -> IvmRefreshManager
    - IVMRefreshResult    -> IvmRefreshResult
    - IVMStreamRef        -> IvmStreamRef
    - IVMDeltaExecutorTest   -> IvmDeltaExecutorTest
    - IVMRefreshManagerTest  -> IvmRefreshManagerTest

--- fe/fe-core/src/main/java/org/apache/doris/catalog/MTMV.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/MTMV.java
@@ -45,7 +45,7 @@ import org.apache.doris.mtmv.MTMVRelatedTableIf;
 import org.apache.doris.mtmv.MTMVRelation;
 import org.apache.doris.mtmv.MTMVSnapshotIf;
 import org.apache.doris.mtmv.MTMVStatus;
-import org.apache.doris.mtmv.ivm.IVMInfo;
+import org.apache.doris.mtmv.ivm.IvmInfo;
 import org.apache.doris.mtmv.ivm.IvmUtil;
 import org.apache.doris.nereids.rules.analysis.SessionVarGuardRewriter;
 import org.apache.doris.qe.ConnectContext;
@@ -93,7 +93,7 @@ public class MTMV extends OlapTable {
     @SerializedName("rs")
     private MTMVRefreshSnapshot refreshSnapshot;
     @SerializedName("ii")
-    private IVMInfo ivmInfo;
+    private IvmInfo ivmInfo;
     // Should update after every fresh, not persist
     // Cache with SessionVarGuardExpr: used when query session variables differ from MV creation variables
     private MTMVCache cacheWithGuard;
@@ -129,7 +129,7 @@ public class MTMV extends OlapTable {
         this.mvPartitionInfo = params.mvPartitionInfo;
         this.relation = params.relation;
         this.refreshSnapshot = new MTMVRefreshSnapshot();
-        this.ivmInfo = new IVMInfo();
+        this.ivmInfo = new IvmInfo();
         this.envInfo = new EnvInfo(-1L, -1L);
         this.sessionVariables = params.sessionVariables;
         mvRwLock = new ReentrantReadWriteLock(true);
@@ -741,7 +741,7 @@ public class MTMV extends OlapTable {
             sessionVariables = Maps.newHashMap();
         }
         if (ivmInfo == null) {
-            ivmInfo = new IVMInfo();
+            ivmInfo = new IvmInfo();
         }
         Map<String, MTMVRefreshPartitionSnapshot> partitionSnapshots = refreshSnapshot.getPartitionSnapshots();
         compatiblePctSnapshot(partitionSnapshots);
