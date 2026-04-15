f4778419f43eb0a033aa4d6becfa458b04cfc847
Calvin Kirs <kirs@apache.org>
2026-04-15 11:33:15 +0800

    Remove 'constraintManager' from module list
    Removed 'constraintManager' from the list of modules in PersistMetaModules.java.

--- fe/fe-core/src/main/java/org/apache/doris/persist/meta/PersistMetaModules.java
+++ fe/fe-core/src/main/java/org/apache/doris/persist/meta/PersistMetaModules.java
@@ -44,7 +44,7 @@ public class PersistMetaModules {
             "globalFunction", "workloadGroups",
             "binlogs", "resourceGroups", "AnalysisMgrV2", "AsyncJobManager", "workloadSchedPolicy",
             "insertOverwrite", "plsql", "dictionaryManager", "indexPolicy", "KeyManagerStore",
-            "constraintManager", "TableStreamManager", "authenticationIntegrations", "roleMappings"
+            "authenticationIntegrations", "roleMappings"
     );
 
     // The modules in `CloudEnv`.
