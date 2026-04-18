6b4e832dc966de7cb14b48006f7af76d04dc4240
Dongyang Li <lidongyang@selectdb.com>
2026-04-18 21:46:06 +0800

    Update regression-conf-custom.groovy

--- regression-test/pipeline/cloud_p0/conf/regression-conf-custom.groovy
+++ regression-test/pipeline/cloud_p0/conf/regression-conf-custom.groovy
@@ -78,6 +78,7 @@ excludeDirectories = "000_the_start_sentinel_do_not_touch," + // keep this line 
     "cloud_p0/multi_cluster," + // run in specific regression pipeline
     "cloud_p0/cache," +
     "pythonudaf_p0," + // will cause a core when the third-party is compiled by clang
+    "pythonudtf_p0," + // will cause a core when the third-party is compiled by clang
     "pythonudf_p0," + // will cause a core when the third-party is compiled by clang
     "shape_check," + // run only in p0 is enough
     "query_p0/cache," + // run only in p0 is enough
