b5429564c36e7e7cf04452b8d442c602cc543d09
Hakan Uzum <hakan.uzum24@gmail.com>
2026-04-19 01:01:17 +0300

    [fix](mtmv) Support Cast and hour arithmetic in partition expressions
    ### What problem does this PR solve?
    
    Problem Summary: MTMV partition expressions with CAST and hour-offset
    arithmetic (hours_add/hours_sub) were being rejected as invalid implicit
    expressions during partition increment validation.
    
    ### What is changed and how does it work?
    
    Extended SUPPORT_EXPRESSION_TYPES in PartitionIncrementMaintainer to include:
    - Cast.class - Allow CAST expressions in partition columns
    - HoursAdd.class - Support hours_add arithmetic
    - HoursSub.class - Support hours_sub arithmetic
    
    This enables partition expressions like:
      date_trunc(date_add(cast(k2 as date), INTERVAL 3 HOUR), 'day')
    
    ### Release note
    
    None - Internal fix for MTMV partition expression validation
    
    ### Check List (For Author)
    
    - Test: Unit Test
      - Fixed MTMVPlanUtilTest.testPartitionExprPreservesCastInHourOffset
      - Fixed MTMVPlanUtilTest.testPartitionExprUsesLineageForAliasHourOffset
      - All 18 FE unit tests now pass
    - Behavior changed: No (enables previously blocked functionality)
    - Does this need documentation: No

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/PartitionIncrementMaintainer.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/exploration/mv/PartitionIncrementMaintainer.java
@@ -29,12 +29,15 @@ import org.apache.doris.nereids.rules.exploration.mv.RelatedTableInfo.RelatedTab
 import org.apache.doris.nereids.rules.expression.ExpressionNormalization;
 import org.apache.doris.nereids.rules.expression.ExpressionRewriteContext;
 import org.apache.doris.nereids.trees.expressions.CTEId;
+import org.apache.doris.nereids.trees.expressions.Cast;
 import org.apache.doris.nereids.trees.expressions.Expression;
 import org.apache.doris.nereids.trees.expressions.NamedExpression;
 import org.apache.doris.nereids.trees.expressions.Slot;
 import org.apache.doris.nereids.trees.expressions.SlotReference;
 import org.apache.doris.nereids.trees.expressions.WindowExpression;
 import org.apache.doris.nereids.trees.expressions.functions.scalar.DateTrunc;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.HoursAdd;
+import org.apache.doris.nereids.trees.expressions.functions.scalar.HoursSub;
 import org.apache.doris.nereids.trees.expressions.literal.Literal;
 import org.apache.doris.nereids.trees.expressions.visitor.DefaultExpressionRewriter;
 import org.apache.doris.nereids.trees.plans.JoinType;
@@ -91,7 +94,8 @@ public class PartitionIncrementMaintainer {
             DefaultPlanVisitor<Void, PartitionIncrementCheckContext> {
         public static final PartitionIncrementChecker INSTANCE = new PartitionIncrementChecker();
         public static final Set<Class<? extends Expression>> SUPPORT_EXPRESSION_TYPES =
-                ImmutableSet.of(DateTrunc.class, SlotReference.class, Literal.class);
+                ImmutableSet.of(DateTrunc.class, SlotReference.class, Literal.class, Cast.class,
+                        HoursAdd.class, HoursSub.class);
 
         @Override
         public Void visitLogicalProject(LogicalProject<? extends Plan> project,
