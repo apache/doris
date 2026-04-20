efc5a5c11591bbea2e1c277748ada921ed5ba2b1
w41ter <maochuan@selectdb.com>
2026-04-20 15:58:00 +0800

    fixup

--- fe/fe-core/src/test/java/org/apache/doris/fs/obj/S3ObjStorageTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/fs/obj/S3ObjStorageTest.java
@@ -65,8 +65,10 @@ import java.io.InputStream;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class S3ObjStorageTest {
