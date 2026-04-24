e3b28308f022155d0979c1a7f1bcbcdebb46291e
guoqiang <guoqiang@selectdb.com>
2026-04-24 11:55:53 +0800

    [test](regression) Remove external tag from broker load func
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: None
    
    Problem Summary: Remove the external suite tag from test_broker_load_func.
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test: Regression test
        - ./run-regression-test.sh --run -d external_table_p0/broker_load -s test_broker_load_func
        - git diff --cached --check
    - Behavior changed: No
    - Does this need documentation: No

--- regression-test/suites/external_table_p0/broker_load/test_broker_load_func.groovy
+++ regression-test/suites/external_table_p0/broker_load/test_broker_load_func.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_broker_load_func", "p0,external") {
+suite("test_broker_load_func", "p0") {
 
     String enabled = context.config.otherConfigs.get("enableHiveTest")
     if (enabled != null && enabled.equalsIgnoreCase("true")) {
