35f065d14b310c31777da584402d17eda527f322
zhiqiang-hhhh <seuhezhiqiang@163.com>
2026-04-30 10:48:10 +0800

    fix compile ut

--- be/test/storage/index/ann/extract_query_vector_test.cpp
+++ be/test/storage/index/ann/extract_query_vector_test.cpp
@@ -54,13 +54,8 @@ public:
 
     bool is_constant() const override { return _is_constant; }
 
-    Status execute_column(VExprContext* context, const Block* block, const Selector* selector,
-                          size_t count, ColumnPtr& result_column) const {
-        return Status::OK();
-    }
-
-    Status execute_column_impl(VExprContext* context, const Block* block, const Selector* selector,
-                               size_t count, ColumnPtr& result_column) const override {
+    Status execute_column(VExprContext* context, const Block* block, Selector* selector,
+                          size_t count, ColumnPtr& result_column) const override {
         return Status::OK();
     }
 
