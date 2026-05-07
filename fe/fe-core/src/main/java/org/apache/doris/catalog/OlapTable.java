d34b16c8fd3b67ed09f474e192090c35fb3d69c5
xy720 <22125576+xy720@users.noreply.github.com>
2026-05-07 20:39:59 +0800

    [bug](cloud restore) forbid restoring table with property 'light_schema change' = 'false' in cloud mode (#62914)
    Avoid restore non-light-schema-change table in cloud mode. Because
    non-light-schema-change table may have different schema with same schema
    version(which is always 0), which will cause very bad problem to
    metadata in cloud mode.
    
    For example:
    
    There is a partitioned table with 'light_schema change' = 'false', its
    tablet schema is
    
    Tablet a1's schema in Partition A before schema change
    
    ```
    schema version 0
    col1                                             uid=0
    col2                                             uid=1
    col3                                             uid=2
    col4                                             uid=3
    __delete_sign_column__                           uid=4
    ```
    
    Now we add column to this table, then:
    
    Tablet a1's schema in Partition A after schema change
    
    ```
    schema version 0
    col1                                             uid=0
    col2                                             uid=1
    col3                                             uid=2
    col4                                             uid=3
    __delete_sign_column__                           uid=4
    col5                                             uid=5
    ```
    
    Now we add partition B to this table, then
    
    Tablet b1's schema in Partition B is:
    
    ```
    schema version 0
    col1                                             uid=0
    col2                                             uid=1
    col3                                             uid=2
    col4                                             uid=3
    col5                                             uid=4
    __delete_sign_column__                           uid=5
    ```
    
    Then we will have two different tablet schema (a1 and b1) with same
    index id and same schema version 0 in this table.
    
    This should not happen in cloud mode. 
    
    if we backup and restore this table to cluster in cloud mode, the
    restored table will not be select correctly.
    
    Co-authored-by: lide <lide@apache.org>

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -807,6 +807,26 @@ public class OlapTable extends Table implements MTMVRelatedTableIf, GsonPostProc
     }
 
     /**
+     * Check properties before restore.
+     */
+    public Status checkPropertiesForRestore() {
+        if (tableProperty != null) {
+            if (Config.isCloudMode()) {
+                // Avoid restore non-light schema change table in cloud mode. Because non-light schema change table
+                // may have different schema with same schema version(which is always 0), which will cause very bad
+                // problem to metadata in cloud mode.
+                if (!tableProperty.getUseSchemaLightChange()) {
+                    String msg = String.format("Restoring table %s which has property `\"light_schema_change\""
+                            + " = \"false\"`, currently cloud mode only supports light schema change table."
+                            + " Please enable `light_schema_change` property before backup and restore.", name);
+                    return new Status(ErrCode.COMMON_ERROR, msg);
+                }
+            }
+        }
+        return Status.OK;
+    }
+
+    /**
      * Set the related properties when is_being_synced properties is true.
      *
      * Some properties, like storage_policy, colocate_with, are not supported by the ccr syncer.
