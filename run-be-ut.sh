82b7fc2a73cf054226fad7d0511cdc0030a97879
nooneuse <nooneuse@live.cn>
2026-05-12 20:39:31 +0800

    fix be ut build script (Part2)

--- run-be-ut.sh
+++ run-be-ut.sh
@@ -152,13 +152,13 @@ echo "Get params:
 echo "Build Backend UT"
 
 echo "install datasketches-cpp to thirdparty path before build backend ut"
-    cd "${DORIS_HOME}"
-    git submodule update --init --recursive contrib/datasketches-cpp
-    DATASKETCHES_BUILD_DIR="${DORIS_HOME}/contrib/datasketches-cpp"
-    cd "${DATASKETCHES_BUILD_DIR}"
-    "${CMAKE_CMD}" -S . -B build/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TP_INSTALLED_DIR -DBUILD_TESTS=OFF
-    "${CMAKE_CMD}" --build build/Release -t install
-    cd "${DORIS_HOME}"
+cd "${DORIS_HOME}"
+git submodule update --init --recursive contrib/datasketches-cpp
+DATASKETCHES_BUILD_DIR="${DORIS_HOME}/contrib/datasketches-cpp"
+cd "${DATASKETCHES_BUILD_DIR}"
+"${CMAKE_CMD}" -S . -B build/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${DORIS_HOME}"/thirdparty/installed -DBUILD_TESTS=OFF
+"${CMAKE_CMD}" --build build/Release -t install
+cd "${DORIS_HOME}"
 
 update_submodule() {
     local submodule_path=$1
