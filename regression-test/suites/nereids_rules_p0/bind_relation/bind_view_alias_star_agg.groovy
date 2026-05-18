36bb726e27308fb6b06b9b1a8159d4e785390a58
lichi <lichi@selectdb.com>
2026-05-18 17:37:27 +0800

    fix case

--- regression-test/suites/nereids_rules_p0/bind_relation/bind_view_alias_star_agg.groovy
+++ regression-test/suites/nereids_rules_p0/bind_relation/bind_view_alias_star_agg.groovy
@@ -20,33 +20,27 @@ suite("bind_view_alias_star_agg") {
     sql "set runtime_filter_mode=OFF"
     sql "SET enable_fallback_to_original_planner=false"
 
-    def viewName = "bind_view_alias_star_agg"
+    sql "DROP VIEW IF EXISTS v_bind_view_alias_star_agg"
 
-    try {
-        sql "DROP VIEW IF EXISTS ${viewName}"
+    sql """
+        create view v_bind_view_alias_star_agg as
+        with sale2 as (
+            select 1 as pack_factory, 2 as area, 3 as purchase_month, 4 as label_model, 50000 as mile_range
+        ),
+        sale3 as (
+            select c.*, s.id, s.id * 10000 as mile_range
+            from (
+                select pack_factory, area, purchase_month, label_model, max(mile_range) / 10000 as max_range
+                from sale2
+                group by pack_factory, area, purchase_month, label_model
+            ) c
+            join (select 1 as id) s
+            where s.id <= max_range
+        )
+        select * from sale3
+    """
 
-        sql """
-            create view ${viewName} as
-            with sale2 as (
-                select 1 as pack_factory, 2 as area, 3 as purchase_month, 4 as label_model, 50000 as mile_range
-            ),
-            sale3 as (
-                select c.*, s.id, s.id * 10000 as mile_range
-                from (
-                    select pack_factory, area, purchase_month, label_model, max(mile_range) / 10000 as max_range
-                    from sale2
-                    group by pack_factory, area, purchase_month, label_model
-                ) c
-                join (select 1 as id) s
-                where s.id <= max_range
-            )
-            select * from sale3
-        """
-
-        order_qt_select_view """
-            select * from ${viewName}
-        """
-    } finally {
-        sql "DROP VIEW IF EXISTS ${viewName}"
-    }
+    order_qt_select_view """
+        select * from v_bind_view_alias_star_agg
+    """
 }
