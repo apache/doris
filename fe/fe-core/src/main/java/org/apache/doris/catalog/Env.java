fcfa966f3ce8163522e6d4a6e3f8fe13cb598e3a
meiyi <meiyi@selectdb.com>
2026-05-22 12:25:39 +0800

    1

--- fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/Env.java
@@ -318,6 +318,7 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
