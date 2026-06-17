7c5967906277d9b1487c785174748249ca3198f8
TengJianPing <tengjianping@selectdb.com>
2026-06-17 10:21:05 +0800

    Update be/test/exprs/function/cast/cast_to_date_test.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- be/test/exprs/function/cast/cast_to_date_test.cpp
+++ be/test/exprs/function/cast/cast_to_date_test.cpp
@@ -92,6 +92,7 @@ TEST_F(FunctionCastTest, string_to_datev1_valid_case_strict_mode) {
             {{std::string("20240501 01")}, std::string("2024-05-01")},
             {{std::string("20230716 1920Z")}, std::string("2023-07-16")},
             {{std::string("20240501T0000")}, std::string("2024-05-01")},
+            {{std::string("20120102030405")}, std::string("2012-01-02")},
     };
     check_function_for_cast_strict_mode<DataTypeDate>(input_types, data_set);
 }
