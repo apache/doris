29390a08db85e18fedde5fe94b71160fb2863eec
minghong <zhouminghong@selectdb.com>
2026-06-26 18:12:03 +0800

    update tpch_sf0.1

--- tools/tpch-tools/constraints/drop-pk-uk-constraints.sql
+++ tools/tpch-tools/constraints/drop-pk-uk-constraints.sql
@@ -1,11 +1,11 @@
--- Drop all TPC-H Primary Key and Unique Key Constraints
-alter table region drop r_pk;
-alter table nation drop n_pk;
-alter table supplier drop s_pk;
-alter table customer drop c_pk;
-alter table part drop p_pk;
-alter table partsupp drop ps_pk;
-alter table orders drop o_pk;
-alter table lineitem drop l_pk;
-alter table region drop r_uk;
-alter table nation drop n_uk;
+-- drop constraint all TPC-H Primary Key and Unique Key Constraints
+alter table region drop constraint r_pk;
+alter table nation drop constraint n_pk;
+alter table supplier drop constraint s_pk;
+alter table customer drop constraint c_pk;
+alter table part drop constraint p_pk;
+alter table partsupp drop constraint ps_pk;
+alter table orders drop constraint o_pk;
+alter table lineitem drop constraint l_pk;
+alter table region drop constraint r_uk;
+alter table nation drop constraint n_uk;
