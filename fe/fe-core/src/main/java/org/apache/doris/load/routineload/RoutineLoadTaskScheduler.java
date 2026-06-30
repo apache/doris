3edfeb78815c266cfd9635e7c4a1ac025204b915
hui lai <laihui@selectdb.com>
2026-06-30 19:38:47 +0800

    branch-4.1: [fix](job) lock routine load task renew on submit failure (#65007)
    pick https://github.com/apache/doris/pull/64731

--- fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadTaskScheduler.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadTaskScheduler.java
@@ -250,22 +250,34 @@ public class RoutineLoadTaskScheduler extends MasterDaemon {
                 routineLoadTaskInfo.getBeId(), errorMsg);
         routineLoadTaskInfo.setBeId(-1);
         RoutineLoadJob routineLoadJob = routineLoadManager.getJob(routineLoadTaskInfo.getJobId());
-        routineLoadJob.setOtherMsg(errorMsg);
+        RoutineLoadTaskInfo newTask;
 
-        // Check if this is a resource pressure error that should not be immediately rescheduled
-        if (errorMsg.contains("TOO_MANY_TASKS") || errorMsg.contains("MEM_LIMIT_EXCEEDED")) {
-            // submit task failed (such as TOO_MANY_TASKS/MEM_LIMIT_EXCEEDED error),
-            // but txn has already begun. Here we will still set the ExecuteStartTime of
-            // this task, which means we "assume" that this task has been successfully submitted.
-            // And this task will then be aborted because of a timeout.
-            // In this way, we can prevent the entire job from being paused due to submit errors,
-            // and we can also relieve the pressure on BE by waiting for the timeout period.
-            routineLoadTaskInfo.setExecuteStartTimeMs(System.currentTimeMillis());
-            return;
+        routineLoadJob.writeLock();
+        try {
+            routineLoadJob.setOtherMsg(errorMsg);
+
+            // Check if this is a resource pressure error that should not be immediately rescheduled
+            if (errorMsg.contains("TOO_MANY_TASKS") || errorMsg.contains("MEM_LIMIT_EXCEEDED")) {
+                // submit task failed (such as TOO_MANY_TASKS/MEM_LIMIT_EXCEEDED error),
+                // but txn has already begun. Here we will still set the ExecuteStartTime of
+                // this task, which means we "assume" that this task has been successfully submitted.
+                // And this task will then be aborted because of a timeout.
+                // In this way, we can prevent the entire job from being paused due to submit errors,
+                // and we can also relieve the pressure on BE by waiting for the timeout period.
+                routineLoadTaskInfo.setExecuteStartTimeMs(System.currentTimeMillis());
+                return;
+            }
+
+            if (routineLoadJob.getState() != JobState.RUNNING
+                    || !routineLoadJob.containsTask(routineLoadTaskInfo.getId())) {
+                return;
+            }
+
+            // for other errors (network issues, BE restart, etc.), reschedule immediately
+            newTask = routineLoadJob.unprotectRenewTask(routineLoadTaskInfo, false);
+        } finally {
+            routineLoadJob.writeUnlock();
         }
-
-        // for other errors (network issues, BE restart, etc.), reschedule immediately
-        RoutineLoadTaskInfo newTask = routineLoadJob.unprotectRenewTask(routineLoadTaskInfo, false);
         addTaskInQueue(newTask);
     }
 
