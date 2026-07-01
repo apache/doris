a7f6e2cd1f7ee222b5c3e2db72b69736029ec3ad
BiteTheDDDDt <xl@selectdb.com>
2026-07-01 18:29:36 +0800

    [chore](gensrc) Remove unused IDL structures
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: None
    
    Problem Summary: Several legacy thrift and protobuf structures are no longer referenced by Doris code or generated IDL entry points. This change removes the unused runtime filter target thrift structure and stale protobuf request/result/message definitions that are no longer used, reducing generated IDL surface area.
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test: Manual test
        - Ran `git diff --check`
        - Ran `DORIS_THIRDPARTY=/mnt/disk8/xiaolei/codespaces/doris/thirdparty make -C gensrc`
        - Verified no `TRuntimeFilterTargetParams` / `rid_to_target_param` references remain outside generated outputs
    - Behavior changed: No
    - Does this need documentation: No

--- gensrc/thrift/PaloInternalService.thrift
+++ gensrc/thrift/PaloInternalService.thrift
@@ -518,13 +518,6 @@ struct TScanRangeParams {
   2: optional i32 volume_id = -1
 }
 
-// deprecated
-struct TRuntimeFilterTargetParams {
-  1: required Types.TUniqueId target_fragment_instance_id
-  // The address of the instance where the fragment is expected to run
-  2: required Types.TNetworkAddress target_fragment_instance_addr
-}
-
 struct TRuntimeFilterTargetParamsV2 {
   1: required list<Types.TUniqueId> target_fragment_instance_ids
   // The address of the instance where the fragment is expected to run
@@ -535,10 +528,6 @@ struct TRuntimeFilterTargetParamsV2 {
 struct TRuntimeFilterParams {
   // Runtime filter merge instance address. Used if this filter has a remote target
   1: optional Types.TNetworkAddress runtime_filter_merge_addr
-
-  // keep 2/3/4/5 unset if BE is not used for merge 
-  // deprecated
-  2: optional map<i32, list<TRuntimeFilterTargetParams>> rid_to_target_param
 
   // Runtime filter ID to the runtime filter desc
   // Used if this filter has a remote target
