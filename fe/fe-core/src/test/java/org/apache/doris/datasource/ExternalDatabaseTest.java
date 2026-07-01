991933b2e58ea99a7569b00040af281e801f6a97
wenzhenghu <wenzhenghu.zju@gmail.com>
2026-07-01 14:02:20 +0800

    [chore](fe) Fix checkstyle issues in external database tests

--- fe/fe-core/src/test/java/org/apache/doris/datasource/ExternalDatabaseTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/datasource/ExternalDatabaseTest.java
@@ -42,7 +42,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExternalDatabaseTest extends TestWithFeService {
