9ec2d6eed0fe12227228816fd02e9f21984297f4
Gabriel <liwenqiang@selectdb.com>
2026-07-14 16:40:20 +0800

    [fix](backport) Handle schema readers without runtime state

--- be/src/format/parquet/vparquet_reader.cpp
+++ be/src/format/parquet/vparquet_reader.cpp
@@ -322,8 +322,8 @@ Status ParquetReader::_open_file() {
         ++_reader_statistics.open_file_num;
         _file_description.mtime =
                 _scan_range.__isset.modification_time ? _scan_range.modification_time : 0;
-        io::FileReaderOptions reader_options =
-                FileFactory::get_reader_options(_state->query_options(), _file_description);
+        io::FileReaderOptions reader_options = FileFactory::get_reader_options(
+                _state ? _state->query_options() : _default_query_options, _file_description);
         _file_reader = DORIS_TRY(io::DelegateReader::create_file_reader(
                 _profile, _system_properties, _file_description, reader_options,
                 io::DelegateReader::AccessMode::RANDOM, _io_ctx));
