1ff0bc162398f5ab942efee48174331e93d121b3
laihui <laihui@selectdb.com>
2026-07-15 18:47:50 +0800

    pick https://github.com/apache/doris/pull/65553

--- gensrc/thrift/FrontendService.thrift
+++ gensrc/thrift/FrontendService.thrift
@@ -643,6 +643,7 @@ struct TRLTaskTxnCommitAttachment {
     9: optional i64 loadCostMs
     10: optional TKafkaRLTaskProgress kafkaRLTaskProgress
     11: optional string errorLogUrl
+    12: optional string firstErrorMsg
 }
 
 struct TTxnCommitAttachment {
@@ -1663,6 +1664,7 @@ struct TRoutineLoadJob {
     19: optional i32 current_abort_task_num
     20: optional bool is_abnormal_pause
     21: optional string compute_group
+    22: optional string first_error_msg
 }
 
 struct TFetchRoutineLoadJobResult {
