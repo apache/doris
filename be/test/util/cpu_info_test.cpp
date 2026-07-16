18cde1c1306fc2a28e3709845bf374258b3fab5a
0AyanamiRei <3244156674@qq.com>
2026-07-16 15:15:22 +0800

    [test](be) Add coverage for refreshing CPU core count
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: #65690
    
    Problem Summary: Add unit coverage for the explicit CPU core count refresh semantics. The test verifies that the cached value remains stable until refresh_current_num_cores() is called, then reflects the latest configured limit, and restores the global configuration afterward.
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test: Unit test added but not run locally per request; clang-format 16 and git diff checks passed
    - Behavior changed: No
    - Does this need documentation: No

---
+++ be/test/util/cpu_info_test.cpp
@@ -0,0 +1,55 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "util/cpu_info.h"
+
+#include <gtest/gtest.h>
+
+#include "common/config.h"
+
+namespace doris {
+namespace {
+
+class NumCoresConfigGuard {
+public:
+    NumCoresConfigGuard() : _num_cores(config::num_cores) {}
+
+    ~NumCoresConfigGuard() {
+        config::num_cores = _num_cores;
+        CpuInfo::refresh_current_num_cores();
+    }
+
+private:
+    int _num_cores;
+};
+
+TEST(CpuInfoTest, RefreshCurrentNumCoresUsesLatestConfiguredLimit) {
+    NumCoresConfigGuard guard;
+
+    config::num_cores = 2;
+    CpuInfo::refresh_current_num_cores();
+    EXPECT_EQ(2, CpuInfo::get_current_num_cores());
+
+    config::num_cores = 4;
+    EXPECT_EQ(2, CpuInfo::get_current_num_cores());
+
+    CpuInfo::refresh_current_num_cores();
+    EXPECT_EQ(4, CpuInfo::get_current_num_cores());
+}
+
+} // namespace
+} // namespace doris
