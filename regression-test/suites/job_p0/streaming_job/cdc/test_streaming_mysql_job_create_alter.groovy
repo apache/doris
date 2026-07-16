c03b4c5963d82c3948e1779105ccd5efb0010193
wudi <wudi@selectdb.com>
2026-07-16 17:22:18 +0800

    [fix](regression) Fix Groovy string concatenation

--- regression-test/suites/job_p0/streaming_job/cdc/test_streaming_mysql_job_create_alter.groovy
+++ regression-test/suites/job_p0/streaming_job/cdc/test_streaming_mysql_job_create_alter.groovy
@@ -36,8 +36,8 @@ suite("test_streaming_mysql_job_create_alter", "p0,external,mysql,external_docke
         String s3_endpoint = getS3Endpoint()
         String bucket = getS3BucketName()
         String driver_url = "https://${bucket}.${s3_endpoint}/regression/jdbc_driver/mysql-connector-j-8.4.0.jar"
-        String normalizedJdbcUrl = "jdbc:mysql://${externalEnvIp}:${mysql_port}"
-                + "?yearIsDateType=false&tinyInt1isBit=false&useUnicode=true&characterEncoding=utf-8"
+        String normalizedJdbcUrl = "jdbc:mysql://${externalEnvIp}:${mysql_port}" +
+                "?yearIsDateType=false&tinyInt1isBit=false&useUnicode=true&characterEncoding=utf-8"
 
         // unexcepted source properties
         test {
