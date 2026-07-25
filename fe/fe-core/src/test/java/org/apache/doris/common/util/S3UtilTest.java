43271f0117c0a84bd6d028f6be90a748efe17822
Refrain <refrain520cc@gmail.com>
2026-07-25 12:52:57 +0800

    [fix](fe) Fix S3UtilTest import formatting
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: #65379
    
    Problem Summary: Remove the extra separator inside the third-party import group in S3UtilTest so FE Checkstyle can pass.
    
    ### Release note
    
    None
    
    ### Check List (For Author)
    
    - Test: No need to test (format-only change)
    - Behavior changed: No
    - Does this need documentation: No

--- fe/fe-core/src/test/java/org/apache/doris/common/util/S3UtilTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/common/util/S3UtilTest.java
@@ -23,7 +23,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
