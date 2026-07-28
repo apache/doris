104aa4ef0b766b863221c5ba37f23977129443a0
yujun <yujun@selectdb.com>
2026-07-28 11:27:49 +0800

    [fix](regression) Scope MTMV debug points by MV name
    Prevent background MTMV tasks from consuming execute-limited IVM fault injection points intended for a specific regression MV.
    
    Key changes:
    - Add MV-name filter points for partition-sync and IVM RPC failure injection.
    - Keep execute: 1 only on the action debug points and extract the FE predicates into helpers.
    - Update both IVM regression suites to configure and clean up filter/action points.
    
    Unit Test:
    - FE incremental compilation.
    - test_ivm_partition_sync_retry and test_ivm_execution_failure_no_fallback regression suites.

--- fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
@@ -108,6 +108,8 @@ public class MTMVTask extends AbstractTask {
     public static final int DEFAULT_REFRESH_PARTITION_NUM = 1;
     public static final String DEBUG_POINT_SKIP_PARTITION_SYNC =
             "MTMVTask.syncPartitionsIfNeeded.skip";
+    public static final String DEBUG_POINT_SKIP_PARTITION_SYNC_FILTER =
+            "MTMVTask.syncPartitionsIfNeeded.skip.filter";
 
     private static final Gson GSON = new Gson();
 
@@ -357,7 +359,7 @@ public class MTMVTask extends AbstractTask {
 
     private void syncPartitionsIfNeeded(ConnectContext ctx, List<TableIf> tableIfs)
             throws JobException, AnalysisException, DdlException, PartitionPlanningException {
-        if (DebugPointUtil.isEnable(DEBUG_POINT_SKIP_PARTITION_SYNC)) {
+        if (isSkipPartitionSyncDebugPointEnabled()) {
             LOG.info("Skip MTMV partition synchronization for debug point, mv={}, taskId={}",
                     mtmv.getName(), getTaskId());
             return;
@@ -394,6 +396,13 @@ public class MTMVTask extends AbstractTask {
                 MTMVPartitionUtil.addPartition(mtmv, partitionKeyDesc);
             }
         }
+    }
+
+    private boolean isSkipPartitionSyncDebugPointEnabled() {
+        String targetMvName = DebugPointUtil.getDebugParamOrDefault(
+                DEBUG_POINT_SKIP_PARTITION_SYNC_FILTER, "mv_name", "");
+        return mtmv.getName().equals(targetMvName)
+                && DebugPointUtil.isEnable(DEBUG_POINT_SKIP_PARTITION_SYNC);
     }
 
     private RefreshRequest resolveRefreshRequest() throws JobException {
