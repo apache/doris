1e6d0f3b84cc9be28c409824ff898739998d7682
Gabriel <liwenqiang@selectdb.com>
2026-08-02 12:13:58 +0800

    [fix](build) Remove unused FileQueryScanNodeTest import

--- fe/fe-core/src/test/java/org/apache/doris/datasource/FileQueryScanNodeTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/datasource/FileQueryScanNodeTest.java
@@ -49,7 +49,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 public class FileQueryScanNodeTest {
     private static final long MB = 1024L * 1024L;
