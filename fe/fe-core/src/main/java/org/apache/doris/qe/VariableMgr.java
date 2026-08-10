2467c930bc1b3a223583e6793bea6a224c6c0705
924060929 <lanhuajian@selectdb.com>
2026-08-11 07:48:32 +0800

    [improvement](fe) Enable Nereids distribute planner after upgrade
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: #66604
    
    Problem Summary: New installations default enable_nereids_distribute_planner to true, but clusters upgrading from a variable version below 400 restore the persisted global session default from their image. Clusters that persisted the previous false default would therefore continue using the legacy distribute planner after upgrading to 4.0. Add enable_nereids_distribute_planner to the existing variable-version-400 migration so that the 3.x-to-4.0 upgrade enables it. The migration uses the existing global-variable edit-log path and does not change clusters whose variable version is already 400.
    
    ### Release note
    
    Clusters upgrading to 4.0 now enable the Nereids distribute planner by default.
    
    ### Check List (For Author)
    
    - Test: Unit Test
        - `VariableMgrTest#testEnableNereidsDistributePlannerAfterUpgrade`
        - FE Checkstyle for fe-core
    - Behavior changed: Yes. Clusters upgrading from a variable version below 400 default to the Nereids distribute planner.
    - Does this need documentation: No

--- fe/fe-core/src/main/java/org/apache/doris/qe/VariableMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/VariableMgr.java
@@ -1083,6 +1083,9 @@ public class VariableMgr {
             VariableMgr.refreshDefaultSessionVariables(updateInfo,
                     SessionVariable.ENABLE_SQL_CACHE,
                     String.valueOf(true));
+            VariableMgr.refreshDefaultSessionVariables(updateInfo,
+                    SessionVariable.ENABLE_NEREIDS_DISTRIBUTE_PLANNER,
+                    String.valueOf(true));
         }
         if (currentVariableVersion < GlobalVariable.CURRENT_VARIABLE_VERSION) {
             VariableMgr.refreshDefaultSessionVariables(updateInfo,
