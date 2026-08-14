e49f64272478ea9bb95de7ed473104e3b24cd237
lidongyang <lidongyang@selectdb.com>
2026-08-14 11:53:54 +0800

    fix: wait for advance cache eviction deterministically

--- be/test/io/cache/block_file_cache_test.cpp
+++ be/test/io/cache/block_file_cache_test.cpp
@@ -7960,9 +7960,14 @@ TEST_F(BlockFileCacheTest, evict_in_advance) {
     ASSERT_EQ(cache.get_stats_unsafe()["index_queue_curr_size"], 0);
     ASSERT_EQ(cache.get_stats_unsafe()["normal_queue_curr_size"], cache_max);
 
-    config::file_cache_evict_in_advance_batch_bytes = 200000;     // evict 2 200000 blocks
-    config::enable_evict_file_cache_in_advance = true;            // enable evict in advance
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // wait for clear
+    config::file_cache_evict_in_advance_batch_bytes = 200000; // evict 2 200000 blocks
+    config::enable_evict_file_cache_in_advance = true;        // enable evict in advance
+    for (int retry = 0; retry < 100; ++retry) {
+        if (cache.get_stats_unsafe()["normal_queue_curr_size"] <= cache_max - 400000) {
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
     ASSERT_EQ(cache.get_stats_unsafe()["disposable_queue_curr_size"], 0);
     ASSERT_EQ(cache.get_stats_unsafe()["ttl_queue_curr_size"], 0);
     ASSERT_EQ(cache.get_stats_unsafe()["index_queue_curr_size"], 0);
