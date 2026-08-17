1a605ec9d081787a9612b3953cfdcd4862908d4c
Mryange <yanxuecheng@selectdb.com>
2026-08-17 20:57:01 +0800

    [fix](fe) Cover remaining microsecond interval functions
    ### What problem does this PR solve?
    
    Issue Number: None
    
    Related PR: #66843
    
    Problem Summary: The second-microsecond, microseconds, and milliseconds add/subtract functions still used concrete scale-6 candidate signatures. During overload selection, TIMESTAMPTZ inputs with scales below 6 could therefore match the DATETIMEV2 candidate before return-type computation. Use wildcard-scale candidates for the remaining functions, preserve the selected type family, and promote the argument and return type to scale 6. Extend the regression matrix so all twelve microsecond interval functions verify both return types and runtime values for TIMESTAMPTZ(0), TIMESTAMPTZ(3), and TIMESTAMPTZ(6).
    
    ### Release note
    
    Preserve TIMESTAMPTZ results for all microsecond interval arithmetic functions at every supported input scale.
    
    ### Check List (For Author)
    
    - Test: FE build and regression test
        - Regression test
    - Behavior changed: Yes, the remaining microsecond interval functions retain TIMESTAMPTZ and return scale 6
    - Does this need documentation: No

--- regression-test/suites/datatype_p0/timestamptz/test_timestamptz_microsecond_interval.groovy
+++ regression-test/suites/datatype_p0/timestamptz/test_timestamptz_microsecond_interval.groovy
@@ -37,60 +37,50 @@ suite("test_timestamptz_microsecond_interval") {
                 '2024-01-02 03:04:05.123456 +08:00')
     """
 
+    def microsecondExpressions = { String column ->
+        [
+            day_add: "date_add(${column}, interval '1 02:03:04.123456' day_microsecond)",
+            day_sub: "date_sub(${column}, interval '1 02:03:04.123456' day_microsecond)",
+            hour_add: "date_add(${column}, interval '02:03:04.123456' hour_microsecond)",
+            hour_sub: "date_sub(${column}, interval '02:03:04.123456' hour_microsecond)",
+            minute_add: "date_add(${column}, interval '03:04.123456' minute_microsecond)",
+            minute_sub: "date_sub(${column}, interval '03:04.123456' minute_microsecond)",
+            second_add: "date_add(${column}, interval '04.123456' second_microsecond)",
+            second_sub: "date_sub(${column}, interval '04.123456' second_microsecond)",
+            microseconds_add: "microseconds_add(${column}, 123456)",
+            microseconds_sub: "microseconds_sub(${column}, 123456)",
+            milliseconds_add: "milliseconds_add(${column}, 123)",
+            milliseconds_sub: "milliseconds_sub(${column}, 123)"
+        ]
+    }
+
+    def runtimeQuery = { String column ->
+        def expressions = microsecondExpressions(column)
+        """
+            select id,
+                ${expressions.values().join(",\n                ")}
+            from test_timestamptz_microsecond_interval
+            order by id
+        """
+    }
+
     ["ts0", "ts3", "ts6"].each { column ->
+        def expressions = microsecondExpressions(column)
         explain {
             sql """
                 select
-                    date_add(${column}, interval '1 02:03:04.123456' day_microsecond) as day_add,
-                    date_sub(${column}, interval '1 02:03:04.123456' day_microsecond) as day_sub,
-                    date_add(${column}, interval '02:03:04.123456' hour_microsecond) as hour_add,
-                    date_sub(${column}, interval '02:03:04.123456' hour_microsecond) as hour_sub,
-                    date_add(${column}, interval '03:04.123456' minute_microsecond) as minute_add,
-                    date_sub(${column}, interval '03:04.123456' minute_microsecond) as minute_sub
+                    ${expressions.collect { alias, expression ->
+                        "${expression} as ${alias}"
+                    }.join(",\n                    ")}
                 from test_timestamptz_microsecond_interval
             """
-            checkSlotTypeOf("day_add", "timestamptz(6)")
-            checkSlotTypeOf("day_sub", "timestamptz(6)")
-            checkSlotTypeOf("hour_add", "timestamptz(6)")
-            checkSlotTypeOf("hour_sub", "timestamptz(6)")
-            checkSlotTypeOf("minute_add", "timestamptz(6)")
-            checkSlotTypeOf("minute_sub", "timestamptz(6)")
+            expressions.keySet().each { alias ->
+                checkSlotTypeOf(alias, "timestamptz(6)")
+            }
         }
     }
 
-    order_qt_timestamptz_microsecond_interval_scale_0 """
-        select id,
-            date_add(ts0, interval '1 02:03:04.123456' day_microsecond),
-            date_sub(ts0, interval '1 02:03:04.123456' day_microsecond),
-            date_add(ts0, interval '02:03:04.123456' hour_microsecond),
-            date_sub(ts0, interval '02:03:04.123456' hour_microsecond),
-            date_add(ts0, interval '03:04.123456' minute_microsecond),
-            date_sub(ts0, interval '03:04.123456' minute_microsecond)
-        from test_timestamptz_microsecond_interval
-        order by id
-    """
-
-    order_qt_timestamptz_microsecond_interval_scale_3 """
-        select id,
-            date_add(ts3, interval '1 02:03:04.123456' day_microsecond),
-            date_sub(ts3, interval '1 02:03:04.123456' day_microsecond),
-            date_add(ts3, interval '02:03:04.123456' hour_microsecond),
-            date_sub(ts3, interval '02:03:04.123456' hour_microsecond),
-            date_add(ts3, interval '03:04.123456' minute_microsecond),
-            date_sub(ts3, interval '03:04.123456' minute_microsecond)
-        from test_timestamptz_microsecond_interval
-        order by id
-    """
-
-    order_qt_timestamptz_microsecond_interval_scale_6 """
-        select id,
-            date_add(ts6, interval '1 02:03:04.123456' day_microsecond),
-            date_sub(ts6, interval '1 02:03:04.123456' day_microsecond),
-            date_add(ts6, interval '02:03:04.123456' hour_microsecond),
-            date_sub(ts6, interval '02:03:04.123456' hour_microsecond),
-            date_add(ts6, interval '03:04.123456' minute_microsecond),
-            date_sub(ts6, interval '03:04.123456' minute_microsecond)
-        from test_timestamptz_microsecond_interval
-        order by id
-    """
+    order_qt_timestamptz_microsecond_interval_scale_0 runtimeQuery("ts0")
+    order_qt_timestamptz_microsecond_interval_scale_3 runtimeQuery("ts3")
+    order_qt_timestamptz_microsecond_interval_scale_6 runtimeQuery("ts6")
 }
