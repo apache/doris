a95b8446f16c97e04ce87dd0d7f70f02c5337507
yangtao555 <yangtao555@jd.com>
2026-08-19 20:33:27 +0800

    [opt](partition prunning) Infer bare-column predicates from monotonic functions for partition pruning

--- regression-test/suites/nereids_rules_p0/partition_prune/always_mono_func.groovy
+++ regression-test/suites/nereids_rules_p0/partition_prune/always_mono_func.groovy
@@ -123,11 +123,11 @@ suite("always_mono_func") {
     // year
     explain {
         sql """ select * from always_mono_func where year(dt) >= 2019 """
-        contains("partitions=3/5 (p3,p4,p5)")
+        contains("partitions=2/5 (p4,p5)")
     }
     explain {
         sql """select * from always_mono_func where year(dt) < 2019 and year(dt) > 2017"""
-        contains("partitions=2/5 (p2,p3)")
+        contains("partitions=1/5 (p3)")
     }
     explain {
         sql """select * from always_mono_func where year(dt) <2023"""
@@ -140,15 +140,15 @@ suite("always_mono_func") {
     }
     explain {
         sql """select * from always_mono_func where to_monday(dt) ='2019-01-01' """
-        contains("partitions=2/5 (p1,p4)")
+        contains("partitions=1/5 (p4)")
     }
     explain {
         sql """select * from always_mono_func where to_monday(dt) >='2018-01-01' and to_monday(dt) <'2019-01-01' """
-        contains("partitions=4/5 (p1,p2,p3,p4)")
+        contains("partitions=2/5 (p3,p4)")
     }
     explain {
         sql """ select * from always_mono_func where to_monday(dt) >= "2019-01-01" """
-        contains("partitions=3/5 (p1,p4,p5)")
+        contains("partitions=2/5 (p4,p5)")
     }
     // to_date
     explain {
@@ -169,7 +169,7 @@ suite("always_mono_func") {
     }
     explain {
         sql """select * from always_mono_func where to_date(dt) >='2018-01-01' and to_date(dt) <'2019-01-01' """
-        contains("partitions=2/5 (p2,p3)")
+        contains("partitions=1/5 (p3)")
     }
     explain {
         sql """ select * from always_mono_func where to_date(dt) > "2019-01-01" """
