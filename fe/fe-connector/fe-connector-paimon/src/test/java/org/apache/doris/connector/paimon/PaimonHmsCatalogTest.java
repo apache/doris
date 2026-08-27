d3eb044d911a1b423e17ae9101325a894e8f4ba6
moke-HU <25567926679@qq.com>
2026-08-28 06:39:07 +0800

    [test](paimon) fix PaimonHmsCatalogTest for CatalogContext API change
    Same root cause as 74ff7c08d60: the test constructed HiveCatalog with
    a raw Options where the 5-arg overload expects CatalogContext. Wrap
    via CatalogContext.create(options).

--- fe/fe-connector/fe-connector-paimon/src/test/java/org/apache/doris/connector/paimon/PaimonHmsCatalogTest.java
+++ fe/fe-connector/fe-connector-paimon/src/test/java/org/apache/doris/connector/paimon/PaimonHmsCatalogTest.java
@@ -104,7 +104,7 @@ public class PaimonHmsCatalogTest {
         HiveConf hiveConf = new HiveConf();
         hiveConf.set("hive.metastore.sasl.enabled", "true");
         HiveCatalog hiveCatalog = new HiveCatalog(
-                new RecordingFileIO(), hiveConf, null, options, "record:///warehouse");
+                new RecordingFileIO(), hiveConf, null, CatalogContext.create(options), "record:///warehouse");
         AtomicReference<Table> createdTable = new AtomicReference<>();
         AtomicReference<String> rpcUser = new AtomicReference<>();
         IMetaStoreClient client = (IMetaStoreClient) Proxy.newProxyInstance(
