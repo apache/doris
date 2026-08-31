4237cfa09bd480fd70cdefb3492dbe15467e6063
linzhenqi <linzhenqi@selectdb.com>
2026-08-31 13:27:03 +0800

    use versioned libc wrappers for lance-c compatibility

--- be/src/glibc-compatibility/musl/preadv.c
+++
@@ -1,9 +0,0 @@
-#define _DEFAULT_SOURCE
-#include <sys/uio.h>
-#include <unistd.h>
-
-#include "syscall.h"
-
-ssize_t preadv(int fd, const struct iovec* iov, int count, off_t offset) {
-    return syscall(SYS_preadv, fd, iov, count, (long)(offset), (long)(offset >> 32));
-}
