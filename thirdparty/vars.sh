8a6c87fd7ddc5842e6a91d5f9b2fddb83efe67a5
dengfangyuan <dengfangyuan@kuaishou.com>
2026-09-01 18:04:28 +0800

    [chore](thirdparty) update paimon-rust-cabdeb9

--- thirdparty/vars.sh
+++ thirdparty/vars.sh
@@ -580,10 +580,10 @@ LANCE_C_SOURCE="lance-c-0.1.9"
 LANCE_C_MD5SUM="7138ed44e92d4bc91d5b522a6b92ed64"
 
 # paimon-rust
-PAIMON_RUST_DOWNLOAD="https://github.com/pzhdfy/paimon-rust/archive/refs/tags/paimon-rust-2fb5e49.tar.gz"
-PAIMON_RUST_NAME="paimon-rust-2fb5e49.tar.gz"
-PAIMON_RUST_SOURCE="paimon-rust-paimon-rust-2fb5e49"
-PAIMON_RUST_MD5SUM="341aef6bd75743e51cdb960260645d69"
+PAIMON_RUST_DOWNLOAD="https://github.com/pzhdfy/paimon-rust/archive/refs/tags/paimon-rust-cabdeb9.tar.gz"
+PAIMON_RUST_NAME="paimon-rust-cabdeb9.tar.gz"
+PAIMON_RUST_SOURCE="paimon-rust-paimon-rust-cabdeb9"
+PAIMON_RUST_MD5SUM="a9b4146517242f0c461fec123ea521db"
 
 # all thirdparties which need to be downloaded is set in array TP_ARCHIVES
 export TP_ARCHIVES=(
