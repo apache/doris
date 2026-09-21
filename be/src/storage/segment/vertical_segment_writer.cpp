25458421b17e907e431712c6e1890f9a96c77784
yiguolei <guolei@selectdb.com>
2026-09-21 15:44:33 +0800

    [refactor](pb) remove default value from column meta pb

--- be/src/storage/segment/vertical_segment_writer.cpp
+++ be/src/storage/segment/vertical_segment_writer.cpp
@@ -121,7 +121,6 @@ void VerticalSegmentWriter::_init_column_meta(ColumnMetaPB* meta, uint32_t colum
     meta->set_encoding(EncodingInfo::resolve_default_encoding(opts.storage_format, column));
     meta->set_compression(_opts.compression_type);
     meta->set_is_nullable(column.is_nullable());
-    meta->set_default_value(column.default_value());
     meta->set_precision(column.precision());
     meta->set_frac(column.frac());
     if (column.has_path_info()) {
